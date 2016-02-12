386eae520d48942df3a324e175cf3f913457eb6b
Shane Russell <shane1337@gmail.com>
2016-02-12 10:48:29 -0500

    Add shell and first input for Form 8

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -1,6 +1,7 @@
 class CertificationsController < ApplicationController
   def new
     render "mismatched_documents" unless appeal.ready_to_certify?
+    @form8 = Form8.new
   end
 
   private
---
+++ app/models/form8.rb
@@ -0,0 +1,4 @@
+class Form8
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+end
