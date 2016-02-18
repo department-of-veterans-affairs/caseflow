ff73f66522fd51d07f186787479abc30c2beeab4
Schwartz <Ori.Schwartz@va.gov>
2016-02-18 15:15:36 -0500

    add testing

---
+++ spec/javascripts/dropdown_spec.js
@@ -0,0 +1,22 @@
+//= require jquery
+//= require dropdown
+
+describe("Dropdown", function() {
+  var $testContainer;
+
+  beforeEach(function() {
+    $testContainer = $("#konacha");
+  });
+
+  context(".bind", function() {
+    it("adds active class to dropdown when clicked", function() {
+      var $dropdown = $('<a class="cf-dropdown-trigger" href="#menu">Trigger</a><ul id="menu"><li>Menu Item</li>/ul>');
+      $dropdown.appendTo($testContainer);
+
+      Dropdown.bind();
+      expect($('#menu').hasClass('active')).to.eq(false)
+      $dropdown.click();
+      expect($('#menu').hasClass('active')).to.eq(true)
+    });
+  });
+});
