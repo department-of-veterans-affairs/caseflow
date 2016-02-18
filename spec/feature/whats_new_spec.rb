66b8819f1e0554732f778d741d0880adbeff1cc2
Shane Russell <shane1337@gmail.com>
2016-02-18 17:04:13 -0500

    Fix what's new spec to work with sniffybara
    Sniffybara used to only be the driver for the first spec,
    now that it's the driver for all the specs, this spec needed
    some updating

--- spec/feature/whats_new_spec.rb
+++ spec/feature/whats_new_spec.rb
@@ -2,14 +2,18 @@ require "rails_helper"
 
 RSpec.feature "What's New" do
   scenario "Initial visit shows what's new indicator" do
+    Fakes::AppealRepository.records = { "1234C" => Fakes::AppealRepository.appeal_not_ready }
+
     visit "certifications/new/1234C"
-    expect(find("#whats-new-item")["class"]).to eq("cf-nav-whatsnew")
+    expect(page).to have_css("#whats-new-item.cf-nav-whatsnew", visible: false)
   end
 
   scenario "What's new indicator is reset after visiting /whats-new" do
+    Fakes::AppealRepository.records = { "1234C" => Fakes::AppealRepository.appeal_not_ready }
+
     visit "certifications/new/1234C"
-    expect(find("#whats-new-item")["class"]).to eq("cf-nav-whatsnew")
+    expect(page).to have_css("#whats-new-item.cf-nav-whatsnew", visible: false)
     visit "/whats-new"
-    expect(find("#whats-new-item")["class"]).to be_falsy
+    expect(page).to_not have_css("#whats-new-item.cf-nav-whatsnew", visible: false)
   end
 end
