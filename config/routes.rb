8b37ad59993cee604206d607c444309284fcdb13
Schwartz <ori.schwartz@va.gov>
2016-02-18 10:50:05 -0500

    whats new page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :certifications, path_names: { new: "new/:vacols_id" }
 
+  get 'whats_new'=> 'whats_new#show'
+
   # Example resource route with options:
   #   resources :products do
   #     member do
