e32a8fe6f3ff289e454199f80453d0ddc9303cba
Schwartz <Ori.Schwartz@va.gov>
2016-02-23 20:56:30 -0500

    move service out of controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,14 +37,3 @@ class ApplicationController < ActionController::Base
     end
   end
 end
-
-class WhatsNewService
-  def self.determine_version
-    update_contents = File.read("app/views/whats_new/show.html.erb")
-    update_contents.hash.to_s
-  end
-
-  def self.version
-    @version ||= WhatsNewService.determine_version
-  end
-end
