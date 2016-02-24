58922e19eaca44fcfddd2980c93afd942c4beedb
Schwartz <Ori.Schwartz@va.gov>
2016-02-23 20:56:02 -0500

    move services to /services

---
+++ app/services/pdf_service.rb
@@ -0,0 +1,36 @@
+require 'pdf_forms'
+
+class PdfService
+
+  def self.pdf_forms
+    @pdf_forms ||= PdfForms.new("pdftk")
+  end
+
+  def self.save_form!(input_pdf_path:, form_values:)
+    output_pdf_path = completed_pdf_path
+
+    tmp_location = "#{output_pdf_path}.tmp"
+    self.pdf_forms.fill_form(input_pdf_path, tmp_location, form_values, flatten: true)
+
+    # Run it through `pdftk cat`. The reason for this is that editable PDFs have
+    # an RSA signature on them which proves they are genuine. pdftk tries to
+    # maintain the editability of a PDF after processing it, but then the
+    # signature doesn't match. The result is that (without `pdftk cat`) Acrobat
+    # shows a warning (other PDF viewers don't care).
+    self.pdf_forms.call_pdftk(tmp_location, "cat", "output", output_pdf_path)
+
+    # Remove it from the tmp_location, leaving it only in final_location
+    File.delete(tmp_location)
+
+    output_pdf_path
+  end
+
+  def self.completed_pdf_path
+    absolute_path_of("form8-#{SecureRandom.hex}.pdf")
+  end
+
+  def self.absolute_path_of(file_name)
+    File.join(Rails.root, 'tmp', 'forms', file_name)
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module CaseflowCertification
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('services')
   end
 end
