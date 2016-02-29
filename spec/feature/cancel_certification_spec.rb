f7712769d5540ecfce7b535c03313684bf2966da
Schwartz <Ori.Schwartz@va.gov>
2016-02-29 14:05:34 -0500

    feature test

---
+++ spec/feature/cancel_certification_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Cancel certification" do
+  scenario "Click cancel link and confirm modal" do
+    User.authenticate!
+
+    Fakes::AppealRepository.records = {
+      "5555C" => Fakes::AppealRepository.appeal_ready_to_certify
+    }
+
+    visit "certifications/new/5555C"
+    click_on "Cancel certification"
+    expect(page).to have_content("Are you sure you can't certify this case?")
+    click_on "Yes, I'm sure"
+    expect(page).to have_content("Case not certified")
+  end
+end
