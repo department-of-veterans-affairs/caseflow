49f8c70ddee4dc66c28ff07f81e2a183eedf1d13
Schwartz <Ori.Schwartz@va.gov>
2016-03-02 16:46:15 -0500

    update test

--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -1,13 +1,16 @@
 require "rails_helper"
 
 RSpec.feature "Login" do
-  scenario "login with valid credentials" do
+
+  before do
     Fakes::AppealRepository.records = {
-      "1234C" => Fakes::AppealRepository.appeal_ready_to_certify
+        "1234C" => Fakes::AppealRepository.appeal_ready_to_certify
     }
 
     Fakes::AuthenticationService.vacols_regional_offices = { "TESTRO" => "pa55word!" }
+  end
 
+  scenario "login with valid credentials" do
     visit "certifications/new/1234C"
 
     # SSOI login
@@ -28,13 +31,22 @@ RSpec.feature "Login" do
 
   scenario "logging out redirects to login page" do
     visit "certifications/new/1234C"
+
+    # SSOI login
+    expect(page).to have_content("User Info")
+    fill_in "Name:", with: "Joe"
+    fill_in "Email:", with: "xyz@va.gov"
+    click_on "Sign In"
+
+    # vacols login
+    expect(page).to have_content("VACOLS credentials")
     fill_in "VACOLS Login ID", with: "TESTRO"
     fill_in "VACOLS Password", with: "pa55word!"
     click_on "Login"
 
-    click_on "DSUSER (TESTRO)"
+    click_on "xyz@va.gov (TESTRO)"
     click_on "Sign out"
     visit "certifications/new/1234C"
-    expect(page).to have_current_path(login_path)
+    expect(page).to have_current_path(Rails.application.config.ssoi_login_path)
   end
 end
