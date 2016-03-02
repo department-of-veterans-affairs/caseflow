290ba559a278a6b32abb752d2c95d01bf8fb037e
Shane Russell <shane.russell.va@gmail.com>
2016-03-02 17:49:39 -0500

    Add connect_vbms and integrate it with document loading

--- app/services/appeal_repository.rb
+++ app/services/appeal_repository.rb
@@ -1,12 +1,20 @@
+require "vbms"
+
 class AppealRepository
   def self.find(vacols_id, _args = {})
     case_record = Records::Case.includes(:folder, :correspondent).find(vacols_id)
 
-    Appeal.from_records(
+    appeal = Appeal.from_records(
       case_record: case_record,
       folder_record: case_record.folder,
       correspondent_record: case_record.correspondent
     )
+
+    appeal.documents = fetch_documents_for(appeal).map do |vbms_document|
+      Document.from_vbms_document(vbms_document)
+    end
+
+    appeal
   end
 
   def self.cerify(_appeal)
@@ -17,5 +25,38 @@ class AppealRepository
     #  @kase.bf41stat = Date.strptime(params[:certification_date], Date::DATE_FORMATS[:va_date])
     #  @kase.save
     #  @kase.efolder_case.upload_form8(corr.snamef, corr.snamemi, corr.snamel, params[:file_name])
+  end
+
+  def self.sanitize_vbms_id(vbms_id)
+    "0000#{vbms_id.gsub(/[^0-9]/, '')}"[-8..-1]
+  end
+
+  def self.fetch_documents_for(appeal)
+    @vbms_client ||= init_vbms_client
+
+    request = VBMS::Requests::ListDocuments.new(sanitize_vbms_id(appeal.vbms_id))
+    @vbms_client.send_request(request)
+  end
+
+  def self.vbms_config
+    config = Rails.application.secrets.vbms.clone
+
+    %w(keyfile saml key cacert cert).each do |file|
+      config[file] = File.join(config["env_dir"], config[file])
+    end
+
+    config
+  end
+
+  def self.init_vbms_client
+    VBMS::Client.new(
+      vbms_config["url"],
+      vbms_config["keyfile"],
+      vbms_config["saml"],
+      vbms_config["key"],
+      vbms_config["keypass"],
+      vbms_config["cacert"],
+      vbms_config["cert"]
+    )
   end
 end
---
+++ spec/models/document_spec.rb
@@ -0,0 +1,10 @@
+
+describe User do
+  context ".from_vbms_document" do
+    let(:vbms_document) do
+      OpenStruct.new(doc_type: "179", received_at: "TEST")
+    end
+    subject { Document.from_vbms_document(vbms_document) }
+    it { is_expected.to have_attributes(type: :form9, received_at: "TEST") }
+  end
+end
