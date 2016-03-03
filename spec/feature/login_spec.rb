2229c48544fa064d01d7ee923858fe77ece5f89b
Schwartz <Ori.Schwartz@va.gov>
2016-03-03 10:10:38 -0500

    make sure RO matches appeal (will 403 otherwise)

--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Login" do
         "1234C" => Fakes::AppealRepository.appeal_ready_to_certify
     }
 
-    Fakes::AuthenticationService.vacols_regional_offices = { "TESTRO" => "pa55word!" }
+    Fakes::AuthenticationService.vacols_regional_offices = { "DSUSER" => "pa55word!" }
   end
 
   scenario "login with valid credentials" do
@@ -20,12 +20,12 @@ RSpec.feature "Login" do
 
     # vacols login
     expect(page).to have_content("VACOLS credentials")
-    fill_in "VACOLS Login ID", with: "TESTRO"
+    fill_in "VACOLS Login ID", with: "DSUSER"
     fill_in "VACOLS Password", with: "pa55word!"
     click_on "Login"
 
     expect(page).to have_current_path(new_certification_path(vacols_id: "1234C"))
-    expect(find("#menu-trigger")).to have_content("xyz@va.gov (TESTRO)")
+    expect(find("#menu-trigger")).to have_content("xyz@va.gov (DSUSER)")
   end
 
   scenario "logging out redirects to login page" do
@@ -39,11 +39,11 @@ RSpec.feature "Login" do
 
     # vacols login
     expect(page).to have_content("VACOLS credentials")
-    fill_in "VACOLS Login ID", with: "TESTRO"
+    fill_in "VACOLS Login ID", with: "DSUSER"
     fill_in "VACOLS Password", with: "pa55word!"
     click_on "Login"
 
-    click_on "xyz@va.gov (TESTRO)"
+    click_on "xyz@va.gov (DSUSER)"
     click_on "Sign out"
     visit "certifications/new/1234C"
     expect(page).to have_current_path(Rails.application.config.ssoi_login_path)
