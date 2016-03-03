f4ad8bb155e9bd7bca9cc2b55013e5d1cfe2fcf4
Shane Russell <shane1337@gmail.com>
2016-03-03 11:00:24 -0500

    Copy help page from caseflow classic
    Updates to come
    fixes #14

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -120,13 +120,49 @@ module ApplicationHelper
     </svg>
   HTML
 
+  WINDOWS_LOGO_OLD_ICON = <<-HTML.freeze
+    <svg version="1.1" class="cf-icon-win95" width="12.4" height="9.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         viewBox="0 0 12.4 9.2" style="enable-background:new 0 0 12.4 9.2;" xml:space="preserve">
+      <g>
+        <path d="M0.9,1v0.3L0.5,1.5V1.1L0.9,1z M0.9,4.3v0.3L0.5,4.8V4.5L0.9,4.3z M0.9,7.7v0.4L0.5,8.2V7.9L0.9,7.7z M0.9,2.1v0.3L0.6,2.5
+          V2.2L0.9,2.1z M0.9,3.2v0.3L0.6,3.7V3.4L0.9,3.2z M0.9,5.5v0.3L0.6,5.9V5.6L0.9,5.5z M0.9,6.6v0.3L0.6,7.1V6.8L0.9,6.6z M2,1.2v0.4
+          L1.4,1.8V1.4L2,1.2z M2,4.6V5L1.4,5.2V4.8L2,4.6z M2,8v0.4L1.4,8.6V8.2L2,8z M1.9,2.3v0.4L1.5,2.9V2.5L1.9,2.3z M1.9,3.5v0.4L1.5,4
+          V3.6L1.9,3.5z M1.9,5.7v0.4L1.5,6.3V5.9L1.9,5.7z M1.9,6.9v0.4L1.5,7.4V7L1.9,6.9z M3,1.4v0.5L2.3,2.2V1.7L3,1.4z M3,4.8v0.5
+          L2.3,5.6V5.1L3,4.8z M3,8.2v0.5L2.3,8.9V8.4L3,8.2z M3,2.6V3L2.4,3.2V2.8L3,2.6z M3,3.7v0.4L2.4,4.4V4L3,3.7z M3,6v0.4L2.4,6.6V6.2
+          L3,6z M3,7.1v0.4L2.4,7.7V7.3L3,7.1z M4.1,1.4V2L3.2,2.4V1.7L4.1,1.4z M4.1,4.8v0.6L3.2,5.8V5.1L4.1,4.8z M4.1,8.1v0.6L3.2,9.1V8.5
+          L4.1,8.1z M4.1,2.6v0.6L3.3,3.4V2.9L4.1,2.6z M4.1,3.7v0.6L3.3,4.6V4L4.1,3.7z M4.1,6v0.6L3.3,6.8V6.2L4.1,6z M4.1,7v0.6L3.3,7.9
+          V7.3L4.1,7z M5.3,1.3v0.8l-1,0.4V1.7L5.3,1.3z M5.3,4.7v0.8l-1,0.4V5.1L5.3,4.7z M5.3,8v0.8l-1,0.4V8.4L5.3,8z M5.2,2.5v0.7
+          L4.4,3.5V2.9L5.2,2.5z M5.2,3.6v0.7L4.4,4.6V3.9L5.2,3.6z M5.2,5.9v0.7L4.4,6.9V6.2L5.2,5.9z M5.2,7v0.7L4.4,8V7.3L5.2,7z M6.4,0.9
+          V2l-1,0.4V1.4C5.8,1.2,6.1,1,6.4,0.9z M6.4,2.1v1l-1,0.4v-1L6.4,2.1z M6.4,3.2v1l-1,0.4v-1L6.4,3.2z M6.4,4.3v1l-1,0.4v-1L6.4,4.3z
+           M6.4,5.4v1l-1,0.4v-1L6.4,5.4z M6.4,6.6v1L5.4,8V7L6.4,6.6z M6.4,7.7v1.1C6,8.9,5.7,9,5.4,9.2V8.1L6.4,7.7z M11.8,0.9v7.9
+          c-0.7-0.4-1.5-0.7-2.5-0.7c-0.8,0-1.8,0.2-2.8,0.5V7.6c0.5-0.2,1.1-0.4,1.8-0.5V4.6C7.8,4.6,7.2,4.8,6.6,5.1V4.4
+          C7.1,4.1,7.7,4,8.3,3.9V1.4C7.8,1.5,7.2,1.7,6.6,1.9V0.8c0.9-0.4,1.8-0.6,2.7-0.6C10.2,0.2,11,0.5,11.8,0.9z M10.8,1.6
+          c-0.4-0.2-0.9-0.3-1.5-0.3c-0.1,0-0.1,0-0.2,0v2.5l0.2,0c0.5,0,1,0.1,1.5,0.2V1.6z M10.8,4.8c-0.5-0.2-1-0.3-1.5-0.3
+          c-0.1,0-0.1,0-0.2,0v2.6h0.2c0.6,0,1.1,0.1,1.5,0.2V4.8z M11.5,9.1V9h-0.1v0h0.2v0h-0.1L11.5,9.1L11.5,9.1z M11.6,9.1V8.9h0
+          l0.1,0.2l0.1-0.2h0v0.2h0V9l-0.1,0.2h0L11.6,9L11.6,9.1L11.6,9.1z"/>
+      </g>
+    </svg>
+  HTML
+
+  WINDOWS_LOGO_ICON = <<-HTML.freeze
+    <svg version="1.1" width="11.9" height="11.7" class="cf-icon-win8" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 11.9 11.7" style="enable-background:new 0 0 11.9 11.7;" xml:space="preserve"><title>Windows 8 or higher icon</title>
+    <g>
+      <path d="M4.9,5.6H0.4V1.8l4.6-0.6V5.6z M4.9,10.5L0.4,9.9V6.2h4.6V10.5z M11.5,5.6H5.4V1.1l6.1-0.8V5.6z M11.5,11.4l-6.1-0.8V6.2
+        h6.1V11.4z"/>
+    </g>
+    </svg>
+  HTML
+
   def svg_icon(name)
     {
       missing: MISSING_ICON,
       found: FOUND_ICON,
       appeal: APPEAL_ICON,
       close: CLOSE_ICON,
-      loading: LOADING_ICON
+      loading: LOADING_ICON,
+      windows_logo_old: WINDOWS_LOGO_OLD_ICON,
+      windows_logo: WINDOWS_LOGO_ICON
     }[name].html_safe
   end
 
