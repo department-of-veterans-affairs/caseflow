2812e186a0949a2bc6e281995567a620a2a2b88e
Schwartz <Ori.Schwartz@va.gov>
2016-03-08 13:49:16 -0500

    remove puts statement

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,6 @@
 class ErrorsController < ApplicationController
-
   def show
     status_code = params[:status_code]
-    puts "errors/#{status_code}"
     render template: "errors/#{status_code}", status: status_code
   end
-
 end
