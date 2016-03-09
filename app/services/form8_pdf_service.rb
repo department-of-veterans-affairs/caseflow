32f22bccc6387570c1838e9a07d48dc64619b1aa
Schwartz <Ori.Schwartz@va.gov>
2016-03-09 15:40:38 -0500

    lint

--- app/services/form8_pdf_service.rb
+++ app/services/form8_pdf_service.rb
@@ -146,6 +146,6 @@ class Form8PdfService
   end
 
   def self.pdf_forms
-    @pdf_forms ||= PdfForms.new("pdftk", :data_format => 'XFdf')
+    @pdf_forms ||= PdfForms.new("pdftk", :data_format => "XFdf")
   end
 end
