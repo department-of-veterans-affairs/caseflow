49022b2543c5c55cfa1cac7bd5eabf1eb24d4fa3
Schwartz <Ori.Schwartz@va.gov>
2016-03-11 14:39:41 -0500

    alias root to help page to be consistent with prod env, define canonical route above alias

--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -81,6 +81,6 @@ RSpec.feature "Login" do
     fill_in "VACOLS Password", with: "pa55word!"
     click_on "Login"
 
-    expect(page).to have_content("What’s New?")
+    expect(page).to have_content("Help")
   end
 end
