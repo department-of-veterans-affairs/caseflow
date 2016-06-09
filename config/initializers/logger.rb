49cc2ba9ff615789ccfb69acb622ea10db3cfcb9
Ori Schwartz <orischwartz@users.noreply.github.com>
2016-06-09 14:35:44 -0400

    TaggedLogging support, enable rollover in staging

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -18,5 +18,6 @@ config.log_tags = log_tags
 
 # roll logger over every 1MB, retain 10
 logger_path = config.paths["log"].first
-Rails.logger = Logger.new(logger_path, 10, 1.megabyte) if Rails.env.production?
+rollover_logger = Logger.new(logger_path, 10, 1.megabyte)
+Rails.logger = ActiveSupport::TaggedLogging.new(rollover_logger) unless Rails.env.development?
 # :nocov:
