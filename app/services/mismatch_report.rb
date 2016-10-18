0331612a22ccad6123e04609f2e25f4ebe4e9121
Chris Given <Christopher.Given@va.gov>
2016-10-18 18:23:57 -0400

    Mismatch only wants paperless appeals

--- app/services/mismatch_report.rb
+++ app/services/mismatch_report.rb
@@ -44,20 +44,6 @@ class MismatchReport < Report
     mismatched.join(", ")
   end
 
-  PAPER_ONLY_OFFICES = [
-    # General Councel
-    "RO89",
-
-    # Education Centers
-    "RO91", "RO92", "RO93", "RO94",
-
-    # Pension
-    "RO80", "RO81", "RO82", "RO83",
-
-    # VHA CO
-    "RO99"
-  ].freeze
-
   def find_records
     VACOLS::Case.joins(:folder, :correspondent).where(%{
 
@@ -70,14 +56,8 @@ class MismatchReport < Report
       -- We're only thinking about cases that have not yet been received by
       -- BVA, so we limit our search to cases in ADV (Advance) status.
 
-      AND (folder.tivbms IS NULL OR
-           folder.tivbms NOT IN (?))
-      -- We also want things *absolutely* marked as a paper case, so let's
-      -- ignore anything that's marked as being worked in the efolder.
-
-      AND bfregoff NOT IN (?)
-      -- Finally, we'll ignore any ROs we know to be paper only, even
-      -- if the appellant has an eFolder.
+      AND folder.tivbms IN ?
+      -- Only efolder cases need apply
 
     }, 2.weeks.ago, "ADV", %w(Y 1 0), PAPER_ONLY_OFFICES)
   end
