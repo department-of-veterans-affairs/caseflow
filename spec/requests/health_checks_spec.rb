a8f18b72bcc2f89f7e80d17feaa537b78c2c8aae
Shane Russell <shane.russell.va@gmail.com>
2016-10-19 16:50:05 -0400

    Add build info to the health check
    fixes #347

---
+++ spec/requests/health_checks_spec.rb
@@ -0,0 +1,13 @@
+describe "Health Check API" do
+  it "returns meta info about the app" do
+    Rails.application.config.build_version = { deployed_at: "the best day ever"}
+
+    get "/health-check"
+
+    expect(response).to be_success
+
+    json = JSON.parse(response.body)
+    expect(json["healthy"]).to be_truthy
+    expect(json["deployed_at"]).to eq("the best day ever")
+  end
+end
