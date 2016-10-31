044d98db7bf2072f7146b0b31f2ca803d399b372
Alex Prokop <alex.prokop@gmail.com>
2016-10-31 16:58:23 -0400

    figures out fields

--- db/migrate/20161028153607_add_form8_data_to_db.rb
+++ db/migrate/20161028153607_add_form8_data_to_db.rb
@@ -15,16 +15,16 @@ class AddForm8DataToDb < ActiveRecord::Migration
       t.string :veteran_name # FirstName LastName
       t.string :_initial_insurance_loan_number
       t.string :insurance_loan_number
-      t.string :_initial_service_connection_for
-      t.string :service_connection_for # How the claim is connected to service record
+      t.text :_initial_service_connection_for
+      t.text :service_connection_for # How the claim is connected to service record
       t.datetime :_initial_service_connection_notification_date
       t.datetime :service_connection_notification_date
-      t.string :_initial_increased_rating_for
-      t.string :increased_rating_for
+      t.text :_initial_increased_rating_for
+      t.text :increased_rating_for
       t.datetime :_initial_increased_rating_notification_date
       t.datetime :increased_rating_notification_date
-      t.string :_initial_other_for
-      t.string :other_for
+      t.text :_initial_other_for
+      t.text :other_for
       t.datetime :_initial_other_notification_date
       t.datetime :other_notification_date
       t.string :_initial_representative_name
@@ -34,7 +34,7 @@ class AddForm8DataToDb < ActiveRecord::Migration
       t.string :_initial_representative_type_specify_other
       t.string :representative_type_specify_other
       t.string :_initial_power_of_attorney # Power of attorney blurb
-      t.string :power_of_attorney
+      t.string :power_of_attorney # Enum: certification that valid POA is in another VA file
       t.string :_initial_power_of_attorney_file
       t.string :power_of_attorney_file
       t.string :_initial_agent_accredited
@@ -49,7 +49,7 @@ class AddForm8DataToDb < ActiveRecord::Migration
       t.boolean :hearing_held
       t.boolean :_initial_hearing_transcript_on_file
       t.boolean :hearing_transcript_on_file
-      t.string :_initial_hearing_requested_explaination
+      t.string :_initial_hearing_requested_explanation
       t.string :hearing_requested_explaination
       t.boolean :_initial_contested_claims_procedures_applicable
       t.boolean :contested_claims_procedures_applicable
@@ -59,17 +59,17 @@ class AddForm8DataToDb < ActiveRecord::Migration
       t.datetime :soc_date
       t.string :_initial_ssoc_required
       t.integer :ssoc_required # enum  "Required and furnished"/"Not required"
-      t.string :_initial_record_other_explaination
-      t.string :record_other_explaination
-      t.string :_initial_remarks
-      t.string :remarks
+      t.text :_initial_record_other_explanation, array: true
+      t.text :record_other_explanation, array: true # One or more from a list of records to send to Veteran's Appeals
+      t.text :_initial_remarks
+      t.text :remarks
       t.string :certifying_office # Not user-modifiable
       t.string :certifying_username # Not user-modifiable
       t.string :_initial_certifying_official_name
       t.string :certifying_official_name
       t.string :_initial_certifying_official_title
       t.string :certifying_official_title
-      t.string :certification_date # Not user-modifiable
+      t.datetime :certification_date # Not user-modifiable
     end
   end
 end
