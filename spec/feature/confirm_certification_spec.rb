a3355e8dffefacb2d5b5c1c8601ac1958d545f31
Alex Prokop <alex.prokop@gmail.com>
2016-11-02 11:31:26 -0400

    adds spec for screen reader link

--- spec/feature/confirm_certification_spec.rb
+++ spec/feature/confirm_certification_spec.rb
@@ -15,6 +15,22 @@ RSpec.feature "Confirm Certification" do
 
   after { Timecop.return }
 
+  scenario "Successful screen reader visit of pdf link" do
+    Certification.create!(vacols_id: "5555C")
+    visit "certifications/5555C"
+    # We want this content to only appear for screen reader users, so
+    # it will not be visible, but it **should** be in the DOM.
+    expect(page).to have_content("The PDF viewer in your browser may not be accessible.")
+    expect(page).to have_css(".usa-sr-only", visible: false)
+    # Sending click or keypress events to elements that are not in the DOM doesn't seem to work,
+    # so let's find the hidden link's href and visit it manually to check that the pdf can be
+    # found there.
+    pdf_href = page.find('#sr-download-link')["href"]
+    visit(pdf_href)
+    content_header = page.response_headers["Content-Disposition"]
+    expect(content_header.include?("form8-TEST.pdf")).to be true
+  end
+
   scenario "Successful confirmation" do
     visit "certifications/5555C"
     expect(page).to have_content("Review Form 8")
@@ -28,17 +44,9 @@ RSpec.feature "Confirm Certification" do
   scenario "Successful confirmation with certification record" do
     certification = Certification.create!(vacols_id: "5555C")
     visit "certifications/5555C"
-    click_on "Upload and certify"
 
+    click_on "Upload and certify"
     expect(certification.reload.completed_at).to eq(Time.zone.now)
   end
 
-  scenario "Visiting certification page as a sighted user" do
-    Certification.create!(vacols_id: "5555C")
-    visit "certifications/5555C"
-    # We want this content to only appear for screen reader users, so
-    # it will not be visible, but it **should** be in the DOM.
-    expect(page).to have_content("The PDF viewer in your browser may not be accessible.")
-    expect(page).to have_css(".off-screen", visible: false)
-  end
 end
