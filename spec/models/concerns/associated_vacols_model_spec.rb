5671671654931f4a013c2e4b13205c356e7c6eba
JD Pagano <jonathanpagano@gmail.com>
2016-11-03 11:54:22 -0400

    rubocooop

--- spec/models/concerns/associated_vacols_model_spec.rb
+++ spec/models/concerns/associated_vacols_model_spec.rb
@@ -1,5 +1,4 @@
 describe AssociatedVacolsModel do
-  
   class TestVacolsModelRepository
     def self.load_vacols_data(model)
       model.foo = "bar"
@@ -11,7 +10,6 @@ describe AssociatedVacolsModel do
 
     vacols_attr_accessor :foo, :bar, :foobar
 
-
     def self.repository
       TestVacolsModelRepository
     end
@@ -22,36 +20,35 @@ describe AssociatedVacolsModel do
   context ".vacols_attr_accessor" do
     context "getter" do
       subject { model.foo }
-      it { is_expected.to eq("bar") } 
+      it { is_expected.to eq("bar") }
     end
 
     context "call setter before first get" do
       before { model.foo = "hello" }
       subject { model.foo }
-      it { is_expected.to eq("hello") } 
+      it { is_expected.to eq("hello") }
     end
 
     context "ensure setter sets value" do
       it do
         expect(model.foo).to eq("bar")
-        model.foo = "hello" 
+        model.foo = "hello"
         expect(model.foo).to eq("hello")
       end
     end
   end
 
-  context "#set_from_vacols" do
-    before { model.set_from_vacols({:foo => 1, :bar => 2, :foobar => 3}) }
+  context "#assign_from_vacols" do
+    before { model.assign_from_vacols(foo: 1, bar: 2, foobar: 3) }
     subject { [model.foo, model.bar, model.foobar] }
     it { is_expected.to eq([1, 2, 3]) }
   end
 
   context "#check_and_load_vacols_data!" do
     it do
-      TestVacolsModelRepository.should_receive(:load_vacols_data).exactly(1).times 
+      TestVacolsModelRepository.should_receive(:load_vacols_data).exactly(1).times
       model.check_and_load_vacols_data!
       model.check_and_load_vacols_data!
-    end 
+    end
   end
-
 end
