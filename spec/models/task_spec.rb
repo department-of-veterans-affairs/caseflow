f788e5a5af0602dd92a445f8f654544fa69b9159
Mark Benjamin <mark@navahq.com>
2016-11-03 18:02:16 -0400

    Removing test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,11 +1,3 @@
 describe Task do
-  before do
-    Task.delete_all
-    Appeal.delete_all
-  end
-  it "persists task to DB" do
-    appeal = Appeal.create(vacols_id: "vacols")
-    task = Task.create(name: "ABC", appeal_id: appeal.id)
-    expect(Task.find(task.id)).to be_an_instance_of(Task)
-  end
+
 end
