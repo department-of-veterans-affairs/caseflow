7f8b9527b1234cc5ffc0b0c6702e535d8798920f
JD Pagano <jonathanpagano@gmail.com>
2016-11-04 17:57:01 -0400

    Add more tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,6 +4,7 @@ describe Task do
     subject { Task.find_by_department(department) }
 
     it 'filters to tasks in the department' do
+      expect(subject).to be_an_instance_of(Task::ActiveRecord_Relation)
       expect(subject.to_sql).to include('CreateEndProduct')
     end
   end
