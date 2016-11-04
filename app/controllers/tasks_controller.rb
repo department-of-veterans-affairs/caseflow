8824474b7c6b6b4f895d49fb04244408f45d3e88
JD Pagano <jonathanpagano@gmail.com>
2016-11-04 13:50:00 -0400

    Remove extra route
    - Empty views
    - Remove show method
    - Empty index method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,6 @@ class TasksController < ApplicationController
   before_action :verify_access
 
   def index
-    @tasks = Task.find_by_department(department).order(created_at: :desc).all
-  end
-
-  def show
-    @task = Task.find(task_id)
   end
 
   private
