e58f36c2ffac52fe6ff1181668c6358148b53aef
JD Pagano <jonathanpagano@gmail.com>
2016-11-04 17:55:21 -0400

    Add test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,2 +1,10 @@
 describe Task do
+  context ".find_by_department" do
+    let(:department) { :dispatch }
+    subject { Task.find_by_department(department) }
+
+    it 'filters to tasks in the department' do
+      expect(subject.to_sql).to include('CreateEndProduct')
+    end
+  end
 end
