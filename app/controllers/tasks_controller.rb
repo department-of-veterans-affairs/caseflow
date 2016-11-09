ce189fc37955b206d58a0373b8b575660ae7e08e
Mark Benjamin <mdbenjam@gmail.com>
2016-11-09 15:24:29 -0500

    Update the routing for EstablishClaims (#429)
    * Updating routing for establishclaim
    
    * Updating tests
    
    * Updating to gracefully handle nil
    
    * Fixing merge
    
    * Fixing tests
    
    * Changing routes to hyphenated
    
    * Fixing feature test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   before_action :verify_access
 
+  class TaskTypeMissingError < StandardError; end
+
   def index
     @completed_count = Task.completed_today.count
     @to_complete_count = Task.to_complete.count
@@ -24,11 +26,11 @@ class TasksController < ApplicationController
   helper_method :next_unassigned_task
 
   def scoped_tasks
-    Task.find_by_department(department).newest_first
+    Task.where(type: type).newest_first
   end
 
-  def department
-    params[:department]
+  def type
+    params[:task_type]
   end
 
   def task_id
@@ -50,13 +52,17 @@ class TasksController < ApplicationController
     "#{prefix}_index"
   end
 
+  def task_roles
+    User::TASK_TYPE_TO_ROLES[type] || fail(TaskTypeMissingError)
+  end
+
   def manager?
     # TODO(jd): Determine real CSS role to be used
-    current_user.can?("manage #{department}")
+    current_user.can?(task_roles[:manager])
   end
 
   def verify_access
     # TODO(jd): Determine real CSS role to be used
-    verify_authorized_roles(department.to_s)
+    verify_authorized_roles(task_roles[:employee])
   end
 end
--- app/models/tasks/create_end_product.rb
+++
@@ -1,2 +0,0 @@
-class CreateEndProduct < Task
-end
