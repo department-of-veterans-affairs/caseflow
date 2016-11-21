1c85472ac72486fbe2627d8849bafebb626dc307
JD Pagano <jonathanpagano@gmail.com>
2016-11-21 16:00:36 -0500

    Let rails be magic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,9 +19,6 @@ class TasksController < ApplicationController
   def assign
     next_unassigned_task.assign!(current_user)
     redirect_to url_for(next_unassigned_task)
-  rescue ActiveRecord::StaleObjectError, Task::AlreadyAssignedError => e
-    Raven.capture_exception(e)
-    not_found
   end
 
   private
