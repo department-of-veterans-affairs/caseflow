c8c3d1660fd97bd99deae6c67dadd2357540796a
JD Pagano <jonathanpagano@gmail.com>
2016-11-28 22:51:11 -0500

    lint fixes

--- client/app/containers/TestPage.jsx
+++ client/app/containers/TestPage.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 export default class TestPage extends React.Component {
   render() {
-    return(<div className="sub-page">
+    return <div className="sub-page">
       Test Page
-      <a onClick={() => { this.props.handleAlert('error', 'test', 'bar') }} className="handleAlert"/>
-      <a onClick={() => { this.props.handleAlertClear()}} className="handleAlertClear"/>
-    </div>);
+      <a onClick={() => {
+        this.props.handleAlert('error', 'test', 'bar');
+      }} className="handleAlert"/>
+      <a onClick={() => {
+        this.props.handleAlertClear();
+      }} className="handleAlertClear"/>
+    </div>;
   }
 }
