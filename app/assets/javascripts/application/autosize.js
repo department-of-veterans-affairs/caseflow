a536d6ddcc38a096ee1ebd7029cc8964af93b6ff
Alex Prokop <alex.prokop@gmail.com>
2016-12-07 10:04:01 -0400

    autosizes certain inputs

---
+++ app/assets/javascripts/application/autosize.js
@@ -0,0 +1,25 @@
+// Finds all textareas with the attribute "autoresize"
+// and ensures that they grow vertically with the number
+// of lines of user input.
+//
+// Credit:
+// https://maximilianhoffmann.com/posts/autoresizing-textareas
+// (Partially modified because the script as written scrolled
+// undesirably)
+//
+(function(){
+  window.autoresize = {
+    init: function() {
+      var resizingTextareas = [].slice.call(document.querySelectorAll('textarea[autoresize]'));
+      resizingTextareas.forEach(function(textarea) {
+        textarea.addEventListener('input', autoresize, false);
+      });
+    }
+  };
+
+  function autoresize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+    this.scrollTop = this.scrollHeight;
+  }
+})()
