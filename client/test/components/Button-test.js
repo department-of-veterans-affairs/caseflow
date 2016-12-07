2bcd2fd0d949eb07419e5dfae2f6c21838050c39
Sharon Warner <sharon.warner56@gmail.com>
2016-12-07 11:33:09 -0500

    Fixing lint issues

--- client/test/components/Button-test.js
+++ client/test/components/Button-test.js
@@ -5,13 +5,15 @@ import Button from '../../app/components/Button';
 
 describe('Button', () => {
   it('does not render while loading', () => {
-  	let onChange = () => true;
+    let onChange = () => true;
     const wrapper = shallow(<Button name="foo" onChange={onChange} loading={true} />);
+
     expect(wrapper.find('button')).to.have.length(0);
   });
   it('renders while not loading', () => {
-  	let onChange = () => true;
+    let onChange = () => true;
     const wrapper = shallow(<Button name="foo" onChange={onChange} loading={false} />);
+
     expect(wrapper.find('button')).to.have.length(1);
   });
 });
