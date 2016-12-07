0265ed91b2c8aaa749fead04910377c677c83e30
Sharon Warner <sharon.warner56@gmail.com>
2016-12-07 10:43:58 -0500

    Added unit test for the Button Component

---
+++ client/test/components/Button-test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Button from '../../app/components/Button';
+
+describe('Button', () => {
+  it('does not render while loading', () => {
+  	let onChange = () => true;
+    const wrapper = shallow(<Button name="foo" onChange={onChange} loading={true} />);
+    expect(wrapper.find('button')).to.have.length(0);
+  });
+  it('renders while not loading', () => {
+  	let onChange = () => true;
+    const wrapper = shallow(<Button name="foo" onChange={onChange} loading={false} />);
+    expect(wrapper.find('button')).to.have.length(1);
+  });
+});
