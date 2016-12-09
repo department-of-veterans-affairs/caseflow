ef3d0c3e32ca82847044966db7ec3548e2d35bcf
Mark Benjamin <mark@navahq.com>
2016-12-09 10:10:26 -0500

    Fixing bug

--- app/controllers/establish_claims_controller.rb
+++ app/controllers/establish_claims_controller.rb
@@ -10,7 +10,7 @@ class EstablishClaimsController < TasksController
   def review
     # Future safeguard for when we give managers a show view
     # for a given task
-    task.start! if current_user == task.user
+    task.start! if current_user == task.user && !task.started?
     render "review"
   end
 
