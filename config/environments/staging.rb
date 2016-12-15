210412e7049a03087e7cb5d9de1c49c9e23bfb5d
Mark Benjamin <mark@navahq.com>
2016-12-15 10:09:54 -0500

    Adding to staging as well

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Setup S3
+  config.s3_enabled = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
