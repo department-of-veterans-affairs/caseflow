e57e9c7189d7ec02e548caccfdf126ff1d8f3a30
Mark Benjamin <mark@navahq.com>
2016-12-19 17:04:18 -0500

    Adding date util

---
+++ client/app/util/validators/DateValidator.js
@@ -0,0 +1,12 @@
+const DATE_REGEX = /(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d\d/;
+
+
+const dateValidator = (message='The date must be in mm/dd/yyyy format.') => function(value) {
+  if (!DATE_REGEX.test(value)) {
+    return message;
+  }
+
+  return null;
+};
+
+export default dateValidator;
