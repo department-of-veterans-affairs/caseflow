61afa67d0fbf4c0d6d269092158ec33c54fb2fc2
Artem Vovk <avovk@makparcorp.com>
2017-01-08 17:35:23 -0500

    added proper rails_helper inclusion; refactored before->let; added a couple of method tests

--- spec/services/form8_pdf_service_spec.rb
+++ spec/services/form8_pdf_service_spec.rb
@@ -1,29 +1,57 @@
 describe Form8PdfService do
   # TODO(alex): this file is undertested. Add more tests
   # as we make modifications to it.
-
-  before do
-    @form8 = Form8.new(
+  let(:form8) do
+    Form8.new(
       certifying_official_title: "Attorney",
       certifying_official_title_specify_other: "Mugatu"
     )
   end
 
   context ".pdf_values_for" do
-    let(:pdf_values) { Form8PdfService.pdf_values_for(@form8) }
+
+    let(:pdf_form8_values) do
+      Form8PdfService.pdf_values_for(form8)
+    end
+
+    it "goes through the fields" do
+      Form8::FORM_FIELDS.each do |field|
+        form8[field] = (0...8).map { (65 + rand(26)).chr }.join
+
+      end
+    end
 
     it "populates values from form8 to pdf" do
       expect(
-        pdf_values["form1[0].#subform[0].#area[0].TextField1[21]"]
+        pdf_form8_values["form1[0].#subform[0].#area[0].TextField1[21]"]
       ).to eq("Attorney")
     end
 
     it "uses the other specified title if official title is 'Other'" do
-      @form8[:certifying_official_title] = "Other"
+      form8[:certifying_official_title] = "Other"
 
       expect(
-        pdf_values["form1[0].#subform[0].#area[0].TextField1[21]"]
+        pdf_form8_values["form1[0].#subform[0].#area[0].TextField1[21]"]
       ).to eq("Mugatu")
     end
+
   end
+
+  context ".save_pdf_for!" do
+    let(:final_location) { Form8PdfService.output_location_for(form8) }
+
+    before do
+      Form8PdfService.save_pdf_for!(form8)
+    end
+
+    it "should have a file at output location" do
+      expect(File.exists?(File.join(Rails.root, "tmp", "pdfs", "form8-#{form8.vacols_id}.pdf"))).to be_truthy
+    end
+
+    it "should not leave file at temporary location" do
+      expect(File.exists?(File.join(Rails.root, "tmp", "pdfs", "form8-#{form8.vacols_id}.tmp"))).to be_falsy
+    end
+  end
+
+
 end
