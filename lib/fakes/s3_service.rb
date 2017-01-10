510468f15abc560dcd097499ddd2802c6b007e08
aroltsch <anya.roltsch@gmail.com>
2017-01-10 10:29:37 -0500

    Store file in s3

--- lib/fakes/s3_service.rb
+++ lib/fakes/s3_service.rb
@@ -1,14 +1,22 @@
 class Fakes::S3Service
   cattr_accessor :files
 
-  def self.store_file(filename, content, _type = :content)
+  def self.store_file(filename, path, _type = :content)
     self.files ||= {}
-    self.files[filename] = content
+    tmp = Tempfile.new(filename)
+    copy_file_contents(path, tmp)
+    self.files[filename] = tmp.path
   end
 
   def self.fetch_file(filename, dest_filepath)
-    File.open(dest_filepath, "wb") do |f|
-      f.write(files[filename])
+    copy_file_contents(files[filename], dest_filepath)
+  end
+
+  def self.copy_file_contents(source_filepath, dest_filepath)
+    File.open(source_filepath, "rb") do |input_stream|
+      File.open(dest_filepath, "wb") do |output_stream|
+        IO.copy_stream(input_stream, output_stream)
+      end
     end
   end
 end
