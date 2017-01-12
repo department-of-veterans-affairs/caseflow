84bf8b813b2f5a48c46a667ad65f43a065f2e12b
Alex Prokop <alex.prokop@gmail.com>
2017-01-12 18:13:09 +0100

    adds default values for metrics auth and removes errant commnet

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   #
   ENV["CASEFLOW_FEEDBACK_URL"] = "test.feedback.url"
+
+  ENV["METRICS_USERNAME"] = "caseflow"
+  ENV["METRICS_PASSWORD"] = "caseflow"
 end
