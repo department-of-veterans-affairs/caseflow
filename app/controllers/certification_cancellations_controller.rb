68d6732680995ce4c26e5d5c5d48a90f54aa3777
Nemo Djuric <nemo.djuric@gmail.com>
2017-01-19 17:33:43 -0500

    Removed redundant layout specification from certification_cancellation controller.

--- app/controllers/certification_cancellations_controller.rb
+++ app/controllers/certification_cancellations_controller.rb
@@ -1,7 +1,6 @@
 class CertificationCancellationsController < ApplicationController
   before_action :verify_authentication
   before_action :verify_access
-  layout "application"
 
   def create
     CertificationCancellation.create(certification_cancellation_params)
