b0eb7c4ffa5fc9d4bd55d8df61071694a916387d
Shane Russell <shane.russell.va@gmail.com>
2017-01-25 09:44:30 -0500

    Consolidate and refactor test endpoints.
    In preperation for adding establish claim setup endpoint

---
+++ app/controllers/test/setup_controller.rb
@@ -0,0 +1,31 @@
+class Test::SetupController < ApplicationController
+  before_action :require_uat_test_user, only: [:setup_certification]
+  before_action :require_demo, only: [:set_user]
+
+  # Used for resetting data in UAT for certification
+  def certification
+    test_appeal_id = ENV["TEST_APPEAL_ID"]
+
+    @certification = Certification.find_by(vacols_id: test_appeal_id)
+    @certification.uncertify!(current_user.css_id)
+    Certification.delete_all(vacols_id: test_appeal_id)
+
+    redirect_to new_certification_path(vacols_id: test_appeal_id)
+  end
+
+  # Set current user in DEMO
+  def set_user
+    session["user"] = User.authentication_service.get_user_session(params[:id])
+    redirect_to "/test/users"
+  end
+
+  private
+
+  def require_uat_test_user
+    redirect_to "/unauthorized" unless Rails.deploy_env?(:uat) && current_user.css_id == ENV["TEST_USER_ID"]
+  end
+
+  def require_demo
+    redirect_to "/unauthorized" unless Rails.deploy_env?(:demo)
+  end
+end
