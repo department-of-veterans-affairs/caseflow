3bd38b0ba7e7ddb19935fcbf9f26ef1f15a834f8
JD Pagano <jonathanpagano@gmail.com>
2017-01-25 11:44:24 -0500

    Fix bug with loading decision pdf
    - Add react proptypes

--- client/app/containers/EstablishClaimPage/EstablishClaim.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaim.jsx
@@ -173,7 +173,7 @@ export default class EstablishClaim extends BaseForm {
     });
   }
 
-  handleModalClose = function (modal) {
+  handleModalClose = (modal) => {
     return () => {
       let stateObject = {};
 
@@ -358,8 +358,15 @@ export default class EstablishClaim extends BaseForm {
     let {
       loading,
       cancelModalDisplay,
-      modalSubmitLoading
+      modalSubmitLoading,
+      specialIssueModalDisplay,
+      specialIssues
     } = this.state;
+
+    let {
+      pdfLink,
+      pdfjsLink
+    } = this.props;
 
     return (
       <div>
@@ -367,11 +374,13 @@ export default class EstablishClaim extends BaseForm {
           <EstablishClaimReview
             decisionType={this.state.reviewForm.decisionType}
             handleDecisionTypeChange={this.handleDecisionTypeChange}
-            specialIssues={this.state.specialIssues}
             handleCancelTaskForSpecialIssue={this.handleCancelTaskForSpecialIssue}
             handleFieldChange={this.handleFieldChange}
             handleModalClose={this.handleModalClose}
-            specialIssueModalDisplay={this.state.specialIssueModalDisplay}
+            pdfLink={pdfLink}
+            pdfjsLink={pdfjsLink}
+            specialIssueModalDisplay={specialIssueModalDisplay}
+            specialIssues={specialIssues}
           />
         }
         { this.isAssociatePage() &&
