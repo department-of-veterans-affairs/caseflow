c783e6cf0c2d46aeba2c29711a5a188cab1e3b86
Mark Benjamin <mark@navahq.com>
2017-01-27 10:35:36 -0500

    Fixing test for ep change data

--- spec/feature/switch_user_spec.rb
+++ spec/feature/switch_user_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Switch User" do
+RSpec.feature "Switch User", focus: true do
   before do
     # Switch user only works in demo deploy env
     ENV["DEPLOY_ENV"] = "dev"
@@ -18,9 +18,11 @@ RSpec.feature "Switch User" do
     expect(page).to have_content("123 (DSUSER)")
   end
 
+  # Note if this test ever fails non-deterministically it may be because
+  # we check the updated data in BGSService before the server responds
+  # to the request. Sync with mdbenjam to try and resolve it.
   scenario "We can switch between test EP data in dev mode" do
     visit "test/users"
-    expect(page).not_to have_content("123 (DSUSER)")
     expect(page).to have_content("All Grants")
     click_on "All Grants"
     expect(page).to have_content("All Grants")
