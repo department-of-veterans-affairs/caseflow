87a1708c5c364243e1baf9acd265c816dbcaf56d
JD Pagano <jonathanpagano@gmail.com>
2017-01-30 10:38:51 -0500

    Hardcode Button type button

--- client/app/components/Button.jsx
+++ client/app/components/Button.jsx
@@ -2,14 +2,6 @@ import React, { PropTypes } from 'react';
 import { loadingSymbolHtml } from './RenderFunctions.jsx';
 
 export default class Button extends React.Component {
-  componentDidMount() {
-    if (this.props.type === 'submit') {
-      console.warn(`Warning! You are using a button with type submit.
-        Was this intended? Make sure to use event.preventDefault() if
-        you're using it with a form and an onClick handler`);
-    }
-  }
-
   render() {
     let {
       classNames,
@@ -35,7 +27,7 @@ export default class Button extends React.Component {
       <button
         id={id || `${type}-${name.replace(/\s/g, '-')}`}
         className={classNames.join(' ')}
-        type={type}
+        type="button"
         disabled={disabled}
         onClick={onClick}>
           {name}
@@ -44,10 +36,6 @@ export default class Button extends React.Component {
     </span>;
   }
 }
-
-Button.defaultProps = {
-  type: 'button'
-};
 
 Button.propTypes = {
   classNames: PropTypes.arrayOf(PropTypes.string),
