7d61718bed1193b76dbb92806783ca62a1c5a1c9
Sharon Warner <sharon.warner56@gmail.com>
2017-02-01 17:23:12 -0500

    Code cleanup

--- app/jobs/prepare_establish_claim_tasks_job.rb
+++ app/jobs/prepare_establish_claim_tasks_job.rb
@@ -1,4 +1,6 @@
 class PrepareEstablishClaimTasksJob < ActiveJob::Base
+  queue_as :default
+
   def perform
     EstablishClaim.where(aasm_state: "unprepared").each do |establish_claim|
       establish_claim.prepare! if establish_claim.appeal.documents
