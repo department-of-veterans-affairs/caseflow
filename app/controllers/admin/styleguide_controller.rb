98ff51d5ab3a1cb3c6b8b35db57c8bbe45398ffd
Marian Lumba <mmlumba@gmail.com>
2017-02-08 17:15:27 -0500

    create the basic setup for caseflow commons style guide

--- app/controllers/admin/styleguide_controller.rb
+++ app/controllers/admin/styleguide_controller.rb
@@ -1,4 +1,10 @@
 class Admin::StyleguideController < ApplicationController
   before_action :verify_system_admin
   layout "styleguide"
+
+  # def code_block( title = nil, lang = nil, &block )
+  # output = capture( &block )
+  # render partial: 'my_html_bits/code_block',
+  #        locals:  {title: title, lang: lang, text: output }
+  # end
 end
