59cd452113d6728e05abd97956c7c4b3c0ff5b6b
Mark Benjamin <mark@navahq.com>
2017-02-08 17:47:56 -0500

    Initial commit

---
+++ app/controllers/document_controller.rb
@@ -0,0 +1,8 @@
+class DocumentController < ApplicationController
+  before_action :verify_system_admin
+
+  def set_label
+    document = Document.find(params[:id])
+    document.update(label: params[:label])
+  end
+end
