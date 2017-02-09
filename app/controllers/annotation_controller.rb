f056e1aaa4a6e5217bc6acd301fb3320e3e77d86
Mark Benjamin <mark@navahq.com>
2017-02-09 12:00:29 -0500

    Moving to create! and update!

--- app/controllers/annotation_controller.rb
+++ app/controllers/annotation_controller.rb
@@ -2,22 +2,21 @@ class AnnotationController < ApplicationController
   before_action :verify_system_admin
 
   def create
-    annotation = Annotation.create(
+    annotation = Annotation.create!(
       annotation_params
     )
-    render json: { errors: "Failed to delete annotation" }, status: :internal_server_error if !annotation.valid?
     render json: { id: annotation.id }
   end
 
   def destroy
+    binding.pry
     deleted = Annotation.delete(params.require(:id))
     render json: { error: "Failed to delete annotation" }, status: :internal_server_error if !deleted
     render json: { }
   end
 
   def update
-    updated = Annotation.find(params[:id]).update(annotation_params)
-    render json: { error: "Failed to delete annotation" }, status: :internal_server_error if !updated
+    Annotation.find(params[:id]).update!(annotation_params)
     render json: { }
   end
 
