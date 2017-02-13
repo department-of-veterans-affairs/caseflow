88a4ea627ec39a12d4ce7f9f4b3ce4e1bfda67fd
Mark Benjamin <mark@navahq.com>
2017-02-13 14:33:04 -0500

    Lint errors

--- client/app/containers/EstablishClaimPage/EstablishClaimReview.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaimReview.jsx
@@ -114,11 +114,10 @@ export default class EstablishClaimReview extends React.Component {
     return this.props.task.appeal.decisions.length > 1;
   }
 
-  buildIssueRow = (issue) => {
-    let description = issue.description.map((descriptor) => {
-      return <div>{descriptor}</div>;
-    }, null);
-  
+  buildIssueRow = (issue, index) => {
+    let description = issue.description.map((descriptor) =>
+      <div key={`${descriptor}-${index}`}>{descriptor}</div>, null);
+
     return [
       issue.program,
       <div>{description}</div>,
