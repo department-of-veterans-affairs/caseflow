9b06df3e849cb32a60627bb856b6159936347746
JD Pagano <joofsh@users.noreply.github.com>
2017-02-15 10:03:15 -0500

    Warmup activerecord for vacols fetching columns & indexes (#919)
    * Warmup activerecord for vacols fetching columns & indexes
    
    * reference the table names directly
    
    * only if dependencies are not faked

--- config/initializers/warmup_vacols.rb
+++ config/initializers/warmup_vacols.rb
@@ -1,6 +1,8 @@
-
-# VACOLS throttles the rate of new connections, create up front to prevent 
+# VACOLS throttles the rate of new connections, create up front to prevent
 # blocking as pool grows under load
+
+WARMUP_TABLES = ["vacols.brieff", "vacols.corres", "vacols.folder"]
+
 ActiveSupport.on_load(:active_record_vacols) do
   db_config =  Rails.application.config.database_configuration[Rails.env]
 
@@ -8,8 +10,10 @@ ActiveSupport.on_load(:active_record_vacols) do
   initial_pool_size = (ENV['DB_CONN_POOL_INITIAL_SIZE'] || db_config['pool'] / 2).to_i
   Rails.logger.info("creating #{initial_pool_size} initial connections...")
 
-  MetricsService.timer "created #{initial_pool_size} connections" do
-    warmup_pool(VACOLS::Record.connection_pool, initial_pool_size)
+  unless ApplicationController.dependencies_faked?
+    MetricsService.timer "created #{initial_pool_size} connections" do
+      warmup_pool(VACOLS::Record.connection_pool, initial_pool_size)
+    end
   end
 end
 
@@ -21,6 +25,14 @@ def warmup_pool(pool, initial_pool_size)
     threads << Thread.new do
       conn = pool.connection
       Rails.logger.info("taking connection #{i}; db pool size: #{pool.connections.size}")
+
+      WARMUP_TABLES.each do |table_name|
+        Rails.logger.info("fetching indexes & columns for #{table_name} on connection #{i}")
+        conn.indexes(table_name)
+        conn.columns(table_name)
+      end
+
+
       latch.count_down
 
       # don't return the connection to the pool until all other threads have taken a connection;
