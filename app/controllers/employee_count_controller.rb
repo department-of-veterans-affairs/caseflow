b6a38320e6a89e980a026cbece8867d3100d5cbe
Sharon Warner <sharon.warner56@gmail.com>
2017-02-16 16:55:37 -0500

    Restrict access to url to managers

--- app/controllers/employee_count_controller.rb
+++ app/controllers/employee_count_controller.rb
@@ -1,6 +1,12 @@
 class EmployeeCountController < ApplicationController
+  before_action :verify_manager_access
+
   def update_count
     Rails.cache.write("employee_count", params[:count])
     render json: {}
+  end
+
+  def verify_manager_access
+    verify_authorized_roles("Manage Claim Establishment")
   end
 end
