b47c2351b4dee00c0f5ff6c0f8e98e9a3023f5b9
Ori Schwartz <ori.schwartz@va.gov>
2017-02-17 16:46:04 -0500

    configure socket timeouts (#965)
    * configure socket timeouts
    
    * only set OCI8 properties when applicable

--- config/initializers/warmup_vacols.rb
+++ config/initializers/warmup_vacols.rb
@@ -1,6 +1,14 @@
 # VACOLS throttles the rate of new connections, create up front to prevent
 # blocking as pool grows under load
 
+# configure timeouts, in seconds, for underlying socket in environments that use Oracle
+if defined? OCI8
+  OCI8.properties[:tcp_connect_timeout] = 10
+  OCI8.properties[:connect_timeout] = 10
+  OCI8.properties[:send_timeout] = 10
+  OCI8.properties[:recv_timeout] = 20
+end
+
 WARMUP_TABLES = ["vacols.brieff", "vacols.corres", "vacols.folder"]
 
 ActiveSupport.on_load(:active_record_vacols) do
