7ad71f1b66d75a9f0a85b99da3dcf40ad34d7e86
Mark Benjamin <mark@navahq.com>
2017-02-17 14:08:00 -0500

    RUBOCOPPPP

--- app/controllers/test/setup_controller.rb
+++ app/controllers/test/setup_controller.rb
@@ -19,9 +19,6 @@ class Test::SetupController < ApplicationController
     fail "Too many ClaimsEstablishment tasks" if EstablishClaim.count > 20
 
     EstablishClaim.delete_all
-    Appeal.all do |appeal|
-      appeal.reset_special_issues
-    end
     TestDataService.prepare_claims_establishment!(vacols_id: full_grant_id, cancel_eps: true, decision_type: "full")
     TestDataService.prepare_claims_establishment!(vacols_id: partial_grant_id, cancel_eps: true)
 
