1d1e2a0b5d9f5da176693261c6ab32ad03916ea0
JD Pagano <jonathanpagano@gmail.com>
2017-02-27 17:49:14 -0500

    revert vacols_data

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,8 +1,5 @@
 DateTime::DATE_FORMATS[:short_date] =
 Time::DATE_FORMATS[:short_date] = "%m/%d/%Y"
 
-DateTime::DATE_FORMATS[:vacols_date] =
-Time::DATE_FORMATS[:vacols_date] = "%d-%^b-%Y"
-
 DateTime::DATE_FORMATS[:json_date] =
 Time::DATE_FORMATS[:json_date] = "%Y/%m/%d"
