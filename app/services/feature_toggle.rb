8a51e70cc5fcea88a0aebe1082cd891649e343d0
aroltsch <anya.roltsch@gmail.com>
2017-02-27 14:55:22 -0500

    Add a helper method to check if feature is enabled for a current user

--- app/services/feature_toggle.rb
+++ app/services/feature_toggle.rb
@@ -1,58 +1,61 @@
 class FeatureToggle
-
   # Keeps track of all enabled features
   FEATURE_LIST_KEY = :feature_list_key
 
   def self.features
-    client.smembers(FEATURE_LIST_KEY)
+    client.smembers(FEATURE_LIST_KEY).map(&:to_sym)
   end
 
   # Method to enable a feature globally or for a specfic set of regional offices
   # Examples:
   # FeatureToggle.enable!(:foo)
   # FeatureToggle.enable!(:bar, regional_offices: ["RO01", "RO02"])
-  def self.enable!(feature, regional_offices: [])
+  def self.enable!(feature, regional_offices: nil)
     # redis method: sadd (add item to a set)
-    client.sadd FEATURE_LIST_KEY, feature unless features.include?(feature.to_s)
+    client.sadd FEATURE_LIST_KEY, feature unless features.include?(feature)
 
-    enable_group(feature: feature,
-                  key: :regional_offices,
-                  value: regional_offices) if regional_offices.present?
+    if regional_offices.present?
+      enable(feature: feature,
+             key: :regional_offices,
+             value: regional_offices)
+    end
     true
   end
-
 
   # Method to disable a feature globally or for a specfic set of regional offices
   # Examples:
   # FeatureToggle.disable!(:foo)
   # FeatureToggle.disable!(:bar, regional_offices: ["RO01", "RO02"])
-  def self.disable!(feature, regional_offices: [])
-    client.multi do
-      # redis method: srem (remove item from a set)
-      client.srem FEATURE_LIST_KEY, feature
-      client.del feature
-    end unless regional_offices.present?
+  def self.disable!(feature, regional_offices: nil)
+    unless regional_offices
+      client.multi do
+        # redis method: srem (remove item from a set)
+        client.srem FEATURE_LIST_KEY, feature
+        client.del feature
+      end
+      return true
+    end
 
-    disable_group(feature: feature,
-                  key: :regional_offices,
-                  value: regional_offices) if regional_offices.present?
+    disable(feature: feature,
+            key: :regional_offices,
+            value: regional_offices)
+
     true
   end
 
   # Method to check if a given feature is enabled for a user
   def self.enabled?(feature, current_user)
-    return false unless features.include?(feature.to_s)
-
-    data = details_for(feature)
-    if data && data["regional_offices"].present?
-      return false unless data["regional_offices"].include?(current_user.regional_office)
-    end
+    return false unless features.include?(feature)
+    regional_offices = get_subkey(feature, :regional_offices)
+    # if regional_offices key is set, check if the feature is enabled for the user's ro
+    # otherwise, it is enabled globally
+    return false if regional_offices.present? && !regional_offices.include?(current_user.regional_office)
     true
   end
 
   # Returns a hash result for a given feature
   def self.details_for(feature)
-    get_data_for_feature(feature) || {} if features.include?(feature.to_s)
+    feature_hash(feature) || {} if features.include?(feature)
   end
 
   def self.client
@@ -60,33 +63,31 @@ class FeatureToggle
   end
 
   class << self
-
     private
 
-    def enable_group(feature:, key:, value:)
-      data = get_data_for_subkey(feature, key.to_s)
+    def enable(feature:, key:, value:)
+      data = get_subkey(feature, key)
       data = data.present? ? data + value : value
-      set_data_for_subkey(feature, key, data)
+      set_subkey(feature, key, data)
     end
 
-    def disable_group(feature:, key:, value:)
-      data = get_data_for_subkey(feature, key.to_s)
+    def disable(feature:, key:, value:)
+      data = get_subkey(feature, key)
       return unless data
-      data = data - value
-      set_data_for_subkey(feature, key, data)
+      set_subkey(feature, key, data - value)
     end
 
-    def get_data_for_feature(feature)
+    def feature_hash(feature)
       data = client.get(feature)
-      JSON.parse(data) if data
+      JSON.parse(data).symbolize_keys if data
     end
 
-    def get_data_for_subkey(feature, key)
-      data = get_data_for_feature(feature)
+    def get_subkey(feature, key)
+      data = feature_hash(feature)
       data[key] if data
     end
 
-    def set_data_for_subkey(feature, key, data)
+    def set_subkey(feature, key, data)
       client.set(feature, { key => data.uniq }.to_json)
     end
   end
--- spec/services/feature_toggle_spec.rb
+++ spec/services/feature_toggle_spec.rb
@@ -1,5 +1,4 @@
 describe FeatureToggle do
-
   let(:user1) { User.new(regional_office: "RO03") }
   let(:user2) { User.new(regional_office: "RO07") }
 
@@ -7,56 +6,182 @@ describe FeatureToggle do
     FeatureToggle.client.flushall
   end
 
-  it "should enable a feature" do
-    FeatureToggle.enable!(:test)
-    # should enable only once
-    FeatureToggle.enable!(:test)
-    FeatureToggle.enable!(:search)
-    expect(FeatureToggle.features.sort).to eq %w(search test)
-    expect(FeatureToggle.enabled?(:search, user1)).to eq true
+  context ".enable!" do
+    context "for everyone" do
+      subject { FeatureToggle.enable!(:search) }
+
+      it "feature is enabled for everyone" do
+        subject
+        expect(FeatureToggle.enabled?(:search, user1)).to eq true
+        expect(FeatureToggle.enabled?(:search, user2)).to eq true
+      end
+    end
+
+    context "for a set of regional_offices" do
+      subject { FeatureToggle.enable!(:test, regional_offices: %w(RO01 RO02 RO03)) }
+
+      it "feature is enabled for users who belong to the regional offices" do
+        subject
+        expect(FeatureToggle.enabled?(:test, user1)).to eq true
+        expect(FeatureToggle.enabled?(:test, user2)).to eq false
+      end
+
+      it "enable for more users" do
+        subject
+        FeatureToggle.enable!(:test, regional_offices: ["RO07"])
+        expect(FeatureToggle.enabled?(:test, user1)).to eq true
+        expect(FeatureToggle.enabled?(:test, user2)).to eq true
+      end
+    end
   end
 
-  it "should disable a feature" do
-    FeatureToggle.enable!(:test)
-    FeatureToggle.enable!(:search)
-    FeatureToggle.disable!(:test)
-    expect(FeatureToggle.features).to eq %w(search)
-    expect(FeatureToggle.enabled?(:test, user1)).to eq false
-    expect(FeatureToggle.enabled?(:search, user1)).to eq true
+  context ".disable!" do
+    context "globally" do
+      before do
+        FeatureToggle.enable!(:search)
+      end
+      subject { FeatureToggle.disable!(:search) }
+
+      it "feature is disabled for everyone" do
+        subject
+        expect(FeatureToggle.enabled?(:search, user1)).to eq false
+        expect(FeatureToggle.enabled?(:search, user2)).to eq false
+      end
+    end
+
+    context "for a set of regional offices" do
+      before do
+        FeatureToggle.enable!(:test, regional_offices: %w(RO07 RO03))
+      end
+      subject { FeatureToggle.disable!(:test, regional_offices: ["RO03"]) }
+
+      it "users who belong to the regional offices can no longer access the feature" do
+        subject
+        expect(FeatureToggle.enabled?(:test, user1)).to eq false
+        expect(FeatureToggle.enabled?(:test, user2)).to eq true
+      end
+    end
+
+    context "when regional_offices becomes an empty array" do
+      before do
+        FeatureToggle.enable!(:test, regional_offices: %w(RO03 RO02 RO09))
+      end
+      subject { FeatureToggle.disable!(:test, regional_offices: %w(RO03 RO02 RO09)) }
+
+      it "feature becomes enabled for everyone" do
+        subject
+        expect(FeatureToggle.enabled?(:test, user1)).to eq true
+        expect(FeatureToggle.enabled?(:test, user2)).to eq true
+      end
+
+      it "feature can be disabled globally" do
+        subject
+        FeatureToggle.disable!(:test)
+        expect(FeatureToggle.enabled?(:test, user1)).to eq false
+        expect(FeatureToggle.enabled?(:test, user2)).to eq false
+      end
+    end
+
+    context "when sending an empty array" do
+      before do
+        FeatureToggle.enable!(:test, regional_offices: %w(RO03 RO02 RO09))
+      end
+      subject { FeatureToggle.disable!(:test, regional_offices: []) }
+
+      it "no regional offices are disabled" do
+        subject
+        expect(FeatureToggle.details_for(:test)[:regional_offices]).to eq %w(RO03 RO02 RO09)
+      end
+    end
+
+    context "when sending incorrect regional offices" do
+      before do
+        FeatureToggle.enable!(:test, regional_offices: %w(RO03 RO02 RO09))
+      end
+      subject { FeatureToggle.disable!(:test, regional_offices: ["RO01"]) }
+
+      it "no regional offices are disabled" do
+        subject
+        expect(FeatureToggle.details_for(:test)[:regional_offices]).to eq %w(RO03 RO02 RO09)
+      end
+    end
   end
 
-  it "should return details for a feature" do
-    FeatureToggle.enable!(:test)
-    expect(FeatureToggle.details_for(:test)).to be {}
-    FeatureToggle.enable!(:test, regional_offices: ["RO08", "RO01"])
-    expect(FeatureToggle.details_for(:test).keys).to eq ["regional_offices"]
+  context ".features" do
+
+    context "when features exist" do
+      before do
+        FeatureToggle.enable!(:test)
+        FeatureToggle.enable!(:test)
+        FeatureToggle.enable!(:search)
+      end
+      subject { FeatureToggle.features.sort }
+
+      it { is_expected.to eq [:search, :test] }
+    end
+
+    context "when features do not exist" do
+      subject { FeatureToggle.features }
+
+      it { is_expected.to eq [] }
+    end
   end
 
-  it "should enable/disable a feature for multiple regional offices" do
-    FeatureToggle.enable!(:test, regional_offices: ["RO01", "RO02", "RO03"])
-    expect(FeatureToggle.details_for(:test)["regional_offices"].sort).to eq ["RO01", "RO02", "RO03"]
-    expect(FeatureToggle.enabled?(:test, user1)).to eq true
-    expect(FeatureToggle.enabled?(:test, user2)).to eq false
+  context ".details_for" do
 
-    # disable RO03
-    FeatureToggle.disable!(:test, regional_offices: ["RO03"])
-    expect(FeatureToggle.details_for(:test)["regional_offices"].sort).to eq ["RO01", "RO02"]
-    expect(FeatureToggle.enabled?(:test, user1)).to eq false
-    expect(FeatureToggle.enabled?(:test, user2)).to eq false
+    subject { FeatureToggle.details_for(:banana) }
 
-    # enable RO07
-    FeatureToggle.enable!(:test, regional_offices: ["RO07"])
-    expect(FeatureToggle.details_for(:test)["regional_offices"].sort).to eq ["RO01", "RO02", "RO07"]
-    expect(FeatureToggle.enabled?(:test, user1)).to eq false
-    expect(FeatureToggle.enabled?(:test, user2)).to eq true
+    context "when enabled globally" do
+      before do
+        FeatureToggle.enable!(:banana)
+      end
+      it { is_expected.to be {} }
+    end
 
-    # feature doesn't exist
-    expect(FeatureToggle.enabled?(:foo, user2)).to eq false
+    context "when not enabled" do
+      it { is_expected.to be nil }
+    end
 
-    # disable the entire feature
-    FeatureToggle.disable!(:test)
-    expect(FeatureToggle.details_for(:test)).to eq nil
-    expect(FeatureToggle.enabled?(:test, user1)).to eq false
-    expect(FeatureToggle.enabled?(:test, user2)).to eq false
+    context "when enabled for a list of regional offices" do
+      before do
+        FeatureToggle.enable!(:banana, regional_offices: %w(RO03 RO02 RO09))
+      end
+      it { is_expected.to eq(regional_offices: %w(RO03 RO02 RO09)) }
+    end
+  end
+
+  context ".enabled?" do
+    context "when enabled for everyone" do
+      before do
+        FeatureToggle.enable!(:search)
+      end
+      subject { FeatureToggle.enabled?(:search, user1) }
+
+      it { is_expected.to eq true }
+    end
+
+    context "when a feature does not exist in redis" do
+      subject { FeatureToggle.enabled?(:foo, user1) }
+
+      it { is_expected.to eq false }
+    end
+
+    context "when enabled for a set of regional_offices" do
+      subject { FeatureToggle.enabled?(:search, user) }
+
+      before do
+        FeatureToggle.enable!(:search, regional_offices: %w(RO01 RO02 RO03))
+      end
+
+      context "if a user is associated with a regional office" do
+        let(:user) { User.new(regional_office: "RO02") }
+        it { is_expected.to eq true }
+      end
+
+      context "if a user is not associated with a regional office" do
+        let(:user) { User.new(regional_office: "RO09") }
+        it { is_expected.to eq false }
+      end
+    end
   end
 end
