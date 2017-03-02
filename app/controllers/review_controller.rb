5601dada7ce57f3baad779864b804d218080e2dc
Mark Benjamin <mark@navahq.com>
2017-03-02 09:46:46 -0500

    Increasing the clientside caching of documents from e-reader to get speed improvements!

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -27,9 +27,8 @@ class ReviewController < ApplicationController
   def pdf
     document = Document.find(params[:id])
 
-    # The line below enables document caching for 12 hours. It's best to disable
-    # this while we're still developing.
-    # expires_in 12.hours, :public => true
+    # The line below enables document caching for 48 hours.
+    expires_in 48.hours, :public => true
     send_file(
       document.serve,
       type: "application/pdf",
