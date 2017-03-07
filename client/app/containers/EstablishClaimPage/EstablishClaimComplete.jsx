50286dd1c991d015b7af9aed9d49e43540140403
Teja Naraparaju <teja.s.naraparaju@gmail.com>
2017-03-07 18:47:15 -0500

    changing the no cases left check to have consistency

--- client/app/containers/EstablishClaimPage/EstablishClaimComplete.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaimComplete.jsx
@@ -21,23 +21,19 @@ export default class EstablishClaimComplete extends React.Component {
     } = this.props;
 
     let casesAssigned, employeeCountInt,
-      noCasesLeft, todayfeedbackText, totalCases;
+      todayfeedbackText, totalCases;
 
     const noMoreCasesMessage = <div>There are no more cases to work today.
     <a href="/dispatch/establish-claim"> Return to homepage</a> to view your work history.
     </div>;
 
-
-    // there are certain containers using this component without these
-    // stats being specified.
-    noCasesLeft = totalCasesToComplete === totalCasesCompleted;
     totalCases = totalCasesToComplete + totalCasesCompleted;
     employeeCountInt = parseInt(employeeCount, PARSE_INT_RADIX);
 
     casesAssigned = employeeCountInt > 0 ?
       Math.ceil(totalCases / employeeCountInt) : 0;
 
-    todayfeedbackText = noCasesLeft ? '' : ' today';
+    todayfeedbackText = availableTasks ? ' today' : '';
 
     return <div>
       <div
@@ -55,9 +51,9 @@ export default class EstablishClaimComplete extends React.Component {
          <div>{
           `Way to go! You have completed ${totalCasesCompleted} out of the
           ${casesAssigned} cases assigned to you${todayfeedbackText}.`}</div>
-          {noCasesLeft ?
-            noMoreCasesMessage :
-            `You can now establish the next claim or go back to your work history.`
+          {availableTasks ?
+            `You can now establish the next claim or go back to your work history.` :
+            noMoreCasesMessage
           }
          </div>
         </ul>
@@ -77,11 +73,16 @@ export default class EstablishClaimComplete extends React.Component {
         />
         }
         { !availableTasks &&
-        <Button
+        <div>
+          <span className="cf-button-associated-text-right">
+            { "There are no more claims in your queue" }
+          </span>
+          <Button
             name={buttonText}
             classNames={["usa-button-disabled", "cf-push-right"]}
             disabled={true}
         />
+        </div>
         }
       </div>
     </div>
