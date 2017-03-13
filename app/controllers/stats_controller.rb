ef1dd8c3a0a6e092173f86c07bc6a532ac8cea01
Sharon Warner <sharon.warner56@gmail.com>
2017-03-13 14:52:59 -0400

    New dispatch stats page

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,8 @@
+class StatsController < ApplicationController
+  before_action :verify_authentication
+  before_action :verify_access
+
+  def verify_access
+    verify_authorized_roles("System Admin")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -83,6 +83,8 @@ Rails.application.routes.draw do
   get 'whats-new' => 'whats_new#show'
 
   get 'certification/stats(/:interval)', to: 'certification_stats#show', as: 'certification_stats'
+  get 'dispatch/stats(/:interval)', to: 'dispatch_stats#show', as: 'dispatch_stats'
+  get 'stats', to: 'stats#show'
 
   get "styleguide", to: "styleguide#show"
 
