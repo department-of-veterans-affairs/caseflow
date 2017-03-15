353899a23c70a8d86eb21af785e6f5694347cd24
Shane Russell <shane1337@gmail.com>
2017-03-15 17:23:34 -0400

    Change <Table> component api. Update all the things. (#1198)
    * Change <Table> component api. Update all the things.
    
    Also add table to the style guide.
    Also remove annoying eslint options `require-jsdoc` and `sort-keys`
    that were doing more harm than good.
    
    * Fix some issues
    
    * Fix some things
    
    * Nitz
    
    * mount instead of shallow

--- client/app/containers/UnpreparedTasksIndex.jsx
+++ client/app/containers/UnpreparedTasksIndex.jsx
@@ -2,31 +2,41 @@ import React, { PropTypes } from 'react';
 import Table from '../components/Table';
 import { formatDate } from '../util/DateUtil';
 
-const TABLE_HEADERS = ['Veteran', 'Decision Type', 'Decision Date', 'Days in Queue'];
 
 export default class UnpreparedTasksIndex extends React.Component {
-
-  buildUnpreparedTaskRow = (task) => [
-    `${task.appeal.veteran_name} (${task.appeal.vbms_id})`,
-    task.appeal.decision_type,
-    formatDate(task.appeal.serialized_decision_date),
-    `${task.days_since_creation} days`
-  ];
-
   render() {
     let {
       unpreparedTasks
     } = this.props;
+
+    let tableColumns = [
+      {
+        header: 'Veteran',
+        valueFunction: (task) =>
+          `${task.appeal.veteran_name} (${task.appeal.vbms_id})`
+      },
+      {
+        header: 'Decision Type',
+        valueFunction: (task) => task.appeal.decision_type
+      },
+      {
+        header: 'Decision Date',
+        valueFunction: (task) => formatDate(task.appeal.serialized_decision_date)
+      },
+      {
+        header: 'Days in Queue',
+        valueFunction: (task) => `${task.days_since_creation} days`
+      }
+    ];
 
     return <div className="cf-app-segment cf-app-segment--alt">
       <h1>Claims Missing Decisions - {formatDate(new Date().toDateString())}</h1>
 
       <div className="usa-grid-full">
         <Table
-          headers={TABLE_HEADERS}
-          buildRowValues={this.buildUnpreparedTaskRow}
-          values={unpreparedTasks}
-        />
+          columns={tableColumns}
+          rowObjects={unpreparedTasks}
+          summary="Appeals missing decisions" />
       </div>
     </div>;
   }
