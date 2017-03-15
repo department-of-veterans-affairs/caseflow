1d58744bb63fbd19c907aa0109bf51704b5d41b2
Alex Prokop <alex.prokop@gmail.com>
2017-03-15 12:24:52 -0400

    fix and formatting

--- client/app/certification/DocumentsCheckSuccess.jsx
+++ client/app/certification/DocumentsCheckSuccess.jsx
@@ -9,36 +9,15 @@ export default class DocumentsCheckSuccess extends React.Component {
   render() {
     return <div>
       <div className="cf-app-segment cf-app-segment--alt">
-      <h2>Check Documents</h2>
-      <DocumentsMatchingBox/>
-      <DocumentsCheckTable/>
+        <h2>Check Documents</h2>
+        <DocumentsMatchingBox/>
+        <DocumentsCheckTable/>
+      </div>
 
-      <p>
-        Caseflow could not find the documents marked with an X (todo icon missing)
-        in the appellant's eFolder. This usually happens when something doesn't match up. Try checking:
-      </p>
-
-      <ul>
-        <li>The <strong>document type</strong> in VBMS to make sure it's <a href="/#mismatched-documents">labeled correctly</a></li>
-        <li>The <strong>document date</strong> &#8212; the date in VBMS must match the date in VACOLS</li>
-      </ul>
-
-      <p>
-        Once you've made corrections,&nbsp;
-        <button type="button" className="cf-action-refresh cf-btn-link cf-btn-link--inline">refresh this page.</button>
-      </p>
-
-      <p>
-        If you can’t find the document,&nbsp;
-        <a href="#confirm-cancel-certification" className="cf-action-openmodal cf-btn-link">cancel this certification.</a>
-      </p>
-
+      <div className="cf-app-segment">
+        <a href="#confirm-cancel-certification" className="cf-action-openmodal cf-btn-link">Cancel Certification</a>
+        <button type="button" className="cf-push-right">Continue</button>
+      </div>
     </div>
-
-    <div className="cf-app-segment">
-      <a href="#confirm-cancel-certification" className="cf-action-openmodal cf-btn-link">Cancel Certification</a>
-      <button type="button" className="cf-push-right cf-action-refresh">Refresh Page</button>
-    </div>
-  </div>
   }
 }
