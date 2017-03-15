e80b6b4b0f89a285c01975a46759495ee992bad2
Ari Perez <ariperez83@gmail.com>
2017-03-15 10:56:13 -0400

    Improvements to Naming and Styling

--- client/app/containers/EstablishClaimPage/EstablishClaimNote.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaimNote.jsx
@@ -95,7 +95,7 @@ export default class EstablishClaimNote extends BaseForm {
       <p>To ensure this claim is routed correctly, we will take the following
       steps in VACOLS:</p>
 
-      <ol className="cf-bold-ordered-list-alpha">
+      <ol className="cf-bold-ordered-list">
         <li type="A">
           <div>
             <span className="inline-label">Change location to: </span>
