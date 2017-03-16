41fe467a4a167670891e40971d2ee6d518af4b1d
Alex Prokop <alex.prokop@gmail.com>
2017-03-16 13:06:06 -0400

    Certification v2 documents success page (#1200)
    * jump off point for v2
    
    * config
    
    * renders v2
    
    * register components
    
    * only show for successful certs
    
    * removes typo
    
    * certification v2
    
    * adds lotsa files
    
    * fixes
    
    * fix and formatting
    
    * makes fixes and adds comments
    
    * ruuuubocop
    
    * cleanup
    
    * fixes finally!
    
    * indent back

---
+++ client/app/certification/DocumentsCheckSuccess.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import DocumentsMatchingBox from './DocumentsMatchingBox';
+import DocumentsCheckTable from './DocumentsCheckTable';
+
+// TODO: refactor to use shared components where helpful
+const DocumentsCheckSuccess = () => {
+  return <div>
+    <div className="cf-app-segment cf-app-segment--alt">
+      <h2>Check Documents</h2>
+      <DocumentsMatchingBox/>
+      <DocumentsCheckTable/>
+    </div>
+
+    <div className="cf-app-segment">
+      <a href="#confirm-cancel-certification"
+        className="cf-action-openmodal cf-btn-link">
+        Cancel Certification
+      </a>
+      <button type="button" className="cf-push-right">
+        Continue
+      </button>
+    </div>
+  </div>;
+};
+
+export default DocumentsCheckSuccess;
