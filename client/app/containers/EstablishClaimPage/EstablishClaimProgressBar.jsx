39504ee9a89ede24bcc8a45717cb322bae4754aa
Alex Prokop <alex.prokop@gmail.com>
2017-03-16 11:19:38 -0400

    indent back

--- client/app/containers/EstablishClaimPage/EstablishClaimProgressBar.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaimProgressBar.jsx
@@ -6,20 +6,20 @@ export default class EstablishClaimProgressBar extends React.Component {
   render() {
     return <ProgressBar
       sections = {
-        [
-          {
-            activated: this.props.isReviewDecision,
-            title: '1. Review Decision'
-          },
-          {
-            activated: this.props.isRouteClaim,
-            title: '2. Route Claim'
-          },
-          {
-            activated: this.props.isConfirmation,
-            title: '3. Confirmation'
-          }
-        ]
+      [
+        {
+          activated: this.props.isReviewDecision,
+          title: '1. Review Decision'
+        },
+        {
+          activated: this.props.isRouteClaim,
+          title: '2. Route Claim'
+        },
+        {
+          activated: this.props.isConfirmation,
+          title: '3. Confirmation'
+        }
+      ]
       }
     />;
   }
