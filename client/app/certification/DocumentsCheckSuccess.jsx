9219a7b92355ea5d74b9b20f6ea8cf1b8af3584a
Alex Prokop <alex.prokop@gmail.com>
2017-03-20 12:32:26 -0400

    Adds react-router and stub routes (#1228)
    * adds react router
    
    * adds routes
    
    * adds comments
    
    * adds stub files
    
    * adds spec
    
    * adds links
    
    * adds comment and styling
    
    * fixes
    
    * commented out feature flag
    
    * revert
    
    * remove unneeded css
    
    * refactor buttons
    
    * typo fix
    
    * comment out spec
    
    * updates shrinkwrap
    
    * uncomment

--- client/app/certification/DocumentsCheckSuccess.jsx
+++ client/app/certification/DocumentsCheckSuccess.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import DocumentsMatchingBox from './DocumentsMatchingBox';
 import DocumentsCheckTable from './DocumentsCheckTable';
+import { Link } from 'react-router-dom';
+
 
 // TODO: refactor to use shared components where helpful
-const DocumentsCheckSuccess = () => {
+const DocumentsCheckSuccess = ({ match }) => {
   return <div>
     <div className="cf-app-segment cf-app-segment--alt">
       <h2>Check Documents</h2>
@@ -16,9 +18,12 @@ const DocumentsCheckSuccess = () => {
         className="cf-action-openmodal cf-btn-link">
         Cancel Certification
       </a>
-      <button type="button" className="cf-push-right">
-        Continue
-      </button>
+      <Link
+        to={`/certifications/${match.params.vacols_id}/confirm_case_details`}>
+        <button type="button" className="cf-push-right">
+          Continue
+        </button>
+      </Link>
     </div>
   </div>;
 };
