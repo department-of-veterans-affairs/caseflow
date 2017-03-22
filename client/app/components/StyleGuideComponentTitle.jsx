777a6a22837ee43cdd7f280cd6dc34057b8183ee
Marian Lumba <mmlumba@gmail.com>
2017-03-22 11:43:06 -0400

    add style guide component title

---
+++ client/app/components/StyleGuideComponentTitle.jsx
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+import { githubHtml } from './RenderFunctions';
+
+export default class StyleGuideComponentTitle extends React.Component {
+
+  viewSourceCode = (link) => {
+    /* eslint-disable max-len */
+
+    let baseUrl = "https://github.com/department-of-veterans-affairs/caseflow/blob/master/client/app/containers/StyleGuide/";
+
+    return <span>
+      <a className="usa-button" href={baseUrl + link} target="_blank">{githubHtml()} View Source Code</a>
+    </span>;
+
+    /* eslint-enable max-len */
+  }
+
+  render() {
+    let {
+      id,
+      link,
+      title
+    } = this.props;
+
+    /* Link is only the name of the file that you want to link to in the
+    Style Guide (aka StyleGuideModal.jsx)*/
+
+    return <div className="usa-width-one-whole">
+      <div className="cf-push-left">
+        <h2 id={id}>{title}</h2>
+      </div>
+      <div className="cf-push-right">
+        {this.viewSourceCode(link)}
+      </div>
+    </div>;
+  }
+}
+
+StyleGuideComponentTitle.props = {
+  id: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
