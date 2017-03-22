2f57321e5bd2b1115d15d1994028fdde99d567c1
Ari Perez <ariperez83@gmail.com>
2017-03-22 09:54:39 -0400

    Add form visually; still need to hook up to props

--- client/app/certification/SignAndCertify.jsx
+++ client/app/certification/SignAndCertify.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
+import TextField from '../components/TextField';
+import DateSelector from '../components/DateSelector';
+import RadioField from '../components/RadioField';
 
 // TODO: refactor to use shared components where helpful
 const SignAndCertify = () => {
   return <div>
-    <div className="cf-app-segment cf-app-segment--alt">
-      <h2>Sign and Certify</h2>
-    </div>
+    <form noValidate id="end_product">
+      <div className="cf-app-segment cf-app-segment--alt">
+        <h2>Sign and Certify</h2>
 
-    {/*
-      Here, we'll add the name/organization/title/date questions
-      and finalize the certification.
-    */}
+        <p>Fill in information about yourself below to sign this certification.</p>
+        <TextField
+         label="Name and location of certifying office:"
+         name="BenefitType"
+         value="C&P Live"
+         readOnly={true}
+        />
+        <TextField
+         label="Organizational elements certifying appeal:"
+         name="a"
+         value="00 - Veteran"
+         readOnly={true}
+        />
+        <TextField
+         label="Name of certifying official:"
+         name="Payee"
+         value="00 - Veteran"
+         readOnly={true}
+        />
+        <RadioField
+         label="Name of certifying official:"
+         name="Payee"
+         options={["Decision Review Officer", "Rating Specialist", "Veterans Service Representative", "Claims Assistant", "Other"]}
+        />
+        <DateSelector
+          label="Decision Date"
+          name="Date:"
+          readOnly={true}
+        />
+      </div>
+    </form>
 
     <div className="cf-app-segment">
       <a href="#confirm-cancel-certification"
         className="cf-action-openmodal cf-btn-link">
-        Cancel Certification
+        Cancel certification
       </a>
       <button type="button" className="cf-push-right">
-        Continue
+        Certify appeal
       </button>
     </div>
   </div>;
