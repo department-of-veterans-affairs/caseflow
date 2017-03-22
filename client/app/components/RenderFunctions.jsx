ddd238c93fbf2752cf7a3584a6fe6906f7273ba3
Marian Lumba <mmlumba@gmail.com>
2017-03-22 11:58:40 -0400

    change it back to failed from master

--- client/app/components/RenderFunctions.jsx
+++ client/app/components/RenderFunctions.jsx
@@ -104,7 +104,7 @@ export let missingSymbolHtml = function() {
   return (
     <svg width="55" height="55" class="cf-icon-missing"
     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 55 55">
-      <title>failed</title>
+      <title>missing icon</title>
       <path d="M52.6 46.9l-6 6c-.8.8-1.9 1.2-3 1.2s-2.2-.4-3-1.2l-13-13-13
       13c-.8.8-1.9 1.2-3 1.2s-2.2-.4-3-1.2l-6-6c-.8-.8-1.2-1.9-1.2-3s.4-2.2
       1.2-3l13-13-13-13c-.8-.8-1.2-1.9-1.2-3s.4-2.2 1.2-3l6-6c.8-.8 1.9-1.2
