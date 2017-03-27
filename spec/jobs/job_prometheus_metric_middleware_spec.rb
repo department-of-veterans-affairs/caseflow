b21174e3028141e2023bdda1f1e0bdde05451487
JD Pagano <jonathanpagano@gmail.com>
2017-03-27 18:16:15 -0400

    Add background job middleware test

---
+++ spec/jobs/job_prometheus_metric_middleware_spec.rb
@@ -0,0 +1,29 @@
+describe JobPrometheusMetricMiddleware do
+  before do
+    @middleware = JobPrometheusMetricMiddleware.new
+
+    @msg = {
+      "args" => [{
+        "job_class" => "FunTestJob"
+      }]
+    }
+    @yield_called = false
+    allow(PrometheusService).to receive(:push_metrics!).and_return(nil)
+  end
+
+
+  context ".call" do
+    let (:call) { @middleware.call(nil, @msg, :default) { @yield_called = true } }
+
+    it "always increments attempts counter" do
+      labels = { name: "FunTestJob" }
+
+      expect(PrometheusService.background_jobs_attempt_counter.values[labels]).to eq(nil)
+      expect(@yield_called).to be_falsey
+      call
+      expect(@yield_called).to be_truthy
+      expect(PrometheusService.background_jobs_attempt_counter.values[labels]).to eq(1)
+    end
+
+  end
+end
