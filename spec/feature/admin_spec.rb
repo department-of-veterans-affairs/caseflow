75f2edefe08abd8ee6617c8dcaae05e3d866ac36
Sharon Warner <sharon.warner56@gmail.com>
2017-03-27 14:53:04 -0400

    Fixing URLs in tests

--- spec/feature/admin_spec.rb
+++ spec/feature/admin_spec.rb
@@ -73,7 +73,7 @@ RSpec.feature "Admin" do
     page.click_on "Create Task"
 
     # ensure new task is created
-    expect(page).to have_current_path("/admin/establish_claim")
+    expect(page).to have_current_path("/admin/establish-claim")
     expect(page).to have_content "Task created"
     expect(Task.count).to eq(3)
 
