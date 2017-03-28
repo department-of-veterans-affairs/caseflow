276b05f17030d94a6ff011eb531aca2ebf757bef
JD Pagano <joofsh@users.noreply.github.com>
2017-03-28 18:55:29 -0400

    Fix sidekiq & job metrics (#1322)
    * Fix sidekiq & job metrics
    
    - Ensure we are using Sidekiq as our adapter for ActiveJob
    - Improve job prometheus metrics by breaking them down by job name
    
    * Fix load circular dependency
    
    * foo
    
    * Add Prometheus Pushgateway
    
    * Add background job middleware test
    
    * Add more tests
    
    * lint
    
    * lint fixes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/app/jobs/middleware/job_prometheus_metric_middleware"
+
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.secrets.redis_url_sidekiq }
 
@@ -7,6 +9,10 @@ Sidekiq.configure_server do |config|
     active_jobs = jobs.reject { |k, v| v["disabled"] }
 
     Sidekiq::Cron::Job.load_from_hash! active_jobs
+  end
+
+  config.server_middleware do |chain|
+    chain.add JobPrometheusMetricMiddleware
   end
 end
 
