69ed7d07f135bd5584fb1cd986ca4a448aed794c
Shade Lawrence Jenifer <shade.jenifer@pro-spheretek.com>
2017-03-29 14:46:15 -0400

    click_loading_button() is now safe_click_on()

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,12 +99,8 @@ def find_label_for(val)
   find(:xpath, "//label[@for='#{val}']")
 end
 
-def click_loading_button(text)
-  find(:xpath, "//button[contains(., '#{text}')]").trigger("click")
-end
-
 # PhantomJS sometimes crashes if an element in focus is removed from the DOM
 # This click action triggers a click event on a button without focusing on it.
 def safe_click_on(text)
-  click_loading_button(text)
+  find(:xpath, "//button[contains(., '#{text}')]").trigger("click")
 end
