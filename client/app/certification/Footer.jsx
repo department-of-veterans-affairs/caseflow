4ca42849b50dcd36ddbd19d524e9b9bdf0dcb2cf
Alex Prokop <alex.prokop@gmail.com>
2017-03-29 13:54:51 -0400

    adds footer todo

--- client/app/certification/Footer.jsx
+++ client/app/certification/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+// TODO: use the footer (see ConfirmHearing.jsx) everywhere,
+// then delete this comment :)
 const Footer = ({ nextPageUrl }) => {
   return <div>
     <div className="cf-app-segment">
