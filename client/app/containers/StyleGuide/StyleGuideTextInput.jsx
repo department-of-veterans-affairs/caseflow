0af426f7faa7d6f45f8c2ec00279d83ba048e7a4
Marian Lumba <mmlumba@gmail.com>
2017-03-29 10:20:07 -0400

    change InlineField to InlineForm and add correct copy for inline field

--- client/app/containers/StyleGuide/StyleGuideTextInput.jsx
+++ client/app/containers/StyleGuide/StyleGuideTextInput.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import TextField from '../../components/TextField';
 import Button from '../../components/Button';
 import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
-import InlineField from '../../components/InlineField';
+import InlineForm from '../../components/InlineForm';
 
 export default function StyleGuideTextInput() {
   return <div>
@@ -14,12 +14,14 @@ export default function StyleGuideTextInput() {
         id="text_input"
         link="StyleGuideTextInput.jsx"
       />
-    <h3>Inline Input</h3>
+    <h3>Inline Text Input</h3>
       <p>
-        Inline form fields are used for single values only. This field will have
-        a label, text input, and a blue CTA button aligned in one row.
+        Text input allows users to insert numbers or text into a field.
+        The width of the input field will vary according to the context,
+        whether it is for a number or name of a state, so that users are primed
+        to know what to type in.
       </p>
-    <InlineField>
+    <InlineForm>
       <TextField
         label="Enter the number of people working today"
         name="dummyEmployeeCount"
@@ -28,6 +30,6 @@ export default function StyleGuideTextInput() {
       <Button
         name="Update"
       />
-    </InlineField>
+  </InlineForm>
   </div>;
 }
--- client/app/containers/TasksManager/TasksManagerEmployeeCount.jsx
+++ client/app/containers/TasksManager/TasksManagerEmployeeCount.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import TextField from '../../components/TextField';
 import Button from '../../components/Button';
-import InlineField from '../../components/InlineField';
+import InlineForm from '../../components/InlineForm';
 
 export default class TasksManagerEmployeeCount extends React.Component {
   render() {
@@ -13,7 +13,7 @@ export default class TasksManagerEmployeeCount extends React.Component {
     } = this.props;
 
     return <div>
-      <InlineField>
+      <InlineForm>
           <TextField
             label="Enter the number of people working today"
             name="employeeCount"
@@ -28,7 +28,7 @@ export default class TasksManagerEmployeeCount extends React.Component {
             onClick={handleEmployeeCountUpdate}
             disabled={!employeeCountForm.employeeCount.value}
           />
-      </InlineField>
+      </InlineForm>
     </div>;
   }
 }
