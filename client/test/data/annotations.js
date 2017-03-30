9cd048f0fb7c9fae4f5942ad6d7555253ce4094d
Mark Benjamin <mark@navahq.com>
2017-03-29 23:09:34 -0400

    Adding helper functions

---
+++ client/test/data/annotations.js
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+export const annotations = [{
+  class: 'Annotation',
+  page: 1,
+  type: 'point',
+  x: 50,
+  y: 60,
+  comment: 'Test Comment',
+  document_id: 2,
+  id: 2
+}];
+/* eslint-enable camelcase */
---
+++ client/test/data/documents.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+export const documents = [
+  {
+    id: 1,
+    filename: 'doc1',
+    received_at: '01/02/2017',
+    label: null,
+    type: 'bva decision'
+  },
+  {
+    id: 2,
+    filename: 'doc2',
+    received_at: '03/04/2017',
+    label: null,
+    type: 'form 9'
+  }
+];
+/* eslint-enable camelcase */
