844a3480d8b1fa847e4e9dd78f5b0dce7f94a2f8
Alex Prokop <alex.prokop@gmail.com>
2017-04-02 23:09:25 -0400

    whitespace

--- client/app/certification/reducers/index.js
+++ client/app/certification/reducers/index.js
@@ -20,75 +20,76 @@ const initialState = {
 };
 
 // TODO: break this out into a separate actions file.
- const updateRepresentativeType = (state, action) => {
-   const update = {};
-   update.represesntativeType = action.payload.representativeType;
+const updateRepresentativeType = (state, action) => {
+  const update = {};
+
+  update.represesntativeType = action.payload.representativeType;
   // if we changed the type to something other than "Other",
   // erase the other representative type if it was specified.
-   if (state.representativeType !== Constants.representativeTypes.OTHER) {
-     update.otherRepresentativeType = null;
-   }
+  if (state.representativeType !== Constants.representativeTypes.OTHER) {
+    update.otherRepresentativeType = null;
+  }
 
-   return Object.assign({}, state, update);
- };
+  return Object.assign({}, state, update);
+};
 
- const certification = function(state = initialState, action = {}) {
-   switch (action.type) {
-   case Constants.CHANGE_REPRESENTATIVE_NAME:
-     return Object.assign({}, state, {
-       representativeName: action.payload.representativeName
-     });
-   case Constants.CHANGE_REPRESENTATIVE_TYPE: {
-     return updateRepresentativeType(state, action);
-   }
-   case Constants.CHANGE_OTHER_REPRESENTATIVE_TYPE: {
-     return Object.assign({}, state, {
-       otherRepresentativeType: action.payload.otherRepresentativeType
-     });
-   }
-   case Constants.CHANGE_VBMS_HEARING_DOCUMENT:
-     return Object.assign({}, state, {
-       hearingDocumentIsInVbms: action.payload.hearingDocumentIsInVbms,
+const certification = function(state = initialState, action = {}) {
+  switch (action.type) {
+  case Constants.CHANGE_REPRESENTATIVE_NAME:
+    return Object.assign({}, state, {
+      representativeName: action.payload.representativeName
+    });
+  case Constants.CHANGE_REPRESENTATIVE_TYPE: {
+    return updateRepresentativeType(state, action);
+  }
+  case Constants.CHANGE_OTHER_REPRESENTATIVE_TYPE: {
+    return Object.assign({}, state, {
+      otherRepresentativeType: action.payload.otherRepresentativeType
+    });
+  }
+  case Constants.CHANGE_VBMS_HEARING_DOCUMENT:
+    return Object.assign({}, state, {
+      hearingDocumentIsInVbms: action.payload.hearingDocumentIsInVbms,
       // If we change the answer for the hearing doc in VBMS question,
       // also wipe the state for the type of hearing the Veteran prefers,
       // since the previous answer is no longer valid.
-       hearingType: null
-     });
-   case Constants.CHANGE_TYPE_OF_FORM9:
-     return Object.assign({}, state, {
-       form9Type: action.payload.form9Type,
+      hearingType: null
+    });
+  case Constants.CHANGE_TYPE_OF_FORM9:
+    return Object.assign({}, state, {
+      form9Type: action.payload.form9Type,
       // If we change the answer for the form 9 type question,
       // also wipe the state for the type of hearing the Veteran prefers,
       // since the previous answer is no longer valid.
-       hearingType: null
-     });
-   case Constants.CHANGE_TYPE_OF_HEARING:
-     return Object.assign({}, state, {
-       hearingType: action.payload.hearingType
-     });
-   case Constants.CHANGE_CERTIFYING_OFFICIAL:
-     return Object.assign({}, state, {
-       certifyingOffice: action.payload.certifyingOffice
-     });
-   case Constants.CHANGE_CERTIFYING_USERNAME:
-     return Object.assign({}, state, {
-       certifyingUsername: action.payload.certifyingUsername
-     });
-   case Constants.CHANGE_CERTIFYING_OFFICIAL_NAME:
-     return Object.assign({}, state, {
-       certifyingOfficialName: action.payload.certifyingOfficialName
-     });
-   case Constants.CHANGE_CERTIFYING_OFFICIAL_TITLE:
-     return Object.assign({}, state, {
-       certifyingOfficialTitle: action.payload.certifyingOfficialTitle
-     });
-   case Constants.CHANGE_CERTIFICATION_DATE:
-     return Object.assign({}, state, {
-       certificationDate: action.payload.certificationDate
-     });
-   default:
-     return state;
-   }
- };
+      hearingType: null
+    });
+  case Constants.CHANGE_TYPE_OF_HEARING:
+    return Object.assign({}, state, {
+      hearingType: action.payload.hearingType
+    });
+  case Constants.CHANGE_CERTIFYING_OFFICIAL:
+    return Object.assign({}, state, {
+      certifyingOffice: action.payload.certifyingOffice
+    });
+  case Constants.CHANGE_CERTIFYING_USERNAME:
+    return Object.assign({}, state, {
+      certifyingUsername: action.payload.certifyingUsername
+    });
+  case Constants.CHANGE_CERTIFYING_OFFICIAL_NAME:
+    return Object.assign({}, state, {
+      certifyingOfficialName: action.payload.certifyingOfficialName
+    });
+  case Constants.CHANGE_CERTIFYING_OFFICIAL_TITLE:
+    return Object.assign({}, state, {
+      certifyingOfficialTitle: action.payload.certifyingOfficialTitle
+    });
+  case Constants.CHANGE_CERTIFICATION_DATE:
+    return Object.assign({}, state, {
+      certificationDate: action.payload.certificationDate
+    });
+  default:
+    return state;
+  }
+};
 
- export default certification;
+export default certification;
