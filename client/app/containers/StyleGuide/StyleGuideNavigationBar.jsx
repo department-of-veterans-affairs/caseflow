cb6c4991a8ffdb4a7737d0c1b98b0ff3ac43359a
Marian Lumba <mmlumba@gmail.com>
2017-04-13 12:12:27 -0400

    add copy for navigation bar

--- client/app/containers/StyleGuide/StyleGuideNavigationBar.jsx
+++ client/app/containers/StyleGuide/StyleGuideNavigationBar.jsx
@@ -9,6 +9,20 @@ export default class StyleGuideNavigationBar extends React.Component {
         id="navigation_bar"
         link="StyleGuideNavigationBar.jsx"
       />
+    <p>
+      The Navigation Bar is a simple white bar that sits on top of every application.
+      Our navigation bar is non-sticky and scrolls out of view as the user scrolls
+      down the page. It includes branding for the specific application on the left;
+      a Caseflow logo and application name (see Application Branding for more details).
+    </p>
+    <p>
+      The Navigation Bar also includes the user menu on the right.
+      This menu indicates which user is signed in and contains links to submit feedback,
+      view the application’s help page, see newly launched features, and log out.
+    </p>
+    <p>
+      The navigation bar is a total of 90px tall with a 1px border-botom colored grey-lighter.
+    </p>
       <nav className="cf-nav">
         <a href="#" id="cf-logo-link">
           <h1 className="cf-logo"><span className="cf-logo-image cf-logo-image-default"></span>Caseflow</h1>
