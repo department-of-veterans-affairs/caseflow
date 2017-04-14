4dfd89ade740dd4f3d37444b0ee89eef79b24df6
Teja N <teja.s.naraparaju@gmail.com>
2017-04-14 15:04:20 -0400

    Adding tags model, updated document generator, updated ui

--- client/app/components/PdfSidebar.jsx
+++ client/app/components/PdfSidebar.jsx
@@ -1,14 +1,29 @@
 import React, { PropTypes } from 'react';
 import { formatDate } from '../util/DateUtil';
 import Comment from '../components/Comment';
+import SearchableDropdown from '../components/SearchableDropdown';
 import EditComment from '../components/EditComment';
 
 // PdfSidebar shows relevant document information and comments.
 // It is intended to be used with the PdfUI component to
 // show a PDF with it's corresponding information.
 export default class PdfSidebar extends React.Component {
+
+  generateOptionsFromTags = (tags) => {
+    if (!tags || tags.length <= 0) {
+      return {};
+    }
+    return tags.map((tag) => {
+      return { value: tag.text, label: tag.text }
+    });
+  };
+
   render() {
     let comments = [];
+
+    const {
+      doc
+    } = this.props;
 
     comments = this.props.comments.map((comment, index) => {
       if (comment.uuid === this.props.editingComment) {
@@ -37,6 +52,18 @@ export default class PdfSidebar extends React.Component {
 
     return <div className="cf-sidebar-wrapper">
         <div className="cf-document-info-wrapper">
+          <div className="cf-heading-alt">
+            Related Issues
+          </div>
+          <SearchableDropdown 
+            name="tags"
+            label="Click in the box below to select, type, or add in issue(s)" 
+            multi={true}
+            creatable={true}
+            options={this.generateOptionsFromTags(doc.tags)}
+            placeholder="Select or type issue"
+            classNames={[]}
+          />
           <div className="cf-heading-alt">Document</div>
           <p className="cf-pdf-meta-title">
             <b>Filename:</b> {this.props.doc.filename}
@@ -47,6 +74,7 @@ export default class PdfSidebar extends React.Component {
           <p className="cf-pdf-meta-title">
             <b>Receipt Date:</b> {formatDate(this.props.doc.receivedAt)}
           </p>
+          <button>Hello</button>
           <div className="cf-heading-alt">
             Comments
             <span className="cf-right-side">
