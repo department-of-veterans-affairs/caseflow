dff378bfc5466c09a57460f2cb906184f052187a
Nick Heiner <nth23@cornell.edu>
2017-04-14 15:08:38 -0400

    Fine, add safe_click_on back in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,3 +98,8 @@ end
 def find_label_for(val)
   find(:xpath, "//label[@for='#{val}']")
 end
+
+# TODO: This was to tip-toe around PhantomJS crashes, delete it when we are 100% with chrome
+def safe_click_on(text)
+  click_on(text)
+end
