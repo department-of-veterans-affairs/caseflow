29342ddc33aad3c0269326f72bb12d033cc9c14f
Sjones352 <sjones@tistatech.com>
2017-04-14 21:48:02 -0400

    added tests

--- app/controllers/help_dispatch_controller.rb
+++ app/controllers/help_dispatch_controller.rb
@@ -1,4 +1,12 @@
 class HelpDispatchController < ApplicationController
-	def show
-   end
+	  def show
+    end
+
+    def logo_name   
+    "Dispatch" 
+   end  
+
+    def logo_path   
+    "establish_claims_path" 
+  end
 end
--- spec/controllers/help_dispatch_controller_spec.rb
+++ spec/controllers/help_dispatch_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HelpDispatchController, type: :controller do
 
+	describe "GET #show" do
+    it "assigns the requested page to @help_dispatch"
+    it "renders the :show template"
+  end
+
 end
