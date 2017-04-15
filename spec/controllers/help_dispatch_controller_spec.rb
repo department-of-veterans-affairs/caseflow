54dc4fb57de59547a8c0e282998e0401f22dd87c
Sjones352 <sjones@tistatech.com>
2017-04-14 23:19:51 -0400

    rspec tests

--- spec/controllers/help_dispatch_controller_spec.rb
+++ spec/controllers/help_dispatch_controller_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HelpDispatchController, type: :controller do
 
-	describe "GET #show" do
-    it "assigns the requested page to @help_dispatch"
-    it "renders the :show template"
+	describe 'GET #show' do
+      context 'Welcome to the Dispatch Help Page!' 
   end
-
 end
