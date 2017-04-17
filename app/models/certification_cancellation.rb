1b4a686239dfef02da2f27859e33346a00c1c268
Nemo Djuric <nemo.djuric@gmail.com>
2017-04-17 10:06:18 -0400

    Fixed styling offenses.

--- app/models/certification_cancellation.rb
+++ app/models/certification_cancellation.rb
@@ -1,4 +1,4 @@
 class CertificationCancellation < ActiveRecord::Base
   belongs_to :certification
-  validates_presence_of :cancellation_reason, :email, :certification_id
+  validates :cancellation_reason, :email, :certification_id, presence: true
 end
