aee1e0d2aafb96267296a0aab60f886264bb019f
Marian Lumba <mmlumba@gmail.com>
2017-04-17 09:45:23 -0400

    add future styleguide test

---
+++ spec/feature/styleguide_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+# Future Style Guide Test...uncomment after fixing accessibility errors
+# RSpec.feature "Style Guide", do
+#   scenario "anyone can visit style guide" do
+#     visit "/styleguide"
+#     expect(page).to have_content("Caseflow Commons")
+#   end
+# end
