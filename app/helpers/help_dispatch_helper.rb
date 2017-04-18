f0b2d5f755db356beb1296d97ac001c96c94defc
Sjones352 <sjones@tistatech.com>
2017-04-18 16:28:37 -0400

    changes to show file

--- app/helpers/help_dispatch_helper.rb
+++
@@ -1,2 +0,0 @@
-module HelpDispatchHelper
-end
--- spec/controllers/help_dispatch_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe HelpDispatchController, type: :controller do
-  describe 'GET #show' do
-    context "Welcome to the Dispatch Help Page!"
-  end
-end
