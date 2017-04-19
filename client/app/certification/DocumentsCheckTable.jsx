28b6a63edeb08d6fb1280a92247e35f7ee128bf4
Vovk <vhaisavovka@VACOLTVOVKA.local>
2017-04-19 10:01:06 -0400

    linters

--- client/app/certification/DocumentsCheckTable.jsx
+++ client/app/certification/DocumentsCheckTable.jsx
@@ -14,46 +14,50 @@ const DocumentsCheckTable = (props) => {
 
       <thead>
         <tr>
-          <th className="cf-txt-c"><span className="usa-sr-only">Status</span>Found in VBMS?</th>
+          <th className="cf-txt-c">
+              <span className="usa-sr-only">Status</span>Found in VBMS?
+          </th>
           <th>Document</th>
           <th>VACOLS date</th>
           <th>VBMS date</th>
         </tr>
       </thead>
 
-      <tbody>
-        <tr id="form9-match">
-          <td className="cf-txt-c"><span>    </span>{props.form9Match ? <FoundIcon/> : <NotFoundIcon/>}</td>
-          <td>Form 9</td>
-          <td>{props.form9Date}</td>
-          <td>{props.form9Date}</td>
-        </tr>
-        <tr id="nod-match">
-          <td className="cf-txt-c"><span>    </span>{props.nodMatch ? <FoundIcon/> : <NotFoundIcon/>}</td>
-          <td>NOD</td>
-          <td>{props.nodDate}</td>
-          <td>{props.nodDate}</td>
-        </tr>
-        <tr id="soc-match">
-          <td className="cf-txt-c">{props.socMatch ? <FoundIcon/> : <NotFoundIcon/>}</td>
-          <td>SOC</td>
-          <td>{props.socDate}</td>
-          <td>{props.socDate}</td>
-        </tr>
-        { typeof props.ssocDatesWithMatches !== 'undefined' &&
-          props.ssocDatesWithMatches.length > 0 &&
-          props.ssocDatesWithMatches.map((ssocDateWithMatch, index) =>
-              <tr id={`ssoc-${index + 1}-match`} key={index}>
-                <td className="cf-txt-c">{ssocDateWithMatch.match ? <FoundIcon/> : <NotFoundIcon/>}</td>
+    <tbody>
+    <tr id="form9-match">
+        <td className="cf-txt-c">{props.form9Match ? <FoundIcon/> : <NotFoundIcon/>}</td>
+        <td>Form 9</td>
+        <td>{props.form9Date}</td>
+        <td>{props.form9Date}</td>
+    </tr>
+    <tr id="nod-match">
+        <td className="cf-txt-c">{props.nodMatch ? <FoundIcon/> : <NotFoundIcon/>}</td>
+        <td>NOD</td>
+        <td>{props.nodDate}</td>
+        <td>{props.nodDate}</td>
+    </tr>
+    <tr id="soc-match">
+        <td className="cf-txt-c">{props.socMatch ? <FoundIcon/> : <NotFoundIcon/>}</td>
+        <td>SOC</td>
+        <td>{props.socDate}</td>
+        <td>{props.socDate}</td>
+    </tr>
+    { typeof props.ssocDatesWithMatches !== 'undefined' &&
+        props.ssocDatesWithMatches.length > 0 &&
+        props.ssocDatesWithMatches.map((ssocDateWithMatch, index) =>
+            <tr id={`ssoc-${index + 1}-match`} key={index}>
+                <td className="cf-txt-c">
+                {ssocDateWithMatch.match ? <FoundIcon/> : <NotFoundIcon/>}
+                </td>
                 <td>SSOC {index + 1}</td>
                 <td>{ssocDateWithMatch.date}</td>
                 <td>{ssocDateWithMatch.date}</td>
-              </tr>
-            )
-        }
-      </tbody>
+            </tr>
+        )
+    }
+    </tbody>
     </table>
-  </div>;
+    </div>;
 };
 
 export default DocumentsCheckTable;
