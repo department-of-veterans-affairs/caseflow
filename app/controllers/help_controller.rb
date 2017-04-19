69a9dccb1ed3a6335fd3dcd922229984d787fad9
Sjones352 <sjones@tistatech.com>
2017-04-18 22:17:47 -0400

    modified help controller

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,3 +1,11 @@
 class HelpController < ApplicationController
   skip_before_action :verify_authentication
+  
+   def logo_name
+    "Dispatch"
+  end
+
+  def logo_path
+    "establish_claims_path"
+  end
 end
