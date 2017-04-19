a42deca35766f00b35bb68bcca43c699ad104b4d
Mark Benjamin <mark@navahq.com>
2017-04-19 16:46:33 -0400

    Adding svg

--- client/app/components/RenderFunctions.jsx
+++ client/app/components/RenderFunctions.jsx
@@ -196,8 +196,15 @@ export const docCategoryIcon = (color) => () =>
 
 
 export const rightTriangle = function() {
-  return (
-    <i className="fa fa-caret-right" aria-hidden="true"></i>
+  return (<svg width="11px" height="18px" viewBox="0 0 21 35" version="1.1">
+      <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Artboard" fill-rule="nonzero" fill="#000000">
+              <g id="svg+xml" transform="translate(3.000000, 4.000000)">
+                  <path d="M16,14.2222222 C16,14.7037037 15.8240741,15.1203704 15.4722222,15.4722222 L3.02777778,27.9166667 C2.67592593,28.2685185 2.25925926,28.4444444 1.77777778,28.4444444 C1.2962963,28.4444444 0.87962963,28.2685185 0.527777778,27.9166667 C0.175925926,27.5648148 0,27.1481481 0,26.6666667 L0,1.77777778 C0,1.2962963 0.175925926,0.87962963 0.527777778,0.527777778 C0.87962963,0.175925926 1.2962963,0 1.77777778,0 C2.25925926,0 2.67592593,0.175925926 3.02777778,0.527777778 L15.4722222,12.9722222 C15.8240741,13.3240741 16,13.7407407 16,14.2222222 Z" id="Shape"></path>
+              </g>
+          </g>
+      </g>
+    </svg>
   );
 };
 /* eslint-enable max-len */
