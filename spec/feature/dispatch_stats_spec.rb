f95d40ab0f67ceffd259b092a7fa1578680b9a2d
Sharon Warner <sharon.warner56@gmail.com>
2017-04-20 17:39:59 -0400

    Code coverage fix

--- spec/feature/dispatch_stats_spec.rb
+++ spec/feature/dispatch_stats_spec.rb
@@ -26,7 +26,8 @@ RSpec.feature "Dispatch Stats Dashboard" do
 
   context ".show" do
     before do
-      Generators::EstablishClaim.create(started_at: 30.minutes.ago, completed_at: 15.minutes.ago, completion_status: 0)
+      establish_claim = Generators::EstablishClaim.create(started_at: 30.minutes.ago, completed_at: 15.minutes.ago, completion_status: 0)
+      establish_claim.appeal.outcoding_date = 17.days.ago
       Generators::EstablishClaim.create(started_at: 30.minutes.ago)
       Rails.cache.clear
       DispatchStats.calculate_all!
