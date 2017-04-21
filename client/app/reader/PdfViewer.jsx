9bee779c2f4a508ab8b45f71760135746b300b93
Nick Heiner <nth23@cornell.edu>
2017-04-21 11:25:49 -0400

    Migrating codebase to adhere to new rules

--- client/app/reader/PdfViewer.jsx
+++ client/app/reader/PdfViewer.jsx
@@ -89,9 +89,9 @@ export default class PdfViewer extends React.Component {
       let annotation = {
         class: 'Annotation',
         page: pageNumber,
-        'type': 'point',
-        'x': coordinates.xPosition,
-        'y': coordinates.yPosition
+        type: 'point',
+        x: coordinates.xPosition,
+        y: coordinates.yPosition
       };
 
       this.setState({
