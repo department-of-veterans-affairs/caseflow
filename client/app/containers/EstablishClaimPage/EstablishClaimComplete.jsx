4cdeb488eacfa311a9bf16d9f46ce4ac48e4b813
Sjones352 <sjones@tistatech.com>
2017-04-21 15:03:56 -0400

    moved emoji to bottom of page

--- client/app/containers/EstablishClaimPage/EstablishClaimComplete.jsx
+++ client/app/containers/EstablishClaimPage/EstablishClaimComplete.jsx
@@ -22,10 +22,6 @@ export default class EstablishClaimComplete extends React.Component {
     let casesAssigned, employeeCountInt,
       hasQuotaReached, quotaReachedMessage, totalCases;
 
-    quotaReachedMessage = <h2 className="cf-msg-screen-deck">
-      Way to go! 💻💪🇺🇸<br/>
-      You have completed all of the total cases assigned to you today.
-    </h2>;
 
     totalCases = totalCasesToComplete + totalCasesCompleted;
     employeeCountInt = parseInt(employeeCount, PARSE_INT_RADIX);
@@ -33,6 +29,8 @@ export default class EstablishClaimComplete extends React.Component {
     casesAssigned = employeeCountInt > 0 ?
       Math.ceil(totalCases / employeeCountInt) : 0;
     hasQuotaReached = (totalCasesCompleted >= casesAssigned) && (casesAssigned > 0);
+
+
 
     return <div>
       <EstablishClaimProgressBar
@@ -54,6 +52,12 @@ export default class EstablishClaimComplete extends React.Component {
         {checklist.map((listValue) => <li key={listValue}>
           <span className="cf-icon-success--bg"></span>{listValue}</li>)}
       </ul>
+
+       <h2 className="cf-msg-screen-deck">
+       <h4>Way to go!</h4>💪💻🇺🇸<br/>
+       <div>You have completed all of the total cases assigned to you today.</div>
+       </h2>
+
     </div>
     <EstablishClaimToolbar
       availableTasks={availableTasks}
