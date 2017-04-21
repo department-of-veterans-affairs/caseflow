5be432ee36838233702ba35a226c26dfd62a86e0
Nick Heiner <nth23@cornell.edu>
2017-04-21 10:30:50 -0400

    fix scss review comments and webpack

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413191800) do
+ActiveRecord::Schema.define(version: 20170414183652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,13 @@ ActiveRecord::Schema.define(version: 20170413191800) do
   end
 
   add_index "form8s", ["certification_id"], name: "index_form8s_on_certification_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "document_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "appeal_id",             null: false
