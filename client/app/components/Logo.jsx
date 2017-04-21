054dc300afa33696d044996fc3982e5a6989f068
Shade Lawrence Jenifer <shade.jenifer@pro-spheretek.com>
2017-04-21 18:41:55 -0400

    Added Logos to styleguide
    Connects #1350

---
+++ client/app/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+export default class Logo extends React.Component {
+  render() {
+    let app = this.props.app || "default";
+
+    let logoClass = "cf-logo-image cf-logo-image-" + app;
+
+    return <span className={logoClass}></span>;
+  }
+}
+
+Logo.propTypes = {
+  app: PropTypes.string
+};
---
+++ client/app/containers/StyleGuide/StyleGuideLogos.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// components
+import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
+import Logo from '../../components/Logo';
+
+export default class StyleLogos extends React.Component {
+  render() {
+    return <div>
+      <StyleGuideComponentTitle
+        title="Logos"
+        id="logos"
+        link="StyleGuideLogos.jsx"
+      />
+      <p>
+        The Caseflow logo, also known as the “Starwave”, is a minimal
+        guilloche pattern that is meant to communicate security, fluidity,
+        integration, dynamism, transparency, intuitiveness, and modernism.
+        Each Caseflow application has it’s own unique accent color but the
+        base color remains the same, gray-lighter.
+      </p>
+      <p>
+        When refering to the whole Caseflow system, you should use the
+        non-colored Caseflow logo.
+      </p>
+      <p className="logo-example"><Logo app="efolder" /><span>e-Folder Express</span></p>
+      <p className="logo-example"><Logo app="certification" /><span>Certification</span></p>
+      <p className="logo-example"><Logo app="dispatch" /><span>Dispatch</span></p>
+      <p className="logo-example"><Logo app="reader" /><span>Reader</span></p>
+      <p className="logo-example"><Logo app="feedback" /><span>Feedback</span></p>
+      <p className="logo-example"><Logo /><span>General Logo</span></p>
+    </div>;
+  }
+}
