b768611e43c721eeb48af0f5f5ab8f3de235ebd5
Teja N <teja.s.naraparaju@gmail.com>
2017-04-24 17:34:48 -0400

    Changing arrow function syntax

--- client/app/reader/actions.js
+++ client/app/reader/actions.js
@@ -2,73 +2,56 @@ import * as Constants from './constants';
 import _ from 'lodash';
 import ApiUtil from '../util/ApiUtil';
 
-export const onReceiveDocs = (documents) => {
-  return {
-    type: Constants.RECEIVE_DOCUMENTS,
-    payload: documents
-  };
-};
+export const onReceiveDocs = (documents) => ({
+  type: Constants.RECEIVE_DOCUMENTS,
+  payload: documents
+});
 
-export const onScrollToComment = (scrollToComment) => {
-  return {
-    type: Constants.SCROLL_TO_COMMENT,
-    payload: { scrollToComment }
-  };
-};
+export const onScrollToComment = (scrollToComment) => ({
+  type: Constants.SCROLL_TO_COMMENT,
+  payload: { scrollToComment }
+});
 
-export const handleSetLastRead = (docId) => {
-  return {
-    type: Constants.LAST_READ_DOCUMENT,
-    payload: {
-      docId
-    }
-  };
-};
+export const handleSetLastRead = (docId) => ({
+  type: Constants.LAST_READ_DOCUMENT,
+  payload: {
+    docId
+  }
+});
 
-export const newTagRequestSuccess = (docId, createdTags) => {
-  return {
-    type: Constants.REQUEST_NEW_TAG_CREATION_SUCCESS,
-    payload: {
-      docId,
-      createdTags
-    }
-  };
-};
+export const newTagRequestSuccess = (docId, createdTags) => ({
+  type: Constants.REQUEST_NEW_TAG_CREATION_SUCCESS,
+  payload: {
+    docId,
+    createdTags
+  }
+});
 
-export const newTagRequestFailed = () => {
-  return {
-    type: Constants.REQUEST_NEW_TAG_CREATION_FAILURE
-  };
-};
+export const newTagRequestFailed = () => ({
+  type: Constants.REQUEST_NEW_TAG_CREATION_FAILURE
+});
 
-export const selectCurrentPdf = (docId) => {
-  return {
-    type: Constants.SELECT_CURRENT_VIEWER_PDF,
-    payload: {
-      docId
-    }
-  };
-};
+export const selectCurrentPdf = (docId) => ({
+  type: Constants.SELECT_CURRENT_VIEWER_PDF,
+  payload: {
+    docId
+  }
+});
 
-export const removeTagRequestFailure = () => {
-  return {
-    type: Constants.REQUEST_REMOVE_TAG_FAILURE
-  };
-};
+export const removeTagRequestFailure = () => ({
+  type: Constants.REQUEST_REMOVE_TAG_FAILURE
+});
 
-export const removeTagRequestSuccess = (docId, tagId) => {
-  return {
-    type: Constants.REQUEST_REMOVE_TAG_SUCCESS,
-    payload: {
-      docId,
-      tagId
-    }
-  };
-};
+export const removeTagRequestSuccess = (docId, tagId) => ({
+  type: Constants.REQUEST_REMOVE_TAG_SUCCESS,
+  payload: {
+    docId,
+    tagId
+  }
+});
 
-
-export const removeTag = (doc, tagId) => {
-  return (dispatch) => {
+export const removeTag = (doc, tagId) => (
+  (dispatch) => {
     dispatch({ type: Constants.REQUEST_REMOVE_TAG });
     ApiUtil.delete(`/document/${doc.id}/tag/${tagId}`).
       then(() => {
@@ -76,11 +59,11 @@ export const removeTag = (doc, tagId) => {
       }, () => {
         dispatch(removeTagRequestFailure());
       });
-  };
-};
+  }
+);
 
-export const addNewTag = (doc, tags) => {
-  return (dispatch) => {
+export const addNewTag = (doc, tags) => (
+  (dispatch) => {
     const currentTags = doc.tags;
 
     // gets the newly added tags
@@ -98,5 +81,5 @@ export const addNewTag = (doc, tags) => {
           dispatch(newTagRequestFailed());
         });
     }
-  };
-};
+  }
+);
