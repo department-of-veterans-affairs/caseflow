87d087c0b14994f7c6472511600722a82e99c990
Teja N <teja.s.naraparaju@gmail.com>
2017-04-24 15:11:25 -0400

    Making a fix for empty node showing "tag already exists" message

--- client/app/components/SearchableDropdown.jsx
+++ client/app/components/SearchableDropdown.jsx
@@ -83,6 +83,10 @@ class SearchableDropdown extends Component {
       };
     }
 
+    if (options && !options.length) {
+      addCreatableOptions.noResultsText = '';
+    }
+
     return <div className="cf-form-dropdown">
       <label className="question-label" htmlFor={name}>
         {label || name} {required && <span className="cf-required">Required</span>}
