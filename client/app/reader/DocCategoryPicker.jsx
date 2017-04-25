ff59c9aec822f1144c609646a6453fead50dd990
Nick Heiner <NickHeiner@users.noreply.github.com>
2017-04-25 15:10:53 -0400

    Dropdown filter component (#1658)
    * Add a new Dropdown Filter component
    * Refactor existing document category component to be shareable 
    * Add redux dev tools and remove redux-logger

---
+++ client/app/reader/DocCategoryPicker.jsx
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+import * as Constants from './constants';
+import Checkbox from '../components/Checkbox';
+
+const CategorySelector = (props) => {
+  const { category, categoryName, handleCategoryToggle, categoryToggleStates } = props;
+  const toggleState = categoryToggleStates[categoryName] || false;
+  const Svg = category.svg;
+  const label = <div className="cf-category-selector">
+      <Svg />
+      <span className="cf-category-name">{category.humanName}</span>
+    </div>;
+
+  const handleChange = (checked) => handleCategoryToggle(categoryName, checked);
+
+  return <Checkbox name={categoryName} onChange={handleChange}
+      label={label} value={toggleState} />;
+};
+
+CategorySelector.propTypes = {
+  category: PropTypes.shape({
+    humanName: PropTypes.string.isRequired,
+    svg: PropTypes.func.isRequired
+  }).isRequired,
+  categoryToggleStates: PropTypes.object,
+  categoryName: PropTypes.string.isRequired
+};
+
+const DocCategoryPicker = ({ categoryToggleStates, handleCategoryToggle }) => {
+  return <ul className="cf-document-category-picker">
+    {
+      _(Constants.documentCategories).
+        toPairs().
+        // eslint-disable-next-line no-unused-vars
+        sortBy(([name, category]) => category.renderOrder).
+        map(
+          ([categoryName, category]) => <li key={categoryName}>
+            <CategorySelector category={category}
+              handleCategoryToggle={handleCategoryToggle}
+              categoryName={categoryName} categoryToggleStates={categoryToggleStates} />
+          </li>
+        ).
+        value()
+    }
+  </ul>;
+};
+
+DocCategoryPicker.propTypes = {
+  handleCategoryToggle: PropTypes.func.isRequired,
+  categoryToggleStates: PropTypes.object
+};
+
+export default DocCategoryPicker;
