7ef2ad7495eb29d4e88033d258dde7337689a6dc
Teja N <teja.s.naraparaju@gmail.com>
2017-04-26 15:40:11 -0400

    Removing inline styling

--- client/app/components/reader/TagTableColumn.jsx
+++ client/app/components/reader/TagTableColumn.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-const MAX_WIDTH = 225;
-
 export default class TagTableColumn extends React.Component {
   render() {
     const {
       doc
     } = this.props;
     const tags = doc.tags;
-    let tagsStyle = { maxWidth: MAX_WIDTH };
 
-    return <div className="document-list-issue-tags" style={tagsStyle}>
+    return <div className="document-list-issue-tags">
       {tags && tags.map((tag) => {
-        return <div className="document-list-issue-tag"
-            style={tagsStyle}>
+        return <div className="document-list-issue-tag">
             {`${tag.text}`}
           </div>;
       })}
