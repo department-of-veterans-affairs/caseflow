3540ea732dc34c730fffd5e9e8327439f652a31c
Teja N <teja.s.naraparaju@gmail.com>
2017-04-26 15:44:22 -0400

    Refactor TagTableColumn component

--- client/app/components/reader/TagTableColumn.jsx
+++ client/app/components/reader/TagTableColumn.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-export default class TagTableColumn extends React.Component {
-  render() {
-    const {
-      doc
-    } = this.props;
-    const tags = doc.tags;
+const TagTableColumn = ({ doc }) => (
+  <div className="document-list-issue-tags">
+    {doc.tags && doc.tags.map((tag) => {
+      return <div className="document-list-issue-tag">
+          {tag.text}
+        </div>;
+    })}
+  </div>
+);
 
-    return <div className="document-list-issue-tags">
-      {tags && tags.map((tag) => {
-        return <div className="document-list-issue-tag">
-            {`${tag.text}`}
-          </div>;
-      })}
-    </div>;
-  }
-}
+export default TagTableColumn;
