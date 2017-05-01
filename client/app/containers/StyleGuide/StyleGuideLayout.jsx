361d95e954e3c725bb90d4ebde5c3cb73b8b6312
Sjones352 <sjones@tistatech.com>
2017-05-01 13:06:22 -0400

    fixed indent error

--- client/app/containers/StyleGuide/StyleGuideLayout.jsx
+++ client/app/containers/StyleGuide/StyleGuideLayout.jsx
@@ -2,42 +2,41 @@ import React from 'react';
 import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
 import StyleGuideAction from './StyleGuideAction';
 
-
 let StyleGuideLayout = () => {
 
   return (
-    <div>
-       <StyleGuideComponentTitle
+  <div>
+    <StyleGuideComponentTitle
         title="Actions"
         id="layout"
         link="StyleGuideLayout.jsx"
         isSubsection={true}
-      />
-      <p>
-        For most task-based pages, Primary and Secondary Actions sit under the App Canvas.
-        The number of actions per page should be limited intentionally.
-        These tasks should relate specifically to the user’s goal for the page they are on.
-      </p>
+    />
+    <p>
+      For most task-based pages, Primary and Secondary Actions sit under the App Canvas.
+      The number of actions per page should be limited intentionally.
+      These tasks should relate specifically to the user’s goal for the page they are on.
+    </p>
 
-      <p>
-        The actions at the bottom of the page are arranged such as the primary
-        task (the task that takes the user forward) is on the bottom right of the App Canvas.
-        The label of this action usually hints at the title of the next page.
-        Escape actions are placed to the left of the primary action.
-        On the bottom left, of the App Canvas, there will be a back link,
-        preferably with a description of where the user will go to
-        or a link to the main page after a user has completed a task.
-        These are actions that allow the user to move back a step
-        or completely leave the task they’re working on.
-      </p>
-       <p>
-        The consistent layout and arrangement of these actions
-        reinforces the users mental model as the use Caseflow.
-        You should avoid placing these actions in other parts
-        of the page without good reason.
-      </p>
-      <StyleGuideAction />
-    </div>
+    <p>
+      The actions at the bottom of the page are arranged such as the primary
+      task (the task that takes the user forward) is on the bottom right of the App Canvas.
+      The label of this action usually hints at the title of the next page.
+      Escape actions are placed to the left of the primary action.
+      On the bottom left, of the App Canvas, there will be a back link,
+      preferably with a description of where the user will go to
+      or a link to the main page after a user has completed a task.
+      These are actions that allow the user to move back a step
+      or completely leave the task they’re working on.
+    </p>
+    <p>
+      The consistent layout and arrangement of these actions
+      reinforces the users mental model as the use Caseflow.
+      You should avoid placing these actions in other parts
+      of the page without good reason.
+    </p>
+    <StyleGuideAction />
+  </div>
 
   );
 };
