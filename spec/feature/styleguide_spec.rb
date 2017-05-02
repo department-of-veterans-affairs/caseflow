3ac056095b7b74af829c25d5ddc9514eb0bcdc5b
Marian Lumba <mmlumba@gmail.com>
2017-05-02 10:34:09 -0400

    adding test for style guide (#1794)
    * omg make style guide accessible again yay
    
    * style changes

--- spec/feature/styleguide_spec.rb
+++ spec/feature/styleguide_spec.rb
@@ -1,11 +1,8 @@
 require "rails_helper"
 
-# Future Style Guide Test...remove skip after fixing accessibility errors
-# :nocov:
 RSpec.feature "Style Guide" do
-  skip "renders and is accessible" do
+  scenario "renders and is accessible" do
     visit "/styleguide"
     expect(page).to have_content("Caseflow Commons")
   end
 end
-# :nocov:
