d68c4705b623ac567f4c76bf26e81eb01abb0453
Sjones352 <sjones@tistatech.com>
2017-05-02 14:42:06 -0400

    Fixed Branch ahead of master schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170427201655) do
-=======
 ActiveRecord::Schema.define(version: 20170502144719) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
