dd6aa3af1012ae478b0fd5bd6d826acf554a612c
Teja N <teja.s.naraparaju@gmail.com>
2017-05-02 12:37:20 -0400

    Making changes to how reader shows receipt dates

--- client/test/karma/util/DateUtil-test.js
+++ client/test/karma/util/DateUtil-test.js
@@ -1,10 +1,16 @@
 import { expect } from 'chai';
-import { formatDate } from '../../../app/util/DateUtil';
+import { formatDate, formatDateStr } from '../../../app/util/DateUtil';
 
 describe('DateUtil', () => {
   context('.formatDate', () => {
     it('returns a date formatted mm/dd/yyyy', () => {
       expect(formatDate('1/2/2017')).to.eq('01/02/2017');
+    });
+  });
+
+  context('.formatDate', () => {
+    it('returns a date formatted mm/dd/yyyy', () => {
+      expect(formatDateStr('2017-04-24')).to.eq('04/24/2017');
     });
   });
 });
