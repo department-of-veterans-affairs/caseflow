ade11ef7d522660356daa4a688bef65a7c1a12f0
Shade Lawrence Jenifer <shade.jenifer@pro-spheretek.com>
2017-05-03 22:45:43 -0400

    StyleGuideAction component DOM validates
    Connects #1839

--- client/app/containers/StyleGuide/StyleGuideAction.jsx
+++ client/app/containers/StyleGuide/StyleGuideAction.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
 import Button from '../../components/Button';
-import InlineForm from '../../components/InlineForm';
-
 
 let StyleGuideAction = () => {
-
-  return (
-    <div>
-     <div className="cf-app cf-push-row cf-sg-layout cf-app-segment cf-app-segment--alt"></div>
-     <p>
-      <div className="cf-app-segment" id="establish-claim-buttons">
-        <div className="usa-width-one-half">
-          <InlineForm>
-            <span><Button
-               name="Back to Preview"
-               classNames={['cf-btn-link']} />
-            </span>
-          </InlineForm>
-        </div>
-        <div className ="cf-push-right">
-          <Button
-            name="Cancel"
-           classNames={['cf-btn-link']}/>
-          <Button
-            name="Submit End Product"
-          />
-        </div>
-      </div>
-     </p>
+  return <div>
+    <div className="cf-app cf-push-row cf-sg-layout cf-app-segment cf-app-segment--alt">
     </div>
-
-  );
+    <div className="cf-app-segment" id="establish-claim-buttons">
+      <div className="cf-push-left">
+        <Button
+          name="Back To Preivew"
+          classNames={['cf-btn-link']}
+        />
+      </div>
+      <div className="cf-push-right">
+        <Button
+          name="Cancel"
+          classNames={['cf-btn-link', 'cf-adjacent-buttons']}
+        />
+        <Button
+          name="Submit End Product"
+          classNames={['usa-button-primary']}
+        />
+      </div>
+    </div>
+  </div>;
 };
 
 export default StyleGuideAction;
