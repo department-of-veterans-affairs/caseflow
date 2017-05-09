ce9ac11bf17f774b29257d5a807fb73f113b701b
Sjones352 <sjones@tistatech.com>
2017-05-09 11:05:50 -0400

    added caseflow log to layout

--- client/app/containers/StyleGuide/StyleGuideFooter.jsx
+++ client/app/containers/StyleGuide/StyleGuideFooter.jsx
@@ -2,11 +2,45 @@ import React from 'react';
 import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
 import StyleGuideAction from './StyleGuideAction';
 import Button from '../../components/Button';
+import DropdownMenu from '../../components/DropdownMenu';
 
+export default class StyleGuideFooter extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      menu: false
+    };
+  }
 
+  handleMenuClick = () => {
+    this.setState((prevState) => ({
+      menu: !prevState.menu
+    }));
+  };
 
+  options = () => {
+    return [
+      {
+        title: 'Help',
+        link: '#footer'
+      },
+      {
+        title: 'Send feedback',
+        link: '#footer'
+      },
+      {
+        title: 'Sign out',
+        link: '#footer'
+      }
+    ];
+  }
 
-let StyleGuideFooter = () => {
+  render() {
+    let {
+      name
+    } = this.props;
+
+    name = 'Dispatch';
   return <div>
       <StyleGuideComponentTitle
         title="Footer"
@@ -21,6 +55,26 @@ let StyleGuideFooter = () => {
      Conveniently, if a developer hover’s over the word
     “Built” they’ll see a tooltip showing the build date 
     of the app that they are viewing.</p>
+
+     <div>
+        <nav className="cf-nav">
+          <a href="#" id="cf-logo-link">
+            <h1 className="cf-logo"><span className="cf-logo-image cf-logo-image-dispatch">
+            </span>Caseflow</h1>
+          </a>
+          <h2 id="page-title" className="cf-application-title">&nbsp; &nbsp; {name}</h2>
+          <div className="cf-dropdown cf-nav-dropdown">
+            <DropdownMenu
+              options={this.options()}
+              onClick={this.handleMenuClick}
+              onBlur={this.handleMenuClick}
+              label="KAVI HARSHAWAT"
+              menu={this.state.menu}
+              />
+          </div>
+      </nav>
+    </div>
+
     <div className="cf-app-segment cf-app-segment--alt"></div>
     <div className="cf-app-segment cf-segment-margin" id="establish-claim-buttons">
       <div className="cf-push-left">
@@ -44,16 +98,20 @@ let StyleGuideFooter = () => {
      <footer className="cf-txt-c usa-grid cf-app-footer">
       <div>
         <div className="cf-push-left">
-          <span>Built</span> with <abbr title="love">♡</abbr> by the
-          <a href="https://www.usds.gov/">Digital Service at the <abbr title="Department of Veterans Affairs">VA</abbr></a>
+          <span>Built</span> with <abbr title="love">♡</abbr>by the
+          <a href="https://www.usds.gov/"> 
+           Digital Service at the
+           <abbr title="Department of Veterans Affairs">VA</abbr>
+          </a>
         </div>
         <div className="cf-push-right">
-          <a target="_blank" href="https://dsva-appeals-feedback-demo-1748368704.us-gov-west-1.elb.amazonaws.com/?redirect=http%3A%2F%2Flocalhost%3A3000%2F&amp;subject=Caseflow" onclick="ga('send', 'event', { eventCategory: 'Menu', eventAction: 'ClickFeedback', eventLabel: 'Feedback', eventValue: 1});">
-            Send feedback
+          <a target="_blank" 
+           href="#feedback">
+           Send feedback
           </a>
         </div>
       </div>
     </footer>
-  </div>;
-};
-export default StyleGuideFooter;
+   </div>;
+  }
+}
