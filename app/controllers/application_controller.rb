d63351beaee81df5ad2c1addd6dac024778712bd
Alex Prokop <alex.prokop@gmail.com>
2017-05-08 20:21:01 -0400

    restricts prod system admin to CSFLOW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -159,7 +159,7 @@ class ApplicationController < ActionController::Base
   end
 
   def verify_system_admin
-    verify_authorized_roles("System Admin")
+    redirect_to "/unauthorized" if !current_user.admin?
   end
 
   def on_vbms_error
