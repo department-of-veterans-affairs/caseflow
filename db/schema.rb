9440c4f8122b48e0df81ead39f55ba8486f016f7
Teja N <teja.s.naraparaju@gmail.com>
2017-05-10 09:29:51 -0400

    New schema created after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508161151) do
+ActiveRecord::Schema.define(version: 20170509155046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,7 +160,6 @@ ActiveRecord::Schema.define(version: 20170508161151) do
     t.string   "agent_accredited"
     t.string   "form_646_of_record"
     t.string   "form_646_not_of_record_explanation"
-    t.string   "hearing_preference"
     t.string   "hearing_requested"
     t.string   "hearing_held"
     t.string   "hearing_transcript_on_file"
@@ -168,8 +167,6 @@ ActiveRecord::Schema.define(version: 20170508161151) do
     t.string   "contested_claims_procedures_applicable"
     t.string   "contested_claims_requirements_followed"
     t.date     "soc_date"
-    t.date     "nod_date"
-    t.date     "form9_date"
     t.string   "ssoc_required"
     t.text     "record_other_explanation"
     t.text     "remarks"
@@ -208,6 +205,9 @@ ActiveRecord::Schema.define(version: 20170508161151) do
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
     t.string   "certifying_official_title_specify_other"
+    t.string   "hearing_preference"
+    t.date     "nod_date"
+    t.date     "form9_date"
   end
 
   add_index "form8s", ["certification_id"], name: "index_form8s_on_certification_id", using: :btree
