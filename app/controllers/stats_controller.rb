3dd047050e9667d4e68ddc86ed1d79fadf135bff
Alex Prokop <alex.prokop@gmail.com>
2017-05-09 20:08:43 -0400

    Locks down system admin (#1932)
    * restricts prod system admin to CSFLOW
    
    * rubocop
    
    * weaken restriction

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,11 @@
 class StatsController < ApplicationController
   before_action :verify_authentication
-  before_action :verify_system_admin
+  before_action :verify_access
+
+  def verify_access
+    # All folks with system admin role
+    # should be able to see the stats page,
+    # even if they don't return true for `user.admin?`
+    verify_authorized_roles("System Admin")
+  end
 end
