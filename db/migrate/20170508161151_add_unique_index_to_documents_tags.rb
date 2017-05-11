c8e07c89cb7d540ee128348d1573e2e0fa4501c4
Teja N <teja.s.naraparaju@gmail.com>
2017-05-11 12:02:25 -0400

    Making changes to migrations

--- db/migrate/20170508161151_add_unique_index_to_documents_tags.rb
+++ db/migrate/20170508161151_add_unique_index_to_documents_tags.rb
@@ -1,7 +1,7 @@
 class AddUniqueIndexToDocumentsTags < ActiveRecord::Migration
+  disable_ddl_transaction!
+  
   def change
-    add_index(:documents_tags, [:document_id, :tag_id], unique: true)
-    remove_index :documents_tags, [:document_id]
-    remove_index :documents_tags, [:tag_id]
+    add_index(:documents_tags, [:document_id, :tag_id], unique: true, algorithm: :concurrently)
   end
 end
