44974cc2ccf92f4e01668e029c4f33de8a13bbf8
Nick Heiner <nth23@cornell.edu>
2017-05-15 13:24:26 -0400

    Add middleware for perf logging in redux

--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import perflogger from 'redux-perf-middleware';
 import thunk from 'redux-thunk';
 import DecisionReviewer from './DecisionReviewer';
 import readerReducer from './reducer';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(readerReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(readerReducer, composeEnhancers(applyMiddleware(thunk, perflogger)));
 
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
