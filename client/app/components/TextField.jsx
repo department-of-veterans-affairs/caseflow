40ea5648a1a813eb8e9ecac4730c951acb492312
Shane Russell <shane1337@gmail.com>
2017-05-17 12:19:31 -0400

    Tweaks to style (#2011)
    * Tweaks to style
    
    connects #1690
    
    * Also tweak text field margin
    
    * lint
    
    * lock tweaks

--- client/app/components/TextField.jsx
+++ client/app/components/TextField.jsx
@@ -73,7 +73,10 @@ TextField.propTypes = {
   errorMessage: PropTypes.string,
   className: PropTypes.arrayOf(PropTypes.string),
   invisible: PropTypes.bool,
-  label: PropTypes.string,
+  label: PropTypes.oneOfType(
+    PropTypes.string,
+    PropTypes.bool,
+  ),
   name: PropTypes.string.isRequired,
   onChange(props) {
     if (!props.readOnly) {
