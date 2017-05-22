58b7c47220149fc2958fe152a4a9d65d16b36462
Nick Heiner <nth23@cornell.edu>
2017-05-22 18:17:09 -0400

    remove comment

--- db/migrate/20170522214928_add_tag_text_index.rb
+++ db/migrate/20170522214928_add_tag_text_index.rb
@@ -5,23 +5,3 @@ class AddTagTextIndex < ActiveRecord::Migration
     add_index(:tags, [:text], unique: true, algorithm: :concurrently)
   end
 end
-
-# select
-#     t.relname as table_name,
-#     i.relname as index_name,
-#     a.attname as column_name
-# from
-#     pg_class t,
-#     pg_class i,
-#     pg_index ix,
-#     pg_attribute a
-# where
-#     t.oid = ix.indrelid
-#     and i.oid = ix.indexrelid
-#     and a.attrelid = t.oid
-#     and a.attnum = ANY(ix.indkey)
-#     and t.relkind = 'r'
-#     and t.relname like 'tag%'
-# order by
-#     t.relname,
-#     i.relname;
