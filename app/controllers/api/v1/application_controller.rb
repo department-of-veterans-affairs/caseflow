36cc8287010082da209ecfe014868b486ed122ec
Shane Russell <shane.russell.va@gmail.com>
2017-05-22 08:12:11 -0400

    Security stuff

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   force_ssl if: :ssl_enabled?
   before_action :strict_transport_security
 
   before_action :setup_fakes
 
   private
+
+  def not_found
+    render json: { status: "not found" }, status: 404
+  end
 
   def ssl_enabled?
     Rails.env.production?
