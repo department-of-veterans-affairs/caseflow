298ddd7bbb6fdb1af1d211668899eabb484eb40f
Teja N <teja.s.naraparaju@gmail.com>
2017-05-25 14:20:39 -0400

    Removing unused var

--- client/app/reader/PdfListView.jsx
+++ client/app/reader/PdfListView.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import DocumentListHeader from '../components/reader/DocumentListHeader';
 import _ from 'lodash';
 import { clearSearch } from './actions';
