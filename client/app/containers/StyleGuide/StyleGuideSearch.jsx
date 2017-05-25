70807538c76fffd1898533a2638610326e540cce
Sjones352 <sjones@tistatech.com>
2017-05-24 21:06:14 -0400

    made a new styleguide search file

---
+++ client/app/containers/StyleGuide/StyleGuideSearch.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import SearchBar from '../../components/SearchBar';
+import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
+
+class StyleGuideSearch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+  
+    }
+
+  }
+
+  render() {
+   
+    const options = [{ value: 'unitedstates',
+      label: 'United States' },
+    { value: 'germany',
+      label: 'Germany' },
+    { value: 'france',
+      label: 'France' },
+    { value: 'russia',
+      label: 'Russia' }];
+
+    return (
+      <div>
+        <StyleGuideComponentTitle
+          title="Search"
+          id="search"
+          link="StyleGuideSearch.jsx"
+        />
+
+        <h3>Search Small</h3>
+        <SearchBar
+         label= "Really"
+        />
+      </div>
+    );
+  }
+}
+
+export default StyleGuideSearch;
