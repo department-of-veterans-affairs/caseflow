fd533dc3a977a4fb82577b743b810033014da9b3
Sharon Warner <sharon.warner56@gmail.com>
2017-05-25 15:13:49 -0400

    Migration for poa information

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522214928) do
+ActiveRecord::Schema.define(version: 20170525191011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,9 +96,6 @@ ActiveRecord::Schema.define(version: 20170522214928) do
     t.string   "bgs_representative_name"
     t.string   "vacols_representative_type"
     t.string   "vacols_representative_name"
-    t.boolean  "poa_matches"
-    t.boolean  "poa_correct_in_vacols"
-    t.boolean  "poa_correct_in_bgs"
     t.string   "representative_type"
     t.string   "representative_name"
     t.boolean  "hearing_change_doc_found_in_vbms"
@@ -110,6 +107,9 @@ ActiveRecord::Schema.define(version: 20170522214928) do
     t.string   "certifying_official_name"
     t.string   "certifying_official_title"
     t.string   "certification_date"
+    t.boolean  "poa_matches"
+    t.boolean  "poa_correct_in_vacols"
+    t.boolean  "poa_correct_in_bgs"
   end
 
   add_index "certifications", ["user_id"], name: "index_certifications_on_user_id", using: :btree
