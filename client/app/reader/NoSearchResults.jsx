493436ea183f155f7977dc0767072ef05b0f22e5
Teja N <teja.s.naraparaju@gmail.com>
2017-05-25 12:47:02 -0400

    Making a pure component and removing a test

--- client/app/reader/NoSearchResults.jsx
+++ client/app/reader/NoSearchResults.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import StatusMessage from '../components/StatusMessage';
 
-const NoSearchResults = (props) => (
-  <div className="section--no-search-results">
-    <StatusMessage
-      title="Search results not found">
-      Sorry! We couldn't find anything for "{props.searchQuery}."<br />
-      Please search again or <a href="#" onClick={props.clearSearch}>go back to the document list.</a>
-    </StatusMessage>
-  </div>
-);
+class NoSearchResults extends PureComponent {
+  render() {
+    return <div className="section--no-search-results">
+      <StatusMessage
+        title="Search results not found">
+        Sorry! We couldn't find anything for "{this.props.searchQuery}."<br />
+        Please search again or <a href="#" onClick={this.props.clearSearch}>go back to the document list.</a>
+      </StatusMessage>
+    </div>;
+  }
+}
 
 NoSearchResults.propTypes = {
   clearSearch: PropTypes.func.isRequired,
