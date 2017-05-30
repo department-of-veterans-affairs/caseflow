b3267d689244a91eb5600772a8e2b243d7cb40f9
Sjones352 <sjones@tistatech.com>
2017-05-29 23:23:49 -0400

    added context to styleguide search

--- client/app/containers/StyleGuide/StyleGuideSearch.jsx
+++ client/app/containers/StyleGuide/StyleGuideSearch.jsx
@@ -10,16 +10,22 @@ class StyleGuideSearch extends Component {
 
     this.state = {
       loading: {
-        search: false
+        search: false,
+        medium: false
       }
+
    };
 
 }
- handleMenuClick = () => {
+   handleMenuClick = () => {
     this.setState((prevState) => ({
-      search: !prevState.search,
-    }));
-  };
+     search: !prevState.search,
+     medium: !prevState.medium,
+   }));
+ };
+
+
+
 
   render() {
     return (
@@ -30,6 +36,16 @@ class StyleGuideSearch extends Component {
           link="StyleGuideSearch.jsx"
         />
         <p>
+        Search bars are a are block that allows users to search for specific content 
+        if they know what search terms to use or can’t find desired content in the main navigation. 
+        In Caseflow they serve as a vital UI component that allows users to 
+        find information necessary in completing their tasks.</p>
+
+        <p>
+        Based on the app there are three sizes: big, medium, and small.
+        There is also a unique Caseflow search behavior that displays a spinning logo to indicate load times.
+        </p> 
+
         <h3>Search Big</h3>
         <div className="cf-sg-searchbar-example">
             <SearchBar
@@ -47,9 +63,7 @@ class StyleGuideSearch extends Component {
               disabled={!this.state.search}
             />
         </div>
-        </p>
         
-        <p>
         <h3>Search Medium</h3>
         <div className="cf-sg-searchbar-example">
             <SearchBar
@@ -57,12 +71,11 @@ class StyleGuideSearch extends Component {
               title="SEARCH MEDIUM"
               classNames="usa-search usa-search-medium"
               onClick={this.handleMenuClick}
-              loading={this.state.search}
+              loading={this.state.medium}
             />
         </div>
-        </p>
-       
         
+        <br />
         <h3>Search Small</h3>
         <div className="cf-sg-searchbar-example">
             <SearchBar
@@ -73,6 +86,7 @@ class StyleGuideSearch extends Component {
               loading={this.state.search}
             />
         </div>
+        
       </div>
     );
   }
