e54e54c1e5ab4ed2df337826ff3835d575c16a6b
Alex Prokop <alex.prokop@gmail.com>
2017-05-31 10:04:04 -0400

    undo stub error

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -201,8 +201,7 @@ class Fakes::BGSService
 
   # TODO: add more test cases
   def find_address_by_participant_id(participant_id)
-    fail
-    # fail if participant_id == ID_TO_RAISE_ERROR
+    fail if participant_id == ID_TO_RAISE_ERROR
 
     address = (self.class.address_records || {})[participant_id]
     address ||= default_address
