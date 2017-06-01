9a71bcbf075c60b935497cd6810239f55b34bfad
Nemo Djuric <nemo.djuric@gmail.com>
2017-06-01 11:02:09 -0400

    Changed parameter in DocumentsMatchingBox

--- client/app/certification/DocumentsMatchingBox.jsx
+++ client/app/certification/DocumentsMatchingBox.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 // TODO: refactor to use shared components if helpful
-const DocumentsMatchingBox = (data) => {
+const DocumentsMatchingBox = ({ areDatesExactlyMatching }) => {
 
   return <div className="usa-alert cf-app-segment usa-alert-success">
             <div className="usa-alert-body">
               <h3 className="usa-alert-heading">All documents found with matching VBMS and VACOLS dates.</h3>
-              {!data.areDatesExactlyMatching && <p className="usa-alert-text">
+              {!areDatesExactlyMatching && <p className="usa-alert-text">
                 SOC and SSOC dates in VBMS can be up to 4 days before the VACOLS date to be considered matching.
                 </p>}
             </div>
