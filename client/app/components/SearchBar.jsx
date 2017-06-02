cab5a6e393c407a36e215c64fe6cd5779130cda6
Marian Lumba <mmlumba@gmail.com>
2017-06-02 12:08:16 -0400

    adding unique id for search button

--- client/app/components/SearchBar.jsx
+++ client/app/components/SearchBar.jsx
@@ -48,7 +48,7 @@ export default class SearchBar extends React.Component {
           onClick={onClearSearch}>
           {closeIcon()}
         </Button>}
-      <Button name="search" onClick={onClick} type="submit" loading={loading}>
+      <Button name={"search-"+id} onClick={onClick} type="submit" loading={loading}>
         <span className={getSrOnlyClassName()}>Search</span>
       </Button>
     </span>;
