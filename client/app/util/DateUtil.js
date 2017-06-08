0f24f430d7a776ff14399134288f8ffe7eb1b7cb
Nemo Djuric <nemo.djuric@gmail.com>
2017-06-08 11:37:05 -0400

    Used formatDateStr function instead.

--- client/app/util/DateUtil.js
+++ client/app/util/DateUtil.js
@@ -8,7 +8,7 @@ export const dateFormatString = 'MM/DD/YYYY';
 export const formatDate = function(dateString) {
   let date = new Date(dateString);
   let month = StringUtil.leftPad(date.getMonth() + ZERO_INDEX_MONTH_OFFSET, 2, '0');
-  let day = StringUtil.leftPad(date.getUTCDate(), 2, '0');
+  let day = StringUtil.leftPad(date.getDate(), 2, '0');
   let year = date.getFullYear();
 
   return `${month}/${day}/${year}`;
