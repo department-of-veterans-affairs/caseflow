57af03d81388560585083432ec53f3c397c0feb7
Mark Benjamin <mark@navahq.com>
2017-06-13 14:53:35 -0400

    nocov

--- app/services/regional_office_repository.rb
+++ app/services/regional_office_repository.rb
@@ -1,4 +1,5 @@
 class RegionalOfficeRepository
+  # :nocov:
   def self.find(ro_id, _args = {})
     office_record = MetricsService.record("VACOLS: loaded RO #{ro_id}",
                                           service: :vacols,
@@ -18,4 +19,5 @@ class RegionalOfficeRepository
     end
     ros.map { |v| RegionalOffice.from_record(office_record: v) }
   end
+  # :nocov:
 end
