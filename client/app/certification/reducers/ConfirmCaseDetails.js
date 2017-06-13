229cd433a34cc16510911d35d0c69cb7c4123dde
Nemo Djuric <nemo.djuric@gmail.com>
2017-06-13 10:57:21 -0400

    Added clearing errors on component change.

--- client/app/certification/reducers/ConfirmCaseDetails.js
+++ client/app/certification/reducers/ConfirmCaseDetails.js
@@ -1,8 +1,22 @@
 import * as Constants from '../constants/constants';
 
+/* eslint max-statements: ["error", 14]*/
+export const updateErroredFields = (fieldName, state) => {
+  const index = state.erroredFields.indexOf(fieldName);
+  const newErroredFields = state.erroredFields;
+
+  if (index !== -1) {
+    newErroredFields.splice(index, 1);
+  }
+
+  return newErroredFields;
+};
+
 export const changeRepresentativeType = (state, action) => {
   const update = {};
+  const updatedErroredFields = updateErroredFields('representativeType', state);
 
+  update.erroredFields = updatedErroredFields;
   update.representativeType = action.payload.representativeType;
   // if we changed the type to something other than "Other",
   // erase the other representative type if it was specified.
@@ -18,14 +32,26 @@ export const changeRepresentativeType = (state, action) => {
 };
 
 export const changeRepresentativeName = (state, action) => {
+  let updatedErroredFields = [];
+
+  if (state.erroredFields.indexOf('representativeName') === -1) {
+    updatedErroredFields = updateErroredFields('representativeNameLength', state);
+  } else {
+    updatedErroredFields = updateErroredFields('representativeName', state);
+  }
+
   return Object.assign({}, state, {
-    representativeName: action.payload.representativeName
+    representativeName: action.payload.representativeName,
+    erroredFields: updatedErroredFields
   });
 };
 
 export const changeOrganizationName = (state, action) => {
+  const updatedErroredFields = updateErroredFields('organizationName', state);
+
   return Object.assign({}, state, {
-    organizationName: action.payload.organizationName
+    organizationName: action.payload.organizationName,
+    erroredFields: updatedErroredFields
   });
 };
 
@@ -36,23 +62,29 @@ export const changeOtherRepresentativeType = (state, action) => {
 };
 
 export const changePoaMatches = (state, action) => {
+  const updatedErroredFields = updateErroredFields('poaMatches', state);
+
   const update = {
     poaMatches: action.payload.poaMatches,
     poaCorrectLocation: null,
     organizationName: null,
     representativeName: null,
-    representativeType: null
+    representativeType: null,
+    erroredFields: updatedErroredFields
   };
 
   return Object.assign({}, state, update);
 };
 
 export const changePoaCorrectLocation = (state, action) => {
+  const updatedErroredFields = updateErroredFields('poaCorrectLocation', state);
+
   const update = {
     poaCorrectLocation: action.payload.poaCorrectLocation,
     organizationName: null,
     representativeName: null,
-    representativeType: null
+    representativeType: null,
+    erroredFields: updatedErroredFields
   };
 
   return Object.assign({}, state, update);
