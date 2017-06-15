ab942a8f4041a8461340901e92cd9c4cf811c37c
Mark Benjamin <mark@navahq.com>
2017-06-15 10:33:03 -0400

    Welcome gate pt 1

--- client/app/components/Link.jsx
+++ client/app/components/Link.jsx
@@ -1,52 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link as ReduxLink} from 'react-router-dom';
 
 export default class Link extends React.Component {
-  typeOfLink = () => {
-    if (this.props.to) {
-      return Link;
-    } else if (this.props.href) {
-      return a;
-    }
-  }
-
   render() {
     let {
       to,
-      href,
       children
     } = this.props;
 
-    const typeOfLink = this.typeOfLink();
-
-    return <typeOfLink
+    return <ReduxLink
         to={to}
-        href={href}
+        type="button"
+        className="usa-button-outline"
       >
         {children}
-      </Link>;
-
-    } else if (href) {
-      return <a >{children}</a>;
-    }
+      </ReduxLink>;
   }
 }
 
-Button.defaultProps = {
-  classNames: ['cf-submit'],
-  type: 'button'
-};
-
-Button.propTypes = {
-  ariaLabel: PropTypes.string,
-  children: PropTypes.node,
-  classNames: PropTypes.arrayOf(PropTypes.string),
-  disabled: PropTypes.bool,
-  id: PropTypes.string,
-  linkStyle: PropTypes.bool,
-  loading: PropTypes.bool,
-  name: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  type: PropTypes.string
+Link.propTypes = {
+  to: PropTypes.string
 };
