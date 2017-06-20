04aa3808937cd68b47da6cbe4719c8b47cda4102
Nemo Djuric <nemo.djuric@gmail.com>
2017-06-20 16:28:15 -0400

    First push with code, view and spec tests.

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -143,6 +143,10 @@ class Certification < ActiveRecord::Base
     where("completed_at IS NOT NULL")
   end
 
+  def self.v2
+    where(v2: true).or(where.not(bgs_representative_type: nil)).or(where.not(bgs_representative_name: nil)).or(where.not(vacols_representative_type: nil)).or(where.not(vacols_representative_name: nil))
+  end
+
   def self.was_missing_doc
     was_missing_nod.or(was_missing_soc)
                    .or(was_missing_ssoc)
