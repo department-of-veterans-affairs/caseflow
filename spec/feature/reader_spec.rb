53ea89b568795c0d6869adb0be60a69382899fa5
Mark Benjamin <mark@navahq.com>
2017-06-21 11:15:09 -0400

    Trying to fix tests

--- spec/feature/reader_spec.rb
+++ spec/feature/reader_spec.rb
@@ -58,7 +58,7 @@ def add_comment(text)
   click_on "Save"
 end
 
-RSpec.feature "Reader" do
+RSpec.feature "Reader", focus: true do
   before do
     FeatureToggle.disable!(:reader)
     FeatureToggle.enable!(:reader)
