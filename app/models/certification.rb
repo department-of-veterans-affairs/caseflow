bf4322eacf0a64abe36e093da700281fd2e1b302
Nemo Djuric <nemo.djuric@gmail.com>
2017-06-22 13:35:50 -0400

    Added comment for v2 method.

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -143,6 +143,8 @@ class Certification < ActiveRecord::Base
     where("completed_at IS NOT NULL")
   end
 
+  # in order to include certifications created before v2 field was introduced, we have additional 'or' conditions
+  # (i.e. bgs_representative_type not nil)
   def self.v2
     where(v2: true).or(where.not(bgs_representative_type: nil))
                    .or(where.not(bgs_representative_name: nil))
