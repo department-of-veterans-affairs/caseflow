30a03f45f3ac4717e5cedd6837b3be6bdd13a445
Marian Lumba <mmlumba@gmail.com>
2017-06-23 15:30:14 -0400

    fix alertbanner

--- client/test/karma/components/AlertBanner-test.js
+++ client/test/karma/components/AlertBanner-test.js
@@ -4,33 +4,33 @@ import { mount } from 'enzyme';
 
 import AlertBanner from '../../../app/components/AlertBanner';
 
-describe.only('AlertBanner', () => {
-    it('shows info banner', () => {
-      let wrapper = mount(<AlertBanner title="Info banner" type="info">
-        This shows an info banner.
-      </AlertBanner>);
+describe('AlertBanner', () => {
+  it('shows info banner', () => {
+    let wrapper = mount(<AlertBanner title="Info banner" type="info">
+      This shows an info banner.
+    </AlertBanner>);
 
-      expect(wrapper.find('.usa-alert-info')).to.have.length(1);
-    })
-    it('shows warning banner', () => {
-      let wrapper = mount(<AlertBanner title="Warning banner" type="warning">
-        This shows a warning banner.
-      </AlertBanner>);
+    expect(wrapper.find('.usa-alert-info')).to.have.length(1);
+  });
+  it('shows warning banner', () => {
+    let wrapper = mount(<AlertBanner title="Warning banner" type="warning">
+      This shows a warning banner.
+    </AlertBanner>);
 
-      expect(wrapper.find('.usa-alert-warning')).to.have.length(1);
-    })
-    it('shows error banner', () => {
-      let wrapper = mount(<AlertBanner title="Error banner" type="error">
-        This shows a error banner.
-      </AlertBanner>);
+    expect(wrapper.find('.usa-alert-warning')).to.have.length(1);
+  });
+  it('shows error banner', () => {
+    let wrapper = mount(<AlertBanner title="Error banner" type="error">
+      This shows a error banner.
+    </AlertBanner>);
 
-      expect(wrapper.find('.usa-alert-error')).to.have.length(1);
-    })
-    it('shows success banner', () => {
-      let wrapper = mount(<AlertBanner title="Success banner" type="success">
-        This shows a success banner.
-      </AlertBanner>);
+    expect(wrapper.find('.usa-alert-error')).to.have.length(1);
+  });
+  it('shows success banner', () => {
+    let wrapper = mount(<AlertBanner title="Success banner" type="success">
+      This shows a success banner.
+    </AlertBanner>);
 
-      expect(wrapper.find('.usa-alert-success')).to.have.length(1);
-    })
-})
+    expect(wrapper.find('.usa-alert-success')).to.have.length(1);
+  });
+});
