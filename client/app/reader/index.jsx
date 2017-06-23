327e1f56625343ae43e4cd6d862713e131569b95
Alex Prokop <alex.prokop@gmail.com>
2017-06-23 13:26:49 -0400

    move out of component

--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -4,20 +4,19 @@ import DecisionReviewer from './DecisionReviewer';
 import readerReducer from './reducer';
 import configureStore from '../util/ConfigureStore';
 
+const store = configureStore({ reducers: readerReducer });
+if (module.hot) {
+  // Enable Webpack hot module replacement for reducers.
+  // Changes made to the reducers while developing should be
+  // available instantly.
+  // Note that this expects the global reducer for each app
+  // to be present at reducers/index.
+  module.hot.accept('./reducer', () => {
+    store.replaceReducer(readerReducer);
+  });
+}
+
 const Reader = (props) => {
-  const store = configureStore({ reducers: readerReducer });
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers.
-    // Changes made to the reducers while developing should be
-    // available instantly.
-    // Note that this expects the global reducer for each app
-    // to be present at reducers/index.
-    module.hot.accept('./reducer', () => {
-      store.replaceReducer(readerReducer);
-    });
-  }
-
   return <Provider store={store}>
       <DecisionReviewer {...props} />
   </Provider>;
