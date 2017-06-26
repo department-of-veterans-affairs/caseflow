9c5dc55cf8a127e9a8a9bec46eb1358613eae2b5
Marian Lumba <mmlumba@gmail.com>
2017-06-26 16:14:26 -0400

    css style changes

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -13,9 +13,17 @@ export default class StyleGuideAccordions extends React.Component {
   <p>Our accordion style was taken from the US Web Design Standards.
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
-    <Collapse accordion={true}>
-      <Panel header="hello" headerClass="my-header-class">this is panel content</Panel>
-      <Panel header="title2">this is panel content2 or other</Panel>
+    <Collapse accordion={true} className="usa-accordion">
+      <Panel header="hello" headerClass="usa-accordion-button">
+        <div className="usa-accordion-content">
+          this is panel content
+        </div>
+      </Panel>
+      <Panel header="title2" headerClass="usa-accordion-button">
+        <div className="usa-accordion-content">
+          this is panel content2 or other
+        </div>
+      </Panel>
     </Collapse>
     </div>;
   }
