d0cdd6bcdd3bf7269a21ab423d6d4be4855dee52
Shade L. Jenifer <shade.jenifer@pro-spheretek.com>
2017-06-26 11:41:43 -0400

    Fix lint

--- client/test/karma/components/LoadingSymbol-test.js
+++ client/test/karma/components/LoadingSymbol-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import LoadingSymbol from '../../../app/components/LoadingSymbol';
 
 describe('LoadingSymbol', () => {
