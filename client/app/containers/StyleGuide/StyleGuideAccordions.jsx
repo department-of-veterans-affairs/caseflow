2d4af2f117cebb787d0a732cb7597dc0e422a1fa
Marian Lumba <mmlumba@gmail.com>
2017-06-27 09:43:47 -0400

    styling changes

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -13,14 +13,17 @@ export default class StyleGuideAccordions extends React.Component {
   <p>Our accordion style was taken from the US Web Design Standards.
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
-    <Accordion>
+  <Accordion className="usa-accordion-bordered">
   				{[1, 2, 3, 4, 5].map((item) => {
   					return (
-  						<AccordionItem title={`Item ${ item }`} slug={item} key={item}>
-  							<div>
+  						<AccordionItem
+                title={`Item ${ item }`}
+                slug={item}
+                key={item}
+                titleClassName="usa-accordion-button"
+                expandedClassname="usa-accordion-content">
   								{`Item ${ item } content`}
   								{item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
-  							</div>
   						</AccordionItem>
   					);
   				})}
