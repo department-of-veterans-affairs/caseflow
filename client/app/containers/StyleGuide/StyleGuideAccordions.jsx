cbbec50de18a9d560f581d8b6dd94681b2cd7c3b
Marian Lumba <mmlumba@gmail.com>
2017-06-27 11:42:40 -0400

    technical notes

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -4,8 +4,9 @@ import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle'
 
 export default class StyleGuideAccordions extends React.Component {
   render = () => {
-    let headerNumber = [1, 2, 3, 4, 5];
-    let accordionText = <p>
+    const headerNumber = [1, 2, 3, 4, 5];
+
+    const accordionText = <p>
       Millions of Americans interact with government services every day.
       Veterans apply for benefits. Students compare financial aid options.
       Small business owners seek loans. Too often, outdated tools and complex
@@ -16,6 +17,14 @@ export default class StyleGuideAccordions extends React.Component {
       Visit USDS.gov to learn more.
     </p>;
 
+    const headerPanels = headerNumber.map((header) => {
+      return (<Panel header={`Example title ${header}`} headerClass="usa-accordion-button" key={header}>
+        <div className="usa-accordion-content">
+          {accordionText}
+        </div>
+      </Panel>);
+    });
+
     return <div>
     <StyleGuideComponentTitle
       title="Accordions"
@@ -25,37 +34,26 @@ export default class StyleGuideAccordions extends React.Component {
   <p>Our accordion style was taken from the US Web Design Standards.
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
+  <p><b>Technical Notes:</b> The whole accordion is placed in the <code>Collapse</code>
+    element while each accordion (header and body) is listed in the <code>Panel</code>
+    element. To obtain your desired border style, specify one of the following classnames
+    in the <code>className</code> prop of the <code>Collapse</code> element.</p>
     <h3>Border</h3>
+      <p>className: <code>usa-accordion-bordered</code></p>
       <Collapse accordion={true} className="usa-accordion-bordered">
-        {headerNumber.map((header) => {
-          return (<Panel header={`Example title ${header}`} headerClass="usa-accordion-button" key={header}>
-            <div className="usa-accordion-content">
-              {accordionText}
-            </div>
-          </Panel>);
-        })}
+        {headerPanels}
       </Collapse>
 
     <h3>Borderless</h3>
+      <p>className: <code>usa-accordion</code></p>
       <Collapse accordion={true} className="usa-accordion">
-        {headerNumber.map((header) => {
-          return (<Panel header={`Example title ${header}`} headerClass="usa-accordion-button" key={header}>
-            <div className="usa-accordion-content">
-              {accordionText}
-            </div>
-          </Panel>);
-        })}
+        {headerPanels}
       </Collapse>
 
     <h3>Border with Outline</h3>
+      <p>className: <code>usa-accordion-bordered-outline</code></p>
       <Collapse accordion={true} className="usa-accordion-bordered-outline">
-        {headerNumber.map((header) => {
-          return (<Panel header={`Example title ${header}`} headerClass="usa-accordion-button" key={header}>
-            <div className="usa-accordion-content">
-              {accordionText}
-            </div>
-          </Panel>);
-        })}
+        {headerPanels}
       </Collapse>
     </div>;
   }
