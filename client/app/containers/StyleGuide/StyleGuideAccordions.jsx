4ce95f7c46c10f2d945e5bf64a05a335094171b5
Marian Lumba <mmlumba@gmail.com>
2017-06-27 11:14:34 -0400

    remove MOST OF THE LINES

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -4,6 +4,18 @@ import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle'
 
 export default class StyleGuideAccordions extends React.Component {
   render = () => {
+    let headerNumber = [1,2,3,4,5];
+    let accordionText = <p>
+      Millions of Americans interact with government services every day.
+      Veterans apply for benefits. Students compare financial aid options.
+      Small business owners seek loans. Too often, outdated tools and complex
+      systems make these interactions cumbersome and frustrating. Enter the
+      United States Digital Service. We partner leading technologists with
+      dedicated public servants to improve the usability and reliability of
+      our government's most important digital services.
+      Visit USDS.gov to learn more.
+    </p>
+
     return <div>
     <StyleGuideComponentTitle
       title="Accordions"
@@ -14,225 +26,36 @@ export default class StyleGuideAccordions extends React.Component {
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
     <h3>Border</h3>
-    <Collapse accordion={true} className="usa-accordion-bordered">
-      <Panel header="Example title 1" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 2" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 3" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 4" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 5" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-    </Collapse>
+      <Collapse accordion={true} className="usa-accordion-bordered">
+        {headerNumber.map((header) => {
+          return (<Panel header={"Example title " + header} headerClass="usa-accordion-button" key={header}>
+            <div className="usa-accordion-content">
+              {accordionText}
+            </div>
+          </Panel>)
+        })}
+      </Collapse>
 
     <h3>Borderless</h3>
-    <Collapse accordion={true} className="usa-accordion">
-      <Panel header="Example title 1" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 2" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 3" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 4" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-      <Panel header="Example title 5" headerClass="usa-accordion-button">
-        <div className="usa-accordion-content">
-          <p>
-            Millions of Americans interact with government services every day.
-            Veterans apply for benefits. Students compare financial aid options.
-            Small business owners seek loans. Too often, outdated tools and complex
-            systems make these interactions cumbersome and frustrating. Enter the
-            United States Digital Service. We partner leading technologists with
-            dedicated public servants to improve the usability and reliability of
-            our government's most important digital services.
-            Visit USDS.gov to learn more.
-          </p>
-        </div>
-      </Panel>
-    </Collapse>
+      <Collapse accordion={true} className="usa-accordion">
+        {headerNumber.map((header) => {
+          return (<Panel header={"Example title " + header} headerClass="usa-accordion-button" key={header}>
+            <div className="usa-accordion-content">
+              {accordionText}
+            </div>
+          </Panel>)
+        })}
+      </Collapse>
 
     <h3>Border with Outline</h3>
       <Collapse accordion={true} className="usa-accordion-bordered-outline">
-        <Panel header="Example title 1" headerClass="usa-accordion-button">
-          <div className="usa-accordion-content">
-            <p>
-              Millions of Americans interact with government services every day.
-              Veterans apply for benefits. Students compare financial aid options.
-              Small business owners seek loans. Too often, outdated tools and complex
-              systems make these interactions cumbersome and frustrating. Enter the
-              United States Digital Service. We partner leading technologists with
-              dedicated public servants to improve the usability and reliability of
-              our government's most important digital services.
-              Visit USDS.gov to learn more.
-            </p>
-          </div>
-        </Panel>
-        <Panel header="Example title 2" headerClass="usa-accordion-button">
-          <div className="usa-accordion-content">
-            <p>
-              Millions of Americans interact with government services every day.
-              Veterans apply for benefits. Students compare financial aid options.
-              Small business owners seek loans. Too often, outdated tools and complex
-              systems make these interactions cumbersome and frustrating. Enter the
-              United States Digital Service. We partner leading technologists with
-              dedicated public servants to improve the usability and reliability of
-              our government's most important digital services.
-              Visit USDS.gov to learn more.
-            </p>
-          </div>
-        </Panel>
-        <Panel header="Example title 3" headerClass="usa-accordion-button">
-          <div className="usa-accordion-content">
-            <p>
-              Millions of Americans interact with government services every day.
-              Veterans apply for benefits. Students compare financial aid options.
-              Small business owners seek loans. Too often, outdated tools and complex
-              systems make these interactions cumbersome and frustrating. Enter the
-              United States Digital Service. We partner leading technologists with
-              dedicated public servants to improve the usability and reliability of
-              our government's most important digital services.
-              Visit USDS.gov to learn more.
-            </p>
-          </div>
-        </Panel>
-        <Panel header="Example title 4" headerClass="usa-accordion-button">
-          <div className="usa-accordion-content">
-            <p>
-              Millions of Americans interact with government services every day.
-              Veterans apply for benefits. Students compare financial aid options.
-              Small business owners seek loans. Too often, outdated tools and complex
-              systems make these interactions cumbersome and frustrating. Enter the
-              United States Digital Service. We partner leading technologists with
-              dedicated public servants to improve the usability and reliability of
-              our government's most important digital services.
-              Visit USDS.gov to learn more.
-            </p>
-          </div>
-        </Panel>
-        <Panel header="Example title 5" headerClass="usa-accordion-button">
-          <div className="usa-accordion-content">
-            <p>
-              Millions of Americans interact with government services every day.
-              Veterans apply for benefits. Students compare financial aid options.
-              Small business owners seek loans. Too often, outdated tools and complex
-              systems make these interactions cumbersome and frustrating. Enter the
-              United States Digital Service. We partner leading technologists with
-              dedicated public servants to improve the usability and reliability of
-              our government's most important digital services.
-              Visit USDS.gov to learn more.
-            </p>
-          </div>
-        </Panel>
+        {headerNumber.map((header) => {
+          return (<Panel header={"Example title " + header} headerClass="usa-accordion-button" key={header}>
+            <div className="usa-accordion-content">
+              {accordionText}
+            </div>
+          </Panel>)
+        })}
       </Collapse>
     </div>;
   }
