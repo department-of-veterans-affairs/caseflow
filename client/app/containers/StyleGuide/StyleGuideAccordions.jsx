b1db1d7fb3f6fc1afe9a2d0e4506b6cb2bb5bbb7
Marian Lumba <mmlumba@gmail.com>
2017-06-27 12:08:14 -0400

    refactoring

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -4,6 +4,25 @@ import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle'
 
 export default class StyleGuideAccordions extends React.Component {
   render = () => {
+    const accordionItemContents = [1, 2, 3, 4, 5].map((item) => {
+      return (
+        <AccordionItem
+          title={`Example title ${item}`}
+          slug={item}
+          key={item}>
+            Millions of Americans interact with government services every day.
+            Veterans apply for benefits. Students compare financial aid options.
+            Small business owners seek loans. Too often, outdated tools and
+            complex systems make these interactions cumbersome and frustrating.
+            Enter the United States Digital Service. We partner leading
+            technologists with dedicated public servants to improve the usability
+            and reliability of our government's most important digital services.
+            Visit USDS.gov to learn more.
+        </AccordionItem>
+      );
+    });
+
+
     return <div>
     <StyleGuideComponentTitle
       title="Accordions"
@@ -14,65 +33,17 @@ export default class StyleGuideAccordions extends React.Component {
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
     <h3>Bordered</h3>
-    <Accordion className="usa-accordion-bordered">
-  				{[1, 2, 3, 4, 5].map((item) => {
-  					return (
-  						<AccordionItem
-                title={`Example title ${ item }`}
-                slug={item}
-                key={item}>
-  								Millions of Americans interact with government services every day.
-                  Veterans apply for benefits. Students compare financial aid options.
-                  Small business owners seek loans. Too often, outdated tools and
-                  complex systems make these interactions cumbersome and frustrating.
-                  Enter the United States Digital Service. We partner leading
-                  technologists with dedicated public servants to improve the usability
-                  and reliability of our government's most important digital services.
-                  Visit USDS.gov to learn more.
-  						</AccordionItem>
-  					);
-  				})}
-  		</Accordion>
-      <h3>Borderless</h3>
+      <Accordion className="usa-accordion-bordered">
+        {accordionItemContents}
+      </Accordion>
+    <h3>Borderless</h3>
       <Accordion className="usa-accordion">
-    				{[1, 2, 3, 4, 5].map((item) => {
-    					return (
-    						<AccordionItem
-                  title={`Example title ${ item }`}
-                  slug={item}
-                  key={item}>
-    								Millions of Americans interact with government services every day.
-                    Veterans apply for benefits. Students compare financial aid options.
-                    Small business owners seek loans. Too often, outdated tools and
-                    complex systems make these interactions cumbersome and frustrating.
-                    Enter the United States Digital Service. We partner leading
-                    technologists with dedicated public servants to improve the usability
-                    and reliability of our government's most important digital services.
-                    Visit USDS.gov to learn more.
-    						</AccordionItem>
-    					);
-    				})}
-    		</Accordion>
-        <h3>Bordered Outline</h3>
-        <Accordion className="usa-accordion-bordered-outline">
-      				{[1, 2, 3, 4, 5].map((item) => {
-      					return (
-      						<AccordionItem
-                    title={`Example title ${ item }`}
-                    slug={item}
-                    key={item}>
-      								Millions of Americans interact with government services every day.
-                      Veterans apply for benefits. Students compare financial aid options.
-                      Small business owners seek loans. Too often, outdated tools and
-                      complex systems make these interactions cumbersome and frustrating.
-                      Enter the United States Digital Service. We partner leading
-                      technologists with dedicated public servants to improve the usability
-                      and reliability of our government's most important digital services.
-                      Visit USDS.gov to learn more.
-      						</AccordionItem>
-      					);
-      				})}
-      		</Accordion>
+        {accordionItemContents}
+      </Accordion>
+    <h3>Bordered Outline</h3>
+      <Accordion className="usa-accordion-bordered-outline">
+        {accordionItemContents}
+      </Accordion>
     </div>;
   }
 }
