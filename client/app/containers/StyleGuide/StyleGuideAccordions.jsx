5eb26a1bb78c3a67c4755b0e8ce08769ee0a1bd7
Marian Lumba <mmlumba@gmail.com>
2017-06-28 09:27:25 -0400

    adding technical notes

--- client/app/containers/StyleGuide/StyleGuideAccordions.jsx
+++ client/app/containers/StyleGuide/StyleGuideAccordions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Accordion, AccordionItem } from 'react-sanfona';
 import StyleGuideComponentTitle from '../../components/StyleGuideComponentTitle';
 
-export default class StyleGuideAccordions extends React.Component {
+export default class StyleGuideAccordions extends React.PureComponent {
   render = () => {
     const accordionItemContents = [1, 2, 3, 4, 5].map((item) => {
       return (
@@ -32,15 +32,22 @@ export default class StyleGuideAccordions extends React.Component {
   <p>Our accordion style was taken from the US Web Design Standards.
     Accordions are a list of headers that can be clicked to hide or reveal additional
     content.</p>
+  <p><b>Technical Notes:</b> The whole accordion is placed in the <code>Accordion</code>
+     element while each accordion (header and body) is listed in the <code>AccordionItem</code>
+     element. To obtain your desired border style, specify one of the following classnames
+     in the <code>className</code> prop of the <code>Accordion</code> element.</p>
     <h3>Bordered</h3>
+      <p>className: <code>usa-accordion-bordered</code></p>
       <Accordion className="usa-accordion-bordered">
         {accordionItemContents}
       </Accordion>
     <h3>Borderless</h3>
+      <p>className: <code>usa-accordion</code></p>
       <Accordion className="usa-accordion">
         {accordionItemContents}
       </Accordion>
     <h3>Bordered Outline</h3>
+      <p>className: <code>usa-accordion-bordered-outline</code></p>
       <Accordion className="usa-accordion-bordered-outline">
         {accordionItemContents}
       </Accordion>
