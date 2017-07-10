51bcd7bd228a5fdaa522cf3e145966c7bace5263
Shade L. Jenifer <shade.jenifer@pro-spheretek.com>
2017-07-10 11:13:40 -0400

    Test DailyDocketContainer and fix DocketsContainer test

--- client/test/karma/hearings/DailyDocketContainer-test.js
+++ client/test/karma/hearings/DailyDocketContainer-test.js
@@ -9,14 +9,14 @@ import { Provider } from 'react-redux';
 import ApiUtil from '../../../app/util/ApiUtil';
 import hearingsReducers from '../../../app/hearings/reducers/index';
 import { populateDockets } from '../../../app/hearings/actions/Dockets';
-import DocketsContainer from '../../../app/hearings/DocketsContainer';
+import DailyDocketContainer from '../../../app/hearings/DailyDocketContainer';
 
 const store = createStore(hearingsReducers, { dockets: {} }, applyMiddleware(thunk));
 
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable max-statements */
-describe.only('DocketsContainer', () => {
+describe.only('DailyDocketContainer', () => {
   let wrapper;
 
   spy(ApiUtil, 'get');
@@ -26,29 +26,54 @@ describe.only('DocketsContainer', () => {
       <Provider store={store}>
         <MemoryRouter initialEntries={['/']}>
           <div>
-            <DocketsContainer veteran_law_judge={{ name: 'me' }} />
+            <DailyDocketContainer veteran_law_judge={{ name: 'me' }} date="2017-06-17"/>
           </div>
         </MemoryRouter>
       </Provider>);
   });
 
-  it('retrieves dockets', () => {
+/*
+  afterEach(() => {
+    wrapper = null;
+  });
+*/
+
+  it('retrieves hearings', () => {
     setTimeout(() => {
       expect(ApiUtil.get.calledOnce).to.be.true;
       ApiUtil.get.restore();
     });
   });
 
-  it('notifies user when no dockets are returned', () => {
+  it('notifies user when no hearings are returned', () => {
     store.dispatch(populateDockets({}));
     expect(wrapper.text()).to.include('You have no upcoming hearings.');
   });
 
-  it('renders loaded dockets', () => {
+  it('renders loaded hearings', () => {
     store.dispatch(populateDockets({
       '2017-06-17': {
         date: '2017-06-17T17:52:09.742-04:00',
-        hearings_hash: [],
+        hearings_hash: [{
+          appeal_id: 68468,
+          appellant_last_first_mi: "VanBuren, James A.",
+          date: "2017-06-30T14:03:42.714Z",
+          representative_name: "Military Order of the Purple Heart",
+          request_type: "CO",
+          user_id: 9,
+          vacols_id: "f10b9ed6a",
+          vbms_id: "3bf55b922",
+          venue: {
+            city: "Baltimore",
+            state: "MD",
+            timezone: "America/New_York"
+          },
+          worksheet_comments_for_attorney: "Look for knee-related medical records",
+          worksheet_contentions: "The veteran believes their knee is hurt",
+          worksheet_evidence: "Medical exam occurred on 10/10/2008",
+          worksheet_military_service: null,
+          worksheet_witness: "Jane Doe attended"
+        }],
         type: 'central_office',
         venue: {
           city: 'Baltimore',
@@ -57,6 +82,6 @@ describe.only('DocketsContainer', () => {
         }
       }
     }));
-    expect(wrapper.text()).to.include('Upcoming Hearing Days');
+    expect(wrapper.text()).to.include('Daily Docket');
   });
 });
