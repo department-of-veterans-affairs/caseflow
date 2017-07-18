a932c3f0e50e6e8082361d198da5e6f5a49d9592
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-18 13:35:23 -0400

    Fixing linting issus in case_decision.rb

--- app/models/vacols/case_decision.rb
+++ app/models/vacols/case_decision.rb
@@ -1,5 +1,5 @@
 class VACOLS::CaseDecision < VACOLS::Record
-  self.table_name "vacols.decass"
+  self.table_name = "vacols.decass"
   self.primary_key = "defolder"
 
   has_one :case, foreign_key: :bfkey
