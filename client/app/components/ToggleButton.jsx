6647d03a0f4e5d19b904a4c9ebf53b9560f0f572
Sjones352 <sjones@tistatech.com>
2017-07-18 11:13:18 -0400

    Modified the box-shadow to gray-light and removed extra spaces in toggle button

--- client/app/components/ToggleButton.jsx
+++ client/app/components/ToggleButton.jsx
@@ -16,13 +16,12 @@ export default class ToggleButton extends React.Component {
    // for each child and adding the result to an array.
 
     const mappedChildren = React.Children.map(children, (child) => {
-
       return React.cloneElement(child, {
         classNames: active === child.props.name ? ['cf-toggle-box-shadow usa-button'] : ['usa-button-outline'],
         onClick: this.handleClick(child.props.name)
       }
     );
-    });
+  });
 
     return <div className ="cf-toggle-button">{mappedChildren}</div>;
   }
