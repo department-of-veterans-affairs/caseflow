d28ad0364572ab85d2d21e1a1927172c7c6c5bea
Artem Vovk <Artem.Vovk@va.gov>
2017-07-18 21:13:40 -0400

    moved xvfb so karma can has eyes

--- docker_test_runner.sh
+++ docker_test_runner.sh
@@ -2,9 +2,12 @@ export RAILS_ENV="test"
 export CHROME_ARGS="--no-sandbox"
 export CHROME_BIN="chromium-browser"
 
+Xvfb +extension RANDR :99 -screen 0 1600x900x16 &
+
+cd client && npm run test
+
 rake db:create
 rake db:schema:load
-Xvfb +extension RANDR :99 -screen 0 1600x900x16 &
 
 rake lint
 rake security
