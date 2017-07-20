1250bc40b6eb8b339d017cecc6af9c72bedfc519
Alex Prokop <alex.prokop@gmail.com>
2017-07-20 12:39:03 -0400

    whitespace

--- client/app/components/LoadingScreen.jsx
+++ client/app/components/LoadingScreen.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { loadingSymbolHtml } from './RenderFunctions';
 
-
 const LoadingScreen = ({ spinnerColor, message }) => {
   return <div
     id="loading-symbol"
