63c0e409e4cabe5c1c9d8b77f427aaf2ec061617
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-21 16:25:37 -0400

    Adding base dir variable for mock efolder service

--- lib/fakes/efolder_service.rb
+++ lib/fakes/efolder_service.rb
@@ -3,24 +3,26 @@ class Fakes::EfolderService
     attr_accessor :document_records
   end
 
+  @@test_pdf_directory = Pathname.new(File.join(Rails.root, "lib", "pdfs"))
+
   # rubocop:disable Metrics/CyclomaticComplexity
   def self.fetch_document_file(document)
     # Mocks a call to <efolder>/documents/<vbms_doc_id>
     path =
       case document.vbms_document_id.to_i
       when 1
-        File.join(Rails.root, "lib", "pdfs", "VA8.pdf")
+        @@test_pdf_directory.join("VA8.pdf")
       when 2
-        File.join(Rails.root, "lib", "pdfs", "Formal_Form9.pdf")
+        @@test_pdf_directory.join("Formal_Form9.pdf")
       when 3
-        File.join(Rails.root, "lib", "pdfs", "Informal_Form9.pdf")
+        @@test_pdf_directory.join("Informal_Form9.pdf")
       when 4
-        File.join(Rails.root, "lib", "pdfs", "FakeDecisionDocument.pdf")
+        @@test_pdf_directory.join("FakeDecisionDocument.pdf")
       when 5
-        File.join(Rails.root, "lib", "pdfs", "megadoc.pdf")
+        @@test_pdf_directory.join("megadoc.pdf")
       else
-        file = File.join(Rails.root, "lib", "pdfs", "redacted", "#{document.vbms_document_id}.pdf")
-        file = File.join(Rails.root, "lib", "pdfs", "KnockKnockJokes.pdf") unless File.exist?(file)
+        file = @@test_pdf_directory.join("redacted", "#{document.vbms_document_id}.pdf")
+        file = @@test_pdf_directory.join("KnockKnockJokes.pdf") unless File.exist?(file)
         file
       end
     IO.binread(path)
