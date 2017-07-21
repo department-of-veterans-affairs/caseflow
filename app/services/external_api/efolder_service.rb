d4ca43ecaa8c89ca1282422f5afd68b704af7c69
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-21 14:39:18 -0400

    Adding efolder http client

--- app/services/external_api/efolder_service.rb
+++ app/services/external_api/efolder_service.rb
@@ -1,3 +1,4 @@
+require "HTTPI"
 
 class ExternalApi::EfolderService
   class << self
@@ -6,31 +7,51 @@ class ExternalApi::EfolderService
     attr_accessor :uploaded_form8, :uploaded_form8_appeal
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
   def self.fetch_document_file(document)
-    # Mocks a call to <efolder>/documents/<vbms_doc_id>
-    path =
-        case document.vbms_document_id.to_i
-          when 1
-            File.join(Rails.root, "lib", "pdfs", "VA8.pdf")
-          when 2
-            File.join(Rails.root, "lib", "pdfs", "Formal_Form9.pdf")
-          when 3
-            File.join(Rails.root, "lib", "pdfs", "Informal_Form9.pdf")
-          when 4
-            File.join(Rails.root, "lib", "pdfs", "FakeDecisionDocument.pdf")
-          when 5
-            File.join(Rails.root, "lib", "pdfs", "megadoc.pdf")
-          else
-            file = File.join(Rails.root, "lib", "pdfs", "redacted", "#{document.vbms_document_id}.pdf")
-            file = File.join(Rails.root, "lib", "pdfs", "KnockKnockJokes.pdf") unless File.exist?(file)
-            file
-        end
-    IO.binread(path)
+    # Makes a GET request to <efolder>/documents/<vbms_doc_id>
+    @efolder_client ||= init_vbms_client
+
+    vbms_id = document.vbms_document_id
+    uri = URI.escape(efolder_base_url + "/files/" + vbms_id)
+    result = get_efolder_response(uri)
+    result && result.content
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
 
   def self.fetch_documents_for(appeal)
-    (document_records || {})[appeal.vbms_id] || @documents || []
+    # Makes a GET request to <efolder>/files/<vbms_id>
+    @vbms_client ||= init_vbms_client
+
+    sanitized_id = appeal.sanitized_vbms_id
+    uri = URI.escape(efolder_base_url + "/documents/" + sanitized_id)
+    documents = get_efolder_response(uri)
+
+    Rails.logger.info("# of Documents retrieved from efolder: #{documents.length}")
+
+    documents.map do |vbms_document|
+      Document.from_vbms_document(vbms_document)
+    end
+  end
+
+  def self.efolder_base_url
+    Rails.application.config.efolder_url
+  end
+
+  def self.get_efolder_response(url)
+    response = []
+
+    MetricsService.record("sent efolder GET request to #{url}",
+                          service: :efolder,
+                          id: id) do
+      request = HTTPI::Request.new(url)
+      response = request.get
+    end
+
+    if response.error?
+      Rails.logger.error "Error sending request to eFolder: #{url}. HTTP Status Code: #{response.code}"
+    else
+      response = response.body
+    end
+
+    response
   end
 end
