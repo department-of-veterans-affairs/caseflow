2e3058322b68fd05bf96996d842b62fd1ecd0fe9
Sharon Warner <sharon.warner56@gmail.com>
2017-07-24 15:29:33 -0400

    Weird linting spacing fix

--- client/app/containers/CaseWorker/HeaderMessage.jsx
+++ client/app/containers/CaseWorker/HeaderMessage.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 export default class HeaderMessage extends React.PureComponent {
   render() {
     const {
-            availableTasks,
-            tasksRemaining
-        } = this.props;
+      availableTasks,
+      tasksRemaining
+    } = this.props;
 
     if (tasksRemaining && !availableTasks) {
       return <div>
-                <h2>All claims in queue completed</h2>
-                <p>There are no more claims to pick up. Please come back later.</p>
-            </div>;
+        <h2>All claims in queue completed</h2>
+        <p>There are no more claims to pick up. Please come back later.</p>
+      </div>;
     }
 
     if (tasksRemaining && availableTasks) {
       return <div>
-                <h2>Get Started!</h2>
-                <p>There are claims ready to get picked up for today.</p>
-            </div>;
+        <h2>Get Started!</h2>
+        <p>There are claims ready to get picked up for today.</p>
+      </div>;
     }
 
     return <span>
