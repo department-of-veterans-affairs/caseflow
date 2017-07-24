69f5578d05e8f991062b5c3368e48fc1eb330e12
Sharon Warner <sharon.warner56@gmail.com>
2017-07-24 15:06:15 -0400

    New file for HeaderMessage component

--- client/app/containers/CaseWorker/CaseWorkerIndex.jsx
+++ client/app/containers/CaseWorker/CaseWorkerIndex.jsx
@@ -5,33 +5,8 @@ import ApiUtil from '../../util/ApiUtil';
 import BaseForm from '../BaseForm';
 import Table from '../../components/Table';
 import Button from '../../components/Button';
+import HeaderMessage from './HeaderMessage';
 import { formatDate } from '../../util/DateUtil';
-
-class HeaderMessage extends React.PureComponent {
-  render() {
-    let {
-      availableTasks,
-      tasksRemaining
-    } = this.props;
-    if (tasksRemaining && !availableTasks) {
-      return <div>
-        <h2>All claims in queue completed</h2>
-        <p>There are no more claims to pick up. Please come back later.</p>
-      </div>;
-    } else if (tasksRemaining && availableTasks) {
-      return <div>
-        <h2>Get Started!</h2>
-        <p>There are claims ready to get picked up for today.</p>
-      </div>;
-    }
-
-    return <span>
-      <h2>Way to go!</h2>
-      <p> You have completed all the claims assigned to you.</p> 💪💻🇺🇸<br/>
-      <h2 className="cf-msg-screen-deck cf-success-emoji-text"/>
-    </span>;
-  }
-}
 
 export default class CaseWorkerIndex extends BaseForm {
   constructor(props) {
