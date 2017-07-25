928adf82c6a82534c05e5a2327cead39910c0513
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-25 09:51:32 -0400

    Stashing changes to Pdf.jsx to be used with #2340

--- client/app/components/Pdf.jsx
+++ client/app/components/Pdf.jsx
@@ -462,24 +462,23 @@ export class Pdf extends React.PureComponent {
       return Promise.resolve(this.predrawnPdfs[file].pdfDocument);
     }
 
-    return PDFJS.getDocument({ url: file,
-      withCredentials: true }).then((pdfDocument) => {
-        if ([...this.props.prefetchFiles, this.props.file].includes(file)) {
+    return PDFJS.getDocument(file).then((pdfDocument) => {
+      if ([...this.props.prefetchFiles, this.props.file].includes(file)) {
         // There is a chance another async call has resolved in the time that
         // getDocument took to run. If so, again just use the cached version.
-          if (_.get(this.predrawnPdfs, [file, 'pdfDocument'])) {
-            return this.predrawnPdfs[file].pdfDocument;
-          }
-          this.predrawnPdfs[file] = {
-            pdfDocument
-          };
-          this.setUpPdfObjects(file, pdfDocument);
-
-          return pdfDocument;
+        if (_.get(this.predrawnPdfs, [file, 'pdfDocument'])) {
+          return this.predrawnPdfs[file].pdfDocument;
         }
+        this.predrawnPdfs[file] = {
+          pdfDocument
+        };
+        this.setUpPdfObjects(file, pdfDocument);
 
-        return null;
-      });
+        return pdfDocument;
+      }
+
+      return null;
+    });
   }
 
   scrollToPageLocation = (pageIndex, yPosition = 0) => {
