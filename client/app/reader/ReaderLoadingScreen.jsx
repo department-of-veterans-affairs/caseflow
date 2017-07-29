ebd5cfd741ceb787dd77312c0c5c528d9dce655f
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-28 22:10:04 -0400

    Fixing formatting in ReaderLoadingScreen

--- client/app/reader/ReaderLoadingScreen.jsx
+++ client/app/reader/ReaderLoadingScreen.jsx
@@ -35,8 +35,11 @@ export class ReaderLoadingScreen extends React.Component {
             return;
           }
 
-          ApiUtil.get(documentUrls[index], { cache: true,
-            withCredentials: true }).then(
+          ApiUtil.get(
+            documentUrls[index],
+            { cache: true,
+              withCredentials: true }
+          ).then(
             () => downloadDocuments(documentUrls, index + PARALLEL_DOCUMENT_REQUESTS)
           );
         };
