51d2a038b2f7cbbc5af2425e70e4a1655edda646
Teja N <teja.s.naraparaju@gmail.com>
2017-07-31 10:31:08 -0400

    Creating a separate component for Header filter message

--- client/app/components/reader/DocumentListHeader.jsx
+++ client/app/components/reader/DocumentListHeader.jsx
@@ -8,6 +8,52 @@ import Analytics from '../../util/AnalyticsUtil';
 import ApiUtil from '../../util/ApiUtil';
 import DocumentsCommentsButton from '../../reader/DocumentsCommentsButton';
 
+
+const HeaderFilterMessage = (props) => {
+
+  const categoryFilters = Object.keys(props.docFilterCriteria.category).some((category) =>
+    props.docFilterCriteria.category[category]
+  );
+  const tagFilters = Object.keys(props.docFilterCriteria.tag).some((tag) =>
+    props.docFilterCriteria.tag[tag]
+  );
+
+  const categoryCount = _.values(props.docFilterCriteria.category).reduce((numberOfCategories, categoryShown) => {
+    return categoryShown ? numberOfCategories + 1 : numberOfCategories;
+  }, 0);
+  const tagCount = _.values(props.docFilterCriteria.tag).length;
+
+  const filteredCategories = [].concat(
+    categoryFilters ? [`Categories (${categoryCount})`] : [],
+    tagFilters ? [`Issue tags (${tagCount})`] : [],
+    props.viewingDocumentsOrComments === 'comments' ? ['Comments'] : []).
+    join(', ');
+
+  if (!filteredCategories.length) {
+    return null;
+  }
+
+  return <p className="document-list-filter-message">Filtering by: {filteredCategories}. <a
+    href="#"
+    id="clear-filters"
+    onClick={props.clearAllFilters}>
+    Clear all filters.</a></p>;
+};
+
+const headerFilterMessageMapDispatchToProps = (dispatch) => ({
+  clearAllFilters: () => {
+    Analytics.event('Claims Folder', 'click', 'Clear all filters');
+    dispatch(clearAllFilters());
+  }
+});
+
+const headerFilterMessageMapStateToProps = (state) => ({
+  viewingDocumentsOrComments: state.viewingDocumentsOrComments
+});
+
+const ConnectedHeaderFilterMessage = connect(headerFilterMessageMapStateToProps,
+                                             headerFilterMessageMapDispatchToProps)(HeaderFilterMessage);
+
 class DocumentListHeader extends React.Component {
   // Record the search value for analytics purposes, don't worry if it fails.
   recordSearch = (query) => {
@@ -19,25 +65,6 @@ class DocumentListHeader extends React.Component {
 
   render() {
     const props = this.props;
-    const { viewingDocumentsOrComments } = this.props;
-
-    const categoryCount = _.values(props.docFilterCriteria.category).reduce((numberOfCategories, categoryShown) => {
-      return categoryShown ? numberOfCategories + 1 : numberOfCategories;
-    }, 0);
-
-    const tagCount = _.values(props.docFilterCriteria.tag).length;
-
-    const categoryFilters = Object.keys(props.docFilterCriteria.category).some((category) =>
-      props.docFilterCriteria.category[category]
-    );
-    const tagFilters = Object.keys(props.docFilterCriteria.tag).some((tag) =>
-      props.docFilterCriteria.tag[tag]
-    );
-    const filteredCategories = [].concat(
-      categoryFilters ? [`Categories (${categoryCount})`] : [],
-      tagFilters ? [`Issue tags (${tagCount})`] : [],
-      viewingDocumentsOrComments === 'comments' ? ['Comments'] : []).
-      join(', ');
 
     return <div>
       <div className="document-list-header">
@@ -58,12 +85,7 @@ class DocumentListHeader extends React.Component {
         </div>
         <DocumentsCommentsButton />
       </div>
-        {Boolean(filteredCategories.length) &&
-        <p className="document-list-filter-message">Filtering by: {filteredCategories}. <a
-          href="#"
-          id="clear-filters"
-          onClick={props.clearAllFilters}>
-          Clear all filters.</a></p> }
+        <ConnectedHeaderFilterMessage docFilterCriteria={props.docFilterCriteria} />
     </div>;
   }
 }
@@ -78,16 +100,11 @@ DocumentListHeader.propTypes = {
 
 const mapStateToProps = (state) => ({
   numberOfDocuments: state.ui.filteredDocIds ? state.ui.filteredDocIds.length : _.size(state.documents),
-  viewingDocumentsOrComments: state.viewingDocumentsOrComments,
-  docFilterCriteria: state.ui.docFilterCriteria,
-  vacolsId: state.loadedAppealId
+  vacolsId: state.loadedAppealId,
+  docFilterCriteria: state.ui.docFilterCriteria
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  clearAllFilters: () => {
-    Analytics.event('Claims Folder', 'click', 'Clear all filters');
-    dispatch(clearAllFilters());
-  },
   clearSearch: () => {
     dispatch(clearSearch());
   },
