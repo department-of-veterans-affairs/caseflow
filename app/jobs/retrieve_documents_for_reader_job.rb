c5cadc424e1a1e87218cd0c3a5d34c9f93244aed
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-07-31 09:45:36 -0400

    Catching HTTP timeouts and announcing job status on slack

--- app/jobs/retrieve_documents_for_reader_job.rb
+++ app/jobs/retrieve_documents_for_reader_job.rb
@@ -8,31 +8,47 @@ class RetrieveDocumentsForReaderJob < ActiveJob::Base
 
     # Args should be set in sidekiq_cron.yml, but default the limit to 1500 if they aren't
     limit = args["limit"] || 1500
-    successful_count = failed_count = docs_attempted = 0
+    counts = { docs_successful_count: 0, docs_failed_count: 0, docs_attempted: 0, appeals_failed_count: 0 }
 
     find_all_active_reader_appeals.each do |appeal|
-      appeal.fetch_documents!(save: true).each do |document|
-        begin
-          unless S3Service.exists?(document.file_name)
-            docs_attempted += 1
-            successful_count += 1 if document.fetch_content
-          end
-        rescue Aws::S3::Errors::ServiceError, VBMS::ClientError => e
-          failed_count += 1
-          Rails.logger.error "Failed to retrieve #{document.file_name}:\n#{e.message}"
+      begin
+        appeal.fetch_documents!(save: true).each do |doc|
+          counts[:docs_attempted] += 1
+          fetch_document_content(doc) ? counts[:docs_successful_count] += 1 : counts[:docs_failed_count] += 1
         end
+      rescue HTTPClient::KeepAliveDisconnected => e
+        # VBMS connection may die when attempting to retrieve list of docs for appeal
+        counts[:appeals_failed_count] += 1
+        Rails.logger.error "Failed to retrieve #{appeal}:\n#{e.message}"
       end
 
-      break if docs_attempted >= limit
+      break if counts[:docs_attempted] >= limit
     end
 
-    Rails.logger.info "Successfully retrieved #{successful_count} documents for Reader cases"
-    Rails.logger.info "#{failed_count} documents failed"
+    log_info(counts)
   end
 
   def find_all_active_reader_appeals
     User.where("'Reader' = ANY(roles)").reduce(Set.new) do |active_appeals, user|
       active_appeals.merge(user.current_case_assignments)
     end
+  end
+
+  def fetch_document_content(doc)
+    doc.fetch_content unless S3Service.exists?(doc.file_name)
+  rescue Aws::S3::Errors::ServiceError, VBMS::ClientError => e
+    Rails.logger.error "Failed to retrieve #{doc.file_name}:\n#{e.message}"
+  end
+
+  def log_info(counts)
+    outout_msg = "RetrieveDocumentsForReaderJob successfully retrieved #{counts[:docs_successful_count]} documents " \
+          "and #{counts[:docs_failed_count]} documents failed"
+    Rails.logger.info outout_msg
+    outout_msg += "\n<!here>" if counts[:docs_failed_count] > 0
+    SlackService.new(url: url).send_notification(outout_msg)
+ end
+
+  def url
+    ENV["SLACK_DISPATCH_ALERT_URL"]
   end
 end
