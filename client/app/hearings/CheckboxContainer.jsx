2672a069d9d8e904489bd45ea3e0cb0f2267b0a5
Shade L. Jenifer <shade.jenifer@pro-spheretek.com>
2017-08-02 10:02:12 -0400

    Save daily docket changes to Redux
    Connects Connects #2473

---
+++ client/app/hearings/CheckboxContainer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as Actions from './actions/dockets';
+import Checkbox from '../components/Checkbox';
+
+export class CheckboxContainer extends React.Component {
+
+  updateCheckbox = (value) => {
+    this.props.updateCheckbox(this.props.action, this.props.id, value);
+  }
+
+  render() {
+    return <Checkbox
+      label={this.props.label}
+      name={this.props.id}
+      onChange={this.updateCheckbox}
+      value={this.props[this.props.id] || this.props.defaultValue || false}
+      tabIndex={this.props.tabIndex}
+    ></Checkbox>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  [ownProps.id]: state[ownProps.id]
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCheckbox: (actionName, prop, value) => {
+    let action = Actions[actionName];
+
+    dispatch(action(prop, value));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckboxContainer);
+
+CheckboxContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+};
