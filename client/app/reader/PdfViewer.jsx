1847e3c8a6aaffe9d299d1af0eae35906b6b34c5
Nick Heiner <nth23@cornell.edu>
2017-08-02 17:35:19 -0400

    Use constants for document navigation

--- client/app/reader/PdfViewer.jsx
+++ client/app/reader/PdfViewer.jsx
@@ -12,6 +12,7 @@ import { isUserEditingText, update } from '../reader/utils';
 import { bindActionCreators } from 'redux';
 import { getFilteredDocuments } from './selectors';
 import * as Constants from '../reader/constants';
+import {CATEGORIES, ACTION_NAMES, INTERACTION_TYPES} from '../reader/analytics';
 import Analytics from '../util/AnalyticsUtil';
 
 export const getNextAnnotationIconPageCoords = (direction, placingAnnotationIconPageCoords, allPagesCoordsBounds) => {
@@ -85,11 +86,19 @@ export class PdfViewer extends React.Component {
     }
 
     if (event.key === 'ArrowLeft') {
-      Analytics.event('Document Viewer', 'key command', 'previous document');
+      Analytics.event(
+        CATEGORIES.VIEW_DOCUMENT_PAGE, 
+        ACTION_NAMES.VIEW_PREVIOUS_DOCUMENT, 
+        INTERACTION_TYPES.KEYBOARD_SHORTCUT
+      );
       this.props.showPdf(this.getPrevDocId())();
     }
     if (event.key === 'ArrowRight') {
-      Analytics.event('Document Viewer', 'key command', 'next document');
+      Analytics.event(
+        CATEGORIES.VIEW_DOCUMENT_PAGE, 
+        ACTION_NAMES.VIEW_NEXT_DOCUMENT, 
+        INTERACTION_TYPES.KEYBOARD_SHORTCUT
+      );
       this.props.showPdf(this.getNextDocId())();
     }
   }
