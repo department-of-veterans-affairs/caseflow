ce968bc674213aeab8db4e88f43a663190c7f0b3
Nemo Djuric <nemo.djuric@gmail.com>
2017-08-04 15:36:46 -0400

    Refactored changeSignAndCertifyForm

--- client/app/certification/reducers/SignAndCertify.js
+++ client/app/certification/reducers/SignAndCertify.js
@@ -2,11 +2,10 @@ import * as CertificationReducers from './Certification';
 
 export const changeSignAndCertifyForm = (state, action) => {
   const update = {};
+  let key = Object.keys(action.payload)[0];
 
-  for (const key of Object.keys(action.payload)) {
-    update[key] = action.payload[key];
-    update.erroredFields = CertificationReducers.clearErroredField(key, state);
-  }
+  update[key] = action.payload[key];
+  update.erroredFields = CertificationReducers.clearErroredField(key, state);
 
   return Object.assign({}, state, update);
 };
