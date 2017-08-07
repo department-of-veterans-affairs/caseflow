5b86f5f6e23ce5593314e961e40f02fcccc485e8
Alex Prokop <alex.prokop@gmail.com>
2017-08-07 16:09:08 -0700

    lint fix

--- app/jobs/middleware/job_request_store_middleware.rb
+++ app/jobs/middleware/job_request_store_middleware.rb
@@ -1,7 +1,7 @@
 # This middleware makes it safe to use RequestStore inside jobs by ensuring
 # that RequestStore is cleared after every job.
 class JobRequestStoreMiddleware
-  def call(worker, msg, queue)
+  def call(_worker, _msg, _queue)
     yield
   ensure
     ::RequestStore.clear! if defined?(::RequestStore)
