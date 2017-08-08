e6223748983810d4c2d02542b5ce197dde2b870d
Alex Prokop <alex.prokop@gmail.com>
2017-08-08 18:12:28 -0400

    remove pry

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -18,7 +18,6 @@ class Certification < ActiveRecord::Base
 
     # We don't run sidekiq in development mode.
     if Rails.env.development? || Rails.env.test?
-      binding.pry
       StartCertificationJob.perform_now(self, RequestStore[:current_user], RequestStore[:current_user].ip_address)
     else
       StartCertificationJob.perform_later(self, RequestStore[:current_user], RequestStore[:current_user].ip_address)
