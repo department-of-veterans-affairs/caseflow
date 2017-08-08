48db15c1b525ec0938cc484f0a432a6fc8743f50
Alex Prokop <alex.prokop@gmail.com>
2017-08-08 09:21:41 -0400

    fix

--- app/jobs/middleware/job_request_store_middleware.rb
+++ app/jobs/middleware/job_request_store_middleware.rb
@@ -1,11 +1,11 @@
 # This middleware makes it safe to use RequestStore inside jobs by ensuring
 # that RequestStore is cleared after every job.
-# :nocov:
 class JobRequestStoreMiddleware
+# :nocov:
   def call(_worker, _msg, _queue)
     yield
   ensure
     ::RequestStore.clear! if defined?(::RequestStore)
   end
-end
 # :nocov:
+end
