72e7cd1ff26ec2fb7d6ec33cdac464ea9d818fed
Mike Ogren <michael_j_ogren@omb.eop.gov>
2017-08-07 21:21:03 -0400

    Using class variable for counts in retrieve docs job

--- app/jobs/retrieve_documents_for_reader_job.rb
+++ app/jobs/retrieve_documents_for_reader_job.rb
@@ -10,25 +10,25 @@ class RetrieveDocumentsForReaderJob < ActiveJob::Base
 
     # Args should be set in sidekiq_cron.yml, but default the limit to 1500 if they aren't
     limit = args["limit"] || DEFAULT_DOCUMENTS_DOWNLOADED_LIMIT
-    counts = { docs_successful: 0, docs_failed: 0, docs_attempted: 0, appeals_failed: 0, consecutive_failures: 0 }
+    @counts = { docs_successful: 0, docs_failed: 0, docs_attempted: 0, appeals_failed: 0, consecutive_failures: 0 }
 
     find_all_active_reader_appeals.each do |user, appeals|
       RequestStore.store[:current_user] = user
-      appeals.each { |appeal| fetch_docs_for_appeal(appeal, counts) }
+      appeals.each { |appeal| fetch_docs_for_appeal(appeal) }
 
-      break if counts[:docs_attempted] >= limit || counts[:consecutive_failures] >= 5
+      break if @counts[:docs_attempted] >= limit || @counts[:consecutive_failures] >= 5
     end
 
-    log_info(counts)
+    log_info
   end
 
-  def record_doc_outcome(doc, counts)
+  def record_doc_outcome(doc)
     if doc
-      counts[:docs_successful] += 1
-      counts[:consecutive_failures] = 0
+      @counts[:docs_successful] += 1
+      @counts[:consecutive_failures] = 0
     else
-      counts[:docs_failed] += 1
-      counts[:consecutive_failures] += 1
+      @counts[:docs_failed] += 1
+      @counts[:consecutive_failures] += 1
     end
   end
 
@@ -38,15 +38,15 @@ class RetrieveDocumentsForReaderJob < ActiveJob::Base
     end
   end
 
-  def fetch_docs_for_appeal(appeal, counts)
+  def fetch_docs_for_appeal(appeal)
     appeal.fetch_documents!(save: true).try(:each) do |doc|
-      counts[:docs_attempted] += 1
-      record_doc_outcome(fetch_document_content(doc), counts)
+      @counts[:docs_attempted] += 1
+      record_doc_outcome(fetch_document_content(doc))
     end
   rescue HTTPClient::KeepAliveDisconnected => e
     # VBMS connection may die when attempting to retrieve list of docs for appeal
-    counts[:appeals_failed] += 1
-    counts[:consecutive_failures] += 1
+    @counts[:appeals_failed] += 1
+    @counts[:consecutive_failures] += 1
     Rails.logger.error "Failed to retrieve appeal id #{appeal.id}:\n#{e.message}"
   end
 
@@ -60,11 +60,11 @@ class RetrieveDocumentsForReaderJob < ActiveJob::Base
     Rails.logger.error "Failed to retrieve #{doc.file_name}:\n#{e.message}"
   end
 
-  def log_info(counts)
-    output_msg = "RetrieveDocumentsForReaderJob successfully retrieved #{counts[:docs_successful]} documents " \
-          "and #{counts[:docs_failed]} document(s) failed. #{counts[:appeals_failed]} appeal(s) failed."
+  def log_info
+    output_msg = "RetrieveDocumentsForReaderJob successfully retrieved #{@counts[:docs_successful]} documents " \
+          "and #{@counts[:docs_failed]} document(s) failed. #{@counts[:appeals_failed]} appeal(s) failed."
 
-    output_msg = "\nJob stopped after #{counts[:consecutive_failures]} failures" if counts[:consecutive_failures] >= 5
+    output_msg = "\nJob stopped after #{@counts[:consecutive_failures]} failures" if @counts[:consecutive_failures] >= 5
 
     Rails.logger.info output_msg
     SlackService.new(url: slack_url).send_notification(output_msg)
