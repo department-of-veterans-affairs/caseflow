d4083365fcfc097e43996b564dc6d10357c73347
Vovk <vhaisavovka@VACOLTVOVKA1.local>
2017-08-13 08:37:20 -0400

    docs for tests

--- spec/feature/switch_user_spec.rb
+++ spec/feature/switch_user_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.feature "Switch User" do
+RSpec.feature "Test Users for Demo" do
   before do
-    # Switch user only works in demo deploy env
-    ENV["DEPLOY_ENV"] = "dev"
+    # Switch user only works in demo
+    ENV["DEPLOY_ENV"] = "demo"
     BGSService.end_product_data = BGSService.all_grants
 
     User.create(station_id: "283", css_id: User::FUNCTIONS.sample)
@@ -12,7 +12,7 @@ RSpec.feature "Switch User" do
     User.authenticate!
   end
 
-  scenario "We can switch between users in the database in dev mode" do
+  scenario "We can switch between users in the database in demo mode" do
     visit "test/users"
     expect(page).not_to have_content("123 (DSUSER)")
     expect(page).to have_content("DSUSER")
@@ -22,7 +22,8 @@ RSpec.feature "Switch User" do
     expect(page).not_to have_content("123 (DSUSER)")
   end
 
-  scenario "We can switch between test EP data in dev mode" do
+  # Dispatch-speific seeding
+  scenario "We can switch between test EP data in demo mode" do
     visit "test/users"
     page.first(:xpath, "//button[@id='main-tab-1']").click
     page.first(:xpath, "//button[@id='button-Seed-all-grants']").click
