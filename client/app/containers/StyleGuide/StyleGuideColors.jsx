5659dcf64c90287744de257ae0dfcebc4afbbf8a
Sjones352 <sjones@tistatech.com>
2017-08-14 12:33:10 -0400

    Connects to Style guide | Add new colors (:#2767)

--- client/app/containers/StyleGuide/StyleGuideColors.jsx
+++ client/app/containers/StyleGuide/StyleGuideColors.jsx
@@ -24,12 +24,11 @@ export default class StyleGuideColors extends React.Component {
     Dispatch: '#844e9f',
     'eFolder Express': '#F0835e',
     Feedback: '#73e5d4',
-    Certification: '#459fd7',
     Reader: '#417505',
-    'Hearing': '#fdc231', 
-    'Procedural': '#5a94ec', 
-    'Medical': '#ff6868', 
-    'Certification': '#3ad2cf'
+    Hearing: '#fdc231',
+    Procedural: '#5a94ec',
+    Medical: '#ff6868',
+    Certification: '#3ad2cf'
   };
 
   Primary = [
@@ -70,10 +69,10 @@ export default class StyleGuideColors extends React.Component {
   ];
 
   Reader = [
-  'Hearing',
-  'Procedural',
-  'Medical',
-  'Certification'
+    'Hearing',
+    'Procedural',
+    'Medical',
+    'Certification'
   ];
 
   colorVar = (name) => {
