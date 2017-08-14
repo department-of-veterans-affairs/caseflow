090873763b38123b2aa2dcc198ed455246f60ced
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-08-14 14:58:41 -0400

    adding constants state

--- client/app/hearings/reducers/index.js
+++ client/app/hearings/reducers/index.js
@@ -56,8 +56,29 @@ export const hearingsReducers = function(state = mapDataToInitialState(), action
   case Constants.SET_TRANSCRIPT_REQUESTED:
     return newHearingState(state, action, { transcript_requested: { $set: action.payload.transcriptRequested } });
 
+  case Constants.SET_CONTENTIONS:
+    return update(state, {
+      worksheet: { contentions: { $set: action.payload.contentions } }
+    });
+
+  case Constants.SET_WORKSHEET_PERIODS:
+    return update(state, {
+      worksheet: { worksheetPeriods: { $set: action.payload.evidence } }
+    });
+
+  case Constants.SET_EVIDENCE:
+    return update(state, {
+      worksheet: { evidence: { $set: action.payload.evidence } }
+    });
+
+  case Constants.SET_WORKSHEET_COMMENTS:
+    return update(state, {
+      worksheet: { worksheetComments: { $set: action.payload.worksheetComments } }
+    });
+
   default: return state;
   }
 };
+
 
 export default hearingsReducers;
