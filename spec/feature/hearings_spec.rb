341de4d91d13a20197aa854543a71dbf3305d43c
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-08-15 08:29:18 -0400

    revert url

--- spec/feature/hearings_spec.rb
+++ spec/feature/hearings_spec.rb
@@ -82,7 +82,7 @@ RSpec.feature "Hearings" do
     end
 
     scenario "Shows a hearing worksheet" do
-      visit "/hearings/worksheets/25f4d2d26"
+      visit "/hearings/dockets/2017-01-06"
 
       link = find(".cf-hearings-docket-appellant", match: :first).find("a")
       link_text = link.text
