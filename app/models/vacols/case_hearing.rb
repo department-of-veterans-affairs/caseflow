115154f703bd87bb21553793db643b4a8971b331
JD Pagano <jonathanpagano@gmail.com>
2017-08-15 13:43:22 -0400

    Remove references to date_diff
    - Previous PR removed the date_diff parameter but failed to remove all
      references

--- app/models/vacols/case_hearing.rb
+++ app/models/vacols/case_hearing.rb
@@ -41,11 +41,8 @@ class VACOLS::CaseHearing < VACOLS::Record
     vdkey is NOT NULL
   ).freeze
 
-  WITHOUT_DISPOSITION_OR_AFTER_DATE = %{
-    hearing_date >= to_date(?, 'YYYY-MM-DD HH24:MI')
-    -- Hearing is after a provided date (a recent hearing)
-
-    OR hearing_disp IS NULL
+  WITHOUT_DISPOSITION = %{
+    hearing_disp IS NULL
     -- an older hearing still awaiting a disposition
   }.freeze
 
@@ -59,8 +56,7 @@ class VACOLS::CaseHearing < VACOLS::Record
 
       select_hearings
         .where("staff.sdomainid = #{id}")
-        .where(WITHOUT_DISPOSITION_OR_AFTER_DATE,
-               relative_vacols_date(date_diff).to_formatted_s(:oracle_date))
+        .where(WITHOUT_DISPOSITION)
         .where(NOT_MASTER_RECORD)
     end
 
