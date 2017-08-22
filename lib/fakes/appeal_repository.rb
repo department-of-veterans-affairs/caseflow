1662d7fd852ca0988d4e892a87306765f712d7d7
Teja <teja.s.naraparaju@gmail.com>
2017-08-22 12:10:35 -0400

    Adding the appeal api call to fetch appeals (#3005)
    Adding the appeal api call to fetch appeals using veteran id

--- lib/fakes/appeal_repository.rb
+++ lib/fakes/appeal_repository.rb
@@ -430,6 +430,32 @@ class Fakes::AppealRepository
       issues: [Generators::Issue.build(vacols_id: "reader_id1")],
       documents: redacted_reader_documents
     )
+    Generators::Appeal.build(
+      vacols_id: "reader_id4",
+      vbms_id: "DEMO123",
+      vacols_record: {
+        template: :ready_to_certify,
+        veteran_first_name: "Joe",
+        veteran_last_name: "Smith",
+        type: "Court Remand",
+        cavc: true,
+        date_assigned: "2013-05-17 00:00:00 UTC".to_datetime,
+        date_received: "2013-05-31 00:00:00 UTC".to_datetime,
+        signed_date: nil,
+        docket_number: "13 11-265",
+        regional_office_key: "RO13"
+      },
+      issues: [Generators::Issue.build(vacols_id: "reader_id1"),
+               Generators::Issue.build(disposition: "Osteomyelitis",
+                                       levels: ["Osteomyelitis"],
+                                       description: [
+                                         "15 - Compensation",
+                                         "26 - Osteomyelitis"
+                                       ],
+                                       program_description: "06 - Medical",
+                                       vacols_id: "reader_id2")],
+      documents: static_reader_documents
+    )
   end
 
   def self.aod(_vacols_id)
