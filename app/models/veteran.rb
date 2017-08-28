8354bab6e870369bb46a33d25259fe3614b684ea
JD Pagano <joofsh@users.noreply.github.com>
2017-08-28 17:12:30 -0400

    Update hearing to_hash with additional fields Part 1 (#3090)
    Load new attributes from appeal repository for veteran date_of_birth, and appellant city & state. Pass these new attributes in the hearing to_hash method

--- app/models/veteran.rb
+++ app/models/veteran.rb
@@ -13,6 +13,7 @@ class Veteran
   ).freeze
 
   attr_accessor(*BGS_ATTRIBUTES)
+  attr_accessor :date_of_birth
 
   COUNTRIES_REQUIRING_ZIP = %w(USA CANADA).freeze
 
@@ -46,6 +47,14 @@ class Veteran
 
   def self.bgs
     BGSService.new
+  end
+
+  def age
+    return unless date_of_birth
+    dob = date_of_birth
+    # Age calc copied from https://stackoverflow.com/a/2357790
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
 
   private
