feb21d91aab5d48e0101e9a9c467cbf86bbd7dfc
Shade L. Jenifer <shade.jenifer@pro-spheretek.com>
2017-08-29 14:38:23 -0400

    Revert actions/Dockets.js to version in commit bcf2f05

--- client/app/hearings/actions/Dockets.js
+++ client/app/hearings/actions/Dockets.js
@@ -21,137 +21,73 @@ export const handleServerError = (err) => ({
   }
 });
 
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-eval */
-export let lastChangeTimestamp = null;
+export const onRepNameChange = (repName) => ({
+  type: Constants.SET_REPNAME,
+  payload: {
+    repName
+  }
+});
 
-export const updatedHearing = (action, milliseconds = 1000) =>
-  (dispatch, getState) => {
+export const onWitnessChange = (witness) => ({
+  type: Constants.SET_WITNESS,
+  payload: {
+    witness
+  }
+});
 
-    dispatch(action);
+export const setNotes = (hearingIndex, notes, date) => ({
+  type: Constants.SET_NOTES,
+  payload: {
+    hearingIndex,
+    notes,
+    date
+  }
+});
 
-    lastChangeTimestamp = Number(new Date());
+export const setDisposition = (hearingIndex, disposition, date) => ({
+  type: Constants.SET_DISPOSITION,
+  payload: {
+    hearingIndex,
+    disposition,
+    date
+  }
+});
 
-    // create a function that, after a moment (default 1000ms),
-    // checks if no subsequent actions have been called updatedHearing().
-    // If true (meaning the time at which the function runs matches
-    // the time at which it was created), save the new data.
+export const setHoldOpen = (hearingIndex, holdOpen, date) => ({
+  type: Constants.SET_HOLD_OPEN,
+  payload: {
+    hearingIndex,
+    holdOpen,
+    date
+  }
+});
 
-    eval(`
-      setTimeout(() => {
-        if (${lastChangeTimestamp} === lastChangeTimestamp) {
-          dispatch({type:'TOGGLE_SAVING'});
-          saveData(getState().dockets, dispatch);
-        }
-      }, ${milliseconds});`
-    );
-    // filter out edited:true + pass that to saveData()?
+export const setAod = (hearingIndex, aod, date) => ({
+  type: Constants.SET_AOD,
+  payload: {
+    hearingIndex,
+    aod,
+    date
+  }
+});
 
-  };
-/* eslint-enable no-unused-vars */
-/* eslint-enable no-eval */
+export const setAddOn = (hearingIndex, addOn, date) => ({
+  type: Constants.SET_ADD_ON,
+  payload: {
+    hearingIndex,
+    addOn,
+    date
+  }
+});
 
-/* eslint-disable no-unused-vars */
-export const saveData = (data, dispatch, url = '/hearings/save_data') => {
-  // ApiUtil.put(url, { data }).
-  //   then(
-  //     () => dispatch({
-  //       type: 'TOGGLE_SAVING'
-  //     }),
-  //     (err) => {
-  //       dispatch(handleServerError(err));
-  //     }
-  //   );
-  // For now, assume the PUT request gets a successful response in 1 second
-  setTimeout(() => {
-    dispatch({
-      type: 'TOGGLE_SAVING'
-    });
-  }, 1000);
-};
-/* eslint-enable no-unused-vars */
-
-
-export const setNotes = (hearingIndex, notes, date) => {
-  return updatedHearing({
-    type: Constants.SET_NOTES,
-    payload: {
-      hearingIndex,
-      notes,
-      date
-    }
-  });
-};
-
-export const setDisposition = (hearingIndex, disposition, date) => {
-  return updatedHearing({
-    type: Constants.SET_DISPOSITION,
-    payload: {
-      hearingIndex,
-      disposition,
-      date
-    }
-  });
-};
-
-export const setHoldOpen = (hearingIndex, holdOpen, date) => {
-  return updatedHearing({
-    type: Constants.SET_HOLD_OPEN,
-    payload: {
-      hearingIndex,
-      holdOpen,
-      date
-    }
-  });
-};
-
-export const setAod = (hearingIndex, aod, date) => {
-  return updatedHearing({
-    type: Constants.SET_AOD,
-    payload: {
-      hearingIndex,
-      aod,
-      date
-    }
-  });
-};
-
-export const setAddOn = (hearingIndex, addOn, date) => {
-  return updatedHearing({
-    type: Constants.SET_ADD_ON,
-    payload: {
-      hearingIndex,
-      addOn,
-      date
-    }
-  });
-};
-
-export const setTranscriptRequested = (hearingIndex, transcriptRequested, date) => {
-  return updatedHearing({
-    type: Constants.SET_TRANSCRIPT_REQUESTED,
-    payload: {
-      hearingIndex,
-      transcriptRequested,
-      date
-    }
-  });
-};
-
-/* eslint-disable no-unused-vars */
-export const saveHearingsBeforeWindowCloses = () => (dispatch, getState) => {
-  const dockets = getState().dockets;
-  // filter out edited:true + send only those to server?
-  // maybe not here...could take too long.  idk...
-
-  // The PUT request doesn't matter right now
-  // ApiUtil.put(
-  //     '/hearings/save_data', { data: dockets }
-  //   ).then(
-  //     // w/o then(), PUT request seems to fail
-  //   );
-};
-/* eslint-enable no-unused-vars */
+export const setTranscriptRequested = (hearingIndex, transcriptRequested, date) => ({
+  type: Constants.SET_TRANSCRIPT_REQUESTED,
+  payload: {
+    hearingIndex,
+    transcriptRequested,
+    date
+  }
+});
 
 export const onContentionsChange = (contentions) => ({
   type: Constants.SET_CONTENTIONS,
