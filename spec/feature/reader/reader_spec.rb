18216dbeb60bd0dc3494c38466f8c6ef6f78c457
Sharon Warner <sharon.warner56@gmail.com>
2017-08-29 17:33:05 -0400

    Feature test for case_summary page

--- spec/feature/reader/reader_spec.rb
+++ spec/feature/reader/reader_spec.rb
@@ -265,6 +265,12 @@ RSpec.feature "Reader" do
       expect(page).to have_content("Reader Help")
     end
 
+    scenario "User visits case_summary page" do
+      visit "/reader/appeal/#{appeal.vacols_id}/documents/case-summary"
+      expect(page).to have_content("Filtering by:")
+      expect(page).to have_content("Categories (1)")
+    end
+
     scenario "Clicking outside pdf or next pdf removes annotation mode" do
       visit "/reader/appeal/#{appeal.vacols_id}/documents/2"
       add_comment_without_clicking_save("text")
