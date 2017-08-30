e28438dbd05f4c030c54e5707f540f56e4adda9c
Alex Prokop <alex.prokop@gmail.com>
2017-08-30 10:18:35 -0400

    adds date field back in

---
+++ app/assets/javascripts/application/date_field.js
@@ -0,0 +1,40 @@
+//= require jquery
+//= require jquery.maskedinput
+
+(function(){
+  function zeroPadLeft(number) {
+    var length = 2,
+        paddedNumber = number.toString();
+
+    for(var i=0; i<length; i++) {
+      paddedNumber = '0' + paddedNumber;
+    }
+
+    return paddedNumber.substr(paddedNumber.length - length);
+  }
+
+  window.DateField =  {
+    init: function(){
+      var self = this;
+
+      $('input[type=date]').attr('type','text').each(function(index, input){
+        // reformat value from ISO to masked pattern
+        var $input = $(input);
+        $input.val(self.formatDate($input.val()));
+      }).mask("99/99/9999",{ placeholder: "mm/dd/yyyy" });
+    },
+
+    formatDate: function(dashDateStr) {
+      var date = new Date(Date.parse(dashDateStr));
+      var dateParts = [];
+
+      if(date.toString() === "Invalid Date") { return ""; }
+
+      dateParts[0] = zeroPadLeft(date.getUTCMonth()+1);
+      dateParts[1] = zeroPadLeft(date.getUTCDate());
+      dateParts[2] = date.getFullYear();
+
+      return dateParts.join('/');
+    }
+  };
+})();
