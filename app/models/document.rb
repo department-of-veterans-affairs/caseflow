33ec9defa0db6993a92fa86b6047ae0b76209fd3
Alex Prokop <alex.prokop@gmail.com>
2017-09-01 09:38:48 -0400

    adds current_user

--- app/models/document.rb
+++ app/models/document.rb
@@ -183,7 +183,7 @@ class Document < ActiveRecord::Base
   def content_url
     if EFolderService == ExternalApi::EfolderService &&
        RequestStore.store[:application] == "reader" &&
-       FeatureToggle.enabled?(:efolder_docs_api)
+       FeatureToggle.enabled?(:efolder_docs_api, RequestStore.store[:current_user])
       URI(ExternalApi::EfolderService.efolder_base_url + "/api/v1/documents/#{efolder_id}").to_s
     else
       "/document/#{id}/pdf"
