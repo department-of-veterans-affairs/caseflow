31c7b50addebc0c1a37d2e794915c1498690effa
Sunil Sadasivan <sunil@navahq.com>
2017-09-03 12:28:02 -0400

    Add simple spec tests for jobs endpoint

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe JobsController, type: :controller do
+  let!(:current_user) { User.authenticate! }
 
+  before(:each) do
+    request.headers["Authorization"] = "Token #{Rails.application.secrets.jobs_auth_token}"
+  end
+
+  describe "POST job async_start" do
+    it "should not be successful due to unauthorized request" do
+      # set up the wrong token
+      request.headers["Authorization"] = "BADTOKEN"
+      post :start_async, {"job_type": "UndefinedJob"}
+      expect(response.status).to eq 401
+    end
+
+    it "should not be successful due to unrecognized job" do
+      post :start_async, {"job_type": "UndefinedJob"}
+      expect(response.status).to eq 422
+    end
+
+    it "should successfully start HeartbeatTasksJob asynchronously" do
+      post :start_async, {"job_type": "HeartbeatTasksJob"}
+      expect(response.status).to eq 200
+      expect(response_body["job_id"]).not_to be_empty
+    end
+  end
+
+  def response_body
+    JSON.parse(response.body)
+  end
 end
