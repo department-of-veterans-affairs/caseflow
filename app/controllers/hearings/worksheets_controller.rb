7e61033806a145e737d81f1586927aaf3903d8a3
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-09-04 20:54:19 -0400

    linting rail

--- app/controllers/hearings/worksheets_controller.rb
+++ app/controllers/hearings/worksheets_controller.rb
@@ -43,8 +43,8 @@ class Hearings::WorksheetsController < HearingsController
         appeal_0: {
           issues: {
             issue_0: {
-              program: 'Compensation',
-              description: 'Left Elbow',
+              program: "Compensation",
+              description: "Left Elbow",
             }
           },
           nod: 99,
