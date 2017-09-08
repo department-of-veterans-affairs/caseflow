62ca1d91d4f75fcbd9060b385088c1f99c31d0ba
Sunil Sadasivan <sunil@navahq.com>
2017-09-08 11:08:15 -0400

    Make jobs whitelist a global constant.

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::JobsController < Api::V1::ApplicationController
-  rescue_from NameError, TypeError, with: :unrecognized_job
+
+  # available jobs supported by this endpoint
+  SUPPORTED_JOBS = {
+    "heartbeat" => HeartbeatTasksJob,
+    "create_establish_claim" => CreateEstablishClaimTasksJob,
+    "prepare_establish_claim" => PrepareEstablishClaimTasksJob
+  }
+
 
   def start_async
-    # available jobs supported by this endpoint
-    @jobs = {
-      "heartbeat": HeartbeatTasksJob,
-      "create_establish_claim": CreateEstablishClaimTasksJob,
-      "prepare_establish_claim": PrepareEstablishClaimTasksJob
-    }
-
     # start job asynchronously as given by the job_type post param
-    job_type = params.require(:job_type)
-    job = @jobs[:"#{job_type}"].perform_later
+    job = SUPPORTED_JOBS[params.require(:job_type)]
+    return unrecognized_job unless job
+
+    job = job.perform_later
     Rails.logger.info("Starting job #{job.job_id}")
     render json: { success: true, job_id: job.job_id }, status: 200
   end
