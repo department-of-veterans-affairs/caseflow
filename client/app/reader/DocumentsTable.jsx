fe9e01f76a6d24b009eb618185d0c58cd7583d3f
Sunil Sadasivan <sunil@navahq.com>
2017-09-11 13:56:27 -0400

    Ensure comments listed in document table is listed by page order

--- client/app/reader/DocumentsTable.jsx
+++ client/app/reader/DocumentsTable.jsx
@@ -264,7 +264,7 @@ class DocumentsTable extends React.Component {
 
       return [{
         valueFunction: (doc) => {
-          const comments = this.props.annotationsPerDocument[doc.id];
+          const comments = _.sortBy(this.props.annotationsPerDocument[doc.id], 'page');
           const commentNodes = comments.map((comment, commentIndex) => {
             return <Comment
               key={comment.uuid}
