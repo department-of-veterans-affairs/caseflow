d2cf62b5670b0982aece07f647bad7c3a2be8e30
Teja N <teja.s.naraparaju@gmail.com>
2017-09-20 08:01:16 -0400

    Renaming spec

--- spec/requests/appeal_controller_spec.rb
+++ spec/requests/appeal_controller_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe "Widget management", type: :request do
+RSpec.describe "Reader Appeal Requests", type: :request do
   let!(:user) { User.authenticate!(roles: ["Reader"]) }
   let(:vacols_record) { :remand_decided }
   let(:appeal) { Generators::Appeal.build(vbms_id: "123456789S", vacols_record: vacols_record) }
 
-  describe "Health Check API" do
+  describe "Appeals Find by Veteran ID Endpoint" do
     let(:headers) { { "HTTP_VETERAN_ID": "111225555S" } }
 
     it "returns not found" do
@@ -11,7 +11,7 @@ RSpec.describe "Widget management", type: :request do
       get "/reader/appeal/veteran-id", nil, headers
       expect(response).to have_http_status(:not_found)
 
-      headers["HTTP_VETERAN_ID"] = "221121212121212222222C"
+      headers["HTTP_VETERAN_ID"] = "22112121xs"
       get "/reader/appeal/veteran-id", nil, headers
       expect(response).to have_http_status(:not_found)
     end
