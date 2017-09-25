4089a7885e093e95bd465b4069881d07e0699571
Sunil Sadasivan <sunil@navahq.com>
2017-09-25 16:59:37 -0400

    Update retry intervals for a bit more exponential backoff

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,6 +1,6 @@
 # set up default exponential backoff parameters
 ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
-  .shoryuken_options(retry_intervals: [3.seconds, 1.minute, 5.minutes, 30.minutes, 1.hour, 4.hours])
+  .shoryuken_options(retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours])
 
 Shoryuken.configure_server do |config|
   # placeholder for shoryuken server middleware
