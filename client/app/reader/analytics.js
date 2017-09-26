3c7387d1ec893cdc59a9d64426fa8546d88b9d05
Nick Heiner <NickHeiner@users.noreply.github.com>
2017-09-26 13:19:26 -0400

    #2865: Add page timings (#3332)
    This starts with Reader. We can roll it out to more apps as we see how it's used.
    
    * Set site speed sample rate to 100
    * Instrument HTTP requests

--- client/app/reader/analytics.js
+++ client/app/reader/analytics.js
@@ -16,6 +16,18 @@ export const INTERACTION_TYPES = {
   KEYBOARD_SHORTCUT: 'keyboard-shortcut'
 };
 
+export const ENDPOINT_NAMES = {
+  DOCUMENT: 'document',
+  ANNOTATION: 'annotation',
+  MARK_DOC_AS_READ: 'mark-doc-as-read',
+  TAG: 'tag',
+  APPEAL_DETAILS: 'appeal-details',
+  APPEAL_DETAILS_BY_VET_ID: 'appeal-details-by-vet-id',
+  CLAIMS_FOLDER_SEARCHES: 'claims-folder-searches',
+  DOCUMENTS: 'documents',
+  DOCUMENT_CONTENT: 'document-content'
+};
+
 const debounceFns = {};
 
 export const reduxAnalyticsMiddleware = (store) => (next) => (action) => {
