4ecd66b99e295b409a6840b91362654c723ef8a4
Sunil Sadasivan <sunil@navahq.com>
2017-09-26 15:19:30 -0400

    Adjust test cases to handle new call parameters

--- spec/jobs/job_prometheus_metric_middleware_spec.rb
+++ spec/jobs/job_prometheus_metric_middleware_spec.rb
@@ -2,10 +2,11 @@ describe JobPrometheusMetricMiddleware do
   before do
     @middleware = JobPrometheusMetricMiddleware.new
 
+    @body = {
+      "job_class" => "FunTestJob"
+    }
     @msg = {
-      "args" => [{
-        "job_class" => "FunTestJob"
-      }]
+      "args" => [ @body ]
     }
     @yield_called = false
     allow(PrometheusService).to receive(:push_metrics!).and_return(nil)
@@ -13,7 +14,7 @@ describe JobPrometheusMetricMiddleware do
   end
 
   context ".call" do
-    let(:call) { @middleware.call(nil, @msg, :default) { @yield_called = true } }
+    let(:call) { @middleware.call(nil, :low_priority, @msg, @body) { @yield_called = true } }
 
     it "always increments attempts counter" do
       expect(PrometheusService.background_jobs_attempt_counter.values[@labels]).to eq(nil)
@@ -26,7 +27,7 @@ describe JobPrometheusMetricMiddleware do
     it "increments error counter on error" do
       expect(PrometheusService.background_jobs_error_counter.values[@labels]).to eq(nil)
       expect do
-        @middleware.call(nil, @msg, :default) { fail("test") }
+        @middleware.call(nil, :low_priority, @msg, @body) { fail("test") }
       end.to raise_error
 
       expect(PrometheusService.background_jobs_error_counter.values[@labels]).to eq(1)
