35fb3a70c6cb36ee2d18eb7623397109645fc9df
Mark Benjamin <mdbenjam@gmail.com>
2017-09-26 16:30:21 -0400

    Adding breadcrumbs (#3344)
    Adding a generic way to implement breadcrumbs in our navbar. To auto-generate breadcrumbs you need to do the following:
    
    Make either Route or PageRoute objects the children of the NavigationBar.
    Give each of those Routes/PageRoutes a breadcrumb attribute that has the name of the breadcrumb.
    Order the routes so that sub pages come after higher level pages.

---
+++ client/app/components/Breadcrumbs.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from './Link';
+import { Route } from 'react-router-dom';
+
+const getElementsWithBreadcrumbs = (element) => {
+  if (!element.props.children) {
+    return [];
+  }
+
+  return React.Children.toArray(element.props.children).reduce((acc, child) => {
+    if (child.props.breadcrumb) {
+      return [...acc, {
+        path: child.props.path,
+        breadcrumb: child.props.breadcrumb
+      }];
+    }
+
+    return [...acc, ...getElementsWithBreadcrumbs(child)];
+  }, []);
+};
+
+// When passed a child component with Route or PageRoute objects that have
+// breadcrumb properties set, this component will generate breadcrumbs as links
+// to those other routes.
+export default class Breadcrumbs extends React.Component {
+  render() {
+    const breadcrumbComponents = getElementsWithBreadcrumbs(this).map(
+    (route) => <Route key={route.breadcrumb} path={route.path} render={
+      (props) => <span>
+          <h2 id="page-title" className="cf-application-title">&nbsp; > &nbsp;</h2>
+          <Link id="cf-logo-link" to={props.match.url}>
+            <h2 id="page-title" className="cf-application-title">{route.breadcrumb}</h2>
+          </Link>
+        </span>
+      } />
+    );
+
+    return <span>{breadcrumbComponents}</span>;
+  }
+}
+
+Breadcrumbs.propTypes = {
+  children: PropTypes.node
+};
