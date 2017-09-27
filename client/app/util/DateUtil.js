510ab66a8147598e0ece4ff281d4c8a2b0d9f0d2
Sharon Warner <sharon.warner56@gmail.com>
2017-09-27 12:13:31 -0400

    Handle empty dates

--- client/app/util/DateUtil.js
+++ client/app/util/DateUtil.js
@@ -7,6 +7,10 @@ const ZERO_INDEX_MONTH_OFFSET = 1;
 export const dateFormatString = 'MM/DD/YYYY';
 
 export const formatDate = function(dateString) {
+  if (!dateString) {
+    return;
+  }
+
   let date = new Date(dateString);
   let month = StringUtil.leftPad(date.getMonth() + ZERO_INDEX_MONTH_OFFSET, 2, '0');
   let day = StringUtil.leftPad(date.getDate(), 2, '0');
