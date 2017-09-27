15db12cf2a0f3d5f1c33e06d06aa775a21c8c3a2
Nick Heiner <nth23@cornell.edu>
2017-09-27 16:22:15 -0400

    Adding more skeletal structure

--- client/app/intake/IntakeFrame.jsx
+++ client/app/intake/IntakeFrame.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+import Footer from '../components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import PageRoute from '../components/PageRoute';
+import First from './pages/first';
 
 export default class IntakeFrame extends React.PureComponent {
   render() {
-    return <h1>INTAKE FUCK YEAH DUDE</h1>;
+    const appName = 'Intake';
+
+    const Router = this.props.router || BrowserRouter;
+
+    return <Router  basename="/intake" {...this.props.routerTestProps}>
+      <div>
+        <NavigationBar
+          appName={appName}
+          userDisplayName={this.props.userDisplayName}
+          dropdownUrls={this.props.dropdownUrls}
+          defaultUrl="/"
+        >
+          <PageRoute
+            exact
+            path="/"
+            title="Welcome | Caseflow Intake"
+            render={() => <First />}/>
+        </NavigationBar>
+        <Footer
+          appName={appName}
+          feedbackUrl={this.props.feedbackUrl}
+          buildDate={this.props.buildDate}
+        />
+      </div>
+    </Router>;
   }
 }
