fc4ee56a9150f0d34b796e1811c4612328b50918
Nick Heiner <nth23@cornell.edu>
2017-09-27 18:10:42 -0400

    adding a strong label option to TextField

--- client/app/intake/pages/review.jsx
+++ client/app/intake/pages/review.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import RadioField from '../../components/RadioField';
+import DateSelector from '../../components/DateSelector';
 
 export default class Review extends React.PureComponent {
+  onElectionChange = () => {
+    // eslint-disable-next-line no-console
+    console.log('not yet implemented');
+  }
+
   render() {
     const radioOptions = [
       { displayText: 'Supplemental Claim' },
@@ -10,7 +16,6 @@ export default class Review extends React.PureComponent {
       { displayText: 'Withdraw all pending appeals' }
     ];
 
-
     return <div>
       <h1>Review Joe Snuffy's opt-in request</h1>
       <p>Check the Veteran's RAMP Opt-In Election form in the Centralized Portal.</p>
@@ -18,7 +23,9 @@ export default class Review extends React.PureComponent {
         name="opt-in-election"
         label={<h3>Which election did the Veteran select?</h3>}
         options={radioOptions}
+        onChange={this.onElectionChange}
       />
+      <DateSelector name="receipt-date" label="What is the Receipt Date for this election form?" strongLabel />
     </div>;
   }
 }
