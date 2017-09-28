013853eda9a6e11f0edecfb6a6f6e22102370141
Nick Heiner <nth23@cornell.edu>
2017-09-28 17:40:53 -0400

    Gate users to those who are authorized

--- app/controllers/intake_controller.rb
+++ app/controllers/intake_controller.rb
@@ -1,4 +1,14 @@
 class IntakeController < ApplicationController
+  before_action :verify_access, :react_routed
+  
+  def set_application
+    RequestStore.store[:application] = "intake"
+  end
+
+  def verify_access
+    verify_authorized_roles("Intake")
+  end
+
   def index
     respond_to do |format|
       format.html { render(:index) }
