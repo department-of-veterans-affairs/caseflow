a2dd13dcd2c96d85d89694181e02ac732fea5709
Nick Heiner <nth23@cornell.edu>
2017-09-28 12:56:06 -0400

    Making progress bar sections active

--- client/app/intake/IntakeFrame.jsx
+++ client/app/intake/IntakeFrame.jsx
@@ -5,12 +5,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import PageRoute from '../components/PageRoute';
 import AppFrame from '../components/AppFrame';
 import AppSegment from '../components/AppSegment';
-import ProgressBar from '../components/ProgressBar';
+import IntakeProgressBar from './components/IntakeProgressBar';
 import PrimaryAppContent from '../components/PrimaryAppContent';
 import BeginPage from './pages/begin';
 import ReviewPage, { ReviewNextButton } from './pages/review';
 import FinishPage, { FinishNextButton } from './pages/finish';
 import CompletedPage, { CompletedNextButton } from './pages/completed';
+import { PAGE_PATHS } from './constants';
 
 export default class IntakeFrame extends React.PureComponent {
   render() {
@@ -18,57 +19,49 @@ export default class IntakeFrame extends React.PureComponent {
 
     const Router = this.props.router || BrowserRouter;
 
-    const progressBarSections = [
-      { title: '1. Begin Intake' },
-      { title: '2. Review Request' },
-      { title: '3. Finish Processing' },
-      { title: '4. Confirmation' }
-    ];
-
     return <Router basename="/intake" {...this.props.routerTestProps}>
       <div>
         <NavigationBar
           appName={appName}
           userDisplayName={this.props.userDisplayName}
           dropdownUrls={this.props.dropdownUrls}
-          defaultUrl="/"
-        >
+          defaultUrl="/">
           <AppFrame>
-            <ProgressBar sections={progressBarSections} />
+            <IntakeProgressBar />
             <PrimaryAppContent>
               <PageRoute
                 exact
-                path="/"
+                path={PAGE_PATHS.BEGIN}
                 title="Begin Intake | Caseflow Intake"
                 component={BeginPage} />
               <PageRoute
                 exact
-                path="/review-request"
+                path={PAGE_PATHS.REVIEW}
                 title="Review Request | Caseflow Intake"
                 component={ReviewPage} />
               <PageRoute
                 exact
-                path="/finish"
+                path={PAGE_PATHS.FINISH}
                 title="Finish | Caseflow Intake"
                 component={FinishPage} />
               <PageRoute
                 exact
-                path="/completed"
+                path={PAGE_PATHS.COMPLETED}
                 title="Completed | Caseflow Intake"
                 component={CompletedPage} />
             </PrimaryAppContent>
             <AppSegment className="cf-workflow-button-wrapper">
               <Route
                 exact
-                path="/review-request"
+                path={PAGE_PATHS.REVIEW}
                 component={ReviewNextButton} />
               <Route
                 exact
-                path="/finish"
+                path={PAGE_PATHS.FINISH}
                 component={FinishNextButton} />
               <Route
                 exact
-                path="/completed"
+                path={PAGE_PATHS.COMPLETED}
                 component={CompletedNextButton} />
             </AppSegment>
           </AppFrame>
---
+++ client/app/intake/components/IntakeProgressBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import ProgressBar from '../../components/ProgressBar';
+import { PAGE_PATHS } from '../constants';
+import _ from 'lodash';
+
+class IntakeProgressBarInner extends React.PureComponent {
+  render() {
+    const progressBarSections = [
+      { 
+        title: '1. Begin Intake',
+        path: PAGE_PATHS.BEGIN 
+      },
+      { 
+        title: '2. Review Request',
+        path: PAGE_PATHS.REVIEW
+      },
+      { 
+        title: '3. Finish Processing',
+        path: PAGE_PATHS.FINISH 
+      },
+      { 
+        title: '4. Confirmation',
+        path: PAGE_PATHS.COMPLETED 
+      }
+    ];
+
+    const progressBarSectionsWithCurrentMarked = progressBarSections.map((section) =>
+      _({ current: section.path === this.props.location.pathname }).
+        merge(section).
+        omit('path').
+        value()
+    );
+
+    return <ProgressBar sections={progressBarSectionsWithCurrentMarked} />;
+  }
+}
+
+export default class IntakeProgressBar extends React.PureComponent {
+  render() {
+    return <Route path="/" component={IntakeProgressBarInner} />;
+  }
+}
