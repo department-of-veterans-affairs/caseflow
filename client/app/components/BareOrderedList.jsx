3fdb454f8e428c145a1507f961cb50a38a888b22
Nick Heiner <nth23@cornell.edu>
2017-09-28 12:59:33 -0400

    remove extra space

--- client/app/components/BareOrderedList.jsx
+++ client/app/components/BareOrderedList.jsx
@@ -5,7 +5,6 @@ export default class BareOrderedList extends React.PureComponent {
   render() {
     const className = classNames('cf-bare-list', this.props.className);
 
-
     return <ol className={className}>
       {
         this.props.items.map((itemFn, index) =>
