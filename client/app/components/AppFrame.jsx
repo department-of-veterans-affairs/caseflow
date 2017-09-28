37588f460483d56018c478e45e42dfe7f7e0db3c
Nick Heiner <nth23@cornell.edu>
2017-09-28 10:09:29 -0400

    syntax cleanup

--- client/app/components/AppFrame.jsx
+++ client/app/components/AppFrame.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export default class AppFrame extends React.PureComponent {
-  render() {
-    return <main className="cf-app-width">
+  render = () =>
+    <main className="cf-app-width">
       {this.props.children}
-    </main>;
-  }
+    </main>
 }
