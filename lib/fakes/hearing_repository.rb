c97ab96c50eddd2e4c5b7a839dfabb306f42c23f
Sharon Warner <sharon.warner56@gmail.com>
2017-10-01 11:38:40 -0400

    Removing unnecessary lint disabling

--- lib/fakes/hearing_repository.rb
+++ lib/fakes/hearing_repository.rb
@@ -62,7 +62,6 @@ class Fakes::HearingRepository
     2.times.each { |i| Generators::Hearings::MasterRecord.build(user: user, date: Time.zone.now + (i + 6).days) }
   end
 
-  # rubocop:disable Metrics/AbcSize
   def self.random_attrs(i)
     {
       vacols_record: OpenStruct.new(vacols_id: 950_330_575 + (i * 1465)),
@@ -77,5 +76,4 @@ class Fakes::HearingRepository
       transcript_requested: false
     }
   end
-  # rubocop:enable Metrics/AbcSize
 end
