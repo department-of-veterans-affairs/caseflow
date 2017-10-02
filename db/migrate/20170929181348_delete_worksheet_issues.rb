8f02b5b963738e95c2c4aaa3a32cf9c1e73eab4a
Anya Roltsch <aroltsch@users.noreply.github.com>
2017-10-02 09:05:11 -0400

    Fix migration warning (#3482)

--- db/migrate/20170929181348_delete_worksheet_issues.rb
+++ db/migrate/20170929181348_delete_worksheet_issues.rb
@@ -1,6 +1,6 @@
 class DeleteWorksheetIssues < ActiveRecord::Migration
   def change
     # Delete issues there were accidentally created by the Status API
-    WorksheetIssue.all.each(&:really_destroy!)
+    WorksheetIssue.find_each(&:really_destroy!)
   end
 end
