2e412c97d35197661ecc411b7e3bbeac223e52e2
Marian Lumba <mmlumba@gmail.com>
2017-10-02 16:14:20 -0400

    Reader accordion refactor pt 2 (#3493)
    Bind addNewTag and removeTag actions to SideBarIssueTags.jsx
    Move handleCategoryToggle function to actions.js
    Remove tagOptions from PdfSidebar

--- client/app/reader/SideBarIssueTags.jsx
+++ client/app/reader/SideBarIssueTags.jsx
@@ -1,16 +1,16 @@
 import React, { PureComponent } from 'react';
+import { bindActionCreators } from 'redux';
 import CannotSaveAlert from '../reader/CannotSaveAlert';
 import { connect } from 'react-redux';
 import SearchableDropdown from '../components/SearchableDropdown';
 import _ from 'lodash';
 
+import { addNewTag, removeTag } from '../reader/actions';
+
 class SideBarIssueTags extends PureComponent {
   render() {
     const {
-      doc,
-      tagOptions,
-      removeTag,
-      addNewTag
+      doc
     } = this.props;
 
     let generateOptionsFromTags = (tags) =>
@@ -28,9 +28,9 @@ class SideBarIssueTags extends PureComponent {
         const tagValue = _.first(deletedValue).label;
         const result = _.find(doc.tags, { text: tagValue });
 
-        removeTag(doc, result.id);
+        this.props.removeTag(doc, result.id);
       } else if (values && values.length) {
-        addNewTag(doc, values);
+        this.props.addNewTag(doc, values);
       }
     };
 
@@ -42,7 +42,7 @@ class SideBarIssueTags extends PureComponent {
         label="Select or tag issue(s)"
         multi={true}
         creatable={true}
-        options={generateOptionsFromTags(tagOptions)}
+        options={generateOptionsFromTags(this.props.tagOptions)}
         placeholder=""
         value={generateOptionsFromTags(doc.tags)}
         onChange={onChange}
@@ -54,10 +54,18 @@ class SideBarIssueTags extends PureComponent {
 
 const mapStateToProps = (state) => {
   return {
-    showErrorMessage: state.readerReducer.ui.pdfSidebar.showErrorMessage
+    showErrorMessage: state.readerReducer.ui.pdfSidebar.showErrorMessage,
+    ..._.pick(state.readerReducer.ui, 'tagOptions')
   };
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators({
+    addNewTag,
+    removeTag
+  }, dispatch)
+});
+
 export default connect(
-  mapStateToProps
+  mapStateToProps, mapDispatchToProps
 )(SideBarIssueTags);
