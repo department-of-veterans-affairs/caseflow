9e6446c8c1630b5bbee28d2d0992e6e9ef0e69c4
Nemo Djuric <nemo.djuric@gmail.com>
2017-10-04 13:33:43 -0400

    Refactored form8_pdf_service and spec test. Fixed typo in save_certification_spec

--- app/services/form8_pdf_service.rb
+++ app/services/form8_pdf_service.rb
@@ -11,80 +11,6 @@ class Form8PdfService
   FIELD_PAGES = Hash.new PDF_PAGE_1
   FIELD_PAGES[:remarks_continued] = PDF_PAGE_2
 
-  FIELD_LOCATIONS_FORM8_V1 = {
-    appellant_name: "TextField1[0]",
-    appellant_relationship: "TextField1[1]",
-    file_number: "TextField1[2]",
-    veteran_name: "TextField1[3]",
-    insurance_loan_number: "TextField1[4]",
-    service_connection_for_initial: "TextField1[5]",
-    service_connection_notification_date: "TextField1[6]",
-    increased_rating_for_initial: "TextField1[7]",
-    increased_rating_notification_date: "TextField1[8]",
-    other_for_initial: "TextField1[9]",
-    other_notification_date: "TextField1[10]",
-    representative: "TextField1[11]",
-    power_of_attorney_file: "TextField1[12]",
-    power_of_attorney: {
-      "POA" => "CheckBox21[0]",
-      "Certification that valid POA is in another VA file" => "CheckBox21[1]"
-    },
-    agent_accredited: {
-      "Yes" => "CheckBox23[0]",
-      "No" => "CheckBox23[1]"
-    },
-    form_646_of_record: {
-      "Yes" => "CheckBox23[2]",
-      "No" => "CheckBox23[3]"
-    },
-    form_646_not_of_record_explanation: "TextField1[13]",
-    hearing_requested: {
-      "Yes" => "CheckBox23[4]",
-      "No" => "CheckBox23[5]"
-    },
-    hearing_on_file: {
-      "Yes" => "CheckBox23[6]",
-      "No" => "CheckBox23[7]"
-    },
-    hearing_requested_explanation: "TextField1[14]",
-    contested_claims_procedures_applicable: {
-      "Yes" => "CheckBox23[8]",
-      "No" => "CheckBox23[9]"
-    },
-    contested_claims_requirements_followed: {
-      "Yes" => "CheckBox23[10]",
-      "No" => "CheckBox23[11]"
-    },
-    soc_date: "TextField1[15]",
-    ssoc_required: {
-      "Required and furnished" => "CheckBox23[12]",
-      "Not required" => "CheckBox23[13]"
-    },
-    record_cf_or_xcf: "CheckBox23[14]",
-    record_inactive_cf: "CheckBox23[19]",
-    record_dental_f: "CheckBox23[25]",
-    record_r_and_e_f: "CheckBox23[15]",
-    record_training_sub_f: "CheckBox23[20]",
-    record_loan_guar_f: "CheckBox23[16]",
-    record_outpatient_f: "CheckBox23[17]",
-    record_hospital_cor: "CheckBox23[22]",
-    record_clinical_rec: "CheckBox23[26]",
-    record_x_rays: "CheckBox23[18]",
-    record_slides: "CheckBox23[23]",
-    record_tissue_blocks: "CheckBox23[27]",
-    record_dep_ed_f: "CheckBox23[24]",
-    record_insurance_f: "CheckBox23[21]",
-    record_other: "CheckBox23[28]",
-    record_other_explanation: "TextField1[16]",
-    remarks_initial: "TextField1[17]",
-    remarks_continued: "TextField1[26]",
-    certifying_office: "TextField1[18]",
-    certifying_username: "TextField1[19]",
-    certifying_official_name: "TextField1[20]",
-    certifying_official_title: "TextField1[21]",
-    certification_date: "TextField1[22]"
-  }.freeze
-
   FIELD_LOCATIONS_FORM8_V2 = {
     veteran_name: "TextField1[0]",
     file_number: "TextField1[1]",
@@ -164,21 +90,12 @@ class Form8PdfService
 
     File.delete(tmp_location) if File.exist?(tmp_location)
 
-    if FeatureToggle.enabled?(:form8_v2, user: RequestStore[:current_user])
-      pdf_forms.fill_form(
-        empty_pdf_location("VA8_v2.pdf"),
-        tmp_location,
-        pdf_values_for(form8, FIELD_LOCATIONS_FORM8_V2),
-        flatten: true
-      )
-    else
-      pdf_forms.fill_form(
-        empty_pdf_location("VA8.pdf"),
-        tmp_location,
-        pdf_values_for(form8, FIELD_LOCATIONS_FORM8_V1),
-        flatten: true
-      )
-    end
+    pdf_forms.fill_form(
+      empty_pdf_location("VA8_v2.pdf"),
+      tmp_location,
+      pdf_values_for(form8, FIELD_LOCATIONS_FORM8_V2),
+      flatten: true
+    )
 
     File.delete(final_location) if File.exist?(final_location)
 
