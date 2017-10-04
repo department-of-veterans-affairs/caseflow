54044d45ef91cd37d71a1edff5d019bad40c9694
Teja N <teja.s.naraparaju@gmail.com>
2017-10-04 09:24:43 -0400

    Return empty array if no issues

--- app/services/appeal_repository.rb
+++ app/services/appeal_repository.rb
@@ -139,7 +139,9 @@ class AppealRepository
   # :nocov:
   def self.issues(vacols_id)
     issues_hash = VACOLS::CaseIssue.descriptions([vacols_id])[vacols_id]
-    issues_hash && issues_hash.map { |issue_hash| Issue.load_from_vacols(issue_hash) }
+    issues_hash ? 
+      issues_hash.map { |issue_hash| Issue.load_from_vacols(issue_hash) } :
+      []
   end
 
   def self.remands_ready_for_claims_establishment
