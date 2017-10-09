9e5dbf0ca92ba464832d2d9f53c72a6f6d6a1695
Sharon Warner <sharon.warner56@gmail.com>
2017-10-09 13:33:09 -0400

    Debugging feature test

--- app/controllers/api/v1/appeals_controller.rb
+++ app/controllers/api/v1/appeals_controller.rb
@@ -35,6 +35,7 @@ class Api::V1::AppealsController < Api::V1::ApplicationController
   end
 
   def verify_feature_enabled
+    puts "verify_feature_enabled?"
     unauthorized unless FeatureToggle.enabled?(:appeals_status)
   end
 
