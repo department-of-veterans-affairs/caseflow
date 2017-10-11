59a92820e6338301df15160f54887f43091da18c
Mark Benjamin <mark@navahq.com>
2017-10-11 17:01:03 -0400

    Changes

--- client/app/reader/DocumentSearch.jsx
+++ client/app/reader/DocumentSearch.jsx
@@ -13,45 +13,23 @@ export class DocumentSearch extends React.PureComponent {
     const style = {
       position: 'absolute',
       background: 'white',
-      top: '0',
-      bottom: '0',
       zIndex: '20'
     };
-
-    const textDivs = this.props.searchObjects.map((obj) => <p>{obj.before}[{this.props.searchTextValue}]{obj.after}</p>);
 
     return <div style={style}>
       <SearchBar
         onChange={this.props.searchText}
       />
-      {textDivs}
+      Found {this.props.searchText} on pages: {this.props.textSnippets.pageIds.join(', ')}
     </div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  const { pageIds, pagesText, searchText } = getTextSearch(state);
   console.log('getTextSearch', getTextSearch(state));
-  // if (!_.isEmpty(pagesText)) {
-  //   debugger;
-  // }
-  const scopedPages = pageIds.map((id) => pagesText[id]);
-  const searchObjects = scopedPages.map((text) => {
-    const split = text.text.split(searchText);
-
-    return _.range(split.length - 1).map((index) => {
-      return {
-        before: split[index].substring(split[index].length - 10),
-        after: split[index + 1].substring(0, 10)
-      };
-    });
-  }).reduce((acc, obj) => acc.concat(obj), []);
 
   return {
-    searchObjects,
-    searchTextValue: searchText,
-    textSnippets: getTextSearch(state),
-    documentSearchString: state.readerReducer.documentSearchString
+    textSnippets: getTextSearch(state)
   }
 };
 
