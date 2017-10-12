b989a683eeada65bf81164baf445ac73b4ad1b5a
Sjones352 <sjones@tistatech.com>
2017-10-12 17:11:58 -0400

    added rspec test for hearing-prep help page

--- spec/feature/hearings_spec.rb
+++ spec/feature/hearings_spec.rb
@@ -71,7 +71,7 @@ RSpec.feature "Hearings" do
       # Validate help link
       find('#menu-trigger').click
       find_link("Help").click
-      expect(page).to have_content("Caseflow Hearings Help")
+      expect(page).to have_content("Welcome to the Hearings Help page!")
     end
 
     scenario "Upcoming docket days correctly handles master records" do
