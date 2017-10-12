df5735165383f1bf27adad4c0a06357a46dd9a18
Sharon Warner <sharon.warner56@gmail.com>
2017-10-12 10:46:08 -0400

    Null strings

--- app/models/worksheet_issue.rb
+++ app/models/worksheet_issue.rb
@@ -11,10 +11,10 @@ class WorksheetIssue < ActiveRecord::Base
   class << self
     def create_from_issue(appeal, issue)
       WorksheetIssue.find_or_create_by(appeal: appeal, vacols_sequence_id: issue.vacols_sequence_id).tap do |record|
-        record.update(program: issue.program ? issue.program.capitalize : "",
+        record.update(program: issue.program.try(:capitalize),
                       name: issue.type[:label],
-                      levels: issue.levels ? issue.levels.join("; ") : "",
-                      description: issue.description ? issue.description.join("\n") : "",
+                      levels: issue.levels.join("; "),
+                      description: issue.description.join("\n"),
                       from_vacols: true)
       end
     end
