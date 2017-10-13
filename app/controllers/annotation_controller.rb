af2a07bae8fd8357ddf7cc2cbb137036deff0e6a
Danny Sellers <dsellers90@gmail.com>
2017-10-13 12:39:59 -0400

    unset default Annotation created_at/updated_at datetimes

--- app/controllers/annotation_controller.rb
+++ app/controllers/annotation_controller.rb
@@ -4,12 +4,7 @@ class AnnotationController < ApplicationController
   ANNOTATION_AUTHORIZED_ROLES = ["Reader"].freeze
 
   def create
-    params = annotation_params.merge(
-      user_id: current_user.id,
-      created_at: Time.now.utc,
-      updated_at: Time.now.utc
-    )
-    annotation = Annotation.create!(params)
+    annotation = Annotation.create!(annotation_params.merge(user_id: current_user.id))
     render json: { id: annotation.id }
   end
 
--- db/migrate/20171012214206_add_default_datetime_to_annotation.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultDatetimeToAnnotation < ActiveRecord::Migration
-  def change
-    change_column_default :annotations, :created_at, Time.now.utc
-    change_column_default :annotations, :updated_at, Time.now.utc
-  end
-end
