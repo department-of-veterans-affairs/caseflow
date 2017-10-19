ab1ea37b3b58999e9481051c94e356334320d756
Mark Benjamin <mdbenjam@gmail.com>
2017-10-19 18:49:31 -0400

    Redux Search (#3571)
    Gets the text from all the documents
    Sets this text in the redux store
    Uses the redux-search to index that text
    Creates an early UI to search this indexed text and displays which pages it finds the search terms on
    Hides it all behind a feature flag.

--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -6,14 +6,31 @@ import thunk from 'redux-thunk';
 import DecisionReviewer from './DecisionReviewer';
 import readerReducer from './reducer';
 import { reduxAnalyticsMiddleware } from './analytics';
+import { reducer as searchReducer, reduxSearch } from 'redux-search';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
-    readerReducer
+    readerReducer,
+    search: searchReducer
   }),
-  composeEnhancers(applyMiddleware(thunk, perfLogger, reduxAnalyticsMiddleware))
+  composeEnhancers(
+    applyMiddleware(thunk, perfLogger, reduxAnalyticsMiddleware),
+    reduxSearch({
+      // Configure redux-search by telling it which resources to index for searching
+      resourceIndexes: {
+        // In this example Books will be searchable by :title and :author
+        extractedText: ['text']
+      },
+      // This selector is responsible for returning each collection of searchable resources
+      resourceSelector: (resourceName, state) => {
+        // In our example, all resources are stored in the state under a :resources Map
+        // For example "books" are stored under state.resources.books
+        return state.readerReducer[resourceName];
+      }
+    })
+  )
 );
 
 if (module.hot) {
