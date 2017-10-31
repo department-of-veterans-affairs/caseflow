8ae572035d9cbca4e598cb8ffcfb176d3cf0d898
lowellnava <lowell@navahq.com>
2017-10-31 11:58:27 -0400

    Change migration file name

--- db/migrate/20171030210701_add_metadata_to_documents.rb
+++ db/migrate/20171030210701_add_metadata_to_documents.rb
@@ -1,9 +1,7 @@
-class AddReceivedAtToDocuments < ActiveRecord::Migration
+class AddMetadataToDocuments < ActiveRecord::Migration
   def change
     add_column :documents, :received_at, :date
-    add_column :documents, :,, :string
     add_column :documents, :document_type, :string
-    add_column :documents, :,, :string
     add_column :documents, :file_number, :string
   end
 end
