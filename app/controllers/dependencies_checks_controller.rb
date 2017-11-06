704a7fb1bd6772bedfff515749ee878cbb27ce72
Nick Heiner <nth23@cornell.edu>
2017-11-06 13:58:36 -0500

    lint

--- app/controllers/dependencies_checks_controller.rb
+++ app/controllers/dependencies_checks_controller.rb
@@ -1,6 +1,6 @@
 class DependenciesChecksController < ApplicationBaseController
   newrelic_ignore_apdex
-  
+
   def show
     render json: { dependencies_outage: DependenciesReportService.outage_present? }
   end
