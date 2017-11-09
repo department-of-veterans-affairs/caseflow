3e6cb5f8fe078e3f10b78b38fa5137d54cb56b7a
Nick Heiner <nth23@cornell.edu>
2017-11-09 16:02:33 -0500

    lint

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,6 @@ class BaseController < ActionController::Base
   after_action :track_request_id
 
   def track_request_id
-    ::NewRelic::Agent.add_custom_attributes({ request_id: request.uuid })
+    ::NewRelic::Agent.add_custom_attributes(request_id: request.uuid)
   end
 end
-  
