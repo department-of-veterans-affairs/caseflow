04f6413f2057da897dd1ce10e771a7ec37dc9a48
Teja N <teja.s.naraparaju@gmail.com>
2017-11-22 08:36:26 -0500

    Fixing lint

--- spec/requests/appeal_controller_spec.rb
+++ spec/requests/appeal_controller_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "Reader Appeal Requests", type: :request do
 
   describe "Appeals controller #show in html" do
     it "redirects the page to Case page" do
-      get "/reader/appeal/#{appeal.vacols_id}"      
+      get "/reader/appeal/#{appeal.vacols_id}"
       expect(response).to have_http_status(:redirect)
     end
   end
