aea88fbb75d0591c7877509ec65f25d449b2742d
Chris Given <cmgiven@gmail.com>
2017-11-29 22:24:18 -0500

    Add status object to v2 API (#3992)
    * Add alerts to API (#3996)
    * Stub out remaining v2 attributes (#4004)

--- app/models/serializers/v2/appeal_serializer.rb
+++ app/models/serializers/v2/appeal_serializer.rb
@@ -8,8 +8,18 @@ class V2::AppealSerializer < ActiveModel::Serializer
   attribute :active?, key: :active
   attribute :aod
   attribute :location
+  attribute :status_hash, key: :status
+  attribute :alerts
 
   attribute :events do
     object.events.map(&:to_hash)
   end
+
+  # Stubbed attributes
+  attribute :aoj { "vba" }
+  attribute :program_area { "compensation" }
+  attribute :description { "" }
+  attribute :docket { nil }
+  attribute :issues { [] }
+  attribute :evidence { [] }
 end
