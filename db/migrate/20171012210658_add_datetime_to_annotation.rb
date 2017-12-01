77dd59d69bf256225117471c647597784ea22882
Mark Benjamin <mark@navahq.com>
2017-11-30 23:24:50 -0500

    Revert "Trying to fix the js test branch"
    This reverts commit a8fdd6dd65f919d9753f64449911b0d85ad01864.

--- db/migrate/20171012210658_add_datetime_to_annotation.rb
+++
@@ -1,6 +0,0 @@
-class AddDatetimeToAnnotation < ActiveRecord::Migration
-  def change
-    add_column :annotations, :created_at, :datetime
-    add_column :annotations, :updated_at, :datetime
-  end
-end
