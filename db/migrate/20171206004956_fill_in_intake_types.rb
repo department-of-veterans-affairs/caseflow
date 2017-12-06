a5bddb565901bd9f795fa3045e834a64109b8e0c
Shane Russell <shane1337@gmail.com>
2017-12-06 14:15:44 -0500

    Update 20171206004956_fill_in_intake_types.rb

--- db/migrate/20171206004956_fill_in_intake_types.rb
+++ db/migrate/20171206004956_fill_in_intake_types.rb
@@ -1,6 +1,6 @@
 class FillInIntakeTypes < ActiveRecord::Migration
   def up
-    # all intakes are Ramp Election Intakes right now. Set type accordingly
+    # all intakes are Ramp Election Intakes right now. Set type accordingly.
     Intake.update_all(type: "RampElectionIntake")
   end
 end
