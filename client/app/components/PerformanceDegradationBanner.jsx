ce086daedc42d507ffcd597b208e7c7e0fa4ea6a
Marian Lumba <mmlumba@gmail.com>
2017-12-08 14:34:20 -0500

    or operator

--- client/app/components/PerformanceDegradationBanner.jsx
+++ client/app/components/PerformanceDegradationBanner.jsx
@@ -32,7 +32,7 @@ export default class PerformanceDegradationBanner extends React.Component {
   checkDependencies() {
     // Don't make a subsequent request for dependency check
     // if the first one hasn't returned from the server still
-    if (this.state.isRequesting && document.hidden) {
+    if (this.state.isRequesting || document.hidden) {
       return;
     }
 
