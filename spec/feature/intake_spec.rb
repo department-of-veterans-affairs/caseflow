9e64d00c1056c6c90240f5eca3f9bedf04b99856
Shane Russell <shane1337@gmail.com>
2017-12-12 01:36:37 -0500

    Intake | More refactoring to support two intake paths (#4122)

--- spec/feature/intake_spec.rb
+++ spec/feature/intake_spec.rb
@@ -315,63 +315,361 @@ RSpec.feature "RAMP Intake" do
       before { FeatureToggle.enable!(:intake_reentry_form) }
       after { FeatureToggle.disable!(:intake_reentry_form) }
 
-      scenario "Attempt to start RAMP refiling for a veteran without a complete RAMP election" do
-        # Create an incomplete RAMP election
-        RampElection.create!(
-          veteran_file_number: "12341234",
-          notice_date: 3.days.ago
-        )
-
-        # Validate that you can't go directly to search
+      scenario "Search for a veteran that does not exist in BGS" do
         visit "/intake"
 
-        # Validate that you cant move forward without selecting a form
-        scroll_element_in_to_view(".cf-submit.usa-button")
-        expect(find(".cf-submit.usa-button")["disabled"]).to eq("true")
-
         within_fieldset("Which form are you processing?") do
           find("label", text: "21-4138 RAMP Selection Form").click
         end
         safe_click ".cf-submit.usa-button"
 
-        fill_in "Search small", with: "12341234"
+        fill_in "Search small", with: "5678"
         click_on "Search"
 
-        # TODO: Show the proper error message
-        expect(page).to have_content("Something went wrong")
-
-        expect(RampRefilingIntake.last).to have_attributes(error_code: "no_complete_ramp_election")
+        expect(page).to have_current_path("/intake/search")
+        expect(page).to have_content("Veteran ID not found")
       end
 
-      scenario "Start a RAMP refiling" do
-        # Create an complete RAMP election
-        ramp_election = RampElection.create!(
-          veteran_file_number: "12341234",
-          notice_date: 3.days.ago,
-          end_product_reference_id: "123"
-        )
+      scenario "Cancel an intake" do
+        RampElection.create!(veteran_file_number: "12341234", notice_date: Date.new(2017, 8, 7))
 
-        # Validate that you can't go directly to search
-        visit "/intake/search"
+        intake = RampElectionIntake.new(veteran_file_number: "12341234", user: current_user)
+        intake.start!
 
-        # Validate that you cant move forward without selecting a form
-        scroll_element_in_to_view(".cf-submit.usa-button")
-        expect(find(".cf-submit.usa-button")["disabled"]).to eq("true")
+        visit "/intake"
 
-        within_fieldset("Which form are you processing?") do
-          find("label", text: "21-4138 RAMP Selection Form").click
-        end
+        safe_click "#cancel-intake"
+        expect(find(".cf-modal-title")).to have_content("Cancel Intake?")
+        safe_click "#close-modal"
+        expect(page).to_not have_css(".cf-modal-title")
+
         safe_click ".cf-submit.usa-button"
+        safe_click ".cf-modal-body .cf-submit"
 
-        fill_in "Search small", with: "12341234"
-        click_on "Search"
+        expect(page).to have_content("Welcome to Caseflow Intake!")
+        expect(page).to_not have_css(".cf-modal-title")
 
-        expect(page).to have_current_path("/intake/review-request")
+        intake.reload
+        expect(intake.completed_at).to eq(Time.zone.now)
+        expect(intake).to be_canceled
+      end
 
-        ramp_refiling = RampRefiling.find_by(veteran_file_number: "12341234")
+      context "RAMP Election" do
+        scenario "Search for a veteran that has not received a RAMP election" do
+          visit "/intake"
 
-        expect(ramp_refiling).to_not be_nil
-        expect(ramp_refiling.ramp_election_id).to eq(ramp_election.id)
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "RAMP Opt-In Election Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "12341234"
+          click_on "Search"
+
+          expect(page).to have_current_path("/intake/search")
+          expect(page).to have_content("A RAMP Opt-in Notice Letter was not sent to this Veteran.")
+        end
+
+        scenario "Search for a veteran with an no active appeals" do
+          RampElection.create!(veteran_file_number: "77776666", notice_date: 5.days.ago)
+
+          visit "/intake"
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "RAMP Opt-In Election Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "77776666"
+          click_on "Search"
+
+          expect(page).to have_current_path("/intake/search")
+          expect(page).to have_content("Ineligible to participate in RAMP: no active appeals")
+        end
+
+        scenario "Search for a veteran with an ineligible appeal" do
+          RampElection.create!(veteran_file_number: "77778888", notice_date: 5.days.ago)
+
+          visit "/intake"
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "RAMP Opt-In Election Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "77778888"
+          click_on "Search"
+
+          expect(page).to have_current_path("/intake/search")
+          expect(page).to have_content("Ineligible to participate in RAMP: appeal is at the Board")
+        end
+
+        scenario "Search for a veteran that has a RAMP election already processed" do
+          ramp_election = RampElection.create!(
+            veteran_file_number: "12341234",
+            notice_date: 5.days.ago
+          )
+
+          RampElectionIntake.create!(
+            user: current_user,
+            detail: ramp_election,
+            completed_at: Time.zone.now,
+            completion_status: :success
+          )
+
+          # Validate you're redirected back to the form select page if you haven't started yet
+          visit "/intake/completed"
+          expect(page).to have_content("Welcome to Caseflow Intake!")
+
+          visit "/intake/review-request"
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "RAMP Opt-In Election Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "12341234"
+          click_on "Search"
+
+          expect(page).to have_content("Search for Veteran by ID")
+          expect(page).to have_content(
+            "A RAMP opt-in with the notice date 08/02/2017 was already processed"
+          )
+
+          error_intake = Intake.last
+          expect(error_intake.completion_status).to eq("error")
+          expect(error_intake.error_code).to eq("ramp_election_already_complete")
+        end
+
+        scenario "Search for a veteran that has received a RAMP election" do
+          RampElection.create!(veteran_file_number: "12341234", notice_date: 5.days.ago)
+
+          # Validate you're redirected back to the search page if you haven't started yet
+          visit "/intake/completed"
+          expect(page).to have_content("Welcome to Caseflow Intake!")
+
+          visit "/intake/review-request"
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "RAMP Opt-In Election Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "12341234"
+          click_on "Search"
+
+          expect(page).to have_current_path("/intake/review-request")
+          expect(page).to have_content("Review Ed Merica's opt-in election")
+
+          intake = RampElectionIntake.find_by(veteran_file_number: "12341234")
+          expect(intake).to_not be_nil
+          expect(intake.started_at).to eq(Time.zone.now)
+          expect(intake.user).to eq(current_user)
+        end
+
+        scenario "Start intake and go back and edit option" do
+          RampElection.create!(veteran_file_number: "12341234", notice_date: Date.new(2017, 8, 7))
+          intake = RampElectionIntake.new(veteran_file_number: "12341234", user: current_user)
+          intake.start!
+
+          # Validate that visiting the finish page takes you back to
+          # the review request page if you haven't yet reviewed the intake
+          visit "/intake/completed"
+
+          # Validate validation
+          fill_in "What is the Receipt Date for this election form?", with: "08/06/2017"
+          safe_click "#button-submit-review"
+
+          expect(page).to have_content("Please select an option.")
+          expect(page).to have_content(
+            "Receipt date cannot be earlier than the election notice date of 08/07/2017"
+          )
+
+          within_fieldset("Which election did the Veteran select?") do
+            find("label", text: "Higher Level Review", match: :prefer_exact).click
+          end
+          fill_in "What is the Receipt Date for this election form?", with: "08/07/2017"
+          safe_click "#button-submit-review"
+
+          expect(page).to have_content("Finish processing Higher-Level Review election")
+
+          click_label "confirm-finish"
+
+          ## Validate error message when complete intake fails
+          allow(Appeal).to receive(:close).and_raise("A random error. Oh no!")
+          safe_click "button#button-submit-review"
+          expect(page).to have_content("Something went wrong")
+
+          page.go_back
+
+          expect(page).to_not have_content("Please select an option.")
+
+          within_fieldset("Which election did the Veteran select?") do
+            find("label", text: "Supplemental Claim").click
+          end
+          safe_click "#button-submit-review"
+
+          expect(find("#confirm-finish", visible: false)).to_not be_checked
+          expect(page).to_not have_content("Something went wrong")
+
+          expect(page).to have_content("Finish processing Supplemental Claim election")
+
+          # Validate the appeal & issue also shows up
+          expect(page).to have_content("This Veteran has 1 eligible appeal, with the following issues")
+          expect(page).to have_content("5252 - Knee, limitation of flexion of")
+          expect(page).to have_content("knee movement")
+        end
+
+        scenario "Complete intake for RAMP Election form" do
+          Fakes::VBMSService.end_product_claim_id = "SHANE9642"
+
+          election = RampElection.create!(
+            veteran_file_number: "12341234",
+            notice_date: Date.new(2017, 8, 7)
+          )
+
+          intake = RampElectionIntake.new(veteran_file_number: "12341234", user: current_user)
+          intake.start!
+
+          # Validate that visiting the finish page takes you back to
+          # the review request page if you haven't yet reviewed the intake
+          visit "/intake/finish"
+
+          within_fieldset("Which election did the Veteran select?") do
+            find("label", text: "Higher Level Review with Informal Conference").click
+          end
+
+          fill_in "What is the Receipt Date for this election form?", with: "08/07/2017"
+          safe_click "#button-submit-review"
+
+          expect(page).to have_content("Finish processing Higher-Level Review election")
+
+          election.reload
+          expect(election.option_selected).to eq("higher_level_review_with_hearing")
+          expect(election.receipt_date).to eq(Date.new(2017, 8, 7))
+
+          # Validate the app redirects you to the appropriate location
+          visit "/intake"
+          safe_click "#button-submit-review"
+          expect(page).to have_content("Finish processing Higher-Level Review election")
+
+          expect(Fakes::AppealRepository).to receive(:close!).with(
+            appeal: Appeal.find_or_create_by_vacols_id(appeal.vacols_id),
+            user: current_user,
+            closed_on: Time.zone.today,
+            disposition_code: "P"
+          )
+
+          safe_click "button#button-submit-review"
+
+          expect(page).to have_content("You must confirm you've completed the steps")
+          expect(page).to_not have_content("Intake completed")
+
+          click_label("confirm-finish")
+          safe_click "button#button-submit-review"
+
+          expect(page).to have_content("Intake completed")
+          expect(page).to have_content(
+            "Established EP: 682HLRRRAMP - Higher Level Review Rating for Station 397"
+          )
+
+          expect(Fakes::VBMSService).to have_received(:establish_claim!).with(
+            claim_hash: {
+              benefit_type_code: "1",
+              payee_code: "00",
+              predischarge: false,
+              claim_type: "Claim",
+              station_of_jurisdiction: "397",
+              date: election.receipt_date.to_date,
+              end_product_modifier: "682",
+              end_product_label: "Higher Level Review Rating",
+              end_product_code: "682HLRRRAMP",
+              gulf_war_registry: false,
+              suppress_acknowledgement_letter: false
+            },
+            veteran_hash: intake.veteran.to_vbms_hash
+          )
+
+          # Validate that you can not go back to previous steps
+          page.go_back
+          expect(page).to have_content("Intake completed")
+
+          page.go_back
+          page.go_back
+          expect(page).to have_content("Welcome to Caseflow Intake!")
+
+          intake.reload
+          expect(intake.completed_at).to eq(Time.zone.now)
+          expect(intake).to be_success
+
+          election.reload
+          expect(election.end_product_reference_id).to eq("SHANE9642")
+
+          # Validate that the intake is no longer able to be worked on
+          visit "/intake/finish"
+          expect(page).to have_content("Welcome to Caseflow Intake!")
+        end
+      end
+
+      context "RAMP Refiling" do
+        scenario "Attempt to start RAMP refiling for a veteran without a complete RAMP election" do
+          # Create an incomplete RAMP election
+          RampElection.create!(
+            veteran_file_number: "12341234",
+            notice_date: 3.days.ago
+          )
+
+          # Validate that you can't go directly to search
+          visit "/intake"
+
+          # Validate that you can't move forward without selecting a form
+          scroll_element_in_to_view(".cf-submit.usa-button")
+          expect(find(".cf-submit.usa-button")["disabled"]).to eq("true")
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "21-4138 RAMP Selection Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "12341234"
+          click_on "Search"
+
+          # TODO: Show the proper error message
+          expect(page).to have_content("Something went wrong")
+
+          expect(RampRefilingIntake.last).to have_attributes(error_code: "no_complete_ramp_election")
+        end
+
+        scenario "Start a RAMP refiling" do
+          # Create an complete RAMP election
+          ramp_election = RampElection.create!(
+            veteran_file_number: "12341234",
+            notice_date: 3.days.ago,
+            end_product_reference_id: "123"
+          )
+
+          # Validate that you can't go directly to search
+          visit "/intake/search"
+
+          # Validate that you can't move forward without selecting a form
+          scroll_element_in_to_view(".cf-submit.usa-button")
+          expect(find(".cf-submit.usa-button")["disabled"]).to eq("true")
+
+          within_fieldset("Which form are you processing?") do
+            find("label", text: "21-4138 RAMP Selection Form").click
+          end
+          safe_click ".cf-submit.usa-button"
+
+          fill_in "Search small", with: "12341234"
+          click_on "Search"
+
+          expect(page).to have_current_path("/intake/review-request")
+
+          ramp_refiling = RampRefiling.find_by(veteran_file_number: "12341234")
+
+          expect(ramp_refiling).to_not be_nil
+          expect(ramp_refiling.ramp_election_id).to eq(ramp_election.id)
+        end
       end
     end
   end
