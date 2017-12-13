8b7eadf07f3888f158030b79e3b104242f6fd74d
Shane Russell <shane.russell.va@gmail.com>
2017-12-12 19:04:03 -0500

    Intake | refactor actions based on form
    Makes way for actions to be seperated based on form selection

--- client/app/intake/pages/search.jsx
+++ client/app/intake/pages/search.jsx
@@ -4,7 +4,7 @@ import Alert from '../../components/Alert';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { doFileNumberSearch, setFileNumberSearch } from '../redux/actions';
+import { doFileNumberSearch, setFileNumberSearch } from '../actions/common';
 import { REQUEST_STATE, PAGE_PATHS, RAMP_INTAKE_STATES } from '../constants';
 import { getRampElectionStatus } from '../redux/selectors';
 
