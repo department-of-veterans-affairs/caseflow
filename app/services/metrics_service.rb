d7063f93742d79cdfa2109eb8835af8009b0583d
Nick Heiner <nth23@cornell.edu>
2017-12-13 15:35:09 -0500

    LOOKS LIKE DOG'S BACK ON THE MENU, BOYS

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -1,7 +1,15 @@
 require "benchmark"
+require 'dogapi'
 
 # see https://dropwizard.github.io/metrics/3.1.0/getting-started/ for abstractions on metric types
 class MetricsService
+
+  dog = Dogapi::Client.new(ENV["DATADOG_API_KEY"])
+
+  def emit_datadog_point(metric_name, metric_value)
+    dog.emit_point(metric_name, metric_value, :service => service, :env => Rails.env, :host => `hostname`.strip)
+  end    
+
   # rubocop:disable Metrics/MethodLength
   def self.record(description, service: nil, name: "unknown")
     return_value = nil
@@ -15,25 +23,29 @@ class MetricsService
     if service
       metric = PrometheusService.send("#{service}_request_latency".to_sym)
 
-      metric.set({ app: app, name: name }, stopwatch.real)
-
+      latency = stopwatch.real
+      metric.set({ app: app, name: name }, latency)
+      emit_datadog_point("service_request_latency", latency)
+      
     end
-
+    
     Rails.logger.info("FINISHED #{description}: #{stopwatch}")
     return_value
   rescue
     if service
       metric = PrometheusService.send("#{service}_request_error_counter".to_sym)
       metric.increment(app: app, name: name)
+      emit_datadog_point("service_request_error", 1)
     end
-
-    # Reraise the same error. We don't want to interfere at all in normal error handling
-    # this is just to capture the metric
+    
+    # Re-raise the same error. We don't want to interfere at all in normal error handling.
+    # This is just to capture the metric
     raise
   ensure
     if service
       metric = PrometheusService.send("#{service}_request_attempt_counter".to_sym)
       metric.increment(app: app, name: name)
+      emit_datadog_point("service_request_attempt", 1)
     end
   end
 end
