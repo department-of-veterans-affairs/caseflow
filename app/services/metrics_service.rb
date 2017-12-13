579b16453329815c6bccb03213a9083026ad75bb
Nick Heiner <nth23@cornell.edu>
2017-12-13 17:50:59 -0500

    lint and other fixes

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -3,7 +3,6 @@ require "dogapi"
 
 # see https://dropwizard.github.io/metrics/3.1.0/getting-started/ for abstractions on metric types
 class MetricsService
-  
   @dog = Dogapi::Client.new(ENV["DATADOG_API_KEY"])
 
   # rubocop:disable Metrics/MethodLength
@@ -21,19 +20,19 @@ class MetricsService
 
       latency = stopwatch.real
       metric.set({ app: app, name: name }, latency)
-      self.emit_datadog_point("service_request_latency", latency, service)
-      
+      emit_datadog_point("request_latency", latency, service)
+
     end
-    
+
     Rails.logger.info("FINISHED #{description}: #{stopwatch}")
     return_value
   rescue
     if service
       metric = PrometheusService.send("#{service}_request_error_counter".to_sym)
       metric.increment(app: app, name: name)
-      self.emit_datadog_point("service_request_error", 1, service)
+      emit_datadog_point("request_error", 1, service)
     end
-    
+
     # Re-raise the same error. We don't want to interfere at all in normal error handling.
     # This is just to capture the metric
     raise
@@ -41,13 +40,12 @@ class MetricsService
     if service
       metric = PrometheusService.send("#{service}_request_attempt_counter".to_sym)
       metric.increment(app: app, name: name)
-      self.emit_datadog_point("service_request_attempt", 1, service)
+      emit_datadog_point("request_attempt", 1, service)
     end
   end
 
-  private
-
-  def self.emit_datadog_point(metric_name, metric_value, service)
-    @dog.emit_point(metric_name, metric_value, :service => service, :env => Rails.env, :host => `hostname`.strip)
-  end    
+  private_class_method def self.emit_datadog_point(metric_name, metric_value, service)
+    @dog.emit_point("caseflow.service.#{metric_name}", metric_value,
+                    service: service, env: Rails.env, host: `hostname`.strip)
+  end
 end
