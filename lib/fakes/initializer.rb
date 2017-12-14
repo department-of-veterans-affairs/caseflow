1d2204baa2a4a656c4dd0321f5e8de64ffe41599
Mark Benjamin <mark@navahq.com>
2017-12-13 20:29:15 -0500

    Trying to fix up fakes

--- lib/fakes/initializer.rb
+++ lib/fakes/initializer.rb
@@ -2,16 +2,17 @@ class Fakes::Initializer
   class << self
     def load!
       PowerOfAttorney.repository = Fakes::PowerOfAttorneyRepository
-      User.authentication_service = Fakes::AuthenticationService
       Hearing.repository = Fakes::HearingRepository
       HearingDocket.repository = Fakes::HearingRepository
-      Appeal.repository = AppealRepository
+      Appeal.repository = Fakes::AppealRepository
       CAVCDecision.repository = Fakes::CAVCDecisionRepository
-      User.appeal_repository = AppealRepository
+      User.appeal_repository = Fakes::AppealRepository
     end
 
     # This method is called only 1 time during application bootup
     def app_init!(rails_env)
+      User.authentication_service = Fakes::AuthenticationService
+
       if rails_env.development? || rails_env.demo?
         # If we are running a rake command like `rake db:seed` or
         # `rake db:schema:load`, we do not want to try and seed the fakes
