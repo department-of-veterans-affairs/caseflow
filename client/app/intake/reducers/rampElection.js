c7f4dd80b470e2dedebfe351ac49016893c15528
Shane Russell <shane1337@gmail.com>
2017-12-20 12:19:51 -0500

    shorten import

--- client/app/intake/reducers/rampElection.js
+++ client/app/intake/reducers/rampElection.js
@@ -1,7 +1,7 @@
 import { ACTIONS, REQUEST_STATE, FORM_TYPES } from '../constants';
 import { update } from '../../util/ReducerUtil';
 import { formatDateStr } from '../../util/DateUtil';
-import { getOptionSelectedError, getReceiptDateError } from '../util/index';
+import { getOptionSelectedError, getReceiptDateError } from '../util';
 import _ from 'lodash';
 
 const formatAppeals = (appeals) => {
