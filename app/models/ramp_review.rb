05674db0470f985624963f97f1d09d1a1b526de9
Shane Russell <shane.russell.va@gmail.com>
2017-12-20 16:48:54 -0500

    Intake | Add validations to RAMP refiling review page
    Follow on from #4204 to complete all AC for the issue.
    
    To test
    - Start a RAMP refiling and enter a date far in the past and click continue.
      You should see an error
    - Start a RAMP refiling and enter a date in the future and click continue,
      You should see an error
    - Start a RAMP refiling of a HLR and select HLR and click continue,
      You should see an alert.
    
    Note: We will be following up on more functionality to support the HLR invalid case
    
    connects #3958

--- app/models/ramp_review.rb
+++ app/models/ramp_review.rb
@@ -10,6 +10,8 @@ class RampReview < ActiveRecord::Base
     appeal: "appeal"
   }
 
+  HIGHER_LEVEL_REVIEW_OPTIONS = %w(higher_level_review higher_level_review_with_hearing).freeze
+
   END_PRODUCT_DATA_BY_OPTION = {
     "supplemental_claim" => { code: "683SCRRRAMP", modifier: "683" },
     "higher_level_review" => { code: "682HLRRRAMP", modifier: "682" },
@@ -23,5 +25,15 @@ class RampReview < ActiveRecord::Base
   # Allows us to enable certain validations only when saving the review
   def start_review!
     @saving_review = true
+  end
+
+  def higher_level_review?
+    HIGHER_LEVEL_REVIEW_OPTIONS.include?(option_selected)
+  end
+
+  private
+
+  def validate_receipt_date_not_in_future
+    errors.add(:receipt_date, "in_future") if Time.zone.today < receipt_date
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -231,17 +231,21 @@ end
 
 def scroll_element_in_to_view(selector)
   expect do
-    page.evaluate_script <<-EOS
-      function() {
-        var elem = document.querySelector('#{selector.gsub("'", "\\\\'")}');
-        if (!elem) {
-          return false;
-        }
-        elem.scrollIntoView();
-        return true;
-      }();
-    EOS
-  end.to become_truthy
+    scroll_to_element_in_view_with_script(selector)
+  end.to become_truthy, "Could not find element #{selector}"
+end
+
+def scroll_to_element_in_view_with_script(selector)
+  page.evaluate_script <<-EOS
+    function() {
+      var elem = document.querySelector('#{selector.gsub("'", "\\\\'")}');
+      if (!elem) {
+        return false;
+      }
+      elem.scrollIntoView();
+      return true;
+    }();
+  EOS
 end
 
 # We generally avoid writing our own polling code, since proper Cappybara use generally
