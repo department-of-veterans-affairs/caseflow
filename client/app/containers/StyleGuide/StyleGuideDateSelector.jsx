01c6833470165525e0608b8526f277c59df10b8f
Danny Sellers <dsellers90@gmail.com>
2017-12-21 14:54:10 -0500

    add copy

--- client/app/containers/StyleGuide/StyleGuideDateSelector.jsx
+++ client/app/containers/StyleGuide/StyleGuideDateSelector.jsx
@@ -22,6 +22,13 @@ export default class StyleGuideDateSelector extends React.PureComponent {
         id="date-input"
         link="StyleGuideDateSelector.jsx"
       />
+      <p>
+        Date input allows users to click in the input box
+        and type in a date quickly and in a format that validates
+        each input. Designers and writers are recommented to add an
+        example in the correct format above the component (example:
+        07/04/1776) so that users have a quick and easy instruction.
+      </p>
       <DateSelector
         name="Example: 07/04/1776"
         onChange={this.onChange}
