c95f864fe87368e7e8b0c1e58fd603fc8e6a5669
Nemo Djuric <nemo.djuric@gmail.com>
2017-12-21 11:00:43 -0500

    Make a one liner for @test_users

--- app/controllers/test/users_controller.rb
+++ app/controllers/test/users_controller.rb
@@ -52,9 +52,7 @@ class Test::UsersController < ApplicationController
 
   # :nocov:
   def index
-    @test_users = User.all.select do |u|
-      User::FUNCTIONS.include?(u.css_id) || u.css_id.include?("System Admin")
-    end
+    @test_users = User.all.select { |u| User::FUNCTIONS.include?(u.css_id) || u.css_id.include?("System Admin") }
     @ep_types = %w(full partial none all)
     render "index"
   end
