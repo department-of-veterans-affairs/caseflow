c8f0b20ebb13bc703ca41757815a570ef7f735d1
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-12-28 15:16:10 -0500

    test tests

--- spec/feature/hearings_spec.rb
+++ spec/feature/hearings_spec.rb
@@ -99,7 +99,7 @@ RSpec.feature "Hearings" do
       find("label", text: "Transcript Requested").click
       visit "/hearings/dockets/2017-01-01"
       expect(page).to have_content("This is a note about the hearing!")
-      expect(page).to have_content("No Show")
+      expect(page).to have_content("Held")
       expect(page).to have_content("30 days")
       expect(page).to have_content("Filed")
       expect(page).to have_content("Postponed")
