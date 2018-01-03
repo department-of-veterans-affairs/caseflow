5d5a85b9168682639fda5261b2032d4dee2535a3
Nick "True Cyber" Heiner <nicholas_t_heiner@omb.eop.gov>
2018-01-03 16:26:46 -0500

    Share Redux Setup (#4264)
    * Add ReduxBase class and use it for existing apps
    * Connect #2026.
    * Fix #2858.
    * Removing hot module reloading for reducers, which may not have been working anyway.
    
    There is additional refactoring to be done for Reader tests.

--- client/app/intake/index.js
+++ client/app/intake/index.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import perfLogger from 'redux-perf-middleware';
+import { combineReducers } from 'redux';
 import IntakeFrame from './IntakeFrame';
 import { intakeReducer, mapDataToInitialIntake } from './reducers/intake';
 import { rampElectionReducer, mapDataToInitialRampElection } from './reducers/rampElection';
 import { rampRefilingReducer, mapDataToInitialRampRefiling } from './reducers/rampRefiling';
-import { getReduxAnalyticsMiddleware } from '../util/getReduxAnalyticsMiddleware';
+import ReduxBase from '../util/ReduxBase';
 
-const Intake = (props) => {
-  // eslint-disable-next-line no-underscore-dangle
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reducer = combineReducers({
+  intake: intakeReducer,
+  rampElection: rampElectionReducer,
+  rampRefiling: rampRefilingReducer
+});
 
-  const reducer = combineReducers({
-    intake: intakeReducer,
-    rampElection: rampElectionReducer,
-    rampRefiling: rampRefilingReducer
-  });
+class Intake extends React.PureComponent {
+  render() {
+    const initialState = {
+      intake: mapDataToInitialIntake(this.props),
+      rampElection: mapDataToInitialRampElection(this.props),
+      rampRefiling: mapDataToInitialRampRefiling(this.props)
+    };
 
-  const store = createStore(
-    reducer,
-    {
-      intake: mapDataToInitialIntake(props),
-      rampElection: mapDataToInitialRampElection(props),
-      rampRefiling: mapDataToInitialRampRefiling(props)
-    },
-    composeEnhancers(applyMiddleware(thunk, perfLogger, getReduxAnalyticsMiddleware('intake')))
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept([
-      './reducers/intake',
-      './reducers/rampElection',
-      './reducers/rampRefiling'
-    ],
-    () => store.replaceReducer(reducer)
-    );
+    return <ReduxBase initialState={initialState} reducer={reducer} analyticsMiddlewareArgs={['intake']}>
+      <IntakeFrame {...this.props} />
+    </ReduxBase>;
   }
-
-  return <Provider store={store}>
-    <IntakeFrame {...props} />
-  </Provider>;
-};
+}
 
 export default Intake;
--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -1,48 +1,31 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import perfLogger from 'redux-perf-middleware';
-import thunk from 'redux-thunk';
+import ReduxBase from '../util/ReduxBase';
+
 import DecisionReviewer from './DecisionReviewer';
 
-import { getReduxAnalyticsMiddleware } from '../util/getReduxAnalyticsMiddleware';
 import { reduxSearch } from 'redux-search';
 import rootReducer from './reducers';
 
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk, perfLogger, getReduxAnalyticsMiddleware()),
-    reduxSearch({
+class Reader extends React.PureComponent {
+  componentWillMount() {
+    const enhancers = [
+      reduxSearch({
       // Configure redux-search by telling it which resources to index for searching
-      resourceIndexes: {
-        // In this example Books will be searchable by :title and :author
-        extractedText: ['text']
-      },
-      // This selector is responsible for returning each collection of searchable resources
-      resourceSelector: (resourceName, state) => {
-        // In our example, all resources are stored in the state under a :resources Map
-        // For example "books" are stored under state.resources.books
-        return state.searchActionReducer[resourceName];
-      }
-    })
-  )
-);
+        resourceIndexes: {
+          extractedText: ['text']
+        },
+        // This selector is responsible for returning each collection of searchable resources
+        resourceSelector: (resourceName, state) => state.searchActionReducer[resourceName]
+      })
+    ];
 
-if (module.hot) {
-  // Enable Webpack hot module replacement for reducers
-  module.hot.accept('./reducers', () => {
-    store.replaceReducer(rootReducer);
-  });
+    this.setState({ enhancers });
+
+  }
+  render = () =>
+    <ReduxBase reducer={rootReducer} enhancers={this.state.enhancers}>
+      <DecisionReviewer {...this.props} />
+    </ReduxBase>;
 }
-
-const Reader = (props) => {
-  return <Provider store={store}>
-    <DecisionReviewer {...props} />
-  </Provider>;
-};
 
 export default Reader;
