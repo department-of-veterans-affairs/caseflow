0d3db61754b9c9fcf2ea194904e7804c70b83186
Nick Heiner <nth23@cornell.edu>
2018-01-04 11:01:31 -0500

    update enzyme

--- client/test/karma/setup.js
+++ client/test/karma/setup.js
@@ -1,3 +1,8 @@
+import enzyme from 'enzyme';
+import EnzymeAdapterReact16 from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new EnzymeAdapterReact16() });
+
 // eslint-disable-next-line no-empty-function
 window.analyticsPageView = function() {
 };
