f9fa69486f364cdbf01a1546f449c83a027857f6
Nick Heiner <nth23@cornell.edu>
2018-01-08 17:29:05 -0500

    DRY out test setup

--- client/test/karma/test-index.js
+++ client/test/karma/test-index.js
@@ -3,17 +3,7 @@ import EnzymeAdapterReact16 from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new EnzymeAdapterReact16() });
 
-// eslint-disable-next-line no-empty-function
-window.analyticsPageView = function() {
-};
-
-// eslint-disable-next-line no-empty-function
-window.analyticsEvent = function() {
-};
-
-// eslint-disable-next-line no-empty-function
-window.analyticsTiming = function() {
-};
+import 'appeals-frontend-toolkit/test/bootstrap';
 
 // require all modules ending in "-test" from the
 // current directory and all subdirectories
