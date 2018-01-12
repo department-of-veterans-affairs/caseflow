9a2e5b6ec9e0605f6bfc076a820c139293447a45
Alex Prokop <alex.prokop@gmail.com>
2018-01-12 17:42:55 -0500

    changes task serializer to not include appeal, it should be its own serializer

--- app/models/serializers/queue/task_serializer.rb
+++ app/models/serializers/queue/task_serializer.rb
@@ -5,13 +5,4 @@ class QueueSerializers::TaskSerializer < ActiveModel::Serializer
   attribute :docket_date
   attribute :appeal_id
   attribute :user_id
-
-
-  attribute :appeal do
-    ActiveModelSerializers::SerializableResource.new(
-      object.appeal,
-      each_serializer: ::V1::AppealSerializer,
-      include: "hearings"
-    ).as_json
-  end
 end
