bd3512b6c1c2d543c1ea2d2b6118057eb4856f4b
Teja N <teja.s.naraparaju@gmail.com>
2018-01-12 03:01:32 -0500

    Finxing lint

--- lib/fakes/appeal_repository.rb
+++ lib/fakes/appeal_repository.rb
@@ -22,8 +22,11 @@ class Fakes::AppealRepository
       user_case_assignments = appeal_records || Fakes::Data::AppealData.default_records
       appeal = user_case_assignments.first
       # Create fake hearings for the first appeal if one doesn't already exist
-      2.times { |i| Fakes::HearingRepository.create_hearing_for_appeal(i, appeal) } 
+      2.times do |i|
         if Hearing.where(appeal: appeal).empty?
+          Fakes::HearingRepository.create_hearing_for_appeal(i, appeal) }
+        end
+      end
 
       user_case_assignments
     end
