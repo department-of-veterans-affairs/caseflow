2eff97a19f07920e8783ebcdbc231142d6a41cbe
Teja <teja.s.naraparaju@gmail.com>
2018-01-12 14:13:48 -0500

    update comment

--- app/models/hearing.rb
+++ app/models/hearing.rb
@@ -192,7 +192,8 @@ class Hearing < ActiveRecord::Base
     def create_from_vacols_record(vacols_record)
       transaction do
         find_or_initialize_by(vacols_id: vacols_record.hearing_pkseq).tap do |hearing|
-          # update hearing if user is nil or if vacols record css is different from
+          # update hearing if user is nil, it's likely when the record doesn't exist and is being created
+          # or if vacols record css is different from
           # who it's assigned to in the db.
           if user_nil_or_assigned_to_another_judge?(hearing.user, vacols_record.css_id)
             hearing.update(
