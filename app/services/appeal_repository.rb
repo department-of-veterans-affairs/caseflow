5ee38588bd47fa960c93e283518679845c72d940
Chris Given <Christopher.Given@va.gov>
2018-01-12 16:43:24 -0500

    Add queries

--- app/services/appeal_repository.rb
+++ app/services/appeal_repository.rb
@@ -460,7 +460,7 @@ class AppealRepository
   end
 
   def self.latest_docket_month
-    conn = VACOLS::Case.connection
+    conn = VACOLS::Record.connection
 
     query = <<-SQL
       select BFD19 from (
@@ -483,6 +483,50 @@ class AppealRepository
   end
 
   def self.docket_counts_by_month
+    conn = VACOLS::Record.connection
+
+    query = <<-SQL
+      select YEAR, MONTH, N, READY_N,
+        coalesce(
+          sum(N) over (order by YEAR, MONTH rows between unbounded preceding and 1 preceding),
+          0
+        ) CUMSUM_N,
+        coalesce(
+          sum(READY_N) over (order by YEAR, MONTH rows between unbounded preceding and 1 preceding),
+          0
+        ) CUMSUM_READY_N
+      from (
+        select extract(year from BFD19) YEAR, extract(month from BFD19) MONTH, count(*) N,
+          count(case when BFMPRO = 'ACT' and (BFCURLOC = '81' or SATTYID is not null) then 1 end) READY_N
+        from BRIEFF
+        inner join STAFF on BFCURLOC = STAFKEY
+        left join (
+          select TSKTKNM, count(*) CNT
+          from ASSIGN
+          where TSKACTCD in ('B', 'B1', 'B2')
+          group by TSKTKNM
+        ) AOD_DIARIES on AOD_DIARIES.TSKTKNM = BFKEY
+        left join (
+          select FOLDER_NR, count(*) CNT
+          from HEARSCHED
+          where HEARING_TYPE IN ('C', 'T', 'V')
+            AND AOD IN ('G', 'Y')
+          group by FOLDER_NR
+        ) AOD_HEARINGS on AOD_HEARINGS.FOLDER_NR = BFKEY
+        where BFMPRO <> 'HIS'
+          and BFD19 is not null
+          and BFAC in ('1', '3')
+          and AOD_DIARIES.CNT is null
+          and AOD_HEARINGS.CNT is null
+        group by extract(year from BFD19), extract(month from BFD19)
+      )
+    SQL
+
+    MetricsService.record("VACOLS: docket_counts_by_month",
+                          name: "docket_counts_by_month",
+                          service: :vacols) do
+      conn.exec_query(query)
+    end
   end
 
   class << self
