1189da9093611393bde47215b6577c4eec9e073b
Alex Prokop <alex.prokop@gmail.com>
2018-01-16 11:44:28 -0500

    makes attorney stuff work

--- app/models/queues/attorney_queue.rb
+++ app/models/queues/attorney_queue.rb
@@ -2,12 +2,12 @@ class AttorneyQueue < BaseQueue
   def self.tasks_with_appeals(user_id)
     css_id = User.find(user_id).css_id
     vacols_tasks = repository.tasks_for_user(css_id)
-    vacols_appeals = repository.appeals_from_tasks(tasks)
+    vacols_appeals = repository.appeals_from_tasks(vacols_tasks)
 
     tasks = vacols_tasks.map do |task|
       DraftDecision.from_vacols(task, user_id)
     end
-    [tasks, appeals]
+    [tasks, vacols_appeals]
   end
 
   # We don't redefine the repository as a fake
--- app/models/tasks/draft_decision.rb
+++ app/models/tasks/draft_decision.rb
@@ -2,18 +2,17 @@ class DraftDecision
   include ActiveModel::Model
   include ActiveModel::Serialization
 
-  ATTRS = [:appeal, :appeal_id, :user_id, :due_on, :assigned_on, :docket_name, :docket_date].freeze
+  ATTRS = [:appeal_id, :user_id, :due_on, :assigned_on, :docket_name, :docket_date].freeze
   attr_accessor(*ATTRS)
 
-  def self.from_vacols(case_assignment, user_id, appeal)
+  def self.from_vacols(case_assignment, user_id)
     new(
       assigned_on: case_assignment.date_assigned,
       due_on: case_assignment.date_due,
       docket_name: "legacy",
       docket_date: case_assignment.docket_date,
       appeal_id: case_assignment.vacols_id,
-      user_id: user_id,
-      appeal: appeal
+      user_id: user_id
     )
   end
 end
