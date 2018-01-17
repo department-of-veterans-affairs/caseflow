f7bd52a1f144c4c308343237193ffadb344208e0
Nick Heiner <nth23@cornell.edu>
2018-01-17 14:29:28 -0500

    lint

--- client/app/components/NavigationBar.jsx
+++ client/app/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PerformanceDegradationBanner from './PerformanceDegradationBanner';
-import NavigationBar from '@department-of-veterans-affairs/appeals-frontend-toolkit/components/NavigationBar'
+import NavigationBar from '@department-of-veterans-affairs/appeals-frontend-toolkit/components/NavigationBar';
 
 export default class CaseflowNavigationBar extends React.PureComponent {
   render = () => <NavigationBar extraBanner={<PerformanceDegradationBanner />} {...this.props} />
