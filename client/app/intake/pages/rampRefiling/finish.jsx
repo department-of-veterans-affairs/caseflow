db0d5945986b0461a5d758caf8629a21515c1565
mmlumba <mmlumba@gmail.com>
2018-01-19 11:09:29 -0500

    add error for finish page in ramp refiling

--- client/app/intake/pages/rampRefiling/finish.jsx
+++ client/app/intake/pages/rampRefiling/finish.jsx
@@ -4,6 +4,7 @@ import CancelButton from '../../components/CancelButton';
 import Checkbox from '../../../components/Checkbox';
 import Button from '../../../components/Button';
 import Table from '../../../components/Table';
+import Alert from '../../../components/Alert';
 import { Redirect } from 'react-router-dom';
 import { PAGE_PATHS, RAMP_INTAKE_STATES, REQUEST_STATE } from '../../constants';
 import { connect } from 'react-redux';
@@ -34,7 +35,8 @@ class Finish extends React.PureComponent {
       hasIneligibleIssue,
       outsideCaseflowStepsConfirmed,
       outsideCaseflowStepsError,
-      issuesSelectedError
+      issuesSelectedError,
+      requestState
     } = this.props;
 
     switch (rampRefilingStatus) {
@@ -88,6 +90,12 @@ class Finish extends React.PureComponent {
 
     return <div>
       <h1>Finish processing RAMP Selection form</h1>
+
+      { requestState === REQUEST_STATE.FAILED &&
+        <Alert title="Something went wrong" type="error" lowerMargin>
+          Please try again. If the problem persists, please contact Caseflow support.
+        </Alert>
+      }
 
       <ol className="cf-bare-list" ref={this.setOutsideCaseflowStepsNode}>
         <li>
@@ -173,7 +181,8 @@ export default connect(
     outsideCaseflowStepsConfirmed: state.rampRefiling.outsideCaseflowStepsConfirmed,
     outsideCaseflowStepsError: state.rampRefiling.outsideCaseflowStepsError,
     issuesSelectedError: state.rampRefiling.issuesSelectedError,
-    finishErrorProcessed: state.rampRefiling.finishErrorProcessed
+    finishErrorProcessed: state.rampRefiling.finishErrorProcessed,
+    requestState: state.rampRefiling.requestStatus.completeIntake
   }),
   (dispatch) => bindActionCreators({
     setIssueSelected,
