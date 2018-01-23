bcf405a29f2465c7a18499b76ee7f233fdf91250
Sharon Warner <sharon@navahq.com>
2018-01-23 13:42:47 -0500

    UAT testing

--- db/migrate/20180123150337_populate_issue_disposition.rb
+++ db/migrate/20180123150337_populate_issue_disposition.rb
@@ -3,8 +3,8 @@ class PopulateIssueDisposition < ActiveRecord::Migration
     Hearing.find_each(batch_size: 100) do |hearing|
       hearing.appeal.issues.each do |issue|
         worksheet_issue = WorksheetIssue.find_by(appeal: hearing.appeal, vacols_sequence_id: issue.vacols_sequence_id)
-        worksheet_issue.update(disposition: issue.formatted_disposition) if worksheet_issue.from_vacols? &&
-          !worksheet_issue.disposition
+        worksheet_issue.update(disposition: issue.formatted_disposition) if worksheet_issue &&
+          worksheet_issue.from_vacols? && !worksheet_issue.disposition
       end
     end
   end
