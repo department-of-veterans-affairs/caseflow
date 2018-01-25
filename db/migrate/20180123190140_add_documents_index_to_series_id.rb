d2d8d21e1383d865a5bbed5a2db26a1c3b749299
Mark Benjamin <mark@navahq.com>
2018-01-25 14:05:43 -0500

    Updating timing on migrations

--- db/migrate/20180123190140_add_documents_index_to_series_id.rb
+++ db/migrate/20180123190140_add_documents_index_to_series_id.rb
@@ -2,6 +2,10 @@ class AddDocumentsIndexToSeriesId < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
+    ActiveRecord::Base.connection.execute "SET statement_timeout = 900000" # 15 minutes
+
     add_index :documents, :series_id, unique: true, algorithm: :concurrently
+  ensure
+    ActiveRecord::Base.connection.execute "SET statement_timeout = 30000" # 30 seconds
   end
 end
