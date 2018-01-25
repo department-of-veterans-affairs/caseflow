121dfcde62996e7c7cf9b4c8d88e380ac881aa25
Danny Sellers <dannysellers@users.noreply.github.com>
2018-01-25 16:17:07 -0500

    Queue | Search for other appeals (#4446)
    * replace <SearchBar/> in /queue with <CaseSelectSearch/>; remove redundant actions; combine reducers
    
    * pass glamor style to CaseSelectSearch rather than SearchBar; add margin under search error alerts
    
    * adjust searchbar width when searching
    
    * fix navigating to /reader/appeal/... from /queue on search for case
    
    * always show case selection modal when searching from /queue
    
    * add integration tests for queue case search
    
    * add note to move CaseSelectSearch to queue after reader welcome gate deprecation

--- client/app/queue/QueueActions.js
+++ client/app/queue/QueueActions.js
@@ -7,14 +7,3 @@ export const onReceiveQueue = ({ tasks, appeals }) => ({
     appeals
   }
 });
-
-export const setSearch = (searchQuery) => ({
-  type: ACTIONS.SET_SEARCH,
-  payload: {
-    searchQuery
-  }
-});
-
-export const clearSearch = () => ({
-  type: ACTIONS.CLEAR_SEARCH
-});
