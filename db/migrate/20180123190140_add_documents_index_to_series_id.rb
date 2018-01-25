aa6dbb79f507f430cdafb5b3b908e2a7d111eac0
Mark Benjamin <mark@navahq.com>
2018-01-24 23:10:59 -0500

    Removing allow_nil

--- db/migrate/20180123190140_add_documents_index_to_series_id.rb
+++ db/migrate/20180123190140_add_documents_index_to_series_id.rb
@@ -2,6 +2,6 @@ class AddDocumentsIndexToSeriesId < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
-    add_index :documents, :series_id, unique: true, allow_nil: true, algorithm: :concurrently
+    add_index :documents, :series_id, unique: true, algorithm: :concurrently
   end
 end
