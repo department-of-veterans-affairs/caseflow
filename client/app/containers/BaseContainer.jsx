238be1ed2c500c4a6a72acccbe1d8811124382ea
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2018-01-25 17:33:51 -0500

    remove ref base

--- client/app/containers/BaseContainer.jsx
+++ client/app/containers/BaseContainer.jsx
@@ -11,7 +11,6 @@ import CaseWorkerIndex from './CaseWorker/CaseWorkerIndex';
 import TestPage from './TestPage';
 import Intake from '../intake';
 import Reader from '../reader/index';
-import Help from '../help/index';
 import EstablishClaimComplete from './EstablishClaimPage/EstablishClaimComplete';
 import EstablishClaimCancel from './EstablishClaimPage/EstablishClaimCanceled';
 import UnpreparedTasksIndex from './UnpreparedTasksIndex';
@@ -22,7 +21,6 @@ const Pages = {
   CaseWorkerIndex,
   CancelCertificationConfirmation,
   DecisionReviewer: Reader,
-  Help,
   Intake,
   EstablishClaim,
   EstablishClaimCancel,
