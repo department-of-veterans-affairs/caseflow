255c50687a4e1be95ab8765ce9b10474a23d78cb
Teja N <teja.s.naraparaju@gmail.com>
2018-02-02 09:32:12 -0500

    modifying the script

--- ci-bin/upload-screenshots-to-s3.bash
+++ ci-bin/upload-screenshots-to-s3.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for f in /Users/teja/git/caseflow/tmp/capybara/*.html; do 
+for f in /home/travis/build/department-of-veterans-affairs/caseflow/tmp/capybara/*.html; do 
   echo "uploading $f to s3";
   aws s3 mv $f s3://dsva-appeals-travis-builds/build-$TRAVIS_BUILD_NUMBER/$TEST_CATEGORY.$f
 done
