6bd0d16bd7499d5b555a3c60948cd87593e69ee6
aroltsch <anya.roltsch@gmail.com>
2018-02-02 11:18:01 -0500

    Add assignment added by information

--- app/models/tasks/draft_decision.rb
+++ app/models/tasks/draft_decision.rb
@@ -2,7 +2,7 @@ class DraftDecision
   include ActiveModel::Model
   include ActiveModel::Serialization
 
-  ATTRS = [:appeal_id, :user_id, :due_on, :assigned_on, :docket_name, :docket_date].freeze
+  ATTRS = [:appeal_id, :user_id, :due_on, :assigned_on, :docket_name, :docket_date, :added_by_name, :added_by_css_id].freeze
   attr_accessor(*ATTRS)
 
   # The serializer requires a method with the name `id`
@@ -15,6 +15,12 @@ class DraftDecision
       assigned_on: case_assignment.date_assigned,
       due_on: case_assignment.date_due,
       docket_name: "legacy",
+      added_by_name: FullName.new(
+                        case_assignment.added_by_first_name,
+                        case_assignment.added_by_middle_name,
+                        case_assignment.added_by_last_name
+                      ).formatted(:readable_full),
+      added_by_css_id: case_assignment.added_by_css_id.present? ? case_assignment.added_by_css_id : "",
       docket_date: case_assignment.docket_date,
       appeal_id: case_assignment.vacols_id,
       user_id: user_id
