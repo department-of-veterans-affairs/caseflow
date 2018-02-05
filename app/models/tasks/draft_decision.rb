1124417d1361e44220757285b73c15defde38bbd
aroltsch <anya.roltsch@gmail.com>
2018-02-05 11:52:00 -0500

    RRRRubocop

--- app/models/tasks/draft_decision.rb
+++ app/models/tasks/draft_decision.rb
@@ -2,7 +2,8 @@ class DraftDecision
   include ActiveModel::Model
   include ActiveModel::Serialization
 
-  ATTRS = [:appeal_id, :user_id, :due_on, :assigned_on, :docket_name, :docket_date, :added_by_name, :added_by_css_id].freeze
+  ATTRS = [:appeal_id, :user_id, :due_on, :assigned_on, :docket_name,
+           :docket_date, :added_by_name, :added_by_css_id].freeze
   attr_accessor(*ATTRS)
 
   # The serializer requires a method with the name `id`
@@ -16,11 +17,11 @@ class DraftDecision
       due_on: case_assignment.date_due,
       docket_name: "legacy",
       added_by_name: FullName.new(
-                        case_assignment.added_by_first_name,
-                        case_assignment.added_by_middle_name,
-                        case_assignment.added_by_last_name
-                      ).formatted(:readable_full),
-      added_by_css_id: case_assignment.added_by_css_id.present? ? case_assignment.added_by_css_id : "",
+        case_assignment.added_by_first_name,
+        case_assignment.added_by_middle_name,
+        case_assignment.added_by_last_name
+      ).formatted(:readable_full),
+      added_by_css_id: case_assignment.added_by_css_id.presence || "",
       docket_date: case_assignment.docket_date,
       appeal_id: case_assignment.vacols_id,
       user_id: user_id
