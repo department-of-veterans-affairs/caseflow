781fcb9deb465e9163a8a8dd79d497a3260b731b
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2018-02-06 10:24:39 -0500

    app segment

--- client/app/errors/Error500.jsx
+++ client/app/errors/Error500.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
 class Error500 extends React.PureComponent {
 
   render() {
-    return <div className="cf-txt-c cf-app-segment cf-app-segment--alt">
+    return <AppSegment filledBackground>
       <h1 className="cf-red-text cf-msg-screen-heading">Something went wrong.</h1>
       <p className="cf-msg-screen-text">
           If you continue to see this page, please contact the help desk.</p>
-    </div>;
-
+    </AppSegment>;
   }
 }
 
