31ae7207cad44aae4634eaa4473105446fef2b93
Shane Russell <shane1337@gmail.com>
2018-02-06 19:45:14 -0500

    Intake | Add number/percentage received by notice date (#4566)

--- spec/feature/intake_stats_spec.rb
+++ spec/feature/intake_stats_spec.rb
@@ -17,21 +17,36 @@ RSpec.feature "Intake Stats Dashboard" do
       end_product_reference_id: "132"
     )
 
+    RampElection.create!(
+      veteran_file_number: "77776663",
+      notice_date: 5.days.ago,
+      receipt_date: 45.minutes.ago,
+      end_product_reference_id: "132"
+    )
+
     expect(CalculateIntakeStatsJob).to receive(:perform_later)
 
     visit "/intake/stats"
-    expect(page).to have_content("RAMP Elections for January (so far)")
-    expect(page).to have_content("Sent 2")
-    expect(page).to have_content("Successfully Received 1")
-    expect(page).to have_content("Median Response Time 7.00 days")
+    expect(find("#ramp-elections-sent")).to have_content("RAMP Elections Sent for January (so far)")
+    expect(find("#ramp-elections-sent")).to have_content("Total 3")
+    expect(find("#ramp-elections-sent")).to have_content("Number Returned 1")
+    expect(find("#ramp-elections-sent")).to have_content("Percentage Returned 33 %")
+
+    expect(find("#ramp-elections-received")).to have_content("RAMP Elections Received for January (so far)")
+    expect(find("#ramp-elections-received")).to have_content("Total 2")
+    expect(find("#ramp-elections-received")).to have_content("Median Response Time 7.00 days")
 
     expect(CalculateIntakeStatsJob).to receive(:perform_later)
 
     click_on "Daily"
-    expect(page).to have_content("RAMP Elections for January 7")
-    expect(page).to have_content("Sent 0")
-    expect(page).to have_content("Successfully Received 1")
-    expect(page).to have_content("Median Response Time 7.00 days")
+    expect(find("#ramp-elections-sent")).to have_content("RAMP Elections Sent for January 7")
+    expect(find("#ramp-elections-sent")).to have_content("Total 0")
+    expect(find("#ramp-elections-sent")).to have_content("Number Returned 0")
+    expect(find("#ramp-elections-sent")).to have_content("Percentage Returned ?? %")
+
+    expect(find("#ramp-elections-received")).to have_content("RAMP Elections Received for January 7")
+    expect(find("#ramp-elections-received")).to have_content("Total 2")
+    expect(find("#ramp-elections-received")).to have_content("Median Response Time 7.00 days")
   end
 
   scenario "Unauthorized user access" do
