7a2302e4321933893fb169379019fa9e68ef65cb
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2018-02-07 11:10:38 -0500

    use react full layout

--- client/app/errors/Error500.jsx
+++ client/app/errors/Error500.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import AppFrame from '../components/AppFrame';
+import NavigationBar from '../components/NavigationBar';
+import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer';
+import { COLORS } from '@department-of-veterans-affairs/caseflow-frontend-toolkit/util/StyleConstants';
+import { BrowserRouter } from 'react-router-dom';
 
 class Error500 extends React.PureComponent {
 
   render() {
-    return <AppSegment filledBackground>
-      <h1 className="cf-red-text cf-msg-screen-heading">Something went wrong.</h1>
-      <p className="cf-msg-screen-text">
-          If you continue to see this page, please contact the help desk.</p>
-    </AppSegment>;
+    return <BrowserRouter>
+      <div>
+        <NavigationBar
+          dropdownUrls={this.props.dropdownUrls}
+          appName="Error 500"
+          defaultUrl="/"
+          logoProps={{
+            accentColor: COLORS.GREY_DARK,
+            overlapColor: COLORS.GREY_DARK
+          }} />
+        <AppFrame>
+          <AppSegment filledBackground>
+            <h1 className="cf-red-text cf-msg-screen-heading">Something went wrong.</h1>
+            <p className="cf-msg-screen-text">If you continue to see this page, please contact the help desk.</p>
+          </AppSegment>
+        </AppFrame>
+        <Footer
+          appName="Help"
+          feedbackUrl={this.props.feedbackUrl}
+          buildDate={this.props.buildDate} />
+      </div>
+    </BrowserRouter>
+    ;
   }
 }
 
