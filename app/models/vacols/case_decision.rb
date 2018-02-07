43854a16b44eb112270db6fa89b808ae9178011f
aroltsch <anya.roltsch@gmail.com>
2018-02-07 09:29:24 -0500

    Create vacols function to update OMO request

--- app/models/vacols/case_decision.rb
+++ app/models/vacols/case_decision.rb
@@ -3,4 +3,26 @@ class VACOLS::CaseDecision < VACOLS::Record
   self.primary_key = "defolder"
 
   has_one :case, foreign_key: :bfkey
+
+  # :nocov:
+  class << self
+    def find_by_vacols_id_and_css_id(css_id, vacols_id)
+      css_id = connection.quote(css_id.upcase)
+      vacols_id = connection.quote(vacols_id)
+
+      where(defolder: vacols_id)
+        .where(decomp: nil)
+        .where("staff.sdomainid = #{css_id}")
+        .joins(<<-SQL)
+          JOIN brieff
+            ON brieff.bfkey = decass.defolder
+          JOIN staff
+            ON brieff.bfcurloc = staff.slogid
+        SQL
+    end
+  end
+
+  def update_case_decision!()
+  end
+  # :nocov:
 end
