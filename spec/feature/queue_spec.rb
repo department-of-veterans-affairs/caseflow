4002357abfe58839d9c8db96d0db7d544cab85d3
Danny Sellers <dsellers90@gmail.com>
2018-02-07 10:49:31 -0500

    test when tasks are assigned / not

--- spec/feature/queue_spec.rb
+++ spec/feature/queue_spec.rb
@@ -100,6 +100,32 @@ RSpec.feature "Queue" do
   end
 
   context "loads task detail views" do
+    context "displays who assigned task" do
+      scenario "appeal has assigner" do
+        appeal = vacols_appeals.reject { |a| a.added_by_first_name.nil? }.first
+        visit "/queue"
+
+        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}']")
+
+        added_by_name = FullName.new(
+          appeal.added_by_first_name,
+          appeal.added_by_middle_name,
+          appeal.added_by_last_name
+        ).formatted(:readable_full)
+
+        expect(page).to have_content("Assigned to you by #{added_by_name} on #{appeal.date_assigned.strftime("%m/%d/%y")}")
+      end
+
+      scenario "appeal has no assigner" do
+        appeal = vacols_appeals.select { |a| a.added_by_first_name.nil? }.first
+        visit "/queue"
+
+        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}']")
+
+        expect(page).to have_content("Assigned to you on #{appeal.date_assigned.strftime("%m/%d/%y")}")
+      end
+    end
+
     context "loads appeal summary view" do
       scenario "appeal has hearing" do
         appeal = vacols_appeals.reject { |a| a.hearings.empty? }.first
@@ -107,7 +133,7 @@ RSpec.feature "Queue" do
 
         visit "/queue"
 
-        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}'")
+        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}']")
 
         expect(page).to have_content("Hearing Preference: #{hearing.type.capitalize}")
         expect(page).to have_content("Hearing held: #{hearing.date.strftime('%-m/%e/%y')}")
@@ -120,7 +146,7 @@ RSpec.feature "Queue" do
 
         visit "/queue"
 
-        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}'")
+        safe_click("a[href='/queue/tasks/#{appeal.vacols_id}']")
 
         expect(page).not_to have_content("Hearing Preference")
 
