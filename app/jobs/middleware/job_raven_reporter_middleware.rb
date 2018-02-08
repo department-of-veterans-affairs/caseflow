2427c90351d82ef2e3de3c3922c21974578af8ac
Evan Kroske <evan@navahq.com>
2018-02-08 11:07:50 -0500

    Fixes lint errors

--- app/jobs/middleware/job_raven_reporter_middleware.rb
+++ app/jobs/middleware/job_raven_reporter_middleware.rb
@@ -1,11 +1,11 @@
 # This job captures all Shoryuken job exceptions and forwards them to Raven.
 #
 class JobRavenReporterMiddleware
-  def call(_worker, _queue, _msg, _body)
+  def call(_worker, queue, _msg, body)
     yield
   rescue StandardError => ex
-    tags = {job: _body['job_class'], queue: _queue}
-    context = {message: _body}
+    tags = { job: body["job_class"], queue: queue }
+    context = { message: body }
     Raven.capture_exception(ex, tags: tags, extra: context)
     raise
   end
