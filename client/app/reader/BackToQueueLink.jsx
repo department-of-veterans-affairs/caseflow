c79ccff93e1ffc4434922c178e80e00d1077d3ac
Alex Prokop <alex.prokop@gmail.com>
2018-02-12 09:37:39 -0700

    uses task code if provided

--- client/app/reader/BackToQueueLink.jsx
+++ client/app/reader/BackToQueueLink.jsx
@@ -6,9 +6,33 @@ const segmentStyling = css({
   marginTop: '3rem'
 });
 
-const BackToQueueLink = ({ queueRedirectUrl }) =>
-  <div {...segmentStyling}>
-    <Link href={queueRedirectUrl}>&lt; Back to Your Queue</Link>
-  </div>;
+const TASK_TYPE_CODES = {
+  "dd": "Draft Decision"
+};
+
+class BackToQueueLink extends React.PureComponent {
+  getRedirectText = () => {
+    const {
+      taskCode,
+      veteranFullName,
+      vbmsId,
+    } = this.props;
+
+    if (taskCode && TASK_TYPE_CODES[taskCode]) {
+      return `${TASK_TYPE_CODES[taskCode]} - ${veteranFullName} (${vbmsId})`
+    }
+    return 'Your Queue';
+  }
+
+  render = () => {
+    const {
+      queueRedirectUrl
+    } = this.props;
+
+    return <div {...segmentStyling}>
+      <Link href={queueRedirectUrl}>&lt; Back to {this.getRedirectText()}</Link>
+    </div>;
+  }
+}
 
 export default BackToQueueLink;
