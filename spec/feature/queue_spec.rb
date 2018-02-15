bd8ae7499d7b34ab1c33e90ff88388d5eb2a1891
Nemo Djuric <nemo.djuric@gmail.com>
2018-02-15 16:01:38 -0500

    Fix queue spec test

--- spec/feature/queue_spec.rb
+++ spec/feature/queue_spec.rb
@@ -110,7 +110,7 @@ RSpec.feature "Queue" do
         safe_click("a[href='/queue/tasks/#{appeal.vacols_id}'")
 
         expect(page).to have_content("Hearing Preference: #{hearing.type.capitalize}")
-        expect(page).to have_content("Hearing held: #{hearing.date.strftime('%-m/%e/%y')}")
+        expect(page).to have_content("Hearing held: #{hearing.date.strftime('%-m/%-d/%y')}")
         expect(page).to have_content("Judge at hearing: #{hearing.user.full_name}")
       end
 
