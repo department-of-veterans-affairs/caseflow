a46f521d28c6e5da6cef61c60117cde8cd88bfcb
aroltsch <anya.roltsch@gmail.com>
2018-02-20 13:25:06 -0500

    RRRubocop

--- app/repositories/judge_repository.rb
+++ app/repositories/judge_repository.rb
@@ -1,7 +1,7 @@
 class JudgeRepository
   # :nocov:
   def self.find_all_judges
-    records = VACOLS::Staff.where(svlj: ["J", "A"], sactive: "A")
+    records = VACOLS::Staff.where(svlj: %w[J A], sactive: "A")
     records.map { |record| Judge.create_from_vacols(record) if record.sdomainid }
   end
   # :nocov:
