11dc34a27ed4bafc1c47a4ee6c98b16753d35764
Teja N <teja.s.naraparaju@gmail.com>
2018-02-21 17:56:45 -0500

    remove review claims folder button

--- client/app/hearings/HearingWorksheet.jsx
+++ client/app/hearings/HearingWorksheet.jsx
@@ -163,12 +163,6 @@ export class HearingWorksheet extends React.PureComponent {
           button="secondary">
           Save as PDF
         </Link>
-        <Link
-          name="review-efolder"
-          href={`${readerLink}?category=case_summary`}
-          button="primary"
-          target="_blank">
-            Review Claims Folder</Link>
       </div>
       }
     </div>;
