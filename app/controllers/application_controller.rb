42034ae48b222514d2240a4e3a23d9f81f0e3962
Mark Benjamin <mark@navahq.com>
2018-02-22 16:51:19 -0500

    Setting up new development environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -202,7 +202,7 @@ class ApplicationController < ApplicationBaseController
 
   class << self
     def dependencies_faked?
-      Rails.env.development? || Rails.env.test? || Rails.env.demo? || Rails.env.ssh_forwarding?
+      Rails.env.development? || Rails.env.test? || Rails.env.demo? || Rails.env.ssh_forwarding? || Rails.env.local_vacols?
     end
   end
 end
