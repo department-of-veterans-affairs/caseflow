97c953937b0df722dab20b1e2d4be3e080294a7c
aroltsch <anya.roltsch@gmail.com>
2018-02-23 09:07:48 -0500

    Rubocop

--- spec/models/attorney_case_reviews/attorney_case_review_spec.rb
+++ spec/models/attorney_case_reviews/attorney_case_review_spec.rb
@@ -35,7 +35,7 @@ describe AttorneyCaseReview do
     context "when all parameters are present and Vacols update is successful" do
       before do
         allow(QueueRepository).to receive(:reassign_case_to_judge).with(params.merge(judge_css_id: judge.css_id))
-                                                                  .and_return(true)
+          .and_return(true)
       end
 
       let(:params) do
