4ca02393c63d788e0eaea74a688061e5df370a31
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2018-03-01 09:41:30 -0500

    removed dispatch code from base

--- client/app/containers/BaseContainer.jsx
+++ client/app/containers/BaseContainer.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // components
-import Alert from '../components/Alert';
-
-// container components
 import StyleGuideIndex from './StyleGuide/StyleGuideIndex';
 import TestPage from './TestPage';
 import Intake from '../intake';
@@ -17,65 +14,26 @@ const Pages = {
   TestPage
 };
 
-// This is the "base page" that wraps all pages rendered directly
+// This is the "base page" that wraps pages rendered directly
 // in a Rails view. This component manages interactivity that exists
-// across *all* React page. For now that includes:
-//   - alerts
+// across React pages imported.
 //
 // The `page` property dictates which page is rendered within this
-// component. e.g.  <BaseContainer page="EstablishClaim" />
+// component. e.g.  <BaseContainer page="StyleGuideIndex" />
 
 export default class BaseContainer extends React.Component {
   constructor(props) {
     super(props);
     window.jqueryOn = false;
-
-    this.state = { alert: null };
-  }
-
-  handleAlert = (type, title, message) => {
-    this.setState({
-      alert: {
-        message,
-        title,
-        type
-      }
-    });
-  }
-
-  handleAlertClear = () => {
-    this.setState({ alert: null });
   }
 
   render() {
     // `rest` signifies all the props passed in from Rails that
     // we want to send directly to the PageComponent
-    let {
-      page,
-      ...rest
-    } = this.props;
-
-    let {
-      alert
-    } = this.state;
-
+    let { page, ...rest } = this.props;
     let PageComponent = Pages[page];
 
-    return <div>
-      {alert && <div className="cf-app-segment">
-        <Alert
-          type={alert.type}
-          title={alert.title}
-          message={alert.message}
-          handleClear={this.handleAlertClear}
-        />
-      </div>}
-      <PageComponent
-        {...rest}
-        handleAlert={this.handleAlert}
-        handleAlertClear={this.handleAlertClear}
-      />
-    </div>;
+    return <PageComponent {...rest} />
   }
 }
 
