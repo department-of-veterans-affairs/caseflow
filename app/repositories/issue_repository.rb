dab0ec6075a43a3548fbf5e7ed05da1f93e4b16d
aroltsch <anya.roltsch@gmail.com>
2018-03-05 13:29:22 -0500

    Change query

--- app/repositories/issue_repository.rb
+++ app/repositories/issue_repository.rb
@@ -13,7 +13,13 @@ class IssueRepository
   def self.create_vacols_issue(css_id, issue_hash)
     issue_hash = IssueMapper.transform_issue_hash(issue_hash)
 
-    validate_issue_codes(issue_hash)
+    if find_issue_reference(program: issue_hash[:program],
+                            issue: issue_hash[:issue],
+                            level_1: issue_hash[:level_1],
+                            level_2: issue_hash[:level_2],
+                            level_3: issue_hash[:level_3]).blank?
+      fail IssueCreationError, "Combination of Vacols Issue codes is invalid: #{issue_hash}"
+    end
 
     staff = VACOLS::Staff.find_by(sdomainid: css_id)
     fail IssueCreationError, "Cannot find user with #{css_id} in VACOLS" unless staff
@@ -24,34 +30,11 @@ class IssueRepository
     Issue.load_from_vacols(record.attributes)
   end
 
-  def self.find_issue_reference(program, issue, level_1)
-    VACOLS::IssueReference.where(
-      prog_code: program,
-      iss_code: issue,
-      lev1_code: level_1
-    )
+  def self.find_issue_reference(program:, issue:, level_1:, level_2:, level_3:)
+    VACOLS::IssueReference.where(prog_code: program, iss_code: issue)
+                          .where("? is null or LEV1_CODE = '##' or LEV1_CODE = ?", level_1, level_1)
+                          .where("? is null or LEV2_CODE = '##' or LEV2_CODE = ?", level_2, level_2)
+                          .where("? is null or LEV3_CODE = '##' or LEV3_CODE = ?", level_3, level_3)
   end
   # :nocov:
-
-  def self.validate_issue_codes(issue_hash)
-    # Ensure combination of PROGAM, ISSUE and LEVEL_1 codes is valid by querying the VACOLS.ISSREF table
-    if find_issue_reference(issue_hash[:program], issue_hash[:issue], issue_hash[:level_1]).empty?
-      fail IssueCreationError, "Combination of Vacols Issue codes is invalid: #{issue_hash}"
-    end
-
-    # Level 2 can be:
-    # a) nil
-    # b) any number from: ["01", "02", "03", "04", "05", "06", "07"]
-    # c) 4-digit number
-    if issue_hash[:level_2] && issue_hash[:level_2] !~ /\A[0-9]{4}|0[1-7]{1}\Z/i
-      fail IssueCreationError, "Level 2 is invalid: #{issue_hash[:level_2]}"
-    end
-
-    # Level 3 can be:
-    # a) nil
-    # b) 4-digit number
-    if issue_hash[:level_3] && issue_hash[:level_3] !~ /\A[0-9]{4}\Z/i
-      fail IssueCreationError, "Level 3 is invalid: #{issue_hash[:level_3]}"
-    end
-  end
 end
