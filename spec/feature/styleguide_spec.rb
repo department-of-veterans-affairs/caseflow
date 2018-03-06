cf9f12e3311306a1e55074228d7a2e9169f59bde
Mark Ferlatte <mark@truss.works>
2018-03-06 14:17:03 -0800

    Stop focusing on styleguide.

--- spec/feature/styleguide_spec.rb
+++ spec/feature/styleguide_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Style Guide" do
-  fscenario "renders and is accessible" do
+  scenario "renders and is accessible" do
     visit "/styleguide"
     expect(page).to have_content("Caseflow Commons")
   end
