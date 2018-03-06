bfee2faa1c4f55f2ae1e84a46e6037d75ce47c73
Mark Ferlatte <mark@truss.works>
2018-03-06 13:55:35 -0800

    Focus on the styleguide spec.

--- spec/feature/styleguide_spec.rb
+++ spec/feature/styleguide_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Style Guide" do
-  scenario "renders and is accessible" do
+  fscenario "renders and is accessible" do
     visit "/styleguide"
     expect(page).to have_content("Caseflow Commons")
   end
