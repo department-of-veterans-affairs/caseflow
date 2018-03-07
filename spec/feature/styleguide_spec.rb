c398c1dfe1d03adf1e4a0a125a931d70932fd9bf
Mark Ferlatte <mark@truss.works>
2018-03-06 16:34:09 -0800

    Try using the selenium_chrome_headless driver.

--- spec/feature/styleguide_spec.rb
+++ spec/feature/styleguide_spec.rb
@@ -1,33 +1,14 @@
 require "rails_helper"
-Capybara.register_driver :slow_sniffybara do |app|
-  chrome_options = ::Selenium::WebDriver::Chrome::Options.new
-
-  chrome_options.add_preference(:download,
-                                prompt_for_download: false,
-                                default_directory: download_directory)
-
-  chrome_options.add_preference(:browser,
-                                disk_cache_dir: cache_directory)
-
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 60 # instead of the default 60
-
-  options = {
-    port: 51_674,
-    browser: :chrome,
-    options: chrome_options,
-    client: client
-  }
-
-  Sniffybara::Driver.current_driver = Sniffybara::Driver.new(app, options)
-end
 
 RSpec.feature "Style Guide" do
+  before do
+    Capybara.current_driver = :selenium_chrome_headless
+  end
+  after do
+    Capybara.use_default_driver
+  end
   scenario "renders and is accessible" do
-    original_driver = Sniffybara::Driver.current_driver
-    Sniffybara::Driver.current_driver = :slow_sniffybara
     visit "/styleguide"
     expect(page).to have_content("Caseflow Commons")
-    Sniffybara::Driver.current_driver = original_driver
   end
 end
