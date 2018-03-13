3933d300e376770fc236de29778ddda08de9e0f9
Chris Given <cmgiven@gmail.com>
2018-03-13 14:32:40 -0400

    Add status for unmatched merges (#4841)

--- app/models/appeal_series.rb
+++ app/models/appeal_series.rb
@@ -191,6 +191,7 @@ class AppealSeries < ActiveRecord::Base
     end
   end
 
+  # rubocop:disable MethodLength
   def disambiguate_status_complete
     case latest_appeal.disposition
     when "Allowed", "Denied"
@@ -208,10 +209,13 @@ class AppealSeries < ActiveRecord::Base
       :death
     when "Reconsideration by Letter"
       :reconsideration
+    when "Merged Appeal"
+      :merged
     else
       :other_close
     end
   end
+  # rubocop:enable MethodLength
 
   def disambiguate_status_remand
     post_decision_ssocs = latest_appeal.ssoc_dates.select { |ssoc| ssoc > latest_appeal.decision_date }
