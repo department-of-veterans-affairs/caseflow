8e3429dfdcd0025199e862b510ce154110a63d51
Sjones352 <sjones@tistatec.com>
2018-03-15 09:56:58 -0400

    added worksheet footer

---
+++ client/app/hearings/components/WorksheetFooter.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class WorksheetFooter extends React.PureComponent {
+
+  render() {
+    const {
+      worksheet
+    } = this.props;
+
+    const veteranName = worksheet.veteran_fi_last_formatted;
+
+    return <div className="cf-footer">
+      {this.props.print &&
+     <div className="cf-push-right">
+       {veteranName}
+       <span className="cf-print-number" />
+     </div>
+      }
+    </div>;
+
+  }
+
+}
+
+const mapStateToProps = (state) => ({
+  worksheet: state.worksheet
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(WorksheetFooter);
