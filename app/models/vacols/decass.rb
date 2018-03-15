b3ea691e43b63f2aa076dfa27d84e842fc20a78d
aroltsch <anya.roltsch@gmail.com>
2018-03-15 12:59:10 -0400

    Block updating/deleting records that do not have uniq primary key

--- app/models/vacols/decass.rb
+++ app/models/vacols/decass.rb
@@ -2,5 +2,23 @@ class VACOLS::Decass < VACOLS::Record
   self.table_name = "vacols.decass"
   self.primary_key = "defolder"
 
+  class DecassError < StandardError; end
+
   has_one :case, foreign_key: :bfkey
+
+  def update(*)
+    update_error_message
+  end
+
+  def update!(*)
+    update_error_message
+  end
+
+  private
+
+  def update_error_message
+    fail DecassError, "Since the primary key is not unique, `update` will update all results
+      with the same `defolder`. Instead use QueueRepository.update_decass_record
+      that uses `defolder` and `deassign` to safely update one record"
+  end
 end
