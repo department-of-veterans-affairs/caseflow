c38c0376e4c6a114aafcccfcb86f2d23045ad5f3
Teja N <teja.s.naraparaju@gmail.com>
2018-03-20 14:49:24 -0400

    moving state to redux

--- client/app/hearings/Dockets.jsx
+++ client/app/hearings/Dockets.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import _ from 'lodash';
@@ -9,9 +10,17 @@ import { Link } from 'react-router-dom';
 import { getDateTime } from './util/DateUtil';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import { css } from 'glamor';
-import { CATEGORIES, ACTIONS } from './analytics';
 
-const PAST_HEARING_TAB_INDEX = 1;
+import { CATEGORIES, ACTIONS } from './analytics';
+import { selectDocketsPageTabIndex } from './actions/Dockets';
+
+const UPCOMING_TAB_INDEX = 0;
+const PAST_TAB_INDEX = 1;
+
+export const DOCKETS_TAB_INDEX_MAPPING = {
+  [UPCOMING_TAB_INDEX]: 'Upcoming',
+  [PAST_TAB_INDEX]: 'Past'
+};
 
 const tableBorder = css({
   border: '1px solid #dadbdc',
@@ -30,13 +39,6 @@ const tabBodyStyling = css({
 });
 
 export class Dockets extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      viewingPastTab: false
-    };
-  }
 
   getType = (type) => {
     const capitalizeFirstChar = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
@@ -50,7 +52,7 @@ export class Dockets extends React.Component {
   }
 
   dateClicked = () => {
-    const action = this.state.viewingPastTab ? ACTIONS.OPEN_PAST_HEARING_DOCKET :
+    const action = this.props.docketsTabIndex === PAST_TAB_INDEX ? ACTIONS.OPEN_PAST_HEARING_DOCKET :
       ACTIONS.OPEN_CURRENT_HEARING_DOCKET;
 
     window.analyticsEvent(CATEGORIES.HEARINGS_DAYS_PAGE, action);
@@ -66,20 +68,7 @@ export class Dockets extends React.Component {
     </Link>;
   }
 
-  onTabSelected = (tabNumber) => {
-    if (tabNumber === PAST_HEARING_TAB_INDEX) {
-      this.togglePastDocketsTab(true);
-      window.analyticsEvent(CATEGORIES.HEARINGS_DAYS_PAGE, ACTIONS.OPEN_PAST_HEARINGS_TAB);
-    } else {
-      this.togglePastDocketsTab(false);
-    }
-  }
-
-  togglePastDocketsTab = (isPastTab) => {
-    this.setState({
-      viewingPastTab: isPastTab
-    });
-  }
+  onTabSelected = (tabNumber) => this.props.selectDocketsPageTabIndex(tabNumber);
 
   getScheduledCount = (docket) => {
     return (docket.master_record ? 0 : docket.hearings_count);
@@ -155,7 +144,7 @@ export class Dockets extends React.Component {
 
     const tabs = [
       {
-        label: 'Upcoming',
+        label: DOCKETS_TAB_INDEX_MAPPING[UPCOMING_TAB_INDEX],
         page: _.size(upcomingRowObjects) ? <Table
           className="hearings"
           columns={columns}
@@ -167,7 +156,7 @@ export class Dockets extends React.Component {
         /> : <p>You currently have no hearings scheduled.</p>
       },
       {
-        label: 'Past',
+        label: DOCKETS_TAB_INDEX_MAPPING[PAST_TAB_INDEX],
         page: _.size(pastRowObjects) ? <Table
           className="hearings"
           columns={columns}
@@ -197,11 +186,17 @@ export class Dockets extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  upcomingHearings: state.upcomingHearings
+  upcomingHearings: state.upcomingHearings,
+  docketsTabIndex: state.docketsTabIndex
 });
 
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  selectDocketsPageTabIndex
+}, dispatch);
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Dockets);
 
 Dockets.propTypes = {
