a470a17802afbd47a9a4add3d862d95697920d2e
Sunil Sadasivan <sunil@navahq.com>
2018-03-23 11:54:29 -0400

    Switch to localstack dependency instead of goaws

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -8,8 +8,13 @@ ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
   .shoryuken_options(retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours])
 
 if ENV['DEPLOY_ENV'] == 'local' or ENV['DEPLOY_ENV'] == 'development'
-  # use a locally mocked SQS server endpoint instead of AWS
-  Shoryuken::Client.sqs.config[:endpoint]=URI('http://localhost:4100')
+  # use a locally mocked SQS server endpoint instead of AWS. We use localstack to mock this.
+  # https://github.com/localstack/localstack/
+  Shoryuken::Client.sqs.config[:endpoint]=URI('http://localhost:4576')
+
+  # create the development queues
+  Shoryuken::Client.sqs.create_queue({ queue_name: ActiveJob::Base.queue_name_prefix + '_low_priority' })
+  Shoryuken::Client.sqs.create_queue({ queue_name: ActiveJob::Base.queue_name_prefix + '_high_priority' })
 end
 
 Shoryuken.configure_server do |config|
