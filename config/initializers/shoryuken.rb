5e326e15cc18e7d921ca836d4cd81fa0c8176d38
Sunil Sadasivan <sunil@navahq.com>
2018-03-23 10:25:57 -0400

    Initial checkin to Support sqs locally

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -7,6 +7,11 @@ require "#{Rails.root}/app/jobs/middleware/job_request_store_middleware"
 ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
   .shoryuken_options(retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours])
 
+if ENV['DEPLOY_ENV'] == 'local' or ENV['DEPLOY_ENV'] == 'development'
+  # use a locally mocked SQS server endpoint instead of AWS
+  Shoryuken::Client.sqs.config[:endpoint]=URI('http://localhost:4100')
+end
+
 Shoryuken.configure_server do |config|
 
   # register all shoryuken middleware
