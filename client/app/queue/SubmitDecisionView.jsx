d92e92e5eb49fc7acc42e5d2615fa34c5e680614
Danny Sellers <dsellers90@gmail.com>
2018-03-27 17:33:05 -0400

    call type type

--- client/app/queue/SubmitDecisionView.jsx
+++ client/app/queue/SubmitDecisionView.jsx
@@ -113,10 +113,8 @@ class SubmitDecisionView extends React.PureComponent {
       data: {
         queue: {
           type: decision.type,
-          issues: _.map(issues, (issue) => ({
-            issue: issue.type,
-            ..._.pick(issue, 'disposition', 'vacols_sequence_id', 'remand_reasons')
-          })),
+          issues: _.map(issues, (issue) =>
+            _.pick(issue, 'disposition', 'vacols_sequence_id', 'remand_reasons', 'type')),
           ...decision.opts
         }
       }
