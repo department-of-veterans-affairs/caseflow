6583212f671786ac2dd74e38a8a602535b200cc6
Sally Maki <sallymaki@gmail.com>
2018-03-27 17:09:59 -0700

    Add control time to ramp election

--- spec/feature/intake_stats_spec.rb
+++ spec/feature/intake_stats_spec.rb
@@ -100,7 +100,6 @@ RSpec.feature "Intake Stats Dashboard" do
 
     expect(CalculateIntakeStatsJob).to receive(:perform_later)
     visit "/intake/stats"
-
     expect(find("#ramp-elections-sent")).to have_content("RAMP Elections Sent for January (so far)")
     expect(find("#ramp-elections-sent")).to have_content("Total 4")
     expect(find("#ramp-elections-sent")).to have_content("Higher Level Reviews Returned 1")
