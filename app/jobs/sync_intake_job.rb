00222ba987866340347716ce4ccb549f0b44996f
Sean Bloomfield <seanbloomfield@gmail.com>
2018-04-03 21:28:15 -0700

    WIP: need tests

--- app/jobs/sync_intake_job.rb
+++ app/jobs/sync_intake_job.rb
@@ -4,10 +4,20 @@ class SyncIntakeJob < ActiveJob::Base
   queue_as :low_priority
 
   def perform
-    # First sync the issues
-    RampElection.active.map(&:recreate_issues_from_contentions!)
+    RampElection.active.map do |ramp_election|
+        # Set user to user who established to avoid sensitivity errors
+        intake = RampElectionIntake
+            .where(detail_id: ramp_election.id, completion_status: "success")
+            .order(:completed_at)
+            .last
+        return unless intake
+        user = intake.user
+        RequestStore.store[:current_user] = user
+        ramp_election.recreate_issues_from_contentions!
+        ramp_election.sync_ep_status!
 
-    # Then sync the EP statuses
-    RampElection.active.map(&:sync_ep_status!)
+        # Sleep for 1 second to avoid tripping BGS alerts
+        sleep 1
+    end
   end
 end
--- spec/jobs/sync_intake_job_spec.rb
+++ spec/jobs/sync_intake_job_spec.rb
@@ -1,15 +1,17 @@
 describe SyncIntakeJob do
   context ".perform" do
     it "calls recreate_issues_from_contentions and sync_ep_status" do
-      ramp_election = instance_double(RampElection)
-      expect(RampElection).to receive(:active).and_return([ramp_election]).twice
-      allow(ramp_election).to receive(:recreate_issues_from_contentions!).and_return(true)
-      allow(ramp_election).to receive(:sync_ep_status!).and_return(true)
+      ramp_election = RampElection.create!(
+        veteran_file_number: "1",
+      )
+
+      intake = RampElectionIntake.create!(
+        user_id: 1, 
+        detail_id: ramp_election.id,
+        completion_status: "success"
+      )
 
       SyncIntakeJob.perform_now
-
-      expect(ramp_election).to have_received(:recreate_issues_from_contentions!)
-      expect(ramp_election).to have_received(:sync_ep_status!)
     end
   end
 end
