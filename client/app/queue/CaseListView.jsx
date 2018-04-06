a610269fbb60d070d5b5adb1b9829185f166b425
lowellrex <lowell@navahq.com>
2018-04-06 13:47:45 -0400

    Add back to queue list link

---
+++ client/app/queue/CaseListView.jsx
@@ -0,0 +1,56 @@
+import { css } from 'glamor';
+import pluralize from 'pluralize';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+
+import CaseListTable from './CaseListTable';
+import { fullWidth } from './constants';
+
+import { clearCaseListSearch } from './CaseList/CaseListActions';
+
+const backLinkStyling = css({
+  float: 'left',
+  marginTop: '-3rem'
+});
+
+// TODO: Add breadcrumbs.
+class CaseListView extends React.PureComponent {
+  // TODO: What is the search results error behaviour here?
+  // As written if a search errored, the child would return to being displayed and the error would show above the list.
+  // Do we want to have the previous search stick around?
+  render() {
+    const appealsCount = this.props.caseList.receivedAppeals.length;
+
+    // Using the first appeal in the list to get the Veteran's name and ID. We expect that data to be
+    // the same for all appeals in the list.
+    const firstAppeal = this.props.caseList.receivedAppeals[0];
+    const docCount = this.props.caseList.documentCountForVeteran;
+
+    return <React.Fragment>
+      <div {...backLinkStyling}>
+        <Link to='/' onClick={this.props.clearCaseListSearch}>&lt; Back to Your Queue</Link>
+      </div>
+      <AppSegment filledBackground>
+        <div>
+          <h1 className="cf-push-left" {...fullWidth}>
+            {appealsCount} {pluralize('case', appealsCount)} found for "{firstAppeal.attributes.veteran_full_name} ({firstAppeal.attributes.vbms_id})"
+          </h1>
+          <p>View { docCount && `${docCount} ` }documents in Caseflow Reader</p>
+          <CaseListTable appeals={this.props.caseList.receivedAppeals} />
+        </div>
+      </AppSegment>
+    </React.Fragment>;
+  };
+}
+
+const mapStateToProps = (state) => ({
+  caseList: state.caseList
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ clearCaseListSearch }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CaseListView);
--- client/app/queue/SearchEnabledView.jsx
+++ client/app/queue/SearchEnabledView.jsx
@@ -1,15 +1,10 @@
 import { css } from 'glamor';
-import pluralize from 'pluralize';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
 import CaseListSearch from './CaseListSearch';
-import CaseListTable from './CaseListTable';
-import { fullWidth } from './constants';
+import CaseListView from './CaseListView';
 
 const searchStyling = (isRequestingAppealsUsingVeteranId) => css({
   '.section-search': {
@@ -26,47 +21,29 @@ const searchStyling = (isRequestingAppealsUsingVeteranId) => css({
   }
 });
 
-// TODO: Add breadcrumbs.
 class SearchEnabledView extends React.PureComponent {
-  viewBody = () => {
-    const appealsCount = this.props.caseList.receivedAppeals.length;
-
-    if (this.props.caseList.shouldUseAppealSearch && appealsCount > 0) {
-      // Using the first appeal in the list to get the Veteran's name and ID. We expect that data to be
-      // the same for all appeals in the list.
-      const firstAppeal = this.props.caseList.receivedAppeals[0];
-      const docCount = this.props.caseList.documentCountForVeteran;
-
-      return <AppSegment filledBackground>
-        <div>
-          <h1 className="cf-push-left" {...fullWidth}>
-            {appealsCount} {pluralize('case', appealsCount)} found for "{firstAppeal.attributes.veteran_full_name} ({firstAppeal.attributes.vbms_id})"
-          </h1>
-          <p>View { docCount && `${docCount} ` }documents in Caseflow Reader</p>
-          <CaseListTable appeals={this.props.caseList.receivedAppeals} />
-        </div>
-      </AppSegment>;
-    }
-
-    return this.props.children;
-  }
-
   // TODO: What is the search results error behaviour here?
   // As written if a search errored, the child would return to being displayed and the error would show above the list.
   // Do we want to have the previous search stick around?
   render() {
+    const {
+      appealCount,
+      feedbackUrl,
+      isRequestingAppealsUsingVeteranId,
+      shouldUseAppealSearch
+    } = this.props;
+
     return <React.Fragment>
       <CaseListSearch
         navigateToPath={(path) => {
           const redirectUrl = encodeURIComponent(window.location.pathname);
-
           location.href = `/reader/appeal${path}?queue_redirect_url=${redirectUrl}`;
         }}
         alwaysShowCaseSelectionModal
-        feedbackUrl={this.props.feedbackUrl}
+        feedbackUrl={feedbackUrl}
         searchSize="big"
-        styling={searchStyling(this.props.isRequestingAppealsUsingVeteranId)} />
-        { this.viewBody() }
+        styling={searchStyling(isRequestingAppealsUsingVeteranId)} />
+        { shouldUseAppealSearch && appealCount > 0 ? <CaseListView /> : this.props.children }
       </React.Fragment>;
   };
 }
@@ -76,8 +53,9 @@ SearchEnabledView.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  caseList: state.caseList,
-  isRequestingAppealsUsingVeteranId: state.caseList.isRequestingAppealsUsingVeteranId
+  appealCount: state.caseList.receivedAppeals.length,
+  isRequestingAppealsUsingVeteranId: state.caseList.isRequestingAppealsUsingVeteranId,
+  shouldUseAppealSearch: state.caseList.shouldUseAppealSearch
 });
 
 export default connect(mapStateToProps)(SearchEnabledView);
