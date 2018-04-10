25da741f6c3bdd121133daadbb77837db55c3bb9
Sean Bloomfield <seanbloomfield@gmail.com>
2018-04-10 16:49:40 -0700

    Makes SyncIntakeJob use system user

--- app/jobs/sync_intake_job.rb
+++ app/jobs/sync_intake_job.rb
@@ -5,18 +5,19 @@ class SyncIntakeJob < ApplicationJob
 
   def perform
     RampElection.active.each do |ramp_election|
-      # Set user to user who established to avoid sensitivity errors
-      # TODO: not all RampElections will have an Intake
-      # so we may need to figure out a default user
-      RequestStore.store[:current_user] = ramp_election.successful_intake.user if ramp_election.successful_intake
+      # Set user to system_user to avoid sensitivity errors
+      RequestStore.store[:current_user] = User.system_user
 
-      ramp_election.recreate_issues_from_contentions!
-      ramp_election.sync_ep_status!
+      begin
+        ramp_election.recreate_issues_from_contentions!
+        ramp_election.sync_ep_status!
+      rescue ActiveRecord::RecordInvalid
+        Rails.logger.error "SyncIntakeJob failed for #{e.message}"
+        Raven.capture_exception(e)
+      end
 
       # Sleep for 1 second to avoid tripping BGS alerts
       sleep 1
-
-      # TODO: need a rescue here for sensitivity errors
     end
   end
 end
