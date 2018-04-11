bd4100a85f100c8946d269a2e05a8c9dca6632fd
lowellrex <lowell@navahq.com>
2018-04-11 17:54:49 -0400

    Clarify constants initializer

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 Constants = Object.new
 
-Dir.glob(File.join(Rails.root, "constants", "*")).each do |filename|
-  constant_name = filename.split("/").last.split(".").first
-  Constants.define_singleton_method("#{constant_name}") { JSON.parse(File.read(filename)) }
+Dir.glob(File.join(Rails.root, "constants", "*")).each do |filepath|
+  constant_name = filepath.split("/").last.split(".").first
+  Constants.define_singleton_method(constant_name.to_s) { JSON.parse(File.read(filepath)) }
 end
