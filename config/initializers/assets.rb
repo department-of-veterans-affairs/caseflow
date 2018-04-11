3d42d469a35e764573f6bb39dedef2e7e56df367
Nolawi Petros <nolawi.petros@gmail.com>
2018-04-11 11:23:16 -0400

    add fonts to the precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets 
+# application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( stats.js )
 Rails.application.config.assets.precompile += %w( pdf.worker.js )
@@ -25,4 +25,4 @@ Rails.application.config.assets.precompile << %w( *.woff *.woff2 *.eot *.ttf )
 # Rails.application.config.assets.precompile += %w( server-bundle.js )
 
 # Add folder with webpack generated assets to assets.paths
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack", "fonts")
