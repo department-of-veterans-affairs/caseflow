0e42eba9b95970e6e8c6a56a5e15187d55cd93e9
Sean Bloomfield <seanbloomfield@gmail.com>
2018-04-11 12:01:29 -0700

    lint

--- app/jobs/sync_intake_job.rb
+++ app/jobs/sync_intake_job.rb
@@ -6,18 +6,6 @@ class SyncIntakeJob < ApplicationJob
   def perform
     # Set user to system_user to avoid sensitivity errors
     RequestStore.store[:current_user] = User.system_user
-
-    RampElection.active.each do |ramp_election|
-      begin
-        ramp_election.recreate_issues_from_contentions!
-        ramp_election.sync_ep_status!
-      rescue ActiveRecord::RecordInvalid
-        Rails.logger.error "SyncIntakeJob failed for #{e.message}"
-        Raven.capture_exception(e)
-      end
-
-      # Sleep for 1 second to avoid tripping BGS alerts
-      sleep 1
-    end
+    RampElection.sync_all!
   end
 end
--- spec/jobs/sync_intake_job_spec.rb
+++ spec/jobs/sync_intake_job_spec.rb
@@ -1,27 +1,11 @@
 describe SyncIntakeJob do
   context ".perform" do
     it "calls recreate_issues_from_contentions and sync_ep_status" do
-      spy = 0
-      allow_any_instance_of(RampElection).to receive(:recreate_issues_from_contentions!) { spy += 1 }
-      allow_any_instance_of(RampElection).to receive(:sync_ep_status!) { spy += 1 }
-
-      user = User.create!(station_id: "123", css_id: "456")
-
-      ramp_election = RampElection.create!(
-        veteran_file_number: "1",
-        established_at: Time.zone.now
-      )
-
-      RampElectionIntake.create!(
-        user_id: user.id,
-        detail: ramp_election,
-        completion_status: "success"
-      )
+      expect(RampElection).to receive(:sync_all!)
 
       SyncIntakeJob.perform_now
 
       expect(RequestStore.store[:current_user].id).to eq(User.system_user.id)
-      expect(spy).to eq(2)
     end
   end
 end
