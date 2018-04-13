3463bf40993dbb41d3f60d57a7575274d142d37e
Teja N <teja.s.naraparaju@gmail.com>
2018-04-13 09:57:30 -0400

    adjusting comments

--- client/app/components/CFRichTextEditor.jsx
+++ client/app/components/CFRichTextEditor.jsx
@@ -81,9 +81,6 @@ export default class CFRichTextEditor extends React.PureComponent {
 
     if (this.props.onChange) {
       // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      // this.props.onChange(value.toString('markdown'));
       this.props.onChange(draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())));
     }
   };
