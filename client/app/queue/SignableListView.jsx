e7e0c979b8e839090da5208cee67f59d4e2860d3
Evan Kroske <evan@navahq.com>
2018-04-16 16:44:22 -0400

    Fixes typo

--- client/app/queue/SignableListView.jsx
+++ client/app/queue/SignableListView.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import _ from 'lodash';
 
 import StatusMessage from '../components/StatusMessage';
-import ReviewableTable from './ReviewableTable';
+import SignableTable from './SignableTable';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import Alert from '../components/Alert';
 
@@ -47,7 +47,7 @@ class SignableListView extends React.PureComponent {
     } else {
       tableContent = <div>
         <h1 {...fullWidth}>Review {reviewableCount} Cases</h1>
-        <ReviewableTable />
+        <SignableTable />
       </div>;
     }
 
