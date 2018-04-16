ebf60ea97d3acfd8fb7db3986e26795bc1807fc4
Chris Given <cmgiven@gmail.com>
2018-04-16 18:19:50 -0400

    Rollback erroneously closed RAMP appeals via the console (#5116)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402231703) do
+ActiveRecord::Schema.define(version: 20180411152656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -352,6 +352,17 @@ ActiveRecord::Schema.define(version: 20180402231703) do
     t.integer "ramp_election_id"
     t.date "nod_date"
     t.index ["ramp_election_id"], name: "index_ramp_closed_appeals_on_ramp_election_id"
+  end
+
+  create_table "ramp_election_rollbacks", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "ramp_election_id"
+    t.string "reason"
+    t.string "reopened_vacols_ids", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ramp_election_id"], name: "index_ramp_election_rollbacks_on_ramp_election_id"
+    t.index ["user_id"], name: "index_ramp_election_rollbacks_on_user_id"
   end
 
   create_table "ramp_elections", id: :serial, force: :cascade do |t|
---
+++ spec/models/ramp_election_rollback_spec.rb
@@ -0,0 +1,91 @@
+describe RampElectionRollback do
+  before do
+    Timecop.freeze(Time.utc(2019, 1, 1, 12, 0, 0))
+  end
+
+  let!(:ramp_election) do
+    RampElection.create!(
+      veteran_file_number: "44444444",
+      option_selected: "higher_level_review",
+      receipt_date: 5.days.ago,
+      end_product_reference_id: "EP1234"
+    )
+  end
+
+  let(:rollback) do
+    RampElectionRollback.new(
+      ramp_election: ramp_election,
+      user: user,
+      reason: reason
+    )
+  end
+
+  let(:user) { Generators::User.build }
+  let(:reason) { "A very good reason" }
+
+  let!(:established_end_product) do
+    Generators::EndProduct.build(
+      veteran_file_number: "44444444",
+      bgs_attrs: {
+        benefit_claim_id: "EP1234",
+        status_type_code: ep_status
+      }
+    )
+  end
+
+  let(:ep_status) { "CAN" }
+
+  context "#valid?" do
+    subject { rollback.valid? }
+
+    it { is_expected.to be true }
+
+    context "when no ramp election" do
+      let(:ramp_election) { nil }
+
+      it { is_expected.to be false }
+    end
+
+    context "when no user" do
+      let(:user) { nil }
+
+      it { is_expected.to be false }
+    end
+
+    context "when no reason" do
+      let(:reason) { nil }
+
+      it { is_expected.to be false }
+    end
+
+    context "when end product isn't canceled" do
+      let(:ep_status) { "PEND" }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  context "#create!" do
+    subject { rollback.save! }
+
+    let!(:appeals_to_reopen) do
+      %w[12345 23456].map do |vacols_id|
+        ramp_election.ramp_closed_appeals.create!(vacols_id: vacols_id)
+        Generators::Appeal.create(vacols_record: :ramp_closed, vacols_id: vacols_id)
+      end
+    end
+
+    it "reopens appeals and rolls back ramp election" do
+      expect(Appeal).to receive(:reopen).with(
+        appeals: appeals_to_reopen,
+        user: user,
+        disposition: "RAMP Opt-in"
+      )
+
+      subject
+
+      expect(ramp_election.reload.end_product_reference_id).to eq(nil)
+      expect(rollback.reload.reopened_vacols_ids).to eq(%w[12345 23456])
+    end
+  end
+end
