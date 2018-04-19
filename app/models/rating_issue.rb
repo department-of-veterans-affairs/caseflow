275a2bb72eca61be1b0744b7ac8f4a921fd05b47
Sean Bloomfield <seanbloomfield@gmail.com>
2018-04-19 14:05:13 -0700

    Adds serializers for Ratings and RatingIssues

--- app/models/rating_issue.rb
+++ app/models/rating_issue.rb
@@ -3,6 +3,13 @@ class RatingIssue
 
   attr_accessor :rba_issue_id, :decision_text
 
+  def ui_hash
+    {
+      rba_issue_id: rba_issue_id,
+      decision_text: decision_text
+    }
+  end
+
   def self.from_bgs_hash(data)
     new(
       rba_issue_id: data[:rba_issue_id],
--- app/models/supplemental_claim_intake.rb
+++ app/models/supplemental_claim_intake.rb
@@ -3,6 +3,16 @@ class SupplementalClaimIntake < Intake
     SupplementalClaim.new(veteran_file_number: veteran_file_number)
   end
 
+  def ui_hash
+    super.merge(
+      ratings: veteran.timely_ratings.map(&:ui_hash)
+    )
+  end
+
+  def veteran
+    @veteran ||= Veteran.new(file_number: veteran_file_number)
+  end
+
   def review!(request_params)
     detail.start_review!
     detail.update(request_params.permit(:receipt_date))
