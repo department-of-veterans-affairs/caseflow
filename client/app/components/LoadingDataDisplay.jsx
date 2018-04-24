703b8d28c1210c34397f010c52414b1a9f85d180
Danny Sellers <dsellers90@gmail.com>
2018-04-24 12:15:10 -0400

    cleanup

--- client/app/components/LoadingDataDisplay.jsx
+++ client/app/components/LoadingDataDisplay.jsx
@@ -71,9 +71,6 @@ class LoadingDataDisplay extends React.PureComponent {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.createLoadPromise !== nextProps.createLoadPromise) {
-      console.warn("createLoadPromise function changed")
-    }
     if (this.props.createLoadPromise.toString() !== nextProps.createLoadPromise.toString()) {
       throw new Error("Once LoadingDataDisplay is instantiated, you can't change the createLoadPromise function.");
     }
