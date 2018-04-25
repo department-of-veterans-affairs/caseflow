5fabd959f41797348e750eda9300612b5403637d
Evan Kroske <evan@navahq.com>
2018-04-25 11:23:58 -0400

    Assign assignment table

---
+++ client/app/queue/JudgeAssignTaskTable.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import moment from 'moment';
+
+import Table from '../components/Table';
+import CaseDetailsLink from './CaseDetailsLink';
+
+import { sortTasks, renderAppealType } from './utils';
+
+class JudgeAssignTaskTable extends React.PureComponent {
+  getKeyForRow = (rowNumber, object) => object.id;
+
+  getAppealForTask = (task, attr) => {
+    const appeal = this.props.appeals[task.vacolsId];
+
+    return attr ? _.get(appeal.attributes, attr) : appeal;
+  };
+
+  getCaseDetailsLink = (task) => <CaseDetailsLink task={task} appeal={this.getAppealForTask(task)} />;
+
+  getQueueColumns = () => [
+    {
+      header: 'Case Details',
+      valueFunction: this.getCaseDetailsLink
+    },
+    {
+      header: 'Type(s)',
+      valueFunction: (task) => renderAppealType(this.getAppealForTask(task))
+    },
+    {
+      header: 'Docket Number',
+      valueFunction: (task) => this.getAppealForTask(task, 'docket_number')
+    },
+    {
+      header: 'Issues',
+      valueFunction: (task) => this.getAppealForTask(task, 'issues.length')
+    },
+    {
+      header: 'Days Waiting',
+      valueFunction: (task) => (
+        moment().
+          startOf('day').
+          diff(moment(task.attributes.assigned_on), 'days'))
+    }
+  ];
+
+  render = () => {
+    return <Table
+      columns={this.getQueueColumns}
+      rowObjects={
+        sortTasks(
+          _.pick(this.props, 'tasks', 'appeals')
+        ).filter((task) => task.attributes.task_type === 'Assign')
+      }
+      getKeyForRow={this.getKeyForRow}
+    />;
+  }
+}
+
+JudgeAssignTaskTable.propTypes = {
+  tasks: PropTypes.object.isRequired,
+  appeals: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => _.pick(state.queue.loadedQueue, 'tasks', 'appeals');
+
+export default connect(mapStateToProps)(JudgeAssignTaskTable);
--- client/app/queue/JudgeReviewTaskListView.jsx
+++ client/app/queue/JudgeReviewTaskListView.jsx
@@ -6,6 +6,7 @@ import _ from 'lodash';
 
 import StatusMessage from '../components/StatusMessage';
 import JudgeReviewTaskTable from './JudgeReviewTaskTable';
+import JudgeAssignTaskTable from './JudgeAssignTaskTable';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
 import {
@@ -16,6 +17,22 @@ import {
 import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
 
 import { fullWidth } from './constants';
+
+const DISPLAYING_REVIEW_TASKS = {
+  title: (reviewableCount) => <h1 {...fullWidth}>Review {reviewableCount} Cases</h1>,
+  switchLink: (that) => <a onClick={() => that.handleSwitchToAssign()} href="#">Switch to Assign Cases</a>,
+  visibleTasks: (tasks) => _.filter(tasks, (task) => task.attributes.task_type === 'Review'),
+  noTasksMessage: () => 'Congratulations! You don\'t have any decisions to sign.',
+  table: () => <JudgeReviewTaskTable />
+};
+
+const DISPLAYING_ASSIGN_TASKS = {
+  title: (reviewableCount) => <h1 {...fullWidth}>Assign {reviewableCount} Cases</h1>,
+  switchLink: (that) => <a onClick={() => that.handleSwitchToReview()} href="#">Switch to Review Cases</a>,
+  visibleTasks: (tasks) => _.filter(tasks, (task) => task.attributes.task_type === 'Assign'),
+  noTasksMessage: () => 'Congratulations! You don\'t have any cases to assign.',
+  table: () => <JudgeAssignTaskTable />
+};
 
 class JudgeReviewTaskListView extends React.PureComponent {
   componentWillUnmount = () => {
@@ -28,20 +45,32 @@ class JudgeReviewTaskListView extends React.PureComponent {
     this.props.resetErrorMessages();
   };
 
+  constructor(props) {
+    super(props);
+    this.state = DISPLAYING_REVIEW_TASKS;
+  }
+
+  handleSwitchToAssign = () => this.setState(DISPLAYING_ASSIGN_TASKS)
+
+  handleSwitchToReview = () => this.setState(DISPLAYING_REVIEW_TASKS)
+
   render = () => {
-    const reviewableCount = _.filter(
-      this.props.tasks,
-      (task) => task.attributes.task_type === 'Review').length;
+    const reviewableCount = this.state.visibleTasks(this.props.tasks).length;
     let tableContent;
 
     if (reviewableCount === 0) {
-      tableContent = <StatusMessage title="Tasks not found">
-        Congratulations! You don't have any decisions to sign.
-      </StatusMessage>;
+      tableContent = <div>
+        {this.state.title(reviewableCount)}
+        {this.state.switchLink(this)}
+        <StatusMessage title="Tasks not found">
+          {this.state.noTasksMessage()}
+        </StatusMessage>
+      </div>;
     } else {
       tableContent = <div>
-        <h1 {...fullWidth}>Review {reviewableCount} Cases</h1>
-        <JudgeReviewTaskTable />
+        {this.state.title(reviewableCount)}
+        {this.state.switchLink(this)}
+        {this.state.table()}
       </div>;
     }
 
