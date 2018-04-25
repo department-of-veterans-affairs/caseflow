e9b3f6f986f69c815657e8f64f36976cca424ecb
lowellrex <lowell@navahq.com>
2018-04-25 15:49:21 -0400

    Change default type of ReaderLink docCount property to number

--- client/app/queue/ReaderLink.jsx
+++ client/app/queue/ReaderLink.jsx
@@ -64,7 +64,7 @@ export default class ReaderLink extends React.PureComponent {
 
 ReaderLink.propTypes = {
   analyticsSource: PropTypes.string,
-  docCount: PropTypes.string,
+  docCount: PropTypes.number,
   longMessage: PropTypes.bool,
   redirectUrl: PropTypes.string,
   taskType: PropTypes.string,
