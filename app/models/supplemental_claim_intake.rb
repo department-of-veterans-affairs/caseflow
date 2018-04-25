cf92ef86680e5cd82a409078b49f34ce74dc85ab
Sally Maki <sallymaki@gmail.com>
2018-04-25 16:44:53 -0400

    WIP getting started

--- app/models/supplemental_claim_intake.rb
+++ app/models/supplemental_claim_intake.rb
@@ -5,6 +5,8 @@ class SupplementalClaimIntake < Intake
 
   def ui_hash
     super.merge(
+      receipt_date: detail.receipt_date,
+      end_product_description: detail.end_product_description,
       ratings: veteran.cached_serialized_timely_ratings
     )
   end
@@ -16,5 +18,13 @@ class SupplementalClaimIntake < Intake
 
   def review_errors
     detail.errors.messages
+  end
+
+  def complete!(_request_params)
+    if detail.create_or_connect_end_product! == :connected
+      update!(error_code: "connected_preexisting_ep")
+    end
+
+    complete_with_status!(:success)
   end
 end
