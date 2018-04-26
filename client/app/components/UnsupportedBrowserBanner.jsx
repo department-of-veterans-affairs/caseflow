3c1936bb8cd5666ac96eb3077195598a608cb59e
Jones <vhaisajoness@VACOLTJONESS5.local>
2018-04-26 14:35:20 -0400

    more changes to banner

--- client/app/components/UnsupportedBrowserBanner.jsx
+++ client/app/components/UnsupportedBrowserBanner.jsx
@@ -8,15 +8,16 @@ export default class UnsupportedBrowserBanner extends React.PureComponent {
     const browser = detect();
     const title = `${browser.name === 'ie' ?
       'Internet Explorer' : StringUtil.snakeCaseToCapitalized(browser.name)} is not supported`;
+    const appName = 'Hearing Prep';
+    const message = <span> To access {appName}, you must use Chrome as your browser.<br />
+    If you need to install Chrome on your computer, please call the <br />
+    VA Enterprise Service Desk at
+      <a className="hearing-status-message" href="tel:855-673-4357"> 855-673-4357 </a>.</span>;
 
     return <div>
       <StatusMessage
-        title={browser.name !== 'chrome' ? title : ''}>
-       To access Hearing Prep, you must use Chrome as your browser.<br />
-       If you need to install Chrome on your computer, please call the <br />
-       VA Enterprise Service Desk at
-        <a className="hearing-status-message" href="tel:855-673-4357"> 855-673-4357 </a>.
-      </StatusMessage>
+        title={browser.name !== 'chrome' && title}
+        leadMessageList={[message]} />
     </div>;
   }
 }
