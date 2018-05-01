70cc53efde6b8ad370c409cfa88bd8d86ec38956
Sally Maki <sallymaki@gmail.com>
2018-05-01 16:45:59 -0700

    Higher level review

--- spec/feature/higher_level_review_intake_spec.rb
+++ spec/feature/higher_level_review_intake_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Supplemental Claim Intake" do
+RSpec.feature "Higher Level Review Intake" do
   before do
     FeatureToggle.enable!(:intake)
 
@@ -48,25 +48,19 @@ RSpec.feature "Supplemental Claim Intake" do
     )
   end
 
-  context "Supplemental Claim" do
+  context "Higher Level Review" do
     it "Creates an end product" do
       Fakes::VBMSService.end_product_claim_id = "IAMANEPID"
 
       visit "/intake"
       safe_click ".Select"
-      expect(page).to have_css(".cf-form-dropdown")
-      expect(page).to have_content("RAMP Selection (VA Form 21-4138)")
-      expect(page).to have_content("Request for Higher-Level Review (VA Form 20-0988)")
-      expect(page).to have_content("Supplemental Claim (VA Form 21-526b)")
-      expect(page).to have_content("Notice of Disagreement (VA Form 10182)")
 
-      safe_click ".Select"
-      fill_in "Which form are you processing?", with: "Supplemental Claim (VA Form 21-526b)"
+      fill_in "Which form are you processing?", with: "Request for Higher-Level Review (VA Form 20-0988)"
       find("#form-select").send_keys :enter
 
       safe_click ".cf-submit.usa-button"
 
-      expect(page).to have_content("process this Supplemental Claim (VA Form 21-526b).")
+      expect(page).to have_content("Higher-Level Review (VA Form 20-0988)")
 
       fill_in "Search small", with: "12341234"
 
@@ -79,8 +73,20 @@ RSpec.feature "Supplemental Claim Intake" do
       expect(page).to have_content(
         "Receipt date cannot be in the future."
       )
+      expect(page).to have_content(
+        "Please select an option."
+      )
 
       fill_in "What is the Receipt Date of this form?", with: "04/20/2018"
+
+      within_fieldset("Did the Veteran request an informal conference?") do
+        find("label", text: "Yes", match: :prefer_exact).click
+      end
+
+      within_fieldset("Did the Veteran request review by the same office?") do
+        find("label", text: "No", match: :prefer_exact).click
+      end
+
       safe_click "#button-submit-review"
 
       expect(page).to have_current_path("/intake/finish")
@@ -88,19 +94,18 @@ RSpec.feature "Supplemental Claim Intake" do
       expect(page).to have_content("Decision date: 04/25/2018")
       expect(page).to have_content("Left knee granted")
 
-      supplemental_claim = SupplementalClaim.find_by(veteran_file_number: "12341234")
+      higher_level_review = HigherLevelReview.find_by(veteran_file_number: "12341234")
+      expect(higher_level_review).to_not be_nil
+      expect(higher_level_review.receipt_date).to eq(Date.new(2018, 4, 20))
+      expect(higher_level_review.informal_conference).to eq(true)
+      expect(higher_level_review.same_office).to eq(false)
 
-      expect(supplemental_claim).to_not be_nil
-      expect(supplemental_claim.receipt_date).to eq(Date.new(2018, 4, 20))
       intake = Intake.find_by(veteran_file_number: "12341234")
 
       find("label", text: "PTSD denied").click
       safe_click "#button-finish-intake"
 
-      expect(page).to have_content("Request for Higher Level Review (VA Form 20-0988) has been processed.")
-      expect(page).to have_content(
-        "Established EP: 040SCRAMA - Supplemental Claim Review Rating for Station 397 - ARC"
-      )
+      expect(page).to_not have_content("Finish processing")
 
       expect(Fakes::VBMSService).to have_received(:establish_claim!).with(
         claim_hash: {
@@ -109,20 +114,14 @@ RSpec.feature "Supplemental Claim Intake" do
           predischarge: false,
           claim_type: "Claim",
           station_of_jurisdiction: "397",
-          date: supplemental_claim.receipt_date.to_date,
-          end_product_modifier: "040",
-          end_product_label: "Supplemental Claim Review Rating",
-          end_product_code: "040SCRAMA",
+          date: higher_level_review.receipt_date.to_date,
+          end_product_modifier: "030",
+          end_product_label: "Higher Level Review Rating",
+          end_product_code: "030HLRAMA",
           gulf_war_registry: false,
           suppress_acknowledgement_letter: false
         },
         veteran_hash: intake.veteran.to_vbms_hash
-      )
-
-      expect(Fakes::VBMSService).to have_received(:create_contentions!).with(
-        veteran_file_number: "12341234",
-        claim_id: "IAMANEPID",
-        contention_descriptions: ["PTSD denied"]
       )
 
       intake.reload
@@ -130,14 +129,8 @@ RSpec.feature "Supplemental Claim Intake" do
 
       expect(intake).to be_success
 
-      supplemental_claim.reload
-      expect(supplemental_claim.end_product_reference_id).to eq("IAMANEPID")
-      expect(supplemental_claim.request_issues.count).to eq 1
-      expect(supplemental_claim.request_issues.first).to have_attributes(
-        rating_issue_reference_id: "def456",
-        rating_issue_profile_date: Date.new(2018, 4, 28),
-        description: "PTSD denied"
-      )
+      higher_level_review.reload
+      expect(higher_level_review.end_product_reference_id).to eq("IAMANEPID")
     end
   end
 end
