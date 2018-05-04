5c1ccaf2df56598314dcc6440b82137f091c22ea
Danny Sellers <dannysellers@users.noreply.github.com>
2018-05-04 13:15:08 -0400

    Queue | Always scroll to top of page after navigating (#5347)
    Resolves #5293 
    
    ### Description
    Because Queue is a single-page app, the browser does not automatically reset scroll position when navigating between 'pages.' This scrolls to the top of the page every time the route changes (within `QueueApp`'s `BrowserRouter`)
    
    ### Acceptance Criteria 
    - [ ] Browser scrolls to top of page on route change
    
    ### Testing Plan
    1. Go to `/queue` as `BVAAABSHIRE`. Scroll to bottom of page, click to go to case details with many tasks (more than one page-length)
    2. Confirm page scrolls back to top of page on route change

--- client/app/queue/components/DecisionViewBase.jsx
+++ client/app/queue/components/DecisionViewBase.jsx
@@ -118,7 +118,7 @@ export default function decisionViewBase(ComponentToWrap) {
           this.props.popBreadcrumb();
         }
 
-        return this.goToStep(prevStepUrl);
+        return this.props.history.push(prevStepUrl);
       }
     };
 
@@ -139,14 +139,14 @@ export default function decisionViewBase(ComponentToWrap) {
       this.props.highlightInvalidFormItems(false);
 
       if (!nextStepHook) {
-        return this.goToStep(this.getNextStepUrl());
+        return this.props.history.push(this.getNextStepUrl());
       }
 
       const hookResult = nextStepHook();
 
       // nextStepHook may return a Promise, in which case do nothing here.
       if (hookResult === true) {
-        return this.goToStep(this.getNextStepUrl());
+        return this.props.history.push(this.getNextStepUrl());
       }
     };
 
@@ -155,7 +155,7 @@ export default function decisionViewBase(ComponentToWrap) {
 
       if (prevProps.savePending && !this.props.savePending) {
         if (this.props.saveSuccessful) {
-          this.goToStep(this.getNextStepUrl());
+          this.props.history.push(this.getNextStepUrl());
         } else {
           this.props.highlightInvalidFormItems(true);
         }
