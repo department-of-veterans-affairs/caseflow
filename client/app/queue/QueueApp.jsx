d6a41797453ead3f57050dbee6ab1112e81b9763
Danny Sellers <dsellers90@gmail.com>
2018-05-08 12:12:36 -0400

    lint

--- client/app/queue/QueueApp.jsx
+++ client/app/queue/QueueApp.jsx
@@ -6,7 +6,6 @@ import _ from 'lodash';
 import { css } from 'glamor';
 import StringUtil from '../util/StringUtil';
 
-import ScrollToTop from '../components/ScrollToTop';
 import PageRoute from '../components/PageRoute';
 import NavigationBar from '../components/NavigationBar';
 import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer';
