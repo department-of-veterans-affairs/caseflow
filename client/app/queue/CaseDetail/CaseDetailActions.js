b8baf098c205e39faa216ff0cf59f936940387bc
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-05-08 13:27:08 -0400

    Move appeal document count logic into dedicated component (#5442)
    Falls out of the work on PR #5413.
    
    Confirm that this works manually by viewing an attorney's queue and confirming that the numbers display properly, and by clicking into a task/case detail view and making sure the number of documents displays correctly.

--- client/app/queue/CaseDetail/CaseDetailActions.js
+++ client/app/queue/CaseDetail/CaseDetailActions.js
@@ -8,9 +8,3 @@ export const setActiveCase = (caseObj) => ({
   type: Constants.SET_ACTIVE_CASE,
   payload: { caseObj }
 });
-
-export const setDocumentCount = (docCount) => ({
-  type: Constants.SET_ACTIVE_CASE_DOCUMENT_COUNT,
-  payload: { docCount }
-});
-
