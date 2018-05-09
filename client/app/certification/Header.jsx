ce1463e0142adaa2b94b0f56161ef57c6f6ff687
Nolawi Petros <nolawi.petros@gmail.com>
2018-05-09 11:45:17 -0400

    cleanup certification header/ remove old jquery

--- client/app/certification/Header.jsx
+++ client/app/certification/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ClipboardIcon } from '../components/RenderFunctions';
-import { css } from 'glamor';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 export class Header extends React.Component {
   render() {
@@ -11,11 +11,6 @@ export class Header extends React.Component {
       serverError
     } = this.props;
 
-    const copyIcon = css({ display: 'inline-block',
-      marginLeft: '.25rem',
-      verticalAlign: '-1px'
-    });
-
     return <div>
       { !serverError && <div id="certifications-header" className="cf-app-segment">
         <div className="cf-txt-uc cf-veteran-name-control cf-push-left">
@@ -24,14 +19,14 @@ export class Header extends React.Component {
 
         <div className="cf-txt-uc cf-apppeal-id-control cf-push-right">
           Veteran ID &nbsp;
-
-          <button type="submit"
-            title="Copy to clipboard"
-            className="cf-apppeal-id"
-            data-clipboard-text={vbmsId}>
-            {vbmsId}
-            <ClipboardIcon className="cf-icon-appeal-id" {...copyIcon} />
-          </button>
+          <CopyToClipboard text={vbmsId}>
+            <button type="submit"
+              title="Copy Veteran ID"
+              className="cf-apppeal-id">
+              {vbmsId}
+              <ClipboardIcon className="cf-icon-appeal-id" />
+            </button>
+          </CopyToClipboard>
         </div>
       </div>}
       <div className="cf-help-divider"></div>
