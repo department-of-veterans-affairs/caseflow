ebbac1339c91d82ce751d4cb6058f2f4d45b307a
lowellrex <lowell@navahq.com>
2018-05-10 13:38:52 -0400

    Checkout JudgeReviewTaskListView.jsx from master to avoid merge conflict

--- client/app/queue/JudgeReviewTaskListView.jsx
+++ client/app/queue/JudgeReviewTaskListView.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import _ from 'lodash';
-import { sprintf } from 'sprintf-js';
 
 import StatusMessage from '../components/StatusMessage';
 import JudgeReviewTaskTable from './JudgeReviewTaskTable';
@@ -17,7 +16,15 @@ import {
 import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
 
 import { fullWidth } from './constants';
-import COPY from '../../../COPY.json';
+import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+
+const DISPLAYING_REVIEW_TASKS = {
+  title: (reviewableCount) => <h1 {...fullWidth}>Review {reviewableCount} Cases</h1>,
+  switchLink: (that) => <Link to={`/queue/${that.props.userId}/assign`}>Switch to Assign Cases</Link>,
+  visibleTasks: (tasks) => _.filter(tasks, (task) => task.attributes.task_type === 'Review'),
+  noTasksMessage: () => 'Congratulations! You don\'t have any decisions to sign.',
+  table: () => <JudgeReviewTaskTable />
+};
 
 class JudgeReviewTaskListView extends React.PureComponent {
   componentWillUnmount = () => {
@@ -30,20 +37,29 @@ class JudgeReviewTaskListView extends React.PureComponent {
     this.props.resetErrorMessages();
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = DISPLAYING_REVIEW_TASKS;
+  }
+
   render = () => {
-    const reviewableCount = _.filter(
-      this.props.tasks,
-      (task) => task.attributes.task_type === 'Review').length;
+    const reviewableCount = this.state.visibleTasks(this.props.tasks).length;
     let tableContent;
 
     if (reviewableCount === 0) {
-      tableContent = <StatusMessage title={COPY.NO_CASES_FOR_JUDGE_REVIEW_TITLE}>
-        {COPY.NO_CASES_FOR_JUDGE_REVIEW_MESSAGE}
-      </StatusMessage>;
+      tableContent = <div>
+        {this.state.title(reviewableCount)}
+        {this.state.switchLink(this)}
+        <StatusMessage title="Cases not found">
+          {this.state.noTasksMessage()}
+        </StatusMessage>
+      </div>;
     } else {
       tableContent = <div>
-        <h1 {...fullWidth}>{sprintf(COPY.JUDGE_CASE_REVIEW_TABLE_TITLE, reviewableCount)}</h1>
-        <JudgeReviewTaskTable />
+        {this.state.title(reviewableCount)}
+        {this.state.switchLink(this)}
+        {this.state.table()}
       </div>;
     }
 
