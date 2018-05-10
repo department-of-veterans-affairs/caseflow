527dba9199f1cc7fab7a52694a02dc5966bc9627
aroltsch <anya.roltsch@gmail.com>
2018-05-10 16:08:09 -0400

    Add test

--- spec/controllers/appeals_controller_spec.rb
+++ spec/controllers/appeals_controller_spec.rb
@@ -35,4 +35,29 @@ RSpec.describe AppealsController, type: :controller do
       end
     end
   end
+
+  describe "GET appeals/appeal_id/document_count" do
+    context "when appeal has documents" do
+      let(:documents) do
+        [
+          Document.new(type: "SSOC", received_at: 6.days.ago),
+          Document.new(type: "SSOC", received_at: 7.days.ago)
+        ]
+      end
+      let(:appeal) { Generators::Appeal.create(vacols_id: "654321", documents: documents) }
+
+      it "should return document count" do
+        get :document_count, params: { appeal_id: appeal.vacols_id }
+        response_body = JSON.parse(response.body)
+        expect(response_body["document_count"]).to eq 2
+      end
+    end
+
+    context "when appeal is not found" do
+      it "should return status 404" do
+        get :document_count, params: { appeal_id: "123456" }
+        expect(response.status).to eq 404
+      end
+    end
+  end
 end
