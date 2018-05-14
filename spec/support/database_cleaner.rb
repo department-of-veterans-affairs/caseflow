989058873962805841660cd01851bcee0ea6ca27
Mark Benjamin <mdbenjam@gmail.com>
2018-05-14 18:08:25 -0400

    Making DB cleaner more like it was

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -44,14 +44,14 @@ RSpec.configure do |config|
   config.before(:each) do
     # ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     # DatabaseCleaner.start
-    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+    # ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
     DatabaseCleaner.start
   end
 
   config.append_after(:each) do
     # ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     # DatabaseCleaner.clean
-    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+    # ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
     DatabaseCleaner.clean
     reset_application!
   end
