cb91ea63b4007fa46a2656ca4ee41e84fefe40a2
Sally Maki <sallymaki@gmail.com>
2018-05-14 13:25:09 -0700

    Appeal (NOD) Finish page (#5526)
    connects #5172

---
+++ client/app/intake/pages/appeal/finish.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Button from '../../../components/Button';
+import TabWindow from '../../../components/TabWindow';
+import CancelButton from '../../components/CancelButton';
+import NonRatedIssues from './nonRatedIssues';
+import RatedIssues from './ratedIssues';
+import { Redirect } from 'react-router-dom';
+import { REQUEST_STATE, PAGE_PATHS, INTAKE_STATES } from '../../constants';
+import { getIntakeStatus } from '../../selectors';
+
+class Finish extends React.PureComponent {
+  render() {
+    const {
+      appeal,
+      veteranName
+    } = this.props;
+
+    const tabs = [{
+      label: 'Rated issues',
+      page: <RatedIssues />
+    }, {
+      label: 'Non-rated issues',
+      page: <NonRatedIssues />
+    }];
+
+    switch (appeal) {
+    case INTAKE_STATES.NONE:
+      return <Redirect to={PAGE_PATHS.BEGIN} />;
+    case INTAKE_STATES.STARTED:
+      return <Redirect to={PAGE_PATHS.REVIEW} />;
+    case INTAKE_STATES.COMPLETED:
+      return <Redirect to={PAGE_PATHS.COMPLETED} />;
+    default:
+    }
+
+    return <div>
+      <h1>Finish processing { veteranName }'s Notice of Disagreement (VA Form 10182)</h1>
+
+      <p>
+        Select or enter all the issues that best match the Veteran's request.
+      </p>
+
+      <TabWindow
+        name="appeal-tabwindow"
+        tabs={tabs} />
+
+    </div>;
+  }
+}
+
+class FinishNextButton extends React.PureComponent {
+  handleClick = () => {
+    this.props.completeIntake(this.props.intakeId, this.props.appeal).then(
+      (completeWasSuccessful) => {
+        if (completeWasSuccessful) {
+          this.props.history.push('/completed');
+        }
+      }
+    );
+  }
+
+  render = () =>
+    <Button
+      name="finish-intake"
+      onClick={this.handleClick}
+      loading={this.props.requestState === REQUEST_STATE.IN_PROGRESS}
+      legacyStyling={false}
+    >
+      Establish appeal
+    </Button>;
+}
+
+const FinishNextButtonConnected = connect(
+  ({ appeal, intake }) => ({
+    requestState: appeal.requestStatus.completeIntake,
+    intakeId: intake.id,
+    appeal
+  })
+)(FinishNextButton);
+
+export class FinishButtons extends React.PureComponent {
+  render = () =>
+    <div>
+      <CancelButton />
+      <FinishNextButtonConnected history={this.props.history} />
+    </div>
+}
+
+export default connect(
+  (state) => ({
+    veteranName: state.intake.veteran.name,
+    appeal: getIntakeStatus(state)
+  })
+)(Finish);
