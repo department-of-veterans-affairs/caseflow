b43d3b3a912e8451bb974fe6ee1d02be578c8652
Danny Sellers <dannysellers@users.noreply.github.com>
2018-05-15 10:08:08 -0400

    Queue | Display info for multiple hearings (#5534)
    Resolves #4403
    
    ### Description
    Hearings section of Appeal Detail tab now displays information for multiple hearings, if applicable.
    
    ### Acceptance Criteria 
    - [ ] Verify that each hearing for an appeal has corresponding data for date held and name of judge at hearing
    
    ### Testing Plan
    (Enable `:queue_welcome_gate` and `:queue_case_search` feature toggles)
    1. Go to `/queue`, search for `760362568S` for an appeal with 1 hearing, `228081153S` for 2 hearings.
    2. Verify `Hearings (Oldest to Newest)` is displayed when > 1 hearing, hidden when 1 hearing.
    3. Verify that `type`, `disposition`, `date`, and `judge` are displayed for each hearing
    
    1 hearing
    ![screen shot 2018-05-14 at 4 17 51 pm](https://user-images.githubusercontent.com/8533004/40021282-5ef6d9ac-5792-11e8-8f2c-a2a033e435aa.png)
    
    
    2 hearings
    ![screen shot 2018-05-14 at 4 17 17 pm](https://user-images.githubusercontent.com/8533004/40021255-51d81e2a-5792-11e8-98ac-11ee91587175.png)

--- client/app/components/BareList.jsx
+++ client/app/components/BareList.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { css } from 'glamor';
 
 export default class BareList extends React.PureComponent {
+  bottomMargin = () => css({
+    marginBottom: this.props.compact ? '5px' : ''
+  });
+
   render() {
     const {
       ListElementComponent,
-      items
+      items,
+      listStyle
     } = this.props;
     const className = classNames('cf-bare-list', this.props.className);
 
-    return <ListElementComponent className={className}>
+    return <ListElementComponent {...listStyle} className={className}>
       {
         items.map((itemFn, index) =>
-          <li key={index}>{itemFn()}</li>
+          <li {...this.bottomMargin()} key={index}>{itemFn()}</li>
         )
       }
     </ListElementComponent>;
@@ -22,10 +28,14 @@ export default class BareList extends React.PureComponent {
 
 BareList.propTypes = {
   ListElementComponent: PropTypes.string,
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  compact: PropTypes.bool,
+  listStyle: PropTypes.object
 };
 
 BareList.defaultProps = {
   ListElementComponent: 'ol',
-  items: []
+  items: [],
+  compact: false,
+  listStyle: {}
 };
