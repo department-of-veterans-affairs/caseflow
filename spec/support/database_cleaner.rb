b229d59b87d20c7827d09d54528fb284dc2250dc
Mark Benjamin <mdbenjam@gmail.com>
2018-05-14 22:22:00 -0400

    Update database_cleaner.rb

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -42,14 +42,14 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    ActiveRecord::Base.establish_connection "#{Rails.env}".to_sym
+    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
     DatabaseCleaner.start
     ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     DatabaseCleaner.start
   end
 
   config.append_after(:each) do
-    ActiveRecord::Base.establish_connection "#{Rails.env}".to_sym
+    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
     DatabaseCleaner.clean
     ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     DatabaseCleaner.clean
