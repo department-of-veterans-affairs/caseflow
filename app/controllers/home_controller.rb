0a76b2bb4877c87df72f6e2044f6aa499c21239c
lowellrex <lowell@navahq.com>
2018-05-15 09:24:27 -0400

    Refactor HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,12 @@ class HomeController < ApplicationController
   skip_before_action :verify_authentication
 
   # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def index
-    if current_user && verify_authentication
-      if feature_enabled?(:case_search_home_page)
-        render("queue/index") && return
-      end
-
-      if user_can_access_queue?
-        redirect_to("/queue") && return
-      end
-    end
-
+    render("queue/index") && return if current_user && verify_authentication && feature_enabled?(:case_search_home_page)
+    redirect_to("/queue") && return if current_user && verify_authentication && user_can_access_queue?
     redirect_to("/help")
   end
+  # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Metrics/CyclomaticComplexity
 end
