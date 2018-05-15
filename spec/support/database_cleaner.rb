6ade45743dddf612e3a002bdecad984d924d959e
Mark Benjamin <mdbenjam@gmail.com>
2018-05-15 09:52:44 -0400

    Trying new things

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -17,14 +17,23 @@ RSpec.configure do |config|
       MSG
     end
     # ActiveRecord::Base.logger = Logger.new($stdout)
+    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+    DatabaseCleaner.clean_with(:truncation)
+    ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     DatabaseCleaner.clean_with(:truncation, except: %w[vftypes issref])
   end
 
   config.before(:each) do
+    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+    DatabaseCleaner.strategy = :transaction
+    ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, db_clean: :truncation) do
+    ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+    DatabaseCleaner.strategy = :truncation
+    ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
     DatabaseCleaner.strategy = :truncation, { except: %w[vftypes issref] }
   end
 
@@ -37,6 +46,9 @@ RSpec.configure do |config|
       # Driver is probably for an external browser with an app
       # under test that does *not* share a database connection with the
       # specs, so use truncation strategy.
+      ActiveRecord::Base.establish_connection Rails.env.to_s.to_sym
+      DatabaseCleaner.strategy = :truncation
+      ActiveRecord::Base.establish_connection "#{Rails.env}_vacols".to_sym
       DatabaseCleaner.strategy = :truncation, { except: %w[vftypes issref] }
     end
   end
