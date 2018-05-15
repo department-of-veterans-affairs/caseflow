ddf78e4665119ae1e14e94cf2515cc0cbde5451e
Teja N <teja.s.naraparaju@gmail.com>
2018-05-15 13:45:16 -0400

    auth for hearing prep

--- spec/controllers/hearings/worksheets_controller_spec.rb
+++ spec/controllers/hearings/worksheets_controller_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe Hearings::WorksheetsController, type: :controller do
+  let!(:user) { User.authenticate!(roles: ["Hearing Prep"]) }
+
   describe "GET worksheet" do
     it "should be fail" do
       get :show, params: { hearing_id: "12121" }, format: "json"
