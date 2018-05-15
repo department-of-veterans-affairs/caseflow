2707ffcc9a4bd1f39915987a1b54cd1a7a321cdb
Teja N <teja.s.naraparaju@gmail.com>
2018-05-15 13:30:01 -0400

    test for 404 error with json

---
+++ spec/controllers/hearings/worksheets_controller_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hearings::WorksheetsController, type: :controller do
+  describe "GET worksheet" do
+    it "should be fail" do
+      get :show, params: { hearing_id: "12121" }, format: "json"
+      expect(response.status).to eq 404
+      expect(response.body).to eq "{\"errors\":[{\"message\":\"Couldn't find Hearing with 'id'=12121\",\"code\":1000}]}"
+    end
+  end
+end
