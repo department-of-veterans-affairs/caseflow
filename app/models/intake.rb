79131209cc9eccaa6cac4729d442fc07933873ac
Sally Maki <sallymaki@gmail.com>
2018-05-16 11:19:13 -0700

    build change

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -39,7 +39,7 @@ class Intake < ApplicationRecord
 
     fail FormTypeNotSupported unless intake_classname
 
-    duplicate_intake_in_progress_by_current_user || intake_classname.constantize.new(
+    @duplicate_intake_in_progress_by_current_user ||= intake_classname.constantize.new(
       veteran_file_number: veteran_file_number,
       user: user
     )
