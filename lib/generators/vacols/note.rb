ea241a7ce8141b70ec32568366b8866fbc09cdc6
Mark Benjamin <mdbenjam@gmail.com>
2018-05-16 14:15:09 +0000

    Fakes and generators to get CircleCI build (#5073)
    ### Description
    This PR makes a number of changes to get Reader feature tests working with FACOLS.
    
    1) Created new generators for VACOLS models.
       - All these generators have sensible defaults to make building out the data easier.
    1) Created a new LegacyAppealV2 generator to make building out FACOLS data simpler.
    1) Added code to the database_cleaner to work with multiple databases, Postgres and FACOLS.
    1) Added code to rails_helper to load in the entirety of the VFTYPES and ISSREF tables before running tests.
       - This data is needed for all issue related operations.
    1) Converted all tests in reader_spec to use FACOLS.
    1) Added a new feature toggle called `:fakes_off` which makes it easy to turn off fakes for a specific test.
    
    ### Acceptance Criteria 
    - [ ] All Reader tests use FACOLS
    - [ ] Code passes tests on CircleCI

---
+++ lib/generators/vacols/note.rb
@@ -0,0 +1,43 @@
+class Generators::Vacols::Note
+  class << self
+    # rubocop:disable Metrics/MethodLength
+    def note_attrs
+      {
+        tasknum: "877483D4",
+        tsktknm: "877483",
+        tskstfas: "JRAYNOR",
+        tskactcd: "B",
+        tskclass: "ACTIVE",
+        tskrqact: "Illum voluptatem consectetur molestiae maiores commodi est est optio.",
+        tskrspn: "Sapiente enim dolores id qui eveniet sequi.",
+        tskdassn: "2017-07-11 00:00:00 UTC",
+        tskdtc: 1,
+        tskddue: "2017-07-12 00:00:00 UTC",
+        tskdcls: "2017-07-11 00:00:00 UTC",
+        tskstown: "JRAYNOR",
+        tskstat: "C",
+        tskownts: nil,
+        tskclstm: nil,
+        tskadusr: "JRAYNOR",
+        tskadtm: "2017-07-11 00:00:00 UTC",
+        tskmdusr: nil,
+        tskmdtm: nil,
+        tsactive: nil,
+        tsspare1: nil,
+        tsspare2: nil,
+        tsspare3: nil,
+        tsread1: nil,
+        tsread: nil,
+        tskorder: nil,
+        tssys: nil
+      }
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    def create(attrs = {})
+      attrs = note_attrs.merge(attrs)
+
+      VACOLS::Note.create(attrs)
+    end
+  end
+end
