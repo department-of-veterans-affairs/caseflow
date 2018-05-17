bb891e791417db7eac61fbfe051d0cfa7728f074
Sally Maki <sallymaki@gmail.com>
2018-05-17 11:58:43 -0700

    Delete appeals class and table (#5602)
    * Delete appeals
    
    * Remove copy to Appeal from LegacyAppeal
    
    * Remove ref to Appeals in AppealSeries
    
    * Remove schema change from other PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510171815) do
+ActiveRecord::Schema.define(version: 20180517174411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,41 +54,6 @@ ActiveRecord::Schema.define(version: 20180510171815) do
     t.datetime "updated_at", null: false
     t.datetime "last_viewed_at"
     t.index ["appeal_id", "user_id"], name: "index_appeal_views_on_appeal_id_and_user_id", unique: true
-  end
-
-  create_table "appeals", id: :serial, force: :cascade do |t|
-    t.string "vacols_id", null: false
-    t.string "vbms_id"
-    t.boolean "rice_compliance", default: false
-    t.boolean "private_attorney_or_agent", default: false
-    t.boolean "waiver_of_overpayment", default: false
-    t.boolean "pension_united_states", default: false
-    t.boolean "vamc", default: false
-    t.boolean "incarcerated_veterans", default: false
-    t.boolean "dic_death_or_accrued_benefits_united_states", default: false
-    t.boolean "vocational_rehab", default: false
-    t.boolean "foreign_claim_compensation_claims_dual_claims_appeals", default: false
-    t.boolean "manlincon_compliance", default: false
-    t.boolean "hearing_including_travel_board_video_conference", default: false
-    t.boolean "home_loan_guaranty", default: false
-    t.boolean "insurance", default: false
-    t.boolean "national_cemetery_administration", default: false
-    t.boolean "spina_bifida", default: false
-    t.boolean "radiation", default: false
-    t.boolean "nonrating_issue", default: false
-    t.boolean "us_territory_claim_philippines", default: false
-    t.boolean "contaminated_water_at_camp_lejeune", default: false
-    t.boolean "mustard_gas", default: false
-    t.boolean "education_gi_bill_dependents_educational_assistance_scholars", default: false
-    t.boolean "foreign_pension_dic_all_other_foreign_countries", default: false
-    t.boolean "foreign_pension_dic_mexico_central_and_south_america_caribb", default: false
-    t.boolean "us_territory_claim_american_samoa_guam_northern_mariana_isla", default: false
-    t.boolean "us_territory_claim_puerto_rico_and_virgin_islands", default: false
-    t.string "dispatched_to_station"
-    t.integer "appeal_series_id"
-    t.boolean "issues_pulled"
-    t.index ["appeal_series_id"], name: "index_appeals_on_appeal_series_id"
-    t.index ["vacols_id"], name: "index_appeals_on_vacols_id", unique: true
   end
 
   create_table "attorney_case_reviews", id: :serial, force: :cascade do |t|
@@ -567,7 +532,6 @@ ActiveRecord::Schema.define(version: 20180510171815) do
 
   add_foreign_key "annotations", "users"
   add_foreign_key "api_views", "api_keys"
-  add_foreign_key "appeals", "appeal_series"
   add_foreign_key "certifications", "users"
   add_foreign_key "legacy_appeals", "appeal_series"
   add_foreign_key "ramp_closed_appeals", "ramp_elections"
