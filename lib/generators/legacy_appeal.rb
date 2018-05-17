5ab3ded55625d082f7adebb9df370523d2ee995f
lowellrex <lowell@navahq.com>
2018-05-17 13:52:13 -0400

    Fix broken generator

--- lib/generators/legacy_appeal.rb
+++ lib/generators/legacy_appeal.rb
@@ -19,7 +19,8 @@ class Generators::LegacyAppeal
       {
         type: "Original",
         file_type: "VBMS",
-        representative: "Military Order of the Purple Heart",
+        representative_name: "Military Order of the Purple Heart",
+        representative_type: "Service Organization",
         veteran_first_name: generate_first_name,
         veteran_middle_initial: "A",
         veteran_last_name: last_name,
