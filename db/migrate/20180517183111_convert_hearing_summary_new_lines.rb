39ca18e51735ff3d048dfc36440b83f27f195fde
Teja N <teja.s.naraparaju@gmail.com>
2018-05-17 16:15:06 -0400

    fixing typo

--- db/migrate/20180517183111_convert_hearing_summary_new_lines.rb
+++ db/migrate/20180517183111_convert_hearing_summary_new_lines.rb
@@ -1,7 +1,7 @@
 class ConvertHearingSummaryNewLines < ActiveRecord::Migration[5.1]
   def change
     Hearing.where("length(summary) > 0").find_each do |hearing|
-      hearing.summary = summary.gsub(/\\n/, "</br>")
+      hearing.summary = hearing.summary.gsub(/\\n/, "</br>")
       hearing.save!
     end
   end
