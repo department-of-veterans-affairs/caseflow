12660526207e2c83c0dac52ea5745765b9d83fc5
Sally Maki <sallymaki@gmail.com>
2018-05-21 11:56:01 -0700

    Add index to prevent multiple intakes per user

--- db/migrate/20180516213251_add_conditional_index_to_intakes.rb
+++ db/migrate/20180516213251_add_conditional_index_to_intakes.rb
@@ -3,5 +3,6 @@ class AddConditionalIndexToIntakes < ActiveRecord::Migration[5.1]
 
   def change
     add_index :intakes, [:type, :veteran_file_number], algorithm: :concurrently, name: 'unique_index_to_avoid_duplicate_intakes', where: "completion_status is NULL", unique: true
+    add_index :intakes, :user_id, algorithm: :concurrently, name: 'unique_index_to_avoid_multiple_intakes', where: "completion_status is NULL", unique: true
   end
 end
