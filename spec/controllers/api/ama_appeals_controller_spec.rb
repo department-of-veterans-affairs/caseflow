87f6c7c4fa395c2a253b8bf0fdbab2ccb74cb772
Mark Benjamin <mdbenjam@gmail.com>
2018-05-22 17:56:02 -0400

    More testing

--- spec/controllers/api/ama_appeals_controller_spec.rb
+++ spec/controllers/api/ama_appeals_controller_spec.rb
@@ -10,12 +10,13 @@ RSpec.describe AmaAppealsController, type: :controller do
         date_of_birth: "05/04/1955"
       }
     end
-    let!(:appeal) { create(:appeal, veteran_object: create(:veteran, bgs_veteran_record: bgs_veteran_record)) }
+    let(:appeal) { create(:appeal, veteran_object: create(:veteran, bgs_veteran_record: bgs_veteran_record)) }
 
     context "when request header does not contain Veteran ID" do
       it "response should error" do
-        get :index
         binding.pry
+        get :index
+        
         expect(response.status).to eq 400
       end
     end
--- spec/factories/appeal.rb
+++ spec/factories/appeal.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
   factory :appeal do
     transient do
-      veteran_object "Test"
+      veteran_object nil
     end
 
-    veteran_file_number do
-      if veteran_object
-        veteran_object.file_number
-      else
-        "783740847"
-      end
-    end
+    # veteran_file_number { "TEST" }
+    # do
+    #    "783740847"
+    #   # if veteran_object
+    #   #   veteran_object.file_number
+    #   # else
+    #   #   "783740847"
+    #   # end
+    # end
   end
 end
