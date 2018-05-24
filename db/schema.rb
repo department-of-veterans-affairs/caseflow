7cc059aec7f436ba7244b09d3d6d18ad96c518f8
Kara Louie <kara@truss.works>
2018-05-24 10:59:01 -0700

    Add uuid column to appeals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524173201) do
+ActiveRecord::Schema.define(version: 20180524174054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
