59ff25387172d1cc9e3c3f535e2d3edfe4fffe83
Danny Sellers <dsellers90@gmail.com>
2018-05-29 12:35:50 -0400

    enable reader link for paper cases

--- client/app/queue/AttorneyTaskTable.jsx
+++ client/app/queue/AttorneyTaskTable.jsx
@@ -64,10 +64,6 @@ class AttorneyTaskTable extends React.PureComponent {
           return null;
         }
 
-        if (this.getAppealForTask(task, 'paper_case')) {
-          return <span {...disabledLinkStyle}>{COPY.ATTORNEY_QUEUE_TABLE_TASK_NO_DOCUMENTS_READER_LINK}</span>;
-        }
-
         return <ReaderLink vacolsId={task.vacolsId}
           analyticsSource={CATEGORIES.QUEUE_TABLE}
           redirectUrl={window.location.pathname}
