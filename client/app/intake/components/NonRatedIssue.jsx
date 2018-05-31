5a49ab5e4dec9c1d3f5c2417adc095f0c8a43a2b
Sally Maki <sallymaki@gmail.com>
2018-05-31 14:14:57 -0700

    non-rated issue styling

--- client/app/intake/components/NonRatedIssue.jsx
+++ client/app/intake/components/NonRatedIssue.jsx
@@ -4,60 +4,77 @@ import TextField from '../../components/TextField';
 import Button from '../../components/Button';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import ISSUE_CATEGORIES from '../../constants.js'
+import { ISSUE_CATEGORIES} from '../constants'
 
-class NonRatedIssue extends React.PureComponent {
-  setIssueCategoryFromDropdown = (issueCategory) => {
-    this.props.setIssueCategory(issueCategory.value);
-  }
+export default class NonRatedIssue extends React.PureComponent {
+  render () {
+    return (
+      <div className="cf-non-rated-issue">
+        <SearchableDropdown
+          name="issue-category"
+          label="Issue category"
+          placeholder="Select or enter..."
+          options={ISSUE_CATEGORIES} />
 
-  render() {
-    return <div>
-      <SearchableDropdown
-        name="issue-category"
-        label="Issue category"
-        placeholder="Select or enter..."
-        options={ISSUE_CATEGORIES}
-        onChange={this.setIssueCategoryFromDropdown}
-        value={this.props.issueCategory} />
-
-      <TextField
-        name="Issue description"
-        onChange={this.onEmailChange}
-        errorMessage={this.state.certificationCancellationForm.email.errorMessage}
-        value={this.state.emailValue} />
-    </div>;
+        <TextField
+          name="Issue description" />
+      </div>
+    )
   }
 }
 
-export default connect(
-  ({ intake }) => ({
-    formType: intake.formType,
-    intakeId: intake.id
-  }),
-  (dispatch) => bindActionCreators({
-    setIssueCategory
-  }, dispatch)
-)(NonRatedIssue);
-
-class SaveIssueButtonUnconnected extends React.PureComponent {
-  handleClick = () => {
-    this.props.saveIssue();
-  }
-
-  render = () =>
-    <Button
-      name="save-issue"
-      onClick={this.handleClick}
-      legacyStyling={false}
-    >
-      Save
-    </Button>;
-}
-
-export const SaveFormButton = connect(
-  ({ intake }) => ({ formType: intake.formType }),
-  (dispatch) => bindActionCreators({
-    clearSearchErrors
-  }, dispatch)
-)(SelectFormButtonUnconnected);
+// class NonRatedIssue extends React.PureComponent {
+//   setIssueCategoryFromDropdown = (issueCategory) => {
+//     this.props.setIssueCategory(issueCategory.value);
+//   }
+//
+//   render() {
+//     return <div>
+//       <SearchableDropdown
+//         name="issue-category"
+//         label="Issue category"
+//         placeholder="Select or enter..."
+//         options={ISSUE_CATEGORIES}
+//         onChange={this.setIssueCategoryFromDropdown}
+//         value={this.props.issueCategory} />
+//
+//       <TextField
+//         name="Issue description"
+//         onChange={this.onEmailChange}
+//         errorMessage={this.state.certificationCancellationForm.email.errorMessage}
+//         value={this.state.emailValue} />
+//     </div>;
+//   }
+// }
+//
+// export default connect(
+//   ({ intake }) => ({
+//     formType: intake.formType,
+//     intakeId: intake.id
+//   }),
+//   (dispatch) => bindActionCreators({
+//     setIssueCategory
+//   }, dispatch)
+// )(NonRatedIssue);
+//
+// class SaveIssueButtonUnconnected extends React.PureComponent {
+//   handleClick = () => {
+//     this.props.saveIssue();
+//   }
+//
+//   render = () =>
+//     <Button
+//       name="save-issue"
+//       onClick={this.handleClick}
+//       legacyStyling={false}
+//     >
+//       Save
+//     </Button>;
+// }
+//
+// export const SaveFormButton = connect(
+//   ({ intake }) => ({ formType: intake.formType }),
+//   (dispatch) => bindActionCreators({
+//     clearSearchErrors
+//   }, dispatch)
+// )(SelectFormButtonUnconnected);
--- client/app/intake/pages/appeal/nonRatedIssues.jsx
+++ client/app/intake/pages/appeal/nonRatedIssues.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../../../components/Button';
+import NonRatedIssue from '../../components/NonRatedIssue'
 import ISSUE_CATEGORIES from '../../constants.js'
 
 export default class NonRatedIssues extends React.PureComponent {
@@ -9,6 +10,9 @@ export default class NonRatedIssues extends React.PureComponent {
       If the Veteran included any additional issues you cannot find in the list above,
       please note them below. Otherwise, leave the section blank.
     </p>
+    <div>
+      <NonRatedIssue />
+    </div>
     <Button
       name="add-issue"
       legacyStyling={false}
