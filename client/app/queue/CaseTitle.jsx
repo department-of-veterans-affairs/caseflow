782a0bec3a08db01f4e536b38a285d4735689816
lowellrex <lowell@navahq.com>
2018-06-01 16:45:00 -0400

    Simplify css styles

--- client/app/queue/CaseTitle.jsx
+++ client/app/queue/CaseTitle.jsx
@@ -1,26 +1,26 @@
-import { after, css, merge } from 'glamor';
+import { css } from 'glamor';
 import React from 'react';
 
 import { COLORS } from '../constants/AppConstants';
 
 const headerStyling = css({
   float: 'left',
+  listStyleType: 'none',
   margin: 0,
   marginTop: '-2rem'
 });
+
 const listStyling = css({
-  marginTop: '-1.5rem',
-  marginLeft: '1rem',
-  '& li': {
-    float: 'left',
-    paddingTop: '0.5rem',
-    paddingBottom: '0.5rem'
-  },
-  // Replaces pipe character with right border
-  '& li:not(:last-child)': merge(
-    after({ content: '""' }),
-    { borderRight: `1px solid ${COLORS.GREY_LIGHT}` }
-  )
+  float: 'left',
+  listStyleType: 'none',
+  margin: '-1.5rem 0 0 1rem',
+  padding: 0
+});
+
+const listItemStyling = css({
+  float: 'left',
+  padding: '0.5rem 1.5rem',
+  ':not(:last-child)': { borderRight: `1px solid ${COLORS.GREY_LIGHT}` }
 });
 
 const horizontalRuleStyling = css({
@@ -28,15 +28,14 @@ const horizontalRuleStyling = css({
   borderTop: `1px solid ${COLORS.GREY_LIGHT}`,
   clear: 'both',
   // Offsets the padding from .cf-app-segment--alt to make the hr full width.
-  margin: '0 -4rem',
-  marginTop: '4rem'
+  margin: '4rem -4rem 0 -4rem'
 });
 
 export default class CaseTitle extends React.PureComponent {
   render = () => <React.Fragment>
     <h1 {...headerStyling}>{this.props.heading}</h1>
-    <ul className="usa-unstyled-list usa-nav-secondary-links" {...listStyling}>
-      {this.props.children.map((child, i) => <li key={i}>{child}</li>)}
+    <ul {...listStyling}>
+      {this.props.children.map((child, i) => <li key={i} {...listItemStyling}>{child}</li>)}
     </ul>
     <hr {...horizontalRuleStyling} />
   </React.Fragment>;
