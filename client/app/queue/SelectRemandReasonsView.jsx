f18212cc3e11310b79c263ed3968a2d4bbf944be
Danny Sellers <dsellers90@gmail.com>
2018-06-04 09:52:27 -0400

    update copy

--- client/app/queue/SelectRemandReasonsView.jsx
+++ client/app/queue/SelectRemandReasonsView.jsx
@@ -78,7 +78,7 @@ class SelectRemandReasonsView extends React.Component {
       {this.getPageName()}
     </h1>
     <p className="cf-lead-paragraph" {...subHeadStyling}>
-      Please select the appropriate remand reason(s) for all the remand dispositions.
+      Please {this.props.userRoles === USER_ROLES.ATTORNEY ? 'select' : 'review'} the appropriate remand reason(s) for all the remand dispositions.
     </p>
     <hr />
     {_.map(_.range(this.state.issuesRendered), (idx) =>
