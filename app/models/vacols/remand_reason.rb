56ffc2c2a2bb6e213e8e6bb3604864af62feafba
Danny Sellers <dsellers90@gmail.com>
2018-06-04 16:38:21 -0400

    add RemandReasonError; prevent calls to update/delete/destroy. update_remand_reasons not working atm

--- app/models/vacols/remand_reason.rb
+++ app/models/vacols/remand_reason.rb
@@ -2,6 +2,8 @@ class VACOLS::RemandReason < VACOLS::Record
   # :nocov:
   self.table_name = "vacols.rmdrea"
 
+  class RemandReasonError < StandardError; end
+
   CODES = Constants::ACTIVE_REMAND_REASONS_BY_ID.values.flat_map(&:keys).concat(
     Constants::INACTIVE_REMAND_REASONS_BY_ID.values.flat_map(&:keys)
   ).freeze
@@ -14,7 +16,7 @@ class VACOLS::RemandReason < VACOLS::Record
   end
 
   def self.load_remand_reasons(rmdkey, rmdissseq)
-    VACOLS::RemandReason.where(rmdkey: rmdkey, rmdissseq: rmdissseq)
+    where(rmdkey: rmdkey, rmdissseq: rmdissseq)
   end
 
   def self.delete_remand_reasons!(rmdkey, rmdissseq)
@@ -22,10 +24,40 @@ class VACOLS::RemandReason < VACOLS::Record
   end
 
   def self.update_remand_reasons!(rmdkey, rmdissseq, remand_reasons)
-    load_remand_reasons(rmdkey, rmdissseq).map.with_index do |_reason, idx|
-      updated_reason = remand_reasons[idx]
-      update(updated_reason)
+    load_remand_reasons(rmdkey, rmdissseq).map.with_index do |reason, idx|
+      updated_reason = remand_reasons[idx].merge(rmdkey: rmdkey, rmdissseq: rmdissseq)
+      reason.update_attributes!(updated_reason)
     end
+  end
+
+  def update(*)
+    update_error_message
+  end
+
+  def update!(*)
+    update_error_message
+  end
+
+  def delete
+    delete_error_message
+  end
+
+  def destroy
+    delete_error_message
+  end
+
+  private
+
+  def update_error_message
+    fail RemandReasonError, "Since the primary key is not unique, `update` will update all results
+      with the same `rmdkey`. Instead use VACOLS::RemandReason.update_remand_reasons! that uses `rmdkey`
+      and `rmdissseq` to safely update one record"
+  end
+
+  def delete_error_message
+    fail RemandReasonError, "Since the primary key is not unique, `delete` or `destroy` will delete
+      all results with the same `rmdkey`. Instead use VACOLS::RemandReason.delete_remand_reasons! that uses
+      `rmdkey` and `rmdissseq` to safely delete one record"
   end
   # :nocov:
 end
