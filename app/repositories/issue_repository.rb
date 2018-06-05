9be38dbc5ef2bfe1701b16b9b4ec60860a1980b1
Danny Sellers <dsellers90@gmail.com>
2018-06-05 15:42:31 -0400

    create new remand reasons on update without changing disposition

--- app/repositories/issue_repository.rb
+++ app/repositories/issue_repository.rb
@@ -66,11 +66,15 @@ class IssueRepository
   def self.update_remand_reasons!(vacols_id, vacols_sequence_id, remand_reasons)
     existing_remand_reasons = VACOLS::RemandReason.load_remand_reasons(vacols_id, vacols_sequence_id)
       .pluck(:rmdval)
+
     reasons_to_delete = existing_remand_reasons - remand_reasons.pluck(:rmdval)
+    reasons_to_create = remand_reasons.reject { |r| existing_remand_reasons.include? r[:rmdval] }
+    reasons_to_update = remand_reasons.reject { |r| reasons_to_create.pluck(:rmdval).include? r[:rmdval] }
 
-    delete_remand_reasons!(vacols_id, vacols_sequence_id, rmdval: reasons_to_delete)
+    delete_remand_reasons!(vacols_id, vacols_sequence_id, rmdval: reasons_to_delete) unless reasons_to_delete.empty?
+    create_remand_reasons!(vacols_id, vacols_sequence_id, reasons_to_create) unless reasons_to_create.empty?
 
-    VACOLS::RemandReason.update_remand_reasons!(vacols_id, vacols_sequence_id, remand_reasons)
+    VACOLS::RemandReason.update_remand_reasons!(vacols_id, vacols_sequence_id, reasons_to_update)
   end
 
   def self.delete_remand_reasons!(vacols_id, vacols_sequence_id, **kwargs)
