e3e01489ac82943ea3d406468539c373b3051404
Shane Russell <shane1337@gmail.com>
2018-06-06 08:00:09 -0700

    Tracer for reclosure of RAMP closed appeals job (#5804)
    connects #5786
    
    The plan here is to merge the code that just queries for appeals that we'd mark for reclosure and report to slack what they are. When we verify that that number is correct, we can go ahead and add the code to actually close them.

--- spec/jobs/sync_intake_job_spec.rb
+++ spec/jobs/sync_intake_job_spec.rb
@@ -1,6 +1,10 @@
 describe SyncIntakeJob do
   context ".perform" do
     it "calls recreate_issues_from_contentions and sync_ep_status" do
+      slack_service = double("SlackService")
+      expect(slack_service).to receive(:send_notification)
+      allow_any_instance_of(SyncIntakeJob).to receive(:slack_service).and_return(slack_service)
+
       expect(RampElection).to receive(:sync_all!)
 
       SyncIntakeJob.perform_now
