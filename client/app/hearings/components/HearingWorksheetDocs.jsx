1d9adc554f616572b8ed1958b4bb01509b6d8f1e
Jones <vhaisajoness@VACOLTJONESS5.local>
2018-06-06 18:26:39 -0400

    Reduce spacing

--- client/app/hearings/components/HearingWorksheetDocs.jsx
+++ client/app/hearings/components/HearingWorksheetDocs.jsx
@@ -39,7 +39,7 @@ class HearingWorksheetDocs extends Component {
           title={accordionTitle}
         >
           <div>
-            <div {...styling}>{worksheet.cached_number_of_documents} Documents in Claims Folder</div>
+            <div {...styling} className="worksheetDocs">{worksheet.cached_number_of_documents} Documents in Claims Folder</div>
 
             {Object.values(worksheetAppeals).map((appeal, key) => {
 
