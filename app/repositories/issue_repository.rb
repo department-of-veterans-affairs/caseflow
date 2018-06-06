7304eb13e8f1fcf309656a8dca1f8685bf2f2b80
Danny Sellers <dsellers90@gmail.com>
2018-06-06 11:23:55 -0400

    track changes to dispositions and remand reasons

--- app/repositories/issue_repository.rb
+++ app/repositories/issue_repository.rb
@@ -61,6 +61,7 @@ class IssueRepository
   end
 
   def self.create_remand_reasons!(vacols_id, vacols_sequence_id, remand_reasons)
+    BusinessMetrics.record(service: :queue, name: "create_remand_reasons")
     VACOLS::RemandReason.create_remand_reasons!(vacols_id, vacols_sequence_id, remand_reasons)
   end
 
@@ -75,10 +76,14 @@ class IssueRepository
     delete_remand_reasons!(vacols_id, vacols_sequence_id, rmdval: reasons_to_delete) unless reasons_to_delete.empty?
     create_remand_reasons!(vacols_id, vacols_sequence_id, reasons_to_create) unless reasons_to_create.empty?
 
-    VACOLS::RemandReason.update_remand_reasons!(vacols_id, vacols_sequence_id, reasons_to_update)
+    unless reasons_to_update.empty?
+      BusinessMetrics.record(service: :queue, name: "update_remand_reasons")
+      VACOLS::RemandReason.update_remand_reasons!(vacols_id, vacols_sequence_id, reasons_to_update)
+    end
   end
 
   def self.delete_remand_reasons!(vacols_id, vacols_sequence_id, **kwargs)
+    BusinessMetrics.record(service: :queue, name: "del_remand_reasons")
     VACOLS::RemandReason.delete_remand_reasons!(vacols_id, vacols_sequence_id, **kwargs)
   end
 
@@ -96,6 +101,8 @@ class IssueRepository
     case Constants::VACOLS_DISPOSITIONS_BY_ID[issue_attrs[:disposition]]
     when "Vacated"
       if record.issdc != "5" && issue_attrs[:readjudication]
+        BusinessMetrics.record(service: :queue, name: "vacated_disposition_issue_edit")
+
         create_vacols_issue!(issue_attrs: record.attributes_for_readjudication.merge(
           vacols_user_id: issue_attrs[:vacols_user_id]
         ))
@@ -115,6 +122,7 @@ class IssueRepository
       delete_remand_reasons!(*args)
       return
     end
+    BusinessMetrics.record(service: :queue, name: "remanded_disposition_issue_edit")
 
     remand_reasons = RemandReasonMapper.convert_to_vacols_format(
       issue_attrs[:vacols_user_id],
