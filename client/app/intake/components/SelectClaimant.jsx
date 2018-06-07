03f078ddca002e0fb6ee503d9a4fcf78ff1b4bdd
Sally Maki <sallymaki@gmail.com>
2018-06-07 15:07:09 -0700

    Ability to choose claimant other than the Veteran - Front end (#5799)
    Connects #5569
    
    Gets Veteran's relationships from BGS, and renders them on the "Review page" in case the claimant is other than the veteran.  This ticket is for Higher Level Review only.

---
+++ client/app/intake/components/SelectClaimant.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import RadioField from '../../components/RadioField';
+import { BOOLEAN_RADIO_OPTIONS } from '../constants';
+
+export default class SelectClaimant extends React.PureComponent {
+  render = () => {
+    const {
+      claimantNotVeteran,
+      setClaimantNotVeteran,
+      claimant,
+      setClaimant,
+      relationships
+    } = this.props;
+
+    let showClaimants = claimantNotVeteran === 'true';
+
+    const claimantLabel = 'Please select the claimant listed on the form. ' +
+    'If you do not see the claimant, you will need to add it through SHARE, ' +
+    'then refresh this page.';
+
+    const claimantOptions = () => {
+      return <div className="cf-claimant-options">
+        <RadioField
+          name="claimant-options"
+          label={claimantLabel}
+          strongLabel
+          vertical
+          options={relationships}
+          onChange={setClaimant}
+          value={claimant}
+        />
+      </div>;
+    };
+
+    return <div className="cf-different-claimant">
+      <RadioField
+        name="different-claimant-option"
+        label="Is the claimant someone other than the Veteran?"
+        strongLabel
+        vertical
+        options={BOOLEAN_RADIO_OPTIONS}
+        onChange={setClaimantNotVeteran}
+        value={claimantNotVeteran}
+      />
+
+      { showClaimants && claimantOptions() }
+    </div>;
+  }
+}
