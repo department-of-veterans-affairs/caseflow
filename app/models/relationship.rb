b6ac76362a445230bfee397d82d9c3d7e1167ca9
Sally Maki <sallymaki@gmail.com>
2018-06-08 16:45:27 -0700

    Change when to array wrap data

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,8 +5,7 @@ class Relationship
   attr_accessor :participant_id, :first_name, :last_name, :relationship_type
 
   class << self
-    def from_bgs_hash(hash)
-      hash_array = Array.wrap(hash)
+    def from_bgs_hash(_hash)
       new(
         participant_id: hash_array[:ptcpnt_id],
         first_name: hash_array[:first_name],
