bc4cb204eee4ee29eb21905d2fd5a14ff7e67290
Teja N <teja.s.naraparaju@gmail.com>
2018-06-08 14:11:54 -0400

    adding changes

--- app/models/vacols/travel_board_schedule.rb
+++ app/models/vacols/travel_board_schedule.rb
@@ -3,25 +3,36 @@ class VACOLS::TravelBoardSchedule < VACOLS::Record
   self.table_name = "vacols.tbsched"
 
   class << self
+
+    def select_tb_with_staff
+      # css_id is stored in the STAFF.SDOMAINID column and corresponds to TBSCHED.tbmem1, tbmem2, tbmem3, tbmem4
+      select("(TBSCHED.TBYEAR||'-'||TBSCHED.TBTRIP||'-'||TBSCHED.TBLEG) as tbsched_vdkey",
+        :tbmem1,
+        :tbmem2,
+        :tbmem3,
+        :tbmem4,
+        :tbro,
+        :tbstdate,
+        :tbenddate)
+      .joins(<<-SQL) 
+        join vacols.staff on
+        staff.sattyid = tbmem1 OR
+        staff.sattyid = tbmem2 OR
+        staff.sattyid = tbmem3 OR
+        staff.sattyid = tbmem4
+      SQL
+    end
+
     def hearings_for_judge(css_id)
       id = connection.quote(css_id)
 
       # css_id is stored in the STAFF.SDOMAINID column and corresponds to TBSCHED.tbmem1, tbmem2, tbmem3, tbmem4
-      select("(TBSCHED.TBYEAR||'-'||TBSCHED.TBTRIP||'-'||TBSCHED.TBLEG) as tbsched_vdkey",
-             :tbmem1,
-             :tbmem2,
-             :tbmem3,
-             :tbmem4,
-             :tbro,
-             :tbstdate,
-             :tbenddate)
-        .joins("join vacols.staff on
-                staff.sattyid = tbmem1 OR
-                staff.sattyid = tbmem2 OR
-                staff.sattyid = tbmem3 OR
-                staff.sattyid = tbmem4")
-        .where("staff.sdomainid = #{id}")
+      select_tb_with_staff.where("staff.sdomainid = #{id}")
         .where("tbstdate > ?", 1.year.ago.beginning_of_day)
+    end
+
+    def hearings_in_date_range(start_date, end_date)
+      select_tb_with_staff.where("tbstdate BETWEEN ? AND ?", start_date, end_date)
     end
   end
 
