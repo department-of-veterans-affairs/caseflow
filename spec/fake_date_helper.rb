c81a3073b0baae472535b47e89efcbe5c3cdd29f
Teja N <teja.s.naraparaju@gmail.com>
2018-06-11 15:36:48 -0400

    adding holidays to the date test generator

--- spec/fake_date_helper.rb
+++ spec/fake_date_helper.rb
@@ -4,9 +4,12 @@ module FakeDateHelper
 
     return nil if (end_date - start_date) < num_of_dates
 
+    holidays = Holidays.between(start_date, end_date, :federal_reserve)
+
     while dates.size < num_of_dates
       date = Faker::Date.between(start_date, end_date)
-      dates.add(date) unless exclude_weekends && (date.saturday? || date.sunday?)
+      dates.add(date) unless (exclude_weekends && (date.saturday? || date.sunday?)) ||
+        holidays.find { |holiday| holiday[:date] == date }.present? 
     end
 
     dates.to_a
