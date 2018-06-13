55a679664559b5ccc232f1ab6253a127e4f52f44
Sally Maki <sallymaki@gmail.com>
2018-06-12 17:33:24 -0700

    Create AmaReview, move date validation and claimants

--- app/models/supplemental_claim.rb
+++ app/models/supplemental_claim.rb
@@ -1,27 +1,11 @@
-class SupplementalClaim < ApplicationRecord
-  include EstablishesEndProduct
-
+class SupplementalClaim < AmaReview
   has_many :request_issues, as: :review_request
   has_many :claimants, as: :review_request
-
-  validate :validate_receipt_date
-  validates :receipt_date, presence: { message: "blank" }, if: :saving_review
-
-  AMA_BEGIN_DATE = Date.new(2018, 4, 17).freeze
 
   attr_reader :saving_review
 
   def start_review!
     @saving_review = true
-  end
-
-  def create_claimants!(claimant_data:)
-    claimants.destroy_all unless claimants.empty?
-    claimants.create_from_intake_data!(claimant_data)
-  end
-
-  def remove_claimants!
-    claimants.destroy_all
   end
 
   def create_end_product_and_contentions!
@@ -98,19 +82,5 @@ class SupplementalClaim < ApplicationRecord
 
   def end_product_station
     "397" # TODO: Change to 499 National Work Queue
-  end
-
-  def validate_receipt_date_not_before_ama
-    errors.add(:receipt_date, "before_ama") if receipt_date < AMA_BEGIN_DATE
-  end
-
-  def validate_receipt_date_not_in_future
-    errors.add(:receipt_date, "in_future") if Time.zone.today < receipt_date
-  end
-
-  def validate_receipt_date
-    return unless receipt_date
-    validate_receipt_date_not_before_ama
-    validate_receipt_date_not_in_future
   end
 end
