811ad04fa72f0826386dbd89318198bad39fcc3c
Mark Benjamin <mdbenjam@gmail.com>
2018-06-13 08:35:38 -0400

    Changing method name

--- app/controllers/appeals_controller.rb
+++ app/controllers/appeals_controller.rb
@@ -73,7 +73,7 @@ class AppealsController < ApplicationController
   end
 
   def appeal
-    @appeal ||= LegacyAppeal.find_appeal_or_legacy_appeal_by_id(params[:id])
+    @appeal ||= Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(params[:id])
   end
 
   def file_number_not_found_error
