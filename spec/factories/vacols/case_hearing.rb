11f8745f19d6d471c2989e8a8e8f3b9871e178a8
Mark Benjamin <mdbenjam@gmail.com>
2018-06-15 22:51:32 -0400

    Trying to build master records

--- spec/factories/vacols/case_hearing.rb
+++ spec/factories/vacols/case_hearing.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     hearing_type "V"
     hearing_date { Time.zone.today }
     room 1
+    sequence(:vdkey, 1_000_000)
 
     trait :disposition_held do
       hearing_disp "H"
@@ -19,5 +20,14 @@ FactoryBot.define do
     trait :disposition_no_show do
       hearing_disp "N"
     end
+
+
+    # after(:build) do |hearing, _evaluator|
+    #   # For video hearings we need to build the master record.
+    #   # binding.pry
+    #   if hearing.hearing_type == "V"
+    #     create(:case_hearing, hearing_type: "C", hearing_pkseq: hearing.vdkey + "RO16")
+    #   end
+    # end
   end
 end
