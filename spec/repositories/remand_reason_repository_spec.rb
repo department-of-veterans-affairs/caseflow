e92458e64e5f19603c2c6d61e99f6d535e331426
Danny Sellers <dsellers90@gmail.com>
2018-06-18 09:44:45 -0400

    add tests for RemandReasonRepository create_ and delete_remand_reasons

--- spec/repositories/remand_reason_repository_spec.rb
+++ spec/repositories/remand_reason_repository_spec.rb
@@ -1,4 +1,12 @@
 describe RemandReasonRepository do
+  before do
+    FeatureToggle.enable!(:test_facols)
+  end
+
+  after do
+    FeatureToggle.disable!(:test_facols)
+  end
+
   context ".update_remand_reasons" do
     subject { RemandReasonRepository.update_remand_reasons(record, issue_attrs) }
     let(:record) do
@@ -59,6 +67,55 @@ describe RemandReasonRepository do
 
       it "deletes existing remand reasons" do
         expect(RemandReasonRepository).to receive(:delete_remand_reasons!)
+        subject
+      end
+    end
+  end
+
+  context ".create_remand_reasons!" do
+    subject { RemandReasonRepository.create_remand_reasons!(vacols_id, vacols_sequence_id, remand_reasons) }
+    let(:vacols_id) { "123456" }
+    let(:vacols_sequence_id) { "3" }
+    let(:remand_reasons) { [{ code: "AB", after_certification: true }] }
+
+    it "creates remand reasons" do
+      expect(RemandReasonRepository).to receive(:create_remand_reasons!)
+        .with("123456", "3", remand_reasons).once
+      expect(BusinessMetrics).to_not receive(:record)
+      subject
+    end
+  end
+
+  context ".delete_remand_reasons!" do
+    subject { RemandReasonRepository.delete_remand_reasons!(vacols_id, vacols_sequence_id, **kwargs) }
+    let(:vacols_id) { "123456" }
+    let(:vacols_sequence_id) { "3" }
+    let(:remand_reasons) do
+      [
+        create(:remand_reason),
+        create(:remand_reason, rmdval: "DI"),
+        create(:remand_reason, rmdval: "AA")
+      ]
+    end
+
+    context "deletes a specific remand reason" do
+      let(:kwargs) {{ rmdval: "DI" }}
+
+      it "deletes a specific remand reason" do
+        expect(RemandReasonRepository).to receive(:delete_remand_reasons!)
+          .with("123456", "3", kwargs).once
+        expect(BusinessMetrics).to_not receive(:record)
+        subject
+      end
+    end
+
+    context "deletes all remand reasons for an issue" do
+      let(:kwargs) {{ }}
+
+      it "deletes all remand reasons for an issue" do
+        expect(RemandReasonRepository).to receive(:delete_remand_reasons!)
+          .with("123456", "3", kwargs).once
+        expect(BusinessMetrics).to_not receive(:record)
         subject
       end
     end
