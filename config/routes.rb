a8f376b2d7cb5f122f09baba4968eb7650c03c0b
Sharon Warner <sharon.warner56@gmail.com>
2018-06-18 16:21:27 -0400

    Frontend beginnings

--- config/routes.rb
+++ config/routes.rb
@@ -92,8 +92,8 @@ Rails.application.routes.draw do
     resources :dockets, only: [:index, :show], param: :docket_date
     resources :worksheets, only: [:update, :show], param: :hearing_id
     resources :appeals, only: [:update], param: :appeal_id
-    resources :hearing_day, only: [:index, :show]
   end
+  get 'hearings/schedule/build', to: "hearings/hearing_day#index"
   get 'hearings/:hearing_id/worksheet', to: "hearings/worksheets#show", as: 'hearing_worksheet'
   get 'hearings/:hearing_id/worksheet/print', to: "hearings/worksheets#show_print"
   post 'hearings/hearing_day', to: "hearings/hearing_day#create"
