56a22a9609fcd0699f7fcc364c117e947f3917f5
Mark Benjamin <mdbenjam@gmail.com>
2018-06-20 16:15:40 -0400

    Unskipping tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -15,8 +15,7 @@ RSpec.describe HomeController, type: :controller do
       end
     end
 
-    context "when visitor is logged in, does not have a personal queue, nor case search access",
-            skip: "This test is failing, I think, because of a rogue FeatureToggle" do
+    context "when visitor is logged in, does not have a personal queue, nor case search access" do
       # fakes/user_repository returns "Judge" for role for "BVAAABSHIRE",
       # so this user will not have a personal queue
       let!(:current_user) { User.authenticate!(css_id: "BVAAABSHIRE") }
@@ -27,8 +26,7 @@ RSpec.describe HomeController, type: :controller do
       end
     end
 
-    context "when visitor is logged in, does not have a personal queue but does have case search access",
-            skip: "This test is failing, I think, because of a rogue FeatureToggle" do
+    context "when visitor is logged in, does not have a personal queue but does have case search access" do
       let!(:current_user) { User.authenticate!(css_id: "BVAAABSHIRE") }
 
       before do
