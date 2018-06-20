52fb5b4b5dfcedf25ee17adc2c878f1b79c58a43
Sharon Warner <sharon.warner56@gmail.com>
2018-06-20 11:45:15 -0400

    Error fix

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,5 @@
 class ApplicationJob < ActiveJob::Base
-  def application_attr(app_name)
+  def self.application_attr(app_name)
     # setup debug context
     Raven.extra_context(application: app_name)
   end
