a3e820a8a34c2e1683501c8f1da728618387e55c
Mark Benjamin <mdbenjam@gmail.com>
2018-06-20 15:41:28 -0400

    Fixing test?

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -30,8 +30,16 @@ RSpec.describe HomeController, type: :controller do
     context "when visitor is logged in, does not have a personal queue but does have case search access",
             skip: "This test is failing, I think, because of a rogue FeatureToggle" do
       let!(:current_user) { User.authenticate!(css_id: "BVAAABSHIRE") }
-      it "should land at /" do
+
+      before do
         FeatureToggle.enable!(:case_search_home_page, users: [current_user.css_id])
+      end
+
+      after do
+        FeatureToggle.disable!(:case_search_home_page, users: [current_user.css_id])
+      end
+
+      it "should land at /" do
         get :index
         expect(response.status).to eq 200
       end
@@ -51,8 +59,16 @@ RSpec.describe HomeController, type: :controller do
 
     context "when visitor is logged in, has a personal queue and case search access" do
       let!(:current_user) { User.authenticate! }
-      it "should redirect to /queue" do
+
+      before do
         FeatureToggle.enable!(:case_search_home_page, users: [current_user.css_id])
+      end
+
+      after do
+        FeatureToggle.disable!(:case_search_home_page, users: [current_user.css_id])
+      end
+
+      it "should redirect to /queue" do
         get :index
         expect(response.status).to eq 302
         expect(URI.parse(response.location).path).to eq("/queue")
