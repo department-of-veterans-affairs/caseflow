4b83c709008048f4dd8903c5fb91d18e52922b2e
Sally Maki <sallymaki@gmail.com>
2018-06-21 17:06:59 -0400

    Fix receipt_date validation

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -1,5 +1,5 @@
 class Appeal < AmaReview
-  validates :docket_type, presence: { message: "blank" }, on: :saving_review
+  validates :docket_type, presence: { message: "blank" }, if: :saving_review
 
   UUID_REGEX = /^\h{8}-\h{4}-\h{4}-\h{4}-\h{12}$/
 
