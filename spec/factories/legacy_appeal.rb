5ca119d863bcf23ad6fcb9351432bbbf7b87fbed
Mark Benjamin <mdbenjam@gmail.com>
2018-06-21 21:27:49 -0400

    Removing changes to veteran factory

--- spec/factories/legacy_appeal.rb
+++ spec/factories/legacy_appeal.rb
@@ -6,11 +6,5 @@ FactoryBot.define do
 
     vacols_id { vacols_case.bfkey }
     vbms_id { vacols_case.bfcorlid }
-
-    after(:create) do |legacy_appeal, _evaluator|
-      if !Veteran.find_by(file_number: legacy_appeal.veteran_file_number)
-        create(:veteran, file_number: legacy_appeal.veteran_file_number)
-      end
-    end
   end
 end
