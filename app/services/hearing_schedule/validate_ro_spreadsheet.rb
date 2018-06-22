b6d7f6c11a40e2a43ecbb8be82e504bfe9f39b94
Sandra Jones <sjones@tistatech.com>
2018-06-22 15:59:31 -0400

    Template Validation for Allocation Days (#5963)
    Template Validation for Allocation Days (#5963)

--- app/services/hearing_schedule/validate_ro_spreadsheet.rb
+++ app/services/hearing_schedule/validate_ro_spreadsheet.rb
@@ -2,6 +2,9 @@ class HearingSchedule::ValidateRoSpreadsheet
   RO_NON_AVAILABILITY_SHEET = 0
   CO_NON_AVAILABILITY_SHEET = 1
   HEARING_ALLOCATION_SHEET = 2
+  HEARING_ALLOCATION_SHEET_TITLE = "Allocation of Regional Office Video Hearings and Central Office Hearings".freeze
+  HEARING_ALLOCATION_SHEET_FIRST_SECOND_HEADER_ROW = [nil, "RO City, State", "BFREGOFF",
+                                                      "Number of Hearing Days Allocated in Date Range"].freeze
 
   class RoDatesNotUnique < StandardError; end
   class RoDatesNotInRange < StandardError; end
@@ -16,6 +19,7 @@ class HearingSchedule::ValidateRoSpreadsheet
   class AllocationRoListedIncorrectly < StandardError; end
   class AllocationDuplicateRo < StandardError; end
   class AllocationCoLocationIncorrect < StandardError; end
+  class AllocationNotFollowed < StandardError; end
 
   def initialize(spreadsheet, start_date, end_date)
     @spreadsheet = spreadsheet
@@ -133,7 +137,14 @@ class HearingSchedule::ValidateRoSpreadsheet
     }
   end
 
-  def validate_hearing_allocation_template; end
+  def validate_hearing_allocation_template
+    unless hearing_allocation_template.row(1)[0] == HEARING_ALLOCATION_SHEET_TITLE &&
+           hearing_allocation_template.column(1)[3].nil? &&
+           hearing_allocation_template.column(5).uniq == [nil] &&
+           hearing_allocation_template.row(2).uniq == HEARING_ALLOCATION_SHEET_FIRST_SECOND_HEADER_ROW
+      fail AllocationNotFollowed
+    end
+  end
 
   def validate_hearing_ro_allocation_days
     unless hearing_ro_allocation_days.all? { |row| row["allocated_days"].is_a?(Numeric) }
