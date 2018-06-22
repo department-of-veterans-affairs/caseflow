445c05559b0d7d28dbff814f04cb536cd4a8dca9
aroltsch <anya.roltsch@gmail.com>
2018-06-22 11:03:35 -0400

    Tests

--- app/models/tasks/co_located_admin_action.rb
+++ app/models/tasks/co_located_admin_action.rb
@@ -1,11 +1,11 @@
 class CoLocatedAdminAction < Task
-  after_initialize :set_assigned_to
   validates :title, inclusion: { in: Constants::CO_LOCATED_ADMIN_ACTIONS.keys.map(&:to_s) }
   validate :assigned_by_role_is_valid
 
   class << self
     def create(params)
       ActiveRecord::Base.multi_transaction do
+        params[:assigned_to] = next_assignee
         records = params.delete(:titles).each_with_object([]) do |title, result|
           result << super(params.merge(title: title))
           result
@@ -17,21 +17,27 @@ class CoLocatedAdminAction < Task
       end
     end
 
-    def latest_task
-      sort(&:created_at).last
-    end
-
-    def last_assignee_css_id
-      last_created_task ? last_created_task.user.css_id : nil
-    end
-
-    def next_assignee_css_id
-      last_assignee_css_id ? list_of_assignees[list_of_assignees.index(last_assignee_css_id) + 1] :
-        list_of_assignees[0]
-    end
+    private
 
     def next_assignee
       User.find_or_create_by(css_id: next_assignee_css_id, station_id: User::BOARD_STATION_ID)
+    end
+
+    def latest_task
+      order("created_at").last
+    end
+
+    def last_assignee_css_id
+      latest_task ? latest_task.assigned_to.css_id : nil
+    end
+
+    def next_assignee_css_id
+      list_of_assignees[next_assignee_index]
+    end
+
+    def next_assignee_index
+      return 0 unless last_assignee_css_id
+      (list_of_assignees.index(last_assignee_css_id) + 1) % list_of_assignees.length
     end
 
     def list_of_assignees
@@ -43,9 +49,5 @@ class CoLocatedAdminAction < Task
 
   def assigned_by_role_is_valid
     errors.add(:assigned_by, "has to be an attorney") if assigned_by && !assigned_by.attorney_in_vacols?
-  end
-
-  def set_assigned_to
-    self.assigned_to = CoLocatedAdminAction.next_assignee
   end
 end
