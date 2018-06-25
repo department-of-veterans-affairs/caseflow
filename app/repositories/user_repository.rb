16951bffa4282d99935d54e78158a72d4ebcaca0
Mark Benjamin <mdbenjam@gmail.com>
2018-06-25 18:17:12 -0400

    Moving caching layer from user repository to user

--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -4,16 +4,16 @@ class UserRepository
       @staff_records ||= {}
     end
 
-    def vacols_uniq_id(css_id)
-      staff_record_by_css_id(css_id).try(:slogid)
-    end
-
-    # STAFF.SVLJ = 'J' indicates a user is a Judge, the field may also have an 'A' which indicates an Acting judge.
-    # If the STAFF.SVLJ is nil and STAFF.SATTYID is not nil then it is an attorney.
-    def vacols_roles(css_id)
+    def user_info_from_vacols(css_id)
       staff_record = staff_record_by_css_id(css_id)
-      return roles_based_on_staff_fields(staff_record) if staff_record
-      []
+
+      {
+        uniq_id: vacols_uniq_id(staff_record),
+        roles: vacols_roles(staff_record),
+        attorney_id: vacols_attorney_id(staff_record),
+        group_id: vacols_group_id(staff_record),
+        full_name: vacols_full_name(staff_record)
+      }
     end
 
     def can_access_task?(css_id, vacols_id)
@@ -22,22 +22,6 @@ class UserRepository
         fail Caseflow::Error::UserRepositoryError, msg
       end
       true
-    end
-
-    # :nocov:
-    def vacols_attorney_id(css_id)
-      staff_record_by_css_id(css_id).try(:sattyid)
-    end
-
-    def vacols_group_id(css_id)
-      staff_record_by_css_id(css_id).try(:stitle) || ""
-    end
-
-    def vacols_full_name(css_id)
-      record = staff_record_by_css_id(css_id)
-      if record
-        FullName.new(record.snamef, record.snamemi, record.snamel).formatted(:readable_full)
-      end
     end
 
     def css_id_by_full_name(full_name)
@@ -72,9 +56,34 @@ class UserRepository
       []
     end
 
+    def vacols_uniq_id(staff_record)
+      staff_record.try(:slogid)
+    end
+
+    # STAFF.SVLJ = 'J' indicates a user is a Judge, the field may also have an 'A' which indicates an Acting judge.
+    # If the STAFF.SVLJ is nil and STAFF.SATTYID is not nil then it is an attorney.
+    def vacols_roles(staff_record)
+      return roles_based_on_staff_fields(staff_record) if staff_record
+      []
+    end
+
+    # :nocov:
+    def vacols_attorney_id(staff_record)
+      staff_record.try(:sattyid)
+    end
+
+    def vacols_group_id(staff_record)
+      staff_record.try(:stitle) || ""
+    end
+
+    def vacols_full_name(staff_record)
+      if staff_record
+        FullName.new(staff_record.snamef, staff_record.snamemi, staff_record.snamel).formatted(:readable_full)
+      end
+    end
+
     def staff_record_by_css_id(css_id)
-      staff_records[css_id] ||= VACOLS::Staff.find_by(sdomainid: css_id)
-      staff_records[css_id]
+      VACOLS::Staff.find_by(sdomainid: css_id)
     end
     # :nocov:
   end
