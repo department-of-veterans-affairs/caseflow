85f122d1a41ce1e0b093a000ccba5b8a6f3a29bc
Mark Benjamin <mdbenjam@gmail.com>
2018-06-26 17:39:01 -0400

    Move backfilling, not nil to separate PR

--- db/migrate/20180626154709_disallow_nil_in_claims_folder_and_appeal_views.rb
+++
@@ -1,6 +0,0 @@
-class DisallowNilInClaimsFolderAndAppealViews < ActiveRecord::Migration[5.1]
-  def change
-    change_column_null :claims_folder_searches, :appeal_type, false
-    change_column_null :appeal_views, :appeal_type, false
-  end
-end
