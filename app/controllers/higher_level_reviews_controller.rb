edddef5ba094bf5be84da6718f021a040f57d862
Sally Maki <sallymaki@gmail.com>
2018-06-26 16:50:23 -0400

    Review changes

--- app/controllers/higher_level_reviews_controller.rb
+++ app/controllers/higher_level_reviews_controller.rb
@@ -1,6 +1,14 @@
 class HigherLevelReviewsController < ApplicationController
   before_action :verify_access, :react_routed, :verify_feature_enabled, :set_application
 
+  private
+
+  def higher_level_review
+    @higher_level_review ||= HigherLevelReview.find_by!(end_product_reference_id: params[:claim_id])
+  end
+
+  helper_method :higher_level_review
+
   def set_application
     RequestStore.store[:application] = "intake"
   end
--- app/controllers/supplemental_claims_controller.rb
+++ app/controllers/supplemental_claims_controller.rb
@@ -1,6 +1,12 @@
 class SupplementalClaimsController < ApplicationController
   before_action :verify_access, :react_routed, :verify_feature_enabled, :set_application
 
+  private
+
+  def supplemental_clai
+    @supplemental_clai  ||= SupplementalClaim.find_by!(end_product_reference_id: params[:claim_id])
+  end
+
   def set_application
     RequestStore.store[:application] = "intake"
   end
