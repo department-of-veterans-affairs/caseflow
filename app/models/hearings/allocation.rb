39468bec39569d70c4871a50bbc9e165bf6530fb
Sharon Warner <sharon.warner56@gmail.com>
2018-06-27 11:23:19 -0400

    Adding transactions for database imports

--- app/models/hearings/allocation.rb
+++ app/models/hearings/allocation.rb
@@ -7,10 +7,12 @@ class Allocation < ApplicationRecord
       ro_allocation_data = spreadsheet.allocation_ro_data
       co_allocation_data = spreadsheet.allocation_co_data
       allocation = []
-      ro_allocation_data.push(co_allocation_data).each do |row|
-        allocation << Allocation.create!(schedule_period: schedule_period,
-                                         allocated_days: row["allocated_days"],
-                                         regional_office: row["ro_code"])
+      transaction do
+        ro_allocation_data.push(co_allocation_data).each do |row|
+          allocation << Allocation.create!(schedule_period: schedule_period,
+                                           allocated_days: row["allocated_days"],
+                                           regional_office: row["ro_code"])
+        end
       end
       allocation
     end
--- app/models/hearings/ro_non_availability.rb
+++ app/models/hearings/ro_non_availability.rb
@@ -3,10 +3,12 @@ class RoNonAvailability < NonAvailability
     def import_ro_non_availability(schedule_period)
       dates = HearingSchedule::GetSpreadsheetData.new(schedule_period.spreadsheet).ro_non_availability_data
       ro_non_availability = []
-      dates.each do |date|
-        ro_non_availability << RoNonAvailability.create!(schedule_period: schedule_period,
-                                                         date: date["date"],
-                                                         object_identifier: date["ro_code"])
+      transaction do
+        dates.each do |date|
+          ro_non_availability << RoNonAvailability.create!(schedule_period: schedule_period,
+                                                           date: date["date"],
+                                                           object_identifier: date["ro_code"])
+        end
       end
       ro_non_availability
     end
