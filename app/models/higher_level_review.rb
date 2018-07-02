7dd33d453dfff034c88e9fbc03e207bb937433bb
Shane Russell <shane.russell.va@gmail.com>
2018-07-02 18:18:07 -0400

    Remove establishes end product
    connects #5531

--- app/models/higher_level_review.rb
+++ app/models/higher_level_review.rb
@@ -6,14 +6,35 @@ class HigherLevelReview < AmaReview
 
   END_PRODUCT_MODIFIERS = %w[030 031 032 033 033 035 036 037 038 039].freeze
 
-  private
-
-  # TODO: Update with real code and modifier data
-  def end_product_code
-    "030HLRR"
+  def end_product_description
+    end_product_establishment.description
   end
 
-  def valid_modifiers
-    END_PRODUCT_MODIFIERS
+  def end_product_base_modifier
+    # This is for EPs not yet created or that failed to create
+    end_product_establishment.valid_modifiers.first
+  end
+
+  def establish_end_product!
+    end_product_establishment.perform!
+
+    update!(
+      end_product_reference_id: end_product_establishment.reference_id,
+      established_at: Time.zone.now
+    )
+  end
+
+  private
+
+  def end_product_establishment
+    @end_product_establishment ||= EndProductEstablishment.new(
+      veteran: veteran,
+      reference_id: end_product_reference_id,
+      claim_date: receipt_date,
+      code: "030HLRR",
+      valid_modifiers: END_PRODUCT_MODIFIERS,
+      station: "397",  # AMC
+      cached_status: end_product_status
+    )
   end
 end
