b13d6a364e69d9e7bdb6bd7270fa99551f96d1b8
Evan Kroske <evan@navahq.com>
2018-07-05 15:51:02 -0400

    Adds a section for the second dropdown

--- client/app/components/AssignWidget.jsx
+++ client/app/components/AssignWidget.jsx
@@ -102,6 +102,8 @@ class AssignWidget extends React.PureComponent<{|
           onChange={(option) => this.props.setSelectedAssignee({ assigneeId: option.value })}
           value={selectedOption}
           styling={css({ width: '30rem' })} />
+        {showOtherSearchBox &&
+          <p>Enter the name of an attorney or judge:</p>}
         <Button
           onClick={this.handleButtonClick}
           name={`Assign ${this.selectedTasks().length} case(s)`}
