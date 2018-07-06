a846839c7aa27ac55b9b9d6b29dbd1e1315d1c66
aroltsch <anya.roltsch@gmail.com>
2018-07-06 10:49:48 -0400

    Upper case roles back

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe UsersController, type: :controller do
   let!(:user) { User.authenticate!(roles: ["System Admin"]) }
   let!(:staff) { create(:staff, :attorney_judge_role, user: user) }
 
-  describe "GET /users?role=judge" do
+  describe "GET /users?role=Judge" do
     let!(:judges) { create_list(:staff, 2, :judge_role) }
 
     context "when role is passed" do
       it "should return a list of judges" do
-        get :index, params: { role: "judge" }
+        get :index, params: { role: "Judge" }
         expect(response.status).to eq 200
         response_body = JSON.parse(response.body)
         expect(response_body["judges"].size).to eq 3
@@ -32,12 +32,12 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "GET /users?role=attorney" do
+  describe "GET /users?role=Attorney" do
     context "when judge ID is passed" do
       let!(:judge) { User.create(css_id: "BVARZIEMANN1", station_id: User::BOARD_STATION_ID) }
 
       it "should return a list of attorneys associated with the judge" do
-        get :index, params: { role: "attorney", judge_css_id: judge.css_id }
+        get :index, params: { role: "Attorney", judge_css_id: judge.css_id }
         expect(response.status).to eq 200
         response_body = JSON.parse(response.body)
         expect(response_body["attorneys"].size).to eq 3
@@ -49,7 +49,7 @@ RSpec.describe UsersController, type: :controller do
       let!(:judges) { create_list(:staff, 2, :judge_role) }
 
       it "should return a list of all attorneys" do
-        get :index, params: { role: "attorney" }
+        get :index, params: { role: "Attorney" }
         expect(response.status).to eq 200
         response_body = JSON.parse(response.body)
         # four regular attorneys and one attorney acting as a judge
