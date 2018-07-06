5f1d0e4ac848a364c11f2101ed3e1956d012bc06
Sally Maki <sallymaki@gmail.com>
2018-07-06 15:31:30 -0700

    Higher level review model

--- spec/models/higher_level_review_spec.rb
+++ spec/models/higher_level_review_spec.rb
@@ -8,19 +8,13 @@ describe HigherLevelReview do
   let(:receipt_date) { SupplementalClaim::AMA_BEGIN_DATE + 1 }
   let(:informal_conference) { nil }
   let(:same_office) { nil }
-  let(:end_product_reference_id) { nil }
-  let(:established_at) { nil }
-  let(:end_product_status) { nil }
 
   let(:higher_level_review) do
-    HigherLevelReview.new(
-      veteran_file_number: veteran_file_number,
+    build(:higher_level_review,
       receipt_date: receipt_date,
+      veteran_file_number: veteran_file_number,
       informal_conference: informal_conference,
-      same_office: same_office,
-      end_product_reference_id: end_product_reference_id,
-      established_at: established_at,
-      end_product_status: end_product_status
+      same_office: same_office
     )
   end
 
