2b1bc3f42c6788025a4570ad3fdfd1270320de37
Sally Maki <sallymaki@gmail.com>
2018-07-09 15:58:24 -0700

    Move Intake tests to FACOLS (#6157)
    * Turn on FACOLS testing
    
    * Fix ramp election rollback vacols test

--- spec/models/ramp_election_intake_spec.rb
+++ spec/models/ramp_election_intake_spec.rb
@@ -1,10 +1,7 @@
 describe RampElectionIntake do
   before do
-    Timecop.freeze(Time.utc(2019, 1, 1, 12, 0, 0))
-  end
-
-  before do
     FeatureToggle.enable!(:test_facols)
+    Timecop.freeze(Time.utc(2019, 1, 1, 12, 0, 0))
   end
 
   after do
