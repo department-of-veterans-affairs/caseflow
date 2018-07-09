f4eb4db2ceb6771ca8653fc1d3dcb5d154c4999e
Evan Kroske <evan@navahq.com>
2018-07-09 10:12:15 -0400

    Removes obnoxious error from unassigned cases page

--- client/app/queue/UnassignedCasesPage.jsx
+++ client/app/queue/UnassignedCasesPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import StatusMessage from '../components/StatusMessage';
 import JudgeAssignTaskTable from './JudgeAssignTaskTable';
 import {
   initialAssignTasksToUser
@@ -9,38 +8,25 @@ import {
 import AssignWidget from './components/AssignWidget';
 
 const UnassignedCasesPage = (props) => {
-  const { tasksAndAppeals: { length: reviewableCount }, userId, featureToggles } = props;
-  let tableContent;
+  const { userId, featureToggles } = props;
 
-  if (reviewableCount === 0) {
-    tableContent = <StatusMessage title="Tasks not found">
-       Congratulations! You don't have any cases to assign.
-    </StatusMessage>;
-  } else {
-    tableContent = <React.Fragment>
-      <h2>Cases to Assign</h2>
-      {featureToggles.judge_assign_cases &&
-        <AssignWidget previousAssigneeId={userId}
-          onTaskAssignment={(params) => props.initialAssignTasksToUser(params)} />}
-      <JudgeAssignTaskTable {...props} />
-    </React.Fragment>;
-  }
-
-  return tableContent;
+  return <React.Fragment>
+    <h2>Cases to Assign</h2>
+    {featureToggles.judge_assign_cases &&
+      <AssignWidget previousAssigneeId={userId}
+        onTaskAssignment={(params) => props.initialAssignTasksToUser(params)} />}
+    <JudgeAssignTaskTable {...props} />
+  </React.Fragment>;
 };
 
 const mapStateToProps = (state) => {
   const {
-    queue: {
-      attorneysOfJudge
-    },
     ui: {
       featureToggles
     }
   } = state;
 
   return {
-    attorneysOfJudge,
     featureToggles
   };
 };
