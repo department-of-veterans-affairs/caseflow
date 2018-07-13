d967cb8c73ec719eb1332894fc490e06e964b030
Teja N <teja.s.naraparaju@gmail.com>
2018-07-13 10:20:21 -0700

    caching results and formatting data as needed

--- spec/models/schedule_period_spec.rb
+++ spec/models/schedule_period_spec.rb
@@ -1,5 +1,14 @@
 describe SchedulePeriod do
   let(:schedule_period) { create(:ro_schedule_period) }
+  let(:allocation) do
+    create(:allocation, regional_office: "RO17", allocated_days: 118, schedule_period: schedule_period)
+  end
+  before do
+    get_unique_dates_for_ro_between("RO17", schedule_period, 35)
+    get_unique_dates_between(schedule_period.start_date, schedule_period.end_date, 20).map do |date|
+      create(:co_non_availability, date: date, schedule_period_id: schedule_period.id)
+    end
+  end
 
   context "spreadsheet" do
     before do
@@ -9,5 +18,11 @@ describe SchedulePeriod do
     subject { schedule_period.spreadsheet }
 
     it { is_expected.to be_a(Roo::Excelx) }
+  end
+
+  context "generate hearing schedule" do
+    it do
+      expect(schedule_period.genenerate_ro_hearing_schedule.count).to eq(allocation.allocated_days)
+    end
   end
 end
