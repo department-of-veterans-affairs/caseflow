259e8e5897bf3bef1aa8c3d17021bcd3c99a67ca
Teja N <teja.s.naraparaju@gmail.com>
2018-07-16 11:45:43 -0400

    fixing specs

--- spec/models/schedule_period_spec.rb
+++ spec/models/schedule_period_spec.rb
@@ -1,8 +1,13 @@
 describe SchedulePeriod do
-  let(:schedule_period) { create(:ro_schedule_period) }
+  let(:schedule_period) do
+    create(:ro_schedule_period, start_date: Date.parse("2019-04-01"),
+                                end_date: Date.parse("2019-09-30"))
+  end
+
   let(:allocation) do
     create(:allocation, regional_office: "RO17", allocated_days: 118, schedule_period: schedule_period)
   end
+
   before do
     get_unique_dates_for_ro_between("RO17", schedule_period, 35)
     get_unique_dates_between(schedule_period.start_date, schedule_period.end_date, 20).map do |date|
@@ -21,8 +26,16 @@ describe SchedulePeriod do
   end
 
   context "generate hearing schedule" do
+    before do
+      allocation
+    end
+
     it do
       expect(schedule_period.ro_hearing_day_allocations.count).to eq(allocation.allocated_days)
+      expect(schedule_period.ro_hearing_day_allocations[0].key?(:hearing_type)).to be_truthy
+      expect(schedule_period.ro_hearing_day_allocations[0].key?(:hearing_date)).to be_truthy
+      expect(schedule_period.ro_hearing_day_allocations[0].key?(:room_info)).to be_truthy
+      expect(schedule_period.ro_hearing_day_allocations[0].key?(:regional_office)).to be_truthy
     end
   end
 end
