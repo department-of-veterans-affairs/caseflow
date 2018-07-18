5fd3ed9b106a7155f0f3141f6736d0d08e6d8da9
Alex Prokop <alex.prokop@gmail.com>
2018-07-18 12:56:02 -0400

    remove byebug

--- app/controllers/idt/api/v1/appeals_controller.rb
+++ app/controllers/idt/api/v1/appeals_controller.rb
@@ -4,9 +4,6 @@ class Idt::Api::V1::AppealsController < ActionController::Base
   def validate_token
     token = request.headers["token"]
     return render json: {message: "Missing token"}, code: 403 unless token
-
-    byebug
-
     return render json: {message: "Invalid token"}, code: 403 unless Idt::Token.active?(token)
   end
 
