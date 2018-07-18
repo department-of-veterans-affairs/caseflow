20bfdcb27fcaac1355a1b0d91b2b412aaffbbc1e
Alex Prokop <alex.prokop@gmail.com>
2018-07-18 14:01:40 -0400

    hail rubocop

--- app/controllers/idt/api/v1/appeals_controller.rb
+++ app/controllers/idt/api/v1/appeals_controller.rb
@@ -3,12 +3,12 @@ class Idt::Api::V1::AppealsController < ActionController::Base
 
   def validate_token
     token = request.headers["TOKEN"]
-    return render json: {message: "Missing token"}, status: 400 unless token
-    return render json: {message: "Invalid token"}, status: 403 unless Idt::Token.active?(token)
+    return render json: { message: "Missing token" }, status: 400 unless token
+    return render json: { message: "Invalid token" }, status: 403 unless Idt::Token.active?(token)
   end
 
   def fetch_appeals
-    # return list of appeals assigned to attorney
+    # TODO: return list of appeals assigned to attorney
     render json: { message: "Successfully authenticated." }
   end
 end
--- app/controllers/idt/authentications_controller.rb
+++ app/controllers/idt/authentications_controller.rb
@@ -2,10 +2,10 @@ class Idt::AuthenticationsController < ApplicationController
   def activate_token
     key = params[:one_time_key]
 
-    return render json: { message: "Missing key."}, status: 400 unless key
+    return render json: { message: "Missing key." }, status: 400 unless key
 
     Idt::Token.activate_proposed_token(key)
-    render json: { message: "Success!"}, status: 200
+    render json: { message: "Success!" }
   rescue Caseflow::Error::InvalidOneTimeKey
     render json: { message: "Invalid key." }, status: 400
   end
