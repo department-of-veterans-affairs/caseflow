4b09f14459a552303199d4fcc2cb6146c47577ce
Evan Kroske <evan@navahq.com>
2018-07-19 17:21:12 -0400

    Fixed merge conflict

--- client/app/queue/utils.js
+++ client/app/queue/utils.js
@@ -13,11 +13,9 @@ import type {
   Issue,
   Issues
 } from './types/models';
-import type { State } from './types/state';
+import type { State, LoadedQueueAppeals } from './types/state';
 import ISSUE_INFO from '../../constants/ISSUE_INFO.json';
 import DIAGNOSTIC_CODE_DESCRIPTIONS from '../../constants/DIAGNOSTIC_CODE_DESCRIPTIONS.json';
-import type { Tasks } from './types/models';
-import type { State, LoadedQueueAppeals } from './types/state';
 import VACOLS_DISPOSITIONS_BY_ID from '../../constants/VACOLS_DISPOSITIONS_BY_ID.json';
 import DECISION_TYPES from '../../constants/APPEAL_DECISION_TYPES.json';
 
