547dedf866b6e4ffede62d2a84e57fae01c537ac
Alex Prokop <alex.prokop@gmail.com>
2018-07-19 10:55:08 -0400

    Creates IDT auth framework (#6264)
    Connects #6254
    
    ### Description
    Adds a prototype of authentication functionality for the IDT to make authenticated API requests to Caseflow. No DB data is returned at this point, so we don't need to feature flag anything.
    
    ### Test plan
    1. `curl localhost:3000/idt/api/v1/token`
    1. Verify response is like `{ one_time_key: "dfsafjslakfs", token: "fdsakfjsa"}`
    1. `curl localhost:3000/idt/api/v1/appeals`
    1. Verify 400 is returned
    1. `curl localhost:3000/idt/api/v1/appeals -H "token: fdsakfjsa"`
    1. Verify 403 is returned
    1. Open `localhost:3000/idt/auth?one_time_key=dfsafjslakfs`
    1. Verify success message is shown
    1. Open `localhost:3000/idt/auth?one_time_key=dfsafjslakfs`
    1. Verify error is shown
    1. `curl  -H "TOKEN: fdsakfjsa" localhost:3000/idt/api/v1/appeals`
    1. Verify 200 is returned

---
+++ app/models/idt/token.rb
@@ -0,0 +1,45 @@
+class Idt::Token
+  VALID_TOKENS_KEY = "valid_tokens_key".freeze
+  ONE_TIME_KEYS_KEY = "one_time_keys_key".freeze
+  TOKEN_VALIDITY_IN_SECONDS = 60 * 60 * 24 * 3
+
+  def self.generate_proposed_token_and_one_time_key
+    one_time_key = SecureRandom.hex(64)
+    token = SecureRandom.hex(64)
+
+    # Associate key and token, so we can later use the one time key
+    # to activate the token.
+    client.set(ONE_TIME_KEYS_KEY + one_time_key, token)
+
+    [one_time_key, token]
+  end
+
+  def self.activate_proposed_token(one_time_key)
+    token = client.get(ONE_TIME_KEYS_KEY + one_time_key)
+
+    fail Caseflow::Error::InvalidOneTimeKey unless token && token.length == 128
+
+    # Remove the one_time_key/token association to ensure it isn't used again,
+    # and move the token to the valid tokens list for the validity period.
+    client.del(ONE_TIME_KEYS_KEY + one_time_key)
+    client.set(VALID_TOKENS_KEY + token, true)
+    client.expire(VALID_TOKENS_KEY + token, TOKEN_VALIDITY_IN_SECONDS)
+
+    true
+  end
+
+  def self.active?(token)
+    # check if token is in valid list and return boolean
+    client.exists(VALID_TOKENS_KEY + token)
+  end
+
+  def self.client
+    # Use separate Redis namespace for test to avoid conflicts between test and dev environments
+    namespace = Rails.env.test? ? :idt_test : :idt
+    @client ||= Redis::Namespace.new(namespace, redis: redis)
+  end
+
+  def self.redis
+    @redis ||= Redis.new(url: Rails.application.secrets.redis_url_cache)
+  end
+end
