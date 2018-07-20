e777e0bb74a2424f748630c0461fd65fa23f0b34
Evan Kroske <evan@navahq.com>
2018-07-20 13:31:34 -0400

    Fixed a bug

--- app/controllers/legacy_tasks_controller.rb
+++ app/controllers/legacy_tasks_controller.rb
@@ -11,7 +11,7 @@ class LegacyTasksController < ApplicationController
   end
 
   def index
-    current_role = params[:role].downcase || user.vacols_roles.first
+    current_role = (params[:role] || user.vacols_roles.first).downcase
     return invalid_role_error unless ROLES.include?(current_role)
     respond_to do |format|
       format.html do
