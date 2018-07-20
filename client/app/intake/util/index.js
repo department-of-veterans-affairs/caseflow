e90894de1476437188abe034199017a27c1a4ad6
Kara Louie <kara@truss.works>
2018-07-20 14:31:44 -0700

    Choose date format for decision date

--- client/app/intake/util/index.js
+++ client/app/intake/util/index.js
@@ -76,7 +76,11 @@ export const formatIssues = (intakeState) => {
           return {
             decision_text: issue.description,
             issue_category: issue.category,
+<<<<<<< Updated upstream
             decision_date: formatDateStringForApi(issue.decisionDate)
+=======
+            decision_date: issue.decisionDate
+>>>>>>> Stashed changes
           };
         })
   };
