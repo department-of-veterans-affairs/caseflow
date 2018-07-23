96e8099ad1125be940011676de33a289fb8697d0
Sharon Warner <sharon.warner56@gmail.com>
2018-07-23 16:30:11 -0400

    Removing CO allocation from spreadsheet template (#6316)
    Connects #6303 
    ### Description
    This PR removes CO allocations from the spreadsheet template and import
    
    ### Testing Plan
    1. Download template and confirm Central Office is removed from the allocation sheet
    1. Confirm test suite passes

--- app/models/hearings/allocation.rb
+++ app/models/hearings/allocation.rb
@@ -4,11 +4,10 @@ class Allocation < ApplicationRecord
   class << self
     def import_allocation(schedule_period)
       spreadsheet = HearingSchedule::GetSpreadsheetData.new(schedule_period.spreadsheet)
-      ro_allocation_data = spreadsheet.allocation_ro_data
-      # co_allocation_data = spreadsheet.allocation_co_data
+      allocation_data = spreadsheet.allocation_data
       allocation = []
       transaction do
-        ro_allocation_data.each do |row|
+        allocation_data.each do |row|
           allocation << Allocation.create!(schedule_period: schedule_period,
                                            allocated_days: row["allocated_days"],
                                            regional_office: row["ro_code"])
