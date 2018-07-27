f3d2a272f0efb812724b267b00767cfaf7acaef3
Alex Prokop <alex.prokop@gmail.com>
2018-07-27 14:23:49 -0400

    pr review tweaks

--- app/repositories/judge_repository.rb
+++ app/repositories/judge_repository.rb
@@ -1,13 +1,13 @@
 class JudgeRepository
   def self.find_all_judges
-    records = VACOLS::Staff.where(svlj: %w[J A], sactive: "A")
+    records = judge_records
     records.select(&:sdomainid).map do |record|
       User.find_or_create_by(css_id: record.sdomainid, station_id: User::BOARD_STATION_ID)
     end
   end
 
   def self.find_all_judges_with_name_and_id
-    records = VACOLS::Staff.where(svlj: %w[J A], sactive: "A")
+    records = judge_records
     # Our user model only contains a full name field, but for certain applications
     # (like the IDT), we need the separate name fields from VACOLS.
     records.map do |record|
@@ -16,5 +16,9 @@ class JudgeRepository
         last_name: record.snamel,
         vacols_attorney_id: record.sattyid }
     end
+  end
+
+  def self.judge_records
+    records = VACOLS::Staff.where(svlj: %w[J A], sactive: "A")
   end
 end
