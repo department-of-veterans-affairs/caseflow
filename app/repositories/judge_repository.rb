2c4928c81951f93e081905d44b7e84b00896b639
Alex Prokop <alex.prokop@gmail.com>
2018-07-27 14:33:12 -0400

    lint

--- app/repositories/judge_repository.rb
+++ app/repositories/judge_repository.rb
@@ -1,16 +1,14 @@
 class JudgeRepository
   def self.find_all_judges
-    records = judge_records
-    records.select(&:sdomainid).map do |record|
+    judge_records.select(&:sdomainid).map do |record|
       User.find_or_create_by(css_id: record.sdomainid, station_id: User::BOARD_STATION_ID)
     end
   end
 
   def self.find_all_judges_with_name_and_id
-    records = judge_records
     # Our user model only contains a full name field, but for certain applications
     # (like the IDT), we need the separate name fields from VACOLS.
-    records.map do |record|
+    judge_records.map do |record|
       { first_name: record.snamef,
         middle_name: record.snamemi,
         last_name: record.snamel,
