9c289e35062a04003b6af10aea52e70bb44b8fdb
Kara Louie <kara@truss.works>
2018-08-01 16:40:07 -0700

    Linting

--- client/app/intake/util/index.js
+++ client/app/intake/util/index.js
@@ -55,6 +55,7 @@ export const formatRelationships = (relationships) => {
 
 export const validateDate = (date) => {
   const datePattern = /^(0[1-9]|1[0-2])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\d\d$/;
+
   if (datePattern.test(date)) {
     return date;
   }
