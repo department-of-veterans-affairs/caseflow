54f4d0ac9b0352c2219c070e1628322dfb7682c7
Evan Kroske <evan@navahq.com>
2018-08-01 17:34:09 -0400

    Adds a column

--- client/app/queue/components/AmaTaskTable.jsx
+++ client/app/queue/components/AmaTaskTable.jsx
@@ -16,6 +16,7 @@ import { renderAppealType } from '../utils';
 import { DateString } from '../../util/DateUtil';
 import { CATEGORIES, redText } from '../constants';
 import COPY from '../../../COPY.json';
+import CO_LOCATED_ADMIN_ACTIONS from '../../../constants/CO_LOCATED_ADMIN_ACTIONS.json';
 
 import type {
   LegacyAppeals,
@@ -40,9 +41,15 @@ class AmaTaskTable extends React.PureComponent<Props> {
     };
   }
 
+  caseTaskColumn = () => ({
+    header: COPY.CASE_LIST_TABLE_TASKS_COLUMN_TITLE,
+    valueFunction: (task: AmaTask) => CO_LOCATED_ADMIN_ACTIONS[task.attributes.title]
+  })
+
   getQueueColumns = () : Array<{ header: string, span?: Function, valueFunction: Function, getSortValue?: Function }> =>
     _.compact([
-      this.caseDetailsColumn()
+      this.caseDetailsColumn(),
+      this.caseTaskColumn()
     ]);
 
   render = () => {
