5b259758bdde1a5e3d106cff84a00209e7abe799
Evan Kroske <evan@navahq.com>
2018-08-02 11:48:54 -0400

    Removes a bunch of unused code

--- client/app/queue/ColocatedTaskListView.jsx
+++ client/app/queue/ColocatedTaskListView.jsx
@@ -5,25 +5,18 @@ import { bindActionCreators } from 'redux';
 
 import AmaTaskTable from './components/AmaTaskTable';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
-import Alert from '../components/Alert';
 
 import {
   amaTasksByAssigneeId
 } from './selectors';
-import {
-  resetErrorMessages,
-  resetSuccessMessages,
-  resetSaveState
-} from './uiReducer/uiActions';
 import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
 
 import { fullWidth } from './constants';
 import type { AmaTask } from './types/models';
-import type { State, UiStateMessage } from './types/state';
+import type { State } from './types/state';
 
 type Params = {|
   userId: number,
-  error: ?UiStateMessage,
   success: string
 |};
 
@@ -31,32 +24,17 @@ type Props = Params & {|
   // From state
   amaTasks: Array<AmaTask>,
   // Action creators
-  clearCaseSelectSearch: typeof clearCaseSelectSearch,
-  resetErrorMessages: typeof resetErrorMessages,
-  resetSuccessMessages: typeof resetSuccessMessages,
-  resetSaveState: typeof resetSaveState,
+  clearCaseSelectSearch: typeof clearCaseSelectSearch
 |};
 
 class ColocatedTaskListView extends React.PureComponent<Props> {
-  componentWillUnmount = () => {
-    this.props.resetSaveState();
-    this.props.resetSuccessMessages();
-    this.props.resetErrorMessages();
-  }
-
   componentDidMount = () => {
     this.props.clearCaseSelectSearch();
-    this.props.resetErrorMessages();
   };
 
   render = () => {
-    const { error, success } = this.props;
     const tableContent = <div>
       <h1 {...fullWidth}></h1>
-      {error && <Alert type="error" title={error.title} scrollOnAlert={false}>
-        {error.detail}
-      </Alert>}
-      {success && <Alert type="success" title={success} scrollOnAlert={false} />}
       <AmaTaskTable tasks={this.props.amaTasks} />
     </div>;
 
@@ -66,39 +44,14 @@ class ColocatedTaskListView extends React.PureComponent<Props> {
   };
 }
 
-ColocatedTaskListView.propTypes = {
-};
-
 const mapStateToProps = (state: State, ownProps: Params) => {
-  const {
-    queue: {
-      stagedChanges: {
-        taskDecision
-      },
-      judges
-    },
-    ui: {
-      messages: {
-        error,
-        success
-      }
-    }
-  } = state;
-
   return {
-    amaTasks: amaTasksByAssigneeId(state)[ownProps.userId],
-    error,
-    success,
-    taskDecision,
-    judges
+    amaTasks: amaTasksByAssigneeId(state)[ownProps.userId]
   };
 };
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
-  clearCaseSelectSearch,
-  resetErrorMessages,
-  resetSuccessMessages,
-  resetSaveState
+  clearCaseSelectSearch
 }, dispatch);
 
 export default (connect(mapStateToProps, mapDispatchToProps)(ColocatedTaskListView): React.ComponentType<Params>);
