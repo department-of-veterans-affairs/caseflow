6b7d7047a84d44046fbdfd77be63f0c507fe3e9b
Evan Kroske <evan@navahq.com>
2018-08-06 16:50:50 -0400

    Makes veteran names human-readable

--- app/models/serializers/work_queue/task_serializer.rb
+++ app/models/serializers/work_queue/task_serializer.rb
@@ -25,7 +25,7 @@ class WorkQueue::TaskSerializer < ActiveModel::Serializer
   end
 
   attribute :veteran_name do
-    object.appeal.veteran_name
+    object.appeal.veteran ? object.appeal.veteran.name.formatted(:readable_full) : nil
   end
 
   attribute :veteran_file_number do
