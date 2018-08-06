442cce87a86c10dec3a099fc6a00f786f6b2e209
Sharon Warner <sharon.warner56@gmail.com>
2018-08-06 12:23:46 -0400

    Too many VLJ ids being sent to VACOLS (#6511)

--- app/models/hearings/judge_non_availability.rb
+++ app/models/hearings/judge_non_availability.rb
@@ -3,7 +3,7 @@ class JudgeNonAvailability < NonAvailability
     def import_judge_non_availability(schedule_period)
       dates = HearingSchedule::GetSpreadsheetData.new(schedule_period.spreadsheet).judge_non_availability_data
       judge_non_availability = []
-      judges = User.css_ids_by_vlj_ids(dates.pluck("vlj_id"))
+      judges = User.css_ids_by_vlj_ids(dates.pluck("vlj_id").uniq)
       transaction do
         dates.each do |date|
           css_id = judges[date["vlj_id"]][:css_id]
