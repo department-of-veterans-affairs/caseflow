83a609018cee620b8207a8be288f897e34de72c4
oscarAtNava <oscar@navapbc.com>
2018-08-07 14:06:04 -0400

    Issue 6447 - Testing Judge Algo

--- spec/factories/judge_schedule_period.rb
+++ spec/factories/judge_schedule_period.rb
@@ -12,9 +12,26 @@ FactoryBot.define do
       user { create(:user) }
     end
 
+    factory :single_nonavail_date_judge_schedule_period do
+      start_date { Date.parse("2018-07-30") }
+      end_date { Date.parse("2018-08-03") }
+      file_name { "singleNonAvailJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
+    factory :two_in_july_judge_schedule_period do
+      start_date { Date.parse("2018-07-30") }
+      end_date { Date.parse("2018-08-03") }
+      file_name { "twoInJulyJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
     before(:create) do
       S3Service.store_file("validJudgeSpreadsheet.xlsx", "spec/support/validJudgeSpreadsheet.xlsx", :filepath)
       S3Service.store_file("blankJudgeSpreadsheet.xlsx", "spec/support/blankJudgeSpreadsheet.xlsx", :filepath)
+      S3Service.store_file("singleNonAvailJudgeSpreadsheet.xlsx",
+                           "spec/support/singleNonAvailJudgeSpreadsheet.xlsx", :filepath)
+      S3Service.store_file("twoInJulyJudgeSpreadsheet.xlsx", "spec/support/twoInJulyJudgeSpreadsheet.xlsx", :filepath)
       create(:staff, sattyid: "860", snamef: "Stuart", snamel: "Huels")
       create(:staff, sattyid: "861", snamef: "Doris", snamel: "Lamphere")
     end
--- spec/models/judge_schedule_period_spec.rb
+++ spec/models/judge_schedule_period_spec.rb
@@ -1,5 +1,7 @@
 describe JudgeSchedulePeriod do
   let(:judge_schedule_period) { create(:judge_schedule_period) }
+  let(:single_nonavail_date_judge_schedule_period) { create(:single_nonavail_date_judge_schedule_period) }
+  let(:two_in_july_judge_schedule_period) { create(:two_in_july_judge_schedule_period) }
 
   context "validate_spreadsheet" do
     subject { judge_schedule_period.validate_spreadsheet }
@@ -24,6 +26,26 @@ describe JudgeSchedulePeriod do
       expect(subject[0].key?(:regional_office)).to be_truthy
       expect(subject[0].key?(:judge_id)).to be_truthy
       expect(subject[0].key?(:judge_name)).to be_truthy
+    end
+  end
+
+  context "Judges are not assigned hearings on their non-availability days", focus: true do
+    let!(:hearing_days) do
+      get_dates_between(single_nonavail_date_judge_schedule_period.start_date,
+                        single_nonavail_date_judge_schedule_period.end_date, 5).map do |date|
+        create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
+      end
+    end
+
+    it "verifies judge algo cannot assign judge to this one week period" do
+      expect {
+        single_nonavail_date_judge_schedule_period.algorithm_assignments
+      }.to raise_error(HearingSchedule::AssignJudgesToHearingDays::CannotAssignJudges)
+    end
+
+    subject { two_in_july_judge_schedule_period.algorithm_assignments }
+    it "evenly splits the week between two judges" do
+      expect(subject.count).to eq(hearing_days.count)
     end
   end
 end
