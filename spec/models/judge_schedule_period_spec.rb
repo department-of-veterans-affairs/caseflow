302793e241f9d2e91e471b6b9a27490744eaba7d
oscarAtNava <oscar@navapbc.com>
2018-08-07 16:41:06 -0400

    Issue 6447 - Testing Judge Algo

--- spec/models/judge_schedule_period_spec.rb
+++ spec/models/judge_schedule_period_spec.rb
@@ -16,7 +16,7 @@ describe JudgeSchedulePeriod do
 
   context "assign judges to hearing days" do
     let!(:hearing_days) do
-      get_dates_between(judge_schedule_period.start_date, judge_schedule_period.end_date, 3).map do |date|
+      get_unique_dates_between(judge_schedule_period.start_date, judge_schedule_period.end_date, 3).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13")
       end
     end
@@ -34,9 +34,9 @@ describe JudgeSchedulePeriod do
     end
   end
 
-  context "Judges are not assigned hearings on their non-availability days" do
+  context "Judges are not assigned hearings on their non-availability days", focus: true do
     let!(:hearing_days) do
-      get_dates_between(single_nonavail_date_judge_schedule_period.start_date,
+      get_unique_dates_between(single_nonavail_date_judge_schedule_period.start_date,
                         single_nonavail_date_judge_schedule_period.end_date, 5).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
       end
@@ -59,7 +59,7 @@ describe JudgeSchedulePeriod do
       create(:july_travel_board_schedule)
     end
     let!(:hearing_days) do
-      get_dates_between(one_month_judge_schedule_period.start_date,
+      get_unique_dates_between(one_month_judge_schedule_period.start_date,
                         one_month_judge_schedule_period.end_date, 20).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
       end
@@ -79,7 +79,7 @@ describe JudgeSchedulePeriod do
 
   context "A judge with a lot of non-availability days still gets as many hearings as possible" do
     let!(:hearing_days) do
-      get_dates_between(one_month_many_noavail_judge_schedule_period.start_date,
+      get_unique_dates_between(one_month_many_noavail_judge_schedule_period.start_date,
                         one_month_many_noavail_judge_schedule_period.end_date, 20).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
       end
@@ -93,7 +93,7 @@ describe JudgeSchedulePeriod do
 
   context "Judges cannot be assigned multiple hearing days on the same day" do
     let!(:hearing_days) do
-      get_dates_between(one_week_one_judge_schedule_period.start_date,
+      get_unique_dates_between(one_week_one_judge_schedule_period.start_date,
                         one_week_one_judge_schedule_period.end_date, 5).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
         create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO17", room: 5)
@@ -108,13 +108,14 @@ describe JudgeSchedulePeriod do
 
     subject { one_week_two_judge_schedule_period.algorithm_assignments }
     it "evenly splits the week between two judges" do
-      expect(subject.count).to eq(hearing_days.count)
+      # two rooms
+      expect(subject.count).to eq(hearing_days.count * 2)
     end
   end
 
   context "One judge is assigned to central office hearings each Wednesday" do
     let!(:hearing_days) do
-      get_dates_between(one_week_one_judge_schedule_period.start_date,
+      get_unique_dates_between(one_week_one_judge_schedule_period.start_date,
                         one_week_one_judge_schedule_period.end_date, 5).map do |date|
         create(:case_hearing, hearing_type: "C", hearing_date: date, room: 1)
       end
