87a461c1f5fc3bb7dacf940ae24595ce067f780f
Evan Kroske <evan@navahq.com>
2018-08-07 17:14:47 -0400

    Extracts new tab into its own component

--- client/app/queue/ColocatedTaskListView.jsx
+++ client/app/queue/ColocatedTaskListView.jsx
@@ -10,8 +10,7 @@ import {
   amaTasksByAssigneeId
 } from './selectors';
 import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
-
-import { fullWidth } from './constants';
+import TabWindow from '../components/TabWindow';
 import type { AmaTask } from './types/models';
 import type { State } from './types/state';
 
@@ -20,37 +19,42 @@ type Params = {|
 |};
 
 type Props = Params & {|
-  // From state
-  amaTasks: Array<AmaTask>,
   // Action creators
   clearCaseSelectSearch: typeof clearCaseSelectSearch
 |};
+
+const NewTasksTab = connect(
+  (state: State, ownProps) => ({ amaTasks: amaTasksByAssigneeId(state)[ownProps.userId] }))(
+  (props) => {
+    return <div>
+      <p>These are new administrative actions that have been assigned to you.</p>
+      <AmaTaskTable tasks={props.amaTasks} />
+    </div>;
+  });
 
 class ColocatedTaskListView extends React.PureComponent<Props> {
   componentDidMount = () => {
     this.props.clearCaseSelectSearch();
   };
 
+  pageNew = () => {
+    return <NewTasksTab userId={this.props.userId} />;
+  }
+
   render = () => {
-    const tableContent = <div>
-      <h1 {...fullWidth}></h1>
-      <AmaTaskTable tasks={this.props.amaTasks} />
-    </div>;
+    const tabs = [{
+      label: 'New',
+      page: this.pageNew()
+    }];
 
     return <AppSegment filledBackground>
-      {tableContent}
+      <TabWindow name="tasks-tabwindow" tabs={tabs} />
     </AppSegment>;
   };
 }
-
-const mapStateToProps = (state: State, ownProps: Params) => {
-  return {
-    amaTasks: amaTasksByAssigneeId(state)[ownProps.userId]
-  };
-};
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   clearCaseSelectSearch
 }, dispatch);
 
-export default (connect(mapStateToProps, mapDispatchToProps)(ColocatedTaskListView): React.ComponentType<Params>);
+export default (connect(null, mapDispatchToProps)(ColocatedTaskListView): React.ComponentType<Params>);
