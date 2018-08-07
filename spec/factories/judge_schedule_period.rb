31ec60661829a998369463f52f06800a0cdafa64
oscarAtNava <oscar@navapbc.com>
2018-08-07 15:21:57 -0400

    Issue 6447 - Testing Judge Algo

--- spec/factories/judge_schedule_period.rb
+++ spec/factories/judge_schedule_period.rb
@@ -26,12 +26,51 @@ FactoryBot.define do
       user { create(:user) }
     end
 
+    factory :one_month_judge_schedule_period do
+      start_date { Date.parse("2018-07-23") }
+      end_date { Date.parse("2018-08-17") }
+      file_name { "oneAllAvailJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
+    factory :one_month_two_judge_schedule_period do
+      start_date { Date.parse("2018-07-23") }
+      end_date { Date.parse("2018-08-17") }
+      file_name { "twoInJulyJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
+    factory :one_month_many_noavail_judge_schedule_period do
+      start_date { Date.parse("2018-07-23") }
+      end_date { Date.parse("2018-08-17") }
+      file_name { "manyNonAvailJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
+    factory :one_week_one_judge_schedule_period do
+      start_date { Date.parse("2018-07-30") }
+      end_date { Date.parse("2018-08-03") }
+      file_name { "oneAllAvailJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
+    factory :one_week_two_judge_schedule_period do
+      start_date { Date.parse("2018-07-30") }
+      end_date { Date.parse("2018-08-03") }
+      file_name { "twoAllAvailJudgeSpreadsheet.xlsx" }
+      user { create(:user) }
+    end
+
     before(:create) do
       S3Service.store_file("validJudgeSpreadsheet.xlsx", "spec/support/validJudgeSpreadsheet.xlsx", :filepath)
       S3Service.store_file("blankJudgeSpreadsheet.xlsx", "spec/support/blankJudgeSpreadsheet.xlsx", :filepath)
       S3Service.store_file("singleNonAvailJudgeSpreadsheet.xlsx",
                            "spec/support/singleNonAvailJudgeSpreadsheet.xlsx", :filepath)
       S3Service.store_file("twoInJulyJudgeSpreadsheet.xlsx", "spec/support/twoInJulyJudgeSpreadsheet.xlsx", :filepath)
+      S3Service.store_file("oneAllAvailJudgeSpreadsheet.xlsx", "spec/support/oneAllAvailJudgeSpreadsheet.xlsx", :filepath)
+      S3Service.store_file("manyNonAvailJudgeSpreadsheet.xlsx",
+                           "spec/support/manyNonAvailJudgeSpreadsheet.xlsx", :filepath)
+      S3Service.store_file("twoAllAvailJudgeSpreadsheet.xlsx", "spec/support/twoAllAvailJudgeSpreadsheet.xlsx", :filepath)
       create(:staff, sattyid: "860", snamef: "Stuart", snamel: "Huels")
       create(:staff, sattyid: "861", snamef: "Doris", snamel: "Lamphere")
     end
--- spec/models/judge_schedule_period_spec.rb
+++ spec/models/judge_schedule_period_spec.rb
@@ -2,6 +2,11 @@ describe JudgeSchedulePeriod do
   let(:judge_schedule_period) { create(:judge_schedule_period) }
   let(:single_nonavail_date_judge_schedule_period) { create(:single_nonavail_date_judge_schedule_period) }
   let(:two_in_july_judge_schedule_period) { create(:two_in_july_judge_schedule_period) }
+  let(:one_month_judge_schedule_period) { create(:one_month_judge_schedule_period) }
+  let(:one_month_two_judge_schedule_period) { create(:one_month_two_judge_schedule_period) }
+  let(:one_month_many_noavail_judge_schedule_period) { create(:one_month_many_noavail_judge_schedule_period) }
+  let(:one_week_one_judge_schedule_period) { create(:one_week_one_judge_schedule_period) }
+  let(:one_week_two_judge_schedule_period) { create(:one_week_two_judge_schedule_period) }
 
   context "validate_spreadsheet" do
     subject { judge_schedule_period.validate_spreadsheet }
@@ -29,7 +34,7 @@ describe JudgeSchedulePeriod do
     end
   end
 
-  context "Judges are not assigned hearings on their non-availability days", focus: true do
+  context "Judges are not assigned hearings on their non-availability days" do
     let!(:hearing_days) do
       get_dates_between(single_nonavail_date_judge_schedule_period.start_date,
                         single_nonavail_date_judge_schedule_period.end_date, 5).map do |date|
@@ -46,6 +51,78 @@ describe JudgeSchedulePeriod do
     subject { two_in_july_judge_schedule_period.algorithm_assignments }
     it "evenly splits the week between two judges" do
       expect(subject.count).to eq(hearing_days.count)
+    end
+  end
+
+  context " judges are not assigned hearings on their travel board days, the week before, or the week after" do
+    let!(:travel_board) do
+      create(:july_travel_board_schedule)
+    end
+    let!(:hearing_days) do
+      get_dates_between(one_month_judge_schedule_period.start_date,
+                        one_month_judge_schedule_period.end_date, 20).map do |date|
+        create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
+      end
+    end
+
+    it "verifies judge algo cannot assign judge to week prior or after assigned TB week" do
+      expect {
+        one_month_judge_schedule_period.algorithm_assignments
+      }.to raise_error(HearingSchedule::AssignJudgesToHearingDays::CannotAssignJudges)
+    end
+
+    subject { one_month_two_judge_schedule_period.algorithm_assignments }
+    it "evenly splits the week between two judges" do
+      expect(subject.count).to eq(hearing_days.count)
+    end
+  end
+
+  context "A judge with a lot of non-availability days still gets as many hearings as possible" do
+    let!(:hearing_days) do
+      get_dates_between(one_month_many_noavail_judge_schedule_period.start_date,
+                        one_month_many_noavail_judge_schedule_period.end_date, 20).map do |date|
+        create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
+      end
+    end
+
+    subject { one_month_many_noavail_judge_schedule_period.algorithm_assignments }
+    it "evenly splits the week between two judges" do
+      expect(subject.count).to eq(hearing_days.count)
+    end
+  end
+
+  context "Judges cannot be assigned multiple hearing days on the same day" do
+    let!(:hearing_days) do
+      get_dates_between(one_week_one_judge_schedule_period.start_date,
+                        one_week_one_judge_schedule_period.end_date, 5).map do |date|
+        create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO13", room: 4)
+        create(:case_hearing, hearing_type: "C", hearing_date: date, folder_nr: "VIDEO RO17", room: 5)
+      end
+    end
+
+    it "verifies judge algo cannot assign judge to multiple hearing days" do
+      expect {
+        one_week_one_judge_schedule_period.algorithm_assignments
+      }.to raise_error(HearingSchedule::AssignJudgesToHearingDays::CannotAssignJudges)
+    end
+
+    subject { one_week_two_judge_schedule_period.algorithm_assignments }
+    it "evenly splits the week between two judges" do
+      expect(subject.count).to eq(hearing_days.count)
+    end
+  end
+
+  context "One judge is assigned to central office hearings each Wednesday" do
+    let!(:hearing_days) do
+      get_dates_between(one_week_one_judge_schedule_period.start_date,
+                        one_week_one_judge_schedule_period.end_date, 5).map do |date|
+        create(:case_hearing, hearing_type: "C", hearing_date: date, room: 1)
+      end
+    end
+
+    subject { one_week_one_judge_schedule_period.algorithm_assignments }
+    it "evenly splits the week between two judges" do
+      expect(subject.count).to eq(1)
     end
   end
 end
