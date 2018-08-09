89928b53751a44ca3f835cb8df7e46528acea51b
lowellrex <lowell@navahq.com>
2018-08-09 14:37:33 -0400

    Add tooltip to new documents icon

--- client/app/queue/components/NewFile.jsx
+++ client/app/queue/components/NewFile.jsx
@@ -1,7 +1,10 @@
 // @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { css } from 'glamor';
+import ReactTooltip from 'react-tooltip';
 import { NewFileIcon } from '../../components/RenderFunctions';
+import { COLORS } from '../../constants/AppConstants';
 import { bindActionCreators } from 'redux';
 import { getNewDocuments } from '../QueueActions';
 import type {
@@ -19,6 +22,17 @@ type Props = Params & {|
   getNewDocuments: Function
 |};
 
+const tooltipID = 'newfile-tip';
+
+const tooltipStyling = css({
+  [`& > #${tooltipID}`]: {
+    backgroundColor: COLORS.GREY_DARK,
+    padding: '0.5rem 1rem',
+    textAlign: 'center'
+  },
+  [`& > #${tooltipID}:after`]: { display: 'none' }
+});
+
 class NewFile extends React.Component<Props> {
   componentDidMount = () => {
     if (!this.props.docs) {
@@ -28,7 +42,20 @@ class NewFile extends React.Component<Props> {
 
   render = () => {
     if (this.props.docs && this.props.docs.length > 0) {
-      return <NewFileIcon />;
+      return <React.Fragment>
+        <span data-tip data-for={tooltipID}>
+          <NewFileIcon />
+        </span>
+        <span {...tooltipStyling} >
+          <ReactTooltip
+            effect="solid"
+            id={tooltipID}
+            multiline
+            offset={{ top: '-10px' }} >
+            This case has new <br />documents
+          </ReactTooltip>
+        </span>
+      </React.Fragment>;
     }
 
     return null;
