699f7c6c6988abe4b83ef543b338036c59f12be0
Kara Louie <kara@truss.works>
2018-08-09 11:43:09 -0700

    Add description text from nonrating issues to create request issues

--- app/models/request_issue.rb
+++ app/models/request_issue.rb
@@ -6,7 +6,7 @@ class RequestIssue < ApplicationRecord
     create!(
       rating_issue_reference_id: data[:reference_id],
       rating_issue_profile_date: data[:profile_date],
-      description: data[:decision_text],
+      description: data[:decision_text] || data[:description],
       decision_date: data[:decision_date],
       issue_category: data[:issue_category]
     )
