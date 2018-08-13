43e1b08c6d655ec1317c95f1125c90dfd61130f4
Danny Sellers <dsellers90@gmail.com>
2018-08-13 14:15:52 -0400

    lint

--- app/models/vacols/case_assignment.rb
+++ app/models/vacols/case_assignment.rb
@@ -82,6 +82,7 @@ class VACOLS::CaseAssignment < VACOLS::Record
       select_tasks.where("brieff.bfkey = #{id}")
     end
 
+    # rubocop:disable Metrics/MethodLength
     def select_tasks
       select("brieff.bfkey as vacols_id",
              "brieff.bfcorlid as vbms_id",
@@ -116,6 +117,7 @@ class VACOLS::CaseAssignment < VACOLS::Record
             ON decass.demdusr = s3.slogid
         SQL
     end
+    # rubocop:enable Metrics/MethodLength
 
     def exists_for_appeals(vacols_ids)
       conn = connection
