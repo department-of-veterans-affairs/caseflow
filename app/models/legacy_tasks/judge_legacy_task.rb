9a230540098ae1d13c4f588e2ed81e0daf76233b
Anya Roltsch <aroltsch@tistatech.com>
2018-08-14 10:49:57 -0400

    Check for existence

--- app/models/legacy_tasks/judge_legacy_task.rb
+++ app/models/legacy_tasks/judge_legacy_task.rb
@@ -2,9 +2,11 @@ class JudgeLegacyTask < LegacyTask
   def self.from_vacols(record, appeal, user_id)
     task = super
     task.type = record.reassigned_to_judge_date.present? ? "Review" : "Assign"
-    task.assigned_at = record.assigned_to_location_date.to_date
-    # If task type is 'assign' that means there was no previous task record yet
-    task.previous_task = LegacyTask.new(assigned_at: record.assigned_to_attorney_date.to_date) if task.type == "Review"
+    task.assigned_at = record.assigned_to_location_date.try(:to_date)
+    if task.type == "Review"
+      # If task type is 'assign' that means there was no previous task record yet
+      task.previous_task = LegacyTask.new(assigned_at: record.assigned_to_attorney_date.try(:to_date))
+    end
     task.work_product = record.work_product
     task
   end
