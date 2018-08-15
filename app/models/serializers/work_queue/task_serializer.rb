a0124c9cb921567b9cb2663d52497bc21c7f1b1d
Danny Sellers <dsellers90@gmail.com>
2018-08-15 16:53:13 -0400

    add task assigned_by_display_name, use in TaskSerializer; add test for assignment back to attorney

--- app/models/serializers/work_queue/task_serializer.rb
+++ app/models/serializers/work_queue/task_serializer.rb
@@ -13,8 +13,8 @@ class WorkQueue::TaskSerializer < ActiveModel::Serializer
 
   attribute :assigned_by do
     {
-      first_name: object.assigned_by.full_name.split(" ").first,
-      last_name: object.assigned_by.full_name.split(" ").last,
+      first_name: object.assigned_by_display_name.first,
+      last_name: object.assigned_by_display_name.last,
       css_id: object.assigned_by.css_id,
       pg_id: object.assigned_by.id
     }
