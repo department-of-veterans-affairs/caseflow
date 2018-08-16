913d6661f633bea37f17eb23fc40e9079ab7930a
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-08-16 15:20:08 -0400

    Redirect VSO users to organizational queue (#6660)
    This PR makes it so requests to `/queue` from VSO users will redirect them to the URL of the organizational queue for the VSO they are associated with. This enables us to point all VSO users to the `/queue` endpoint in training so we don't have to remember the URLs for each VSO. Additionally, this prevents VSO users from accessing personalized task queues until (if?) those are enabled.

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -7,10 +7,25 @@ class QueueController < ApplicationController
   end
 
   def index
+    path = vso_organization_queue_path
+    redirect_to(path) && return if path
+
     render "queue/index"
   end
 
   def check_queue_out_of_service
     render "out_of_service", layout: "application" if Rails.cache.read("queue_out_of_service")
+  end
+
+  private
+
+  def vso_organization_queue_path
+    return unless current_user.vso_employee?
+
+    Vso.where.not(feature: nil).each do |vso|
+      return vso.path if FeatureToggle.enabled?(vso.feature.to_sym, user: current_user)
+    end
+
+    "/unauthorized"
   end
 end
