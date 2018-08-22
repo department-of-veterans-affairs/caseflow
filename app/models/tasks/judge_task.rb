fd1eacde802af11695a774da708f398bc6139145
Anya Roltsch <aroltsch@tistatech.com>
2018-08-22 12:35:41 -0400

    Differentiate between assign vs review tasks for judge tasks

--- app/models/tasks/judge_task.rb
+++ app/models/tasks/judge_task.rb
@@ -1,2 +1,11 @@
 class JudgeTask < Task
+  validates :action, inclusion: { in: %w[assign review] }
+
+  before_create :set_action
+
+  private
+
+  def set_action
+    self.action = :assign
+  end
 end
