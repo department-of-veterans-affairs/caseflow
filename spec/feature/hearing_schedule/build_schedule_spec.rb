ce25ad256d4fec5258d25d3daf8d876b18b9bbdc
Sharon Warner <sharon.warner56@gmail.com>
2018-08-22 16:14:54 -0400

    Feature tests for judge assignment`

--- spec/feature/hearing_schedule/build_schedule_spec.rb
+++ spec/feature/hearing_schedule/build_schedule_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "Build Hearing Schedule" do
       fill_in "startDate", with: "01012018"
       fill_in "endDate", with: "05312018"
       click_on "Continue"
-      page.has_content?("We have assigned your video hearings")
+      expect(page).to have_content("We have assigned your video hearings")
       expect(SchedulePeriod.count).to eq(1)
       expect(RoNonAvailability.count).to eq(227)
       expect(CoNonAvailability.count).to eq(4)
@@ -21,13 +21,47 @@ RSpec.feature "Build Hearing Schedule" do
       expect(allocation_count).to eq(358)
       click_on "Confirm assignments"
       click_on "Confirm upload"
-      expect(page).not_to have_content("We are uploading to VACOLS.")
+      expect(page).not_to have_content("We are uploading to VACOLS.", wait: 10)
       expect(page).to have_content("You have successfully assigned hearings between 01/01/2018 and 05/31/2018")
       hearing_day_count = HearingDay.load_days(Date.new(2018, 1, 1), Date.new(2018, 5, 31)).flatten
         .select do |hearing_day|
           hearing_day.try(:folder_nr) && hearing_day.folder_nr.include?("VIDEO")
         end.count
       expect(hearing_day_count).to eq(allocation_count)
+    end
+
+    context "Build Judge Hearing Schedule" do
+      before do
+        create(:staff, sattyid: "860", snamef: "Stuart", snamel: "Huels")
+        create(:staff, sattyid: "861", snamef: "Doris", snamel: "Lamphere")
+        create(:case_hearing, hearing_type: "C", hearing_date: Date.new(2018, 4, 2), folder_nr: "VIDEO RO13")
+        create(:case_hearing, hearing_type: "C", hearing_date: Date.new(2018, 4, 4), folder_nr: "VIDEO RO13")
+        create(:case_hearing, hearing_type: "C", hearing_date: Date.new(2018, 4, 6), folder_nr: "VIDEO RO13")
+        create(:case_hearing, hearing_type: "C", hearing_date: Date.new(2018, 4, 8), folder_nr: "VIDEO RO13")
+      end
+
+      let!(:current_user) do
+        User.authenticate!(roles: ["Build HearSched"])
+      end
+
+      scenario "Judge assignment process" do
+        visit "hearings/schedule/build"
+        click_on "Upload files"
+        find("label", text: "Judge non-availability").click
+        attach_file("judge_file_upload", Rails.root + "spec/support/validJudgeSpreadsheet.xlsx", visible: false)
+        fill_in "startDate", with: "04012018"
+        fill_in "endDate", with: "04302018"
+        click_on "Continue"
+        expect(page).to have_content("We have assigned your judges")
+        expect(SchedulePeriod.count).to eq(1)
+        expect(JudgeNonAvailability.count).to eq(3)
+        click_on "Confirm assignments"
+        click_on "Confirm upload"
+        expect(page).not_to have_content("We are uploading to VACOLS.", wait: 10)
+        expect(page).to have_content("You have successfully assigned judges to hearings")
+        vlj_ids = HearingDay.load_days(Date.new(2018, 4, 1), Date.new(2018, 4, 30)).flatten.map(&:board_member)
+        expect(vlj_ids.count).to eq(4)
+      end
     end
   end
 end
