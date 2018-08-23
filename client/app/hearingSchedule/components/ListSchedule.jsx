0dfeb30b4f8faafd76bf7cee47ff77f601272b91
Sjones352 <sjones@tistatech.com>
2018-08-23 17:21:21 -0400

    removed duplicates in list schedule

--- client/app/hearingSchedule/components/ListSchedule.jsx
+++ client/app/hearingSchedule/components/ListSchedule.jsx
@@ -52,8 +52,6 @@ export default class ListSchedule extends React.Component {
         valueName: 'vlj'
       }
     ];
-    
-  
 
     const hearingScheduleRows = _.map(hearingSchedule, (hearingDay) => ({
       hearingDate: formatDate(hearingDay.hearingDate),
@@ -63,7 +61,7 @@ export default class ListSchedule extends React.Component {
       vlj: hearingDay.judgeName
     }));
 
-    const removeDuplidatedDate = _.uniqBy(hearingScheduleRows, (row) => [row.hearingDate, row.roomInfo].join(';'));
+    const removeDuplidatedDate = _.uniqBy(hearingScheduleRows, (row) => [row.hearingDate, row.roomInfo].join());
 
     return <AppSegment filledBackground>
       <h1 className="cf-push-left">{COPY.HEARING_SCHEDULE_VIEW_PAGE_HEADER}</h1>
