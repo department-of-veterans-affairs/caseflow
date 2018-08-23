348d1340355caa81c2d12885fda8ca682038c660
lowellrex <lowell@navahq.com>
2018-08-23 16:23:20 -0400

    Add tests

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -1,37 +1,39 @@
 class GenericTask < Task
-  def create_from_params(params)
-    create_child_task(params)
-    update_status(params[:status])
-  end
-
   # Only request to PATCH /tasks/:id we expect for GenericTasks is to mark the task complete.
   def update_from_params(_params)
     mark_as_complete!
   end
 
-  private
+  class << self
+    def create_from_params(params)
+      parent = Task.find(params[:parent_id])
+      create_child_task(parent, params)
+      update_status(parent, params[:status])
+    end
 
-  def create_child_task(params)
-    # Create an assignee from the input arguments so we throw an error if the assignee does not exist.
-    assignee = Object.const_get(params[:assigned_to_type]).find(params[:assigned_to_id])
-    parent = Task.find(params[:parent_id])
+    private
 
-    GenericTask.create!(
-      appeal: parent.appeal,
-      assigned_by_id: parent.assigned_to_id,
-      parent_id: parent.id,
-      assigned_to: assignee
-    )
-  end
+    def create_child_task(parent, params)
+      # Create an assignee from the input arguments so we throw an error if the assignee does not exist.
+      assignee = Object.const_get(params[:assigned_to_type]).find(params[:assigned_to_id])
 
-  def update_status(status)
-    return unless status
+      GenericTask.create!(
+        appeal: parent.appeal,
+        assigned_by_id: parent.assigned_to_id,
+        parent_id: parent.id,
+        assigned_to: assignee
+      )
+    end
 
-    case status
-    when "completed"
-      mark_as_complete!
-    else
-      update!(status: status)
+    def update_status(parent, status)
+      return unless status
+
+      case status
+      when "completed"
+        parent.mark_as_complete!
+      else
+        parent.update!(status: status)
+      end
     end
   end
 end
