4a04ae1058a1c9f994d2e21427c5afcc2d11e33c
Alex Prokop <alex.prokop@gmail.com>
2018-08-27 11:06:14 -0400

    lint fixes

--- app/models/serializers/idt/v1/document_serializer.rb
+++ app/models/serializers/idt/v1/document_serializer.rb
@@ -3,7 +3,7 @@ class Idt::V1::DocumentSerializer < ActiveModel::Serializer
     object.vacols_id
   end
   attribute :written_by do
-  	[object.written_by_first_name, object.written_by_last_name].join(" ")
+    [object.written_by_first_name, object.written_by_last_name].join(" ")
   end
   attribute :document_id
 end
