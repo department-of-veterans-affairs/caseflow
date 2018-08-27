0c1fc6b0cd67eaaa287183dc6b013d2f18024cbd
Anya Roltsch <aroltsch@tistatech.com>
2018-08-27 13:41:48 -0400

    Rubocop

--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -95,7 +95,7 @@ class UserRepository
     def check_other_staff_fields(staff_record)
       return ["attorney"] if staff_record.sattyid
       return ["colocated"] if staff_record.stitle == "A1" || staff_record.stitle == "A2"
-      return ["dispatch"] if staff_record.sdept = "DSP"
+      return ["dispatch"] if staff_record.sdept == "DSP"
       []
     end
 
