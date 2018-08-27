ff93a2f8669560b219eb653eea4ac9685e59ee37
Alex Prokop <alex.prokop@gmail.com>
2018-08-27 08:24:02 -0400

    allows attorneys, judges, and colocated to use idt api

--- app/controllers/idt/api/v1/base_controller.rb
+++ app/controllers/idt/api/v1/base_controller.rb
@@ -8,7 +8,8 @@ class Idt::Api::V1::BaseController < ActionController::Base
   end
 
   def verify_access
-    return render json: { message: "User must be attorney" }, status: 403 unless user.attorney_in_vacols?
+    return true if user.attorney_in_vacols? || user.judge_in_vacols? || user.colocated_in_vacols?
+    return render json: { message: "User must be attorney, judge, or colocated" }, status: 403
   end
 
   def user
