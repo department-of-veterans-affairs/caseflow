3d3c86eabc76c9eef2c0d4dc6b5c78ace43712b2
Chris Given <cmgiven@gmail.com>
2018-08-27 13:04:04 -0400

    Fix for CAVC option being displayed on non-Board dispositions (#6743)
    * Fix for CAVC option being displayed on non-Board dispositions
    
    * Fix test

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -169,7 +169,7 @@ class LegacyAppeal < ApplicationRecord
   end
 
   def cavc_due_date
-    return unless decision_date
+    return unless decided_by_bva?
     (decision_date + 120.days).to_date
   end
 
