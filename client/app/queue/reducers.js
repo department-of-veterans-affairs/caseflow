71e63b2edcff87cf2c58bdef6ca1b53528ebc712
Alex Prokop <alex.prokop@gmail.com>
2018-08-28 12:21:25 -0400

    delete appeal

--- client/app/queue/reducers.js
+++ client/app/queue/reducers.js
@@ -85,7 +85,8 @@ const workQueueReducer = (state = initialState, action = {}): QueueState => {
   case ACTIONS.DELETE_APPEAL:
     return update(state, {
       appeals: { $unset: action.payload.appealId },
-      tasks: { $unset: action.payload.appealId }
+      tasks: { $unset: action.payload.appealId },
+      amaTasks: { $unset: action.payload.appealId }
     });
   case ACTIONS.EDIT_APPEAL:
     return update(state, {
