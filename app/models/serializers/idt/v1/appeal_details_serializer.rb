af38cad3b2a9a36b003c32f77ef5b7adb3f7c707
aroltsch <aroltsch@tistatech.com>
2018-09-04 14:36:41 -0400

    Implement IDT AMA details - version 1

--- app/models/serializers/idt/v1/appeal_details_serializer.rb
+++ app/models/serializers/idt/v1/appeal_details_serializer.rb
@@ -1,6 +1,6 @@
 class Idt::V1::AppealDetailsSerializer < ActiveModel::Serializer
   def id
-    object.vacols_id
+    object.is_a?(LegacyAppeal) ? object.vacols_id : object.uuid
   end
 
   attribute :veteran_first_name
@@ -9,50 +9,51 @@ class Idt::V1::AppealDetailsSerializer < ActiveModel::Serializer
   end
   attribute :veteran_last_name
   attribute :veteran_gender
-  attribute :veteran_is_deceased do
-    !!object.notice_of_death_date
-  end
+  attribute :veteran_is_deceased
 
-  attribute :appellant_is_not_veteran do
-    !!object.appellant_first_name
-  end
+  attribute :appellant_is_not_veteran
   attribute :appellant_first_name
   attribute :appellant_middle_name do
     object.appellant_middle_initial
   end
   attribute :appellant_last_name
   attribute :appellant_name_suffix
+
   attribute :file_number do
-    object.sanitized_vbms_id
+    object.is_a?(LegacyAppeal) ? object.sanitized_vbms_id : object.veteran_file_number
   end
   attribute :citation_number
   attribute :docket_number
-  attribute :number_of_issues do
-    object.issues.length
-  end
+  attribute :number_of_issues
+
   attribute :issues do
-    object.issues.map do |issue|
-      ActiveModelSerializers::SerializableResource.new(
-        issue,
-        serializer: ::WorkQueue::IssueSerializer
-      ).as_json[:data][:attributes]
+    if object.is_a?(LegacyAppeal)
+      object.issues.map do |issue|
+        ActiveModelSerializers::SerializableResource.new(
+          issue,
+          serializer: ::WorkQueue::IssueSerializer
+        ).as_json[:data][:attributes]
+      end
+    else
+      # TODO: implement AMA logic
+      []
     end
   end
 
   # TODO: - expand rep name into separate fields
   attribute :representative_name do
-    object.power_of_attorney.vacols_representative_name
+    object.is_a?(LegacyAppeal) ? object.power_of_attorney.vacols_representative_name : "not implemented"
   end
   attribute :representative_type do
-    object.power_of_attorney.vacols_representative_type
+    object.is_a?(LegacyAppeal) ? object.power_of_attorney.vacols_representative_type : "not implemented"
   end
 
-  attribute :aod
+  attribute :advanced_on_docket
   attribute :cavc
   attribute :status
   attribute :previously_selected_for_quality_review
 
   attribute :outstanding_mail do
-    object.outstanding_vacols_mail?
+    object.is_a?(LegacyAppeal) ? object.outstanding_vacols_mail? : "not implemented"
   end
 end
