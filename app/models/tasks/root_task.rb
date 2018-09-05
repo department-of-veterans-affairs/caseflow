4edc8198b7003a4c0c255d20838b1d59c4d82a65
Mark Benjamin <mdbenjam@gmail.com>
2018-09-05 11:43:33 -0400

    Lint errors

--- app/models/tasks/root_task.rb
+++ app/models/tasks/root_task.rb
@@ -1,10 +1,9 @@
 class RootTask < Task
-  def when_child_task_completed
-  end
+  def when_child_task_completed; end
 
   class << self
     def create_root_and_sub_tasks!(appeal)
-      root_task = create!({ appeal_id: appeal.id, appeal_type: appeal.class.name, assigned_to: Bva.singleton })
+      root_task = create!(appeal_id: appeal.id, appeal_type: appeal.class.name, assigned_to: Bva.singleton)
       create_vso_subtask!(appeal, root_task)
     end
 
