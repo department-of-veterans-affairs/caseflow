55cd834e68e7e73101763f40f6265a0b8ae47f89
aroltsch <aroltsch@tistatech.com>
2018-09-06 11:32:24 -0400

    Rubocop

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -338,8 +338,8 @@ class Fakes::BGSService
     )
   end
 
-  def fetch_person_info(participant_id)
-    { first_name: ["Tom", "Patricia"].sample, last_name: ["Brady", "Charles"].sample, middle_name: ["Edward", "Aaron"].sample }
+  def fetch_person_info(_participant_id)
+    { first_name: %w[Tom Patricia].sample, last_name: %w[Brady Charles].sample, middle_name: %w[Edward Aaron].sample }
   end
 
   # TODO: add more test cases
