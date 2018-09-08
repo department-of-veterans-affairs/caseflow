b2cb7c6a83d26a9a6e9667a2800ee67d9fe91da5
Sally Maki <sallymaki@gmail.com>
2018-09-07 17:17:41 -0700

    Remove end_product_status from higher_level_review in spec

--- spec/models/claim_review_spec.rb
+++ spec/models/claim_review_spec.rb
@@ -13,7 +13,6 @@ describe ClaimReview do
   let(:receipt_date) { SupplementalClaim::AMA_BEGIN_DATE + 1 }
   let(:informal_conference) { nil }
   let(:same_office) { nil }
-  let(:end_product_status) { nil }
 
   let(:rating_request_issue) do
     RequestIssue.new(
@@ -47,8 +46,7 @@ describe ClaimReview do
       veteran_file_number: veteran_file_number,
       receipt_date: receipt_date,
       informal_conference: informal_conference,
-      same_office: same_office,
-      end_product_status: end_product_status
+      same_office: same_office
     )
   end
 
