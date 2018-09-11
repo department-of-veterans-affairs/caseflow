bc09c6f0ec64c65c156e6a01928d28199a5eca37
Danny Sellers <dsellers90@gmail.com>
2018-09-11 14:06:15 -0400

    always set task to in_progress; update store w/response

--- client/app/queue/CaseDetailsLink.jsx
+++ client/app/queue/CaseDetailsLink.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { css } from 'glamor';
 
+import { setTaskAttrs } from './QueueActions';
 import ApiUtil from '../util/ApiUtil';
 import COPY from '../../COPY.json';
 import USER_ROLE_TYPES from '../../constants/USER_ROLE_TYPES.json';
 import { subHeadTextStyle } from './constants';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+import { prepareTasksForStore } from './utils';
 
 class CaseDetailsLink extends React.PureComponent {
   onClick = () => {
-    const {
-      task,
-      appeal
-    } = this.props;
-
-    if (appeal.docketName !== 'legacy') {
-      const payload = {
-        data: {
-          task: {
-            status: 'in_progress'
-          }
+    const { task } = this.props;
+    const payload = {
+      data: {
+        task: {
+          status: 'in_progress'
         }
-      };
+      }
+    };
 
-      ApiUtil.patch(`/tasks/${task.taskId}`, payload);
-    }
+    ApiUtil.patch(`/tasks/${task.taskId}`, payload).
+      then((resp) => {
+        const response = JSON.parse(resp.text);
+        const preparedTasks = prepareTasksForStore(response.tasks.data);
+
+        this.props.setTaskAttrs(task.externalAppealId, preparedTasks[task.externalAppealId]);
+      });
 
     return this.props.onClick ? this.props.onClick(arguments) : true;
   }
@@ -84,4 +87,8 @@ const mapStateToProps = (state) => ({
   userRole: state.ui.userRole
 });
 
-export default connect(mapStateToProps)(CaseDetailsLink);
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  setTaskAttrs
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CaseDetailsLink);
