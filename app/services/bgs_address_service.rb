2a37fab70bac99e22d2973ab9c67f240c3fe1d88
Mark Benjamin <mdbenjam@gmail.com>
2018-09-13 16:41:36 -0400

    Adding error handling in the case of no address

--- app/services/bgs_address_service.rb
+++ app/services/bgs_address_service.rb
@@ -7,7 +7,13 @@ class BgsAddressService
   bgs_attr_accessor :address_line_1, :address_line_2, :city, :country, :state, :zip
 
   def fetch_bgs_record
-    bgs.find_address_by_participant_id(participant_id)
+    begin
+      bgs.find_address_by_participant_id(participant_id)
+    rescue Savon::Error => e
+      # If there is no addresses for this participant id then we get an error.
+      # catch it and return an empty array
+      {}
+    end
   end
 
   def bgs
