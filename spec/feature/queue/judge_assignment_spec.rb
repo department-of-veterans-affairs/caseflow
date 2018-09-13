f89ac8b4e2d1e9229bc06a6853c155f8cb22782d
Alex Prokop <alex.prokop@gmail.com>
2018-09-13 13:10:39 -0400

    lint fix

--- spec/feature/queue/judge_assignment_spec.rb
+++ spec/feature/queue/judge_assignment_spec.rb
@@ -1,7 +1,5 @@
 require "rails_helper"
 
-require "rails_helper"
-
 def click_dropdown(opt_idx, container = page)
   dropdown = container.find(".Select-control")
   dropdown.click
@@ -10,20 +8,20 @@ def click_dropdown(opt_idx, container = page)
 end
 
 RSpec.feature "Judge assignment to attorney" do
-	# Note: these tests rely on the mapping in attorney_judge_teams.rb, the CSS IDs come from there.
+  # Note: these tests rely on the mapping in attorney_judge_teams.rb, the CSS IDs come from there.
 
-	let(:judge) { create(:user, css_id: "BVABDANIEL", full_name: "Billie Daniel") }
+  let(:judge) { create(:user, css_id: "BVABDANIEL", full_name: "Billie Daniel") }
   let(:attorney_one) { create(:user, css_id: "BVAMZEMLAK", full_name: "Moe Syzlak") }
-  let(:attorney_two) { create(:user, css_id: "BVAAMACGYVER2", full_name: "Alice Macgyvertwo") }	  
+  let(:attorney_two) { create(:user, css_id: "BVAAMACGYVER2", full_name: "Alice Macgyvertwo") }
   let(:appeal_one) { FactoryBot.create(:appeal) }
   let(:appeal_two) { FactoryBot.create(:appeal) }
 
   before do
-	  create(:staff, :judge_role, slogid: "TEST0", sdomainid: judge.css_id)
-	  create(:staff, :attorney_role, slogid: "TEST1", sdomainid: attorney_one.css_id)
-	  create(:staff, :attorney_role, slogid: "TEST2", sdomainid: attorney_two.css_id)
-		create(:ama_judge_task, :in_progress, assigned_to: judge, appeal: appeal_one)
-		create(:ama_judge_task, :in_progress, assigned_to: judge, appeal: appeal_two)
+    create(:staff, :judge_role, slogid: "TEST0", sdomainid: judge.css_id)
+    create(:staff, :attorney_role, slogid: "TEST1", sdomainid: attorney_one.css_id)
+    create(:staff, :attorney_role, slogid: "TEST2", sdomainid: attorney_two.css_id)
+    create(:ama_judge_task, :in_progress, assigned_to: judge, appeal: appeal_one)
+    create(:ama_judge_task, :in_progress, assigned_to: judge, appeal: appeal_two)
     FeatureToggle.enable!(:test_facols)
     FeatureToggle.enable!(:judge_assignment_to_attorney)
     User.authenticate!(user: judge)
@@ -36,24 +34,24 @@ RSpec.feature "Judge assignment to attorney" do
 
   context "Can move appeals between attorneys" do
     scenario "submits draft decision" do
-    	visit "/queue"
-    	click_on "Switch to Assign Cases"
+      visit "/queue"
+      click_on "Switch to Assign Cases"
 
-    	expect(page).to have_content("Cases to Assign (2)")
+      expect(page).to have_content("Cases to Assign (2)")
 
-    	expect(page).to have_content("Assign 0 cases")
+      expect(page).to have_content("Assign 0 cases")
 
-    	case_rows = page.find_all("tr[id^='table-row-']")
-    	expect(case_rows.length).to eq(2)
+      case_rows = page.find_all("tr[id^='table-row-']")
+      expect(case_rows.length).to eq(2)
 
-    	case_rows.each do |row|
-    		# TODO: how do we check these checkboxes?
+      case_rows.each do |row|
+        # TODO: how do we check these checkboxes?
 
-    		# Approaches I tried:
-    		# row.find(".cf-form-checkbox").click -> does not seem to change the checkbox state.
+        # Approaches I tried:
+        # row.find(".cf-form-checkbox").click -> does not seem to change the checkbox state.
         # row.find("input", visible: false).click -> fails since you're not allowed to click hidden elements
         # page.find("label[for=\"#{appeal_one.uuid}\"]").click -> fails with message:
-        # Element is not clickable. Other element would receive the click: 
+        # Element is not clickable. Other element would receive the click:
         # <div class="cf-form-checkbox">...</div>
       end
     end
