10e7ac149e5eb76d9847a2b734e4b070e6913c39
Alex Prokop <alex.prokop@gmail.com>
2018-09-21 06:57:23 -0700

    comments

--- app/models/tasks/judge_task.rb
+++ app/models/tasks/judge_task.rb
@@ -18,13 +18,11 @@ class JudgeTask < Task
     children_attorney_tasks[0]
   end
 
-
-  # This function to be run in production when we need to ass
+  # This function to be manually run in production when we need to assign judge tasks.
   def self.assign_judge_tasks_for_unassigned_ramp_appeals(dry_run: false)
     # Find all root tasks with no children, that means they are not assigned.
     root_tasks_needing_assignment = RootTask.left_outer_joins(:children).all.select { |t| t.children.empty? }
     root_tasks_needing_assignment.each do |root_task|
-
       if (dry_run)
         puts "Dry run. Would assign judge task for #{root_task.appeal.id} to #{next_assignee.css_id}"
       else
@@ -41,6 +39,7 @@ class JudgeTask < Task
   end
 
   # 2 judges for now just to test
+  # TODO: add production list.
   def self.list_of_assignees
     %w[BVAAABSHIRE BVAOFRANECKI]
   end
