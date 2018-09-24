9aa398d6dcf50e97c319096aba530ac0ca088d53
Mark Benjamin <mdbenjam@gmail.com>
2018-09-24 17:20:56 -0400

    Adding case timeline

--- client/app/queue/CaseTimeline.jsx
+++ client/app/queue/CaseTimeline.jsx
@@ -7,19 +7,38 @@ import BareList from '../components/BareList';
 import { boldText } from './constants';
 import { DateString } from '../util/DateUtil';
 import Address from './components/Address';
+import { GrayDot, GreenCheckmark } from '../components/RenderFunctions';
+import moment from 'moment';
+import { COLORS } from '@department-of-veterans-affairs/caseflow-frontend-toolkit/util/StyleConstants';
 
-const detailListStyling = css({
-  paddingLeft: 0,
-  listStyle: 'none',
-  marginBottom: '3rem'
+const grayLine = css({
+  width: '5px',
+  minHeight: '50px',
+  background: COLORS.GREY_LIGHT,
+  margin: 'auto'
+});
+
+const tableCellWithIcon = css({
+  textAlign: 'center',
+  border: 'none',
+  padding: 0
+});
+
+const tableCell = css({
+  border: 'none',
+  verticalAlign: 'top',
+  padding: '3px'
 });
 
 export default class CaseTimeline extends React.PureComponent {
-  getEventRow = ({ title, date }) => {
+  getEventRow = ({ title, date }, lastRow) => {
+    const formattedDate = date ? moment(date).format('MM/DD/YYYY') : null;
+    const eventImage = date ? <GreenCheckmark /> : <GrayDot />;
+
     return <tr key={title}>
-      <td>{date}</td>
-      <td>0</td>
-      <td>{title}</td>
+      <td {...tableCell}>{formattedDate}</td>
+      <td {...tableCellWithIcon}>{eventImage}{!lastRow && <div {...grayLine} />}</td>
+      <td {...tableCell}>{title}</td>
     </tr>;
   }
 
@@ -28,17 +47,17 @@ export default class CaseTimeline extends React.PureComponent {
 
     const events = [
       {
-        title: "Notice of disagreement received",
-        date: appeal.nodReceiptDate
-      },
-      {
         title: "BVA Decision pending",
         date: appeal.decisionDate
+      },
+      {
+        title: "Notice of disagreement received",
+        date: appeal.nodReceiptDate
       }
     ];
     return <table>
-      {events.map((event) => {
-        return this.getEventRow(event);
+      {events.map((event, index) => {
+        return this.getEventRow(event, index === events.length - 1);
       })}
     </table>
   };
