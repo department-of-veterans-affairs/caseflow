4c8f314bb6c7cb9530157f013b8f380817265bb4
Alex Prokop <alex.prokop@gmail.com>
2018-09-24 08:13:17 -0700

    fix

--- app/models/tasks/judge_task.rb
+++ app/models/tasks/judge_task.rb
@@ -20,8 +20,34 @@ class JudgeTask < Task
 
   #:nocov:
   # This function to be manually run in production when we need to assign judge tasks.
-  def self.assign_judge_tasks_for_unassigned_ramp_appeals(dry_run: false)
+  def self.assign_ramp_judge_tasks(dry_run: false)
     # Find all root tasks with no children, that means they are not assigned.
+    root_tasks_needing_assignment = RootTask.left_outer_joins(:children).all.select { |t| t.children.empty? }
+    root_tasks_needing_assignment.each do |root_task|
+      if dry_run
+        Rails.logger.info("Dry run. Would assign judge task for #{root_task.appeal.id} to #{next_assignee.css_id}")
+      else
+        task = create!(appeal: root_task.appeal,
+                       parent: root_task,
+                       action: "assign",
+                       appeal_type: "Appeal",
+                       assigned_at: Time.zone.now,
+                       assigned_to: next_assignee)
+
+        Rails.logger.info("Assigned judge task with id: #{task.id} to #{task.assigned_to}")
+      end
+    end
+  end
+
+  def self.assign_ramp_judge_tasks_for_vso_appeals(dry_run: false)
+    # Find all root tasks with no children, that means they are not assigned.
+
+    tasks = GenericTask.
+      .where(assigned_to_type: "Vso")
+      .where("completed_at IS NULL")
+      .where("parent_id IS NOT NULL")
+      .where("assigned at < ?", 30.days.ago)
+
     root_tasks_needing_assignment = RootTask.left_outer_joins(:children).all.select { |t| t.children.empty? }
     root_tasks_needing_assignment.each do |root_task|
       if dry_run
