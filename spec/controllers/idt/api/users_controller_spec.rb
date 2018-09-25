a70eb7253b487488310676b1545c7dab14b353e6
aroltsch <aroltsch@tistatech.com>
2018-09-25 15:33:17 -0400

    Trying to fix test

--- spec/controllers/idt/api/users_controller_spec.rb
+++ spec/controllers/idt/api/users_controller_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe Idt::Api::V1::UsersController, type: :controller do
           get :index
           expect(response.status).to eq 200
           response_body = JSON.parse(response.body)["data"]
+          expect(response_body["css_id"]).to eq "TEST_ID"
           expect(response_body["roles"]).to eq ["attorney"]
         end
       end
@@ -37,6 +38,7 @@ RSpec.describe Idt::Api::V1::UsersController, type: :controller do
           get :index
           expect(response.status).to eq 200
           response_body = JSON.parse(response.body)["data"]
+          expect(response_body["css_id"]).to eq "TEST_ID"
           expect(response_body["roles"]).to eq ["dispatch"]
         end
       end
