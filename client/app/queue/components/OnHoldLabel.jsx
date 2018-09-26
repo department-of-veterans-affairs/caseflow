d922c450134634051bad4faa6512d7f0caa708ab
lowellrex <lowell@navahq.com>
2018-09-26 16:20:42 -0400

    Fix start of day issue in date comparison

--- client/app/queue/components/OnHoldLabel.jsx
+++ client/app/queue/components/OnHoldLabel.jsx
@@ -2,7 +2,8 @@
 import moment from 'moment';
 import type { Task } from '../types/models';
 
-export const numDaysOnHold = (task: Task) => moment().diff(task.placedOnHoldAt, 'days');
+export const numDaysOnHold = (task: Task) => moment().startOf('day').
+  diff(task.placedOnHoldAt, 'days');
 
 type Props = { task: Task };
 const OnHoldLabel = (props: Props) => `${numDaysOnHold(props.task)} of ${props.task.onHoldDuration || '?'}`;
