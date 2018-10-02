a1806a16dd6d984bda5af82d57ec2374f08d0347
Jia Huang <jialiya.huang0@gmail.com>
2018-10-02 09:40:06 -0700

    Hook up add issues button (#7205)
    Connects #6982
    
    ![add_issues_click](https://user-images.githubusercontent.com/577629/46317952-be60b380-c589-11e8-93a8-196c96c1210f.gif)

--- spec/feature/intake/appeal_spec.rb
+++ spec/feature/intake/appeal_spec.rb
@@ -290,5 +290,11 @@ RSpec.feature "Appeal Intake" do
     safe_click "#button-add-issue"
     expect(page).to have_content("Left knee granted")
     expect(page).to have_content("PTSD denied")
+
+    # adding an issue should show the issue
+    find("label", text: "Left knee granted").click
+    safe_click ".add-issue"
+
+    expect(page).to have_content("1. Left knee granted")
   end
 end
--- spec/feature/intake/higher_level_review_spec.rb
+++ spec/feature/intake/higher_level_review_spec.rb
@@ -544,6 +544,12 @@ RSpec.feature "Higher-Level Review" do
       safe_click "#button-add-issue"
       expect(page).to have_content("Left knee granted")
       expect(page).to have_content("PTSD denied")
+
+      # adding an issue should show the issue
+      find("label", text: "Left knee granted").click
+      safe_click ".add-issue"
+
+      expect(page).to have_content("1. Left knee granted")
     end
 
     scenario "HLR non-comp" do
