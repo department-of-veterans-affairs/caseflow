e33fe7cbafd97a902283c49a56200861cd1e58c0
lowellrex <lowell@navahq.com>
2018-10-02 18:07:23 -0400

    Fix lint nit

--- app/controllers/idt/api/v1/appeals_controller.rb
+++ app/controllers/idt/api/v1/appeals_controller.rb
@@ -2,7 +2,7 @@ class Idt::Api::V1::AppealsController < Idt::Api::V1::BaseController
   protect_from_forgery with: :exception
   before_action :verify_access
 
-  skip_before_filter :verify_authenticity_token, :only => [:outcode]
+  skip_before_action :verify_authenticity_token, only: [:outcode]
 
   rescue_from StandardError do |e|
     Raven.capture_exception(e)
