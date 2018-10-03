c5e9f49b27e93e52bc4d8e929183f1c2bd4872a1
Alex Prokop <alex.prokop@gmail.com>
2018-10-03 09:54:11 -0400

    change outcode response

--- app/controllers/idt/api/v1/appeals_controller.rb
+++ app/controllers/idt/api/v1/appeals_controller.rb
@@ -13,7 +13,7 @@ class Idt::Api::V1::AppealsController < Idt::Api::V1::BaseController
     end
   end
 
-  rescue_from ActionController::ParameterMissing do |e|
+  rescue_from ActiveRecord::RecordInvalid, ActionController::ParameterMissing do |e|
     render(json: { message: e.message }, status: 400)
   end
 
@@ -32,7 +32,7 @@ class Idt::Api::V1::AppealsController < Idt::Api::V1::BaseController
 
   def outcode
     BvaDispatchTask.outcode(appeal, outcode_params, user)
-    render json: json_appeal_details
+    render json: { message: "Success!"}
   end
 
   private
