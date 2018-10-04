576529f5d184747b6ce531fc72fc09a3e94b2afd
lowellrex <lowell@navahq.com>
2018-10-04 18:28:01 -0400

    Add URL to QR organization row

--- app/models/organizations/quality_review.rb
+++ app/models/organizations/quality_review.rb
@@ -1,5 +1,5 @@
 class QualityReview < Organization
   def self.singleton
-    QualityReview.first || QualityReview.create(name: "Quality Review")
+    QualityReview.first || QualityReview.create(name: "Quality Review", url: "quality-review")
   end
 end
