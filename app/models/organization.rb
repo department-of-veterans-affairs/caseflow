c33acf6a9a06682c135b4ea229ddab30e5940a1f
Mark Benjamin <mdbenjam@gmail.com>
2018-10-04 13:50:25 -0400

    Adding a comment

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,6 +5,9 @@ class Organization < ApplicationRecord
   def self.assignable(task)
     organizations = where(type: [nil, BvaDispatch.name])
 
+    # Exclude the current organization from the list of assignable organizations if the
+    # task is assigned to this organization or the task is a child of a task assigned to
+    # this organization. Prevents assignment loops.
     if task.assigned_to_type == name
       organizations.where.not(id: task.assigned_to_id)
     elsif task.assigned_to_type == User.name && task.parent && task.parent.assigned_to_type == name
