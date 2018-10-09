9d0363be5b4cbe87df19981ce47bfdc8c3a85778
Sharon Warner <sharon.warner56@gmail.com>
2018-10-09 13:18:14 -0400

    Daily docket UI (#7250)

--- config/routes.rb
+++ config/routes.rb
@@ -120,6 +120,7 @@ Rails.application.routes.draw do
     resources :hearing_day, only: [:update, :show], param: :hearing_key
   end
   get 'hearings/schedule', to: "hearings/hearing_day#index"
+  get 'hearings/schedule/docket/:ro_name/:date', to: "hearings/hearing_day#index"
   get 'hearings/schedule/build', to: "hearing_schedule#build_schedule_index"
   get 'hearings/schedule/build/upload', to: "hearing_schedule#build_schedule_index"
   get 'hearings/schedule/build/upload/:schedule_period_id', to: "hearing_schedule#build_schedule_index"
