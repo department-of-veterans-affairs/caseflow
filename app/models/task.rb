1ba860ae623df1ca3b51b4bef6204038ac4dd6c7
aroltsch <aroltsch@tistatech.com>
2018-10-10 11:00:59 -0400

    Any judge should have permissions to modify task info

--- app/models/task.rb
+++ app/models/task.rb
@@ -39,7 +39,7 @@ class Task < ApplicationRecord
   end
 
   def self.create_from_params(params, current_user)
-    verify_user_can_assign(current_user)
+    verify_user_can_assign!(current_user)
     params = params.each { |p| p["instructions"] = [p["instructions"]] if p.key?("instructions") }
     create(params)
   end
@@ -88,18 +88,22 @@ class Task < ApplicationRecord
     update_status_if_children_tasks_are_complete
   end
 
-  def can_user_access?(user)
-    return true if assigned_to == user || assigned_by == user
+  def can_be_accessed_by_user?(user)
+    if assigned_to == user || 
+      (parent && parent.assigned_to == user) || 
+      Constants::AttorneyJudgeTeams::JUDGES[Rails.current_env].keys.include?(user.css_id)
+      return true 
+    end
     false
   end
 
-  def verify_user_access(user)
-    unless can_user_access?(user)
+  def verify_user_access!(user)
+    unless can_be_accessed_by_user?(user)
       fail Caseflow::Error::ActionForbiddenError, message: "Current user cannot access this task"
     end
   end
 
-  def self.verify_user_can_assign(user)
+  def self.verify_user_can_assign!(user)
     unless user.attorney_in_vacols? ||
            (user.judge_in_vacols? && FeatureToggle.enabled?(:judge_assignment_to_attorney, user: user))
       fail Caseflow::Error::ActionForbiddenError, message: "Current user cannot assign this task"
