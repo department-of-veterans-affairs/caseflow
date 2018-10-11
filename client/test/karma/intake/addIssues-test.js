35af6cf7f299a75f31c25836581353008c290e74
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-10-11 00:49:58 -0700

    remove .only

--- client/test/karma/intake/addIssues-test.js
+++ client/test/karma/intake/addIssues-test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import { AddIssues } from '../../../app/intake/pages/AddIssues';
 import { Redirect } from 'react-router-dom';
 
-describe.only('AddIssues', () => {
+describe('AddIssues', () => {
   it('renders Redirect when formType is undefined', () => {
     const wrapper = shallow(<AddIssues formType={undefined}/>);
     expect(wrapper.find(Redirect)).to.have.lengthOf(1);
