13e1a03755555d4f2616731c9bba5724fec716a7
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-10-11 10:22:11 -0700

    expect true violates eslint

--- client/test/karma/intake/addIssues-test.js
+++ client/test/karma/intake/addIssues-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { expect } from 'chai';
+import { assert } from 'chai';
 import { shallow } from 'enzyme';
 import { AddIssues } from '../../../app/intake/pages/addIssues';
 import { Redirect } from 'react-router-dom';
@@ -8,6 +8,7 @@ import { PAGE_PATHS } from '../../../app/intake/constants';
 describe('AddIssues', () => {
   it('redirects to intake start when formType is undefined, like after canceling an intake', () => {
     const wrapper = shallow(<AddIssues />);
-    expect(wrapper.contains(<Redirect to={PAGE_PATHS.BEGIN} />)).to.be.true;
+
+    assert.isTrue(wrapper.contains(<Redirect to={PAGE_PATHS.BEGIN} />));
   });
 });
