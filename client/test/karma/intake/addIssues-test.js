fc7ceccf0425559b1e319aac538c462132c8eeeb
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-10-11 01:00:31 -0700

    fix lint

--- client/test/karma/intake/addIssues-test.js
+++ client/test/karma/intake/addIssues-test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { AddIssues } from '../../../app/intake/pages/AddIssues';
+import { AddIssues } from '../../../app/intake/pages/addIssues';
 import { Redirect } from 'react-router-dom';
 
 describe('AddIssues', () => {
   it('renders Redirect when formType is undefined', () => {
-    const wrapper = shallow(<AddIssues formType={undefined}/>);
+    const wrapper = shallow(<AddIssues />);
+
     expect(wrapper.find(Redirect)).to.have.lengthOf(1);
   });
 });
