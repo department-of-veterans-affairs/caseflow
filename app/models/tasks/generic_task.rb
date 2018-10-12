6b182b2e84aecf1ec68e2314cd2ac5a6a4467f4e
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-10-12 11:25:06 -0400

    Limit VSO task action options to only 'Mark task complete' (#7374)

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -1,6 +1,8 @@
 class GenericTask < Task
   def allowed_actions(user)
-    return [] if assigned_to != user && assigned_to_type != "Organization"
+    return [] if assigned_to != user && !assigned_to.is_a?(Organization)
+
+    return [{ label: "Mark task complete", value: "mark_task_complete" }] if assigned_to.is_a?(Vso)
 
     [
       {
