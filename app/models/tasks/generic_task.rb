f06c9ea279c3f9f002a096cc07b6016f8bde261e
lowellrex <lowell@navahq.com>
2018-10-15 10:25:09 -0400

    Increase strictness of allowed actions returned to front-end

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -1,6 +1,7 @@
 class GenericTask < Task
+  # rubocop:disable Metrics/AbcSize
   def allowed_actions(user)
-    if assigned_to.is_a?(Vso)
+    if assigned_to.is_a?(Vso) && assigned_to.user_has_access?(user)
       return [Constants.TASK_ACTIONS.MARK_COMPLETE.to_h]
     end
 
@@ -12,7 +13,7 @@ class GenericTask < Task
       ]
     end
 
-    if assigned_to.is_a?(Organization)
+    if assigned_to.is_a?(Organization) && assigned_to.user_has_access?(user)
       return [
         Constants.TASK_ACTIONS.ASSIGN_TO_TEAM.to_h,
         Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h,
@@ -22,6 +23,7 @@ class GenericTask < Task
 
     []
   end
+  # rubocop:enable Metrics/AbcSize
 
   def update_from_params(params, current_user)
     verify_user_access!(current_user)
