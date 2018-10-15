ac7b48ab26e99292aedd253ab5124458464d30dd
lowellrex <lowell@navahq.com>
2018-10-15 15:58:29 -0400

    Use ActiveRecord query interface. Move recently_completed to Task model

--- app/models/queues/colocated_queue.rb
+++ app/models/queues/colocated_queue.rb
@@ -3,28 +3,24 @@ class ColocatedQueue
 
   attr_accessor :user
 
-  # rubocop:disable Style/SymbolProc
+  # rubocop:disable Style/SymbolProc, Rails/FindEach
   def tasks
-    relevant_tasks.select { |t| t.assigned_to == user }.each { |t| t.update_if_hold_expired! }
+    relevant_tasks.where(assigned_to: user).each { |t| t.update_if_hold_expired! }
   end
 
   def tasks_by_appeal_id(appeal_id, appeal_type)
-    tasks = relevant_tasks.select { |t| t.appeal_id == appeal_id && t.appeal_type == appeal_type }
-    tasks.each { |t| t.update_if_hold_expired! }
+    relevant_tasks.where(appeal_id: appeal_id, appeal_type: appeal_type).each { |t| t.update_if_hold_expired! }
   end
-  # rubocop:enable Style/SymbolProc
+  # rubocop:enable Style/SymbolProc, Rails/FindEach
 
   private
 
   def relevant_tasks
-    [incomplete_tasks, recently_completed_tasks].flatten
+    incomplete_tasks.or(recently_completed_tasks)
   end
 
   def recently_completed_tasks
-    ColocatedTask.where(
-      status: Constants.TASK_STATUSES.completed,
-      completed_at: (Time.zone.now - 2.weeks)..Time.zone.now
-    )
+    ColocatedTask.recently_completed
   end
 
   def incomplete_tasks
