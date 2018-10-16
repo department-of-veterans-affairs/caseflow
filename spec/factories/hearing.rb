d260f53213ff1c279bdc71d8ed3c2879a197fd2b
Sharon Warner <sharon.warner56@gmail.com>
2018-10-16 15:59:05 -0400

    Tests to facols

--- spec/factories/hearing.rb
+++ spec/factories/hearing.rb
@@ -4,7 +4,10 @@ FactoryBot.define do
       case_hearing { create(:case_hearing, user: current_user) }
     end
 
-    appeal { create(:legacy_appeal, vacols_case: create(:case, case_hearings: [case_hearing])) }
+    appeal do
+      create(:legacy_appeal, vacols_case: create(:case_with_form_9, case_issues:
+        [create(:case_issue), create(:case_issue)], case_hearings: [case_hearing]))
+    end
     vacols_id { case_hearing.hearing_pkseq }
   end
 end
