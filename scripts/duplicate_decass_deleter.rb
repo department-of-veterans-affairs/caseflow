da5693613349be3542d904f421f5c55344f199c1
Evan Kroske <evan@navahq.com>
2018-10-17 11:20:10 -0400

    Fixes lint errors

--- scripts/duplicate_decass_deleter.rb
+++ scripts/duplicate_decass_deleter.rb
@@ -1,18 +1,5 @@
 # bundle exec rails runner scripts/enable_features_dev.rb
 
-def sql_date(d)
-  if d.nil?
-    return "null"
-  end
-end
-
-def sql_num(n)
-  if d.nil?
-    return "null"
-  end
-  n.to_s
-end
-
 def sql_fmt(o)
   if o.nil?
     return "is null"
@@ -29,59 +16,60 @@ def sql_fmt(o)
   fail o.inspect
 end
 
+# rubocop:disable Metrics/AbcSize
 def predicate_from_record(record, limit)
-  <<EOS
-rownum <= #{limit} and
-defolder #{sql_fmt(record.defolder)} and
-deatty #{sql_fmt(record.deatty)} and
-deteam #{sql_fmt(record.deteam)} and
-depdiff #{sql_fmt(record.depdiff)} and
-defdiff #{sql_fmt(record.defdiff)} and
-deassign #{sql_fmt(record.deassign)} and
-dereceive #{sql_fmt(record.dereceive)} and
-dehours #{sql_fmt(record.dehours)} and
-deprod #{sql_fmt(record.deprod)} and
-detrem #{sql_fmt(record.detrem)} and
-dearem #{sql_fmt(record.dearem)} and
-deoq #{sql_fmt(record.deoq)} and
-deadusr #{sql_fmt(record.deadusr)} and
-deadtim #{sql_fmt(record.deadtim)} and
-deprogrev #{sql_fmt(record.deprogrev)} and
-deatcom #{sql_fmt(record.deatcom)} and
-debmcom #{sql_fmt(record.debmcom)} and
-demdusr #{sql_fmt(record.demdusr)} and
-demdtim #{sql_fmt(record.demdtim)} and
-delock #{sql_fmt(record.delock)} and
-dememid #{sql_fmt(record.dememid)} and
-decomp #{sql_fmt(record.decomp)} and
-dedeadline #{sql_fmt(record.dedeadline)} and
-deicr #{sql_fmt(record.deicr)} and
-defcr #{sql_fmt(record.defcr)} and
-deqr1 #{sql_fmt(record.deqr1)} and
-deqr2 #{sql_fmt(record.deqr2)} and
-deqr3 #{sql_fmt(record.deqr3)} and
-deqr4 #{sql_fmt(record.deqr4)} and
-deqr5 #{sql_fmt(record.deqr5)} and
-deqr6 #{sql_fmt(record.deqr6)} and
-deqr7 #{sql_fmt(record.deqr7)} and
-deqr8 #{sql_fmt(record.deqr8)} and
-deqr9 #{sql_fmt(record.deqr9)} and
-deqr10 #{sql_fmt(record.deqr10)} and
-deqr11 #{sql_fmt(record.deqr11)} and
-dedocid #{sql_fmt(record.dedocid)} and
-derecommend #{sql_fmt(record.derecommend)}
+  <<EOS.strip_heredoc
+    rownum <= #{limit} and
+    defolder #{sql_fmt(record.defolder)} and
+    deatty #{sql_fmt(record.deatty)} and
+    deteam #{sql_fmt(record.deteam)} and
+    depdiff #{sql_fmt(record.depdiff)} and
+    defdiff #{sql_fmt(record.defdiff)} and
+    deassign #{sql_fmt(record.deassign)} and
+    dereceive #{sql_fmt(record.dereceive)} and
+    dehours #{sql_fmt(record.dehours)} and
+    deprod #{sql_fmt(record.deprod)} and
+    detrem #{sql_fmt(record.detrem)} and
+    dearem #{sql_fmt(record.dearem)} and
+    deoq #{sql_fmt(record.deoq)} and
+    deadusr #{sql_fmt(record.deadusr)} and
+    deadtim #{sql_fmt(record.deadtim)} and
+    deprogrev #{sql_fmt(record.deprogrev)} and
+    deatcom #{sql_fmt(record.deatcom)} and
+    debmcom #{sql_fmt(record.debmcom)} and
+    demdusr #{sql_fmt(record.demdusr)} and
+    demdtim #{sql_fmt(record.demdtim)} and
+    delock #{sql_fmt(record.delock)} and
+    dememid #{sql_fmt(record.dememid)} and
+    decomp #{sql_fmt(record.decomp)} and
+    dedeadline #{sql_fmt(record.dedeadline)} and
+    deicr #{sql_fmt(record.deicr)} and
+    defcr #{sql_fmt(record.defcr)} and
+    deqr1 #{sql_fmt(record.deqr1)} and
+    deqr2 #{sql_fmt(record.deqr2)} and
+    deqr3 #{sql_fmt(record.deqr3)} and
+    deqr4 #{sql_fmt(record.deqr4)} and
+    deqr5 #{sql_fmt(record.deqr5)} and
+    deqr6 #{sql_fmt(record.deqr6)} and
+    deqr7 #{sql_fmt(record.deqr7)} and
+    deqr8 #{sql_fmt(record.deqr8)} and
+    deqr9 #{sql_fmt(record.deqr9)} and
+    deqr10 #{sql_fmt(record.deqr10)} and
+    deqr11 #{sql_fmt(record.deqr11)} and
+    dedocid #{sql_fmt(record.dedocid)} and
+    derecommend #{sql_fmt(record.derecommend)}
 EOS
 end
+# rubocop:enable Metrics/AbcSize
 
 DRY_RUN = !ARGV.include?("--nodry_run")
 if !DRY_RUN
   puts "WARNING: This is NOT a dry run."
 end
 
-records = VACOLS::Decass.select("defolder").
-  where("deadtim >= ?", Date.new(2018, 8, 16)).
-  group("defolder").having("count(*) > 1")
-defolders = records.map { |r| r.defolder }
+defolders = VACOLS::Decass.select("defolder")
+  .where("deadtim >= ?", Date.new(2018, 8, 16))
+  .group("defolder").having("count(*) > 1").map(&:defolder)
 puts "Found #{defolders.length} cases with too many Decass records"
 defolders.each do |defolder|
   puts "Processing case #{defolder}"
@@ -95,27 +83,25 @@ defolders.each do |defolder|
       end
       records_with_json_rep[j].push(r)
     end
-    records_with_json_rep.each_pair do |k, records|
-      if records.length == 1
+    records_with_json_rep.each_pair do |k, records_duplicate|
+      if records_duplicate.length == 1
         next
       end
-      query_select = <<EOS
-select 1
-from decass
-where #{predicate_from_record(records[0], records.length - 1)}
+      query_select = <<EOS.strip_heredoc
+        select 1
+        from decass
+        where #{predicate_from_record(records_duplicate[0], records_duplicate.length - 1)}
 EOS
       num_found = 0
       cursor = VACOLS::Decass.connection.execute(query_select)
-      while r = cursor.fetch()
-        num_found += 1
-      end
-      fail unless num_found == records.length - 1
+      num_found += 1 while cursor.fetch
+      fail unless num_found == records_duplicate.length - 1
 
-      puts "Deleting #{records.length - 1} duplicates of #{k}"
-      query_delete = <<EOS
-delete
-from decass
-where #{predicate_from_record(records[0], records.length - 1)}
+      puts "Deleting #{records_duplicate.length - 1} duplicates of #{k}"
+      query_delete = <<EOS.strip_heredoc
+        delete
+        from decass
+        where #{predicate_from_record(records_duplicate[0], records_duplicate.length - 1)}
 EOS
       if !DRY_RUN
         VACOLS::Decass.connection.execute(query_delete)
