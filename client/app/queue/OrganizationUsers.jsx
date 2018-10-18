e84a430e40e0f0b476448edcc3a21ebe6589e4c2
Mark Benjamin <mdbenjam@gmail.com>
2018-10-18 15:45:57 -0400

    Adding UI

---
+++ client/app/queue/OrganizationUsers.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+import { sprintf } from 'sprintf-js';
+
+import TaskTable from './components/TaskTable';
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+
+import {
+  tasksWithAppealSelector
+} from './selectors';
+import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
+
+import { fullWidth } from './constants';
+import COPY from '../../COPY.json';
+import ApiUtil from '../util/ApiUtil';
+import Alert from '../components/Alert';
+import Button from '../components/Button';
+import SearchableDropdown from '../components/SearchableDropdown';
+
+export default class OrganizationUsers extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      organizationUsers: [],
+      remainingUsers: [],
+      loading: true,
+      error: null,
+      addingUser: null,
+      removingUser: {}
+    };
+  };
+
+  componentDidMount = () => {
+    ApiUtil.get(`/organizations/${this.props.organization}/users`).then((response) => {
+      const resp = JSON.parse(response.text);
+
+      this.setState({
+        organizationUsers: resp.organization_users.data,
+        remainingUsers: resp.remaining_users.data,
+        loading: false
+      });
+    }, (error) => {
+      this.setState({
+        loading: false,
+        error: {
+          title: "Failed to load users",
+          body: error.message
+        }
+      })
+    });
+  };
+
+  dropdownOptions = () => {
+    return this.state.remainingUsers.map((user) => {
+      return { label: this.formatName(user), value: user };
+    });
+  };
+
+  formatName = (user) => {
+    return `${user.attributes.full_name} (${user.attributes.css_id})`;
+  };
+
+  addUser = ({ value }) => {
+    const data = {
+      id: value.id
+    };
+
+    this.setState({
+      addingUser: value
+    });
+
+    ApiUtil.post(`/organizations/${this.props.organization}/users`, { data }).then((response) => {
+      this.setState({
+        organizationUsers: [...this.state.organizationUsers, value],
+        remainingUsers: this.state.remainingUsers.filter((user) => user.id !== value.id),
+        addingUser: null
+      });
+    }, (error) => {
+      this.setState({
+        addingUser: null,
+        error: {
+          title: "Failed to add user",
+          body: error.message
+        }
+      })
+    });
+  }
+
+  removeUser = (user) => () => {
+    this.setState({
+      removingUser: { ...this.state.removingUser, [user.id]: true }
+    });
+
+    ApiUtil.delete(`/organizations/${this.props.organization}/users/${user.id}`).then((response) => {
+      this.setState({
+        remainingUsers: [...this.state.remainingUsers, user],
+        organizationUsers: this.state.organizationUsers.filter((arrayUser) => arrayUser.id !== user.id),
+        removingUser: { ...this.state.removingUser, [user.id]: false }
+      });
+    }, (error) => {
+      this.setState({
+        removingUser: { ...this.state.removingUser, [user.id]: false },
+        error: {
+          title: "Failed to remove user",
+          body: error.message
+        }
+      })
+    });
+  }
+
+  mainContent = () => {
+    if (this.state.loading) {
+      return <h2>{COPY.LOADING}</h2>;
+    }
+
+    const listOfUsers = this.state.organizationUsers.map((user) => {
+      return <li>{this.formatName(user)} &nbsp;
+        <Button
+          name="Remove user"
+          classNames={['usa-button-secondary']}
+          loading={this.state.removingUser[user.id]}
+          onClick={this.removeUser(user)} />
+      </li>;
+    });
+
+    return <React.Fragment>
+      <ul>{listOfUsers}</ul>
+      <h1>Add a user to the team:</h1>
+      <SearchableDropdown
+        name="Add user"
+        hideLabel
+        searchable
+        readOnly={this.state.addingUser}
+        placeholder={this.state.addingUser ? `Adding user ${this.formatName(this.state.addingUser)}` : "Select user to add"}
+        value={null}
+        onChange={this.addUser}
+        options={this.dropdownOptions()} />
+    </React.Fragment>;
+  }
+
+  render = () => {
+    return <AppSegment filledBackground>
+      { this.state.error && <Alert title={this.state.error.title} type="error">
+        {this.state.error.body}
+      </Alert>}
+      <div>
+        <h1>{this.props.organization.charAt(0).toUpperCase() + this.props.organization.substring(1)} team</h1>
+        {this.mainContent()}
+      </div>
+    </AppSegment>;
+  };
+}
