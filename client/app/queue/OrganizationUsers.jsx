c42fbe56b3d6595d6d78c2c2f8cd3aa377d5bca6
Mark Benjamin <mdbenjam@gmail.com>
2018-10-18 17:11:45 -0400

    Lint errors

--- client/app/queue/OrganizationUsers.jsx
+++ client/app/queue/OrganizationUsers.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import _ from 'lodash';
-import { sprintf } from 'sprintf-js';
 
-import TaskTable from './components/TaskTable';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
-import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 
-import {
-  tasksWithAppealSelector
-} from './selectors';
-import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
-
-import { fullWidth } from './constants';
-import COPY from '../../COPY.json';
 import ApiUtil from '../util/ApiUtil';
 import Alert from '../components/Alert';
 import Button from '../components/Button';
@@ -38,7 +24,7 @@ export default class OrganizationUsers extends React.PureComponent {
       addingUser: null,
       removingUser: {}
     };
-  };
+  }
 
   loadingPromise = () => {
     return ApiUtil.get(`/organizations/${this.props.organization}/users`).then((response) => {
@@ -54,16 +40,17 @@ export default class OrganizationUsers extends React.PureComponent {
       this.setState({
         loading: false,
         error: {
-          title: "Failed to load users",
+          title: 'Failed to load users',
           body: error.message
         }
-      })
+      });
     });
   };
 
   dropdownOptions = () => {
     return this.state.remainingUsers.map((user) => {
-      return { label: this.formatName(user), value: user };
+      return { label: this.formatName(user),
+        value: user };
     });
   };
 
@@ -80,7 +67,7 @@ export default class OrganizationUsers extends React.PureComponent {
       addingUser: value
     });
 
-    ApiUtil.post(`/organizations/${this.props.organization}/users`, { data }).then((response) => {
+    ApiUtil.post(`/organizations/${this.props.organization}/users`, { data }).then(() => {
       this.setState({
         organizationUsers: [...this.state.organizationUsers, value],
         remainingUsers: this.state.remainingUsers.filter((user) => user.id !== value.id),
@@ -90,32 +77,35 @@ export default class OrganizationUsers extends React.PureComponent {
       this.setState({
         addingUser: null,
         error: {
-          title: "Failed to add user",
+          title: 'Failed to add user',
           body: error.message
         }
-      })
+      });
     });
   }
 
   removeUser = (user) => () => {
     this.setState({
-      removingUser: { ...this.state.removingUser, [user.id]: true }
+      removingUser: { ...this.state.removingUser,
+        [user.id]: true }
     });
 
-    ApiUtil.delete(`/organizations/${this.props.organization}/users/${user.id}`).then((response) => {
+    ApiUtil.delete(`/organizations/${this.props.organization}/users/${user.id}`).then(() => {
       this.setState({
         remainingUsers: [...this.state.remainingUsers, user],
         organizationUsers: this.state.organizationUsers.filter((arrayUser) => arrayUser.id !== user.id),
-        removingUser: { ...this.state.removingUser, [user.id]: false }
+        removingUser: { ...this.state.removingUser,
+          [user.id]: false }
       });
     }, (error) => {
       this.setState({
-        removingUser: { ...this.state.removingUser, [user.id]: false },
+        removingUser: { ...this.state.removingUser,
+          [user.id]: false },
         error: {
-          title: "Failed to remove user",
+          title: 'Failed to remove user',
           body: error.message
         }
-      })
+      });
     });
   }
 
@@ -138,7 +128,9 @@ export default class OrganizationUsers extends React.PureComponent {
         hideLabel
         searchable
         readOnly={Boolean(this.state.addingUser)}
-        placeholder={this.state.addingUser ? `Adding user ${this.formatName(this.state.addingUser)}` : "Select user to add"}
+        placeholder={
+          this.state.addingUser ? `Adding user ${this.formatName(this.state.addingUser)}` : 'Select user to add'
+        }
         value={null}
         onChange={this.addUser}
         options={this.dropdownOptions()} />
