e98f59ebaa642bef0f24b800ebc8c61d5085efa1
lowellrex <lowell@navahq.com>
2018-10-19 13:54:12 -0400

    Simplify MailTask.create_from_params()

--- app/models/tasks/mail_task.rb
+++ app/models/tasks/mail_task.rb
@@ -1,24 +1,22 @@
 class MailTask < GenericTask
   class << self
-    def create_from_params(params_array, current_user)
-      verify_user_can_assign!(current_user)
+    def create_from_params(params, user)
+      verify_user_can_assign!(user)
 
-      params_array.map do |params|
-        root_task = RootTask.find_by(appeal_id: params[:appeal].id)
-        unless root_task
-          fail(Caseflow::Error::NoRootTask, message: "Could not find root task for appeal with ID #{params[:appeal]}")
-        end
-
-        mail_task = create!(
-          appeal: root_task.appeal,
-          parent_id: root_task.id,
-          assigned_to: MailTeam.singleton
-        )
-
-        params[:parent_id] = mail_task.id
+      root_task = RootTask.find_by(appeal_id: params[:appeal].id)
+      unless root_task
+        fail(Caseflow::Error::NoRootTask, message: "Could not find root task for appeal with ID #{params[:appeal]}")
       end
 
-      GenericTask.create_from_params(params_array, current_user)
+      mail_task = create!(
+        appeal: root_task.appeal,
+        parent_id: root_task.id,
+        assigned_to: MailTeam.singleton
+      )
+
+      # Create a child task off of the mail organization's task so we can track how that task was created.
+      params[:parent_id] = mail_task.id
+      GenericTask.create_from_params(params, user)
     end
 
     def verify_user_can_assign!(user)
