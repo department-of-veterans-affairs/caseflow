c744acf4b7df2a3f3c23ae5086051f61fbdcc628
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-10-22 14:48:47 -0700

    lint

--- client/app/intakeEdit/pages/canceled.jsx
+++ client/app/intakeEdit/pages/canceled.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+
 import StatusMessage from '../../components/StatusMessage';
 import { FORM_TYPES } from '../../intake/constants';
 
@@ -12,7 +14,7 @@ class Canceled extends Component {
     } = this.props;
     const formName = _.find(FORM_TYPES, { key: formType }).name;
     const message = `No changes were made to ${veteran.name}'s (ID #${veteran.fileNumber}) Request for ${formName}.
-Go to VBMS claim details and click the “Edit in Caseflow” button to return to edit.`
+Go to VBMS claim details and click the “Edit in Caseflow” button to return to edit.`;
 
     return <div>
       <StatusMessage
