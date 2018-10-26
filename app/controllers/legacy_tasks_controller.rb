a0ad58c799a73a6b4c8ed947a787ababf7f0ef67
Alex Prokop <alex.prokop@gmail.com>
2018-10-26 14:52:07 -0400

    ws

--- app/controllers/legacy_tasks_controller.rb
+++ app/controllers/legacy_tasks_controller.rb
@@ -48,7 +48,7 @@ class LegacyTasksController < ApplicationController
 
   def assign_to_judge
     # If the user being assigned to is a judge, do not create a DECASS record, just
-    # update the location to the assigned judge. 
+    # update the location to the assigned judge.
     appeal = LegacyAppeal.find(legacy_task_params[:appeal_id])
     QueueRepository.update_location_to_judge(appeal.vacols_id, legacy_task_params[:assigned_to])
 
