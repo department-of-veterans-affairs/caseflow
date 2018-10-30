a9214690cfb2b13c0aff2423dd7de3801ed55411
aroltsch <aroltsch@tistatech.com>
2018-10-30 11:57:49 -0400

    Decision becomes uploadable

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,4 +1,13 @@
 class Decision < ApplicationRecord
+  include UploadableDocument
   belongs_to :appeal
   validates :citation_number, format: { with: /\AA\d{8}\Z/i }
+
+  def document_type
+    "BVA Decision"
+  end
+
+  def pdf_location
+    "/Users/vhaisaroltsa/Desktop/sample.pdf"
+  end
 end
