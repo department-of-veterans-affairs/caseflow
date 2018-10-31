ef4babbb5ac0354fcb9b036724a1cce8cb5f8ca3
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-10-31 16:00:37 -0700

    update names to rating/nonrating and unique checklist keys

--- client/app/intake/pages/decisionReviewIntakeCompleted.jsx
+++ client/app/intake/pages/decisionReviewIntakeCompleted.jsx
@@ -15,21 +15,21 @@ const getAppealChecklistItems = (requestIssues) => [<Fragment>
 // higher level reviews & supplemental claims
 const getClaimReviewChecklistItems = (formType, requestIssues, isInformalConferenceRequested) => {
   const checklist = [];
-  const ratedIssues = requestIssues.filter((ri) => ri.isRated || ri.isUnidentified);
-  const nonRatedIssues = requestIssues.filter((ri) => ri.isRated === false);
+  const ratingIssues = requestIssues.filter((ri) => ri.isRated || ri.isUnidentified);
+  const nonratingIssues = requestIssues.filter((ri) => ri.isRated === false);
   const claimReviewName = _.find(FORM_TYPES, { key: formType }).shortName;
 
-  if (ratedIssues.length > 0) {
+  if (ratingIssues.length > 0) {
     checklist.push(<Fragment>
       <strong>A {claimReviewName} Rating EP is being established:</strong>
-      {ratedIssues.map((ri, i) => <p key={i}>Contention: {ri.description}</p>)}
+      {ratingIssues.map((ri, i) => <p key={`rating-issue-${i}`}>Contention: {ri.description}</p>)}
     </Fragment>);
   }
 
-  if (nonRatedIssues.length > 0) {
+  if (nonratingIssues.length > 0) {
     checklist.push(<Fragment>
       <strong>A {claimReviewName} Nonrating EP is being established:</strong>
-      {nonRatedIssues.map((nri, i) => <p key={i}>Contention: {nri.contentionText}</p>)}
+      {nonratingIssues.map((nri, i) => <p key={`nonrating-issue-${i}`}>Contention: {nri.contentionText}</p>)}
     </Fragment>);
   }
 
