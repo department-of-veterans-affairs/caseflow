f19222fbf520170424d1c013b5be26847aef673c
Andrew Lomax <andrewlomaxr@gmail.com>
2018-11-02 11:32:55 -0400

    change field name to bva_coordinator

--- client/app/hearingSchedule/components/DailyDocket.jsx
+++ client/app/hearingSchedule/components/DailyDocket.jsx
@@ -294,7 +294,7 @@ export default class DailyDocket extends React.Component {
       </div>
       <span className="cf-push-right">
         VLJ: {this.props.dailyDocket.judgeFirstName} {this.props.dailyDocket.judgeLastName} <br />
-        Coordinator: {this.props.dailyDocket.vdbvapoc} <br/>
+      Coordinator: {this.props.dailyDocket.bvaCoordinator} <br/>
         Hearing type: {this.props.dailyDocket.hearingType}
       </span>
       <div {...noMarginStyling}>
