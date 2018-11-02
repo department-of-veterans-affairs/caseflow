df2158f31707b5465072c2403b59f71963f55d71
Mark Benjamin <mdbenjam@gmail.com>
2018-11-02 16:04:56 -0400

    Fixing rubocop

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,16 +8,18 @@ class ApplicationJob < ActiveJob::Base
   end
 
   around_perform do |job, block|
-    block.call
-  rescue StandardError => ex
-    tags = { job: job.class.name, queue: job.queue_name }
-    context = {
-      job_id: job.job_id,
-      queue_name: job.queue_name,
-      job_class: job.class.name
-    }
-    Raven.capture_exception(ex, tags: tags, extra: context)
-    raise
+    begin
+      block.call
+    rescue StandardError => ex
+      tags = { job: job.class.name, queue: job.queue_name }
+      context = {
+        job_id: job.job_id,
+        queue_name: job.queue_name,
+        job_class: job.class.name
+      }
+      Raven.capture_exception(ex, tags: tags, extra: context)
+      raise
+    end
   end
 
   before_perform do
