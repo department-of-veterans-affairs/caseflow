dee75daeaf0c868350ae154b280299e3e99bbae9
Joe Yang <joey@thirdslant.com>
2018-11-05 12:19:01 -0800

    add selector to only pull tasks from the active organization

--- client/app/queue/OrganizationQueue.jsx
+++ client/app/queue/OrganizationQueue.jsx
@@ -2,48 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import _ from 'lodash';
 import { sprintf } from 'sprintf-js';
 
 import TabWindow from '../components/TabWindow';
 import TaskTable from './components/TaskTable';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
-import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 
 import type { State } from './types/state';
 import type { TaskWithAppeal } from './types/models';
 
 import {
   getUnassignedOrganizationalTasks,
-  getAssignedOrganizationalTasks,
+  getOnHoldOrganizationalTasks,
   getCompletedOrganizationalTasks,
-  tasksWithAppealSelector
+  tasksByOrganization
 } from './selectors';
 
 import { clearCaseSelectSearch } from '../reader/CaseSelect/CaseSelectActions';
-import TASK_STATUSES from '../../constants/TASK_STATUSES.json';
 
 import { fullWidth } from './constants';
 import COPY from '../../COPY.json';
-
-const countCasesByType = (tasks) => {
-  const numberOfCases = {
-    [TASK_STATUSES.assigned]: 0,
-    [TASK_STATUSES.in_progress]: 0,
-    [TASK_STATUSES.on_hold]: 0,
-    [TASK_STATUSES.completed]: 0
-  };
-
-  return _.reduce(tasks, (cases, task) => {
-    if (cases[task.status]) {
-      cases[task.status] += 1;
-    } else {
-      cases[task.status] = 1;
-    }
-
-    return cases;
-  }, numberOfCases);
-};
 
 class OrganizationQueue extends React.PureComponent {
   componentDidMount = () => {
@@ -51,23 +29,22 @@ class OrganizationQueue extends React.PureComponent {
   }
 
   render = () => {
-    const numberOfCases = countCasesByType(this.props.tasks);
     const tabs = [
       {
         label: sprintf(
           COPY.ORGANIZATIONAL_QUEUE_PAGE_UNASSIGNED_TAB_TITLE,
-          numberOfCases.assigned + numberOfCases.in_progress),
-        page: <UnassignedTasksTab />
+          this.props.numberOfTasks.unassigned),
+        page: <UnassignedTasksTab organizationName={this.props.organizationName} />
       },
       {
         label: sprintf(
           COPY.ORGANIZATIONAL_QUEUE_PAGE_ASSIGNED_TAB_TITLE,
-          numberOfCases.on_hold),
-        page: <AssignedTasksTab />
+          this.props.numberOfTasks.onHold),
+        page: <AssignedTasksTab organizationName={this.props.organizationName} />
       },
       {
         label: COPY.ORGANIZATIONAL_QUEUE_PAGE_COMPLETE_TAB_TITLE,
-        page: <CompletedTasksTab />
+        page: <CompletedTasksTab organizationName={this.props.organizationName} />
       }
     ];
 
@@ -89,7 +66,12 @@ OrganizationQueue.propTypes = {
 
 const mapStateToProps = (state) => {
   return ({
-    tasks: tasksWithAppealSelector(state)
+    numberOfTasks: {
+      unassigned: getUnassignedOrganizationalTasks(state).length,
+      onHold: getOnHoldOrganizationalTasks(state).length,
+      completed: getCompletedOrganizationalTasks(state).length
+    },
+    tasks: tasksByOrganization(state)
   });
 };
 
@@ -104,12 +86,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(OrganizationQueue);
 const UnassignedTasksTab = connect(
   (state: State) => ({ tasks: getUnassignedOrganizationalTasks(state) }))(
   (props: { tasks: Array<TaskWithAppeal> }) => {
-    const noTasks = !_.size(props.tasks);
-
-    const content = noTasks ?
-      <p>{COPY.ORGANIZATIONAL_QUEUE_EMPTY_STATE_MESSAGE}
-        <b><Link to="/search">{COPY.NO_CASES_IN_QUEUE_LINK_TEXT}</Link></b>.
-      </p> :
+    return <React.Fragment>
+      <p>{sprintf(COPY.COLOCATED_QUEUE_PAGE_UNASSIGNED_TASKS_DESCRIPTION, props.organizationName)}</p>
       <TaskTable
         includeDetailsLink
         includeTask
@@ -118,22 +96,15 @@ const UnassignedTasksTab = connect(
         includeDaysWaiting
         includeReaderLink
         tasks={props.tasks}
-      />;
-
-    return <React.Fragment>
-      {content}
+      />
     </React.Fragment>;
   });
 
 const AssignedTasksTab = connect(
-  (state: State) => ({ tasks: getAssignedOrganizationalTasks(state) }))(
+  (state: State) => ({ tasks: getOnHoldOrganizationalTasks(state) }))(
   (props: { tasks: Array<TaskWithAppeal> }) => {
-    const noTasks = !_.size(props.tasks);
-
-    const content = noTasks ?
-      <p>{COPY.ORGANIZATIONAL_QUEUE_EMPTY_STATE_MESSAGE}
-        <b><Link to="/search">{COPY.NO_CASES_IN_QUEUE_LINK_TEXT}</Link></b>.
-      </p> :
+    return <React.Fragment>
+      <p>{sprintf(COPY.COLOCATED_QUEUE_PAGE_ASSIGNED_TASKS_DESCRIPTION, props.organizationName)}</p>
       <TaskTable
         includeDetailsLink
         includeTask
@@ -142,22 +113,15 @@ const AssignedTasksTab = connect(
         includeDaysWaiting
         includeReaderLink
         tasks={props.tasks}
-      />;
-
-    return <React.Fragment>
-      {content}
+      />
     </React.Fragment>;
   });
 
 const CompletedTasksTab = connect(
   (state: State) => ({ tasks: getCompletedOrganizationalTasks(state) }))(
   (props: { tasks: Array<TaskWithAppeal> }) => {
-    const noTasks = !_.size(props.tasks);
-
-    const content = noTasks ?
-      <p>{COPY.ORGANIZATIONAL_QUEUE_EMPTY_STATE_MESSAGE}
-        <b><Link to="/search">{COPY.NO_CASES_IN_QUEUE_LINK_TEXT}</Link></b>.
-      </p> :
+    return <React.Fragment>
+      <p>{sprintf(COPY.COLOCATED_QUEUE_PAGE_COMPLETE_TASKS_DESCRIPTION, props.organizationName)}</p>
       <TaskTable
         includeDetailsLink
         includeTask
@@ -166,9 +130,6 @@ const CompletedTasksTab = connect(
         includeDaysWaiting
         includeReaderLink
         tasks={props.tasks}
-      />;
-
-    return <React.Fragment>
-      {content}
+      />
     </React.Fragment>;
   });
--- client/app/queue/OrganizationQueueLoadingScreen.jsx
+++ client/app/queue/OrganizationQueueLoadingScreen.jsx
@@ -12,6 +12,10 @@ import {
   onReceiveQueue
 } from './QueueActions';
 
+import {
+  setActiveOrganizationId
+} from './uiReducer/uiActions';
+
 type Params = {|
   children: React.Node,
   urlToLoad: string
@@ -26,7 +30,12 @@ class OrganizationQueueLoadingScreen extends React.PureComponent<Props> {
   // TODO: Short-circuit this request if we already have the tasks for this organization's queue.
   createLoadPromise = () => ApiUtil.get(this.props.urlToLoad, { timeout: { response: 5 * 60 * 1000 } }).then(
     (response) => {
-      const { tasks: { data: tasks } } = JSON.parse(response.text);
+      const {
+        tasks: { data: tasks },
+        id
+      } = JSON.parse(response.text);
+
+      this.props.setActiveOrganizationId(id);
 
       this.props.onReceiveQueue(extractAppealsAndAmaTasks(tasks));
     }
@@ -60,7 +69,8 @@ class OrganizationQueueLoadingScreen extends React.PureComponent<Props> {
 }
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
-  onReceiveQueue
+  onReceiveQueue,
+  setActiveOrganizationId
 }, dispatch);
 
 export default (connect(null, mapDispatchToProps)(OrganizationQueueLoadingScreen): React.ComponentType<Params>);
