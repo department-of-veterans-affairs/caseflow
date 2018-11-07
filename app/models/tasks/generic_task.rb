69827417e93d7253fcdc855b0bbd04f2f1811def
Mark Benjamin <mdbenjam@gmail.com>
2018-11-07 15:47:24 -0500

    Moving all tasks to VSO tasks

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -1,10 +1,6 @@
 class GenericTask < Task
   # rubocop:disable Metrics/AbcSize
   def available_actions(user)
-    if assigned_to.is_a?(Vso) && assigned_to.user_has_access?(user)
-      return [Constants.TASK_ACTIONS.MARK_COMPLETE.to_h]
-    end
-
     if assigned_to == user
       return [
         Constants.TASK_ACTIONS.ASSIGN_TO_TEAM.to_h,
