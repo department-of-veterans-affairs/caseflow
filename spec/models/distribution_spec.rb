cd8d677908136b922feab35089fc66c3905a5e5e
Chris Given <Christopher.Given@va.gov>
2018-11-07 21:09:59 -0800

    A few more tests

--- spec/models/distribution_spec.rb
+++ spec/models/distribution_spec.rb
@@ -1,6 +1,7 @@
 describe Distribution do
-  let(:judge) { create(:user) }
-  let!(:vacols_judge) { create(:staff, :judge_role, sdomainid: judge.css_id) }
+  let(:css_id) { "BVARZIEMANN1" }
+  let(:judge) { create(:user, css_id: css_id) }
+  let!(:vacols_judge) { create(:staff, :judge_role, sdomainid: css_id) }
 
   context "validations" do
     subject { Distribution.create(judge: user) }
@@ -33,6 +34,30 @@ describe Distribution do
       it "does not validate" do
         expect(subject.errors.details).to have_key(:judge)
       end
+    end
+  end
+
+  context "#batch_size" do
+    subject { Distribution.create(judge: judge).send(:batch_size) }
+
+    it "is the number of attorneys times 5" do
+      expect(subject).to eq(15)
+    end
+
+    context "when judge is not known" do
+      let(:css_id) { "RANDO" }
+
+      it "is equal to 10" do
+        expect(subject).to eq(10)
+      end
+    end
+  end
+
+  context "#total_batch_size" do
+    subject { Distribution.create(judge: judge).send(:total_batch_size) }
+
+    it "is the total number of attorneys times 5" do
+      expect(subject).to eq(45)
     end
   end
 end
