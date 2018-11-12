75f1f2ada046c0b37a034b8b4be33a2188ac39ff
Peter Karman <peter@truss.works>
2018-11-12 12:01:20 -0600

    Refactor intake feature tests to reduce boilerplate

--- spec/support/intake_helpers.rb
+++ spec/support/intake_helpers.rb
@@ -5,4 +5,38 @@ module IntakeHelpers
     fill_in "Notes", with: note
     safe_click ".add-issue"
   end
+
+  def click_intake_add_issue
+    safe_click "#button-add-issue"
+  end
+
+  def click_intake_finish
+    safe_click "#button-finish-intake"
+  end
+
+  def add_intake_rating_issue(description, note = nil)
+    find_all("label", text: description).first.click
+    fill_in("Notes", with: note) if note
+    safe_click ".add-issue"
+  end
+
+  def add_intake_nonrating_issue(category:, description:, date:)
+    safe_click ".no-matching-issues"
+    expect(page.text).to match(/Does issue \d match any of these issue categories?/)
+    expect(page).to have_button("Add this issue", disabled: true)
+    fill_in "Issue category", with: category
+    find("#issue-category").send_keys :enter
+    fill_in "Issue description", with: description
+    fill_in "Decision date", with: date
+    expect(page).to have_button("Add this issue", disabled: false)
+    safe_click ".add-issue"
+  end
+
+  def add_intake_unidentified_issue(description)
+    safe_click ".no-matching-issues"
+    safe_click ".no-matching-issues"
+    expect(page).to have_content("Describe the issue to mark it as needing further review.")
+    fill_in "Transcribe the issue as it's written on the form", with: description
+    safe_click ".add-issue"
+  end
 end
