5194305c3324a21bc5677a4d42a1c0810e539b78
Chris Given <Christopher.Given@va.gov>
2018-11-13 18:47:53 -0500

    Add basic tests for the job

---
+++ spec/jobs/start_distribution_job_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe StartDistributionJob do
+  let(:judge) { create(:user, css_id: "MYNAMEISJUDGE") }
+  let!(:vacols_judge) { create(:staff, :judge_role, sdomainid: "MYNAMEISJUDGE") }
+  let(:distribution) { Distribution.new(judge: judge) }
+
+  context ".perform" do
+    subject { StartDistributionJob.perform_now(distribution) }
+    it "calls distribute!" do
+      expect(distribution).to receive(:distribute!)
+      subject
+    end
+
+    context "when distribute! errors" do
+      it "logs the error" do
+        allow(distribution).to receive(:distribute!).and_raise(StandardError)
+        allow(Rails.logger).to receive(:info)
+        expect(Rails.logger).to receive(:info).with("StartDistributionJob failed: StandardError")
+        subject
+      end
+    end
+  end
+end
