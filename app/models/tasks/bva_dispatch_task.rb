92c625b200287d0578262058e88cd808da7465d6
lowellrex <lowell@navahq.com>
2018-11-13 16:15:35 -0500

    Move BvaDispatch membership to OrganizationsUser model

--- app/models/tasks/bva_dispatch_task.rb
+++ app/models/tasks/bva_dispatch_task.rb
@@ -40,7 +40,7 @@ class BvaDispatchTask < GenericTask
     private
 
     def list_of_assignees
-      Constants::BvaDispatchTeams::USERS[Rails.current_env]
+      BvaDispatch.singleton.users.order(:id).pluck(:css_id)
     end
   end
 end
