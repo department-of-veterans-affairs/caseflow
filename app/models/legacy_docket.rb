81d65bc8e997698fbafab6d95991c1cdd56eb681
Chris Given <cmgiven@gmail.com>
2018-11-13 20:02:27 -0500

    Legacy Automatic Case Distribution (#7692)
    * Legacy docket model
    
    * Fleshing out distribution stuff
    
    * End of day
    
    * Quick fix
    
    * Apparently no special treatment for CUEs
    
    * More tweaks
    
    * Additional Jed fixes
    
    * Migrations and tweaks
    
    * Tweaks
    
    * Getting started on case docket specs
    
    * distribute_nonpriority_appeal specs finished
    
    * Make batch_update_vacols_location private
    
    * distribute_priority_appeals specs
    
    * Docket only counts cases with a form 9
    
    * Alex changes
    
    * Remove redundant comment
    
    * Support acting judges
    
    * Limit acting judges to 7000 range
    
    * Missing question mark
    
    * Testing validations
    
    * A few more tests
    
    * It's working
    
    * Maybe fix broken test on CI
    
    * PR updates, including better locking strategy and refactoring the SQL queries
    
    * It is not necessary to ensure that all rows are locked, as any locked case storage row will block a simultaneous distribution from obtaining its desired lock
    
    * Move batch case move stuff to VACOLS::Case
    
    * Ignore brakeman false positive
    
    * Move distribute! to a job
    
    * Lint fixes
    
    * Revert reps dump to master
    
    * Nocov max_attempts
    
    * Add basic tests for the job
    
    * Automatic Case Distribution Frontend (#7805)
    
    * Automatic case distribution frontend
    
    * Show empty task table after unassigned_cases distribution error
    
    * This ordering is more readable
    
    * Typo
    
    * Method length
    
    * Eslint fixes
    
    * Missed one
    
    * Fix flow
    
    * Turns out acting judges recieve genpop cases too
    
    * Strip out other acting_judge stuff that's not needed
    
    * Missed one

---
+++ app/models/legacy_docket.rb
@@ -0,0 +1,54 @@
+class LegacyDocket
+  include ActiveModel::Model
+
+  # When counting the total number of appeals on the legacy docket for purposes of docket balancing, we
+  # include NOD-stage appeals at a discount reflecting the likelihood that they will advance to a Form 9.
+  NOD_ADJUSTMENT = 0.4
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def count(priority: nil, ready: nil)
+    counts_by_priority_and_readiness.inject(0) do |sum, row|
+      next sum unless (priority.nil? || (priority ? 1 : 0) == row["priority"]) &&
+                      (ready.nil? || (ready ? 1 : 0) == row["ready"])
+      sum + row["n"]
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+
+  def weight
+    count(priority: false) + nod_count * NOD_ADJUSTMENT
+  end
+
+  def distribute_priority_appeals(distribution, genpop: "any", limit: 1)
+    LegacyAppeal.repository.distribute_priority_appeals(distribution.judge, genpop, limit).map do |record|
+      distribution.distributed_cases.create(case_id: record["bfkey"],
+                                            docket: "legacy",
+                                            priority: true,
+                                            ready_at: VacolsHelper.normalize_vacols_datetime(record["bfdloout"]),
+                                            genpop: record["vlj"].nil?,
+                                            genpop_query: genpop)
+    end
+  end
+
+  def distribute_nonpriority_appeals(distribution, genpop: "any", range: nil, limit: 1)
+    LegacyAppeal.repository.distribute_nonpriority_appeals(distribution.judge, genpop, range, limit).map do |record|
+      distribution.distributed_cases.create(case_id: record["bfkey"],
+                                            docket: "legacy",
+                                            priority: false,
+                                            ready_at: VacolsHelper.normalize_vacols_datetime(record["bfdloout"]),
+                                            docket_index: record["docket_index"],
+                                            genpop: record["vlj"].nil?,
+                                            genpop_query: genpop)
+    end
+  end
+
+  private
+
+  def counts_by_priority_and_readiness
+    @counts_by_priority_and_readiness ||= LegacyAppeal.repository.docket_counts_by_priority_and_readiness
+  end
+
+  def nod_count
+    @nod_count ||= LegacyAppeal.repository.nod_count
+  end
+end
