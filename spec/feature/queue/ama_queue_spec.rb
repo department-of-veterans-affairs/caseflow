bb1606b5b8e3601493c3878d473d19c9d4bdf650
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-11-14 08:30:13 -0500

    Move path helper function from VSO to Organization (#7843)
    I was noodling on how we could use the Organization model to determine judge team membership and I noticed that the `path()` method only existed for VSOs and not generic Organizations. This PR changes that and creates the `user_admin_path` helper function as well.

--- spec/feature/queue/ama_queue_spec.rb
+++ spec/feature/queue/ama_queue_spec.rb
@@ -214,7 +214,7 @@ RSpec.feature "AmaQueue" do
       end
 
       scenario "assign case to self" do
-        visit "/organizations/#{translation_organization.url}"
+        visit translation_organization.path
 
         click_on "Pal Smith"
 
@@ -360,7 +360,7 @@ RSpec.feature "AmaQueue" do
     end
 
     scenario "return case to judge" do
-      visit "/organizations/#{quality_review_organization.url}"
+      visit quality_review_organization.path
       click_on "Bob Smith"
 
       find(".Select-control", text: "Select an action").click
