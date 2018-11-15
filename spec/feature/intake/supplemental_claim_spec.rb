a443a34745f4aa215faca5589db411afaab0d7e2
Sally Maki <sallymaki@gmail.com>
2018-11-15 16:59:45 -0500

    Fix tests

--- spec/feature/intake/supplemental_claim_spec.rb
+++ spec/feature/intake/supplemental_claim_spec.rb
@@ -412,7 +412,7 @@ RSpec.feature "Supplemental Claim Intake" do
   end
 
   it "Requires Payee Code for compensation and pension benefit types and non-Veteran claimant" do
-    intake = HigherLevelReviewIntake.new(veteran_file_number: veteran.file_number, user: current_user)
+    intake = SupplementalClaimIntake.new(veteran_file_number: veteran.file_number, user: current_user)
     intake.start!
     visit "/intake"
 
@@ -423,14 +423,6 @@ RSpec.feature "Supplemental Claim Intake" do
     end
 
     fill_in "What is the Receipt Date of this form?", with: "04/20/2018"
-
-    within_fieldset("Did the Veteran request an informal conference?") do
-      find("label", text: "No", match: :prefer_exact).click
-    end
-
-    within_fieldset("Did the Veteran request review by the same office?") do
-      find("label", text: "No", match: :prefer_exact).click
-    end
 
     within_fieldset("Is the claimant someone other than the Veteran?") do
       find("label", text: "Yes", match: :prefer_exact).click
