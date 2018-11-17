1c3445e660159ff92f6bb607bb6614344e2cc83e
Joe Yang <joey@thirdslant.com>
2018-11-16 18:23:31 -0600

    add comment to explain hearing badge margin

--- client/app/queue/components/HearingBadge.jsx
+++ client/app/queue/components/HearingBadge.jsx
@@ -45,6 +45,7 @@ const DocketTypeBadge = ({ hearing }) => {
     </ul>
   </div>;
 
+  // We expect this badge to be shown in a table, so we use this to get rid of the standard table padding.
   return <div {...css({ marginRight: '-3rem' })} className="cf-hearing-badge">
     <Tooltip id={`badge-${hearing.id}`} text={tooltipText} position="bottom">
       <span {...badgeStyling}>H</span>
