068dfec77634a991a448a50e2216c4ece95bc9dd
aroltsch <aroltsch@tistatech.com>
2018-11-19 13:00:54 -0500

    Migrate existing decision issues to use request_decision_issues table

--- app/models/request_decision_issue.rb
+++ app/models/request_decision_issue.rb
@@ -1,4 +1,8 @@
 class RequestDecisionIssue < ApplicationRecord
   belongs_to :request_issue
   belongs_to :decision_issue
+
+  validates :request_issue, :decision_issue, presence: true
+  validates :request_issue, uniqueness:
+    { scope: :decision_issue, message: "Combination of request issue and decision issue must be unique" }
 end
