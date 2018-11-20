915567409dcd0ca72fdb09317f745abdcbb2a781
Sjones352 <sjones@tistatech.com>
2018-11-20 11:45:50 -0500

    central hearings

--- app/models/concerns/hearing_concern.rb
+++ app/models/concerns/hearing_concern.rb
@@ -2,6 +2,6 @@ module HearingConcern
   extend ActiveSupport::Concern
 
   def request_type
-    (type != :central_office) ? type.to_s.capitalize : "Central"
+    (type != :central) ? type.to_s.capitalize : "Central"
   end
 end
