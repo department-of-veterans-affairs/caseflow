acb885fcfd29464d8ca4ec41082143585099432a
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-11-21 14:30:12 -0500

    Include named subclasses in list of assignable organizations (#7933)
    Noticed this bug when preparing my local environment for a demo with the mail team this morning. The list of organizations returned by the [where statement in `assignable()`](https://github.com/department-of-veterans-affairs/caseflow/blob/952ab605bf2aa6a1f7dbc7366de06784c24781a8/app/models/organization.rb#L7) excludes organizations that are subclasses of the main Organization class unless they are explicitly included in that list. This was a result of a decision made [when we initially added](https://github.com/department-of-veterans-affairs/caseflow/pull/7183#pullrequestreview-160016295) the ability to list assignable organizations.
    
    This change fixes the immediate problem and aims to be more flexible to the addition of new organizations going forward.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,18 +4,13 @@ class Organization < ApplicationRecord
   has_many :users, through: :organizations_users
 
   def self.assignable(task)
-    organizations = where(type: [nil, BvaDispatch.name])
+    select { |org| org.can_receive_task?(task) }
+  end
 
-    # Exclude the current organization from the list of assignable organizations if the
-    # task is assigned to this organization or the task is a child of a task assigned to
-    # this organization. Prevents assignment loops.
-    if task.assigned_to_type == name
-      organizations.where.not(id: task.assigned_to_id)
-    elsif task.assigned_to_type == User.name && task.parent && task.parent.assigned_to_type == name
-      organizations.where.not(id: task.parent.assigned_to_id)
-    else
-      organizations
-    end
+  def can_receive_task?(task)
+    return false if task.assigned_to == self
+    return false if task.assigned_to.is_a?(User) && task.parent && task.parent.assigned_to == self
+    true
   end
 
   def user_has_access?(user)
