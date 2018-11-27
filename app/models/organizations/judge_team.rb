00348d5cd3a4cb233f412db11d2dc69153642e2a
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-11-27 11:50:13 -0500

    Rename administrated_teams to administered_teams (#8000)
    Missed one suggestion to rename this method in #7964. This PR renames the method.

--- app/models/organizations/judge_team.rb
+++ app/models/organizations/judge_team.rb
@@ -1,6 +1,6 @@
 class JudgeTeam < Organization
   def self.for_judge(user)
-    user.administrated_teams.select { |team| team.is_a?(JudgeTeam) }.first
+    user.administered_teams.select { |team| team.is_a?(JudgeTeam) }.first
   end
 
   def self.create_for_judge(user)
