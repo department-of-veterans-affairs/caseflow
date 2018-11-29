d689f452d690a57fb342df0e6b55732dd6c7bafd
Jia Huang <jialiya.huang0@gmail.com>
2018-11-28 17:10:12 -0800

    adding associated_end_products to ratings (#8029)
    Connects #7889
    
    When this PR is merged, cached_serialized_ratings should be cleared

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -10,38 +10,58 @@ class Rating
   ONE_YEAR_PLUS_DAYS = 372.days
   TWO_LIFETIMES_DAYS = 250.years
 
-  def issues
-    @issues ||= fetch_issues
+  def ui_hash
+    serialize
   end
 
   # If you change this method, you will need
   # to clear cache in prod for your changes to
   # take effect immediately.
   # See DecisionReview#cached_serialized_timely_ratings.
-  def ui_hash
+  def serialize
     {
       participant_id: participant_id,
       profile_date: profile_date,
       promulgation_date: promulgation_date,
-      issues: issues.map(&:ui_hash)
+      issues: issues.map(&:serialize)
     }
+  end
+
+  def associated_end_products
+    associated_claims_data.map do |claim_data|
+      EndProduct.new(
+        claim_id: claim_data[:clm_id],
+        claim_type_code: claim_data[:bnft_clm_tc]
+      )
+    end
+  end
+
+  def issues
+    return [] if rating_profile[:rating_issues].nil?
+
+    [rating_profile[:rating_issues]].flatten.map do |issue_data|
+      RatingIssue.from_bgs_hash(self, issue_data)
+    end
   end
 
   private
 
-  def fetch_issues
-    response = BGSService.new.fetch_rating_profile(
+  def associated_claims_data
+    return [] if rating_profile[:associated_claims].nil?
+    [rating_profile[:associated_claims]].flatten
+  end
+
+  def fetch_rating_profile
+    BGSService.new.fetch_rating_profile(
       participant_id: participant_id,
       profile_date: profile_date
     )
-
-    return [] if response[:rating_issues].nil?
-
-    [response[:rating_issues]].flatten.map do |issue_data|
-      RatingIssue.from_bgs_hash(self, issue_data)
-    end
   rescue Savon::Error
-    []
+    {}
+  end
+
+  def rating_profile
+    @rating_profile ||= fetch_rating_profile
   end
 
   class << self
