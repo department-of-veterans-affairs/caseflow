a15cd9efc4145b122a8bd95496c96e8e4c237cbb
Ricardo Kreyhsig <44415076+rkreyhsig@users.noreply.github.com>
2018-11-29 10:10:13 -0500

    Issue order in Case Details (#7776) (#7968)
    Resolves #7776 - Issue order in Case Details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181127201444) do
+ActiveRecord::Schema.define(version: 20181128225613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -685,6 +685,7 @@ ActiveRecord::Schema.define(version: 20181127201444) do
     t.string "ineligible_reason"
     t.string "vacols_id"
     t.string "vacols_sequence_id"
+    t.datetime "created_at"
     t.index ["contention_reference_id", "removed_at"], name: "index_request_issues_on_contention_reference_id_and_removed_at", unique: true
     t.index ["end_product_establishment_id"], name: "index_request_issues_on_end_product_establishment_id"
     t.index ["ineligible_due_to_id"], name: "index_request_issues_on_ineligible_due_to_id"
