department-of-veterans-affairs/caseflow

commit 5270977f8e4cbbea0c2f4f1a5ff5474fcd21b96c

Refactor added issue into its own component class (#7959)
connects #7879 

### Description
Refactor added issue rendering and eligibility check into their own component.

### Acceptance Criteria
- [x] Code compiles correctly
- [x] Tests keep passing

client/app/intake/components/AddedIssue.jsx
@@ -1,0 +1,89 @@
+import _ from 'lodash';
+import React from 'react';
+
+import INELIGIBLE_REQUEST_ISSUES from '../../../constants/INELIGIBLE_REQUEST_ISSUES.json';
+
+class AddedIssue extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errorMsg: '',
+      cssKlasses: ['issue-desc']
+    };
+  }
+
+  componentDidMount() {
+    if (this.needsEligibilityCheck()) {
+      this.updateEligibleState();
+    }
+  }
+
+  needsEligibilityCheck() {
+    let { issue, requestIssues } = this.props;
+
+    if (!requestIssues) {
+      return false;
+    }
+    if (issue.ineligibleReason) {
+      return true;
+    }
+
+    let existingRequestIssue = _.some(requestIssues, { reference_id: issue.referenceId });
+
+    if (existingRequestIssue) {
+      return false;
+    }
+
+    return true;
+  }
+
+  updateEligibleState() {
+    let { issue, formType } = this.props;
+
+    let errorMsg = '';
+
+    if (issue.isUnidentified) {
+      return;
+    }
+
+    if (issue.titleOfActiveReview) {
+      errorMsg = INELIGIBLE_REQUEST_ISSUES.duplicate_of_issue_in_active_review.replace(
+        '{review_title}', issue.titleOfActiveReview
+      );
+    } else if (issue.ineligibleReason) {
+      errorMsg = INELIGIBLE_REQUEST_ISSUES[issue.ineligibleReason];
+    } else if (issue.timely === false && formType !== 'supplemental_claim' && issue.untimelyExemption !== 'true') {
+      errorMsg = INELIGIBLE_REQUEST_ISSUES.untimely;
+    } else if (issue.sourceHigherLevelReview && formType === 'higher_level_review') {
+      errorMsg = INELIGIBLE_REQUEST_ISSUES.previous_higher_level_review;
+    } else if (issue.beforeAma) {
+      errorMsg = INELIGIBLE_REQUEST_ISSUES.before_ama;
+    }
+
+    if (errorMsg !== '') {
+      this.setState((state) => {
+        return { errorMsg,
+          cssKlasses: state.cssKlasses.concat(['not-eligible'])
+        };
+      });
+    }
+  }
+
+  render() {
+    let { issue, issueIdx } = this.props;
+
+    return <div className={this.state.cssKlasses.join(' ')}>
+      <span className="issue-num">{issueIdx + 1}.&nbsp;</span>
+      { issue.text } {this.state.errorMsg}
+      { issue.date && <span className="issue-date">Decision date: { issue.date }</span> }
+      { issue.notes && <span className="issue-notes">Notes:&nbsp;{ issue.notes }</span> }
+      { issue.untimelyExemptionNotes &&
+        <span className="issue-notes">Untimely Exemption Notes:&nbsp;{issue.untimelyExemptionNotes}</span>
+      }
+    </div>;
+  }
+
+}
+
+export default AddedIssue;

client/app/intake/pages/addIssues.jsx
@@ -11,9 +11,9 @@ import UnidentifiedIssuesModal from '../components/UnidentifiedIssuesModal';
 import UntimelyExemptionModal from '../components/UntimelyExemptionModal';
 import LegacyOptInModal from '../components/LegacyOptInModal';
 import Button from '../../components/Button';
+import AddedIssue from '../components/AddedIssue';
 import ErrorAlert from '../components/ErrorAlert';
 import { REQUEST_STATE, FORM_TYPES, PAGE_PATHS } from '../constants';
-import INELIGIBLE_REQUEST_ISSUES from '../../../constants/INELIGIBLE_REQUEST_ISSUES.json';
 import { formatDate } from '../../util/DateUtil';
 import { formatAddedIssues, getAddIssuesFields } from '../util/issues';
 import Table from '../../components/Table';
@@ -46,43 +46,6 @@ export class AddIssuesPage extends React.Component {
     } else {
       this.props.removeIssue(index);
     }
-  }
-
-  needsEligibilityCheck = (issue, intakeData) => {
-    if (!intakeData.requestIssues) {
-      return false;
-    }
-    if (issue.ineligibleReason) {
-      return true;
-    }
-
-    let existingRequestIssue = _.some(intakeData.requestIssues, { reference_id: issue.referenceId });
-
-    if (existingRequestIssue) {
-      return false;
-    }
-
-    return true;
-  }
-
-  checkIfEligible = (issue, formType) => {
-    if (issue.isUnidentified) {
-      return false;
-    } else if (issue.titleOfActiveReview) {
-      return INELIGIBLE_REQUEST_ISSUES.duplicate_of_issue_in_active_review.replace(
-        '{review_title}', issue.titleOfActiveReview
-      );
-    } else if (issue.ineligibleReason) {
-      return INELIGIBLE_REQUEST_ISSUES[issue.ineligibleReason];
-    } else if (issue.timely === false && formType !== 'supplemental_claim' && issue.untimelyExemption !== 'true') {
-      return INELIGIBLE_REQUEST_ISSUES.untimely;
-    } else if (issue.sourceHigherLevelReview && formType === 'higher_level_review') {
-      return INELIGIBLE_REQUEST_ISSUES.previous_higher_level_review;
-    } else if (issue.beforeAma) {
-      return INELIGIBLE_REQUEST_ISSUES.before_ama;
-    }
-
-    return true;
   }
 
   onClickAddIssue = (ratingIssueCount) => {
@@ -126,30 +89,12 @@ export class AddIssuesPage extends React.Component {
       return <div className="issues">
         <div>
           { issues.map((issue, index) => {
-            let issueKlasses = ['issue-desc'];
-            let addendum = '';
-
-            if (this.needsEligibilityCheck(issue, intakeData)) {
-              let isEligible = this.checkIfEligible(issue, formType);
-
-              if (isEligible !== true) {
-                if (isEligible !== false) {
-                  addendum = isEligible;
-                }
-                issueKlasses.push('not-eligible');
-              }
-            }
-
             return <div className="issue" key={`issue-${index}`}>
-              <div className={issueKlasses.join(' ')}>
-                <span className="issue-num">{index + 1}.&nbsp;</span>
-                { issue.text } {addendum}
-                { issue.date && <span className="issue-date">Decision date: { issue.date }</span> }
-                { issue.notes && <span className="issue-notes">Notes:&nbsp;{ issue.notes }</span> }
-                { issue.untimelyExemptionNotes &&
-                  <span className="issue-notes">Untimely Exemption Notes:&nbsp;{issue.untimelyExemptionNotes}</span>
-                }
-              </div>
+              <AddedIssue
+                issue={issue}
+                issueIdx={index}
+                requestIssues={intakeData.requestIssues}
+                formType={formType} />
               <div className="issue-action">
                 <Button
                   onClick={() => this.onRemoveClick(index)}

commit 61dcc609e0a561a6c49d3f16851e20f854ca472f

Fix React bug with state and props out of sync (#7993)
connects #7772 

### Description
The refactor in 5270977f8e4cbbea0c2f4f1a5ff5474fcd21b96c introduced a regression due to how React components are rendered. When a list of child components is rendered, and then one is removed, all the child components bump up the stack. That meant that issues might inherit the state of the component just before them, causing them to appear as eligibility errors when they are not.

The solution is to calculate eligibility errors at every `render` instead of assuming that an issue and a component will stay together forever.

### Acceptance Criteria
- [x] Code compiles correctly

client/app/intake/components/AddedIssue.jsx
@@ -4,21 +4,6 @@ import React from 'react';
 import INELIGIBLE_REQUEST_ISSUES from '../../../constants/INELIGIBLE_REQUEST_ISSUES.json';
 
 class AddedIssue extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      errorMsg: '',
-      cssKlasses: ['issue-desc']
-    };
-  }
-
-  componentDidMount() {
-    if (this.needsEligibilityCheck()) {
-      this.updateEligibleState();
-    }
-  }
-
   needsEligibilityCheck() {
     let { issue, requestIssues } = this.props;
 
@@ -29,22 +14,29 @@ class AddedIssue extends React.PureComponent {
       return true;
     }
 
-    let existingRequestIssue = _.some(requestIssues, { reference_id: issue.referenceId });
+    let existingRequestIssue = _.filter(requestIssues, { reference_id: issue.referenceId })[0];
 
-    if (existingRequestIssue) {
+    // leaving this here to make it easier to debug in future.
+    // console.log('existingRequestIssue', existingRequestIssue);
+
+    if (existingRequestIssue && !existingRequestIssue.ineligible_reason) {
       return false;
     }
 
     return true;
   }
 
-  updateEligibleState() {
+  getEligibility() {
     let { issue, formType } = this.props;
 
+    // console.log('getEligibility', formType, issue);
+
     let errorMsg = '';
+    const cssKlassesWithError = ['issue-desc', 'not-eligible'];
 
     if (issue.isUnidentified) {
-      return;
+      return { errorMsg,
+        cssKlasses: cssKlassesWithError.concat(['issue-unidentified']) };
     }
 
     if (issue.titleOfActiveReview) {
@@ -62,20 +54,31 @@ class AddedIssue extends React.PureComponent {
     }
 
     if (errorMsg !== '') {
-      this.setState((state) => {
-        return { errorMsg,
-          cssKlasses: state.cssKlasses.concat(['not-eligible'])
-        };
-      });
+      return { errorMsg,
+        cssKlasses: cssKlassesWithError };
     }
   }
 
   render() {
     let { issue, issueIdx } = this.props;
+    let eligibleState = {
+      errorMsg: '',
+      cssKlasses: ['issue-desc']
+    };
 
-    return <div className={this.state.cssKlasses.join(' ')}>
+    // console.log('needsEligibilityCheck', issue, this.needsEligibilityCheck());
+
+    if (this.needsEligibilityCheck()) {
+      let eligibilityCheck = this.getEligibility();
+
+      if (eligibilityCheck) {
+        eligibleState = eligibilityCheck;
+      }
+    }
+
+    return <div className={eligibleState.cssKlasses.join(' ')}>
       <span className="issue-num">{issueIdx + 1}.&nbsp;</span>
-      { issue.text } {this.state.errorMsg}
+      { issue.text } {eligibleState.errorMsg}
       { issue.date && <span className="issue-date">Decision date: { issue.date }</span> }
       { issue.notes && <span className="issue-notes">Notes:&nbsp;{ issue.notes }</span> }
       { issue.untimelyExemptionNotes &&

lib/fakes/bgs_service.rb
@@ -111,9 +111,10 @@ class Fakes::BGSService
         RequestIssue.find_or_create_by!(
           review_request: hlr,
           end_product_establishment: epe,
-          rating_issue_reference_id: in_active_review_reference_id,
-          rating_issue_profile_date: in_active_review_receipt_date - 1
-        )
+          rating_issue_reference_id: in_active_review_reference_id
+        ) do |reqi|
+          reqi.rating_issue_profile_date = Time.zone.today - 100
+        end
         Generators::EndProduct.build(
           veteran_file_number: veteran.file_number,
           bgs_attrs: { benefit_claim_id: in_active_review_reference_id }

spec/feature/intake/appeal_spec.rb
@@ -431,16 +431,20 @@ RSpec.feature "Appeal Intake" do
     )
 
     # add unidentified issue
+    expect(page).to_not have_css(".issue-unidentified")
     click_intake_add_issue
     add_intake_unidentified_issue("This is an unidentified issue")
     expect(page).to have_content("3 issues")
     expect(page).to have_content("This is an unidentified issue")
+    expect(find_intake_issue_by_number(3)).to have_css(".issue-unidentified")
+    expect_ineligible_issue(3)
 
     # add ineligible issue
     click_intake_add_issue
     add_intake_rating_issue("Old injury in review")
     expect(page).to have_content("4 issues")
     expect(page).to have_content("4. Old injury in review is ineligible because it's already under review as a Appeal")
+    expect_ineligible_issue(4)
 
     # add untimely rating request issue
     click_intake_add_issue
@@ -449,6 +453,7 @@ RSpec.feature "Appeal Intake" do
     expect(page).to have_content("5 issues")
     expect(page).to have_content("I am an exemption note")
     expect(page).to_not have_content("5. Really old injury #{Constants.INELIGIBLE_REQUEST_ISSUES.untimely}")
+    expect_ineligible_issue(5)
 
     # remove and re-add with different answer to exemption
     click_remove_intake_issue("5")
@@ -458,6 +463,7 @@ RSpec.feature "Appeal Intake" do
     expect(page).to have_content("5 issues")
     expect(page).to have_content("I am an exemption note")
     expect(page).to have_content("5. Really old injury #{Constants.INELIGIBLE_REQUEST_ISSUES.untimely}")
+    expect_ineligible_issue(5)
 
     # add untimely nonrating request issue
     click_intake_add_issue
@@ -473,6 +479,7 @@ RSpec.feature "Appeal Intake" do
     expect(page).to have_content(
       "Another Description for Active Duty Adjustments #{Constants.INELIGIBLE_REQUEST_ISSUES.untimely}"
     )
+    expect_ineligible_issue(6)
 
     # add before_ama ratings
     click_intake_add_issue
@@ -480,6 +487,7 @@ RSpec.feature "Appeal Intake" do
     expect(page).to have_content(
       "7. Non-RAMP Issue before AMA Activation #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
     )
+    expect_ineligible_issue(7)
 
     # Eligible because it comes from a RAMP decision
     click_intake_add_issue
@@ -497,6 +505,7 @@ RSpec.feature "Appeal Intake" do
     expect(page).to have_content(
       "A nonrating issue before AMA #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
     )
+    expect_ineligible_issue(9)
 
     click_intake_finish
 

spec/feature/intake/edit_spec.rb
@@ -41,7 +41,7 @@ RSpec.feature "Edit issues" do
       promulgation_date: receipt_date,
       profile_date: profile_date,
       issues: [
-        { reference_id: "abc123", decision_text: "Left knee granted" },
+        { reference_id: "abc123", decision_text: "Left knee granted", contention_reference_id: "000" },
         { reference_id: "def456", decision_text: "PTSD denied" }
       ]
     )
@@ -249,7 +249,7 @@ RSpec.feature "Edit issues" do
         RequestIssue.create!(
           review_request: higher_level_review,
           issue_category: "Military Retired Pay",
-          description: "nonrating description",
+          description: "eligible nonrating description",
           contention_reference_id: "1234",
           ineligible_reason: nil,
           decision_date: Date.new(2018, 5, 1)
@@ -260,7 +260,7 @@ RSpec.feature "Edit issues" do
         RequestIssue.create!(
           review_request: higher_level_review,
           issue_category: "Active Duty Adjustments",
-          description: "nonrating description",
+          description: "untimely nonrating description",
           contention_reference_id: "12345",
           ineligible_reason: :untimely
         )
@@ -284,9 +284,19 @@ RSpec.feature "Edit issues" do
           rating_issue_profile_date: rating.profile_date,
           review_request: higher_level_review,
           description: "PTSD denied",
-          contention_reference_id: "123",
+          contention_reference_id: "111",
           ineligible_reason: :duplicate_of_issue_in_active_review,
           ineligible_due_to: ri_in_review
+        )
+      end
+
+      let!(:ri_previous_hlr) do
+        RequestIssue.create!(
+          rating_issue_reference_id: "abc123",
+          rating_issue_profile_date: rating.profile_date,
+          review_request: another_higher_level_review,
+          description: "Left knee granted",
+          contention_reference_id: "000"
         )
       end
 
@@ -296,8 +306,9 @@ RSpec.feature "Edit issues" do
           rating_issue_profile_date: rating.profile_date,
           review_request: higher_level_review,
           description: "Left knee granted",
-          contention_reference_id: "123",
-          ineligible_reason: :previous_higher_level_review
+          contention_reference_id: "222",
+          ineligible_reason: :previous_higher_level_review,
+          ineligible_due_to: ri_previous_hlr
         )
       end
 
@@ -323,6 +334,13 @@ RSpec.feature "Edit issues" do
         )
       end
 
+      let(:ep_claim_id) do
+        EndProductEstablishment.find_by(
+          source: higher_level_review,
+          code: "030HLRNR"
+        ).reference_id
+      end
+
       before do
         another_higher_level_review.create_issues!([ri_in_review])
         higher_level_review.create_issues!([
@@ -337,10 +355,6 @@ RSpec.feature "Edit issues" do
       end
 
       it "shows the Higher-Level Review Edit page with ineligibility messages" do
-        ep_claim_id = EndProductEstablishment.find_by(
-          source: higher_level_review,
-          code: "030HLRNR"
-        ).reference_id
         visit "higher_level_reviews/#{ep_claim_id}/edit"
 
         expect(page).to have_content(
@@ -358,6 +372,94 @@ RSpec.feature "Edit issues" do
         )
         expect(page).to have_content(
           "#{eligible_ri_before_ama.contention_text} Decision date:"
+        )
+      end
+
+      it "re-applies eligibility check on remove/re-add of ineligible issue" do
+        visit "higher_level_reviews/#{ep_claim_id}/edit"
+
+        expect(page).to have_content("6 issues")
+        expect_eligible_issue(1)
+        expect_ineligible_issue(2)
+        expect_ineligible_issue(3)
+        expect_ineligible_issue(4)
+        expect_ineligible_issue(5)
+        expect_eligible_issue(6)
+
+        # remove and re-add each ineligible issue. when re-added, it should always be issue 6.
+
+        # 2
+        ri_with_previous_hlr_issue_num = find_intake_issue_number_by_text(ri_with_previous_hlr.contention_text)
+        click_remove_intake_issue(ri_with_previous_hlr_issue_num)
+        click_remove_issue_confirmation
+
+        expect(page).to_not have_content(
+          "#{ri_with_previous_hlr.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.previous_higher_level_review}"
+        )
+
+        click_intake_add_issue
+        add_intake_rating_issue(ri_with_previous_hlr.contention_text)
+
+        expect_ineligible_issue(6)
+        expect(page).to have_content(
+          "#{ri_with_previous_hlr.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.previous_higher_level_review}"
+        )
+
+        # 3
+        ri_in_review_issue_num = find_intake_issue_number_by_text(ri_in_review.contention_text)
+        click_remove_intake_issue(ri_in_review_issue_num)
+        click_remove_issue_confirmation
+
+        expect(page).to_not have_content(
+          "#{ri_in_review.contention_text} is ineligible because it's already under review as a Higher-Level Review"
+        )
+
+        click_intake_add_issue
+        add_intake_rating_issue(ri_in_review.contention_text)
+
+        expect_ineligible_issue(6)
+        expect(page).to have_content(
+          "#{ri_in_review.contention_text} is ineligible because it's already under review as a Higher-Level Review"
+        )
+
+        # 4
+        untimely_request_issue_num = find_intake_issue_number_by_text(untimely_request_issue.contention_text)
+        click_remove_intake_issue(untimely_request_issue_num)
+        click_remove_issue_confirmation
+
+        expect(page).to_not have_content(
+          "#{untimely_request_issue.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.untimely}"
+        )
+
+        click_intake_add_issue
+        click_intake_no_matching_issues
+        add_intake_nonrating_issue(
+          category: "Active Duty Adjustments",
+          description: untimely_request_issue.contention_text,
+          date: "01/01/2016"
+        )
+        add_untimely_exemption_response("No", "I am a nonrating exemption note")
+
+        expect_ineligible_issue(6)
+        expect(page).to have_content(
+          "#{untimely_request_issue.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.untimely}"
+        )
+
+        # 5
+        ri_before_ama_num = find_intake_issue_number_by_text(ri_before_ama.contention_text)
+        click_remove_intake_issue(ri_before_ama_num)
+        click_remove_issue_confirmation
+
+        expect(page).to_not have_content(
+          "#{ri_before_ama.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
+        )
+
+        click_intake_add_issue
+        add_intake_rating_issue(ri_before_ama.contention_text)
+
+        expect_ineligible_issue(6)
+        expect(page).to have_content(
+          "#{ri_before_ama.contention_text} #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
         )
       end
     end
@@ -561,6 +663,8 @@ RSpec.feature "Edit issues" do
         add_intake_unidentified_issue("This is an unidentified issue")
         expect(page).to have_content("5 issues")
         expect(page).to have_content("This is an unidentified issue")
+        expect(find_intake_issue_by_number(5)).to have_css(".issue-unidentified")
+        expect_ineligible_issue(5)
 
         # add issue before AMA
         click_intake_add_issue
@@ -568,6 +672,7 @@ RSpec.feature "Edit issues" do
         expect(page).to have_content(
           "Non-RAMP Issue before AMA Activation #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
         )
+        expect_ineligible_issue(6)
 
         # add RAMP issue before AMA
         click_intake_add_issue

spec/support/intake_helpers.rb
@@ -70,6 +70,26 @@ module IntakeHelpers
     end
   end
 
+  def find_intake_issue_by_text(text)
+    find_all(:xpath, './/div[@class="issues"]/*/div[@class="issue"]').each do |node|
+      if node.text =~ /#{text}/
+        return node
+      end
+    end
+  end
+
+  def find_intake_issue_number_by_text(text)
+    find_intake_issue_by_text(text).find(".issue-num").text.delete(".")
+  end
+
+  def expect_ineligible_issue(number)
+    expect(find_intake_issue_by_number(number)).to have_css(".not-eligible")
+  end
+
+  def expect_eligible_issue(number)
+    expect(find_intake_issue_by_number(number)).to_not have_css(".not-eligible")
+  end
+
   def setup_legacy_opt_in_appeals(veteran_file_number)
     # create two legacy appeals with 2 issues each
     create(:legacy_appeal, vacols_case: