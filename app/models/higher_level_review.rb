9ead39c1e5caaa026bf3a1e33de2d449dd84a8a1
Jia Huang <jialiya.huang0@gmail.com>
2018-11-30 12:39:39 -0800

    remove supplemental claim creating dta

--- app/models/higher_level_review.rb
+++ app/models/higher_level_review.rb
@@ -48,10 +48,6 @@ class HigherLevelReview < ClaimReview
     END_PRODUCT_MODIFIERS
   end
 
-  def on_sync(end_product_establishment)
-    super { create_dta_supplemental_claim }
-  end
-
   def issue_code(rating: true)
     rating ? END_PRODUCT_RATING_CODE : END_PRODUCT_NONRATING_CODE
   end
@@ -60,54 +56,6 @@ class HigherLevelReview < ClaimReview
 
   def informal_conference?
     informal_conference
-  end
-
-  def create_dta_supplemental_claim
-    return if dta_issues_needing_follow_up.empty?
-
-    dta_supplemental_claim.create_issues!(build_follow_up_dta_issues)
-
-    if run_async?
-      ClaimReviewProcessJob.perform_later(dta_supplemental_claim)
-    else
-      ClaimReviewProcessJob.perform_now(dta_supplemental_claim)
-    end
-  end
-
-  def build_follow_up_dta_issues
-    dta_issues_needing_follow_up.map do |dta_issue|
-      # do not copy over end product establishment id,
-      # review request, removed_at, disposition, and contentions
-      RequestIssue.new(
-        review_request: dta_supplemental_claim,
-        parent_request_issue_id: dta_issue.id,
-        rating_issue_reference_id: dta_issue.rating_issue_reference_id,
-        rating_issue_profile_date: dta_issue.rating_issue_profile_date,
-        description: dta_issue.description,
-        issue_category: dta_issue.issue_category,
-        decision_date: dta_issue.decision_date
-      )
-    end
-  end
-
-  def dta_issues_needing_follow_up
-    @dta_issues_needing_follow_up ||= request_issues.no_follow_up_issues.where(disposition: DTA_ERRORS)
-  end
-
-  def dta_supplemental_claim
-    @dta_supplemental_claim ||= SupplementalClaim.create!(
-      veteran_file_number: veteran_file_number,
-      receipt_date: Time.zone.now.to_date,
-      is_dta_error: true,
-      benefit_type: benefit_type,
-      legacy_opt_in_approved: legacy_opt_in_approved,
-      veteran_is_not_claimant: veteran_is_not_claimant
-    ).tap do |sc|
-      sc.create_claimants!(
-        participant_id: claimant_participant_id,
-        payee_code: payee_code
-      )
-    end
   end
 
   def new_end_product_establishment(ep_code)
