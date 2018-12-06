a2006fe21a53444dc6638b0bb8b999b08b3d6586
aroltsch <aroltsch@tistatech.com>
2018-12-06 13:31:48 -0500

    Allow attorneys and judges to update benefit type for decision issues

--- spec/feature/queue/checkout_flows_spec.rb
+++ spec/feature/queue/checkout_flows_spec.rb
@@ -118,6 +118,7 @@ RSpec.feature "Checkout flows" do
 
       let(:decision_issue_text) { "This is a test decision issue" }
       let(:decision_issue_disposition) { "Remanded" }
+      let(:benefit_type) { "Education" }
 
       scenario "veteran is the appellant" do
         visit "/queue"
@@ -152,6 +153,9 @@ RSpec.feature "Checkout flows" do
         find(".Select-control", text: "Select Disposition").click
         find("div", class: "Select-option", text: decision_issue_disposition).click
 
+        find(".Select-control", text: "Benefit type").click
+        find("div", class: "Select-option", text: benefit_type).click
+
         click_on "Save"
 
         # Ensure the decision issue is on the select disposition screen
@@ -183,6 +187,7 @@ RSpec.feature "Checkout flows" do
 
         expect(appeal.decision_issues.count).to eq(1)
         expect(appeal.decision_issues.first.description).to eq(decision_issue_text)
+        expect(appeal.decision_issues.first.benefit_type).to eq(benefit_type.lowecase)
         expect(appeal.decision_issues.first.remand_reasons.first.code).to eq("service_treatment_records")
       end
     end
