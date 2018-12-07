fb82323ca66adbedd082f73262cd78d3980b01c0
Andrew Lomax <andrewlomaxr@gmail.com>
2018-12-06 21:07:44 -0500

    get_distance spec

--- spec/services/external_api/facilities_locator_service_spec.rb
+++ spec/services/external_api/facilities_locator_service_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe ExternalApi::FacilitiesLocatorService, focus: true do
   context "#get_distance" do
-    subject { FacilitiesLocatorService.get_nearest([0.0, 0.0], ["vha_688"]) }
+    subject { FacilitiesLocatorService.get_distance([0.0, 0.0], ["vha_688"]) }
 
     it "return list of nearest facilities" do
       expect(subject[0]).to include(:address, :lat, :long, :id, :name)
