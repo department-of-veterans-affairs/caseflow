98718e76af646daa2dd08c24d03b609b6f6211b3
hschallh <hschallh@gmail.com>
2018-12-07 11:38:02 -0500

    Re-add Reader document list VBMS/VVA timestamps
    * Remove shortcut to return null on nil manifest timestamp values
      that kept error messages from displaying
    * Add check for nil VBMS manifest timestamp
    
    References #6049

--- client/app/reader/LastRetrievalInfo.jsx
+++ client/app/reader/LastRetrievalInfo.jsx
@@ -4,12 +4,10 @@ import { connect } from 'react-redux';
 
 class UnconnectedLastRetrievalInfo extends React.PureComponent {
   render() {
-    if (!this.props.manifestVbmsFetchedAt) {
-      return null;
-    }
-
     return [
-      <div id="vbms-manifest-retrieved-at" key="vbms">Last VBMS retrieval: {this.props.manifestVbmsFetchedAt}</div>,
+      this.props.manifestVbmsFetchedAt ?
+        <div id="vbms-manifest-retrieved-at" key="vbms">Last VBMS retrieval: {this.props.manifestVbmsFetchedAt}</div> :
+        <div className="cf-red-text" key="vbms">Unable to display VBMS documents at this time</div>,
       this.props.manifestVvaFetchedAt ?
         <div id="vva-manifest-retrieved-at" key="vva">Last VVA retrieval: {this.props.manifestVvaFetchedAt}</div> :
         <div className="cf-red-text" key="vva">Unable to display VVA documents at this time</div>
