1be0cf5dc42f1c0717c891f90602eb85c32cd8d8
aroltsch <aroltsch@tistatech.com>
2018-12-11 17:11:40 -0500

    Return an Error object instead of hash

--- client/app/queue/EvaluateDecisionView.jsx
+++ client/app/queue/EvaluateDecisionView.jsx
@@ -156,7 +156,9 @@ class EvaluateDecisionView extends React.PureComponent {
       payload,
       { title: successMsg,
         detail: <DispatchSuccessDetail task={task} /> }).
-      then(() => this.props.deleteAppeal(appealId));
+      then(() => this.props.deleteAppeal(appealId), (response) => {
+        console.log(response);
+      });
   }
 
   getDisplayOptions = (opts) => _.map(JUDGE_CASE_REVIEW_OPTIONS[opts.toUpperCase()],
