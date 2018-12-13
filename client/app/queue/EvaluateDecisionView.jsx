4e29d963dc3ec6bd5610ec0bf4a335fd1885d059
rkreyhsig <rkreyhsig@tistatech.com>
2018-12-13 14:14:54 -0500

    Removing other instances of CaseSnapshot

--- client/app/queue/EvaluateDecisionView.jsx
+++ client/app/queue/EvaluateDecisionView.jsx
@@ -14,7 +14,6 @@ import CheckboxGroup from '../components/CheckboxGroup';
 import Checkbox from '../components/Checkbox';
 import TextareaField from '../components/TextareaField';
 import CaseTitle from './CaseTitle';
-import CaseSnapshot from './CaseSnapshot';
 import Alert from '../components/Alert';
 
 import { deleteAppeal } from './QueueActions';
@@ -211,7 +210,6 @@ class EvaluateDecisionView extends React.PureComponent {
       {error && <Alert title={error.title} type="error" styling={css(marginTop(0), marginBottom(1))}>
         {error.detail}
       </Alert>}
-      <CaseSnapshot appealId={appealId} hideDropdown />
       <hr {...hrStyling} />
 
       {appeal.isLegacyAppeal && <React.Fragment>
