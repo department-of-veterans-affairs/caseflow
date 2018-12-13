bd3d4a6c493932fb9f47029fb7a58018a557d148
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2018-12-13 12:50:08 -0500

    Tomas/8015 Simplify task auth logic (#8248)
    Resolves #8015
    
    ### Description
    Renames and simplifies the methods that control access to tasks.

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -47,7 +47,7 @@ class GenericTask < Task
   # rubocop:enable Metrics/MethodLength
 
   def update_from_params(params, current_user)
-    verify_user_access!(current_user)
+    verify_user_can_update!(current_user)
 
     return reassign(params[:reassign], current_user) if params[:reassign]
 
@@ -67,7 +67,7 @@ class GenericTask < Task
     [sibling, self, children_to_update].flatten
   end
 
-  def can_be_accessed_by_user?(user)
+  def can_be_updated_by_user?(user)
     available_actions_unwrapper(user).any?
   end
 
@@ -89,7 +89,7 @@ class GenericTask < Task
       fail Caseflow::Error::ChildTaskAssignedToSameUser if parent.assigned_to_id == params[:assigned_to_id] &&
                                                            parent.assigned_to_type == params[:assigned_to_type]
 
-      parent.verify_user_access!(user)
+      parent.verify_user_can_update!(user)
 
       params = modify_params(params)
       child = create_child_task(parent, user, params)
