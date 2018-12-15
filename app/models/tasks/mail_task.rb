3d7a56402cc0bf79c38e287666e45783897e11f2
lowellrex <lowell@navahq.com>
2018-12-15 02:25:17 -0500

    Add round robin assignment to colocated mail tasks

--- app/models/tasks/mail_task.rb
+++ app/models/tasks/mail_task.rb
@@ -15,6 +15,7 @@ class MailTask < GenericTask
       MailTask.subclasses.map { |subclass| { value: subclass.name, label: subclass.label } }
     end
 
+    # TODO: Put this whole thing in a transaction so if we fail to create any of the child tasks, we don't create any.
     def create_from_params(params, user)
       verify_user_can_create!(user)
 
@@ -23,13 +24,13 @@ class MailTask < GenericTask
         fail(Caseflow::Error::NoRootTask, message: "Could not find root task for appeal with ID #{params[:appeal]}")
       end
 
+      # Create a task assigned to the mail organization with a child task so we can track how that child was created.
       mail_task = create!(
         appeal: root_task.appeal,
         parent_id: root_task.id,
         assigned_to: MailTeam.singleton
       )
 
-      # Create a child task off of the mail organization's task so we can track how that task was created.
       create_child_task(mail_task, user, params)
     end
 
@@ -45,40 +46,66 @@ class MailTask < GenericTask
   end
 end
 
+class ColocatedMailTask < MailTask
+  include RoundRobinAssigner
+
+  class << self
+    def latest_task
+      Task.where(assigned_to_type: User.name, assigned_to_id: [assignee_pool.pluck(:id)]).order("created_at").last
+    end
+
+    def assignee_pool
+      Colocated.singleton.non_admins.sort_by(&:id)
+    end
+
+    def list_of_assignees
+      assignee_pool.pluck(:css_id)
+    end
+
+    # TODO: This seems a little inefficient. We're effectively replicating GenericTask.create_child_task.
+    # Can we do better?
+    def create_child_task(parent, user, params)
+      parent.update_status(Constants.TASK_STATUSES.on_hold)
+
+      vlj_support_org_task = Task.create!(
+        type: name,
+        appeal: parent.appeal,
+        assigned_by_id: child_assigned_by_id(parent, user),
+        parent_id: parent.id,
+        assigned_to: Colocated.singleton,
+        instructions: params[:instructions]
+      )
+
+      super(vlj_support_org_task, user, params)
+    end
+
+    def get_child_task_assignee(_params)
+      next_assignee
+    end
+  end
+end
+
 # TODO: Flesh out routing rules based on status of appeal.
-# Should incoming mail tasks be automatically routed to members of the VLJ support staff?
-class AddressChangeMailTask < MailTask
+class AddressChangeMailTask < ColocatedMailTask
   class << self
     def label
       COPY::ADDRESS_CHANGE_MAIL_TASK_LABEL
     end
-
-    def get_child_task_assignee(_params)
-      Colocated.singleton
-    end
   end
 end
 
-class EvidenceOrArgumentMailTask < MailTask
+class EvidenceOrArgumentMailTask < ColocatedMailTask
   class << self
     def label
       COPY::EVIDENCE_OR_ARGUMENT_MAIL_TASK_LABEL
     end
-
-    def get_child_task_assignee(_params)
-      Colocated.singleton
-    end
   end
 end
 
-class PowerOfAttorneyRelatedMailTask < MailTask
+class PowerOfAttorneyRelatedMailTask < ColocatedMailTask
   class << self
     def label
       COPY::POWER_OF_ATTORNEY_MAIL_TASK_LABEL
-    end
-
-    def get_child_task_assignee(_params)
-      Colocated.singleton
     end
   end
 end
