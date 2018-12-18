291c8ab7a6551bf1adcf7461baaa90cc836b277b
Andrew Lomax <andrewlomaxr@gmail.com>
2018-12-18 15:14:22 -0500

    trim fake response

--- lib/fakes/facilities_locator_service.rb
+++ lib/fakes/facilities_locator_service.rb
@@ -9,25 +9,14 @@ class Fakes::FacilitiesLocatorService < ExternalApi::FacilitiesLocatorService
         "classification": "VA Medical Center (VAMC)",
         "lat": 38.9311137,
         "long": -77.0109110499999,
-        "website": "http://www.washingtondc.va.gov",
         "distance": 500,
         "address": {
-          "mailing": {
             "address_1": "50 Irving Street, Northwest",
             "address_2": "string",
             "address_3": "string",
             "city": "Washington",
             "state": "DC",
             "zip": "20422-0001"
-          },
-          "physical": {
-            "address_1": "50 Irving Street, Northwest",
-            "address_2": "string",
-            "address_3": "string",
-            "city": "Washington",
-            "state": "DC",
-            "zip": "20422-0001"
-          }
         }
       }
     ]
