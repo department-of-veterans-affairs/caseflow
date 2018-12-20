d0a35974a71bfc31707334fb927e79cf5efe3106
Mark Benjamin <mdbenjam@gmail.com>
2018-12-20 15:18:21 -0500

    Removing tests

--- spec/requests/appeal_controller_spec.rb
+++ spec/requests/appeal_controller_spec.rb
@@ -1,28 +1,5 @@
-RSpec.describe "Reader Appeal Requests", type: :request do
+RSpec.describe "Reader Appeal Requests", type: :request, focus: true do
   let!(:user) { User.authenticate!(roles: ["Reader"]) }
-  let(:vacols_record) { :remand_decided }
-  let(:appeal) do
-    create(:legacy_appeal, vacols_case: create(:case, bfcorlid: "123456789S", case_issues: [create(:case_issue)]))
-  end
-  let(:appeal_with_no_issues) do
-    create(:legacy_appeal, vacols_case: create(:case, bfcorlid: "123456788S", case_issues: []))
-  end
-
-  describe "Appeals Find by Veteran ID Endpoint" do
-    let(:headers) { { "HTTP_VETERAN_ID": "111225555S" } }
-
-    it "passes the request " do
-      headers["HTTP_VETERAN_ID"] = appeal[:vbms_id]
-      get "/reader/appeal/veteran-id", params: nil, headers: headers
-      expect(response).to have_http_status(:success)
-    end
-
-    it "doesn't return appeals without issues" do
-      headers["HTTP_VETERAN_ID"] = appeal_with_no_issues[:vbms_id]
-      get "/reader/appeal/veteran-id", params: nil, headers: headers
-      expect(JSON.parse(response.body)["appeals"].size).to be(0)
-    end
-  end
 
   describe "Appeals controller #show in html" do
     it "redirects the page to Case page" do
