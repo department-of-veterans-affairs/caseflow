e18cc275cb9969dbda0b48894cda670a9ac93ec8
Kenshiro Nakagawa <kencheeto@gmail.com>
2018-12-20 15:43:43 -0800

    add coverage for SCs and refactor

--- spec/feature/intake/intake_edit_confirmation_spec.rb
+++ spec/feature/intake/intake_edit_confirmation_spec.rb
@@ -10,11 +10,7 @@ RSpec.feature "Intake Edit Confirmation" do
   let!(:current_user) { User.authenticate!(roles: ["Mail Intake"]) }
   let!(:intake) { create(:intake, :completed, detail: decision_review, user_id: current_user.id) }
 
-  describe "given a HLR with a rating request issue" do
-    let(:decision_review) do
-      create(:higher_level_review, veteran_file_number: create(:veteran).file_number)
-    end
-
+  describe "given a claim review with a rating request issue" do
     let(:rating) do
       Generators::Rating.build(
         participant_id: decision_review.veteran.participant_id,
@@ -39,61 +35,72 @@ RSpec.feature "Intake Edit Confirmation" do
       decision_review.establish!
     end
 
-    it "confirms that an EP is being established" do
-      visit "higher_level_reviews/#{get_claim_id(decision_review)}/edit"
-      click_intake_add_issue
-      click_intake_no_matching_issues
-      add_intake_nonrating_issue(date: (decision_review.receipt_date - 1.month).strftime("%m/%d/%Y"))
-      click_edit_submit
-      click_number_of_issues_changed_confirmation
+    [:higher_level_review, :supplemental_claim].each do |claim_review_type|
+      describe "given the claim review is a #{claim_review_type}" do
+        let(:decision_review) do
+          create(claim_review_type, veteran_file_number: create(:veteran).file_number)
+        end
+        let(:edit_path_base) { claim_review_type.to_s.pluralize }
 
-      expect(page).to have_current_path(
-        "/higher_level_reviews/#{get_claim_id(decision_review)}/edit/confirmation"
-      )
-      expect(page).to have_content("A Higher-Level Review Nonrating EP is being established")
-    end
+        it "confirms that an EP is being established" do
+          visit "#{edit_path_base}/#{get_claim_id(decision_review)}/edit"
+          click_intake_add_issue
+          click_intake_no_matching_issues
+          add_intake_nonrating_issue(date: (decision_review.receipt_date - 1.month).strftime("%m/%d/%Y"))
+          click_edit_submit
+          click_number_of_issues_changed_confirmation
 
-    it "shows when an EP is being canceled" do
-      visit "higher_level_reviews/#{get_claim_id(decision_review)}/edit"
-      # first add a nonrating issue so we can remove the rating issue & EP
-      click_intake_add_issue
-      click_intake_no_matching_issues
-      add_intake_nonrating_issue(date: (decision_review.receipt_date - 1.month).strftime("%m/%d/%Y"))
-      click_remove_intake_issue(1)
-      click_remove_issue_confirmation
-      click_edit_submit
+          expect(page).to have_current_path(
+            "/#{edit_path_base}/#{get_claim_id(decision_review)}/edit/confirmation"
+          )
+          expect(page).to have_content("A #{decision_review.class.review_title} Nonrating EP is being established")
+        end
 
-      expect(page).to have_current_path(
-        "/higher_level_reviews/#{get_claim_id(decision_review)}/edit/confirmation"
-      )
-      expect(page).to have_content("A Higher-Level Review Rating EP is being canceled")
-    end
+        it "shows when an EP is being canceled" do
+          visit "#{edit_path_base}/#{get_claim_id(decision_review)}/edit"
+          # first add a nonrating issue so we can remove the rating issue & EP
+          click_intake_add_issue
+          click_intake_no_matching_issues
+          add_intake_nonrating_issue(date: (decision_review.receipt_date - 1.month).strftime("%m/%d/%Y"))
+          click_remove_intake_issue(1)
+          click_remove_issue_confirmation
+          click_edit_submit
 
-    it "shows when an EP is being updated" do
-      visit "higher_level_reviews/#{get_claim_id(decision_review)}/edit"
-      click_intake_add_issue
-      add_intake_rating_issue("Left knee granted")
-      click_edit_submit
-      click_number_of_issues_changed_confirmation
+          expect(page).to have_current_path(
+            "/#{edit_path_base}/#{get_claim_id(decision_review)}/edit/confirmation"
+          )
+          expect(page).to have_content("A #{decision_review.class.review_title} Rating EP is being canceled")
+        end
 
-      expect(page).to have_current_path(
-        "/higher_level_reviews/#{get_claim_id(decision_review)}/edit/confirmation"
-      )
-      expect(page).to have_content("Contentions on Higher-Level Review Rating EP are being updated")
-    end
+        it "shows when an EP is being updated" do
+          visit "#{edit_path_base}/#{get_claim_id(decision_review)}/edit"
+          click_intake_add_issue
+          add_intake_rating_issue("Left knee granted")
+          click_edit_submit
+          click_number_of_issues_changed_confirmation
 
-    it "includes warnings about unidentified issues" do
-      visit "higher_level_reviews/#{get_claim_id(decision_review)}/edit"
-      click_intake_add_issue
-      add_intake_unidentified_issue
-      click_edit_submit
-      click_still_have_unidentified_issue_confirmation
-      click_number_of_issues_changed_confirmation
+          expect(page).to have_current_path(
+            "/#{edit_path_base}/#{get_claim_id(decision_review)}/edit/confirmation"
+          )
+          expect(page).to have_content(
+            "Contentions on #{decision_review.class.review_title} Rating EP are being updated"
+          )
+        end
 
-      expect(page).to have_current_path(
-        "/higher_level_reviews/#{get_claim_id(decision_review)}/edit/confirmation"
-      )
-      expect(page).to have_content("There is still an unidentified issue")
+        it "includes warnings about unidentified issues" do
+          visit "#{edit_path_base}/#{get_claim_id(decision_review)}/edit"
+          click_intake_add_issue
+          add_intake_unidentified_issue
+          click_edit_submit
+          click_still_have_unidentified_issue_confirmation
+          click_number_of_issues_changed_confirmation
+
+          expect(page).to have_current_path(
+            "/#{edit_path_base}/#{get_claim_id(decision_review)}/edit/confirmation"
+          )
+          expect(page).to have_content("There is still an unidentified issue")
+        end
+      end
     end
   end
 end
