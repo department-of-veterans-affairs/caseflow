b80a5838aaaf4034ab9c66eaa373e186f4ab0090
Ricardo Kreyhsig <44415076+rkreyhsig@users.noreply.github.com>
2018-12-26 16:25:08 -0500

    Add VLJ and Attorney working case to Universal Case Title (PR) (#8426)
    Resolves #8289 - Add VLJ and Attorney working case to Universal Case Title
    
    | Before | After |
    | --- | --- |
    |![screen shot 2018-12-26 at 1 46 55 pm](https://user-images.githubusercontent.com/44415076/50454335-d770ca80-0914-11e9-8c2d-c9212889c22a.png)|![screen shot 2018-12-26 at 1 46 28 pm](https://user-images.githubusercontent.com/44415076/50454338-dcce1500-0914-11e9-8422-a44670145a81.png)|

--- spec/feature/queue/case_details_spec.rb
+++ spec/feature/queue/case_details_spec.rb
@@ -578,4 +578,29 @@ RSpec.feature "Case details" do
       end
     end
   end
+
+  describe "VLJ and Attorney working case in Universal Case Title" do
+    let(:attorney_user) { FactoryBot.create(:user) }
+    let(:judge_user) { FactoryBot.create(:user) }
+    let(:root_task) { FactoryBot.create(:root_task) }
+    let(:appeal) { root_task.appeal }
+    let!(:atty_task) do
+      FactoryBot.create(:ama_attorney_task, appeal: appeal, parent: root_task, assigned_by: judge_user,
+                                            assigned_to: attorney_user)
+    end
+    let!(:judge_task) do
+      FactoryBot.create(:ama_judge_task, appeal: appeal, parent: atty_task, assigned_by: judge_user,
+                                         assigned_to: judge_user)
+    end
+
+    context "Attorney has been assigned" do
+      it "is displayed in the Universal Case Title" do
+        visit "/queue/appeals/#{appeal.uuid}"
+        expect(page).to have_content(COPY::TASK_SNAPSHOT_ASSIGNED_JUDGE_LABEL)
+        expect(page).to have_content(judge_user.full_name)
+        expect(page).to have_content(COPY::TASK_SNAPSHOT_ASSIGNED_ATTORNEY_LABEL)
+        expect(page).to have_content(attorney_user.full_name)
+      end
+    end
+  end
 end
