d0242f6e778631b27619e1e633bfc1f521f1d6ec
lowellrex <lowell@navahq.com>
2018-12-27 07:52:40 -0500

    Consolidate Queue classes

--- app/models/queues/generic_queue.rb
+++ app/models/queues/generic_queue.rb
@@ -3,17 +3,21 @@ class GenericQueue
 
   attr_accessor :user
 
+  # rubocop:disable Style/SymbolProc
   def tasks
-    relevant_tasks.where(assigned_to: user)
+    (relevant_tasks + relevant_attorney_tasks).each { |t| t.update_if_hold_expired! }
   end
-
-  def tasks_by_appeal_id(appeal_id, appeal_type)
-    relevant_tasks.where(appeal_id: appeal_id, appeal_type: appeal_type)
-  end
+  # rubocop:enable Style/SymbolProc
 
   private
 
   def relevant_tasks
-    Task.incomplete_or_recently_completed
+    Task.incomplete_or_recently_completed.where(assigned_to: user)
+  end
+
+  def relevant_attorney_tasks
+    # If the user is a judge there will be attorneys in the list, if the user is not a judge the list of attorneys will
+    # be an empty set and this function will also then return an empty set.
+    AttorneyTask.incomplete_or_recently_completed.where(assigned_to: Judge.new(user).attorneys)
   end
 end
