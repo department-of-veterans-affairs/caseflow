3c4e24b766ba28e5db73f5ce8eb9b2b79bcdaeff
lowellrex <lowell@navahq.com>
2018-12-27 08:17:03 -0500

    Only fetch attorney tasks for people users, not organizations

--- app/models/queues/generic_queue.rb
+++ app/models/queues/generic_queue.rb
@@ -16,6 +16,8 @@ class GenericQueue
   end
 
   def relevant_attorney_tasks
+    return [] unless user.is_a?(User)
+
     # If the user is a judge there will be attorneys in the list, if the user is not a judge the list of attorneys will
     # be an empty set and this function will also then return an empty set.
     AttorneyTask.incomplete_or_recently_completed.where(assigned_to: Judge.new(user).attorneys)
