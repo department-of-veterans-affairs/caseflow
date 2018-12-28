49577c0ef4e89fdf208c6f4d6ace77e4004aec9a
Peter Karman <peter@truss.works>
2018-12-28 16:22:59 -0600

    Use named and relative dates rather than hardcoded strings

--- spec/feature/intake/appeal_spec.rb
+++ spec/feature/intake/appeal_spec.rb
@@ -1,7 +1,6 @@
-require "rails_helper"
 require "support/intake_helpers"
 
-RSpec.feature "Appeal Intake" do
+feature "Appeal Intake" do
   include IntakeHelpers
 
   before do
@@ -11,7 +10,7 @@ RSpec.feature "Appeal Intake" do
     FeatureToggle.enable!(:intake_legacy_opt_in, users: [current_user.css_id])
 
     Time.zone = "America/New_York"
-    Timecop.freeze(Time.utc(2018, 11, 28))
+    Timecop.freeze(post_ramp_start_date)
   end
 
   after do
@@ -41,11 +40,15 @@ RSpec.feature "Appeal Intake" do
                               participant_id: "44444444")
   end
 
-  let(:receipt_date) { Date.new(2018, 9, 20) }
+  let(:future_date) { Time.zone.now + 30.days }
+
+  let(:receipt_date) { post_ramp_start_date - 30.days }
 
   let(:untimely_days) { 372.days }
 
-  let(:profile_date) { Time.zone.local(2018, 9, 15) }
+  let(:profile_date) { post_ramp_start_date - 35.days }
+
+  let(:untimely_date) { receipt_date - 373.days }
 
   let!(:rating) do
     Generators::Rating.build(
@@ -106,13 +109,13 @@ RSpec.feature "Appeal Intake" do
     click_on "Search"
     expect(page).to have_current_path("/intake/review_request")
 
-    fill_in "What is the Receipt Date of this form?", with: "12/15/2018"
+    fill_in "What is the Receipt Date of this form?", with: future_date.strftime("%D")
     click_intake_continue
 
     expect(page).to have_content("Receipt date cannot be in the future.")
     expect(page).to have_content("Please select an option.")
 
-    fill_in "What is the Receipt Date of this form?", with: "09/20/2018"
+    fill_in "What is the Receipt Date of this form?", with: receipt_date.strftime("%D")
 
     within_fieldset("Which review option did the Veteran request?") do
       find("label", text: "Evidence Submission", match: :prefer_exact).click
@@ -170,7 +173,7 @@ RSpec.feature "Appeal Intake" do
     add_intake_nonrating_issue(
       category: "Active Duty Adjustments",
       description: "Description for Active Duty Adjustments",
-      date: "10/27/2018"
+      date: profile_date.strftime("%D")
     )
 
     expect(page).to have_content("2 issues")
@@ -190,7 +193,7 @@ RSpec.feature "Appeal Intake" do
     expect(appeal.request_issues.count).to eq 2
     expect(appeal.request_issues.first).to have_attributes(
       rating_issue_reference_id: "def456",
-      rating_issue_profile_date: profile_date,
+      rating_issue_profile_date: profile_date.asctime.in_time_zone(Time.zone),
       description: "PTSD denied",
       decision_date: nil,
       benefit_type: "compensation"
@@ -201,7 +204,7 @@ RSpec.feature "Appeal Intake" do
       rating_issue_profile_date: nil,
       issue_category: "Active Duty Adjustments",
       description: "Description for Active Duty Adjustments",
-      decision_date: 1.month.ago.to_date,
+      decision_date: profile_date,
       benefit_type: "compensation"
     )
   end
@@ -212,7 +215,7 @@ RSpec.feature "Appeal Intake" do
 
     visit "/intake"
 
-    fill_in "What is the Receipt Date of this form?", with: "04/20/2018"
+    fill_in "What is the Receipt Date of this form?", with: receipt_date.strftime("%D")
 
     within_fieldset("Which review option did the Veteran request?") do
       find("label", text: "Evidence Submission", match: :prefer_exact).click
@@ -407,7 +410,7 @@ RSpec.feature "Appeal Intake" do
     add_intake_nonrating_issue(
       category: "Active Duty Adjustments",
       description: "Description for Active Duty Adjustments",
-      date: "10/27/2018"
+      date: profile_date.strftime("%D")
     )
     expect(page).to have_content("2 issues")
 
@@ -457,7 +460,7 @@ RSpec.feature "Appeal Intake" do
     add_intake_nonrating_issue(
       category: "Active Duty Adjustments",
       description: "Another Description for Active Duty Adjustments",
-      date: "04/19/2016"
+      date: untimely_date.strftime("%D")
     )
     add_untimely_exemption_response("No", "I am an untimely exemption")
     expect(page).to have_content("6 issues")
@@ -486,7 +489,7 @@ RSpec.feature "Appeal Intake" do
     add_intake_nonrating_issue(
       category: "Drill Pay Adjustments",
       description: "A nonrating issue before AMA",
-      date: "10/19/2017"
+      date: pre_ramp_start_date.strftime("%D")
     )
     expect(page).to have_content(
       "A nonrating issue before AMA #{Constants.INELIGIBLE_REQUEST_ISSUES.before_ama}"
@@ -528,11 +531,10 @@ RSpec.feature "Appeal Intake" do
     )).to_not be_nil
 
     active_duty_adjustments_request_issue = RequestIssue.find_by!(
-      review_request_type: "Appeal",
-      review_request_id: appeal.id,
+      review_request: appeal,
       issue_category: "Active Duty Adjustments",
       description: "Description for Active Duty Adjustments",
-      decision_date: 1.month.ago
+      decision_date: profile_date
     )
 
     expect(active_duty_adjustments_request_issue.untimely?).to eq(false)
@@ -572,6 +574,12 @@ RSpec.feature "Appeal Intake" do
              review_request: appeal,
              description: "A nonrating issue before AMA",
              ineligible_reason: :before_ama
+    )).to_not be_nil
+
+    expect(RequestIssue.find_by(
+             review_request: appeal,
+             description: "A nonrating issue before AMA",
+             decision_date: pre_ramp_start_date
     )).to_not be_nil
 
     duplicate_request_issues = RequestIssue.where(rating_issue_reference_id: duplicate_reference_id)
@@ -670,7 +678,7 @@ RSpec.feature "Appeal Intake" do
         add_intake_nonrating_issue(
           category: "Active Duty Adjustments",
           description: "Description for Active Duty Adjustments",
-          date: "10/25/2017",
+          date: profile_date.strftime("%D"),
           legacy_issues: true
         )
 
