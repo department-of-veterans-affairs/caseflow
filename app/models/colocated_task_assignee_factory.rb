8560376f9b78745390de26d25ec872ccf754aa91
lowellrex <lowell@navahq.com>
2019-01-02 11:40:45 -0500

    Fix rubocop nits

--- app/models/colocated_task_assignee_factory.rb
+++ app/models/colocated_task_assignee_factory.rb
@@ -1,5 +1,6 @@
 class ColocatedTaskAssigneeFactory < RoundRobinTaskAssigneeFactory
-  def initialize(list_of_assignees: Colocated.singleton.non_admins.sort_by(&:id).pluck(:css_id), task_type: ColocatedTask)
+  def initialize(list_of_assignees: Colocated.singleton.non_admins.sort_by(&:id).pluck(:css_id),
+                 task_type: ColocatedTask)
     super
   end
 end
