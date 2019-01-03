8ac5eeb325bc7acdca24b46c44525254f8c39e21
Jia Huang <jialiya.huang0@gmail.com>
2019-01-03 12:50:09 -0800

    Edit request issues by id (#8405)
    Does the following:
    * Going to intake/edit sends previously added request issues ids. If these ids exist, don't create a new issue. If these ids do not exist (such as when removing or adding a new issue), remove/create the issue 
    * Fixes a bug where if you have an existing request issue A contesting a decision issue and save via the edit page, request issue A gets removed and readded.
    * removed some extra js code used for the old intake edit page

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -204,6 +204,7 @@ class Appeal < DecisionReview
       issue.update!(benefit_type: benefit_type, veteran_participant_id: veteran.participant_id)
       issue.create_legacy_issue_optin if issue.legacy_issue_opted_in?
     end
+    request_issues.reload
   end
 
   def serializer_class
