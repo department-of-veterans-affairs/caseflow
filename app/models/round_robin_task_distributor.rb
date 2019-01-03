024e1f7a9ee4747c372d07df2ab5af58828cb098
lowellrex <lowell@navahq.com>
2019-01-03 12:27:34 -0500

    Use ActiveModel

--- app/models/round_robin_task_distributor.rb
+++ app/models/round_robin_task_distributor.rb
@@ -1,11 +1,10 @@
 class RoundRobinTaskDistributor
-  def initialize(list_of_assignees:, task_type:)
-    @list_of_assignees = list_of_assignees
-    @task_type = task_type
-  end
+  include ActiveModel::Model
+
+  attr_accessor :list_of_assignees, :task_type
 
   def latest_task
-    @task_type.where(assigned_to_type: User.name).order("created_at").last
+    task_type.where(assigned_to_type: User.name).order("created_at").last
   end
 
   def last_assignee_css_id
@@ -13,21 +12,21 @@ class RoundRobinTaskDistributor
   end
 
   def last_assignee_index
-    @list_of_assignees.index(last_assignee_css_id)
+    list_of_assignees.index(last_assignee_css_id)
   end
 
   def next_assignee_index
     return 0 unless last_assignee_css_id
     return 0 unless last_assignee_index
-    (last_assignee_index + 1) % @list_of_assignees.length
+    (last_assignee_index + 1) % list_of_assignees.length
   end
 
   def next_assignee_css_id
-    if @list_of_assignees.blank?
+    if list_of_assignees.blank?
       fail Caseflow::Error::RoundRobinTaskDistributorError, message: "list_of_assignees cannot be empty"
     end
 
-    @list_of_assignees[next_assignee_index]
+    list_of_assignees[next_assignee_index]
   end
 
   def next_assignee
