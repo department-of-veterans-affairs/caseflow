414a1b69af8c190e2858e8f67a1a1894415f0090
Mark Benjamin <mdbenjam@gmail.com>
2019-01-07 16:20:08 -0500

    Add veteran ahl job (#8428)
    Resolves #8414 and merges #8402
    
    ### Description
    - Adds `VADotGovService`
    - Adds `FetchHearingLocationsForVeteransJob`

---
+++ app/services/external_api/va_dot_gov_service.rb
@@ -0,0 +1,184 @@
+require "json"
+
+class ExternalApi::VADotGovService
+  class << self
+    # :nocov:
+    def get_distance(lat:, long:, ids:)
+      page = 1
+      facility_results = []
+      remaining_ids = ids
+
+      until remaining_ids.empty?
+        results = fetch_facilities_with_ids(
+          query: { lat: lat, long: long, page: page },
+          ids: remaining_ids
+        )
+
+        remaining_ids -= results[:facilities].pluck(:id)
+        facility_results += results[:facilities]
+
+        break if !results[:has_next]
+
+        page += 1
+        sleep 1
+      end
+
+      unless remaining_ids.empty?
+        msg = "Unable to find api.va.gov facility data for: #{remaining_ids.join(', ')}."
+        fail Caseflow::Error::VaDotGovAPIError, code: 500, message: msg
+      end
+
+      track_pages(page)
+
+      facility_results
+    end
+
+    # rubocop:disable Metrics/ParameterLists
+    def geocode(
+        address_line1:, address_line2: nil,
+        address_line3: nil, city:, state:, zip_code:, country:
+      )
+      # rubocop:enable Metrics/ParameterLists
+      response = send_va_dot_gov_request(
+        body: geocode_body(
+          address_line1: address_line1, address_line2: address_line2,
+          address_line3: address_line3, city: city,
+          state: state, zip_code: zip_code, country: country
+        ),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        endpoint: address_validation_endpoint,
+        method: :post
+      )
+
+      resp_body = JSON.parse(response.body)
+      check_for_error(response_body: resp_body, code: response.code)
+
+      [resp_body["geocode"]["latitude"], resp_body["geocode"]["longitude"]]
+    end
+
+    private
+
+    def base_url
+      ENV["VA_DOT_GOV_API_URL"] || ""
+    end
+
+    def facilities_endpoint
+      "va_facilities/v0/facilities"
+    end
+
+    def address_validation_endpoint
+      "address_validation/v1/validate"
+    end
+
+    # rubocop:disable Metrics/ParameterLists
+    def geocode_body(
+        address_line1:, address_line2: nil,
+        address_line3: nil, city:, state:, zip_code:, country:
+      )
+      # rubocop:enable Metrics/ParameterLists
+      {
+        requestAddress: {
+          addressLine1: address_line1,
+          addressLine2: address_line2,
+          addressLine3: address_line3,
+          city: city,
+          stateProvince: {
+            code: state
+          },
+          requestCountry: {
+            country_code: country
+          },
+          zipCode5: zip_code
+        }
+      }
+    end
+
+    def facility_json(facility, distance)
+      attrs = facility["attributes"]
+      dist = distance["distance"] if distance
+
+      {
+        id: facility["id"],
+        type: facility["type"],
+        facility_type: attrs["facility_type"],
+        name: attrs["name"],
+        classification: attrs["classification"],
+        address: attrs["address"]["physical"],
+        lat: attrs["lat"],
+        long: attrs["long"],
+        distance: dist
+      }
+    end
+
+    def fetch_facilities_with_ids(query:, ids:)
+      response = send_va_dot_gov_request(
+        query: query,
+        endpoint: facilities_endpoint
+      )
+      resp_body = JSON.parse(response.body)
+      check_for_error(response_body: resp_body, code: response.code)
+
+      facilities = resp_body["data"]
+      distances = resp_body["meta"]["distances"]
+      has_next = !resp_body["links"]["next"].nil?
+      selected_facilities = facilities.select { |facility| ids.include? facility["id"] }
+
+      facilities_result = selected_facilities.map do |selected|
+        distance = distances.find { |dist| dist["id"] == selected["id"] }
+        facility_json(selected, distance)
+      end
+
+      { facilities: facilities_result, has_next: has_next }
+    end
+
+    def send_va_dot_gov_request(query: {}, headers: {}, endpoint:, method: :get, body: nil)
+      url = URI.escape(base_url + endpoint)
+      request = HTTPI::Request.new(url)
+      request.query = query
+      request.open_timeout = 30
+      request.read_timeout = 30
+      request.body = body.to_json unless body.nil?
+      request.headers = headers.merge(apikey: ENV["VA_DOT_GOV_API_KEY"])
+
+      MetricsService.record("api.va.gov #{method.to_s.upcase} request to #{url}",
+                            service: :va_dot_gov,
+                            name: endpoint) do
+        case method
+        when :get
+          HTTPI.get(request)
+        when :post
+          HTTPI.post(request)
+        end
+      end
+    end
+
+    def check_for_error(response_body:, code:)
+      case code
+      when 200 # rubocop:disable Lint/EmptyWhen
+      when 400
+        fail Caseflow::Error::VaDotGovRequestError, code: code, message: response_body
+      when 500
+        fail Caseflow::Error::VaDotGovServerError, code: code, message: response_body
+      else
+        msg = "Error: #{response_body}, HTTP code: #{code}"
+        fail Caseflow::Error::VaDotGovServerError, code: code, message: msg
+      end
+    end
+
+    def track_pages(pages)
+      DataDogService.emit_gauge(
+        metric_group: "service",
+        metric_name: "pages_requested",
+        metric_value: pages,
+        app_name: RequestStore[:application],
+        attrs: {
+          service: "va_dot_gov"
+        }
+      )
+    end
+    # :nocov:
+  end
+end
---
+++ lib/fakes/va_dot_gov_service.rb
@@ -0,0 +1,144 @@
+class Fakes::VADotGovService < ExternalApi::VADotGovService
+  def self.send_va_dot_gov_request(endpoint:, **_args)
+    if endpoint == facilities_endpoint
+      HTTPI::Response.new 200, {}, fake_facilities_data.to_json
+    elsif endpoint == address_validation_endpoint
+      HTTPI::Response.new 200, {}, fake_address_data.to_json
+    end
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def self.fake_address_data
+    {
+      "address": {
+        "county": {
+          "name": "Manassas Park City",
+          "countyFipsCode": "51685"
+        },
+        "stateProvince": {
+          "name": "Virginia",
+          "code": "VA"
+        },
+        "country": {
+          "name": "United States",
+          "code": "USA",
+          "fipsCode": "US",
+          "iso2Code": "US",
+          "iso3Code": "USA"
+        },
+        "addressLine1": "8633 Union Pl",
+        "city": "Manassas Park",
+        "zipCode5": "20111",
+        "zipCode4": "2434"
+      },
+      "geocode": {
+        "calcDate": "2019-01-03T17:33:57+00:00",
+        "locationPrecision": 31.0,
+        "latitude": 38.768185,
+        "longitude": -77.450033
+      },
+      "addressMetaData": {
+        "confidenceScore": 100.0,
+        "addressType": "Domestic",
+        "deliveryPointValidation": "CONFIRMED",
+        "residentialDeliveryIndicator": "RESIDENTIAL",
+        "validationKey": 113_008_568
+      }
+    }
+  end
+
+  def self.fake_facilities_data
+    # RO01
+    {
+      "data": [
+        {
+          "id": "vba_301",
+          "type": "va_facilities",
+          "attributes": {
+            "name": "Holdrege VA Clinic",
+            "facility_type": "va_health_facility",
+            "classification": "Primary Care CBOC",
+            "website": nil,
+            "lat": 40.4454392100001,
+            "long": -99.37959413,
+            "address": {
+              "mailing": {
+
+              },
+              "physical": {
+                "zip": "68949-1705",
+                "city": "Holdrege",
+                "state": "NE",
+                "address_1": "1118 Burlington Street",
+                "address_2": "",
+                "address_3": nil
+              }
+            },
+            "phone": {
+              "fax": "555-555-3775 x",
+              "main": "555-555-3760 x",
+              "pharmacy": "555-555-0827 x",
+              "after_hours": "555-555-5555 x",
+              "patient_advocate": "555-555-5555 x7933",
+              "mental_health_clinic": "555-555-5555",
+              "enrollment_coordinator": "555-555-5555 x"
+            },
+            "hours": {
+              "friday": "800AM-430PM",
+              "monday": "800AM-430PM",
+              "sunday": "-",
+              "tuesday": "800AM-430PM",
+              "saturday": "-",
+              "thursday": "800AM-430PM",
+              "wednesday": "800AM-430PM"
+            },
+            "services": {
+              "other": [
+                "Online Scheduling"
+              ],
+              "health": %w[
+                MentalHealthCare
+                PrimaryCare
+                Audiology
+                Cardiology
+              ],
+              "last_updated": "2019-01-02"
+            },
+            "satisfaction": {
+              "health": {
+
+              },
+              "effective_date": nil
+            },
+            "wait_times": {
+              "health": [],
+              "effective_date": "2018-12-24"
+            }
+          }
+        }
+      ],
+      "links": {
+        "self": "https://api.vets.gov/services/",
+        "first": "https://api.vets.gov/services/",
+        "prev": nil,
+        "next": nil,
+        "last": "https://api.vets.gov/services/"
+      },
+      "meta": {
+        "pagination": {
+          "current_page": 1,
+          "per_page": 30,
+          "total_pages": 77,
+          "total_entries": 2289
+        },
+        "distances": [
+          {
+            "id": "vba_301",
+            "distance": 60.07
+          }
+        ]
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
+end
