44ecc5367cb7282d2bc0b0b0750d8fa9a1c264ba
lowellrex <lowell@navahq.com>
2019-01-07 19:48:12 -0500

    Add test for mail task creation transaction block

--- spec/models/tasks/mail_task_spec.rb
+++ spec/models/tasks/mail_task_spec.rb
@@ -4,24 +4,25 @@ describe MailTask do
     let(:appeal) { FactoryBot.create(:appeal) }
     let(:mail_team) { MailTeam.singleton }
 
+    # Use AodMotionMailTask because we do create subclasses of MailTask, never MailTask itself.
+    let(:task_class) { AodMotionMailTask }
+    let(:params) { { appeal: appeal, parent_id: root_task_id, type: task_class.name } }
+
     before do
       OrganizationsUser.add_user_to_organization(user, mail_team)
     end
 
     context "when no root_task exists for appeal" do
-      let(:params) { { appeal: appeal, parent_id: nil } }
+      let(:root_task_id) { nil }
 
       it "throws an error" do
-        expect { MailTask.create_from_params(params, user) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { task_class.create_from_params(params, user) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
 
     context "when root_task exists for appeal" do
-      let!(:root_task) { FactoryBot.create(:root_task, appeal: appeal) }
-
-      # Use AodMotionMailTask because we do create subclasses of MailTask, never MailTask itself.
-      let(:task_class) { AodMotionMailTask }
-      let(:params) { { appeal: appeal, parent_id: root_task.id, type: task_class.name } }
+      let(:root_task) { FactoryBot.create(:root_task, appeal: appeal) }
+      let(:root_task_id) { root_task.id }
 
       it "creates AodMotionMailTask assigned to MailTeam and AodTeam" do
         expect { task_class.create_from_params(params, user) }.to_not raise_error
@@ -36,6 +37,20 @@ describe MailTask do
         expect(child_task.class).to eq(task_class)
         expect(child_task.assigned_to).to eq(AodTeam.singleton)
         expect(child_task.children.length).to eq(0)
+      end
+    end
+
+    context "when child task creation fails" do
+      let(:root_task) { FactoryBot.create(:root_task, appeal: appeal) }
+      let(:root_task_id) { root_task.id }
+
+      before do
+        allow(task_class).to receive(:create_child_task).and_raise(StandardError)
+      end
+
+      it "should not create any mail tasks" do
+        expect { task_class.create_from_params(params, user) }.to raise_error(StandardError)
+        expect(root_task.children.length).to eq(0)
       end
     end
   end
