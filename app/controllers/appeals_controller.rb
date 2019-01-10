0d89c4e1ab29a691148f4022e1b557ad04611ccf
hschallhorn <hschallhorn@tistatech.com>
2019-01-10 07:54:29 -0500

    Add error handling

--- app/controllers/appeals_controller.rb
+++ app/controllers/appeals_controller.rb
@@ -27,6 +27,8 @@ class AppealsController < ApplicationController
 
   def document_count
     render json: { document_count: appeal.number_of_documents_from_caseflow }
+  rescue StandardError => e
+    return handle_non_critical_error("document_count", e)
   end
 
   def new_documents
