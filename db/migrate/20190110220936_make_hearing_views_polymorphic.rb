403fe68d8ca645bc4b7d4a7b79e3cd8e920237b6
Sharon Warner <sharon.warner56@gmail.com>
2019-01-11 10:16:13 -0500

    Updating old records

--- db/migrate/20190110220936_make_hearing_views_polymorphic.rb
+++ db/migrate/20190110220936_make_hearing_views_polymorphic.rb
@@ -5,5 +5,9 @@ class MakeHearingViewsPolymorphic < ActiveRecord::Migration[5.1]
     add_column :hearing_views, :hearing_type, :string
     remove_index :hearing_views, [:hearing_id, :user_id]
     add_index :hearing_views, [:hearing_id, :user_id, :hearing_type], unique: true
+
+    HearingView.find_each do |hearing_view|
+      hearing_view.update!(hearing_type: "LegacyHearing")
+    end
   end
 end
