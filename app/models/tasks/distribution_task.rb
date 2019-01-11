11708d85e81a2c3a359cf0a610f53819ca2565ff
Alex Prokop <alex.prokop@gmail.com>
2019-01-11 05:02:18 -0500

    add comment

--- app/models/tasks/distribution_task.rb
+++ app/models/tasks/distribution_task.rb
@@ -1,7 +1,8 @@
 class DistributionTask < GenericTask
   # Prevent this task from being marked complete
   # when a child task (e.g. evidence submission)
-  # is marked complete.
+  # is marked complete because distribution tasks are used
+  # to signal that cases are ready for assignment to judges.
   def update_status_if_children_tasks_are_complete
     if children.any? && children.all? { |t| t.status == Constants.TASK_STATUSES.completed }
       update!(status: :assigned)
