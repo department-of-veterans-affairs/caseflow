32e92843b994a40a4558a9efc790a89e18e80643
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2019-01-11 11:45:11 -0500

    Update document_count to use Caseflow Documents table (#8564)
    References #8192 
    
    ### Acceptance Criteria
    - [x] Calls to `document_count` to not make calls to vacols or vbms
    - [x] `document_count` returns the correct number of documents

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -135,8 +135,15 @@ class LegacyAppeal < ApplicationRecord
     )
   end
 
-  delegate :documents, :number_of_documents, :new_documents_for_user,
+  delegate :documents, :new_documents_for_user, :number_of_documents,
            :manifest_vbms_fetched_at, :manifest_vva_fetched_at, to: :document_fetcher
+
+  # Number of documents stored locally via nightly RetrieveDocumentsForReaderJob.
+  # Fall back to count from VBMS if no local documents are found.
+  def number_of_documents_from_caseflow
+    count = Document.where(file_number: veteran_file_number).size
+    (count != 0) ? count : number_of_documents
+  end
 
   def number_of_documents_after_certification
     return 0 unless certification_date
