53ef0861dbfe9b00b3a48f5fd0e148026a262380
hschallhorn <hschallhorn@tistatech.com>
2019-01-11 11:29:02 -0500

    Clarify method

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -49,6 +49,8 @@ class Appeal < DecisionReview
   delegate :documents, :manifest_vbms_fetched_at, :number_of_documents,
            :new_documents_for_user, :manifest_vva_fetched_at, to: :document_fetcher
 
+  # Number of documents stored locally via nightly RetrieveDocumentsForReaderJob.
+  # Fall back to count from VBMS if no local documents are found.
   def number_of_documents_from_caseflow
     count = Document.where(file_number: veteran_file_number).size
     (count != 0) ? count : number_of_documents
--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -138,6 +138,8 @@ class LegacyAppeal < ApplicationRecord
   delegate :documents, :new_documents_for_user, :number_of_documents,
            :manifest_vbms_fetched_at, :manifest_vva_fetched_at, to: :document_fetcher
 
+  # Number of documents stored locally via nightly RetrieveDocumentsForReaderJob.
+  # Fall back to count from VBMS if no local documents are found.
   def number_of_documents_from_caseflow
     count = Document.where(file_number: veteran_file_number).size
     (count != 0) ? count : number_of_documents
