10820eac16a6988ea8792a054c23d90337f0dce4
Alex Prokop <alex.prokop@gmail.com>
2019-01-14 15:31:16 -0500

    fix spec

--- app/models/concerns/timeable_task.rb
+++ app/models/concerns/timeable_task.rb
@@ -2,15 +2,6 @@ module TimeableTask
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def create(args)
-      fail Caseflow::Error::MissingTimerMethod unless method_defined?(:when_timer_ends)
-      fail Caseflow::Error::MissingTimerMethod unless respond_to?(:timer_delay)
-
-      super(args).tap do |task|
-        TaskTimer.create!(task: task, submitted_at: Time.zone.now + timer_delay)
-      end
-    end
-
     def create!(args)
       fail Caseflow::Error::MissingTimerMethod unless method_defined?(:when_timer_ends)
       fail Caseflow::Error::MissingTimerMethod unless respond_to?(:timer_delay)
--- spec/models/concerns/timeable_task_spec.rb
+++ spec/models/concerns/timeable_task_spec.rb
@@ -26,19 +26,16 @@ describe TimeableTask do
 
   it "creates a task timer with the correct delay when we create some timed task" do
     task = SomeTimedTask.create!(appeal: appeal, assigned_to: Bva.singleton)
-    another_task = SomeTimedTask.create(appeal: create(:appeal), assigned_to: Bva.singleton)
     timers = TaskTimer.where(task: task)
-    other_timers = TaskTimer.where(task: another_task)
     expect(timers.length).to eq(1)
     expect(timers.first.submitted_at).to eq(Time.zone.today + 5.days)
-    expect(other_timers.first.submitted_at).to eq(Time.zone.today + 5.days)
   end
 
   context "when not correctly configured" do
     subject { AnotherTimedTask.create!(appeal: appeal, assigned_to: Bva.singleton) }
 
     it "errors if we do not pass a timer delay" do
-      expect { subject }.to raise_error(Caseflow::Error::MissingTimerInfo)
+      expect { subject }.to raise_error(Caseflow::Error::MissingTimerMethod)
     end
   end
 end
