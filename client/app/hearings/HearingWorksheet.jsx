f70a596a7d085eda4ef0988e5d2920ba8b4d85db
Sharon Warner <sharon.warner56@gmail.com>
2019-01-14 10:09:11 -0500

    Linting updates

--- client/app/hearings/HearingWorksheet.jsx
+++ client/app/hearings/HearingWorksheet.jsx
@@ -118,7 +118,7 @@ export class HearingWorksheet extends React.PureComponent {
     return <div>
       <HearingWorksheetDocs {...this.props} />
       <HearingWorksheetStream {...this.props} print={this.props.print} />
-    </div>
+    </div>;
   };
 
   getHearingWorksheet = () => {
@@ -134,7 +134,7 @@ export class HearingWorksheet extends React.PureComponent {
 
     const firstWorksheetPage = <div className="cf-hearings-first-page">
       {worksheetHeader}
-      {this.props.worksheet.docket_name === "hearing" ? this.getHearingWorksheet() : this.getLegacyHearingWorksheet()}
+      {this.props.worksheet.docket_name === 'hearing' ? this.getHearingWorksheet() : this.getLegacyHearingWorksheet()}
       {this.props.print &&
         <WorksheetFooter
           veteranName={this.props.worksheet.veteran_fi_last_formatted}
