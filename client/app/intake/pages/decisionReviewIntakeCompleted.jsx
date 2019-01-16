a7fbee21c1f22e8ac81a9e07378ef71e096c3037
Sally Maki <sallymaki@gmail.com>
2019-01-16 11:53:39 -0800

    decisionReviewIntakeCompleted page

--- client/app/intake/pages/decisionReviewIntakeCompleted.jsx
+++ client/app/intake/pages/decisionReviewIntakeCompleted.jsx
@@ -14,28 +14,33 @@ import { LOGO_COLORS } from '../../constants/AppConstants';
 
 const leadMessageList = ({ veteran, formName, requestIssues }) => {
   const unidentifiedIssues = requestIssues.filter((ri) => ri.isUnidentified);
+  const eligibleRequestIssues = requestIssues.filter((ri) => !ri.ineligibleReason);
 
-  if (unidentifiedIssues.length === 0) {
-    return [
-      `${veteran.name}'s (ID #${veteran.fileNumber}) ` +
-        `Request for ${formName} has been processed. ` +
-        'If you need to edit this, go to VBMS claim details and click the “Edit in Caseflow” button.',
+  const leadMessageArr = [`${veteran.name}'s (ID #${veteran.fileNumber}) Request for ${formName} has been processed.`]
+
+  if (eligibleRequestIssues.length !== 0) {
+    if (unidentifiedIssues.length !== 0) {
+      leadMessageArr.push(
+        <Alert type="warning">
+          <h2>Unidentified issue</h2>
+          <p>There is still an unidentified issue that needs to be resolved before sending the notice
+          letter. To edit, go to VBMS claim details and click the “Edit in Caseflow” button.</p>
+          {unidentifiedIssues.map((ri, i) => <p className="cf-red-text" key={`unidentified-alert-${i}`}>
+            Unidentified issue: no issue matched for requested "{ri.description}"
+          </p>)}
+        </Alert>
+      )
+    } else {
+      leadMessageArr.push(
+        'If you need to edit this, go to VBMS claim details and click the “Edit in Caseflow” button.'
+      )
+    }
+    leadMessageArr.push(
       <strong>Edit the notice letter to reflect the status of requested issues.</strong>
-    ];
-  }
+    )
+  };
 
-  return [
-    `${veteran.name}'s (ID #${veteran.fileNumber}) Request for ${formName} has been processed.`,
-    <Alert type="warning">
-      <h2>Unidentified issue</h2>
-      <p>There is still an unidentified issue that needs to be resolved before sending the notice
-      letter. To edit, go to VBMS claim details and click the “Edit in Caseflow” button.</p>
-      {unidentifiedIssues.map((ri, i) => <p className="cf-red-text" key={`unidentified-alert-${i}`}>
-        Unidentified issue: no issue matched for requested "{ri.description}"
-      </p>)}
-    </Alert>,
-    <strong>Edit the notice letter to reflect the status of requested issues.</strong>
-  ];
+  return leadMessageArr;
 };
 
 const getChecklistItems = (formType, requestIssues, isInformalConferenceRequested) => {
@@ -75,7 +80,7 @@ const getChecklistItems = (formType, requestIssues, isInformalConferenceRequeste
     </Fragment>);
   }
 
-  if (isInformalConferenceRequested) {
+  if (eligibleRequestIssues.length > 0 && isInformalConferenceRequested) {
     checklist.push('Informal Conference Tracked Item');
   }
 
