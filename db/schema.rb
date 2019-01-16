62684d9ec2bd6ae35cfe77afee74e000a39085d6
Andrew Lomax <andrewlomaxr@gmail.com>
2019-01-16 17:08:09 -0500

    no schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20190111000717) do
+ActiveRecord::Schema.define(version: 20190110162001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
