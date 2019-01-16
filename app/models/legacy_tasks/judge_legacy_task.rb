05ac0f6ca4486cf4658d0268371b648db46dc3e8
rkreyhsig <rkreyhsig@tistatech.com>
2019-01-16 10:27:34 -0500

    Adding timeline_title to judge_legacy_task model

--- app/models/legacy_tasks/judge_legacy_task.rb
+++ app/models/legacy_tasks/judge_legacy_task.rb
@@ -19,6 +19,10 @@ class JudgeLegacyTask < LegacyTask
     end
   end
 
+  def timeline_title
+    COPY::CASE_TIMELINE_JUDGE_TASK
+  end
+
   def self.from_vacols(record, appeal, user_id)
     task = super
     task.action = record.reassigned_to_judge_date.present? ? "review" : "assign"
