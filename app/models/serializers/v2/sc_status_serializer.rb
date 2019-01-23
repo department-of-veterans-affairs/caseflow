84a623978f95cad2e532da907cb5c7e3d8494b66
Carrasco <vacocarram@vacoltcarram4.dva.va.gov>
2019-01-22 19:55:45 -0500

    fixing some typos

---
+++ app/models/serializers/v2/sc_status_serializer.rb
@@ -0,0 +1,33 @@
+class SCStatusSerializer < V2::AppealSerializer
+  type :supplemental_claim
+
+  def id
+    object.review_status_id
+  end
+
+  attribute :linked_review_ids, key: :appeal_ids
+
+  attribute :type do
+    # this does not apply to SC
+  end
+
+  attribute :location do
+    # for SC will always be aoj
+    "aoj"
+  end
+
+  attribute :incomplete_history do
+    false
+  end
+
+  attribute :aod do
+    # does not apply to SC
+  end
+
+  attribute :docket do
+    # doesn't apply to SC
+  end
+
+  attribute :events do
+  end
+end
