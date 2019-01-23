cafe891350a9ab25029c94878d464f3b20fef4cd
Peter Karman <peter@truss.works>
2019-01-23 17:12:53 -0600

    simplify more sql

--- app/models/concerns/asyncable.rb
+++ app/models/concerns/asyncable.rb
@@ -82,7 +82,6 @@ module Asyncable
 
     def potentially_stuck
       processable
-        .or(expired_without_processing)
         .or(attempted_without_being_submitted)
         .order_by_oldest_submitted
     end
