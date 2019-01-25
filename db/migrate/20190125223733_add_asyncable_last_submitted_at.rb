00e4b788fca9eae2e93bf857575a718913643e8c
Peter Karman <peter@truss.works>
2019-01-25 17:34:17 -0600

    refactor migration to update data

--- db/migrate/20190125223733_add_asyncable_last_submitted_at.rb
+++ db/migrate/20190125223733_add_asyncable_last_submitted_at.rb
@@ -1,5 +1,5 @@
 class AddAsyncableLastSubmittedAt < ActiveRecord::Migration[5.1]
-  def change
+  def asyncable_tables
     [
       :request_issues,
       :board_grant_effectuations,
@@ -9,8 +9,22 @@ class AddAsyncableLastSubmittedAt < ActiveRecord::Migration[5.1]
       :appeals,
       :supplemental_claims,
       :higher_level_reviews
-    ].each do |tbl|
-      add_column tbl, :last_submitted_at, :datetime
+    ]
+  end
+
+  def up
+    asyncable_tables.each do |tbl|
+      safety_assured do
+        submitted_at_column = tbl.to_s.classify.constantize.submitted_at_column
+        add_column tbl, :last_submitted_at, :datetime
+        execute "UPDATE #{tbl} SET last_submitted_at=#{submitted_at_column}"
+      end
+    end
+  end
+
+  def down
+    asyncable_tables.each do |tbl|
+      remove_column tbl, :last_submitted_at
     end
   end
 end
