6b45010d5d572e6668289bf5bd139916bb9c293d
Jia Huang <jialiya.huang0@gmail.com>
2019-01-25 17:03:26 -0800

    allow assigned tasks to be completed

--- spec/models/tasks/board_grant_effectuation_task_spec.rb
+++ spec/models/tasks/board_grant_effectuation_task_spec.rb
@@ -1,11 +1,32 @@
 describe BoardGrantEffectuationTask do
-  let(:task) { create(:board_grant_effectuation_task).becomes(described_class) }
+  let(:task_status) { "assigned" }
+  let(:task) { create(:board_grant_effectuation_task, status: task_status).becomes(described_class) }
 
   context "#label" do
     subject { task.label }
 
     it "has a label of Board Grant" do
       expect(subject).to eq "Board Grant"
+    end
+  end
+
+  describe "#complete_with_payload!" do
+    subject { task.complete_with_payload!(nil, nil) }
+
+    context "assigned task" do
+      it "can be completed" do
+        expect(subject).to eq true
+        task.reload
+        expect(task.status).to eq "completed"
+      end
+    end
+
+    context "completed task" do
+      let(:task_status) { "completed" }
+
+      it "cannot be completed again" do
+        expect(subject).to eq false
+      end
     end
   end
 end
