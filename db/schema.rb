3c72b76a212ba0296ec709bd4c18ec9b33559a0e
Andrew Lomax <andrewlomaxr@gmail.com>
2019-01-28 16:05:15 -0500

    use master schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190122230514) do
+ActiveRecord::Schema.define(version: 20190125223733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
