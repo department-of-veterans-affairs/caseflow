45a1e29c15d6509c5723300702d92b7a75f10245
Shane Russell <shane1337@gmail.com>
2019-01-28 17:33:22 -0800

    Always use the contention disposition for decision issue sync

--- spec/models/request_issue_spec.rb
+++ spec/models/request_issue_spec.rb
@@ -927,16 +927,35 @@ describe RequestIssue do
                code: ep_code)
       end
 
+      let!(:contention) do
+        Generators::Contention.build(
+          id: contention_reference_id,
+          claim_id: end_product_establishment.reference_id,
+          disposition: "allowed"
+        )
+      end
+
       context "with rating ep" do
         context "when associated rating exists" do
           let(:associated_claims) { [{ clm_id: end_product_establishment.reference_id, bnft_clm_tc: ep_code }] }
 
           context "when matching rating issues exist" do
+            let!(:decision_issue_not_matching_disposition) do
+              create(
+                :decision_issue,
+                decision_review: review,
+                participant_id: veteran.participant_id,
+                disposition: "denied",
+                rating_issue_reference_id: contested_rating_issue_reference_id
+              )
+            end
+
             it "creates decision issues based on rating issues" do
               subject
               expect(rating_request_issue.decision_issues.count).to eq(1)
               expect(rating_request_issue.decision_issues.first).to have_attributes(
                 rating_issue_reference_id: contested_rating_issue_reference_id,
+                disposition: "allowed",
                 participant_id: veteran.participant_id,
                 promulgation_date: ratings.promulgation_date,
                 decision_text: "Left knee granted",
@@ -958,19 +977,30 @@ describe RequestIssue do
                 expect { subject }.to raise_error(RequestIssue::NilEndProductLastActionDate)
               end
             end
+
+            context "when decision issue with disposition and rating issue already exists" do
+              let!(:preexisting_decision_issue) do
+                create(
+                  :decision_issue,
+                  decision_review: review,
+                  participant_id: veteran.participant_id,
+                  disposition: "allowed",
+                  rating_issue_reference_id: contested_rating_issue_reference_id
+                )
+              end
+
+              it "links preexisting decision issue to request issue" do
+                subject
+                expect(rating_request_issue.decision_issues.count).to eq(1)
+                expect(rating_request_issue.decision_issues.first).to eq(preexisting_decision_issue)
+                expect(rating_request_issue.processed?).to eq(true)
+              end
+            end
           end
 
           context "when no matching rating issues exist" do
             let(:issues) do
               [{ reference_id: "xyz456", decision_text: "PTSD denied", contention_reference_id: "bad_id" }]
-            end
-
-            let!(:contention) do
-              Generators::Contention.build(
-                id: contention_reference_id,
-                claim_id: end_product_establishment.reference_id,
-                disposition: "allowed"
-              )
             end
 
             it "creates decision issues based on contention disposition" do
