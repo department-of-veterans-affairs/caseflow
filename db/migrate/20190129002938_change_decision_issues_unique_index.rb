bdc1eb45935842bc9d8c792fef5be5eaa41f2587
Shane Russell <shane1337@gmail.com>
2019-01-29 11:18:17 -0800

    update spacing

--- db/migrate/20190129002938_change_decision_issues_unique_index.rb
+++ db/migrate/20190129002938_change_decision_issues_unique_index.rb
@@ -2,15 +2,15 @@ class ChangeDecisionIssuesUniqueIndex < ActiveRecord::Migration[5.1]
   disable_ddl_transaction!
 
   def up
-	  remove_index :intakes, name: 'decision_issues_uniq_idx'
+    remove_index :intakes, name: 'decision_issues_uniq_idx'
 
-	  add_index(
-	  	:decision_issues,
-	  	[:rating_issue_reference_id, :disposition, :participant_id],
-	  	unique: true,
-	  	name: "decision_issues_uniq_by_disposition_and_ref_id",
-	  	algorithm: :concurrently
-	  )
+    add_index(
+      :decision_issues,
+      [:rating_issue_reference_id, :disposition, :participant_id],
+      unique: true,
+      name: "decision_issues_uniq_by_disposition_and_ref_id",
+      algorithm: :concurrently
+    )
   end
 
   def down
