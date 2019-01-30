fc4d60ad31b11dbf74a9492db6e74a2de99eb022
Andrew Lomax <andrewlomaxr@gmail.com>
2019-01-30 09:05:08 -0500

    option null when delete input value?

--- client/app/components/DataDropdowns/RegionalOffice.jsx
+++ client/app/components/DataDropdowns/RegionalOffice.jsx
@@ -71,7 +71,7 @@ class RegionalOfficeDropdown extends React.Component {
         strongLabel
         readOnly={readOnly}
         value={this.getSelectedOption()}
-        onChange={(option) => onChange(option.value, option.label)}
+        onChange={(option) => onChange((option || {}).value, (option || {}).label)}
         options={options}
         errorMessage={errorMessage}
         placeholder={placeholder} />
