e0a050d7b7709fbcb8807c58c07bba9527b01825
Sharon Warner <sharon.warner56@gmail.com>
2019-01-30 11:04:55 -0500

    Permissions updates

--- app/controllers/hearing_schedule_controller.rb
+++ app/controllers/hearing_schedule_controller.rb
@@ -1,10 +1,15 @@
 class HearingScheduleController < ApplicationController
   before_action :react_routed, :check_hearing_schedule_out_of_service
   before_action :verify_build_hearing_schedule_access, only: [:build_schedule_index]
-  before_action :verify_edit_hearing_schedule_access
+  before_action :verify_hearings_access, only: [:hearing_details_index]
+  before_action :verify_edit_hearing_schedule_access, except: [:hearing_details_index]
 
   def set_application
     RequestStore.store[:application] = "hearing_schedule"
+  end
+
+  def hearing_details_index
+    render "hearing_schedule/index"
   end
 
   def build_schedule_index
@@ -21,6 +26,10 @@ class HearingScheduleController < ApplicationController
 
   def verify_edit_hearing_schedule_access
     verify_authorized_roles("Edit HearSched", "Build HearSched")
+  end
+
+  def verify_hearings_access
+    verify_authorized_roles("Hearing Prep", "Edit HearSched", "Build HearSched")
   end
 
   def check_hearing_schedule_out_of_service
