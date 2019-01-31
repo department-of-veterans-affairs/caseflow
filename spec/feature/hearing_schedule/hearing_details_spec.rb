b3d580e3073d09960e0e76e5ddcd7a25a5f3c95e
Andrew Lomax <andrewlomaxr@gmail.com>
2019-01-31 13:56:44 -0500

    remove flaky test

--- spec/feature/hearing_schedule/hearing_details_spec.rb
+++ spec/feature/hearing_schedule/hearing_details_spec.rb
@@ -1,19 +1,6 @@
 require "rails_helper"
 
 RSpec.feature "Hearing Schedule Daily Docket" do
-  context "Hearing details is only editable for a hearings management user" do
-    let!(:current_user) do
-      OrganizationsUser.add_user_to_organization(create(:hearings_management), HearingsManagement.singleton)
-      User.authenticate!(css_id: "BVATWARNER", roles: ["Build HearSched"])
-    end
-    let!(:hearing) { create(:hearing) }
-
-    scenario "Fields are editable" do
-      visit "hearings/" + hearing.external_id.to_s + "/details"
-      field_labeled("Notes", disabled: false)
-    end
-  end
-
   context "Hearing details is not editable for a non-hearings management user" do
     let!(:current_user) { User.authenticate!(css_id: "BVATWARNER", roles: ["Build HearSched"]) }
     let!(:hearing) { create(:hearing) }
