6b039d36b5dafff54a7d4ee1269e2f24a22126a1
Mark Benjamin <mdbenjam@gmail.com>
2019-01-31 11:24:45 -0500

    Adidng cancel task modal

---
+++ client/app/queue/components/CancelTaskModal.jsx
@@ -0,0 +1,91 @@
+// @flow
+import * as React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { sprintf } from 'sprintf-js';
+
+import COPY from '../../../COPY.json';
+import CO_LOCATED_ADMIN_ACTIONS from '../../../constants/CO_LOCATED_ADMIN_ACTIONS.json';
+
+import {
+  taskById,
+  appealWithDetailSelector
+} from '../selectors';
+import { onReceiveAmaTasks } from '../QueueActions';
+import {
+  requestPatch
+} from '../uiReducer/uiActions';
+import editModalBase from './EditModalBase';
+import { taskActionData } from '../utils';
+
+import type { State } from '../types/state';
+import type { Task, Appeal } from '../types/models';
+
+type Params = {|
+  task: Task,
+  taskId: string,
+  appeal: Appeal,
+  appealId: string,
+  modalType: string,
+|};
+
+type Props = Params & {|
+  saveState: boolean,
+  history: Object,
+  requestPatch: typeof requestPatch,
+  onReceiveAmaTasks: typeof onReceiveAmaTasks
+|};
+
+class CompleteTaskModal extends React.Component<Props> {
+  submit = () => {
+    const {
+      task
+    } = this.props;
+    const payload = {
+      data: {
+        task: {
+          status: 'canceled'
+        }
+      }
+    };
+    const successMsg = {
+      title: taskActionData(this.props).message_title,
+      detail: taskActionData(this.props).message_detail
+    };
+
+    return this.props.requestPatch(`/tasks/${task.taskId}`, payload, successMsg).
+      then((resp) => {
+        const response = JSON.parse(resp.text);
+
+        this.props.onReceiveAmaTasks(response.tasks.data);
+      });
+  }
+
+  render = () => {
+    return <div>{taskActionData(this.props).modal_body}</div>;
+  };
+}
+
+const mapStateToProps = (state: State, ownProps: Params) => ({
+  task: taskById(state, { taskId: ownProps.taskId }),
+  appeal: appealWithDetailSelector(state, ownProps),
+  saveState: state.ui.saveState.savePending
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  requestPatch,
+  onReceiveAmaTasks
+}, dispatch);
+
+const propsToText = (props) => {
+  return {
+    title: taskActionData(props).modal_title
+  };
+};
+
+export default (withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(editModalBase(
+    CompleteTaskModal, { propsToText }
+  ))
+): React.ComponentType<Params>);
