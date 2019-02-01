3b92869c22d0688d3fde0b2a7e71ce7dada4acba
Alex Prokop <alex.prokop@gmail.com>
2019-02-01 16:10:18 -0500

    adds appeal and docket methods

---
+++ app/models/docket.rb
@@ -0,0 +1,73 @@
+class Docket
+  include ActiveModel::Model
+
+  def docket_type
+    fail Caseflow::Error::MustImplementInSubclass
+  end
+
+  def appeals(priority: nil, ready: nil)
+    fail "'ready for distribution' value cannot be false" if ready == false
+
+    scope = docket_appeals
+    scope = scope.merge(Appeal.ready_for_distribution) if ready == true
+    scope = scope.merge(Appeal.all_priority) if priority == true
+    scope = scope.merge(Appeal.all_nonpriority) if priority == false
+
+    if priority == true
+      scope.merge(Appeal.ordered_by_distribution_ready_date) 
+    else 
+      scope.order('receipt_date')
+    end
+  end
+
+  def count(priority: nil, ready: nil)
+    appeals(priority, ready).count
+  end
+
+  def weight
+    count
+  end
+
+  def age_of_n_oldest_priority_appeals(n)
+    appeals(priority: true, ready: true).limit(n).map(&:ready_for_distribution_at)
+  end
+
+  # CMGTODO: unique index on distributed_cases.case_id to prevent distributing the same appeal twice
+  # CMGTODO: update DistributedCase validation and add judge_task association
+  def distribute_appeals(distribution, priority: false, limit: 1)
+    transaction do
+      appeals = appeals(priority: priority, ready: true).limit(limit)
+
+      tasks = assign_judge_tasks_for_appeals(appeals, distribution.judge)
+
+      tasks.map do |task|
+        distribution.distributed_cases.create!(case_id: task.appeal.uuid,
+                                               docket: docket_type,
+                                               priority: priority,
+                                               ready_at: task.appeal.ready_for_distribution_at,
+                                               judge_task: task)
+      end
+    end
+  end
+
+  private
+
+  def docket_appeals
+    Appeal.active.where(docket_type: docket_type)
+  end
+
+  def assign_judge_tasks_for_appeals(appeals, judge)
+    appeals.map do |appeal|
+      Rails.logger.info("Assigning judge task for appeal #{appeal.id}")
+      task = JudgeTask.create!(appeal: appeal,
+                               parent: appeal.root_task,
+                               appeal_type: Appeal.name,
+                               assigned_at: Time.zone.now,
+                               assigned_to: judge,
+                               action: "assign")
+      Rails.logger.info("Assigned judge task with task id #{task.id} to #{task.assigned_to.css_id}")
+
+      task
+    end
+  end
+end
