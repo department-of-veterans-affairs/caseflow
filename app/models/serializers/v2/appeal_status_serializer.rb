b2d25f4cb664ab36b341b97514b890348bf34a53
Carrasco <vacocarram@VACOLTCARRAM4.local>
2019-02-03 15:01:20 -0500

    Made stand alone serializers for each claim and appeal. Renamed original serializer to reflect that it is for legacy appeals.

--- app/models/serializers/v2/appeal_status_serializer.rb
+++ app/models/serializers/v2/appeal_status_serializer.rb
@@ -1,4 +1,4 @@
-class V2::AppealStatusSerializer < V2::AppealSerializer
+class V2::AppealStatusSerializer < ActiveModel::Serializer
   type :appeal
 
   def id
@@ -6,33 +6,52 @@ class V2::AppealStatusSerializer < V2::AppealSerializer
   end
 
   attribute :linked_review_ids, key: :appeal_ids
-  attribute :advanced_on_docket, key: :aod
-  attribute :active_status?, key: :active
 
-  attribute :type do
-    "original"
+  attribute :updated do
+    Time.zone.now.in_time_zone("Eastern Time (US & Canada)").round.iso8601
   end
 
   attribute :incomplete_history do
     false
   end
 
+  attribute :type do
+    "original"
+  end
+
+  attribute :active_status?, key: :active
+  attribute :description
+  attribute :advanced_on_docket, key: :aod
+  attribute :location
+
   attribute :aoj do
     "other"
   end
 
+  attribute :program, key: :program_area
+  attribute :status_hash, key: :status
+  attribute :alerts
+
   attribute :docket do
     # to be implemented
+    # as docket_hash in appeal object
   end
 
+  attribute :issues
+
   attribute :events do
-    # to be implemented
+    # to be implemented in appeal object
   end
 
   attribute :issues do
     # to be implemented
     # will need to override method used
     # issues already exists in appeal
+    []
+  end
+
+  # Stubbed attributes
+  attribute :evidence do
     []
   end
 end
