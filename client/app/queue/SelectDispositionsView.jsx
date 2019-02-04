c91f490c54d8048fe4b1ccfda9bb383f3e7e3b6b
youngfreezyVA <fareezahmed@Tistas-MacBook-Pro.local>
2019-02-04 13:47:01 -0500

    fix spacing by over riding default paragraph styles

--- client/app/queue/SelectDispositionsView.jsx
+++ client/app/queue/SelectDispositionsView.jsx
@@ -37,6 +37,8 @@ const connectedIssueDiv = css({
   marginBottom: '10px'
 });
 
+const connectedIssueParagraphStyle = css({ marginTop: '0px !important' });
+
 const exampleDiv = css({
   color: COLORS.GREY,
   fontStyle: 'Italic'
@@ -304,7 +306,7 @@ class SelectDispositionsView extends React.PureComponent {
         {!editingExistingIssue &&
           <React.Fragment>
             <h3>{COPY.DECISION_ISSUE_MODAL_TITLE}</h3>
-            <p>{COPY.DECISION_ISSUE_MODAL_SUB_TITLE}</p>
+            <p {...connectedIssueParagraphStyle}>{COPY.DECISION_ISSUE_MODAL_SUB_TITLE}</p>
           </React.Fragment>
         }
 
@@ -373,7 +375,7 @@ class SelectDispositionsView extends React.PureComponent {
           })}
         />
         <h3>{COPY.DECISION_ISSUE_MODAL_CONNECTED_ISSUES_DESCRIPTION}</h3>
-        <p {...exampleDiv}>{COPY.DECISION_ISSUE_MODAL_CONNECTED_ISSUES_EXAMPLE}</p>
+        <p {...exampleDiv} {...connectedIssueParagraphStyle}>{COPY.DECISION_ISSUE_MODAL_CONNECTED_ISSUES_EXAMPLE}</p>
         <h3>{COPY.DECISION_ISSUE_MODAL_CONNECTED_ISSUES_TITLE}</h3>
         <SearchableDropdown
           name="Issues"
