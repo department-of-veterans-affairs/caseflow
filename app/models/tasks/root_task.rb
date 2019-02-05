4e67f6dd82345cca346ba56772911fb9e300cc32
Sharon Warner <sharon.warner56@gmail.com>
2019-02-05 12:04:51 -0500

    HoldHearingTask!

--- app/models/tasks/root_task.rb
+++ app/models/tasks/root_task.rb
@@ -78,7 +78,7 @@ class RootTask < GenericTask
       )
     end
 
-    def create_hearing_tasks!(appeal, parent)
+    def create_hearing_schedule_task!(appeal, parent)
       ScheduleHearingTask.create!(
         appeal: appeal,
         parent: parent,
@@ -93,7 +93,7 @@ class RootTask < GenericTask
         if appeal.evidence_submission_docket?
           create_evidence_submission_task!(appeal, distribution_task)
         elsif appeal.hearing_docket?
-          create_hearing_tasks!(appeal, distribution_task)
+          create_hearing_schedule_task!(appeal, distribution_task)
         else
           vso_tasks = create_ihp_tasks!(appeal, distribution_task)
           # If the appeal is direct docket and there are no ihp tasks,
