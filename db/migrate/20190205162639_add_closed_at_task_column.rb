f3c0f25c3301bb3e0dbeb9850c3cce007bdf58fd
lowellrex <lowell@navahq.com>
2019-02-05 11:31:49 -0500

    Use find_each

--- db/migrate/20190205162639_add_closed_at_task_column.rb
+++ db/migrate/20190205162639_add_closed_at_task_column.rb
@@ -2,9 +2,13 @@ class AddClosedAtTaskColumn < ActiveRecord::Migration[5.1]
   def up
     safety_assured do
       add_column :tasks, :closed_at, :datetime
-      execute "UPDATE tasks SET closed_at=completed_at"
+      Task.find_each { |t| t.update!(closed_at: t.completed_at) }
     end
   end
 
-  def down; end
+  def down
+    safety_assured do
+      remove_column :tasks, :closed_at
+    end
+  end
 end
