f675cde680fd97ea4410ef9a07f718132db81e4a
Peter Karman <peter@truss.works>
2019-02-04 19:04:07 -0600

    parallel CI may have trouble clearing cache

--- spec/support/clear_cache.rb
+++ spec/support/clear_cache.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
     REDIS_NAMESPACES.each { |namespace| delete_matched(namespace: namespace) }
   end
 
-  config.after(:each) do
+  config.after(:all) do
     Rails.cache.clear
     REDIS_NAMESPACES.each { |namespace| delete_matched(namespace: namespace) }
   end
