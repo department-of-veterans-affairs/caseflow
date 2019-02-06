50b5263b59c86580e188dff8fa7fa23cb55c31b8
Peter Karman <peter@truss.works>
2019-02-06 09:45:18 -0600

    mark safety assured

--- db/migrate/20190206154117_mark_jobs_prior20181001_processed.rb
+++ db/migrate/20190206154117_mark_jobs_prior20181001_processed.rb
@@ -1,7 +1,9 @@
 class MarkJobsPrior20181001Processed < ActiveRecord::Migration[5.1]
   def up
-    [:higher_level_reviews, :supplemental_claims].each do |tbl|
-      execute "UPDATE #{tbl} SET establishment_processed_at=establishment_submitted_at WHERE establishment_attempted_at IS NULL AND establishment_submitted_at < '2018-10-01'"
+    safety_assured do
+      [:higher_level_reviews, :supplemental_claims].each do |tbl|
+        execute "UPDATE #{tbl} SET establishment_processed_at=establishment_submitted_at WHERE establishment_attempted_at IS NULL AND establishment_submitted_at < '2018-10-01'"
+      end
     end
   end
 
