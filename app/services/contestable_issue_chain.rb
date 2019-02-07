815049b3a969f231aa6673144da47f1e3e8cc119
Jia Huang <jialiya.huang0@gmail.com>
2019-02-07 14:05:23 -0800

    fix returns

--- app/services/contestable_issue_chain.rb
+++ app/services/contestable_issue_chain.rb
@@ -11,7 +11,7 @@ class ContestableIssueChain
       next_decision_issue_in_chain = decision_review.decision_issues.find{ |issue| issue.associated_request_issue.id == next_request_issue.id}
     end
 
-    return next_decision_issue_in_chain
+    next_decision_issue_in_chain
   end
 
   private
@@ -21,6 +21,6 @@ class ContestableIssueChain
     contested_decision_issue_ids = decision_review.request_issues.map(&:contested_decision_issue_id)
     last_in_chain_decision_issues = decision_review.decision_issues.select{ |issue| issue.id not in contested_decision_issue_ids }
     # decision issues that don't have a request issue contesting them are latest in a chain
-    return true if contestable_issue.decision_issue_id in last_in_chain_decision_issues
+    contestable_issue.decision_issue_id in last_in_chain_decision_issues
   end
 end
