eed05b0bfd2133fa5feedad4f131d72173b1cef4
Alex Prokop <alex.prokop@gmail.com>
2019-02-07 10:10:56 -0500

    wip

--- app/models/direct_review_docket.rb
+++ app/models/direct_review_docket.rb
@@ -4,4 +4,20 @@ class DirectReviewDocket < Docket
   def docket_type
     "direct_review"
   end
+
+  def self.nonpriority_receipts_per_year
+    # hardcode this figure as a baseline before we have any data
+
+    today = Time.zone.today
+
+    if today < Time.new(2019, 0o4, 0o1)
+      return 38_500
+    elsif today < Time.new(2020, 0o4, 0o1)
+
+    end
+
+    # if today < Time.new
+  end
+
+  def self.nonpriority_receipts_since; end
 end
