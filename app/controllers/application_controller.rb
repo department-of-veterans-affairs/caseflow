f26b2db42909e438645a41c2af24f18a521a887b
Joey Yang <joey@thirdslant.com>
2019-02-07 11:04:38 -0800

    add link to user management page in top menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,21 @@ class ApplicationController < ApplicationBaseController
 
   private
 
+  def manage_teams_menu_items
+    current_user.administered_teams.map do |team|
+      url = if team.type == "JudgeTeam"
+              team.name
+            else
+              team.url
+            end
+
+      {
+        title: "#{team.name} team management",
+        link: "/organizations/#{url}/users"
+      }
+    end
+  end
+
   def handle_non_critical_error(endpoint, err)
     if !err.class.method_defined? :serialize_response
       code = (err.class == ActiveRecord::RecordNotFound) ? 404 : 500
@@ -108,12 +123,15 @@ class ApplicationController < ApplicationBaseController
       }
     ]
 
-    if ApplicationController.dependencies_faked?
-      urls.append(title: "Switch User",
-                  link: url_for(controller: "/test/users", action: "index"))
+    if current_user.administered_teams.any?
+      urls.concat(manage_teams_menu_items)
     end
-    urls.append(title: "Sign Out",
-                link: url_for(controller: "/sessions", action: "destroy"))
+
+    if ApplicationController.dependencies_faked?
+      urls.append(title: "Switch User", link: url_for(controller: "/test/users", action: "index"))
+    end
+
+    urls.append(title: "Sign Out", link: url_for(controller: "/sessions", action: "destroy"))
 
     urls
   end
