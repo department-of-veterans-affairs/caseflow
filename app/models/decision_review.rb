b21e572624833992bb1a983c4ef0f2b5cef93f51
Carrasco <vacocarram@VACOLTCARRAM4.local>
2019-02-10 20:32:17 -0500

    fixing linting issue

--- app/models/decision_review.rb
+++ app/models/decision_review.rb
@@ -337,7 +337,7 @@ class DecisionReview < ApplicationRecord
     if issue_diagnostic_code && Constants::DIAGNOSTIC_CODE_DESCRIPTIONS[issue_diagnostic_code]
       description = Constants::DIAGNOSTIC_CODE_DESCRIPTIONS[issue_diagnostic_code]["status_description"]
       description[0] = description[0].upcase
-      
+
       return description if request_issues.count - 1 == 0
 
       return "#{description} and 1 other" if request_issues.count - 1 == 1
