303e9dc2fb904a00a1b40f73a0e12eff1c6fb531
Chris Given <Christopher.Given@va.gov>
2019-02-11 15:06:49 -0500

    Add tests

--- app/models/dockets/direct_review_docket.rb
+++ app/models/dockets/direct_review_docket.rb
@@ -7,14 +7,15 @@ class DirectReviewDocket < Docket
   end
 
   def due_count
-    appeals(priority: false, ready: true)
-      .where("target_decision_date <= ?", BECOMES_DUE.days.from_now)
+    appeal_ids = appeals(priority: false, ready: true)
+      .where("target_decision_date <= ?", BECOMES_DUE.days.ago)
+    Appeal.where(id: appeal_ids).count
   end
 
   def time_until_due_of_oldest_appeal
     oldest_target = appeals(priority: false).limit(1).first.target_decision_date
-    time_until_due = Time.zone.today.to_date - oldest_target + BECOMES_DUE
-    time_until_due.clamp([0, time_until_due_of_new_appeal])
+    time_until_due = Integer(oldest_target - Time.zone.today.to_date) + BECOMES_DUE
+    time_until_due.clamp(0, time_until_due_of_new_appeal)
   end
 
   def time_until_due_of_new_appeal
@@ -32,11 +33,17 @@ class DirectReviewDocket < Docket
       march_first = Date.new(2019, 3, 1)
       days_since_march_first = Integer(today - march_first)
       appeals_since_march_first =
-        DirectReviewDocket.all_nonpriority.where("receipt_date > ?", march_first).count.length
+        all_nonpriority.where("receipt_date > ?", march_first).count.length
       # appeals_since_march_first / adjusted_appeals = days_since_march_first / 365
       ((appeals_since_march_first * 365) / days_since_march_first).round
     else
-      DirectReviewDocket.all_nonpriority.where("receipt_date > ?", 1.year.ago).count.length
+      all_nonpriority.where("receipt_date > ?", 1.year.ago).count.length
     end
+  end
+
+  private
+
+  def all_nonpriority
+    Appeal.all_nonpriority.where(docket_type: "direct_review")
   end
 end
--- spec/models/dockets/direct_review_docket_spec.rb
+++ spec/models/dockets/direct_review_docket_spec.rb
@@ -1,13 +1,53 @@
 describe DirectReviewDocket do
+  before do
+    FeatureToggle.enable!(:ama_auto_case_distribution)
+  end
+
+  after do
+    FeatureToggle.disable!(:ama_auto_case_distribution)
+  end
+
   context "#due_count" do
     subject { DirectReviewDocket.new.due_count }
+
+    before do
+      Timecop.freeze(Date.new(2021, 2, 19))
+
+      (300..309).each do |i|
+        appeal = create(:appeal,
+                        :with_tasks,
+                        docket_type: "direct_review",
+                        receipt_date: i.days.ago)
+        appeal.set_target_decision_date!
+      end
+    end
+
+    it "returns the expected count" do
+      expect(subject).to eq(5)
+    end
   end
 
   context "#time_until_due_of_oldest_appeal" do
     subject { DirectReviewDocket.new.time_until_due_of_oldest_appeal }
+
+    before do
+      Timecop.freeze(Date.new(2020, 2, 19))
+
+      (102..105).each do |i|
+        appeal = create(:appeal,
+                        :with_tasks,
+                        docket_type: "direct_review",
+                        receipt_date: i.days.ago)
+        appeal.set_target_decision_date!
+      end
+    end
+
+    it "returns the time until due" do
+      expect(subject).to eq(200)
+    end
   end
 
-  context "#nonpriority_receipts_per_year!" do
+  context "#nonpriority_receipts_per_year" do
     subject { DirectReviewDocket.new.nonpriority_receipts_per_year }
 
     context "before April 1st, 2019" do
