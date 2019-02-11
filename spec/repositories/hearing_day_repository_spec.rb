74efe6984a42dc04e273869909111fa0a8d6e5bb
Sjones352 <sjones@tistatech.com>
2019-02-11 16:21:27 -0500

    added more code coverage for hearing day repository spec file

--- spec/repositories/hearing_day_repository_spec.rb
+++ spec/repositories/hearing_day_repository_spec.rb
@@ -1,10 +1,24 @@
 describe HearingDayRepository do
   context ".fetch_hearing_day_slots" do
     subject { HearingDayRepository.fetch_hearing_day_slots(regional_office) }
-    let(:regional_office) { "RO18" }
-    it {
-      is_expected.to eq(12)
-    }
+
+    context "returns slots for Winston-Salem" do
+      let(:regional_office) { "RO18" }
+
+      it { is_expected.to eq 12 }
+    end
+
+    context "returns slots for Denver" do
+      let(:regional_office) { "RO37" }
+
+      it { is_expected.to eq 10 }
+    end
+
+    context "returns slots for Los_Angeles" do
+      let(:regional_office) { "RO44" }
+
+      it { is_expected.to eq 8 }
+    end
   end
 
   context ".ro staff hash" do
