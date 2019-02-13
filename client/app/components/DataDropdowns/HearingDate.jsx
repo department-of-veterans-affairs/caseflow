3ccadee199549435f5602d4b6a09e47dd49b108b
Andrew Lomax <andrewlomaxr@gmail.com>
2019-02-13 10:20:24 -0500

    option || {}

--- client/app/components/DataDropdowns/HearingDate.jsx
+++ client/app/components/DataDropdowns/HearingDate.jsx
@@ -103,7 +103,7 @@ class HearingDateDropdown extends React.Component {
         strongLabel
         readOnly={readOnly}
         value={this.getSelectedOption()}
-        onChange={(option) => onChange(option.value, option.label)}
+        onChange={(option) => onChange((option || {}).value, (option || {}).label)}
         options={this.props.hearingDates.options}
         errorMessage={errorMessage}
         placeholder={placeholder} />
