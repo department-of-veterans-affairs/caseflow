853244a3070310f8e8abf945bce5daf298d82f11
Mark Benjamin <mdbenjam@gmail.com>
2019-02-14 12:34:23 -0500

    Fixing tests

--- app/models/organizations/hearings_admin.rb
+++
@@ -1,5 +0,0 @@
-class HearingAdmin < Organization
-  def self.singleton
-    HearingAdmin.first || HearingAdmin.create(name: "Hearing Admin", url: "hearing-admin")
-  end
-end
--- lib/fakes/va_dot_gov_service.rb
+++ lib/fakes/va_dot_gov_service.rb
@@ -28,12 +28,12 @@ class Fakes::VADotGovService < ExternalApi::VADotGovService
     {
       "address": {
         "county": {
-          "name": "Manassas Park City",
-          "countyFipsCode": "51685"
+          "name": "Deltona",
+          "countyFipsCode": "32738"
         },
         "stateProvince": {
-          "name": "Virginia",
-          "code": "VA"
+          "name": "Florida",
+          "code": "FL"
         },
         "country": {
           "name": "United States",
@@ -42,11 +42,11 @@ class Fakes::VADotGovService < ExternalApi::VADotGovService
           "iso2Code": "US",
           "iso3Code": "USA"
         },
-        "addressLine1": "8633 Union Pl",
+        "addressLine1": "8633 Fordham St.",
         "addressLine2": "",
         "addressLine3": "",
-        "city": "Manassas Park",
-        "zipCode5": "20111",
+        "city": "Deltona",
+        "zipCode5": "32738",
         "zipCode4": "2434"
       },
       "geocode": {
