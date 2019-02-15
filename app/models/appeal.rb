7e44acf4bc5e6eef062334143816799c3776417d
Chris Given <Christopher.Given@va.gov>
2019-02-15 10:58:08 -0500

    Exclude appeals that require(d) IHPs for the purposes of calculating the age of the Direct Review docket

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -52,6 +52,13 @@ class Appeal < DecisionReview
               DistributionTask.name, Constants.TASK_STATUSES.assigned, 1)
   }
 
+  scope :non_ihp, lambda {
+    joins(:tasks)
+      .group("appeals.id")
+      .having("count(case when tasks.type = ? then 1 end) >= ?",
+              InformalHearingPresentationTask.name, 1)
+  }
+
   scope :active, lambda {
     joins(:tasks)
       .group("appeals.id")
