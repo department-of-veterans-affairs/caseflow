361c583e11b5fa69ecc02d6e2ad594e823fc24b7
hschallhorn <hschallhorn@tistatech.com>
2019-02-15 08:24:13 -0500

    Fix logic on when to display an error

--- client/app/queue/AppealDocumentCount.jsx
+++ client/app/queue/AppealDocumentCount.jsx
@@ -62,10 +62,8 @@ class AppealDocumentCount extends React.PureComponent {
         return docCountForAppeal.precise;
       } else if (cached && _.isNumber(docCountForAppeal.cached)) {
         return docCountForAppeal.cached;
-      } else if (docCountForAppeal.error) {
-        return docCountForAppeal.error;
-      } else if (loadingText && docCountForAppeal.loading) {
-        return <span {...documentCountStyling}>Loading number of docs...</span>;
+      } else if (loadingText && (docCountForAppeal.loading || docCountForAppeal.error)) {
+        return docCountForAppeal.error || <span {...documentCountStyling}>Loading number of docs...</span>;
       }
     }
 
