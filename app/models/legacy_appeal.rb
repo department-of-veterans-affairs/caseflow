1bfbdea264953e15741344da5be3b7780cb7ffe7
Andrew Lomax <andrewlomaxr@gmail.com>
2019-02-15 08:46:52 -0500

    use address from bg if veteran is claimant

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -345,7 +345,7 @@ class LegacyAppeal < ApplicationRecord
         middle_name: veteran_middle_initial,
         last_name: veteran_last_name,
         name_suffix: veteran_name_suffix,
-        address: get_address_from_corres_entry(case_record.correspondent),
+        address: address_from_veteran_record,
         representative: representative_to_hash
       }
     end
