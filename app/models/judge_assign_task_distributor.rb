8d0f31304af5d8737bd1ffd1cb57ec13f1c4d5a5
Tomas Apodaca <tomas@navahq.com>
2019-02-19 15:09:55 -0800

    this model's no longer used as of fc609bad9e

--- app/models/judge_assign_task_distributor.rb
+++
@@ -1,6 +0,0 @@
-class JudgeAssignTaskDistributor < RoundRobinTaskDistributor
-  def initialize(assignee_pool: Constants::RampJudges::USERS[Rails.current_env],
-                 task_class: JudgeAssignTask)
-    super
-  end
-end
