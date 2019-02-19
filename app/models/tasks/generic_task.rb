9f98693819274d19a020ec26e059d3cb1ab17b8c
Joey Yang <joey@thirdslant.com>
2019-02-19 12:09:24 -0500

    lint

--- app/models/tasks/generic_task.rb
+++ app/models/tasks/generic_task.rb
@@ -17,6 +17,7 @@ class GenericTask < Task
   end
 
   # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def available_actions(user)
     return [] unless user
 
@@ -48,6 +49,7 @@ class GenericTask < Task
     []
   end
   # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   private
 
