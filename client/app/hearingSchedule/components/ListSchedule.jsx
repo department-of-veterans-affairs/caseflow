7ba48efdf55ab0c6fb4f570074e826669e0150b4
akainic <annekainic@gmail.com>
2019-02-19 17:50:50 -0500

    Remove obsolete code

--- client/app/hearingSchedule/components/ListSchedule.jsx
+++ client/app/hearingSchedule/components/ListSchedule.jsx
@@ -24,21 +24,6 @@ const downloadButtonStyling = css({
   marginTop: '60px'
 });
 
-const formatVljName = (lastName, firstName) => {
-  if (lastName && firstName) {
-    return `${lastName}, ${firstName}`;
-  }
-};
-
-// const judgeNameToIdMap = (hearings) => {
-//   let nameToIdMap = {};
-
-//   _.forEach(hearings, (hearingDay) => nameToIdMap[formatVljName(hearingDay.judgeLastName,
-//     hearingDay.judgeFirstName)] = hearingDay.judgeId);
-
-//   return nameToIdMap;
-// };
-
 const inlineFormStyling = css({
   '> div': {
     ' & .cf-inline-form': {
