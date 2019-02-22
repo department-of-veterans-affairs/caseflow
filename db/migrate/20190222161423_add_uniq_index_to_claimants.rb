57c77a1989c3d7189bf1d9670a417049811b6776
Anya Roltsch <aroltsch@users.noreply.github.com>
2019-02-22 12:44:52 -0500

    Revert "Add uniq index on claimants table to avoid duplicate participant ids (#9608)"
    This reverts commit 717ba37fd4739fd83721900e6086d7a2af4c82c6.

--- db/migrate/20190222161423_add_uniq_index_to_claimants.rb
+++
@@ -1,8 +0,0 @@
-class AddUniqIndexToClaimants < ActiveRecord::Migration[5.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :claimants, [:participant_id, :review_request_id, :review_request_type], name: "uniq_index_on_participant_id_review_request", unique: true, algorithm: :concurrently
-  end
-end
-
