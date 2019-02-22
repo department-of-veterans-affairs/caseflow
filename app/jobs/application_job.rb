0cde2e60d08ab70372f1a90b93577aecb13a670e
Peter Karman <peter@truss.works>
2019-02-22 09:46:01 -0600

    clear Raven context after each job is performed

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -7,12 +7,16 @@ class ApplicationJob < ActiveJob::Base
     attr_reader :app_name
   end
 
-  before_perform do |job|
+  around_perform do |job, block|
     # setup debug context
     Raven.tags_context(job: job.class.name, queue: job.queue_name)
     Raven.extra_context(application: self.class.app_name.to_s)
     if self.class.app_name.present?
       RequestStore.store[:application] = "#{self.class.app_name}_job"
     end
+
+    block.call
+
+    Raven::Context.clear!
   end
 end
