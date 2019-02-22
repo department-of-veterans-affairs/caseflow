43a313cb1a86a8dca6a88375517884c528c5952f
hschallhorn <hschallhorn@tistatech.com>
2019-02-22 15:59:14 -0500

    Add comments back in as well

--- app/models/concerns/document_concern.rb
+++ app/models/concerns/document_concern.rb
@@ -1,6 +1,8 @@
 module DocumentConcern
   extend ActiveSupport::Concern
 
+  # Number of documents stored locally via nightly RetrieveDocumentsForReaderJob.
+  # Fall back to count from VBMS if no local documents are found.
   def number_of_documents_from_caseflow
     count = Document.where(file_number: veteran_file_number).size
     (count != 0) ? count : number_of_documents
