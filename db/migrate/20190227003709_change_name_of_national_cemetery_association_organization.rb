89ae82cb8d348dc15c4cc4a6643a946d4ac07b5c
Sally Maki <sallymaki@gmail.com>
2019-02-26 16:39:06 -0800

    Update name of nca

---
+++ db/migrate/20190227003709_change_name_of_national_cemetery_association_organization.rb
@@ -0,0 +1,5 @@
+class ChangeNameOfNationalCemeteryAssociationOrganization < ActiveRecord::Migration[5.1]
+  def change
+    Organization.find_by(url:"nca").update!(name: "National Cemetery Administration")
+  end
+end
