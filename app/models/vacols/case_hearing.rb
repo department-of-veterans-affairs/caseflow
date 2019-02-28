5d7b8d0f01745ea8454ee89bb44e7f3c4cb8ca10
Sharon Warner <sharon.warner56@gmail.com>
2019-02-28 11:04:15 -0500

    Removing some VACOLS hearing day functionality (#9661)

--- app/models/vacols/case_hearing.rb
+++ app/models/vacols/case_hearing.rb
@@ -65,7 +65,7 @@ class VACOLS::CaseHearing < VACOLS::Record
     end
 
     def hearings_for_hearing_days(hearing_day_ids)
-      select_hearings.where(vdkey: hearing_day_ids)
+      select_hearings.where(vdkey: hearing_day_ids).where("hearing_date > ?", Date.new(2019, 1, 1))
     end
 
     def for_appeal(appeal_vacols_id)
@@ -95,23 +95,6 @@ class VACOLS::CaseHearing < VACOLS::Record
       select_schedule_days.where("folder_nr = ? and trunc(hearing_date) between ? and ?",
                                  "VIDEO #{regional_office}", VacolsHelper.day_only_str(start_date),
                                  VacolsHelper.day_only_str(end_date)).order(:hearing_date)
-    end
-
-    def create_hearing!(hearing_info)
-      attrs = hearing_info.each_with_object({}) { |(k, v), result| result[COLUMN_NAMES[k]] = v }
-      attrs.except!(nil)
-      # Store time value in UTC to VACOLS
-      hear_date = attrs[:hearing_date]
-      converted_date = hear_date.is_a?(Date) ? hear_date : Time.zone.parse(hear_date).to_datetime
-      attrs[:hearing_date] = VacolsHelper.format_datetime_with_utc_timezone(converted_date)
-      MetricsService.record("VACOLS: create_hearing!",
-                            service: :vacols,
-                            name: "create_hearing") do
-        create(attrs.merge(addtime: VacolsHelper.local_time_with_utc_timezone,
-                           adduser: current_user_slogid,
-                           folder_nr: hearing_info[:regional_office] ? "VIDEO #{hearing_info[:regional_office]}" : nil,
-                           hearing_type: HearingDay::REQUEST_TYPES[:central]))
-      end
     end
 
     def create_child_hearing!(hearing_info)
