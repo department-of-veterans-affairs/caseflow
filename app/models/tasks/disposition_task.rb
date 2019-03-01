a0395a4d8b350822afb2bf8d16f59398c2d04e67
Andrew Lomax <andrewlomaxr@gmail.com>
2019-02-28 19:04:37 -0500

    working version

--- app/models/tasks/disposition_task.rb
+++ app/models/tasks/disposition_task.rb
@@ -1,7 +1,7 @@
 class DispositionTask < GenericTask
   class << self
     def create_disposition_task!(appeal, parent, hearing)
-      DispositionTask.create!(
+      disposition_task = DispositionTask.create!(
         appeal: appeal,
         parent: parent,
         assigned_to: Bva.singleton
@@ -10,6 +10,115 @@ class DispositionTask < GenericTask
       if parent.is_a? HearingTask
         HearingTaskAssociation.create!(hearing: hearing, hearing_task: parent)
       end
+
+      disposition_task
     end
+  end
+
+  def available_actions(user)
+    if (assigned_to && assigned_to == user) || task_is_assigned_to_users_organization?(user)
+      return [Constants.TASK_ACTIONS.POSTPONE_HEARING.to_h]
+    end
+
+    []
+  end
+
+  def add_schedule_hearing_task_admin_actions_data
+    {
+      redirect_after: "/queue/appeals/#{appeal.external_id}",
+      message_detail: COPY::ADD_HEARING_ADMIN_TASK_CONFIRMATION_DETAIL,
+      selected: nil,
+      options: HearingAdminActionTask.subclasses.sort_by(&:label).map do |subclass|
+        { value: subclass.name, label: subclass.label }
+      end
+    }
+  end
+
+  def hearing_task
+    parent
+  end
+
+  def update_with_params(params, _user)
+    disposition_params = params.delete(:business_payloads)[:values]
+
+    if params[:status] == Constants.TASK_STATUSES.cancelled
+      case disposition_params[:disposition]
+      when "postponed"
+        after_disposition_update = disposition_params[:after_disposition_update]
+        postponed(after_disposition_update: after_disposition_update)
+      when "held"
+        held
+      when "no_show"
+        no_show
+      end
+    end
+  end
+
+  def reschedule(hearing_pkseq:, hearing_time:, hearing_location: nil)
+    new_hearing_task = hearing_task.cancel_and_recreate
+
+    new_hearing = slot_new_hearing(
+      hearing_pkseq, hearing_time, hearing_location
+    )
+    self.class.create_disposition_task!(appeal, new_hearing_task, new_hearing)
+  end
+
+  def schedule_later(with_admin_action_klass: nil, instructions: nil)
+    new_hearing_task = hearing_task.cancel_and_recreate
+
+    schedule_task = ScheduleHearingTask.create!(
+      parent: new_hearing_task,
+      appeal: appeal,
+      assigned_to: HearingsManagement.singleton
+    )
+    if with_admin_action_klass.present?
+      with_admin_action_klass.constantize.create!(
+        parent: schedule_task,
+        appeal: appeal,
+        instructions: instructions.present? ? [instructions] : nil,
+        assigned_to: HearingsManagement.singleton
+      )
+    end
+  end
+
+  def release() end
+
+  def postponed(after_disposition_update:)
+    hearing = hearing_task.hearing_task_association.hearing
+    hearing.update(disposition: "postponed")
+
+    case after_disposition_update[:action]
+    when "reschedule"
+      new_hearing_attrs = after_disposition_update[:new_hearing_attrs]
+      reschedule(
+        hearing_pkseq: new_hearing_attrs[:hearing_pkseq], hearing_time: new_hearing_attrs[:hearing_time],
+        hearing_location: new_hearing_attrs[:hearing_location]
+      )
+    when "schedule_later"
+      schedule_later(
+        with_admin_action_klass: after_disposition_update[:with_admin_action_klass],
+        instructions: after_disposition_update[:admin_action_instructions]
+      )
+    end
+  end
+
+  def no_show() end
+
+  def cancelled() end
+
+  def held() end
+
+  private
+
+  def slot_new_hearing(hearing_day_id, hearing_time, hearing_location)
+    hearing = HearingRepository.slot_new_hearing(hearing_day_id,
+                                                 appeal: appeal,
+                                                 hearing_location_attrs: hearing_location&.to_hash,
+                                                 scheduled_time: hearing_time&.stringify_keys)
+    if appeal.is_a?(LegacyAppeal)
+      AppealRepository.update_location!(appeal, LegacyAppeal::LOCATION_CODES[:caseflow])
+    end
+
+    hearing
   end
 end
---
+++ spec/models/tasks/disposition_task_spec.rb
@@ -0,0 +1,92 @@
+describe DispositionTask do
+  describe "postponement" do
+    let(:appeal) { FactoryBot.create(:appeal) }
+    let!(:hearing) { FactoryBot.create(:hearing, appeal: appeal) }
+    let!(:root_task) { FactoryBot.create(:root_task, appeal: appeal) }
+    let!(:hearing_task) { FactoryBot.create(:hearing_task, parent: root_task, appeal: appeal) }
+    let!(:disposition_task) { DispositionTask.create_disposition_task!(appeal, hearing_task, hearing) }
+    let(:after_disposition_update) { nil }
+
+    let(:params) do
+      {
+        status: "cancelled",
+        business_payloads: {
+          values: {
+            disposition: "postponed",
+            after_disposition_update: after_disposition_update
+          }
+        }
+      }
+    end
+
+    context "when hearing should be scheduled later" do
+      let(:after_disposition_update) do
+        {
+          action: "schedule_later"
+        }
+      end
+
+      it "creates a new HearingTask and ScheduleHearingTask" do
+        disposition_task.update_with_params(params, nil)
+
+        expect(Hearing.first.disposition).to eq "postponed"
+        expect(HearingTask.count).to eq 2
+        expect(HearingTask.first.status).to eq "cancelled"
+        expect(DispositionTask.first.status).to eq "cancelled"
+        expect(ScheduleHearingTask.count).to eq 1
+        expect(ScheduleHearingTask.first.parent.id).to eq HearingTask.last.id
+      end
+    end
+
+    context "when hearing should be scheduled later with admin action" do
+      let(:instructions) { "Fix this." }
+      let(:after_disposition_update) do
+        {
+          action: "schedule_later",
+          with_admin_action_klass: "HearingAdminActionIncarceratedVeteranTask",
+          admin_action_instructions: instructions
+        }
+      end
+
+      it "creates a new HearingTask and ScheduleHearingTask with admin action" do
+        disposition_task.update_with_params(params, nil)
+
+        expect(Hearing.first.disposition).to eq "postponed"
+        expect(HearingTask.count).to eq 2
+        expect(HearingTask.first.status).to eq "cancelled"
+        expect(DispositionTask.first.status).to eq "cancelled"
+        expect(ScheduleHearingTask.count).to eq 1
+        expect(ScheduleHearingTask.first.parent.id).to eq HearingTask.last.id
+        expect(HearingAdminActionIncarceratedVeteranTask.count).to eq 1
+        expect(HearingAdminActionIncarceratedVeteranTask.last.instructions).to eq [instructions]
+      end
+    end
+
+    context "when hearing should be resecheduled" do
+      let(:after_disposition_update) do
+        {
+          action: "reschedule",
+          new_hearing_attrs: {
+            hearing_pkseq: HearingDay.first.id,
+            hearing_location: { facility_id: "vba_370", distance: 10 },
+            hearing_time: { h: "12", m: "30", offset: "-05:00" }
+          }
+        }
+      end
+
+      it "creates a new hearing with a new DispositionTask" do
+        disposition_task.update_with_params(params, nil)
+
+        expect(Hearing.count).to eq 2
+        expect(Hearing.first.disposition).to eq "postponed"
+        expect(Hearing.last.hearing_location.facility_id).to eq "vba_370"
+        expect(Hearing.last.scheduled_time.strftime("%I:%M%p")).to eq "12:30PM"
+        expect(HearingTask.count).to eq 2
+        expect(HearingTask.first.status).to eq "cancelled"
+        expect(HearingTask.last.hearing_task_association.hearing.id).to eq Hearing.last.id
+        expect(DispositionTask.count).to eq 2
+        expect(DispositionTask.first.status).to eq "cancelled"
+      end
+    end
+  end
+end
