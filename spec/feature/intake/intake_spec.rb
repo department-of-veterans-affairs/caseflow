2e9f989776532e26c9a0ea3941fad79951122127
Sally Maki <sallymaki@gmail.com>
2019-03-05 17:20:12 -0500

    Remove gender validation on Veteran for Intake (#9792)
    connects #9709

--- spec/feature/intake/intake_spec.rb
+++ spec/feature/intake/intake_spec.rb
@@ -196,7 +196,7 @@ RSpec.feature "Intake" do
         expect(page).to have_current_path("/intake/search")
         expect(page).to have_content("Please fill in the following field(s) in the Veteran's profile in VBMS or")
         expect(page).to have_content(
-          "the corporate database, then retry establishing the EP in Caseflow: ssn, sex, country."
+          "the corporate database, then retry establishing the EP in Caseflow: ssn, country."
         )
         expect(page).to have_content("This Veteran's address is too long. Please edit it in VBMS or SHARE")
       end
