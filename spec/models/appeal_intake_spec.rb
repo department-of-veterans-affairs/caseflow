aacecaa82169caa2e8e551fd8fd47be9183bee55
Sally Maki <sallymaki@gmail.com>
2019-03-06 10:40:08 -0500

    Backfill data (#9743)
    connects #8239 
    
    I'm realizing I probably could have done this in the first PR - but this is to backfill the claimant data for Decision Review. Next I will check it out to make sure everything is there before doing the switch and removing review_request.

--- spec/models/appeal_intake_spec.rb
+++ spec/models/appeal_intake_spec.rb
@@ -114,7 +114,8 @@ describe AppealIntake do
       expect(intake.detail.claimants.count).to eq 1
       expect(intake.detail.claimants.first).to have_attributes(
         participant_id: intake.veteran.participant_id,
-        payee_code: nil
+        payee_code: nil,
+        decision_review: intake.detail
       )
     end
 
@@ -141,7 +142,8 @@ describe AppealIntake do
         expect(intake.detail.claimants.count).to eq 1
         expect(intake.detail.claimants.first).to have_attributes(
           participant_id: "1234",
-          payee_code: nil
+          payee_code: nil,
+          decision_review: intake.detail
         )
       end
 
