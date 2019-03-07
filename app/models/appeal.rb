11f4e05a550dafd49d8940bc832ec44da21e8310
Sally Maki <sallymaki@gmail.com>
2019-03-07 14:44:33 -0500

    Handle future decision dates for DTA SC

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -23,6 +23,7 @@ class Appeal < DecisionReview
   has_one :special_issue_list
 
   with_options on: :intake_review do
+    validate :validate_receipt_date
     validates :receipt_date, :docket_type, presence: { message: "blank" }
     validates :veteran_is_not_claimant, inclusion: { in: [true, false], message: "blank" }
     validates :legacy_opt_in_approved, inclusion: { in: [true, false], message: "blank" }, if: :legacy_opt_in_enabled?
