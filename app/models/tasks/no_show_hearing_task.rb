179293f52ab20deb4bd2e5ff16faa054475644b4
lowellrex <lowell@navahq.com>
2019-03-08 09:38:40 -0500

    Automatically set assignee for hearing tasks

--- app/models/tasks/no_show_hearing_task.rb
+++ app/models/tasks/no_show_hearing_task.rb
@@ -3,4 +3,11 @@
 # Task created after an appellant no-shows for a hearing. Gives the hearings team the options to decide how to handle
 # the no-show hearing after the judge indicates that the appellant no-showed.
 class NoShowHearingTask < GenericTask
+  before_validation :set_assignee
+
+  private
+
+  def set_assignee
+    self.assigned_to = HearingAdmin.singleton
+  end
 end
