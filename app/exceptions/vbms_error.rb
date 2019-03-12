1112f957502143176caa96301b5bbf65600f43a9
Peter Karman <peter@truss.works>
2019-03-12 14:59:24 -0500

    small refactor to shrink method size

--- app/exceptions/vbms_error.rb
+++ app/exceptions/vbms_error.rb
@@ -9,7 +9,29 @@ class VBMSError < RuntimeError
   end
 
   class << self
-    # rubocop:disable Metrics/MethodLength
+    KNOWN_ERRORS = {
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3288/
+      "additional review due to an Incident Flash" => "VBMS::IncidentFlashError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/4035/
+      "Retrieving Contention list failed. System error." => "VBMS::TransientError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3405/
+      "Unable to associate rated issue, rated issue does not exist" => "VBMS::RatedIssueMissingError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3894/
+      "Requested result set exceeds acceptable size." => "VBMS::DocumentTooBigError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3293/
+      "WssVerification Exception - Security Verification Exception" => "VBMS::SecurityError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3965/
+      "VBMS is currently unavailable due to maintenance." => "VBMS::DownForMaintenanceError",
+
+      # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3274/
+      "The data value of the PostalCode did not satisfy" => "VBMS::BadPostalCodeError"
+    }.freeze
+
     def from_vbms_http_error(vbms_http_error)
       error_message = if vbms_http_error.try(:body)
                         # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3124/
@@ -18,43 +40,15 @@ class VBMSError < RuntimeError
                         vbms_http_error.message
                       end
 
-      known_errors = {
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3288/
-        "additional review due to an Incident Flash" => "VBMS::IncidentFlashError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/4035/
-        "Retrieving Contention list failed. System error." => "VBMS::TransientError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3405/
-        "Unable to associate rated issue, rated issue does not exist" => "VBMS::RatedIssueMissingError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3894/
-        "Requested result set exceeds acceptable size." => "VBMS::DocumentTooBigError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3293/
-        "WssVerification Exception - Security Verification Exception" => "VBMS::SecurityError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3965/
-        "VBMS is currently unavailable due to maintenance." => "VBMS::DownForMaintenanceError",
-
-        # https://sentry.ds.va.gov/department-of-veterans-affairs/caseflow/issues/3274/
-        "The data value of the PostalCode did not satisfy the following condition" => "VBMS::BadPostalCodeError"
-
-      }
-
       new_error = nil
-      known_errors.each_key do |msg_str|
-        msg = /#{msg_str}/
+      KNOWN_ERRORS.each do |msg_str, error_class_name|
+        next unless error_message =~ /#{msg_str}/
 
-        next unless msg.match(error_message)
-
-        error_class = known_errors[msg_str].constantize
-        new_error = error_class.new(vbms_http_error)
+        new_error = error_class_name.constantize.new(vbms_http_error)
         break
       end
       new_error ||= new(vbms_http_error)
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
 
