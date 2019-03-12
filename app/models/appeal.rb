deb27928383e1eae13d910a2c06232e7f1f7be15
aroltsch <aroltsch@tistatech.com>
2019-03-12 19:36:13 -0400

    Remove unneeded code

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -168,10 +168,6 @@ class Appeal < DecisionReview
     request_issues.active.all.sort_by(&:id)
   end
 
-  def decided_request_issues
-    request_issues.select(&:decided?)
-  end
-
   def issues
     { decision_issues: decision_issues, request_issues: request_issues }
   end
