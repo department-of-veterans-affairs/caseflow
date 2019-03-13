9d9fb8ae7e1859702e5478779c51b303c99c2694
Alex Prokop <alex.prokop@gmail.com>
2019-03-13 14:19:40 -0400

    fix spec

--- spec/models/tasks/generic_task_spec.rb
+++ spec/models/tasks/generic_task_spec.rb
@@ -14,6 +14,7 @@ describe GenericTask do
           Constants.TASK_ACTIONS.ASSIGN_TO_TEAM.to_h,
           Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
           Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+          Constants.TASK_ACTIONS.PLACE_HOLD.to_h,
           Constants.TASK_ACTIONS.CANCEL_TASK.to_h
         ]
       end
