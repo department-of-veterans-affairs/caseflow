c8c38abc60c799b3d316ce60af9a50c7880810f8
Peter Karman <peter@truss.works>
2019-03-13 14:09:58 -0500

    raise exception, do not catch it

--- app/models/request_issues_update.rb
+++ app/models/request_issues_update.rb
@@ -50,12 +50,7 @@ class RequestIssuesUpdate < ApplicationRecord
 
     potential_end_products_to_remove = []
     removed_issues.select(&:end_product_establishment).each do |request_issue|
-      begin
-        request_issue.end_product_establishment.remove_contention!(request_issue)
-      rescue EndProductEstablishment::ContentionNotFound => _err
-        next
-      end
-
+      request_issue.end_product_establishment.remove_contention!(request_issue)
       potential_end_products_to_remove << request_issue.end_product_establishment
     end
 
