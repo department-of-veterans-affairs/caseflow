068c0a15e563cd9e5981dd58ca4b69bbd2da106f
Peter Karman <peter@truss.works>
2019-03-15 13:50:47 -0500

    Add new AsyncableJobsReporter class

--- app/services/asyncable_jobs.rb
+++ app/services/asyncable_jobs.rb
@@ -19,20 +19,6 @@ class AsyncableJobs
     jobs.select { |j| msg_regex.match?(j[j.class.error_column]) }
   end
 
-  # summarize all jobs by class and error and age
-  def summary
-    report = {}
-    jobs.each do |job|
-      cls = job.class.to_s
-      age = ((Time.zone.now - job.sort_by_last_submitted_at) / 3600 / 24).ceil.to_s
-      err = job[job.class.error_column]
-      report[cls] ||= {}
-      report[cls][age] ||= { err => 0 }
-      report[cls][age][err] += 1
-    end
-    report
-  end
-
   private
 
   # TODO: how to support paging when coallescing so many different models?
--- spec/services/asyncable_jobs_reporter_spec.rb
+++ spec/services/asyncable_jobs_reporter_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-describe AsyncableJobs do
+describe AsyncableJobsReporter do
+  before do
+    seven_am_random_date = Time.new(2019, 3, 29, 7, 0, 0).in_time_zone
+    Timecop.freeze(seven_am_random_date)
+  end
+
   let(:veteran) { create(:veteran) }
   let!(:hlr) do
     create(:higher_level_review,
@@ -40,45 +45,36 @@ describe AsyncableJobs do
            veteran_file_number: veteran.file_number)
   end
 
-  describe "#jobs" do
-    it "returns an Array of model instances that consume Asyncable concern" do
-      expect(subject.jobs).to be_a(Array)
-      expect(subject.jobs.length).to eq(6)
-      expect(subject.jobs).to include(hlr)
-      expect(subject.jobs).to include(sc)
-      expect(subject.jobs).to include(sc2)
-      expect(subject.jobs).to include(sc_not_submitted)
-      expect(subject.jobs).to include(sc_not_attempted_expired)
-      expect(subject.jobs).to include(sc_not_attempted)
-    end
+  subject { AsyncableJobsReporter.new(jobs: AsyncableJobs.new.jobs) }
 
-    it "sorts by the submited_at column, descending order" do
-      expect(subject.jobs).to eq([sc_not_attempted_expired, hlr, sc, sc2, sc_not_attempted, sc_not_submitted])
-    end
-
-    it "includes all unprocessed jobs regardless of whether they have expired" do
-      expect(subject.jobs.select(&:expired_without_processing?).count).to eq(4)
-      expect(subject.jobs.reject(&:expired_without_processing?).count).to eq(2)
+  describe "#summary" do
+    it "returns Hash summarizing jobs" do
+      expect(subject.summary).to eq(
+        "SupplementalClaim" => {
+          "8" => { "none" => 1 },
+          "6" => { "bad" => 2 },
+          "2" => { "none" => 1 },
+          "0" => { "bad" => 1 }
+        },
+        "HigherLevelReview" => {
+          "7" => { "bad" => 1 }
+        }
+      )
     end
   end
 
-  describe "#find_by_error" do
-    it "searches by regex" do
-      expect(subject.find_by_error(/bad problem/).count).to eq(4)
-    end
-
-    it "searches by string" do
-      expect(subject.find_by_error("bad problem").count).to eq(4)
-    end
-  end
-
-  describe "#models" do
-    it "returns list of Asyncable-consuming models" do
-      expect(subject.models).to include(HigherLevelReview)
-    end
-
-    it "rejects abstract classes" do
-      expect(subject.models).to_not include(DecisionReview)
+  describe "#summarize" do
+    it "writes a report to stdout" do
+      msg = <<~HEREDOC
+        HigherLevelReview has 1 jobs 7 days old with error bad
+        HigherLevelReview has 1 total jobs in queue
+        SupplementalClaim has 1 jobs 0 days old with error bad
+        SupplementalClaim has 1 jobs 2 days old with error none
+        SupplementalClaim has 2 jobs 6 days old with error bad
+        SupplementalClaim has 1 jobs 8 days old with error none
+        SupplementalClaim has 5 total jobs in queue
+      HEREDOC
+      expect(subject.summarize).to eq(msg.chomp)
     end
   end
 end
