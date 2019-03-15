100048bb0c6e59e4a9ce220a3f9eb7c4878a873f
Peter Karman <pkarman@users.noreply.github.com>
2019-03-15 10:43:17 -0400

    Remove intake_legacy_opt_in feature flag, update seeds (#9973)
    connects #9972
    
    Description
    While trying to locally validate #9816 I discovered that our legacy opt-in code was (a) not enabled in development, and (b) the seed data was not updated to reflect AMA date passage.

--- app/models/decision_review.rb
+++ app/models/decision_review.rb
@@ -158,7 +158,6 @@ class DecisionReview < ApplicationRecord
   end
 
   def serialized_legacy_appeals
-    return [] unless legacy_opt_in_enabled?
     return [] unless available_legacy_appeals.any?
 
     available_legacy_appeals.map do |legacy_appeal|
@@ -384,10 +383,6 @@ class DecisionReview < ApplicationRecord
 
     validate_receipt_date_not_before_ama
     validate_receipt_date_not_in_future
-  end
-
-  def legacy_opt_in_enabled?
-    FeatureToggle.enabled?(:intake_legacy_opt_in, user: RequestStore.store[:current_user])
   end
 
   def description
