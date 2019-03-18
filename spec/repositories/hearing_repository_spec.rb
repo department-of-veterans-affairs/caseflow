78a290c9f95387dd5e732c2a41c2e29bc766501e
Andrew Lomax <andrewlomaxr@gmail.com>
2019-03-18 15:50:10 -0400

    Legacy Hearing regional_office from HearingDay, not AppealConcern (#9956)
    Resolves #9954
    
    - adds `#hearing_day` to LegacyHearing which looks up the `HearingDay` based on the `hearing_day_id` from VACOLS
    - accesses `regional_office` from `hearing_day`

--- spec/repositories/hearing_repository_spec.rb
+++ spec/repositories/hearing_repository_spec.rb
@@ -38,7 +38,6 @@ describe HearingRepository do
 
     let(:hearing_hash) do
       OpenStruct.new(
-        hearing_venue: "SO62",
         hearing_date: date,
         hearing_type: HearingDay::REQUEST_TYPES[:video],
         hearing_pkseq: "12345678",
@@ -55,7 +54,6 @@ describe HearingRepository do
     end
 
     it "assigns values properly" do
-      expect(subject.venue[:city]).to eq("San Antonio")
       expect(subject.request_type).to eq(HearingDay::REQUEST_TYPES[:video])
       expect(subject.vacols_record).to eq(hearing_hash)
       expect(subject.scheduled_for.class).to eq(ActiveSupport::TimeWithZone)
@@ -66,7 +64,6 @@ describe HearingRepository do
       expect(subject.notes).to eq "test notes"
       expect(subject.representative_name).to eq "test rep name"
       expect(subject.representative).to eq "Jewish War Veterans"
-      expect(subject.regional_office_key).to eq "SO62"
     end
   end
 
