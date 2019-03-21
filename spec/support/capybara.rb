bd29970096e6fc44c7ea004acbb68ac4e355c07d
Moncef Belyamani <moncef@truss.works>
2019-03-21 10:13:05 -0400

    Fix default Capybara driver setting
    **Why**: While testing PR #9503, I removed the conditional and forgot to
    set it back before merging.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -65,7 +65,7 @@ Capybara::Screenshot.register_driver(:sniffybara_headless) do |driver, path|
   driver.browser.save_screenshot(path)
 end
 
-Capybara.default_driver = :sniffybara_headless
+Capybara.default_driver = ENV["CI"] ? :sniffybara_headless : :parallel_sniffybara
 # the default default_max_wait_time is 2 seconds
 Capybara.default_max_wait_time = 5
 
