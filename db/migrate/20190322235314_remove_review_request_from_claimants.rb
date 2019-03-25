3baf3f571b33dc722eede306712ba0c30264d0f2
Sally Maki <sallymaki@gmail.com>
2019-03-25 15:59:56 -0700

    Review changes

--- db/migrate/20190322235314_remove_review_request_from_claimants.rb
+++ db/migrate/20190322235314_remove_review_request_from_claimants.rb
@@ -1,18 +1,18 @@
 class RemoveReviewRequestFromClaimants < ActiveRecord::Migration[5.1]
   def change
     safety_assured do
-      remove_column :claimants, :review_request_id, :integer
-      remove_column :claimants, :review_request_type, :string
+      remove_column :claimants, :review_request_id
+      remove_column :claimants, :review_request_type
     end
 
-    change_table_comment(:claimants, "The claimant for each Decision Review, and its payee_code if required.")
+    change_table_comment(:claimants, "This table bridges decision reviews to participants when the participant is listed as a claimant on the decision review. A participant can be a claimant on multiple decision reviews.")
 
-    change_column_comment(:claimants, :decision_review_id, "The ID of the Decision Review the claimant is on.")
+    change_column_comment(:claimants, :decision_review_id, "The ID of the decision review the claimant is on.")
 
-    change_column_comment(:claimants, :decision_review_type, "The type of Decision Review the claimant is on.")
+    change_column_comment(:claimants, :decision_review_type, "The type of decision review the claimant is on.")
 
-    change_column_comment(:claimants, :participant_id, "The participant ID of the claimant selected on a Decision Review.")
+    change_column_comment(:claimants, :participant_id, "The participant ID of the claimant.")
 
-    change_column_comment(:claimants, :payee_code, "The payee_code for the claimant selected on a Decision Review, if applicable. Payee_code is required for claimants when the claim is processed in VBMS and the Veteran is not the claimant. For Supplemental Claims automatically created due to remanded decisions, this is automatically selected based on the last payee code used for the same claimant on a previous end product, if available.")
+    change_column_comment(:claimants, :payee_code, "The payee_code for the claimant, if applicable. payee_code is required when the claim is processed in VBMS.")
   end
 end
