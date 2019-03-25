c493f23ec549bf1af4b6de982fd12a990c9f0a96
Peter Karman <peter@truss.works>
2019-03-25 16:02:38 -0500

    Add new data-key attribute using issue index

--- spec/support/intake_helpers.rb
+++ spec/support/intake_helpers.rb
@@ -241,7 +241,7 @@ module IntakeHelpers
 
   def click_remove_intake_issue_dropdown(text)
     issue_el = find_intake_issue_by_text(text)
-    issue_num = issue_el[:id].sub(/^issue-/, "").to_i - 1
+    issue_num = issue_el[:"data-key"].sub(/^issue-/, "")
     find("#issue-action-#{issue_num}").click
     find("#issue-action-#{issue_num}_remove").click
     click_remove_issue_confirmation
