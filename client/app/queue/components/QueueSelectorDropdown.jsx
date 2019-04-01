6dd56fd16363702bbb101226a1f81f329a52f2b1
hschallhorn <hschallhorn@tistatech.com>
2019-04-01 18:22:15 -0400

    Use dropdown button component rather than custom dopdown

--- client/app/queue/components/QueueSelectorDropdown.jsx
+++ client/app/queue/components/QueueSelectorDropdown.jsx
@@ -2,25 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from 'glamor';
 
-import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+import DropdownButton from '../../components/DropdownButton'
 import COPY from '../../../COPY.json';
 
 // NOTE: parent container needs to be given 'position: relative'
-const styles = {
-  dropdownTrigger: css({
-    marginRight: 0
-  }),
-  dropdownButton: css({
-    position: 'absolute',
-    top: '48px',
-    right: '40px'
-  }),
-  dropdownList: css({
-    top: '3.55rem',
-    right: '0',
-    width: '26rem'
-  })
-};
+const style = css({
+  position: 'absolute',
+  top: '48px',
+  right: '40px'
+});
 
 export default class QueueSelectorDropdown extends React.Component {
   constructor(props) {
@@ -38,38 +28,24 @@ export default class QueueSelectorDropdown extends React.Component {
 
   render = () => {
     const { items } = this.props;
-    let dropdownButtonList;
 
     if (items.length < 1) {
       return null;
     }
 
-    if (this.state.menu) {
-      dropdownButtonList = <ul className="cf-dropdown-menu active" {...styles.dropdownList}>
-        {items.map((item) => {
-          const linkProps = {
-            className: 'usa-button-secondary usa-button',
-            onClick: this.toggleMenuVisible,
-            href: item.href,
-            to: item.to
-          };
+    const list = items.map((item) => {
+      return {
+        title: item.label,
+        target: item.to || item.href
+      };
+    });
 
-          return <li key={item.key}>
-            <Link {...linkProps}>
-              {item.label}
-            </Link>
-          </li>;
-        })}
-      </ul>;
-    }
-
-    return <div className="cf-dropdown" {...styles.dropdownButton}>
-      <a onClick={this.toggleMenuVisible}
-        className="cf-dropdown-trigger usa-button usa-button-secondary"
-        {...styles.dropdownTrigger}>
-        {COPY.CASE_LIST_TABLE_QUEUE_DROPDOWN_LABEL}
-      </a>
-      {dropdownButtonList}
+    return <div className="cf-dropdown" {...style}>
+      <DropdownButton
+        lists={list}
+        onClick={this.toggleMenuVisible}
+        label={COPY.CASE_LIST_TABLE_QUEUE_DROPDOWN_LABEL}
+      />
     </div>;
   }
 }
