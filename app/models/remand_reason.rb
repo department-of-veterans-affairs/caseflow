9fd4ec257463b2bbaabc6593eebdad116dafb80d
aroltsch <aroltsch@tistatech.com>
2019-04-01 12:48:34 -0400

    Lint

--- app/models/remand_reason.rb
+++ app/models/remand_reason.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RemandReason < ApplicationRecord
-  self.ignored_columns = %w(request_issue_id)
+  self.ignored_columns = %w[request_issue_id]
   validates :code, inclusion: { in: Constants::AMA_REMAND_REASONS_BY_ID.values.map(&:keys).flatten }
   validates :post_aoj, inclusion: { in: [true, false] }
   belongs_to :decision_issue
