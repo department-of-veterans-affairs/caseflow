f9c9da54672429db89363876684b946591055b20
Sjones352 <sdjones@go-ct.com>
2019-04-02 21:02:48 -0400

    removed disable length

--- app/models/task.rb
+++ app/models/task.rb
@@ -268,13 +268,14 @@ class Task < ApplicationRecord
     nil
   end
 
-  # rubocop:disable Metrics/LineLength
   def cancel_task_and_child_subtasks
     # Cancel all descendants at the same time to avoid after_update hooks marking some tasks as completed.
     descendant_ids = descendants.pluck(:id)
-    Task.active.where(id: descendant_ids).update_all(status: Constants.TASK_STATUSES.cancelled, closed_at: Time.zone.now)
+    Task.active.where(id: descendant_ids).update_all(
+      status: Constants.TASK_STATUSES.cancelled,
+      closed_at: Time.zone.now
+    )
   end
-  # rubocop:enable Metrics/LineLength
 
   def assign_to_organization_data(_user = nil)
     organizations = Organization.assignable(self).map do |organization|
