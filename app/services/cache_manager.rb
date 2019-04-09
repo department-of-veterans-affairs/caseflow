a27ce1683ba2bd825e121309e02f122080220862
Peter Karman <peter@truss.works>
2019-04-09 17:16:01 -0500

    Add more cache keys

--- app/services/cache_manager.rb
+++ app/services/cache_manager.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 class CacheManager
   class NoSuchBucket < StandardError; end
 
   BUCKETS = {
     bgs: [
-
+      "bgs_can_access_*",
+      "SupplementalClaim-*-ratings-*-cached-*",
+      "HigherLevelReview-*-ratings-*-cached-*",
+      "Appeal-*-ratings-*-cached-*"
     ],
-    vbms: [
-
-    ],
+    vbms: [],
     vacols: [
       "#{Rails.env}_list_of_judges_from_vacols",
       "#{Rails.env}_list_of_judges_from_vacols_with_name_and_id",
@@ -15,13 +18,27 @@ class CacheManager
       "#{Rails.env}_list_of_hearing_coordinators_from_vacols"
     ],
     caseflow: [
-
+      "EstablishClaim-*-cached-*",
+      "LegacyAppeal-*-cached-*",
+      "LegacyHearing-*-cached-*",
+      "JudgeSchedulePeriod-*-cached-*",
+      "SchedulePeriod-*-cached-*",
+      "RoSchedulePeriod-*-cached-*",
+      "RampElectionIntake-*-cached-*",
+      "IntakeStats-last-calculated-timestamp"
     ]
   }.freeze
 
+  def self.rails_cache
+    @rails_cache ||= ActiveSupport::Cache.lookup_store(Rails.configuration.cache_store)
+  end
+
+  # NOTE that development and test envs currently do not use the Redis store, so this only applies
+  # to production environments (AWS)
   def self.cache_store
     @cache_store ||= begin
       redis_conn = Redis.new(url: Rails.application.secrets.redis_url_cache)
+      Redis::Namespace.new("cache", redis: redis_conn)
     end
   end
 
@@ -30,6 +47,7 @@ class CacheManager
     keys = []
     self.class.cache_store.scan_each do |key|
       next if key.match?(/^(feature_toggle|functions):/)
+
       keys << key
     end
     keys
@@ -39,8 +57,14 @@ class CacheManager
     key_names = BUCKETS[bucket.to_sym]
     fail NoSuchBucket, bucket unless key_names
 
+    rails_cache = self.class.rails_cache
+
     key_names.each do |key|
-      Rails.cache.delete(key)
+      if key.match?(/[\*\[\?]/)
+        rails_cache.delete_matched(key)
+      else
+        rails_cache.delete(key)
+      end
     end
   end
 end
