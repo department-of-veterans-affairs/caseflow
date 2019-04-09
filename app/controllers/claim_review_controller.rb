f4fd5806dfd6f7cb66141ad6dc8f99b0d913bb0c
Peter Karman <peter@truss.works>
2019-04-09 17:28:11 -0500

    add vbms or share

--- app/controllers/claim_review_controller.rb
+++ app/controllers/claim_review_controller.rb
@@ -11,7 +11,7 @@ class ClaimReviewController < ApplicationController
     # we call the serialization method here before the view does so we can rescue any data errors
     claim_review.ui_hash
   rescue RequestIssue::MissingDecisionDate => _err
-    flash[:error] = "One or more ratings may be locked on this Claim. Please try again in 24 hours."
+    flash[:error] = "VBMS or SHARE: One or more ratings may be locked on this Claim. Please try again in 24 hours."
     render "errors/500", layout: "application", status: :unprocessable_entity
   end
 
