c22dedf588f8eca870cdc4f918faafd6b54d7c10
lowellrex <lowell@navahq.com>
2019-04-09 09:12:49 -0400

    Remove unused serializer attribute

--- app/models/serializers/work_queue/user_serializer.rb
+++ app/models/serializers/work_queue/user_serializer.rb
@@ -4,8 +4,4 @@ class WorkQueue::UserSerializer
   include FastJsonapi::ObjectSerializer
   attribute :css_id
   attribute :full_name
-
-  attribute :is_admin do |object, params|
-    params[:check_if_admin] ? params[:organization].user_is_admin?(object) : nil
-  end
 end
