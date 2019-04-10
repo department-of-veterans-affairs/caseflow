b9b01a7ce11df5bbfa0121f221eadd74492671a8
youngfreezyVA <fareezahmed@macbook-pro-3.lan>
2019-04-10 09:15:28 -0400

    fix last code climate issue :-/

--- app/controllers/appeals_controller.rb
+++ app/controllers/appeals_controller.rb
@@ -46,7 +46,9 @@ class AppealsController < ApplicationController
     ids = params[:appeal_ids].split(",")
     document_counts_by_id = {}
     ids.each do |id|
-      document_counts_by_id[id] = appeal_by_id(id).number_of_documents
+      document_counts_by_id[id] =
+        Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(id)
+        .number_of_documents
     end
     document_counts_by_id
   end
@@ -112,10 +114,6 @@ class AppealsController < ApplicationController
 
   def appeal
     @appeal ||= Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(params[:appeal_id])
-  end
-
-  def appeal_by_id(id)
-    Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(id)
   end
 
   def url_appeal_uuid
