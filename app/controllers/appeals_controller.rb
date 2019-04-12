7dd0a2eb7c2a1af6d4913ac2a7a11018497265ea
youngfreezyVA <fareezahmed@MacBook-Pro-3.local>
2019-04-12 12:02:05 -0400

    add document_counts_by_appeal_id workflow

--- app/controllers/appeals_controller.rb
+++ app/controllers/appeals_controller.rb
@@ -35,38 +35,9 @@ class AppealsController < ApplicationController
   end
 
   def document_counts_by_id
-    render json: { document_counts_by_id: build_document_counts_hash({}) }
+    render json: { document_counts_by_id: DocumentCountsByAppealIdHash.new({}, params[:appeal_ids.split(",")]).call }
   rescue Caseflow::Error::EfolderAccessForbidden => error
     render(error.serialize_response)
-  end
-
-  def build_document_counts_hash(document_counts_by_id_hash)
-    params[:appeal_ids].split(",").each do |appeal_id|
-      begin
-        set_document_count_value_for_appeal_id(document_counts_by_id_hash, appeal_id)
-      rescue StandardError => err
-        handle_document_count_error(err, document_counts_by_id_hash, appeal_id)
-        next
-      end
-    end
-    document_counts_by_id_hash
-  end
-
-  def handle_document_count_error(err, document_counts_by_id_hash, appeal_id)
-    err_obj = serialize_error(err)
-    document_counts_by_id_hash[appeal_id] = {
-      error: err_obj[:err], status: err_obj[:code], count: nil
-    }
-  end
-
-  def set_document_count_value_for_appeal_id(hash, appeal_id)
-    hash[appeal_id] = {
-      count: Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(appeal_id)
-        .number_of_documents,
-      status: 200,
-      error: nil
-    }
-    hash
   end
 
   def power_of_attorney
