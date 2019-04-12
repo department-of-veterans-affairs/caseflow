b28bcb46112bcccf8a550d3fdb2dd9fe57251ad9
Sally Maki <sallymaki@gmail.com>
2019-04-12 16:35:07 -0700

    Fix edit message

--- client/app/intake/pages/decisionReviewEditCompleted.jsx
+++ client/app/intake/pages/decisionReviewEditCompleted.jsx
@@ -19,6 +19,8 @@ const leadMessageList = ({ veteran, formName, requestIssues, addedIssues }) => {
     } else if (_.every(addedIssues, (ri) => ri.withdrawalPending)) {
       return 'withdrawn';
     }
+
+    return 'processed';
   };
 
   const leadMessageArr = [
