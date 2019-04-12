87849101d001ffd9711374d6adef03dcb4f9480c
youngfreezyVA <fareezahmed@MacBook-Pro-3.local>
2019-04-12 13:31:43 -0400

    code climate feedback

--- app/workflows/document_counts_by_appeal_id_hash.rb
+++ app/workflows/document_counts_by_appeal_id_hash.rb
@@ -21,30 +21,38 @@ class DocumentCountsByAppealIdHash
 
   def build_document_counts_hash(document_counts_by_id_hash, appeal_ids)
     # Collect appeal objects sequentially so we don't exhaust DB pool
-    appeals = appeal_ids.each_with_object({}) do |appeal_id, result|
-      result[appeal_id] = Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(appeal_id)
-    end
-
+    appeals = collect_appeal_objects_sequentially(appeal_ids)
     # Spin up a new thread of each appeal and then call join on each thread
     # Creating threads without calling join on them will cause the main thread
     # to continue without waiting and possibly exit before the child threads have finished
+    create_thread_for_each_appeal(appeals, document_counts_by_id_hash)
+    document_counts_by_id_hash
+  end
+
+  def collect_appeal_objects_sequentially(appeal_ids)
+    appeal_ids.each_with_object({}) do |appeal_id, result|
+      result[appeal_id] = Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(appeal_id)
+    end
+  end
+
+  def create_thread_for_each_appeal(appeals, document_counts_by_id_hash)
     appeals.map do |appeal_id, appeal|
       Thread.new do
         begin
           set_document_count_value_for_appeal_id(document_counts_by_id_hash, appeal_id, appeal)
         rescue StandardError => err
+          err = serialize_error(err)
           handle_document_count_error(err, document_counts_by_id_hash, appeal_id)
           next
         end
       end
     end.map(&:join)
-    document_counts_by_id_hash
+
   end
 
   def handle_document_count_error(err, document_counts_by_id_hash, appeal_id)
-    err_obj = serialize_error(err)
     document_counts_by_id_hash[appeal_id] = {
-      error: err_obj[:err], status: err_obj[:code], count: nil
+      error: err[:err], status: err[:code], count: nil
     }
   end
 
