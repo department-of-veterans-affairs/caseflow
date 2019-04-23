d751d71b591e0777392e34d52238591185717005
youngfreezyVA <fareezahmed@macbook-pro-3.lan>
2019-04-23 15:20:34 -0400

    move mocks to separate file

--- client/test/karma/queue/ColocatedTaskListView-test.js
+++ client/test/karma/queue/ColocatedTaskListView-test.js
@@ -12,6 +12,7 @@ import { onReceiveQueue, receiveNewDocumentsForTask, errorFetchingDocumentCount,
   from '../../../app/queue/QueueActions';
 import { setUserCssId } from '../../../app/queue/uiReducer/uiActions';
 import { BrowserRouter } from 'react-router-dom';
+import { ids, mockResponse } from './mocks';
 
 describe('ColocatedTaskListView', () => {
   let wrapperColocatedTaskListView = null;
@@ -173,39 +174,10 @@ describe('ColocatedTaskListView', () => {
       expect(docketNumber.text()).to.include(appeal.docketNumber);
       expect(daysWaiting.text()).to.equal('1');
       expect(documents.html()).to.include(`/reader/appeal/${task.externalAppealId}/documents`);
-      const ids = ['3575931', '3619838', '3625593', '3626186', 'e22b7880-84a3-4f23-b5ba-0b9837eca4e8',
-        '8d8b533e-3a45-42d9-92c2-a736a436dc67', 'fff266d5-2952-4baa-b8fe-abf41cc33eda',
-        '80949be2-2a7b-419c-83a1-abfbe4d2aa56', 'a814cfbc-c6ef-4835-998b-db5cbb882c0a',
-        'fff266d5-2952-4baa-b8fe-abf41cc33eda', '80949be2-2a7b-419c-83a1-abfbe4d2aa56',
-        'a814cfbc-c6ef-4835-998b-db5cbb882c0a', '2096907', '2226048', '2249056', '2306397', '2657227',
-        'f31dc143-768c-45e1-b2ba-19af4df25be9', 'ab78acb4-6335-4579-8679-71f8871740b4',
-        '5def02ad-e376-43be-be59-a31f768b6073'];
 
       store.dispatch(errorFetchingDocumentCount(ids));
       expect(wrapper.find('td').at(6).
         text()).to.include('View  docs');
-      const mockResponse = {
-        'a814cfbc-c6ef-4835-998b-db5cbb882c0a': {
-          count: 1,
-          status: 200,
-          error: null
-        },
-        '8d8b533e-3a45-42d9-92c2-a736a436dc67': {
-          count: 1,
-          status: 200,
-          error: null
-        },
-        '80949be2-2a7b-419c-83a1-abfbe4d2aa56': {
-          count: 0,
-          status: 200,
-          error: null
-        },
-        'fff266d5-2952-4baa-b8fe-abf41cc33eda': {
-          count: 1,
-          status: 200,
-          error: null
-        }
-      };
 
       store.dispatch(setAppealDocCount(mockResponse));
       expect(wrapper.find('td').at(5).
---
+++ client/test/karma/queue/mocks.js
@@ -0,0 +1,26 @@
+export const ids = ['3575931', '3619838', '3625593', '3626186', 'e22b7880-84a3-4f23-b5ba-0b9837eca4e8',
+  '8d8b533e-3a45-42d9-92c2-a736a436dc67', 'fff266d5-2952-4baa-b8fe-abf41cc33eda',
+  '80949be2-2a7b-419c-83a1-abfbe4d2aa56', 'a814cfbc-c6ef-4835-998b-db5cbb882c0a',
+  'fff266d5-2952-4baa-b8fe-abf41cc33eda', '80949be2-2a7b-419c-83a1-abfbe4d2aa56',
+  'a814cfbc-c6ef-4835-998b-db5cbb882c0a', '2096907', '2226048', '2249056', '2306397', '2657227',
+  'f31dc143-768c-45e1-b2ba-19af4df25be9', 'ab78acb4-6335-4579-8679-71f8871740b4',
+  '5def02ad-e376-43be-be59-a31f768b6073'];
+
+export const mockResponse = {
+  'a814cfbc-c6ef-4835-998b-db5cbb882c0a': {
+    count: 1,
+    status: 200
+  },
+  '8d8b533e-3a45-42d9-92c2-a736a436dc67': {
+    count: 1,
+    status: 200
+  },
+  '80949be2-2a7b-419c-83a1-abfbe4d2aa56': {
+    count: 0,
+    status: 200
+  },
+  'fff266d5-2952-4baa-b8fe-abf41cc33eda': {
+    count: 1,
+    status: 200
+  }
+};
