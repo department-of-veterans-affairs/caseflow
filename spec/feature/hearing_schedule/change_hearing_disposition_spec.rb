cb165577a91be3e36e9839e99f0ed393edf7df02
Tomas Apodaca <tomas@navahq.com>
2019-04-22 17:16:20 -0700

    update ChangeHearingDispositionTasks behavior
    ChangeHearingDispositionTasks are assigned to the HearingAdmin org and
    can only be assigned to HearingAdmin users.

--- spec/feature/hearing_schedule/change_hearing_disposition_spec.rb
+++ spec/feature/hearing_schedule/change_hearing_disposition_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.feature "Change hearing disposition" do
   let(:current_full_name) { "Leonela Harbold" }
-  let(:hearing_user) { FactoryBot.create(:user, full_name: current_full_name, css_id: "BVATWARNER", station_id: 101) }
+  let(:hearing_admin_user) { FactoryBot.create(:user, full_name: current_full_name, station_id: 101) }
   let(:hearing_day) { FactoryBot.create(:hearing_day) }
   let(:veteran) { FactoryBot.create(:veteran, first_name: "Chibueze", last_name: "Vanscoy", file_number: 800_888_001) }
   let(:appeal) { FactoryBot.create(:appeal, :hearing_docket, veteran_file_number: veteran.file_number) }
@@ -17,9 +17,8 @@ RSpec.feature "Change hearing disposition" do
   let(:instructions_text) { "This is why I'm changing this hearing's disposition." }
 
   before do
-    OrganizationsUser.add_user_to_organization(hearing_user, HearingAdmin.singleton)
-    OrganizationsUser.add_user_to_organization(hearing_user, HearingsManagement.singleton)
-    User.authenticate!(user: hearing_user)
+    OrganizationsUser.add_user_to_organization(hearing_admin_user, HearingAdmin.singleton)
+    User.authenticate!(user: hearing_admin_user)
   end
 
   context "there are transcription and mail team members" do
@@ -69,26 +68,47 @@ RSpec.feature "Change hearing disposition" do
     end
   end
 
-  context "there's a BVA team member" do
-    let(:bva_user) { FactoryBot.create(:user, full_name: "Sun Ma") }
-
-    before do
-      OrganizationsUser.add_user_to_organization(bva_user, Bva.singleton)
+  scenario "change hearing disposition to cancelled" do
+    step "visit the hearing admin organization queue and click on the veteran's name" do
+      visit "/organizations/#{HearingAdmin.singleton.url}"
+      expect(page).to have_content("Unassigned (1)")
+      click_on veteran_link_text
     end
 
-    scenario "change hearing disposition to cancelled" do
+    step "change the hearing disposition to cancelled" do
+      click_dropdown(prompt: "Select an action", text: "Change hearing disposition")
+      click_dropdown(prompt: "Select", text: "Cancelled", container: ".cf-modal-body")
+      fill_in "Notes", with: instructions_text
+      click_button("Submit")
+      expect(page).to have_content("Successfully changed hearing disposition to Cancelled")
+    end
+
+    step "return to the hearing admin organization queue and verify that the task is no longer there" do
+      click_queue_switcher COPY::CASE_LIST_TABLE_QUEUE_DROPDOWN_TEAM_CASES_LABEL % HearingAdmin.singleton.name
+      expect(page).to have_content("Unassigned (0)")
+    end
+  end
+
+  context "there's a hearings management user" do
+    let(:hearing_mgmt_user) { FactoryBot.create(:user, full_name: "Janaan Handal", station_id: 101) }
+
+    before do
+      OrganizationsUser.add_user_to_organization(hearing_mgmt_user, HearingsManagement.singleton)
+    end
+
+    scenario "change hearing disposition to postponed" do
       step "visit the hearing admin organization queue and click on the veteran's name" do
         visit "/organizations/#{HearingAdmin.singleton.url}"
         expect(page).to have_content("Unassigned (1)")
         click_on veteran_link_text
       end
 
-      step "change the hearing disposition to cancelled" do
+      step "change the hearing disposition to postponed" do
         click_dropdown(prompt: "Select an action", text: "Change hearing disposition")
-        click_dropdown(prompt: "Select", text: "Cancelled", container: ".cf-modal-body")
+        click_dropdown(prompt: "Select", text: "Postponed", container: ".cf-modal-body")
         fill_in "Notes", with: instructions_text
         click_button("Submit")
-        expect(page).to have_content("Successfully changed hearing disposition to Cancelled")
+        expect(page).to have_content("Successfully changed hearing disposition to Postponed")
       end
 
       step "return to the hearing admin organization queue and verify that the task is no longer there" do
@@ -96,46 +116,18 @@ RSpec.feature "Change hearing disposition" do
         expect(page).to have_content("Unassigned (0)")
       end
 
-      step "visit and verify that the hearing task is completed in the bva queue" do
-        User.authenticate!(user: bva_user)
-        visit "/organizations/#{Bva.singleton.url}"
-        click_on COPY::QUEUE_PAGE_COMPLETE_TAB_TITLE
-        expect(page).to have_content veteran_link_text
-        expect(page).to have_content "Hearing Task"
+      step "visit the hearings management organization queue and verify that the schedule hearing task is there" do
+        User.authenticate!(user: hearing_mgmt_user)
+        visit "/organizations/#{HearingsManagement.singleton.url}"
+        expect(page).to have_content("Unassigned (1)")
+        click_on veteran_link_text
+        expect(page).to have_content(ScheduleHearingTask.last.label)
       end
-    end
-  end
 
-  scenario "change hearing disposition to postponed" do
-    step "visit the hearing admin organization queue and click on the veteran's name" do
-      visit "/organizations/#{HearingAdmin.singleton.url}"
-      expect(page).to have_content("Unassigned (1)")
-      click_on veteran_link_text
-    end
-
-    step "change the hearing disposition to postponed" do
-      click_dropdown(prompt: "Select an action", text: "Change hearing disposition")
-      click_dropdown(prompt: "Select", text: "Postponed", container: ".cf-modal-body")
-      fill_in "Notes", with: instructions_text
-      click_button("Submit")
-      expect(page).to have_content("Successfully changed hearing disposition to Postponed")
-    end
-
-    step "return to the hearing admin organization queue and verify that the task is no longer there" do
-      click_queue_switcher COPY::CASE_LIST_TABLE_QUEUE_DROPDOWN_TEAM_CASES_LABEL % HearingAdmin.singleton.name
-      expect(page).to have_content("Unassigned (0)")
-    end
-
-    step "visit the hearings management organization queue and verify that the schedule hearing task is there" do
-      click_queue_switcher COPY::CASE_LIST_TABLE_QUEUE_DROPDOWN_TEAM_CASES_LABEL % HearingsManagement.singleton.name
-      expect(page).to have_content("Unassigned (1)")
-      click_on veteran_link_text
-      expect(page).to have_content(ScheduleHearingTask.last.label)
-    end
-
-    step "verify that the instructions are visible on the schedule hearing task" do
-      find("#currently-active-tasks button", text: COPY::TASK_SNAPSHOT_VIEW_TASK_INSTRUCTIONS_LABEL).click
-      expect(page).to have_content(instructions_text)
+      step "verify that the instructions are visible on the schedule hearing task" do
+        find("#currently-active-tasks button", text: COPY::TASK_SNAPSHOT_VIEW_TASK_INSTRUCTIONS_LABEL).click
+        expect(page).to have_content(instructions_text)
+      end
     end
   end
 
@@ -146,7 +138,7 @@ RSpec.feature "Change hearing disposition" do
       click_on veteran_link_text
     end
 
-    step "change the hearing disposition to cancelled" do
+    step "change the hearing disposition to no show" do
       click_dropdown(prompt: "Select an action", text: "Change hearing disposition")
       click_dropdown(prompt: "Select", text: "No Show", container: ".cf-modal-body")
       fill_in "Notes", with: instructions_text
@@ -167,14 +159,25 @@ RSpec.feature "Change hearing disposition" do
     end
   end
 
-  context "there's another member in the hearing admin organization" do
-    let(:other_full_name) { "Remika Hanisco" }
-    let!(:other_user) { FactoryBot.create(:user, full_name: other_full_name, css_id: "OTHERUSER", station_id: 101) }
+  context "there are other hearing admin and hearings management members" do
+    let(:other_admin_full_name) { "Remika Hanisco" }
+    let!(:other_admin_user) { FactoryBot.create(:user, full_name: other_admin_full_name, station_id: 101) }
+    let(:admin_full_names) { ["Bisar Helget", "Rose Hidrogo", "Rihab Hancin", "Abby Hudmon"] }
+    let(:mgmt_full_names) { ["Claudia Heraty", "Nouf Heigl", "Hayley Houlahan", "Bahiya Haese"] }
     let(:assign_instructions_text) { "This is why I'm assigning this to you." }
 
     before do
-      OrganizationsUser.add_user_to_organization(other_user, HearingAdmin.singleton)
-      OrganizationsUser.add_user_to_organization(other_user, HearingsManagement.singleton)
+      OrganizationsUser.add_user_to_organization(other_admin_user, HearingAdmin.singleton)
+
+      admin_full_names.each do |name|
+        user = FactoryBot.create(:user, full_name: name, station_id: 101)
+        OrganizationsUser.add_user_to_organization(user, HearingAdmin.singleton)
+      end
+
+      mgmt_full_names.each do |name|
+        user = FactoryBot.create(:user, full_name: name, station_id: 101)
+        OrganizationsUser.add_user_to_organization(user, HearingsManagement.singleton)
+      end
     end
 
     scenario "assign change hearing disposition task to another user" do
@@ -186,14 +189,19 @@ RSpec.feature "Change hearing disposition" do
 
       step "assign the task to the other user" do
         click_dropdown(prompt: "Select an action", text: "Assign to person")
-        click_dropdown(text: other_full_name, container: ".cf-modal-body")
+        choices = click_dropdown(text: other_admin_full_name, container: ".cf-modal-body")
+
+        # only hearing admin users can be assigned the task
+        expect(choices).to include(*admin_full_names)
+        expect(choices).to_not include(*mgmt_full_names)
+
         fill_in COPY::ADD_COLOCATED_TASK_INSTRUCTIONS_LABEL, with: assign_instructions_text
         click_on "Submit"
-        expect(page).to have_content COPY::REASSIGN_TASK_SUCCESS_MESSAGE % other_full_name
+        expect(page).to have_content COPY::REASSIGN_TASK_SUCCESS_MESSAGE % other_admin_full_name
       end
 
       step "the other user logs in and sees the task in their queue" do
-        User.authenticate!(user: other_user)
+        User.authenticate!(user: other_admin_user)
         visit "/queue"
         click_on "#{appeal.veteran_full_name} (#{appeal.veteran_file_number})"
         expect(page).to have_content(ChangeHearingDispositionTask.last.label)
