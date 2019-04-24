96c2bd177d892bc8cdf4024d5f994fdcad4c516d
Kevin Moore <k.james.moore@gmail.com>
2019-04-24 11:46:18 -0400

    Initial scaffolding of PrivacyActTask.
    Inherits from Generic Task. Done primarily to write an available_actions subtask.

--- app/models/tasks/privacy_task.rb
+++ app/models/tasks/privacy_task.rb
@@ -4,17 +4,41 @@
 # Task to track when an appeal has been assigned to Privacy Team
 
 class PrivacyActTask < GenericTask
+  # def available_actions(user)
+  #   return super if assigned_to != user
+  #
+  #   [
+  #     Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+  #     Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
+  #     Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+  #   ]
+  # end
+
   def available_actions(user)
-    return super if assigned_to != user
+    return [] unless user
 
-    [
-      Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
-      # Constants.TASK_ACTIONS.RETURN_TO_JUDGE.to_h,
-      Constants.TASK_ACTIONS.CANCEL_TASK.to_h
-    ]
-  end
+    if assigned_to == user
+      return [
+        Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
+        Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+        Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+      ]
+    end
 
-  def self.create_from_root_task(root_task)
-    create!(assigned_to: PrivacyTeam.singleton, parent_id: root_task.id, appeal: root_task.appeal)
+    if task_is_assigned_to_user_within_organization?(user)
+      return [
+        Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h
+      ]
+    end
+
+    if task_is_assigned_to_users_organization?(user)
+      return [
+        Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h,
+        Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+        Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+      ]
+    end
+
+    []
   end
 end
