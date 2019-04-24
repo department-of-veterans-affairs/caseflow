bfd615fa26bc118e72730e0766464813c1265bf5
Andrew Lomax <andrewlomaxr@gmail.com>
2019-04-24 11:58:15 -0400

    right test

--- spec/feature/hearing_schedule/list_schedule_spec.rb
+++ spec/feature/hearing_schedule/list_schedule_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature "List Schedule" do
       scenario "Correct days are displayed" do
         visit "hearings/schedule"
 
-        expect(page).to_not have_content(Hearing::HEARING_TYPES[HearingDay.first.request_type.to_sym])
+        expect(page).to have_content(Hearing::HEARING_TYPES[HearingDay.first.request_type.to_sym])
       end
 
       scenario "All buttons are visible" do
