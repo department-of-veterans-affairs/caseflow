ee6795a2945044e8abfeb59fd682dd36b34563d0
aroltsch <aroltsch@tistatech.com>
2019-04-24 14:05:21 -0400

    Add more tests

--- spec/workflows/document_counts_by_appeal_id_spec.rb
+++ spec/workflows/document_counts_by_appeal_id_spec.rb
@@ -10,36 +10,47 @@ describe DocumentCountsByAppealId do
           DocumentCountsByAppealId.new(
             appeal_ids: %w[123 123 123 123 123 123]
           ).call
-        end .to raise_error(Caseflow::Error::TooManyAppealIds)
+        end.to raise_error(Caseflow::Error::TooManyAppealIds)
       end
     end
-    context "when there are less than 5 ids in the request" do
-      let(:veteran1) { create(:veteran) }
 
-      let!(:ama_appeal) { create(:appeal, veteran: veteran1, number_of_claimants: 2) }
+    context "when there are less than 5 ids in the request" do
+      let!(:ama_appeal) { create(:appeal) }
 
       it "returns the appropriate hash via private methods" do
+        allow_any_instance_of(DocumentFetcher).to receive(:number_of_documents)
+          .and_return(10)
         result = DocumentCountsByAppealId.new(
           appeal_ids: [ama_appeal.external_id]
         ).call
         count_hash = result[ama_appeal.external_id]
         expect(count_hash).to_not eq(nil)
-        expect(count_hash[:count]).to eq(0)
+        expect(count_hash[:count]).to eq(10)
         expect(count_hash[:status]).to eq(200)
         expect(count_hash[:error]).to eq(nil)
       end
     end
+
+    context "when there is an error fetching documents" do
+      let!(:ama_appeal) { create(:appeal) }
+
+      it "returns the appropriate hash via private methods" do
+        error_msg = "Document count did not succeed"
+        error = Caseflow::Error::DocumentRetrievalError.new(code: 502, message: error_msg)
+        allow_any_instance_of(DocumentFetcher).to receive(:number_of_documents).and_raise(error)
+        result = DocumentCountsByAppealId.new(
+          appeal_ids: [ama_appeal.external_id]
+        ).call
+        response = { ama_appeal.external_id => { error: error_msg, status: 502 } }
+        expect(result).to eq response
+      end
+    end
+
     context "when the appeal id does not exist" do
-      let(:veteran1) { create(:veteran) }
-
-      let!(:ama_appeal) { create(:appeal, veteran: veteran1, number_of_claimants: 2) }
-
-      it "throws an ActiveRecord not found error" do
-        expect do
-          DocumentCountsByAppealId.new(
-            appeal_ids: %w[31r13r13r]
-          ).call
-        end .to raise_error(ActiveRecord::RecordNotFound)
+      it "returns a hash with error code" do
+        result = DocumentCountsByAppealId.new(appeal_ids: %w[31r13r13r]).call
+        response = { "31r13r13r" => { error: "ActiveRecord::RecordNotFound", status: 404 } }
+        expect(result).to eq response
       end
     end
   end
