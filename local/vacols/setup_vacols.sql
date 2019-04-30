64a4fdee9472be9dfd91e621ad27c05f65860050
aroltsch <aroltsch@tistatech.com>
2019-04-30 14:04:21 -0400

    Create vacols dev DB

--- local/vacols/setup_vacols.sql
+++ local/vacols/setup_vacols.sql
@@ -6,6 +6,17 @@ CREATE USER VACOLS_TEST IDENTIFIED BY VACOLS_TEST;
 GRANT UNLIMITED TABLESPACE TO VACOLS_TEST;
 GRANT CREATE SESSION TO VACOLS_TEST;
 
+CREATE USER VACOLS_DEV IDENTIFIED BY VACOLS_DEV;
+GRANT UNLIMITED TABLESPACE TO VACOLS_DEV;
+GRANT CREATE SESSION TO VACOLS_DEV;
+
+@/ORCL/vacols_copy_2_tables_dev.sql
+@/ORCL/vacols_copy_3_indexes_dev.sql
+@/ORCL/vacols_copy_7_sequences_dev.sql
+@/ORCL/vacols_copy_4_triggers_dev.sql
+@/ORCL/vacols_copy_5_functions_dev.sql
+@/ORCL/vacols_copy_6_procedures_dev.sql
+
 @/ORCL/vacols_copy_1_tablespaces.sql
 @/ORCL/vacols_copy_2_tables.sql
 @/ORCL/vacols_copy_3_indexes.sql
