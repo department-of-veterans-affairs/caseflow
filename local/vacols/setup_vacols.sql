b5dd959074ac558638d377728fe04072a423a65c
aroltsch <aroltsch@tistatech.com>
2019-05-01 10:07:37 -0400

    Fix

--- local/vacols/setup_vacols.sql
+++ local/vacols/setup_vacols.sql
@@ -10,6 +10,8 @@ CREATE USER VACOLS_DEV IDENTIFIED BY VACOLS_DEV;
 GRANT UNLIMITED TABLESPACE TO VACOLS_DEV;
 GRANT CREATE SESSION TO VACOLS_DEV;
 
+@/ORCL/vacols_copy_1_tablespaces.sql
+
 @/ORCL/vacols_copy_2_tables_dev.sql
 @/ORCL/vacols_copy_3_indexes_dev.sql
 @/ORCL/vacols_copy_7_sequences_dev.sql
@@ -17,11 +19,10 @@ GRANT CREATE SESSION TO VACOLS_DEV;
 @/ORCL/vacols_copy_5_functions_dev.sql
 @/ORCL/vacols_copy_6_procedures_dev.sql
 
-@/ORCL/vacols_copy_1_tablespaces.sql
-@/ORCL/vacols_copy_2_tables.sql
-@/ORCL/vacols_copy_3_indexes.sql
-@/ORCL/vacols_copy_7_sequences.sql
-@/ORCL/vacols_copy_4_triggers.sql
-@/ORCL/vacols_copy_5_functions.sql
-@/ORCL/vacols_copy_6_procedures.sql
+@/ORCL/vacols_copy_2_tables_test.sql
+@/ORCL/vacols_copy_3_indexes_test.sql
+@/ORCL/vacols_copy_7_sequences_test.sql
+@/ORCL/vacols_copy_4_triggers_test.sql
+@/ORCL/vacols_copy_5_functions_test.sql
+@/ORCL/vacols_copy_6_procedures_test.sql
 exit
