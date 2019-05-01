d34adcf3df91350de1b634cb52f2889374cca832
aroltsch <aroltsch@tistatech.com>
2019-05-01 11:28:17 -0400

    Move files under vacols_copy

--- local/vacols/setup_vacols.sql
+++ local/vacols/setup_vacols.sql
@@ -2,27 +2,25 @@ CONNECT sys/Oradoc_db1@BVAP as sysdba
 
 ALTER SESSION set "_ORACLE_SCRIPT"=true;
 
-CREATE USER VACOLS_TEST IDENTIFIED BY VACOLS_TEST;
-GRANT UNLIMITED TABLESPACE TO VACOLS_TEST;
-GRANT CREATE SESSION TO VACOLS_TEST;
-
 CREATE USER VACOLS_DEV IDENTIFIED BY VACOLS_DEV;
 GRANT UNLIMITED TABLESPACE TO VACOLS_DEV;
 GRANT CREATE SESSION TO VACOLS_DEV;
 
+CREATE USER VACOLS_TEST IDENTIFIED BY VACOLS_TEST;
+GRANT UNLIMITED TABLESPACE TO VACOLS_TEST;
+GRANT CREATE SESSION TO VACOLS_TEST;
+
 @/ORCL/vacols_copy_1_tablespaces.sql
-
 @/ORCL/vacols_copy_2_tables_dev.sql
-@/ORCL/vacols_copy_3_indexes_dev.sql
-@/ORCL/vacols_copy_7_sequences_dev.sql
-@/ORCL/vacols_copy_4_triggers_dev.sql
-@/ORCL/vacols_copy_5_functions_dev.sql
-@/ORCL/vacols_copy_6_procedures_dev.sql
-
 @/ORCL/vacols_copy_2_tables_test.sql
+@/ORCL/vacols_copy_3_indexes_dev.sql
 @/ORCL/vacols_copy_3_indexes_test.sql
+@/ORCL/vacols_copy_7_sequences_dev.sql
 @/ORCL/vacols_copy_7_sequences_test.sql
+@/ORCL/vacols_copy_4_triggers_dev.sql
 @/ORCL/vacols_copy_4_triggers_test.sql
+@/ORCL/vacols_copy_5_functions_dev.sql
 @/ORCL/vacols_copy_5_functions_test.sql
+@/ORCL/vacols_copy_6_procedures_dev.sql
 @/ORCL/vacols_copy_6_procedures_test.sql
 exit
