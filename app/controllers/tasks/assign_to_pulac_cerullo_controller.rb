fc92cabac83656412d12083c1e6e3ecf89521698
youngfreezyVA <fareezahmed@MacBook-Pro-3.local>
2019-05-02 14:33:51 -0400

    render json to get a 200 instead of 204

--- app/controllers/tasks/assign_to_pulac_cerullo_controller.rb
+++ app/controllers/tasks/assign_to_pulac_cerullo_controller.rb
@@ -5,13 +5,17 @@ class Tasks::AssignToPulacCerulloController < ApplicationController
     task = task_params
     task_id = task[:appeal][:id]
     assigned_by_id = task[:assignedTo][:id]
-    Task.create!(
+    child_task = Task.create!(
       type: task[:type],
       appeal: Appeal.find_by(id: task_id),
       assigned_by_id: assigned_by_id,
       parent_id: task[:id],
       assigned_to: PulacCurello.singleton
     )
+    render json: {
+      child_task_assigned_to_pulac_cerullo: child_task
+    }
+
   end
 
   def task_params
