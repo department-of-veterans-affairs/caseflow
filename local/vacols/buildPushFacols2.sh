9b9f4cbf0efb09672ff4be6e40668bf2368d4c85
Enrique Valenzuela <evalenzuela@evalenzuela.local>
2019-05-02 15:22:45 -0400

    created a vacols image with oracle all in one, testing it with that

--- local/vacols/buildPushFacols2.sh
+++ local/vacols/buildPushFacols2.sh
@@ -2,9 +2,8 @@
 
 eval $(aws ecr get-login --no-include-email --region us-gov-west-1)
 
-docker build -t facols2 .
-docker tag facols2:latest 008577686731.dkr.ecr.us-gov-west-1.amazonaws.com/facols2:version1
-if docker push 008577686731.dkr.ecr.us-gov-west-1.amazonaws.com/facols2:version1 ; then
+docker tag fake:2 008577686731.dkr.ecr.us-gov-west-1.amazonaws.com/facols2:2
+if docker push 008577686731.dkr.ecr.us-gov-west-1.amazonaws.com/facols2:2 ; then
   echo 'Success the latest docker image has been pushed.'
 else
   echo 'Failed. You likely need to sign in with MFA https://aws.amazon.com/premiumsupport/knowledge-center/authenticate-mfa-cli/'
