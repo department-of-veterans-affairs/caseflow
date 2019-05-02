07f7ffbb9ac8e5d2d3b7ecf79126794ec0d3293b
Sally Maki <sallymaki@gmail.com>
2019-05-02 14:57:42 -0700

    Add type to column removal to allow for rollback

--- db/migrate/20190430225016_remove_issue_category_from_request_issue.rb
+++ db/migrate/20190430225016_remove_issue_category_from_request_issue.rb
@@ -1,5 +1,5 @@
 class RemoveIssueCategoryFromRequestIssue < ActiveRecord::Migration[5.1]
   def change
-    safety_assured { remove_column :request_issues, :issue_category }
+    safety_assured { remove_column :request_issues, :issue_category, :string }
   end
 end
