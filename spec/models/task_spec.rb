2f6a3333ef55884d4a180c3152944baad031bda7
Tomas Apodaca <tomas@navahq.com>
2019-05-07 22:08:04 -0700

    move task action funcs into new repository object
    Freeing up space in models/task.rb which had gone over its line limit.
    Also removed timeline_details method from models/task.rb which was no
    longer being used.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -185,90 +185,6 @@ describe Task do
     end
   end
 
-  describe "#assign_to_user_data" do
-    let(:organization) { create(:organization, name: "Organization") }
-    let(:users) { create_list(:user, 3) }
-
-    before do
-      allow(organization).to receive(:users).and_return(users)
-    end
-
-    context "when assigned_to is an organization" do
-      let(:task) { create(:generic_task, assigned_to: organization) }
-
-      it "should return all members" do
-        expect(task.assign_to_user_data[:options]).to match_array(users.map { |u| { label: u.full_name, value: u.id } })
-      end
-
-      it "should return the task type of task" do
-        expect(task.assign_to_user_data[:type]).to eq(task.type)
-      end
-    end
-
-    context "when assigned_to's parent is an organization" do
-      let(:parent) { create(:generic_task, assigned_to: organization) }
-      let(:task) { create(:generic_task, assigned_to: users.first, parent: parent) }
-
-      it "should return all members except user" do
-        user_output = users[1..users.length - 1].map { |u| { label: u.full_name, value: u.id } }
-        expect(task.assign_to_user_data[:options]).to match_array(user_output)
-      end
-    end
-
-    context "when assigned_to is a user" do
-      let(:task) { create(:generic_task, assigned_to: users.first) }
-
-      it "should return all members except user" do
-        expect(task.assign_to_user_data[:options]).to match_array([])
-      end
-    end
-  end
-
-  describe "#return_to_attorney_data" do
-    let(:attorney) { FactoryBot.create(:user, station_id: User::BOARD_STATION_ID, full_name: "Janet Avilez") }
-    let!(:vacols_atty) { FactoryBot.create(:staff, :attorney_role, sdomainid: attorney.css_id) }
-    let(:judge) { FactoryBot.create(:user, station_id: User::BOARD_STATION_ID, full_name: "Aaron Judge") }
-    let!(:vacols_judge) { FactoryBot.create(:staff, :judge_role, sdomainid: judge.css_id) }
-    let!(:judge_team) { JudgeTeam.create_for_judge(judge) }
-    let(:judge_task) { FactoryBot.create(:ama_judge_decision_review_task, assigned_to: judge) }
-    let!(:attorney_task) do
-      FactoryBot.create(:ama_attorney_task, assigned_to: attorney, parent: judge_task, appeal: judge_task.appeal)
-    end
-
-    subject { judge_task.return_to_attorney_data }
-
-    context "there aren't any attorneys on the JudgeTeam" do
-      it "still shows the assigned attorney in selected and options" do
-        expect(subject[:selected]).to eq attorney
-        expect(subject[:options]).to eq [{ label: attorney.full_name, value: attorney.id }]
-      end
-    end
-
-    context "there are attorneys on the JudgeTeam" do
-      let(:attorney_names) { ["Jesse Abrecht", "Brenda Akery", "Crystal Andregg"] }
-
-      before do
-        OrganizationsUser.add_user_to_organization(attorney, judge_team)
-
-        attorney_names.each do |attorney_name|
-          another_attorney_on_the_team = FactoryBot.create(
-            :user, station_id: User::BOARD_STATION_ID, full_name: attorney_name
-          )
-          FactoryBot.create(:staff, :attorney_role, user: another_attorney_on_the_team)
-          OrganizationsUser.add_user_to_organization(another_attorney_on_the_team, judge_team)
-        end
-      end
-
-      it "shows the assigned attorney in selected, and all attorneys in options" do
-        expect(subject[:selected]).to eq attorney
-        expect(judge_team.non_admins.count).to eq attorney_names.count + 1
-        judge_team.non_admins.each do |team_attorney|
-          expect(subject[:options]).to include(label: team_attorney.full_name, value: team_attorney.id)
-        end
-      end
-    end
-  end
-
   describe "#cancel_task_and_child_subtasks" do
     let(:appeal) { create(:appeal) }
     let!(:top_level_task) { create(:task, appeal: appeal) }
@@ -481,9 +397,11 @@ describe Task do
 
       # Monkey patching might not be the best option, but we want to define a test_func
       # for our available actions unwrapper to call. This is the simplest way to do it
-      class Task
-        def test_func(_user)
-          { type: Task.name }
+      class TaskActionRepository
+        class << self
+          def test_func(_task, _user)
+            { type: Task.name }
+          end
         end
       end
 
@@ -656,18 +574,6 @@ describe Task do
         expect(task.closed_at).to eq(nil)
         task.update!(status: status)
         expect(task.closed_at).to_not eq(nil)
-      end
-    end
-  end
-
-  describe ".cancel_task_data" do
-    let(:task) { FactoryBot.create(:generic_task, assigned_by_id: assigner_id) }
-    subject { task.cancel_task_data }
-
-    context "when the task has no assigner" do
-      let(:assigner_id) { nil }
-      it "fills in the assigner name with placeholder text" do
-        expect(subject[:message_detail]).to eq(format(COPY::MARK_TASK_COMPLETE_CONFIRMATION_DETAIL, "the assigner"))
       end
     end
   end
