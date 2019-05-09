920210c733de85fa3ebfe1e2b439a45a1b3429ab
Andrew Lomax <andrewlomaxr@gmail.com>
2019-05-09 06:55:58 -0700

    move build_params method outside of context

--- spec/services/hearing_time_service_spec.rb
+++ spec/services/hearing_time_service_spec.rb
@@ -1,33 +1,32 @@
 # frozen_string_literal: true
 
 describe HearingTimeService do
+  describe "#build_params_with_time" do
+    let!(:params) do
+      { scheduled_time_string: "13:30" }
+    end
+
+    it "returns scheduled_time string parameter and removes scheduled_time_string param" do
+      expect(HearingTimeService.build_params_with_time(hearing, params)).to eq(scheduled_time: "13:30")
+      expect(params).to eq(scheduled_time_string: "13:30")
+    end
+  end
+
+  describe "#build_legacy_params_with_time" do
+    let(:params) do
+      { scheduled_time_string: "13:30" }
+    end
+
+    it "returns scheduled_time string parameter and removes scheduled_time_string param" do
+      expected_params = { scheduled_for: legacy_hearing.scheduled_for.change(hour: 13, min: 30) }
+      expect(HearingTimeService.build_legacy_params_with_time(legacy_hearing, params)).to eq(expected_params)
+      expect(params).to eq(scheduled_time_string: "13:30")
+    end
+  end
+
   context "with a legacy hearing and a hearing scheduled for 12:00pm" do
     let!(:hearing) { create(:hearing, scheduled_time: "12:00") }
     let!(:legacy_hearing) { create(:legacy_hearing, scheduled_for: Time.now.utc.change(hour: 12, min: 0)) }
-
-    describe "#build_params_with_time" do
-      let!(:params) do
-        { scheduled_time_string: "13:30" }
-      end
-
-      it "returns scheduled_time string parameter and removes scheduled_time_string param" do
-        expect(HearingTimeService.build_params_with_time(hearing, params)).to eq(scheduled_time: "13:30")
-        expect(params).to eq(scheduled_time_string: "13:30")
-      end
-    end
-
-    describe "#build_legacy_params_with_time" do
-      let(:params) do
-        { scheduled_time_string: "13:30" }
-      end
-
-      it "returns scheduled_time string parameter and removes scheduled_time_string param" do
-        expected_params = { scheduled_for: legacy_hearing.scheduled_for.change(hour: 13, min: 30) }
-        expect(HearingTimeService.build_legacy_params_with_time(legacy_hearing, params)).to eq(expected_params)
-        expect(params).to eq(scheduled_time_string: "13:30")
-      end
-    end
-
     describe "#to_s" do
       it "builds string with time" do
         expect(legacy_hearing.time.to_s).to eq("12:00")
