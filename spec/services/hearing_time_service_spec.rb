07c53f1b2b4a5db3e90c705661b4724b7928701f
Andrew Lomax <andrewlomaxr@gmail.com>
2019-05-09 06:54:25 -0700

    refactor tests

--- spec/services/hearing_time_service_spec.rb
+++ spec/services/hearing_time_service_spec.rb
@@ -1,40 +1,44 @@
 # frozen_string_literal: true
 
 describe HearingTimeService do
-  describe "#build_params_with_time" do
-    let!(:hearing) { create(:hearing, scheduled_time: "12:00") }
-    let!(:legacy_hearing) { create(:legacy_hearing, scheduled_for: Time.now.utc.change(hour: 12, min: 0)) }
-    let!(:params) do
-      { scheduled_time_string: "13:30" }
-    end
-
-    it "returns scheduled_time string parameter and removes scheduled_time_string param" do
-      expect(HearingTimeService.build_params_with_time(hearing, params)).to eq(scheduled_time: "13:30")
-      expect(params).to eq(scheduled_time_string: "13:30")
-    end
-  end
-
-  describe "#build_legacy_params_with_time" do
-    let!(:hearing) { create(:hearing, scheduled_time: "12:00") }
-    let!(:legacy_hearing) { create(:legacy_hearing, scheduled_for: Time.now.utc.change(hour: 12, min: 0)) }
-    let(:params) do
-      { scheduled_time_string: "13:30" }
-    end
-
-    it "returns scheduled_time string parameter and removes scheduled_time_string param" do
-      expected_params = { scheduled_for: legacy_hearing.scheduled_for.change(hour: 13, min: 30) }
-      expect(HearingTimeService.build_legacy_params_with_time(legacy_hearing, params)).to eq(expected_params)
-      expect(params).to eq(scheduled_time_string: "13:30")
-    end
-  end
-
-  describe "#to_s" do
+  context "with a legacy hearing and a hearing scheduled for 12:00pm" do
     let!(:hearing) { create(:hearing, scheduled_time: "12:00") }
     let!(:legacy_hearing) { create(:legacy_hearing, scheduled_for: Time.now.utc.change(hour: 12, min: 0)) }
 
-    it "builds string with time" do
-      expect(legacy_hearing.time.to_s).to eq("12:00")
-      expect(hearing.time.to_s).to eq("12:00")
+    describe "#build_params_with_time" do
+      let!(:params) do
+        { scheduled_time_string: "13:30" }
+      end
+
+      it "returns scheduled_time string parameter and removes scheduled_time_string param" do
+        expect(HearingTimeService.build_params_with_time(hearing, params)).to eq(scheduled_time: "13:30")
+        expect(params).to eq(scheduled_time_string: "13:30")
+      end
+    end
+
+    describe "#build_legacy_params_with_time" do
+      let(:params) do
+        { scheduled_time_string: "13:30" }
+      end
+
+      it "returns scheduled_time string parameter and removes scheduled_time_string param" do
+        expected_params = { scheduled_for: legacy_hearing.scheduled_for.change(hour: 13, min: 30) }
+        expect(HearingTimeService.build_legacy_params_with_time(legacy_hearing, params)).to eq(expected_params)
+        expect(params).to eq(scheduled_time_string: "13:30")
+      end
+    end
+
+    describe "#to_s" do
+      it "builds string with time" do
+        expect(legacy_hearing.time.to_s).to eq("12:00")
+        expect(hearing.time.to_s).to eq("12:00")
+      end
+    end
+
+    describe "#to_datetime" do
+      it "converts legacy hearings to format consistent with hearings scheduled_time column" do
+        expect(legacy_hearing.time.to_datetime.iso8601).to eq("2000-01-01T12:00:00Z")
+      end
     end
   end
 
@@ -49,14 +53,6 @@ describe HearingTimeService do
         expect(hearing.time.central_office_time).to eq("15:00")
         expect(legacy_hearing.time.central_office_time).to eq("15:00")
       end
-    end
-  end
-
-  describe "#to_datetime" do
-    let!(:legacy_hearing) { create(:legacy_hearing, scheduled_for: Time.now.utc.change(hour: 12, min: 0)) }
-
-    it "converts legacy hearings to format consistent with hearings scheduled_time column" do
-      expect(legacy_hearing.time.to_datetime.iso8601).to eq("2000-01-01T12:00:00Z")
     end
   end
 end
