48a45ca435ff4f61aa6726a90d4152a0eddd0158
Ferris Tseng <ferris@navapbc.com>
2019-05-10 13:19:56 -0400

    add validation to cancel modal form

--- client/app/queue/components/CancelTaskAssignRegionalOfficeModal.jsx
+++ client/app/queue/components/CancelTaskAssignRegionalOfficeModal.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -8,24 +9,48 @@ import COPY from '../../../COPY.json';
 import QueueFlowModal from './QueueFlowModal';
 import RegionalOfficeDropdown from '../../components/DataDropdowns/RegionalOffice';
 import TextareaField from '../../components/TextareaField';
+import { taskById } from '../selectors';
+import { highlightInvalidFormItems } from '../uiReducer/uiActions';
 
 class CancelTaskAssignRegionalOfficeModal extends React.Component {
-  onRegionalOfficeSelected = (value, label) => {
-  };
+  constructor(props) {
+    super(props);
 
-  onNotesChanged = (notes) => {
+    this.state = {
+      regionalOffice: null,
+      notes: ''
+    };
+  }
+
+  onRegionalOfficeSelected = (value) => {
+    this.setState({ regionalOffice: value });
+    this.props.highlightInvalidFormItems(false);
+  }
+
+  validateRegionalOfficePopulated = () => !_.isNull(this.state.regionalOffice);
+
+  onNotesChanged = (notes) => this.setState({ notes });
+
+  onSubmit = () => {
+    // Return a future
   };
 
   render = () => {
+    const { hasError } = this.props;
+
     return (
       <QueueFlowModal
         title={COPY.CANCEL_TASK_AND_ASSIGN_REGIONAL_OFFICE_MODAL_TITLE}
         button={COPY.CANCEL_TASK_AND_ASSIGN_REGIONAL_OFFICE_MODAL_BUTTON}
+        submit={this.onSubmit}
+        validateForm={this.validateRegionalOfficePopulated}
       >
         <p>
           {COPY.CANCEL_TASK_AND_ASSIGN_REGIONAL_OFFICE_MODAL_DETAIL}
         </p>
         <RegionalOfficeDropdown
+          errorMessage={hasError ? COPY.REGIONAL_OFFICE_REQUIRED_MESSAGE : null}
+          value={this.state.regionalOffice}
           onChange={this.onRegionalOfficeSelected}
         />
         <TextareaField
@@ -39,11 +64,15 @@ class CancelTaskAssignRegionalOfficeModal extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return { ...ownProps };
-};
+const mapStateToProps = (state, ownProps) => (
+  {
+    task: taskById(state, { taskId: ownProps.taskId }),
+    hasError: state.ui.highlightFormItems
+  }
+);
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
+  highlightInvalidFormItems
 }, dispatch);
 
 export default (withRouter(
