f52d453136a20dcdc2bd4a37342197f81c88159f
Ferris Tseng <ferris@navapbc.com>
2019-05-13 17:09:08 -0400

    drop extra whitespace

--- app/models/tasks/hearing_admin_action_verify_address_task.rb
+++ app/models/tasks/hearing_admin_action_verify_address_task.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class HearingAdminActionVerifyAddressTask < HearingAdminActionTask
   after_update :fetch_closest_ro_and_ahls, if: :task_just_completed?
-    
+
   def self.label
     "Verify Address"
   end
@@ -19,7 +21,7 @@ class HearingAdminActionVerifyAddressTask < HearingAdminActionTask
     verify_user_can_update!(current_user)
 
     payload_values = params.delete(:business_payloads)&.dig(:values)
-    
+
     case params[:status]
     when Constants.TASK_STATUSES.completed
       fetch_closest_ro_and_ahls
@@ -42,7 +44,7 @@ class HearingAdminActionVerifyAddressTask < HearingAdminActionTask
     appeal.va_dot_gov_address_validator.update_closest_ro_and_ahls
   end
 
-  private 
+  private
 
   def task_just_completed?
     saved_change_to_attribute?("status") && status == Constants.TASK_STATUSES.completed
