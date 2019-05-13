dbbfc54a056fd8b8eadcdf2a0de859db3c0c3ebe
youngfreezyVA <fareezahmed@macbook-pro-3.lan>
2019-05-13 14:50:58 -0400

    assign to Kate

--- app/models/organizations/pulac_curello.rb
+++ app/models/organizations/pulac_curello.rb
@@ -4,4 +4,8 @@ class PulacCurello < Organization
   def self.singleton
     PulacCurello.first || PulacCurello.create(name: Constants.LIT_SUPPORT.PULAC_CURELLO, url: "pulac-curello")
   end
+
+  def next_assignee(options = {})
+    users.first
+  end
 end
