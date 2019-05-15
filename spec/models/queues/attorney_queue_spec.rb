4c273ff514dcbbe49bab2acc7d6287111207a1ea
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2019-05-15 11:45:26 -0400

    Show reassigned ColocatedTasks in AttorneyQueue (#10774)
    Resolves #10575.

--- spec/models/queues/attorney_queue_spec.rb
+++ spec/models/queues/attorney_queue_spec.rb
@@ -9,11 +9,31 @@ describe AttorneyQueue do
     subject { AttorneyQueue.new(user: user).tasks }
 
     context "when colocated admin actions are on hold" do
-      let!(:action1) { create(:colocated_task, assigned_by: user) }
-      let!(:action2) { create(:colocated_task, appeal: appeal, assigned_by: user) }
-      let!(:action3) { create(:colocated_task, appeal: appeal, assigned_by: user, status: "completed") }
-      let!(:action4) { create(:colocated_task, assigned_by: user, status: "completed") }
-      let!(:action5) { create(:colocated_task, assigned_by: user, status: "in_progress") }
+      let!(:vlj_support_staff) do
+        OrganizationsUser.add_user_to_organization(FactoryBot.create(:user), Colocated.singleton)
+        Colocated.singleton.users.first
+      end
+
+      let!(:action1) { create(:colocated_task, assigned_by: user, assigned_to: Colocated.singleton) }
+      let!(:action2) { create(:colocated_task, appeal: appeal, assigned_by: user, assigned_to: Colocated.singleton) }
+      let!(:action3) do
+        create(
+          :colocated_task,
+          appeal: appeal,
+          assigned_by: user,
+          assigned_to: Colocated.singleton
+        ).tap do |task|
+          task.children.first.update!(status: Constants.TASK_STATUSES.completed)
+        end
+      end
+      let!(:action4) do
+        create(:colocated_task, assigned_by: user, assigned_to: Colocated.singleton).tap do |task|
+          task.children.first.update!(status: Constants.TASK_STATUSES.completed)
+        end
+      end
+      let!(:action5) do
+        create(:colocated_task, assigned_by: user, status: "in_progress", assigned_to: Colocated.singleton)
+      end
 
       it "should return the list" do
         expect(subject.size).to eq 3
@@ -24,11 +44,30 @@ describe AttorneyQueue do
     end
 
     context "when complete and incomplete colocated admin actions exist for an appeal" do
+      let!(:vlj_support_staff) do
+        OrganizationsUser.add_user_to_organization(FactoryBot.create(:user), Colocated.singleton)
+        Colocated.singleton.users.first
+      end
+
       let!(:completed_action) do
-        FactoryBot.create(:colocated_task, :completed, appeal: appeal, assigned_by: user)
+        FactoryBot.create(
+          :colocated_task,
+          appeal: appeal,
+          assigned_by: user,
+          assigned_to: Colocated.singleton
+        ).tap do |task|
+          task.children.first.update!(status: Constants.TASK_STATUSES.completed)
+        end
       end
       let!(:incomplete_action) do
-        FactoryBot.create(:colocated_task, :on_hold, appeal: appeal, assigned_by: user).becomes(ColocatedTask)
+        FactoryBot.create(
+          :colocated_task,
+          appeal: appeal,
+          assigned_by: user,
+          assigned_to: Colocated.singleton
+        ).becomes(ColocatedTask).tap do |task|
+          task.children.first.update!(status: Constants.TASK_STATUSES.on_hold)
+        end
       end
 
       it "should only return the incomplete colocated admin actions" do
