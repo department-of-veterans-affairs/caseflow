02a6f41a5c1400c2cfeb8c6b73e4c22c24077048
youngfreezyVA <fareezahmed@macbook-pro-3.lan>
2019-05-16 08:50:21 -0400

    code climate

--- app/models/tasks/pulac_curello_task.rb
+++ app/models/tasks/pulac_curello_task.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class PulacCurelloTask < GenericTask
-    # Skip unique verification
+  # Skip unique verification
   def verify_org_task_unique; end
 
   def label
     "Pulac Curello"
   end
-
 end
-
   
