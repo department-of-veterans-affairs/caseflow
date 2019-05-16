749cd4dafa113ff65acfb3f556264878befcae8b
Sjones352 <sjones@tistatech.com>
2019-05-16 11:11:52 -0400

    edited description shows

--- app/models/request_issue.rb
+++ app/models/request_issue.rb
@@ -193,7 +193,8 @@ class RequestIssue < ApplicationRecord
         vacols_sequence_id: data[:vacols_sequence_id],
         contested_decision_issue_id: data[:contested_decision_issue_id],
         ineligible_reason: data[:ineligible_reason],
-        ineligible_due_to_id: data[:ineligible_due_to_id]
+        ineligible_due_to_id: data[:ineligible_due_to_id],
+        edited_description: data[:edited_description]
       }
     end
     # rubocop:enable Metrics/MethodLength
