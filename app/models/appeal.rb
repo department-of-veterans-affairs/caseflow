53d82e6fbd1b556335184e08917bcd87133fe7ef
youngfreezyVA <fareezahmed@macbook-pro-3.lan>
2019-05-16 08:39:39 -0400

    spacing

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -137,12 +137,13 @@ class Appeal < DecisionReview
   # mirroring Legacy Appeals' location code in VACOLS
   def assigned_to_location
     return COPY::CASE_LIST_TABLE_POST_DECISION_LABEL if root_task&.status == Constants.TASK_STATUSES.completed
-    
+
     active_tasks = tasks.active.not_tracking
     return most_recently_assigned_to_label(active_tasks) if active_tasks.any?
 
     on_hold_tasks = tasks.on_hold.not_tracking
     return most_recently_assigned_to_label(on_hold_tasks) if on_hold_tasks.any?
+
     return most_recently_assigned_to_label(tasks) if tasks.any?
 
     status_hash[:type].to_s.titleize
