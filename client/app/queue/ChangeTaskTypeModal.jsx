e2363f149e1b3385585b6406a6480947625f3265
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2019-05-17 08:48:20 -0400

    Change task type (FE only) (#10693)
    * add test to update task type
    
    * stash
    
    * Add change task type to colocated actions
    
    * Removed unused
    
    * Add test for colocated
    
    * Do not redirect to queue after submitting
    
    * move location of change task type data
    
    * fix state
    
    * refactor copy as tasks are not limited to colocated
    
    * Remove focus: true
    
    * Add change mail task type test
    
    * Add test for task that should be rerouted to another assignee when the task type is changed
    
    * Don't allow users to access change type until backend changes are made
    
    * don't repeat logic for mail task options
    
    * skip failing tests
    
    * skip failing tests and lint
    
    * skip failing tests
    
    * Add reasoning for commented out task action
    
    * Remove test for backend changes that don't yet exist
    
    * Fake backend methods to ensure tests work rather than skipping
    
    * Remove unused file
    
    * Rename variables to be clear the type is changing, not the actions
    
    * Fixes based on comments about buildPayload and actionTemplate
    
    * Remove artifacts from form with multiple actions and store whole option in state so we don't have to search for labels
    
    * Remove updateActionField
    
    * Remove errant space
    
    * Remove extra props
    
    * Remove unused import
    
    * Move change_task_type_data to TaskActionRepository
    
    * Beter organization of methods
    
    * Codeclimate fixes
    
    * Fix call
    
    * Move actionNameOfTask into utils to get the old task's label
    
    * Update test to reflect change on how the task label is presented
    
    * Update test to reflect change on how the task label is presented
    
    * Only update test to reflect change on how the old task label is presented
    
    * Fix test failing on ci but passing locally
    
    * Legacy and colocated tasks require the same data for admin actions. Combine to make code climate happy (and I guess also avoid duplication)
    
    * update actionNameOfTask to match master
    
    * Fix test failing from change in action name function in master

---
+++ client/app/queue/ChangeTaskTypeModal.jsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { sprintf } from 'sprintf-js';
+
+import TextareaField from '../components/TextareaField';
+import SearchableDropdown from '../components/SearchableDropdown';
+import Alert from '../components/Alert';
+
+import { highlightInvalidFormItems, requestPatch } from './uiReducer/uiActions';
+import { setAppealAttrs, onReceiveAmaTasks } from './QueueActions';
+
+import {
+  appealWithDetailSelector,
+  taskById
+} from './selectors';
+import { marginTop } from './constants';
+import COPY from '../../COPY.json';
+
+import { taskActionData, actionNameOfTask } from './utils';
+import QueueFlowModal from './components/QueueFlowModal';
+
+class ChangeTaskTypeModal extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      actionOption: null,
+      instructions: ''
+    };
+  }
+
+  validateForm = () => Boolean(this.state.actionOption) && Boolean(this.state.instructions);
+
+  buildPayload = () => {
+    const { appeal } = this.props;
+    const { actionOption, instructions } = this.state;
+
+    return {
+      data: {
+        task: {
+          action: actionOption.value,
+          instructions,
+          type: taskActionData(this.props).type || actionOption.value,
+          external_id: appeal.externalId
+        }
+      }
+    };
+  }
+
+  submit = () => {
+    const { task } = this.props;
+    const { actionOption } = this.state;
+
+    const payload = this.buildPayload();
+
+    const msgTitle = COPY.CHANGE_TASK_TYPE_CONFIRMATION_TITLE;
+    const oldTaskType = actionNameOfTask(task);
+    const successMsg = {
+      title: sprintf(msgTitle, oldTaskType, actionOption.label),
+      detail: COPY.CHANGE_TASK_TYPE_CONFIRMATION_DETAIL
+    };
+
+    return this.props.requestPatch(`/tasks/${task.taskId}`, payload, successMsg).
+      then((response) => {
+        const amaTasks = JSON.parse(response.text).tasks.data;
+
+        this.props.onReceiveAmaTasks({ amaTasks });
+      }).
+      catch(() => {
+        // handle the error from the frontend
+      });
+  }
+
+  actionForm = () => {
+    const { highlightFormItems } = this.props;
+    const { instructions, actionOption } = this.state;
+
+    return <React.Fragment>
+      <div>
+        <div {...marginTop(4)}>
+          <SearchableDropdown
+            errorMessage={highlightFormItems && !actionOption ? COPY.FORM_ERROR_FIELD_REQUIRED : null}
+            name={COPY.CHANGE_TASK_TYPE_ACTION_LABEL}
+            placeholder="Select an action type"
+            options={taskActionData(this.props).options}
+            onChange={(option) => option && this.setState({ actionOption: option })}
+            value={actionOption && actionOption.value} />
+        </div>
+        <div {...marginTop(4)}>
+          <TextareaField
+            errorMessage={highlightFormItems && !instructions ? COPY.FORM_ERROR_FIELD_REQUIRED : null}
+            name={COPY.CHANGE_TASK_TYPE_INSTRUCTIONS_LABEL}
+            onChange={(value) => this.setState({ instructions: value })}
+            value={instructions} />
+        </div>
+      </div>
+    </React.Fragment>;
+  };
+
+  render = () => {
+    const { error } = this.props;
+
+    return <QueueFlowModal
+      validateForm={this.validateForm}
+      submit={this.submit}
+      title={COPY.CHANGE_TASK_TYPE_SUBHEAD}
+      button={COPY.CHANGE_TASK_TYPE_SUBHEAD}
+      pathAfterSubmit={`/queue/appeals/${this.props.appealId}`}
+    >
+      {error && <Alert title={error.title} type="error">
+        {error.detail}
+      </Alert>}
+      { this.actionForm() }
+    </QueueFlowModal>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  highlightFormItems: state.ui.highlightFormItems,
+  error: state.ui.messages.error,
+  appeal: appealWithDetailSelector(state, ownProps),
+  task: taskById(state, { taskId: ownProps.taskId })
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  highlightInvalidFormItems,
+  requestPatch,
+  onReceiveAmaTasks,
+  setAppealAttrs
+}, dispatch);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ChangeTaskTypeModal));
