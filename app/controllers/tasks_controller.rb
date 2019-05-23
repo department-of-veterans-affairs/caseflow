a55f28371d739faf9d69ee786204602eb67cfeb6
Fareez Ahmed <46791771+youngfreezyVA@users.noreply.github.com>
2019-05-23 16:41:58 -0400

    Pulac (#10862)
    * save work so far - progress PR
    
    * remove debugging stuff
    
    * correct label name
    
    * rename methods for clarity
    
    * rename methods for clarity
    
    * scope assignemnt to specific sub classes - theres probably a better way to do it than this
    
    * remove func which is causing all tasks to get assigned
    
    * save work so far
    
    * try to use existing tasks endpoint instead
    
    * remove extra route and add coment
    
    * get pulac curello assignment working
    
    * make creation easier to read
    
    * Remove unecessary stuff
    
    * render json to get a 200 instead of 204
    
    * try something else
    
    * add comment
    
    * some optimizations
    
    * get case out of lit support users queue when assigned to PulacCurello
    
    * re use tasks endpoint instead of new route and controllers
    
    * Move available actions into specific task classes
    
    * re use assignToView.jsx
    
    * re use assignToView.jsx
    
    * remove console.logs
    
    * add back constants
    
    * commas
    
    * assign to Kate
    
    * update task action to task action repository and update modal title and body
    
    * make modal work
    
    * remove unused method
    
    * add new task type and remove code for old mail tasks
    
    * add Pulac Curello Task Type
    
    * fix issue with assigned_to_label
    
    * add Kathleen Sosna to seeds.rb and pulac curello org
    
    * spacing
    
    * code climate
    
    * code climate
    
    * trailing whitespace
    
    * use constant for task label
    
    * code climate
    
    * try to fix tests
    
    * revert filtering out by on_hold since that breaks the app and lots of places exepct those tasks to be present
    
    * revert change to most recently assigned label since this change will be in separate pr
    
    * tests
    
    * add tests for the three relevant mail task types
    
    * DRY on tests
    
    * add back check for user having access to lit support
    
    * fix tests
    
    * remove constants file and update references
    
    * fix test description and add proper check
    
    * code climate issues
    
    * remove space
    
    * spell pulac Cerullo correctly
    
    * delete old files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,7 @@ class TasksController < ApplicationController
     MailTask: MailTask,
     InformalHearingPresentationTask: InformalHearingPresentationTask,
     PrivacyActTask: PrivacyActTask,
-    PulacCurelloTask: PulacCurelloTask
+    PulacCerulloTask: PulacCerulloTask
   }.freeze
 
   def set_application
