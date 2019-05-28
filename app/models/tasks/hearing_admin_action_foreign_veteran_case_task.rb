12fbe82b8b77d93af5ded97beec2c86b77ac0518
Ferris Tseng <ferris@navapbc.com>
2019-05-28 18:59:17 -0400

    update RO when foreign veteran case task is completed

--- app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
+++ app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
@@ -14,4 +14,17 @@ class HearingAdminActionForeignVeteranCaseTask < HearingAdminActionTask
       Constants.TASK_ACTIONS.SEND_TO_SCHEDULE_VETERAN_LIST.to_h
     ] | hearing_admin_actions
   end
+
+  def update_from_params(params, current_user)
+    verify_user_can_update!(current_user)
+
+    payload_values = params.delete(:business_payloads)&.dig(:values)
+
+    case params[:status]
+    when Constants.TASK_STATUSES.completed
+      appeal.assign_ro_and_update_ahls(payload_values[:regional_office_value])
+    end
+
+    super(params, current_user)
+  end
 end
