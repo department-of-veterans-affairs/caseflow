205447c3c66c2a0c963266a7c4b215e200066ca4
Sharon Warner <sharon.warner56@gmail.com>
2019-05-28 19:01:23 -0400

    Removing unused code

--- client/app/hearings/components/HearingDayEditModal.jsx
+++ client/app/hearings/components/HearingDayEditModal.jsx
@@ -40,7 +40,7 @@ class HearingDayEditModal extends React.Component {
     };
   }
 
-  componentWillMount = () => {
+  componentDidMount = () => {
     // find labels in options before passing values to modal
     this.initialState();
     if (this.props.dailyDocket.roomInfo) {
@@ -56,23 +56,6 @@ class HearingDayEditModal extends React.Component {
       this.initialState();
     }
   }
-
-  formatRoomOptions = () => {
-    const roomOptions = [
-      { label: '',
-        value: '' }
-    ];
-    const rooms = Object.keys(HEARING_ROOMS_LIST);
-
-    for (const roomKey of rooms) {
-      const room = HEARING_ROOMS_LIST[roomKey];
-
-      roomOptions.push({ label: room.label,
-        value: roomKey });
-    }
-
-    return roomOptions;
-  };
 
   modalConfirmButton = () => {
     return <Button
