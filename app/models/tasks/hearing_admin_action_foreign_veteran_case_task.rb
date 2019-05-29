21d00248fe83a90e4d0a9b2550583a74ef90a754
Ferris Tseng <ferris@navapbc.com>
2019-05-29 15:13:59 -0400

    add permissions around available actions

--- app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
+++ app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
@@ -8,11 +8,15 @@ class HearingAdminActionForeignVeteranCaseTask < HearingAdminActionTask
   def available_actions(user)
     hearing_admin_actions = available_hearing_user_actions(user)
 
-    [
-      Constants.TASK_ACTIONS.PLACE_HOLD.to_h,
-      Constants.TASK_ACTIONS.CANCEL_FOREIGN_VETERANS_CASE_TASK.to_h,
-      Constants.TASK_ACTIONS.SEND_TO_SCHEDULE_VETERAN_LIST.to_h
-    ] | hearing_admin_actions
+    if (assigned_to &.== user) || HearingsManagement.singleton.user_has_access?(user)
+      return [
+        Constants.TASK_ACTIONS.PLACE_HOLD.to_h,
+        Constants.TASK_ACTIONS.CANCEL_FOREIGN_VETERANS_CASE_TASK.to_h,
+        Constants.TASK_ACTIONS.SEND_TO_SCHEDULE_VETERAN_LIST.to_h
+      ] | hearing_admin_actions
+    end
+
+    hearing_admin_actions
   end
 
   def update_from_params(params, current_user)
