7c17fbe1e15958be214b4b19a458851b4b6756b7
Ferris Tseng <ferris@navapbc.com>
2019-05-29 10:46:42 -0400

    set parent instructions for schedule hearing task

--- app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
+++ app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
@@ -16,15 +16,21 @@ class HearingAdminActionForeignVeteranCaseTask < HearingAdminActionTask
   end
 
   def update_from_params(params, current_user)
-    verify_user_can_update!(current_user)
-
     payload_values = params.delete(:business_payloads)&.dig(:values)
+
+    if parent.is_a?(ScheduleHearingTask)
+      params["instructions"] = flattened_instructions(params)
+
+      parent.update!(instructions: params.delete("instructions"))
+    end
+
+    super(params, current_user)
 
     case params[:status]
     when Constants.TASK_STATUSES.completed
       appeal.assign_ro_and_update_ahls(payload_values[:regional_office_value])
     end
 
-    super(params, current_user)
+    [self]
   end
 end
