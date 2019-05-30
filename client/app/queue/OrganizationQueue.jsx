department-of-veterans-affairs/caseflow

commit cb3cf31bc2e86d418fc45a4fd724adec0585215e

Inject docket number column into TaskTable instance in OrganizationQueue. (#10735)
Partially resolves #10619, which calls for injecting columns into `TaskTable` instances via a `customColumns` array, as opposed to setting properties.  

In order to tackle this more granularly, we have elected to do this one column at a time. This is the first PR in this series of work and represents the injection of the docket number column.

client/app/queue/OrganizationQueue.jsx
@@ -5,7 +5,7 @@ import { sprintf } from 'sprintf-js';
 import { css } from 'glamor';
 
 import TabWindow from '../components/TabWindow';
-import TaskTable from './components/TaskTable';
+import TaskTable, { docketNumberColumn } from './components/TaskTable';
 import QueueOrganizationDropdown from './components/QueueOrganizationDropdown';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
@@ -85,10 +85,10 @@ class OrganizationQueue extends React.PureComponent {
             {sprintf(COPY.ALL_CASES_QUEUE_TABLE_TAB_DESCRIPTION, this.props.organizationName)}
           </p>
           <TaskTable
+            customColumns={[docketNumberColumn(this.props.trackingTasks, false)]}
             includeDetailsLink
             includeIssueCount
             includeType
-            includeDocketNumber
             tasks={this.props.trackingTasks}
           />
         </React.Fragment>
@@ -147,12 +147,12 @@ export default connect(mapStateToProps, mapDispatchToProps)(OrganizationQueue);
 const UnassignedTaskTableTab = ({ description, tasks, organizationName }) => <React.Fragment>
   <p className="cf-margin-top-0">{description}</p>
   <TaskTable
+    customColumns={[docketNumberColumn(tasks, false)]}
     includeHearingBadge
     includeDetailsLink
     includeTask
     includeRegionalOffice={organizationName === 'Hearing Management' || organizationName === 'Hearing Admin'}
     includeType
-    includeDocketNumber
     includeDaysWaiting
     includeReaderLink
     includeNewDocsIcon
@@ -163,14 +163,15 @@ const UnassignedTaskTableTab = ({ description, tasks, organizationName }) => <Re
 
 const TaskTableWithUserColumnTab = ({ description, tasks, organizationName }) => <React.Fragment>
   <p className="cf-margin-top-0">{description}</p>
+
   <TaskTable
+    customColumns={[docketNumberColumn(tasks, false)]}
     includeHearingBadge
     includeDetailsLink
     includeTask
     includeRegionalOffice={organizationName === 'Hearing Management' || organizationName === 'Hearing Admin'}
     includeType
     includeAssignedTo
-    includeDocketNumber
     includeDaysWaiting
     tasks={tasks}
   />

client/app/queue/components/TaskTable.jsx
@@ -36,6 +36,43 @@ import COPY from '../../../COPY.json';
 import CO_LOCATED_ADMIN_ACTIONS from '../../../constants/CO_LOCATED_ADMIN_ACTIONS.json';
 import ORGANIZATION_NAMES from '../../../constants/ORGANIZATION_NAMES.json';
 
+const hasDASRecord = (task, requireDasRecord) => {
+  return (task.appeal.isLegacyAppeal && requireDasRecord) ? Boolean(task.taskId) : true;
+};
+
+const collapseColumn = (requireDasRecord) => (task) => hasDASRecord(task, requireDasRecord) ? 1 : 0;
+
+export const docketNumberColumn = (tasks, requireDasRecord) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_DOCKET_NUMBER_COLUMN_TITLE,
+    enableFilter: true,
+    tableData: tasks,
+    columnName: 'appeal.docketName',
+    customFilterLabels: DOCKET_NAME_FILTERS,
+    anyFiltersAreSet: true,
+    label: 'Filter by docket name',
+    valueName: 'docketName',
+    valueFunction: (task) => {
+      if (!hasDASRecord(task, requireDasRecord)) {
+        return null;
+      }
+
+      return <React.Fragment>
+        <DocketTypeBadge name={task.appeal.docketName} number={task.appeal.docketNumber} />
+        <span>{task.appeal.docketNumber}</span>
+      </React.Fragment>;
+    },
+    span: collapseColumn(requireDasRecord),
+    getSortValue: (task) => {
+      if (!hasDASRecord(task, requireDasRecord)) {
+        return null;
+      }
+
+      return `${task.appeal.docketName || ''} ${task.appeal.docketNumber}`;
+    }
+  };
+};
+
 export class TaskTableUnconnected extends React.PureComponent {
   getKeyForRow = (rowNumber, object) => object.uniqueId
 
@@ -50,11 +87,7 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   taskHasDASRecord = (task) => {
-    if (task.appeal.isLegacyAppeal && this.props.requireDasRecord) {
-      return task.taskId;
-    }
-
-    return true;
+    return hasDASRecord(task, this.props.requireDasRecord);
   }
 
   collapseColumnIfNoDASRecord = (task) => this.taskHasDASRecord(task) ? 1 : 0
@@ -172,34 +205,7 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   caseDocketNumberColumn = () => {
-    return this.props.includeDocketNumber ? {
-      header: COPY.CASE_LIST_TABLE_DOCKET_NUMBER_COLUMN_TITLE,
-      enableFilter: true,
-      tableData: this.props.tasks,
-      columnName: 'appeal.docketName',
-      customFilterLabels: DOCKET_NAME_FILTERS,
-      anyFiltersAreSet: true,
-      label: 'Filter by docket name',
-      valueName: 'docketName',
-      valueFunction: (task) => {
-        if (!this.taskHasDASRecord(task)) {
-          return null;
-        }
-
-        return <React.Fragment>
-          <DocketTypeBadge name={task.appeal.docketName} number={task.appeal.docketNumber} />
-          <span>{task.appeal.docketNumber}</span>
-        </React.Fragment>;
-      },
-      span: this.collapseColumnIfNoDASRecord,
-      getSortValue: (task) => {
-        if (!this.taskHasDASRecord(task)) {
-          return null;
-        }
-
-        return `${task.appeal.docketName || ''} ${task.appeal.docketNumber}`;
-      }
-    } : null;
+    return this.props.includeDocketNumber ? docketNumberColumn(this.props.tasks, this.props.requireDasRecord) : null;
   }
 
   caseIssueCountColumn = () => {

commit ca1d9add005111d75eef93ca4754e6d18a6bf8f3

Extract TaskTable column-creation methods into pure functions. (#10931)
OrganizationQueue now imports pure functions from TaskTable component file and uses them to inject columns into the 3 different variants of TaskTable instances it can create.

Closes #10619. 

Also fixes docket number column misplacement that occurred in an earlier PR.

Current Hearing Management queue with Docket Number column in wrong place, as introduced in #10735:
![](https://user-images.githubusercontent.com/1034221/58658502-9b3ffc80-82ee-11e9-843a-383e2f419c34.png)
 

Hearing Management queue with Docket Number column returned to correct place:
![](https://user-images.githubusercontent.com/1034221/58658273-2967b300-82ee-11e9-8a75-5506e1b85918.png)

client/app/queue/OrganizationQueue.jsx
@@ -6,7 +6,9 @@ import { css } from 'glamor';
 
 import BulkAssignModal from './components/BulkAssignModal';
 import TabWindow from '../components/TabWindow';
-import TaskTable, { docketNumberColumn, hearingBadgeColumn, detailsColumn, taskColumn } from './components/TaskTable';
+import TaskTable, { docketNumberColumn, hearingBadgeColumn, detailsColumn,
+  taskColumn, regionalOfficeColumn, issueCountColumn, typeColumn,
+  assignedToColumn, daysWaitingColumn, readerLinkColumn } from './components/TaskTable';
 import QueueOrganizationDropdown from './components/QueueOrganizationDropdown';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
@@ -89,11 +91,11 @@ class OrganizationQueue extends React.PureComponent {
           </p>
           <TaskTable
             customColumns={[
-              docketNumberColumn(this.props.trackingTasks, false),
-              detailsColumn(this.props.trackingTasks, false, this.props.userRole)
+              detailsColumn(this.props.trackingTasks, false, this.props.userRole),
+              issueCountColumn(false),
+              typeColumn(this.props.trackingTasks, false),
+              docketNumberColumn(this.props.trackingTasks, false)
             ]}
-            includeIssueCount
-            includeType
             tasks={this.props.trackingTasks}
           />
         </React.Fragment>
@@ -150,39 +152,48 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrganizationQueue);
 
-const UnassignedTaskTableTab = ({ description, tasks, organizationName, userRole }) => <React.Fragment>
-  <p className="cf-margin-top-0">{description}</p>
-  { organizationName === 'Hearing Admin' && <BulkAssignModal tasks={tasks} /> }
-  <TaskTable
-    customColumns={[
-      docketNumberColumn(tasks, false),
-      hearingBadgeColumn(tasks),
-      detailsColumn(tasks, false, userRole),
-      taskColumn(tasks)
-    ]}
-    includeRegionalOffice={organizationName === 'Hearing Management' || organizationName === 'Hearing Admin'}
-    includeType
-    includeDaysWaiting
-    includeReaderLink
-    includeNewDocsIcon
-    tasks={tasks}
-  />
-</React.Fragment>;
+const UnassignedTaskTableTab = ({ description, tasks, organizationName, userRole }) => {
+  let columns = [hearingBadgeColumn(tasks), detailsColumn(tasks, false,
+    userRole), taskColumn(tasks), typeColumn(tasks, false),
+  docketNumberColumn(tasks, false), daysWaitingColumn(false),
+  readerLinkColumn(false, true)];
 
-const TaskTableWithUserColumnTab = ({ description, tasks, organizationName, userRole }) => <React.Fragment>
-  <p className="cf-margin-top-0">{description}</p>
+  if (organizationName === 'Hearing Management' || organizationName === 'Hearing Admin') {
+    columns = [hearingBadgeColumn(tasks), detailsColumn(tasks, false,
+      userRole), taskColumn(tasks), regionalOfficeColumn(tasks),
+    typeColumn(tasks, false), docketNumberColumn(tasks, false),
+    daysWaitingColumn(false), readerLinkColumn(false, true)];
+  }
 
-  <TaskTable
-    customColumns={[
-      docketNumberColumn(tasks, false),
-      hearingBadgeColumn(tasks),
-      detailsColumn(tasks, false, userRole),
-      taskColumn(tasks)
-    ]}
-    includeRegionalOffice={organizationName === 'Hearing Management' || organizationName === 'Hearing Admin'}
-    includeType
-    includeAssignedTo
-    includeDaysWaiting
-    tasks={tasks}
-  />
-</React.Fragment>;
+  return (<React.Fragment>
+    <p className="cf-margin-top-0">{description}</p>
+    { organizationName === 'Hearing Admin' && <BulkAssignModal tasks={tasks} /> }
+    <TaskTable
+      customColumns={columns}
+      tasks={tasks}
+    />
+  </React.Fragment>);
+};
+
+const TaskTableWithUserColumnTab = ({ description, tasks, organizationName, userRole }) => {
+  let columns = [hearingBadgeColumn(tasks), detailsColumn(tasks, false,
+    userRole), taskColumn(tasks), typeColumn(tasks, false),
+  assignedToColumn(tasks), docketNumberColumn(tasks, false),
+  daysWaitingColumn(false)];
+
+  if (organizationName === 'Hearing Management' || organizationName === 'Hearing Admin') {
+    columns = [hearingBadgeColumn(tasks), detailsColumn(tasks, false,
+      userRole), taskColumn(tasks), regionalOfficeColumn(tasks),
+    typeColumn(tasks, false), assignedToColumn(tasks),
+    docketNumberColumn(tasks, false), daysWaitingColumn(false)];
+  }
+
+  return (<React.Fragment>
+    <p className="cf-margin-top-0">{description}</p>
+    <TaskTable
+      customColumns={columns}
+      tasks={tasks}
+    />
+    }
+  </React.Fragment>);
+};

client/app/queue/components/TaskTable.jsx
@@ -110,6 +110,105 @@ export const taskColumn = (tasks) => {
   };
 };
 
+export const regionalOfficeColumn = (tasks) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_REGIONAL_OFFICE_COLUMN_TITLE,
+    enableFilter: true,
+    tableData: tasks,
+    columnName: 'closestRegionalOffice',
+    anyFiltersAreSet: true,
+    label: 'Filter by regional office',
+    valueFunction: (task) => task.closestRegionalOffice ? task.closestRegionalOffice : 'Unknown',
+    getSortValue: (task) => task.closestRegionalOffice
+  };
+};
+
+export const issueCountColumn = (requireDasRecord) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_APPEAL_ISSUE_COUNT_COLUMN_TITLE,
+    valueFunction: (task) => hasDASRecord(task, requireDasRecord) ? task.appeal.issueCount : null,
+    span: collapseColumn(requireDasRecord),
+    getSortValue: (task) => hasDASRecord(task, requireDasRecord) ? task.appeal.issueCount : null
+  };
+};
+
+export const typeColumn = (tasks, requireDasRecord) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_APPEAL_TYPE_COLUMN_TITLE,
+    enableFilter: true,
+    tableData: tasks,
+    columnName: 'appeal.caseType',
+    anyFiltersAreSet: true,
+    label: 'Filter by type',
+    valueName: 'caseType',
+    valueFunction: (task) => hasDASRecord(task, requireDasRecord) ?
+      renderAppealType(task.appeal) :
+      <span {...redText}>{COPY.ATTORNEY_QUEUE_TABLE_TASK_NEEDS_ASSIGNMENT_ERROR_MESSAGE}</span>,
+    span: (task) => hasDASRecord(task, requireDasRecord) ? 1 : 5,
+    getSortValue: (task) => {
+      // We append a * before the docket number if it's a priority case since * comes before
+      // numbers in sort order, this forces these cases to the top of the sort.
+      if (task.appeal.isAdvancedOnDocket || task.appeal.caseType === LEGACY_APPEAL_TYPES.CAVC_REMAND) {
+        return `*${task.appeal.docketNumber}`;
+      }
+
+      return task.appeal.docketNumber;
+    }
+  };
+};
+
+export const assignedToColumn = (tasks) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_APPEAL_LOCATION_COLUMN_TITLE,
+    enableFilter: true,
+    tableData: tasks,
+    columnName: 'assignedTo.name',
+    anyFiltersAreSet: true,
+    label: 'Filter by assignee',
+    valueFunction: (task) => task.assignedTo.name,
+    getSortValue: (task) => task.assignedTo.name
+  };
+};
+
+export const readerLinkColumn = (requireDasRecord, includeNewDocsIcon) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_APPEAL_DOCUMENT_COUNT_COLUMN_TITLE,
+    span: collapseColumn(requireDasRecord),
+    valueFunction: (task) => {
+      if (!hasDASRecord(task, requireDasRecord)) {
+        return null;
+      }
+
+      return <ReaderLink appealId={task.externalAppealId}
+        analyticsSource={CATEGORIES.QUEUE_TABLE}
+        redirectUrl={window.location.pathname}
+        appeal={task.appeal}
+        newDocsIcon={includeNewDocsIcon}
+        task={task}
+        docCountBelowLink />;
+    }
+  };
+};
+
+export const daysWaitingColumn = (requireDasRecord) => {
+  return {
+    header: COPY.CASE_LIST_TABLE_TASK_DAYS_WAITING_COLUMN_TITLE,
+    span: collapseColumn(requireDasRecord),
+    tooltip: <React.Fragment>Calendar days since <br /> this case was assigned</React.Fragment>,
+    align: 'center',
+    valueFunction: (task) => {
+      return <React.Fragment>
+        <span className={taskHasCompletedHold(task) ? 'cf-red-text' : ''}>{moment().startOf('day').
+          diff(moment(task.assignedOn), 'days')}</span>
+        { taskHasCompletedHold(task) ? <ContinuousProgressBar level={moment().startOf('day').
+          diff(task.placedOnHoldAt, 'days')} limit={task.onHoldDuration} warning /> : null }
+      </React.Fragment>;
+    },
+    getSortValue: (task) => moment().startOf('day').
+      diff(moment(task.assignedOn), 'days')
+  };
+};
+
 export class TaskTableUnconnected extends React.PureComponent {
   getKeyForRow = (rowNumber, object) => object.uniqueId
 
@@ -179,41 +278,11 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   caseTypeColumn = () => {
-    return this.props.includeType ? {
-      header: COPY.CASE_LIST_TABLE_APPEAL_TYPE_COLUMN_TITLE,
-      enableFilter: true,
-      tableData: this.props.tasks,
-      columnName: 'appeal.caseType',
-      anyFiltersAreSet: true,
-      label: 'Filter by type',
-      valueName: 'caseType',
-      valueFunction: (task) => this.taskHasDASRecord(task) ?
-        renderAppealType(task.appeal) :
-        <span {...redText}>{COPY.ATTORNEY_QUEUE_TABLE_TASK_NEEDS_ASSIGNMENT_ERROR_MESSAGE}</span>,
-      span: (task) => this.taskHasDASRecord(task) ? 1 : 5,
-      getSortValue: (task) => {
-        // We append a * before the docket number if it's a priority case since * comes before
-        // numbers in sort order, this forces these cases to the top of the sort.
-        if (task.appeal.isAdvancedOnDocket || task.appeal.caseType === LEGACY_APPEAL_TYPES.CAVC_REMAND) {
-          return `*${task.appeal.docketNumber}`;
-        }
-
-        return task.appeal.docketNumber;
-      }
-    } : null;
+    return this.props.includeType ? typeColumn(this.props.tasks, this.props.requireDasRecord) : null;
   }
 
   caseAssignedToColumn = () => {
-    return this.props.includeAssignedTo ? {
-      header: COPY.CASE_LIST_TABLE_APPEAL_LOCATION_COLUMN_TITLE,
-      enableFilter: true,
-      tableData: this.props.tasks,
-      columnName: 'assignedTo.name',
-      anyFiltersAreSet: true,
-      label: 'Filter by assignee',
-      valueFunction: (task) => task.assignedTo.name,
-      getSortValue: (task) => task.assignedTo.name
-    } : null;
+    return this.props.includeAssignedTo ? assignedToColumn(this.props.tasks) : null;
   }
 
   caseDocketNumberColumn = () => {
@@ -221,12 +290,7 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   caseIssueCountColumn = () => {
-    return this.props.includeIssueCount ? {
-      header: COPY.CASE_LIST_TABLE_APPEAL_ISSUE_COUNT_COLUMN_TITLE,
-      valueFunction: (task) => this.taskHasDASRecord(task) ? task.appeal.issueCount : null,
-      span: this.collapseColumnIfNoDASRecord,
-      getSortValue: (task) => this.taskHasDASRecord(task) ? task.appeal.issueCount : null
-    } : null;
+    return this.props.includeIssueCount ? issueCountColumn(this.props.requireDasRecord) : null;
   }
 
   caseDueDateColumn = () => {
@@ -253,22 +317,7 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   caseDaysWaitingColumn = () => {
-    return this.props.includeDaysWaiting ? {
-      header: COPY.CASE_LIST_TABLE_TASK_DAYS_WAITING_COLUMN_TITLE,
-      span: this.collapseColumnIfNoDASRecord,
-      tooltip: <React.Fragment>Calendar days since <br /> this case was assigned</React.Fragment>,
-      align: 'center',
-      valueFunction: (task) => {
-        return <React.Fragment>
-          <span className={taskHasCompletedHold(task) ? 'cf-red-text' : ''}>{moment().startOf('day').
-            diff(moment(task.assignedOn), 'days')}</span>
-          { taskHasCompletedHold(task) ? <ContinuousProgressBar level={moment().startOf('day').
-            diff(task.placedOnHoldAt, 'days')} limit={task.onHoldDuration} warning /> : null }
-        </React.Fragment>;
-      },
-      getSortValue: (task) => moment().startOf('day').
-        diff(moment(task.assignedOn), 'days')
-    } : null;
+    return this.props.includeDaysWaiting ? daysWaitingColumn(this.props.requireDasRecord) : null;
   }
 
   caseDaysOnHoldColumn = () => (this.props.includeDaysOnHold ? {
@@ -302,36 +351,13 @@ export class TaskTableUnconnected extends React.PureComponent {
   }
 
   caseReaderLinkColumn = () => {
-    return !this.props.userIsVsoEmployee && this.props.includeReaderLink ? {
-      header: COPY.CASE_LIST_TABLE_APPEAL_DOCUMENT_COUNT_COLUMN_TITLE,
-      span: this.collapseColumnIfNoDASRecord,
-      valueFunction: (task) => {
-        if (!this.taskHasDASRecord(task)) {
-          return null;
-        }
-
-        return <ReaderLink appealId={task.externalAppealId}
-          analyticsSource={CATEGORIES.QUEUE_TABLE}
-          redirectUrl={window.location.pathname}
-          appeal={task.appeal}
-          newDocsIcon={this.props.includeNewDocsIcon}
-          task={task}
-          docCountBelowLink />;
-      }
-    } : null;
+    return !this.props.userIsVsoEmployee && this.props.includeReaderLink ?
+      readerLinkColumn(this.props.requireDasRecord, this.props.includeNewDocsIcon) :
+      null;
   }
 
   caseRegionalOfficeColumn = () => {
-    return this.props.includeRegionalOffice ? {
-      header: COPY.CASE_LIST_TABLE_REGIONAL_OFFICE_COLUMN_TITLE,
-      enableFilter: true,
-      tableData: this.props.tasks,
-      columnName: 'closestRegionalOffice',
-      anyFiltersAreSet: true,
-      label: 'Filter by regional office',
-      valueFunction: (task) => task.closestRegionalOffice ? task.closestRegionalOffice : 'Unknown',
-      getSortValue: (task) => task.closestRegionalOffice
-    } : null;
+    return this.props.includeRegionalOffice ? regionalOfficeColumn(this.props.tasks) : null;
   }
 
   getQueueColumns = () =>