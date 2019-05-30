7f083f8b82fc2d1775a46359e7969b53555c20f6
Peter Karman <peter@truss.works>
2019-05-30 17:24:53 -0500

    lint

--- app/queries/stuck_appeals_query.rb
+++ app/queries/stuck_appeals_query.rb
@@ -8,11 +8,15 @@ class StuckAppealsQuery
   private
 
   def stuck_appeals
-    stuck_query("Appeal") + Appeal.where.not(id: Task.select(:appeal_id).where(appeal_type: "Appeal"))
+    stuck_query("Appeal") + zero_tasks_query("Appeal")
   end
 
   def stuck_legacy_appeals
     stuck_query("LegacyAppeal")
+  end
+
+  def zero_tasks_query(klass_name)
+    klass_name.constantize.where.not(id: Task.select(:appeal_id).where(appeal_type: klass_name))
   end
 
   def stuck_query(klass_name)
@@ -21,8 +25,7 @@ class StuckAppealsQuery
     klass.left_joins(:decision_documents).where(decision_documents: { id: nil })
       .where.not(id: Task.select(:appeal_id).where(appeal_type: klass_name).inactive)
       .where.not(id: Task.select(:appeal_id).where(appeal_type: klass_name)
-                       .where(type: "RootTask", status: Constants.TASK_STATUSES.cancelled)
-      )
+                       .where(type: "RootTask", status: Constants.TASK_STATUSES.cancelled))
       .joins(:tasks)
       .group("#{table}.id")
       .having("count(tasks) = count(case when tasks.status = 'on_hold' then 1 end)")
