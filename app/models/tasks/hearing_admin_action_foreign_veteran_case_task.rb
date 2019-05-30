20f7e7ee8fda02c0bcfaec7e71dbaeb036841d7f
Ferris Tseng <ferris@navapbc.com>
2019-05-30 13:43:03 -0400

    fix issues from merges

--- app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
+++ app/models/tasks/hearing_admin_action_foreign_veteran_case_task.rb
@@ -10,7 +10,7 @@ class HearingAdminActionForeignVeteranCaseTask < HearingAdminActionTask
 
     if (assigned_to &.== user) || HearingsManagement.singleton.user_has_access?(user)
       return [
-        Constants.TASK_ACTIONS.PLACE_HOLD.to_h,
+        appropriate_timed_hold_task_action,
         Constants.TASK_ACTIONS.CANCEL_FOREIGN_VETERANS_CASE_TASK.to_h,
         Constants.TASK_ACTIONS.SEND_TO_SCHEDULE_VETERAN_LIST.to_h
       ] | hearing_admin_actions
