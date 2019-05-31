2521d2c1b67649ea3640b8e635995b8309b8e68e
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2019-05-31 18:10:10 -0400

    Add PrivateBar class and creation UI (#10960)
    Resolves #9517.
    
    ![add-private-bar](https://user-images.githubusercontent.com/32683958/58719654-3a6ffd00-839e-11e9-9967-2725ccca2fd1.gif)
    
    Now that we've [tested that a `FieldVso`-style approach works](https://github.com/department-of-veterans-affairs/caseflow/issues/9527#issuecomment-492644994), we can create a specific class for this entity and give Caseflow admins the ability to create these in the UI.
    
    ### TODO:
    After this is merged and pushed live, we should update the two existing `FieldVso` organizations for private bar attorneys to have the correct type (`PrivateBar`).

--- config/routes.rb
+++ config/routes.rb
@@ -208,6 +208,7 @@ Rails.application.routes.draw do
   resources :team_management, only: [:index, :update]
   get '/team_management(*rest)', to: 'team_management#index'
   post '/team_management/judge_team/:user_id', to: 'team_management#create_judge_team'
+  post '/team_management/private_bar', to: 'team_management#create_private_bar'
   post '/team_management/national_vso', to: 'team_management#create_national_vso'
   post '/team_management/field_vso', to: 'team_management#create_field_vso'
 
