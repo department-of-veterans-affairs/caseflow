2412a956072d5afdaa232546e97eca40525a914c
Peter Karman <peter@truss.works>
2019-06-03 15:42:07 -0500

    single line block

--- app/models/concerns/has_business_line.rb
+++ app/models/concerns/has_business_line.rb
@@ -5,9 +5,7 @@ module HasBusinessLine
 
   def business_line
     business_line_name = Constants::BENEFIT_TYPES[benefit_type]
-    @business_line ||= BusinessLine.find_or_create_by(name: business_line_name) do |org|
-      org.url = benefit_type
-    end
+    @business_line ||= BusinessLine.find_or_create_by(name: business_line_name) { |org| org.url = benefit_type }
   end
 
   def processed_in_vbms?
