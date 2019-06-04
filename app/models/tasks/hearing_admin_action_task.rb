66b09c4e2d2e2c34b2812f108ec54fb628c0a145
lowellrex <lowell@navahq.com>
2019-06-04 17:37:22 -0400

    Add require_dependency

--- app/models/tasks/hearing_admin_action_task.rb
+++ app/models/tasks/hearing_admin_action_task.rb
@@ -63,3 +63,12 @@ class HearingAdminActionTask < GenericTask
     super || (on_timed_hold? && children.open.where.not(type: TimedHoldTask.name).empty?)
   end
 end
+
+require_dependency "hearing_admin_action_contested_claimant_task"
+require_dependency "hearing_admin_action_foia_privacy_request_task"
+require_dependency "hearing_admin_action_foreign_veteran_case_task"
+require_dependency "hearing_admin_action_incarcerated_veteran_task"
+require_dependency "hearing_admin_action_missing_forms_task"
+require_dependency "hearing_admin_action_other_task"
+require_dependency "hearing_admin_action_verify_address_task"
+require_dependency "hearing_admin_action_verify_poa_task"
