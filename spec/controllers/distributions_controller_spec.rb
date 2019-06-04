fa38bbc776978f6644ce8478940562b9ea3d40b7
hschallhorn <hschallhorn@tistatech.com>
2019-06-04 18:45:54 -0400

    Add tast coverage for pending distributions

--- spec/controllers/distributions_controller_spec.rb
+++ spec/controllers/distributions_controller_spec.rb
@@ -13,6 +13,37 @@ describe DistributionsController do
         expect(body["errors"].first["error"]).to eq "not_judge"
       end
     end
+
+    context "there is a pending distribution" do
+      it "returns the pending distribution and no more were created" do
+        judge = create(:user)
+        create(:staff, :judge_role, sdomainid: judge.css_id)
+        User.authenticate!(user: judge)
+
+        distribution = Distribution.create!(judge: judge, status: "pending")
+        number_of_distributions = Distribution.count
+
+        get :new
+
+        body = JSON.parse(response.body)
+        expect(body["distribution"]["id"]).to eq distribution.id
+        expect(body["distribution"]["status"]).to eq "pending"
+        expect(Distribution.count).to eq number_of_distributions
+      end
+    end
+
+    context "current user is a judge" do
+      it "renders the created distribution as json" do
+        judge = create(:user)
+        create(:staff, :judge_role, sdomainid: judge.css_id)
+        User.authenticate!(user: judge)
+        get :new
+
+        expect(response.status).to eq 200
+        body = JSON.parse(response.body)
+        expect(body["distribution"].keys).to match_array(%w[id created_at updated_at status distributed_cases_count])
+      end
+    end
   end
 
   describe "#show" do
