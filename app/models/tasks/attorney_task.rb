bbdd438748a9d9c54b08b2919cb0111060eeb317
hschallhorn <hschallhorn@tistatech.com>
2019-06-06 15:57:59 -0400

    Add create_many_from_params for attorney task that
    1. Closes the JudgeAssignTask
    2. Opens a JudgeReviewTask
    3. Creates an AttorneyTask as the review task's child
    
    This replaces the changing the type of a JudgeAssignTask to
    JudgeReviewTask when an attorney task is created

--- app/models/tasks/attorney_task.rb
+++ app/models/tasks/attorney_task.rb
@@ -14,6 +14,23 @@ class AttorneyTask < Task
   validate :assigned_to_role_is_valid
   validate :child_attorney_tasks_are_completed, on: :create
 
+  class << self
+    def create_many_from_params(params_array, _user)
+      ActiveRecord::Base.multi_transaction do
+        params_array.map do |params|
+          judge_assign_task = JudgeAssignTask.find(params[:parent_id])
+          judge_review_task = JudgeDecisionReviewTask.create!(judge_assign_task.slice(:appeal, :assigned_to, :parent))
+
+          judge_assign_task.update!(status: Constants.TASK_STATUSES.completed)
+
+          attorney_task = create!(params.merge(parent_id: judge_review_task.id))
+
+          [attorney_task, judge_review_task, judge_assign_task]
+        end.flatten
+      end
+    end
+  end
+
   def available_actions(user)
     if parent.is_a?(JudgeTask) && parent.assigned_to == user
       return [Constants.TASK_ACTIONS.ASSIGN_TO_ATTORNEY.to_h]
