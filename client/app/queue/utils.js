9cbc3297a6447c8d7044faf209c0e2a4217051e0
Fareez Ahmed <fareez.ahmed@tistatech.com>
2019-06-12 15:45:26 -0400

    revert change to util.js

--- client/app/queue/utils.js
+++ client/app/queue/utils.js
@@ -536,10 +536,7 @@ export const nullToFalse = (key, obj) => {
   return obj;
 };
 
-export const sortTaskList = (taskList, appeal) => {
-  console.log(taskList[0], 'the first task');
-  console.log(appeal.decisionDate, 'the appeal decision date');
-
+export const sortTaskList = (taskList) => {
   return taskList.sort((prev, next) => {
     return new Date(next.closedAt || next.createdAt).getTime() -
     new Date(prev.closedAt || prev.createdAt).getTime();
