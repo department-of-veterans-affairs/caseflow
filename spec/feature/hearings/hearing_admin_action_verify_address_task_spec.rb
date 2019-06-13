1492576df931a2bc8f458714a84b26271557b395
Ed Mangimelli <edmangimelli@gmail.com>
2019-06-13 09:44:00 -0500

    WIP

--- spec/feature/hearings/hearing_admin_action_verify_address_task_spec.rb
+++ spec/feature/hearings/hearing_admin_action_verify_address_task_spec.rb
@@ -85,6 +85,78 @@ RSpec.shared_examples "Address Verify Task for Appeal" do
       expect(appeal.class.first.available_hearing_locations.count).to eq 1
     end
   end
+
+  describe "UI tests" do
+    let(:instructions_text) { "This is why I want to cancel the task!" }
+    
+    context "with a hearing admin member" do
+      before do
+        OrganizationsUser.add_user_to_organization(user, HearingAdmin.singleton)
+
+        User.authenticate!(user: user)
+
+        visit("/queue/appeals/#{appeal_id}")
+      end
+
+      it "has 'Cancel task and assign Regional Office' option" do
+        expect(page).to have_content("Verify Address")
+
+        click_dropdown(text: Constants.TASK_ACTIONS.CANCEL_ADDRESS_VERIFY_TASK_AND_ASSIGN_REGIONAL_OFFICE.label)
+      end
+
+      context "in cancel task modal" do
+        before do
+          click_dropdown(text: Constants.TASK_ACTIONS.CANCEL_ADDRESS_VERIFY_TASK_AND_ASSIGN_REGIONAL_OFFICE.label)
+        end
+
+        it "has Regional Office dropdown and notes field" do
+          expect(page).to have_field("regionalOffice")
+          expect(page).to have_field("notes")
+        end
+
+        it "Regional Office and notes are editable" do
+          click_dropdown(text: "Atlanta, GA")
+          fill_in("Notes", with: instructions_text)
+        end
+
+        it "can't submit form without specifying a Regional Office" do
+          click_button("Confirm")
+
+          expect(page).to have_content COPY::REGIONAL_OFFICE_REQUIRED_MESSAGE
+        end
+
+        it "can submit form with Regional Office and no notes" do
+          click_dropdown(text: "Atlanta, GA")
+
+          click_button("Confirm")
+
+          expect(page).to have_content COPY::CANCEL_ADDRESS_VERIFY_TASK_AND_ASSIGN_REGIONAL_OFFICE_MODAL_UPDATED_SUCCESS_TITLE
+        end
+
+        it "can submit form with Regional Office and notes" do
+          click_dropdown(text: "Atlanta, GA")
+          fill_in("Notes", with: instructions_text)
+
+          click_button("Confirm")
+
+          expect(page).to have_content COPY::CANCEL_ADDRESS_VERIFY_TASK_AND_ASSIGN_REGIONAL_OFFICE_MODAL_UPDATED_SUCCESS_TITLE
+        end
+      end
+    end
+
+    context "with a regular member" do
+      before do
+        User.authenticate!(user: user)
+      end
+
+      it "has no actions" do
+        visit("/queue/appeals/#{appeal_id}")
+
+        expect(page).to have_content("Verify Address")
+        expect(page).not_to have_selector(".Select-control")
+      end
+    end
+  end
 end
 
 RSpec.feature HearingAdminActionVerifyAddressTask do
