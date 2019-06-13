815576a4ab257deb9c62509b2f4c7623e1970c47
Ferris Tseng <ferris@navapbc.com>
2019-06-13 10:48:45 -0400

    Revert "make method a wrapper around find_by_file_number_or_ssn"
    This reverts commit 1b3896a695060b33a284b31d642bbf053749263c.

--- app/queries/veteran_finder_query.rb
+++ app/queries/veteran_finder_query.rb
@@ -25,10 +25,18 @@ class VeteranFinderQuery
 
   class << self
     def find_by_ssn_or_file_number(file_number_or_ssn:)
-      veteran = Veteran.find_by_file_number_or_ssn(file_number_or_ssn)
+      # Multiple possibilities:
+      #
+      #   1. The input is an SSN or File Number that maps directly to the `file_number`
+      #      field for a vet.
+      #   2. The input is an SSN that requires a BGS lookup for the File Number.
+      #   3. Combination of both.
+      veteran_file_number_or_ssn = Veteran.find_by(file_number: file_number_or_ssn)
+      veteran_bgs_lookup = if file_number_or_ssn.length == 9
+                             Veteran.find_by_ssn(file_number_or_ssn)
+                           end
 
-      # TODO will this return multiple veterans in the future?
-      [veteran].select { |vet| !vet.nil? }.uniq(&:id)
+      [veteran_file_number_or_ssn, veteran_bgs_lookup].select { |vet| !vet.nil? }.uniq(&:id)
     end
 
     def find_appeals_with_file_numbers(file_numbers:)
