48bc79f78882fc27b9279b55b102799bebd287da
Sally Maki <sallymaki@gmail.com>
2019-06-17 14:10:09 -0700

    Update contention text in VBMS in request issues update (#11030)
    connects #10795
    
    ### Description
    Update contentions in VBMS when their text is updated in RequestIssuesUpdate
    Change RequestIssuesUpdate to perform now instead of asynchronously
    
    *Schema Changes Only*
    
    * [x] Column comments updated
    * [ ] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
    * [ ] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
    * [x] #appeals-schema notified with summary and link to this PR

--- app/models/request_issues_update.rb
+++ app/models/request_issues_update.rb
@@ -58,10 +58,10 @@ class RequestIssuesUpdate < ApplicationRecord
     review.end_product_establishments.each { |epe| epe.user = user } if review.processed_in_vbms?
 
     review.establish!
-
+    edited_issues.each { |issue| RequestIssueContention.new(issue).update_text! }
     potential_end_products_to_remove = []
     removed_or_withdrawn_issues.select(&:end_product_establishment).each do |request_issue|
-      request_issue.end_product_establishment.remove_contention!(request_issue)
+      RequestIssueContention.new(request_issue).remove!
       potential_end_products_to_remove << request_issue.end_product_establishment
     end
 
@@ -199,7 +199,7 @@ class RequestIssuesUpdate < ApplicationRecord
     edited_issue_data.each do |edited_issue|
       RequestIssue.find(
         edited_issue[:request_issue_id].to_s
-      ).save_edit_contention_text!(edited_issue[:edited_description])
+      ).save_edited_contention_text!(edited_issue[:edited_description])
     end
   end
 
