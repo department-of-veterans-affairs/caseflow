4be31fc6eddff3e406aaf1a0de0c45c63856963d
lowellrex <lowell@navahq.com>
2019-06-17 17:17:57 -0400

    Fix failing tests

--- spec/models/tasks/judge_dispatch_return_task_spec.rb
+++ spec/models/tasks/judge_dispatch_return_task_spec.rb
@@ -14,7 +14,7 @@ describe JudgeDispatchReturnTask do
       let(:expected_actions) do
         [
           Constants.TASK_ACTIONS.ADD_ADMIN_ACTION.to_h,
-          Constants.TASK_ACTIONS.PLACE_TIMED_HOLD.to_h,
+          Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
           Constants.TASK_ACTIONS.JUDGE_AMA_CHECKOUT.to_h,
           Constants.TASK_ACTIONS.JUDGE_DISPATCH_RETURN_TO_ATTORNEY.to_h,
           Constants.TASK_ACTIONS.CANCEL_TASK.to_h
