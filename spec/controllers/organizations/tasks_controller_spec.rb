c04595117586c5e506554a2c069d20fa37d5667a
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2019-06-17 09:55:33 -0400

    Pass queue table config to front-end from back-end (#11006)
    * Pass queue table config to front-end from back-end
    
    * Remove front-end work
    
    * Undo some changes
    
    * Back out changes that will be accomplished in #11017
    
    * Include tasks in tab config
    
    * Change name of tasks element to allow for replacement in the future
    
    * add label field
    
    * Inline columns
    
    * Describe some tests we need to write
    
    * Add controller test
    
    * Add GenericQueue tests
    
    * Add organizations tests
    
    * Add tests for QueueConfig
    
    * Remove outdated TODO comment
    
    * Fix merge conflict

--- spec/controllers/organizations/tasks_controller_spec.rb
+++ spec/controllers/organizations/tasks_controller_spec.rb
@@ -76,6 +76,14 @@ RSpec.describe Organizations::TasksController, type: :controller do
         expect(response_body.size).to eq 2
         expect(response_body[0]["attributes"]["available_hearing_locations"]).to be_empty
       end
+
+      it "has a response body with the correct shape" do
+        get(:index, params: { organization_url: url })
+        expect(response.status).to eq(200)
+        response_body = JSON.parse(response.body)
+
+        expect(response_body.keys).to match_array(%w[organization_name tasks id is_vso queue_config])
+      end
     end
 
     context "when user doesn't have proper role" do
