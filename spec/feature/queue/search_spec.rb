f20bef0b910d18dc257911e7cf3f60c9a2172873
Sally Maki <sallymaki@gmail.com>
2019-06-18 16:57:25 -0700

    Fix queue search spec

--- spec/feature/queue/search_spec.rb
+++ spec/feature/queue/search_spec.rb
@@ -122,8 +122,7 @@ RSpec.feature "Search" do
                   :end_product_establishment,
                   source: higher_level_review,
                   veteran_file_number: appeal.veteran_file_number,
-                  synced_status: "CAN",
-                  committed_at: Time.zone.now
+                  synced_status: "CAN"
                 )
               end
               let!(:end_product_establishment_2) do
@@ -131,8 +130,7 @@ RSpec.feature "Search" do
                   :end_product_establishment,
                   source: higher_level_review,
                   veteran_file_number: appeal.veteran_file_number,
-                  synced_status: "CLR",
-                  committed_at: Time.zone.now
+                  synced_status: "CLR"
                 )
               end
               let!(:end_product_establishment_3) do
@@ -184,6 +182,8 @@ RSpec.feature "Search" do
               context "the EP has been established" do
                 before do
                   higher_level_review.reload.establish!
+                  end_product_establishment_1.commit!
+                  end_product_establishment_2.commit!
                 end
 
                 it "shows the end product status" do
