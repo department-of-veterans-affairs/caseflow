36c300a7ed5b073fe1a34ff708942c07f16b402e
Ferris Tseng <ferris@navapbc.com>
2019-06-18 12:53:17 -0400

    update route parameter naming to indicate plurality

--- config/routes.rb
+++ config/routes.rb
@@ -194,7 +194,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
 
-  get 'cases/:caseflow_veteran_id', to: 'appeals#show_case_list'
+  get 'cases/:caseflow_veteran_ids', to: 'appeals#show_case_list'
   get 'cases_to_schedule/:ro', to: 'tasks#ready_for_hearing_schedule'
 
   scope path: '/queue' do
