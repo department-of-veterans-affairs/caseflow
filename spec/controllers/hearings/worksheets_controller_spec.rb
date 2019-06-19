1eb5576c6de47680e276bc35c23eff418b9b5704
Ferris Tseng <ferris@navapbc.com>
2019-06-19 16:16:01 -0400

    drop focus

--- spec/controllers/hearings/worksheets_controller_spec.rb
+++ spec/controllers/hearings/worksheets_controller_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Hearings::WorksheetsController, type: :controller, focus: true do
+RSpec.describe Hearings::WorksheetsController, type: :controller do
   let!(:user) { User.authenticate!(roles: ["Hearing Prep"]) }
   let(:legacy_hearing) { create(:legacy_hearing) }
   let(:hearing) { create(:hearing, :with_tasks) }
