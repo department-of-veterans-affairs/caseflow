712d3d96cc20cabc102859d0fb7cc1339898537c
Philip Becker <philip.becker@oddball.io>
2019-06-20 14:35:05 -0400

    remove unused code

--- app/controllers/lighthouse_api/docs/v0/docs_controller.rb
+++ app/controllers/lighthouse_api/docs/v0/docs_controller.rb
@@ -3,7 +3,6 @@ module LighthouseApi
     module V0
       class DocsController < LighthouseApi::ApiController
         def intakes
-          # swagger = YAML.safe_load(File.read('intakes.yaml'))
           swagger = YAML.safe_load(File.read('app/controllers/lighthouse_api/docs/v0/intakes.yaml'))
           render json: swagger
         end
