668ca1e57490cce60d06aa3ab5977b4db3ab7c30
Peter Karman <peter@truss.works>
2019-06-20 17:24:59 -0500

    revert factory changes

--- spec/factories/veteran.rb
+++ spec/factories/veteran.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     name_suffix { "II" }
 
     transient do
-      ssn { Generators::Random.unique_ssn }
+      ssn { "987654321" }
 
       bgs_veteran_record do
         {
@@ -27,7 +27,7 @@ FactoryBot.define do
       end
     end
 
-    sequence(:file_number, 10_000_000)
+    sequence(:file_number, 100_000_000)
     sequence(:participant_id, 500_000_000)
 
     after(:build) do |veteran, evaluator|
