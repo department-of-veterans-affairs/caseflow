57764c392127e848908460aa152e9374a171d2a2
Philip Becker <philip.becker@oddball.io>
2019-06-20 10:56:02 -0400

    test for higher_level_review documentation

--- spec/requests/lighthouse_api/docs/v0/docs_controller_spec.rb
+++ spec/requests/lighthouse_api/docs/v0/docs_controller_spec.rb
@@ -2,12 +2,24 @@ require "rails_helper"
 
 RSpec.describe LighthouseApi::Docs::V0::DocsController, type: :request do
   describe '#intakes' do
-    fit 'should show openapi spec json' do
+    it 'should successfully return openapi spec' do
       get '/lighthouse_api/docs/v0/intakes'
       expect(response).to have_http_status(200)
-
       json = JSON.parse(response.body)
       expect(json["openapi"]).to eq('3.0.0')
+    end
+    describe '/higher_level_review documentation' do
+      before(:each) do
+        get '/lighthouse_api/docs/v0/intakes'
+      end
+      let(:hlr_doc){
+        json = JSON.parse(response.body)
+        json['paths']['/higher_level_review']
+      }
+      it 'should have POST' do
+        expect(hlr_doc).to include('post')
+      end
+      # TODO when doc is real, verify some other stuff about it
     end
   end
 end
