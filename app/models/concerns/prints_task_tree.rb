ac56605566621851da47025ed6095302d15e7ee5
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2019-06-24 11:15:11 -0700

    Print task trees (#11171)
    ### Description
    Make it possible to print task trees from the rails console.
    
    ```
    > puts RootTask.last.structure_render(:id, :status, :closed_at)
    
    RootTask 220, on_hold, (closed_at)
    ├── BvaDispatchTask 226, on_hold, (closed_at)
    │   └── BvaDispatchTask 227, assigned, (closed_at)
    ├── JudgeAssignTask 224, completed, 2019-06-14 22:35:44 UTC
    │   └── AttorneyTask 225, completed, 2019-06-14 22:35:44 UTC
    └── DistributionTask 221, on_hold, (closed_at)
        └── HearingTask 222, on_hold, (closed_at)
            └── ScheduleHearingTask 223, assigned, (closed_at)
    ```
    
    ```
    > puts LegacyAppeal.first.structure_render(:id, :status, :created_at)
    
    LegacyAppeal 1
    ├── RootTask 347, assigned, 2019-06-14 22:35:49 UTC
    └── ColocatedTask 348, on_hold, 2019-06-14 22:35:49 UTC
        └── ColocatedTask 349, on_hold, 2019-06-14 22:35:49 UTC
    ```

---
+++ app/models/concerns/prints_task_tree.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module PrintsTaskTree
+  extend ActiveSupport::Concern
+
+  def structure_render(*atts)
+    TTY::Tree.new(structure(*atts)).render
+  end
+
+  def structure(*atts)
+    leaf_name = "#{self.class.name} #{task_tree_attributes(*atts)}"
+    { "#{leaf_name}": task_tree_children.map { |child| child.structure(*atts) } }
+  end
+
+  private
+
+  def task_tree_children
+    return children.order(:id) if is_a? Task
+
+    tasks.where(parent_id: nil).order(:id)
+  end
+
+  def task_tree_attributes(*atts)
+    return attributes_to_s(*atts) if is_a? Task
+
+    id.to_s
+  end
+
+  def attributes_to_s(*atts)
+    atts_list = []
+    atts.each do |att|
+      value = attributes[att.to_s]
+      value = "(#{att})" if value.blank?
+      atts_list << value
+    end
+    atts_list.flatten.compact.join(", ")
+  end
+end
