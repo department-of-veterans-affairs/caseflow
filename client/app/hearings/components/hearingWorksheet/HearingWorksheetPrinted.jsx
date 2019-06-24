1921e52a76270564f92435bbfe1bf1a7073d4eb1
Ferris Tseng <ferris@navapbc.com>
2019-06-24 17:36:43 -0400

    add hearing summary to printed hearing worksheet

--- client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
+++ client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import DOMPurify from 'dompurify';
 import WorksheetFooter from './WorksheetFooter';
 import WorksheetHeader from './WorksheetHeader';
 
 export class HearingWorksheetPrinted extends React.Component {
+
   render() {
+    const { worksheet } = this.props;
+
     return (
       <div>
-        <WorksheetHeader print={true} />
         <WorksheetFooter
           veteranName={this.props.worksheet.veteran_fi_last_formatted}
         />
+        <WorksheetHeader print={true} />
+        <form className="cf-hearings-worksheet-form" id="cf-hearings-worksheet-summary">
+          <div className="cf-hearings-worksheet-data">
+            <label>Hearing Summary</label>
+            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(worksheet.summary) }} />
+          </div>
+        </form>
       </div>
     );
   }
