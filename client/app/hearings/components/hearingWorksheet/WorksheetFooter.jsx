40f9b0296d438d979853ee40ffe1763d7dcf9e26
Ferris Tseng <ferris@navapbc.com>
2019-06-24 17:36:13 -0400

    use text-align right instead of floating right in footer

--- client/app/hearings/components/hearingWorksheet/WorksheetFooter.jsx
+++ client/app/hearings/components/hearingWorksheet/WorksheetFooter.jsx
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 
 const WorksheetFooter = ({ veteranName }) => (
   <div className="cf-print-footer">
-    <div className="cf-push-right">
-      <div>
-        {veteranName},
-        <span className="cf-print-number" />
-      </div>
+    <div>
+      {veteranName},
+      <span className="cf-print-number" />
     </div>
   </div>
 );
