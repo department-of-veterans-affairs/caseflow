f085b64a907bddea03b1fbb3981c3f771b6a26e2
Philip Becker <philip.becker@oddball.io>
2019-06-25 18:19:13 -0400

    code climate fixes

--- app/controllers/api/docs/v3/docs_controller.rb
+++ app/controllers/api/docs/v3/docs_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class Api::Docs::V3::DocsController < ActionController::Base
+  protect_from_forgery with: :null_session
   def decision_reviews
-    swagger = YAML.safe_load(File.read('app/controllers/api/docs/v3/decision_reviews.yaml'))
+    swagger = YAML.safe_load(File.read("app/controllers/api/docs/v3/decision_reviews.yaml"))
     render json: swagger
   end
 end
