790702ce4b766b6166b1425048d9ed89d57fa779
Ferris Tseng <ferris@navapbc.com>
2019-06-25 16:46:26 -0400

    split up large method; update spacing between appeal sections

--- client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
+++ client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
@@ -10,7 +10,54 @@ import { formatDateStr, formatArrayOfDateStrings } from '../../../util/DateUtil'
 
 export class HearingWorksheetPrinted extends React.Component {
 
-  getLegacyHearingWorksheetDocsSection() {
+  getLegacyHearingWorksheetIssuesSection(appeal) {
+    const { worksheetIssues } = this.props;
+
+    return (
+      <div>
+        <h4>Issues</h4>
+      </div>
+    );
+  }
+
+  getLegacyHearingWorksheetDocsSection(appeal) {
+    return (
+      <div className="cf-hearings-worksheet-data">
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>Prior BVA Deci.</h4>
+          <div className="cf-hearings-headers">{formatDateStr(appeal.prior_bva_decision_date)}</div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>NOD</h4>
+          <div className="cf-hearings-headers">{formatDateStr(appeal.nod_date)}</div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>SOC</h4>
+          <div className="cf-hearings-headers">{formatDateStr(appeal.soc_date)}</div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>Form 9</h4>
+          <div className="cf-hearings-headers">{formatDateStr(appeal.form9_date)}</div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>SSOC</h4>
+          <div className="cf-hearings-headers">{formatArrayOfDateStrings(appeal.ssoc_dates)}</div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell">
+          <h4>Certification</h4>
+          <div className="cf-hearings-headers">
+            {!appeal.certification_date ? "Not certified" : formatDateStr(appeal.certification_date)}
+          </div>
+        </div>
+        <div className="cf-hearings-worksheet-data-cell double">
+          <h4>Docs since Cert.</h4>
+          <div className="cf-hearings-headers">{appeal.cached_number_of_documents_after_certification}</div>
+        </div>
+      </div>
+    );
+  }
+
+  getLegacyHearingSection() {
     const { worksheetAppeals } = this.props;
 
     //worksheetAppeals["1"] = worksheetAppeals["23"];//test
@@ -19,13 +66,13 @@ export class HearingWorksheetPrinted extends React.Component {
       <div className="cf-hearings-procedural-history">
         {
           Object.values(worksheetAppeals).map((appeal, key) => (
-            <div>
+            <div key={key} className="cf-hearings-appeal-procedural-history">
               <h4>
                 Appeal Stream {key + 1} - Docket #{appeal.docket_number}
                 {appeal.contested_claim && "  CC"}
                 {appeal.dic && "  DIC"}
               </h4>
-              <div className="cf-hearings-worksheet-data">
+              {this.getLegacyHearingWorksheetDocsSection(appeal)}
                 <div className="cf-hearings-worksheet-data-cell">
                   <h4>Prior BVA Deci.</h4>
                   <div className="cf-hearings-headers">{formatDateStr(appeal.prior_bva_decision_date)}</div>
@@ -74,16 +121,7 @@ export class HearingWorksheetPrinted extends React.Component {
           veteranName={this.props.worksheet.veteran_fi_last_formatted}
         />
         <WorksheetHeader print={true} />
-        {isLegacy && this.getLegacyHearingWorksheetDocsSection()}
-        {false && <div>
-          <Table
-            className="cf-hearings-worksheet-issues"
-            columns={[]}
-            rowObjects={[]}
-            summary="Worksheet Issues"
-            getKeyForRow={_.identity}
-          />
-        </div>}
+        {isLegacy && this.getLegacyHearingSection()}
         <form className="cf-hearings-worksheet-form" id="cf-hearings-worksheet-summary">
           <div className="cf-hearings-worksheet-data">
             <label>Hearing Summary</label>
