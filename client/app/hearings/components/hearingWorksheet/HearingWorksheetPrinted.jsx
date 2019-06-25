4802135e6dfb694b7f03f3c8f7eff1e7193d1cef
Ferris Tseng <ferris@navapbc.com>
2019-06-25 17:17:43 -0400

    start populating issues section for legacy appeal

--- client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
+++ client/app/hearings/components/hearingWorksheet/HearingWorksheetPrinted.jsx
@@ -5,17 +5,27 @@ import DOMPurify from 'dompurify';
 import WorksheetFooter from './WorksheetFooter';
 import WorksheetHeader from './WorksheetHeader';
 import Table from '../../../components/Table';
+import { filterCurrentIssues, filterIssuesOnAppeal } from '../../utils';
 import { formatDateStr, formatArrayOfDateStrings } from '../../../util/DateUtil';
-
 
 export class HearingWorksheetPrinted extends React.Component {
 
   getLegacyHearingWorksheetIssuesSection(appeal) {
     const { worksheetIssues } = this.props;
+    const currentIssues = filterCurrentIssues(
+      filterIssuesOnAppeal(worksheetIssues, appeal.id)
+    );
 
     return (
       <div>
         <h4>Issues</h4>
+        {
+          Object.values(currentIssues).map((issue, key) => (
+            <div key={key}>
+              {issue.id}
+            </div>
+          ))
+        }
       </div>
     );
   }
@@ -63,7 +73,7 @@ export class HearingWorksheetPrinted extends React.Component {
     //worksheetAppeals["1"] = worksheetAppeals["23"];//test
 
     return (
-      <div className="cf-hearings-procedural-history">
+      <div>
         {
           Object.values(worksheetAppeals).map((appeal, key) => (
             <div key={key} className="cf-hearings-appeal-procedural-history">
@@ -73,37 +83,7 @@ export class HearingWorksheetPrinted extends React.Component {
                 {appeal.dic && "  DIC"}
               </h4>
               {this.getLegacyHearingWorksheetDocsSection(appeal)}
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>Prior BVA Deci.</h4>
-                  <div className="cf-hearings-headers">{formatDateStr(appeal.prior_bva_decision_date)}</div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>NOD</h4>
-                  <div className="cf-hearings-headers">{formatDateStr(appeal.nod_date)}</div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>SOC</h4>
-                  <div className="cf-hearings-headers">{formatDateStr(appeal.soc_date)}</div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>Form 9</h4>
-                  <div className="cf-hearings-headers">{formatDateStr(appeal.form9_date)}</div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>SSOC</h4>
-                  <div className="cf-hearings-headers">{formatArrayOfDateStrings(appeal.ssoc_dates)}</div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell">
-                  <h4>Certification</h4>
-                  <div className="cf-hearings-headers">
-                    {!appeal.certification_date ? "Not certified" : formatDateStr(appeal.certification_date)}
-                  </div>
-                </div>
-                <div className="cf-hearings-worksheet-data-cell double">
-                  <h4>Docs since Cert.</h4>
-                  <div className="cf-hearings-headers">{appeal.cached_number_of_documents_after_certification}</div>
-                </div>
-              </div>
+              {this.getLegacyHearingWorksheetIssuesSection(appeal)}
             </div>
           ))
         }
@@ -117,9 +97,7 @@ export class HearingWorksheetPrinted extends React.Component {
 
     return (
       <div>
-        <WorksheetFooter
-          veteranName={this.props.worksheet.veteran_fi_last_formatted}
-        />
+        <WorksheetFooter veteranName={worksheet.veteran_fi_last_formatted} />
         <WorksheetHeader print={true} />
         {isLegacy && this.getLegacyHearingSection()}
         <form className="cf-hearings-worksheet-form" id="cf-hearings-worksheet-summary">
