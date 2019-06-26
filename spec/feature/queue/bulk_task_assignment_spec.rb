fd21e6338c1039a0c15fac107c0b731cceb4e4c8
Peter Karman <peter@truss.works>
2019-06-26 12:31:22 -0500

    Add failing test to show where flake is

--- spec/feature/queue/bulk_task_assignment_spec.rb
+++ spec/feature/queue/bulk_task_assignment_spec.rb
@@ -17,6 +17,7 @@ RSpec.feature "Bulk task assignment" do
       task_type.click
       number_of_tasks = options.find { |option| option.text =~ /3/ }
       number_of_tasks.click
+      expect(page).to_not have_content("Please select a value")
       submit = all("button", text: "Assign Tasks")[0]
       submit.click
     end
@@ -39,7 +40,6 @@ RSpec.feature "Bulk task assignment" do
       expect(page).to have_content("Assigned (3)")
       expect(NoShowHearingTask.where(assigned_to: user).size).to eq 3
     end
-
 
     it "filters regional office by task types" do
       # RO17 == St. Petersburg
