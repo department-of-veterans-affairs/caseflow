a04297997a89a6505ae32ab091ddc984d14e5345
Ed Mangimelli <edmangimelli@gmail.com>
2019-07-01 13:45:01 -0500

    add tests

---
+++ spec/requests/api/metadata_controller_spec.rb
@@ -0,0 +1,9 @@
+describe Api::MetadataController, type: :request do
+  describe '#index' do
+    it 'should successfully return metadata json object' do
+      get '/api/metadata'
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)).to have_key('meta')
+    end
+  end
+end
