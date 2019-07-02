c5e2ac5816da1d3e04f68952749599ca10c9273a
Moncef Belyamani <moncef@truss.works>
2019-07-02 17:07:17 -0400

    Simplify appeal_withdrawal_spec

--- spec/workflows/appeal_withdrawal_spec.rb
+++ spec/workflows/appeal_withdrawal_spec.rb
@@ -21,7 +21,7 @@ describe "Withdrawing an appeal" do
 
       tasks = appeal_with_many_request_issues.tasks.reload
 
-      expect(distribution_task(tasks).status).to eq "on_hold"
+      expect(distribution_task(tasks).status).to eq "assigned"
       expect(track_veteran_task(tasks).status).to eq "in_progress"
       expect(appeal_with_many_request_issues.root_task.status).to eq "on_hold"
     end
@@ -164,7 +164,6 @@ describe "Withdrawing an appeal" do
         request_issues: build_list(:request_issue, 1, contested_issue_description: "Knee pain")
       )
       create_track_veteran_tasks(appeal)
-      update_distribution_task_to_be_on_hold(appeal)
       appeal
     end
   end
@@ -181,7 +180,6 @@ describe "Withdrawing an appeal" do
       )
       appeal.save!
       create_track_veteran_tasks(appeal)
-      update_distribution_task_to_be_on_hold(appeal)
       appeal
     end
   end
@@ -204,10 +202,7 @@ describe "Withdrawing an appeal" do
         ineligible_reason: "untimely",
         decision_review: appeal
       )
-      appeal.request_issues = [eligible_request_issue, ineligible_request_issue]
-      appeal.save!
       create_track_veteran_tasks(appeal)
-      update_distribution_task_to_be_on_hold(appeal)
       appeal
     end
   end
@@ -231,21 +226,13 @@ describe "Withdrawing an appeal" do
         closed_at: Time.zone.now,
         decision_review: appeal
       )
-      appeal.request_issues = [eligible_request_issue, closed_request_issue]
-      appeal.save!
       create_track_veteran_tasks(appeal)
-      update_distribution_task_to_be_on_hold(appeal)
       appeal
     end
   end
 
   def create_track_veteran_tasks(appeal)
     create(:track_veteran_task, appeal: appeal)
-  end
-
-  def update_distribution_task_to_be_on_hold(appeal)
-    # Distribution task is created as part of :with_tasks trait on the appeal factory
-    distribution_task(appeal.tasks).update(status: Constants.TASK_STATUSES.on_hold)
   end
 
   def user
