df9c50923f60e61aead587a27c28e139343182d4
Ferris Tseng <ferris@navapbc.com>
2019-07-03 14:10:08 -0400

    Add Print All Button for Hearing Worksheets (#11262)
    Resolves #10293
    
    ### Description
    
    * Add new container to get multiple hearings to print
    * Create new route that takes an comma-separate list of IDs
      * Use the print route that accepts multiple IDs instead of the one that accepts a single ID for consistency
    * Wrap hearing worksheet in a table as a workaround to get footers displaying correctly for the multi-worksheet print view
    
    ### Acceptance Criteria
    
    - [x] Verify there is a button at the top of the Hearing Prep Daily Docket that when pressed, sends all the VLJ Hearing Worksheets for that day to the default printer to print.

--- client/app/util/PrintUtil.js
+++ client/app/util/PrintUtil.js
@@ -1,11 +1,14 @@
+import _ from 'lodash';
 import { PRINT_WINDOW_TIMEOUT_IN_MS } from '../constants/AppConstants';
 
 export const openPrintDialogue = () => {
   setTimeout(() => window.print(), PRINT_WINDOW_TIMEOUT_IN_MS);
 };
 
-export const navigateToPrintPage = () => {
-  // A route for the current path + /print must exist for this to
-  // work.
-  window.open(`${window.location.pathname}/print`, '_blank', 'noopener noreferrer');
+export const navigateToPrintPage = (url) => {
+  window.open(
+    _.isUndefined(url) ? `${window.location.pathname}/print` : url,
+    '_blank',
+    'noopener noreferrer'
+  );
 };
