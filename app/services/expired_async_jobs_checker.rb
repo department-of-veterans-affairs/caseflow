23a727edc2b60ce92038f20afa6ef5b426f31e7c
Peter Karman <peter@truss.works>
2019-07-05 15:09:21 -0500

    add test for DataIntegrityChecksJob

--- app/services/expired_async_jobs_checker.rb
+++ app/services/expired_async_jobs_checker.rb
@@ -4,6 +4,8 @@ class ExpiredAsyncJobsChecker < DataIntegrityChecker
   def call
     jobs = AsyncableJobs.new(page_size: -1).jobs.select(&:expired_without_processing?)
     job_reporter = AsyncableJobsReporter.new(jobs: jobs)
+    return unless jobs.count > 0
+
     add_to_report "Expired Jobs: #{jobs.count} expired unfinished asyncable jobs exist in the queue."
     add_to_report job_reporter.summarize
   end
---
+++ spec/jobs/data_integrity_checks_job_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+describe DataIntegrityChecksJob do
+  let(:expired_async_jobs_checker) { ExpiredAsyncJobsChecker.new }
+  let(:untracked_legacy_appeals_checker) { UntrackedLegacyAppealsChecker.new }
+  let(:slack_service) { SlackService.new(url: "http://www.example.com") }
+
+  before do
+    allow(ExpiredAsyncJobsChecker).to receive(:new).and_return(expired_async_jobs_checker)
+    allow(UntrackedLegacyAppealsChecker).to receive(:new).and_return(untracked_legacy_appeals_checker)
+    allow(SlackService).to receive(:new).and_return(slack_service)
+    [expired_async_jobs_checker, untracked_legacy_appeals_checker].each do |checker|
+      allow(checker).to receive(:call).and_call_original
+      allow(checker).to receive(:report?).and_call_original
+      allow(checker).to receive(:report).and_call_original
+    end
+    allow(slack_service).to receive(:send_notification).and_call_original
+  end
+
+  describe "#perform" do
+    it "does not send slack notifications unless there is a report" do
+      described_class.perform_now
+  
+      expect(expired_async_jobs_checker).to have_received(:call).once
+      expect(expired_async_jobs_checker).to have_received(:report?).once
+      expect(expired_async_jobs_checker).to_not have_received(:report)
+  
+      expect(untracked_legacy_appeals_checker).to have_received(:call).once
+      expect(untracked_legacy_appeals_checker).to have_received(:report?).once
+      expect(untracked_legacy_appeals_checker).to_not have_received(:report)
+  
+      expect(slack_service).to_not have_received(:send_notification)
+    end
+  end
+end
