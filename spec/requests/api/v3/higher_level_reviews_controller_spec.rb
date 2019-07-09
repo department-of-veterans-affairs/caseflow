60204c314c3b1b5ea425e566eeec89128eda72d0
Philip Becker <philip.becker@oddball.io>
2019-07-09 15:19:37 -0400

    initial expectation

--- spec/requests/api/v3/higher_level_reviews_controller_spec.rb
+++ spec/requests/api/v3/higher_level_reviews_controller_spec.rb
@@ -1,5 +1,10 @@
 describe Api::V3::HigherLevelReviewsController, type: :request do
   describe '#create' do
-    it 'should return a 202 on success'
+    it 'should return a 202 on success' do
+      get '/api/docs/v3/decision_reviews'
+      expect(response).to have_http_status(202)
+      # json = JSON.parse(response.body)
+      # expect(json["openapi"]).to eq('3.0.0')
+    end
   end
 end
