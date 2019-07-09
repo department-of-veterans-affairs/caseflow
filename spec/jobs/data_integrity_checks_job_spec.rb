b986cb82057b32a82ec7e7fee6ea44d4e69675c7
Peter Karman <peter@truss.works>
2019-07-09 14:22:59 -0500

    more tests

--- spec/jobs/data_integrity_checks_job_spec.rb
+++ spec/jobs/data_integrity_checks_job_spec.rb
@@ -14,7 +14,7 @@ describe DataIntegrityChecksJob do
       allow(checker).to receive(:report?).and_call_original
       allow(checker).to receive(:report).and_call_original
     end
-    allow(slack_service).to receive(:send_notification).and_call_original
+    allow(slack_service).to receive(:send_notification).and_return(true)
   end
 
   describe "#perform" do
@@ -30,6 +30,21 @@ describe DataIntegrityChecksJob do
       expect(untracked_legacy_appeals_checker).to_not have_received(:report)
 
       expect(slack_service).to_not have_received(:send_notification)
+    end
+
+    context "expired async jobs exist" do
+      before do
+        expired_async_jobs_checker.add_to_report "1 expired async job"
+      end
+
+      it "sends slack notification if there is a report" do
+        described_class.perform_now
+
+        expect(expired_async_jobs_checker).to have_received(:call).once
+        expect(expired_async_jobs_checker).to have_received(:report?).once
+        expect(expired_async_jobs_checker).to have_received(:report).once
+        expect(slack_service).to have_received(:send_notification)
+      end
     end
   end
 end
