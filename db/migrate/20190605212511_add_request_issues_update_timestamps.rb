ed94428823f4a20a567dfb7973c46aedfb454bae
Peter Karman <peter@truss.works>
2019-07-09 16:33:52 -0500

    add comments

--- db/migrate/20190605212511_add_request_issues_update_timestamps.rb
+++ db/migrate/20190605212511_add_request_issues_update_timestamps.rb
@@ -1,6 +1,6 @@
 class AddRequestIssuesUpdateTimestamps < ActiveRecord::Migration[5.1]
   def change
-    add_column :request_issues_updates, :created_at, :datetime
-    add_column :request_issues_updates, :updated_at, :datetime
+    add_column :request_issues_updates, :created_at, :datetime, comment: "Timestamp when record was initially created"
+    add_column :request_issues_updates, :updated_at, :datetime, comment: "Timestamp when record was last updated."
   end
 end
