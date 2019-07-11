100d49ec7025951460ea5fd1cbfe0603b0577759
Philip Becker <philip.becker@oddball.io>
2019-07-11 18:53:26 -0400

    start work on IntakeStatus response

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -6,9 +6,24 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < ActionController::
   def create
     # new HigherLevelReview and return the serialized version
     mock_hlr = HigherLevelReview.new(uuid: 'FAKEuuid-mock-test-fake-mocktestdata')
-    # add a detail
+    # TODO return an IntakeStatus object
     render json: {data: {type: 'high_level_review', id: mock_hlr.uuid, attributes: mock_hlr.attributes}}, status: 202 # TODO add serializer for intake
   rescue => e
     render plain: e.message
+  end
+
+private
+  def intake_status(hlr)
+    #FIXME get this order right
+    #REVIEW should this be in asyncable?
+    if hlr.processed?
+      :processed
+    elsif hlr.attempted?
+      :attempted
+    elsif hlr.submitted?
+      :submitted
+    elsif hlr.cancelled?
+      :cancelled
+    end
   end
 end
