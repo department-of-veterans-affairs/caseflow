d069a2f3fe8dc3d9cda0b0918cc1802d602b70e3
Philip Becker <philip.becker@oddball.io>
2019-07-12 17:30:42 -0400

    support content header

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -8,6 +8,7 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < ActionController::
       uuid: 'FAKEuuid-mock-test-fake-mocktestdata',
       establishment_submitted_at: Time.zone.now # having this timestamp marks it as submitted
     )
+    response.set_header('Content-Location', "#{request.base_url}/api/v3/decision_review/higher_level_reviews/intake_status/999") #FIXME should be an intake id
     render json: intake_status(mock_hlr), status: 202
   end
 
