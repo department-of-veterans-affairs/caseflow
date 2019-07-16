6eb305069abfb972faed6a9159217f57016134f9
Sharon Warner <sharon.warner56@gmail.com>
2019-07-16 12:53:20 -0400

    Code climate fixes

--- app/services/hearing_schedule/validate_judge_spreadsheet.rb
+++ app/services/hearing_schedule/validate_judge_spreadsheet.rb
@@ -5,6 +5,9 @@ class HearingSchedule::ValidateJudgeSpreadsheet
   SPREADSHEET_HEADERS = [nil, "Judge Name", "VLJ #", "Date"].freeze
   SPREADSHEET_EMPTY_COLUMN = [nil].freeze
 
+  TEMPLATE_ERROR = "The template was not followed. Please redownload the template and try again."
+  WRONG_DATE_FORMAT_ERROR = "These dates are in the wrong format: "
+
   class JudgeDatesNotCorrectFormat < StandardError; end
   class JudgeTemplateNotFollowed < StandardError; end
   class JudgeDatesNotUnique < StandardError; end
@@ -24,7 +27,7 @@ class HearingSchedule::ValidateJudgeSpreadsheet
     unless @spreadsheet_template[:title] == SPREADSHEET_TITLE &&
            @spreadsheet_template[:headers] == SPREADSHEET_HEADERS &&
            @spreadsheet_template[:empty_column] == SPREADSHEET_EMPTY_COLUMN
-      @errors << JudgeTemplateNotFollowed.new("The template was not followed. Please redownload the template and try again.")
+      @errors << JudgeTemplateNotFollowed.new(TEMPLATE_ERROR)
     end
   end
 
@@ -51,39 +54,44 @@ class HearingSchedule::ValidateJudgeSpreadsheet
       vacols_judges[vlj_id][:last_name].casecmp(name.split(", ")[0].strip.downcase).zero?
   end
 
-  def get_incorrectly_formatted_dates
-    @spreadsheet_data.reject { |row| row["date"].instance_of?(Date) || row["date"] == "N/A" }.pluck("date")
+  def filter_incorrectly_formatted_dates
+    @spreadsheet_data.reject do |row|
+      date = row["date"]
+      date.instance_of?(Date) || date == "N/A"
+    end.pluck("date")
   end
 
-  def get_nonunique_judges
+  def filter_nonunique_judges
     @spreadsheet_data.select { |row| @spreadsheet_data.count(row) > 1 }.pluck("vlj_id").uniq
   end
 
-  def get_out_of_range_dates
-    @spreadsheet_data.reject do |row|
+  def filter_out_of_range_dates
+    out_of_range_dates = @spreadsheet_data.reject do |row|
       !row["date"].instance_of?(Date) || (row["date"] >= @start_date && row["date"] <= @end_date)
-    end.pluck("date").map { |date| date.strftime("%m/%d/%Y") }
+    end.pluck("date")
+
+    out_of_range_dates.map { |date| date.strftime("%m/%d/%Y") }
   end
 
-  def get_judges_not_in_db
+  def filter_judges_not_in_db
     vacols_judges = User.css_ids_by_vlj_ids(@spreadsheet_data.pluck("vlj_id").uniq)
     @spreadsheet_data.select { |row| !judge_in_vacols?(vacols_judges, row["name"], row["vlj_id"]) }.pluck("vlj_id")
   end
 
   def validate_judge_non_availability_dates
-    incorrectly_formatted_dates = get_incorrectly_formatted_dates
+    incorrectly_formatted_dates = filter_incorrectly_formatted_dates
     if incorrectly_formatted_dates.count > 0
-      @errors << JudgeDatesNotCorrectFormat.new("These dates are in the wrong format: " + incorrectly_formatted_dates.to_s)
+      @errors << JudgeDatesNotCorrectFormat.new(WRONG_DATE_FORMAT_ERROR + incorrectly_formatted_dates.to_s)
     end
-    nonunique_judges = get_nonunique_judges
+    nonunique_judges = filter_nonunique_judges
     if nonunique_judges.count > 0
       @errors << JudgeDatesNotUnique.new("These judges have duplicate dates: " + nonunique_judges.to_s)
     end
-    out_of_range_dates = get_out_of_range_dates
+    out_of_range_dates = filter_out_of_range_dates
     if out_of_range_dates.count > 0
       @errors << JudgeDatesNotInRange.new("These dates are out of the selected range: " + out_of_range_dates.to_s)
     end
-    judges_not_in_db = get_judges_not_in_db
+    judges_not_in_db = filter_judges_not_in_db
     if judges_not_in_db.count > 0
       @errors << JudgeNotInDatabase.new("These judges are not in the database: " + judges_not_in_db.to_s)
     end
