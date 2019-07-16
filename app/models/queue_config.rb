9218ce4a0b1abab03e0e008814d0a34b6cdcb6c4
lowellrex <lowell@navahq.com>
2019-07-16 08:57:54 -0400

    Remove user argument

--- app/models/queue_config.rb
+++ app/models/queue_config.rb
@@ -41,7 +41,7 @@ class QueueConfig
 
     # Only return tasks in the configuration if we are using it to populate the first page of results.
     # Otherwise avoid the overhead of the additional database requests.
-    tasks = use_task_pages_api?(user) ? serialized_tasks_for_user(task_pager.paged_tasks, user, tab.columns) : []
+    tasks = use_task_pages_api?(user) ? serialized_tasks_for_user(task_pager.paged_tasks, tab.columns) : []
 
     endpoint = "#{organization.path}/task_pages?#{Constants.QUEUE_CONFIG.TAB_NAME_REQUEST_PARAM}=#{tab.name}"
 
@@ -54,7 +54,7 @@ class QueueConfig
     )
   end
 
-  def serialized_tasks_for_user(tasks, user, columns)
+  def serialized_tasks_for_user(tasks, columns)
     return [] if tasks.empty?
 
     primed_tasks = AppealRepository.eager_load_legacy_appeals_for_tasks(tasks)
@@ -62,10 +62,7 @@ class QueueConfig
     WorkQueue::TaskColumnSerializer.new(
       primed_tasks,
       is_collection: true,
-      params: {
-        user: user,
-        columns: columns
-      }
+      params: { columns: columns }
     ).serializable_hash[:data]
   end
 end
