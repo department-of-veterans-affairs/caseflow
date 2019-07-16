8b38beb294c4a8e8762e47ed5fd71c2a3fb86ce0
Philip Becker <philip.becker@oddball.io>
2019-07-16 15:47:48 -0400

    fix linting issues with asynable tests

--- spec/models/concerns/asyncable_spec.rb
+++ spec/models/concerns/asyncable_spec.rb
@@ -7,32 +7,33 @@ describe Asyncable do
 
     attr_accessor :submitted_at, :attempted_at, :processed_at, :canceled_at
 
-    def [](column) # access attrs as if they were activerecord columns
-      self.send(column.to_sym)
+    # access attrs as if they were activerecord columns
+    def [](column)
+      send(column.to_sym)
     end
   end
 
   describe "#asyncable_status" do
-    it 'should return :not_yet_submitted if there is no timestamps' do
+    it "should return :not_yet_submitted if there is no timestamps" do
       subject = TestAsyncable.new
       expect(subject.asyncable_status).to eq :not_yet_submitted
     end
-    it 'should return :processed when the processed timestamp is set' do
+    it "should return :processed when the processed timestamp is set" do
       t = Time.zone.now
       subject = TestAsyncable.new(submitted_at: t - 10.minutes, attempted_at: t - 5.minutes, processed_at: t)
       expect(subject.asyncable_status).to eq :processed
     end
-    it 'should return :canceled when the canceled timestamp is set' do
+    it "should return :canceled when the canceled timestamp is set" do
       t = Time.zone.now
       subject = TestAsyncable.new(submitted_at: t - 10.minutes, attempted_at: t - 5.minutes, canceled_at: t)
       expect(subject.asyncable_status).to eq :canceled
     end
-    it 'should return :attempted when the attempted timestamp is set' do
+    it "should return :attempted when the attempted timestamp is set" do
       t = Time.zone.now
       subject = TestAsyncable.new(submitted_at: t - 5.minutes, attempted_at: t)
       expect(subject.asyncable_status).to eq :attempted
     end
-    it 'should return :submitted when the submitted timestamp is set' do
+    it "should return :submitted when the submitted timestamp is set" do
       subject = TestAsyncable.new(submitted_at: Time.zone.now)
       expect(subject.asyncable_status).to eq :submitted
     end
