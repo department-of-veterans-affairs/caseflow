ebb1f5867bbc7697a7903a798daf6f3459bf6915
Philip Becker <philip.becker@oddball.io>
2019-07-16 15:13:53 -0400

    tests for asyncable_status

--- spec/models/concerns/asyncable_spec.rb
+++ spec/models/concerns/asyncable_spec.rb
@@ -8,8 +8,7 @@ fdescribe Asyncable do #TODO remove f
     attr_accessor :submitted_at, :attempted_at, :processed_at, :canceled_at
 
     def [](column) # access attrs as if they were activerecord columns
-      name = column.match(/^\w+_at/).to_s
-      self.send(name.to_sym)
+      self.send(column.to_sym)
     end
   end
 
@@ -20,10 +19,25 @@ fdescribe Asyncable do #TODO remove f
       subject = TestAsyncable.new
       expect(subject.asyncable_status).to eq :not_yet_submitted
     end
-    it 'should return :processed when the processed timestamp is set'
-    it 'should return :canceled when the canceled timestamp is set'
-    it 'should return :attempted when the attempted timestamp is set'
-    it 'should return :submitted when the submitted timestamp is set'
+    it 'should return :processed when the processed timestamp is set' do
+      t = Time.zone.now
+      subject = TestAsyncable.new(submitted_at: t - 10.minutes, attempted_at: t - 5.minutes, processed_at: t)
+      expect(subject.asyncable_status).to eq :processed
+    end
+    it 'should return :canceled when the canceled timestamp is set' do
+      t = Time.zone.now
+      subject = TestAsyncable.new(submitted_at: t - 10.minutes, attempted_at: t - 5.minutes, canceled_at: t)
+      expect(subject.asyncable_status).to eq :canceled
+    end
+    it 'should return :attempted when the attempted timestamp is set' do
+      t = Time.zone.now
+      subject = TestAsyncable.new(submitted_at: t - 5.minutes, attempted_at: t)
+      expect(subject.asyncable_status).to eq :attempted
+    end
+    it 'should return :submitted when the submitted timestamp is set' do
+      subject = TestAsyncable.new(submitted_at: Time.zone.now)
+      expect(subject.asyncable_status).to eq :submitted
+    end
     # TODO add cases with multiple timestamps
   end
 end
