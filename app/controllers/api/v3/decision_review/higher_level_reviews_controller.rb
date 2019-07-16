bcb21d6d143b25114c38e41592434b577fd59e7b
Philip Becker <philip.becker@oddball.io>
2019-07-16 15:38:07 -0400

    linting fixes

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -5,18 +5,23 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < ActionController::
 
   def create
     mock_hlr = HigherLevelReview.new(
-      uuid: 'FAKEuuid-mock-test-fake-mocktestdata',
+      uuid: "FAKEuuid-mock-test-fake-mocktestdata",
       establishment_submitted_at: Time.zone.now # having this timestamp marks it as submitted
     )
-    response.set_header('Content-Location', "#{request.base_url}/api/v3/decision_review/higher_level_reviews/intake_status/999") #FIXME should be an intake id
+    response.set_header(
+      "Content-Location",
+      # id returned is static, if a mock intake_status is created, this should match
+      "#{request.base_url}/api/v3/decision_review/higher_level_reviews/intake_status/999"
+    )
     render json: intake_status(mock_hlr), status: 202
   end
 
-private
+  private
+
   def intake_status(higher_level_review)
     {
       data: {
-        type: 'IntakeStatus',
+        type: "IntakeStatus",
         id: higher_level_review.uuid,
         attributes: {
           status: higher_level_review.asyncable_status
