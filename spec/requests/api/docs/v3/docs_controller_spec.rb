c220bacef2ae529c4d179f18fd6e7a80e7fed71c
Philip Becker <philip.becker@oddball.io>
2019-07-19 17:49:31 -0400

    double quotes for linting

--- spec/requests/api/docs/v3/docs_controller_spec.rb
+++ spec/requests/api/docs/v3/docs_controller_spec.rb
@@ -1,45 +1,45 @@
 describe Api::Docs::V3::DocsController, type: :request do
-  describe '#decision_reviews' do
-    it 'should successfully return openapi spec' do
-      get '/api/docs/v3/decision_reviews'
+  describe "#decision_reviews" do
+    it "should successfully return openapi spec" do
+      get "/api/docs/v3/decision_reviews"
       expect(response).to have_http_status(200)
       json = JSON.parse(response.body)
-      expect(json["openapi"]).to eq('3.0.0')
+      expect(json["openapi"]).to eq("3.0.0")
     end
-    describe '/higher_level_reviews documentation' do
+    describe "/higher_level_reviews documentation" do
       before(:each) do
-        get '/api/docs/v3/decision_reviews'
+        get "/api/docs/v3/decision_reviews"
       end
       let(:hlr_doc) do
         json = JSON.parse(response.body)
-        json['paths']['/higher_level_reviews']
+        json["paths"]["/higher_level_reviews"]
       end
-      it 'should have POST' do
-        expect(hlr_doc).to include('post')
+      it "should have POST" do
+        expect(hlr_doc).to include("post")
       end
     end
-    describe '/higher_level_reviews/intake_status/{uuid} documentation' do
+    describe "/higher_level_reviews/intake_status/{uuid} documentation" do
       before(:each) do
-        get '/api/docs/v3/decision_reviews'
+        get "/api/docs/v3/decision_reviews"
       end
       let(:hlr_intake_status_doc) do
         json = JSON.parse(response.body)
-        json['paths']['/higher_level_reviews/intake_status/{uuid}']
+        json["paths"]["/higher_level_reviews/intake_status/{uuid}"]
       end
-      it 'should have GET' do
-        expect(hlr_intake_status_doc).to include('get')
+      it "should have GET" do
+        expect(hlr_intake_status_doc).to include("get")
       end
     end
-    describe '/higher_level_reviews/{uuid} documentation' do
+    describe "/higher_level_reviews/{uuid} documentation" do
       before(:each) do
-        get '/api/docs/v3/decision_reviews'
+        get "/api/docs/v3/decision_reviews"
       end
       let(:hlr_intake_status_doc) do
         json = JSON.parse(response.body)
-        json['paths']['/higher_level_reviews/{uuid}']
+        json["paths"]["/higher_level_reviews/{uuid}"]
       end
-      it 'should have GET' do
-        expect(hlr_intake_status_doc).to include('get')
+      it "should have GET" do
+        expect(hlr_intake_status_doc).to include("get")
       end
     end
   end
