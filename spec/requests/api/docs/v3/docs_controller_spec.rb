a11d5c76b345b0166dc8b6988d3fb593dcfe722b
Philip Becker <philip.becker@oddball.io>
2019-07-19 17:22:26 -0400

    expand docs spec

--- spec/requests/api/docs/v3/docs_controller_spec.rb
+++ spec/requests/api/docs/v3/docs_controller_spec.rb
@@ -6,7 +6,7 @@ describe Api::Docs::V3::DocsController, type: :request do
       json = JSON.parse(response.body)
       expect(json["openapi"]).to eq('3.0.0')
     end
-    describe '/higher_level_review documentation' do
+    describe '/higher_level_reviews documentation' do
       before(:each) do
         get '/api/docs/v3/decision_reviews'
       end
@@ -17,7 +17,30 @@ describe Api::Docs::V3::DocsController, type: :request do
       it 'should have POST' do
         expect(hlr_doc).to include('post')
       end
-      # TODO when doc is real, verify some other stuff about it
+    end
+    describe '/higher_level_reviews/intake_status/{uuid} documentation' do
+      before(:each) do
+        get '/api/docs/v3/decision_reviews'
+      end
+      let(:hlr_intake_status_doc) do
+        json = JSON.parse(response.body)
+        json['paths']['/higher_level_reviews/intake_status/{uuid}']
+      end
+      it 'should have GET' do
+        expect(hlr_intake_status_doc).to include('get')
+      end
+    end
+    describe '/higher_level_reviews/{uuid} documentation' do
+      before(:each) do
+        get '/api/docs/v3/decision_reviews'
+      end
+      let(:hlr_intake_status_doc) do
+        json = JSON.parse(response.body)
+        json['paths']['/higher_level_reviews/{uuid}']
+      end
+      it 'should have GET' do
+        expect(hlr_intake_status_doc).to include('get')
+      end
     end
   end
 end
