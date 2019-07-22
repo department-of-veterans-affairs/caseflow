d79d917443a8863c0d429cf70eee5b584cd840b2
Peter Karman <peter@truss.works>
2019-07-22 09:05:32 -0500

    lint

--- spec/feature/intake/higher_level_review/edit_spec.rb
+++ spec/feature/intake/higher_level_review/edit_spec.rb
@@ -1021,7 +1021,7 @@ feature "Higher Level Review Edit issues" do
           { description: RequestIssue::UNIDENTIFIED_ISSUE_MSG },
           { description: "Left knee granted" },
           { description: "Issue before AMA Activation from RAMP" },
-          { description: "PTSD denied" }
+          description: "PTSD denied"
         ),
         user: current_user
       )
