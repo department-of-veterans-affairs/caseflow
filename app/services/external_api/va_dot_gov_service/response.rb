b97221299aecb69267b5d0e6ddad7f303c34a927
Andrew Lomax <andrewlomaxr@gmail.com>
2019-07-22 17:39:04 -0400

    add response message

--- app/services/external_api/va_dot_gov_service/response.rb
+++ app/services/external_api/va_dot_gov_service/response.rb
@@ -9,7 +9,9 @@ class ExternalApi::VADotGovService::Response
     @response = api_response
     @code = response.code
     @body = JSON.parse(response.body).symbolize_keys
-    @messages = body[:messages].map { |message| ResponseMessage.new(message) }
+    @messages = body[:messages].map do |message|
+      ExternalApi::VADotGovService::Response::ResponseMessage.new(message)
+    end
   end
 
   def error
