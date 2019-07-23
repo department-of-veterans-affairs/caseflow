109409358304a3fb33b18400ba3ac8477c0dfb83
Kat Tipton <kat@navapbc.com>
2019-07-23 11:32:26 -0400

    Reorder functions

--- app/models/tasks/special_case_movement_task.rb
+++ app/models/tasks/special_case_movement_task.rb
@@ -5,12 +5,12 @@
 #   case distribution
 
 class SpecialCaseMovementTask < GenericTask
-  before_create :verify_parent_task_type, :verify_user_organization
-  after_create :after_create_function
+  before_create :verify_parent_task_type, :verify_user_organization, :verify_appeal_distributable
+  after_create :close_and_create_judge_task
 
   private
 
-  def after_create_function
+  def close_and_create_judge_task
     JudgeAssignTask.create!(appeal: appeal,
       parent: appeal.root_task,
       assigned_to: assigned_to,
@@ -22,11 +22,6 @@ class SpecialCaseMovementTask < GenericTask
     parent.update!(status: Constants.TASK_STATUSES.completed)
   end
 
-  def verify_user_organization
-    return true if assigned_by.organizations.include?(SpecialCasemovementTeam.singleton)
-    false
-  end
-
   def verify_parent_task_type
     # For now, we expect the parent to always be the distribution task.
     #   This may change as we add more 'from' scenarios
@@ -34,5 +29,10 @@ class SpecialCaseMovementTask < GenericTask
       fail(Caseflow::Error::InvalidParentTask,
            message: "Special Case Movement must have a Distribution task parent")
     end
+  end
+
+  def verify_user_organization
+    return true if assigned_by.organizations.include?(SpecialCasemovementTeam.singleton)
+    false
   end
 end
