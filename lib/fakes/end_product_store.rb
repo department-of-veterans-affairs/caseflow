2b1f91e306ed99ba8f558e789452fece6afa62d3
Peter Karman <peter@truss.works>
2019-07-23 13:50:34 -0500

    refactor

--- lib/fakes/end_product_store.rb
+++ lib/fakes/end_product_store.rb
@@ -3,6 +3,54 @@
 class Fakes::EndProductStore
   REDIS_NS ||= "end_product_records_#{Rails.env}"
 
+  class Contention < OpenStruct
+    def initialize(hash)
+      super
+      self.start_date = Time.zone.parse(start_date)
+      self.submit_date = Time.zone.parse(submit_date)
+    end
+  end
+
+  # we call it a "child" because even though Redis has only key:value pairs,
+  # logically the object is a child of an EndProduct
+  class ChildStore < Fakes::EndProductStore
+    def initialize(parent_key:, child_key:, child:)
+      @parent_key = parent_key
+      @child_key = child_key.to_s
+      @child = child
+    end
+
+    def create
+      children = fetch_children || {}
+      children[child_key] = child
+      deflate_and_store(parent_key, children)
+    end
+
+    def update
+      children = fetch_children
+      fail "No values for #{parent_key}" unless children
+
+      children[child_key] = child
+      deflate_and_store(parent_key, children)
+    end
+
+    def remove
+      children = fetch_children
+      fail "No values for #{parent_key}" unless children
+
+      children.delete(child_key)
+      deflate_and_store(key, children)
+    end
+
+    private
+
+    attr_reader :parent_key, :child_key, :child
+
+    def fetch_children
+      fetch_and_inflate(parent_key)
+    end
+  end
+
   def self.cache_store
     @cache_store ||= begin
       redis_conn = Redis.new(url: Rails.application.secrets.redis_url_cache)
@@ -15,8 +63,8 @@ class Fakes::EndProductStore
   end
 
   def clear!
-    self.class.all_keys.each do |k|
-      self.class.cache_store.redis.del(k)
+    self.class.all_keys.each do |key|
+      self.class.cache_store.redis.del(key)
     end
   end
 
@@ -49,48 +97,31 @@ class Fakes::EndProductStore
   # contentions are "children" of End Products but we store by claim_id
   # rather than veteran_id to make look up easier. Dispositions are similar.
   def create_contention(contention)
-    claim_id = contention.claim_id
-    create_ep_child(child: contention, key: contention_key(claim_id))
+    contention_child_store(contention).create
   end
 
   def update_contention(contention)
-    claim_id = contention.claim_id
-    update_ep_child(child: contention, key: contention_key(claim_id))
+    contention_child_store(contention).update
   end
 
   def remove_contention(contention)
-    claim_id = contention.claim_id
-    remove_ep_child(child: contention, key: contention_key(claim_id))
-  end
-
-  def contention_key(claim_id)
-    "contention_#{claim_id}"
+    contention_child_store(contention).remove
   end
 
   def inflated_contentions_for(claim_id)
-    children_to_structs(contention_key(claim_id)).each do |cont|
-      cont.start_date = Time.zone.parse(cont.start_date)
-      cont.submit_date = Time.zone.parse(cont.submit_date)
-    end
+    children_to_structs(contention_key(claim_id)).map { |struct| Contention.new(struct) }
   end
 
   def create_disposition(disposition)
-    claim_id = disposition.claim_id
-    create_ep_child(child: disposition, key: disposition_key(claim_id), id_attr: :contention_id)
+    disposition_child_store(disposition).create
   end
 
   def update_disposition(disposition)
-    claim_id = disposition.claim_id
-    update_ep_child(child: disposition, key: disposition_key(claim_id), id_attr: :contention_id)
+    disposition_child_store(disposition).update
   end
 
   def remove_disposition(disposition)
-    claim_id = disposition.claim_id
-    remove_ep_child(child: disposition, key: disposition_key(claim_id), id_attr: :contention_id)
-  end
-
-  def disposition_key(claim_id)
-    "disposition_#{claim_id}"
+    disposition_child_store(disposition).remove
   end
 
   def inflated_dispositions_for(claim_id)
@@ -99,37 +130,29 @@ class Fakes::EndProductStore
 
   private
 
+  def contention_key(claim_id)
+    "contention_#{claim_id}"
+  end
+
+  def disposition_key(claim_id)
+    "disposition_#{claim_id}"
+  end
+
+  def contention_child_store(contention)
+    claim_id = contention.claim_id
+    ChildStore.new(parent_key: contention_key(claim_id), child: contention, child_key: contention.id)
+  end
+
+  def disposition_child_store(disposition)
+    claim_id = disposition.claim_id
+    ChildStore.new(parent_key: disposition_key(claim_id), child: disposition, child_key: disposition.contention_id)
+  end
+
   def deflate_and_store(key, payload)
     self.class.cache_store.set(key, payload.to_json)
   end
 
   def children_to_structs(key)
-    (children_for(key) || {}).values.map { |hash| OpenStruct.new(hash[:table]) }
-  end
-
-  def children_for(key)
-    fetch_and_inflate(key)
-  end
-
-  def create_ep_child(child:, key:, id_attr: :id)
-    children = children_for(key) || {}
-    children[child[id_attr]] = child
-    deflate_and_store(key, children)
-  end
-
-  def update_ep_child(child:, key:, id_attr: :id)
-    children = children_for(key)
-    fail "No values for #{key}" unless children
-
-    children[child[id_attr].to_s] = child
-    deflate_and_store(key, children)
-  end
-
-  def remove_ep_child(child:, key:, id_attr: :id)
-    children = children_for(key)
-    fail "No values for #{key}" unless children
-
-    children.delete(child[id_attr].to_s)
-    deflate_and_store(key, children)
+    (fetch_and_inflate(key) || {}).values.map { |hash| OpenStruct.new(hash[:table]) }
   end
 end
