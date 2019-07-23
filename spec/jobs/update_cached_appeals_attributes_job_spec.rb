d1256c213c02bd7a0b73fbdf713929ff76e35683
Kevin Moore <hello@kevmo.io>
2019-07-23 15:24:59 -0400

    Spec

--- spec/jobs/update_cached_appeals_attributes_job_spec.rb
+++ spec/jobs/update_cached_appeals_attributes_job_spec.rb
@@ -3,41 +3,97 @@
 require "rails_helper"
 
 describe UpdateCachedAppealsAttributesJob do
-  let!(:vacols_folder1) {
-    VACOLS::Folder.create(ticknum: "1")
-  }
-  let!(:vacols_folder2) {
-    VACOLS::Folder.create(ticknum: "2")
-  }
+  # 
+  # let!(:vacols_case1) { create(:case) }
+  # let!(:vacols_case2) { create(:case) }
 
-  let!(:legacy_appeal1) { LegacyAppeal.create(vacols_id: "1") }
-  let!(:appeal2) { LegacyAppeal.create(vacols_id: "2") }
+  # let!(:legacy_appeal1) { FactoryBot.create(:legacy_appeal, vacols_case: vacols_case1) }
+  # let!(:legacy_appeal2) { FactoryBot.create(:legacy_appeal, vacols_case: vacols_case2) }
 
-  context "When there are open legacy and AMA appeals, the cache" do
+  let!(:appeal1) { FactoryBot.create(:appeal) }
+  let!(:appeal2) { FactoryBot.create(:appeal) }
+  let!(:appeal3) { FactoryBot.create(:appeal) }
 
-    it "creates cached appeals for all appeals associated with open tasks" do
-    end
+  let!(:ama_appeals) { [appeal1, appeal2, appeal3] }
 
-    if "associates the correct docket_number with the correct vacols_id" do
-    end
 
-    it "creates the proper docket numbers for AMA appeals" do
-    end
+  context "when the job runs successfully" do
+    # let!(:legacy_appeals) { [legacy_appeal1, legacy_appeal2] }
+
+      # legacy_appeals.each do |appeal|
+      #   FactoryBot.create(:bva_dispatch_task, :in_progress, appeal: appeal, appeal_type: LegacyAppeal.name)
+      #   FactoryBot.create(:ama_judge_task, :completed, appeal: appeal, appeal_type: LegacyAppeal.name)
+      #   FactoryBot.create(:ama_attorney_task, :in_progress, appeal: appeal, appeal_type: LegacyAppeal.name)
+      # end
+
+      # ama_appeals.each do |appeal|
+      #   FactoryBot.create(:bva_dispatch_task, :in_progress, appeal: appeal, appeal_type: Appeal.name)
+      #   FactoryBot.create(:ama_judge_task, :completed, appeal: appeal, appeal_type: Appeal.name)
+      #   FactoryBot.create(:ama_attorney_task, :in_progress, appeal: appeal, appeal_type: Appeal.name)
+      # end
+      #
+      # UpdateCachedAppealsAttributesJob.perform_now
+
 
     it "creates the correct number of cached appeals" do
+
+      ama_appeals.each do |appeal|
+        FactoryBot.create(:bva_dispatch_task, :in_progress, appeal: appeal, appeal_type: Appeal.name)
+        FactoryBot.create(:ama_judge_task, :completed, appeal: appeal, appeal_type: Appeal.name)
+        FactoryBot.create(:ama_attorney_task, :in_progress, appeal: appeal, appeal_type: Appeal.name)
+      end
+
+      UpdateCachedAppealsAttributesJob.perform_now
+
+      expect(Task.all.count).to eq(12)
+      expect(Task.open.count).to eq(9)
+
+      expect(CachedAppeal.all.count).to eq(3)
+
     end
-
-    it "does not create more than 1 cached appeal for each appeal" do
-    end
-
-    it "caches the docket_types correctly" do
-    end
-
-    it "fails gracefully" do
-    end 
-
 
   end
 
-
 end
+
+
+
+# describe UpdateCachedAppealsAttributesJob do
+#   let(:vacols_folder1) {
+#     VACOLS::Folder.create(ticknum: "1")
+#   }
+#   let(:vacols_folder2) {
+#     VACOLS::Folder.create(ticknum: "2")
+#   }
+#
+#   let(:legacy_appeal1) { LegacyAppeal.create(vacols_id: "1") }
+#   let(:appeal2) { LegacyAppeal.create(vacols_id: "2") }
+#
+#   context "When there are open legacy and AMA appeals, the cache" do
+#
+#     it "creates cached appeals for all appeals associated with open tasks" do
+#     end
+#
+#     if "associates the correct docket_number with the correct vacols_id" do
+#     end
+#
+#     it "creates the proper docket numbers for AMA appeals" do
+#     end
+#
+#     it "creates the correct number of cached appeals" do
+#     end
+#
+#     it "does not create more than 1 cached appeal for each appeal" do
+#     end
+#
+#     it "caches the docket_types correctly" do
+#     end
+#
+#     it "fails gracefully" do
+#     end
+#
+#
+#   end
+#
+#
+# end
