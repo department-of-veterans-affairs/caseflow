288129114c74cc715046cfd37cb4a83df6f7ceae
Andrew Lomax <andrewlomaxr@gmail.com>
2019-07-24 17:08:00 -0400

    add service tests

--- spec/services/external_api/va_dot_gov_service_spec.rb
+++ spec/services/external_api/va_dot_gov_service_spec.rb
@@ -1,15 +1,46 @@
 # frozen_string_literal: true
 
 describe ExternalApi::VADotGovService do
-  describe "#get_facility_data" do
-    before do
-      stub_const("VADotGovService", Fakes::VADotGovService)
+  before(:each) do
+    stub_const("VADotGovService", Fakes::VADotGovService)
+  end
+
+  describe "#validate_address" do
+    it "returns validated address" do
+      result = VADotGovService.validate_address(
+        address_line1: "fake address",
+        address_line2: "fake address",
+        address_line3: "fake address",
+        city: "City",
+        state: "State",
+        zip_code: "Zip",
+        country: "US"
+      )
+
+      expect(result[:error]).to be_nil
+      expect(result[:valid_address]).to_not be_nil
     end
+  end
 
+  describe "#get_distance" do
+    it "returns distance to facilities" do
+      result = VADotGovService.get_distance(
+        ids: %w[vha_539 vha_757 vha_539],
+        lat: 0.0,
+        long: 0.0
+      )
+
+      expect(result[:facilities].pluck(:facility_id)).to eq(%w[vha_539 vha_757 vha_539])
+      expect(result[:error]).to be_nil
+    end
+  end
+
+  describe "#get_facility_data" do
     it "returns facility data" do
-      results = VADotGovService.get_facility_data(ids: %w[vha_539 vha_757 vha_539])
+      result = VADotGovService.get_facility_data(ids: %w[vha_539 vha_757 vha_539])
 
-      expect(results.pluck(:facility_id)).to eq(%w[vha_539 vha_757 vha_539])
+      expect(result[:facilities].pluck(:facility_id)).to eq(%w[vha_539 vha_757 vha_539])
+      expect(result[:error]).to be_nil
     end
   end
 end
