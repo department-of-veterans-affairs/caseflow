de6b207bccca3e8bb6d5cd925de8530a929ede0f
Peter Karman <peter@truss.works>
2019-07-24 15:12:42 -0500

    add claimants.participant_id index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190724200447) do
+ActiveRecord::Schema.define(version: 20190724201133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -235,6 +235,7 @@ ActiveRecord::Schema.define(version: 20190724200447) do
     t.string "participant_id", null: false, comment: "The participant ID of the claimant."
     t.string "payee_code", comment: "The payee_code for the claimant, if applicable. payee_code is required when the claim is processed in VBMS."
     t.index ["decision_review_type", "decision_review_id"], name: "index_claimants_on_decision_review_type_and_decision_review_id"
+    t.index ["participant_id"], name: "index_claimants_on_participant_id"
   end
 
   create_table "claims_folder_searches", id: :serial, force: :cascade do |t|
