e86c59687fa7a8e6f2d195787a960a011220fb8f
Peter Karman <peter@truss.works>
2019-07-24 14:55:18 -0500

    pull person attributes from bgs

---
+++ app/models/concerns/bgs_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module BgsService
+  extend ActiveSupport::Concern
+
+  # both class and instance method
+  class_methods do
+    def bgs
+      BGSService.new
+    end
+  end
+
+  def bgs
+    BGSService.new
+  end
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Person < ApplicationRecord
+  include BgsService
+
   has_many :advance_on_docket_motions
   has_many :claimants, primary_key: :participant_id, foreign_key: :participant_id
   validates :participant_id, presence: true
@@ -9,16 +11,45 @@ class Person < ApplicationRecord
     advanced_on_docket_based_on_age || advanced_on_docket_motion_granted(appeal_receipt_date)
   end
 
-  # If we do not yet have the date of birth saved in Caseflow's DB, then
-  # we want to fetch it from BGS, save it to the DB, then return it
   def date_of_birth
-    super || begin
-      update(date_of_birth: BGSService.new.fetch_person_info(participant_id)[:birth_date]) if persisted?
-      super
-    end
+    cached_or_fetched_from_bgs(attr_name: :date_of_birth, bgs_attr: :birth_date)
+  end
+
+  def first_name
+    cached_or_fetched_from_bgs(attr_name: :first_name)
+  end
+
+  def last_name
+    cached_or_fetched_from_bgs(attr_name: :last_name)
+  end
+
+  def middle_name
+    cached_or_fetched_from_bgs(attr_name: :middle_name)
+  end
+
+  def name_suffix
+    cached_or_fetched_from_bgs(attr_name: :name_suffix)
+  end
+
+  def name
+    FullName.new(first_name, "", last_name)
   end
 
   private
+
+  def cached_or_fetched_from_bgs(attr_name:, bgs_attr: nil)
+    bgs_attr ||= attr_name
+    self[attr_name] || begin
+      return unless bgs_person
+
+      update!(attr_name: bgs_person[bgs_attr]) if persisted?
+      self[attr_name]
+    end
+  end
+
+  def bgs_person
+    @bgs_person ||= bgs.fetch_person_info(participant_id)
+  end
 
   def advanced_on_docket_based_on_age
     date_of_birth && date_of_birth < 75.years.ago
