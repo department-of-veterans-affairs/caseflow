20e7719597ee968b1689b95f8cbb27a929eddebd
Kat Tipton <kat@navapbc.com>
2019-07-24 10:11:46 -0400

    moar codeclime

--- spec/models/tasks/special_case_movement_spec.rb
+++ spec/models/tasks/special_case_movement_spec.rb
@@ -5,18 +5,17 @@ require "rails_helper"
 describe SpecialCaseMovementTask do
   describe ".create" do
     context "with Special Case Movement Team user" do
+      let(:scm_user) { FactoryBot.create(:user) }
       before do
-        let (:scm_user) { FactoryBot.create(:user) }
         OrganizationsUser.add_user_to_organization(scm_user,
                                                    SpecialCaseMovementTeam.singleton)
       end
 
       context "appeal ready for distribution" do
-        before do
-          let (:appeal) { FactoryBot.create(:appeal,
-                                            :with_post_intake_tasks,
-                                            docket_type: "direct_review")
-          }
+        let(:appeal) do
+          FactoryBot.create(:appeal,
+                            :with_post_intake_tasks,
+                            docket_type: "direct_review")
         end
         context "with no blocking tasks" do
           it "should create the SCM task and JudgeAssign task" do
@@ -34,11 +33,9 @@ describe SpecialCaseMovementTask do
         end
 
         context "with blocking mail task" do
-          before do
-            FactoryBot.create(:congressional_interest_mail_task,
-                              appeal: appeal,
-                              parent: appeal.root_task)
-          end
+          FactoryBot.create(:congressional_interest_mail_task,
+                            appeal: appeal,
+                            parent: appeal.root_task)
           it "should error with appeal not ready" do
             expect do
               SpecialCaseMovementTask.create(appeal: appeal,
@@ -50,11 +47,10 @@ describe SpecialCaseMovementTask do
       end
 
       context "appeal at the evidence window state" do
-        before do
-          let (:appeal) { FactoryBot.create(:appeal,
-                                            :with_post_intake_tasks,
-                                            docket_type: "evidence_submission")
-          }
+        let(:appeal) do
+          FactoryBot.create(:appeal,
+                            :with_post_intake_tasks,
+                            docket_type: "evidence_submission")
         end
         context "with distribution task on_hold" do
           it "should error with appeal not ready" do
@@ -80,12 +76,11 @@ describe SpecialCaseMovementTask do
     end
 
     context "with regular user" do
-      before do
-        let (:user) { FactoryBot.create(:user) }
-        let (:appeal) { FactoryBot.create(:appeal,
-                                          :with_post_intake_tasks,
-                                          docket_type: "direct_review")
-        }
+      let(:user) { FactoryBot.create(:user) }
+      let(:appeal) do
+        FactoryBot.create(:appeal,
+                          :with_post_intake_tasks,
+                          docket_type: "direct_review")
       end
 
       it "should error with user error" do
