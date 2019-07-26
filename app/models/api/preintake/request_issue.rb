a3b5255e1caf1c4c434d403d81d5dfae1c1ac086
Ed Mangimelli <edmangimelli@gmail.com>
2019-07-26 01:12:48 -0500

    WIP

--- app/models/api/preintake/request_issue.rb
+++ app/models/api/preintake/request_issue.rb
@@ -3,24 +3,35 @@
 class Api::Preintake::RequestIssue
   include Api::Validation
 
-  attr_reader :notes, :decision_id, :request_issue_ids, :rating_id, :legacy_id, :benefit_type, :category, :decision_date, :decision_text
+  attr_reader(
+    :notes,
+    :decision_id,
+    :request_issue_ids,
+    :rating_id,
+    :legacy_id,
+    :benefit_type,
+    :category,
+    :decision_date,
+    :decision_text
+  )
 
   def initialize(options)
     contests = options["contests"]
     options = options.except "contests"
     case contests
-    when "on_file_decision"; validate_for_contesting_decision options
-    when "on_file_rating"; validate_for_contesting_rating options
-    when "on_file_legacy_issue"; validate_for_contesting_legacy_issue options
-    when "other"; validate_for_contesting_other options
+    when "on_file_decision" then validate_for_contesting_decision options
+    when "on_file_rating" then validate_for_contesting_rating options
+    when "on_file_legacy_issue" then validate_for_contesting_legacy_issue options
+    when "other" then validate_for_contesting_other options
     else
-      raise ArgumentError, "cannot contest: <#{contests}>" if exception
-    end 
+      fail ArgumentError, "cannot contest: <#{contests}>" if exception
+    end
   end
 
   # used for the "complete" step in the intake form process
   # nils used instead of leaving keys out to be as explicit as possible
   # TODO: where do request_issue_ids go?
+  # rubocop:disable Metrics/MethodLength
   def complete_hash
     identified = benefit_type && category
     {
@@ -41,22 +52,23 @@ class Api::Preintake::RequestIssue
       ineligible_reason: nil,
       ineligible_due_to_id: nil,
       edited_description: nil,
-      correction_type: nil,
+      correction_type: nil
     }
   end
+  # rubocop:enable Metrics/MethodLength
 
   private
 
-  def validate_notes notes
+  def validate_notes(notes)
     is_string? notes, key: :notes
   end
 
-  def validate_id id, key:
+  def validate_id(id, key:)
     is_int_or_int_string? id, key: key
     to_int id
   end
 
-  def validate_for_contesting_decision options
+  def validate_for_contesting_decision(options)
     options = options.except "contests"
 
     # required keys
@@ -77,7 +89,7 @@ class Api::Preintake::RequestIssue
     end
   end
 
-  def validate_for_contesting_rating options
+  def validate_for_contesting_rating(options)
     options = options.except "contests"
 
     these_are_the_hash_keys? options, keys: %w[notes rating_id]
@@ -89,7 +101,7 @@ class Api::Preintake::RequestIssue
     @rating_id = validate_id rating_id, key: :rating_id
   end
 
-  def validate_for_contesting_legacy_issue options
+  def validate_for_contesting_legacy_issue(options)
     options = options.except "contests"
 
     these_are_the_hash_keys? options, keys: %w[notes legacy_id legacy_opt_in_approved]
@@ -103,17 +115,17 @@ class Api::Preintake::RequestIssue
     @legacy_id = validate_id legacy_id, key: :legacy_id
   end
 
-  def validate_for_contesting_other options
+  def validate_for_contesting_other(options)
     options = options.except "contests"
-    
+
     # required keys
     hash_has_at_least_these_keys? options, keys: %w[notes]
     # possible keys
     hash_keys_are_within_this_set? options, keys: %w[notes benefit_type category decision_date decision_text]
 
     notes, benefit_type, category, decision_date, decision_text = options.values_at(
-                                           "notes", "benefit_type", "category", "decision_date", "decision_text"
-                                        )
+      "notes", "benefit_type", "category", "decision_date", "decision_text"
+    )
     @notes = validate_notes notes
 
     is_null_or_benefit_type? @benefit_type = benefit_type, key: :benefit_type
