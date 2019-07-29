c0a9d0def914d0ca5c1c352e1e59973f4a5c5f41
Peter Karman <peter@truss.works>
2019-07-29 12:40:52 -0500

    override documented private method to avoid confusing nil and blank

--- config/initializers/activerecord_types.rb
+++ config/initializers/activerecord_types.rb
@@ -4,7 +4,9 @@ require "stringex/unidecoder"
 require "stringex/core_ext"
 
 class AsciiString < ActiveRecord::Type::String
-  def cast(value)
+  private
+
+  def cast_value(value)
     limit ? value.to_s.to_ascii[0, limit-1] : value.to_s.to_ascii
   end
 end
