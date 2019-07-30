34590a5ab42cc31a27eeefbac8fb4298dbb46709
Kat Tipton <kat@navapbc.com>
2019-07-30 12:20:08 -0400

    Defensively cancel TaskTimers with closed parents during job runs (#11591)
    Bumps #11442 
    
    ### Description
    We now cancel TaskTimers on Tasks that are cancelled through a callback (#11581).  
    
    This additionally adds a defensive cancel if we find TaskTimers with closed parents when the job runs.
    
    This should also close any outstanding TaskTimers that were orphaned before #11581 was released on it's first run post release. Confirming that happens properly is the last step in #11442 before it can be marked completed.

--- app/models/task_timer.rb
+++ app/models/task_timer.rb
@@ -14,7 +14,15 @@ class TaskTimer < ApplicationRecord
     with_active_tasks.processable.attemptable.unexpired.order_by_oldest_submitted
   end
 
+  def self.requires_cancelling
+    with_closed_tasks.processable.order_by_oldest_submitted
+  end
+
   def self.with_active_tasks
     includes(:task).where.not(tasks: { status: Task.closed_statuses })
+  end
+
+  def self.with_closed_tasks
+    includes(:task).where(tasks: { status: Task.closed_statuses })
   end
 end
