bdfa60ca3f08ba34154be1c7da151a45e7e331b1
Ed Mangimelli <edmangimelli@gmail.com>
2019-07-31 07:55:34 -0500

    WIP

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -24,8 +24,36 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < ActionController::
 
     render json: intake_status(higher_level_review), status: :accepted
   rescue StandardError => error
-    error_hash = Api::V3::HigherLevelReviewProcessor.error_hash_from_error_code(error.try(:error_code))
-    render json: { errors: [error_hash] }, status: error_hash[:status]
+    status, title = (
+      case (processor.intake.try(:error_code) || error.try(:error_code)).to_s
+      when "invalid_file_number"
+        [422, "Veteran ID not found"]
+      when "veteran_not_found"
+        [404, "Veteran not found"]
+      when "veteran_has_multiple_phone_numbers"
+        [422, "The Veteran has multiple active phone numbers"]
+      when "veteran_not_accessible"
+        [403, "You don't have permission to view this Veteran's information"]
+      when "veteran_not_modifiable"
+        [422, "You don't have permission to intake this Veteran"]
+      when "veteran_not_valid"
+        [422, "The Veteran's profile has missing or invalid information required to create an EP."]
+      when "duplicate_intake_in_progress"
+        [409, "Intake In Progress"]
+      when "reserved_veteran_file_number"
+        [422, "Invalid veteran file number"]
+      when "incident_flash"
+        [422, "The veteran has an incident flash"]
+      else; [nil, nil]
+      end
+    )
+    unless status || title
+      status = 422
+      title = "Unknown error"
+      code = "unknown_error"
+    end
+
+    render json: { errors: [{ status: status, title: title, code: code }] }, status: status
   end
 
   def mock_create
