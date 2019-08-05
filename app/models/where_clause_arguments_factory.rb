97e5945cc6a6919fc057253fdbf61abba67073ff
lowellrex <lowell@navahq.com>
2019-08-05 09:58:43 -0400

    Actually make this work

--- app/models/where_clause_arguments_factory.rb
+++ app/models/where_clause_arguments_factory.rb
@@ -4,7 +4,7 @@ class WhereClauseArgumentsFactory
   # ["col=docketNumberColumn&val=legacy,evidence_submission", "col=taskColumn&val=TranslationTask"]
   # ->
   # [
-  #   "cached_appeals_attributes.docket_type IN ? AND tasks.type in ?",
+  #   "cached_appeals_attributes.docket_type IN (?) AND tasks.type in (?)",
   #   ["legacy", "evidence_submission"],
   #   ["TranslationTask"]
   # ]
@@ -13,7 +13,7 @@ class WhereClauseArgumentsFactory
 
     filters = filter_params.map { |filter_string| FilterParameter.from_string(filter_string) }
 
-    where_string = filters.map { |filter| "#{filter.column} IN ?" }.join(" AND ")
+    where_string = filters.map { |filter| "#{filter.column} IN (?)" }.join(" AND ")
     where_arguments = filters.map(&:values)
 
     [where_string] + where_arguments
