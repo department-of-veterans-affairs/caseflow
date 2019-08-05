3cbc0064c6de6a8c10e7914bdc813300cc590ad7
Kevin Moore <k.james.moore@gmail.com>
2019-08-05 10:59:05 -0400

    Linting

--- app/models/task_pager.rb
+++ app/models/task_pager.rb
@@ -39,8 +39,6 @@ class TaskPager
     when Constants.QUEUE_CONFIG.DOCKET_NUMBER_COLUMN
       tasks_sorted_by_docket_number(tasks)
 
-      # tasks_with_cached_appeal_attributes(tasks).order("cached_appeal_attributes.docket_number #{sort_order}")
-
     # Columns not yet supported:
     #
     # APPEAL_TYPE_COLUMN
