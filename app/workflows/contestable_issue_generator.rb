f5fa3fc05f6e6dd7eae009ff9aa4c1f2d1ad71da
aroltsch <aroltsch@tistatech.com>
2019-08-07 13:41:37 -0400

    More renaming

--- app/workflows/contestable_issue_generator.rb
+++ app/workflows/contestable_issue_generator.rb
@@ -22,12 +22,6 @@ class ContestableIssueGenerator
     from_ratings.reject { |contestable_issue| decision_issue_duplicate_exists?(contestable_issue) }
   end
 
-  def decision_issue_duplicate_exists?(contestable_issue)
-    from_decision_issues.any? do |potential_duplicate|
-      contestable_issue.rating_issue_reference_id == potential_duplicate.rating_issue_reference_id
-    end
-  end
-
   def contestable_decision_issues
     from_decision_issues.reject { |contestable_issue| contestable_issue.decision_issue&.voided? }
   end
@@ -35,7 +29,7 @@ class ContestableIssueGenerator
   def from_ratings
     return [] unless receipt_date
 
-    cached_rating_issues
+    rating_issues_for_review
       .select { |issue| issue.profile_date && issue.profile_date.to_date < receipt_date }
       .map { |rating_issue| ContestableIssue.from_rating_issue(rating_issue, review) }
   end
@@ -46,9 +40,15 @@ class ContestableIssueGenerator
     end
   end
 
-  def cached_rating_issues
+  def rating_issues_for_review
     review.cached_serialized_ratings.inject([]) do |result, rating_hash|
       result + rating_hash[:issues].map { |rating_issue_hash| RatingIssue.deserialize(rating_issue_hash) }
+    end
+  end
+
+  def decision_issue_duplicate_exists?(contestable_issue)
+    from_decision_issues.any? do |potential_duplicate|
+      contestable_issue.rating_issue_reference_id == potential_duplicate.rating_issue_reference_id
     end
   end
 end
