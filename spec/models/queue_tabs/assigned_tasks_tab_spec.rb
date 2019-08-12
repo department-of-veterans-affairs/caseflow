0883c87539fe27059ff5cd6e754444010f64ef01
lowellrex <lowell@navahq.com>
2019-08-12 13:19:17 -0400

    Add first tasks test

--- spec/models/queue_tabs/assigned_tasks_tab_spec.rb
+++ spec/models/queue_tabs/assigned_tasks_tab_spec.rb
@@ -7,10 +7,11 @@ describe AssignedTasksTab, :postgres do
   let(:tab) { AssignedTasksTab.new(params) }
   let!(:params) do
     {
-      assignee: create(:organization),
+      assignee: assignee,
       show_regional_office_column: show_regional_office_column
     }
   end
+  let(:assignee) { create(:organization) }
   let(:show_regional_office_column) { false }
 
   describe ".columns" do
@@ -33,6 +34,27 @@ describe AssignedTasksTab, :postgres do
 
       it "includes the regional office column" do
         expect(subject).to include(Constants.QUEUE_CONFIG.REGIONAL_OFFICE_COLUMN)
+      end
+    end
+  end
+
+  describe ".tasks" do
+    subject { tab.tasks }
+
+    context "when there are tasks assigned to the assignee and other folks" do
+      let!(:other_folks_tasks) { create_list(:generic_task, 11) }
+      let!(:assignee_active_tasks) { create_list(:generic_task, 4, :assigned, assigned_to: assignee) }
+      let!(:assignee_on_hold_tasks) { create_list(:generic_task, 3, :assigned, assigned_to: assignee) }
+      let!(:on_hold_tasks_children) do
+        assignee_on_hold_tasks.map do |task|
+          create_list(:generic_task, 2, parent_id: task.id)
+          task.update!(status: Constants.TASK_STATUSES.on_hold)
+          task.children
+        end.flatten
+      end
+
+      it "only returns the tasks that are children of the assignee's on hold tasks" do
+        expect(subject).to match_array(on_hold_tasks_children)
       end
     end
   end
