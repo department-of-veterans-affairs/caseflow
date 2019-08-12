282c26a3306673837b8da7dc6e761752b92b9757
Kat Tipton <kat@navapbc.com>
2019-08-12 15:59:10 -0400

    Single VACOLs calls for AOD and case status

--- app/jobs/update_cached_appeals_attributes_job.rb
+++ app/jobs/update_cached_appeals_attributes_job.rb
@@ -25,23 +25,30 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
     appeals = Appeal.find(Task.open.where(appeal_type: Appeal.name).pluck(:appeal_id).uniq)
     request_issues_to_cache = request_issue_counts_for_appeal_ids(appeals.pluck(:id))
     veteran_names_to_cache = veteran_names_for_file_numbers(appeals.pluck(:veteran_file_number))
+    appeal_aod_status = aod_status_for_appeals(appeals)
 
     appeals_to_cache = appeals.map do |appeal|
       regional_office = RegionalOffice::CITIES[appeal.closest_regional_office]
       {
         appeal_id: appeal.id,
+        appeal_type: Appeal.name,
+        case_type: appeal.type,
+        closest_regional_office_city: regional_office ? regional_office[:city] : COPY::UNKNOWN_REGIONAL_OFFICE,
+        issue_count: request_issues_to_cache[appeal.id] || 0,
         docket_type: appeal.docket_type,
         docket_number: appeal.docket_number,
-        appeal_type: Appeal.name,
-        closest_regional_office_city: regional_office ? regional_office[:city] : COPY::UNKNOWN_REGIONAL_OFFICE,
-        case_type: Appeal.type,
-        is_aod: Appeal.aod
-        issue_count: request_issues_to_cache[appeal.id] || 0,
-        veteran_name: veteran_names_to_cache[appeal.veteran_file_number]
+        is_aod: appeal_aod_status[appeal.id]
+        veteran_name: veteran_names_to_cache[appeal.veteran_file_number],
       }
     end
 
-    update_columns = [:closest_regional_office_city, :issue_count, :docket_type, :docket_number, :veteran_name, :case_type, :is_aod]
+    update_columns = [:case_type,
+                      :closest_regional_office_city,
+                      :docket_type,
+                      :docket_number,
+                      :is_aod,
+                      :issue_count,
+                      :veteran_name]
     CachedAppeal.import appeals_to_cache, on_duplicate_key_update: { conflict_target: [:appeal_id, :appeal_type],
                                                                      columns: update_columns }
 
@@ -61,11 +68,11 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
     values_to_cache = legacy_appeals.map do |appeal|
       regional_office = RegionalOffice::CITIES[appeal.closest_regional_office]
       {
+        vacols_id: appeal.vacols_id,
         appeal_id: appeal.id,
         appeal_type: LegacyAppeal.name,
-        vacols_id: appeal.vacols_id,
-        docket_type: appeal.docket_name, # "legacy"
         closest_regional_office_city: regional_office ? regional_office[:city] : COPY::UNKNOWN_REGIONAL_OFFICE
+        docket_type: appeal.docket_name, # "legacy"
       }
     end
 
@@ -82,32 +89,27 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
       vacols_folders = VACOLS::Folder.where(ticknum: vacols_ids).pluck(:ticknum, :tinum, :ticorkey)
       issue_counts_to_cache = issues_counts_for_vacols_folders(vacols_ids)
       veteran_names_to_cache = veteran_names_for_correspondent_ids(vacols_folders.map { |folder| folder[2] })
+      aod_status_to_cache = aod_status_for_vacols_id(vacols_folders.map { |folder| folder[0] })
+      case_status_to_cache = case_status_for_vacols_id(vacols_folders.map { |folder| folder[0] })
 
       values_to_cache = vacols_folders.map do |vacols_folder|
         {
           vacols_id: vacols_folder[0],
+          case_type: case_status_to_cache[vacols_folder[0]],
           docket_number: vacols_folder[1],
           issue_count: issue_counts_to_cache[vacols_folder[0]] || 0,
-          veteran_name: veteran_names_to_cache[vacols_folder[2]]
+          is_aod: aod_status_to_cache[vacols_folder[0]],
+          veteran_name: veteran_names_to_cache[vacols_folder[2]],
         }
       end
-      values_to_cache.each do |value|
-        bfac = VACOLS::Case.where(bfkey: value[:vacols_id]).pluck(:bfac)
-        value[:case_type] = VACOLS::Case::BFAC_TYPE_CACHE_KEY[bfac[0]]
-        aod = VACOLS::Case.joins(VACOLS::Case::JOIN_AOD)
-          .where(bfkey: value[:vacols_id])
-          .pluck(:aod)
-        value[:is_aod] = !!aod[0]
-      end
 
+      update_columns = [:docket_number,
+                        :issue_count,
+                        :veteran_name,
+                        :case_type,
+                        :is_aod]
       CachedAppeal.import values_to_cache, on_duplicate_key_update: { conflict_target: [:vacols_id],
-                                                                      columns: [
-                                                                        :docket_number,
-                                                                        :issue_count,
-                                                                        :veteran_name,
-                                                                        :case_type,
-                                                                        :is_aod
-                                                                      ]}
+                                                                     columns: update_columns }
     end
   end
 
@@ -149,26 +151,46 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
 
   private
 
-  def request_issue_counts_for_appeal_ids(appeal_ids)
-    RequestIssue.where(decision_review_id: appeal_ids, decision_review_type: Appeal.name)
-      .group(:decision_review_id).count
+  def aod_status_for_appeals(appeals)
+    Appeal.where(id: appeals).map do |appeal|
+      [appeal.id, appeal.advanced_on_docket]
+    end.to_h
+  end
+
+  def aod_status_for_vacols_id(vacols_ids)
+    aods = VACOLS::Case.joins(VACOLS::Case::JOIN_AOD).where(bfkey: vacols_ids).pluck(:aod).map do |aod|
+      aod == 1 ? true : false
+    end
+    vacols_ids.zip(aods).to_h
+  end
+
+  def case_status_for_vacols_id(vacols_ids)
+    statuses = VACOLS::Case.where(bfkey: vacols_ids).pluck(:bfac).map do |value|
+      VACOLS::Case::BFAC_TYPE_CACHE_KEY[value]
+    end
+    vacols_ids.zip(statuses).to_h
   end
 
   def issues_counts_for_vacols_folders(vacols_ids)
     VACOLS::CaseIssue.where(isskey: vacols_ids).group(:isskey).count
   end
 
-  def veteran_names_for_file_numbers(veteran_file_numbers)
-    Veteran.where(file_number: veteran_file_numbers).map do |veteran|
-      # Matches how last names are split and sorted on the front end (see: TaskTable.detailsColumn.getSortValue)
-      [veteran.file_number, "#{veteran.last_name.split(' ').last}, #{veteran.first_name}"]
-    end.to_h
+  def request_issue_counts_for_appeal_ids(appeal_ids)
+    RequestIssue.where(decision_review_id: appeal_ids, decision_review_type: Appeal.name)
+      .group(:decision_review_id).count
   end
 
   def veteran_names_for_correspondent_ids(correspondent_ids)
     # folders is an array of [ticknum, tinum, ticorkey] for each folder
     VACOLS::Correspondent.where(stafkey: correspondent_ids).map do |corr|
       [corr.stafkey, "#{corr.snamel.split(' ').last}, #{corr.snamef}"]
+    end.to_h
+  end
+
+  def veteran_names_for_file_numbers(veteran_file_numbers)
+    Veteran.where(file_number: veteran_file_numbers).map do |veteran|
+      # Matches how last names are split and sorted on the front end (see: TaskTable.detailsColumn.getSortValue)
+      [veteran.file_number, "#{veteran.last_name.split(' ').last}, #{veteran.first_name}"]
     end.to_h
   end
 end
