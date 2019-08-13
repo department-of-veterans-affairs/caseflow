adad94309cad2027f048ac2a7eee4da0bf7e3aff
Moncef Belyamani <monfresh@users.noreply.github.com>
2019-08-13 14:50:10 -0400

    Add DB cleanup to tests that need them (#11759)

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :task do
     assigned_at { rand(30..35).days.ago }
-    assigned_by { create(:user) }
-    assigned_to { create(:user) }
+    association :assigned_by, factory: :user
+    association :assigned_to, factory: :user
     appeal { create(:legacy_appeal, vacols_case: create(:case)) }
     action { nil }
     type { Task.name }
