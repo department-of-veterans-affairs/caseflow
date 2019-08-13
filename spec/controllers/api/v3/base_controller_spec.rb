b71aa636c5a9524399d03060dcb20e86a4a822b9
Philip Becker <philip.becker@oddball.io>
2019-08-13 14:17:51 -0400

    remove unnecessary requires

--- spec/controllers/api/v3/base_controller_spec.rb
+++ spec/controllers/api/v3/base_controller_spec.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "support/vacols_database_cleaner"
-require "rails_helper"
-
 RSpec.describe Api::V3::BaseController, type: :controller do
   describe "#api_released used in before_action" do
     controller do
