857b64fd179b0d8725b2b9c1d2249c5ded1852fa
Philip Becker <philip.becker@oddball.io>
2019-08-13 14:26:00 -0400

    add an api_key for the test

--- spec/requests/api/v3/higher_level_reviews_controller_spec.rb
+++ spec/requests/api/v3/higher_level_reviews_controller_spec.rb
@@ -1,23 +1,28 @@
 # frozen_string_literal: true
 
-describe Api::V3::DecisionReview::HigherLevelReviewsController, type: :request do
+require "support/database_cleaner"
+
+describe Api::V3::DecisionReview::HigherLevelReviewsController, :postgres, type: :request do
   before do
     FeatureToggle.enable!(:api_v3)
   end
+
+  let!(:api_key) { ApiKey.create!(consumer_name: "ApiV3 Test Consumer").key_string }
+
   describe "#create" do
     it "should return a 202 on success" do
-      post "/api/v3/decision_review/higher_level_reviews"
+      post "/api/v3/decision_review/higher_level_reviews", headers: { "Authorization" => "Token #{api_key}" }
       expect(response).to have_http_status(202)
     end
     it "should be a jsonapi IntakeStatus response" do
-      post "/api/v3/decision_review/higher_level_reviews"
+      post "/api/v3/decision_review/higher_level_reviews", headers: { "Authorization" => "Token #{api_key}" }
       json = JSON.parse(response.body)
       expect(json["data"].keys).to include("id", "type", "attributes")
       expect(json["data"]["type"]).to eq "IntakeStatus"
       expect(json["data"]["attributes"]["status"]).to be_a String
     end
     it "should include a Content-Location header" do
-      post "/api/v3/decision_review/higher_level_reviews"
+      post "/api/v3/decision_review/higher_level_reviews", headers: { "Authorization" => "Token #{api_key}" }
       expect(response.headers.keys).to include("Content-Location")
       expect(response.headers["Content-Location"]).to match "/api/v3/decision_review/higher_level_reviews/intake_status"
     end
