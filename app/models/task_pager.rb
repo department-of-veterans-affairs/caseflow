de18219b766190913ad2c6f66acbe18e96373fc2
Kat Tipton <kat@navapbc.com>
2019-08-15 14:46:30 -0400

    defensively specify User assigned tasks

--- app/models/task_pager.rb
+++ app/models/task_pager.rb
@@ -67,7 +67,8 @@ class TaskPager
   # rubocop:enable Metrics/CyclomaticComplexity
 
   def tasks_sorted_by_assignee_name(tasks)
-    tasks.joins("join users on tasks.assigned_to_id = users.id").order("users.full_name #{sort_order}")
+    tasks.joins("left join users on tasks.assigned_to_id = users.id and tasks.assigned_to_type = '#{User.name}'")
+         .order("users.full_name #{sort_order}")
   end
 
   def tasks_sorted_by_docket_number(tasks)
