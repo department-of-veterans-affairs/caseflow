08ef0e7c1b197423081954fc9faf9270d9862bd9
Peter Karman <peter@truss.works>
2019-08-16 14:51:04 -0500

    refactor

---
+++ app/queries/vacols_location_batch_updater.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class VacolsLocationBatchUpdater
+  def initialize(location:, vacols_ids:, user_id:)
+    @location = location
+    @vacols_ids = vacols_ids
+    @user_id = (user_id || "DSUSER").upcase
+  end
+
+  def call
+    conn = connection
+
+    MetricsService.record("VACOLS: batch_update_vacols_location",
+                          service: :vacols,
+                          name: "batch_update_vacols_location") do
+      conn.transaction do
+        conn.execute(sanitize_sql_array([<<-SQL, location, vacols_ids]))
+          update BRIEFF
+          set BFDLOCIN = SYSDATE,
+              BFCURLOC = ?,
+              BFDLOOUT = SYSDATE,
+              BFORGTIC = NULL
+          where BFKEY in (?)
+        SQL
+
+        conn.execute(sanitize_sql_array([<<-SQL, user_id, vacols_ids]))
+          update PRIORLOC
+          set LOCDIN = SYSDATE,
+              LOCSTRCV = ?,
+              LOCEXCEP = 'Y'
+          where LOCKEY in (?) and LOCDIN is null
+        SQL
+
+        insert_strs = vacols_ids.map do |vacols_id|
+          sanitize_sql_array(
+            [
+              "into PRIORLOC (LOCDOUT, LOCDTO, LOCSTTO, LOCSTOUT, LOCKEY) values (SYSDATE, SYSDATE, ?, ?, ?)",
+              location,
+              user_id,
+              vacols_id
+            ]
+          )
+        end
+
+        conn.execute("insert all #{insert_strs.join(' ')} select 1 from dual")
+      end
+    end
+  end
+
+  private
+
+  attr_reader :location, :vacols_ids, :user_id
+end
