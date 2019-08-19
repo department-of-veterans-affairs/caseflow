13d348e15a061b8ee55f85febfc1a4ce50439d55
lowellrex <lowell@navahq.com>
2019-08-19 08:47:56 -0400

    Add tests to instantiation routine

--- spec/models/task_filter_spec.rb
+++ spec/models/task_filter_spec.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require "support/database_cleaner"
 require "rails_helper"
 
-describe TaskFilter do
-  describe ".new" do
-    subject { TaskFilter.new(filter_params: filter_params) }
+describe TaskFilter, :postgres do
+  describe ".new"  do
+    let(:args) { { filter_params: filter_params } }
 
-    context "when input argument is nil" do
+    subject { TaskFilter.new(args) }
+
+    context "when input filter_params argument is nil" do
       let(:filter_params) { nil }
 
       it "raises an MissingRequiredProperty error" do
@@ -14,7 +17,7 @@ describe TaskFilter do
       end
     end
 
-    context "when input argument is a string" do
+    context "when input filter_params argument is a string" do
       let(:filter_params) { "filter_params" }
 
       it "raises an MissingRequiredProperty error" do
@@ -22,7 +25,7 @@ describe TaskFilter do
       end
     end
 
-    context "when input argument an empty array" do
+    context "when input filter_params argument an empty array" do
       let(:filter_params) { [] }
 
       it "instantiates without error" do
@@ -31,12 +34,34 @@ describe TaskFilter do
       end
     end
 
-    context "when input argument an array formatted as expected" do
+    context "when input filter_params argument an array formatted as expected" do
       let(:filter_params) { ["col=#{Constants.QUEUE_CONFIG.TASK_TYPE_COLUMN}&val=#{RootTask.name}"] }
 
       it "instantiates without error" do
         expect { subject }.to_not raise_error
         expect(subject).to be_a(TaskFilter)
+      end
+    end
+
+    context "when the input tasks argument is not an ActiveRecord::Relation object" do
+      let(:args) { { tasks: [create(:generic_task)] } }
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Caseflow::Error::MissingRequiredProperty)
+      end
+    end
+
+    context "when all input arguments are valid" do
+      let(:filter_params) { ["col=#{Constants.QUEUE_CONFIG.TASK_TYPE_COLUMN}&val=#{RootTask.name}"] }
+      let(:tasks) { Task.where(id: create_list(:generic_task, 6).pluck(:id)) }
+
+      let(:args) { { filter_params: filter_params, tasks: tasks } }
+
+      it "instantiates with given arguments" do
+        expect { subject }.to_not raise_error
+
+        expect(subject.filter_params).to eq(filter_params)
+        expect(subject.tasks).to eq(tasks)
       end
     end
   end
