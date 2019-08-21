7e6faab363ae015ffc7e2bf892dc3b5adcf566a0
Peter Karman <peter@truss.works>
2019-08-21 15:29:27 -0500

    Add MissedJobSweeperJob

---
+++ app/jobs/missed_job_sweeper_job.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# who will watch the watcher? we will!
+# periodic check that jobs we expected to run, have run.
+# if not, try them again.
+class MissedJobSweeperJob < ApplicationJob
+  queue_as :high
+  application_attr :queue
+
+  def perform
+    check_distribution_jobs
+  end
+
+  private
+
+  def check_distribution_jobs
+    missed_jobs = Distribution.pending.where("created_at > ?", 1.hour.ago)
+    missed_jobs.each do |distribution|
+      slack_service.send_notification("Restarted Distribution job #{distribution.id}")
+      StartDistributionJob.perform_now(distribution, distribution.judge)
+    end
+  end
+end
