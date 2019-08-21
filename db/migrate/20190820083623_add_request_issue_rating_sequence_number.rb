7acd767563bb5a467cf73f846fee924eba0d7da4
Peter Karman <peter@truss.works>
2019-08-21 08:46:39 -0500

    rename column

--- db/migrate/20190820083623_add_request_issue_rating_sequence_number.rb
+++ db/migrate/20190820083623_add_request_issue_rating_sequence_number.rb
@@ -1,5 +1,5 @@
 class AddRequestIssueRatingSequenceNumber < ActiveRecord::Migration[5.1]
   def change
-    add_column :request_issues, :contested_rating_issue_sequence_id, :string, comment: "The BGS rating_sequence_number for contested issues."
+    add_column :request_issues, :contested_rating_decision_reference_id, :string, comment: "The BGS id for contested rating decisions. These may not have corresponding contested_rating_issue_reference_id values."
   end
 end
