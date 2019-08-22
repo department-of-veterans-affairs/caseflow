7921171f8c318459bba88f3ebab132ca9c5985fa
Tomas Apodaca <tomas@navapbc.com>
2019-08-22 06:51:31 -0700

    revert test

--- spec/jobs/missed_job_sweeper_job_spec.rb
+++ spec/jobs/missed_job_sweeper_job_spec.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "support/database_cleaner"
 
-describe MissedJobSweeperJob do
+describe MissedJobSweeperJob, :postgres do
+  let(:slack_service) { SlackService.new(url: "http://www.example.com") }
+
+  before do
+    allow(SlackService).to receive(:new).and_return(slack_service)
+    allow(slack_service).to receive(:send_notification) { true }
+  end
+
   context ".perform" do
-    it "calls distribute! and sends a slack notification" do
-      slack_service = double("SlackService")
-      allow(slack_service).to receive(:send_notification) { true }
-      allow_any_instance_of(described_class).to receive(:slack_service).and_return(slack_service)
+    subject { described_class.perform_now }
 
-      distribution_id = 111
-      distribution = double("Distribution", id: distribution_id, judge: double("User"))
-      allow(distribution).to receive(:distribute!)
-      allow(Distribution).to receive(:pending).and_return(Distribution)
-      allow(Distribution).to receive(:where).with("created_at < ?", kind_of(ActiveSupport::TimeWithZone)) do |*args|
-        expect(args[1]).to be >= 1.hour.ago
-      end.and_return([distribution])
+    it "calls distribute!" do
+      judge = create(:user, css_id: "MYNAMEISJUDGE")
+      allow(judge).to receive(:judge_in_vacols?) { true }
+      distribution = Distribution.create!(judge: judge)
+      Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
+        subject
 
-      described_class.perform_now
-
-      expect(distribution).to have_received(:distribute!).once
-      message = "Restarting jobs for Distributions: [#{distribution_id}]"
-      expect(slack_service).to have_received(:send_notification).with(message).once
+        # we'll get error status and that's ok. it's because we don't bother to set up appeals first.
+        expect(distribution.reload).to be_error
+        message = "Restarting jobs for Distributions: [#{distribution.id}]"
+        expect(slack_service).to have_received(:send_notification).with(message).once
+      end
     end
   end
 end
