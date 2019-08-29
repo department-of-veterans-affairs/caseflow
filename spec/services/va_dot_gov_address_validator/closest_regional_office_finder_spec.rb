c0fb01db565a21991d4d2d8e4e46e8c8ad49ce94
Andrew Lomax <andrewlomaxr@gmail.com>
2019-08-29 18:18:33 -0400

    add mock-ficlity data

--- spec/services/va_dot_gov_address_validator/closest_regional_office_finder_spec.rb
+++ spec/services/va_dot_gov_address_validator/closest_regional_office_finder_spec.rb
@@ -34,3 +34,20 @@ describe VaDotGovAddressValidator::ClosestRegionalOfficeFinder do
     end
   end
 end
+
+def mock_facility_data(id:, distance: 10, city: "Fake City", state: "PA")
+  {
+    facility_id: id,
+    type: "",
+    distance: distance,
+    facility_type: "",
+    name: "Fake Name",
+    classification: "",
+    lat: 0.0,
+    long: 0.0,
+    address: "Fake Address",
+    city: city,
+    state: state,
+    zip_code: "00000"
+  }
+end
