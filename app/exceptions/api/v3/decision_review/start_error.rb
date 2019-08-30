ec2e56b852c60c3d12b0a5443c468ab631d2c24a
Ed Mangimelli <edmangimelli@gmail.com>
2019-08-30 15:16:00 -0500

    remove byebug from StartError

--- app/exceptions/api/v3/decision_review/start_error.rb
+++ app/exceptions/api/v3/decision_review/start_error.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
-#require "byebug"
 
 class Api::V3::DecisionReview::StartError < StandardError
   attr_reader :error_code
 
   def initialize(intake)
-    #byebug
     @error_code = intake.error_code || :intake_start_failed
     super("intake.start! did not throw an exception, but did return a falsey value")
   end
