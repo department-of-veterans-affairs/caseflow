6e09f94192de80c7906f9c13e40da87c25ec2faf
Peter Karman <peter@truss.works>
2019-09-03 14:47:10 -0500

    spec factory uses update_columns to bypass callbacks, update test for non-comp completed

--- app/controllers/decision_reviews_controller.rb
+++ app/controllers/decision_reviews_controller.rb
@@ -53,7 +53,7 @@ class DecisionReviewsController < ApplicationController
 
   def completed_tasks
     apply_task_serializer(
-      business_line.tasks.recently_closed(includes([:assigned_to, :appeal]).order(closed_at: :desc).select(&:completed?)
+      business_line.tasks.recently_closed.includes([:assigned_to, :appeal]).order(closed_at: :desc).select(&:completed?)
     )
   end
 
