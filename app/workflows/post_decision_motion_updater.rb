632f5512d22803a27b259455c56ddcae2c0c447f
aroltsch <aroltsch@tistatech.com>
2019-09-04 12:24:54 -0400

    Implement logic when judge finishes reviewing motion to vacate task

---
+++ app/workflows/post_decision_motion_updater.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class PostDecisionMotionUpdater
+  include ActiveModel::Model
+
+  attr_reader :assigned_to_id, :task, :disposition, :vacate_type
+
+  def initialize(task, params)
+    @task = task
+    @assigned_to_id = params[:assigned_to_id]
+    @disposition = params[:disposition]
+    @vacate_type = params[:vacate_type]
+  end
+
+  def process
+    ActiveRecord::Base.transaction do
+      motion = create_motion
+      return unless motion
+      perform_action_based_on_disposition
+    end
+  end
+
+  private
+
+  def create_motion
+    motion = PostDecisionMotion.new(
+      task: task,
+      disposition: disposition,
+      vacate_type: vacate_type
+    )
+    unless motion.valid?
+      errors.messages.merge!(motion.errors.messages)
+      return
+    end
+    motion.save
+  end
+
+  def perform_action_based_on_disposition
+    # If it is a grant, judge assigns it to the drafting attorney,
+    # otherwise, it goes back to the motions attorney
+    if disposition == "granted"
+      return task_class.create!(
+        appeal: task.appeal,
+        parent: task,
+        assigned_by: task.assigned_to,
+        assigned_to: assigned_to
+      )
+    end
+    task.update(status: Constants.TASK_STATUSES.completed)
+  end
+
+  def task_class
+    @task_class ||= (vacate_type + "_task").classify.constantize
+  end
+
+  def assigned_to
+    @assigned_to ||= User.find_by(id: assigned_to_id)
+  end
+end
---
+++ spec/workflows/post_decision_motion_updater_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "support/database_cleaner"
+require "rails_helper"
+
+describe PostDecisionMotionUpdater, :postgres do
+  describe "#process" do
+
+
+  end
+end
