c58998a04ee6ad7a15668b6406b792d5d4138620
Peter Karman <peter@truss.works>
2019-09-04 15:39:40 -0500

    lint

--- app/jobs/warm_bgs_caches_job.rb
+++ app/jobs/warm_bgs_caches_job.rb
@@ -67,9 +67,7 @@ class WarmBgsCachesJob < CaseflowJob
     hearings.each do |hearing|
       veteran = hearing.appeal.veteran
 
-      next unless veteran
-
-      if veteran.stale_attributes?
+      if veteran&.stale_attributes?
         veteran.update_cached_attributes!
         veterans_updated += 1
       end
