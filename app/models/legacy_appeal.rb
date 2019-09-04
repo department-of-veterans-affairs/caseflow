1bd184c0884a7a6a6b42613d604db5468877f971
Peter Karman <peter@truss.works>
2019-09-04 15:37:33 -0500

    fix loop bug

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -230,7 +230,7 @@ class LegacyAppeal < ApplicationRecord
   end
 
   def veteran
-    @veteran ||= Veteran.find_or_create_by_file_number_or_ssn(veteran_file_number)
+    @veteran ||= Veteran.find_or_create_by_file_number_or_ssn(sanitized_vbms_id)
   end
 
   def veteran_ssn
