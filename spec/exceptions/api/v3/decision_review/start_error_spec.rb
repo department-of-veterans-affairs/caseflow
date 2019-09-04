342fb1868aa665531162223f7b5c58cdf13cac4a
Ed Mangimelli <edmangimelli@gmail.com>
2019-09-04 04:24:04 -0500

    switch to using described_class

--- spec/exceptions/api/v3/decision_review/start_error_spec.rb
+++ spec/exceptions/api/v3/decision_review/start_error_spec.rb
@@ -14,20 +14,18 @@ context Api::V3::DecisionReview::StartError do
   end
 
   context ".new" do
-    subject { Api::V3::DecisionReview::StartError }
-
     it "creating the exception should not raise an exception" do
-      expect { subject.new(intake) }.not_to raise_error
+      expect { described_class.new(intake) }.not_to raise_error
     end
 
     it "should have error code :intake_review_failed" do
-      expect(subject.new(intake).error_code).to eq(:intake_start_failed)
+      expect(described_class.new(intake).error_code).to eq(:intake_start_failed)
     end
 
     it "should have error code :cat" do
       intake_with_error_code = intake
       intake_with_error_code.error_code = :veteran_not_valid
-      expect(subject.new(intake_with_error_code).error_code).to eq("veteran_not_valid")
+      expect(described_class.new(intake_with_error_code).error_code).to eq("veteran_not_valid")
     end
   end
 end
