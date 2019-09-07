0a3c7aed1d5950ff6cbfb5678e51e5a700dc954c
aroltsch <aroltsch@tistatech.com>
2019-09-07 07:57:17 -0400

    Lint

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -736,6 +736,7 @@ class LegacyAppeal < ApplicationRecord
   def attorney_case_review
     # # Created at date will be nil if there is no decass record created for this appeal yet
     return unless vacols_case_review&.created_at
+
     AttorneyCaseReview.find_by(task_id: "#{vacols_id}-#{VacolsHelper.day_only_str(vacols_case_review.created_at)}")
   end
 
