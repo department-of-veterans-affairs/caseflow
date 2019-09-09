9c3b8b77b874982528e2e7db944da95961953584
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-09-09 14:58:25 -0400

    write unit tests for create_judge_assign_task

--- app/workflows/das_deprecation/case_distribution.rb
+++ app/workflows/das_deprecation/case_distribution.rb
@@ -3,14 +3,12 @@
 class DasDeprecation::CaseDistribution
 	class << self
 		def create_judge_assign_task(record, judge)
-			binding.pry
-			puts "CREATE JUDGE ASSIGN TASK IS CALLED"
-			legacy_appeal =  LegacyAppeal.find_or_create_by_vacols_id(record["bfkey"])
+			legacy_appeal = LegacyAppeal.find_or_create_by_vacols_id(record["bfkey"])
 			legacy_appeal.transaction do
 	      root_task = RootTask.find_or_create_by!(appeal: legacy_appeal)
 	      JudgeAssignTask.create!(appeal: legacy_appeal, parent: root_task, assigned_to: judge)
 
-	      yield
+	      yield if block_given?
 	    end
 		end
 	end
