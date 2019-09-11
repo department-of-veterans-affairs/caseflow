330919e29ce9c866844def3cb09ceb358cae717a
Sjones352 <sjones@tistatech.com>
2019-09-11 10:34:11 -0400

    show claimant for all benefit types

--- client/app/intake/util/issues.js
+++ client/app/intake/util/issues.js
@@ -5,7 +5,6 @@ import { FORM_TYPES } from '../constants';
 
 const getNonVeteranClaimant = (intakeData) => {
   const claimant = intakeData.relationships.filter((relationship) => {
-    // console.log("veteran", relationship.value === intakeData.claimant);
     return relationship.value === intakeData.claimant;
   });
 
@@ -17,16 +16,12 @@ const getNonVeteranClaimant = (intakeData) => {
 };
 
 const getClaimantField = (formType, veteran, intakeData) => {
-  // if (formType === 'appeal' || intakeData.benefitType === 'compensation') {
     const claimant = intakeData.veteranIsNotClaimant ? getNonVeteranClaimant(intakeData) : veteran.name;
 
     return [{
       field: 'Claimant',
       content: claimant
     }];
-  // }
-
-  // return [];
 };
 
 export const legacyIssue = (issue, legacyAppeals) => {
