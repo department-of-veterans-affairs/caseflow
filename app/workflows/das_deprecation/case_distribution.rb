8ce742b282928fad4e07b462b6dacee6b5ab4c4f
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-09-11 17:54:01 -0400

    refactor the creation of JudgeAssignTask for AMA and Legacy

--- app/workflows/das_deprecation/case_distribution.rb
+++ app/workflows/das_deprecation/case_distribution.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class DasDeprecation::CaseDistribution
-  class << self
-    def create_judge_assign_task(record, judge)
-      legacy_appeal = LegacyAppeal.find_or_create_by_vacols_id(record["bfkey"])
-      legacy_appeal.transaction do
-        root_task = RootTask.find_or_create_by!(appeal: legacy_appeal)
-        JudgeAssignTask.create!(appeal: legacy_appeal, parent: root_task, assigned_to: judge)
+	class << self
+		def create_judge_assign_task(record, judge)
+			legacy_appeal = LegacyAppeal.find_or_create_by_vacols_id(record["bfkey"])
+			legacy_appeal.transaction do
+	      root_task = RootTask.find_or_create_by!(appeal: legacy_appeal)
+	      JudgeAssignTaskCreator.new(appeal: legacy_appeal, judge: judge).call
 
-        yield if block_given?
-      end
-    end
-  end
+	      yield if block_given?
+	    end
+		end
+	end
 end
