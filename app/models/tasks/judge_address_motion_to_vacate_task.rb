ebdc11a5ba6cba7585940278a717b62da438fe68
JC Quirin <jcq@jcq.local>
2019-09-13 14:21:57 -0700

    Updates to JudgeAddressMotionToVacateTask: pulac cerullo action just on by default

--- app/models/tasks/judge_address_motion_to_vacate_task.rb
+++ app/models/tasks/judge_address_motion_to_vacate_task.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class JudgeAddressMotionToVacateTask < JudgeTask
-  def additional_available_actions(user)
-    actions = []
-
-    if LitigationSupport.singleton.user_has_access?(user)
-      actions.push(Constants.TASK_ACTIONS.LIT_SUPPORT_PULAC_CERULLO.to_h)
-    end
+  def additional_available_actions(_user)
+    actions = [Constants.TASK_ACTIONS.LIT_SUPPORT_PULAC_CERULLO.to_h]
 
     if assigned_to.is_a?(User) && FeatureToggle.enabled?(:review_motion_to_vacate)
       actions.push(Constants.TASK_ACTIONS.ADDRESS_MOTION_TO_VACATE.to_h)
