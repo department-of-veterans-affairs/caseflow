4c40b3821ec21e51995c807396cb50da73fb4947
Peter Karman <peter@truss.works>
2019-09-16 10:52:43 -0500

    add intake link

--- client/app/inbox/index.jsx
+++ client/app/inbox/index.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReduxBase from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/ReduxBase';
 import NavigationBar from '../components/NavigationBar';
-import CaseSearchLink from '../components/CaseSearchLink';
+import IntakeLink from '../components/IntakeLink';
 import { BrowserRouter } from 'react-router-dom';
 import PageRoute from '../components/PageRoute';
 import AppFrame from '../components/AppFrame';
@@ -29,7 +29,7 @@ class Inbox extends React.PureComponent {
               accentColor: LOGO_COLORS.INTAKE.ACCENT,
               overlapColor: LOGO_COLORS.INTAKE.OVERLAP
             }}
-            rightNavElement={<CaseSearchLink />}
+            rightNavElement={<IntakeLink />}
             userDisplayName={this.props.userDisplayName}
             dropdownUrls={this.props.dropdownUrls}
             topMessage={null}
