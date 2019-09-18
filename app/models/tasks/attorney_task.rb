fa05b44c0fe9350e98d7f6a9b71f5a0916b52cc7
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-09-18 18:26:16 -0400

    fix test failure

--- app/models/tasks/attorney_task.rb
+++ app/models/tasks/attorney_task.rb
@@ -40,8 +40,13 @@ class AttorneyTask < Task
     COPY::ATTORNEY_TASK_LABEL
   end
 
+  # cancel task when a judge reassigns task to another attorney
   def cancel_task
-    cancel_task_and_child_subtasks
+    descendant_ids = descendants.pluck(:id)
+    Task.where(id: descendant_ids).update_all(
+      status: Constants.TASK_STATUSES.cancelled,
+      closed_at: Time.zone.now
+    )
   end
   
   private
