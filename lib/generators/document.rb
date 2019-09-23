f1f8676d780bb9b4a12d83116a4cced8d73a8885
Peter Karman <pkarman@users.noreply.github.com>
2019-09-23 12:30:12 -0500

    Add missing timestamp columns (#12185)
    Resolves #9793 & Resolves #12070 
    
    ### Description
    All rails tables should have timestamps.
    
    *Schema Changes Only*
    
    * [x] Column comments updated
    * [x] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
    * [x] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
    * [x] #appeals-schema notified with summary and link to this PR

--- lib/generators/document.rb
+++ lib/generators/document.rb
@@ -12,9 +12,7 @@ class Generators::Document
         received_at: 3.days.ago,
         upload_date: 2.days.ago,
         type: ["Form 8", "Form 9", "NOD", "SOC", "SSOC"].sample,
-        file_number: Random.rand(999_999_999).to_s,
-        created_at: rand(3).days.ago,
-        updated_at: rand(3).days.ago
+        file_number: Random.rand(999_999_999).to_s
       }
     end
 
