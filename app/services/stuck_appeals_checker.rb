5b4fe8fb06d69a92933360ccd27a37aa5dac67d7
Peter Karman <peter@truss.works>
2019-09-25 10:40:39 -0500

    lint

--- app/services/stuck_appeals_checker.rb
+++ app/services/stuck_appeals_checker.rb
@@ -2,10 +2,14 @@
 
 class StuckAppealsChecker < DataIntegrityChecker
   def call
-    query = AppealsWithNoTasksOrAllTasksOnHoldQuery.new
-    stuck_appeals = query.call
     return unless stuck_appeals.count > 0
 
     add_to_report "Stuck Appeals: #{stuck_appeals.count} reported by AppealsWithNoTasksOrAllTasksOnHoldQuery"
+  end
+
+  private
+
+  def stuck_appeals
+    @stuck_appeals ||= AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
   end
 end
