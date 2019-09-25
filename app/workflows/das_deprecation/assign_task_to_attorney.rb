7be5a2ae8cba9b0b94ea9f05fe75f97c871b883a
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-09-25 13:46:30 -0400

    modify assign_task_to_attorny to use attorney_task_creator

--- app/workflows/das_deprecation/assign_task_to_attorney.rb
+++ app/workflows/das_deprecation/assign_task_to_attorney.rb
@@ -4,30 +4,26 @@ class DasDeprecation::AssignTaskToAttorney
   class << self
     def create_attorney_task(vacols_id, assigned_by, assigned_to)
       appeal = LegacyAppeal.find_by(vacols_id: vacols_id)
-      judge_assign_task = JudgeAssignTask.find_by(appeal_id: LegacyAppeal.find_by(vacols_id: vacols_id).id)
-      judge_review_task = JudgeDecisionReviewTask.create!(
-        appeal: appeal,
-        assigned_to: assigned_to,
-        parent: judge_assign_task
-      )
-      judge_assign_task.update!(status: Constants.TASK_STATUSES.completed)
-      AttorneyTask.create!(
+      judge_assign_task = JudgeAssignTask.find_by(appeal: appeal)
+      task_params = {
         appeal: appeal,
         assigned_by: assigned_by,
-        assigned_to: assigned_to,
-        parent: judge_review_task
-      )
+        assigned_to: assigned_to
+      }
+      AttorneyTaskCreator.new(judge_assign_task, task_params).call.first
     end
 
     def reassign_attorney_task(vacols_id, assigned_by, assigned_to)
-      AttorneyTask.find_by(appeal_id: LegacyAppeal.find_by(vacols_id: vacols_id).id).cancel_task
-      create_attorney_task(vacols_id, assigned_by, assigned_to)
+      binding.pry
+      task = AttorneyTask.open.find_by(appeal_id: LegacyAppeal.find_by(vacols_id: vacols_id).id)
+      task.update_from_params({ assigned_to_id: assigned_to.id }, assigned_by).first
     end
 
     def should_perform_workflow?(appeal_id)
       return false if !FeatureToggle.enabled?(:legacy_das_deprecation, user: RequestStore.store[:current_user])
 
-      !JudgeAssignTask.find_by(appeal_id: appeal_id).nil?
+      appeal = LegacyAppeal.find(appeal_id)
+      !JudgeAssignTask.find_by(appeal: appeal).nil?
     end
   end
 end
