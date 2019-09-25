767cec748ae6f81c5b511c0db7604bc5094edc03
Peter Karman <peter@truss.works>
2019-09-25 16:44:13 -0500

    note is text column

--- db/migrate/20190925205112_create_job_notes.rb
+++ db/migrate/20190925205112_create_job_notes.rb
@@ -4,7 +4,7 @@ class CreateJobNotes < ActiveRecord::Migration[5.1]
       t.belongs_to :job, polymorphic: true, null: false, comment: "The job to which the note applies"
       t.belongs_to :user, null: false, comment: "The user who created the note"
       t.timestamps null: false, comment: "Default created_at/updated_at"
-      t.string :note, null: false, comment: "The note"
+      t.text :note, null: false, comment: "The note"
       t.boolean :send_to_intake_user, default: false, comment: "Should the note trigger a message to the job intake user"
     end
   end
