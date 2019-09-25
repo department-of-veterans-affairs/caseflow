5d1a2efb441b3011b49d016f01a943758116c897
Peter Karman <peter@truss.works>
2019-09-25 06:46:33 -0500

    Add dispatched appeals on hold

--- app/queries/appeals_with_no_tasks_or_all_tasks_on_hold_query.rb
+++ app/queries/appeals_with_no_tasks_or_all_tasks_on_hold_query.rb
@@ -2,13 +2,22 @@
 
 class AppealsWithNoTasksOrAllTasksOnHoldQuery
   def call
-    [stuck_appeals, appeals_with_zero_tasks].flatten
+    [stuck_appeals, appeals_with_zero_tasks, dispatched_appeals_on_hold].flatten
   end
 
   private
 
+  def dispatched_appeals_on_hold
+    Appeal.where(id: tasks_for("Appeal").where(type: "RootTask", status: Constants.TASK_STATUSES.on_hold))
+      .where(id: completed_dispatch_tasks("Appeal"))
+  end
+
   def stuck_appeals
     stuck_query("Appeal")
+  end
+
+  def completed_dispatch_tasks(klass_name)
+    tasks_for(klass_name).where(type: %w[BvaDispatchTask QualityReviewTask], status: Constants.TASK_STATUSES.completed)
   end
 
   def established_appeals
