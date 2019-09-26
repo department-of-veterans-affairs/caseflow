d5f0e08fb570a84a5d1d23cafb2421d43b4dcc2c
Peter Karman <peter@truss.works>
2019-09-26 15:53:18 -0500

    treat blank? like nil

--- app/models/hearing_location.rb
+++ app/models/hearing_location.rb
@@ -5,6 +5,6 @@ class HearingLocation < ApplicationRecord
 
   def street_address
     addr = Constants::REGIONAL_OFFICE_FACILITY_ADDRESS[facility_id]
-    %w[address_1 address_2 address_3].map { |line| addr[line] }.compact.join(", ")
+    %w[address_1 address_2 address_3].map { |line| addr[line] }.reject(&:blank?).join(", ")
   end
 end
--- spec/models/hearing_location_spec.rb
+++ spec/models/hearing_location_spec.rb
@@ -9,5 +9,11 @@ describe HearingLocation do
 
       expect(hearing_location.street_address).to eq("130 South Elmwood Ave, Suite 601")
     end
+
+    it "rejects nulls and blank values" do
+      hearing_location = described_class.new(facility_id: "vba_317")
+
+      expect(hearing_location.street_address).to eq("9500 Bay Pines Blvd.")
+    end
   end
 end
