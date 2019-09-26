251ead99fa9c8c3eee9038b50aa085737d2864ba
Peter Karman <peter@truss.works>
2019-09-26 15:29:23 -0500

    Add HearingLocation.street_address

--- app/models/hearing_location.rb
+++ app/models/hearing_location.rb
@@ -2,4 +2,9 @@
 
 class HearingLocation < ApplicationRecord
   belongs_to :hearing, polymorphic: true
+
+  def street_address
+    addr = Constants::REGIONAL_OFFICE_FACILITY_ADDRESS[facility_id]
+    %w[address_1 address_2 address_3].map { |line| addr[line] }.compact.join(", ")
+  end
 end
