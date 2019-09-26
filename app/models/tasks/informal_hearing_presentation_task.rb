b31dd22a37c6386443a0965c917059a3f9e13541
hschallhorn <hschallhorn@tistatech.com>
2019-09-26 08:51:48 -0400

    Code climate

--- app/models/tasks/informal_hearing_presentation_task.rb
+++ app/models/tasks/informal_hearing_presentation_task.rb
@@ -6,47 +6,42 @@
 # BVA typically (but not always) waits for an IHP to be submitted before making a decision.
 
 class InformalHearingPresentationTask < Task
-  # TODO: figure out how long IHP tasks will take to expire,
+  # https://github.com/department-of-veterans-affairs/caseflow/issues/10824
+  # Figure out how long IHP tasks will take to expire,
   # then make them timeable
   # include TimeableTask
 
+  USER_ACTIONS = [
+    Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
+    Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
+    Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+    Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+  ].freeze
+
+  ADMIN_ACTIONS = [
+    Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h
+  ].freeze
+
+  ORG_ACTIONS = [
+    Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h,
+    Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+    Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+  ].freeze
+
   def available_actions(user)
     if assigned_to == user
-      return user_actions
+      return USER_ACTIONS
     end
 
     if task_is_assigned_to_user_within_organization?(user) && parent.assigned_to.user_is_admin?(user)
-      return admin_actions
+      return ADMIN_ACTIONS
     end
 
     if task_is_assigned_to_users_organization?(user)
-      return org_actions
+      return ORG_ACTIONS
     end
 
     []
-  end
-
-  def user_actions
-    [
-      Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
-      Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
-      Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
-      Constants.TASK_ACTIONS.CANCEL_TASK.to_h
-    ]
-  end
-
-  def admin_actions
-    [
-      Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h
-    ]
-  end
-
-  def org_actions
-    [
-      Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h,
-      Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
-      Constants.TASK_ACTIONS.CANCEL_TASK.to_h
-    ]
   end
 
   def self.label
