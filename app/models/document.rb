d2a547355ce0110332cb3295eba545f8a47add66
Peter Karman <peter@truss.works>
2019-10-01 11:11:26 -0500

    do not save changesets for documents and annotations

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,7 +5,8 @@ class Document < ApplicationRecord
   has_many :document_views
   has_many :documents_tags
   has_many :tags, through: :documents_tags
-  has_paper_trail only: [:description, :category_case_summary, :category_medical, :category_other, :category_procedural]
+  has_paper_trail only: [:description, :category_case_summary, :category_medical, :category_other, :category_procedural],
+                  save_changes: false
 
   self.inheritance_column = nil
 
