914f4a758220e0425421b77ec451c19b67c07f7e
Ed Mangimelli <edmangimelli@gmail.com>
2019-10-02 10:13:26 -0500

    WIP

--- app/controllers/api/v3/decision_review/intake_statuses_controller.rb
+++ app/controllers/api/v3/decision_review/intake_statuses_controller.rb
@@ -2,6 +2,73 @@
 
 class Api::V3::DecisionReview::IntakeStatusesController < Api::V3::BaseController
   def show
-    render status: :ok
+    unless decision_review
+      render_no_decision_review_error
+      return
+    end
+
+    render_hash, headers = intake_status.render_hash_and_headers
+
+    load_headers(headers)
+    render render_hash
+  rescue StandardError
+    render_unknown_error
+  end
+
+  private
+
+  def uuid
+    params[:uuid]
+  end
+
+  def decision_review
+    @decision_review ||= DecisionReview.where_uuid(uuid)
+  end
+
+  def intake
+    @intake ||= decision_review.intake
+  end
+
+  def intake_status
+    Api::V3::DecisionReview::IntakeStatus.new(intake)
+  end
+
+  def load_headers(headers)
+    headers.each { |key, val| response.set_header(key, val) }
+  end
+
+  def render_unknown_error
+    render_error status: 500, code: :unknown_error, title: "Unknown error"
+  end
+
+  def render_no_decision_review_error
+    render_error(
+      status: 404,
+      code: :decision_review_not_found,
+      title: "Unable to find a DecisionReview with uuid: #{uuid}"
+    )
+  end
+
+  def render_error(status:, code:, title:)
+    render(
+      json: { errors: [{ status: status, code: code, title: title }] },
+      status: status
+    )
   end
 end
+
+# i think render_hash should become render_hash_and_headers
+#
+# [{json, status}, {Location: stnrset}]
+#
+# returns 200
+# until 300
+#
+#
+# HigherLevelReviewController needs to override this and return 202
+#
+# update docs for more asyncable statuses
+#
+# make intake_status_error class ?
+#
+# update docs to add status to returns?
--- app/services/api/v3/decision_review/intake_status.rb
+++ app/services/api/v3/decision_review/intake_status.rb
@@ -5,16 +5,27 @@ class Api::V3::DecisionReview::IntakeStatus
     @intake = intake
   end
 
-  def render_hash
+  # returns an array of two elements
+  #  [0] the arguments for the render command
+  #  [1] any headers that need to be set (will default to {}
+  def render_hash_and_headers
     @intake.reload if @intake.id
-    {
-      json: {
-        type: intake.detail_type,
-        id: detail&.uuid,
-        attributes: { status: detail&.asyncable_status }
+
+    return no_decision_review_render_hash_and_headers unless detail
+
+    decision_review_status, http_status, headers = statuses_and_headers
+
+    [
+      { # arguments for render
+        json: {
+          type: intake.detail_type,
+          id: detail.uuid,
+          attributes: { status: decision_review_status }
+        },
+        status: http_status
       },
-      status: :accepted
-    }
+      headers # headers
+    ]
   end
 
   private
@@ -22,4 +33,28 @@ class Api::V3::DecisionReview::IntakeStatus
   attr_reader :intake
 
   delegate :detail, to: :intake
+
+  def no_decision_review_render_hash_and_headers
+    error = {
+      status: 500,
+      code: :intake_has_no_associated_decision_review,
+      title: "Intake has no associated decision review."
+    }
+
+    [
+      { json: { errors: [error] }, status: error[:status] }, # arguments for render
+      {} # headers
+    ]
+  end
+
+  def statuses_and_headers
+    decision_review_status = detail.asyncable_status
+    http_status, headers = if decision_review_status == :submitted
+                             [:see_other, { Location: "" }]
+                           else
+                             [:ok, {}]
+                           end
+
+    [decision_review_status, http_status, headers]
+  end
 end
