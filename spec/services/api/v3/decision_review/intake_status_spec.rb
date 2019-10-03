8cce3e7f57f76ea59686f1118e115c04ba4cead2
Ed Mangimelli <edmangimelli@gmail.com>
2019-10-03 11:50:47 -0500

    WIP

--- spec/services/api/v3/decision_review/intake_status_spec.rb
+++ spec/services/api/v3/decision_review/intake_status_spec.rb
@@ -18,8 +18,11 @@ context Api::V3::DecisionReview::IntakeStatus, :postgres do
     HigherLevelReview.create!(veteran_file_number: veteran_file_number)
   end
 
+  def new_intake_status(intake)
+    Api::V3::DecisionReview::IntakeStatus.new(intake, reload: false)
+  end
+
   context "#to_json" do
-    subject { Api::V3::DecisionReview::IntakeStatus.new(intake) }
     it("returns the correctly-formatted JSON:API response when the intake has a decision review") do
       decision_review = fake_higher_level_review
 
@@ -31,7 +34,7 @@ context Api::V3::DecisionReview::IntakeStatus, :postgres do
 
       intake = fake_intake(decision_review)
 
-      intake_status = Api::V3::DecisionReview::IntakeStatus.new(intake, reload: false)
+      intake_status = new_intake_status(intake)
 
       expect(intake_status.to_json).to be_a(Hash)
       expect(intake_status.to_json.keys).to contain_exactly(:data)
@@ -47,7 +50,7 @@ context Api::V3::DecisionReview::IntakeStatus, :postgres do
     it("returns an error when the intake does not have a decision review") do
       intake = fake_intake
 
-      intake_status = Api::V3::DecisionReview::IntakeStatus.new(intake, reload: false)
+      intake_status = new_intake_status(intake)
 
       expect(intake_status.to_json).to be_a(Hash)
       expect(intake_status.to_json.keys).to contain_exactly(:errors)
@@ -56,6 +59,41 @@ context Api::V3::DecisionReview::IntakeStatus, :postgres do
       expect(intake_status.to_json[:errors][0]).to be_a(Hash) 
       expect(intake_status.to_json[:errors][0].keys).to contain_exactly(:status, :code, :title) 
       expect(intake_status.to_json[:errors][0][:status]).to be > 399
+    end
+  end
+  context "#http_status" do
+    it("returns NO_DECISION_REVIEW_HTTP_STATUS when the intake has no decision review") do
+      intake = fake_intake
+
+      intake_status = new_intake_status(intake)
+
+      expect(intake_status.http_status).to eq(Api::V3::DecisionReview::IntakeStatus::NO_DECISION_REVIEW_HTTP_STATUS)
+    end
+
+    it("returns NOT_SUBMITTED_HTTP_STATUS if asyncable_status isn't :submitted") do
+      decision_review = fake_higher_level_review
+
+      asyncable_status = "zebra"
+      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
+
+      intake = fake_intake(decision_review)
+
+      intake_status = new_intake_status(intake)
+
+      expect(intake_status.http_status).to eq(Api::V3::DecisionReview::IntakeStatus::NOT_SUBMITTED_HTTP_STATUS)
+    end
+
+    it("returns SUBMITTED_HTTP_STATUS if asyncable_status is :submitted") do
+      decision_review = fake_higher_level_review
+
+      asyncable_status = :submitted
+      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
+
+      intake = fake_intake(decision_review)
+
+      intake_status = new_intake_status(intake)
+
+      expect(intake_status.http_status).to eq(Api::V3::DecisionReview::IntakeStatus::SUBMITTED_HTTP_STATUS)
     end
   end
 end
