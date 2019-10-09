b089b647e91e5b539ee5db8fe74f5516ff660b05
JC Quirin <jcq@jcq.local>
2019-10-09 14:15:54 -0700

    linting fixes

--- client/app/pulacCerullo/CavcLinkInfo.jsx
+++ client/app/pulacCerullo/CavcLinkInfo.jsx
@@ -4,8 +4,8 @@ import { cavcUrl } from '.';
 import CopyTextButton from '../components/CopyTextButton';
 
 export const CavcLinkInfo = () => (
-    <div>
-        Copy and paste the CAVC website link into Internet Explorer{' '}
-        <CopyTextButton text={new URL(cavcUrl).hostname} textToCopy={cavcUrl} label="uscourts.cavc.gov" />
-      </div>
-)
+  <div>
+    Copy and paste the CAVC website link into Internet Explorer{' '}
+    <CopyTextButton text={new URL(cavcUrl).hostname} textToCopy={cavcUrl} label="uscourts.cavc.gov" />
+  </div>
+);
