e21282bd6a16402c20171a6f7f23f29cf360cfdf
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2019-10-10 09:41:34 -0400

    UI for marking users inactive (#12312)
    * Add dropdown for admins and base controller for management page
    
    * Add route to get all users and to update the status of a user
    
    * Add front end and feature test
    
    * Update alert messages
    
    * Code climate
    
    * Clear error on success
    
    * Remove unused route
    
    * Fix old test
    
    * Some design feedback
    
    * Formatting
    
    * Don't touch serializer
    
    * More formatting because everything fails on CI anyway so what else is there to do?
    
    * Update tests to check for new copy
    
    * Undo some stuff, add search users by css_id
    
    * Parens!
    
    * MORE PARENS!
    
    * Use search rather than dropdown
    
    * words words words
    
    * Code Climate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :deny_non_bva_admins, only: [:represented_organizations]
+  before_action :deny_non_bva_admins, only: [:represented_organizations, :update, :search_by_css_id]
 
   def index
     case params[:role]
@@ -21,6 +21,22 @@ class UsersController < ApplicationController
     render json: {}
   end
 
+  def search_by_css_id
+    user = User.find_by_css_id(css_id)
+
+    fail ActiveRecord::RecordNotFound unless user
+
+    render json: { user: user }
+  end
+
+  def update
+    if params.key?(:status)
+      user_to_modify.update_status!(params[:status])
+    end
+
+    render json: { user: user_to_modify }, status: :ok
+  end
+
   def represented_organizations
     render json: { represented_organizations: User.find(params[:id]).vsos_user_represents }
   end
@@ -30,6 +46,22 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def css_id
+    unless params[:css_id]
+      fail(
+        Caseflow::Error::InvalidParameter,
+        parameter: "css_id",
+        message: "Must provide a css id"
+      )
+    end
+
+    params[:css_id]
+  end
+
+  def user_to_modify
+    @user_to_modify ||= User.find(params.require(:id))
+  end
 
   def json_users(users)
     ::WorkQueue::UserSerializer.new(users, is_collection: true)
