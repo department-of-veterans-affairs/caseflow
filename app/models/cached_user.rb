8c36cf36945b41285af9ced505fa2a1fc1759ce3
Peter Karman <peter@truss.works>
2019-10-10 12:44:47 -0500

    add CachedUser model

---
+++ app/models/cached_user.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CachedUser < ApplicationRecord
+  self.table_name = "cached_user_attributes"
+
+  class << self
+    def sync_from_vacols
+      VACOLS::Staff.find_each do |staff|
+        # we set attributes both in find_or_create_by block for not-null constraints
+        # on initial creation, and to update stale attributes
+        cached_user = find_or_create_by(sdomainid: staff.sdomainid) do |cuser|
+          cuser.sattyid = staff.sattyid
+          cuser.svlj = staff.svlj
+          cuser.slogid = staff.slogid
+          cuser.stafkey = staff.stafkey
+          cuser.sactive = staff.sactive
+        end
+        cached_user.sattyid = staff.sattyid
+        cached_user.svlj = staff.svlj
+        cached_user.slogid = staff.slogid
+        cached_user.stafkey = staff.stafkey
+        cached_user.sactive = staff.sactive
+        cached_user.save! if cached_user.changed?
+      end
+    end
+  end
+end
