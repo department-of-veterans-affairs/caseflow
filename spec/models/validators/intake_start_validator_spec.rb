6b519a55b8e9c55b0910b3d7380b34297fedcd8a
Ed Mangimelli <edmangimelli@gmail.com>
2019-10-11 10:36:09 -0500

    WIP

--- spec/models/validators/intake_start_validator_spec.rb
+++ spec/models/validators/intake_start_validator_spec.rb
@@ -10,14 +10,14 @@ describe IntakeStartValidator, :postgres do
     let(:veteran) { create(:veteran) }
 
     let(:intake) do
-      ->(user:) do
-        create(:intake, veteran_file_number: veteran.file_number, user: user)
+      ->(u) do
+        create(:intake, veteran_file_number: veteran.file_number, user: u)
       end
     end
 
     let(:validator) do
-      ->(user:) do
-        described_class.new(intake: intake[user: user])
+      ->(u) do
+        described_class.new(intake: intake[user: u])
       end
     end
 
@@ -25,13 +25,19 @@ describe IntakeStartValidator, :postgres do
     #   allow_any_instance_of(BGSService).to receive(:may_modify?) { raise }
     # end
 
-    it "returns false" do
-      expect(validator[user: user].send(:user_may_modify_veteran_file?)).to be false
+    it do
+      allow_any_instance_of(BGSService).to receive(:may_modify?) { true }
+      expect(validator[user].send(:user_may_modify_veteran_file?)).to be true
+    end
+
+    it do
+      allow_any_instance_of(BGSService).to receive(:may_modify?) { false }
+      expect(validator[user].send(:user_may_modify_veteran_file?)).to be false
     end
 
     it "returns true when user is api_user" do
       expect(
-        validator[user: User.api_user].send(:user_may_modify_veteran_file?)
+        validator[User.api_user].send(:user_may_modify_veteran_file?)
       ).to be true
     end
   end
