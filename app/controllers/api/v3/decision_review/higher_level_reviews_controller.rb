e1cfaad32c1a11c7a96aee5b8e2d4148ffdfc603
Philip Becker <philip.becker@oddball.io>
2019-10-10 23:03:09 -0400

    use serializer for output

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_dependency Rails.root.join('app', 'serializers', 'api', 'v3', 'higher_level_review')
 
 class Api::V3::DecisionReview::HigherLevelReviewsController < Api::V3::BaseController
   def create
@@ -21,7 +22,7 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < Api::V3::BaseContr
   end
 
   def show
-    higher_level_review = HighLevelReview.find_by_uuid(params[:id])
+    higher_level_review = HigherLevelReview.find_by_uuid(params[:id])
     render json: Api::V3::HigherLevelReviewSerializer.new(higher_level_review)
   end
 
