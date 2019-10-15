031b0c6969e56c04c984b4f5758d4eed0b0290a5
Ed Mangimelli <edmangimelli@gmail.com>
2019-10-15 01:21:35 -0500

    update intake_status_spec to conform to caseflow conventions

--- spec/services/api/v3/decision_review/intake_status_spec.rb
+++ spec/services/api/v3/decision_review/intake_status_spec.rb
@@ -7,140 +7,149 @@ context Api::V3::DecisionReview::IntakeStatus, :postgres do
   let(:veteran_file_number) { "123456789" }
 
   let(:higher_level_review) do
-    create(:higher_level_review, veteran_file_number: veteran_file_number)
+    hlr = create(:higher_level_review, veteran_file_number: veteran_file_number)
+    hlr.reload # set uuid
+    allow(hlr).to receive(:asyncable_status) { asyncable_status }
+    hlr
   end
+
+  let(:decision_review) { higher_level_review }
 
   let(:intake) do
     create(:intake, veteran_file_number: veteran_file_number, detail: decision_review)
   end
 
+  subject { described_class.new(intake) }
+
   context "#to_json" do
-    subject { intake_status = described_class.new(intake) }
+    let(:json) { subject.to_json }
 
-    context do
-      let(:decision_review) do
-        hlr = higher_level_review
+    context "when asyncable status is submitted" do
+      let(:asyncable_status) { :submitted }
 
-        asyncable_status = "dog"
-        allow(hlr).to receive(:asyncable_status) { asyncable_status }
-
-        uuid = "cat"
-        allow(hlr).to receive(:uuid) { uuid }
-
-        hlr
+      it "returns the correct json" do
+        expect(subject.to_json).to be_a(Hash)
       end
 
-      it("returns the correctly-formatted JSON:API response when the intake has a decision review") do
-        expect(intake_status.to_json).to be_a(Hash)
-        expect(intake_status.to_json.keys).to contain_exactly(:data)
-        expect(intake_status.to_json[:data]).to be_a(Hash)
-        expect(intake_status.to_json[:data].keys).to contain_exactly(:type, :id, :attributes)
-        expect(intake_status.to_json[:data][:type]).to eq(decision_review.class.name)
-        expect(intake_status.to_json[:data][:id]).to eq(uuid)
-        expect(intake_status.to_json[:data][:attributes]).to be_a(Hash)
-        expect(intake_status.to_json[:data][:attributes].keys).to contain_exactly(:status)
-        expect(intake_status.to_json[:data][:attributes][:status]).to eq(asyncable_status)
+      it("is correctly shaped") do
+        expect(subject.to_json.keys).to contain_exactly(:data)
+        expect(subject.to_json[:data]).to be_a(Hash)
+        expect(subject.to_json[:data].keys).to contain_exactly(:type, :id, :attributes)
+        expect(subject.to_json[:data][:attributes]).to be_a(Hash)
+        expect(subject.to_json[:data][:attributes].keys).to contain_exactly(:status)
+      end
+
+      it("has the correct values") do
+        expect(subject.to_json[:data][:type]).to eq(decision_review.class.name)
+        expect(subject.to_json[:data][:id]).to eq(higher_level_review.uuid)
+        expect(subject.to_json[:data][:attributes][:status]).to eq(asyncable_status)
       end
     end
 
-    context do
+    context "when asyncable status isn't :submitted" do
+      let(:asyncable_status) { "dog" }
+
+      it("returns json") do
+        expect(json).to be_a(Hash)
+      end
+
+      it("is correctly shaped") do
+        expect(json.keys).to contain_exactly(:data)
+        expect(json[:data]).to be_a(Hash)
+        expect(json[:data].keys).to contain_exactly(:type, :id, :attributes)
+        expect(json[:data][:attributes]).to be_a(Hash)
+        expect(json[:data][:attributes].keys).to contain_exactly(:status)
+      end
+
+      it("has the correct values") do
+        expect(json[:data][:type]).to eq(decision_review.class.name)
+        expect(json[:data][:id]).to eq(higher_level_review.uuid)
+        expect(json[:data][:attributes][:status]).to eq(asyncable_status)
+      end
+    end
+
+    context "when the intake doesn't have a decision review" do
       let(:decision_review) { nil }
 
-      it("returns an error when the intake does not have a decision review") do
+      it("returns json") do
         expect(subject.to_json).to be_a(Hash)
+      end
+
+      it("is correctly shaped") do
         expect(subject.to_json.keys).to contain_exactly(:errors)
         expect(subject.to_json[:errors]).to be_a(Array)
         expect(subject.to_json[:errors].length).to eq(1)
         expect(subject.to_json[:errors][0]).to be_a(Hash)
         expect(subject.to_json[:errors][0].keys).to contain_exactly(:status, :code, :title)
+      end
+
+      it("has an error http status") do
         expect(subject.to_json[:errors][0][:status]).to be > 399
       end
     end
   end
 
   context "#http_status" do
-    context do
+    context "when asyncable status is submitted" do
+      let(:asyncable_status) { :submitted }
+
+      it("returns SUBMITTED_HTTP_STATUS") do
+        expect(subject.http_status).to eq(
+          Api::V3::DecisionReview::IntakeStatus::SUBMITTED_HTTP_STATUS
+        )
+      end
+    end
+
+    context "when asyncable status isn't :submitted" do
+      let(:asyncable_status) { "zebra" }
+
+      it("returns NOT_SUBMITTED_HTTP_STATUS") do
+        expect(subject.http_status).to eq(
+          Api::V3::DecisionReview::IntakeStatus::NOT_SUBMITTED_HTTP_STATUS
+        )
+      end
+    end
+
+    context "when the intake doesn't have a decision review" do
       let(:decision_review) { nil }
-    it("returns NO_DECISION_REVIEW_HTTP_STATUS when the intake has no decision review") do
-      intake = create_intake[]
 
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status).to eq(
-        Api::V3::DecisionReview::IntakeStatus::NO_DECISION_REVIEW_HTTP_STATUS
-      )
-    end
-
-    it("returns NOT_SUBMITTED_HTTP_STATUS if asyncable_status isn't :submitted") do
-      decision_review = create_higher_level_review
-
-      asyncable_status = "zebra"
-      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
-
-      intake = create_intake[decision_review]
-
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status).to eq(
-        Api::V3::DecisionReview::IntakeStatus::NOT_SUBMITTED_HTTP_STATUS
-      )
-    end
-
-    it("returns SUBMITTED_HTTP_STATUS if asyncable_status is :submitted") do
-      decision_review = create_higher_level_review
-
-      asyncable_status = :submitted
-      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
-
-      intake = create_intake[decision_review]
-
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status).to eq(
-        Api::V3::DecisionReview::IntakeStatus::SUBMITTED_HTTP_STATUS
-      )
+      it("returns NO_DECISION_REVIEW_HTTP_STATUS") do
+        expect(subject.http_status).to eq(
+          Api::V3::DecisionReview::IntakeStatus::NO_DECISION_REVIEW_HTTP_STATUS
+        )
+      end
     end
   end
 
   context "#http_status_for_new_intake" do
-    it("returns NO_DECISION_REVIEW_HTTP_STATUS when the intake has no decision review") do
-      intake = create_intake[]
+    context "when asyncable status is submitted" do
+      let(:asyncable_status) { :submitted }
 
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status_for_new_intake).to eq(
-        Api::V3::DecisionReview::IntakeStatus::NO_DECISION_REVIEW_HTTP_STATUS
-      )
+      it("returns SUBMITTED_HTTP_STATUS") do
+        expect(subject.http_status_for_new_intake).to eq(
+          Api::V3::DecisionReview::IntakeStatus::SUBMITTED_HTTP_STATUS
+        )
+      end
     end
 
-    it("returns NOT_SUBMITTED_HTTP_STATUS_FOR_NEW_INTAKE if asyncable_status isn't :submitted") do
-      decision_review = create_higher_level_review
+    context "when asyncable status isn't :submitted" do
+      let(:asyncable_status) { "zebra" }
 
-      asyncable_status = "zebra"
-      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
-
-      intake = create_intake[decision_review]
-
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status_for_new_intake).to eq(
-        Api::V3::DecisionReview::IntakeStatus::NOT_SUBMITTED_HTTP_STATUS_FOR_NEW_INTAKE
-      )
+      it("returns NOT_SUBMITTED_HTTP_STATUS_FOR_NEW_INTAKE") do
+        expect(subject.http_status_for_new_intake).to eq(
+          Api::V3::DecisionReview::IntakeStatus::NOT_SUBMITTED_HTTP_STATUS_FOR_NEW_INTAKE
+        )
+      end
     end
 
-    it("returns SUBMITTED_HTTP_STATUS if asyncable_status is :submitted") do
-      decision_review = create_higher_level_review
+    context "when the intake doesn't have a decision review" do
+      let(:decision_review) { nil }
 
-      asyncable_status = :submitted
-      allow(decision_review).to receive(:asyncable_status) { asyncable_status }
-
-      intake = create_intake[decision_review]
-
-      intake_status = new_intake_status[intake]
-
-      expect(intake_status.http_status_for_new_intake).to eq(
-        Api::V3::DecisionReview::IntakeStatus::SUBMITTED_HTTP_STATUS
-      )
+      it("returns NO_DECISION_REVIEW_HTTP_STATUS") do
+        expect(subject.http_status_for_new_intake).to eq(
+          Api::V3::DecisionReview::IntakeStatus::NO_DECISION_REVIEW_HTTP_STATUS
+        )
+      end
     end
   end
 end
