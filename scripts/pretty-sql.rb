dff66ea760c34f2520cc2e75326b7f476dcdc5ff
Peter Karman <peter@truss.works>
2019-10-15 11:42:11 -0500

    lint

--- scripts/pretty-sql.rb
+++ scripts/pretty-sql.rb
@@ -2,23 +2,11 @@
 
 # bundle exec rails runner scripts/pretty-sql.rb
 
-require 'anbt-sql-formatter/formatter'
-
-def formatter
-  @formatter ||= build_formatter
-end
-
-def build_formatter
-  rule = AnbtSql::Rule.new
-  rule.keyword = AnbtSql::Rule::KEYWORD_UPPER_CASE
-  %w[count sum substr date].each { |func_name| rule.function_names << func_name.upcase }
-  rule.indent_string = '  '
-  AnbtSql::Formatter.new(rule)
-end
+require "anbt-sql-formatter/formatter"
 
 DRY_RUN = ARGV.include?("--dry_run")
 
-for file in ARGV.reject { |arg| arg =~ /^--/ }
+ARGV.reject { |arg| arg =~ /^--/ }.each do |file|
   puts file
   buf = File.read(file)
   pretty_sql = formatter.format(buf)
@@ -28,5 +16,19 @@ for file in ARGV.reject { |arg| arg =~ /^--/ }
     next
   end
 
-  File.open(file, 'w') { |f| f.puts pretty_sql }
+  File.open(file, "w") { |f| f.puts pretty_sql }
+end
+
+private
+
+def formatter
+  @formatter ||= build_formatter
+end
+
+def self.build_formatter
+  rule = AnbtSql::Rule.new
+  rule.keyword = AnbtSql::Rule::KEYWORD_UPPER_CASE
+  %w[count sum substr date].each { |func_name| rule.function_names << func_name.upcase }
+  rule.indent_string = "  "
+  AnbtSql::Formatter.new(rule)
 end
