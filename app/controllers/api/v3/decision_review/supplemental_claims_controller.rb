74651276a5dd621da983bb5715947e38cf5a2984
Ed Mangimelli <edmangimelli@gmail.com>
2019-10-16 02:28:42 -0500

    return :not_found for stub endpoints

--- app/controllers/api/v3/decision_review/supplemental_claims_controller.rb
+++ app/controllers/api/v3/decision_review/supplemental_claims_controller.rb
@@ -3,11 +3,11 @@
 class Api::V3::DecisionReview::SupplementalClaimsController < Api::V3::BaseController
   # stub
   def create
-    render json: {}, status: :ok
+    render json: {}, status: :not_found
   end
 
   # stub
   def show
-    render json: {}, status: :ok
+    render json: {}, status: :not_found
   end
 end
