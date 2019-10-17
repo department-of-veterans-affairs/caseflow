3080ee285efac3d79b49f3c13dfbf932a0cbd8df
Ferris Tseng <ferris@navapbc.com>
2019-10-17 15:25:09 -0400

    use Time.zone.today to avoid bugs

--- app/repositories/virtual_hearing_repository.rb
+++ app/repositories/virtual_hearing_repository.rb
@@ -7,13 +7,13 @@ class VirtualHearingRepository
         .where(hearing_type: Hearing.name)
         .joins("INNER JOIN hearings ON hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Date.today)
+        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
 
       virtual_hearings_for_legacy_hearings = VirtualHearing.eligible_for_deletion
         .where(hearing_type: LegacyHearing.name)
         .joins("INNER JOIN legacy_hearings ON legacy_hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = legacy_hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Date.today)
+        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
 
       virtual_hearings_for_ama_hearings + virtual_hearings_for_legacy_hearings
     end
