1a273d2dfffb8ad8d3230dd59babe556d0a69158
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-10-18 14:22:15 -0400

    write a fake file for pexip

---
+++ lib/fakes/pexip_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Fakes::PexipService < ExternalApi::PexipService
+  def initialize(*); end
+
+  def create_conference(**args)
+    binding.pry
+    return ExternalApi::PexipService::CreateResponse.new(HTTPI::Response.new(400, {}, {})) if args[:"error"]
+    { "conference_id": "9001" }
+  end
+
+  def delete_conference(**args)
+    ExternalApi::PexipService::DeleteResponse.new(HTTPI::Response.new(404, {}, {})) if args[:"error"] 
+  end
+end
