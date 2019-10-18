c6fe7ec4ce1fd89d45256598701be37276d3d9ce
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-10-18 14:21:58 -0400

    modify unit tests

--- spec/services/external_api/pexip_service_spec.rb
+++ spec/services/external_api/pexip_service_spec.rb
@@ -28,26 +28,82 @@ describe ExternalApi::PexipService do
       }
     end
 
+    subject { pexip_service.create_conference(host_pin: 1234, guest_pin: 5678, name: "1111111") }
+
+    let(:success_create_resp) { HTTPI::Response.new(201, { "Location" => "api/admin/configuration/v1/conference/1234" }, {}) }
+    let(:error_create_resp) { HTTPI::Response.new(400, {}, { "conference" => { "name" => ["Virtual room for this name already exist."] } }.to_json) }
+
+    # let(:error_create_resp) { HTTPI::Response.new(400, {}, { "conference" => { "name" => ["Virtual room for this name already exist."] } }.to_json) }
+
     it "calls #send_pexip_request" do
       expect(pexip_service).to receive(:send_pexip_request)
-      pexip_service.create_conference(1234, 5678, "1111111")
+      subject
     end
 
-    it "passed correct arguments to send_pexip_request" do
+    it "passed correct arguments to #send_pexip_request" do
       expect(pexip_service).to receive(:send_pexip_request).with(endpoint, :post, body: body)
-      pexip_service.create_conference(1234, 5678, "1111111")
+      subject
+    end
+
+    it "returns an instance of ExternalApi::PexipService::CreateResponse class" do
+      allow(pexip_service).to receive(:send_pexip_request).with(endpoint, :post, body: body)
+        .and_return(success_create_resp)
+
+      expect(subject).to be_instance_of(ExternalApi::PexipService::CreateResponse)
+    end
+
+    it "success response" do
+      allow(pexip_service).to receive(:send_pexip_request).with(endpoint, :post, body: body)
+        .and_return(success_create_resp)
+
+      expect(subject.code).to eq(201)
+      expect(subject.success?).to eq(true)
+      expect(subject.data).to eq({ "conference_id": "1234" })
+    end
+
+    it "error response" do
+      allow(pexip_service).to receive(:send_pexip_request).with(endpoint, :post, body: body)
+        .and_return(error_create_resp)
+
+      expect(subject.code).to eq(400)
+      expect(subject.success?).to eq(false)
+      expect(subject.error).to eq(Caseflow::Error::PexipBadRequestError.new(code: 400, message: "Virtual room for this name already exist."))
     end
   end
 
-  describe "#delete_conference" do
-    it "calls send_pexip_request" do
+  describe "#delete_conference", focus: true do
+    subject { pexip_service.delete_conference(conference_id: "123") }
+
+    let(:success_del_resp) { HTTPI::Response.new(204, {}, {}) }
+    let(:error_del_resp) { HTTPI::Response.new(404, {}, {}) }
+
+    it "calls #send_pexip_request" do
       expect(pexip_service).to receive(:send_pexip_request)
-      pexip_service.delete_conference(123)
+      subject
     end
 
-    it "passed correct arguments to send_pexip_request" do
+    it "passed correct arguments to #send_pexip_request" do
       expect(pexip_service).to receive(:send_pexip_request).with("#{endpoint}123/", :delete)
-      pexip_service.delete_conference(123)
+      subject
     end
+
+    it "success response" do
+      allow(pexip_service).to receive(:send_pexip_request).with("#{endpoint}123/", :delete)
+        .and_return(success_del_resp)
+
+      expect(subject.code).to eq(204)
+      expect(subject.success?).to eq(true)
+      expect(subject.data).to eq(nil)
+    end
+
+    it "error response" do
+      allow(pexip_service).to receive(:send_pexip_request).with("#{endpoint}123/", :delete)
+        .and_return(error_del_resp)
+
+      expect(subject.code).to eq(404)
+      expect(subject.success?).to eq(false)
+      expect(subject.error).to eq(Caseflow::Error::PexipNotFoundError.new(code: 404, message: "No error message"))
+    end
+
   end
 end
