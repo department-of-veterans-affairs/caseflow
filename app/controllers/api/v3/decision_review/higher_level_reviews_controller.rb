58633939ca6e053784b9bf5285f3c31edc992d86
Philip Becker <philip.becker@oddball.io>
2019-10-21 14:51:42 -0400

    formatting fixes

--- app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
+++ app/controllers/api/v3/decision_review/higher_level_reviews_controller.rb
@@ -29,8 +29,6 @@ class Api::V3::DecisionReview::HigherLevelReviewsController < Api::V3::BaseContr
     higher_level_review = HigherLevelReview.find_by_uuid(params[:id])
     options = {include: [:veteran, :claimant, :request_issues]}
     render json: Api::V3::HigherLevelReviewSerializer.new(higher_level_review, options)
-  # rescue StandardError => error
-  #   byebug
   end
 
   private
