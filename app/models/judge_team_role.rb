cedbc4fe1fcd47f529a928254a13f4108989a5b8
lowellrex <lowell@navahq.com>
2019-10-22 12:27:52 -0400

    More rubocop fixes

--- app/models/judge_team_role.rb
+++ app/models/judge_team_role.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class JudgeTeamRole < ApplicationRecord
-  belongs_to :organizations_user, class_name: ::OrganizationsUser.name
+  belongs_to :organizations_user, class_name: "::OrganizationsUser"
 
   has_one :user, through: :organizations_user
   has_one :organization, through: :organizations_user
