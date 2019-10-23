7c07c49d03f9d6f328737500ed871a89cd0150c0
JC Quirin <jcq@jcq.local>
2019-10-23 15:34:23 -0400

    added fetchTaskDataForOrg fn to OrganizationDashboardView

--- client/app/queue/dashboard/OrganizationDashboardView.jsx
+++ client/app/queue/dashboard/OrganizationDashboardView.jsx
@@ -3,17 +3,33 @@ import PropTypes from 'prop-types';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import ApiUtil from '../../util/ApiUtil';
 
-const loadOrganization = async (orgUrl) => {
+const fetchOrganization = async (orgUrl) => {
   const { body: org } = await ApiUtil.get(`/organizations/${orgUrl}`);
 
   return org;
 };
 
+const fetchTaskDataForOrg = async (orgId) => {
+  const { body } = await ApiUtil.get('/tasks/visualization', { query: { organization_id: orgId } });
+
+  return body.tasks;
+};
+
 export const OrganizationDashboardView = ({ organization: orgUrl }) => {
   const [org, setOrg] = useState({});
+  const [taskData, setTaskData] = useState([]);
 
   useEffect(() => {
-    loadOrganization(orgUrl).then((res) => setOrg(res));
+    async function fetchData() {
+      const organization = await fetchOrganization(orgUrl);
+
+      setOrg(organization);
+
+      const res = await fetchTaskDataForOrg(organization.id);
+
+      setTaskData(res);
+    }
+    fetchData();
   }, [orgUrl]);
 
   return <AppSegment filledBackground>{org && <h1>{org.name} Dashboard</h1>}</AppSegment>;
