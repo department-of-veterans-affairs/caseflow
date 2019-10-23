1db305944bde22a5b917e91be2e90e8a556a1f71
Philip Becker <philip.becker@oddball.io>
2019-10-23 11:51:44 -0400

    remove rubocop suggestion from serializer

--- app/serializers/api/v3/higher_level_review_serializer.rb
+++ app/serializers/api/v3/higher_level_review_serializer.rb
@@ -8,22 +8,30 @@ class Api::V3::HigherLevelReviewSerializer
   self.record_type = "HigherLevelReview"
 
   attribute :status, &:fetch_status
-
   attributes :aoj, :description, :benefit_type, :receipt_date, :informal_conference,
              :same_office, :legacy_opt_in_approved, :alerts, :events
   attribute :program_area, &:program
 
-  has_one :veteran, &:veteran
+  # disable SymbolProc because these relationships aren't Rails relationships
+  # rubocop:disable Style/SymbolProc
+
+  has_one :veteran do |object|
+    object.veteran
+  end
 
   has_one :claimant do |object|
     object.claimants.first
   end
 
-  has_many :decision_issues, &:fetch_all_decision_issues
+  has_many :decision_issues do |object|
+    object.fetch_all_decision_issues
+  end
 
   has_many :request_issues do |object|
     object.request_issues.includes(
       :decision_review, :contested_decision_issue
     ).active_or_ineligible_or_withdrawn
   end
+  # rubocop:enable Style/SymbolProc
+
 end
