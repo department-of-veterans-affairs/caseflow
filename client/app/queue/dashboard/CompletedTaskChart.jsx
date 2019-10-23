89d717a075571813c06032a5fb522c1ae92a419c
Alsia Plybeah <alsia@navapbc.com>
2019-10-23 15:39:14 -0400

    create chart component

---
+++ client/app/queue/dashboard/CompletedTaskChart.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import PropTypes from 'prop-types';
+
+const CompletedTaskChart = ({ data/* Data to be passed into the graph goes here */ }) => (
+  <ResponsiveLine
+    data={data}
+    xScale= {{ type: 'point' }}
+    yScale= {{ type: 'linear',
+      min: 'auto',
+      max: 'auto' }}
+    axisBottom={{
+      orient: 'bottom',
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Date',
+      legendOffset: 36,
+      legendPosition: 'middle'
+    }}
+    axisLeft={{
+      orient: 'left',
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Completed Tasks',
+      legendOffset: -40,
+      legendPosition: 'middle'
+    }}
+    pointSize={10}
+    pointBorderWidth={2}
+    pointLabel="y"
+    pointLabelYOffset={-12}
+    useMesh
+    animate
+  />
+);
+
+export default CompletedTaskChart;
+
+CompletedTaskChart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    date: PropTypes.date,
+    taskComplete: PropTypes.bool
+  }))
+};
