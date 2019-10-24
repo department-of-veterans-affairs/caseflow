17ef0f4217acca8f90de6e37b88cce2cf3d184a9
Alsia Plybeah <alsia@navapbc.com>
2019-10-24 15:48:45 -0400

    adjust margin of chart

--- client/app/queue/dashboard/CompletedTaskChart.jsx
+++ client/app/queue/dashboard/CompletedTaskChart.jsx
@@ -3,38 +3,39 @@ import { ResponsiveLine } from '@nivo/line';
 import PropTypes from 'prop-types';
 
 const CompletedTaskChart = ({ data/* Data to be passed into the graph goes here */ }) => (
-  <div style={{height: 200}}>
-  <ResponsiveLine
-    data={data}
-    xScale= {{ type: 'point' }}
-    yScale= {{ type: 'linear',
-      min: 'auto',
-      max: 'auto' }}
-    axisBottom={{
-      orient: 'bottom',
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'Date',
-      legendOffset: 36,
-      legendPosition: 'middle'
-    }}
-    axisLeft={{
-      orient: 'left',
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'Completed Tasks',
-      legendOffset: -40,
-      legendPosition: 'middle'
-    }}
-    pointSize={10}
-    pointBorderWidth={2}
-    pointLabel="y"
-    pointLabelYOffset={-12}
-    useMesh
-    animate
-  />
+  <div style={{ height: 350 }}>
+    <ResponsiveLine
+      data={data}
+      xScale= {{ type: 'point' }}
+      yScale= {{ type: 'linear',
+        min: 'auto',
+        max: 'auto' }}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      axisBottom={{
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Date',
+        legendOffset: -10,
+        legendPosition: 'middle'
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Completed Tasks',
+        legendOffset: 40,
+        legendPosition: 'middle'
+      }}
+      pointSize={10}
+      pointBorderWidth={2}
+      pointLabel="y"
+      pointLabelYOffset={-12}
+      useMesh
+      animate
+    />
   </div>
 );
 
