17cb35b9d96f574604f578c2c6392265ad8e3e24
hschallhorn <hschallhorn@tistatech.com>
2019-10-28 14:07:19 -0400

    Only reopen judge assign tasks if attorney tasks are cancelled

--- spec/models/tasks/attorney_quality_review_task_spec.rb
+++ spec/models/tasks/attorney_quality_review_task_spec.rb
@@ -16,4 +16,23 @@ describe AttorneyQualityReviewTask do
       )
     end
   end
+
+  fcontext "when cancelling the task" do
+    let(:atty) { create(:user) }
+    let(:judge) { create(:user) }
+    let!(:atty_staff) { create(:staff, :attorney_role, sdomainid: atty.css_id) }
+    let!(:judge_staff) { create(:staff, :judge_role, sdomainid: judge.css_id) }
+    let(:parent) { create(:ama_judge_dispatch_return_task) }
+    let!(:task) do
+      create(:ama_judge_dispatch_return_to_attorney_task, assigned_by: judge, assigned_to: atty, parent: parent)
+    end
+
+    subject { task.update!(status: Constants.TASK_STATUSES.cancelled) }
+
+    it "cancels the task and moves the parent back to the judge's queue" do
+      expect(subject).to be true
+      expect(task.reload.status).to eq Constants.TASK_STATUSES.cancelled
+      expect(parent.reload.status).to eq Constants.TASK_STATUSES.assigned
+    end
+  end
 end
