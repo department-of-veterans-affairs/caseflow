ce9b7f2a59807884549aa3bb81fe452682252084
Philip Becker <philip.becker@oddball.io>
2019-10-28 18:05:30 -0400

    validate receipt date

--- app/controllers/api/v3/decision_review/issues_controller.rb
+++ app/controllers/api/v3/decision_review/issues_controller.rb
@@ -2,6 +2,7 @@
 
 class Api::V3::DecisionReview::IssuesController < Api::V3::BaseController
   def index
+    # REVIEW move these before filters?
     veteran = Veteran.find_by_file_number(request.headers['veteranId'])
     unless veteran
       render_error(
@@ -11,7 +12,16 @@ class Api::V3::DecisionReview::IssuesController < Api::V3::BaseController
       )
       return
     end
-    # receipt_date = request.headers['receiptDate']
+
+    receipt_date = Date.parse(request.headers['receiptDate'])
+    if receipt_date < Constants::DATES["AMA_ACTIVATION"].to_date || Date.today < receipt_date
+      render_error(
+        status: 422,
+        code: :bad_receipt_date,
+        title: "Bad receipt date"
+      )
+      return
+    end
 
     # successfully render issues
     render plain: 'so many issues'
