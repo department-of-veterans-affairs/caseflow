ec458ea8de6c9cef8d2d4e617ab279f40013d9ec
lowellrex <lowell@navahq.com>
2019-10-29 10:48:17 -0400

    Add link to where I found this feature

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,6 +49,7 @@ RSpec.configure do |config|
      ENV["REACT_ON_RAILS_ENV"] != "HOT"
 
     # Only compile webpack-bundle.js for feature tests.
+    # https://github.com/shakacode/react_on_rails/blob/master/docs/basics/rspec-configuration.md#rspec-configuration
     ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config, :requires_webpack_assets)
     config.define_derived_metadata(file_path: %r{spec/feature}) do |metadata|
       metadata[:requires_webpack_assets] = true
