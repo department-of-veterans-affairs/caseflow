6d2dc0d71b1f06f29e494374c6a5e1dfe3ffeffc
Philip Becker <philip.becker@oddball.io>
2019-10-29 17:54:08 -0400

    use better variable names

--- app/serializers/api/v3/higher_level_review_serializer.rb
+++ app/serializers/api/v3/higher_level_review_serializer.rb
@@ -15,20 +15,20 @@ class Api::V3::HigherLevelReviewSerializer
   # disable SymbolProc because these relationships aren't Rails relationships
   # rubocop:disable Style/SymbolProc
 
-  has_one :veteran do |object|
-    object.veteran
+  has_one :veteran do |higher_level_review|
+    higher_level_review.veteran
   end
 
-  has_one :claimant do |object|
-    object.claimants.first
+  has_one :claimant do |higher_level_review|
+    higher_level_review.claimants.first
   end
 
-  has_many :decision_issues do |object|
-    object.fetch_all_decision_issues
+  has_many :decision_issues do |higher_level_review|
+    higher_level_review.fetch_all_decision_issues
   end
 
-  has_many :request_issues do |object|
-    object.request_issues.includes(
+  has_many :request_issues do |higher_level_review|
+    higher_level_review.request_issues.includes(
       :decision_review, :contested_decision_issue
     ).active_or_ineligible_or_withdrawn
   end
--- app/serializers/api/v3/request_issue_serializer.rb
+++ app/serializers/api/v3/request_issue_serializer.rb
@@ -11,8 +11,8 @@ class Api::V3::RequestIssueSerializer
              :ineligible_due_to_id, :withdrawal_date, :contested_issue_description,
              :end_product_code, :title_of_active_review
 
-  attribute :active do |object|
-    object.closed_at.nil? && object.ineligible_reason.nil?
+  attribute :active do |request_issue|
+    request_issue.closed_at.nil? && request_issue.ineligible_reason.nil?
   end
 
   attribute :status_description, &:api_status_description
@@ -25,7 +25,7 @@ class Api::V3::RequestIssueSerializer
   attribute :legacy_appeal_issue_id, &:vacols_sequence_id
   attribute :decision_review_title, &:review_title
   attribute :decision_issue_id, &:contested_decision_issue_id
-  attribute :end_product_cleared do |object|
-    object.end_product_establishment&.status_cleared?
+  attribute :end_product_cleared do |request_issue|
+    request_issue.end_product_establishment&.status_cleared?
   end
 end
