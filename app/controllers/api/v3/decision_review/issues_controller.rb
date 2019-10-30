ec6f9d37026b0ce400cf99f8f7799faf1cb56b5a
Philip Becker <philip.becker@oddball.io>
2019-10-30 17:58:06 -0400

    return issues json

--- app/controllers/api/v3/decision_review/issues_controller.rb
+++ app/controllers/api/v3/decision_review/issues_controller.rb
@@ -26,8 +26,7 @@ class Api::V3::DecisionReview::IssuesController < Api::V3::BaseController
     # going to have to do this for each type
     standin_claim_review = HigherLevelReview.new(veteran_file_number: veteran.file_number, receipt_date: receipt_date)
     issues = ContestableIssueGenerator.new(standin_claim_review).contestable_issues
-    # TODO render issues as JSONAPI elements
-    # successfully render issues
-    render plain: "Found #{issues.count} Issues"
+    # TODO render issues as JSONAPI objects
+    render json: issues
   end
 end
