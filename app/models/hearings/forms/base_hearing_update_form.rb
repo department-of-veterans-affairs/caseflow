e8cc6b8a1e3adb828323f20ba91292b7a30c3287
Ferris Tseng <ferris@navapbc.com>
2019-10-30 14:02:08 -0400

    implement update logic, and start thinking about how to change the hearing request type

--- app/models/hearings/forms/base_hearing_update_form.rb
+++ app/models/hearings/forms/base_hearing_update_form.rb
@@ -13,7 +13,11 @@ class BaseHearingUpdateForm
   def update
     update_hearing
     update_advance_on_docket_motion unless advance_on_docket_motion_attributes.nil?
-    create_virtual_hearing unless virtual_hearing_attributes.nil?
+    
+    if not virtual_hearing_attributes.nil?
+      create_or_update_virtual_hearing
+      # TODO Start the job to create the Pexip conference here?
+    end
   end
 
   protected
@@ -29,16 +33,42 @@ class BaseHearingUpdateForm
     motion.update(advance_on_docket_motion_attributes)
   end
 
-  def create_virtual_hearing
+  def create_or_update_virtual_hearing
     created = false
 
-    VirtualHearing.where.not(status: :cancelled).find_or_create_by!(hearing: hearing) do |virtual_hearing|
-      virtual_hearing.veteran_email = virtual_hearing_attributes[:veteran_email]
-      virtual_hearing.judge_email = virtual_hearing_attributes[:judge_email]
-      virtual_hearing.representative_email = virtual_hearing_attributes[:representative_email]
-      created = true
-    end
+    ActiveRecord::Base.transaction do
+      if hearing.request_type == HearingDay::REQUEST_TYPES[:central]
+        # Converting from a central hearing to a virtual hearing
+        # How to update the request type?
+        #   * Get the hearing day,
+        #   * Duplicate the hearing day
+        #   * Check if a duplicate already exists
+        #   * Update the request type
+      end
 
-    fail ActiveRecord::RecordNotUnique if !created
+      scope = VirtualHearing.where.not(status: :cancelled)
+
+      # TODO All of this is not atomic :(. Revisit later, since Rails 6 offers an upsert.
+      virtual_hearing = scope.find_or_create_by!(hearing: hearing) do |virtual_hearing|
+        virtual_hearing.veteran_email = virtual_hearing_attributes[:veteran_email]
+        virtual_hearing.judge_email = virtual_hearing_attributes[:judge_email]
+        virtual_hearing.representative_email = virtual_hearing_attributes[:representative_email]
+        created = true
+      end
+
+      if not created
+        # Don't update these fields if the corresponding email address was not
+        # included in the updates.
+        emails_sent_updates = {
+          veteran_email_sent: !virtual_hearing_attributes.key?(:veteran_email),
+          judge_email_sent: !virtual_hearing_attributes.key?(:judge_email),
+          representative_email_sent: !virtual_hearing_attributes.key?(:representative_email)
+        }.reject { |_k, v| v == true }
+
+        updates = virtual_hearing_attributes.compact.merge(emails_sent_updates)
+
+        virtual_hearing.update(updates)
+      end
+    end
   end
 end
