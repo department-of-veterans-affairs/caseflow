084d18aa76d5a5d89a79db40b49d17929469f9f5
Ferris Tseng <ferris@navapbc.com>
2019-10-31 17:43:05 -0400

    start implementing the actual deleting

--- app/jobs/virtual_hearings/delete_conferences_job.rb
+++ app/jobs/virtual_hearings/delete_conferences_job.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
-class DeleteConferencesJob < ApplicationJob
+class VirtualHearings::DeleteConferencesJob < ApplicationJob
   queue_with_priority :low_priority
   application_attr :hearing_schedule
 
   def perform
-    puts "Hyello"
+    VirtualHearingRepository.ready_for_deletion.map do |virtual_hearing|
+      delete_conference(virtual_hearing)
+    end
   end
 
   private
 
-  def delete_conference(conference)
+  def pexip_service
+    @service ||= PexipService.new(
+      host: ENV["PEXIP_MANAGEMENT_NODE_HOST"],
+      port: ENV["PEXIP_MANAGEMENT_NODE_PORT"],
+      user_name: ENV["PEXIP_USERNAME"],
+      password: ENV["PEXIP_PASSWORD"],
+      client_host: ENV["PEXIP_CLIENT_HOST"]
+    )
+  end
 
-  rescue
-
+  def delete_conference(virtual_hearing)
+    service.delete_conference(conference_id: virtual_hearing.conference_id)
+  rescue PexipNotFoundError
+    virtual_hearing.conference_deleted = true
+  rescue PexipApiError
+    virtual_hearing.conference_deleted = true
   end
 end
