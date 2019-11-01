82cb7ac39d8578c0405e69fa1c10ab0ff488d00a
Ferris Tseng <ferris@navapbc.com>
2019-11-01 11:02:45 -0400

    more fleshing out of deleting a virtual hearing

--- app/jobs/virtual_hearings/delete_conferences_job.rb
+++ app/jobs/virtual_hearings/delete_conferences_job.rb
@@ -7,6 +7,10 @@ class VirtualHearings::DeleteConferencesJob < ApplicationJob
   def perform
     VirtualHearingRepository.ready_for_deletion.map do |virtual_hearing|
       delete_conference(virtual_hearing)
+
+      send_cancellation_emails if virtual_hearing.cancelled?
+
+      virtual_hearing.save!
     end
   end
 
@@ -23,10 +27,37 @@ class VirtualHearings::DeleteConferencesJob < ApplicationJob
   end
 
   def delete_conference(virtual_hearing)
-    service.delete_conference(conference_id: virtual_hearing.conference_id)
+    pexip_service.delete_conference(conference_id: virtual_hearing.conference_id)
   rescue PexipNotFoundError
+    # Assume the conference was already deleted if it's no longer in Pexip.
     virtual_hearing.conference_deleted = true
-  rescue PexipApiError
-    virtual_hearing.conference_deleted = true
+  rescue PexipApiError => error
+    Rails.logger.error("Failed to delete conference from Pexip with error: #{error}")
+
+    capture_exception(
+      error: error,
+      extra: {
+        hearing_id: virtual_hearing.hearing_id,
+        virtual_hearing_id: virtual_hearing.id,
+        pexip_conference_Id: virtual_hearing.conference_id
+      }
+    )
+  end
+
+  def send_cancellation_emails(virtual_hearing)
+    if !virtual_hearing.veteran_email_sent
+      # TODO: Send the email
+      virtual_hearing.veteran_email_sent = true
+    end
+
+    if !virtual_hearing.judge_email_sent
+      # TODO: Send the email
+      virtual_hearing.judge_email_sent = true
+    end
+
+    if !virtual_hearing.representative_email.nil? && !virtual_hearing.representative_email_sent
+      # TODO: Send the email
+      virtual_hearing.representative_email_sent = true
+    end
   end
 end
