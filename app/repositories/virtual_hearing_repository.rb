1a648ab1908f5c11648e18b23b6467690f81d8d2
Ferris Tseng <ferris@navapbc.com>
2019-11-01 10:35:48 -0400

    update virtual hearing repository ready_for_deletion to always return cancelled hearings

--- app/repositories/virtual_hearing_repository.rb
+++ app/repositories/virtual_hearing_repository.rb
@@ -7,13 +7,21 @@ class VirtualHearingRepository
         .where(hearing_type: Hearing.name)
         .joins("INNER JOIN hearings ON hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
+        .where(
+          "hearing_days.scheduled_for < :today OR virtual_hearings.status = :status",
+          today: Time.zone.today,
+          status: :cancelled
+        )
 
       virtual_hearings_for_legacy_hearings = VirtualHearing.eligible_for_deletion
         .where(hearing_type: LegacyHearing.name)
         .joins("INNER JOIN legacy_hearings ON legacy_hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = legacy_hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
+        .where(
+          "hearing_days.scheduled_for < :today OR virtual_hearings.status = :status",
+          today: Time.zone.today,
+          status: :cancelled
+        )
 
       virtual_hearings_for_ama_hearings + virtual_hearings_for_legacy_hearings
     end
