6dd7954e028ff3575d70fe16eef83201d0feaf80
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-11-07 14:05:08 -0500

    Rubaiyat/minor changes to create conference job (#12639)
    - few non blocking changes to create conference job

--- app/jobs/virtual_hearings/pexip_client.rb
+++ app/jobs/virtual_hearings/pexip_client.rb
@@ -3,11 +3,11 @@
 module VirtualHearings::PexipClient
   def client
     @client ||= PexipService.new(
-      host: ENV["PEXIP_API_HOST"],
-      port: ENV["PEXIP_API_PORT"],
-      user_name: ENV["PEXIP_API_USERNANME"],
-      password: ENV["PEXIP_API_PASSWORD"],
-      client_host: ENV["PEXIP_API_CLIENT"]
+      host: ENV["PEXIP_MANAGEMENT_NODE_HOST"],
+      port: ENV["PEXIP_MANAGEMENT_NODE_PORT"],
+      user_name: ENV["PEXIP_USERNAME"],
+      password: ENV["PEXIP_PASSWORD"],
+      client_host: ENV["PEXIP_CLIENT_HOST"]
     )
   end
 end
