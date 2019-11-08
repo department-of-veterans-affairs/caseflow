bb017034688c2277b51602dc59724ac4b4deb851
Ferris Tseng <ferris@navapbc.com>
2019-11-08 13:14:28 -0500

    add another hearing range test

--- spec/controllers/hearings/hearing_day_controller_spec.rb
+++ spec/controllers/hearings/hearing_day_controller_spec.rb
@@ -44,5 +44,18 @@ describe Hearings::HearingDayController, :all_dbs do
         expect(hearing_days["hearings"][0]["id"]).to eq hearing_day.id
       end
     end
+
+    context "with one hearing day outside of date range" do
+      let!(:hearing_day) do
+        create(:hearing_day, scheduled_for: Time.zone.now.to_date)
+      end
+      let(:params) { { start_date: Time.zone.now.to_date + 2.days } }
+
+      it "returns 200 and no hearing days", :aggregate_failures do
+        expect(subject.status).to eq 200
+        hearing_days = JSON.parse(subject.body)
+        expect(hearing_days["hearings"].size).to eq 0
+      end
+    end
   end
 end
