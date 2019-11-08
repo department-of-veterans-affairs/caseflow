d3f4bf80f2e35353e20f72cd47f48d3bd03771fd
Ferris Tseng <ferris@navapbc.com>
2019-11-08 17:50:09 -0500

    Clean Virtual Hearing Conferences Job (#12550)
    Resolves #12188 
    
    ### Description
    
      * Add job to delete virtual hearings from Pexip if a virtual hearing is completed or cancelled
      * Add tests around the job

--- app/repositories/virtual_hearing_repository.rb
+++ app/repositories/virtual_hearing_repository.rb
@@ -7,15 +7,38 @@ class VirtualHearingRepository
         .where(hearing_type: Hearing.name)
         .joins("INNER JOIN hearings ON hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
+        .where(
+          "hearing_days.scheduled_for < :today OR virtual_hearings.status = :status",
+          today: Time.zone.today,
+          status: :cancelled
+        )
 
       virtual_hearings_for_legacy_hearings = VirtualHearing.eligible_for_deletion
         .where(hearing_type: LegacyHearing.name)
         .joins("INNER JOIN legacy_hearings ON legacy_hearings.id = virtual_hearings.hearing_id")
         .joins("INNER JOIN hearing_days ON hearing_days.id = legacy_hearings.hearing_day_id")
-        .where("hearing_days.scheduled_for < :today", today: Time.zone.today)
+        .where(
+          "hearing_days.scheduled_for < :today OR virtual_hearings.status = :status",
+          today: Time.zone.today,
+          status: :cancelled
+        )
 
       virtual_hearings_for_ama_hearings + virtual_hearings_for_legacy_hearings
+    end
+
+    def cancelled_hearings_with_pending_emails
+      VirtualHearing
+        .cancelled
+        .where(<<-SQL, false, false, false)
+          (
+            virtual_hearings.judge_email_sent = ?
+            OR virtual_hearings.veteran_email_sent = ?
+            OR (
+              NOT virtual_hearings.representative_email = null
+              AND virtual_hearings.representative_email = ?
+            )
+          )
+        SQL
     end
   end
 end
