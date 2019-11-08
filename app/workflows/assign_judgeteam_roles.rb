ecc16d1f80b885f4808dfa5f50fda65708bb3175
Kat Tipton <kat@navapbc.com>
2019-11-08 12:11:57 -0500

    rubocop

--- app/workflows/assign_judgeteam_roles.rb
+++ app/workflows/assign_judgeteam_roles.rb
@@ -18,37 +18,59 @@ class AssignJudgeteamRoles
       end
 
       admins = judge_team.admins
-
-      if admins.empty?
-        warn "Judge Team ID #{judge_team.id} has no admin members. Requires manual cleanup. Not assigning roles to team."
-        next
+      if process_admins(admins: admins, judge_team: judge_team)
+        nonadmins = judge_team.attorneys
+        process_nonadmins(nonadmins: nonadmins, judge_team: judge_team)
       end
+    end
+  end
 
-      if admins.count > 1
-        warn "Judge Team ID #{judge_team.id} has multiple admin members. Requires manual cleanup. Not assigning roles to team."
-        next
-      end
+  private
 
-      admins.each do |admin|
-        message =  "#{admin.css_id} JudgeTeamLead of #{judge_team.name}"
-        if @dry_run
-          puts "Would make #{message}"
-        else
-          org_user = OrganizationsUser.existing_record(admin, judge_team)
-          JudgeTeamLead.find_or_create_by(organizations_user: org_user)
-          Rails.logger.info("Setting #{message}")
-        end
+  def process_admins(admins, judge_team)
+    if incorrect_admin_count?(admins.count)
+      return false
+    end
+
+    admins.each do |admin|
+      message = "#{admin.css_id} JudgeTeamLead of #{judge_team.name}"
+      if @dry_run
+        warn "Would make #{message}"
+      else
+        org_user = OrganizationsUser.existing_record(admin, judge_team)
+        JudgeTeamLead.find_or_create_by(organizations_user: org_user)
+        Rails.logger.info("Setting #{message}")
       end
-      nonadmins = judge_team.attorneys
-      nonadmins.each do |atty|
-        message = "#{atty.css_id} DecisionDraftingAttorney of #{judge_team.name}"
-        if @dry_run
-          puts "Would make #{message}"
-        else
-          org_user = OrganizationsUser.existing_record(atty, judge_team)
-          DecisionDraftingAttorney.find_or_create_by(organizations_user: org_user)
-          Rails.logger.info("Setting #{message}")
-        end
+    end
+
+    true
+  end
+
+  def incorrect_admin_count?(_admin_count)
+    if admins.empty?
+      warn "Judge Team ID #{judge_team.id} has no admin members. Requires manual cleanup. \
+        Not assigning roles to team."
+      return true
+    end
+
+    if admins.count > 1
+      warn "Judge Team ID #{judge_team.id} has multiple admin members. Requires manual cleanup. \
+        Not assigning roles to team."
+      return true
+    end
+
+    false
+  end
+
+  def process_nonadmins(nonadmins, judge_team)
+    nonadmins.each do |atty|
+      message = "#{atty.css_id} DecisionDraftingAttorney of #{judge_team.name}"
+      if @dry_run
+        warn "Would make #{message}"
+      else
+        org_user = OrganizationsUser.existing_record(atty, judge_team)
+        DecisionDraftingAttorney.find_or_create_by(organizations_user: org_user)
+        Rails.logger.info("Setting #{message}")
       end
     end
   end
