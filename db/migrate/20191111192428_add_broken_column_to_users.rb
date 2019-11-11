ba7eaf7edf01f484de492db6bdb1f3368ada3431
Yang Yang <yangyang@navapbc.com>
2019-11-11 15:34:38 -0500

    Revert "Test CI migration check with a broken migration"
    This reverts commit eaad9e1f4048a2453a3a05c11b1ec4c5453b7af8.

--- db/migrate/20191111192428_add_broken_column_to_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddBrokenColumnToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :broken_column, :strong, comment: "This line works unless a typo somehow sneaks in."
-  end
-end
