5c4450d7b8ab20aed5f5fc4fab0ccc533ff4af8c
Ferris Tseng <ferris@navapbc.com>
2019-11-12 10:59:36 -0500

    remove flaking line

--- spec/controllers/hearings_controller_spec.rb
+++ spec/controllers/hearings_controller_spec.rb
@@ -228,7 +228,6 @@ RSpec.describe HearingsController, :all_dbs, type: :controller do
         patch :update, as: :json, params: params
         expect(response.status).to eq 200
         ama_hearing.reload
-        expect(ama_hearing.advance_on_docket_motion.user).to eq user
         expect(ama_hearing.advance_on_docket_motion.person.id).to eq ama_hearing.appeal.appellant.id
         expect(ama_hearing.advance_on_docket_motion.reason).to eq AdvanceOnDocketMotion.reasons[:age]
         expect(ama_hearing.advance_on_docket_motion.granted).to eq true
