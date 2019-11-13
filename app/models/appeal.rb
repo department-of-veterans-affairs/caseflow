679a0a75daef44e5645ac52e050a8f8c6ffd0f9d
Peter Karman <peter@truss.works>
2019-11-13 14:18:48 -0600

    fix name

--- app/models/appeal.rb
+++ app/models/appeal.rb
@@ -245,7 +245,7 @@ class Appeal < DecisionReview
   end
 
   def status
-    AppealStatus.new(appeal: self)
+    BVAAppealStatus.new(appeal: self)
   end
 
   def previously_selected_for_quality_review
