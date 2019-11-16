b44e522e761b905e5d694f9c5b31187f2d09b8ae
Sally Maki <sallymaki@gmail.com>
2019-11-15 17:38:38 -0800

    Do not save two claimants during intake, use last claimant if there are two (#12588)
    * Use most recent claimant selection for decision reviews
    
    * Refactor to define claimant on decision review, remove instances of multiple ama claimants in tests
    
    * Fix syntax error, add feature test.
    
    * Test for bug fix passing for all decision review types
    
    * Clean up, update a couple of comments
    
    * Update more spots referencing claimants, force boolean on veteran_is_not_claimant in serializers
    
    * Test regressions, code climate
    
    * Clean up
    
    * Add reload to tests
    
    * Change which database cleaner is in new spec file

--- spec/feature/hearings/daily_docket/build_hearsched_spec.rb
+++ spec/feature/hearings/daily_docket/build_hearsched_spec.rb
@@ -3,7 +3,7 @@
 require "support/vacols_database_cleaner"
 require "rails_helper"
 
-RSpec.feature "Hearing Schedule Daily Docket for Build HearSched", :all_dbs do
+feature "Hearing Schedule Daily Docket for Build HearSched", :all_dbs do
   let!(:actcode) { create(:actcode, actckey: "B", actcdtc: "30", actadusr: "SBARTELL", acspare1: "59") }
   let!(:current_user) { User.authenticate!(css_id: "BVATWARNER", roles: ["Build HearSched"]) }
 
