69b798fb89da23eb6b56567a22ecee7920e0c6d7
Yang Yang <yangyang@navapbc.com>
2019-11-19 17:53:23 -0500

    Handle job failures after 24 hours with message to user

--- app/services/asyncable_job_messaging.rb
+++ app/services/asyncable_job_messaging.rb
@@ -3,6 +3,9 @@
 # AsyncableJobMessaging handles the decision-making around how and whether events in the
 # lifecycle of an async job triggers sending a message to the user's inbox.
 class AsyncableJobMessaging
+
+  FAILING_TIME_BEFORE_MESSAGING = 1.days.freeze
+
   attr_reader :job, :current_user
 
   def initialize(job:, current_user: nil)
@@ -26,10 +29,31 @@ class AsyncableJobMessaging
         Message.create!(
           detail: job_note,
           text: message_text,
-          user: job.asyncable_user
+          user: job.asyncable_user,
+          message_type: "job_note_added"
         )
       end
       job_note
     end
+  end
+
+  def handle_job_failure(err:)
+    return unless job.asyncable_user
+    return unless job.submitted?
+    return unless Time.zone.now - job[job.class.submitted_at_column] >= FAILING_TIME_BEFORE_MESSAGING
+    return if Message.where(detail: job, message_type: "job_failing").any?
+
+    if err.length > 80
+      err = err[0..80] + "..."
+    end
+    message_text = <<-EOS.strip_heredoc
+      The Intake processing job for <a href="#{job.path}">#{job.class} #{job.id}</a> was unable to complete because of an error: #{err}
+    EOS
+    Message.create!(
+      detail: job,
+      text: message_text,
+      user: job.asyncable_user,
+      message_type: "job_failing"
+    )
   end
 end
--- spec/services/asyncable_job_messaging.rb
+++ spec/services/asyncable_job_messaging.rb
@@ -34,4 +34,43 @@ describe AsyncableJobMessaging, :postgres do
       end
     end
   end
+
+  describe "#handle_job_failure" do
+    let(:owner) { create(:default_user) }
+    let(:job) { create(:higher_level_review, :requires_processing, intake: create(:intake, user: owner)) }
+    let(:messaging) { AsyncableJobMessaging.new(job: job) }
+
+    subject { messaging.handle_job_failure(err: "sample error text") }
+
+    context "when a failure happens within 24 hours" do
+      it "doesn't send a message to the job owner" do
+        message_count = owner.messages.count
+        subject
+        expect(owner.messages.count).to eq message_count
+      end
+    end
+
+    context "when a failure first happens after 24 hours" do
+      it "sends a message to the job owner" do
+        job
+        Timecop.travel(Time.zone.now.tomorrow)
+        message_count = owner.messages.count
+        subject
+        expect(owner.messages.count).to eq message_count + 1
+        expect(owner.messages.last.text).to match("unable to complete because of an error")
+        expect(owner.messages.last.text).to match(job.path)
+      end
+    end
+
+    context "when multiple failures happen after 24 hours" do
+      it "sends only one message to the job owner" do
+        job
+        Timecop.travel(Time.zone.now.tomorrow)
+        message_count = owner.messages.count
+        3.times { messaging.handle_job_failure(err: "sample error text") }
+        expect(owner.messages.count).to eq message_count + 1
+      end
+    end
+
+  end
 end
