b465e9b2499f183e2fdd0fe98ce91c9e69188d7d
Peter Karman <peter@truss.works>
2019-11-19 14:47:12 -0600

    Add remaining appeals columns and indices

--- db/etl/migrate/20191111164808_create_etl_appeals.rb
+++ db/etl/migrate/20191111164808_create_etl_appeals.rb
@@ -1,7 +1,8 @@
 class CreateEtlAppeals < ActiveRecord::Migration[5.1]
   def change
-    create_table :appeals do |t|
+    create_table :appeals, comment: "Denormalized BVA NODs" do |t|
       t.timestamps null: false, comment: "Default created_at/updated_at for the ETL record"
+
       t.integer "appeal_id", null: false, comment: "ID of the Appeal"
       t.string "docket_number", null: false, limit: 50, comment: "Docket number"
       t.string "docket_type", null: false, limit: 50, comment: "Docket type"
@@ -10,6 +11,19 @@ class CreateEtlAppeals < ActiveRecord::Migration[5.1]
       t.datetime "established_at", null: false, comment: "Timestamp for when the appeal was intaken successfully"
       t.uuid "uuid", null: false, comment: "The universally unique identifier for the appeal"
       t.string "status", null: false, limit: 32, comment: "Calculated BVA status based on Tasks"
+      t.boolean "legacy_opt_in_approved", comment: "Indicates whether a Veteran opted to withdraw matching issues from the legacy process. If there is a matching legacy issue and it is not withdrawn then it is ineligible for the decision review."
+      t.boolean "veteran_is_not_claimant", comment: "Selected by the user during intake, indicates whether the Veteran is the claimant, or if the claimant is someone else such as a dependent. Must be TRUE if Veteran is deceased."
+      t.string "poa_participant_id", limit: 20, comment: "Used to identify the power of attorney (POA)"
+      t.date "docket_range_date", comment: "Date that appeal was added to hearing docket range."
+      t.date "target_decision_date", comment: "If the appeal docket is direct review, this sets the target decision date for the appeal, which is one year after the receipt date."
+      t.string "closest_regional_office", limit: 20, comment: "The code for the regional office closest to the Veteran on the appeal."
+
+      t.index ["veteran_is_not_claimant"]
+      t.index ["uuid"]
+      t.index ["veteran_file_number"]
+      t.index ["docket_type"]
+      t.index ["receipt_date"]
+      t.index ["poa_participant_id"]
     end
   end
 end
--- db/etl/schema.rb
+++ db/etl/schema.rb
@@ -15,17 +15,29 @@ ActiveRecord::Schema.define(version: 20191111164808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "appeals", force: :cascade do |t|
+  create_table "appeals", force: :cascade, comment: "Denormalized BVA NODs" do |t|
     t.integer "appeal_id", null: false, comment: "ID of the Appeal"
+    t.string "closest_regional_office", limit: 20, comment: "The code for the regional office closest to the Veteran on the appeal."
     t.datetime "created_at", null: false, comment: "Default created_at/updated_at for the ETL record"
     t.string "docket_number", limit: 50, null: false, comment: "Docket number"
+    t.date "docket_range_date", comment: "Date that appeal was added to hearing docket range."
     t.string "docket_type", limit: 50, null: false, comment: "Docket type"
     t.datetime "established_at", null: false, comment: "Timestamp for when the appeal was intaken successfully"
+    t.boolean "legacy_opt_in_approved", comment: "Indicates whether a Veteran opted to withdraw matching issues from the legacy process. If there is a matching legacy issue and it is not withdrawn then it is ineligible for the decision review."
+    t.string "poa_participant_id", limit: 20, comment: "Used to identify the power of attorney (POA)"
     t.date "receipt_date", null: false, comment: "Receipt date of the NOD form"
     t.string "status", limit: 32, null: false, comment: "Calculated BVA status based on Tasks"
+    t.date "target_decision_date", comment: "If the appeal docket is direct review, this sets the target decision date for the appeal, which is one year after the receipt date."
     t.datetime "updated_at", null: false, comment: "Default created_at/updated_at for the ETL record"
     t.uuid "uuid", null: false, comment: "The universally unique identifier for the appeal"
     t.string "veteran_file_number", limit: 20, null: false, comment: "Veteran file number"
+    t.boolean "veteran_is_not_claimant", comment: "Selected by the user during intake, indicates whether the Veteran is the claimant, or if the claimant is someone else such as a dependent. Must be TRUE if Veteran is deceased."
+    t.index ["docket_type"], name: "index_appeals_on_docket_type"
+    t.index ["poa_participant_id"], name: "index_appeals_on_poa_participant_id"
+    t.index ["receipt_date"], name: "index_appeals_on_receipt_date"
+    t.index ["uuid"], name: "index_appeals_on_uuid"
+    t.index ["veteran_file_number"], name: "index_appeals_on_veteran_file_number"
+    t.index ["veteran_is_not_claimant"], name: "index_appeals_on_veteran_is_not_claimant"
   end
 
 end
