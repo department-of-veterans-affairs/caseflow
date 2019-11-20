895cd0979c4593805440c5031690a9fbb22e11db
Yang Yang <yangyang@navapbc.com>
2019-11-20 17:37:26 -0500

    Message user when a job succeeds after 24 hours

--- spec/services/asyncable_job_messaging_spec.rb
+++ spec/services/asyncable_job_messaging_spec.rb
@@ -78,4 +78,34 @@ describe AsyncableJobMessaging, :postgres do
       end
     end
   end
+
+  describe "#handle_job_success" do
+    let(:owner) { create(:default_user) }
+    let(:job) { create(:higher_level_review, :requires_processing, intake: create(:intake, user: owner)) }
+    let(:messaging) { AsyncableJobMessaging.new(job: job) }
+
+    subject { messaging.handle_job_success }
+
+    context "when a success happens within 24 hours" do
+      it "doesn't send a message to the job owner" do
+        message_count = owner.messages.count
+        subject
+        expect(owner.messages.count).to eq message_count
+      end
+    end
+
+    context "when a success happens after 24 hours of failure" do
+      it "sends a message to the job owner" do
+        job
+        messaging.handle_job_failure
+        Timecop.travel(Time.zone.now.tomorrow)
+        messaging.handle_job_failure
+        message_count = owner.messages.count
+        subject
+        expect(owner.messages.count).to eq message_count + 1
+        expect(owner.messages.last.text).to match("has successfully been processed")
+        expect(owner.messages.last.text).to match(job.path)
+      end
+    end
+  end
 end
