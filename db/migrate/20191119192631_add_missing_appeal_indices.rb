b179dee5f3c57e3eeca38c90664a63b1be8e89c1
Peter Karman <peter@truss.works>
2019-11-20 10:20:12 -0600

    Revert "trigger danger warning"
    This reverts commit 3c7ae25d8e498a52e8b066d476ba45999c4576f0.

--- db/migrate/20191119192631_add_missing_appeal_indices.rb
+++ db/migrate/20191119192631_add_missing_appeal_indices.rb
@@ -3,7 +3,5 @@ class AddMissingAppealIndices < ActiveRecord::Migration[5.1]
 
   def change
     add_index :claims_folder_searches, [:appeal_id, :appeal_type], algorithm: :concurrently
-    # trigger warning
-    add_index :documents, [:foo]
   end
 end
