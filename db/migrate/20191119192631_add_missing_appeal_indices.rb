8b017bb2efb056f11715f98f6b2ff4032bb7e009
Peter Karman <peter@truss.works>
2019-11-20 09:53:51 -0600

    reset timeout afterwards

--- db/migrate/20191119192631_add_missing_appeal_indices.rb
+++ db/migrate/20191119192631_add_missing_appeal_indices.rb
@@ -4,5 +4,7 @@ class AddMissingAppealIndices < ActiveRecord::Migration[5.1]
   def change
     ActiveRecord::Base.connection.execute "SET statement_timeout = 1800000" # 30 minutes
     add_index :claims_folder_searches, [:appeal_id, :appeal_type], algorithm: :concurrently
+  ensure
+    ActiveRecord::Base.connection.execute "SET statement_timeout = 30000" # 30 seconds
   end
 end
