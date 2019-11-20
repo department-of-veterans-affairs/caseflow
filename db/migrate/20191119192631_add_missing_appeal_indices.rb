e6a8a2af833f7b6938cc459d952c9034499edef6
Peter Karman <peter@truss.works>
2019-11-20 09:52:55 -0600

    Set longer timeout for migration

--- db/migrate/20191119192631_add_missing_appeal_indices.rb
+++ db/migrate/20191119192631_add_missing_appeal_indices.rb
@@ -2,6 +2,7 @@ class AddMissingAppealIndices < ActiveRecord::Migration[5.1]
   disable_ddl_transaction!
 
   def change
+    ActiveRecord::Base.connection.execute "SET statement_timeout = 1800000" # 30 minutes
     add_index :claims_folder_searches, [:appeal_id, :appeal_type], algorithm: :concurrently
   end
 end
