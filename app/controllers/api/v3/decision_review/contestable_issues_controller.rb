6d008acbf1111577559ec67fef151eff7b240674
Philip Becker <philip.becker@oddball.io>
2019-11-22 15:09:00 -0500

    oops

--- app/controllers/api/v3/decision_review/contestable_issues_controller.rb
+++ app/controllers/api/v3/decision_review/contestable_issues_controller.rb
@@ -32,24 +32,24 @@ class Api::V3::DecisionReview::ContestableIssuesController < Api::V3::BaseContro
     issues = ContestableIssueGenerator.new(standin_claim_review).contestable_issues
     # this generates this error in UAT:
     #  BGS::PublicError (Logon ID APIUSER Not Found in the Benefits Gateway Service (BGS). Contact your ISO if you need assistance gaining access to BGS.)
-    byebug
-    render json: Api::V3::IssueSerializer.new(issues)
+    render json: issues.collect { |issue| contestable_issue_data(issue) }
   end
 
 private
 
   def contestable_issue_data(contestable_issue)
-    {
+    byebug
+    attributes = {
       "decisionText": "veteran status verified",
       "decisionDate": "2019-07-11",
       "category": "Eligibility | Veteran Status",
 
       "decisionIssueId": contestable_issue.decision_issue&.id
-
-      # these are related to legacy??
-      # "ratingIssueId": "12345678",
-      # "legacyAppealId": "9876543210",
-      # "legacyAppealIssueId": 1
     }.reject{ |_, value| value.nil? }
+
+    {
+      type: 'ContestableIssue',
+      attributes: attributes
+    }
   end
 end
