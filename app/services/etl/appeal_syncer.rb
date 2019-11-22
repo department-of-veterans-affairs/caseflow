7ce2fec692bdae0ae234c9a9193d20c2ac8ca076
Peter Karman <peter@truss.works>
2019-11-22 14:41:01 -0600

    tests

--- app/services/etl/appeal_syncer.rb
+++ app/services/etl/appeal_syncer.rb
@@ -2,7 +2,7 @@
 
 class ETL::AppealSyncer < ETL::Syncer
   def origin_class
-    Appeal
+    ::Appeal
   end
 
   def target_class
@@ -12,6 +12,8 @@ class ETL::AppealSyncer < ETL::Syncer
   private
 
   def instances_needing_update
+    return origin_class unless incremental?
+
     AppealsUpdatedSinceQuery.new(since_date: since).call
   end
 end
