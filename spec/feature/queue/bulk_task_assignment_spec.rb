984aeb2b4c6183246b3bd74d9d4456a4b21e1862
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2019-11-22 14:30:09 -0500

    Remove use_task_pages_api feature toggle now that all users have it enabled (#12744)
    Resolves #12216, Resolves #12781.
    
    ### Description
    Removes all mention of the `use_task_pages_api` as it has been enabled for all users since [#2224](https://github.com/department-of-veterans-affairs/appeals-deployment/pull/2224)
    
    ### Acceptance Criteria
    - [ ] `use_task_pages_api` feature toggle is removed
    - [ ] All current tests pass

--- spec/feature/queue/bulk_task_assignment_spec.rb
+++ spec/feature/queue/bulk_task_assignment_spec.rb
@@ -141,38 +141,33 @@ RSpec.feature "Bulk task assignment", :postgres do
     end
   end
 
-  context "when tasks in queue are paginated" do
-    before { FeatureToggle.enable!(:use_task_pages_api, users: [user.css_id]) }
-    after { FeatureToggle.disable!(:use_task_pages_api, users: [user.css_id]) }
+  context "when there are more tasks than will fit on a single page" do
+    let(:task_count) { TaskPager::TASKS_PER_PAGE + 2 }
+    let(:regional_offices) { RegionalOffice::CITIES.keys.last(task_count) }
 
-    context "when there are more tasks than will fit on a single page" do
-      let(:task_count) { TaskPager::TASKS_PER_PAGE + 2 }
-      let(:regional_offices) { RegionalOffice::CITIES.keys.last(task_count) }
-
-      before do
-        regional_offices.each do |ro|
-          appeal = create(:appeal, :hearing_docket, closest_regional_office: ro)
-          create(:no_show_hearing_task, appeal: appeal)
-        end
+    before do
+      regional_offices.each do |ro|
+        appeal = create(:appeal, :hearing_docket, closest_regional_office: ro)
+        create(:no_show_hearing_task, appeal: appeal)
       end
+    end
 
-      it "correctly populates modal dropdowns with all options" do
-        visit(org.path)
+    it "correctly populates modal dropdowns with all options" do
+      visit(org.path)
 
-        expect(page).to have_content(COPY::BULK_ASSIGN_BUTTON_TEXT)
-        click_button(text: COPY::BULK_ASSIGN_BUTTON_TEXT)
-        expect(page).to have_content(COPY::BULK_ASSIGN_MODAL_TITLE)
+      expect(page).to have_content(COPY::BULK_ASSIGN_BUTTON_TEXT)
+      click_button(text: COPY::BULK_ASSIGN_BUTTON_TEXT)
+      expect(page).to have_content(COPY::BULK_ASSIGN_MODAL_TITLE)
 
-        options = find("select[id='Regional office']").find_all("option")
+      options = find("select[id='Regional office']").find_all("option")
 
-        # Skip the first two since they are 1) "Select" and 2) an empty option to reset the dropdown.
-        expect(options.count).to eq(task_count + 2)
-        regional_office_options = options.last(task_count).map(&:text)
+      # Skip the first two since they are 1) "Select" and 2) an empty option to reset the dropdown.
+      expect(options.count).to eq(task_count + 2)
+      regional_office_options = options.last(task_count).map(&:text)
 
-        # Sort the regional offices we expect to see by city name.
-        sorted_regional_offices = regional_offices.map { |ro| RegionalOffice::CITIES[ro][:city] }.sort
-        expect(regional_office_options).to eq(sorted_regional_offices)
-      end
+      # Sort the regional offices we expect to see by city name.
+      sorted_regional_offices = regional_offices.map { |ro| RegionalOffice::CITIES[ro][:city] }.sort
+      expect(regional_office_options).to eq(sorted_regional_offices)
     end
   end
 end
