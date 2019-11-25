fdc13e75852d0e28a6a1f29e6083a29b56cb778c
Peter Karman <peter@truss.works>
2019-11-25 10:37:21 -0600

    resurrect file

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative "stubbable_user"
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].strategy = :transaction
+  end
+
+  config.before(:each, db_clean: :truncation) do
+    DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].strategy = :truncation
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    unless driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+
+      DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner[:active_record, { connection: Rails.env.to_s.to_sym }].clean
+    clean_application!
+  end
+end
