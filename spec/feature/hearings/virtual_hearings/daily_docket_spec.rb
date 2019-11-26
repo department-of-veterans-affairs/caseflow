3d82f7c53e89d058cd37b42a8111724c85527601
Andrew Lomax <andrewlomaxr@gmail.com>
2019-11-26 15:29:21 -0500

    rm database cleaner

--- spec/feature/hearings/virtual_hearings/daily_docket_spec.rb
+++ spec/feature/hearings/virtual_hearings/daily_docket_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "support/vacols_database_cleaner"
 require "rails_helper"
 
 RSpec.feature "Editing virtual hearing information on daily Docket", :all_dbs do
