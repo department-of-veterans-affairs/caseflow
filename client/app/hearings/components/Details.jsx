03579666e67d553e5e477d1ecae02b333ddd6f3b
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2019-11-26 17:54:25 -0500

    Update client/app/hearings/components/Details.jsx
    Co-Authored-By: Andrew Lomax <andrewlomaxr@gmail.com>

--- client/app/hearings/components/Details.jsx
+++ client/app/hearings/components/Details.jsx
@@ -73,7 +73,6 @@ class HearingDetails extends React.Component {
     const { hearing } = this.props;
     const transcription = hearing.transcription || {};
     const virtualHearing = hearing.virtualHearing || {};
-      veteranEmail: null,
       representativeEmail: null,
       status: null
     };
