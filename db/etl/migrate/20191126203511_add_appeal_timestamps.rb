068c81c42e38f7e5729bb1335607bd941bbb3f49
Peter Karman <peter@truss.works>
2019-11-26 14:42:22 -0600

    update etl comments

--- db/etl/migrate/20191126203511_add_appeal_timestamps.rb
+++ db/etl/migrate/20191126203511_add_appeal_timestamps.rb
@@ -2,8 +2,10 @@ class AddAppealTimestamps < ActiveRecord::Migration[5.1]
   disable_ddl_transaction!
 
   def change
-    add_column :appeals, :appeal_created_at, :datetime, null: false
-    add_column :appeals, :appeal_updated_at, :datetime, null: false
+    add_column :appeals, :appeal_created_at, :datetime, null: false, comment: "appeals.created_at"
+    add_column :appeals, :appeal_updated_at, :datetime, null: false, comment: "appeals.updated_at"
+    change_column_comment :appeals, :created_at, "Creation timestamp for the ETL record"
+    change_column_comment :appeals, :updated_at, "Updated timestamp for the ETL record"
 
     add_index :appeals, :appeal_created_at, algorithm: :concurrently
     add_index :appeals, :appeal_updated_at, algorithm: :concurrently
