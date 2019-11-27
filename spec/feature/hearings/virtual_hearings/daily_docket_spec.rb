3516af67ff930fdfeaeb51a3d98cd825640d2507
Andrew Lomax <andrewlomaxr@gmail.com>
2019-11-26 21:00:09 -0500

    fix test

--- spec/feature/hearings/virtual_hearings/daily_docket_spec.rb
+++ spec/feature/hearings/virtual_hearings/daily_docket_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature "Editing virtual hearing information on daily Docket", :all_dbs do
     expect(page).to have_content(COPY::VIRTUAL_HEARING_MODAL_CHANGE_HEARING_TIME_TITLE)
     expect(page).to have_content(COPY::VIRTUAL_HEARING_CHANGE_HEARING_BUTTON)
     click_button(COPY::VIRTUAL_HEARING_CHANGE_HEARING_BUTTON)
-    expect(page).to have_content(COPY::VIRTUAL_HEARING_USER_ALERTS["EMAILS_UPDATED"]["MESSAGES"]["TO_VETERAN"])
+    expect(page).to have_content(COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % hearing.appeal.veteran.name)
 
     virtual_hearing.reload
     expect(virtual_hearing.veteran_email).to eq("newEmail@testingEmail.com")
@@ -29,6 +29,6 @@ RSpec.feature "Editing virtual hearing information on daily Docket", :all_dbs do
     visit "hearings/schedule/docket/" + hearing.hearing_day.id.to_s
     choose("hearingTime1_other", allow_label_click: true)
     click_dropdown(name: "optionalHearingTime1", index: 3)
-    click_button("Change-Hearing-Time-button-id-close")
+    click_button("Change-to-Virtual-Hearing-button-id-close")
   end
 end
