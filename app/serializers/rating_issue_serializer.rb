777fcb8f8c75b12fc939473fab0d0e6cddd43591
Sjones352 <sdjones@go-ct.com>
2019-12-04 14:55:33 -0500

    WIP progress

---
+++ app/serializers/rating_issue_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class RatingIssueSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attribute :participant_id
+  attribute :reference_id
+  attribute :decision_text
+  attribute :promulgation_date
+  attribute :profile_date
+  attribute :ramp_claim_id
+  attribute :rba_contentions_data
+  attribute :diagnostic_code
+  attribute :benefit_type
+  attribute :associated_end_products.map(&:serialize)
+end
