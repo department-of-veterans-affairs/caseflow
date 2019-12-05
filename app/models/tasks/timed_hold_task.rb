3c1a55ffb5b851cde1aeeb7a5416786b0efce18b
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2019-12-05 11:52:59 -0500

    Create function to handle updating task instructions (#12850)
    * Create function to handle updating task instructions
    
    * Fix tests
    
    * Fix text?
    
    * Explicit return from update_from_params

--- app/models/tasks/timed_hold_task.rb
+++ app/models/tasks/timed_hold_task.rb
@@ -15,11 +15,8 @@ class TimedHoldTask < Task
   def self.create_from_parent(parent_task, days_on_hold:, assigned_by: nil, instructions: nil)
     multi_transaction do
       if parent_task.is_a?(Task)
-        parent_task.update!(
-          instructions: [parent_task.instructions, instructions].flatten.compact,
-          # Set on_hold_duration to nil so that we override any old-style holds when we create a new timed hold.
-          on_hold_duration: nil
-        )
+        # Set on_hold_duration to nil so that we override any old-style holds when we create a new timed hold.
+        parent_task.update_with_instructions(instructions: instructions, on_hold_duration: nil)
       end
       create!(
         appeal: parent_task.appeal,
