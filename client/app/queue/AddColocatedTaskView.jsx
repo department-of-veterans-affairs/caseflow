e83a655282c92471688da5c48327057fd3ed1ea9
yoomlam <yoom@navapbc.com>
2019-12-05 15:44:35 -0600

    change resulting from removing the task type from the task action repository data

--- client/app/queue/AddColocatedTaskView.jsx
+++ client/app/queue/AddColocatedTaskView.jsx
@@ -81,7 +81,7 @@ class AddColocatedTaskView extends React.PureComponent {
         return {
           label: action.actionLabel,
           instructions: action.instructions,
-          type: taskActionData(this.props).type || action.actionLabel,
+          type: action.actionLabel,
           external_id: appeal.externalId,
           parent_id: task.isLegacy ? null : task.taskId
         };
