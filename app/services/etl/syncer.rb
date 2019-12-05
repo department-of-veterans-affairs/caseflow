92a160fd09cec1b062e27d770dccd01be7486c12
Peter Karman <pkarman@users.noreply.github.com>
2019-12-05 13:05:11 -0600

    Wrap each ETL batch in its own transaction (#12859)
    connects #12659 
    
    ## Description
    
    For long-running syncs (e.g. hours) we want to avoid losing all our work if something fails.
    
    We use transactions for performance (otherwise each `.save` gets its own transaction), so group each batch into its own transaction (default batch size is 1000 records).
    
    See https://api.rubyonrails.org/classes/ActiveRecord/Batches.html#method-i-find_in_batches for details.

--- app/services/etl/syncer.rb
+++ app/services/etl/syncer.rb
@@ -13,11 +13,14 @@ class ETL::Syncer
   end
 
   def call
-    target_class.transaction do
-      instances_needing_update.find_each do |original|
-        next if filter?(original)
+    instances_needing_update.find_in_batches.with_index do |originals, batch|
+      Rails.logger.debug("Starting batch #{batch} for #{target_class}")
+      target_class.transaction do
+        originals.each do |original|
+          next if filter?(original)
 
-        target_class.sync_with_original(original).save!
+          target_class.sync_with_original(original).save!
+        end
       end
     end
   end
