208668560144ef93fcd15a958fabaa60520c14cf
Kat Tipton <kat@navapbc.com>
2019-12-09 16:55:04 -0500

    first draft ama quality review selection update & cap

--- app/models/quality_review_case_selector.rb
+++ app/models/quality_review_case_selector.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class QualityReviewCaseSelector
-  QUALITY_REVIEW_SELECTION_PROBABILITY = 0.04
+  # AMA Limit and Probability. See Legacy numbers at app/models/judge_case_review.rb:28
+  # As of Dec 2019, we want AMA and Legacy to use the same numbers
+  MONTHLY_LIMIT_OF_QUAILITY_REVIEWS = 137
+  QUALITY_REVIEW_SELECTION_PROBABILITY = 0.032
 
-  def self.select_case_for_quality_review?
-    rand < QUALITY_REVIEW_SELECTION_PROBABILITY
+  class << self
+    def select_case_for_quality_review?
+      reached_monthly_limit_in_quality_reviews? ? false : rand < QUALITY_REVIEW_SELECTION_PROBABILITY
+    end
+  end
+
+  private
+
+  def reached_monthly_limit_in_quality_reviews?
+    QualityReviewTask.this_month.size >= MONTHLY_LIMIT_OF_QUAILITY_REVIEWS
   end
 end
