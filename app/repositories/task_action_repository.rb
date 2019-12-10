department-of-veterans-affairs/caseflow

commit b50dedadfff96281a394ee2355d2691e8ab43f9b

remove task's action field references (#12868)
Resolves #12279

### Description
Replaced uses of `:action` with `:type` where appropriate; otherwise removed its use.

### Acceptance Criteria
- Make sure there are no references to the now-defunct action field [more of a satisfaction criteria than action item]

app/repositories/task_action_repository.rb
@@ -203,13 +203,12 @@ class TaskActionRepository
       {
         redirect_after: "/queue",
         selected: nil,
-        options: Constants::CO_LOCATED_ADMIN_ACTIONS.map do |key, value|
+        options: ColocatedTask.subclasses.map do |subclass|
           {
-            label: value,
-            value: key
+            label: subclass.label,
+            value: subclass.name
           }
-        end,
-        type: ColocatedTask.name
+        end
       }
     end
 

app/controllers/tasks/change_type_controller.rb
@@ -16,7 +16,7 @@ class Tasks::ChangeTypeController < TasksController
   end
 
   def update_params
-    change_type_params = params.require(:task).permit(:action, :instructions)
+    change_type_params = params.require(:task).permit(:type, :instructions)
     change_type_params[:instructions] = task.flattened_instructions(change_type_params)
     change_type_params
   end

app/controllers/tasks_controller.rb
@@ -185,7 +185,8 @@ class TasksController < ApplicationController
   def valid_task_classes
     additional_task_classes = Hash[
       *MailTask.subclasses.map { |subclass| [subclass.to_s.to_sym, subclass] }.flatten,
-      *HearingAdminActionTask.subclasses.map { |subclass| [subclass.to_s.to_sym, subclass] }.flatten
+      *HearingAdminActionTask.subclasses.map { |subclass| [subclass.to_s.to_sym, subclass] }.flatten,
+      *ColocatedTask.subclasses.map { |subclass| [subclass.to_s.to_sym, subclass] }.flatten
     ]
     TASK_CLASSES_LOOKUP.merge(additional_task_classes)
   end
@@ -209,18 +210,13 @@ class TasksController < ApplicationController
 
   def create_params
     @create_params ||= [params.require("tasks")].flatten.map do |task|
-      task = task.permit(:type, :instructions, :action, :label, :assigned_to_id,
-                         :assigned_to_type, :external_id, :parent_id, business_payloads: [:description, values: {}])
+      appeal = Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(task[:external_id])
+      task = task.permit(:type, :instructions, :assigned_to_id,
+                         :assigned_to_type, :parent_id, business_payloads: [:description, values: {}])
         .merge(assigned_by: current_user)
-        .merge(appeal: Appeal.find_appeal_by_id_or_find_or_create_legacy_appeal_by_vacols_id(task[:external_id]))
+        .merge(appeal: appeal)
 
-      task.delete(:external_id)
       task = task.merge(assigned_to_type: User.name) if !task[:assigned_to_type]
-
-      # Allow actions to be passed with either the key "action" or "label" while we transition to using "label" in place
-      # of "action" so requests coming from browsers that have older versions of the javascript bundle succeed.
-      task = task.merge(action: task.delete(:label)) if task[:label]
-
       task
     end
   end

app/models/task.rb
@@ -101,11 +101,10 @@ class Task < ApplicationRecord
       end
     end
 
-    def modify_params(params)
+    def modify_params_for_create(params)
       if params.key?(:instructions) && !params[:instructions].is_a?(Array)
         params[:instructions] = [params[:instructions]]
       end
-      params.delete(:action)
       params
     end
 
@@ -152,7 +151,7 @@ class Task < ApplicationRecord
 
       verify_user_can_create!(user, parent_task)
 
-      params = modify_params(params)
+      params = modify_params_for_create(params)
       child = create_child_task(parent_task, user, params)
       parent_task.update!(status: params[:status]) if params[:status]
       child

app/models/tasks/colocated_task.rb
@@ -20,7 +20,7 @@ class ColocatedTask < Task
     def create_from_params(params, user)
       parent_task = params[:parent_id] ? Task.find(params[:parent_id]) : nil
       verify_user_can_create!(user, parent_task)
-      params = modify_params(params)
+      params = modify_params_for_create(params)
       create!(params)
     end
 
@@ -31,8 +31,9 @@ class ColocatedTask < Task
         params_array = params_array.map do |params|
           # Find the task type for a given action.
           create_params = params.clone
-          new_task_type = find_subclass_by_action(create_params.delete(:action).to_s)
-          create_params.merge!(type: new_task_type&.name, assigned_to: new_task_type&.default_assignee)
+          new_task_type = Object.const_get(params[:type])
+          # new_task_type should be one of the valid_task_classes in tasks_controller; otherwise fail here
+          create_params.merge!(type: new_task_type.name, assigned_to: new_task_type.default_assignee)
         end
 
         team_tasks = super(params_array, user)
@@ -69,6 +70,7 @@ class ColocatedTask < Task
       subclasses.find { |task_class| task_class.label == Constants::CO_LOCATED_ADMIN_ACTIONS[action] }
     end
 
+    # Is this method still relevant given ticket #12279 and related tickets?
     def actions_assigned_to_colocated
       Constants::CO_LOCATED_ADMIN_ACTIONS.keys.select do |action|
         find_subclass_by_action(action).methods(false).exclude?(:default_assignee)
@@ -114,7 +116,7 @@ class ColocatedTask < Task
   end
 
   def create_twin_of_type(params)
-    task_type = ColocatedTask.find_subclass_by_action(params[:action])
+    task_type = Object.const_get(params[:type])
     ColocatedTask.create!(
       appeal: appeal,
       parent: parent,

app/models/tasks/mail_task.rb
@@ -51,7 +51,7 @@ class MailTask < Task
           )
         end
 
-        params = modify_params(params)
+        params = modify_params_for_create(params)
         create_child_task(parent_task, user, params)
       end
     end
@@ -95,7 +95,7 @@ class MailTask < Task
   end
 
   def create_twin_of_type(params)
-    task_type = Object.const_get(params[:action])
+    task_type = Object.const_get(params[:type])
     parent_task = task_type.create!(
       appeal: appeal,
       parent: parent,

client/app/queue/AddColocatedTaskView.jsx
@@ -79,9 +79,8 @@ class AddColocatedTaskView extends React.PureComponent {
     return this.state.adminActions.map(
       (action) => {
         return {
-          label: action.actionLabel,
           instructions: action.instructions,
-          type: taskActionData(this.props).type || action.actionLabel,
+          type: action.actionLabel,
           external_id: appeal.externalId,
           parent_id: task.isLegacy ? null : task.taskId
         };

client/app/queue/ChangeTaskTypeModal.jsx
@@ -28,20 +28,20 @@ class ChangeTaskTypeModal extends React.PureComponent {
     super(props);
 
     this.state = {
-      actionOption: null,
+      typeOption: null,
       instructions: ''
     };
   }
 
-  validateForm = () => Boolean(this.state.actionOption) && Boolean(this.state.instructions);
+  validateForm = () => Boolean(this.state.typeOption) && Boolean(this.state.instructions);
 
   buildPayload = () => {
-    const { actionOption, instructions } = this.state;
+    const { typeOption, instructions } = this.state;
 
     return {
       data: {
         task: {
-          action: actionOption.value,
+          type: typeOption.value,
           instructions
         }
       }
@@ -50,12 +50,12 @@ class ChangeTaskTypeModal extends React.PureComponent {
 
   submit = () => {
     const { task } = this.props;
-    const { actionOption } = this.state;
+    const { typeOption } = this.state;
 
     const payload = this.buildPayload();
 
     const successMsg = {
-      title: sprintf(COPY.CHANGE_TASK_TYPE_CONFIRMATION_TITLE, task.label, actionOption.label),
+      title: sprintf(COPY.CHANGE_TASK_TYPE_CONFIRMATION_TITLE, task.label, typeOption.label),
       detail: COPY.CHANGE_TASK_TYPE_CONFIRMATION_DETAIL
     };
 
@@ -70,18 +70,18 @@ class ChangeTaskTypeModal extends React.PureComponent {
 
   actionForm = () => {
     const { highlightFormItems } = this.props;
-    const { instructions, actionOption } = this.state;
+    const { instructions, typeOption } = this.state;
 
     return <React.Fragment>
       <div>
         <div {...marginTop(4)}>
           <SearchableDropdown
-            errorMessage={highlightFormItems && !actionOption ? COPY.FORM_ERROR_FIELD_REQUIRED : null}
+            errorMessage={highlightFormItems && !typeOption ? COPY.FORM_ERROR_FIELD_REQUIRED : null}
             name={COPY.CHANGE_TASK_TYPE_ACTION_LABEL}
             placeholder="Select an action type"
             options={taskActionData(this.props).options}
-            onChange={(option) => option && this.setState({ actionOption: option })}
-            value={actionOption && actionOption.value} />
+            onChange={(option) => option && this.setState({ typeOption: option })}
+            value={typeOption && typeOption.value} />
         </div>
         <div {...marginTop(4)}>
           <TextareaField

commit ee0e4dda256fa75de113109644605e07dee1a722

hotfix for colocated action count mismatch (#12921)
Resolves bug introduced in PR #12868 

### Description
See https://github.com/department-of-veterans-affairs/caseflow/pull/12868/files#r356064882

`ColocatedTask.subclasses.count` returns 19.
`Constants::CO_LOCATED_ADMIN_ACTIONS.count` returns 18.
Causing failure: https://circleci.com/gh/department-of-veterans-affairs/caseflow/96078

The list of colocated tasks offered to the user is manually maintained. Not all subclasses of `ColocatedTask` should be offered to the user.

Remember to update both:
- `client/constants/CO_LOCATED_ADMIN_ACTIONS.json`
- `client/constants/CO_LOCATED_ADMIN_ACTION_CLASSES.json`

### Acceptance Criteria
- [ ] Code compiles correctly and tests pass

app/repositories/task_action_repository.rb
@@ -203,10 +203,10 @@ class TaskActionRepository
       {
         redirect_after: "/queue",
         selected: nil,
-        options: ColocatedTask.subclasses.map do |subclass|
+        options: Constants::CO_LOCATED_ADMIN_ACTIONS.map do |key, value|
           {
-            label: subclass.label,
-            value: subclass.name
+            label: value,
+            value: ColocatedTask.find_subclass_by_action(key).name
           }
         end
       }