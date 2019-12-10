9369f47655e395bc5e1025b0ea989f524e0f9ffa
Kat Tipton <kat@navapbc.com>
2019-12-10 16:27:23 -0500

    Fix bad test. PR feedback

--- app/models/quality_review_case_selector.rb
+++ app/models/quality_review_case_selector.rb
@@ -2,17 +2,20 @@
 
 class QualityReviewCaseSelector
   # AMA Limit and Probability. See Legacy numbers at app/models/judge_case_review.rb:28
-  # As of Dec 2019, we want AMA and Legacy to use the same numbers
+  # As of Dec 2019, we want AMA and Legacy to use the same cap. The percentages may differ. The
+  # goal is to get to the cap as steadily across the month as possible
   MONTHLY_LIMIT_OF_QUAILITY_REVIEWS = 137
   QUALITY_REVIEW_SELECTION_PROBABILITY = 0.032
 
   class << self
     def select_case_for_quality_review?
-      reached_monthly_limit_in_quality_reviews? ? false : rand < QUALITY_REVIEW_SELECTION_PROBABILITY
+      return false if reached_monthly_limit_in_quality_reviews?
+
+      rand < QUALITY_REVIEW_SELECTION_PROBABILITY
     end
 
     def reached_monthly_limit_in_quality_reviews?
-      QualityReviewTask.this_month.size >= MONTHLY_LIMIT_OF_QUAILITY_REVIEWS
+      QualityReviewTask.created_this_month.size >= MONTHLY_LIMIT_OF_QUAILITY_REVIEWS
     end
   end
 end
