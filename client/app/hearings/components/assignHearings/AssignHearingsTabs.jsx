c27e9339d74bc733451e53b0def3c22e18ad91ea
Ferris Tseng <ferris@navapbc.com>
2019-12-12 13:02:35 -0500

    remove unused

--- client/app/hearings/components/assignHearings/AssignHearingsTabs.jsx
+++ client/app/hearings/components/assignHearings/AssignHearingsTabs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import moment from 'moment';
 import _ from 'lodash';
