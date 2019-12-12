039ccc1bd1674b8dcde524250401792ef1e9724f
Sjones352 <sdjones@go-ct.com>
2019-12-12 17:15:52 -0500

    fixed lint error

--- app/serializers/intake/supplemental_claim_serializer.rb
+++ app/serializers/intake/supplemental_claim_serializer.rb
@@ -5,7 +5,6 @@ class Intake::SupplementalClaimSerializer < Intake::ClaimReviewSerializer
   set_key_transform :camel_lower
 
   attribute :isDtaError, &:decision_review_remanded?
-
   attribute :formType do
     "supplemental_claim"
   end
