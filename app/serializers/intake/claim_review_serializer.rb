c147ccf593e3cd14f283d646c23a6d0a0fdcaa36
Sjones352 <sdjones@go-ct.com>
2019-12-16 11:26:23 -0500

    removed binding pry

--- app/serializers/intake/claim_review_serializer.rb
+++ app/serializers/intake/claim_review_serializer.rb
@@ -4,9 +4,9 @@ class Intake::ClaimReviewSerializer < Intake::DecisionReviewSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
 
-  attribute :async_job_url, &:async_job_url
-  attribute :benefit_type, &:benefit_type
-  attribute :payee_code, &:payee_code
+  attribute :async_job_url
+  attribute :benefit_type
+  attribute :payee_code
   attribute :has_cleared_rating_ep, &:cleared_rating_ep?
   attribute :has_cleared_nonrating_ep, &:cleared_nonrating_ep?
 end
