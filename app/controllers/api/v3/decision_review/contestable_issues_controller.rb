ded80090ad2bdaa9e5e2aa5cc0a5de994ab4a2d3
Philip Becker <philip.becker@oddball.io>
2019-12-17 15:27:07 -0500

    use VeteranFinder.find_best_match

--- app/controllers/api/v3/decision_review/contestable_issues_controller.rb
+++ app/controllers/api/v3/decision_review/contestable_issues_controller.rb
@@ -37,7 +37,7 @@ class Api::V3::DecisionReview::ContestableIssuesController < Api::V3::BaseContro
   end
 
   def set_veteran_from_header
-    @veteran = Veteran.find_by_file_number(request.headers["veteranId"])
+    @veteran = VeteranFinder.find_best_match(request.headers["veteranId"])
     unless @veteran
       render_error(
         status: 404,
