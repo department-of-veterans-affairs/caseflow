b2901830fa18e6c3cefbe2f353beb8e0ed968336
hschallhorn <hschallhorn@tistatech.com>
2019-12-19 12:20:06 -0500

    Up probability. Run some test numbers in CI

--- spec/models/quality_review_case_selector_spec.rb
+++ spec/models/quality_review_case_selector_spec.rb
@@ -1,16 +1,42 @@
 # frozen_string_literal: true
 
 describe QualityReviewCaseSelector, :all_dbs do
-  context "after reaching the monthly limit" do
-    let!(:qr_tasks) do
-      QualityReviewCaseSelector::MONTHLY_LIMIT_OF_QUAILITY_REVIEWS.times do
-        QualityReviewTask.create(assigned_to: create(:user), appeal: create(:appeal), created_at: Time.zone.now)
+  describe ".reached_monthly_limit_in_quality_reviews?" do
+    context "when a realistic number of cases are completed" do
+      # Pulled from prod with:
+      # Task.where(
+      #   appeal_type: Appeal.name,
+      #   assigned_to_type: Organization.name,
+      #   type: [QualityReviewTask.name, BvaDispatchTask.name],
+      #   created_at: 2.month.ago.beginning_of_month..2.month.ago.end_of_month
+      # ).count
+      let(:complete_cases_count) { 850 }
+      let!(:qr_tasks) do
+        complete_cases_count.times do
+          if QualityReviewCaseSelector.select_case_for_quality_review?
+            QualityReviewTask.create(assigned_to: create(:user), appeal: create(:appeal), created_at: Time.zone.now)
+          end
+        end
+      end
+
+      ensure_stable do
+        fit "should hit the monthly limit" do
+          puts QualityReviewTask.count
+          expect(QualityReviewCaseSelector.reached_monthly_limit_in_quality_reviews?).to be(true)
+        end
       end
     end
 
-    context ".reached_monthly_limit_in_quality_reviews?"
-    it "return true to indicate the limit was reached" do
-      expect(QualityReviewCaseSelector.reached_monthly_limit_in_quality_reviews?).to be(true)
+    context "after reaching the monthly limit" do
+      let!(:qr_tasks) do
+        QualityReviewCaseSelector::MONTHLY_LIMIT_OF_QUAILITY_REVIEWS.times do
+          QualityReviewTask.create(assigned_to: create(:user), appeal: create(:appeal), created_at: Time.zone.now)
+        end
+      end
+
+      it "returns true" do
+        expect(QualityReviewCaseSelector.reached_monthly_limit_in_quality_reviews?).to be(true)
+      end
     end
   end
 end
