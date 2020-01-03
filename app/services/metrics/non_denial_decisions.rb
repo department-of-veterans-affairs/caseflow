150d98b841d77909534530b4236be1047c727360
Andrew Lomax <andrewlomaxr@gmail.com>
2020-01-02 20:47:15 -0800

    working version

--- app/services/metrics/non_denial_decisions.rb
+++ app/services/metrics/non_denial_decisions.rb
@@ -3,13 +3,17 @@
 # Metric ID: 1709076052
 # Metric definition: (Number EPs with date<=7 days after outcoding date/number non-denial decisions)
 
-class Metrics::HearingsShowRate < Metrics::Base
-  def call
-    if Time.zone.now.to_date - start_date < 7.days
-      fail Metrics::DateRange::DateRangeError, "Start date must be 7 days or more ago"
-    end
+class Metrics::NonDenialDecisions < Metrics::Base
+  def initialize(date_range)
+    super(date_range)
 
-    end_products_created_within_7_days_of_outcoding / non_denial_decisions.count
+    if start_or_end_date_within_7_days
+      fail Metrics::DateRange::DateRangeError, "Start and end dates must be 7 days or more ago"
+    end
+  end
+
+  def call
+    end_products_created_within_7_days_of_outcoding.count / appeals_with_non_denial_decisions.count.to_f
   end
 
   def name
@@ -22,30 +26,28 @@ class Metrics::HearingsShowRate < Metrics::Base
 
   private
 
-  def offset_end_date
-    # report end date should never be within 7 days of current day
-    return Time.zone.now.to_date - 7.days if (Time.zone.now.to_date - end_date) < 7.days
-
-    end_date
+  def start_or_end_date_within_7_days
+    seven_days_ago = (Time.zone.now - 7.days).to_date
+    end_date > seven_days_ago || start_date > seven_days_ago
   end
 
-  def appeals
-    @appeals ||= BvaDispatchTask.where("closed_at >= ? AND closed_at <= ?", start_date, offset_end_date)
+  def appeals_in_range
+    BvaDispatchTask.where("closed_at >= ? AND closed_at <= ?", start_date, end_date)
       .where(status: Constants.TASK_STATUSES.completed)
       .includes(:appeal)
       .map(&:appeal)
   end
 
-  def non_denial_decisions
-    @non_denial_decisions ||= DecisionIssue.where.not(disposition: "Denied")
-      .where(decision_review: appeals)
+  def appeals_with_non_denial_decisions
+    @appeals_with_non_denial_decisions ||= DecisionIssue.where
+      .not(disposition: "Denied")
+      .where(decision_review: appeals_in_range)
+      .includes(:decision_review)
+      .map(&:decision_review)
   end
 
-  def non_denial_decion_documents
-    DecisionDocument.where(
-      appeal_id: non_denial_decisions.pluck(:decision_review_id),
-      appeal_type: "Appeal"
-    )
+  def non_denial_decision_documents
+    DecisionDocument.where(appeal: appeals_with_non_denial_decisions)
   end
 
   def non_denial_end_products
@@ -54,10 +56,10 @@ class Metrics::HearingsShowRate < Metrics::Base
 
   def end_products_created_within_7_days_of_outcoding
     non_denial_end_products.select do |end_product|
-      (
-        end_product.source.appeal.tasks.find { |task| task.type == "BvaDispatchTask" }.closed_at
-        - end_product.created_at
-      ) <= 7.days
+      bva_dispatch_task = end_product.source.appeal.tasks.completed.find do |task|
+        task.type == "BvaDispatchTask"
+      end
+      bva_dispatch_task.closed_at - end_product.created_at <= 7.days
     end
   end
 end
--- spec/services/metrics/non_denial_decisions_spec.rb
+++ spec/services/metrics/non_denial_decisions_spec.rb
@@ -2,28 +2,45 @@
 
 describe Metrics::NonDenialDecisions, :postgres do
   let(:user) { create(:user) }
-  let(:start_date) { Time.zone.now - 31.days }
-  let(:end_date) { Time.zone.now - 1.day }
+  let(:start_date) { Time.zone.now - 45.days }
+  let(:end_date) { Time.zone.now - 15.days }
   let(:date_range) { Metrics::DateRange.new(start_date, end_date) }
+  let(:outcode_params) do
+    {
+      citation_number: "12312312",
+      decision_date: Date.new(1989, 11, 9).to_s,
+      file: "longfilenamehere",
+      redacted_document_location: "C://Windows/User/BVASWIFTT/Documents/NewDecision.docx"
+    }
+  end
   let(:number_of_decisions_in_range) { 25 }
-  let(:number_of_end_products_created_in_7_days) { 5 }
+  let(:number_of_end_products_created_in_7_days) { 10 }
   before do
-    decision_issues = (1...number_of_decisions_in_range).map do
+    bva_dispatcher = create(:user)
+    BvaDispatch.singleton.add_user(bva_dispatcher)
+    decision_issues = (0...number_of_decisions_in_range).map do
       appeal = create(:appeal, :outcoded, decision_documents: [create(:decision_document)])
-      BvaDispatchTask.create_from_root_task(appeal.root_task).update(assigned_to: user)
+      BvaDispatchTask.create_from_root_task(appeal.root_task).update(status: Constants.TASK_STATUSES.completed)
       create(:decision_issue, decision_review: appeal)
     end
 
+    BvaDispatchTask.where(status: Constants.TASK_STATUSES.completed).update_all(closed_at: Time.zone.now - 30.days)
     decision_issues.sample(number_of_end_products_created_in_7_days).each do |decision|
       create(:end_product_establishment, source: decision.decision_review.decision_documents.first)
     end
-
-    decision_issue
   end
 
   subject { Metrics::NonDenialDecisions.new(date_range).call }
 
   it do
-    expect(subject).to eq(number_of_end_products_created_in_7_days / number_of_decisions_in_range.to_float)
+    expect(subject).to eq(number_of_end_products_created_in_7_days / number_of_decisions_in_range.to_f)
+  end
+
+  context "when start date is within 7 days" do
+    let(:start_date) { Time.zone.now - 4.days }
+
+    it do
+      expect { subject }.to raise_error(Metrics::DateRange::DateRangeError)
+    end
   end
 end
