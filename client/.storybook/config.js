ec83a9aa801150011cf81f6814ac65d426fd451b
JC Quirin <jcq@jcq.local>
2020-01-03 10:44:50 -0800

    updated config so stories can be read from either: - client/stories - client/app (requires .stories.js naming scheme)

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,6 +2,9 @@ import { configure, addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure([
+    require.context('../stories', true, /\.stories\.js$/),
+    require.context('../app', true, /\.stories\.js$/)
+], module);
 
 addDecorator(withA11y);
