ecefae66537181f190d6b613363aa14725fe40c6
Philip Becker <philip.becker@oddball.io>
2020-01-07 18:14:15 -0500

    use ssn instead of veteran_id

--- app/controllers/api/v3/decision_review/contestable_issues_controller.rb
+++ app/controllers/api/v3/decision_review/contestable_issues_controller.rb
@@ -30,7 +30,8 @@ class Api::V3::DecisionReview::ContestableIssuesController < Api::V3::BaseContro
   end
 
   def set_veteran_from_header
-    @veteran = VeteranFinder.find_best_match(request.headers["veteranId"])
+    file_number = BGSService.new.fetch_file_number_by_ssn(request.headers["ssn"])
+    @veteran = VeteranFinder.find_best_match(file_number) if file_number
     unless @veteran
       render_error(
         status: 404,
