b70467e9059e9227cdfc0a43e552f923e3938b3f
Peter Karman <peter@truss.works>
2020-01-07 16:49:47 -0600

    lint

--- app/services/intake_reporter.rb
+++ app/services/intake_reporter.rb
@@ -3,21 +3,16 @@
 # Summarize the completion time and request issue categories for a range of Decision Review Intakes
 
 class IntakeReporter
-  def initialize(type:, start_date: Constants::DATES["AMA_ACTIVATION"].to_date, end_date: Time.zone.today)
+  include Reporter
+
+  def initialize(type:, start_date: Constants::DATES["AMA_ACTIVATION"].to_date, end_date: Time.zone.tomorrow)
     @start_date = start_date
     @end_date = end_date
     @type = type
   end
 
-  def summary
-    summary = {
-      rating_issue: 0,
-      rating_decision: 0,
-      nonrating: 0,
-      decision_issue: 0,
-      unidentified: 0,
-      ineligible: 0
-    }
+  # rubocop:disable Metrics/AbcSize
+  def report
     establishment_durations = []
     intakes.each do |intake|
       establishment_durations << (intake.completed_at - intake.started_at).to_i
@@ -35,6 +30,7 @@ class IntakeReporter
     summary[:median_establishment_time] = median(establishment_durations)
     summary
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
@@ -44,11 +40,14 @@ class IntakeReporter
     Intake.success.where(detail_type: type).where("started_at > ?", start_date).where("started_at <= ?", end_date)
   end
 
-  def median(times)
-    return 0 if times.empty?
-
-    len = times.length
-    (times[(len - 1) / 2] + times[len / 2]) / 2.0
+  def summary
+    @summary ||= {
+      rating_issue: 0,
+      rating_decision: 0,
+      nonrating: 0,
+      decision_issue: 0,
+      unidentified: 0,
+      ineligible: 0
+    }
   end
 end
-  
