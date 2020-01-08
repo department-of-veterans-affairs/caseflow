915acaacd599a04f50fe57564ec9a9da819d281e
Ed Mangimelli <edmangimelli@gmail.com>
2020-01-08 10:27:40 -0600

    add test to hlr intake processor that ensures that, given contestable issue ids, the contestable issue is looked up, and the request issue is correctly populated

--- spec/services/api/v3/decision_review/higher_level_review_intake_processor_spec.rb
+++ spec/services/api/v3/decision_review/higher_level_review_intake_processor_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "support/intake_helpers"
+require "pp"
 
 describe Api::V3::DecisionReview::IntakeProcessor, :all_dbs do
   include IntakeHelpers
@@ -103,5 +104,32 @@ describe Api::V3::DecisionReview::IntakeProcessor, :all_dbs do
 
   describe "#higher_level_review" do
     it { expect(subject.run!.higher_level_review).to be_a(HigherLevelReview) }
+    context do
+      let(:request_issues) { subject.run!.higher_level_review.request_issues.to_a }
+      let(:request_issue) { request_issues.first }
+      it { expect(request_issues).to be_an Array }
+      it { expect(request_issues.size).to be 1 }
+      it do
+        expect(
+          "rating_issue_reference_id" => request_issue[:contested_rating_issue_reference_id],
+          "rating_issue_diagnostic_code" => request_issue[:contested_rating_issue_diagnostic_code],
+          "rating_decision_reference_id" => request_issue[:contested_rating_decision_reference_id],
+          #"decision_text" => request_issue[:nonrating_issue_description],
+          "is_unidentified" => request_issue[:is_unidentified],
+          #"decision_date" => request_issue[:decision_date],
+          "benefit_type" => request_issue[:benefit_type],
+          "ramp_claim_id" => request_issue[:ramp_claim_id],
+          "contested_decision_issue_id" => request_issue[:contested_decision_issue_id]
+        ).to eq(
+          Api::V3::DecisionReview::ContestableIssueParams.new(
+            decision_review_class: HigherLevelReview,
+            veteran: veteran,
+            receipt_date: receipt_date,
+            benefit_type: benefit_type,
+            params: contestable_issue
+          ).intakes_controller_params.as_json.slice("rating_issue_reference_id", "rating_issue_diagnostic_code", "rating_decision_reference_id", "is_unidentified", "benefit_type", "ramp_claim_id", "contested_decision_issue_id")
+        )
+      end
+    end
   end
 end
