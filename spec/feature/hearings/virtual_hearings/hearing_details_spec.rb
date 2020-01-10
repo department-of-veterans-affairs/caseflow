4fe001a2d95371aa7727405786d7f3fb0f19cbc7
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-01-10 13:44:48 -0500

    removing unnecessary parameters

--- spec/feature/hearings/virtual_hearings/hearing_details_spec.rb
+++ spec/feature/hearings/virtual_hearings/hearing_details_spec.rb
@@ -108,7 +108,7 @@ RSpec.feature "Editing Virtual Hearings from Hearing Details", :all_dbs do
 
     visit "hearings/" + hearing.external_id.to_s + "/details"
 
-    expect(page).to have_field("Veteran Email", with: "new@email.com", disabled: false)
-    expect(page).to have_field("POA/Representative Email", with: "rep@testingEmail.com", disabled: false)
+    expect(page).to have_field("Veteran Email", with: "new@email.com")
+    expect(page).to have_field("POA/Representative Email", with: "rep@testingEmail.com")
   end
 end
