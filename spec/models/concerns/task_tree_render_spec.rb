f58a862e0842f1b3913a91cfdf9cf5ec70d269c6
yoomlam <yoom@navapbc.com>
2020-01-10 16:26:23 -0600

    remove unnecessary comments

--- spec/models/concerns/task_tree_render_spec.rb
+++ spec/models/concerns/task_tree_render_spec.rb
@@ -11,7 +11,6 @@ describe TaskTreeRender do
 
   context ".tree is called on an appeal" do
     it "returns all tasks for the appeal" do
-      # puts @appeal.tree
       rows_hash, metadata = @appeal.tree_hash
       expect(rows_hash.count).to eq 1
       expect(metadata.rows.count).to eq @appeal.tasks.count
@@ -26,9 +25,9 @@ describe TaskTreeRender do
     it "returns dereferenced column chain '[:assigned_to, :type]'" do
       _rows_hash, metadata = @appeal.tree_hash(:id, [:assigned_to, :type])
       expect(metadata.col_keys).to eq ["id", "[:assigned_to, :type]"]
-      @appeal.tasks.each do |t|
-        expect(metadata.rows[t]["[:assigned_to, :type]"]).to eq t.assigned_to&.type if t.assigned_to.is_a?(Organization)
-        expect(metadata.rows[t]["[:assigned_to, :type]"]).to eq "" if t.assigned_to.is_a?(User)
+      @appeal.tasks.each do |tsk|
+        expect(metadata.rows[tsk]["[:assigned_to, :type]"]).to eq tsk.assigned_to&.type if tsk.assigned_to.is_a?(Organization)
+        expect(metadata.rows[tsk]["[:assigned_to, :type]"]).to eq "" if tsk.assigned_to.is_a?(User)
       end
     end
 
@@ -73,7 +72,6 @@ describe TaskTreeRender do
 
     it "highlights specified task with an asterisk, even if no columns are specified" do
       task_to_highlight = @appeal.tasks.sample
-      # puts @appeal.root_task.tree(highlight: task_to_highlight.id)
 
       _rows_hash, metadata = @appeal.root_task.tree_hash(highlight: task_to_highlight.id)
       check_for_highlight(metadata, task_to_highlight)
@@ -81,7 +79,6 @@ describe TaskTreeRender do
 
     it "highlights specified task with an asterisk, even if highlight column is not specified" do
       task_to_highlight = @appeal.tasks.sample
-      # puts @appeal.root_task.tree(highlight: task_to_highlight.id)
 
       _rows_hash, metadata = @appeal.root_task.tree_hash(:id, :status, highlight: task_to_highlight.id)
       check_for_highlight(metadata, task_to_highlight)
