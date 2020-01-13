1ab7600ba6ca49357db9362a44f75d12ffb35aa1
Ferris Tseng <ferris@navapbc.com>
2020-01-13 12:41:50 -0500

    copy AHLs into task attributes

--- client/app/queue/utils.js
+++ client/app/queue/utils.js
@@ -102,7 +102,8 @@ const taskAttributesFromRawTask = (task) => {
     timelineTitle: task.attributes.timeline_title,
     hideFromQueueTableView: task.attributes.hide_from_queue_table_view,
     hideFromTaskSnapshot: task.attributes.hide_from_task_snapshot,
-    hideFromCaseTimeline: task.attributes.hide_from_case_timeline
+    hideFromCaseTimeline: task.attributes.hide_from_case_timeline,
+    availableHearingLocations: task.attributes.available_hearing_locations
   };
 };
 
