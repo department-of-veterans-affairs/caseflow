9ca2132e95b9c68907965a69185e384f8f1f6d8b
hschallhorn <hschallhorn@tistatech.com>
2020-01-13 19:22:47 -0500

    Really try to get this test to work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -319,9 +319,9 @@ describe User, :all_dbs do
 
     context "when the user is a judge team admin" do
       let(:judge) { create(:user) }
-      let!(:judge_team) { JudgeTeam.create_for_judge(judge) }
+      let!(:judge_team) { create(:judge_team, :has_judge_team_lead_as_admin) }
 
-      before { OrganizationsUser.make_user_admin(user, judge_team) }
+      before { OrganizationsUser.make_user_admin(user, judge_team.reload!) }
 
       it "returns the judge team the user is an admin on" do
         is_expected.to include(
