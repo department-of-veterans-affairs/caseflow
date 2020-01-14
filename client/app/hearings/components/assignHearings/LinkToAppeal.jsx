9c4739e47a05ad1577e429deed6041c759a2fec3
Ferris Tseng <ferris@navapbc.com>
2020-01-14 17:10:20 -0500

    addressing lint

--- client/app/hearings/components/assignHearings/LinkToAppeal.jsx
+++ client/app/hearings/components/assignHearings/LinkToAppeal.jsx
@@ -1,4 +1,5 @@
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+import PropTypes from 'prop-types';
 import React from 'react';
 import moment from 'moment';
 
@@ -13,6 +14,15 @@ const LinkToAppeal = ({ appealExternalId, hearingDay, regionalOffice, children }
       {children}
     </Link>
   );
-}
+};
+
+LinkToAppeal.propTypes = {
+  appealExternalId: PropTypes.string,
+  children: PropTypes.node,
+  hearingDay: PropTypes.shape({
+    scheduledFor: PropTypes.string
+  }),
+  regionalOffice: PropTypes.string
+};
 
 export default LinkToAppeal;
