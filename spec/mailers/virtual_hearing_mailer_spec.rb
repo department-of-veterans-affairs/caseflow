cdcd7f25c80d4ba0ecf595703a5023f85495f00d
Ferris Tseng <ferris@navapbc.com>
2020-01-16 17:36:47 -0500

    update test to check content of emails for correct time

--- spec/mailers/virtual_hearing_mailer_spec.rb
+++ spec/mailers/virtual_hearing_mailer_spec.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 describe VirtualHearingMailer do
-  let(:hearing) { build(:hearing, hearing_location: HearingLocation.create) }
+  let(:regional_office) { "RO06" }
+  let(:hearing_day) { build(:hearing_day, regional_office: regional_office) }
+  let(:hearing) do
+    build(
+      :hearing,
+      scheduled_time: "6:30PM", # This time is in UTC
+      hearing_day: hearing_day,
+      regional_office: regional_office
+    )
+  end
   let(:virtual_hearing) { build(:virtual_hearing, hearing: hearing) }
 
   shared_examples_for "it can send an email to a recipient with the title" do |title_key|
@@ -40,14 +49,43 @@ describe VirtualHearingMailer do
   end
 
   context "for judge" do
-    it_should_behave_like "it can send an email to a recipient with the title", :judge
+    let(:title) { MailRecipient::RECIPIENT_TITLES[:judge] }
+
+    it_should_behave_like "it can send an email to a recipient with the title"
   end
 
   context "for veteran" do
-    it_should_behave_like "it can send an email to a recipient with the title", :veteran
+    let(:title) { MailRecipient::RECIPIENT_TITLES[:veteran] }
+    let(:recipient) { MailRecipient.new(name: "LastName", email: "veteran@test.com", title: title) }
+
+    it_should_behave_like "it can send an email to a recipient with the title"
+
+    subject do
+      VirtualHearingMailer.confirmation(
+        mail_recipient: recipient,
+        virtual_hearing: virtual_hearing
+      )
+    end
+
+    context "on east coast" do
+      it "has the correct time in the confirmation email" do
+        expect(subject.html_part.body).to include("1:30pm EST")
+      end
+    end
+
+    context "on west coast" do
+      # Oakland, CA Regional Office
+      let(:regional_office) { "RO43" }
+
+      it "has the correct time in the confirmation email" do
+        expect(subject.html_part.body).to include("10:30am PST")
+      end
+    end
   end
 
   context "for representative" do
-    it_should_behave_like "it can send an email to a recipient with the title", :representative
+    let(:title) { MailRecipient::RECIPIENT_TITLES[:representative] }
+
+    it_should_behave_like "it can send an email to a recipient with the title"
   end
 end
