2cde6b1c495d96b51bc164cb1cdc6d22090792f2
hschallhorn <hschallhorn@tistatech.com>
2020-01-16 09:24:28 -0500

    Fix merge

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -337,11 +337,11 @@ describe User, :all_dbs do
         it "returns assigned cases link for judge" do
           is_expected.to include(
             name: "Assign #{judge.css_id}",
-            url: format("queue/%<id>s/assign", id: judge.id)
+            url: format("/queue/%<id>s/assign", id: judge.id)
           )
           is_expected.not_to include(
             name: "Assign #{user.css_id}",
-            url: format("queue/%<id>s/assign", id: user.id)
+            url: format("/queue/%<id>s/assign", id: user.id)
           )
         end
       end
