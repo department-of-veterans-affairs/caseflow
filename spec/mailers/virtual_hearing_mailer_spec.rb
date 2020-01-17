231b16ce36d61ebd9167390c3f5df77c0a4ff620
Ferris Tseng <ferris@navapbc.com>
2020-01-17 15:46:41 -0500

    add test to check time displayed for legacy hearing

--- spec/mailers/virtual_hearing_mailer_spec.rb
+++ spec/mailers/virtual_hearing_mailer_spec.rb
@@ -2,16 +2,22 @@
 
 describe VirtualHearingMailer do
   let(:regional_office) { "RO06" }
-  let(:hearing_day) { build(:hearing_day, regional_office: regional_office) }
+  let(:hearing_day) do
+    create(
+      :hearing_day,
+      request_type: HearingDay::REQUEST_TYPES[:video],
+      regional_office: regional_office
+    )
+  end
   let(:hearing) do
-    build(
+    create(
       :hearing,
       scheduled_time: "10:30AM",
       hearing_day: hearing_day,
       regional_office: regional_office
     )
   end
-  let(:virtual_hearing) { build(:virtual_hearing, hearing: hearing) }
+  let(:virtual_hearing) { create(:virtual_hearing, hearing: hearing) }
 
   shared_examples_for "it can send an email to a recipient with the title" do
     let(:recipient) { MailRecipient.new(name: "LastName", email: "email@test.com", title: title) }
@@ -66,19 +72,47 @@ describe VirtualHearingMailer do
       )
     end
 
-    context "on east coast" do
-      it "has the correct time in the confirmation email" do
-        expect(subject.html_part.body).to include("10:30am EST")
+    shared_examples_for "it has the correct time in the email body" do |expected_est_time, expected_pst_time|
+      context "on east coast" do
+        it "has the correct time in the confirmation email" do
+          expect(subject.html_part.body).to include("#{expected_est_time} EST")
+        end
+      end
+
+      context "on west coast" do
+        # Oakland, CA Regional Office
+        let(:regional_office) { "RO43" }
+
+        it "has the correct time in the confirmation email" do
+          expect(subject.html_part.body).to include("#{expected_pst_time} PST")
+        end
       end
     end
 
-    context "on west coast" do
-      # Oakland, CA Regional Office
-      let(:regional_office) { "RO43" }
+    context "with ama hearing" do
+      it_should_behave_like "it has the correct time in the email body", "10:30am", "10:30am"
+    end
 
-      it "has the correct time in the confirmation email" do
-        expect(subject.html_part.body).to include("10:30am PST")
+    context "with legacy hearing" do
+      let(:case_hearing) do
+        hearing_date = Time.use_zone("America/New_York") { Time.zone.now.change(hour: 10, min: 30) }
+
+        create(
+          :case_hearing,
+          hearing_type: hearing_day.request_type,
+          hearing_date: VacolsHelper.format_datetime_with_utc_timezone(hearing_date) # VACOLS always has EST time
+        )
       end
+      let(:hearing) do
+        create(
+          :legacy_hearing,
+          case_hearing: case_hearing,
+          hearing_day: hearing_day,
+          hearing_day_id: hearing_day.id
+        )
+      end
+
+      it_should_behave_like "it has the correct time in the email body", "10:30am", "7:30am"
     end
   end
 
