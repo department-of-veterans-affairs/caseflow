739ac8ee6588e5031482f57d30ae02675e4d86a9
Sjones352 <sjones@tistatech.com>
2020-01-17 19:07:43 -0500

    cache manager test passing

--- spec/services/cache_manager_spec.rb
+++ spec/services/cache_manager_spec.rb
@@ -11,14 +11,21 @@ describe CacheManager do
   let(:ro_schedule_period) { build_stubbed(:ro_schedule_period) }
   let(:ro_cache_key) { "RoSchedulePeriod-#{ro_schedule_period.id}-cached-submitting_to_vacols" }
 
+  describe "#all_cache_keys" do
+    it "returns all keys in the cache" do
+      ro_schedule_period.start_confirming_schedule
+
+      expect(subject.all_cache_keys).to include(ro_cache_key)
+    end
+  end
+
   describe "#clear" do
     it "removes cached Caseflow attributes" do
       expect(Rails.cache.exist?(ro_cache_key)).to be_falsey
       ro_schedule_period.start_confirming_schedule
+      
       expect(Rails.cache.exist?(ro_cache_key)).to be_truthy
-
       subject.clear(:caseflow)
-
       expect(Rails.cache.exist?(ro_cache_key)).to be_falsey
     end
   end
