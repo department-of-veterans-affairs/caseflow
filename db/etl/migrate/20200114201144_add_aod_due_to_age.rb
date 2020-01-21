dc489c3649cca8d185509896abab15fa248486e8
Peter Karman <peter@truss.works>
2020-01-21 15:09:46 -0600

    Add ods.appeals.aod_due_to_dob

--- db/etl/migrate/20200114201144_add_aod_due_to_age.rb
+++ db/etl/migrate/20200114201144_add_aod_due_to_age.rb
@@ -1,5 +1,5 @@
 class AddAodDueToAge < ActiveRecord::Migration[5.1]
   def change
-    add_column :appeals, :aod_due_to_dob, :boolean, default: false, comment: "Calculated every day based on Claimant DOB"
+    add_column :appeals, :aod_due_to_dob, :boolean, comment: "Calculated every day based on Claimant DOB"
   end
 end
