ec434c2246a35dbbd533275aa355600cd66445ab
Peter Karman <peter@truss.works>
2020-01-21 14:33:37 -0600

    uat only

--- config/initializers/set_time_zone.rb
+++ config/initializers/set_time_zone.rb
@@ -1,1 +1,4 @@
-Time.zone = Rails.configuration.time_zone = "America/New_York" if Rails.current_env == :development
+# in process of fixing system time zone issues.
+# setting to :uat only for now.
+
+Time.zone = Rails.configuration.time_zone = "America/New_York" if Rails.current_env == :uat
