4fcf1eeb7de78213d148e1c110911fd2afcd74ed
Alsia Plybeah <alsia@navapbc.com>
2020-01-24 14:36:48 -0500

    update ama model; add logic to front-end

--- app/models/hearing.rb
+++ app/models/hearing.rb
@@ -152,6 +152,10 @@ class Hearing < ApplicationRecord
     )
   end
 
+  def scheduled_for_past?
+    scheduled_for < DateTime.now.in_time_zone(regional_office_timezone)
+  end
+
   def time
     @time ||= HearingTimeService.new(hearing: self)
   end
