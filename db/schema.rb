af05de2aef50a8a6dfcd4a1ea487eb23216f3b68
Yang Yang <yangyang@navapbc.com>
2020-01-28 11:55:33 -0500

    Backfill docket number and type into new stream fields, via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200123200907) do
+ActiveRecord::Schema.define(version: 20200128153700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
