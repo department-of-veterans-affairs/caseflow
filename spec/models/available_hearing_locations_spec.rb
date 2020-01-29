d56616e671c5fd21c60bbad23adb71038057c260
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-01-29 13:27:04 -0500

    code climate fix

--- spec/models/available_hearing_locations_spec.rb
+++ spec/models/available_hearing_locations_spec.rb
@@ -12,7 +12,7 @@ describe AvailableHearingLocations, :all_dbs do
       facility_type: "va_benefits_facility",
       facility_id: "vba_372"
     )
-    end
+  end
 
   let!(:location2) do
     AvailableHearingLocations.create(
@@ -25,17 +25,21 @@ describe AvailableHearingLocations, :all_dbs do
     )
   end
 
-  describe("#formatted_facility_type") do
-    it ("correctly formats facility type") do
+  describe "#formatted_facility_type" do
+    it "correctly formats facility type" do
       expect(location1.formatted_facility_type).to eq("(BVA) ")
       expect(location2.formatted_facility_type).to eq("(RO) ")
     end
   end
 
-  describe("#formatted_location") do
-    it ("correctly formats location") do
-      expect(location1.formatted_location).to eq("#{location1.city}, #{location1.state} #{location1.formatted_facility_type}")
-      expect(location2.formatted_location).to eq("#{location2.city}, #{location2.state} #{location2.formatted_facility_type}")
+  describe "#formatted_location" do
+    it "correctly formats location" do
+      expect(location1.formatted_location).to eq(
+        "#{location1.city}, #{location1.state} #{location1.formatted_facility_type}"
+      )
+      expect(location2.formatted_location).to eq(
+        "#{location2.city}, #{location2.state} #{location2.formatted_facility_type}"
+      )
     end
   end
 end
