79e6a3d0ba5a0c5c1068db42869f2af804016b20
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-01-29 17:20:14 -0500

    Add cols to cached_appeal_attributes for pagination (#13285)
    connects #13141
    
    ### Description
    - add two columns to cached_appeal_attributes table: `power_of_attorney_name` and `suggested_hearing_location` which will be used for filtering in hearings table
    - add the newly created columns to update_cached_appeals_attributes_job
    - create serializer for suggested_hearing_location which will be used in frontend in pagination work
    
    ### Database Changes
    *Only for Schema Changes*
    
    * [ ] Timestamps (created_at, updated_at) for new tables
    * [x] Column comments updated
    * [ ] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
    * [ ] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
    * [x] #appeals-schema notified with summary and link to this PR

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -15,6 +15,7 @@ class LegacyAppeal < ApplicationRecord
   include Taskable
   include PrintsTaskTree
   include HasTaskHistory
+  include AppealAvailableHearingLocations
 
   belongs_to :appeal_series
   has_many :dispatch_tasks, foreign_key: :appeal_id, class_name: "Dispatch::Task"
