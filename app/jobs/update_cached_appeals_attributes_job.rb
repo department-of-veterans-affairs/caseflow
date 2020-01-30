department-of-veterans-affairs/caseflow

commit 79e6a3d0ba5a0c5c1068db42869f2af804016b20

Add cols to cached_appeal_attributes for pagination (#13285)
connects #13141

### Description
- add two columns to cached_appeal_attributes table: `power_of_attorney_name` and `suggested_hearing_location` which will be used for filtering in hearings table
- add the newly created columns to update_cached_appeals_attributes_job
- create serializer for suggested_hearing_location which will be used in frontend in pagination work

### Database Changes
*Only for Schema Changes*

* [ ] Timestamps (created_at, updated_at) for new tables
* [x] Column comments updated
* [ ] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
* [ ] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
* [x] #appeals-schema notified with summary and link to this PR

app/jobs/update_cached_appeals_attributes_job.rb
@@ -5,6 +5,7 @@ BATCH_SIZE = 1000
 class UpdateCachedAppealsAttributesJob < CaseflowJob
   # For time_ago_in_words()
   include ActionView::Helpers::DateHelper
+
   queue_with_priority :low_priority
 
   APP_NAME = "caseflow_job"
@@ -47,6 +48,8 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
         docket_type: appeal.docket_type,
         docket_number: appeal.docket_number,
         is_aod: appeal_aod_status.include?(appeal.id),
+        power_of_attorney_name: appeal.representative_name,
+        suggested_hearing_location: appeal.suggested_hearing_location&.formatted_location,
         veteran_name: veteran_names_to_cache[appeal.veteran_file_number]
       }
     end
@@ -59,6 +62,8 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
                       :docket_number,
                       :is_aod,
                       :issue_count,
+                      :power_of_attorney_name,
+                      :suggested_hearing_location,
                       :veteran_name]
     CachedAppeal.import appeals_to_cache, on_duplicate_key_update: { conflict_target: [:appeal_id, :appeal_type],
                                                                      columns: update_columns }

.reek.yml
@@ -139,6 +139,7 @@ detectors:
       - HearingDispositionChangeJob
       - Helpers::AppealHearingHelper#available_hearing_locations
       - Helpers::AppealHearingHelper#hearings
+      - Helpers::AppealHearingHelper#suggested_hearing_location
       - LatestRatingDisabilityEvaluation#latest_disability_evaluation
       - LegacyDocket#age_of_n_oldest_priority_appeals
       - OrganizationOnHoldTasksTab#name

app/models/appeal.rb
@@ -10,6 +10,7 @@ class Appeal < DecisionReview
   include Taskable
   include PrintsTaskTree
   include HasTaskHistory
+  include AppealAvailableHearingLocations
 
   has_many :appeal_views, as: :appeal
   has_many :claims_folder_searches, as: :appeal

app/models/concerns/appeal_available_hearing_locations.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+module AppealAvailableHearingLocations
+  extend ActiveSupport::Concern
+
+  def suggested_hearing_location
+    # return the closest hearing location
+    available_hearing_locations&.min_by { |loc| loc.distance }
+  end
+end

app/models/hearings/available_hearing_locations.rb
@@ -3,4 +3,46 @@
 class AvailableHearingLocations < ApplicationRecord
   belongs_to :veteran, foreign_key: :file_number, primary_key: :veteran_file_number
   belongs_to :appeal, polymorphic: true
+
+  def to_hash
+    {
+      name: name,
+      address: address,
+      city: city,
+      state: state,
+      distance: distance,
+      facility_id: facility_id,
+      facility_type: facility_type,
+      classification: classification,
+      zip_code: zip_code,
+      formatted_facility_type: formatted_facility_type
+    }
+  end
+
+  # this will eventually replace getFacilityType in AppealHearingLocations
+  # in upcoming pagination work
+  def formatted_facility_type
+    case facility_type
+    when "vet_center"
+      "(Vet Center)"
+    when "va_health_facility"
+      "(VHA)"
+    when "va_benefits_facility"
+      return "(BVA)" if facility_id == "vba_372"
+
+      determine_vba_facility_type
+    else
+      ""
+    end
+  end
+
+  def determine_vba_facility_type
+    classification.include?("Regional") ? "(RO)" : "(VBA)"
+  end
+
+  def formatted_location
+    # Format location in 'City, State, (Facility Type)' format
+    # for filter values on the frontend (see: AppealHearingsTable)
+    "#{city}, #{state} #{formatted_facility_type}"
+  end
 end

app/models/legacy_appeal.rb
@@ -15,6 +15,7 @@ class LegacyAppeal < ApplicationRecord
   include Taskable
   include PrintsTaskTree
   include HasTaskHistory
+  include AppealAvailableHearingLocations
 
   belongs_to :appeal_series
   has_many :dispatch_tasks, foreign_key: :appeal_id, class_name: "Dispatch::Task"

app/models/serializers/work_queue/appeal_serializer.rb
@@ -74,6 +74,7 @@ class WorkQueue::AppealSerializer
   attribute :closest_regional_office
 
   attribute(:available_hearing_locations) { |object| available_hearing_locations(object) }
+  attribute(:suggested_hearing_location) { |object| suggested_hearing_location(object) }
 
   attribute :external_id, &:uuid
 

app/models/serializers/work_queue/legacy_appeal_serializer.rb
@@ -50,6 +50,7 @@ class WorkQueue::LegacyAppealSerializer
   attribute :closest_regional_office
 
   attribute(:available_hearing_locations) { |object| available_hearing_locations(object) }
+  attribute(:suggested_hearing_location) { |object| suggested_hearing_location(object) }
 
   attribute :docket_name do
     "legacy"

commit 5ef3aedd010d48f46087978da01bdf090202d47a

Set current_user in job for BGS (#13309)
Fixes regression introduced in 79e6a3d0ba5a0c5c1068db42869f2af804016b20

app/jobs/update_cached_appeals_attributes_job.rb
@@ -12,6 +12,7 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
   METRIC_GROUP_NAME = UpdateCachedAppealsAttributesJob.name.underscore
 
   def perform
+    RequestStore.store[:current_user] = User.system_user
     ama_appeals_start = Time.zone.now
     cache_ama_appeals
     datadog_report_time_segment(segment: "cache_ama_appeals", start_time: ama_appeals_start)