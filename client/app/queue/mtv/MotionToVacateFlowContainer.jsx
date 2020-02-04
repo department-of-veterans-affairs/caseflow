b88924da796d9c187784ba2e15a462eebad92ae4
JC Quirin <jcq@jcq.local>
2020-02-04 13:53:04 -0800

    updated MotionToVacateFlowContainer to load ReviewVacatedDecisionIssuesView

--- client/app/queue/mtv/MotionToVacateFlowContainer.jsx
+++ client/app/queue/mtv/MotionToVacateFlowContainer.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { useParams, useRouteMatch, Switch, Route } from 'react-router';
 import { taskById, appealWithDetailSelector } from '../selectors';
 import { MotionToVacateContextProvider } from './MotionToVacateContext';
+import { ReviewVacatedDecisionIssuesView } from './ReviewVacatedDecisionIssuesView';
 
 export const MotionToVacateFlowContainer = () => {
   const { path } = useRouteMatch();
@@ -19,8 +20,7 @@ export const MotionToVacateFlowContainer = () => {
         <Switch>
           <Route path={`${path}/draft_decision/vacatures`}>
             {/* Insert component from #13007 here */}
-            {/* <ReviewVacatedDecisionIssuesView appeal={appeal} /> */}
-            <></>
+            <ReviewVacatedDecisionIssuesView appeal={appeal} />
           </Route>
           <Route path={`${path}/draft_decision/add_decisions`}>
             {/* Insert component from #13071 here */}
