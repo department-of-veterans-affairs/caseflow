c9f6ad4accd80267eb4bad55ef90a50b7549087a
Ferris Tseng <ferris@navapbc.com>
2020-02-04 17:03:54 -0500

    add links instead of issue numbers

--- app/models/legacy_hearing.rb
+++ app/models/legacy_hearing.rb
@@ -185,10 +185,12 @@ class LegacyHearing < ApplicationRecord
   end
 
   def scheduled_for_past?
-    # FIXME: scheduled_for date is inconsistent in many places. (#13273)
+    # FIXME: scheduled_for date is inconsistent in many places.
+    # (https://github.com/department-of-veterans-affairs/caseflow/issues/13273)
     # scheduled_for should either pulled from VACOLS or from the associated hearing_day,
     # but some method exclusively use the value from VACOLS. The hearing_day association to
     # legacy hearings was added in #11741.
+    # (https://github.com/department-of-veterans-affairs/caseflow/pull/11741)
     scheduled_date = if hearing_day_id_refers_to_vacols_row?
                        # Handles conversion of a VACOLS time (EST) to the timezone of the RO
                        time.local_time
