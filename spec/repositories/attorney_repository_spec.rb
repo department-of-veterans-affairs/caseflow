907cf3231b177dec7902dda86d61bd085e7b8aad
yoomlam <yoom@navapbc.com>
2020-02-08 11:50:57 -0600

    unflake attorney_repository_spec

--- spec/repositories/attorney_repository_spec.rb
+++ spec/repositories/attorney_repository_spec.rb
@@ -3,24 +3,48 @@
 describe JudgeRepository, :all_dbs do
   let(:staff_count) { 3 }
 
-  let!(:judges) { create_list(:staff, staff_count, :judge_role) }
-  let!(:acting_judges) { create_list(:staff, staff_count, :attorney_judge_role) }
-  let!(:attorneys) { create_list(:staff, staff_count, :attorney_role) }
-  let!(:misc_staff) { create_list(:staff, staff_count, sactive: "I") }
+
+  before(:all) do
+    judges = create_list(:staff, 3, :judge_role)
+    acting_judges = create_list(:staff, 3, :attorney_judge_role)
+    attorneys = create_list(:staff, 3, :attorney_role)
+    misc_staff = create_list(:staff, 3, sactive: "I")
+
+    judges.each_with_index do |s, index|
+      s.sdomainid = "JUDGE#{index}"
+      s.snamef = "J#{index}"
+      s.sattyid = nil if index == 0
+    end.map(&:save!)
+    acting_judges.each_with_index do |s, index|
+      s.sdomainid = "ACTING#{index}"
+      s.snamef = "Aj#{index}"
+    end.map(&:save!)
+    attorneys.each_with_index do |s, index|
+      s.sdomainid = "ATTY#{index}"
+      s.snamef = "A#{index}"
+    end.map(&:save!)
+    misc_staff.each_with_index do |s, index|
+      s.sdomainid = "MISC#{index}"
+      s.snamef = "M#{index}"
+    end.map(&:save!)
+  end
 
   context ".find_all_having_attorney_ids" do
     subject { AttorneyRepository.find_all_having_attorney_ids }
 
     it "should return only active attorneys, judges, and acting judges" do
-      expect(subject.length).to eq(judges.count + acting_judges.count + attorneys.count)
+      expect(subject.pluck(:css_id)).to include("ATTY0", "ATTY1", "ATTY2", "ACTING0", "ACTING1", "ACTING2",
+                                                "JUDGE1", "JUDGE2")
+      expect(subject.pluck(:css_id)).not_to include("JUDGE0", "MISC0", "MISC1", "MISC2")
     end
   end
 
   context ".find_all_having_attorney_ids_excluding_judges" do
     subject { AttorneyRepository.find_all_having_attorney_ids_excluding_judges }
 
-    it "should return only active attorneys, judges, and acting judges" do
-      expect(subject.length).to eq(acting_judges.count + attorneys.count)
+    it "should return only active attorneys and acting judges" do
+      expect(subject.pluck(:css_id)).to include("ATTY0", "ATTY1", "ATTY2", "ACTING0", "ACTING1", "ACTING2")
+      expect(subject.pluck(:css_id)).not_to include("JUDGE0", "JUDGE1", "JUDGE2", "MISC0", "MISC1", "MISC2")
     end
   end
 end
--- spec/repositories/judge_repository_spec.rb
+++ spec/repositories/judge_repository_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 describe JudgeRepository, :all_dbs do
-  let(:staff_count) { 3 }
 
-  let!(:judges) { create_list(:staff, staff_count, :judge_role) }
-  let!(:acting_judges) { create_list(:staff, staff_count, :attorney_judge_role) }
-  let!(:attorneys) { create_list(:staff, staff_count, :attorney_role) }
+  before(:all) do
+    judges = create_list(:staff, 3, :judge_role)
+    acting_judges = create_list(:staff, 3, :attorney_judge_role)
+    attorneys = create_list(:staff, 3, :attorney_role)
 
-  before do
     judges.each_with_index do |s, index|
       s.sdomainid = "JUDGE#{index}"
       s.snamef = "J#{index}"
