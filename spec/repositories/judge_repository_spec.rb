a2509a5c2e5566c27b5e17cf49e57eb548133506
yoomlam <yoom@navapbc.com>
2020-02-08 11:35:50 -0600

    unflake JudgeRepository test

--- spec/repositories/judge_repository_spec.rb
+++ spec/repositories/judge_repository_spec.rb
@@ -7,27 +7,36 @@ describe JudgeRepository, :all_dbs do
   let!(:acting_judges) { create_list(:staff, staff_count, :attorney_judge_role) }
   let!(:attorneys) { create_list(:staff, staff_count, :attorney_role) }
 
+  before do
+    judges.each_with_index do |s, index|
+      s.sdomainid = "JUDGE#{index}"
+      s.snamef = "J#{index}"
+    end.map(&:save!)
+    acting_judges.each_with_index do |s, index|
+      s.sdomainid = "ACTING#{index}"
+      s.snamef = "Aj#{index}"
+    end.map(&:save!)
+    attorneys.each_with_index do |s, index|
+      s.sdomainid = "ATTY#{index}"
+      s.snamef = "A#{index}"
+    end.map(&:save!)
+  end
+
   context ".find_all_judges" do
     subject { JudgeRepository.find_all_judges }
 
     it "should return only judges and acting judges" do
-      expect(subject.length).to eq(judges.count + acting_judges.count)
+      expect(subject.pluck(:css_id)).to include("ACTING0", "ACTING1", "ACTING2", "JUDGE0", "JUDGE1", "JUDGE2")
+      expect(subject.pluck(:css_id)).not_to include("ATTY0", "ATTY1", "ATTY2")
     end
   end
 
   context ".find_all_judges_with_name_and_id" do
     subject { JudgeRepository.find_all_judges_with_name_and_id }
-    before do
-      judges.each { |s| s.snamef = "Judge J" }.map(&:save!)
-      acting_judges.each { |s| s.snamef = "Acting J" }.map(&:save!)
-    end
 
-    it "should return only judges and acting judges" do
-      expect(subject.length).to eq(judges.count + acting_judges.count)
-    end
-
-    it "should return names" do
-      expect(subject.map { |s| s[:first_name] }.uniq).to contain_exactly("Judge J", "Acting J")
+    it "should return names of only judges and acting judges" do
+      expect(subject.pluck(:first_name)).to include("J0", "J1", "J2", "Aj0", "Aj1", "Aj2")
+      expect(subject.pluck(:first_name)).not_to include("A0", "A1", "A2")
     end
   end
 end
