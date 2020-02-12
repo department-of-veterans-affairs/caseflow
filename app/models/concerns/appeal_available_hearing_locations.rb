0b8ea26d3d1005d96b2bf616817bca11d2ef524b
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-02-12 11:39:15 -0500

    code climate

--- app/models/concerns/appeal_available_hearing_locations.rb
+++ app/models/concerns/appeal_available_hearing_locations.rb
@@ -7,7 +7,7 @@ module AppealAvailableHearingLocations
     # return the closest hearing location
     available_hearing_locations&.min_by do |loc|
       next if !loc.distance.nil?
-      
+
       loc.distance
     end
   end
