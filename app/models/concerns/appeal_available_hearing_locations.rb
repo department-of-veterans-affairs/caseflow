5cc725e5c0895f820df73934122959059f558d9c
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-02-12 19:09:04 -0500

    major logic error fix

--- app/models/concerns/appeal_available_hearing_locations.rb
+++ app/models/concerns/appeal_available_hearing_locations.rb
@@ -7,7 +7,7 @@ module AppealAvailableHearingLocations
   def suggested_hearing_location
     # return the closest hearing location
     available_hearing_locations&.min_by do |loc|
-      next if !loc.distance.nil?
+      next if loc.distance.nil?
 
       loc.distance
     end
