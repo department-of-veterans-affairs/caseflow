77e2f3b76a80ebf0b83ca23aa9606e44d17d945c
Peter Karman <peter@truss.works>
2020-02-17 14:54:40 -0600

    Add data integrity check for open task with closed parent

--- app/services/open_tasks_with_closed_at_checker.rb
+++ app/services/open_tasks_with_closed_at_checker.rb
@@ -8,11 +8,20 @@ class OpenTasksWithClosedAtChecker < DataIntegrityChecker
       add_to_report "Verify data error with `Task.open.where.not(closed_at: nil)`"
       add_to_report "These tasks likely were manually re-opened and should have closed_at set to NULL"
     end
+    suspect_tasks = open_tasks_with_closed_parent
+    if suspect_tasks.count > 0
+      add_to_report "#{suspect_tasks.count} open Task(s) with a closed parent Task"
+      add_to_report "Verify with `Task.where(id: [#{suspect_tasks.pluck(:id).join(',')}])`"
+    end
   end
 
   private
 
   def open_tasks_with_closed_at_defined
     Task.open.where.not(closed_at: nil)
+  end
+
+  def open_tasks_with_closed_parent
+    Task.open.joins(:parent).includes(:parent).where.not(parents_tasks: { closed_at: nil })
   end
 end
