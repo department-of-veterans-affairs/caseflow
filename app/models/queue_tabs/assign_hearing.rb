f06ce41932e0251a4cccd7c4a1138bec8313b7f3
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-02-19 15:46:34 -0500

    move tests out of hearing_coordinator_schedule_queue_spec into assign_hearing_spec & add prev logic for legacy appeals in assign hearing

--- app/models/queue_tabs/assign_hearing.rb
+++ app/models/queue_tabs/assign_hearing.rb
@@ -6,6 +6,8 @@
 # and legacyAssignHearingTab which are paginated.
 
 class AssignHearing
+  include ActiveModel::Model
+
   attr_accessor :regional_office_key, :appeal_type
 
   def initialize(appeal_type:, regional_office_key:)
@@ -16,13 +18,40 @@ class AssignHearing
   # return schedule hearing tasks joined with CachedAppeal selected
   # by regional office
   def tasks
-    @tasks ||=
+    tasks =
       ScheduleHearingTask
         .includes(*task_includes)
         .active
         .where(appeal_type: appeal_type)
         .joins(CachedAppeal.left_join_from_tasks_clause)
-        .where("cached_appeal_attributes.closest_regional_office_key = ?", regional_office_key)
+
+    # For legacy appeals, we need to only provide a central office hearing if they explicitly
+    # chose one. Likewise, we can't use DC if it's the closest regional office unless they
+    # chose a central office hearing.
+    @tasks ||=
+      if appeal_type == "LegacyAppeal"
+        legacy_tasks(tasks)
+      else
+        tasks.where("cached_appeal_attributes.closest_regional_office_key = ?", regional_office_key)
+      end
+  end
+
+  def legacy_tasks(tasks)
+    central_office_ids = VACOLS::Case.where(bfhr: 1, bfcurloc: "CASEFLOW").pluck(:bfkey)
+    central_office_legacy_appeal_ids = LegacyAppeal.where(vacols_id: central_office_ids).pluck(:id)
+
+    if regional_office_key == "C"
+      tasks.where("cached_appeal_attributes.appeal_id IN (?)", central_office_legacy_appeal_ids)
+    else
+      tasks_by_ro = tasks.where("cached_appeal_attributes.closest_regional_office_key = ?", regional_office_key)
+
+      # For context: https://github.com/rails/rails/issues/778#issuecomment-432603568
+      if central_office_legacy_appeal_ids.empty?
+        tasks_by_ro
+      else
+        tasks_by_ro.where("cached_appeal_attributes.appeal_id NOT IN (?)", central_office_legacy_appeal_ids)
+      end
+    end
   end
 
   def to_hash
--- spec/models/assign_hearing_spec.rb
+++ spec/models/assign_hearing_spec.rb
@@ -1,95 +1,222 @@
 # frozen_string_literal: true
 
 describe AssignHearing do
-  let(:tab) { AssignHearing.new(params) }
   let(:regional_office_key) { "RO18" }
-  let(:assignee) { HearingsManagement.singleton }
-  let(:params) do
-    {
-      appeal_type: Appeal.name,
-      regional_office_key: regional_office_key
-    }
-  end
-  let(:appeal) do
-    create(
-      :appeal,
-      closest_regional_office: regional_office_key
-    )
-  end
-
-  let!(:hearing_location1) do
-    create(
-      :available_hearing_locations,
-      appeal_id: appeal.id,
-      appeal_type: "Appeal",
-      city: "New York",
-      state: "NY",
-      facility_id: "vba_372",
-      facility_type: "va_benefits_facility",
-      distance: 9
-    )
-  end
-
-  let!(:hearing_location2) do
-    create(
-      :available_hearing_locations,
-      appeal_id: appeal.id,
-      appeal_type: "Appeal",
-      city: "San Francisco",
-      state: "CA",
-      distance: 100
-    )
-  end
-
-  let!(:task1) { create(:schedule_hearing_task, assigned_to: assignee, appeal: appeal) }
-  let!(:task2) { create(:schedule_hearing_task, assigned_to: assignee, appeal: appeal) }
-
-  let(:cache_appeals) { UpdateCachedAppealsAttributesJob.new.cache_ama_appeals }
+  let(:cache_ama_appeals) { UpdateCachedAppealsAttributesJob.new.cache_ama_appeals }
+  let(:cache_legacy_appeals) { UpdateCachedAppealsAttributesJob.new.cache_legacy_appeals }
 
   describe ".tasks" do
-    subject { tab.tasks }
+    context "Legacy Appeal cases" do
+      let(:number_of_cases) { 10 }
 
-    it "returns correct tasks" do
-      cache_appeals
-      expect(subject.select(&:id)).to include(task1, task2)
+      context "when there are no cases CO hearings" do
+        let(:params) do
+          {
+            appeal_type: LegacyAppeal.name,
+            regional_office_key: regional_office_key
+          }
+        end
+        let!(:cases) do
+          create_list(:case, number_of_cases,
+                      bfregoff: regional_office_key,
+                      bfhr: "2",
+                      bfcurloc: "57",
+                      bfdocind: HearingDay::REQUEST_TYPES[:video])
+        end
+
+        let!(:c_number_case) do
+          create(
+            :case,
+            bfcorlid: "1234C",
+            bfregoff: regional_office_key,
+            bfhr: "2",
+            bfcurloc: 57,
+            bfdocind: HearingDay::REQUEST_TYPES[:video]
+          )
+        end
+
+        let!(:veterans) do
+          VACOLS::Case.all.map do |vacols_case|
+            create(
+              :veteran,
+              file_number: LegacyAppeal.veteran_file_number_from_bfcorlid(vacols_case.bfcorlid)
+            )
+          end
+        end
+
+        let!(:non_hearing_cases) do
+          create_list(:case, number_of_cases)
+        end
+
+        before do
+          AppealRepository.create_schedule_hearing_tasks.each do |appeal|
+            appeal.update(closest_regional_office: regional_office_key)
+          end
+
+          cache_legacy_appeals
+        end
+
+        it "returns tasks for all relevant appeals in location 57" do
+          tasks = AssignHearing.new(appeal_type: LegacyAppeal.name, regional_office_key: regional_office_key).tasks
+          expect(tasks.map { |task| task.appeal.vacols_id }).to match_array(
+            cases.pluck(:bfkey) + [c_number_case.bfkey]
+          )
+        end
+      end
+
+      context "when there are cases with central office hearings" do
+        let!(:cases) do
+          create_list(:case, number_of_cases,
+                      bfregoff: regional_office_key,
+                      bfhr: "1",
+                      bfcurloc: "57",
+                      bfdocind: HearingDay::REQUEST_TYPES[:central])
+        end
+
+        let!(:video_cases) do
+          create_list(:case, number_of_cases,
+                      bfregoff: regional_office_key,
+                      bfhr: "2",
+                      bfcurloc: "57",
+                      bfdocind: HearingDay::REQUEST_TYPES[:video])
+        end
+
+        let!(:veterans) do
+          VACOLS::Case.all.map do |vacols_case|
+            create(
+              :veteran,
+              file_number: LegacyAppeal.veteran_file_number_from_bfcorlid(vacols_case.bfcorlid)
+            )
+          end
+        end
+
+        before do
+          AppealRepository.create_schedule_hearing_tasks.each do |appeal|
+            appeal.update(closest_regional_office: regional_office_key)
+          end
+
+          cache_legacy_appeals
+        end
+
+        it "returns tasks for all CO hearings in location 57" do
+          tasks = AssignHearing.new(appeal_type: LegacyAppeal.name, regional_office_key: "C").tasks
+          expect(tasks.map { |task| task.appeal.vacols_id }).to match_array(cases.pluck(:bfkey))
+        end
+
+        it "does not return tasks for regional office when marked as CO" do
+          AppealRepository.create_schedule_hearing_tasks
+          cache_legacy_appeals
+
+          tasks = AssignHearing.new(appeal_type: LegacyAppeal.name, regional_office_key: regional_office_key).tasks
+          expect(tasks.map { |task| task.appeal.vacols_id }).to match_array(video_cases.pluck(:bfkey))
+        end
+      end
+    end
+
+    context "AMA cases" do
+      let(:veteran_at_ro) { create(:veteran) }
+      let(:appeal_for_veteran_at_ro) do
+        create(:appeal, veteran: veteran_at_ro, closest_regional_office: regional_office_key)
+      end
+      let!(:schedule_hearing_task) { create(:schedule_hearing_task, appeal: appeal_for_veteran_at_ro) }
+
+      let(:veteran_at_different_ro) { create(:veteran) }
+      let(:appeal_for_veteran_at_different_ro) do
+        create(:appeal, veteran: veteran_at_different_ro, closest_regional_office: "RO04")
+      end
+      let!(:hearing_task_for_other_veteran) do
+        create(:schedule_hearing_task, appeal: appeal_for_veteran_at_different_ro)
+      end
+
+      it "returns tasks for all appeals associated with Veterans at regional office" do
+        cache_ama_appeals
+        tasks = AssignHearing.new(appeal_type: Appeal.name, regional_office_key: regional_office_key).tasks
+
+        expect(tasks.count).to eq(1)
+        expect(tasks[0].id).to eq(schedule_hearing_task.id)
+      end
     end
   end
 
-  describe ".power_of_attorney_name_options" do
-    subject { tab.power_of_attorney_name_options }
-
-    it "returns correct options" do
-      cache_appeals
-
-      expect(subject.first[:value]).to eq(URI.escape(URI.escape(appeal.representative_name)))
-      expect(subject.first[:displayText]).to eq("#{appeal.representative_name} (2)")
+  context "AMA appeals" do
+    let(:tab) { AssignHearing.new(params) }
+    let(:assignee) { HearingsManagement.singleton }
+    let(:params) do
+      {
+        appeal_type: Appeal.name,
+        regional_office_key: regional_office_key
+      }
     end
-  end
-
-  describe ".suggested_location_options" do
-    subject { tab.suggested_location_options }
-
-    it "returns correct options" do
-      cache_appeals
-
-      expect(subject.first[:value]).to eq(URI.escape(URI.escape(hearing_location1.formatted_location)))
-      expect(subject.first[:displayText]).to eq("#{hearing_location1.formatted_location} (2)")
+    let(:appeal) do
+      create(
+        :appeal,
+        closest_regional_office: regional_office_key
+      )
     end
-  end
 
-  describe ".columns" do
-    subject { tab.columns }
-
-    it "returns columns with the correct keys" do
-      expect(subject.first.keys).to match_array([:name, :filter_options])
+    let!(:hearing_location1) do
+      create(
+        :available_hearing_locations,
+        appeal_id: appeal.id,
+        appeal_type: "Appeal",
+        city: "New York",
+        state: "NY",
+        facility_id: "vba_372",
+        facility_type: "va_benefits_facility",
+        distance: 9
+      )
     end
-  end
 
-  describe ".to_hash" do
-    subject { tab.to_hash }
+    let!(:hearing_location2) do
+      create(
+        :available_hearing_locations,
+        appeal_id: appeal.id,
+        appeal_type: "Appeal",
+        city: "San Francisco",
+        state: "CA",
+        distance: 100
+      )
+    end
 
-    it "returns a hash with the correct key" do
-      expect(subject.keys).to match_array([:columns])
+    let!(:task1) { create(:schedule_hearing_task, assigned_to: assignee, appeal: appeal) }
+    let!(:task2) { create(:schedule_hearing_task, assigned_to: assignee, appeal: appeal) }
+
+    describe ".power_of_attorney_name_options" do
+      subject { tab.power_of_attorney_name_options }
+
+      it "returns correct options" do
+        cache_ama_appeals
+
+        expect(subject.first[:value]).to eq(URI.escape(URI.escape(appeal.representative_name)))
+        expect(subject.first[:displayText]).to eq("#{appeal.representative_name} (2)")
+      end
+    end
+
+    describe ".suggested_location_options" do
+      subject { tab.suggested_location_options }
+
+      it "returns correct options" do
+        cache_ama_appeals
+
+        expect(subject.first[:value]).to eq(URI.escape(URI.escape(hearing_location1.formatted_location)))
+        expect(subject.first[:displayText]).to eq("#{hearing_location1.formatted_location} (2)")
+      end
+    end
+
+    describe ".columns" do
+      subject { tab.columns }
+
+      it "returns columns with the correct keys" do
+        expect(subject.first.keys).to match_array([:name, :filter_options])
+      end
+    end
+
+    describe ".to_hash" do
+      subject { tab.to_hash }
+
+      it "returns a hash with the correct key" do
+        expect(subject.keys).to match_array([:columns])
+      end
     end
   end
 end
