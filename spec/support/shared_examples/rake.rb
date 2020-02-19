52098a5a130f48b3db3acb59dbc66567482db5f0
Peter Karman <peter@truss.works>
2020-02-19 12:05:42 -0600

    lint

--- spec/support/shared_examples/rake.rb
+++ spec/support/shared_examples/rake.rb
@@ -1,14 +1,16 @@
-# spec/support/shared_examples/rake.rb
+# frozen_string_literal: true
+
+# via https://thoughtbot.com/blog/test-rake-tasks-like-a-boss
 require "rake"
 
 shared_context "rake" do
   let(:rake)      { Rake::Application.new }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
+  let(:task_path) { "lib/tasks/#{task_name.split(':').first}" }
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
-    $".reject {|file| file == Rails.root.join("#{task_path}.rake").to_s }
+    $LOADED_FEATURES.reject { |file| file == Rails.root.join("#{task_path}.rake").to_s }
   end
 
   before do
