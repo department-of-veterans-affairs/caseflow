1d81b67b1055cff064f09ce5cfbd8cd2ebb94670
jcq <2581274+jcq@users.noreply.github.com>
2020-02-20 18:15:17 -0800

    MTV | Attorney Checkout | Add Decisions View (#13402)
    Connects #13071
    
    ### Description
    This implements the "Add decisions" screen of the MTV attorney checkout flow. This view only applies to `vacate_and_readjudication` vacate types.
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly

--- client/app/queue/mtv/mtvCheckoutSteps.js
+++ client/app/queue/mtv/mtvCheckoutSteps.js
@@ -5,15 +5,15 @@ export const views = {
 };
 
 // This might be more elegantly modeled w/ a finite state machine lib like xstate
-export const getSteps = ({ type, vacateType }) => {
+export const getSteps = ({ caseType, vacateType }) => {
   switch (vacateType?.toLowerCase()) {
   case 'straight_vacate':
   case 'vacate_and_de_novo':
     return ['review_vacatures', 'submit'];
-  case 'vacate_and_readjudicate':
+  case 'vacate_and_readjudication':
     return ['review_vacatures', 'add_decisions', 'submit'];
   default:
-    return type?.toLowerCase() === 'de_novo' ? ['add_decisions', 'submit'] : [];
+    return caseType?.toLowerCase() === 'de_novo' ? ['add_decisions', 'submit'] : [];
   }
 };
 
@@ -26,5 +26,5 @@ export const getNextStep = (current, steps) => {
 export const getPrevStep = (current, steps) => {
   const idx = steps.indexOf(current);
 
-  return idx > 0 ? steps(idx - 1) : null;
+  return idx > 0 ? steps[idx - 1] : null;
 };
