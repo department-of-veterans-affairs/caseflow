5cac91cc217bff128900812e62d0df6a8db4c8ac
JC Quirin <jcq@jcq.local>
2020-02-21 12:01:04 -0800

    added success alerts after JudgeAddressMotionToVacate these are conditional based on the disposition selected

---
+++ client/app/queue/mtv/mtvMessages.js
@@ -0,0 +1,32 @@
+import {
+  JUDGE_ADDRESS_MTV_SUCCESS_TITLE_GRANTED,
+  JUDGE_ADDRESS_MTV_SUCCESS_DETAIL_GRANTED,
+  JUDGE_ADDRESS_MTV_SUCCESS_TITLE_DENIED,
+  JUDGE_ADDRESS_MTV_SUCCESS_DETAIL_DENIED
+} from '../../../COPY';
+import { sprintf } from 'sprintf-js';
+
+export const addressMTVSuccessAlert = ({ data, appeal }) => {
+  const { disposition } = data;
+  const { veteranFullName } = appeal;
+
+  switch (disposition) {
+  case 'granted':
+  case 'partial':
+    return {
+      title: sprintf(JUDGE_ADDRESS_MTV_SUCCESS_TITLE_GRANTED, veteranFullName),
+      detail: sprintf(JUDGE_ADDRESS_MTV_SUCCESS_DETAIL_GRANTED)
+    };
+  case 'denied':
+  case 'dismissed':
+    return {
+      title: sprintf(JUDGE_ADDRESS_MTV_SUCCESS_TITLE_DENIED, veteranFullName),
+      detail: sprintf(JUDGE_ADDRESS_MTV_SUCCESS_DETAIL_DENIED, disposition)
+    };
+  default:
+    return {
+      title: 'Task Complete',
+      detail: ' '
+    };
+  }
+};
