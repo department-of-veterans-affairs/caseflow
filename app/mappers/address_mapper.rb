department-of-veterans-affairs/caseflow

commit 8839eb1b63f7e84d8a399f3288688d1316e2af25

Fix undefined appellant address (#13411)
Resolves #13321

### Description

  * Fix bug where representative's participant id is being used to fetch the appellant's address for LegacyAppeal
  * Update tests to setup veteran and appellant address data in BGS for legacy appeals
  * Remove some unused functions and tests
  * Small variable naming change to remove a lint comment

app/mappers/address_mapper.rb
@@ -28,20 +28,6 @@ module AddressMapper
     }
   end
 
-  def get_address_from_veteran_record(veteran)
-    return nil unless veteran
-
-    {
-      address_line_1: veteran.address_line1,
-      address_line_2: veteran.address_line2,
-      address_line_3: veteran.address_line3,
-      city: veteran.city,
-      state: veteran.state,
-      country: veteran.country,
-      zip: veteran.zip_code
-    }
-  end
-
   def get_address_from_rep_entry(rep_entry)
     return {} unless rep_entry
 

app/models/legacy_appeal.rb
@@ -11,7 +11,6 @@ class LegacyAppeal < ApplicationRecord
   include AssociatedVacolsModel
   include BgsService
   include CachedAttributes
-  include AddressMapper
   include Taskable
   include PrintsTaskTree
   include HasTaskHistory
@@ -227,6 +226,12 @@ class LegacyAppeal < ApplicationRecord
     !!appellant_first_name
   end
 
+  def person_for_appellant
+    return nil if appellant_ssn.blank?
+
+    bgs.fetch_person_by_ssn(appellant_ssn)
+  end
+
   def veteran_if_exists
     @veteran_if_exists ||= Veteran.find_by_file_number(veteran_file_number)
   end
@@ -345,7 +350,7 @@ class LegacyAppeal < ApplicationRecord
         middle_name: appellant_middle_initial,
         last_name: appellant_last_name,
         name_suffix: appellant_name_suffix,
-        address: get_address_from_corres_entry(case_record.correspondent),
+        address: appellant_address,
         representative: representative_to_hash
       }
     else
@@ -354,7 +359,7 @@ class LegacyAppeal < ApplicationRecord
         middle_name: veteran_middle_initial,
         last_name: veteran_last_name,
         name_suffix: veteran_name_suffix,
-        address: get_address_from_veteran_record(veteran) || get_address_from_corres_entry(case_record.correspondent),
+        address: appellant_address,
         representative: representative_to_hash
       }
     end
@@ -822,7 +827,15 @@ class LegacyAppeal < ApplicationRecord
   end
 
   def bgs_address_service
-    @bgs_address_service ||= BgsAddressService.new(participant_id: representative_participant_id)
+    participant_id = if appellant_is_not_veteran
+                       person_for_appellant&.[](:ptcpnt_id)
+                     else
+                       veteran&.participant_id
+                     end
+
+    return nil if participant_id.blank?
+
+    @bgs_address_service ||= BgsAddressService.new(participant_id: participant_id)
   end
 
   def location_code_is_caseflow?
@@ -965,17 +978,6 @@ class LegacyAppeal < ApplicationRecord
       return "#{file_number.gsub(/^0*/, '')}C" if file_number.length.between?(3, 9)
 
       fail Caseflow::Error::InvalidFileNumber
-    end
-
-    # Because SSN is not accurate in VACOLS, we pull the file
-    # number from BGS for the SSN and use that to look appeals
-    # up in VACOLS
-    def vbms_id_for_ssn(ssn)
-      file_number = bgs.fetch_file_number_by_ssn(ssn)
-
-      fail ActiveRecord::RecordNotFound unless file_number
-
-      convert_file_number_to_vacols(file_number)
     end
 
     # Returns a hash of appeals with appeal_id as keys and

spec/factories/legacy_appeal.rb
@@ -4,10 +4,29 @@ FactoryBot.define do
   factory :legacy_appeal do
     transient do
       vacols_case { nil }
+      veteran_address { nil }
+      appellant_address { nil }
     end
 
     vacols_id { vacols_case&.bfkey || "123456" }
     vbms_id { vacols_case&.bfcorlid }
+
+    after(:create) do |appeal, evaluator|
+      if evaluator.veteran_address.present?
+        vbms_id = appeal.sanitized_vbms_id
+        veteran = Veteran.find_by(file_number: vbms_id) || create(:veteran, file_number: vbms_id)
+
+        (BGSService.address_records ||= {}).update(veteran.participant_id => evaluator.veteran_address)
+      end
+
+      if evaluator.appellant_address.present? && appeal.appellant_ssn.present?
+        # Creating a veteran has a side effect of populating `BGSService.veteran_store`.
+        # BGS should be setup to return the appellant's participant ID from their SSN.
+        appellant = create(:veteran, ssn: appeal.appellant_ssn)
+
+        (BGSService.address_records ||= {}).update(appellant.participant_id => evaluator.appellant_address)
+      end
+    end
 
     trait :with_schedule_hearing_tasks do
       after(:create) do |appeal, _evaluator|
@@ -29,11 +48,12 @@ FactoryBot.define do
 
     trait :with_veteran do
       after(:create) do |legacy_appeal, evaluator|
-        veteran = create(
+        file_number = legacy_appeal.veteran_file_number
+        veteran = Veteran.find_by(file_number: legacy_appeal.veteran_file_number) || create(
           :veteran,
-          file_number: legacy_appeal.veteran_file_number,
           first_name: "Bob",
-          last_name: "Smith"
+          last_name: "Smith",
+          file_number: file_number
         )
 
         if evaluator.vacols_case
@@ -42,6 +62,20 @@ FactoryBot.define do
           evaluator.vacols_case.correspondent.ssalut = "PhD"
           evaluator.vacols_case.correspondent.save
         end
+      end
+    end
+
+    trait :with_veteran_address do
+      veteran_address do
+        {
+          addrs_one_txt: FakeConstants.BGS_SERVICE.DEFAULT_ADDRESS_LINE_1,
+          addrs_two_txt: FakeConstants.BGS_SERVICE.DEFAULT_ADDRESS_LINE_2,
+          addrs_three_txt: FakeConstants.BGS_SERVICE.DEFAULT_ADDRESS_LINE_3,
+          city_nm: FakeConstants.BGS_SERVICE.DEFAULT_CITY,
+          cntry_nm: FakeConstants.BGS_SERVICE.DEFAULT_COUNTRY,
+          postal_cd: FakeConstants.BGS_SERVICE.DEFAULT_STATE,
+          zip_prefix_nbr: FakeConstants.BGS_SERVICE.DEFAULT_ZIP
+        }
       end
     end
   end

client/app/queue/components/AssignHearingModal.jsx
@@ -1,31 +1,30 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { sprintf } from 'sprintf-js';
-import {
-  resetErrorMessages,
-  showErrorMessage,
-  showSuccessMessage,
-  resetSuccessMessages,
-  requestPatch
-} from '../uiReducer/uiActions';
-import COPY from '../../../COPY.json';
-import { CENTRAL_OFFICE_HEARING, VIDEO_HEARING } from '../../hearings/constants';
-import { fullWidth } from '../constants';
-import { formatDateStr } from '../../util/DateUtil';
-import ApiUtil from '../../util/ApiUtil';
 import { withRouter } from 'react-router-dom';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import _ from 'lodash';
+
+import { CENTRAL_OFFICE_HEARING, VIDEO_HEARING } from '../../hearings/constants';
 import {
   appealWithDetailSelector,
   scheduleHearingTasksForAppeal
 } from '../selectors';
-import { onReceiveAmaTasks, onReceiveAppealDetails } from '../QueueActions';
+import { formatDateStr } from '../../util/DateUtil';
+import { fullWidth } from '../constants';
+import { onReceiveAppealDetails } from '../QueueActions';
 import { prepareAppealForStore } from '../utils';
-import _ from 'lodash';
-import QueueFlowModal from './QueueFlowModal';
+import {
+  showErrorMessage,
+  requestPatch
+} from '../uiReducer/uiActions';
 import Alert from '../../components/Alert';
+import ApiUtil from '../../util/ApiUtil';
 import AssignHearingForm from '../../hearings/components/modalForms/AssignHearingForm';
+import COPY from '../../../COPY';
+import QueueFlowModal from './QueueFlowModal';
 
 class AssignHearingModal extends React.PureComponent {
   constructor(props) {
@@ -188,13 +187,12 @@ class AssignHearingModal extends React.PureComponent {
   render = () => {
     const { appeal, openHearing } = this.props;
     const { showErrorMessages, showFullHearingDayWarning } = this.state;
-    const { address_line_1, city, state, zip } = appeal.appellantAddress || {};
+    const { address_line_1: addressLine1, city, state, zip } = appeal.appellantAddress || {};
 
     if (openHearing) {
       return null;
     }
 
-    /* eslint-disable camelcase */
     return (
       <QueueFlowModal
         submit={this.submit}
@@ -214,7 +212,7 @@ class AssignHearingModal extends React.PureComponent {
           }
           <p>
             Veteran Address<br />
-            {address_line_1}<br />
+            {addressLine1}<br />
             {`${city}, ${state} ${zip}`}
           </p>
           <AssignHearingForm
@@ -227,6 +225,41 @@ class AssignHearingModal extends React.PureComponent {
   }
 }
 
+AssignHearingModal.propTypes = {
+  openHearing: PropTypes.shape({
+    date: PropTypes.string
+  }),
+  appeal: PropTypes.shape({
+    appellantAddress: PropTypes.object,
+    externalId: PropTypes.string,
+    isLegacyAppeal: PropTypes.bool,
+    regionalOffice: PropTypes.object,
+    veteranFullName: PropTypes.string
+  }),
+  assignHearingForm: PropTypes.shape({
+    apiFormattedValues: PropTypes.object,
+    errorMessages: PropTypes.shape({
+      hasErrorMessages: PropTypes.bool
+    }),
+    hearingDay: PropTypes.shape({
+      hearingDate: PropTypes.string,
+      regionalOffice: PropTypes.string
+    })
+  }),
+  hearingDay: PropTypes.shape({
+    hearingDate: PropTypes.string,
+    regionalOffice: PropTypes.string
+  }),
+  scheduleHearingTask: PropTypes.shape({
+    taskId: PropTypes.string
+  }),
+  history: PropTypes.object,
+  onReceiveAppealDetails: PropTypes.func,
+  requestPatch: PropTypes.func,
+  showErrorMessage: PropTypes.func,
+  selectedRegionalOffice: PropTypes.string
+};
+
 const mapStateToProps = (state, ownProps) => ({
   scheduleHearingTask: scheduleHearingTasksForAppeal(state, { appealId: ownProps.appealId })[0],
   openHearing: _.find(
@@ -235,19 +268,13 @@ const mapStateToProps = (state, ownProps) => ({
   ),
   assignHearingForm: state.components.forms.assignHearing,
   appeal: appealWithDetailSelector(state, ownProps),
-  saveState: state.ui.saveState.savePending,
   selectedRegionalOffice: state.components.selectedRegionalOffice,
-  regionalOfficeOptions: state.components.regionalOffices,
   hearingDay: state.ui.hearingDay
 });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   showErrorMessage,
-  resetErrorMessages,
-  showSuccessMessage,
-  resetSuccessMessages,
   requestPatch,
-  onReceiveAmaTasks,
   onReceiveAppealDetails
 }, dispatch);
 

spec/controllers/hearings_controller_spec.rb
@@ -322,7 +322,7 @@ RSpec.describe HearingsController, :all_dbs, type: :controller do
 
     context "for legacy appeals" do
       let!(:vacols_case) { create(:case) }
-      let!(:legacy_appeal) { create(:legacy_appeal, vacols_case: vacols_case) }
+      let!(:legacy_appeal) { create(:legacy_appeal, :with_veteran_address, vacols_case: vacols_case) }
 
       it "returns an address" do
         get :find_closest_hearing_locations,

spec/feature/hearings/daily_docket/build_hearsched_spec.rb
@@ -13,7 +13,7 @@ feature "Hearing Schedule Daily Docket for Build HearSched", :all_dbs do
     end
 
     let!(:vacols_case) { create(:case, bfcorlid: "123456789S") }
-    let!(:legacy_appeal) { create(:legacy_appeal, vacols_case: vacols_case) }
+    let!(:legacy_appeal) { create(:legacy_appeal, :with_veteran, vacols_case: vacols_case) }
     let!(:hearing_location) do
       create(:available_hearing_locations,
              appeal_id: legacy_appeal.id,

spec/models/legacy_appeal_spec.rb
@@ -8,9 +8,16 @@ describe LegacyAppeal, :all_dbs do
   let(:yesterday) { 1.day.ago.to_formatted_s(:short_date) }
   let(:twenty_days_ago) { 20.days.ago.to_formatted_s(:short_date) }
   let(:last_year) { 365.days.ago.to_formatted_s(:short_date) }
+  let(:veteran_address) { nil }
+  let(:appellant_address) { nil }
 
   let(:appeal) do
-    create(:legacy_appeal, vacols_case: vacols_case)
+    create(
+      :legacy_appeal,
+      vacols_case: vacols_case,
+      veteran_address: veteran_address,
+      appellant_address: appellant_address
+    )
   end
 
   context "includes PrintsTaskTree concern" do
@@ -2189,13 +2196,7 @@ describe LegacyAppeal, :all_dbs do
              snamef: "Bobby",
              snamemi: "F",
              snamel: "Veteran",
-             ssalut: "",
-             saddrst1: "123 K St. NW",
-             saddrst2: "Suite 456",
-             saddrcty: "Washington",
-             saddrstt: "DC",
-             saddrcnty: nil,
-             saddrzip: "20001")
+             ssalut: "")
     end
     let!(:representative) do
       create(:representative,
@@ -2211,6 +2212,17 @@ describe LegacyAppeal, :all_dbs do
              repzip: "10000")
     end
     let!(:vacols_case) { create(:case, correspondent: correspondent, bfso: "T") }
+    let(:veteran_address) do
+      {
+        addrs_one_txt: "123 K St. NW",
+        addrs_two_txt: "Suite 456",
+        addrs_three_txt: nil,
+        city_nm: "Washington",
+        postal_cd: "DC",
+        cntry_nm: nil,
+        zip_prefix_nbr: "20001"
+      }
+    end
 
     context "when veteran is the appellant and addresses are included" do
       it "the veteran is returned with addresses" do
@@ -2222,6 +2234,7 @@ describe LegacyAppeal, :all_dbs do
           address: {
             address_line_1: "123 K St. NW",
             address_line_2: "Suite 456",
+            address_line_3: nil,
             city: "Washington",
             state: "DC",
             country: nil,
@@ -2262,6 +2275,7 @@ describe LegacyAppeal, :all_dbs do
           address: {
             address_line_1: "123 K St. NW",
             address_line_2: "Suite 456",
+            address_line_3: nil,
             city: "Washington",
             state: "DC",
             country: nil,
@@ -2292,15 +2306,21 @@ describe LegacyAppeal, :all_dbs do
                snamef: "Bobby",
                snamemi: "F",
                snamel: "Veteran",
-               saddrst1: "123 K St. NW",
-               saddrst2: "Suite 456",
-               saddrcty: "Washington",
-               saddrstt: "DC",
-               saddrcnty: nil,
-               saddrzip: "20001",
                sspare1: "Claimant",
                sspare2: "Tommy",
-               sspare3: "G")
+               sspare3: "G",
+               ssn: "123456789")
+      end
+      let(:appellant_address) do
+        {
+          addrs_one_txt: "456 K St. NW",
+          addrs_two_txt: "Suite 789",
+          addrs_three_txt: nil,
+          city_nm: "Washington",
+          postal_cd: "DC",
+          cntry_nm: nil,
+          zip_prefix_nbr: "20001"
+        }
       end
 
       it "the appellant is returned" do
@@ -2310,8 +2330,9 @@ describe LegacyAppeal, :all_dbs do
           last_name: "Claimant",
           name_suffix: nil,
           address: {
-            address_line_1: "123 K St. NW",
-            address_line_2: "Suite 456",
+            address_line_1: "456 K St. NW",
+            address_line_2: "Suite 789",
+            address_line_3: nil,
             city: "Washington",
             state: "DC",
             country: nil,

spec/models/serializers/work_queue/legacy_appeal_serializer_spec.rb
@@ -2,7 +2,7 @@
 
 describe WorkQueue::LegacyAppealSerializer, :all_dbs do
   let(:user) { create(:user) }
-  let(:legacy_appeal) { create(:legacy_appeal, vacols_case: create(:case)) }
+  let(:legacy_appeal) { create(:legacy_appeal, :with_veteran_address, vacols_case: create(:case)) }
 
   subject { described_class.new(legacy_appeal, params: { user: user }) }
 

commit 07529457be1afb18c7b1f04afd2df36af9b1b106

Fallback to appellant address in VACOLS for LegacyAppeal (#13549)
https://dsva.slack.com/archives/CD5DAQNCU/p1582834251001700

### Description

Add fallback to VACOLS for appellant address since BGS doesn't always have data. This tries to preserve populating the address from a consistent source (e.g. calling `claimant[:address]` and `appellant_address` should give you the same thing)

Fixes a regression from: https://github.com/department-of-veterans-affairs/caseflow/pull/13411

.reek.yml
@@ -28,6 +28,10 @@ detectors:
     exclude:
       - Address
       - Fakes::PersistentStore#convert_dates_from
+  UncommunicativeMethodName:
+    exclude:
+      - LegacyAppeal#appellant_address_line_1
+      - LegacyAppeal#appellant_address_line_2
   UncommunicativeParameterName:
     exclude:
       - Address
@@ -113,6 +117,7 @@ detectors:
     exclude:
       - HearingLocation
       - JudgeTeam
+      - LegacyAppeal
       - QueueConfig
       - RequestIssue
       - BulkTaskReassignment
@@ -135,6 +140,7 @@ detectors:
   UtilityFunction:
     public_methods_only: true
     exclude:
+      - AddressMapper#get_address_from_veteran_record
       - ClaimReviewAsyncStatsReporter#seconds_to_hms
       - ETL::Builder#last_built
       - ETL::Syncer#filter?

app/mappers/address_mapper.rb
@@ -29,6 +29,20 @@ module AddressMapper
     }
   end
 
+  def get_address_from_veteran_record(veteran)
+    return nil unless veteran
+
+    {
+      address_line_1: veteran.address_line1,
+      address_line_2: veteran.address_line2,
+      address_line_3: veteran.address_line3,
+      city: veteran.city,
+      state: veteran.state,
+      country: veteran.country,
+      zip: veteran.zip_code
+    }
+  end
+
   def get_address_from_rep_entry(rep_entry)
     return {} unless rep_entry
 

app/models/legacy_appeal.rb
@@ -11,6 +11,7 @@ class LegacyAppeal < ApplicationRecord
   include AssociatedVacolsModel
   include BgsService
   include CachedAttributes
+  include AddressMapper
   include Taskable
   include PrintsTaskTree
   include HasTaskHistory
@@ -78,11 +79,6 @@ class LegacyAppeal < ApplicationRecord
   # NOTE: we cannot currently match end products to a specific appeal.
   delegate :end_products,
            to: :veteran,
-           allow_nil: true
-
-  delegate :address, :address_line_1, :address_line_2, :city, :country, :state, :zip,
-           to: :bgs_address_service,
-           prefix: :appellant,
            allow_nil: true
 
   cache_attribute :aod do
@@ -219,8 +215,42 @@ class LegacyAppeal < ApplicationRecord
     (decision_date + 120.days).to_date
   end
 
-  def appellant
-    claimant
+  def appellant_address
+    appellant_address_from_bgs = bgs_address_service&.address
+
+    # Attempt to get the address from BGS, or fall back to VACOLS. These are expected
+    # to have the same hash keys.
+    if appellant_is_not_veteran
+      appellant_address_from_bgs || get_address_from_corres_entry(case_record.correspondent)
+    else
+      appellant_address_from_bgs ||
+        get_address_from_veteran_record(veteran) ||
+        get_address_from_corres_entry(case_record.correspondent)
+    end
+  end
+
+  def appellant_address_line_1
+    appellant_address&.[](:address_line_1)
+  end
+
+  def appellant_address_line_2
+    appellant_address&.[](:address_line_2)
+  end
+
+  def appellant_city
+    appellant_address&.[](:city)
+  end
+
+  def appellant_country
+    appellant_address&.[](:country)
+  end
+
+  def appellant_state
+    appellant_address&.[](:state)
+  end
+
+  def appellant_zip
+    appellant_address&.[](:zip)
   end
 
   def appellant_is_not_veteran
@@ -365,6 +395,8 @@ class LegacyAppeal < ApplicationRecord
       }
     end
   end
+
+  alias appellant claimant
 
   # reptype C is a contested claimant
   def contested_claimants