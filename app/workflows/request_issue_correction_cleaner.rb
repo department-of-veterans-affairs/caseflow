b980c752df17283167eb539d11aff1b01cff6c1f
Sally Maki <sallymaki@gmail.com>
2020-02-28 14:59:14 -0800

    Remove extra line

--- app/workflows/request_issue_correction_cleaner.rb
+++ app/workflows/request_issue_correction_cleaner.rb
@@ -23,7 +23,6 @@ class RequestIssueCorrectionCleaner
   def request_issue_to_remove
     @request_issue_to_remove ||= begin
       request_issue = contested_decision_issue&.contesting_remand_request_issue
-
       request_issue unless request_issue == correction_request_issue
     end
   end
