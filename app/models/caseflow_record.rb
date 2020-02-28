0ecff8f83698d889158795fe044195abb209100f
Peter Karman <peter@truss.works>
2020-02-28 16:59:04 -0600

    comment

--- app/models/caseflow_record.rb
+++ app/models/caseflow_record.rb
@@ -6,4 +6,8 @@ class CaseflowRecord < ApplicationRecord
   # all caseflow db models should inherit from this class.
   # all vacols models inherit from VACOLS::Record
   # all etl models inherit from ETL::Record
+
+  # In theory, this class should not need any additional code.
+  # It's here to make generating documentation easier, by clearly
+  # delineating db models via class inheritance.
 end
