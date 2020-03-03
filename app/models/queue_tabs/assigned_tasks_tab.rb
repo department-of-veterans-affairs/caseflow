41ab59f8be3d5e6d1b1b46f8b147a31bfd6207d1
hschallhorn <hschallhorn@tistatech.com>
2020-03-03 10:18:04 -0500

    Rename colocated queue copy to be more generic

--- app/models/queue_tabs/assigned_tasks_tab.rb
+++ app/models/queue_tabs/assigned_tasks_tab.rb
@@ -12,7 +12,7 @@ class AssignedTasksTab < QueueTab
   end
 
   def description
-    COPY::COLOCATED_QUEUE_PAGE_NEW_TASKS_DESCRIPTION
+    COPY::USER_QUEUE_PAGE_NEW_TASKS_DESCRIPTION
   end
 
   def tasks
