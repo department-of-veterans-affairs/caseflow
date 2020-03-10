d13bb804144d87c19823189618682fc7e9654060
Ferris Tseng <ferris@navapbc.com>
2020-03-10 17:43:42 -0400

    add logic to determine where to place the docket line relative to the current page

--- app/models/hearings/schedule_hearing_task_pager.rb
+++ app/models/hearings/schedule_hearing_task_pager.rb
@@ -19,6 +19,52 @@ class Hearings::ScheduleHearingTaskPager < TaskPager
     @tasks_for_tab ||= tab.tasks
   end
 
+  # Returns all results after the current page (unfiltered) in sorted order.
+  def tasks_after_current_page
+    tasks_for_tab.offset(page * TASKS_PER_PAGE)
+  end
+
+  # Get count of records where AOD = true OR docket range date is before end date 
+  # AFTER the current page
+  #
+  # If there are records after the current page, set "docket_line_index" to null
+  #
+  # If there aren't records after the current page, traverse the list of tasks, and figure
+  # out where the last record with a docket_range_date is, and have that be the index for 
+  # "docket_line_index"
+  def docket_line_index(current_page)
+    case appeal_type
+    when Appeal.name
+      appeals_past_docket_line_count = tasks_after_current_page
+        .joins(Arel.sql(<<-SQL))
+          LEFT JOIN appeals
+          ON tasks.appeal_id = appeals.id
+          AND tasks.appeal_type = 'Appeal'
+        SQL
+        .where(Arel.sql(<<-SQL), current_date: Time.zone.today)
+          cached_appeal_attributes.is_aod OR
+          appeals.docket_range_date < :current_date
+        SQL
+        .count
+
+      return nil if appeals_past_docket_line_count > 0
+
+      docket_range_cutoff = (Time.zone.today + 1.month).end_of_month
+      idx = current_page.size - 1
+      current_page.reverse_each do |task|
+        if task&.appeal&.docket_range_date &.< docket_range_cutoff
+          break
+        else
+          idx -= 1
+        end
+      end
+
+      idx < 0 ? nil : idx
+    else
+      nil
+    end
+  end
+
   # Sorting by docket number within each category of appeal: AOD and normal.
   def sorted_tasks(tasks)
     tasks.order(Arel.sql(<<-SQL))
