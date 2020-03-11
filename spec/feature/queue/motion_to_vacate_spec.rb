ce9fc1cce5b9195caf8f3ab47adc38fdabbbefb3
JC Quirin <jcq@jcq.lan>
2020-03-11 12:32:38 -0700

    updated test that checks for success banner after attorney sends MTV to judge

--- spec/feature/queue/motion_to_vacate_spec.rb
+++ spec/feature/queue/motion_to_vacate_spec.rb
@@ -127,7 +127,9 @@ RSpec.feature "Motion to vacate", :all_dbs do
 
         # Return back to user's queue
         expect(page).to have_current_path("/queue")
-        expect(page).to have_content(format(COPY::MOTIONS_ATTORNEY_REVIEW_MTV_SUCCESS_TITLE, appeal.veteran_full_name))
+        
+        expect(page).to have_content(format(COPY::MOTIONS_ATTORNEY_REVIEW_MTV_SUCCESS_TITLE, judge2.display_name))
+        expect(page).to have_content(COPY::MOTIONS_ATTORNEY_REVIEW_MTV_SUCCESS_DETAIL)
 
         # Verify new task was created
         judge_task = JudgeAddressMotionToVacateTask.find_by(assigned_to: judge2)
