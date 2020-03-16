2a2fad51a39874fe12ce136a7aac36c8c1ab019b
jcq <2581274+jcq@users.noreply.github.com>
2020-03-16 10:05:16 -0700

    Restructured LookupParticipantIdModal to Remove Redux Alerts (#13640)
    Connects #13615
    
    ### Description
    This restructures the `LookupParticipantIdModal` component to remove Redux connections and re-implement success/error `Alert` within this component rather than `QueueFlowModal` (from which they were removed).
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly
    - [ ] Alerts show within `LookupParticipantIdModal`
    - [ ] Alerts do not show on Team Management page behind modal
    - [ ] Redux-connected success Alerts do not show in other components that rely on `QueueFlowModal`
    
    ### Testing Plan
    1. Go to "Caseflow Team Management"
    2. Scroll down to "Private Bar" section and click "Look up Participant ID" button
    3. Perform lookups and verify alert presence
    
    ### User Facing Changes
    
     - [ ] Screenshots of UI changes added to PR & Original Issue
     
    ### Documentation Updates
    - [ ] Topline File Descriptions Added or Updated as Needed

--- client/app/queue/LookupParticipantIdModal.jsx
+++ client/app/queue/LookupParticipantIdModal.jsx
@@ -3,33 +3,13 @@ import ApiUtil from '../util/ApiUtil';
 import Button from '../components/Button';
 import COPY from '../../COPY';
 import LoadingDataDisplay from '../components/LoadingDataDisplay';
-import PropTypes from 'prop-types';
 import SearchableDropdown from '../components/SearchableDropdown';
 import TextField from '../components/TextField';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import { css } from 'glamor';
 import { LOGO_COLORS } from '../constants/AppConstants';
-import {
-  hideErrorMessage,
-  hideSuccessMessage,
-  requestGet,
-  showErrorMessage
-} from './uiReducer/uiActions';
-import { withRouter } from 'react-router-dom';
 import QueueFlowModal from './components/QueueFlowModal';
-
-const textFieldStyling = css({
-  float: 'left',
-  padding: '0 1.5rem',
-  width: '50%'
-});
-
-const searchButtonStyling = css({
-  marginTop: '1rem',
-  textAlign: 'right',
-  width: '100%'
-});
+import Alert from '../components/Alert';
+import styles from './LookupParticipantIdModal.module.scss';
 
 class LookupParticipantIdModal extends React.Component {
   constructor(props) {
@@ -40,64 +20,71 @@ class LookupParticipantIdModal extends React.Component {
       selectedUser: null,
       results: null,
       css_id: null,
-      station_id: null
+      station_id: null,
+      alert: null,
+      error: ''
     };
-  }
-
-  componentWillUnmount = () => this.clearAlerts();
-
-  clearAlerts = () => {
-    this.props.hideErrorMessage();
-    this.props.hideSuccessMessage();
   }
 
   loadingPromise = () => {
     return ApiUtil.get('/users?role=non_judges').then((resp) => {
       return this.setState({ users: resp.body.non_judges.data });
     });
-  }
+  };
 
-  setCssId = (value) => this.setState({
-    css_id: value,
-    selectedUser: null
-  });
+  setCssId = (value) =>
+    this.setState({
+      css_id: value,
+      selectedUser: null
+    });
 
-  setStationId = (value) => this.setState({
-    selectedUser: null,
-    station_id: value
-  });
+  setStationId = (value) =>
+    this.setState({
+      selectedUser: null,
+      station_id: value
+    });
 
-  selectUser = (value) => this.setState({
-    css_id: null,
-    selectedUser: value,
-    station_id: null
-  });
+  selectUser = (value) =>
+    this.setState({
+      css_id: null,
+      selectedUser: value,
+      station_id: null
+    });
 
   formatName = (user) => `${user.attributes.full_name} (${user.attributes.css_id})`;
 
-  dropdownOptions = () =>
-    this.state.users.map((user) => ({ label: this.formatName(user),
-      value: user }));
+  dropdownOptions = () => this.state.users.map((user) => ({ label: this.formatName(user),
+    value: user }));
 
-  search = () => {
-    let url = '';
-
-    if (this.state.css_id && this.state.station_id) {
-      url = `/user_info/represented_organizations?css_id=${this.state.css_id}&station_id=${this.state.station_id}`;
-    } else {
-      url = `/users/${this.state.selectedUser.value.id}/represented_organizations`;
-    }
+  search = async () => {
+    const { css_id: cssId, station_id: stationId, selectedUser } = this.state;
+    const url =
+      cssId && stationId ?
+        `/user_info/represented_organizations?css_id=${cssId}&station_id=${stationId}` :
+        `/users/${selectedUser.value.id}/represented_organizations`;
 
     // Clear previous results before every attempt.
-    this.setState({ representedOrganizations: null });
-    this.clearAlerts();
+    this.setState({
+      representedOrganizations: null,
+      alert: null
+    });
 
-    return this.props.requestGet(url).
-      then((resp) => this.setState({ representedOrganizations: resp.body.represented_organizations })).
-      catch(
-        // Errors caught and displayed in requestGet().
-      );
-  }
+    try {
+      const { res } = await ApiUtil.get(url);
+
+      this.setState({
+        representedOrganizations: res.body.represented_organizations,
+        alert: 'success'
+      });
+    } catch ({ response }) {
+      const error = response?.body?.errors?.[0];
+
+      this.setState({
+        alert: 'error',
+        error: error.detail
+      });
+    }
+  };
 
   representedOrganizationsList = () => {
     const orgs = this.state.representedOrganizations;
@@ -110,77 +97,75 @@ class LookupParticipantIdModal extends React.Component {
       return <p>{COPY.LOOKUP_PARTICIPANT_ID_MODAL_NO_ORGS_MESSAGE}</p>;
     }
 
-    return <ol>
-      { orgs.map((org, idx) =>
-        <li key={idx}>{org.representative_name}, {org.representative_type}. Participant ID: {org.participant_id}</li>
-      ) }
-    </ol>;
+    return (
+      <ol>
+        {orgs.map((org, idx) => (
+          <li key={idx}>
+            {org.representative_name}, {org.representative_type}. Participant ID: {org.participant_id}
+          </li>
+        ))}
+      </ol>
+    );
   };
 
   render = () => {
-    return <QueueFlowModal
-      title={COPY.LOOKUP_PARTICIPANT_ID_MODAL_TITLE}
-      pathAfterSubmit="/team_management"
-      button="Close"
-      submit={() => Promise.resolve()}
-    >
-      <LoadingDataDisplay
-        createLoadPromise={this.loadingPromise}
-        loadingComponentProps={{
-          spinnerColor: LOGO_COLORS.QUEUE.ACCENT,
-          message: 'Loading users...'
-        }}
-        failStatusMessageProps={{ title: 'Unable to load users' }}>
-        <React.Fragment>
-          <SearchableDropdown
-            name={COPY.LOOKUP_PARTICIPANT_ID_SELECT_USER_LABEL}
-            hideLabel
-            searchable
-            placeholder={COPY.LOOKUP_PARTICIPANT_ID_SELECT_USER_LABEL}
-            value={this.state.selectedUser}
-            onChange={this.selectUser}
-            options={this.dropdownOptions()} />
-          <p {...css({ fontWeight: 'bold',
-            textAlign: 'center' })}>OR</p>
-          <div {...textFieldStyling}>
-            <TextField
-              name="CSS ID"
-              value={this.state.css_id}
-              onChange={this.setCssId}
+    const { alert, error } = this.state;
+
+    return (
+      <QueueFlowModal
+        title={COPY.LOOKUP_PARTICIPANT_ID_MODAL_TITLE}
+        pathAfterSubmit="/team_management"
+        button="Close"
+        submit={() => Promise.resolve()}
+      >
+        <LoadingDataDisplay
+          createLoadPromise={this.loadingPromise}
+          loadingComponentProps={{
+            spinnerColor: LOGO_COLORS.QUEUE.ACCENT,
+            message: 'Loading users...'
+          }}
+          failStatusMessageProps={{ title: 'Unable to load users' }}
+        >
+          <React.Fragment>
+            {alert && (
+              <div className={styles.alert}>
+                {alert === 'success' && <Alert type={alert} title="Lookup Succeeded" />}
+                {alert === 'error' && (
+                  <Alert type={alert} title="Error">
+                    {error ? error : COPY.LOOKUP_PARTICIPANT_ID_MODAL_ERROR_GENERIC}
+                  </Alert>
+                )}
+              </div>
+            )}
+
+            <SearchableDropdown
+              name={COPY.LOOKUP_PARTICIPANT_ID_SELECT_USER_LABEL}
+              hideLabel
+              searchable
+              placeholder={COPY.LOOKUP_PARTICIPANT_ID_SELECT_USER_LABEL}
+              value={this.state.selectedUser}
+              onChange={this.selectUser}
+              options={this.dropdownOptions()}
             />
-          </div>
-          <div {...textFieldStyling}>
-            <TextField
-              name="Station ID"
-              value={this.state.station_id}
-              onChange={this.setStationId}
-            />
-          </div>
-          { this.representedOrganizationsList() }
-          <div {...searchButtonStyling}>
-            <Button name="Search" onClick={this.search} />
-          </div>
-        </React.Fragment>
-      </LoadingDataDisplay>
-    </QueueFlowModal>;
+            <p {...css({ fontWeight: 'bold',
+              textAlign: 'center' })}>OR</p>
+            <div className={styles.textfield}>
+              <TextField name="CSS ID" value={this.state.css_id} onChange={this.setCssId} />
+            </div>
+            <div className={styles.textfield}>
+              <TextField name="Station ID" value={this.state.station_id} onChange={this.setStationId} />
+            </div>
+            {this.representedOrganizationsList()}
+            <div className={styles.searchBtn}>
+              <Button name="Search" onClick={this.search} />
+            </div>
+          </React.Fragment>
+        </LoadingDataDisplay>
+      </QueueFlowModal>
+    );
   };
 }
 
-LookupParticipantIdModal.propTypes = {
-  hideErrorMessage: PropTypes.func,
-  hideSuccessMessage: PropTypes.func,
-  requestGet: PropTypes.func,
-  showErrorMessage: PropTypes.func
-};
+LookupParticipantIdModal.propTypes = {};
 
-const mapStateToProps = () => ({});
-
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  hideErrorMessage,
-  hideSuccessMessage,
-  requestGet,
-  showErrorMessage
-}, dispatch);
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LookupParticipantIdModal));
-
+export default LookupParticipantIdModal;
