73e3fbc43a31836edf51d617d1e4ac244208bba4
hschallhorn <hschallhorn@tistatech.com>
2020-03-18 11:11:08 -0400

    Clean up tests

--- spec/controllers/distributions_controller_spec.rb
+++ spec/controllers/distributions_controller_spec.rb
@@ -33,44 +33,43 @@ describe DistributionsController, :all_dbs do
     end
 
     context "provided user is a judge" do
+      before { create(:staff, :judge_role, sdomainid: user.css_id) }
+
       it "renders the created distribution as json" do
-        create(:staff, :judge_role, sdomainid: user.css_id)
         subject
 
         expect(response.status).to eq 200
         body = JSON.parse(response.body)
         expect(body["distribution"].keys).to match_array(%w[id created_at updated_at status distributed_cases_count])
       end
-    end
 
-    context "provided user is a judge, but is not the current user" do
-      it "returns an error" do
-        create(:staff, :judge_role, sdomainid: user.css_id)
-        other_user = create(:user)
-        User.authenticate!(user: other_user)
+      context "but is not the logged in user" do
+        let(:authed_user) { create(:user) }
 
-        subject
+        before { User.authenticate!(user: authed_user) }
 
-        body = JSON.parse(response.body)
-        expect(body["errors"].first["title"]).to eq "Cannot request cases for another judge"
-      end
-    end
+        it "returns an error" do
+          subject
 
-    context "provided user is a judge and current user is on the special case movement team" do
-      before { FeatureToggle.enable!(:scm_view_judge_assign_queue) }
-      after { FeatureToggle.disable!(:scm_view_judge_assign_queue) }
+          body = JSON.parse(response.body)
+          expect(body["errors"].first["title"]).to eq "Cannot request cases for another judge"
+        end
 
-      it "renders the created distribution as json" do
-        create(:staff, :judge_role, sdomainid: user.css_id)
-        scm_user = create(:user)
-        SpecialCaseMovementTeam.singleton.add_user(scm_user)
-        User.authenticate!(user: scm_user)
+        context "but scm is enabled" do
+          before do
+            FeatureToggle.enable!(:scm_view_judge_assign_queue)
+            SpecialCaseMovementTeam.singleton.add_user(authed_user)
+          end
+          after { FeatureToggle.disable!(:scm_view_judge_assign_queue) }
 
-        subject
+          it "renders the created distribution as json" do
+            subject
 
-        expect(response.status).to eq 200
-        body = JSON.parse(response.body)
-        expect(body["distribution"].keys).to match_array(%w[id created_at updated_at status distributed_cases_count])
+            expect(response.status).to eq 200
+            body = JSON.parse(response.body)
+            expect(body["distribution"].keys).to match_array(%w[id created_at updated_at status distributed_cases_count])
+          end
+        end
       end
     end
   end
