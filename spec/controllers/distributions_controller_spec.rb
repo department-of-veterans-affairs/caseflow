dd2c2039fb02495bd053fdf7d97779f4d2309d57
hschallhorn <hschallhorn@tistatech.com>
2020-03-18 15:16:39 -0400

    fix test

--- spec/controllers/distributions_controller_spec.rb
+++ spec/controllers/distributions_controller_spec.rb
@@ -67,7 +67,7 @@ describe DistributionsController, :all_dbs do
 
             expect(response.status).to eq 200
             body = JSON.parse(response.body)
-            expect(body["errors"].length).to eq 0
+            expect(body["errors"]).to eq nil
             expect(body["distribution"].keys).to match_array(
               %w[id created_at updated_at status distributed_cases_count]
             )
