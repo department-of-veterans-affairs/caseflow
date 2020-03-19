b09f6c3efe37b70ff412df68494090ad438564c9
Tomas Apodaca <tomas@navapbc.com>
2020-03-19 15:45:00 -0700

    add conditional to migration

--- db/migrate/20200318204112_add_judge_email_and_judge_email_sent_to_virtual_hearings.rb
+++ db/migrate/20200318204112_add_judge_email_and_judge_email_sent_to_virtual_hearings.rb
@@ -1,10 +1,14 @@
 class AddJudgeEmailAndJudgeEmailSentToVirtualHearings < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
-      add_column :virtual_hearings, :judge_email, :string,
-                 comment: "Judge's email address"
-      add_column :virtual_hearings, :judge_email_sent, :boolean, default: false, null: false,
-                 comment: "Whether or not a notification email was sent to the judge"
+      unless column_exists? :virtual_hearings, :judge_email, :string
+        add_column :virtual_hearings, :judge_email, :string,
+                   comment: "Judge's email address"
+      end
+      unless column_exists? :virtual_hearings, :judge_email_sent, :boolean
+        add_column :virtual_hearings, :judge_email_sent, :boolean, default: false, null: false,
+                   comment: "Whether or not a notification email was sent to the judge"
+      end
     end
   end
 end
