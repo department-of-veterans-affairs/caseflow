f64f2325055616a4f3f07ed9805472e6610fe5be
Tomas Apodaca <tomas@navapbc.com>
2020-03-20 08:46:43 -0700

    make less complex for codeclimate

--- db/migrate/20200318204112_add_judge_email_and_judge_email_sent_to_virtual_hearings.rb
+++ db/migrate/20200318204112_add_judge_email_and_judge_email_sent_to_virtual_hearings.rb
@@ -1,14 +1,27 @@
 class AddJudgeEmailAndJudgeEmailSentToVirtualHearings < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured do
-      unless column_exists? :virtual_hearings, :judge_email, :string
-        add_column :virtual_hearings, :judge_email, :string,
-                   comment: "Judge's email address"
-      end
-      unless column_exists? :virtual_hearings, :judge_email_sent, :boolean
-        add_column :virtual_hearings, :judge_email_sent, :boolean, default: false, null: false,
-                   comment: "Whether or not a notification email was sent to the judge"
-      end
+  def maybe_add_judge_email
+    unless column_exists? :virtual_hearings, :judge_email, :string
+      add_column :virtual_hearings, :judge_email, :string,
+                 comment: "Judge's email address"
     end
+  end
+
+  def maybe_add_judge_email_sent
+    unless column_exists? :virtual_hearings, :judge_email_sent, :boolean
+      add_column :virtual_hearings, :judge_email_sent, :boolean, default: false, null: false,
+                 comment: "Whether or not a notification email was sent to the judge"
+    end
+  end
+
+  def up
+    safety_assured do
+      maybe_add_judge_email
+      maybe_add_judge_email_sent
+    end
+  end
+
+  def down
+    remove_column :virtual_hearings, :judge_email, :string
+    remove_column :virtual_hearings, :judge_email_sent, :boolean
   end
 end
