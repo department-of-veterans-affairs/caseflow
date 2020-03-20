747f86ba4baa6372329b046fdde7c6182d31a238
jcq <2581274+jcq@users.noreply.github.com>
2020-03-20 16:10:11 -0700

    Updates re Hyperlink Field in MTV Deny/Dismiss Flow (#13765)
    Connects #13725
    
    ### Description
    This makes numerous adjustments around the handling of the `hyperlink` field in MTV flows
    
    ### Acceptance Criteria
    - [x] Code compiles correctly
    - [x] Remove "help text" from hyperlink field for attorney view
    - [x] Remove "help text" from hyperlink field for judge view
    - [x] Auto-format judge task instructions so that hyperlink is a link (open in new tab)

--- client/app/queue/mtv/MotionsAttorneyDisposition.jsx
+++ client/app/queue/mtv/MotionsAttorneyDisposition.jsx
@@ -19,6 +19,7 @@ import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolki
 import { css } from 'glamor';
 import { MTVTaskHeader } from './MTVTaskHeader';
 import { DISPOSITION_TEXT } from '../../../constants/MOTION_TO_VACATE';
+import { dispositionStrings } from './mtvConstants';
 import { sprintf } from 'sprintf-js';
 
 const formatReviewAttyInstructions = ({ disposition, hyperlink, instructions }) => {
@@ -58,9 +59,6 @@ export const MotionsAttorneyDisposition = ({ judges, selectedJudge, task, appeal
     return true;
   };
 
-  const labelText = <span>Upload the draft to your shared drive and add the location below,<br></br>
-              or encrypt it and email it to the VLJ who originally signed the case.</span>;
-
   return (
     <div className="address-motion-to-vacate">
       <AppSegment filledBackground>
@@ -89,13 +87,13 @@ export const MotionsAttorneyDisposition = ({ judges, selectedJudge, task, appeal
         {disposition && disposition === 'denied' && (
           <TextField
             name="hyperlink"
-            label={MOTIONS_ATTORNEY_REVIEW_MTV_HYPERLINK_LABEL}
+            label={sprintf(MOTIONS_ATTORNEY_REVIEW_MTV_HYPERLINK_LABEL, dispositionStrings[disposition])}
             value={hyperlink}
             onChange={(val) => setHyperlink(val)}
             optional
             strongLabel
-            labelText={labelText}
-            className={['mtv-review-hyperlink']} />
+            className={['mtv-review-hyperlink', 'cf-margin-bottom-2rem']}
+          />
         )}
 
         <SearchableDropdown
