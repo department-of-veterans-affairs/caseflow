d424c0b273589d5202c71b4ac599075b2d280604
yoomlam <yoom@navapbc.com>
2020-03-23 17:43:15 -0500

    run all stats collectors in one job

---
+++ app/jobs/collectors/request_issues_stats_collector.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Collectors::RequestIssuesStatsCollector
+  METRIC_NAME_PREFIX = "request_issues.unidentified_with_contention"
+
+  def collect_stats
+    start_of_month = Time.zone.now.prev_month.beginning_of_month
+    {}.tap do |stats|
+      req_issues = unidentified_request_issues_with_contention(start_of_month, start_of_month.next_month)
+      stats[METRIC_NAME_PREFIX] = req_issues.count
+
+      req_issues.group(:closed_status).count.each do |ben_type, count|
+        stats["#{METRIC_NAME_PREFIX}.st.#{ben_type || 'nil'}"] = count
+      end
+
+      req_issues.group(:benefit_type).count.each do |ben_type, count|
+        stats["#{METRIC_NAME_PREFIX}.ben.#{ben_type}"] = count
+      end
+
+      dr_counts_by_type = req_issues.group(:decision_review_type).count
+      dr_counts_by_type.each do |dr_type, count|
+        stats["#{METRIC_NAME_PREFIX}.dr.#{dr_type}"] = count
+      end
+
+      # Could use `req_issues.group(:veteran_participant_id).count.count` but there's a count discrepancy
+      stats["#{METRIC_NAME_PREFIX}.vet_count"] = req_issues.map(&:decision_review).map(&:veteran_file_number).uniq.count
+    end
+  end
+
+  private
+
+  def unidentified_request_issues_with_contention(start_date, end_date)
+    RequestIssue.where.not(contention_reference_id: nil)
+      .where(is_unidentified: true)
+      .where("created_at >= ?", start_date)
+      .where("created_at < ?", end_date)
+  end
+end
---
+++ spec/jobs/stats_collector_job_spec.rb
@@ -0,0 +1,153 @@
+# frozen_string_literal: true
+
+describe StatsCollectorJob do
+  context "when the entire job fails" do
+    let(:error_msg) { "Some dummy error" }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:run_collectors).and_raise(error_msg)
+    end
+
+    it "sends a message to Slack that includes the error" do
+      slack_msg = ""
+      allow_any_instance_of(SlackService).to receive(:send_notification) { |_, first_arg| slack_msg = first_arg }
+
+      described_class.perform_now
+
+      expected_msg = "#{described_class.name} failed after running for .*. Fatal error: #{error_msg}"
+      expect(slack_msg).to match(/#{expected_msg}/)
+    end
+  end
+
+  class ExampleDailyCollector
+    def collect_stats
+      {
+        "ex_metric_daily" => 9,
+        "ex_metric_daily.stats1" => 2,
+        "ex_metric_daily.statsA.metric2" => 4
+      }
+    end
+  end
+
+  context "on different days" do
+    class ExampleWeeklyCollector
+      def collect_stats
+        {}
+      end
+    end
+    class ExampleMonthlyCollector
+      def collect_stats
+        {}
+      end
+    end
+
+    before do
+      stub_const("StatsCollectorJob::DAILY_COLLECTORS", { "test_daily_collector" => ExampleDailyCollector }.freeze)
+      stub_const("StatsCollectorJob::WEEKLY_COLLECTORS", "test_weekly_collector" => ExampleWeeklyCollector)
+      stub_const("StatsCollectorJob::MONTHLY_COLLECTORS", "test_monthly_collector" => ExampleMonthlyCollector)
+    end
+    context "on Tuesday (2020, 3, 24)" do
+      it "daily collector runs" do
+        expect_any_instance_of(ExampleDailyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleWeeklyCollector).not_to receive(:collect_stats)
+        expect_any_instance_of(ExampleMonthlyCollector).not_to receive(:collect_stats)
+        Timecop.freeze(2020, 3, 24) { described_class.perform_now }
+      end
+    end
+    context "on Sunday (2020, 3, 22)" do
+      it "daily and weekly collectors run" do
+        expect_any_instance_of(ExampleDailyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleWeeklyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleMonthlyCollector).not_to receive(:collect_stats)
+        Timecop.freeze(2020, 3, 22) { described_class.perform_now }
+      end
+    end
+    context "on first day of the month (2020, 4, 1)" do
+      it "daily and monthly collectors run" do
+        expect_any_instance_of(ExampleDailyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleWeeklyCollector).not_to receive(:collect_stats)
+        expect_any_instance_of(ExampleMonthlyCollector).to receive(:collect_stats)
+        Timecop.freeze(2020, 4, 1) { described_class.perform_now }
+      end
+    end
+    context "on first day of the month that is a Sunday (2020, 3, 1)" do
+      it "daily, weekly, and monthly collectors run" do
+        expect_any_instance_of(ExampleDailyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleWeeklyCollector).to receive(:collect_stats)
+        expect_any_instance_of(ExampleMonthlyCollector).to receive(:collect_stats)
+        Timecop.freeze(2020, 3, 1) { described_class.perform_now }
+      end
+    end
+  end
+
+  context "when the job runs successfully" do
+    let(:daily_collectors) { { "test_daily_collector" => ExampleDailyCollector }.freeze }
+    before do
+      stub_const("StatsCollectorJob::DAILY_COLLECTORS", daily_collectors)
+      stub_const("StatsCollectorJob::WEEKLY_COLLECTORS", {})
+      stub_const("StatsCollectorJob::MONTHLY_COLLECTORS", {})
+    end
+    context "Datadog" do
+      let(:emitted_gauges) { [] }
+      let(:collector_gauges) do
+        emitted_gauges.select { |gauge| gauge[:metric_group] == StatsCollectorJob.name.underscore }
+          .map { |gauge| [gauge[:metric_name], gauge] }.to_h
+      end
+      let(:runtime_gauges) do
+        emitted_gauges.select { |gauge| gauge[:metric_name] == "runtime" }
+          .map { |gauge| [gauge[:metric_group], gauge] }.to_h
+      end
+
+      context "with a daily collector" do
+        it "records the job's runtime and collector stats" do
+          allow(DataDogService).to receive(:emit_gauge) { |args| emitted_gauges.push(args) }
+
+          described_class.perform_now
+
+          expect(runtime_gauges["stats_collector_job"][:metric_value]).not_to be_nil
+          expect(runtime_gauges["stats_collector_job.test_daily_collector"][:metric_value]).not_to be_nil
+
+          expect(collector_gauges["ex_metric_daily"][:metric_value]).to eq(9)
+          expect(collector_gauges["ex_metric_daily.stats1"][:metric_value]).to eq(2)
+          expect(collector_gauges["ex_metric_daily.statsA.metric2"][:metric_value]).to eq(4)
+        end
+      end
+
+      context "with failing and daily collectors" do
+        class FailingCollector
+          def collect_stats
+            fail "meant to fail"
+          end
+        end
+        let(:daily_collectors) do
+          {
+            "failing_collector" => FailingCollector,
+            "test_daily_collector" => ExampleDailyCollector
+          }.freeze
+        end
+
+        it "records the job's runtime and collector stats despite a failing collector" do
+          allow(DataDogService).to receive(:emit_gauge) { |args| emitted_gauges.push(args) }
+
+          slack_msg = ""
+          allow_any_instance_of(SlackService).to receive(:send_notification) { |_, first_arg| slack_msg = first_arg }
+
+          described_class.perform_now
+
+          # check failing collector
+          expect(runtime_gauges["stats_collector_job.failing_collector"][:metric_value]).not_to be_nil
+          expected_msg = "failing_collector failed after running for less than a minute. Fatal error: meant to fail"
+          expect(slack_msg).to eq(expected_msg)
+
+          # check subsequent collector
+          expect(runtime_gauges["stats_collector_job"][:metric_value]).not_to be_nil
+          expect(runtime_gauges["stats_collector_job.test_daily_collector"][:metric_value]).not_to be_nil
+
+          expect(collector_gauges["ex_metric_daily"][:metric_value]).to eq(9)
+          expect(collector_gauges["ex_metric_daily.stats1"][:metric_value]).to eq(2)
+          expect(collector_gauges["ex_metric_daily.statsA.metric2"][:metric_value]).to eq(4)
+        end
+      end
+    end
+  end
+end
