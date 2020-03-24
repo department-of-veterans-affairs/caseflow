adaf1cbc204742f691ac4119c36841f5325bbf8e
Kat Tipton <kat@navapbc.com>
2020-03-24 10:46:00 -0400

    Added migration for new special issues
        Burn Pit
        Military Sexual Trauma (MST)
        Blue Water
        US Court of Appeals for Veterans Claims (CAVC)
        No Special Issues

---
+++ db/migrate/20200324135048_add_new_issues_to_special_issue_list.rb
@@ -0,0 +1,22 @@
+class AddNewIssuesToSpecialIssueList < ActiveRecord::Migration[5.2]
+  def up
+    add_column :special_issue_lists, :burn_pit, :boolean, comment: "Burn Pit"
+    add_column :special_issue_lists, :military_sexual_trama, :boolean, comment: "Military Sexual Trauma (MST)"
+    add_column :special_issue_lists, :blue_water, :boolean, comment: "Blue Water"
+    add_column :special_issue_lists, :cavc, :boolean, comment: "US Court of Appeals for Veterans Claims (CAVC)"
+    add_column :special_issue_lists, :no_special_issues, :boolean, comment: "Affirmative no special issues, added belatedly"
+    change_column_default :special_issue_lists, :burn_pit, false
+    change_column_default :special_issue_lists, :military_sexual_trama, false
+    change_column_default :special_issue_lists, :blue_water, false
+    change_column_default :special_issue_lists, :cavc, false
+    change_column_default :special_issue_lists, :no_special_issues, false
+  end
+
+  def down
+    remove_column :special_issue_lists, :burn_pit
+    remove_column :special_issue_lists, :military_sexual_trama
+    remove_column :special_issue_lists, :blue_water
+    remove_column :special_issue_lists, :cavc
+    remove_column :special_issue_lists, :no_special_issues
+  end
+end
