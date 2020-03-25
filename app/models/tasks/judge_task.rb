2aeca291668f934e51b55b17bdfa6a0f6136da7d
hschallhorn <hschallhorn@tistatech.com>
2020-03-25 14:38:27 -0400

    Fix more tests

--- app/models/tasks/judge_task.rb
+++ app/models/tasks/judge_task.rb
@@ -18,7 +18,7 @@ class JudgeTask < Task
         Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h,
         additional_available_actions(user)
       ].flatten
-    elsif user.can_act_on_behalf_of_judges?
+    elsif user&.can_act_on_behalf_of_judges?
       [
         Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h,
         additional_available_actions(user)
