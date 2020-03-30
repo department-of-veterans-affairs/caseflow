63910235e0d41c80b483899b7c1ecf92a05dfcb2
hschallhorn <hschallhorn@tistatech.com>
2020-03-30 13:55:59 -0400

    Remove tasks from org tasks controller as it is handled in queue config

--- app/controllers/organizations/tasks_controller.rb
+++ app/controllers/organizations/tasks_controller.rb
@@ -7,35 +7,15 @@ class Organizations::TasksController < OrganizationsController
   def index
     render json: {
       organization_name: organization.name,
-      tasks: json_tasks(tasks),
       id: organization.id,
       is_vso: organization.is_a?(::Representative),
-      queue_config: queue_config
+      queue_config: QueueConfig.new(assignee: organization).to_hash
     }
   end
 
   private
 
-  def tasks
-    return [] if queue_config[:use_task_pages_api]
-
-    Rails.logger.debug("starting GenericQueue tasks")
-    GenericQueue.new(user: organization, limit: 1000).tasks
-  end
-
-  def queue_config
-    QueueConfig.new(assignee: organization).to_hash
-  end
-
   def organization_url
     params[:organization_url]
-  end
-
-  def json_tasks(tasks)
-    AmaAndLegacyTaskSerializer.create_and_preload_legacy_appeals(
-      tasks: tasks,
-      params: { user: current_user },
-      ama_serializer: organization.ama_task_serializer
-    ).call
   end
 end
