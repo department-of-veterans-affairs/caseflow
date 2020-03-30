d5d9c093d99669f3865e80848c9f41e5c599bbc2
yoomlam <yoom@navapbc.com>
2020-03-30 16:05:56 -0500

    clarify code with better variable names, comments

--- app/services/collectors/request_issues_stats_collector.rb
+++ app/services/collectors/request_issues_stats_collector.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Collect stats for unidentified RequestIssues that have contentions,
+# grouped by closed_status, benefit_type, decision_review_type, etc.
+# The stats are typically used for monthly reporting.
+# This collector is used by StatsCollectorJob.
 class Collectors::RequestIssuesStatsCollector
   METRIC_NAME_PREFIX = "request_issues.unidentified_with_contention"
 
@@ -11,17 +15,17 @@ class Collectors::RequestIssuesStatsCollector
     {}.tap do |stats|
       stats[METRIC_NAME_PREFIX] = req_issues.count
 
-      req_issues.group(:closed_status).count.each do |ben_type, count|
-        stats["#{METRIC_NAME_PREFIX}.st.#{ben_type || 'nil'}"] = count
+      req_issues.group(:closed_status).count.each do |status, count|
+        stats["#{METRIC_NAME_PREFIX}.closed_status.#{status || 'nil'}"] = count
       end
 
       req_issues.group(:benefit_type).count.each do |ben_type, count|
-        stats["#{METRIC_NAME_PREFIX}.ben.#{ben_type}"] = count
+        stats["#{METRIC_NAME_PREFIX}.benefit.#{ben_type}"] = count
       end
 
       dr_counts_by_type = req_issues.group(:decision_review_type).count
       dr_counts_by_type.each do |dr_type, count|
-        stats["#{METRIC_NAME_PREFIX}.dr.#{dr_type}"] = count
+        stats["#{METRIC_NAME_PREFIX}.decision_review.#{dr_type}"] = count
       end
 
       # Could use `req_issues.group(:veteran_participant_id).count.count` but there's a count discrepancy
