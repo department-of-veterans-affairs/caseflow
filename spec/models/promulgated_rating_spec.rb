af5524d1fcc565b33c4b81f5e6eefa9b38b6fea9
Sally Maki <sallymaki@gmail.com>
2020-04-01 09:07:52 -0700

    Move some tests to subclass spec

--- spec/models/promulgated_rating_spec.rb
+++ spec/models/promulgated_rating_spec.rb
@@ -1,6 +1,62 @@
 # frozen_string_literal: true
 
 describe PromulgatedRating do
+  context ".fetch_timely" do
+    let(:receipt_date) { Time.zone.today }
+
+    subject { PromulgatedRating.fetch_timely(participant_id: "DRAYMOND", from_date: receipt_date) }
+
+    let!(:rating) do
+      Generators::Rating.build(
+        participant_id: "DRAYMOND",
+        promulgation_date: receipt_date - 371.days
+      )
+    end
+
+    let!(:untimely_rating) do
+      Generators::Rating.build(
+        participant_id: "DRAYMOND",
+        promulgation_date: receipt_date - 373.days
+      )
+    end
+
+    it "returns rating objects for timely ratings" do
+      expect(subject.count).to eq(1)
+    end
+
+    context "when multiple timely ratings exist" do
+      let!(:another_rating) do
+        Generators::Rating.build(
+          participant_id: "DRAYMOND",
+          promulgation_date: receipt_date - 370.days
+        )
+      end
+
+      it "returns rating objects sorted desc by promulgation_date for all timely ratings" do
+        expect(subject.count).to eq(2)
+
+        expect(subject.first).to have_attributes(
+          participant_id: "DRAYMOND",
+          promulgation_date: receipt_date - 370.days
+        )
+
+        expect(subject.last).to have_attributes(
+          participant_id: "DRAYMOND",
+          promulgation_date: receipt_date - 371.days
+        )
+      end
+    end
+
+    context "when BGS returns an error" do
+      it "throws NilRatingProfileListError" do
+        allow_any_instance_of(Fakes::BGSService).to receive(:fetch_ratings_in_range).and_return(error: "Oops")
+        expect do
+          PromulgatedRating.fetch_timely(participant_id: "DRAYMOND", from_date: receipt_date)
+        end.to raise_error(Rating::NilRatingProfileListError)
+      end
+    end
+  end
+
   context ".from_bgs_hash" do
     subject { PromulgatedRating.from_bgs_hash(bgs_record) }
 
