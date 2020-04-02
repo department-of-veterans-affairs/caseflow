c54ffbac7101eb3e61395bc7f2b1819c91dc0a16
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-04-01 22:47:31 -0400

    remove unnecessary concern

--- app/models/hearings/forms/base_hearing_update_form.rb
+++ app/models/hearings/forms/base_hearing_update_form.rb
@@ -170,4 +170,52 @@ class BaseHearingUpdateForm
   def veteran_full_name
     @veteran_full_name ||= hearing.appeal&.veteran&.name&.to_s || "the veteran"
   end
+
+  def only_emails_updated?
+    email_changed = virtual_hearing_attributes&.key?(:veteran_email) ||
+                    virtual_hearing_attributes&.key?(:representative_email) ||
+                    judge_id.present?
+
+    email_changed && !virtual_hearing_cancelled? && !virtual_hearing_created?
+  end
+
+  def email_change_type
+    if virtual_hearing_attributes&.key?(:veteran_email) && virtual_hearing_attributes&.key?(:representative_email)
+      "CHANGED_VETERAN_AND_POA_EMAIL"
+    elsif virtual_hearing_attributes&.key?(:veteran_email)
+      "CHANGED_VETERAN_EMAIL"
+    elsif virtual_hearing_attributes&.key?(:representative_email)
+      "CHANGED_POA_EMAIL"
+    elsif judge_id.present?
+      "CHANGED_VLJ_EMAIL"
+    end
+  end
+
+  def change_type
+    if virtual_hearing_created?
+      "CHANGED_TO_VIRTUAL"
+    elsif virtual_hearing_cancelled?
+      "CHANGED_FROM_VIRTUAL"
+    elsif only_time_updated?
+      "CHANGED_HEARING_TIME"
+    elsif only_emails_updated?
+      email_change_type
+    end
+  end
+
+  def add_virtual_hearing_alert
+    nested_alert = VirtualHearingUserAlertBuilder.new(
+      change_type: change_type,
+      alert_type: :info,
+      veteran_full_name: veteran_full_name
+    ).call.to_hash
+
+    nested_alert[:next] = VirtualHearingUserAlertBuilder.new(
+      change_type: change_type,
+      alert_type: :success,
+      veteran_full_name: veteran_full_name
+    ).call.to_hash
+
+    @virtual_hearing_alert = nested_alert
+  end
 end
