7814abbe7a1e46fd99de81370e8f8abd199e9a44
Kat Tipton <kat@navapbc.com>
2020-04-02 14:43:02 -0400

    Pass FeatureToggle to FE, new Special Issues, new Enabler function

--- client/app/constants/SpecialIssues.js
+++ client/app/constants/SpecialIssues.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
-const SPECIAL_ISSUES = [
+// Fields:
+// node:                     first choice display name; has html
+// display:                  fallback plaintext display name
+// queueDisplay:             fallback plaintext display name, only in Queue
+// specialIssue:             React's ID
+// stationOfJurisdiction:    Can be null, Regional, or this hash. ?rerouting magic at dispatch?
+// {
+//   key:                    ?Does some reouting magic at Dispatch
+//   location:               ?Does some reouting magic at Dispatch
+// },
+// snakeCase:                Rails' ID
+// unhandled: {
+//   emailAddress:           ?Does some reouting magic at Dispatch
+//   regionalOffice:         ?Does some reouting magic at Dispatch
+// },
+// nonCompensation:          ?Not used
+// queueSection:             for Queue display, which section it goes in
+// queueSectionOrder:        for Queue display, the order to display
+
+export const SPECIAL_ISSUES = [
   {
     display: 'Contaminated Water at Camp LeJeune',
     queueDisplay: 'Contaminated water at Camp LeJeune',
@@ -12,7 +31,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'contaminated_water_at_camp_lejeune',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 4
+    queueSectionOrder: 3
   },
   {
     display: 'DIC - death, or accrued benefits - United States',
@@ -141,7 +160,7 @@ const SPECIAL_ISSUES = [
     unhandled: null,
     nonCompensation: true,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 6
+    queueSectionOrder: 4
   },
   {
     display: 'Mustard Gas',
@@ -153,7 +172,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'mustard_gas',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 3
+    queueSectionOrder: 6
   },
   {
     display: 'National Cemetery Administration',
@@ -176,7 +195,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'nonrating_issue',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 1
+    queueSectionOrder: 7
   },
   {
     display: 'Pension - United States',
@@ -209,7 +228,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'radiation',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 7
+    queueSectionOrder: 8
   },
   {
     display: 'Rice Compliance',
@@ -219,7 +238,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'rice_compliance',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 5
+    queueSectionOrder: 9
   },
   {
     display: 'Spina Bifida',
@@ -229,7 +248,7 @@ const SPECIAL_ISSUES = [
     snakeCase: 'spina_bifida',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 2
+    queueSectionOrder: 10
   },
   {
     display: 'U.S. Territory claim - American Samoa, Guam, Northern ' +
@@ -309,8 +328,62 @@ const SPECIAL_ISSUES = [
     },
     nonCompensation: true,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 8
+    queueSectionOrder: 12
   }
 ];
 
+// Special Issues added in 2020-Spring. Currently seperate from the above in order to be
+// locked to the FeatureToggle :special_issues_revamp down below. After the feature toggle is fully enabled
+// & removed these can be cleanly merged into the above array. Reference PR #13781
+export const NEW_SPECIAL_ISSUES = [
+  {
+    display: 'Burn Pit',
+    queueDisplay: 'Burn Pit',
+    specialIssue: 'burnPit',
+    snakeCase: 'Burn Pit',
+    unhandled: null,
+    queueSection: 'issuesOnAppeal',
+    queueSectionOrder: 2
+  },
+  {
+    display: 'Military Sexual Trauma (MST)',
+    queueDisplay: 'Military Sexual Trauma (MST)',
+    specialIssue: 'militarySexualTrauma',
+    snakeCase: 'military_sexual_trauma',
+    unhandled: null,
+    queueSection: 'issuesOnAppeal',
+    queueSectionOrder: 5
+  },
+  {
+    display: 'Blue Water',
+    queueDisplay: 'Blue Water',
+    specialIssue: 'blueWater',
+    snakeCase: 'blue_water',
+    unhandled: null,
+    queueSection: 'issuesOnAppeal',
+    queueSectionOrder: 1
+  },
+  {
+    display: 'US Court of Appeals for Veterans Claims (CAVC)',
+    queueDisplay: 'US Court of Appeals for Veterans Claims (CAVC)',
+    specialIssue: 'usCourtOfAppealsForVeteransClaims',
+    snakeCase: 'us_court_of_appeals_for_veterans_claims',
+    unhandled: null,
+    queueSection: 'issuesOnAppeal',
+    queueSectionOrder: 11
+  },
+  {
+    node: <b>No Special Issues</b>,
+    display: 'No Special Issues',
+    queueDisplay: 'No Special Issues',
+    specialIssue: 'noSpecialIssues',
+    snakeCase: 'no_special_issues',
+    unhandled: null,
+    queueSection: 'noSpecialIssues',
+  }
+];
+
+// NEW_SPECIAL_ISSUES added in 2020-Spring. Currently listed seperate from the main list in order to be
+// locked to the FeatureToggle :special_issues_revamp in SpecialIssueEnabler. As a part of removing the feature toggle, merge the
+// arrays and clean up this export.
 export default SPECIAL_ISSUES;
