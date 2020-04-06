e8b0aadbbd0608ff2f268f257c69d23932c96f82
Ferris Tseng <ferris@navapbc.com>
2020-04-06 16:21:51 -0400

    reorganize hearing details inputs; use flexBox

--- client/app/hearings/components/details/DetailsInputs.jsx
+++ client/app/hearings/components/details/DetailsInputs.jsx
@@ -11,12 +11,15 @@ import {
   HearingRoomDropdown
 } from '../../../components/DataDropdowns/index';
 import { VIRTUAL_HEARING_HOST, virtualHearingRoleForUser } from '../../utils';
-import { rowThirds } from './style';
+import { rowThirds, flexParent, columnThird, columnDoubleSpacer } from './style';
 import COPY from '../../../../COPY';
 import Checkbox from '../../../components/Checkbox';
 import HearingTypeDropdown from './HearingTypeDropdown';
 import TextField from '../../../components/TextField';
 import TextareaField from '../../../components/TextareaField';
+import TranscriptionDetailsInputs from './TranscriptionDetailsInputs';
+import TranscriptionProblemInputs from './TranscriptionProblemInputs';
+import TranscriptionRequestInputs from './TranscriptionRequestInputs';
 import VirtualHearingLink from '../VirtualHearingLink';
 
 // Displays the emails associated with the virtual hearing.
@@ -49,6 +52,7 @@ const EmailSection = (
         readOnly={readOnlyEmails}
         onChange={(representativeEmail) => updateVirtualHearing({ representativeEmail })}
       />
+      <div />
     </div>
   );
 };
@@ -110,93 +114,153 @@ VirtualLinkSection.propTypes = {
   isVirtual: PropTypes.bool
 };
 
+const TranscriptionSection = (
+  { hearing, updateHearing, transcription, updateTranscription, readOnly }
+) => (
+  <React.Fragment>
+    <div className="cf-help-divider" />
+    <div>
+      <h2>Transcription Details</h2>
+      <TranscriptionDetailsInputs
+        transcription={transcription}
+        update={updateTranscription}
+        readOnly={readOnly}
+      />
+      <div className="cf-help-divider" />
+
+      <h3>Transcription Problem</h3>
+      <TranscriptionProblemInputs
+        transcription={transcription}
+        update={updateTranscription}
+        readOnly={readOnly}
+      />
+      <div className="cf-help-divider" />
+
+      <h3>Transcription Request</h3>
+      <TranscriptionRequestInputs
+        hearing={hearing}
+        update={updateHearing}
+        readOnly={readOnly}
+      />
+      <div className="cf-help-divider" />
+    </div>
+  </React.Fragment> 
+);
+
+TranscriptionSection.propTypes = {
+  hearing: PropTypes.object,
+  readOnly: PropTypes.bool,
+  updateHearing: PropTypes.func,
+  updateTranscription: PropTypes.func
+};
+
 const DetailsInputs = (props) => {
   const {
     hearing,
-    update,
-    readOnly,
     isLegacy,
+    isVirtual,
     openVirtualHearingModal,
+    readOnly,
+    requestType,
+    transcription,
+    update,
+    updateHearing,
+    updateTranscription,
     updateVirtualHearing,
     virtualHearing,
-    enableVirtualHearings,
-    requestType,
-    isVirtual,
     wasVirtual
   } = props;
+  const { userCanScheduleVirtualHearings } = useContext(HearingsUserContext);
+  const enableVirtualHearings = userCanScheduleVirtualHearings && requestType !== 'Central';
 
   return (
-    <>
+    <React.Fragment>
+      <div {...rowThirds}>
+        <JudgeDropdown
+          name="judgeDropdown"
+          value={hearing?.judgeId}
+          readOnly={readOnly}
+          onChange={(judgeId) => update({ judgeId })}
+        />
+        <HearingCoordinatorDropdown
+          name="hearingCoordinatorDropdown"
+          value={hearing?.bvaPoc}
+          readOnly={readOnly}
+          onChange={(bvaPoc) => update({ bvaPoc })}
+        />
+        <HearingRoomDropdown
+          name="hearingRoomDropdown"
+          value={hearing?.room}
+          readOnly={readOnly}
+          onChange={(room) => update({ room })}
+        />
+      </div>
       {enableVirtualHearings &&
-        <div {...rowThirds}>
-          <HearingTypeDropdown
-            virtualHearing={virtualHearing}
-            requestType={requestType}
-            updateVirtualHearing={updateVirtualHearing}
-            openModal={openVirtualHearingModal}
-            readOnly={hearing.scheduledForIsPast || (isVirtual && virtualHearing && !virtualHearing.jobCompleted)}
-          />
+        <React.Fragment>
+          <div className="cf-help-divider" />
+          <div {...flexParent}>
+            <HearingTypeDropdown
+              virtualHearing={virtualHearing}
+              requestType={requestType}
+              updateVirtualHearing={updateVirtualHearing}
+              openModal={openVirtualHearingModal}
+              readOnly={hearing?.scheduledForIsPast || (isVirtual && virtualHearing && !virtualHearing.jobCompleted)}
+              styling={columnThird}
+            />
+            <div {...columnDoubleSpacer} />
+          </div>
+        </React.Fragment>
+      }
+      {(isVirtual || wasVirtual) &&
+        <React.Fragment>
+          <div className="cf-help-divider" />
+          <h3>{wasVirtual && "Previous "}Virtual Hearing Details</h3>
           <VirtualLinkSection
             hearing={hearing}
             virtualHearing={virtualHearing}
             isVirtual={isVirtual}
           />
+          <EmailSection
+            hearing={hearing}
+            virtualHearing={virtualHearing}
+            isVirtual={isVirtual}
+            wasVirtual={wasVirtual}
+            readOnly={readOnly}
+            updateVirtualHearing={updateVirtualHearing}
+          />
+        </React.Fragment>
+      }
+      <div className="cf-help-divider" />
+      {!isLegacy &&
+        <div>
+          <strong>Waive 90 Day Evidence Hold</strong>
+          <Checkbox
+            label="Yes, Waive 90 Day Evidence Hold"
+            name="evidenceWindowWaived"
+            disabled={readOnly}
+            value={hearing?.evidenceWindowWaived || false}
+            onChange={(evidenceWindowWaived) => update({ evidenceWindowWaived })}
+          />
         </div>
       }
-      <EmailSection
-        hearing={hearing}
-        virtualHearing={virtualHearing}
-        isVirtual={isVirtual}
-        wasVirtual={wasVirtual}
-        readOnly={readOnly}
-        updateVirtualHearing={updateVirtualHearing}
-      />
-      <div {...rowThirds}>
-        <JudgeDropdown
-          name="judgeDropdown"
-          value={hearing.judgeId}
-          readOnly={readOnly}
-          onChange={(judgeId) => update({ judgeId })}
-        />
-      </div>
-      <div {...rowThirds}>
-        <HearingRoomDropdown
-          name="hearingRoomDropdown"
-          value={hearing.room}
-          readOnly={readOnly}
-          onChange={(room) => update({ room })}
-        />
-        <HearingCoordinatorDropdown
-          name="hearingCoordinatorDropdown"
-          value={hearing.bvaPoc}
-          readOnly={readOnly}
-          onChange={(bvaPoc) => update({ bvaPoc })}
-        />
-        {!isLegacy &&
-          <div>
-            <strong>Waive 90 Day Evidence Hold</strong>
-            <Checkbox
-              label="Yes, Waive 90 Day Evidence Hold"
-              name="evidenceWindowWaived"
-              disabled={readOnly}
-              value={hearing.evidenceWindowWaived || false}
-              onChange={(evidenceWindowWaived) => update({ evidenceWindowWaived })}
-            />
-          </div>
-        }
-      </div>
+      <div className="cf-help-divider" />
       <TextareaField
         name="Notes"
         strongLabel
-        styling={css({
-          display: 'block',
-          maxWidth: '100%'
-        })}
+        styling={css({ display: 'block', maxWidth: '100%' })}
         disabled={readOnly}
-        value={hearing.notes || ''}
+        value={hearing?.notes || ''}
         onChange={(notes) => update({ notes })}
       />
-    </>
+      {!isLegacy &&
+        <TranscriptionSection
+          hearing={hearing}
+          readOnly={readOnly}
+          updateHearing={updateHearing}
+          updateTranscription={updateTranscription}
+        />
+      }
+    </React.Fragment>
   );
 };
 
@@ -223,7 +287,10 @@ DetailsInputs.propTypes = {
   }),
   enableVirtualHearings: PropTypes.bool,
   isVirtual: PropTypes.bool,
-  wasVirtual: PropTypes.bool
+  wasVirtual: PropTypes.bool,
+  transcription: PropTypes.object,
+  updateHearing: PropTypes.func,
+  updateTranscription: PropTypes.func
 };
 
 export default DetailsInputs;
