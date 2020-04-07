968f9588d5c1a882246e97524c04ebca9c2206a2
Sally Maki <sallymaki@gmail.com>
2020-04-07 11:43:32 -0700

    Code climate

--- app/models/concerns/latest_rating_disability_evaluation.rb
+++ app/models/concerns/latest_rating_disability_evaluation.rb
@@ -6,7 +6,7 @@ module LatestRatingDisabilityEvaluation
   class_methods do
     def latest_disability_evaluation(disability)
       latest = Array.wrap(disability[:disability_evaluations] || disability[:disability_evaluation])
-        .max_by{ |dis_eval| dis_eval[:conv_begin_dt] || dis_eval[:begin_dt] || dis_eval[:dis_dt] }
+        .max_by { |dis_eval| dis_eval[:conv_begin_dt] || dis_eval[:begin_dt] || dis_eval[:dis_dt] }
 
       latest || {}
     end
