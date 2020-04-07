775a29059c7002c8b7c491d9e5421d96581075ac
Kat Tipton <kat@navapbc.com>
2020-04-07 11:17:29 -0400

    Teach incorrect BVAIntake/CaseReview correction

--- app/models/organizations/case_review.rb
+++ app/models/organizations/case_review.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 
-class BvaIntake < Organization
+class CaseReview < Organization
   def self.singleton
-    # after the production database is correct, replace the tmp function with BvaIntake.first
-    find_and_update_incorrect_bva_intake || BvaIntake.create(name: "Bva Intake", url: "bva-intake")
+    # after the production database is correct, replace the tmp function with CaseReview.first
+    find_and_update_incorrect_bva_intake || CaseReview.create(name: "Case Review", url: "case-review")
   end
 
   private
@@ -12,12 +13,12 @@ class BvaIntake < Organization
   def self.find_and_update_incorrect_bva_intake
     # check if the mismatch exists; if not default back to BvaIntake.first
     case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
-    return BvaIntake.first unless case_review_misnamed
+    return CaseReview.first unless case_review_misnamed
 
     # we're the first invocation - fix the mismatch
     case_review_misnamed.update(type: "CaseReview")
 
-    # return BvaIntake.first; if it doesn't exist it will fall through up in singleton to the create
-    BvaIntake.first
+    # return CaseReview.first; if it doesn't exist it will fall through up in singleton to the create - though it _should_ since we just created it via that update!
+    CaseReview.first
   end
 end
