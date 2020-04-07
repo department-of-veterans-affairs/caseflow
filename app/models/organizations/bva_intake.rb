8eb558c9fa174c3d259abdadaa35a1b3c96326d5
Kat Tipton <kat@navapbc.com>
2020-04-07 11:41:46 -0400

    CodeClimate improvements

--- app/models/organizations/bva_intake.rb
+++ app/models/organizations/bva_intake.rb
@@ -1,23 +1,26 @@
+# frozen_string_literal: true
 
 class BvaIntake < Organization
-  def self.singleton
-    # after the production database is correct, replace the tmp function with BvaIntake.first
-    find_and_update_incorrect_bva_intake || BvaIntake.create(name: "Bva Intake", url: "bva-intake")
-  end
+  class << self
+    def singleton
+      # after the production database is correct, replace the tmp function with BvaIntake.first
+      find_and_update_incorrect_bva_intake || BvaIntake.create(name: "Bva Intake", url: "bva-intake")
+    end
 
-  private
+    private
 
-  # temporary function to split the original mistake where the org Case Review was
-  # referred to as BvaIntake. Can be removed after the production database is up to date.
-  def self.find_and_update_incorrect_bva_intake
-    # check if the mismatch exists; if not default back to BvaIntake.first
-    case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
-    return BvaIntake.first unless case_review_misnamed
+    # temporary function to split the original mistake where the org Case Review was
+    # referred to as BvaIntake. Can be removed after the production database is up to date.
+    def find_and_update_incorrect_bva_intake
+      # check if the mismatch exists; if not default back to BvaIntake.first
+      case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
+      return BvaIntake.first unless case_review_misnamed
 
-    # we're the first invocation - fix the mismatch
-    case_review_misnamed.update(type: "CaseReview")
+      # we're the first invocation - fix the mismatch
+      case_review_misnamed.update(type: "CaseReview")
 
-    # return BvaIntake.first; if it doesn't exist it will fall through up in singleton to the create
-    BvaIntake.first
+      # return BvaIntake.first; if it doesn't exist it will fall through up in singleton to the create
+      BvaIntake.first
+    end
   end
 end
--- app/models/organizations/case_review.rb
+++ app/models/organizations/case_review.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
 class CaseReview < Organization
-  def self.singleton
-    # after the production database is correct, replace the tmp function with CaseReview.first
-    find_and_update_incorrect_bva_intake || CaseReview.create(name: "Case Review", url: "case-review")
-  end
+  class << self
+    def singleton
+      # after the production database is correct, replace the tmp function with CaseReview.first
+      find_and_update_incorrect_bva_intake || CaseReview.create(name: "Case Review", url: "case-review")
+    end
 
-  private
+    private
 
-  # temporary function to split the original mistake where the org Case Review was
-  # referred to as BvaIntake. Can be removed after the production database is up to date.
-  def self.find_and_update_incorrect_bva_intake
-    # check if the mismatch exists; if not default back to BvaIntake.first
-    case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
-    return CaseReview.first unless case_review_misnamed
+    # temporary function to split the original mistake where the org Case Review was
+    # referred to as BvaIntake. Can be removed after the production database is up to date.
+    def find_and_update_incorrect_bva_intake
+      # check if the mismatch exists; if not default back to BvaIntake.first
+      case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
+      return CaseReview.first unless case_review_misnamed
 
-    # we're the first invocation - fix the mismatch
-    case_review_misnamed.update(type: "CaseReview")
+      # we're the first invocation - fix the mismatch
+      case_review_misnamed.update(type: "CaseReview")
 
-    # return CaseReview.first; if it doesn't exist it will fall through up in singleton to the create - though it _should_ since we just created it via that update!
-    CaseReview.first
+      # return CaseReview.first; if it doesn't exist it will fall through up
+      # in singleton to the create - though it _should_ since we just
+      # created it via that update!
+      CaseReview.first
+    end
   end
 end
