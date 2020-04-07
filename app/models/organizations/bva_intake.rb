a2cd353ae35013bc18ea5c36426a3e9573d3fac2
Kat Tipton <kat@navapbc.com>
2020-04-07 14:29:33 -0400

    Remove prod data migration code

--- app/models/organizations/bva_intake.rb
+++ app/models/organizations/bva_intake.rb
@@ -3,24 +3,7 @@
 class BvaIntake < Organization
   class << self
     def singleton
-      # after the production database is correct, replace the tmp function with BvaIntake.first
-      find_and_update_incorrect_bva_intake || BvaIntake.create(name: "BVA Intake", url: "bva-intake")
-    end
-
-    private
-
-    # temporary function to split the original mistake where the org Case Review was
-    # referred to as BvaIntake. Can be removed after the production database is up to date.
-    def find_and_update_incorrect_bva_intake
-      # check if the mismatch exists; if not default back to BvaIntake.first
-      case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
-      return BvaIntake.first unless case_review_misnamed
-
-      # we're the first invocation - fix the mismatch
-      case_review_misnamed.update(type: "CaseReview")
-
-      # return BvaIntake.first; if it doesn't exist it will fall through up in singleton to the create
-      BvaIntake.first
+      BvaIntake.first || BvaIntake.create(name: "BVA Intake", url: "bva-intake")
     end
   end
 end
--- app/models/organizations/case_review.rb
+++ app/models/organizations/case_review.rb
@@ -3,26 +3,7 @@
 class CaseReview < Organization
   class << self
     def singleton
-      # after the production database is correct, replace the tmp function with CaseReview.first
-      find_and_update_incorrect_bva_intake || CaseReview.create(name: "Case Review", url: "case-review")
-    end
-
-    private
-
-    # temporary function to split the original mistake where the org Case Review was
-    # referred to as BvaIntake. Can be removed after the production database is up to date.
-    def find_and_update_incorrect_bva_intake
-      # check if the mismatch exists; if not default back to BvaIntake.first
-      case_review_misnamed = Organization.find_by(name: "Case Review", type: "BvaIntake")
-      return CaseReview.first unless case_review_misnamed
-
-      # we're the first invocation - fix the mismatch
-      case_review_misnamed.update(type: "CaseReview")
-
-      # return CaseReview.first; if it doesn't exist it will fall through up
-      # in singleton to the create - though it _should_ since we just
-      # created it via that update!
-      CaseReview.first
+      CaseReview.first || CaseReview.create(name: "Case Review", url: "case-review")
     end
   end
 end
