a3e55e5a43ed9bfe4cb033bab3a36abdf4632ae0
Ferris Tseng <ferris@navapbc.com>
2020-04-16 17:24:36 -0400

    add some model tests

---
+++ spec/factories/sent_hearing_email_event.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :sent_hearing_email_event do
+    association :sent_by, factory: :user
+    hearing
+    
+    email_address { "test@caseflow.va.gov" }
+    email_type { "confirmation" }
+    recipient_role { "veteran" }
+  end
+end
