e6edc37548af7197f58c609d90b1095b14648f5e
Sjones352 <sjones@tistatech.com>
2020-04-20 22:57:09 -0400

    removed trailing line

--- app/models/tasks/judge_decision_review_task.rb
+++ app/models/tasks/judge_decision_review_task.rb
@@ -14,10 +14,12 @@ class JudgeDecisionReviewTask < JudgeTask
                            else
                              Constants.TASK_ACTIONS.JUDGE_LEGACY_CHECKOUT.to_h
                            end
-     
-    [(Constants.TASK_ACTIONS.LIT_SUPPORT_PULAC_CERULLO.to_h if ama? && appeal.vacate?),
-     judge_checkout_label,
-     Constants.TASK_ACTIONS.JUDGE_RETURN_TO_ATTORNEY.to_h].compact
+
+    [
+      (Constants.TASK_ACTIONS.LIT_SUPPORT_PULAC_CERULLO.to_h if ama? && appeal.vacate?),
+      judge_checkout_label,
+      Constants.TASK_ACTIONS.JUDGE_RETURN_TO_ATTORNEY.to_h
+    ].compact
   end
 
   def self.label
