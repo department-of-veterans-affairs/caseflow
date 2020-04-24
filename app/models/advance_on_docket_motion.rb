0ff9c988e5f1f4518eb6eccf4b4a46f34e284ffc
hschallhorn <hschallhorn@tistatech.com>
2020-04-24 11:41:57 -0400

    Add scopes

--- app/models/advance_on_docket_motion.rb
+++ app/models/advance_on_docket_motion.rb
@@ -4,10 +4,6 @@ class AdvanceOnDocketMotion < CaseflowRecord
   belongs_to :person
   belongs_to :user
 
-  enum status: {
-    granted: "granted",
-    denied: "denied"
-  }
   enum reason: {
     financial_distress: "financial_distress",
     age: "age",
@@ -15,18 +11,25 @@ class AdvanceOnDocketMotion < CaseflowRecord
     other: "other"
   }
 
+  scope :granted, -> { where(granted: true) }
+  scope :for_person, ->(person_id) { where(person_id: person_id) }
+  scope :eligable_due_to_age, -> { age }
+  scope :eligable_due_to_date, lambda { |receipt_date|
+    where(created_at: receipt_date..DateTime::Infinity.new).where.not(id: age)
+  }
+
   class << self
     def granted_for_person?(person_id, appeal_receipt_date)
-      eligable_motions(person_id, appeal_receipt_date).where(granted: true).any?
+      eligable_due_to_date(appeal_receipt_date).or(eligable_due_to_age).for_person(person_id).granted.any?
     end
 
-    def eligable_motions(person_id, appeal_receipt_date)
-      where(created_at: appeal_receipt_date..DateTime::Infinity.new, person_id: person_id)
+    def eligable_motions_for_appeal(person_id, appeal_receipt_date)
+      eligable_due_to_date(appeal_receipt_date).for_person(person_id)
     end
 
     def create_or_update_by_appeal(appeal, attrs)
       person_id = appeal.claimant.person.id
-      motion = eligable_motions(person_id, appeal.receipt_date).first || create(person_id: person_id)
+      motion = eligable_due_to_date(appeal.receipt_date).for_person(person_id).first || create(person_id: person_id)
 
       motion.update(attrs)
     end
