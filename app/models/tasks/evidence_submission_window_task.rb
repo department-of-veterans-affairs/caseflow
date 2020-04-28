74cd9ae4e8a7d88538b98b7fcc676d47aabf94ca
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-04-28 16:44:15 -0400

    Changed timer end calculation for esw tasks

--- app/models/tasks/evidence_submission_window_task.rb
+++ app/models/tasks/evidence_submission_window_task.rb
@@ -14,10 +14,21 @@ class EvidenceSubmissionWindowTask < Task
     update!(status: :completed)
   end
 
+  # Determines when the ESW task should be expired
   def timer_ends_at
+    # Timer should be the date the hearing was scheduled if the hearing is present
     from_date = hearing.hearing_day&.scheduled_for if hearing.present?
-    from_date ||= appeal.receipt_date
 
+    # Timer should be at receipt date if the appeal is in the ESW docket
+    from_date ||= appeal.receipt_date if appeal.evidence_submission_docket?
+
+    # Timer should be the date the hearing was withdrawn for all other appeals
+    from_date ||= parent.children.find_by(
+      type: ScheduleHearingTask.name,
+      status: Constants.TASK_STATUSES.cancelled
+    ).closed_at
+
+    # Add 90 days to the timer based on the date above
     from_date + 90.days
   end
 
