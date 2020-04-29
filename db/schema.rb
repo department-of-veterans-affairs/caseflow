4192ec2bad8386aa7fab833579a1cc8667b966df
yoomlam <yoom@navapbc.com>
2020-04-29 18:23:47 -0500

    add standard timestamp fields

--- db/schema.rb
+++ db/schema.rb
@@ -1422,7 +1422,9 @@ ActiveRecord::Schema.define(version: 2020_04_29_184512) do
   create_table "work_modes", comment: "Captures user's current work mode for appeals being worked", force: :cascade do |t|
     t.integer "appeal_id", null: false, comment: "Appeal ID -- use as FK to AMA appeals and legacy appeals"
     t.string "appeal_type", null: false, comment: "Whether appeal_id is for AMA or legacy appeals"
+    t.datetime "created_at", null: false, comment: "Standard created_at/updated_at timestamps"
     t.boolean "overtime", default: false, comment: "Whether the appeal is currently marked as being worked as overtime"
+    t.datetime "updated_at", null: false, comment: "Standard created_at/updated_at timestamps"
     t.index ["appeal_type", "appeal_id"], name: "index_work_modes_on_appeal_type_and_appeal_id", unique: true
   end
 
