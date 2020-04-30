e349eac090ac4f2410a042b20999866e5a712588
Peter Karman <peter@truss.works>
2020-04-30 08:25:43 -0500

    more tests

--- spec/services/etl/hearing_syncer_spec.rb
+++ spec/services/etl/hearing_syncer_spec.rb
@@ -2,12 +2,16 @@
 
 describe ETL::HearingSyncer, :etl, :all_dbs do
   let(:etl_build) { ETL::Build.create }
+  let(:regional_office) { "RO89" }
   let!(:virtual_hearing) { create(:virtual_hearing, hearing: video_hearing) }
   let(:video_hearing) do
-    build(:hearing, :with_tasks, hearing_day: hearing_day)
+    build(:hearing, :with_tasks, hearing_day: hearing_day, regional_office: regional_office)
   end
   let(:hearing_day) do
-    create(:hearing_day, regional_office: "RO89", request_type: HearingDay::REQUEST_TYPES[:video])
+    create(:hearing_day, regional_office: regional_office, request_type: HearingDay::REQUEST_TYPES[:video])
+  end
+  let!(:regional_hearing) do
+    create(:hearing, :with_tasks, hearing_day: hearing_day)
   end
 
   describe "#call" do
@@ -17,9 +21,14 @@ describe ETL::HearingSyncer, :etl, :all_dbs do
       it "syncs" do
         subject
 
-        expect(Hearing.count).to eq(1)
-        expect(ETL::Hearing.count).to eq(1)
-        expect(ETL::Hearing.first.hearing_request_type).to eq "Virtual"
+        expect(Hearing.count).to eq(2)
+        expect(ETL::Hearing.count).to eq(2)
+        etl_virtual_hearing = ETL::Hearing.find_by(hearing_id: virtual_hearing.hearing_id)
+        etl_regional_hearing = ETL::Hearing.find_by(hearing_id: regional_hearing.id)
+        expect(etl_virtual_hearing.hearing_request_type).to eq "Virtual"
+        expect(etl_virtual_hearing.hearing_location_zip_code).to eq "20001"
+        expect(etl_regional_hearing.hearing_request_type).to eq "Video"
+        expect(etl_regional_hearing.hearing_location_zip_code).to be_nil
       end
     end
 
@@ -34,11 +43,11 @@ describe ETL::HearingSyncer, :etl, :all_dbs do
       it "skips orphans" do
         subject
 
-        expect(Hearing.count).to eq(2)
-        expect(ETL::Hearing.count).to eq(1)
+        expect(Hearing.count).to eq(3)
+        expect(ETL::Hearing.count).to eq(2)
         expect(etl_build_table).to be_complete
         expect(etl_build_table.rows_rejected).to eq(1)
-        expect(etl_build_table.rows_inserted).to eq(1)
+        expect(etl_build_table.rows_inserted).to eq(2)
         expect(etl_build_table.rows_updated).to eq(0)
       end
     end
