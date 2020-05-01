d812d767d3d21f195153133826dd3cfcede56491
Peter Karman <peter@truss.works>
2020-05-01 13:12:21 -0500

    jobs

---
+++ db/seeds/jobs.rb
@@ -0,0 +1,15 @@
+module Seeds
+  class Jobs
+    def seed!
+      perform_seeding_jobs
+    end
+
+    private
+
+    def perform_seeding_jobs
+      # Active Jobs which populate tables based on seed data
+      UpdateCachedAppealsAttributesJob.perform_now
+      NightlySyncsJob.perform_now
+    end
+  end
+end
