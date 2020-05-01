445625cc0195d0c1d238c23eac1414077e8bb794
Peter Karman <peter@truss.works>
2020-05-01 13:05:41 -0500

    dispatch

---
+++ db/seeds/dispatch.rb
@@ -0,0 +1,26 @@
+module Seeds
+  class Dispatch
+    def seed!
+      setup_dispatch
+    end
+
+    private
+
+    def setup_dispatch
+      CreateEstablishClaimTasksJob.perform_now
+      Timecop.freeze(Date.yesterday) do
+        # Tasks prepared on today's date will not be picked up
+        ::Dispatch::Task.all.each(&:prepare!)
+        # Appeal decisions (decision dates) for partial grants have to be within 3 days
+        CSV.foreach(Rails.root.join("local/vacols", "cases.csv"), headers: true) do |row|
+          row_hash = row.to_h
+          if %w[amc_full_grants remands_ready_for_claims_establishment].include?(row_hash["vbms_key"])
+            VACOLS::Case.where(bfkey: row_hash["vacols_id"]).first.update(bfddec: Time.zone.today)
+          end
+        end
+      end
+    rescue AASM::InvalidTransition
+      Rails.logger.info("Taks prepare job skipped - tasks were already prepared...")
+    end
+  end
+end
---
+++ spec/seeds/dispatch_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+describe Seeds::Dispatch do
+  describe "#seed!" do
+    subject { described_class.new.seed! }
+
+    before do
+      # TODO these are expensive to run, esp Facols.
+      #Seeds::Users.new.seed!
+      puts "start facols seed #{Time.zone.now}"
+      Seeds::Facols.new.seed!
+      puts "end facols seed #{Time.zone.now}"
+    end
+
+    it "creates all kinds of dispatch seeds" do
+      expect { subject }.to_not raise_error
+    end
+  end
+end     
