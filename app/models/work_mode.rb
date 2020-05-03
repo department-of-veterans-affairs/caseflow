e59d90a7e4093ee6ac554d942068b905792b82bc
yoomlam <yoom@navapbc.com>
2020-05-03 14:22:10 -0500

    add model validation; check if update succeeds

--- app/models/work_mode.rb
+++ app/models/work_mode.rb
@@ -6,9 +6,15 @@
 class WorkMode < ApplicationRecord
   belongs_to :appeal, polymorphic: true
 
+  validates :appeal_id, presence: true
+  validates :appeal_type, presence: true
+
+  class WorkModeCouldNotUpdateError < StandardError; end
+
   def self.create_or_update_by_appeal(appeal, attrs)
     work_mode = appeal.work_mode || WorkMode.new(appeal: appeal).tap { appeal.reload }
-    work_mode.update(attrs)
-    work_mode
+    return work_mode if work_mode.update(attrs)
+
+    fail WorkModeCouldNotUpdateError
   end
 end
