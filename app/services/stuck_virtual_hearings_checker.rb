7cc875267da499958acf3af21f8410f013fb94c1
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-05-04 12:31:05 -0400

    crate new class,
    Co-authored-by: Stephen <stephenahalliburton@gmail.com>

---
+++ app/services/stuck_virtual_hearings_checker.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Data integrity checker for notifying when a virtual hearing is 'stuck'
+# i.e the pexip conference creation is pending and/or all emails haven't successfuly sent
+class StuckVirtualHearingsChecker < DataIntegrityChecker
+  def call
+    build_report(stuck_virtual_hearings)
+  end
+
+  def stuck_virtual_hearings
+    VirtualHearingRepository.hearings_with_pending_conference_or_emails.select do |virtual_hearing|
+      virtual_hearing.updated_at < Time.now - 2.hours
+    end
+  end
+
+  # sending to appeals-tango for now, might later change to #appeals-hearings
+  def slack_channel
+    "#appeals-tango"
+  end
+
+  def build_report(stuck_virtual_hearings)
+    return if stuck_virtual_hearings.empty?
+
+    stuck_count = stuck_virtual_hearings.count
+
+    add_to_report "Found #{stuck_count} stuck virtual hearing#{stuck_count > 1 ? 's': ''}: "
+    stuck_virtual_hearings.each do |stuck_vh|
+      add_to_report "VirtualHearing.find(#{stuck_vh.id}) last processed at #{stuck_vh.establishment.processed_at}"
+    end
+  end
+end
