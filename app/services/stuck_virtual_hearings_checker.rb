1ebc88ddd0e6cafade3c13424fb8f2c3ef89911a
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-05-04 18:34:00 -0400

    small changes

--- app/services/stuck_virtual_hearings_checker.rb
+++ app/services/stuck_virtual_hearings_checker.rb
@@ -23,9 +23,11 @@ class StuckVirtualHearingsChecker < DataIntegrityChecker
 
     stuck_count = stuck_virtual_hearings.count
 
-    add_to_report "Found #{stuck_count} stuck #{(stuck_count > 1) ? 'virtual hearings' : 'virtual hearing'}: "
+    add_to_report "Found #{stuck_count} stuck #{'virtual hearing'.pluralize(stuck_count)}: "
     stuck_virtual_hearings.each do |stuck_vh|
       add_to_report "VirtualHearing.find(#{stuck_vh.id}) last processed at #{stuck_vh.establishment.processed_at}"
     end
+
+    add_to_report "The #{'virtual hearing'.pluralize(stuck_count)} may not progress without manual intervention"
   end
 end
