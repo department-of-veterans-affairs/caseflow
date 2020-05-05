42430d29a25cecf93eaf461f7754a98574a34049
Peter Karman <peter@truss.works>
2020-05-05 09:42:18 -0500

    refactor

--- app/models/etl/vha_decision_review.rb
+++ app/models/etl/vha_decision_review.rb
@@ -7,25 +7,15 @@
 #   * HIGHER_LEVEL_REVIEWS: Intake data for Higher level Reviews
 #   * SUPPLEMENTAL CLAIMS: Intake data for Supplemental Claims
 #   * APPEALS: Used to keep track of information for AMA appeals
-#   * DECISION_ISSUES: Issue-level dispositions for AMA claims/appeals
-#   * REMAND_REASONS: Remand reason for decision issues.
 #
 # To make it simple for VHA to pull the data they need, we're reporting data as a single table.
 
 class ETL::VhaDecisionReview < ETL::Record
   class << self
-
     private
 
-    # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
-    def merge_original_attributes_to_target(original, target)
-
-      target.decision_review_id = original.id
-      target.decision_review_type = original.class.name
-
+    def common_decision_review_attributes
       [
-        # common Appeal, HLR, and SC attributes
         :benefit_type,
         :establishment_processed_at,
         :establishment_submitted_at,
@@ -33,21 +23,26 @@ class ETL::VhaDecisionReview < ETL::Record
         :receipt_date,
         :uuid,
         :veteran_file_number,
-        :veteran_is_not_claimant,
-      ].each do |attr|
-        target[attr] = original[attr]
-      end
+        :veteran_is_not_claimant
+      ]
+    end
 
+    def unique_higher_level_review_attributes
       [
-        # attributes unique to HLR
         :informal_conference,
-        :same_office,
+        :same_office
+      ]
+    end
 
-        # attributes unique to SC
+    def unique_supplemental_claim_attributes
+      [
         :decision_review_remanded_id,
-        :decision_review_remanded_type,
+        :decision_review_remanded_type
+      ]
+    end
 
-        # attributes unique to Appeal
+    def unique_appeal_attributes
+      [
         :closest_regional_office,
         :docket_range_date,
         :docket_type,
@@ -56,17 +51,28 @@ class ETL::VhaDecisionReview < ETL::Record
         :stream_docket_number,
         :stream_type,
         :target_decision_date
-      ].each do |attr|
-        target[attr] = original[attr] if original.respond_to?(:attr)
+      ]
+    end
+
+    def unique_attributes_method_name(klass)
+      "unique_#{klass.underscore}_attributes".to_sym
+    end
+
+    def merge_original_attributes_to_target(original, target)
+      target.decision_review_id = original.id
+      target.decision_review_type = original.class.name
+
+      common_decision_review_attributes.each do |attr|
+        target[attr] = original[attr]
       end
 
-      # TODO: check if results from each model's methods should be captured in a column
+      send(unique_attributes_method_name, original.class.name).each do |attr|
+        target[attr] = original[attr]
+      end
 
-      # DecisionIssue attributes
-      decision_issues = DecisionIssue.where(decision_review_type: target[:decision_review_type], decision_review_id: target[:decision_review_id])
-      # TODO: add decision_issues?
+      # to do: based on VHA feedback we might need to add more calculated values
+
+      target
     end
-    # rubocop:enable Metrics/AbcSize
-    # rubocop:enable Metrics/MethodLength
   end
 end
