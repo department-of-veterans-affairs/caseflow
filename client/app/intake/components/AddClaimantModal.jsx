3763f24fb665fbbc53f2e85ba4b6801c9b788971
JC Quirin <jcq@jcq.lan>
2020-05-06 11:33:48 -0700

    restructured sample data to make it clear `AddClaimantModal` is focused on returning participantId

--- client/app/intake/components/AddClaimantModal.jsx
+++ client/app/intake/components/AddClaimantModal.jsx
@@ -1,20 +1,31 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../../components/Modal';
 import { ADD_CLAIMANT_MODAL_TITLE, ADD_CLAIMANT_MODAL_DESCRIPTION } from '../../../COPY';
 import ReactMarkdown from 'react-markdown';
 import SearchableDropdown from '../../components/SearchableDropdown';
 
-const tempOpts = [
-  { label: 'Attorney 1', value: 'CSS_ID_1' },
-  { label: 'Attorney 2', value: 'CSS_ID_2' },
-  { label: 'Attorney 3', value: 'CSS_ID_3' }
+const sampleData = [
+  { id: 1, fullName: 'Attorney 1', cssId: 'CSS_ID_1', participantId: 1 },
+  { id: 2, fullName: 'Attorney 2', cssId: 'CSS_ID_2', participantId: 2 },
+  { id: 3, fullName: 'Attorney 3', cssId: 'CSS_ID_3', participantId: 3 }
 ];
 
 export const AddClaimantModal = ({ onCancel, onSubmit }) => {
   const [claimant, setClaimant] = useState(null);
-  const inValid = useMemo(() => !claimant, [claimant]);
+  const [searchResults] = useState(sampleData);
+  const [dropdownOpts, setDropdownOpts] = useState([]);
+  const isInvalid = useMemo(() => !claimant, [claimant]);
   const handleChange = (value) => setClaimant(value);
+
+  useEffect(() => {
+    setDropdownOpts(
+      searchResults.map((item) => ({
+        label: item.fullName,
+        value: item.participantId
+      }))
+    );
+  }, [searchResults]);
 
   const buttons = [
     {
@@ -25,8 +36,8 @@ export const AddClaimantModal = ({ onCancel, onSubmit }) => {
     {
       classNames: ['usa-button', 'usa-button-primary'],
       name: 'Add this claimant',
-      onClick: () => onSubmit({ claimant }),
-      disabled: inValid
+      onClick: () => onSubmit({ participantId: claimant.value }),
+      disabled: isInvalid
     }
   ];
 
@@ -35,7 +46,7 @@ export const AddClaimantModal = ({ onCancel, onSubmit }) => {
       <div>
         <ReactMarkdown source={ADD_CLAIMANT_MODAL_DESCRIPTION} />
       </div>
-      <SearchableDropdown label="Claimant's name" options={tempOpts} onChange={handleChange} value={claimant} />
+      <SearchableDropdown label="Claimant's name" options={dropdownOpts} onChange={handleChange} value={claimant} />
     </Modal>
   );
 };
