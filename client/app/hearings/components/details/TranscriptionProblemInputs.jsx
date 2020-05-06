89f68e261353531c0aae42861e07515536df2882
Ferris Tseng <ferris@navapbc.com>
2020-05-06 12:28:29 -0400

    move transcription options to constants file

--- client/app/hearings/components/details/TranscriptionProblemInputs.jsx
+++ client/app/hearings/components/details/TranscriptionProblemInputs.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import _ from 'lodash';
 
 import { rowThirds } from './style';
-
+import DateSelector from '../../../components/DateSelector';
 import RadioField from '../../../components/RadioField';
 import SearchableDropdown from '../../../components/SearchableDropdown';
-import DateSelector from '../../../components/DateSelector';
+import TRANSCRIPTION_PROBLEM_TYPES from
+  '../../../../constants/TRANSCRIPTION_PROBLEM_TYPES.json';
+import TRANSCRIPTION_REQUESTED_REMEDIES from
+  '../../../../constants/TRANSCRIPTION_REQUESTED_REMEDIES.json';
+
+const TRANSCRIPTION_PROBLEM_OPTIONS = _.concat(
+  _.map(
+    _.values(TRANSCRIPTION_PROBLEM_TYPES),
+    (value) => ( { label: value, value } )
+  ),
+  [
+    {
+      label: '',
+      value: null
+    }
+  ]
+);
+
+const TRANSCRIPTION_REMEDIES_OPTIONS = _.concat(
+  _.map(
+    _.values(TRANSCRIPTION_REQUESTED_REMEDIES),
+    (value) => ( { displayText: value, value } )
+  ),
+  [
+    {
+      value: '',
+      displayText: 'None'
+    }
+  ]
+);
 
 const TranscriptionProblemInputs = ({ transcription, update, readOnly }) => (
   <div {...rowThirds}>
@@ -16,28 +45,7 @@ const TranscriptionProblemInputs = ({ transcription, update, readOnly }) => (
       strongLabel
       readOnly={readOnly}
       value={transcription?.problemType}
-      options={[
-        {
-          label: '',
-          value: null
-        },
-        {
-          label: 'No audio',
-          value: 'No audio'
-        },
-        {
-          label: 'Poor Audio Quality',
-          value: 'Poor Audio Quality'
-        },
-        {
-          label: 'Incomplete Hearing',
-          value: 'Incomplete Hearing'
-        },
-        {
-          label: 'Other (see notes)',
-          value: 'Other (see notes)'
-        }
-      ]}
+      options={TRANSCRIPTION_PROBLEM_OPTIONS}
       onChange={(option) => update({ problemType: (option || {}).value })}
     />
     <DateSelector
@@ -53,28 +61,14 @@ const TranscriptionProblemInputs = ({ transcription, update, readOnly }) => (
       name="requestedRemedy"
       label="Requested Remedy"
       strongLabel
-      options={[
-        {
-          value: '',
-          displayText: 'None',
-          disabled: readOnly || _.isEmpty(transcription?.problemType)
-        },
-        {
-          value: 'Proceed without transcript',
-          displayText: 'Proceeed without transcript',
-          disabled: readOnly || _.isEmpty(transcription?.problemType)
-        },
-        {
-          value: 'Proceed with partial transcript',
-          displayText: 'Process with partial transcript',
-          disabled: readOnly || _.isEmpty(transcription?.problemType)
-        },
-        {
-          value: 'New hearing',
-          displayText: 'New hearing',
-          disabled: readOnly || _.isEmpty(transcription?.problemType)
-        }
-      ]}
+      options={
+        _.map(
+          TRANSCRIPTION_REMEDIES_OPTIONS,
+          (entry) => (
+            _.extend(entry, { disabled: readOnly || _.isEmpty(transcription?.problemType) })
+          )
+        )
+      }
       value={transcription?.requestedRemedy || ''}
       onChange={(requestedRemedy) => update({ requestedRemedy })}
     />
