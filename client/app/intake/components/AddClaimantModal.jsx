5c99f45712f01b8415b5faca6dcb9adf1ab346ef
JC Quirin <jcq@jcq.lan>
2020-05-06 16:11:22 -0700

    implemented debounce & async search to `AddClaimantModal`

--- client/app/intake/components/AddClaimantModal.jsx
+++ client/app/intake/components/AddClaimantModal.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { debounce } from 'lodash';
 import Modal from '../../components/Modal';
 import { ADD_CLAIMANT_MODAL_TITLE, ADD_CLAIMANT_MODAL_DESCRIPTION } from '../../../COPY';
 import ReactMarkdown from 'react-markdown';
@@ -11,21 +12,34 @@ const sampleData = [
   { id: 3, fullName: 'Attorney 3', cssId: 'CSS_ID_3', participantId: 3 }
 ];
 
-export const AddClaimantModal = ({ onCancel, onSubmit }) => {
-  const [claimant, setClaimant] = useState(null);
-  const [searchResults] = useState(sampleData);
-  const [dropdownOpts, setDropdownOpts] = useState([]);
-  const isInvalid = useMemo(() => !claimant, [claimant]);
-  const handleChange = (value) => setClaimant(value);
+const fetchAttorneys = async (search = '') => {
+  // TODO: replace with actual API call
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const res = sampleData.filter((item) => item.fullName.toLowerCase().includes(search));
 
-  useEffect(() => {
-    setDropdownOpts(
-      searchResults.map((item) => ({
+      return res ? resolve(res) : reject('no records found');
+    }, 1500);
+  });
+};
+const debouncedFetch = debounce(fetchAttorneys, 250, { leading: true, trailing: false });
+const getClaimantOpts = async (search = '') => {
+  // Enforce minimum search length (we'll simply return empty array rather than throw error)
+  const options =
+    search.length < 3 ?
+      [] :
+      (await debouncedFetch(search)).map((item) => ({
         label: item.fullName,
         value: item.participantId
-      }))
-    );
-  }, [searchResults]);
+      }));
+
+  return { options };
+};
+
+export const AddClaimantModal = ({ onCancel, onSubmit }) => {
+  const [claimant, setClaimant] = useState(null);
+  const isInvalid = useMemo(() => !claimant, [claimant]);
+  const handleChange = (value) => setClaimant(value);
 
   const buttons = [
     {
@@ -46,7 +60,15 @@ export const AddClaimantModal = ({ onCancel, onSubmit }) => {
       <div>
         <ReactMarkdown source={ADD_CLAIMANT_MODAL_DESCRIPTION} />
       </div>
-      <SearchableDropdown label="Claimant's name" options={dropdownOpts} onChange={handleChange} value={claimant} />
+      <SearchableDropdown
+        name="search"
+        label="Claimant's name"
+        onChange={handleChange}
+        value={claimant}
+        async={getClaimantOpts}
+        options={[]}
+        debounce={250}
+      />
     </Modal>
   );
 };
