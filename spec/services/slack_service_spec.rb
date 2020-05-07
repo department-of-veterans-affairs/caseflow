b19d21b4d7d3bf865c7a9c805610f57f724ef6a9
Peter Karman <peter@truss.works>
2020-05-07 08:51:06 -0500

    Slack color based on keywords

--- spec/services/slack_service_spec.rb
+++ spec/services/slack_service_spec.rb
@@ -2,9 +2,18 @@
 
 describe SlackService do
   let(:slack_service) { SlackService.new(url: "http://www.example.com") }
+  let(:http_agent) { double("http") }
+
+  before do
+    @http_params = nil
+    allow(HTTPClient).to receive(:new) { http_agent }
+    allow(http_agent).to receive(:post) do |url, params|
+      @http_params = params
+      "response"
+    end
+  end
 
   it "posts to http" do
-    allow_any_instance_of(HTTPClient).to receive(:post).and_return("response")
     response = slack_service.send_notification("hello")
     expect(response).to eq("response")
   end
@@ -12,8 +21,39 @@ describe SlackService do
   context "when it is run in the uat environment" do
     it "does not make post request" do
       stub_const("ENV", "DEPLOY_ENV" => "uat")
-      expect_any_instance_of(HTTPClient).to_not receive(:post)
+      #expect_any_instance_of(HTTPClient).to_not receive(:post)
       slack_service.send_notification("filler message contents")
+      expect(@http_params).to be_nil
+    end
+  end
+
+  context "color selection" do
+    context "title contains ERROR" do
+      it "picks red color" do
+        slack_service.send_notification("filler message contents", "[ERROR] ouch!")
+        expect(@http_params[:body]).to match /"#ff0000"/
+      end
+    end
+
+    context "message contains error" do
+      it "picks red color" do
+        slack_service.send_notification("there was an error")
+        expect(@http_params[:body]).to match /"#ff0000"/
+      end
+    end
+
+    context "message contains warning" do
+      it "picks yellow color" do
+        slack_service.send_notification("a warning of something")
+        expect(@http_params[:body]).to match /"#ffff00"/
+      end
+    end
+
+    context "no magic string" do
+      it "defaults to gray" do
+        slack_service.send_notification("hello world")
+        expect(@http_params[:body]).to match /"#cccccc"/
+      end
     end
   end
 end
