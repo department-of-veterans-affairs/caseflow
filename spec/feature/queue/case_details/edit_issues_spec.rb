810bfc2abe0c6a28b9038d8d93e072b012c45cdd
Hunter Schallhorn <45575454+hschallhorn@users.noreply.github.com>
2020-05-08 15:55:14 -0400

    Allow intake users to edit issues from the case details screen (#14237)
    Resolves board request from demo
    
    ### Description
    Allows intake users to see the edit issues link on the case details page. Also removes references to feature toggle that have been enabled for all users for 9 months.
    
    ### Testing!
    1. Log in as an intake user BVAISHAW and go to an ama case
    2. Ensure you can see the "Correct issues" link in case details
    
    ### UI Changes
    ![Screen Shot 2020-05-08 at 2 23 48 PM](https://user-images.githubusercontent.com/45575454/81436519-a246d000-9137-11ea-91ed-c588124bb61c.png)

--- spec/feature/queue/case_details/edit_issues_spec.rb
+++ spec/feature/queue/case_details/edit_issues_spec.rb
@@ -1,48 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.feature "Edit Issues", :all_dbs do
-  before do
-    FeatureToggle.enable!(:allow_judge_edit_issues)
+  shared_examples "can NOT view the 'Correct Issues' link" do
+    it "does not show 'Correct Issues' link" do
+      visit("/queue/appeals/#{appeal.uuid}")
+
+      expect(page).to have_no_content(COPY::CORRECT_REQUEST_ISSUES_LINK)
+    end
   end
 
-  after do
-    FeatureToggle.disable!(:allow_judge_edit_issues)
+  shared_examples "can view the 'Correct Issues' link" do
+    it "shows 'Correct Issues' link" do
+      visit("/queue/appeals/#{appeal.uuid}")
+
+      expect(page).to have_content(COPY::CORRECT_REQUEST_ISSUES_LINK)
+    end
+  end
+
+  let(:user) { create(:user, station_id: User::BOARD_STATION_ID) }
+  let(:appeal) { create(:appeal) }
+
+  before { User.authenticate!(user: user) }
+
+  context "for a misc user" do
+    it_behaves_like "can NOT view the 'Correct Issues' link"
+  end
+
+  context "for an intake user" do
+    before { BvaIntake.singleton.add_user(user) }
+    it_behaves_like "can view the 'Correct Issues' link"
+  end
+
+  context "for an intake user" do
+    before { CaseReview.singleton.add_user(user) }
+    it_behaves_like "can view the 'Correct Issues' link"
   end
 
   context "for a judge" do
-    let(:judge) { create(:user, station_id: User::BOARD_STATION_ID) }
-    let!(:judge_staff) { create(:staff, :judge_role, sdomainid: judge.css_id) }
-
-    before do
-      User.authenticate!(user: judge)
-    end
+    let!(:user_staff) { create(:staff, :judge_role, sdomainid: user.css_id) }
+    let!(:hearing) { create(:hearing, appeal: appeal, judge: user) }
 
     context "that is assigned to a hearing" do
-      let(:appeal) { create(:appeal) }
-      let!(:hearing) { create(:hearing, appeal: appeal, judge: judge) }
-
-      it "can view the 'Correct Issues' link" do
-        visit("/queue/appeals/#{appeal.uuid}")
-
-        expect(page).to have_content(COPY::CORRECT_REQUEST_ISSUES_LINK)
-      end
+      it_behaves_like "can view the 'Correct Issues' link"
     end
 
     context "that is not assigned to a hearing" do
-      let(:appeal) { create(:appeal) }
-      let!(:hearing) do
-        create(
-          :hearing,
-          appeal: appeal,
-          judge: create(:user, station_id: User::BOARD_STATION_ID)
-        )
-      end
+      before { hearing.update!(judge: create(:user)) }
 
-      it "can NOT view the 'Correct Issues' link" do
-        visit("/queue/appeals/#{appeal.uuid}")
-
-        expect(page).to_not have_content(COPY::CORRECT_REQUEST_ISSUES_LINK)
-      end
+      it_behaves_like "can NOT view the 'Correct Issues' link"
     end
   end
 end
