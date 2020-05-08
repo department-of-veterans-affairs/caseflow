3576c5587b570135a4f774eabdd32a4f2b090a4a
JC Quirin <jcq@jcq.lan>
2020-05-08 14:38:48 -0700

    initial work on CreateBgsAttorneys migration

---
+++ db/migrate/20200508210719_create_bgs_attorneys.rb
@@ -0,0 +1,16 @@
+class CreateBgsAttorneys < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bgs_attorneys comment: "Attorney data cached from BGS — used for claimants" do |t|
+      t.timestamps null: false, comment: "Standard created_at/updated_at timestamps"
+      t.datetime "last_synced_at", comment: "The last time BGS was checked"
+      t.string "participant_id", null: false, comment: "Participant ID"
+      t.string "name", null: false, comment: "Name"
+      t.string "type", null: false, comment: "Type of Record"
+      t.string "legacy_poa_cd", comment: "Legacy POA code"
+
+      t.index ["created_at"]
+      t.index ["updated_at"]
+      t.index ["last_synced_at"]
+    end
+  end
+end
