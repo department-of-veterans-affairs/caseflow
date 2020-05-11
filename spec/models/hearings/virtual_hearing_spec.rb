c8342b235fa3f1cf9c12f1e4692b4f4f9623e2dc
Ferris Tseng <ferris@navapbc.com>
2020-05-11 12:33:46 -0400

    add tests to ensure that new fail-safe virtual hearing validation works

--- spec/models/hearings/virtual_hearing_spec.rb
+++ spec/models/hearings/virtual_hearing_spec.rb
@@ -33,5 +33,73 @@ describe VirtualHearing do
 
       it { expect(subject).to be(false) }
     end
+
+    shared_examples_for "hearing with existing virtual hearing" do
+      context "has existing active virtual hearing" do
+        let!(:existing_virtual_hearing) do
+          create(
+            :virtual_hearing,
+            :initialized,
+            :all_emails_sent,
+            status: :active,
+            hearing: hearing
+          )
+        end
+        let(:virtual_hearing) { build(:virtual_hearing, hearing: hearing) }
+
+        it "is invalid" do
+          hearing.reload
+          expect(subject).to be(false)
+        end
+      end
+
+      context "has existing cancelled virtual hearing" do
+        let!(:existing_virtual_hearing) do
+          create(
+            :virtual_hearing,
+            :initialized,
+            :all_emails_sent,
+            status: :cancelled,
+            hearing: hearing
+          )
+        end
+        let(:virtual_hearing) { build(:virtual_hearing, hearing: hearing) }
+
+        it "is valid" do
+          hearing.reload
+          expect(subject).to be(true)
+        end
+      end
+    end
+
+    context "for a video ama hearing" do
+      let(:hearing) do
+        create(
+          :hearing,
+          hearing_day: create(
+            :hearing_day,
+            request_type: HearingDay::REQUEST_TYPES[:video],
+            regional_office: "RO01"
+          )
+        )
+      end
+
+      it_behaves_like "hearing with existing virtual hearing"
+    end
+
+    context "for video legacy hearing" do
+      let(:hearing) do
+        create(
+          :legacy_hearing,
+          hearing_day: create(
+            :hearing_day,
+            request_type: HearingDay::REQUEST_TYPES[:video],
+            regional_office: "RO01"
+          )
+        )
+      end
+
+      it_behaves_like "hearing with existing virtual hearing"
+    end
   end
 end
