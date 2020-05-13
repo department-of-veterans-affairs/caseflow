3aa2ba63612a220efdf2a59d02fff110b23e1136
hschallhorn <hschallhorn@tistatech.com>
2020-05-12 22:56:25 -0400

    Remove assignee_label from cached appeals

--- app/jobs/update_cached_appeals_attributes_job.rb
+++ app/jobs/update_cached_appeals_attributes_job.rb
@@ -33,7 +33,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
     appeals = Appeal.includes(:available_hearing_locations).where(id: open_appeals_from_tasks(Appeal.name))
     request_issues_to_cache = request_issue_counts_for_appeal_ids(appeals.pluck(:id))
     veteran_names_to_cache = veteran_names_for_file_numbers(appeals.pluck(:veteran_file_number))
-    appeal_assignees_to_cache = assignees_for_caseflow_appeal_ids(appeals.pluck(:id), Appeal.name)
 
     appeal_aod_status = aod_status_for_appeals(appeals)
     representative_names = representative_names_for_appeals(appeals)
@@ -43,7 +42,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
       {
         appeal_id: appeal.id,
         appeal_type: Appeal.name,
-        assignee_label: appeal_assignees_to_cache[appeal.id],
         case_type: appeal.type,
         closest_regional_office_city: regional_office ? regional_office[:city] : COPY::UNKNOWN_REGIONAL_OFFICE,
         closest_regional_office_key: regional_office ? appeal.closest_regional_office : COPY::UNKNOWN_REGIONAL_OFFICE,
@@ -57,8 +55,7 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
       }
     end
 
-    update_columns = [:assignee_label,
-                      :case_type,
+    update_columns = [:case_type,
                       :closest_regional_office_city,
                       :closest_regional_office_key,
                       :docket_type,
@@ -147,7 +144,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
 
       issue_counts_to_cache = issues_counts_for_vacols_folders(batch_vacols_ids)
       aod_status_to_cache = VACOLS::Case.aod(batch_vacols_ids)
-      appeal_assignees_to_cache = assignees_for_vacols_id(vacols_cases)
 
       correspondent_ids = vacols_folders.map { |folder| folder[:correspondent_id] }
       veteran_names_to_cache = veteran_names_for_correspondent_ids(correspondent_ids)
@@ -157,7 +153,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
 
         {
           vacols_id: folder[:vacols_id],
-          assignee_label: appeal_assignees_to_cache[folder[:vacols_id]],
           case_type: vacols_case[:status],
           docket_number: folder[:docket_number],
           issue_count: issue_counts_to_cache[folder[:vacols_id]] || 0,
@@ -166,7 +161,7 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
         }
       end
 
-      update_columns = [:assignee_label, :docket_number, :issue_count, :veteran_name, :case_type, :is_aod]
+      update_columns = [:docket_number, :issue_count, :veteran_name, :case_type, :is_aod]
       CachedAppeal.import values_to_cache, on_duplicate_key_update: { conflict_target: [:vacols_id],
                                                                       columns: update_columns }
 
@@ -234,36 +229,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
     end.to_h
   end
 
-  def assignees_for_caseflow_appeal_ids(appeal_ids, appeal_type)
-    active_appeals = caseflow_appeals_assignees(appeal_ids, appeal_type, Task.active)
-    on_hold_appeals = caseflow_appeals_assignees(appeal_ids, appeal_type, Task.on_hold)
-
-    on_hold_appeals.merge(active_appeals)
-  end
-
-  def assignees_for_vacols_id(vacols_cases)
-    # Grab statuses from input hash of VACOLS cases.
-    vacols_statuses = vacols_cases.keys.map do |vacols_id|
-      [vacols_id, vacols_cases[vacols_id][:location]]
-    end.to_h
-
-    # Grab the appeal_ids for the VACOLS cases in CASEFLOW status
-    caseflow_vacols_ids = vacols_statuses.select { |_key, value| value == "CASEFLOW" }.keys
-    caseflow_vacols_to_appeal_id = LegacyAppeal.where(vacols_id: caseflow_vacols_ids).pluck(:vacols_id, :id).to_h
-
-    # Lookup more detailed Caseflow location for CASEFLOW vacols status
-    caseflow_statuses_by_appeal_id = assignees_for_caseflow_appeal_ids(caseflow_vacols_to_appeal_id.values,
-                                                                       LegacyAppeal.name)
-    # Map back to VACOLS id
-    caseflow_statuses_by_vacol_id = {}
-    caseflow_vacols_to_appeal_id.each do |vacols_id, appeal_id|
-      caseflow_statuses_by_vacol_id[vacols_id] = caseflow_statuses_by_appeal_id[appeal_id]
-    end
-
-    # Overwrite VACOLS Caseflow location with Caseflow detailed location
-    vacols_statuses.merge(caseflow_statuses_by_vacol_id)
-  end
-
   def case_fields_for_vacols_ids(vacols_ids)
     # array of arrays will become hash with bfkey as key.
     # [
@@ -286,25 +251,6 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
         }
       ]
     end.to_h
-  end
-
-  def caseflow_appeals_assignees(appeal_ids, appeal_type, tasks)
-    ordered_tasks = tasks
-      .visible_in_queue_table_view
-      .where(appeal_type: appeal_type, appeal_id: appeal_ids)
-      .order(:appeal_id, created_at: :desc)
-
-    first_task_assignees_per_caseflow_appeal(ordered_tasks)
-  end
-
-  def first_task_assignees_per_caseflow_appeal(tasks)
-    org_tasks = tasks.joins("left join organizations on tasks.assigned_to_id = organizations.id")
-      .where("tasks.assigned_to_type = 'Organization'").pluck(:created_at, :appeal_id, "organizations.name")
-    user_tasks = tasks.joins("left join users on tasks.assigned_to_id = users.id")
-      .where("tasks.assigned_to_type = 'User'").pluck(:created_at, :appeal_id, "users.css_id")
-
-    # Combine the user & org tasks, preferring the most recently created (last) task
-    (org_tasks + user_tasks).sort_by { |task| task[0] }.map { |task| task.drop(1) }.to_h
   end
 
   def issues_counts_for_vacols_folders(vacols_ids)
