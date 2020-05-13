b5ba17bffba251b96c8f9837bda38a32e9b21157
hschallhorn <hschallhorn@tistatech.com>
2020-05-12 22:19:15 -0400

    Show task assignee name in queue tables

--- app/models/task.rb
+++ app/models/task.rb
@@ -77,6 +77,10 @@ class Task < CaseflowRecord
                                  )
                                }
 
+  scope :with_assignees, -> { joins(Task.joins_with_assignees_clause) }
+
+  scope :with_assigners, -> { joins(Task.joins_with_assigners_clause) }
+
   scope :with_cached_appeals, -> { joins(Task.joins_with_cached_appeals_clause) }
 
   ############################################################################################
@@ -173,6 +177,21 @@ class Task < CaseflowRecord
         assigned_to: params[:assigned_to] || child_task_assignee(parent, params),
         instructions: params[:instructions]
       )
+    end
+
+    def joins_with_assigners_clause
+      "LEFT JOIN (SELECT id, full_name AS display_name FROM users) AS assigners ON assigners.id = tasks.assigned_by_id"
+    end
+
+    def assignees_table_clause
+      "(SELECT id, 'Organization' AS type, name AS display_name FROM organizations " \
+        "UNION " \
+      "SELECT id, 'User' AS type, full_name AS display_name FROM users) AS assignees"
+    end
+
+    def joins_with_assignees_clause
+      "INNER JOIN #{Task.assignees_table_clause} ON " \
+      "assignees.id = tasks.assigned_to_id AND assignees.type = tasks.assigned_to_type"
     end
 
     def joins_with_cached_appeals_clause
