0f9dcf8a1711eff7070187243b54c9be2a83a05b
JC Quirin <jcq@jcq.lan>
2020-05-14 13:06:16 -0700

    linting

--- app/models/bgs_attorney.rb
+++ app/models/bgs_attorney.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class BgsAttorney < CaseflowRecord
-    include AssociatedBgsRecord
-    include BgsService
-  
-    class BgsAttorneyNotFound < StandardError; end
+  include AssociatedBgsRecord
+  include BgsService
 
-    class << self
-        def fetch_bgs_attorneys
-            # client.data.find_power_of_attorneys
-            bgs.find_power_of_attorneys
-        end
+  class BgsAttorneyNotFound < StandardError; end
+
+  class << self
+    def fetch_bgs_attorneys
+      # Implement connection to existing BGS service method
+      # client.data.find_power_of_attorneys
+      # bgs.find_power_of_attorneys
     end
+  end
 end
