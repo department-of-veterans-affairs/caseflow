12f0d5f753b019efa93ce888e7c8dae500a17d54
Peter Karman <peter@truss.works>
2020-05-13 23:04:54 -0500

    test

---
+++ spec/models/distributed_case_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+describe DistributedCase do
+  let!(:vacols_judge) { create(:staff, :judge_role, sdomainid: judge.css_id) }
+  let(:judge) { create(:user, :judge) }
+  let(:distribution) { Distribution.create!(judge: judge) }
+  let(:distributed_case) do
+    DistributedCase.create!(
+      distribution: distribution,
+      ready_at: Time.zone.now,
+      docket: "foo",
+      priority: false,
+      case_id: "123"
+    )
+  end
+
+  describe "#redistribute!" do
+    subject { distributed_case.redistribute! }
+
+    it "updates the case_id" do
+      subject
+
+      expect(distributed_case.reload.case_id).to match(/123-redistributed-/)
+    end
+  end
+end   
