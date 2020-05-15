39b6a987f4f941bd6c71aea245f264944734b5f3
Yang Yang <yangyang@navapbc.com>
2020-05-15 11:31:10 -0400

    Flesh out attorney search tests

--- app/services/attorney_search.rb
+++ app/services/attorney_search.rb
@@ -3,7 +3,9 @@
 require "fuzzy_match"
 
 class AttorneySearch
-  attr_accessor :first_letters
+  # only return results scoring at least this fraction of the top score
+  RELATIVE_SCORE_THRESHOLD = 0.5
+
   attr_reader :query_text
 
   def initialize(query_text)
@@ -11,14 +13,23 @@ class AttorneySearch
   end
 
   def fetch_attorneys
-    return [] if first_letters.empty?
-
     candidates_by_name = {}
     first_letter_candidates.each do |atty|
       (candidates_by_name[atty.name] ||= []) << atty
     end
     top_names = top_matched_names(candidates_by_name.keys)
     top_names.map { |name| candidates_by_name[name] }.flatten
+  end
+
+  # get all attorneys from PG that have first-letter matches against the query text
+  def first_letter_candidates
+    return [] if first_letters.empty?
+
+    @first_letter_candidates ||= begin
+      regexes = first_letters.map { |ch| "\\m" + ch } # \m is POSIX regex for start-of-word
+      where = (["name ~* ?"] * regexes.length).join(" AND ")
+      BgsAttorney.where(where, *regexes)
+    end
   end
 
   private
@@ -29,20 +40,12 @@ class AttorneySearch
     end
   end
 
-  # get all attorneys from PG that have first-letter matches against the query text
-  def first_letter_candidates
-    @first_letter_candidates ||= begin
-      regexes = first_letters.map { |ch| "\\m" + ch } # \m is POSIX regex for start-of-word
-      where = (["name ~* ?"] * regexes.length).join(" AND ")
-      BgsAttorney.where(where, *regexes)
-    end
-  end
-
   def top_matched_names(haystack)
     return [] if haystack.empty?
 
     # maps each name in haystack to [name, Dice's coefficient, Levenshtein distance]
     results = FuzzyMatch.new(haystack).find_all_with_score(query_text)
-    results.select { |res| res[1] >= results[0][1] * 0.5 }.map(&:first)
+    threshold = results[0][1] * RELATIVE_SCORE_THRESHOLD
+    results.select { |res| res[1] >= threshold }.map(&:first)
   end
 end
