department-of-veterans-affairs/caseflow

commit c54a195633d9b21b439a46a418e5c019fbdfd396

BgsPowerOfAttorney model (#13922)
Resolves #13883 

### Description

Converts the `BgsPowerOfAttorney` service class to an Active Record model.

There's a lot of files touched in this PR, but most of them are fixing test fixture data to work with this new model.

### Testing Plan
1. Monitor nightly warm caches job for errors.

### User Facing Changes

None.
 
### Documentation Updates
- [x] Topline File Descriptions Added or Updated as Needed

### Database Changes
*Only for Schema Changes*

* [x] Timestamps (created_at, updated_at) for new tables
* [x] Column comments updated
* [x] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
* [x] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
* [x] DB schema docs updated with `make docs`
* [x] #appeals-schema notified with summary and link to this PR

.reek.yml
@@ -64,6 +64,7 @@ detectors:
       - Fakes::EndProductStore
       - Fakes::BGSService#get_participant_id_for_user
       - Fakes::BGSServiceRecordMaker
+      - Fakes::BGSService#fetch_poas_by_participant_ids
       - Fakes::PersistentStore#convert_dates_from
       - FetchDocumentsForReaderJob#fetch_for_appeal
       - HearingSerializerBase
@@ -128,6 +129,9 @@ detectors:
       - RequestIssue
       - BulkTaskReassignment
       - Task
+  TooManyConstants:
+    exclude:
+      - Fakes::BGSServicePOA
   TooManyInstanceVariables:
     exclude:
       - AmaAppealDispatch
@@ -163,6 +167,7 @@ detectors:
       - OrganizationOnHoldTasksTab#name
       - OrganizationTrackingTasksTab#column_names
       - PendingIncompleteAndUncancelledTaskTimersQuery#call
+      - PowerOfAttorneyMapper#get_claimant_poa_from_bgs_poa
       - Reporter#average
       - Reporter#median
       - Reporter#seconds_to_hms

app/controllers/idt/api/v1/base_controller.rb
@@ -10,7 +10,7 @@ class Idt::Api::V1::BaseController < ActionController::Base
 
   # :nocov:
   rescue_from StandardError do |error|
-    Raven.capture_exception(error)
+    log_error(error)
     if error.class.method_defined?(:serialize_response)
       render(error.serialize_response)
     else
@@ -71,5 +71,13 @@ class Idt::Api::V1::BaseController < ActionController::Base
 
   def feature_enabled?(feature)
     FeatureToggle.enabled?(feature, user: user)
+  end
+
+  private
+
+  def log_error(error)
+    Raven.capture_exception(error)
+    Rails.logger.error(error)
+    Rails.logger.error(error.backtrace.join("\n"))
   end
 end

app/controllers/idt/api/v1/veterans_controller.rb
@@ -30,15 +30,25 @@ class Idt::Api::V1::VeteransController < Idt::Api::V1::BaseController
   end
 
   def fetch_poa_with_address
-    poa = bgs.fetch_poa_by_file_number(veteran[:file_number])
+    return {} unless bgs_poa.found?
 
-    return {} unless poa
+    poa_address = bgs_poa.representative_address
 
-    poa_address = bgs.find_address_by_participant_id(poa[:participant_id])
+    return serializable_poa unless poa_address
 
-    return poa unless poa_address
+    serializable_poa.merge(poa_address)
+  end
 
-    poa.merge(poa_address)
+  def bgs_poa
+    @bgs_poa ||= BgsPowerOfAttorney.new(file_number: veteran[:file_number])
+  end
+
+  def serializable_poa
+    {
+      representative_name: bgs_poa.representative_name,
+      representative_type: bgs_poa.representative_type,
+      participant_id: bgs_poa.poa_participant_id
+    }
   end
 
   def json_veteran_details

app/jobs/warm_bgs_caches_job.rb
@@ -18,6 +18,10 @@ class WarmBgsCachesJob < CaseflowJob
   private
 
   def warm_poa_caches
+    # We do 2 passes to update our POA cache (bgs_power_of_attorney table).
+    # 1. Look at the 1000 oldest cached records and update them.
+    # 2. Look at Claimants w/o POA associated record and create cached record.
+
     # we average about 10k Claimant rows created a week.
     # we very rarely update the Claimant record after we create it.
     # so we can use the Claimant.updated_at to reflect how recently we've
@@ -28,13 +32,32 @@ class WarmBgsCachesJob < CaseflowJob
     # we only care about Appeal Claimants because that's all
     # UpdateCachedAppealsAttributesJob cares about.
     # assuming we have 40k open appeals/claimants at any given time,
-    # and we cache each one for 30 days, we want to cache about 1400 a day.
+    # we want to check about 1400 a day to cycle through them all once a month.
+
+    warm_poa_for_oldest_claimants
+    warm_poa_for_oldest_cached_records
+  end
+
+  def warm_poa_for_oldest_cached_records
     start_time = Time.zone.now
-    oldest_claimants_for_open_appeals.limit(1400).each do |claimant|
-      claimant.representative_name # updates_cache
+    oldest_bgs_poa_records.limit(1000).each do |bgs_poa|
+      bgs_poa.update_cached_attributes! if bgs_poa.stale_attributes?
+    end
+    datadog_report_time_segment(segment: "warm_poa_bgs", start_time: start_time)
+  end
+
+  def warm_poa_for_oldest_claimants
+    start_time = Time.zone.now
+    oldest_claimants_with_poa.each do |claimant|
+      bgs_poa = claimant.power_of_attorney
+      bgs_poa.save_with_updated_bgs_record! if bgs_poa.stale_attributes?
       claimant.update!(updated_at: Time.zone.now)
     end
-    datadog_report_time_segment(segment: "warm_poa_caches", start_time: start_time)
+    datadog_report_time_segment(segment: "warm_poa_claimants", start_time: start_time)
+  end
+
+  def oldest_claimants_with_poa
+    oldest_claimants_for_open_appeals.limit(1400).select { |claimant| claimant.power_of_attorney.present? }
   end
 
   def claimants_for_open_appeals
@@ -43,6 +66,10 @@ class WarmBgsCachesJob < CaseflowJob
 
   def oldest_claimants_for_open_appeals
     claimants_for_open_appeals.order(updated_at: :asc)
+  end
+
+  def oldest_bgs_poa_records
+    BgsPowerOfAttorney.order(last_synced_at: :asc)
   end
 
   def open_appeals_from_tasks

app/mappers/power_of_attorney_mapper.rb
@@ -11,8 +11,9 @@ module PowerOfAttorneyMapper
     base.extend(PowerOfAttorneyMapper)
   end
 
+  # used by fetch_poas_by_participant_id (for User)
   def get_poa_from_bgs_poa(bgs_rep = {})
-    return {} unless bgs_rep
+    return {} unless bgs_rep&.dig(:org_type_nm)
 
     bgs_type = bgs_rep[:org_type_nm]
     {
@@ -23,9 +24,30 @@ module PowerOfAttorneyMapper
     }
   end
 
+  # used by fetch_poas_by_participant_ids (for Claimants)
+  # and fetch_poa_by_file_number
+  def get_claimant_poa_from_bgs_poa(bgs_record = {})
+    return {} unless bgs_record.dig(:power_of_attorney)
+
+    bgs_rep = bgs_record[:power_of_attorney]
+    bgs_type = bgs_rep[:org_type_nm]
+    {
+      representative_type: BGS_REP_TYPE_TO_REP_TYPE[bgs_type] || "Other",
+      representative_name: bgs_rep[:nm],
+      # Used to find the POA address
+      participant_id: bgs_rep[:ptcpnt_id],
+      # pass through other attrs
+      authzn_change_clmant_addrs_ind: bgs_rep[:authzn_change_clmant_addrs_ind],
+      authzn_poa_access_ind: bgs_rep[:authzn_poa_access_ind],
+      legacy_poa_cd: bgs_rep[:legacy_poa_cd],
+      file_number: bgs_record[:file_number],
+      claimant_participant_id: bgs_record[:ptcpnt_id]
+    }
+  end
+
   def get_hash_of_poa_from_bgs_poas(bgs_resp)
     [bgs_resp].flatten.each_with_object({}) do |poa, hsh|
-      hsh[poa[:ptcpnt_id]] = get_poa_from_bgs_poa(poa[:power_of_attorney])
+      hsh[poa[:ptcpnt_id]] = get_claimant_poa_from_bgs_poa(poa)
     end
   end
 

app/models/appeal.rb
@@ -354,7 +354,7 @@ class Appeal < DecisionReview
   end
 
   def representatives
-    vso_participant_ids = power_of_attorneys.map(&:participant_id) - [nil]
+    vso_participant_ids = power_of_attorneys.map(&:participant_id).compact.uniq
     Representative.where(participant_id: vso_participant_ids)
   end
 

app/models/bgs_power_of_attorney.rb
@@ -1,0 +1,160 @@
+# frozen_string_literal: true
+
+class BgsPowerOfAttorney < CaseflowRecord
+  include AssociatedBgsRecord
+  include BgsService
+
+  has_many :claimants, primary_key: :claimant_participant_id, foreign_key: :participant_id
+
+  CACHED_BGS_ATTRIBUTES = [
+    :representative_name,
+    :representative_type,
+    :authzn_change_clmant_addrs_ind,
+    :authzn_poa_access_ind,
+    :legacy_poa_cd,
+    :poa_participant_id,
+    :claimant_participant_id,
+    :file_number
+  ].freeze
+
+  delegate :email_address,
+           to: :person, prefix: :representative
+
+  validates :claimant_participant_id, :poa_participant_id, :representative_name, :representative_type, presence: true
+
+  before_save :update_cached_attributes!
+
+  class << self
+    # Neither file_number nor claimant_participant_id is unique by itself,
+    # but we treat them that way for easy lookup. They are unique together in our db.
+    # Since this is a cache we only want to mirror what BGS has and leave the
+    # data integrity to them.
+    def find_or_create_by_file_number(file_number)
+      find_or_create_by!(file_number: file_number)
+    end
+
+    def find_or_create_by_claimant_participant_id(claimant_participant_id)
+      find_or_create_by!(claimant_participant_id: claimant_participant_id)
+    end
+
+    def find_or_load_by_file_number(file_number)
+      find_by(file_number: file_number) || new(file_number: file_number)
+    end
+
+    def fetch_bgs_poa_by_participant_id(pid)
+      bgs.fetch_poas_by_participant_ids([pid])[pid]
+    end
+  end
+
+  def representative_name
+    cached_or_fetched_from_bgs(attr_name: :representative_name)
+  end
+
+  def representative_type
+    cached_or_fetched_from_bgs(attr_name: :representative_type)
+  end
+
+  def authzn_change_clmant_addrs_ind
+    cached_or_fetched_from_bgs(attr_name: :authzn_change_clmant_addrs_ind)
+  end
+
+  def authzn_poa_access_ind
+    cached_or_fetched_from_bgs(attr_name: :authzn_poa_access_ind)
+  end
+
+  def legacy_poa_cd
+    cached_or_fetched_from_bgs(attr_name: :legacy_poa_cd)
+  end
+
+  def representative_address
+    @representative_address ||= load_bgs_address!
+  end
+
+  def poa_participant_id
+    cached_or_fetched_from_bgs(attr_name: :poa_participant_id, bgs_attr: :participant_id)
+  end
+
+  alias participant_id poa_participant_id
+
+  def claimant_participant_id
+    cached_or_fetched_from_bgs(attr_name: :claimant_participant_id)
+  end
+
+  def file_number
+    cached_or_fetched_from_bgs(attr_name: :file_number)
+  end
+
+  def stale_attributes?
+    return false if not_found?
+
+    stale_attributes.any?
+  end
+
+  def update_cached_attributes!
+    stale_attributes.each { |attr| send(attr) }
+    self.last_synced_at = Time.zone.now
+  end
+
+  def save_with_updated_bgs_record!
+    stale_attributes.each do |attr|
+      self[attr] = nil # local object attr empty, should trigger re-fetch of bgs record
+      send(attr)
+    end
+    save!
+  end
+
+  def found?
+    return false if not_found?
+
+    bgs_record.keys.any?
+  end
+
+  private
+
+  def not_found?
+    bgs_record == :not_found
+  end
+
+  def person
+    @person ||= Person.find_or_create_by(participant_id: poa_participant_id)
+  end
+
+  def stale_attributes
+    CACHED_BGS_ATTRIBUTES.select { |attr| self[attr].nil? || self[attr].to_s != bgs_record[attr].to_s }
+  end
+
+  def cached_or_fetched_from_bgs(attr_name:, bgs_attr: nil)
+    bgs_attr ||= attr_name
+    self[attr_name] ||= begin
+      return if bgs_record == :not_found
+
+      bgs_record.dig(bgs_attr)
+    end
+  end
+
+  def fetch_bgs_record
+    if self[:claimant_participant_id]
+      fetch_bgs_record_by_claimant_participant_id
+    elsif self[:file_number]
+      bgs.fetch_poa_by_file_number(self[:file_number])
+    else
+      fail "Must define claimant_participant_id or file_number"
+    end
+  end
+
+  def fetch_bgs_record_by_claimant_participant_id
+    pid = self[:claimant_participant_id]
+    poa = self.class.fetch_bgs_poa_by_participant_id(pid)
+
+    return if poa.blank?
+
+    poa[:claimant_participant_id] ||= pid
+    poa
+  end
+
+  def load_bgs_address!
+    return nil if !participant_id
+
+    BgsAddressService.new(participant_id: poa_participant_id).address
+  end
+end

app/models/claimant.rb
@@ -22,7 +22,7 @@ class Claimant < CaseflowRecord
   end
 
   def power_of_attorney
-    @power_of_attorney ||= BgsPowerOfAttorney.new(claimant_participant_id: participant_id)
+    @power_of_attorney ||= find_power_of_attorney
   end
 
   delegate :representative_name,
@@ -75,6 +75,22 @@ class Claimant < CaseflowRecord
   end
 
   private
+
+  def find_power_of_attorney
+    find_power_of_attorney_by_pid || find_power_of_attorney_by_file_number
+  end
+
+  def find_power_of_attorney_by_pid
+    BgsPowerOfAttorney.find_or_create_by_claimant_participant_id(participant_id)
+  rescue ActiveRecord::RecordInvalid # not found at BGS by PID
+    nil
+  end
+
+  def find_power_of_attorney_by_file_number
+    BgsPowerOfAttorney.find_or_create_by_file_number(decision_review.veteran_file_number)
+  rescue ActiveRecord::RecordInvalid # not found at BGS
+    nil
+  end
 
   def bgs_address_service
     @bgs_address_service ||= BgsAddressService.new(participant_id: participant_id)

commit f15e16253fcaea1c39905eb85573bc7baf0b498e

Fix regression with POA missing cache (#14305)
### Description

Fixes regression introduced in #13922 with the advent of the BgsPowerOfAttorney AR model.

We were skipping our local db cache and always making BGS calls, which slowed the job down to take hours instead of minutes.

.reek.yml
@@ -88,7 +88,7 @@ detectors:
       - Seeds::Users#create_aod_user_and_tasks
       - SyncReviewsJob
       - TaskTimerJob
-      - UpdateCachedAppealsAttributesJob#assignees_for_vacols_id
+      - UpdateCachedAppealsAttributesJob
       - UserReporter#report_user_related_records
       - User#current_case_assignments_with_views
       - User#appeal_has_task_assigned_to_user?
@@ -143,6 +143,7 @@ detectors:
       - RequestIssue
       - BulkTaskReassignment
       - Task
+      - UpdateCachedAppealsAttributesJob
   TooManyConstants:
     exclude:
       - Fakes::BGSServicePOA

app/jobs/update_cached_appeals_attributes_job.rb
@@ -103,7 +103,7 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
     values_to_cache = legacy_appeals.map do |appeal|
       regional_office = RegionalOffice::CITIES[appeal.closest_regional_office]
       # bypass PowerOfAttorney model completely and always prefer BGS cache
-      bgs_poa = BgsPowerOfAttorney.new(file_number: appeal.veteran_file_number)
+      bgs_poa = fetch_bgs_power_of_attorney_by_file_number(appeal.veteran_file_number)
       {
         vacols_id: appeal.vacols_id,
         appeal_id: appeal.id,
@@ -111,7 +111,7 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
         closest_regional_office_city: regional_office ? regional_office[:city] : COPY::UNKNOWN_REGIONAL_OFFICE,
         closest_regional_office_key: regional_office ? appeal.closest_regional_office : COPY::UNKNOWN_REGIONAL_OFFICE,
         docket_type: appeal.docket_name, # "legacy"
-        power_of_attorney_name: bgs_poa.representative_name || appeal.representative_name,
+        power_of_attorney_name: (bgs_poa&.representative_name || appeal.representative_name),
         suggested_hearing_location: appeal.suggested_hearing_location&.formatted_location
       }
     end
@@ -127,6 +127,14 @@ class UpdateCachedAppealsAttributesJob < CaseflowJob
                                                                     ] }
   end
   # rubocop:enable Metrics/MethodLength
+
+  def fetch_bgs_power_of_attorney_by_file_number(file_number)
+    return if file_number.blank?
+
+    BgsPowerOfAttorney.find_or_create_by_file_number(file_number)
+  rescue ActiveRecord::RecordInvalid # not found at BGS
+    BgsPowerOfAttorney.new(file_number: file_number)
+  end
 
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize