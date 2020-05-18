93968089d247c1b0ac8f2f6f01bb39843356d7da
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-05-18 10:55:10 -0400

    Add Retry logic to DeleteConferencesJob (#14254)
    Connects  #14125 
    
    ### Description
    - write method `log_virtual_hearing_state` for base job class `ConferenceJob` which logs the base virtual hearing state; `DeleteConferencesJob` and `CreateConferenceJob` override the method
    - add retry logic to `DeleteConferencesJob` similar to how `CreateConferenceJob` does it
    - write tests
    - add new method `cancellation_emails_sent?` to `VirtualHearing`

--- app/jobs/virtual_hearings/conference_job.rb
+++ app/jobs/virtual_hearings/conference_job.rb
@@ -11,4 +11,16 @@ class VirtualHearings::ConferenceJob < ApplicationJob
       metric_group: Constants.DATADOG_METRICS.HEARINGS.VIRTUAL_HEARINGS_GROUP_NAME
     }
   end
+
+  def log_virtual_hearing_state(virtual_hearing)
+    Rails.logger.info(
+      "Virtual Hearing for hearing (#{virtual_hearing.hearing_type} [#{virtual_hearing.hearing_id}])"
+    )
+    Rails.logger.info(
+      "Emails Sent: (" \
+      "veteran: [#{virtual_hearing.veteran_email_sent} | null?: #{virtual_hearing.veteran_email.nil?}], " \
+      "rep: [#{virtual_hearing.representative_email_sent} | null?: #{virtual_hearing.representative_email.nil?}], " \
+      "judge: [#{virtual_hearing.judge_email_sent} | null?: #{virtual_hearing.judge_email.nil?}])"
+    )
+  end
 end
