1411cee50cf053af941973edc0ed90d7b6888107
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-05-19 16:53:33 -0400

    Added separate setup file for jest

--- client/test/node/setup.js
+++ client/test/node/setup.js
@@ -19,6 +19,24 @@ global.window.requestIdleCallback = (func) => {
   func();
 };
 
+// JSDOM returns undefined for these properties, so we can mock them out globally here.
+// Every DOM element will now return 100 for its offsetHeight. We gate it with the if
+// statement since when running in watch mode, this definition got called multiple times
+// throwing an error.
+if (!global.window.HTMLElement.prototype.offsetHeight) {
+  Object.defineProperties(global.window.HTMLElement.prototype, {
+    offsetHeight: {
+      get() {
+        return 100;
+      }
+    }
+  });
+}
+
+global.window.performance = {
+  now: () => 'RUNNING_IN_NODE'
+};
+
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     global[property] = document.defaultView[property];
@@ -28,12 +46,3 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
-
-if (jest) {
-  global.window.performance.now = jest.fn().mockReturnValue('RUNNING_IN_NODE');
-  global.scrollTo = jest.fn();
-} else {
-  global.window.performance = {
-    now: () => 'RUNNING_IN_NODE'
-  };
-}
