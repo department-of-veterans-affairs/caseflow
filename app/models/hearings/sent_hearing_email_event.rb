90bde744a555b42e6c4fb42b050d73330df5fe15
Tomas Apodaca <tomas@navapbc.com>
2020-05-18 17:15:14 -0700

    suppress overwriting existing method warning

--- app/models/hearings/sent_hearing_email_event.rb
+++ app/models/hearings/sent_hearing_email_event.rb
@@ -16,6 +16,7 @@ class SentHearingEmailEvent < CaseflowRecord
 
   # Overrides the generated method for compatibility with old data prior to:
   #   https://github.com/department-of-veterans-affairs/caseflow/issues/14147
+  class << self; undef_method :sent_to_appellant; end
   scope :sent_to_appellant, -> { where(recipient_role: [:veteran, :appellant]) }
 
   # Email types are specified in `SendEmail#email_for_recipient`
