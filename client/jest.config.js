17c07117b9a0857afdad6755c99395cb759de2af
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-05-19 18:36:46 -0400

    Converted to jest.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   setupFilesAfterEnv: ['./test/app/jestSetup.js'],
   transformIgnorePatterns: ['node_modules/(?!@department-of-veterans-affairs/caseflow-frontend-toolkit)'],
   collectCoverage: true,
+  // eslint-disable-next-line no-process-env
   coverageDirectory: process.env.JEST_DIR,
   collectCoverageFrom: ['app/**/*.js']
 };
