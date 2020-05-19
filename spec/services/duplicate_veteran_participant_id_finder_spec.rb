e49891964f47420bc6ae32a5f93eeb96081d8731
Peter Karman <peter@truss.works>
2020-05-19 10:47:24 -0500

    refactor test to stub bgs

--- spec/services/duplicate_veteran_participant_id_finder_spec.rb
+++ spec/services/duplicate_veteran_participant_id_finder_spec.rb
@@ -1,21 +1,38 @@
 # frozen_string_literal: true
 
 describe DuplicateVeteranParticipantIDFinder, :postgres do
-  it "returns duplicate participant IDs" do
-    stub_const("BGSService", ExternalApi::BGSService)
-    RequestStore[:current_user] = create(:user)
+  let(:bgs) { ExternalApi::BGSService.new(client: bgs_client) }
+  let(:bgs_client) { double("bgs") }
+  let(:people_service) { double("people") }
+  let(:ssn) { "987654321" }
+  let(:file_number) { "111111111S" }
+  let(:participant_id) { "123456" }
+  let(:second_participant_id) { "765432" }
+  let(:veteran) do
+    create(:veteran, ssn: ssn, participant_id: participant_id, file_number: file_number)
+  end
+  let(:bgs_veteran_record) do
+    {
+      ptcpnt_id: participant_id,
+      file_number: file_number,
+      ssn: ssn
+    }
+  end
 
-    ssn = "987654321"
-    file_number = "111111111S"
-    participant_id = "123456"
-    second_participant_id = "765432"
-    veteran = create(:veteran, ssn: ssn, participant_id: participant_id, file_number: file_number)
+  before do
+    allow(bgs_client).to receive(:people) { people_service }
+    allow(people_service).to receive(:find_by_ssn) do
+     { ptcpnt_id: second_participant_id }
+    end
+    allow(bgs).to receive(:fetch_veteran_info).with(file_number) { bgs_veteran_record }
+    allow(BGSService).to receive(:new) { bgs }
+  end
 
-    allow(Veteran).to receive(:find_or_create_by_file_number).and_return(veteran)
-    allow(veteran).to receive(:fetch_bgs_record).and_return({})
-    allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return(ptcpnt_id: second_participant_id)
+  describe "#call" do
+    subject { described_class.new(veteran: veteran).call }
 
-    pids = DuplicateVeteranParticipantIDFinder.new(veteran: veteran).call
-    expect(pids).to match_array [participant_id, second_participant_id]
+    it "returns duplicate participant IDs" do
+      expect(subject).to match_array [participant_id, second_participant_id]
+    end
   end
 end
