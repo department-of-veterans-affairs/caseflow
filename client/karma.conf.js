a09989d7e7972c26c0b9ec7bbba80f85672927e6
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-05-19 16:41:14 -0400

    Moved setup files to original location

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -6,7 +6,9 @@ const _ = require('lodash');
 const process = require('process');
 const webpackConfig = require('./webpack.config');
 
-const files = ['test/karma/test-index.js'];
+const files = [
+  'test/karma/test-index.js'
+];
 
 const filesPreprocessorObject = _(files).
   map((file) => [file, ['webpack']]).
@@ -16,8 +18,8 @@ const filesPreprocessorObject = _(files).
 module.exports = function(config) {
   config.set({
     browsers: ['Chrome'],
-    frameworks: ['jest', 'snapshot'],
-    reporters: ['jest'],
+    frameworks: ['mocha', 'snapshot', 'mocha-snapshot'],
+    reporters: ['mocha'],
     singleRun: true,
 
     browserConsoleLogOptions: {
@@ -25,7 +27,14 @@ module.exports = function(config) {
       level: ''
     },
 
-    files: ['**/__snapshots__/**/*.md', ...files],
+    files: [
+      '**/__snapshots__/**/*.md',
+      ...files
+    ],
+
+    mochaReporter: {
+      showDiff: true
+    },
 
     junitReporter: {
       outputDir: process.env.KARMA_JUNIT_OUTPUT_DIR,
@@ -33,29 +42,23 @@ module.exports = function(config) {
       useBrowserName: false
     },
 
-    preprocessors: _.merge(
-      {
-        '**/__snapshots__/**/*.md': ['snapshot']
-      },
-      filesPreprocessorObject
-    ),
+    preprocessors: _.merge({
+      '**/__snapshots__/**/*.md': ['snapshot']
+    }, filesPreprocessorObject),
 
     snapshot: {
       update: Boolean(process.env.UPDATE),
       prune: Boolean(process.env.PRUNE)
     },
 
-    webpack: _.merge(
-      {
-        watch: true,
-        externals: {
-          cheerio: 'window',
-          'react/addons': true,
-          'react/lib/ExecutionEnvironment': true,
-          'react/lib/ReactContext': true
-        }
-      },
-      webpackConfig
-    )
+    webpack: _.merge({
+      watch: true,
+      externals: {
+        cheerio: 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+      }
+    }, webpackConfig)
   });
 };
