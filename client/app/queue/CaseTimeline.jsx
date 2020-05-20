236517a8a2879acdb5dfb42aac776169ec9036d1
hschallhorn <hschallhorn@tistatech.com>
2020-05-20 14:37:19 -0400

    Table atty fix

--- client/app/queue/CaseTimeline.jsx
+++ client/app/queue/CaseTimeline.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { caseTimelineTasksForAppeal } from './selectors';
-import COPY from '../../COPY.json';
+import COPY from '../../COPY';
 import TaskRows from './components/TaskRows';
 
 class CaseTimeline extends React.PureComponent {
@@ -12,15 +13,20 @@ class CaseTimeline extends React.PureComponent {
     } = this.props;
 
     return <React.Fragment>
-      {COPY.CASE_TIMELINE_HEADER}
-      <table id="case-timeline-table">
+      <table id="case-timeline-table" summary="layout table">
+        <tr><th className="taskContainerStyling" colSpan="100%">{COPY.CASE_TIMELINE_HEADER}</th></tr>
         <tbody>
-          { <TaskRows appeal={appeal} taskList={tasks} timeline /> }
+          <TaskRows appeal={appeal} taskList={tasks} timeline />
         </tbody>
       </table>
     </React.Fragment>;
   }
 }
+
+CaseTimeline.propTypes = {
+  appeal: PropTypes.object,
+  tasks: PropTypes.array
+};
 
 const mapStateToProps = (state, ownProps) => {
   return {
