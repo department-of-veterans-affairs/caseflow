03157f3aed3247e887b7c48075d3e46d915bba85
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-05-21 14:18:06 -0400

    Changed the default jest test reporters

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   setupFilesAfterEnv: ['./test/app/jestSetup.js'],
   transformIgnorePatterns: ['node_modules/(?!@department-of-veterans-affairs/caseflow-frontend-toolkit)'],
   collectCoverage: true,
-  coverageReporters: ['lcov', 'jest-junit'],
+  reporters: ['json', 'lcov', 'text', 'clover', 'jest-junit'],
   // eslint-disable-next-line no-process-env
   coverageDirectory: process.env.JEST_DIR,
   collectCoverageFrom: ['app/**/*.{js,jsx}']
