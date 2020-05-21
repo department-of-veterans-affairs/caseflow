646c583e8450744859d9db033b80dcc4a29e6593
Peter Karman <peter@truss.works>
2020-05-21 07:44:33 -0500

    add "established" logic

--- app/sql/line-of-business.sql
+++ app/sql/line-of-business.sql
@@ -6,49 +6,69 @@
 select * from request_issues where benefit_type = "vha";
 
 -- Find the related Decision Reviews
+-- NOTE we must determine that the Decisions Reviews have been fully established
 
-select * from appeals where id in (
+select * from appeals
+where established_at is not null
+and id in (
   select decision_review_id
   from request_issues
   where decision_review_type = "Appeal"
     and benefit_type = "vha"
 );
 
-select * from higher_level_reviews where benefit_type = "vha";
-select * from supplemental_claims where benefit_type = "vha";
+select * from higher_level_reviews
+where establishment_processed_at is not null
+  and benefit_type = "vha";
+
+select * from supplemental_claims
+where establishment_processed_at is not null
+  and benefit_type = "vha";
 
 -- With the related Veterans
 
-select * from appeals, veterans where veterans.file_number = appeals.veteran_file_number
+select * from appeals, veterans
+where veterans.file_number = appeals.veteran_file_number
+  and appeals.established_at is not null
   and appeals.id in (
     select decision_review_id from request_issues
     where decision_review_type = "Appeal" and benefit_type = "vha"
   );
 
 select * from higher_level_reviews, veterans
- where higher_level_reviews.veteran_file_number = veterans.file_number
+where higher_level_reviews.veteran_file_number = veterans.file_number
+  and higher_level_reviews.establishment_processed_at is not null
   and higher_level_reviews.benefit_type = "vha";
 
 select * from supplemental_claims, veterans
-  where supplemental_claims.veteran_file_number = veterans.file_number
-   and supplemental_claims.benefit_type = "vha";
+where supplemental_claims.veteran_file_number = veterans.file_number
+  and supplemental_claims.establishment_processed_at is not null
+  and supplemental_claims.benefit_type = "vha";
 
 -- Find the decision issues
 
 select * from decision_issues
- where decision_review_type = "Appeal"
- and decision_review_id in (
-  select decision_review_id
-  from request_issues
-  where decision_review_type = "Appeal"
-    and benefit_type = "vha"
+where decision_review_type = "Appeal"
+  and decision_review_id in (
+    select decision_review_id
+    from request_issues
+    where decision_review_type = "Appeal"
+      and benefit_type = "vha"
 );
 
 select * from decision_issues
- where decision_review_type = "HigherLevelReview"
- and decision_review_id in (select id from higher_level_reviews where benefit_type = "vha");
+where decision_review_type = "HigherLevelReview"
+  and decision_review_id in (
+    select id from higher_level_reviews
+    where establishment_processed_at is not null
+      and benefit_type = "vha"
+);
 
 select * from decision_issues
- where decision_review_type = "SupplementalClaim"
- and decision_review_id in (select id from supplemental_claims where benefit_type = "vha");
+where decision_review_type = "SupplementalClaim"
+  and decision_review_id in (
+    select id from supplemental_claims
+    where establishment_processed_at is not null
+      and benefit_type = "vha"
+);
 
