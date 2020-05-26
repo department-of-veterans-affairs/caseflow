7607a8ed13b5daac89132ff7d8c1780710853bed
yoomlam <yoom@navapbc.com>
2020-05-26 15:46:11 -0500

    add tests for LOB sql queries

--- app/sql/line-of-business.sql
+++ app/sql/line-of-business.sql
@@ -3,11 +3,13 @@
 -- Line of Business is tracked via 'benefit_type' on the 'request_issues' table
 -- In these examples we are using 'vha'
 
+-- @QUERY_NAME: vha_request_issues
 select * from request_issues where benefit_type = 'vha';
 
 -- Find the related Decision Reviews
 -- NOTE we must determine that the Decisions Reviews have been fully established
 
+-- @QUERY_NAME: vha_established_appeals
 select * from appeals
 where established_at is not null
 and id in (
@@ -17,16 +19,19 @@ and id in (
     and benefit_type = 'vha'
 );
 
+-- @QUERY_NAME: vha_established_hlrs
 select * from higher_level_reviews
 where establishment_processed_at is not null
   and benefit_type = 'vha';
 
+-- @QUERY_NAME: vha_established_scs
 select * from supplemental_claims
 where establishment_processed_at is not null
   and benefit_type = 'vha';
 
 -- With the related Veterans
 
+-- @QUERY_NAME: vha_established_appeals_by_vet_filenumber
 select * from appeals, veterans
 where veterans.file_number = appeals.veteran_file_number
   and appeals.established_at is not null
@@ -35,11 +40,13 @@ where veterans.file_number = appeals.veteran_file_number
     where decision_review_type = 'Appeal' and benefit_type = 'vha'
   );
 
+-- @QUERY_NAME: vha_established_hlrs_by_vet_filenumber
 select * from higher_level_reviews, veterans
 where higher_level_reviews.veteran_file_number = veterans.file_number
   and higher_level_reviews.establishment_processed_at is not null
   and higher_level_reviews.benefit_type = 'vha';
 
+-- @QUERY_NAME: vha_established_scs_by_vet_filenumber
 select * from supplemental_claims, veterans
 where supplemental_claims.veteran_file_number = veterans.file_number
   and supplemental_claims.establishment_processed_at is not null
@@ -47,6 +54,7 @@ where supplemental_claims.veteran_file_number = veterans.file_number
 
 -- Find the decision issues
 
+-- @QUERY_NAME: vha_appeal_decisions
 select * from decision_issues
 where decision_review_type = 'Appeal'
   and decision_review_id in (
@@ -56,6 +64,7 @@ where decision_review_type = 'Appeal'
       and benefit_type = 'vha'
 );
 
+-- @QUERY_NAME: vha_established_hlr_decisions
 select * from decision_issues
 where decision_review_type = 'HigherLevelReview'
   and decision_review_id in (
@@ -64,6 +73,7 @@ where decision_review_type = 'HigherLevelReview'
       and benefit_type = 'vha'
 );
 
+-- @QUERY_NAME: vha_established_sc_decisions
 select * from decision_issues
 where decision_review_type = 'SupplementalClaim'
   and decision_review_id in (
--- spec/support/sql_helpers.rb
+++ spec/support/sql_helpers.rb
@@ -10,6 +10,20 @@ module SQLHelpers
     File.read(sql_file)
   end
 
+  def to_sql_query_hash(statements)
+    queries_hash = {}
+    statements.each do |statement|
+      query_name = statement.scan(/@QUERY_NAME: *(.*)$/)&.last&.first
+      queries_hash[query_name] = statement if query_name
+    end
+    queries_hash
+  end
+
+  def read_sql_as_hash(file_name)
+    sql_statements = read_sql(file_name).split(";")
+    to_sql_query_hash(sql_statements)
+  end
+
   def execute_sql(file_name)
     sql = read_sql(file_name)
     result = ApplicationRecord.connection.exec_query(sql)
