49a64e852e2b546a3a19924a3420227468375dd3
Peter Karman <peter@truss.works>
2020-06-02 15:39:08 -0500

    query for any open child by type

---
+++ app/queries/etl/unknown_status_with_open_child_task_query.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ETL::UnknownStatusWithOpenChildTaskQuery
+  def initialize(child_task_type)
+    @child_task_type = child_task_type
+  end
+
+  def call
+    appeals_with_open_child_task
+  end
+
+  private
+
+  attr_reader :child_task_type
+
+  def appeals_with_open_child_task
+    ETL::Appeal.where(status: "UNKNOWN").where(appeal_id: open_child_tasks_of_type)
+  end
+
+  def open_child_tasks_of_type
+    ETL::Task.select(:appeal_id)
+      .where(appeal_type: "Appeal", task_type: child_task_type, task_status: Task.open_statuses)
+  end
+end
