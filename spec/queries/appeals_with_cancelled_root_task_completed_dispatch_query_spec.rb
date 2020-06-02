130023bb3703f8c1b9d332f453b10e48a367910d
Peter Karman <peter@truss.works>
2020-06-02 13:26:02 -0500

    review suggestions

--- spec/queries/appeals_with_cancelled_root_task_completed_dispatch_query_spec.rb
+++ spec/queries/appeals_with_cancelled_root_task_completed_dispatch_query_spec.rb
@@ -3,7 +3,7 @@
 describe AppealsWithCancelledRootTaskCompletedDispatchQuery, :postgres do
   let!(:dispatched_appeal_with_cancelled_root_task) do
     appeal = create(:appeal, :with_post_intake_tasks)
-    create(:bva_dispatch_task, :completed, appeal: appeal)
+    create(:bva_dispatch_task, :completed, appeal: appeal, parent: appeal.root_task)
     appeal.root_task.cancelled!
     appeal
   end
@@ -11,7 +11,7 @@ describe AppealsWithCancelledRootTaskCompletedDispatchQuery, :postgres do
   describe "#call" do
     subject { described_class.new.call }
 
-    it "returns array of appeals" do
+    it "returns array of matching appeals" do
       expect(subject).to match_array([dispatched_appeal_with_cancelled_root_task])
     end
   end
