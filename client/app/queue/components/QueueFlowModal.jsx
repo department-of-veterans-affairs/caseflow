5243adb128c3acdc7341cc4d75b38b9eda9b98c2
hschallhorn <hschallhorn@tistatech.com>
2020-06-02 10:57:56 -0400

    Clarifying comments!

--- client/app/queue/components/QueueFlowModal.jsx
+++ client/app/queue/components/QueueFlowModal.jsx
@@ -40,6 +40,8 @@ class QueueFlowModal extends React.PureComponent {
     this.props.
       submit().
       then(() => {
+        // Not every component that uses queue flow modal sets saveSuccessful, so we may have a null here. Until every
+        // component sets saveSuccessful on success or failure, this cannot be updated to saveSuccessful === true
         if (this.props.saveSuccessful !== false) {
           this.closeHandler();
         }
