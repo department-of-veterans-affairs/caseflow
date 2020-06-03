3c0cd24309349cf66c75c2c0c8366656dae264a0
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-06-03 14:52:32 -0400

    use reject_if to ignore empty attributes

--- spec/models/tasks/schedule_hearing_task_spec.rb
+++ spec/models/tasks/schedule_hearing_task_spec.rb
@@ -89,10 +89,7 @@ describe ScheduleHearingTask, :all_dbs do
             description: "Update",
             values: {
               "hearing_day_id": hearing_day.id,
-              "scheduled_time_string": "09:00",
-              "hearing_location": {
-                facility_id: "vba_301"
-              }
+              "scheduled_time_string": "09:00"
             }
           }
         }
