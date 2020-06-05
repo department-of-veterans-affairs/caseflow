c698d047bb0007a06636966bc9317c15fd66fd3f
JC Quirin <jcq@jcq.lan>
2020-06-05 16:22:05 -0700

    added some docgen comments to Button propTypes

--- client/app/components/Button.jsx
+++ client/app/components/Button.jsx
@@ -38,22 +38,23 @@ export default class Button extends React.Component {
 
       children = loadingText || 'Loading...';
 
-      return <span>
-        <button
-          id={`${id || `${type}-${name.replace(/\s/g, '-')}`}-loading`}
-          className={`${propClassNames.join(' ')} cf-${app} cf-loading`}
-          type={type}
-          disabled
-          aria-label={ariaLabel}>
-          <span className="cf-loading-icon-container">
-            <span className="cf-loading-icon-front">
-              <span className="cf-loading-icon-back">
-                {children}
+      return (
+        <span>
+          <button
+            id={`${id || `${type}-${name.replace(/\s/g, '-')}`}-loading`}
+            className={`${propClassNames.join(' ')} cf-${app} cf-loading`}
+            type={type}
+            disabled
+            aria-label={ariaLabel}
+          >
+            <span className="cf-loading-icon-container">
+              <span className="cf-loading-icon-front">
+                <span className="cf-loading-icon-back">{children}</span>
               </span>
             </span>
-          </span>
-        </button>
-      </span>;
+          </button>
+        </span>
+      );
     };
 
     children = children || name;
@@ -72,17 +73,20 @@ export default class Button extends React.Component {
       'usa-button-red': redStyling
     });
 
-    const button = <button
-      id={id || (name && `${type}-${name.replace(/\s/g, '-')}`)}
-      className={buttonClasses}
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-      title={title}
-      aria-label={ariaLabel}
-      {...styling}>
-      {children}
-    </button>;
+    const button = (
+      <button
+        id={id || (name && `${type}-${name.replace(/\s/g, '-')}`)}
+        className={buttonClasses}
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+        title={title}
+        aria-label={ariaLabel}
+        {...styling}
+      >
+        {children}
+      </button>
+    );
 
     /**
      * If we having a loading indicator, then we'll wrap the <button> in a <span>.
@@ -98,16 +102,21 @@ export default class Button extends React.Component {
       return button;
     }
 
-    return <span>
-      { button }
-      { loading && <LoadingIndicator /> }
-    </span>;
+    return (
+      <span>
+        {button}
+        {loading && <LoadingIndicator />}
+      </span>
+    );
   }
 }
 
 Button.defaultProps = {
   classNames: ['cf-submit'],
-  type: 'button'
+  type: 'button',
+  loading: false,
+  willNeverBeLoading: false,
+  linkStyling: false
 };
 
 Button.propTypes = {
@@ -116,7 +125,15 @@ Button.propTypes = {
   classNames: PropTypes.arrayOf(PropTypes.string),
   disabled: PropTypes.bool,
   id: PropTypes.string,
+
+  /**
+   * Styles the button like a link when true
+   */
   linkStyling: PropTypes.bool,
+
+  /**
+   * Boolean that disables button and shows a loading indicator
+   */
   loading: (props, propName) => {
     const loading = props[propName];
 
@@ -133,11 +150,12 @@ Button.propTypes = {
     }
   },
   willNeverBeLoading: PropTypes.bool,
-  name: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.node
-  ]),
+  name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   onClick: PropTypes.func,
-  type: PropTypes.string,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+
+  /**
+   * Accepts an object that is spread out as props on `button` element
+   */
   styling: PropTypes.object
 };
