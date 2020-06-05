cdab081e7b81a7f11e40eb1b49cea19a0e1cee67
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-06-05 15:39:41 -0400

    Added story and tests for TabWindow component

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   setupFilesAfterEnv: ['./test/app/jestSetup.js'],
   transformIgnorePatterns: ['node_modules/(?!@department-of-veterans-affairs/caseflow-frontend-toolkit)'],
   collectCoverage: true,
+  // eslint-disable-next-line no-process-env
   reporters: process.env.TEST_REPORTER ? [process.env.TEST_REPORTER] : ['default', 'jest-junit'],
   // eslint-disable-next-line no-process-env
   coverageDirectory: process.env.JEST_DIR,
