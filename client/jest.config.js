b396760d82f7364f77c709502be0924e6cb12030
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-06-10 11:16:02 -0400

    Updated based on lint errors

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-env */
 module.exports = {
   moduleNameMapper: {
     '^app/(.*)$': '<rootDir>/app/$1',
@@ -6,13 +7,12 @@ module.exports = {
   },
   setupFilesAfterEnv: ['./test/app/jestSetup.js'],
   transformIgnorePatterns: ['node_modules/(?!@department-of-veterans-affairs/caseflow-frontend-toolkit)'],
-  // eslint-disable-next-line no-process-env
   // eslint-disable-next-line no-undefined
   collectCoverage: process.env.TEST_REPORTER !== undefined,
-  // eslint-disable-next-line no-process-env
   reporters: process.env.TEST_REPORTER ? [process.env.TEST_REPORTER] : ['default', 'jest-junit'],
-  // eslint-disable-next-line no-process-env
   coverageDirectory: process.env.JEST_DIR,
   collectCoverageFrom: ['app/**/*.{js,jsx}'],
   snapshotSerializers: ['enzyme-to-json/serializer']
 };
+
+/* eslint-enable no-process-env */
