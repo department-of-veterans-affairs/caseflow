07d53a624deddb79f36c89ffd4c0cff269960579
Stephen Halliburton <stephenhalliburton@TST-C02VKASTHTD5.fios-router.home>
2020-06-11 10:29:48 -0400

    Fixed merge conflicts

--- client/jest.config.js
+++ client/jest.config.js
@@ -7,15 +7,9 @@ module.exports = {
   },
   setupFilesAfterEnv: ['./test/app/jestSetup.js'],
   transformIgnorePatterns: ['node_modules/(?!@department-of-veterans-affairs/caseflow-frontend-toolkit)'],
-<<<<<<< HEAD
-  collectCoverage: false,
-  reporters: ['default', 'jest-junit'],
-  // eslint-disable-next-line no-process-env
-=======
   // eslint-disable-next-line no-undefined
   collectCoverage: process.env.TEST_REPORTER !== undefined,
   reporters: process.env.TEST_REPORTER ? [process.env.TEST_REPORTER] : ['default', 'jest-junit'],
->>>>>>> 14b822db13a7b7ccee503c98a27b9920ad6808eb
   coverageDirectory: process.env.JEST_DIR,
   collectCoverageFrom: ['app/**/*.{js,jsx}'],
   snapshotSerializers: ['enzyme-to-json/serializer']
