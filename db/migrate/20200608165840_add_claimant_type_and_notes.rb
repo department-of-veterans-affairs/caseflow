707f6bb17f6547ab01ee5b9968e67505fe004e20
Sally Maki <sallymaki@gmail.com>
2020-06-11 12:43:59 -0700

    Update DB comment

--- db/migrate/20200608165840_add_claimant_type_and_notes.rb
+++ db/migrate/20200608165840_add_claimant_type_and_notes.rb
@@ -1,6 +1,6 @@
 class AddClaimantTypeAndNotes < ActiveRecord::Migration[5.2]
   def up
-    add_column :claimants, :type, :string, comment: "The class name for the single table inheritance type of Claimant, for example Veteran, Dependent, Attorney, or Other."
+    add_column :claimants, :type, :string, comment: "The class name for the single table inheritance type of Claimant, for example VeteranClaimant, DependentClaimant, AttorneyClaimant, or OtherClaimant."
     change_column_default :claimants, :type, "Claimant"
   end
 
