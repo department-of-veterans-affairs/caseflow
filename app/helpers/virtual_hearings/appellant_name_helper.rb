2e7c60d2c123199818f73cba7f06b707c04f28dc
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-06-15 14:59:35 -0400

    use safe assign

--- app/helpers/virtual_hearings/appellant_name_helper.rb
+++ app/helpers/virtual_hearings/appellant_name_helper.rb
@@ -8,6 +8,6 @@ module VirtualHearings::AppellantNameHelper
   def formatted_appellant_name(appeal)
     return appeal.appellant_fullname_readable || "the appellant" if appeal.appellant_is_not_veteran
 
-    appeal.veteran&.name.formatted(:readable_fi_last_formatted) || "the veteran"
+    appeal.veteran&.name&.formatted(:readable_fi_last_formatted) || "the veteran"
   end
 end
