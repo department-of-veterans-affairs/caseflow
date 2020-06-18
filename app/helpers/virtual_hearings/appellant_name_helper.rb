3d80b915817eaf9a227800e6122011edd91dccb9
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-06-18 15:28:43 -0400

    account for article

--- app/helpers/virtual_hearings/appellant_name_helper.rb
+++ app/helpers/virtual_hearings/appellant_name_helper.rb
@@ -11,9 +11,16 @@ module VirtualHearings::AppellantNameHelper
     appeal&.veteran&.name&.formatted(:readable_fi_last_formatted) || "the veteran"
   end
 
-  def appellant_or_veteran(appeal)
-    return "Appellant" if appeal.appellant_is_not_veteran
+  def appellant_or_veteran(appeal, include_article=false)
+    appellant_or_veteran = appeal.appellant_is_not_veteran ? "Appellant" : "Veteran"
+    return appellant_or_veteran if !include_article
 
-    "Veteran"
+    indefinite_articlerize(appellant_or_veteran)
+  end
+
+  # prepends a or an to param
+  # taken from https://stackoverflow.com/questions/5381738/rails-article-helper-a-or-an
+  def indefinite_articlerize(params_word)
+    %w(a e i o u).include?(params_word[0].downcase) ? "an #{params_word}" : "a #{params_word}"
   end
 end
