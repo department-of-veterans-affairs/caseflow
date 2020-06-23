598c4b86299dca50cbe682de0f8e51acdae453d3
JC Quirin <jcq@jcq.lan>
2020-06-22 18:26:06 -0700

    updated async fn in `AddClaimantModal` to match new syntax for `react-select`

--- client/app/intake/components/AddClaimantModal.jsx
+++ client/app/intake/components/AddClaimantModal.jsx
@@ -2,22 +2,29 @@ import React, { useState, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { debounce } from 'lodash';
 import Modal from '../../components/Modal';
-import { ADD_CLAIMANT_MODAL_TITLE, ADD_CLAIMANT_MODAL_DESCRIPTION } from '../../../COPY';
+import {
+  ADD_CLAIMANT_MODAL_TITLE,
+  ADD_CLAIMANT_MODAL_DESCRIPTION
+} from '../../../COPY';
 import ReactMarkdown from 'react-markdown';
 import SearchableDropdown from '../../components/SearchableDropdown';
 import ApiUtil from '../../util/ApiUtil';
 
-const relationshipOpts = [{ label: 'Attorney (previously or currently)', value: 'attorney' }];
+const relationshipOpts = [
+  { label: 'Attorney (previously or currently)', value: 'attorney' }
+];
 
 const fetchAttorneys = async (search = '') => {
-  const res = await ApiUtil.get('/intake/attorneys', { query: { query: search } });
+  const res = await ApiUtil.get('/intake/attorneys', {
+    query: { query: search }
+  });
 
   return res?.body;
 };
 const getClaimantOpts = async (search = '', asyncFn) => {
   // Enforce minimum search length (we'll simply return empty array rather than throw error)
   if (search.length < 3) {
-    return { options: [] };
+    return [];
   }
 
   const res = await asyncFn(search);
@@ -26,12 +33,16 @@ const getClaimantOpts = async (search = '', asyncFn) => {
     value: item.participant_id
   }));
 
-  return { options };
+  return options;
 };
 // We'll show all items returned from the backend instead of using default substring matching
-const filterOptions = (options) => options;
+const filterOption = () => true;
 
-export const AddClaimantModal = ({ onCancel, onSubmit, onSearch = fetchAttorneys }) => {
+export const AddClaimantModal = ({
+  onCancel,
+  onSubmit,
+  onSearch = fetchAttorneys
+}) => {
   const [claimant, setClaimant] = useState(null);
   const [relationship, setRelationship] = useState(relationshipOpts[0]);
   const isInvalid = useMemo(() => !claimant, [claimant]);
@@ -40,7 +51,7 @@ export const AddClaimantModal = ({ onCancel, onSubmit, onSearch = fetchAttorneys
 
   const asyncFn = useCallback(
     debounce((search, callback) => {
-      getClaimantOpts(search, onSearch).then((res) => callback(null, res));
+      getClaimantOpts(search, onSearch).then((res) => callback(res));
     }, 250),
     [onSearch]
   );
@@ -54,13 +65,19 @@ export const AddClaimantModal = ({ onCancel, onSubmit, onSearch = fetchAttorneys
     {
       classNames: ['usa-button', 'usa-button-primary'],
       name: 'Add this claimant',
-      onClick: () => onSubmit({ name: claimant.label, participantId: claimant.value }),
+      onClick: () =>
+        onSubmit({ name: claimant.label, participantId: claimant.value }),
       disabled: isInvalid
     }
   ];
 
   return (
-    <Modal title={ADD_CLAIMANT_MODAL_TITLE} buttons={buttons} closeHandler={onCancel} id="add_claimant_modal">
+    <Modal
+      title={ADD_CLAIMANT_MODAL_TITLE}
+      buttons={buttons}
+      closeHandler={onCancel}
+      id="add_claimant_modal"
+    >
       <div>
         <ReactMarkdown source={ADD_CLAIMANT_MODAL_DESCRIPTION} />
       </div>
@@ -77,9 +94,9 @@ export const AddClaimantModal = ({ onCancel, onSubmit, onSearch = fetchAttorneys
         label="Claimant's name"
         onChange={handleChangeClaimant}
         value={claimant}
-        filterOptions={filterOptions}
+        filterOption={filterOption}
         async={asyncFn}
-        options={[]}
+        defaultOptions
         debounce={250}
       />
     </Modal>
