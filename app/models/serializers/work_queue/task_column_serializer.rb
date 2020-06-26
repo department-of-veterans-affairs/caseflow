d69ef158980ceb0ee620564b8f332601ba098a68
hschallhorn <hschallhorn@tistatech.com>
2020-06-26 13:32:59 -0400

    Lint and test

--- app/models/serializers/work_queue/task_column_serializer.rb
+++ app/models/serializers/work_queue/task_column_serializer.rb
@@ -119,7 +119,10 @@ class WorkQueue::TaskColumnSerializer
   end
 
   attribute :status do |object, params|
-    columns = [Constants.QUEUE_CONFIG.COLUMNS.DAYS_ON_HOLD.name]
+    columns = [
+      Constants.QUEUE_CONFIG.COLUMNS.DAYS_ON_HOLD.name,
+      Constants.QUEUE_CONFIG.COLUMNS.CASE_DETAILS_LINK.name
+    ]
 
     if serialize_attribute?(params, columns)
       object.status
