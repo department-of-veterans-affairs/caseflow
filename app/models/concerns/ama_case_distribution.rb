1405259e8447bd8d813b447ffb0943c1aba6a1c2
hschallhorn <hschallhorn@tistatech.com>
2020-06-30 09:30:35 -0400

    Revert changes

--- app/models/concerns/ama_case_distribution.rb
+++ app/models/concerns/ama_case_distribution.rb
@@ -28,6 +28,7 @@ module AmaCaseDistribution
     distribute_appeals(:hearing, @rem, priority: true, genpop: "not_genpop")
 
     # Distribute nonpriority appeals that are tied to judges.
+    # Legacy docket appeals that are tied to judges are only distributed when they are within the docket range.
     distribute_appeals(:legacy, @rem, priority: false, genpop: "not_genpop", range: legacy_docket_range)
     distribute_appeals(:hearing, @rem, priority: false, genpop: "not_genpop")
 
