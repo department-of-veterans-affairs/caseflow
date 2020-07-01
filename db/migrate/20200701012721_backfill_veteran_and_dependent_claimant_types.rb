5c6b445863dfe73e536ebc392d57e1b8fc2206df
Sally Maki <sallymaki@gmail.com>
2020-06-30 20:24:42 -0700

    Use sql for claimants without payee code

--- db/migrate/20200701012721_backfill_veteran_and_dependent_claimant_types.rb
+++ db/migrate/20200701012721_backfill_veteran_and_dependent_claimant_types.rb
@@ -4,7 +4,6 @@ class BackfillVeteranAndDependentClaimantTypes < ActiveRecord::Migration[5.2]
   def up
     veterans_with_payee_code = Claimant.where(payee_code: "00", type: "Claimant")
     dependents_with_payee_code = Claimant.where.not(payee_code: ["00", nil]).where(type: "Claimant")
-    claimants_without_payee_code = Claimant.where(payee_code: nil, type: "Claimant")
 
     veterans_with_payee_code.unscoped.in_batches do |relation|
       relation.update_all type: "VeteranClaimant"
@@ -16,14 +15,64 @@ class BackfillVeteranAndDependentClaimantTypes < ActiveRecord::Migration[5.2]
       sleep(0.1)
     end
 
-    claimants_without_payee_code.each do |claimant|
-      next unless claimant.decision_review
+    safety_assured do
+    # Claimants might not have a payee code for appeals or non-comp decision reviews
 
-      if claimant.decision_review.veteran_is_not_claimant
-        claimant.update!(type: "DependentClaimant")
-      else
-        claimant.update!(type: "VeteranClaimant")
-      end
+    # Handle veteran claimants without payee code
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'VeteranClaimant'
+          FROM appeals
+          WHERE
+            claimants.decision_review_id = appeals.id AND claimants.decision_review_type = 'Appeal'
+            AND (appeals.veteran_is_not_claimant IS NULL OR appeals.veteran_is_not_claimant = FALSE);
+      SQL
+
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'VeteranClaimant'
+          FROM higher_level_reviews
+          WHERE
+            claimants.decision_review_id = higher_level_reviews.id AND claimants.decision_review_type = 'HigherLevelReview'
+            AND (higher_level_reviews.veteran_is_not_claimant IS NULL OR higher_level_reviews.veteran_is_not_claimant = FALSE);
+      SQL
+
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'VeteranClaimant'
+          FROM supplemental_claims
+          WHERE
+            claimants.decision_review_id = supplemental_claims.id AND claimants.decision_review_type = 'SupplementalClaim'
+            AND (supplemental_claims.veteran_is_not_claimant IS NULL OR supplemental_claims.veteran_is_not_claimant = FALSE);
+      SQL
+
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'DependentClaimant'
+          FROM appeals
+          WHERE
+            claimants.decision_review_id = appeals.id AND claimants.decision_review_type = 'Appeal'
+            AND appeals.veteran_is_not_claimant = TRUE;
+      SQL
+
+      # Handle dependent claimants without payee code
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'DependentClaimant'
+          FROM higher_level_reviews
+          WHERE
+            claimants.decision_review_id = higher_level_reviews.id AND claimants.decision_review_type = 'HigherLevelReview'
+            AND higher_level_reviews.veteran_is_not_claimant = TRUE;
+      SQL
+
+      execute <<-SQL
+        UPDATE claimants
+          SET type = 'DependentClaimant'
+          FROM supplemental_claims
+          WHERE
+            claimants.decision_review_id = supplemental_claims.id AND claimants.decision_review_type = 'SupplementalClaim'
+            AND supplemental_claims.veteran_is_not_claimant = TRUE;
+      SQL
     end
   end
 
