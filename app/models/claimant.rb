ee11c3051894ea33e702570a21068fa98429389a
JC Quirin <jcq@jcq.lan>
2020-07-10 07:57:58 -0700

    updated logic for bypassing Claimant validation for OtherClaimant

--- app/models/claimant.rb
+++ app/models/claimant.rb
@@ -9,9 +9,14 @@ class Claimant < CaseflowRecord
   belongs_to :decision_review, polymorphic: true
   belongs_to :person, primary_key: :participant_id, foreign_key: :participant_id
 
-  validate do |claimant|
-    ClaimantValidator.new(claimant).validate unless proc { |klass| klass.is_a? OtherClaimant }
+  validate  do |claimant|
+    if(claimant.is_a? OtherClaimant)
+      true
+    else
+      ClaimantValidator.new(claimant).validate
+    end
   end
+
   validates :participant_id,
             uniqueness: { scope: [:decision_review_id, :decision_review_type],
                           on: :create }
