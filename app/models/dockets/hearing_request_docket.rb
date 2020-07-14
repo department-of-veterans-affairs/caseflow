3bd0698e339b4393e5cd67236d5be943045c5a08
hschallhorn <hschallhorn@tistatech.com>
2020-07-14 12:09:10 -0400

    Fix bug on wrong number of hearing cases distributed

--- app/models/dockets/hearing_request_docket.rb
+++ app/models/dockets/hearing_request_docket.rb
@@ -20,6 +20,14 @@ class HearingRequestDocket < Docket
       base_relation: base_relation, genpop: genpop, judge: distribution.judge
     ).call
 
+    # genpop 'any' returns 2 arrays of the limited base relation. This means if we only request 2 cases, appeals is a
+    # 2x2 array containing 4 cases overall and we will end up distributing 4 cases rather than 2. Instead, reinstate the
+    # limit here by filtering out the newest cases
+    if genpop == "any"
+      appeals_to_reject = appeals.flatten.sort_by(&:ready_for_distribution_at).drop(limit)
+      appeals = [appeals.first - appeals_to_reject, appeals.last - appeals_to_reject]
+    end
+
     HearingRequestCaseDistributor.new(
       appeals: appeals, genpop: genpop, distribution: distribution, priority: priority
     ).call
