63e75832af648e1b37c4d2dde4fff29fe2f727da
Sandra Jones <sjones@tistatech.com>
2020-07-20 18:29:36 -0400

    fixes failing tests

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -36,8 +36,7 @@ class Rating
       unpromulgated = unsorted.select { |rating| rating.promulgation_date.nil? }
       sorted = unsorted.reject do |rating|
         rating.promulgation_date.nil? || rating.promulgation_date < start_date
-      end
-      sorted.sort_by(&:promulgation_date).reverse
+      end.sort_by(&:promulgation_date).reverse
 
       unpromulgated + sorted
     end
