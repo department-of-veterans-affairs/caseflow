a4d5dfa9d59af5459f551780e9510b632bca4250
JC Quirin <jcq@jcq.lan>
2020-07-20 14:23:25 -0700

    updated stories for TextareaField

--- client/app/components/TextareaField.jsx
+++ client/app/components/TextareaField.jsx
@@ -5,10 +5,15 @@ import pluralize from 'pluralize';
 
 import { FormLabel } from './FormLabel';
 
+/**
+ * A text area allows multiple lines of text so that users can enter detailed and descriptive requested information.
+ * This freeform field allows users to write as much as they need to.
+ * When the message is longer than the length of the box, a scroll bar will appear on the side.
+ */
 export default class TextareaField extends React.Component {
   onChange = (event) => {
     this.props.onChange(event.target.value);
-  }
+  };
 
   render() {
     const {
@@ -27,53 +32,69 @@ export default class TextareaField extends React.Component {
       disabled,
       labelStyling,
       placeholder,
-      optional
+      optional,
     } = this.props;
 
-    const className = 'cf-form-textarea' +
-          `${errorMessage ? ' usa-input-error' : ''}`;
+    const className =
+      'cf-form-textarea' + `${errorMessage ? ' usa-input-error' : ''}`;
 
     // There is only a value for this variable if maxlength and value props are not null.
     // Otherwise characterLimitCount will be null also.
-    const characterLimitCount = (Boolean(maxlength) && Boolean(value)) ? (maxlength - value.length) : null;
+    const characterLimitCount =
+      Boolean(maxlength) && Boolean(value) ? maxlength - value.length : null;
 
-    const labelContents = <FormLabel label={label} name={name} required={required} optional={optional} />;
+    const labelContents = (
+      <FormLabel
+        label={label}
+        name={name}
+        required={required}
+        optional={optional}
+      />
+    );
 
     // hideLabel still leaves the label element in the DOM (for a11y purposes)
     // but makes it invisible to any screens
-    return <div className={className} {...styling}>
-      <label {...labelStyling}
-        className={classNamesFn({ 'sr-only': hideLabel }, 'question-label')}
-        htmlFor={id || name}>
-        {
-          strongLabel ?
-            <strong>{labelContents}</strong> :
-            labelContents
-        }
-      </label>
-      {errorMessage && <span className="usa-input-error-message">{errorMessage}</span>}
-      <textarea {...textAreaStyling}
-        name={name}
-        id={id || name}
-        onChange={this.onChange}
-        onKeyDown={this.props.onKeyDown}
-        type={type}
-        value={value}
-        maxLength={maxlength}
-        disabled={disabled}
-        placeholder={placeholder}
-      />
-      { characterLimitCount !== null && <p>
-        <i>{characterLimitCount} {pluralize('character', characterLimitCount)} left</i>
-      </p> }
-    </div>;
+    return (
+      <div className={className} {...styling}>
+        <label
+          {...labelStyling}
+          className={classNamesFn({ 'sr-only': hideLabel }, 'question-label')}
+          htmlFor={id || name}
+        >
+          {strongLabel ? <strong>{labelContents}</strong> : labelContents}
+        </label>
+        {errorMessage && (
+          <span className="usa-input-error-message">{errorMessage}</span>
+        )}
+        <textarea
+          {...textAreaStyling}
+          name={name}
+          id={id || name}
+          onChange={this.onChange}
+          onKeyDown={this.props.onKeyDown}
+          type={type}
+          value={value}
+          maxLength={maxlength}
+          disabled={disabled}
+          placeholder={placeholder}
+        />
+        {characterLimitCount !== null && (
+          <p>
+            <i>
+              {characterLimitCount}{' '}
+              {pluralize('character', characterLimitCount)} left
+            </i>
+          </p>
+        )}
+      </div>
+    );
   }
 }
 
 TextareaField.defaultProps = {
   disabled: false,
   optional: false,
-  required: false
+  required: false,
 };
 
 TextareaField.propTypes = {
@@ -94,5 +115,5 @@ TextareaField.propTypes = {
   optional: PropTypes.bool,
   required: PropTypes.bool,
   labelStyling: PropTypes.object,
-  textAreaStyling: PropTypes.object
+  textAreaStyling: PropTypes.object,
 };
---
+++ client/app/components/TextareaField.stories.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+// import { useArgs } from '@storybook/client-api';
+
+import TextareaField from './TextareaField';
+
+export default {
+  title: 'Commons/Components/Form Fields/TextareaField',
+  component: TextareaField,
+  parameters: {
+    controls: { expanded: true },
+  },
+
+  args: {
+    name: 'textArea',
+    label: 'Enter some text',
+  },
+  argTypes: {},
+};
+
+const Template = (args) => {
+  //   const [_args, updateArgs] = useArgs();
+  //   const handleChange = (value) => updateArgs({ value });
+  const [value, setValue] = useState('');
+  const handleChange = (val) => setValue(val);
+
+  return <TextareaField {...args} onChange={handleChange} value={value} />;
+};
+
+export const Basic = Template.bind({});
+
+export const CharacterLimit = Template.bind({});
+CharacterLimit.args = {
+  maxlength: 100,
+  label: 'Enter your text here (with character limit)'
+};
