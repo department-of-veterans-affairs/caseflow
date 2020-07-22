9fcc26d4ecfea494dd9840b16d76b19f31cf2b02
Ferris Tseng <ferris@navapbc.com>
2020-07-22 16:13:52 -0400

    only show the default appellant tz and representative tz, don't change the underlying value if null

--- client/app/hearings/components/details/VirtualHearingForm.jsx
+++ client/app/hearings/components/details/VirtualHearingForm.jsx
@@ -24,17 +24,19 @@ export const VirtualHearingForm = (
   const appellantTitle = getAppellantTitleForHearing(hearing);
   const user = useContext(HearingsUserContext);
 
-  // Prefill appellant/veteran email address and representative email on mount.
   useEffect(() => {
     // Try to use the existing timezones if present
     const { appellantTz, representativeTz } = (virtualHearing || {});
 
     // Set the  timezone if not already set
     update('virtualHearing', {
-      [!representativeTz && 'representativeTz']: representativeTz || hearing?.representativeTz,
-      [!appellantTz && 'appellantTz']: appellantTz || hearing?.appellantTz,
+      [!representativeTz && 'representativeTz']: representativeTz,
+      [!appellantTz && 'appellantTz']: appellantTz,
     });
   }, []);
+
+  const defaultAppellantTz = hearing?.appellantTz;
+  const defaultRepresentativeTz = hearing?.representativeTz;
 
   return (
     <ContentSection
@@ -55,7 +57,7 @@ export const VirtualHearingForm = (
             {hearing.readableRequestType !== 'Video' && <div className="usa-width-one-third">
               <Timezone
                 required
-                value={virtualHearing?.appellantTz}
+                value={virtualHearing?.appellantTz || defaultAppellantTz}
                 onChange={(appellantTz) => update('virtualHearing', { appellantTz })}
                 readOnly={readOnlyEmails}
                 time={hearing.scheduledTimeString}
@@ -80,7 +82,7 @@ export const VirtualHearingForm = (
           <div className={classNames('usa-grid', { [marginTop(30)]: true })}>
             {hearing.readableRequestType !== 'Video' && <div className="usa-width-one-third">
               <Timezone
-                value={virtualHearing?.representativeTz}
+                value={virtualHearing?.representativeTz || defaultRepresentativeTz}
                 onChange={(representativeTz) => update('virtualHearing', { representativeTz })}
                 readOnly={readOnlyEmails || !virtualHearing?.representativeEmail}
                 time={hearing.scheduledTimeString}
