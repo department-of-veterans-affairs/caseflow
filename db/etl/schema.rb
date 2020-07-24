30c9a35265855c47a8aa76e9397d3c529613e5be
Kat Tipton <kat@navapbc.com>
2020-07-24 11:46:30 -0400

    Update schema.rb

--- db/etl/schema.rb
+++ db/etl/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+#futz
+
 ActiveRecord::Schema.define(version: 2020_07_15_110603) do
 
   # These are extensions that must be enabled in order to support this database
