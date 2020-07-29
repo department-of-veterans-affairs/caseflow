department-of-veterans-affairs/caseflow

commit 0c8b2d45af375620ff82a37c5bdde79240dd9a92

Update Intake Review Endpoint to Support Different Claimant Types (#14648)
Connects #14505

### Description
This updates the `IntakesController#review` endpoint to support different claimant types. It also updates the controller schema for said endpoint.

### Acceptance Criteria
- [ ] Code compiles correctly

client/app/intake/components/SelectClaimant.jsx
@@ -8,6 +8,7 @@ import {
   DECEASED_PAYEE_CODES,
   LIVING_PAYEE_CODES
 } from '../constants';
+import { convertStringToBoolean } from '../util';
 import COPY from '../../../COPY';
 import { useSelector } from 'react-redux';
 import Button from '../../components/Button';
@@ -82,11 +83,18 @@ export const SelectClaimant = (props) => {
     veteranIsNotClaimant,
     attorneyFees
   ]);
+  const handleVeteranIsNotClaimant = (value) => {
+    const boolValue = convertStringToBoolean(value);
+
+    setVeteranIsNotClaimant(boolValue);
+    setClaimant({ claimant: null, claimantType: (boolValue ? 'dependent' : 'veteran') });
+  };
   const handleRemove = () => {
     setNewClaimant(null);
-    setClaimant(null);
+    setClaimant({ claimant: null, claimantType: 'dependent', claimantNotes: null });
   };
-  const handleAddClaimant = ({ name, participantId, claimantNotes }) => {
+  const handleSelectDependent = (value) => setClaimant({ claimant: value, claimantType: 'dependent' });
+  const handleAddClaimant = ({ name, participantId, claimantType, claimantNotes }) => {
     setNewClaimant({
       displayElem: <RemovableRadioLabel
         text={`${name || 'Claimant not listed'}, Attorney`} onRemove={handleRemove} notes={claimantNotes} />,
@@ -94,7 +102,7 @@ export const SelectClaimant = (props) => {
       defaultPayeeCode: '',
       claimantNotes
     });
-    setClaimant(participantId, claimantNotes);
+    setClaimant({ claimant: participantId ?? null, claimantType, claimantNotes, claimantName: name });
     setShowClaimantModal(false);
   };
   const handlePayeeCodeChange = (event) => setPayeeCode(event ? event.value : null);
@@ -114,7 +122,7 @@ export const SelectClaimant = (props) => {
           strongLabel
           vertical
           options={radioOpts}
-          onChange={setClaimant}
+          onChange={handleSelectDependent}
           value={claimant}
           errorMessage={claimantError}
         />
@@ -141,7 +149,7 @@ export const SelectClaimant = (props) => {
     // disable veteran claimant option if veteran is deceased
     veteranClaimantOptions = BOOLEAN_RADIO_OPTIONS_DISABLED_FALSE;
     // set claimant value to someone other than the veteran
-    setVeteranIsNotClaimant('true');
+    handleVeteranIsNotClaimant('true');
   }
 
   return (
@@ -152,7 +160,7 @@ export const SelectClaimant = (props) => {
         strongLabel
         vertical
         options={veteranClaimantOptions}
-        onChange={setVeteranIsNotClaimant}
+        onChange={handleVeteranIsNotClaimant}
         errorMessage={veteranIsNotClaimantError}
         value={veteranIsNotClaimant === null ? null : veteranIsNotClaimant.toString()}
       />

spec/feature/intake/review_page_spec.rb
@@ -70,11 +70,9 @@ feature "Intake Review Page", :postgres do
     end
 
     context "when the user goes back and edits the claimant" do
-      let(:veteran_is_not_claimant) { false }
-
       describe "given an appeal" do
         it "only saves one claimant" do
-          review = start_appeal(veteran, veteran_is_not_claimant: veteran_is_not_claimant).first
+          review = start_appeal(veteran).first
 
           check_edited_claimant(review)
         end
@@ -83,11 +81,7 @@ feature "Intake Review Page", :postgres do
       [:higher_level_review, :supplemental_claim].each do |claim_review_type|
         describe "given a #{claim_review_type}" do
           it "only saves one claimant" do
-            review = start_claim_review(
-              claim_review_type,
-              veteran: veteran,
-              veteran_is_not_claimant: veteran_is_not_claimant
-            ).first
+            review = start_claim_review(claim_review_type, veteran: veteran).first
 
             check_edited_claimant(review)
           end
@@ -116,7 +110,7 @@ feature "Intake Review Page", :postgres do
     end
 
     context "when the Veteran is not the claimant" do
-      let(:veteran_is_not_claimant) { true }
+      let(:claim_participant_id) { "20678356" }
       let!(:recent_end_product_with_claimant) do
         Generators::EndProduct.build(
           veteran_file_number: veteran.file_number,
@@ -151,7 +145,7 @@ feature "Intake Review Page", :postgres do
 
         describe "given an appeal" do
           it "does not require the claimant to have an address" do
-            review = start_appeal(veteran, veteran_is_not_claimant: veteran_is_not_claimant).first
+            review = start_appeal(veteran, claim_participant_id: claim_participant_id).first
 
             check_claimant_address_error(review, benefit_type, error_text)
           end
@@ -163,7 +157,8 @@ feature "Intake Review Page", :postgres do
               review = start_claim_review(
                 claim_review_type,
                 veteran: veteran,
-                veteran_is_not_claimant: veteran_is_not_claimant
+                claim_participant_id: claim_participant_id,
+                no_claimant: true
               ).first
 
               check_claimant_address_error(review, benefit_type, error_text)
@@ -180,7 +175,7 @@ feature "Intake Review Page", :postgres do
                 review = start_claim_review(
                   claim_review_type,
                   veteran: veteran,
-                  veteran_is_not_claimant: veteran_is_not_claimant,
+                  claim_participant_id: claim_participant_id,
                   benefit_type: benefit_type
                 ).first
 
@@ -204,7 +199,8 @@ feature "Intake Review Page", :postgres do
               review = start_claim_review(
                 claim_review_type,
                 veteran: veteran,
-                veteran_is_not_claimant: veteran_is_not_claimant
+                claim_participant_id: claim_participant_id,
+                no_claimant: true
               ).first
 
               check_claimant_address_error(review, benefit_type, error_text)
@@ -217,7 +213,7 @@ feature "Intake Review Page", :postgres do
         [:higher_level_review, :supplemental_claim].each do |claim_review_type|
           describe "given a #{claim_review_type}" do
             it "requires payee code and shows default value" do
-              start_claim_review(claim_review_type, veteran: veteran, veteran_is_not_claimant: veteran_is_not_claimant)
+              start_claim_review(claim_review_type, veteran: veteran, claim_participant_id: claim_participant_id)
               check_pension_and_compensation_payee_code
             end
           end
@@ -234,7 +230,7 @@ feature "Intake Review Page", :postgres do
             start_higher_level_review(
               veteran,
               benefit_type: benefit_type,
-              veteran_is_not_claimant: veteran_is_not_claimant
+              claim_participant_id: claim_participant_id
             )
             check_no_relationships_behavior
           end
@@ -245,7 +241,7 @@ feature "Intake Review Page", :postgres do
             start_supplemental_claim(
               veteran,
               benefit_type: benefit_type,
-              veteran_is_not_claimant: veteran_is_not_claimant
+              claim_participant_id: claim_participant_id
             )
             check_no_relationships_behavior
           end
@@ -253,10 +249,7 @@ feature "Intake Review Page", :postgres do
 
         context "appeal" do
           it "shows message and does not allow user to continue" do
-            start_appeal(
-              veteran,
-              veteran_is_not_claimant: veteran_is_not_claimant
-            )
+            start_appeal(veteran, claim_participant_id: claim_participant_id)
             check_no_relationships_behavior
           end
         end
@@ -269,10 +262,7 @@ feature "Intake Review Page", :postgres do
 
         context "without attorney_fees feature toggle" do
           it "doesn't allow adding new claimants" do
-            start_appeal(
-              veteran,
-              veteran_is_not_claimant: veteran_is_not_claimant
-            )
+            start_appeal(veteran, claim_participant_id: claim_participant_id)
 
             visit "/intake"
 
@@ -293,9 +283,10 @@ feature "Intake Review Page", :postgres do
           let(:attorney) { attorneys.last }
 
           it "allows adding new claimants" do
-            start_appeal(
+            appeal, _intake = start_appeal(
               veteran,
-              veteran_is_not_claimant: veteran_is_not_claimant
+              claim_participant_id: claim_participant_id,
+              no_claimant: true
             )
 
             visit "/intake"
@@ -308,23 +299,33 @@ feature "Intake Review Page", :postgres do
 
             expect(page).to have_content("+ Add Claimant")
 
-            click_button("+ Add Claimant")
-            expect(page).to have_selector("#add_claimant_modal")
-            expect(page).to have_button("Add this claimant", disabled: true)
-            claimant_search(attorney.name)
-            select_claimant(0)
-            expect(page).to have_button("Add this claimant", disabled: false)
-            click_button "Add this claimant"
-            expect(page).to_not have_selector("#add_claimant_modal")
-            expect(page).to have_content("#{attorney.name}, Attorney")
+            add_existing_attorney(attorney)
+
+            # Verify that this can be removed
             find(".remove-item").click
             expect(page).to_not have_content("#{attorney.name}, Attorney")
+
+            # Add again
+            add_existing_attorney(attorney)
+
+            click_button "Continue to next step"
+
+            expect(page).to have_current_path("/intake/add_issues")
+
+            expect(Claimant.count).to eq(1)
+            expect(appeal.claimant).to have_attributes(
+              type: "AttorneyClaimant",
+              participant_id: attorney.participant_id
+            )
+
+            expect(page).to have_content("#{attorney.name}, Attorney")
           end
 
           scenario "when claimant is not listed" do
-            start_appeal(
+            appeal, _intake = start_appeal(
               veteran,
-              veteran_is_not_claimant: veteran_is_not_claimant
+              claim_participant_id: claim_participant_id,
+              no_claimant: true
             )
 
             visit "/intake"
@@ -337,23 +338,61 @@ feature "Intake Review Page", :postgres do
 
             expect(page).to have_content("+ Add Claimant")
 
-            click_button("+ Add Claimant")
-            expect(page).to have_content("Claimant not listed")
-            expect(page).to have_button("Add this claimant", disabled: true)
-            find("label[for=noClaimant]").click
-            expect(page).to have_content("Notes e.g.")
-            expect(page).to have_button("Add this claimant", disabled: true)
+            notes = "Unlisted claimant: Sandra Smith"
 
-            fill_in "Notes e.g.", with: "The claimant's name not listed is Sandra smith!"
-            expect(page).to have_button("Add this claimant", disabled: false)
-            click_button "Add this claimant"
-            expect(page).to_not have_selector("#add_claimant_modal")
-            expect(page).to have_content("Claimant not listed, Attorney")
-            expect(page).to have_content("The claimant's name not listed is Sandra smith!")
+            add_unlisted_claimant(notes)
+
+            # Verify removal
             find(".remove-item").click
-            expect(page).to_not have_content("The claimant's name not listed is Sandra smith!")
+            expect(page).to_not have_content(notes)
+
+            # Add again
+            add_unlisted_claimant(notes)
+
+            click_button "Continue to next step"
+
+            expect(page).to have_current_path("/intake/add_issues")
+
+            expect(Claimant.count).to eq(1)
+            expect(appeal.claimant).to have_attributes(
+              type: "OtherClaimant",
+              participant_id: veteran.participant_id,
+              notes: notes
+            )
+
+            expect(page).to have_content(notes)
           end
         end
+
+        def add_existing_attorney(attorney)
+          click_button("+ Add Claimant")
+          expect(page).to have_selector("#add_claimant_modal")
+          expect(page).to have_button("Add this claimant", disabled: true)
+          claimant_search(attorney.name)
+          select_claimant(0)
+          expect(page).to have_button("Add this claimant", disabled: false)
+          click_button "Add this claimant"
+          expect(page).to_not have_selector("#add_claimant_modal")
+          expect(page).to have_content("#{attorney.name}, Attorney")
+        end
+
+        # rubocop: disable Metrics/AbcSize
+        def add_unlisted_claimant(notes)
+          click_button("+ Add Claimant")
+          expect(page).to have_content("Claimant not listed")
+          expect(page).to have_button("Add this claimant", disabled: true)
+          find("label[for=notListed]").click
+          expect(page).to have_content("Notes e.g.")
+          expect(page).to have_button("Add this claimant", disabled: true)
+
+          fill_in "Notes e.g.", with: notes
+          expect(page).to have_button("Add this claimant", disabled: false)
+          click_button "Add this claimant"
+          expect(page).to_not have_selector("#add_claimant_modal")
+          expect(page).to have_content("Claimant not listed, Attorney")
+          expect(page).to have_content(notes)
+        end
+        # rubocop: enable Metrics/AbcSize
 
         def claimant_search(search)
           find(".dropdown-claimant").fill_in "claimant", with: search
@@ -407,9 +446,12 @@ def check_pension_and_compensation_payee_code
   expect(page).to have_content(
     "Receipt date cannot be in the future."
   )
-  expect(page).to have_content("Please select an option.")
 
   fill_in "What is the Receipt Date of this form?", with: Time.zone.today.mdY
+
+  click_intake_continue
+
+  expect(page).to have_content("Please select an option.")
 
   within_fieldset("What is the Benefit Type?") do
     find("label", text: "Pension", match: :prefer_exact).click

app/models/appeal_intake.rb
@@ -39,32 +39,11 @@ class AppealIntake < DecisionReviewIntake
 
   private
 
-  def create_claimant!
-    if request_params[:veteran_is_not_claimant] == true
-      claimant_type = "DependentClaimant"
-      participant_id = request_params[:claimant]
-    else
-      claimant_type = "VeteranClaimant"
-      participant_id = veteran.participant_id
-    end
-
-    Claimant.find_or_initialize_by(
-      decision_review: detail
-    ).tap do |claimant|
-      claimant.type = claimant_type
-      claimant.participant_id = participant_id
-      claimant.notes = request_params[:claimant_notes]
-      claimant.save!
-    end
-    update_person!
+  def need_payee_code?
+    false
   end
 
-  def review_params
-    request_params.permit(
-      :receipt_date,
-      :docket_type,
-      :veteran_is_not_claimant,
-      :legacy_opt_in_approved
-    )
+  def review_param_keys
+    %w[receipt_date docket_type legacy_opt_in_approved]
   end
 end

app/models/attorney_claimant.rb
@@ -4,6 +4,8 @@
 # An attorney can be a claimant when contesting attorney fees.
 
 class AttorneyClaimant < Claimant
+  validate { |claimant| ClaimantValidator.new(claimant).validate }
+
   delegate :name, to: :bgs_attorney
 
   private

app/models/bgs_related_claimant.rb
@@ -3,6 +3,8 @@
 class BgsRelatedClaimant < Claimant
   include AssociatedBgsRecord
 
+  validate { |claimant| ClaimantValidator.new(claimant).validate }
+
   def fetch_bgs_record
     general_info = bgs.fetch_claimant_info_by_participant_id(participant_id)
     name_info = bgs.fetch_person_info(participant_id)

app/models/claim_review_intake.rb
@@ -40,39 +40,11 @@ class ClaimReviewIntake < DecisionReviewIntake
 
   private
 
-  def create_claimant!
-    if request_params[:veteran_is_not_claimant] == true
-      claimant_type = "DependentClaimant"
-      participant_id = request_params[:claimant]
-      payee_code = need_payee_code? ? request_params[:payee_code] : nil
-    else
-      claimant_type = "VeteranClaimant"
-      participant_id = veteran.participant_id
-      payee_code = nil
-    end
-
-    Claimant.find_or_initialize_by(
-      decision_review: detail
-    ).tap do |claimant|
-      claimant.type = claimant_type
-      claimant.participant_id = participant_id
-      claimant.payee_code = payee_code
-      claimant.save!
-    end
-    update_person!
-  end
-
   def need_payee_code?
-    # payee_code is only required for claim reviews where the veteran is
-    # not the claimant and the benefit_type is compensation or pension
-    return unless request_params[:veteran_is_not_claimant] == true
+    # payee_code is only required for claim reviews where the claimant is a dependent
+    # and the benefit_type is compensation or pension
+    return unless claimant_class_name == "DependentClaimant"
 
     ClaimantValidator::BENEFIT_TYPE_REQUIRES_PAYEE_CODE.include?(request_params[:benefit_type])
   end
-
-  # :nocov:
-  def review_params
-    fail Caseflow::Error::MustImplementInSubclass
-  end
-  # :nocov:
 end

app/models/claimant.rb
@@ -9,7 +9,6 @@ class Claimant < CaseflowRecord
   belongs_to :decision_review, polymorphic: true
   belongs_to :person, primary_key: :participant_id, foreign_key: :participant_id
 
-  validate { |claimant| ClaimantValidator.new(claimant).validate }
   validates :participant_id,
             uniqueness: { scope: [:decision_review_id, :decision_review_type],
                           on: :create }

app/models/decision_review.rb
@@ -156,6 +156,14 @@ class DecisionReview < CaseflowRecord
     claimant&.participant_id
   end
 
+  def claimant_type
+    claimant_class_name&.sub(/Claimant$/, "")&.downcase
+  end
+
+  def claimant_class_name
+    claimant&.type
+  end
+
   def finalized_decision_issues_before_receipt_date
     fail NotImplementedError
   end

commit 0ada9c30d4e6be12941759ac4e8165469548b15e

Fix bug preventing selection of deceased vet's dependent claimant (#14799)
### Description
This PR fixes a bug I introduced in #14648 that prevents Intake users from selecting dependent claimants when the veteran is deceased.

The bug is caused by ascribing two distinct intentions to the `handleVeteranIsNotClaimant` function: the user clicking on `yes`/`no` for whether the claimant is the veteran, and setting the corresponding Redux state under the hood. The `deceased` conditional intends the latter but the function implements the former -- therefore I've added the word `Change` to the function name.

### Acceptance Criteria
- [ ] Code compiles correctly
- [ ] Intake user can select dependent claimant for a deceased veteran

### Testing Plan
- Manual test:
  - Start a claim review intake (HLR or SC)
  - Make sure the selected veteran has a `date_of_death` set in the DB
  - On the review screen, make sure the deceased veteran can't be selected, and their dependent(s) can.
- Added a check in the deceased veteran spec for selecting a dependent claimant.

### User Facing Changes
No real changes, but this is what the UI looks like with the bug preventing claimant selection:
<img width="509" src="https://user-images.githubusercontent.com/282869/88844161-42991780-d1b0-11ea-8306-a8b71a668554.png">

And with the bug fixed (just business as usual):
<img width="482" src="https://user-images.githubusercontent.com/282869/88844184-49c02580-d1b0-11ea-9768-2995bf2b3dae.png">

client/app/intake/components/SelectClaimant.jsx
@@ -83,7 +83,7 @@ export const SelectClaimant = (props) => {
     veteranIsNotClaimant,
     attorneyFees
   ]);
-  const handleVeteranIsNotClaimant = (value) => {
+  const handleVeteranIsNotClaimantChange = (value) => {
     const boolValue = convertStringToBoolean(value);
 
     setVeteranIsNotClaimant(boolValue);
@@ -149,7 +149,7 @@ export const SelectClaimant = (props) => {
     // disable veteran claimant option if veteran is deceased
     veteranClaimantOptions = BOOLEAN_RADIO_OPTIONS_DISABLED_FALSE;
     // set claimant value to someone other than the veteran
-    handleVeteranIsNotClaimant('true');
+    setVeteranIsNotClaimant(true);
   }
 
   return (
@@ -160,7 +160,7 @@ export const SelectClaimant = (props) => {
         strongLabel
         vertical
         options={veteranClaimantOptions}
-        onChange={handleVeteranIsNotClaimant}
+        onChange={handleVeteranIsNotClaimantChange}
         errorMessage={veteranIsNotClaimantError}
         value={veteranIsNotClaimant === null ? null : veteranIsNotClaimant.toString()}
       />

spec/feature/intake/review_page_spec.rb
@@ -427,6 +427,9 @@ def check_deceased_veteran_cant_be_payee
   # verify that veteran cannot be selected
   expect(page.has_no_content?("00 - Veteran")).to eq(true)
   expect(page).to have_content("10 - Spouse")
+
+  find("label", text: "Bob Vance, Spouse", match: :prefer_exact).click
+  expect(find(".cf-select__value-container")).to have_content("10 - Spouse")
 end
 
 # rubocop: disable Metrics/AbcSize