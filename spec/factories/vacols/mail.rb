62c9954f1a2b499855491e96be317acb9441db43
hschallhorn <hschallhorn@tistatech.com>
2020-07-29 20:20:11 -0400

    Ensure we're getting actually ready legacy appeals when calling counts_by_priority_and_readiness

--- spec/factories/vacols/mail.rb
+++ spec/factories/vacols/mail.rb
@@ -3,5 +3,15 @@
 FactoryBot.define do
   factory :mail, class: VACOLS::Mail do
     mltype { "02" }
+
+    trait :blocking do
+      mltype { "03" }
+      mlcompdate { nil }
+    end
+
+    trait :non_blocking do
+      mltype { "03" }
+      mlcompdate { Time.zone.now.to_date }
+    end
   end
 end
