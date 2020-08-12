73522008afa195a8aa3b999b0347544b3e70215c
JC Quirin <jcq@jcq.lan>
2020-08-12 09:29:19 -0700

    migrated TextareaField to functional component; added named as well as default export to TextareaField;

--- client/app/components/TextareaField.jsx
+++ client/app/components/TextareaField.jsx
@@ -10,87 +10,85 @@ import { FormLabel } from './FormLabel';
  * This freeform field allows users to write as much as they need to.
  * When the message is longer than the length of the box, a scroll bar will appear on the side.
  */
-export default class TextareaField extends React.Component {
-  onChange = (event) => {
-    this.props.onChange(event.target.value);
+export const TextareaField = (props) => {
+  const handleChange = (event) => {
+    props?.onChange?.(event.target.value);
   };
 
-  render() {
-    const {
-      errorMessage,
-      hideLabel,
-      id,
-      maxlength,
-      label,
-      strongLabel,
-      name,
-      required,
-      type,
-      value,
-      styling,
-      textAreaStyling,
-      disabled,
-      labelStyling,
-      placeholder,
-      optional,
-    } = this.props;
+  const {
+    errorMessage,
+    hideLabel,
+    id,
+    maxlength,
+    label,
+    strongLabel,
+    name,
+    required,
+    type,
+    value,
+    styling,
+    textAreaStyling,
+    disabled,
+    labelStyling,
+    placeholder,
+    optional,
+  } = props;
 
-    const className = classNamesFn('cf-form-textarea', {
-      'usa-input-error': Boolean(errorMessage),
-    });
+  const className = classNamesFn('cf-form-textarea', {
+    'usa-input-error': Boolean(errorMessage),
+  });
 
-    // There is only a value for this variable if maxlength and value props are not null.
-    // Otherwise characterLimitCount will be null also.
-    const characterLimitCount =
-      Boolean(maxlength) && Boolean(value) ? maxlength - value.length : null;
+  // There is only a value for this variable if maxlength and value props are not null.
+  // Otherwise characterLimitCount will be null also.
+  const characterLimitCount =
+    Boolean(maxlength) && Boolean(value) ? maxlength - value.length : null;
 
-    const labelContents = (
-      <FormLabel
-        label={label}
+  const labelContents = (
+    <FormLabel
+      label={label}
+      name={name}
+      required={required}
+      optional={optional}
+    />
+  );
+
+  // hideLabel still leaves the label element in the DOM (for a11y purposes)
+  // but makes it invisible to any screens
+  return (
+    <div className={className} {...styling}>
+      <label
+        {...labelStyling}
+        className={classNamesFn({ 'sr-only': hideLabel }, 'question-label')}
+        htmlFor={id || name}
+      >
+        {strongLabel ? <strong>{labelContents}</strong> : labelContents}
+      </label>
+      {errorMessage && (
+        <span className="usa-input-error-message">{errorMessage}</span>
+      )}
+      <textarea
+        {...textAreaStyling}
         name={name}
-        required={required}
-        optional={optional}
+        id={id || name}
+        onChange={handleChange}
+        onKeyDown={props.onKeyDown}
+        type={type}
+        value={value}
+        maxLength={maxlength}
+        disabled={disabled}
+        placeholder={placeholder}
       />
-    );
-
-    // hideLabel still leaves the label element in the DOM (for a11y purposes)
-    // but makes it invisible to any screens
-    return (
-      <div className={className} {...styling}>
-        <label
-          {...labelStyling}
-          className={classNamesFn({ 'sr-only': hideLabel }, 'question-label')}
-          htmlFor={id || name}
-        >
-          {strongLabel ? <strong>{labelContents}</strong> : labelContents}
-        </label>
-        {errorMessage && (
-          <span className="usa-input-error-message">{errorMessage}</span>
-        )}
-        <textarea
-          {...textAreaStyling}
-          name={name}
-          id={id || name}
-          onChange={this.onChange}
-          onKeyDown={this.props.onKeyDown}
-          type={type}
-          value={value}
-          maxLength={maxlength}
-          disabled={disabled}
-          placeholder={placeholder}
-        />
-        {characterLimitCount !== null && (
-          <p>
-            <i>
-              {characterLimitCount}{' '}
-              {pluralize('character', characterLimitCount)} left
-            </i>
-          </p>
-        )}
-      </div>
-    );
-  }
-}
+      {characterLimitCount !== null && (
+        <p>
+          <i>
+            {characterLimitCount} {pluralize('character', characterLimitCount)}{' '}
+            left
+          </i>
+        </p>
+      )}
+    </div>
+  );
+};
 
 TextareaField.defaultProps = {
   disabled: false,
@@ -118,3 +116,5 @@ TextareaField.propTypes = {
   labelStyling: PropTypes.object,
   textAreaStyling: PropTypes.object,
 };
+
+export default TextareaField;
