031fca65542a84c3cd6569ab7cc9d89982d34569
Kat Tipton <kathryn.tipton@gmail.com>
2020-08-13 10:27:45 -0400

    Refactors shared CaseMovement tests into shared file

--- spec/models/tasks/special_case_movement/blocked_special_case_movement_task_spec.rb
+++ spec/models/tasks/special_case_movement/blocked_special_case_movement_task_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../special_case_movement_shared_examples"
+
 describe BlockedSpecialCaseMovementTask do
   describe ".create" do
     context "with Case Movement Team user" do
@@ -14,16 +16,6 @@ describe BlockedSpecialCaseMovementTask do
 
       before do
         SpecialCaseMovementTeam.singleton.add_user(cm_user)
-      end
-
-      shared_examples "completed distribution and at JudgeAssign" do
-        it "should create the SCM task and JudgeAssign task" do
-          expect { subject }.not_to raise_error
-          scm_task =  appeal.tasks.where(type: BlockedSpecialCaseMovementTask.name).first
-          expect(scm_task.status).to eq(Constants.TASK_STATUSES.completed)
-          judge_task = appeal.tasks.open.where(type: JudgeAssignTask.name).first
-          expect(judge_task.status).to eq(Constants.TASK_STATUSES.assigned)
-        end
       end
 
       shared_examples "cancelled distribution children" do
@@ -49,6 +41,8 @@ describe BlockedSpecialCaseMovementTask do
           it_behaves_like "completed distribution and at JudgeAssign"
         end
 
+        it_behaves_like "succeeds when there's a nonblocking mail task"
+
         context "with (dispatch) blocking mail task" do
           before do
             # TODO: this _should_ not cancel after we finish
@@ -71,21 +65,6 @@ describe BlockedSpecialCaseMovementTask do
           it_behaves_like "completed distribution and at JudgeAssign"
           it_behaves_like "cancelled distribution children"
         end
-
-        context "with a nonblocking mail task" do
-          before do
-            create(:aod_motion_mail_task,
-                   appeal: appeal,
-                   parent: appeal.root_task)
-          end
-          it_behaves_like "completed distribution and at JudgeAssign"
-          it "still has the open mail task" do
-            aod_mail_task = AodMotionMailTask.where(appeal: appeal).first
-            expect(aod_mail_task.open?).to eq(true)
-            expect { subject }.not_to raise_error
-            expect(aod_mail_task.reload.open?).to eq(true)
-          end
-        end
       end
 
       context "appeal at the evidence window state" do
@@ -101,62 +80,14 @@ describe BlockedSpecialCaseMovementTask do
           it_behaves_like "cancelled distribution children"
         end
 
-        context "with the evidence window task as parent" do
-          let(:evidence_window_task) { appeal.tasks.open.where(type: EvidenceSubmissionWindowTask.name).first }
-
-          subject do
-            BlockedSpecialCaseMovementTask.create!(appeal: appeal,
-                                                   assigned_to: cm_user,
-                                                   assigned_by: cm_user,
-                                                   parent: evidence_window_task)
-          end
-
-          it "should error with wrong parent type" do
-            expect { subject }.to raise_error(Caseflow::Error::InvalidParentTask)
-          end
-        end
+        it_behaves_like "wrong parent task type"
       end
 
-      context "appeal at the judge already" do
-        let(:appeal) do
-          create(:appeal,
-                 :assigned_to_judge,
-                 docket_type: Constants.AMA_DOCKETS.direct_review)
-        end
-        let(:dist_task) { appeal.tasks.where(type: DistributionTask.name).first }
-
-        subject do
-          BlockedSpecialCaseMovementTask.create!(appeal: appeal,
-                                                 assigned_to: cm_user,
-                                                 assigned_by: cm_user,
-                                                 parent: dist_task)
-        end
-
-        it "should error with appeal not distributable" do
-          expect { subject }.to raise_error(Caseflow::Error::IneligibleForBlockedSpecialCaseMovement)
-        end
+      it_behaves_like "appeal past distribution fails" do
+        let(:expected_error) { Caseflow::Error::IneligibleForBlockedSpecialCaseMovement }
       end
     end
 
-    context "with regular user" do
-      let(:user) { create(:user) }
-      let(:appeal) do
-        create(:appeal,
-               :with_post_intake_tasks,
-               docket_type: Constants.AMA_DOCKETS.direct_review)
-      end
-      let(:dist_task) { appeal.tasks.active.where(type: DistributionTask.name).first }
-
-      subject do
-        BlockedSpecialCaseMovementTask.create!(appeal: appeal,
-                                               assigned_to: user,
-                                               assigned_by: user,
-                                               parent: dist_task)
-      end
-
-      it "should error with user error" do
-        expect { subject }.to raise_error(Caseflow::Error::ActionForbiddenError)
-      end
-    end
+    it_behaves_like "fails with wrong user"
   end
 end
