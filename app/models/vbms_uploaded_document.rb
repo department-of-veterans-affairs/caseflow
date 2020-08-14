9cd3745334cfcf3c6be16826ea77c065e3947292
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2020-08-14 15:37:10 -0700

    remove the temporary appeal_type setting

--- app/models/vbms_uploaded_document.rb
+++ app/models/vbms_uploaded_document.rb
@@ -6,17 +6,9 @@ class VbmsUploadedDocument < CaseflowRecord
   belongs_to :appeal, polymorphic: true
   validates :document_type, presence: true
 
-  before_create :temporarily_set_appeal_type
-
   attribute :file, :string
 
   def cache_file
     UploadDocumentToVbms.new(document: self).cache_file
-  end
-
-  private
-
-  def temporarily_set_appeal_type
-    self.appeal_type = Appeal.name
   end
 end
