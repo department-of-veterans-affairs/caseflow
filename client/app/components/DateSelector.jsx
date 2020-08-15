e62d983e945a05cab2f1b84234dc77fcd147e6df
JC Quirin <jcq@jcq.lan>
2020-08-14 19:37:49 -0700

    added `inputRef` and `inputProps` to DateSelector; updated proptypes for DateSelector;

--- client/app/components/DateSelector.jsx
+++ client/app/components/DateSelector.jsx
@@ -75,16 +75,67 @@ export const DateSelector = (props) => {
 };
 
 DateSelector.propTypes = {
-  errorMessage: PropTypes.string,
+
+  /**
+   * The initial value of the `input` element; use for uncontrolled components where not using `value` prop
+   */
+  defaultValue: PropTypes.string,
+
   dateErrorMessage: PropTypes.string,
+
+  /**
+   * Text (or other node) to display to indicate an error state
+   */
+  errorMessage: PropTypes.string,
   invisible: PropTypes.bool,
+
+  /**
+   * Props to be applied to the `input` element
+   */
+  inputProps: PropTypes.object,
+
+  /**
+   * Pass a ref to the `input` element
+   */
+  inputRef: PropTypes.oneOfType([
+    // Either a function
+    PropTypes.func,
+    // Or the instance of a DOM native element (see the note about SSR)
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]),
+
+  /**
+   * Text to display in a `label` element
+   */
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+
+  /**
+   * String to be applied to the `name` attribute of the `input` element
+   */
   name: PropTypes.string.isRequired,
+
+  /**
+   * Callback fired when value is changed
+   *
+   * @param {string} value The current value of the component
+   */
   onChange: PropTypes.func,
   readOnly: PropTypes.bool,
+
+  /**
+   * When true, will display text indicating that the field is required
+   */
   required: PropTypes.bool,
+
+  /**
+   * Specifies the `type` parameter for the underlying `input` element
+   */
   type: PropTypes.string,
   validationError: PropTypes.string,
+
+  /**
+   * The value of the `input` element; required for a controlled component
+   */
   value: PropTypes.string,
 };
 
