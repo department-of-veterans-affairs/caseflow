3590a780697d765714265809916de5aad3c85024
rubaiyat22 <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-08-21 16:41:09 -0400

    remove old line

--- app/services/virtual_hearings/convert_to_virtual_hearing_service.rb
+++ app/services/virtual_hearings/convert_to_virtual_hearing_service.rb
@@ -8,9 +8,8 @@ class VirtualHearings::ConvertToVirtualHearingService
   class << self
     # converts a hearing to virtual and returns alerts
     def convert_hearing_to_virtual(hearing, virtual_hearing_attributes)
-      update_attributes = { hearing: hearing, virtual_hearing_attributes: virtual_hearing_attributes }
-
       update_attributes = { virtual_hearing_attributes: virtual_hearing_attributes.to_h, hearing: hearing}
+
       form = if hearing.is_a?(LegacyHearing)
                LegacyHearingUpdateForm.new(update_attributes)
              else
