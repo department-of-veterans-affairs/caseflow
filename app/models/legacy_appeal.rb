50b594ffe15e3e183d9d1b14e582c1d43ead6806
Kat Tipton <kathryn.tipton@gmail.com>
2020-09-02 14:46:49 -0400

    Removes ignored columns now that they're dropped

--- app/models/legacy_appeal.rb
+++ app/models/legacy_appeal.rb
@@ -7,13 +7,6 @@
 # Legacy appeals have VACOLS and BGS as dependencies.
 
 class LegacyAppeal < CaseflowRecord
-  # Reverting changes that supported new contentions in Caseflow Dispatch. See #14714
-  self.ignored_columns = %w[burn_pit
-                            military_sexual_trauma
-                            blue_water
-                            us_court_of_appeals_for_veterans_claims
-                            no_special_issues]
-
   include AppealConcern
   include AssociatedVacolsModel
   include BgsService
