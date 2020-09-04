b1300cd1b408a48cf6a6f9c3db735a816e8687bb
Yoom Lam <55255674+yoomlam@users.noreply.github.com>
2020-09-04 09:35:56 -0500

    Kat's updates
    Co-authored-by: Kat Tipton <kat@navapbc.com>

--- db/migrate/20200904140217_change_special_issue_lists_comment.rb
+++ db/migrate/20200904140217_change_special_issue_lists_comment.rb
@@ -1,6 +1,6 @@
 class ChangeSpecialIssueListsComment < ActiveRecord::Migration[5.2]
   def up
-    change_table_comment :special_issue_lists, "Associates special issues to an AMA or legacy appeal for Queue. Caseflow Dispatch uses special issues stored in legacy_appeals."
+    change_table_comment :special_issue_lists, "Associates special issues to an AMA or legacy appeal for Caseflow Queue. Caseflow Dispatch uses special issues stored in legacy_appeals. They are intentionally disconnected."
     change_column_comment :special_issue_lists, :appeal_id, "The ID of the appeal associated with this record"
     change_column_comment :special_issue_lists, :appeal_type, "The type of appeal associated with this record"
   end
