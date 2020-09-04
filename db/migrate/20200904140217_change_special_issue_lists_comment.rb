4c306f092a5a0d3e1f209511e7f31622205c66e9
yoomlam <yoom@navapbc.com>
2020-09-04 09:28:54 -0500

    support rollback

--- db/migrate/20200904140217_change_special_issue_lists_comment.rb
+++ db/migrate/20200904140217_change_special_issue_lists_comment.rb
@@ -1,5 +1,12 @@
 class ChangeSpecialIssueListsComment < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_table_comment :special_issue_lists, "Associates special issues to an AMA or legacy appeal for Queue. Caseflow Dispatch uses special issues stored in legacy_appeals."
+    change_column_comment :special_issue_lists, :appeal_id, "The ID of the appeal associated with this record"
+    change_column_comment :special_issue_lists, :appeal_type, "The type of appeal associated with this record"
+  end
+  def down
+    change_table_comment :special_issue_lists, ""
+    change_column_comment :special_issue_lists, :appeal_id, ""
+    change_column_comment :special_issue_lists, :appeal_type, ""
   end
 end
