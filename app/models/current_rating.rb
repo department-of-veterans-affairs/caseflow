74810b4cc957a6809c2106398f86958c63a67b24
Yang Yang <yangyang@navapbc.com>
2020-09-04 10:53:18 -0400

    Add class description for CurrentRating

--- app/models/current_rating.rb
+++ app/models/current_rating.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# CurrentRating provides a Rating implementation based on data returned by the BGS endpoint
+# rating_profile.find_current_rating_profile_by_ptcpnt_id. Unlike its siblings RatingAtIssue
+# and PromulgatedRating, this fetches a single rating per call. Thus, fetch_in_range and
+# ratings_from_bgs_response are left unimplemented, and a few other methods aren't used.
+
 class CurrentRating < Rating
   class << self
     def fetch_by_participant_id(participant_id)
