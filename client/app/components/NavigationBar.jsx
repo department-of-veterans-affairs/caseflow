3e085fe2a5c0cb190392a3856e82b808b68798ad
hschallhorn <hschallhorn@tistatech.com>
2020-09-08 16:13:07 -0400

    cc

--- client/app/components/NavigationBar.jsx
+++ client/app/components/NavigationBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PerformanceDegradationBanner from './PerformanceDegradationBanner';
 import NavigationBar from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/NavigationBar';
 import { withRouter } from 'react-router';
@@ -9,6 +10,10 @@ class CaseflowNavigationBar extends React.PureComponent {
     extraBanner={<PerformanceDegradationBanner />}
     {...this.props} />
 }
+
+CaseflowNavigationBar.propTypes = {
+  location: PropTypes.object
+};
 
 // When a PureComponent has <Route> children, they will not re-render under certain conditions
 // unless we wrap the PureComponent in withRouter.
