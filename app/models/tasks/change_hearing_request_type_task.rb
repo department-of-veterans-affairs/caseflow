5f22bbbca5cac6e7505e207632952305cec07fae
Ferris Tseng <ferris@navapbc.com>
2020-09-10 16:07:07 -0400

    create new task for capturing convert to video/virtual action

---
+++ app/models/tasks/change_hearing_request_type_task.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ChangeHearingRequestTypeTask < Task
+  def self.label
+    "Change hearing request type"
+  end
+
+  def self.hide_from_queue_table_view
+    true
+  end
+
+  def default_instructions
+    [COPY::CHANGE_HEARING_REQUEST_TYPE_TASK_DEFAULT_INSTRUCTIONS]
+  end
+
+  def available_actions(user)
+    return [
+      Constants.TASK_ACTIONS.CHANGE_HEARING_REQUEST_TYPE_TO_VIDEO.to_h,
+      Constants.TASK_ACTIONS.CHANGE_HEARING_REQUEST_TYPE_TO_VIRTUAL.to_h
+    ] if user.can_change_hearing_request_type?
+
+    []
+  end
+end
