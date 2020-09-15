8b73b636db73362185197cf5fd75920da6f80cc4
yoomlam <yoom@navapbc.com>
2020-09-15 10:05:31 -0500

    remove extra space

--- db/migrate/20200915143743_edit_task_timers_comments.rb
+++ db/migrate/20200915143743_edit_task_timers_comments.rb
@@ -2,7 +2,7 @@ class EditTaskTimersComments < Caseflow::Migration
   def up
     change_table_comment :task_timers, "A task timer allows an associated task's (like EvidenceSubmissionWindowTask and TimedHoldTask) method to be run asynchronously after timer expires."
     change_column_comment :task_timers, :attempted_at, "Async timestamp for most recent attempt to run Task#when_timer_ends."
-    change_column_comment :task_timers, :canceled_at, "Timestamp when job was abandoned.  Associated task is typically cancelled."
+    change_column_comment :task_timers, :canceled_at, "Timestamp when job was abandoned. Associated task is typically cancelled."
     change_column_comment :task_timers, :last_submitted_at, "Async timestamp for most recent job start. Initially set to when timer should expire (Task#timer_ends_at)."
     change_column_comment :task_timers, :processed_at, "Async timestamp for when the job completes successfully. Associated task's method Task#when_timer_ends ran successfully."
     change_column_comment :task_timers, :task_id, "ID of the associated Task to be run."
