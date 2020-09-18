fe6ab3787dbad1280715169722d456707a83d7b8
Meckila Britt <meckilabritt@navapbc.com>
2020-09-17 20:02:01 -0700

    Use to_s instead of interpolation

--- spec/services/decision_date_checker_spec.rb
+++ spec/services/decision_date_checker_spec.rb
@@ -14,18 +14,18 @@ describe DecisionDateChecker, :postgres do
   context "# call" do
     it "should find request issues without decision dates" do
       subject.call
-      expect(subject.report).to include("#{request_issues[1].id}")
-      expect(subject.report).to include("#{request_issues[2].id}")
+      expect(subject.report).to include(request_issues[1].id.to_s)
+      expect(subject.report).to include(request_issues[2].id.to_s)
     end
 
     it "should not check rating issues" do
       subject.call
-      expect(subject.report).not_to include("#{request_issues[3].id}")
+      expect(subject.report).not_to include(request_issues[3].id.to_s)
     end
 
     it "should not find unidentified" do
       subject.call
-      expect(subject.report).not_to include("#{request_issues[4]}")
+      expect(subject.report).not_to include(request_issues[4].id.to_s)
     end
   end
 end
