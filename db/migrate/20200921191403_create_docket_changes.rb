05690743c0378944347727c0614ac159559dc957
JC Quirin <jcq@jcq.lan>
2020-09-21 17:24:17 -0700

    updated `CreateDocketChanges` migration

--- db/migrate/20200921191403_create_docket_changes.rb
+++ db/migrate/20200921191403_create_docket_changes.rb
@@ -2,19 +2,16 @@
 
 class CreateDocketChanges < ActiveRecord::Migration[5.2]
   def change
-    create_table :docket_changes, comment: "Stores the disposition and associated data for Docket Change motions", force: :cascade do |t|
-      t.bigint "appeal_id"
-      t.datetime "created_at", null: false
+    create_table :docket_changes, comment: "Stores the disposition and associated data for Docket Change motions" do |t|
+      t.belongs_to :appeal, null: false, foreign_key: true
+      t.belongs_to :task, null: false, foreign_key: true
       t.datetime "receipt_date", null: false
       t.string "docket", comment: "The new docket"
       t.string "disposition", comment: "Possible options are Grant, Partial Grant, and Deny"
-      t.bigint "task_id", comment: "Refers to the task use during checkout flow"
-      t.datetime "updated_at", null: false
       t.integer "granted_decision_issue_ids", comment: "When a docket change is partially granted, this includes an array of the appeal's decision issue IDs that were selected for the new docket. For full grant, this includes all prior decision issue IDs.", array: true
+      t.timestamps null: false, comment: "Standard created_at/updated_at timestamps"
 
-      t.index ["appeal_id"], name: "index_docket_changes_on_appeal_id"
-      t.index ["task_id"], name: "index_docket_changes_on_task_id"
-      t.index ["updated_at"], name: "index_docket_changes_on_updated_at"
+      t.index ["created_at"]
     end
   end
 end
