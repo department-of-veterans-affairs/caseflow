a6c851cc2ea9cd6625a576eced6e8ca05a52b174
Yang Yang <yangyang@navapbc.com>
2020-09-23 12:27:09 -0400

    Fix test

--- spec/feature/intake/intake_spec.rb
+++ spec/feature/intake/intake_spec.rb
@@ -148,6 +148,7 @@ feature "Intake", :all_dbs do
 
     scenario "Search for a veteran but search throws an unhandled exception" do
       expect_any_instance_of(Intake).to receive(:start!).and_raise("random error")
+      allow(Raven).to receive(:last_event_id).and_return("sentry_event_id")
       visit "/intake"
       select_form(Constants.INTAKE_FORM_NAMES.higher_level_review)
       safe_click ".cf-submit.usa-button"
@@ -159,7 +160,7 @@ feature "Intake", :all_dbs do
 
       expect(page).to have_current_path("/intake/search")
       expect(page).to have_content("Something went wrong")
-      expect(page).to have_content(/Error code \w+-\w+-\w+-\w+/)
+      expect(page).to have_content("Error code sentry_event_id")
     end
 
     scenario "Search for a veteran with an incident flash" do
