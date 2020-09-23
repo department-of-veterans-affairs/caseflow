department-of-veterans-affairs/caseflow

commit 60243456f43d46918a8bdbfbd7e63cdbf1670256

Smarter recovery from VA.gov limit errors (#14924)
Resolves #14710

### Description

- Remove mandatory sleep call in `VaDotGovService`
- Update job to wait before recovering from a `VaDotGovLimitError`
  - The job has checks to ensure the job never runs longer than an hour (which is when a new instance of the job should start running)
- Update `VaDotGovLimitError` to store the remaining time to wait from the API
- Add additional test cases

### Testing Plan

1. Monitor in production

app/services/external_api/va_dot_gov_service.rb
@@ -309,18 +309,6 @@ class ExternalApi::VADotGovService
       request.body = body.to_json unless body.nil?
       request.headers = headers.merge(apikey: ENV["VA_DOT_GOV_API_KEY"])
 
-      # Rate limit requests to VA.gov veteran verification API. This is meant to be an aggressive,
-      # temporary safety measure because it's more costly (in terms of computing resources) if we
-      # hit the rate limit. The sleep will be removed as a part of the work on this issue:
-      # https://github.com/department-of-veterans-affairs/caseflow/issues/14710
-      #
-      # Rate Limit: https://developer.va.gov/explore/verification/docs/veteran_confirmation?version=current
-      #
-      # > We implemented basic rate limiting of 60 requests per minute. If you exceed this quota,
-      # > your request will return a 429 status code. You may petition for increased rate limits by
-      # > emailing and requests will be decided on a case by case basis.
-      sleep 1
-
       MetricsService.record("api.va.gov #{method.to_s.upcase} request to #{url}",
                             service: :va_dot_gov,
                             name: endpoint) do

.reek.yml
@@ -81,6 +81,7 @@ detectors:
       - Fakes::BGSService#fetch_poas_by_participant_ids
       - Fakes::PersistentStore#convert_dates_from
       - FetchDocumentsForReaderJob#fetch_for_appeal
+      - FetchHearingLocationsForVeteransJob#sleep_before_retry_on_limit_error
       - HearingSerializerBase
       - LegacyDocket#count
       - RedistributedCase#legacy_appeal_relevant_tasks

app/jobs/fetch_hearing_locations_for_veterans_job.rb
@@ -5,6 +5,8 @@ class FetchHearingLocationsForVeteransJob < ApplicationJob
   application_attr :hearing_schedule
 
   QUERY_LIMIT = 750
+  JOB_DURATION = 1.hour
+
   def create_schedule_hearing_tasks
     HearingTaskTreeInitializer.create_schedule_hearing_tasks
   end
@@ -22,6 +24,10 @@ class FetchHearingLocationsForVeteransJob < ApplicationJob
       .order("ahl_updated_at nulls first")
   end
 
+  # Gets appeals that are ready for geomatching.
+  #
+  # @return     [Array<Appeal, LegacyAppeal>]
+  #   An array of appeals that are ready for geomatching, bounded by the `QUERY_LIMIT`.
   def appeals
     @appeals ||= find_appeals_ready_for_geomatching(LegacyAppeal).first(QUERY_LIMIT / 2) +
                  find_appeals_ready_for_geomatching(Appeal).first(QUERY_LIMIT / 2)
@@ -29,27 +35,84 @@ class FetchHearingLocationsForVeteransJob < ApplicationJob
 
   def perform
     RequestStore.store[:current_user] = User.system_user
+
+    @job_expected_end_time = Time.zone.now + JOB_DURATION
+    current_appeal = 0
+
     create_schedule_hearing_tasks
 
-    appeals.each do |appeal|
-      begin
-        appeal.reload # we only selected id and ahl_update_at, reload all columns
-        geomatch_result = geomatch(appeal)
-        record_geomatched_appeal(appeal.external_id, geomatch_result[:status])
-      rescue Caseflow::Error::VaDotGovLimitError
-        Rails.logger.error("VA.gov returned a rate limit error")
-        record_geomatched_appeal(appeal.external_id, "limit_error")
-        break
-      rescue StandardError => error
-        capture_exception(error: error, extra: { appeal_external_id: appeal.external_id })
-        record_geomatched_appeal(appeal.external_id, "error")
+    loop do
+      remaining_appeals = appeals[current_appeal..-1]
+
+      break if remaining_appeals.empty? || job_running_past_expected_end_time?
+
+      remaining_appeals.each do |appeal|
+        break if job_running_past_expected_end_time?
+
+        begin
+          geomatch(appeal)
+
+          current_appeal += 1
+        rescue Caseflow::Error::VaDotGovLimitError
+          sleep_before_retry_on_limit_error
+
+          break
+        rescue StandardError
+          # For unknown errors, we capture the exeception in Sentry. This error could represent
+          # a broad range of things, so we just skip geomatching for the appeal, and expect
+          # that a developer looks into it.
+          current_appeal += 1
+        end
       end
     end
   end
 
-  def geomatch(appeal)
-    geomatch_result = appeal.va_dot_gov_address_validator.update_closest_ro_and_ahls
+  private
 
+  attr_accessor :job_expected_end_time
+
+  # Returns whether or not the job is running beyond the expected end time.
+  def job_running_past_expected_end_time?
+    Time.zone.now > job_expected_end_time
+  end
+
+  # Pauses execution based on an error received from VA.gov.
+  #
+  # @note This is its own method so that it can be stubbed by the test suite.
+  def sleep_before_retry_on_limit_error
+    sleep 15
+  end
+
+  # Performs geomatching for an appeal.
+  #
+  # @param appeal  [Appeal, Legacy] the appeal to geomatch
+  #
+  # @raise      [Caseflow::Error::VaDotGovLimitError]
+  #   Re-raises limit error for caller to handle.
+  # @raise      [StandardError]
+  #   Re-raises standard error for caller to handle.
+  def geomatch(appeal)
+    appeal.reload # we only selected id and ahl_update_at, reload all columns
+
+    begin
+      geomatch_result = appeal.va_dot_gov_address_validator.update_closest_ro_and_ahls
+      handle_geocode_status(appeal, geomatch_result)
+      record_geomatched_appeal(appeal.external_id, geomatch_result[:status])
+    rescue Caseflow::Error::VaDotGovLimitError
+      Rails.logger.error("VA.gov returned a rate limit error")
+      record_geomatched_appeal(appeal.external_id, "limit_error")
+      raise
+    rescue StandardError => error
+      capture_exception(error: error, extra: { appeal_external_id: appeal.external_id })
+      record_geomatched_appeal(appeal.external_id, "error")
+      raise
+    end
+  end
+
+  # Handles the status from the VaDotGovAddressValidator.
+  #
+  # @param geomatch_result  [Hash] The result from geocoding.
+  def handle_geocode_status(appeal, geomatch_result)
     case geomatch_result[:status]
     when VaDotGovAddressValidator::STATUSES[:matched_available_hearing_locations],
       VaDotGovAddressValidator::STATUSES[:philippines_exception]
@@ -58,8 +121,6 @@ class FetchHearingLocationsForVeteransJob < ApplicationJob
       VaDotGovAddressValidator::STATUSES[:created_foreign_veteran_admin_action]
       create_available_hearing_location_for_errored_appeal(appeal)
     end
-
-    geomatch_result
   end
 
   def cancel_admin_actions_for_matched_appeal(appeal)

spec/jobs/fetch_hearing_locations_for_veterans_job_spec.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-require "faker"
-
-describe FetchHearingLocationsForVeteransJob, :all_dbs do
+describe FetchHearingLocationsForVeteransJob do
   let!(:job) { FetchHearingLocationsForVeteransJob.new }
+
+  before do
+    # Force the job to only run for 5 seconds. This overrides the default job
+    # duration which is supposed to take into account latency from the actual
+    # API.
+    stub_const("FetchHearingLocationsForVeteransJob::JOB_DURATION", 5.seconds)
+  end
 
   describe "find_appeals_ready_for_geomatching" do
     let!(:legacy_appeal_with_ro_updated_one_day_ago) { create(:legacy_appeal, vacols_case: create(:case)) }
@@ -91,15 +96,19 @@ describe FetchHearingLocationsForVeteransJob, :all_dbs do
 
     describe "#perform" do
       before(:each) do
-        allow_any_instance_of(VaDotGovAddressValidator).to receive(:update_closest_ro_and_ahls)
-          .and_return(status: :matched_available_hearing_locations)
+        allow_any_instance_of(VaDotGovAddressValidator).to(
+          receive(:update_closest_ro_and_ahls)
+            .and_return(status: :matched_available_hearing_locations)
+        )
       end
 
       subject { FetchHearingLocationsForVeteransJob.new }
 
       it "creates schedule hearing tasks for appeal and records a geomatched result" do
-        expect(subject).to receive(:record_geomatched_appeal)
-          .with(legacy_appeal.external_id, :matched_available_hearing_locations)
+        expect(subject).to(
+          receive(:record_geomatched_appeal)
+            .with(legacy_appeal.external_id, :matched_available_hearing_locations)
+        )
 
         subject.perform
 
@@ -127,12 +136,15 @@ describe FetchHearingLocationsForVeteransJob, :all_dbs do
         end
       end
 
-      context "when appeal can't be matched" do
+      context "when appeal can't be geomatched" do
         let(:appeal) { create(:appeal, :with_schedule_hearing_tasks) }
 
         before do
-          allow_any_instance_of(VaDotGovAddressValidator).to receive(:update_closest_ro_and_ahls)
-            .and_return(status: :created_verify_address_admin_action)
+          allow_any_instance_of(VaDotGovAddressValidator).to(
+            receive(:update_closest_ro_and_ahls)
+              .and_return(status: :created_verify_address_admin_action)
+          )
+
           AvailableHearingLocations.create(appeal: appeal, facility_id: "fake_152", updated_at: Time.zone.now - 15.days)
         end
 
@@ -146,28 +158,104 @@ describe FetchHearingLocationsForVeteransJob, :all_dbs do
       end
 
       context "when API limit is reached" do
-        before(:each) do
-          allow_any_instance_of(VaDotGovAddressValidator).to receive(:update_closest_ro_and_ahls)
-            .and_raise(Caseflow::Error::VaDotGovLimitError.new(code: 500, message: "Error"))
+        let(:limit_error) { Caseflow::Error::VaDotGovLimitError.new(code: 500, message: "Error") }
+
+        # intercept sleep requests
+        before { allow(subject).to(receive(:sleep_before_retry_on_limit_error)) }
+
+        context "and limit error is always returned" do
+          before(:each) do
+            allow_any_instance_of(VaDotGovAddressValidator).to(
+              receive(:update_closest_ro_and_ahls).and_raise(limit_error)
+            )
+          end
+
+          context "and VaDotGovLimitError wait time exceeds the expected end time of the job" do
+            it "records a geomatch error once" do
+              allow(subject).to(receive(:job_running_past_expected_end_time?).and_return(false, false, true))
+
+              expect(subject).to(
+                receive(:record_geomatched_appeal).with(legacy_appeal.external_id, "limit_error").once
+              )
+
+              subject.perform
+            end
+          end
+
+          context "and VaDotGovLimitError wait time doesn't immediately exceed the expected end time of the job" do
+            before do
+              allow(subject).to(
+                receive(:job_running_past_expected_end_time?).and_return(false, false, false, false, true)
+              )
+            end
+
+            it "records multiple geomatch errors" do
+              expect(subject).to(
+                receive(:record_geomatched_appeal).with(legacy_appeal.external_id, "limit_error").at_least(:once)
+              )
+
+              subject.perform
+            end
+
+            context "with multiple appeals" do
+              let!(:veteran_3) { create(:veteran, file_number: "000000000") }
+              let!(:appeal) { create(:appeal, veteran_file_number: "000000000") }
+              let!(:task_2) { create(:schedule_hearing_task, appeal: appeal) }
+
+              it "retries the same appeal again" do
+                expect(subject).to(
+                  receive(:record_geomatched_appeal).with(legacy_appeal.external_id, "limit_error").at_least(:twice)
+                )
+                expect(subject).not_to(
+                  receive(:record_geomatched_appeal).with(appeal.external_id, any_args)
+                )
+
+                subject.perform
+              end
+            end
+          end
         end
 
-        it "records a geomatch error" do
-          expect(subject).to receive(:record_geomatched_appeal)
-            .with(legacy_appeal.external_id, "limit_error")
+        context "and limit error is only returned the first time" do
+          before(:each) do
+            @va_dot_gov_address_validator_count = 0
 
-          subject.perform
+            allow_any_instance_of(VaDotGovAddressValidator).to(receive(:update_closest_ro_and_ahls) do
+              @va_dot_gov_address_validator_count += 1
+              if @va_dot_gov_address_validator_count == 1
+                fail limit_error
+              else
+                { status: :matched_available_hearing_locations }
+              end
+            end)
+          end
+
+          context "retries to geomatch" do
+            it "retries geomatching hearing locations for appeal" do
+              expect(subject).to(receive(:record_geomatched_appeal).once.with(legacy_appeal.external_id, "limit_error"))
+              expect(subject).to(
+                receive(:record_geomatched_appeal)
+                  .once
+                  .with(legacy_appeal.external_id, :matched_available_hearing_locations)
+              )
+
+              subject.perform
+            end
+          end
         end
       end
 
       context "when unknown error is thrown" do
         before(:each) do
-          allow_any_instance_of(VaDotGovAddressValidator).to receive(:update_closest_ro_and_ahls)
-            .and_raise(StandardError)
+          allow_any_instance_of(VaDotGovAddressValidator).to(
+            receive(:update_closest_ro_and_ahls).and_raise(StandardError)
+          )
         end
 
         it "records a geomatch error" do
-          expect(subject).to receive(:record_geomatched_appeal)
-            .with(legacy_appeal.external_id, "error")
+          expect(subject).to(
+            receive(:record_geomatched_appeal).with(legacy_appeal.external_id, "error")
+          )
           expect(Raven).to receive(:capture_exception)
 
           subject.perform

commit da983963223f503a6677af0a23caddc59d13ba19

re-add sleep (#15301)
Connects #14924

### Description
Re-adds the one-second sleep between va.gov service calls, as our solution for dealing with rate limit errors from the service introduced in #14924 is not performing as expected. Once we've debugged that issue, we'll remove the sleep again.

app/services/external_api/va_dot_gov_service.rb
@@ -309,6 +309,18 @@ class ExternalApi::VADotGovService
       request.body = body.to_json unless body.nil?
       request.headers = headers.merge(apikey: ENV["VA_DOT_GOV_API_KEY"])
 
+      # Rate limit requests to VA.gov veteran verification API. This is meant to be an aggressive,
+      # temporary safety measure because it's more costly (in terms of computing resources) if we
+      # hit the rate limit. The sleep will be removed as a part of the work on this issue:
+      # https://github.com/department-of-veterans-affairs/caseflow/issues/14710
+      #
+      # Rate Limit: https://developer.va.gov/explore/verification/docs/veteran_confirmation?version=current
+      #
+      # > We implemented basic rate limiting of 60 requests per minute. If you exceed this quota,
+      # > your request will return a 429 status code. You may petition for increased rate limits by
+      # > emailing and requests will be decided on a case by case basis.
+      sleep 1
+
       MetricsService.record("api.va.gov #{method.to_s.upcase} request to #{url}",
                             service: :va_dot_gov,
                             name: endpoint) do