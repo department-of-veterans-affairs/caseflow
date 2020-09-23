440f5d0e25a9e864ce448f27dcc254091915bb73
hschallhorn <hschallhorn@tistatech.com>
2020-09-23 18:06:42 -0400

    WIP

---
+++ app/models/cavc_remamd.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Record to store information captured when processing a cavc remand in caseflow
+class CavcRemand < CaseflowRecord
+  belongs_to :created_by, class_name: "User"
+  belongs_to :updated_by, class_name: "User"
+  belongs_to :appeal
+
+  validates :created_by, :updated_by, :appeal, :attorney_represented, :cavc_judge_full_name, :type, :decision_date,
+            :decision_issue_ids, :instructions, presence: true
+  validates :judgement_date, :mandate_date, presence: true, if: -> { jmr? || jmpr? }
+  validates :cavc_judge_full_name, inclusion: { in: Constants.CAVC_JUDGE_FULL_NAMES }
+  validate :decision_issue_ids_match_appeal_decision_issues, if: :jmr?
+
+  enum type: {
+    Constants.CAVC_TYPES.remand.to_sym => Constants.CAVC_TYPES.remand,
+    Constants.CAVC_TYPES.straight_reversal.to_sym => Constants.CAVC_TYPES.straight_reversal,
+    Constants.CAVC_TYPES.death_dismissal.to_sym => Constants.CAVC_TYPES.death_dismissal
+  }
+
+  enum remand_type: {
+    Constants.CAVC_TYPES.jmr.to_sym => Constants.CAVC_TYPES.jmr,
+    Constants.CAVC_TYPES.jmpr.to_sym => Constants.CAVC_TYPES.jmpr,
+    Constants.CAVC_TYPES.mdr.to_sym => Constants.CAVC_TYPES.mdr
+  }
+
+  def decision_issue_ids_match_appeal_decision_issues
+    appeal.decision_issues.to_set == decision_issue_ids.to_set
+  end
+end
---
+++ spec/models/cavc_remand_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+describe CavcRemand do
+  fdescribe ".create!" do
+    subject { CavcRemand.create!(params) }
+
+    let(:created_by) { create(:user) }
+    let(:updated_by) { create(:user) }
+    let(:appeal) { create(:appeal) }
+    let(:attorney_represented) { true }
+    let(:cavc_judge_full_name) { Constants.CAVC_JUDGE_FULL_NAMES.first }
+    let(:type) { Constants.CAVC_TYPES.first  }
+    let(:remand_type) { Constants.CAVC_REMAND_TYPES.first  }
+    let(:decision_date) { 5.days.ago }
+    let(:judgement_date) { 4.days.ago }
+    let(:mandate_date) { 3.days.ago }
+    let(:decision_issue_ids) do
+      create_list(
+        :decision_issue,
+        3,
+        :rating,
+        decision_review: appeal,
+        disposition: "denied",
+        description: "Decision issue description",
+        decision_text: "decision issue"
+      ).map(&:id)
+    end
+    let(:instructions) { "Intructions!" }
+
+    let(:params) do
+      {
+        created_by: created_by,
+        updated_by: updated_by,
+        appeal: appeal,
+        attorney_represented: attorney_represented,
+        cavc_judge_full_name: cavc_judge_full_name,
+        type: type,
+        remand_type: remand_type,
+        decision_date: decision_date,
+        judgement_date: judgement_date,
+        mandate_date: mandate_date,
+        decision_issue_ids: decision_issue_ids,
+        instructions: instructions
+      }
+    end
+
+    it "creates the record" do
+      expect { subject }.not_to raise_error
+      params.keys.each { |key| expect(subject.send(key)).to eq params[key] }
+    end
+
+    context "when missing required attributes" do
+      let(:created_by) { create(:user) }
+
+      it "does not save the record" do
+        expect { subject }.to raise_error.with("cannot be blank")
+      end
+    end
+
+    context "When the judge is not in our list of judges" do
+      let(:cavc_judge_full_name) { "Aaron Judge_HearingsAndCases Abshire" }
+
+      it "does not save the record" do
+        expect(subject.valid?).to be false
+        expect(subject.errors[:created_by].first).to eq "cannot be blank"
+      end
+    end
+  end
+end
