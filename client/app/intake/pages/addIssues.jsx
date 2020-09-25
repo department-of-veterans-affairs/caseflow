efec254f9589fc42b5621998d29edce8f7f8dedf
Yang Yang <yangyang@navapbc.com>
2020-09-25 14:20:21 -0400

    Fix lint and test

--- client/app/intake/pages/addIssues.jsx
+++ client/app/intake/pages/addIssues.jsx
@@ -151,7 +151,8 @@ class AddIssuesPage extends React.Component {
     }
 
     const requestState = intakeData.requestStatus.completeIntake || intakeData.requestStatus.requestIssuesUpdate;
-    const requestErrorCode = intakeData.requestStatus.completeIntakeErrorCode || intakeData.requestIssuesUpdateErrorCode;
+    const requestErrorCode =
+      intakeData.requestStatus.completeIntakeErrorCode || intakeData.requestIssuesUpdateErrorCode;
     const requestErrorUUID = intakeData.requestStatus.completeIntakeErrorUUID;
     const showInvalidVeteranError =
       !intakeData.veteranValid &&
@@ -325,7 +326,9 @@ class AddIssuesPage extends React.Component {
         )}
         <h1 className="cf-txt-c">{messageHeader}</h1>
 
-        {requestState === REQUEST_STATE.FAILED && <ErrorAlert errorCode={requestErrorCode} errorUUID={requestErrorUUID} />}
+        {requestState === REQUEST_STATE.FAILED && (
+          <ErrorAlert errorCode={requestErrorCode} errorUUID={requestErrorUUID} />
+        )}
 
         {showInvalidVeteranError && (
           <ErrorAlert errorCode="veteran_not_valid" errorData={intakeData.veteranInvalidFields} />
