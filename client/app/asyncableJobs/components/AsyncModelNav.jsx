8ed91d05ef99c694ad719a201cf1d5f3abf6f46a
Meckila Britt <meckilabritt@navapbc.com>
2020-09-28 16:04:18 -0700

    WIP set up jobs search by veteran id

--- client/app/asyncableJobs/components/AsyncModelNav.jsx
+++ client/app/asyncableJobs/components/AsyncModelNav.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import DropdownButton from '../../components/DropdownButton';
+import SearchBar from '../../components/SearchBar';
+import ApiUtil from '../../util/ApiUtil';
 
 const DATE_TIME_FORMAT = 'ddd MMM DD HH:mm:ss YYYY';
 
 export default class AsyncModelNav extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      veteranId: '',
+      isFetchingSearchResults: false,
+      error: false
+    };
+  }
+
   modelNameLinks = () => {
     const links = [];
     const modelNames = this.props.models.sort();
@@ -30,6 +42,25 @@ export default class AsyncModelNav extends React.PureComponent {
     return numLinks > 4 ? <DropdownButton lists={links} label="Filter by Job Type" /> : links;
   }
 
+  handleVeteranIdSearch = () => {
+    const searchTerm = this.state.veteranId;
+    this.setState({ isFetchingSearchResults: true });
+
+    // Get all jobs 
+    ApiUtil.get("/jobs", { headers: { 'case-search': searchTerm } })
+    .then((response) => {
+      // const jobResults = response.body
+      // and filter by vet id after
+
+    })
+    .catch(() => {
+      this.setState({
+        isFetchingSearchResults: false,
+        error: 'No jobs found for Veteran ID'
+      });
+    });
+  }
+
   render = () => {
     const currentPath = this.props.asyncableJobKlass ?
       `/asyncable_jobs/${this.props.asyncableJobKlass}/jobs` :
@@ -41,6 +72,14 @@ export default class AsyncModelNav extends React.PureComponent {
         <a style={{ marginRight: '.5em' }} href="/jobs" className="cf-link-btn">All jobs</a>
         {this.modelNameLinks()}
         <a style={{ float: 'right' }} href={`${currentPath}.csv`} className="cf-link-btn">Download as CSV</a>
+        <SearchBar
+          size="small"
+          title="Search by Veteran ID"
+          value={this.state.veteranId}
+          onSubmit={this.handleVeteranIdSearch}
+          loading={this.state.isFetchingSearchResults}
+          submitUsingEnterKey
+      />
       </div>
     </div>;
   }
