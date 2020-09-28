71cf8182ce3a1f56d03d1bc5795b55635cb89fc6
Sandra Jones <sjones@tistatech.com>
2020-09-28 16:54:09 -0400

    updated default value

--- db/migrate/20200928194631_backfill_request_issue_type.rb
+++ db/migrate/20200928194631_backfill_request_issue_type.rb
@@ -1,4 +1,7 @@
 class BackfillRequestIssueType < ActiveRecord::Migration[5.2]
   def change
+  	RequestIssue.unscoped.in_batches do |relation|
+      relation.update_all type: "RequestIssue"
+      sleep(0.1)
   end
 end
