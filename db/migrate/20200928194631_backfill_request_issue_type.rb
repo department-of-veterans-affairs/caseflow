5f34a463b4f036dcc7a12f032532cbf2fd3bab5e
Sandra Jones <sjones@tistatech.com>
2020-09-28 17:47:38 -0400

    added end syntax

--- db/migrate/20200928194631_backfill_request_issue_type.rb
+++ db/migrate/20200928194631_backfill_request_issue_type.rb
@@ -3,5 +3,6 @@ class BackfillRequestIssueType < ActiveRecord::Migration[5.2]
   	RequestIssue.unscoped.in_batches do |relation|
       relation.update_all type: "RequestIssue"
       sleep(0.1)
+    end
   end
 end
