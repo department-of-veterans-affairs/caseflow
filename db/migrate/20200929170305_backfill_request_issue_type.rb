f02eebc94cc61007312a69a4dcd1c5005d9c3976
Sandra Jones <sjones@tistatech.com>
2020-09-29 17:15:32 -0400

    rollback and made changes

--- db/migrate/20200929170305_backfill_request_issue_type.rb
+++ db/migrate/20200929170305_backfill_request_issue_type.rb
@@ -4,7 +4,7 @@ class BackfillRequestIssueType < ActiveRecord::Migration[5.2]
 
   def change
   	RequestIssue.unscoped.in_batches do |relation|
-      relation.update_all type: "RequestIssue"
+      relation.update_all request_issue_type: "RequestIssue"
       sleep(0.1)
     end
   end
