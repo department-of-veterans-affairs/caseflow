dc0ff4db21a52d9b797048a1bcf556b439d1b9a3
Kat Tipton <kathryn.tipton@gmail.com>
2020-09-29 10:38:27 -0400

    Renames some fields!

--- app/models/cavc_remand.rb
+++ app/models/cavc_remand.rb
@@ -1,27 +1,28 @@
 # frozen_string_literal: true
 
 # Record to store information captured when processing a cavc remand in caseflow
+
 class CavcRemand < CaseflowRecord
   belongs_to :created_by, class_name: "User"
   belongs_to :updated_by, class_name: "User"
   belongs_to :appeal
 
-  validates :created_by, :updated_by, :appeal, :cavc_docket_number, :attorney_represented, :cavc_judge_full_name,
-            :cavc_type, :decision_date, :decision_issue_ids, :instructions, presence: true
+  validates :created_by, :updated_by, :appeal, :cavc_docket_number, :represented_by_attorney, :cavc_judge_full_name,
+            :cavc_decision_type, :decision_date, :decision_issue_ids, :instructions, presence: true
   validates :judgement_date, :mandate_date, presence: true, if: -> { jmr? || jmpr? }
   validates :cavc_judge_full_name, inclusion: { in: Constants::CAVC_JUDGE_FULL_NAMES }
   validate :decision_issue_ids_match_appeal_decision_issues, if: :jmr?
 
-  enum cavc_type: {
-    Constants.CAVC_TYPES.remand.to_sym => Constants.CAVC_TYPES.remand,
-    Constants.CAVC_TYPES.straight_reversal.to_sym => Constants.CAVC_TYPES.straight_reversal,
-    Constants.CAVC_TYPES.death_dismissal.to_sym => Constants.CAVC_TYPES.death_dismissal
+  enum cavc_decision_type: {
+    Constants.CAVC_DECISION_TYPES.remand.to_sym => Constants.CAVC_DECISION_TYPES.remand,
+    Constants.CAVC_DECISION_TYPES.straight_reversal.to_sym => Constants.CAVC_DECISION_TYPES.straight_reversal,
+    Constants.CAVC_DECISION_TYPES.death_dismissal.to_sym => Constants.CAVC_DECISION_TYPES.death_dismissal
   }
 
-  enum remand_type: {
-    Constants.CAVC_REMAND_TYPES.jmr.to_sym => Constants.CAVC_REMAND_TYPES.jmr,
-    Constants.CAVC_REMAND_TYPES.jmpr.to_sym => Constants.CAVC_REMAND_TYPES.jmpr,
-    Constants.CAVC_REMAND_TYPES.mdr.to_sym => Constants.CAVC_REMAND_TYPES.mdr
+  enum remand_subtype: {
+    Constants.CAVC_REMAND_SUBTYPES.jmr.to_sym => Constants.CAVC_REMAND_SUBTYPES.jmr,
+    Constants.CAVC_REMAND_SUBTYPES.jmpr.to_sym => Constants.CAVC_REMAND_SUBTYPES.jmpr,
+    Constants.CAVC_REMAND_SUBTYPES.mdr.to_sym => Constants.CAVC_REMAND_SUBTYPES.mdr
   }
 
   def decision_issue_ids_match_appeal_decision_issues
--- db/migrate/20200923192959_create_cavc_remand.rb
+++ db/migrate/20200923192959_create_cavc_remand.rb
@@ -6,17 +6,17 @@ class CreateCavcRemand < Caseflow::Migration
       t.bigint "updated_by_id", null: false, comment: "User that updated this record. For MDR remands, judgement and mandate dates will be added after the record is first created."
 
       t.string "cavc_docket_number", null: false, comment: "Docket number of the CAVC judgement"
-      t.boolean "attorney_represented", null: false, comment: "Whether or not the appellant was represented by an attorney"
+      t.boolean "represented_by_attorney", null: false, comment: "Whether or not the appellant was represented by an attorney"
       t.string "cavc_judge_full_name", null: false, comment: "CAVC judge that passed the judgement on the remand"
-      t.string "cavc_type", null: false, comment: "CAVC Decision type. Currently one of 'Remand', 'Straight Reversal', and 'Death Dismissal'"
-      t.string "remand_type", comment: "Type of remand. Can be null if the cavc decision type is not 'Remand'. One of 'JMP', 'JMPR', and 'MDR'"
-      t.date "decision_date", null: false, comment: "Date CAVC issued a decision"
-      t.date "judgement_date", null: false, comment: "Date CAVC issued a judgement"
-      t.date "mandate_date", null: false, comment: "Date mandate was ready"
+      t.string "cavc_decision_type", null: false, comment: "CAVC Decision type. Currently one of 'Remand', 'Straight Reversal', and 'Death Dismissal'"
+      t.string "remand_subtype", comment: "Type of remand. Can be null if the cavc decision type is not 'Remand'. One of 'JMP', 'JMPR', and 'MDR'"
+      t.date "decision_date", null: false, comment: "Date CAVC issued a decision, according to the CAVC"
+      t.date "judgement_date", null: false, comment: "Date CAVC issued a judgement, according to the CAVC"
+      t.date "mandate_date", null: false, comment: "Date mandate was ready, according to the CAVC"
       t.bigint "decision_issue_ids", default: [], array: true, comment: "Decision issues being remanded. For a JMR, all decision issues on the previous appeal will be remanded. For a JMPR, only some"
       t.string "instructions", null: false, comment: "Intructions and context provided upon creation of the remand record"
 
-      t.timestamps null: false, comment: "Default created_at/updated_at"
+      t.timestamps null: false, comment: "Default timestamps"
     end
   end
 end
