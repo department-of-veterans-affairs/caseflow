73f377ff2993754a58528abbc757492e3faaae1a
Sandra Jones <sjones@tistatech.com>
2020-09-30 12:02:36 -0400

    changed it to up instead of change

--- db/migrate/20200929170305_backfill_request_issue_type.rb
+++ db/migrate/20200929170305_backfill_request_issue_type.rb
@@ -2,10 +2,13 @@ class BackfillRequestIssueType < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
 
-  def change
+  def up
   	RequestIssue.unscoped.in_batches do |relation|
       relation.update_all type: "RequestIssue"
       sleep(0.1)
     end
+  end
+
+  def down
   end
 end
