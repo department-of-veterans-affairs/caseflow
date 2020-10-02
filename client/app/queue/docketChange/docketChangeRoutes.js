87a81f6ac478bd5f04787b5d2828b8a8536b1e4e
JC Quirin <2581274+jcq@users.noreply.github.com>
2020-10-02 11:34:27 -0700

    added RecommendDocketSwitchContainer to routes;

--- client/app/queue/docketChange/docketChangeRoutes.js
+++ client/app/queue/docketChange/docketChangeRoutes.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import PageRoute from '../../components/PageRoute';
 
 import TASK_ACTIONS from '../../../constants/TASK_ACTIONS';
+import { RecommendDocketSwitchContainer } from './recommendDocketSwitch/RecommendDocketSwitchContainer';
 
 const PageRoutes = [
   <PageRoute
@@ -11,8 +12,7 @@ const PageRoutes = [
     }`}
     title={`${TASK_ACTIONS.DOCKET_CHANGE_SEND_TO_JUDGE.label} | Caseflow`}
   >
-    {/* Replace with actual component */}
-    <h2>Send to Judge</h2>
+    <RecommendDocketSwitchContainer />
   </PageRoute>,
 
   <PageRoute
