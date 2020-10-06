635361497c34353304f4e81e6b72f375861e374f
Stephen Halliburton <shalliburton@tistatech.com>
2020-10-06 09:55:06 -0700

    Updated jest tests for virtual hearing form

--- client/test/app/hearings/components/ScheduleVeteranForm.test.js
+++ client/test/app/hearings/components/ScheduleVeteranForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import { ScheduleVeteranForm } from 'app/hearings/components/ScheduleVeteranForm';
 import { ReadOnly } from 'app/hearings/components/details/ReadOnly';
@@ -15,6 +15,7 @@ import { AddressLine } from 'app/hearings/components/details/Address';
 import { HearingTime } from 'app/hearings/components/modalForms/HearingTime';
 import { AppellantSection } from 'app/hearings/components/VirtualHearings/AppellantSection';
 import { RepresentativeSection } from 'app/hearings/components/VirtualHearings/RepresentativeSection';
+import { Timezone } from 'app/hearings/components/VirtualHearings/Timezone';
 
 // Set the spies
 const changeSpy = jest.fn();
@@ -47,7 +48,7 @@ describe('ScheduleVeteranForm', () => {
 
   test('Displays hearing form when regional office is selected', () => {
     // Render the address component
-    const scheduleVeteran = shallow(
+    const scheduleVeteran = mount(
       <ScheduleVeteranForm
         goBack={cancelSpy}
         submit={submitSpy}
@@ -70,12 +71,17 @@ describe('ScheduleVeteranForm', () => {
     expect(scheduleVeteran.find(AppealHearingLocationsDropdown)).toHaveLength(1);
     expect(scheduleVeteran.find(HearingDateDropdown)).toHaveLength(1);
     expect(scheduleVeteran.find(HearingTime)).toHaveLength(1);
+    expect(scheduleVeteran.find(AddressLine)).toHaveLength(1);
+
+    expect(scheduleVeteran.find(AppellantSection)).toHaveLength(0);
+    expect(scheduleVeteran.find(RepresentativeSection)).toHaveLength(0);
+
     expect(scheduleVeteran).toMatchSnapshot();
   });
 
   test('Displays Virtual Hearing form fields when type is changed to Virtual', () => {
     // Render the address component
-    const scheduleVeteran = shallow(
+    const scheduleVeteran = mount(
       <ScheduleVeteranForm
         virtual
         goBack={cancelSpy}
@@ -83,10 +89,10 @@ describe('ScheduleVeteranForm', () => {
         onChange={changeSpy}
         appeal={{
           ...amaAppeal,
-          regionalOffice: defaultHearing.regionalOfficeKey,
         }}
         hearing={{
           ...defaultHearing,
+          regionalOffice: defaultHearing.regionalOfficeKey,
           virtualHearing: virtualHearing.virtualHearing
         }}
       />,
@@ -95,8 +101,34 @@ describe('ScheduleVeteranForm', () => {
       }
     );
 
+    // CHeck for virtual hearing fields
     expect(scheduleVeteran.find(AppellantSection)).toHaveLength(1);
     expect(scheduleVeteran.find(RepresentativeSection)).toHaveLength(1);
+
+    // Ensure the Veteran address is not displayed
+    expect(scheduleVeteran.find(ReadOnly).first().
+      find(AddressLine)).toHaveLength(0);
+
+    // Ensure Video-only fields are not displayed
+    expect(scheduleVeteran.find(AppealHearingLocationsDropdown)).toHaveLength(0);
+    expect(scheduleVeteran.find(ReadOnly).first().
+      text()).toEqual('Hearing LocationVirtual');
+
+    // Make sure there are no timezones for non-Central ROs
+    expect(scheduleVeteran.find(Timezone)).toHaveLength(0);
+
+    // Change the regional office to Central
+    scheduleVeteran.setProps({
+      hearing: {
+        ...defaultHearing,
+        regionalOffice: 'C',
+        virtualHearing: virtualHearing.virtualHearing
+      }
+    });
+
+    // Make sure the timezones display after changing to Central
+    expect(scheduleVeteran.find(Timezone)).toHaveLength(2);
+
     expect(scheduleVeteran).toMatchSnapshot();
   });
 
