1638236f223012d3b833a1626c234965362938d7
Sandra Jones <sjones@tistatech.com>
2020-10-08 16:40:37 -0400

    fixed lint errors

--- spec/factories/end_product_updates.rb
+++ spec/factories/end_product_updates.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :end_product_updates do
+  factory :end_product_update do
     original_decision_review_type { "HigherLevelReview" }
-    original_code { "030HLRR" }
-    new_code { "031HLRR" }
+    original_code { "030HLRNR" }
+    new_code { "031HLRNR" }
     user_id { "BVATWARNER" }
 
     traits_for_enum(:status, success: "success", error: "error")
