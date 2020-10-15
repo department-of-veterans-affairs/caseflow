d535be8f3a3925d58a99fd169fd18eaf84985cf2
Stephen Halliburton <shalliburton@tistatech.com>
2020-10-15 13:00:09 -0700

    Resolved merge conflicts

--- app/workflows/tasks_for_appeal.rb
+++ app/workflows/tasks_for_appeal.rb
@@ -58,7 +58,7 @@ class TasksForAppeal
 
   def initialize_hearing_tasks_for_travel_board?
     appeal.is_a?(LegacyAppeal) &&
-      appeal.sanitized_hearing_request_type == :travel_board &&
+      appeal.current_hearing_request_type == :travel_board &&
       user.can_change_hearing_request_type? &&
       appeal.active?
   end
