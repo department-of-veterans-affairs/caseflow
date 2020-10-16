7bb1de27d37f2ff0815f4649820b3a23f861e5b0
JC Quirin <2581274+jcq@users.noreply.github.com>
2020-10-16 13:52:25 -0700

    added factory for `:docket_switch_ruling_task`; added model test for `DocketSwitchRulingTask`;

---
+++ spec/models/tasks/docket_switch_ruling_task_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe DocketSwitchRulingTask, :postgres do
+  let(:task_class) { DocketSwitchRulingTask }
+  let(:judge) { create(:user) }
+
+  describe ".additional_available_actions" do
+    let(:task) { task_class.create!(appeal: root_task.appeal, assigned_to: judge) }
+
+    subject { task.additional_available_actions(judge) }
+
+    context "without docket_change feature toggle" do
+      it "returns the available_actions as defined by Task" do
+        expect(subject).to eq([])
+      end
+    end
+
+    context "with docket_change feature toggle" do
+      before { FeatureToggle.enable!(:docket_change) }
+      after { FeatureToggle.disable!(:docket_change) }
+
+      it "returns the available_actions as defined by Task" do
+        expect(subject).to eq([Constants.TASK_ACTIONS.DOCKET_SWITCH_JUDGE_RULING.to_h])
+      end
+    end
+  end
+end
