9c6aa37ec2c5a9e0ac4d0af8949928469e9cbb56
rubaiyat22 <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-10-16 13:45:36 -0400

    remove old test and write new

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -864,9 +864,16 @@ RSpec.describe TasksController, :all_dbs, type: :controller do
 
       context "when task is ChangeHearingRequestTypeTask" do
         let(:attorney_user) { create(:user) }
+        let(:vacols_case) do
+          create(
+            :case,
+            :assigned,
+            bfcorlid: "0000000000S",
+            bfcurloc: LegacyAppeal::LOCATION_CODES[:schedule_hearing],
+          )
+        end
         let!(:legacy_appeal) do
-          create(:legacy_appeal,
-                 vacols_case: create(:case, :assigned, bfcorlid: "0000000000S", user: attorney_user))
+          create(:legacy_appeal, vacols_case: vacols_case)
         end
 
         let(:task_type) { :changed_hearing_request_type }
@@ -901,6 +908,13 @@ RSpec.describe TasksController, :all_dbs, type: :controller do
             appeal: legacy_appeal
           ).status).to eq(Constants.TASK_STATUSES.completed)
           expect(ScheduleHearingTask.find_by(appeal: legacy_appeal).status).to eq(Constants.TASK_STATUSES.assigned)
+        end
+
+        it "changes the vacols location to CASEFLOW" do
+          expect { subject }
+            .to change { vacols_case.reload.bfcurloc }
+            .from(LegacyAppeal::LOCATION_CODES[:schedule_hearing])
+            .to(LegacyAppeal::LOCATION_CODES[:caseflow])
         end
       end
     end
