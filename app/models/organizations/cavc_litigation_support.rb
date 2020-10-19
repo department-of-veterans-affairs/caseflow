3148c1d1239e044853c1dbaa5bdcf3ae1559a8f1
yoomlam <yoom@navapbc.com>
2020-10-19 10:53:36 -0500

    add more code comments

--- app/models/organizations/cavc_litigation_support.rb
+++ app/models/organizations/cavc_litigation_support.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+##
+# Team that handles CAVC-related tasks.
+# For Litigation Support team members who only work on CAVC cases.
+
 class CavcLitigationSupport < Organization
   def self.singleton
     CavcLitigationSupport.first ||
