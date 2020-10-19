36487b5349ec3cbd3d3069ccfe16e151ac17e558
yoomlam <yoom@navapbc.com>
2020-10-19 09:42:13 -0500

    add test for new org

---
+++ spec/models/organizations/cavc_litigation_support_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe CavcLitigationSupport do
+    describe ".singleton" do
+      it "is named correctly" do
+        expect(CavcLitigationSupport.singleton).to have_attributes(name: "CAVC Litigation Support")
+      end
+    end
+  
+    describe ".users_can_create_mail_task?" do
+      it "should always be true" do
+        expect(CavcLitigationSupport.singleton.users_can_create_mail_task?).to eq(true)
+      end
+    end
+  end
+  
