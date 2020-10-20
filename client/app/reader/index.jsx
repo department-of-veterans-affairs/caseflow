a7677a7b284e258aea0a5bb04c42cbefa678a70c
Stephen Halliburton <shalliburton@tistatech.com>
2020-10-20 08:33:22 -0700

    Updated to use aliasing

--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Internal Dependencies
-import ReaderAppLegacy from './ReaderApp';
+import ReaderAppLegacy from 'app/reader/ReaderApp';
 
 const Reader = (props) => !props.featureToggles?.readerVersion2 && <ReaderAppLegacy {...props} />;
 
