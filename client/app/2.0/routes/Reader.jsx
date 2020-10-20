d87126f3961c7ebaedd34c4c93d0d406ac950058
Stephen Halliburton <shalliburton@tistatech.com>
2020-10-20 08:48:41 -0700

    Added stubs to new files

---
+++ client/app/2.0/routes/Reader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReaderRoutes = () => {
+  return <div />;
+};
+
+export default ReaderRoutes;
--- client/app/reader/index.jsx
+++ client/app/reader/index.jsx
@@ -5,7 +5,11 @@ import PropTypes from 'prop-types';
 // Internal Dependencies
 import ReaderAppLegacy from 'app/reader/ReaderApp';
 
-const Reader = (props) => !props.featureToggles?.readerVersion2 && <ReaderAppLegacy {...props} />;
+// Version 2 does not distinguish by sub-application, so just call it Caseflow
+import Caseflow from 'app/2.0/root';
+
+const Reader = (props) => props.featureToggles?.readerVersion2 ?
+  <Caseflow {...props} /> : <ReaderAppLegacy {...props} />;
 
 Reader.propTypes = {
   featureToggles: PropTypes.objectOf({
