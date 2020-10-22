5148bfb226edceef0959e070f504e646b456b8a5
Sandra Jones <sjones@tistatech.com>
2020-10-21 21:16:45 -0400

    added test

--- spec/models/tasks/docket_switch_denied_task_spec.rb
+++ spec/models/tasks/docket_switch_denied_task_spec.rb
@@ -1,38 +1,25 @@
 # frozen_string_literal: true
 
-describe DocketSwitchMailTask, :postgres do
+describe DocketSwitchDeniedTask, :postgres do
   let(:user) { create(:user) }
   let(:cotb_team) { ClerkOfTheBoard.singleton }
-  let(:root_task) { create(:root_task) }
-  let(:task_class) { DocketSwitchDeniedTask }
-
-  let(:task_actions) do
-    [
-      Constants.TASK_ACTIONS.CHANGE_TASK_TYPE.to_h,
-      Constants.TASK_ACTIONS.ASSIGN_TO_TEAM.to_h,
-      Constants.TASK_ACTIONS.REASSIGN_TO_PERSON.to_h,
-      Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
-      Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
-      Constants.TASK_ACTIONS.CANCEL_TASK.to_h
-    ]
-  end
 
   before do
     cotb_team.add_user(user)
   end
 
-  fdescribe ".available_actions" do
-    let(:attorney_task) { task_class.create!(appeal: root_task.appeal, parent_id: root_task.id, assigned_to: user) }
+  describe ".available_actions" do
+    let(:attorney_task) { create(:ama_judge_decision_review_task) }
 
     subject { attorney_task.available_actions(user) }
 
     context "when the current user is not a member of the Clerk of the Board team" do
       before { allow_any_instance_of(ClerkOfTheBoard).to receive(:user_has_access?).and_return(false) }
-
       context "without docket_change feature toggle" do
-        it "returns the available_actions as defined by Task" do
-          binding.pry
-          expect(subject).to eq(task_actions)
+        it "returns the correct label" do
+          expect(DocketSwitchDeniedTask.new.label).to eq(
+            COPY::DOCKET_SWITCH_DENIED_TASK_LABEL
+          )
         end
       end
 
@@ -41,24 +28,7 @@ describe DocketSwitchMailTask, :postgres do
         after { FeatureToggle.disable!(:docket_change) }
 
         it "returns the available_actions as defined by Task" do
-          expect(subject).to eq(task_actions)
-        end
-      end
-    end
-
-    context "when the current user is a member of the Clerk of the Board team" do
-      context "without docket_change feature toggle" do
-        it "returns the available_actions as defined by Task" do
-          expect(subject).to eq(task_actions)
-        end
-      end
-
-      context "with docket_change feature toggle" do
-        before { FeatureToggle.enable!(:docket_change) }
-        after { FeatureToggle.disable!(:docket_change) }
-
-        it "returns the available_actions as defined by Task" do
-          expect(subject).to eq(task_actions + [Constants.TASK_ACTIONS.DOCKET_SWITCH_DENIED.to_h])
+          expect(subject).to eq []
         end
       end
     end
