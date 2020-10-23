0b3a48be261ea1dd85de58891b83ebfe5506ef0d
Sandra Jones <sjones@tistatech.com>
2020-10-22 20:36:59 -0400

    tests passing

--- spec/models/tasks/docket_switch_granted_task_spec.rb
+++ spec/models/tasks/docket_switch_granted_task_spec.rb
@@ -1,17 +1,34 @@
 # frozen_string_literal: true
 
 describe DocketSwitchGrantedTask, :postgres do
-  let(:user) { create(:user) }
   let(:cotb_team) { ClerkOfTheBoard.singleton }
+  let(:root_task) { create(:root_task) }
+  let(:task_class) { DocketSwitchGrantedTask }
+  let(:judge) { create(:user, :with_vacols_judge_record, full_name: "Judge the First", css_id: "JUDGE_1") }
+
+  let(:attorney) { create(:user, :with_vacols_attorney_record) }
+
+  let(:task_actions) do
+    [Constants.TASK_ACTIONS.REVIEW_DECISION_DRAFT.to_h,
+     Constants.TASK_ACTIONS.ADD_ADMIN_ACTION.to_h,
+     Constants.TASK_ACTIONS.CANCEL_AND_RETURN_TASK.to_h]
+  end
 
   before do
-    cotb_team.add_user(user)
+    cotb_team.add_user(attorney)
   end
 
   describe ".available_actions" do
-    let(:attorney_task) { create(:ama_judge_decision_review_task) }
+    let(:attonery_task) do
+      task_class.create!(
+        appeal: root_task.appeal,
+        parent_id: root_task.id,
+        assigned_to: attorney,
+        assigned_by: judge
+      )
+    end
 
-    subject { attorney_task.available_actions(user) }
+    subject { attonery_task.available_actions(attorney) }
 
     context "when the current user is not a member of the Clerk of the Board team" do
       before { allow_any_instance_of(ClerkOfTheBoard).to receive(:user_has_access?).and_return(false) }
@@ -28,7 +45,24 @@ describe DocketSwitchGrantedTask, :postgres do
         after { FeatureToggle.disable!(:docket_change) }
 
         it "returns the available_actions as defined by Task" do
-          expect(subject).to eq []
+          expect(subject).to eq(task_actions)
+        end
+      end
+    end
+
+    context "when the current user is a member of the Clerk of the Board team" do
+      context "without docket_change feature toggle" do
+        it "returns the available_actions as defined by Task" do
+          expect(subject).to eq(task_actions)
+        end
+      end
+
+      context "with docket_change feature toggle" do
+        before { FeatureToggle.enable!(:docket_change) }
+        after { FeatureToggle.disable!(:docket_change) }
+
+        it "returns the available_actions as defined by Task" do
+          expect(subject).to eq(task_actions + [Constants.TASK_ACTIONS.DOCKET_SWITCH_GRANTED.to_h])
         end
       end
     end
