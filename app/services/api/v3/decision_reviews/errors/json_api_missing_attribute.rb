9c0170fd9c35ba12c7c16b510b7f5a1c5796b544
Steven Templeman <steven.templeman@oddball.io>
2020-10-26 14:32:34 -0700

    Change helper methods to class methods in JsonApiMissingAttribute error class (code climate error)

--- app/services/api/v3/decision_reviews/errors/json_api_missing_attribute.rb
+++ app/services/api/v3/decision_reviews/errors/json_api_missing_attribute.rb
@@ -10,49 +10,49 @@ class Api::V3::DecisionReviews::Errors::JsonApiMissingAttribute < StandardError
   end
 
   def all_errors
-    required_errors = details.select { |detail| required_error?(detail) }
-    other_errors = details.reject { |detail| required_error?(detail) }
+    required_errors = details.select { |detail| self.class.required_error?(detail) }
+    other_errors = details.reject { |detail| self.class.required_error?(detail) }
     errors = []
     unless required_errors.empty?
-      errors.concat(required_errors.map { |error| build_required_errors(error) }.reduce(:concat))
+      errors.concat(required_errors.map { |error| self.class.build_required_errors(error) }.reduce(:concat))
     end
-    errors.concat(other_errors.map { |error| build_error(error) })
+    errors.concat(other_errors.map { |error| self.class.build_error(error) })
     errors.uniq
   end
 
-  private
-
-  def to_human(detail)
-    if detail["type"] == "required"
-      "The property #{to_source(detail)} did not contain the required key #{detail['details']['missing_key']}"
-    else
-      "The property #{detail['data_pointer']} did not match the following requirements #{detail['schema']}"
+  class << self
+    def to_human(detail)
+      if detail["type"] == "required"
+        "The property #{to_source(detail)} did not contain the required key #{detail['details']['missing_key']}"
+      else
+        "The property #{detail['data_pointer']} did not match the following requirements #{detail['schema']}"
+      end
     end
-  end
 
-  def to_source(detail)
-    detail["data_pointer"].empty? ? "/" : detail["data_pointer"]
-  end
-
-  def build_error(detail)
-    {
-      status: 422,
-      detail: to_human(detail),
-      source: to_source(detail)
-    }
-  end
-
-  def build_required_errors(required_error)
-    required_error["details"]["missing_keys"].map do |missing_key|
-      build_error(
-        "type" => "required",
-        "data_pointer" => required_error["data_pointer"],
-        "details" => { "missing_key" => missing_key }
-      )
+    def to_source(detail)
+      detail["data_pointer"].empty? ? "/" : detail["data_pointer"]
     end
-  end
 
-  def required_error?(detail)
-    detail["type"] == "required"
+    def build_error(detail)
+      {
+        status: 422,
+        detail: to_human(detail),
+        source: to_source(detail)
+      }
+    end
+
+    def build_required_errors(required_error)
+      required_error["details"]["missing_keys"].map do |missing_key|
+        build_error(
+          "type" => "required",
+          "data_pointer" => required_error["data_pointer"],
+          "details" => { "missing_key" => missing_key }
+        )
+      end
+    end
+
+    def required_error?(detail)
+      detail["type"] == "required"
+    end
   end
 end
