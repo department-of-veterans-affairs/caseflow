585d7c7aad0f5b922e27a26b26b6132eba4b3210
hschallhorn <hschallhorn@tistatech.com>
2020-10-29 09:56:32 -0400

    Update validator to accept multiple valid task types

--- app/models/validators/parent_task_validator.rb
+++ app/models/validators/parent_task_validator.rb
@@ -9,14 +9,30 @@
 
 class ParentTaskValidator < ActiveModel::Validator
   def validate(record)
-    record.errors.add(:parent, "parent should be a #{options[:task_type].name}") unless correct_parent_type?(record)
+    record.errors.add(:parent, error_message) unless correct_parent_type?(record)
   end
 
   private
 
   def correct_parent_type?(record)
+    if multiple_correct_parent_types?
+      return options[:task_types].map(&:name).include? record.parent&.type
+    end
+
     return true if options[:task_type].nil?
 
     record.parent&.type == options[:task_type].name
+  end
+
+  def multiple_correct_parent_types?
+    options[:task_types].present?
+  end
+
+  def error_message
+    if multiple_correct_parent_types?
+      return "parent should be one of #{options[:task_types].map(&:name).join(', ')}"
+    end
+
+    "parent should be a #{options[:task_type].name}"
   end
 end
---
+++ spec/models/validators/parent_task_validator_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+describe ParentTaskValidator, :postgres do
+  describe "#validate" do
+    let(:parent_task_type) { Task }
+    let(:appeal) { create(:appeal) }
+    let(:assigned_to) { create(:user) }
+    let(:parent_task) { create(:task, type: parent_task_type.name, assigned_to: assigned_to, appeal: appeal) }
+
+    subject { task_class.new(parent: parent_task, assigned_to: assigned_to, appeal: appeal) }
+
+    context "when there is no valid parent task type" do
+      class NoParentTypeTask < Task
+        validates :parent, presence: true, parentTask: { task_type: nil }, on: :create
+      end
+
+      let(:task_class) { NoParentTypeTask }
+
+      it "produces no errors" do
+        expect(subject.valid?).to be true
+        expect(subject.errors.count).to eq 0
+      end
+    end
+
+    context "when there is one valid parent task type" do
+      class OneParentTypeTask < Task
+        validates :parent, presence: true, parentTask: { task_type: FoiaTask }, on: :create
+      end
+
+      let(:task_class) { OneParentTypeTask }
+
+      context "and the parent's task type is invalid" do
+        it "fails validation" do
+          expect(subject.valid?).to be false
+          expect(subject.errors.count).to eq 1
+          expect(subject.errors.messages[:parent].first).to eq "parent should be a FoiaTask"
+        end
+      end
+
+      context "and the parent's task type is valid" do
+        let(:parent_task_type) { FoiaTask }
+
+        it "produces no errors" do
+          expect(subject.valid?).to be true
+          expect(subject.errors.count).to eq 0
+        end
+      end
+    end
+
+    fcontext "when there are multiple valid parent task types" do
+      class MultipleParentTypeTask < Task
+        validates :parent, presence: true, parentTask: { task_types: [FoiaTask, MailTask] }, on: :create
+      end
+
+      let(:task_class) { MultipleParentTypeTask }
+
+      context "and the parent's task type is invalid" do
+        it "fails validation" do
+          expect(subject.valid?).to be false
+          expect(subject.errors.count).to eq 1
+          expect(subject.errors.messages[:parent].first).to eq "parent should be one of FoiaTask, MailTask"
+        end
+      end
+
+      context "and the parent's task type is valid" do
+        let(:parent_task_type) { FoiaTask }
+
+        it "produces no errors" do
+          expect(subject.valid?).to be true
+          expect(subject.errors.count).to eq 0
+        end
+      end
+    end
+  end
+end
