444f2ece4bf8d2b471fdb55590c5e29f66a26d6a
hschallhorn <hschallhorn@tistatech.com>
2020-10-29 10:27:58 -0400

    Code climate

--- spec/models/validators/parent_task_validator_spec.rb
+++ spec/models/validators/parent_task_validator_spec.rb
@@ -2,6 +2,17 @@
 
 describe ParentTaskValidator, :postgres do
   describe "#validate" do
+    shared_examples "valid parent task type" do
+      context "and the parent's task type is valid" do
+        let(:parent_task_type) { FoiaTask }
+
+        it "produces no errors" do
+          expect(subject.valid?).to be true
+          expect(subject.errors.count).to eq 0
+        end
+      end
+    end
+
     let(:parent_task_type) { Task }
     let(:appeal) { create(:appeal) }
     let(:assigned_to) { create(:user) }
@@ -16,10 +27,7 @@ describe ParentTaskValidator, :postgres do
 
       let(:task_class) { NoParentTypeTask }
 
-      it "produces no errors" do
-        expect(subject.valid?).to be true
-        expect(subject.errors.count).to eq 0
-      end
+      it_behaves_like "valid parent task type"
     end
 
     context "when there is one valid parent task type" do
@@ -29,20 +37,13 @@ describe ParentTaskValidator, :postgres do
 
       let(:task_class) { OneParentTypeTask }
 
+      it_behaves_like "valid parent task type"
+
       context "and the parent's task type is invalid" do
         it "fails validation" do
           expect(subject.valid?).to be false
           expect(subject.errors.count).to eq 1
           expect(subject.errors.messages[:parent].first).to eq "should be a FoiaTask"
-        end
-      end
-
-      context "and the parent's task type is valid" do
-        let(:parent_task_type) { FoiaTask }
-
-        it "produces no errors" do
-          expect(subject.valid?).to be true
-          expect(subject.errors.count).to eq 0
         end
       end
     end
@@ -54,20 +55,13 @@ describe ParentTaskValidator, :postgres do
 
       let(:task_class) { MultipleParentTypeTask }
 
+      it_behaves_like "valid parent task type"
+
       context "and the parent's task type is invalid" do
         it "fails validation" do
           expect(subject.valid?).to be false
           expect(subject.errors.count).to eq 1
           expect(subject.errors.messages[:parent].first).to eq "should be one of FoiaTask, MailTask"
-        end
-      end
-
-      context "and the parent's task type is valid" do
-        let(:parent_task_type) { FoiaTask }
-
-        it "produces no errors" do
-          expect(subject.valid?).to be true
-          expect(subject.errors.count).to eq 0
         end
       end
     end
