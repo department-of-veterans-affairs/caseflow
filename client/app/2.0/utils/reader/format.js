12bf887b973c84ed6c543d8a41f1ca2ed76ee340
Stephen Halliburton <shalliburton@tistatech.com>
2020-11-01 15:19:20 -0800

    Fixed merge conflicts

--- client/app/2.0/utils/reader/format.js
+++ client/app/2.0/utils/reader/format.js
@@ -7,6 +7,22 @@ import { formatNameShort } from 'app/util/FormatUtil';
 import { CACHE_TIMEOUT_HOURS, CATEGORIES } from 'store/constants/reader';
 
 /**
+ * Helper Method to Format the Rows for the comments table
+ * @param {array} documents -- The list of documents
+ * @param {object} annotations -- The list of annotations for each document
+ * @param {string} search -- The search query
+ */
+export const formatCommentRows = (documents, annotations, search) => {
+  // This method will be filled in by a later PR, for now just return everything
+  return {
+    documents,
+    annotations,
+    search,
+    rows: []
+  };
+};
+
+/**
  * Helper Method to format the message for the filters in the document list header
  * @param {Object} tag -- The tags being filtered
  * @param {Object} category -- The category being filtered
