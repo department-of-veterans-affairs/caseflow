3cc2f6a364a8bb3389e5bc87361510c67ca0163a
hschallhorn <hschallhorn@tistatech.com>
2020-11-02 13:39:12 -0500

    Only pass needed information to the front end

--- app/models/serializers/work_queue/task_column_serializer.rb
+++ app/models/serializers/work_queue/task_column_serializer.rb
@@ -252,7 +252,9 @@ class WorkQueue::TaskColumnSerializer
     columns = [Constants.QUEUE_CONFIG.COLUMNS.TASK_TYPE.name, Constants.QUEUE_CONFIG.COLUMNS.DAYS_WAITING.name]
 
     if serialize_attribute?(params, columns)
-      object.appeal.latest_informal_hearing_presentation_task
+      task = object.appeal.latest_informal_hearing_presentation_task
+
+      task ? { requested_at: task.assigned_at, recieved_at: task.closed_at } : {}
     end
   end
 
