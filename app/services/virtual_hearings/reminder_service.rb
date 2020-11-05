e1681ab63e6aacaf14066a1675028b4522b861a6
Ferris Tseng <ferris@navapbc.com>
2020-11-05 16:48:50 -0500

    refactor logic about whether or not to send reminder emails to a separate class

---
+++ app/services/virtual_hearings/reminder_service.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+##
+# Service that determines whether or not a send a reminder to the appellant or representative
+# about their virtual hearing.
+
+class VirtualHearings::ReminderService
+  def initialize(virtual_hearing, last_sent_reminder)
+    @virtual_hearing = virtual_hearing
+    @last_sent_reminder = last_sent_reminder
+  end
+
+  def should_send_reminder_email?
+    send_reminder = (
+      should_send_2_day_reminder? ||
+      should_send_3_day_friday_reminder? ||
+      should_send_7_day_reminder?
+    )
+
+    days_until_hearing > 0 && send_reminder
+  end
+
+  private
+
+  attr_reader :virtual_hearing
+  attr_reader :last_sent_reminder
+
+  def should_send_2_day_reminder?
+    days_until_hearing <= 2 && days_from_hearing_day_to_last_sent_reminder > 2
+  end
+
+  def should_send_3_day_friday_reminder?
+    days_until_hearing <= 3 && virtual_hearing.hearing.scheduled_for.monday?
+  end
+
+  def should_send_7_day_reminder?
+    days_until_hearing <= 7 && days_from_hearing_day_to_last_sent_reminder > 7
+  end
+
+  def days_until_hearing
+    (virtual_hearing.hearing.scheduled_for - Time.zone.now.utc) / 1.day
+  end
+
+  def days_from_hearing_day_to_last_sent_reminder
+    # Pick arbitrarily big value if the reminder has never been sent.
+    return 9999 if last_sent_reminder.nil?
+
+    (virtual_hearing.hearing.scheduled_for - last_sent_reminder) / 1.day
+  end
+end
