be4eaf2215cbe23a9a95da53d262a83793144d0b
Alexander <ajspottswood@gmail.com>
2020-11-17 12:03:56 -0500

    add test file

---
+++ client/test/app/queue/AddCavcRemandView.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import AddCavcRemandView from 'app/queue/AddCavcRemandView';
+
+describe('AddCavcRemandView', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+});
