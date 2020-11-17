1b2a1c6a62f460259258733244f332b93f3b3f2b
hschallhorn <hschallhorn@tistatech.com>
2020-11-17 12:52:38 -0500

    Remove smart issues selection, add consistant headers, mild clean up

--- client/app/queue/AddCavcRemandView.jsx
+++ client/app/queue/AddCavcRemandView.jsx
@@ -104,22 +104,6 @@ const AddCavcRemandView = (props) => {
   // populate all of our checkboxes on initial render
   useEffect(() => selectAllIssues(), []);
 
-  // clears all decision issue checkboxes
-  const clearAllIssues = () => {
-    setIssues({});
-  };
-
-  // if the subType is JMR check all decision issue checkboxes
-  const checkSubType = (val) => {
-    if (val === CAVC_REMAND_SUBTYPES.jmr) {
-      selectAllIssues();
-      setSubType(val);
-    } else {
-      clearAllIssues();
-      setSubType(val);
-    }
-  };
-
   const onIssueChange = (evt) => {
     setIssues({ ...issues, [evt.target.name]: evt.target.checked });
   };
@@ -161,77 +145,76 @@ const AddCavcRemandView = (props) => {
       <h1>{COPY.ADD_CAVC_PAGE_TITLE}</h1>
       <p>{COPY.ADD_CAVC_DESCRIPTION}</p>
       <TextField
-        name={<h4>{COPY.CAVC_DOCKET_NUMBER_LABEL}</h4>}
+        name={<h3>{COPY.CAVC_DOCKET_NUMBER_LABEL}</h3>}
         value={docketNumber}
-        onChange={setDocketNumber} />
+        onChange={setDocketNumber}
+      />
       <RadioField
-        label={<h4 id="horizontal-radio">{COPY.CAVC_ATTORNEY_LABEL}</h4>}
+        label={<h3 id="horizontal-radio">{COPY.CAVC_ATTORNEY_LABEL}</h3>}
         name="attorney-options"
         options={attorneyOptions}
         value={attorney}
-        onChange={(val) => setAttorney(val)} />
+        onChange={(val) => setAttorney(val)}
+      />
       <SearchableDropdown
         name="judge-dropdown"
         label={<h3>{COPY.CAVC_JUDGE_LABEL}</h3>}
         searchable
         value={judge}
         onChange={(val) => setJudge(val)}
-        options={judgeOptions} />
+        options={judgeOptions}
+      />
       <RadioField
         label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_TYPE_LABEL}</h3>}
         name="type-options"
         options={typeOptions}
         value={type}
-        onChange={(val) => setType(val)} />
+        onChange={(val) => setType(val)}
+      />
       {type === CAVC_DECISION_TYPES.remand &&
       <RadioField
         label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_SUB_TYPE_LABEL}</h3>}
         name="sub-type-options"
         options={subTypeOptions}
         value={subType}
-        onChange={(val) => checkSubType(val)} /> }
-      <h4 {...labelStyling}>{COPY.CAVC_COURT_DECISION_DATE}</h4>
+        onChange={(val) => setSubType(val)}
+      /> }
+      <h3 {...labelStyling}>{COPY.CAVC_COURT_DECISION_DATE}</h3>
       <DateSelector
         type="date"
         value={decisionDate}
-        onChange={(val) => setDecisionDate(val)} />
-      {subType === CAVC_REMAND_SUBTYPES.mdr &&
-        <Alert
-          type="info"
-          classname="usa-alert-slim"
-          message={COPY.CAVC_MDR_MESSAGE}
-          styling={alertStyling}
-          lowerMargin />}
-      <>
-        <h4 {...labelStyling}>{COPY.CAVC_JUDGEMENT_DATE}</h4>
-        <DateSelector
-          type="date"
-          value={judgementDate}
-          onChange={(val) => setJudgementDate(val)} />
-        <h4 {...labelStyling}>{COPY.CAVC_MANDATE_DATE}</h4>
-        <DateSelector
-          type="date"
-          value={mandateDate}
-          onChange={(val) => setMandateDate(val)} />
-      </>
+        onChange={(val) => setDecisionDate(val)}
+      />
+      <h3 {...labelStyling}>{COPY.CAVC_JUDGEMENT_DATE}</h3>
+      <DateSelector
+        type="date"
+        value={judgementDate}
+        onChange={(val) => setJudgementDate(val)}
+      />
+      <h3 {...labelStyling}>{COPY.CAVC_MANDATE_DATE}</h3>
+      <DateSelector
+        type="date"
+        value={mandateDate}
+        onChange={(val) => setMandateDate(val)}
+      />
       <h3>{COPY.CAVC_ISSUES_LABEL}</h3>
-      {subType !== CAVC_REMAND_SUBTYPES.jmr && (!selectedIssues.length ||
-       selectedIssues.length === issueOptions.length) && <Button
+      <Button
         name={selectedIssues.length ? 'Unselect all' : 'Select all'}
         styling={buttonStyling}
         linkStyling
-        onClick={selectAllIssues} />}
+        onClick={selectAllIssues}
+      />
       <CheckboxGroup
         options={issueOptions()}
         values={issues}
         onChange={(val) => onIssueChange(val)}
-        disableAll={type === CAVC_DECISION_TYPES.remand && subType === CAVC_REMAND_SUBTYPES.jmr} />
-      {subType === CAVC_REMAND_SUBTYPES.jmr && <i>*Joint Motion for Remand (JMR) automatically selects all issues</i>}
+      />
       <TextareaField
         label={<h3 {...labelStyling}>{COPY.CAVC_INSTRUCTIONS_LABEL}</h3>}
         name="context-and-instructions-textBox"
         value={text}
-        onChange={(val) => setText(val)} />
+        onChange={(val) => setText(val)}
+      />
     </QueueFlowPage>
   );
 };
