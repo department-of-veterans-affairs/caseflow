9ae79d8f841d0c384ce4a066ce75089532b8a556
hschallhorn <hschallhorn@tistatech.com>
2020-11-17 13:00:28 -0500

    Break out components

--- client/app/queue/AddCavcRemandView.jsx
+++ client/app/queue/AddCavcRemandView.jsx
@@ -17,12 +17,10 @@ import DateSelector from '../components/DateSelector';
 import CheckboxGroup from '../components/CheckboxGroup';
 import TextareaField from '../components/TextareaField';
 import Button from '../components/Button';
-import Alert from '../components/Alert';
 import SearchableDropdown from '../components/SearchableDropdown';
 
 const labelStyling = css({ marginTop: '2.5rem' });
 const buttonStyling = css({ paddingLeft: '0' });
-const alertStyling = css({ width: '52rem' });
 
 const judgeOptions = [].concat(
   _.map(CAVC_JUDGE_FULL_NAMES, (value) => ({
@@ -135,6 +133,94 @@ const AddCavcRemandView = (props) => {
       });
   };
 
+  const docketNumberField = <TextField
+    name={<h3>{COPY.CAVC_DOCKET_NUMBER_LABEL}</h3>}
+    value={docketNumber}
+    onChange={setDocketNumber}
+  />;
+
+  const representedField = <RadioField
+    label={<h3 id="horizontal-radio">{COPY.CAVC_ATTORNEY_LABEL}</h3>}
+    name="attorney-options"
+    options={attorneyOptions}
+    value={attorney}
+    onChange={(val) => setAttorney(val)}
+  />;
+
+  const judgeField = <SearchableDropdown
+    name="judge-dropdown"
+    label={<h3>{COPY.CAVC_JUDGE_LABEL}</h3>}
+    searchable
+    value={judge}
+    onChange={(val) => setJudge(val)}
+    options={judgeOptions}
+  />;
+
+  const typeField = <RadioField
+    label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_TYPE_LABEL}</h3>}
+    name="type-options"
+    options={typeOptions}
+    value={type}
+    onChange={(val) => setType(val)}
+  />;
+
+  const remandTypeField = <RadioField
+    label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_SUB_TYPE_LABEL}</h3>}
+    name="sub-type-options"
+    options={subTypeOptions}
+    value={subType}
+    onChange={(val) => setSubType(val)}
+  />;
+
+  const decisionField = <React.Fragment>
+    <h3 {...labelStyling}>{COPY.CAVC_COURT_DECISION_DATE}</h3>
+    <DateSelector
+      type="date"
+      value={decisionDate}
+      onChange={(val) => setDecisionDate(val)}
+    />
+  </React.Fragment>;
+
+  const judgementField = <React.Fragment>
+    <h3 {...labelStyling}>{COPY.CAVC_JUDGEMENT_DATE}</h3>
+    <DateSelector
+      type="date"
+      value={judgementDate}
+      onChange={(val) => setJudgementDate(val)}
+    />
+  </React.Fragment>;
+
+  const mandateField = <React.Fragment>
+    <h3 {...labelStyling}>{COPY.CAVC_MANDATE_DATE}</h3>
+    <DateSelector
+      type="date"
+      value={mandateDate}
+      onChange={(val) => setMandateDate(val)}
+    />
+  </React.Fragment>;
+
+  const issuesField = <React.Fragment>
+    <h3>{COPY.CAVC_ISSUES_LABEL}</h3>
+    <Button
+      name={selectedIssues.length ? 'Unselect all' : 'Select all'}
+      styling={buttonStyling}
+      linkStyling
+      onClick={selectAllIssues}
+    />
+    <CheckboxGroup
+      options={issueOptions()}
+      values={issues}
+      onChange={(val) => onIssueChange(val)}
+    />
+  </React.Fragment>;
+
+  const instructionsField = <TextareaField
+    label={<h3 {...labelStyling}>{COPY.CAVC_INSTRUCTIONS_LABEL}</h3>}
+    name="context-and-instructions-textBox"
+    value={text}
+    onChange={(val) => setText(val)}
+  />;
+
   return (
     <QueueFlowPage
       appealId={appealId}
@@ -144,77 +230,16 @@ const AddCavcRemandView = (props) => {
       {...otherProps} >
       <h1>{COPY.ADD_CAVC_PAGE_TITLE}</h1>
       <p>{COPY.ADD_CAVC_DESCRIPTION}</p>
-      <TextField
-        name={<h3>{COPY.CAVC_DOCKET_NUMBER_LABEL}</h3>}
-        value={docketNumber}
-        onChange={setDocketNumber}
-      />
-      <RadioField
-        label={<h3 id="horizontal-radio">{COPY.CAVC_ATTORNEY_LABEL}</h3>}
-        name="attorney-options"
-        options={attorneyOptions}
-        value={attorney}
-        onChange={(val) => setAttorney(val)}
-      />
-      <SearchableDropdown
-        name="judge-dropdown"
-        label={<h3>{COPY.CAVC_JUDGE_LABEL}</h3>}
-        searchable
-        value={judge}
-        onChange={(val) => setJudge(val)}
-        options={judgeOptions}
-      />
-      <RadioField
-        label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_TYPE_LABEL}</h3>}
-        name="type-options"
-        options={typeOptions}
-        value={type}
-        onChange={(val) => setType(val)}
-      />
-      {type === CAVC_DECISION_TYPES.remand &&
-      <RadioField
-        label={<h3 {...labelStyling} id="vertical-radio">{COPY.CAVC_SUB_TYPE_LABEL}</h3>}
-        name="sub-type-options"
-        options={subTypeOptions}
-        value={subType}
-        onChange={(val) => setSubType(val)}
-      /> }
-      <h3 {...labelStyling}>{COPY.CAVC_COURT_DECISION_DATE}</h3>
-      <DateSelector
-        type="date"
-        value={decisionDate}
-        onChange={(val) => setDecisionDate(val)}
-      />
-      <h3 {...labelStyling}>{COPY.CAVC_JUDGEMENT_DATE}</h3>
-      <DateSelector
-        type="date"
-        value={judgementDate}
-        onChange={(val) => setJudgementDate(val)}
-      />
-      <h3 {...labelStyling}>{COPY.CAVC_MANDATE_DATE}</h3>
-      <DateSelector
-        type="date"
-        value={mandateDate}
-        onChange={(val) => setMandateDate(val)}
-      />
-      <h3>{COPY.CAVC_ISSUES_LABEL}</h3>
-      <Button
-        name={selectedIssues.length ? 'Unselect all' : 'Select all'}
-        styling={buttonStyling}
-        linkStyling
-        onClick={selectAllIssues}
-      />
-      <CheckboxGroup
-        options={issueOptions()}
-        values={issues}
-        onChange={(val) => onIssueChange(val)}
-      />
-      <TextareaField
-        label={<h3 {...labelStyling}>{COPY.CAVC_INSTRUCTIONS_LABEL}</h3>}
-        name="context-and-instructions-textBox"
-        value={text}
-        onChange={(val) => setText(val)}
-      />
+      {docketNumberField}
+      {representedField}
+      {judgeField}
+      {typeField}
+      {type === CAVC_DECISION_TYPES.remand && remandTypeField }
+      {decisionField}
+      {judgementField}
+      {mandateField}
+      {issuesField}
+      {instructionsField}
     </QueueFlowPage>
   );
 };
