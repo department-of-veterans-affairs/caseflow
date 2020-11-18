65675d91a45b12f214d654fb4654f410083f2783
hschallhorn <hschallhorn@tistatech.com>
2020-11-17 20:52:58 -0500

    Get storybook entry up and running

--- client/app/queue/AddCavcRemandView.stories.js
+++ client/app/queue/AddCavcRemandView.stories.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+
+import ReduxBase from '../components/ReduxBase';
+import reducer from './reducers';
 
 import AddCavcRemandView from './AddCavcRemandView';
 
@@ -7,6 +12,45 @@ export default {
   component: AddCavcRemandView,
 };
 
-const Template = (args) => <AddCavcRemandView {...args} />;
+const appealId = '1234';
+const decisionIssues = [{ description: 'This is the description of the decision issue.' }];
+const highlightFormItems = false;
+const backEndError = null;
+const initialState = {
+  queue: {
+    appealDetails: {
+      [appealId]: {
+        decisionIssues
+      },
+    },
+    stagedChanges: {
+      appeals: {}
+    }
+  },
+  ui: {
+    highlightFormItems,
+    messages: {
+      error: backEndError
+    },
+    modals: {
+      cancelCheckout: false
+    },
+    featureToggles: {
+      special_issues_revamp: false
+    },
+    saveState: {
+      savePending: false,
+      saveSuccessful: false
+    }
+  }
+};
+
+const Template = (args) => (
+  <BrowserRouter>
+    <ReduxBase initialState={initialState} store={createStore(reducer, initialState)} reducer={reducer}>
+      <AddCavcRemandView appealId={appealId} {...args} />
+    </ReduxBase>
+  </BrowserRouter>
+);
 
 export const Default = Template.bind({});
