5632c8ef1f77e98a86589b4080c4c7fefb4c5a24
Sally Maki <sallymaki@gmail.com>
2020-11-23 17:02:46 -0800

    Initial code - modal working, feature test

--- client/app/intake/pages/addIssues.jsx
+++ client/app/intake/pages/addIssues.jsx
@@ -40,6 +40,7 @@ import {
 } from '../actions/addIssues';
 import COPY from '../../../COPY';
 import { EditClaimLabelModal } from '../../intakeEdit/components/EditClaimLabelModal';
+import { ConfirmClaimLabelModal } from '../../intakeEdit/components/ConfirmClaimLabelModal';
 
 class AddIssuesPage extends React.Component {
   constructor(props) {
@@ -145,20 +146,35 @@ class AddIssuesPage extends React.Component {
   openEditClaimLabelModal = (endProductCode) => {
     this.setState({
       showEditClaimLabelModal: true,
-      selectedEPCode: endProductCode
+      selectedEpCode: endProductCode
     });
   }
   closeEditClaimLabelModal = () => {
     this.setState({
       showEditClaimLabelModal: false,
-      selectedEPCode: null
+      showConfirmClaimLabelModal: false,
+      previousEpCode: null,
+      selectedEpCode: null
     });
   }
-  // eslint-disable-next-line no-unused-vars
-  handleEditClaimLabel = (newCode) => {
-    // TODO: save the updated code
 
-    this.closeEditClaimLabelModal();
+  // eslint-disable-next-line no-unused-vars
+  handleEditClaimLabel = (data) => {
+    this.setState({
+      showEditClaimLabelModal: false,
+      showConfirmClaimLabelModal: true,
+      previousEpCode: data.oldCode,
+      selectedEpCode: data.newCode
+    });
+  }
+
+  submitClaimLabelEdit = () => {
+    console.log(`Submitting request to switch from ${this.state.previousEpCode} to ${this.state.selectedEpCode}...`);
+    this.setState({
+      showConfirmClaimLabelModal: false,
+      previousEpCode: null,
+      selectedEpCode: null
+    });
   }
 
   render() {
@@ -370,9 +386,17 @@ class AddIssuesPage extends React.Component {
         )}
         {this.state.showEditClaimLabelModal && (
           <EditClaimLabelModal
-            existingEpCode={this.state.selectedEPCode}
+            selectedEpCode={this.state.selectedEpCode}
             onCancel={this.closeEditClaimLabelModal}
             onSubmit={this.handleEditClaimLabel}
+          />
+        )}
+        {this.state.showConfirmClaimLabelModal && (
+          <ConfirmClaimLabelModal
+            previousEpCode={this.state.previousEpCode}
+            newEpCode={this.state.selectedEpCode}
+            onCancel={this.closeEditClaimLabelModal}
+            onSubmit={this.submitClaimLabelEdit}
           />
         )}
         <h1 className="cf-txt-c">{messageHeader}</h1>
---
+++ client/app/intakeEdit/components/ConfirmClaimLabelModal.jsx
@@ -0,0 +1,52 @@
+import React, { useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'app/components/Modal';
+import {
+  CONFIRM_CLAIM_LABEL_MODAL_TITLE,
+  CONFIRM_CLAIM_LABEL_MODAL_BODY,
+} from 'app/../COPY';
+
+import EP_CLAIM_TYPES from 'constants/EP_CLAIM_TYPES';
+
+export const ConfirmClaimLabelModal = ({ previousEpCode, newEpCode, onCancel, onSubmit }) => {
+  const buttons = [
+    {
+      classNames: ['cf-modal-link', 'cf-btn-link'],
+      name: 'Cancel',
+      onClick: onCancel,
+    },
+    {
+      classNames: ['usa-button', 'usa-button-primary'],
+      name: 'Confirm',
+      onClick: () => onSubmit?.({
+        previousEpCode: previousEpCode,
+        newEpCode: newEpCode
+      }),
+    },
+  ];
+
+  return (
+    <Modal
+      title={CONFIRM_CLAIM_LABEL_MODAL_TITLE}
+      buttons={buttons}
+      closeHandler={onCancel}
+      id="confirm-claim-label-modal"
+    >
+      <div style={{ marginBottom: '24px' }}>
+        <strong>
+          Previous label: {EP_CLAIM_TYPES[previousEpCode]?.official_label}
+          <br />
+          New label: {EP_CLAIM_TYPES[newEpCode]?.official_label}
+        </strong>
+      </div>
+      <p>{CONFIRM_CLAIM_LABEL_MODAL_BODY}</p>
+    </Modal>
+  );
+};
+
+ConfirmClaimLabelModal.propTypes = {
+  previousEpCode: PropTypes.string.isRequired,
+  newEpCode: PropTypes.string.isRequired,
+  onCancel: PropTypes.func,
+  onSubmit: PropTypes.func.isRequired,
+};
