373c0884dbe5aca6c3902356e696479e0800e613
hschallhorn <hschallhorn@tistatech.com>
2020-11-25 17:01:39 -0500

    Add testing validation for docket number

--- client/test/app/queue/AddCavcRemandView.test.js
+++ client/test/app/queue/AddCavcRemandView.test.js
@@ -6,6 +6,8 @@ import { amaAppeal } from 'test/data/appeals';
 
 import AddCavcRemandView from 'app/queue/AddCavcRemandView';
 
+import COPY from 'COPY';
+
 describe('AddCavcRemandView', () => {
   beforeEach(() => jest.clearAllMocks());
 
@@ -25,6 +27,34 @@ describe('AddCavcRemandView', () => {
     expect(cavcForm).toMatchSnapshot();
   });
 
-  // describe('form validations', () => {});
+  describe('form validations', () => {
+    const errorClass = '.usa-input-error-message';
+
+    const validationErrorShows = (component, errorMessage) => {
+      component.find('#button-next-button').simulate('click');
+
+      return component.find(errorClass).findWhere((node) => node.props().children === errorMessage).length > 0;
+    };
+
+    describe('docket number validations', () => {
+      const error = COPY.CAVC_DOCKET_NUMBER_ERROR;
+
+      it('shows error on blank docket number', () => {
+        const cavcForm = setup({ appealId });
+
+        expect(validationErrorShows(cavcForm, error)).toBeTruthy();
+      });
+
+      it('shows error on incorrectly formatted docket number', () => {
+        const cavcForm = setup({ appealId });
+
+        cavcForm.find('#docket-number').simulate('change', { target: { value: 'bad docket number' } });
+
+        expect(validationErrorShows(cavcForm, error)).toBeTruthy();
+      });
+    });
+  });
+
   // describe('deselecting "remand" hides remand subtypes', () => {});
+  // describe('all issues are selected on page load', () => {});
 });
