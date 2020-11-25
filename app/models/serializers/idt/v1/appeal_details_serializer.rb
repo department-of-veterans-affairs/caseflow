1cb39c8cd0365013e8d7eda555667bccb8bdbac8
Sally Maki <sallymaki@gmail.com>
2020-11-24 16:08:09 -0800

    Add model test for new functionality

--- app/models/serializers/idt/v1/appeal_details_serializer.rb
+++ app/models/serializers/idt/v1/appeal_details_serializer.rb
@@ -48,7 +48,7 @@ class Idt::V1::AppealDetailsSerializer
           first_name: claimant.first_name,
           middle_name: claimant.middle_name,
           last_name: claimant.last_name,
-          full_name: claimant.first_name.present? ? nil : claimant.name&.upcase,
+          full_name: claimant.last_name.present? ? nil : claimant.name&.upcase,
           name_suffix: "",
           address: address,
           representative: claimant.representative_name ? representative : nil
