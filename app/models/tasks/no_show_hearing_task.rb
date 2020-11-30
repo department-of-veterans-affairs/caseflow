3aaebe09d67f77e971c28645d7036c3e69b64b04
rubaiyat22 <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-11-30 17:33:43 -0500

    write comments for NoShowHearingTask

--- app/models/tasks/no_show_hearing_task.rb
+++ app/models/tasks/no_show_hearing_task.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
+##
 # Task created after an appellant no-shows for a hearing. Gives the hearings team the options to decide how to handle
 # the no-show hearing after the judge indicates that the appellant no-showed.
+#
+# Task is created as a child of AssignHearingDispositionTask with a TimedHoldTask which is set to expire after
+# DAYS_ON_HOLD days. Before the task expires, users can manually complete this task, postpone hearing, or create a
+# ChangeHearingDispositionTask.
+#
+# If DAYS_ON_HOLD as passed, TaskTimerJob cleans up the TimedHoldTask and automatically completes NoShowHearingTask.
+#
+# Completion/cancellation of  NoShowHearingTaskcan trigger closing of parent AssignHearingDispositionTask and
+# if AssignHearingDispositionTask was the last open task of grandparent HearingTask, either of the following can happen:
+#  - If appeal is AMA, create an EvidenceSubmissionWindowTask as child of HearingTask OR
+#  - If appeal is Legacy, route location according to logic in HearingTask#update_legacy_appeal_location
+##
 class NoShowHearingTask < Task
   before_validation :set_assignee
 
