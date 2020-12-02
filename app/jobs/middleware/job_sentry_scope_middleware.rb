ba9146ae6c34c09ec94eaf48803bec491f0c9b51
Ferris Tseng <ferris@navapbc.com>
2020-12-02 12:26:32 -0500

    add .new!!

--- app/jobs/middleware/job_sentry_scope_middleware.rb
+++ app/jobs/middleware/job_sentry_scope_middleware.rb
@@ -10,19 +10,19 @@ class JobSentryScopeMiddleware
     job_id = body["job_id"]
     msg_id = sqs_msg.message_id
     job_start_time = Time.zone.now
-    #job_info = AwsJobLogHelper::JobInfo(
-    #  job_class,
-    #  msg_id,
-    #  start_time: job_start_time,
-    #  end_time: job_start_time + 1.day # Assume the job will complete in a day
-    #)
+    job_info = AwsJobLogHelper::JobInfo.new(
+      job_class,
+      msg_id,
+      start_time: job_start_time,
+      end_time: job_start_time + 1.day # Assume the job will complete in a day
+    )
 
     tags = {
       application: job_class.try(:app_name)
     }
     # Note: job class and job id are already sent to Sentry by default.
     context = {
-      #aws_log_url: job_info.url,
+      aws_log_url: job_info.url,
       msg_id: msg_id,
       start_time: job_start_time
     }
