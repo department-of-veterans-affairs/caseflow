794205f37d17c69961ebecf23fa2e27c1a9c23dd
pbradn <pbradin@tistatech.com>
2020-12-02 09:25:13 -0500

    update user model for line too long

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,7 +91,8 @@ class User < CaseflowRecord # rubocop:disable Metrics/ClassLength
   end
 
   def can_view_edit_nod_date?
-    (BvaIntake.singleton.users.include?(self) || ClerkOfTheBoard.singleton.users.include?(self)) && FeatureToggle.enabled?(:edit_nod_date, user: self)
+    (BvaIntake.singleton.users.include?(self) || ClerkOfTheBoard.singleton.users.include?(self)) &&
+      FeatureToggle.enabled?(:edit_nod_date, user: self)
   end
 
   def can_vso_hearing_schedule?
