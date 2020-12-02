5a57e0f77dc3ce75a295e553709107cfe3c5cf8c
Matthew Kerns <matthewkerns@TST-C02YV319LVDC.local>
2020-12-02 16:06:04 -0500

    Added Navigation Bar Story Controls

--- client/app/components/NavigationBar.stories.js
+++ client/app/components/NavigationBar.stories.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 import NavigationBar from './NavigationBar';
 import PerformanceDegradationBanner from './PerformanceDegradationBanner';
 import { LOGO_COLORS } from '../constants/AppConstants';
+import { StaticRouter, Route } from 'react-router-dom';
+import ReduxBase from '../components/ReduxBase';
+import queueReducer, { initialState } from '../queue/reducers';
 
-export default {
-  title: 'Commons/Components/Layout/NavigationBar',
-  component: NavigationBar,
-  decorators: [() => (<MemoryRouter><NavigationBar /></MemoryRouter>)]
-};
-
-const Template = (args) => <NavigationBar {...args} />;
-
-export const Basic = Template.bind({});
-Basic.args = {
-  appName: 'Caseflow App Name',
+const storyState = {
+  appName: '',
   key: '/queue',
   extraBanner: <PerformanceDegradationBanner showBanner={false} />,
-  logoProps: {
-    accentColor: LOGO_COLORS.INTAKE.ACCENT,
-    overlapColor: LOGO_COLORS.INTAKE.OVERLAP
-  },
-  userDisplayName: 'Rick Sanchez',
+  userDisplayName: '',
   dropdownUrls: [{
     title: 'Queue',
     link: '/queue',
@@ -29,5 +18,54 @@ Basic.args = {
     border: false
   }],
   topMessage: null,
-  defaultUrl: '/'
+  defaultUrl: '/path/to/desired/route'
+
 };
+
+const RouterDecorator = (storyFn) => (
+  <StaticRouter
+    location={{
+      pathname: '/',
+    }}
+  >
+    <Route
+      path="/"
+    >
+      {storyFn()}
+    </Route>
+  </StaticRouter>
+);
+
+const ReduxDecorator = (storyFn) => (
+  <ReduxBase
+    reducer={queueReducer}
+    initialState={{ queue: { ...initialState, ...storyState } }}
+  >
+    {storyFn()}
+  </ReduxBase>
+);
+
+export default {
+  title: 'Commons/Components/Layout/NavigationBar',
+  component: NavigationBar,
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {
+    appName: 'Navigation Bar Demo',
+    userDisplayName: 'Rick Sanchez',
+    logoProps: {
+      accentColor: LOGO_COLORS.QUEUE.ACCENT,
+      overlapColor: LOGO_COLORS.QUEUE.OVERLAP
+    }
+  },
+  argTypes: {
+    appName: { description: 'Name of application.', control: 'text' },
+    userDisplayName: { description: 'Display name of the current User.', control: 'text' },
+    logoProps: { description: 'Props passed down to the `CaseflowLogo` component.' }
+  },
+  decorators: [RouterDecorator, ReduxDecorator],
+};
+const Template = (args) => <NavigationBar {...args} />;
+
+export const Basic = Template.bind({});
