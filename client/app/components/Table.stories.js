36e1f63c4ec2c99ed429b550cd48766815d3074d
Kat Tipton <kathryn.tipton@gmail.com>
2020-12-03 09:50:33 -0500

    Adds Table storybook at parity with Styleguide

--- client/app/components/Table.stories.js
+++ client/app/components/Table.stories.js
@@ -58,6 +58,14 @@ export default {
   component: Table,
 };
 
+export const Minimal = (args) => <Table {...args} />;
+Minimal.args = {
+  columns,
+  rowObjects,
+  summary: 'Example minimal table',
+  slowReRendersAreOk: true
+};
+
 export const Basic = (args) => {
   const setRowClassNames = (rowObject) => {
     return rowObject.likesSports ? 'cf-success' : '';
@@ -70,29 +78,34 @@ Basic.args = {
   rowObjects,
   summary: 'Example table',
   slowReRendersAreOk: true
-}
+};
 Basic.parameters = {
   docs: {
     description: {
       story:
-        'We use tables to display information across Caseflow. Most frequently they are used in users Queues but we sometimes use them to help users accomplish a specific task. For aesthetic purposes, tables in Caseflow are borderless.<br><br>Table headings should be bold and with a white background.<br><br> Often tables will contain an primary action a user can take on the table item. These actions should always be placed in the right most column of the table and should be right aligned with the edge of the table.',
+      `We use tables to display information across Caseflow. Most frequently they are used in users Queues but we
+      sometimes use them to help users accomplish a specific task. For aesthetic purposes, tables in Caseflow are
+      borderless.<br><br> Table headings should be bold and with a white background.<br><br> Often tables will contain
+      an primary action a user can take on the table item. These actions should always be placed in the right most
+      column of the table and should be right aligned with the edge of the table.`
     },
   },
 };
 
-
-export const Queue = (args) => <Table {...args} />
+export const Queue = (args) => <Table {...args} />;
 Queue.args = {
   columns: columnsWithAction,
   rowObjects,
   summary: 'Example queue table',
   slowReRendersAreOk: true
-}
+};
 Queue.parameters = {
   docs: {
     description: {
       story:
-        'Tables are most frequently used in users\' Queues or a list of work items for a user to take action on. Queues are shown in the standard App Canvas as tables. A distinct feature of queue tables is the right-aligned actionable link, such as "Assign >>," located on the far right column.',
+        `Tables are most frequently used in users' Queues or a list of work items for a user to take action on. Queues
+        are shown in the standard App Canvas as tables. A distinct feature of queue tables is the right-aligned
+        actionable link, such as "Assign >>," located on the far right column.`
     },
   },
 };
