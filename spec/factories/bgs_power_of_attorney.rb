2eacceb0667fbea2534890f326241b5ad4987045
rubaiyat22 <Rubaiyat.Rashid51@myhunter.cuny.edu>
2020-12-04 11:14:42 -0500

    fix reamining tests and move caching to bgs factory

--- spec/factories/bgs_power_of_attorney.rb
+++ spec/factories/bgs_power_of_attorney.rb
@@ -6,5 +6,18 @@ FactoryBot.define do
     sequence(:poa_participant_id)
     representative_name { "POA Name" }
     representative_type { "VSO" }
+
+    trait :with_name_cached do
+      transient do
+        appeal { nil }
+      end
+      after(:create) do |bgs_power_of_attorney, _evaluator|
+        CachedAppeal.create!(
+          appeal_id: _evaluator.appeal.id,
+          appeal_type: _evaluator.appeal.class.name,
+          power_of_attorney_name: _evaluator.appeal.reload.representative_name
+        )
+      end
+    end
   end
 end
--- spec/feature/hearings/assign_hearings_table_spec.rb
+++ spec/feature/hearings/assign_hearings_table_spec.rb
@@ -267,14 +267,10 @@ RSpec.feature "Assign Hearings Table" do
       Appeal.all.each_with_index do |appeal, idx|
         create(
           :bgs_power_of_attorney,
+          :with_name_cached,
+          appeal: appeal,
           claimant_participant_id: appeal.claimant.participant_id,
           representative_name: "Attorney #{idx}"
-        )
-        create(
-          :cached_appeal,
-          appeal_id: appeal.id,
-          appeal_type: Appeal.name,
-          power_of_attorney_name: appeal.reload.representative_name
         )
       end
     end
