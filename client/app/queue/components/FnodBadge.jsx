d646f1086ce93d1a06bf44d95c714fc3ef7492b6
Meckila Britt <meckilabritt@navapbc.com>
2020-12-17 14:05:11 -0800

    Fix lint errors

--- client/app/queue/components/FnodBadge.jsx
+++ client/app/queue/components/FnodBadge.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import * as React from 'react';
-import { css } from 'glamor';
 
 import { COLORS } from '../../constants/AppConstants';
 import Badge from './Badge';
