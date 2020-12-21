c30acd0159c7417d05c87d27a45e52950fea08d2
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2020-12-21 09:58:32 -0800

    don't use subject to call the tested method

--- spec/services/virtual_hearings/sequence_conference_id_spec.rb
+++ spec/services/virtual_hearings/sequence_conference_id_spec.rb
@@ -2,16 +2,14 @@
 
 describe VirtualHearings::SequenceConferenceId do
   context ".next" do
-    subject { described_class.next }
-
     it "returns values in sequence" do
-      first_value = subject
-      second_value = subject
+      first_value = described_class.next
+      second_value = described_class.next
       expect(first_value.to_i).to eq(second_value.to_i - 1)
     end
 
     it "returns a seven character string starting with '0'" do
-      check_value = subject
+      check_value = described_class.next
       expect(check_value.class).to eq String
       expect(check_value.length).to eq 7
       expect(check_value[0]).to eq "0"
