8cf2342ac656b06c386ec130ed0dcfd328154612
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2020-12-21 15:27:49 -0800

    test for sequence cycle

--- spec/services/virtual_hearings/sequence_conference_id_spec.rb
+++ spec/services/virtual_hearings/sequence_conference_id_spec.rb
@@ -14,5 +14,14 @@ describe VirtualHearings::SequenceConferenceId do
       expect(check_value.length).to eq 7
       expect(check_value[0]).to eq "0"
     end
+
+    it "cycles when the last number is reached" do
+      sequence_name = VirtualHearings::SequenceConferenceId::SEQUENCE_NAME
+      ActiveRecord::Base.connection.execute "SELECT setval('#{sequence_name}', 9999998)"
+      first_value = described_class.next
+      expect(first_value).to eq "9999999"
+      second_value = described_class.next
+      expect(second_value).to eq "0000001"
+    end
   end
 end
