2d918aaf16a1869d583fc078671e8d2050b46305
Brian Gantick <briangantick@navapbc.com>
2020-12-22 11:18:31 -0500

    Use simple version

--- app/models/nod_date_update.rb
+++ app/models/nod_date_update.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NodDateUpdate < CaseflowRecord
-  include HasAppealUpdatedSince
+  include HasSimpleAppealUpdatedSince
 
   belongs_to :appeal
   belongs_to :user
