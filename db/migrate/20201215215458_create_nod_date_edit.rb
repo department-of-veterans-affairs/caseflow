fe5dc4053ee01bae7f238e062306f12ddd8f96aa
Brian Gantick <briangantick@navapbc.com>
2020-12-22 09:13:46 -0500

    Update db/migrate/20201215215458_create_nod_date_edit.rb
    Co-authored-by: Yoom Lam <Dung.Lam1@va.gov>

--- db/migrate/20201215215458_create_nod_date_edit.rb
+++ db/migrate/20201215215458_create_nod_date_edit.rb
@@ -2,8 +2,8 @@ class CreateNodDateEdit < Caseflow::Migration
   def change
     create_table :nod_date_edits do |t|
       t.references "appeal", null: false, foreign_key: true, comment: "Appeal that NOD date is being edited for"
-      t.date "old_value", null: false, comment: "Value before update"
-      t.date "new_value", null: false, comment: "Value after update"
+      t.date "old_date", null: false, comment: "Date before update"
+      t.date "new_date", null: false, comment: "Date after update"
       t.references "user", null: false, foreign_key: true, comment: "User that created this record"
       t.string "change_reason", null: false, comment: "Reason for change - entry_error or new_info"
 
