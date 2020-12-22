629060416b88bc4ce4a9c0470d070631382ca40a
Brian Gantick <briangantick@navapbc.com>
2020-12-22 15:41:06 -0500

    Update validates

--- app/models/nod_date_update.rb
+++ app/models/nod_date_update.rb
@@ -6,7 +6,7 @@ class NodDateUpdate < CaseflowRecord
   belongs_to :appeal
   belongs_to :user
 
-  validates :appeal, :user, :old_value, :new_value, :change_reason, presence: true
+  validates :appeal, :user, :old_date, :new_date, :change_reason, presence: true
 
   enum change_reason: {
     entry_error: "entry_error",
