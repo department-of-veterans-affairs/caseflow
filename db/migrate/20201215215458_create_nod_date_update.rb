15853587098031f6a8d329dd3d8fee2598b755bc
Brian Gantick <briangantick@navapbc.com>
2020-12-22 16:56:13 -0500

    Update db/migrate/20201215215458_create_nod_date_update.rb
    Co-authored-by: Yoom Lam <Dung.Lam1@va.gov>

--- db/migrate/20201215215458_create_nod_date_update.rb
+++ db/migrate/20201215215458_create_nod_date_update.rb
@@ -5,7 +5,7 @@ class CreateNodDateUpdate < Caseflow::Migration
       t.date "old_date", null: false, comment: "Date before update"
       t.date "new_date", null: false, comment: "Date after update"
       t.references "user", null: false, foreign_key: true, comment: "User that updated the NOD date"
-      t.string "change_reason", null: false, comment: "Reason for change - entry_error or new_info"
+      t.string "change_reason", null: false, comment: "Reason for change: entry_error or new_info"
 
       t.timestamps null: false, comment: "Default created_at/updated_at timestamps"
     end
