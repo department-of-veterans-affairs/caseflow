2d5fdf515cb7bc465e9d04d3f2081622ebf0c862
Brian Gantick <briangantick@navapbc.com>
2020-12-22 10:33:32 -0500

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -995,16 +995,16 @@ ActiveRecord::Schema.define(version: 2020_12_15_215458) do
     t.index ["updated_at"], name: "index_messages_on_updated_at"
   end
 
-  create_table "nod_date_edits", force: :cascade do |t|
-    t.bigint "appeal_id", null: false, comment: "Appeal that NOD date is being edited for"
+  create_table "nod_date_updates", force: :cascade do |t|
+    t.bigint "appeal_id", null: false, comment: "Appeal for which the NOD date is being edited"
     t.string "change_reason", null: false, comment: "Reason for change - entry_error or new_info"
     t.datetime "created_at", null: false, comment: "Default created_at/updated_at timestamps"
-    t.date "new_value", null: false, comment: "Value after update"
-    t.date "old_value", null: false, comment: "Value before update"
+    t.date "new_date", null: false, comment: "Date after update"
+    t.date "old_date", null: false, comment: "Date before update"
     t.datetime "updated_at", null: false, comment: "Default created_at/updated_at timestamps"
     t.bigint "user_id", null: false, comment: "User that created this record"
-    t.index ["appeal_id"], name: "index_nod_date_edits_on_appeal_id"
-    t.index ["user_id"], name: "index_nod_date_edits_on_user_id"
+    t.index ["appeal_id"], name: "index_nod_date_updates_on_appeal_id"
+    t.index ["user_id"], name: "index_nod_date_updatess_on_user_id"
   end
 
   create_table "non_availabilities", force: :cascade do |t|
@@ -1597,8 +1597,8 @@ ActiveRecord::Schema.define(version: 2020_12_15_215458) do
   add_foreign_key "legacy_hearings", "users", column: "created_by_id"
   add_foreign_key "legacy_hearings", "users", column: "updated_by_id"
   add_foreign_key "legacy_issue_optins", "legacy_issues"
-  add_foreign_key "nod_date_edits", "appeals"
-  add_foreign_key "nod_date_edits", "users"
+  add_foreign_key "nod_date_updates", "appeals"
+  add_foreign_key "nod_date_updates", "users"
   add_foreign_key "organizations_users", "users"
   add_foreign_key "post_decision_motions", "appeals"
   add_foreign_key "post_decision_motions", "tasks"
