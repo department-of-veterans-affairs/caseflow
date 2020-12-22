4c58ba3f3bf0a380672bbbd052d6277bbbe4439b
Brian Gantick <briangantick@navapbc.com>
2020-12-22 17:08:52 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -995,14 +995,14 @@ ActiveRecord::Schema.define(version: 2020_12_15_215458) do
     t.index ["updated_at"], name: "index_messages_on_updated_at"
   end
 
-  create_table "nod_date_updates", force: :cascade do |t|
+  create_table "nod_date_updates", comment: "Tracks changes to an AMA appeal's receipt date (aka, NOD date)", force: :cascade do |t|
     t.bigint "appeal_id", null: false, comment: "Appeal for which the NOD date is being edited"
-    t.string "change_reason", null: false, comment: "Reason for change - entry_error or new_info"
+    t.string "change_reason", null: false, comment: "Reason for change: entry_error or new_info"
     t.datetime "created_at", null: false, comment: "Default created_at/updated_at timestamps"
     t.date "new_date", null: false, comment: "Date after update"
     t.date "old_date", null: false, comment: "Date before update"
     t.datetime "updated_at", null: false, comment: "Default created_at/updated_at timestamps"
-    t.bigint "user_id", null: false, comment: "User that created this record"
+    t.bigint "user_id", null: false, comment: "User that updated the NOD date"
     t.index ["appeal_id"], name: "index_nod_date_updates_on_appeal_id"
     t.index ["user_id"], name: "index_nod_date_updates_on_user_id"
   end
