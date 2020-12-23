4e26994cd1274c31b7b3c887fecc1db0ad1cdc05
Will Drougas <wdrougas@tistatech.com>
2020-12-23 18:33:05 -0500

    controller update

--- app/controllers/docket_switches_controller.rb
+++ app/controllers/docket_switches_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DocketSwitchesController < ApplicationController
+  before_action :verify_task_access, only: [:create]
+
   def create
     docket_switch = DocketSwitch.new(*docket_switch_params)
     if docket_switch.errors.present?
@@ -12,6 +14,16 @@ class DocketSwitchesController < ApplicationController
   end
 
   private
+
+  def verify_task_access
+    if task.assigned_to != current_user
+      fail Caseflow::Error::ActionForbiddenError, message: "Only task assignee can perform this action"
+    end
+  end
+
+  def task
+    @task ||= Task.find(docket_switch_params[:task_id])
+  end
 
   def docket_switch_params
     params.permit(:disposition, :task_id, :receipt_date, :context, :old_docket_stream_id)
