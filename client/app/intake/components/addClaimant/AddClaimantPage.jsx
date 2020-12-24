2a113052837ddfe29238623b19a38a8add8e7f65
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2020-12-24 12:04:15 -0800

    added copy to AddClaimantPage

--- client/app/intake/components/addClaimant/AddClaimantPage.jsx
+++ client/app/intake/components/addClaimant/AddClaimantPage.jsx
@@ -7,6 +7,8 @@ import { useAddClaimantForm } from './utils';
 import { FormProvider } from 'react-hook-form';
 import { useHistory } from 'react-router';
 
+import { ADD_CLAIMANT_PAGE_DESCRIPTION } from 'app/../COPY';
+
 export const AddClaimantPage = () => {
   const { goBack } = useHistory();
 
@@ -30,6 +32,7 @@ export const AddClaimantPage = () => {
         }
       >
         <h1>Add Claimant</h1>
+        <p>{ADD_CLAIMANT_PAGE_DESCRIPTION}</p>
         <AddClaimantForm />
       </IntakeLayout>
     </FormProvider>
