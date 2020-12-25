37b0e9e9698ac22597f9e953080286234d940354
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2020-12-24 18:20:26 -0800

    Updates to DocketSwitchReviewRequestForm jest tests

--- client/test/app/queue/docketSwitch/DocketSwitchReviewRequestForm.test.js
+++ client/test/app/queue/docketSwitch/DocketSwitchReviewRequestForm.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import { DocketSwitchReviewRequestForm } from 'app/queue/docketSwitch/grant/DocketSwitchReviewRequestForm';
 import {
   DOCKET_SWITCH_GRANTED_REQUEST_LABEL,
-  DOCKET_SWITCH_GRANTED_REQUEST_INSTRUCTIONS
+  DOCKET_SWITCH_GRANTED_REQUEST_INSTRUCTIONS,
 } from 'app/../COPY';
 import { sprintf } from 'sprintf-js';
 
@@ -15,18 +15,25 @@ describe('DocketSwitchReviewRequestForm', () => {
   const appellantName = 'Claimant 1';
   const defaults = { onSubmit, onCancel, appellantName };
 
-
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('renders correctly', () => {
-    const { container } = render(<DocketSwitchReviewRequestForm {...defaults} />);
+    const { container } = render(
+      <DocketSwitchReviewRequestForm {...defaults} />
+    );
 
     expect(container).toMatchSnapshot();
 
-    expect(screen.getByText(sprintf(DOCKET_SWITCH_GRANTED_REQUEST_LABEL, appellantName))).toBeInTheDocument();
-    expect(screen.getByText(DOCKET_SWITCH_GRANTED_REQUEST_INSTRUCTIONS)).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        sprintf(DOCKET_SWITCH_GRANTED_REQUEST_LABEL, appellantName)
+      )
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(DOCKET_SWITCH_GRANTED_REQUEST_INSTRUCTIONS)
+    ).toBeInTheDocument();
   });
 
   it('fires onCancel', async () => {
@@ -37,57 +44,70 @@ describe('DocketSwitchReviewRequestForm', () => {
     await userEvent.click(screen.getByRole('button', { name: /cancel/i }));
     expect(onCancel).toHaveBeenCalled();
   });
- 
+
   describe('form validation for all granted issues', () => {
+    const receiptDate = '2020-10-01';
+    const disposition = 'granted';
+    const docketType = 'direct_review';
 
-  const receiptDate = '2020-10-01';
-  const radioButton = 'Grant all issues'
-    const fillForm = async () => {
-      //   Set receipt date
-      await fireEvent.change(screen.getByLabelText(/receipt date/i), { target: { value: receiptDate } });
-
-
-      //   Enter context/instructions
-      await fireEvent.change(screen.getByLabelText(/grant all issues/i), { target: { value: radioButton } });
-    };
-
-
-  it('fires onSubmit with correct values', async () => {
-
-    render(<DocketSwitchReviewRequestForm {...defaults} />);
+    it('fires onSubmit with correct values', async () => {
+      render(<DocketSwitchReviewRequestForm {...defaults} />);
 
       const submit = screen.getByRole('button', { name: /Continue/i });
 
-      await fillForm();
+      // Set receipt date
+      await fireEvent.change(screen.getByLabelText(/receipt date/i), {
+        target: { value: receiptDate },
+      });
+
+      //   Set disposition
+      await userEvent.click(
+        screen.getByRole('radio', { name: /grant all issues/i })
+      );
+
+      // Wait for docketType to show up
+      await waitFor(() => {
+        expect(screen.getByRole('radio', { name: /direct review/i })).toBeInTheDocument();
+      });
+
+      //   Set docketType
+      await userEvent.click(
+        screen.getByRole('radio', { name: /direct review/i })
+      );
+
+      await waitFor(() => {
+        expect(submit).toBeEnabled();
+      });
 
       await userEvent.click(submit);
 
-      waitFor(() => {
+      await waitFor(() => {
         expect(onSubmit).toHaveBeenCalledWith({
-          receiptDate,
+          receiptDate: (new Date(receiptDate)).toString(),
           disposition,
+          docketType
         });
       });
     });
   });
 
   describe('form validation for granted partial issues', () => {
-
-  const receiptDate = '2020-10-01';
-  const radioButton = 'Grant a partial switch'
+    const receiptDate = '2020-10-01';
+    const disposition = 'partially_granted';
     const fillForm = async () => {
       //   Set receipt date
-      await fireEvent.change(screen.getByLabelText(/receipt date/i), { target: { value: receiptDate } });
-
+      await fireEvent.change(screen.getByLabelText(/receipt date/i), {
+        target: { value: receiptDate },
+      });
 
       //   Enter context/instructions
-      await fireEvent.change(screen.getByLabelText(/grant a partial switch/i), { target: { value: radioButton } });
+      await fireEvent.change(screen.getByLabelText(/grant a partial switch/i), {
+        target: { value: disposition },
+      });
     };
 
-
-  it('fires onSubmit with correct values', async () => {
-
-    render(<DocketSwitchReviewRequestForm {...defaults} />);
+    it('fires onSubmit with correct values', async () => {
+      render(<DocketSwitchReviewRequestForm {...defaults} />);
 
       const submit = screen.getByRole('button', { name: /Continue/i });
 
