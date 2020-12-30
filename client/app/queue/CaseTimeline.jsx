b409e02323d9a3d162942c0132d559e13632c7d4
pbradn <paula.bradin@va.gov>
2020-12-30 10:03:51 -0500

    Linting fixes

--- client/app/queue/CaseTimeline.jsx
+++ client/app/queue/CaseTimeline.jsx
@@ -7,7 +7,8 @@ import { caseTimelineTasksForAppeal } from './selectors';
 
 export const CaseTimeline = ({ appeal }) => {
   const tasks = useSelector((state) => caseTimelineTasksForAppeal(state, { appealId: appeal.externalId }));
-  const canEditNodDate = useSelector((state) => state.ui.canEditNodDate);  
+  const canEditNodDate = useSelector((state) => state.ui.canEditNodDate);
+  
   return (
     <React.Fragment>
       {COPY.CASE_TIMELINE_HEADER}
