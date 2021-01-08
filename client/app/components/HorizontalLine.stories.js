320414717c3ba828d15c60636555613a44655916
Meckila <68879427+meckilabritt-navapbc@users.noreply.github.com>
2021-01-08 14:04:10 -0800

    Add horizontal line story (#15690)

---
+++ client/app/components/HorizontalLine.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default {
+  title: 'Commons/Components/Layout/Horizontal Line',
+};
+
+const Template = () => (
+  <div className="cf-help-divider"></div>
+);
+
+export const HorizontalLine = Template.bind({});
