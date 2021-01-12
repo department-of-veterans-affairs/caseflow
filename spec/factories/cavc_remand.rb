1ab049a62109543fc27438c02ef859f73f29bc1c
Kat Tipton <kathryn.tipton@gmail.com>
2021-01-12 15:05:20 -0500

    Correct to only add source_appeal

--- spec/factories/cavc_remand.rb
+++ spec/factories/cavc_remand.rb
@@ -15,13 +15,14 @@ FactoryBot.define do
     after(:build) do |cavc_remand, evaluator|
       cavc_remand.created_by = (evaluator.created_by || User.first)
 
-      if evaluator.appeal
-        cavc_remand.appeal = evaluator.appeal
+      if evaluator.source_appeal
+        cavc_remand.source_appeal = evaluator.source_appeal
+        cavc_remand.appeal = evaluator.source_appeal
 
         cavc_remand.decision_issue_ids = if !evaluator.decision_issue_ids.empty?
                                            evaluator.decision_issue_ids
                                          else
-                                           evaluator.appeal.decision_issues.pluck(:id)
+                                           evaluator.source_appeal.decision_issues.pluck(:id)
                                          end
       else
         judge = JudgeTeam.first.admin
@@ -30,7 +31,7 @@ FactoryBot.define do
 
         description = "Service connection for pain disorder is granted with an evaluation of 70\% effective May 1 2011"
         notes = "Pain disorder with 100\% evaluation per examination"
-        appeal = create(:appeal,
+        source_appeal = create(:appeal,
                         :dispatched,
                         veteran_file_number: veteran.file_number,
                         associated_judge: judge,
@@ -38,16 +39,17 @@ FactoryBot.define do
         create_list(:request_issue, 2,
                     :rating,
                     :with_rating_decision_issue,
-                    decision_review: appeal,
+                    decision_review: source_appeal,
                     veteran_participant_id: veteran.participant_id,
                     contested_issue_description: description,
                     notes: notes)
-        cavc_remand.appeal = appeal
+        cavc_remand.source_appeal = source_appeal
+        cavc_remand.appeal = source_appeal
 
         cavc_remand.decision_issue_ids = if !evaluator.decision_issue_ids.empty?
                                            evaluator.decision_issue_ids
                                          else
-                                           cavc_remand.appeal.decision_issues.pluck(:id)
+                                           cavc_remand.source_appeal.decision_issues.pluck(:id)
                                          end
       end
     end
