e4cefff3da82aeee442e92f1b15f4f55df343f8d
Kat Tipton <kathryn.tipton@gmail.com>
2021-01-12 17:00:18 -0500

    Deprecates and removes cavc_remand appeal_id

--- app/controllers/cavc_remands_controller.rb
+++ app/controllers/cavc_remands_controller.rb
@@ -29,15 +29,13 @@ class CavcRemandsController < ApplicationController
   end
 
   def create_params
-    params.merge!(created_by_id: current_user.id, updated_by_id: current_user.id, source_appeal_id: source_appeal.id,
-                  appeal_id: source_appeal.id)
+    params.merge!(created_by_id: current_user.id, updated_by_id: current_user.id, source_appeal_id: source_appeal.id)
     params.require(required_params)
     params.permit(required_params << :remand_subtype).merge(params.permit(decision_issue_ids: []))
   end
 
   def required_params
     [
-      :appeal_id,
       :source_appeal_id,
       :cavc_decision_type,
       :cavc_docket_number,
