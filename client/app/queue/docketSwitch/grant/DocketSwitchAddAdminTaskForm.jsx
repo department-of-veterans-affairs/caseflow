23fbde118ca917cfcb306c24d1c441d44135a8f0
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-01-14 17:48:15 -0800

    initial work on DocketSwitchAddAdminTaskForm

---
+++ client/app/queue/docketSwitch/grant/DocketSwitchAddAdminTaskForm.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Controller, useFormContext } from 'react-hook-form';
+import SearchableDropdown from 'app/components/SearchableDropdown';
+
+import colocatedAdminActions from 'constants/CO_LOCATED_ADMIN_ACTIONS';
+import TextareaField from 'app/components/TextareaField';
+import {
+  ADD_COLOCATED_TASK_INSTRUCTIONS_LABEL,
+  FORM_ERROR_FIELD_REQUIRED,
+} from 'app/../COPY';
+import { css } from 'glamor';
+import Button from '../../../components/Button';
+
+const fieldStyles = css({
+  marginTop: '4rem',
+});
+
+const actionOptions = Object.entries(colocatedAdminActions).map(
+  ([value, label]) => ({ label, value })
+);
+
+export const DocketSwitchAddAdminTaskForm = ({ baseName, onRemove }) => {
+  const { control, errors, register } = useFormContext();
+
+  const handleRemove = () => onRemove();
+
+  return (
+    <>
+      <div className={fieldStyles}>
+        <Controller
+          name={`${baseName}.type`}
+          control={control}
+          render={({ name, onChange: onSelectChange }) => {
+            return (
+              <SearchableDropdown
+                name={name}
+                label="Select the type of task you'd like to open:"
+                onChange={onSelectChange}
+                options={actionOptions}
+              />
+            );
+          }}
+        />
+      </div>
+
+      <div className={fieldStyles}>
+        <TextareaField
+          errorMessage={
+            errors?.[baseName]?.instructions ? FORM_ERROR_FIELD_REQUIRED : null
+          }
+          name={`${baseName}.instructions`}
+          label={ADD_COLOCATED_TASK_INSTRUCTIONS_LABEL}
+          inputRef={register}
+        />
+      </div>
+      <div>
+        <Button linkStyling onClick={handleRemove}>
+          Remove this action
+        </Button>
+      </div>
+    </>
+  );
+};
+
+DocketSwitchAddAdminTaskForm.propTypes = {
+  baseName: PropTypes.string.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
