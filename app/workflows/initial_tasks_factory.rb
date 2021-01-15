df13d7e59d116d41257d9eca2f59c185e7e5eca3
yoomlam <Dung.Lam1@va.gov>
2021-01-15 13:46:22 -0600

    put Mdr Cavc Remand appeal on hold

--- app/workflows/initial_tasks_factory.rb
+++ app/workflows/initial_tasks_factory.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class InitialTasksFactory
-  def initialize(appeal)
+  def initialize(appeal, cavc_remand = nil)
     @appeal = appeal
     @root_task = RootTask.find_or_create_by!(appeal: appeal)
+
+    if @appeal.cavc?
+      @cavc_remand = cavc_remand
+      @cavc_remand ||= appeal.cavc_remand
+
+      fail "CavcRemand required for CAVC-Remand appeal #{@appeal.id}" unless @cavc_remand
+    end
   end
 
   def create_root_and_sub_tasks!
@@ -27,8 +34,7 @@ class InitialTasksFactory
     distribution_task = DistributionTask.create!(appeal: @appeal, parent: @root_task)
 
     if @appeal.cavc?
-      cavc_task = CavcTask.create!(appeal: @appeal, parent: distribution_task)
-      SendCavcRemandProcessedLetterTask.create!(appeal: @appeal, parent: cavc_task)
+      create_tasks_for_cavc(distribution_task)
     elsif @appeal.evidence_submission_docket?
       EvidenceSubmissionWindowTask.create!(appeal: @appeal, parent: distribution_task)
     elsif @appeal.hearing_docket?
@@ -38,6 +44,19 @@ class InitialTasksFactory
       # If the appeal is direct docket and there are no ihp tasks,
       # then it is initially ready for distribution.
       distribution_task.ready_for_distribution! if vso_tasks.empty?
+    end
+  end
+
+  # For AMA appeals. Create appropriate subtasks based on the CAVC Remand subtype
+  def create_tasks_for_cavc(distribution_task)
+    cavc_task = CavcTask.create!(appeal: @appeal, parent: distribution_task)
+
+    if @cavc_remand.mdr?
+      MdrTask.create_with_hold(cavc_task)
+    elsif @cavc_remand.jmr? || @cavc_remand.jmpr?
+      SendCavcRemandProcessedLetterTask.create!(appeal: @appeal, parent: cavc_task)
+    else
+      fail "Not yet supported"
     end
   end
 end
