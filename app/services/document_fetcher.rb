fe9b173674d73f3e0fdb28e22b0bdb0668be505e
yoomlam <Dung.Lam1@va.gov>
2021-01-19 15:20:57 -0600

    use DuplicateVbmsDocumentIdError

--- app/services/document_fetcher.rb
+++ app/services/document_fetcher.rb
@@ -132,6 +132,9 @@ class DocumentFetcher
               nonexact_dup_docs_count: nonexact_dups_hash.count,
               nonexact_dups_hash: nonexact_dups_hash,
               docs_as_csv: docs_as_csv.join("") }
-    Raven.capture_message("Document records with duplicate vbms_document_id: #{warning_message}", extra: extra)
+    Raven.capture_exception(DuplicateVbmsDocumentIdError.new("Document records with duplicate vbms_document_id: #{warning_message}"), 
+                            extra: extra)
   end
+
+  class DuplicateVbmsDocumentIdError < RuntimeError; end
 end
