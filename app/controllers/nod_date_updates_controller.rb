95f9aa02bdb6f882b7ebc0669f87a7c1d07c541f
pbradn <paula.bradin@va.gov>
2021-01-19 09:30:51 -0500

    WHitespace fixes

--- app/controllers/nod_date_updates_controller.rb
+++ app/controllers/nod_date_updates_controller.rb
@@ -3,13 +3,12 @@
 # Controller to edit NOD dates and store the related info in the nod_date_updates table
 
 class NodDateUpdatesController < ApplicationController
-  
   def create
     nod_date_update = NodDateUpdate.create!(updated_params)
-    appeal.update_receipt_date!(receipt_date: params[:receipt_date]) 
+    appeal.update_receipt_date!(receipt_date: params[:receipt_date])
     render json: { nod_date_update: nod_date_update }, status: :created
   end
-  
+
   def update
     if existing_nod_date_update
       nod_date_update = NodDateUpdate.update(updated_params)
@@ -19,9 +18,9 @@ class NodDateUpdatesController < ApplicationController
       create
     end
   end
-  
+
   private
-  
+
   def appeal
     @appeal ||= Appeal.find_by_uuid(params[:appeal_id])
   end
@@ -29,28 +28,27 @@ class NodDateUpdatesController < ApplicationController
   def existing_nod_date_update
     @existing_nod_date_update ||= NodDateUpdate.find_by(appeal_id: appeal.id)
   end
-  
+
   def updated_params
     params.merge!(
-      user_id: current_user.id, 
-      appeal_id: appeal.id, 
-      old_date: appeal.receipt_date, 
+      user_id: current_user.id,
+      appeal_id: appeal.id,
+      old_date: appeal.receipt_date,
       new_date: params["receipt_date"],
       change_reason: params["change_reason"]["value"]
     )
     params.permit(required_params)
   end
-  
+
   def required_params
-      [
-        :user_id,
-        :appeal_id,
-        :old_date,
-        :new_date,
-        :change_reason,
-        :created_at,
-        :updated_at
-      ]
+    [
+      :user_id,
+      :appeal_id,
+      :old_date,
+      :new_date,
+      :change_reason,
+      :created_at,
+      :updated_at
+    ]
   end
 end
-  
