7dd1c32596dfb230957a58a1ee5b55531279836d
Ferris Tseng <ferris@navapbc.com>
2021-01-20 16:17:31 -0500

    use appellant_state which supports both legacy and ama appeals

--- app/helpers/virtual_hearings/appellant_location_helper.rb
+++ app/helpers/virtual_hearings/appellant_location_helper.rb
@@ -6,7 +6,7 @@
 
 module VirtualHearings::AppellantLocationHelper
   def appellant_state(appeal)
-    state_code = appeal.appellant_is_not_veteran ? appeal.appellant&.state : appeal.veteran&.state
+    state_code = appeal.appellant_is_not_veteran ? appeal.appellant_state : appeal.veteran_state
     state_code || "??" # ?? Fallback if for some reason there isn't any state
   end
 end
