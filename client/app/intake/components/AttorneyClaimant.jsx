18eb9fa172fd75e414b15a088d3eedb7d4283f75
Sally Maki <Sally.Maki@va.gov>
2021-01-20 12:40:33 -0800

    Fix flakey docket switch task handler spec

---
+++ client/app/intake/components/AttorneyClaimant.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { debounce } from 'lodash';
+import Modal from '../../components/Modal';
+import {
+  ADD_CLAIMANT_MODAL_TITLE,
+  ADD_CLAIMANT_MODAL_DESCRIPTION
+} from '../../../COPY';
+import ReactMarkdown from 'react-markdown';
+import SearchableDropdown from '../../components/SearchableDropdown';
+import ApiUtil from '../../util/ApiUtil';
+import Checkbox from '../../components/Checkbox';
+import TextareaField from '../../components/TextareaField';
+
+const ALLOW_UNLISTED_CLAIMANTS = false;
+
+const relationshipOpts = [
+  { label: 'Attorney (previously or currently)', value: 'attorney' },
+];
+
+const fetchAttorneys = async (search = '') => {
+  const res = await ApiUtil.get('/intake/attorneys', {
+    query: { query: search }
+  });
+
+  return res?.body;
+};
+const getClaimantOpts = async (search = '', asyncFn) => {
+  // Enforce minimum search length (we'll simply return empty array rather than throw error)
+  if (search.length < 3) {
+    return [];
+  }
+
+  const res = await asyncFn(search);
+  const options = res.map((item) => ({
+    label: item.name,
+    value: item.participant_id,
+  }));
+
+  return options;
+};
+// We'll show all items returned from the backend instead of using default substring matching
+const filterOption = () => true;
+
+export const AttorneyClaimant = ({
+  onCancel,
+  onSubmit,
+  onSearch = fetchAttorneys,
+}) => {
+  const [claimant, setClaimant] = useState(null);
+  const [relationship, setRelationship] = useState(relationshipOpts[0]);
+  const [unlistedClaimant, setUnlistedClaimant] = useState(false);
+  const [claimantNotes, setClaimantNotes] = useState('');
+  const isInvalid = useMemo(() => {
+    return (
+      (!unlistedClaimant && !claimant) || (unlistedClaimant && !claimantNotes)
+    );
+  }, [claimant, unlistedClaimant, claimantNotes]);
+
+  const handleChangeRelationship = (value) => setRelationship(value);
+  const handleChangeClaimant = (value) => setClaimant(value);
+  const handleNotListed = (value) => setUnlistedClaimant(value);
+  const handleClaimantNotes = (value) => setClaimantNotes(value);
+
+  const asyncFn = useCallback(
+    debounce((search, callback) => {
+      getClaimantOpts(search, onSearch).then((res) => callback(res));
+    }, 250),
+    [onSearch]
+  );
+
+  const buttons = [
+    {
+      classNames: ['cf-modal-link', 'cf-btn-link'],
+      name: 'Cancel',
+      onClick: onCancel,
+    },
+    {
+      classNames: ['usa-button', 'usa-button-primary'],
+      name: 'Add this claimant',
+      onClick: () =>
+        onSubmit({
+          name: claimant?.label,
+          participantId: claimant?.value,
+          claimantType: (unlistedClaimant ? 'other' : 'attorney'),
+          claimantNotes,
+        }),
+      disabled: isInvalid,
+    },
+  ];
+
+  useEffect(() => {
+    if (!unlistedClaimant) {
+      setClaimantNotes('');
+    }
+  }, [unlistedClaimant]);
+
+  return (
+      <SearchableDropdown
+        name="claimant"
+        label="Claimant's name"
+        onChange={handleChangeClaimant}
+        value={claimant}
+        filterOption={filterOption}
+        async={asyncFn}
+        defaultOptions
+        readOnly={unlistedClaimant}
+        debounce={250}
+        strongLabel
+        isClearable
+        placeholder="Type to search..."
+      />
+  );
+};
+
+AttorneyClaimant.propTypes = {
+  onCancel: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onSearch: PropTypes.func,
+  readOnly: PropTypes.bool,
+};
