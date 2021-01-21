28e987ba3575d303441558e69566af8084bbd661
karinamzalez <karinamzalez@gmail.com>
2021-01-21 11:58:31 -0600

    AddressForm: - remove dead code - add styling - remove <form> element and use shorthand (<>) for React.Fragment

--- client/app/components/AddressForm.jsx
+++ client/app/components/AddressForm.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import {STATES} from '../constants/AppConstants'
-import RadioField from 'app/components/RadioField';
+import { STATES } from '../constants/AppConstants'
 import TextField from 'app/components/TextField';
 import SearchableDropdown from 'app/components/SearchableDropdown';
 
 export const AddressForm = () => {
 
   return (
-    <React.Fragment>
-      <form>
-        <br />
-        <TextField
-          name="Street address 1"
-          label="Street address 1"
-          strongLabel
-        />
-        <TextField
-          name="Street address 2"
-          label="Street address 2"
-          optional
-          strongLabel
-        />
+    <>
+      <TextField
+        name="Street address 1"
+        label="Street address 1"
+        strongLabel
+      />
+      <TextField
+        name="Street address 2"
+        label="Street address 2"
+        optional
+        strongLabel
+      />
+      <StreetAddress>
         <TextField
           name="Street address 3"
           label="Street address 3"
           optional
           strongLabel
         />
-        <div className="cf-progress-bar">
+      </StreetAddress>
+      <CityState>
         <TextField
           name="City"
           label="City"
@@ -37,11 +36,12 @@ export const AddressForm = () => {
         />
         <SearchableDropdown
           name="State"
-          label="State" 
+          label="State"
           options={STATES}
           strongLabel
         />
-        </div>
+      </CityState>
+      <ZipCountry>
         <TextField
           name="Zip"
           label="Zip"
@@ -52,9 +52,32 @@ export const AddressForm = () => {
           label="Country"
           strongLabel
         />
-      </form>
-    </React.Fragment>
+      </ZipCountry>
+    </>
   );
 };
+
+// Styles ----------------------------
+
+const CityState = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 320px 130px;
+  margin-bottom: 1em;
+  align-items: center;
+  input {
+    margin-bottom: 0;
+  }
+`;
+
+const ZipCountry = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 140px 310px;
+`;
+
+const StreetAddress = styled.div`
+  margin-bottom:0;
+`;
 
 export default AddressForm;
