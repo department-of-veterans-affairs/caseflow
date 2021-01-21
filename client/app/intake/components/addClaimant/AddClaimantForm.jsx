364f4dfc46165f19bf4b5b8e1b04c22d16ee26b2
karinamzalez <karinamzalez@gmail.com>
2021-01-21 12:00:27 -0600

    AddClaimantForm: - remove dead code (unused imports and commented out code) - move OtherForm radio button and logic into separate component - import and use OtherForm

--- client/app/intake/components/addClaimant/AddClaimantForm.jsx
+++ client/app/intake/components/addClaimant/AddClaimantForm.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import * as Constants from '../../constants';
 
 import { Controller, useFormContext } from 'react-hook-form';
 
+import IndividualForm from './IndividualForm';
 import SearchableDropdown from 'app/components/SearchableDropdown';
-import IndividualForm from './IndividualForm'
-import AddressForm from 'app/components/AddressForm'
-import RadioField from 'app/components/RadioField';;
-import TextField from 'app/components/TextField';
+import OtherForm from './OtherForm';
+
+const individual = ['spouse', 'child', 'individual'];
 
 const relationshipOpts = [
   { label: 'Attorney (previously or currently)', value: 'attorney' },
@@ -17,61 +16,12 @@ const relationshipOpts = [
   { label: 'Other', value: 'other' },
 ];
 
-const otherOpts = [
-  {displayText: 'Organization', value: 'organization'},
-  {displayText: 'Individual', value: 'individual'}
-];
-
-// const radioOpts = useMemo(() => {
-//     return [
-//       ...otherOpts,
-//     ];
-//   }, [otherOpts]);
-
-
-const individuals = ['spouse', 'child'];
-const other = ['other']
-
-// const handleOtherClaimant = (value) => {
-//   debugger
-//   if (value === "organization") {
-//     return (
-//       <div>
-//         <TextField
-//          name="organization" 
-//          label="Organization name" 
-//          strongLabel
-//         />
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <IndividualForm />
-//     )
-//   }
-// };
-
 export const AddClaimantForm = ({ onSubmit }) => {
-  const { control, handleSubmit, register, watch } = useFormContext();
-  const watchRelationship = watch('relationship')
-  const watchType = watch('type')
-  
+  const { control, handleSubmit, watch } = useFormContext();
+  const watchRelationship = watch('relationship'); /* set in SearchableDropdown */
+  const watchType = watch('type'); /* set in OtherForm */
 
-  const setOtherClaimant = () => {
-    console.log("type", watchType)
-    return (
-      <div style={{ marginTop: '32px' }}>
-        <RadioField
-          name="type" 
-          label="Is the claimant an organization or individual?"
-          inputRef={register}
-          strongLabel
-          vertical
-          options={otherOpts}
-        />
-      </div>
-    );
-  };
+  const renderIndividualForm = individual.includes(watchType || watchRelationship?.value);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -83,46 +33,13 @@ export const AddClaimantForm = ({ onSubmit }) => {
         options={relationshipOpts}
         strongLabel
       />
-      {individuals.includes(watchRelationship?.value) && <IndividualForm />}
-      {other.includes(watchRelationship?.value) && setOtherClaimant()}
-      {watchType == 'organization' && 
-        <div style={{ marginTop: '26px' }}> 
-          <TextField 
-            name="organization" 
-            label="Organization name" 
-            strongLabel
-          /> 
-          <AddressForm /> 
-          <TextField
-              name="email"
-              label="Claimant email"
-              optional
-              strongLabel
-          />
-          <TextField
-            name="phone number"
-            label="Phone number"
-            optional
-            strongLabel
-          />
-          <RadioField
-            options={Constants.BOOLEAN_RADIO_OPTIONS}
-            vertical
-            label="Do you have a VA Form 21-22 for this claimant?"
-            name="21-22-radio"
-          />
-        </div>
-      }
-      {watchType == 'individual' &&
-        <div>
-          <IndividualForm />
-        </div>
-      }
+      { watchRelationship?.value === 'other' && <OtherForm />}
+      { renderIndividualForm && <IndividualForm /> }
     </form>
   );
 };
 
 AddClaimantForm.propTypes = {
   onSubmit: PropTypes.func
-}; 
+};
 
