38619fa6570734aa1c1f2864fb1222012ce086e4
karinamzalez <karinamzalez@gmail.com>
2021-01-21 12:01:36 -0600

    IndifidualForm: - remove dead code (<form> element, and unused imports - add form styles

--- client/app/intake/components/addClaimant/IndividualForm.jsx
+++ client/app/intake/components/addClaimant/IndividualForm.jsx
@@ -1,61 +1,76 @@
 import React from 'react';
+import styled from 'styled-components';
 import * as Constants from '../../constants';
 
 import RadioField from 'app/components/RadioField';
 import TextField from 'app/components/TextField';
-import AddressForm from 'app/components/AddressForm'
+import AddressForm from 'app/components/AddressForm';
 
 export const IndividualForm = () => {
 
   return (
-    <React.Fragment>
-      <form>
-        <br />
-        <TextField
-          name="First name"
-          label="First name"
-          strongLabel
-        />
-        <TextField
-          name="Middle name/initial"
-          label="Middle name/initial"
-          optional
-          strongLabel
-        />
-        <TextField
-          name="Last name"
-          label="Last name"
-          optional
-          strongLabel
-        />
+    <>
+      <br />
+      <TextField
+        name="First name"
+        label="First name"
+        strongLabel
+      />
+      <TextField
+        name="Middle name/initial"
+        label="Middle name/initial"
+        optional
+        strongLabel
+      />
+      <TextField
+        name="Last name"
+        label="Last name"
+        optional
+        strongLabel
+      />
+      <Suffix>
         <TextField
           name="suffix"
           label="Suffix"
           optional
           strongLabel
         />
-        {<AddressForm />}
-        <TextField
-          name="email"
-          label="Claimant email"
-          optional
-          strongLabel
-        />
+      </Suffix>
+      <AddressForm />
+      <TextField
+        name="email"
+        label="Claimant email"
+        optional
+        strongLabel
+      />
+      <PhoneNumber>
         <TextField
           name="phone number"
           label="Phone number"
           optional
           strongLabel
         />
-        <RadioField
-          options={Constants.BOOLEAN_RADIO_OPTIONS}
-          vertical
-          label="Do you have a VA Form 21-22 for this claimant?"
-          name="21-22-radio"
-        />
-      </form>
-    </React.Fragment>
+      </PhoneNumber>
+      <RadioField
+        options={Constants.BOOLEAN_RADIO_OPTIONS}
+        vertical
+        label="Do you have a VA Form 21-22 for this claimant?"
+        name="21-22-radio"
+        strongLabel
+      />
+    </>
   );
 };
+
+// Styles ----------------------------
+
+const Suffix = styled.div`
+  max-width: 8em;
+`;
+
+const PhoneNumber = styled.div`
+  width: 240px;
+  margin-bottom: 2em;
+`;
 
 export default IndividualForm;
