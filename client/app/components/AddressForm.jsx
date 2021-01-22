597c4d00970f2e3cae303fb5e321492f06a32ffe
Will Drougas <wdrougas@tistatech.com>
2021-01-22 16:43:46 -0500

    removed glamor from AddressForm

--- client/app/components/AddressForm.jsx
+++ client/app/components/AddressForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { css } from 'glamor';
 
 import { STATES } from '../constants/AppConstants';
 import TextField from 'app/components/TextField';
