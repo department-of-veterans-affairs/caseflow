d6fb6bdd99d2406586c8135561e4bd4bc4e76315
yoomlam <Dung.Lam1@va.gov>
2021-01-22 11:10:04 -0600

    forgot to update test

--- spec/workflows/initial_tasks_factory_spec.rb
+++ spec/workflows/initial_tasks_factory_spec.rb
@@ -216,7 +216,7 @@ describe InitialTasksFactory, :postgres do
       shared_examples "remand appeal" do
         it "blocks distribution with a CavcTask" do
           expect_any_instance_of(InitialTasksFactory).to receive(:create_root_and_sub_tasks!).once.and_call_original
-          expect_any_instance_of(InitialTasksFactory).to receive(:create_tasks_for_cavc).once.and_call_original
+          expect_any_instance_of(InitialTasksFactory).to receive(:create_cavc_subtasks).once.and_call_original
           remand_appeal = subject.remand_appeal
 
           expect(DistributionTask.find_by(appeal: remand_appeal).status).to eq("on_hold")
