91a73f163770ce23d1ae292e5a5d402a9f54f597
Meckila <68879427+meckila-navapbc@users.noreply.github.com>
2021-01-25 16:42:30 -0800

    Add issue timeliness check for NOD Date Change (#15783)
    * Set up validation method in NodDateUpdate
    
    * Resolve rspec
    
    * Remove fdescribe
    
    * Refine wording in spec test
    
    Co-authored-by: Brian Gantick <briangantick@navapbc.com>

---
+++ spec/models/nod_date_update_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+describe NodDateUpdate, :all_dbs do
+  let(:timely_request_issue) { create(:request_issue, id: 1, decision_date: 381.days.ago) }
+  let(:untimely_request_issue_with_exemption) do
+    create(:request_issue,
+           id: 2,
+           decision_date: 2.years.ago,
+           untimely_exemption: true)
+  end
+  let(:request_issues) { [timely_request_issue, untimely_request_issue_with_exemption] }
+  let(:appeal) { create(:appeal, request_issues: request_issues) }
+  let(:nod_date_update) { build(:nod_date_update, appeal: appeal) }
+
+  context "validate_all_issues_timely" do
+    subject { nod_date_update.validate_all_issues_timely }
+
+    it "should fail validation if timely issue without exemption becomes untimely" do
+      subject
+
+      expect(nod_date_update.errors[:new_date][0][:message]).to include(
+        "Timeliness of one or more issues is affected by NOD date change"
+      )
+      expect(nod_date_update.errors[:new_date][0][:affected_issues].first.id).to eq(1)
+      expect(nod_date_update.errors[:new_date][0][:unaffected_issues].first.id).to eq(2)
+    end
+
+    it "should pass validation if untimely issue has exemption status" do
+      nod_date_update.new_date = 391.days.ago
+      subject
+      expect(nod_date_update.errors[:new_date]).to match_array([])
+    end
+  end
+end
