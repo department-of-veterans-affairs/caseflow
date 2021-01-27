e49f81fbecf2cd01c8110b283990d3d221641166
Kat Tipton <kathryn.tipton@gmail.com>
2021-01-27 16:27:19 -0500

    Moves existing CAVC seeds to new file

---
+++ db/seeds/cavc_ama_appeals.rb
@@ -0,0 +1,26 @@
+module Seeds
+  class CavcAmaAppeals < Base
+    def initialize
+      @ama_appeals = []
+    end
+
+    def seed!
+      create_cavc_ama_appeals
+    end
+
+    private
+
+    def create_cavc_ama_appeals
+      create_cavc_appeals_at_send_letter
+    end
+
+    def create_cavc_appeals_at_send_letter
+      9.times do
+        create(:cavc_remand,
+               judge: JudgeTeam.first.admin,
+               attorney: JudgeTeam.first.non_admins.first,
+               veteran: Veteran.first)
+      end
+    end
+  end
+end
