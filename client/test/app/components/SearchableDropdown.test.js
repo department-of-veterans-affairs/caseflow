80922e82efaf31caf2182df9c624ca2cb9b5ffec
karinamzalez <karinamzalez@gmail.com>
2021-01-27 22:08:13 -0600

    SearchableDropdown-test: remove dead code (un-used imports)

--- client/test/app/components/SearchableDropdown.test.js
+++ client/test/app/components/SearchableDropdown.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {
   render,
-  fireEvent,
   screen,
-  wait,
-  waitFor,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import selectEvent from 'react-select-event';
