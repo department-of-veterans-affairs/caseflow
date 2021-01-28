347646539ee131bd4cdb29f2fcca3f64abcd8945
karinamzalez <karinamzalez@gmail.com>
2021-01-27 22:03:03 -0600

    AddressForm: replace styled-components styling with glamor

--- client/app/components/AddressForm.jsx
+++ client/app/components/AddressForm.jsx
@@ -1,50 +1,79 @@
 import React from 'react';
+import { css } from 'glamor';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { Controller } from 'react-hook-form';
+
 import { STATES } from '../constants/AppConstants';
 import TextField from 'app/components/TextField';
 import SearchableDropdown from 'app/components/SearchableDropdown';
+
+const city = css({
+  marginBottom: '0'
+});
+
+const cityState = css({
+  display: 'grid',
+  gridGap: '10px',
+  gridTemplateColumns: '320px 130px',
+  marginBottom: '0em',
+  marginTop: '-1em',
+  alignItems: 'center',
+});
+
+const field = css({
+  marginBottom: '1.5em'
+});
+
+const address3 = css({
+  marginTop: '-.5em',
+  marginBottom: '-.65em'
+});
+
+const zipCountry = css({
+  display: 'grid',
+  gridGap: '10px',
+  gridTemplateColumns: '140px 310px',
+  marginBottom: '-.65em'
+});
 
 export const AddressForm = ({ control, register, watch }) => {
   const watchPartyType = watch('partyType');
 
   return (
     <React.Fragment>
-      <FieldDiv>
-        <TextField
-          name="address1"
-          label="Street address 1"
-          inputRef={register}
-          strongLabel
-        />
-      </FieldDiv>
-      <FieldDiv>
-        <TextField
-          name="address2"
-          label="Street address 2"
-          inputRef={register}
-          optional
-          strongLabel
-        />
-      </FieldDiv>
+      <TextField
+        name="address1"
+        label="Street address 1"
+        inputRef={register}
+        strongLabel
+        {...field}
+      />
+      <TextField
+        name="address2"
+        label="Street address 2"
+        inputRef={register}
+        optional
+        strongLabel
+        {...field}
+      />
       {
-        watchPartyType === 'organization' && <StreetAddress>
+        watchPartyType === 'organization' &&
           <TextField
             name="address3"
             label="Street address 3"
             inputRef={register}
             optional
             strongLabel
+            {...address3}
           />
-        </StreetAddress>
       }
-      <CityState>
+      <div {...cityState}>
         <TextField
           name="city"
           label="City"
           inputRef={register}
           strongLabel
+          {...city}
         />
         <Controller
           control={control}
@@ -54,8 +83,8 @@ export const AddressForm = ({ control, register, watch }) => {
           strongLabel
           as={SearchableDropdown}
         />
-      </CityState>
-      <ZipCountry>
+      </div>
+      <div {...zipCountry}>
         <TextField
           name="zip"
           label="Zip"
@@ -68,43 +97,15 @@ export const AddressForm = ({ control, register, watch }) => {
           inputRef={register}
           strongLabel
         />
-      </ZipCountry>
+      </div>
     </React.Fragment>
   );
 };
 
 AddressForm.propTypes = {
-  control: PropTypes.func,
+  control: PropTypes.object,
   register: PropTypes.func,
   watch: PropTypes.func,
 };
-
-const CityState = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 320px 130px;
-  margin-bottom: 0em;
-  margin-top: -1em;
-  align-items: center;
-  input {
-    margin-bottom: 0;
-  }
-`;
-
-const ZipCountry = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 140px 310px;
-  margin-bottom: -.65em;
-`;
-
-const StreetAddress = styled.div`
-  margin-top: -.5em;
-  margin-bottom: -.65em;
-`;
-
-const FieldDiv = styled.div`
-  margin-bottom: 1.5em;
-`;
 
 export default AddressForm;
