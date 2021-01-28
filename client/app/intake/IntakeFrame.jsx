0681730723f8efd9b363431c2243ca9879f895f4
karinamzalez <karinamzalez@gmail.com>
2021-01-27 22:04:40 -0600

    IntakeFrame: wrap AddClaimantPage in FromProvider and IntakeLayout to address axe violations (accessibility) -- pass methods to AddClaimantPage

--- client/app/intake/IntakeFrame.jsx
+++ client/app/intake/IntakeFrame.jsx
@@ -5,6 +5,7 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { FormProvider } from 'react-hook-form';
 import NavigationBar from '../components/NavigationBar';
 import CaseSearchLink from '../components/CaseSearchLink';
 import InboxLink from '../components/InboxLink';
@@ -24,9 +25,24 @@ import { toggleCancelModal, submitCancel } from './actions/intake';
 import { LOGO_COLORS } from '../constants/AppConstants';
 import { IntakeLayout } from './components/IntakeLayout';
 import { AddClaimantPage } from './addClaimant/AddClaimantPage';
+import { AddClaimantButtons } from './addClaimant/AddClaimantButtons';
+import { useAddClaimantForm } from './addClaimant/utils';
+import { ADD_CLAIMANT_PAGE_DESCRIPTION } from 'app/../COPY';
 
 export const IntakeFrame = (props) => {
   const history = useHistory();
+  const methods = useAddClaimantForm();
+
+  const {
+    formState: { isValid },
+    handleSubmit,
+  } = methods;
+  const onSubmit = (formData) => {
+    // Update this to...
+    // Add claimant info to Redux
+    // Probably handle submission of both claimant and remaining intake info (from Review step)
+    return formData;
+  };
 
   const appName = 'Intake';
 
@@ -35,6 +51,8 @@ export const IntakeFrame = (props) => {
     null;
 
   let rightNavElements = <CaseSearchLink newWindow />;
+
+  const handleBack = () => history.goBack();
 
   if (props.featureToggles.inbox) {
     rightNavElements = (
@@ -103,7 +121,23 @@ export const IntakeFrame = (props) => {
             path={PAGE_PATHS.ADD_CLAIMANT}
             title="Add Claimant | Caseflow Intake"
           >
-            <AddClaimantPage />
+            <FormProvider {...methods}>
+              <IntakeLayout
+                buttons={
+                  <AddClaimantButtons
+                    onBack={handleBack}
+                    onSubmit={handleSubmit(onSubmit)}
+                    isValid={isValid}
+                  />
+                }
+              >
+                <h1>Add Claimant</h1>
+                <p>{ADD_CLAIMANT_PAGE_DESCRIPTION}</p>
+                <AddClaimantPage
+                  methods={methods}
+                />
+              </IntakeLayout>
+            </FormProvider>
           </PageRoute>
 
           <PageRoute
