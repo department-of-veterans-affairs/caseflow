6b8d2fe420ecf20c1ef3b8709416005f62304bf0
sahalliburton <61989526+sahalliburton@users.noreply.github.com>
2021-02-02 12:01:44 -0500

    Update client/app/hearings/components/HearingTypeConversionModal.jsx
    Co-authored-by: Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>

--- client/app/hearings/components/HearingTypeConversionModal.jsx
+++ client/app/hearings/components/HearingTypeConversionModal.jsx
@@ -57,7 +57,7 @@ export const HearingTypeConversionModal = ({
       const changedRequestType = formatChangeRequestType(hearingType);
 
       // Determine the closest regional office
-      const closestRegionalOffice = hearingType === 'Video' ? null : HEARING_REQUEST_TYPES.central;
+      const closestRegionalOffice = hearingType === 'Video' ? null : "C";
 
       const data = {
         task: {
