4ae459e02a81eb27e323c1f43857074fe9c458a7
Meckila Britt <meckilabritt@navapbc.com>
2021-02-03 12:42:42 -0800

    Set up nod date update timeline component

--- app/models/serializers/work_queue/appeal_serializer.rb
+++ app/models/serializers/work_queue/appeal_serializer.rb
@@ -37,6 +37,19 @@ class WorkQueue::AppealSerializer
     end
   end
 
+  attribute :nod_date_updates do |object|
+    object.nod_date_updates.map do |nod_date_update|
+      updated_by = nod_date_update.user
+      {
+        old_date: nod_date_update.old_date,
+        new_date: nod_date_update.new_date,
+        change_reason: nod_date_update.change_reason,
+        closed_at: nod_date_update.updated_at, # alias to sort updates with tasks
+        updated_by: updated_by.full_name
+      }
+    end
+  end
+
   attribute :can_edit_request_issues do |object, params|
     AppealRequestIssuesPolicy.new(user: params[:user], appeal: object).editable?
   end
