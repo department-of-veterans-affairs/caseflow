04c76c919c0efe47a2ea15f4f4cecc744fe6073d
Will Drougas <wdrougas@tistatech.com>
2021-02-04 17:59:20 -0500

    migration implemented

---
+++ db/migrate/20210204223904_remove_rating_issue_unique_index_from_decision_issues.rb
@@ -0,0 +1,11 @@
+class RemoveRatingIssueUniqueIndexFromDecisionIssues < Caseflow::Migration
+  def up
+  	add_safe_index :decision_issues, ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_by_disposition_and_ref_id"
+  	remove_index :decision_issues, name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
+  end
+
+  def down
+  	remove_index :decision_issues, name: "decision_issues_by_disposition_and_ref_id"
+  	add_safe_index :decision_issues, ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
+  end
+end
