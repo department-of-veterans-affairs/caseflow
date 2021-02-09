de51198516c88c5955856bed4e5f56aaeac1696c
Will Drougas <wdrougas@tistatech.com>
2021-02-09 15:48:04 -0500

    spacing

--- db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
+++ db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
@@ -1,6 +1,6 @@
 class RemoveRatingIssueIndexFromDecisionIssues < Caseflow::Migration
   def up
-  	remove_index :decision_issues, column: ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
+    remove_index :decision_issues, column: ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
     add_safe_index :decision_issues, :rating_issue_reference_id
   end
 
