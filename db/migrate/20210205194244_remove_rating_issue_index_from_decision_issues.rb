ded6065707d32ca69332ce98acaee009c22cace2
Will Drougas <wdrougas@tistatech.com>
2021-02-09 15:45:20 -0500

    updated migration to add rating issue index

--- db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
+++ db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
@@ -1,5 +1,10 @@
 class RemoveRatingIssueIndexFromDecisionIssues < Caseflow::Migration
-  def change
+  def up
   	remove_index :decision_issues, column: ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
+    add_safe_index :decision_issues, :rating_issue_reference_id
+  end
+
+  def down
+    # Removing uniqueness index, do not want to rollback
   end
 end
