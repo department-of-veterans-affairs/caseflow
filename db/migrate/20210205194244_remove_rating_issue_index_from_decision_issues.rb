09f06a96147acd4df85b4121a5a0a3d20cc396bb
Will Drougas <wdrougas@tistatech.com>
2021-02-09 19:35:51 -0500

    migration implemented and test added

--- db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
+++ db/migrate/20210205194244_remove_rating_issue_index_from_decision_issues.rb
@@ -1,10 +1,13 @@
 class RemoveRatingIssueIndexFromDecisionIssues < Caseflow::Migration
   def up
+    # add_index :decision_issues, ["rating_issue_reference_id", "disposition", "participant_id"], unique: true, name: "decision_issues_uniq_by_disposition_and_ref_id", algorithm: :concurrently
     remove_index :decision_issues, column: ["rating_issue_reference_id", "disposition", "participant_id"], name: "decision_issues_uniq_by_disposition_and_ref_id", unique: true
     add_safe_index :decision_issues, :rating_issue_reference_id
   end
 
   def down
     # Removing uniqueness index, do not want to rollback
+    # remove_index :decision_issues, name: "index_decision_issues_on_rating_issue_reference_id"
+    # add_index :decision_issues, ["rating_issue_reference_id", "disposition", "participant_id"], unique: true, name: "decision_issues_uniq_by_disposition_and_ref_id", algorithm: :concurrently
   end
 end
