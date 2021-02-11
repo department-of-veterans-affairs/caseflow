fc5fe1dec1b9a5be74022a6c133e20d22d92e1dd
Matthew Kerns <matthewkerns@TST-C02YV319LVDC.local>
2021-02-11 10:49:34 -0500

    Made recommended changes based on CodeClimate issues

--- app/services/update_veteran_attribs_service.rb
+++ app/services/update_veteran_attribs_service.rb
@@ -3,11 +3,17 @@
 class UpdateVeteranAttribsService
   def update_veterans_for_appeals(appeal_ids)
     appeal_ids.map do |appeal_id|
-      appeal = Appeal.find_appeal_by_uuid_or_find_or_create_legacy_appeal_by_vacols_id(appeal_id)
-      if appeal.veteran
-        appeal.veteran.refresh_attributes
-        appeal.veteran.id
-      end
+      update_veteran_for_appeal(appeal_id)
+    end
+  end
+
+  private
+
+  def update_veteran_for_appeal(appeal_id)
+    appeal = Appeal.find_appeal_by_uuid_or_find_or_create_legacy_appeal_by_vacols_id(appeal_id)
+    if appeal.veteran
+      appeal.veteran.refresh_attributes
+      appeal.veteran.id
     end
   end
 end
