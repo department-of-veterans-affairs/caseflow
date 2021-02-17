1ab3c5ac7ffb1f8614fd280e6cbea995f537709b
Will Drougas <wdrougas@tistatech.com>
2021-02-17 08:57:53 -0500

    lint fixes

--- app/repositories/task_action_repository.rb
+++ app/repositories/task_action_repository.rb
@@ -19,9 +19,10 @@ class TaskActionRepository
 
     def mail_assign_to_organization_data(task, user = nil)
       options = MailTask.subclass_routing_options(user)
-      valid_options = if task.appeal.outcoded? 
-            options.reject { |opt| opt[:value] == "DocketSwitchMailTask" }  
-          else 
+      valid_options = 
+          if task.appeal.outcoded?
+            options.reject { |opt| opt[:value] == "DocketSwitchMailTask" }
+          else
             options.reject { |opt| opt[:value] == "VacateMotionMailTask" }
           end
       { options: valid_options }
