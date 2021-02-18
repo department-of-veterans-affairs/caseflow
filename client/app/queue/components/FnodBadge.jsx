ffe2cd30bcb3ddde3315ea0d56c35562d05201fd
charleyf <Charles.Ferguson@va.gov>
2021-02-18 11:56:58 -0500

    Fix leftover code missed when merging

--- client/app/queue/components/FnodBadge.jsx
+++ client/app/queue/components/FnodBadge.jsx
@@ -3,25 +3,11 @@ import * as React from 'react';
 
 import { COLORS } from '../../constants/AppConstants';
 import Badge from './Badge';
-import { DateString } from '../../util/DateUtil';
 
 /**
  * Component to display a FNOD badge if Veteran.date_of_death is not null and
  * the Veteran is the appellant.
  */
-
-const listStyling = css({
-  listStyle: 'none',
-  textAlign: 'left',
-  marginBottom: 0,
-  padding: 0,
-  '& > li': {
-    marginBottom: 0,
-    '& > strong': {
-      color: COLORS.WHITE
-    }
-  }
-});
 
 const FnodBadge = (props) => {
   const { veteranAppellantDeceased, uniqueId, show, tooltipText } = props;
