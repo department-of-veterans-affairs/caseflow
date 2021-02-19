787087ca1cd2f1587149a9b1d64f3085b92b6cdb
Kat Tipton <kathryn.tipton@gmail.com>
2021-02-19 11:57:55 -0500

    WIP Functional SameMandate date selector

--- client/app/queue/AddCavcRemandView.jsx
+++ client/app/queue/AddCavcRemandView.jsx
@@ -44,10 +44,12 @@ const typeOptions = _.map(_.keys(CAVC_DECISION_TYPES), (key) => ({
   value: key
 }));
 
-const isMandateDifferentOptions = {
-    id: decisionIssue.id,
-    label: decisionIssue.description
-};
+const isMandateSameOptions = [
+  { displayText: 'Yes',
+    value: 'true' },
+  { displayText: 'No',
+    value: 'false' },
+];
 
 const isMandateProvidedOptions = [
   { displayText: 'Yes',
@@ -85,7 +87,7 @@ const AddCavcRemandView = (props) => {
   const [issues, setIssues] = useState({});
   const [instructions, setInstructions] = useState(null);
   const [isMandateProvided, setMandateProvided] = useState('true');
-  const [isMandateDifferent, setMandateDifferent] = useState({});
+  const [isMandateSame, setMandateSame] = useState('true');
 
   const supportedDecisionTypes = {
     [CAVC_DECISION_TYPES.remand]: featureToggles.cavc_remand,
@@ -124,10 +126,6 @@ const AddCavcRemandView = (props) => {
   // populate all of our checkboxes on initial render
   useEffect(() => selectAllIssues(), []);
 
-  const onMandateDiffersChange = (evt) => {
-    setMandateDifferent({ ...issues, [evt.target.name]: evt.target.checked });
-  };
-
   const onIssueChange = (evt) => {
     setIssues({ ...issues, [evt.target.name]: evt.target.checked });
   };
@@ -139,7 +137,7 @@ const AddCavcRemandView = (props) => {
   const mandateAvailable = () => {
     return !(type === CAVC_DECISION_TYPES.remand && mdrSubtype()) && (isMandateProvided === 'true');
   };
-  const mandateDiffers = () => isMandateDifferent === 'true';
+  const mandateSame = () => isMandateSame === 'true';
   const validDocketNumber = () => (/^\d{2}-\d{1,5}$/).exec(docketNumber);
   const validJudge = () => Boolean(judge);
   const validDecisionDate = () => Boolean(decisionDate) && validateDateNotInFuture(decisionDate);
@@ -263,13 +261,13 @@ const AddCavcRemandView = (props) => {
     vertical
   />;
 
-  const mandateDiffersField = <CheckboxGroup
+  const mandateSameField = <RadioField
     styling={radioLabelStyling}
     label={COPY.CAVC_REMAND_MANDATE_DIFFER}
     name="remand-different-toggle"
-    options={isMandateDifferentOptions}
-    values={isMandateDifferent}
-    onChange={(val) => onMandateDiffersChange(val)}
+    options={isMandateSameOptions}
+    values={isMandateSame}
+    onChange={(val) => setMandateSame(val)}
     strongLabel
   />;
 
@@ -317,9 +315,6 @@ const AddCavcRemandView = (props) => {
     strongLabel
   />;
 
-  const mandateSection = {
-  }
-
   const issuesField = <React.Fragment>
     <legend><strong>{COPY.CAVC_ISSUES_LABEL}</strong></legend>
     <Button
@@ -364,9 +359,9 @@ const AddCavcRemandView = (props) => {
       {type !== CAVC_DECISION_TYPES.remand && mandateProvidedField }
       {decisionField}
       {type === CAVC_DECISION_TYPES.remand && mdrSubtype() && mdrBanner }
-      {mandateAvailable() && mandateDiffersField }
-      {mandateAvailable() && mandateDiffers() && judgementField }
-      {mandateAvailable() && mandateDiffers() && mandateField }
+      {mandateAvailable() && mandateSameField }
+      {mandateAvailable() && !mandateSame() && judgementField }
+      {mandateAvailable() && !mandateSame() && mandateField }
       {!mandateAvailable() && type !== CAVC_DECISION_TYPES.remand && noMandateBanner }
       {issuesField}
       {instructionsField}
