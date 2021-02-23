f0befa648044779d820fe3c7627bee0a3e1caad9
Yoom Lam <Dung.Lam1@va.gov>
2021-02-23 13:39:15 -0600

    type: Appeals
    Co-authored-by: Matt Wagner <mattwagner@navapbc.com>

--- db/migrate/20210222214631_add_cavc_mdr_federal_circuit.rb
+++ db/migrate/20210222214631_add_cavc_mdr_federal_circuit.rb
@@ -3,6 +3,6 @@
 class AddCavcMdrFederalCircuit < Caseflow::Migration
   def change
     add_column :cavc_remands, :federal_circuit, :boolean,
-               comment: "Whether the case has been appealed to the US Court of Appeal for the Federal Circuit"
+               comment: "Whether the case has been appealed to the US Court of Appeals for the Federal Circuit"
   end
 end
