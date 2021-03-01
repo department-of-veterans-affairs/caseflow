7652161c37a564a65d23f160f677230c921da4eb
Will Drougas <wdrougas@tistatech.com>
2021-03-01 15:48:04 -0500

    story rendering

--- client/app/queue/docketSwitch/DocketSwitchAlertBanner.stories.js
+++ client/app/queue/docketSwitch/DocketSwitchAlertBanner.stories.js
@@ -1,43 +1,49 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 
 import DocketSwitchAlertBanner from './DocketSwitchAlertBanner';
+
+const RouterDecorator = (Story) => (
+	<MemoryRouter>
+		<Story />
+	</MemoryRouter>
+);
 
 const fullGrantDocketSwitch = {
   disposition: 'granted',
   docket_type: 'direct_review',
-  new_docket_stream_id: '2',
   old_docket_stream_id: '1'
 };
+
+const partialGrantDocketSwitch = {
+	disposition: 'partially_granted',
+	docket_type: 'evidence_submission',
+	new_docket_stream_id: '2',
+	old_docket_stream_id: '3'
+}
 
 const appeal = {
   externalId: '1',
   docketSwitch: fullGrantDocketSwitch
 };
 
-// const partialGrantDocketSwitch = {
-// 	disposition: 'partially_granted',
-// 	docket_type: 'evidence_submission'
-// }
+const appeal2 = {
+	externalId: '2',
+	docketSwitch: partialGrantDocketSwitch
+}
 
-// const appeal2 = {
-//   externalId: '2',
-//   issues: [
-//     { id: 3, program: 'compensation', description: 'issue description 3' },
-//     { id: 4, program: 'compensation', description: 'issue description 4' },
-//   ],
-//   veteranFullName: 'John Doe',
-//   veteranFileNumber: '123456789',
-//   veteranInfo: {
-//     veteran: {
-//       full_name: 'John Doe',
-//     },
-//   },
-// };
+const appeal3 = {
+	externalId: '3',
+	switchedDocket: partialGrantDocketSwitch
+}
+
+
 
 export default {
   title: 'Queue/Docket Switch/DocketSwitchAlertBanner',
   component: DocketSwitchAlertBanner,
   parameters: {},
+  decorators: [RouterDecorator],
   args: {
   	appeal: appeal
   }
@@ -47,17 +53,17 @@ const Template = (args) => (
   <DocketSwitchAlertBanner {...args} />
 );
 
-export const Basic = Template.bind({});
-// FullGrant.args = {
-//   appeal: appeal
-// };
+export const FullGrant = Template.bind({});
+FullGrant.args = {
+  appeal: appeal
+};
 
-// export const PartialGrantNewDocket = Template.bind({});
-// PartialGrantNewDocket.args = {
-//   appeal: appeal
-// };
+export const PartialGrantNewDocket = Template.bind({});
+PartialGrantNewDocket.args = {
+  appeal: appeal2
+};
 
-// export const PartialGrantOldDocket = Template.bind({});
-// PartialGrantOldDocket.args = {
-//   appeal: appeal
-// };
+export const PartialGrantOldDocket = Template.bind({});
+PartialGrantOldDocket.args = {
+  appeal: appeal3
+};
