8a73eb15686e6ab4bdfa6da31330b11150dd4e74
Will Drougas <wdrougas@tistatech.com>
2021-03-02 18:27:30 -0500

    lint fix

--- client/app/queue/TaskSnapshot.jsx
+++ client/app/queue/TaskSnapshot.jsx
@@ -60,8 +60,8 @@ export const TaskSnapshot = ({ appeal, hideDropdown, tasks, showPulacCerulloAler
       </div>
       <div {...alertStyling} {...sectionSegmentStyling}>
         { appeal.switchedDockets.map((docketSwitch) =>
-          <DocketSwitchAlertBanner appeal={appeal} docketSwitch={docketSwitch}/>
-        ) }
+          <DocketSwitchAlertBanner appeal={appeal} docketSwitch={docketSwitch} />
+        )}
       </div>
       <div {...sectionSegmentStyling}>{sectionBody}</div>
     </div>
