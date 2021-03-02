3b64a4d59bc572a7ba79ba243be61d5feb1d8aa1
Yang Yang <yangyang@navapbc.com>
2021-03-02 11:45:58 -0500

    Fix microsecond precision loss by avoiding PG roundtrip

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -569,15 +569,13 @@ describe User, :all_dbs do
       it "updates last_login_at if it was more than 5 minutes ago" do
         user = create(:user, last_login_at: Time.zone.now - 10.minutes)
         session["user"]["pg_user_id"] = user.id
-        expect(subject).to eq user
-        expect(user.reload.last_login_at).to eq Time.zone.now
+        expect(subject.last_login_at).to eq Time.zone.now
       end
 
       it "does not update last_login_at if it was less than 5 minutes ago" do
         user = create(:user, last_login_at: Time.zone.now - 1.minute)
         session["user"]["pg_user_id"] = user.id
-        expect(subject).to eq user
-        expect(user.reload.last_login_at).to eq Time.zone.now - 1.minute
+        expect(subject.last_login_at).to eq Time.zone.now - 1.minute
       end
 
       describe "check SQL queries are only called when needed" do
