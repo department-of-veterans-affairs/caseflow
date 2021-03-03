bd6b1d80b09e849d01e06466b93805072c0125ed
Will Drougas <wdrougas@tistatech.com>
2021-03-02 21:09:54 -0500

    quick spacing fix

--- client/app/queue/TaskSnapshot.jsx
+++ client/app/queue/TaskSnapshot.jsx
@@ -60,8 +60,9 @@ export const TaskSnapshot = ({ appeal, hideDropdown, tasks, showPulacCerulloAler
       </div>
       <div {...alertStyling} {...sectionSegmentStyling}>
         { appeal.switchedDockets ? appeal.switchedDockets.map((docketSwitch) =>
-          <DocketSwitchAlertBanner appeal={appeal} docketSwitch={docketSwitch} />
-        ) : ''}
+          <DocketSwitchAlertBanner appeal={appeal} docketSwitch={docketSwitch} />) :
+          ''
+        }
       </div>
       <div {...sectionSegmentStyling}>{sectionBody}</div>
     </div>
