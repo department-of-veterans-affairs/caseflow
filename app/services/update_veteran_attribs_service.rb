67894aff2e735c70be8fbbedcd9edde400740b5a
Brian Gantick <briangantick@navapbc.com>
2021-03-07 18:45:59 -0500

    Set inital array

--- app/services/update_veteran_attribs_service.rb
+++ app/services/update_veteran_attribs_service.rb
@@ -4,7 +4,8 @@ class UpdateVeteranAttribsService
   def update_veterans_for_appeals(appeal_ids)
     ama_ids, legacy_ids = appeal_ids.partition { |id| UUID_REGEX.match?(id) }
 
-    @appeals = Appeal.includes(:veterans).where(id: ama_ids)
+    @appeals = []
+    @appeals << Appeal.includes(:veterans).where(id: ama_ids)
     @appeals << LegacyAppeal.includes(:veterans).where(id: legacy_ids)
 
     @appeals.each { |appeal| update_veteran_for_appeal(appeal) }
