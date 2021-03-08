bbf9f092f686007350d44d7213fe4f26d7f40a3b
Brian Gantick <briangantick@navapbc.com>
2021-03-08 06:53:39 -0500

    clean up and refine

--- app/services/update_veteran_attribs_service.rb
+++ app/services/update_veteran_attribs_service.rb
@@ -3,23 +3,22 @@
 class UpdateVeteranAttribsService
   UUID_REGEX = /^\h{8}-\h{4}-\h{4}-\h{4}-\h{12}$/.freeze
 
+  class << self
+    def update_veterans_for_appeals(appeal_ids)
+      ama_ids, legacy_ids = appeal_ids.partition { |id| UUID_REGEX.match?(id) }
 
-  def self.update_veterans_for_appeals(appeal_ids)
-    ama_ids, legacy_ids = appeal_ids.partition { |id| UUID_REGEX.match?(id) }
+      appeals ||= []
+      appeals << Appeal.includes(:veterans).where(id: ama_ids)
+      appeals << LegacyAppeal.includes(:veterans).where(id: legacy_ids)
 
-    appeals ||= []
-    appeals << Appeal.includes(:veterans).where(id: ama_ids)
-    appeals << LegacyAppeal.includes(:veterans).where(id: legacy_ids)
+      appeals.each { |appeal| update_veteran_for_appeal(appeal) }
+    end
 
-    appeals.each { |appeal| update_veteran_for_appeal(appeal) }
-  end
-
-  private
-
-  def self.update_veteran_for_appeal(appeal)
-    if appeal.veteran
-      appeal.veteran.refresh_attributes
-      appeal.veteran.id
+    def update_veteran_for_appeal(appeal)
+      if appeal.veteran
+        appeal.veteran.refresh_attributes
+        appeal.veteran.id
+      end
     end
   end
 end
