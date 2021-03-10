91f532024fae5e0c45d11dd02045bbebeeccf26b
Brian Gantick <briangantick@navapbc.com>
2021-03-10 09:44:09 -0500

    Make tests more explicit

--- spec/services/update_veteran_attribs_service_spec.rb
+++ spec/services/update_veteran_attribs_service_spec.rb
@@ -11,18 +11,23 @@ describe UpdateVeteranAttribsService do
     let(:date_of_death) { "2020-12-08" }
 
     before do
-      # veteran.unload_bgs_record # force it to reload from BGS
       Fakes::BGSService.edit_veteran_record(veteran.file_number, :date_of_death, date_of_death)
-      Fakes::BGSService.edit_veteran_record(veteran2.file_number, :date_of_death, date_of_death)
     end
 
     subject { UpdateVeteranAttribsService.update_veterans_for_appeals(appeal_ids) }
 
-    it "updates our veteran_records date_of_death" do
-      expect { subject }.not_to raise_error
+    it "runs update on all veterans linked to appeals that are passed in" do
+      expect(subject.count).to eq(2)
+    end
 
+    it "updates date_of_death for a veteran with a change in BGS" do
+      subject
       expect(veteran.reload.date_of_death).to eq(Date.parse(date_of_death))
-      expect(veteran2.reload.date_of_death).to eq(Date.parse(date_of_death))
+    end
+
+    it "doesn't update date_of_death if there is no change" do
+      subject
+      expect(veteran2.reload.date_of_death).to eq(nil)
     end
   end
 end
