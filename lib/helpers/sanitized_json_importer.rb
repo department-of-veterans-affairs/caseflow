cfd0c92111ca754019713ad7f44b7f52f7bbaa84
yoomlam <Dung.Lam1@va.gov>
2021-03-10 17:40:48 -0600

    more refactoring

--- lib/helpers/sanitized_json_importer.rb
+++ lib/helpers/sanitized_json_importer.rb
@@ -11,12 +11,14 @@ class SanitizedJsonImporter
     new(File.read(filename))
   end
 
+  ID_OFFSET = 1_000_000_000
+
   def initialize(file_contents, id_offset: ID_OFFSET)
     @records_hash = JSON.parse(file_contents)
     @imported_records = {}
 
     # Keep track of id mappings for these record types to reassociate to newly imported records
-    @mapped_ids = {
+    @id_mapping = {
       Appeal.name.underscore => {},
       User.name.underscore => {},
       Organization.name.underscore => {}
@@ -49,47 +51,45 @@ class SanitizedJsonImporter
     imported_records
   end
 
+  # TODO: These 3 diff_* methods will be refactored later
   # Compare tasks
   def self.diff_task_hashes(task_list, appeal)
     a3_tasks = JSON.parse(appeal.tasks.order(:id).to_json(methods: :type))
-    a3_hash = a3_tasks.index_by { |t| t["id"] }
+    a3_hash = a3_tasks.index_by { |task| task["id"] }
 
     task_list.map do |task|
-      task_hash = task.to_h
-      hash1 = task_hash
-      hash2 = a3_hash[hash1["id"]]
-      diff_hashes(hash1, hash2)
+      diff_hashes(task.to_h, a3_hash[task_hash["id"]])
     end
   end
 
-  ID_OFFSET = 1_000_000_000
-
-  def self.diff_records(record1, record2, **kwargs)
-    hash1 = SanitizedJsonExporter.record_to_hash(record1)
-    hash2 = SanitizedJsonExporter.record_to_hash(record2)
-    diff_hashes(hash1, hash2, **kwargs)
+  def self.diff_records(record_a, record_b, **kwargs)
+    hash_a = SanitizedJsonExporter.record_to_hash(record_a)
+    hash_b = SanitizedJsonExporter.record_to_hash(record_b)
+    diff_hashes(hash_a, hash_b, **kwargs)
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity
-  def self.diff_hashes(hash1, hash2, ignore_id_offset: true, convert_timestamps: true)
+  # :reek:BooleanParameter
+  # :reek:LongParameterList
+  def self.diff_hashes(hash_a, hash_b, ignore_id_offset: true, convert_timestamps: true)
     # https://stackoverflow.com/questions/4928789/how-do-i-compare-two-hashes
-    array_diff = (hash2.to_a - hash1.to_a) + (hash1.to_a - hash2.to_a)
+    array_diff = (hash_b.to_a - hash_a.to_a) + (hash_a.to_a - hash_b.to_a)
 
     # Ignore some differences if they are expected or equivalent
     array_diff.map(&:first).uniq.inject([]) do |diffs, key|
-      if ignore_id_offset && integer?(hash2[key])
-        next diffs if (hash2[key].to_i - hash1[key].to_i).abs == ID_OFFSET
+      if ignore_id_offset && integer?(hash_b[key])
+        next diffs if (hash_b[key].to_i - hash_a[key].to_i).abs == ID_OFFSET
       end
 
-      if convert_timestamps && (hash1[key].try(:to_time) || hash2[key].try(:to_time))
-        time1 = hash1[key].try(:to_time)&.to_s || hash1[key]
-        time2 = hash2[key].try(:to_time)&.to_s || hash2[key]
-        next diffs if time1 == time2
+      if convert_timestamps && (hash_a[key].try(:to_time) || hash_b[key].try(:to_time))
+        time_a = hash_a[key].try(:to_time)&.to_s || hash_a[key]
+        time_b = hash_b[key].try(:to_time)&.to_s || hash_b[key]
+        next diffs if time_a == time_b
 
-        next diffs << [key, time1, time2]
+        next diffs << [key, time_a, time_b]
       end
 
-      next diffs << [key, hash1[key], hash2[key]]
+      next diffs << [key, hash_a[key], hash_b[key]]
     end
   end
   # rubocop:enable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity
@@ -111,8 +111,7 @@ class SanitizedJsonImporter
   # * http://ashleyangell.com/2019/06/skipping-an-activerecord-callback-programatically/
   module SkipCallbacks
     def run_callbacks(kind, *args, &block)
-      case kind
-      when :save, :create
+      if [:save, :create].include?(kind)
         # puts "==== Skipping callbacks for #{kind}: #{args}"
         nil
       else
@@ -128,9 +127,10 @@ class SanitizedJsonImporter
   def import_record(clazz, key: clazz.name, obj_hash: @records_hash[key])
     fail "No JSON data for key '#{key}'.  Keys: #{@records_hash.keys}" unless obj_hash
 
-    obj_description = "#{obj_hash['id']} #{obj_hash['type']} #{obj_hash.select { |k, _v| k.include?('_id') }}"
-    # Don't import if it already exists
-    if existing_record(clazz, obj_hash) && NONDUPLICATE_TYPES.include?(clazz)
+    obj_description = "#{obj_hash['id']} #{obj_hash['type']} " \
+                      "#{obj_hash.select { |obj_key, _v| obj_key.include?('_id') }}"
+    # Don't import if certain types of records already exists
+    if NONDUPLICATE_TYPES.include?(clazz) && existing_record(clazz, obj_hash)
       puts "Using existing #{clazz} instead of importing: #{obj_description}"
       return
     end
@@ -140,13 +140,15 @@ class SanitizedJsonImporter
 
     adjust_unique_identifiers(clazz, obj_hash)
 
+    # Handle Organization type specially because each organization has a `singleton`
     if clazz == Organization && !org_already_exists?(obj_hash)
       puts "Creating #{clazz} '#{obj_hash['name']}' with original id #{obj_description}"
       return clazz.create!(obj_hash)
     end
 
-    adjust_ids(clazz, obj_hash)
+    adjust_ids_by_offset(clazz, obj_hash)
     reassociate_with_imported_records(clazz, obj_hash)
+
     puts "Creating #{clazz} #{obj_description}"
     create_new_record(orig_id, clazz, obj_hash)
   end
@@ -155,6 +157,7 @@ class SanitizedJsonImporter
     Organization.find_by(url: obj_hash["url"]) || Organization.find_by(id: obj_hash["id"])
   end
 
+  # :reek:FeatureEnvy
   def adjust_unique_identifiers(clazz, obj_hash)
     label = if clazz <= Organization
               obj_hash["url"] += "_imported" if Organization.find_by(url: obj_hash["url"])
@@ -168,21 +171,29 @@ class SanitizedJsonImporter
     end
   end
 
+  # :reek:FeatureEnvy
+  def adjust_ids_by_offset(clazz, obj_hash)
+    obj_hash["id"] += @id_offset
+
+    if clazz <= Task
+      obj_hash["parent_id"] += @id_offset if obj_hash["parent_id"]
+    end
+  end
+
+  ID_MAPPING_TYPES = [Appeal, User, Organization].freeze
+
+  # :reek:FeatureEnvy
   def create_new_record(orig_id, clazz, obj_hash)
+    # Record new id for certain record types
+    @id_mapping[clazz.name.underscore][orig_id] = obj_hash["id"] if ID_MAPPING_TYPES.include?(clazz)
+
     case clazz.name
-    when Appeal.name
-      @mapped_ids[Appeal.name.underscore][orig_id] = obj_hash["id"]
     when Task.name
       # Create the task without validation or callbacks
       new_task = Task.new(obj_hash)
       new_task.extend(SkipCallbacks) # patch only this in-memory instance of the task
       new_task.save(validate: false)
       return new_task
-    when User.name
-      @mapped_ids[User.name.underscore][orig_id] = obj_hash["id"]
-    when Organization.name
-      # Create org with different id
-      @mapped_ids[Organization.name.underscore][orig_id] = obj_hash["id"]
     else
       if clazz == Claimant
         # Per appeal, set of participant_ids must be unique
@@ -194,42 +205,43 @@ class SanitizedJsonImporter
     clazz.create!(obj_hash)
   end
 
-  def mapped_appeal_ids
-    @mapped_ids[Appeal.name.underscore]
+  def appeal_id_mapping
+    @id_mapping[Appeal.name.underscore]
   end
 
-  def mapped_user_ids
-    @mapped_ids[User.name.underscore]
+  def user_id_mapping
+    @id_mapping[User.name.underscore]
   end
 
-  def mapped_org_ids
-    @mapped_ids[Organization.name.underscore]
+  def org_id_mapping
+    @id_mapping[Organization.name.underscore]
   end
 
   # rubocop:disable Metrics/PerceivedComplexity
+  # :reek:FeatureEnvy
   def reassociate_with_imported_records(clazz, obj_hash)
     # pp "Reassociate #{clazz}"
     if clazz <= Claimant
       if obj_hash["decision_review_type"] == "Appeal"
-        reassociate(obj_hash, "decision_review_id", mapped_appeal_ids)
+        reassociate(obj_hash, "decision_review_id", appeal_id_mapping)
       else
         puts "To-do: HLR, SC, LegacyAppeal"
       end
     elsif clazz <= Task
-      reassociate(obj_hash, "appeal_id", mapped_appeal_ids)
-      reassociate(obj_hash, "assigned_by_id", mapped_user_ids)
+      reassociate(obj_hash, "appeal_id", appeal_id_mapping)
+      reassociate(obj_hash, "assigned_by_id", user_id_mapping)
 
       if obj_hash["assigned_to_type"] == "User"
-        reassociate(obj_hash, "assigned_to_id", mapped_user_ids)
+        reassociate(obj_hash, "assigned_to_id", user_id_mapping)
       elsif obj_hash["assigned_to_type"] == "Organization"
-        reassociate(obj_hash, "assigned_to_id", mapped_org_ids)
+        reassociate(obj_hash, "assigned_to_id", org_id_mapping)
       end
     end
   end
   # rubocop:enable Metrics/PerceivedComplexity
 
-  def reassociate(obj_hash, id_field, id_mapping)
-    obj_hash[id_field] = id_mapping[obj_hash[id_field]] if id_mapping[obj_hash[id_field]]
+  def reassociate(obj_hash, id_field, record_id_mapping)
+    obj_hash[id_field] = record_id_mapping[obj_hash[id_field]] if record_id_mapping[obj_hash[id_field]]
   end
 
   def existing_record(clazz, obj_hash)
@@ -237,6 +249,7 @@ class SanitizedJsonImporter
     existing_record if existing_record && same_unique_attributes?(existing_record, obj_hash)
   end
 
+  # :reek:FeatureEnvy
   def same_unique_attributes?(existing_record, obj_hash)
     case existing_record
     when Organization
@@ -245,16 +258,8 @@ class SanitizedJsonImporter
       existing_record.css_id == obj_hash["css_id"]
     when Claimant
       # check if claimant is associated with appeal we just imported
-      new_appeal_id = mapped_appeal_ids[obj_hash["decision_review_id"]]
+      new_appeal_id = appeal_id_mapping[obj_hash["decision_review_id"]]
       existing_record.decision_review_id == new_appeal_id
-    end
-  end
-
-  def adjust_ids(clazz, obj_hash)
-    obj_hash["id"] += @id_offset
-
-    if clazz <= Task
-      obj_hash["parent_id"] += @id_offset if obj_hash["parent_id"]
     end
   end
 
