3c11853569a6de945b0626ec70468e0c14336381
Tomas Apodaca <Thomas.Apodaca@va.gov>
2021-03-16 14:57:19 -0700

    missed this rename/symbol key opportunity

--- app/services/hearing_schedule/validate_judge_spreadsheet.rb
+++ app/services/hearing_schedule/validate_judge_spreadsheet.rb
@@ -101,7 +101,7 @@ class HearingSchedule::ValidateJudgeSpreadsheet
     if rows_without_judge_name_match.count > 0
       @errors << JudgeNameDoesNotMatchIdInDatabase.new(
         "These judges names do not match the database: " +
-          rows_without_judge_name_match.map { |judge| "#{judge['name']} (id: #{judge['vlj_id']})" }.join(", ")
+          rows_without_judge_name_match.map { |row| "#{row[:name]} (id: #{row[:vlj_id]})" }.join(", ")
       )
     end
   end
