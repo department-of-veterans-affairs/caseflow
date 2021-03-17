916e760c1334450bbc90fe0bbcf317d3bebaa858
sahalliburton <shalliburton@tistatech.com>
2021-03-17 10:01:30 -0700

    Refactored the LastRetrievalAlert component (#15519)

--- client/app/2.0/utils/format.js
+++ client/app/2.0/utils/format.js
@@ -1,9 +1,41 @@
 // External Dependencies
 import { sortBy, compact } from 'lodash';
+import moment from 'moment';
 
 // Local Dependencies
 import { categoryFieldNameOfCategoryName } from 'app/reader/utils';
 import { documentCategories } from 'app/reader/constants';
+
+export const CACHE_TIMEOUT_HOURS = 3;
+
+/**
+ * Helper Method to format the times for the Last Retrieval Alert
+ * @param {string} manifestVbmsFetchedAt -- The last time the VBMS Manifest was fetched
+ * @param {string} manifestVvaFetchedAt -- The last time the VVA Manifest was fetched
+ */
+export const formatAlertTime = (manifestVbmsFetchedAt, manifestVvaFetchedAt) => {
+  // Create the formatted times
+  const formattedTimes = {
+    staleCacheTime: moment().subtract(CACHE_TIMEOUT_HOURS, 'h'),
+    vbmsTimestamp: moment(manifestVbmsFetchedAt, 'MM/DD/YY HH:mma Z'),
+    vvaTimestamp: moment(manifestVvaFetchedAt, 'MM/DD/YY HH:mma Z'),
+  };
+
+  // Calculate whether the cache is stale
+  const stale = formattedTimes.vbmsTimestamp.isBefore(formattedTimes.staleCacheTime) ||
+    formattedTimes.vvaTimestamp.isBefore(formattedTimes.staleCacheTime);
+
+  // Check that document manifests have been received from VVA and VBMS
+  if (stale) {
+    // Calculate the time
+    formattedTimes.now = moment();
+    formattedTimes.vbmsDiff = formattedTimes.diff(formattedTimes.vbmsTimestamp, 'hours');
+    formattedTimes.vvaDiff = formattedTimes.diff(formattedTimes.vvaTimestamp, 'hours');
+  }
+
+  // Return all of the Formatted Times
+  return formattedTimes;
+};
 
 /**
  * Helper Method to format the message for the filters in the document list header
