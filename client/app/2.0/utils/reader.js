90298c8be0a7d5a747f0922095e043f9eefe886a
sahalliburton <shalliburton@tistatech.com>
2021-03-17 09:56:16 -0700

    Shalliburton/15435 05 components pdf ui (#15559)
    * Added initial files
    
    * Refactored component parts of PdfUI

--- client/app/2.0/utils/reader.js
+++ client/app/2.0/utils/reader.js
@@ -23,6 +23,30 @@ export const setDocumentDetails = (state) => ({
 });
 
 /**
+ * Helper Method to display search text on document search
+ * @param {string} searchTerm -- The term which is being search
+ * @param {number} totalMatchesInFile -- The total matches to the search term in the current file
+ * @param {number} currentMatchIndex -- The Current Index of the match
+ */
+export const formatSearchText = (searchTerm, totalMatchesInFile, currentMatchIndex) => {
+  // Check the match index if there is a search term
+  if (searchTerm.length) {
+    // Return the Matches in file if found
+    if (totalMatchesInFile > 0) {
+      return `${currentMatchIndex + 1} of ${totalMatchesInFile}`;
+    } else if (totalMatchesInFile > 9999) {
+      return `${currentMatchIndex + 1} of many`;
+    }
+
+    // Return zero matches if none found
+    return '0 of 0';
+  }
+
+  // Default to return empty text
+  return '';
+};
+
+/**
  * Helper Method to get the page number by page index
  * @param {number} index -- The index of the current page
  */
@@ -51,3 +75,4 @@ export const translateX = (rotation, outerHeight, outerWidth) =>
  */
 export const columnCount = (width, pageWidth, numPages) =>
   Math.min(Math.max(Math.floor(width / pageWidth), 1), numPages);
+
