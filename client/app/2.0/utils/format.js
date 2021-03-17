dbf10c7aa7426bbf8ead7249df2fb5eda80cda5c
sahalliburton <shalliburton@tistatech.com>
2021-03-17 10:03:38 -0700

    Shalliburton/15435 05 components back to queue link (#15515)
    * Refactored BackToQueueLink component
    
    * Updated paths for styles

--- client/app/2.0/utils/format.js
+++ client/app/2.0/utils/format.js
@@ -9,6 +9,28 @@ import { documentCategories } from 'app/reader/constants';
 export const CACHE_TIMEOUT_HOURS = 3;
 
 /**
+ * Helper Method to format the redirect text in the `Back to Queue Link`
+ * @param {object} props -- The format components for the redirect link
+ */
+export const formatRedirectText = ({ queueTaskType, veteranFullName, vbmsId }) => {
+  // Set the Base text
+  const text = '&lt; Back to ';
+
+  // Set the text to your cases if there is no task type
+  if (!queueTaskType) {
+    return `${text}your cases`;
+  }
+
+  // Set the Text to the Task Type if we do not have the Veterans full name
+  if (!veteranFullName) {
+    return text + queueTaskType;
+  }
+
+  // Default to the Veterans name and VBMS ID
+  return `${text}${veteranFullName} (${vbmsId})`;
+};
+
+/**
  * Helper Method to format the Appeal type based on claim info
  * @param {Object} claim -- The claim to determine the appeal info
  * @returns {string} -- The formatted appeal type
