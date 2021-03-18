d50c55f798285d47c4fd6ecf8ce65ef655fdeb2a
charleyf <Charles.Ferguson@va.gov>
2021-03-18 16:42:05 -0400

    Put storybook metadata into stories file

--- client/app/components/NumberField.stories.js
+++ client/app/components/NumberField.stories.js
@@ -2,9 +2,22 @@ import React, { useState } from 'react';
 
 import NumberField from './NumberField';
 
+export default {
+  title: 'Commons/Components/Form Fields/NumberField',
+  component: NumberField,
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {
+  },
+  argTypes: {
+  },
+};
+
 const Template = () => {
   const [number, setNumber] = useState(5);
   // Todo, add readonly story
+  // Todo, add isInteger: false story
 
   return <NumberField
     label="Enter the number of things"
