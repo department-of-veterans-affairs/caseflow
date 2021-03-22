7ab093f034d5f9fd716838cf040703681689c1f4
charleyf <charleyferguson@navapbc.com>
2021-03-22 14:15:33 -0400

    Add Indices to "request_issues" (#16030)
    Attempt to address a recurring DataDog alert: Caseflow DB CPU Load is High
    
    ### Description
    The last two Mondays (03/15 and 03/22) there have been PagerDuty alerts from the [Caseflow DB CPU Load is High](https://app.datadoghq.com/monitors/17226190) DataDog monitor. The (extremely helpful) instructions in that alert suggest looking at the [slow queries in NewRelic](https://one.newrelic.com/launcher/nr1-core.explorer?pane=eyJuZXJkbGV0SWQiOiJhcG0tbmVyZGxldHMuZGF0YWJhc2VzIiwiZW50aXR5SWQiOiJNVGM0T0RRMU9IeEJVRTE4UVZCUVRFbERRVlJKVDA1OE9EQTJNRGcxTkRRIiwibGVnYWN5RGF0YWJhc2VzIjpmYWxzZSwic3BhUGF0aCI6bnVsbCwic2VsZWN0ZWREYXRhc3RvcmVUeXBlIjoiUG9zdGdyZXMiLCJzZWxlY3RlZFNlcmllcyI6ImVkYzVmNWVkMWZkMzViMWZlODEwNjk0MjhhNjYxNzdhM2M3ODk3MWMiLCJkcmlsbGRvd24iOnsibWV0cmljVGltZXNsaWNlTmFtZSI6IkRhdGFzdG9yZS9zdGF0ZW1lbnQvUG9zdGdyZXMvUmVxdWVzdElzc3VlL2ZpbmQifX0=&sidebars[0]=eyJuZXJkbGV0SWQiOiJucjEtY29yZS5hY3Rpb25zIiwiZW50aXR5SWQiOiJNVGM0T0RRMU9IeEJVRTE4UVZCUVRFbERRVlJKVDA1OE9EQTJNRGcxTkRRIiwic2VsZWN0ZWROZXJkbGV0Ijp7Im5lcmRsZXRJZCI6ImFwbS1uZXJkbGV0cy5kYXRhYmFzZXMifX0=&platform[accountId]=1788458&platform[timeRange][duration]=604800000&platform[$isFallbackTimeRange]=false) and adding indexes on any unindexed columns referenced.
    
    This PR adds indexes to the two unindexed string columns that query uses. [Edit, only one, see the below comment]
    
    As per Yang's comment (in the issue below), an index on nonrating_issue_category doesn't make sense.
    
    > After looking at all of the slow RequestIssue queries in NewRelic, I think we can go with just the index on veteran_participant_id and not need the one on nonrating_issue_category. And thinking about the content of these fields, that makes sense -- participant ID is specific enough to narrow the result set down to tens of rows, whereas NR issue category is essentially an enum, with a huge number of null rows.
    
    ### Additional Checks
    * [x] Local rollback removes indices
    
    ### Database Changes
    *Only for Schema Changes*
    
    * [ ] Timestamps (created_at, updated_at) for new tables
    * [ ] Column comments updated
    * [ ] Query profiling performed (eyeball Rails log, check bullet and fasterer output)
    * [x] Appropriate indexes added (especially for foreign keys, polymorphic columns, and unique constraints)
    * [x] DB schema docs updated with `make docs`
    * [x] #appeals-schema notified with summary and link to this PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_154002) do
+ActiveRecord::Schema.define(version: 2021_03_22_135622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1230,6 +1230,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_154002) do
     t.index ["ineligible_due_to_id"], name: "index_request_issues_on_ineligible_due_to_id"
     t.index ["ineligible_reason"], name: "index_request_issues_on_ineligible_reason"
     t.index ["updated_at"], name: "index_request_issues_on_updated_at"
+    t.index ["veteran_participant_id"], name: "index_veteran_participant_id"
   end
 
   create_table "request_issues_updates", comment: "Keeps track of edits to request issues on a decision review that happen after the initial intake, such as removing and adding issues.  When the decision review is processed in VBMS, this also tracks whether adding or removing contentions in VBMS for the update has succeeded.", force: :cascade do |t|
