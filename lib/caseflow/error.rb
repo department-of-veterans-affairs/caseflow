8f7a43212cb3028f00d087107220f2170d89a831
Will Drougas <wdrougas@tistatech.com>
2021-03-26 16:20:42 -0400

    added docket switch duplicate error

--- lib/caseflow/error.rb
+++ lib/caseflow/error.rb
@@ -376,4 +376,10 @@ module Caseflow::Error
       @message = args[:message]
     end
   end
+
+  class DuplicateDocketSwitchError < StandardError; 
+    def initialize
+      @message = "Docket Switch Already Submitted"
+    end
+  end
 end
