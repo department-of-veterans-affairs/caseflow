6dbf5bdcb1bc6643e4ae262b45f538f9bed90691
charleyf <Charles.Ferguson@va.gov>
2021-03-29 16:49:02 -0400

    Rough out the structure of a new button

--- client/app/hearings/components/assignHearings/AssignHearings.jsx
+++ client/app/hearings/components/assignHearings/AssignHearings.jsx
@@ -9,15 +9,24 @@ import { NoUpcomingHearingDayMessage } from './Messages';
 import AssignHearingsTabs from './AssignHearingsTabs';
 import Button from '../../../components/Button';
 
+const horizontalRuleStyling = css({
+  border: 0,
+  width: '90%',
+  borderTop: `1px solid ${COLORS.GREY_LIGHT}`,
+  margin: 'auto',
+});
+
 const sectionNavigationListStyling = css({
   '& > li': {
-    backgroundColor: COLORS.GREY_BACKGROUND,
     color: COLORS.PRIMARY,
     borderWidth: 0
   }
 });
 
 const buttonColorSelected = css({
+  width: '90%',
+  paddingTop: '1em',
+  paddingBottom: '1em',
   backgroundColor: COLORS.GREY_DARK,
   color: COLORS.WHITE,
   borderRadius: '0.1rem 0.1rem 0 0',
@@ -27,7 +36,7 @@ const buttonColorSelected = css({
   }
 });
 
-const roSelectionStyling = css({ marginTop: '10px' });
+const roSelectionStyling = css({ marginTop: '10px', textAlign: 'center' });
 
 const UpcomingHearingDaysNav = ({
   upcomingHearingDays, selectedHearingDay,
@@ -37,6 +46,11 @@ const UpcomingHearingDaysNav = ({
     Object.values(upcomingHearingDays),
     (hearingDay) => hearingDay.scheduledFor, 'asc'
   );
+
+  /*
+                      {`${moment(hearingDay.scheduledFor).format('ddd M/DD/YYYY')}
+                      ${hearingDay.room ?? ''}`
+  */
 
   return (
     <div className="usa-width-one-fourth" {...roSelectionStyling}>
@@ -51,12 +65,29 @@ const UpcomingHearingDaysNav = ({
               return (
                 <li key={hearingDay.id} >
                   <Button
-                    styling={dateSelected ? buttonColorSelected : {}}
+                    styling={dateSelected ? buttonColorSelected : css({ width: '90%', paddingTop: '1em', paddingBottom: '1em' })}
                     onClick={() => onSelectedHearingDayChange(hearingDay)}
                     linkStyling>
-                    {`${moment(hearingDay.scheduledFor).format('ddd M/DD/YYYY')}  
-                      ${hearingDay.room ?? ''}`}
+                    <div>
+                      <div {...css({ width: '67%', display: 'inline-block', textAlign: 'left' })} >
+                        <div>
+                        Thu Apr 1
+                        </div>
+                        <div {...css({ textOverflow: 'ellipsis', overflowX: 'hidden', overflowY: 'hidden', whiteSpace: 'nowrap' })}>
+                          Virtual * VLJVigsittaboorn
+                        </div>
+                      </div>
+                      <div {...css({ width: '33%', display: 'inline-block', textAlign: 'right', overflowX: 'hidden', overflowY: 'hidden' })} >
+                        <div>
+                        2 of 4
+                        </div>
+                        <div>
+                        scheduled
+                        </div>
+                      </div>
+                    </div>
                   </Button>
+                  <hr {...horizontalRuleStyling} />
                 </li>
               );
             }
