e57dca7fa70590441d54c0bf9a4074d13935d594
Will Drougas <wdrougas@tistatech.com>
2021-04-01 17:54:43 -0400

    updated attorney_user method in task_handler

--- app/workflows/docket_switch/task_handler.rb
+++ app/workflows/docket_switch/task_handler.rb
@@ -74,8 +74,8 @@ class DocketSwitch::TaskHandler
   end
 
   def attorney_user
-    granted_task = old_docket_stream.tasks.find { |task| task.is_a?(DocketSwitchGrantedTask) }
-
+    granted_task = old_docket_stream.tasks.completed.find_by(type: "DocketSwitchGrantedTask", assigned_to_type: "User")
+    binding.pry
     granted_task&.assigned_to
   end
 end
