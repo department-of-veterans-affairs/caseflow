ccb9e5e906f94506265228f43b52dd8d7f7fdb38
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-04-07 16:01:21 -0400

    additional work re handling of granted substitution basics form

--- client/app/queue/grantedSubstitution/GrantedSubstitutionBasicsView.jsx
+++ client/app/queue/grantedSubstitution/GrantedSubstitutionBasicsView.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { useHistory, useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { format } from 'date-fns';
+
+import { appealWithDetailSelector } from 'app/queue/selectors';
 import { GrantedSubstitutionForm } from './GrantedSubstitutionBasicsForm';
 
+import { updateData, stepForward } from './grantedSubstitution.slice';
+
 export const GrantedSubstitutionBasicsView = () => {
-  return <GrantedSubstitutionForm />;
+  const { appealId } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const appeal = useSelector((state) =>
+    appealWithDetailSelector(state, { appealId })
+  );
+
+  const { formData: prevValues } = useSelector((state) => state.grantedSubstitution);
+
+  const handleCancel = () => history.push(`/queue/appeals/${appealId}`);
+  const handleSubmit = async (formData) => {
+    // Here we'll dispatch action to update Redux store with our form data
+    // Initially, we may also dispatch async thunk action to submit the basics to the backend because of how stories are sliced
+
+    dispatch(updateData({
+      formData: {
+        ...formData,
+        substitutionDate: format(formData.substitutionDate, 'yyyy-MM-dd'),
+      },
+    }));
+  };
+
+  return (
+    <GrantedSubstitutionForm onCancel={handleCancel} onSubmit={handleSubmit} />
+  );
 };
---
+++ client/app/queue/grantedSubstitution/grantedSubstitution.slice.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import ApiUtil from '../../util/ApiUtil';
+import { onReceiveAmaTasks } from '../QueueActions';
+
+const initialState = {
+  step: 0,
+
+  /**
+   * This will hold substitution date, participantId, etc
+   */
+  formData: {
+    substitutionDate: null,
+    participantId: null,
+  },
+};
+
+const grantedSubstitutionSlice = createSlice({
+  name: 'docketSwitch',
+  initialState,
+  reducers: {
+    cancel: () => ({ ...initialState }),
+    stepForward: (state) => ({ ...state, step: state.step + 1 }),
+    stepBack: (state) => ({ ...state, step: state.step ? state.step - 1 : 0 }),
+    updateData: (state, action) => {
+      const { formData: updates } = action.payload;
+
+      state.formData = {
+        ...state.formData,
+        ...updates,
+        newTasks: updates?.newTasks,
+      };
+    },
+  },
+});
+
+// We can likely use a variant of this to submit to the backend
+// (need more info on what we are saving and what is being returned)
+export const completeDocketSwitchGranted = createAsyncThunk(
+  'grantedSubstitution/submit',
+  async (data) => {
+    try {
+      const res = await ApiUtil.post('/appellant_substitutions', { data });
+      const attrs = res.body?.data?.attributes;
+
+      return {
+        oldAppealId: attrs?.old_appeal_uuid,
+        newAppealId: attrs?.new_appeal_uuid,
+      };
+    } catch (error) {
+      console.error('Error granting docket switch', error);
+      throw error;
+    }
+  }
+);
+
+export const {
+  cancel,
+  stepForward,
+  stepBack,
+  updateData,
+} = grantedSubstitutionSlice.actions;
+
+export default grantedSubstitutionSlice.reducer;
