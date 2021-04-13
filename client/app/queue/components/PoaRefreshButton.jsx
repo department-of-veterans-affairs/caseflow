0db588c3c60fad102abcc7c0e764a8e55689460e
Jeremy Curcio <jeremy1026@gmail.com>
2021-04-13 08:30:11 -0400

    Change to patch and use ApiUtils

--- client/app/queue/components/PoaRefreshButton.jsx
+++ client/app/queue/components/PoaRefreshButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { editAppeal, poaSyncDateUpdates } from '../QueueActions';
 // import { useDispatch, useSelector } from 'react-redux';
-// import ApiUtil from '../../util/ApiUtil';
+import ApiUtil from '../../util/ApiUtil';
 import Button from '../../components/Button';
 import Alert from '../../components/Alert';
 import { css } from 'glamor';
@@ -10,16 +10,35 @@ import { css } from 'glamor';
 export const PoaRefreshButton = ({ appealId }) => {
 	console.log(appealId)
   const updatePOA = () => {
-    fetch(`/appeals/${appealId}/update_power_of_attorney`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          alert = {
-            type: result.status,
-            message: result.message
-          }
-        }
-      )
+    // fetch(`/appeals/${appealId}/update_power_of_attorney`)
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       alert = {
+    //         type: result.status,
+    //         message: result.message
+    //       }
+    //     }
+    //   )
+
+    ApiUtil.patch(`/appeals/${appealId}/update_power_of_attorney`).then((data) => {
+      console.log(data.body);
+    //   dispatch(editAppeal(appealId, {
+    //     nodDate: data.body.nodDate,
+    //     docketNumber: data.body.docketNumber,
+    //     reason: data.body.changeReason
+    //   }));
+
+    //   if (data.body.affectedIssues) {
+    //     setIssues({ affectedIssues: data.body.affectedIssues, unaffectedIssues: data.body.unaffectedIssues });
+    //     setTimelinessError(true);
+    //   } else {
+    //     dispatch(editNodDateUpdates(appealId, data.body.nodDateUpdate));
+    //     dispatch(showSuccessMessage(successMessage));
+    //     onSubmit?.();
+    //     window.scrollTo(0, 0);
+    //   }
+    });
   };
 
   return (
