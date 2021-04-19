81588f25f058c4a5f74c346e3c216a26acaed5f4
Jeremy Curcio <jeremy1026@gmail.com>
2021-04-19 11:46:30 -0400

    Show loading button

--- client/app/queue/components/PoaRefreshButton.jsx
+++ client/app/queue/components/PoaRefreshButton.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import ApiUtil from '../../util/ApiUtil';
 import Button from '../../components/Button';
+import SmallLoader from '../../components/SmallLoader';
 import { setPoaRefreshAlert } from '../uiReducer/uiActions';
 
 export const PoaRefreshButton = ({ appealId, poaId }) => {
   const dispatch = useDispatch();
+  const [buttonText, setButtonText] = useState("Refresh POA");  
+
   const updatePOA = () => {
+    setButtonText(<SmallLoader message="Refresh POA" spinnerColor="#417505" />)
     ApiUtil.patch(`/appeals/${appealId}/update_power_of_attorney/${poaId}`).then((data) => {
       dispatch(setPoaRefreshAlert(data.body.status, data.body.message));
-    });
+      setButtonText("Refresh POA")
+   });
   };
 
   return (
@@ -21,7 +26,7 @@ export const PoaRefreshButton = ({ appealId, poaId }) => {
             classNames={['usa-button-secondary', 'cf-push-right']}
             onClick={() => updatePOA()}
           >
-            Refresh POA
+            {buttonText}
         </Button>
     </div>
   );
