911b1e520a07e93978701e359cb49ecb85eecd21
charleyf <Charles.Ferguson@va.gov>
2021-04-20 16:45:37 -0400

    Finalize TimePicker draft

--- client/app/hearings/components/TimePicker.jsx
+++ client/app/hearings/components/TimePicker.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import moment from 'moment-timezone';
 
 import HEARING_TIME_OPTIONS from '../../../constants/HEARING_TIME_OPTIONS';
 import SearchableDropdown from '../../components/SearchableDropdown';
 import { formatTimeSlotLabel } from '../utils';
 
+// A searchable dropdown with options like:
+// 8:30 AM Pacific Time (US & Canada) / 11:30 AM Eastern Time (US & Canada)
 export const TimePicker = ({
   componentIndex,
   onChange,
@@ -13,15 +14,11 @@ export const TimePicker = ({
   value,
 }) => {
 
-  // Okay, so a searchable dropdown with options like:
-  // 8:30 AM Pacific Time (US & Canada) / 11:30 AM Eastern Time (US & Canada)
-  // Values should have UTC offsets like this:
-  // value: "08:30:00-07:00"
   const formatOptions = (zone, options) => {
     const formattedOptions = options.map((option) => {
       return {
-        // We interpret the 'value' as being in roTimezone
-        value: moment.tz(option.value, 'HH:mm', zone),
+        // We interpret the 'value' as being in roTimezone by not changing it
+        value: option.value,
         // The label is the same as the label on the timeslots
         label: formatTimeSlotLabel(option.value, zone)
       };
@@ -38,6 +35,8 @@ export const TimePicker = ({
       placeholder="Select a time"
       options={formatOptions(roTimezone, HEARING_TIME_OPTIONS)}
       value={value}
+      // This is responsible for updating the 'scheduledTimeString' which is in eastern
+      // it is not responsible for updating what's selected in the dropdown
       onChange={(option) => onChange(option ? option.value : null)}
     />
   );
