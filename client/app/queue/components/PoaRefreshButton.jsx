70f049ebab250cced9d263ee434e1d3ad991ac0d
Jeremy Curcio <jeremy1026@gmail.com>
2021-04-20 09:57:14 -0400

    Add featrue toggle for refresh button

--- client/app/queue/components/PoaRefreshButton.jsx
+++ client/app/queue/components/PoaRefreshButton.jsx
@@ -5,10 +5,13 @@ import ApiUtil from '../../util/ApiUtil';
 import Button from '../../components/Button';
 import SmallLoader from '../../components/SmallLoader';
 import { setPoaRefreshAlert } from '../uiReducer/uiActions';
+import { useSelector } from 'react-redux';
 
 export const PoaRefreshButton = ({ appealId, poaId }) => {
   const dispatch = useDispatch();
   const [buttonText, setButtonText] = useState("Refresh POA");  
+  const viewPoaRefreshButton = useSelector((state) => state.ui.featureToggles.view_poa_refresh_button);
+
 
   const updatePOA = () => {
     setButtonText(<SmallLoader message="Refresh POA" spinnerColor="#417505" />)
@@ -18,8 +21,8 @@ export const PoaRefreshButton = ({ appealId, poaId }) => {
    });
   };
 
-  return (
-    <div>
+  return <React.Fragment>
+    {viewPoaRefreshButton && ( <div>
         <Button
             type="button"
             name="Refresh Poa"
@@ -29,7 +32,8 @@ export const PoaRefreshButton = ({ appealId, poaId }) => {
             {buttonText}
         </Button>
     </div>
-  );
+    )}
+  </React.Fragment>
 };
 
 PoaRefreshButton.propTypes = {
