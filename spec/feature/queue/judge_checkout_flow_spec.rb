63d9f30c9596dd3d222af7e533b07c41e7b86b8c
yoomlam <Dung.Lam1@va.gov>
2021-04-21 19:33:59 -0500

    handle auto-scrolling

--- spec/feature/queue/judge_checkout_flow_spec.rb
+++ spec/feature/queue/judge_checkout_flow_spec.rb
@@ -72,17 +72,12 @@ RSpec.feature "Judge checkout flow", :all_dbs do
       click_on "Continue"
 
       expect(page).to have_content("Issue 2 of 2")
-
-      # all("label", text: "No notice sent")[1].click
+      expect(page).to have_content("Continue") # helps prevent flake due to page auto-scrolling
       containing_div = find("h2", text: "Issue 2 of 2").find(:xpath, "..")
       checkbox = containing_div.find("label", text: "No notice sent")
       checkbox.click
-
-      # expect(page).to have_content("Pre AOJ", count: 2, wait: 5)
-      # all("label", text: "Pre AOJ")[1].click
       containing_fieldset = checkbox.find(:xpath, "ancestor::fieldset")
-      radiobutton = containing_fieldset.find("label", text: "Pre AOJ")
-      radiobutton.click
+      containing_fieldset.find("label", text: "Pre AOJ").click
       click_on "Continue"
 
       # Evaluate Decision page
