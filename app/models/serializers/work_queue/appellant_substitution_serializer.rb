e9f0641d8b746cb6d7a95bf04541aaea4c038664
yoomlam <Dung.Lam1@va.gov>
2021-04-23 10:35:07 -0500

    reduce query costs until needed

--- app/models/serializers/work_queue/appellant_substitution_serializer.rb
+++ app/models/serializers/work_queue/appellant_substitution_serializer.rb
@@ -9,18 +9,20 @@ class WorkQueue::AppellantSubstitutionSerializer
   attribute :poa_participant_id
   attribute :target_appeal_id
 
-  attribute :source_appeal_uuid do |object|
-    object.source_appeal&.uuid
-  end
-  attribute :target_appeal_uuid do |object|
-    object.target_appeal&.uuid
-  end
+  # Uncomment the following as needed and write corresponding tests
 
-  attribute :source_decision_issues do |object|
-    object.source_appeal&.decision_issues
-  end
+  # attribute :source_appeal_uuid do |object|
+  #   object.source_appeal&.uuid
+  # end
+  # attribute :target_appeal_uuid do |object|
+  #   object.target_appeal&.uuid
+  # end
 
-  attribute :created_by do |object|
-    object.created_by&.full_name
-  end
+  # attribute :source_decision_issues do |object|
+  #   object.source_appeal&.decision_issues
+  # end
+
+  # attribute :created_by do |object|
+  #   object.created_by&.full_name
+  # end
 end
