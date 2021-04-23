9986f3f1ecc13c69dd60d9483c087113d0257235
Tomas Apodaca <Thomas.Apodaca@va.gov>
2021-04-23 15:46:47 -0700

    add a test for rolled-back issues

--- spec/models/legacy_issue_optin_spec.rb
+++ spec/models/legacy_issue_optin_spec.rb
@@ -5,13 +5,30 @@ describe LegacyIssueOptin, :all_dbs do
   let(:remand_case) do
     create(:case, :status_remand, bfkey: "remand", case_issues: [remand_issue])
   end
-  let(:lio) { create(:legacy_issue_optin, request_issue: request_issue, optin_processed_at: Time.zone.now - 5.hours) }
+  let(:optin_date) { Time.zone.now - 5.hours }
+  let(:rollback_date) { nil }
+  let(:lio) do
+    create(
+      :legacy_issue_optin,
+      request_issue: request_issue,
+      optin_processed_at: optin_date,
+      rollback_processed_at: rollback_date
+    )
+  end
   let(:request_issue) { create(:request_issue, vacols_id: remand_case.bfkey, vacols_sequence_id: remand_issue.issseq) }
 
   describe ".related_remand_issues" do
     it "joins with request_issues" do
       expect(described_class.related_remand_issues(lio.vacols_id).count).to eq(1)
       expect(described_class.related_remand_issues(lio.vacols_id).first).to eq(lio)
+    end
+
+    context "the issue has been rolled back" do
+      let(:rollback_date) { Time.zone.now - 1.day }
+
+      it "doesn't return rolled-back issues" do
+        expect(described_class.related_remand_issues(lio.vacols_id).count).to eq(0)
+      end
     end
   end
 end
