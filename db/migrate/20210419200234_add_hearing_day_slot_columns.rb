6f1ca82698b9245ae9efa0d31ea3a11b2f99a2f9
charleyf <Charles.Ferguson@va.gov>
2021-04-23 10:45:42 -0400

    Indicate begins_at_time_string is in eastern timezone

--- db/migrate/20210419200234_add_hearing_day_slot_columns.rb
+++ db/migrate/20210419200234_add_hearing_day_slot_columns.rb
@@ -4,6 +4,6 @@ class AddHearingDaySlotColumns < Caseflow::Migration
   def change
     add_column :hearing_days, :number_of_slots, :integer, comment: "The number of slots possible for this day"
     add_column :hearing_days, :slot_length_minutes, :integer, comment: "How long each timeslot is for this day"
-    add_column :hearing_days, :begins_at_time_string, :string, comment: "When the hearing day begins, slots will not appear before this time"
+    add_column :hearing_days, :begins_at_time_string, :string, comment: "in eastern timezone, slots will not appear before this time"
   end
 end
