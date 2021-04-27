b8a6a2880a474f6b3f10abf2a9005d68542da058
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-04-27 11:08:54 -0400

    updated jest tests/snapshots for SubstituteAppellantTasksForm & KeyDetails

--- client/test/app/queue/substituteAppellant/KeyDetails.test.js
+++ client/test/app/queue/substituteAppellant/KeyDetails.test.js
@@ -5,15 +5,22 @@ import { axe } from 'jest-axe';
 import { sub } from 'date-fns';
 
 import { KeyDetails } from 'app/queue/substituteAppellant/tasks/KeyDetails';
+import { MemoryRouter } from 'react-router';
 
 describe('KeyDetails', () => {
   const defaults = {
+    appealId: 'abc123',
     nodDate: sub(new Date(), { days: 30 }),
     dateOfDeath: sub(new Date(), { days: 15 }),
     substitutionDate: sub(new Date(), { days: 10 }),
   };
 
-  const setup = (props) => render(<KeyDetails {...defaults} {...props} />);
+  const setup = (props) =>
+    render(
+      <MemoryRouter>
+        <KeyDetails {...defaults} {...props} />
+      </MemoryRouter>
+    );
 
   it('renders default state correctly', () => {
     const { container } = setup();
