263208d4f2bbfaa2de01fc553338c992089cc24c
yoomlam <Dung.Lam1@va.gov>
2021-04-28 12:03:53 -0500

    clean up timeline_concern

--- app/controllers/concerns/export_timeline_concern.rb
+++ app/controllers/concerns/export_timeline_concern.rb
@@ -9,33 +9,36 @@ module ExportTimelineConcern
   # :reek:FeatureEnvy
   def tasks_as_timeline_data
     sje.records_hash[Task.table_name].map do |record|
-      record = record.clone
       significant_duration = record["closed_at"] - record["created_at"] > 120 if record["closed_at"]
-      end_time = significant_duration ? record["closed_at"] : nil
-      {
-        id: "#{Task.name}#{record['id']}",
-        tableName: Task.table_name,
-        status: record["status"],
-        content: "#{record['type']}_#{record['id']}",
-        start: record["created_at"],
-        end: end_time
-      }
+      record.clone.tap do |clone_record|
+        clone_record["id"] = "#{Task.name}#{record['id']}"
+        clone_record["tableName"] = Task.table_name
+        clone_record["content"] = "#{record['type']}_#{record['id']}"
+        clone_record["start"] = record["created_at"]
+        clone_record["end"] = significant_duration ? record["closed_at"] : nil
+        clone_record["taskType"] = record["type"]
+
+        # clear these values to prevent conflict the Timeline Item's fields
+        clone_record["type"] = nil
+      end
     end
   end
 
   # :reek:FeatureEnvy
   def intakes_as_timeline_data
     sje.records_hash[Intake.table_name].map do |record|
-      record = record.clone
       significant_duration = record["completed_at"] - record["created_at"] > 120 if record["completed_at"]
-      end_time = significant_duration ? record["completed_at"] : nil
-      {
-        id: "#{record['type']}#{record['id']}",
-        tableName: Intake.table_name,
-        content: "#{record['type']}_#{record['id']}",
-        start: record["created_at"],
-        end: end_time
-      }
+      record.clone.tap do |clone_record|
+        clone_record["id"] = "#{record['type']}#{record['id']}"
+        clone_record["tableName"] = Intake.table_name
+        clone_record["content"] = "#{record['type']}_#{record['id']}"
+        clone_record["start"] = record["created_at"]
+        clone_record["end"] = significant_duration ? record["completed_at"] : nil
+        clone_record["intakeType"] = record["type"]
+
+        # clear these values to prevent conflict the Timeline Item's fields
+        clone_record["type"] = nil
+      end
     end
   end
 end
