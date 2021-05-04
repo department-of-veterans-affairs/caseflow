1c986967d9f85e246ff4b10dc643e77309ecddb3
charleyf <Charles.Ferguson@va.gov>
2021-05-04 10:07:53 -0400

    Add correctly ordered list of time options

--- client/app/hearings/components/modalForms/CustomTimeModal.jsx
+++ client/app/hearings/components/modalForms/CustomTimeModal.jsx
@@ -1,17 +1,63 @@
+// React
 import React, { useState } from 'react';
+// Libraries
 import PropTypes from 'prop-types';
-import Modal from '../../../components/Modal';
 import Select from 'react-select';
+import moment from 'moment-timezone';
+// Components
+import Modal from '../../../components/Modal';
+
+const generateTimes = (roTimezone, intervalMinutes = 15) => {
+  // Start at midnight '00:00' is the first minute of a day
+  const currentTime = moment.tz('00:00', 'HH:mm', roTimezone);
+  // End at 23:59, the last minute of a day
+  const elevenFiftyNine = moment.tz('23:59', 'HH:mm', roTimezone);
+
+  const times = [];
+
+  // Go through the day in fifteen minute increments and store each increment
+  while (currentTime.isBefore(elevenFiftyNine)) {
+    // Moment has mutable objects so clone() is necessary
+    times.push(currentTime.clone());
+    currentTime.add(intervalMinutes, 'minute');
+  }
+
+  return times;
+};
+
+const moveTimesToEndOfArray = (newFirstValue, times) => {
+  // Find the index of newFirstValue
+  const firstValueIndex = times.findIndex((time) => time.isSame(newFirstValue));
+
+  // Remove all values before newFirstValue from the front of the array
+  const beforeFirstValue = times.slice(0, firstValueIndex);
+  const afterFirstValue = times.slice(firstValueIndex);
+
+  // Add the values back onto the end of the array
+  return afterFirstValue.concat(beforeFirstValue);
+};
+
+const formatTimesToOptionObjects = (times) => {
+  return times.map((time) => {
+    return {
+      value: time.format('HH:mm'),
+      label: time.format('h:mm A')
+    };
+  });
+};
 
 const generateOrderedTimeOptions = (roTimezone) => {
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ];
+
+  // Get an array with every fifteen minute increment in a day
+  // [00:00, 00:15, ... , 23:45]
+  const times = generateTimes(roTimezone);
+  // Move everything after beginsAt to the end of the array
+  const beginsAt = moment.tz('08:30', 'HH:mm', roTimezone);
+  const reorderedTimes = moveTimesToEndOfArray(beginsAt, times);
+  // Put the times into the format expected by the select
+  const options = formatTimesToOptionObjects(reorderedTimes);
 
   return options;
-
 };
 
 export const CustomTimeModal = ({ onConfirm, onCancel, roTimezone }) => {
