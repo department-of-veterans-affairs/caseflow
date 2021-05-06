07c1181d2d1e0c1a0f168831535738f233bcf989
yoomlam <Dung.Lam1@va.gov>
2021-05-06 11:27:12 -0500

    convert test to feature test

--- spec/fixes/investigate_scm_cant_reassign_spec.rb
+++ spec/fixes/investigate_scm_cant_reassign_spec.rb
@@ -3,7 +3,14 @@
 require "helpers/sanitized_json_configuration.rb"
 require "helpers/sanitized_json_importer.rb"
 
-describe "CaseMovementTeam task actions" do
+feature "CaseMovementTeam task actions" do
+  before do
+    User.authenticate!(css_id: "SANFORDBVAM")
+  end
+
+  let(:attorney_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Talam") }
+  let!(:attorney_staff) { create(:staff, :attorney_role, user: attorney_user) }
+
   # Ticket https://github.com/department-of-veterans-affairs/caseflow/issues/16205#
   # https://github.com/department-of-veterans-affairs/dsva-vacols/issues/187
   # Target state: TBD -- see tcket
@@ -14,14 +21,24 @@ describe "CaseMovementTeam task actions" do
       sji.imported_records[Appeal.table_name].first
     end
 
-    it "produces error and user can't reassign to attorney" do
-      # To replicate error:
-      # binding.pry # Uncomment this line then do the following manually
-      # Clicking on "Assign to attorney" shows the "Assign task" modal. 
-      # Clicking on "Select a user" shows "Other". 
-      # Clicking on "Other" and starting to type "TALAM" shows the attorney. 
-      # Clicking Submit button shows an "Error assigning tasks" error banner in the modal 
+    scenario "produces error and user can't reassign to attorney" do
+      visit "/queue/appeals/#{appeal.uuid}"
+
+      # Clicking on "Assign to attorney" shows the "Assign task" modal.
+      click_dropdown(prompt: "Select an action", text: "Assign to attorney")
+
+      # Clicking on "Select a user" shows "Other".
+      click_dropdown(prompt: "Select a user", text: "Other")
+
+      # Clicking on "Other" and starting to type "TALAM" shows the attorney.
+      click_dropdown(prompt: "Select a user", text: attorney_user.full_name)
+      fill_in(COPY::ADD_COLOCATED_TASK_INSTRUCTIONS_LABEL, with: "\nSCM user reassigning to different attorney")
+
+      # Clicking Submit button shows an "Error assigning tasks" error banner in the modal
       # (and an error message in the DevTools console).
+      click_on "Submit"
+      expect(page).to have_content("Error assigning tasks")
+      # binding.pry # Uncomment this line to see error message
     end
   end
 end
