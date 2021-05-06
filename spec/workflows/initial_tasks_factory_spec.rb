bcfe7ede858497521d9df4d3302a5444648712ee
karinamzalez <karinamzalez@gmail.com>
2021-05-06 13:03:37 -0500

    fix new test indentation

--- spec/workflows/initial_tasks_factory_spec.rb
+++ spec/workflows/initial_tasks_factory_spec.rb
@@ -68,9 +68,10 @@ describe InitialTasksFactory, :postgres do
         context "on veteran date of death present" do
           let(:appeal) do
             create(:appeal,
-              docket_type: Constants.AMA_DOCKETS.direct_review, claimants: [ create(:claimant, participant_id: participant_id_with_no_vso) ],
-              veteran: create(:veteran, date_of_death: 30.days.ago.to_date)
-            )
+                   docket_type: Constants.AMA_DOCKETS.direct_review,
+                   claimants: [ create(:claimant, participant_id: participant_id_with_no_vso) ],
+                   veteran: create(:veteran, date_of_death: 30.days.ago.to_date)
+                  )
           end
 
           it "is ready for distribution" do
