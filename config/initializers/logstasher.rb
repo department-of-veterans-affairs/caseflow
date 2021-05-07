c15c5dc18b06cf907af858af265b2f750fd7af76
Anya Roltsch <aroltsch@tistatech.com>
2021-05-07 10:54:25 -0400

    Test smth

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,5 +1,5 @@
 if LogStasher.enabled?
-  LogStasher.add_custom_fields_to_request_context do |fields|
+  LogStasher.add_custom_fields do |fields|
     # This block is run in application_controller context,
     # so you have access to all controller methods
     # You can log custom request fields using this block
