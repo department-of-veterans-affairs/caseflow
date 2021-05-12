baee837181901f4071b6b333c2b9e7dd18b82107
Stephen Halliburton <stephen.halliburton@va.gov>
2021-05-12 13:50:40 -0700

    Updated variable name

--- app/models/ro_schedule_period.rb
+++ app/models/ro_schedule_period.rb
@@ -78,13 +78,13 @@ class RoSchedulePeriod < SchedulePeriod
     generate_hearings_days.allocate_hearing_days_to_ros
 
     # Distribute the requested days without the room constraint per RO
-    hearing_days_without_room = format_ro_hearing_data(generate_hearings_days.allocate_no_room_hearing_days_to_ros)
+    non_co_hearing_days = format_ro_hearing_data(generate_hearings_days.allocate_no_room_hearing_days_to_ros)
 
     # Distribute the available Central Office hearing days
     co_hearing_days = generate_hearings_days.generate_co_hearing_days_schedule
 
     # Combine the available hearing days
-    hearing_days =  co_hearing_days + hearing_days_without_room
+    hearing_days =  co_hearing_days + non_co_hearing_days
     hearing_days.sort_by { |day| day[:scheduled_for] }
   end
 end
