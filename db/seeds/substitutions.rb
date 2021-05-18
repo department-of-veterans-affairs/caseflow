9d30922a1659d3323518e0892217c21d1a325b4a
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-05-18 15:04:09 -0400

    updated substitutions seeds to create full task tree prior to date_of_death being set

--- db/seeds/substitutions.rb
+++ db/seeds/substitutions.rb
@@ -14,25 +14,37 @@ module Seeds
       attorney = User.find_by_css_id("BVASCASPER1")
       judge = User.find_by_css_id("BVAAABSHIRE")
 
-      # Deceased veteran
-      veteran = Veteran.find_by_file_number(45_454_545)
+      # Create soon-to-be deceased veteran
+      # date_of_death needs to be nil to create full task tree
+      veteran = FactoryBot.create(
+        :veteran,
+        file_number: 54_545_454,
+        first_name: "Jane",
+        last_name: "Deceased",
+      )
+
+      # Need to set date_of_death after creating appeal or various tasks won't get created
+      date_of_death = 30.days.ago
 
       notes = "Pain disorder with 100\% evaluation per examination"
       notes += ". Created with the dispatched factory trait"
 
-      appeal = create(
+      appeal = FactoryBot.create(
         :appeal,
         :dispatched_with_decision_issue,
         disposition: "dismissed_death",
         number_of_claimants: 1,
         veteran: veteran,
         docket_type: docket_type,
-        receipt_date: veteran.date_of_death + 5.days,
+        receipt_date: date_of_death + 5.days,
         closest_regional_office: "RO17",
         associated_judge: judge,
-        associated_attorney: attorney,
+        associated_attorney: attorney
       )
-      end
+
+      # Reset date of death
+      veteran.update!(date_of_death: date_of_death)
+    end
 
     def setup_substitution_seeds
       ["direct_review", "evidence_submission", "hearings"].each do |docket_type|
