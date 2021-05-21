c6d5a85e7021aea2e7c66fa29d0a47883a420eff
Nik Weinmeister <nweinmeister@tistatech.com>
2021-05-21 15:06:20 -0400

    Fix test

--- client/test/app/intake/pages/review.test.js
+++ client/test/app/intake/pages/review.test.js
@@ -1,18 +1,19 @@
 import {TestableSchema} from 'app/intake/pages/review';
+import { REVIEW_OPTIONS } from 'app/intake/constants';
 
-const assertValidSchema = async (schema, testSchema, isValid) => {
+const assertValidSchema = async (schema, testSchema, selectedForm, isValid) => {
   await schema
-    .isValid(testSchema)
+    .isValid(testSchema, { context: {selectedForm: selectedForm}})
     .then((valid) => expect(valid).toBe(isValid))
 }
 
 describe('schema', () => {
   it('is valid after feb 19, 2019', async () => {
     const validSchema = { receiptDate: '01/01/2020' }
-    await assertValidSchema(TestableSchema, validSchema, true)
+    await assertValidSchema(TestableSchema, validSchema, REVIEW_OPTIONS.APPEAL.key, true)
   })
   it('is invalid before feb 19, 2019', async () => {
     const invalidSchema = { receiptDate: '02/18/2019' }
-    await assertValidSchema(TestableSchema, invalidSchema, false)
+    await assertValidSchema(TestableSchema, invalidSchema, REVIEW_OPTIONS.APPEAL.key, false)
   })
 });
