baf09aadd712550e1474411cb3619c8353051def
Yoom Lam <yoom@navapbc.com>
2021-05-21 12:20:40 -0500

    Revert "Add foreign keys for hearings tables (Phase 6) (#16270)" (#16279)
    Reverts department-of-veterans-affairs/caseflow#16270

--- db/migrate/20210519184436_validate_foreign_keys_for_appeals.rb
+++
@@ -1,7 +0,0 @@
-class ValidateForeignKeysForAppeals < Caseflow::Migration
-  def change
-    validate_foreign_key "dispatch_tasks", column: "appeal_id"
-    validate_foreign_key "legacy_hearings", column: "appeal_id"
-    validate_foreign_key "hearings", column: "appeal_id"
-  end
-end
