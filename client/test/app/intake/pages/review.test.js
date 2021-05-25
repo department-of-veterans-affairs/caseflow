be4d1eb0d91a5d6741d312612092c543cc65f048
Nik Weinmeister <nweinmeister@tistatech.com>
2021-05-25 15:27:03 -0400

    Add test

--- client/test/app/intake/pages/review.test.js
+++ client/test/app/intake/pages/review.test.js
@@ -1,9 +1,9 @@
 import {reviewAppealSchema} from 'app/intake/pages/appeal/review';
 import { REVIEW_OPTIONS } from 'app/intake/constants';
 
-const assertValidSchema = async (schema, testSchema, selectedForm, useAmaActivationDate, isValid) => {
+const assertValidSchema = async (schema, testSchema, useAmaActivationDate, isValid) => {
   await schema
-    .isValid(testSchema, { context: {selectedForm: selectedForm, useAmaActivationDate: useAmaActivationDate}})
+    .isValid(testSchema, { context: {useAmaActivationDate: useAmaActivationDate}})
     .then((valid) => expect(valid).toBe(isValid))
 }
 
@@ -13,33 +13,36 @@ const AFTER_AMA_DATE = '02/20/2019'
 const BEFORE_AMA_TEST_DATE = '10/31/2017'
 const AFTER_AMA_TEST_DATE = '11/02/2017'
 
+const validReviewAppealData = {
+  'receipt-date': AFTER_AMA_DATE,
+  'docket-type': 'docket',
+  'docket-type': 'type',
+  'legacy-opt-in': 'true',
+  'different-claimant-option': 'false'
+} 
+
 describe('schema', () => {
   describe('useAmaActivationDate', () => {
     it('is valid after feb 19, 2019', async () => {
-      const validSchema = { receiptDate: AFTER_AMA_DATE }
-      await assertValidSchema(reviewAppealSchema, validSchema, REVIEW_OPTIONS.APPEAL.key, true, true)
+      await assertValidSchema(reviewAppealSchema, validReviewAppealData, true, true)
     })
     it('is invalid before feb 19, 2019', async () => {
-      const invalidSchema = { receiptDate: BEFORE_AMA_DATE }
-      await assertValidSchema(reviewAppealSchema, invalidSchema, REVIEW_OPTIONS.APPEAL.key, true, false)
-    })
-    it('is valid before feb 19, 2019 if not appeal', async () => {
-      const validSchema = { receiptDate: BEFORE_AMA_DATE }
-      await assertValidSchema(reviewAppealSchema, validSchema, 'not_appeal', true, true)
+      const invalidSchema = validReviewAppealData
+      invalidSchema['receipt-date'] = BEFORE_AMA_DATE
+      await assertValidSchema(reviewAppealSchema, invalidSchema, true, false)
     })
   })
   describe('!useAmaActivationDate', () => {
     it('is valid after nov 1, 2017', async () => {
-      const validSchema = { receiptDate: AFTER_AMA_TEST_DATE }
-      await assertValidSchema(reviewAppealSchema, validSchema, REVIEW_OPTIONS.APPEAL.key, false, true)
+      const validSchema = validReviewAppealData
+      validSchema['receipt-date'] = AFTER_AMA_TEST_DATE
+      await assertValidSchema(reviewAppealSchema, validReviewAppealData, false, true)
     })
     it('is invalid before nov 1, 2017', async () => {
-      const invalidSchema = { receiptDate: BEFORE_AMA_TEST_DATE }
-      await assertValidSchema(reviewAppealSchema, invalidSchema, REVIEW_OPTIONS.APPEAL.key, false, false)
-    })
-    it('is valid before nov 1, 2017 if not appeal', async () => {
-      const validSchema = { receiptDate: BEFORE_AMA_TEST_DATE }
-      await assertValidSchema(reviewAppealSchema, validSchema, 'not_appeal', true, true)
+      const invalidSchema = validReviewAppealData
+      invalidSchema['receipt-date'] = BEFORE_AMA_TEST_DATE
+
+      await assertValidSchema(reviewAppealSchema, invalidSchema, false, false)
     })
   })
 });
