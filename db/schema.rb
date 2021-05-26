3cc8f3a5cf929680abc21a7c6dae065af5e249e6
Will Drougas <wdrougas@tistatech.com>
2021-05-25 21:08:33 -0400

    updated migration date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_184436) do
+ActiveRecord::Schema.define(version: 2021_05_21_143811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
