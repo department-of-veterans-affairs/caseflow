e1456bbe3ce665b345e35ece095b1a1a8ee58c41
Will Drougas <wdrougas@tistatech.com>
2021-06-01 11:23:50 -0400

    spacing fix

--- db/migrate/20210601143548_add_decision_issue_end_product_establishment_foreign_keys_for_request_issues.rb
+++ db/migrate/20210601143548_add_decision_issue_end_product_establishment_foreign_keys_for_request_issues.rb
@@ -1,6 +1,6 @@
 class AddDecisionIssueEndProductEstablishmentForeignKeysForRequestIssues < Caseflow::Migration
   def change
-  	add_foreign_key "request_issues", "decision_issues", column: "contested_decision_issue_id", validate: false    
+    add_foreign_key "request_issues", "decision_issues", column: "contested_decision_issue_id", validate: false    
     add_foreign_key "request_issues", "end_product_establishments", validate: false
   end
 end
