58df424adf20fc2e2846d16e811dbd3a32d583cf
Will Drougas <wdrougas@tistatech.com>
2021-06-01 10:43:42 -0400

    updated class inheritance to Caseflow::Migration

--- db/migrate/20210601143548_add_decision_issue_end_product_establishment_foreign_keys_for_request_issues.rb
+++ db/migrate/20210601143548_add_decision_issue_end_product_establishment_foreign_keys_for_request_issues.rb
@@ -1,4 +1,4 @@
-class AddDecisionIssueEndProductEstablishmentForeignKeysForRequestIssues < ActiveRecord::Migration[5.2]
+class AddDecisionIssueEndProductEstablishmentForeignKeysForRequestIssues < Caseflow::Migration
   def change
   	add_foreign_key "request_issues", "decision_issues", column: "contested_decision_issue_id", validate: false    
     add_foreign_key "request_issues", "end_product_establishments", validate: false
