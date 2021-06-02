19f280662834ad136607c518db3c64e8f5a9c8e5
Stephen Halliburton <stephen.halliburton@va.gov>
2021-06-02 08:46:30 -0700

    Put moment back into Comment component

--- client/app/2.0/components/reader/DocumentList/CommentsTable/Comment.jsx
+++ client/app/2.0/components/reader/DocumentList/CommentsTable/Comment.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { noop } from 'lodash';
+import moment from 'moment';
 
 // Local Dependencies
 import Highlight from 'app/components/Highlight';
