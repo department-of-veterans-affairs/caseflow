e3bcb04611348522d646a60f4986985349776e96
Yoom Lam <yoom@navapbc.com>
2021-06-02 08:20:37 -0500

    Add explain endpoint for appeals for System Admins only (#16310)
    Innovation project: [Appeal Narrative](https://dsva.slack.com/archives/C022T6KSE6S/p1622571159006000)
    
    This first PR provides data (and data dumps) to be potentially shown to a System Admin user. Subsequent PRs will replace the frontend with a more non-engineer friendly UI.
    
    ### Description
    
    Add `explain` endpoint for appeals with placeholder files. 
    The endpoint only works for System Admins (i.e., `user.admin? => true`).
    
    Shows task tree and intake rendering in the browser.
    
    Changes copied from draft PR #16039.
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly
    - [ ] `explain` endpoint shows data about the appeal
    
    ### Testing Plan
    1. In dev Rails console, list System Admin users: `Functions.list_all`
    2. In Caseflow, log in as a System Admin (e.g., `BVASCASPER1`)
    3. Go to their queue: http://localhost:3000/queue and click on a case to open Case Details. Try with AMA and legacy cases.
    4. Change the url from `queue` to `explain`, like this: `http://localhost:3000/explain/appeals/31927be1-8447-44c4-ad66-8b73ae8bf401`
    5. Click on the blue 'Help' text
       - Append `?show_pii`to the URL like this: `http://localhost:3000/explain/appeals/31927be1-8447-44c4-ad66-8b73ae8bf401?show_pii` or click on the link
       - Customize task tree columns like this: `http://localhost:3000/explain/appeals/31927be1-8447-44c4-ad66-8b73ae8bf401?fields=id,status,updated_at,CLO_DATE` or click on the link for an example
       - Click on the `json` link to show the JSON file contents, which can then be examined for PII and then saved locally for importing.
    
    Repeat the above logged in as a non-admin. Should see an error:
    ```
    {"errors":[{"status":403,"title":"Additional access needed","detail":"Additional access needed"}]}
    ```
    
    You can also add `binding.pry` in `explain_spec.rb` and examine the page.
    
    ### User Facing Changes
    ![image](https://user-images.githubusercontent.com/55255674/112224053-c3ea6200-8bf8-11eb-903b-2ee5405a12a7.png)
    ![image](https://user-images.githubusercontent.com/55255674/112227334-7d4b3680-8bfd-11eb-8ed5-d9f1b1a70df3.png)
    
    ### Code Documentation Updates
    - [ ] Add or update code comments at the top of the class, module, and/or component.

---
+++ spec/controllers/explain_controller_spec.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+require "helpers/sanitized_json_configuration.rb"
+require "helpers/sanitized_json_exporter.rb"
+require "helpers/intake_renderer.rb"
+
+describe ExplainController, :all_dbs, type: :controller do
+  include TaskHelpers
+
+  describe "GET explain/appeals/:appeal_id" do
+    let(:user_roles) { ["System Admin"] }
+    before do
+      User.authenticate!(roles: user_roles)
+    end
+
+    context ".json request for a legacy appeal" do
+      let(:user) { create(:user) }
+      let(:role) { :judge_role }
+      let!(:staff_record) { create(:staff, role, sdomainid: user.css_id) }
+      let(:appeal) { create(:legacy_appeal, vacols_case: create(:case, :assigned, user: user, document_id: "NONE")) }
+      subject { get :show, params: { appeal_id: appeal.vacols_id }, as: response_format }
+      context ".json request" do
+        let(:response_format) { :json }
+        it "returns 'unsupported' string" do
+          subject
+          expect(JSON.parse(response.body)).to eq "(LegacyAppeals are not yet supported)"
+        end
+      end
+      context ".text request" do
+        let(:response_format) { :text }
+        it "returns a minimal task tree" do
+          subject
+          expect(response.body).to include "LegacyAppeal #{appeal.id}"
+          expect(response.body).to include "JudgeLegacyDecisionReviewTask"
+          expect(response.body).to match(/assigned_to:.*User id: #{user.id}/)
+        end
+      end
+    end
+
+    let(:veteran) { create(:veteran, file_number: "111447777", middle_name: "Middle") }
+    let(:appeal) do
+      create(:appeal,
+             :advanced_on_docket_due_to_motion,
+             :with_schedule_hearing_tasks,
+             :with_post_intake_tasks,
+             veteran: veteran)
+    end
+
+    let(:response_format) { :json }
+    let(:appeal_id) { appeal.uuid }
+    subject { get :show, params: { appeal_id: appeal_id }, as: response_format }
+
+    context "when not an System Admin" do
+      let(:user_roles) { [] }
+      it "returns valid JSON tree" do
+        subject
+        expect(response.response_code).to eq 403
+        json_body = JSON.parse(response.body)
+        expect(json_body["errors"].first["title"]).to eq "Additional access needed"
+      end
+    end
+
+    context ".json request" do
+      let(:response_format) { :json }
+      it "returns valid JSON tree" do
+        subject
+        json_body = JSON.parse(response.body)
+        expect(json_body.keys).to include("metadata", "appeals", "veterans", "tasks", "users", "organizations")
+        expect(json_body["veterans"].first["file_number"]).not_to eq veteran.file_number
+      end
+    end
+
+    context ".text request" do
+      let(:response_format) { :text }
+      it "returns plain text task tree and intake tree" do
+        subject
+        # task tree
+        expect(response.body).to include "Appeal #{appeal.id}"
+        expect(response.body).to include "RootTask"
+        expect(response.body).to include "HearingTask"
+        expect(response.body).to include "ScheduleHearingTask"
+
+        # intake render output
+        expect(response.body).to include "VeteranClaimant"
+        expect(response.body).to include "breadcrumbs:"
+        expect(response.body).to include "Veteran #{appeal.veteran.id}"
+      end
+    end
+
+    context ".html (default) request" do
+      let(:response_format) { :html }
+      it "responds without error" do
+        subject
+        expect(response).to be_ok
+      end
+    end
+
+    context "when show_pii is true" do
+      subject { get :show, params: { appeal_id: appeal_id, show_pii: true }, as: response_format }
+      context ".json request" do
+        let(:response_format) { :json }
+        it "returns valid JSON tree" do
+          subject
+          json_body = JSON.parse(response.body)
+          expect(json_body.keys).to include("metadata", "appeals", "veterans", "tasks", "users", "organizations")
+          expect(json_body["veterans"].first["file_number"]).to eq veteran.file_number
+        end
+      end
+    end
+  end
+end
