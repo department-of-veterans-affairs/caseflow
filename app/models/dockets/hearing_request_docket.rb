bc6686a51f5f82a052dc96f5aef3625de53e5a18
lowellrex <lowell@navahq.com>
2021-06-03 18:41:12 -0400

    Make all case distributions for AMA hearing request docket use genpop

--- app/models/dockets/hearing_request_docket.rb
+++ app/models/dockets/hearing_request_docket.rb
@@ -10,35 +10,21 @@ class HearingRequestDocket < Docket
   end
 
   def age_of_n_oldest_genpop_priority_appeals(num)
-    HearingRequestDistributionQuery.new(
-      base_relation: ready_priority_appeals.limit(num), genpop: "only_genpop"
-    ).call.map(&:ready_for_distribution_at)
+    ready_priority_appeals.limit(num).map(&:ready_for_distribution_at)
   end
 
   # Hearing cases distinguish genpop from cases tied to a judge
   def genpop_priority_count
-    HearingRequestDistributionQuery.new(base_relation: ready_priority_appeals, genpop: "only_genpop").call.count
+    ready_priority_appeals.count
   end
 
+  # "genpop" attribute is unused because all Hearing Request docket appeals are "genpop". Leaving
+  # "genpop" attribute in the function signature for compatibility with other Dockets.
   def distribute_appeals(distribution, priority: false, genpop: "any", limit: 1)
-    base_relation = appeals(priority: priority, ready: true).limit(limit)
-
-    appeals = HearingRequestDistributionQuery.new(
-      base_relation: base_relation, genpop: genpop, judge: distribution.judge
-    ).call
-
-    appeals = self.class.limit_genpop_appeals(appeals, limit) if genpop.eql? "any"
+    appeals = appeals(priority: priority, ready: true).limit(limit)
 
     HearingRequestCaseDistributor.new(
-      appeals: appeals, genpop: genpop, distribution: distribution, priority: priority
+      appeals: appeals, distribution: distribution, priority: priority
     ).call
-  end
-
-  def self.limit_genpop_appeals(appeals_array, limit)
-    # genpop 'any' returns 2 arrays of the limited base relation. This means if we only request 2 cases, appeals is a
-    # 2x2 array containing 4 cases overall and we will end up distributing 4 cases rather than 2. Instead, reinstate the
-    # limit here by filtering out the newest cases
-    appeals_to_reject = appeals_array.flatten.sort_by(&:ready_for_distribution_at).drop(limit)
-    appeals_array.map { |appeals| appeals - appeals_to_reject }
   end
 end
--- app/workflows/hearing_request_case_distributor.rb
+++ app/workflows/hearing_request_case_distributor.rb
@@ -9,11 +9,11 @@ class HearingRequestCaseDistributor
   end
 
   def call
-    appeals_to_distribute.map do |appeal, genpop_value|
+    appeals.map do |appeal|
       Distribution.transaction do
         rename_any_existing_distributed_case(appeal)
         task = create_judge_assign_task_for_appeal(appeal)
-        create_distribution_case_for_task(task, genpop_value)
+        create_distribution_case_for_task(task)
       end
     end
   end
@@ -30,39 +30,19 @@ class HearingRequestCaseDistributor
 
   attr_reader :appeals, :genpop, :distribution, :priority
 
-  def appeals_to_distribute
-    not_genpop_appeals.map { |appeal| [appeal, false] }.concat(only_genpop_appeals.map { |appeal| [appeal, true] })
-  end
-
   def create_judge_assign_task_for_appeal(appeal)
     JudgeAssignTaskCreator.new(appeal: appeal, judge: distribution.judge).call
   end
 
-  def create_distribution_case_for_task(task, genpop_value)
+  def create_distribution_case_for_task(task)
     distribution.distributed_cases.create!(
       case_id: task.appeal.uuid,
       docket: Constants.AMA_DOCKETS.hearing,
       priority: priority,
       ready_at: task.appeal.ready_for_distribution_at,
       task: task,
-      genpop: genpop_value,
+      genpop: :true,
       genpop_query: genpop
     )
-  end
-
-  def not_genpop_appeals
-    return appeals if genpop == "not_genpop"
-
-    return appeals[0] if genpop == "any"
-
-    []
-  end
-
-  def only_genpop_appeals
-    return appeals if genpop == "only_genpop"
-
-    return appeals[1] if genpop == "any"
-
-    []
   end
 end
