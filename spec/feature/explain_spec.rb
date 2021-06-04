0c72bf47ffae7b2a8881ae03ddbe51e14f772768
yoomlam <Dung.Lam1@va.gov>
2021-06-04 16:35:57 -0500

    use real appeal in rspec

--- spec/feature/explain_spec.rb
+++ spec/feature/explain_spec.rb
@@ -2,6 +2,7 @@
 
 require "helpers/sanitized_json_configuration.rb"
 require "helpers/sanitized_json_exporter.rb"
+require "helpers/sanitized_json_importer.rb"
 require "helpers/intake_renderer.rb"
 
 RSpec.feature "Explain JSON" do
@@ -84,5 +85,19 @@ RSpec.feature "Explain JSON" do
   scenario "admin visits explain page for appellant_substitution CAVC-remanded appeal" do
     visit "explain/appeals/#{appellant_substitution.target_appeal.uuid}"
     expect(page).to have_content("Appeal.find(#{appellant_substitution.target_appeal.id})")
+  end
+
+  context "for a realistic appeal" do
+    let(:json_filename) { "appeal-21430.json" }
+    let(:real_appeal) do
+      sji = SanitizedJsonImporter.from_file("spec/records/#{json_filename}", verbosity: 0)
+      sji.import
+      sji.imported_records[Appeal.table_name].first
+    end
+    it "present realistic appeal events" do
+      visit "explain/appeals/#{real_appeal.uuid}"
+      binding.pry
+      # expect(page).to have_content("Appeal.find(#{appellant_substitution.target_appeal.id})")
+    end
   end
 end
