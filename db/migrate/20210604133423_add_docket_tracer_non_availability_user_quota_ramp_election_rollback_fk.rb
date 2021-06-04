c5ec655605e4aa0a073f9ba04264368b6af7338d
Will Drougas <wdrougas@tistatech.com>
2021-06-04 09:38:29 -0400

    spacing fix

--- db/migrate/20210604133423_add_docket_tracer_non_availability_user_quota_ramp_election_rollback_fk.rb
+++ db/migrate/20210604133423_add_docket_tracer_non_availability_user_quota_ramp_election_rollback_fk.rb
@@ -1,6 +1,6 @@
 class AddDocketTracerNonAvailabilityUserQuotaRampElectionRollbackFk < Caseflow::Migration
   def change
-  	add_foreign_key "docket_tracers", "docket_snapshots", validate: false
+    add_foreign_key "docket_tracers", "docket_snapshots", validate: false
     add_foreign_key "non_availabilities", "schedule_periods", validate: false
     add_foreign_key "user_quotas", "team_quotas", validate: false
     add_foreign_key "ramp_election_rollbacks", "ramp_elections", validate: false
--- db/migrate/20210604133455_validate_docket_tracer_non_availability_user_quota_ramp_election_rollback_fk.rb
+++ db/migrate/20210604133455_validate_docket_tracer_non_availability_user_quota_ramp_election_rollback_fk.rb
@@ -1,6 +1,6 @@
 class ValidateDocketTracerNonAvailabilityUserQuotaRampElectionRollbackFk < Caseflow::Migration
   def change
-  	validate_foreign_key "docket_tracers", "docket_snapshots"
+    validate_foreign_key "docket_tracers", "docket_snapshots"
     validate_foreign_key "non_availabilities", "schedule_periods"
     validate_foreign_key "user_quotas", "team_quotas"
     validate_foreign_key "ramp_election_rollbacks", "ramp_elections"
