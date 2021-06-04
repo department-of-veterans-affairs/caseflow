4b6f99eb5a581fd0b0a782b56b919fbf944355c3
Anya Roltsch <aroltsch@tistatech.com>
2021-06-04 11:01:44 -0400

    Remove

--- docker-bin/build.sh
+++ docker-bin/build.sh
@@ -42,7 +42,6 @@ cd ../../
 printf "commit: `git rev-parse HEAD`\ndate: `git log -1 --format=%cd`" > config/build_version.yml
 
 credstash -t appeals-credstash get datadog.api.key > config/datadog.key
-credstash -t appeals-credstash get appoptics.api.key > config/appoptics.key
 
 # Build Docker
 echo -e "\tCreating Caseflow App Docker Image"
@@ -50,6 +49,5 @@ docker build -t caseflow . --no-cache
 
 echo -e "\tCleaning Up..."
 rm -rf config/datadog.key
-rm -rf config/appoptics.key
 rm -rf docker-bin/oracle_libs/
 echo -e "\tBuilding Caseflow Docker App: Completed"
