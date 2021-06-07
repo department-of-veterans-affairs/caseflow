9797aa05d1a5ee9b8bd88b2c3c485493bf774121
yoomlam <Dung.Lam1@va.gov>
2021-06-07 11:30:06 -0500

    comment out binding

--- app/controllers/explain_controller.rb
+++ app/controllers/explain_controller.rb
@@ -20,7 +20,7 @@ class ExplainController < ApplicationController
         format.json { render json: sanitized_json }
       end
     rescue StandardError => error
-      binding.pry
+      # binding.pry
       raise error.full_message
     end
   end
