c2109585a04f9606d66c07eb87f2b9eaaa41063c
charleyf <Charles.Ferguson@va.gov>
2021-06-07 15:38:41 -0400

    Add todo

--- lib/helpers/hearing_renderer.rb
+++ lib/helpers/hearing_renderer.rb
@@ -173,6 +173,7 @@ class HearingRenderer
 
   def get_unscheduled_hearings(appeal)
     unscheduled = appeal.tasks.open.of_type(:ScheduleHearingTask)&.pluck(:id)&.map { |i| "id: #{i}" }
+    # TODO, does this get the regional_office correctly?
     regional_office = appeal.closest_regional_office || appeal.regional_office&.key
     ro_label = get_ro_label(regional_office)
     unscheduled.nil? ? "" : ["Unsched hearing #{unscheduled} for #{ro_label}"]
