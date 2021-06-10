63070680a3f64dcbfcbbaca0c91d4c4b326a6ede
lowellrex <lowell@navahq.com>
2021-06-10 14:47:13 -0400

    Undo simplification so tests pass

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -86,8 +86,9 @@ class Docket
 
   def docket_appeals
     Appeal.joins(:claimants)
+      .joins("left join unrecognized_appellants on claimants.id = unrecognized_appellants.claimant_id")
       .where(docket_type: docket_type)
-      .where("claimants.type <> 'OtherClaimant'")
+      .where("unrecognized_appellants.id is null")
       .extending(Scopes)
   end
 
