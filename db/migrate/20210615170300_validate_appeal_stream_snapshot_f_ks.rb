17d16e46932b6fd325b322b1b1c07d7ddd3da0d2
Will Drougas <wdrougas@tistatech.com>
2021-06-15 13:18:31 -0400

    spacing fix

--- db/migrate/20210615170300_validate_appeal_stream_snapshot_f_ks.rb
+++ db/migrate/20210615170300_validate_appeal_stream_snapshot_f_ks.rb
@@ -1,6 +1,6 @@
 class ValidateAppealStreamSnapshotFKs < Caseflow::Migration
   def change
-  	validate_foreign_key "hearing_appeal_stream_snapshots", "legacy_appeals"    
+    validate_foreign_key "hearing_appeal_stream_snapshots", "legacy_appeals"    
     validate_foreign_key "hearing_appeal_stream_snapshots", "legacy_hearings"
   end
 end
