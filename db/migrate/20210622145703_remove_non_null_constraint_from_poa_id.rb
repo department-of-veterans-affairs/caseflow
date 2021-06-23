aca558629e17c602ee2eba87a754bbb6b1cc2a4d
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-06-23 11:29:01 -0400

    update schema comments

--- db/migrate/20210622145703_remove_non_null_constraint_from_poa_id.rb
+++ db/migrate/20210622145703_remove_non_null_constraint_from_poa_id.rb
@@ -1,10 +1,11 @@
 class RemoveNonNullConstraintFromPoaId < Caseflow::Migration
   def up
     change_column_null :appellant_substitutions, :poa_participant_id, true
+    change_column_default :appellant_substitutions, :poa_participant_id, from: "", to: nil
   end
 
   def down
-    change_column_null :appellant_substitutions, :poa_participant_id, false
     change_column_default :appellant_substitutions, :poa_participant_id, from: nil, to: ""
+    change_column_null :appellant_substitutions, :poa_participant_id, false, ""
   end
 end
