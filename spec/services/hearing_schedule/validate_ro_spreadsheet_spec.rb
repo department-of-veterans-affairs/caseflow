b5016ea48df0a7c5a5d7a2e90195b5dfacc48922
sahalliburton <shalliburton@tistatech.com>
2021-06-24 09:22:37 -0700

    Apply suggestions from code review
    Co-authored-by: Rubaiyat Rashid <Rubaiyat.Rashid@va.gov>

--- spec/services/hearing_schedule/validate_ro_spreadsheet_spec.rb
+++ spec/services/hearing_schedule/validate_ro_spreadsheet_spec.rb
@@ -203,6 +203,7 @@ describe HearingSchedule::ValidateRoSpreadsheet do
       expect(subject.first.message).to include("RO01")
       expect(subject.first.message).to include("RO02")
       expect(subject.first.message).to include("RO49")
+      # RO06 has 0 for virtual allocation so it does not fail
       expect(subject.first.message).not_to include("RO06")
     end
   end
@@ -222,6 +223,7 @@ describe HearingSchedule::ValidateRoSpreadsheet do
       expect(subject.first.message).to include("RO01")
       expect(subject.first.message).to include("RO02")
       expect(subject.first.message).to include("RO59")
+      # RO06 has 0 for virtual allocation so it does not fail
       expect(subject.first.message).not_to include("RO06")
     end
   end
@@ -241,6 +243,7 @@ describe HearingSchedule::ValidateRoSpreadsheet do
       expect(subject.first.message).to include("RO01")
       expect(subject.first.message).to include("RO02")
       expect(subject.first.message).to include("RO39")
+      # RO06 has 0 for virtual allocation so it does not fail
       expect(subject.first.message).not_to include("RO06")
     end
   end
