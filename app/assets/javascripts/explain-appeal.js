e5a739459ccbf1e6b80b4db85a6bd04b50b110b2
yoomlam <Dung.Lam1@va.gov>
2021-06-25 17:43:23 -0500

    styling timeline

--- app/assets/javascripts/explain-appeal.js
+++ app/assets/javascripts/explain-appeal.js
@@ -1,4 +1,5 @@
 //= require vis-timeline/dist/vis-timeline-graph2d.min.js
+// Example timeline visualizations: https://visjs.github.io/vis-timeline/examples/timeline/
 
 /**
  * Support code for app/views/explain/show.html.erb,
@@ -6,6 +7,30 @@
  * Also see accompanying app/assets/stylesheets/explain_appeal.css.
  * These are added to the application in config/initializers/assets.rb.
  */
+
+// called from app/views/explain/show.html.erb
+function addTimeline(elementId, timeline_data){
+  const timelineElement = document.getElementById(elementId);
+  const items = new vis.DataSet(decorateTimelineItems(timeline_data));
+  const groups = groupEventItems(timeline_data);
+  const timeline_options = {
+    width: '95%',
+    maxHeight: 800,
+    horizontalScroll: true,
+    verticalScroll: true,
+    min: new Date(2018, 0, 1), // TODO: calculate earliest
+    max: new Date((new Date()).valueOf() + 1000*60*60*24*30*12), // add exta time so that labels are visible
+    zoomMin: 1000 * 60 * 60, // 1 hour in milliseconds
+    orientation: {axis: 'both'},  // show time axis on both the top and bottom
+    stack: true,
+    zoomKey: 'ctrlKey',
+    order: (a, b) => b.record_id - a.record_id,
+    tooltip: {
+      followMouse: true
+    }
+  };
+  return new vis.Timeline(timelineElement, items, groups, timeline_options);
+}
 
 const taskNodeColor = {
   assigned: "#00dd00",
@@ -16,9 +41,10 @@ const taskNodeColor = {
 }
 
 const itemDecoration = {
-  tasks: { style: (item)=>"background-color: "+taskNodeColor[item.status] }
+  tasks: { style: (item)=>"OLD-background-color: "+taskNodeColor[item.status] }
 };
 
+// https://visjs.github.io/vis-timeline/docs/timeline/#items
 function decorateTimelineItems(items){
   items.forEach(item => {
     if(!itemDecoration.hasOwnProperty(item.tableName)) return;
@@ -33,11 +59,28 @@ function decorateTimelineItems(items){
   return items;
 }
 
-function addTimeline(elementId, timeline_data){
-  const timeline = document.getElementById(elementId);
-  const items = new vis.DataSet(decorateTimelineItems(timeline_data));
-  const timeline_options = {
-    width: '95%'
-  };
-  new vis.Timeline(timeline, items, timeline_options);
+// https://visjs.github.io/vis-timeline/docs/timeline/#groups
+function groupEventItems(items){
+  var groups = new vis.DataSet();
+  // TODO: add checkboxes to set 'visible' value on groups
+  groups.add({
+      id: 0,
+      content: 'phase',
+      className: 'group_phase',
+      order: 0,
+      visible: true
+  });
+  groups.add({
+      id: 1,
+      content: 'tasks',
+      className: 'group_tasks',
+      order: 1,
+  });
+  groups.add({
+    id: 2,
+    content: 'intakes',
+    className: 'group_intakes',
+    order: 2,
+  });
+return groups;
 }
--- app/controllers/concerns/explain_timeline_concern.rb
+++ app/controllers/concerns/explain_timeline_concern.rb
@@ -13,12 +13,19 @@ module ExplainTimelineConcern
     tasks_as_timeline_data + intakes_as_timeline_data
   end
 
+  EVENT_GROUPS = {
+
+  }
+
+  BACKGROUND_TASK_TYPES = %w[DistributionTask JudgeDecisionReviewTask].freeze
+
   # :reek:FeatureEnvy
   def tasks_as_timeline_data
     sje.records_hash[Task.table_name].map do |record|
       significant_duration = record["closed_at"] - record["created_at"] > 120 if record["closed_at"]
       record.clone.tap do |clone_record|
         clone_record["id"] = "#{Task.name}#{record['id']}"
+        clone_record["record_id"] = record["id"]
         clone_record["tableName"] = Task.table_name
         clone_record["content"] = "#{record['type']}_#{record['id']}"
         clone_record["start"] = record["created_at"]
@@ -26,7 +33,20 @@ module ExplainTimelineConcern
         clone_record["taskType"] = record["type"]
 
         # clear these values to prevent conflict the Timeline Item's fields
-        clone_record["type"] = nil
+        # clone_record["type"] = "point" unless significant_duration
+        if BACKGROUND_TASK_TYPES.include?(record["type"])
+          clone_record["type"] = "background"
+          clone_record["group"] = nil # so that background is visible in all groups
+          clone_record["end"] ||= record["closed_at"] || Date.today # needs to be set for 'background' types
+        else
+          clone_record["type"] = nil
+          clone_record["group"] = 1
+        end
+
+        # for visualization
+        # clone_record["group"] = Task.table_name
+        clone_record["title"] = "<pre style='font-size:0.84em'><code>#{JSON.pretty_generate(clone_record)}</code></pre>"
+        clone_record["className"] = "#{record['type']} task_#{record['status']}"
       end
     end
   end
@@ -37,6 +57,7 @@ module ExplainTimelineConcern
       significant_duration = record["completed_at"] - record["started_at"] > 120 if record["completed_at"]
       record.clone.tap do |clone_record|
         clone_record["id"] = "#{record['type']}#{record['id']}"
+        clone_record["record_id"] = record["id"]
         clone_record["tableName"] = Intake.table_name
         clone_record["content"] = "#{record['type']}_#{record['id']}"
         clone_record["start"] = record["started_at"]
@@ -45,6 +66,7 @@ module ExplainTimelineConcern
 
         # clear these values to prevent conflict the Timeline Item's fields
         clone_record["type"] = nil
+        clone_record["group"] = 2
       end
     end
   end
