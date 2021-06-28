3cb5d5d21c259b39d75ec77ed2ad549c4639e16e
nava <barshirtcliff@navapbc.com>
2021-06-28 16:34:11 -0700

    add :ihp_allowed to poa serializer (new serializer)

---
+++ app/models/serializers/work_queue/power_of_attorney_serializer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class WorkQueue::PowerOfAttorneySerializer
+  include FastJsonapi::ObjectSerializer
+  attribute :id
+  attribute :claimant_participant_id
+  attribute :poa_participant_id
+  attribute :representative_name
+  attribute :representative_type
+  attribute :file_number
+  attribute :authzn_change_clmant_addrs_ind
+  attribute :authzn_poa_access_ind
+  attribute :legacy_poa_cd
+  attribute :last_synced_at
+  attribute :created_at
+  attribute :updated_at
+
+  non_ihp_writing_org_types = ['FieldVso', 'PrivateBar']
+  attribute :ihp_allowed do |object|
+    org = Organization.find_by(participant_id: object.poa_participant_id)
+    org != nil && !(non_ihp_writing_org_types.include? org.type)
+  end
+end
