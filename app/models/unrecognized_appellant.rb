2d5aeb58ddba2f0cadfa23fa235ada63da6f40d3
Nik Weinmeister <nweinmeister@tistatech.com>
2021-06-28 14:14:27 -0400

    Adding versioning relationships for unrecognized appellants

--- app/models/unrecognized_appellant.rb
+++ app/models/unrecognized_appellant.rb
@@ -7,6 +7,11 @@ class UnrecognizedAppellant < CaseflowRecord
   belongs_to :unrecognized_party_detail, dependent: :destroy
   belongs_to :unrecognized_power_of_attorney, class_name: "UnrecognizedPartyDetail", dependent: :destroy
 
+  has_many :versions, class_name: "UnrecognizedAppellant", foreign_key: "current_version_id"
+  belongs_to :current_version, class_name: "UnrecognizedAppellant", optional: true
+
+  belongs_to :created_by, class_name: "User", optional: true
+
   def power_of_attorney
     @power_of_attorney ||= begin
       if poa_participant_id
@@ -15,5 +20,9 @@ class UnrecognizedAppellant < CaseflowRecord
         UnrecognizedPowerOfAttorney.new(unrecognized_power_of_attorney)
       end
     end
+  end
+
+  def original_version
+    versions.first || self
   end
 end
--- spec/models/unrecognized_appellant_spec.rb
+++ spec/models/unrecognized_appellant_spec.rb
@@ -23,4 +23,39 @@ describe UnrecognizedAppellant do
       it { is_expected.to be_nil }
     end
   end
+
+  describe "#current_version" do
+    fcontext "when there is more than one version of the unrecognized appellant" do
+      it "returns a list of versions" do
+        current_user = User.last
+        ua_detail = create(:unrecognized_party_detail, :individual) 
+        # Initial creation of UA
+        ua_current = create(:unrecognized_appellant, unrecognized_party_detail: ua_detail)
+
+        expect(ua_current.original_version).to eq ua_current
+
+        ## Begin update process
+        # Duplicate the current UA and UA details
+        ua_old_version_1 = ua_current.dup
+        ua_details_old_version = ua_current.unrecognized_party_detail.dup
+        # Point the duplicated version to the current version and link duplicated details
+        ua_old_version_1.update(current_version: ua_current, unrecognized_party_detail: ua_details_old_version)
+        # Update the current version attributes
+        ua_current.unrecognized_party_detail.update(name: "Updated")
+
+        ## Do another update
+        ua_old_version_2 = ua_current.dup
+        ua_details_old_version = ua_current.unrecognized_party_detail.dup
+        ua_old_version_2.update(current_version: ua_current, unrecognized_party_detail: ua_details_old_version)
+        ua_current.unrecognized_party_detail.update(name: "Updated Again")
+
+        expect(ua_current.versions.count).to eq 2
+        expect(ua_old_version_1.current_version).to eq ua_current
+        expect(ua_current.unrecognized_party_detail.name).to eq "Updated Again Smith"
+        expect(ua_current.original_version.unrecognized_party_detail.name).to eq "Jane Smith"
+
+        binding.pry
+      end
+    end
+  end
 end
