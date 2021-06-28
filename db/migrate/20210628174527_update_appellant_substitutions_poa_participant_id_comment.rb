e729b6a2fe0c5070f227ee5ccdcd71e3345c0541
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-06-28 16:47:10 -0400

    make change reversible

--- db/migrate/20210628174527_update_appellant_substitutions_poa_participant_id_comment.rb
+++ db/migrate/20210628174527_update_appellant_substitutions_poa_participant_id_comment.rb
@@ -1,5 +1,9 @@
 class UpdateAppellantSubstitutionsPoaParticipantIdComment < Caseflow::Migration[5.2]
-  def change
+  def up
     change_column_comment(:appellant_substitutions, :poa_participant_id, "Identifier of the appellant's POA, if they have a CorpDB participant_id. Null if the substitute appellant has no POA.")
+  end
+
+  def down
+    change_column_comment(:appellant_substitutions, :poa_participant_id, "Identifier of the appellant's POA, if they have a CorpDB participant_id")
   end
 end
