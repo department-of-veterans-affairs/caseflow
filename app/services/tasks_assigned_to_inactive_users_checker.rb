5fe6214dc429adaa2fa9aff43fdd9db7531d7dc6
Yoom Lam <yoom@navapbc.com>
2021-06-29 14:15:39 -0500

    New checker job to list tasks assigned to inactive users (#16430)
    ### Description
    To prevent stalled or stuck appeals, this PR creates new checker job to list tasks assigned to inactive users.
    
    This new checker is added to an existing `DataIntegrityChecksJob`, which runs daily.
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly
    - [ ] Checker reports to Slack a list of all tasks assigned to inactive users
    
    ### Testing Plan
    See RSpec.
    
    Once deployed to UAT, run: 
    ```ruby
    checker = TasksAssignedToInactiveUsersChecker.new
    checker.tasks_for_inactive_users
    checker.report
    ```

---
+++ app/services/tasks_assigned_to_inactive_users_checker.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+##
+# Checks for all open tasks assigned to inactive users.
+
+class TasksAssignedToInactiveUsersChecker < DataIntegrityChecker
+  def slack_channel
+    "#appeals-echo"
+  end
+
+  def call
+    return if tasks_for_inactive_users.count == 0
+
+    add_to_report "type, appeal_type, appeal_id, task.id, assigned_to_id, status\n#{tasks_report}"
+    add_to_report "Task count grouped by task type: #{tasks_for_inactive_users.group(:type).count}"
+    add_to_report "Task count grouped by assignee: #{task_count_by_assignee}"
+    add_to_report "To resolve them, see https://github.com/department-of-veterans-affairs/caseflow/wiki/Resolving-Background-Job-Alerts#tasksassignedtoinactiveusers"
+  end
+
+  def tasks_for_inactive_users
+    @tasks_for_inactive_users ||= Task.open.where(assigned_to: inactive_users)
+  end
+
+  private
+
+  def tasks_report
+    tasks_for_inactive_users.order(:type, :appeal_type, :appeal_id, :id, :assigned_to_id)
+      .pluck(:type, :appeal_type, :appeal_id, :id, :assigned_to_id, :status)
+      .map { |task_attribs| task_attribs.join(", ") }
+      .join("\n")
+  end
+
+  def inactive_users
+    User.where(status: :inactive)
+  end
+
+  def task_count_by_assignee
+    tasks_for_inactive_users.group(:assigned_to_type, :assigned_to_id).count
+  end
+end
