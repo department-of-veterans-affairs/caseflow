c50d84b7047c968e5f7edbe9973d8f19848dcb8e
yoomlam <Dung.Lam1@va.gov>
2021-06-29 13:53:58 -0500

    fix lint too long

--- app/services/tasks_assigned_to_inactive_users_checker.rb
+++ app/services/tasks_assigned_to_inactive_users_checker.rb
@@ -13,7 +13,7 @@ class TasksAssignedToInactiveUsersChecker < DataIntegrityChecker
 
     add_to_report "type, appeal_type, appeal_id, task.id, assigned_to_id, status\n#{tasks_report}"
     add_to_report "Task count grouped by task type: #{tasks_for_inactive_users.group(:type).count}"
-    add_to_report "Task count grouped by assignee: #{tasks_for_inactive_users.group(:assigned_to_type, :assigned_to_id).count}"
+    add_to_report "Task count grouped by assignee: #{task_count_by_assignee}"
     add_to_report "To resolve them, see https://github.com/department-of-veterans-affairs/caseflow/wiki/Resolving-Background-Job-Alerts#tasksassignedtoinactiveusers"
   end
 
@@ -32,5 +32,9 @@ class TasksAssignedToInactiveUsersChecker < DataIntegrityChecker
 
   def inactive_users
     User.where(status: :inactive)
+  end
+
+  def task_count_by_assignee
+    tasks_for_inactive_users.group(:assigned_to_type, :assigned_to_id).count
   end
 end
