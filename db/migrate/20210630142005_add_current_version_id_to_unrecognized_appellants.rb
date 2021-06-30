4900a53c67c002be95f9ccf6c69b90d1edf4d0ca
Nik Weinmeister <nweinmeister@tistatech.com>
2021-06-30 09:44:32 -0400

    Caseflow migration

--- db/migrate/20210630142005_add_current_version_id_to_unrecognized_appellants.rb
+++ db/migrate/20210630142005_add_current_version_id_to_unrecognized_appellants.rb
@@ -1,4 +1,4 @@
-class AddCurrentVersionIdToUnrecognizedAppellants < ActiveRecord::Migration[5.2]
+class AddCurrentVersionIdToUnrecognizedAppellants < Caseflow::Migration
   def change
     add_reference :unrecognized_appellants, :current_version, index: false, foreign_key: { to_table: :unrecognized_appellants }, comment: "The current version ID for this unrecognized appellant"
     add_reference :unrecognized_appellants, :created_by, index: false, foreign_key: { to_table: :users}, comment: "The ID of the user that created this version of the unrecognized appellant"
