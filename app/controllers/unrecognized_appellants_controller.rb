feeda52af6e50815867f7e73d15472bd01d47666
Will Drougas <wdrougas@tistatech.com>
2021-06-30 12:15:03 -0400

    cc fixes

--- app/controllers/unrecognized_appellants_controller.rb
+++ app/controllers/unrecognized_appellants_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class UnrecognizedAppellantsController < ApplicationController
-
   def update
     unrecognized_appellant = UnrecognizedAppellant.find(params[:unrecognized_appellant_id])
 
     if unrecognized_appellant.update_with_versioning!(unrecognized_appellant_params)
 	  render json: unrecognized_appellant, include: [:unrecognized_party_detail]
 	else
-	  render json: unrecognized_appellant, status: :bad_request
+      render json: unrecognized_appellant, status: :bad_request
 	end
   end
 
@@ -18,8 +17,8 @@ class UnrecognizedAppellantsController < ApplicationController
     params.require("unrecognized_appellant").permit(
 		:relationship,
 		unrecognized_party_detail: [
-			:name, :middle_name, :last_name, :suffix, :address_line_1, :address_line_2, :address_line_3, 
-			:city, :state, :zip, :country, :phone_number, :email_address
+		  :name, :middle_name, :last_name, :suffix, :address_line_1, :address_line_2, :address_line_3, 
+		  :city, :state, :zip, :country, :phone_number, :email_address
 		]
 	)
   end
