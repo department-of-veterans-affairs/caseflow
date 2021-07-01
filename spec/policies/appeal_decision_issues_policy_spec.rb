ddf2f562093c65360c582241717ee8b0d8d00d6d
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-07-01 11:42:53 -0400

    fix broken feature test and flesh out unit test

--- spec/policies/appeal_decision_issues_policy_spec.rb
+++ spec/policies/appeal_decision_issues_policy_spec.rb
@@ -2,27 +2,62 @@
 
 describe AppealDecisionIssuesPolicy, :postgres do
   describe "#visible_decision_issues" do
-    let(:user) { create(:user, :vso_role) }
-
     subject { AppealDecisionIssuesPolicy.new(user: user, appeal: appeal).visible_decision_issues }
 
-    context "when a decision issue has not yet reached its decision date" do
-      let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
-      it "cannot be seen by a VSO user" do
-        expect(subject).to match_array([])
+    context "when user has VSO role" do
+      let(:user) { create(:user, :vso_role) }
+      let(:result) { subject }
+
+      context "when a decision issue has not yet reached its decision date" do
+        let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
+
+        it "cannot be seen" do
+          expect(result).to match_array([])
+        end
+      end
+
+      context "when a decision date has reached or passed its decision date" do
+        let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
+
+        it "can be seen" do
+          expect(result[0].id).to eq(appeal.decision_issues[0].id)
+          expect(result[1].id).to eq(appeal.decision_issues[1].id)
+        end
+
+        it "its issue description is not visible" do
+          expect(result[0].description).to be_nil
+        end
       end
     end
 
-    context "when a decision date has reached or passed its decision date" do
-      let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
-      let(:result) { subject }
-      it "can be seen by a VSO user" do
-        expect(result[0].id).to eq(appeal.decision_issues[0].id)
-        expect(result[1].id).to eq(appeal.decision_issues[1].id)
+    context "when user has no VSO role" do
+      let(:user) { create(:user, :reader_role) }
+
+      context "when a decision issue has not yet reached its decision date" do
+        let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
+        let(:result) { subject }
+
+        it "can be seen" do
+          expect(result[0].id).to eq(appeal.decision_issues[0].id)
+        end
+
+        it "its issue description is visible" do
+          expect(result[0].description).to eq(appeal.decision_issues[0].description)
+        end
       end
 
-      it "VSO user cannot see the issue description or notes" do
-        expect(result[0].description).to be_nil
+      context "when a decision date has reached or passed its decision date" do
+        let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
+        let(:result) { subject }
+
+        it "can be seen" do
+          expect(result[0].id).to eq(appeal.decision_issues[0].id)
+          expect(result[1].id).to eq(appeal.decision_issues[1].id)
+        end
+
+        it "its issue description is visible" do
+          expect(result[0].description).to eq(appeal.decision_issues[0].description)
+        end
       end
     end
   end
