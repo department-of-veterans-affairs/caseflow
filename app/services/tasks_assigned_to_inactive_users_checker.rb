f5ed9fbb082957b254eb4c8ec29ab6e72584a47d
yoomlam <Dung.Lam1@va.gov>
2021-07-06 10:25:00 -0500

    reword message

--- app/services/tasks_assigned_to_inactive_users_checker.rb
+++ app/services/tasks_assigned_to_inactive_users_checker.rb
@@ -11,7 +11,7 @@ class TasksAssignedToInactiveUsersChecker < DataIntegrityChecker
   def call
     return if tasks_for_inactive_users.count == 0
 
-    add_to_report "To resolve them, see https://github.com/department-of-veterans-affairs/caseflow/wiki/Resolving-Background-Job-Alerts#tasksassignedtoinactiveusers"
+    add_to_report "To resolve, see https://github.com/department-of-veterans-affairs/caseflow/wiki/Resolving-Background-Job-Alerts#tasksassignedtoinactiveusers"
     add_to_report "\ntype, appeal_type, appeal_id, task.id, assigned_to_id, status\n#{tasks_report}"
     add_to_report "\nTask count grouped by task type: #{tasks_for_inactive_users.group(:type).count}"
     add_to_report "\nTask count grouped by assignee: #{task_count_by_assignee}"
