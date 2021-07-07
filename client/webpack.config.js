4b8560ce59ed9a5023be01333f7993440891edb9
Stephen Halliburton <stephen.halliburton@va.gov>
2021-07-07 12:16:56 -0700

    Appended webpack to chunk names to prevent git tracking

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,7 @@ const config = {
     filename: 'webpack-bundle.js',
     sourceMapFilename: 'sourcemap-[file].map',
     path: path.join(__dirname, '../app/assets/javascripts'),
-    chunkFilename: '[name].[contenthash].chunk.js',
+    chunkFilename: '[name].[contenthash].webpack.chunk.js',
     publicPath: devBuild && !testBuild ? 'http://localhost:3500/' : 'assets/'
   },
   optimization: {
