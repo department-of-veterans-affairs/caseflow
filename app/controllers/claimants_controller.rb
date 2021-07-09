department-of-veterans-affairs/caseflow

commit 9fb01abc2238f8f3e86445d89c1c8f2d29f7a1c5

Resolve POA information when Substitute Appellant does not have a POA (#16389)
Resolves [Jira ticket 1753](https://vajira.max.gov/browse/CASEFLOW-1753).

### Description
If a dependent has no POA, that dependent should still be able to be selected as a substitute appellant.  This PR makes that possible by removing power of attorney as a requirement for creating a substitute appellant.  Additionally, when a substitute appellant has a power of attorney, that information remains visible in case details.

### Acceptance Criteria
- [ ] Code compiles correctly
- [ ] In the substitute appellant flow, user can select as claimant a dependent who has no power of attorney 

### Testing Plan
1. Run the new migration in `db/migrate/20210622145703_remove_non_null_constraint_from_poa_id.rb`.
2. Generate an appeal for testing.
````
RequestStore[:current_user] = User.system_user
appeal = FactoryBot.create(:appeal, :dispatched_with_decision_issue)
````
3. Proceed through the appellant substitution flow and select the child “Tom Brady” as appellant.  This claimant does not have a POA in our fake data. 
<img width="1792" alt="SelectTomBrady" src="https://user-images.githubusercontent.com/80347702/123117602-e36b7380-d40f-11eb-9f72-d615a7de9aaa.png">



4. Ensure that there is no power of attorney on the case details page.  Should see the below for “Appellant’s Power of Attorney.”
<img width="1792" alt="Screen Shot 2021-06-22 at 12 30 00 PM" src="https://user-images.githubusercontent.com/80347702/123117849-19105c80-d410-11eb-9f90-2869ebfd3ea2.png">

5. Ensure that you don’t get any errors when you click the “refresh POA” button.


6. Roll back the migration and ensure that nothing breaks.  The `down` component of this migration should change any null values for `poa_participant` id to be empty strings.

### User Facing Changes

app/controllers/claimants_controller.rb
@@ -2,7 +2,7 @@
 
 class ClaimantsController < ApplicationController
   def refresh_claimant_poa
-    poa = BgsPowerOfAttorney.find_or_create_by_claimant_participant_id(params[:participant_id])
+    poa = BgsPowerOfAttorney.find_by(claimant_participant_id: params[:participant_id])
     # Because `update_cached_attributes!` is in a before save hook for BGSPowerOfAttorney, the below code
     # updates the poa before saving it
     poa&.save!

app/controllers/appellant_substitutions_controller.rb
@@ -11,11 +11,11 @@ class AppellantSubstitutionsController < ApplicationController
     :substitution_date,
     :claimant_type,
     :substitute_participant_id,
-    :poa_participant_id,
     :created_by_id
   ].freeze
 
   PERMITTED_PARAMS = {
+    poa_participant_id: nil,
     # blank or empty values for these params will fail the required validation, so put them here instead
     selected_task_ids: [],
     task_params: {}

app/models/appellant_substitution.rb
@@ -10,7 +10,6 @@ class AppellantSubstitution < CaseflowRecord
   validates :created_by, :source_appeal, :substitution_date,
             :claimant_type, # Claimant record type for the substitute
             :substitute_participant_id,
-            :poa_participant_id,
             presence: true
   validates :selected_task_ids,
             :task_params,
@@ -28,7 +27,7 @@ class AppellantSubstitution < CaseflowRecord
   end
 
   def power_of_attorney
-    BgsPowerOfAttorney.find_by(poa_participant_id: poa_participant_id)
+    poa_participant_id ? BgsPowerOfAttorney.find_by(poa_participant_id: poa_participant_id) : nil
   end
 
   private

client/app/queue/substituteAppellant/review/SubstituteAppellantReviewContainer.jsx
@@ -82,7 +82,7 @@ export const SubstituteAppellantReviewContainer = () => {
       substitution_date: existingValues.substitutionDate,
       claimant_type: existingValues.claimantType,
       substitute_participant_id: existingValues.participantId,
-      poa_participant_id: poa.poa_participant_id,
+      poa_participant_id: poa ? poa.poa_participant_id : null,
       selected_task_ids: existingValues.taskIds,
       task_params: buildTaskCreationParameters()
     };

db/migrate/20210622145703_remove_non_null_constraint_from_poa_id.rb
@@ -1,0 +1,11 @@
+class RemoveNonNullConstraintFromPoaId < Caseflow::Migration
+  def up
+    change_column_null :appellant_substitutions, :poa_participant_id, true
+    change_column_default :appellant_substitutions, :poa_participant_id, from: "", to: nil
+  end
+
+  def down
+    change_column_default :appellant_substitutions, :poa_participant_id, from: nil, to: ""
+    change_column_null :appellant_substitutions, :poa_participant_id, false, ""
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_221829) do
+ActiveRecord::Schema.define(version: 2021_06_22_145703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_221829) do
     t.string "claimant_type", null: false, comment: "Claimant type of substitute; needed to create Claimant record"
     t.datetime "created_at", null: false, comment: "Standard created_at/updated_at timestamps"
     t.bigint "created_by_id", null: false, comment: "User that created this record"
-    t.string "poa_participant_id", null: false, comment: "Identifier of the appellant's POA, if they have a CorpDB participant_id"
+    t.string "poa_participant_id", comment: "Identifier of the appellant's POA, if they have a CorpDB participant_id. Null if the substitute appellant has no POA."
     t.bigint "selected_task_ids", default: [], null: false, comment: "User-selected task ids from source appeal", array: true
     t.bigint "source_appeal_id", null: false, comment: "The relevant source appeal for this substitution"
     t.string "substitute_participant_id", null: false, comment: "Participant ID of substitute appellant"

lib/fakes/bgs_service.rb
@@ -640,6 +640,34 @@ class Fakes::BGSService
         ssn: nil,
         ssn_verified_ind: nil,
         terminate_reason: nil
+      },
+      {
+        authzn_change_clmant_addrs_ind: nil,
+        authzn_poa_access_ind: nil,
+        award_begin_date: nil,
+        award_end_date: nil,
+        award_ind: "N",
+        award_type: "CPL",
+        date_of_birth: "09051998",
+        date_of_death: nil,
+        dependent_reason: nil,
+        dependent_terminate_date: nil,
+        email_address: "tom.brady@caseflow.gov",
+        fiduciary: nil,
+        file_number: nil,
+        first_name: "TOM",
+        gender: nil,
+        last_name: "BRADY",
+        middle_name: "EDWARD",
+        poa: nil,
+        proof_of_dependecy_ind: nil,
+        ptcpnt_id: "no-such-pid",
+        relationship_begin_date: "09121999",
+        relationship_end_date: nil,
+        relationship_type: "Child",
+        ssn: "666004444",
+        ssn_verified_ind: nil,
+        terminate_reason: nil
       }
     ]
   end

spec/controllers/appellant_substitutions_controller_spec.rb
@@ -64,6 +64,25 @@ RSpec.describe AppellantSubstitutionsController, type: :controller do
       end
     end
 
+    context "when the substitute appellant does not have a power of attorney" do
+      let(:poa_participant_id) { nil }
+      it "creates the Appellant Substitution and a new appeal" do
+        substitution_count = AppellantSubstitution.count
+        subject
+
+        expect(response.status).to eq(201)
+        response_body = JSON.parse(response.body)
+
+        expect(response_body["substitution"]["source_appeal_id"]).to eq(source_appeal.id)
+        expect(AppellantSubstitution.count).to eq(substitution_count + 1)
+
+        expect(response_body["targetAppeal"]["id"])
+          .to eq(AppellantSubstitution.find(response_body["substitution"]["id"]).target_appeal_id)
+        expect(response_body["targetAppeal"]["stream_docket_number"]).to eq(source_appeal.docket_number)
+        expect(response_body["targetAppeal"]["stream_type"]).to eq(source_appeal.stream_type)
+      end
+    end
+
     context "with a ClerkOfTheBoard user" do
       context "when insufficient parameters are provided" do
         let(:substitute_participant_id) { nil }

commit 19e0d30cd11fd6d3c91607c2d002696cd95f9f07

fix bug introduced in 9fb01abc2238f8f3e86445d89c1c8f2d29f7a1c5

app/controllers/claimants_controller.rb
@@ -2,7 +2,7 @@
 
 class ClaimantsController < ApplicationController
   def refresh_claimant_poa
-    poa = BgsPowerOfAttorney.find_by(claimant_participant_id: params[:participant_id])
+    poa = BgsPowerOfAttorney.find_or_create_by_claimant_participant_id(params[:participant_id])
     # Because `update_cached_attributes!` is in a before save hook for BGSPowerOfAttorney, the below code
     # updates the poa before saving it
     poa&.save!