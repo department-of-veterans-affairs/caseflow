7cf3117a660de5dd3e571552dcbc31c39266fd0c
Nik Weinmeister <nweinmeister@tistatech.com>
2021-07-12 14:11:00 -0400

    Lint

--- client/app/queue/editAppellantInformation/EditAppellantInformation.jsx
+++ client/app/queue/editAppellantInformation/EditAppellantInformation.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { FormProvider } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { AddClaimantForm } from '../../intake/addClaimant/AddClaimantForm';
 import { useAddClaimantForm } from '../../intake/addClaimant/utils';
-import {updateAppellantInformation} from './actions';
+import { updateAppellantInformation } from './actions';
 import Button from '../../components/Button';
-
 
 const EditAppellantInformation = () => {
   const dispatch = useDispatch();
@@ -21,7 +20,7 @@ const EditAppellantInformation = () => {
     // TODO: Get appellant ID from existing appeal's appellant
     // For now, we'll just pass in a random ID for WIP
     dispatch(updateAppellantInformation(values, 1));
-  }
+  };
 
   return <div>
     <h1>Edit Appellant Information</h1>
