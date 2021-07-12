0dccac31cef95e7f4f7eda15d2b38553a7094c53
Nik Weinmeister <nweinmeister@tistatech.com>
2021-07-12 17:14:26 -0400

    Fix comment

--- client/app/queue/editAppellantInformation/utils.js
+++ client/app/queue/editAppellantInformation/utils.js
@@ -1,5 +1,5 @@
 // Used to map form data from the form to the shape expected by the API
 export const mapAppellantDataToApi = (appellant) => {
-  // TODO: Map the incoming data to match the shape expected by the API
+  // CASEFLOW-1923: Map the incoming data to match the shape expected by the API
   return appellant;
 }
