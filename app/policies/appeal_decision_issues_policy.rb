ff4175e34426ed9bf4b25fef90063314d39852c0
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-07-12 14:05:19 -0400

    move feature toggle conditional to policy

--- app/policies/appeal_decision_issues_policy.rb
+++ app/policies/appeal_decision_issues_policy.rb
@@ -7,17 +7,21 @@ class AppealDecisionIssuesPolicy
   end
 
   def visible_decision_issues
-    if @user.vso_employee?
-      visible_issues = @appeal.decision_issues.select do |issue|
-        # VSO users should not be able to see decision issues until the issue decision date
-        Time.now.utc > issue.caseflow_decision_date
+    if FeatureToggle.enabled?(:restrict_poa_visibility, user: @user)
+      if @user.vso_employee?
+        visible_issues = @appeal.decision_issues.select do |issue|
+          # VSO users should not be able to see decision issues until the issue decision date
+          Time.now.utc > issue.caseflow_decision_date
+        end
+        modified_issues = visible_issues.map do |issue|
+          # VSO users should not be able to see decision issues' descriptions, regardless of the issue decision date
+          issue.description = nil
+          issue
+        end
+        modified_issues
+      else
+        @appeal.decision_issues
       end
-      modified_issues = visible_issues.map do |issue|
-        # VSO users should not be able to see decision issues' descriptions, regardless of the issue decision date
-        issue.description = nil
-        issue
-      end
-      modified_issues
     else
       @appeal.decision_issues
     end
--- spec/policies/appeal_decision_issues_policy_spec.rb
+++ spec/policies/appeal_decision_issues_policy_spec.rb
@@ -4,58 +4,76 @@ describe AppealDecisionIssuesPolicy, :postgres do
   describe "#visible_decision_issues" do
     subject { AppealDecisionIssuesPolicy.new(user: user, appeal: appeal).visible_decision_issues }
 
-    context "when user has VSO role" do
-      let(:user) { create(:user, :vso_role) }
-      let(:result) { subject }
+    context "when the restrict poa visibility feature toggle is turned on" do
+      before { FeatureToggle.enable!(:restrict_poa_visibility) }
+      after { FeatureToggle.disable!(:restrict_poa_visibility) }
+      context "when user has VSO role" do
+        let(:user) { create(:user, :vso_role) }
+        let(:result) { subject }
 
-      context "when a decision issue has not yet reached its decision date" do
-        let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
+        context "when a decision issue has not yet reached its decision date" do
+          let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
 
-        it "cannot be seen" do
-          expect(subject).to be_empty
+          it "cannot be seen" do
+            expect(subject).to be_empty
+          end
+        end
+
+        context "when a decision date has reached or passed its decision date" do
+          let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
+
+          it "can be seen" do
+            expect(result[0].id).to eq(appeal.decision_issues[0].id)
+            expect(result[1].id).to eq(appeal.decision_issues[1].id)
+          end
+
+          it "its issue description is not visible" do
+            expect(result[0].description).to be_nil
+          end
         end
       end
 
-      context "when a decision date has reached or passed its decision date" do
-        let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
+      context "when user has no VSO role" do
+        let(:user) { create(:user) }
 
-        it "can be seen" do
-          expect(result[0].id).to eq(appeal.decision_issues[0].id)
-          expect(result[1].id).to eq(appeal.decision_issues[1].id)
+        context "when a decision issue has not yet reached its decision date" do
+          let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
+          let(:result) { subject }
+
+          it "can be seen" do
+            expect(result[0].id).to eq(appeal.decision_issues[0].id)
+          end
+
+          it "its issue description is visible" do
+            expect(result[0].description).to eq(appeal.decision_issues[0].description)
+          end
         end
 
-        it "its issue description is not visible" do
-          expect(result[0].description).to be_nil
+        context "when a decision date has reached or passed its decision date" do
+          let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
+          let(:result) { subject }
+
+          it "can be seen" do
+            expect(result[1].id).to eq(appeal.decision_issues[1].id)
+          end
+
+          it "its issue description is visible" do
+            expect(result[0].description).to eq(appeal.decision_issues[0].description)
+          end
         end
       end
     end
 
-    context "when user has no VSO role" do
-      let(:user) { create(:user) }
+    context "when the restrict poa visibility feature toggle is not turned on" do
+      context "when user has VSO role" do
+        let(:user) { create(:user, :vso_role) }
 
-      context "when a decision issue has not yet reached its decision date" do
-        let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
-        let(:result) { subject }
+        context "when a decision issue has not yet reached its decision date" do
+          let(:appeal) { create(:appeal, :decision_issue_with_future_date) }
 
-        it "can be seen" do
-          expect(result[0].id).to eq(appeal.decision_issues[0].id)
-        end
-
-        it "its issue description is visible" do
-          expect(result[0].description).to eq(appeal.decision_issues[0].description)
-        end
-      end
-
-      context "when a decision date has reached or passed its decision date" do
-        let(:appeal) { create(:appeal, :dispatched_with_decision_issue) }
-        let(:result) { subject }
-
-        it "can be seen" do
-          expect(result[1].id).to eq(appeal.decision_issues[1].id)
-        end
-
-        it "its issue description is visible" do
-          expect(result[0].description).to eq(appeal.decision_issues[0].description)
+          it "can be seen" do
+            expect(subject).to match_array(appeal.decision_issues)
+          end
         end
       end
     end
