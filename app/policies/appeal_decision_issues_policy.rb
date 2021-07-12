376605bd93c0285dbebf2da7e73a62fc770c21d0
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-07-12 15:18:56 -0400

    refactor appeal decision issues policy

--- app/policies/appeal_decision_issues_policy.rb
+++ app/policies/appeal_decision_issues_policy.rb
@@ -8,20 +8,26 @@ class AppealDecisionIssuesPolicy
 
   def visible_decision_issues
     if FeatureToggle.enabled?(:restrict_poa_visibility, user: @user)
-      if @user.vso_employee?
-        visible_issues = @appeal.decision_issues.select do |issue|
-          # VSO users should not be able to see decision issues until the issue decision date
-          Time.now.utc > issue.caseflow_decision_date
-        end
-        modified_issues = visible_issues.map do |issue|
-          # VSO users should not be able to see decision issues' descriptions, regardless of the issue decision date
-          issue.description = nil
-          issue
-        end
-        modified_issues
-      else
-        @appeal.decision_issues
+      restricted_decision_issues
+    else
+      @appeal.decision_issues
+    end
+  end
+
+  private
+
+  def restricted_decision_issues
+    if @user.vso_employee?
+      visible_issues = @appeal.decision_issues.select do |issue|
+        # VSO users should not be able to see decision issues until the issue decision date
+        Time.now.utc > issue.caseflow_decision_date
       end
+      modified_issues = visible_issues.map do |issue|
+        # VSO users should not be able to see decision issues' descriptions, regardless of the issue decision date
+        issue.description = nil
+        issue
+      end
+      modified_issues
     else
       @appeal.decision_issues
     end
