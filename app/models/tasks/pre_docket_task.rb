1c1a01a168244479baaf958cb2ef81f5a80755d7
Alexander <ajspottswood@gmail.com>
2021-07-13 15:15:55 -0400

    code climate

--- app/models/tasks/pre_docket_task.rb
+++ app/models/tasks/pre_docket_task.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ##
-# Task that will serve as the parent task for the entirety of the pre docket workflow. This task will be assigned to 
+# Task that will serve as the parent task for the entirety of the pre docket workflow. This task will be assigned to
 # the Bva organization and remain "on hold" until the AssessDocumentationTask is completed.
 
 class PreDocketTask < Task
