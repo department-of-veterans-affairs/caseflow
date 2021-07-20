9659cca632524335050c723068d790a3484072e5
yoomlam <Dung.Lam1@va.gov>
2021-07-20 10:40:37 -0500

    update test

--- spec/jobs/incomplete_distributions_job_spec.rb
+++ spec/jobs/incomplete_distributions_job_spec.rb
@@ -11,28 +11,36 @@ describe IncompleteDistributionsJob, :postgres do
   context ".perform" do
     subject { described_class.perform_now }
 
+    let(:judge) do
+      create(:user, css_id: "MYNAMEISJUDGE").tap do |judge|
+        allow(judge).to receive(:judge_in_vacols?) { true }
+      end
+    end
+    let!(:distribution) { Distribution.create!(judge: judge) }
+
     it "detects pending Distribution" do
-      judge = create(:user, css_id: "MYNAMEISJUDGE")
-      allow(judge).to receive(:judge_in_vacols?) { true }
-      distribution = Distribution.create!(judge: judge)
       Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
         subject
 
-        # we'll get error status and that's ok. it's because we don't bother to set up appeals first.
-        expect(distribution.reload).to be_error
         message = "Restarting jobs for pending Distributions: [#{distribution.id}]"
         expect(slack_service).to have_received(:send_notification).with(message).once
       end
     end
 
     it "detects stalled Distribution" do
-      judge = create(:user, css_id: "MYNAMEISJUDGE")
-      allow(judge).to receive(:judge_in_vacols?) { true }
-      distribution = Distribution.create!(judge: judge)
       distribution.started_at = Time.zone.now
       distribution.started!
       Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
-        binding.pry
+        subject
+
+        message = "Restarting jobs for stalled Distributions: [#{distribution.id}]"
+        expect(slack_service).to have_received(:send_notification).with(message).once
+      end
+    end
+
+    it "detects stalled Distribution without started_at" do
+      distribution.started!
+      Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
         subject
 
         message = "Restarting jobs for stalled Distributions: [#{distribution.id}]"
@@ -42,9 +50,6 @@ describe IncompleteDistributionsJob, :postgres do
 
     context "there are no incomplete Distributions" do
       it "doesn't send a slack message" do
-        judge = create(:user, css_id: "MYNAMEISJUDGE")
-        allow(judge).to receive(:judge_in_vacols?) { true }
-        Distribution.create!(judge: judge)
         Timecop.travel(Time.zone.now + 30.minutes) do
           subject
 
