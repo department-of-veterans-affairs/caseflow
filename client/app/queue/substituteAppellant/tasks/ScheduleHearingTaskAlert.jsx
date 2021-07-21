86152e7ce5788da05dfd9001c87f391da0aca53a
pbradn <paula.bradin@va.gov>
2021-07-21 09:30:40 -0400

    Style changes for the alert to shrink font and shift icon lower

--- client/app/queue/substituteAppellant/tasks/ScheduleHearingTaskAlert.jsx
+++ client/app/queue/substituteAppellant/tasks/ScheduleHearingTaskAlert.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import { css } from 'glamor';
 import Alert from 'app/components/Alert';
 import { SUBSTITUTE_APPELLANT_SCHEDULE_HEARING_TASK_TEXT } from 'app/../COPY';
+
+const textStyling = css({
+  fontSize: '1.7rem',
+  lineHeight: '1',
+});
+
+const alert = <div {...textStyling}>{SUBSTITUTE_APPELLANT_SCHEDULE_HEARING_TASK_TEXT}</div>;
 
 export const ScheduleHearingTaskAlert = (props) => (
   <Alert
     type="info"
-    message={SUBSTITUTE_APPELLANT_SCHEDULE_HEARING_TASK_TEXT}
+    message={alert}
     {...props}
   />
 );
