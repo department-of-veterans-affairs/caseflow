2a1e57be0696f9279cedb985c26d54f8dd8e272a
rubaiyat22 <Rubaiyat.Rashid@va.gov>
2021-07-23 10:29:22 -0400

    remove indices from sent_hearing_email_events

--- db/migrate/20210712200047_add_indices_to_sent_hearing_email_events.rb
+++
@@ -1,8 +0,0 @@
-class AddIndicesToSentHearingEmailEvents < Caseflow::Migration
-  def change
-    safety_assured do
-      add_safe_index :sent_hearing_email_events, [:recipient_role]
-      add_safe_index :sent_hearing_email_events, [:email_type]
-    end
-  end
-end
