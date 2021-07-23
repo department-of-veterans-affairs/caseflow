194684658ae0cf61b1c91d437d4d59a62a18172f
Rubaiyat Rashid <Rubaiyat.Rashid@va.gov>
2021-07-23 10:53:14 -0400

    Write new tests (#16528)
    * write new tests
    
    * fix flaky
    
    * modify tests based on some changes in logic with role
    
    * small change
    
    * attempt to fix flaky
    
    * another attempt
    
    * use within 1 second to test for reminder_sent_at
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomas Apodaca <Thomas.Apodaca@va.gov>
    
    * verfiy non existence of recipients
    
    * add tests for coverage (#16536)
    
    Co-authored-by: Tomas Apodaca <Thomas.Apodaca@va.gov>

--- spec/factories/hearing_email_recipient.rb
+++ spec/factories/hearing_email_recipient.rb
@@ -2,5 +2,30 @@
 
 FactoryBot.define do
   factory :hearing_email_recipient do
+    hearing
+    type { HearingEmailRecipient.name }
+    email_address { "test@email.com" }
+    email_sent { false }
+    timezone { nil }
+
+    trait :initialized do
+      email_sent { true }
+      timezone { "America/New_York" }
+    end
+
+    trait :appellant_hearing_email_recipient do
+      initialize_with { AppellantHearingEmailRecipient.new(attributes) }
+      type { AppellantHearingEmailRecipient.name }
+    end
+
+    trait :representative_hearing_email_recipient do
+      initialize_with { RepresentativeHearingEmailRecipient.new(attributes) }
+      type { RepresentativeHearingEmailRecipient.name }
+    end
+
+    trait :judge_hearing_email_recipient do
+      initialize_with { JudgeHearingEmailRecipient.new(attributes) }
+      type { JudgeHearingEmailRecipient.name }
+    end
   end
 end
