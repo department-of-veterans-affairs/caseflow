a6569e9f8d9a3e2f785ad85f5c75f67c09a2c0b0
Will Drougas <wdrougas@tistatech.com>
2021-07-27 17:21:26 -0400

    migration failing

--- db/migrate/20210727210019_validate_attorney_case_review_f_ks.rb
+++ db/migrate/20210727210019_validate_attorney_case_review_f_ks.rb
@@ -1,6 +1,6 @@
 class ValidateAttorneyCaseReviewFKs < Caseflow::Migration
   def change
     validate_foreign_key "attorney_case_reviews", "attorney_id"
-    validate_foreign_key "attorney_case_reviews", "judge_id"
+    validate_foreign_key "attorney_case_reviews", "reviewing_judge_id"
   end
 end
