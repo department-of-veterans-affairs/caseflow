d9dbc7ef5e8f9573ca103518fd359d1ca8d6ef25
Will Drougas <wdrougas@tistatech.com>
2021-07-27 14:51:48 -0400

    updated change

--- db/migrate/20210726142344_add_attorney_case_review_f_ks.rb
+++ db/migrate/20210726142344_add_attorney_case_review_f_ks.rb
@@ -2,6 +2,5 @@ class AddAttorneyCaseReviewFKs < Caseflow::Migration
   def change
     add_foreign_key "attorney_case_reviews", "users", column: "attorney_id", validate: false
     add_foreign_key "attorney_case_reviews", "users", column: "reviewing_judge_id", validate: false
-    add_foreign_key "attorney_case_reviews", "tasks", validate: false
   end
 end
