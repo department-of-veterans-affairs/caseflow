5da8f7e19ffb64ab9a7a334f8ba4c91d82b24fad
karinamzalez <karinamzalez@gmail.com>
2021-07-28 11:06:04 -0500

    TabPanel: add tabPanelTabIndex prop and tabIndexVal

--- client/app/components/tabs/TabPanel.jsx
+++ client/app/components/tabs/TabPanel.jsx
@@ -12,6 +12,7 @@ const propTypes = {
   children: PropTypes.node.isRequired,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   fullWidth: PropTypes.bool,
+  tabPanelTabIndex: PropTypes.bool
 };
 
 export const TabPanel = ({
@@ -19,6 +20,7 @@ export const TabPanel = ({
   className = '',
   children,
   value,
+  tabPanelTabIndex
 }) => {
   const ctx = useContext(TabContext);
   const active = ctx.value === value.toString();
@@ -36,13 +38,22 @@ export const TabPanel = ({
     }
   }, [active, ctx.mountOnEnter, ctx.unmountOnExit, children]);
 
+  const tabIndexVal = () => {
+    if (tabPanelTabIndex) {
+      // prop passed from TestUsers -- this TabIndex val accounts for JAWs accessibility quirk
+      return -1;
+    }
+
+    return active ? 0 : -1;
+  };
+
   return (
     <Component
       role="tabpanel"
       id={`${ctx.idPrefix}-tabpanel-${value}`}
       aria-hidden={!active}
       className={classNames}
-      tabIndex={-1}
+      tabIndex={tabIndexVal}
     >
       {contents}
     </Component>
