0eb134ab89e8777759ab80608bde157fd74db0b4
rubaiyat22 <Rubaiyat.Rashid@va.gov>
2021-07-28 17:03:34 -0400

    lint fixes

--- client/app/hearings/components/DocketStartTimes.jsx
+++ client/app/hearings/components/DocketStartTimes.jsx
@@ -5,32 +5,32 @@ import { shortZoneName } from '../utils';
 
 import { RadioField } from '../../components/RadioField';
 
-export const DocketStartTimes = (props) => {
-  const {
-    hearingStartTime,
-    setSlotCount,
-    setHearingStartTime,
-    roTimezone
-  } = props;
-
-  const fullDayAmPm = null
-  const halfDayAm = '8:30'
-  const halfDayPm = '12:30'
+export const DocketStartTimes = ({
+  hearingStartTime,
+  setSlotCount,
+  setHearingStartTime,
+  roTimezone
+}) => {
+  const fullDayAmPm = null;
+  const halfDayAm = '8:30';
+  const halfDayPm = '12:30';
 
   const options = () => {
     const amTimeInEastern =
-      moment(moment.tz(halfDayAm, 'h:mm A', roTimezone)).tz('America/New_York').format('h:mm A');
+      moment(moment.tz(halfDayAm, 'h:mm A', roTimezone)).tz('America/New_York').
+        format('h:mm A');
     const pmTimeInEastern =
-      moment(moment.tz(halfDayPm, 'h:mm A', roTimezone)).tz('America/New_York').format('h:mm A');
+      moment(moment.tz(halfDayPm, 'h:mm A', roTimezone)).tz('America/New_York').
+        format('h:mm A');
 
-    const zoneName = shortZoneName(roTimezone)
-    const fullDayAmPmLabel = `Full-Day AM & PM (10 slots at ${halfDayAm} AM & ${halfDayPm} PM ${zoneName})`
-    let halfDayAmLabel = `Half-Day AM (5 slots at ${halfDayAm} AM ${name} / ${amTimeInEastern} Eastern)`
-    let halfDayPmLabel = `Half-Day PM (5 slots at ${halfDayPm} PM ${name} / ${pmTimeInEastern} Eastern)`
+    const zoneName = shortZoneName(roTimezone);
+    const fullDayAmPmLabel = `Full-Day AM & PM (10 slots at ${halfDayAm} AM & ${halfDayPm} PM ${zoneName})`;
+    let halfDayAmLabel = `Half-Day AM (5 slots at ${halfDayAm} AM ${name} / ${amTimeInEastern} Eastern)`;
+    let halfDayPmLabel = `Half-Day PM (5 slots at ${halfDayPm} PM ${name} / ${pmTimeInEastern} Eastern)`;
 
     if (zoneName === 'Eastern') {
-      halfDayAmLabel = `Half-Day AM (5 slots at ${amTimeInEastern} ${zoneName})`
-      halfDayPmLabel = `Half-Day PM (5 slots at ${pmTimeInEastern} ${zoneName})`
+      halfDayAmLabel = `Half-Day AM (5 slots at ${amTimeInEastern} ${zoneName})`;
+      halfDayPmLabel = `Half-Day PM (5 slots at ${pmTimeInEastern} ${zoneName})`;
     }
 
     return [
@@ -40,31 +40,35 @@ export const DocketStartTimes = (props) => {
         value: halfDayAm },
       { displayText: halfDayPmLabel,
         value: halfDayPm }
-    ]
-  }
+    ];
+  };
 
-  const docketTypeSlotCount = {
+  const startTimesSlotCount = {
     [fullDayAmPm]: 10,
     [halfDayAm]: 5,
     [halfDayPm]: 5
-  }
+  };
 
-  const handleOnChange = (value) => {
-    value === fullDayAmPm ? setHearingStartTime(null) : setHearingStartTime(value)
+  const onChange = (value) => {
+    if (value === fullDayAmPm) {
+      setHearingStartTime(null);
+    } else {
+      setHearingStartTime(value);
+    }
 
-    setSlotCount(docketTypeSlotCount[value] ?? null)
-  }
+    setSlotCount(startTimesSlotCount[value] ?? null);
+  };
 
   return (
     <RadioField
-      name={'docketAvailableTimes'}
-      label={'Available Times'}
+      name="docketStartTimes"
+      label="Available Times"
       strongLabel
       options={options()}
-      onChange={(value) => handleOnChange(value)}
+      onChange={(value) => onChange(value)}
       value={hearingStartTime}
     />
-  )
+  );
 };
 
 DocketStartTimes.propTypes = {
