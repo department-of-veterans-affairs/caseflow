14f7af94764241cde4b3fdc00b93eaacedeef571
Yang Yang <yangyang@navapbc.com>
2021-07-29 12:34:06 -0400

    Add extra context to Sentry event for VBMS upload jobs

--- app/jobs/upload_document_to_vbms_job.rb
+++ app/jobs/upload_document_to_vbms_job.rb
@@ -8,7 +8,19 @@ class UploadDocumentToVbmsJob < CaseflowJob
     RequestStore.store[:application] = "idt"
     RequestStore.store[:current_user] = User.system_user
 
-    document = VbmsUploadedDocument.find_by(id: document_id)
+    @document = VbmsUploadedDocument.find_by(id: document_id)
+    add_extra_context_to_sentry
     UploadDocumentToVbms.new(document: document).call
+  end
+
+  private
+
+  attr_reader :document
+
+  def add_extra_context_to_sentry
+    Raven.extra_context(
+      vbms_uploaded_document_id: document.id,
+      case_details_path: "/queue/appeals/#{document.appeal.external_id}"
+    )
   end
 end
--- spec/jobs/upload_document_to_vbms_job_spec.rb
+++ spec/jobs/upload_document_to_vbms_job_spec.rb
@@ -2,14 +2,16 @@
 
 describe UploadDocumentToVbmsJob, :postgres do
   describe ".perform" do
+    let(:document) { create(:vbms_uploaded_document) }
+    let(:service) { instance_double(UploadDocumentToVbms) }
+
+    subject { UploadDocumentToVbmsJob.perform_now(document_id: document.id) }
+
     it "calls #call on UploadDocumentToVbms instance" do
-      document = create(:vbms_uploaded_document)
-
-      service = instance_double(UploadDocumentToVbms)
       expect(UploadDocumentToVbms).to receive(:new).with(document: document).and_return(service)
+      expect(Raven).to receive(:extra_context)
       expect(service).to receive(:call)
-
-      UploadDocumentToVbmsJob.perform_now(document_id: document.id)
+      subject
     end
   end
 end
