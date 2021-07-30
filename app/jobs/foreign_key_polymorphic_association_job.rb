4b295bcea40de48a79815f60c984f7f0bc5509c2
Will Drougas <wdrougas@tistatech.com>
2021-07-30 13:51:30 -0400

    start collecting records

--- app/jobs/foreign_key_polymorphic_association_job.rb
+++ app/jobs/foreign_key_polymorphic_association_job.rb
@@ -10,10 +10,10 @@ class ForeignKeyPolymporphicAssociationJob < CaseflowJob
   end
 
   def find_orphaned_records
-    sent_hearing_email_events = SentHearingEmailEvent.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|she| she.hearing == nil}
-    hearing_email_recipients = HearingEmailRecipient.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|her| her.hearing == nil}
-    special_issue_lists = SpecialIssueList.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|sil| sil.appeal == nil}
-    tasks = Task.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|t| t.appeal == nil}
-    vbms_uploaded_documents = VbmsUploadedDocument.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|vbms| vbms.appeal == nil}
+    # sent_hearing_email_events = SentHearingEmailEvent.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|she| she.hearing == nil}
+    # hearing_email_recipients = HearingEmailRecipient.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|her| her.hearing == nil}
+    # special_issue_lists = SpecialIssueList.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|sil| sil.appeal == nil}
+    # tasks = Task.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|t| t.appeal == nil}
+    # vbms_uploaded_documents = VbmsUploadedDocument.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|vbms| vbms.appeal == nil}
   end
 end
