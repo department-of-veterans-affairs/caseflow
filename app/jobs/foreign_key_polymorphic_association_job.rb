77ffa2573b62221a283f1e624bc0923dd41898e1
Will Drougas <wdrougas@tistatech.com>
2021-07-30 13:18:08 -0400

    job update

--- app/jobs/foreign_key_polymorphic_association_job.rb
+++ app/jobs/foreign_key_polymorphic_association_job.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-# This job exists to act as a foreign key for polymorphic associations
 class ForeignKeyPolymporphicAssociationJob < CaseflowJob
   queue_with_priority :low_priority
+
+  def perform
+    RequestStore.store[:current_user] = User.system_user
+ 
+    find_orphaned_records
+  end
+
+  def find_orphaned_records
+    sent_hearing_email_events = SentHearingEmailEvent.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|she| she.hearing == nil}
+    hearing_email_recipients = HearingEmailRecipient.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|her| her.hearing == nil}
+    special_issue_lists = SpecialIssueList.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|sil| sil.appeal == nil}
+    tasks = Task.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|t| t.appeal == nil}
+    vbms_uploaded_documents = VbmsUploadedDocument.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|vbms| vbms.appeal == nil}
+  end
 end
