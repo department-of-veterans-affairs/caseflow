2e11eb4990bba93db517425a8396acaaec8ba5c2
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-08-02 18:03:25 -0400

    test of invalid JudgeAssignTask status

--- spec/fixes/investigate_duplicate_judge_tasks_spec.rb
+++ spec/fixes/investigate_duplicate_judge_tasks_spec.rb
@@ -1,24 +1,27 @@
-
+# frozen_string_literal: true
 
 feature "duplicate JudgeAssignTask investigation" do
   before do
-    User.authenticate!(css_id: "BVAAABSHIRE")
+    User.authenticate!(user: judge_user)
   end
 
   # Ticket: https://github.com/department-of-veterans-affairs/dsva-vacols/issues/212#
   # Target state: there should be no more than 1 open JudgeAssignTask
   describe "Judge reassigns JudgeAssignTask in first tab and complete task in second tab" do
-    let(:judge) { create(:user) }
-    let!(:vacols_judge) { create(:staff, :judge_role, sdomainid: judge.css_id) }
-    let(:judge_team) { JudgeTeam.create_for_judge(judge) }
-  
-    let(:attorney) { create(:user, :with_vacols_attorney_record) }
-    let(:appeal) { create(:appeal, :at_attorney_drafting, associated_judge: judge, associated_attorney: attorney) }
+    let(:judge_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Anna Juarez") }
+    let!(:judge_staff) { create(:staff, :judge_role, user: judge_user) }
+    let(:judge_team) { JudgeTeam.create_for_judge(judge_user) }
+    let(:attorney_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Steven Ahr") }
+    let!(:attorney_staff) { create(:staff, :attorney_role, user: attorney_user) }
+
+    let(:root_task) { create(:root_task) }
+    let(:judge_assign_task) { create(:ama_judge_assign_task, parent: root_task, assigned_to: judge_user) }
+    let(:appeal) { judge_assign_task.appeal }
+    let(:uuid) { appeal.uuid }
 
     scenario "Caseflow creates multiple JudgeDecisionReview and JudgeAssign tasks" do
       # open a window and visit case details page, window A
       visit "/queue/appeals/#{appeal.uuid}"
-      binding.pry
 
       # open a second window and visit case details page, window B
       second_window = open_new_window
@@ -26,22 +29,27 @@ feature "duplicate JudgeAssignTask investigation" do
         visit "/queue/appeals/#{appeal.uuid}"
       end
 
-      # in window A, reassign the JudgeAssignTask to another judge 
-      # window A: expect message - task reassigned
+      # in window A, reassign the JudgeAssignTask to another judge
+      first_judge_assign_task_id = appeal.tasks.select { |task| task.type == "JudgeAssignTask" }[0].id
       click_dropdown(prompt: "Select an action", text: "Re-assign to a judge")
-      expect(page).to have_content(COPY::REASSIGN_TASK_SUCCESS_MESSAGE)
-      first_judge_assign_task = appeal.tasks.select{ |task| task.type == "JudgeAssignTask" }[0]
-      # test that JudgeAssignTask is cancelled
-      expect(first_judge_assign_task.status).to eq("cancelled")
+      click_dropdown(prompt: "Select a user", text: judge_user.full_name)
+      fill_in "taskInstructions", with: "reassign this task! teamwork makes the dreamwork!"
+      click_on "Submit"
+
+      expect(page).to have_content(COPY::REASSIGN_TASK_SUCCESS_MESSAGE, judge_user.full_name)
+      expect(Task.find(first_judge_assign_task_id).status).to eq("cancelled")
 
       within_window second_window do
-        # in window B, complete the JudgeAssignTask
-        # window B: expect message - attorney now has task
         click_dropdown(prompt: "Select an action", text: "Assign to attorney")
-        expect(page).to have_content(COPY::ASSIGN_TASK_SUCCESS_MESSAGE)
+        click_dropdown(prompt: "Select a user", text: "Other")
+        click_dropdown(prompt: "Select a user", text: attorney_user.full_name)
+        fill_in "taskInstructions", with: "assign to attorney"
+        click_on "Submit"
+
+        expect(page).to have_content(COPY::ASSIGN_TASK_SUCCESS_MESSAGE, attorney_user.full_name)
 
         # below will fail before fix is made - app currently allows the invalid flow of a JudgeAssignTask going from cancelled to complete
-        expect(first_judge_assign_task.status).to eq("cancelled")
+        # expect(Task.find(first_judge_assign_task_id).status).to eq("cancelled")
       end
 
       appeal.reload
