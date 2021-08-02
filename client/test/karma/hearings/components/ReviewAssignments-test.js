387d754adb4002f9d7fc9178d6a00eb9afdba62f
Stephen Halliburton <stephen.halliburton@va.gov>
2021-08-02 11:09:05 -0700

    Updated import for karma test

--- client/test/karma/hearings/components/ReviewAssignments-test.js
+++ client/test/karma/hearings/components/ReviewAssignments-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
-import ReviewAssignments from '../../../../app/hearings/components/ReviewAssignments';
+import { ReviewAssignments } from '../../../../app/hearings/components/ReviewAssignments';
 
 describe('ReviewAssignments', () => {
   it('renders the RO/CO alert', () => {
