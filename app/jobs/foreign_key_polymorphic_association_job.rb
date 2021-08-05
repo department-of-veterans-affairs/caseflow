3baac4e7679925e30addf24496b76e2cc76ff6c4
yoomlam <Dung.Lam1@va.gov>
2021-08-05 09:24:06 -0500

    after pairing

--- app/jobs/foreign_key_polymorphic_association_job.rb
+++ app/jobs/foreign_key_polymorphic_association_job.rb
@@ -4,13 +4,28 @@ class ForeignKeyPolymorphicAssociationJob < CaseflowJob
   queue_with_priority :low_priority
 
   CLASSES_WITH_POLYMORPH_ASSOC = {
-    SpecialIssueList => { association_id_column: :appeal_id, association_method: :appeal },
-    Task => { association_id_column: :appeal_id, association_method: :appeal },
-    VbmsUploadedDocument => { association_id_column: :appeal_id, association_method: :appeal }
+    Claimant => { association_id_column: :participant_id,
+                  association_type_column: nil,
+                  association_method: :person },
+    # Claimant => { association_id_column: :decision_review_id,
+    #               association_type_column: :decision_review_type,
+    #               association_method: :decision_review },
+    HearingEmailRecipient => { association_id_column: :hearing_id,
+                               association_type_column: :hearing_type,
+                               association_method: :hearing },
+    SentHearingEmailEvent => { association_id_column: :hearing_id,
+                               association_type_column: :hearing_type,
+                               association_method: :hearing },
+    SpecialIssueList => { association_id_column: :appeal_id,
+                          association_type_column: :appeal_type,
+                          association_method: :appeal },
+    Task => { association_id_column: :appeal_id,
+              association_type_column: :appeal_type,
+              association_method: :appeal },
+    VbmsUploadedDocument => { association_id_column: :appeal_id,
+                              association_type_column: :appeal_type,
+                              association_method: :appeal }
   }.freeze
-  # SentHearingEmailEvent.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|she| she.hearing == nil}
-  # HearingEmailRecipient.unscoped.includes(:hearing).where.not(hearing_id: nil).select{|her| her.hearing == nil}
-  # VbmsUploadedDocument.unscoped.includes(:appeal).where.not(appeal_id: nil).select{|vbms| vbms.appeal == nil}
 
   def perform
     # RequestStore.store[:current_user] = User.system_user
@@ -23,10 +38,23 @@ class ForeignKeyPolymorphicAssociationJob < CaseflowJob
   def find_orphaned_records(klass)
     association_id_column = CLASSES_WITH_POLYMORPH_ASSOC[klass][:association_id_column]
     association_method = CLASSES_WITH_POLYMORPH_ASSOC[klass][:association_method]
+
+    # TODO: Change/optimize this query to address claimant.person not returning nil after person.destroy!
     orphan_records = klass.unscoped.includes(association_method).where.not(association_id_column => nil)
       .select { |rec| rec.send(association_method).nil? }
     if orphan_records.any?
       slack_service.send_notification("Found #{klass.name} orphaned record: #{orphan_records.map(&:id)}")
+    end
+
+    association_type_column = CLASSES_WITH_POLYMORPH_ASSOC[klass][:association_type_column]
+    if association_type_column
+      unusual_records = klass.unscoped.includes(association_method).where(association_id_column => nil)
+        .where.not(association_type_column => nil)
+        .select { |rec| rec.send(association_method).nil? }
+      if unusual_records.any?
+        slack_service.send_notification("Found #{klass.name} record with nil #{association_id_column} " \
+          "but non-nil #{association_type_column}: #{unusual_records.map(&:id)}")
+      end
     end
   end
 end
--- spec/jobs/foreign_key_polymorphic_association_job_spec.rb
+++ spec/jobs/foreign_key_polymorphic_association_job_spec.rb
@@ -14,13 +14,14 @@ describe ForeignKeyPolymorphicAssociationJob, :postgres do
   let!(:sil) { SpecialIssueList.create(appeal: appeal) }
   # let(:task) { create(:distribution_task) }
 
-  context "_id is nil" do
+  context "_id is nil regardless of existence of associated record" do
     before do
       sil.update_column(:appeal_id, nil)
-      appeal.destroy!
+      sil.update_column(:appeal_type, nil)
+      appeal.destroy! if [true, false].sample
     end
     it "does not send alert" do
-      expect(Appeal.count).to eq 0
+      expect(Appeal.count).to eq(0).or eq(1)
       subject
       expect(slack_service).not_to have_received(:send_notification)
     end
@@ -34,6 +35,19 @@ describe ForeignKeyPolymorphicAssociationJob, :postgres do
   end
 
   context "_id is nil but _type is non-nil" do
+    before do
+      sil.update_column(:appeal_id, nil)
+      appeal.destroy! if [true, false].sample
+    end
+    it "sends alert" do
+      expect(Appeal.count).to eq(0).or eq(1)
+      expect(sil.appeal_type).not_to eq nil
+
+      subject
+
+      message = "Found SpecialIssueList record with nil appeal_id but non-nil appeal_type: [#{sil.id}]"
+      expect(slack_service).to have_received(:send_notification).with(message).once
+    end
   end
 
   # This is the main objective of the job
@@ -73,6 +87,32 @@ describe ForeignKeyPolymorphicAssociationJob, :postgres do
         message = /Found .* orphaned record/
         expect(slack_service).to have_received(:send_notification).with(message).twice
       end
+    end
+  end
+
+  context "when checking Claimant.participant_id foreign key" do
+    let(:claimant) { appeal.claimant }
+    context "associated Person exists" do
+      it "does not send alert" do
+        expect(claimant.person).not_to eq nil
+        expect(Person.find_by_participant_id(claimant.participant_id)).not_to eq nil
+        subject
+        expect(slack_service).not_to have_received(:send_notification)
+      end
+    end
+    context "associated Person does not exist" do
+      before do
+        claimant.person.destroy!
+      end
+      # it "sends alert" do
+      #   expect(Person.find_by_participant_id(claimant.participant_id)).to eq nil
+      #   binding.pry
+
+      #   subject
+
+      #   message = "Found SpecialIssueList record with nil appeal_id but non-nil appeal_type: [#{sil.id}]"
+      #   expect(slack_service).to have_received(:send_notification).with(message).once
+      # end
     end
   end
 end
