2922a655be779f75ac4e5aa02797fa01cc505169
Tomas Apodaca <Thomas.Apodaca@va.gov>
2021-08-06 01:42:14 -0700

    not using REQUEST_TYPE_LABELS anymore

--- client/app/hearings/components/ReviewAssignments.jsx
+++ client/app/hearings/components/ReviewAssignments.jsx
@@ -11,7 +11,7 @@ import Modal from '../../components/Modal';
 import Table from '../../components/Table';
 import StatusMessage from '../../components/StatusMessage';
 import { formatDateStr } from '../../util/DateUtil';
-import { SPREADSHEET_TYPES, REQUEST_TYPE_LABELS } from '../constants';
+import { SPREADSHEET_TYPES } from '../constants';
 
 const tableStyling = css({
   '& > thead > tr > th': { backgroundColor: '#f1f1f1' },
