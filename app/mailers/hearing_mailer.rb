d7c6a5910e814a605601c270c8a92ff152456704
rubaiyat22 <Rubaiyat.Rashid@va.gov>
2021-08-06 16:55:39 -0400

    modify location_changed copy and update rake

--- app/mailers/hearing_mailer.rb
+++ app/mailers/hearing_mailer.rb
@@ -14,12 +14,13 @@ class HearingMailer < ActionMailer::Base
 
     @recipient = email_recipient
     @virtual_hearing = virtual_hearing
+    @hearing = virtual_hearing&.hearing
 
     attachments[calendar_invite_name] = cancellation_calendar_invite
 
     mail(
       to: recipient.email,
-      subject: "Your Board hearing location has changed – Do Not Reply"
+      subject: cancellation_subject
     )
   end
 
@@ -70,6 +71,15 @@ class HearingMailer < ActionMailer::Base
   private
 
   attr_reader :recipient, :virtual_hearing
+
+  def cancellation_subject
+    case recipient.title
+    when HearingEmailRecipient::RECIPIENT_TITLES[:appellant]
+      "Your Board hearing will be held at #{@hearing&.hearing_location_or_regional_office&.name}"
+    when HearingEmailRecipient::RECIPIENT_TITLES[:representative]
+      "#{appellant_name}’s hearing will be held at #{@hearing&.hearing_location_or_regional_office&.name}"
+    end
+  end
 
   def confirmation_calendar_invite
     Hearings::CalendarService.confirmation_calendar_invite(virtual_hearing, recipient, link)
