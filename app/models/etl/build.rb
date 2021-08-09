5d17ecdfe02bc99d96501da3c98792da888e17e2
yoomlam <Dung.Lam1@va.gov>
2021-08-09 08:23:22 -0500

    set default for ETL::Build

--- app/models/etl/build.rb
+++ app/models/etl/build.rb
@@ -13,6 +13,9 @@ class ETL::Build < ETL::Record
 
   has_many :etl_build_tables, class_name: "ETL::BuildTable", foreign_key: "etl_build_id"
 
+  attribute :started_at, default: -> { Time.zone.now }
+  attribute :status, default: -> { :running }
+
   def tables
     etl_build_tables.pluck(:table_name).compact
   end
