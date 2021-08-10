3fc0fb9ca2ff82672ab846b4bb9d36b399e9f70a
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-08-10 12:06:16 -0400

    debug spec to actually reassign judge task to a different judge

--- spec/fixes/investigate_duplicate_judge_tasks_spec.rb
+++ spec/fixes/investigate_duplicate_judge_tasks_spec.rb
@@ -11,6 +11,12 @@ feature "duplicate JudgeAssignTask investigation" do
     let(:judge_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Anna Juarez") }
     let!(:judge_staff) { create(:staff, :judge_role, user: judge_user) }
     let(:judge_team) { JudgeTeam.create_for_judge(judge_user) }
+    let(:judge_user_second) do
+      create(:user, station_id: User::BOARD_STATION_ID, css_id: "BVAAABSHIRE",
+                    full_name: "Aaron Judge_HearingsAndCases Abshire")
+    end
+    let!(:judge_staff_second) { create(:staff, :judge_role, user: judge_user_second) }
+
     let(:attorney_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Steven Ahr") }
     let!(:attorney_staff) { create(:staff, :attorney_role, user: attorney_user) }
     let(:attorney_user_second) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "John Lee") }
@@ -23,26 +29,26 @@ feature "duplicate JudgeAssignTask investigation" do
 
     scenario "Caseflow creates multiple JudgeDecisionReview and JudgeAssign tasks" do
       # open a window and visit case details page, window A
-      visit "/queue/appeals/#{appeal.uuid}"
+      visit "/queue/appeals/#{uuid}"
       appeal.reload.treee
 
       # open a second window and visit case details page, window B
       second_window = open_new_window
       within_window second_window do
-        visit "/queue/appeals/#{appeal.uuid}"
+        visit "/queue/appeals/#{uuid}"
       end
 
       # in window A, reassign the JudgeAssignTask to another judge
       first_judge_assign_task_id = appeal.tasks.select { |task| task.type == "JudgeAssignTask" }[0].id
       click_dropdown(prompt: "Select an action", text: "Re-assign to a judge")
-      click_dropdown(prompt: "Select a user", text: judge_user.full_name)
+      click_dropdown(prompt: "Select a user", text: judge_user_second.full_name)
       fill_in "taskInstructions", with: "reassign this task! teamwork makes the dreamwork!"
       click_on "Submit"
       appeal.reload.treee
 
-      expect(page).to have_content(COPY::REASSIGN_TASK_SUCCESS_MESSAGE, judge_user.full_name)
+      expect(page).to have_content(COPY::REASSIGN_TASK_SUCCESS_MESSAGE, judge_user_second.full_name)
       expect(Task.find(first_judge_assign_task_id).status).to eq("cancelled")
-      visit "/queue/appeals/#{appeal.uuid}"
+      visit "/queue/appeals/#{uuid}"
 
       # in window B, complete the JudgeAssignTask
       within_window second_window do
@@ -55,28 +61,21 @@ feature "duplicate JudgeAssignTask investigation" do
 
         expect(page).to have_content(COPY::ASSIGN_TASK_SUCCESS_MESSAGE, attorney_user.full_name)
 
-        visit "/queue/appeals/#{appeal.uuid}"
+        visit "/queue/appeals/#{uuid}"
         # Complete JudgeAssignTask to create an invalid number of AttorneyTasks
-        # select the second task action dropdown
-        assign_task_row = find("table[summary='layout table'] tr:nth-child(3)")
-        # select the "assign to attorney" dropdown because there were two dropdowns with this option
-        click_dropdown({text: "Assign to attorney"}, assign_task_row)
+        click_dropdown(prompt: "Select an action", text: "Assign to attorney")
         click_dropdown(prompt: "Select a user", text: "Other")
         click_dropdown(prompt: "Select a user", text: attorney_user_second.full_name)
         fill_in "taskInstructions", with: "mimic incorrect flow"
         click_on "Submit"
         appeal.reload.treee
 
-        # below will fail before fix is made
-        # app currently allows the invalid flow of a JudgeAssignTask going from cancelled to complete
+        # app currently allows the invalid flow of a JudgeAssignTask going from cancelled to completed
         expect(Task.find(first_judge_assign_task_id).status).to eq("completed")
       end
 
-      visit "/queue/appeals/#{appeal.uuid}"
-      # see what dropdowns are there
-
-      # TODO: test that appeal has multiple JudgeDecisionReviewTasks
-      # TODO: test that appeal has multiple AttorneyTasks
+      visit "/queue/appeals/#{uuid}"
+      appeal.reload.treee
     end
   end
 end
