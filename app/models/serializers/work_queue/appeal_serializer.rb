068d67499e476526c0532a3c23ab339f0a8a9105
karinamzalez <karinamzalez@gmail.com>
2021-08-10 12:22:07 -0500

    serializer: remove otherclaimant check from suffix

--- app/models/serializers/work_queue/appeal_serializer.rb
+++ app/models/serializers/work_queue/appeal_serializer.rb
@@ -90,7 +90,7 @@ class WorkQueue::AppealSerializer
   end
 
   attribute :appellant_suffix do |object|
-    object.claimant.is_a?(OtherClaimant) ? object.claimant&.suffix : nil
+    object.claimant&.suffix
   end
 
   attribute :appellant_date_of_birth do |object|
