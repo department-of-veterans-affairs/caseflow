e7288b3fd94dcddcc3da4d3526456c6d58b10ebf
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-08-10 17:04:35 -0400

    add wait statements

--- spec/fixes/investigate_duplicate_judge_tasks_spec.rb
+++ spec/fixes/investigate_duplicate_judge_tasks_spec.rb
@@ -6,8 +6,8 @@ feature "duplicate JudgeAssignTask investigation" do
   end
 
   # Ticket: https://github.com/department-of-veterans-affairs/dsva-vacols/issues/212#
-  # Target state: there should be no more than 1 open JudgeAssignTask
-  describe "Judge reassigns JudgeAssignTask in first tab and complete task in second tab" do
+  # Target state: JudgeAssignTask should not change status from cancelled to completed
+  describe "Judge reassigns JudgeAssignTask in first tab and completes the same task in second tab" do
     let(:judge_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Anna Juarez") }
     let!(:judge_staff) { create(:staff, :judge_role, user: judge_user) }
     let(:judge_team) { JudgeTeam.create_for_judge(judge_user) }
@@ -30,12 +30,14 @@ feature "duplicate JudgeAssignTask investigation" do
     scenario "Caseflow creates multiple JudgeDecisionReview and JudgeAssign tasks" do
       # open a window and visit case details page, window A
       visit "/queue/appeals/#{uuid}"
+      expect(page).to have_content(appeal.veteran.first_name, wait: 30)
       appeal.reload.treee
 
       # open a second window and visit case details page, window B
       second_window = open_new_window
       within_window second_window do
         visit "/queue/appeals/#{uuid}"
+        expect(page).to have_content(appeal.veteran.first_name, wait: 30)
       end
 
       # in window A, reassign the JudgeAssignTask to another judge
@@ -44,6 +46,7 @@ feature "duplicate JudgeAssignTask investigation" do
       click_dropdown(prompt: "Select a user", text: judge_user_second.full_name)
       fill_in "taskInstructions", with: "reassign this task! teamwork makes the dreamwork!"
       click_on "Submit"
+      expect(page).to have_content(appeal.veteran.first_name, wait: 30)
       appeal.reload.treee
 
       expect(page).to have_content(COPY::REASSIGN_TASK_SUCCESS_MESSAGE, judge_user_second.full_name)
@@ -68,6 +71,7 @@ feature "duplicate JudgeAssignTask investigation" do
         click_dropdown(prompt: "Select a user", text: attorney_user_second.full_name)
         fill_in "taskInstructions", with: "mimic incorrect flow"
         click_on "Submit"
+        expect(page).to have_content(appeal.veteran.first_name, wait: 30)
         appeal.reload.treee
 
         # app currently allows the invalid flow of a JudgeAssignTask going from cancelled to completed
