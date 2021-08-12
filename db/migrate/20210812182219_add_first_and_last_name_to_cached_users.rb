187a3cd24df0f8ad0d74916797fb94bd198e1ddb
odonnadieu <87207304+odonnadieu@users.noreply.github.com>
2021-08-12 16:38:36 -0400

    Update db/migrate/20210812182219_add_first_and_last_name_to_cached_users.rb

--- db/migrate/20210812182219_add_first_and_last_name_to_cached_users.rb
+++ db/migrate/20210812182219_add_first_and_last_name_to_cached_users.rb
@@ -1,7 +1,6 @@
 class AddFirstAndLastNameToCachedUsers < Caseflow::Migration
-  # These columns store the User's first and last name values coming from VACOLS
   def change
-    add_column :cached_user_attributes, :snamef, :string
-    add_column :cached_user_attributes, :snamel, :string
+    add_column :cached_user_attributes, :snamef, :string, comment: "User's First Name in VACOLS"
+    add_column :cached_user_attributes, :snamel, :string, comment: "User's Last Name in VACOLS"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -280,8 +280,8 @@ ActiveRecord::Schema.define(version: 2021_08_12_182219) do
     t.string "sdomainid", null: false
     t.string "slogid", null: false
     t.string "smemgrp", limit: 8
-    t.string "snamef"
-    t.string "snamel"
+    t.string "snamef", comment: "User's First Name in VACOLS"
+    t.string "snamel", comment: "User's Last Name in VACOLS"
     t.string "stafkey", null: false
     t.string "stitle", limit: 16
     t.string "svlj"
@@ -1441,7 +1441,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_182219) do
     t.index ["date", "task_type"], name: "index_team_quotas_on_date_and_task_type", unique: true
     t.index ["updated_at"], name: "index_team_quotas_on_updated_at"
   end
-  
+
   create_table "transcriptions", force: :cascade do |t|
     t.datetime "created_at", comment: "Automatic timestamp of when transcription was created"
     t.date "expected_return_date", comment: "Expected date when transcription would be returned by the transcriber"
