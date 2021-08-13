deb91e9077bf96298ff04154acb7e7f48fc51d99
Will Drougas <wdrougas@tistatech.com>
2021-08-13 09:01:39 -0400

    update if else

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -585,7 +585,7 @@ class Fakes::BGSService
   # rubocop:disable Metrics/MethodLength
   def find_all_relationships(participant_id:)
     if %w[444444444 555555555].include?(participant_id)
-      nil
+      []
     else
       [
         {
