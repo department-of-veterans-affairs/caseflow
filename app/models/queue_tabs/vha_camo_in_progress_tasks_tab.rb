d9491cfdcf1ea15b76395794ccbd1ce2f1884e1e
Matt Wagner <mattwagner@navapbc.com>
2021-08-13 19:44:07 -0400

    Fix typo in COPY.json constant

--- app/models/queue_tabs/vha_camo_in_progress_tasks_tab.rb
+++ app/models/queue_tabs/vha_camo_in_progress_tasks_tab.rb
@@ -6,7 +6,7 @@ class VhaCamoInProgressTasksTab < QueueTab
   attr_accessor :show_reader_link_column, :allow_bulk_assign
 
   def label
-    COPY::ORGANIZATIONAL_QUEUE_PAGE_IN_PROGESS_TAB_TITLE
+    COPY::ORGANIZATIONAL_QUEUE_PAGE_IN_PROGRESS_TAB_TITLE
   end
 
   def self.tab_name
