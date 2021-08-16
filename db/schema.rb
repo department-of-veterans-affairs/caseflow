c2a02ca5fa73f1a8e687872eff574752a11fe11c
yoomlam <Dung.Lam1@va.gov>
2021-08-16 08:27:38 -0500

    adjust migration date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_122051) do
+ActiveRecord::Schema.define(version: 2021_07_29_180404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
