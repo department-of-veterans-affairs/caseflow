39234884655caab0c11b5f75567cbe8f8919426d
yoomlam <Dung.Lam1@va.gov>
2021-08-17 09:10:01 -0500

    send Slack msg to #appeals-data-workgroup as well

--- app/jobs/foreign_key_polymorphic_association_job.rb
+++ app/jobs/foreign_key_polymorphic_association_job.rb
@@ -58,6 +58,8 @@ class ForeignKeyPolymorphicAssociationJob < CaseflowJob
         #{record_ids.map(&:to_s).join('\n')}
     MSG
     slack_service.send_notification(message)
+    # Also send a message to #appeals-data-workgroup
+    slack_service.send_notification(slack_msg, klass, "#appeals-data-workgroup")
   end
 
   # Maps the includes_method to a hash containing all the possible types. Each hash entry is:
