c86d5078925db383a7f4d0eca8631a48c37a014d
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-08-17 14:20:12 -0400

    investigate feature test bug

--- spec/fixes/investigate_duplicate_judge_tasks_spec.rb
+++ spec/fixes/investigate_duplicate_judge_tasks_spec.rb
@@ -2,15 +2,15 @@
 
 feature "duplicate JudgeAssignTask investigation" do
   before do
-    User.authenticate!(user: judge_user)
+    User.authenticate!(css_id: "PETERSBVAM")
   end
 
   # Ticket: https://github.com/department-of-veterans-affairs/dsva-vacols/issues/212#
   # Desired Target state: JudgeAssignTask should not change status from cancelled to completed
   describe "Judge reassigns JudgeAssignTask in first tab and completes the same task in second tab" do
-    let(:judge_user) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Anna Juarez") }
-    let!(:judge_staff) { create(:staff, :judge_role, user: judge_user) }
-    let(:judge_team) { JudgeTeam.create_for_judge(judge_user) }
+    let(:judge_user) { User.find_by_css_id("PETERSBVAM") }
+    let(:judge_role) { create(:staff, :judge_role, user: judge_user) }
+
     let(:judge_user_second) do
       create(:user, station_id: User::BOARD_STATION_ID, css_id: "BVAAABSHIRE",
                     full_name: "Aaron Judge_HearingsAndCases Abshire")
@@ -21,7 +21,16 @@ feature "duplicate JudgeAssignTask investigation" do
     let!(:attorney_staff) { create(:staff, :attorney_role, user: attorney_user) }
     let(:attorney_user_second) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "John Lee") }
     let!(:attorney_staff_second) { create(:staff, :attorney_role, user: attorney_user_second) }
-
+    let(:attorney_user_third) { User.find_by_css_id("BVAEERDMAN") }
+    let(:attorney_user_fourth) { create(:user, station_id: User::BOARD_STATION_ID, full_name: "Jia James") }
+    let!(:attorney_staff_fourth) { create(:staff, :attorney_role, user: attorney_user_fourth) }
+    let(:judge_team) do
+      JudgeTeam.create_for_judge(judge_user).tap do |team|
+        team.add_user(attorney_user)
+        team.add_user(attorney_user_second)
+        team.add_user(attorney_user_third)
+      end
+    end
     let(:root_task) { create(:root_task) }
     let(:judge_assign_task) { create(:ama_judge_assign_task, parent: root_task, assigned_to: judge_user) }
     let(:appeal) { judge_assign_task.appeal }
@@ -44,6 +53,7 @@ feature "duplicate JudgeAssignTask investigation" do
       first_judge_assign_task_id = appeal.tasks.select { |task| task.type == "JudgeAssignTask" }[0].id
       click_dropdown(prompt: "Select an action", text: "Re-assign to a judge")
       click_dropdown(prompt: "Select a user", text: judge_user_second.full_name)
+
       fill_in "taskInstructions", with: "reassign this task! teamwork makes the dreamwork!"
       click_on "Submit"
       expect(page).to have_content(appeal.veteran.first_name, wait: 30)
@@ -57,6 +67,7 @@ feature "duplicate JudgeAssignTask investigation" do
       within_window second_window do
         click_dropdown(prompt: "Select an action", text: "Assign to attorney")
         click_dropdown(prompt: "Select a user", text: "Other")
+        # Below breaks because attorneys don't load
         click_dropdown(prompt: "Select a user", text: attorney_user.full_name)
         fill_in "taskInstructions", with: "assign to attorney"
         click_on "Submit"
