465331ced114415dfefc71fd1f0ec2f38a398b87
yoomlam <Dung.Lam1@va.gov>
2021-08-18 14:45:00 -0500

    lint

--- app/models/etl/decision_document.rb
+++ app/models/etl/decision_document.rb
@@ -6,6 +6,10 @@ class ETL::DecisionDocument < ETL::Record
   class << self
     private
 
+    # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/PerceivedComplexity
     def merge_original_attributes_to_target(original, target)
       target.attributes = original.attributes.reject { |key| %w[created_at updated_at].include?(key) }
       target.decision_document_created_at = original.created_at
@@ -14,11 +18,7 @@ class ETL::DecisionDocument < ETL::Record
       appeal = original.appeal
       target.docket_number = appeal.docket_number
 
-      attorney_case_review = if appeal.is_a?(Appeal)
-                              appeal.latest_attorney_case_review
-                            else
-                              appeal.attorney_case_review
-                            end
+      attorney_case_review = appeal.is_a?(Appeal) ? appeal.latest_attorney_case_review : appeal.attorney_case_review
       target.attorney_case_review_id = attorney_case_review&.id || 0
       target.attorney_user_id = attorney_case_review&.attorney_id
 
@@ -32,12 +32,24 @@ class ETL::DecisionDocument < ETL::Record
       else
         judge_case_review = appeal.judge_case_review
         target.judge_case_review_id = judge_case_review&.id || 0
-        target.judge_user_id = judge_case_review&.judge_id || appeal.reviewing_judge.try(:assigned_by)&.assigned_by_user_id
+        target.judge_user_id = judge_case_review&.judge_id ||
+                               appeal.reviewing_judge.try(:assigned_by)&.assigned_by_user_id
+
+        target.judge_user_id ||= attorney_case_review&.reviewing_judge_id
+        target.attorney_user_id ||= judge_case_review&.attorney_id
+        if judge_case_review && attorney_case_review
+          check_equal(original.id, "judge_user_id", judge_case_review.judge_id, attorney_case_review.reviewing_judge_id)
+          check_equal(original.id, "attorney_user_id", judge_case_review.attorney_id, attorney_case_review.attorney_id)
+        end
 
         # To-do: appeal.vacols_case_review is also available
       end
 
       target
     end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocop:enable Metrics/PerceivedComplexity
   end
 end
