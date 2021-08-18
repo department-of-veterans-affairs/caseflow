7ac5d7522febf726f29121ecb915ba646fe0cc70
Meckila Britt <meckilabritt@navapbc.com>
2021-08-18 11:04:51 -0700

    Resolve lint issue

--- client/app/test/TestUsers.jsx
+++ client/app/test/TestUsers.jsx
@@ -129,10 +129,10 @@ export default function TestUsers(props) {
     tab.page = <div>
       <ul>
         {Object.keys(app.links).map((name) => {
-            let readableName = StringUtil.snakeCaseToCapitalized(name);
+          let readableName = StringUtil.snakeCaseToCapitalized(name);
 
-            return <li key={name} aria-labelledby={name}>
-              <a href={app.links[name]} id={name} role="link" aria-label={readableName}>{readableName}</a>
+          return <li key={name} aria-labelledby={name}>
+            <a href={app.links[name]} id={name} role="link" aria-label={readableName}>{readableName}</a>
           </li>;
         })}
       </ul>
