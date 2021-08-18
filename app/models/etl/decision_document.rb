d6c56c2b4c658883622f3fbc5fabf9de673e5070
yoomlam <Dung.Lam1@va.gov>
2021-08-18 14:24:31 -0500

    use judge_case_review.judge_id for legacy appeals

--- app/models/etl/decision_document.rb
+++ app/models/etl/decision_document.rb
@@ -26,12 +26,15 @@ class ETL::DecisionDocument < ETL::Record
         judge_case_review = appeal.latest_judge_case_review
         target.judge_case_review_id = judge_case_review.id
 
-        target.judge_user_id = judge_case_review.judge.id
+        target.judge_user_id = judge_case_review.judge_id
         check_equal(original.id, "reviewing_judge_name",
                     appeal.reviewing_judge_name, judge_case_review.judge.full_name)
       else
-        target.judge_case_review_id = appeal.judge_case_review&.id || 0
-        target.judge_user_id = appeal.reviewing_judge.try(:assigned_by)&.assigned_by_user_id
+        judge_case_review = appeal.judge_case_review
+        target.judge_case_review_id = judge_case_review&.id || 0
+        target.judge_user_id = judge_case_review&.judge_id || appeal.reviewing_judge.try(:assigned_by)&.assigned_by_user_id
+
+        # To-do: appeal.vacols_case_review is also available
       end
 
       target
