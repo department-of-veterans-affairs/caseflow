ac5891b15331847bd2da0f845c15e75948896e80
nava <barshirtcliff@navapbc.com>
2021-08-19 16:20:37 -0700

    snag ama-only judge factory from other branch

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -45,6 +45,15 @@ FactoryBot.define do
       roles { ["Hearing Prep"] }
     end
 
+    trait :ama_only_judge do
+      after(:create) do |judge|
+        JudgeTeam.for_judge(judge)&.update(ama_only_push: true, ama_only_request: true) ||
+          JudgeTeam.create_for_judge(judge, ama_only_push: true, ama_only_request: true)
+      end
+
+      roles { ["Hearing Prep"] }
+    end
+
     trait :with_vacols_judge_record do
       after(:create) do |user|
         create(:staff, :judge_role, user: user)
