40e4207767dd26b9ac1cc5f9a2ee54c54a70be19
nava <barshirtcliff@navapbc.com>
2021-08-19 15:37:28 -0700

    schema comments

--- db/etl/schema.rb
+++ db/etl/schema.rb
@@ -404,8 +404,8 @@ ActiveRecord::Schema.define(version: 2021_08_18_172716) do
 
   create_table "organizations", comment: "Copy of Organizations table", force: :cascade do |t|
     t.boolean "accepts_priority_pushed_cases", comment: "Whether a JudgeTeam currently accepts distribution of automatically pushed priority cases"
-    t.boolean "ama_only_push", default: false
-    t.boolean "ama_only_request", default: false
+    t.boolean "ama_only_push", default: false, comment: "whether a JudgeTeam should only get AMA appeals during the PushPriorityAppealsToJudgesJob"
+    t.boolean "ama_only_request", default: false, comment: "whether a JudgeTeam should only get AMA appeals when requesting more cases"
     t.datetime "created_at"
     t.string "name"
     t.string "participant_id", comment: "Organizations BGS partipant id"
