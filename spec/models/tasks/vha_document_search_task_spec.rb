31bca3ec25b319166220ea9a598e599198d6b0e8
Sally Maki <Sally.Maki@va.gov>
2021-08-19 03:26:42 -0700

    Remove accidental insertion

--- spec/models/tasks/vha_document_search_task_spec.rb
+++ spec/models/tasks/vha_document_search_task_spec.rb
@@ -18,17 +18,4 @@ describe VhaDocumentSearchTask, :postgres do
 
     it { is_expected.to eq [] }
   end
-
-  describe "#ui_hash" do
-    let(:veteran) { create(:veteran) }
-    let(:appeal) { create(:appeal, veteran_file_number: veteran.file_number) }
-    let(:non_comp_org) { create(:business_line, name: "Non-Comp Org", url: "nco") }
-    let(:task) do
-      create(:veteran_record_request_task, appeal: appeal, assigned_to: non_comp_org)
-    end
-
-    it "renders JSON" do
-      expect(task.ui_hash[:type]).to eq("Record Request")
-    end
-  end
 end
