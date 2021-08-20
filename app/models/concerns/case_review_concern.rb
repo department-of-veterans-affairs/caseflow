76de20abebf172a96411edf9878609973a09ea6f
yoomlam <Dung.Lam1@va.gov>
2021-08-20 11:05:43 -0500

    see what breaks

--- app/models/concerns/case_review_concern.rb
+++ app/models/concerns/case_review_concern.rb
@@ -3,19 +3,23 @@
 module CaseReviewConcern
   extend ActiveSupport::Concern
 
+  # belongs_to :appeal, polymorphic: true
+
   attr_accessor :issues
 
   included do
     validates :task, presence: true, unless: :legacy?
   end
 
-  def appeal
+  def appeal_through_task
     @appeal ||= if legacy?
                   LegacyAppeal.find_or_create_by(vacols_id: vacols_id)
                 else
                   Task.find(task_id).appeal
                 end
   end
+
+  private
 
   def legacy?
     (task_id =~ LegacyTask::TASK_ID_REGEX) ? true : false
