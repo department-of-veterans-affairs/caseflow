e6595be15a203a340f718b551d319e9837af119d
yoomlam <Dung.Lam1@va.gov>
2021-08-20 16:46:03 -0500

    remove irrelevant comment

--- db/migrate/20210820153715_add_appeal_association_to_case_reviews.rb
+++ db/migrate/20210820153715_add_appeal_association_to_case_reviews.rb
@@ -6,7 +6,6 @@ class AddAppealAssociationToCaseReviews < Caseflow::Migration
     add_column :judge_case_reviews, :appeal_id, :bigint, comment: "The ID of the appeal this case review is associated with"
     add_column :judge_case_reviews, :appeal_type, :string, comment: "The type of appeal this case review is associated with"
 
-    # Adding index separately as strong_migrations suggests
     add_safe_index :attorney_case_reviews, [:appeal_type, :appeal_id], algorithm: :concurrently
     add_safe_index :judge_case_reviews, [:appeal_type, :appeal_id], algorithm: :concurrently
   end
