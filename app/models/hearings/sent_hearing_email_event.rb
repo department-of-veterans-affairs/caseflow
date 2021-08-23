6e9714396000807d7b4d5e8cf50eeeb04c0a266b
Omar Donnadieu <omercado@tistatech.com>
2021-08-23 16:35:59 -0400

    WIP

--- app/models/hearings/sent_hearing_email_event.rb
+++ app/models/hearings/sent_hearing_email_event.rb
@@ -32,6 +32,18 @@ class SentHearingEmailEvent < CaseflowRecord
     }
   ), _prefix: :is
 
+  # error to capture any instances where we try to update `sent_status`
+  # with an invalid value.
+  class InvalidReportedStatus < StandardError; end
+
+  # error to capture any instances we attempt to send email to a coordinator
+  # regarding email status but `sent_status_email_external_message_id` exists.
+  class SentStatusEmailAlreadySent < StandardError; end
+
+  EMAIL_REPORTED_SENT_STATUSES = %w[
+    new sending sent failed inconclusive blacklisted canceled
+  ].freeze
+
   def sent_to_role
     case recipient_role
     when "judge"
@@ -51,9 +63,74 @@ class SentHearingEmailEvent < CaseflowRecord
     end
   end
 
+  def handle_reported_status(reported_status)
+    return if sent_status
+
+    update!(sent_status_checked_at: Datetime.now)
+
+    if !reported_status_valid?(reported_status)
+      invalid_reported_status_failure(reported_status)
+    elsif reported_status_sent?(reported_status)
+      update!(sent_status: true)
+    elsif reported_status_failed?(reported_status)
+      update!(sent_status: false)
+
+      handle_failed_email_status(reported_status)
+    else
+      update!(sent_status: nil)
+    end
+  end
+
   private
 
   def assign_sent_at_time
     self.sent_at ||= Time.now.utc
+  end
+
+  def reported_status_valid?(reported_status)
+    EMAIL_REPORTED_SENT_STATUSES.include?(reported_status)
+  end
+
+  def reported_status_sent?(reported_status)
+    reported_status == "sent"
+  end
+
+  def reported_status_sending?(reported_status)
+    reported_status == "sending"
+  end
+
+  def reported_status_new?(reported_status)
+    reported_status == "new"
+  end
+
+  def reported_status_failed?(reported_status)
+    %w[failed inconclusive canceled blacklisted].include?(reported_status)
+  end
+
+  def invalid_reported_status_failure(reported_status)
+    Raven.capture_exception(
+      InvalidReportedStatus.new(
+        "Cannot update sent_status with invalid status: (#{reported_status})"
+      )
+    )
+  end
+
+  def email_already_sent_failure
+    Raven.capture_exception(
+      SentStatusEmailAlreadySent.new(
+        "Cannot sent an email to because sent_status_email_external_message_id: " \
+          "(#{sent_status_email_external_message_id}) already exists."
+      )
+    )
+  end
+
+  def handle_failed_email_status(reported_status)
+    if sent_status_email_external_message_id.present?
+      email_already_sent_failure
+    else
+      HearingEmailStatusMailer.new(
+        sent_event: self, reported_status: reported_status
+      ).call
+    end
   end
 end
