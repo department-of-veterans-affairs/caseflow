fee9d5f89f74082c530c26ecacab8a5eccd8d39b
Stephen Halliburton <stephen.halliburton@va.gov>
2021-08-25 11:50:11 -0700

    Finished removing unused files

--- app/services/external_api/gov_delivery_service/create_response.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-class ExternalApi::GovDeliveryService::CreateResponse < ExternalApi::GovDeliveryService::Response; end
--- app/services/external_api/gov_delivery_service/delete_response.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-class ExternalApi::GovDeliveryService::DeleteResponse < ExternalApi::GovDeliveryService::Response; end
