748e13d8f35635dc85065806a68614e2114bd3a4
Stephen Halliburton <stephen.halliburton@va.gov>
2021-08-25 11:46:47 -0700

    Added method to retrieve status of email delivery

--- lib/fakes/gov_delivery_service.rb
+++ lib/fakes/gov_delivery_service.rb
@@ -1,31 +1,7 @@
 # frozen_string_literal: true
 
 class Fakes::GovDeliveryService
-  def initialize(**args)
-    @status_code = args[:status_code]
-  end
-
-  def create_webhook(*)
-    return ExternalApi::GovDeliveryService::CreateResponse.new(HTTPI::Response.new(@status_code, {}, {})) if error?
-
-    ExternalApi::GovDeliveryService::CreateResponse.new(HTTPI::Response.new(200, {}, {}))
-  end
-
-  def delete_webhook(*)
-    return ExternalApi::GovDeliveryService::DeleteResponse.new(HTTPI::Response.new(@status_code, {}, {})) if error?
-
-    ExternalApi::GovDeliveryService::DeleteResponse.new(HTTPI::Response.new(200, {}, {}))
-  end
-
-  def list_all_webhooks(*)
-    return ExternalApi::GovDeliveryService::Response.new(HTTPI::Response.new(@status_code, {}, {})) if error?
-
+  def self.get_message_by_event(*)
     ExternalApi::GovDeliveryService::Response.new(HTTPI::Response.new(200, {}, {}))
-  end
-
-  private
-
-  def error?
-    [401, 403, 404, 500, 502, 503].include? @status_code
   end
 end
--- spec/services/external_api/gov_delivery_service_spec.rb
+++ spec/services/external_api/gov_delivery_service_spec.rb
@@ -11,17 +11,82 @@ describe ExternalApi::GovDeliveryService do
     stub_const("ENV", "SSL_CERT_FILE" => cert_file_location)
   end
 
-  let(:gov_delivery_service) { ExternalApi::GovDeliveryService.new }
-  let(:email_endpoint) { "/messages/email" }
-  let(:webhook_endpoint) { "/webhooks" }
-  let(:webhook_id) { "456" }
+  let(:hearing_date) { Time.zone.now }
+  let(:hearing_day_request_type) { HearingDay::REQUEST_TYPES[:video] }
+  let(:hearing_day_ro) { "RO01" }
+  let(:hearing_day) do
+    create(
+      :hearing_day,
+      request_type: hearing_day_request_type,
+      regional_office: hearing_day_ro,
+      scheduled_for: hearing_date
+    )
+  end
+  let(:hearing) do
+    create(
+      :hearing,
+      hearing_day: hearing_day,
+      created_at: Time.zone.now - 14.days
+    )
+  end
+  let!(:appellant_recipient) do
+    create(
+      :hearing_email_recipient,
+      :appellant_hearing_email_recipient,
+      hearing: hearing,
+      timezone: "America/New_York"
+    )
+  end
+  let(:email_event) do
+    create(
+      :sent_hearing_email_event,
+      :reminder,
+      recipient_role: HearingEmailRecipient::RECIPIENT_ROLES[:veteran],
+      email_recipient: hearing.appellant_recipient
+    )
+  end
+  let(:endpoint) { "#{email_event.external_message_id}/recipients" }
 
-  shared_examples "error responses" do
-    describe "response failure" do
+  describe "#get_message_status" do
+    let(:event_status) { "sent" }
+
+    subject { ExternalApi::GovDeliveryService.get_message_by_event(email_event: email_event) }
+
+    let(:success_create_resp) do
+      HTTPI::Response.new(200, {}, { "status" => event_status }.to_json)
+    end
+
+    it "calls #send_gov_delivery_request" do
+      expect(ExternalApi::GovDeliveryService).to receive(:send_gov_delivery_request)
+      subject
+    end
+
+    it "passed correct arguments to #send_gov_delivery_request" do
+      expect(ExternalApi::GovDeliveryService).to receive(:send_gov_delivery_request).with(endpoint, :get)
+      subject
+    end
+
+    it "returns an instance of ExternalApi::GovDeliveryService::CreateResponse class" do
+      allow(ExternalApi::GovDeliveryService).to receive(:send_gov_delivery_request).with(endpoint, :get)
+        .and_return(success_create_resp)
+
+      expect(subject).to be_instance_of(ExternalApi::GovDeliveryService::Response)
+    end
+
+    it "success response" do
+      allow(ExternalApi::GovDeliveryService).to receive(:send_gov_delivery_request).with(endpoint, :get)
+        .and_return(success_create_resp)
+
+      expect(subject.code).to eq(200)
+      expect(subject.success?).to eq(true)
+      expect(subject.body.dig("status")).to eq(event_status)
+    end
+
+    context "response failure" do
       let!(:error_code) { nil }
 
       before(:each) do
-        allow(gov_delivery_service).to receive(:send_gov_delivery_request)
+        allow(ExternalApi::GovDeliveryService).to receive(:send_gov_delivery_request)
           .and_return(HTTPI::Response.new(error_code, {}, {}.to_json))
       end
 
@@ -79,116 +144,5 @@ describe ExternalApi::GovDeliveryService do
         end
       end
     end
-  end
-
-  describe "#create_webhook" do
-    let(:external_message_id) { "1234" }
-    let(:body) do
-      {
-        "url": "",
-        "event_type": "sent"
-      }
-    end
-
-    subject { gov_delivery_service.create_webhook(external_message_id: external_message_id, event_type: "sent") }
-
-    let(:success_create_resp) do
-      HTTPI::Response.new(200, {}, { "_links" => { "self" => webhook_id } }.to_json)
-    end
-
-    it "calls #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request)
-      subject
-    end
-
-    it "passed correct arguments to #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :post, body: body)
-      subject
-    end
-
-    it "returns an instance of ExternalApi::GovDeliveryService::CreateResponse class" do
-      allow(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :post, body: body)
-        .and_return(success_create_resp)
-
-      expect(subject).to be_instance_of(ExternalApi::GovDeliveryService::CreateResponse)
-    end
-
-    it "success response" do
-      allow(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :post, body: body)
-        .and_return(success_create_resp)
-
-      expect(subject.code).to eq(200)
-      expect(subject.success?).to eq(true)
-      expect(subject.body.dig("_links", "self")).to eq(webhook_id)
-    end
-
-    include_examples "error responses"
-  end
-
-  describe "#delete_webhook" do
-    subject { gov_delivery_service.delete_webhook(webhook_id: webhook_id) }
-
-    let(:success_del_resp) { HTTPI::Response.new(200, {}, {}) }
-
-    it "calls #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request)
-      subject
-    end
-
-    it "passed correct arguments to #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request)
-        .with("#{webhook_endpoint}/#{webhook_id}", :delete)
-      subject
-    end
-
-    it "success response" do
-      allow(gov_delivery_service).to receive(:send_gov_delivery_request)
-        .with("#{webhook_endpoint}/#{webhook_id}", :delete)
-        .and_return(success_del_resp)
-
-      expect(subject.code).to eq(200)
-      expect(subject.success?).to eq(true)
-      expect(subject.data).to eq(nil)
-    end
-
-    include_examples "error responses"
-  end
-
-  describe "#list_all_webhooks" do
-    let(:webhooks_list) { [["failed", "/webhooks/12345"], ["inconclusive", "/webhooks/23456"]] }
-
-    subject { gov_delivery_service.list_all_webhooks }
-
-    let(:success_create_resp) do
-      HTTPI::Response.new(200, {}, { "_links" => { "self" => webhooks_list } }.to_json)
-    end
-
-    it "calls #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request)
-      subject
-    end
-
-    it "passed correct arguments to #send_gov_delivery_request" do
-      expect(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :get)
-      subject
-    end
-
-    it "returns an instance of ExternalApi::GovDeliveryService::CreateResponse class" do
-      allow(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :get)
-        .and_return(success_create_resp)
-
-      expect(subject).to be_instance_of(ExternalApi::GovDeliveryService::Response)
-    end
-
-    it "success response" do
-      allow(gov_delivery_service).to receive(:send_gov_delivery_request).with(webhook_endpoint, :get)
-        .and_return(success_create_resp)
-
-      expect(subject.code).to eq(200)
-      expect(subject.success?).to eq(true)
-      expect(subject.body.dig("_links", "self")).to eq(webhooks_list)
-    end
-
-    include_examples "error responses"
   end
 end
