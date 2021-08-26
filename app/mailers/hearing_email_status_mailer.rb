54150d29aef82edb84647c7947caafd54d457f5b
charleyf <Charles.Ferguson@va.gov>
2021-08-26 16:37:58 -0400

    Keep all information extraction in the mailer, add new methods to helpers for later use

--- app/mailers/hearing_email_status_mailer.rb
+++ app/mailers/hearing_email_status_mailer.rb
@@ -4,13 +4,19 @@ class HearingEmailStatusMailer < ActionMailer::Base
   default from: "Board of Veterans' Appeals <BoardofVeteransAppealsHearings@messages.va.gov>"
   layout "hearing_email_status_mailer"
   helper VirtualHearings::LinkHelper
-  helper Hearings::AppellantNameHelper
+  include Hearings::AppellantNameHelper
+  include Hearings::CalendarTemplateHelper
 
   def notification(sent_hearing_email_event:)
     @sent_hearing_email_event = sent_hearing_email_event
 
-    # Make it easier for templates to access hearing information
+    # Extract each piece of data the templates use
     @hearing = @sent_hearing_email_event.hearing
+    @hearing_type = Constants::HEARING_REQUEST_TYPES.key(@hearing.request_type).titleize
+    @email_type = @sent_hearing_email_event.email_type.downcase
+    @email_address = @sent_hearing_email_event.email_address
+    @veteran_name = formatted_appellant_name(@hearing.appeal)
+    @hearing_date = Hearings::CalendarTemplateHelper.hearing_date_only(@hearing)
 
     mail(
       to: @sent_hearing_email_event.email_address,
@@ -19,10 +25,6 @@ class HearingEmailStatusMailer < ActionMailer::Base
   end
 
   def notification_subject
-    hearing_type = Constants::HEARING_REQUEST_TYPES.key(@hearing.request_type).titleize
-    email_type = @sent_hearing_email_event.email_type.downcase
-    email_address = @sent_hearing_email_event.email_address
-
-    "#{hearing_type} #{email_type} email failed to send to #{email_address}"
+    "#{@hearing_type} #{@email_type} email failed to send to #{@email_address}"
   end
 end
