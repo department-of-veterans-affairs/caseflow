04c35aa3dc94ba3f63b8b1080693eb324b62f17b
Nik Weinmeister <nweinmeister@tistatech.com>
2021-08-26 08:56:15 -0400

    Prevent update of POA if POA already exists

--- app/controllers/unrecognized_appellants_controller.rb
+++ app/controllers/unrecognized_appellants_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UnrecognizedAppellantsController < ApplicationController
+  before_action :find_unrecognized_appellant, only: [:update_power_of_attorney]
+  before_action :prevent_update_if_poa_exists, only: [:update_power_of_attorney]
+
   def update
     unrecognized_appellant = UnrecognizedAppellant.find(params[:id])
     if unrecognized_appellant.update_with_versioning!(unrecognized_appellant_params, current_user)
@@ -11,15 +14,22 @@ class UnrecognizedAppellantsController < ApplicationController
   end
 
   def update_power_of_attorney
-    unrecognized_appellant = UnrecognizedAppellant.find(params[:unrecognized_appellant_id])
-    if unrecognized_appellant.update_power_of_attorney!(unrecognized_appellant_params)
-      render json: unrecognized_appellant, include: [:unrecognized_power_of_attorney]
+    if @unrecognized_appellant.update_power_of_attorney!(unrecognized_appellant_params)
+      render json: @unrecognized_appellant, include: [:unrecognized_power_of_attorney]
     else
-      render json: unrecognized_appellant, status: :bad_request
+      render json: @unrecognized_appellant, status: :bad_request
     end
   end
 
   private
+  
+  def prevent_update_if_poa_exists
+    render json: {}, status: :bad_request if @unrecognized_appellant.power_of_attorney
+  end
+
+  def find_unrecognized_appellant
+    @unrecognized_appellant = UnrecognizedAppellant.find(params[:unrecognized_appellant_id])
+  end
 
   def unrecognized_appellant_params
     params.require("unrecognized_appellant").permit(
