e6981d4dba8b76fab39619092c65cca5aa833f7f
charleyf <Charles.Ferguson@va.gov>
2021-08-27 13:28:42 -0400

    Have the spec confirm the copy

--- spec/mailers/hearing_email_status_mailer_spec.rb
+++ spec/mailers/hearing_email_status_mailer_spec.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 describe HearingEmailStatusMailer do
-  describe "notification" do
+  describe "notification contents" do
+    include Hearings::AppellantNameHelper
+    include VirtualHearings::LinkHelper
+
     let(:email_address) { "test@caseflow.va.gov" }
     let(:email_type) { "confirmation" }
     let(:hearing) { create(:hearing, :video) }
@@ -13,16 +16,31 @@ describe HearingEmailStatusMailer do
         email_type: email_type
       )
     end
+    let(:email) { described_class.notification(sent_hearing_email_event: sent_hearing_email_event) }
 
     it "has the correct subject" do
-      email = described_class.notification(sent_hearing_email_event: sent_hearing_email_event)
       hearing_type = Constants::HEARING_REQUEST_TYPES.key(hearing.request_type).titleize
       correct_subject = "#{hearing_type} #{email_type} email failed to send to #{email_address}"
       expect(email.subject).to eq(correct_subject)
     end
 
-    it "includes some other key piece of information" do
-      expect(true).to eq(true)
+    it "has the formatted veteran name" do
+      hearing_type = Constants::HEARING_REQUEST_TYPES.key(hearing.request_type).titleize
+      veteran_formatted_name = formatted_appellant_name(hearing.appeal)
+      veteran_sentence_fragment = "You scheduled a #{hearing_type} hearing for #{veteran_formatted_name}"
+
+      expect(email.body).to include(veteran_sentence_fragment)
+    end
+
+    it "has the correct email address" do
+      email_sentence_fragment = "entered the appellant email #{email_address}"
+
+      expect(email.body).to include(email_sentence_fragment)
+    end
+
+    it "has the correct link" do
+      link = external_link hearing_details_url(hearing), display_text: "update it on the Hearing Details page"
+      expect(email.body).to include(link)
     end
   end
 end
