cc9426ca40bb8f7443d08f6f00d4d11976946dfd
charleyf <Charles.Ferguson@va.gov>
2021-08-27 14:06:06 -0400

    Rework spec to work with new database stucture

--- spec/jobs/hearings/send_sent_status_email_spec.rb
+++ spec/jobs/hearings/send_sent_status_email_spec.rb
@@ -6,7 +6,7 @@ describe Hearings::SendSentStatusEmail do
     let(:sent_hearing_admin_email_event) do
       create(:sent_hearing_admin_email_event, sent_hearing_email_event: sent_hearing_email_event)
     end
-    let(:sender) { described_class.new(sent_hearing_email_event: sent_hearing_email_event) }
+    let(:sender) { described_class.new(sent_hearing_admin_email_event: sent_hearing_admin_email_event) }
     let(:external_message_id) { "a-very-long-id" }
     subject { sender.call }
 
@@ -14,9 +14,11 @@ describe Hearings::SendSentStatusEmail do
       # Ensure a call HearingEmailStatusMailer.notification()
       expect(HearingEmailStatusMailer).to receive(:notification).once.and_call_original
       # Mock the external_message_id method
-      allow_any_instance_of(described_class).to receive(:external_message_id).and_return(external_message_id)
+      allow_any_instance_of(described_class).to receive(:get_external_message_id).and_return(external_message_id)
+      # Call the sender
+      subject
       # SendSentStatusEmail should set the external_message_id
-      expect(subject).external_message_id.to eq(external_message_id)
+      expect(sent_hearing_admin_email_event.external_message_id).to eq(external_message_id)
     end
 
     it "fails to send when there is no email on the event" do
@@ -28,8 +30,10 @@ describe Hearings::SendSentStatusEmail do
       expect(Rails.logger).to receive(:info)
       # Expect we logged the failure to DataDog
       expect(DataDogService).to receive(:increment_counter)
-      # We should not set an external_message_id
-      expect(subject).external_message_id.to be_falsey
+      # Call the sender
+      subject
+      # We should not set an external_message_id because we didnt sent an email
+      expect(sent_hearing_admin_email_event.external_message_id).to be_falsey
     end
   end
 end
