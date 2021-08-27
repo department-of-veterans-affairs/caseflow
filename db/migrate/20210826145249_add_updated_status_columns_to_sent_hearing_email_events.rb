3034be92cb2d8a9c18c80b5ffbdb0969c223626b
odonnadieu <87207304+odonnadieu@users.noreply.github.com>
2021-08-27 14:39:40 -0400

    Update db/migrate/20210826145249_add_updated_status_columns_to_sent_hearing_email_events.rb
    Co-authored-by: charleyf <charleyferguson@navapbc.com>

--- db/migrate/20210826145249_add_updated_status_columns_to_sent_hearing_email_events.rb
+++ db/migrate/20210826145249_add_updated_status_columns_to_sent_hearing_email_events.rb
@@ -1,4 +1,4 @@
-class AddUpdatedStatusColumnsToSentHearingEmailEvents < ActiveRecord::Migration[5.2]
+class AddUpdatedStatusColumnsToSentHearingEmailEvents < Caseflow::Migration
   def change
     add_column :sent_hearing_email_events, :send_successful, :boolean, comment: "This column keeps track of whether the email was sent or not"
     add_column :sent_hearing_email_events, :send_successful_checked_at, :datetime, comment: "The date the status was last checked/updated in the GovDelivery API"
