27399c39a5192b2628615118dd9dd75beb5b03c4
Omar Donnadieu <omercado@tistatech.com>
2021-08-27 09:45:40 -0400

    Added SentHearingEmailEvent#handle_reported_status

--- app/models/hearings/sent_hearing_email_event.rb
+++ app/models/hearings/sent_hearing_email_event.rb
@@ -15,6 +15,8 @@ class SentHearingEmailEvent < CaseflowRecord
 
   before_create :assign_sent_at_time
 
+  has_many :sent_hearing_admin_email_events
+
   # Allows all keys specified in `MailRecipient::RECIPIENT_TITLES`
   enum recipient_role: HearingEmailRecipient::RECIPIENT_ROLES.keys.map { |key| [key, key.to_s] }.to_h,
        _prefix: :sent_to
@@ -33,6 +35,22 @@ class SentHearingEmailEvent < CaseflowRecord
       reminder: "reminder"
     }
   ), _prefix: :is
+
+  # error to capture any instances where we try to update `sent_status`
+  # with an invalid value.
+  class InvalidReportedStatus < StandardError; end
+
+  # error to capture any instances we attempt to send email to a coordinator
+  # regarding email status but `sent_status_email_external_message_id` exists.
+  class SentStatusEmailAlreadySent < StandardError; end
+
+  EMAIL_REPORTED_SENT_STATUSES = %w[
+    new sending sent failed inconclusive blacklisted canceled
+  ].freeze
+
+  FAILED_EMAIL_REPORTED_SENT_STATUSES = %w[
+    failed inconclusive canceled blacklisted
+  ].freeze
 
   def sent_to_role
     case recipient_role
@@ -53,9 +71,82 @@ class SentHearingEmailEvent < CaseflowRecord
     end
   end
 
+  def handle_reported_status(reported_status)
+    # Exit if the email has been marked as 'sent'
+    return if send_successful
+
+    # Exit if the hearing is not virtual
+    return if !hearing.virtual?
+
+    # Update the date/time of the last attempt to verify the status
+    update!(send_successful_checked_at: Time.zone.now)
+
+    if !reported_status_valid?(reported_status)
+      invalid_reported_status_failure(reported_status)
+    elsif reported_status_sent?(reported_status)
+      update!(send_successful: true)
+    elsif reported_status_failed?(reported_status)
+      update!(send_successful: false)
+
+      handle_failed_email_status
+    else
+      update!(send_successful: nil)
+    end
+  end
+
   private
 
   def assign_sent_at_time
     self.sent_at ||= Time.now.utc
+  end
+
+  def reported_status_valid?(reported_status)
+    EMAIL_REPORTED_SENT_STATUSES.include?(reported_status)
+  end
+
+  def reported_status_sent?(reported_status)
+    reported_status == "sent"
+  end
+
+  def reported_status_sending?(reported_status)
+    reported_status == "sending"
+  end
+
+  def reported_status_new?(reported_status)
+    reported_status == "new"
+  end
+
+  def reported_status_failed?(reported_status)
+    FAILED_EMAIL_REPORTED_SENT_STATUSES.include?(reported_status)
+  end
+
+  def invalid_reported_status_failure(reported_status)
+    Raven.capture_exception(
+      InvalidReportedStatus.new(
+        "Cannot update sent_status with invalid status: (#{reported_status})"
+      )
+    )
+  end
+
+  def email_already_sent_failure
+    Raven.capture_exception(
+      SentStatusEmailAlreadySent.new(
+        "Cannot sent an email to because we already have attempeted to send this email: " \
+          "(#{external_message_id}) 1 time."
+      )
+    )
+  end
+
+  def handle_failed_email_status
+    # QUESTION: Do we want attempt to send a delivery failure email more than once?
+    if sent_hearing_admin_email_events.present?
+      email_already_sent_failure
+    else
+      sent_hearing_admin_email_event = sent_hearing_admin_email_events.create
+
+      Hearings::SendSentStatusEmail.new(
+        sent_hearing_admin_email_event: sent_hearing_admin_email_event
+      ).call
+    end
   end
 end
--- spec/models/hearings/sent_hearing_email_event_spec.rb
+++ spec/models/hearings/sent_hearing_email_event_spec.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 describe SentHearingEmailEvent do
+  # TODO: This will have to be removed once we have a HearingEmailStatusMailer class
+  class Hearings::SendSentStatusEmail
+    def initialize(**args); end
+    def call(**args); end
+  end
+
   context "#create" do
     let(:user) { create(:user) }
     let(:hearing) { create(:hearing) }
@@ -33,6 +39,107 @@ describe SentHearingEmailEvent do
 
       it "fails validation" do
         expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context "#handle_reported_status" do
+    let(:sent_hearing_email_event) do
+      create(
+        :sent_hearing_email_event,
+        :with_hearing,
+        send_successful_checked_at: DateTime.now
+      )
+    end
+
+    it "takes one argument." do
+      expect { sent_hearing_email_event.handle_reported_status }.to raise_error(ArgumentError)
+    end
+
+    it "does nothing if the send_successful is true." do
+      status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+      sent_hearing_email_event.update(send_successful: true)
+
+      sent_hearing_email_event.handle_reported_status("sent")
+
+      expect(sent_hearing_email_event.send_successful_checked_at).to eq(status_checked_at_was)
+      expect(sent_hearing_email_event.send_successful).to be(true)
+    end
+
+    it "updates send_successful_checked_at if the status given is 'new'." do
+      status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+
+      sent_hearing_email_event.handle_reported_status("new")
+
+      expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+      expect(sent_hearing_email_event.send_successful).to be(nil)
+      expect(sent_hearing_email_event.sent_hearing_admin_email_events.count).to be(0)
+    end
+
+    it "updates send_successful_checked_at if the status given is 'sending'." do
+      status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+
+      sent_hearing_email_event.handle_reported_status("sending")
+
+      expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+      expect(sent_hearing_email_event.send_successful).to be(nil)
+      expect(sent_hearing_email_event.sent_hearing_admin_email_events.count).to be(0)
+    end
+
+    it "updates send_successful_checked_at and sets send_successful to true when the status is 'sent'." do
+      status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+
+      sent_hearing_email_event.handle_reported_status("sent")
+
+      expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+      expect(sent_hearing_email_event.send_successful).to be(true)
+    end
+
+    it "raises InvalidReportedStatus when an invalid status is given." do
+      status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+      allow(Raven).to receive(:capture_exception)
+
+      sent_hearing_email_event.handle_reported_status("bananas")
+
+      expect(Raven).to have_received(:capture_exception).with(SentHearingEmailEvent::InvalidReportedStatus)
+      expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+    end
+
+    context "failed status" do
+      SentHearingEmailEvent::FAILED_EMAIL_REPORTED_SENT_STATUSES.each do |failed_status|
+        it "raises SentStatusEmailAlreadySent when #{failed_status} is given a sent_hearing_admin_email_event record exists." do
+          status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+          sent_hearing_email_event.sent_hearing_admin_email_events.create
+
+          allow(Raven).to receive(:capture_exception)
+
+          sent_hearing_email_event.handle_reported_status(failed_status)
+
+          expect(Raven).to have_received(:capture_exception).with(SentHearingEmailEvent::SentStatusEmailAlreadySent)
+          expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+          expect(sent_hearing_email_event.send_successful).to be(false)
+        end
+
+        it "invokes HearingEmailStatusMailer when #{failed_status} is given." do
+          status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+
+          expect(Hearings::SendSentStatusEmail).to receive(:new).once
+
+          sent_hearing_email_event.handle_reported_status(failed_status)
+
+          expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+          expect(sent_hearing_email_event.send_successful).to be(false)
+        end
+
+        it "creates a delivery_failure record when #{failed_status} is given." do
+          status_checked_at_was = sent_hearing_email_event.send_successful_checked_at
+
+          sent_hearing_email_event.handle_reported_status(failed_status)
+
+          expect(sent_hearing_email_event.delivery_failures.count).to eq(1)
+          expect(sent_hearing_email_event.send_successful_checked_at).to be > status_checked_at_was
+          expect(sent_hearing_email_event.send_successful).to be(false)
+        end
       end
     end
   end
