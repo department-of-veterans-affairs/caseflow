cdd3a2a8221c36a567497eed5d6ba265cfb49d45
charleyf <charleyferguson@navapbc.com>
2021-08-30 17:15:23 -0400

    Add Mailer and Sender Classes for Delivery Notification Emails (#16680)
    Resolves [CASEFLOW-2213](https://vajira.max.gov/browse/CASEFLOW-2213)
    
    ### Description
    These changes are part of the work to handle sending delivery notifications for hearings emails. The ticket this is a subtask on is [CASEFLOW-2165](https://vajira.max.gov/browse/CASEFLOW-2165).
    
    Specifically, these changes implement two classes that are analogous to `Hearings::SendEmail` and `HearingMailer` and supporting template files.
    
    This PR also adds to `emails.rake` for testing purposes and adds convenience traits to the hearing factory for creating a video or a virtual hearing.
    
    ### Acceptance Criteria
    - [ ] Create a new HearingEmailStatusMailer class
      - [ ]  Should subclass ActionMailer::Base like HearingMailer
      - [ ]  Should have a notification method that accepts a SentHearingEmailEvent.
      - [ ] This class and the accompanying files and classes described below are solely for the purpose of sending emails about delivery failures to coordinators.
    - [ ] Email copy needs to be written and approved, [so mock it in for now].
    - [ ] Create a new send mail job for the status emails called Hearings::SendSentStatusEmail.
      - [ ] Charley: Based on the tech spec don't think we actually want a job, more an analog of Hearings::SendEmail. That's what I implemented
      - [ ] Use the Hearings::SendEmail class for guidance
      - [ ] See the tech spec for suggestions on how to invoke the [non] job, and how to call the mailer from the [non] job.
    
    
    ### Testing Plan
    1. Test The Email Copy
        - Look at the spec that examines email copy here: `spec/mailers/hearing_email_status_mailer_spec.rb`
        - Generate test emails by doing this `bundle exec rake emails:hearings:status_emails`
        - Examine the resulting email in `tmp/notification_appellant.html`
    2. Test the sender logic
        - To test this, you unfortunately need the db changes from another PR. Locally, that can be done like this:
    ```zsh
    git checkout charley/CASEFLOW-2213-mailer-and-job-for-delivery-failure-emails # this PRs branch
    git merge omar/CASEFLOW-2212-sent-hearing-email-events-db-1 # The PR with the database changes we need
    make migrate # To include the DB changes in your db
    ```
    2. (cont) Test the sender logic
        - Look at the spec that examines what we'd like to do when trying to send an email: `spec/jobs/hearings/send_sent_status_email_spec.rb`
        - Examine the logging (datadog and otherwise).
    3. Make sure that this code does meet the ACs in combination with the other two PRs.
    4. Quickly look over the testing framework changes in `emails.rake` and `factories/hearing.rb`. I also added a factory for the new email class.

---
+++ app/mailers/hearing_email_status_mailer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class HearingEmailStatusMailer < ActionMailer::Base
+  default from: "Board of Veterans' Appeals <BoardofVeteransAppealsHearings@messages.va.gov>"
+  layout "hearing_email_status_mailer"
+  helper VirtualHearings::LinkHelper
+  include Hearings::AppellantNameHelper
+  include Hearings::CalendarTemplateHelper
+
+  def notification(sent_hearing_email_event:)
+    @sent_hearing_email_event = sent_hearing_email_event
+
+    # Extract each piece of data the templates use
+    @hearing = sent_hearing_email_event.hearing
+    @hearing_type = sent_hearing_email_event.hearing.hearing_request_type
+    # Email types are: "confirmation", "cancellation", "updated_time_confirmation", "reminder"
+    # The gsub is so that "updated_time_confirmation" shows as "Updated Time Confirmation"
+    @email_type = sent_hearing_email_event.email_type.tr("_", " ").downcase
+    @email_address = sent_hearing_email_event.email_address
+    @recipient_role = sent_hearing_email_event.recipient_role
+    @veteran_name = formatted_appellant_name(sent_hearing_email_event.hearing.appeal)
+    @hearing_date = Hearings::CalendarTemplateHelper.hearing_date_only(sent_hearing_email_event.hearing)
+
+    mail(
+      to: @sent_hearing_email_event.email_address,
+      subject: notification_subject
+    )
+  end
+
+  def notification_subject
+    "#{@hearing_type} #{@email_type} email failed to send to #{@email_address}"
+  end
+end
