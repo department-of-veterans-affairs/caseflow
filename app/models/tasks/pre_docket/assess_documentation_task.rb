9b15bf94e4595ca8d280ce87df6c51110e10ba1a
Matthew Kerns <matthewkerns@TST-C02YV319LVDC.local>
2021-08-30 11:58:54 -0400

    Added Complete Task logic to class

--- app/models/tasks/pre_docket/assess_documentation_task.rb
+++ app/models/tasks/pre_docket/assess_documentation_task.rb
@@ -6,4 +6,42 @@
 # will also need to move up the chain as well i.e. Regional -> Program etc.
 
 class AssessDocumentationTask < Task
+  validates :parent, presence: true,
+                     parentTask: { task_types: VhaCamo },
+                     on: :create
+
+  # before_validation :set_assignee
+
+  # Actions that can be taken on both organization and user tasks
+  # TASK_ACTIONS = [
+  #   Constants.TASK_ACTIONS.SEND_TO_TRANSLATION_BLOCKING_DISTRIBUTION.to_h,
+  #   Constants.TASK_ACTIONS.SEND_TO_TRANSCRIPTION_BLOCKING_DISTRIBUTION.to_h,
+  #   Constants.TASK_ACTIONS.SEND_TO_PRIVACY_TEAM_BLOCKING_DISTRIBUTION.to_h,
+  #   Constants.TASK_ACTIONS.SEND_IHP_TO_COLOCATED_BLOCKING_DISTRIBUTION.to_h,
+  #   Constants.TASK_ACTIONS.CLARIFY_POA_BLOCKING_CAVC.to_h
+  # ].freeze
+
+  # Actions a user can take on a task assigned to someone on their team
+  USER_ACTIONS = [
+    Constants.TASK_ACTIONS.COMPLETE_TASK.to_h,
+  ].concat(TASK_ACTIONS).freeze
+
+  # Actions that make sense only for Org-assigned tasks
+  ORG_ACTIONS = [
+    Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h
+  ].concat(TASK_ACTIONS).freeze
+
+  def self.label
+    COPY::VHA_ASSESS_DOCUMENTATION_TASK_LABEL
+  end
+
+  def available_actions(user)
+    if task_is_assigned_to_users_organization?(user)
+      ORG_ACTIONS
+    elsif user_actions_available?(user)
+      USER_ACTIONS
+    else
+      []
+    end
+  end
 end
