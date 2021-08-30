b77db28675b572c53e32d7eb65e39774f36b5f11
charleyf <Charles.Ferguson@va.gov>
2021-08-30 15:59:40 -0400

    More reek fixes

--- app/mailers/hearing_email_status_mailer.rb
+++ app/mailers/hearing_email_status_mailer.rb
@@ -11,15 +11,15 @@ class HearingEmailStatusMailer < ActionMailer::Base
     @sent_hearing_email_event = sent_hearing_email_event
 
     # Extract each piece of data the templates use
-    @hearing = @sent_hearing_email_event.hearing
-    @hearing_type = @hearing.hearing_request_type
+    @hearing = sent_hearing_email_event.hearing
+    @hearing_type = sent_hearing_email_event.hearing.hearing_request_type
     # Email types are: "confirmation", "cancellation", "updated_time_confirmation", "reminder"
     # The gsub is so that "updated_time_confirmation" shows as "Updated Time Confirmation"
-    @email_type = @sent_hearing_email_event.email_type.tr("_", " ").downcase
-    @email_address = @sent_hearing_email_event.email_address
-    @recipient_role = @sent_hearing_email_event.recipient_role
-    @veteran_name = formatted_appellant_name(@hearing.appeal)
-    @hearing_date = Hearings::CalendarTemplateHelper.hearing_date_only(@hearing)
+    @email_type = sent_hearing_email_event.email_type.tr("_", " ").downcase
+    @email_address = sent_hearing_email_event.email_address
+    @recipient_role = sent_hearing_email_event.recipient_role
+    @veteran_name = formatted_appellant_name(sent_hearing_email_event.hearing.appeal)
+    @hearing_date = Hearings::CalendarTemplateHelper.hearing_date_only(sent_hearing_email_event.hearing)
 
     mail(
       to: @sent_hearing_email_event.email_address,
