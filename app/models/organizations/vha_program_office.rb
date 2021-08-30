df80ddcd4200c4e7dc701529272fd93e6f771fbe
Alexander <ajspottswood@gmail.com>
2021-08-30 15:17:13 -0400

    CC override

--- app/models/organizations/vha_program_office.rb
+++ app/models/organizations/vha_program_office.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+# nocov pending additional model behavior
+# :nocov:
 class VhaProgramOffice < Organization
   def can_receive_task?(_task)
     false
   end
 end
+# :nocov:
