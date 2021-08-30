f6ebd7abe8373f75733764efff9223f037a813c0
Brian Gantick <briangantick@navapbc.com>
2021-08-30 09:14:03 -0400

    Assign AssessDocumentationTask to VISN functionality

--- app/models/tasks/pre_docket/assess_documentation_task.rb
+++ app/models/tasks/pre_docket/assess_documentation_task.rb
@@ -6,4 +6,17 @@
 # will also need to move up the chain as well i.e. Regional -> Program etc.
 
 class AssessDocumentationTask < Task
+  def available_actions(user)
+    return [] unless assigned_to.user_has_access?(user)
+
+    if assigned_to.is_a?(VhaProgramOffice)
+      return [Constants.TASK_ACTIONS.VHA_ASSIGN_TO_REGIONAL_OFFICE.to_h]
+    end
+
+    if assigned_to.is_a?(VhaRegionalOffice)
+      return []
+    end
+
+    []
+  end
 end
