b57d28468d0be3e98804349cd38b51910167ec7d
Matthew Kerns <matthewkerns@TST-C02YV319LVDC.local>
2021-08-31 12:41:37 -0400

    Added Complete Task logic to class

--- app/models/tasks/pre_docket/assess_documentation_task.rb
+++ app/models/tasks/pre_docket/assess_documentation_task.rb
@@ -13,35 +13,34 @@ class AssessDocumentationTask < Task
   # before_validation :set_assignee
 
   # Actions that can be taken on both organization and user tasks
-  # TASK_ACTIONS = [
-  #   Constants.TASK_ACTIONS.SEND_TO_TRANSLATION_BLOCKING_DISTRIBUTION.to_h,
-  #   Constants.TASK_ACTIONS.SEND_TO_TRANSCRIPTION_BLOCKING_DISTRIBUTION.to_h,
-  #   Constants.TASK_ACTIONS.SEND_TO_PRIVACY_TEAM_BLOCKING_DISTRIBUTION.to_h,
-  #   Constants.TASK_ACTIONS.SEND_IHP_TO_COLOCATED_BLOCKING_DISTRIBUTION.to_h,
-  #   Constants.TASK_ACTIONS.CLARIFY_POA_BLOCKING_CAVC.to_h
-  # ].freeze
+  TASK_ACTIONS = [
+    Constants.TASK_ACTIONS.MARK_COMPLETE.to_h,
+  ].freeze
 
   # Actions a user can take on a task assigned to someone on their team
-  USER_ACTIONS = [
-    Constants.TASK_ACTIONS.COMPLETE_TASK.to_h,
-  ].concat(TASK_ACTIONS).freeze
+  # USER_ACTIONS = [
+  #   Constants.TASK_ACTIONS.COMPLETE_TASK.to_h,
+  # ].concat(TASK_ACTIONS).freeze
 
   # Actions that make sense only for Org-assigned tasks
-  ORG_ACTIONS = [
-    Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h
-  ].concat(TASK_ACTIONS).freeze
+  # ORG_ACTIONS = [
+  #   Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h
+  # ].concat(TASK_ACTIONS).freeze
 
   def self.label
     COPY::VHA_ASSESS_DOCUMENTATION_TASK_LABEL
   end
 
   def available_actions(user)
-    if task_is_assigned_to_users_organization?(user)
-      ORG_ACTIONS
-    elsif user_actions_available?(user)
-      USER_ACTIONS
-    else
-      []
-    end
+    return [] unless assigned_to.user_has_access?(user)
+
+    TASK_ACTIONS
+    # if task_is_assigned_to_users_organization?(user)
+    #   ORG_ACTIONS
+    # elsif user_actions_available?(user)
+    #   USER_ACTIONS
+    # else
+    #   []
+    # end
   end
 end
