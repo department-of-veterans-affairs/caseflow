c455c729ddd72f971e8d97195dd83093cc1e5728
karinamzalez <karinamzalez@gmail.com>
2021-08-31 12:46:02 -0500

    editPOAInformation/utils: map date to and from API

--- client/app/queue/editPOAInformation/utils.js
+++ client/app/queue/editPOAInformation/utils.js
@@ -1,32 +1,52 @@
-export const mapPOADataFromApi = (appeal) => {
-  const poa = appeal.powerOfAttorney;
-  const poaAddress = poa.representative_address;
-  debugger;
-  console.log(appeal.powerOfAttorney);
-  return {
-    relationship: 'attorney',
-    addressLine1: poaAddress.address_line_1,
-    addressLine2: poaAddress.address_line_2,
-    addressLine3: poaAddress.address_line_3,
-    city: poaAddress.city,
-    country: poaAddress.country,
-  };
-};
-
 export const mapPOADataToApi = (_poa) => {
-  debugger;
-  return {
-    // address is nested object in response, should it not be here?
-    unrecognized_power_of_attorney: {
-      addressLine1: _poa.address_line_1,
-      addressLine2: _poa.address_line_2,
-      addressLine3: _poa.address_line_3,
-      city: _poa.city,
-      country: _poa.country,
-      state: _poa.state,
-      zip: _poa.zip,
-      label: _poa.label,
-      value: _poa.value
+  const returnval = {
+    unrecognized_appellant: {
+      poa_participant_id: null,
+      unrecognized_power_of_attorney: {
+        party_type: _poa.partyType,
+        name: _poa.firstName,
+        address_line_1: _poa.addressLine1,
+        address_line_2: _poa.addressLine2,
+        address_line_3: _poa.addressLine3,
+        city: _poa.city,
+        country: _poa.country,
+        state: _poa.state,
+        zip: _poa.zip,
+        label: _poa.label,
+        value: _poa.value
+      }
     }
   };
+
+  if (_poa.listedAttorney.value !== 'not_listed') {
+    returnval.unrecognized_appellant.poa_participant_id = _poa.listedAttorney.value;
+  }
+
+  return returnval;
 };
+
+export const mapPOADataFromApi = (appeal) => {
+  let returnval = {
+    relationship: 'attorney'
+  };
+
+  if (appeal.hasPOA) {
+    const poa = appeal.powerOfAttorney;
+    const poaAddress = poa.representative_address;
+
+    returnval = { ...returnval,
+      addressLine1: poaAddress.address_line_1,
+      addressLine2: poaAddress.address_line_2,
+      addressLine3: poaAddress.address_line_3,
+      city: poaAddress.city,
+      country: poaAddress.country,
+      email: poa.representative_email_address,
+      name: poa.representative_name,
+      type: poa.representative_type,
+      zone: poa.representative_tz,
+    };
+  }
+
+  return returnval;
+};
+
