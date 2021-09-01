3c837ee9a48960989d8776bb6a4dcb2b97965d41
Anya Roltsch <aroltsch@tistatech.com>
2021-09-01 11:26:23 -0400

    testing

--- docker-bin/startup.sh
+++ docker-bin/startup.sh
@@ -24,11 +24,14 @@ date
 echo "Waiting for Vacols to be ready"
 rake local:vacols:wait_for_connection
 
+echo "Creating DB in PG"
+rake db:setup
+
 echo "Seeding Facols"
 rake local:vacols:seed
 
-echo "Creating and seeding DB in PG"
-rake db:setup
+echo "Seeding DB in PG"
+rake db:seed
 
 echo "Enabling Feature Flags"
 bundle exec rails runner scripts/enable_features_dev.rb
