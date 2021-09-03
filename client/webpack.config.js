4eaf90f36ec6b8490e130d48b42d57f43184f805
Stephen Halliburton <stephen.halliburton@va.gov>
2021-09-03 10:50:52 -0700

    Removed chunking

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,6 @@ const config = {
     filename: 'webpack-bundle.js',
     sourceMapFilename: 'sourcemap-[file].map',
     path: path.join(__dirname, '../app/assets/javascripts'),
-    chunkFilename: '[name].[contenthash].webpack.chunk.js',
     publicPath: devBuild && !testBuild ? 'http://localhost:3500/' : 'auto'
   },
   plugins: [
