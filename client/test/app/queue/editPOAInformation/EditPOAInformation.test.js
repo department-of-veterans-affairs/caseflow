a7c4670faef1936604dea17caa9780802b709ac8
karinamzalez <karinamzalez@gmail.com>
2021-09-03 14:17:21 -0500

    EditPOAInformation.test.js: add accessibility test

--- client/test/app/queue/editPOAInformation/EditPOAInformation.test.js
+++ client/test/app/queue/editPOAInformation/EditPOAInformation.test.js
@@ -21,4 +21,12 @@ describe('EditPOAInformation', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  it('passes a11y testing', async () => {
+    const container = setup();
+
+    const results = await axe(container.html());
+
+    expect(results).toHaveNoViolations();
+  });
 });
