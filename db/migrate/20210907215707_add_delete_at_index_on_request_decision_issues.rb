ea1432ee11fe5f126443dc07f1fad2e115b16bec
yoomlam <Dung.Lam1@va.gov>
2021-09-07 17:36:58 -0500

    use add_safe_index

--- db/migrate/20210907215707_add_delete_at_index_on_request_decision_issues.rb
+++ db/migrate/20210907215707_add_delete_at_index_on_request_decision_issues.rb
@@ -3,6 +3,6 @@ class AddDeleteAtIndexOnRequestDecisionIssues < Caseflow::Migration
     # Use a partial index since we don't need to query by a non-null deleted_at value.
     # https://www.johnnunemaker.com/rails-postgres-partial-indexing/
     # A partial index should be cheaper: https://dba.stackexchange.com/a/102513
-    add_index(:request_decision_issues, :deleted_at, where: "deleted_at IS NULL", algorithm: :concurrently)
+    add_safe_index(:request_decision_issues, :deleted_at, where: "deleted_at IS NULL", algorithm: :concurrently)
   end
 end
