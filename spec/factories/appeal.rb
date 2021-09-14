02c24b5f570eee73a8d7841b56a78d9ca6ca4a06
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-09-14 16:49:12 -0400

    updated :with_decision_issue trait on :appeal factory to have 1:1 relationship between decision issue and `decided` request issue

--- spec/factories/appeal.rb
+++ spec/factories/appeal.rb
@@ -463,20 +463,12 @@ FactoryBot.define do
       description = "Service connection for pain disorder is granted with an evaluation of 70\% effective May 1 2011"
       notes = "Pain disorder with 100\% evaluation per examination"
       after(:create) do |appeal, evaluator|
-        request_issue = create(:request_issue,
-                               :rating,
-                               :with_rating_decision_issue,
-                               decision_review: appeal,
-                               veteran_participant_id: appeal.veteran.participant_id,
-                               contested_issue_description: description,
-                               notes: notes)
         decision_issue = create(:decision_issue,
                                 :rating,
                                 decision_review: appeal,
                                 disposition: evaluator.disposition,
                                 description: "Issue description",
                                 decision_text: "Decision text")
-        request_issue.decision_issues << decision_issue
       end
     end
 
