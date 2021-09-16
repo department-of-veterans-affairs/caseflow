4e01adc0f635448b093cda4698f8292e9a734f7f
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-09-16 16:28:44 -0400

    add task instructions

--- spec/fixes/remove_unrecognized_appellant_cases_from_alert_spec.rb
+++ spec/fixes/remove_unrecognized_appellant_cases_from_alert_spec.rb
@@ -23,7 +23,9 @@ describe "Appeals with unrecognized appellants" do
 
         BvaDispatchTask.create_from_root_task(appeal.root_task)
         user_dispatch_task = appeal.tasks.where(type: "BvaDispatchTask").where(assigned_to_type: "User").first
-        EngineeringTask.create!(parent: user_dispatch_task, status: Constants.TASK_STATUSES.assigned, appeal: appeal)
+        EngineeringTask.create!(parent: user_dispatch_task, status: Constants.TASK_STATUSES.assigned, appeal: appeal,
+                                instructions: "This task is on hold while Caseflow engineering devises a solution for
+        appeals with unrecognized appellants.")
         appeal.reload.treee
         updated_query_result = AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
         expect(updated_query_result.count).to eq(0)
