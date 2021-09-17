af772fb8c513d6f94b3319f2c246093ba968b5d2
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-09-17 16:22:56 -0400

    refactor spec to use shared_examples

--- spec/fixes/remove_unrecognized_appellant_cases_from_alert_spec.rb
+++ spec/fixes/remove_unrecognized_appellant_cases_from_alert_spec.rb
@@ -4,33 +4,46 @@ require "helpers/sanitized_json_configuration.rb"
 require "helpers/sanitized_json_importer.rb"
 
 describe "Appeals with unrecognized appellants" do
-  ["spec/records/unrecognized_appellants/appeal-75406.json",
-   "spec/records/unrecognized_appellants/appeal-113251.json",
-   "spec/records/unrecognized_appellants/appeal-160271.json",
-   "spec/records/unrecognized_appellants/appeal-164926.json"].each do |path|
-    DatabaseCleaner.clean_with(:truncation)
-    sji = SanitizedJsonImporter.from_file(path, verbosity: 6)
+  shared_examples "appeal has an engineering task added to it" do |path|
+    it "no longer shows this appeal in the AppealsWithNoTasksOrAllTasksOnHoldQuery" do
+      sji = SanitizedJsonImporter.from_file(path, verbosity: 6)
+      sji.import
+      appeal = sji.imported_records[Appeal.table_name].first
+      add_nonadmin_user_to_bva_organization
 
-    context "appeal has an engineering task added to it" do
-      it "no longer shows this appeal in the AppealsWithNoTasksOrAllTasksOnHoldQuery" do
-        sji.import
-        appeal = sji.imported_records[Appeal.table_name].first
-        add_nonadmin_user_to_bva_organization
+      query_result = AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
+      # below is the count before the engineering task is created
+      expect(query_result.count).to eq(1)
+      BvaDispatchTask.create_from_root_task(appeal.root_task)
+      user_dispatch_task = appeal.tasks.where(type: "BvaDispatchTask").where(assigned_to_type: "User").first
+      EngineeringTask.create!(parent: user_dispatch_task, status: Constants.TASK_STATUSES.assigned, appeal: appeal,
+                              instructions: "This task is on hold while Caseflow engineering devises a solution for "\
+                              "appeals with unrecognized appellants.")
 
-        query_result = AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
-        # below is the count before the engineering task is created
-        expect(query_result.count).to eq(1)
-
-        BvaDispatchTask.create_from_root_task(appeal.root_task)
-        user_dispatch_task = appeal.tasks.where(type: "BvaDispatchTask").where(assigned_to_type: "User").first
-        EngineeringTask.create!(parent: user_dispatch_task, status: Constants.TASK_STATUSES.assigned, appeal: appeal,
-                                instructions: "This task is on hold while Caseflow engineering devises a solution for
-        appeals with unrecognized appellants.")
-        appeal.reload.treee
-        updated_query_result = AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
-        expect(updated_query_result.count).to eq(0)
-      end
+      appeal.reload.treee
+      updated_query_result = AppealsWithNoTasksOrAllTasksOnHoldQuery.new.call
+      expect(updated_query_result.count).to eq(0)
     end
+  end
+
+  context "appeal with id 75406" do
+    include_examples "appeal has an engineering task added to it",
+                     "spec/records/unrecognized_appellants/appeal-75406.json"
+  end
+
+  context "appeal with id 113251" do
+    include_examples "appeal has an engineering task added to it",
+                     "spec/records/unrecognized_appellants/appeal-113251.json"
+  end
+
+  context "appeal with id 160271" do
+    include_examples "appeal has an engineering task added to it",
+                     "spec/records/unrecognized_appellants/appeal-160271.json"
+  end
+
+  context "appeal with id 164926" do
+    include_examples "appeal has an engineering task added to it",
+                     "spec/records/unrecognized_appellants/appeal-164926.json"
   end
 
   # Need to explicitly add a nonadmin user to the bva organization
