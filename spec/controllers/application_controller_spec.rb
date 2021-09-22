54c03b52e818da90590260f4f00c4cace6be6b9e
Eileen McFarland <eileenmcfarland@navapbc.com>
2021-09-22 16:56:38 -0400

    try to test error in application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -60,4 +60,48 @@ describe ApplicationController, type: :controller do
       end
     end
   end
+
+  describe "error handling v2" do
+    context "when a MultipleOpenTasksOfSameTypeError error is raised" do
+      subject { fail Caseflow::Error::MultipleOpenTasksOfSameTypeError, task_type: "JudgeDecisionReviewTask" }
+
+      it "reports to sentry that the error is not actionable" do
+        expect { subject }.to raise_error(Caseflow::Error::MultipleOpenTasksOfSameTypeError) do |_err|
+          expect(Raven).to receive(:capture_exception).with(anything, extra: { error_uuid: anything, actionable: false })
+        end
+      end
+    end
+  end
+
+  describe "error handling" do
+    let!(:attorney) { create(:user) }
+    let!(:judge) { create(:user) }
+    let!(:attorney_staff) { create(:staff, :attorney_role, sdomainid: attorney.css_id) }
+    let!(:judge_staff) { create(:staff, :judge_role, sdomainid: judge.css_id) }
+    let!(:assign_task) { create(:ama_judge_assign_task, assigned_to: judge, parent: create(:root_task)) }
+    let!(:assignee) { attorney }
+    let!(:params) do
+      { external_id: assign_task.appeal.external_id, parent_id: assign_task.id,
+        assigned_to_id: assignee.id }
+    end
+    # TODO: figure out how to post to the judge_assign_tasks_controller create action
+    # subject { post "/judge_assign_tasks", to: "judge_assign_tasks#create", as: :create }
+    subject { post "/judge_assign_tasks", :controller=>"judge_assign_tasks", as: :create, params: { tasks: params } }
+    # subject { post :create, params: { tasks: params } }
+
+    before do
+      User.authenticate!(user: judge)
+      judge_decision_review_task = double(JudgeDecisionReviewTask)
+      allow(judge_decision_review_task).to(
+        receive(:create!).and_raise(Caseflow::Error::MultipleOpenTasksOfSameTypeError)
+      )
+    end
+
+    context "when an appeal has more than one open active task of the same type" do
+      it "reports the error as non-actionable to Sentry" do
+        subject
+        expect(Raven).to receive(:capture_exception).with(anything, extra: { error_uuid: anything, actionable: false })
+      end
+    end
+  end
 end
