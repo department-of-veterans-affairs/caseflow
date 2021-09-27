6d98f3c208b3cc9212fefe979705dd3f02c0a8f1
charleyf <Charles.Ferguson@va.gov>
2021-09-27 14:04:28 -0400

    Fix error caused by other PR merge

--- app/models/hearing.rb
+++ app/models/hearing.rb
@@ -60,14 +60,13 @@ class Hearing < CaseflowRecord
            :decision_issues, :available_hearing_locations, :closest_regional_office, :advanced_on_docket?,
            to: :appeal
   delegate :external_id, to: :appeal, prefix: true
-  delegate :hearing_day_full?, to: :hearing_day
-  delegate :regional_office, to: :hearing_day, prefix: true
   delegate :timezone, :name, to: :regional_office, prefix: true
 
   # ActiveRecord can interpret the associated hearing_day as null because acts_as_paranoid
   # allows us to soft-delete hearing_days by setting the deleted_at value.
   # As a result, we need to set allow_nil to true for these attributes/functions.
 
+  delegate :hearing_day_full?, to: :hearing_day, allow_nil: true
   delegate :regional_office, to: :hearing_day, prefix: true, allow_nil: true
 
   after_create :update_fields_from_hearing_day
@@ -97,6 +96,7 @@ class Hearing < CaseflowRecord
   # the hearings on that hearing_day give request type == 'V'. This is part
   # of the ACs. This code should NOT be here past October 2021.
   def request_type
+    return nil if !hearing_day 
     return "V" if hearing_day.request_type == "T"
 
     hearing_day.request_type
