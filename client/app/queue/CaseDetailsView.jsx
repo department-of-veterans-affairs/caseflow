adce0062cb93e079ede800ff62c7dcb9e83924b9
karinamzalez <karinamzalez@gmail.com>
2021-09-30 17:29:40 -0500

    CaseDetailsView: replace hasVLJSupportRole with userCanEditUnrecognizedPOA

--- client/app/queue/CaseDetailsView.jsx
+++ client/app/queue/CaseDetailsView.jsx
@@ -161,7 +161,7 @@ export const CaseDetailsView = (props) => {
     appeal.appellantType === APPELLANT_TYPES.OTHER_CLAIMANT && props.featureToggles.edit_unrecognized_appellant;
 
   const editPOAInformation =
-  !appeal.hasPOA && props.hasVLJSupportRole && props.featureToggles.edit_unrecognized_appellant_poa;
+  !appeal.hasPOA && props.userCanEditUnrecognizedPOA && props.featureToggles.edit_unrecognized_appellant_poa;
 
   const supportCavcRemand =
   currentUserIsOnCavcLitSupport && !appeal.isLegacyAppeal;
@@ -323,7 +323,7 @@ CaseDetailsView.propTypes = {
   userCanAccessReader: PropTypes.bool,
   veteranCaseListIsVisible: PropTypes.bool,
   userCanScheduleVirtualHearings: PropTypes.bool,
-  hasVLJSupportRole: PropTypes.bool,
+  userCanEditUnrecognizedPOA: PropTypes.bool,
   scheduledHearingId: PropTypes.string,
   pollHearing: PropTypes.bool,
   stopPollingHearing: PropTypes.func,
