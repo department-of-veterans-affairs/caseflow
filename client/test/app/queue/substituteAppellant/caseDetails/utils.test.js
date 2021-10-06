1b148c7901b96e3f1b8548ce6bd824caa893842d
J.C. Quirin <2581274+jcq@users.noreply.github.com>
2021-10-06 15:36:32 -0400

    refactored substitution-related util fns in caseDetails/utils.js

--- client/test/app/queue/substituteAppellant/caseDetails/utils.test.js
+++ client/test/app/queue/substituteAppellant/caseDetails/utils.test.js
@@ -1,6 +1,189 @@
-import { shouldSupportSubstituteAppellant } from 'app/queue/substituteAppellant/caseDetails/utils';
+import {
+  appealHasDeathDismissal,
+  appealHasSubstitution,
+  appealSupportsSubstitution,
+  supportsSubstitutionPostDispatch,
+  supportsSubstitutionPreDispatch
+} from 'app/queue/substituteAppellant/caseDetails/utils';
 
-describe('shouldSupportSubstituteAppellant', () => {
+describe('appealHasDeathDismissal', () => {
+  const appeal = {
+    isLegacyAppeal: false
+  };
+
+  describe('without decision issue', () => {
+    const decisionIssues = [];
+
+    it('returns false', () => {
+      expect(appealHasDeathDismissal({ ...appeal, decisionIssues })).toBe(false);
+    });
+  });
+
+  describe('without dismissed_death disposition', () => {
+    const decisionIssues = [{ id: 1, disposition: 'allowed' }, { id: 2, disposition: 'denied' }];
+
+    it('returns false', () => {
+      expect(appealHasDeathDismissal({ ...appeal, decisionIssues })).toBe(false);
+    });
+  });
+
+  describe('with dismissed_death disposition', () => {
+    const
+      decisionIssues = [
+        { id: 1, disposition: 'allowed' },
+        { id: 2, disposition: 'dismissed_death' },
+      ];
+
+    it('returns true', () => {
+      expect(appealHasDeathDismissal({ ...appeal, decisionIssues })).toBe(true);
+    });
+  });
+});
+
+describe('appealHasSubstitution', () => {
+  describe('appeal has existing substitution', () => {
+    const appeal = { substitutions: [{ source_appeal_id: 1, target_appeal_id: 2 }] };
+
+    it('returns true', () => {
+      expect(appealHasSubstitution(appeal)).toBe(true);
+    });
+  });
+
+  describe('appeal has no existing substitution', () => {
+    const appeal = { substitutions: [] };
+
+    it('returns false', () => {
+      expect(appealHasSubstitution(appeal)).toBe(false);
+    });
+  });
+});
+
+describe('appealSupportsSubstitution', () => {
+  test('requires AMA appeal', () => {
+    const legacyAppeal = { isLegacyAppeal: true };
+
+    expect(appealSupportsSubstitution(legacyAppeal)).toBe(false);
+  });
+
+  test('requires `original` appeal stream type', () => {
+    const remandAppeal = { caseType: 'Remand' };
+
+    expect(appealSupportsSubstitution(remandAppeal)).toBe(false);
+  });
+
+  test('requires veteran appellant', () => {
+    const appeal = { appellantIsNotVeteran: true };
+
+    expect(appealSupportsSubstitution(appeal)).toBe(false);
+  });
+
+  test('requires all params', () => {
+    const appeal = { isLegacyAppeal: false, caseType: 'Original', appellantIsNotVeteran: false };
+
+    expect(appealSupportsSubstitution(appeal)).toBe(true);
+  });
+});
+
+describe('supportsSubstitutionPreDispatch', () => {
+  const appeal = {
+    appellantIsNotVeteran: false,
+    caseType: 'Original',
+    decisionIssues: [],
+    docketName: 'direct_review',
+    isLegacyAppeal: false,
+    veteranAppellantDeceased: true
+  };
+  const featureToggles = {
+    listed_granted_substitution_before_dismissal: true,
+  };
+  const defaults = {
+    appeal,
+    currentUserOnClerkOfTheBoard: true,
+    featureToggles,
+    userIsCobAdmin: false,
+  };
+
+  describe('with sensible defaults', () => {
+    it('returns true', () => {
+      const args = { ...defaults };
+
+      expect(supportsSubstitutionPreDispatch(args)).toBe(true);
+    });
+  });
+
+  describe('with appeal.appellantIsNotVeteran', () => {
+    it('returns false', () => {
+      const args = {
+        ...defaults,
+        appeal: { ...appeal, appellantIsNotVeteran: true },
+      };
+
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+    });
+  });
+
+  describe('with other caseTypes', () => {
+    const ineligibleCaseTypes = ['vacate', 'de_novo', 'court_remand'];
+
+    it.each(ineligibleCaseTypes)('for %s, returns false', (caseType) => {
+      const args = { ...defaults, appeal: { ...appeal, caseType } };
+
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+    });
+  });
+
+  describe('when not AMA appeal', () => {
+    it('returns false', () => {
+      const args = { ...defaults, appeal: { ...appeal, isLegacyAppeal: true } };
+
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+    });
+  });
+
+  describe('with existing substitution', () => {
+    it('returns false', () => {
+      const args = { ...defaults, hasSubstitution: true };
+
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+    });
+  });
+
+  describe('with non-COB user', () => {
+    it('returns false', () => {
+      const args = { ...defaults, currentUserOnClerkOfTheBoard: false };
+
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+    });
+  });
+
+  describe('when not death dismissal', () => {
+    const nonDismissedDecisionIssues = [
+      { id: 1, disposition: 'something_else' },
+    ];
+    const testAppeal = {
+      ...appeal,
+      decisionIssues: nonDismissedDecisionIssues,
+    };
+
+    describe('with regular COB user', () => {
+      it('returns false', () => {
+        const args = { ...defaults, appeal: testAppeal };
+
+        expect(supportsSubstitutionPostDispatch(args)).toBe(false);
+      });
+    });
+
+    describe('with COB admin', () => {
+      it('returns true', () => {
+        const args = { ...defaults, appeal: testAppeal, userIsCobAdmin: true };
+
+        expect(supportsSubstitutionPostDispatch(args)).toBe(true);
+      });
+    });
+  });
+});
+
+describe('supportsSubstitutionPostDispatch', () => {
   const appeal = {
     appellantIsNotVeteran: false,
     caseType: 'Original',
@@ -23,7 +206,7 @@ describe('shouldSupportSubstituteAppellant', () => {
     it('returns true', () => {
       const args = { ...defaults };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(true);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(true);
     });
   });
 
@@ -34,7 +217,7 @@ describe('shouldSupportSubstituteAppellant', () => {
         appeal: { ...appeal, appellantIsNotVeteran: true },
       };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
     });
   });
 
@@ -44,7 +227,7 @@ describe('shouldSupportSubstituteAppellant', () => {
     it.each(ineligibleCaseTypes)('for %s, returns false', (caseType) => {
       const args = { ...defaults, appeal: { ...appeal, caseType } };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
     });
   });
 
@@ -52,7 +235,7 @@ describe('shouldSupportSubstituteAppellant', () => {
     it('returns false', () => {
       const args = { ...defaults, appeal: { ...appeal, isLegacyAppeal: true } };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
     });
   });
 
@@ -60,7 +243,7 @@ describe('shouldSupportSubstituteAppellant', () => {
     it('returns false', () => {
       const args = { ...defaults, hasSubstitution: true };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
     });
   });
 
@@ -68,19 +251,24 @@ describe('shouldSupportSubstituteAppellant', () => {
     it('returns false', () => {
       const args = { ...defaults, currentUserOnClerkOfTheBoard: false };
 
-      expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+      expect(supportsSubstitutionPostDispatch(args)).toBe(false);
     });
   });
 
   describe('when not death dismissal', () => {
-    const nonDismissedDecisionIssues = [{ id: 1, disposition: 'something_else' }];
-    const testAppeal = { ...appeal, decisionIssues: nonDismissedDecisionIssues };
+    const nonDismissedDecisionIssues = [
+      { id: 1, disposition: 'something_else' },
+    ];
+    const testAppeal = {
+      ...appeal,
+      decisionIssues: nonDismissedDecisionIssues,
+    };
 
     describe('with regular COB user', () => {
       it('returns false', () => {
         const args = { ...defaults, appeal: testAppeal };
 
-        expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+        expect(supportsSubstitutionPostDispatch(args)).toBe(false);
       });
     });
 
@@ -88,7 +276,7 @@ describe('shouldSupportSubstituteAppellant', () => {
       it('returns true', () => {
         const args = { ...defaults, appeal: testAppeal, userIsCobAdmin: true };
 
-        expect(shouldSupportSubstituteAppellant(args)).toBe(true);
+        expect(supportsSubstitutionPostDispatch(args)).toBe(true);
       });
     });
   });
@@ -107,7 +295,7 @@ describe('shouldSupportSubstituteAppellant', () => {
           },
         };
 
-        expect(shouldSupportSubstituteAppellant(args)).toBe(false);
+        expect(supportsSubstitutionPostDispatch(args)).toBe(false);
       });
     });
 
@@ -115,7 +303,7 @@ describe('shouldSupportSubstituteAppellant', () => {
       it('returns true', () => {
         const args = { ...defaults, appeal: hearingAppeal };
 
-        expect(shouldSupportSubstituteAppellant(args)).toBe(true);
+        expect(supportsSubstitutionPostDispatch(args)).toBe(true);
       });
     });
   });
