8828de534a20bbf7b7ff2a81568b32c0fdeaf9b5
Meckila Britt <meckilabritt@navapbc.com>
2021-10-08 10:56:50 -0700

    Run codemod on DateUtil test

--- client/test/app/util/DateUtil-test.js
+++ client/test/app/util/DateUtil-test.js
@@ -1,49 +1,48 @@
-import { expect } from 'chai';
 import { formatDate, formatDateStr, doDatesMatch, formatArrayOfDateStrings } from '../../../app/util/DateUtil';
 
 describe('DateUtil', () => {
-  context('.formatDate', () => {
+  describe('.formatDate', () => {
     it('returns a date formatted mm/dd/yyyy', () => {
       let date = new Date('1/2/2017');
 
-      expect(formatDate(date.toISOString())).to.eq('01/02/2017');
+      expect(formatDate(date.toISOString())).toBe('01/02/2017');
     });
     it('must be a ISO8601 string', () => {
       let badISOString = function() {
         formatDate('1/2/2017');
       };
 
-      expect(badISOString).to.throw(Error);
+      expect(badISOString).toThrowError(Error);
     });
   });
 
-  context('.formatArrayOfDateStrings', () => {
+  describe('.formatArrayOfDateStrings', () => {
     it('returns a comma separated string of dates formatted mm/dd/yyyy', () => {
-      expect(formatArrayOfDateStrings(['1/2/2017', '1/3/2017'])).to.eq('01/02/2017, 01/03/2017');
+      expect(formatArrayOfDateStrings(['1/2/2017', '1/3/2017'])).toBe('01/02/2017, 01/03/2017');
     });
   });
 
-  context('.formatDateStr', () => {
+  describe('.formatDateStr', () => {
     it('returns a date formatted mm/dd/yyyy', () => {
-      expect(formatDateStr('2017-04-24')).to.eq('04/24/2017');
-      expect(formatDateStr('1/2/2017')).to.eq('01/02/2017');
+      expect(formatDateStr('2017-04-24')).toBe('04/24/2017');
+      expect(formatDateStr('1/2/2017')).toBe('01/02/2017');
     });
   });
 
   /* eslint-disable no-unused-expressions */
-  context('.doDatesMatch', () => {
+  describe('.doDatesMatch', () => {
     it('checks to see a string query is part of a date', () => {
-      expect(doDatesMatch('2022-06-31', '06/31/2022')).to.be.true;
-      expect(doDatesMatch('2017-06-28', '06-28-2017')).to.be.true;
-      expect(doDatesMatch('2017-06-12', '6/12/20')).to.be.true;
-      expect(doDatesMatch('2017-06-12', '6/')).to.be.true;
-      expect(doDatesMatch('2022-02-12', '02-')).to.be.true;
-      expect(doDatesMatch('2022-02-01', '2-1-2022')).to.be.true;
-      expect(doDatesMatch('2022-02-12', '-02-12')).to.be.false;
-      expect(doDatesMatch('2022-02-12', 'a2-12-22')).to.be.false;
-      expect(doDatesMatch('2022-02-12', '02-2022-02')).to.be.false;
-      expect(doDatesMatch('2022-02-12', '2017 ')).to.be.false;
-      expect(doDatesMatch('2022-02-12', '/2022')).to.be.true;
+      expect(doDatesMatch('2022-06-31', '06/31/2022')).toBe(true);
+      expect(doDatesMatch('2017-06-28', '06-28-2017')).toBe(true);
+      expect(doDatesMatch('2017-06-12', '6/12/20')).toBe(true);
+      expect(doDatesMatch('2017-06-12', '6/')).toBe(true);
+      expect(doDatesMatch('2022-02-12', '02-')).toBe(true);
+      expect(doDatesMatch('2022-02-01', '2-1-2022')).toBe(true);
+      expect(doDatesMatch('2022-02-12', '-02-12')).toBe(false);
+      expect(doDatesMatch('2022-02-12', 'a2-12-22')).toBe(false);
+      expect(doDatesMatch('2022-02-12', '02-2022-02')).toBe(false);
+      expect(doDatesMatch('2022-02-12', '2017 ')).toBe(false);
+      expect(doDatesMatch('2022-02-12', '/2022')).toBe(true);
     });
   });
   /* eslint-disable no-unused-expressions */
