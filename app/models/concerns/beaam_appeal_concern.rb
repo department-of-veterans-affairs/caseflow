4861c790f90a1d5212a854de970463e6240c60fb
yoomlam <Dung.Lam1@va.gov>
2021-10-14 15:39:13 -0500

    identify beaam appeals

---
+++ app/models/concerns/beaam_appeal_concern.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# This concern is used to identify appeals associated with the BEAAM program
+# See https://github.com/department-of-veterans-affairs/caseflow/wiki/BEAAM-Appeals
+# and fulfills goal 2 of option 1 in the Tech Spec: Enable application code to identify BEAAM appeals
+module BeaamAppealConcern
+  extend ActiveSupport::Concern
+
+  BEAAM_CASE_IDS = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+                    36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+                    50, 51,
+                    53].freeze
+
+  def beaam?
+    BEAAM_CASE_IDS.include?(id)
+  end
+end
---
+++ spec/models/concerns/beaam_appeal_concern_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+describe BeaamAppealConcern do
+  describe "#beaam?" do
+    let(:appeal) { create(:appeal, id: 1) }
+    subject { appeal.beaam? }
+    context "non-BEAAM appeal" do
+      it { is_expected.to eq false }
+    end
+    context "BEAAM appeal" do
+      let(:appeal) { create(:appeal, id: BeaamAppealConcern::BEAAM_CASE_IDS.sample) }
+      it { is_expected.to eq true }
+    end
+  end
+end
