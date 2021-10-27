8f9ec181049c3ef9f1af17b38c0312673e6577e5
Abby Ranson <stephen.halliburton@va.gov>
2021-10-27 09:12:33 -0700

    Started updating tests to handle different hearing types

--- spec/feature/hearings/hearing_details_spec.rb
+++ spec/feature/hearings/hearing_details_spec.rb
@@ -1,34 +1,15 @@
 # frozen_string_literal: true
 
-RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
+RSpec.feature "Hearing Details", :all_dbs do
   let(:user) { create(:user, css_id: "BVATWARNER", roles: ["Build HearSched"]) }
-
-  before do
-    create(:staff, sdept: "HRG", sactive: "A", snamef: "ABC", snamel: "EFG")
-    create(:staff, svlj: "J", sactive: "A", snamef: "HIJ", snamel: "LMNO")
+  let!(:hearing) { create(:hearing, :with_tasks) }
+  let(:expected_alert) { COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % hearing.appeal.veteran.name }
+  let(:virtual_hearing_alert) do
+    COPY::VIRTUAL_HEARING_PROGRESS_ALERTS["CHANGED_TO_VIRTUAL"]["TITLE"] % hearing.appeal.veteran.name
   end
 
-  context "Hearing details is not editable for a non-hearings management user" do
-    let!(:current_user) { User.authenticate!(user: user) }
-    let!(:hearing) { create(:hearing, :with_tasks) }
-
-    scenario "Fields are not editable" do
-      visit "hearings/" + hearing.external_id.to_s + "/details"
-      expect(page).to have_field("Notes", disabled: true)
-    end
-  end
-
-  context "Hearing details for AMA hearing" do
-    let!(:current_user) do
-      HearingsManagement.singleton.add_user(user)
-      User.authenticate!(user: user)
-    end
-    let!(:hearing) { create(:hearing, :with_tasks) }
-    let(:expected_alert) { COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % hearing.appeal.veteran.name }
-
-    scenario "User can update fields" do
-      visit "hearings/" + hearing.external_id.to_s + "/details"
-
+  shared_examples "always updatable fields" do
+    scenario "user can select judge, hearing room, hearing coordinator, and add notes" do
       # wait until the label displays before trying to interact with the dropdown
       find("div", class: "dropdown-judgeDropdown", text: COPY::DROPDOWN_LABEL_JUDGE)
       click_dropdown(name: "judgeDropdown", index: 0)
@@ -42,6 +23,30 @@ RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
       find("label", text: "Yes, Waive 90 Day Evidence Hold").click
 
       fill_in "Notes", with: generate_words(10)
+      click_button("Save")
+
+      expect(page).to have_content(expected_alert)
+    end
+  end
+
+  shared_examples "non-virtual hearing type conversion" do
+    scenario "user can convert hearing type to virtual" do
+      click_dropdown(name: "hearingType", index: 0)
+      fill_in "Veteran Email (for these notifications only)", with: "email@testingEmail.com"
+      fill_in "POA/Representative Email (for these notifications only)", with: "email@testingEmail.com"
+      click_button("button-Save")
+
+      expect(page).to have_no_content(expected_alert)
+      expect(page).to have_content(virtual_hearing_alert)
+    end
+  end
+
+  shared_examples "AMA updatable fields" do
+    before do
+      visit "hearings/" + hearing.external_id.to_s + "/details"
+    end
+
+    scenario "user can update transcription fields" do
       fill_in "taskNumber", with: "123456789"
       click_dropdown(name: "transcriber", index: 1)
       fill_in "sentToTranscriberDate", with: "04012019"
@@ -63,7 +68,7 @@ RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
       expect(page).to have_content(expected_alert)
     end
 
-    context "has transcription details" do
+    context "when hearing already has transcription details" do
       let!(:transcription) do
         create(
           :transcription,
@@ -74,9 +79,7 @@ RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
       end
 
       # This test ensures that a bug related to sending partial form data is fixed.
-      #
       #   See: https://github.com/department-of-veterans-affairs/caseflow/issues/14130
-      #
       scenario "can update fields without side-effects to transcription" do
         visit "hearings/#{hearing.external_id}/details"
 
@@ -127,51 +130,12 @@ RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
     end
   end
 
-  context "Hearing details for Legacy hearing" do
-    let!(:current_user) do
-      HearingsManagement.singleton.add_user(user)
-      User.authenticate!(user: user)
+  shared_examples "Legacy updatable fields" do
+    before do
+      visit "hearings/" + hearing.external_id.to_s + "/details"
     end
 
-    let!(:legacy_hearing) { create(:legacy_hearing, :with_tasks, regional_office: "RO06") }
-    let(:expected_alert) { COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % legacy_hearing.appeal.veteran.name }
-    let(:virtual_hearing_alert) do
-      COPY::VIRTUAL_HEARING_PROGRESS_ALERTS["CHANGED_TO_VIRTUAL"]["TITLE"] % legacy_hearing.appeal.veteran.name
-    end
-
-    scenario "User can edit Judge and change virtual hearings" do
-      visit "hearings/" + legacy_hearing.external_id.to_s + "/details"
-      expect(page).to have_field("judge-dropdown", disabled: false)
-      expect(page).to have_field("hearing-coordinator-dropdown", disabled: false)
-      expect(page).to have_field("hearing-room-dropdown", disabled: false)
-      expect(page).to have_field("Notes", disabled: false)
-      expect(page).to have_no_selector("label", text: "Yes, Waive 90 Day Evidence Hold")
-
-      click_dropdown(name: "hearingType", index: 0)
-      fill_in "Veteran Email (for these notifications only)", with: "email@testingEmail.com"
-      fill_in "POA/Representative Email (for these notifications only)", with: "email@testingEmail.com"
-      click_button("button-Save")
-
-      expect(page).to have_no_content(expected_alert)
-      expect(page).to have_content(virtual_hearing_alert)
-    end
-
-    scenario "User can select judge, hearing room, hearing coordinator, and add notes" do
-      visit "hearings/" + legacy_hearing.external_id.to_s + "/details"
-
-      click_dropdown(name: "judgeDropdown", index: 0, wait: 30)
-      click_dropdown(name: "hearingCoordinatorDropdown", index: 0, wait: 30)
-      click_dropdown(name: "hearingRoomDropdown", index: 0, wait: 30)
-
-      fill_in "Notes", with: generate_words(10)
-
-      click_button("Save")
-      expect(page).to have_content(expected_alert)
-    end
-
-    scenario "User can not edit transcription" do
-      visit "hearings/" + legacy_hearing.external_id.to_s + "/details"
-
+    scenario "user cannot update transcription fields" do
       expect(page).to have_no_field("taskNumber")
       expect(page).to have_no_field("transcriber")
       expect(page).to have_no_field("sentToTranscriberDate")
@@ -182,6 +146,70 @@ RSpec.feature "Hearing Schedule Daily Docket", :all_dbs do
       expect(page).to have_no_field("requestedRemedy")
       expect(page).to have_no_field("copySentDate")
       expect(page).to have_no_field("copyRequested")
+    end
+  end
+
+  before do
+    create(:staff, sdept: "HRG", sactive: "A", snamef: "ABC", snamel: "EFG")
+    create(:staff, svlj: "J", sactive: "A", snamef: "HIJ", snamel: "LMNO")
+  end
+
+  context "with unauthorized user role (non-hearings management)" do
+    let!(:current_user) { User.authenticate!(user: user) }
+
+    scenario "Fields are not editable" do
+      visit "hearings/" + hearing.external_id.to_s + "/details"
+      expect(page).to have_field("Notes", disabled: true)
+    end
+  end
+
+  context "with authorized user role" do
+    let!(:current_user) do
+      HearingsManagement.singleton.add_user(user)
+      User.authenticate!(user: user)
+    end
+    let(:expected_alert) { COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % hearing.appeal.veteran.name }
+
+    context "when hearing is AMA" do
+      let!(:hearing) { create(:hearing, :with_tasks) }
+
+      context "when type is Video" do
+        include_examples "always updatable fields"
+        include_examples "AMA updatable fields"
+        include_examples "non-virtual hearing type conversion"
+      end
+
+      context "when type is Central" do
+        include_examples "always updatable fields"
+        include_examples "AMA updatable fields"
+        include_examples "non-virtual hearing type conversion"
+      end
+
+      context "when type is Virtual" do
+        include_examples "always updatable fields"
+        include_examples "AMA updatable fields"
+      end
+    end
+
+    context "when hearing is Legacy" do
+      let!(:hearing) { create(:legacy_hearing, :with_tasks, regional_office: "RO06") }
+
+      context "when type is Video" do
+        include_examples "always updatable fields"
+        include_examples "Legacy updatable fields"
+        include_examples "non-virtual hearing type conversion"
+      end
+
+      context "when type is Central" do
+        include_examples "always updatable fields"
+        include_examples "Legacy updatable fields"
+        include_examples "non-virtual hearing type conversion"
+      end
+
+      context "when type is Virtual" do
+        include_examples "always updatable fields"
+        include_examples "Legacy updatable fields"
+      end
     end
   end
 end
