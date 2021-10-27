8a6c682147453d68bc697edadac8964be316e3e2
Abby Ranson <stephen.halliburton@va.gov>
2021-10-27 13:08:00 -0700

    Refactored hearing details rspec

--- spec/feature/hearings/hearing_details_spec.rb
+++ spec/feature/hearings/hearing_details_spec.rb
@@ -10,6 +10,76 @@ RSpec.feature "Hearing Details", :all_dbs do
     COPY::VIRTUAL_HEARING_PROGRESS_ALERTS["CHANGED_TO_VIRTUAL"]["TITLE"] % hearing.appeal.veteran.name
   end
 
+  let(:pre_loaded_veteran_email) { hearing.appeal.veteran.email_address }
+  let(:pre_loaded_rep_email) { hearing.appeal.representative_email_address }
+  let(:fill_in_veteran_email) { "email@testingEmail.com" }
+  let(:fill_in_veteran_email) { "new@email.com" }
+  let(:fill_in_veteran_tz) { "America/New_York" }
+  let(:fill_in_rep_email) { "rep@testingEmail.com" }
+  let(:fill_in_rep_tz) { "America/Chicago" }
+  let(:pexip_url) { "fake.va.gov" }
+
+  shared_examples "with existing email recipient" do
+    context "with existing appellant email only" do
+      before do
+        hearing.create_or_update_recipients(
+          type: AppellantHearingEmailRecipient,
+          email_address: fill_in_veteran_email
+        )
+      end
+
+      it "preloads the appellant email" do
+        visit "hearings/" + hearing.external_id.to_s + "/details"
+        click_dropdown(name: "hearingType", index: 0)
+        expect(page).to have_content(COPY::CONVERT_HEARING_TITLE % "Virtual")
+
+        expect(page).to have_field("Veteran Email", with: fill_in_veteran_email)
+        expect(page).to have_field("POA/Representative Email", with: pre_loaded_rep_email)
+      end
+    end
+
+    context "with existing representative email only" do
+      before do
+        hearing.create_or_update_recipients(
+          type: RepresentativeHearingEmailRecipient,
+          email_address: fill_in_rep_email
+        )
+      end
+
+      it "preloads the representative email" do
+        visit "hearings/" + hearing.external_id.to_s + "/details"
+        click_dropdown(name: "hearingType", index: 0)
+        expect(page).to have_content(COPY::CONVERT_HEARING_TITLE % "Virtual")
+
+        expect(page).to have_field("Veteran Email", with: pre_loaded_veteran_email)
+        expect(page).to have_field("POA/Representative Email", with: fill_in_rep_email)
+      end
+    end
+
+    context "with existing representative and appellant emails" do
+      before do
+        hearing.create_or_update_recipients(
+          type: RepresentativeHearingEmailRecipient,
+          email_address: fill_in_rep_email
+        )
+
+        hearing.create_or_update_recipients(
+          type: AppellantHearingEmailRecipient,
+          email_address: fill_in_veteran_email
+        )
+      end
+
+      it "preloads the representative and appellant emails" do
+        visit "hearings/" + hearing.external_id.to_s + "/details"
+        click_dropdown(name: "hearingType", index: 0)
+        expect(page).to have_content(COPY::CONVERT_HEARING_TITLE % "Virtual")
+
+        expect(page).to have_field("Veteran Email", with: fill_in_veteran_email)
+        expect(page).to have_field("POA/Representative Email", with: fill_in_rep_email)
+      end
+    end
+  end
+
   shared_examples "always updatable fields" do
     scenario "user can select judge, hearing room, hearing coordinator, and add notes" do
       # wait until the label displays before trying to interact with the dropdowns
@@ -46,111 +116,37 @@ RSpec.feature "Hearing Details", :all_dbs do
     end
   end
 
-  shared_examples "AMA updatable fields" do
-    before do
-      visit "hearings/" + hearing.external_id.to_s + "/details"
+  shared_examples "all hearing types" do
+    context "when type is Video" do
+      before do
+        hearing.hearing_day.update!(regional_office: "RO06", request_type: "V")
+      end
+
+      include_examples "always updatable fields"
+      include_examples "non-virtual hearing type conversion"
     end
 
-    scenario "user can update transcription fields" do
-      fill_in "taskNumber", with: "123456789"
-      click_dropdown(name: "transcriber", index: 1)
-      fill_in "sentToTranscriberDate", with: "04012019"
-      fill_in "expectedReturnDate", with: "04022019"
-      fill_in "uploadedToVbmsDate", with: "04032019"
-
-      click_dropdown(name: "problemType", index: 1)
-      fill_in "problemNoticeSentDate", with: "04042019"
-      find(
-        ".cf-form-radio-option",
-        text: Constants.TRANSCRIPTION_REQUESTED_REMEDIES.PROCEED_WITHOUT_TRANSCRIPT
-      ).click
-
-      find("label", text: "Yes, Transcript Requested").click
-      fill_in "copySentDate", with: "04052019"
-
-      click_button("Save")
-
-      expect(page).to have_content(expected_alert)
+    context "when type is Central" do
+      include_examples "always updatable fields"
+      include_examples "non-virtual hearing type conversion"
     end
 
-    context "when hearing already has transcription details" do
-      let!(:transcription) do
+    context "when type is Virtual" do
+      let!(:virtual_hearing) do
         create(
-          :transcription,
+          :virtual_hearing,
+          :initialized,
+          status: :active,
           hearing: hearing,
-          problem_type: Constants.TRANSCRIPTION_PROBLEM_TYPES.POOR_AUDIO,
-          requested_remedy: Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING
+          appellant_email: "existing_veteran_email@caseflow.gov",
+          appellant_email_sent: true,
+          judge_email: "existing_judge_email@caseflow.gov",
+          judge_email_sent: true,
+          representative_email: nil
         )
       end
 
-      # This test ensures that a bug related to sending partial form data is fixed.
-      #   See: https://github.com/department-of-veterans-affairs/caseflow/issues/14130
-      scenario "can update fields without side-effects to transcription" do
-        visit "hearings/#{hearing.external_id}/details"
-
-        step "ensure page has existing transcription details" do
-          expect(
-            page.find(".dropdown-problemType .cf-select__value-container")
-          ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.POOR_AUDIO)
-          expect(
-            find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
-          ).to be_checked
-        end
-
-        step "changing only problem type preserves already populated fields" do
-          click_dropdown(name: "problemType", index: 0)
-          click_button("Save")
-
-          expect(page).to have_content(expected_alert)
-
-          visit "hearings/#{hearing.external_id}/details"
-
-          expect(
-            page.find(".dropdown-problemType .cf-select__value-container")
-          ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.NO_AUDIO)
-          expect(
-            find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
-          ).to be_checked
-          expect(Transcription.count).to be(2)
-        end
-
-        step "changing notes preserves already populated fields and doesn't create new transcription" do
-          fill_in "Notes", with: "Test Notes Test Notes"
-          click_button("Save")
-
-          expect(page).to have_content(expected_alert)
-
-          visit "hearings/#{hearing.external_id}/details"
-
-          expect(page).to have_content("Test Notes Test Notes")
-          expect(
-            page.find(".dropdown-problemType .cf-select__value-container")
-          ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.NO_AUDIO)
-          expect(
-            find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
-          ).to be_checked
-          expect(Transcription.count).to be(2)
-        end
-      end
-    end
-  end
-
-  shared_examples "Legacy updatable fields" do
-    before do
-      visit "hearings/" + hearing.external_id.to_s + "/details"
-    end
-
-    scenario "user cannot update transcription fields" do
-      expect(page).to have_no_field("taskNumber")
-      expect(page).to have_no_field("transcriber")
-      expect(page).to have_no_field("sentToTranscriberDate")
-      expect(page).to have_no_field("expectedReturnDate")
-      expect(page).to have_no_field("uploadedToVbmsDate")
-      expect(page).to have_no_field("problemType")
-      expect(page).to have_no_field("problemNoticeSentDate")
-      expect(page).to have_no_field("requestedRemedy")
-      expect(page).to have_no_field("copySentDate")
-      expect(page).to have_no_field("copyRequested")
+      include_examples "always updatable fields"
     end
   end
 
@@ -171,75 +167,114 @@ RSpec.feature "Hearing Details", :all_dbs do
     let(:expected_alert) { COPY::HEARING_UPDATE_SUCCESSFUL_TITLE % hearing.appeal.veteran.name }
 
     context "when hearing is AMA" do
-      context "when type is Video" do
-        let!(:hearing) { create(:hearing, :with_tasks, regional_office: "RO06", scheduled_time: "9:30AM") }
-
-        include_examples "always updatable fields"
-        include_examples "AMA updatable fields"
-        include_examples "non-virtual hearing type conversion"
+      before do
+        visit "hearings/" + hearing.external_id.to_s + "/details"
       end
 
-      context "when type is Central" do
-        include_examples "always updatable fields"
-        include_examples "AMA updatable fields"
-        include_examples "non-virtual hearing type conversion"
+      include_examples "all hearing types"
+
+      scenario "user can update transcription fields" do
+        fill_in "taskNumber", with: "123456789"
+        click_dropdown(name: "transcriber", index: 1)
+        fill_in "sentToTranscriberDate", with: "04012019"
+        fill_in "expectedReturnDate", with: "04022019"
+        fill_in "uploadedToVbmsDate", with: "04032019"
+
+        click_dropdown(name: "problemType", index: 1)
+        fill_in "problemNoticeSentDate", with: "04042019"
+        find(
+          ".cf-form-radio-option",
+          text: Constants.TRANSCRIPTION_REQUESTED_REMEDIES.PROCEED_WITHOUT_TRANSCRIPT
+        ).click
+
+        find("label", text: "Yes, Transcript Requested").click
+        fill_in "copySentDate", with: "04052019"
+
+        click_button("Save")
+
+        expect(page).to have_content(expected_alert)
       end
 
-      context "when type is Virtual" do
-        let!(:virtual_hearing) do
+      context "when hearing already has transcription details" do
+        let!(:transcription) do
           create(
-            :virtual_hearing,
-            :initialized,
-            status: :active,
+            :transcription,
             hearing: hearing,
-            appellant_email: "existing_veteran_email@caseflow.gov",
-            appellant_email_sent: true,
-            judge_email: "existing_judge_email@caseflow.gov",
-            judge_email_sent: true,
-            representative_email: nil
+            problem_type: Constants.TRANSCRIPTION_PROBLEM_TYPES.POOR_AUDIO,
+            requested_remedy: Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING
           )
         end
 
-        include_examples "always updatable fields"
-        include_examples "AMA updatable fields"
+        # This test ensures that a bug related to sending partial form data is fixed.
+        #   See: https://github.com/department-of-veterans-affairs/caseflow/issues/14130
+        scenario "can update fields without side-effects to transcription" do
+          visit "hearings/#{hearing.external_id}/details"
+
+          step "ensure page has existing transcription details" do
+            expect(
+              page.find(".dropdown-problemType .cf-select__value-container")
+            ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.POOR_AUDIO)
+            expect(
+              find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
+            ).to be_checked
+          end
+
+          step "changing only problem type preserves already populated fields" do
+            click_dropdown(name: "problemType", index: 0)
+            click_button("Save")
+
+            expect(page).to have_content(expected_alert)
+
+            visit "hearings/#{hearing.external_id}/details"
+
+            expect(
+              page.find(".dropdown-problemType .cf-select__value-container")
+            ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.NO_AUDIO)
+            expect(
+              find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
+            ).to be_checked
+            expect(Transcription.count).to be(2)
+          end
+
+          step "changing notes preserves already populated fields and doesn't create new transcription" do
+            fill_in "Notes", with: "Test Notes Test Notes"
+            click_button("Save")
+
+            expect(page).to have_content(expected_alert)
+
+            visit "hearings/#{hearing.external_id}/details"
+
+            expect(page).to have_content("Test Notes Test Notes")
+            expect(
+              page.find(".dropdown-problemType .cf-select__value-container")
+            ).to have_content(Constants.TRANSCRIPTION_PROBLEM_TYPES.NO_AUDIO)
+            expect(
+              find_field(Constants.TRANSCRIPTION_REQUESTED_REMEDIES.NEW_HEARING, visible: false)
+            ).to be_checked
+            expect(Transcription.count).to be(2)
+          end
+        end
       end
     end
 
     context "when hearing is Legacy" do
-      context "when type is Video" do
-        let!(:hearing) { create(:legacy_hearing, :with_tasks, regional_office: "RO06", scheduled_for: "9:30AM") }
-
-        include_examples "always updatable fields"
-        include_examples "Legacy updatable fields"
-        include_examples "non-virtual hearing type conversion"
+      before do
+        visit "hearings/" + hearing.external_id.to_s + "/details"
       end
 
-      context "when type is Central" do
-        let!(:hearing) { create(:legacy_hearing, :with_tasks, regional_office: "C", scheduled_for: "9:30AM") }
+      include_examples "all hearing types"
 
-        include_examples "always updatable fields"
-        include_examples "Legacy updatable fields"
-        include_examples "non-virtual hearing type conversion"
-      end
-
-      context "when type is Virtual" do
-        let!(:hearing) { create(:legacy_hearing, :with_tasks, regional_office: "C", scheduled_for: "9:30AM") }
-        let!(:virtual_hearing) do
-          create(
-            :virtual_hearing,
-            :all_emails_sent,
-            status: :active,
-            hearing: hearing,
-            appellant_email: "existing_veteran_email@caseflow.gov",
-            appellant_email_sent: true,
-            judge_email: "existing_judge_email@caseflow.gov",
-            judge_email_sent: true,
-            representative_email: nil
-          )
-        end
-
-        include_examples "always updatable fields"
-        include_examples "Legacy updatable fields"
+      scenario "user cannot update transcription fields" do
+        expect(page).to have_no_field("taskNumber")
+        expect(page).to have_no_field("transcriber")
+        expect(page).to have_no_field("sentToTranscriberDate")
+        expect(page).to have_no_field("expectedReturnDate")
+        expect(page).to have_no_field("uploadedToVbmsDate")
+        expect(page).to have_no_field("problemType")
+        expect(page).to have_no_field("problemNoticeSentDate")
+        expect(page).to have_no_field("requestedRemedy")
+        expect(page).to have_no_field("copySentDate")
+        expect(page).to have_no_field("copyRequested")
       end
     end
   end
