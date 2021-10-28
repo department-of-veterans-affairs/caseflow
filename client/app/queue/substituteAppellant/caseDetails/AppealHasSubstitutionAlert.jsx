eaec0ac7e5274a3f752e9c4c992d8f6aa8b49827
pbradin <pbradin@tistatech.com>
2021-10-28 09:32:53 -0400

    Updates to not show new appeal stream link if there is no new appeal stream

--- client/app/queue/substituteAppellant/caseDetails/AppealHasSubstitutionAlert.jsx
+++ client/app/queue/substituteAppellant/caseDetails/AppealHasSubstitutionAlert.jsx
@@ -5,14 +5,20 @@ import Alert from 'app/components/Alert';
 
 import { SUBSTITUTE_APPELLANT_SOURCE_APPEAL_ALERT_DESCRIPTION } from 'app/../COPY';
 
+const appealIsSameAppealSubstitution = ({ targetAppealId }) => {
+  return (targetAppealId === 'undefined' || targetAppealId === null);
+};
+
 export const AppealHasSubstitutionAlert = ({ targetAppealId }) => {
   return (
     <Alert type="info">
       {SUBSTITUTE_APPELLANT_SOURCE_APPEAL_ALERT_DESCRIPTION}{' '}
+      {!appealIsSameAppealSubstitution &&
       <span>
-        See the new{' '}
+          See the new{' '}
         <Link to={`/queue/appeals/${targetAppealId}`}>appeal stream</Link>.
       </span>
+      }
     </Alert>
   );
 };
