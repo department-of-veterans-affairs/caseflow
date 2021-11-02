94e4f0d255e34e7e33e0226155957da78235c499
yoomlam <Dung.Lam1@va.gov>
2021-11-02 12:29:55 -0500

    fix AvailableHearingLocations#veteran

--- app/models/hearings/available_hearing_locations.rb
+++ app/models/hearings/available_hearing_locations.rb
@@ -9,7 +9,8 @@
 class AvailableHearingLocations < CaseflowRecord
   include HasAppealUpdatedSince
 
-  belongs_to :veteran, foreign_key: :file_number, primary_key: :veteran_file_number
+  belongs_to :veteran, primary_key: :file_number, foreign_key: :veteran_file_number,
+    inverse_of: :available_hearing_locations
   belongs_to :appeal, polymorphic: true
 
   def to_hash
