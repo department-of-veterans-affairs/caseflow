521f8f2d958b0ae078e8adbaf187f74110c5bfcb
charleyf <Charles.Ferguson@va.gov>
2021-11-03 13:54:00 -0400

    Restructure various parts

--- app/models/concerns/has_hearing_email_recipients_concern.rb
+++ app/models/concerns/has_hearing_email_recipients_concern.rb
@@ -5,20 +5,25 @@ module HasHearingEmailRecipientsConcern
   extend ActiveSupport::Concern
 
   def appellant_recipient
+    # If there's an existing AppellantHearingEmailRecipient return that
     recipient = email_recipients.find_by(type: "AppellantHearingEmailRecipient")
+    return recipient if recipient.present?
 
-    if recipient.blank? && appellant_email_address.present?
+    # If there is no AppellantHearingEmailRecipient, its possible that the email address for
+    # this recipient exist only on the virtual hearing and needs to be migrated
+    if appellant_email_address.present?
+      # Create a new recipient from the virtual hearing, this is a migration from when we stored
+      # email recipients only for virtual hearings
       recipient = create_or_update_recipients(
         type: AppellantHearingEmailRecipient,
-        email_address: appellant_email_address,
-        timezone: appellant_tz,
+        email_address: appellant_email_address, timezone: appellant_tz,
         email_sent: virtual_hearing.present? ? virtual_hearing[:appellant_email_sent] : true
       )
 
+      # Update the email events that were pointing at the virtual hearing to use the newly
+      # created AppellantHearingEmailRecipient
       update_email_events(recipient, recipient.roles)
     end
-
-    recipient
   end
 
   def representative_recipient
@@ -94,49 +99,40 @@ module HasHearingEmailRecipientsConcern
 
   def appellant_email_address
     recipient = email_recipients.find_by(type: "AppellantHearingEmailRecipient")
+    return recipient&.email_address if recipient.present?
 
-    if recipient.blank?
-      virtual_hearing.present? ? virtual_hearing[:appellant_email] : appeal&.appellant_email_address
-    else
-      recipient&.email_address
-    end
+    # If no existing recipient, fall back to the virtual hearing
+    # If we don't have an existing recipient, fall back to the virtual hearing
+    virtual_hearing.present? ? virtual_hearing[:appellant_email] : appeal&.appellant_email_address
   end
 
   def appellant_tz
     recipient = email_recipients.find_by(type: "AppellantHearingEmailRecipient")
+    return recipient&.timezone || appeal&.appellant_tz if recipient.present?
 
-    if recipient.blank?
-      virtual_hearing.present? ? virtual_hearing[:appellant_tz] : appeal&.appellant_tz
-    else
-      recipient&.timezone || appeal&.appellant_tz
-    end
+    # If no existing recipient, fall back to the virtual hearing
+    virtual_hearing.present? ? virtual_hearing[:appellant_tz] : appeal&.appellant_tz
   end
 
   def representative_email_address
     recipient = email_recipients.find_by(type: "RepresentativeHearingEmailRecipient")
+    return recipient&.email_address if recipient.present?
 
-    if recipient.blank?
-      if virtual_hearing.present?
-        virtual_hearing[:representative_email].presence
-      else
-        representative_tz.present? ? appeal&.representative_email_address : nil
-      end
-    else
-      recipient&.email_address
-    end
+    return virtual_hearing[:representative_email].presence if virtual_hearing.present?
+
+    # If no existing recipient, fall back to the virtual hearing
+    representative_tz.present? ? appeal&.representative_email_address : nil
   end
 
   def representative_tz
     recipient = email_recipients.find_by(type: "RepresentativeHearingEmailRecipient")
+    return recipient&.timezone if recipient.present?
 
-    if recipient.blank?
-      begin
-        virtual_hearing.present? ? virtual_hearing[:representative_tz].presence : appeal&.representative_tz
-      rescue Module::DelegationError
-        nil
-      end
-    else
-      recipient&.timezone
+    begin
+      # If no existing recipient, fall back to the virtual hearing
+      virtual_hearing.present? ? virtual_hearing[:representative_tz].presence : appeal&.representative_tz
+    rescue Module::DelegationError
+      nil
     end
   end
 
