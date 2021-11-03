51a813c25941ce5d9443a49d8bad60542da1d366
Libbey White <libbeywhite@navapbc.com>
2021-11-03 12:32:30 -0700

    Remove unused param

--- app/serializers/hearings/hearing_day_serializer.rb
+++ app/serializers/hearings/hearing_day_serializer.rb
@@ -75,7 +75,7 @@ class HearingDaySerializer
     Hearing::HEARING_TYPES[hearing_day.request_type.to_sym]
   end
 
-  def self.serialize_collection(hearing_days, current_user)
+  def self.serialize_collection(hearing_days)
     video_hearing_days_request_types = HearingDayRequestTypeQuery.new.call
     filled_slots_count_for_days = HearingDayFilledSlotsQuery.new(hearing_days).call
     judge_names = HearingDayJudgeNameQuery.new(hearing_days).call
