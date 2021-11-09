5e6531d4c7f17d5b5e08aff8d39afb35cfbbfcbf
Bryce Sampson <bsampson@tistatech.com>
2021-11-09 09:20:09 -0700

    fix lint errors

--- client/app/components/icons/LoadingSymbol.jsx
+++ client/app/components/icons/LoadingSymbol.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { COLORS } from '@department-of-veterans-affairs/caseflow-frontend-toolkit/util/StyleConstants';
