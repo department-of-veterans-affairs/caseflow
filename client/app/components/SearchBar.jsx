5a3e17b0b88b6a80627423ae12e2fc30999dfb27
Bryce Sampson <bsampson@tistatech.com>
2021-11-09 08:17:06 -0700

    add documentation to storybook for all icons.

--- client/app/components/SearchBar.jsx
+++ client/app/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CloseIcon, LoadingSymbol } from './icons/LoadingSymbol';
+import { LoadingSymbol } from './icons/LoadingSymbol';
+import { CloseIcon } from './icons/CloseIcon';
 import { COLORS } from '@department-of-veterans-affairs/caseflow-frontend-toolkit/util/StyleConstants';
 import Button from './Button';
 import classnames from 'classnames';
