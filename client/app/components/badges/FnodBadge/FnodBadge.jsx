85d722fd5d8897d9567d7d46deb6497a75449362
Bryce Sampson <bsampson@tistatech.com>
2021-11-12 11:21:10 -0700

    fix various proptype errors when loading case details page

--- client/app/components/badges/FnodBadge/FnodBadge.jsx
+++ client/app/components/badges/FnodBadge/FnodBadge.jsx
@@ -22,7 +22,7 @@ const FnodBadge = (props) => {
 FnodBadge.propTypes = {
   veteranAppellantDeceased: PropTypes.bool,
   uniqueId: PropTypes.string,
-  tooltipText: PropTypes.string
+  tooltipText: PropTypes.object
 };
 
 export default FnodBadge;
