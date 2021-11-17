f2ed6a3fe8d157e03e9673b1efd34315d2feffd4
Joel Robinson <jrobinson@tistatech.com>
2021-11-17 11:27:26 -0600

    Added migrations status to healthcheck.

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -8,9 +8,21 @@ class HealthChecksController < ActionController::Base
   newrelic_ignore_apdex
 
   def show
+    migrations = check_migrations
     body = {
       healthy: true
-    }.merge(Rails.application.config.build_version || {})
+    }.merge(Rails.application.config.build_version || {}).merge(migrations)
     render(json: body, status: :ok)
+  end
+
+  private
+  def check_migrations
+    migrations = []
+    pending_migrations = false
+    ActiveRecord::Base.connection.migration_context.migrations_status.each do |status, version, name|
+        migrations << { status: status, version: version, name: name }
+        pending_migrations = true if status != "up"
+    end
+    { migrations: migrations, pending_migrations: pending_migrations }
   end
 end
