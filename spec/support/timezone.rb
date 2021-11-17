50f7e2b169f05cc883b369f80e341ed7ea61b3da
Omar Donnadieu <omercado@tistatech.com>
2021-11-17 15:26:56 -0500

    Added new test for spec/services/hearing_time_service_spec.rb

---
+++ spec/support/timezone.rb
@@ -0,0 +1,6 @@
+# Configuration option to change Timezone at a per test basis via metadata attributes
+RSpec.configure do |config|
+  config.around :example, :tz do |example|
+    Time.use_zone(example.metadata[:tz]) { example.run }
+  end
+end
