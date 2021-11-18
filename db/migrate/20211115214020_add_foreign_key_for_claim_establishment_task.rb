14f29696f55ebda77dffc497582c913b0439de26
yoomlam <Dung.Lam1@va.gov>
2021-11-18 12:23:30 -0600

    resolve merge issue

--- db/migrate/20211115214020_add_foreign_key_for_claim_establishment_task.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyForClaimEstablishmentTask < Caseflow::Migration
-  def change
-    add_foreign_key "claim_establishments", "dispatch_tasks", column: "task_id", validate: false
-  end
-end
--- db/migrate/20211115214040_validate_foreign_key_for_claim_establishment_task.rb
+++
@@ -1,5 +0,0 @@
-class ValidateForeignKeyForClaimEstablishmentTask < ActiveRecord::Migration[5.2]
-  def change
-    validate_foreign_key "claim_establishments", column: "task_id"
-  end
-end
