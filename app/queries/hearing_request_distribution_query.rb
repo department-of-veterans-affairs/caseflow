0007f9b204f57c6524121be886f02bbc1cec3ad1
Matt Wagner <mattwagner@navapbc.com>
2021-11-18 12:56:44 -0500

    Change table alias from t1 to distribution_task

--- app/queries/hearing_request_distribution_query.rb
+++ app/queries/hearing_request_distribution_query.rb
@@ -80,11 +80,11 @@ class HearingRequestDistributionQuery
     def with_assigned_distribution_task_sql
       # both `appeal_type` and `appeal_id` necessary due to composite index
       <<~SQL
-        INNER JOIN tasks AS t1
-        ON t1.appeal_type = 'Appeal'
-        AND t1.appeal_id = appeals.id
-        AND t1.type = 'DistributionTask'
-        AND t1.status = 'assigned'
+        INNER JOIN tasks AS distribution_task
+        ON distribution_task.appeal_type = 'Appeal'
+        AND distribution_task.appeal_id = appeals.id
+        AND distribution_task.type = 'DistributionTask'
+        AND distribution_task.status = 'assigned'
       SQL
     end
 
@@ -98,7 +98,7 @@ class HearingRequestDistributionQuery
     def exceeding_affinity_threshold
       joins(with_assigned_distribution_task_sql)
         .where(hearings: { disposition: "held" })
-        .where("t1.assigned_at <= ?", Constants::DISTRIBUTION["hearing_case_affinity_days"].days.ago)
+        .where("distribution_task.assigned_at <= ?", Constants::DISTRIBUTION["hearing_case_affinity_days"].days.ago)
     end
 
     # Historical note: We formerly had not_tied_to_any_active_judge until CASEFLOW-1928,
