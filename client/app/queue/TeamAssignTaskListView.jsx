8938c1248294ccbded74dec5a9019b43d498c0f2
Brian Gantick <briangantick@navapbc.com>
2021-11-29 11:33:57 -0500

    Clean up data dump for readability

--- client/app/queue/TeamAssignTaskListView.jsx
+++ client/app/queue/TeamAssignTaskListView.jsx
@@ -63,7 +63,7 @@ class TeamAssignTaskListView extends React.PureComponent {
           <h1>Assign {unassignedTasksCount} Cases{(userCssId === targetUserCssId) ? '' : ` for ${targetUserCssId}`}</h1>
           <QueueOrganizationDropdown organizations={organizations} />
         </div>
-        { JSON.stringify(attorneysOfJudge) }
+        <pre>{ JSON.stringify(attorneysOfJudge, null, 2) }</pre>
         {!userIsCamoEmployee &&
           <div className="usa-width-one-fourth">
             <ul className="usa-sidenav-list">
