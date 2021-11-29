8a9f16e584107dbc71f1a99fa7c176b4496037e5
Jeremy Curcio <jcurcio@tistatech.com>
2021-11-29 08:53:51 -0500

    Remove extra stuff from custom tab models

--- app/models/queue_tabs/bva_intake_completed_tab.rb
+++ app/models/queue_tabs/bva_intake_completed_tab.rb
@@ -9,16 +9,8 @@ class BvaIntakeCompletedTab < QueueTab
     COPY::ORGANIZATIONAL_QUEUE_COMPLETED_TAB_TITLE
   end
 
-  def self.tab_name
-    Constants.QUEUE_CONFIG.COMPLETED_TASKS_TAB_NAME
-  end
-
   def description
     format(COPY::ORGANIZATIONAL_QUEUE_PAGE_BVA_COMPLETED_TASKS_DESCRIPTION, assignee.name)
-  end
-
-  def tasks
-    Task.includes(*task_includes).visible_in_queue_table_view.where(assigned_to: assignee).active
   end
 
   def column_names
