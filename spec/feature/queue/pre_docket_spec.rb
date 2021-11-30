8132562e9a88e3f932f90a68cbb3dbeff2da2633
Brian Gantick <briangantick@navapbc.com>
2021-11-29 19:25:21 -0500

    Only add assign to VISN action if FT is enabled (#16981)
    Resolves [CASEFLOW-2588](https://vajira.max.gov/browse/CASEFLOW-2588)
    
    ### Description
    Hides "Assign to VISN" action unless `visn_predocket_workflow` feature toggle is enabled
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly
    
    ### Testing Plan
    1. As a VHA PO user, visit an appeal that has been assigned by CAMO
    2. Ensure the "Assign to VISN" action doesn't show up in the action dropdown.
    3. Add the `visn_predocket_workflow` feature toggle and revisit the appeal. The "Assign to VISN" action should show up in the action dropdown.
    
    ### User Facing Changes
    
     BEFORE|AFTER
     ---|---
    <img width="1474" alt="Screen Shot 2021-11-22 at 8 37 03 AM" src="https://user-images.githubusercontent.com/640182/142871300-b19e29bf-79ed-402a-bbe1-3a48e39071be.png">|<img width="1460" alt="Screen Shot 2021-11-22 at 8 37 42 AM" src="https://user-images.githubusercontent.com/640182/142871264-60c4dd93-c1ed-44b3-89f8-054320958cbd.png">

--- spec/feature/queue/pre_docket_spec.rb
+++ spec/feature/queue/pre_docket_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "Pre-Docket intakes", :all_dbs do
   before do
     FeatureToggle.enable!(:vha_predocket_workflow)
     FeatureToggle.enable!(:vha_predocket_appeals)
+    FeatureToggle.enable!(:visn_predocket_workflow)
     FeatureToggle.enable!(:docket_vha_appeals)
     bva_intake.add_user(bva_intake_user)
     camo.add_user(camo_user)
@@ -16,6 +17,7 @@ RSpec.feature "Pre-Docket intakes", :all_dbs do
   after do
     FeatureToggle.disable!(:vha_predocket_workflow)
     FeatureToggle.disable!(:vha_predocket_appeals)
+    FeatureToggle.disable!(:visn_predocket_workflow)
     FeatureToggle.disable!(:docket_vha_appeals)
   end
 
