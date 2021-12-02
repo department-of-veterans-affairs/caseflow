ab0023f8b9b6ec43db117ed4e4d4143b6ec36832
odonnadieu <87207304+odonnadieu@users.noreply.github.com>
2021-12-02 14:45:18 -0500

    Pagination Prep Hearings APP (#16994)
    ### Description
    This is a refactor of the `HearingDayRange` mainly of `#upcoming_days_for_judge` making the Hearings index page for judges substantially more efficient. I have also refactored the main method used for all non-vso, non-judge users.
    
    ### Acceptance Criteria
    - [ ] Code compiles correctly
    - [ ] The Hearings index page /hearings/schedule should load for all users (VSO, judges and all the rest)
    
    ### Testing Plan
    1. Shadow user BVARERDMAN
    2. Go to /hearings/schedule The page should load the HearingDays
    3. Shadow user BILLIE_VSO
    4. Go to /hearings/schedule The page should load the HearingDays
    5. Shadow user BVASYELLOW
    6. Go to /hearings/schedule The page should load the HearingDays
    
    In addition to testing it locally I wrote a script which I ran in production to verified that judges are getting the same hearings with the new query. https://hackmd.io/8Q3IjHWbR4WR8OUyCBdYlA

--- app/services/hearing_day_range.rb
+++ app/services/hearing_day_range.rb
@@ -12,48 +12,41 @@ class HearingDayRange
   end
 
   def load_days
-    query = if regional_office.nil?
-              HearingDay.where(
-                "DATE(scheduled_for) between ? and ?", start_date, end_date
-              )
-            elsif [HearingDay::REQUEST_TYPES[:central], HearingDay::REQUEST_TYPES[:virtual]].include?(regional_office)
-              HearingDay.where(
-                "request_type = ? and DATE(scheduled_for) between ? and ? and regional_office IS NULL",
-                regional_office, # regional_office stores the hearing request type in this case
-                start_date,
-                end_date
-              )
-            else
-              HearingDay.where(
-                "regional_office = ? and DATE(scheduled_for) between ? and ?",
-                regional_office,
-                start_date,
-                end_date
-              )
-            end
+    hearing_days_in_range = HearingDay
+      .in_range(start_date, end_date)
+      .includes(:judge)
 
-    query.includes(:judge)
+    if regional_office.nil?
+      hearing_days_in_range
+    # regional_office stores the hearing request type in this case
+    elsif central_or_virtual?(regional_office)
+      hearing_days_in_range.where(
+        request_type: regional_office,
+        regional_office: nil
+      )
+    else
+      hearing_days_in_range.where(regional_office: regional_office)
+    end
   end
 
   def upcoming_days_for_judge(user)
-    days_in_range = hearing_days_in_range
+    hearing_days_in_range = HearingDay
+      .in_range(start_date, end_date)
+
     vacols_hearings = HearingRepository.fetch_hearings_for_parents_assigned_to_judge(
-      days_in_range.first(1000).pluck(:id), user
+      hearing_days_in_range.first(1000).pluck(:id), user
     )
 
-    days_in_range.select do |hearing_day|
-      self.class.hearing_day_for_judge?(hearing_day, user) || !vacols_hearings[hearing_day.id.to_s].nil?
-    end
+    hearing_days_in_range.for_judge_schedule(user, vacols_hearings.keys)
   end
 
   def upcoming_days_for_vso_user(user)
-    days_in_range = hearing_days_in_range
+    hearing_days_in_range = HearingDay
+      .in_range(start_date, end_date)
+      .includes(:judge, hearings: [appeal: [tasks: :assigned_to]])
+    ama_days = ama_days_for_vso_user(hearing_days_in_range, user)
 
-    ama_days = days_in_range.select do |hearing_day|
-      self.class.ama_hearing_day_for_vso_user?(hearing_day, user)
-    end
-
-    remaining_days = days_in_range.where.not(id: ama_days.pluck(:id)).order(
+    remaining_days = hearing_days_in_range.where.not(id: ama_days.pluck(:id)).order(
       Arel.sql(
         "CASE WHEN regional_office = '#{user.regional_office}' THEN 1 ELSE 2 END, "\
         "scheduled_for"
@@ -73,6 +66,12 @@ class HearingDayRange
     end
 
     ama_days + vacols_days
+  end
+
+  def ama_days_for_vso_user(days_range, user)
+    days_range.select do |hearing_day|
+      self.class.ama_hearing_day_for_vso_user?(hearing_day, user)
+    end
   end
 
   def load_days_for_user(user)
@@ -121,10 +120,6 @@ class HearingDayRange
   class << self
     def open_hearing_day?(hearing_day, scheduled_hearings)
       scheduled_hearings.size < hearing_day.total_slots
-    end
-
-    def hearing_day_for_judge?(hearing_day, user)
-      hearing_day.judge == user || hearing_day.hearings.any? { |hearing| hearing.judge == user }
     end
 
     def ama_hearing_day_for_vso_user?(hearing_day, user)
@@ -184,8 +179,10 @@ class HearingDayRange
     end
   end
 
-  def hearing_days_in_range
-    HearingDay.includes(:judge, hearings: [appeal: [tasks: :assigned_to]])
-      .where("DATE(scheduled_for) between ? and ?", start_date, end_date)
+  def central_or_virtual?(regional_office)
+    [
+      HearingDay::REQUEST_TYPES[:central],
+      HearingDay::REQUEST_TYPES[:virtual]
+    ].include?(regional_office)
   end
 end
