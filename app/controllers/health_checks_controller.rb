b33cd5c7fed50cdb45e1fc3ce902d743f3dd1d98
Joel Robinson <jrobinson@tistatech.com>
2021-12-03 16:01:09 -0600

    Refactor.

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -17,10 +17,9 @@ class HealthChecksController < ActionController::Base
   private
 
   def migration_status
-    migrations = []
-    ActiveRecord::Base.connection.migration_context.migrations_status.map do |status, version, name|
-      migrations << { status: status, version: version, name: name } unless status == "up"
-    end
+    migrations = ActiveRecord::Base.connection.migration_context.migrations_status.map do |status, version, name|
+      { status: status, version: version, name: name } unless status == "up"
+    end.compact
     { pending_migrations: migrations.any?, migrations: migrations }
   end
 end
