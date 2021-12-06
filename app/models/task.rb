ae7f0919e1bb52565b03b0cd36d477a0183e39a0
Jeremy Curcio <jeremy1026@gmail.com>
2021-12-06 10:15:20 -0500

    Add BVA Intake Queue (#16993)
    Resolves CASEFLOW-2276
    
    ### Description
    Adds new custom BVA Intake Queue to Caseflow.
    
    ### Acceptance Criteria
    - [ ] BVA Intake org has a custom queue which includes a tab to show all cases in the pre-docket workflow
    - [ ] The new tab is is given the following columns:
             Case Details
              Task owner (e.g. which organization currently has the open tasks)
              Tasks
              Days since last status change
              Total Days

--- app/models/task.rb
+++ app/models/task.rb
@@ -404,6 +404,14 @@ class Task < CaseflowRecord
     (timed_hold_task&.timer_end_time&.to_date &.- timed_hold_task&.timer_start_time&.to_date)&.to_i
   end
 
+  def calculated_last_change_duration
+    (Time.zone.today - updated_at&.to_date)&.to_i
+  end
+
+  def calculated_duration_from_board_intake
+    (Time.zone.today - created_at&.to_date)&.to_i
+  end
+
   def update_task_type(params)
     multi_transaction do
       new_branch_task = first_ancestor_of_type.create_twin_of_type(params)
