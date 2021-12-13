d6665f6a620099e246012a7a068bbf171f119b9c
yoomlam <Dung.Lam1@va.gov>
2021-12-13 15:01:26 -0600

    add links to alert

--- app/services/open_tasks_with_parent_not_on_hold.rb
+++ app/services/open_tasks_with_parent_not_on_hold.rb
@@ -2,9 +2,6 @@
 
 ##
 # See https://query.prod.appeals.va.gov/question/309-parent-tasks-not-on-hold-with-open-children-tasks
-# For InformalHearingPresentationTask, https://vajira.max.gov/browse/CASEFLOW-2499
-# For HearingTask with a parent assigned DistributionTask, https://dsva.slack.com/archives/C3EAF3Q15/p1633041954109500
-# For NoShowHearingTask, https://vajira.max.gov/browse/CASEFLOW-2558
 
 class OpenTasksWithParentNotOnHold < DataIntegrityChecker
   def call
@@ -14,6 +11,7 @@ class OpenTasksWithParentNotOnHold < DataIntegrityChecker
                     "task".pluralize(suspect_tasks.count) +
                     " with non-on_hold parent task"
       add_to_report "Counts: #{suspect_tasks.group(:type, 'parents_tasks.type', 'parents_tasks.status').count}"
+      add_to_report ONGOING_INVESTIGATIONS
     end
   end
 
@@ -26,4 +24,10 @@ class OpenTasksWithParentNotOnHold < DataIntegrityChecker
   def open_tasks_with_parent_not_on_hold
     Task.open.joins(:parent).includes(:parent).where.not(parents_tasks: { status: :on_hold })
   end
+
+  ONGOING_INVESTIGATIONS = %(
+    For InformalHearingPresentationTask, https://vajira.max.gov/browse/CASEFLOW-2499
+    For HearingTask with a parent assigned DistributionTask, https://dsva.slack.com/archives/C3EAF3Q15/p1633041954109500
+    For NoShowHearingTask, https://vajira.max.gov/browse/CASEFLOW-2558
+  )
 end
