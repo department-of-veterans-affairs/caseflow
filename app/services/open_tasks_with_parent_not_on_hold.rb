25a78d0e1d0337061259113b848d78f6068177bf
yoomlam <Dung.Lam1@va.gov>
2021-12-13 15:59:33 -0600

    move parent.closed_at check

--- app/services/open_tasks_with_parent_not_on_hold.rb
+++ app/services/open_tasks_with_parent_not_on_hold.rb
@@ -5,12 +5,14 @@
 
 class OpenTasksWithParentNotOnHold < DataIntegrityChecker
   def call
-    suspect_tasks = open_tasks_with_parent_not_on_hold
-    if suspect_tasks.count > 0
-      add_to_report "#{suspect_tasks.count} open " +
-                    "task".pluralize(suspect_tasks.count) +
-                    " with non-on_hold parent task"
-      add_to_report "Counts: #{suspect_tasks.group(:type, 'parents_tasks.type', 'parents_tasks.status').count}"
+    task_ids = open_tasks_with_parent_not_on_hold.pluck(:id) - ignored_tasks_with_closed_root_task_parent.pluck(:id)
+    if task_ids.count > 0
+      add_to_report "#{task_ids.count} open " +
+                    "task".pluralize(task_ids.count) +
+                    " with a non-on_hold parent task (ignoring TrackVeteranTask and *MailTasks)"
+      tasks_with_parents = Task.where(id: task_ids).joins(:parent).includes(:parent)
+      grouped_suspect_tasks = tasks_with_parents.group(:type, "parents_tasks.type", "parents_tasks.status").count
+      add_to_report "Counts: #{grouped_suspect_tasks.entries.map(&:to_s).join("\n")}"
       add_to_report ONGOING_INVESTIGATIONS
     end
   end
@@ -30,4 +32,17 @@ class OpenTasksWithParentNotOnHold < DataIntegrityChecker
     For HearingTask with a parent assigned DistributionTask, https://dsva.slack.com/archives/C3EAF3Q15/p1633041954109500
     For NoShowHearingTask, https://vajira.max.gov/browse/CASEFLOW-2558
   )
+
+  # It's acceptable to have a closed RootTask parent for:
+  # - MailTasks -- https://dsva.slack.com/archives/CJL810329/p1634239591067100?thread_ts=1634224678.055200&cid=CJL810329
+  # - TrackVeteranTask -- https://dsva.slack.com/archives/CJL810329/p1634581182080100?thread_ts=1634553075.073600&cid=CJL810329
+  IGNORED_TASKS_WITH_CLOSED_ROOTTASK_PARENT = [
+    *MailTask.descendants.map(&:name),
+    "TrackVeteranTask"
+  ].freeze
+
+  def ignored_tasks_with_closed_root_task_parent
+    Task.open.joins(:parent).includes(:parent).where(type: IGNORED_TASKS_WITH_CLOSED_ROOTTASK_PARENT)
+      .where(parents_tasks: { type: "RootTask", status: %w[completed cancelled] })
+  end
 end
