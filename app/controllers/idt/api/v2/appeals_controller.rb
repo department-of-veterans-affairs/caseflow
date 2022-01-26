926da0a87937635c442e741d21c97a2c01f614da
jshields13 <jeremy.shields@va.gov>
2022-01-26 12:21:21 -0500

    CASEFLOW-2893 adde dthe endpoints for documents list and single document and added them to the routs.rb

--- app/controllers/idt/api/v2/appeals_controller.rb
+++ app/controllers/idt/api/v2/appeals_controller.rb
@@ -29,6 +29,22 @@ class Idt::Api::V2::AppealsController < Idt::Api::V1::BaseController
     end
   end
 
+  def appeal_documents
+    AppealView.find_by(appeal: appeal, user: current_user).update!(last_viewed_at: Time.zone.now)
+    MetricsService.record "Get appeal #{appeal_id} document data" do
+      render json: {
+        appealDocuments: documents,
+        annotations: annotations,
+        manifestVbmsFetchedAt: manifest_vbms_fetched_at,
+        manifestVvaFetchedAt: manifest_vva_fetched_at
+      }
+    end
+  end
+
+  def appeals_single_document
+    render json: documents.select { |doc| doc["id"] == document_id.to_i }
+  end
+
   private
 
   # :reek:DuplicateMethodCall { allow_calls: ['result.extra'] }
@@ -61,5 +77,48 @@ class Idt::Api::V2::AppealsController < Idt::Api::V1::BaseController
 
   def appeal_id
     params[:appeal_id]
+  end
+
+  def document_id
+    params[:document_id]
+  end
+
+  def annotations
+    Annotation.where(document_id: document_ids).map(&:to_hash)
+  end
+
+  def document_ids
+    @document_ids ||= appeal.document_fetcher.find_or_create_documents!.pluck(:id)
+  end
+
+  delegate :manifest_vbms_fetched_at, :manifest_vva_fetched_at, to: :appeal
+
+  def documents
+    # Create a hash mapping each document_id that has been read to true
+    read_documents_hash = current_user.document_views.where(document_id: document_ids)
+      .each_with_object({}) do |document_view, object|
+      object[document_view.document_id] = true
+    end
+
+    tags_by_doc_id = load_tags_by_doc_id
+
+    @documents = appeal.document_fetcher.find_or_create_documents!.map do |document|
+      document.to_hash.tap do |object|
+        object[:opened_by_current_user] = read_documents_hash[document.id] || false
+        object[:tags] = tags_by_doc_id[document.id].to_a
+      end
+    end
+  end
+
+  def load_tags_by_doc_id
+    tags_by_doc_id = Hash[document_ids.map { |key, _| [key, Set[]] }]
+    Tag.includes(:documents_tags).where(documents_tags: { document_id: document_ids }).each do |tag|
+      # tag.documents_tags returns extraneous documents outside document_ids, so
+      # only capture tags associated with docs associated with document_ids
+      (tag.documents_tags.pluck(:document_id) & document_ids).each do |doc_id|
+        tags_by_doc_id[doc_id].add(tag)
+      end
+    end
+    tags_by_doc_id
   end
 end
