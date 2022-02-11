c207d463bfcc7541a66c571b2808d926c6ba6c66
j-n-t-l <611441@bah.com>
2022-02-11 01:04:08 -0500

    Revert "changed syntax"
    This reverts commit 56b3ee52261ce0a4e64fafbfbaa39a10dae0198a.

--- app/models/hearings/hearing_email_recipient.rb
+++ app/models/hearings/hearing_email_recipient.rb
@@ -23,7 +23,7 @@ class HearingEmailRecipient < CaseflowRecord
 
   include BelongsToPolymorphicHearingConcern
   belongs_to_polymorphic_hearing(:hearing)
-  belongs_to :appeal, foreign_key: :appeal_id
+  belongs_to :appeal
 
   def reminder_sent_at
     email_events
