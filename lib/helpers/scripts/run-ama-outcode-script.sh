dfb31f8b96cfdcae1b36e4210b1b23b898acbb0f
jshields13 <jeremy.shields@va.gov>
2022-03-09 13:54:54 -0500

    Updated scripts to have full path to linux ruby rails

--- lib/helpers/scripts/run-ama-outcode-script.sh
+++ lib/helpers/scripts/run-ama-outcode-script.sh
@@ -1,4 +1,4 @@
-rails c << DONETOKEN
+cd /opt/caseflow-certification/src; bin/rails c << DONETOKEN
 x = WarRoom::Outcode.new
 x.ama_run("$1")
 DONETOKEN
--- lib/helpers/scripts/run-legacy-outcode-script.sh
+++ lib/helpers/scripts/run-legacy-outcode-script.sh
@@ -1,5 +1,4 @@
-rails c << DONETOKEN
+cd /opt/caseflow-certification/src; bin/rails c << DONETOKEN
 x = WarRoom::Outcode.new
 x.legacy_run("$1")
 DONETOKEN
-
