2847a386d1342ba7597030a97c3d52bae6364a48
jshields13 <jeremy.shields@va.gov>
2022-03-09 14:17:44 -0500

    changed paths

--- lib/helpers/scripts/duplicate_veteran_updater.sh
+++ lib/helpers/scripts/duplicate_veteran_updater.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 cd /opt/caseflow-certification/src; bin/rails c << DONETOKEN
-=======
-rails c << DONETOKEN
->>>>>>> master
 RequestStore[:current_user] = User.system_user
 
 dvc = dvc = WarRoom::OutcodeWithDuplicateVeteran.new
--- lib/helpers/scripts/run-legacy-outcode-script.sh
+++ lib/helpers/scripts/run-legacy-outcode-script.sh
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 cd /opt/caseflow-certification/src; bin/rails c << DONETOKEN
 x = WarRoom::Outcode.new
 x.legacy_run("$1")
 DONETOKEN
-=======
-rails c << DONETOKEN
-x = WarRoom::Outcode.new
-x.legacy_run("$1")
-DONETOKEN
-
->>>>>>> master
