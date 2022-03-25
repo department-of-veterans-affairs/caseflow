60466b580ee688d6cafbd9a3e610f938d0b3f391
sulak-j <justin.sulak@va.gov>
2022-03-25 12:58:01 -0500

    fixed code climate issues

--- app/models/organizations/education_emo.rb
+++ app/models/organizations/education_emo.rb
@@ -3,8 +3,7 @@
 # Executive Management Office inside Education
 
 class EducationEmo < Organization
-    def self.singleton
-        EducationEmo.first || EducationEmo.create(name: "EXECUTIVE MANAGEMENT OFFICE", url: "edu-emo")
-    end
-
+  def self.singleton
+    EducationEmo.first || EducationEmo.create(name: "EXECUTIVE MANAGEMENT OFFICE", url: "edu-emo")
+  end
 end
