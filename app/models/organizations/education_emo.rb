7cbc2046ac0056a3e2b5066b5508ff5f26a7d4cb
sulak-j <justin.sulak@va.gov>
2022-03-25 12:17:27 -0500

    Creating the EMO organization for Education

---
+++ app/models/organizations/education_emo.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Executive Management Office inside Education
+
+class EducationEmo < Organization
+    def self.singleton
+        EducationEmo.first || EducationEmo.create(name: "EXECUTIVE MANAGEMENT OFFICE", url: "edu-emo")
+    end
+
+end
