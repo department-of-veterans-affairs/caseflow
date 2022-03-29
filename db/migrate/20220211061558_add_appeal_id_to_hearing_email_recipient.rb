cf3cdd55604c0ccc8305f7f50d76a20f0612a7e7
j-n-t-l <611441@bah.com>
2022-03-29 14:35:24 -0400

    CASEFLOW-3704 added appeal type and appeal id to hearingemailrecipient through safe indexing

--- db/migrate/20220211061558_add_appeal_id_to_hearing_email_recipient.rb
+++
@@ -1,6 +0,0 @@
-class AddAppealIdToHearingEmailRecipient < ActiveRecord::Migration[5.2]
-  def change
-    add_foreign_key "hearing_email_recipients", "appeals", column: "appeal_id", validate: false
-    validate_foreign_key "hearing_email_recipients", column: "appeal_id"
-  end
-end
