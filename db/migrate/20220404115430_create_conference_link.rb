b275f1fe854451bb7982261f4066f374c777ae26
Lauren Berry <lberry@take2it.com>
2022-04-04 15:29:01 -0400

    Adding changes to create conference link table

---
+++ db/migrate/20220404115430_create_conference_link.rb
@@ -0,0 +1,20 @@
+class CreateConferenceLink < ActiveRecord::Migration[5.2]
+    def change
+        create_table :conference_link do |t|
+            t.belongs_to :hearing_day
+            t.bigint     :id, null: false
+            t.string     :alias
+            t.string     :alias_with_host
+            t.boolean    :conference_deleted, default: false, null: false
+            t.integer    :conference_id
+            t.datetime   :created_at, null: false 
+            t.bigint     :created_by_id, null: false
+            t.bigint     :hearing_day_id, null: false
+            t.string     :host_hearing_link, null: false 
+            t.integer    :host_pin, null: false
+            t.string     :host_pin_long, limit: 8, null: false  
+            t.datetime   :updated_at,
+            t.bigint     :update_by_id, null: false
+        end
+    end
+end
