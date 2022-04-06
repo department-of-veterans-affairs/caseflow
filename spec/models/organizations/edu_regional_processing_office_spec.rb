3a3840c2e1c980d7de463345658cc181103206e1
AimanK <Akaya005@fiu.edu>
2022-04-06 10:33:51 -0400

    Add additional RSpec test instance for RPO

--- spec/models/organizations/edu_regional_processing_office_spec.rb
+++ spec/models/organizations/edu_regional_processing_office_spec.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 describe EduRegionalProcessingOffice, :postgres do
-    let(:regional_processing_office) { VhaRegionalOffice.create!(name: "Regional Office", url: "regional-office") }
+    let(:regional_processing_office) { EduRegionalProcessingOffice.create!(name: "Regional Processing Office", url: "Regional Processing Office") }
+
+    describe ".create!" do
+        it "creates a Regional Processing Office" do
+          expect(regional_processing_office.name).to eq("Regional Processing Office")
+        end
+      end
   
   
     describe ".can_receive_task?" do
       let(:appeal) { create(:appeal) }
-      let(:doc_task) { create(:vha_document_search_task, appeal: appeal) }
+      let(:doc_task) { create(:education_document_search_task, appeal: appeal) }
   
       it "returns false because program offices should not have vha document search tasks assigned to them" do
         expect(regional_processing_office.can_receive_task?(doc_task)).to eq(false)
