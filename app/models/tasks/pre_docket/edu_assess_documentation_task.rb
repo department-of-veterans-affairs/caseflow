6f086ae906cd87a31e6812776ae8f3a6f240d403
Jonathan Tsang <tsang_jonathan@bah.com>
2022-04-11 11:40:57 -0400

    updated model and rspec

--- app/models/tasks/pre_docket/edu_assess_documentation_task.rb
+++ app/models/tasks/pre_docket/edu_assess_documentation_task.rb
@@ -8,41 +8,16 @@ class EduAssessDocumentationTask < Task
     validates :parent, presence: true,
                        on: :create
   
-    # Actions that can be taken on both organization and user tasks
-    DEFAULT_ACTIONS = [
-      # Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
-      # Constants.TASK_ACTIONS.READY_FOR_REVIEW.to_h
-    ].freeze
-  
-    RPO_ACTIONS = [
-      Constants.TASK_ACTIONS.EDU_REGIONAL_PROCESSING_OFFICE_RETURN_TO_EMO.to_h,
-      Constants.TASK_ACTIONS.EDU_READY_FOR_REVIEW.to_h
+    TASK_ACTIONS = [
+      # TODO
     ].freeze
 
     def available_actions(user)
       return [] unless assigned_to.user_has_access?(user)
   
-      task_actions = Array.new(DEFAULT_ACTIONS)
-  
-  
-      if assigned_to.is_a?(EduRegionalProcessingOffice)
-        task_actions.concat(RPO_ACTIONS)
-      end
-  
-      ## TODO: create "mark task in progress" task action function
-      if appeal.tasks.in_progress.none? { |task| task.is_a?(EduAssessDocumentationTask) }
-        task_actions.concat([Constants.TASK_ACTIONS.EDU_MARK_TASK_IN_PROGRESS.to_h].freeze)
-      end
-  
-      task_actions
+      TASK_ACTIONS
     end
-  
-    # def when_child_task_completed(child_task)
-    #   append_instruction(child_task.instructions.last) if child_task.assigned_to.is_a?(VhaRegionalOffice)
-  
-    #   super
-    # end
-  
+
     def self.label
       COPY::ASSESS_DOCUMENTATION_TASK_LABEL
     end
--- spec/models/tasks/edu_assess_documentation_task_spec.rb
+++ spec/models/tasks/edu_assess_documentation_task_spec.rb
@@ -7,18 +7,16 @@ describe EduAssessDocumentationTask, :postgres do
       describe "for regional processing office user" do
         let(:regional_processing_office) { EduRegionalProcessingOffice.create!(name: "Regional Processing Office", url: "Regional Processing Office") }
         let(:regional_processing_office_task) { create(:edu_assess_documentation_task, assigned_to: regional_processing_office) }
-  
+        
+        it "task should be assigned to RPO" do
+           expect(regional_processing_office_task.assigned_to).to eq(regional_processing_office)
+        end
+
         before { regional_processing_office.add_user(user) }
   
         subject { regional_processing_office_task.available_actions(user) }
   
-        available_actions = [
-        #   Constants.TASK_ACTIONS.TOGGLE_TIMED_HOLD.to_h,
-        #   Constants.TASK_ACTIONS.READY_FOR_REVIEW.to_h,
-          Constants.TASK_ACTIONS.EDU_REGIONAL_PROCESSING_OFFICE_RETURN_TO_EMO.to_h,
-          Constants.TASK_ACTIONS.EDU_READY_FOR_REVIEW.to_h,
-          Constants.TASK_ACTIONS.EDU_MARK_TASK_IN_PROGRESS.to_h
-        ]
+        available_actions = []
   
         it { is_expected.to eq available_actions }
       end
