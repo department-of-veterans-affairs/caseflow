211d315755a974903ce19287bb94b31dc7d67460
Matthew Thornton <Thornton_Matthew@bah.com>
2022-04-13 15:04:31 -0400

    Show tasks in assigned tab once EMO sends it to BVA Intake/RPO until it is docketed/canceled

--- app/models/queue_tabs/education_emo_assigned_tasks_tab.rb
+++ app/models/queue_tabs/education_emo_assigned_tasks_tab.rb
@@ -1,36 +1,47 @@
 # frozen_string_literal: true
 
 class EducationEmoAssignedTasksTab < QueueTab
-    validate :assignee_is_organization
+  validate :assignee_is_organization
 
-    attr_accessor :show_reader_link_column, :allow_bulk_assign
+  attr_accessor :show_reader_link_column, :allow_bulk_assign
 
-    def label
-      COPY::ORGANIZATIONAL_QUEUE_PAGE_ASSIGNED_TAB_TITLE
-    end
-
-    def self.tab_name
-      Constants.QUEUE_CONFIG.EDUCATION_ASSIGNED_TASKS_TAB_NAME
-    end
-
-    def description
-      format(COPY::USER_QUEUE_PAGE_ASSIGNED_TASKS_DESCRIPTION, assignee.name)
-    end
-
-    def tasks
-      # TODO: Adjust to meet AC
-      on_hold_task_children_and_timed_hold_parents
-    end
-
-    def column_names
-      [
-        Constants.QUEUE_CONFIG.COLUMNS.BADGES.name,
-        Constants.QUEUE_CONFIG.COLUMNS.CASE_DETAILS_LINK.name,
-        Constants.QUEUE_CONFIG.COLUMNS.TASK_TYPE.name,
-        Constants.QUEUE_CONFIG.COLUMNS.ISSUE_COUNT.name,
-        Constants.QUEUE_CONFIG.COLUMNS.DAYS_WAITING.name,
-        Constants.QUEUE_CONFIG.COLUMNS.APPEAL_TYPE.name,
-        Constants.QUEUE_CONFIG.COLUMNS.TASK_ASSIGNEE.name
-      ].compact
-    end
+  def label
+    COPY::ORGANIZATIONAL_QUEUE_PAGE_ASSIGNED_TAB_TITLE
   end
+
+  def self.tab_name
+    Constants.QUEUE_CONFIG.EDUCATION_ASSIGNED_TASKS_TAB_NAME
+  end
+
+  def description
+    format(COPY::USER_QUEUE_PAGE_ASSIGNED_TASKS_DESCRIPTION, assignee.name)
+  end
+
+  def assigned_tasks_ids_where_parent_has_been_docketed
+    closed_tasks
+      .select { |task| task.parent.status == Constants.TASK_STATUSES.completed }
+      .pluck(:id)
+  end
+
+  def tasks_emo_assigned_elsewhere
+    on_hold_tasks.map(&:id) + closed_tasks.completed.map(&:id)
+  end
+
+  def tasks
+    Task.includes(*task_includes).visible_in_queue_table_view.where(
+      id: (tasks_emo_assigned_elsewhere - assigned_tasks_ids_where_parent_has_been_docketed)
+    )
+  end
+
+  def column_names
+    [
+      Constants.QUEUE_CONFIG.COLUMNS.BADGES.name,
+      Constants.QUEUE_CONFIG.COLUMNS.CASE_DETAILS_LINK.name,
+      Constants.QUEUE_CONFIG.COLUMNS.TASK_TYPE.name,
+      Constants.QUEUE_CONFIG.COLUMNS.ISSUE_COUNT.name,
+      Constants.QUEUE_CONFIG.COLUMNS.DAYS_WAITING.name,
+      Constants.QUEUE_CONFIG.COLUMNS.APPEAL_TYPE.name,
+      Constants.QUEUE_CONFIG.COLUMNS.TASK_ASSIGNEE.name
+    ].compact
+  end
+end
