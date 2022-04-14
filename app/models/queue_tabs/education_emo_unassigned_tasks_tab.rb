a5d2ad6c12131e3971ef4024853db24830d6b856
sulak-j <justin.sulak@va.gov>
2022-04-14 18:54:23 -0500

    Removed whitespace from unassigned_tasks_tab.rb

--- app/models/queue_tabs/education_emo_unassigned_tasks_tab.rb
+++ app/models/queue_tabs/education_emo_unassigned_tasks_tab.rb
@@ -20,8 +20,6 @@ class EducationEmoUnassignedTasksTab < QueueTab
   # In each case, if the EMO task is assigned, then it should meet each use case for the unassigned tab
   def tasks
     Task.includes(*task_includes).visible_in_queue_table_view.where(assigned_to: assignee).active
-    #Task.includes(*task_includes).visible_in_queue_table_view.where(education_document_search_task.assigned)
-    
   end
 
   # Column names defined in each tab in education predocket due to different columns needed
