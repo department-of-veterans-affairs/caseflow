120035581027cc9f1ececaf6b713ec9538cc9f80
Matthew Thornton <Thornton_Matthew@bah.com>
2022-04-18 16:01:19 -0400

    Remove comments

--- app/models/tasks/pre_docket/education_document_search_task.rb
+++ app/models/tasks/pre_docket/education_document_search_task.rb
@@ -14,9 +14,6 @@ class EducationDocumentSearchTask < Task
   end
 
   TASK_ACTIONS = [
-    # commented out because the method is not yet created
-    # Constants.TASK_ACTIONS.EDUCATION_ASSIGN_TO_RPO.to_h,
-    # Constants.TASK_ACTIONS.EDUCATION_SEND_TO_BOARD_INTAKE.to_h
     Constants.TASK_ACTIONS.EMO_RETURN_TO_BOARD_INTAKE.to_h
   ].freeze
 
