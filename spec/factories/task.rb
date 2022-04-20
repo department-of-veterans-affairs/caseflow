30f3f184b383f40cd6eee9b62e3316b85124c464
Matthew Thornton <Thornton_Matthew@bah.com>
2022-04-20 16:58:05 -0400

    Fix edu preo-docket task factories

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -505,13 +505,14 @@ FactoryBot.define do
       end
 
       factory :education_document_search_task, class: EducationDocumentSearchTask do
-        parent { create(:pre_docket_task, appeal: appeal) }
+        parent { create(:pre_docket_task, appeal: appeal, assigned_to: BvaIntake.singleton) }
         assigned_to { EducationEmo.singleton }
         assigned_by { nil }
       end
 
       factory :education_assess_documentation_task, class: EducationAssessDocumentationTask do
         parent { create(:education_document_search_task, appeal: appeal) }
+        assigned_to { EduRegionalProcessingOffice.first }
         assigned_by { nil }
       end
 
