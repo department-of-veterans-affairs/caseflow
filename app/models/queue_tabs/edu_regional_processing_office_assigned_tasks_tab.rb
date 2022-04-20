19386146db937d9ff7798c52be7f4bdfcd2186a2
Matthew Thornton <Thornton_Matthew@bah.com>
2022-04-20 16:37:56 -0400

    Fix some string keys

--- app/models/queue_tabs/edu_regional_processing_office_assigned_tasks_tab.rb
+++ app/models/queue_tabs/edu_regional_processing_office_assigned_tasks_tab.rb
@@ -1,28 +1,25 @@
 # frozen_string_literal: true
 
 class EduRegionalProcessingOfficeAssignedTasksTab < QueueTab
-    validate :assignee_is_organization
-  
-    attr_accessor :show_reader_link_column, :allow_bulk_assign
-  
-    def label
-      COPY::EDU_REGIONAL_PROCESSING_OFFICE_ASSIGNED_TAB_TITLE
-    end
-  
-    def self.tab_name
-      Constants.QUEUE_CONFIG.EDU_REGIONAL_PROCESSING_OFFICE_ASSIGNED_TASKS_TAB_NAME
-    end
-  
-    def description
-      format(COPY::EDU_REGIONAL_PROCESSING_OFFICE_QUEUE_PAGE_ASSIGNED_TASKS_DESCRIPTION, assignee.name)
-    end
-  
-    def tasks
-        assigned_tasks()
-    end
-  
-    def column_names
-    EduRegionalProcessingOffice::COLUMN_NAMES
-    end
+  validate :assignee_is_organization
+
+  def label
+    COPY::ORGANIZATION_QUEUE_TABLE_TITLE
   end
-  
+
+  def self.tab_name
+    Constants.QUEUE_CONFIG.EDU_REGIONAL_PROCESSING_OFFICE_ASSIGNED_TASKS_TAB_NAME
+  end
+
+  def description
+    format(COPY::ORGANIZATIONAL_QUEUE_PAGE_ASSIGNED_TASKS_DESCRIPTION, assignee.name)
+  end
+
+  def tasks
+    assigned_tasks
+  end
+
+  def column_names
+    EduRegionalProcessingOffice::COLUMN_NAMES
+  end
+end
