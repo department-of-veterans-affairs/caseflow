a75a366ce54aeab0a803dfa48dcc769bd706f616
AimanK <Akaya005@fiu.edu>
2022-04-21 10:51:31 -0400

    Clean up formatting for EMO Org completed_tasks_tab and add edit to tasks method

--- app/models/queue_tabs/education_emo_completed_tasks_tab.rb
+++ app/models/queue_tabs/education_emo_completed_tasks_tab.rb
@@ -1,31 +1,35 @@
 # frozen_string_literal: true
 
 class EducationEMOCompletedTasksTab < QueueTab
-    validate :assignee_is_organization
-  
-    def label
-      COPY::EDUCATION_ORGANIZATIONAL_QUEUE_PAGE_COMPLETED_TAB_TITLE
-    end
-  
-    def self.tab_name
-      Constants.QUEUE_CONFIG.EDUCATION_COMPLETED_TASKS_TAB_NAME
-    end
-  
-    def description
-       COPY::EDUCATION_EMO_QUEUE_PAGE_COMPLETED_TASKS_DESCRIPTION
-    end
+  validate :assignee_is_organization
 
-    def task_ids_assigned_to_bva
-        closed_tasks.map(&:id)
-    end
-  
-    def tasks
-      Task.includes(*task_includes).visible_in_queue_table_view.where(
-        id: (task_ids_assigned_to_bva)
-      ).completed
-    end
-  
-    def column_names
-      EducationEmo::COLUMN_NAMES
-    end
+  def label
+    COPY::EDUCATION_ORGANIZATIONAL_QUEUE_PAGE_COMPLETED_TAB_TITLE
   end
+
+  def self.tab_name
+    Constants.QUEUE_CONFIG.EDUCATION_COMPLETED_TASKS_TAB_NAME
+  end
+  
+  def description
+    COPY::EDUCATION_EMO_QUEUE_PAGE_COMPLETED_TASKS_DESCRIPTION
+  end
+  
+  def task_ids_where_parent_has_been_closed
+    closed_tasks.select { |task| task.parent.closed? }.pluck(:id)
+  end
+    
+  def task_ids_assigned_to_bva
+    closed_tasks.map(&:id)
+  end
+  
+  def tasks
+    Task.includes(*task_includes).visible_in_queue_table_view.where(
+      id: (task_ids_assigned_to_bva + task_ids_where_parent_has_been_closed)
+    ).completed
+  end
+  
+  def column_names
+    EducationEmo::COLUMN_NAMES
+  end
+end
