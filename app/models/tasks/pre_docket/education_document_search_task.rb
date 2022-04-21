18c2c3dcf8c824cf7a7612d1dc9d319b1c5e87f7
jtsangVA <98970951+jtsangVA@users.noreply.github.com>
2022-04-21 13:51:14 -0400

    Jonathan/caseflow 4870 emo send to bva intake (#17188)
    * initial commit
    
    * removed 'VHA' from 'VHA_COMPLETE_TASK_MODAL_TITLE'
    
    * undoing last commit
    
    * new task action & modal creation
    
    * modal updates
    
    * updated text
    
    * rspec test
    
    * renaming VHA specific vars
    
    * require source if 'other' was selected
    
    * fixed lint warning
    
    * rspec updates
    
    * updated test
    
    Co-authored-by: Jonathan Tsang <tsang_jonathan@bah.com>

--- app/models/tasks/pre_docket/education_document_search_task.rb
+++ app/models/tasks/pre_docket/education_document_search_task.rb
@@ -16,7 +16,7 @@ class EducationDocumentSearchTask < Task
   TASK_ACTIONS = [
     # commented out because the method is not yet created
     # Constants.TASK_ACTIONS.EDUCATION_ASSIGN_TO_RPO.to_h,
-    # Constants.TASK_ACTIONS.EDUCATION_SEND_TO_BOARD_INTAKE.to_h
+    Constants.TASK_ACTIONS.EMO_SEND_TO_BOARD_INTAKE_FOR_REVIEW.to_h
   ].freeze
 
   def self.label
