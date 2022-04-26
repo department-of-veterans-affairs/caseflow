6f547ae3eb7417849d9163a8d1126bd2c4534fb2
sulak-j <justin.sulak@va.gov>
2022-04-26 17:36:46 -0500

    added more spec tests

--- spec/models/queue_tabs/education_rpo_completed_tasks_tab_spec.rb
+++ spec/models/queue_tabs/education_rpo_completed_tasks_tab_spec.rb
@@ -27,11 +27,36 @@ describe EducationRpoCompletedTasksTab, :postgres do
       let!(:assignee_completed_tasks) do
         create_list(:education_assess_documentation_task, 4, :completed, assigned_to: assignee)
       end
+      let!(:assignee_assigned_tasks) do
+        create_list(:education_assess_documentation_task, 4, :assigned, assigned_to: assignee)
+      end
 
       it "returns completed tasks" do
-        expect(subject).to include(
-          assignee_completed_tasks
-        )
+        expect(subject.to_a.difference(assignee_completed_tasks).any?).to eq false
+      end
+
+      it "does not return tasks that are not completed" do
+        expect(subject.to_a.difference(assignee_assigned_tasks).any?).to eq true
+      end
+    end
+
+    context "when the tasks are currenlty assigned to the assignee" do
+      let!(:assignee_assigned_tasks) do
+        create_list(:education_assess_documentation_task, 4, :assigned, assigned_to: assignee)
+      end
+
+      it "does not return assigned tasks" do
+        expect(subject.to_a.difference(assignee_assigned_tasks).any?).to eq false
+      end
+    end
+
+    context "when there are tasks in progress by the assignee" do
+      let!(:assignee_in_progress_tasks) do
+        create_list(:education_assess_documentation_task, 4, :in_progress, assigned_to: assignee)
+      end
+
+      it "does not return in progress tasks" do
+        expect(subject.to_a.difference(assignee_in_progress_tasks).any?).to eq false
       end
     end
   end
