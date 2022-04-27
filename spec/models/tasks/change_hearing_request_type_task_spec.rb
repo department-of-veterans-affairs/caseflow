a81784757a8c3c68277e68d50cc5778c7eaec950
j-n-t-l <611441@bah.com>
2022-04-27 11:53:45 -0400

    CASEFLOW-5066 reverted some old changes, moved update_to_new_poa to change...task, added tests for update_to_new_poa

--- spec/models/tasks/change_hearing_request_type_task_spec.rb
+++ spec/models/tasks/change_hearing_request_type_task_spec.rb
@@ -295,4 +295,98 @@ describe ChangeHearingRequestTypeTask do
       end
     end
   end
+  describe "update to new poa" do
+    let!(:appeal) { create(:appeal) }
+    let!(:root_task) { create(:root_task, appeal: appeal) }
+
+    subject { TrackVeteranTask.sync_tracking_tasks(appeal) }
+
+    context "When former represenative VSO is assigned non-Tracking tasks" do
+      let!(:old_vso) { create(:vso, name: "Remember Korea") }
+      let!(:new_vso) { create(:vso) }
+      let!(:root_task) { create(:root_task, appeal: appeal) }
+    end
+    let!(:change_type_task) do
+      create(
+        :change_hearing_request_type_task,
+        parent: root_task,
+        appeal: root_task.appeal,
+        assigned_to: old_vso
+      )
+    end
+    before { allow_any_instance_of(Appeal).to receive(:representatives).and_return([new_vso]) }
+
+    it "cancels all tasks of former VSO" do
+      subject
+      expect(ihp_org_task.reload.status).to eq(Constants.TASK_STATUSES.cancelled)
+    end
+    it "makes duplicates of active tasks for new representation" do
+      expect(new_vso.tasks.count).to eq(0)
+      expect(subject).to eq([1, 2])
+      expect(new_vso.tasks.count).to eq(2)
+    end
+    context "when the appeal has no VSOs" do
+      before { allow_any_instance_of(Appeal).to receive(:representatives).and_return([]) }
+
+      context "when there are no existing TrackVeteranTasks" do
+        it "does not create or cancel any TrackVeteranTasks" do
+          task_count_before = TrackVeteranTask.count
+
+          expect(subject).to eq([0, 0])
+          expect(TrackVeteranTask.count).to eq(task_count_before)
+        end
+      end
+
+      context "when there is an existing open TrackVeteranTasks" do
+        let(:vso) { create(:vso) }
+        let!(:tracking_task) { create(:track_veteran_task, appeal: appeal, assigned_to: vso) }
+
+        it "cancels old TrackVeteranTask, does not create any new tasks" do
+          active_task_count_before = TrackVeteranTask.open.count
+
+          expect(subject).to eq([0, 1])
+          expect(TrackVeteranTask.open.count).to eq(active_task_count_before - 1)
+        end
+      end
+    end
+    context "when the appeal has two VSOs" do
+      let(:representing_vsos) { create_list(:vso, 2) }
+      before { allow_any_instance_of(Appeal).to receive(:representatives).and_return(representing_vsos) }
+
+      context "when there are no existing TrackVeteranTasks" do
+        it "creates 2 new TrackVeteranTasks and 2 IHP Tasks" do
+          task_count_before = TrackVeteranTask.count
+
+          expect(subject).to eq([2, 0])
+          expect(TrackVeteranTask.count).to eq(task_count_before + 2)
+        end
+      end
+
+      context "when there is an existing open TrackVeteranTasks for a different VSO" do
+        before do
+          create(:track_veteran_task, appeal: appeal, assigned_to: create(:vso))
+        end
+
+        it "cancels old TrackVeteranTask, creates 2 new TrackVeteran and 2 new IHP tasks" do
+          expect(subject).to eq([2, 1])
+        end
+      end
+
+      context "when there are already TrackVeteranTasks for both VSOs" do
+        before do
+          representing_vsos.each do |vso|
+            create(:track_veteran_task, appeal: appeal, assigned_to: vso)
+          end
+        end
+
+        it "does not create or cancel any TrackVeteranTasks" do
+          task_count_before = TrackVeteranTask.count
+
+          expect(subject).to eq([0, 0])
+          expect(TrackVeteranTask.count).to eq(task_count_before)
+        end
+      end
+    end
+    
+  end
 end
