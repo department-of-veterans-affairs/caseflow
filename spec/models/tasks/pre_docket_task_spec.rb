e63dd7fe16d7667c3e9ed61bbf4c868d2782f4fb
Matthew Thornton <Thornton_Matthew@bah.com>
2022-05-03 13:41:20 -0400

    Remove commented out test assertions

--- spec/models/tasks/pre_docket_task_spec.rb
+++ spec/models/tasks/pre_docket_task_spec.rb
@@ -36,14 +36,12 @@ describe PreDocketTask, :postgres do
     context "When the user is a BVA Intake user" do
       let(:user) { bva_intake_user }
 
-      # it { is_expected.to eq PreDocketTask::TASK_ACTIONS }
       it { is_expected.to eq pre_docket_task.available_actions(user) }
     end
 
     context "When the user is a BVA Intake admin" do
       let(:user) { bva_intake_admin_user }
 
-      # it { is_expected.to eq PreDocketTask::TASK_ACTIONS }
       it { is_expected.to eq pre_docket_task.available_actions(user) }
     end
 
