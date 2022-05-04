bbbc07284c12a6c9c45037ca8d26fea58d03c041
Matthew Thornton <Thornton_Matthew@bah.com>
2022-05-04 16:01:54 -0400

    Update #available_actions spec test

--- spec/models/tasks/education_assess_documentation_task_spec.rb
+++ spec/models/tasks/education_assess_documentation_task_spec.rb
@@ -2,11 +2,14 @@
 
 describe EducationAssessDocumentationTask, :postgres do
   let(:user) { create(:user) }
+  let(:regional_processing_office) { create(:edu_regional_processing_office) }
 
-  context "#available_actions" do
-    describe "for regional processing office user" do
-      let(:regional_processing_office) { create(:edu_regional_processing_office) }
+  before { regional_processing_office.add_user(user) }
 
+  describe "#available_actions" do
+    subject { regional_processing_office_task.available_actions(user) }
+
+    context "for regional processing office user" do
       let(:regional_processing_office_task) do
         create(:education_assess_documentation_task, assigned_to: regional_processing_office)
       end
@@ -15,13 +18,14 @@ describe EducationAssessDocumentationTask, :postgres do
         expect(regional_processing_office_task.assigned_to).to eq(regional_processing_office)
       end
 
-      before { regional_processing_office.add_user(user) }
+      it "in progress action is available whenever task is assigned" do
+        is_expected.to include Constants.TASK_ACTIONS.RPO_MARK_TASK_IN_PROGRESS.to_h
+      end
 
-      subject { regional_processing_office_task.available_actions(user) }
-
-      available_actions = []
-
-      it { is_expected.to eq available_actions }
+      it "in progress action is not available whenever task is already in progress" do
+        regional_processing_office_task.in_progress!
+        is_expected.to_not include Constants.TASK_ACTIONS.RPO_MARK_TASK_IN_PROGRESS.to_h
+      end
     end
   end
 end
