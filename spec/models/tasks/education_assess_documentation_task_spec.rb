dc1aae2f81253ca4a33dd86a54cab4af393887da
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2022-05-09 11:53:56 -0400

    Aiman&MattT/CASEFLOW-4519 RPO -> EMO Task Action and Modal (#17222)
    * Add tests
    
    * Add RPO -> EMO task
    
    * Add route to modal to QueueApp
    
    * Adjust CancelTaskModal to be more flexible. Added a default success message in case message_title isn't passed in.
    
    * Fix typo in test description
    
    * Update CancelTaskModal snapshot
    
    * Fix edu_assess_doc spec test
    
    * Move UtilityFunction to a private method to satisfy reek
    
    * Flip describe and context
    
    * Fix structure of #available_actions spec test
    
    * Update pre_docket_spec.rb
    
    Co-authored-by: Aiman Kayad <Akaya005@fiu.edu>

--- spec/models/tasks/education_assess_documentation_task_spec.rb
+++ spec/models/tasks/education_assess_documentation_task_spec.rb
@@ -2,11 +2,14 @@
 
 describe EducationAssessDocumentationTask, :postgres do
   let(:user) { create(:user) }
+  let(:regional_processing_office) { create(:edu_regional_processing_office) }
 
-  context "#available_actions" do
-    describe "for regional processing office user" do
-      let(:regional_processing_office) { create(:edu_regional_processing_office) }
+  before { regional_processing_office.add_user(user) }
 
+  describe "#available_actions" do
+    subject { regional_processing_office_task.available_actions(user) }
+
+    context "for regional processing office user" do
       let(:regional_processing_office_task) do
         create(:education_assess_documentation_task, assigned_to: regional_processing_office)
       end
@@ -15,13 +18,9 @@ describe EducationAssessDocumentationTask, :postgres do
         expect(regional_processing_office_task.assigned_to).to eq(regional_processing_office)
       end
 
-      before { regional_processing_office.add_user(user) }
-
-      subject { regional_processing_office_task.available_actions(user) }
-
-      available_actions = []
-
-      it { is_expected.to eq available_actions }
+      it "available tasks actions includes Return to EMO action" do
+        is_expected.to include Constants.TASK_ACTIONS.REGIONAL_PROCESSING_OFFICE_RETURN_TO_EMO.to_h
+      end
     end
   end
 end
