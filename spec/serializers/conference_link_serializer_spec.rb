8ebeca28c901a8605ed8a611ee229ee9b3626355
George Hovakimyan <george.hovakimyan@lefant.org>
2022-05-09 21:46:42 -0700

    Added attribute and nil tests

--- spec/serializers/conference_link_serializer_spec.rb
+++ spec/serializers/conference_link_serializer_spec.rb
@@ -12,11 +12,9 @@ describe ConferenceLinkSerializer, :all_dbs do
 
   let(:hearing_day) { create(:hearing_day) }
   let(:conference_link) { create(:conference_link, hearing_day_id: hearing_day.id) }
-  
-  subject { described_class.new(conference_link) }
 
   context "Converting conference link to hash" do
-
+    subject { described_class.new(conference_link) }
     before do
       allow(ENV).to receive(:[]).with("VIRTUAL_HEARING_PIN_KEY").and_return "mysecretkey"
       allow(ENV).to receive(:[]).with("VIRTUAL_HEARING_URL_HOST").and_return "example.va.gov"
@@ -29,6 +27,26 @@ describe ConferenceLinkSerializer, :all_dbs do
 
     it "calling serializable_hash return host link" do
       expect(subject.serializable_hash[:data][:attributes][:host_link]).to eq(conference_link.host_link)
+    end
+
+    it "calling serializable_hash return host pin" do
+      expect(subject.serializable_hash[:data][:attributes][:host_pin]).to eq(conference_link.host_pin)
+    end
+
+    it "calling serializable_hash return alias" do
+      expect(subject.serializable_hash[:data][:attributes][:alias]).to eq(conference_link.alias)
+    end
+  end
+
+  context "No conference link is passed in" do
+    subject { described_class.new(nil) }
+
+    it "conference link serializer exists" do
+      expect(subject).not_to eq(nil)
+    end
+
+    it "calling serializable_hash returns NoMethodError" do
+      expect { subject.serializable_hash[:data][:attributes] }.to raise_error(NoMethodError)
     end
   end
 end
