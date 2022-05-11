1e7e15b7afa56c3c3d7c609daed469549468f98d
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2022-05-11 14:20:40 -0400

    MattT/CASEFLOW-5464 Only show 'Return to X' task action if org X has closed their task. (#17223)
    * Remove commented out test assertions
    
    * Update pre_docket_spec tests
    
    * Only show Return to X tasks if X org has closed their more recent task
    
    * remove nested conditional
    
    * Check if all children are closed instead
    
    * Require at least one child_task to look at org assignments
    
    * Use safe navigation operator instead to avoid complexity warning
    
    Co-authored-by: Matthew Thornton <ThorntonMatthew@users.noreply.github.com>

--- app/models/tasks/pre_docket/pre_docket_task.rb
+++ app/models/tasks/pre_docket/pre_docket_task.rb
@@ -25,11 +25,14 @@ class PreDocketTask < Task
     task_actions = Array.new(TASK_ACTIONS)
     child_task = children.first
 
-    if child_task.task_is_assigned_to_organization?(VhaCamo.singleton)
+    return task_actions unless children.all?(&:closed?)
+
+    if child_task&.task_is_assigned_to_organization?(VhaCamo.singleton)
       task_actions.concat(VHA_ACTIONS)
-    elsif child_task.task_is_assigned_to_organization?(EducationEmo.singleton)
+    elsif child_task&.task_is_assigned_to_organization?(EducationEmo.singleton)
       task_actions.concat(EDU_ACTIONS)
     end
+
     task_actions
   end
 
