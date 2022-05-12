9c63905f7721425041ac97a94e51f81df937ed05
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-05-12 15:47:19 -0400

    Adjust params to task action method

--- app/repositories/task_action_repository.rb
+++ app/repositories/task_action_repository.rb
@@ -485,7 +485,7 @@ class TaskActionRepository
       }
     end
 
-    def education_rpo_send_to_board_intake_for_review(*)
+    def education_rpo_send_to_board_intake_for_review(task, user)
       org = Organization.find(task.assigned_to_id)
       queue_url = org.url
       {
