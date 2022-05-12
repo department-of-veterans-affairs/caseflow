983c2065b224b7381802f7810db9f77d7c8ba97b
Stephan Ward <stephan.ward@va.gov>
2022-05-12 17:20:24 -0400

    ConferenceLinkButton Jest Test v2

--- client/test/app/hearings/components/conferenceLink/ConferenceLinkButton.test.js
+++ client/test/app/hearings/components/conferenceLink/ConferenceLinkButton.test.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Button } from 'app/hearings/components/dailyDocket/DailyDocketRow';
-import { shallow } from 'enzyme';
 
 describe('Test Button component', () => {
-
-  it('Test click event without throwing an error', () => {
-    const { conferenceLink } = this.props;
-
-    window.open(conferenceLink?.hostLink);
-    const conferenceLinkOnClick = jest.fn();
-
-    const button = shallow((<Button
+  it('Test click event', () => {
+    const mockCallBack = jest.fn();
+    const button = ((<Button
       classNames={['usa-button-secondary']}
       type="button"
-      onClick={this.conferenceLinkOnClick} > Connect to Recording System</Button>));
+      name="conferenceOnClick"
+      onClick={mockCallBack} > Connect to Recording System</Button>));
 
-    button.find('button').simulate('click');
-    expect(conferenceLinkOnClick.mock.calls.length).toEqual(1);
-  }
-  );
+    button.simulate('click');
+    expect(mockCallBack.mock.calls).toEqual(true);
+  });
 });
