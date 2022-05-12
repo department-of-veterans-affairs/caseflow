d1a1afd9d75696a4daf7942b59606ad67005e584
Stephan Ward <stephan.ward@va.gov>
2022-05-12 13:47:11 -0400

    Soft Reset to Unit Testing

--- client/test/app/hearings/components/conferenceLink/ConferenceLinkButton.test.js
+++ client/test/app/hearings/components/conferenceLink/ConferenceLinkButton.test.js
@@ -3,7 +3,8 @@ import { Button } from 'app/hearings/components/dailyDocket/DailyDocketRow';
 import { shallow } from 'enzyme';
 
 describe('Test Button component', () => {
-  it('Test click event', () => {
+
+  it('Test click event without throwing an error', () => {
     const { conferenceLink } = this.props;
 
     window.open(conferenceLink?.hostLink);
@@ -16,16 +17,6 @@ describe('Test Button component', () => {
 
     button.find('button').simulate('click');
     expect(conferenceLinkOnClick.mock.calls.length).toEqual(1);
-  });
+  }
+  );
 });
-/*Test Button component › Test click event
-
-TypeError: Cannot read property 'conferenceLinkOnClick' of undefined
-
-14 |       classNames={['usa-button-secondary']}
-15 |       type="button"
-> 16 |       onClick={this.conferenceLinkOnClick} > Connect to Recording System</Button>));
-   |                     ^
-17 |
-18 |     button.find('button').simulate('click');
-19 |     expect(conferenceLinkOnClick.mock.calls.length).toEqual(1);*/
