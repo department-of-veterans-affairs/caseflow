c35c5d5183fd1ccd9259c6626a22541317f83973
Stephan Ward <stephan.ward@va.gov>
2022-05-16 13:29:59 -0400

    Jest test for Test Conference Link click event added.

--- client/test/app/hearings/components/dailyDocket/DailyDocketRow.test.js
+++ client/test/app/hearings/components/dailyDocket/DailyDocketRow.test.js
@@ -7,7 +7,7 @@ import { createStore } from "redux";
 import { dailyDocketReducer } from "../../../../data/hearings/dailyDocket/reducer/dailyDocketReducer";
 import { dailyDokcetProps } from "../../../../data/hearings/dailyDocket/dailyDocketProps";
 import DailyDocketRow from "../../../../../app/hearings/components/dailyDocket/DailyDocketRow";
-
+import { shallow } from 'enzyme';
 import COPY from "../../../../../COPY";
 
 let store;
@@ -27,5 +27,15 @@ describe('DailyDocketRow', () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+});
+
+describe('Test Conference Link Button', () => {
+  it('Test click event', () => {
+    const conferenceLink = jest.fn();
+    const button = shallow((<button onClick={conferenceLink}>Connect to Recording System</button>));
+
+    button.find('button').simulate('click');
+    expect(conferenceLink.mock.calls.length).toEqual(1);
   });
 });
