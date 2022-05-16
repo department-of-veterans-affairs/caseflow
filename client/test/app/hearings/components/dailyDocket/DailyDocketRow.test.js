42d3b15edfc24271922f6b07fee1331acfe5456a
Stephan Ward <stephan.ward@va.gov>
2022-05-16 14:05:19 -0400

    Removed unused import COPY

--- client/test/app/hearings/components/dailyDocket/DailyDocketRow.test.js
+++ client/test/app/hearings/components/dailyDocket/DailyDocketRow.test.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { axe } from "jest-axe";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { dailyDocketReducer } from "../../../../data/hearings/dailyDocket/reducer/dailyDocketReducer";
-import { dailyDokcetProps } from "../../../../data/hearings/dailyDocket/dailyDocketProps";
-import DailyDocketRow from "../../../../../app/hearings/components/dailyDocket/DailyDocketRow";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { dailyDocketReducer } from '../../../../data/hearings/dailyDocket/reducer/dailyDocketReducer';
+import { dailyDokcetProps } from '../../../../data/hearings/dailyDocket/dailyDocketProps';
+import DailyDocketRow from '../../../../../app/hearings/components/dailyDocket/DailyDocketRow';
 import { shallow } from 'enzyme';
-import COPY from "../../../../../COPY";
 
 let store;
 
