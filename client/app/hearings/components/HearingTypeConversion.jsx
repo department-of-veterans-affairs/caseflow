64c68c98f944d11f172324471cad4ee1ee296db1
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-05-23 14:33:39 -0400

    Remove representative_email from payload since it's read-only on the form and never changes.

--- client/app/hearings/components/HearingTypeConversion.jsx
+++ client/app/hearings/components/HearingTypeConversion.jsx
@@ -62,8 +62,7 @@ export const HearingTypeConversion = ({
                 email_recipients: {
                   appellant_tz: appeal?.appellantTz,
                   representative_tz: appeal?.powerOfAttorney?.representative_tz,
-                  appellant_email: appeal?.veteranInfo?.veteran?.email_address,
-                  representative_email: appeal?.powerOfAttorney?.representative_email_address,
+                  appellant_email: appeal?.veteranInfo?.veteran?.email_address
                 }
               }
             }
