3ee6ffbdf081086e8f00dca7c26d271ab729b721
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-06-08 22:11:14 -0400

    Disable camel case check in context

--- client/app/hearings/contexts/HearingTypeConversionContext.jsx
+++ client/app/hearings/contexts/HearingTypeConversionContext.jsx
@@ -7,16 +7,17 @@ export const HearingTypeConversionProvider = ({ children, initialAppeal }) => {
   // Create state for appellant timezone check
   const [isAppellantTZEmpty, setIsAppellantTZEmpty] = useState(initialAppeal.appellantTz);
 
-  // Create state for rep timezone check
+  /* eslint-disable camelcase */
   const [isRepTZEmpty, setIsRepTZEmpty] = useState(initialAppeal.powerOfAttorney?.representative_tz);
+
+  const [originalEmail, setOriginalEmail] = useState(initialAppeal.veteranInfo?.veteran?.email_address || '');
+  /* eslint-enable camelcase */
 
   // Create state to check if confirm field is empty
   const [confirmIsEmpty, setConfirmIsEmpty] = useState(true);
 
   // initiliaze hook to manage state for email validation
   const [isNotValidEmail, setIsNotValidEmail] = useState(true);
-
-  const [originalEmail, setOriginalEmail] = useState(initialAppeal.veteranInfo?.veteran?.email_address || '');
 
   const updateAppellantEmail = (appeal, email) => {
     appeal.veteranInfo.veteran.email_address = email;
