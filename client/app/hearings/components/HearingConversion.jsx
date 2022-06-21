67b87a8fe7d0d2ad595eba9777a40460e77bfb95
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-06-21 12:24:24 -0400

    Remove unused import

--- client/app/hearings/components/HearingConversion.jsx
+++ client/app/hearings/components/HearingConversion.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
