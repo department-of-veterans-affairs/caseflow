d399ad73bf95a443b513e395df456da5f5f8ff50
Billy Stockton <Billy.Stockton@va.gov>
2022-06-23 12:16:42 -0400

    Code Climate - Created Template shared_context_intake.rb

---
+++ spec/support/shared_context/shared_context_intake.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.configure { |rspec| rspec.shared_context_metadata_behavior = :apply_to_host_groups }
+
+RSpec.shared_context "intake", shared_context: :appealrepo do
+  before do
+    
+  end
+end
