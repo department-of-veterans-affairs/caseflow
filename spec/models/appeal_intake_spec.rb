761c1381799a4bbd3778fa85aed59e891aeb04a8
Billy Stockton <Billy.Stockton@va.gov>
2022-06-27 11:47:10 -0400

    Code Climate - Refactored supplemental_claim_intake_spec.rb, higher_level_review_intake_spec.rb, appeal_intake_spec.rb and shared_context_intake.rb

--- spec/models/appeal_intake_spec.rb
+++ spec/models/appeal_intake_spec.rb
@@ -392,28 +392,7 @@ describe AppealIntake, :all_dbs do
         ]
       end
 
-      context "legacy_opt_approved is true" do
-        let(:legacy_opt_in_approved) { true }
-
-        it "submits a LegacyIssueOptin" do
-          expect(LegacyIssueOptin.count).to eq 0
-          expect_any_instance_of(LegacyOptinManager).to receive(:process!).once
-
-          subject
-
-          expect(LegacyIssueOptin.count).to eq 1
-        end
-      end
-
-      context "legacy_opt_in_approved is false" do
-        it "does not submit a LegacyIssueOptin" do
-          expect(LegacyIssueOptin.count).to eq 0
-
-          subject
-
-          expect(LegacyIssueOptin.count).to eq 0
-        end
-      end
-    end
+      include_context "legacy opt in", include_shared: true
+    end    
   end
 end
--- spec/models/supplemental_claim_intake_spec.rb
+++ spec/models/supplemental_claim_intake_spec.rb
@@ -170,90 +170,21 @@ describe SupplementalClaimIntake, :all_dbs do
 
     include_context "intake", include_shared: true
 
-    # context "when benefit type is non comp" do
-    #   before { RequestStore[:current_user] = user }
-    #   let(:benefit_type) { "voc_rehab" }
+    context "when a legacy VACOLS opt-in occurs" do
+      include_context "legacy appeal", include_shared: true
 
-    #   it "creates DecisionReviewTask" do
-    #     subject
+      let(:issue_data) do
+        {
+          profile_date: "2018-04-30T11:11:00.000-04:00",
+          reference_id: "reference-id",
+          decision_text: "decision text",
+          vacols_id: legacy_appeal.vacols_id,
+          vacols_sequence_id: vacols_issue.issseq
+        }
+      end   
 
-    #     intake.detail.reload
-
-    #     expect(intake.detail.tasks.count).to eq(1)
-    #     expect(intake.detail.tasks.first).to be_a(DecisionReviewTask)
-    #   end
-
-    #   it "adds user to organization" do
-    #     subject
-    #     expect(OrganizationsUser.find_by(user: user, organization: intake.detail.business_line)).to_not be_nil
-    #   end
-    # end
-
-    # context "when a legacy VACOLS opt-in occurs" do
-    #   let(:vacols_issue) { create(:case_issue) }
-    #   let(:vacols_case) { create(:case, case_issues: [vacols_issue]) }
-    #   let(:legacy_appeal) do
-    #     create(:legacy_appeal, vacols_case: vacols_case)
-    #   end
-
-    #   let(:issue_data) do
-    #     {
-    #       profile_date: "2018-04-30T11:11:00.000-04:00",
-    #       reference_id: "reference-id",
-    #       decision_text: "decision text",
-    #       vacols_id: legacy_appeal.vacols_id,
-    #       vacols_sequence_id: vacols_issue.issseq
-    #     }
-    #   end
-
-    #   context "legacy_opt_in_approved is false" do
-    #     it "does not submit a LegacyIssueOptin" do
-    #       expect(LegacyIssueOptin.count).to eq 0
-
-    #       subject
-
-    #       expect(LegacyIssueOptin.count).to eq 0
-    #     end
-    #   end
-
-    #   context "legacy_opt_approved is true" do
-    #     let(:legacy_opt_in_approved) { true }
-
-    #     it "submits a LegacyIssueOptin" do
-    #       expect(LegacyIssueOptin.count).to eq 0
-    #       expect_any_instance_of(LegacyOptinManager).to receive(:process!).once
-
-    #       subject
-
-    #       expect(LegacyIssueOptin.count).to eq 1
-    #     end
-    #   end
-    # end
-
-    context "when the intake was already complete" do
-      let(:completed_at) { Time.zone.now }
-
-      it "does nothing" do
-        subject
-
-        expect(Fakes::VBMSService).to_not have_received(:establish_claim!)
-        expect(Fakes::VBMSService).to_not have_received(:create_contentions!)
-        expect(Fakes::VBMSService).to_not have_received(:associate_rating_request_issues!)
-      end
-    end
-
-    context "when the intake is pending" do
-      let(:completion_started_at) { Time.zone.now }
-
-      it "does nothing" do
-        subject
-
-        expect(Fakes::VBMSService).to_not have_received(:establish_claim!)
-        expect(Fakes::VBMSService).to_not have_received(:create_contentions!)
-        expect(Fakes::VBMSService).to_not have_received(:associate_rating_request_issues!)
-      end
-    end
-
-    include_context "intake", include_shared: true
+      include_context "legacy opt in", include_shared: true
+      include_context "completed intake", include_shared: true
+    end   
   end
 end
