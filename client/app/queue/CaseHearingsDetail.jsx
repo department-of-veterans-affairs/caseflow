092f1e8cdb9011f2e47fae513931329e2cdbc2c5
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-07-01 12:02:04 -0400

    Init deadline to today

--- client/app/queue/CaseHearingsDetail.jsx
+++ client/app/queue/CaseHearingsDetail.jsx
@@ -77,10 +77,10 @@ class CaseHearingsDetail extends React.PureComponent {
 
   getHearingAttrs = (hearing, userIsVsoEmployee, vsoVirtualOptIn, currentUserEmailPresent) => {
     const today = new Date();
-    const deadline = new Date();
+    const deadline = new Date(today);
     const hearingDay = new Date(hearing.date);
 
-    deadline.setDate(today.getDate() + 10);
+    deadline.setDate(deadline.getDate() + 10);
     // show convert to virtual link if user is vso, hearing isn't virtual, and scheduled date is not within deadline
     const hearingAttrs = [{
       label: 'Type',
