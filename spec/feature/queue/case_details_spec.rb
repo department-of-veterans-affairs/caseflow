27d5b117a2cdf1738c2e48b0edd2d21233dacf88
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-07-06 13:46:53 -0400

    Fix assertion

--- spec/feature/queue/case_details_spec.rb
+++ spec/feature/queue/case_details_spec.rb
@@ -2501,7 +2501,7 @@ RSpec.feature "Case details", :all_dbs do
         visit "/queue/appeals/#{hearing.appeal.uuid}"
 
         expect(page.has_content?("Contact the Hearing Coordinator")).to eq true
-        expect(page.has_no_content?(COPY::VSO_UNABLE_TO_CONVERT_TO_VIRTUAL_TEXT)).to eq true
+        expect(page.has_content?(COPY::VSO_UNABLE_TO_CONVERT_TO_VIRTUAL_TEXT)).to eq true
         expect(page.has_no_content?(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)).to eq true
       end
     end
