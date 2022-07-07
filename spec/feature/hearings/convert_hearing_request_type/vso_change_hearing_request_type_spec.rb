9760847fb08c13d73bc7b27a47e6ac5da64574c6
sulak-j <justin.sulak@va.gov>
2022-07-07 11:08:02 -0500

    complete merge from master

--- spec/feature/hearings/convert_hearing_request_type/vso_change_hearing_request_type_spec.rb
+++ spec/feature/hearings/convert_hearing_request_type/vso_change_hearing_request_type_spec.rb
@@ -2,102 +2,22 @@
 
 RSpec.feature "Convert hearing request type" do
   before do
-<<<<<<< HEAD
-=======
     FeatureToggle.enable!(:vso_virtual_opt_in)
->>>>>>> master
     FeatureToggle.enable!(:schedule_veteran_virtual_hearing)
     HearingsManagement.singleton.add_user(hearing_coord)
     vso.add_user(vso_user)
   end
-<<<<<<< HEAD
-  after { FeatureToggle.disable!(:schedule_veteran_virtual_hearing) }
-  let!(:appeal) do
-    a = create(:appeal, :with_schedule_hearing_tasks, :hearing_docket, number_of_claimants: 1)
-    a.update!(changed_hearing_request_type: Constants.HEARING_REQUEST_TYPES.video,
-              veteran_is_not_claimant: true)
-    a
-  end
-=======
 
   after do
     FeatureToggle.disable!(:vso_virtual_opt_in)
     FeatureToggle.disable!(:schedule_veteran_virtual_hearing)
   end
 
->>>>>>> master
   let!(:hearing_day) { create(:hearing_day, :video, scheduled_for: Time.zone.today + 14.days, regional_office: "RO63") }
   let!(:hearing_day2) { create(:hearing_day, :video, scheduled_for: Time.zone.today + 7.days, regional_office: "RO63") }
   let!(:vso) { create(:vso, name: "VSO", role: "VSO", url: "vso-url", participant_id: "8054") }
   let!(:vso_user) { create(:user, :vso_role, email: "DefinitelyNotNull@All.com") }
   let!(:hearing_coord) { create(:user, roles: ["Edit HearSched", "Build HearSched"]) }
-<<<<<<< HEAD
-  let!(:poa) { create(:bgs_power_of_attorney, :with_name_cached, appeal: appeal) }
-
-  context "When appeal has no scheduled hearings" do
-    scenario "convert to virtual link appears and leads to task form" do
-      step "navigate to the VSO Form" do
-        User.authenticate!(user: vso_user)
-        visit "queue/appeals/#{appeal.uuid}"
-        expect(page).to have_content("Video")
-        click_link(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)
-      end
-
-      step "verify pre-population of fields" do
-        expect(page).to have_content("Convert Hearing To Virtual")
-
-        ["#{appeal.appellant.first_name} #{appeal.appellant.last_name}",
-         poa.representative_name,
-         poa.representative_type,
-         "Pacific Time (US & Canada)",
-         "Eastern Time (US & Canada)",
-         vso_user.email].each do |field_value|
-          expect(page).to have_content(field_value)
-        end
-
-        expect(page).to have_field("Appellant Email", with: appeal.appellant.email_address)
-      end
-
-      step "test form validation and submit it" do
-        # Check if button is disabled on page load
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-
-        # Affirm checkboxes first to test other fields
-        click_label("Affirm Permission")
-        click_label("Affirm Access")
-
-        # Check if buttone remains disabled
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-
-        # Fill out email field and expect validation message on invalid email
-        fill_in "Appellant Email", with: "appellant@test"
-        find("body").click
-        expect(page).to have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL)
-        fill_in "Appellant Email", with: "appellant@test.com"
-        expect(page).to_not have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL)
-
-        # Check if button remains disabled
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-
-        # Fill out confirm email field and expect validation message on unmatched email
-        fill_in "Confirm Appellant Email", with: "appellant@not-matching"
-        find("body").click
-        expect(page).to have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL_MATCH)
-        fill_in "Confirm Appellant Email", with: "appellant@test.com"
-        expect(page).to_not have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL_MATCH)
-
-        # Set appellant tz to null
-        click_dropdown(name: "appellantTz", index: 0)
-
-        # Check if button remains disabled
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-
-        # Set rep tz to null
-        click_dropdown(name: "representativeTz", index: 0)
-
-        # Check if button remains disabled
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-=======
 
   def appellant_name
     if appeal.is_a?(Appeal)
@@ -270,24 +190,10 @@ RSpec.feature "Convert hearing request type" do
         expect(page).to have_button("Convert to Virtual Hearing", disabled: true)
         click_dropdown(name: "representativeTz", index: 2)
         expect(page).to have_button("Convert to Virtual Hearing", disabled: false)
->>>>>>> master
 
         # Set appellant and rep timezones to something not null
         click_dropdown(name: "appellantTz", index: 1)
         click_dropdown(name: "representativeTz", index: 2)
-<<<<<<< HEAD
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: false)
-
-        # Alter original email to make sure that the confirm email validation picks up on the change.
-        fill_in "Appellant Email", with: "valid@something-different.com"
-        expect(page).to have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL_MATCH)
-        expect(page).to have_button("button-Convert-Hearing-To-Virtual", disabled: true)
-
-        fill_in "Appellant Email", with: "appellant@test.com"
-        expect(page).to_not have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL_MATCH)
-        # Convert button should now be enabled
-        click_button("Convert Hearing To Virtual")
-=======
         expect(page).to have_button("Convert to Virtual Hearing", disabled: false)
 
         # Alter original email to make sure that the confirm email validation picks up on the change.
@@ -299,61 +205,17 @@ RSpec.feature "Convert hearing request type" do
         expect(page).to_not have_content(COPY::CONVERT_HEARING_VALIDATE_EMAIL_MATCH)
 
         click_button "Convert to Virtual Hearing"
->>>>>>> master
       end
 
       step "Confirm success message" do
         expect(page).to have_content(
-<<<<<<< HEAD
-          "You have successfully converted #{appeal.appellant.first_name} " \
-          "#{appeal.appellant.last_name}'s hearing to virtual"
-=======
           "You have successfully converted #{appellant_name}'s hearing to virtual"
->>>>>>> master
         )
         expect(page).to have_content(COPY::VSO_CONVERT_HEARING_TYPE_SUCCESS_DETAIL)
       end
     end
   end
 
-<<<<<<< HEAD
-  context "When appeal has no scheduled hearings" do
-    scenario "no link appears in hearing section" do
-      step "navigate to the VSO Form" do
-        appeal.update!(changed_hearing_request_type: Constants.HEARING_REQUEST_TYPES.virtual)
-        User.authenticate!(user: vso_user)
-        visit "queue/appeals/#{appeal.uuid}"
-        expect(page).to have_content("Virtual")
-        expect(page).not_to have_link(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)
-      end
-    end
-  end
-
-  context "When appeal has a scheduled hearing in over 11 days" do
-    scenario "convert to virtual link appears and leads to hearing form" do
-      step "select from dropdown" do
-        User.authenticate!(user: hearing_coord)
-        visit "queue/appeals/#{appeal.uuid}"
-        click_dropdown(text: Constants.TASK_ACTIONS.SCHEDULE_VETERAN.label)
-      end
-
-      step "fill in form" do
-        fill_in "Notes", with: "asfsefdsfdf"
-        click_dropdown(name: "regionalOffice", index: 1)
-        click_dropdown(name: "appealHearingLocation", index: 0)
-        click_dropdown(name: "hearingDate", index: 1)
-        click_dropdown(name: "optionalHearingTime0", index: 0)
-        click_button(text: "Schedule")
-      end
-      step "navigate to the hearings form" do
-        appeal = Appeal.last
-        User.authenticate!(user: vso_user)
-        visit "queue/appeals/#{appeal.uuid}"
-        expect(page).to have_content("Video")
-        expect(page).to have_link(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)
-        click_link(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)
-        expect(page).to have_current_path("/hearings/#{appeal.hearings.first.uuid}/details")
-=======
   describe "for AMA appeals and hearings" do
     let!(:appeal) do
       a = create(:appeal, :with_schedule_hearing_tasks, :hearing_docket, number_of_claimants: 1)
@@ -435,38 +297,10 @@ RSpec.feature "Convert hearing request type" do
         let!(:hearing) { create(:hearing, hearing_day: hearing_day, appeal: appeal) }
 
         it_behaves_like "scheduled hearings"
->>>>>>> master
       end
     end
   end
 
-<<<<<<< HEAD
-  context "When hearing is within 11 days of scheduled time" do
-    scenario "info alert appears instead of link" do
-      step "select from dropdown" do
-        User.authenticate!(user: hearing_coord)
-        visit "queue/appeals/#{appeal.uuid}"
-        click_dropdown(text: Constants.TASK_ACTIONS.SCHEDULE_VETERAN.label)
-      end
-
-      step "fill in form" do
-        fill_in "Notes", with: "asfsefdsfdf"
-        click_dropdown(name: "regionalOffice", index: 1)
-        click_dropdown(name: "appealHearingLocation", index: 0)
-        click_dropdown(name: "hearingDate", index: 0)
-        click_dropdown(name: "optionalHearingTime0", index: 0)
-        click_button(text: "Schedule")
-      end
-
-      step "vso user" do
-        appeal = Appeal.last
-        User.authenticate!(user: vso_user)
-        visit "queue/appeals/#{appeal.uuid}"
-
-        expect(page).not_to have_link(COPY::VSO_CONVERT_TO_VIRTUAL_TEXT)
-        expect(page).to have_content(COPY::VSO_UNABLE_TO_CONVERT_TO_VIRTUAL_TEXT)
-      end
-=======
   describe "for legacy appeals and hearings" do
     let(:ssn) { Generators::Random.unique_ssn }
     let!(:vacols_case) { create(:case, :representative_american_legion) }
@@ -488,7 +322,6 @@ RSpec.feature "Convert hearing request type" do
       let!(:hearing) { create(:legacy_hearing, hearing_day: hearing_day2, appeal: appeal) }
 
       it_behaves_like "scheduled hearings"
->>>>>>> master
     end
   end
 end
