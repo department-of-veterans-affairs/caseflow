f30aef370cade0956e94a3ec8daf6f5d1a909a7f
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-07-15 23:22:30 -0400

    Change scope names to prevent collisions if both concerns are included as mixins to the same class

--- app/models/hearings/hearing_email_recipient.rb
+++ app/models/hearings/hearing_email_recipient.rb
@@ -21,10 +21,11 @@ class HearingEmailRecipient < CaseflowRecord
   validates :email_address, presence: true, on: :create
   has_many :email_events, class_name: "SentHearingEmailEvent", foreign_key: :email_recipient_id
 
-  include BelongsToPolymorphicHearingConcern
-  belongs_to_polymorphic_hearing(:hearing)
   include HasAppealUpdatedSince
+  include BelongsToPolymorphicHearingConcern
   include BelongsToPolymorphicAppealConcern
+
+  belongs_to_polymorphic_hearing(:hearing)
   belongs_to_polymorphic_appeal(:appeal)
 
   def reminder_sent_at
