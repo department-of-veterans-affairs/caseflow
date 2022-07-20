33a39101df7621565f8ceb70465aaa9d21cfbd0e
Matt Roth <roth_matthew@bah.com>
2022-07-19 20:24:40 -0400

    switch to Caseflow::Migration

--- db/migrate/20220719230336_add_timeliness_to_judge_case_reviews.rb
+++ db/migrate/20220719230336_add_timeliness_to_judge_case_reviews.rb
@@ -1,4 +1,4 @@
-class AddTimelinessToJudgeCaseReviews < ActiveRecord::Migration[5.2]
+class AddTimelinessToJudgeCaseReviews < Caseflow::Migration
   def change
 
     add_column :judge_case_reviews, :timeliness, :string, comment: "Documents if the drafted decision by an attorney was provided on a timely or untimely manner."
