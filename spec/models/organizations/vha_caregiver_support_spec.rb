58a12575e7917887155ec4969bc789fa1afbb543
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-07-28 10:20:54 -0400

    Add some unit tests for the VhaCaregiverSupport class

---
+++ spec/models/organizations/vha_caregiver_support_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe VhaCaregiverSupport, :postgres do
+  let(:vha_csp) do
+    VhaCaregiverSupport.create(name: "VHA Caregiver Support Program", url: "vha-csp")
+  end
+
+  describe ".create!" do
+    it "organization that was created has expected name" do
+      expect(vha_csp.name).to eq("VHA Caregiver Support Program")
+    end
+
+    it "organization that was created has expected url" do
+      expect(vha_csp.url).to eq("vha-csp")
+    end
+  end
+
+  describe ".can_receive_task?" do
+    let(:appeal) { create(:appeal) }
+    let(:task) { create(:task, appeal: appeal) }
+
+    # This comes into play for any task with the "ASSIGN_TO_TEAM" task action
+    it "returns false because VHA CSP office cannot have tasks manually assigned to them" do
+      expect(vha_csp.can_receive_task?(task)).to eq(false)
+    end
+  end
+end
