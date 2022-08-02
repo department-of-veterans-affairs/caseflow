cc6bc1b46ae8e35c11be873b95db421a222ba937
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-08-02 14:41:10 -0400

    Init RSPEC

---
+++ spec/models/appellant_notification_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "helpers/appellant_notification.rb"
+
+describe AppellantNotification do
+  describe AppellantNotification::AppealDocketed do
+    describe ".distribution_task" do
+      let(:appeal) { create(:appeal, :with_pre_docket_task) }
+      let(:task_factory) do
+        InitialTasksFactory.prepend(AppellantNotification::AppealDocketed)
+        InitialTasksFactory.new(appeal)
+      end
+      it "will notify appellant when an appeal is docketed" do
+        task_factory.distribution_task
+        sleep(1)
+        expect(Shoryuken::Client.sqs.receive_message(
+                 queue_url: "http://localhost:4576/000000000000/caseflow_development_send_notifications"
+               )).to eq("Bob")
+      end
+    end
+  end
+end
+
+# describe AppellantNotification::AppealDocketed do
+#   describe ".distribution_task" do
+#     let(:appeal) { create(:appeal, :with_pre_docket_task) }
+#     let(:spaghetti) do
+#       InitialTasksFactory.prepend(AppellantNotification::AppealDocketed)
+#       InitialTasksFactory.new(appeal)
+#     end
+#     it "will notify appellant when an appeal is docketed" do
+#       spaghetti.distribution_task
+#       expect(appeal.tasks.any? {|t| t.type == "DistributionTask"}).to eq true
+#     end
+#   end
+# end
