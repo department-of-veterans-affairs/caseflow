e94262000c5f21de342728d55c89fed77c933f73
lowelllewis <lewis_lowell@bah.com>
2022-08-05 08:08:25 -0400

    Updates to setup scripts and docker compose file for M1 setup.

--- scripts/dev_env_setup_step1.sh
+++ scripts/dev_env_setup_step1.sh
@@ -36,26 +36,18 @@ esac
 
 # === The rest assumes Mac OS, so there are no case statements.
 
-echo "==> Installing Homebrew"
-if which brew > /dev/null; then
-  echo "Homebrew is already installed. Skipping installation."
-else
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 echo "==> Installing the base dependencies"
 brew install rbenv nodenv yarn jq shared-mime-info
 brew tap ouchxp/nodenv
 brew install nodenv-nvmrc
-brew install postgres
-brew install --cask chromedriver
-chromedriver --version
+#brew install --cask chromedriver
+#chromedriver --version
 
 echo "==> Setting up rbenv and nodenv"
 rbenv init
 nodenv init
 
-if ! grep -q rbenv ~/.bash_profile; then
+if ! grep -q rbenv ~/.zshrc; then
 	echo "==> Updating ~/.zshrc"
 	cat <<-EOF >> ~/.zshrc
 
@@ -73,18 +65,3 @@ echo "==> Installing InstantClient"
 brew tap InstantClientTap/instantclient
 brew install instantclient-basic
 brew install instantclient-sdk
-
-echo "
-===================================
-You must do the following manually:
-"
-
-echo "==> Downloading PDFtk Server"
-if ! [ -f pdftk_server-2.02-mac_osx-10.11-setup.pkg ]; then
-	brew install wget
-	wget "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg"
-fi
-echo "==> To install PDFtk, run the package installer using Privilege Management"
-
-echo "==> Close this terminal, open a new terminal, and run ./dev_env_setup_step2.sh
-	in the new terminal."
