4228ee6bf724fa990166e0f0e52d55af9d45bfb8
Alicia Perry <alicia.perry@va.gov>
2022-08-05 15:37:50 -0500

    pushing to branch for testing on 7439 and 7440

--- app/services/external_api/va_notify_service.rb
+++ app/services/external_api/va_notify_service.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 require "json"
+require "base64"
+require "digest"
+
 
 class ExternalApi::VANotifyService
-  BASE_URL = ENV["notification-api-url"] || "https://dev-api.va.gov/vanotify"
-  CLIENT_SECRET = ENV["notification-client-secret"] || ""
-
+  BASE_URL = ENV["notification-api-url"] || ""
+  CLIENT_SECRET = ENV["service-api-key"] || ""
+  SERVICE_ID = ""
   SEND_EMAIL_NOTIFICATION_ENDPOINT = "/v2/notifications/email"
   GET_STATUS_ENDPOINT = "/v2/notifications/"
+  TEMPLATE_ENDPOINT = "/service/#{SERVICE_ID}/template"
+  CALLBACK_ENDPOINT = "/service/#{SERVICE_ID}/callback"
+  HEADERS = {
+    "Content-Type": "application/json", Accept: "application/json"
+  }.freeze
   
 
   class << self
@@ -40,14 +48,34 @@ class ExternalApi::VANotifyService
 
     end
 
-    def create_callback
 
+    def create_callback(url, callback_type, bearer_token, callback_channel)
+      request = { 
+
+         body: {
+          url: url,
+          callback_type: callback_type,
+          bearer_token: bearer_token,
+          callback_channel: callback_channel
+        
+        },
+  
+          headers: HEADERS,
+          endpoint: CALLBACK_ENDPOINT, method: :post
+        
+        }
+  
     end
 
     def get_callback
-        
-    end
+       request = {
+        headers: HEADERS,
+        endpoint: CALLBACK_ENDPOINT, method: :get
 
+       }
+       send_va_notify_request(request)
+    end
+  
     def create_template
 
     end
@@ -55,5 +83,37 @@ class ExternalApi::VANotifyService
     def get_template
 
     end
+    private
+
+    def generate_token
+      jwt_secret = CLIENT_SECRET
+      header = {
+        typ: "JWT",
+        alg: "HS256"
+      }
+      current_timestamp = DateTime.now.strftime("%Q").to_i / 1000.floor
+      data = {
+        iss: SERVICE_ID,
+        iat: current_timestamp
+      }
+      stringified_header = header.to_json.encode("UTF-8")
+      encoded_header = base64url(stringified_header)
+      stringified_data = data.to_json.encode("UTF-8")
+      encoded_data = base64url(stringified_data)
+      token = "#{encoded_header}.#{encoded_data}"
+      signature = OpenSSL::HMAC.digest('SHA256', jwt_secret, token)
+      signature = base64url(signature)
+      signed_token = "#{token}.#{signature}"
+      signed_token
+    end
+
+    def base64url(source)
+      encoded_source = Base64.encode64(source)
+      encoded_source = encoded_source.sub(/=+$/, '')
+      encoded_source = encoded_source.gsub(/\+/, '-')
+      encoded_source = encoded_source.gsub(/\//, '_')
+      encoded_source
+    end 
   end
+  
 end
