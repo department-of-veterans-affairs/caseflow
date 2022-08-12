531f874f31fb23e459ae4abc5d3ba1655a88e84d
Marc Steele <Marc.Steele@va.gov>
2022-08-12 16:20:09 -0400

    APPEALS-7004 Decision Mailed Spec/ new factory

--- app/models/prepend/va_notify/appeal_decision_mailed.rb
+++ app/models/prepend/va_notify/appeal_decision_mailed.rb
@@ -7,11 +7,35 @@ module AppealDecisionMailed
   @@template_name = self.to_s
   # rubocop:enable all
 
-  def update_with_instructions(params)
+  # def update_with_instructions(params)
+  #   # original method defined in app/models/task.rb
+  #   super
+  #   if name == "BvaDispatchTask" && contested_claim?
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name}Contested")
+  #   elsif name == "BvaDispatchTask"
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name}NonContested")
+  #   end
+  # end
+
+  # Legacy
+  # was previously working
+  def complete_root_task!
+    # original method defined in app/workflows/legacy_appeal_dispatch.rb
     super
-    if name == "BvaDispatchTask" && contested_claim?
+    if appeal.contested_claim
       AppellantNotification.notify_appellant(@appeal, "#{@@template_name}Contested")
-    elsif name == "BvaDispatchTask"
+    else
+      AppellantNotification.notify_appellant(@appeal, "#{@@template_name}NonContested")
+    end
+  end
+
+  # AMA
+  def complete_dispatch_root_task!
+    # original method defined in app/workflows/ama_appeal_dispatch.rb
+    super
+    if appeal.contested_claim?
+      AppellantNotification.notify_appellant(@appeal, "#{@@template_name}Contested")
+    else
       AppellantNotification.notify_appellant(@appeal, "#{@@template_name}NonContested")
     end
   end
