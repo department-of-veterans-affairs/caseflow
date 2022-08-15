e01780e78e1f6f7137f5ebb0ab8594ca1a559a5d
Enrilo Ugalde <ugalde_enrilo@bah.com>
2022-08-15 16:17:35 -0700

    added switch cases for all dependencies to display and hide

--- app/services/dependencies_report_service.rb
+++ app/services/dependencies_report_service.rb
@@ -17,6 +17,8 @@ class DependenciesReportService
        "VBMS.FindDocumentVersionReference",
        "VVA"].freeze
 
+       DEGRADED_DEPENDENCIES = []
+
     # this method is in case we need list of dependencies/services that are degraded
     def degraded_dependencies
       str_report = Rails.cache.read(:dependencies_report)
@@ -30,10 +32,55 @@ class DependenciesReportService
 
     def dependencies_report
       case Rails.cache.read(:degraded_service_banner)
+
+      #BGS
+      when :display_bgs
+        DEGRADED_DEPENDENCIES.push("BGS")
+        return DEGRADED_DEPENDENCIES
+      when :hide_bgs
+        DEGRADED_DEPENDENCIES.delete("BGS")
+        return DEGRADED_DEPENDENCIES
+
+      #VBMS
+      when :display_vbms
+          return DEGRADED_DEPENDENCIES.push("VBMS")
+      when :hide_vbms
+        DEGRADED_DEPENDENCIES.delete("VBMS")
+        return DEGRADED_DEPENDENCIES
+
+      #VVA
+      when :display_vva
+        return DEGRADED_DEPENDENCIES.push("VVA")
+      when :hide_vva
+      DEGRADED_DEPENDENCIES.delete("VVA")
+      return DEGRADED_DEPENDENCIES
+
+      #VACOLS
+      when :display_vacols
+        return DEGRADED_DEPENDENCIES.push("VACOLS")
+      when :hide_vacols
+        DEGRADED_DEPENDENCIES.delete("VACOLS")
+        return DEGRADED_DEPENDENCIES
+
+      #GOV_DELIVERY
+      when :display_gov_delivery
+        return DEGRADED_DEPENDENCIES.push("GOV_DELIVERY")
+      when :hide_gov_delivery
+        DEGRADED_DEPENDENCIES.delete("GOV_DELIVERY")
+        return DEGRADED_DEPENDENCIES  
+
+      #VA_DOT_GOV
+      when :display_va_dot_gov
+        return DEGRADED_DEPENDENCIES.push("VA_DOT_GOV")
+      when :hide_va_dot_gov
+        DEGRADED_DEPENDENCIES.delete("VA_DOT_GOV")
+        return DEGRADED_DEPENDENCIES
+
+      #ALL DEPENDENCIES
       when :always_show
-        return ALL_DEPENDENCIES
-      when :never_show
-        return []
+        return DEGRADED_DEPENDENCIES.push(ALL_DEPENDENCIES)
+      when :clear_all
+        return DEGRADED_DEPENDENCIES.clear()
       end
       degraded_dependencies
     rescue StandardError => error
