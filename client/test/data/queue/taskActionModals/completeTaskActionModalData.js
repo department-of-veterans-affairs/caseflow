78998d3e3fcc0fb8f99dd5c0d008a7a96ece470c
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-08-15 23:21:36 -0400

    Trim data some more

--- client/test/data/queue/taskActionModals/completeTaskActionModalData.js
+++ client/test/data/queue/taskActionModals/completeTaskActionModalData.js
@@ -1,42 +1,41 @@
-/* eslint-disable max-lines */
 const uiData = {
   ui: {
-  highlightFormItems: false,
-  messages: {
-    success: null,
-    error: null
+    highlightFormItems: false,
+    messages: {
+      success: null,
+      error: null
     },
-  saveState: {
-    savePending: false,
-    saveSuccessful: null
+    saveState: {
+      savePending: false,
+      saveSuccessful: null
     },
-  featureToggles: {
-    collect_video_and_central_emails: true,
-    enable_hearing_time_slots: true,
-    schedule_veteran_virtual_hearing: true,
-    overtime_revamp: true,
-    overtime_persistence: true,
-    mdr_cavc_remand: true,
-    reversal_cavc_remand: true,
-    dismissal_cavc_remand: true,
-    editNodDate: true,
-    fnod_badge: true,
-    fnod_banner: true,
-    view_nod_date_updates: true,
-    poa_button_refresh: true,
-    poa_auto_refresh: true,
-    poa_auto_ihp_update: true,
-    edit_unrecognized_appellant: true,
-    edit_unrecognized_appellant_poa: true,
-    listed_granted_substitution_before_dismissal: true,
-    restrict_poa_visibility: true,
-    vha_predocket_workflow: true,
-    vha_irregular_appeals: true,
-    vso_virtual_opt_in: true,
-    das_case_timeliness: true
+    featureToggles: {
+      collect_video_and_central_emails: true,
+      enable_hearing_time_slots: true,
+      schedule_veteran_virtual_hearing: true,
+      overtime_revamp: true,
+      overtime_persistence: true,
+      mdr_cavc_remand: true,
+      reversal_cavc_remand: true,
+      dismissal_cavc_remand: true,
+      editNodDate: true,
+      fnod_badge: true,
+      fnod_banner: true,
+      view_nod_date_updates: true,
+      poa_button_refresh: true,
+      poa_auto_refresh: true,
+      poa_auto_ihp_update: true,
+      edit_unrecognized_appellant: true,
+      edit_unrecognized_appellant_poa: true,
+      listed_granted_substitution_before_dismissal: true,
+      restrict_poa_visibility: true,
+      vha_predocket_workflow: true,
+      vha_irregular_appeals: true,
+      vso_virtual_opt_in: true,
+      das_case_timeliness: true
     }
   }
-};  
+};
 
 export const camoToBvaIntakeData = {
   queue: {
@@ -147,136 +146,8 @@ export const camoToBvaIntakeData = {
     },
     appeals: {
       '419ce568-387c-4ac6-a5f5-00a1554cea36': {
-        id: '1898',
-        appellant_hearing_email_recipient: null,
-        representative_hearing_email_recipient: null,
-        externalId: '419ce568-387c-4ac6-a5f5-00a1554cea36',
-        docketName: 'evidence_submission',
-        withdrawn: false,
-        removed: false,
-        overtime: false,
-        contestedClaim: false,
-        veteranAppellantDeceased: false,
-        isLegacyAppeal: false,
-        caseType: 'Original',
-        isAdvancedOnDocket: false,
-        issueCount: 0,
-        docketNumber: '220802-1898',
-        assignedAttorney: null,
-        assignedJudge: null,
-        distributedToJudge: false,
-        veteranFullName: 'Bob Smithhansen',
-        veteranFileNumber: '500000262',
-        isPaperCase: false,
-        readableHearingRequestType: null,
-        readableOriginalHearingRequestType: null,
-        vacateType: null,
-        hearings: [],
-        currentUserEmail: null,
-        currentUserTimezone: 'America/New_York',
-        completedHearingOnPreviousAppeal: false,
-        issues: [],
-        decisionIssues: [],
-        canEditRequestIssues: false,
-        unrecognizedAppellantId: null,
-        appellantIsNotVeteran: false,
-        appellantFullName: 'Bob Smithhansen',
-        appellantFirstName: 'Bob',
-        appellantMiddleName: null,
-        appellantLastName: 'Smithhansen',
-        appellantSuffix: null,
-        appellantDateOfBirth: '1992-08-02',
-        appellantAddress: {
-          address_line_1: '9999 MISSION ST',
-          address_line_2: 'UBER',
-          address_line_3: 'APT 2',
-          city: 'SAN FRANCISCO',
-          zip: '94103',
-          country: 'USA',
-          state: 'CA'
-        },
-        appellantEmailAddress: 'Bob.Smithhansen@test.com',
-        appellantPhoneNumber: null,
-        appellantType: 'VeteranClaimant',
-        appellantPartyType: null,
-        appellantTz: 'America/Los_Angeles',
-        appellantRelationship: 'Veteran',
-        hasPOA: {
-          id: 352,
-          authzn_change_clmant_addrs_ind: null,
-          authzn_poa_access_ind: null,
-          claimant_participant_id: '500000375',
-          created_at: '2022-08-02T11:21:58.947-04:00',
-          file_number: '00001234',
-          last_synced_at: '2022-08-02T11:21:58.947-04:00',
-          legacy_poa_cd: '100',
-          poa_participant_id: '600153863',
-          representative_name: 'Clarence Darrow',
-          representative_type: 'Attorney',
-          updated_at: '2022-08-02T11:21:58.947-04:00'
-        },
-        assignedToLocation: 'VHA CAMO',
-        veteranDateOfDeath: null,
-        closestRegionalOffice: null,
-        closestRegionalOfficeLabel: null,
-        availableHearingLocations: [],
-        status: 'pre_docketed',
-        decisionDate: null,
-        nodDate: '2022-08-02',
-        nodDateUpdates: [],
-        certificationDate: null,
-        powerOfAttorney: {
-          representative_type: 'Attorney',
-          representative_name: 'Clarence Darrow',
-          representative_address: {
-            address_line_1: '9999 MISSION ST',
-            address_line_2: 'UBER',
-            address_line_3: 'APT 2',
-            city: 'SAN FRANCISCO',
-            zip: '94103',
-            country: 'USA',
-            state: 'CA'
-          },
-          representative_email_address: 'jamie.fakerton@caseflowdemo.com',
-          representative_tz: 'America/Los_Angeles',
-          poa_last_synced_at: '2022-08-02T11:21:58.947-04:00'
-        },
-        cavcRemand: null,
-        regionalOffice: null,
-        caseflowVeteranId: 376,
-        documentID: null,
-        caseReviewId: null,
-        canEditDocumentId: false,
-        attorneyCaseRewriteDetails: {
-          note_from_attorney: null,
-          untimely_evidence: null
-        },
-        docketSwitch: null,
-        switchedDockets: [],
-        appellantSubstitution: null,
-        substitutions: [],
-        hasSameAppealSubstitution: true,
-        remandSourceAppealId: null,
-        remandJudgeName: null,
-        veteranInfo: {
-          veteran: {
-            full_name: 'Bob Smithhansen',
-            gender: 'F',
-            date_of_birth: '08/02/1992',
-            date_of_death: null,
-            email_address: 'Bob.Smithhansen@test.com',
-            address: {
-              address_line_1: '1234 Main Street',
-              address_line_2: null,
-              address_line_3: null,
-              city: 'Orlando',
-              state: 'FL',
-              zip: '12345',
-              country: 'USA'
-            },
-            relationships: null
-          }
-        }
+        id: '1632',
+        externalId: '419ce568-387c-4ac6-a5f5-00a1554cea36'
       }
     }
   },
@@ -379,7 +250,7 @@ export const camoToProgramOfficeToCamoData = {
         appealType: 'Appeal',
         addedByCssId: null,
         appealId: 1632,
-        externalAppealId: 'bc7ba6d4-48a7-436b-97e8-62bc061c604e',
+        externalAppealId: '419ce568-387c-4ac6-a5f5-00a1554cea36',
         assignedOn: '2022-08-08T22:11:55.277-04:00',
         closestRegionalOffice: null,
         createdAt: '2022-08-02T12:03:32.187-04:00',
@@ -408,13 +279,6 @@ export const camoToProgramOfficeToCamoData = {
         },
         convertedOn: null,
         taskId: '7119',
-        parentId: 7118,
-        label: 'Review Documentation',
-        documentId: null,
-        externalHearingId: null,
-        workProduct: null,
-        previousTaskAssignedOn: null,
-        placedOnHoldAt: '2022-08-08T22:11:34.349-04:00',
         status: 'assigned',
         instructions: [],
         availableActions: [
@@ -471,13 +335,7 @@ export const camoToProgramOfficeToCamoData = {
         type: 'AssessDocumentationTask',
         appealType: 'Appeal',
         appealId: 1632,
-        externalAppealId: 'bc7ba6d4-48a7-436b-97e8-62bc061c604e',
-        assignedOn: '2022-08-08T22:11:34.279-04:00',
-        closestRegionalOffice: null,
-        createdAt: '2022-08-08T22:11:34.279-04:00',
-        closedAt: '2022-08-08T22:11:55.204-04:00',
-        startedAt: null,
-        status: 'completed',
+        externalAppealId: '419ce568-387c-4ac6-a5f5-00a1554cea36',
         assigneeName: 'Prosthetics',
         assignedTo: {
           cssId: null,
@@ -497,151 +355,11 @@ export const camoToProgramOfficeToCamoData = {
       }
     },
     appeals: {
-      'bc7ba6d4-48a7-436b-97e8-62bc061c604e': {
+      '419ce568-387c-4ac6-a5f5-00a1554cea36': {
         id: '1632',
-        appellant_hearing_email_recipient: null,
-        representative_hearing_email_recipient: null,
-        externalId: 'bc7ba6d4-48a7-436b-97e8-62bc061c604e',
-        docketName: 'direct_review',
-        withdrawn: false,
-        removed: false,
-        overtime: false,
-        contestedClaim: false,
-        veteranAppellantDeceased: false,
-        isLegacyAppeal: false,
-        caseType: 'Original',
-        isAdvancedOnDocket: false,
-        issueCount: 1,
-        docketNumber: '220731-1632',
-        assignedAttorney: null,
-        assignedJudge: null,
-        distributedToJudge: false,
-        veteranFullName: 'Bob Smithframi',
-        veteranFileNumber: '500000009',
-        isPaperCase: false,
-        readableHearingRequestType: null,
-        readableOriginalHearingRequestType: null,
-        vacateType: null,
-        hearings: [],
-        currentUserEmail: null,
-        currentUserTimezone: 'America/New_York',
-        completedHearingOnPreviousAppeal: false,
-        issues: [
-          {
-            id: 3080,
-            program: 'vha',
-            description: 'Caregiver | Tier Level - Caregiver Queue Test',
-            notes: null,
-            diagnostic_code: null,
-            remand_reasons: [],
-            closed_status: null,
-            decision_date: '2022-07-31'
-          }
-        ],
-        decisionIssues: [],
-        canEditRequestIssues: false,
-        unrecognizedAppellantId: null,
-        appellantIsNotVeteran: false,
-        appellantFullName: 'Bob Smithframi',
-        appellantFirstName: 'Bob',
-        appellantMiddleName: null,
-        appellantLastName: 'Smithframi',
-        appellantSuffix: null,
-        appellantDateOfBirth: '1992-08-02',
-        appellantAddress: {
-          address_line_1: '9999 MISSION ST',
-          address_line_2: 'UBER',
-          address_line_3: 'APT 2',
-          city: 'SAN FRANCISCO',
-          zip: '94103',
-          country: 'USA',
-          state: 'CA'
-        },
-        appellantEmailAddress: 'Bob.Smithframi@test.com',
-        appellantPhoneNumber: null,
-        appellantType: 'VeteranClaimant',
-        appellantPartyType: null,
-        appellantTz: 'America/Los_Angeles',
-        appellantRelationship: 'Veteran',
-        hasPOA: {
-          id: 1490,
-          authzn_change_clmant_addrs_ind: null,
-          authzn_poa_access_ind: null,
-          claimant_participant_id: '500000009',
-          created_at: '2022-08-02T12:02:34.112-04:00',
-          file_number: '00001234',
-          last_synced_at: '2022-08-02T12:02:34.112-04:00',
-          legacy_poa_cd: '100',
-          poa_participant_id: '600153863',
-          representative_name: 'Clarence Darrow',
-          representative_type: 'Attorney',
-          updated_at: '2022-08-02T12:02:34.112-04:00'
-        },
-        assignedToLocation: 'VHA CAMO',
-        veteranDateOfDeath: null,
-        closestRegionalOffice: null,
-        closestRegionalOfficeLabel: null,
-        availableHearingLocations: [],
-        status: 'pre_docketed',
-        decisionDate: null,
-        nodDate: '2022-07-31',
-        nodDateUpdates: [],
-        certificationDate: null,
-        powerOfAttorney: {
-          representative_type: 'Attorney',
-          representative_name: 'Clarence Darrow',
-          representative_address: {
-            address_line_1: '9999 MISSION ST',
-            address_line_2: 'UBER',
-            address_line_3: 'APT 2',
-            city: 'SAN FRANCISCO',
-            zip: '94103',
-            country: 'USA',
-            state: 'CA'
-          },
-          representative_email_address: 'jamie.fakerton@caseflowdemo.com',
-          representative_tz: 'America/Los_Angeles',
-          poa_last_synced_at: '2022-08-02T12:02:34.112-04:00'
-        },
-        cavcRemand: null,
-        regionalOffice: null,
-        caseflowVeteranId: 10,
-        documentID: null,
-        caseReviewId: null,
-        canEditDocumentId: false,
-        attorneyCaseRewriteDetails: {
-          note_from_attorney: null,
-          untimely_evidence: null
-        },
-        docketSwitch: null,
-        switchedDockets: [],
-        appellantSubstitution: null,
-        substitutions: [],
-        hasSameAppealSubstitution: true,
-        remandSourceAppealId: null,
-        remandJudgeName: null,
-        veteranInfo: {
-          veteran: {
-            full_name: 'Bob Smithframi',
-            gender: 'F',
-            date_of_birth: '08/02/1992',
-            date_of_death: null,
-            email_address: 'Bob.Smithframi@test.com',
-            address: {
-              address_line_1: '1234 Main Street',
-              address_line_2: null,
-              address_line_3: null,
-              city: 'Orlando',
-              state: 'FL',
-              zip: '12345',
-              country: 'USA'
-            },
-            relationships: null
-          }
-        }
+        externalId: '419ce568-387c-4ac6-a5f5-00a1554cea36'
       }
     }
   },
   ...uiData
 };
-/* eslint-enable max-lines */
