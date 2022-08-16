6f3d3312d0f1e9df9fa8107aad0ef3d7854ae99f
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-08-16 12:00:57 -0400

    Remove reader link test since that attr wasn't used to conditionally display the reader link column. Adjust some of the test setups.

--- spec/models/queue_tabs/vha_caregiver_support_unassigned_tasks_tab_spec.rb
+++ spec/models/queue_tabs/vha_caregiver_support_unassigned_tasks_tab_spec.rb
@@ -2,38 +2,15 @@
 
 describe VhaCaregiverSupportUnassignedTasksTab, :postgres do
   let(:tab) { VhaCaregiverSupportUnassignedTasksTab.new(params) }
-  let(:params) do
-    {
-      assignee: assignee,
-      show_reader_link_column: show_reader_link_column
-    }
-  end
-  let(:assignee) { create(:vha_caregiver_support) }
-  let(:show_reader_link_column) { false }
+  let(:params) { { assignee: assignee } }
+  let(:assignee) { VhaCaregiverSupport.singleton }
 
   describe ".column_names" do
     subject { tab.column_names }
 
     context "when only the assignee argument is passed when instantiating an VhaCaregiverSupportUnassignedTasksTab" do
-      let(:params) { { assignee: create(:vha_caregiver_support) } }
-
       it "returns the correct number of columns" do
         expect(subject.length).to eq(8)
-      end
-    end
-
-    context "when we want to show the reader link column" do
-      let(:show_reader_link_column) { true }
-      let(:params) do
-        { assignee: create(:vha_caregiver_support),
-          show_reader_link_column: show_reader_link_column }
-      end
-
-      it "returns the correct number of columns" do
-        expect(subject.length).to eq(8)
-      end
-      it "includes the reader link column" do
-        expect(subject).to include(Constants.QUEUE_CONFIG.COLUMNS.DOCUMENT_COUNT_READER_LINK.name)
       end
     end
   end
@@ -54,7 +31,6 @@ describe VhaCaregiverSupportUnassignedTasksTab, :postgres do
 
     context "when we want to show the user the description" do
       it "matches description for unassigned tasks tab" do
-        expect(subject).to eq(COPY::VHA_CAREGIVER_SUPPORT_QUEUE_PAGE_UNASSIGNED_TASKS_DESCRIPTION)
         expect(subject).to eq("Cases assigned to VHA Caregiver Support Program:")
       end
     end
