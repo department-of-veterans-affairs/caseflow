5b3ed517c0de1bf38164499025edd286b26c9dca
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-08-16 08:10:47 -0400

    DRY up tests

--- client/test/app/queue/components/CompleteTaskModal.test.js
+++ client/test/app/queue/components/CompleteTaskModal.test.js
@@ -58,6 +58,18 @@ const renderCompleteTaskModal = (modalType, storeValues, taskType) => {
   );
 };
 
+const enterModalOptions = (radioSelection, instructionsFieldName, instructions) => {
+  const radioFieldToSelect = screen.getByLabelText(radioSelection);
+  const instructionsField = screen.getByRole('textbox', { name: instructionsFieldName });
+
+  userEvent.click(radioFieldToSelect);
+  userEvent.type(instructionsField, instructions);
+
+  userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+};
+
+const getReceivedInstructions = () => requestPatchSpy.mock.calls[0][1].data.task.instructions;
+
 beforeEach(() => {
   requestPatchSpy = jest.spyOn(uiActions, 'requestPatch').
     mockImplementation(() => jest.fn(() => Promise.resolve({
@@ -84,36 +96,26 @@ describe('CompleteTaskModal', () => {
     test('CAMO Notes section only appears once whenever CAMO sends appeal back to BVA Intake', () => {
       renderCompleteTaskModal('vha_send_to_board_intake', camoToBvaIntakeData, taskType);
 
-      const radioFieldToSelect = screen.getByLabelText('Correct documents have been successfully added');
-      const instructionsField = screen.getByRole('textbox', { name: 'Provide additional context and/or documents:' });
+      enterModalOptions(
+        'Correct documents have been successfully added',
+        'Provide additional context and/or documents:',
+        'CAMO -> BVA Intake'
+      );
 
-      userEvent.click(radioFieldToSelect);
-      userEvent.type(instructionsField, 'CAMO -> BVA Intake');
-
-      userEvent.click(screen.getByRole('button', { name: 'Submit' }));
-
-      let dataParam = requestPatchSpy.mock.calls[0][1];
-      let taskInstructions = dataParam.data.task.instructions;
-
-      expect(taskInstructions).toBe('\n**CAMO Notes:** CAMO -> BVA Intake');
+      expect(getReceivedInstructions()).toBe('\n**CAMO Notes:** CAMO -> BVA Intake');
     });
 
     test('PO Details appear next to Program Office Notes section', () => {
       renderCompleteTaskModal('vha_send_to_board_intake', camoToProgramOfficeToCamoData, taskType);
 
-      const radioFieldToSelect = screen.getByLabelText('Correct documents have been successfully added');
-      const instructionsField = screen.getByRole('textbox', { name: 'Provide additional context and/or documents:' });
-
-      userEvent.click(radioFieldToSelect);
-      userEvent.type(instructionsField, 'CAMO -> BVA Intake');
-
-      userEvent.click(screen.getByRole('button', { name: 'Submit' }));
-
-      let dataParam = requestPatchSpy.mock.calls[0][1];
-      let taskInstructions = dataParam.data.task.instructions;
+      enterModalOptions(
+        'Correct documents have been successfully added',
+        'Provide additional context and/or documents:',
+        'CAMO -> BVA Intake'
+      );
 
       /* eslint-disable-next-line max-len */
-      expect(taskInstructions).toBe('\n**Status:** Correct documents have been successfully added\n\n**CAMO Notes:** CAMO -> BVA Intake\n\n**Program Office Notes:** Documents for this appeal are stored in VBMS.\n\n**Detail:**\n\n PO back to CAMO!\n\n');
+      expect(getReceivedInstructions()).toBe('\n**Status:** Correct documents have been successfully added\n\n**CAMO Notes:** CAMO -> BVA Intake\n\n**Program Office Notes:** Documents for this appeal are stored in VBMS.\n\n**Detail:**\n\n PO back to CAMO!\n\n');
     });
   });
 });
