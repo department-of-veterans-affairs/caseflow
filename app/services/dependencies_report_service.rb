030b7120d67980babc3d64304f33e5e557ac05d8
Enrilo Ugalde <ugalde_enrilo@bah.com>
2022-08-17 11:57:39 -0700

    made changes to report service to only return Name ex 'BGS'

--- app/services/dependencies_report_service.rb
+++ app/services/dependencies_report_service.rb
@@ -3,19 +3,29 @@
 class DependenciesReportService
   class << self
 
-    ALL_DEPENDENCIES_KEYS =
-      [:degraded_service_banner_bgs,
-        :degraded_service_banner_vbms,
-        :degraded_service_banner_vva,
-        :degraded_service_banner_vacols,
-        :degraded_service_banner_gov_delivery,
-        :degraded_service_banner_va_dot_gov].freeze
+    ALL_DEPENDENCIES_MAP = {
+      degraded_service_banner_bgs: "BGS",
+      degraded_service_banner_vbms: "VBMS",
+      degraded_service_banner_vva: "VVA",
+      degraded_service_banner_vacols: "VACOLS",
+      degraded_service_banner_gov_delivery: "GOV DELIVERY",
+      degraded_service_banner_va_dot_gov: "VA DOT GOV"
+    }
+      # [:degraded_service_banner_bgs,
+      #   :degraded_service_banner_vbms,
+      #   :degraded_service_banner_vva,
+      #   :degraded_service_banner_vacols,
+      #   :degraded_service_banner_gov_delivery,
+      #   :degraded_service_banner_va_dot_gov].freeze
 
     def dependencies_report
 
-      keys = Rails.cache.read_multi(*ALL_DEPENDENCIES_KEYS)
+      keys = Rails.cache.read_multi(*ALL_DEPENDENCIES_MAP.keys())
           degraded_arr = keys.select { |k,v| v == :display }
-          degraded_arr.keys()
+          # Display Systems = [:degraded_service_banner_bgs, :degraded_service_banner_vbms]
+          displaySystems = degraded_arr.keys()
+          # Loop through displaySystems with to create a new array with the NAME ["BGS", "VBMS"]
+          displaySystems.map{|degradedServiceKey| ALL_DEPENDENCIES_MAP[degradedServiceKey] }
 
     rescue StandardError => error
       Rails.logger.warn "Exception thrown while checking dependency "\
