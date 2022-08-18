76bb50a2589ebe4ec0c90bec2e5ce57860145d2d
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2022-08-18 12:16:11 -0400

    MattT/APPEALS-7453: Appeals With Caregiver Labels are Correctly Routed to CSP Organization's Queue (#17343)
    * Route caregiver appeals to CSP
    
    * Add test for CSP routing to
    
    pre_docket_tasks_factory_spec.rb
    
    * #caregiver_issue? test
    
    * Change caregiver_issue? to caregiver_has_issues?
    
    * Swap conditional as VhaCamo is the default assignee
    
    * Add caregiver intake feature test
    
    * Add additional assertions to existing VHA CAMO intake feature test
    
    Co-authored-by: Matthew Thornton <ThorntonMatthew@users.noreply.github.com>

--- spec/workflows/pre_docket_tasks_factory_spec.rb
+++ spec/workflows/pre_docket_tasks_factory_spec.rb
@@ -5,71 +5,84 @@ describe PreDocketTasksFactory, :postgres do
     before { bva_intake.add_user(bva_intake_user) }
 
     let(:bva_intake) { BvaIntake.singleton }
-    let(:camo) { VhaCamo.singleton }
     let(:bva_intake_user) { create(:intake_user) }
-
-    let(:appeal) { create(:appeal, intake: create(:intake, user: bva_intake_user)) }
 
     subject { PreDocketTasksFactory.new(appeal).call_vha }
 
-    it "creates a PreDocket task and child CAMO task" do
-      expect(appeal.tasks.count).to eq 0
+    shared_examples "VhaDocumentSearchTask is assigned to assignee" do
+      it "creates a PreDocket task and child VhaDocumentSearchTask" do
+        expect(appeal.tasks.count).to eq 0
 
-      subject
+        subject
 
-      pre_docket_task = Task.find_by(
-        appeal: appeal,
-        type: "PreDocketTask",
-        status: Constants.TASK_STATUSES.on_hold,
-        assigned_to: bva_intake
-      )
-      camo_task = pre_docket_task.children.first
+        pre_docket_task = Task.find_by(
+          appeal: appeal,
+          type: "PreDocketTask",
+          status: Constants.TASK_STATUSES.on_hold,
+          assigned_to: bva_intake
+        )
+        camo_task = pre_docket_task.children.first
 
-      expect(pre_docket_task).to_not be nil
-      expect(pre_docket_task.parent.is_a?(RootTask)).to be true
-      expect(camo_task).to have_attributes(
-        type: "VhaDocumentSearchTask",
-        status: Constants.TASK_STATUSES.assigned,
-        assigned_by: bva_intake_user,
-        assigned_to: camo,
-        parent: pre_docket_task
-      )
+        expect(pre_docket_task).to_not be nil
+        expect(pre_docket_task.parent.is_a?(RootTask)).to be true
+        expect(camo_task).to have_attributes(
+          type: "VhaDocumentSearchTask",
+          status: Constants.TASK_STATUSES.assigned,
+          assigned_by: bva_intake_user,
+          assigned_to: assignee,
+          parent: pre_docket_task
+        )
+      end
+    end
+
+    context "with an appeal without a caregiver issue" do
+      let(:appeal) { create(:appeal, intake: create(:intake, user: bva_intake_user)) }
+      let(:assignee) { VhaCamo.singleton }
+
+      it_behaves_like "VhaDocumentSearchTask is assigned to assignee"
+    end
+
+    context "with an appeal with a caregiver issue" do
+      let(:appeal) { create(:appeal, :with_vha_issue, intake: create(:intake, user: bva_intake_user)) }
+      let(:assignee) { VhaCaregiverSupport.singleton }
+
+      it_behaves_like "VhaDocumentSearchTask is assigned to assignee"
     end
   end
+end
 
-  context "PreDocket Appeals for Education" do
-    before { bva_intake.add_user(bva_intake_user) }
+context "PreDocket Appeals for Education" do
+  before { bva_intake.add_user(bva_intake_user) }
 
-    let(:bva_intake) { BvaIntake.singleton }
-    let(:emo) { EducationEmo.singleton }
-    let(:bva_intake_user) { create(:intake_user) }
+  let(:bva_intake) { BvaIntake.singleton }
+  let(:emo) { EducationEmo.singleton }
+  let(:bva_intake_user) { create(:intake_user) }
 
-    let(:appeal) { create(:appeal, intake: create(:intake, user: bva_intake_user)) }
+  let(:appeal) { create(:appeal, intake: create(:intake, user: bva_intake_user)) }
 
-    subject { PreDocketTasksFactory.new(appeal).call_edu }
+  subject { PreDocketTasksFactory.new(appeal).call_edu }
 
-    it "creates a PreDocket task and child Education task" do
-      expect(appeal.tasks.count).to eq 0
+  it "creates a PreDocket task and child Education task" do
+    expect(appeal.tasks.count).to eq 0
 
-      subject
+    subject
 
-      pre_docket_task = Task.find_by(
-        appeal: appeal,
-        type: "PreDocketTask",
-        status: Constants.TASK_STATUSES.on_hold,
-        assigned_to: bva_intake
-      )
-      emo_task = pre_docket_task.children.first
+    pre_docket_task = Task.find_by(
+      appeal: appeal,
+      type: "PreDocketTask",
+      status: Constants.TASK_STATUSES.on_hold,
+      assigned_to: bva_intake
+    )
+    emo_task = pre_docket_task.children.first
 
-      expect(pre_docket_task).to_not be nil
-      expect(pre_docket_task.parent.is_a?(RootTask)).to be true
-      expect(emo_task).to have_attributes(
-        type: "EducationDocumentSearchTask",
-        status: Constants.TASK_STATUSES.assigned,
-        assigned_by: bva_intake_user,
-        assigned_to: emo,
-        parent: pre_docket_task
-      )
-    end
+    expect(pre_docket_task).to_not be nil
+    expect(pre_docket_task.parent.is_a?(RootTask)).to be true
+    expect(emo_task).to have_attributes(
+      type: "EducationDocumentSearchTask",
+      status: Constants.TASK_STATUSES.assigned,
+      assigned_by: bva_intake_user,
+      assigned_to: emo,
+      parent: pre_docket_task
+    )
   end
 end
