d6915114c9ac9dc38fae7a82dd5b5485e2479853
j-n-t-l <611441@bah.com>
2022-08-18 09:22:42 -0400

    APPEALS-7004 updated conditional for ihptaskcomplete

--- app/models/prepend/va_notify/ihp_task_complete.rb
+++ app/models/prepend/va_notify/ihp_task_complete.rb
@@ -7,19 +7,11 @@ module IhpTaskComplete
   @@template_name = self.to_s
   # rubocop:enable all
 
-  # def update_status_if_children_tasks_are_closed(child_task)
-  #   # original method defined in app/models/task.rb
-  #   super
-  #   if %w[InformalHearingPresentationTask IhpColocatedTask].include?(type) 
-  #     AppellantNotification.notify_appellant(appeal, @@template_name)
-  #   end
-  # end
-
+  # original method in app/models/task.rb
   def update_from_params(params, user)
     super
     if %w[InformalHearingPresentationTask IhpColocatedTask].include?(type) &&
-       params[:status] == Constants.TASK_STATUSES.completed &&
-       params[:assigned_to_type] == "Organization"
+       params[:status] == Constants.TASK_STATUSES.completed
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
   end
