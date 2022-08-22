bbbaea811a4cb701c732f66ec9ed81a9f712c6da
KiMauVA <Ki.Mau@va.gov>
2022-08-22 13:24:18 -0400

    APPEALS-7006 - Initial Setup

---
+++ app/jobs/vanotify/fetch_all_active_ama_appeals_job.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Job to fetch all currently active AMA Appeals
+class FetchAllActiveAppeals < CaseflowJob
+    queue_with_priority :low_priority
+  
+    def create_ama_appeals_list
+      Rails.logger.info "Fetch Active AMA Appeals"
+    end
+
+    def create_hlr_appeals_list
+        Rails.logger.info "Fetch Active High Level Review Appeals"
+    end
+
+    def create_supplemental_appeals_list
+        Rails.logger.info "Fetch Active Supplemental Claims Appeals"
+    end
+
+    def create_legacy_appeals_list
+        Rails.logger.info "Fetch Active Legacy Appeals"
+    end
+end
