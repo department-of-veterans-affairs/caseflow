e0b64e51f172ca0da1b795b8b31b76e8008a1414
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-08-22 16:47:57 -0400

    Remove apepals from VHA CSP's Completed tab if the appeal is returned to VHA CSP from BVA Intake

--- app/models/queue_tabs/vha_caregiver_support_completed_tasks_tab.rb
+++ app/models/queue_tabs/vha_caregiver_support_completed_tasks_tab.rb
@@ -16,7 +16,16 @@ class VhaCaregiverSupportCompletedTasksTab < QueueTab
   end
 
   def tasks
-    recently_completed_tasks
+    parent_task_ids = recently_completed_tasks.map(&:parent_id)
+
+    # For every appeal that has a recently completed VHA CSP task, determine
+    # which VHA CSP task is actually the most recent for the entire appeal
+    # so that the appeal doesn't show up multiple times in the VHA CSP's queue.
+    most_overall_recent_tasks = Task.where(parent_id: parent_task_ids, assigned_to: assignee)
+      .group(:appeal_id)
+      .maximum(:id)
+
+    Task.where(id: most_overall_recent_tasks.values).recently_completed
   end
 
   def column_names
