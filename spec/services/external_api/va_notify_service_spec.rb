e04d08811675c0b6e26b075248f6cd10ddb833e4
Eric Moore <moore_eric@bah.com>
2022-08-23 16:26:51 -0400

    edited rspec test

--- spec/services/external_api/va_notify_service_spec.rb
+++ spec/services/external_api/va_notify_service_spec.rb
@@ -13,6 +13,7 @@ describe ExternalApi::VANotifyService do
   let(:error_response_body) { { "result": "error", "message": { "token": ["error"] } }.to_json }
   let(:phone_number) { "+19876543210" }
   let(:status) { "in-progress" }
+  let(:participant_id) {"123456789"}
   let(:success_response) do
     HTTPI::Response.new(200, {}, notification_response_body)
   end
@@ -40,7 +41,7 @@ describe ExternalApi::VANotifyService do
 
   context "notifications sent" do
     describe "email and sms sent" do
-      subject { ExternalApi::VANotifyService.send_notifications(email_address, email_template_id, nil, nil, status) }
+      subject { ExternalApi::VANotifyService.send_notifications(participant_id, email_template_id, nil) }
       it "email and sms sent successfully" do
         allow(HTTPI).to receive(:post).and_return(success_response)
         expect(subject.body["template"]["id"]).to eq(email_template_id)
