daeef2f4825044f7c0e0604911acb41fe2a568f5
Matt Roth <roth_matthew@bah.com>
2022-08-24 10:43:10 -0400

    move scheduled jobs list to it's own file

--- spec/controllers/api/v1/jobs_controller_spec.rb
+++ spec/controllers/api/v1/jobs_controller_spec.rb
@@ -34,14 +34,6 @@ RSpec.describe Api::V1::JobsController, :postgres, type: :controller do
     end
   end
 
-  context "for all jobs" do
-    Api::V1::JobsController::SUPPORTED_JOBS.each_value do |job_class|
-      it "#{job_class.name} is not queued in default queue" do
-        expect(job_class.queue_name).not_to eq("default")
-      end
-    end
-  end
-
   def response_body
     JSON.parse(response.body)
   end
