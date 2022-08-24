0d82926e88be6ca648f682900e4fb40cd5c187dd
Stephan Ward <stephan.ward@va.gov>
2022-08-24 14:39:17 -0400

    Cleaned

--- client/app/intake/pages/SplitAppealConfirm.jsx
+++ client/app/intake/pages/SplitAppealConfirm.jsx
@@ -1,151 +1,16 @@
 import React, { useContext } from 'react';
 import { StateContext } from '../../intakeEdit/IntakeEditFrame';
 import COPY from '../../../COPY';
-import Table from '../../components/Table';
 
 const SplitAppealConfirm = () => {
   const { reason } = useContext(StateContext);
-  const columns = React.useMemo(() => (
-    [
-      {
-        id: '1',
-        Header: '',
-        accessor: '',
-        Footer: '',
-        columns: [
-          {
-            id: '2',
-            Header: 'Veteran',
-            accessor: 'veteran',
-            Footer: '',
-            columns: [
-              {
-                id: '3',
-                Header: 'Docket Number',
-                accessor: 'stream_docket_number',
-                Footer: '',
-                columns: [
-                  {
-                    id: '4',
-                    Header: 'Review Option',
-                    accessor: 'review_option',
-                    Footer: '',
-                    columns: [
-                      {
-                        id: '5',
-                        Header: 'Issues(s)',
-                        accessor: 'Key0',
-                        Footer: '',
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      },
-      {
-        id: '6',
-        Header: 'Original Appeal Stream',
-        accessor: 'original_appeal_stream',
-        Footer: '',
-        columns: [
-          {
-            id: '7',
-            Header: 'Veteran Name Placeholder',
-            accessor: 'veteran_name_placeholder',
-            Footer: '',
-            columns: [
-              {
-                id: '8',
-                Header: 'Docket Number Value Placeholder',
-                accessor: 'docket_number_value_placeholder',
-                Footer: '',
-                columns: [
-                  {
-                    id: '9',
-                    Header: 'Review Option Placeholder',
-                    accessor: 'review_option_placeholder',
-                    Footer: '',
-                    columns: [
-                      {
-                        id: '10',
-                        Header: 'Issues Placeholder',
-                        Footer: '',
-                        accessor: 'Key1'
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      },
-      {
-        id: '11',
-        Header: 'New Appeal Stream',
-        accessor: 'new_appeal_stream',
-        Footer: '',
-        columns: [
-          {
-            id: '12',
-            Header: 'Veteran Name Placeholder',
-            accessor: 'veteran_name_placeholder',
-            Footer: '',
-            columns: [
-              {
-                id: '13',
-                Header: 'Docket Number Value Placeholder',
-                accessor: 'docket_number_value_placeholder',
-                Footer: '',
-                columns: [
-                  {
-                    id: '14',
-                    Header: 'Review Option Placeholder',
-                    accessor: 'review_option_placeholder',
-                    Footer: '',
-                    columns: [
-                      {
-                        id: '15',
-                        Header: 'Issues Placeholder',
-                        Footer: '',
-                        accessor: 'Key2'
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  ), []);
-  const rowObjects = React.useMemo(() => (
-    [
-      {
-        Key0: 'Julian',
-        Key1: 'hhs.gov',
-        Key2: 'Teal'
-      },
-      {
-        Key0: 'Ketti',
-        Key1: 'desdev.cn',
-        Key2: 'Turquoise'
-      },
-      {
-        Key0: 'Fallon',
-        Key1: 'elegantthemes.com',
-        Key2: 'Yellow'
-      },
-      {
-        Key0: 'Leela',
-        Key1: 'oakley.com',
-        Key2: 'Crimson'
-      },
-    ]), []);
+  const data = [
+    { col1: 'Veteran', col2: 'Some Custom Data', col3: 'Some Custom Data'},
+    { col1: 'Appellant', col2: 'Some Custom Data', col3: 'Some Custom Data'},
+    { col1: 'Docket Number', col2: 'Some Custom Data', col3: 'Some Custom Data'},
+    { col1: 'Review Option', col2: 'Some Custom Data', col3: 'Some Custom Data'},
+    { col1: 'Issues(s)', col2: 'Some Custom Data', col3: 'Some Custom Data'},
+  ];
 
   return (
     <>
@@ -159,29 +24,27 @@ const SplitAppealConfirm = () => {
         <span style={{ flexBasis: '75%' }}>{reason}</span>
       </div>
       <br /><br />
-      <p>Here we will add the table</p>
+      <p>The table currently dynamically renders data from an array.
+        The Array.map() method allows you to iterate over an array and modify
+        its elements using a callback function.The callback function will then
+        be executed on each of the array’s elements. In this case, we will just
+        return a table row on each iteration</p>
       <div className="App">
         <table>
           <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Gender</th>
+            <th></th>
+            <th>Original Appeal Stream</th>
+            <th>New Appeal Stream</th>
           </tr>
-          <tr>
-            <td>Anom</td>
-            <td>19</td>
-            <td>Male</td>
-          </tr>
-          <tr>
-            <td>Megha</td>
-            <td>19</td>
-            <td>Female</td>
-          </tr>
-          <tr>
-            <td>Subham</td>
-            <td>25</td>
-            <td>Male</td>
-          </tr>
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.col1}</td>
+                <td>{val.col2}</td>
+                <td>{val.col3}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
     </>
