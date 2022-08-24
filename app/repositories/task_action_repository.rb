b8ccee4f8783517ed9b1b8457bbd4cba849b687e
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-08-24 16:15:56 -0400

    Revert bva_intake_return_to_caregiver update

--- app/repositories/task_action_repository.rb
+++ app/repositories/task_action_repository.rb
@@ -561,15 +561,17 @@ class TaskActionRepository
     end
 
     def bva_intake_return_to_caregiver(task, _user)
-      caregiver_org = VhaCaregiverSupport.singleton
+      org = Organization.find(task.assigned_to_id)
+      queue_url = org.url
+      caregiver = VhaCaregiverSupport.singleton
       {
-        selected: caregiver_org,
-        options: [{ label: caregiver_org.name, value: caregiver_org.id }],
+        selected: caregiver,
+        options: [{ label: caregiver.name, value: caregiver.id }],
         modal_title: COPY::BVA_INTAKE_RETURN_TO_CAREGIVER_MODAL_TITLE,
         modal_body: COPY::BVA_INTAKE_RETURN_TO_CAREGIVER_MODAL_BODY,
         message_title: format(COPY::BVA_INTAKE_RETURN_TO_CAREGIVER_CONFIRMATION_TITLE, task.appeal.veteran_full_name),
         type: VhaDocumentSearchTask.name,
-        redirect_after: "/organizations/#{caregiver_org.url}"
+        redirect_after: "/organizations/#{queue_url}"
       }
     end
 
