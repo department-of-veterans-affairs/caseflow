a818349d24623b6ee55796364b96c1f00b82d04e
Stephan Ward <stephan.ward@va.gov>
2022-08-25 17:23:24 -0400

    Too much refactoring

--- client/app/intake/pages/ReviewAppealView.jsx
+++ client/app/intake/pages/ReviewAppealView.jsx
@@ -2,14 +2,98 @@ import React, { useContext } from 'react';
 import COPY from '../../../COPY';
 import PropTypes from 'prop-types';
 import { StateContext } from '../../intakeEdit/IntakeEditFrame';
+import { css } from 'glamor';
+import SearchableDropdown from '../../components/SearchableDropdown';
+import TextareaField from '../../components/TextareaField';
+import CheckboxGroup from '../../components/CheckboxGroup';
+import SPLIT_APPEAL_REASONS from '../../../constants/SPLIT_APPEAL_REASONS';
+import { formatDateStr } from '../../util/DateUtil';
+import BENEFIT_TYPES from '../../../constants/BENEFIT_TYPES';
+import _ from 'lodash';
+
+const issueListStyling = css({ marginTop: '0rem', marginLeft: '6rem' });
 
 const ReviewAppealView = (props) => {
   const { serverIntake } = props;
+  const {
+    reason,
+    setReason,
+    otherReason,
+    setOtherReason,
+    selectedIssues,
+    setSelectedIssues
+  } = useContext(StateContext);
   const requestIssues = serverIntake.requestIssues;
-  const { reason } = useContext(StateContext);
+
+  const onReasonChange = (selection) => {
+    setReason(selection.value);
+  };
+
+  const reasonOptions = _.map(SPLIT_APPEAL_REASONS, (value) => ({
+    label: value,
+    value
+  }));
+
+  const onOtherReasonChange = (value) => {
+    setOtherReason(value);
+  };
+
+  const onIssueChange = (evt) => {
+    setSelectedIssues({ ...selectedIssues, [evt.target.name]: evt.target.checked });
+  };
+
+  const issueOptions = () => requestIssues.map((issue) => ({
+    id: issue.id.toString(),
+    label:
+      <>
+        <span>{issue.description}</span><br />
+        <span>Benefit Type: {BENEFIT_TYPES[issue.benefit_type]}</span><br />
+        <span>Decision Date: {formatDateStr(issue.approx_decision_date)}</span>
+        <br /><br />
+      </>
+  }));
 
   return (
     <>
+      <>
+        <h1>{COPY.SPLIT_APPEAL_CREATE_TITLE}</h1>
+        <span>{COPY.SPLIT_APPEAL_CREATE_SUBHEAD}</span>
+        <br /><br />
+        <SearchableDropdown
+          name="splitAppealReasonDropdown"
+          label={COPY.SPLIT_APPEAL_CREATE_REASONING_TITLE}
+          strongLabel
+          value={reason}
+          onChange={onReasonChange}
+          options={reasonOptions}
+        />
+        <br />
+        {reason === 'Other' && (
+          <TextareaField
+            name="reason"
+            label="Reason for split"
+            id="otherReason"
+            textAreaStyling={css({ height: '50px' })}
+            maxlength={350}
+            value={otherReason}
+            onChange={onOtherReasonChange}
+            optional
+          />
+        )}
+        <br />
+        <h3>{COPY.SPLIT_APPEAL_CREATE_SELECT_ISSUES_TITLE}</h3>
+        <CheckboxGroup
+          vertical
+          name="issues"
+          label={COPY.SPLIT_APPEAL_CREATE_SELECT_ISSUES_TITLE}
+          hideLabel
+          values={selectedIssues}
+          onChange={(val) => onIssueChange(val)}
+          options={issueOptions()}
+          styling={issueListStyling}
+          strongLabel
+        />
+      </>
       <div>
         <h1 style={{ margin: '0px' }}>{COPY.SPLIT_APPEAL_REVIEW_TITLE}</h1>
         <span>{COPY.SPLIT_APPEAL_REVIEW_SUBHEAD}</span>
