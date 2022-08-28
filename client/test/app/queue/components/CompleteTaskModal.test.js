f25337276b6127d60fb8d8be7c1da28a50778318
Billy Stockton <Billy.Stockton@va.gov>
2022-08-28 15:15:50 -0400

    Jest Test for Case Details Formatting changes

--- client/test/app/queue/components/CompleteTaskModal.test.js
+++ client/test/app/queue/components/CompleteTaskModal.test.js
@@ -5,11 +5,15 @@ import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
+import COPY from '../../../../COPY';
 
 import {
   postData,
   camoToBvaIntakeData,
-  camoToProgramOfficeToCamoData
+  camoToProgramOfficeToCamoData,
+  caregiverToIntakeData,
+  emoToBvaIntakeData,
+  rpoToBvaIntakeData
 } from '../../../data/queue/taskActionModals/completeTaskActionModalData';
 import * as uiActions from 'app/queue/uiReducer/uiActions';
 import CompleteTaskModal from 'app/queue/components/CompleteTaskModal';
@@ -58,14 +62,19 @@ const renderCompleteTaskModal = (modalType, storeValues, taskType) => {
   );
 };
 
-const enterModalOptions = (radioSelection, instructionsFieldName, instructions) => {
+const enterModalOptions = (radioSelection, instructionsFieldName, instructions, buttonText, otherSource) => {
   const radioFieldToSelect = screen.getByLabelText(radioSelection);
   const instructionsField = screen.getByRole('textbox', { name: instructionsFieldName });
 
   userEvent.click(radioFieldToSelect);
   userEvent.type(instructionsField, instructions);
+  if (otherSource) {
+    const otherSourceField = screen.getByRole('textbox', { name: 'If "Other" was chosen indicate the source.' });
 
-  userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    userEvent.type(otherSourceField, otherSource)
+  }
+
+  userEvent.click(screen.getByRole('button', { name: buttonText  }));
 };
 
 const getReceivedInstructions = () => requestPatchSpy.mock.calls[0][1].data.task.instructions;
@@ -86,6 +95,7 @@ afterEach(() => {
 describe('CompleteTaskModal', () => {
   describe('vha_send_to_board_intake', () => {
     const taskType = 'VhaDocumentSearchTask';
+    const buttonText = COPY.MODAL_SUBMIT_BUTTON;
 
     test('modal title is Send to Board Intake', () => {
       renderCompleteTaskModal('vha_send_to_board_intake', camoToBvaIntakeData, taskType);
@@ -99,7 +109,8 @@ describe('CompleteTaskModal', () => {
       enterModalOptions(
         'Correct documents have been successfully added',
         'Provide additional context and/or documents:',
-        'CAMO -> BVA Intake'
+        'CAMO -> BVA Intake',
+        buttonText
       );
 
       expect(getReceivedInstructions()).toBe(
@@ -114,7 +125,8 @@ describe('CompleteTaskModal', () => {
       enterModalOptions(
         'Correct documents have been successfully added',
         'Provide additional context and/or documents:',
-        'CAMO -> BVA Intake'
+        'CAMO -> BVA Intake',
+        buttonText
       );
 
       expect(getReceivedInstructions()).toBe(
@@ -122,6 +134,129 @@ describe('CompleteTaskModal', () => {
         '**CAMO Notes:** CAMO -> BVA Intake\n\n' +
         '**Program Office Notes:** Documents for this appeal are stored in VBMS.\n\n' +
         '**Detail:**\n\n PO back to CAMO!\n\n'
+      );
+    });
+  });
+
+  describe('vha_caregiver_support_send_to_board_intake_for_review', () => {
+    const taskType = 'VhaDocumentSearchTask';
+    const buttonText = COPY.MODAL_SEND_BUTTON
+
+    test('modal title is Ready for Review', () => {
+      renderCompleteTaskModal('vha_caregiver_support_send_to_board_intake_for_review', caregiverToIntakeData, taskType);
+      expect(screen.getByText('Ready for Review')).toBeTruthy();
+    });
+
+    test('When VBMS is chosen in Modal', () => {
+      renderCompleteTaskModal('vha_caregiver_support_send_to_board_intake_for_review', caregiverToIntakeData, taskType);
+
+      enterModalOptions(
+        'VBMS',
+        'Provide details such as file structure or file path',
+        'CAREGIVER -> BVA Intake',
+        buttonText
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in VBMS.' +
+        '\n\n**Detail:**\n\nCAREGIVER -> BVA Intake\n'
+      );
+    });
+
+    test('When Other is Chosen in Modal', () => {
+      renderCompleteTaskModal('vha_caregiver_support_send_to_board_intake_for_review', caregiverToIntakeData, taskType);
+
+      enterModalOptions(
+        'Other',
+        'Provide details such as file structure or file path',
+        'CAREGIVER -> BVA Intake',
+        buttonText,
+        'Other Source'
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in Other Source.' +
+        '\n\n**Detail:**\n\nCAREGIVER -> BVA Intake\n'
+      );
+    });
+  });
+
+  describe('emo_send_to_board_intake_for_review', () => {
+    const taskType = 'EducationDocumentSearchTask';
+    const buttonText = COPY.MODAL_SUBMIT_BUTTON;
+
+    test('modal title is Ready for Review', () => {
+      renderCompleteTaskModal('emo_send_to_board_intake_for_review', emoToBvaIntakeData, taskType);
+      expect(screen.getByText('Ready for Review')).toBeTruthy();
+    });
+
+    test('When Centralized Mail Portal is chosen in Modal', () => {
+      renderCompleteTaskModal('emo_send_to_board_intake_for_review', emoToBvaIntakeData, taskType);
+
+      enterModalOptions(
+        'Centralized Mail Portal',
+        'Provide details such as file structure or file path',
+        'EMO -> BVA Intake',
+        buttonText
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in Centralized Mail Portal.' +
+        '\n\n**Detail:**\n\nEMO -> BVA Intake\n'
+      );
+    });
+
+    test('When Other is Chosen in Modal', () => {
+      renderCompleteTaskModal('emo_send_to_board_intake_for_review', emoToBvaIntakeData, taskType);
+
+      enterModalOptions(
+        'Other',
+        'Provide details such as file structure or file path',
+        'EMO -> BVA Intake',
+        buttonText,
+        'Other Source'
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in Other Source.' +
+        '\n\n**Detail:**\n\nEMO -> BVA Intake\n'
+      );
+    });
+  });
+
+  describe('rpo_send_to_board_intake_for_review', () => {
+    const taskType = 'EducationAssessDocumentationTask';
+    const buttonText = COPY.MODAL_SUBMIT_BUTTON;
+
+    test('modal title is Ready for Review', () => {
+      renderCompleteTaskModal('emo_send_to_board_intake_for_review', rpoToBvaIntakeData, taskType);
+      expect(screen.getByText('Ready for Review')).toBeTruthy();
+    });
+
+    test('When Centralized Mail Portal is chosen in Modal', () => {
+      renderCompleteTaskModal('rpo_send_to_board_intake_for_review', rpoToBvaIntakeData, taskType);
+
+      enterModalOptions(
+        'Centralized Mail Portal',
+        'Provide details such as file structure or file path',
+        'RPO -> BVA Intake',
+        buttonText
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in Centralized Mail Portal.' +
+        '\n\n**Detail:**\n\nRPO -> BVA Intake\n'
+      );
+    });
+
+    test('When Other is Chosen in Modal', () => {
+      renderCompleteTaskModal('rpo_send_to_board_intake_for_review', rpoToBvaIntakeData, taskType);
+
+      enterModalOptions(
+        'Other',
+        'Provide details such as file structure or file path',
+        'RPO -> BVA Intake',
+        buttonText,
+        'Other Source'
+      );
+      expect(getReceivedInstructions()).toBe(
+        'Documents for this appeal are stored in Other Source.' +
+        '\n\n**Detail:**\n\nRPO -> BVA Intake\n'
       );
     });
   });
