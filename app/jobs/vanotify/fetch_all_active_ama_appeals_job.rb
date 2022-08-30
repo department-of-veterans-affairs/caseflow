994e57bcca87979a576fff47dfc8e7ed3ad2e09f
KiMauVA <Ki.Mau@va.gov>
2022-08-30 15:37:56 -0400

    APPEALS-7006 Fetch AMA Appeals Update

--- app/jobs/vanotify/fetch_all_active_ama_appeals_job.rb
+++ app/jobs/vanotify/fetch_all_active_ama_appeals_job.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 # Job to fetch all currently active AMA Appeals
-class FetchAllActiveAppeals < CaseflowJob
-#    queue_with_priority :low_priority
+class FetchAllActiveAMAAppeals < CaseflowJob
+  queue_with_priority :low_priority
   
-    def find_active_appeals
-      #Fetch Active AMA Appeals
-      active_ama_appeals = Array.new
-      ama_appeals = Appeal.open.where(
-        appeal_active: true
-      )
-      ama_info = Array.new
-      active_ama_appeals.each {|i| ama_info << "#{a.id}, #{a.claimaint.participant_id}, AMA Appeal"}
+    def perform
+      find_active_ama_appeals
+    end
+
+    private
+
+    def find_active_ama_appeals
+      active_ama_appeals = []
+      active_ama_appeals_tasks = Task.where(
+        appeal_type: "Appeal",
+        status: %w[assigned on hold in progress],
+        closed_at: nil
+      ).uniq(&:appeal_id)
+      active_ama_appeals_tasks.each do |task|
+        active_ama_appeals.push(task.ama_appeal)
+      end
+      active_ama_appeals
     end
 end
