82ccb0ec4c96600ecbeb6dfff5bf7c4485d2c5e6
Minhazur Rahaman <minhazur9@gmail.com>
2022-08-31 11:11:06 -0400

    APPEALS-8397 pointed it to low_priority queue and cleaned the code a little

--- app/jobs/vanotify/fetch_all_active_supplemental_appeals_job.rb
+++ app/jobs/vanotify/fetch_all_active_supplemental_appeals_job.rb
@@ -2,7 +2,7 @@
 
 # Job to fetch all currently active Supplemantal Claims
 class FetchAllActiveSupplementalClaimsJob < CaseflowJob
-  queue_as ApplicationController.dependencies_faked? ? :send_notifications : :"send_notifications.fifo"
+  queue_with_priority :low_priority
 
   def perform
     find_active_supplemental_claims
@@ -10,17 +10,15 @@ class FetchAllActiveSupplementalClaimsJob < CaseflowJob
 
   private
 
+  # Fetch Active Supplemantal Claims
   def find_active_supplemental_claims
-    # Fetch Active Supplemantal Claims
-    active_supplemantal_claims = []
-    active_supplemantal_claim_tasks = Task.where(
+    active_supplemental_claims = []
+    active_supplemental_claim_tasks = Task.where(
       appeal_type: "SupplementalClaim",
       status: %w[assigned on_hold in_progress],
       closed_at: nil
     ).uniq(&:appeal_id)
-    active_supplemantal_claim_tasks.each do |task|
-      active_supplemantal_claims.push(task.supplemental_claim)
-    end
-    active_supplemantal_claims
+    active_supplemental_claim_tasks.each { |task| active_supplemental_claims.push(task.supplemental_claim) }
+    active_supplemental_claims
   end
 end
