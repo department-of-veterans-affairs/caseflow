f0efd4a03a66450c177fb1d1bf597fee1b1e898b
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-09-01 12:05:29 -0400

    Revert last change. Using users_regional_office without selected_regional_office causes infinite loop.

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,9 @@ class User < CaseflowRecord # rubocop:disable Metrics/ClassLength
 
   # If RO is ambiguous from station_office, use the user-defined RO. Otherwise, use the unambigous RO.
   def regional_office
-    ro_is_ambiguous_from_station_office? ? users_regional_office : station_offices
+    upcase = ->(str) { str ? str.upcase : str }
+
+    ro_is_ambiguous_from_station_office? ? (selected_regional_office || upcase.call(@regional_office)) : station_offices
   end
 
   def users_regional_office
