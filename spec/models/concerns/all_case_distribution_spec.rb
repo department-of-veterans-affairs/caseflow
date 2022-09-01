b3e3867b3af59812f5c395cc18bc61643e7b5d91
Craig Reese <Reese_Craig@bah.com>
2022-09-01 09:22:04 -0400

    changed 'allow' to 'expect' in all case distribution tests

--- spec/models/concerns/all_case_distribution_spec.rb
+++ spec/models/concerns/all_case_distribution_spec.rb
@@ -7,6 +7,7 @@ describe AllCaseDistribution, :all_dbs do
 
     attr_accessor :judge
 
+    # the value here doesn't matter but if a test is checking batch_size then its values need to add to this
     def batch_size
       12
     end
@@ -34,32 +35,32 @@ describe AllCaseDistribution, :all_dbs do
 
     it "calls each method and returns the array of objects received from each method" do
       # method from distributing legacy appeals when :priority_acd enabled
-      allow_any_instance_of(LegacyDocket).to receive(:distribute_nonpriority_appeals)
+      expect_any_instance_of(LegacyDocket).to receive(:distribute_nonpriority_appeals)
         .and_return([])
 
       # returning {} from num_oldest_priority_appeals_by_docket will bypass
       # distribute_limited_priority_appeals_from_all_dockets not iterate over anything
-      allow(@new_acd).to receive(:num_oldest_priority_appeals_by_docket)
+      expect(@new_acd).to receive(:num_oldest_priority_appeals_by_docket)
         .and_return({})
 
       # distribute all nonpriority appeals from all dockets
-      allow(@new_acd).to receive(:num_oldest_nonpriority_appeals_for_judge_by_docket)
+      expect(@new_acd).to receive(:num_oldest_nonpriority_appeals_for_judge_by_docket)
         .with(@new_acd.judge, @new_acd.batch_size)
         .and_return(nonpriority_count_hash)
 
-      allow_any_instance_of(LegacyDocket).to receive(:distribute_appeals)
+      expect_any_instance_of(LegacyDocket).to receive(:distribute_appeals)
         .with(@new_acd, priority: false, style: "request", limit: nonpriority_count_hash[:legacy])
         .and_return(add_object_to_return_array(nonpriority_count_hash[:legacy]))
 
-      allow_any_instance_of(DirectReviewDocket).to receive(:distribute_appeals)
+      expect_any_instance_of(DirectReviewDocket).to receive(:distribute_appeals)
         .with(@new_acd, priority: false, style: "request", limit: nonpriority_count_hash[:direct_review])
         .and_return(add_object_to_return_array(nonpriority_count_hash[:direct_review]))
 
-      allow_any_instance_of(EvidenceSubmissionDocket).to receive(:distribute_appeals)
+      expect_any_instance_of(EvidenceSubmissionDocket).to receive(:distribute_appeals)
         .with(@new_acd, priority: false, style: "request", limit: nonpriority_count_hash[:evidence_submission])
         .and_return(add_object_to_return_array(nonpriority_count_hash[:evidence_submission]))
 
-      allow_any_instance_of(HearingRequestDocket).to receive(:distribute_appeals)
+      expect_any_instance_of(HearingRequestDocket).to receive(:distribute_appeals)
         .with(@new_acd, priority: false, style: "request", limit: nonpriority_count_hash[:hearing])
         .and_return(add_object_to_return_array(nonpriority_count_hash[:hearing]))
 
