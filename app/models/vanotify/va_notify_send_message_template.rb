d5e9998f89a6c0d48b263c7238baa18797ee0900
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-09-01 15:27:11 -0400

    APPEALS-8615 Updated Naming Conventions

--- app/models/vanotify/va_notify_send_message_template.rb
+++ app/models/vanotify/va_notify_send_message_template.rb
@@ -2,14 +2,15 @@
 
 # Class used by create_payload_method in AppellantNotification module to create message
 class VANotifySendMessageTemplate
-  # Constructs message that is sent to VA Notify
+  # Constructs Object with attributes that are sent to VA Notify
   # Appeal ID is UUID for AMA Appeals and Vacols ID for Legacy Appeals
+  # message_attributes is a hash containing strings of participant_id, appeal_id, appeal_type, & status
   attr_reader :participant_id, :template_name, :appeal_id, :appeal_type, :status
-  def initialize(info, template_name)
-    @participant_id = info[:participant_id]
+  def initialize(message_attributes, template_name)
+    @participant_id = message_attributes[:participant_id]
     @template_name = template_name
-    @appeal_id = info[:appeal_id]
-    @appeal_type = info[:appeal_type]
-    @status = info[:status]
+    @appeal_id = message_attributes[:appeal_id]
+    @appeal_type = message_attributes[:appeal_type]
+    @status = message_attributes[:status]
   end
 end
