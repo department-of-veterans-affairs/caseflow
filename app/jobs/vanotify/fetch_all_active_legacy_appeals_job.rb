0685d045e0ebcca90e88fa6dfb36e549c4d36f80
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-09-01 15:07:45 -0400

    APPEALS-8394 Updated Logic

--- app/jobs/vanotify/fetch_all_active_legacy_appeals_job.rb
+++ app/jobs/vanotify/fetch_all_active_legacy_appeals_job.rb
@@ -3,23 +3,23 @@
 # Job to fetch all currently active Legacy Appeals
 class FetchAllActiveLegacyAppeals < CaseflowJob
   queue_with_priority :low_priority
-    def perform
-      find_active_legacy_appeals
-    end
-  
-    private
-  
-    def find_active_legacy_appeals
-      active_legacy_appeals = []
-      active_legacy_appeals_root_tasks = Task.where(
-        type: "RootTask",
-        appeal_type: "LegacyAppeal",
-        status: %w[assigned on hold in progress],
-        closed_at: nil
-      )
-      active_legacy_appeals_root_tasks.each do |task|
-        active_legacy_appeals.push(task.appeal)
-      end
-      active_legacy_appeals
-      end
+  def perform
+    find_active_legacy_appeals
   end
+
+  private
+
+  def find_active_legacy_appeals
+    active_legacy_appeals = Array.new
+    active_legacy_appeals_root_tasks = Task.where(
+      type: "RootTask",
+      appeal_type: "LegacyAppeal",
+      status: %w[assigned on hold in progress],
+      closed_at: nil
+    )
+    active_legacy_appeals_root_tasks.each do |task|
+      active_legacy_appeals.push(task.appeal)
+    end
+    active_legacy_appeals
+  end
+end
