2f49912c73c5284fa25d4443dec63012481a9d36
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-09-02 08:35:50 -0400

    Skip setting updated_by on Hearings and LegacyHearings for VSO conversions. This causes issues with the scheduled_for time. We track VSO conversions in another fashion.

--- app/models/concerns/updated_by_user_concern.rb
+++ app/models/concerns/updated_by_user_concern.rb
@@ -12,8 +12,14 @@ module UpdatedByUserConcern
   private
 
   def assign_updated_by_user
+    return if vso_hearing_update
+
     return if RequestStore.store[:current_user] == User.system_user && updated_by.present?
 
     self.updated_by = RequestStore.store[:current_user] if RequestStore.store[:current_user].present?
+  end
+
+  def vso_hearing_update
+    (is_a?(Hearing) || is_a?(LegacyHearing)) && RequestStore.store[:current_user].vso_employee?
   end
 end
