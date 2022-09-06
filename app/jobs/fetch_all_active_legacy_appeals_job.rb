53dcde86ead9a625536e9ad02994ec234737f92f
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-09-06 13:58:47 -0400

    APPEALS-8394 Added to RSPEC and Fixed Linting

--- app/jobs/fetch_all_active_legacy_appeals_job.rb
+++ app/jobs/fetch_all_active_legacy_appeals_job.rb
@@ -4,12 +4,13 @@
 class FetchAllActiveLegacyAppealsJob < CaseflowJob
   queue_with_priority :low_priority
   def perform
+    find_active_legacy_appeals
   end
 
   private
 
   def find_active_legacy_appeals
-    active_legacy_appeals = Array.new
+    active_legacy_appeals = []
     active_legacy_appeals_root_tasks = Task.where(
       type: "RootTask",
       appeal_type: "LegacyAppeal",
--- spec/jobs/fetch_all_active_legacy_appeals_job_spec.rb
+++ spec/jobs/fetch_all_active_legacy_appeals_job_spec.rb
@@ -2,6 +2,12 @@
 
 describe FetchAllActiveLegacyAppealsJob do
   subject { FetchAllActiveLegacyAppealsJob.new }
+  describe ".perform" do
+    it "returns an array of all active legacy appeals" do
+      expect(subject).to receive(:find_active_legacy_appeals)
+      subject.perform
+    end
+  end
   describe "find_active_legacy_appeals" do
     let!(:legacy_task) do
       Array.new(1) { create(:task, type: "RootTask") }
@@ -9,7 +15,6 @@ describe FetchAllActiveLegacyAppealsJob do
     let!(:ama_task) do
       Array.new(1) { create(:ama_task, type: "RootTask") }
     end
-    let!(:empty_array) { [] }
     context "when database has a RootTask for a Legacy Appeal with an assigned status" do
       it "should return an array with the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).to eq(legacy_task.map(&:appeal))
@@ -17,7 +22,7 @@ describe FetchAllActiveLegacyAppealsJob do
     end
     context "when database has a RootTask for a Legacy Appeal with an on_hold status" do
       before do
-        legacy_task.each{|t| t.update!(status: Constants.TASK_STATUSES.on_hold)}
+        legacy_task.each { |t| t.update!(status: Constants.TASK_STATUSES.on_hold) }
       end
       it "should return an array with the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).to eq(legacy_task.map(&:appeal))
@@ -25,7 +30,7 @@ describe FetchAllActiveLegacyAppealsJob do
     end
     context "when database has a RootTask for a Legacy Appeal with a cancelled status" do
       before do
-        legacy_task.each{|t| t.update!(status: Constants.TASK_STATUSES.cancelled)}
+        legacy_task.each { |t| t.update!(status: Constants.TASK_STATUSES.cancelled) }
       end
       it "should not return the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).not_to include(legacy_task.map(&:appeal))
@@ -33,7 +38,7 @@ describe FetchAllActiveLegacyAppealsJob do
     end
     context "when database has a RootTask for a Legacy Appeal with a completed status" do
       before do
-        legacy_task.each{|t| t.update!(status: Constants.TASK_STATUSES.completed)}
+        legacy_task.each { |t| t.update!(status: Constants.TASK_STATUSES.completed) }
       end
       it "should not return the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).not_to include(legacy_task.map(&:appeal))
@@ -41,7 +46,7 @@ describe FetchAllActiveLegacyAppealsJob do
     end
     context "when database has a Non-RootTask for a Legacy Appeal" do
       before do
-        legacy_task.each{|t| t.update!(type: "NonRootTask")}
+        legacy_task.each { |t| t.update!(type: "NonRootTask") }
       end
       it "should not return the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).not_to include(legacy_task.map(&:appeal))
@@ -49,7 +54,7 @@ describe FetchAllActiveLegacyAppealsJob do
     end
     context "when database has a task for a Legacy Appeal with a non nil closed_at attribute" do
       before do
-        legacy_task.each{|t| t.update!(closed_at: Time.zone.now)}
+        legacy_task.each { |t| t.update!(closed_at: Time.zone.now) }
       end
       it "should not return the appeal tied to that task" do
         expect(subject.send(:find_active_legacy_appeals)).not_to include(legacy_task.map(&:appeal))
