7f24e135fd9110fa41463fbc6bd5ead7f3998be8
Noelle Adkin <adkin_noelle@bah.com>
2022-09-08 10:03:29 -0400

    missing super.

--- app/models/tasks/distribution_task.rb
+++ app/models/tasks/distribution_task.rb
@@ -65,4 +65,6 @@ def when_child_task_completed(child_task)
   if appeal.tasks.open.where(type: HearingTask.name).empty? && appeal.is_a?(Appeal)
     IhpTasksFactory.new(parent).create_ihp_tasks!
   end
+
+  super
 end
