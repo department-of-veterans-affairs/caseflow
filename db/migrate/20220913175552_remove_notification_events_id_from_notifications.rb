292949d53807bb5a512c90eb0b396bf0231c4708
Marc Steele <Marc.Steele@va.gov>
2022-09-13 14:11:03 -0400

    APPEALS-9053 remove notification_events_id from notification table

---
+++ db/migrate/20220913175552_remove_notification_events_id_from_notifications.rb
@@ -0,0 +1,5 @@
+class RemoveNotificationEventsIdFromNotifications < Caseflow::Migration
+  def change
+    safety_assured { remove_column :notifications, :notification_events_id, :bigint }
+  end
+end
