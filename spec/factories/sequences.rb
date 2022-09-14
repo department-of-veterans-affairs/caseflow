76a7eb61c0c4d25de50e33573dabf4868b24bc02
Craig Reese <Reese_Craig@bah.com>
2022-09-14 16:57:47 -0400

    changed sequence initial values to conditionals to allow circleci build

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  # initialize these values at 1 in case the DB relations are not created yet which could error
+  # during the build process
+  correspondent_key_initial_value = 1
+  case_key_initial_value = 1
+  veteran_initial_value = 1
+  contention_reference_initial_value = 1
+
   # these get the highest existing value within the filter and uses that + 1 as the
   # initial value; filter values gives sufficient margin to allow seeds to be run hundreds
   # of times before a data collision occurs
-  correspondent_key_initial_value =
-    VACOLS::Correspondent.all.map(&:stafkey).map(&:to_i).filter { |key| key < 49_999 }.max.to_i + 1
+  if VACOLS::Correspondent.exists?
+    correspondent_key_initial_value =
+      VACOLS::Correspondent.all.map(&:stafkey).map(&:to_i).filter { |key| key < 49_999 }.max.to_i + 1
+  end
 
-  case_key_initial_value =
-    VACOLS::Case.all.map(&:bfkey).map(&:to_i).filter { |key| key < 99_999 }.max.to_i + 1
+  if VACOLS::Case.exists?
+    case_key_initial_value =
+      VACOLS::Case.all.map(&:bfkey).map(&:to_i).filter { |key| key < 99_999 }.max.to_i + 1
+  end
 
-  veteran_initial_value = Veteran.all.map(&:file_number).max.to_i + 1
+  if Veteran.exists?
+    veteran_initial_value = Veteran.all.map(&:file_number).max.to_i + 1
+  end
 
-  contention_reference_initial_value =
-    RequestIssue.all.map(&:contention_reference_id).map(&:to_i).filter { |id| id < 100_000 }.max.to_i + 1
+  if RequestIssue.exists?
+    contention_reference_initial_value =
+      RequestIssue.all.map(&:contention_reference_id).map(&:to_i).filter { |id| id < 100_000 }.max.to_i + 1
+  end
 
   # BRIEFF.BFCORLID in VACOLS, file_number/veteran_file_number in Caseflow
   sequence :veteran_file_number, veteran_initial_value do |n|
