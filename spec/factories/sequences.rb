3624139f35d42395e7713943ae6e638e99c9e1cb
Craig Reese <Reese_Craig@bah.com>
2022-09-15 10:56:09 -0400

    modified sequences file to catch PG::UndefinedTable error

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -1,33 +1,43 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  # initialize these values at 1 in case the DB relations are not created yet which could error
-  # during the build process
-  correspondent_key_initial_value = 1
-  case_key_initial_value = 1
-  veteran_initial_value = 1
-  contention_reference_initial_value = 1
-
-  # these get the highest existing value within the filter and uses that + 1 as the
+  # get the highest existing value within the filter for each table and use that + 1 as the
   # initial value; filter values gives sufficient margin to allow seeds to be run hundreds
   # of times before a data collision occurs
-  if VACOLS::Correspondent.exists?
+  # if a table doesn't yet exist the rescued error will occur, in that case set the value to 1
+  begin
     correspondent_key_initial_value =
       VACOLS::Correspondent.all.map(&:stafkey).map(&:to_i).filter { |key| key < 49_999 }.max.to_i + 1
+  rescue ActiveRecord::StatementInvalid => error
+    raise if !error.message.include?("PG::UndefinedTable")
+
+    correspondent_key_initial_value = 1
   end
 
-  if VACOLS::Case.exists?
+  begin
     case_key_initial_value =
       VACOLS::Case.all.map(&:bfkey).map(&:to_i).filter { |key| key < 99_999 }.max.to_i + 1
+  rescue ActiveRecord::StatementInvalid => error
+    raise if !error.message.include?("PG::UndefinedTable")
+
+    case_key_initial_value = 1
   end
 
-  if Veteran.exists?
-    veteran_initial_value = Veteran.all.map(&:file_number).max.to_i + 1
-  end
-
-  if RequestIssue.exists?
+  begin
     contention_reference_initial_value =
       RequestIssue.all.map(&:contention_reference_id).map(&:to_i).filter { |id| id < 100_000 }.max.to_i + 1
+  rescue ActiveRecord::StatementInvalid => error
+    raise if !error.message.include?("PG::UndefinedTable")
+
+    contention_reference_initial_value = 1
+  end
+
+  begin
+    veteran_initial_value = Veteran.all.map(&:file_number).max.to_i + 1
+  rescue ActiveRecord::StatementInvalid => error
+    raise if !error.message.include?("PG::UndefinedTable")
+
+    veteran_initial_value = 1
   end
 
   # BRIEFF.BFCORLID in VACOLS, file_number/veteran_file_number in Caseflow
