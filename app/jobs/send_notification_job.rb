e8634a18dfa6c48fd084d68ff0fca2fb7292b7c0
Marc Steele <Marc.Steele@va.gov>
2022-09-19 16:17:50 -0400

    APPEALS-9053 Removed comment

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -98,7 +98,6 @@ class SendNotificationJob < CaseflowJob
       response = VANotifyService.send_email_notifications(message.participant_id, notification_audit_record.id.to_s, email_template_id, status = "")
       if !response.nil? && response != ""
         to_update = { notification_content: response.body["content"]["body"], va_notify_notification_id: response.body["id"] }
-        # should new column be updated from response?
         update_notification_audit_record(notification_audit_record, to_update)
       end
     end
