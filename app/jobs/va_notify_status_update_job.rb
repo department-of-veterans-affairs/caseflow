00a0b13ecc8b649851394bb081371ed6ba4e8313
Jeremy Shields <jeremy.shields@va.gov>
2022-09-20 12:55:34 -0400

    APPEALS-9300 Updated logic to update the staus for each notification row

--- app/jobs/va_notify_status_update_job.rb
+++ app/jobs/va_notify_status_update_job.rb
@@ -4,29 +4,40 @@ class VANotifyStatusUpdateJob < ApplicationJob
   queue_with_priority :low_priority
   application_attr :hearing_schedule
 
-  QUERY_LIMIT = ENV["VA_NOTIFY_STATUS_UPDATE_BATCH_LIMIT"]
+  QUERY_LIMIT = "650"#{ENV["VA_NOTIFY_STATUS_UPDATE_BATCH_LIMIT"]
 
   def perform
     notifications_not_processed.each do |notification|
-      va_notify_notification_id = notification.va_notify_notification_id
-      if !va_notify_notification_id.nil? || va_notify_notification_id != ""
-        
-      else
-        log_error("Notification record id:" + notification.id + "Does not have a VA Notification Id associated with it.")
+      sms_external_id = notification.sms_notification_external_id
+      email_external_id = notification.email_notification_external_id
+      case notification.notification_type
+      when "Email"
+        update_attributes = get_current_status(email_external_id, "Email")
+        update_notification_audit_record(notification, update_attributes)
+      when "SMS"
+        update_attributes = get_current_status(sms_external_id)
+        update_notification_audit_record(notification, update_attributes, "SMS")
+      when "Email and SMS"
+        update_attributes = get_current_status(email_external_id, "Email")
+        update_notification_audit_record(notification, update_attributes)
+        update_attributes = get_current_status(sms_external_id, "SMS")
+        update_notification_audit_record(notification, update_attributes)
       end
+      notification.save!
     end
   end
 
   private
 
   def notifications_not_processed
-    @notifications_not_processed ||= find_notifications_not_processed.first(QUERY_LIMIT)
+    find_notifications_not_processed.first(QUERY_LIMIT.to_i)
   end
 
   def find_notifications_not_processed
-    Notification.where("(email_notification_status = 'Success' AND sms_notification_status = 'Success') \
-      OR (email_notification_status = 'Success' AND sms_notification_status = '') \
-      OR (email_notification_status = '' AND sms_notification_status = 'Success')")
+    Notification.where("(notification_type = 'Email' AND email_notification_status = 'Success') \
+      OR (notification_type = 'SMS' AND sms_notification_status = 'Success') \
+      OR (notification_type = 'Email and SMS' AND \
+         (sms_notification_status = 'Success' OR email_notification_status = 'Success'))")
   end
 
   def log_error(message)
@@ -37,7 +48,24 @@ class VANotifyStatusUpdateJob < ApplicationJob
     Rails.logger.info(message)
   end
 
-  def get_current_status(notification_id)
+  def get_current_status(notification_id, type)
     response = VANotifyService.get_status(notification_id)
+    if response.code == 200
+      response_data = response.raw_body
+      if type == "Email"
+        { "email_notification_status" => response_data.status, "recipient_email" => response_data.email_address }
+      elsif type == "SMS"
+        { "sms_notification_status" => response_data.status, "recipient_phone_number" => response_data.phone_number }
+      end
+    else
+      log_error("VA Notify APi returned error for notification " + notification_id)
+      nil
+    end
+  end
+
+  def update_notification_audit_record(notification_audit_record, to_update)
+    to_update&.each do |key, value|
+      notification_audit_record[key] = value
+    end
   end
 end
