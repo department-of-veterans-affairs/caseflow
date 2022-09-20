1fff89dd7d33b7b9b37d38ee5d2083045c4edc67
Jeremy Shields <jeremy.shields@va.gov>
2022-09-20 15:20:58 -0400

    APPEALS-9300 Added comments to columns

--- db/migrate/20220920120441_add_sms_notification_external_id_to_notifications.rb
+++ db/migrate/20220920120441_add_sms_notification_external_id_to_notifications.rb
@@ -1,5 +1,5 @@
-class AddSmsNotificationExternalIdToNotifications < ActiveRecord::Migration[5.2]
+class AddSmsNotificationExternalIdToNotifications < Caseflow::Migration
   def change
-    add_column :notifications, :sms_notification_external_id, :string
+    add_column :notifications, :sms_notification_external_id, :string, comment: "VA Notify Notification Id for the sms notification send through their API "
   end
 end
--- db/migrate/20220920122149_add_email_notification_external_id_to_notifications.rb
+++ db/migrate/20220920122149_add_email_notification_external_id_to_notifications.rb
@@ -1,5 +1,5 @@
-class AddEmailNotificationExternalIdToNotifications < ActiveRecord::Migration[5.2]
+class AddEmailNotificationExternalIdToNotifications < Caseflow::Migration
   def change
-    add_column :notifications, :email_notification_external_id, :string
+    add_column :notifications, :email_notification_external_id, :string, comment: "VA Notify Notification Id for the email notification send through their API "
   end
 end
