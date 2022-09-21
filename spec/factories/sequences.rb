0fc4799ab9a5d12b8f18dc77fe388fbc3cf45ca1
Craig Reese <Reese_Craig@bah.com>
2022-09-21 15:00:57 -0400

    typo

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -2,16 +2,16 @@
 
 FactoryBot.define do
   # BRIEFF.BFCORLID in VACOLS, file_number/veteran_file_number in Caseflow
-  sequence :veteran_file_number, veteran_initial_value do |n|
+  sequence :veteran_file_number do |n|
     format("%<n>09d", n: n)
   end
 
   # CORRES.STAFKEY, BRIEFF.BFCORKEY, FOLDER.TICKNUM
-  sequence :vacols_correspondent_key, correspondent_key_initial_value
+  sequence :vacols_correspondent_key
 
   # BRIEFF.BFKEY
-  sequence :vacols_case_key, case_key_initial_value
+  sequence :vacols_case_key
 
   # AMA factories: bgs_attorney, bgs_power_of_attorney, claimant, decision_issue, person, relationship, veteran
-  sequence :participant_id, participant_id_initial_value
+  sequence :participant_id
 end
