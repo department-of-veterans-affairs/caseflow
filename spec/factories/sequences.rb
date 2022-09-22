2814a689af8fcdddd9219dc85456c1c827dc6781
Craig Reese <109101548+craigrva@users.noreply.github.com>
2022-09-22 12:32:31 -0400

    fixed intake issues with sequencing changes (#17490)
    * fixed intake issues with sequencing changes
    
    * Update sequences.rb
    
    fixed leading zeroes

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -1,28 +1,32 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  # returns digits 3-9 of epoch time to be used for unique id sequencing
+  # returns digits 4-8 of epoch time to be used for unique id sequencing
   def time
-    Time.now.to_i.to_s.split("")[2..8].join
+    Time.now.to_i.to_s.split("")[3..7].join
   end
 
   # BRIEFF.BFCORLID in VACOLS, file_number/veteran_file_number in Caseflow
   sequence :veteran_file_number do |n|
-    time.concat(format("%<n>05d", n: n))
+    FactoryBot.rewind_sequences if n == 9999
+    time.concat(format("%<n>04d", n: n))
   end
 
   # CORRES.STAFKEY, BRIEFF.BFCORKEY, FOLDER.TICKNUM
   sequence :vacols_correspondent_key do |n|
-    time.concat(format("%<n>05d", n: n))
+    FactoryBot.rewind_sequences if n == 9999
+    time.concat(format("%<n>04d", n: n))
   end
 
   # BRIEFF.BFKEY
   sequence :vacols_case_key do |n|
-    time.concat(format("%<n>05d", n: n))
+    FactoryBot.rewind_sequences if n == 9999
+    time.concat(format("%<n>04d", n: n))
   end
 
   # AMA factories: bgs_attorney, bgs_power_of_attorney, claimant, decision_issue, person, relationship, veteran
   sequence :participant_id do |n|
-    time.concat(format("%<n>05d", n: n))
+    FactoryBot.rewind_sequences if n == 9999
+    time.concat(format("%<n>04d", n: n))
   end
 end
