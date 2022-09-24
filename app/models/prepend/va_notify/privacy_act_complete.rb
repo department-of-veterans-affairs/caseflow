055e583438daa2d6cda02beb0f8846f9a7f05a49
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-09-24 03:10:13 -0400

    APPEALS-9 hotfix for Ihp Pending and Privacy Reqeust Complete

--- app/models/prepend/va_notify/privacy_act_complete.rb
+++ app/models/prepend/va_notify/privacy_act_complete.rb
@@ -10,7 +10,9 @@ module PrivacyActComplete
   def update_status_if_children_tasks_are_closed(child_task)
     # original method defined in app/models/task.rb
     super_return_value = super
-    if type.to_s.include?("Foia") || type.to_s.include?("PrivacyAct")
+    if type.to_s.include?("PreRoutingFoiaColocatedTask") ||
+       type.to_s.include?("FoiaColocatedTask") ||
+       type.to_s.include?("PrivacyAct")
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
     super_return_value
