72d9316a174a5d96de46413f2f937042965bd627
Marc Steele <Marc.Steele@va.gov>
2022-09-27 09:16:17 -0400

    APPEALS-9336 Changed method to hook into to process! in decision_document.rb

--- app/models/prepend/va_notify/appeal_decision_mailed.rb
+++ app/models/prepend/va_notify/appeal_decision_mailed.rb
@@ -7,21 +7,32 @@ module AppealDecisionMailed
   @@template_name = "Appeal decision mailed"
   # rubocop:enable all
 
-  # Legacy
-  def complete_root_task!
-    # original method defined in app/workflows/legacy_appeal_dispatch.rb
-    super_return_value = super
-    if appeal.contested_claim
-      AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Contested claims)")
-    else
-      AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Non-contested claims)")
-    end
-    super_return_value
-  end
+  # # Legacy
+  # def complete_root_task!
+  #   # original method defined in app/workflows/legacy_appeal_dispatch.rb
+  #   super_return_value = super
+  #   if appeal.contested_claim
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Contested claims)")
+  #   else
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Non-contested claims)")
+  #   end
+  #   super_return_value
+  # end
 
-  # AMA
-  def complete_dispatch_root_task!
-    # original method defined in app/workflows/ama_appeal_dispatch.rb
+  # # AMA
+  # def complete_dispatch_root_task!
+  #   # original method defined in app/workflows/ama_appeal_dispatch.rb
+  #   super_return_value = super
+  #   if appeal.contested_claim?
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Contested claims)")
+  #   else
+  #     AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Non-contested claims)")
+  #   end
+  #   super_return_value
+  # end
+
+  def process!
+    # original method defined in app/models/decision_document.rb
     super_return_value = super
     if appeal.contested_claim?
       AppellantNotification.notify_appellant(@appeal, "#{@@template_name} (Contested claims)")
@@ -30,4 +41,5 @@ module AppealDecisionMailed
     end
     super_return_value
   end
+
 end
