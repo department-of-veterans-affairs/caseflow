102ee43d413cb25c3829e91b8f50c03650fc8294
(Jeffrey) Aaron Willis <98484567+Aaron-Willis@users.noreply.github.com>
2022-09-27 13:51:18 -0400

    hotfix/APPEALS-9419 Resolved VSO IHP pending issues and reactivated corresponding Prepend Statements. (#17512)
    Resolves APPEALS-9419
    
    Description
    Resolved VSO IHP pending issues and reactivated corresponding Prepend Statements.
    
    Acceptance Criteria
     Resolve issue where VSO IHP pending notification was incorrectly sending notifications out when and EvidenceWindowSubmissionTask was being updated to a closed status (cancelled OR completed) due to appellant having Non-writing IHP Representation (such as a field VSO).
    Testing Plan
    Go to https://vajira.max.gov/browse/APPEALS-9425
    Testing Execution
    Go to https://vajira.max.gov/browse/APPEALS-9438

--- app/models/prepend/va_notify/ihp_task_pending.rb
+++ app/models/prepend/va_notify/ihp_task_pending.rb
@@ -10,14 +10,19 @@ module IhpTaskPending
   # original method defined in app/workflows/ihp_tasks_factory.rb
   def create_ihp_tasks!
     super_return_value = super
-    AppellantNotification.notify_appellant(@parent.appeal, @@template_name)
+    appeal_tasks_created = super_return_value.map { |task| task.class.to_s }
+    if appeal_tasks_created.any?("InformalHearingPresentationTask")
+      AppellantNotification.notify_appellant(@parent.appeal, @@template_name)
+    end
     super_return_value
   end
 
   # original method defined in app/models/tasks/colocated_task.rb
   def create_from_params(params, user)
     super_return_value = super
-    if name == "IhpColocatedTask"
+    task_array = []
+    super_return_value&.appeal.tasks.each { |task| task_array.push(task.class.to_s) }
+    if super_return_value.class.to_s == "IhpColocatedTask" && task_array.include?("IhpColocatedTask")
       AppellantNotification.notify_appellant(super_return_value.appeal, @@template_name)
     end
     super_return_value
