816a75ff25f78d22e877b2a5338987c595e1edee
= <tyler.broyles@va.gov>
2022-09-28 15:52:31 -0400

    Turned validation back on for vha for start hold modal.

--- client/app/queue/components/StartHoldModal.jsx
+++ client/app/queue/components/StartHoldModal.jsx
@@ -119,7 +119,7 @@ class StartHoldModal extends React.Component {
       title={TASK_ACTIONS.PLACE_TIMED_HOLD.label}
       button={COPY.MODAL_PUT_TASK_ON_HOLD_BUTTON}
       pathAfterSubmit={`/queue/appeals/${this.props.appealId}`}
-      // submitDisabled={this.isVHAHold() && !this.validateForm()}
+      submitDisabled={this.isVHAHold() && !this.validateForm()}
       validateForm={this.validateForm}
       submitButtonClassNames={['usa-button']}
       submit={this.submit}
