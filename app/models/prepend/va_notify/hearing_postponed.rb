15ee497181ee3028df2709d8aeab220cf00ea4cb
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-09-28 21:02:50 -0400

    Check for an incoming postponed disposition update to determine if to send notification

--- app/models/prepend/va_notify/hearing_postponed.rb
+++ app/models/prepend/va_notify/hearing_postponed.rb
@@ -3,15 +3,15 @@
 # Module to notify appellant if Hearing is Postponed
 module HearingPostponed
   extend AppellantNotification
-  # rubocop:disable all
+  # rubocop:disable Style/ClassVars
   @@template_name = "Postponement of hearing"
-  # rubocop:enable all
+  # rubocop:enable Style/ClassVars
 
   # Legacy Appeals
   # original method defined in app/models/legacy_hearing.rb
   def update_caseflow_and_vacols(hearing_hash)
     super_return_value = super
-    if postponed?
+    if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed
       appeal = LegacyAppeal.find(appeal_id)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
@@ -21,7 +21,7 @@ module HearingPostponed
   # original method defined in app/models/tasks/assign_hearing_disposition_task.rb
   def update_hearing(hearing_hash)
     super_return_value = super
-    if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed && appeal.class.to_s == "Appeal"
+    if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed && appeal.is_a?(Appeal)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
     super_return_value
