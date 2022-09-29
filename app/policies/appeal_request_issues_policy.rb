4a2fe4883279a84b8416f7c79c458f6d9d8718ba
HunJerBAH <Jeremy.Hunton@va.gov>
2022-09-29 08:03:31 -0400

    updated linting issue

--- app/policies/appeal_request_issues_policy.rb
+++ app/policies/appeal_request_issues_policy.rb
@@ -27,7 +27,7 @@ class AppealRequestIssuesPolicy
 
   def editable_by_ssc_team_member?
     SupervisorySeniorCouncil.singleton.users.include?(user) &&
-    FeatureToggle.enabled?(:split_appeal_workflow)
+      FeatureToggle.enabled?(:split_appeal_workflow)
   end
 
   def current_user_can_edit_issues?
