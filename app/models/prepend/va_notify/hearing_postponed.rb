73d83c66a4a1636ec7db87847ce806cff40abea4
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-09-29 15:00:35 -0400

    APPEALS-9585 Added Updated Logic and Comments

--- app/models/prepend/va_notify/hearing_postponed.rb
+++ app/models/prepend/va_notify/hearing_postponed.rb
@@ -7,17 +7,18 @@ module HearingPostponed
   @@template_name = "Postponement of hearing"
   # rubocop:enable all
 
-  # Legacy Appeals
+  # Legacy Hearing Postponed from the Daily Docket
   # original method defined in app/models/legacy_hearing.rb
   def update_caseflow_and_vacols(hearing_hash)
     super_return_value = super
-    if postponed?
+    if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed
       appeal = LegacyAppeal.find(appeal_id)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
     super_return_value
   end
 
+  # Legacy OR AMA Hearing Postponed from Queue
   # original method defined in app/models/tasks/assign_hearing_disposition_task.rb
   def update_hearing(hearing_hash)
     super_return_value = super
--- app/models/prepend/va_notify/hearing_withdrawn.rb
+++ app/models/prepend/va_notify/hearing_withdrawn.rb
@@ -7,6 +7,7 @@ module HearingWithdrawn
   @@template_name = "Withdrawal of hearing"
   # rubocop:enable all
 
+  # Legacy OR AMA Hearing Withdrawn from Queue
   # original method defined in app/models/tasks/assign_hearing_disposition_task.rb
   def update_hearing(hearing_hash)
     super_return_value = super
@@ -16,10 +17,11 @@ module HearingWithdrawn
     super_return_value
   end
 
+  # Legacy Hearing Withdrawn from the Daily Docket
   # original method defined in app/models/legacy_hearing.rb
   def update_caseflow_and_vacols(hearing_hash)
     super_return_value = super
-    if cancelled?
+    if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.cancelled
       appeal = LegacyAppeal.find(appeal_id)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
