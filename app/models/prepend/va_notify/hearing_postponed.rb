d7729efbbb71a944b5b62a7b32d17175b11b02c5
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-09-28 21:03:19 -0400

    Attempt to remove super return values since nothing seems to use them.

--- app/models/prepend/va_notify/hearing_postponed.rb
+++ app/models/prepend/va_notify/hearing_postponed.rb
@@ -10,20 +10,20 @@ module HearingPostponed
   # Legacy Appeals
   # original method defined in app/models/legacy_hearing.rb
   def update_caseflow_and_vacols(hearing_hash)
-    super_return_value = super
+    super
+
     if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed
       appeal = LegacyAppeal.find(appeal_id)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
-    super_return_value
   end
 
   # original method defined in app/models/tasks/assign_hearing_disposition_task.rb
   def update_hearing(hearing_hash)
-    super_return_value = super
+    super
+
     if hearing_hash[:disposition] == Constants.HEARING_DISPOSITION_TYPES.postponed && appeal.is_a?(Appeal)
       AppellantNotification.notify_appellant(appeal, @@template_name)
     end
-    super_return_value
   end
 end
