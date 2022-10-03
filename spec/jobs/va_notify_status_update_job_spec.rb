b0bcd4c94f991ff302007b10862741589ebd0e4a
Marc Steele <Marc.Steele@va.gov>
2022-10-03 11:22:49 -0400

    APPEALS-9336 Update rspec for other jobs

--- spec/jobs/va_notify_status_update_job_spec.rb
+++ spec/jobs/va_notify_status_update_job_spec.rb
@@ -12,7 +12,39 @@ describe VANotifyStatusUpdateJob, type: :job do
   let(:notifications_email_and_sms) do
     FactoryBot.create_list :notification_email_and_sms, 10
   end
+  let(:email_only) do
+    create(:notification,
+           appeals_id: "5d70058f-8641-4155-bae8-5af4b61b1576",
+           appeals_type: "Appeal",
+           event_type: "Hearing scheduled",
+           event_date: Time.zone.today,
+           notification_type: "Email",
+           email_notification_status: "Success")
+  end
+  let(:sms_only) do
+    create(:notification,
+           appeals_id: "5d70058f-8641-4155-bae8-5af4b61b1576",
+           appeals_type: "Appeal",
+           event_type: "Hearing scheduled",
+           event_date: Time.zone.today,
+           notification_type: "SMS",
+           sms_notification_status: "Success")
+  end
+  let(:email_and_sms) do
+    create(:notification,
+           appeals_id: "5d70058f-8641-4155-bae8-5af4b61b1576",
+           appeals_type: "Appeal",
+           event_type: "Hearing scheduled",
+           event_date: Time.zone.today,
+           notification_type: "Email",
+           email_notification_status: "Success",
+           sms_notification_status: "Success")
+  end
   let(:queue_name) { "caseflow_test_low_priority" }
+
+  before do
+    Seeds::NotificationEvents.new.seed!
+  end
 
   after(:each) do
     clear_enqueued_jobs
@@ -41,6 +73,63 @@ describe VANotifyStatusUpdateJob, type: :job do
         expect(Rails.logger).not_to receive(:error)
         expect { VANotifyStatusUpdateJob.perform_now.to receive(:send_to_va_notify) }
       end
+    end
+
+    describe "feature flags" do
+      describe "Email" do
+        it "updates the Notification when successful" do
+          email_only.email_notification_external_id = SecureRandom.uuid
+          allow(job).to receive(:notifications_not_processed).and_return([email_only])
+          job.perform_now
+          expect(email_only.email_notification_status).to eq("created")
+        end
+        it "logs when external id is not present" do
+          allow(job).to receive(:notifications_not_processed).and_return([email_only])
+          job.perform_now
+          expect(email_only.email_notification_status).to eq("No External Id")
+        end
+      end
+
+      describe "SMS" do
+        it "updates the Notification when successful" do
+          sms_only.sms_notification_external_id = SecureRandom.uuid
+          allow(job).to receive(:notifications_not_processed).and_return([sms_only])
+          job.perform_now
+          expect(sms_only.sms_notification_status).to eq("created")
+        end
+        it "logs when external id is not present" do
+          allow(job).to receive(:notifications_not_processed).and_return([sms_only])
+          job.perform_now
+          expect(sms_only.sms_notification_status).to eq("No External Id")
+        end
+      end
+
+      describe "Email and SMS" do
+        it "updates the Notification when successful" do
+          email_and_sms.sms_notification_external_id = SecureRandom.uuid
+          email_and_sms.email_notification_external_id = SecureRandom.uuid
+          allow(job).to receive(:notifications_not_processed).and_return([email_and_sms])
+          job.perform_now
+          expect(email_and_sms.sms_notification_status && email_and_sms.email_notification_status).to eq("created")
+        end
+        it "logs when external id is not present" do
+          allow(job).to receive(:notifications_not_processed).and_return([email_and_sms])
+          job.perform_now
+          expect(email_and_sms.sms_notification_status && email_and_sms.email_notification_status).to eq("No External Id")
+        end
+      end
+    end
+  end
+
+  context "#get_current_status" do
+    subject(:job) { VANotifyStatusUpdateJob.perform_later }
+    it "logs error when response is not 200" do
+      email_and_sms.sms_notification_external_id = SecureRandom.uuid
+      email_and_sms.email_notification_external_id = SecureRandom.uuid
+      allow(job).to receive(:notifications_not_processed).and_return([email_and_sms])
+      allow(VANotifyService).to receive(:get_status).and_return(HTTPI::Response.new(404, [], ""))
+      expect(job).to receive(:log_error).with(/VA Notify API returned error/)
+      job.perform_now
     end
   end
 end
