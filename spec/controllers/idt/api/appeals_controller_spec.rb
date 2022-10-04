5361a613021fe1cee3587867732949799291391a
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-10-03 22:02:16 -0400

    Fix UniqBeforePluck lint errors

--- spec/controllers/idt/api/appeals_controller_spec.rb
+++ spec/controllers/idt/api/appeals_controller_spec.rb
@@ -231,8 +231,8 @@ RSpec.describe Idt::Api::V1::AppealsController, type: :controller do
           # Only 2 of these are relevant to the AMA appeal.
           # AttorneyCaseReviews affect the "documents" attribute, so only 2 documents are returned.
           expect(AttorneyCaseReview.count).to eq 3
-          expect(AttorneyCaseReview.pluck(:appeal_type).uniq).to match_array %w[Appeal LegacyAppeal]
-          expect(AttorneyCaseReview.pluck(:appeal_id).uniq.size).to eq 1
+          expect(AttorneyCaseReview.distinct.pluck(:appeal_type)).to match_array %w[Appeal LegacyAppeal]
+          expect(AttorneyCaseReview.distinct.pluck(:appeal_id).size).to eq 1
 
           tasks.first.update(assigned_at: 5.days.ago)
           tasks.second.update(assigned_at: 15.days.ago)
