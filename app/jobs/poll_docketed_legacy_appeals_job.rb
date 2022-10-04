1de7e58b81b4c8e0ee74e1897f093df90e333923
Minhazur Rahaman <minhazur9@gmail.com>
2022-10-04 13:41:03 -0400

    APPEALS-9359 wrote logic for getting recent docketed appeals

--- app/jobs/poll_docketed_legacy_appeals_job.rb
+++ app/jobs/poll_docketed_legacy_appeals_job.rb
@@ -6,7 +6,15 @@ class PollDocketedLegacyAppealsJob < CaseflowJob
   queue_as :low_priority
   application_attr :hearing_schedule
 
-  def perform(vacols_id)
-    
+  def perform
+    vacols_appeals = VACOLS::CaseDocket.where(bfmpro: "ACT", bfac: %w[1 3 7])
+    claim_histories = VACOLS::Priorloc.where(lockey: vacols_appeals.pluck(:bfkey), locstto: "01")
+    vacols_ids = []
+    claim_histories.each do |claim_history|
+      if Time.zone.today.strftime("%d").to_i - claim_history.locdout.strftime("%d").to_i < 1
+        vacols_ids.push(claim_history.lockey)
+      end
+    end
+    vacols_ids
   end
 end
