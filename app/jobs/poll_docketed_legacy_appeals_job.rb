691686581fcf5a41a3d052f270c3c40572259c40
Minhazur Rahaman <minhazur9@gmail.com>
2022-10-04 13:56:40 -0400

    APPEALS-9359 added comments and cleaned up code

--- app/jobs/poll_docketed_legacy_appeals_job.rb
+++ app/jobs/poll_docketed_legacy_appeals_job.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: true
 
-# Purpose: Active Job that handles polling VACOLS for legacy appeals that have been recently docketed
+# Active Job that handles polling VACOLS for legacy appeals that have been recently docketed
 
 class PollDocketedLegacyAppealsJob < CaseflowJob
   queue_as :low_priority
   application_attr :hearing_schedule
 
   def perform
+    most_recent_docketed_appeals(claim_histories)
+  end
+
+  # Purpose: To get a list of claim location histories to be used to get the date for docketing
+  # Return: An array of case location history records
+  def claim_histories
     vacols_appeals = VACOLS::CaseDocket.where(bfmpro: "ACT", bfac: %w[1 3 7])
-    claim_histories = VACOLS::Priorloc.where(lockey: vacols_appeals.pluck(:bfkey), locstto: "01")
+    VACOLS::Priorloc.where(lockey: vacols_appeals.pluck(:bfkey), locstto: "01")
+  end
+
+  # Purpose: To get a list of all vacols ids for the most recent docketed appeals to be used for sending notifications
+  # Params: claim_histories - An array of case location history records
+  # Return: An array of vacols ids
+  def most_recent_docketed_appeals(claim_histories)
     vacols_ids = []
     claim_histories.each do |claim_history|
       if Time.zone.today.strftime("%d").to_i - claim_history.locdout.strftime("%d").to_i < 1
