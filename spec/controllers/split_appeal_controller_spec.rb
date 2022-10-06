fc67ab17351c97853fe8a3aa5acb350be0cdf72a
Stephan Ward <stephan.ward@va.gov>
2022-10-06 16:32:28 -0400

    rSpec Split Appeal Controller

--- spec/controllers/split_appeal_controller_spec.rb
+++ spec/controllers/split_appeal_controller_spec.rb
@@ -1,5 +1,25 @@
 RSpec.describe SplitAppealController, :postgres do
-  let(:user) { User.authenticate!(roles: ["Hearing Prep"]) }
-  let(:appeal_id)
-end
+  let(:user_css_id) { "SPLTAPPLSNOW" }
+  let(:appeal_id) { "1" }
+  let(:split_issue) { "Some Split Issue" }
+  let(:split_other_reason) { "Some split_other_reason" }
+  let(:split_reason) { "Some split_other_reason" }
+  let(:appeal) { Appeal.find(appeal_id) }
   
+  describe split_appeal do
+    context
+      let(FeatureToggle.enabled?(:split_appeal_workflow)) == True;
+      let(:user_css_id) { "SPLTAPPLSNOW" }
+      let(:appeal_id) { "1" }
+      let(:split_issue) { "Some Split Issue" }
+      let(:split_other_reason) { "Some split_other_reason" }
+      let(:split_reason) { "Some split_other_reason" }
+      let(:appeal) { Appeal.find(appeal_id) }
+      let(:dup_appeal) { appeal.amoeba_dup }
+      if dup_appeal != Appeal.find(appeal_id)
+        return success
+      else
+        return fail
+      end
+    end
+end
