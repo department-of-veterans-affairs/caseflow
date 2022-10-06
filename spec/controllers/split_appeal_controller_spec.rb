e618e4afc7145d66d18164cff3f28c024f4be879
Stephan Ward <stephan.ward@va.gov>
2022-10-06 14:19:38 -0400

    Drafting New PR

--- spec/controllers/split_appeal_controller_spec.rb
+++ spec/controllers/split_appeal_controller_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe SplitAppealController, :postgres do
-    let(:user) { User.authenticate!(roles: ["Hearing Prep"]) }
-    let(:appeal_id) 
+  let(:user) { User.authenticate!(roles: ["Hearing Prep"]) }
+  let(:appeal_id)
 end
   
