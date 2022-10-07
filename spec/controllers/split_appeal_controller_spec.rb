0e57195e2b6133d42da62572c828b43d97a946e7
Stephan Ward <stephan.ward@va.gov>
2022-10-07 08:15:17 -0400

    Small Refractor

--- spec/controllers/split_appeal_controller_spec.rb
+++ spec/controllers/split_appeal_controller_spec.rb
@@ -5,21 +5,16 @@ RSpec.describe SplitAppealController, :postgres do
   let(:split_other_reason) { "Some split_other_reason" }
   let(:split_reason) { "Some split_other_reason" }
   let(:appeal) { Appeal.find(appeal_id) }
-  
-  describe split_appeal do
-    context
-      let(FeatureToggle.enabled?(:split_appeal_workflow)) == True;
-      let(:user_css_id) { "SPLTAPPLSNOW" }
-      let(:appeal_id) { "1" }
-      let(:split_issue) { "Some Split Issue" }
-      let(:split_other_reason) { "Some split_other_reason" }
-      let(:split_reason) { "Some split_other_reason" }
-      let(:appeal) { Appeal.find(appeal_id) }
-      let(:dup_appeal) { appeal.amoeba_dup }
-      if dup_appeal != Appeal.find(appeal_id)
-        return success
-      else
-        return fail
-      end
-    end
+     
+  it "Will split the appeal on click" do
+    let(FeatureToggle.enabled?(:split_appeal_workflow)) == True;
+    let(:user_css_id) { "SPLTAPPLSNOW" }
+    let(:appeal_id) { "1" }
+    let(:split_issue) { "Some Split Issue" }
+    let(:split_other_reason) { "Some split_other_reason" }
+    let(:split_reason) { "Some split_other_reason" }
+    let(:appeal) { Appeal.find(appeal_id) }
+    let(:dup_appeal) { appeal.amoeba_dup }
+    expect(dup_appeal).not_to be_(appeal_id)
+  end
 end
