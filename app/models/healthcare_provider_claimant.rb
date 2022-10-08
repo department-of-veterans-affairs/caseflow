a5ef36c5c44a2aed98a27a8734444e3483bf5987
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-10-07 22:57:12 -0400

    Add class comment

--- app/models/healthcare_provider_claimant.rb
+++ app/models/healthcare_provider_claimant.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+# HealthcareProviderClaimant is used whenever a HCP (Healthcare Provider)
+# is not listed in CorpDB, allowing a claim to be processed through Intake
+# despite this absence.
+
 class HealthcareProviderClaimant < OtherClaimant; end
