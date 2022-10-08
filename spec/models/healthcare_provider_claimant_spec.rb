2b6e27027963d27a28541e042e46495eef071a80
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-10-07 22:44:08 -0400

    Add factory

---
+++ spec/models/healthcare_provider_claimant_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+describe HealthcareProviderClaimant do
+  let(:appeal_with_healthcare_provider_claimant) do
+    create(
+      :appeal,
+      has_healthcare_provider_claimant: true,
+      veteran_is_not_claimant: true
+    )
+  end
+
+  subject { appeal_with_healthcare_provider_claimant.claimant }
+
+  context "#unrecognized_claimant?" do
+    it "HealthcareProvider is considered an unrecognized appellant" do
+      expect(subject.unrecognized_claimant?).to eq false
+    end
+  end
+end
