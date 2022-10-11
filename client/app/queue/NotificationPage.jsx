26b92b72932140cbba319d845e08872dbb8f54f3
Eric Moore <moore_eric@bah.com>
2022-10-11 15:14:23 -0400

    create  NotificationPage

---
+++ client/app/queue/NotificationPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+
+export const NotificationPage = (props) => {
+  return (
+    <>
+
+      {/* <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sunt iste, tempore ipsa dolore consequatur impedit eaque exercitationem aliquid vero nisi reiciendis minima accusantium aut quam corporis non tenetur laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem delectus possimus sint omnis, numquam temporibus doloremque deleniti, rerum repellendus, cupiditate maxime voluptate voluptatibus alias sapiente saepe optio at praesentium itaque? Architecto est modi esse hic possimus, amet voluptate nulla, iure sunt excepturi necessitatibus voluptatum sequi! Ipsum, consectetur soluta, suscipit fuga, vel ratione magni rerum nobis quo quos voluptatum deserunt numquam sapiente quam. Accusamus, maxime nisi tempora repudiandae esse atque aliquam fugiat hic beatae, obcaecati quidem assumenda provident sed dolore consequuntur inventore sequi! Rerum tempore, praesentium eveniet ipsa voluptatum iusto illum exercitationem perferendis voluptas dolores nulla sit, in delectus minus laborum veniam! Doloremque quas tempore eos iure impedit velit at pariatur quam corrupti libero, dicta delectus odio soluta vel laboriosam. A corrupti tenetur reiciendis incidunt nihil laboriosam, minus similique omnis quam enim sequi laborum dolores fugit excepturi, possimus sint! Molestias autem exercitationem, optio similique minima praesentium illo quo quasi vero, reiciendis animi velit. At fugit facilis debitis ullam eveniet eius quisquam provident rerum vero labore ex culpa, reprehenderit aperiam inventore repellat temporibus, iusto exercitationem facere quae consequuntur praesentium. Recusandae nisi eum suscipit quas aspernatur, minima animi laborum nulla placeat nobis quam? Corporis maxime voluptates commodi porro mollitia, saepe numquam optio sequi!</p> */}
+    </>
+  );
+};
+
