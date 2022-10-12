ca22dc88e713bc36ef81e6d8d1b62bcf053ad604
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-10-11 22:24:39 -0400

    Properly route users back to / upon cancelling intake on AddClaimantPage

--- client/app/intake/addClaimant/AddClaimantPage.jsx
+++ client/app/intake/addClaimant/AddClaimantPage.jsx
@@ -36,6 +36,13 @@ export const AddClaimantPage = ({ onAttorneySearch = fetchAttorneys, featureTogg
     })
   );
 
+  // Return to homepage of Intake upon cancellation
+  const detectCancellation = useMemo(() => {
+    if (!formType) {
+      return <Redirect to={PAGE_PATHS.BEGIN} />;
+    }
+  }, [formType]);
+
   const selectedForm = useMemo(() => {
     return Object.values(FORM_TYPES).find((item) => item.key === formType);
   }, [formType]);
@@ -127,9 +134,11 @@ export const AddClaimantPage = ({ onAttorneySearch = fetchAttorneys, featureTogg
           />
         )}
       </IntakeLayout>
+      {detectCancellation}
     </FormProvider>
   );
 };
+
 AddClaimantPage.propTypes = {
   featureToggles: PropTypes.object,
   onAttorneySearch: PropTypes.func,
