cfb81b9d7c8bf846246f14fed11afaf3dac8ec2e
j-n-t-l <611441@bah.com>
2022-10-14 15:18:06 -0400

    APPEALS-10065 refactored vbms service to have two upload_document_to_vbms

--- app/services/external_api/vbms_service.rb
+++ app/services/external_api/vbms_service.rb
@@ -38,7 +38,13 @@ class ExternalApi::VBMSService
     ExternalApi::VbmsDocumentSeriesForAppeal.new(file_number: appeal.veteran_file_number).fetch
   end
 
-  def self.upload_document_to_vbms(veteran_file_number, uploadable_document)
+  def self.upload_document_to_vbms(appeal, uploadable_document)
+    @vbms_client ||= init_vbms_client
+    response = initialize_upload(appeal.veteran_file_number, uploadable_document)
+    upload_document(appeal.veteran_file_number, response.upload_token, uploadable_document.pdf_location)
+  end
+
+  def self.upload_document_to_vbms_veteran(veteran_file_number, uploadable_document)
     @vbms_client ||= init_vbms_client
     response = initialize_upload(veteran_file_number, uploadable_document)
     upload_document(veteran_file_number, response.upload_token, uploadable_document.pdf_location)
