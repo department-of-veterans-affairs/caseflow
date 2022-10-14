64c1427ee61eab172353bea000b796433d6f730d
Stephan Ward <stephan.ward@va.gov>
2022-10-14 12:51:35 -0400

    Restored Orginal Controller

--- app/controllers/split_appeal_controller.rb
+++ app/controllers/split_appeal_controller.rb
@@ -29,29 +29,6 @@ class SplitAppealController < ApplicationController
 
       # run extra duplicate methods to finish split
       dup_appeal.finalize_split_appeal(appeal, user_css_id)
-
-      # save the dup_appeal as a record in the Caseflow SplitCorrelationTable & that Tableau reports off of.
-      if SplitCorrelationTable.find_by(original_appeal_uuid: appeal.uuid) == false
-        SplitCorrelationTable.create!(
-          appeal_id: dup_appeal.id, 
-          appeal_type: dup_appeal.docket_type, 
-          appeal_uuid: dup_appeal.uuid, 
-          created_at: Time.zone.now, 
-          created_by_id: user_css_id,
-          original_appeal_id: appeal.id,
-          original_appeal_uuid: appeal.uuid,
-          original_request_issue_ids: appeal.request_issues.ids,
-          relationship_type: "split_appeal",
-          split_other_reason: split_other_reason,
-          split_reason: split_reason,
-          split_request_issue_ids: split_issue,
-          updated_at: Time.zone.now,
-          updated_by_id: user_css_id,
-          working_split_status: Constants.TASK_STATUSES.in_progress
-        );
-        #iterate over the request issues array and update accordingly for old & new
-      end
-
     end
   end
 end
