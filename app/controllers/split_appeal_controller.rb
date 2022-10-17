ab7dc04843c83b7821262546e626724858e36a53
Stephan Ward <stephan.ward@va.gov>
2022-10-17 17:46:51 -0400

    Code Reached Array

--- app/controllers/split_appeal_controller.rb
+++ app/controllers/split_appeal_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SplitAppealController < ApplicationController
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   def split_appeal
     if FeatureToggle.enabled?(:split_appeal_workflow)
@@ -29,16 +29,26 @@ class SplitAppealController < ApplicationController
 
       # run extra duplicate methods to finish split
       dup_appeal.finalize_split_appeal(appeal, user_css_id)
-
-      SplitCorrelationTable.create_split_record(
-        appeal_id,
-        split_issue,
-        split_other_reason,
-        split_reason,
-        appeal,
-        dup_appeal,
-        user_css_id
-      )
+      
+      split_record = SplitCorrelationTable.new
+        (
+        appeal_id = dup_appeal.id,
+        appeal_type = dup_appeal.docket_type,
+        appeal_uuid = dup_appeal.uuid,
+        created_at = Time.zone.now.utc,
+        created_by_id = user_css_id,
+        original_appeal_id = appeal.id,
+        original_appeal_uuid = appeal.uuid,
+        original_request_issue_ids = appeal.request_issues.ids,
+        relationship_type = "split_appeal",
+        split_other_reason = split_other_reason,
+        split_reason = split_reason,
+        split_request_issue_ids = split_issue,
+        updated_at = Time.zone.now.utc,
+        updated_by_id = user_css_id,
+        working_split_status = Constants.TASK_STATUSES.in_progress
+        )
+      end
     end
   end
 end
--- app/models/split_correlation_table.rb
+++ app/models/split_correlation_table.rb
@@ -1,34 +1,28 @@
-# frozen_string_literal: true
-
-class SplitCorrelationTable < CaseflowRecord
+class SplitCorrelationTable < ApplicationController
   def create_split_record
-    Rails.logger.info(
-      "Trying to create split record for Appeal Id #{original_appeal_id} or UUID #{original_appeal_uuid}."
+    appeal = Appeal.find(appeal_id)
+    dup_appeal = appeal.amoeba_dup
+    dup_appeal.save
+    user_css_id = params[:user]
+    dup_appeal.finalize_split_appeal(appeal, user_css_id)
+
+    split_record = SplitCorrelationTable.new
+    (
+      appeal_id = dup_appeal.id,
+      appeal_type = dup_appeal.docket_type,
+      appeal_uuid = dup_appeal.uuid,
+      created_at = Time.zone.now.utc,
+      created_by_id = user_css_id.id,
+      original_appeal_id = appeal.id,
+      original_appeal_uuid = appeal.uuid,
+      original_request_issue_ids = appeal.request_issues.ids,
+      relationship_type = "split_appeal",
+      split_other_reason = split_other_reason,
+      split_reason = split_reason,
+      split_request_issue_ids = split_issue,
+      updated_at = Time.zone.now.utc,
+      updated_by_id = user_css_id,
+      working_split_status = Constants.TASK_STATUSES.in_progress
     )
-    begin
-      split_record = SplitCorrelationTable.new
-      split_record = split_record.save
-      update!(
-        appeal_id: dup_appeal.id,
-        appeal_type: dup_appeal.docket_type,
-        appeal_uuid: dup_appeal.uuid,
-        created_at: Time.zone.now.utc,
-        created_by_id: user_css_id.id,
-        original_appeal_id: appeal.id,
-        original_appeal_uuid: appeal.uuid,
-        original_request_issue_ids: appeal.request_issues.ids,
-        relationship_type: "split_appeal",
-        split_other_reason: split_other_reason,
-        split_reason: split_reason,
-        split_request_issue_ids: split_issue,
-        updated_at: Time.zone.now.utc,
-        updated_by_id: user_css_id,
-        working_split_status: Constants.TASK_STATUSES.in_progress
-      )
-      split_record.save
-    rescue StandardError => error
-      Raven.capture_exception(error: error)
-      raise error
-    end
   end
 end
