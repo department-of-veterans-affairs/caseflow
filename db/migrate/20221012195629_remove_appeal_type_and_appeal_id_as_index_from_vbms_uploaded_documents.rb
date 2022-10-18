528bd1f6a761b157645514539cb8ec518b3b23ed
j-n-t-l <611441@bah.com>
2022-10-18 13:03:46 -0400

    APPEALS-10065 updated migration parent class

--- db/migrate/20221012195629_remove_appeal_type_and_appeal_id_as_index_from_vbms_uploaded_documents.rb
+++ db/migrate/20221012195629_remove_appeal_type_and_appeal_id_as_index_from_vbms_uploaded_documents.rb
@@ -1,4 +1,4 @@
-class RemoveAppealTypeAndAppealIdAsIndexFromVbmsUploadedDocuments < ActiveRecord::Migration[5.2]
+class RemoveAppealTypeAndAppealIdAsIndexFromVbmsUploadedDocuments < Caseflow::Migration
   def change
     remove_index :vbms_uploaded_documents, :appeal_id
     remove_index :vbms_uploaded_documents, name: "index_vbms_uploaded_documents_on_appeal_type_and_appeal_id"
--- db/migrate/20221013173538_add_veteran_file_number_index_to_vbms_uploaded_documents.rb
+++ db/migrate/20221013173538_add_veteran_file_number_index_to_vbms_uploaded_documents.rb
@@ -1,7 +1,5 @@
-class AddVeteranFileNumberIndexToVbmsUploadedDocuments < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
+class AddVeteranFileNumberIndexToVbmsUploadedDocuments < Caseflow::Migration
   def change
-    add_index :vbms_uploaded_documents, :veteran_file_number, algorithm: :concurrently
+    add_safe_index :vbms_uploaded_documents, :veteran_file_number, algorithm: :concurrently
   end
 end
