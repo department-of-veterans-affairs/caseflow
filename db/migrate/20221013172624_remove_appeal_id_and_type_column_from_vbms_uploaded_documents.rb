305f5aa759002bebbc8a066bf6d725208c6403a5
j-n-t-l <611441@bah.com>
2022-10-18 14:16:01 -0400

    APPEALS-10065 added fixes to migrations to make them reversible

--- db/migrate/20221013172624_remove_appeal_id_and_type_column_from_vbms_uploaded_documents.rb
+++ db/migrate/20221013172624_remove_appeal_id_and_type_column_from_vbms_uploaded_documents.rb
@@ -1,6 +1,6 @@
 class RemoveAppealIdAndTypeColumnFromVbmsUploadedDocuments < Caseflow::Migration
   def change
-    safety_assured { remove_column :vbms_uploaded_documents, :appeal_id }
-    safety_assured { remove_column :vbms_uploaded_documents, :appeal_type }
+    safety_assured { remove_column :vbms_uploaded_documents, :appeal_id, :string }
+    safety_assured { remove_column :vbms_uploaded_documents, :appeal_type, :string }
     end
 end
