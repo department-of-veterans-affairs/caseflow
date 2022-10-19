8dfeb87ffca5a5dfbb18ebe847041cd7babc8eba
sulak-j <justin.sulak@va.gov>
2022-10-19 14:23:04 -0500

    adding route to misc in user controller

--- app/controllers/test/users_controller.rb
+++ app/controllers/test/users_controller.rb
@@ -60,7 +60,8 @@ class Test::UsersController < ApplicationController
       links: {
         styleguide: "/styleguide",
         stats: "/stats",
-        jobs: "/jobs"
+        jobs: "/jobs",
+        admin: "/admin"
       }
     }
   ].freeze
