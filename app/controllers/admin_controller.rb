0a0db2478b55cc858916cf27e14d8d9d1eb2d997
sulak-j <justin.sulak@va.gov>
2022-10-19 15:13:25 -0500

    adding code into the admin_controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
-  skip_before_action :verify_authentication
+  skip_before_action :verify_authentication [
+    :show
+  ]
+
+  def show
+    no_cache
+    respond_to do |format|
+      format.html { render template: "admin/index" }
+    end
+  end
 end
