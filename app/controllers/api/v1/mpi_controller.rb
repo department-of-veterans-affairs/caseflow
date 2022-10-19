94ec520d9afe1ef15ad4682444b0f28e72a49896
Calvin <Calvin.Costa@va.gov>
2022-10-19 14:29:15 -0400

    changed params to snake case

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::MpiController < Api::ApplicationController
   # {POST Method for Veteran ID, Deceased Indicator, Deceased Time}
   def veteran_updates
     id = allowed_params[:veterans_id]
-    deceased_ind = allowed_params[:deceasedInd]
-    deceased_time = allowed_params[:deceasedTime]
+    deceased_ind = allowed_params[:deceased_ind]
+    deceased_time = allowed_params[:deceased_time]
     render json: { success: true }, status: :ok
   end
 
