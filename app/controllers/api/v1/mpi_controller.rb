c1994fc4962e1d5879c85c3926d7a7dc169e92c3
Calvin <Calvin.Costa@va.gov>
2022-10-19 12:46:35 -0400

    updated controller to fit AC better

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -3,13 +3,9 @@
 class Api::V1::MpiController < Api::ApplicationController
   # {POST Method for Veteran ID, Deceased Indicator, Deceased Time}
   def veteran_updates
-
-    id = Correspondent[allowed_params[:veterans_id]]
-    
-    deceased_ind = Correspondent[allowed_params[:deceasedInd]]
-    
-    deceased_time = Correspondent[allowed_params[:deceasedTime]]
-    
+    id = allowed_params[:veterans_id]
+    deceased_ind = allowed_params[:deceasedInd]
+    deceased_time = allowed_params[:deceasedTime]
     render json: { success: true }, status: :ok
   end
 
