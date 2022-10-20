d91b3386089bac1af492e4462b79e8aa1fed1e87
Eric Moore <moore_eric@bah.com>
2022-10-20 17:13:39 -0400

    APPEALS-8487 made one test

--- client/test/app/hearings/NotificationsView.test.js
+++ client/test/app/hearings/NotificationsView.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { render,screen  } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { NotificationsView } from 'app/queue/NotificationsView';
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 
 const createReducer = (storeValues) => {
   return function (state = storeValues) {
@@ -15,71 +14,59 @@ const createReducer = (storeValues) => {
   };
 };
 
-const setup = (props={}) => {
-  const reducer = createReducer(props);
-  const defaults = {};
+const setup = (state) => {
+  const reducer = createReducer(state);
+  const props = {
+    appealId: 'e1bdff31-4268-4fd4-a157-ebbd48013d91',
+    attr: 'dedfsd',
+    attr2: 'qrdfds'
+  };
 
- 
   const store = createStore(reducer);
 
   return render(
     <Provider store={store} >
       <Router>
-      <NotificationsView {...defaults} />
+        <NotificationsView {...props} />
       </Router>
     </Provider>
   );
 };
 const appeal = {
-  id:"1987",
-appellant_hearing_email_recipient:null,
-representative_hearing_email_recipient:null,
-externalId:"e1bdff31-4268-4fd4-a157-ebbd48013d91",
-docketName:"hearing",
-withdrawn:false,
-removed:false,
-overtime:false,
-contestedClaim:false,
-veteranAppellantDeceased:false,
-isLegacyAppeal:false,
-caseType:"Original",
-isAdvancedOnDocket:false,
-issueCount:0,
-docketNumber:"220715-1987",
-assignedAttorney:null,
-assignedJudge:null,
-distributedToJudge:false,
-veteranFullName:"Bob Smithschumm",
-veteranFileNumber:"200000161",
-isPaperCase:false,
-readableHearingRequestType:null,
-readableOriginalHearingRequestType:null,
-vacateType:null
+  id: '1987',
+  isLegacyAppeal: false,
+  docketNumber: '220715-1987',
+  veteranFullName: 'Bob Smithschumm',
+  veteranFileNumber: '200000161',
+  hearings: [],
 };
 
-const props = {
-  
-    queue:{
-      appealId:"e1bdff31-4268-4fd4-a157-ebbd48013d91",
-      appeals:{"e1bdff31-4268-4fd4-a157-ebbd48013d91":appeal} 
+const state = {
+  queue: {
+    appealId: 'e1bdff31-4268-4fd4-a157-ebbd48013d91',
+    appeals: { 'e1bdff31-4268-4fd4-a157-ebbd48013d91': appeal },
+    mostRecentlyHeldHearingForAppeal: {}
 
-    },
-    ui:{
-    organizations:
-    [{name: "Hearings Management",  url: "hearings-management"}]
-    
-    },
-  
- 
-}
-
+  },
+  appealId: 'e1bdff31-4268-4fd4-a157-ebbd48013d91',
+  ui: {
+    organizations: [{ name: 'Hearings Management', url: 'hearings-management' }],
+    featureToggles: {
+      overtime_revamp: false
+    }
+  },
+};
 
 describe('NotificationsTest', () => {
+
+
+  it('renders title correctly', () => {
+
+    const {container} = setup(state);
+    const header = container.querySelector('h1').innerHTML
+    expect(header).toBe('Case notifications for Bob Smithschumm')
   
-  
-  it('renders correctly', () => {
-   
-        const  container  = setup(props);
- });
+  });
 
 });
+
