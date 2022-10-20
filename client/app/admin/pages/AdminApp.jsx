a0428071538e49b3f4cc927e42ddd7357a710fc8
sulak-j <justin.sulak@va.gov>
2022-10-20 14:13:18 -0500

    Adding styling to admin app

--- client/app/admin/pages/AdminApp.jsx
+++ client/app/admin/pages/AdminApp.jsx
@@ -4,33 +4,50 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import PageRoute from '../../components/PageRoute';
 // import ReduxBase from '../components/ReduxBase';
 import reducers from '../reducers/index';
-// export const Admin = ({
-//   sys_admin,
-//   errors,
-//   showDivider,
-//   formFieldsOnly,
-//   update,
-//   actionType
-// }) => {
-// export const AdminApp = () => {
-//   return (
-//     <h1>System Admin UI</h1>
-//   );
-// };
-
+import NavigationBar from '../../components/NavigationBar';
+import CaseSearchLink from '../../components/CaseSearchLink';
+import { LOGO_COLORS } from '../../constants/AppConstants';
+import AppFrame from '../../components/AppFrame';
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer';
 export default class AdminApp extends React.PureComponent {
   render = () => <BrowserRouter basename="/admin">
-    <h1>System Admin UI</h1>
-    <div>
-      <Switch>
-        <PageRoute
-          exact
-          path="/admin"
-          title="admin"
-          render={this.admin}
-        />
-      </Switch>
-    </div>
+    <NavigationBar
+      wideApp
+      defaultUrl="/admin"
+      // userDisplayName={this.props.userDisplayName}
+      // dropdownUrls={this.props.dropdownUrls}
+      // applicationUrls={this.props.applicationUrls}
+      logoProps={{
+        overlapColor: LOGO_COLORS.QUEUE.OVERLAP,
+        accentColor: LOGO_COLORS.QUEUE.ACCENT,
+      }}
+      rightNavElement={<CaseSearchLink />}
+      appName="System Admin"
+    >
+      <AppFrame wideApp>
+        <AppSegment filledBackground>
+          <h1>System Admin UI</h1>
+          <div />
+          <div>
+            <Switch>
+              <PageRoute
+                exact
+                path="/admin"
+                title="admin"
+                render={this.admin}
+              />
+            </Switch>
+          </div>
+        </AppSegment>
+      </AppFrame>
+    </NavigationBar>
+    <Footer
+      wideApp
+      appName=""
+      // feedbackUrl={this.props.feedbackUrl}
+      // buildDate={this.props.buildDate}
+    />
   </BrowserRouter>
 }
 
