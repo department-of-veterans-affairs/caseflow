60f5ce4538d583a681b04e355134953055226514
j-n-t-l <611441@bah.com>
2022-10-21 13:35:15 -0400

    APPEALS-10065 updated params in controller

--- app/controllers/idt/api/v1/upload_vbms_document_controller.rb
+++ app/controllers/idt/api/v1/upload_vbms_document_controller.rb
@@ -11,31 +11,31 @@ class Idt::Api::V1::UploadVbmsDocumentController < Idt::Api::V1::BaseController
 
   def create
     # Find veteran from appeal id and check with db
-    if request.parameters["appeal_id"].present?
-      appeal = LegacyAppeal.find_by_vacols_id(request.parameters["appeal_id"]) || Appeal.find_by_uuid(request.parameters["appeal_id"])
+    if params["appeal_id"].present?
+      appeal = LegacyAppeal.find_by_vacols_id(params["appeal_id"]) || Appeal.find_by_uuid(params["appeal_id"])
       if appeal.nil?
         fail Caseflow::Error::AppealNotFound, "IDT Standard Error ID: " + SecureRandom.uuid + " The appeal was unable to be found."
       else
-        request.parameters["veteran_file_number"] = appeal.veteran_file_number
+        params["veteran_file_number"] = appeal.veteran_file_number
       end
 
     # check file number with bgs
-    elsif request.parameters["veteran_file_number"].present?
-      veteran = bgs.fetch_veteran_info(request.parameters["veteran_file_number"])
+    elsif params["veteran_file_number"].present?
+      veteran = bgs.fetch_veteran_info(params["veteran_file_number"])
       if veteran.nil?
         fail Caseflow::Error::VeteranNotFound, "IDT Standard Error ID: " + SecureRandom.uuid + " The veteran was unable to be found."
       end
 
     # Find file number from ssn and check with bgs
-    elsif request.parameters["veteran_ssn"].present?
-      file_number = bgs.fetch_file_number_by_ssn(request.parameters["veteran_ssn"])
+    elsif params["veteran_ssn"].present?
+      file_number = bgs.fetch_file_number_by_ssn(params["veteran_ssn"])
       if file_number.nil?
         fail Caseflow::Error::VeteranNotFound, "IDT Standard Error ID: " + SecureRandom.uuid + " The veteran was unable to be found."
       end
 
-      request.parameters["veteran_file_number"] = file_number
+      params["veteran_file_number"] = file_number
     end
-    result = PrepareDocumentUploadToVbms.new(request.parameters, current_user).call
+    result = PrepareDocumentUploadToVbms.new(params, current_user).call
 
     if result.success?
       render json: { message: "Document successfully queued for upload." }
