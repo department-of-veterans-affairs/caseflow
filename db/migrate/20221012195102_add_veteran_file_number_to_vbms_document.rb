db34d623914ffd65aa53366a971b0353d0fd0b5e
jshields13 <jeremy.shields@va.gov>
2022-10-24 18:49:41 -0400

    APPEALS-10065 fix migration

--- db/migrate/20221012195102_add_veteran_file_number_to_vbms_document.rb
+++ db/migrate/20221012195102_add_veteran_file_number_to_vbms_document.rb
@@ -1,5 +1,5 @@
 class AddVeteranFileNumberToVbmsDocument < Caseflow::Migration
   def change
-    add_column :vbms_uploaded_documents, :veteran_file_number, :string, null: false
+    add_column :vbms_uploaded_documents, :veteran_file_number, :string
   end
 end
