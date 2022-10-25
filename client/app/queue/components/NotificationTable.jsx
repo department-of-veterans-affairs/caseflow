6b640e62906d8e49e15a60b315d3836e205c4821
Minhazur Rahaman <minhazur9@gmail.com>
2022-10-25 13:55:47 -0400

    APPEALS-8488 added the rest of the columns

--- client/app/queue/components/NotificationTable.jsx
+++ client/app/queue/components/NotificationTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import QueueTable from '../QueueTable';
-import { eventTypeColumn, notificationDateColumn } from './NotificationTableColumns';
+import { eventTypeColumn, notificationDateColumn, notificationTypeColumn, recipientInformationColumn, statusColumn } from './NotificationTableColumns';
 import NOTIFICATION_CONFIG from '../../../constants/NOTIFICATION_CONFIG';
 
 const NotificationTable = () => {
@@ -11,14 +11,14 @@ const NotificationTable = () => {
         "created_at" : "2022-10-19T19:07:57.179Z",
         "email_enabled" : true,
         "email_notification_external_id" : null,
-        "email_notification_status" : null,
+        "email_notification_status" : 'sent',
         "event_date" : "2022-10-19",
         "event_type" : "Appeal docketed",
         "notification_content" : null,
         "notification_type" : "Email and SMS",
         "notified_at" : null,
         "participant_id" : "500000123",
-        "recipient_email" : null,
+        "recipient_email" : "test@caseflow.com",
         "recipient_phone_number" : null,
         "sms_notification_external_id" : null,
         "sms_notification_status" : null,
@@ -31,14 +31,14 @@ const NotificationTable = () => {
         "created_at" : "2022-10-19T19:07:58.267Z",
         "email_enabled" : true,
         "email_notification_external_id" : null,
-        "email_notification_status" : null,
+        "email_notification_status" : 'sent',
         "event_date" : "2022-10-20",
         "event_type" : "Hearing scheduled",
         "notification_content" : null,
         "notification_type" : "Email and SMS",
         "notified_at" : null,
         "participant_id" : null,
-        "recipient_email" : null,
+        "recipient_email" : "vanotify@caseflow.com",
         "recipient_phone_number" : null,
         "sms_notification_external_id" : null,
         "sms_notification_status" : null,
@@ -48,7 +48,10 @@ const NotificationTable = () => {
   const createColumnObject = (column) => {
     const functionForColumn = {
       [NOTIFICATION_CONFIG.COLUMNS.EVENT_TYPE.name]: eventTypeColumn(data),
-      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_DATE.name]: notificationDateColumn(data)
+      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_DATE.name]: notificationDateColumn(data),
+      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_TYPE.name]: notificationTypeColumn(data),
+      [NOTIFICATION_CONFIG.COLUMNS.RECIPIENT_INFORMATION.name]: recipientInformationColumn(data),
+      [NOTIFICATION_CONFIG.COLUMNS.STATUS.name]: statusColumn(data)
     };
 
     return functionForColumn[column.name];
