1a8361b017f903bc65e2dc1c24e150d6363c6ace
jshields13 <jeremy.shields@va.gov>
2022-10-24 20:05:16 -0400

    APPEALS-100065

--- db/migrate/20221024200000_alter_vbms_document_upload_appeal_id_columns.rb
+++ db/migrate/20221024200000_alter_vbms_document_upload_appeal_id_columns.rb
@@ -1,6 +1,6 @@
 class AlterVbmsDocumentUploadAppealIdColumns < Caseflow::Migration
     def change
-      change_column_null :appeal_id, :notified_at, false
-      change_column_null :appeal_type, :notified_at, false
+      change_column_null :vbms_uploaded_documents, :appeal_id, false
+      change_column_null :vbms_uploaded_documents, :appeal_type, false
     end
   end
