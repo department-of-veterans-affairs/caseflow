8b33fca62e483fec10ee6c08c172898f670ae96e
Minhazur Rahaman <minhazur9@gmail.com>
2022-10-26 10:36:45 -0400

    APPEALS-8488 data now getting fetched from notification table

--- client/app/queue/components/NotificationTable.jsx
+++ client/app/queue/components/NotificationTable.jsx
@@ -1,57 +1,38 @@
 import React from 'react';
 import QueueTable from '../QueueTable';
-import { eventTypeColumn, notificationDateColumn, notificationTypeColumn, recipientInformationColumn, statusColumn } from './NotificationTableColumns';
+import {
+  eventTypeColumn,
+  notificationDateColumn,
+  notificationTypeColumn,
+  recipientInformationColumn,
+  statusColumn } from './NotificationTableColumns';
 import NOTIFICATION_CONFIG from '../../../constants/NOTIFICATION_CONFIG';
+import { useState, useEffect } from 'react';
+import ApiUtil from '../../util/ApiUtil';
 
 const NotificationTable = () => {
-  const data = [{
-        "id" : 24,
-        "appeals_id" : "601560025",
-        "appeals_type" : "LegacyAppeal",
-        "created_at" : "2022-10-19T19:07:57.179Z",
-        "email_enabled" : true,
-        "email_notification_external_id" : null,
-        "email_notification_status" : 'sent',
-        "event_date" : "2022-10-19",
-        "event_type" : "Appeal docketed",
-        "notification_content" : null,
-        "notification_type" : "Email and SMS",
-        "notified_at" : null,
-        "participant_id" : "500000123",
-        "recipient_email" : "test@caseflow.com",
-        "recipient_phone_number" : null,
-        "sms_notification_external_id" : null,
-        "sms_notification_status" : null,
-        "updated_at" : "2022-10-19T19:07:57.179Z"
-      },
-      {
-        "id" : 25,
-        "appeals_id" : "601620353",
-        "appeals_type" : "LegacyAppeal",
-        "created_at" : "2022-10-19T19:07:58.267Z",
-        "email_enabled" : true,
-        "email_notification_external_id" : null,
-        "email_notification_status" : 'sent',
-        "event_date" : "2022-10-20",
-        "event_type" : "Hearing scheduled",
-        "notification_content" : null,
-        "notification_type" : "Email and SMS",
-        "notified_at" : null,
-        "participant_id" : null,
-        "recipient_email" : "vanotify@caseflow.com",
-        "recipient_phone_number" : null,
-        "sms_notification_external_id" : null,
-        "sms_notification_status" : null,
-        "updated_at" : "2022-10-19T19:07:58.267Z"
-      }
-    ]
+
+  const [notificationList, setNotificationList] = useState([]);
+
+  const fetchNotifications = () => {
+  ApiUtil.get("/appeals/e7646b46-3b1d-4cff-9988-a891e3626a2d/notifications")
+  .then(((response) => {
+    const { notifications } = response.body;
+    setNotificationList(notifications.data)
+  })).catch(((setNotificationList([]))));
+};
+
+  useEffect(() => {
+    fetchNotifications()
+  },[])
+
   const createColumnObject = (column) => {
     const functionForColumn = {
-      [NOTIFICATION_CONFIG.COLUMNS.EVENT_TYPE.name]: eventTypeColumn(data),
-      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_DATE.name]: notificationDateColumn(data),
-      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_TYPE.name]: notificationTypeColumn(data),
-      [NOTIFICATION_CONFIG.COLUMNS.RECIPIENT_INFORMATION.name]: recipientInformationColumn(data),
-      [NOTIFICATION_CONFIG.COLUMNS.STATUS.name]: statusColumn(data)
+      [NOTIFICATION_CONFIG.COLUMNS.EVENT_TYPE.name]: eventTypeColumn(notificationList),
+      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_DATE.name]: notificationDateColumn(notificationList),
+      [NOTIFICATION_CONFIG.COLUMNS.NOTIFICATION_TYPE.name]: notificationTypeColumn(notificationList),
+      [NOTIFICATION_CONFIG.COLUMNS.RECIPIENT_INFORMATION.name]: recipientInformationColumn(notificationList),
+      [NOTIFICATION_CONFIG.COLUMNS.STATUS.name]: statusColumn(notificationList)
     };
 
     return functionForColumn[column.name];
@@ -70,7 +51,7 @@ const NotificationTable = () => {
   return (
     <QueueTable
       columns={columnsFromConfig(NOTIFICATION_CONFIG.COLUMNS)}
-      rowObjects={data}
+      rowObjects={notificationList}
       enablePagination
       casesPerPage={15}
       numberofPages={1}
