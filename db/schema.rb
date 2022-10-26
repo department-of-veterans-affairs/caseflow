aab56b871321279900f0782a5710e56719eb43d4
Marc Steele <Marc.Steele@va.gov>
2022-10-26 09:35:35 -0400

    APPEALS-10597 Added columns to vbms_uploaded_documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_173538) do
+ActiveRecord::Schema.define(version: 2022_10_25_184724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1108,6 +1108,7 @@ ActiveRecord::Schema.define(version: 2022_10_13_173538) do
     t.string "appeals_id", null: false, comment: "ID of the Appeal"
     t.string "appeals_type", null: false, comment: "Type of Appeal"
     t.datetime "created_at", comment: "Timestamp of when Noticiation was Created"
+    t.boolean "email_enabled", default: true, null: false
     t.string "email_notification_external_id", comment: "VA Notify Notification Id for the email notification send through their API "
     t.string "email_notification_status", comment: "Status of the Email Notification"
     t.date "event_date", null: false, comment: "Date of Event"
@@ -1118,7 +1119,7 @@ ActiveRecord::Schema.define(version: 2022_10_13_173538) do
     t.string "participant_id", comment: "ID of Participant"
     t.string "recipient_email", comment: "Participant's Email Address"
     t.string "recipient_phone_number", comment: "Participants Phone Number"
-    t.string "sms_notification_external_id", comment: "VA Notify Notification Id for the sms notification send through their API "
+    t.string "sms_notification_external_id"
     t.string "sms_notification_status", comment: "Status of SMS/Text Notification"
     t.datetime "updated_at", comment: "TImestamp of when Notification was Updated"
     t.index ["appeals_id", "appeals_type"], name: "index_appeals_notifications_on_appeals_id_and_appeals_type"
@@ -1600,9 +1601,13 @@ ActiveRecord::Schema.define(version: 2022_10_13_173538) do
   end
 
   create_table "vbms_uploaded_documents", force: :cascade do |t|
+    t.bigint "appeal_id", null: false
+    t.string "appeal_type", null: false
     t.datetime "attempted_at"
     t.datetime "canceled_at", comment: "Timestamp when job was abandoned"
     t.datetime "created_at", null: false
+    t.string "document_name"
+    t.string "document_subject"
     t.string "document_type", null: false
     t.string "error"
     t.datetime "last_submitted_at"
@@ -1610,7 +1615,7 @@ ActiveRecord::Schema.define(version: 2022_10_13_173538) do
     t.datetime "submitted_at"
     t.datetime "updated_at", null: false
     t.datetime "uploaded_to_vbms_at"
-    t.string "veteran_file_number", null: false
+    t.string "veteran_file_number"
     t.index ["updated_at"], name: "index_vbms_uploaded_documents_on_updated_at"
     t.index ["veteran_file_number"], name: "index_vbms_uploaded_documents_on_veteran_file_number"
   end
