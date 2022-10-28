d693c6cae952fc0de931b6f89d011e15a5384f54
Marc Steele <Marc.Steele@va.gov>
2022-10-28 11:44:32 -0400

    APPEALS-10311 Corrected spelling error and reordered columns

--- db/scripts/audit/add_row_to_appeal_states_audit_table_function.sql
+++ db/scripts/audit/add_row_to_appeal_states_audit_table_function.sql
@@ -5,9 +5,10 @@ create or replace function caseflow_audit.add_row_to_appeal_states_audit() retur
 		if (TG_OP = 'DELETE') then
 			insert into caseflow_audit.appeal_states_audit select 'D', now(), user, OLD.*;
 		elsif (TG_OP = 'UPDATE') then
-			insert into caseflow_audit.appeal_states_audit select 'U', now(), user, OLD.*;
+        -- this should be new, I think
+			insert into caseflow_audit.appeal_states_audit select 'U', now(), user, NEW.*;
 		elsif (TG_OP = 'INSERT') then
-			insert into caseflow_audit.appeal_states_audit select 'I', now(), user, OLD.*;
+			insert into caseflow_audit.appeal_states_audit select 'I', now(), user, NEW.*;
 		end if;
 		return null;
 	end;
