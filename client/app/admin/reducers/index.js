418c25dcab86100bf776e7c265ae32b2b5d8989d
calvincostaBAH <108481161+calvincostaBAH@users.noreply.github.com>
2022-10-28 10:13:51 -0400

    Calvin/appeals 10328 (#17634)
    * initial commit, create admin folder and index.js
    
    * more file and folder creation and name working
    
    * Attempting to get admin page working
    
    * attempting router changes
    
    * Testing an admin controller
    
    * adding route to misc in user controller
    
    * adding code into the admin_controller
    
    * creating index html in views
    
    * making a new index call
    
    * removing comment in routes.rb
    
    * adding verify access template
    
    * adding reducers and browser history for router
    
    * moving the header above div
    
    * Adding styling to admin app
    
    * adjustments to admin for feature toggle
    
    * added proptypes to component
    
    * removed extraneous code and added some comments
    
    * cleaning up comments
    
    * added veteran_extract method + route
    
    * removing case search link from admin page
    
    * cc fix
    
    * removing commented-out code from earlier testing
    
    * created actions + reducer for veteran extract
    
    Co-authored-by: sulak-j <justin.sulak@va.gov>
    Co-authored-by: Jonathan Tsang <tsang_jonathan@bah.com>
    Co-authored-by: Matt Roth <mattcroth@gmail.com>

--- client/app/admin/reducers/index.js
+++ client/app/admin/reducers/index.js
@@ -2,10 +2,35 @@ import { combineReducers } from 'redux';
 import { timeFunction } from '../../util/PerfDebug';
 import commonComponentsReducer from '../../components/common/reducers';
 import { featureToggleReducer } from './featureToggle';
+import { ACTIONS } from '../constants';
+
+const extractInitialState = {};
+
+export const extractReducer = (state = extractInitialState, action) => {
+  switch (action.type) {
+  case ACTIONS.POST_VETERAN_EXTRACT:
+    return { ...state };
+  case ACTIONS.STARTED_VETERAN_EXTRACT:
+    return {
+      ...state,
+      manualExtractionStatus: action.payload.status,
+      manualExtractionSuccess: action.payload.success,
+    };
+  case ACTIONS.FAILED_VETERAN_EXTRACT:
+    return {
+      ...state,
+      manualExtractionStatus: action.payload.status,
+      manualExtractionSuccess: false,
+    };
+  default:
+    return state;
+  }
+};
 
 const combinedReducer = combineReducers({
   featureToggle: featureToggleReducer,
-  components: commonComponentsReducer
+  components: commonComponentsReducer,
+  extractReducer
 });
 
 export default timeFunction(
