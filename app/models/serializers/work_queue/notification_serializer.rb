86592acda8fba93cf032122b46e4fdc20ebeddb7
Jeremy Shields <jeremy.shields@va.gov>
2022-10-28 15:51:17 -0400

    aaron/APPEALS-10522 (#17638)
    Resolves #APPEALS-10522
    
    ### Description
    Added Notification Content to response when calling notifications API.
    
    ### Acceptance Criteria
    - [x] Notification Content Attribute is present within each notification model returned via notifications API call.
    - [x] Statuses of 'No claimant' & 'No participant_id' are excluded from being returned. 
    
    ### Testing Plan
    1. Go to https://vajira.max.gov/browse/APPEALS-10460
    
    ### Test Execution
    1. Go to https://vajira.max.gov/browse/APPEALS-10584
    2. Go to https://vajira.max.gov/browse/APPEALS-10588
    3. https://vajira.max.gov/browse/APPEALS-10640

--- app/models/serializers/work_queue/notification_serializer.rb
+++ app/models/serializers/work_queue/notification_serializer.rb
@@ -8,4 +8,5 @@ class WorkQueue::NotificationSerializer
   attribute :recipient_email
   attribute :recipient_phone_number
   attribute :sms_notification_status
+  attribute :notification_content
 end
