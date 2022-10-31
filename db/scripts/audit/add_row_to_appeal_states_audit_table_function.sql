12c0ce3a23951479bfc7eafaf9f2a93db2490e87
jshields13 <jeremy.shields@va.gov>
2022-10-31 15:06:01 -0400

    APPEALS-10311 Update trigger to use sequences

--- db/scripts/audit/add_row_to_appeal_states_audit_table_function.sql
+++ db/scripts/audit/add_row_to_appeal_states_audit_table_function.sql
@@ -3,12 +3,12 @@ create or replace function caseflow_audit.add_row_to_appeal_states_audit() retur
 	$appeal_states_audit$
 	begin 
 		if (TG_OP = 'DELETE') then
-			insert into caseflow_audit.appeal_states_audit select 'D', now(), user, OLD.*;
+			insert into caseflow_audit.appeal_states_audit select nextval('caseflow_audit.appeal_states_audit_id_seq'::regclass), 'D', OLD.*;
 		elsif (TG_OP = 'UPDATE') then
         -- this should be new, I think
-			insert into caseflow_audit.appeal_states_audit select 'U', now(), user, NEW.*;
+			insert into caseflow_audit.appeal_states_audit select nextval('caseflow_audit.appeal_states_audit_id_seq'::regclass), 'U', NEW.*;
 		elsif (TG_OP = 'INSERT') then
-			insert into caseflow_audit.appeal_states_audit select 'I', now(), user, NEW.*;
+			insert into caseflow_audit.appeal_states_audit select nextval('caseflow_audit.appeal_states_audit_id_seq'::regclass), 'I', NEW.*;
 		end if;
 		return null;
 	end;
