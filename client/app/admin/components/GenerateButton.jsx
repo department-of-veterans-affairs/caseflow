83f907bc2c9b90a9facc121f9032475d0cabd493
samasudhirreddy <sudhirreddy.sama@va.gov>
2022-10-31 11:21:52 -0500

    Added alert

--- client/app/admin/components/GenerateButton.jsx
+++ client/app/admin/components/GenerateButton.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 
 class GenerateButton extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modal: false
+    };
+  }
   // TODO This function for testing purposes only
   onClickChangeText = () => {
-    let x = document.getElementById('generate-extract');
-
-    if (x.innerHTML === 'Generate') {
-      x.innerHTML = 'Button Clicked.';
-    } else {
-      x.innerHTML = 'Generate';
-    }
+    this.setState({ modal: true });
   };
 
   render = () => {
@@ -19,16 +21,21 @@ class GenerateButton extends React.PureComponent {
     } = this.props;
 
     return (
-      <Button
-        id="generate-extract"
-        linkStyling
-        willNeverBeLoading
-        // TODO this onClick function will need to be changed
-        onClick={() => this.onClickChangeText()}
-        {...btnProps}
-      >
-        Generate
-      </Button>
+      <>
+        <Button
+          id="generate-extract"
+          onClick={() => this.onClickChangeText()}
+          {...btnProps}
+        >
+          Generate
+        </Button>
+        {this.state.modal && <Modal title="The file contains PII information, click OK to proceed"
+        confirmButton={ <Button onClick={() => {this.setState({ modal: false });}}>Okay</Button>}
+        closeHandler={() => {this.setState({ modal: false });}}
+        >
+        Whenever you are click on Okay button then file will start downloading.
+        </Modal>}
+      </>
     );
   };
 }
