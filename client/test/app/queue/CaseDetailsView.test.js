ab1543772e0d81574ae000552d1dd666c6d30c0c
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-11-02 17:59:44 -0400

    Get renderCaseDetailsView to work

--- client/test/app/queue/CaseDetailsView.test.js
+++ client/test/app/queue/CaseDetailsView.test.js
@@ -1,37 +1,87 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import  CaseDetailsView  from "../../../app/queue/CaseDetailsView";
-import { StoreValue } from './CaseDetailsViewData';
+import CaseDetailsView from "../../../app/queue/CaseDetailsView";
 import { queueWrapper as Wrapper } from 'test/data/stores/queueStore';
-import { amaAppeal,legacyAppeal } from '../../data/appeals';
+import { amaAppeal, legacyAppeal, powerOfAttorney } from '../../data/appeals';
 import COPY from '../../../COPY';
 
-const defaultProps = {userCanScheduleVirtualHearings:true,
-userCanAccessReader:true,
-userCanEditUnrecognizedPOA:true,
-vsoVirtualOptIn:true,
-}
+const defaultProps = {
+  userCanScheduleVirtualHearings: true,
+  userCanAccessReader: true,
+  userCanEditUnrecognizedPOA: true,
+  vsoVirtualOptIn: true,
+};
+
 const renderCaseDetailsView = (hasNotifications, appealData) => {
-  const storevalues = {queue:{appeals:[{...appealData, hasPOA:true, hasNotifications}]}}
-const props = {...defaultProps,appealId:appealData.id}
+  const stagedAppealData = {
+    [appealData.id]: {
+      ...appealData,
+      hasPOA: true,
+      hasNotifications,
+      isPaperCase: true,
+      powerOfAttorney,
+      appellantType: 'VeteranClaimant'
+    }
+  };
+
+  const storeValues = {
+    queue: {
+      appeals: [stagedAppealData],
+      appealDetails: stagedAppealData,
+      mostRecentlyHeldHearingForAppeal: {},
+      loadingAppealDetail: {
+        [appealData.externalId]: {
+          powerOfAttorney: {
+            loading: true
+          },
+          veteranInfo: {
+            loading: false
+          }
+        }
+      },
+      docCountForAppeal: {
+        [appealData.externalId]: {
+          docCountText: 0,
+          loading: false
+        }
+      },
+    },
+    ui: {
+      organizations: [
+        {
+          name: 'BVA Intake',
+          url: 'bva-intake'
+        }
+      ],
+      poaAlert: {
+        powerOfAttorney
+      }
+    }
+  };
+
+  const props = { ...defaultProps, appealId: appealData.id };
+
   render(
-    <Wrapper {...storevalues} >
-    <CaseDetailsView {...props} />
-  </Wrapper>
-  )
-}
+    <Wrapper {...storeValues} >
+      <CaseDetailsView {...props} />
+    </Wrapper>
+  );
+};
 
 describe('NotificationsLink', () => {
   describe('When there are notifications', () => {
     it('link appears with ama appeal', () => {
-      renderCaseDetailsView(true,amaAppeal)
+      renderCaseDetailsView(true,amaAppeal);
 
-      expect(screen.getByText(COPY.VIEW_NOTIFICATION_LINK)).toBeTruthy()
+      expect(screen.getByRole('link', { name: COPY.VIEW_NOTIFICATION_LINK })).toBeTruthy();
+    });
 
-    })
-  
-  })
+    // legacy with notifications
+  });
+
   describe('When there are\'not notifications', () => {
-  
-  })
-})
+    // ama without notifications
+
+    // legacy without notifications
+  });
+});
