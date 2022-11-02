4cbd11dd8cf91e7b8a62d123a561c46990363919
Sean Craig <Craig_Sean@bah.com>
2022-11-02 14:19:26 -0500

    fixed bug where no_veteran in vacols threw an error

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -11,11 +11,17 @@ class Api::V1::MpiController < Api::ApplicationController
 
     mpi_update = MpiUpdatePersonEvent.create!(api_key: api_key, created_at: Time.zone.now, update_type: :started)
     result = VACOLS::Correspondent.update_veteran_nod(veteran)
-    updated_veteran = VACOLS::Correspondent.find_veteran(veteran[:id])
-    info_column = {
-      veteran: veteran[:id],
-      updated_veteran: updated_veteran.rows.first[1]
-    }
+    if result != :no_veteran
+      updated_veteran = VACOLS::Correspondent.find_veteran(veteran[:id])
+      info_column = {
+        veteran: veteran[:id],
+        updated_veteran: updated_veteran.rows.first[1]
+      }
+    else
+      info_column = {
+        veteran: veteran[:id]
+      }
+    end
     info_column[:updated_column] = "deceased_time" if result == :successful
     mpi_update.update!(update_type: result, completed_at: Time.zone.now, info: info_column)
     render json: { success: result }, status: :ok
