85c24061ae7343c2a272f2d83515dbf941a02efe
Sean Craig <Craig_Sean@bah.com>
2022-11-03 10:15:26 -0500

    Cleaned up some of the logic in the controller

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -8,20 +8,19 @@ class Api::V1::MpiController < Api::ApplicationController
       deceased_ind: allowed_params[:deceased_ind],
       deceased_time: allowed_params[:deceased_time]
     }
-
     mpi_update = MpiUpdatePersonEvent.create!(api_key: api_key, created_at: Time.zone.now, update_type: :started)
-    result = VACOLS::Correspondent.update_veteran_nod(veteran)
     info_column = { veteran_id: veteran[:id] }
-    if result != :no_veteran
+    result = VACOLS::Correspondent.update_veteran_nod(veteran)
+    if result == :successful
       updated_veteran = VACOLS::Correspondent.find_veteran(veteran[:id])
-      info_column[:updated_veteran] = updated_veteran.rows.first[1]
+      info_column[:updated_column] = "deceased_time"
+      info_column[:updated_deceased_time] = updated_veteran.rows.first[1]
     end
-    info_column[:updated_column] = "deceased_time" if result == :successful
     mpi_update.update!(update_type: result, completed_at: Time.zone.now, info: info_column)
     render json: { success: result }, status: :ok
   rescue StandardError => error
-    # info_column[:error] = error
-    mpi_update.update!(update_type: :error, completed_at: Time.zone.now, info: error)
+    info_column[:error] = error
+    mpi_update.update!(update_type: :error, completed_at: Time.zone.now, info: info_column)
     raise error
   end
 
