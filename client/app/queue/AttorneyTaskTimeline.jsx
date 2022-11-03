972e84ebc0d0d080070d5700e0a0aeab9ebdbb9f
kristeja <112115264+kristeja@users.noreply.github.com>
2022-11-03 12:37:32 -0700

    Ki/appeals 9797 (#17654)
    * APPEALS-9795 added case type indicator and related styles
    
    * APPEALS-9795 fixed broken jest tests
    
    * APPEALS-9797 - CaseflowTimeline Copy Original
    
    * creating show/hide timeline feature
    
    * fix tasktimeline
    
    * APPEALS-9797 Timeline Functional
    
    * APPEALS-9797 Updated CSS, Fixed EDV file
    
    * added new labels and view/hide timeline feature adjusted css
    
    * APPEALS-10353 Attorney closedDate, linter, imports
    
    * APPEALS-10353 Fixed Linting
    
    Co-authored-by: KiMauVA <Ki.Mau@va.gov>
    Co-authored-by: IsaiahBar <isaiah.barnett@va.gov>

---
+++ client/app/queue/AttorneyTaskTimeline.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import AttorneyTaskRows from './components/AttorneyTaskRows';
+import { getAllTasksForAppeal } from './selectors';
+import { css } from 'glamor';
+
+export const AttorneyTaskTimeline = ({ appeal }) => {
+  const tasks = useSelector((state) => getAllTasksForAppeal(state, { appealId: appeal.externalId }));
+  const TimelineStyling = css({ margin: '1rem 0' });
+
+  return (
+    <React.Fragment>
+      <table {...TimelineStyling} id="attorney-task-timeline-table" summary="attorney timeline table">
+        <tbody>
+          <AttorneyTaskRows appeal={appeal}
+            taskList={tasks}
+            timeline
+          />
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+};
+
+AttorneyTaskTimeline.propTypes = {
+  appeal: PropTypes.object
+};
