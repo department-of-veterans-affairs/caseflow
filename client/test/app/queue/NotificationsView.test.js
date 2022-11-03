a0ff6ea583f53efaa0022681ebefc62f69e2f0a7
Minhazur Rahaman <minhazur9@gmail.com>
2022-11-03 10:42:00 -0400

    APPEALS-8488 removed seed file to avoid conflicts, fixed jest tests

--- client/test/app/queue/NotificationsView.test.js
+++ client/test/app/queue/NotificationsView.test.js
@@ -7,6 +7,21 @@ import {
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { axe } from 'jest-axe';
+import ApiUtil from '../../../app/util/ApiUtil';
+
+const createSpyGet = (data) => {
+  return jest.spyOn(ApiUtil, 'get').
+    mockImplementation(() => new Promise((resolve) => resolve({ body: data })));
+};
+
+beforeEach(() => {
+  createSpyGet([]);
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 const createReducer = (storeValues) => {
   return function (state = storeValues) {
     return state;
