52c3c7f1652cc159c03938da3c0cfec28dfb8541
harishvanga333 <HarishReddy.Vanga@va.gov>
2022-11-04 10:44:25 -0500

    refactor code

--- lib/helpers/cancel_active_task_array.rb
+++ lib/helpers/cancel_active_task_array.rb
@@ -15,27 +15,17 @@ module WarRoom
       # Sets the id of user or organization.
       user = assigned_to_id.to_i
       puts("Checking for a user or organization...")
-      # Show the user if found.
+      # If user not found fail interrupt
       if User.find_by_id(user).nil?
+        puts("Unable to find user #{user} based upon User.find_by_id.")
+        fail Interrupt
       end
-      # Show the organization if found.
-      if !Organization.find_by_id(user).nil?
-      else
+      # If organization not found fail interrupt
+      if Organization.find_by_id(user).nil?
         puts("Unable to find organization #{user} based upon Organization.find_by_id.")
-      end
-      # Checks if the assigned_to_id is found in the User or organization table
-      if !User.find_by_id(user).nil? || !Organization.find_by_id(user).nil?
-      else
-        puts("Unable to find user or organization by the assigned_to_id of #{user}")
         fail Interrupt
       end
-      # If a assigned to ID is found, this Checks to see if task type exists in the task table.
-      # Checks if the user or organization has tasks in general.
-      if (!User.find_by_id(user).nil? || Organization.find_by_id(user).nil?) &&
-         Task.where(assigned_to_id: user, type: task_type).nil? == true
-        puts("Unable to find tasks #{task_type} that were assigned to that User ID of #{user}...")
-        fail Interrupt
-      end
+
       # Checks that the assigned_to have the specified task type
       if Task.where(assigned_to_id: user, task: task_type).nil?
         puts("Unable to find task type. Have you checked the Metabase
@@ -48,7 +38,7 @@ module WarRoom
       # as a "Status" of "assigned", "in_progress", or "on_hold"
       array_task_ids = Task.where(assigned_to_id: user,
                                   type: task_type, status: %w[on_hold in_progress assigned])
-      puts(array_task_ids.to_s)
+      puts("array_task_ids #{array_task_ids.to_s}")
       if array_task_ids.empty?
         puts("Unable to establish array of the task type
                       and respective task ids. Aborting...")
@@ -60,10 +50,11 @@ module WarRoom
                   unless associated, complex, or dependent before or after tasks methods
                   related to other tasks")
       # Cancels the task array from the array_task_ids by the task find by
-      array_task_ids = Task.where(assigned_to_id: user, type: task_type, status: %w[on_hold in_progress assigned])
+      puts("RequestStore[:current_user] #{RequestStore[:current_user]}")
+      puts("User.system_user #{User.system_user}")
       RequestStore[:current_user] = User.system_user
       array_task_ids.update(status: Constants.TASK_STATUSES.cancelled,
-                            updated_at: Time.zone.now, cancelled_by_id: RequestStore[:current_user]&.id)
+                            updated_at: Time.zone.now, cancelled_by_id: RequestStore[:current_user].id)
       puts("Task #{array_task_ids} completed")
     end
   end
