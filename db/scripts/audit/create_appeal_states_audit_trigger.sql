8b1712a91eff7d96c4f34b4a64dbdd953c3e743c
Marc Steele <71673522+msteele96@users.noreply.github.com>
2022-11-04 13:57:09 -0400

    msteele/APPEALS-10311 (#17620)
    Resolves [APPEALS-10311](https://vajira.max.gov/browse/APPEALS-10311)
    
    ### Description
    Added executable ruby files to db/scripts/audit and set up make commands to run those files in the correct order for building and destroying a caseflow_audit schema in postgres. This sets up a trigger to log every change in the public.appeal_states table to the caseflow_audit.appeal_states_audit table.
    
    ### Acceptance Criteria
    A sql script exists that creates a caseflow audit schema
    
    A sql script exists that creates an audit trigger that:
      copies all columns of a row to the matching appeal_states_audit table row
      inserts an action into the types_of_change column:
        I - When the an insert to a a row happens.
        U - When an update to a row happens
        D - When a row is deleted.
    trigger runs on each time one of the actions happens on a row of the appeal-states table.
    
    A sql script exists that will create the an audit table for appeal_states
    
    ### Testing Plan
    1. Go to https://vajira.max.gov/browse/APPEALS-10774

---
+++ db/scripts/audit/create_appeal_states_audit_trigger.sql
@@ -0,0 +1,4 @@
+create trigger appeal_states_audit_trigger
+after insert or update or delete on public.appeal_states
+for each row
+execute procedure caseflow_audit.add_row_to_appeal_states_audit();
