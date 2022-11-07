8e4393ea54bb5e024b8fddfdff631ccb34150c43
samasudhirreddy <sudhirreddy.sama@va.gov>
2022-11-07 10:29:19 -0600

    changed class component to function component

--- client/app/admin/components/GenerateButton.jsx
+++ client/app/admin/components/GenerateButton.jsx
@@ -1,42 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Modal from '../../components/Modal';
 import Alert from '../../components/Alert';
 import LoadingContainer from '../../components/LoadingContainer';
 import { LOGO_COLORS } from 'app/constants/AppConstants';
 
-class GenerateButton extends React.PureComponent {
-  constructor(props) {
-    super(props);
+function GenerateButton(...btnProps) {
 
-    this.state = {
-      modal: false,
-      isLoading: false,
-      showBanner: false,
-      showErrorBanner: false
-    };
-  }
-  // TODO This function for testing purposes only
-  onClickChangeText = () => {
-    this.setState({
-      ...this.state,
-      modal: true
-    });
+  // state properties 
+  const [modal, setModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showBanner, setShowBanner] = useState(false);
+  const [showErrorBanner, setShowErrorBanner] = useState(false);
+
+
+  const onClickChangeText = () => {
+    setModal(true);
   };
 
-  onClickConfirmation = () => {
-    this.setState({
-      ...this.state,
-      modal: false,
-      isLoading: true
-    });
+  const onClickConfirmation = () => {
+    setModal(false);
+    setIsLoading(true);
 
     var request = new XMLHttpRequest();
     request.responseType = 'blob';
     request.open('get', '/admin.csv', true);
     request.send();
 
-    request.onreadystatechange =  () => {
+    request.onreadystatechange = () => {
       if (request.readyState === 4 && request.status === 200) {
         const downloadLink = document.createElement("a");
         const blob = new Blob(["\ufeff", request.response]);
@@ -49,74 +40,63 @@ class GenerateButton extends React.PureComponent {
         document.body.removeChild(downloadLink);
 
         // stop loading
-        this.setState({
-          ...this.state,
-          isLoading: false,
-          showBanner: true
-        });
-      } else if(request.readyState === 4 && (request.status < 200 || request.status >= 300)){
-         // stop loading
-         this.setState({
-          ...this.state,
-          isLoading: false,
-          showErrorBanner: true
-        });
+        setIsLoading(false);
+        setShowBanner(true);
+      } else if (request.readyState === 4 && (request.status < 200 || request.status >= 300)) {
+        // stop loading
+        setIsLoading(false);
+        setShowErrorBanner(true);
       }
     };
   }
 
-  render = () => {
-    const {
-      ...btnProps
-    } = this.props;
+  return (
+    <div style={{ height: '75vh' }}>
+      {
+        showBanner &&
+        <div style={{ padding: '10px' }}>
+          <Alert message="download success" type="success" />
+        </div>
+      }
+      {
+        showErrorBanner &&
+        <div style={{ padding: '10px' }}>
+          <Alert message="download failed" type="error" />
+        </div>
+      }
+      {
+        !isLoading &&
+        <Button
+          id="generate-extract"
+          onClick={() => onClickChangeText()}
+          {...btnProps}
+        >
+          Generate
+        </Button>
+      }
+      {
+        modal &&
 
-    return (
-      <div style={{ height: '75vh' }}>
-        {
-          this.state.showBanner &&
-          <div style={{ padding: '10px' }}>
-            <Alert message="download success" type="success" />
+        <Modal title="The file contains PII information, click OK to proceed"
+          confirmButton={<Button onClick={() => { onClickConfirmation() }}>Okay</Button>}
+          closeHandler={() => { setModal(false); }}
+        >
+          Whenever you are click on Okay button then file will start downloading.
+        </Modal>
+      }
+      {
+        isLoading &&
+
+        <LoadingContainer color={LOGO_COLORS.QUEUE.ACCENT}>
+          <div className="loading-div">
+            Action is Running...
           </div>
-        }
-        {
-          this.state.showErrorBanner &&
-          <div style={{ padding: '10px' }}>
-            <Alert message="download failed" type="error" />
-          </div>
-        }
-        {
-          !this.state.isLoading &&
-          <Button
-            id="generate-extract"
-            onClick={() => this.onClickChangeText()}
-            {...btnProps}
-          >
-            Generate
-          </Button>
-        }
-        {
-          this.state.modal &&
-
-          <Modal title="The file contains PII information, click OK to proceed"
-            confirmButton={<Button onClick={ () => {this.onClickConfirmation()}}>Okay</Button>}
-            closeHandler={() => { this.setState({ ...this.state, modal: false }); }}
-          >
-            Whenever you are click on Okay button then file will start downloading.
-          </Modal>
-        }
-        {
-          this.state.isLoading &&
-
-          <LoadingContainer color={LOGO_COLORS.QUEUE.ACCENT}>
-            <div className="loading-div">
-              Action is Running...
-            </div>
-          </LoadingContainer>
-        }
-      </div>
-    );
-  };
+        </LoadingContainer>
+      }
+    </div>
+  );
 }
+
 GenerateButton.propTypes = {
 };
 
