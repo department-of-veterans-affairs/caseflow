ddb6404a6d06520eae9178b640409bd27b721f8c
samasudhirreddy <sudhirreddy.sama@va.gov>
2022-11-07 07:30:36 -0600

    fetching csv file from controller

--- client/app/admin/components/GenerateButton.jsx
+++ client/app/admin/components/GenerateButton.jsx
@@ -13,6 +13,7 @@ class GenerateButton extends React.PureComponent {
       modal: false,
       isLoading: false,
       showBanner: false,
+      showErrorBanner: false
     };
   }
   // TODO This function for testing purposes only
@@ -23,14 +24,45 @@ class GenerateButton extends React.PureComponent {
     });
   };
 
+  onClickConfirmation = () => {
+    this.setState({
+      ...this.state,
+      modal: false,
+      isLoading: true
+    });
 
+    var request = new XMLHttpRequest();
+    request.responseType = 'blob';
+    request.open('get', '/admin.csv', true);
+    request.send();
 
-  timeOut = (mills) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(true);
-      }, mills);
-    })
+    request.onreadystatechange =  () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const downloadLink = document.createElement("a");
+        const blob = new Blob(["\ufeff", request.response]);
+        const url = URL.createObjectURL(blob);
+        downloadLink.href = url;
+        downloadLink.download = "data.csv";
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+
+        // stop loading
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          showBanner: true
+        });
+      } else if(request.readyState === 4 && (request.status < 200 || request.status >= 300)){
+         // stop loading
+         this.setState({
+          ...this.state,
+          isLoading: false,
+          showErrorBanner: true
+        });
+      }
+    };
   }
 
   render = () => {
@@ -39,11 +71,17 @@ class GenerateButton extends React.PureComponent {
     } = this.props;
 
     return (
-      <div style={{height: '75vh'}}>
+      <div style={{ height: '75vh' }}>
         {
-          this.state.showBanner && 
-          <div style={{padding: '10px'}}>
+          this.state.showBanner &&
+          <div style={{ padding: '10px' }}>
             <Alert message="download success" type="success" />
+          </div>
+        }
+        {
+          this.state.showErrorBanner &&
+          <div style={{ padding: '10px' }}>
+            <Alert message="download failed" type="error" />
           </div>
         }
         {
@@ -60,34 +98,7 @@ class GenerateButton extends React.PureComponent {
           this.state.modal &&
 
           <Modal title="The file contains PII information, click OK to proceed"
-            confirmButton={<Button onClick={async () => {
-              this.setState({
-                ...this.state,
-                modal: false,
-                isLoading: true
-              });
-
-              // window.open('./test.csv', '_parent', 'download');
-              var fileData = "VET_LAST_NAME,VET_FIRST_NAME,VET_MIDDLE_NAME,VET_DATE_OF_BIRTH,VET_SSN,VET_PARTICIPANT_ID,VET_FILE_NUMBER,APPELLANT_LAST_NAME,APPELLANT_FIRST_NAME,APPELLANT_MIDDLE_NAME,APPELLANT_DATE_OF_BIRTH,APPELLANT_SSN,APPELLANT_GENDER,APPELLANT_ADDRESS,APPELLANT_PHONE,APPELLANT_EMAIL,APPELLANT_EDI_PI,APPELLANT_CORP_PID,APPELLANT_VACOLS_INTERNAL_ID,RELATIONSHIP_TO_VETERAN";
-              var downloadLink = document.createElement("a");
-              var blob = new Blob(["\ufeff", fileData]);
-              var url = URL.createObjectURL(blob);
-              downloadLink.href = url;
-              downloadLink.download = "data.csv";
-
-              document.body.appendChild(downloadLink);
-              downloadLink.click();
-              document.body.removeChild(downloadLink);
-
-              // wait until download complete
-              await this.timeOut(5000);
-              // stop loading 
-              this.setState({
-                ...this.state,
-                isLoading: false,
-                showBanner: true
-              });
-            }}>Okay</Button>}
+            confirmButton={<Button onClick={ () => {this.onClickConfirmation()}}>Okay</Button>}
             closeHandler={() => { this.setState({ ...this.state, modal: false }); }}
           >
             Whenever you are click on Okay button then file will start downloading.
