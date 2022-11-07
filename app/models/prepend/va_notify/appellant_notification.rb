3c4a7990eb245a09a4399d88425f77c39fdaad97
j-n-t-l <611441@bah.com>
2022-11-07 16:32:17 -0500

    APPEALS-10312 added appeal search and fixed open privacy act task check

--- app/models/prepend/va_notify/appellant_notification.rb
+++ app/models/prepend/va_notify/appellant_notification.rb
@@ -65,6 +65,11 @@ module AppellantNotification
   # to keep track of the state of each appeal
   def self.appeal_mapper(appeal_id, appeal_type, event)
     current_user = RequestStore[:current_user] || User.system_user
+    if appeal_type == "Appeal"
+      appeal = Appeal.find_by(id: appeal_id)
+    else
+      appeal = LegacyAppeal.find_by(id: appeal_id)
+    end
     appeal_state = AppealState.find_by(appeal_id: appeal_id, appeal_type: appeal_type) ||
                    AppealState.create!(appeal_id: appeal_id, appeal_type: appeal_type, created_by_id: current_user.id)
     case event
@@ -92,14 +97,19 @@ module AppellantNotification
     when "vso_ihp_pending"
       appeal_state.update!(vso_ihp_pending: true, vso_ihp_complete: false, updated_by_id: current_user.id)
     when "vso_ihp_complete"
-      if !appeal.tasks.open.where(type.include?("Ihp")) &&
-         !appeal.tasks.open.where(type.include?("InformalHearingPresentationTask"))
+      # Only updates appeal state if ALL ihp tasks are completed
+      if appeal.tasks.open.where(type: IhpColocatedTask.name).empty? &&
+         appeal.tasks.open.where(type: InformalHearingPresentationTask.name).empty?
         appeal_state.update!(vso_ihp_complete: true, vso_ihp_pending: false, updated_by_id: current_user.id)
       end
     when "privacy_act_pending"
       appeal_state.update!(privacy_act_pending: true, privacy_act_complete: false, updated_by_id: current_user.id)
     when "privacy_act_complete"
-      if !appeal.tasks.open.where(type.include?("Foia")) && !appeal.tasks.open.where(type.include?("PrivacyAct"))
+      # Only updates appeal state if ALL privacy act tasks are completed
+      open_tasks = appeal.tasks.open
+      if open_tasks.where(type: FoiaColocatedTask.name).empty? && open_tasks.where(type: PrivacyActTask.name).empty? &&
+         open_tasks.where(type: HearingAdminActionFoiaPrivacyRequestTask.name).empty? &&
+         open_tasks.where(type: FoiaRequestMailTask.name).empty? && open_tasks.where(type: PrivacyMailRequestTask.name).empty?
         appeal_state.update!(privacy_act_complete: true, privacy_act_pending: false, updated_by_id: current_user.id)
       end
     end
