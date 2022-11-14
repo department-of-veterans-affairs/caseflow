8c3d1a6be41b8c3fae0a1bdddf63529cf51f49b9
HunJerBAH <Jeremy.Hunton@va.gov>
2022-11-14 16:31:06 -0500

    changed migration to use up/down to be safer on deploy.

--- db/migrate/20221114191336_change_split_correlation_tables.rb
+++ db/migrate/20221114191336_change_split_correlation_tables.rb
@@ -1,8 +1,15 @@
 class ChangeSplitCorrelationTables < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_table "split_correlation_tables" do |t|
       t.integer :split_request_issue_id, :original_request_issue_id, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
       safety_assured { t.remove :split_request_issue_ids, :original_request_issue_ids }
+    end
+  end
+  def down
+    change_table "split_correlation_tables" do |t|
+      t.remove :split_request_issue_id, :original_request_issue_id
+      t.integer "split_request_issue_ids", null: false, comment: "An array of the split request issue IDs that were transferred to the split appeal.", array: true
+      t.integer "original_request_issue_ids", null: false, comment: "An array of the original request issue IDs that were transferred to the split appeal.", array: true
     end
   end
 end
