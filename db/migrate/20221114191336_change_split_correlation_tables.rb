79d53bc5d2b4c557dccfff6a5087d6bdbdbe045e
HunJerBAH <Jeremy.Hunton@va.gov>
2022-11-15 10:55:20 -0500

    removed commented out code

--- db/migrate/20221114191336_change_split_correlation_tables.rb
+++ db/migrate/20221114191336_change_split_correlation_tables.rb
@@ -1,19 +1,8 @@
 class ChangeSplitCorrelationTables < ActiveRecord::Migration[5.2]
   def change
-    # change_table "split_correlation_tables" do |t|
-    #   t.integer :split_request_issue_id, :original_request_issue_id, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
-    # end
     add_column :split_correlation_tables, :split_request_issue_id, :integer, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
     add_column :split_correlation_tables, :original_request_issue_id, :integer, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
     safety_assured { remove_column :split_correlation_tables, :split_request_issue_ids, :integer }
     safety_assured { remove_column :split_correlation_tables, :original_request_issue_ids, :integer }
   end
-  # def down
-  #   change_table "split_correlation_tables" do |t|
-  #     t.integer "split_request_issue_ids", null: false, comment: "An array of the split request issue IDs that were transferred to the split appeal.", array: true
-  #     t.integer "original_request_issue_ids", null: false, comment: "An array of the original request issue IDs that were transferred to the split appeal.", array: true
-  #   end
-  #   safety_assured { remove_column :split_correlation_tables, :split_request_issue_id }
-  #   safety_assured { remove_column :split_correlation_tables, :original_request_issue_id }
-  # end
 end
