b1fe25e58fbf4e183cc00c1c52e7d5e8436cf522
HunJerBAH <Jeremy.Hunton@va.gov>
2022-11-15 09:39:58 -0500

    refactored migration to have saftey_assured outside of change_table block

--- db/migrate/20221114191336_change_split_correlation_tables.rb
+++ db/migrate/20221114191336_change_split_correlation_tables.rb
@@ -2,14 +2,16 @@ class ChangeSplitCorrelationTables < ActiveRecord::Migration[5.2]
   def up
     change_table "split_correlation_tables" do |t|
       t.integer :split_request_issue_id, :original_request_issue_id, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
-      safety_assured { t.remove :split_request_issue_ids, :original_request_issue_ids }
     end
+    safety_assured { remove_column :split_correlation_tables, :split_request_issue_ids }
+    safety_assured { remove_column :split_correlation_tables, :original_request_issue_ids }
   end
   def down
     change_table "split_correlation_tables" do |t|
-      safety_assured { t.remove :split_request_issue_id, :original_request_issue_id }
       t.integer "split_request_issue_ids", null: false, comment: "An array of the split request issue IDs that were transferred to the split appeal.", array: true
       t.integer "original_request_issue_ids", null: false, comment: "An array of the original request issue IDs that were transferred to the split appeal.", array: true
     end
+    safety_assured { remove_column :split_correlation_tables, :split_request_issue_id }
+    safety_assured { remove_column :split_correlation_tables, :original_request_issue_id }
   end
 end
