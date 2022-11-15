b64384d4e9d9c3933166675106ce3d88448c5867
HunJerBAH <Jeremy.Hunton@va.gov>
2022-11-15 10:33:12 -0500

    added type to remove_column clause in migration

--- db/migrate/20221114191336_change_split_correlation_tables.rb
+++ db/migrate/20221114191336_change_split_correlation_tables.rb
@@ -5,8 +5,8 @@ class ChangeSplitCorrelationTables < ActiveRecord::Migration[5.2]
     # end
     add_column :split_correlation_tables, :split_request_issue_id, :integer, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
     add_column :split_correlation_tables, :original_request_issue_id, :integer, null: false, comment: "The original request issue id and the corresponding request issue id created from the split appeal process."
-    safety_assured { remove_column :split_correlation_tables, :split_request_issue_ids }
-    safety_assured { remove_column :split_correlation_tables, :original_request_issue_ids }
+    safety_assured { remove_column :split_correlation_tables, :split_request_issue_ids, :integer }
+    safety_assured { remove_column :split_correlation_tables, :original_request_issue_ids, :integer }
   end
   # def down
   #   change_table "split_correlation_tables" do |t|
