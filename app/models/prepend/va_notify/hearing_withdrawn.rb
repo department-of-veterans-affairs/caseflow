eb68b966354cb9ffdb4bb4360d6148f876335505
Marc Steele <Marc.Steele@va.gov>
2022-11-22 09:11:23 -0500

    APPEALS-10316 Changed prepended files to query model directly instead of using relationships which caused infinite loop

--- app/models/prepend/va_notify/hearing_withdrawn.rb
+++ app/models/prepend/va_notify/hearing_withdrawn.rb
@@ -36,10 +36,19 @@ module HearingWithdrawn
   #
   # Response: none
   def update_appeal_states_on_hearing_withdrawn
-    if disposition == Constants.HEARING_DISPOSITION_TYPES.cancelled
-      MetricsService.record("Updating HEARING_WITHDRAWN column in Appeal States Table to FALSE for #{appeal.class.to_s} ID #{appeal.id}".yellow,
-                            name: "AppellantNotification.appeal_mapper") do
-        AppellantNotification.appeal_mapper(appeal.id, appeal.class.to_s, "hearing_withdrawn")
+    if is_a?(LegacyHearing)
+      if VACOLS::CaseHearing.find_by(hearing_pkseq: vacols_id).hearing_disp == "C"
+        MetricsService.record("Updating HEARING_WITHDRAWN in Appeal States Table for #{appeal.class.to_s} ID #{appeal.id}".yellow,
+                              name: "AppellantNotification.appeal_mapper") do
+          AppellantNotification.appeal_mapper(appeal.id, appeal.class.to_s, "hearing_withdrawn")
+        end
+      end
+    elsif is_a?(Hearing)
+      if disposition == Constants.HEARING_DISPOSITION_TYPES.cancelled
+        MetricsService.record("Updating HEARING_WITHDRAWN in Appeal States Table for #{appeal.class.to_s} ID #{appeal.id}".yellow,
+                              name: "AppellantNotification.appeal_mapper") do
+          AppellantNotification.appeal_mapper(appeal.id, appeal.class.to_s, "hearing_withdrawn")
+        end
       end
     end
   end
