ab1067fdf9e27330a7447530afdf43776c1cfed0
j-n-t-l <611441@bah.com>
2022-11-22 19:49:46 -0500

    APPEALS-10307 updated quarterlynotificationjob

--- app/jobs/quarterly_notifications_job.rb
+++ app/jobs/quarterly_notifications_job.rb
@@ -16,6 +16,12 @@ class QuarterlyNotificationsJob < CaseflowJob
         appeal_state = AppealState.find(appeal: appeal)
         hearing_rescheduled(appeal_state, appeal)
         appeal_docketed(appeal_state, appeal)
+        hearing_scheduled(appeal_state, appeal)
+        privacy_act_pending(appeal_state, appeal)
+        vso_ihp_pending(appeal_state, appeal)
+        hearing_scheduled_privacy_pending(appeal_state, appeal)
+        hearing_rescheduled_privacy_pending(appeal_state, appeal)
+        vso_ihp_privacy_pending(appeal_state, appeal)
       end
     end
   end
@@ -54,7 +60,7 @@ class QuarterlyNotificationsJob < CaseflowJob
       AppellantNotification.notify_appellant(appeal, "Hearing to be Rescheduled")
     end
   end
-
+  # FIXME need to check tasks to find difference between scheduled hearing & rescheduled hearing
   def appeal_docketed(appeal_state, appeal)
     if !appeal_state.hearing_scheduled && !appeal_state.vso_ihp_pending &&
        !appeal_state.privacy_act_pending && !appeal_state.scheduled_in_error &&
@@ -67,6 +73,7 @@ class QuarterlyNotificationsJob < CaseflowJob
     end
   end
 
+  # FIXME need to check tasks to find difference between scheduled hearing & rescheduled hearing
   def hearing_scheduled(appeal_state, appeal)
     if appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
        !appeal_state.privacy_act_pending
@@ -81,6 +88,51 @@ class QuarterlyNotificationsJob < CaseflowJob
     if !appeal_state.hearing_scheduled && !appeal_state.vso_ihp_pending &&
        appeal_state.privacy_act_pending
       AppellantNotification.notify_appellant(appeal, "Privacy Act Pending")
+    end
+  end
+
+  def vso_ihp_pending(appeal_state, appeal)
+    if !appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+       !appeal_state.privacy_act_pending
+      AppellantNotification.notify_appellant(appeal, "VSO IHP Pending")
+    end
+  end
+
+  def hearing_scheduled_privacy_pending(appeal_state, appeal)
+    if appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+       appeal_state.privacy_act_pending
+      AppellantNotification.notify_appellant(appeal, "Hearing Scheduled /  Privacy Act Pending")
+    elsif appeal_state.hearing_scheduled && !appeal_state.vso_ihp_pending &&
+          appeal_state.privacy_act_pending
+      AppellantNotification.notify_appellant(appeal, "Hearing Scheduled /  Privacy Act Pending")
+    end
+  end
+
+  def vso_ihp_privacy_pending(appeal_state, appeal)
+    if !appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+       appeal_state.privacy_act_pending
+      AppellantNotification.notify_appellant(appeal, "VSO IHP Pending / Privacy Act Pending")
+    elsif appeal_state.hearing_scheduled && !appeal_state.vso_ihp_pending &&
+          appeal_state.privacy_act_pending
+      AppellantNotification.notify_appellant(appeal, "Hearing Scheduled /  Privacy Act Pending")
+    end
+  end
+
+  def hearing_rescheduled_privacy_pending(appeal_state, appeal)
+    if !appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+       appeal_state.privacy_act_pending && appeal_state.scheduled_in_error
+      AppellantNotification.notify_appellant(appeal, "Hearing to be Rescheduled / Privacy Act Pending")
+    elsif !appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+          appeal_state.privacy_act_pending && appeal_state.scheduled_in_error
+      AppellantNotification.notify_appellant(appeal, "Hearing to be Rescheduled / Privacy Act Pending")
+    elsif !appeal_state.hearing_scheduled && appeal_state.vso_ihp_pending &&
+          appeal_state.privacy_act_pending && !appeal_state.scheduled_in_error &&
+          appeal_state.hearing_postponed
+      AppellantNotification.notify_appellant(appeal, "Hearing to be Rescheduled / Privacy Act Pending")
+    elsif !appeal_state.hearing_scheduled && !appeal_state.vso_ihp_pending &&
+          appeal_state.privacy_act_pending && !appeal_state.scheduled_in_error &&
+          appeal_state.hearing_postponed
+      AppellantNotification.notify_appellant(appeal, "Hearing to be Rescheduled / Privacy Act Pending")
     end
   end
 end
