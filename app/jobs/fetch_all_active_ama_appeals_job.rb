802b3ca9d0aa8137d51a101683e19bd4c44c4740
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-11-28 13:58:49 -0500

    APPEALS-8945 Added safety navigation operator

--- app/jobs/fetch_all_active_ama_appeals_job.rb
+++ app/jobs/fetch_all_active_ama_appeals_job.rb
@@ -60,7 +60,7 @@ class FetchAllActiveAmaAppealsJob < CaseflowJob
       all_appeal_states = appeal_states.inject(&:merge)
       AppealState.create(all_appeal_states)
     rescue StandardError => error
-      Rails.logger.error("#{appeal.class} ID #{appeal.id} was unable to create an appeal_states record because of "\
+      Rails.logger.error("#{appeal&.class} ID #{appeal&.id} was unable to create an appeal_states record because of "\
          "#{error}".red)
     end
   end
