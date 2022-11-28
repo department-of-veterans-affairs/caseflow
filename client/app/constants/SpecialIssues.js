09097130f83e43f9c3c51f411cf7caa5d94d9923
kristeja <112115264+kristeja@users.noreply.github.com>
2022-11-28 10:04:23 -0800

    APPEALS-8930 Add a "PACT Act" checkbox option to the "Select Special Issue" screen within Caseflow (#17715)
    * initial commit
    
    * APPEALS-10642 - Cleaned up commits (#17725)
    
    * APPEALS-10645: added "pact_act" param to controller (#17719)
    
    * APPEALS-10646 Added "PACT Act" checkbox option to the Legacy & AMA Special Issues
    
    * APPEALS-10647 - Commit Cleanup CSS Fix (#17739)
    
    Co-authored-by: Ki Mau <ki.mau@va.gov>
    Co-authored-by: Isaiah Saucedo <isaiah.saucedo@va.gov>

--- client/app/constants/SpecialIssues.js
+++ client/app/constants/SpecialIssues.js
@@ -230,7 +230,7 @@ export const SPECIAL_ISSUES = [
     snakeCase: 'radiation',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 8
+    queueSectionOrder: 9
   },
   {
     display: 'Rice Compliance',
@@ -240,7 +240,7 @@ export const SPECIAL_ISSUES = [
     snakeCase: 'rice_compliance',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 9
+    queueSectionOrder: 10
   },
   {
     display: 'Spina Bifida',
@@ -250,7 +250,7 @@ export const SPECIAL_ISSUES = [
     snakeCase: 'spina_bifida',
     unhandled: null,
     queueSection: 'issuesOnAppeal',
-    queueSectionOrder: 10
+    queueSectionOrder: 11
   },
   {
     display: 'U.S. Territory claim - American Samoa, Guam, Northern ' +
@@ -364,6 +364,16 @@ export const AMA_SPECIAL_ISSUES = [
     unhandled: null,
     queueSection: 'issuesOnAppeal',
     queueSectionOrder: 1,
+    isAmaRelevant: true
+  },
+  {
+    display: 'PACT Act',
+    queueDisplay: 'PACT Act',
+    specialIssue: 'pactAct',
+    snakeCase: 'pact_act',
+    unhandled: null,
+    queueSection: 'issuesOnAppeal',
+    queueSectionOrder: 8,
     isAmaRelevant: true
   },
   {
