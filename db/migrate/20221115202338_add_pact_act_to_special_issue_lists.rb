35f9371fdaa44e620ec9eb1007b958bd84749c21
kristeja <krishnateja.golla@va.gov>
2022-11-29 10:06:40 -0800

    updated migration file

--- db/migrate/20221115202338_add_pact_act_to_special_issue_lists.rb
+++ db/migrate/20221115202338_add_pact_act_to_special_issue_lists.rb
@@ -1,9 +1,5 @@
 class AddPactActToSpecialIssueLists < Caseflow::Migration
-  def up
+  def change
     add_column :special_issue_lists, :pact_act, :boolean, default: false, comment: "The Sergeant First Class (SFC) Heath Robinson Honoring our Promise to Address Comprehensive Toxics (PACT) Act"
-  end
-
-  def down
-    remove_column :special_issue_lists, :pact_act
   end
 end
