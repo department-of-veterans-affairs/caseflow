0c3cb431178688a96ae00ede644b3fcfb77a7dd1
Craig Reese <Reese_Craig@bah.com>
2022-11-30 15:53:00 -0500

    adding tests

--- spec/seeds/static_test_data_spec.rb
+++ spec/seeds/static_test_data_spec.rb
@@ -19,27 +19,43 @@ describe Seeds::StaticTestCaseData do
   end
 
   context "#seed!" do
-    context "for APPEALS-8386, ASR-272 Calculation Functionality for Timely/Untimely Checkbox on DAS" do
-      # required because the judge team for user BVAGSPORER is used specifically in the described_class
-      # and judge team attorneys need to be in VACOLS for creating the attorney tasks
-      before do
-        { "BVAGSPORER" => { attorneys: %w[BVAOTRANTOW BVAGBOTSFORD BVAJWEHNER1] } }.each_pair do |judge_css_id, h|
-          judge = User.find_or_create_by(css_id: judge_css_id, station_id: 101)
-          create(:staff, :judge_role, sdomainid: judge_css_id)
-          judge_team = JudgeTeam.for_judge(judge) || JudgeTeam.create_for_judge(judge)
-          h[:attorneys].each do |css_id|
-            judge_team.add_user(User.find_or_create_by(css_id: css_id, station_id: 101))
-            create(:staff, :attorney_role, sdomainid: css_id)
-          end
+    # required because the judge team for user BVAGSPORER is used specifically in the described_class
+    # and judge team attorneys need to be in VACOLS for creating the attorney tasks
+    before do
+      { "BVAGSPORER" => { attorneys: %w[BVAOTRANTOW BVAGBOTSFORD BVAJWEHNER1] } }.each_pair do |judge_css_id, h|
+        judge = User.find_or_create_by(css_id: judge_css_id, station_id: 101)
+        create(:staff, :judge_role, sdomainid: judge_css_id)
+        judge_team = JudgeTeam.for_judge(judge) || JudgeTeam.create_for_judge(judge)
+        h[:attorneys].each do |css_id|
+          judge_team.add_user(User.find_or_create_by(css_id: css_id, station_id: 101))
+          create(:staff, :attorney_role, sdomainid: css_id)
         end
       end
 
+      seed.seed!
+    end
+
+    context "for APPEALS-8386, ASR-272 Calculation Functionality for Timely/Untimely Checkbox on DAS" do
       it "creates appeals and veterans, and increments file_number and participant_id" do
-        seed.seed!
         expect(Appeal.count).to be >= 12
         expect(Veteran.count).to be >= 12
         expect(seed.instance_variable_get(:@file_number)).to be >= 400_000_012
         expect(seed.instance_variable_get(:@participant_id)).to be >= 800_000_012
+      end
+    end
+
+    context "for APPEALS-8729, Unsolicited Veteran Updates from MPI" do
+      it "creates the veterans and adds them to the redis store" do
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:867895432")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:678849874")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:784456431")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:673489455")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:748997154")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:448167748")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:334568484")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:349628761")).to be true
+        expect(Fakes::VeteranStore.all_keys.include?("veterans_#{Rails.env}:764889132")).to be true
+        fail('manual')
       end
     end
   end
