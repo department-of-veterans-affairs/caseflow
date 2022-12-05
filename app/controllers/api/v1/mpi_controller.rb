f11de0a722e9eeacc3a9340fdcf22b633933b568
Matt Roth <mattcroth@gmail.com>
2022-12-05 17:18:22 -0500

    Update mpi_controller.rb

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -3,6 +3,7 @@
 class Api::V1::MpiController < Api::ApplicationController
   # {POST Method for Veteran ID, Deceased Indicator, Deceased Time}
   def veteran_updates
+    Rails.logger.info("Queue ART says start. veterans_id: #{allowed_params[:veterans_id]} , deceased_time: #{allowed_params[:deceased_time]}")
     veteran = {
       id: allowed_params[:veterans_id],
       deceased_time: allowed_params[:deceased_time]
