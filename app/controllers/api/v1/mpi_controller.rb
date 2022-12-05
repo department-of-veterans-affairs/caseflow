7c2edc4dae7c6c0591864ca2b77f46112980114b
Matt Roth <mattcroth@gmail.com>
2022-12-05 11:04:13 -0500

    Add deceased_time (#17775)
    * Update mpi_controller.rb
    
    * Update mpi_controller.rb

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::MpiController < Api::ApplicationController
       id: allowed_params[:veterans_id],
       deceased_time: allowed_params[:deceased_time]
     }
-    response_info_column = { veteran_id: veteran[:id] }
+    response_info_column = { veteran_id: veteran[:id], deceased_time: veteran[:deceased_time] }
     mpi_update = MpiUpdatePersonEvent.create!(api_key: api_key, created_at: Time.zone.now, update_type: :started)
     result = VACOLS::Correspondent.update_veteran_nod(veteran).to_sym
     if result == :successful || result == :already_deceased_time_changed
@@ -18,9 +18,7 @@ class Api::V1::MpiController < Api::ApplicationController
     mpi_update.update!(update_type: result, completed_at: Time.zone.now, info: response_info_column)
     render json: { result: result }, status: :ok
   rescue StandardError => error
-    if !Rails.deploy_env?(:prod) && !Rails.deploy_env?(:preprod)
-      response_info_column[:error] = error
-    end
+    response_info_column[:error] = error
     mpi_update.update!(update_type: :error, completed_at: Time.zone.now, info: response_info_column)
     raise error
   end
