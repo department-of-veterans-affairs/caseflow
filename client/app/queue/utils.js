161bdc61f61b5333247f32b3aacf2ae56993e4d0
Matt Roth <roth_matthew@bah.com>
2022-12-06 07:34:09 -0500

    add location history to serializer

--- client/app/queue/utils.js
+++ client/app/queue/utils.js
@@ -469,6 +469,7 @@ export const prepareAppealForStore = (appeals) => {
       remandSourceAppealId: appeal.attributes.remand_source_appeal_id,
       remandJudgeName: appeal.attributes.remand_judge_name,
       hasNotifications: appeal.attributes.has_notifications,
+      locationHistory: appeal.attributes.location_history,
     };
 
     return accumulator;
