9f3986b291c818d14acc326103e66ab6ce0f536e
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-12-08 10:34:17 -0500

    APPEALS-12007 Added attribute reader to va_notify_send_message_template.rb

--- app/models/vanotify/va_notify_send_message_template.rb
+++ app/models/vanotify/va_notify_send_message_template.rb
@@ -5,7 +5,7 @@ class VANotifySendMessageTemplate
   # Constructs Object with attributes that are sent to VA Notify
   # Appeal ID is UUID for AMA Appeals and Vacols ID for Legacy Appeals
   # message_attributes is a hash containing strings of participant_id, appeal_id, appeal_type, & status
-  attr_reader :participant_id, :template_name, :appeal_id, :appeal_type, :status
+  attr_reader :participant_id, :template_name, :appeal_id, :appeal_type, :status, :quarterly_status
   def initialize(message_attributes, template_name, quarterly_status = nil)
     @participant_id = message_attributes[:participant_id]
     @template_name = template_name
