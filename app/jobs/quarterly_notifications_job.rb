2978bf77e5069437b66abce3cf609b47bd12f55f
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-12-13 11:12:44 -0500

    APPEALS-12007-v2 Added Constants for Quarterly Statuses and Added 3rd arguement in quarterly notifications job to include quarterly status

--- app/jobs/quarterly_notifications_job.rb
+++ app/jobs/quarterly_notifications_job.rb
@@ -47,44 +47,80 @@ class QuarterlyNotificationsJob < CaseflowJob
     if appeal_state.hearing_postponed || appeal_state.scheduled_in_error
       # appeal status is Hearing to be Rescheduled / Privacy Act Pending
       if appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.hearing_to_be_rescheduled_privacy_pending
+        )
       # appeal status is Hearing to be Rescheduled
       else
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.hearing_to_be_rescheduled
+        )
       end
     # if there's a hearing scheduled
     elsif appeal_state.hearing_scheduled
       # if there's privacy act tasks pending
       # appeal status is Hearing Scheduled /  Privacy Act Pending
       if appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.hearing_scheduled_privacy_pending
+        )
       # if there's no privacy act tasks pending
       # appeal status is Hearing Scheduled
       elsif !appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.hearing_scheduled
+        )
       end
     # if there's no hearing scheduled and no hearing withdrawn
     elsif !appeal_state.hearing_withdrawn
       # if there's ihp tasks pending and privacy act tasks pending
       # appeal status is VSO IHP Pending / Privacy Act Pending
       if appeal_state.vso_ihp_pending && appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.ihp_pending_privacy_pending
+        )
       # if there's no ihp tasks pending and there are privacy act tasks pending
       # appeal status is Privacy Act Pending
       elsif !appeal_state.vso_ihp_pending && appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.privacy_pending
+        )
       # if there's no privacy acts pending and there are ihp tasks pending
       # appeal status is VSO IHP Pending
       elsif appeal_state.vso_ihp_pending && !appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.ihp_pending
+        )
       # if there's no privacy acts pending or ihp tasks pending
       # appeal status is Appeal Docketed
-      elsif !appeal_state.vso_ihp_pending && !appeal_state.privacy_act_pending
-        AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+      elsif !appeal_state.vso_ihp_pending && !appeal_state.privacy_act_pending && appeal_state.appeal_docketed
+        AppellantNotification.notify_appellant(
+          appeal,
+          "Quarterly Notification",
+          Constants.QUARTERLY_STATUSES.appeal_docketed
+        )
       end
     # appeal status is Appeal Docketed
-    else
-      AppellantNotification.notify_appellant(appeal, "Quarterly Notification")
+    elsif appeal_state.appeal_docketed && appeal_state.hearing_withdrawn
+      AppellantNotification.notify_appellant(
+        appeal,
+        "Quarterly Notification",
+        Constants.QUARTERLY_STATUSES.appeal_docketed
+      )
     end
   end
 end
