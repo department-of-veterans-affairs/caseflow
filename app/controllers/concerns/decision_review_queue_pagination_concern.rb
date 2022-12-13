d068c59ba37e1d3817b594270c1bfd050c35bdec
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-12-12 22:19:41 -0500

    Extract pagination for DR controller to DecisionReviewQueuePaginationConcern

---
+++ app/controllers/concerns/decision_review_queue_pagination_concern.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Provides methods to the DecisionReviewsController so that it can paginate
+# tasks for business lines' decision review queues.
+#
+# Requires a method named 'business_line' to be implemented that returns the current
+# BusinessLine as an object.
+module DecisionReviewQueuePaginationConcern
+  extend ActiveSupport::Concern
+
+  DEFAULT_TASKS_PER_PAGE = 15
+
+  def pagination_json(task_list)
+    task_count = task_list.size
+    total_pages = (task_count / DEFAULT_TASKS_PER_PAGE.to_f).ceil
+
+    {
+      tasks: {
+        data: apply_task_serializer(paginate_tasks(task_list))
+      },
+      tasks_per_page: DEFAULT_TASKS_PER_PAGE,
+      task_page_count: total_pages,
+      total_task_count: task_count
+    }
+  end
+
+  def paginate_tasks(task_list)
+    tasks = task_list.is_a?(Array) ? Kaminari.paginate_array(task_list) : task_list
+
+    tasks.page(allowed_params[:page] || 1).per(DEFAULT_TASKS_PER_PAGE)
+  end
+
+  def apply_task_serializer(tasks)
+    tasks.map { |task| task.ui_hash.merge(business_line: business_line_slug) }
+  end
+end
