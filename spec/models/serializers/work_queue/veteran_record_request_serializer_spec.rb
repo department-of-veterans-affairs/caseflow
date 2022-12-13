94cc862984f798c02fdcece3b16147d0d41868de
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-12-12 19:12:46 -0500

    Fix VeteranRecordRequestSerializer spec

--- spec/models/serializers/work_queue/veteran_record_request_serializer_spec.rb
+++ spec/models/serializers/work_queue/veteran_record_request_serializer_spec.rb
@@ -18,11 +18,13 @@ describe WorkQueue::VeteranRecordRequestSerializer, :postgres do
           appeal: { id: appeal.uuid.to_s, isLegacyAppeal: false, issueCount: 0 },
           veteran_participant_id: veteran.participant_id,
           assigned_on: task.assigned_at,
+          assigned_at: task.assigned_at,
           closed_at: task.closed_at,
           started_at: task.started_at,
           tasks_url: "/decision_reviews/nco",
           id: task.id,
           created_at: task.created_at,
+          issue_count: 0,
           type: "Record Request"
         }
       }
