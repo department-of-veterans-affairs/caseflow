2a308b556eac564b1567a26cc5b7a3940ab29b5a
Sean Craig <Craig_Sean@bah.com>
2022-12-14 15:17:34 -0600

    small fix

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::MpiController < Api::ApplicationController
       deceased_time: allowed_params[:deceased_time]
     }
 
-    response_info_column = { veteran_ssn: veteran[:veterans_ssn], veteran_pat: veteran[veterans_pat], deceased_time: veteran[:deceased_time] }
+    response_info_column = { veteran_ssn: veteran[:veterans_ssn], veteran_pat: veteran[:veterans_pat], deceased_time: veteran[:deceased_time] }
 
     mpi_update = MpiUpdatePersonEvent.create!(api_key: api_key, created_at: Time.zone.now, update_type: :started)
     result = VACOLS::Correspondent.update_veteran_nod(veteran).to_sym
