049768a8116a5e0ad2524f83534bfa6fa1551bd7
Sean Craig <110493538+seancva@users.noreply.github.com>
2022-12-14 15:31:42 -0600

    SeanC/APPEALS-12586 (#17829)
    * added if for new status being returned
    
    * fixed linting issue
    
    * updated controller to allow for pat as a param
    
    * changed variable names
    
    * changed varible names
    
    * changes to correspondent
    
    * added PAT to logs
    
    * formatted PAT correctly
    
    * updated controller to search by pat or ssn
    
    * updated how the veteran is searched for
    
    * added veterans_pat to info column
    
    * small fix
    
    * fixed typo
    
    Co-authored-by: Sean Craig <Craig_Sean@bah.com>
    Co-authored-by: Craig Reese <Reese_Craig@bah.com>

--- app/controllers/api/v1/mpi_controller.rb
+++ app/controllers/api/v1/mpi_controller.rb
@@ -3,16 +3,19 @@
 class Api::V1::MpiController < Api::ApplicationController
   # {POST Method for Veteran ID, Deceased Indicator, Deceased Time}
   def veteran_updates
-    Rails.logger.info("Queue ART says start. veterans_id: #{allowed_params[:veterans_id]} , deceased_time: #{allowed_params[:deceased_time]}")
+    Rails.logger.info("Queue ART says start. veterans_id: #{allowed_params[:veterans_ssn]} ,veterans_pat: #{allowed_params[:veterans_pat]} deceased_time: #{allowed_params[:deceased_time]}")
     veteran = {
-      id: allowed_params[:veterans_id],
+      veterans_ssn: allowed_params[:veterans_ssn],
+      veterans_pat: allowed_params[:veterans_pat].split("^")[0],
       deceased_time: allowed_params[:deceased_time]
     }
-    response_info_column = { veteran_id: veteran[:id], deceased_time: veteran[:deceased_time] }
+
+    response_info_column = { veteran_ssn: veteran[:veterans_ssn], veteran_pat: veteran[:veterans_pat], deceased_time: veteran[:deceased_time] }
+
     mpi_update = MpiUpdatePersonEvent.create!(api_key: api_key, created_at: Time.zone.now, update_type: :started)
     result = VACOLS::Correspondent.update_veteran_nod(veteran).to_sym
     if result == :successful || result == :already_deceased_time_changed
-      updated_veteran = VACOLS::Correspondent.find_by(ssn:veteran[:id])
+      updated_veteran = VACOLS::Correspondent.find_by(stafkey: veteran[:veterans_pat]) || VACOLS::Correspondent.find_by(ssn: veteran[:veterans_ssn])
       response_info_column[:updated_column] = "deceased_time"
       response_info_column[:updated_deceased_time] = updated_veteran.sfnod
     end
@@ -30,6 +33,6 @@ class Api::V1::MpiController < Api::ApplicationController
   end
 
   def allowed_params
-    params.permit(:veterans_id, :deceased_time)
+    params.permit(:veterans_ssn, :veterans_pat, :deceased_time)
   end
 end
