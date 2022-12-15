a532fbfa1e8d26cdd4aca2a22cdd22f240163102
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-12-15 14:47:37 -0500

    APPEALS-12611 Removed column name update migration

--- db/migrate/20221215193736_update_notification_content_column_name_to_email_notification_content_in_notifications.rb
+++
@@ -1,9 +0,0 @@
-class UpdateNotificationContentColumnNameToEmailNotificationContentInNotifications < Caseflow::Migration
-  def up
-    rename_column :notifications, :notification_content, :email_notification_content, comment: "Full Email Text Content of Notification"
-  end
-
-  def down
-    rename_column :notifications, :email_notification_content, :notification_content, comment: "Full Text Content of Notification"
-  end
-end
