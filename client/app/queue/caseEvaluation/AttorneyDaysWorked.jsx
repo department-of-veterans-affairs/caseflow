1e4b974da80e84bed1fdb3dc43b4cf51e628f325
jshields13 <jeremy.shields@va.gov>
2022-12-16 09:26:40 -0500

    Fix bad merge

--- client/app/queue/caseEvaluation/AttorneyDaysWorked.jsx
+++ client/app/queue/caseEvaluation/AttorneyDaysWorked.jsx
@@ -16,39 +16,13 @@ export const AttorneyDaysWorked = (props) => {
   const allChildrenTasks = [];
   const justAttorneyTasks = [];
 
-const attorneyAssignedStyling = css({ width: '30%' });
-
-const isSameRange = (rangeA, rangeB) => {
-  return rangeA.start.isSame(rangeB.start, 'day') && rangeA.end.isSame(rangeB.end, 'day');
-};
-
-const calculateDaysWorked = (tasks, daysAssigned) => {
-  let sumOfDays = 0;
-
-  // Map all tasks' createdAt to closedAt to a moment.range object
-  const dateRanges = tasks.map((task) => moment.range(moment(task.createdAt), moment(task.closedAt)));
-
-  const uniqueDateRanges = [];
-  const alreadyAccountedForDateRanges = [];
-
-  // For all tasks' date ranges find overlapping date ranges
-  dateRanges.forEach((dateRange) => {
-
-    // If a date range was already found to be overlapping do not create another combined overlapping date range
-    if (alreadyAccountedForDateRanges.findIndex((range) => isSameRange(dateRange, range)) < 0) {
-
-      // Find all overlapping date ranges
-      const overlappingDateRanges = dateRanges.filter((range) => dateRange.overlaps(range, { adjacent: true }));
-
-      // Track used date ranges so they are not used again
-      alreadyAccountedForDateRanges.push(...overlappingDateRanges);
-
-      // Add all overlapping date ranges together
-      let overlappingDateRange = dateRange.clone();
-
-      overlappingDateRanges.forEach((range) => {
-        overlappingDateRange = overlappingDateRange.add(range, { adjacent: true });
-      });
+  if (isLegacy) {
+    allChildrenTasks.push(...attorneyTasks);
+  } else {
+    attorneyTasks.forEach((attorneyTaskTree) => {
+      justAttorneyTasks.push(attorneyTaskTree.attorneyTask);
+      allChildrenTasks.push(...attorneyTaskTree.childrenTasks);
+    });
   }
 
   const daysWorkedUpdated = calculateDaysWorked(allChildrenTasks, daysAssigned, justAttorneyTasks);
