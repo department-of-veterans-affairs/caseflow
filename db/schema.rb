9ed550bcab18c032b11096706b4cb916adf6ba1e
jshields13 <jeremy.shields@va.gov>
2022-12-16 16:47:10 -0500

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_191336) do
+ActiveRecord::Schema.define(version: 2022_12_15_202259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
