f44ee742eb3c9074edd1b9efad99f9ec676e29d5
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-12-18 22:04:35 -0500

    Add same trait from HLRs to SCs

--- spec/factories/supplemental_claim.rb
+++ spec/factories/supplemental_claim.rb
@@ -21,6 +21,22 @@ FactoryBot.define do
       end
     end
 
+    trait :with_vha_issue do
+      after(:create) do |supplemental_claim, evaluator|
+        create(:request_issue,
+               benefit_type: "vha",
+               nonrating_issue_category: "Beneficiary Travel | Special Mode",
+               nonrating_issue_description: "VHA issue description ",
+               decision_review: supplemental_claim,
+               decision_date: 1.month.ago)
+
+        if evaluator.veteran
+          supplemental_claim.veteran_file_number = evaluator.veteran.file_number
+          supplemental_claim.save
+        end
+      end
+    end
+
     trait :processed do
       establishment_processed_at { Time.zone.now }
     end
