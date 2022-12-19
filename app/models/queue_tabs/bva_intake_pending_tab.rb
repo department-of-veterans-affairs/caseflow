564346d7de0ea7889b1054d8ebfef7c73c3c2cef
= <tyler.broyles@va.gov>
2022-12-19 11:31:23 -0500

    Added a for enabling a default sorting column and default sorting direction to a queue_tab/queue_config. Updated the receipt date queue_config sorting configuration to work as a default through task_sorter. Removed the specific sort for the receipt_date column from task_sorter. Added a date field exemption for UPPER in task_sorter. Added a joins(:ama_appeal) to the bva_intake tabs tasks queries so it can automatically sort on that field without needed a special query/exemption in task_sorter.

--- app/models/queue_tabs/bva_intake_pending_tab.rb
+++ app/models/queue_tabs/bva_intake_pending_tab.rb
@@ -18,10 +18,19 @@ class BvaIntakePendingTab < QueueTab
   end
 
   def tasks
-    on_hold_task_children.open
+    # Original
+    # on_hold_task_children.open
+    # With includes added again since it gets eaten by on_hold_task_children
+    # on_hold_task_children.includes(*task_includes).open
+    # with join for sorting without nasty case statement
+    on_hold_task_children.open.includes(*task_includes, :cancelled_by).joins(:ama_appeal)
   end
 
   def column_names
     BvaIntake::COLUMN_NAMES
+  end
+
+  def default_sorting_column
+    QueueColumn.from_name(Constants.QUEUE_CONFIG.COLUMNS.RECEIPT_DATE_INTAKE.name)
   end
 end
