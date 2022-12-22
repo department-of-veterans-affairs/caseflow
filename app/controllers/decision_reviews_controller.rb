8d52662bb0f0183944882b6e5b9900164548016c
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2022-12-22 12:17:00 -0500

    Serialize tasks upon task completion

--- app/controllers/decision_reviews_controller.rb
+++ app/controllers/decision_reviews_controller.rb
@@ -34,7 +34,10 @@ class DecisionReviewsController < ApplicationController
     if task
       if task.complete_with_payload!(decision_issue_params, decision_date)
         business_line.tasks.reload
-        render json: { in_progress_tasks: in_progress_tasks, completed_tasks: completed_tasks }, status: :ok
+        render json: {
+          in_progress_tasks: apply_task_serializer(in_progress_tasks),
+          completed_tasks: apply_task_serializer(completed_tasks)
+        }, status: :ok
       else
         error = StandardError.new(task.error_code)
         Raven.capture_exception(error, extra: { error_uuid: error_uuid })
