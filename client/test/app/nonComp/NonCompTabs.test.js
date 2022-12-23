e762471ea58565f4fe00f949db7417304fe4782f
Lianna Newman <newman_lianna@bah.com>
2022-12-23 08:33:55 -0500

    Added updates to tests which are passing on the 12481-and12483-combined branch. Note tests will need to be run with backend attached.

--- client/test/app/nonComp/NonCompTabs.test.js
+++ client/test/app/nonComp/NonCompTabs.test.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import '@testing-library/jest-dom';
 import NonCompTabsUnconnected from 'app/nonComp/components/NonCompTabs';
-import { TaskTableUnconnected } from '../../../app/queue/components/TaskTable';
+
+const basicProps = {
+  businessLine: 'Veterans Health Administration',
+  businessLineUrl: 'vha',
+  baseTasksUrl: '/decision_reviews/vha',
+  selectedTask: null,
+  decisionIssuesStatus: { },
+};
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
-const renderNonCompTabs = (props) => {
+const createReducer = (storeValues) => {
+  return function (state = storeValues) {
+
+    return state;
+  };
+};
+
+const renderNonCompTabs = () => {
+
+  const nonCompTabsReducer = createReducer(basicProps);
+
+  const store = createStore(nonCompTabsReducer);
+
   return render(
-    <NonCompTabsUnconnected {...props} />
+    <Provider store={store}>
+      <NonCompTabsUnconnected />
+    </Provider>
   );
 };
 
@@ -24,45 +47,17 @@ afterEach(() => {
 
 describe('NonCompTabs', () => {
   it('renders a tab titled "In progress tasks"', () => {
-    const props = {
-      tabs: [{
-        label: 'In progress tasks'
-      },
-      {
-        label: 'Completed tasks'
-      }]
-    };
 
-    renderNonCompTabs(props);
+    renderNonCompTabs(basicProps);
 
-    expect(props.tabs[0]).toBe('In progress tasks');
+    expect(screen.getAllByText('In progress tasks')).toBeTruthy();
   });
 
   it('renders a tab titled "Completed tasks"', () => {
-    const props = {
-      tabs: [{
-        label: 'In progress tasks'
-      },
-      {
-        label: 'Completed tasks'
-      }]
-    };
 
-    renderNonCompTabs(props);
+    renderNonCompTabs(basicProps);
 
-    expect(props.tabs[1]).toBe('In progress tasks');
+    expect(screen.getAllByText('Completed tasks')).toBeTruthy();
   });
 
-  it('renders TaskTableUnconnected Component', () => {
-    const renderTaskTableUnconnected = (props) => {
-      return render(
-        <TaskTableUnconnected {...props} />
-      );
-    };
-
-    expect(renderTaskTableUnconnected).toContain(
-      this.state.taskPageApiEndpoint,
-      this.state.useTaskPageApi,
-      this.state.tabPaginationOptions);
-  });
 });
