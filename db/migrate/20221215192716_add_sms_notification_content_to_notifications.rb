c4b733e197c68ef2ef2db2290b2ed610d9bbda85
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2022-12-28 11:10:24 -0500

    fix merge conflict left in code

--- db/migrate/20221215192716_add_sms_notification_content_to_notifications.rb
+++ db/migrate/20221215192716_add_sms_notification_content_to_notifications.rb
@@ -1,9 +1,5 @@
 class AddSmsNotificationContentToNotifications < Caseflow::Migration
   def change
-<<<<<<< HEAD
     add_column :notifications, :sms_notification_content, :text, null: true, comment: "Full SMS Text Content of Notification"
-=======
-    add_column :notifications, :sms_notification_content, :string, null: true, comment: "Full SMS Text Content of Notification"
->>>>>>> master
   end
 end
