c7f069048210056049e2af2d4fa63dc31da91eb1
Minhazur Rahaman <minhazur9@gmail.com>
2022-12-29 10:20:41 -0500

    APPEALS-12775 added a reversible method to the migration

--- db/migrate/20221223211349_change_date_to_timestamp.rb
+++ db/migrate/20221223211349_change_date_to_timestamp.rb
@@ -1,5 +1,9 @@
 class ChangeDateToTimestamp < ActiveRecord::Migration[5.2]
-  def change
+  def up
     safety_assured {change_column :notifications, :event_date, :timestamp}
+  end
+
+  def down
+    safety_assured {change_column :notifications, :event_date, :date}
   end
 end
