7b3041fcf4ba45f4ce463c5a2f1ea5aaeac49007
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2022-12-29 18:29:26 -0500

    APPEALS-12997 removed color from rails logging

--- app/models/prepend/va_notify/privacy_act_pending.rb
+++ app/models/prepend/va_notify/privacy_act_pending.rb
@@ -42,7 +42,7 @@ module PrivacyActPending
     if PRIVACY_ACT_TASKS.include?(type) && !PRIVACY_ACT_TASKS.include?(parent&.type)
       MetricsService.record("Updating PRIVACY_ACT_PENDING column in Appeal States Table to TRUE and
                              PRIVACY_ACT_COMPLETE to FALSE "\
-        "for #{appeal.class} ID #{appeal.id}".yellow,
+        "for #{appeal.class} ID #{appeal.id}",
                             service: nil,
                             name: "AppellantNotification.appeal_mapper") do
         AppellantNotification.appeal_mapper(appeal.id, appeal.class.to_s, "privacy_act_pending")
