bfd3a3af7b6cb49582c6fbe0cf69a9fdcf60d9be
Minhazur Rahaman <minhazur9@gmail.com>
2023-01-19 12:23:31 -0500

    APPEALS-13520 first test completed

--- spec/jobs/hearings/travel_board_hearing_sync_job_spec.rb
+++ spec/jobs/hearings/travel_board_hearing_sync_job_spec.rb
@@ -2,17 +2,22 @@
 
 describe Hearings::TravelBoardHearingSyncJob do
   let(:current_user) { create(:user, roles: ["System Admin"]) }
-  let(:vacols_ids) { %w[12340 12341 12342 12343 12344 12345 12346 12347 12348 12349] }
+  let(:vacols_ids) { %w[123450 123451 123452 123453 123454 123455 123456 123457 123458 123459] }
+  let(:new_caseflow_vacols_ids) { %w[123450 123451 123452 123453 123455 123458 123459] }
   let(:legacy_appeal) { create(:legacy_appeal) }
   let(:cases) {
     create_list(:case, 10) do |vacols_case, i|
-      bfcurloc = (i == 4 || i == 7) ? "1" : LegacyAppeal::LOCATION_CODES[:schedule_hearing]
-      vacols_case.update!(bfkey: vacols_ids[i], bfcurloc: bfcurloc)
+      bfhr = (i == 4 || i == 7) ? "1" : VACOLS::Case::HEARING_PREFERENCE_TYPES_V2[:TRAVEL_BOARD][:vacols_value]
+      vacols_case.update!(
+        bfkey: vacols_ids[i],
+        bfcurloc: LegacyAppeal::LOCATION_CODES[:schedule_hearing],
+        bfhr: bfhr
+      )
     end
   }
 
   describe "#perform" do
-    subject { Hearings::TravelBoardHearingSyncJob.new.perform }
+    subject { Hearings::TravelBoardHearingSyncJob.new }
 
     before do
       current_user
@@ -22,8 +27,8 @@ describe Hearings::TravelBoardHearingSyncJob do
     end
 
     it "fetches and creates travel board legacy appeals that were not in caseflow before" do
-      expect(LegacyAppeal.all.pluck(:vacols_id))
-      subject
+      expect(subject.send(:fetch_vacols_travel_board_appeals, LegacyAppeal.all.pluck(:vacols_id), 250)
+        .pluck(:vacols_id)).to eq(new_caseflow_vacols_ids)
     end
   end
 end
