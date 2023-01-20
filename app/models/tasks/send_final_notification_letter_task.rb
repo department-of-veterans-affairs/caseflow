ea103698c6822027d955537b511bc30e233e182c
HunJerBAH <Jeremy.Hunton@va.gov>
2023-01-20 11:32:10 -0500

    added tasks actions to final notification task

--- app/models/tasks/send_final_notification_letter_task.rb
+++ app/models/tasks/send_final_notification_letter_task.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class SendFinalNotificationLetterTask < LetterTask
+  validates :parent, presence: true
+
+  def available_actions(user)
+    if assigned_to.user_has_access?(user) # feature toggle will go here
+      SEND_FINAL_NOTIFICATION_LETTER_TASK_ACTIONS
+    else
+      []
+    end
+  end
+
+  SEND_FINAL_NOTIFICATION_LETTER_TASK_ACTIONS = [
+    Constants.TASK_ACTIONS.MARK_FINAL_NOTIFICATION_LETTER_TASK_COMPLETE.to_h,
+    Constants.TASK_ACTIONS.RESEND_INITIAL_NOTIFICATION_LETTER.to_h,
+    Constants.TASK_ACTIONS.RESEND_FINAL_NOTIFICATION_LETTER.to_h,
+    Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+  ].freeze
 end
