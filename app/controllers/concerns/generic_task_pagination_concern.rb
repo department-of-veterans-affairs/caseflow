0bcda613bf6edc1acf23274f4d4a2507c20b5e0b
Lianna Newman <newman_lianna@bah.com>
2023-01-20 16:32:13 -0500

    Merge conflict error

--- app/controllers/concerns/generic_task_pagination_concern.rb
+++ app/controllers/concerns/generic_task_pagination_concern.rb
@@ -42,7 +42,7 @@ module GenericTaskPaginationConcern
   def pagination_query_params(sort_by_column)
     {
       sort_order: allowed_params[Constants.QUEUE_CONFIG.SORT_DIRECTION_REQUEST_PARAM.to_sym],
-      sort_by: allowed_params[Constants.QUEUE_CONFIG.SORT_COLUMN_REQUEST_PARAM.to_sym],
+      sort_by: sort_by_column,
       filters: allowed_params[Constants.QUEUE_CONFIG.FILTER_COLUMN_REQUEST_PARAM.to_sym],
       search_query: allowed_params[Constants.QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM.to_sym]
     }.compact
