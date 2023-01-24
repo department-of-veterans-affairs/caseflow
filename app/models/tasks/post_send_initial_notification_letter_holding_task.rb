3c94cbc61b4e50948bc2fbe1a7d2b99a4ffb13a9
Maite Piedra Yera <maite.piedrayera@va.gov>
2023-01-24 18:18:02 -0500

    Create PostSendInitialNotificationLetterHoldingTask model

---
+++ app/models/tasks/post_send_initial_notification_letter_holding_task.rb
@@ -0,0 +1,21 @@
+class PostSendInitialNotificationLetterHoldingTask  < TimedHoldTask
+  include TimeableTask
+
+  validates :parent, presence: true, on: :create
+
+  def available_actions(user)
+    if assigned_to.user_has_access?(user) &&
+      FeatureToggle.enabled?(:cc_appeal_workflow)
+      POST_SEND_INITIAL_NOTIFICATION_LETTER_HOLDING_TASK_ACTIONS
+    else
+      []
+    end
+  end
+
+  POST_SEND_INITIAL_NOTIFICATION_LETTER_HOLDING_TASK_ACTIONS = [
+    Constants.TASK_ACTIONS.CANCEL_CONTESTED_CLAIM_TASK.to_h,
+    Constants.TASK_ACTIONS.RESEND_INITIAL_NOTIFICATION_LETTER.to_h,
+    Constants.TASK_ACTIONS.PROCEED_FINAL_NOTIFICATION_LETTER.to_h,
+  ].freeze
+
+end
---
+++ spec/models/post_send_initial_notification_letter_holding_task_spec.rb
@@ -0,0 +1,58 @@
+describe PostSendInitialNotificationLetterHoldingTask do
+    let(:user) { create(:user) }
+    let(:cob_team) { ClerkOfTheBoard.singleton }
+    let(:root_task) { create(:root_task) }
+    let(:distribution_task) { create(:distribution_task, parent: root_task) }
+    let(:task_class) { PostSendInitialNotificationLetterHoldingTask }
+    before do
+      cob_team.add_user(user)
+    end
+
+    describe ".verify_user_can_create" do
+      let(:params) { { appeal: root_task.appeal, parent_id: distribution_task_id, type: task_class.name} }
+      let(:distribution_task_id) { distribution_task.id }
+
+      context "when no distribution_task exists for appeal" do
+        let(:distribution_task_id) { nil }
+
+        it "throws an error" do
+          expect { task_class.create_from_params(params, user) }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+
+      # test contexts for successfully creating task when an appeal has a CC will go here once other tasks are made
+    end
+
+    describe ".available_actions" do
+      let(:post_send_initial_notification_letter_holding_task) {
+        task_class.create!(appeal: distribution_task.appeal, parent_id: distribution_task.id, assigned_to: cob_team)
+      }
+
+      let(:available_task_actions) do
+        [
+          Constants.TASK_ACTIONS.CANCEL_CONTESTED_CLAIM_TASK.to_h,
+          Constants.TASK_ACTIONS.RESEND_INITIAL_NOTIFICATION_LETTER.to_h,
+          Constants.TASK_ACTIONS.PROCEED_FINAL_NOTIFICATION_LETTER.to_h
+        ]
+      end
+
+      context "the user is not a member of COB" do
+        let(:non_cob_user) { create(:user) }
+
+        subject { final_notification_letter_task.available_actions(non_cob_user) }
+
+        it "returns no actions" do
+          expect(subject).to_not eql(available_task_actions)
+          expect(subject).to eql([])
+        end
+      end
+
+      context "the user is a member of COB" do
+        subject { final_notification_letter_task.available_actions(user) }
+
+        it "returns the task actions" do
+          expect(subject).to eql(available_task_actions)
+        end
+      end
+    end
+  end
