bacece4ca17e36a94fb5d4ffbca7532ee74ff38c
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-01-24 11:01:15 -0600

    added tests for cavc dashboard selection_bases

--- spec/seeds/cavc_dashboard_data_spec.rb
+++ spec/seeds/cavc_dashboard_data_spec.rb
@@ -18,4 +18,20 @@ describe Seeds::CavcDashboardData do
     ).children.count).to eq 4
     expect(CavcDecisionReason.find_by(decision_reason: "AMA specific remand?").children.count).to eq 7
   end
+
+  it "#seed! creates all selection_bases for dispositions per APPEALS-13250" do
+    Seeds::CavcSelectionBasisData.new.seed!
+
+    expect(CavcSelectionBasis.where(category: "other_due_process_protection").count).to eq 18
+    expect(CavcSelectionBasis.where(category: "prior_examination_inadequate").count).to eq 200
+    expect(CavcSelectionBasis.where(category: "prior_opinion_inadequate").count).to eq 205
+    expect(CavcSelectionBasis.where(category: "consider_statute").count).to eq 19
+    expect(CavcSelectionBasis.where(category: "consider_regulation").count).to eq 81
+    expect(CavcSelectionBasis.where(category: "consider_diagnostic_code").count).to eq 1066
+    expect(CavcSelectionBasis.where(category: "consider_caselaw").count).to eq 196
+    expect(CavcSelectionBasis.where(category: "misapplication_statute").count).to eq 19
+    expect(CavcSelectionBasis.where(category: "misapplication_regulation").count).to eq 81
+    expect(CavcSelectionBasis.where(category: "misapplication_diagnostic_code").count).to eq 1066
+    expect(CavcSelectionBasis.where(category: "misapplication_caselaw").count).to eq 196
+  end
 end
