bf5deeda29026972574ad3394b682348c1e20d82
Craig Reese <109101548+craigrva@users.noreply.github.com>
2023-01-25 11:23:48 -0500

    Craig/appeals 13248 add cavc_dashboard_disposition (#17969)
    * working migration file
    
    * fix default value in migration
    
    * fix schema, change concerns in model
    
    * refactor migration, fix model and associated constants
    
    * modify migration, modify validations, add seed data
    
    * added route and frontend code to load appeal specific initial data
    
    * added tests for new controller route, model, seed file
    
    * removed check constraint from migration, added validation to disposition model

--- spec/controllers/cavc_dashboard_controller_spec.rb
+++ spec/controllers/cavc_dashboard_controller_spec.rb
@@ -5,21 +5,39 @@ RSpec.describe CavcDashboardController, type: :controller do
   let(:authorized_user) { create(:user) }
   before { User.authenticate!(user: authorized_user) }
 
-  it "#cavc_decision_reasons returns all CavcDecisionReasons in DB" do
-    Seeds::CavcDashboardData.new.seed!
+  context "for routes not specific to an appeal" do
+    it "#cavc_decision_reasons returns all CavcDecisionReasons" do
+      Seeds::CavcDashboardData.new.seed!
 
-    get :cavc_decision_reasons
+      get :cavc_decision_reasons
 
-    expect(response.status).to eq 200
-    expect(JSON.parse(response.body).count).to eq CavcDecisionReason.count
+      expect(response.status).to eq 200
+      expect(JSON.parse(response.body).count).to eq CavcDecisionReason.count
+    end
+
+    it "#cavc_selection_bases returns all CavcSelectionBases in DB" do
+      Seeds::CavcSelectionBasisData.new.seed!
+
+      get :cavc_selection_bases
+
+      expect(response.status).to eq 200
+      expect(JSON.parse(response.body).count).to eq CavcSelectionBasis.count
+    end
   end
 
-  it "#cavc_selection_bases returns all CavcSelectionBases in DB" do
-    Seeds::CavcSelectionBasisData.new.seed!
+  context "for routes specific to an appeal" do
+    before { Seeds::CavcDashboardData.new.seed! }
 
-    get :cavc_selection_bases
+    it "#index returns index data from format.json" do
+      remand = CavcRemand.last
+      appeal_uuid = Appeal.find(remand.remand_appeal_id).uuid
 
-    expect(response.status).to eq 200
-    expect(JSON.parse(response.body).count).to eq CavcSelectionBasis.count
+      get :index, params: { format: :json, appeal_id: appeal_uuid }
+      response_body = JSON.parse(response.body)
+
+      expect(response_body.key?("dashboard_dispositions")).to be true
+      expect(response_body["dashboard_dispositions"].count)
+        .to eq CavcDashboardDisposition.where(cavc_remand: remand).count
+    end
   end
 end
