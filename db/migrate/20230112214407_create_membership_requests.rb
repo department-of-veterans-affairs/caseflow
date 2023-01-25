4b83c6b550ddd8ec94bd8f931889f211c7bb074a
Bhavani Pachipulusu <bvni@users.noreply.github.com>
2023-01-25 17:14:54 -0500

    APPEALS-13122: Follow schema best practices

--- db/migrate/20230112214407_create_membership_requests.rb
+++ db/migrate/20230112214407_create_membership_requests.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateMembershipRequests < ActiveRecord::Migration[5.2]
+class CreateMembershipRequests < Caseflow::Migration
   def change
     create_table :membership_requests do |t|
       t.integer :requested_by, index: true
