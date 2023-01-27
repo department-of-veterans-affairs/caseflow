e3f5df485c9b3335adcd3d5aa02ccf1175420344
Sean Craig <Craig_Sean@bah.com>
2023-01-27 13:11:28 -0600

    Made requested changes to NonCompTabs and refactored TaskTableTab into a new file

--- client/app/nonComp/components/NonCompTabs.jsx
+++ client/app/nonComp/components/NonCompTabs.jsx
@@ -2,21 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import SearchBar from '../../components/SearchBar';
 import TabWindow from '../../components/TabWindow';
-import { TaskTableUnconnected } from '../../queue/components/TaskTable';
 import QUEUE_CONFIG from '../../../constants/QUEUE_CONFIG';
-import {
-  claimantColumn,
-  veteranParticipantIdColumn,
-  veteranSsnColumn,
-  decisionReviewTypeColumn,
-} from './TaskTableColumns';
-import {
-  buildDecisionReviewFilterInformation,
-  extractEnabledTaskFilters
-} from '../util/index';
 import COPY from '../../../COPY';
+import TaskTableTab from './TaskTableTab';
 
 class NonCompTabsUnconnected extends React.PureComponent {
   render = () => {
@@ -39,7 +28,7 @@ class NonCompTabsUnconnected extends React.PureComponent {
 
     const tabs = [{
       label: 'In progress tasks',
-      page: <TaskTableTabConnected
+      page: <TaskTableTab
         key="inprogress"
         baseTasksUrl={`${this.props.baseTasksUrl}?${QUEUE_CONFIG.TAB_NAME_REQUEST_PARAM}=in_progress`}
         tabPaginationOptions={tabPaginationOptions}
@@ -48,7 +37,7 @@ class NonCompTabsUnconnected extends React.PureComponent {
           defaultSortIdx: 3 }} />
     }, {
       label: 'Completed tasks',
-      page: <TaskTableTabConnected
+      page: <TaskTableTab
         key="completed"
         baseTasksUrl={`${this.props.baseTasksUrl}?${QUEUE_CONFIG.TAB_NAME_REQUEST_PARAM}=completed`}
         tabPaginationOptions={tabPaginationOptions}
@@ -75,112 +64,6 @@ NonCompTabsUnconnected.propTypes = {
   })
 };
 
-class TaskTableTab extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    let searchText = '';
-
-    // Set the search text to the get parameters if it exists
-    if (this.props.tabPaginationOptions[QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM]) {
-      searchText = this.props.tabPaginationOptions[QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM];
-    }
-
-    this.state = {
-      predefinedColumns: this.props.predefinedColumns,
-      searchText,
-      searchValue: searchText,
-    };
-  }
-
-  onChange = (value) => {
-    if (!value) {
-      // Edge case to reset the value if the user completely backspaces all of the text in the search input
-      this.setState({ searchText: '', searchValue: '' });
-    }
-
-    this.setState({ searchText: value });
-  };
-
-  // Use a different state variable for debouncing
-  // Pass this new value to queue table so it will resend the search on props/state change.
-  onSearch = (value) => this.setState({ searchValue: value });
-
-  onClearSearch = () => {
-    this.setState({ searchText: '', searchValue: '' });
-  }
-
-  getTableColumns = () => [
-    claimantColumn(),
-    {
-      ...decisionReviewTypeColumn(),
-      ...buildDecisionReviewFilterInformation(
-        this.props.filterableTaskTypes,
-        this.enabledTaskFilters()
-      )
-    },
-    this.props.featureToggles.decisionReviewQueueSsnColumn ?
-      veteranSsnColumn() :
-      veteranParticipantIdColumn()
-  ];
-
-  enabledTaskFilters = () => extractEnabledTaskFilters(
-    this.props.tabPaginationOptions[`${QUEUE_CONFIG.FILTER_COLUMN_REQUEST_PARAM}[]`]
-  )
-
-  render = () => {
-    this.props.tabPaginationOptions[QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM] = this.state.searchValue;
-
-    return <React.Fragment>
-      {this.props.description && <div className="cf-noncomp-queue-completed-task">{this.props.description}</div>}
-      <div className="cf-search-ahead-parent cf-push-right cf-noncomp-search">
-        <SearchBar
-          id="searchBar"
-          size="small"
-          title={
-            this.props.featureToggles.decisionReviewQueueSsnColumn ?
-              'Search by Claimant Name, Veteran Participant ID, File Number or SSN' :
-              ''
-          }
-          onChange={this.onChange}
-          recordSearch={this.onSearch}
-          placeholder="Type to search..."
-          onClearSearch={this.onClearSearch}
-          isSearchAhead
-          value={this.state.searchText}
-        />
-      </div>
-      <div className="section-hearings-list">
-        <TaskTableUnconnected
-          {...this.state.predefinedColumns}
-          getKeyForRow={(row, object) => object.id}
-          customColumns={this.getTableColumns()}
-          includeIssueCount
-          tasks={[]}
-          taskPagesApiEndpoint={this.props.baseTasksUrl}
-          useTaskPagesApi
-          tabPaginationOptions={this.props.tabPaginationOptions}
-        />
-      </div>
-    </React.Fragment>;
-  }
-}
-TaskTableTab.propTypes = {
-  description: PropTypes.node,
-  predefinedColumns: PropTypes.object,
-  tasks: PropTypes.array,
-  featureToggles: PropTypes.bool,
-  baseTasksUrl: PropTypes.string,
-  tabPaginationOptions: PropTypes.shape({
-    [QUEUE_CONFIG.SORT_COLUMN_REQUEST_PARAM]: PropTypes.string,
-    [QUEUE_CONFIG.SORT_DIRECTION_REQUEST_PARAM]: PropTypes.string,
-    [QUEUE_CONFIG.PAGE_NUMBER_REQUEST_PARAM]: PropTypes.string,
-    [`${QUEUE_CONFIG.FILTER_COLUMN_REQUEST_PARAM}[]`]: PropTypes.arrayOf(PropTypes.string),
-    [QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM]: PropTypes.string,
-    onPageLoaded: PropTypes.func
-  }),
-  filterableTaskTypes: PropTypes.object,
-};
-
 const NonCompTabs = connect(
   (state) => ({
     currentTab: state.currentTab,
@@ -188,11 +71,5 @@ const NonCompTabs = connect(
     taskFilterDetails: state.taskFilterDetails
   })
 )(NonCompTabsUnconnected);
-
-const TaskTableTabConnected = connect(
-  (state) => ({
-    featureToggles: state.featureToggles
-  })
-)(TaskTableTab);
 
 export default NonCompTabs;
