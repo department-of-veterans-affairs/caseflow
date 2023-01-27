753e7166323711961b9fe07b42b3a3669a708731
Bhavani Pachipulusu <bvni@users.noreply.github.com>
2023-01-27 14:04:46 -0500

    APPEALS-13122: Apply suggestions from code review
    Co-authored-by: Tyler Broyles <109369527+TylerBroyles@users.noreply.github.com>

--- db/migrate/20230126230602_create_membership_requests.rb
+++ db/migrate/20230126230602_create_membership_requests.rb
@@ -3,10 +3,10 @@ class CreateMembershipRequests < Caseflow::Migration
     create_table :membership_requests do |t|
       t.references :organization, foreign_key: true, comment: "The organization that the membership request is asking to join"
 
-      t.references :requestor, foreign_key: { to_table: :users }, comment: "The requestor for this membership"
-      t.references :decider, foreign_key: { to_table: :users }, index: false, null: true, comment: "The user who decides the membership_request"
+      t.references :requestor, foreign_key: { to_table: :users }, comment: "The User that is requesting access to the organization"
+      t.references :decider, foreign_key: { to_table: :users }, index: false, null: true, comment: "The user who decides the status of the membership request"
 
-      t.datetime :decided_at, comment: "The time when the membership request was decided at"
+      t.datetime :decided_at, comment: "The date and time when the deider user made a decision about the membership request"
       t.string :status, null: false, default: "assigned", comment: "The status of the membership request at any given point of time"
 
       t.timestamps
