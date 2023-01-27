8cf87938ee40f34766416123733a8aadb53b5b0b
Bhavani Pachipulusu <bvni@users.noreply.github.com>
2023-01-27 13:26:55 -0500

    APPEALS-13122: Update foreign_keys

--- db/migrate/20230126230602_update_membership_requests_associations.rb
+++ db/migrate/20230126230602_update_membership_requests_associations.rb
@@ -1,10 +1,10 @@
 class UpdateMembershipRequestsAssociations < Caseflow::Migration
   def change
     create_table :membership_requests do |t|
-      t.references :organization, comment: "The organization that the membership request is asking to join"
+      t.references :organization, foreign_key: true, comment: "The organization that the membership request is asking to join"
 
-      t.references :requestor, foreign_key: { to_table: :membership_requests }, comment: "The requestor for this membership"
-      t.references :decider, foreign_key: { to_table: :membership_requests }, index: false, null: true, comment: "The user who decides the membership_request"
+      t.references :requestor, foreign_key: { to_table: :users }, comment: "The requestor for this membership"
+      t.references :decider, foreign_key: { to_table: :users }, index: false, null: true, comment: "The user who decides the membership_request"
 
       t.datetime :decided_at, comment: "The time when the membership request was decided at"
       t.string :status, null: false, default: "assigned", comment: "The status of the membership request at any given point of time"
