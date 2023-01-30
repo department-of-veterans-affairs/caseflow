4e286ff6cdbfb94cd664843f56cd160a75069d04
kristeja <krishnateja.golla@va.gov>
2023-01-30 09:19:48 -0800

    APPEALS-12719 update seed data for dispatched appeals and refactored to fix code climate issues

--- db/seeds/test_case_data.rb
+++ db/seeds/test_case_data.rb
@@ -12,26 +12,6 @@ module Seeds
 
     private
 
-    def initial_id_values
-      @file_number ||= 400_000_000
-      @participant_id ||= 800_000_000
-      while Veteran.find_by(file_number: format("%<n>09d", n: @file_number + 1)) ||
-            VACOLS::Correspondent.find_by(ssn: format("%<n>09d", n: @file_number + 1))
-        @file_number += 2000
-        @participant_id += 2000
-      end
-    end
-
-    def create_veteran(options = {})
-      @file_number += 1
-      @participant_id += 1
-      params = {
-        file_number: format("%<n>09d", n: @file_number),
-        participant_id: format("%<n>09d", n: @participant_id)
-      }
-      create(:veteran, params.merge(options))
-    end
-
     def create_limbo_appeals
       5.times do
         create_priority_appeal_no_person
