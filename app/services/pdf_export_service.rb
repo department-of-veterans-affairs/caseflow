a3407138bc0c6c450074e650ee942517fe6a573c
j-n-t-l <611441@bah.com>
2023-01-31 15:20:44 -0500

    APPEALS-13311 changed methods to do all operations in one

--- app/services/pdf_export_service.rb
+++ app/services/pdf_export_service.rb
@@ -2,22 +2,51 @@
 
 class PdfExportService
   S3_BUCKET_NAME = "appeals-status-migrations"
+
+  # Purpose: Service that creates pdf file from html template
+  #
+  # Params: template_name (string), object (json)
+  # the object is data that contains
+  # Veteran Name (First and Last)
+  # Veteran file number
+  # Docket Number
+  # Docket Type
+  # Appeal Stream Type
+  # Hearing type
+  # Rows of notifications
+  # Event type
+  # Notification Date
+  # Notification Type
+  # Recipient Information
+  # Status
+  # Notification Content
+  #
+  # Returns: s2 file upload location
   def call(template_name, object = nil)
     upload_pdf_to_s3(template_name, object)
     S3_BUCKET_NAME
   end
 
-  def create_pdf_from_template(template_name, object = nil)
-    kit = PDFKit.new(template_name, :page_size => 'Letter')
+  # Purpose: Creates pdf from template using pdfkit
+  # Finds and renders the template based on template_name
+  # stores created pdf file in s3 bucket
+  #
+  # Params: template_name (string), object (json)
+  #
+  # Returns: pdf file created
+  def create_store_pdf_from_template(template_name, object = nil)
+    # render template
+    template = render_to_string :template => 'app/views/templates/' + template_name
+    # create new pdfkit object from template
+    kit = PDFKit.new(template, :page_size => 'Letter')
+    # add CSS styling
     kit.stylesheets << '/app/assets/stylesheets/notification_pdf_style.css'
-    pdf = kit.to_pdf
-    # INSERT RENDERING STUFF HERE
-    pdf
-  end
-
-  def upload_pdf_to_s3(template_name, object = nil)
-    pdf = create_pdf_from_template(template_name, object)
-    filename = Time.zone.now.strftime("ama-migration-%Y-%m-%d--%H-%M.pdf")
-    S3Service.store_file(S3_BUCKET_NAME + "/" + filename, pdf)
+    # create file name and file path
+    file_name = "test.pdf"
+    file_path = "#{Rails.root}/#{file_name}"
+    # create pdf file from pdfkit object
+    pdf = kit.to_pdf(file_path)
+    # store file in s3 bucket
+    S3Service.store_file(SchedulePeriod::S3_BUCKET_NAME + "/" + file_name, pdf, :file_path)
   end
 end
