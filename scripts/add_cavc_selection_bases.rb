a71097f977f1d68e76f12ee85fa996a3546b785a
Matt Roth <roth_matthew@bah.com>
2023-02-01 13:48:28 -0500

    create run_seed_data script and cleanup seeding

--- scripts/add_cavc_selection_bases.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# bundle exec rails runner scripts/add_cavc_selection_bases.rb
-
-Dir[Rails.root.join("db/seeds/*.rb")].sort.each { |f| require f }
-Seeds::CavcSelectionBasisData.new.seed!
