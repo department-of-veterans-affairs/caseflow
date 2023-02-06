1ac1feb1c311f536d2dfa11968e260a3839f6eea
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-02-06 14:21:50 -0800

    made changes to to appeal hearing location

--- app/models/concerns/appeal_available_hearing_locations.rb
+++ app/models/concerns/appeal_available_hearing_locations.rb
@@ -6,8 +6,9 @@ module AppealAvailableHearingLocations
   # :nocov:
   def suggested_hearing_location
     # return the closest hearing location, rejecting locations with nil distances
-    distance = proc(&:distance)
-    available_hearing_locations&.reject distance.nil? &.min_by(distance)
+    # available_hearing_locations&.reject { |loc| loc.distance.nil? }&.min_by { |loc| loc.distance }
+    distance_is_nil = proc { |loc| loc.distance.nil? }
+    available_hearing_locations&.reject(&:distance_is_nil) &.min_by(&:distance)
   end
   # :nocov:
 end
