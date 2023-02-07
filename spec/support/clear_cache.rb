26f6d2f77ae8892feb58cebee3a86e53300a7220
Anusha Palliyil <palliyil_anusha@bah.com>
2023-02-07 13:53:37 -0500

    removed comments at the bottom of the file

--- spec/support/clear_cache.rb
+++ spec/support/clear_cache.rb
@@ -31,37 +31,3 @@ def delete_matched(namespace:)
   redis = Redis.new(url: "redis://localhost:6379/1")
   redis.scan_each(match: "#{namespace}:*") { |key| redis.del(key) }
 end
-  =begin
-rescue => exception
-
-end# frozen_string_literal: true
-
-REDIS_NAMESPACES = [
-  "idt_test",
-  "end_product_records_test",
-  "test_all",
-  "test_#{ENV['TEST_SUBCATEGORY']}"
-].freeze
-
-FeatureToggle.cache_namespace = "test_#{ENV['TEST_SUBCATEGORY'] || 'all'}"
-
-RSpec.configure do |config|
-  config.before(:all) do
-    Rails.cache.clear
-    REDIS_NAMESPACES.each { |namespace| delete_matched(namespace: namespace) }
-  end
-
-  config.after(:each) do
-    Rails.cache.clear
-    REDIS_NAMESPACES.each { |namespace| delete_matched(namespace: namespace) }
-  rescue Errno::ENOENT, Errno::ENOTEMPTY => error
-    # flakey at CircleCI. Don't fail tests because of this.
-    Rails.logger.error(error)
-  end
-end
-
-def delete_matched(namespace:)
-  redis = Redis.current
-  redis.scan_each(match: "#{namespace}:*") { |key| redis.del(key) }
-end
-=end
