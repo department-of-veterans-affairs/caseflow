861562866402616e19f036547289aedce60ccf5e
Kamala Madamanchi <kamala.madamanchi@va.gov>
2023-02-08 00:34:55 -0600

    Resovled success message

--- client/app/queue/components/CompleteTaskModal.jsx
+++ client/app/queue/components/CompleteTaskModal.jsx
@@ -384,7 +384,7 @@ const MODAL_TYPE_ATTRS = {
   },
   proceed_final_notification_letter: {
     buildSuccessMsg: () => ({
-      title: sprintf(COPY.PROCEED_FINAL_NOTIFICATION_LETTER_SUCCESS),
+      title: sprintf(COPY.PROCEED_FINAL_NOTIFICATION_LETTER_TASK_SUCCESS),
     }),
     title: () => COPY.PROCEED_FINAL_NOTIFICATION_LETTER_TITLE,
     getContent: ProceedFinalNotificationLetterTaskModal,
