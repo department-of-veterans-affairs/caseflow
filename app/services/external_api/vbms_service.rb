da5d4b9ed83b8edc1809d46f082fda64219e6c15
Matt Roth <roth_matthew@bah.com>
2023-02-09 16:40:51 -0500

    Update vbms_service.rb

--- app/services/external_api/vbms_service.rb
+++ app/services/external_api/vbms_service.rb
@@ -205,7 +205,7 @@ class ExternalApi::VBMSService
 
   def self.send_and_log_request(vbms_id, request, override_vbms_client = nil)
     name = request.class.name.split("::").last
-    MetricsService.record("sent VBMS request #{request.class} for #{vbms_id}.",
+    MetricsService.record("sent VBMS request #{request.class} for #{vbms_id}",
                           service: :vbms,
                           name: name) do
       (override_vbms_client || @vbms_client).send_request(request)
