792e7503c9ba3ba4c35fe2b2516b4be6f92443a5
calvincostaBAH <108481161+calvincostaBAH@users.noreply.github.com>
2023-02-10 11:25:46 -0500

    Calvin/appeals 13254 (#18033)
    * got request_issues data and added issues section
    
    * some styling tests
    
    * added new dropdown and grid
    
    * got rid of extra code/styling
    
    * added cavc dashboard issues
    
    * refactored to make code cleaner
    
    * added safeguards + tests
    
    * fixed jest test issue
    
    * seperated issues and cavcIssues
    
    ---------
    
    Co-authored-by: Shruthi Sibi <109103820+shruthisibi@users.noreply.github.com>

--- client/app/queue/cavcDashboard/cavcDashboardConstants.js
+++ client/app/queue/cavcDashboard/cavcDashboardConstants.js
@@ -5,6 +5,8 @@ export const ACTIONS = {
 };
 
 export const LABELS = {
+  CAVC_DASHBOARD_ISSUES: 'Issues',
+  CAVC_DASHBOARD_DISPOSITIONS: 'Dispositions',
   BOARD_DECISION_DATE: 'Board Decision Date',
   BOARD_DOCKET_NUMBER: 'Board Docket Number',
   CAVC_DECISION_DATE: 'CAVC Decision Date',
