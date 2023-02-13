4f99ca5f57bf635d951a1a4badced5edb0beac2d
Minhazur Rahaman <minhazur9@gmail.com>
2023-02-13 15:48:32 -0500

    APPEALS-13127 new method to call on PdfExportService for generating and saving for both appeal and legacy appeal

--- app/models/concerns/appeal_notification_report_concern.rb
+++ app/models/concerns/appeal_notification_report_concern.rb
@@ -4,6 +4,6 @@ module AppealNotificationReportConcern
   extend ActiveSupport::Concern
 
   def upload_notification_report!
-
+    file_location = PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
   end
 end
