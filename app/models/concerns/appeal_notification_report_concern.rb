d71be3420f381a04b2cbb6cf65c7aa92dd0cfcf1
Minhazur Rahaman <minhazur9@gmail.com>
2023-02-16 15:21:02 -0500

    APPEALS-13127 updated concern to create document_params

--- app/models/concerns/appeal_notification_report_concern.rb
+++ app/models/concerns/appeal_notification_report_concern.rb
@@ -3,6 +3,10 @@
 module AppealNotificationReportConcern
   extend ActiveSupport::Concern
 
+  included do
+    attribute :veteran_file_number
+  end
+
   class PDFGenerationError < StandardError
     def initialize(message = "PDF or file path failed to generate")
       @message = message
@@ -12,7 +16,15 @@ module AppealNotificationReportConcern
 
   def upload_notification_report!
     begin
-      PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
+      file = PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
+      document_params =
+        {
+          veteran_file_number: veteran_file_number,
+          document_type: "BVA Case Notifications",
+          document_name: "notification-report",
+          application: "notification-report",
+          file: file
+        }
     rescue StandardError
       raise PDFGenerationError
     end
