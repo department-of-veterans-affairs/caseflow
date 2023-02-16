7365f20d2c401a7ae87fd4d6e1bc57b29e75b992
Minhazur Rahaman <minhazur9@gmail.com>
2023-02-16 13:58:31 -0500

    APPEALS-13127 pdf generation now getting rescued from any error by throwing exception

--- app/models/concerns/appeal_notification_report_concern.rb
+++ app/models/concerns/appeal_notification_report_concern.rb
@@ -10,25 +10,11 @@ module AppealNotificationReportConcern
     end
   end
 
-  included do
-    if is_a?(Appeal) == "Appeal"
-      attribute :uuid
-    elsif is_a?(Appeal) == "LegacyAppeal"
-      attribute :vacols_id
-    end
-  end
-
-  def file_path_is_valid?(file_path)
-    if is_a?(Appeal)
-      file_path =~ /notification_report_pdf_template_#{uuid}\.pdf/
-    elsif is_a?(LegacyAppeal)
-      file_path =~ /notification_report_pdf_template_#{vacols_id}\.pdf/
-    end
-  end
-
   def upload_notification_report!
-    file_location = PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
-    fail PDFGenerationError unless file_path_is_valid?(file_location)
-    # UploadDocumentToVbmsJob.perform_later(document_source: file_location)
+    begin
+      PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
+    rescue StandardError
+      raise PDFGenerationError
+    end
   end
 end
