89af7569d5fc1569853ef7858e9e7df5968bcf51
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-02-17 17:46:03 -0600

    basis for selection dropdown, comments, and fixed child checkbox value

--- client/app/queue/cavcDashboard/CavcDecisionReasons.jsx
+++ client/app/queue/cavcDashboard/CavcDecisionReasons.jsx
@@ -7,17 +7,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { css } from 'glamor';
 import PropTypes from 'prop-types';
 import { setCheckedDecisionReasons } from './cavcDashboardActions';
+import SearchableDropdown from '../../components/SearchableDropdown';
 
 const CavcDecisionReasons = ({ uniqueId }) => {
 
   const checkboxStyling = css({
     paddingLeft: '2.5%',
-    marginBlock: 'auto'
+    marginBlock: '0.75rem'
   });
 
   const childCheckboxStyling = css({
     paddingLeft: '5%',
-    marginBlock: 'auto'
+    marginBlock: '0.5rem'
+  });
+
+  const basisForSelectionStylingNoChild = css({
+    paddingLeft: '5%',
+    fontWeight: 'normal'
+  });
+
+  const basisForSelectionStylingWithChild = css({
+    paddingLeft: '7.5%',
+    fontWeight: 'normal'
   });
 
   const decisionReasons = useSelector((state) => state.cavcDashboard.decision_reasons);
@@ -48,11 +59,14 @@ const CavcDecisionReasons = ({ uniqueId }) => {
     return obj;
   }, {}));
 
-  // toggling state of checkbox when checkbox is clicked
+  // counter for parent checkboxes that are checked to display next to the header
+  const decisionReasonCount = Object.keys(checkedReasons).filter((key) => checkedReasons[key].checked).length;
+
   useEffect(() => {
     dispatch(setCheckedDecisionReasons(checkedReasons, uniqueId));
   }, [checkedReasons]);
 
+  // toggling state of checkbox when checkbox is clicked
   const handleCheckboxChange = (value, checkboxId) => {
     // if checkboxId < parentReasons.length then it is a parent checkbox therefore update parent checked state
     if (checkboxId <= parentReasons.length) {
@@ -81,6 +95,7 @@ const CavcDecisionReasons = ({ uniqueId }) => {
               };
             }
 
+            // keep child object the same if checkboxId does not match to child.id
             return child;
           })
         };
@@ -97,33 +112,56 @@ const CavcDecisionReasons = ({ uniqueId }) => {
     const childrenOfParent = childReasons.filter((child) => child.parent_decision_reason_id === parent.id);
 
     return (
-      <div key={parent.id}>
+      <div>
+        {/* render parent checkboxes */}
         <Checkbox
+          key={parent.id}
           name={`checkbox-${parent.id}-${uniqueId}`}
           label={parent.decision_reason}
           onChange={(value) => handleCheckboxChange(value, parent.id)}
           value={checkedReasons[parent.id]?.checked}
           styling={checkboxStyling}
         />
+        {/* render child checkbox if parent is checked */}
         {checkedReasons[parent.id]?.checked && (
           <div>
             {childrenOfParent.map((child) => (
-              <Checkbox
-                key={child.id}
-                name={`checkbox-${child.id}-${uniqueId}`}
-                label={child.decision_reason}
-                onChange={(value) => handleCheckboxChange(value, child.id)}
-                value={checkedReasons[child.id]?.checked}
-                styling={childCheckboxStyling}
-              />
+              <div>
+                <Checkbox
+                  key={child.id}
+                  name={`checkbox-${child.id}-${uniqueId}`}
+                  label={child.decision_reason}
+                  onChange={(value) => handleCheckboxChange(value, child.id)}
+                  value={checkedReasons[parent.id].children.find((x) => x.id === child.id).checked}
+                  styling={childCheckboxStyling}
+                />
+                {/* check if child checkbox is checked and basis category exists if so render component */}
+                {checkedReasons[parent.id]?.children.find(
+                  (x) => x.id === child.id && x.basis_for_selection_category)?.checked && (
+                    <SearchableDropdown
+                      name={`decision-reason-basis-${child.id}`}
+                      label="Basis for this selection"
+                      placeholder="Type to search..."
+                      styling={basisForSelectionStylingWithChild}
+                    />
+                  )}
+              </div>
             ))}
+            {/* check if parent checkbox has basis category but no child, if so render component  */}
+            {/* eslint-disable-next-line */}
+            {checkedReasons[parent.id]?.basis_for_selection_category && (
+              <SearchableDropdown
+                name={`decision-reason-basis-${parent.id}`}
+                label="Basis for this selection"
+                placeholder="Type to search..."
+                styling={basisForSelectionStylingNoChild}
+              />
+            )}
           </div>
         )}
       </div>
-    )
+    );
   });
-
-  const decisionReasonCount = Object.keys(checkedReasons).filter((key) => checkedReasons[key].checked).length;
 
   return (
     <>
