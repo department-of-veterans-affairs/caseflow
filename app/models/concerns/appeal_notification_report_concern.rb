2886f1d117848bea9555152de890cc1eba749f7a
Minhazur Rahaman <minhazur9@gmail.com>
2023-02-21 10:10:27 -0500

    APPEALS-13127 added comments

--- app/models/concerns/appeal_notification_report_concern.rb
+++ app/models/concerns/appeal_notification_report_concern.rb
@@ -11,13 +11,15 @@ module AppealNotificationReportConcern
     end
   end
 
+  # Exception for when PDF fails to generate
   class PDFGenerationError < StandardError
-    def initialize(message = "PDF or file path failed to generate")
+    def initialize(message = "PDF failed to generate")
       @message = message
       super(message)
     end
   end
 
+  # Exception for when PDF fails to upload
   class PDFUploadError < StandardError
     def initialize(message = "Error while trying to prepare or upload PDF")
       @message = message
@@ -25,6 +27,8 @@ module AppealNotificationReportConcern
     end
   end
 
+  # Purpose: Generate the PDF and then prepares the document for uploading to S3 or VBMS
+  # Returns: nil
   def upload_notification_report!
     begin
       document_params =
@@ -37,6 +41,7 @@ module AppealNotificationReportConcern
           file: notification_report
         }
       PrepareDocumentUploadToVbms.new(document_params, User.system_user, self).call
+      nil
     rescue StandardError
       raise PDFUploadError
     end
@@ -44,6 +49,8 @@ module AppealNotificationReportConcern
 
   private
 
+  # Purpose: Creates the name for the document
+  # Returns: The document name
   def notification_document_name
     if is_a?(Appeal)
       "notification-report_#{uuid}_#{Time.now.utc.strftime('%Y%m%d%k%M%S')}"
@@ -52,6 +59,8 @@ module AppealNotificationReportConcern
     end
   end
 
+  # Purpose: Generates the PDF
+  # Returns: The generated PDF encoded in base64
   def notification_report
     begin
       file = PdfExportService.create_and_save_pdf("notification_report_pdf_template", self)
