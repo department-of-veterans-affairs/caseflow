a2286a8ec6a164c1a22691721dd1a6aec5027f55
cacevesva <109166981+cacevesva@users.noreply.github.com>
2023-02-24 10:28:37 -0800

    Update 20230222133918_add_completed_by_to_task_model.rb
    Added column comment for completed_by_id

--- db/migrate/20230222133918_add_completed_by_to_task_model.rb
+++ db/migrate/20230222133918_add_completed_by_to_task_model.rb
@@ -2,6 +2,6 @@ class AddCompletedByToTaskModel < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_column :tasks, :completed_by_id, :integer
+    add_column :tasks, :completed_by_id, :integer, comment: "ID of user that marked task complete"
   end
 end
