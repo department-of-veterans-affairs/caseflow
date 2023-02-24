cd7df3e6099f1da7cfde965354079cfc1ed45f2b
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-02-24 10:05:40 -0600

    testing db failure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_17_013023) do
+ActiveRecord::Schema.define(version: 2023_02_22_214508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
