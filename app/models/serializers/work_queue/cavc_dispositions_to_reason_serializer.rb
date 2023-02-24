db26067a39d554c8fcfcd7b1d30fec9d03edbc94
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-02-24 10:21:03 -0600

    test

--- app/models/serializers/work_queue/cavc_dispositions_to_reason_serializer.rb
+++ app/models/serializers/work_queue/cavc_dispositions_to_reason_serializer.rb
@@ -2,7 +2,7 @@ class WorkQueue::CavcDispositionsToReasonSerializer
   include FastJsonapi::ObjectSerializer
 
   attribute :id
-  attribute :cavc_dashboard_disposition
-  attribute :decision_reason
-  attribute :basis_for_selection
+  attribute :cavc_dashboard_dispositions_id
+  attribute :decision_reason_id
+  attribute :basis_for_selection_id
 end
