f2095214779ec0ad8fb9824764defbc59b8e67c8
sulak-j <justin.sulak@va.gov>
2023-02-27 14:39:31 -0600

    Adding cancel modal to cavcdashboard footer

--- client/app/queue/cavcDashboard/CavcDashboardFooter.jsx
+++ client/app/queue/cavcDashboard/CavcDashboardFooter.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../components/Button';
+import CancelCavcDashboardChangeModal from './CancelCavcDashboardChangeModal';
 
 export const CavcDashboardFooter = (props) => {
   const { userCanEdit, history } = props;
+  // const { appealId, userCanEdit, history } = props;
+  const [cancelModalIsOpen, setCancelModalIsOpen] = useState(false);
+
+  const closeHandler = () => {
+    setCancelModalIsOpen(!cancelModalIsOpen);
+  };
 
   //      history.push(`/queue/appeals/${appeal.externalId}`);
   //  const cancel = () => history.goBack();
@@ -19,20 +26,25 @@ export const CavcDashboardFooter = (props) => {
   if (userCanEdit) {
     return (
       <>
-        <Button onClick={cancel}>Cancel</Button>
+        <Button onClick={closeHandler}>Cancel</Button>
         <Button onClick={save}>Save Changes</Button>
+        {
+          (cancelModalIsOpen) &&
+          <CancelCavcDashboardChangeModal closeHandler={closeHandler} {...props} />
+        }
       </>
     );
   }
 
   return (
     <>
-      <Button onClick={() => history.goBack()}>Close</Button>
+      <Button onClick={cancel}>Close</Button>
     </>
   );
 };
 
 CavcDashboardFooter.propTypes = {
+  // appealId: PropTypes.string,
   userCanEdit: PropTypes.bool.isRequired,
   // Router inherited props
   history: PropTypes.object
