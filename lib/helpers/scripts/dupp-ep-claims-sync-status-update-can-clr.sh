2bc04d599a6cd56f53be0038774f869fdcd41c53
Stephan Ward <stephan.ward@va.gov>
2023-02-28 13:12:52 -0500

    Adjusted script to find file.

--- lib/helpers/scripts/dupp-ep-claims-sync-status-update-can-clr.sh
+++ lib/helpers/scripts/dupp-ep-claims-sync-status-update-can-clr.sh
@@ -3,24 +3,33 @@
 # This script runs the WarRoom::DuppEpClaimsSyncStatusUpdateCanClr job in the Caseflow Rails console.
 # Usage: ./dupp-ep-claims-sync-status-update-can-clr.sh <arg1> <arg2>
 # This will check if the first and second arguments are either
-# "Yes" or "No". If they are not, it will print an error message
+# (Yes, No) for manual remediation or (No, Yes) for auto remediation.
+# If they are not, it will print an error message
 # and exit with a non-zero status.
 # The first arguement is for manual remediation for HLR or Supplimental claim. The second argumentment is for auto-remediation for dupplicateEP error for HLR or SC.
 
 set -e # Exit script immediately if any command exits with a non-zero status.
 
-cd /opt/caseflow-certification/src
+set sudo su
+
+cd /opt/caseflow-certification
+
+cd src/
+
+cd lib
+
+cd helpers
 
 if [[ "$1" == "No" && "$2" == "No" ]]; then
   echo "There is no remediation to be performed."
 elif [[ "$1" == "Yes" && "$2" == "No" ]]; then
-  echo "You chosed the manual duplicateEP resolution for HLR or SC to be performed."
-  bin/rails runner 'ActiveRecord::Base.transaction do; WarRoom::DuppEpClaimsSyncStatusUpdateCanClr.new.run("manual", "sc_hlr"); end'
+  echo "You chose the manual duplicateEP resolution for HLR or SC to be performed."
+  bin/rails runner ActiveRecord::Base.transaction do; WarRoom::DuppEpClaimsSyncStatusUpdateCanClr.new.run("manual", "sc_hlr"); end
   bin/rails c <<EOF
 EOF
 elif [[ "$1" == "No" && "$2" == "Yes" ]]; then
-  echo "You chosed the automated duplicateEP remediation for HLR and SC."
-  bin/rails runner 'ActiveRecord::Base.transaction do; WarRoom::DuppEpClaimsSyncStatusUpdateCanClr.new.run("auto", "sc_hlr"); end'
+  echo "You chose the automated duplicateEP remediation for HLR and SC."
+  bin/rails runner ActiveRecord::Base.transaction do; WarRoom::DuppEpClaimsSyncStatusUpdateCanClr.new.run("auto", "sc_hlr"); end
   bin/rails c <<EOF
 EOF
 elif [[ "$1" == "Yes" && "$2" == "Yes" ]]; then
