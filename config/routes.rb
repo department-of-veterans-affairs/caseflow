b182f8eb3bacf43ebedbf1f3e67ebb50c7d7f672
Craig Reese <109101548+craigrva@users.noreply.github.com>
2023-03-01 13:58:12 -0500

    Craig/appeals 13264 (#18135)
    * modified seed data to add dispositions to reasons links
    
    * added front end methods for making call to backend, added some seed data, added save route to backend, work in progress on cavc dashboard controller save
    
    * major refactor; cavc_dashboard_dispositions created when new issues are added, added create/update logic to backend
    
    * added issue type to checked boxes, backend saves checked boxes as dispositions to reasons
    
    * added save redirect to case details, refactor controller methods for rubocop
    
    * added code to enable/disable save for state differences
    
    * added decision reasons to initial state and save button enabled conditional
    
    * added test for controller #save
    
    * fixed linting, added/modified/fixed tests
    
    * added methods to delete removed dashboard issues, updated tests for deleting dashboard issues
    
    * use disposition ID instead of index for updating dispositions
    
    * revert an unintended merge change to cavc_dashboard'
    
    * remove unused commented code

--- config/routes.rb
+++ config/routes.rb
@@ -291,6 +291,7 @@ Rails.application.routes.draw do
   scope path: "/cavc_dashboard" do
     get "/cavc_decision_reasons", to: "cavc_dashboard#cavc_decision_reasons"
     get "/cavc_selection_bases", to: "cavc_dashboard#cavc_selection_bases"
+    post "/save", to: "cavc_dashboard#save"
   end
 
   # allow requests for CAVC Dashboard to go through /cavc_dashboard/:appeal_id/* to declutter the queue path above
