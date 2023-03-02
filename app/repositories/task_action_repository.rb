53a8830a22999eb34674a3ad572dc70ae78c2a29
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-03-02 13:50:39 -0500

    Remove duplicate attributes

--- app/repositories/task_action_repository.rb
+++ app/repositories/task_action_repository.rb
@@ -488,7 +488,6 @@ class TaskActionRepository
         modal_title: COPY::VHA_SEND_TO_BOARD_INTAKE_MODAL_TITLE,
         modal_button_text: COPY::MODAL_SUBMIT_BUTTON,
         type: VhaDocumentSearchTask.name,
-        modal_button_text: COPY::MODAL_SEND_BUTTON,
         redirect_after: "/organizations/#{VhaCamo.singleton.url}"
       }
     end
@@ -498,7 +497,6 @@ class TaskActionRepository
         modal_title: COPY::DOCUMENTS_READY_FOR_BOARD_INTAKE_REVIEW_MODAL_TITLE,
         modal_button_text: COPY::MODAL_SEND_BUTTON,
         type: EducationDocumentSearchTask.name,
-        modal_button_text: COPY::MODAL_SEND_BUTTON,
         redirect_after: "/organizations/#{EducationEmo.singleton.url}",
         body_optional: true
       }
