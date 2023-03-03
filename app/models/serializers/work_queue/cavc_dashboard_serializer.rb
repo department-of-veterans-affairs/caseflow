d6c509ae5d46899d277cde27574cf3d139623881
Matt Roth <roth_matthew@bah.com>
2023-03-03 14:11:07 -0500

    update display of issues

--- app/models/serializers/work_queue/cavc_dashboard_serializer.rb
+++ app/models/serializers/work_queue/cavc_dashboard_serializer.rb
@@ -11,11 +11,21 @@ class WorkQueue::CavcDashboardSerializer
       WorkQueue::CavcDashboardDispositionSerializer.new(cdd).serializable_hash[:data][:attributes]
     end
   end
-  attribute :cavc_dashboard_issues
+  attribute :cavc_dashboard_issues do |object|
+    object.cavc_dashboard_issues.order(:id)
+  end
   attribute :cavc_decision_date
   attribute :cavc_docket_number
   attribute :cavc_remand
   attribute :joint_motion_for_remand
 
-  attribute :remand_request_issues
+  attribute :remand_request_issues do |object|
+    object.remand_request_issues.map do |issue|
+      {
+        id: issue.id,
+        benefit_type: issue.benefit_type,
+        description: issue.description,
+      }
+    end
+  end
 end
