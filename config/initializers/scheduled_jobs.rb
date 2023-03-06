e639f94b8acc8ebe6c5c43cadde0ea48a6c1c7b3
Brandon Reed <reed_brandon@bah.com>
2023-03-06 16:21:38 -0500

    Added notification_efolder_sync_job to scheduled jobs

--- config/initializers/scheduled_jobs.rb
+++ config/initializers/scheduled_jobs.rb
@@ -39,5 +39,6 @@ SCHEDULED_JOBS = {
     "fetch_all_active_ama_appeals_job" => FetchAllActiveAmaAppealsJob,
     "fetch_all_active_legacy_appeals_job" => FetchAllActiveLegacyAppealsJob,
     "retrieve_and_cache_reader_documents_job" => RetrieveAndCacheReaderDocumentsJob,
-    "travel_board_hearing_sync_job" => Hearings::TravelBoardHearingSyncJob
+    "travel_board_hearing_sync_job" => Hearings::TravelBoardHearingSyncJob,
+    "notification_efolder_sync_job" => NotificationEfolderSyncJob
 }.freeze
