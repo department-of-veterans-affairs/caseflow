8165953776c456354e32401cf41ec8f706c49756
Minhazur Rahaman <minhazur9@gmail.com>
2023-03-07 11:36:14 -0500

    APPEALS-15868 added comments

--- client/app/hearings/components/dailyDocket/DailyDocketGuestLinkSection.jsx
+++ client/app/hearings/components/dailyDocket/DailyDocketGuestLinkSection.jsx
@@ -5,8 +5,10 @@ import CopyTextButton from '../../../components/CopyTextButton';
 
 export const DailyDocketGuestLinkSection = ({ linkInfo, requestType, hasAccess }) => {
 
+  // Conference Link Information
   const { alias, guestLink, guestPin } = linkInfo;
 
+  // The hearing type that the guest link is for
   const hearingType = {
     C: 'central hearings',
     V: 'non-virtual hearings',
@@ -29,20 +31,34 @@ export const DailyDocketGuestLinkSection = ({ linkInfo, requestType, hasAccess }
     justifyContent: 'space-around'
   };
 
+  // Props needed for the copy text button component
   const CopyTextButtonProps = {
     text: 'Copy Guest Link',
     label: 'Copy Guest Link',
     textToCopy: guestLink
   };
 
+  /**
+   * Render information about the guest link
+   * @param {conferenceRoom} - The conference link alias
+   * @param {pin} - The guest pin
+   * @param {roleAccess} - Boolean for if the current user has access to the guest link
+   * @returns The room information
+  */
+  const renderRoomInfo = (conferenceRoom, pin, roleAccess) => {
+    return (
+      <div style={roomInfoContainerStyle}>
+        <h3>Conference Room:<span style={{ fontWeight: 'normal' }}>{conferenceRoom}</span></h3>
+        <h3>PIN:<span style={{ fontWeight: 'normal' }}>{pin}#</span></h3>
+        {roleAccess && <h3><CopyTextButton {...CopyTextButtonProps} /></h3>}
+      </div>
+    );
+  };
+
   return (
     <div style={containerStyle}>
       <h3>Guest links for {hearingType[requestType] || hearingType.V}</h3>
-      <div style={roomInfoContainerStyle}>
-        <h3>Conference Room:<span style={{ fontWeight: 'normal' }}>{alias}</span></h3>
-        <h3>PIN:<span style={{ fontWeight: 'normal' }}>{guestPin}#</span></h3>
-        {hasAccess && <h3><CopyTextButton {...CopyTextButtonProps} /></h3>}
-      </div>
+      {renderRoomInfo(alias, guestPin, hasAccess)}
     </div>
   );
 };
