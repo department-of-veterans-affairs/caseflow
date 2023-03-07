cae26b911bbb380e07b0fc80494f250215707d47
Stephan Ward <stephan.ward@va.gov>
2023-03-07 15:54:36 -0500

    Wrapped in transaction.

--- db/seeds/duplicate_ep_data.rb
+++ db/seeds/duplicate_ep_data.rb
@@ -1,43 +1,51 @@
 # rails runner db/seeds/duplicate_ep_data.rb to execute script.
 
-# Generate 5 HigherLevelReview records
-5.times do
-  hlr = HigherLevelReview.create!(
-    establishment_error: 'duplicateep',
-    veteran: Veteran.create!(
-      # add veteran attributes as needed
+ActiveRecord::Base.transaction do
+  # Generate 5 HigherLevelReview records
+  1.times do
+    hlr = HigherLevelReview.create!(
+      establishment_error: 'duplicateep',
+      veteran: Veteran.create!(
+        file_number: '123546789',
+        first_name: 'John',
+        last_name: 'Doe',
+        ssn: '123_54_6789',
+      )
     )
-  )
-  claimant = Claimant.create!(
-    # add claimant attributes as needed
-  )
-  end_product = EndProduct.create!(
-    claimant: claimant,
-    claim_type_code: '030',
-    status_type_code: %w[CAN CLR].shuffle.first,
-    last_action_date: [Date.today, 1.day.ago.to_date].sample,
-    veteran: hlr.veteran
-  )
-  puts "Created HigherLevelReview with ID #{hlr.id}, Veteran with ID #{hlr.veteran.id}, EndProduct with ID #{end_product.id}, and Claimant with ID #{claimant.id}"
-end
+    claimant = Claimant.create!(
+      # add claimant attributes as needed
+    )
+    end_product = EndProduct.create!(
+      claimant: claimant,
+      claim_type_code: '030',
+      status_type_code: %w[CAN CLR].shuffle.first,
+      last_action_date: [Date.today, 1.day.ago.to_date].sample,
+      veteran: hlr.veteran
+    )
+    puts "Created HigherLevelReview with ID #{hlr.id}, Veteran with ID #{hlr.veteran.id}, EndProduct with ID #{end_product.id}, and Claimant with ID #{claimant.id}"
+  end
 
-# Generate 5 SupplementalClaim records
-5.times do
-  sc = SupplementalClaim.create!(
-    establishment_error: 'duplicateep',
-    veteran: Veteran.create!(
-      # add veteran attributes as needed
+  # Generate 5 SupplementalClaim records
+  1.times do
+    sc = SupplementalClaim.create!(
+      establishment_error: 'duplicateep',
+      veteran: Veteran.find_or_create_by_file_number(
+        file_number: '123564789',
+        first_name: 'Jane',
+        last_name: 'Doe',
+        ssn: '123_56_4789',
+      )
     )
-  )
-  claimant = Claimant.create!(
-    # add claimant attributes as needed
-  )
-  end_product = EndProduct.create!(
-    claimant: claimant,
-    claim_type_code: '040',
-    status_type_code: %w[CAN CLR].shuffle.first,
-    last_action_date: [Date.today, 1.day.ago.to_date].sample,
-    veteran: sc.veteran
-  )
-  puts "Created SupplementalClaim with ID #{sc.id}, Veteran with ID #{sc.veteran.id}, EndProduct with ID #{end_product.id}, and Claimant with ID #{claimant.id}"
+    claimant = Claimant.create!(
+      # add claimant attributes as needed
+    )
+    end_product = EndProduct.create!(
+      claimant: claimant,
+      claim_type_code: '040',
+      status_type_code: %w[CAN CLR].shuffle.first,
+      last_action_date: [Date.today, 1.day.ago.to_date].sample,
+      veteran: sc.veteran
+    )
+    puts "Created SupplementalClaim with ID #{sc.id}, Veteran with ID #{sc.veteran.id}, EndProduct with ID #{end_product.id}, and Claimant with ID #{claimant.id}"
+  end
 end
