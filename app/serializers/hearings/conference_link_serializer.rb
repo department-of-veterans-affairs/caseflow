dbb3c9190793618aaea837bf4e997897fa79456a
Minhazur Rahaman <minhazur9@gmail.com>
2023-03-08 10:33:49 -0500

    APPEALS-15868 added new conference link attributes to serializer

--- app/serializers/hearings/conference_link_serializer.rb
+++ app/serializers/hearings/conference_link_serializer.rb
@@ -5,4 +5,6 @@ class ConferenceLinkSerializer
   attribute :host_pin
   attribute :host_link, &:host_link
   attribute :alias, &:alias_with_host
+  attribute :guest_pin, &:guest_pin_long
+  attribute :guest_link, &:guest_hearing_link
 end
