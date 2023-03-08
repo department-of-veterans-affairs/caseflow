e5b4f515f2b708e5b294a481f6b057dd64a85794
Stephan Ward <stephan.ward@va.gov>
2023-03-08 12:15:50 -0500

    Start Over Point

--- db/seeds/duplicate_ep_error_data.rb
+++ db/seeds/duplicate_ep_error_data.rb
@@ -5,7 +5,7 @@
 # This ensures that no duplicates are created, even if the script is run multiple times.
 # This ensures that the file_number is unique.
 
-require 'faker'
+ require 'faker'
 
 ActiveRecord::Base.transaction do
   # Generate 1 HigherLevelReview record
@@ -17,59 +17,48 @@ ActiveRecord::Base.transaction do
 
     hlr = HigherLevelReview.create!(
       establishment_error: 'duplicateep',
-      veteran: Veteran.create!(
-        file_number: file_number,
-        first_name: 'John',
-        last_name: 'Doe',
-        ssn: Faker::Base.regexify(/^\d{3}-\d{2}-\d{4}$/),
-      )
+      veteran_file_number: file_number,
     )
 
-    claimant = Claimant.create!(
+    end_product_establishments = EndProductEstablishment.create!(
+      code: '030HLRNR',
+      synced_status: %w[CAN CLR].shuffle.first,
+      last_synced_at: [Date.today, 1.day.ago.to_date].sample,
+      veteran: hlr.veteran,
+      veteran_file_number: file_number)
+    end
+
+    #claimant = Claimant.create!(
       # add claimant attributes as needed
-    )
-
-    end_product = EndProduct.create!(
-      claimant: claimant,
-      claim_type_code: '030',
-      status_type_code: %w[CAN CLR].shuffle.first,
-      last_action_date: [Date.today, 1.day.ago.to_date].sample,
-      veteran: hlr.veteran
-    )
+    #)
 
     #Todo: Create EPE
     puts "Created HigherLevelReview with ID #{hlr.id}, Veteran with ID #{hlr.veteran.id}, and EndProduct with ID #{end_product.id}"
   end
 
   # Generate 1 SupplementalClaim record
-  1.times do
-    file_number = loop do
-      random_file_number = "123#{Faker::Number.number(digits: 5)}"
-      break random_file_number unless Veteran.exists?(file_number: random_file_number)
-    end
+  #1.times do
+    #file_number = loop do
+      #random_file_number = "123#{Faker::Number.number(digits: 5)}"
+      #break random_file_number unless Veteran.exists?(file_number: random_file_number)
+    #end
 
-    sc = SupplementalClaim.create!(
-      establishment_error: 'duplicateep',
-      veteran: Veteran.create!(
-        file_number: file_number,
-        first_name: 'Jane',
-        last_name: 'Doe',
-        ssn: Faker::Base.regexify(/^\d{3}-\d{2}-\d{4}$/),
-      )
-    )
+    #sc = SupplementalClaim.create!(
+      #establishment_error: 'duplicateep',
+    #)
 
-    claimant = Claimant.create!(
+    #claimant = Claimant.create!(
       # add claimant attributes as needed
-    )
+    #)
 
-    end_product = EndProduct.create!(
-      claimant: claimant,
-      claim_type_code: '040',
-      status_type_code: %w[CAN CLR].shuffle.first,
-      last_action_date: [Date.today, 1.day.ago.to_date].sample,
-      veteran: sc.veteran
-    )
+    #end_product_establishments = EndProductEstablishment.create!(
+      #claimant: claimant,
+      #code: '040',
+      #status_type_code: %w[CAN CLR].shuffle.first,
+      #last_action_date: [Date.today, 1.day.ago.to_date].sample,
+      #veteran: sc.veteran
+    #)
 
-    puts "Created SupplementalClaim with ID #{sc.id}, Veteran with ID #{sc.veteran.id} and EndProduct with ID #{end_product.id}"
-  end
+    #puts "Created SupplementalClaim with ID #{sc.id}, Veteran with ID #{sc.veteran.id} and EndProduct with ID #{end_product.id}"
+  #end
 end
