e7b6344d0b78e950a021d93d3711ccd389005003
Lianna Newman <newman_lianna@bah.com>
2023-03-13 17:43:39 -0500

    Imported Newly Created VHA Info Alert to general Alert file so flash message would appear

--- client/app/nonComp/components/Alerts.jsx
+++ client/app/nonComp/components/Alerts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Alert from '../../components/Alert';
+import { VHAOrgJoinInfo } from '../../queue/membershipRequest/Alerts';
 
 export class ErrorAlert extends React.PureComponent {
   render() {
@@ -43,28 +44,6 @@ SuccessAlert.propTypes = {
   successCode: PropTypes.string
 };
 
-export class VHAOrgJoinInfoNotice extends React.PureComponent {
-  render() {
-    const VHAOrgJoinInfoNoticeObject = {
-      vhaOrgJoinInfoNotified: {
-        title: 'VHA Team Access',
-        body: 'If you are a VHA team member, you will need access to VHA-specific' +
-        ' pages to perform your duties. Press the “Request access” button below to' +
-        ' be redirected to the VHA section within the Help page, where you can' +
-        ' submit a form for access.'
-      }
-    }[this.props.VHAOrgJoinInfoNoticeCode];
-
-    return <Alert title={VHAOrgJoinInfoNoticeObject.title} type="info" lowerMargin>
-      {VHAOrgJoinInfoNoticeObject.body}
-    </Alert>;
-  }
-}
-
-VHAOrgJoinInfoNotice.propTypes = {
-  VHAOrgJoinInfoNoticeCode: PropTypes.string
-};
-
 export class FlashAlerts extends React.PureComponent {
   render() {
     let alerts = this.props.flash.map((flash, idx) => {
@@ -79,7 +58,7 @@ export class FlashAlerts extends React.PureComponent {
       } else if (flash[0] === 'edited') {
         flashMsg = <Alert key={idx} title="Edit Completed" type="success" lowerMargin>{flash[1]}</Alert>;
       } else if (flash[0] === 'show_vha_org_join_info') {
-        flashMsg = <Alert key={idx} title="VHA Team Accesss" type="info" lowerMargin> {} </Alert>;
+        flashMsg = <VHAOrgJoinInfo />;
       }
 
       return flashMsg;
