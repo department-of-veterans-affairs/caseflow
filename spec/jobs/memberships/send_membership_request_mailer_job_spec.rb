f58f06258e0f35fb62bbcc56cc81d95504951230
Sean Craig <Craig_Sean@bah.com>
2023-03-15 13:17:22 -0500

    small fix

--- spec/jobs/memberships/send_membership_request_mailer_job_spec.rb
+++ spec/jobs/memberships/send_membership_request_mailer_job_spec.rb
@@ -53,8 +53,8 @@ describe Memberships::SendMembershipRequestMailerJob do
     end
 
     context "an error is thrown" do
+      let(:type) { "SendAdminsMembershipRequestSubmissionEmail" }
       it "rescues error and logs to sentry" do
-        let(:type) { "SendAdminsMembershipRequestSubmissionEmail" }
         allow_any_instance_of(MembershipRequestMailer).to receive(:membership_request_submission).and_raise(error)
         subject
         expect(@raven_called).to eq(true)
