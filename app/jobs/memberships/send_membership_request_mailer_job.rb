6772bfb8b229c070ea2025f0a3af2b856da3e475
Sean Craig <Craig_Sean@bah.com>
2023-03-15 14:39:06 -0500

    added external_message_id method to send_membership_request_mailer_job

--- app/jobs/memberships/send_membership_request_mailer_job.rb
+++ app/jobs/memberships/send_membership_request_mailer_job.rb
@@ -3,6 +3,8 @@
 class Memberships::SendMembershipRequestMailerJob < CaseflowJob
   queue_with_priority :low_priority
 
+  LOG_PREFIX = "SendMembershipRequest"
+
   def perform(email_type, recipient_info)
     MembershipRequestMailer.with(recipient_info: recipient_info).send(
       email_to_send(email_type)
@@ -21,6 +23,31 @@ class Memberships::SendMembershipRequestMailerJob < CaseflowJob
       :updated_membership_request_status
     else
       fail ArgumentError, "Unable to send email `#{email_type}`"
+    end
+  end
+
+  def external_message_id(msg)
+    if msg.is_a?(GovDelivery::TMS::EmailMessage)
+      response = msg.response
+      response_external_url = response.body.dig("_link", "self")
+
+      DataDogService.increment_counter(\
+        app_name: Constants.DATADOG_METRICS.DISPATCH.APP_NAME,
+        metric_group: Constants.DATADOG_METRICS.DISPATCH.OUTCODE_GROUP_NAME,
+        metric_name: "email.error",
+        attrs: {
+          email_type: type
+        }
+      )
+
+      log = log.message.merge(
+        status: response.status,
+        gov_delivery_id: response_external_url,
+        message: "GovDelivery returned (code: #{response.status}) (external url: #{response_external_url})"
+      )
+      Rails.logger.info("#{LOG_PREFIX} #{log}")
+
+      response_external_url
     end
   end
 end
