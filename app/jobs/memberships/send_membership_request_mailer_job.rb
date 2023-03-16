a6668f0ac838a06fe4d059491663af4dcee47477
Sean Craig <Craig_Sean@bah.com>
2023-03-16 11:53:47 -0500

    fixed the code to work

--- app/jobs/memberships/send_membership_request_mailer_job.rb
+++ app/jobs/memberships/send_membership_request_mailer_job.rb
@@ -7,9 +7,11 @@ class Memberships::SendMembershipRequestMailerJob < CaseflowJob
   TYPE_LABEL = "Send Membership Request notification email"
 
   def perform(email_type, mailer_parameters)
-    MembershipRequestMailer.with(mailer_parameters).send(
-      email_to_send(email_type)
-    ).deliver_now!
+    send_email(
+      MembershipRequestMailer.with(mailer_parameters).send(
+        email_to_send(email_type)
+      ), mailer_parameters
+    )
   end
 
   private
@@ -33,6 +35,7 @@ class Memberships::SendMembershipRequestMailerJob < CaseflowJob
     method_name
   end
 
+  # :nocov:
   def external_message_id(msg)
     if msg.is_a?(GovDelivery::TMS::EmailMessage)
       response = msg.response
@@ -58,37 +61,58 @@ class Memberships::SendMembershipRequestMailerJob < CaseflowJob
     end
   end
 
-  def send_email(email)
+  # :nocov:
+  def send_email(email, mailer_parameters)
+    # Why are we using `deliver_now!`? The documentation mentions that it ignores the flags:
+    #
+    #   * `perform_deliveries`
+    #   * `raise_delivery_errors`
+    #
+    # https://github.com/mikel/mail/blob/8fbb17d4d5364c77cc870769d451bc2739b3a8ce/lib/mail/message.rb#L261-L272
+    #
+    # `perform_deliveries` we will always want to be true. There isn't an environment
+    # where we wouldn't want to send emails (in the test environment this is already
+    # stubbed out).
+    #
+    # `raise_delivery_errors` we want to be true, but this is the default. This flag
+    # is mostly intended if you want to ignore errors when sending emails to invalid
+    # email addresses (which we don't want). We want to break flow if GovDelivery
+    # returns a non-successful error message, which it should do by default.
+    #
+    # In summary, ignoring those 2 flags is perfectly fine since we are ok with the
+    # default behavior of them both being `true`.
+    #
+    # The benefit of using `deliver_now!` is that it returns the actual response from
+    # GovDelivery. The actual web response gives Caseflow the ability to track
+    # the email after it has been accepted by GovDelivery.
     if email.nil?
-      log = log_message.merge(status: "error", message: "No #{TYPE_LABEL} was sent because no email address is defined")
+      log = log_message(mailer_parameters).merge(status: "error", message: "No #{TYPE_LABEL} was sent because no email address is defined")
       Rails.logger.info("#{LOG_PREFIX} #{log}")
       return false
     end
 
-    log = log_message.merge(status: "info", message: "Sending #{TYPE_LABEL} to #{recipient_info} ...")
+    log = log_message(mailer_parameters).merge(status: "info", message: "Sending #{TYPE_LABEL} to #{mailer_parameters[:recipient_info]} ...")
     Rails.logger.info("#{LOG_PREFIX} #{log}")
     msg = email.deliver_now!
   rescue StandardError, Savon::Error, BGS::ShareError => error
     # Savon::Error and BGS::ShareError are sometimes thrown when making requests to BGS endpoints\
     Raven.capture_exception(error)
-    log = log_message.merge(status: "error", message: "Failed to send #{TYPE_LABEL} to #{recipient_info} : #{error}")
+    log = log_message(mailer_parameters).merge(status: "error", message: "Failed to send #{TYPE_LABEL} to #{mailer_parameters[:recipient_info]} : #{error}")
     Rails.logger.warn("#{LOG_PREFIX} #{log}")
     Rails.logger.warn(error.backtrace.join($INPUT_RECORD_SEPARATOR))
     false
   else
     message_id = external_message_id(msg)
-    message = "Requested GovDelivery to send #{TYPE_LABEL} to #{recipient_info} - #{message_id}"
-    log = log_message.merge(status: "success", gov_delivery_id: message_id, message: message)
+    message = "Requested GovDelivery to send #{TYPE_LABEL} to #{mailer_parameters[:recipient_info]} - #{message_id}"
+    log = log_message(mailer_parameters).merge(status: "success", gov_delivery_id: message_id, message: message)
     Rails.logger.info("#{LOG_PREFIX} #{log}")
     true
   end
 
-  def log_message
+  def log_message(mailer_parameters)
     {
       class: self.class,
-      appeal_id: @appeal.id,
-      recipient_info: @recipient_info,
-      email_type: @email_type
+      mailer_parameters: mailer_parameters
     }
   end
 end
