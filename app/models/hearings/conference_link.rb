3da2a4f9eae4fc5a396536036f610082378efb1d
Minhazur Rahaman <minhazur9@gmail.com>
2023-03-27 12:11:14 -0400

    APPEALS-19191 fixed serializer and guest_link check

--- app/models/hearings/conference_link.rb
+++ app/models/hearings/conference_link.rb
@@ -47,7 +47,7 @@ class ConferenceLink < CaseflowRecord
   end
 
   def guest_link
-    return guest_hearing_link if !guest_hearing_link.nil?
+    return guest_hearing_link if !guest_hearing_link.empty?
 
     if !alias_name.nil?
       link_service = VirtualHearings::LinkService.new(alias_name)
