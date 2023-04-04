3d6b21b3e1c59ffa30755ff1900061265aa1836a
Minhazur Rahaman <minhazur9@gmail.com>
2023-04-04 09:57:49 -0400

    APPEALS-19651 fixed code climate errors

--- spec/jobs/ama_notification_efolder_sync_job_spec.rb
+++ spec/jobs/ama_notification_efolder_sync_job_spec.rb
@@ -26,14 +26,14 @@ describe AmaNotificationEfolderSyncJob, type: :job do
         )
       end
     }
-    # rubocop:disable convention:Layout/LineLength
+    # rubocop:disable Layout/LineLength
     let(:make_appeals_outcoded) {
       BvaDispatchTask.find(appeal_id: 6, assigned_to_type: "User").update!(status: "completed", closed_at: 2.days.ago)
       BvaDispatchTask.find(appeal_id: 6, assigned_to_type: "Organization").update!(status: "completed", closed_at: 2.days.ago)
       BvaDispatchTask.find(appeal_id: 7, assigned_to_type: "User").update!(status: "completed", closed_at: today)
       BvaDispatchTask.find(appeal_id: 7, assigned_to_type: "Organization").update!(status: "completed", closed_at: today)
     }
-    # rubocop:enable convention:Layout/LineLength
+    # rubocop:enable Layout/LineLength
     let(:first_run_outcoded_appeals) { [Appeal.find(7)] }
     let(:second_run_outcoded_appeals) { [Appeal.find(7)] }
     let(:first_run_never_synced_appeals) { Appeal.first(5) + Appeal.last(2) }
