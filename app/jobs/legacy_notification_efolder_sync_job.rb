210cd3638a3ffc7d90587aa9a6b4a5ccf604cf8b
Marc Steele <Marc.Steele@va.gov>
2023-04-05 15:42:04 -0400

    APPEALS-19651 Added Tomdoc comments

--- app/jobs/legacy_notification_efolder_sync_job.rb
+++ app/jobs/legacy_notification_efolder_sync_job.rb
@@ -3,14 +3,14 @@
 class LegacyNotificationEfolderSyncJob < CaseflowJob
   queue_with_priority :low_priority
 
-  # * Query gives back a list of active Legacy Appeals that have notifications
-  # * and has a VBMS uploaded document associated with the appeal and has the
-  # * document_type = "BVA Case Notifications" and the last notification associated with the Appeal
-  # * notification datetime is after the vbms uploaded doc uploaded_datetime. If no record exists it will
-  # * also return the appeal.
-
   BATCH_LIMIT = ENV["LEGACY_NOTIFICATION_REPORT_SYNC_LIMIT"] || 500
 
+  # Purpose: Determines which appeals need a notificatino report generated and uploaded to efolder,
+  #          then uploads reports for those appeals
+  #
+  # Params: none
+  #
+  # Return: Array of appeals that were attempted to upload notification reports to efolder
   def perform
     RequestStore[:current_user] = User.system_user
     all_active_legacy_appeals = appeals_recently_outcoded + appeals_never_synced + ready_for_resync
@@ -19,7 +19,12 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
 
   private
 
-  # A list of Appeals that have been outcoded within the last 24 hours
+  # Purpose: Determines a list of appeals that have been closed in the last day
+  #          These would not be found by other queries and are most important to be synced fist
+  #
+  # Params: none
+  #
+  # Return: Array of appeals that were closed within the last 24 hours
   def appeals_recently_outcoded
     LegacyAppeal
       .where(id: RootTask.where(
@@ -31,15 +36,17 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
       .uniq)
   end
 
-  # A list of appeals that have never had notification reports uploaded
+  # Purpose: Determines which appeals have never had a notification report uploaded to efolder
+  #
+  # Params: none
+  #
+  # Return: Array of appeals that have never been synced and meet all requirements for syncing
   def appeals_never_synced
-    # A list of unique appeal ids (Primary Key) that exist in VBMSUploadedDocument and are of type BVA Case Notification
     appeal_ids_synced = VbmsUploadedDocument.distinct
       .where(appeal_type: "LegacyAppeal", document_type: "BVA Case Notifications")
       .where.not(attempted_at: nil)
       .pluck(:appeal_id)
 
-    # A list of Appeals that have never had notification reports generated and synced with VBMS
     appeals_without_reports = LegacyAppeal
       .where(id: RootTask.open.where(appeal_type: "LegacyAppeal").pluck(:appeal_id))
       .where.not(id: appeal_ids_synced)
@@ -49,9 +56,12 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
     end
   end
 
-  # A list of appeals that already have notification reports uploaded
+  # Purpose: Determines which appeals need a NEW notification report uploaded to efolder
+  #
+  # Params: none
+  #
+  # Return: Array of appeals that are ready for a new notification report
   def ready_for_resync
-    # Ids for the latest Notification Report for every Legacy Appeal ordered from oldest to newest
     previously_synced_appeal_ids = VbmsUploadedDocument
       .where(appeal_type: "LegacyAppeal", document_type: "BVA Case Notifications")
       .where.not(attempted_at: nil)
@@ -59,10 +69,15 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
       .uniq(&:appeal_id)
       .reverse.pluck(:appeal_id)
 
-    # Appeals for all the previously synced reports from oldest to newest
     get_appeals_from_prev_synced_ids(previously_synced_appeal_ids).compact
   end
 
+  # Purpose: Determines if a new notification has happened since the last time a
+  #          notification report was uploaded to efolder
+  #
+  # Params: Array of appeal ids (primary key)
+  #
+  # Return: Array of active appeals and nil values (if inactive or failed checks)
   def get_appeals_from_prev_synced_ids(appeal_ids)
     appeal_ids.map do |appeal_id|
       begin
@@ -83,6 +98,7 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
 
   # Purpose: Syncs the notification reports in VBMS with the notification table for each appeal
   # Params: appeals - LegacyAppeals records in need of a new notification report to be generated
+  # Return: none
   def sync_notification_reports(appeals)
     Rails.logger.info("Starting to sync notification reports for Legacy appeals")
     gen_count = 0
@@ -100,7 +116,7 @@ class LegacyNotificationEfolderSyncJob < CaseflowJob
 
   # Purpose: Will return the most recent notification associated with the appeal
   # Params: vacols_id - The vacols_id of the appeal record that the notification is associated
-  # Returns: The most recent notification record
+  # Returns: The most recent notification record for the given appeal
   def last_notification_of_appeal(vacols_id)
     Notification
       .where(appeals_id: vacols_id)
