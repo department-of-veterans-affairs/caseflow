7db45dcbe451aba91e418ca1e8f247bd65efbb6a
Marc Steele <Marc.Steele@va.gov>
2023-04-13 12:37:57 -0400

    APPEALS-19651 Update scope formatting to allow for multiline

--- app/models/vbms_uploaded_document.rb
+++ app/models/vbms_uploaded_document.rb
@@ -5,9 +5,9 @@ class VbmsUploadedDocument < CaseflowRecord
 
   attribute :file, :string
 
-  # rubocop:disable Layout/LineLength
-  scope :successfully_uploaded, -> { where(error: nil).where.not(uploaded_to_vbms_at: nil, attempted_at: nil, processed_at: nil) }
-  # rubocop:enable Layout/LineLength
+  scope :successfully_uploaded, lambda {
+    where(error: nil).where.not(uploaded_to_vbms_at: nil, attempted_at: nil, processed_at: nil)
+  }
 
   def cache_file
     UploadDocumentToVbms.new(document: self).cache_file
