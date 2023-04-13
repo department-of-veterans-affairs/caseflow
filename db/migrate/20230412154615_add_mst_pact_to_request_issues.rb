6d7e09652b430fbad8befe29cc44820e7114afb8
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-04-13 13:55:51 -0500

    typo in migration

--- db/migrate/20230412154615_add_mst_pact_to_request_issues.rb
+++ db/migrate/20230412154615_add_mst_pact_to_request_issues.rb
@@ -1,6 +1,6 @@
 class AddMstPactToRequestIssues < ActiveRecord::Migration[5.2]
   def change
-    add_column :request_issues, :mst_status, :boolean, comment: "Indicates if issue is related to Military Sexual Trauam (MST)"
+    add_column :request_issues, :mst_status, :boolean, comment: "Indicates if issue is related to Military Sexual Trauma (MST)"
     add_column :request_issues, :pact_status, :boolean, comment: "Indicates if issue is related to Promise to Address Comprehensive Toxics (PACT) Act"
   end
 end
