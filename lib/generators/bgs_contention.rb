a190d053bcb9064ec99eac460db04e0aedf202f5
HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
2023-04-18 12:24:50 -0400

    HunJerBAH/APPEALS-17509: Mocking BGS Contentions in Test Data (#18457)
    * added mst and pact option to contention generator
    
    * added mst and pact option to bgs_contention generator
    
    * updated record maker to build epes with mst/pact contentions
    
    * refactored to create both mst and pact contention cases for veterans
    
    * added duplicate contention error handling in generator

--- lib/generators/bgs_contention.rb
+++ lib/generators/bgs_contention.rb
@@ -16,8 +16,58 @@ class Generators::BgsContention
       }
     end
 
+    def default_attrs_with_mst
+      {
+        reference_id: generate_external_id,
+        text: "Generic contention with MST claim",
+        type_code: "SUP",
+        medical_indicator: "1",
+        orig_source_type_code: "APP",
+        begin_date: Time.zone.today,
+        claim_id: generate_external_id,
+        special_issues: {
+          :call_id=>"12345",
+          :jrn_dt=>5.days.ago,
+          :name=>"SpecialIssue",
+          :spis_tc=>"MST",
+          :spis_tn=>"Military Sexual Trauma (MST)"
+        }
+      }
+    end
+
+    def default_attrs_with_pact
+      {
+        reference_id: generate_external_id,
+        text: "Generic contention with PACT claim",
+        type_code: "SUP",
+        medical_indicator: "1",
+        orig_source_type_code: "APP",
+        begin_date: Time.zone.today,
+        claim_id: generate_external_id,
+        special_issues: {
+          :call_id=>"12345",
+          :jrn_dt=>5.days.ago,
+          :name=>"SpecialIssue",
+          :spis_tc=>"PACT",
+          :spis_tn=>"PACT"
+        }
+      }
+    end
+
     def build(attrs = {})
       attrs = default_attrs.merge(attrs)
+
+      OpenStruct.new(attrs).tap { |contention| Fakes::BGSService.end_product_store.create_contention(contention) }
+    end
+
+    def build_mst_contention(attrs = {})
+      attrs = default_attrs_with_mst.merge(attrs)
+
+      OpenStruct.new(attrs).tap { |contention| Fakes::BGSService.end_product_store.create_contention(contention) }
+    end
+
+    def build_pact_contention(attrs = {})
+      attrs = default_attrs_with_pact.merge(attrs)
 
       OpenStruct.new(attrs).tap { |contention| Fakes::BGSService.end_product_store.create_contention(contention) }
     end
