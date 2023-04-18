1548c3530d9f1b706aea0310dcedec1907448fbd
j-n-t-l <611441@bah.com>
2023-04-18 11:27:12 -0400

    APPEALS-20062 did a migration and updated logic and rspec

--- app/controllers/api/v1/va_notify_controller.rb
+++ app/controllers/api/v1/va_notify_controller.rb
@@ -1,37 +1,30 @@
 # frozen_string_literal: true
 
 class Api::V1::VaNotifyController < Api::ApplicationController
-  # {POST Method for Veteran ID, Deceased Indicator, Deceased Time}
+
+  # class NotificationNotFound < StandardError; end
+
   def notifications_update
-    notif = Notification.find(params["id"])
-    if notif
-      if notif[:email_notification_external_id] == params["email_notification_external_id"]
-        notif[:email_notification_status] = params["email_notification_status"]
-      elsif notif[:sms_notification_external_id] == params["sms_notification_external_id"]
-        notif[:sms_notification_status] = params["sms_notification_status"]
+    if params["type"] == "email"
+      notif = Notification.find_by(email_notification_external_id: params["id"])
+      if notif
+        notif[:email_notification_status] = params["status"]
+      else
+        uuid = SecureRandom.uuid
+        Rails.logger.error("An email notification with id " + params["id"] + " could not be found." + "Error ID: " + uuid)
+        # Raven.capture_exception(error, extra: { error_uuid: uuid })
+        fail
       end
-    else
-      notif = Notification.new(
-        appeals_id: params["appeals_id"],
-        appeals_type: params["appeals_type"],
-        created_at: params["created_at"],
-        email_enabled: params["email_enabled"],
-        email_notification_content: params["email_notification_content"],
-        email_notification_external_id: params["email_notification_external_id"],
-        email_notification_status: params["email_notification_status"],
-        event_date: params["event_date"],
-        event_type: params["event_type"],
-        notification_content: params["notification_content"],
-        notification_type: params["notification_type"],
-        notified_at: params["notified_at"],
-        participant_id: params["participant_id"],
-        recipient_email: params["recipient_email"],
-        recipient_phone_number: params["recipient_phone_number"],
-        sms_notification_content: params["sms_notification_content"],
-        sms_notification_external_id: params["sms_notification_external_id"],
-        sms_notification_status: params["sms_notification_status"],
-        updated_at: params["updated_at"]
-      )
+    elsif params["type"] == "sms"
+      notif = Notification.find_by(sms_notification_external_id: params["id"])
+      if notif
+        notif[:sms_notification_status] = params["status"]
+      else
+        uuid = SecureRandom.uuid
+        Rails.logger.error("An SMS notification with id " + params["id"] + " could not be found." + "Error ID: " + uuid)
+        # Raven.capture_exception(error, extra: { error_uuid: uuid })
+        fail
+      end
     end
     notif.save
   end
--- spec/controllers/api/v1/va_notify_controller_spec.rb
+++ spec/controllers/api/v1/va_notify_controller_spec.rb
@@ -1,59 +1,133 @@
 # frozen_string_literal: true
 
 describe Api::V1::VaNotifyController, type: :controller do
-  let!(:veteran) { create(:veteran) }
-  let!(:appeal) { create(:appeal, veteran: veteran) }
-  let!(:notification_email) { create(:notification, appeals_id: appeal.uuid, appeals_type: "Appeal", event_date: "2023-02-27 13:11:51.91467", event_type: "Appeal docketed", notification_type: "Email", notified_at: "2023-02-28 14:11:51.91467", email_notification_status: "No Claimant Found") }
-  let!(:notification_sms) { create(:notification, appeals_id: appeal.uuid, appeals_type: "Appeal", event_date: "2023-02-27 13:11:51.91467", event_type: "Appeal docketed", notification_type: "Email", notified_at: "2023-02-28 14:11:51.91467", sms_notification_status: "Preferences Declined") }
-  let(:msg) { VANotifySendMessageTemplate.new(success_message_attributes, good_template_name) }
+  let!(:appeal) { create(:appeal) }
+  let(:notification_email) { create(:notification, appeals_id: appeal.uuid, appeals_type: "Appeal", event_date: "2023-02-27 13:11:51.91467", event_type: "Quarterly Notification", notification_type: "Email", notified_at: "2023-02-28 14:11:51.91467", email_notification_external_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6", email_notification_status: "No Claimant Found") }
+  let(:notification_sms) { create(:notification, appeals_id: appeal.uuid, appeals_type: "Appeal", event_date: "2023-02-27 13:11:51.91467", event_type: "Quarterly Notification", notification_type: "Email", notified_at: "2023-02-28 14:11:51.91467", sms_notification_external_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6", sms_notification_status: "Preferences Declined") }
+  # let(:msg) { VANotifySendMessageTemplate.new(success_message_attributes, good_template_name) }
 
   context "notification record not found" do
   end
 
   context "email notification status is changed" do
-    let(:payload) do
+    let(:payload_email) do
       {
-        "appeals_id": appeal.uuid,
-        "appeals_type": "Appeal",
-        "created_at": params["created_at"],
-        "email_enabled": true,
-        "email_notification_content": msg,
-        "email_notification_external_id": notification_email.id,
-        "email_notification_status": "Success",
-        "event_date": params["event_date"],
-        "event_type": params["event_type"],
-        "notification_content": params["notification_content"],
-        "notification_type": params["notification_type"],
-        "notified_at": params["notified_at"],
-        "participant_id": params["participant_id"],
-        "recipient_email": params["recipient_email"],
-        "recipient_phone_number": params["recipient_phone_number"],
-        "updated_at": params["updated_at"]
-    }
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        body: "string",
+        completed_at: "2023-04-17T12:38:48.699Z",
+        created_at: "2023-04-17T12:38:48.699Z",
+        created_by_name: "string",
+        email_address: "user@example.com",
+        line_1: "string",
+        line_2: "string",
+        line_3: "string",
+        line_4: "string",
+        line_5: "string",
+        line_6: "string",
+        phone_number: "+16502532222",
+        postage: "string",
+        postcode: "string",
+        reference: "string",
+        scheduled_for: "2023-04-17T12:38:48.699Z",
+        sent_at: "2023-04-17T12:38:48.699Z",
+        sent_by: "string",
+        status: "created",
+        subject: "string",
+        type: "email"
+      }
+    end
+    it "updates status of notification" do
+      byebug
+      post :notifications_update, params: payload_email
+      expect(notification_email.status).to eq("created")
     end
   end
 
   context "sms notification status is changed" do
-    let(:payload) do
+    let(:payload_sms) do
       {
-        "appeals_id": appeal.uuid,
-        "appeals_type": "Appeal",
-        "created_at": params["created_at"],
-        "email_enabled": false,
-        "event_date": params["event_date"],
-        "event_type": params["event_type"],
-        "notification_content": params["notification_content"],
-        "notification_type": params["notification_type"],
-        "notified_at": params["notified_at"],
-        "participant_id": params["participant_id"],
-        "recipient_email": params["recipient_email"],
-        "recipient_phone_number": params["recipient_phone_number"],
-        "sms_notification_content": params["sms_notification_content"],
-        "sms_notification_external_id": params["sms_notification_external_id"],
-        "sms_notification_status": params["sms_notification_status"],
-        "updated_at": params["updated_at"]
-    }
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "body": "string",
+        "completed_at": "2023-04-17T12:38:48.699Z",
+        "created_at": "2023-04-17T12:38:48.699Z",
+        "created_by_name": "string",
+        "email_address": "user@example.com",
+        "line_1": "string",
+        "line_2": "string",
+        "line_3": "string",
+        "line_4": "string",
+        "line_5": "string",
+        "line_6": "string",
+        "phone_number": "+16502532222",
+        "postage": "string",
+        "postcode": "string",
+        "recipient_identifiers": [
+          {
+            "id_type": "VAPROFILEID",
+            "id_value": "string"
+          }
+        ],
+        "reference": "string",
+        "scheduled_for": "2023-04-17T12:38:48.699Z",
+        "sent_at": "2023-04-17T12:38:48.699Z",
+        "sent_by": "string",
+        "status": "created",
+        "subject": "string",
+        "template": {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "uri": "string",
+          "version": 0
+        },
+        "type": "sms"
+      }
+    end
+    it "updates status of notification" do
+      post :notifications_update, params: payload_sms
+      expect(notification_email.status).to eq("created")
     end
   end
-
+  context "notification does not exist" do
+    let(:payload_fake) do
+      {
+        "id": "fake",
+        "body": "string",
+        "completed_at": "2023-04-17T12:38:48.699Z",
+        "created_at": "2023-04-17T12:38:48.699Z",
+        "created_by_name": "string",
+        "email_address": "user@example.com",
+        "line_1": "string",
+        "line_2": "string",
+        "line_3": "string",
+        "line_4": "string",
+        "line_5": "string",
+        "line_6": "string",
+        "phone_number": "+16502532222",
+        "postage": "string",
+        "postcode": "string",
+        "recipient_identifiers": [
+          {
+            "id_type": "VAPROFILEID",
+            "id_value": "string"
+          }
+        ],
+        "reference": "string",
+        "scheduled_for": "2023-04-17T12:38:48.699Z",
+        "sent_at": "2023-04-17T12:38:48.699Z",
+        "sent_by": "string",
+        "status": "created",
+        "subject": "string",
+        "template": {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "uri": "string",
+          "version": 0
+        },
+        "type": "sms"
+      }
+    end
+    it "updates status of notification" do
+      post :notifications_update, params: payload_fake
+      error_msg = JSON.parse(response.body)["message"]
+      expect(error_msg).to include("could not be found.")
+    end
+  end
 end
