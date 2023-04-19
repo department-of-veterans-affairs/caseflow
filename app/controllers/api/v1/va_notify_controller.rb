f9008105c00a16c9a0853e51b562886e2d53dbe0
j-n-t-l <611441@bah.com>
2023-04-19 12:25:40 -0400

    APPEALS-20062 split up method into smaller ones

--- app/controllers/api/v1/va_notify_controller.rb
+++ app/controllers/api/v1/va_notify_controller.rb
@@ -8,31 +8,49 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   # Response: Update corresponding Notification status
   def notifications_update
     if params["type"] == "email"
-      # find notification through external id
-      notif = Notification.find_by(email_notification_external_id: params["id"])
-      # update notification if it exists
-      if notif
-        notif.update!(email_notification_status: params["status"])
-      # log external id if notification doesn't exist
-      else
-        uuid = SecureRandom.uuid
-        error_msg = "An email notification with id " + params["id"] + " could not be found. " + "Error ID: " + uuid
-        Rails.logger.error(error_msg)
-        render json: { message: error_msg}, status: :internal_server_error
-      end
+      email_update
     elsif params["type"] == "sms"
-      # find notification through external id
-      notif = Notification.find_by(sms_notification_external_id: params["id"])
-      # update notification if it exists
-      if notif
-        notif.update!(sms_notification_status: params["status"])
-      # log external id if notification doesn't exist
-      else
-        uuid = SecureRandom.uuid
-        error_msg = "An SMS notification with id " + params["id"] + " could not be found. " + "Error ID: " + uuid
-        Rails.logger.error(error_msg)
-        render json: { message: error_msg}, status: :internal_server_error
-      end
+      sms_update
+    end
+  end
+
+  # Purpose: Finds and updates notification if type is email
+  #
+  # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
+  #
+  # Response: Update corresponding email Notification status
+  def email_update
+    # find notification through external id
+    notif = Notification.find_by(email_notification_external_id: params["id"])
+    # update notification if it exists
+    if notif
+      notif.update!(email_notification_status: params["status"])
+    # log external id if notification doesn't exist
+    else
+      uuid = SecureRandom.uuid
+      error_msg = "An email notification with id " + params["id"] + " could not be found. " + "Error ID: " + uuid
+      Rails.logger.error(error_msg)
+      render json: { message: error_msg}, status: :internal_server_error
+    end
+  end
+
+  # Purpose: Finds and updates notification if type is SMS
+  #
+  # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
+  #
+  # Response: Update corresponding SMS Notification status
+  def sms_update
+    # find notification through external id
+    notif = Notification.find_by(sms_notification_external_id: params["id"])
+    # update notification if it exists
+    if notif
+      notif.update!(sms_notification_status: params["status"])
+    # log external id if notification doesn't exist
+    else
+      uuid = SecureRandom.uuid
+      error_msg = "An SMS notification with id " + params["id"] + " could not be found. " + "Error ID: " + uuid
+      Rails.logger.error(error_msg)
+      render json: { message: error_msg}, status: :internal_server_error
     end
   end
 end
