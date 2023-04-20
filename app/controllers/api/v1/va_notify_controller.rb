bda47ce2ec0ef982822614c45da9a718ffc58d94
j-n-t-l <611441@bah.com>
2023-04-20 15:54:52 -0400

    APPEALS-20062 added required_params

--- app/controllers/api/v1/va_notify_controller.rb
+++ app/controllers/api/v1/va_notify_controller.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 class Api::V1::VaNotifyController < Api::ApplicationController
-
   # Purpose: POST request to VA Notify API to update status for a Notification entry
   #
   # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
   #
   # Response: Update corresponding Notification status
   def notifications_update
-    if params["type"] == "email"
+    if required_params[:type] == "email"
       email_update
-    elsif params["type"] == "sms"
+    elsif required_params[:type] == "sms"
       sms_update
     end
   end
@@ -24,7 +23,8 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   # Response: json error message with uuid and 500 error
   def log_error(type)
     uuid = SecureRandom.uuid
-    error_msg = "An " + type + " notification with id " + params["id"] + " could not be found. " + "Error ID: " + uuid
+    error_msg = "An " + type + "notification with id " + required_params[:id] +
+      " could not be found. " + "Error ID: " + uuid
     Rails.logger.error(error_msg)
     render json: { message: error_msg }, status: :internal_server_error
   end
@@ -36,13 +36,13 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   # Response: Update corresponding email Notification status
   def email_update
     # find notification through external id
-    notif = Notification.find_by(email_notification_external_id: params["id"])
+    notif = Notification.find_by(email_notification_external_id: required_params[:id])
     # log external id if notification doesn't exist
-    return log_error(params["type"]) unless notif
+    return log_error(required_params[:type]) unless notif
 
     # update notification if it exists
-    notif.update!(email_notification_status: params["status"])
-    render json: { message: "Email notification successfully updated: ID " + params["id"] }
+    notif.update!(email_notification_status: required_params[:status])
+    render json: { message: "Email notification successfully updated: ID " + required_params[:id] }
   end
 
   # Purpose: Finds and updates notification if type is SMS
@@ -52,12 +52,18 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   # Response: Update corresponding SMS Notification status
   def sms_update
     # find notification through external id
-    notif = Notification.find_by(sms_notification_external_id: params["id"])
+    notif = Notification.find_by(sms_notification_external_id: required_params[:id])
     # log external id if notification doesn't exist
-    return log_error(params["type"]) unless notif
+    return log_error(required_params[:type]) unless notif
 
     # update notification if it exists
-    notif.update!(sms_notification_status: params["status"])
-    render json: { message: "SMS notification successfully updated: ID " + params["id"] }
+    notif.update!(sms_notification_status: params[:status])
+    render json: { message: "SMS notification successfully updated: ID " + required_params[:id] }
+  end
+
+  def required_params
+    id_param, type_param, status_param = params.require([:id, :type, :status])
+
+    { id: id_param, type: type_param, status: status_param }
   end
 end
