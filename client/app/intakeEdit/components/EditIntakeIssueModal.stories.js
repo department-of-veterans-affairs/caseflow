1e819fa573024cee05ba6033324b49ddd0b06066
KiMauVA <Ki.Mau@va.gov>
2023-04-24 15:07:05 -0400

    APPEALS-17507 - Updated storybook

--- client/app/intakeEdit/components/EditIntakeIssueModal.stories.js
+++ client/app/intakeEdit/components/EditIntakeIssueModal.stories.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import { EditIntakeIssueModal } from './EditIntakeIssueModal';
-
 export default {
   title: 'Intake/Edit Issues/Edit Intake Issues',
   component: EditIntakeIssueModal,
   decorators: [],
   parameters: {},
   args: {
+    currentIssue: {
+      issueIndex: 1,
+      category: 'Delimiting Date Issues | 38 U.S.C. ch. 1606',
+      description: 'Test Issue',
+      decisionDate: '04/04/2023'
+    },
+    currentIssueBenefitType: 'Education',
+    mstCheckboxValue: false,
+    pactCheckboxValue: false,
   },
   argTypes: {
     onCancel: { action: 'cancel' },
@@ -24,4 +32,5 @@ Basic.parameters = {
     storyDescription:
       'This is used to edit intake issues on the Edit Issues page',
   },
+
 };
