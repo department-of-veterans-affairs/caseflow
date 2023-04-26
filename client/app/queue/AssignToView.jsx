16123a673179e6ad15aeaf8b2fdbbe1f289e1c71
Craig Reese <109101548+craigrva@users.noreply.github.com>
2023-04-26 16:21:35 -0400

    Craig/appeals 21533 (#18506)
    * update VAMC options to sort alphabetically
    
    * fix sort to only visns, fix vamcname in getvisn

--- client/app/queue/AssignToView.jsx
+++ client/app/queue/AssignToView.jsx
@@ -174,11 +174,27 @@ class AssignToView extends React.Component {
 
   determineOptions = (actionData) => {
     if (this.isVHAAssignToRegional()) {
-      return actionData.options[this.state.assignToVHARegionalOfficeSelection];
+      const actionDataOptions = actionData.options[this.state.assignToVHARegionalOfficeSelection];
+
+      return this.state.assignToVHARegionalOfficeSelection === 'visn' ?
+        actionDataOptions : this.sortVisns(actionDataOptions);
     }
 
     return actionData.options;
   };
+
+  sortVisns = (options) => {
+    return options.sort((optA, optB) => {
+      if (optA.label < optB.label) {
+        return -1;
+      }
+      if (optA.label > optB.label) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
 
   determineDropDownLabel = (actionData) => {
     if (this.isVHAAssignToRegional()) {
@@ -223,7 +239,7 @@ class AssignToView extends React.Component {
       return actionData.options.visn.find((element) => element.value === this.state.selectedValue);
     }
 
-    const VamcName = actionData.options.vamc[this.state.selectedValue].label;
+    const VamcName = actionData.options.vamc.find((element) => element.value === this.state.selectedValue).label;
     const VisnName = VHA_VAMCS.find((element) => element.name === VamcName).visn;
 
     const VisnOption = actionData.options.visn.find((element) => element.label.includes(VisnName));
