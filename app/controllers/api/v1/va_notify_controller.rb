9ab05542157e5e26858d17ac5c7f8b31800ca718
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-05-03 09:26:29 -0400

    type to notification_type

--- app/controllers/api/v1/va_notify_controller.rb
+++ app/controllers/api/v1/va_notify_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   #
   # Response: Update corresponding Notification status
   def notifications_update
-    if required_params[:type] == "email"
+    if required_params[:notification_type] == "email"
       email_update
-    elsif required_params[:type] == "sms"
+    elsif required_params[:notification_type] == "sms"
       sms_update
     end
   end
@@ -21,10 +21,10 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   # Params:  Notification type string, either "email" or "SMS"
   #
   # Response: json error message with uuid and 500 error
-  def log_error(type)
+  def log_error(notification_type)
     uuid = SecureRandom.uuid
-    error_msg = "An " + type + "notification with id " + required_params[:id] +
-                " could not be found. " + "Error ID: " + uuid
+    error_msg = "An #{notification_type} notification with id #{required_params[:id]} could not be found. " \
+                "Error ID: #{uuid}"
     Rails.logger.error(error_msg)
     render json: { message: error_msg }, status: :internal_server_error
   end
@@ -38,7 +38,7 @@ class Api::V1::VaNotifyController < Api::ApplicationController
     # find notification through external id
     notif = Notification.find_by(email_notification_external_id: required_params[:id])
     # log external id if notification doesn't exist
-    return log_error(required_params[:type]) unless notif
+    return log_error(required_params[:notification_type]) unless notif
 
     # update notification if it exists
     notif.update!(email_notification_status: required_params[:status])
@@ -54,7 +54,7 @@ class Api::V1::VaNotifyController < Api::ApplicationController
     # find notification through external id
     notif = Notification.find_by(sms_notification_external_id: required_params[:id])
     # log external id if notification doesn't exist
-    return log_error(required_params[:type]) unless notif
+    return log_error(required_params[:notification_type]) unless notif
 
     # update notification if it exists
     notif.update!(sms_notification_status: params[:status])
@@ -62,8 +62,8 @@ class Api::V1::VaNotifyController < Api::ApplicationController
   end
 
   def required_params
-    id_param, type_param, status_param = params.require([:id, :type, :status])
+    id_param, notification_type_param, status_param = params.require([:id, :notification_type, :status])
 
-    { id: id_param, type: type_param, status: status_param }
+    { id: id_param, notification_type: notification_type_param, status: status_param }
   end
 end
