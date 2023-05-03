0f7100b430872bcea6b52b3d9339dc445fdd65b4
Prajwal Amatya <pamatya@take2it.com>
2023-05-03 03:40:41 -0600

    removed blank line, merged decision review spec into disposition spec

--- client/app/nonComp/pages/TaskPage.stories.js
+++ client/app/nonComp/pages/TaskPage.stories.js
@@ -36,7 +36,6 @@ const Template = (args) => {
       {...args}
     />
   );
-
 };
 
 export const TaskPageCompleted = Template.bind({});
--- spec/feature/non_comp/dispositions_spec.rb
+++ spec/feature/non_comp/dispositions_spec.rb
@@ -248,4 +248,64 @@ feature "NonComp Dispositions Task Page", :postgres do
       end
     end
   end
+
+  context "Decision Review Task Page for High level Claims" do
+    before do
+      User.stub = user
+      vha_org.add_user(user)
+      Timecop.travel(Time.zone.local(2023, 0o2, 0o1))
+      FeatureToggle.enable!(:decision_review_queue_ssn_column)
+    end
+
+    after do
+      FeatureToggle.disable!(:decision_review_queue_ssn_column)
+      Timecop.return
+    end
+
+    let!(:vha_org) { create(:business_line, name: "Veterans Health Administration", url: "vha") }
+    let(:user) { create(:default_user) }
+    let(:veteran) { create(:veteran) }
+    let(:decision_date) { Time.zone.now + 10.days }
+
+    let!(:in_progress_task) do
+      create(:higher_level_review, :with_vha_issue, :create_business_line, benefit_type: "vha", veteran: veteran)
+    end
+
+    let(:business_line_url) { "decision_reviews/vha" }
+    let(:dispositions_url) { "#{business_line_url}/tasks/#{in_progress_task.id}" }
+
+    it "vha decision Review workflow" do
+      step "submit button should be disabled and cancel returns back to business line" do
+        visit dispositions_url
+        expect(page).to have_button("Complete", disabled: true)
+        click_on "Cancel"
+        expect(page).to have_current_path("/#{business_line_url}", ignore_query: true)
+      end
+
+      step "completing a task should redirect to completed task tab" do
+        visit dispositions_url
+        fill_in "decision-date", with: decision_date.strftime("%m/%d/%Y")
+        fill_in_disposition(0, "Granted", "granted")
+
+        scroll_to(page, align: :bottom)
+
+        expect(page).to have_button("Complete", disabled: false)
+        click_button("Complete")
+        expect(page).to have_current_path("/#{business_line_url}?tab=completed&page=1")
+      end
+
+      step "completed Decision review task should have specific decision date provided during completion" do
+        visit dispositions_url
+        expect(page).to have_selector("h1", text: "Veterans Health Administration")
+        expect(page).to have_content(veteran.name)
+        expect(page.find("textarea").disabled?).to be true
+
+        disposition_dropdown = page.find("div.cf-select")
+        expect(disposition_dropdown).to have_content("Granted")
+        expect(disposition_dropdown).to have_css(".cf-select--is-disabled")
+        expect(page).to have_text(COPY::DISPOSITION_DECISION_DATE_LABEL)
+        expect(page.find_by_id("decision-date").value).to have_content(decision_date.strftime("%Y-%m-%d"))
+      end
+    end
+  end
 end
