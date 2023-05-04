873b3be8a5a8cccbeac655aa7864aaffd81dff68
= <tyler.broyles@va.gov>
2023-05-04 10:26:08 -0400

    Moved let variables down into the correct context block in the task_serializer_spec.rb file.

--- spec/models/serializers/work_queue/task_serializer_spec.rb
+++ spec/models/serializers/work_queue/task_serializer_spec.rb
@@ -6,24 +6,6 @@ describe WorkQueue::TaskSerializer, :postgres do
   let!(:parent) { create(:ama_task, assigned_to: user) }
   let(:days_on_hold) { 18 }
   let(:appeal) { create(:appeal) }
-  let!(:request_issues) do
-    requests = [
-      RequestIssue.create!(
-        decision_review: task.appeal,
-        decision_review_type: "Appeal",
-        nonrating_issue_category: "Caregiver",
-        type: "RequestIssue", benefit_type: "Compensation"
-      ),
-      RequestIssue.create!(
-        decision_review: task.appeal,
-        decision_review_type: "Appeal",
-        nonrating_issue_category: "Prosthetics",
-        type: "RequestIssue", benefit_type: "Compensation"
-      )
-    ]
-    task.reload
-    requests
-  end
 
   describe "#as_json" do
     subject { described_class.new(parent).serializable_hash[:data][:attributes] }
@@ -82,6 +64,24 @@ describe WorkQueue::TaskSerializer, :postgres do
 
     context "displays nonrating_issue_category" do
       let(:task) { EvidenceSubmissionWindowTask.create!(appeal: parent.appeal, assigned_to: user, parent: parent) }
+      let!(:request_issues) do
+        requests = [
+          RequestIssue.create!(
+            decision_review: task.appeal,
+            decision_review_type: "Appeal",
+            nonrating_issue_category: "Caregiver",
+            type: "RequestIssue", benefit_type: "Compensation"
+          ),
+          RequestIssue.create!(
+            decision_review: task.appeal,
+            decision_review_type: "Appeal",
+            nonrating_issue_category: "Prosthetics",
+            type: "RequestIssue", benefit_type: "Compensation"
+          )
+        ]
+        task.reload
+        requests
+      end
 
       it "returns the array of nonrating_issue_category" do
         expect(subject[:issue_types]).to include("Caregiver", "Prosthetics")
