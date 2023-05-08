0517ca59350642d2a2d4be8db0bbffedc4e115a0
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-05-08 18:39:02 -0400

    Saving version and series ref Ids whenever uploading a doc to VBMS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_164013) do
+ActiveRecord::Schema.define(version: 2023_05_08_202742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1788,6 +1788,67 @@ ActiveRecord::Schema.define(version: 2023_03_17_164013) do
     t.index ["updated_at"], name: "index_users_on_updated_at"
   end
 
+  create_table "vbms_communication_packages", force: :cascade do |t|
+    t.string "comm_package_name", null: false
+    t.datetime "created_at", null: false
+    t.bigint "created_by_id"
+    t.bigint "document_referenced", default: [], array: true
+    t.string "file_number", comment: "number associated with the documents."
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.bigint "updated_by_id"
+    t.bigint "vbms_uploaded_document_id"
+    t.index ["created_by_id"], name: "index_vbms_communication_packages_on_created_by_id"
+    t.index ["updated_by_id"], name: "index_vbms_communication_packages_on_updated_by_id"
+    t.index ["vbms_uploaded_document_id"], name: "index_vbms_communication_packages_on_vbms_uploaded_document_id"
+  end
+
+  create_table "vbms_distribution_destinations", force: :cascade do |t|
+    t.string "address_line_1", null: false, comment: "PII. If destination_type is domestic, international, or military then Must not be null."
+    t.string "address_line_2", comment: "PII. If treatLine2AsAddressee is [true] then must not be null"
+    t.string "address_line_3", comment: "PII. If treatLine3AsAddressee is [true] then must not be null"
+    t.string "address_line_4", comment: "PII."
+    t.string "address_line_5", comment: "PII."
+    t.string "address_line_6", comment: "PII."
+    t.string "city", comment: "PII. If type is [domestic, international, military] then Must not be null"
+    t.string "country_code", comment: "Must be exactly two-letter ISO 3166 code."
+    t.string "country_name"
+    t.datetime "created_at", null: false
+    t.bigint "created_by_id"
+    t.string "destination_type", null: false, comment: "Must be 'domesticAddress', 'internationalAddress', 'militaryAddress', 'derived', 'email', or 'sms'. Cannot be 'physicalAddress'."
+    t.string "email_address"
+    t.string "phone_number", comment: "PII."
+    t.string "postal_code"
+    t.string "state", comment: "PII. Must be exactly two-letter ISO 3166-2 code. If destination_type is domestic or military then Must not be null"
+    t.boolean "treat_line_2_as_addressee"
+    t.boolean "treat_line_3_as_addressee"
+    t.datetime "updated_at", null: false
+    t.bigint "updated_by_id"
+    t.bigint "vbms_distribution_id"
+    t.index ["created_by_id"], name: "index_vbms_distribution_destinations_on_created_by_id"
+    t.index ["updated_by_id"], name: "index_vbms_distribution_destinations_on_updated_by_id"
+    t.index ["vbms_distribution_id"], name: "index_vbms_distribution_destinations_on_vbms_distribution_id"
+  end
+
+  create_table "vbms_distributions", force: :cascade do |t|
+    t.string "claimant_station_of_jurisdiction", comment: "Can't be null if [recipient_type] is ro-colocated."
+    t.datetime "created_at", null: false
+    t.bigint "created_by_id"
+    t.string "first_name", comment: "recipient's first name. If Type is [person] then it cant be null."
+    t.string "last_name", comment: "recipient's last name. If Type is [person] then it cant be null."
+    t.string "middle_name", comment: "recipient's middle name."
+    t.string "name", comment: "should only be used for non-person entity names. Not null if [recipient_type] is organization, ro-colocated, or System."
+    t.string "participant_id", comment: "recipient's participant id."
+    t.string "poa_code", comment: "Can't be null if [recipient_type] is ro-colocated. The recipients POA code"
+    t.string "recipient_type", null: false, comment: "Must be one of [person, organization, ro-colocated, System]."
+    t.datetime "updated_at", null: false
+    t.bigint "updated_by_id"
+    t.bigint "vbms_communication_package_id"
+    t.index ["created_by_id"], name: "index_vbms_distributions_on_created_by_id"
+    t.index ["updated_by_id"], name: "index_vbms_distributions_on_updated_by_id"
+    t.index ["vbms_communication_package_id"], name: "index_vbms_distributions_on_vbms_communication_package_id"
+  end
+
   create_table "vbms_uploaded_documents", force: :cascade do |t|
     t.bigint "appeal_id", comment: "Appeal/LegacyAppeal ID; use as FK to appeals/legacy_appeals"
     t.string "appeal_type", comment: "'Appeal' or 'LegacyAppeal'"
@@ -1795,8 +1856,10 @@ ActiveRecord::Schema.define(version: 2023_03_17_164013) do
     t.datetime "canceled_at", comment: "Timestamp when job was abandoned"
     t.datetime "created_at", null: false
     t.string "document_name"
+    t.string "document_series_reference_id", comment: "UUID that is provided by eFolder that represents the group of documentsthis document belongs to. Think of a series as a stack of versions."
     t.string "document_subject"
     t.string "document_type", null: false
+    t.string "document_version_reference_id", comment: "UUID that is provided by eFolder that represents the specific version of the document."
     t.string "error"
     t.datetime "last_submitted_at"
     t.datetime "processed_at"
@@ -2067,6 +2130,15 @@ ActiveRecord::Schema.define(version: 2023_03_17_164013) do
   add_foreign_key "unrecognized_appellants", "users", column: "created_by_id"
   add_foreign_key "user_quotas", "team_quotas"
   add_foreign_key "user_quotas", "users"
+  add_foreign_key "vbms_communication_packages", "users", column: "created_by_id"
+  add_foreign_key "vbms_communication_packages", "users", column: "updated_by_id"
+  add_foreign_key "vbms_communication_packages", "vbms_uploaded_documents"
+  add_foreign_key "vbms_distribution_destinations", "users", column: "created_by_id"
+  add_foreign_key "vbms_distribution_destinations", "users", column: "updated_by_id"
+  add_foreign_key "vbms_distribution_destinations", "vbms_distributions"
+  add_foreign_key "vbms_distributions", "users", column: "created_by_id"
+  add_foreign_key "vbms_distributions", "users", column: "updated_by_id"
+  add_foreign_key "vbms_distributions", "vbms_communication_packages"
   add_foreign_key "virtual_hearing_establishments", "virtual_hearings"
   add_foreign_key "virtual_hearings", "users", column: "created_by_id"
   add_foreign_key "virtual_hearings", "users", column: "updated_by_id"
