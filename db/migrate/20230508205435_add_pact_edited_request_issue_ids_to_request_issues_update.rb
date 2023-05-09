2ada0b485a06e3b48501e5001f862b87547a7d43
William Medders <medders_william@bah.com>
2023-05-09 08:56:12 -0500

    Refactor: combine migrations for RequestIssuesUpdate into 1 migration

--- db/migrate/20230508205435_add_pact_edited_request_issue_ids_to_request_issues_update.rb
+++
@@ -1,5 +0,0 @@
-class AddPactEditedRequestIssueIdsToRequestIssuesUpdate < ActiveRecord::Migration[5.2]
-  def change
-    add_column :request_issues_updates, :pact_edited_request_issue_ids, :integer, comment: "An array of the request issue IDs that were updated to be associated with PACT in request issues update", array: true
-  end
-end
