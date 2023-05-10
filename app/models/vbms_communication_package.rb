9e2a4ce64ab2e8ed26b63837405568e723ea1ff3
Jeff Marks <jeff.t.marks@gmail.com>
2023-05-10 14:46:39 -0400

    Create validations for VbmsCommunicationPackage (minus validations for document_referenced)

--- app/models/vbms_communication_package.rb
+++ app/models/vbms_communication_package.rb
@@ -2,4 +2,17 @@
 class VbmsCommunicationPackage < CaseflowRecord
   has_one :vbms_uploaded_document
   has_many :vbms_distributions
+
+  validates :file_number, :comm_package_name, :document_referenced, presence: true
+
+  # file_number format can be validated with bgs_service.fetch_verteran_info(file_number)
+  #   – Would this validation be more appropriate in PacMan controller and not here?
+
+  # PacMan docs suggested multiline ^ and $ anchors for regex, but I changed to \A and \Z as suggested by rails log
+  validates :comm_package_name, format: { with: /\A[\w !*+,-.:;=?]{1,225}\Z/ }
+
+  # document_referenced has current data type of array of bigint values
+  #   - if you try and store object with "id" and "copies" keys into db it will be converted to nil object
+  #   - need to solve before being able to validate "id" and "copies"
+  validates :document_referenced, length: { minimum: 1 }
 end
