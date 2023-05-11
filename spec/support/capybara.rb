36668b00bd8d1c2fc6138f62ba0961024c6de940
Prajwal Amatya <pamatya@take2it.com>
2023-05-11 03:07:07 -0600

    reverting to see if error exists

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -53,7 +53,6 @@ Capybara.register_driver(:sniffybara_headless) do |app|
   chrome_options.args << "--headless"
   chrome_options.args << "--disable-gpu"
   chrome_options.args << "--window-size=1200,1200"
-  chrome_options.args << "--disable-dev-shm-usage"
 
   options = {
     service: ::Selenium::WebDriver::Service.chrome(args: { port: 51_674 }),
