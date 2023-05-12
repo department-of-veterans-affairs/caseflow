00ab925dcb6dccbc589e49e7ff6b3039cfdc4f37
Jeff Marks <jeff.t.marks@gmail.com>
2023-05-12 16:01:44 -0400

    Fixed formatting on VbmsDistribution

--- app/models/vbms_distribution.rb
+++ app/models/vbms_distribution.rb
@@ -5,7 +5,7 @@ class VbmsDistribution < CaseflowRecord
   has_one :vbms_distribution_destination
 
   with_options presence: true do
-    validates :recipient_type, inclusion: { in: %w(organization person system ro-colocated)}
+    validates :recipient_type, inclusion: { in: %w(organization person system ro-colocated) }
     validates :first_name, :last_name, if: :is_person?
     validates :name, unless: :is_person?
     validates :poa_code, :claimant_station_of_jurisdiction, if: :is_ro_colocated?
