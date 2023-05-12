0f1df2b4f831b98ebd3b080aebb72247f37ba153
Jeff Marks <jeff.t.marks@gmail.com>
2023-05-12 10:40:38 -0400

    Refactored unit tests for VbmsCommunicationPackage

--- spec/models/vbms_communication_package_spec.rb
+++ spec/models/vbms_communication_package_spec.rb
@@ -4,7 +4,7 @@ describe VbmsCommunicationPackage, :postgres do
   let(:package) do
     VbmsCommunicationPackage.new(
       file_number: "329780002",
-      comm_package_name: "Test Package Name",
+      comm_package_name: "test package name",
       vbms_uploaded_document: class_double(VbmsUploadedDocument)
     )
   end
@@ -13,33 +13,25 @@ describe VbmsCommunicationPackage, :postgres do
     expect(package).to be_valid
   end
 
-  it "is not valid with nil filenumber" do
+  it "is not valid without a filenumber" do
     package.filenumber = nil
     expect(package).to_not be_valid
   end
 
-  it "is not valid with empty string as filenumber" do
-    package.filenumber = ""
-    expect(package).to_not be_valid
-  end
-
-  it "is not valid with nil communication package name" do
+  it "is not valid without a communication package name" do
     package.comm_package_name = nil
     expect(package).to_not be_valid
   end
 
-  it "is not valid with empty string as communication package name" do
-    package.comm_package_name = ""
-    expect(package).to_not be_valid
-  end
-
   it "is not valid if communication package name exceeds 255 characters" do
-    package.comm_package_name = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    invalid_package_name = ""
+    256.times { invalid_package_name << "x" }
+    package.comm_package_name = invalid_package_name
     expect(package).to_not be_valid
   end
 
   it "is not valid without a user friendly communication package name" do
-    package.comm_package_name = "(Test Package Name)"
+    package.comm_package_name = "(test package name with parentheses)"
     expect(package).to_not be_valid
   end
 
