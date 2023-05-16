2c3c6a588ca96fbd51408ef64da486497b94d25d
j-n-t-l <611441@bah.com>
2023-05-16 18:35:19 -0400

    APPEALS-21118 extra logging and added rspec file

--- app/jobs/mail_request_job.rb
+++ app/jobs/mail_request_job.rb
@@ -7,13 +7,15 @@ class MailRequestJob < CaseflowJob
 
   def perform(vbms_comm_package)
     package_response = ExternalApi::PacmanService.send_communication_package_request(vbms_comm_package.file_number,
-                                                                            vbms_comm_package.comm_package_name,
-                                                                            vbms_comm_package.document_referenced)
+                                                                                     vbms_comm_package.comm_package_name,
+                                                                                     vbms_comm_package.document_referenced)
+    log_info(package_response)
     if package_response.code == 201
       vbms_comm_package.update!(status: "success")
-      distribution_response = create_distribution(vbms_comm_package.id)
+      create_distribution(vbms_comm_package.id)
     else
       vbms_comm_package.update!(status: "error")
+      log_error(error_msg(package_response.code))
     end
   end
 
@@ -21,12 +23,32 @@ class MailRequestJob < CaseflowJob
     dist = VbmsDistribution.find_by(vbms_communication_package_id: package_id)
     dist_dest = VbmsDistributionDestination.find_by(dist.id)
     distribution = ExternalApi::PacmanService.send_distribution_request(package_id, dist[:recipient], Array(dist_dest))
+    log_info(distribution)
+    if distribution.code != 201
+      log_error(error_msg(distribution.code))
+    end
     distribution
   end
 
-  def log_error(error)
+  def log_error(error_msg)
     uuid = SecureRandom.uuid
-    Rails.logger.error(error.to_s + "Error ID: " + uuid)
-    Raven.capture_exception(error, extra: { error_uuid: uuid })
+    Rails.logger.error(error_msg + "Error ID: " + uuid)
+    Raven.capture_exception(error_msg, extra: { error_uuid: uuid })
+  end
+
+  def error_msg(code)
+    if code == 400
+      "400 PacmanBadRequestError The server cannot create the new communication package due to a client error "
+    elsif code == 403
+      "403 PacmanForbiddenError The server cannot create the new communication package due to insufficient privileges."
+    elsif code == 404
+      "404 PacmanNotFoundError The communication package could not be found but may be available again in the future.
+       Subsequent requests by the client are permissible. "
+    end
+  end
+
+  def log_info(info_message)
+    uuid = SecureRandom.uuid
+    Rails.logger.info(info_message + "ID: " + uuid)
   end
 end
