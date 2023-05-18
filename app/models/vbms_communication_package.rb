d0c020fbab5d9b7f4c9b7784ff037870bc9441bc
Jeff Marks <jeff.t.marks@gmail.com>
2023-05-18 16:14:21 -0400

    Added validations for copies and removed document_referenced

--- app/models/vbms_communication_package.rb
+++ app/models/vbms_communication_package.rb
@@ -4,10 +4,7 @@ class VbmsCommunicationPackage < CaseflowRecord
   belongs_to :vbms_uploaded_document, optional: false
   has_many :vbms_distributions
 
-  validates :file_number, :comm_package_name, :document_referenced, presence: true
+  validates :file_number, :comm_package_name, presence: true
   validates :comm_package_name, length: { in: 1..255 }, format: { with: /\A[\w !*+,-.:;=?]{1,255}\Z/ }
-
-  validates :document_referenced, length: { minimum: 1 }
-  #   - document_referenced has current data type of array of bigint values
-  #   - need to change before being able to validate nested values of "id" and "copies"
+  # validates :copies, length: { in: 1..500 }
 end
