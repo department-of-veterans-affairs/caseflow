404bf8e63ab6d5026e0884ebd9aa4f1964952741
= <tyler.broyles@va.gov>
2023-05-19 15:31:45 -0400

    Removed the camoTasks data file since it wasn't working in the storybook file anyhow. Cleaned up the UnassignedCasesPage storybook file. It still does not have task data in it.

--- client/app/queue/UnassignedCasesPage.stories.js
+++ client/app/queue/UnassignedCasesPage.stories.js
@@ -1,11 +1,8 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import UnassignedCasesPage from './UnassignedCasesPage';
 import queueReducer, { initialState } from './reducers';
 import ReduxBase from 'app/components/ReduxBase';
 import { initialState as uiState } from 'app/queue/uiReducer/uiReducer';
-import { amaTasksSplit } from '../../test/data/tasks';
-import { target } from 'glamor';
-import { camoTasks } from '../../test/data/camoTasks';
 
 const vhaProgramOffices = {
   data: [
@@ -52,69 +49,17 @@ const vhaProgramOffices = {
   ]
 };
 
-const FullReduxDecorator = (Story, options) => {
+const ReduxDecorator = (Story, options) => {
   const state = initialState;
   const { args } = options;
 
   state.ui = uiState;
 
-  state.ui.userCssId = 'TYLERNAVTEST';
-
-  state.ui.userInfo = {
-    id: 3943,
-    cssId: 'TYLERNAVTEST',
-    fullName: 'Tyler Broyles',
-    displayName: 'TYLERNAVTEST (VACO)'
-  };
+  state.ui.userCssId = 'TESTUSER';
 
   state.vhaProgramOffices = vhaProgramOffices;
 
-  state.amaTasks = camoTasks;
-
-  // state.tasks = amaTasksSplit;
-
-  console.log('initial state is:');
-
-  // state.ui = {};
-
-  // state.intake.formType = 'appeal';
-  // if (args.formName) {
-  //   state.intake.formType = snakeCase(args.formName);
-  // }
-
-  // Set up all state variables ahead of time so swapping formName doesn't cause issues
-  // state.appeal.isStarted = 'STARTED';
-  // state.appeal.relationships = relationships;
-  // state.higherLevelReview.isStarted = 'STARTED';
-  // state.higherLevelReview.relationships = relationships;
-  // state.supplementalClaim.isStarted = 'STARTED';
-  // state.supplementalClaim.relationships = relationships;
-  // state.rampRefiling.isStarted = 'STARTED';
-  // state.rampRefiling.relationships = relationships;
-  // state.rampElection.isStarted = 'STARTED';
-  // state.rampElection.relationships = relationships;
-
-  // if (args.featureToggles.vhaClaimReviewEstablishment) {
-  //   state.featureToggles.vhaClaimReviewEstablishment = args.featureToggles.vhaClaimReviewEstablishment;
-  // }
-
-  // if (args.userIsCamoEmployee) {
-  //   console.log(state);
-  //   console.log(args.userIsCamoEmployee);
-  //   // state.userInformation.userIsVhaEmployee = args.userIsVhaEmployee;
-  //   state.ui.userIsCamoEmployee = args.userIsCamoEmployee;
-  //   // state.userIsCamoEmployee = args.userIsCamoEmployee;
-  // }
-
-  // if (args.userIsCamoEmployee) {
-  //   state.amaTasks = camoTasks;
-  // } else {
-  //   state.amaTasks = [];
-  // }
-
   state.ui.userIsCamoEmployee = args.userIsCamoEmployee;
-
-  console.log(state);
 
   return <ReduxBase reducer={queueReducer} initialState={state}>
     <Story />
@@ -124,11 +69,9 @@ const FullReduxDecorator = (Story, options) => {
 export default {
   title: 'Queue/Bulk Assign/Unassigned Cases Page',
   component: UnassignedCasesPage,
-  decorators: [FullReduxDecorator],
+  decorators: [ReduxDecorator],
   parameters: {},
-  // args: defaultArgs,
-  // args: { tasks: Object.values(camoTasks).map((value) => (value)) },
-  args: { tasks: [1, 2, 3] },
+  args: {},
   argTypes: {
     userIsCamoEmployee: {
       control: { type: 'boolean' }
@@ -137,18 +80,7 @@ export default {
 };
 
 const Template = (args) => {
-  // useMemo(() => {
-  //   if (args.formName) {
-  //     const formKey = snakeCase(args.formName);
-
-  //     args.formHeader = headerMappings[formKey];
-  //     args.schema = schemaMappings(args.featureToggles)[formKey];
-  //   }
-  // }, [args.formName]);
-
   return <UnassignedCasesPage {...args} />;
 };
 
 export const Normal = Template.bind({});
-// AllIntakes.args = defaultArgs;
-// AllIntakes.decorators = [FullReduxDecorator, RouterDecorator];
