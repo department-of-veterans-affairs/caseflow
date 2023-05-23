3a5c8e9707662c0ae27ef12121d911192bcc273b
breedbah <brandon.reed3@va.gov>
2023-05-23 08:07:36 -0400

    added feature toggle to enable_features_dev

--- scripts/enable_features_dev.rb
+++ scripts/enable_features_dev.rb
@@ -57,7 +57,8 @@ end
 disabled_flags = [
   "legacy_das_deprecation",
   "cavc_dashboard_workflow",
-  "poa_auto_refresh"
+  "poa_auto_refresh",
+  "cc_vacatur_visibility"
 ]
 
 all_features = AllFeatureToggles.new.call.flatten.uniq
