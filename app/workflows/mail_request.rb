f317e155644a0e2bb06a5ea4041a37db167aade6
Jonathan Cohen <cohen_jonathan@bah.com>
2023-05-24 10:00:55 -0400

    removed email/phone number param checks and validation logic.

--- app/workflows/mail_request.rb
+++ app/workflows/mail_request.rb
@@ -8,11 +8,10 @@ class MailRequest
   :participant_id, :poa_code, :claimant_station_of_jurisdiction, :destination_type,
   :address_line_1, :address_line_2, :address_line_3, :address_line_4, :address_line_5,
   :address_line_6, :city, :country_code, :postal_code, :state, :treat_line_2_as_addressee,
-  :treat_line_3_as_addressee, :country_name, :email_address, :phone_number
+  :treat_line_3_as_addressee, :country_name
 
   with_options presence: true do
     validates :recipient_type, if: :recipient_type_valid?
-
     validates :first_name, :last_name, if: :person?
     validates :poa_code, :claimant_station_of_jurisdiction, if: :ro_colocated?
     validates :destination_type, if: :destination_type_valid?
@@ -21,11 +20,9 @@ class MailRequest
     validates :address_line_3, if: :line_3_addressee?
     validates :state, :postal_code, if: :us_address?
     validates :country_name, if: :country_name_required?
-    validates :email_address, if: :email_required?
-    validates :phone_number, if: :phone_number_required?
   end
   validate :name, unless: :person?
-  # code that checks if a vbms_distribution with held params are valid?
+
 
   EXPECTED_PARAMS_FOR_RECIPIENTS_AND_DESTINATIONS = [
                                                       :recipient_type,
@@ -50,8 +47,7 @@ class MailRequest
                                                       :treat_line_2_as_addressee,
                                                       :treat_line_3_as_addressee,
                                                       :country_name,
-                                                      :email_address,
-                                                      :phone_number
+                                                      :vbms_communication_package
                                                     ].freeze
 
   def initialize(params)
@@ -78,14 +74,18 @@ class MailRequest
     @treat_line_2_as_addressee = @params[:treat_line_2_as_addressee]
     @treat_line_3_as_addressee = @params[:treat_line_3_as_addressee]
     @country_name = @params[:country_name]
-    @email_address = @params[:email_address]
-    @phone_number = @params[:phone_number]
+    @vbms_communication_package = @params[:vbms_communication_package]
+    @vbms_distribution_id = nil
+    @comm_package_id = nil
   end
 
   def call
     if valid?
-      create_a_vbms_distribution
-      create_a_vbms_distribution_destination
+      distribution = create_a_vbms_distribution
+      @vbms_distribution_id = distribution.id
+      byebug
+      destination = create_a_vbms_distribution_destination
+      byebug
     else
       raise Caseflow::Error::MissingRecipientInfo
     end
@@ -102,76 +102,60 @@ class MailRequest
   private
 
   def recipient_type_valid?
-    unless @params[:recipient_type].blank?
-      valid_var = %w[organization person system ro-colocated].include?(
-        @params[:recipient_type].slice(1, @params[:recipient_type].length - 2))
+    unless @recipient_type.blank?
+      valid_var = %w[organization person system ro-colocated].include?(@recipient_type)
       valid_var
     end
   end
 
   def person?
-    unless @params[:recipient_type].blank?
-      @params[:recipient_type].slice(1, @params[:recipient_type].length - 2) == "person"
+    unless @recipient_type.blank?
+      @recipient_type == "person"
     end
   end
 
   def ro_colocated?
-    unless @params[:recipient_type].blank?
-      @params[:recipient_type].slice(1, @params[:recipient_type].length - 2) == "ro-colocated"
+    unless @recipient_type.blank?
+      @recipient_type == "ro-colocated"
     end
   end
 
   def destination_type_valid?
-    unless @params[:destination_type].blank?
+    unless @destination_type.blank?
       %w[domesticAddress internationalAddress militaryAddress derived email sms].include?(
-        @params[:destination_type].slice(1, @params[:destination_type].length - 2)
+        @destination_type
       )
     end
   end
 
   def physical_mail?
-    unless @params[:destination_type].blank?
-      %w[domesticAddress internationalAddress militaryAddress].include?(
-        @params[:destination_type].slice(1, @params[:destination_type].length - 2)
-      )
+    unless @destination_type.blank?
+      %w[domesticAddress internationalAddress militaryAddress].include?(@destination_type)
+
     end
   end
 
   def line_2_addressee?
-    unless @params[:treat_line_2_as_addressee].blank?
-      @params[:treat_line_2_as_addressee] == true
+    unless @treat_line_2_as_addressee.blank?
+      @treat_line_2_as_addressee == true
     end
   end
 
   def line_3_addressee?
-    unless @params[:treat_line_3_as_addressee].blank?
-      @params[:treat_line_3_as_addressee] == true
+    unless @treat_line_3_as_addressee.blank?
+      @treat_line_3_as_addressee == true
     end
   end
 
   def country_name_required?
-    unless @params[:destination_type].blank?
-      @params[:destination_type].slice(1, @params[:destination_type].length - 2) == "internationalAddress"
+    unless @destination_type.blank?
+      @destination_type == "internationalAddress"
     end
   end
 
   def us_address?
-    unless @params[:destination_type].blank?
-      %w[domesticAddress militaryAddress].include?(
-        @params[:destination_type].slice(1, @params[:destination_type].length - 2)
-      )
-    end
-  end
-
-  def email_required?
-    unless @params[:destination_type].blank?
-      @params[:destination_type].slice(1, @params[:destination_type].length - 2) == "email"
-    end
-  end
-
-  def phone_number_required?
-    unless @params[:destination_type].blank?
-      @params[:destination_type].slice(1, @params[:destination_type].length - 2) == "sms"
+    unless @destination_type.blank?
+      %w[domesticAddress militaryAddress].include?(@destination_type)
     end
   end
 
@@ -185,14 +169,14 @@ class MailRequest
       address_line_5: @address_line_5,
       address_line_6: @address_line_6,
       city: @city,
-      country_code: @country,
+      country_code: @country_code,
       postal_code: @postal_code,
       state: @state,
       treat_line_2_as_addressee: @treat_line_2_as_addressee,
       treat_line_3_as_addressee: @treat_line_3_as_addressee,
       country_name: @country_name,
-      email_address: @email_address,
-      phone_number: @phone_number
+      vbms_distribution_id: @vbms_distribution_id
+
     }
   end
 
@@ -206,8 +190,8 @@ class MailRequest
       participant_id: @participant_id,
       poa_code: @poa_code,
       claimant_station_of_jurisdiction: @claimant_station_of_jurisdiction
+      # vbms_communication_package_id: 3
+
     }
   end
-
-
 end
