900b3ea9eefd0a9375d0f16c6e626536d3faa809
Jonathan Cohen <cohen_jonathan@bah.com>
2023-05-25 14:47:53 -0400

    factories for valid mail request object and invalid mail request object created.

--- spec/factories/mail_request.rb
+++ spec/factories/mail_request.rb
@@ -2,11 +2,30 @@
 
 FactoryBot.define do
   factory :valid_mail_request do
-
+    recipient_type { "person" }
+    first_name { "Bob" }
+    last_name  { "Smithcole" }
+    participant_id { 640_460_002 }
+    destination_type { "domesticAddress" }
+    address_line_1 { "1234 Main Street" }
+    city { "Orlando" }
+    country_code { "US"}
+    postal_code { "12345" }
+    state { "FL" }
+    treat_line_2_as_addressee { false }
+    treat_line_3_as_addressee { false }
   end
 
   factory :invalid_mail_request do
-
+    recipient_type { "person" }
+    first_name { "Bob" }
+    participant_id { 640_460_002 }
+    address_line_1 { "1234 Main Street" }
+    city { "Orlando" }
+    country_code { "US"}
+    postal_code { 12_345 }
+    state { "FL" }
+    treat_line_2_as_addressee { false }
+    treat_line_3_as_addressee { false }
   end
 end
-
