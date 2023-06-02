686d1a83fcbf2a907731657d4a6f0b77f8cac0bf
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-02 14:01:32 -0400

    Parent swap

--- app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-class HearingPostponementRequestMailTask < MailTask
+class HearingPostponementRequestMailTask < HearingRequestMailTask
 end
--- app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-class HearingWithdrawalRequestMailTask < MailTask
+class HearingWithdrawalRequestMailTask < HearingRequestMailTask
 end
