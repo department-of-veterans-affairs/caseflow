7f69ac45241c3c616076491c78036b4709b19377
j-n-t-l <611441@bah.com>
2023-06-02 14:17:42 -0400

    APPEALS-21118 updated create_distribution with recipient and destination hashes

--- app/jobs/mail_request_job.rb
+++ app/jobs/mail_request_job.rb
@@ -3,9 +3,7 @@
 class MailRequestJob < CaseflowJob
   queue_with_priority :low_priority
   application_attr :intake
-# copies based on number of recipients
-# But then how to get document reference id?
-# is that even needed still? if not, pacman service needs to be changed
+
   def perform(vbms_uploaded_document, mail_request)
     package_response = ExternalApi::PacmanService.send_communication_package_request(vbms_uploaded_document.veteran_file_number,
                                                                                      mail_request.name,
@@ -37,48 +35,51 @@ class MailRequestJob < CaseflowJob
     )
   end
 
-  def create_distribution(package_id, mail_request)
-    VbmsDistribution.new(
-      claimant_station_of_jurisdiction: mail_request.claimant_station_of_jurisdiction,
-      created_at: Time.zone.now,
-      created_by_id: "",
-      first_name: mail_request.first_name,
-      last_name: mail_request.last_name,
-      middle_name: mail_request.middle_name,
-      name: mail_request.name,
-      participant_id: mail_request.participant_id,
-      poa_code: mail_request.poa_code,
-      recipient_type: mail_request.recipient_type,
-      updated_at: Time.zone.now,
-      updated_by_id: "",
-      vbms_communication_package_id: package_id
-    )
-
-  end
-
-  def create_distribution_destinations(dist_id, mail_request)
-
-  end
-
-  def get_recipient_array(mail_request)
-
-  end
-
-  def get_destination_array(mail_request)
-
-  end
-# multiple recipients and destinations?
-# how will those be packaged in mail_request?
   def create_distribution_request(package_id, mail_request)
-    distribution_response = ExternalApi::PacmanService.send_distribution_request(package_id, dist[:recipient], Array(dist_dest))
-    log_info(distribution_response)
-    if distribution_response.code == 201
-      distribution = create_distribution(package_id, mail_request)
-      create_distribution_destinations
-    else
-      log_error(error_msg(distribution.code))
+    distributions = VbmsDistribution.find(participant_id: mail_request.participant_id)
+    distributions.each do |dist|
+      distribution_destination = VbmsDistributionDestination.find(vbms_distribution_id: dist.id)
+      distribution_response = ExternalApi::PacmanService.send_distribution_request(package_id,
+                                                                                   get_recipient_hash(distribution),
+                                                                                   get_destinations_hash(distribution_destination))
+      log_info(distribution_response)
+      if distribution_response.code == 201
+        dist.update!(vbms_communication_package_id: package_id)
+      else
+        log_error(error_msg(distribution.code))
+      end
     end
-    distribution.uuid
+  end
+
+  def get_recipient_hash(distribution)
+    {
+      type: distribution.recipient_type,
+      name: distribution.name,
+      firstName: distribution.first_name,
+      middleName: distribution.middle_name,
+      lastName: distribution.last_name,
+      participant_id: distribution.participant_id,
+      poaCode: distribution.poa_code,
+      claimantStationOfJurisdiction: distribution.claimant_station_of_jurisdiction
+    }
+  end
+
+  def get_destinations_hash(destination)
+    [{
+      "type" => destination.destination_type,
+      "addressLine1" => destination.address_line_1,
+      "addressLine2" => destination.address_line_2,
+      "addressLine3" => destination.address_line_3,
+      "addressLine4" => destination.address_line_4,
+      "addressLine5" => destination.address_line_5,
+      "addressLine6" => destination.address_line_6,
+      "city" => destination.city,
+      "state" => destination.state,
+      "postalCode" => destination.postal_code,
+      "countryName" => destination.country_name,
+      "countryCode" => destination.country_code
+    }]
+
   end
 
   def log_error(error_msg)
