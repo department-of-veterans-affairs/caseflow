c463ccaedc6716e7329948e67977a69ad8faa9d4
= <tyler.broyles@va.gov>
2023-06-05 12:16:52 -0400

    Fixed a bunch of rspec tests to have the correct number of column names.

--- client/app/components/FilterSummary.jsx
+++ client/app/components/FilterSummary.jsx
@@ -20,7 +20,6 @@ const ALTERNATE_COLUMN_NAMES = {
 const FilterSummary = ({ filteredByList, clearFilteredByList }) => {
   let filterSummary = null;
   let filterListContent = [];
-
   const clearAllFiltersLink = <button
     onClick={() => clearFilteredByList({})}
     className="cf-btn-link cf-clear-filters-link"> Clear all filters</button>;
