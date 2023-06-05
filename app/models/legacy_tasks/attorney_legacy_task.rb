47bfbf026ef9537eb2485bff2af67992d59e6531
vinner57 <128258952+vinner57@users.noreply.github.com>
2023-06-05 12:19:55 -0400

    refactoring attorney legacy task

--- app/models/legacy_tasks/attorney_legacy_task.rb
+++ app/models/legacy_tasks/attorney_legacy_task.rb
@@ -12,7 +12,7 @@ class AttorneyLegacyTask < LegacyTask
         Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h,
         Constants.TASK_ACTIONS.ASSIGN_TO_ATTORNEY_LEGACY.to_h
       ]  
-    elsif current_user && (current_user.attorney? || role == "attorney")
+    elsif current_user&.attorney? || role == "attorney"
       [
         Constants.TASK_ACTIONS.REVIEW_LEGACY_DECISION.to_h,
         Constants.TASK_ACTIONS.SUBMIT_OMO_REQUEST_FOR_REVIEW.to_h,
