929b1029faacf0ba7a9c9a4cc0afc8de8d978bc8
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-05 19:48:09 -0400

    Add labels

--- app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class HearingPostponementRequestMailTask < HearingRequestMailTask
+  def self.label
+    "Hearing postponement request"
+  end
 end
--- app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class HearingWithdrawalRequestMailTask < HearingRequestMailTask
+  def self.label
+    "Hearing withdrawal request"
+  end
 end
