489d27c6ed7f6a7adc8395e0746f1645f4d73548
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-05 22:23:55 -0400

    Add comment about orgs

--- app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_withdrawal_request_mail_task.rb
@@ -6,6 +6,16 @@ class HearingWithdrawalRequestMailTask < HearingRequestMailTask
       "Hearing withdrawal request"
     end
 
+    # Only users who are members of orgs where
+    #
+    #   def users_can_create_mail_task?
+    #     true
+    #   end
+    #
+    # can create mail tasks. Those orgs are the same 4 listed in the AC.
+    #
+    # This method is only being overridden so that HearingRequestMailTask
+    # doesn't pop up in the dropdown.
     def allow_creation?(*)
       true
     end
