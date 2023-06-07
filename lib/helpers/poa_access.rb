dc621b279c538a6cec97a3c1a1203b2ad816d5bb
raymond-hughes <raymond.hughes2@va.gov>
2023-06-07 12:11:18 -0400

    Adding spec and updating script accordingly.

--- lib/helpers/poa_access.rb
+++ lib/helpers/poa_access.rb
@@ -1,51 +1,49 @@
-=begin
-Legacy Appeals - no POA access when spouse not located in people table
-=end
-
 # frozen_string_literal: true
 
 module WarRoom
-  module PoaAccess
-    def run(file_number:)
+  class PoaAccess
+    # Legacy Appeals - no POA access when spouse not in people table
+    def initialize(appeals_id, poa_participant_id)
+      @appeals_id = appeals_id
+      @poa_participant_id = poa_participant_id
       RequestStore[:current_user] = User.system_user
+    end
 
-      appeal = Appeal.find_appeal_by_uuid_or_find_or_create_legacy_appeal_by_vacols_id(file_number)
-
-      # locate poa and verify it returns no record
-      # returns an nil record which we will need to delete later
-      poa = appeal.power_of_attorney
-
+    def run
+      # only allow records that are not_found to be remidiated
       unless poa.bgs_record == :not_found
-        puts("bgs record was found. Aborting...")
+        puts("bgs record exists. Aborting...")
         fail Interrupt
       end
 
-      # 2. Setup - Locate Participant ID.
-
-      # 3. Execute -
-
-      # Clean up
-      # clear memoization on legacy appeals
-      appeal.power_of_attorney&.try(:clear_bgs_power_of_attorney!)
-
-      poa = appeal.bgs_power_of_attorney
-
-      if poa.blank?
-        # noop
-      elsif poa.bgs_record == :not_found
-        poa.destroy!
-      end
+      # due diligence to clean up records that may have been created when looking for an existing record
+      purge_poa!
 
       # Create person record
-      person = Person.find_or_create_by_participant_id(poa_participant_id)
+      Person.find_or_create_by_participant_id(@poa_participant_id)
 
       # Create bgs POA record
       # NOTE: The POA is updated in a before_save callback. It will pull in all the attrs for the created POA
-      BgsPowerOfAttorney.find_or_create_by_claimant_participant_id(poa_participant_id)
+      BgsPowerOfAttorney.find_or_create_by_claimant_participant_id(@poa_participant_id)
 
-      # 4. Confirm fix
-      appeal = Appeal.find_appeal_by_uuid_or_find_or_create_legacy_appeal_by_vacols_id("some_file_num")
+      # Confirm fix
       appeal.bgs_power_of_attorney.present?
+    end
+
+    def appeal
+      Appeal.find_appeal_by_uuid_or_find_or_create_legacy_appeal_by_vacols_id(@appeals_id)
+    end
+
+    def poa
+      # clear memoization on legacy appeals
+      appeal.power_of_attorney&.try(:clear_bgs_power_of_attorney!)
+      appeal.bgs_power_of_attorney
+    end
+
+    private
+
+    def purge_poa!
+      poa.destroy!
     end
   end
 end
