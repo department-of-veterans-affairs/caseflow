01a7e426baf1804f40cfc9ec94180e27769b3ce3
Jonathan Tsang <tsang_jonathan@bah.com>
2023-06-08 16:43:52 -0400

    updated model comment

--- app/models/external_modals/vbms_ext_claim.rb
+++ app/models/external_modals/vbms_ext_claim.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-##
-# VbmsExtClaim records represents Vbms external claim records
-##
+# This model represents entries in the vbms_ext_claim table
+# VbmsExtClaims can have an associated EndProductEstablishment
+
 class VbmsExtClaim < CaseflowRecord
   self.table_name = "vbms_ext_claim"
   self.primary_key = "CLAIM_ID"
