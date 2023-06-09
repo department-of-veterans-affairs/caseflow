ce248753351d4ca69a6affe933b286f6afe80cab
HunJerBAH <Jeremy.Hunton@va.gov>
2023-06-09 12:45:32 -0400

    made edit mst/pact and vbms mst/pact not mandatory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_09_153757) do
+ActiveRecord::Schema.define(version: 2023_06_09_161731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1571,15 +1571,15 @@ ActiveRecord::Schema.define(version: 2023_06_09_153757) do
     t.string "created_by_css_id", null: false, comment: "CSS ID of the user that made the special issue change"
     t.bigint "created_by_id", null: false, comment: "User ID of the user that made the special issue change"
     t.bigint "issue_id", null: false, comment: "ID of the issue that was changed"
-    t.boolean "mst_from_vbms", default: false, comment: "Indication that the MST status originally came from VBMS on intake"
+    t.boolean "mst_from_vbms", comment: "Indication that the MST status originally came from VBMS on intake"
     t.string "mst_reason_for_change", comment: "Reason for changing the MST status on an issue"
     t.boolean "original_mst_status", null: false, comment: "Original MST special issue status of the issue"
     t.boolean "original_pact_status", null: false, comment: "Original PACT special issue status of the issue"
-    t.boolean "pact_from_vbms", default: false
+    t.boolean "pact_from_vbms"
     t.string "pact_reason_for_change", comment: "Reason for changing the PACT status on an issue"
     t.bigint "task_id", null: false, comment: "Task ID of the IssueUpdateTask or EstablishmentTask used to log this issue in the case timeline"
-    t.boolean "updated_mst_status", null: false, comment: "Updated MST special issue status of the issue"
-    t.boolean "updated_pact_status", null: false, comment: "Updated PACT special issue status of the issue"
+    t.boolean "updated_mst_status", comment: "Updated MST special issue status of the issue"
+    t.boolean "updated_pact_status", comment: "Updated PACT special issue status of the issue"
   end
 
   create_table "special_issue_lists", comment: "Associates special issues to an AMA or legacy appeal for Caseflow Queue. Caseflow Dispatch uses special issues stored in legacy_appeals. They are intentionally disconnected.", force: :cascade do |t|
