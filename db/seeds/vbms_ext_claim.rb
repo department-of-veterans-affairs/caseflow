ea6c6811bb3ecdeb387220c1f3be70cf97efa53c
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-06-09 14:17:18 -0700

    APPEALS-21357 Refactored Method for in_sync epe and vbms claims to reflect epe.ref_id to match vec.claim_idfor both HLR and SUP CLAIM. Make target is succesful. completeing make reset now to test local reset and reseeding

--- db/seeds/vbms_ext_claim.rb
+++ db/seeds/vbms_ext_claim.rb
@@ -5,8 +5,8 @@ module Seeds
 
   class VbmsExtClaim < Base
     def seed!
-      # create_vbms_ext_claims_with_no_end_product_establishment
-      # create_in_sync_epes_and_vbms_ext_claims
+      create_vbms_ext_claims_with_no_end_product_establishment
+      create_in_sync_epes_and_vbms_ext_claims
       create_out_of_sync_epes_and_vbms_ext_claims
     end
 
@@ -115,6 +115,102 @@ module Seeds
 	end
 
 	def create_in_sync_epes_and_vbms_ext_claims
+    # 25 High Level Review, End Product Establishments that have a sync_status of canceled and are in_sync with
+    # vbms_ext_claims
+    25.times do |n|
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
+      vec = create(:vbms_ext_claim, :canceled)
+      veteran = create(:veteran)
+      higher_level_review = create(:higher_level_review,
+                                    veteran_file_number: veteran.file_number)
+      eligible_request_issue = create(:request_issue,
+                                      decision_review: higher_level_review,
+                                      nonrating_issue_category: "Military Retired Pay",
+                                      nonrating_issue_description: "nonrating description",
+                                      ineligible_reason: nil,
+                                      benefit_type: "compensation",
+                                      decision_date: Date.new(2018, 5, 1))
+      create(:end_product_establishment,
+             :canceled,
+             source: higher_level_review,
+             reference_id: vec.claim_id,
+             veteran_file_number: veteran.file_number,
+             claimant_participant_id: veteran.participant_id)
+    end
+    # 25 High Level Review, End Product Establishments that have a sync_status of cleared and are in_sync with
+    # vbms_ext_claims
+    25.times do |n|
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
+      vec = create(:vbms_ext_claim, :cleared)
+      veteran = create(:veteran)
+      higher_level_review = create(:higher_level_review,
+                                    veteran_file_number: veteran.file_number)
+      eligible_request_issue = create(:request_issue,
+                                      decision_review: higher_level_review,
+                                      nonrating_issue_category: "Military Retired Pay",
+                                      nonrating_issue_description: "nonrating description",
+                                      ineligible_reason: nil,
+                                      benefit_type: "compensation",
+                                      decision_date: Date.new(2018, 5, 1))
+      create(:end_product_establishment,
+             :cleared,
+              source: higher_level_review,
+              reference_id: vec.claim_id,
+              veteran_file_number: veteran.file_number,
+              claimant_participant_id: veteran.participant_id)
+
+    end
+    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of cleared and are in_sync with
+    # # vbms_ext_claims
+    25.times do |n|
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
+      vec = create(:vbms_ext_claim, :cleared)
+      veteran = create(:veteran)
+      supplemental_claim = create(:supplemental_claim,
+            veteran_file_number: veteran.file_number,
+            receipt_date: Time.zone.now,
+            benefit_type: "compensation")
+      eligible_request_issue = create(:request_issue,
+                                      decision_review: supplemental_claim,
+                                      nonrating_issue_category: "Military Retired Pay",
+                                      nonrating_issue_description: "nonrating description",
+                                      ineligible_reason: nil,
+                                      benefit_type: "compensation",
+                                      decision_date: Date.new(2018, 5, 1))
+      create(:end_product_establishment,
+             :cleared,
+              source: supplemental_claim,
+              reference_id: vec.claim_id,
+              veteran_file_number: veteran.file_number,
+              claimant_participant_id: veteran.participant_id)
+
+    end
+
+    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of canceled and are in_sync with
+    # # vbms_ext_claims
+    25.times do |n|
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
+      vec = create(:vbms_ext_claim, :canceled)
+      veteran = create(:veteran)
+      supplemental_claim = create(:supplemental_claim,
+            veteran_file_number: veteran.file_number,
+            receipt_date: Time.zone.now,
+            benefit_type: "compensation")
+      eligible_request_issue = create(:request_issue,
+                                      decision_review: supplemental_claim,
+                                      nonrating_issue_category: "Military Retired Pay",
+                                      nonrating_issue_description: "nonrating description",
+                                      ineligible_reason: nil,
+                                      benefit_type: "compensation",
+                                      decision_date: Date.new(2018, 5, 1))
+      create(:end_product_establishment,
+             :canceled,
+              source: supplemental_claim,
+              reference_id: vec.claim_id,
+              veteran_file_number: veteran.file_number,
+              claimant_participant_id: veteran.participant_id)
+
+    end
 
 
 	end
