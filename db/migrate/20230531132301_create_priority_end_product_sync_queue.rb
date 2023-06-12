d5f28b697001d7f311b1a51a2852d965ed2b053e
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2023-06-12 08:56:27 -0400

    APPEALS-21353 Updated default value for status to NOT_PROCESSED.

--- db/migrate/20230531132301_create_priority_end_product_sync_queue.rb
+++ db/migrate/20230531132301_create_priority_end_product_sync_queue.rb
@@ -3,7 +3,7 @@ class CreatePriorityEndProductSyncQueue < Caseflow::Migration
     create_table :priority_end_product_sync_queue, comment: "Queue of End Product Establishments that need to sync with VBMS" do |t|
       t.integer :end_product_establishment_id, unique: true, null: false, comment: "ID of end_product_establishment record to be synced"
       t.uuid :batch_id, null: true, comment: "A unique UUID for the batch the record is executed with"
-      t.string :status, null: false, default: "PENDING", comment: "A status to indicate what state the record is in such as PROCESSING and PROCESSED"
+      t.string :status, null: false, default: "NOT_PROCESSED", comment: "A status to indicate what state the record is in such as PROCESSING and PROCESSED"
       t.timestamp :created_at, null: false, comment: "Date and Time the record was inserted into the queue"
       t.timestamp :last_batched_at, null: true, comment: "Date and Time the record was last batched"
       t.string :error_messages, array: true, default: [], comment: "Array of Error Message(s) containing Batch ID and specific error if a failure occurs"
