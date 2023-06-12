03e9b01eddb288c6e2d1da6815510079cd88c656
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-06-12 16:06:57 -0700

    APPEALS-21357 Refactored in_sync seeding method for vbms_claims to match vbms_ext_claim level_status_code and epe sync status. to mock when vbms_claims are synced with caseflows end_prod_establishments. I also comments to make sure its clear what each method and total vbms_ext_claims are being seeded as well is got rid of extra spaces

--- db/seeds/vbms_ext_claim.rb
+++ db/seeds/vbms_ext_claim.rb
@@ -4,18 +4,20 @@
 module Seeds
 
   class VbmsExtClaim < Base
+    # creates and seeds 325 total vbms_ext_claims
     def seed!
-      # create_vbms_ext_claims_with_no_end_product_establishment
-      # create_in_sync_epes_and_vbms_ext_claims
+      create_vbms_ext_claims_with_no_end_product_establishment
+      create_in_sync_epes_and_vbms_ext_claims
       create_out_of_sync_epes_and_vbms_ext_claims
     end
 
 	 private
 
+  # creates out_of_sync vbms_ext_claims and end_produdct_establishments
 	def create_out_of_sync_epes_and_vbms_ext_claims
     # 25 High Level Review, End Product Establishments that have a sync_status of cleared and are out_of_sync with
     # vbms_ext_claims
-    25.times do |n|
+    25.times do
       veteran = create(:veteran)
       # out_of_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
       vec = create(:vbms_ext_claim,
@@ -41,12 +43,10 @@ module Seeds
               code: vec.type_code,
               veteran_file_number: veteran.file_number,
               claimant_participant_id: veteran.participant_id)
-
-
     end
     # 25 High Level Review, End Product Establishments that have a sync_status of canceled and are out_of_sync with
     # vbms_ext_claims
-    25.times do |n|
+    25.times do
       veteran = create(:veteran)
       # out_of_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
       vec = create(:vbms_ext_claim,
@@ -72,11 +72,10 @@ module Seeds
              code: vec.type_code,
              veteran_file_number: veteran.file_number,
              claimant_participant_id: veteran.participant_id)
-
     end
     # # 25 Supplemental Claims, End Product Establishments that have a sync_status of cleared and are out_of_sync with
     # # vbms_ext_claims
-    25.times do |n|
+    25.times do
       veteran = create(:veteran)
       # out_of_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
       vec = create(:vbms_ext_claim,
@@ -104,12 +103,11 @@ module Seeds
              code: vec.type_code,
              veteran_file_number: veteran.file_number,
              claimant_participant_id: veteran.participant_id)
-
     end
 
-    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of canceled and are out_sync with
+    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of canceled and are out_of_sync with
     # # vbms_ext_claims
-    25.times do |n|
+    25.times do
       veteran = create(:veteran)
       # out_of_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
       vec = create(:vbms_ext_claim,
@@ -137,19 +135,20 @@ module Seeds
              code: vec.type_code,
              veteran_file_number: veteran.file_number,
              claimant_participant_id: veteran.participant_id)
-
     end
-
-
 	end
 
+  # creates in_sync vbms_ext_claims and end_produdct_establishments
 	def create_in_sync_epes_and_vbms_ext_claims
     # 25 High Level Review, End Product Establishments that have a sync_status of canceled and are in_sync with
     # vbms_ext_claims
-    25.times do |n|
-      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
-      vec = create(:vbms_ext_claim, :canceled)
+    25.times do
       veteran = create(:veteran)
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
+      vec = create(:vbms_ext_claim,
+                   :canceled,
+                   :hlr,
+                   claimant_person_id: veteran.participant_id)
       higher_level_review = create(:higher_level_review,
                                     veteran_file_number: veteran.file_number)
       eligible_request_issue = create(:request_issue,
@@ -161,17 +160,24 @@ module Seeds
                                       decision_date: Date.new(2018, 5, 1))
       create(:end_product_establishment,
              :canceled,
-             source: higher_level_review,
-             reference_id: vec.claim_id,
-             veteran_file_number: veteran.file_number,
-             claimant_participant_id: veteran.participant_id)
+              source: higher_level_review,
+              reference_id: vec.claim_id,
+              established_at: vec.establishment_date,
+              claim_date: vec.claim_date,
+              modifier: vec.ep_code,
+              code: vec.type_code,
+              veteran_file_number: veteran.file_number,
+              claimant_participant_id: veteran.participant_id)
     end
     # 25 High Level Review, End Product Establishments that have a sync_status of cleared and are in_sync with
     # vbms_ext_claims
-    25.times do |n|
-      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
-      vec = create(:vbms_ext_claim, :cleared)
+    25.times do
       veteran = create(:veteran)
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
+      vec = create(:vbms_ext_claim,
+                   :cleared,
+                   :hlr,
+                   claimant_person_id: veteran.participant_id)
       higher_level_review = create(:higher_level_review,
                                     veteran_file_number: veteran.file_number)
       eligible_request_issue = create(:request_issue,
@@ -183,18 +189,24 @@ module Seeds
                                       decision_date: Date.new(2018, 5, 1))
       create(:end_product_establishment,
              :cleared,
-              source: higher_level_review,
-              reference_id: vec.claim_id,
-              veteran_file_number: veteran.file_number,
-              claimant_participant_id: veteran.participant_id)
-
+             source: higher_level_review,
+             reference_id: vec.claim_id,
+             established_at: vec.establishment_date,
+             claim_date: vec.claim_date,
+             modifier: vec.ep_code,
+             code: vec.type_code,
+             veteran_file_number: veteran.file_number,
+             claimant_participant_id: veteran.participant_id)
     end
     # # 25 Supplemental Claims, End Product Establishments that have a sync_status of cleared and are in_sync with
     # # vbms_ext_claims
-    25.times do |n|
-      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
-      vec = create(:vbms_ext_claim, :cleared)
+    25.times do
       veteran = create(:veteran)
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
+      vec = create(:vbms_ext_claim,
+                   :cleared,
+                   :slc,
+                   claimant_person_id: veteran.participant_id)
       supplemental_claim = create(:supplemental_claim,
             veteran_file_number: veteran.file_number,
             receipt_date: Time.zone.now,
@@ -208,19 +220,25 @@ module Seeds
                                       decision_date: Date.new(2018, 5, 1))
       create(:end_product_establishment,
              :cleared,
-              source: supplemental_claim,
-              reference_id: vec.claim_id,
-              veteran_file_number: veteran.file_number,
-              claimant_participant_id: veteran.participant_id)
-
+             source: supplemental_claim,
+             reference_id: vec.claim_id,
+             established_at: vec.establishment_date,
+             claim_date: vec.claim_date,
+             modifier: vec.ep_code,
+             code: vec.type_code,
+             veteran_file_number: veteran.file_number,
+             claimant_participant_id: veteran.participant_id)
     end
 
-    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of canceled and are in_sync with
+    # # 25 Supplemental Claims, End Product Establishments that have a sync_status of canceled and are out_sync with
     # # vbms_ext_claims
-    25.times do |n|
-      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CLR"
-      vec = create(:vbms_ext_claim, :canceled)
+    25.times do
       veteran = create(:veteran)
+      # in_sync vbms_ext_claim LEVEL_STATUS_CODE "CAN"
+      vec = create(:vbms_ext_claim,
+                   :canceled,
+                   :slc,
+                   claimant_person_id: veteran.participant_id)
       supplemental_claim = create(:supplemental_claim,
             veteran_file_number: veteran.file_number,
             receipt_date: Time.zone.now,
@@ -234,16 +252,18 @@ module Seeds
                                       decision_date: Date.new(2018, 5, 1))
       create(:end_product_establishment,
              :canceled,
-              source: supplemental_claim,
-              reference_id: vec.claim_id,
-              veteran_file_number: veteran.file_number,
-              claimant_participant_id: veteran.participant_id)
-
+             source: supplemental_claim,
+             reference_id: vec.claim_id,
+             established_at: vec.establishment_date,
+             claim_date: vec.claim_date,
+             modifier: vec.ep_code,
+             code: vec.type_code,
+             veteran_file_number: veteran.file_number,
+             claimant_participant_id: veteran.participant_id)
     end
-
-
 	end
 
+  # creates 124 vbms_ext_claims not associated with an end_product_establishment
 	def create_vbms_ext_claims_with_no_end_product_establishment
     # creates 50 none epe assocated vbms_ext_claims with LEVEL_STATUS_CODE "CLR"
     50.times do
