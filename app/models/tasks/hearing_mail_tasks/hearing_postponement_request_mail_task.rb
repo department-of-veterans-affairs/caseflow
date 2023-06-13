cffc658a5f1d78b307053dfbdfdae55f754d1a5e
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-12 22:57:18 -0400

    Some hearing task tomfoolery

--- app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
@@ -23,4 +23,54 @@ class HearingPostponementRequestMailTask < HearingRequestMailTask
   def available_actions(_user)
     TASK_ACTIONS
   end
+
+  def update_from_params(params, current_user)
+    if params[:status] == Constants.TASK_STATUSES.completed
+      multi_transaction do
+        update!(
+          status: Constants.TASK_STATUSES.completed,
+          completed_by: current_user
+        )
+
+        cancel_existing_hearing_tasks!(current_user)
+
+        create_new_root_hearing_task!
+      end
+    end
+  end
+
+  private
+
+  # Cancels HearingTasks and HearingPostponementRequestMailTasks
+  def cancel_existing_hearing_tasks!(current_user)
+    # How would this need to change for legacy appeals?
+    appeal.tasks.active.each do |task|
+      next unless [HearingTask.name, name].include?(task.type)
+
+      task.update!(
+        status: Constants.TASK_STATUSES.cancelled,
+        cancelled_by: current_user
+      )
+    end
+  end
+
+  # Kind of just guessing here and putting the new HearingTask
+  # under the same parent of the most recent HearingTask in the tree.
+  #
+  #
+  def create_new_root_hearing_task!
+    most_recent_hearing_task = locate_most_recent_hearing_task
+
+    if most_recent_hearing_task
+      HearingTask.create!(
+        appeal: appeal,
+        parent: most_recent_hearing_task.parent,
+        assigned_to: Bva.singleton
+      )
+    end
+  end
+
+  def locate_most_recent_hearing_task
+    appeal.tasks.where(type: HearingTask.name).order(created_at: :desc).first
+  end
 end
