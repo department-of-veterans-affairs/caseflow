260dbea91d1438d423159e1825347347114c4025
Jonathan Cohen <cohen_jonathan@bah.com>
2023-06-13 12:02:37 -0400

    changes made to mail_request syntax.

--- app/workflows/mail_request.rb
+++ app/workflows/mail_request.rb
@@ -7,35 +7,10 @@ class MailRequest
   include MailRequestValidator::Distribution
   include MailRequestValidator::DistributionDestination
 
-  attr_accessor :recipient_type, :name, :first_name, :middle_name, :last_name,
-                :participant_id, :poa_code, :claimant_station_of_jurisdiction, :destination_type,
-                :address_line_1, :address_line_2, :address_line_3, :address_line_4, :address_line_5,
-                :address_line_6, :city, :country_code, :postal_code, :state, :treat_line_2_as_addressee,
-                :treat_line_3_as_addressee, :country_name, :vbms_distribution_id, :comm_package_id
+  attr_reader :vbms_distribution_id, :comm_package_id
 
   def initialize(recipient_and_destination_hash)
-    @recipient_type = recipient_and_destination_hash[:recipient_type]
-    @name = recipient_and_destination_hash[:name]
-    @first_name = recipient_and_destination_hash[:first_name]
-    @middle_name = recipient_and_destination_hash[:middle_name]
-    @last_name = recipient_and_destination_hash[:last_name]
-    @participant_id = recipient_and_destination_hash[:participant_id]
-    @poa_code = recipient_and_destination_hash[:poa_code]
-    @claimant_station_of_jurisdiction = recipient_and_destination_hash[:claimant_station_of_jurisdiction]
-    @destination_type = recipient_and_destination_hash[:destination_type]
-    @address_line_1 = recipient_and_destination_hash[:address_line_1]
-    @address_line_2 = recipient_and_destination_hash[:address_line_2]
-    @address_line_3 = recipient_and_destination_hash[:address_line_3]
-    @address_line_4 = recipient_and_destination_hash[:address_line_4]
-    @address_line_5 = recipient_and_destination_hash[:address_line_5]
-    @address_line_6 = recipient_and_destination_hash[:address_line_6]
-    @city = recipient_and_destination_hash[:city]
-    @country_code = recipient_and_destination_hash[:country_code]
-    @postal_code = recipient_and_destination_hash[:postal_code]
-    @state = recipient_and_destination_hash[:state]
-    @treat_line_2_as_addressee = recipient_and_destination_hash[:treat_line_2_as_addressee]
-    @treat_line_3_as_addressee = recipient_and_destination_hash[:treat_line_3_as_addressee]
-    @country_name = recipient_and_destination_hash[:country_name]
+    @recipient_info = recipient_and_destination_hash
     @vbms_distribution_id = nil
     @comm_package_id = nil
   end
@@ -62,35 +37,129 @@ class MailRequest
 
   def destination_params_parse
     {
-      destination_type: @destination_type,
-      address_line_1: @address_line_1,
-      address_line_2: @address_line_2,
-      address_line_3: @address_line_3,
-      address_line_4: @address_line_4,
-      address_line_5: @address_line_5,
-      address_line_6: @address_line_6,
-      city: @city,
-      country_code: @country_code,
-      postal_code: @postal_code,
-      state: @state,
-      treat_line_2_as_addressee: @treat_line_2_as_addressee,
-      treat_line_3_as_addressee: @treat_line_3_as_addressee,
-      country_name: @country_name,
-      vbms_distribution_id: @vbms_distribution_id
+      destination_type: destination_type,
+      address_line_1: address_line_1,
+      address_line_2: address_line_2,
+      address_line_3: address_line_3,
+      address_line_4: address_line_4,
+      address_line_5: address_line_5,
+      address_line_6: address_line_6,
+      city: city,
+      country_code: country_code,
+      postal_code: postal_code,
+      state: state,
+      treat_line_2_as_addressee: treat_line_2_as_addressee,
+      treat_line_3_as_addressee: treat_line_3_as_addressee,
+      country_name: country_name,
+      vbms_distribution_id: vbms_distribution_id
 
     }
   end
 
   def recipient_params_parse
     {
-      recipient_type: @recipient_type,
-      name: @name,
-      first_name: @first_name,
-      middle_name: @middle_name,
-      last_name: @last_name,
-      participant_id: @participant_id,
-      poa_code: @poa_code,
-      claimant_station_of_jurisdiction: @claimant_station_of_jurisdiction
+      recipient_type: recipient_type,
+      name: name,
+      first_name: first_name,
+      middle_name: middle_name,
+      last_name: last_name,
+      participant_id: participant_id,
+      poa_code: poa_code,
+      claimant_station_of_jurisdiction: claimant_station_of_jurisdiction
     }
+  end
+
+  def recipient_type
+    @recipient_info[:recipient_type]
+  end
+
+  def name
+    @recipient_info[:name]
+  end
+
+  def first_name
+    @recipient_info[:first_name]
+  end
+
+  def middle_name
+    @recipient_info[:middle_name]
+  end
+
+  def last_name
+    @recipient_info[:last_name]
+  end
+
+  def participant_id
+    @recipient_info[:participant_id]
+  end
+
+  def poa_code
+    @recipient_info[:poa_code]
+  end
+
+  def claimant_station_of_jurisdiction
+    @recipient_info[:claimant_station_of_jurisdiction]
+  end
+
+  def destination_type
+    @recipient_info[:destination_type]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_1
+    @recipient_info[:address_line_1]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_2
+    @recipient_info[:address_line_2]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_3
+    @recipient_info[:address_line_3]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_4
+    @recipient_info[:address_line_4]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_5
+    @recipient_info[:address_line_5]
+  end
+
+  # :reek:UncommunicativeMethodName
+  def address_line_6
+    @recipient_info[:address_line_6]
+  end
+
+  def city
+    @recipient_info[:city]
+  end
+
+  def country_code
+    @recipient_info[:country_code]
+  end
+
+  def postal_code
+    @recipient_info[:postal_code]
+  end
+
+  def state
+    @recipient_info[:state]
+  end
+
+  def treat_line_2_as_addressee
+    @recipient_info[:treat_line_2_as_addressee]
+  end
+
+  def treat_line_3_as_addressee
+    @recipient_info[:treat_line_3_as_addressee]
+  end
+
+  def country_name
+    @recipient_info[:country_name]
   end
 end
