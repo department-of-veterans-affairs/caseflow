b7adf8537425071ae64e5f0009c353f74ab5d38e
sulak-j <95875751+sulak-j@users.noreply.github.com>
2023-06-16 12:09:55 -0500

    disabling feature toggle covering justification (#18806)

--- scripts/enable_features_dev.rb
+++ scripts/enable_features_dev.rb
@@ -57,7 +57,8 @@ end
 disabled_flags = [
   "legacy_das_deprecation",
   "cavc_dashboard_workflow",
-  "poa_auto_refresh"
+  "poa_auto_refresh",
+  "justification_reason"
 ]
 
 all_features = AllFeatureToggles.new.call.flatten.uniq
