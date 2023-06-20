bd5f764bccf72ced1087c90594d639b614232aed
SHarshain <133917878+SHarshain@users.noreply.github.com>
2023-06-20 13:55:01 -0400

    APPEALS-23475. Record Successhandling in the metrics (#18774)
    * APPEALS-23475. Record Successhandling in the metrics
    
    * Added successhandling on other HTTP methods and some refactor
    
    * Added feature toggle- metricsLogRestSuccess
    
    * APPEALS-23475. Test case for success handling
    
    * reverting changes
    
    ---------
    
    Co-authored-by: SHarshain <spoosa@razortalent.com>

--- client/app/util/ApiUtil.js
+++ client/app/util/ApiUtil.js
@@ -5,6 +5,7 @@ import StringUtil from './StringUtil';
 import uuid from 'uuid';
 import _ from 'lodash';
 import { timeFunctionPromise } from '../util/PerfDebug';
+import moment from 'moment';
 
 export const STANDARD_API_TIMEOUT_MILLISECONDS = 60 * 1000;
 export const RESPONSE_COMPLETE_LIMIT_MILLISECONDS = 5 * 60 * 1000;
@@ -47,6 +48,9 @@ const errorHandling = (url, error, method, options = {}) => {
   const message = `UUID: ${id}.\nProblem with ${method} ${url}.\n${error}`;
 
   console.error(new Error(message));
+  options.t1 = performance.now();
+  options.end = moment().format();
+  options.duration = options.t1 - options.t0;
 
   // Need to renable this check before going to master
   // if (options?.metricsLogRestError) {
@@ -63,31 +67,80 @@ const errorHandling = (url, error, method, options = {}) => {
         error
       }),
       sent_to: 'javascript_console',
+      start: options.start,
+      end: options.end,
+      duration: options.duration,
     }
   };
 
-  request.
-    post('/metrics/v2/logs').
-    set(getHeadersObject()).
-    send(data).
-    use(nocache).
-    on('error', (err) => console.error(`Metric not recorded\nUUID: ${uuid.v4()}.\n: ${err}`)).
-    end();
+ ApiUtil.postMetricLogs('/metrics/v2/logs', { data: data });
   // }
+};
+
+const successHandling = (url, res, method, options = {}) => {
+  const id = uuid.v4();
+  const message = `UUID: ${id}.\nSuccess with ${method} ${url}.\n${res.status}`;
+
+  // Need to renable this check before going to master
+  options.t1 = performance.now();
+  options.end = moment().format();
+  options.duration = options.t1 - options.t0;
+
+  // if (options?.metricsLogRestSuccess) {
+  const data = {
+    metric: {
+      uuid: id,
+      name: `caseflow.client.rest.${method.toLowerCase()}.info`,
+      message,
+      type: 'info',
+      product: 'caseflow',
+      metric_attributes: JSON.stringify({
+        method,
+        url
+      }),
+      sent_to: 'javascript_console',
+      sent_to_info: JSON.stringify({
+        metric_group: "Rest call",
+        metric_name: "Javascript request",
+        metric_value: options.duration,
+        app_name: "JS reader",
+        attrs: {
+          service: "rest service",
+          endpoint: url,
+          uuid: id
+        }
+      }),
+
+      start: options.start,
+      end: options.end,
+      duration: options.duration,
+    }
+  };
+
+  ApiUtil.postMetricLogs('/metrics/v2/logs', { data: data });
 };
 
 const httpMethods = {
   delete(url, options = {}) {
+    options.t0 = performance.now();
+    options.start = moment().format();
+
     return request.
       delete(url).
       set(getHeadersObject(options.headers)).
       send(options.data).
       use(nocache).
-      on('error', (err) => errorHandling(url, err, 'DELETE', options));
+      on('error', (err) => errorHandling(url, err, 'DELETE', options)).
+      then(res => {
+        successHandling(url, res, 'DELETE', options);
+        return res;
+      });
   },
 
   get(url, options = {}) {
     const timeoutSettings = Object.assign({}, defaultTimeoutSettings, _.get(options, 'timeout', {}));
+    options.t0 = performance.now();
+    options.start = moment().format();
 
     let promise = request.
       get(url).
@@ -109,34 +162,69 @@ const httpMethods = {
     }
 
     return promise.
-      use(nocache);
+      use(nocache).
+      then(res => {
+        successHandling(url, res, 'GET', options);
+        return res;
+      });
   },
 
   patch(url, options = {}) {
+    options.t0 = performance.now();
+    options.start = moment().format();
+
     return request.
       post(url).
       set(getHeadersObject({ 'X-HTTP-METHOD-OVERRIDE': 'patch' })).
       send(options.data).
       use(nocache).
-      on('error', (err) => errorHandling(url, err, 'PATCH', options));
+      on('error', (err) => errorHandling(url, err, 'PATCH', options)).
+      then(res => {
+        successHandling(url, res, 'PATCH', options);
+        return res;
+      });
   },
 
   post(url, options = {}) {
+    options.t0 = performance.now();
+    options.start = moment().format();
+
     return request.
       post(url).
       set(getHeadersObject(options.headers)).
       send(options.data).
       use(nocache).
-      on('error', (err) => errorHandling(url, err, 'POST', options));
+      on('error', (err) => errorHandling(url, err, 'POST', options)).
+      then(res => {
+        successHandling(url, res, 'POST', options);
+        return res;
+      });
   },
 
   put(url, options = {}) {
+    options.t0 = performance.now();
+    options.start = moment().format();
+
     return request.
       put(url).
       set(getHeadersObject(options.headers)).
       send(options.data).
       use(nocache).
-      on('error', (err) => errorHandling(url, err, 'PUT', options));
+      on('error', (err) => errorHandling(url, err, 'PUT', options)).
+      then(res => {
+        successHandling(url, res, 'PUT', options);
+        return res;
+      });
+  },
+
+  postMetricLogs(url, options = {}) {
+    return request.
+      post('/metrics/v2/logs').
+      set(getHeadersObject()).
+      send(options.data).
+      use(nocache).
+      on('error', (err) => console.error(`Metric not recorded\nUUID: ${uuid.v4()}.\n: ${err}`)).
+      end();
   }
 };
 
