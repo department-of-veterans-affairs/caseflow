332bb79d558f1ae7513e99e1a009f864c59533e8
William Medders <medders_william@bah.com>
2023-06-21 08:22:10 -0500

    WIP refactor CaseReviewsController#update_request_issues_for_mst_and_pact

--- app/controllers/case_reviews_controller.rb
+++ app/controllers/case_reviews_controller.rb
@@ -54,8 +54,10 @@ class CaseReviewsController < ApplicationController
   def update_request_issues_for_mst_and_pact(appeal)
     params[:tasks][:issues].each do |issue|
       RequestIssue.find(issue[:request_issue_ids]).each do |ri|
-        if ri.mst_status != issue[:mstStatus] || ri.pact_status != issue[:pactStatus]
-          create_issue_update_task(ri, issue, appeal)
+        if !issue[:mstStatus].nil? && !issue[:pactStatus].nil?
+          if ri.mst_status != issue[:mstStatus] || ri.pact_status != issue[:pactStatus]
+            create_issue_update_task(ri, issue, appeal)
+          end
         end
         ri.update(mst_status: issue[:mstStatus], pact_status: issue[:pactStatus])
       end
