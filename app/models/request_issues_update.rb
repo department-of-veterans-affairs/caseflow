8e228f295a8eb1706c1fcf39be795b3215ea4e23
HunJerBAH <Jeremy.Hunton@va.gov>
2023-06-26 11:10:49 -0400

    commented out code to allow multiple edits

--- app/models/request_issues_update.rb
+++ app/models/request_issues_update.rb
@@ -177,14 +177,14 @@ class RequestIssuesUpdate < CaseflowRecord
   end
 
   def validate_before_perform
-    if !changes?
-      @error_code = :no_changes
-    elsif RequestIssuesUpdate.where(review: review).where.not(id: id).processable.exists?
-      if @error_code == :no_changes
-        RequestIssuesUpdate.where(review: review).where.not(id: id).processable.last.destroy
-      end
-      @error_code = :previous_update_not_done_processing
-    end
+    # if !changes?
+    #   @error_code = :no_changes
+    # elsif RequestIssuesUpdate.where(review: review).where.not(id: id).processable.exists?
+    #   if @error_code == :no_changes
+    #     RequestIssuesUpdate.where(review: review).where.not(id: id).processable.last.destroy
+    #   end
+    #   @error_code = :previous_update_not_done_processing
+    # end
 
     !@error_code
   end
