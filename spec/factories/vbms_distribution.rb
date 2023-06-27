ff2ad6866a8504ee9cc9378bf20e777b7eb4aaec
j-n-t-l <611441@bah.com>
2023-06-27 16:43:40 -0400

    APPEALS-21118 fixed vbms factories

--- spec/factories/vbms_distribution.rb
+++ spec/factories/vbms_distribution.rb
@@ -2,18 +2,18 @@
 
 FactoryBot.define do
   factory :vbms_distribution do
-    claimant_station_of_jurisdiction
-    created_at
-    created_by_id
-    first_name
-    last_name
-    middle_name
-    name
-    participant_id
-    poa_code
-    recipient_type
-    updated_at
-    updated_by_id
-    vbms_communication_package_id
+    claimant_station_of_jurisdiction { nil }
+    created_at { Time.zone.now}
+    created_by_id { nil }
+    first_name { "Bob" }
+    last_name { "Bobjoe" }
+    middle_name { "Joe" }
+    name { nil }
+    participant_id { generate :participant_id}
+    poa_code { nil }
+    recipient_type { "person" }
+    updated_at { Time.zone.now }
+    updated_by_id { nil }
+    vbms_communication_package_id { "673c8b4a-cb7d-4fdf-bc4d-998d6d5d7431" }
   end
 end
