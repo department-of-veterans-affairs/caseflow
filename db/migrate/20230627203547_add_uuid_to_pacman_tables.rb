d0e710235153718f276bfaca7463e19a7c0e0bac
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-27 16:38:50 -0400

    DB migration to add UUID columns to Pacman tables

---
+++ db/migrate/20230627203547_add_uuid_to_pacman_tables.rb
@@ -0,0 +1,24 @@
+class AddUuidToPacmanTables < Caseflow::Migration
+  def up
+    add_column :vbms_communication_packages,
+      :uuid,
+      :string,
+      comment: "UUID of the communication package in Package Manager (Pacman)"
+
+    add_column :vbms_distribution_destinations,
+      :uuid,
+      :string,
+      comment: "UUID of the distrubtion destination in Package Manager (Pacman)"
+
+    add_column :vbms_distributions,
+      :uuid,
+      :string,
+      comment: "UUID of the distrubtion in Package Manager (Pacman)"
+  end
+
+  def down
+    remove_column :vbms_communication_packages, :uuid
+    remove_column :vbms_distribution_destinations, :uuid
+    remove_column :vbms_distributions, :uuid
+  end
+end
