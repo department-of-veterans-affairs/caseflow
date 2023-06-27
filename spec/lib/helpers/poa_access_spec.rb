e3908c1bbd7136b725327e2ef0734abc67a0ce03
raymond-hughes <raymond.hughes2@va.gov>
2023-06-27 10:04:36 -0400

    Update spec to correctly build a new poa and use the new poa's pid in the test

--- spec/lib/helpers/poa_access_spec.rb
+++ spec/lib/helpers/poa_access_spec.rb
@@ -3,21 +3,23 @@
 require "helpers/poa_access"
 
 describe "WarRoom::PoaAccess" do
+  before { FeatureToggle.enable!(:poa_auto_refresh) }
+  after { FeatureToggle.disable!(:poa_auto_refresh) }
+
   let(:appeal) { create(:legacy_appeal, vacols_case: create(:case)) }
+  let(:poa) { create(:bgs_power_of_attorney, claimant_participant_id: "2", poa_participant_id: "1") }
 
   context "when run against a legacy appeal" do
-
-    subject { WarRoom::PoaAccess.new(appeal.vacols_id, "fake-pid").run }
-
-    it "interrupts when poa is present" do
+    subject { WarRoom::PoaAccess.new(appeal.vacols_id, "not-used").run }
+    it "interrupts when poa is found" do
       expect { subject }.to raise_error do |error|
         expect(error).to be_a(Interrupt)
       end
     end
 
-    subject { WarRoom::PoaAccess.new(appeal.vacols_id, "").run }
-
-    it "creates a person record for spouse" do
+    subject { WarRoom::PoaAccess.new(appeal.vacols_id, poa.claimant_participant_id).run }
+    it "creates a person record for spouse when poa :not_found" do
+      # To avoid adding to the large amount of dummy data lets just coerce the record to be :not_found
       allow_any_instance_of(BgsPowerOfAttorney).to receive(:bgs_record).and_return(:not_found)
 
       expect(subject).to be true
