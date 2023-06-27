11ebd187956071c400ba8a94120f690ef3a2cd5e
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-06-27 05:55:42 -0400

    ✨ Introduce rake task remediations:cancel_vrr_tasks_open_for_vre

---
+++ spec/lib/tasks/remediations/cancel_vrr_tasks_open_for_vre_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+describe "remediations/cancel_vrr_tasks_open_for_vre" do
+  include_context "rake"
+
+  describe "remediations:cancel_vrr_tasks_open_for_vre" do
+    it "delegates to CancelTasksAndDescendants" do
+      task_relation = double("task_relation")
+
+      expect(CancelTasksAndDescendants::VeteranRecordRequestsOpenForVREQuery).
+        to receive(:call).and_return(task_relation)
+
+      expect(CancelTasksAndDescendants).to receive(:call).with(task_relation)
+
+      Rake::Task["remediations:cancel_vrr_tasks_open_for_vre"].invoke
+    end
+  end
+end
