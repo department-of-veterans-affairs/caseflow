9fc2908f3be3681050dfc1203e477b165c216ca0
j-n-t-l <611441@bah.com>
2023-06-27 11:56:46 -0400

    APPEALS-21118 fixed current_user in rspec

--- spec/jobs/mail_request_job_spec.rb
+++ spec/jobs/mail_request_job_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 describe MailRequestJob do
-  let!(:user) { create(:user, id: 1) }
+  let!(:current_user) { User.authenticate! }
   let!(:vbms_file) { create(:vbms_uploaded_document) }
   let!(:mail_request) { build(:mail_request) }
   context "successful " do
     it "creates a new VbmsCommunicationPackage" do
       mail_request.call
-      mail_package = { distributions: [mail_request.to_json], copies: 1, created_by_id: user.id }
+      mail_package = { distributions: [mail_request.to_json], copies: 1, created_by_id: 1 }
       MailRequestJob.perform_now(vbms_file, mail_package)
       expect { MailRequestJob.perform_now(vbms_file, mail_package) }.to change { VbmsCommunicationPackage.count }.by(1)
       expect(VbmsCommunicationPackage.first.status).to eq("success")
