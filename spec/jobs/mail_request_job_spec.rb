195965b1c2ef82530d7924b872dc06e568bb11de
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-27 23:19:01 -0400

    Try new mail_request_spec setup

--- spec/jobs/mail_request_job_spec.rb
+++ spec/jobs/mail_request_job_spec.rb
@@ -1,20 +1,30 @@
 # frozen_string_literal: true
 
 describe MailRequestJob do
+  include ActiveJob::TestHelper
+
   let!(:current_user) { User.authenticate! }
   let!(:vbms_file) { create(:vbms_uploaded_document) }
   let!(:mail_request) { build(:mail_request) }
+
   context "successful " do
     it "creates a new VbmsCommunicationPackage" do
       mail_request.call
       mail_package = { distributions: [mail_request.to_json], copies: 1, created_by_id: 1 }
-      MailRequestJob.perform_now(vbms_file, mail_package)
-      expect { MailRequestJob.perform_now(vbms_file, mail_package) }.to change { VbmsCommunicationPackage.count }.by(1)
-      expect(VbmsCommunicationPackage.first.status).to eq("success")
+
+      expect do
+        perform_enqueued_jobs { MailRequestJob.perform_later(vbms_file, mail_package) }
+      end.to change { VbmsCommunicationPackage.count }.by(1)
+
+      expect(
+        find_comm_package_via_distribution_id(mail_request.vbms_distribution_id).status
+      ).to eq("success")
     end
   end
-  # context "400 error in package request" do
-  #   it "does not create a new VbmsCommunicationPackage"
-  #   expect { MailRequestJob.perform_now(vbms_file, nil) }.to change { VbmsCommunicationPackage.count }.by(0)
-  # end
+
+  def find_comm_package_via_distribution_id(distro_id)
+    distribution = VbmsDistribution.find(distro_id)
+
+    VbmsCommunicationPackage.find(distribution.vbms_communication_package_id)
+  end
 end
