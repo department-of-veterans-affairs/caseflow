ea6e3dcb574a156a6444c7f68cbdc146e35c06e4
Craig Reese <Reese_Craig@bah.com>
2023-06-28 13:08:52 -0500

    fix error in previous conflict resolution

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_202742) do
+ActiveRecord::Schema.define(version: 2023_05_08_224138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
