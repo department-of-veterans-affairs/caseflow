d8a0e679e28a11b419efa19c09e038b23e8d7cbd
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-28 16:58:21 -0400

    Spec test tweaks

--- spec/jobs/mail_request_job_spec.rb
+++ spec/jobs/mail_request_job_spec.rb
@@ -5,12 +5,16 @@ describe MailRequestJob do
   let!(:current_user) { User.authenticate! }
   let!(:vbms_file) { create(:vbms_uploaded_document) }
   let!(:mail_request) { build(:mail_request) }
-  context "successful " do
+
+  context "Successful execution of MailRequestJob" do
     it "creates a new VbmsCommunicationPackage" do
       mail_request.call
       mail_package = { distributions: [mail_request.to_json], copies: 1, created_by_id: current_user.id }
-      MailRequestJob.perform_now(vbms_file, mail_package)
-      expect { MailRequestJob.perform_now(vbms_file, mail_package) }.to change { VbmsCommunicationPackage.count }.by(1)
+
+      expect do
+        perform_enqueued_jobs { MailRequestJob.perform_later(vbms_file, mail_package) }
+      end.to change { VbmsCommunicationPackage.count }.by(1)
+
       expect(find_comm_package_via_distribution_id(mail_request.vbms_distribution_id).status).to eq("success")
     end
   end
