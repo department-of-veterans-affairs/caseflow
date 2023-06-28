dbbd77878bd4e2b874cfaf168c34087a1d3b09d2
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-06-27 23:21:55 -0400

    Update comm package factory

--- spec/factories/vbms_communication_package.rb
+++ spec/factories/vbms_communication_package.rb
@@ -2,15 +2,15 @@
 
 FactoryBot.define do
   factory :vbms_communication_package do
-    comm_package_name { nil }
+    association :vbms_uploaded_document, factory: :vbms_uploaded_document
+
+    created_by_id { create(:user).id }
+    comm_package_name { Faker::Book.title }
     created_at { Time.zone.now }
-    created_by_id { nil }
-    document_referenced { nil }
-    file_number { nil }
+    file_number { generate :veteran_file_number }
     status { nil }
     updated_at { Time.zone.now }
     updated_by_id { nil }
-    vbms_uploaded_document_id { nil }
     uuid { nil }
   end
 end
