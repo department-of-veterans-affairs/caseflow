79050e111ea4c216a554f38c085cee991f4f565c
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-06-28 17:34:21 -0500

    typo fix

--- spec/feature/queue/judge_checkout_flow_spec.rb
+++ spec/feature/queue/judge_checkout_flow_spec.rb
@@ -818,7 +818,7 @@ RSpec.feature "Judge checkout flow", :all_dbs do
         check("Military Sexual Trauma (MST)", allow_label_click: true, visible: false)
         check("PACT Act", allow_label_click: true, visible: false)
 
-        click on "Continue"
+        click_on "Continue"
 
         all("a", text: "Edit Issue")[1].click
         uncheck("Military Sexual Trauma (MST)", allow_label_click: true, visible: false)
