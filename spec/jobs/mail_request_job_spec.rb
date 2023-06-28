8cc772e97d13eac025fbc983341c36b7450fb6c3
j-n-t-l <611441@bah.com>
2023-06-28 15:00:05 -0400

    APPEALS-21118 changed rspec

--- spec/jobs/mail_request_job_spec.rb
+++ spec/jobs/mail_request_job_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 describe MailRequestJob do
+  include ActiveJob::TestHelper
   let!(:current_user) { User.authenticate! }
   let!(:vbms_file) { create(:vbms_uploaded_document) }
   let!(:mail_request) { build(:mail_request) }
@@ -10,7 +11,12 @@ describe MailRequestJob do
       mail_package = { distributions: [mail_request.to_json], copies: 1, created_by_id: 1 }
       MailRequestJob.perform_now(vbms_file, mail_package)
       expect { MailRequestJob.perform_now(vbms_file, mail_package) }.to change { VbmsCommunicationPackage.count }.by(1)
-      expect(VbmsCommunicationPackage.first.status).to eq("success")
+      expect(find_comm_package_via_distribution_id(mail_request.vbms_distribution_id).status).to eq("success")
     end
+  end
+  def find_comm_package_via_distribution_id(distro_id)
+    distribution = VbmsDistribution.find(distro_id)
+
+    VbmsCommunicationPackage.find(distribution.vbms_communication_package_id)
   end
 end
