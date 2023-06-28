db6154f17b5c58a832c419329c71f33c00a505f7
j-n-t-l <611441@bah.com>
2023-06-28 12:52:35 -0400

    APPEALS-21118 fixed vbms comm package factory

--- spec/factories/vbms_communication_package.rb
+++ spec/factories/vbms_communication_package.rb
@@ -2,14 +2,14 @@
 
 FactoryBot.define do
   factory :vbms_communication_package do
-    comm_package_name { nil }
+    association :vbms_uploaded_document, factory: :vbms_uploaded_document
+    comm_package_name { "DocumentName_" + Time.zone.now.to_s }
+    copies { 1 }
     created_at { Time.zone.now }
-    created_by_id { nil }
-    document_referenced { nil }
-    file_number { nil }
+    created_by_id { create(:user).id }
+    file_number { generate :veteran_file_number }
     status { nil }
     updated_at { Time.zone.now }
     updated_by_id { nil }
-    vbms_uploaded_document_id { nil }
   end
 end
