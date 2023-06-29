ec3b23ab36d5d9252b438aed3fe72628a8b92095
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2023-06-28 21:51:50 -0400

    APPEALS-22705 Refactor of Batch Process and BatchProcessPriorityEPSyncJob.

--- app/jobs/batch_processes/batch_process_priority_ep_sync_job.rb
+++ app/jobs/batch_processes/batch_process_priority_ep_sync_job.rb
@@ -1,13 +1,28 @@
+# frozen_string_literal: true
+
 class BatchProcessPriorityEPSyncJob < CaseflowJob
   queue_with_priority :low_priority
-  application_attr :queue
+
+  before_perform do |job|
+    JOB_ATTR = job
+  end
 
   def perform
     begin
-      created_batch = BatchProcess.batch_priority_end_product_sync!
-      BatchProcess.process_priority_end_product_sync!(created_batch)
+      batch = ActiveRecord::Base.transaction do
+        records_to_batch = BatchProcess.find_records_to_batch
+        next unless records_to_batch.any?
+
+        BatchProcess.build_priority_end_product_sync_batch!(records_to_batch)
+      end
+      if batch
+        batch.process_priority_end_product_sync!
+      else
+        Rails.logger.info("No Records Available to Batch.  Time: #{Time.zone.now}")
+      end
     rescue StandardError => error
-      capture_exception(error: error)
+      Rails.logger.error("Error: #{error.inspect}, Job ID: #{JOB_ATTR&.job_id}, Job Time: #{Time.zone.now}")
+      capture_exception(error: error, extra: { job_id: JOB_ATTR&.job_id.to_s, job_time: Time.zone.now.to_s })
     end
   end
 end
--- app/models/priority_queues/priority_end_product_sync_queue.rb
+++ app/models/priority_queues/priority_end_product_sync_queue.rb
@@ -6,4 +6,18 @@ class PriorityEndProductSyncQueue < CaseflowRecord
   self.table_name = "priority_end_product_sync_queue"
 
   belongs_to :end_product_establishment
+  belongs_to :batch_process, foreign_key: "batch_id"
+  has_one :caseflow_stuck_records, as: :stuck_record
+
+  def synced_status!
+    update!(status: "SYNCED")
+  end
+
+  def unbatch!(errors)
+    update!(batch_id: nil, status: "ERROR", error_messages: errors)
+  end
+
+  def stuck!
+    CaseflowStuckRecord.create!(stuck_record: self, error_messages: error_messages, determined_stuck_at: Time.zone.now)
+  end
 end
