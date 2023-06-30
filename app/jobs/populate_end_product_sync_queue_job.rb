3fc3ad3b5c8022d69833d26f8043a1e04a8e8b46
nkirby-va <nicholas.kirby@va.gov>
2023-06-30 11:04:50 -0400

    update query logic

--- app/jobs/populate_end_product_sync_queue_job.rb
+++ app/jobs/populate_end_product_sync_queue_job.rb
@@ -25,14 +25,14 @@ class PopulateEndProductSyncQueueJob < CaseflowJob
 
   def find_priority_end_product_establishments_to_sync
     get_batch = <<-SQL
-      select id
-        from end_product_establishments
-        inner join vbms_ext_claim
-        on end_product_establishments.reference_id=vbms_ext_claim."CLAIM_ID"::varchar
-        where (end_product_establishments.synced_status not in ('CLR','CAN') or end_product_establishments.synced_status is null)
-          and vbms_ext_claim."LEVEL_STATUS_CODE" in ('CLR','CAN')
-          and end_product_establishments.id not in (select end_product_establishment_id from priority_end_product_sync_queue)
-        limit #{ENV["END_PRODUCT_QUEUE_BATCH_LIMIT"]};
+    select id
+      from end_product_establishments
+      inner join vbms_ext_claim
+      on end_product_establishments.reference_id = vbms_ext_claim."CLAIM_ID"::varchar
+      where end_product_establishments.synced_status <> vbms_ext_claim."LEVEL_STATUS_CODE"
+        and vbms_ext_claim."LEVEL_STATUS_CODE" in ('CLR','CAN')
+        and end_product_establishments.id not in (select end_product_establishment_id from priority_end_product_sync_queue)
+      limit #{ENV["END_PRODUCT_QUEUE_BATCH_LIMIT"]};
     SQL
 
     ActiveRecord::Base.connection.exec_query(get_batch).rows.flatten
