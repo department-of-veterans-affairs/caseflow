680e259da1c7fec41ae837663ba824a466fa20ef
raymond-hughes <raymond.hughes2@va.gov>
2023-06-30 09:36:45 -0400

    Finalize POA test

--- spec/lib/helpers/poa_access_spec.rb
+++ spec/lib/helpers/poa_access_spec.rb
@@ -3,16 +3,21 @@
 require "helpers/poa_access"
 
 describe "WarRoom::PoaAccess" do
-  let(:appeal) { create(:legacy_appeal, vacols_case: create(:case)) }
-  let(:poa) { create(:bgs_power_of_attorney) }
+  let!(:legacy_appeal) { create(:legacy_appeal, vacols_case: create(:case)) }
+  let!(:poa) { create(:bgs_power_of_attorney) }
 
   context "when run against a legacy appeal" do
-    subject { WarRoom::PoaAccess.new(appeal.vacols_id, poa.claimant_participant_id).run }
-    it "creates a person record for spouse when poa :not_found" do
+    it "creates a person record" do
       # To avoid adding to the large amount of dummy data lets just coerce the record to be :not_found
       allow_any_instance_of(BgsPowerOfAttorney).to receive(:bgs_record).and_return(:not_found)
 
-      expect(subject).to be true
+      expect(Person).to receive(:find_or_create_by_participant_id).with(poa.claimant_participant_id)
+
+      expect(BgsPowerOfAttorney).to receive(:find_or_create_by_claimant_participant_id).with(poa.claimant_participant_id)
+
+      remediation_success = WarRoom::PoaAccess.new(legacy_appeal.vacols_id, poa.claimant_participant_id).run
+
+      expect(remediation_success).to be true
     end
   end
 end
