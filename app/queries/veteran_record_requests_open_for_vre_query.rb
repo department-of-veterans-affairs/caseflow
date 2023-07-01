2791072402e8b6ce146f7fd2b2d9e22e71c7f185
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-06-30 23:01:15 -0400

    :bulb: Add to comment

--- app/queries/veteran_record_requests_open_for_vre_query.rb
+++ app/queries/veteran_record_requests_open_for_vre_query.rb
@@ -4,7 +4,8 @@ class VeteranRecordRequestsOpenForVREQuery
   VRE_BUSINESS_LINE_NAME = "Veterans Readiness and Employment"
 
   # @return [ActiveRecord::Relation] VeteranRecordRequest tasks that are
-  #   both open and assigned to the VRE business line
+  #   both open and assigned to the 'Veterans Readiness and Employment' business
+  #   line (f.k.a 'Vocational Rehabilitation and Employment')
   def self.call
     vre_business_line = BusinessLine.where(name: VRE_BUSINESS_LINE_NAME)
     VeteranRecordRequest.open.where(assigned_to: vre_business_line)
