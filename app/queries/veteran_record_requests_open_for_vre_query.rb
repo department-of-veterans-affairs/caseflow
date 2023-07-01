fd89a81b5018d5fa64328a521c35388afd0a0836
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-06-30 23:02:52 -0400

    :recycle: Reference Constants::BENEFIT_TYPES instead of local constant

--- app/queries/veteran_record_requests_open_for_vre_query.rb
+++ app/queries/veteran_record_requests_open_for_vre_query.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class VeteranRecordRequestsOpenForVREQuery
-  VRE_BUSINESS_LINE_NAME = "Veterans Readiness and Employment"
-
   # @return [ActiveRecord::Relation] VeteranRecordRequest tasks that are
   #   both open and assigned to the 'Veterans Readiness and Employment' business
   #   line (f.k.a 'Vocational Rehabilitation and Employment')
   def self.call
-    vre_business_line = BusinessLine.where(name: VRE_BUSINESS_LINE_NAME)
+    vre_business_line =
+      BusinessLine.where(name: Constants::BENEFIT_TYPES["voc_rehab"])
+
     VeteranRecordRequest.open.where(assigned_to: vre_business_line)
   end
 end
