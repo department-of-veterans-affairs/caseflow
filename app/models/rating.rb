089274d66863248f184e08dc9092a3ec9cc95da5
HunJerBAH <Jeremy.Hunton@va.gov>
2023-07-10 15:57:48 -0400

    added next for nil payload

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -108,6 +108,8 @@ class Rating
       serialized_hash[:rba_contentions_data].each do |rba|
         rba_contention = rba.with_indifferent_access
         response.each do |resp|
+          next if resp.dig(:contentions, :cntntn_id).nil?
+
           contentions_data << resp[:contentions] if resp.dig(:contentions, :cntntn_id) == rba_contention.dig(:cntntn_id)
         end
       end
