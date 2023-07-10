fdc394b1c721a2ab48d56b5a2a84a85589b04e2f
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-07-10 18:28:45 -0400

    :white_check_mark: Fix broken spec

--- spec/lib/tasks/remediations/cancel_vrr_tasks_open_for_vre_spec.rb
+++ spec/lib/tasks/remediations/cancel_vrr_tasks_open_for_vre_spec.rb
@@ -7,7 +7,7 @@ describe "remediations/cancel_vrr_tasks_open_for_vre" do
     it "delegates to CancelTasksAndDescendants" do
       task_relation = double("task_relation")
 
-      expect(CancelTasksAndDescendants::VeteranRecordRequestsOpenForVREQuery)
+      expect(VeteranRecordRequestsOpenForVREQuery)
         .to receive(:call).and_return(task_relation)
 
       expect(CancelTasksAndDescendants).to receive(:call).with(task_relation)
