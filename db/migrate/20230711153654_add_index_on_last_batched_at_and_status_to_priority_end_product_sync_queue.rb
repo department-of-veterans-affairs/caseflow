8829a779eb30a972d6cb686e0b1937c713a9730a
Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>
2023-07-13 22:06:24 -0400

    APPEALS-24678 Added indexing on last_batched_at & status columns in Priority End Product Sync Queue table.

---
+++ db/migrate/20230711153654_add_index_on_last_batched_at_and_status_to_priority_end_product_sync_queue.rb
@@ -0,0 +1,6 @@
+class AddIndexOnLastBatchedAtAndStatusToPriorityEndProductSyncQueue < Caseflow::Migration
+  def change
+    add_safe_index :priority_end_product_sync_queue, [:last_batched_at], name: "index_priority_ep_sync_queue_on_last_batched_at", unique: false
+    add_safe_index :priority_end_product_sync_queue, [:status], name: "index_priority_ep_sync_queue_on_last_batched_at", unique: false
+  end
+end
