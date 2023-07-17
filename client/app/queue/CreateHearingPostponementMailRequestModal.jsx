76063b54cf1ccd7f3d9d9e602902479c9e553868
Marc Steele <Marc.Steele@va.gov>
2023-07-17 17:03:29 -0400

    APPEALS-24996 Create new file for modal component

---
+++ client/app/queue/CreateHearingPostponementMailRequestModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import QueueFlowModal from "./components/QueueFlowModal";
+
+const CreateHearingPostponementMailRequestModal = (props) => {
+
+  return (
+    <QueueFlowModal>
+
+    </QueueFlowModal>
+  )
+}
+
+export default CreateHearingPostponementMailRequestModal;
