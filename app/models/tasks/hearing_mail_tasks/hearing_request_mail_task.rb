1f06445f9ef338a5d8146ff9afd0db8033e19b82
Jeff Marks <jeff.t.marks@gmail.com>
2023-07-17 15:26:31 -0400

    APPEALS-24992 Added verification to prevent creation of HearingRequestMailTask

--- app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
@@ -3,6 +3,8 @@
 class HearingRequestMailTask < MailTask
   validates :parent, presence: true, parentTask: { task_type: HearingTask }, on: :create
 
+  before_create :verify_request_type_designated
+
   class << self
     def allow_creation?(*)
       false
@@ -18,6 +20,14 @@ class HearingRequestMailTask < MailTask
 
     def update_from_params(params, current_user)
       super(params, current_user)
+    end
+  end
+
+  private
+
+  def verify_request_type_designated
+    if type == "HearingRequestMailTask"
+      fail Caseflow::Error::InvalidTaskTypeOnTaskCreate, task_type: type
     end
   end
 end
