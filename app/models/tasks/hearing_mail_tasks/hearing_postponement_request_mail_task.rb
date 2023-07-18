9ca4da98814fc5440edbb6a0b85b7d81c3dcc62d
Jeff Marks <jeff.t.marks@gmail.com>
2023-07-18 13:25:31 -0400

    APPEALS-24994 Completed HearingPostponementRequestMailTask

---
+++ app/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class HearingPostponementRequestMailTask < HearingRequestMailTask
+  class << self
+    def label
+      COPY::HEARING_POSTPONEMENT_REQUEST_MAIL_TASK_LABEL
+    end
+
+    def allow_creation?(*)
+      true
+    end
+  end
+
+  TASK_ACTIONS = [
+    Constants.TASK_ACTIONS.CHANGE_TASK_TYPE.to_h,
+    Constants.TASK_ACTIONS.COMPLETE_AND_POSTPONE.to_h,
+    Constants.TASK_ACTIONS.ASSIGN_TO_TEAM.to_h,
+    Constants.TASK_ACTIONS.ASSIGN_TO_PERSON.to_h,
+    Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+  ].freeze
+
+  def available_actions(user)
+    return [] unless user.in_hearing_admin_team?
+
+    if active_schedule_hearing_task? || open_assign_hearing_disposition_task?
+      TASK_ACTIONS
+    else
+      [
+        Constants.TASK_ACTIONS.CHANGE_TASK_TYPE.to_h,
+        Constants.TASK_ACTIONS.CANCEL_TASK.to_h
+      ]
+    end
+  end
+
+  private
+
+  def active_schedule_hearing_task?
+    appeal.tasks.find_by(type: ScheduleHearingTask.name)&.active?
+  end
+
+  def open_assign_hearing_disposition_task?
+    # ChangeHearingDispositionTask is a descendant of AssignHearingDispositionTask
+    disposition_task_names = [AssignHearingDispositionTask.name, ChangeHearingDispositionTask.name]
+    open_task = appeal.tasks.where(type: disposition_task_names).open.first
+    open_task && hearing_not_scheduled_in_past(open_task)
+  end
+
+  # Ensure hearing associated with AssignHearingDispositionTask is not scheduled in the past
+  def hearing_not_scheduled_in_past(task)
+    task.hearing.scheduled_for >= Time.zone.now
+  end
+end
--- app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
@@ -7,8 +7,7 @@
 # HearingRequestMailTask is itself not an assignable task type
 
 class HearingRequestMailTask < MailTask
-  # Is parentTask option necessary?
-  validates :parent, presence: true, parentTask: { task_type: HearingTask }, on: :create
+  validates :parent, presence: true, on: :create
 
   # Is this check necessary, and would it be more effective reworked as a validation statement? Or no difference
   before_create :verify_request_type_designated
@@ -18,22 +17,23 @@ class HearingRequestMailTask < MailTask
       false
     end
 
+    # All descendant postponement/withdrawal tasks will initially be assigned to the Hearing Admin org
     def default_assignee(_task)
       HearingAdmin.singleton
     end
+  end
 
-    def available_actions(_user)
-      []
-    end
+  def available_actions(_user)
+    []
+  end
 
-    def update_from_params(params, current_user)
-      super(params, current_user)
-    end
+  def update_from_params(params, current_user)
+    super(params, current_user)
   end
 
   private
 
-  # Ensure create is called on a descendant class and not directly on this class
+  # Ensure create is called on a valid descendant task and not directly on this class
   def verify_request_type_designated
     valid_request_types = %w[HearingPostponementRequestMailTask HearingWithdrawalRequestMailTask]
 
