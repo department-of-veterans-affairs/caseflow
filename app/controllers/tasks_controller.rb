22d666db24b98d50bf81644ace8cf10f18758275
Jeff Marks <jeff.t.marks@gmail.com>
2023-07-18 14:17:27 -0400

    APPEALS-24994 Added HPR to TASK_CLASSES_LOOKUP hash and changed 'subclasses' to 'descendants' in MailTask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,6 +30,7 @@ class TasksController < ApplicationController
     EducationDocumentSearchTask: EducationDocumentSearchTask,
     FoiaTask: FoiaTask,
     HearingAdminActionTask: HearingAdminActionTask,
+    HearingPostponementrequestMailTask: HearingPostponementrequestMailTask,
     InformalHearingPresentationTask: InformalHearingPresentationTask,
     JudgeAddressMotionToVacateTask: JudgeAddressMotionToVacateTask,
     JudgeAssignTask: JudgeAssignTask,
