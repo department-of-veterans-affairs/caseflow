e2bc818aad15d688e76a4807f72f72e662c3513d
SHarshain <spoosa@razortalent.com>
2023-07-19 14:08:10 -0400

    added newrelic ignore helper

--- app/controllers/idt/api/v2/appeals_controller.rb
+++ app/controllers/idt/api/v2/appeals_controller.rb
@@ -3,6 +3,7 @@
 class Idt::Api::V2::AppealsController < Idt::Api::V1::BaseController
   protect_from_forgery with: :exception
   before_action :verify_access
+  newrelic_ignore
 
   skip_before_action :verify_authenticity_token, only: [:outcode]
 
