60765a62120a3137be6a0f2d89e33e27ab7426ac
Jonathan Tsang <tsang_jonathan@bah.com>
2023-07-19 15:12:11 -0400

    APPEALS-24400 added sync lock concern and error

---
+++ app/models/concerns/sync_lock.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "redis"
+
+module SyncLock
+  extend ActiveSupport::Concern
+  LOCK_TIMEOUT = ENV["SYNC_LOCK_MAX_DURATION"] || 60
+
+  def hlr_sync_lock
+    if decision_review.is_a?(HigherLevelReview) && block_given?
+      redis = Redis.new(url: Rails.application.secrets.redis_url_cache)
+      lock_key = "hlr_sync_lock:#{end_product_establishment.id}"
+
+      begin
+        sync_lock_acquired = redis.setnx(lock_key, true)
+
+        fail Caseflow::Error::SyncLockFailed, "#{Time.zone.now}." unless sync_lock_acquired
+
+        redis.expire(lock_key, LOCK_TIMEOUT.to_i)
+        yield
+      ensure
+        redis.del(lock_key)
+      end
+    elsif block_given?
+      yield
+    end
+  end
+end
