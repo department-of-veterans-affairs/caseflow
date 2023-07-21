8a2e81e16b0b7ab01091f2efbdc1a65cc907611a
= <tyler.broyles@va.gov>
2023-07-21 11:22:37 -0400

    Removed unused import.

--- client/test/app/nonComp/NonCompTabs.test.js
+++ client/test/app/nonComp/NonCompTabs.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, wait, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import '@testing-library/jest-dom';
