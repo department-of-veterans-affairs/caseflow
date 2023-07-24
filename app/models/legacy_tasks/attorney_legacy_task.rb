1702c556b61fc17ad79db386efa5fab9a24821fc
Kamala Madamanchi <kamala.madamanchi@va.gov>
2023-07-24 15:07:40 -0500

    Feature toggle to showup the dropdown

--- app/models/legacy_tasks/attorney_legacy_task.rb
+++ app/models/legacy_tasks/attorney_legacy_task.rb
@@ -7,25 +7,25 @@ class AttorneyLegacyTask < LegacyTask
     # so we use the absence of this value to indicate that there is no case assignment and return no actions.
     return [] unless task_id
 
-    if current_user&.can_act_on_behalf_of_judges? &&
+    if current_user&.can_act_on_behalf_of_judges? && FeatureToggle.enabled?(:vlj_legacy_appeal) &&
        (appeal.case_record.reload.bfcurloc == "57" || appeal.case_record.reload.bfcurloc == "CASEFLOW")
       [
         Constants.TASK_ACTIONS.BLOCKED_SPECIAL_CASE_MOVEMENT_LEGACY.to_h
       ]
+    elsif (current_user&.judge_in_vacols? || current_user&.can_act_on_behalf_of_judges?) &&
+          FeatureToggle.enabled?(:vlj_legacy_appeal)
+      [
+        Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h,
+        Constants.TASK_ACTIONS.ASSIGN_TO_ATTORNEY_LEGACY.to_h
+      ]
+    elsif role == "attorney" && current_user == assigned_to
+      [
+        Constants.TASK_ACTIONS.REVIEW_LEGACY_DECISION.to_h,
+        Constants.TASK_ACTIONS.SUBMIT_OMO_REQUEST_FOR_REVIEW.to_h,
+        Constants.TASK_ACTIONS.ADD_ADMIN_ACTION.to_h
+      ]
     else
-      if current_user&.judge_in_vacols? || current_user&.can_act_on_behalf_of_judges?
-        [
-          Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h,
-          Constants.TASK_ACTIONS.ASSIGN_TO_ATTORNEY_LEGACY.to_h
-        ] elsif current_user&.attorney? || role == "attorney"
-            [
-              Constants.TASK_ACTIONS.REVIEW_LEGACY_DECISION.to_h,
-              Constants.TASK_ACTIONS.SUBMIT_OMO_REQUEST_FOR_REVIEW.to_h,
-              Constants.TASK_ACTIONS.ADD_ADMIN_ACTION.to_h
-            ]
-      else
-        []
-      end
+      []
     end
   end
 
@@ -36,7 +36,7 @@ class AttorneyLegacyTask < LegacyTask
   def label
     return false if appeal.case_record.nil?
 
-    if appeal.case_record.reload.bfcurloc == "57" || appeal.case_record.reload.bfcurloc == "CASEFLOW"
+    if (appeal.case_record.reload.bfcurloc == "57" || appeal.case_record.reload.bfcurloc == "CASEFLOW") && FeatureToggle.enabled?(:vlj_legacy_appeal)
       COPY::ATTORNEY_REWRITE_TASK_LEGACY_LABEL
     else
       COPY::ATTORNEY_REWRITE_TASK_LABEL
