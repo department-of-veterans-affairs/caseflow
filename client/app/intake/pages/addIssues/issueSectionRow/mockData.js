5591e12c18326a495eead84e0ff4f53afc284885
Brandon Dorner <brandondorner@yahoo.com>
2023-07-25 14:40:47 -0500

    Create `issueSectionRow` stories
    The stories are testing:
    - The basic issueSectionRow functionality
    - issueSectionRow when the issue has no decision date

---
+++ client/app/intake/pages/addIssues/issueSectionRow/mockData.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-empty-function */
+const issueSectionRowProps = {
+  editPage: true,
+  featureToggles: {},
+  formType: 'test',
+  intakeData: {},
+  onClickIssueAction: () => {},
+  sectionIssues: [
+    {
+      index: 0,
+      id: '4277',
+      text: 'Medical and Dental Care Reimbursement - Issue',
+      benefitType: 'vha',
+      date: '2023-07-11',
+      decisionReviewTitle: 'Supplemental Claim',
+      category: 'Medical and Dental Care Reimbursement',
+      editable: true,
+    },
+  ],
+  userCanWithdrawIssues: true,
+  withdrawReview: false
+};
+
+export { issueSectionRowProps };
