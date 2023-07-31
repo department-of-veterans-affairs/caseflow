3e0963a169d7d4be87bc8623215c8195720af343
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-07-31 14:35:21 -0400

    :construction:  Apply 'bin/rails app:update' generated changes to 'config/initializers/assets.rb'

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,18 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('client', 'node_modules')
-
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("client", "node_modules")
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets 
+# application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( explain-appeal-timeline.js )
 Rails.application.config.assets.precompile += %w( explain-appeal-network.js )
 Rails.application.config.assets.precompile += %w( stats.js )
@@ -31,10 +31,3 @@ Rails.application.config.assets.precompile += %w( explain_appeal_timeline.css )
 
 Rails.application.config.assets.precompile += %w( favicon.ico )
 Rails.application.config.assets.precompile << %w( *.woff *.woff2 *.eot *.ttf )
-# Add client/assets/ folders to asset pipeline's search path.
-# If you do not want to move existing images and fonts from your Rails app
-# you could also consider creating symlinks there that point to the original
-# rails directories. In that case, you would not add these paths here.
-# If you have a different server bundle file than your client bundle, you'll
-# need to add it here, like this:
-# Rails.application.config.assets.precompile += %w( server-bundle.js )
