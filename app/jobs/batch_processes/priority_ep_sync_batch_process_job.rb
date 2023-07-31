e7bfc4cf7a404f12d3b3b9f4c8e9e1544d82649e
Eli Brown <elilogbro@gmail.com>
2023-07-31 15:22:09 -0400

    added RedisMutex lock on the PriorityEpSyncBatchProcessJob

---
+++ app/jobs/batch_processes/priority_ep_sync_batch_process_job.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class PriorityEpSyncBatchProcessJob < CaseflowJob
+  queue_with_priority :low_priority
+
+  # Using macro-style definition. The locking scope will be TheClass#method and only one method can run at any given time.
+  include RedisMutex::Macro
+
+  # Default options for RedisMutex#with_lock
+  # :block  => 1    # Specify in seconds how long you want to wait for the lock to be released.
+  #                 # Specify 0 if you need non-blocking sematics and return false immediately. (default: 1)
+  # :sleep  => 0.1  # Specify in seconds how long the polling interval should be when :block is given.
+  #                 # It is NOT recommended to go below 0.01. (default: 0.1)
+  # :expire => 10   # Specify in seconds when the lock should be considered stale when something went wrong
+  #                 # with the one who held the lock and failed to unlock. (default: 10)
+
+  before_perform do |job|
+    JOB_ATTR = job
+  end
+
+  def perform
+    begin
+      RedisMutex.with_lock("PriorityEpSyncBatchProcessJob", block: 30, expire: 60) do # key => "PriorityEpSyncBatchProcessJob"
+        @batch = ActiveRecord::Base.transaction do
+          records_to_batch = PriorityEpSyncBatchProcess.find_records_to_batch
+          next if records_to_batch.empty?
+
+          PriorityEpSyncBatchProcess.create_batch!(records_to_batch)
+        end
+      end
+
+      if @batch
+        @batch.process_batch!
+      else
+        Rails.logger.info("No Records Available to Batch.  Job ID: #{JOB_ATTR&.job_id}.  Time: #{Time.zone.now}")
+      end
+    rescue StandardError => error
+      Rails.logger.error("Error: #{error.inspect}, Job ID: #{JOB_ATTR&.job_id}, Job Time: #{Time.zone.now}")
+      capture_exception(error: error,
+                        extra: { job_id: JOB_ATTR&.job_id.to_s,
+                                 job_time: Time.zone.now.to_s })
+    end
+  end
+end
