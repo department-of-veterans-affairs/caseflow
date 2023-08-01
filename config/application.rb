8838d3cbc00392db5a2d74e58380d959528e5296
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-08-01 15:56:06 -0400

    :bulb: Apply 'bin/rails app:update' generated comment changes to 'config/application.rb'

--- config/application.rb
+++ config/application.rb
@@ -67,8 +67,9 @@ module CaseflowCertification
     # ==================================================================================================================
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
