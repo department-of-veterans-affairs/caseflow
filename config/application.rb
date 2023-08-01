a157e1cb4d538fc0b4ccc1a81faa69abd252b3cf
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-08-01 16:04:54 -0400

    :bulb: Slide comments for cleaner diff with future generated changes

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,11 @@ module CaseflowCertification
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
     # ==================================================================================================================
     # Rails 5.0 config overrides
     # ------------------------------------------------------------------------------------------------------------------
@@ -65,11 +70,6 @@ module CaseflowCertification
     # Default (starting v5.2): true
     Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = false
     # ==================================================================================================================
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
