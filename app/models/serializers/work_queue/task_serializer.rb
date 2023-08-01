7dd7137a2ad3edc00a1191311cf0b7e76c7c56f1
kristeja <112115264+kristeja@users.noreply.github.com>
2023-08-01 15:48:53 -0700

    Revert "APPEALS-17497: MST & PACT Special issues identification"

--- app/models/serializers/work_queue/task_serializer.rb
+++ app/models/serializers/work_queue/task_serializer.rb
@@ -35,7 +35,7 @@ class WorkQueue::TaskSerializer
   end
 
   attribute :completed_by do |object|
-    object.try(:completed_by).try(:css_id)
+    object.try(:completed_by).try(:css_id) unless object.appeal.is_a?(LegacyAppeal)
   end
 
   attribute :assigned_to do |object|
@@ -127,14 +127,6 @@ class WorkQueue::TaskSerializer
 
   attribute :contested_claim do |object|
     object.appeal.try(:contested_claim?)
-  end
-
-  attribute :mst do |object|
-    object.appeal.try(:mst?)
-  end
-
-  attribute :pact do |object|
-    object.appeal.try(:pact?)
   end
 
   attribute :veteran_appellant_deceased do |object|
