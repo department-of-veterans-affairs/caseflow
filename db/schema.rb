7569c8732d56bc76fed5c60f78b869036e482a53
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-08-01 09:08:49 -0400

    Remove flipper tables

--- db/schema.rb
+++ db/schema.rb
@@ -790,22 +790,6 @@ ActiveRecord::Schema.define(version: 2023_07_31_194341) do
     t.index ["user_id"], name: "index_end_product_updates_on_user_id"
   end
 
-  create_table "flipper_features", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "key", null: false
-    t.datetime "updated_at", null: false
-    t.index ["key"], name: "index_flipper_features_on_key", unique: true
-  end
-
-  create_table "flipper_gates", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "feature_key", null: false
-    t.string "key", null: false
-    t.datetime "updated_at", null: false
-    t.string "value"
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
-  end
-
   create_table "form8s", id: :serial, force: :cascade do |t|
     t.string "_initial_appellant_name"
     t.string "_initial_appellant_relationship"
