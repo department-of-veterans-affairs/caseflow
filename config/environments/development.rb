0738c70713675c153461b916bc737ab47372d673
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-08-01 17:20:59 -0400

    :recycle: Restore single-quoted strings from 'bin/rails app:update'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,11 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp", "caching-dev.txt").exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.cache_store = :redis_store, Rails.application.secrets.redis_url_cache, { expires_in: 24.hours }
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
