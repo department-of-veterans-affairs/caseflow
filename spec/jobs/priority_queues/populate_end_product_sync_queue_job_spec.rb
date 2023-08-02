47b652c1c5a032b9abb655af07fbd869312a2121
Eli Brown <elilogbro@gmail.com>
2023-08-02 15:25:37 -0400

    reformatted rails logger message

--- spec/jobs/priority_queues/populate_end_product_sync_queue_job_spec.rb
+++ spec/jobs/priority_queues/populate_end_product_sync_queue_job_spec.rb
@@ -74,7 +74,8 @@ describe PopulateEndProductSyncQueueJob, type: :job do
         allow(Rails.logger).to receive(:info)
         PopulateEndProductSyncQueueJob.perform_now
         expect(Rails.logger).to have_received(:info).with(
-          "PopulateEndProductSyncQueueJob is not able to find any batchable EPE records.  Job will be enqueued again once 1-hour mark is hit.  Job ID: #{PopulateEndProductSyncQueueJob::JOB_ATTR&.job_id}."\
+          "PopulateEndProductSyncQueueJob is not able to find any batchable EPE records."\
+          "  Job will be enqueued again after 1-hour mark passes.  Job ID: #{PopulateEndProductSyncQueueJob::JOB_ATTR&.job_id}."\
           "  Time: #{Time.zone.now}"
         )
       end
