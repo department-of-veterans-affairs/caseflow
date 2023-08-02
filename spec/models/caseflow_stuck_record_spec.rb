48fad6c1590de7b18323d03cfe418a60c5383b96
Eli Brown <elilogbro@gmail.com>
2023-08-02 09:49:44 -0400

    stubbed consts for jobs to prevent rspec timeout

--- spec/models/caseflow_stuck_record_spec.rb
+++ spec/models/caseflow_stuck_record_spec.rb
@@ -5,6 +5,19 @@ describe CaseflowStuckRecord, :postgres do
     let!(:end_product_establishment) do
       create(:end_product_establishment, :canceled_hlr_with_cleared_vbms_ext_claim)
     end
+
+    before do
+      # Force the jobs to only run long enough to iterate through the loop once each.
+      # This overrides the default job duration which is supposed to continue
+      # iterating through the job for an hour.
+      #
+      # Changing the sleep duration to 0 prevents latency.
+      stub_const("PopulateEndProductSyncQueueJob::JOB_DURATION", 0.0001.seconds)
+      stub_const("PopulateEndProductSyncQueueJob::SLEEP_DURATION", 0.seconds)
+      stub_const("PriorityEpSyncBatchProcessJob::JOB_DURATION", 0.001.seconds)
+      stub_const("PriorityEpSyncBatchProcessJob::SLEEP_DURATION", 0.seconds)
+    end
+
     let!(:caseflow_stuck_record) do
       PopulateEndProductSyncQueueJob.perform_now
       3.times do
@@ -14,7 +27,7 @@ describe CaseflowStuckRecord, :postgres do
       CaseflowStuckRecord.first
     end
 
-    it "will return the end_product_establishment when the stuck record is from the Priortiy End Product Sync Queue" do
+    it "will return the end_product_establishment when the stuck record is from the Priority End Product Sync Queue" do
       expect(caseflow_stuck_record.end_product_establishment).to eq(end_product_establishment)
     end
   end
