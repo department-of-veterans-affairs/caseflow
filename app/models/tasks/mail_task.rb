95b05017a23f0c4499dd80e14ac5e7895680db5a
Marc Steele <Marc.Steele@va.gov>
2023-08-02 11:23:04 -0400

    APPEALS-25001 Added comment to legacy mail tasks constant

--- app/models/tasks/mail_task.rb
+++ app/models/tasks/mail_task.rb
@@ -20,6 +20,8 @@ class MailTask < Task
   def verify_org_task_unique; end
   prepend PrivacyActPending
 
+  # This constant is more efficient than iterating through all mail tasks
+  # and filtering out almost all of them since only HPR and HWR are approved for now
   LEGACY_MAIL_TASKS = [
     { label: "Hearing postponement request", value: "HearingPostponementRequestMailTask" }
   ].freeze
