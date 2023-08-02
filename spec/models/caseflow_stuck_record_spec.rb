1771b5cde387a94584f44be0614d761741e185c3
Eli Brown <elilogbro@gmail.com>
2023-08-02 10:45:04 -0400

    moved before block outside of inner describe block

--- spec/models/caseflow_stuck_record_spec.rb
+++ spec/models/caseflow_stuck_record_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 describe CaseflowStuckRecord, :postgres do
+  before do
+    # Force the jobs to only run long enough to iterate through the loop once each.
+    # This overrides the default job duration which is supposed to continue
+    # iterating through the job for an hour.
+    #
+    # Changing the sleep duration to 0 prevents latency.
+    stub_const("PopulateEndProductSyncQueueJob::JOB_DURATION", 0.0001.seconds)
+    stub_const("PopulateEndProductSyncQueueJob::SLEEP_DURATION", 0.seconds)
+    stub_const("PriorityEpSyncBatchProcessJob::JOB_DURATION", 0.001.seconds)
+    stub_const("PriorityEpSyncBatchProcessJob::SLEEP_DURATION", 0.seconds)
+  end
+
   describe "#end_product_establishment" do
     let!(:end_product_establishment) do
       create(:end_product_establishment, :canceled_hlr_with_cleared_vbms_ext_claim)
-    end
-
-    before do
-      # Force the jobs to only run long enough to iterate through the loop once each.
-      # This overrides the default job duration which is supposed to continue
-      # iterating through the job for an hour.
-      #
-      # Changing the sleep duration to 0 prevents latency.
-      stub_const("PopulateEndProductSyncQueueJob::JOB_DURATION", 0.0001.seconds)
-      stub_const("PopulateEndProductSyncQueueJob::SLEEP_DURATION", 0.seconds)
-      stub_const("PriorityEpSyncBatchProcessJob::JOB_DURATION", 0.001.seconds)
-      stub_const("PriorityEpSyncBatchProcessJob::SLEEP_DURATION", 0.seconds)
     end
 
     let!(:caseflow_stuck_record) do
