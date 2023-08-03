5bf6da7f8d893994debc53f2720574d3f07957e3
= <tyler.broyles@va.gov>
2023-08-03 16:49:17 -0400

    Added a todo comment and added a intakes_controller_spec test for no vha issue without a decision date.

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -59,6 +59,7 @@ class IntakesController < ApplicationController
     # TODO: See if this errors for Ramp intakes? Idk what model ramp intakes use
     if !detail.is_a?(Appeal) && detail.try(:processed_in_caseflow?)
       # TODO: Ask if they want this message changed for intake as well
+      # TODO: This might not work because of the async job....
       flash[:success] = (detail.benefit_type == "vha") ? detail.try(:success_message) : success_message
       render json: { serverIntake: { redirect_to: detail.redirect_url_from_intake } }
     else
