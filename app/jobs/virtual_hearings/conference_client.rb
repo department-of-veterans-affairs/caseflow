9877152bc62ebe7f24cace858cfbdbe36e9af3ef
Ariana Konhilas <konhilas_ariana@bah.com>
2023-08-04 09:52:18 -0400

    APPEALS-25136: updates to the conference client

--- app/jobs/virtual_hearings/conference_client.rb
+++ app/jobs/virtual_hearings/conference_client.rb
@@ -3,24 +3,22 @@
 module VirtualHearings::ConferenceClient
   def client
     case user.meeting_type
-      when "pexip"
-        @client ||= PexipService.new(
-          host: ENV["PEXIP_MANAGEMENT_NODE_HOST"],
-          port: ENV["PEXIP_MANAGEMENT_NODE_PORT"],
-          user_name: ENV["PEXIP_USERNAME"],
-          password: ENV["PEXIP_PASSWORD"],
-          client_host: ENV["PEXIP_CLIENT_HOST"]
-        )
-      end
-      when "webex"
-        @client ||= WebexService.new(
-          host: ENV["WEBEX_MANAGEMENT_NODE_HOST"],
-          port: ENV["WEBEX_MANAGEMENT_NODE_PORT"],
-          user_name: ENV["WEBEX_USERNAME"],
-          password: ENV["WEBEX_PASSWORD"],
-          client_host: ENV["WEBEX_CLIENT_HOST"]
-        )
-      end
+    when "pexip"
+      @client ||= PexipService.new(
+        host: ENV["PEXIP_MANAGEMENT_NODE_HOST"],
+        port: ENV["PEXIP_MANAGEMENT_NODE_PORT"],
+        user_name: ENV["PEXIP_USERNAME"],
+        password: ENV["PEXIP_PASSWORD"],
+        client_host: ENV["PEXIP_CLIENT_HOST"]
+      )
+    when "webex"
+      @client ||= WebexService.new(
+        host: ENV["WEBEX_MANAGEMENT_NODE_HOST"],
+        port: ENV["WEBEX_MANAGEMENT_NODE_PORT"],
+        user_name: ENV["WEBEX_USERNAME"],
+        password: ENV["WEBEX_PASSWORD"],
+        client_host: ENV["WEBEX_CLIENT_HOST"]
+      )
     end
   end
 end
