d6c6799382dbae3d6b26fe117ba765ac8af26500
Eli Brown <elilogbro@gmail.com>
2023-08-07 13:10:02 -0400

    batch_process_priority_ep_sync_job_spec

--- spec/jobs/batch_processes/batch_process_priority_ep_sync_job_spec.rb
+++ spec/jobs/batch_processes/batch_process_priority_ep_sync_job_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require "./app/jobs/batch_processes/batch_process_priority_ep_sync_job.rb"
+require "./app/jobs/batch_processes/priority_ep_sync_batch_process_job.rb"
 
-describe BatchProcessPriorityEpSyncJob, type: :job do
+describe PriorityEpSyncBatchProcessJob, type: :job do
   let!(:syncable_end_product_establishments) do
     create_list(:end_product_establishment, 99, :active_hlr_with_cleared_vbms_ext_claim)
   end
@@ -16,7 +16,7 @@ describe BatchProcessPriorityEpSyncJob, type: :job do
     PriorityEndProductSyncQueue.all
   end
 
-  subject { BatchProcessPriorityEpSyncJob.perform_now }
+  subject { PriorityEpSyncBatchProcessJob.perform_now }
 
   describe "#perform" do
     context "when 99 records can sync successfully and 1 cannot" do
@@ -74,7 +74,7 @@ describe BatchProcessPriorityEpSyncJob, type: :job do
       before do
         allow(Rails.logger).to receive(:error)
         allow(Raven).to receive(:capture_exception)
-        allow(BatchProcessPriorityEpSync).to receive(:find_records_to_batch)
+        allow(PriorityEpSyncBatchProcess).to receive(:find_records_to_batch)
           .and_raise(StandardError, "Oh no!  This is bad!")
         subject
       end
@@ -82,7 +82,7 @@ describe BatchProcessPriorityEpSyncJob, type: :job do
       it "the error will be logged" do
         expect(Rails.logger).to have_received(:error).with(
           "Error: #<StandardError: Oh no!  This is bad!>,"\
-          " Job ID: #{BatchProcessPriorityEpSyncJob::JOB_ATTR.job_id}, Job Time: #{Time.zone.now}"
+          " Job ID: #{PriorityEpSyncBatchProcessJob::JOB_ATTR.job_id}, Job Time: #{Time.zone.now}"
         )
       end
 
@@ -90,7 +90,7 @@ describe BatchProcessPriorityEpSyncJob, type: :job do
         expect(Raven).to have_received(:capture_exception)
           .with(instance_of(StandardError),
                 extra: {
-                  job_id: BatchProcessPriorityEpSyncJob::JOB_ATTR.job_id.to_s,
+                  job_id: PriorityEpSyncBatchProcessJob::JOB_ATTR.job_id.to_s,
                   job_time: Time.zone.now.to_s
                 })
       end
@@ -105,7 +105,7 @@ describe BatchProcessPriorityEpSyncJob, type: :job do
 
       it "a message that says 'No Records Available to Batch' will be logged" do
         expect(Rails.logger).to have_received(:info).with(
-          "No Records Available to Batch.  Job ID: #{BatchProcessPriorityEpSyncJob::JOB_ATTR&.job_id}."\
+          "No Records Available to Batch.  Job ID: #{PriorityEpSyncBatchProcessJob::JOB_ATTR&.job_id}."\
           "  Time: #{Time.zone.now}"
         )
       end
