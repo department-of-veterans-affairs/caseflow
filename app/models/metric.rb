48bec49f5a4f604b8c0cece58b3690355ece4f4e
Chris-Martine <christopher.martine@va.gov>
2023-08-07 09:56:29 -0400

    Remove default testing metric user from metric model

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -77,7 +77,7 @@ class Metric < CaseflowRecord
   def self.default_object(klass, params, user)
     {
       uuid: params[:uuid],
-      user: user || User.new(full_name: "Fake user for metrics", css_id: SecureRandom.uuid, station_id: 'Metrics'),
+      user: user,
       metric_name: params[:name] || METRIC_TYPES[:log],
       metric_class: klass&.try(:name) || klass&.class.name || self.name,
       metric_group: params[:group] || METRIC_GROUPS[:service],
