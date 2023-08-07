a20d896580c4437390e74c8ddcd239ecf214b972
Eli Brown <elilogbro@gmail.com>
2023-08-07 12:08:54 -0400

    removed migration

--- db/migrate/20230801195310_add_columns_to_caseflow_stuck_records.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToCaseflowStuckRecords < Caseflow::Migration
-  def change
-    add_column :caseflow_stuck_records, :remediated, :boolean, default: false, null: false, comment: "Reflects if the stuck record has been reviewed and fixed"
-    add_column :caseflow_stuck_records, :remediation_notes, :text, comment: "Brief description of the encountered issue and remediation strategy"
-    add_column :caseflow_stuck_records, :updated_at, :datetime, comment: "The time an update occurred on the record"
-  end
-end
