4c122ca670ac7797f66eff5cecb3a317d03b1db5
breedbah <brandon.reed3@va.gov>
2023-08-08 16:24:19 -0400

    APPEALS-25137 adding mocking ApiUtil to tests

--- client/test/app/queue/organizationUsers/OrganizationUsers.test.js
+++ client/test/app/queue/organizationUsers/OrganizationUsers.test.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import OrganizationUsers from 'app/queue/OrganizationUsers';
+import ApiUtil from 'client/app/util/ApiUtil.js';
+
+jest.mock('/client/app/util/ApiUtil');
 
 describe('Conference Selection Visibility Feature Toggle', () => {
-  it('renders the nested component when conferenceSelectionVisibility is true',async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('renders the nested component when conferenceSelectionVisibility is true', async () => {
+    const conferenceSelectionVisibilityValue = true;
+
+    ApiUtil.get.mockResolvedValue({
+      body: {
+        organizationName: 'Hearing Admin',
+
+      },
+    });
+
     render(
       <OrganizationUsers
-        organizationName='Hearing Admin'
-        conferenceSelectionVisibility={true}
+        conferenceSelectionVisibility= {conferenceSelectionVisibilityValue}
       />
     );
 
+    const nestedText = await screen.findByText('Remove from team:');
     screen.debug();
-    const nestedText = await screen.findByTestId("conference-radio-button");
-      expect(nestedText).toBeInTheDocument();
+    expect(nestedText).toBeInTheDocument();
   });
 
   // it("does not render the nested component for other conditions", () => {
