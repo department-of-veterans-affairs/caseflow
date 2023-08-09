8862f5fd8fed955ee53d14543d6e490496c1b1ce
breedbah <brandon.reed3@va.gov>
2023-08-09 09:42:41 -0400

    APPEALS-25137 D.R.Y. out code in tests

--- client/test/app/queue/organizationUsers/OrganizationUsers.test.js
+++ client/test/app/queue/organizationUsers/OrganizationUsers.test.js
@@ -10,106 +10,65 @@ describe('Conference Selection Visibility Feature Toggle', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  it('Finds component by Text (Webex and Pexip) when conferenceSelectionVisibility is false', async () => {
+  ApiUtil.get.mockResolvedValue({
+    body: {
+      organization_name: 'Hearing Admin',
+      judge_team: false,
+      dvc_team: false,
+      organization_users: {
+        data: [
+          {
+            id: '126',
+            type: 'administered_user',
+            attributes: {
+              css_id: 'BVASORANGE',
+              full_name: 'Felicia BuildAndEditHearingSchedule Orange',
+              email: null,
+              admin: false,
+              dvc: null,
+            },
+          },
+          {
+            id: '2000001601',
+            type: 'administered_user',
+            attributes: {
+              css_id: 'AMBRISVACO',
+              full_name: 'Gail Maggio V',
+              email: 'juli@stroman-kertzmann.net',
+              admin: true,
+              dvc: null,
+            },
+          },
+        ],
+      },
+      membership_requests: [],
+      isVhaOrg: false,
+    },
+  });
+  it('Finds component by Text when conferenceSelectionVisibility is false', async () => {
     const conferenceSelectionVisibilityValue = false;
-
-    ApiUtil.get.mockResolvedValue({
-      body: {
-        organization_name: 'Hearing Admin',
-        judge_team: false,
-        dvc_team: false,
-        organization_users: {
-          data: [
-            {
-              id: '126',
-              type: 'administered_user',
-              attributes: {
-                css_id: 'BVASORANGE',
-                full_name: 'Felicia BuildAndEditHearingSchedule Orange',
-                email: null,
-                admin: false,
-                dvc: null
-              }
-            },
-            {
-              id: '2000001601',
-              type: 'administered_user',
-              attributes: {
-                css_id: 'AMBRISVACO',
-                full_name: 'Gail Maggio V',
-                email: 'juli@stroman-kertzmann.net',
-                admin: true,
-                dvc: null
-              }
-            },
-          ],
-        },
-        membership_requests: [],
-        isVhaOrg: false
-      }
-    });
 
     const { findAllByText } = render(
       <OrganizationUsers
         conferenceSelectionVisibility={conferenceSelectionVisibilityValue}
       />
     );
-    const nestedTextWebex = await findAllByText('Webex');
-    const nestedTextPexip = await findAllByText('Pexip');
+    const nestedText = await findAllByText('Webex');
 
-    expect(nestedTextWebex[0]).toBeInTheDocument();
-    expect(nestedTextPexip[0]).toBeInTheDocument();
+    expect(nestedText[0]).toBeInTheDocument();
   });
 
-  it('Finds component by Text (Webex and Pexip) when conferenceSelectionVisibility is true', async () => {
+  it('Component does not render when conferenceSelectionVisibility is true', async () => {
     const conferenceSelectionVisibilityValue = true;
-
-    ApiUtil.get.mockResolvedValue({
-      body: {
-        organization_name: 'Hearing Admin',
-        judge_team: false,
-        dvc_team: false,
-        organization_users: {
-          data: [
-            {
-              id: '126',
-              type: 'administered_user',
-              attributes: {
-                css_id: 'BVASORANGE',
-                full_name: 'Felicia BuildAndEditHearingSchedule Orange',
-                email: null,
-                admin: false,
-                dvc: null,
-              },
-            },
-            {
-              id: '2000001601',
-              type: 'administered_user',
-              attributes: {
-                css_id: 'AMBRISVACO',
-                full_name: 'Gail Maggio V',
-                email: 'juli@stroman-kertzmann.net',
-                admin: true,
-                dvc: null,
-              },
-            },
-          ],
-        },
-        membership_requests: [],
-        isVhaOrg: false,
-      },
-    });
 
     const { queryAllByText } = render(
       <OrganizationUsers
         conferenceSelectionVisibility={conferenceSelectionVisibilityValue}
       />
     );
-    const nestedTextWebex = await queryAllByText('Webex');
-    const nestedTextPexip = await queryAllByText('Pexip');
+    const nestedText = await queryAllByText('Webex');
 
-    expect(nestedTextWebex).toHaveLength(0);
-    expect(nestedTextPexip).toHaveLength(0);
+    expect(nestedText).toHaveLength(0);
   });
 
   it('Component does not render when orginization_name is  ot Hearing Admin', async () => {
@@ -157,10 +116,7 @@ describe('Conference Selection Visibility Feature Toggle', () => {
       />
     );
     const nestedTextWebex = await queryAllByText('Webex');
-    const nestedTextPexip = await queryAllByText('Pexip');
 
     expect(nestedTextWebex).toHaveLength(0);
-    expect(nestedTextPexip).toHaveLength(0);
-
   });
 });
