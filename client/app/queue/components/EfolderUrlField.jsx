c17c473358231763431cf8e91aab91a3a5a948f7
Marc Steele <Marc.Steele@va.gov>
2023-08-14 15:38:19 -0400

    APPEALS-24727 Fixed import statement

--- client/app/queue/components/EfolderUrlField.jsx
+++ client/app/queue/components/EfolderUrlField.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import debounce from 'lodash';
+import { debounce } from 'lodash';
 
 import TextField from '../../components/TextField';
 import ApiUtil from '../../util/ApiUtil';
@@ -21,39 +21,42 @@ const EfolderUrlField = (props) => {
   };
 
   const handleChange = useCallback(
-    debounce((value) => {
-      console.log('Debounced!');
+    (value) => {
       props?.onChange?.(value);
-      if (efolderLinkRegexMatch(value)) {
-        console.log('Valid regex match');
-        // start loading spinner
-        const seriesId = captureDocumentSeriesId(value);
-        const appealId = () => '1234';
 
-        ApiUtil.get(`/appeals/${appealId}/document/${seriesId}`).
-          then((response) => {
-            // stop loading spinner
+      debounce((value) => {
+        console.log('Debounced!');
+        if (efolderLinkRegexMatch(value)) {
+          console.log('Valid regex match');
+          // start loading spinner
+          const seriesId = captureDocumentSeriesId(value);
+          const appealId = () => '1234';
 
-            // if true
-            // set own valid prop to true
-            // if false
-            // set own valid prop to false
-            // show error message (doen't exist in efolder)
-          }).
-          catch((response) => {
-            // stop loading spinner
-            // handle errors
-          });
+          ApiUtil.get(`/appeals/${appealId}/document/${seriesId}`).
+            then((response) => {
+              // stop loading spinner
 
-        // stop loading spinner
-      } else {
-        console.log('Invalid efolder regex match');
-        // https://benefits-int-delivery.slack.com/archives/C03NCPYRXK2/p1687881917481399?thread_ts=1687878651.089549&cid=C03NCPYRXK2
-        // Show error message as described in thread ^^ (invalid link format)
-        // Block form submission until resolved
-      }
-      // We'll need to dial in this delay a bit.
-    }, 500)
+              // if true
+              // set own valid prop to true
+              // if false
+              // set own valid prop to false
+              // show error message (doen't exist in efolder)
+            }).
+            catch((response) => {
+              // stop loading spinner
+              // handle errors
+            });
+
+          // stop loading spinner
+        } else {
+          console.log('Invalid efolder regex match');
+          // https://benefits-int-delivery.slack.com/archives/C03NCPYRXK2/p1687881917481399?thread_ts=1687878651.089549&cid=C03NCPYRXK2
+          // Show error message as described in thread ^^ (invalid link format)
+          // Block form submission until resolved
+        }
+        // We'll need to dial in this delay a bit.
+      }, 500);
+    }
   );
 
   return <>
