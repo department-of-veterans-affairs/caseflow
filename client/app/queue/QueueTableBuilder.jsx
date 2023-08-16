1e6462549f7de06f72c35aea8f085559df01c05c
Calvin <Calvin.Costa@va.gov>
2023-08-16 16:27:45 -0400

    Added method to only return unique tasks in queue

--- client/app/queue/QueueTableBuilder.jsx
+++ client/app/queue/QueueTableBuilder.jsx
@@ -183,7 +183,19 @@ const QueueTableBuilder = (props) => {
     const { isVhaOrg } = props;
 
     if (tabConfig.contains_legacy_tasks) {
-      tasks.unshift(...props.assignedTasks);
+      if (tasks) {
+        props.assignedTasks.forEach((assignedTask) => {
+          const i = tasks.findIndex((task) => task.externalAppealId === assignedTask.uniqueId);
+
+          if (i <= -1) {
+            tasks.unshift(assignedTask);
+          }
+        });
+      } else {
+        tasks.unshift(...props.assignedTasks);
+      }
+
+
       totalTaskCount = tasks.length;
 
       noCasesMessage = totalTaskCount === 0 && (
