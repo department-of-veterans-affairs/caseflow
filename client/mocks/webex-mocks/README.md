717b69c85792e6d235a3efec0e2eed93579b7700
breedbah <brandon.reed3@va.gov>
2023-08-16 13:55:08 -0400

    APPEALS-28105 Adding error messages

--- client/mocks/webex-mocks/README.md
+++ client/mocks/webex-mocks/README.md
@@ -2,24 +2,21 @@ Setup json server
 
 Step 1: Open a terminal
 
-Step 2: Navigate to the caseflow application
+Step 2: Navigate to the caseflow/client
 
 step 3: Run command: npm install json-server
 
-step 4: Run command: npx json-server --watch client/mocks/webex-mocks/webex-mock.json --port 3001
+step 4: Run command: npm run webex-server
 
-*info: You will recieve all available routes within the terminal under 'Resources'
+\*info: You will recieve all available routes within the terminal under 'Resources'
 
-*info: port must be set on a different port to run due to caseflow running on port 3000
+\*info: port must be set on a different port to run due to caseflow running on port 3000
 
-step 5: Open a browser window in chrome and navigate to localhost:3001 [You will get an empty object]
+step 5: Open a browser window in chrome and navigate to localhost:3001 [You will get the default page]
 
-*info: reference guides
-[https://jsonplaceholder.typicode.com/guide/]
-[https://blog.logrocket.com/how-to-bootstrap-your-project-with-json-server/]
+\*info: reference guides
+[https://github.com/typicode/json-server/blob/master/README.md]
 
-step 6: Append the key to the path you are wanting to query [localhost:30001/conference-links]
 
-*info: this will give you the list of objects with the corresponding key
-
-step 7: Append the id to GET the specific id [localhost:30001/conference-links/1]
+Tutorial Resources:
+[https://www.youtube.com/watch?v=_1kNqAybxW0&list=PLC3y8-rFHvwhc9YZIdqNL5sWeTCGxF4ya&index=1]
--- client/mocks/webex-mocks/webex-mock-server.js
+++ client/mocks/webex-mocks/webex-mock-server.js
@@ -1,42 +1,98 @@
-const jsonServer = require("json-server");
-const express = require("express");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("mocks/webex-mocks/webex-mock.json"); // your mock data file
+const router = jsonServer.router('mocks/webex-mocks/webex-mock.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 // Example custom routes for specific error handling
-server.get("/error-400", (req, res) => {
-  res
-    .status(400)
-    .json({
-      message: "The request was invalid or cannot be otherwise served.",
-    });
+server.get('/error-400', (req, res) => {
+  res.status(400).json({
+    message: 'The request was invalid or cannot be otherwise served.',
+  });
 });
 
-server.get("/error-401", (req, res) => {
-  res
-    .status(401)
-    .json({ message: "Authentication credentials were missing or incorrect." });
+server.get('/error-401', (req, res) => {
+  res.status(401).json({
+    message: 'Authentication credentials were missing or incorrect.'
+  });
+});
+
+server.get('/error-403', (req, res) => {
+  res.status(403).json({
+    message: 'The request is understood, but it has been refused or access is not allowed',
+  });
+});
+
+server.get('/error-405', (req, res) => {
+  res.status(405).json({
+    message:
+      'The request was made to a resource using an HTTP request method that is not supported.',
+  });
+});
+
+server.get('/error-409', (req, res) => {
+  res.status(409).json({
+    message:
+      'The request could not be processed because it conflicts with some established rule of the system.',
+  });
+});
+
+server.get('/error-410', (req, res) => {
+  res.status(410).json({
+    message: 'The requested resource is no longer available.',
+  });
 });
 
 // ... Similarly, add routes for other error codes ...
 
+// To handle the default behavior, use the router middleware last
+server.use(router);
+
 // Middleware to handle not-found items
 server.use((req, res, next) => {
-  if (req.method === 'GET' && res.locals.data == null) {
-    res.status(404).json({ message: "Item not found" });
+  if (req.method === 'GET' && res.locals.data === null) {
+    res.status(404).json({ message: 'Item not found' });
   } else {
     next();
   }
 });
 
-// To handle the default behavior, use the router middleware last
-server.use(router);
+const errorRoutes = [
+  {
+    path: '/error-400',
+    description: 'The request was invalid or cannot be otherwise served.'
+  },
+  {
+    path: '/error-401',
+    description: 'Authentication credentials were missing or incorrect.'
+  }
+  // ... Add other error routes here
+];
 
-// Start the server
-server.listen(3001, () => {
-  console.log("JSON Server is running on port 3001");
+// ...
+
+server.listen(3050, () => {
+  console.log(' \\{^_^}/ hi!\n');
+  console.log(' Loading mocks/webex-mocks/webex-mock.json');
+  console.log(' Done\n');
+
+  console.log(' Resources:');
+  const routes = Object.keys(router.db.getState());
+
+  routes.forEach((route) => {
+    console.log(` http://localhost:3050/${route}`);
+  });
+
+  console.log('\n Error Routes:');
+  errorRoutes.forEach(route => {
+    console.log(` ${route.path} - ${route.description}`);
+  });
+
+  console.log('\n Home');
+  console.log(' http://localhost:3050');
+
+  console.log('\n Type s + enter at any time to create a snapshot of the database');
+  console.log(' Watching...');
 });
