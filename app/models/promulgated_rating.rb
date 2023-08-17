d6bc27cb09d56a0dc02e52508a8bcefa1e73bc69
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-08-17 09:43:06 -0400

    Add logging on rating fetch retries

--- app/models/promulgated_rating.rb
+++ app/models/promulgated_rating.rb
@@ -70,6 +70,14 @@ class PromulgatedRating < Rating
   rescue Savon::Error
     {}
   rescue BGS::ShareError
+    Raven.capture_exception(error)
+
+    DataDogService.increment_counter(
+      metric_group: "errors",
+      metric_name: "rating_fetch_retires",
+      app_name: RequestStore[:application]
+    )
+
     retry_fetching_rating_profile
   end
 
