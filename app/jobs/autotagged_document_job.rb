27b7cf74830e69dbef43ad3eeabfaaf0bff31e99
SanthiParakal133 <132940479+SanthiParakal133@users.noreply.github.com>
2023-08-17 16:46:33 -0400

    scheduled job to auto tag a document (#19200)
    * APPEALS-19820 scheduled job to auto tag a document
    
    
    ---------
    
    Co-authored-by: youfoundmanesh <maneshreddy.kommidi@va.gov>

---
+++ app/jobs/autotagged_document_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AutotaggedDocumentJob < CaseflowJob
+  queue_with_priority :low_priority
+
+  def perform
+    return unless FeatureToggle.enabled?(:auto_tagging_ability)
+
+    Document.where(auto_tagged: false).each do |doc|
+      list_of_tags = ExternalApi::ClaimEvidenceService.get_key_phrases_from_document(doc.series_id)
+      list_of_tags.each do |tag|
+        new_tag = Tag.find_or_create_by(tag)
+        begin
+          doc.tags << new_tag
+        rescue ActiveRecord::RecordNotUnique
+          errors.push(new_tag.text => "This tag already exists for the document.")
+        end
+    end
+  end
+end
