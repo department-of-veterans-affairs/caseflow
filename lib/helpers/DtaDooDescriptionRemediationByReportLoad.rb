f56c2251f3792f11ac80f2b927e18cc6bc0110c2
raymond-hughes <raymond.hughes2@va.gov>
2023-08-21 08:21:17 -0400

    Updating script to fix duplication

--- lib/helpers/DtaDooDescriptionRemediationByReportLoad.rb
+++ lib/helpers/DtaDooDescriptionRemediationByReportLoad.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 module WarRoom
-
   # Purpose: This remediation is intended to resolve an ongoing issue where a user in VBMS
   # will inadvertantly set the disposition on a contention incorecctly when dealing with
   # DTA Errors and Difference of Opinion.
   class DtaDooDescriptionRemediationByReportLoad
-
-    S3_FILE_NAME = "dta-doo-description-remediation-logs/dta-doo-description-remediation-log"
+    S3_FILE_NAME = "dta-doo-description-remediation-logs"
     S3_ACL = "private"
     S3_ENCRYPTION = "AES256"
     S3_BUCKET = "data-remediation-output"
 
-    def run_by_report_load(report_load, env='prod')
+    def run_by_report_load(report_load)
       logs = ["DtaDooDescriptionRemediation::Log\n"]
       no_remand_generated = []
       remand_generated = []
@@ -20,93 +18,73 @@ module WarRoom
       # Set the user
       RequestStore[:current_user] = User.system_user
 
-      decision_issue_ids = get_decision_issue_ids(rep_load)
+      decision_issues = DecisionIssue.where(id: get_decision_issue_ids(report_load))
 
-      decision_issues = DecisionIssue.where(id: decision_issue_ids)
-
-      # higher_levels_reviews = decision_issues.map { |di| di.decision_review if di.decision_review.is_a?(HigherLevelReview) }.compact.uniq
       higher_levels_reviews = decision_issues.where(type: HigherLevelReview).map(&:decision_review).compact.uniq
 
       higher_levels_reviews.each do |hlr|
-        decision_issues = decision_issues.select { |di| di.decision_review_id == hlr.id && di.decision_review_type == 'HigherLevelReview' }
+        decision_issues = decision_issues.select do |di|
+          di.decision_review_id == hlr.id && di.decision_review_type == 'HigherLevelReview'
+        end
+
         decision_issues.each do |di|
-          prev_disp = di.disposition ? di.disposition : "null"
-          new_disp = ""
-          if di.description.downcase.include?("duty to assist") &&
-             !di.disposition.include?("DTA Error")              &&
-             !di.disposition.include?("Difference of Opinion")
+          next if di.disposition.include?("Difference of Opinion")
+          next if di.disposition.include?("DTA Error")
 
-            new_disp = "DTA Error"
-            di.update!(disposition: new_disp)
+          new_disp = if di.description.downcase.include?("duty to assist")
+                       "DTA Error"
+                     elsif di.description.downcase.include?("difference of opinion")
+                       "Difference of Opinion"
+                     end
 
-            log_message = <<-TEXT
-              #{Time.zone.now} DtaDooDescriptionRemediation::Log
-              HLR ID: #{di.decision_review.id}.  DI ID: #{di.id}
-              Previous Disposition: '#{prev_disp}'.
-              DI Description: #{di.description}
-              Updating Disposition to '#{new_disp}'.
-            TEXT
-            logs.push(log_message)
+          logs.push <<-TEXT
+            #{Time.zone.now} DtaDooDescriptionRemediation::Log
+            HLR ID: #{di.decision_review.id}.  DI ID: #{di.id}
+            Previous Disposition: '#{di.disposition}'.
+            DI Description: #{di.description}
+            Updating Disposition to '#{new_disp}'.
+          TEXT
 
-          elsif di.description.downcase.include?("difference of opinion") &&
-                !di.disposition.include?("Difference of Opinion")         &&
-                !di.disposition.include?("DTA Error")
-
-            new_disp = "Difference of Opinion"
-            di.update!(disposition: new_disp)
-
-            log_message = <<-TEXT
-              #{Time.zone.now} DtaDooDescriptionRemediation::Log
-              HLR ID: #{di.decision_review.id}.  DI ID: #{di.id}
-              Previous Disposition: '#{prev_disp}'.
-              DI Description: #{di.description}
-              Updating Disposition to '#{new_disp}'.
-            TEXT
-            logs.push(log_message)
-          end
+          di.update!(disposition: new_disp)
         end
 
         hlr.create_remand_supplemental_claims!
-        log_message = <<-TEXT
+        logs.push <<-TEXT
           #{Time.zone.now} DtaDooDescriptionRemediation::Log
           Creating Remand Supplemental Claim for
           HLR ID: #{hlr.id}.
         TEXT
-        logs.push(log_message)
       end
 
       logs.push("Remediation Summary Report\n");
-      remand_count = 0;
-      no_remand_count = 0;
+      remand_count = 0
+      no_remand_count = 0
 
       higher_levels_reviews.each do |hlr|
-        supp = SupplementalClaim.find_by(decision_review_remanded_id: hlr.id, decision_review_remanded_type: "HigherLevelReview")
+        supp = SupplementalClaim.find_by(decision_review_remanded_id: hlr.id,
+                                         decision_review_remanded_type: "HigherLevelReview")
         if supp
           remand_count += 1
-          remand_generated.push("Remand Supplemental Claim ID: #{supp.id} was generated for HLR ID: #{hlr.id}.  Claim ID: #{supp&.end_product_establishments&.first&.reference_id}")
+          remand_generated.push("Remand Supplemental Claim ID: #{supp.id} was generated for HLR ID: #{hlr.id}.
+                                Claim ID: #{supp&.end_product_establishments&.first&.reference_id}")
         else
           no_remand_count += 1
           no_remand_generated.push("Error: No Remand Supplemental Claim was generated for HLR ID: #{hlr.id}.")
         end
       end
 
-      log_message = <<-TEXT
+      logs.push <<-TEXT
         Expected Number of Remand Supplemental Claims to be generated: #{hlrs.count}.
         Number of Remand Supplemental Claims created: #{remand_count}.
         Number of Remand Supplemental Claims NOT created: #{no_remand_count}.
       TEXT
-      logs.push(log_message)
 
-      logs = logs + remand_generated + no_remand_generated;
-      upload_logs_to_aws_s3 logs
-
-      # return report to user running remediation
-      logs
-    rescue StandardError => e
-      logs.push("DtaDooDescriptionRemediation::Error -- #{e.message}"\
+      logs = logs + remand_generated + no_remand_generated
+    rescue StandardError => error
+      logs.push("DtaDooDescriptionRemediation::Error -- #{error.message}"\
         "Time: #{Time.zone.now}"\
-        "#{e.backtrace}")
-
+        "#{error.backtrace}")
+    ensure
       upload_logs_to_aws_s3 logs
       logs
     end
@@ -122,12 +100,13 @@ module WarRoom
       filepath = temporary_file.path
       temporary_file.write(content)
       temporary_file.flush
-      s3bucket.object("#{S3_FILE_NAME}-#{Time.zone.now}").upload_file(filepath, acl: S3_ACL, server_side_encryption: S3_ENCRYPTION)
+      s3bucket.object("#{S3_FILE_NAME}-#{Time.zone.now}")
+        .upload_file(filepath, acl: S3_ACL, server_side_encryption: S3_ENCRYPTION)
       temporary_file.close!
     end
 
     # Grab qualifying descision issue IDs so we know what to remediate
-    def get_decision_issue_ids(rep_load, conn)
+    def get_decision_issue_ids(rep_load)
       # Establish connection
       conn = ActiveRecord::Base.connection
 
@@ -205,6 +184,5 @@ module WarRoom
 
       response
     end
-
   end
 end
