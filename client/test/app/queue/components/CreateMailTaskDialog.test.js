e96754a2e0707ae105d7bed020f695af6420a1a6
Marc Steele <Marc.Steele@va.gov>
2023-08-22 09:23:22 -0400

    APPEALS-24727 Update Jest test

--- client/test/app/queue/components/CreateMailTaskDialog.test.js
+++ client/test/app/queue/components/CreateMailTaskDialog.test.js
@@ -12,6 +12,9 @@ import {
 } from './modalUtils';
 import { rootTaskData } from '../../../data/queue/taskActionModals/taskActionModalData';
 import userEvent from '@testing-library/user-event';
+import COPY from '../../../../COPY';
+import ApiUtil from '../../../../app/util/ApiUtil';
+jest.mock('../../../../app/util/ApiUtil');
 
 const renderCreateMailTaskDialog = (storeValues, taskType) => {
   const appealId = getAppealId(storeValues);
@@ -56,13 +59,16 @@ describe('CreateMailTaskDialog', () => {
   });
 
   describe('after selecting Hearing Postponement Request', () => {
+    const label = 'Include Caseflow Reader document hyperlink to request a hearing postponement';
+    const validInput = 'https://vefs-claimevidence-ui-uat.stage.bip.va.gov/file/12345678-1234-1234-1234-twelvetwelve';
+    const instructionsLabel = 'Provide instructions and context for this action';
+
     test('efolder url link field is present', () => {
       setUpMailTaskDialog();
 
       userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
 
-      expect(screen.getByLabelText('Include Caseflow Reader document hyperlink to request a hearing postponement')).
-        toBeTruthy();
+      expect(screen.getByLabelText(label)).toBeTruthy();
     });
 
     test('instructions field is present', () => {
@@ -70,7 +76,62 @@ describe('CreateMailTaskDialog', () => {
 
       userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
 
-      expect(screen.getByLabelText('Provide instructions and context for this action')).toBeTruthy();
+      expect(screen.getByLabelText(instructionsLabel)).toBeTruthy();
+    });
+
+    test('efolder url link field displays error with invalid link format', async () => {
+      jest.useFakeTimers('modern');
+      setUpMailTaskDialog();
+
+      userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
+      userEvent.type(screen.getByLabelText(label), 'asdf');
+
+      expect(await screen.findByText(COPY.EFOLDER_INVALID_LINK_FORMAT)).toBeInTheDocument();
+    });
+
+    test('efolder url link field displays error with vbms when appropriate', async () => {
+      jest.useFakeTimers('modern');
+      setUpMailTaskDialog();
+
+      const response = { status: 500, statusText: 'Error', ok: false };
+
+      ApiUtil.get.mockResolvedValue(response);
+
+      userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
+      userEvent.type(screen.getByLabelText(label), validInput);
+
+      expect(await screen.findByText(COPY.EFOLDER_CONNECTION_ERROR)).toBeInTheDocument();
+      expect(await screen.findByText('Retry')).toBeInTheDocument();
+    });
+
+    test('document not found message appears when no document exists', async () => {
+      jest.useFakeTimers('modern');
+      setUpMailTaskDialog();
+
+      const response = { status: 200, body: { document_presence: false } };
+
+      ApiUtil.get.mockResolvedValue(response);
+
+      userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
+      userEvent.type(screen.getByLabelText(instructionsLabel), 'test instructions');
+      userEvent.type(screen.getByLabelText(label), validInput);
+
+      expect(await screen.findByText(COPY.EFOLDER_DOCUMENT_NOT_FOUND)).toBeInTheDocument();
+    });
+
+    test('submit button becomes enabled when required fields are complete', async () => {
+      jest.useFakeTimers('modern');
+      setUpMailTaskDialog();
+
+      const response = { status: 200, body: { document_presence: true } };
+
+      ApiUtil.get.mockResolvedValue(response);
+
+      userEvent.type(screen.getByRole('combobox'), 'Hearing postponement request{enter}');
+      userEvent.type(screen.getByLabelText(instructionsLabel), 'test instructions');
+      userEvent.type(screen.getByLabelText(label), validInput);
+
+      expect(await screen.findByText('Submit')).toBeEnabled();
     });
   });
 });
