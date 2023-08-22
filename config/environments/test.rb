c49eef71028c9478477ab9d4d2464ff20c67a077
(Jeffrey) Aaron Willis <98484567+Aaron-Willis@users.noreply.github.com>
2023-08-22 15:00:42 -0400

    aaron/APPEALS-28367 (#19210)
    * APPEALS-28367 Added Slack Notification upon job failure on priority_ep_sync_batch_process_job.rb.  Updated RSPEC.
    
    * APPEALS-28367 Added slack  alerts for job failures in populate end product sync queue job.  Refactored error logging and sentry alerts in populate end product sync queue job & priority ep sync batch process job.  Updated RSPEC.
    
    * APPEALS-28367 Added Slack Alert to BatchProcessRescueJob upon failure to reprocess.  Updated RSPEC.
    
    * APPEALS-28367 Added more negative testing for slack alerts in populate_end_product_sync_queue_job.rb.
    
    * APPEALS-28367 Refactored batch_process.rb by adding private attr_accessors for counter instance variables.
    
    * APPEALS-28367 Removed extranneous blank line from end_product_establishment factory.
    
    * APPEALS-28367 Added Slack Alert negative testing to RSPEC for batch_process_rescue_job & priority_ep_sync_batch_process_job_spec.
    
    * APPEALS-28367 Fixed linting issue for ready_to_batch scope in priority_end_product_sync_queue.rb.
    
    * APPEALS-28367 Refactored RSPEC.
    
    * APPEALS-28367 Removed Instance variables from RSPECs.  Updated ENV variables in test.rb
    
    * APPEALS-28367 Updated parameter name in no-op method in batch_process.rb to resolve code climate issue.  Updated RSPEC to reflect this change.
    
    * APPEALS-28367 Added Reek statements to address various code climate alerts.
    
    * APPEALS-28367 Removed SLEEP DURATION Stubbing in caseflow_stuck_records to address code climate alert.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -89,10 +89,10 @@ Rails.application.configure do
   ENV["AWS_ACCESS_KEY_ID"] ||= "dummykeyid"
   ENV["AWS_SECRET_ACCESS_KEY"] ||= "dummysecretkey"
 
-# BatchProcess ENVs
-    # priority_ep_sync
+  # BatchProcess ENVs
+  # priority_ep_sync
   ENV["BATCH_PROCESS_JOB_DURATION"] ||= "50" # Number of minutes the job will run for
-  ENV["BATCH_PROCESS_SLEEP_DURATION"] ||= "5" # Number of seconds between loop iterations
+  ENV["BATCH_PROCESS_SLEEP_DURATION"] ||= "0" # Number of seconds between loop iterations
   ENV["BATCH_PROCESS_BATCH_LIMIT"]||= "100" # Max number of records in a batch
   ENV["BATCH_PROCESS_ERROR_DELAY"] ||= "3" # In number of hours
   ENV["BATCH_PROCESS_MAX_ERRORS_BEFORE_STUCK"] ||= "3" # When record errors for X time, it's declared stuck
@@ -117,8 +117,8 @@ Rails.application.configure do
 
   # Populate End Product Sync Queue ENVs
   ENV["END_PRODUCT_QUEUE_JOB_DURATION"] ||= "50" # Number of minutes the job will run for
-  ENV["END_PRODUCT_QUEUE_SLEEP_DURATION"] ||= "5" # Number of seconds between loop iterations
-  ENV["END_PRODUCT_QUEUE_BATCH_LIMIT"] ||= "500" # Max number of records in a batch
+  ENV["END_PRODUCT_QUEUE_SLEEP_DURATION"] ||= "0" # Number of seconds between loop iterations
+  ENV["END_PRODUCT_QUEUE_BATCH_LIMIT"] ||= "250" # Max number of records in a batch
 
   # Travel Board Sync Batch Size
   ENV["TRAVEL_BOARD_HEARING_SYNC_BATCH_LIMIT"] ||= "250"
