6b92f100ccc00e0eb65c8f190c2386c03e6f6d98
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-08-24 09:33:47 -0700

    APPEALS-29120 - init commit, fixed linting and code climate issues rspec passes

--- app/models/concerns/sync_lock.rb
+++ app/models/concerns/sync_lock.rb
@@ -12,10 +12,12 @@ module SyncLock
       lock_key = "hlr_sync_lock:#{end_product_establishment.id}"
 
       begin
-        # create the sync lock with a key, value pair only IF it doesn't already exist and give it an expiration time upon creation
-        sync_lock_acquired = redis.set(lock_key, "lock is set", :nx => true, :ex => LOCK_TIMEOUT.to_i)
+        # create the sync lock with a key, value pair only IF it doesn't already exist
+        # and give it an expiration time upon creation
+        sync_lock_acquired = redis.set(lock_key, "lock is set", nx: true, ex: LOCK_TIMEOUT.to_i)
 
-        fail Caseflow::Error::SyncLockFailed, message: "#{Time.zone.now}" unless sync_lock_acquired
+        fail Caseflow::Error::SyncLockFailed, message: Time.zone.now.to_s unless sync_lock_acquired
+
         # set expire as another failsafe
         redis.expire(lock_key, LOCK_TIMEOUT.to_i)
         yield
