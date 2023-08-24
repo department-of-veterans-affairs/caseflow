e2d120a7de0929d71e6bfba0e51232b7f7cf611d
631068 <rickybmanus@gmail.com>
2023-08-24 12:56:47 -0400

    Fixed submit validation for post aoj

--- app/models/remand_reason.rb
+++ app/models/remand_reason.rb
@@ -2,6 +2,5 @@
 
 class RemandReason < CaseflowRecord
   validates :code, inclusion: { in: Constants::AMA_REMAND_REASONS_BY_ID.values.map(&:keys).flatten }
-  validates :post_aoj, inclusion: { in: [true, false] }
   belongs_to :decision_issue
 end
