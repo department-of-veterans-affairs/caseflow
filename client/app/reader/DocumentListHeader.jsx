384ea1ff30eb228853aca093649d26742c43146a
IsaiahBar <isaiah.barnett@va.gov>
2023-08-25 10:11:07 -0400

    update Results For

--- client/app/reader/DocumentListHeader.jsx
+++ client/app/reader/DocumentListHeader.jsx
@@ -11,7 +11,15 @@ import DocumentsCommentsButton from './DocumentsCommentsButton';
 import HeaderFilterMessage from './HeaderFilterMessage';
 import SearchBar from '../components/SearchBar';
 
+
 class DocumentListHeader extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
   // Record the search value for analytics purposes. Don't worry if it fails.
   recordSearch = (query) => {
     ApiUtil.post(
@@ -28,25 +36,33 @@ class DocumentListHeader extends React.Component {
       });
   }
 
+  handleSearchChange = (value) => {
+    this.setState({
+      searchInput: value
+    });
+  }
   render() {
     const props = this.props;
 
     return <div>
       <div className="document-list-header">
-        <div className="search-bar-and-doc-count cf-search-ahead-parent">
+        <div className="search-bar-and-doc-count">
           <div><SearchBar
             id="searchBar"
-            onChange={props.setSearch}
+            onSubmit= {props.setSearch}
             onClearSearch={props.clearSearch}
             recordSearch={this.recordSearch}
-            isSearchAhead
+            onChange={this.handleSearchChange}
+            value={this.state.searchInput}
             placeholder="Type to search..."
-            value={props.docFilterCriteria.searchQuery}
-            size="small"
+            submitUsingEnterKey
           />
-          <ul>
-            <b>Results for:</b> {props.docFilterCriteria.searchQuery}
-          </ul>
+          { props.docFilterCriteria.searchQuery && (
+            <ul>
+              <b text>Results for:</b> {props.docFilterCriteria.searchQuery}
+            </ul>
+          )
+          }
           </div>
           <div className="num-of-documents">
             {props.numberOfDocuments} Documents
