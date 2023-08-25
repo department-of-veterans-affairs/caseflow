3736ca68a71dcaffeb7e81d43bd43b84eca162aa
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-08-25 14:48:56 -0700

    commit to init GHA

--- app/models/concerns/sync_lock.rb
+++ app/models/concerns/sync_lock.rb
@@ -14,7 +14,7 @@ module SyncLock
 
       begin
         # create the sync lock with a key, value pair only IF it doesn't already exist
-        # and give it an expiration time upon creation
+        # and give it an expiration time upon creation.
         sync_lock_acquired = redis.set(lock_key, "lock is set", nx: true, ex: LOCK_TIMEOUT.to_i)
 
         fail Caseflow::Error::SyncLockFailed, message: Time.zone.now.to_s unless sync_lock_acquired
