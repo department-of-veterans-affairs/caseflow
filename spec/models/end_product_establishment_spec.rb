933411b8f9bf8f546aa57fe424e36f4da82ef725
Will Love <wlove29@gmail.com>
2023-08-25 12:38:55 -0400

    fixed linting on sync and added test for RedisMutex lock error (#19237)
    * fixed linting on sync and added test for RedisMutex lock error
    
    * added epe id to display in error message
    
    * fixed linting
    
    * rubocop long line fixes
    
    * fixed new line causing test failure
    
    * fixed new lines
    
    * fixed rubocop long lines
    
    * APPEALS-28960 Updated formatting of log message and content to include "#sync!".
    
    ---------
    
    Co-authored-by: TuckerRose <tuckerrose@tuckerroses-mbp.lan>
    Co-authored-by: Jeffrey Aaron Willis <Willis_Jeffrey@bah.com>

--- spec/models/end_product_establishment_spec.rb
+++ spec/models/end_product_establishment_spec.rb
@@ -851,6 +851,19 @@ describe EndProductEstablishment, :postgres do
       ]
     end
 
+    context "when lock acquisition fails" do
+      before do
+        allow(RedisMutex).to receive(:with_lock).and_raise(RedisMutex::LockError)
+      end
+
+      it "logs the error message" do
+        expect(Rails.logger).to receive(:error)
+          .with("Failed to acquire lock for EPE ID: #{end_product_establishment.id}!"\
+                "  #sync! is being called by another process. Please try again later.")
+        end_product_establishment.sync!
+      end
+    end
+
     context "when matching end product has not yet been established" do
       it "raises EstablishedEndProductNotFound error" do
         expect { subject }.to raise_error(EndProductEstablishment::EstablishedEndProductNotFound)
