e5d3d78df96ba6c40638def6bb0a5f48e5a5c772
Prajwal Amatya <pamatya@take2it.com>
2023-08-25 12:26:29 -0600

    refactor step 1 sanitized json

---
+++ db/seeds/businesss_line.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Veterans Health Administration related seeds
+
+module Seeds
+  class BusinessLine < Base
+    def seed!
+      create_business_lines
+    end
+
+    private
+    def create_business_lines
+
+    end
+  end
+end
