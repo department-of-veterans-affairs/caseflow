1d072485d435accd7c0d0e19f90f760861bd7ec2
raymond-hughes <raymond.hughes2@va.gov>
2023-08-30 19:36:19 -0400

    Fixing error from testing in UAT

--- lib/helpers/scripts/dta_dto_description_remediation_by_report_load.sh
+++ lib/helpers/scripts/dta_dto_description_remediation_by_report_load.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd /opt/caseflow-certification/src; bin/rails c << DONETOKEN
 x = WarRoom::DtaDooDescriptionRemediationByReportLoad.new
-x.run_by_report_load("$1", "$2")
+x.run_by_report_load("$1")
 DONETOKEN
