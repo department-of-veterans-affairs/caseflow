7ea173d73df38f3f5126f66818d372cd8e91a56a
Jonathan Cohen <cohen_jonathan@bah.com>
2023-08-30 12:08:21 -0400

    updates to the DeleteConferenceLinkJob file. migration received at branching was also ran at this point.

--- app/jobs/hearings/delete_conference_link_job.rb
+++ app/jobs/hearings/delete_conference_link_job.rb
@@ -4,7 +4,33 @@ class DeleteConferenceLinkJob < CaseflowJob
   queue_with_priority :low_priority
 
   def perform
-
+    links_for_past_date = retreive_stale_conference_links
+    soft_delete_links(links_for_past_date)
   end
 
+  private
+
+  def retreive_stale_conference_links
+    ConferenceLink.joins(:hearing_day).where("scheduled_for < ?", Date.today)
+  end
+
+  def soft_delete_links(collection)
+    collection.each do |old_link|
+      old_link.update!(update_conf_links)
+    end
+  end
+
+  def update_conf_links
+    {
+      conference_deleted: true,
+      updated_by_id: RequestStore[:current_user],
+      updated_at: Time.zone.now,
+      guest_hearing_link: nil,
+      # guest_pin: nil,
+      guest_pin_long: nil,
+      host_link: nil,
+      host_pin: nil,
+      host_pin_long: nil
+    }
+  end
 end
