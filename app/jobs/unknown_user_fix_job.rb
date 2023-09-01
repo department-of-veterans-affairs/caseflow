36353f67e8beed4e2c47e77d13b726c9c1fad9da
Griffin Dooley <dooleygriffin@gmail.com>
2023-09-01 13:58:09 -0400

    Regex for datetime

--- app/jobs/unknown_user_fix_job.rb
+++ app/jobs/unknown_user_fix_job.rb
@@ -3,11 +3,20 @@
 class UnknownUserFixJob < CaseflowJob
   ERROR_TEXT = "UnknownUser"
 
-  def perform(date)
-    stuck_job_report_service = StuckJobReportService.new
+  def perform(date = "2023-08-07")
+    date = date.to_s
+    pattern = /^\d{4}-\d{2}-\d{2}$/
+    # return puts "incorrect date format" if !date&.match?(pattern)
 
+    if !date&.match?(pattern)
+      puts "incorrect date format, use \"YYYY-mm-dd\""
+      return
+    end
+
+    date = Time.zone.parse(date)
     return if rius_with_errors.blank?
 
+    stuck_job_report_service = StuckJobReportService.new
     stuck_job_report_service.append_record_count(rius_with_errors.count, ERROR_TEXT)
 
     rius_with_errors.each do |single_riu|
@@ -22,15 +31,9 @@ class UnknownUserFixJob < CaseflowJob
         stuck_job_report_service.append_errors(single_riu.class.name, single_riu.id, error)
       end
     end
-
-
   end
 
   def rius_with_errors
     RequestIssuesUpdate.where("error ILIKE ?", "%#{ERROR_TEXT}%")
   end
 end
-
-
-# Time.parse("2001-12-21")
-
