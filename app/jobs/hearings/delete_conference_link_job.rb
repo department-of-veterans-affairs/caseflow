ee1b125bcbd201e4dc1d17262ad3c9e5c4e7ad57
Jonathan Cohen <cohen_jonathan@bah.com>
2023-09-01 17:14:04 -0400

    APPEALS-29006 Updated job with comments providing explanation on the methods used.Also included a begin/end block for some error handling through use of the log_error method provided through inheritance.

--- app/jobs/hearings/delete_conference_link_job.rb
+++ app/jobs/hearings/delete_conference_link_job.rb
@@ -1,32 +1,54 @@
 # frozen_string_literal: true
 
+##
+#
+
 class DeleteConferenceLinkJob < CaseflowJob
   queue_with_priority :low_priority
 
   def perform
-    links_for_past_date = retreive_stale_conference_links
-    soft_delete_links(links_for_past_date)
+    begin
+      links_for_past_date = retreive_stale_conference_links
+      links_soft_removal(links_for_past_date)
+    rescue ActiveRecordError => error
+      log_error(error)
+    end
   end
 
   private
 
+  # Purpose: Queries the database table of conference_links that are associated with a hearing_day that has already passed.
+  #
+  # Params: None
+  #
+  # Return: A collection of links that have passed.
   def retreive_stale_conference_links
     ConferenceLink.joins(:hearing_day).where("scheduled_for < ?", Date.today)
   end
 
-  def soft_delete_links(collection)
+  # Purpose: Iterates through a collection of links, updating each item and then soft_deleting.
+  #
+  # Params: An array of conference_links.
+  #
+  # Return: None
+  def links_soft_removal(collection)
     collection.each do |old_link|
-      old_link.update!(update_conf_links)
+      # old_link.update!(update_conf_links)
+      old_link.destroy
     end
   end
 
+  # Purpose: Updates conference_link attributes when passed into the 'update!' method.
+  #
+  # Params: None
+  #
+  # Return: Hash that will update the conference_link
   def update_conf_links
     {
       conference_deleted: true,
       updated_by_id: RequestStore[:current_user],
       updated_at: Time.zone.now,
       guest_hearing_link: nil,
-      # guest_pin: nil,
       guest_pin_long: nil,
       host_link: nil,
       host_pin: nil,
@@ -34,5 +56,5 @@ class DeleteConferenceLinkJob < CaseflowJob
     }
   end
 
-  # TODO Create a Hearing Day scheduled in the future. Given that the link is created, that would allow me to test the job and see how many it receives.
+  # TODO
 end
