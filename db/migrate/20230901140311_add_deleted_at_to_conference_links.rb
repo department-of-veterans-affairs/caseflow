4b82f856f2dcf1a66e88a10766c0bc2e0d3c3774
Jonathan Cohen <cohen_jonathan@bah.com>
2023-09-01 14:33:49 -0400

    schema where it needs to be. before new migration.

--- db/migrate/20230901140311_add_deleted_at_to_conference_links.rb
+++
@@ -1,6 +0,0 @@
-class AddDeletedAtToConferenceLinks < Caseflow::Migration
-  def change
-    add_column :conference_links, :deleted_at, :datetime, comment: "Neccessary column for soft delete functionality."
-    add_index :conference_links, :deleted_at
-  end
-end
