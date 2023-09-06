2eda1307f28e74f9de784b0ad999b011b90ba688
breedbah <brandon.reed3@va.gov>
2023-09-06 11:54:59 -0400

    APPELAS-25116 update daily docket lint

--- client/app/hearings/components/dailyDocket/DailyDocket.jsx
+++ client/app/hearings/components/dailyDocket/DailyDocket.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
-import moment from "moment";
-import { css } from "glamor";
-import _ from "lodash";
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { css } from 'glamor';
+import _ from 'lodash';
 
-import AppSegment from "@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment";
-import Alert from "../../../components/Alert";
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import Alert from '../../../components/Alert';
 import {
   LockModal,
   RemoveHearingModal,
   DispositionModal,
-} from "./DailyDocketModals";
-import Button from "../../../components/Button";
-import StatusMessage from "../../../components/StatusMessage";
-import DailyDocketRows from "./DailyDocketRows";
-import DailyDocketEditLinks from "./DailyDocketEditLinks";
+} from './DailyDocketModals';
+import Button from '../../../components/Button';
+import StatusMessage from '../../../components/StatusMessage';
+import DailyDocketRows from './DailyDocketRows';
+import DailyDocketEditLinks from './DailyDocketEditLinks';
 import {
   isPreviouslyScheduledHearing,
   userJudgeOrCoordinator,
-} from "../../utils";
-import { navigateToPrintPage } from "../../../util/PrintUtil";
-import { encodeQueryParams } from "../../../util/QueryParamsUtil";
-import COPY from "../../../../COPY";
-import UserAlerts from "../../../components/UserAlerts";
-import HEARING_DISPOSITION_TYPES from "../../../../constants/HEARING_DISPOSITION_TYPES";
-import { ScheduledInErrorModal } from "../ScheduledInErrorModal";
-import { PexipDailyDocketGuestLink } from "./PexipDailyDocketGuestLink";
-import { WebexDailyDocketGuestLink } from "./WebexDailyDocketGuestLink";
+} from '../../utils';
+import { navigateToPrintPage } from '../../../util/PrintUtil';
+import { encodeQueryParams } from '../../../util/QueryParamsUtil';
+import COPY from '../../../../COPY';
+import UserAlerts from '../../../components/UserAlerts';
+import HEARING_DISPOSITION_TYPES from '../../../../constants/HEARING_DISPOSITION_TYPES';
+import { ScheduledInErrorModal } from '../ScheduledInErrorModal';
+import { PexipDailyDocketGuestLink } from './PexipDailyDocketGuestLink';
+import { WebexDailyDocketGuestLink } from './WebexDailyDocketGuestLink';
 
 const alertStyling = css({
-  marginBottom: "30px",
+  marginBottom: '30px',
 });
 
 const Alerts = ({
@@ -46,14 +46,10 @@ const Alerts = ({
         type="success"
         styling={alertStyling}
         title={
-          dailyDocket.lock
-            ? "You have successfully locked this Hearing Day"
-            : "You have successfully unlocked this Hearing Day"
+          dailyDocket.lock ? 'You have successfully locked this Hearing Day' : 'You have successfully unlocked this Hearing Day'
         }
         message={
-          dailyDocket.lock
-            ? "You cannot add more veterans to this hearing day, but you can edit existing entries"
-            : "You can now add more veterans to this hearing day"
+          dailyDocket.lock ? 'You cannot add more veterans to this hearing day, but you can edit existing entries' : 'You can now add more veterans to this hearing day'
         }
       />
     )}
@@ -81,9 +77,9 @@ const Alerts = ({
         type="error"
         styling={alertStyling}
         title={`VACOLS Hearing Day ${moment(dailyDocket.scheduledFor).format(
-          "M/DD/YYYY"
+          'M/DD/YYYY'
         )}
-           cannot be locked in Caseflow.`}
+          cannot be locked in Caseflow.`}
         message="VACOLS Hearing Day cannot be locked"
       />
     )}
@@ -133,9 +129,7 @@ export default class DailyDocket extends React.Component {
 
     // for Central hearing days, return 'C'
     // Otherwise assume it's a video hearing day and return RO key
-    return dailyDocket.requestType === "C"
-      ? "C"
-      : dailyDocket.regionalOfficeKey;
+    return dailyDocket.requestType === 'C' ? 'C' : dailyDocket.regionalOfficeKey;
   };
 
   openDispositionModal = ({
@@ -190,7 +184,7 @@ export default class DailyDocket extends React.Component {
       (hearing) => hearing.externalId
     );
     const queryString = encodeQueryParams({
-      hearing_ids: hearingIds.join(","),
+      hearing_ids: hearingIds.join(','),
       keep_open: true,
     });
 
@@ -298,7 +292,7 @@ export default class DailyDocket extends React.Component {
           <div className="cf-push-right">
             {user.userHasHearingPrepRole && (
               <Button
-                classNames={["usa-button-secondary"]}
+                classNames={['usa-button-secondary']}
                 onClick={this.navigateToPrintAllPage}
                 disabled={_.isEmpty(docketHearings)}
               >
@@ -325,19 +319,17 @@ export default class DailyDocket extends React.Component {
           user={user}
         />
         {!hasDocketHearings && (
-          <div {...css({ marginTop: "75px" })}>
+          <div {...css({ marginTop: '75px' })}>
             <StatusMessage
               title={
-                user.userHasHearingPrepRole
-                  ? COPY.HEARING_SCHEDULE_DOCKET_JUDGE_WITH_NO_HEARINGS
-                  : COPY.HEARING_SCHEDULE_DOCKET_NO_VETERANS
+                user.userHasHearingPrepRole ? COPY.HEARING_SCHEDULE_DOCKET_JUDGE_WITH_NO_HEARINGS : COPY.HEARING_SCHEDULE_DOCKET_NO_VETERANS
               }
               type="status"
             />
           </div>
         )}
         {hasPrevHearings && (
-          <div {...css({ marginTop: "75px" })}>
+          <div {...css({ marginTop: '75px' })}>
             <h1>Previously Scheduled</h1>
             <DailyDocketRows
               hidePreviouslyScheduled={false}
