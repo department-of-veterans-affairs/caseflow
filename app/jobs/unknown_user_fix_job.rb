0ff26ff20d65507d8d369630f88eb663f3596584
AdamShawBAH <adam.shaw@va.gov>
2023-09-07 14:12:36 -0400

    Error handling added

--- app/jobs/unknown_user_fix_job.rb
+++ app/jobs/unknown_user_fix_job.rb
@@ -6,21 +6,24 @@ class UnknownUserFixJob < CaseflowJob
   def perform(date = "2023-08-07")
     date = date.to_s
     pattern = /^\d{4}-\d{2}-\d{2}$/
-    # return puts "incorrect date format" if !date&.match?(pattern)
 
-    if !date&.match?(pattern)
-      puts "incorrect date format, use \"YYYY-mm-dd\""
-      return
+    begin
+      if !date.match?(pattern)
+        raise ArgumentError, "Incorrect date format, use 'YYYY-mm-dd'"
+      end
+
+      parsed_date = Time.zone.parse(date)
+    rescue ArgumentError => e
+      Rails.logger.error("Error: #{e.message}")
     end
 
-    date = Time.zone.parse(date)
     return if rius_with_errors.blank?
 
     stuck_job_report_service = StuckJobReportService.new
     stuck_job_report_service.append_record_count(rius_with_errors.count, ERROR_TEXT)
 
     rius_with_errors.each do |single_riu|
-      next if single_riu.created_at > date
+      next if single_riu.created_at > parsed_date
 
       stuck_job_report_service.append_single_record(single_riu.class.name, single_riu.id)
 
