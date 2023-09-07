b23eb849614f13f8e1a6faaf4ce67d63a4b704c6
Marc Steele <Marc.Steele@va.gov>
2023-09-07 10:17:05 -0400

    APPEALS-24720 Refactor utility methods from HPR to parent

--- app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
+++ app/models/tasks/hearing_mail_tasks/hearing_request_mail_task.rb
@@ -33,6 +33,27 @@ class HearingRequestMailTask < MailTask
     []
   end
 
+  # Purpose: Only show task assigned to "HearingAdmin" on the Case Timeline
+  # Params: None
+  # Return: boolean if task is assigned to MailTeam
+  def hide_from_case_timeline
+    assigned_to.is_a?(MailTeam)
+  end
+
+  # Purpose: Determines if there is an open hearing
+  # Params: None
+  # Return: The hearing if one exists
+  def open_hearing
+    @open_hearing ||= open_assign_hearing_disposition_task&.hearing
+  end
+
+  # Purpose: Gives the latest hearing task
+  # Params: None
+  # Return: The hearing task
+  def hearing_task
+    @hearing_task ||= open_hearing&.hearing_task || active_schedule_hearing_task.parent
+  end
+
   private
 
   # Ensure create is called on a descendant mail task and not directly on the HearingRequestMailTask class
@@ -42,18 +63,42 @@ class HearingRequestMailTask < MailTask
     end
   end
 
-  def active_schedule_hearing_task?
-    appeal.tasks.where(type: ScheduleHearingTask.name).active.any?
+  # Purpose: Gives the latest active hearing task
+  # Params: None
+  # Return: The latest active hearing task
+  def active_schedule_hearing_task
+    appeal.tasks.of_type(ScheduleHearingTask.name).active.first
   end
 
-  def open_assign_hearing_disposition_task?
-    # ChangeHearingDispositionTask is a subclass of AssignHearingDispositionTask
-    disposition_task_names = [AssignHearingDispositionTask.name, ChangeHearingDispositionTask.name]
-    open_task = appeal.tasks.where(type: disposition_task_names).open.first
+  # ChangeHearingDispositionTask is a subclass of AssignHearingDispositionTask
+  ASSIGN_HEARING_DISPOSITION_TASKS = [
+    AssignHearingDispositionTask.name,
+    ChangeHearingDispositionTask.name
+  ].freeze
 
-    return false unless open_task&.hearing
+  # Purpose: Gives the latest active assign hearing disposition task
+  # Params: None
+  # Return: The latest active assign hearing disposition task
+  def open_assign_hearing_disposition_task
+    @open_assign_hearing_disposition_task ||= appeal.tasks.of_type(ASSIGN_HEARING_DISPOSITION_TASKS).open&.first
+  end
 
-    # Ensure hearing associated with AssignHearingDispositionTask is not scheduled in the past
-    !open_task.hearing.scheduled_for_past?
+  # Purpose: Associated appeal has an upcoming hearing with an open status
+  # Params: None
+  # Return: Returns a boolean if the appeal has an upcoming hearing
+  def hearing_scheduled_and_awaiting_disposition?
+    return false unless open_hearing
+
+    # Ensure associated hearing is not scheduled for the past
+    !open_hearing.scheduled_for_past?
+  end
+
+  # Purpose: Deletes the old scheduled virtual hearings
+  # Params: None
+  # Return: Returns nil
+  def clean_up_virtual_hearing
+    if open_hearing.virtual?
+      perform_later_or_now(VirtualHearings::DeleteConferencesJob)
+    end
   end
 end
