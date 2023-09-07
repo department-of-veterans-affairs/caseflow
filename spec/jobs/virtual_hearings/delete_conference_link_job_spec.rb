d289761ee0ead32eb2930d5af388187c701565b7
Jonathan Cohen <cohen_jonathan@bah.com>
2023-09-07 14:33:58 -0400

    APPEALS-29006 comment cleanup in test.

--- spec/jobs/virtual_hearings/delete_conference_link_job_spec.rb
+++ spec/jobs/virtual_hearings/delete_conference_link_job_spec.rb
@@ -11,7 +11,6 @@ describe VirtualHearings::DeleteConferenceLinkJob do
   let!(:past_hearing_day_with_link) { FactoryBot.create(:hearing_day, :virtual, :past_with_link) }
 
   context ".perform" do
-    # subject(:job) { VirtualHearings::DeleteConferenceLinkJob.new }
     it "When conference links in the DB are past the date of the date the job is run" do
       expect(ConferenceLink.count).to be(2)
       perform_enqueued_jobs { VirtualHearings::DeleteConferenceLinkJob.perform_now }
