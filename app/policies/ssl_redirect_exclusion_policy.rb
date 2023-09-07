42f5322ec18fb8ce438a1d3aecca6c452f239fe2
AnandEdara <131183324+AnandEdara@users.noreply.github.com>
2023-09-07 08:53:17 -0500

    Update ssl_redirect_exclusion_policy.rb

--- app/policies/ssl_redirect_exclusion_policy.rb
+++ app/policies/ssl_redirect_exclusion_policy.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
 require 'set'
 
 class SslRedirectExclusionPolicy
-  
   EXEMPT_PATH_PATTERNS = Set.new([
-    %r(^/api/docs/v3/),
-    %r(^/api/metadata$),
-    %r(^/health-check$),
-    %r(^/idt/api/v1/),
-    %r(^/idt/api/v2/)
+      %r{^/api/docs/v3/},
+      %r{^/api/metadata$},
+      %r{^/health-check$},
+      %r{^/idt/api/v1/},
+      %r{^/idt/api/v2/}
   ]).freeze
 
   def self.call(request)
