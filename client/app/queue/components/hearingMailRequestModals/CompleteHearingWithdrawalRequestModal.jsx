30a9c3710b42032b2241791efe813bbc1fb194d3
Jeff Marks <jeff.t.marks@gmail.com>
2023-09-08 14:34:37 -0400

    APPEALS-24723 Add validation on modal

--- client/app/queue/components/hearingMailRequestModals/CompleteHearingWithdrawalRequestModal.jsx
+++ client/app/queue/components/hearingMailRequestModals/CompleteHearingWithdrawalRequestModal.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import QueueFlowModal from '../QueueFlowModal';
 import TextareaField from '../../../components/TextareaField';
 import COPY from '../../../../COPY';
 
 const CompleteHearingWithdrawalRequestModal = (props) => {
+  const [instructions, setInstructions] = useState('');
+
   const validateForm = () => {
-    return true;
+    return instructions !== '';
   };
 
-  const submit = () => {
-    console.log(props);
-    console.log('submit!');
-  };
+  const submit = () => console.log(props);
 
   return (
     <QueueFlowModal
-      title="Mark as complete"
-      button="Mark as complete"
+      title="Mark as complete and withdraw hearing"
+      button="Mark as complete & withdraw hearing"
       submitDisabled={!validateForm()}
       validateForm={validateForm}
       submit={submit}
       // pathAfterSubmit={`/queue/appeals/${appealId}`}
     >
+      <div>By marking this task as complete, you will withdraw the hearing</div>
+      <br />
+      <div>{COPY.WITHDRAW_HEARING.AMA.MODAL_BODY}</div>
+      <br />
       <TextareaField
         label={`${COPY.PROVIDE_INSTRUCTIONS_AND_CONTEXT_LABEL}:`}
         name="instructionsField"
         id="completePostponementInstructions"
-        // onChange={(value) => dispatch({ type: 'instructions', payload: value })}
-        // value={state.instructions}
-        // styling={marginBottom(0)}
+        onChange={setInstructions}
+        value={instructions}
       />
     </QueueFlowModal>
   );
