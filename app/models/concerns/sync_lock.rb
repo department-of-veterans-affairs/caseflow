ee170c05e8459124894c106a8c85e9d236dbf4c9
Jonathan Tsang <tsang_jonathan@bah.com>
2023-09-12 14:22:06 -0400

    updated logging conditional logic

--- app/models/concerns/sync_lock.rb
+++ app/models/concerns/sync_lock.rb
@@ -10,20 +10,21 @@ module SyncLock
     if decision_review.is_a?(HigherLevelReview) && block_given?
       redis = Redis.new(url: Rails.application.secrets.redis_url_cache)
       lock_key = "hlr_sync_lock:#{end_product_establishment.id}"
-      Rails.logger.info(lock_key + " has been created")
 
       begin
         # create the sync lock with a key, value pair only IF it doesn't already exist
         # and give it an expiration time upon creation.
         sync_lock_acquired = redis.set(lock_key, "lock is set", nx: true, ex: LOCK_TIMEOUT.to_i)
+        Rails.logger.info(lock_key + " has been created") if sync_lock_acquired
 
         fail Caseflow::Error::SyncLockFailed, message: Time.zone.now.to_s unless sync_lock_acquired
 
         yield
       ensure
-        redis.del(lock_key)
-        # if lock_key is false then log has been released
-        unless redis.get(lock_key)
+        # Delete the lock upon exiting if it was created during this session
+        redis.del(lock_key) if sync_lock_acquired
+        # if lock was acquired and is later unretrievable, then it was deleted/expired
+        if !redis.get(lock_key) && sync_lock_acquired
           Rails.logger.info(lock_key + " has been released")
         end
       end
