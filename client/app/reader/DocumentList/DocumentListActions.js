406e9639e8d1b6ed281ea7d0f052abefd9b6e241
Ryan Marshall <rnmarshall1993@gmail.com>
2023-09-13 13:25:50 -0700

    added analytics and removed console.log

--- client/app/reader/DocumentList/DocumentListActions.js
+++ client/app/reader/DocumentList/DocumentListActions.js
@@ -132,12 +132,18 @@ export const clearDocFilters = () => (dispatch) => {
 
 export const setDocTypes = (docToAdd) => (dispatch) => {
   dispatch({
-  type: Constants.SET_DOC_TYPES,
-  payload: {
-    docToAdd
-  }
-  })
-}
+    type: Constants.SET_DOC_TYPES,
+    payload: {
+      docToAdd
+    },
+    meta: {
+      analytics: {
+        category: CATEGORIES.CLAIMS_FOLDER_PAGE,
+        action: 'set-doc-types'
+      }
+    }
+  });
+};
 
 export const clearTagFilters = () => (dispatch) => {
   dispatch({
@@ -162,13 +168,13 @@ export const setRecieptDateFilter = (recieptFilterType, recieptDatesHash) => (di
     meta: {
       analytics: {
         category: CATEGORIES.CLAIMS_FOLDER_PAGE,
-        action: 'set RecieptFilterType-' + recieptFilterType,
+        action: `set RecieptFilterType-${ recieptFilterType}`,
         label: 'setRecieptFilter'
       }
     }
   });
   dispatch(updateFilteredIdsAndDocs());
-}
+};
 // Scrolling
 
 export const setDocListScrollPosition = (scrollTop) => ({
