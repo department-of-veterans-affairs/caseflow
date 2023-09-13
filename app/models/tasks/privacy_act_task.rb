26837d8cbf6a40ef65aad4084e1308ba196130a4
Kamala Madamanchi <110078646+kamala-07@users.noreply.github.com>
2023-09-12 19:15:18 -0500

    Kamalam7/appeals 29116 v1 (#19429)
    * Change to Edge Scenario1
    
    * Change for Edge Scenario 1
    
    * APPEALS-29116 Scenario-1 edge case
    
    ---------
    
    Co-authored-by: piedram <maite.piedrayera@va.gov>
    Co-authored-by: cacevesva <109166981+cacevesva@users.noreply.github.com>

--- app/models/tasks/privacy_act_task.rb
+++ app/models/tasks/privacy_act_task.rb
@@ -7,7 +7,7 @@ class PrivacyActTask < Task
   include CavcAdminActionConcern
 
   ## Tag to determine if this task is considered a blocking task for Legacy Appeal Distribution
-  def legacy_blocking
+  def self.legacy_blocking?
     true
   end
 
