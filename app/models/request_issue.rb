05d653a47db76f3f9c115b13458b682e324e8c37
Jonathan Tsang <tsang_jonathan@bah.com>
2023-09-14 18:22:48 -0400

    hotfix

--- app/models/request_issue.rb
+++ app/models/request_issue.rb
@@ -423,13 +423,13 @@ class RequestIssue < CaseflowRecord
   def sync_decision_issues!(target_time = Time.zone.now)
     # add sleep timer for uat testing specific RequestIssues tied to certain EPE
     if (end_product_establishment_id == 4142 || [16606, 16607, 16608, 16609, 16610].include?(id))
-      target_time = Time.zone.new(2023, 9, 15, 14, 00, 0)
+      target_time = Time.new(2023, 9, 15, 14, 00, 0)
     elsif (end_product_establishment_id == 4143 || [16611, 16612, 16613, 16614, 16615].include?(id))
-      target_time = Time.zone.new(2023, 9, 15, 14, 10, 0)
+      target_time = Time.new(2023, 9, 15, 14, 10, 0)
     elsif (end_product_establishment_id == 4144 || [16616, 16617, 16618, 16619, 16620].include?(id))
-      target_time = Time.zone.new(2023, 9, 15, 14, 20, 0)
+      target_time = Time.new(2023, 9, 15, 14, 20, 0)
     elsif (end_product_establishment_id == 4145 || [16621, 16622, 16623, 16624, 16625].include?(id))
-      target_time = Time.zone.new(2023, 9, 15, 14, 30, 0)
+      target_time = Time.new(2023, 9, 15, 14, 30, 0)
     end
 
     sleep_until(target_time)
