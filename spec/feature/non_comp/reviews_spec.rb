67a32220ae1536423064083da48a524c92715baa
= <tyler.broyles@va.gov>
2023-09-14 11:35:34 -0400

    Updated reviews_spec.rb file to work with new factory changes.

--- spec/feature/non_comp/reviews_spec.rb
+++ spec/feature/non_comp/reviews_spec.rb
@@ -9,11 +9,16 @@ feature "NonComp Reviews Queue", :postgres do
   let(:veteran_a_on_hold) { create(:veteran, first_name: "Douglas", participant_id: "87474", ssn: "999393976") }
   let(:veteran_b_on_hold) { create(:veteran, first_name: "Gaius", participant_id: "601172", ssn: "191039395") }
   let(:veteran_c) { create(:veteran, first_name: "Ccc", participant_id: "1002345", ssn: "128455943") }
-  let(:hlr_a_on_hold) { create(:higher_level_review, veteran_file_number: veteran_a_on_hold.file_number) }
-  let(:hlr_b_on_hold) { create(:higher_level_review, veteran_file_number: veteran_b_on_hold.file_number) }
-  let(:hlr_a) { create(:higher_level_review, veteran_file_number: veteran_a.file_number) }
-  let(:hlr_b) { create(:higher_level_review, veteran_file_number: veteran_b.file_number) }
-  let(:hlr_c) { create(:higher_level_review, veteran_file_number: veteran_c.file_number) }
+  let(:claimant_type) { :veteran_claimant }
+  let(:hlr_a_on_hold) do
+    create(:higher_level_review, veteran_file_number: veteran_a_on_hold.file_number, claimant_type: claimant_type)
+  end
+  let(:hlr_b_on_hold) do
+    create(:higher_level_review, veteran_file_number: veteran_b_on_hold.file_number, claimant_type: claimant_type)
+  end
+  let(:hlr_a) { create(:higher_level_review, veteran_file_number: veteran_a.file_number, claimant_type: claimant_type) }
+  let(:hlr_b) { create(:higher_level_review, veteran_file_number: veteran_b.file_number, claimant_type: claimant_type) }
+  let(:hlr_c) { create(:higher_level_review, veteran_file_number: veteran_c.file_number, claimant_type: claimant_type) }
   let(:appeal) { create(:appeal, veteran: veteran_c) }
 
   let!(:request_issue_a) do
