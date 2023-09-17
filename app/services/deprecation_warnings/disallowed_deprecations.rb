96b54b626d9caab2c14d53ac58903d24c873346f
AnandEdara <131183324+AnandEdara@users.noreply.github.com>
2023-09-17 15:20:28 -0500

    Update disallowed_deprecations.rb

--- app/services/deprecation_warnings/disallowed_deprecations.rb
+++ app/services/deprecation_warnings/disallowed_deprecations.rb
@@ -11,9 +11,17 @@ module DisallowedDeprecations
     /Dangerous query method \(method whose arguments are used as raw SQL\) called with non\-attribute argument\(s\)/
   ].freeze
 
+   # Regular expressions for Rails 6.1 deprecation warnings that we have addressed in the codebase
+  RAILS_6_1_FIXED_DEPRECATION_WARNING_REGEXES = [
+    /update_attributes!*\(/,
+    /render file: should be given the absolute path to a file/,
+    /ActionView::Base instances should be constructed with a lookup context, assignments, and a controller./
+  ].freeze
+
   # Regular expressions for deprecation warnings that should raise an exception on detection
   DISALLOWED_DEPRECATION_WARNING_REGEXES = [
     *RAILS_5_2_FIXED_DEPRECATION_WARNING_REGEXES
+    *RAILS_6_1_FIXED_DEPRECATION_WARNING_REGEXES
   ].freeze
 
   # @param message [String] deprecation warning message to be checked against disallow list
