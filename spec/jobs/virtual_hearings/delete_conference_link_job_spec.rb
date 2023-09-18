747a40027e2a19ccf285281cdf09665af5fcedd8
Jonathan Cohen <cohen_jonathan@bah.com>
2023-09-18 15:25:16 -0400

    removed added code in test.rb, provided environment variables necessary, within the test.

--- spec/jobs/virtual_hearings/delete_conference_link_job_spec.rb
+++ spec/jobs/virtual_hearings/delete_conference_link_job_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-describe VirtualHearings::DeleteConferenceLinkJob do
+describe VirtualHearings::DeleteConferenceLinkJob, :all_dbs do
   include ActiveJob::TestHelper
 
   let!(:current_user) { create(:user, roles: ["System Admin"]) }
   let!(:judge) { Judge.new(create(:user)) }
 
+  before do
+    allow_any_instance_of(VirtualHearings::LinkService).to receive(:pin_key).and_return("mysecretkey")
+    allow_any_instance_of(VirtualHearings::LinkService).to receive(:host).and_return("example.va.gov")
+    allow_any_instance_of(VirtualHearings::LinkService).to receive(:path).and_return("/sample")
+  end
 
   describe ".perform" do
     context "When conference links in the DB are past the date of the date the job is run" do
