fb5bfa945a026796b782de17ccf6f270c3e84f27
AnandEdara <131183324+AnandEdara@users.noreply.github.com>
2023-09-21 04:00:47 -0500

    Update ssl_redirect_exclusion_policy.rb

--- app/policies/ssl_redirect_exclusion_policy.rb
+++ app/policies/ssl_redirect_exclusion_policy.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "set"
-
 class SslRedirectExclusionPolicy
   EXEMPT_PATH_PATTERNS = [
     %r{^/api/metadata$},
@@ -10,6 +8,7 @@ class SslRedirectExclusionPolicy
     %r{^/api/docs/v3/},
     %r{^/idt/api/v2/}
   ].freeze
+  
   def self.call(request)
     # Check if the request path matches any of the exempt patterns
     EXEMPT_PATH_PATTERNS.any? { |pattern| pattern.match?(request.path) }
