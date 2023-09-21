7efd8165e6f0258e3d746e094b0da644d2be10a6
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-09-21 11:32:45 -0400

    🚨 Lint: Remove trailing whitespace

--- app/policies/ssl_redirect_exclusion_policy.rb
+++ app/policies/ssl_redirect_exclusion_policy.rb
@@ -8,7 +8,7 @@ class SslRedirectExclusionPolicy
     %r{^/api/docs/v3/},
     %r{^/idt/api/v2/}
   ].freeze
-  
+
   def self.call(request)
     # Check if the request path matches any of the exempt patterns
     EXEMPT_PATH_PATTERNS.any? { |pattern| pattern.match?(request.path) }
