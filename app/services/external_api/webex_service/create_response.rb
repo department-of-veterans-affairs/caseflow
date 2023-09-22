c93bd8dc21b6394f2cc62776d77bc71b30b11e06
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-09-22 19:16:37 -0400

    Fix data

--- app/services/external_api/webex_service/create_response.rb
+++ app/services/external_api/webex_service/create_response.rb
@@ -2,16 +2,6 @@
 
 class ExternalApi::WebexService::CreateResponse < ExternalApi::WebexService::Response
   def data
-    begin
-      JSON.parse(resp.raw_body)
-    rescue JSON::ParserError => error
-      Rails.logger.error(error)
-      Raven.capture_exception(error)
-
-      ConferenceCreationError.new(
-        code: 500,
-        message: "Response received from Webex could not be parsed."
-      ).serialize_response
-    end
+    resp.raw_body
   end
 end
