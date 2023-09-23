8233e5d25bda5419191c8319f1e64df37d607709
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-09-22 20:58:42 -0400

    Remove IC

--- lib/fakes/webex_service.rb
+++ lib/fakes/webex_service.rb
@@ -1,19 +1,6 @@
 # frozen_string_literal: true
 
 class Fakes::WebexService
-  SAMPLE_CIPHER = "eyJwMnMiOiJvUlZHZENlck9OanYxWjhLeHRub3p4NklPUUNzTVdEdWFMakRXR09kLTh4Tk91OUVyWDQ1aUZ6TG5FY" \
-                  "nlJeTZTam40Vk1kVEZHU1pyaE5pRiIsInAyYyI6MzIzMzAsImF1ZCI6ImE0ZDg4NmIwLTk3OWYtNGUyYy1hOTU4LT" \
-                  "NlOGMxNDYwNWU1MSIsImlzcyI6IjU0OWRhYWNiLWJmZWUtNDkxOS1hNGU0LWMwOTQ0YTY0ZGUyNSIsImN0eSI6Ikp" \
-                  "XVCIsImVuYyI6IkEyNTZHQ00iLCJhbGciOiJQQkVTMi1IUzUxMitBMjU2S1cifQ.cm6FWc6Bl4vB_utAc_bswG82m" \
-                  "UXhxkITkI0tZDGzzh5TKdoWSS1Mjw.L8mGls6Kp3lsa8Wz.fLen-yV2sTpWlkLFCszQjcG5V9FhJVwoNB9Ky9BgCp" \
-                  "T46cFWUe-wmyn1yIZcGxFcDcwhhKwW3PVuQQ1xjl-z63o67esrvaWAjgglSioKiOFTJF1M94d4gVj2foSQtYKzR8S" \
-                  "nI6wW5X5KShcVjxjchT7xDNxnHtIZoG-Zda_aOOfz_WK18rhNcyvb-BY7cSwTMhbnuuXO0-cuJ7wNyDbvqEfWXALf" \
-                  "j87a2_WopcwK-x-8TQ20bzZKUrugt0FRj6VKxOCzxDhozmWDFMRu8Dpj2UrS7Fo-JQf_I1oN0O-Dwf5r8ItcNQEu5" \
-                  "X0tcRazhrHSNWfOL2DOaDyHawi4oxc7MqaNRxxyrpy2qYw06_TzBwRKlMFZ8fT7-GJbDlE3nqWlNw3mlRuvhu80CH" \
-                  "SO5RK5a1obU4sfLX0Fsxl-csC-1QjcHuKOSP_ozb6l7om-WeOdbSV99Fjy68egjH1NhMQIcVwpG0fy2j8r3sN4nz0" \
-                  "RSe3LXoK78JqRxk6XuaQCDkr6TmG5YjHQ2FFw1tP1ekHpNIL2oJNVAKKPgget7LRuSiM6jg.628e3hFPmZCoqXuyY" \
-                  "2OriQ"
-
   DEFAULT_MEETING_PROPERTIES = {
     hostUserId: Faker::Alphanumeric.alphanumeric(number: 79),
     hostDisplayName: "BVA Caseflow",
@@ -80,7 +67,6 @@ class Fakes::WebexService
   def initialize(**args)
     @status_code = args[:status_code]
     @error_message = args[:error_message] || "Error"
-    @instant_connect = args[:use_instant_connect]
   end
 
   def create_conference(virtual_hearing)
@@ -98,11 +84,7 @@ class Fakes::WebexService
       HTTPI::Response.new(
         200,
         {},
-        if @instant_connect
-          generate_instant_connect_conference(virtual_hearing)
-        else
-          generate_meetings_api_conference(virtual_hearing)
-        end
+        generate_meetings_api_conference(virtual_hearing)
       )
     )
   end
@@ -135,23 +117,6 @@ class Fakes::WebexService
         description: @error_message
       ],
       trackingId: "ROUTER_#{SecureRandom.uuid}"
-    }
-  end
-
-  def link_info(num_links = 1)
-    Array.new(num_links).map do
-      {
-        cipher: SAMPLE_CIPHER,
-        short: Faker::Alphanumeric.alphanumeric(number: 7, min_alpha: 3, min_numeric: 1)
-      }
-    end
-  end
-
-  def generate_instant_connect_conference(virtual_hearing)
-    {
-      host: link_info,
-      guest: link_info(args[:num_guests]),
-      baseUrl: "https://instant-usgov.webex.com/visit/"
     }
   end
 
