478b710340f5c3c9de556615c33211feb8d38f06
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-09-23 22:47:37 -0400

    Add mock link creation

--- app/models/hearings/webex_conference_link.rb
+++ app/models/hearings/webex_conference_link.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class WebexConferenceLink < ConferenceLink
+  def guest_pin
+    nil
+  end
+
+  def guest_link
+    guest_hearing_link
+  end
+
   private
 
   def generate_conference_information
-    fail NotImplementedError
+    # These links are just placeholders until the service class(es) is available.
+    update!(
+      host_link: "https://test.webex.com/meeting/#{Faker::Alphanumeric.alphanumeric(number: 32).downcase}",
+      guest_hearing_link: "https://test.webex.com/meeting/#{Faker::Alphanumeric.alphanumeric(number: 32).downcase}"
+    )
   end
 end
