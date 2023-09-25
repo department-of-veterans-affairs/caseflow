afe9ea932ee5158f6b5c9475b34fd110d0de03ea
nkirby-va <nicholas.kirby@va.gov>
2023-09-25 11:23:57 -0400

    add more tests for ep_codes

--- spec/sql/triggers/populate_end_product_sync_queue_spec.rb
+++ spec/sql/triggers/populate_end_product_sync_queue_spec.rb
@@ -34,37 +34,59 @@ describe "vbms_ext_claim trigger to populate end_product_sync_que table", :postg
         expect(PriorityEndProductSyncQueue.count).to eq 1
         expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq logged_epe2.id
       end
-    end
 
-    context "we do not log inserted (on creation) vbms_ext_claims" do
       let(:logged_epe3) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
-      let(:logged_ext_claim3) { create(:vbms_ext_claim, :rdc, :hlr, id: 300_000) }
+      let(:logged_ext_claim3) { create(:vbms_ext_claim, :canceled, :hlr, id: 300_000, ep_code: '930') }
 
-      it "that are rdc, have a \"03%\" EP_CODE,
+      it "that are cancelled, have a \"93%\" EP_CODE,
             with out sync status, not in pepsq table " do
         logged_epe3
         logged_ext_claim3
-        expect(PriorityEndProductSyncQueue.count).to eq 0
+        expect(PriorityEndProductSyncQueue.count).to eq 1
+        expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq logged_epe3.id
       end
 
       let(:logged_epe4) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
-      let(:logged_ext_claim4) { create(:vbms_ext_claim, :canceled, EP_CODE: "999", id: 300_000) }
+      let(:logged_ext_claim4) { create(:vbms_ext_claim, :cleared, id: 300_000, ep_code: '680') }
 
-      it "that are canceled, have a wrong EP_CODE,
-            with a nil sync status, not in pepsq table " do
+      it "that are cleared, have a \"68%\" EP_CODE,
+            with out sync status, not in pepsq table " do
         logged_epe4
         logged_ext_claim4
+        expect(PriorityEndProductSyncQueue.count).to eq 1
+        expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq logged_epe4.id
+      end
+    end
+
+    context "we do not log inserted (on creation) vbms_ext_claims" do
+      let(:logged_epe5) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
+      let(:logged_ext_claim5) { create(:vbms_ext_claim, :rdc, :hlr, id: 300_000) }
+
+      it "that are rdc, have a \"03%\" EP_CODE,
+            with out sync status, not in pepsq table " do
+        logged_epe5
+        logged_ext_claim5
         expect(PriorityEndProductSyncQueue.count).to eq 0
       end
 
-      let(:logged_epe5) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
-      let(:logged_ext_claim5) { create(:vbms_ext_claim, :canceled, :slc, id: 300_000) }
+      let(:logged_epe6) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
+      let(:logged_ext_claim6) { create(:vbms_ext_claim, :canceled, EP_CODE: "999", id: 300_000) }
+
+      it "that are canceled, have a wrong EP_CODE,
+            with a nil sync status, not in pepsq table " do
+        logged_epe6
+        logged_ext_claim6
+        expect(PriorityEndProductSyncQueue.count).to eq 0
+      end
+
+      let(:logged_epe7) { create(:end_product_establishment, synced_status: nil, reference_id: 300_000) }
+      let(:logged_ext_claim7) { create(:vbms_ext_claim, :canceled, :slc, id: 300_000) }
 
       it "that are canceled, have a wrong EP_CODE,
             with a nil sync status, already in the pepsq table " do
-        logged_epe5
-        PriorityEndProductSyncQueue.create(end_product_establishment_id: logged_epe5.id)
-        logged_ext_claim5
+        logged_epe7
+        PriorityEndProductSyncQueue.create(end_product_establishment_id: logged_epe7.id)
+        logged_ext_claim7
         expect(PriorityEndProductSyncQueue.count).to eq 1
       end
     end
@@ -86,30 +108,45 @@ describe "vbms_ext_claim trigger to populate end_product_sync_que table", :postg
     end
 
     context "we only log updated vbms_ext_claims" do
-      it "that are cleared, have a \"04%\" EP_CODE,
+      it "that are cleared, *have a \"04%\" EP_CODE,
             different sync status, and are not in pepsq table" do
         @logged_ext_claim.update(LEVEL_STATUS_CODE: "CLR")
         expect(PriorityEndProductSyncQueue.count).to eq 1
         expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq @logged_epe.id
       end
 
-      it "that are cancelled, have a \"03%\" EP_CODE,
+      it "that are cancelled, *have a \"03%\" EP_CODE,
             with out sync status, not in pepsq table " do
         @logged_epe.update(synced_status: nil)
         @logged_ext_claim.update(LEVEL_STATUS_CODE: "CAN", EP_CODE: "030")
         expect(PriorityEndProductSyncQueue.count).to eq 1
         expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq @logged_epe.id
       end
+
+      it "that are cleared, *have a \"93%\" EP_CODE,
+            different sync status, and are not in pepsq table" do
+        @logged_ext_claim.update(LEVEL_STATUS_CODE: "CLR", EP_CODE: "930")
+        expect(PriorityEndProductSyncQueue.count).to eq 1
+        expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq @logged_epe.id
+      end
+
+      it "that are cancelled, *have a \"68%\" EP_CODE,
+            with out sync status, not in pepsq table " do
+        @logged_epe.update(synced_status: nil)
+        @logged_ext_claim.update(LEVEL_STATUS_CODE: "CAN", EP_CODE: "680")
+        expect(PriorityEndProductSyncQueue.count).to eq 1
+        expect(PriorityEndProductSyncQueue.first.end_product_establishment_id).to eq @logged_epe.id
+      end
     end
 
     context "we do not log updated vbms_ext_claims" do
-      it "that are rdc, have a \"03%\" EP_CODE,
+      it "*that are rdc, have a \"03%\" EP_CODE,
             with out sync status, not in pepsq table " do
         @logged_ext_claim.update(LEVEL_STATUS_CODE: "RDC", EP_CODE: "030")
         expect(PriorityEndProductSyncQueue.count).to eq 0
       end
 
-      it "that are canceled, have a wrong EP_CODE,
+      it "that are canceled, *have a wrong EP_CODE,
             with a nil sync status, not in pepsq table " do
         @logged_epe.update(synced_status: nil)
         @logged_ext_claim.update(LEVEL_STATUS_CODE: "CAN", EP_CODE: "999")
@@ -117,7 +154,7 @@ describe "vbms_ext_claim trigger to populate end_product_sync_que table", :postg
       end
 
       it "that are canceled, have a wrong EP_CODE,
-            with a nil sync status, already in the pepsq table " do
+            with a nil sync status, *already in the pepsq table " do
         PriorityEndProductSyncQueue.create(end_product_establishment_id: @logged_epe.id)
         expect(PriorityEndProductSyncQueue.count).to eq 1
       end
