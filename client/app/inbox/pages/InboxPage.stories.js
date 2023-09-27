edb4a21a30c008aed293b1c1ec862a5cf8a99d03
Sean Craig <sean.craig2@va.gov>
2023-09-27 15:42:19 -0500

    Cleaning up the lint

--- client/app/inbox/pages/InboxPage.stories.js
+++ client/app/inbox/pages/InboxPage.stories.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { InboxMessagesPage } from './InboxPage';
 
+const legacyAppealLink = '<a href="/queue/appeals/541030342">Veteran ID 541032910</a>';
+
+const appealLink = '<a href="/queue/appeals/7f8db963-c147-4bf6-ad7f-dd2d175d537a">Veteran ID 541032909</a>';
+
+const errorText = '\nCaseflow is having trouble contacting the virtual hearing scheduler.\n';
+
+const supportLink = 'For help, submit a support ticket using <a href="https://yourit.va.gov/">YourIT</a>.\n';
+
 const messages = [
   {
     created_at: '2023-09-22T15:18:39.800-04:00',
@@ -9,8 +17,7 @@ const messages = [
     id: 2,
     message_type: null,
     read_at: null,
-    text:
-      '<a href="/queue/appeals/541030342">Veteran ID 541032910</a> - Hearing time not updated\nCaseflow is having trouble contacting the virtual hearing scheduler.\nFor help, submit a support ticket using <a href="https://yourit.va.gov/">YourIT</a>.\n',
+    text: `${legacyAppealLink} - Hearing time not updated. ${errorText} ${supportLink}`,
     updated_at: '2023-09-27T09:56:03.747-04:00',
     user_id: 125
   },
@@ -21,8 +28,7 @@ const messages = [
     id: 1,
     message_type: null,
     read_at: null,
-    text:
-      '<a href=\"/queue/appeals/7f8db963-c147-4bf6-ad7f-dd2d175d537a\">Veteran ID 541032909</a> - Virtual hearing not scheduled\nCaseflow is having trouble contacting the virtual hearing scheduler.\nFor help, submit a support ticket using <a href=\"https://yourit.va.gov/\">YourIT</a>.\n',
+    text: `${appealLink} - Virtual hearing not scheduled. ${errorText} ${supportLink}`,
     updated_at: '2023-09-27T09:58:44.807-04:00',
     user_id: 125
   }
