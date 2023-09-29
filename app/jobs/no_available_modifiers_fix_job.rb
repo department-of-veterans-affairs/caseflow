084e23ce938a7a7d952e811b010e7e357c3fb610
Griffin Dooley <dooleygriffin@gmail.com>
2023-09-29 15:58:33 -0400

    refactored job

---
+++ app/jobs/no_available_modifiers_fix_job.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class NoAvailableModifiersFixJob < CaseflowJob
+  ERROR_TEXT = "EndProductModifierFinder::NoAvailableModifiers"
+
+  attr_reader :stuck_job_report_service
+
+  def initialize
+    @stuck_job_report_service = StuckJobReportService.new
+    super
+  end
+
+  def perform
+    veterans_with_errors.each do |vet_fn|
+      active_count = current_active_eps_count(vet_fn) || 0
+      available_space = 10 - active_count
+      next if available_space <= 0
+
+      supp_claims = supp_claims_on_veteran(vet_fn)
+      break if supp_claims.empty?
+
+      process_supplemental_claims(supp_claims, available_space)
+    end
+  end
+
+  def process_supplemental_claims(supp_claims, available_space)
+    supp_claims.each do |sc|
+      break if available_space <= 0
+
+      ActiveRecord::Base.transaction do
+        DecisionReviewProcessJob.new.perform(sc)
+      rescue StandardError => error
+        log_error(error)
+        stuck_job_report_service.append_errors(sc.class.name, sc.id, error)
+      end
+    end
+  end
+
+  def end_products_on_veteran(file_number)
+    EndProductEstablishment.where(veteran_file_number: file_number)
+  end
+
+  def supp_claims_on_veteran(file_number)
+    SupplementalClaim.where("establishment_error ILIKE ?", "%#{ERROR_TEXT}%").where(veteran_file_number: file_number)
+  end
+
+  def current_active_eps_count(file_number)
+    EndProductEstablishment.where(veteran_file_number: file_number).pluck(:synced_status).tally["PEND"]
+  end
+
+  def veterans_with_errors
+    SupplementalClaim.where("establishment_error ILIKE ?",
+                            "%#{ERROR_TEXT}%").pluck(:veteran_file_number).uniq
+  end
+end
---
+++ spec/jobs/no_available_modifiers_fix_job_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+describe NoAvailableModifiersFixJob, :postres do
+  let(:error_text) { "EndProductModifierFinder::NoAvailableModifiers" }
+  let(:file_number) { "123454321" }
+
+  let!(:vet) do
+    create(
+      :veteran,
+      file_number: file_number
+    )
+  end
+
+  let!(:supplemental_claim_with_error) do
+    create(
+      :supplemental_claim,
+      veteran_file_number: file_number,
+      establishment_error: error_text
+    )
+  end
+
+  let!(:epe) do
+    create(
+      :end_product_establishment,
+      veteran_file_number: file_number,
+      source_type: "SupplementalClaim",
+      source_id: supplemental_claim_with_error.id,
+      modifier: nil
+    )
+  end
+
+  before do
+    create_list(:end_product_establishment, 5, veteran_file_number: file_number, modifier: nil,
+                                               source_type: "SupplementalClaim")
+    create_list(:end_product_establishment, 2, veteran_file_number: file_number, modifier: "030",
+                                               source_type: "HigherLevelReview")
+  end
+
+  subject { described_class.new }
+
+  context "when there are fewer than 10 active end products" do
+    describe "when there are 0 active end products" do
+      it "runs decision_review_process_job on up to 10 Supplemental Claims" do
+        subject.perform
+      end
+    end
+
+    describe "when there are 0 active end products" do
+      it "runs decision_review_process_job on up to 5 Supplemental Claims" do
+        subject.perform
+      end
+    end
+  end
+end
