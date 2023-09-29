d989c8a0cf6616ca7877ba1579a1cb35e3a4612f
AdamShawBAH <adam.shaw@va.gov>
2023-09-29 10:53:24 -0400

    unknown user fix job

---
+++ app/jobs/unknown_user_fix_job.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class UnknownUserFixJob < CaseflowJob
+  ERROR_TEXT = "UnknownUser"
+
+  def perform(date = "2023-08-07")
+    date = date.to_s
+    pattern = /^\d{4}-\d{2}-\d{2}$/
+    if !date.match?(pattern)
+      fail ArgumentError, "Incorrect date format, use 'YYYY-mm-dd'"
+    end
+
+    begin
+      parsed_date = Time.zone.parse(date)
+    rescue ArgumentError => error
+      Rails.logger.error("Error: #{error.message}")
+      raise error
+    end
+    return if rius_with_errors.blank?
+
+    stuck_job_report_service = StuckJobReportService.new
+    stuck_job_report_service.append_record_count(rius_with_errors.count, ERROR_TEXT)
+    rius_with_errors.each do |single_riu|
+      next if single_riu.created_at > parsed_date
+
+      stuck_job_report_service.append_single_record(single_riu.class.name, single_riu.id)
+
+      resolve_error_on_records(single_riu)
+    end
+    stuck_job_report_service.append_record_count(rius_with_errors.count, ERROR_TEXT)
+  end
+
+  # :reek:FeatureEnvy
+  def resolve_error_on_records(object_type)
+    ActiveRecord::Base.transaction do
+      object_type.clear_error!
+    rescue StandardError => error
+      log_error(error)
+      STUCK_JOB_REPORT_SERVICE.append_errors(object_type.class.name, object_type.id, error)
+    end
+  end
+
+  def rius_with_errors
+    RequestIssuesUpdate.where("error ILIKE ?", "%#{ERROR_TEXT}%")
+  end
+end
