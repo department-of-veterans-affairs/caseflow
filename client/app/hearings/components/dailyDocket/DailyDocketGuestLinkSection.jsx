3b93f416bf0317a83476b190cb4f041745877827
breedbah <brandon.reed3@va.gov>
2023-10-02 15:40:01 -0400

    APPEALS-25116 linting fixes DAilyDocketGuestLinkSection

--- client/app/hearings/components/dailyDocket/DailyDocketGuestLinkSection.jsx
+++ client/app/hearings/components/dailyDocket/DailyDocketGuestLinkSection.jsx
@@ -1,40 +1,40 @@
 /* eslint-disable id-length */
-import React from "react";
-import PropTypes from "prop-types";
-import CopyTextButton from "../../../components/CopyTextButton";
-import { GUEST_LINK_LABELS } from "../../constants";
+import React from 'react';
+import PropTypes from 'prop-types';
+import CopyTextButton from '../../../components/CopyTextButton';
+import { GUEST_LINK_LABELS } from '../../constants';
 
 export const DailyDocketGuestLinkSection = ({ linkInfo }) => {
   const containerStyle = {
-    marginLeft: "-40px",
-    marginRight: "-40px",
+    marginLeft: '-40px',
+    marginRight: '-40px',
   };
 
   const roomInfoStyle = (index) => ({
-    backgroundColor: index === 0 ? "#f1f1f1" : "white",
-    justifyContent: "space-between",
-    display: "flex",
-    width: "100%",
-    height: "50px",
+    backgroundColor: index === 0 ? '#f1f1f1" : "white',
+    justifyContent: 'space-between',
+    display: 'flex',
+    width: '100%',
+    height: '50px',
   });
 
   // Props needed for the copy text button component
   const CopyTextButtonProps = {
     text: GUEST_LINK_LABELS.COPY_GUEST_LINK,
     label: GUEST_LINK_LABELS.COPY_GUEST_LINK,
-    textToCopy: "",
+    textToCopy: '',
   };
 
   // Takes pin from guestLink
   // const usePinFromLink = () => guestLink?.match(/pin=\d+/)[0]?.split('=')[1];
   // Takes alias from guestLink
   const useAliasFromLink = (link) => {
-    if (link.type === "PexipConferenceLink") {
+    if (link.type === 'PexipConferenceLink') {
       return (
-        link.alias || link.guestLink?.match(/pin=\d+/)[0]?.split("=")[1] || null
+        link.alias || link.guestLink?.match(/pin=\d+/)[0]?.split('=')[1] || null
       );
-    } else if (link.type === "WebexConferenceLink") {
-      const newLink = "instant-usgov";
+    } else if (link.type === 'WebexConferenceLink') {
+      const newLink = 'instant-usgov';
       return link.alias || newLink || null;
     }
 
@@ -42,12 +42,12 @@ export const DailyDocketGuestLinkSection = ({ linkInfo }) => {
   };
 
   const extractPin = (link) => {
-    if (link.type === "PexipConferenceLink") {
-      return link.guestPin + "#" || link.guestLink?.match(/pin=(\d+)/)?.[1];
-    } else if (link.type === "WebexConferenceLink") {
+    if (link.type === 'PexipConferenceLink') {
+      return link.guestPin +'"#' || link.guestLink?.match(/pin=(\d+)/)?.[1];
+    } else if (link.type === 'WebexConferenceLink') {
       const pinRegex = /(\d{5})$/;
       const match = link.guestLink.match(pinRegex);
-      return match ? match[1] + "#" : "N/A";
+      return match ? match[1] + '#' : 'N/A';
     }
 
     return null;
@@ -75,44 +75,44 @@ export const DailyDocketGuestLinkSection = ({ linkInfo }) => {
             <div key={index} style={roomInfoStyle(index)}>
               <h3
                 style={{
-                  width: "350px",
-                  display: "flex",
-                  marginBottom: "0px",
-                  alignItems: "center",
-                  marginLeft: "10px",
+                  width: '350px',
+                  display: 'flex',
+                  marginBottom: '0px',
+                  alignItems: 'center',
+                  marginLeft: '10px',
                 }}
               >
-                {type === "PexipConferenceLink"
+                {type === 'PexipConferenceLink'
                   ? GUEST_LINK_LABELS.PEXIP_GUEST_LINK_SECTION_LABEL
                   : GUEST_LINK_LABELS.WEBEX_GUEST_LINK_SECTION_LABEL}
               </h3>
 
               <h3
                 style={{
-                  display: "flex",
-                  marginBottom: "0px",
-                  alignItems: "center",
-                  width: "400px",
+                  display: 'flex',
+                  marginBottom: '0px',
+                  alignItems: 'center',
+                  width: '400px',
                 }}
               >
-                {GUEST_LINK_LABELS.GUEST_CONFERENCE_ROOM + ":"}
-                <span style={{ fontWeight: "normal" }}>{alias || "N/A"}</span>
+                {GUEST_LINK_LABELS.GUEST_CONFERENCE_ROOM + ':'}
+                <span style={{ fontWeight: 'normal' }}>{alias || 'N/A'}</span>
               </h3>
               <h3
                 style={{
-                  width: "max-content",
-                  display: "flex",
-                  alignItems: "center",
-                  marginBottom: "0px",
+                  width: 'max-content',
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: '0px',
                 }}
               >
-                {GUEST_LINK_LABELS.GUEST_PIN + ":"}
+                {GUEST_LINK_LABELS.GUEST_PIN + ':'}
                 {linkGuestPin ? (
                   <span
                     style={{
-                      fontWeight: "normal",
-                      paddingRight: "10px",
-                      display: "flex",
+                      fontWeight: 'normal',
+                      paddingRight: '10px',
+                      display: 'flex',
                     }}
                   >
                     {linkGuestPin}
@@ -120,9 +120,9 @@ export const DailyDocketGuestLinkSection = ({ linkInfo }) => {
                 ) : (
                   <span
                     style={{
-                      fontWeight: "normal",
-                      paddingRight: "60px",
-                      display: "flex",
+                      fontWeight: 'normal',
+                      paddingRight: '60px',
+                      display: 'flex',
                     }}
                   >
                     N/A
@@ -132,10 +132,10 @@ export const DailyDocketGuestLinkSection = ({ linkInfo }) => {
               <h3
                 style={{
 
-                  display: "flex",
-                  alignItems: "center",
-                  marginBottom: "0px",
-                  marginRight: "10px",
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: '0px',
+                  marginRight: '10px',
                 }}
               >
                 <CopyTextButton {...CopyTextButtonProps} />
