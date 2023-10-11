e07f48c2cfd3b6f331be3f084b1e4df7ee1adc40
Alexander Smith <alex.smith.99@thunderyard.com>
2023-10-11 09:03:06 -0600

    Fix Ruby and JS Lint Issues

--- app/controllers/reader/document_content_searches_controller.rb
+++ app/controllers/reader/document_content_searches_controller.rb
@@ -7,16 +7,16 @@ class Reader::DocumentContentSearchesController < Reader::ApplicationController
         {
           "45256": [
             "She sells sea shells",
-            "by the sea shore",
+            "by the sea shore"
           ]
         },
         {
           "68629": [
             "Nobody could catch cold by the sea",
             "nobody wanted appetite by the sea",
-            "Sea air was healing, softening, relaxing",
+            "Sea air was healing, softening, relaxing"
           ]
-        },
+        }
       ]
     }
   end
--- client/app/components/FetchSearchBar.jsx
+++ client/app/components/FetchSearchBar.jsx
@@ -1,29 +1,28 @@
-import React from "react";
-import { useState } from "react";
-import ApiUtil from "../util/ApiUtil";
+import { useState } from React;
+import ApiUtil from '../util/ApiUtil';
 
 const FetchSearchBar = () => {
-
   const [searchText, setSearchText] = useState('');
   const handleSearchTextChange = (event) => {
     setSearchText(event.target.value);
-  }
+  };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    ApiUtil.get("/reader/appeal/:appeal_id/document_content_searches?search_term=" + searchText).then((response) => console.log(response));
-  }
+  const handleClick = (event) => {
+    event.preventDefault();
+    ApiUtil.get(`/reader/appeal/:appeal_id/document_content_searches?search_term=${searchText}`)
+      .then((response) => console.log(response));
+  };
 
   return (
-    <div style={{width:'100%'}}>
-      <p style={{textAlign:'center'}}>Search document contents</p>
+    <div style={{ width: '100%' }}>
+      <p style={{ textAlign: 'center' }}>Search document contents</p>
       <span style={{
-        width:'100%',
-        display:'flex',
-        justifyContent:'flex-end'
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'flex-end'
       }}>
-      <input value={searchText} onChange={handleSearchTextChange}/>
-      <button className="cf-submit usa-button" onClick={handleClick}>Search</button>
+        <input value={searchText} onChange={handleSearchTextChange} />
+        <button className='cf-submit usa-button' onClick={handleClick}>Search</button>
       </span>
     </div>
   );
