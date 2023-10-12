c23d296b67e4e03ab50b1af7b7f73a4ccfcb56fc
nkirby-va <nicholas.kirby@va.gov>
2023-10-12 10:27:28 -0400

    add beginning of data object construction with old serializer

---
+++ app/models/serializers/v3/decision_issue_serializer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class V3::DecisionIssueSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :caseflow_decision_date, :created_at, :decision_text, :deleted_at,
+             :description, :diagnostic_code, :disposition, :end_product_last_action_date,
+             :percent_number, :rating_issue_reference_id, :rating_profile_date,
+             :rating_promulgation_date, :subject_text, :updated_at
+end
---
+++ app/models/serializers/v3/request_issue_serializer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# use via V3::RequestIssueSerializer.new(<request issue obj>, include: [:decision_issues]).serializable_hash.to_json
+class V3::RequestIssueSerializer
+  include FastJsonapi::ObjectSerializer
+
+  has_many :decision_issues, serializer: V3::DecisionIssueSerializer
+
+  attributes :benefit_type, :closed_status, :contention_reference_id, :contested_decision_issue_id,
+             :contested_issue_description, :contested_rating_decision_reference_id,
+             :contested_rating_issue_diagnostic_code, :contested_rating_issue_profile_date,
+             :contested_rating_issue_reference_id, :corrected_by_request_issue_id,
+             :correction_type, :created_at, :decision_date, :decision_review_id,
+             :decision_review_type, :edited_description, :end_product_establishment_id,
+             :ineligible_due_to_id, :ineligible_reason, :is_unidentified,
+             :nonrating_issue_category, :nonrating_issue_description,
+             :notes, :ramp_claim_id, :split_issue_status, :unidentified_issue_text,
+             :untimely_exemption, :untimely_exemption_notes, :updated_at, :vacols_id,
+             :vacols_sequence_id, :verified_unidentified_issue, :veteran_participant_id
+
+end
