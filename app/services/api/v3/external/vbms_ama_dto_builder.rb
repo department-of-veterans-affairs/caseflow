f8b39bca478d426a548bf990a331e90d8ca4aaac
nkirby-va <nicholas.kirby@va.gov>
2023-10-13 14:17:08 -0400

    fix vbms_ama_dto_builder

--- app/services/api/v3/external/vbms_ama_dto_builder.rb
+++ app/services/api/v3/external/vbms_ama_dto_builder.rb
@@ -3,28 +3,21 @@
 # use for option 2 w/o jsonapi or fast_json serializer
 # RequestIssue.for_vbms.includes(:decision_issues_for_vbms).where(veteran_participant_id: "574727696").as_json(root: true, include: :decision_issues_for_vbms)
 class Api::V3::External::VbmsAmaDtoBuilder
-  '''def initialize(veteran_participant_id)
+  attr_reader :json_response
+
+  def initialize(veteran_participant_id)
     veteran_participant_id = veteran_participant_id.to_s
-    {
+    @json_response = {
       "veteran_participant_id": veteran_participant_id,
       "legacy_appeals_present": false,
       "data": build_request_issue_decision_issue_json(veteran_participant_id)
-    }
+    }.to_json
   end
 
   private
 
   def build_request_issue_decision_issue_json(veteran_participant_id)
-    request_issue_decision_issue_hash_array =
-      RequestIssue.includes(:decision_issues)
-        .where(veteran_participant_id: veteran_participant_id)
-        .as_json(root: true, include: :decision_issues)
-
-    format_request_issue_decision_issue_hash(request_issue_decision_issue_hash_array)
+    RequestIssue.for_vbms.includes(:decision_issues_for_vbms).where(veteran_participant_id: veteran_participant_id).page(1).as_json(root: true, include: :decision_issues_for_vbms).to_json
   end
-
-  def format_request_issue_decision_issue_hash(request_issue_decision_issue_hash_array)
-
-  end'''
 
 end
