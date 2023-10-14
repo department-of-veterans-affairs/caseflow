87fbc257a993d1243a2a6dff80febb98c09b978f
raymond-hughes <raymond.hughes2@va.gov>
2023-10-14 19:24:14 -0400

    Adding comments for context

--- app/jobs/email_status_update_job.rb
+++ app/jobs/email_status_update_job.rb
@@ -8,6 +8,8 @@ class EmailStatusUpdateJob < CaseflowJob
 
     redis = Redis.new(url: Rails.application.secrets.redis_url_cache)
 
+    # prefer scan so we only load a single record into memory,
+    # dumping the whole list could cause performance issues when job runs
     redis.scan_each(match: "email_update:*") do |key, val|
       rows_updated = Notification.where(
         email_notification_external_id: key[/\d+/]
