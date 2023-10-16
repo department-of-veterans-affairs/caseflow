e0963ef5142b0a3e88199f8452fbe3f2eb1968e6
= <tyler.broyles@va.gov>
2023-10-16 16:55:09 -0400

    Fixed the users slice to work with all of the possible return values and included the additional get params that were used in some of the users queries.

--- client/app/nonComp/actions/usersSlice.js
+++ client/app/nonComp/actions/usersSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import ApiUtil from '../../util/ApiUtil';
+import { find, get, has } from 'lodash';
 
 // Define the initial state
 const initialState = {
@@ -9,19 +10,30 @@ const initialState = {
 };
 
 // TODO: I need a better name for this. Maybe the whole slice too
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async ({ queryType, queryParam }, thunkApi) => {
+// TODO: Also need some sort of comment to help display all the possible query values and the call structure
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async ({ queryType, queryParams }, thunkApi) => {
   let usersEndpoint = '/users';
+  const { query, judgeID, excludeOrg } = queryParams;
 
   if (queryType === 'organization') {
-    usersEndpoint = `${usersEndpoint}?organization=${queryParam}`;
-  } else if (queryType === 'cssID') {
+    usersEndpoint = `${usersEndpoint}?organization=${query}`;
+  } else if (queryType === 'css_id') {
     // This searches by user name and by css id but the query param is always called css_id
-    usersEndpoint = `${usersEndpoint}?css_id=${queryParam}`;
+    usersEndpoint = `${usersEndpoint}?css_id=${query}`;
   } else if (queryType === 'role') {
-    usersEndpoint = `${usersEndpoint}?role=${queryParam}`;
+    usersEndpoint = `${usersEndpoint}?role=${query}`;
   } else {
     // TODO: The meta tags don't work with our version of rtk
     return thunkApi.rejectWithValue('Invalid query type', { analytics: true });
+  }
+
+  // Optional get parameters that some of the controller actions use
+  if (judgeID) {
+    usersEndpoint = `${usersEndpoint}&judge_id=${judgeID}`;
+  }
+
+  if (excludeOrg) {
+    usersEndpoint = `${usersEndpoint}&exclude_org=${excludeOrg}`;
   }
 
   try {
@@ -29,10 +41,17 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async ({ queryTyp
 
     // Use the first key since the returning body can be a variety of different keys e.g. users, judges, attorneys
     const usersKey = Object.keys(response.body)[0];
-    const preparedUsers = response.body[usersKey].map(({ attributes, ...rest }) => ({ ...attributes, ...rest }));
+
+    // Sometimes the key is .data and sometimes it's the toplevel object so try .data first and then the toplevel
+    const possibleKeys = [`${usersKey}.data`, `${usersKey}`];
+    const foundKeyPath = find(possibleKeys, (keyPath) => has(response.body, keyPath));
+    const result = foundKeyPath ? get(response.body, foundKeyPath) : null;
+
+    // Some of the data contains .attributes and some doesn't so expand all the attributes out to the top level objects
+    const preparedData = result.map(({ attributes, ...rest }) => ({ ...attributes, ...rest }));
     const meta = { analytics: true };
 
-    return { data: preparedUsers, meta };
+    return { data: preparedData, meta };
 
   } catch (error) {
     console.error(error);
