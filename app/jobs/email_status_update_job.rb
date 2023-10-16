d1d408fe32bee685ccf264fbae9f622a6ec55a99
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-10-16 09:03:45 -0400

    Update regex

--- app/jobs/email_status_update_job.rb
+++ app/jobs/email_status_update_job.rb
@@ -3,6 +3,8 @@
 class EmailStatusUpdateJob < CaseflowJob
   queue_with_priority :high_priority
 
+  UUID_REGEX = /[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/
+
   def perform
     RequestStore[:current_user] = User.system_user
 
@@ -12,10 +14,10 @@ class EmailStatusUpdateJob < CaseflowJob
     # dumping the whole list could cause performance issues when job runs
     redis.scan_each(match: "email_update:*") do |key, val|
       rows_updated = Notification.where(
-        email_notification_external_id: key[/\d+/]
+        email_notification_external_id: key[UUID_REGEX]
       ).update_all(email_notification_status: val)
 
-      log_error(val) if rows_updated.zero?
+      fail StandardError if rows_updated.zero?
 
       # cleanup keys
       redis.del key
