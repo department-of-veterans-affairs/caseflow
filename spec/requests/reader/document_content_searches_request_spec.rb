6d88c962c99fa730d8024652c014cdd681b90b03
HunJerBAH <Jeremy.Hunton@va.gov>
2023-10-16 14:09:37 -0400

    Created cache test for document content searches

--- spec/requests/reader/document_content_searches_request_spec.rb
+++ spec/requests/reader/document_content_searches_request_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe "Reader::DocumentContentSearchesController", type: :request do
 
   context "with a valid appeal" do
     context "when an appeal has documents" do
-      let(:document1) { create(:document) }
-      let(:document2) { create(:document) }
+      let(:document1) { create(:document, series_id: "012345") }
+      let(:document2) { create(:document, series_id: "678910") }
       let(:appeal) { create(:appeal, documents: [document1, document2]) }
 
       context "when matching text exists" do
+        let(:document_contents) { "foo bar baz" }
         before do
-          expect(ClaimEvidenceService).to receive(:get_ocr_document).twice.and_return("foo bar baz")
+          expect(ClaimEvidenceService).to receive(:get_ocr_document).twice.and_return(document_contents)
         end
 
         it "successfully finds matching documents" do
@@ -22,6 +23,17 @@ RSpec.describe "Reader::DocumentContentSearchesController", type: :request do
           expect(response).to have_http_status(:success)
           expect(response.body).to include(document1.vbms_document_id)
           expect(response.body).to include(document2.vbms_document_id)
+        end
+
+        it "caches document contents from the claim evidence API" do
+          get "/reader/appeal/#{appeal.uuid}/document_content_searches?search_term=foo"
+          # check each doc has doc contents contained within the cache
+          cache_key_1 = "claim_evidence_document_content_#{document1.series_id}"
+          cache_key_2 = "claim_evidence_document_content_#{document2.series_id}"
+          expect(Rails.cache.exist?(cache_key_1))
+          expect(Rails.cache.fetch(cache_key_1)).to eq(document_contents)
+          expect(Rails.cache.exist?(cache_key_2))
+          expect(Rails.cache.fetch(cache_key_2)).to eq(document_contents)
         end
       end
 
