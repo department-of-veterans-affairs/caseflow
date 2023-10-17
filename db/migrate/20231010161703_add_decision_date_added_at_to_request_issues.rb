d770c1ddee9c159a058d4ef032e81a00600ea56c
Sean Craig <sean.craig2@va.gov>
2023-10-17 16:07:23 -0500

    removed version number

--- db/migrate/20231010161703_add_decision_date_added_at_to_request_issues.rb
+++ db/migrate/20231010161703_add_decision_date_added_at_to_request_issues.rb
@@ -1,4 +1,4 @@
-class AddDecisionDateAddedAtToRequestIssues < Caseflow::Migration[5.2]
+class AddDecisionDateAddedAtToRequestIssues < Caseflow::Migration
   def change
     add_column :request_issues, :decision_date_added_at, :datetime, comment: "Denotes when a decision date was added"
   end
