aa32c2433ddde668209cd7d42078e131b338c7c6
nkirby-va <nicholas.kirby@va.gov>
2023-10-18 00:29:12 -0400

    update dto builder namespace in controller

--- app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
+++ app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
@@ -5,6 +5,6 @@ class Api::V3::VbmsIntake::Ama::VeteransController < Api::V3::BaseController
   def show
     @veteran = Veteran.find(params[:participant_id]).id
     @page = ActiveRecord::Base.sanitize_sql(params[:page].to_i)
-    render json: Api::V3::VbmsIntake::VbmsAmaDtoBuilder.new(@veteran, @page).json_response if @veteran
+    render json: Api::V3::VbmsIntake::Ama::VbmsAmaDtoBuilder.new(@veteran, @page).json_response if @veteran
   end
 end
