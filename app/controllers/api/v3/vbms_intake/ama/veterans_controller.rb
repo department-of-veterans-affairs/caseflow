0a676c616e7839a9e76701473a57b361dcc5ca59
nkirby-va <nicholas.kirby@va.gov>
2023-10-18 01:18:41 -0400

    default to page(1) and add check

--- app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
+++ app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
@@ -4,7 +4,8 @@
 class Api::V3::VbmsIntake::Ama::VeteransController < Api::V3::BaseController
   def show
     @veteran = Veteran.find_by(participant_id: params[:participant_id]).participant_id
-    @page = ActiveRecord::Base.sanitize_sql(params[:page].to_i)
+    @page = ActiveRecord::Base.sanitize_sql(params[:page].to_i) if params[:page]
+    @page ||= 1 # default to page(1) since page(0) == page(1) via kaminari; purpose to avoid confusion
     render json: Api::V3::VbmsIntake::Ama::VbmsAmaDtoBuilder.new(@veteran, @page).json_response if @veteran
   end
 end
