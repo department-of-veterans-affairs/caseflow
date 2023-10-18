d7c54b22c8ed2501efa9566f3604706927ba7fb3
nkirby-va <nicholas.kirby@va.gov>
2023-10-18 01:30:54 -0400

    add ternary to disallow page(0)

--- app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
+++ app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
@@ -5,7 +5,8 @@ class Api::V3::VbmsIntake::Ama::VeteransController < Api::V3::BaseController
   def show
     @veteran = Veteran.find_by(participant_id: params[:participant_id]).participant_id
     @page = ActiveRecord::Base.sanitize_sql(params[:page].to_i) if params[:page]
-    @page ||= 1 # default to page(1) since page(0) == page(1) via kaminari; purpose to avoid confusion
+    # Disallow page(0) since page(0) == page(1) in kaminari. This is to avoid confusion.
+    (@page == 0) ? @page = 1 : @page ||= 1
     render json: Api::V3::VbmsIntake::Ama::VbmsAmaDtoBuilder.new(@veteran, @page).json_response if @veteran
   end
 end
