2c05654f10c5ab74580367c1cec375e177950f2a
raymond-hughes <raymond.hughes2@va.gov>
2023-10-18 16:57:53 -0400

    Adding a notification factory so we get an update

--- spec/jobs/process_notification_status_updates_job_spec.rb
+++ spec/jobs/process_notification_status_updates_job_spec.rb
@@ -2,21 +2,33 @@
 
 describe ProcessNotificationStatusUpdatesJob, :postgres do
   context ".perform" do
-    subject { described_class.perform_now }
+    before do
+      Seeds::NotificationEvents.new.seed!
+    end
+
+    let(:appeal) { create(:appeal, veteran_file_number: "500000102", receipt_date: 6.months.ago.to_date.mdY) }
+    let!(:notification) do
+      create(:notification, appeals_id: appeal.uuid,
+                            appeals_type: "Appeal",
+                            event_date: 6.days.ago,
+                            event_type: "Quarterly Notification",
+                            notification_type: "Email")
+    end
 
     it "processes notifications from redis cache" do
-      # should we mock this?
       redis = Redis.new(url: Rails.application.secrets.redis_url_cache)
 
-      # add notification statuses to redis
-      5.times { |i| redis.set("email_update_test:1234#{i}:test_status", 0) }
+      # clean-up any keys left behind
+      redis.scan_each(match: "*_update:*") { |key| redis.del(key) }
 
-      expect(redis.keys.grep(/email_update_test:/).count).to eq(5)
+      redis.set("email_update:#{notification.id}:test_status", 0)
+
+      expect(redis.keys.grep(/email_update:/).count).to eq(1)
 
       # does this run?
       described_class.perform_now
 
-      expect(redis.keys.grep(/email_update_test:/).count).to eq(0)
+      expect(redis.keys.grep(/email_update:/).count).to eq(0)
     end
   end
 end
