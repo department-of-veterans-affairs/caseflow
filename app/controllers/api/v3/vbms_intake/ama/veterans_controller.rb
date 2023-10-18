d0ac7b63c413ffce7da3230489040e049151b092
nkirby-va <nicholas.kirby@va.gov>
2023-10-18 00:53:34 -0400

    update veteran to participant_id

--- app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
+++ app/controllers/api/v3/vbms_intake/ama/veterans_controller.rb
@@ -3,7 +3,7 @@
 # :reek:InstanceVariableAssumption
 class Api::V3::VbmsIntake::Ama::VeteransController < Api::V3::BaseController
   def show
-    @veteran = Veteran.find(params[:participant_id]).id
+    @veteran = Veteran.find_by(participant_id: params[:participant_id]).participant_id
     @page = ActiveRecord::Base.sanitize_sql(params[:page].to_i)
     render json: Api::V3::VbmsIntake::Ama::VbmsAmaDtoBuilder.new(@veteran, @page).json_response if @veteran
   end
