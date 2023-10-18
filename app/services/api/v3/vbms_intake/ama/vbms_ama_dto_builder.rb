7aad9fa20e3d819511f2ffdc15ae511299c02bd0
nkirby-va <nicholas.kirby@va.gov>
2023-10-18 00:23:35 -0400

    switch to jsonapi-serializer method

--- app/services/api/v3/vbms_intake/ama/vbms_ama_dto_builder.rb
+++ app/services/api/v3/vbms_intake/ama/vbms_ama_dto_builder.rb
@@ -1,34 +1,36 @@
 # frozen_string_literal: true
 
+#:reek:TooManyInstanceVariables
 class Api::V3::VbmsIntake::Ama::VbmsAmaDtoBuilder
   attr_reader :json_response
 
   # TODO: add method for legacy
   def initialize(veteran_participant_id, page)
-    veteran_participant_id = veteran_participant_id.to_s
-    request_issue_count = total_request_issue_size(veteran_participant_id)
-    offset = RequestIssue.default_per_page
-    @json_response = {
-      "page": page,
-      "offset": offset,
-      "total_request_issues": request_issue_count,
-      "total_nubmer_of_pages": (request_issue_count / offset.to_f).ceil,
-      "veteran_participant_id": veteran_participant_id,
-      "legacy_appeals_present": false,
-      "data": build_request_issue_decision_issue_json(veteran_participant_id, page)
-    }.to_json
+    @page = page
+    @veteran_participant_id = veteran_participant_id.to_s
+    @request_issues = serialized_request_issues(veteran_participant_id)
+    @request_issue_size = @request_issues.size
+    @offset = RequestIssue.default_per_page
+    @json_response = build_json_response
   end
 
   private
 
-  def total_request_issue_size(veteran_participant_id)
-    RequestIssue.where(veteran_participant_id: veteran_participant_id).size.to_i
+  def serialized_request_issues(veteran_participant_id)
+    Api::V3::VbmsIntake::Ama::RequestIssueSerializer.new(
+      RequestIssue.includes(:decision_issues).where(veteran_participant_id: veteran_participant_id)
+    ).serializable_hash[:data]
   end
 
-  def build_request_issue_decision_issue_json(veteran_participant_id, page)
-    RequestIssue.for_vbms.includes(:decision_issues_for_vbms)
-      .where(veteran_participant_id: veteran_participant_id)
-      .page(page).as_json(root: true, include: :decision_issues_for_vbms).to_json
+  def build_json_response
+    {
+      "page": @page,
+      "total_nubmer_of_pages": (@request_issue_size / @offset.to_f).ceil,
+      "total_request_issues_for_vet": @request_issue_size,
+      "max_request_issues_per_page": @offset,
+      "veteran_participant_id": @veteran_participant_id,
+      "legacy_appeals_present": false,
+      "request_issues": @request_issues.to_json
+    }.to_json
   end
-
 end
