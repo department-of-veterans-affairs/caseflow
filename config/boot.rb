37f6990e757da2f9048e09bc70ba04c2deada84e
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-10-18 16:24:02 -0400

    Revert "Jcroteau/appeals 26093 update to rails 5 2 z (#19715)"
    This reverts commit 89ef7231e540ac49866065b3e40c50f7dd205398.

--- config/boot.rb
+++ config/boot.rb
@@ -2,4 +2,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 ENV["NLS_LANG"] = "AMERICAN_AMERICA.UTF8"
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require 'bootsnap/setup'
