68830b17a386bb61cb4cf9fc918ce895be5fbbc1
raymond-hughes <raymond.hughes2@va.gov>
2023-10-18 16:35:21 -0400

    Updating test

--- spec/jobs/process_notification_status_updates_job_spec.rb
+++ spec/jobs/process_notification_status_updates_job_spec.rb
@@ -9,12 +9,14 @@ describe ProcessNotificationStatusUpdatesJob, :postgres do
       redis = Redis.new(url: Rails.application.secrets.redis_url_cache)
 
       # add notification statuses to redis
-      5.times { |i| redis.set("email_update:1234#{i}:test_status", 0) }
-      expect(redis.keys(match: "email_update:").count).to eq(5)
+      5.times { |i| redis.set("email_update_test:1234#{i}:test_status", 0) }
 
+      expect(redis.keys.grep(/email_update_test:/).count).to eq(5)
+
+      # does this run?
       described_class.perform_now
 
-      expect(redis.keys(match: "email_update:").count).to eq(0)
+      expect(redis.keys.grep(/email_update_test:/).count).to eq(0)
     end
   end
 end
