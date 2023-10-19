306072fd16cc2eeb32e3ef881997a1ab3ad26fb1
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-10-18 20:16:32 -0400

    🚧 Apply 'bin/rails app:update' changes to 'application.rb'

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,14 +14,14 @@ module CaseflowCertification
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+
     # Override new 'zeitwerk' autoloading default to preserve 'classic' behavior.
     # We must transition to 'zeitwerk' when upgrading to Rails 7.0.
     config.autoloader = :classic
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     #===================================================================================================================
     # Rails default overrides
