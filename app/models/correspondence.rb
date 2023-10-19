ffbaad18de8ee42cdc0273e00f74f6c3582d1468
Kevin Ma <magavakevin10@gmail.com>
2023-10-19 12:24:53 -0400

    Added top-level comment

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+# Correspondence is a top level object similar to Appeals.
+# Serves as a collection of all data related to Correspondence workflow
 class Correspondence < CaseflowRecord
   has_many :correspondence_documents
 
   # has_many :appeals, through: :correspondence_appeals
-
   # has_many :tasks
-
   # has_many :correspondence_types
 
   # has_many :correspondence_correspondences
