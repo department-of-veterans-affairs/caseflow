539dc041fdaaa4a1fb9e3276d49015d09365867d
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-10-19 17:11:26 -0400

    🔧 Assume default for 'config.action_dispatch.cookies_same_site_protection'
    The default value (`lax`) is safe to assume.
    This will still block CSRF attacks but also allow for a
    "logged-in" experience when authenticated users navigate
    to the site across origins.
    
    Note: `SameSite=lax` is the default for Chrome.

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -6,12 +6,6 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-# Specify cookies SameSite protection level: either :none, :lax, or :strict.
-#
-# This change is not backwards compatible with earlier Rails versions.
-# It's best enabled when your entire app is migrated and stable on 6.1.
-# Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
-
 # Generate CSRF tokens that are encoded in URL-safe Base64.
 #
 # This change is not backwards compatible with earlier Rails versions.
