0a537c1213b8c1d9889f16da46ee6011108b3619
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-10-20 20:51:04 -0400

    ⚗️ Experiment: Support multiple databases the "Rails Way"
    This is an experimental attempt to resolve the following CI build error
    raised during the 'Database setup' stage of the GitHub Workflow:
    
      ActiveRecord::AdapterNotSpecified: The `test` database is not configured for the `test` environment.
    
    I was able to trigger this error locally by running the following task:
    
      DB=etl bundle exec rake db:migrate:status
    
    After migrating from the 'multiverse' gem to the standard Rails 6+ approach
    for configuring multiple databases, the above error no longer occurs.
    
    Let's see if it holds up in CI... 🤞

--- db/etl_schema.rb
+++ db/etl_schema.rb
@@ -2,7 +2,7 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
+# Note that this etl_schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
