96096e8991b5f9d2a4315cd9cb2ae755406a3066
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-10-23 16:24:54 -0500

    spec update 2

--- spec/seeds/correspondence.rb
+++ spec/seeds/correspondence.rb
@@ -19,9 +19,11 @@ describe Seeds::Correspondence do
   end
 
   describe "#seed!" do
-    it "creates correspondences" do
+    it "creates correspondences with one document each" do
       seed.seed!
       expect(Correspondence.count).to eq(10)
+      expect(Correspondence.first.correspondence_documents.count).to eq(1)
+      expect(Correspondence.last.correspondence_documents.count).to eq(1)
       expect(CorrespondenceDocument.count).to eq(10)
     end
   end
