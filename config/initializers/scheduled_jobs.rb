f981048ef1ad69f67f649f7cf2651ef0650bf21c
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-10-23 13:47:26 -0400

    Trigger CI

--- config/initializers/scheduled_jobs.rb
+++ config/initializers/scheduled_jobs.rb
@@ -52,4 +52,6 @@ SCHEDULED_JOBS = {
     "change_hearing_request_type_task_cancellation_job" => ChangeHearingRequestTypeTaskCancellationJob,
     "cannot_delete_contention_remediation_job" => CannotDeleteContentionRemediationJob,
     "contention_not_found_remediation_job" => ContentionNotFoundRemediationJob
+
+
 }.freeze
