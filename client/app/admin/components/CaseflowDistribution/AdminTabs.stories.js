919bdc67d48755eb1743c95d903afbfc7f7d5b15
SHarshain <spoosa@razortalent.com>
2023-10-23 14:46:52 -0400

    WIP

--- client/app/admin/components/CaseflowDistribution/AdminTabs.stories.js
+++ client/app/admin/components/CaseflowDistribution/AdminTabs.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { css } from 'glamor';
+import { connect } from 'react-redux';
 
 import { MemoryRouter } from 'react-router';
 // import { ActiveLiver } from './ActiveLiver';
@@ -12,9 +13,7 @@ import InteractableLever from './InteractableLever';
 import { levers } from 'test/data/adminCaseDistributionLevers';
 import Alert from 'app/components/Alert';
 import UserAlerts from 'app/components/UserAlerts';
-// import { VHA_MEMBERSHIP_REQUEST_AUTOMATIC_VHA_ACCESS_NOTE,
-//   VHA_MEMBERSHIP_REQUEST_DISABLED_OPTIONS_INFO_MESSAGE,
-//   VHA_MEMBERSHIP_REQUEST_FORM_SUBMIT_SUCCESS_MESSAGE } from '../client/COPY.json';
+import {VHA_MEMBERSHIP_REQUEST_DISABLED_OPTIONS_INFO_MESSAGE} from  '../../../../COPY';
 
 // const judgeTeam = createJudgeTeam(1)[0];
 
@@ -23,6 +22,59 @@ const RouterDecorator = (Story) => (
     <Story />
   </MemoryRouter>
 );
+
+const leverTop = css({
+  '& p': {margin: '10px 0', fontSize: '14px'}
+});
+
+const leverContainer = css({
+  width: '100%',
+  border: '1px solid #e4e2e0',
+  borderRadius : '4px',
+  marginTop: '20px',
+  display: 'inline-block',
+  '& h4' : {margin:'0', background: '#e4e2e0', padding: '10px 20px'},
+  '& strong' : {fontSize: '14px'}
+});
+
+const leverContent = css({
+  width: '100%',
+  padding: '20px',
+  paddingBottom: '0',
+  boxSizing: 'border-box',
+  '& p' : {marginTop: '0', marginBottom: '15px', fontSize: '14px'}
+});
+
+const leverHead = css({
+  width: '100%',
+  borderBottom: '1px solid #e4e2e0',
+  display: 'inline-block',
+  paddingBottom: '15px'
+});
+
+const leverLeft = css({
+  width: '70%',
+  display: 'inline-block',
+  marginRight: '30px'
+});
+
+const leverRight = css({
+  width: '25%',
+  display: 'inline-block'
+});
+
+// const leverBottom = css({
+//   width: '100%',
+//   padding: '20px',
+//   boxSizing: 'border-box',
+//   display: 'inline-block'
+// });
+
+// const floatRight = css({
+//   float: 'right',
+//   margin: '0'
+// });
+
 
 export default {
   title: 'Admin/Caseflow Distribution/AdminTabs',
@@ -36,40 +88,38 @@ export default {
   }
 };
 
-// const Template = (args) => (
-//   // <table className={tableStyling}>
-//   //   <tbody>
-//       <AdminTabs {...args} />
-//   //   </tbody>
-//   // </table>
-// );
-
-// export const Default = Template.bind({});
-
 export const tabs = (args) => (
 
   <Tabs {...args}>
     <Tab title="Case Distribution" value="1">
-      <strong>Caseflow Distribution content</strong>
-      <p>This is the first lever. It is a boolean with the default value of true. Therefore there should be a two radio buttons that display true and false as the example with true being the default option chosen.</p>
+      <div className={leverTop}>
+        <strong>Caseflow Distribution content</strong>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      </div>
       {/* <Alert
-            type="info"
-            message={VHA_MEMBERSHIP_REQUEST_DISABLED_OPTIONS_INFO_MESSAGE}
-          /> */}
-      {/* <React.Fragment>
-        <UserAlerts />
-        <Alert
-          type="Info"
-          message='You cannot add more veterans to this hearing day, but you can edit existing entries'
-        />
-      </React.Fragment> */}
-
-      { levers.map((lever) => {
-        return lever ? (
-          <InteractableLever key={lever.item} lever={lever} />
-        ) : null;
-      })}
-
+          type="info"
+          message="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      /> */}
+      <div className={leverContainer}>
+        <h4>Active Data Elements</h4>
+        <div className={leverContent}>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p><strong> <sup>*</sup> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</strong></p>
+          <div className={leverHead}>
+            <div className={leverLeft}><strong>Data Elements</strong></div>
+            <div className={leverRight}><strong>Value</strong></div>
+          </div>
+          { levers.map((lever) => {
+            return lever ? (
+              <InteractableLever key={lever.item} lever={lever} />
+            ) : null;
+          })}
+          {/* <div className={leverBottom}>
+            <a href='#' >Cancel</a>
+            <button className={floatRight}>Save Changes</button>
+          </div> */}
+        </div>
+      </div>
     </Tab>
     <Tab title="Veteran Extract" value="2">
       Veteran Extract content
--- client/app/admin/components/CaseflowDistribution/InteractableLever.jsx
+++ client/app/admin/components/CaseflowDistribution/InteractableLever.jsx
@@ -5,10 +5,13 @@ import PropTypes, { number } from 'prop-types';
 import { css } from 'glamor';
 import TextField from 'app/components/TextField';
 import RadioField from 'app/components/RadioField';
+import NumberField from 'app/components/NumberField';
 
 const activeLever = css({
   width: '100%',
-  marginTop: '30px'
+  padding: '15px',
+  borderBottom: '1px solid #e4e2e0',
+  boxSizing: 'border-box'
 });
 
 const leverLeft = css({
@@ -16,158 +19,106 @@ const leverLeft = css({
   display: 'inline-block',
   verticalAlign: 'top',
   marginRight: '30px',
-  '& p' : {fontSize: '12px'}
+  '& p' : { marginBottom: '0 !important'}
 });
 
-const leaverRight = css({
+const leverRight = css({
   width: '25%',
   display: 'inline-block',
   '& input' : {width: '60px', height:'20px', marginRight: '10px', fontSize: '12px', display: 'inline-block'},
-  '& .cf-form-radio span' : {display: 'none'}
+  '& .cf-form-radio span' : {display: 'none'},
+  '& .cf-form-textinput span' : {display: 'none'},
+  '& div:first-child' : {width: 'auto', display: 'inline-block'}
 });
 
 const radioOptions = [{
   displayText: 'True',
-  value: 0
+  value: false
 }, {
   displayText: 'False',
-  value: 1
+  value: true
 }];
 
 const radioOptions2 = [{
   displayText: 'Option 1',
-  value: 0
+  value: 1
 }];
 
 const radioOptions3 = [{
   displayText: 'Option 2',
-  value: 0
+  value: 2
 }, {
   displayText: 'Option 3',
-  value: 1
+  value: 3
 }];
 
 const radioOnChange =() => {
-  console.log("I am changed :" +this.options)
+  console.log("I am Clicked")
 }
 
 const InteractableLever = ({ lever }) => {
-  if(lever.data_type=='boolean'){
-    return (
-      <div className={activeLever}>
-        <div className={leverLeft}>
-          <strong>{lever.title}</strong>
-          <p>{lever.description}</p>
-        </div>
-        <div className={leaverRight}>
-          <RadioField
-              vertical
-              label={false}
-              name={lever.item}
-              options={radioOptions}
-              // key={approxDecisionDate}
-              // value={this.state.selectedContestableIssueIndex}
-              // onChange={this.radioOnChange}
-            />
-          <span>{lever.unit}</span>
-        </div>
+  return (
+    <div className={activeLever}>
+      <div className={leverLeft}>
+        <strong>{lever.title}</strong>
+        <p>{lever.description}</p>
       </div>
-    );
-  }
-  else if(lever.data_type=='number'){
-    return (
-      <div className={activeLever}>
-        <div className={leverLeft}>
-          <strong>{lever.title}</strong>
-          <p>{lever.description}</p>
-        </div>
-        <div className={leaverRight}>
-          <input type="number" value={lever.value} />
-          {/* <TextField
-              maxLength={4}
-              name="page"
-              // label="Page"
-              // onChange={setPageNumber}
-              // onKeyPress={handleKeyPress}
-              value={lever.value}
-              // required={true}
-              type={number}
-          /> */}
-          <span>{lever.unit}</span>
-        </div>
-      </div>
-    );
-  }
-  else if(lever.data_type=='radio'){
-    return (
-      <div className={activeLever}>
-        <div className={leverLeft}>
-          <strong>{lever.title}</strong>
-          <p>{lever.description}</p>
-        </div>
-        <div className={leaverRight}>
-            <RadioField
-              vertical
-              label={false}
-              name={lever.item}
-              options={radioOptions2}
-              // key={approxDecisionDate}
-              // value={this.state.selectedContestableIssueIndex}
-              onChange={radioOnChange}
-            />
-          <input type="text" value={lever.value} />
-          <RadioField
-              vertical
-              label={false}
-              name={lever.item}
-              options={radioOptions3}
-              // key={approxDecisionDate}
-              // value={this.state.selectedContestableIssueIndex}
-              onChange={radioOnChange}
-            />
-          {/* <input type="text" value={lever.value} /> */}
-          {/* <TextField
-              maxLength={4}
-              name="page"
-              // label="Page"
-              // onChange={setPageNumber}
-              // onKeyPress={handleKeyPress}
-              value={lever.value}
-              // required={true}
-              type={number}
-          /> */}
-          <span>{lever.unit}</span>
-        </div>
-      </div>
-    );
-  }
-  else if(lever.data_type=='combination'){
-    return (
-      <div className={activeLever}>
-        <div className={leverLeft}>
-          <strong>{lever.title}</strong>
-          <p>{lever.description}</p>
-        </div>
-        <div className={leaverRight}>
-          <input type="text" value='Toggle' />
-          {/* <TextField
-              maxLength={4}
-              name="page"
-              // label="Page"
-              // onChange={setPageNumber}
-              // onKeyPress={handleKeyPress}
-              value={lever.value}
-              // required={true}
-              type={number}
-          /> */}
-          <span>{lever.unit}</span>
-        </div>
-      </div>
-    );
-  }
-  else {
-    return null;
-  }
+
+      {(() => {
+        if (lever.data_type=='boolean') {
+          return (
+            <div className={leverRight}>
+              <RadioField
+                  vertical
+                  label={false}
+                  name={lever.item}
+                  options={radioOptions}
+                  value={lever.value}
+                />
+              <span>{lever.unit}</span>
+          </div>
+          );
+        } else if(lever.data_type=='number'){
+          return (
+            <div className={leverRight}>
+              <NumberField
+                  name="number"
+                  value={lever.value}
+              />
+              <span>{lever.unit}</span>
+            </div>
+          );
+        } else if(lever.data_type=='radio'){
+          return (
+            <div className={leverRight}>
+              <RadioField
+                vertical
+                label={false}
+                name={lever.item}
+                options={radioOptions2}
+                onChange={radioOnChange}
+              />
+              <input type="text" value={lever.title} />
+              <RadioField
+                  vertical
+                  label={false}
+                  name={lever.value}
+                  options={radioOptions3}
+                  onChange={radioOnChange}
+              />
+            </div>
+          );
+        } else if(lever.data_type=='combination'){
+          return (
+            <div className={leverRight}>
+                <input type="text" value='Toggle' />
+                <span>{lever.unit}</span>
+            </div>
+          );
+        }
+      })()}
+    </div>
+  );
 };
 
 InteractableLever.propTypes = {
