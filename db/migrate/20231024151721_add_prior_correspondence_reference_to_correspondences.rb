0611863267702a6b154c2253c3192911409feb43
divyadasari-va <divya.dasari@va.gov>
2023-10-24 10:49:34 -0600

    Spell check

--- db/migrate/20231024151721_add_prior_correspondence_reference_to_correspondences.rb
+++ db/migrate/20231024151721_add_prior_correspondence_reference_to_correspondences.rb
@@ -2,7 +2,7 @@ class AddPriorCorrespondenceReferenceToCorrespondences < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
-    add_reference :correspondences, :prior_correspondence, null: false, foreign_key: { to_table: :correspondences }, index: false, comment: "Foreign key to Coreespondences table"
+    add_reference :correspondences, :prior_correspondence, null: false, foreign_key: { to_table: :correspondences }, index: false, comment: "Foreign key to Correspondences table"
     add_index :correspondences, :prior_correspondence_id, algorithm: :concurrently
   end
 end
