3a0d87f362cc0b745b1df7d45506a292b5ba28ed
minhazur9 <65432922+minhazur9@users.noreply.github.com>
2023-10-24 15:43:05 -0400

    Update spec/services/va_dot_gov_address_validator_spec.rb
    Co-authored-by: Marc Steele <71673522+msteele96@users.noreply.github.com>

--- spec/services/va_dot_gov_address_validator_spec.rb
+++ spec/services/va_dot_gov_address_validator_spec.rb
@@ -36,7 +36,7 @@ describe VaDotGovAddressValidator do
     context "when appellant does not live in the us" do
       let(:ro_facility_id) { nil }
       before { appeal.instance_variable_set(:@address, non_us_address) }
-      it "returns RO71" do
+      it "returns RO11" do
         expect(subject).to eq("RO11")
       end
     end
