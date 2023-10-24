b7e9b5c8952c6bc6464749455a75fdb259e2cb7e
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-10-24 15:20:42 -0500

    forgot seed update

--- db/seeds/correspondence.rb
+++ db/seeds/correspondence.rb
@@ -37,12 +37,6 @@ module Seeds
 
         corres = create(
           :correspondence,
-          uuid: SecureRandom.uuid,
-          portal_entry_date: Time.zone.now,
-          source_type: "Mail",
-          package_document_type_id: 1250,
-          cmp_packet_number: rand(1_000_000_000..9_999_999_999),
-          va_date_of_receipt: Time.zone.yesterday,
           veteran_id: veteran.id
         )
         create(
