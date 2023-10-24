61281cd95cd78e80632c08272d0556655fc2a35a
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-10-24 15:02:25 -0500

    update correspondence seed and factory

--- spec/factories/correspondence.rb
+++ spec/factories/correspondence.rb
@@ -5,7 +5,9 @@ FactoryBot.define do
     uuid { SecureRandom.uuid }
     portal_entry_date { Time.zone.now }
     source_type { "Mail" }
-    package_document_type_id { 1250 }
+    package_document_type_id { 16 }
+    correspondence_type_id { 9 }
+    cmp_queue_id { 1 }
     cmp_packet_number { rand(1_000_000_000..9_999_999_999) }
     va_date_of_receipt { Time.zone.yesterday }
   end
