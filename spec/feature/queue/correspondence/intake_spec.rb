29403fae911b5629116d6b1eaf71afd3dff73801
Ryan Marshall <rnmarshall1993@gmail.com>
2023-10-25 13:29:17 -0700

    added unit tests

--- spec/feature/queue/correspondence/intake_spec.rb
+++ spec/feature/queue/correspondence/intake_spec.rb
@@ -1,9 +1,54 @@
-RSpec.feature('The Correspondence Intake page') do
+# frozen_string_literal: true
+
+RSpec.feature("The Correspondence Intake page") do
   context "intake form shell" do
+    before :each do
+      User.authenticate!(roles: ["Mail Intake"])
+      @correspondence_uuid = "123456789"
+      visit "/queue/correspondence/#{@correspondence_uuid}/intake"
+    end
+
     it "the intake page exists" do
-      correspondence_uuid = '123456789'
-      visit "/queue/correspondence/#{correspondence_uuid}/intake"
-      expect current_path.to eq("/queue/correspondence/123456789/intake")
+      expect(page).to have_current_path("/queue/correspondence/#{@correspondence_uuid}/intake")
+    end
+
+    it "successfully navigates on cancel link click" do
+      click_on("button-Cancel")
+      expect(page).to have_current_path("/queue/correspondence")
+    end
+
+    it "successfully advances to the second step" do
+      click_on("button-continue")
+      expect(page).to have_button("Continue")
+      expect(page).to have_button("Back")
+      expect(page).to have_no_button("Submit")
+    end
+
+    it "successfully advances to the final step" do
+      click_on("button-continue")
+      click_on("button-continue")
+      expect(page).to have_button("Submit")
+      expect(page).to have_button("Back")
+      expect(page).to have_no_button("Continue")
+    end
+
+    it "successfully returns to the first step" do
+      click_on("button-continue")
+      click_on("button-back-button")
+
+      expect(page).to have_button("Continue")
+      expect(page).to have_no_button("Back")
+      expect(page).to have_no_button("Submit")
+    end
+
+    it "successfully returns to the second step" do
+      click_on("button-continue")
+      click_on("button-continue")
+      click_on("button-back-button")
+
+      expect(page).to have_button("Continue")
+      expect(page).to have_button("Back")
+      expect(page).to have_no_button("Submit")
     end
   end
 end
