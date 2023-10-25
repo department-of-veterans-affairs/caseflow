c23e43631654bf6c3c3c56755827f803e5cd3de2
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-10-25 14:23:39 -0400

    Add initializers

---
+++ config/initializers/credstash.rb
@@ -0,0 +1,5 @@
+CredStash.configure do |config|
+  # Table name will probably change
+  config.table_name = 'credential-store'
+  config.storage = (Rails.env.development? || Rails.env.test?) ? :dynamodb_local : :dynamodb
+end
