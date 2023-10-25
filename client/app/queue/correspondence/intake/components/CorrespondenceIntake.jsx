97e5cdf98f2cec97bbb3f5c9f1ba0d3df05b5368
William Medders <medders_william@bah.com>
2023-10-25 10:23:41 -0500

    Added button functionality

--- client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
+++ client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
@@ -1,44 +1,81 @@
-import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import ProgressBar from 'app/components/ProgressBar';
 import Button from '../../../../components/Button';
+import { css } from 'glamor';
 
 const progressBarSections = [
   {
     title: '1. Add Related Correspondence',
-    current: false
+    step: 1
   },
   {
     title: '2. Review Tasks & Appeals',
-    current: true
+    step: 2
   },
   {
     title: '3. Confirm',
-    current: false
+    step: 3
   },
 ];
 
 export const CorrespondenceIntake = () => {
-  const sections = useMemo(
-    () =>
-      progressBarSections.map(({ title, current }) => ({
-        title,
-        current
-      })),
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const nextStep = () => {
+    if (currentStep < 3) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const prevStep = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const sections = progressBarSections.map(({ title, step }) => ({
+    title,
+    current: (step === currentStep)
+  }),
   );
 
   return <div>
     <ProgressBar sections={sections} />
-    <Button
-      name="Cancel"
-      classNames={['cf-btn-link']} />
-    <Button
-      type="button"
-      name="Continue"
-      classNames={['cf-push-right']}>
-        Continue
-    </Button>
+    <a href="/queue/correspondence">
+      <Button
+        name="cancel"
+        href="/queue/correspondence"
+        classNames={['cf-btn-link']} />
+    </a>
+    <div >
+      {currentStep > 1 &&
+      <Button
+        type="button"
+        onClick={prevStep}
+        name="back-button"
+        classNames={['usa-button-secondary', 'button-back-button']}>
+          Back
+      </Button>}
+      {currentStep < 3 &&
+      <Button
+        type="button"
+        onClick={nextStep}
+        name="continue"
+        styling={css({ marginLeft: '1rem' })}
+        classNames={['cf-right-side']}>
+          Continue
+      </Button>}
+      {currentStep === 3 &&
+      <Button
+        type="button"
+        name="Submit"
+        styling={css({ marginLeft: '1rem' })}
+        classNames={['cf-right-side']}>
+          Submit
+      </Button>}
+    </div>
   </div>;
 };
+
 export default CorrespondenceIntake;
 
