faab3344ee3f10121fa2f96b40c58d1affa9ff06
divyadasari-va <135847343+divyadasari-va@users.noreply.github.com>
2023-10-26 12:36:37 -0600

    Divya/appeals 29404 (#19787)
    * Associate Correspondence to Correspondences Database table
    
    * Spell check
    
    * migration file change with caseflow migration inheretence
    
    * Added correspondences index as per the new guidance and removed other commits from db schema
    
    * Removed add_index
    
    * Merge the changes into one file and removed 2nd migration file
    
    * Test case for correspondence relation
    
    ---------
    
    Co-authored-by: Jim Foley <james.foley4@va.gov>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_24_162133) do
+ActiveRecord::Schema.define(version: 2023_10_25_143040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -220,7 +220,7 @@ ActiveRecord::Schema.define(version: 2023_10_24_162133) do
     t.index ["veteran_file_number"], name: "index_available_hearing_locations_on_veteran_file_number"
   end
 
-  create_table "batch_processes", primary_key: "batch_id", id: :uuid, default: -> { "uuid_generate_v4()" }, comment: "A generalized table for batching and processing records within caseflow", force: :cascade do |t|
+  create_table "batch_processes", primary_key: "batch_id", id: :uuid, default: -> { "uuid_generate_v4()" }, comment: "The unique id of the created batch", comment: "A generalized table for batching and processing records within caseflow", force: :cascade do |t|
     t.string "batch_type", null: false, comment: "Indicates what type of record is being batched"
     t.datetime "created_at", null: false, comment: "Date and Time that batch was created."
     t.datetime "ended_at", comment: "The date/time that the batch finsished processing"
@@ -608,6 +608,7 @@ ActiveRecord::Schema.define(version: 2023_10_24_162133) do
     t.text "notes", comment: "Comes from CMP; can be updated by user"
     t.integer "package_document_type_id", comment: "Represents entire CMP package document type"
     t.datetime "portal_entry_date", comment: "Time when correspondence is created in Caseflow"
+    t.bigint "prior_correspondence_id", null: false, comment: "Foreign key to Correspondences table"
     t.string "source_type", comment: "An information identifier we get from CMP"
     t.datetime "updated_at", null: false, comment: "Standard created_at/updated_at timestamps"
     t.bigint "updated_by_id", comment: "Foreign key to users table"
@@ -617,6 +618,7 @@ ActiveRecord::Schema.define(version: 2023_10_24_162133) do
     t.index ["assigned_by_id"], name: "index_correspondences_on_assigned_by_id"
     t.index ["cmp_queue_id"], name: "index_correspondences_on_cmp_queue_id"
     t.index ["correspondence_type_id"], name: "index_correspondences_on_correspondence_type_id"
+    t.index ["prior_correspondence_id"], name: "index_on_prior_correspondence_id"
     t.index ["updated_by_id"], name: "index_correspondences_on_updated_by_id"
     t.index ["veteran_id"], name: "index_correspondences_on_veteran_id"
   end
@@ -2123,6 +2125,7 @@ ActiveRecord::Schema.define(version: 2023_10_24_162133) do
   add_foreign_key "conference_links", "users", column: "created_by_id"
   add_foreign_key "conference_links", "users", column: "updated_by_id"
   add_foreign_key "correspondence_documents", "correspondences"
+  add_foreign_key "correspondences", "correspondences", column: "prior_correspondence_id"
   add_foreign_key "dispatch_tasks", "legacy_appeals", column: "appeal_id"
   add_foreign_key "dispatch_tasks", "users"
   add_foreign_key "distributed_cases", "distributions"
