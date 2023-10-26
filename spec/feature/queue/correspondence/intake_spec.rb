b90d4dd39a5eba92af5415de6aae4e53d99b396e
sulak-j <justin.sulak@va.gov>
2023-10-26 11:46:29 -0500

    Writing test for feature toggle

--- spec/feature/queue/correspondence/intake_spec.rb
+++ spec/feature/queue/correspondence/intake_spec.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.feature("The Correspondence Intake page") do
+  context "intake form feature toggle" do
+    before :each do
+      User.authenticate!(roles: ["Mail Intake"])
+      @correspondence_uuid = "123456789"
+    end
+
+    it "routes user to /unaithorized if the feature toggle is disabled" do
+      FeatureToggle.disable!(:correspondence_queue)
+      visit "/queue/correspondence/#{@correspondence_uuid}/intake"
+      expect(page).to have_current_path("/unauthorized")
+    end
+
+    it "routes to intake if feature toggle is enabled" do
+      it "routes user to /unaithorized if the feature toggle is disabled" do
+        FeatureToggle.enable!(:correspondence_queue)
+        visit "/queue/correspondence/#{@correspondence_uuid}/intake"
+        expect(page).to have_current_path("/queue/correspondence")
+      end
+    end
+  end
+
   context "intake form shell" do
     before :each do
       User.authenticate!(roles: ["Mail Intake"])
