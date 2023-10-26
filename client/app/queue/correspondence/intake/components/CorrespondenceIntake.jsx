592594119a14f2701e49b9022a4023895fb262c0
KiMauVA <Ki.Mau@va.gov>
2023-10-26 12:14:29 -0400

    APPEALS-29403 - Updated with Preliminary Work

--- client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
+++ client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import ProgressBar from 'app/components/ProgressBar';
 import Button from '../../../../components/Button';
+import RadioField from '../../../../components/RadioField';
 
 const progressBarSections = [
   {
     title: '1. Add Related Correspondence',
+    current: true,
     step: 1
   },
   {
     title: '2. Review Tasks & Appeals',
+    current: false,
     step: 2
   },
   {
     title: '3. Confirm',
+    current: false,
     step: 3
   },
+];
+
+const priorMailAnswer = [
+  { displayText: 'Yes',
+    value: 'yes' },
+  { displayText: 'No',
+    value: 'no' }
 ];
 
 export const CorrespondenceIntake = () => {
@@ -38,6 +49,12 @@ export const CorrespondenceIntake = () => {
   }),
   );
 
+  const [selectedValue, setSelectedValue] = useState('no');
+
+  const handleRadioChange = (event) => {
+    setSelectedValue(event);
+  };
+
   return <div>
     <ProgressBar
       sections={sections}
@@ -51,6 +68,28 @@ export const CorrespondenceIntake = () => {
           href="/queue/correspondence"
           classNames={['cf-btn-link', 'cf-left-side']} />
       </a>
+      {currentStep === 1 &&
+        <div className="cf-app-segment cf-app-segment--alt">
+          <h1>Add Related Correspondence</h1>
+          <p>Add any related correspondence to the mail package that is in progress.</p>
+
+          <h3>Associate with prior Mail</h3>
+
+          <p>Is this correspondence related to prior mail?</p>
+          <RadioField
+            name=""
+            options={priorMailAnswer}
+            value={selectedValue}
+            onChange={handleRadioChange} />
+
+          {selectedValue === 'yes' && (
+            <div className="cf-app-segment cf-app-segment--alt">
+              <p>Please select the prior mail to link to this correspondence</p>
+              <p>Viewing 1-15 out of 200 total</p>
+            </div>
+          )}
+        </div>
+      }
       {currentStep < 3 &&
       <Button
         type="button"
