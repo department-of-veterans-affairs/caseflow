72c846f56bae4c155a41b9daa595fa3e7f067210
Prajwal Amatya <pamatya@take2it.com>
2023-10-26 00:27:13 -0600

    linter issue fixed

--- client/app/nonComp/components/NonCompReportFilter.jsx
+++ client/app/nonComp/components/NonCompReportFilter.jsx
@@ -3,7 +3,7 @@ import { Controller, useFormContext } from 'react-hook-form';
 import PropTypes from 'prop-types';
 
 import SearchableDropdown from '../../components/SearchableDropdown';
-import REPORT_TYPE_OPITIONS from '../../../constants/REPORT_TYPE_OPTIONS.json';
+import REPORT_TYPE_OPITIONS from '../../../constants/REPORT_TYPE_OPTIONS';
 
 const NonCompReportFilter = ({ control }) => (
   <>
