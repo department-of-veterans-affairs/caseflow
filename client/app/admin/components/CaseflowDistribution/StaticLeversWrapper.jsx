6d62bca4b0c3d2e08021341f33bfbcca2e44dc06
mbeardy <michael.beard@va.gov>
2023-10-26 14:28:26 -0500

    adds staticleverwrapper componenet

--- client/app/admin/components/CaseflowDistribution/StaticLeversWrapper.jsx
+++ client/app/admin/components/CaseflowDistribution/StaticLeversWrapper.jsx
@@ -1,26 +1,37 @@
-// client/app/admin/components/CaseflowDistribution/StaticLeverWrapper.js
-
 import React from 'react';
-import StaticLever from './FutureLever';
+import StaticLever from './StaticLever';
 import PropTypes from 'prop-types';
 
-const StaticLeverWrapper = ({ InteractableLevers, levers }) => {
+const StaticLeverWrapper = ({ levers }) => {
   return (
     <div>
-      <h3>Inactive Levers</h3>
-      {InteractableLevers.map((leverItem) => {
-        const lever = levers.find((l) => l.item === leverItem);
-        return lever ? (
-          <StaticLever key={lever.item} lever={lever} />
-        ) : null;
-      })}
+      {levers.map((lever) => (
+        <StaticLever key={lever.title} lever={lever} />
+      ))}
     </div>
   );
 };
 
 StaticLeverWrapper.propTypes = {
-  StaticLevers: PropTypes.array.isRequired,
-  levers: PropTypes.array.isRequired,
+  levers: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      data_type: PropTypes.string.isRequired,
+      value: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]).isRequired,
+      unit: PropTypes.string.isRequired,
+      is_active: PropTypes.bool.isRequired,
+      options: PropTypes.arrayOf(
+        PropTypes.shape({
+          item: PropTypes.string.isRequired,
+          data_type: PropTypes.string.isRequired,
+          value: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]).isRequired,
+          text: PropTypes.string,
+          unit: PropTypes.string,
+        })
+      ),
+    })
+  ).isRequired,
 };
 
 export default StaticLeverWrapper;
