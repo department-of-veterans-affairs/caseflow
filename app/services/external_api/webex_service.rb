ebebc01caefb73f02fb16f02ec9462d3d6127852
Minhazur Rahaman <minhazur9@gmail.com>
2023-10-26 16:46:11 -0400

    APPEALS_33661 made method for refreshing access token

--- app/services/external_api/webex_service.rb
+++ app/services/external_api/webex_service.rb
@@ -1,11 +1,37 @@
 # frozen_string_literal: true
 
 class ExternalApi::WebexService
+  BASE_URL = "#{ENV['WEBEX_HOST']}#{ENV['WEBEX_DOMAIN']}"
+  AUTH_URL = "/v1/access_token"
+  GRANT_TYPE = "refresh_token"
+  CLIENT_ID = ENV["WEBEX_CLIENT_ID"]
+  CLIENT_SECRET = ENV["WEBEX_CLIENT_SECRET"]
+  REFRESH_TOKEN = ENV["WEBEX_REFRESH_TOKEN"]
+  SCOPE = ""
+  HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded", Accept: "application/json"
+  }.freeze
+
   def create_conference(*)
     fail NotImplementedError
   end
 
   def delete_conference(*)
     fail NotImplementedError
+  end
+
+  # Purpose: Refreshing the access token to access the API
+  # Return: The response body
+  def refresh_access_token
+    url = URI::DEFAULT_PARSER.escape(BASE_URL + GET_TOKEN_ENDPOINT)
+    params = {
+      grant_type: GRANT_TYPE,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      refresh_token: REFRESH_TOKEN
+    }
+    encoded_params = URI.encode_www_form(params)
+    response = Faraday.post(url, encoded_params)
+    response.body
   end
 end
