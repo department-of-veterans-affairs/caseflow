5a564d5a7bfd3769910ed5dfccf653317195ad16
Michael Beard <131783726+mbeardy@users.noreply.github.com>
2023-10-27 16:01:19 -0500

    adds staticlever, test, story components to caseflow distribution (#19818)

---
+++ client/app/admin/components/CaseflowDistribution/StaticLever.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import StaticLever from './StaticLever';
+
+describe('StaticLever', () => {
+  const lever = {
+    title: 'Test Title',
+    description: 'Test Description',
+    data_type: 'number',
+    value: 10,
+    unit: 'Days',
+    is_active: true,
+    options: [],
+  };
+
+  it('renders the title', () => {
+    const wrapper = shallow(<StaticLever lever={lever} />);
+
+    expect(wrapper.find('td').at(0).
+      text()).toEqual('Test Title');
+  });
+
+  it('renders the description', () => {
+    const wrapper = shallow(<StaticLever lever={lever} />);
+
+    expect(wrapper.find('td').at(1).
+      text()).toEqual('Test Description');
+  });
+
+  it('renders the value and unit', () => {
+    const wrapper = shallow(<StaticLever lever={lever} />);
+
+    expect(wrapper.find('td').at(2).
+      text()).toEqual('10 Days');
+  });
+});
