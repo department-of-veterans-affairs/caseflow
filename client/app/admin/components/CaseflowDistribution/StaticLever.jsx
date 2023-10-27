a8eecda2b90dffa0c099d1315e84c9c8777e2af1
mbeardy <michael.beard@va.gov>
2023-10-27 15:17:38 -0500

    fixes styling, removes top border

--- client/app/admin/components/CaseflowDistribution/StaticLever.jsx
+++ client/app/admin/components/CaseflowDistribution/StaticLever.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 const StaticLever = ({ lever }) => {
   const renderValue = () => {
     switch (lever.data_type) {
@@ -16,68 +15,50 @@ const StaticLever = ({ lever }) => {
       return null;
     }
   };
-
-  const topLineStyle = {
-    borderTop: '2px solid #eee',
-    // marginBottom: '-110px',
-    position: 'absolute',
-  };
-
-  const bottomLineStyle = {
-    borderBottom: '2px solid #eee',
-    // marginBottom: '10px',
-    marginTop: '-20px',
-  };
-
   const titleStyling = {
     border: 'none',
-    paddingTop: '10px',
-    // marginTop: '20',
-    marginBottom: '6px',
-    paddingBottom: '7px',
+    paddingTop: '20px',
+    marginTop: '0',
+    paddingBottom: '0',
     verticalAlign: 'text-top',
     fontFamily: 'Source Sans Pro',
-    fontWeight: '700',
-    fontSize: '15px',
-    lineHeight: '.25em',
+    fontWeight: 'bold',
+    fontSize: '17px',
+    lineHeight: '1.5em/33px',
+    columnSpan: 'all',
   };
-
   const descriptionStyling = {
     border: 'none',
     paddingTop: '0',
-    marginTop: '-5px',
+    marginTop: '0',
+    maxWidth: '80%',
     paddingRight: '20px',
-    paddingBottom: '-20px',
+    paddingBottom: '20px',
     verticalAlign: 'text-top',
     fontFamily: 'Source Sans Pro',
     fontWeight: '400',
     fontSize: '15px',
-    lineHeight: '1.5em/26px',
-    // borderBottom: '1px solid #ccc',
+    lineHeight: '1.5em/33px',
+    borderBottom: '1px solid #ccc',
   };
-
   const valueStyling = {
     border: 'none',
     paddingTop: '0',
     marginTop: '0',
-    paddingBottom: '-20px',
-    // paddingRight: '20px',
+    whiteSpace: 'noWrap',
+    paddingBottom: '20px',
+    paddingRight: '20px',
     verticalAlign: 'text-top',
     fontFamily: 'Source Sans Pro',
     fontWeight: '400',
     fontSize: '15px',
     lineHeight: '1.5em/33px',
     textAlign: 'right',
-    // borderBottom: '1px solid #ccc',
-    display: 'flex',
-    alignItems: 'baseline',
-    justifyContent: 'flex-end',
+    borderBottom: '1px solid #ccc',
   };
-
   const tableStyling = {
     borderCollapse: 'collapse',
   };
-
   const valueWithUnit = renderValue();
   const valueArray = valueWithUnit.split(' ');
   const value = valueArray[0];
@@ -85,23 +66,20 @@ const StaticLever = ({ lever }) => {
 
   return (
     <div>
-      <div style={topLineStyle}>
-        <table style={tableStyling}>
-          <tbody>
-            <tr>
-              <td style={titleStyling}>{lever.title}</td>
-            </tr>
-            <tr>
-              <td style={descriptionStyling}>{lever.description}</td>
-              <td style={valueStyling}>
-                <span style={{ marginRight: '5px' }}>{value} </span>
-                <span>{unit}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div style={bottomLineStyle}></div>
-      </div>
+      <table style={tableStyling}>
+        <tbody>
+          <tr>
+            <td style={titleStyling}>{lever.title}</td>
+          </tr>
+          <tr>
+            <td style={descriptionStyling}>{lever.description}</td>
+            <td style={valueStyling}>
+              <span style={{ marginRight: '5px' }}>{value} </span>
+              <span>{unit}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -125,5 +103,5 @@ StaticLever.propTypes = {
     ),
   }).isRequired,
 };
-
 export default StaticLever;
+
