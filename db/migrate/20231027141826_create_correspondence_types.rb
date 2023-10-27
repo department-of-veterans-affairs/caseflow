9793c0c888dd61335f79b67c7dda5dc188d30581
vinner57 <128258952+vinner57@users.noreply.github.com>
2023-10-27 11:08:59 -0400

    adding correspondence type data

---
+++ db/migrate/20231027141826_create_correspondence_types.rb
@@ -0,0 +1,9 @@
+class CreateCorrespondenceTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :correspondence_types do |t|
+      t.string :name
+      t.boolean :active, default: true
+      t.timestamps
+    end
+  end
+end
