65e964e42692fb3ac13006d7b8caed972f02555b
divyadasari-va <divya.dasari@va.gov>
2023-10-27 08:39:30 -0600

    Changing the prior_correspondence_id default value to null

--- db/migrate/20231024151721_add_prior_correspondence_reference_to_correspondences.rb
+++ db/migrate/20231024151721_add_prior_correspondence_reference_to_correspondences.rb
@@ -2,7 +2,7 @@ class AddPriorCorrespondenceReferenceToCorrespondences < Caseflow::Migration
   disable_ddl_transaction!
 
   def change
-    add_column :correspondences, :prior_correspondence_id, :integer, null: false
+    add_column :correspondences, :prior_correspondence_id, :integer, null: true
     add_safe_index :correspondences, [:prior_correspondence_id], name: "index_on_prior_correspondence_id"
   end
 end
