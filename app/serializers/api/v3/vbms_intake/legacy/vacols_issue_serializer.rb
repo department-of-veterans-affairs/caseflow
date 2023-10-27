93e454d7ac95befda0ce1baf6ed63f23306b2f58
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-10-27 16:01:35 -0700

    APPEALS-33403 - create vbms_legacy_appeal_serializer, and was able to get a reponse on the first vacols_id, json response has both legacy appeal and vacols issue.

--- app/serializers/api/v3/vbms_intake/legacy/vacols_issue_serializer.rb
+++ app/serializers/api/v3/vbms_intake/legacy/vacols_issue_serializer.rb
@@ -4,6 +4,11 @@ class Api::V3::VbmsIntake::Legacy::VacolsIssueSerializer
   include JSONAPI::Serializer
 
   set_type :issue
-
-  attribute :vacols_issue, &:vbms_attributes
+  attribute :id
+  attribute :legacy_appeal do |object|
+    object.appeal.try do |appeal|
+      Api::V3::VbmsIntake::Legacy::VbmsLegacyAppealSerializer.new(appeal).serializable_hash
+    end
+  end
+  attribute :vacols_issue, &:intake_attributes
 end
