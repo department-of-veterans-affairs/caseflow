1961d77abf29351130d4981aa2ed4e2e96528dde
Alexander Smith <alex.smith.99@thunderyard.com>
2023-10-30 12:19:22 -0600

    Create Initial Tasks and Appeals Page

--- client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
+++ client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ProgressBar from 'app/components/ProgressBar';
 import Button from '../../../../components/Button';
 
+import { AddTasksAppealsView } from './TasksAppeals/AddTasksAppealsView';
+
 const progressBarSections = [
   {
     title: '1. Add Related Correspondence',
@@ -36,13 +38,17 @@ export const CorrespondenceIntake = () => {
     title,
     current: (step === currentStep)
   }),
-  );
+);
 
   return <div>
     <ProgressBar
       sections={sections}
       classNames={['cf-progress-bar', 'cf-']}
-      styling={{ style: { marginBottom: '5rem', float: 'right' } }} />
+      styling={{ style: { marginBottom: '5rem', float: 'right' } }}
+    />
+    {currentStep === 2 &&
+      <AddTasksAppealsView />
+    }
     <div>
       <a href="/queue/correspondence">
         <Button
---
+++ client/app/queue/correspondence/intake/components/TasksAppeals/AddTasksAppealsView.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Checkbox from '../../../../../components/Checkbox';
+
+const mailTasksLeft = [
+  'Change of address',
+  'Evidence or argument',
+  'Returned or undeliverable mail'
+];
+
+const mailTasksRight = [
+  'Sent to ROJ',
+  'VACOLS updated',
+  'Associated with Claims Folder'
+];
+
+export const AddTasksAppealsView = () => {
+  return (
+    <div style={{ border: "2px solid #e0e1e2", marginBottom: "2rem", padding: "3rem 4rem" }}>
+      <h1 style={{ marginBottom: "10px" }}>Review Tasks & Appeals</h1>
+      <p>Review any previously complete tasks by the mail team and add new tasks for either the mail package or for linked appeals, if any.</p>
+      <div>
+        <h2 style={{ margin: "25px auto 15px auto" }}>Mail Tasks</h2>
+        <div style={{ border: "2px solid #e0e1e2", padding: "0rem 2rem" }}>
+          <p style={{ marginBottom: "0.5rem" }}>Select any tasks completed by the Mail team for this correspondence.</p>
+          <div style={{ display: "inline-block", marginRight: "14rem" }}>
+            {mailTasksLeft.map(function(name, index){
+              return <Checkbox
+                key={index}
+                name={name}
+                label={name}
+              />
+            })}
+          </div>
+          <div style={{ display: "inline-block" }}>
+            {mailTasksRight.map(function(name, index){
+              return <Checkbox
+                key={index}
+                name={name}
+                label={name}
+              />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTasksAppealsView;
