223546f536faa90d7aa5e63e2a090a4b452a94b1
mbeardy <michael.beard@va.gov>
2023-10-30 15:53:29 -0500

    adding stories with wrapper

--- client/app/admin/components/CaseflowDistribution/StaticLeversWrapper.test.js
+++ client/app/admin/components/CaseflowDistribution/StaticLeversWrapper.test.js
@@ -6,13 +6,9 @@ import { levers } from 'test/data/adminCaseDistributionLevers';
 describe('StaticLeverWrapper', () => {
   it('renders inactive levers correctly', () => {
     const inactiveLevers = levers.filter((lever) => !lever.is_active);
-    const { getByTestId } = render(
-      <StaticLeverWrapper levers={inactiveLevers} />
-    );
-    const inactiveLever1 = getByTestId('is_active');
-    const inactiveLever2 = getByTestId('is_active');
+    const { getByText } = render(<StaticLeverWrapper />);
+    const inactiveLever = getByText(inactiveLevers[0].description);
 
-    expect(inactiveLever1).toHaveTextContent(inactiveLevers[0].value);
-    expect(inactiveLever2).toHaveTextContent(inactiveLevers[1].value);
+    expect(inactiveLever).toBeInTheDocument();
   });
 });
