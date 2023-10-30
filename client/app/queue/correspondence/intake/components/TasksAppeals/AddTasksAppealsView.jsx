062162242d25bcd075691a0c583a5ecde4cf819f
Alexander Smith <alex.smith.99@thunderyard.com>
2023-10-30 14:38:43 -0600

    Fix Misc. Issues
    - Fix incorrect use of double quotes.
    
    - Use class for border styling.

--- client/app/queue/correspondence/intake/components/TasksAppeals/AddTasksAppealsView.jsx
+++ client/app/queue/correspondence/intake/components/TasksAppeals/AddTasksAppealsView.jsx
@@ -15,29 +15,34 @@ const mailTasksRight = [
 
 export const AddTasksAppealsView = () => {
   return (
-    <div style={{ border: "2px solid #e0e1e2", marginBottom: "2rem", padding: "3rem 4rem" }}>
-      <h1 style={{ marginBottom: "10px" }}>Review Tasks & Appeals</h1>
-      <p>Review any previously complete tasks by the mail team and add new tasks for either the mail package or for linked appeals, if any.</p>
+    <div className="gray-border" style={{ marginBottom: '2rem', padding: '3rem 4rem' }}>
+      <h1 style={{ marginBottom: '10px' }}>Review Tasks & Appeals</h1>
+      <p>Review any previously complete tasks by the mail team and add new tasks for
+      either the mail package or for linked appeals, if any.</p>
       <div>
-        <h2 style={{ margin: "25px auto 15px auto" }}>Mail Tasks</h2>
-        <div style={{ border: "2px solid #e0e1e2", padding: "0rem 2rem" }}>
-          <p style={{ marginBottom: "0.5rem" }}>Select any tasks completed by the Mail team for this correspondence.</p>
-          <div style={{ display: "inline-block", marginRight: "14rem" }}>
-            {mailTasksLeft.map(function(name, index){
-              return <Checkbox
-                key={index}
-                name={name}
-                label={name}
-              />
+        <h2 style={{ margin: '25px auto 15px auto' }}>Mail Tasks</h2>
+        <div className="gray-border" style={{ padding: '0rem 2rem' }}>
+          <p style={{ marginBottom: '0.5rem' }}>Select any tasks completed by the Mail team for this correspondence.</p>
+          <div style={{ display: 'inline-block', marginRight: '14rem' }}>
+            {mailTasksLeft.map((name, index) => {
+              return (
+                <Checkbox
+                  key={index}
+                  name={name}
+                  label={name}
+                />
+              );
             })}
           </div>
-          <div style={{ display: "inline-block" }}>
-            {mailTasksRight.map(function(name, index){
-              return <Checkbox
-                key={index}
-                name={name}
-                label={name}
-              />
+          <div style={{ display: 'inline-block' }}>
+            {mailTasksRight.map((name, index) => {
+              return (
+                <Checkbox
+                  key={index}
+                  name={name}
+                  label={name}
+                />
+              )
             })}
           </div>
         </div>
