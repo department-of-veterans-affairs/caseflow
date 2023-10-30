61b6b259ea894b5b773a33f4494096f0e1329ef0
631068 <rickybmanus@gmail.com>
2023-10-30 11:12:43 -0400

    Created Distribution Content Area and updated history for implementation.

--- client/app/admin/components/CaseflowDistribution/LeverHistory.jsx
+++ client/app/admin/components/CaseflowDistribution/LeverHistory.jsx
@@ -8,20 +8,38 @@ const LeverHistory = (props) => {
   const leverHistoryStyling = css({
     borderLeft: '0',
     borderRight: '0',
-    borderBottom: '0',
+    borderTop: '0',
+    borderColor: '#d6d7d9;',
     paddingTop: '0',
     marginTop: '0',
     paddingBottom: '0',
     verticalAlign: 'text-top',
     fontFamily: 'Source Sans Pro',
     fontWeight: '400',
-    fontSize: '17px',
-    lineHeight: '1.5em/33px'
+    fontSize: '19px',
+    lineHeight: '1.3em/25px'
+  });
+  const leverHistoryTableHeaderStyling = css({
+    borderLeft: '0',
+    borderRight: '0',
+    borderTop: '0',
+    borderColor: '#d6d7d9;',
+    fontFamily: 'Source Sans Pro',
+    fontWeight: '700',
+    fontSize: '19px',
+    lineHeight: '1.3em/25px'
   });
 
   return (
     <div>
       <table>
+        <tr>
+          <th {...leverHistoryTableHeaderStyling}>Date of Last Change</th>
+          <th {...leverHistoryTableHeaderStyling}>User ID</th>
+          <th {...leverHistoryTableHeaderStyling}>Data Element Changed</th>
+          <th {...leverHistoryTableHeaderStyling}>Previous Value</th>
+          <th {...leverHistoryTableHeaderStyling}>Updated Value</th>
+        </tr>
         <tbody>{props.historyData.map((entry, index) =>
           <tr key={index}>
             <td {...leverHistoryStyling}>{entry.created_at}</td>
