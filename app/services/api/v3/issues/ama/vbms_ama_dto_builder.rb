7d3a6ebdfbd70f56410a4ccfd732603db37a7509
nkirby-va <nicholas.kirby@va.gov>
2023-10-31 15:11:38 -0400

    add description of class

--- app/services/api/v3/issues/ama/vbms_ama_dto_builder.rb
+++ app/services/api/v3/issues/ama/vbms_ama_dto_builder.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This class is responsible for building the AMA Request Issue Response.
+
 #:reek:TooManyInstanceVariables
 class Api::V3::Issues::Ama::VbmsAmaDtoBuilder
   attr_reader :hash_response
