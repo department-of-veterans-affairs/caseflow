d80f05c299fd8fe24a9576ea0d47e1d449ceacef
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-10-31 14:28:47 -0400

    🔧 Assume defaults for ActiveStorage settings

--- config/application.rb
+++ config/application.rb
@@ -106,14 +106,6 @@ module CaseflowCertification
     # Default as of 6.0: false
     Rails.application.config.action_dispatch.return_only_media_type_on_content_type = true
 
-    # Send Active Storage analysis and purge jobs to dedicated queues.
-    # Rails.application.config.active_storage.queues.analysis = :active_storage_analysis
-    # Rails.application.config.active_storage.queues.purge    = :active_storage_purge
-
-    # When assigning to a collection of attachments declared via `has_many_attached`, replace existing
-    # attachments instead of appending. Use #attach to add new attachments without replacing existing ones.
-    # Rails.application.config.active_storage.replace_on_assign_to_many = true
-
     # Use ActionMailer::MailDeliveryJob for sending parameterized and normal mail.
     #
     # The default delivery jobs (ActionMailer::Parameterized::DeliveryJob, ActionMailer::DeliveryJob),
