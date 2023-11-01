1c2ad71df3ce8f1160ff1a3516a0a2fda869286a
Calvin <Calvin.Costa@va.gov>
2023-11-01 15:16:02 -0400

    made changes based on craigs comments

--- app/jobs/ineligible_judges_job.rb
+++ app/jobs/ineligible_judges_job.rb
@@ -10,12 +10,15 @@ class IneligibleJudgesJob < CaseflowJob
   application_attr :queue
 
   def perform
+    @start_time ||= Time.zone.now
     case_distribution_ineligible_judges
 
     log_success
   rescue StandardError => error
-    log_error(self.class.name, error)
+    log_error(error)
   end
+
+  private
 
   # {Grabs both vacols and caseflow ineligible judges then merges into one list with duplicates merged if they have the same CSS_ID/SDOMAINID}
   def case_distribution_ineligible_judges
@@ -25,28 +28,16 @@ class IneligibleJudgesJob < CaseflowJob
         .flat_map do |k, v|
         next v unless k
 
-        v.reduce(&:merge).tap { |h| h.delete(:css_id) if h.key?(:sdomainid) }
+        v.reduce(&:merge)
       end
     end
   end
 
   def log_success
-    start_time ||= Time.zone.now
-    duration = time_ago_in_words(start_time)
+    duration = time_ago_in_words(@start_time)
     msg = "#{self.class.name} completed after running for #{duration}."
     Rails.logger.info(msg)
 
     slack_service.send_notification("[INFO] #{msg}", self.class.to_s) # may not need this
-  end
-
-  def log_error(class_name, err)
-    start_time ||= Time.zone.now
-    duration = time_ago_in_words(start_time)
-    msg = "#{class_name} failed after running for #{duration}. Fatal error: #{err.message}"
-    Rails.logger.info(msg)
-    Rails.logger.info(err.backtrace.join("\n"))
-
-    Raven.capture_exception(msg)
-    slack_service.send_notification("[ERROR] #{msg}", self.class.to_s)
   end
 end
