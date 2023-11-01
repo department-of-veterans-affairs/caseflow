4ff3644f22b39ba26212117d1f165289721b1717
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-11-01 13:23:02 +0000

    Lint roll

--- config/initializers/credstash.rb
+++ config/initializers/credstash.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 CredStash.configure do |config|
   # Table name will probably change
-  config.table_name = 'credential-store'
+  config.table_name = "credential-store"
   config.storage = (Rails.env.development? || Rails.env.test?) ? :dynamodb_local : :dynamodb
 end
