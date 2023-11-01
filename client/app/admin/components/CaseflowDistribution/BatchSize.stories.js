b617618e1a27cb83126b6c52a6d1dabf787481a2
SHarshain <133917878+SHarshain@users.noreply.github.com>
2023-11-01 11:04:00 -0400

    Sharsha/appeals 32338 (#19772)
    * fix the syntax in test data
    
    * added new line EOF to fix parse error
    
    * added AdminTabs story to access the InteractableLever
    
    * WIP: nested and toggle. Completed Boolean and field component scenarios
    
    * Uppercase the unit to match the wireframe
    
    * WIP
    
    * Removed the new line
    
    * refactor and move the Scenarios into seperate component
    
    * updated the test data levers
    
    * fixed Id violation
    
    * fix indentation
    
    * match with the feature branch
    
    * add styling for the headers and fixed Header violation
    
    * code cleanup
    
    * Add the label for number
    
    * refactor
    
    * Move the components into a seperate stories, added reducers, additional filters to the lever list
    
    ---------
    
    Co-authored-by: SHarshain <spoosa@razortalent.com>

---
+++ client/app/admin/components/CaseflowDistribution/BatchSize.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BatchSize from './BatchSize';
+import { levers } from 'test/data/adminCaseDistributionLevers';
+import { createStore } from 'redux';
+import leversReducer from 'app/admin/reducers/Levers/leversReducer';
+
+export default {
+  title: 'Admin/Caseflow Distribution/InteractableLevers',
+  component: BatchSize
+};
+
+const preloadedState = {
+  levers: JSON.parse(JSON.stringify(levers)),
+  initial_levers: JSON.parse(JSON.stringify(levers))
+};
+
+const leverStore = createStore(leversReducer, preloadedState);
+
+const leverList = ['lever_5', 'lever_6', 'lever_7', 'lever_8'];
+const batchSizeLevers = []
+levers.forEach((lever) => {
+  if(lever.data_type === "number" && leverList.includes(lever.item)){
+    batchSizeLevers.push(lever.item)
+  }
+});
+
+export const batchSize = (args) => (
+  <BatchSize leverList={batchSizeLevers} leverStore={leverStore} />
+);
---
+++ client/app/admin/components/CaseflowDistribution/DocketTimeGoals.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import DocketTimeGoals from './DocketTimeGoals';
+import { levers } from 'test/data/adminCaseDistributionLevers';
+import { createStore } from 'redux';
+import leversReducer from 'app/admin/reducers/Levers/leversReducer';
+
+
+export default {
+  title: 'Admin/Caseflow Distribution/InteractableLevers',
+  component: DocketTimeGoals
+};
+
+const preloadedState = {
+  levers: JSON.parse(JSON.stringify(levers)),
+  initial_levers: JSON.parse(JSON.stringify(levers))
+};
+
+const leverStore = createStore(leversReducer, preloadedState);
+
+const leverList = ['lever_10', 'lever_11', 'lever_12'];
+const docketLevers = []
+levers.forEach((lever) => {
+  if(lever.data_type === "combination" && leverList.includes(lever.item)){
+    docketLevers.push(lever.item)
+  }
+});
+
+export const docketTimeGoals = (args) => (
+    <DocketTimeGoals leverList={docketLevers} leverStore={leverStore} />
+);
