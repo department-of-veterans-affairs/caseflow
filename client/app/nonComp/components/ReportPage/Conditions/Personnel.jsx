3d9ae460dd10823b358be75adde473d148540cb9
Sean Craig <sean.craig2@va.gov>
2023-11-06 13:33:20 -0600

    Created the basic functionality of the component with dummy data

--- client/app/nonComp/components/ReportPage/Conditions/Personnel.jsx
+++ client/app/nonComp/components/ReportPage/Conditions/Personnel.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import { Controller } from 'react-hook-form';
+import PropTypes from 'prop-types';
+import SearchableDropdown from 'app/components/SearchableDropdown';
 
-export const Personnel = () => {
-  return <div>personnel test component please ignore</div>;
+export const Personnel = ({ control, register }) => {
+  const VHA_TEAM_MEMBERS = [
+    {
+      label: 'Option 1',
+      value: 'option1'
+    },
+    {
+      label: 'Option 2',
+      value: 'option2'
+    },
+    {
+      label: 'Option 3',
+      value: 'option3'
+    }
+  ];
+
+  return (
+    <>
+      <Controller
+        control={control}
+        defaultValue
+        name="personnel"
+        render={() => {
+          return (
+            <>
+              <SearchableDropdown
+                label="VHA team members"
+                name="VHA team members"
+                options={VHA_TEAM_MEMBERS}
+                inputRef={register}
+                multi
+              />
+            </>
+          );
+        }}
+      />
+    </>
+  );
+};
+
+Personnel.propTypes = {
+  control: PropTypes.object,
+  register: PropTypes.func
 };
