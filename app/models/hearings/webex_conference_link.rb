4116e6708d9503df4f2128912e435901cee2a6d4
breedbah <brandon.reed3@va.gov>
2023-11-06 10:30:03 -0500

    APPEALS-34501 update arguments

--- app/models/hearings/webex_conference_link.rb
+++ app/models/hearings/webex_conference_link.rb
@@ -14,7 +14,13 @@ class WebexConferenceLink < ConferenceLink
   def generate_conference_information
     meeting_type.update!(service_name: "webex")
 
-    conference = WebexService.new.create_conference(hearing_day)
+    conference = WebexService.new(
+      host: ENV["WEBEX_HOST_IC"],
+      aud: ENV["WEBEX_ORGANIZATION"],
+      apikey: ENV["WEBEX_BOTTOKEN"],
+      domain: ENV["WEBEX_DOMAIN_IC"],
+      api_endpoint: ENV["WEBEX_API_IC"]
+    ).create_conference(hearing_day)
 
     base_url = conference.data[:baseUrl]
 
