0c6055f875976464c3b53475bb35ebee7024f0a8
nkirby-va <nicholas.kirby@va.gov>
2023-11-06 14:27:47 -0500

    add more fields on #active? test

--- spec/models/request_issue_spec.rb
+++ spec/models/request_issue_spec.rb
@@ -1114,6 +1114,16 @@ describe RequestIssue, :all_dbs do
   context "#active?" do
     let(:request_issue) { create(:request_issue, ineligible_reason: nil, closed_at: nil, split_issue_status: nil) }
 
+    context "all individual fields are looked at" do
+      it "should result in active? as true" do
+        expect(request_issue.eligible?).to eq true
+        expect(request_issue.closed_at).to eq nil
+        expect(request_issue.split_issue_status).to eq nil
+        expect(request_issue.split_issue_status).to_not eq "in_progress"
+        expect(request_issue.active?).to eq true
+      end
+    end
+
     context "when the current request issue has no ineligible reason, not closed, and no split issue status" do
       it "should be active" do
         expect(request_issue.active?).to eq true
