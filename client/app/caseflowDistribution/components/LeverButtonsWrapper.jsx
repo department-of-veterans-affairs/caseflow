0f09134f588bf457a0a5763cc11c3543fba2883b
Michael Beard <131783726+mbeardy@users.noreply.github.com>
2023-11-08 13:24:00 -0600

    adds modal to case dist contect, adds COPY, moves save button to modal, updates leverbuttons (#19919)

--- client/app/caseflowDistribution/components/LeverButtonsWrapper.jsx
+++ client/app/caseflowDistribution/components/LeverButtonsWrapper.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LeverCancelButton, LeverSaveButton } from './LeverButtons';
+import { LeverCancelButton } from './LeverButtons';
+import { LeverSaveButton } from './LeverModal';
 import { css } from 'glamor';
 
 const saveButtonStyling = css({
