8329adda6363c2641db40bcad9c9b71c622491c2
HunJerBAH <Jeremy.Hunton@va.gov>
2023-11-08 09:44:26 -0500

    alphabetized associations on correspondence model

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -5,9 +5,9 @@
 class Correspondence < CaseflowRecord
   has_many :correspondence_documents
   has_many :correspondence_intakes
-  belongs_to :prior_correspondence, class_name: "Correspondence", optional: true
-  belongs_to :package_document_type
   belongs_to :correspondence_type
+  belongs_to :package_document_type
+  belongs_to :prior_correspondence, class_name: "Correspondence", optional: true
   belongs_to :veteran
   # has_many :appeals, through: :correspondence_appeals
   # has_many :tasks
