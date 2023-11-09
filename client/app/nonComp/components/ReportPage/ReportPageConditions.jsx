b2f2348bbc89181d68a2e50a8bf3f6aea96746c4
Brandon Dorner <brandonleedorner@gmail.com>
2023-11-09 09:53:21 -0600

    Add Facility input to Generate Task Report page

--- client/app/nonComp/components/ReportPage/ReportPageConditions.jsx
+++ client/app/nonComp/components/ReportPage/ReportPageConditions.jsx
@@ -8,12 +8,13 @@ import Button from 'app/components/Button';
 import * as yup from 'yup';
 import { daysWaitingSchema } from './Conditions/DaysWaiting';
 import { decisionReviewTypeSchema } from './Conditions/DecisionReviewType';
+import { facilitySchema } from './Conditions/Facility';
 import * as ERRORS from 'constants/REPORT_PAGE_VALIDATION_ERRORS';
 
 const conditionOptionSchemas = {
   daysWaiting: daysWaitingSchema,
   decisionReviewType: decisionReviewTypeSchema,
-  facility: yup.object(),
+  facility: facilitySchema,
   issueDisposition: yup.object(),
   issueType: yup.object(),
   personnel: personnelSchema
