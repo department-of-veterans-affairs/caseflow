3bf1df408ccd1245674e5066b5d9519f617b348b
AdamShawBAH <adam.shaw@va.gov>
2023-11-13 14:53:52 -0500

    initial commit for stuck job master scheduler
    started spec file and updated scheduler job
    
    updated spec
    
    spec edit
    
    updated scheduler job code climate
    
    rubocopped
    
    update to job perform
    
    updated spec
    
    added perfor_later on instance method
    
    swapped out ruby prof for simple time logging
    
    spec edit
    
    added modules, updated sc_dta with perform method
    
    fixed spec, modules and job
    
    code climate and spec corrections
    
    jobs fix 4 sched, fixed sj err cntr code climate
    
    updates to jobs and scheduler
    
    updates to scheduler and spec
    
    addressing code smells
    
    code smell update
    
    job fixes for code climate
    
    code climate
    
    code climate updates
    
    code climate fixes
    
    code climate fixes
    
    code climate fixes
    
    reporter formatting

--- app/jobs/claim_date_dt_fix_job.rb
+++ app/jobs/claim_date_dt_fix_job.rb
@@ -1,32 +1,40 @@
 # frozen_string_literal: true
 
 class ClaimDateDtFixJob < CaseflowJob
-  ERROR_TEXT = "ClaimDateDt"
-
-  attr_reader :stuck_job_report_service
-
   def initialize
     @stuck_job_report_service = StuckJobReportService.new
+    @start_time = nil
+    @end_time = nil
+    super
   end
 
   def perform
-    process_decision_documents
+    start_time
+
+    loop_through_and_call_process_records
+
+    end_time
+    log_processing_time
   end
 
-  def process_decision_documents
-    return if decision_docs_with_errors.blank?
+  def error_text
+    "ClaimDateDt"
+  end
 
-    stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
+  def loop_through_and_call_process_records
+    return if records_with_errors.blank?
 
-    decision_docs_with_errors.each do |single_decision_document|
+    @stuck_job_report_service.append_record_count(records_with_errors.count, error_text)
+
+    records_with_errors.each do |single_decision_document|
       next unless valid_decision_document?(single_decision_document)
 
-      process_decision_document(single_decision_document)
+      process_records(single_decision_document)
     end
 
-    stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
+    @stuck_job_report_service.append_record_count(records_with_errors.count, error_text)
 
-    stuck_job_report_service.write_log_report(ERROR_TEXT)
+    @stuck_job_report_service.write_log_report(error_text)
   end
 
   def valid_decision_document?(decision_document)
@@ -35,16 +43,28 @@ class ClaimDateDtFixJob < CaseflowJob
   end
 
   # :reek:FeatureEnvy
-  def process_decision_document(decision_document)
+  def process_records(decision_document)
     ActiveRecord::Base.transaction do
       decision_document.clear_error!
     rescue StandardError => error
       log_error(error)
-      stuck_job_report_service.append_errors(decision_document.class.name, decision_document.id, error)
+      @stuck_job_report_service.append_errors(decision_document.class.name, decision_document.id, error)
     end
   end
 
-  def decision_docs_with_errors
-    DecisionDocument.where("error ILIKE ?", "%#{ERROR_TEXT}%")
+  def records_with_errors
+    DecisionDocument.where("error ILIKE ?", "%#{error_text}%")
+  end
+
+  def log_processing_time
+    (@end_time && @start_time) ? @end_time - @start_time : 0
+  end
+
+  def start_time
+    @start_time ||= Time.zone.now
+  end
+
+  def end_time
+    @end_time ||= Time.zone.now
   end
 end
