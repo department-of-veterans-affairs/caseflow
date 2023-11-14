53d4a3d94477e286157f59a57a27a11b54e62fc1
Sean Craig <sean.craig2@va.gov>
2023-11-14 12:25:21 -0600

    Added snapshot tests and one for removing members

--- client/test/app/nonComp/Personnel.test.js
+++ client/test/app/nonComp/Personnel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { axe } from 'jest-axe';
+import { axe } from 'jest-axe';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { applyMiddleware, createStore, compose } from 'redux';
@@ -8,7 +8,7 @@ import { render, screen } from '@testing-library/react';
 import selectEvent from 'react-select-event';
 
 import ReportPage from 'app/nonComp/pages/ReportPage';
-import ApiUtil from '../../../app/util/ApiUtil';
+import ApiUtil from 'app/util/ApiUtil';
 import CombinedNonCompReducer from 'app/nonComp/reducers';
 
 const getUsers = () => {
@@ -16,7 +16,7 @@ const getUsers = () => {
     users: {
       data: [
         {
-          id: '20',
+          id: '01',
           type: 'user',
           attributes: {
             css_id: 'VHAUSER01',
@@ -25,23 +25,14 @@ const getUsers = () => {
           }
         },
         {
-          id: '21',
+          id: '02',
           type: 'user',
           attributes: {
             css_id: 'VHAUSER02',
             full_name: 'VHAUSER02',
             email: null
           }
-        },
-        {
-          id: '2000006012',
-          type: 'user',
-          attributes: {
-            css_id: 'VHAUSER03',
-            full_name: 'Susanna Bahringer DDS',
-            email: 'marilou_doyle@hahn.org'
-          }
-        },
+        }
       ]
     }
   };
@@ -68,8 +59,6 @@ describe('Personnel', () => {
     );
   };
 
-  const selectPlaceholder = 'Select...';
-
   const navigateToPersonnel = async () => {
     const addConditionBtn = screen.getByText('Add Condition');
 
@@ -85,6 +74,23 @@ describe('Personnel', () => {
     await navigateToPersonnel();
   });
 
+  const selectPlaceholder = 'Select...';
+  const teamMember1 = 'VHAUSER01';
+
+  it('passes a11y testing', async () => {
+    const { container } = setup();
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  it('renders correctly', async () => {
+    const { container } = setup();
+
+    expect(container).toMatchSnapshot();
+  });
+
   it('renders a dropdown with the correct label', async () => {
     expect(screen.getByText('VHA team members')).toBeInTheDocument();
     expect(screen.getAllByText(selectPlaceholder).length).toBe(2);
@@ -92,7 +98,6 @@ describe('Personnel', () => {
 
   it('allows to select multiple options from dropdown', async () => {
     let selectText = screen.getAllByText(selectPlaceholder);
-    const teamMember1 = 'VHAUSER01';
 
     await selectEvent.select(selectText[1], [teamMember1]);
 
@@ -103,5 +108,18 @@ describe('Personnel', () => {
 
     expect(screen.getByText(teamMember1)).toBeInTheDocument();
     expect(screen.getByText(teamMember2)).toBeInTheDocument();
+  });
+
+  it('selects an option from dropdown, then removes it and renders an error', async () => {
+    const selectText = screen.getAllByText(selectPlaceholder);
+
+    await selectEvent.select(selectText[1], [teamMember1]);
+    expect(screen.getByText(teamMember1)).toBeInTheDocument();
+
+    const clearBtn = document.querySelector('.cf-select__indicator.cf-select__clear-indicator');
+
+    userEvent.click(clearBtn);
+
+    expect(screen.queryByText(teamMember1)).not.toBeInTheDocument();
   });
 });
