0e49e2282b000d2f2a780ccc0e7c5a4945605a54
Prajwal Amatya <122557351+pamatyatake2@users.noreply.github.com>
2023-11-14 12:07:41 -0700

    Pamatya/appeals 30889 (#19878)
    * WIP: first Commit
    
    * adding few fixes
    
    * test part 1
    
    * fixing css, test and adding optional text
    
    * fixing spec failure in GHA
    
    * reverting to individual test
    
    * Renamed NonCompReportFilter to RHFControlledDropdown
    
    * making linter happy
    
    * removing unused file
    
    * fixing PR comments
    
    * adding validation
    
    * Adding none as an option
    
    * removing unused variable
    
    * Changing text of the validation message

--- client/app/nonComp/pages/ReportPage.jsx
+++ client/app/nonComp/pages/ReportPage.jsx
@@ -6,11 +6,19 @@ import Button from 'app/components/Button';
 import NonCompLayout from 'app/nonComp/components/NonCompLayout';
 import { ReportPageConditions } from '../components/ReportPage/ReportPageConditions';
 
+import RHFControlledDropdownContainer from 'app/nonComp/components/ReportPage/RHFControlledDropdown';
+import { timingSchema, TimingSpecification } from 'app/nonComp/components/ReportPage/TimingSpecification';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 import Checkbox from 'app/components/Checkbox';
 import RadioField from 'app/components/RadioField';
-import NonCompReportFilterContainer from 'app/nonComp/components/NonCompReportFilter';
 
-import REPORT_TYPE_CONSTANTS from 'constants/REPORT_TYPE_CONSTANTS';
+import {
+  REPORT_TYPE_OPTIONS,
+  RADIO_EVENT_TYPE_OPTIONS,
+  SPECTIFIC_EVENT_OPTIONS
+} from 'constants/REPORT_TYPE_CONSTANTS';
 
 const buttonInnerContainerStyle = css({
   display: 'flex',
@@ -21,6 +29,10 @@ const buttonOuterContainerStyling = css({
   display: 'flex',
   justifyContent: 'space-between',
   marginTop: '4rem',
+});
+
+const schema = yup.object().shape({
+  timing: timingSchema
 });
 
 const ReportPageButtons = ({
@@ -132,6 +144,12 @@ const RHFRadioButton = ({ options, name, control }) => {
 const ReportPage = ({ history }) => {
   const defaultFormValues = {
     reportType: '',
+    conditions: [],
+    timing: {
+      range: null,
+      startDate: '',
+      endDate: '',
+    },
     radioEventAction: 'all_events_action',
     specificEventType: {
       added_decision_date: '',
@@ -144,11 +162,15 @@ const ReportPage = ({ history }) => {
       completed_disposition: '',
       removed_issue: '',
       withdrew_issue: '',
-    },
-    conditions: []
+    }
   };
 
-  const methods = useForm({ defaultValues: { ...defaultFormValues } });
+  const methods = useForm({
+    defaultValues: { ...defaultFormValues },
+    resolver: yupResolver(schema),
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit'
+  });
 
   const { reset, watch, formState, control, handleSubmit } = methods;
 
@@ -169,10 +191,15 @@ const ReportPage = ({ history }) => {
       <h1>Generate task report</h1>
       <FormProvider {...methods}>
         <form>
-          <NonCompReportFilterContainer />
+          <RHFControlledDropdownContainer
+            header="Type of report"
+            name="reportType"
+            label="Report Type"
+            options={REPORT_TYPE_OPTIONS}
+          />
           {watchReportType === 'event_type_action' ? (
             <RHFRadioButton
-              options={REPORT_TYPE_CONSTANTS.RADIO_EVENT_TYPE_OPTIONS}
+              options={RADIO_EVENT_TYPE_OPTIONS}
               methods={methods}
               name="radioEventAction"
             />
@@ -181,13 +208,17 @@ const ReportPage = ({ history }) => {
           {watchReportType === 'event_type_action' &&
           watchRadioEventAction === 'specific_events_action' ? (
               <RHFCheckboxGroup
-                options={REPORT_TYPE_CONSTANTS.SPECTIFIC_EVENT_OPTIONS}
+                options={SPECTIFIC_EVENT_OPTIONS}
                 control={control}
                 name="specificEventType"
               />
             ) : null
           }
-          <ReportPageConditions />
+          {watchReportType === 'event_type_action' ?
+            <TimingSpecification /> :
+            null
+          }
+          {formState.isDirty ? <ReportPageConditions /> : null}
         </form>
       </FormProvider>
     </NonCompLayout>
