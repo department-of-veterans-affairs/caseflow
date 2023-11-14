16b34bfe5700113b7b4b72aebd64b41a64649d1c
AdamShawBAH <adam.shaw@va.gov>
2023-11-14 15:16:39 -0500

    updated spec

--- spec/jobs/stuck_job_scheduler_job_spec.rb
+++ spec/jobs/stuck_job_scheduler_job_spec.rb
@@ -11,24 +11,12 @@ describe StuckJobSchedulerJob, :postgres do
 
       job_instance.perform_master_stuck_job_with_profiling
     end
-
-    # it "uploads profiling logs to S3" do
-    #   allow(RubyProf).to receive(:profile).and_yield # Mock the RubyProf.profile method
-    #   allow(job_instance).to receive(:perform_master_stuck_job)
-
-    #   expect(job_instance).to receive(:upload_logs_to_s3)
-
-    #   job_instance.perform_master_stuck_job_with_profiling
-    # end
   end
 
   describe "#perform_master_stuck_job" do
-    let(:job_instance) { StuckJobSchedulerJob.new }
-
     it "executes each child job even if one fails" do
-
-       # Stub the new method for each child job to call the original implementation
-      allow(ClaimDateDtFixJob).to receive(:new).and_call_original
+      # Stub the new method for each child job to call the original implementation
+      allow(ClaimDateDtFixJob).to receive(:new).and_return("blah")
       allow(BgsShareErrorFixJob).to receive(:new).and_call_original
       allow(ClaimNotEstablishedFixJob).to receive(:new).and_call_original
       allow(NoAvailableModifiersFixJob).to receive(:new).and_call_original
@@ -46,20 +34,6 @@ describe StuckJobSchedulerJob, :postgres do
       expect_any_instance_of(DtaScCreationFailedFixJob).to receive(:perform)
 
       StuckJobSchedulerJob.new.perform_master_stuck_job
-    end
-  end
-
-
-
-
-  describe "#execute_stuck_job" do
-    let(:job_instance) { StuckJobSchedulerJob.new }
-
-    it "rescues StandardError and logs the error" do
-      expect_any_instance_of(ClaimDateDtFixJob).to receive(:perform).and_raise(StandardError)
-
-      expect { job_instance.execute_stuck_job(ClaimDateDtFixJob) }.not_to raise_error
-      expect(job_instance.send(:logs).last).to include("Error occurred:")
     end
   end
 end
