d101ec7919c06028dd1548f5f0d31182ac3e800e
Clay Sheppard <csheepy@gmail.com>
2023-11-15 12:48:39 -0600

    APPEALS-30937 - ‘Days Waiting’ condition for report (#19870)
    * wip add conditions to report page
    
    * more words for placeholder
    
    * add frontend tests for conditions
    
    * lots of cleanup and reorganize components
    
    * code climate style file fixes
    
    * fix dropdown width
    
    * fix select width a different way
    
    * un-fix select width
    
    * really unfix it
    
    * really REALLY unfix it
    
    * fix conditional for displaying condition content
    
    * fix name passing for condition components
    
    * wip commit
    
    * use better conditional rendering logic
    
    * kind of got the styling right
    
    * add frontend tests for DaysWaiting component
    
    * change class name
    
    * cherry-pick Brandon's refactor
    
    Refactor ConditionContent rendering
    
    There's two things that get done in this commit.
    
    1. Add a "middle content" section that is conditionally displayed.
    This displays the "includes" text for all but the `daysWaiting`
    condition. As a result of this some css changes were made to correctly
    display this text.
    
    2. Remove the getConditionContent() function and instead change that
    to a memoized conditionContent variable. I can't find an article on it
    but I was taught to avoid calling functions that render React
    components within jsx. I believe it caused rendering/state issues but
    idk I can't find anything on it so who knows. At the least this should
    have better performance using useMemo
    
    * css wrangling
    
    ensure the select has a margin in a better way
    change gap to grid-gap to appease outdated scss-lint?
    
    * add proptypes to DaysWaiting
    
    * update snapshot
    
    * first pass at adding yup to report page
    
    * fix state problem and add error display to number fields
    
    * add error message to report type
    
    * move days waiting options to contants file
    
    * update report type yup error message
    
    * refactor errors, add more errors
    
    * memoize condition filter options
    
    * remove unnecesary returns
    
    * refactor silly function
    
    * remove overly paranoid code
    
    * fix error focusing and relaated console warnings on react selects
    
    * resolve merge oopsies
    
    * update snapshot
    
    * refactor to bring yup schemas closer to the things they describe
    
    * remove extra brackets
    
    * fix error overlapping
    
    * alter error text for max days < min days
    
    * remove report type validation
    
    it's not needed
    
    * change wording on missing condition
    
    * add comment admitting defeat
    
    * refactor error logic
    
    * Remove punctuation from validation errors
    
    This is a change requested by BA/UX.
    
    * remove validations we don't want
    
    * fix imports
    
    * add new navigation step to days waiting test
    
    ---------
    
    Co-authored-by: Brandon Dorner <brandonleedorner@gmail.com>

--- client/app/nonComp/components/ReportPage/ReportPageConditions.jsx
+++ client/app/nonComp/components/ReportPage/ReportPageConditions.jsx
@@ -3,11 +3,36 @@ import { useFormContext, useFieldArray } from 'react-hook-form';
 import { ConditionContainer } from './ConditionContainer';
 import Button from 'app/components/Button';
 
+import * as yup from 'yup';
+import { daysWaitingSchema } from './Conditions/DaysWaiting';
+import * as ERRORS from 'constants/REPORT_PAGE_VALIDATION_ERRORS';
+
+const conditionOptionSchemas = {
+  daysWaiting: daysWaitingSchema,
+  decisionReviewType: yup.object(),
+  facility: yup.object(),
+  issueDisposition: yup.object(),
+  issueType: yup.object(),
+  personnel: yup.object()
+};
+
+export const conditionsSchema = yup.array().of(
+  yup.lazy((value) => {
+    return yup.object(
+      { condition: yup.string().typeError(ERRORS.MISSING_CONDITION).
+        oneOf(['daysWaiting', 'decisionReviewType', 'facility', 'issueDisposition', 'issueType', 'personnel']).
+        required(),
+      options: conditionOptionSchemas[value.condition]
+      });
+  })
+);
+
 export const ReportPageConditions = () => {
   const { control, watch } = useFormContext();
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'conditions',
+    defaultValues: [{ condition: '', options: {} }]
   });
 
   const watchFieldArray = watch('conditions');
@@ -23,11 +48,11 @@ export const ReportPageConditions = () => {
       <hr style={{ marginTop: '50px', marginBottom: '50px' }} />
       <h2>Conditions</h2>
       {controlledFields.map((field, index) => {
-        return <ConditionContainer key={field.id} {... { control, index, remove }} />;
+        return <ConditionContainer key={field.id} {... { control, index, remove, field }} />;
       })}
       <Button
         disabled={watchFieldArray.length >= 5}
-        onClick={() => append({ condition: '' })}>
+        onClick={() => append({ condition: '', options: {} })}>
       Add Condition</Button>
     </div>
   );
