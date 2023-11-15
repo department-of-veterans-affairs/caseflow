e1d696891c0cd650ad31bffa9ebff4b63b30d63e
isaiahsaucedo <irsaucedo5@gmail.com>
2023-11-15 07:59:07 -0600

    fixed judge ref.

--- db/seeds/appeals_held_by_inactive_judges.rb
+++ db/seeds/appeals_held_by_inactive_judges.rb
@@ -122,7 +122,7 @@ module Seeds
         # Use a hearings user so the factories don't try to create one (and sometimes fail)
         inactive_judge = User.find_by_css_id("BVADSLADER")
         # call to make inactive
-        create_ineligible_judge(judge)
+        create_ineligible_judge(inactive_judge)
         active_judge = User.find_by_css_id("BVAAABSHIRE")
         attorney = User.find_by_css_id("BVASCASPER1")
         # Set this for papertrail when creating vacols_case
