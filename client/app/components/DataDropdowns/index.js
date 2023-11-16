79a338cb8cefd8e640e1e75fd787a13c148f9616
piedram <maite.piedrayera@va.gov>
2023-11-16 10:41:44 -0500

    Delete componenet and create a new one

--- client/app/components/DataDropdowns/index.js
+++ client/app/components/DataDropdowns/index.js
@@ -4,7 +4,6 @@ Dropdowns that require data from AJAX calls
 
 export { HearingRoomDropdown } from './HearingRoom';
 export { default as JudgeDropdown } from './Judge';
-export { default as DocumentTypeCorrespondenceDropdown } from './DocumentTypeCorrespondence';
 export { default as HearingCoordinatorDropdown } from './HearingCoordinator';
 export { default as HearingDateDropdown } from './HearingDate';
 export { default as RegionalOfficeDropdown } from './RegionalOffice';
--- client/app/queue/correspondence/component/EditDocumentTypeModal.jsx
+++ client/app/queue/correspondence/component/EditDocumentTypeModal.jsx
@@ -1,73 +1,100 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import ApiUtil from '../../../util/ApiUtil';
 import { sprintf } from 'sprintf-js';
-import { DocumentTypeCorrespondenceDropdown } from '../../../components/DataDropdowns';
+import SearchableDropdown from 'app/components/SearchableDropdown';
 
 import COPY from '../../../../COPY';
 // import { onReceiveAmaTasks } from '../../queue/QueueActions';
-import { showErrorMessage, showSuccessMessage } from '../../uiReducer/uiActions';
+// import { showErrorMessage, showSuccessMessage } from '../../uiReducer/uiActions';
 
 // import ApiUtil from '../../util/ApiUtil';
 import Modal from '../../../components/Modal';
 import Button from '../../../components/Button';
 
-export const EditDocumentTypeModal = ({
-  document,
-  onCancel
-}) => {
-  const [loading] = useState(false);
-  // const [unscheduledNotes, setUnscheduledNotes] = useState(task?.unscheduledHearingNotes?.notes);
+class EditDocumentTypeModal extends React.Component {
 
-  const disable = true;
-  // eslint-disable-next-line no-empty-function
-  const submit = async () => { };
+  constructor(props) {
+    super(props);
 
-  return (
-    <Modal
-      title= {sprintf(COPY.TITLE_MODAL_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}
-      closeHandler={onCancel}
-      confirmButton={<Button disabled={disable} onClick={submit}>Save</Button>}
-      cancelButton={<Button linkStyling disabled={loading} onClick={onCancel}>Cancel</Button>}
-    >
-      <div>
-        <p>{sprintf(COPY.TEXT_MODAL_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}</p>
-      </div>
-      <div style={{ fontWeight: 'bold' }}>{sprintf(COPY.ORIGINAL_DOC_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}</div>
-      <div>{document.documentName}</div>
+    this.state = {
+      packageDocument: '',
+      dateError: '',
+      showEditModal: false,
+      packageOptions: '',
+      loading: false
+    };
+  }
 
-      <div>
-        <DocumentTypeCorrespondenceDropdown
-          name="correspondence_new_doc"
-          label= {sprintf(COPY.NEW_DOC_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}
-          value={document?.documentName}
-          // onChange={(value, label) => this.onTypeChange({ value, label })}
+  componentDidMount() {
+    setTimeout(this.getPackages, 0);
+  }
+
+  getPackages = () => {
+    ApiUtil.get('/queue/correspondence/getTypo').then((resp) => {
+      const documents = resp.body.allDocuments.map((doc) => ({
+        label: doc.name,
+        value: doc.id.toString()
+      }));
+
+      this.setState({ packageOptions: documents });
+    });
+  }
+
+  packageDocumentOnChange = (value) => {
+    this.setState({
+      packageDocument: value
+    });
+  };
+
+  render() {
+    // const [loading] = useState(false);
+    const { modalState, onCancel, document } = this.props;
+    const disable = true;
+    const { packageDocument } = this.state;
+
+    const submit = async () => {
+      return 0;
+    };
+
+    return (
+      <Modal
+        title= {sprintf(COPY.TITLE_MODAL_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}
+        closeHandler={onCancel}
+        confirmButton={<Button disabled={disable} onClick={submit}>Save</Button>}
+        cancelButton={<Button linkStyling disabled={this.loading} onClick={onCancel}>Cancel</Button>}
+      >
+        <div>
+          <p>{sprintf(COPY.TEXT_MODAL_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}</p>
+        </div>
+        <div style={{ fontWeight: 'bold' }}>
+          {sprintf(COPY.ORIGINAL_DOC_EDIT_DOCUMENT_TYPE_CORRESPONDENCE)}
+        </div>
+        <div>
+          {document.documentName}
+
+        </div>
+        <br />
+        <SearchableDropdown
+          name="issue-category"
+          label="Package document type"
+          strongLabel = {false}
+          placeholder="Select or enter..."
+          options={this.state.packageOptions}
+          value={packageDocument}
+          onChange={this.packageDocumentOnChange}
         />
-      </div>
 
-    </Modal>
-  );
-};
+      </Modal>
+    );
+
+  }
+}
 
 EditDocumentTypeModal.propTypes = {
   modalState: PropTypes.bool,
   onCancel: PropTypes.func,
   document: PropTypes.object,
-  // onChange: PropTypes.func,
-  // coordinator: PropTypes.shape({
-  //   value: PropTypes.string
-  // }),
-  // selectHearingCoordinator: PropTypes.func
 };
 
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(
-    {
-      showErrorMessage,
-      showSuccessMessage
-    },
-    dispatch
-  );
-
-export default connect(null, mapDispatchToProps)(EditDocumentTypeModal);
+export default EditDocumentTypeModal;
