a302b187ecf7c004f521ac8d0ae08674af83c59a
breedbah <brandon.reed3@va.gov>
2023-11-20 14:32:09 -0500

    APPEALS_29702 update serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -265,6 +265,10 @@ class User < CaseflowRecord # rubocop:disable Metrics/ClassLength
     roles.include?("VSO")
   end
 
+  def can_view_vlj?
+    roles.include?("RO ViewHearSched")
+  end
+
   def camo_employee?
     member_of_organization?(VhaCamo.singleton) && FeatureToggle.enabled?(:vha_predocket_workflow, user: self)
   end
