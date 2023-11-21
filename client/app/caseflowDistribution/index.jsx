e7f3235a82e1145e3243672dc44f8864158ff968
631068 <rickybmanus@gmail.com>
2023-11-21 11:52:15 -0500

    Passed props from backend to frontend, fixed index and CaseflowDistributionApp

--- client/app/caseflowDistribution/index.jsx
+++ client/app/caseflowDistribution/index.jsx
@@ -1,28 +1,90 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
 
-import CaseflowDistributionContent from './pages/CaseflowDistributionApp';
+import React from 'react';
+import ReduxBase from '../components/ReduxBase';
+import NavigationBar from '../components/NavigationBar';
+import { BrowserRouter } from 'react-router-dom';
+import PageRoute from '../components/PageRoute';
+import AppFrame from '../components/AppFrame';
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import { LOGO_COLORS } from '../constants/AppConstants';
+import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer';
+import leversReducer from './reducers/Levers/leversReducer';
+import CaseSearchLink from '../components/CaseSearchLink';
+
+import CaseflowDistributionApp from './pages/CaseflowDistributionApp';
+import { createStore } from 'redux';
 
 class CaseflowDistribution extends React.PureComponent {
-  render() {
-    console.log("this.props:", this.props)
-    const {acd_levers, acd_history} = this.props
-    return (
-      <div>
-        <h1>Hello world from CaseflowDistribution index</h1>
-        <h2>Levers</h2>
-        <div>{JSON.stringify(acd_levers)}</div>
-        <div>{JSON.stringify(acd_history)}</div>
-        <div>{JSON.stringify(acd_history)}</div>
-      </div>
-    )
-  }
-}
 
-CaseflowDistribution.propTypes = {
-  acd_levers: PropTypes.array,
-  acd_history: PropTypes.array,
-  user_is_an_acd_admin: PropTypes.bool
+  render() {
+    const preloadedState = {
+      levers: JSON.parse(JSON.stringify(this.props.acd_levers)),
+      initial_levers: JSON.parse(JSON.stringify(this.props.acd_levers)),
+      formatted_history: JSON.parse(JSON.stringify(this.props.acd_history))
+    };
+
+    const leverStore = createStore(leversReducer, preloadedState);
+    const Router = this.props.router || BrowserRouter;
+    const initialState = leversReducer.initialState;
+    const appName = 'Caseflow Distribution';
+
+    console.log('this.props:', this.props);
+    const { acd_levers, acd_history } = this.props;
+
+    return (
+      <ReduxBase initialState={initialState} reducer={leversReducer}>
+        <Router {...this.props.routerTestProps}>
+          <div>
+            <NavigationBar
+              wideApp
+              defaultUrl={
+                this.props.caseSearchHomePage || this.props.hasCaseDetailsRole ?
+                  '/search' :
+                  '/queue'
+              }
+              userDisplayName={this.props.userDisplayName}
+              dropdownUrls={this.props.dropdownUrls}
+              applicationUrls={this.props.applicationUrls}
+              logoProps={{
+                overlapColor: LOGO_COLORS.QUEUE.OVERLAP,
+                accentColor: LOGO_COLORS.QUEUE.ACCENT,
+              }}
+              rightNavElement={<CaseSearchLink />}
+              appName="Caseflow Admin"
+            >
+              <AppFrame>
+                <AppSegment filledBackground>
+                  <div>
+                    <PageRoute
+                      exact
+                      path="/acd-controls"
+                      title="CaseflowDistribution | Caseflow"
+                      component={() => {
+                        return (
+                          <CaseflowDistributionApp
+                            acd_levers={this.props.acd_levers}
+                            acd_history={this.props.acd_history}
+                            user_is_an_acd_admin = {this.props.user_is_an_acd_admin}
+                            leverStore={leverStore}
+                          />
+                        );
+                      }}
+                    />
+                  </div>
+                </AppSegment>
+              </AppFrame>
+            </NavigationBar>
+            <Footer
+              appName={appName}
+              feedbackUrl={this.props.feedbackUrl}
+              buildDate={this.props.buildDate}
+            />
+          </div>
+        </Router>
+      </ReduxBase>
+    );
+  }
 }
 
 export default CaseflowDistribution;
--- client/app/caseflowDistribution/pages/CaseflowDistributionApp.jsx
+++ client/app/caseflowDistribution/pages/CaseflowDistributionApp.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CaseflowDistributionContent from '../components/CaseflowDistributionContent';
 
-class CaseflowDistributionContent extends React.PureComponent {
+class CaseflowDistributionApp extends React.PureComponent {
   render() {
-    <div className="cf-app-segment cf-app-segment--alt">
-      <div> {/*Wrapper*/}
-        <h1>Hello World From CaseflowDistributionContent</h1>
-        {this.props.acd_levers.map((lever) => {
-          <div>{lever.item}</div>
-        })}
+
+    return (
+      <div className="cf-app-segment cf-app-segment--alt">
+        <div> {/* Wrapper*/}
+          <h1>Hello World From CaseflowDistributionContent</h1>
+          { console.log('this.props Caseflow Distribution App:', this.props)}
+          <CaseflowDistributionContent
+            levers = {this.props.acd_levers}
+            saveChanges = {[]}
+            formattedHistory={this.props.acd_history}
+            leverStore={this.props.leverStore}
+            isAdmin = {this.props.user_is_an_acd_admin}
+          />
+        </div>
       </div>
-    </div>
+    );
 
   }
 }
 
-CaseflowDistributionContent.propTypes = {
+CaseflowDistributionApp.propTypes = {
   acd_levers: PropTypes.array,
   acd_history: PropTypes.array,
   user_is_an_acd_admin: PropTypes.bool
 };
 
-export default CaseflowDistributionContent;
+export default CaseflowDistributionApp;
