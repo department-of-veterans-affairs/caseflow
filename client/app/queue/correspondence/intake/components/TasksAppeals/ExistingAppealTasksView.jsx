5a5a38463afebbf74b266d411ea8a3d4bc69cb0b
Alexander Smith <alex.smith.99@thunderyard.com>
2023-11-22 11:14:26 -0700

    Add Link and Tweak Styles
    - Update styles to have more consistent spacing between page
    elements.
    
    - Add link to appeal in "tasks for related appeals" section of the
    intake form.

--- client/app/queue/correspondence/intake/components/TasksAppeals/ExistingAppealTasksView.jsx
+++ client/app/queue/correspondence/intake/components/TasksAppeals/ExistingAppealTasksView.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ExternalLinkIcon } from '../../../../../components/icons/ExternalLinkIcon';
+import { ICON_SIZES, COLORS } from '../../../../../constants/AppConstants';
 
 export const ExistingAppealTasksView = (props) => {
   return (
     <div>
-      <strong>Tasks: Appeal #{props.appeal.docketNumber}</strong>
+      <strong>
+        Tasks:&nbsp;Appeal&nbsp;
+        <a href={`/queue/appeals/${props.appeal.externalId}`} target="_blank">
+          #{props.appeal.docketNumber}
+          <span className="cf-pdf-external-link-icon" style={{ display: "inline-block", verticalAlign: "middle" }}>
+            <ExternalLinkIcon color={COLORS.PRIMARY} size={ICON_SIZES.SMALL} />
+          </span>
+        </a>
+      </strong>
     </div>
   );
 };
