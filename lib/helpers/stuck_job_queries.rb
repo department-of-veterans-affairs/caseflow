0706ddc7034bd7d0289982aef1d30e8c5f6b6b83
AdamShawBAH <adam.shaw@va.gov>
2023-11-24 15:56:21 -0500

    fixed spec, modules and job

--- lib/helpers/stuck_job_queries.rb
+++ lib/helpers/stuck_job_queries.rb
@@ -7,15 +7,19 @@ module StuckJobQueries
   end
 
   def request_issues_updates_with_errors(error_text)
-    RequestIssuesUpdate.where("error ILIKE?", "%#{error_text}%")
+    RequestIssuesUpdate.where("error ILIKE ?", "%#{error_text}%")
   end
 
   def board_grant_effectuations_with_errors(error_text)
-    BoardGrantEffectuation.where("decision_sync_error ILIKE?", "%#{error_text}%")
+    BoardGrantEffectuation.where("decision_sync_error ILIKE ?", "%#{error_text}%")
+  end
+
+  def hlrs_with_errors
+    HigherLevelReview.where("establishment_error ILIKE ?", "%#{error_text}%")
   end
 
   def higher_level_review_with_errors(error_text)
-    HigherLevelReview.where("establishment_error ILIKE?", "%#{error_text}%")
+    HigherLevelReview.where("establishment_error ILIKE ?", "%#{error_text}%")
   end
 
   def supplemental_claims_with_errors(error_text)
--- spec/jobs/stuck_job_scheduler_job_spec.rb
+++ spec/jobs/stuck_job_scheduler_job_spec.rb
@@ -1,49 +1,68 @@
 # frozen_string_literal: true
 
+require Rails.root.join("app", "jobs", "stuck_job_scheduler_job.rb")
+require Rails.root.join("lib", "helpers", "stuck_jobs_error_counter.rb")
+
 describe StuckJobSchedulerJob, :postgres do
-  # describe "#perform_master_stuck_job_with_profiling" do
-  #   let(:job_instance) { StuckJobSchedulerJob.new }
 
-  #   it "profiles the execution of each child job" do
-  #     allow(RubyProf).to receive(:profile).and_yield # Mock the RubyProf.profile method
-
-  #     expect(job_instance).to receive(:perform_master_stuck_job)
-
-  #     job_instance.perform
-  #   end
-  # end
+  let(:stuck_job_report_service) { instance_double("StuckJobReportService") }
 
   subject { described_class.new }
 
-  describe "#perform_master_stuck_job" do
-    it 'enqueues each child job' do
+  before do
+    allow(StuckJobReportService).to receive(:new).and_return(stuck_job_report_service)
+    allow(stuck_job_report_service).to receive(:log_time).and_return(Time.now)
+  end
 
+  describe "#perform" do
+    it "executes perform_parent_stuck_job" do
+      expect(subject).to receive(:perform_parent_stuck_job)
+      expect(stuck_job_report_service).to receive(:execution_time)
+      .with(StuckJobSchedulerJob, an_instance_of(Time), an_instance_of(Time))
+      subject.perform
+    end
+  end
 
-      expect {
-        ClaimDateDtFixJob.perform_now
-      }.to have_enqueued_job(ClaimDateDtFixJob).with(no_args)
-        # allow(ClaimDateDtFixJob).to receive(:perform_later).and_return('asdas')
-        # expect(ClaimDateDtFixJob).to receive(:perform_later)
-        # allow(BgsShareErrorFixJob).to receive(:perform_later)
-        # allow(ClaimNotEstablishedFixJob).to receive(:perform_later)
-        # allow(NoAvailableModifiersFixJob).to receive(:perform_later)
-        # allow(PageRequestedByUserFixJob).to receive(:perform_later)
-        # allow(ScDtaForAppealFixJob).to receive(:perform_later)
-        # allow(DtaScCreationFailedFixJob).to receive(:perform_later)
-        # subject.perform
-# binding.pry
-        # expect(ClaimDateDtFixJob).to have_been_enqueued
-      # expect { StuckJobSchedulerJob.new.perform_master_stuck_job }.to(
-      #   have_been_enqueued(ClaimDateDtFixJob)
-        # .and have_enqueued_job(BgsShareErrorFixJob)
-        # .and have_enqueued_job(ClaimNotEstablishedFixJob)
-        # .and have_enqueued_job(NoAvailableModifiersFixJob)
-        # .and have_enqueued_job(PageRequestedByUserFixJob)
-        # # .and have_enqueued_job(ScDtaForAppealFixJob)
-        # .and have_enqueued_job(DtaScCreationFailedFixJob)
-        # Add additional stuck jobs here
-      # )
+  describe "#perform_parent_stuck_job" do
+    it "executes execute_stuck_job for each job in STUCK_JOBS_ARRAY" do
+      StuckJobSchedulerJob::STUCK_JOBS_ARRAY.each do |job_class|
+        expect(subject).to receive(:execute_stuck_job).with(job_class)
+      end
 
+      subject.perform_parent_stuck_job
+    end
+  end
+
+  describe "#execute_stuck_job" do
+    let(:stuck_job_class) { ClaimDateDtFixJob }
+
+    before do
+      allow(Rails.logger).to receive(:info)
+      allow(stuck_job_report_service).to receive(:error_count_message)
+      allow(stuck_job_report_service).to receive(:execution_time)
+      allow(stuck_job_report_service).to receive(:append_dividier)
+    end
+
+    it "executes perform_now on the child job class" do
+      initial_error_count = 1 # Set the initial error count as needed for your test
+      allow(StuckJobsErrorCounter).to receive(:errors_count_for_job).and_return(initial_error_count)
+      expect(Rails.logger).to receive(:info).with("#{stuck_job_class} started.")
+
+      # Assuming perform_now raises an error
+      allow(stuck_job_class).to receive(:perform_now).and_raise(StandardError, 'SomeError')
+
+      expect(Rails.logger).to receive(:info).with("#{stuck_job_class} failed to run with error: SomeError.")
+      subject.execute_stuck_job(stuck_job_class)
+
+    end
+
+    it "logs error count and execution time" do
+      allow(stuck_job_class).to receive(:perform_now)
+
+      expect(stuck_job_report_service).to receive(:error_count_message).twice
+      expect(stuck_job_report_service).to receive(:execution_time).once
+
+      subject.execute_stuck_job(stuck_job_class)
     end
   end
 end
