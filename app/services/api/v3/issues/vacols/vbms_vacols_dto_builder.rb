a95c5650dc737a6e0ed63e0a42b23a967eadcea4
TuckerRose <tuckerrose@tuckerroses-mbp.lan>
2023-11-29 10:16:10 -0500

    fixed more code climate issues

--- app/services/api/v3/issues/vacols/vbms_vacols_dto_builder.rb
+++ app/services/api/v3/issues/vacols/vbms_vacols_dto_builder.rb
@@ -3,19 +3,24 @@
 #:reek:TooManyInstanceVariables
 class Api::V3::Issues::Vacols::VbmsVacolsDtoBuilder
   attr_reader :hash_response, :vacols_issue_count
+  DEFAULT_PER_PAGE = RequestIssue.default_per_page
 
   def initialize(veteran, page, per_page = nil)
     @page = page
     @veteran_participant_id = veteran.participant_id&.to_s
     @veteran_file_number = veteran.file_number&.to_s
     @vacols_issue_count = total_vacols_issue_count
-    @offset = per_page || RequestIssue.default_per_page # LegacyIssues will be consistent with AMA RequestIssues
+    @offset = determine_per_page(per_page) # LegacyIssues will be consistent with AMA RequestIssues
     @vacols_issues = serialized_vacols_issues
     @total_number_of_pages = (@vacols_issue_count / @offset.to_f).ceil
     @hash_response = build_hash_response
   end
 
   private
+
+  def determine_per_page(per_page)
+    per_page || DEFAULT_PER_PAGE
+  end
 
   def total_vacols_issue_count
     vacols_veteran_file_number = LegacyAppeal.convert_file_number_to_vacols(@veteran_file_number)
