ae57c8795c625cded38ee59a6e57c4d128ea4254
Alec Kagebein <Alec.Kagebein@va.gov>
2023-11-30 09:59:15 -0600

    AlecK/APPEALS-36253 Idt requires

--- spec/controllers/idt/api/v2/appeals_controller_spec.rb
+++ spec/controllers/idt/api/v2/appeals_controller_spec.rb
@@ -1,5 +1,6 @@
 SingleCov.covered!
 # frozen_string_literal: true
+require "#{Rails.root}/app/models/idt/idt_token.rb"
 
 RSpec.describe Idt::Api::V2::AppealsController, :postgres, :all_dbs, type: :controller do
   describe "GET appeals" do
