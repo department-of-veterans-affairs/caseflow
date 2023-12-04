bab45e072f98e9bd607dcdc7e838b65261311bcd
Kevin Ma <magavakevin10@gmail.com>
2023-12-04 09:38:25 -0500

    Removed unused imports

--- client/app/queue/correspondence/pdfPreview/CorrespondencePdfToolBar.jsx
+++ client/app/queue/correspondence/pdfPreview/CorrespondencePdfToolBar.jsx
@@ -1,5 +1,4 @@
 import { css } from 'glamor';
-import { map, size, sortBy } from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from '../../../components/Link';
