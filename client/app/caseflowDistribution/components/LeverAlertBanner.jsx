fc2ae8066cf650525e098ccf6d875b534934bd89
mbeardy <michael.beard@va.gov>
2023-12-06 11:58:11 -0600

    updates alert styling

--- client/app/caseflowDistribution/components/LeverAlertBanner.jsx
+++ client/app/caseflowDistribution/components/LeverAlertBanner.jsx
@@ -3,21 +3,10 @@ import PropTypes from 'prop-types';
 import styles from 'app/styles/caseDistribution/InteractableLevers.module.scss';
 
 const LeverAlertBanner = ({ title, message, type }) => {
-  // const bannerStyle = {
-  //   backgroundColor: type === 'success' ? '#e7f4e4' : '#f44336',
-  //   top: 0,
-  //   left: 0,
-  //   width: "100%",
-  //   zIndex: 9999,
-  //   position: 'absolute',
-  //   margin: 0,
-  //   padding: 0,
-
-  // };
-
+  const leverBannerAlerts = `${styles.leverAlertBanner} ${type === 'success' ? styles.leverAlertBannerSuccess : styles.leverAlertBannerError}`;
 
   return (
-    <div className={`${styles.LeverAlertBanner}`}>
+    <div className={leverBannerAlerts}>
       <h3>{title}</h3>
       <p>{message}</p>
     </div>
