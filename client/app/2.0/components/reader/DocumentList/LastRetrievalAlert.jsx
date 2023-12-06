c3d59a9deb2fb72b22c7eea6127788a6c24f9c27
kshiflett88 <kodishiflett@gmail.com>
2023-12-06 13:23:14 -0500

    restore 2.0 LastRetrievalAlert

--- client/app/2.0/components/reader/DocumentList/LastRetrievalAlert.jsx
+++ client/app/2.0/components/reader/DocumentList/LastRetrievalAlert.jsx
@@ -4,24 +4,24 @@ import PropTypes from 'prop-types';
 
 // Local Dependencies
 import Alert from 'app/components/Alert';
-import { alertStyles } from '../../../styles/reader/DocumentList/LastRetrievalAlert';
-import { formatAlertTime } from '../../../utils/reader/format';
+import { alertStyles } from 'styles/reader/DocumentList/LastRetrievalAlert';
+import { formatAlertTime } from 'utils/reader';
 
 /**
  * Last Retrieval Alert Component
  * @param {Object} props -- VBMS Manifest Details
  */
-export const LastRetrievalAlert = ({ appeal, manifestVbmsFetchedAt }) => {
+export const LastRetrievalAlert = ({ appeal, manifestVbmsFetchedAt, manifestVvaFetchedAt }) => {
   // Get the formatted times
-  const { now, vbmsDiff } = formatAlertTime(manifestVbmsFetchedAt);
+  const { now, vbmsDiff, vvaDiff } = formatAlertTime(manifestVbmsFetchedAt, manifestVvaFetchedAt);
 
   return (
     <React.Fragment>
-      {(!manifestVbmsFetchedAt) && (
+      {(!manifestVbmsFetchedAt || !manifestVvaFetchedAt) && (
         <div {...alertStyles}>
           <Alert title="Error" type="error">
             Some of {appeal.veteran_full_name}'s documents are not available at the moment due to
-            a loading error from VBMS. As a result, you may be viewing a partial list of claims folder documents.
+            a loading error from VBMS or VVA. As a result, you may be viewing a partial list of claims folder documents.
             <br />
             <br />
             Please refresh your browser at a later point to view a complete list of documents in the claims
@@ -32,7 +32,7 @@ export const LastRetrievalAlert = ({ appeal, manifestVbmsFetchedAt }) => {
       {now && (
         <div {...alertStyles}>
           <Alert title="Warning" type="warning">
-            We last synced with VBMS {vbmsDiff} hours ago. If you'd like to check for new
+            We last synced with VBMS and VVA {Math.max(vbmsDiff, vvaDiff)} hours ago. If you'd like to check for new
             documents, refresh the page.
           </Alert>
         </div>
@@ -43,6 +43,7 @@ export const LastRetrievalAlert = ({ appeal, manifestVbmsFetchedAt }) => {
 
 LastRetrievalAlert.propTypes = {
   manifestVbmsFetchedAt: PropTypes.string,
+  manifestVvaFetchedAt: PropTypes.string,
   appeal: PropTypes.object,
 };
 
