bdc93ab3f478d1e9b3b75d6573bb0a1a6282bace
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-12-07 17:06:31 -0600

    change structure of correspondence_tasks_controller spec

--- spec/controllers/correspondence_tasks_controller_spec.rb
+++ spec/controllers/correspondence_tasks_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe CorrespondenceTasksController, :all_dbs, type: :controller do
   let(:veteran) { create(:veteran) }
   let(:correspondence) { create(:correspondence, veteran_id: veteran.id) }
   let(:current_user) { create(:user) }
+  let(:task_creation_params) { { correspondence_uuid: correspondence.uuid, correspondence_id: correspondence.id } }
 
   before do
     Fakes::Initializer.load!
@@ -13,24 +14,22 @@ RSpec.describe CorrespondenceTasksController, :all_dbs, type: :controller do
   end
 
   describe "POST #create_package_action_task" do
-    before do
-      post :create_package_action_task, params: {
-        correspondence_uuid: correspondence.uuid,
-        correspondence_id: correspondence.id,
-        type: "removePackage",
-        instructions: ["please remove task, thanks"]
-      }
-    end
+    context "RemovePackageTask creation" do
+      before do
+        task_creation_params.merge!({ type: "removePackage", instructions: ["please remove task, thanks"] })
+        post :create_package_action_task, params: task_creation_params
+      end
 
-    it "creates remove package task successfully" do
-      expect(response).to have_http_status(:ok)
-      remove_package_task = RemovePackageTask.find_by(appeal_id: correspondence.id, type: RemovePackageTask.name)
-      parent = ReviewPackageTask.find_by(appeal_id: correspondence.id, type: ReviewPackageTask.name)
-      expect(remove_package_task.appeal).to eq(correspondence)
-      expect(remove_package_task.parent_id).to eq(parent.id)
-      expect(remove_package_task.instructions).to eq(["please remove task, thanks"])
-      expect(remove_package_task.status).to eq(Constants.TASK_STATUSES.assigned)
-      expect(parent.status).to eq(Constants.TASK_STATUSES.on_hold)
+      it "creates remove package task successfully" do
+        expect(response).to have_http_status(:ok)
+        remove_package_task = RemovePackageTask.find_by(appeal_id: correspondence.id, type: RemovePackageTask.name)
+        parent = ReviewPackageTask.find_by(appeal_id: correspondence.id, type: ReviewPackageTask.name)
+        expect(remove_package_task.appeal).to eq(correspondence)
+        expect(remove_package_task.parent_id).to eq(parent.id)
+        expect(remove_package_task.instructions).to eq(["please remove task, thanks"])
+        expect(remove_package_task.status).to eq(Constants.TASK_STATUSES.assigned)
+        expect(parent.status).to eq(Constants.TASK_STATUSES.on_hold)
+      end
     end
   end
 end
