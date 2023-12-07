8eb604eceac17b959dba691b127355aac998258a
kshiflett88 <kodishiflett@gmail.com>
2023-12-07 09:21:42 -0500

    Cleaned up test case and reverted export

--- client/test/app/reader/LastRetrievalAlert.test.js
+++ client/test/app/reader/LastRetrievalAlert.test.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { LastRetrievalAlert } from '../../../app/reader/LastRetrievalAlert';
+import LastRetrievalAlert from '../../../app/reader/LastRetrievalAlert';
 import { alertMessage, warningMessage } from '../constants/LastRetrievalAlert'
 import moment from 'moment';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../../../app/reader/reducers';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import documentListReducer from '../../../app/reader/DocumentList/DocumentListReducer'
 
 describe('LastRetrievalAlert', () => {
-  const defaultProps = {
-    appeal: {
-      veteran_full_name: "John Doe"
-    },
-    manifestVbmsFetchedAt: null
+  const setup = (date = null) => {
+    const store = createStore(documentListReducer, applyMiddleware(thunk));
+
+    return render(
+      <Provider store={store}>
+        <LastRetrievalAlert
+          appeal={{veteran_full_name: "John Doe"}}
+          manifestVbmsFetchedAt={date}
+        />
+      </Provider>
+    );
   };
 
-  const renderComponent = (props = {}) => render(
-    <LastRetrievalAlert {...defaultProps} {...props} />
-  );
 
   it('does not render alert or warning message when eFolder document has been fetched and now is null', () => {
-    const component = renderComponent({manifestVbmsFetchedAt: '05/10/23 10:34am EDT -0400'});
+    setup('05/10/23 10:34am EDT -0400');
 
-    expect(screen.queryByText(alertMessage)).toBeFalsy();
+    expect(screen.queryByText(alertMessage, { exact: false })).toBeFalsy();
     expect(screen.queryByText(warningMessage)).toBeFalsy();
   });
 
   it('renders alert message when eFolder Document has not been fetched', () => {
-    const component = renderComponent();
+    setup();
 
     expect(screen.getByText(alertMessage, { exact: false })).toBeTruthy();
     expect(screen.queryByText(warningMessage)).toBeFalsy();
   });
 
   it('renders warning message when now is returned', () => {
-    Date.now = jest.fn().mockReturnValue(new Date('2023-05-11T12:40:00.000Z'));
+    Date.now = jest.fn(() => new Date("2023-05-11T12:40:00.000Z"));
 
-    const component = renderComponent({manifestVbmsFetchedAt: '05/10/23 10:34am EDT -0400'});
+    setup('05/10/23 10:34am EDT -0400');
 
-    expect(screen.getByText(warningMessage, { exact: false })).toBeTruthy();
     expect(screen.queryByText(alertMessage)).toBeFalsy();
+    expect(screen.getByText(warningMessage, { exact: false })).toBeTruthy();
   });
 });
