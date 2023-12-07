51bdd14032982b1875097975997f0389e2034c93
Zackary Borges-Rowe <borges-rowe_zackary@bah.com>
2023-12-07 13:45:00 -0700

    add more tests and add more logs to account for changes that occur on records

--- app/jobs/system_encountered_unknown_error_job.rb
+++ app/jobs/system_encountered_unknown_error_job.rb
@@ -3,14 +3,13 @@
 class SystemEncounteredUnknownErrorJob < CaseflowJob
   ERROR_TEXT = "The system has encountered an unknown error"
 
-  attr_reader :stuck_job_report_service
-
   def initialize
     @stuck_job_report_service = StuckJobReportService.new
+    super
   end
 
   def perform
-    process_decision_documents
+    loop_through_and_call_process_records
   rescue StandardError => error
     log_error(error)
     raise error
@@ -24,43 +23,54 @@ class SystemEncounteredUnknownErrorJob < CaseflowJob
     decision_document.processed_at.present? && decision_document.uploaded_to_vbms_at.present?
   end
 
-  def process_decision_documents
+  def handle_decision_document_without_epe(decision_document)
+    ActiveRecord::Base.transaction do
+      upload_document_to_vbms(decision_document)
+      @stuck_job_report_service.append_single_record(decision_document.class.name, decision_document.id)
+      decision_document.clear_error!
+    end
+  end
+
+  def loop_through_and_call_process_records
     return if decision_docs_with_errors.blank?
 
-    stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
+    @stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
 
     decision_docs_with_errors.each do |single_decision_document|
-      next unless valid_decision_document?(single_decision_document)
-
-      process_decision_document(single_decision_document)
+      if valid_decision_document?(single_decision_document)
+        process_records(single_decision_document)
+      else
+        @stuck_job_report_service.logs.push("This Decision Document with the ID of #{single_decision_document.id} is invalid.")
+      end
     end
 
-    stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
-
-    stuck_job_report_service.write_log_report(ERROR_TEXT)
+    @stuck_job_report_service.append_record_count(decision_docs_with_errors.count, ERROR_TEXT)
+    @stuck_job_report_service.write_log_report(ERROR_TEXT)
   end
 
   # :reek:FeatureEnvy
-  def process_decision_document(decision_document)
+  def process_records(decision_document)
     dd_epe = decision_document.end_product_establishments
-
     if dd_epe.empty?
-      ActiveRecord::Base.transaction do
-        upload_document_to_vbms(decision_document)
-        decision_document.clear_error!
-      end
+      handle_decision_document_without_epe(decision_document)
     elsif all_epes_valid?(dd_epe)
+      @stuck_job_report_service.append_single_record(decision_document.class.name, decision_document.id)
       decision_document.clear_error!
+    else
+      @stuck_job_report_service.logs.push("This Decision Document with the ID of #{decision_document.id} has invalid End Product Establishments.")
     end
   rescue StandardError => error
-    log_error(decision_document&.id, error)
+    log_error(error)
   end
 
   def all_epes_valid?(epes)
-    valid_epes = epes.map do |single_epe|
-      single_epe.established_at.present? && single_epe.reference_id.present?
+    epes.each do |single_epe|
+      if single_epe.established_at.present? && single_epe.reference_id.present?
+        next
+      else
+        return false
+      end
     end
-    !valid_epes.include?(false)
   end
 
   def decision_docs_with_errors
--- spec/jobs/system_encountered_unknown_error_job_spec.rb
+++ spec/jobs/system_encountered_unknown_error_job_spec.rb
@@ -3,98 +3,95 @@
 describe SystemEncounteredUnknownErrorJob, :postgres do
   let(:system_encountered_unknown_error) { "The system has encountered an unknown error" }
   let(:file_number) { "123456789" }
-  let(:appeal) do
+
+  my_time = Time.zone.now
+
+  let!(:decision_document) do
     create(
-      :appeal,
-      veteran_file_number: file_number
+      :decision_document,
+      error: system_encountered_unknown_error,
+      processed_at: 7.days.ago,
+      uploaded_to_vbms_at: 7.days.ago
     )
   end
 
+  let!(:epe) do
+    create(
+      :end_product_establishment,
+      source_id: decision_document.id,
+      source_type: "DecisionDocument",
+      established_at: 7.days.ago,
+      reference_id: decision_document.id
+    )
+  end
+
+  before do
+    create_list(
+      :decision_document,
+      3,
+      error: system_encountered_unknown_error,
+      processed_at: 7.days.ago,
+      uploaded_to_vbms_at: 7.days.ago
+    )
+
+    DecisionDocument.all.each do |decision_document|
+      if !decision_document.end_product_establishments.empty?
+        next
+      else
+        create(
+          :end_product_establishment,
+          source_id: decision_document.id,
+          source_type: "DecisionDocument",
+          established_at: 7.days.ago,
+          reference_id: decision_document.id
+        )
+      end
+    end
+  end
   subject { described_class.new }
 
   context "When there are no decision documents with errors" do
-    let!(:decision_document) do
-      create(
-        :decision_document,
-        error: nil,
-        processed_at: 7.days.ago,
-        uploaded_to_vbms_at: 7.days.ago,
-        appeal_id: appeal.id
-      )
-    end
-
-    let!(:epe) do
-      create(
-        :end_product_establishment,
-        source_id: decision_document.id,
-        source_type: "DecisionDocument",
-        established_at: nil,
-        reference_id: nil
-      )
-    end
-
     it "Does not process any decision docs" do
+      decision_document.update!(error: nil)
       subject.perform
       expect(decision_document.error).to be_nil
     end
   end
 
-  context "when there is no EPE for the decision document" do
-    let!(:decision_document) do
-      create(
-        :decision_document,
-        error: system_encountered_unknown_error,
-        processed_at: 7.days.ago,
-        uploaded_to_vbms_at: 7.days.ago,
-        appeal_id: appeal.id
-      )
+  context "When the Decision Document is not valid" do
+    it "logs the error and does not clear the error for the decision document" do
+      decision_document.update!(processed_at: nil)
+      subject.perform
+      expect(decision_document.reload.error).to eq(system_encountered_unknown_error)
     end
+  end
 
-    let!(:epe) do
-      create(
-        :end_product_establishment,
-        source_id: decision_document.id,
-        source_type: "DecisionDocument",
-        established_at: nil,
-        reference_id: nil
-      )
-    end
-
+  context "when there are no EPE's for the decision document" do
     it "performs upload document to VBMS and clears error" do
       epe.destroy
       expect(ExternalApi::VBMSService).to receive(:upload_document_to_vbms)
-
       subject.perform
       expect(decision_document.reload.error).to be_nil
+      expect(decision_document.reload.updated_at).to be_within(10.seconds).of(my_time)
     end
   end
 
   context "When there are one or more EPE's that belong to a decision document" do
-    let!(:decision_doc_with_error) do
-      create(
-        :decision_document,
-        error: system_encountered_unknown_error,
-        processed_at: 7.days.ago,
-        uploaded_to_vbms_at: 7.days.ago,
-        appeal_id: appeal.id
-      )
+    context "When the EPE's are all valid" do
+      it "clears the error field" do
+        expect(subject.decision_docs_with_errors.count).to eq(4)
+
+        subject.perform
+
+        expect(subject.decision_docs_with_errors.count).to eq(0)
+      end
     end
-
-    let!(:epe) do
-      create(
-        :end_product_establishment,
-        source_id: decision_doc_with_error.id,
-        source_type: "DecisionDocument",
-        established_at: 7.days.ago
-      )
-    end
-
-    it "clears the error field" do
-      expect(subject.decision_docs_with_errors.count).to eq(1)
-
-      subject.perform
-
-      expect(subject.decision_docs_with_errors.count).to eq(0)
+    context "When there is an invalid EPE" do
+      it "logs the error and does not clear the error for the decision document" do
+        epe.update!(reference_id: nil)
+        subject.perform
+        expect(decision_document.reload.error).to eq(system_encountered_unknown_error)
+      end
     end
   end
 end
