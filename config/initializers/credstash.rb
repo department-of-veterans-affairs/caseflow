74d15024240e7299593d577ea3303c7e125efaaa
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-12-08 12:43:42 -0500

    Update table name

--- config/initializers/credstash.rb
+++ config/initializers/credstash.rb
@@ -2,6 +2,6 @@
 
 CredStash.configure do |config|
   # Table name will probably change
-  config.table_name = "credential-store"
+  config.table_name = "appeals-rotating-tokens"
   config.storage = (Rails.env.development? || Rails.env.test?) ? :dynamodb_local : :dynamodb
 end
