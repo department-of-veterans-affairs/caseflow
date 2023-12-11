95f41547c9e3726cdb7e2622da73a86dcc54f99e
Christopher Aceves <christopher.aceves@va.gov>
2023-12-11 14:42:45 -0800

    Adding back in 2 lines but remove duplicate lines in previous commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,6 +69,8 @@ class SeedDB
     # Always run this as last one
     call_and_log_seed_step Seeds::StaticTestCaseData
     call_and_log_seed_step Seeds::StaticDispatchedAppealsTestData
+    call_and_log_seed_step Seeds::AdditionalRemandedAppeals
+    call_and_log_seed_step Seeds::AdditionalLegacyRemandedAppeals
     call_and_log_seed_step Seeds::AutoTexts
   end
 end
