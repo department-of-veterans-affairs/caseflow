7525ef08553bee7ff8e56a278f4cc0dfd69aa97a
Deepak <santhi.parakal@va.gov>
2023-12-11 10:07:44 -0500

    update the specs

--- client/test/app/queue/correspondences/modals/PackageActionModal.test.js
+++ client/test/app/queue/correspondences/modals/PackageActionModal.test.js
@@ -81,4 +81,45 @@ describe('PackageActionModal rendering', () => {
     expect(screen.getByRole('button', { name: 'Confirm request' })).not.toBeDisabled();
 
   });
+
+  it('renders the split package action modal', () => {
+
+    const firstName = veteranInformation.veteran_name.first_name.toString();
+    const lastName = veteranInformation.veteran_name.last_name.toString();
+    const fileNumber = veteranInformation.file_number.toString();
+
+    renderPackageActionModal('splitPackage');
+
+    expect(screen.getByText('Request split package')).toBeInTheDocument();
+    expect(screen.getByText('Veteran Details')).toBeInTheDocument();
+    expect(screen.getByText('Select a reason for splitting this package')).toBeInTheDocument();
+    expect(screen.getByText(correspondenceData.cmp_packet_number.toString())).toBeInTheDocument();
+    expect(screen.getByText(packageDocumentTypeData.name)).toBeInTheDocument();
+
+    expect(screen.getByText(`${firstName} ${lastName}`, { exact: false })).toBeInTheDocument();
+    expect(screen.getByText(`${fileNumber}`, { exact: false })).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: 'Confirm request' })).toBeDisabled();
+
+    const radio1 = screen.getByRole('radio', { name: 'Package contains documents related to more than one person.' });
+
+    userEvent.click(radio1);
+    expect(radio1).toBeChecked();
+
+    const radio2 = screen.getByRole('radio',
+      { name: 'Package contains documents that must be processed by multiple business lines.' });
+
+    userEvent.click(radio2);
+    expect(radio2).toBeChecked();
+
+    const otherRadioOption = screen.getByRole('radio', { name: 'Other' });
+
+    userEvent.click(otherRadioOption);
+    const textbox = screen.getByRole('textbox', { name: 'Reason for split' });
+
+    expect(textbox).toBeInTheDocument();
+    userEvent.type(textbox, 'test text for check');
+    expect(screen.getByRole('button', { name: 'Confirm request' })).not.toBeDisabled();
+
+  });
 });
