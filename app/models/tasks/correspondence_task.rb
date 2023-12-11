848f083689c592a581defa09d07a270cea540baa
piedram <maite.piedrayera@va.gov>
2023-12-11 11:18:26 -0500

    update status

--- app/models/tasks/correspondence_task.rb
+++ app/models/tasks/correspondence_task.rb
@@ -3,35 +3,41 @@ class CorrespondenceTask < Task
   before_create :verify_org_task_unique
   validate :status_is_valid_on_create, on: :create
   validate :assignee_status_is_valid_on_create, on: :create
+  class << self
 
-  def verify_org_task_unique
-    if Task.where(
-      appeal_id: appeal_id,
-      appeal_type: appeal_type,
-      type: type
-    ).any?
-      fail(
-        Caseflow::Error::DuplicateOrgTask,
-        task_type: self.class.name,
-        assignee_type: assigned_to.class.name
-      )
-    end
-  end
-
-  private
-  def status_is_valid_on_create
-    if status != Constants.TASK_STATUSES.assigned
-      fail Caseflow::Error::InvalidStatusOnTaskCreate, task_type: type
+    def verify_org_task_unique
+      binding.pry
+      if Task.where(
+        appeal_id: appeal_id,
+        appeal_type: appeal_type,
+        type: type
+      ).any?
+        fail(
+          Caseflow::Error::DuplicateOrgTask,
+          task_type: self.class.name,
+          assignee_type: assigned_to.class.name
+        )
+      end
     end
 
-    true
-  end
+    private
+    def status_is_valid_on_create
+      binding.pry
+      if self.type == "ReviewPackageTask"
+        status = Constants.TASK_STATUSES.unassigned
+      elsif status != Constants.TASK_STATUSES.assigned
+        fail Caseflow::Error::InvalidStatusOnTaskCreate, task_type: type
+      end
 
-  def assignee_status_is_valid_on_create
-    if parent&.child_must_have_active_assignee? && assigned_to.is_a?(User) && !assigned_to.active?
-      fail Caseflow::Error::InvalidAssigneeStatusOnTaskCreate, assignee: assigned_to
+      true
     end
 
-    true
+    def assignee_status_is_valid_on_create
+      if parent&.child_must_have_active_assignee? && assigned_to.is_a?(User) && !assigned_to.active?
+        fail Caseflow::Error::InvalidAssigneeStatusOnTaskCreate, assignee: assigned_to
+      end
+
+      true
+    end
   end
 end
