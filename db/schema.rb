64445aa2e81c56f489ec3de645083b8a9d69a117
cacevesva <109166981+cacevesva@users.noreply.github.com>
2023-12-12 09:57:20 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1845,7 +1845,6 @@ ActiveRecord::Schema.define(version: 2023_12_05_171256) do
     t.text "instructions", default: [], array: true
     t.integer "parent_id"
     t.datetime "placed_on_hold_at"
-    t.jsonb "previous", default: [], array: true
     t.datetime "started_at"
     t.string "status", default: "assigned"
     t.string "type"
