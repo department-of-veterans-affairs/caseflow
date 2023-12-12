e2f2814a7f909fecf412016c9c95dac0ac002623
631966 <amy.detwiler@va.gov>
2023-12-12 15:28:56 -0500

    took out seeds from merge conflict with master whose files don't exist

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,8 +66,6 @@ class SeedDB
     # Always run this as last one
     call_and_log_seed_step Seeds::StaticTestCaseData
     call_and_log_seed_step Seeds::StaticDispatchedAppealsTestData
-    call_and_log_seed_step Seeds::AdditionalRemandedAppeals
-    call_and_log_seed_step Seeds::AdditionalLegacyRemandedAppeals
     call_and_log_seed_step Seeds::CaseDistributionLevers
   end
 end
