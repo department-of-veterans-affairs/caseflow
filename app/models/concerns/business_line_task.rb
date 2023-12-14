a1e034d8da09f5cb6e60669d6a5f647ca600c67c
Sean Craig <sean.craig2@va.gov>
2023-12-14 14:04:45 -0600

    addressing first comment from review

--- app/models/concerns/business_line_task.rb
+++ app/models/concerns/business_line_task.rb
@@ -3,10 +3,10 @@
 module BusinessLineTask
   extend ActiveSupport::Concern
 
-  def complete_with_payload!(_decision_issue_params, _decision_date, current_user)
+  def complete_with_payload!(_decision_issue_params, _decision_date, user)
     return false unless validate_task
 
-    update!(status: Constants.TASK_STATUSES.completed, closed_at: Time.zone.now, completed_by: current_user)
+    update!(status: Constants.TASK_STATUSES.completed, closed_at: Time.zone.now, completed_by: user)
   end
 
   private
