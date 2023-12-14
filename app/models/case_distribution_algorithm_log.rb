98def47dfb91633f36326c676d908d7cf9642231
SHarshain <spoosa@razortalent.com>
2023-12-14 09:40:20 -0500

    APPEALS-34623. Added model associations and spec

--- app/models/case_distribution_algorithm_log.rb
+++ app/models/case_distribution_algorithm_log.rb
@@ -1,3 +1,8 @@
 class CaseDistributionAlgorithmLog < ApplicationRecord
-	scope :past_year, -> {where(created_at: (Time.zone.now - 1.year)...Time.zone.now)}
+  belongs_to :starting_distribution, class_name: "Distribution"
+  belongs_to :ending_distribution, class_name: "Distribution"
+  belongs_to :starting_case, class_name: "DistributedCase"
+  belongs_to :ending_case, class_name: "DistributedCase"
+
+  scope :past_year, -> {where(created_at: (Time.zone.now - 1.year)...Time.zone.now)}
 end
