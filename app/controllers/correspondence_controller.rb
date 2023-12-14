6e44fefd534808bace965ec0ea336a3f1f911570
divyadasari-va <divya.dasari@va.gov>
2023-12-14 10:41:35 -0700

    Page not found fix

--- app/controllers/correspondence_controller.rb
+++ app/controllers/correspondence_controller.rb
@@ -195,7 +195,16 @@ class CorrespondenceController < ApplicationController
   end
 
   def veteran_by_correspondence
-    @veteran_by_correspondence ||= Veteran.find(correspondence&.veteran_id)
+    return unless correspondence&.veteran_id
+
+    @veteran_by_correspondence ||= begin
+      veteran = Veteran.find_by(id: correspondence.veteran_id)
+      if veteran.nil?
+        # Handle the case where the veteran is not found
+        puts "Veteran not found for ID: #{correspondence.veteran_id}"
+      end
+      veteran
+    end
   end
 
   def veterans_with_correspondences
