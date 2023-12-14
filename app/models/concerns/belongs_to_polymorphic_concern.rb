cbf2991752ff8c868223387a5e37ef2afa2d6794
Jeremy Croteau <jeremiah.croteau@va.gov>
2023-12-14 13:30:34 -0500

    ⚗️ Experiment: Remove `includes` from polymorphic assocation scope block

--- app/models/concerns/belongs_to_polymorphic_concern.rb
+++ app/models/concerns/belongs_to_polymorphic_concern.rb
@@ -26,27 +26,14 @@ module BelongsToPolymorphicConcern
       # Define self_table_name here so it can be used in the belongs_to lambda, where `self.table_name` is different
       self_table_name = table_name
 
-      # The use of `includes(self_table_name)` relies on an association being defined in the other class (eg, Appeal).
-      # The association may be singular (`has_one`) or plural (`has_many`),
-      # which is reflected in `inverse_association_name`.
-      inverse_association_name = inverse_association_name(type_name)
-      # DecisionIssue does not have an inverse association with LegacyAppeal
-      return unless inverse_association_name
-
       belongs_to method_name,
-                 -> { includes(inverse_association_name).where(self_table_name => { type_column => type_name }) },
+                 -> { where(self_table_name => { type_column => type_name }) },
                  class_name: type_name, foreign_key: id_column.to_s, optional: true
 
       define_method method_name do
         # `super()` will call the method created by the `belongs_to` above
         super() if send(type_column) == type_name
       end
-    end
-
-    def inverse_association_name(type_name)
-      klass = type_name.constantize
-      # Ignore polymorphic associations, which don't have a `klass` and will raise and error
-      klass.reflections.values.reject(&:polymorphic?).detect { |assoc| assoc.klass == self }&.name
     end
   end
 end
