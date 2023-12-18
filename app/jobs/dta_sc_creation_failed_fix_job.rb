672057065aaf43c080074febc0b0e8ebe41cbdea
AdamShawBAH <adam.shaw@va.gov>
2023-12-18 12:15:05 -0500

    initial commit for dta for appeals

--- app/jobs/dta_sc_creation_failed_fix_job.rb
+++ app/jobs/dta_sc_creation_failed_fix_job.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class DtaScCreationFailedFixJob < CaseflowJob
-  include MasterSchedulerInterface
-
   def initialize
     @stuck_job_report_service = StuckJobReportService.new
     @start_time = nil
     @end_time = nil
     super
+  end
+
+  def error_text
+    "DTA SC creation failed"
   end
 
   # :reek:FeatureEnvy
@@ -27,17 +29,59 @@ class DtaScCreationFailedFixJob < CaseflowJob
     log_processing_time
   end
 
-  # :reek:FeatureEnvy
   def loop_through_and_call_process_records
-    records_with_errors.each do |hlr|
-      next unless SupplementalClaim.find_by(
-        decision_review_remanded_id: hlr.id,
-        decision_review_remanded_type: "HigherLevelReview"
-      )
+    process_records_for_hlrs_with_errors if hlrs_with_errors.present?
+    process_records_for_appeals_with_errors if appeals_with_errors.present?
+  end
+
+  # Methods for remdiating HLRs with errors
+
+  def process_records_for_hlrs_with_errors
+    hlrs_with_errors.each do |hlr|
+      next unless remanded_hlr?(hlr)
 
       @stuck_job_report_service.append_single_record(hlr.class.name, hlr.id)
-
       process_records(hlr)
+    end
+  end
+
+  def remanded_hlr?(hlr)
+    SupplementalClaim.find_by(
+      decision_review_remanded_id: hlr.id,
+      decision_review_remanded_type: "HigherLevelReview"
+    )
+  end
+
+  # Methods for remdiating Appeals with errors
+
+  def process_records_for_appeals_with_errors
+    appeals_with_errors.each do |appeal|
+      if valid_appeal?(appeal)
+        @stuck_job_report_service.append_single_record(appeal.class.name, appeal.id)
+        process_records(appeal)
+      end
+    end
+  end
+
+  def valid_appeal?(appeal)
+    return false unless appeal.established_at
+
+    set_default_payee_code(appeal.claimant) if appeal.claimant.payee_code.nil?
+  end
+
+  def set_default_payee_code(claimant)
+    claimant.update!(payee_code: claimant_type_to_payee_code(claimant.type))
+  end
+
+  def claimant_type_to_payee_code(type)
+    case type
+    when "VeteranClaimant"
+      "00"
+    when "DependentClaimant"
+      "10"
+    else
+      raise ArgumentError, "Unsupported claimant type: #{type}"
+      # Or add other Claimant types if necessary
     end
   end
 
@@ -51,12 +95,21 @@ class DtaScCreationFailedFixJob < CaseflowJob
     end
   end
 
-  def error_text
-    "DTA SC creation failed"
+  def hlrs_with_errors
+    HigherLevelReview.where("establishment_error ILIKE ?", "%#{error_text}%")
+  end
+
+  def appeals_with_errors
+    Appeal.where("establishment_error ILIKE ?", "%#{error_text}%")
   end
 
   def records_with_errors
-    HigherLevelReview.where("establishment_error ILIKE ?", "%#{error_text}%")
+    hlrs = hlrs_with_errors
+    appeals = appeals_with_errors
+
+    all_records_with_errors = hlrs + appeals
+
+    all_records_with_errors
   end
 
   def log_processing_time
@@ -71,3 +124,4 @@ class DtaScCreationFailedFixJob < CaseflowJob
     @end_time ||= Time.zone.now
   end
 end
+
--- spec/jobs/dta_sc_creation_failed_fix_job_spec.rb
+++ spec/jobs/dta_sc_creation_failed_fix_job_spec.rb
@@ -7,11 +7,13 @@ describe DtaScCreationFailedFixJob, :postgres do
 
   let!(:hlr) { create(:higher_level_review, veteran_file_number: veteran_file_number, establishment_error: dta_error) }
   let!(:sc) { create(:supplemental_claim, veteran_file_number: veteran_file_number, decision_review_remanded: hlr) }
+  let!(:appeal) { create(:appeal, establishment_error: dta_error, veteran: veteran) }
+  let!(:claimant) { create(:claimant, decision_review_id: appeal.id, decision_review_type: "Appeal") }
 
-  it_behaves_like "a Master Scheduler serializable object", DtaScCreationFailedFixJob
+  # it_behaves_like "a Master Scheduler serializable object", DtaScCreationFailedFixJob
 
+  subject { described_class.new }
   context "#dta_sc_creation_failed_fix" do
-    subject { described_class.new }
 
     context "When SC has decision_review_remanded_id and decision_review_remanded_type" do
       it "clears the error field on related HLR" do
@@ -35,6 +37,39 @@ describe DtaScCreationFailedFixJob, :postgres do
           subject.perform
           expect(hlr.reload.establishment_error).to eql(dta_error)
         end
+      end
+    end
+
+    context "When the appeal is established and the claimant has a payee code" do
+      it "clears the error on the appeal" do
+        subject.perform
+        expect(appeal.reload.establishment_error).to be_nil
+      end
+    end
+
+    context "When the appeal is established, but the payee_code is nil" do
+      it "updates the payee_code and clears the error" do
+        appeal.claimant.update(payee_code: nil)
+        subject.perform
+        expect(appeal.reload.establishment_error).to be_nil
+        expect(appeal.reload.claimant.payee_code).to eq("00")
+      end
+
+      describe "when the claimant_type is a DependentClaimant" do
+        it "updates the payee_code to 10 and clears the error" do
+          appeal.claimant.update(type: "DependentClaimant")
+          subject.perform
+          expect(appeal.reload.establishment_error).to be_nil
+          expect(appeal.reload.claimant.payee_code).to eq("10")
+        end
+      end
+    end
+
+    context "When the appeal is not established" do
+      it "does not clear the error" do
+        appeal.update(established_at: nil)
+        subject.perform
+        expect(appeal.reload.establishment_error).to eq(dta_error)
       end
     end
   end
