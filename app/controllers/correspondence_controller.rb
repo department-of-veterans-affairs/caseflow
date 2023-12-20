a961163a0841a912c711f5b4160de7e264cf87b0
William Medders <medders_william@bah.com>
2023-12-20 10:32:18 -0600

    User can link correspondences to appeals without adding tasks

--- app/controllers/correspondence_controller.rb
+++ app/controllers/correspondence_controller.rb
@@ -160,6 +160,7 @@ class CorrespondenceController < ApplicationController
     ActiveRecord::Base.transaction do
       begin
         create_correspondence_relations(correspondence_id)
+        link_appeals_to_correspondence(correspondence_id)
         add_tasks_to_related_appeals(correspondence_id)
         complete_waived_evidence_submission_tasks(correspondence_id)
       rescue ActiveRecord::RecordInvalid
@@ -210,12 +211,18 @@ class CorrespondenceController < ApplicationController
     end
   end
 
+  def link_appeals_to_correspondence(correspondence_id)
+    params[:related_appeal_ids].map do |appeal_id|
+      CorrespondencesAppeal.find_or_create_by(correspondence_id: correspondence_id, appeal_id: appeal_id)
+    end
+  end
+
   def complete_waived_evidence_submission_tasks(correspondence_id)
     params[:waived_evidence_submission_window_tasks]&.map do |task|
       evidence_submission_window_task = EvidenceSubmissionWindowTask.find(task[:task_id])
       instructions = evidence_submission_window_task.instructions
-      appeal = evidence_submission_window_task&.appeal
-      CorrespondencesAppeal.find_or_create_by(correspondence_id: correspondence_id, appeal_id: appeal.id)
+      # appeal = evidence_submission_window_task&.appeal
+      # CorrespondencesAppeal.find_or_create_by(correspondence_id: correspondence_id, appeal_id: appeal.id)
       evidence_submission_window_task.when_timer_ends
       evidence_submission_window_task.update!(instructions: (instructions << task[:waive_reason]))
     end
@@ -224,7 +231,7 @@ class CorrespondenceController < ApplicationController
   def add_tasks_to_related_appeals(correspondence_id)
     params[:tasks_related_to_appeal]&.map do |data|
       appeal = Appeal.find(data[:appeal_id])
-      CorrespondencesAppeal.find_or_create_by(correspondence_id: correspondence_id, appeal_id: appeal.id)
+      # CorrespondencesAppeal.find_or_create_by(correspondence_id: correspondence_id, appeal_id: appeal.id)
       data[:klass].constantize.create_from_params(
         {
           appeal: appeal,
