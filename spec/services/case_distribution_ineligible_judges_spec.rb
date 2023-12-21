ac2f32d0aa1c631fa520d4669f9e3faa8636b901
Craig Reese <109101548+craigrva@users.noreply.github.com>
2023-12-21 12:11:03 -0600

    Craig/rework acd (#20170)
    * update SATTYID sequence to return unique values
    
    * some test fixes
    
    * fix more tests
    
    * fix more tests
    
    * fix query and test
    
    * more test fixes
    
    * rework job and service to remove AC 4 and 5
    
    * enable local cache and add comments to test file
    
    * fix list size error, disable statistics
    
    * added error handling for sattyid cache
    
    * fix broken specs
    
    ---------
    
    Co-authored-by: calvincostaBAH <108481161+calvincostaBAH@users.noreply.github.com>
    Co-authored-by: Calvin <Calvin.Costa@va.gov>
    Co-authored-by: kristeja <112115264+kristeja@users.noreply.github.com>
    Co-authored-by: kristeja <krishnateja.golla@va.gov>

--- spec/services/case_distribution_ineligible_judges_spec.rb
+++ spec/services/case_distribution_ineligible_judges_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe CaseDistributionIneligibleJudges, :postgres do
-  describe ".ineligible_vacols_judges" do
+  describe "ineligible vacols judges" do
     context "when inactive staff exists" do
       let!(:active_non_judge_staff) { create(:staff) }
       let!(:inactive_non_judge_staff) { create(:staff, :inactive) }
@@ -9,8 +9,10 @@ describe CaseDistributionIneligibleJudges, :postgres do
       let!(:inactive_judge_staff) { create(:staff, :judge_role, :inactive) }
       let!(:non_judge_with_sattyid) { create(:staff, sattyid: 9999) }
       let!(:attorney_judge_staff) { create(:staff, :attorney_judge_role) }
+      # The associated caseflow user record needs to be ACTIVE with an inactive staff record
+      let!(:inactive_judge_caseflow_record) { create(:user, :judge, css_id: inactive_judge_staff.sdomainid) }
 
-      it "returns ineligible vacols judges" do
+      it ".ineligible_vacols_judges returns correct records" do
         result = described_class.ineligible_vacols_judges
 
         expect(result).to contain_exactly(
@@ -23,61 +25,57 @@ describe CaseDistributionIneligibleJudges, :postgres do
         )
         expect(result.size).to eq(2)
       end
+
+      it ".vacols_judges_with_caseflow_records combines records correctly" do
+        result = described_class.vacols_judges_with_caseflow_records
+
+        expect(result).to contain_exactly(
+          { sattyid: inactive_judge_staff.sattyid,
+            sdomainid: inactive_judge_staff.sdomainid,
+            svlj: inactive_judge_staff.svlj,
+            id: inactive_judge_caseflow_record.id,
+            css_id: inactive_judge_caseflow_record.css_id },
+          { sattyid: non_judge_with_sattyid.sattyid,
+            sdomainid: non_judge_with_sattyid.sdomainid,
+            svlj: non_judge_with_sattyid.svlj }
+        )
+        expect(result.size).to eq(2)
+      end
     end
   end
 
-  describe ".ineligible_caseflow_judges" do
+  describe "ineligible caseflow judges" do
     context "when caseflow users are exists" do
       let!(:inactive_user) { create(:user, :inactive) }
-      let!(:active_user) { create(:user, :judge) }
+      let!(:inactive_user_no_vacols_record) { create(:user, :inactive) }
+      let!(:active_user) { create(:user) }
+      # The associated staff record needs to be ACTIVE with an inactive caseflow user record
+      let!(:inactive_user_vacols_record) { create(:staff, user: inactive_user )}
 
-      it "returns ineligible caseflow judges but not active judges" do
+      it ".ineligible_caseflow_judges returns correct records" do
         result = described_class.ineligible_caseflow_judges
 
-        expect(result.size).to eq(1)
-        expect(result.first[:css_id]).to eq(inactive_user.css_id)
-        expect(result.first[:css_id]).not_to eq(active_user.css_id)
+        expect(result).to contain_exactly(
+          { id: inactive_user.id, css_id: inactive_user.css_id },
+          { id: inactive_user_no_vacols_record.id, css_id: inactive_user_no_vacols_record.css_id },
+        )
+        expect(result).not_to include({ id: active_user.id, css_id: active_user.css_id })
+        expect(result.size).to eq(2)
       end
-    end
 
-    context "when caseflow users are tied to judge team organizations" do
-      let!(:active_judge_user) { create(:user, :judge) }
-      let!(:inactive_judge_user) { create(:user, :judge) }
-      before { inactive_judge_user.organizations.last.update(status: "inactive") }
+      it ".caseflow_judges_with_vacols_records combines records correctly" do
+        result = described_class.caseflow_judges_with_vacols_records
 
-      it "returns ineligible judge team users but not eligible judge team users" do
-        result = described_class.ineligible_caseflow_judges
-
-        expect(result.size).to eq(1)
-        expect(result.first[:css_id]).to eq(inactive_judge_user.css_id)
-        expect(result.first[:css_id]).not_to eq(active_judge_user.css_id)
-      end
-    end
-  end
-
-  describe ".non_admin_users_of_judge_teams" do
-    context "when Judge Team is admin" do
-      let!(:user) { create(:user) }
-      let!(:organization) { create(:judge_team) }
-      let!(:organizations_user) { create(:organizations_user, :admin, organization: organization, user: user) }
-
-      it "returns an empty array" do
-        result = described_class.non_admin_users_of_judge_teams
-
-        expect(result).to eq([])
-      end
-    end
-
-    context "when Judge Team is non admin" do
-      let!(:user) { create(:user) }
-      let!(:organization) { create(:judge_team) }
-      let!(:organizations_user) { create(:organizations_user, organization: organization, user: user) }
-
-      it "returns an array" do
-        result = described_class.non_admin_users_of_judge_teams
-
-        expect(result.size).to eq(1)
-        expect(result).to eq([user.id])
+        expect(result).to contain_exactly(
+          { id: inactive_user.id,
+            css_id: inactive_user.css_id,
+            sattyid: inactive_user_vacols_record.sattyid,
+            sdomainid: inactive_user_vacols_record.sdomainid,
+            svlj: inactive_user_vacols_record.svlj },
+          { id: inactive_user_no_vacols_record.id,
+            css_id: inactive_user_no_vacols_record.css_id }
+        )
+        expect(result.size).to eq(2)
       end
     end
   end
