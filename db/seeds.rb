e9dd1ae48a94ab1565f6b60449d9d21ab9762477
Christopher Detlef <>
2023-12-22 14:26:39 -0500

    Remove distribution data seeds so they don't interfere with lever testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,7 +56,7 @@ class SeedDB
     call_and_log_seed_step Seeds::VeteransHealthAdministration
     call_and_log_seed_step Seeds::MTV
     call_and_log_seed_step Seeds::Education
-    call_and_log_seed_step Seeds::PriorityDistributions
+    # call_and_log_seed_step Seeds::PriorityDistributions
     call_and_log_seed_step Seeds::CaseDitributionTestData
     call_and_log_seed_step Seeds::TestCaseData
     call_and_log_seed_step Seeds::Notifications
