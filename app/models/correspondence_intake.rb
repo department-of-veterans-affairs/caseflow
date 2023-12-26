a9423ee0e163290fff49b559fd0104ba957e4d61
HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
2023-12-26 11:03:56 -0500

    Revert "Feature/appeals 27311"

--- app/models/correspondence_intake.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CorrespondenceIntake < ApplicationRecord
-  belongs_to :correspondence
-  belongs_to :user
-
-  validates_presence_of :correspondence_id
-  validates_presence_of :user_id
-end
--- app/models/correspondences_appeal.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class CorrespondencesAppeal < ApplicationRecord
-  belongs_to :correspondence
-  belongs_to :appeal
-end
