763c9f300310ecb094fa3836fbedc1453e3e545e
HunJerBAH <Jeremy.Hunton@va.gov>
2023-12-27 17:21:36 -0500

    created place holder lever actions

---
+++ client/app/caseflowDistribution/reducers/Levers/leversActions.js
@@ -0,0 +1,61 @@
+import { ACTIONS } from './leversActionTypes';
+
+export const loadLevers = (levers) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.LOAD_LEVERS,
+      payload: {
+        levers
+      }
+    });
+  };
+
+export const updateLeverValue = (leverValue) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.UPDATE_LEVER_VALUE,
+      payload: {
+        leverValue
+      }
+    });
+  };
+
+export const formatLeverHistory = (leverHistory) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.FORMAT_LEVER_HISTORY,
+      payload: {
+        leverHistory
+      }
+    });
+  };
+
+export const saveLever = (lever) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.SAVE_LEVERS,
+      payload: {
+        lever
+      }
+    });
+  };
+
+export const reverLevers = (lever) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.REVERT_LEVERS,
+      payload: {
+        lever
+      }
+    });
+  };
+
+export const successBanner = (banner) =>
+  (dispatch) => {
+    dispatch({
+      type: ACTIONS.SHOW_SUCCESS_BANNER,
+      payload: {
+        banner
+      }
+    });
+  };
