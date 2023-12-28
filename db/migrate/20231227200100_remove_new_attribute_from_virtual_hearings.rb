8ed0d1c5dca756c56c26d76c522d31d0de859957
breedbah <brandon.reed3@va.gov>
2023-12-28 07:46:04 -0500

    removed the accidental new attribute

--- db/migrate/20231227200100_remove_new_attribute_from_virtual_hearings.rb
+++ db/migrate/20231227200100_remove_new_attribute_from_virtual_hearings.rb
@@ -1,5 +1,11 @@
 class RemoveNewAttributeFromVirtualHearings < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :virtual_hearings, :new_attribute, :string
+  def up
+    VirtualHearing.ignored_columns = ["new_attribute"]
+    safety_assured { remove_column :virtual_hearings, :new_attribute, :string }
+  end
+
+  def down
+    add_column :virtual_hearings, :new_attribute, :string
+    VirtualHearing.reset_ignored_columns
   end
 end
