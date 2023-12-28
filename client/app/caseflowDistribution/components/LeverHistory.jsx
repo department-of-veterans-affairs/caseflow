ad40a84702ff4223d873b96a32fb0c8e4cba7b60
631966 <amy.detwiler@va.gov>
2023-12-28 00:35:53 -0500

    APPEALS-34224 something went terribly wrong

--- client/app/caseflowDistribution/components/LeverHistory.jsx
+++ client/app/caseflowDistribution/components/LeverHistory.jsx
@@ -1,59 +1,55 @@
-/* eslint-disable func-style */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from 'app/styles/caseDistribution/LeverHistory.module.scss';
 
 const LeverHistory = (props) => {
   const { leverStore } = props;
-  const historyEntries = leverStore.getState().formatted_history
   const [historyData, setHistoryData] = React.useState([]);
-  const [history, setHistory] = useState([]);
-  const [historySize, setHistorySize] = useState(0);
-
 
   function formatHistoryData(historyEntries) {
-
     if (!historyEntries) return [];
+    const formatted = historyEntries.reduce((accumulator, entry) => {
+      const existingEntry = accumulator.find(
+        (item) => item.created_at === entry.created_at && item.user_id === entry.user_id
+      );
 
-   const formatted = historyEntries.reduce((accumulator, entry) => {
-    // Find an existing entry in the accumulator with the same timestamp and user
-    const existingEntry = accumulator.find(
-      (item) => item.created_at === entry.created_at && item.user_id === entry.user_id
-    );
-    // If an existing entry is found, update its values
-    if (existingEntry) {
-      existingEntry.titles.push(entry.title);
-      existingEntry.previous_values.push(entry.previous_value);
-      existingEntry.updated_values.push(entry.update_value);
-      existingEntry.units.push(entry.unit || 'null');
-    } else {
-      // If no existing entry is found, create a new entry
-      const newEntry = {
-        created_at: entry.created_at,
-        user_id: entry.user_id,
-        user_name: entry.user_name,
-        titles: [entry.title],
-        previous_values: [entry.previous_value],
-        updated_values: [entry.update_value],
-        units: [entry.unit || 'null'],
-      };
-      accumulator.push(newEntry);
-    }
-    return accumulator;
-  }, []);
-  return formatted
-}
+      if (existingEntry) {
+        existingEntry.titles.push(entry.title);
+        existingEntry.previous_values.push(entry.previous_value);
+        existingEntry.updated_values.push(entry.update_value);
+        existingEntry.units.push(entry.unit || 'null');
+      } else {
+        const newEntry = {
+          created_at: entry.created_at,
+          user_id: entry.user_id,
+          user_name: entry.user_name,
+          titles: [entry.title],
+          previous_values: [entry.previous_value],
+          updated_values: [entry.update_value],
+          units: [entry.unit || 'null'],
+        };
+        accumulator.push(newEntry);
+      }
+      return accumulator;
+    }, []);
+    return formatted;
+  }
 
   useEffect(() => {
-    // Format the historyData based on the changes in formatted_history
-    const formattedEntries = formatHistoryData(historyEntries);
-    // Update the component state with the formatted history data
-    setHistoryData(formattedEntries);
-  }, [historyEntries]);
+    const unsubscribe = leverStore.subscribe(() => {
+      const historyEntries = leverStore.getState().formatted_history;
+      const formattedEntries = formatHistoryData(historyEntries);
+      setHistoryData(formattedEntries);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [leverStore]);
 
   return (
     <div>
-      <table key={historySize}>
+      <table key={historyData.length}>
         <tbody>
           <tr>
             <th className={styles.leverHistoryTableHeaderStyling}>Date of Last Change</th>
@@ -63,35 +59,35 @@ const LeverHistory = (props) => {
             <th className={styles.leverHistoryTableHeaderStyling}>Updated Value</th>
           </tr>
         </tbody>
-        <tbody key={historySize}>{historyData && historyData.map((entry, index) =>
-          <tr key={`${historySize}-${index}`}>
-            <td className={styles.historyTableStyling}>{entry.created_at}</td>
-            <td className={styles.historyTableStyling}>{entry.user}</td>
-            <td className={styles.historyTableStyling}>
-              <ol>
-                {entry.titles.map((title) => {
-                  return <li key={title}>{title}</li>;
-                })
-                }
-              </ol>
-            </td>
-            <td className={styles.historyTableStyling}>
-              <ol>
-                {entry.previous_values.map((previousValue, idx) => {
-                  return <li key={previousValue}>{previousValue}{' '}{entry.units[idx]}</li>;
-                })
-                }
-              </ol>
-            </td>
-            <td className={styles.historyTableStyling}>
-              <ol>
-                {entry.updated_values.map((updatedValue, idx) => {
-                  return <li key={updatedValue}>{updatedValue}{' '}{entry.units[idx]}</li>;
-                })
-                }
-              </ol>
-            </td>
-          </tr>)}
+        <tbody key={historyData.length}>
+          {historyData &&
+            historyData.map((entry, index) => (
+              <tr key={`${historyData.length}-${index}`}>
+                <td className={styles.historyTableStyling}>{entry.created_at}</td>
+                <td className={styles.historyTableStyling}>{entry.user_name}</td>
+                <td className={styles.historyTableStyling}>
+                  <ol>
+                    {entry.titles.map((title) => (
+                      <li key={title}>{title}</li>
+                    ))}
+                  </ol>
+                </td>
+                <td className={styles.historyTableStyling}>
+                  <ol>
+                    {entry.previous_values.map((previousValue, idx) => (
+                      <li key={previousValue}>{previousValue}{' '}{entry.units[idx]}</li>
+                    ))}
+                  </ol>
+                </td>
+                <td className={styles.historyTableStyling}>
+                  <ol>
+                    {entry.updated_values.map((updatedValue, idx) => (
+                      <li key={updatedValue}>{updatedValue}{' '}{entry.units[idx]}</li>
+                    ))}
+                  </ol>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
