7717673f826baf6fa8d2d24ccd71285bc1e1b90b
Matthew Roth <roth_matthew@bah.com>
2024-01-03 09:14:47 -0500

    Update 20240103134452_update_case_distribution_audit_lever_entry.rb

--- db/migrate/20240103134452_update_case_distribution_audit_lever_entry.rb
+++ db/migrate/20240103134452_update_case_distribution_audit_lever_entry.rb
@@ -3,7 +3,6 @@ class UpdateCaseDistributionAuditLeverEntry < ActiveRecord::Migration[5.2]
     safety_assured do
       remove_column :case_distribution_audit_lever_entries, :user_name, :string
       remove_column :case_distribution_audit_lever_entries, :title, :string
-      change_column_null :case_distribution_audit_lever_entries, :case_distribution_lever_id, false
       change_column_null :case_distribution_audit_lever_entries, :user_id, false
     end
   end
