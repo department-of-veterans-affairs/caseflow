b29bdd423a9bd4cd008b6e1ab423de571b17fc4f
William Medders <medders_william@bah.com>
2024-01-04 12:06:50 -0600

    Added seed_helper module to cavc dashboard data

--- db/seeds/cavc_dashboard_data.rb
+++ db/seeds/cavc_dashboard_data.rb
@@ -3,6 +3,8 @@
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 module Seeds
   class CavcDashboardData < Base
+    include SeedHelpers
+
     def initialize
       initial_id_values
     end
@@ -32,16 +34,6 @@ module Seeds
       while CavcRemand.find_by(cavc_docket_number: format("%<y>2d-%<n>4d", y: @year, n: @cavc_docket_number_last_four))
         @cavc_docket_number_last_four += 100
       end
-    end
-
-    def create_veteran(options = {})
-      @file_number += 1
-      @participant_id += 1
-      params = {
-        file_number: format("%<n>09d", n: @file_number),
-        participant_id: format("%<n>09d", n: @participant_id)
-      }
-      create(:veteran, params.merge(options))
     end
 
     def create_cavc_dashboards_with_blank_dispositions
