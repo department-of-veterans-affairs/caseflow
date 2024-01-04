39e2ab1c17b198c6385e8fc198f6a99e331699ca
William Medders <medders_william@bah.com>
2024-01-04 12:03:51 -0600

    Added seed_helper module to static test data

--- db/seeds/static_test_data.rb
+++ db/seeds/static_test_data.rb
@@ -6,6 +6,8 @@
 
 module Seeds
   class StaticTestCaseData < Base
+    include SeedHelpers
+
     def initialize
       initial_id_values
     end
@@ -26,16 +28,6 @@ module Seeds
         @file_number += 2000
         @participant_id += 2000
       end
-    end
-
-    def create_veteran(options = {})
-      @file_number += 1
-      @participant_id += 1
-      params = {
-        file_number: format("%<n>09d", n: @file_number),
-        participant_id: format("%<n>09d", n: @participant_id)
-      }
-      create(:veteran, params.merge(options))
     end
 
     def cases_for_timely_calculations_on_das
