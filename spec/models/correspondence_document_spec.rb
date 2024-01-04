86e22ae35e990a9663942fdd6df96dec6c9fefef
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2024-01-04 15:42:48 -0600

    update spec

--- spec/models/correspondence_document_spec.rb
+++ spec/models/correspondence_document_spec.rb
@@ -14,22 +14,9 @@ describe CorrespondenceDocument, :postgres do
 
   before do
     FeatureToggle.enable!(:correspondence_queue)
-    S3Service.store_file(
-      "#{CorrespondenceDocument::S3_BUCKET_NAME}/#{document.uuid}",
-      "lib/pdfs/KnockKnockJokes.pdf", :filepath
-    )
   end
 
-  it "pdf_name returns name of pdf" do
-    expect(document.pdf_name).to eq("#{document.uuid}.pdf")
-  end
-
-  it "s3_location returns path to doc in s3" do
-    expect(document.s3_location).to eq("#{CorrespondenceDocument::S3_BUCKET_NAME}/#{document.uuid}")
-  end
-
-  it "pdf_location stores file fetched from S3" do
-    document.pdf_location
-    expect(document.output_location).to eq(File.join(Rails.root, "tmp", "pdfs", document.pdf_name).to_s)
+  it "pdf_location gets file path of static document" do
+    expect(document.pdf_location).to eq(File.join(Rails.root, "lib", "pdfs", "KnockKnockJokes.pdf").to_s)
   end
 end
