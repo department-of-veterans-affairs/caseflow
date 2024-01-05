4c78154472bf83f30b5b2bb5e6daceddce7ceb85
AdamShawBAH <adam.shaw@va.gov>
2024-01-05 13:46:00 -0500

    rubcopped

--- app/jobs/dta_sc_creation_failed_fix_job.rb
+++ app/jobs/dta_sc_creation_failed_fix_job.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative "../../lib/helpers/master_scheduler_interface.rb"
 class DtaScCreationFailedFixJob < CaseflowJob
   include MasterSchedulerInterface
