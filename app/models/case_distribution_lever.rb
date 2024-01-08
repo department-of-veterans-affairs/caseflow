8a40ea10dddcb42db2dcc57806ec8793807cb585
Matthew Roth <roth_matthew@bah.com>
2024-01-08 12:57:34 -0500

    move lever updating logic into the model

--- app/models/case_distribution_lever.rb
+++ app/models/case_distribution_lever.rb
@@ -23,12 +23,6 @@ class CaseDistributionLever < ApplicationRecord
     #{Constants.DISTRIBUTION.nod_adjustment}
   )
 
-  def update_levers(lever_list)
-    lever_list.each do |updated_lever|
-      updated_lever.save!
-    end
-  end
-
   def distribution_value
     if self.data_type == Constants.ACD_LEVERS.radio
       option = self.options.detect{|opt| opt['item'] == self.value}
@@ -40,83 +34,80 @@ class CaseDistributionLever < ApplicationRecord
 
   class << self
     def find_integer_lever(lever)
-      # binding.pry
       return 0 unless INTEGER_LEVERS.include?(lever)
       CaseDistributionLever.find_by_item(lever).try(:distribution_value).to_i
     end
 
     def find_float_lever(lever)
-      # binding.pry
       return 0 unless FLOAT_LEVERS.include?(lever)
       CaseDistributionLever.find_by_item(lever).try(:distribution_value).to_f
     end
-  end
 
-  def update_acd_levers(current_levers)
-    grouped_levers = current_levers.index_by { |lever| lever["id"] }
-    prevous_levers = CaseDistributionLever.where(id: grouped_levers.keys).index_by { |lever| lever["id"] }
-    errors = []
+    def update_acd_levers(current_levers, current_user)
+      grouped_levers = current_levers.index_by { |lever| lever["id"] }
+      previous_levers = CaseDistributionLever.where(id: grouped_levers.keys).index_by { |lever| lever["id"] }
+      errors = []
 
-    ActiveRecord::Base.transaction do
-      levers = CaseDistributionLever.update(grouped_levers.keys, grouped_levers.values)
-
-      unless levers.all?(&:changed?)
-        errors = levers.select(&:invalid?).map { |lever| "Lever :#{lever.title} - #{lever.errors.full_messages}" }.join("<br/>")
-      end
-    end
-
-    errors.concat(add_audit_lever_entries(previous_levers, levers))
-  end
-
-  def add_audit_lever_entries(previous_levers, levers)
-    entries = []
-    levers.each do |lever|
-      previous_lever = previous_levers[lever.id]
-      entries.push ({
-        user: current_user,
-        case_distribution_lever: lever,
-        user_name: current_user.css_id,
-        title: lever.title,
-        previous_value: entry_data["original_value"],
-        update_value: entry_data["current_value"],
-        created_at: entry_data["created_at"]
-
-      })
-    end
-
-    begin
       ActiveRecord::Base.transaction do
-        CaseDistributionAuditLeverEntry.create(entries)
+        levers = CaseDistributionLever.update(grouped_levers.keys, grouped_levers.values)
+
+        unless levers.all?(&:changed?)
+          errors = levers.select(&:invalid?).map { |lever| "Lever :#{lever.title} - #{lever.errors.full_messages}" }.join("<br/>")
+        end
       end
-    rescue StandardError => error
-      return [error]
+
+      errors.concat(add_audit_lever_entries(previous_levers, levers, current_user))
     end
 
-    []
-  end
-
-  def format_audit_lever_entries(audit_lever_entries_data)
-    formatted_audit_lever_entries = []
-
-    begin
-      audit_lever_entries_data.each do |entry_data|
-        lever = CaseDistributionLever.find_by_title entry_data["lever_title"]
-
-        formatted_audit_lever_entries.push ({
+    def add_audit_lever_entries(previous_levers, levers, current_user)
+      entries = []
+      levers.each do |lever|
+        previous_lever = previous_levers[lever.id]
+        entries.push ({
           user: current_user,
           case_distribution_lever: lever,
           user_name: current_user.css_id,
           title: lever.title,
-          previous_value: entry_data["original_value"],
-          update_value: entry_data["current_value"],
-          created_at: entry_data["created_at"]
+          previous_value: previous_lever.value,
+          update_value: lever.value
 
         })
       end
-    rescue Exception => error
-      return error
+
+      begin
+        ActiveRecord::Base.transaction do
+          CaseDistributionAuditLeverEntry.create(entries)
+        end
+      rescue StandardError => error
+        return [error]
+      end
+
+      []
     end
 
-    formatted_audit_lever_entries
+    def format_audit_lever_entries(audit_lever_entries_data)
+      formatted_audit_lever_entries = []
+
+      begin
+        audit_lever_entries_data.each do |entry_data|
+          lever = CaseDistributionLever.find_by_title entry_data["lever_title"]
+
+          formatted_audit_lever_entries.push ({
+            user: current_user,
+            case_distribution_lever: lever,
+            user_name: current_user.css_id,
+            title: lever.title,
+            previous_value: entry_data["original_value"],
+            update_value: entry_data["current_value"],
+            created_at: entry_data["created_at"]
+
+          })
+        end
+      rescue Exception => error
+        return error
+      end
+
+      formatted_audit_lever_entries
+    end
   end
 end
