853ca06c6dfdf4ce0cdfe09cb124f6eb971cabe5
Alex Smith <alex.smith.99@thunderyard.com>
2024-01-08 17:16:36 -0700

    Use FeatureToggle for non-prod Uploads (#20437)
    * Use FeatureToggle for non-prod Uploads
    
    - When not in production, use a new feature toggle to control the
    success/failure of the ClaimEvidenceService document upload for
    correspondences vs.  the old, hard-coded boolean that was being
    used.
    
    - Add spec to test upload behavior.
    
    * Rearrange Code
    
    Put primary case first in if statement for better readability.
    
    * Fix Correspondence Bugs and Issues
    
    - Move eFolder doc uploading to a new service and update code to
    use it.
    
    - Refactor CorrespondenceIntakeProcessor based on missed
    requirements so that it correctly uses the result of doc uploads
    to control flow.
    
    - Add specs throughout and fix some Rubocop violations.
    
    * Improve Error Reporting Messages
    
    * Fix Issues Causing Spec Failures
    
    - Restore removed correspondence_document methods.
    
    - Add mocks for doc upload to intake form submit specs.
    
    * Fix check for veteran ID
    
    * Fix correspondence_document_spec
    
    * Fix efolder uploader Spec Failures
    
    - Update spec based on new check for existing failed task.
    
    - Enhance existing specs.
    
    ---------
    
    Co-authored-by: Jim Foley <james.foley4@va.gov>

--- spec/support/correspondence_helpers.rb
+++ spec/support/correspondence_helpers.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 module CorrespondenceHelpers
+  def current_user
+    @current_user ||= create(:user, roles: ["Mail Team"])
+  end
+
   def setup_access
     FeatureToggle.enable!(:correspondence_queue)
-    user = create(:user, roles: ["Mail Team"])
-    MailTeam.singleton.add_user(user)
-    User.authenticate!(user: user)
+    MailTeam.singleton.add_user(current_user)
+    User.authenticate!(user: current_user)
+
+    mock_doc_uploader = instance_double(CorrespondenceDocumentsEfolderUploader)
+
+    allow(CorrespondenceDocumentsEfolderUploader).to receive(:new).and_return(mock_doc_uploader)
+    allow(mock_doc_uploader).to receive(:upload_documents_to_claim_evidence).and_return(true)
   end
 
   def visit_intake_form_with_correspondence_load
@@ -14,6 +22,8 @@ module CorrespondenceHelpers
     54.times do
       create(
         :correspondence,
+        :with_correspondence_intake_task,
+        assigned_to: current_user,
         veteran_id: veteran.id,
         uuid: SecureRandom.uuid,
         va_date_of_receipt: Time.zone.local(2023, 1, 1)
@@ -40,6 +50,8 @@ module CorrespondenceHelpers
     3.times do
       create(
         :correspondence,
+        :with_correspondence_intake_task,
+        assigned_to: current_user,
         veteran_id: veteran.id,
         uuid: SecureRandom.uuid,
         va_date_of_receipt: Time.zone.local(2023, 1, 1)
@@ -59,6 +71,8 @@ module CorrespondenceHelpers
     3.times do
       create(
         :correspondence,
+        :with_correspondence_intake_task,
+        assigned_to: current_user,
         veteran_id: veteran.id,
         uuid: SecureRandom.uuid,
         va_date_of_receipt: Time.zone.local(2023, 1, 1)
@@ -76,6 +90,8 @@ module CorrespondenceHelpers
     veteran = create(:veteran, last_name: "Smith", file_number: "12345678")
     create(
       :correspondence,
+      :with_correspondence_intake_task,
+      assigned_to: current_user,
       veteran_id: veteran.id,
       uuid: SecureRandom.uuid,
       va_date_of_receipt: Time.zone.local(2023, 1, 1)
@@ -103,7 +119,14 @@ module CorrespondenceHelpers
   def active_evidence_submissions_tasks
     setup_access
     veteran = create(:veteran, last_name: "Smith", file_number: "12345678")
-    create(:correspondence, veteran_id: veteran.id, uuid: SecureRandom.uuid, va_date_of_receipt: Time.zone.local(2023, 1, 1))
+    create(
+      :correspondence,
+      :with_correspondence_intake_task,
+      assigned_to: current_user,
+      veteran_id: veteran.id,
+      uuid: SecureRandom.uuid,
+      va_date_of_receipt: Time.zone.local(2023, 1, 1)
+    )
     2.times do
       appeal = create(:appeal, veteran_file_number: veteran.file_number)
       InitialTasksFactory.new(appeal).create_root_and_sub_tasks!
@@ -117,6 +140,8 @@ module CorrespondenceHelpers
     veteran = create(:veteran, last_name: "Smith", file_number: "12345678")
     create(
       :correspondence,
+      :with_correspondence_intake_task,
+      assigned_to: current_user,
       veteran_id: veteran.id,
       uuid: SecureRandom.uuid,
       va_date_of_receipt: Time.zone.local(2023, 1, 1)
