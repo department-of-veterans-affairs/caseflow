22da17164cdc53a2da93af3f686f3e94e7a252b7
mbeardy <michael.beard@va.gov>
2024-01-09 11:01:10 -0600

    "removes linting changes from reducer"

--- client/app/caseDistribution/reducers/levers/leversReducer.js
+++ client/app/caseDistribution/reducers/levers/leversReducer.js
@@ -1,13 +1,15 @@
-import { ACTIONS } from '../levers/leversActionTypes';
+import {ACTIONS } from '../levers/leversActionTypes';
 import { update } from '../../../util/ReducerUtil';
+import { Constant } from '../../constants'
 
+// saveChangesActivated, editedLevers, formattedHistory, changesOccurred should be deleted. Refactor where it is used before deletion
 export const initialState = {
   saveChangesActivated: false,
   editedLevers: [],
   levers: {},
   backendLevers: [],
   formattedHistory: {},
-  historyList: {},
+  historyList: [],
   changesOccurred: false,
   showSuccessBanner: false,
 };
@@ -15,46 +17,92 @@ export const initialState = {
 const leversReducer = (state = initialState, action = {}) => {
   switch (action.type) {
 
-  case ACTIONS.INITIAL_LOAD:
-    return update(state, {
-      levers: {
-        $set: action.payload.levers
-      },
-      backendLevers: {
-        $set: action.payload.levers
-      }
-    });
+    case ACTIONS.INITIAL_LOAD:
+      // all of the logic to append currentValue and backendValue needs to be moved to LOAD_LEVERS
+      // remove all calls to INITIAL_LOAD and use LOAD_LEVERS instead
+      const leverGroups = Object.keys(action.payload.levers);
+
+      const leversWithValues = () => {
+        return leverGroups.reduce((updatedLevers, leverGroup) => {
+          updatedLevers[leverGroup] = action.payload.levers[leverGroup].map(lever => {
+            let value = null;
+            const group = lever.lever_group
+            if (group === Constant.AFFINITY) {
+              value = lever.options.find(option => option.item === lever.value).value;
+            } else if (group === Constant.DOCKET_DISTRIBUTION_PRIOR) {
+              value = `${lever.is_toggle_active}-${lever.value}`;
+             }
+            else {
+              value = lever.value;
+            }
+
+            // Add backendValue and currentValue attributes
+            return {
+              ...lever,
+              backendValue: value,
+              currentValue: value,
+            };
+          });
+
+          return updatedLevers;
+        }, {});
+      };
+
+      const updatedLeversWithValues = leversWithValues();
+
+      return update(state, {
+        levers: {
+          $set: updatedLeversWithValues,
+        },
+        backendLevers: {
+          $set: updatedLeversWithValues,
+        },
+      });
 
   case ACTIONS.LOAD_LEVERS:
+    // const leverGroups = Object.keys(action.payload.levers)
+    // const levers = leverGroups.forEach(leverGroup => leverGroup.forEach(lever => {
+    //   let value = null;
+    //    switch(lever.lever_group) {
+    //     case Constant.AFFINITY:
+    //       value = lever.options[lever.value].value
+    //       return
+    //     default:
+    //       value = lever.value
+    //       return
+    //   }
+    //   lever.backendValue = value;
+    //   lever.currentValue = value;
+    // }))
     return update(state, {
       levers: {
         $set: action.payload.levers
       }
     });
 
-  case ACTIONS.UPDATE_LEVER:
-    const { leverGroup, leverItem, value, usesOption, usesToggle, toggleValue } = action.payload;
-    const updateLeverValue = (lever) => {
-      if (usesOption) {
-        return { ...lever, option: [{ value }] }; // Update the option value
-      } else if (usesToggle) {
-        return { ...lever, value, is_toggle_active: toggleValue }; // Update both value and is_toggle_active
-      }
-
-      return { ...lever, value }; // Update only the value
-
-    };
-    const updatedLever = state.levers[leverGroup].map((lever) =>
-      lever.item === leverItem ? updateLeverValue(lever) : lever
-    );
-
-    return {
-      ...state,
-      levers: {
-        ...state.levers,
-        [leverGroup]: updatedLever,
-      },
-    };
+    case ACTIONS.UPDATE_LEVER:
+      const { leverGroup, leverItem, value, optionValue, toggleValue } = action.payload;
+      const updateLeverValue = (lever) => {
+        if (leverGroup === Constant.AFFINITY) {
+          const selectedOption = lever.options.find(option => option.item ===value)
+          selectedOption.value = optionValue
+          return { ...lever, currentValue: optionValue, value };
+        } else if (leverGroup === Constant.DOCKET_DISTRIBUTION_PRIOR) {
+          return { ...lever, value, currentValue: `${toggleValue}-${value}`, is_toggle_active: toggleValue };
+        } else {
+          return { ...lever, value, currentValue: value };
+        }
+      };
+      const updatedLever = state.levers[leverGroup].map((lever) =>
+        lever.item === leverItem ? updateLeverValue(lever) : lever
+      );
+      return {
+        ...state,
+        levers: {
+          ...state.levers,
+          [leverGroup]: updatedLever,
+        },
+      };
   // needs to be reworked; remove comment when done
   case ACTIONS.FORMAT_LEVER_HISTORY:
     return {
@@ -93,6 +141,8 @@ const leversReducer = (state = initialState, action = {}) => {
       showSuccessBanner: false
     };
 
+
+
   default:
     return state;
   }
@@ -120,7 +170,7 @@ export const formatLeverHistory = (lever_history_list) => {
 
 // this should probably be moved into the action in leversAction.js
 export const updateLevers = (current_levers, updated_lever, hasValueChanged) => {
-  const leverIndex = current_levers.findIndex((lever) => lever.item === updated_lever.item);
+  const leverIndex = current_levers.findIndex((lever) => lever.item == updated_lever.item);
 
   if (leverIndex !== -1) {
 
