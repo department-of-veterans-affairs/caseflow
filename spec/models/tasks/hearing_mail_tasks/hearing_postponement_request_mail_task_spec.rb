94e83b84dbf14dfb6e1e3feed31e1eb009bfe413
Jeff Marks <jeff.t.marks@gmail.com>
2024-01-10 14:28:01 -0500

    hotfix/APPEALS-37679 Update HPR spec for HPR completion before and after hearing

--- spec/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task_spec.rb
+++ spec/models/tasks/hearing_mail_tasks/hearing_postponement_request_mail_task_spec.rb
@@ -31,6 +31,198 @@ describe HearingPostponementRequestMailTask, :postgres do
     end
   end
 
+  describe "#update_from_params" do
+    let(:user) { create(:user) }
+    let(:parent_hpr) { create(:hearing_postponement_request_mail_task, :postponement_request_with_scheduled_hearing) }
+    let!(:hpr) { parent_hpr.children.first }
+    let(:params) do
+      {
+        status: "completed",
+        business_payloads: {
+          values: {
+            disposition: "postponed",
+            after_disposition_update: { action: "schedule_later" },
+            date_of_ruling: "2024-01-10",
+            instructions: "test",
+            granted: true
+          }
+        }
+      }
+    end
+    let(:appeal) { hpr.appeal }
+    let(:disposition_task) { appeal.tasks.of_type(AssignHearingDispositionTask.name).first }
+    let(:disposition_task_status) { "assigned" }
+    let(:schedule_task) { appeal.tasks.of_type(ScheduleHearingTask.name).first }
+    let(:hearing_task) { hpr.hearing_task }
+    let(:hearing) { disposition_task.hearing }
+
+    subject { hpr.update_from_params(params, user) }
+
+    shared_examples "whether granted or denied" do
+      it "completes HPR parent and child task" do
+        expect(parent_hpr.status).to eq("on_hold")
+        expect(hpr.status).to eq("assigned")
+        subject
+        expect(parent_hpr.status).to eq("completed")
+        expect(hpr.status).to eq("completed")
+      end
+    end
+
+    shared_examples "whether reschedule or schedule later" do
+      include_examples "whether granted or denied"
+
+      it "cancels and recreates HearingTask" do
+        original_hearing_task = hearing_task
+        expect(original_hearing_task.status).to eq("on_hold")
+        expect { subject }.to change(HearingTask, :count).by(1)
+        expect(original_hearing_task.reload.status).to eq("cancelled")
+      end
+    end
+
+    context "postponement granted with schedule later action" do
+      context "for appeal with open AssignHearingDispositionTask" do
+        shared_examples "whether hearing upcoming or in past" do
+          include_examples "whether reschedule or schedule later"
+
+          it "cancels AssignHearingDispositionTask and creates new ScheduleHearingTask" do
+            expect(disposition_task.status).to eq(disposition_task_status)
+            expect { subject }.to change(ScheduleHearingTask, :count).by(1)
+            expect(disposition_task.reload.status).to eq("cancelled")
+          end
+        end
+
+        context "and upcoming hearing" do
+          include_examples "whether hearing upcoming or in past"
+
+          it "updates previous hearing's disposition to postponed" do
+            expect(hearing.disposition).to be_nil
+            subject
+            expect(hearing.reload.disposition).to eq("postponed")
+          end
+        end
+
+        context "and hearing marked no show and NoShowHearingTask created" do
+          let(:disposition_task_status) { "on_hold" }
+
+          before do
+            hearing.update(disposition: "no_show")
+            disposition_task.no_show!
+            allow(hpr).to receive(:hearing_scheduled_and_awaiting_disposition?).and_return(false)
+          end
+
+          include_examples "whether hearing upcoming or in past"
+
+          it "doesn't update previous hearing's disposition" do
+            expect(hearing.disposition).to eq("no_show")
+            subject
+            expect(hearing.reload.disposition).to eq("no_show")
+          end
+        end
+      end
+
+      context "for appeal with active ScheduleHearingTask" do
+        let(:parent_hpr) do
+          create(:hearing_postponement_request_mail_task, :postponement_request_with_unscheduled_hearing)
+        end
+
+        include_examples "whether reschedule or schedule later"
+
+        it "cancels ScheduleHearingTask and creates new ScheduleHearingTask" do
+          original_schedule_task = schedule_task
+          expect(original_schedule_task.status).to eq("assigned")
+          expect { subject }.to change(ScheduleHearingTask, :count).by(1)
+          expect(original_schedule_task.reload.status).to eq("cancelled")
+        end
+      end
+    end
+
+    context "postponement granted with reschedule immediately action" do
+      let(:virtual_hearing_day) do
+        create(
+          :hearing_day,
+          request_type: HearingDay::REQUEST_TYPES[:virtual],
+          scheduled_for: Time.zone.today + 160.days,
+          regional_office: "RO39"
+        )
+      end
+      let(:params) do
+        {
+          status: "cancelled",
+          business_payloads: {
+            values: {
+              disposition: "postponed",
+              after_disposition_update: {
+                action: "reschedule",
+                new_hearing_attrs: {
+                  email_recipients: {
+                    appellant_tz: "America/Los_Angeles",
+                    representative_tz: "America/Los_Angeles",
+                    appellant_email: "test@test.com"
+                  },
+                  scheduled_time_string: "09:00",
+                  hearing_day_id: virtual_hearing_day.id,
+                  hearing_location: nil,
+                  virtual_hearing_attributes: {
+                    appellant_tz: "America/Los_Angeles",
+                    representative_tz: "America/Los_Angeles",
+                    appellant_email: "test@test.com"
+                  }
+                }
+              },
+              date_of_ruling: "2024-01-10",
+              instructions: "test",
+              granted: true
+            }
+          }
+        }
+      end
+
+      before do
+        RequestStore[:current_user] = user
+      end
+
+      context "for appeal with open AssignHearingDispositionTask" do
+        shared_examples "whether hearing upcoming or in past" do
+          include_examples "whether reschedule or schedule later"
+
+          it "cancels AssignHearingDispositionTask and creates new AssignHearingDispositionTask" do
+            expect(disposition_task.status).to eq(disposition_task_status)
+            expect { subject }.to change(AssignHearingDispositionTask, :count).by(1)
+            expect(disposition_task.reload.status).to eq("cancelled")
+          end
+        end
+
+        context "and hearing upcoming" do
+          include_examples "whether hearing upcoming or in past"
+
+          it "updates previous hearing's disposition to postponed" do
+            expect(hearing.disposition).to be_nil
+            subject
+            expect(hearing.reload.disposition).to eq("postponed")
+          end
+        end
+
+        context "and hearing marked no show and NoShowHearingTask created" do
+          let(:disposition_task_status) { "on_hold" }
+
+          before do
+            hearing.update(disposition: "no_show")
+            disposition_task.no_show!
+            allow(hpr).to receive(:hearing_scheduled_and_awaiting_disposition?).and_return(false)
+          end
+
+          include_examples "whether hearing upcoming or in past"
+
+          it "doesn't update previous hearing's disposition" do
+            expect(hearing.disposition).to eq("no_show")
+            subject
+            expect(hearing.reload.disposition).to eq("no_show")
+          end
+        end
+      end
+    end
+  end
+
   describe "hearing postponed through completion of alternate task" do
     let(:appeal) { hpr.appeal }
     let(:child_hpr) { hpr.children.first }
