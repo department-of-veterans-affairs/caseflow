d1ff2182fd22062161a7b960feac74351791c16f
Michael Beard <michael.beard@va.gov>
2024-01-10 14:06:20 -0600

    "updates batchsize variables and updatelever function"

--- client/app/caseDistribution/components/BatchSize.jsx
+++ client/app/caseDistribution/components/BatchSize.jsx
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
 // import { ACTIONS } from 'app/caseDistribution/reducers/levers/leversActionTypes';
 import { css } from 'glamor';
 import styles from 'app/styles/caseDistribution/InteractableLevers.module.scss';
 import NumberField from 'app/components/NumberField';
-import leverInputValidation from './LeverInputValidation';
 import COPY from '../../../COPY';
-import ACD_LEVERS from '../../../constants/ACD_LEVERS';
-import { checkIfOtherChangesExist } from '../utils';
-import { getUserIsAcdAdmin } from '../reducers/levers/leversSelector';
+import { Constant } from '../constants';
+import { getUserIsAcdAdmin, getLeversByGroup } from '../reducers/levers/leversSelector';
+import { updateLeverState } from '../reducers/levers/leversActions';
 
-const BatchSize = (props) => {
-  const theState = useSelector((state) => state);
-  const isUserAcdAdmin = getUserIsAcdAdmin(theState);
-
+const BatchSize = () => {
   const leverNumberDiv = css({
     '& .cf-form-int-input': { width: 'auto', display: 'inline-block', position: 'relative' },
     '& .cf-form-int-input .input-container': { width: 'auto', display: 'inline-block', verticalAlign: 'middle' },
@@ -26,6 +21,7 @@ const BatchSize = (props) => {
   const dispatch = useDispatch();
   const theState = useSelector((state) => state);
   const storeLevers = getLeversByGroup(theState, Constant.LEVERS, Constant.BATCH);
+  const isUserAcdAdmin = getUserIsAcdAdmin(theState);
   const [errorMessagesList] = useState(errorMessages);
   const [batchSizeLevers, setBatchSizeLevers] = useState(storeLevers);
 
@@ -33,46 +29,30 @@ const BatchSize = (props) => {
     setBatchSizeLevers(storeLevers);
   }, [storeLevers]);
 
-  const updateLever = (index) => (event) => {
-    setBatchSizeLevers((prevLevers) =>
-      prevLevers.map((lever, i) => {
-        if (index === i) {
-          console.log('Updating Lever:', lever);
-          const initialLever = theState.backendLevers.find((backendLever) => backendLever.item === lever.item);
-          console.log('Initial Lever:', lever.item);
-          const validationResponse = leverInputValidation(lever, event, errorMessagesList, initialLever);
-
-          if (validationResponse.statement === ACD_LEVERS.SUCCESS || validationResponse.statement === ACD_LEVERS.FAIL) {
-            // dispatch(updateLeverState(Constant.BATCH, lever.item, event));
-          } else if (validationResponse.statement === ACD_LEVERS.DUPLICATE && checkIfOtherChangesExist(lever)) {
-            // dispatch(updateLeverState(Constant.BATCH, lever.item, event));
-          }
-        }
-
-        return lever;
-      })
-    );
+  const updateLever = (leverItem, value = false) => (event) => {
+    dispatch(updateLeverState(leverItem, value, event, null));
   };
 
-  return (
-    <div className={styles.leverContent}>
-      <div className={styles.leverHead}>
-        <h2>{COPY.CASE_DISTRIBUTION_BATCHSIZE_H2_TITLE}</h2>
-        <div className={styles.leverLeft}><strong>{COPY.CASE_DISTRIBUTION_BATCHSIZE_LEVER_LEFT_TITLE}</strong></div>
-        <div className={styles.leverRight}><strong>{COPY.CASE_DISTRIBUTION_BATCHSIZE_LEVER_RIGHT_TITLE}</strong></div>
-      </div>
-      {batchSizeLevers && batchSizeLevers.map((lever, index) => (
-        <div className={styles.activeLever} key={`${lever.item}-${index}`}>
-          <div className={styles.leverLeft}>
-            <strong className={lever.is_disabled_in_ui ? styles.leverDisabled : styles.leverActive}>
-              {lever.title}
-            </strong>
-            <p className={lever.is_disabled_in_ui ? styles.leverDisabled : styles.leverActive}>
-              {lever.description}
-            </p>
-          </div>
-          <div className={`${styles.leverRight} ${leverNumberDiv}`}>
-            {isUserAcdAdmin ?
+  if (isUserAcdAdmin) {
+    return (
+      <div className={styles.leverContent}>
+        <div className={styles.leverHead}>
+          <h2>{COPY.CASE_DISTRIBUTION_BATCHSIZE_H2_TITLE}</h2>
+          <div className={styles.leverLeft}><strong>{COPY.CASE_DISTRIBUTION_BATCHSIZE_LEVER_LEFT_TITLE}</strong></div>
+          <div className={styles.leverRight}><strong>{COPY.CASE_DISTRIBUTION_BATCHSIZE_LEVER_RIGHT_TITLE}</strong></div>
+        </div>
+        {batchSizeLevers && batchSizeLevers.map((lever, index) => (
+          <div className={styles.activeLever} key={`${lever.item}-${index}`}>
+            <div className={styles.leverLeft}>
+              <strong className={lever.is_disabled_in_ui ? styles.leverDisabled : styles.leverActive}>
+                {lever.title}
+              </strong>
+              <p className={lever.is_disabled_in_ui ? styles.leverDisabled : styles.leverActive}>
+                {lever.description}
+              </p>
+            </div>
+            <div className={`${styles.leverRight} ${leverNumberDiv}`}>
+
               <NumberField
                 name={lever.item}
                 label={lever.unit}
@@ -80,20 +60,23 @@ const BatchSize = (props) => {
                 readOnly={lever.is_disabled_in_ui}
                 value={lever.value}
                 errorMessage={errorMessagesList[lever.item]}
-                onChange={updateLever(index, lever.item, lever.item)}
+                onChange={updateLever(index, lever.item, lever.value)}
                 tabIndex={lever.is_disabled_in_ui ? -1 : null}
-              /> :
-              <label className={lever.is_disabled_in_ui ? styles.leverDisabled : styles.leverActive}>
-                {lever.value} {lever.unit}
-              </label>
-            }
+              />
+              {!lever.is_disabled_in_ui && (
+                <label className={styles.leverActive}>
+                  {lever.value} {lever.unit}
+                </label>
+              )}
+
+            </div>
           </div>
-        </div>
-      ))}
-      <h4 className={styles.footerStyling}>{COPY.CASE_DISTRIBUTION_FOOTER_ASTERISK_DESCRIPTION}</h4>
-      <div className="cf-help-divider"></div>
-    </div>
-  );
+        ))}
+        <h4 className={styles.footerStyling}>{COPY.CASE_DISTRIBUTION_FOOTER_ASTERISK_DESCRIPTION}</h4>
+        <div className="cf-help-divider"></div>
+      </div>
+    );
+  }
 };
 
 export default BatchSize;
