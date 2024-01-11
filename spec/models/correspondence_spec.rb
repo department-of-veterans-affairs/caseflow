f218e51eb25b9a97cf5a5c8f9002ccfc75da050b
Ryan Marshall <rnmarshall1993@gmail.com>
2024-01-11 08:25:31 -0800

    updated spec to fix failing validations

--- spec/models/correspondence_spec.rb
+++ spec/models/correspondence_spec.rb
@@ -11,20 +11,8 @@ RSpec.describe Correspondence, type: :model do
   end
 
   it "can be bi-directionally related to other correspondences" do
-    c_1 = Correspondence.create!(
-      updated_by_id: 1,
-      correspondence_type_id: 1,
-      assigned_by_id: 1,
-      veteran_id: 1,
-      package_document_type_id: 1
-    )
-    c_2 = Correspondence.create!(
-      updated_by_id: 2,
-      correspondence_type_id: 2,
-      assigned_by_id: 2,
-      veteran_id: 2,
-      package_document_type_id: 2
-    )
+    c_1 = create(:correspondence)
+    c_2 = create(:correspondence)
 
     expect(c_1.related_correspondences).to eq([])
     expect(c_2.related_correspondences).to eq([])
@@ -42,20 +30,8 @@ RSpec.describe Correspondence, type: :model do
 
   describe "Create Correspondence Root Task and Review Package task as child" do
     it "Create Root Task and Review Package task for correspondence" do
-      correspondence = Correspondence.create!(
-        uuid: SecureRandom.uuid,
-        portal_entry_date: Time.zone.now,
-        source_type: "Mail",
-        package_document_type_id: 15,
-        correspondence_type_id: 8,
-        cmp_queue_id: 1,
-        cmp_packet_number: 9_999_999_999,
-        va_date_of_receipt: Time.zone.yesterday,
-        notes: "This is a note from CMP.",
-        assigned_by_id: 81,
-        updated_by_id: 81,
-        veteran_id: 1
-      )
+      correspondence = create(:correspondence)
+
 
       ct = CorrespondenceTask.find_by(appeal_id: correspondence.id, type: "CorrespondenceTask")
       expect(ct.appeal_id).to eq(correspondence.id)
