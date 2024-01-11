05fca813183fbebec352e5dc9dfc5331ed82235f
Ryan Marshall <rnmarshall1993@gmail.com>
2024-01-11 06:20:25 -0800

    Updated correspondence factors to supply all required foreign key objects.

--- spec/factories/correspondence.rb
+++ spec/factories/correspondence.rb
@@ -1,19 +1,30 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+
+  User.find_or_create_by(
+    css_id: "TEST_USER",
+    station_id: 101
+  )
+
   factory :correspondence do
     uuid { SecureRandom.uuid }
     portal_entry_date { Time.zone.now }
     source_type { "Mail" }
     package_document_type_id { 15 }
-    correspondence_type_id { 9 }
+    correspondence_type { CorrespondenceType.find_or_create_by(name: "a correspondence type.") }
+    # correspondence_type_id { correspondence_type.id }
     cmp_queue_id { 1 }
     cmp_packet_number { rand(1_000_000_000..9_999_999_999) }
     va_date_of_receipt { Time.zone.yesterday }
     notes { "This is a note from CMP." }
-    assigned_by_id { 81 }
-    updated_by_id { 81 }
-    veteran_id { 1 }
+    # binding.pry
+    assigned_by_id { User.first.id }
+    updated_by_id { User.first.id }
+    veteran_id {   Veteran.find_or_create_by(
+      last_name: "Smith", file_number: "12345678"
+    ).id }
+    package_document_type { PackageDocumentType.create! }
 
     trait :with_single_doc do
       after(:create) do |correspondence|
@@ -23,7 +34,7 @@ FactoryBot.define do
 
     trait :with_correspondence_intake_task do
       transient do
-        assigned_to { nil }
+        assigned_to { User.first }
       end
 
       after(:create) do |correspondence, evaluator|
