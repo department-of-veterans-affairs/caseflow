df9a7492796bb40d5866ef7e5f52dfa6e0ca17ad
Ryan Marshall <rnmarshall1993@gmail.com>
2024-01-11 13:22:24 -0800

    Updated to use factories for user, removed useless assignments

--- spec/factories/correspondence.rb
+++ spec/factories/correspondence.rb
@@ -1,27 +1,19 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  User.find_or_create_by(
-    css_id: "TEST_USER",
-    station_id: 101
-  )
-
   factory :correspondence do
-    correspondence_type
-    veteran
-    user
-    package_document_type
     uuid { SecureRandom.uuid }
     portal_entry_date { Time.zone.now }
     source_type { "Mail" }
-    package_document_type_id { 15 }
     cmp_queue_id { 1 }
     cmp_packet_number { rand(1_000_000_000..9_999_999_999) }
     va_date_of_receipt { Time.zone.yesterday }
     notes { "This is a note from CMP." }
-    assigned_by_id { user.id }
-    updated_by_id { user.id }
-    veteran_id { veteran.id }
+    assigned_by factory: :user
+
+    correspondence_type
+    veteran
+    package_document_type
 
     trait :with_single_doc do
       after(:create) do |correspondence|
