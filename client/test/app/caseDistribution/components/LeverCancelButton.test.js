381b797ac27ad33d3cd63145e909fbe144f9f248
Michael Beard <michael.beard@va.gov>
2024-01-12 10:37:58 -0600

    "removed comments from test"

--- client/test/app/caseDistribution/components/LeverCancelButton.test.js
+++ client/test/app/caseDistribution/components/LeverCancelButton.test.js
@@ -7,7 +7,6 @@ import { LeverCancelButton } from '../../../../app/caseDistribution/components/L
 import COPY from '../../../../COPY';
 import leversReducer from '../../../../app/caseDistribution/reducers/levers/leversReducer';
 
-// Mock the useDispatch and useSelector hooks
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useDispatch: jest.fn(),
@@ -19,7 +18,6 @@ describe('LeverCancelButton', () => {
   let dispatchMock;
 
   beforeEach(() => {
-    // Set up a mock Redux store
     const mockInitialState = {
       levers: [],
       historyList: [],
@@ -27,11 +25,9 @@ describe('LeverCancelButton', () => {
 
     store = createStore(leversReducer, mockInitialState);
 
-    // Mock the dispatch function
     dispatchMock = jest.fn();
     useDispatch.mockReturnValue(dispatchMock);
 
-    // Mock the useSelector to return the desired state
     useSelector.mockReturnValue(mockInitialState);
   });
 
@@ -42,9 +38,7 @@ describe('LeverCancelButton', () => {
       </Provider>
     );
 
-    // Add assertions based on your component's expected behavior
     expect(getByText(COPY.CASE_DISTRIBUTION_LEVER_CANCEL_BUTTON)).toBeInTheDocument();
-    // Add more assertions as needed
   });
 
   it('dispatches the revertLevers action when clicked', async () => {
@@ -55,15 +49,12 @@ describe('LeverCancelButton', () => {
     );
     const cancelButton = getByText(COPY.CASE_DISTRIBUTION_LEVER_CANCEL_BUTTON);
 
-    // Check if the button is present
     expect(cancelButton).toBeInTheDocument();
 
-    // Simulate a button click
     fireEvent.click(cancelButton);
 
     await waitFor(() => {
 
-      // Verify that dispatch was called with the expected action
       expect(dispatchMock).toHaveBeenCalledWith(expect.any(Function));});
   });
 });
