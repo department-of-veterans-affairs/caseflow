5ca519d380d3bc2cbc8a935d77b42deb899adf3c
Sean Craig <sean.craig2@va.gov>
2024-01-16 14:19:35 -0600

    Created a factory for SCT

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -79,5 +79,9 @@ FactoryBot.define do
       type { "HearingsManagement" }
       name { "Hearings Management" }
     end
+
+    factory :specialty_case_team do
+      type { SpecialtyCaseTeam.name }
+    end
   end
 end
