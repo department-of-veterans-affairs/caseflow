5989cbed21bb7430c6288d51c5c9c662afafe29e
Jonathan Cohen <cohen_jonathan@bah.com>
2024-01-16 15:04:51 -0500

    APPEALS-37039 migrations made. up and down methods need to be completed.

---
+++ db/migrate/20240116173757_add_scheduled_in_time_zone_column_to_hearings_table.rb
@@ -0,0 +1,9 @@
+class AddScheduledInTimeZoneColumnToHearingsTable < Caseflow::Migration
+  def up
+
+  end
+
+  def down
+
+  end
+end
---
+++ db/migrate/20240116173849_add_scheduled_in_time_zone_column_to_legacy_hearings_table.rb
@@ -0,0 +1,9 @@
+class AddScheduledInTimeZoneColumnToLegacyHearingsTable < Caseflow::Migration
+  def up
+
+  end
+
+  def down
+
+  end
+end
