2aa00bd2545d6983cf76c8e9b820fb8294920f94
Minhazur Rahaman <minhazur9@gmail.com>
2024-01-17 10:29:24 -0500

    APPEALS-31621 fixed typo

--- app/models/hearings/transcription_transaction.rb
+++ app/models/hearings/transcription_transaction.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TranscriptionTransaction < CaseflowRecord
-  include BelongsToPolymorphicAppealConcerns
+  include BelongsToPolymorphicAppealConcern
   belongs_to_polymorphic_appeal :appeal
   belongs_to :transcriptions
   belongs_to :transcript
