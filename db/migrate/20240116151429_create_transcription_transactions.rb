5a44e96c8ab15d69184e60d951225b4f71197d9c
Jeff Marks <jeff.t.marks@gmail.com>
2024-01-17 13:06:58 -0500

    APPEALS-31619 Change null options on migration

--- db/migrate/20240116151429_create_transcription_transactions.rb
+++ db/migrate/20240116151429_create_transcription_transactions.rb
@@ -2,26 +2,26 @@ class CreateTranscriptionTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transcription_transactions do |t|
       t.string :file_name, null: false, comment: "File name, with extension, of the transcription file migrated by caseflow"
-      t.bigint :appeal_id, null: false, comment: "ID of the appeal associated with this record"
-      t.string :appeal_type, null: false, comment: "Type of appeal associated with this record"
+      t.bigint :appeal_id, comment: "ID of the appeal associated with this record"
+      t.string :appeal_type, comment: "Type of appeal associated with this record"
       t.string :docket_number, null: false, comment: "Docket number of associated appeal"
       t.string :file_status, null: false, comment: "Status of the file, could be one of nil, 'Found in BOX', 'Error in upload to BOX', 'AWS Uploaded', 'Error in upload to AWS', or 'File Upload out of Sequence'"
 
-      t.datetime :date_receipt_webex, null: false, comment: "Timestamp when file was added to webex"
-      t.datetime :date_vtt_converted, null: false, comment: "Timestamp when file was converted from vtt"
-      t.datetime :date_mp4_converted, null: false, comment: "Timestamp when file was converted from mp4"
-      t.datetime :date_upload_box, null: false, comment: "Timestamp when file was added to box"
-      t.datetime :date_upload_aws, null: false, comment: "Timestamp when file was loaded to AWS"
+      t.datetime :date_receipt_webex, comment: "Timestamp when file was added to webex"
+      t.datetime :date_vtt_converted, comment: "Timestamp when file was converted from vtt"
+      t.datetime :date_mp4_converted, comment: "Timestamp when file was converted from mp4"
+      t.datetime :date_upload_box, comment: "Timestamp when file was added to box"
+      t.datetime :date_upload_aws, comment: "Timestamp when file was loaded to AWS"
 
-      t.string :aws_link_mp4, null: false, comment: "Link to be used by HMB to download original audio file from AWS S3"
-      t.string :aws_link_vtt, null: false, comment: "Link to be used by HMB to download original transcript file from AWS S3"
-      t.string :aws_link_mp3, null: false, comment: "Link to be used by HMB to download transformed audio file from AWS S3"
-      t.string :aws_link_rtf, null: false, comment: "Link to be used by HMB to download transformed transcript file from AWS S3"
-      t.string :aws_link_pdf, null: false, comment: "Link to be used by HMB to download pdf transcript file from AWS S3"
-      t.string :aws_link_xls, null: false, comment: "Link to be used by HMB to download a transcript error file report from AWS S3"
+      t.string :aws_link_mp4, comment: "Link to be used by HMB to download original audio file from AWS S3"
+      t.string :aws_link_vtt, comment: "Link to be used by HMB to download original transcript file from AWS S3"
+      t.string :aws_link_mp3, comment: "Link to be used by HMB to download transformed audio file from AWS S3"
+      t.string :aws_link_rtf, comment: "Link to be used by HMB to download transformed transcript file from AWS S3"
+      t.string :aws_link_pdf, comment: "Link to be used by HMB to download pdf transcript file from AWS S3"
+      t.string :aws_link_xls, comment: "Link to be used by HMB to download a transcript error file report from AWS S3"
 
-      t.bigint :created_by_id, null: false, comment: "The user who created the transcription record"
-      t.bigint :updated_by_id, null: false, comment: "The user who most recently updated the transcription file"
+      t.bigint :created_by_id, comment: "The user who created the transcription record"
+      t.bigint :updated_by_id, comment: "The user who most recently updated the transcription file"
       t.timestamps
     end
 
