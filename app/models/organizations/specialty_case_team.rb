5645c5d2cbd005a8d7bd2911c46379d780aaf9f3
Sean Craig <sean.craig2@va.gov>
2024-01-17 09:24:01 -0600

    Added some comments for the sct org

--- app/models/organizations/specialty_case_team.rb
+++ app/models/organizations/specialty_case_team.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 # The Spcialty Case Team
+# An appeal is routed to this organization when both of the following are true:
+#   - Veterans Health Administration (VHA) is selected as the benefit type
+#   - VHA issues are added during the intake of an appeal
+# The appeal is to be routed following appeal distribution request
 
 class SpecialtyCaseTeam < Organization
   def self.singleton
