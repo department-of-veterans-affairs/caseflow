a13854ab708905b7dad5bad590c92f46b8ddc283
Calvin <Calvin.Costa@va.gov>
2024-01-18 13:04:15 -0500

    simplified method

--- app/models/dockets/hearing_request_docket.rb
+++ app/models/dockets/hearing_request_docket.rb
@@ -78,7 +78,6 @@ class HearingRequestDocket < Docket
     # genpop 'only_genpop' returns 2 arrays of the limited base relation. This means if we only request 2 cases, appeals is a
     # 2x2 array containing 4 cases overall and we will end up distributing 4 cases rather than 2. Instead, reinstate the
     # limit here by filtering out the newest cases
-    appeals_to_reject = appeals_array.flatten.sort_by(&:receipt_date).drop(limit)
-    appeals_array - appeals_to_reject
+    appeals_array.flatten.sort_by(&:receipt_date).first(limit)
   end
 end
