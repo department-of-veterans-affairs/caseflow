d748cfe7d75f74d86c89d7d0233c798872daf42d
631068 <rickybmanus@gmail.com>
2024-01-18 14:16:56 -0500

    Fixed linting error with SaveModal

--- client/app/caseDistribution/components/SaveModal.jsx
+++ client/app/caseDistribution/components/SaveModal.jsx
@@ -73,10 +73,13 @@ export const SaveModal = (props) => {
       isOpen
       onClose={() => setShowModal(false)}
       closeHandler={() => setShowModal(false)}
-      title={COPY.CASE_DISTRIBUTION_MODAL_TITLE}
-      confirmButton={<Button id="save-modal-confirm" onClick={handleConfirmButton}>{COPY.MODAL_CONFIRM_BUTTON}</Button>}
-      cancelButton={<Button id="save-modal-cancel" onClick={() => setShowModal(false)}>{COPY.MODAL_CANCEL_BUTTON}</Button>}
       className={styles.updatedModalStyling}
+      title={COPY.CASE_DISTRIBUTION_MODAL_TITLE}
+      confirmButton={<Button id="save-modal-confirm" onClick={handleConfirmButton}>
+        {COPY.MODAL_CONFIRM_BUTTON}</Button>}
+
+      cancelButton={<Button id="save-modal-cancel" onClick={() => setShowModal(false)}>
+        {COPY.MODAL_CANCEL_BUTTON}</Button>}
     >
       <p>{COPY.CASE_DISTRIBUTION_MODAL_DESCRIPTION}</p>
       {leverList()}
