987f14ea0b76a6c110ebd1dc62937699464aa4a8
Kevin Ma <magavakevin10@gmail.com>
2024-01-18 12:25:16 -0500

    Added initla business logic for assignment

--- app/jobs/auto_assign_correspondence_job.rb
+++ app/jobs/auto_assign_correspondence_job.rb
@@ -5,5 +5,16 @@ class AutoAssignCorrespondenceJob < CaseflowJob
 
   def perform
     Rails.logger.info("Auto assign correspondences job.....")
+    unassigned_correspondences = Correspondence.joins("INNER JOIN tasks ON tasks.appeal_id = correspondences.id")
+      .where("tasks.status" => "unassigned")
+      .where("tasks.appeal_type" => "Correspondence")
+      .order(va_date_of_receipt: :desc)
+    unassigned_correspondences.each do |unassigned_correspondence|
+      begin
+        unassigned_correspondence.update!(assigned_to: MailTeamSupervisor.singleton)
+      rescue ActiveRecord::RecordInvalid => error
+        invalid_record_error(error.record)
+      end
+    end
   end
 end
