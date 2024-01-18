77d30d842e34c1252fc2a0aadfb29df04a14f695
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-18 17:08:57 -0500

    🐛 AppealState: Remove self-`includes` from association scope blocks
    While upgrading to  Rails 6.0, it was discovered that a `joins`
    on an association containing a self-`includes` in the association's
    scope block will alter the generated `INNER JOIN` clause in a way
    that opens the door to duplicate records in the result set, causing bugs.
    
    In anticipation of the Rails 6.0 upgrade, we must then identify all
    such associations, remove the offending self-`includes`, and make
    whatever consequent changes necessary to accomodate this change.

--- spec/models/appeal_state_spec.rb
+++ spec/models/appeal_state_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 describe AppealState do
-  it_behaves_like "AppealState belongs_to polymorphic appeal"
+  it_behaves_like "AppealState belongs_to polymorphic appeal" do
+    let!(:_user) { create(:user) } # A User needs to exist for `appeal_state` factories
+  end
 end
