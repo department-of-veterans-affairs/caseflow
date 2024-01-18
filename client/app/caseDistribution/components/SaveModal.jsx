f41683530a8d3b54d0a742775654ab144f33bae6
Christopher Detlef <>
2024-01-18 13:39:12 -0500

    Updated feature tests

--- client/app/caseDistribution/components/SaveModal.jsx
+++ client/app/caseDistribution/components/SaveModal.jsx
@@ -74,8 +74,8 @@ export const SaveModal = (props) => {
       onClose={() => setShowModal(false)}
       closeHandler={() => setShowModal(false)}
       title={COPY.CASE_DISTRIBUTION_MODAL_TITLE}
-      confirmButton={<Button onClick={handleConfirmButton}>{COPY.MODAL_CONFIRM_BUTTON}</Button>}
-      cancelButton={<Button onClick={() => setShowModal(false)}>{COPY.MODAL_CANCEL_BUTTON}</Button>}
+      confirmButton={<Button id="save-modal-confirm" onClick={handleConfirmButton}>{COPY.MODAL_CONFIRM_BUTTON}</Button>}
+      cancelButton={<Button id="save-modal-cancel" onClick={() => setShowModal(false)}>{COPY.MODAL_CANCEL_BUTTON}</Button>}
       className={styles.updatedModalStyling}
     >
       <p>{COPY.CASE_DISTRIBUTION_MODAL_DESCRIPTION}</p>
--- spec/feature/automatic_case_distribution/acd_levers/ama_np_dist_goals_by_docket_lever_spec.rb
+++ spec/feature/automatic_case_distribution/acd_levers/ama_np_dist_goals_by_docket_lever_spec.rb
@@ -70,12 +70,23 @@ RSpec.feature "AMA Non-priority Distribution Goals by Docket Levers" do
     scenario "lever history displays on page" do
       visit "case-distribution-controls"
       confirm_page_and_section_loaded
+
+      expect(page).not_to have_content("123 days")
+      expect(page).not_to have_content("300 days")
+
       fill_in ama_direct_reviews_field, with: "123"
-      find("#LeversSaveButton").click
-      find(".cf-submit").click
+      click_save_button
+      click_modal_confirm_button
 
-      expect(page).to have_css(".entry-updated-values > ol > li", text: "123")
+      expect(page).to have_content("123 days")
+      expect(page).not_to have_content("300 days")
 
+      fill_in ama_direct_reviews_field, with: "300"
+      click_save_button
+      click_modal_confirm_button
+
+      expect(page).to have_content("123 days")
+      expect(page).to have_content("300 days")
     end
   end
 end
@@ -85,4 +96,12 @@ def confirm_page_and_section_loaded
   expect(page).to have_content("AMA Hearings")
   expect(page).to have_content("AMA Direct Review")
   expect(page).to have_content("AMA Evidence Submission")
+end
+
+def click_save_button
+  find("#LeversSaveButton").click
+end
+
+def click_modal_confirm_button
+  find("#save-modal-confirm").click
 end
