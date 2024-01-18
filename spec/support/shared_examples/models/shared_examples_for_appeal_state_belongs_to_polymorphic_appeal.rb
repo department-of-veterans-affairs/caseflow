f300379e3ef5ffefac7d0a5f4277a5a304622941
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-18 17:08:57 -0500

    🐛 AppealState: Remove self-`includes` from association scope blocks
    While upgrading to  Rails 6.0, it was discovered that a `joins`
    on an association containing a self-`includes` in the association's
    scope block will alter the generated `INNER JOIN` clause in a way
    that opens the door to duplicate records in the result set, causing bugs.
    
    In anticipation of the Rails 6.0 upgrade, we must then identify all
    such associations, remove the offending self-`includes`, and make
    whatever consequent changes necessary to accomodate this change.

--- spec/support/shared_examples/models/shared_examples_for_appeal_state_belongs_to_polymorphic_appeal.rb
+++ spec/support/shared_examples/models/shared_examples_for_appeal_state_belongs_to_polymorphic_appeal.rb
@@ -4,8 +4,6 @@ require "query_subscriber"
 
 shared_examples "AppealState belongs_to polymorphic appeal" do
   context do
-    let!(:_user) { create(:user) } # A User needs to exist for `appeal_state` factories
-
     context "'appeal'-related associations" do
       it { should belong_to(:appeal) }
       it { should belong_to(:ama_appeal).class_name("Appeal").with_foreign_key(:appeal_id).optional }
@@ -18,7 +16,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
           # Create records having different `appeal_type` but the same `appeal_id`. This will ensure the test fails in
           # the case where the `joins` result contains duplicate entries for records having the same `appeal_id` but
           # different `appeal_type`.
-          let(:shared_id) { 1 }
+          let(:shared_id) { 99999 }
           let!(:_legacy_appeal_state) { create(:appeal_state, appeal: create(:legacy_appeal, id: shared_id)) }
 
           context "when there are no AppealStates with AMA appeals" do
@@ -33,7 +31,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
         end
 
         context "when eager loading with `includes`" do
-          subject { AppealState.ama.includes(:ama_appeal) }
+          subject { AppealState.ama.includes(:appeal) }
 
           let!(:_legacy_appeal_state) { create(:appeal_state, :legacy) }
 
@@ -48,7 +46,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
 
             it "prevents N+1 queries" do
               QuerySubscriber.new.tap do |subscriber|
-                subscriber.track { subject.map { |record| record.ama_appeal.id } }
+                subscriber.track { subject.map { |record| record.appeal.id } }
                 expect(subscriber.queries.count).to eq 2
               end
             end
@@ -56,7 +54,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
         end
 
         context "when eager loading with `preload`" do
-          subject { AppealState.ama.preload(:ama_appeal) }
+          subject { AppealState.ama.preload(:appeal) }
 
           let!(:_legacy_appeal_state) { create(:appeal_state, :legacy) }
 
@@ -71,27 +69,10 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
 
             it "prevents N+1 queries" do
               QuerySubscriber.new.tap do |subscriber|
-                subscriber.track { subject.map { |record| record.ama_appeal.id } }
+                subscriber.track { subject.map { |record| record.appeal.id } }
                 expect(subscriber.queries.count).to eq 2
               end
             end
-          end
-        end
-
-        context "when called on an individual AppealState" do
-          subject { appeal_state.ama_appeal }
-
-          context "when the AppealState is not associated with an AMA appeal" do
-            let(:appeal_state) { create(:appeal_state, :legacy) }
-
-            it { should be_nil }
-          end
-
-          context "when the appeal_state is associated with an AMA appeal" do
-            let(:appeal_state) { create(:appeal_state, appeal: ama_appeal) }
-            let(:ama_appeal) { create(:appeal) }
-
-            it { should eq(ama_appeal) }
           end
         end
       end
@@ -103,7 +84,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
           # Create records having different `appeal_type` but the same `appeal_id`. This will ensure the test fails in
           # the case where the `joins` result contains duplicate entries for records having the same `appeal_id` but
           # different `appeal_type`.
-          let(:shared_id) { 1 }
+          let(:shared_id) { 99999 }
           let!(:_ama_appeal_state) { create(:appeal_state, appeal: create(:appeal, id: shared_id)) }
 
           context "when there are no AppealStates with Legacy appeals" do
@@ -118,7 +99,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
         end
 
         context "when eager loading with `includes`" do
-          subject { AppealState.legacy.includes(:legacy_appeal) }
+          subject { AppealState.legacy.includes(:appeal) }
 
           let!(:_ama_appeal_state) { create(:appeal_state, :ama) }
 
@@ -133,7 +114,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
 
             it "prevents N+1 queries" do
               QuerySubscriber.new.tap do |subscriber|
-                subscriber.track { subject.map { |record| record.legacy_appeal.id } }
+                subscriber.track { subject.map { |record| record.appeal.id } }
                 expect(subscriber.queries.count).to eq 2
               end
             end
@@ -141,7 +122,7 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
         end
 
         context "when eager loading with `preload`" do
-          subject { AppealState.legacy.preload(:legacy_appeal) }
+          subject { AppealState.legacy.preload(:appeal) }
 
           let!(:_ama_appeal_state) { create(:appeal_state, :ama) }
 
@@ -156,27 +137,10 @@ shared_examples "AppealState belongs_to polymorphic appeal" do
 
             it "prevents N+1 queries" do
               QuerySubscriber.new.tap do |subscriber|
-                subscriber.track { subject.map { |record| record.legacy_appeal.id } }
+                subscriber.track { subject.map { |record| record.appeal.id } }
                 expect(subscriber.queries.count).to eq 2
               end
             end
-          end
-        end
-
-        context "when called on an individual AppealState" do
-          subject { appeal_state.legacy_appeal }
-
-          context "when the AppealState is not associated with a Legacy appeal" do
-            let(:appeal_state) { create(:appeal_state, :ama) }
-
-            it { should be_nil }
-          end
-
-          context "when the appeal_state is associated with a Legacy appeal" do
-            let(:appeal_state) { create(:appeal_state, appeal: legacy_appeal) }
-            let(:legacy_appeal) { create(:legacy_appeal) }
-
-            it { should eq(legacy_appeal) }
           end
         end
       end
