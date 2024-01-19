ff7119891f77fa92049dcba5c00fdbe7eb2d98c3
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-19 15:53:06 -0500

    🐛 JudgeCaseReview: Remove self-`includes` from association scope blocks
    While upgrading to  Rails 6.0, it was discovered that a `joins`
    on an association containing a self-`includes` in the association's
    scope block will alter the generated `INNER JOIN` clause in a way
    that opens the door to duplicate records in the result set, causing bugs.
    
    In anticipation of the Rails 6.0 upgrade, we must then identify all
    such associations, remove the offending self-`includes`, and make
    whatever consequent changes necessary to accomodate this change.

--- app/models/concerns/judge_case_review_belongs_to_polymorphic_appeal_concern.rb
+++ app/models/concerns/judge_case_review_belongs_to_polymorphic_appeal_concern.rb
@@ -7,11 +7,11 @@ module JudgeCaseReviewBelongsToPolymorphicAppealConcern
     belongs_to :appeal, polymorphic: true
 
     belongs_to :ama_appeal,
-               -> { includes(:judge_case_reviews).where(judge_case_reviews: { appeal_type: "Appeal" }) },
+               -> { where(judge_case_reviews: { appeal_type: "Appeal" }) },
                class_name: "Appeal", foreign_key: "appeal_id", optional: true
 
     belongs_to :legacy_appeal,
-               -> { includes(:judge_case_reviews).where(judge_case_reviews: { appeal_type: "LegacyAppeal" }) },
+               -> { where(judge_case_reviews: { appeal_type: "LegacyAppeal" }) },
                class_name: "LegacyAppeal", foreign_key: "appeal_id", optional: true
 
     scope :ama, -> { where(appeal_type: "Appeal") }
