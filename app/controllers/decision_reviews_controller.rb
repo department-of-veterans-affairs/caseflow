ffc2569f353fa17cd6db1bbe2c1b7cfef9e06812
Clay Sheppard <csheepy@gmail.com>
2024-01-19 08:48:00 -0600

    update verify_task to only accept decision review tasks

--- app/controllers/decision_reviews_controller.rb
+++ app/controllers/decision_reviews_controller.rb
@@ -220,7 +220,7 @@ class DecisionReviewsController < ApplicationController
   end
 
   def verify_task
-    unless task
+    unless task&.is_a?(DecisionReviewTask)
       render json: { error: "Task #{task_id} not found" }, status: :not_found
     end
   end
