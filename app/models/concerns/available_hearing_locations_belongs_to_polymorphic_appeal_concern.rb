e1b7a461365f61609d84a565b33001417401014b
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-18 23:13:29 -0500

    🔥 AvailableHearingLocations: Remove unused method overrides

--- app/models/concerns/available_hearing_locations_belongs_to_polymorphic_appeal_concern.rb
+++ app/models/concerns/available_hearing_locations_belongs_to_polymorphic_appeal_concern.rb
@@ -11,20 +11,10 @@ module AvailableHearingLocationsBelongsToPolymorphicAppealConcern
                     .where(available_hearing_locations: { appeal_type: "Appeal" }) },
                class_name: "Appeal", foreign_key: "appeal_id", optional: true
 
-    def ama_appeal
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "Appeal"
-    end
-
     belongs_to :legacy_appeal,
                -> { includes(:available_hearing_locations)
                     .where(available_hearing_locations: { appeal_type: "LegacyAppeal" }) },
                class_name: "LegacyAppeal", foreign_key: "appeal_id", optional: true
-
-    def legacy_appeal
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "LegacyAppeal"
-    end
 
     scope :ama, -> { where(appeal_type: "Appeal") }
     scope :legacy, -> { where(appeal_type: "LegacyAppeal") }
