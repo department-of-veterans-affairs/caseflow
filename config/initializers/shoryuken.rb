9b6d0dc6fd9dd123d1e9510948bf0792106c13ae
j-n-t-l <611441@bah.com>
2024-01-19 16:04:06 -0500

    APPEALS-37127 added middleware to shoryuken file

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,6 +1,7 @@
 require "#{Rails.root}/app/jobs/middleware/job_monitoring_middleware"
 require "#{Rails.root}/app/jobs/middleware/job_request_store_middleware"
 require "#{Rails.root}/app/jobs/middleware/job_sentry_scope_middleware"
+require "#{Rails.root}/app/jobs/middleware/job_time_zone_middleware"
 
 # set up default exponential backoff parameters
 ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
@@ -32,5 +33,6 @@ Shoryuken.configure_server do |config|
     chain.add JobMonitoringMiddleware
     chain.add JobRequestStoreMiddleware
     chain.add JobSentryScopeMiddleware
+    chain.add JobTimeZoneMiddleware
   end
 end
