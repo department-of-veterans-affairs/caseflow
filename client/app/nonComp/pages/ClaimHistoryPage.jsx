a0d92addaf6c5d5fcd7623c42b4f36ec9de9c37c
Clay Sheppard <csheepy@gmail.com>
2024-01-19 13:23:19 -0600

    post-ba/ux style changes

--- client/app/nonComp/pages/ClaimHistoryPage.jsx
+++ client/app/nonComp/pages/ClaimHistoryPage.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import NonCompLayout from '../components/NonCompLayout';
 import Link from 'app/components/Link';
+import styled from 'styled-components';
+
+const LinkDiv = styled.div`
+  display: inline-block;
+  margin-top: 1rem;
+`;
 
 const ClaimHistoryPage = () => {
 
@@ -11,12 +17,12 @@ const ClaimHistoryPage = () => {
 
   const returnLink = `../${task.id}`;
 
-  return <>
-    <Link to={returnLink}><b><u>&lt; Back to Decision Review</u></b></Link>
+  return <div className="individual-claim-history">
+    <LinkDiv> <Link to={returnLink}><b><u>&lt; Back to Decision Review</u></b></Link></LinkDiv>
     <NonCompLayout>
       <h1>{task.claimant.name}</h1>
     </NonCompLayout>
-  </>;
+  </div>;
 };
 
 export default ClaimHistoryPage;
