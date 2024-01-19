9ceaae5eec227e002f95b815a059aaa47d0501d6
Alexander Smith <alex.smith.99@thunderyard.com>
2024-01-19 14:11:37 -0700

    Fix Data Setup Causing Spec Failure

--- spec/services/correspondence_auto_assigner_spec.rb
+++ spec/services/correspondence_auto_assigner_spec.rb
@@ -7,7 +7,6 @@ describe CorrespondenceAutoAssigner do
   let(:current_user) { create(:intake_user) }
 
   let!(:correspondence) { create(:correspondence, :with_single_doc, veteran_id: veteran.id, uuid: SecureRandom.uuid) }
-  let!(:unassigned_task) { create(:review_package_task, appeal: correspondence, status: "unassigned") }
 
   describe "#do_auto_assignment" do
     it "successfully creates a ReviewPackageTask and updates the existing task" do
@@ -15,8 +14,9 @@ describe CorrespondenceAutoAssigner do
         described.do_auto_assignment(current_user_id: current_user.id)
       end.to change(ReviewPackageTask, :count)
 
-      expect(unassigned_task.assigned_to).to eq(InboundOpsTeam.singleton)
-      expect(unassigned_task.status).to eq(:on_hold)
+      created = ReviewPackageTask.last
+      expect(created.assigned_to).to eq(InboundOpsTeam.singleton)
+      expect(created.status).to eq("assigned")
     end
   end
 end
