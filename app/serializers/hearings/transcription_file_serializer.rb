5ef3afd3ada659a46b1d7d42c62312a074ff5de1
Ariana Konhilas <konhilas_ariana@bah.com>
2024-01-22 12:51:04 -0500

    APPEALS-31820: updated serializer to match new db columns and updated AC

--- app/serializers/hearings/transcription_file_serializer.rb
+++ app/serializers/hearings/transcription_file_serializer.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-class TranscriptionFilesSerializer
+class TranscriptionFileSerializer
   include FastJsonapi::ObjectSerializer
 
   attribute :docket_number
-  attribute :date_uploaded_aws
+  attribute :date_upload_aws
   attribute :file_name
   attribute :file_type
-  attribute :aws_link_mp4
-  attribute :aws_link_mp3
-  attribute :aws_link_vtt
-  attribute :aws_link_rtf
+  attribute :aws_link
   attribute :file_status
 end
