49181a697a9e8bdda3e7095bdb021d3f0a2df8ae
Sean Craig <sean.craig2@va.gov>
2024-01-22 09:19:49 -0600

    created the model for the specialty_case_team_completed_tasks_tab

---
+++ app/models/queue_tabs/specialty_case_team_completed_tasks_tab.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class SpecialtyCaseTeamCompletedTasksTab < QueueTab
+  validate :assignee_is_organization
+
+  attr_accessor :show_reader_link_column, :allow_bulk_assign
+  delegate :column_names, to: :specialty_case_team
+
+  def label
+    COPY::ORGANIZATIONAL_QUEUE_COMPLETED_TAB_TITLE
+  end
+
+  def self.tab_name
+    Constants.QUEUE_CONFIG.COMPLETED_TASKS_TAB_NAME
+  end
+
+  def description
+    COPY::SPECIALTY_CASE_TEAM_QUEUE_PAGE_COMPLETED_TAB_DESCRIPTION
+  end
+
+  def tasks
+    recently_completed_tasks
+  end
+
+  def column_names
+    SpecialtyCaseTeam::COLUMN_NAMES
+  end
+end
