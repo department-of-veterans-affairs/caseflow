553510580fa6d3089ac37a67f11d54d18d533dbc
William Medders <medders_william@bah.com>
2024-01-24 09:19:57 -0600

    allow correspondence tasks

--- app/models/correspondence_task_pager.rb
+++ app/models/correspondence_task_pager.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class CorrespondenceTaskPager < TaskPager
+  def paged_tasks
+    @paged_tasks ||= begin
+      tasks = sorted_tasks(filtered_tasks)
+      pagination_enabled ? tasks.page(page).per(TASKS_PER_PAGE) : tasks
+    end
+  end
 end
