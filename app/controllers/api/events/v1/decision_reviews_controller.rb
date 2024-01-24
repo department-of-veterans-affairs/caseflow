de6f439bd04816258af96001861f6f45628686ab
Enrilo Ugalde <ugalde_enrilo@bah.com>
2024-01-24 10:08:38 -0800

    APPEALS-38232 - renamed controller to AC req, added decision_review_created_error endpoint, added decision_review_created_error method, and added RSpec for new method

--- app/controllers/api/events/v1/decision_reviews_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Api::Events::V1::DecisionReviewsController < Api::ApplicationController
-
-  def decision_review_created
-    render json: { message: "Decision Review Created" }, status: :created
-  end
-end
