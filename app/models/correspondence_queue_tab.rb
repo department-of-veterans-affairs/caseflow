8c095e6f74d9480ba80cb3e1fdbf55bf5fba3228
William Medders <medders_william@bah.com>
2024-01-24 08:36:24 -0600

    Override methods to work with correspondence tasks

--- app/models/correspondence_queue_tab.rb
+++ app/models/correspondence_queue_tab.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class CorrespondenceQueueTab < QueueTab
+  def columns
+    column_names.map do |column_name|
+      CorrespondenceQueueColumn.new(name: column_name)
+    end
+  end
+
+  def task_includes
+    [
+      { appeal: [:package_document_type] },
+      :assigned_by,
+      :assigned_to,
+      :children,
+      :parent
+    ]
+  end
 end
