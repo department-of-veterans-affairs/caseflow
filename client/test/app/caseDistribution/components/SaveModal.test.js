84c2ddd965fd0d9431411a52ca49c6c69d518686
Michael Beard <michael.beard@va.gov>
2024-01-24 12:17:27 -0600

    "adds export to save button and updates test"

--- client/test/app/caseDistribution/components/SaveModal.test.js
+++ client/test/app/caseDistribution/components/SaveModal.test.js
@@ -1,110 +1,38 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { LeverSaveButton } from '../../../../app/caseDistribution/components/LeverSaveButton';
-import * as leversActions from 'app/caseDistribution/reducers/levers/leversActions';
 import SaveModal from '../../../../app/caseDistribution/components/SaveModal';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from 'app/caseDistribution/reducers/root';
+import thunk from 'redux-thunk';
+import { levers, testingBatchLevers } from '../../../data/adminCaseDistributionLevers';
+import { loadLevers, setUserIsAcdAdmin } from 'app/caseDistribution/reducers/levers/leversActions';
+import { mount } from 'enzyme';
 
-jest.mock('../../../../app/caseDistribution/components/SaveModal', () => jest.fn(() => null));
+describe('Save Modal', () => {
 
-describe('LeverSaveButton', () => {
-  const initialState = {
-    caseDistributionLevers: {
-      levers: [],
-      isUserAcdAdmin: false,
-      leversErrors: [],
-    }
-  };
+  const getStore = () => createStore(
+    rootReducer,
+    applyMiddleware(thunk));
 
-  const mockReducer = (state = initialState, action) => {
-    switch (action.type) {
-    case leversActions.setUserIsAcdAdmin().type:
-      return {
-        ...state,
-        caseDistributionLevers: {
-          ...state.caseDistributionLevers,
-          isUserAcdAdmin: action.payload.isUserAcdAdmin,
-        },
-      };
-
-    case leversActions.loadLevers().type:
-      return {
-        ...state,
-        caseDistributionLevers: {
-          ...state.caseDistributionLevers,
-          levers: action.payload.levers,
-          leversErrors: action.payload.errors || [],
-        },
-      };
-
-    case leversActions.saveLevers().type: {
-      const updatedLevers = action.payload.levers;
-      const updatedState = {
-        ...state,
-        caseDistributionLevers: {
-          ...state.caseDistributionLevers,
-          levers: updatedLevers,
-        },
-      };
-
-      // console.log('Saved Levers:', updatedLevers);
-
-      return updatedState;
-    }
-
-    default:
-      return state;
-    }
-  };
-
-  let store;
-  let dispatchMock;
-
-  beforeEach(() => {
-    dispatchMock = jest.fn();
-    store = createStore(mockReducer, applyMiddleware(thunk));
-    jest.spyOn(store, 'dispatch').mockImplementation(dispatchMock);
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('renders the component correctly', () => {
-    const { getByText } = render(
+  let batchSizeLevers = levers.filter((lever) => (lever.lever_group === 'batch'));
+  let leversWithBatchLevers = { batch: batchSizeLevers };
+
+  it('renders the Save Modal for Member Users', () => {
+    const store = getStore();
+
+    store.dispatch(loadLevers(leversWithBatchLevers));
+    store.dispatch(setUserIsAcdAdmin(false));
+
+    render(
       <Provider store={store}>
-        <LeverSaveButton />
+        <SaveModal />
       </Provider>
     );
-
-    expect(getByText('Save')).toBeInTheDocument();
+    expect(document.querySelector('#modal_id-title')).toBeNull();
   });
-
-  it('opens the modal when Save button is clicked', async () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <LeverSaveButton />
-      </Provider>
-    );
-
-    fireEvent.click(getByText('Save'));
-
-    await waitFor(() => {
-      expect(dispatchMock).toHaveBeenCalled();
-      expect(dispatchMock).toHaveBeenCalledWith(expect.any(Function));
-    });
-
-    // const saveLeversAction = leversActions.saveLevers({});
-
-    // store.dispatch(saveLeversAction);
-
-    // // expect(dispatchMock).toHaveBeenCalledWith(saveLevers(updatedLevers));
-
-    // await waitFor(() => {
-    //   expect(SaveModal).toHaveBeenCalledWith(
-    //     expect.objectContaining({
-    //       setShowModal: expect.any(Function),
-    //     })
-    //   );
-    // });
-  });
-
 });
