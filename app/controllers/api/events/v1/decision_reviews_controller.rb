db04777c987399d0ce239625441fa7b75a5846dc
TuckerRose <tuckerrose@tuckerroses-mbp.lan>
2024-01-24 11:50:23 -0500

    removed before action

--- app/controllers/api/events/v1/decision_reviews_controller.rb
+++ app/controllers/api/events/v1/decision_reviews_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::Events::V1::DecisionReviewsController < Api::ApplicationController
-  before_action :verify_authentication_token
-  protect_from_forgery with: :null_session
 
   def decision_review_created
     render json: { message: "Decision Review Created" }, status: :created
