8cbc07f0a4d2263aeba7fe3ad1cc5182512b8b84
631068 <rickybmanus@gmail.com>
2024-01-25 17:18:53 -0500

    Updated unit tests to improve coverage on DocketTimeGoals

--- client/test/app/caseDistribution/components/DocketTimeGoals.test.js
+++ client/test/app/caseDistribution/components/DocketTimeGoals.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor} from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import DocketTimeGoals from 'app/caseDistribution/components/DocketTimeGoals';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -9,8 +9,9 @@ import {
   testingDocketDistributionPriorLevers,
   testingDocketTimeGoalsLevers
 } from '../../../data/adminCaseDistributionLevers';
-import { loadLevers, setUserIsAcdAdmin} from 'app/caseDistribution/reducers/levers/leversActions';
+import { loadLevers, setUserIsAcdAdmin } from 'app/caseDistribution/reducers/levers/leversActions';
 import { mount } from 'enzyme';
+import userEvent from '@testing-library/user-event';
 
 describe('Docket Time Goals Lever', () => {
 
@@ -22,12 +23,6 @@ describe('Docket Time Goals Lever', () => {
     jest.clearAllMocks();
   });
 
-  // "static": "static",
-  // "batch": "batch",
-  // "affinity": "affinity",
-  // "docket_distribution_prior": "docket_distribution_prior",
-  // "docket_time_goal": "docket_time_goal"
-
   let levers = {
     docket_distribution_prior: testingDocketDistributionPriorLevers,
 
@@ -36,7 +31,7 @@ describe('Docket Time Goals Lever', () => {
   let testTimeGoalLever = testingDocketTimeGoalsLevers[0];
   let testDistPriorLever = testingDocketDistributionPriorLevers[0];
 
-  it.skip('renders Batch Size Levers for Member Users', () => {
+  it('renders Docket Time Goals Levers for Member Users', () => {
     const store = getStore();
 
     store.dispatch(loadLevers(levers));
@@ -47,30 +42,32 @@ describe('Docket Time Goals Lever', () => {
         <DocketTimeGoals />
       </Provider>
     );
-    // expect(document.querySelector('.active-lever > .lever-left')).toHaveTextContent(lever.title);
-    // expect(document.querySelector('.active-lever > .lever-left')).toHaveTextContent(lever.description);
-    // expect(document.querySelector('.active-lever > .lever-right')).toHaveTextContent(lever.value);
+
+    expect(document.querySelector('.active-lever > .lever-middle')).toHaveTextContent(testTimeGoalLever.value);
+    expect(document.querySelector('.active-lever > .lever-middle')).toHaveTextContent(testTimeGoalLever.unit);
+    expect(document.querySelector('.active-lever > .lever-right')).
+      toHaveTextContent(testTimeGoalLever.is_toggle_active ? 'On' : 'Off');
   });
 
-  it.skip('renders Batch Size Levers for Admin Users', () => {
+  it('renders Docket Time Goals Levers for Admin Users', () => {
     const store = getStore();
 
     store.dispatch(loadLevers(levers));
     store.dispatch(setUserIsAcdAdmin(true));
 
-    render(
+    const wrapper = mount(
       <Provider store={store}>
         <DocketTimeGoals />
-      </Provider>
-    );
+      </Provider>);
 
-    // expect(document.querySelector('.active-lever > .lever-left')).toHaveTextContent(lever.title);
-    // expect(document.querySelector('.active-lever > .lever-left')).toHaveTextContent(lever.description);
-    // expect(document.querySelector('.active-lever > .lever-right')).toHaveTextContent(lever.value);
+    let leverTimeGoal = wrapper.find('input[name="ama_hearings_docket_time_goals"]');
+    let leverDistPrior = wrapper.find('input[name="ama_hearings_start_distribution_prior_to_goals"]');
+
+    waitFor(() => expect(leverTimeGoal).toHaveTextContent(testTimeGoalLever.value));
+    waitFor(() => expect(leverDistPrior).toHaveTextContent(testDistPriorLever.value));
   });
 
-
-  it.skip('sets input of Time Goal Lever to invalid for error and sets input to valid to remove error', () => {
+  it('sets input of Time Goal Lever to invalid for error and sets input to valid to remove error', () => {
     const eventForError = { target: { value: 1234 } };
     const eventForValid = { target: { value: 30 } };
 
@@ -94,7 +91,7 @@ describe('Docket Time Goals Lever', () => {
 
     waitFor(() => expect(inputField.prop('value').toBe(eventForError.target.value)));
     waitFor(() => expect(inputField.prop('errorMessage').
-      toBe(`Please enter a value greater than or equal to ${ testTimeGoalLever.min_value }`)));
+      toBe('Please enter a value from 0 to 999')));
 
     // Calls simulate change to set value within min/max range
     waitFor(() => inputField.simulate('change', eventForValid));
@@ -105,39 +102,23 @@ describe('Docket Time Goals Lever', () => {
     waitFor(() => expect(inputField.prop('errorMessage').toBe('')));
   });
 
-  it.skip('enabled and disables the toggle lever button', () => {
-    const eventForError = { target: { value: 1234 } };
-    const eventForValid = { target: { value: 30 } };
-
+  it('enabled and disables the toggle lever button', () => {
     const store = getStore();
 
     store.dispatch(loadLevers(levers));
     store.dispatch(setUserIsAcdAdmin(true));
 
-    let wrapper = mount(
+    const wrapper = mount(
       <Provider store={store}>
         <DocketTimeGoals />
-      </Provider>
-    );
-
-    const wrapper = mount(      <Provider store={store}>
-        <DocketTimeGoals />
       </Provider>);
-    const instance = wrapper.instance();
 
-    const instance = wrapper.instance();
-    // Toggle Lever
-    instance.toggleLever(0);
+    let leverToggle = wrapper.find(`#toggle-switch-${testDistPriorLever.item} > .toggleButton`);
+
+    waitFor(() => userEvent.click(leverToggle));
+
     wrapper.update();
 
-    // waitFor(() => expect(inputField.prop('value').toBe(eventForError.target.value)));
-
-    // wrapper.instance().toggleLever(0);
-    // wrapper.update();
-
-    // waitFor(() => expect(inputField.prop('value').toBe(eventForValid.target.value)));
-
+    expect(wrapper).toBeDefined();
   });
-
-
 });
