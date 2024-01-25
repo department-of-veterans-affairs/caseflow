35d08ead31aa30313b6312eaf9e9b9e4580fe810
Craig Reese <Reese_Craig@bah.com>
2024-01-25 12:45:59 -0600

    added nonpriority iterations to until clause in requested_distribution

--- app/models/concerns/by_docket_date_distribution.rb
+++ app/models/concerns/by_docket_date_distribution.rb
@@ -28,7 +28,10 @@ module ByDocketDateDistribution
 
     unless FeatureToggle.enabled?(:acd_disable_nonpriority_distributions, user: RequestStore.store[:current_user])
       # Distribute the oldest nonpriority appeals from any docket if we haven't distributed {batch_size} appeals
-      distribute_nonpriority_appeals_from_all_dockets_by_age_to_limit(@rem) until @rem <= 0
+      # @nonpriority_iterations guards against an infinite loop if not enough cases are ready to distribute
+      until @rem <= 0 || @nonpriority_iterations >= 10
+        distribute_nonpriority_appeals_from_all_dockets_by_age_to_limit(@rem)
+      end
     end
     @appeals
   end
