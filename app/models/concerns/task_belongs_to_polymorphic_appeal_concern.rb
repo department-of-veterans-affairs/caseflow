0e4e6e8296f1272adbfe9bc856149045d751aa04
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-24 19:06:52 -0500

    🔥 Task: Remove unused method overrides

--- app/models/concerns/task_belongs_to_polymorphic_appeal_concern.rb
+++ app/models/concerns/task_belongs_to_polymorphic_appeal_concern.rb
@@ -10,37 +10,17 @@ module TaskBelongsToPolymorphicAppealConcern
                -> { includes(:tasks).where(tasks: { appeal_type: "Appeal" }) },
                class_name: "Appeal", foreign_key: "appeal_id", optional: true
 
-    def ama_appeal
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "Appeal"
-    end
-
     belongs_to :legacy_appeal,
                -> { includes(:tasks).where(tasks: { appeal_type: "LegacyAppeal" }) },
                class_name: "LegacyAppeal", foreign_key: "appeal_id", optional: true
-
-    def legacy_appeal
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "LegacyAppeal"
-    end
 
     belongs_to :higher_level_review,
                -> { includes(:tasks).where(tasks: { appeal_type: "HigherLevelReview" }) },
                class_name: "HigherLevelReview", foreign_key: "appeal_id", optional: true
 
-    def higher_level_review
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "HigherLevelReview"
-    end
-
     belongs_to :supplemental_claim,
                -> { includes(:tasks).where(tasks: { appeal_type: "SupplementalClaim" }) },
                class_name: "SupplementalClaim", foreign_key: "appeal_id", optional: true
-
-    def supplemental_claim
-      # `super()` will call the method created by the `belongs_to` above
-      super() if appeal_type == "SupplementalClaim"
-    end
 
     scope :ama, -> { where(appeal_type: "Appeal") }
     scope :legacy, -> { where(appeal_type: "LegacyAppeal") }
