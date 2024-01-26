c5ba4bdd6c57678046ce847b49f6c8610d4eb998
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-26 13:01:58 -0500

    ✅ VbmsUploadedDocument: Add test harness for 'belongs_to polymorphic appeal'

---
+++ spec/support/shared_examples/models/shared_examples_for_vbms_uploaded_document_belongs_to_polymorphic_appeal.rb
@@ -0,0 +1,206 @@
+# frozen_string_literal: true
+
+require "query_subscriber"
+
+shared_examples "VbmsUploadedDocument belongs_to polymorphic appeal" do
+  context do
+    context "'appeal'-related associations" do
+      it { should belong_to(:appeal) }
+      it { should belong_to(:ama_appeal).class_name("Appeal").with_foreign_key(:appeal_id).optional }
+      it { should belong_to(:legacy_appeal).class_name("LegacyAppeal").with_foreign_key(:appeal_id).optional }
+
+      describe "ama_appeal" do
+        context "when used in `joins` query" do
+          subject { VbmsUploadedDocument.joins(:ama_appeal) }
+
+          # Create records having different `appeal_type` but the same `appeal_id`. This will ensure the test fails in
+          # the case where the `joins` result contains duplicate entries for records having the same `appeal_id` but
+          # different `appeal_type`.
+          let(:shared_id) { 99999 }
+          let!(:_legacy_vbms_uploaded_document) do
+            create(:vbms_uploaded_document, appeal: create(:legacy_appeal, vacols_case: create(:case), id: shared_id))
+          end
+
+          context "when there are no VbmsUploadedDocument with AMA appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with AMA appeals" do
+            let!(:ama_vbms_uploaded_document) { create(:vbms_uploaded_document, appeal: create(:appeal, id: shared_id)) }
+
+            it { should contain_exactly(ama_vbms_uploaded_document) }
+          end
+        end
+
+        context "when eager loading with `includes`" do
+          subject { VbmsUploadedDocument.ama.includes(:ama_appeal) }
+
+          let!(:_legacy_vbms_uploaded_document) { create(:vbms_uploaded_document, :legacy) }
+
+          context "when there are no VbmsUploadedDocument with AMA appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with AMA appeals" do
+            let!(:ama_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 10, :ama) }
+
+            it { should contain_exactly(*ama_vbms_uploaded_documents) }
+
+            it "prevents N+1 queries" do
+              QuerySubscriber.new.tap do |subscriber|
+                subscriber.track { subject.map { |record| record.ama_appeal.id } }
+                expect(subscriber.queries.count).to eq 2
+              end
+            end
+          end
+        end
+
+        context "when eager loading with `preload`" do
+          subject { VbmsUploadedDocument.ama.preload(:ama_appeal) }
+
+          let!(:_legacy_vbms_uploaded_document) { create(:vbms_uploaded_document, :legacy) }
+
+          context "when there are no VbmsUploadedDocument with AMA appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with AMA appeals" do
+            let!(:ama_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 10, :ama) }
+
+            it { should contain_exactly(*ama_vbms_uploaded_documents) }
+
+            it "prevents N+1 queries" do
+              QuerySubscriber.new.tap do |subscriber|
+                subscriber.track { subject.map { |record| record.ama_appeal.id } }
+                expect(subscriber.queries.count).to eq 2
+              end
+            end
+          end
+        end
+
+        context "when called on an individual VbmsUploadedDocument" do
+          subject { vbms_uploaded_document.ama_appeal }
+
+          context "when the VbmsUploadedDocument is not associated with an AMA appeal" do
+            let(:vbms_uploaded_document) { create(:vbms_uploaded_document, :legacy) }
+
+            it { should be_nil }
+          end
+
+          context "when the VbmsUploadedDocument is associated with an AMA appeal" do
+            let(:vbms_uploaded_document) { create(:vbms_uploaded_document, appeal: ama_appeal) }
+            let(:ama_appeal) { create(:appeal) }
+
+            it { should eq(ama_appeal) }
+          end
+        end
+      end
+
+      describe "legacy_appeal" do
+        context "when used in `joins` query" do
+          subject { VbmsUploadedDocument.joins(:legacy_appeal) }
+
+          # Create records having different `appeal_type` but the same `appeal_id`. This will ensure the test fails in
+          # the case where the `joins` result contains duplicate entries for records having the same `appeal_id` but
+          # different `appeal_type`.
+          let(:shared_id) { 99999 }
+          let!(:_ama_vbms_uploaded_document) { create(:vbms_uploaded_document, appeal: create(:appeal, id: shared_id)) }
+
+          context "when there are no VbmsUploadedDocument with Legacy appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with Legacy appeals" do
+            let!(:legacy_vbms_uploaded_document) do
+              create(:vbms_uploaded_document, appeal: create(:legacy_appeal, vacols_case: create(:case), id: shared_id))
+            end
+
+            it { should contain_exactly(legacy_vbms_uploaded_document) }
+          end
+        end
+
+        context "when eager loading with `includes`" do
+          subject { VbmsUploadedDocument.legacy.includes(:legacy_appeal) }
+
+          let!(:_ama_vbms_uploaded_document) { create(:vbms_uploaded_document, :ama) }
+
+          context "when there are no VbmsUploadedDocument with Legacy appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with Legacy appeals" do
+            let!(:legacy_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 10, :legacy) }
+
+            it { should contain_exactly(*legacy_vbms_uploaded_documents) }
+
+            it "prevents N+1 queries" do
+              QuerySubscriber.new.tap do |subscriber|
+                subscriber.track { subject.map { |record| record.legacy_appeal.id } }
+                expect(subscriber.queries.count).to eq 2
+              end
+            end
+          end
+        end
+
+        context "when eager loading with `preload`" do
+          subject { VbmsUploadedDocument.legacy.preload(:legacy_appeal) }
+
+          let!(:_ama_vbms_uploaded_document) { create(:vbms_uploaded_document, :ama) }
+
+          context "when there are no VbmsUploadedDocument with Legacy appeals" do
+            it { should be_none }
+          end
+
+          context "when there are VbmsUploadedDocument with Legacy appeals" do
+            let!(:legacy_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 10, :legacy) }
+
+            it { should contain_exactly(*legacy_vbms_uploaded_documents) }
+
+            it "prevents N+1 queries" do
+              QuerySubscriber.new.tap do |subscriber|
+                subscriber.track { subject.map { |record| record.legacy_appeal.id } }
+                expect(subscriber.queries.count).to eq 2
+              end
+            end
+          end
+        end
+
+        context "when called on an individual VbmsUploadedDocument" do
+          subject { vbms_uploaded_document.legacy_appeal }
+
+          context "when the VbmsUploadedDocument is not associated with a Legacy appeal" do
+            let(:vbms_uploaded_document) { create(:vbms_uploaded_document, :ama) }
+
+            it { should be_nil }
+          end
+
+          context "when the VbmsUploadedDocument is associated with a Legacy appeal" do
+            let(:vbms_uploaded_document) { create(:vbms_uploaded_document, appeal: legacy_appeal) }
+            let(:legacy_appeal) { create(:legacy_appeal) }
+
+            it { should eq(legacy_appeal) }
+          end
+        end
+      end
+    end
+
+    context "'appeal'-related scopes" do
+      let!(:ama_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 2, :ama) }
+      let!(:legacy_vbms_uploaded_documents) { create_list(:vbms_uploaded_document, 2, :legacy) }
+
+      describe ".ama" do
+        it "returns only VbmsUploadedDocument belonging to AMA appeals" do
+          expect(VbmsUploadedDocument.ama).to be_an(ActiveRecord::Relation)
+          expect(VbmsUploadedDocument.ama).to contain_exactly(*ama_vbms_uploaded_documents)
+        end
+      end
+
+      describe ".legacy" do
+        it "returns only VbmsUploadedDocument belonging to Legacy appeals" do
+          expect(VbmsUploadedDocument.legacy).to be_an(ActiveRecord::Relation)
+          expect(VbmsUploadedDocument.legacy).to contain_exactly(*legacy_vbms_uploaded_documents)
+        end
+      end
+    end
+  end
+end
