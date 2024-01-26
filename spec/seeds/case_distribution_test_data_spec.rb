d4211a3a8e92759111f91ccf2e1cd03a74166f46
SHarshain <spoosa@razortalent.com>
2024-01-26 16:14:10 -0500

    APPEALS-38526. 100% test coverage for Case distribution test data seed

--- spec/seeds/case_distribution_test_data_spec.rb
+++ spec/seeds/case_distribution_test_data_spec.rb
@@ -20,6 +20,24 @@ describe Seeds::CaseDitributionTestData do
     end
   end
 
+  context "initial values when veterans are found" do
+    before do
+      Veteran.create!(file_number: "703000201", participant_id: "713000000")
+      Veteran.create!(file_number: "706000201", participant_id: "716000000")
+      Veteran.create!(file_number: "709000201", participant_id: "719000000")
+    end
+
+    it "are set properly for incremented file_number and participant id" do
+      test_data_seed = Seeds::CaseDitributionTestData.new
+      expect(test_data_seed.instance_variable_get(:@legacy_inactive_admin_judge_team_file_number)).to eq 703_002_200
+      expect(test_data_seed.instance_variable_get(:@legacy_inactive_admin_judge_team_participant_id)).to eq 713_002_000
+      expect(test_data_seed.instance_variable_get(:@direct_review_file_number)).to eq 706_002_200
+      expect(test_data_seed.instance_variable_get(:@direct_review_participant_id)).to eq 716_002_000
+      expect(test_data_seed.instance_variable_get(:@ama_hearing_held_file_number)).to eq 709_002_200
+      expect(test_data_seed.instance_variable_get(:@ama_hearing_held_participant_id)).to eq 719_002_000
+    end
+  end
+
   context "#seed!" do
     it "creates test data for case distribution" do
       seed.seed!
@@ -28,10 +46,10 @@ describe Seeds::CaseDitributionTestData do
       expect( CaseDistributionLever.count).to eq 20
 
       expect(Appeal.where(docket_type: "direct_review").count).to eq 38
-      expect(Appeal.where(docket_type: "direct_review").first.receipt_date).to eq Date.strptime("01/21/2004","%m/%d/%Y")
+      expect(Appeal.where(docket_type: "direct_review").first.receipt_date).to eq (Date.today - (20.years + 1.day))
 
       expect(Appeal.where(docket_type: "hearing").count).to eq 35
-      expect(Appeal.where(docket_type: "hearing").first.receipt_date).to eq Date.strptime("01/19/2004","%m/%d/%Y")
+      expect(Appeal.where(docket_type: "hearing").first.receipt_date).to eq (Date.today- (20.years + 3.days))
 
       expect(AppealState.where(appeal_type: "Appeal").count).to eq 73
       expect(Claimant.count).to eq 73
@@ -49,6 +67,8 @@ describe Seeds::CaseDitributionTestData do
 
       expect(Veteran.where(first_name: "Bob").first.file_number).to eq "709000201"
       expect(Veteran.where(first_name: "Bob").first.participant_id).to eq "719000001"
+      expect(Veteran.where(first_name: "Bob").last.file_number).to eq "706000238"
+      expect(Veteran.where(first_name: "Bob").last.participant_id).to eq "716000038"
       expect(Veteran.count).to eq 108
     end
   end
