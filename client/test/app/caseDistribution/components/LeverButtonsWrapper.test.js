901c589cc38fa8b9b203d5f48cf79fab8095b7cb
Michael Beard <michael.beard@va.gov>
2024-01-26 12:46:13 -0600

    "adds test cases  for save and cancel buttons"

--- client/test/app/caseDistribution/components/LeverButtonsWrapper.test.js
+++ client/test/app/caseDistribution/components/LeverButtonsWrapper.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import LeverButtonsWrapper from 'app/caseDistribution/components/LeverButtonsWrapper';
 import { Provider } from 'react-redux';
 import rootReducer from 'app/caseDistribution/reducers/root';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import leversReducer from 'app/caseDistribution/reducers/levers/leversReducer';
-import { ACTIONS } from 'app/caseDistribution/reducers/levers/leversActionTypes';
-import * as leverData from 'test/data/adminCaseDistributionLevers';
+import * as leversSelectors from 'app/caseDistribution/reducers/levers/leversSelector';
+import { resetLevers } from 'app/caseDistribution/reducers/levers/leversActions';
+import fetchMock from 'jest-fetch-mock';
+
 
 describe('LeverButtonsWrapper', () => {
+
+  const getStore = () => createStore(
+    rootReducer,
+    applyMiddleware(thunk));
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  test('renders save and cancel buttons', () => {
-
-    const getStore = () => createStore(
-      rootReducer,
-      applyMiddleware(thunk));
-
+  it('renders save and cancel buttons', () => {
     const store = getStore();
 
     render(
@@ -36,5 +36,57 @@ describe('LeverButtonsWrapper', () => {
     const cancelButton = screen.getByRole('button', { name: /Cancel/i });
 
     expect(cancelButton).toBeInTheDocument();
+  });
+
+  test('Save Button be disabled initially', () => {
+    const store = getStore();
+
+    render(
+      <Provider store={store}>
+        <LeverButtonsWrapper />
+      </Provider>
+    );
+
+    expect(screen.getByText('Save')).toBeDisabled();
+  });
+
+  it('activates Save Button when conditions are met', () => {
+    const store = getStore();
+
+    const changedLeversData = [
+      { title: 'Alternate Batch Size*',
+        backendValue: '50',
+        value: '15',
+        data_type: 'number' },
+    ];
+
+    jest.spyOn(leversSelectors, 'hasChangedLevers').mockReturnValue(changedLeversData);
+
+    render(
+      <Provider store={store}>
+        <LeverButtonsWrapper />
+      </Provider>
+    );
+
+    expect(screen.getByText('Save')).not.toBeDisabled();
+  });
+
+  test('clicking the Cancel Button resets levers to their initial state', async () => {
+    const store = getStore();
+    const resetLeversAction = resetLevers();
+
+    render(
+      <Provider store={store}>
+        <LeverButtonsWrapper />
+      </Provider>
+    );
+
+    const cancelButton = screen.getByRole('button', { name: /Cancel/i });
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(resetLeversAction);
+    });
   });
 });
