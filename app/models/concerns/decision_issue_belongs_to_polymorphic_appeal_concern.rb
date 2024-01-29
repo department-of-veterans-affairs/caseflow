b6bc482af17005b32a9916fe729affa2d2bf85e9
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-29 17:59:09 -0500

    🔥 DecisionIssue: Remove unused method overrides

--- app/models/concerns/decision_issue_belongs_to_polymorphic_appeal_concern.rb
+++ app/models/concerns/decision_issue_belongs_to_polymorphic_appeal_concern.rb
@@ -10,28 +10,13 @@ module DecisionIssueBelongsToPolymorphicAppealConcern
                -> { includes(:decision_issues).where(decision_issues: { decision_review_type: "Appeal" }) },
                class_name: "Appeal", foreign_key: "decision_review_id", optional: true
 
-    def ama_appeal
-      # `super()` will call the method created by the `belongs_to` above
-      super() if decision_review_type == "Appeal"
-    end
-
     belongs_to :higher_level_review,
                -> { includes(:decision_issues).where(decision_issues: { decision_review_type: "HigherLevelReview" }) },
                class_name: "HigherLevelReview", foreign_key: "decision_review_id", optional: true
 
-    def higher_level_review
-      # `super()` will call the method created by the `belongs_to` above
-      super() if decision_review_type == "HigherLevelReview"
-    end
-
     belongs_to :supplemental_claim,
                -> { includes(:decision_issues).where(decision_issues: { decision_review_type: "SupplementalClaim" }) },
                class_name: "SupplementalClaim", foreign_key: "decision_review_id", optional: true
-
-    def supplemental_claim
-      # `super()` will call the method created by the `belongs_to` above
-      super() if decision_review_type == "SupplementalClaim"
-    end
 
     scope :ama, -> { where(decision_review_type: "Appeal") }
     scope :higher_level_review, -> { where(decision_review_type: "HigherLevelReview") }
