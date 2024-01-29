6a59bc92995a1d5cd63ce21848a69c199045e6c3
sulak-j <justin.sulak@va.gov>
2024-01-29 17:18:42 -0600

    adding efolder ulpoad failed logic for routing

--- app/models/tasks/efolder_upload_failed_task.rb
+++ app/models/tasks/efolder_upload_failed_task.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class EfolderUploadFailedTask < ReviewPackageTask
+
+  def task_url
+    if self.parent.is_a?(ReviewPackageTask)
+      "/queue/correspondence/#{self.correspondence.uuid}/review_package"
+    elseif self.parent.is_a?(CorrespondenceIntakeTask)
+      "/queue/correspondence/#{self.correspondence.uuid}/intake"
+    end
+  end
 end
