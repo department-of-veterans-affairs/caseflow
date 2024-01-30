3898bddbb97b97eb454ded4b5aed85f7ad102faf
Prajwal Amatya <pamatya@take2it.com>
2024-01-30 04:52:02 -0700

    adding serializer and serializer spec for change history

---
+++ app/models/serializers/work_queue/decision_review_change_history_serializer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class WorkQueue::DecisionReviewChangeHistorySerializer
+  include FastJsonapi::ObjectSerializer
+
+  set_id { SecureRandom.uuid }
+  attribute :taskID, &:task_id
+  attribute :eventType, &:event_type
+  attribute :eventUser, &:readable_user_name
+  attribute :eventDate, &:event_date
+  attribute :claimType, &:readable_claim_type
+  attribute :readableEventType, &:readable_event_type
+  attribute :claimantName, &:claimant_name
+
+  attribute :detail do |object|
+    {
+      benefitType: object.benefit_type,
+      issueType: object.issue_type,
+      issueDescription: object.issue_description,
+      decisionDate: object.decision_date,
+      disposition: object.disposition,
+      decisionDescription: object.decision_description,
+      withdrawlRequestDate: object.withdrawal_request_date
+    }
+  end
+end
