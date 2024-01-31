839f6dd044fbbe42695648b6b5de96e069953e5e
sulak-j <justin.sulak@va.gov>
2024-01-31 17:26:11 -0600

    attempting a correspondence root task addition

--- app/models/tasks/correspondence_root_task.rb
+++ app/models/tasks/correspondence_root_task.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class CorrespondenceRootTask < CorrespondenceTask
+  # if self.status=="on_hold"
+  #   def task_url
+  #     "/test"
+  #   end
+  # end
+  def task_url
+    # this returns nil if the task status is not completed... unsure what is expected behavior
+    if self.status=="completed"
+      "/under_construction"
+    end
+  end
 end
