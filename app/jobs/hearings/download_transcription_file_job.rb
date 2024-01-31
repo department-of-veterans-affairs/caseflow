b90a1b356ca150d79bd8645a4f04cffb716a218f
Jeff Marks <jeff.t.marks@gmail.com>
2024-01-31 15:50:50 -0500

    APPEALS-38907 Implement S3 upload

--- app/jobs/hearings/download_transcription_file_job.rb
+++ app/jobs/hearings/download_transcription_file_job.rb
@@ -10,19 +10,38 @@ class DownloadTranscriptionFileJob < CaseflowJob
   queue_with_priority :low_priority
 
   class TranscriptionFileNameError < StandardError; end
+  class HearingAssociationError < StandardError; end
 
-  retry_on(OpenURI::HTTPError, wait: :exponentially_longer) do |job, exception|
+  retry_on(OpenURI::HTTPError, attempts: 10, wait: :exponentially_longer) do |job, exception|
     Rails.logger.error("#{job.class.name} (#{job.job_id}) failed with error: #{exception}")
 
-    # Raven.capture_exception(exception, extra: extra)
-    # Send email to VA?
+    file_name = job.arguments.first[:file_name]
+    hearing_id = file_name.split("_")[1]
+    extra = {
+      application: job.class.app_name.to_s,
+      hearing_id: hearing_id,
+      file_name: file_name,
+      job_id: job.job_id
+    }
+    Raven.capture_exception(exception, extra: extra)
+
+    # TO-DO
+    # send_error_email_to_va_ops(exception)
   end
 
   discard_on(TranscriptionFileNameError) do |job, exception|
-    Rails.logger.warn("Discarding #{job.class.name} (#{job.job_id})failed with error: #{exception}")
+    Rails.logger.warn("Discarding #{job.class.name} (#{job.job_id}) failed with error: #{exception}")
+
+    # TO-DO
+    # send_error_email_to_va_ops(exception)
   end
 
   VALID_FILE_TYPES = %w[mp4 vtt].freeze
+
+  S3_BUCKET_NAMES = {
+    mp4: "transcript_audio",
+    vtt: "transcript_raw"
+  }.freeze
 
   # Purpose: Downloads audio (mp4) or transcript (vtt) file from temporary download link provided by
   #          GetRecordingDetailsJob
@@ -32,10 +51,14 @@ class DownloadTranscriptionFileJob < CaseflowJob
   def perform(download_link:, file_name:)
     ensure_current_user_is_set
     @file_name = file_name
+    ensure_hearing_held
     @transcription_file = find_or_create_transcription_file
 
     begin
-      download_to_tmp(download_link)
+      download_file_to_tmp(download_link)
+      upload_file_to_s3
+      # queue_transform_job
+      byebug
     ensure
       clean_up_tmp_location
     end
@@ -52,14 +75,14 @@ class DownloadTranscriptionFileJob < CaseflowJob
   #         file_name - string, to be parsed for appeal/hearing identifiers
   #
   # Returns: nil if successful, OpenURI::HTTPError if failure
-  def download_to_tmp(link)
+  def download_file_to_tmp(link)
     begin
       URI.open(link) do |download|
         IO.copy_stream(download, tmp_location)
       end
-      update_file_status_after_download(status: :success, date: Time.zone.now)
+      update_file_after_download(status: :success, date: Time.zone.now)
     rescue OpenURI::HTTPError => error
-      update_file_status_after_download(status: :failure)
+      update_file_after_download(status: :failure)
       raise error
     end
   end
@@ -69,6 +92,34 @@ class DownloadTranscriptionFileJob < CaseflowJob
   # Returns: string, directory path
   def tmp_location
     File.join(Rails.root, "tmp", "transcription_files", file_type, file_name)
+  end
+
+  def upload_file_to_s3
+    begin
+      S3Service.store_file(s3_bucket, tmp_location, :filepath)
+      update_file_after_upload(status: :success, aws_link: s3_file_location, date: Time.zone.now)
+    rescue StandardError => error
+      update_file_after_upload(status: :failure)
+      raise error
+    end
+  end
+
+  def s3_bucket
+    S3_BUCKET_NAMES[file_type.to_sym]
+  end
+
+  def s3_file_location
+    s3_bucket + "/" + file_name
+  end
+
+  def queue_transform_job
+    job = case file_type
+          when "mp4"
+            TransformMp4ToMp3Job
+          when "vtt"
+            TransformVttToRtfJob
+          end
+    job.perform(s3_file_location)
   end
 
   # Purpose: Removes temporary file (if it exists) from tmp folder after job success/fails
@@ -91,52 +142,56 @@ class DownloadTranscriptionFileJob < CaseflowJob
   #
   # Returns: boolean
   def file_type_invalid?
-    return unless VALID_FILE_TYPES.exclude?(file_type)
+    return false if VALID_FILE_TYPES.include?(file_type)
 
     fail TranscriptionFileNameError, "Invalid file type"
   end
 
+  # Purpose: Hearing for which the transcription was created
+  #
+  # Returns: Hearing object
+  def hearing
+    @hearing ||= parse_hearing
+  end
+
+  # Purpose: Parses hearing details from identifiers present in file name
+  #
+  # Returns: Hearing object or error if hearing not able to be found
+  def parse_hearing
+    begin
+      hearing_details = file_name.split(".").first
+      hearing_id = hearing_details.split("_")[1]
+      hearing_type = hearing_details.split("_")[2]
+      hearing_type.constantize.find(hearing_id)
+    rescue StandardError
+      raise TranscriptionFileNameError, "File name missing sufficient hearing identifiers"
+    end
+  end
+
+  # Purpose: If disposition of hearing is not marked as held, sends email to VA Operations Team and continues with
+  #          download
+  def ensure_hearing_held
+    return if hearing.held?
+
+    # TO-DO
+    # send_error_email_to_va_ops(HearingAssociationError.new("Hearing disposition not set to held"))
+  end
+
   # Purpose: Appeal associated with the hearing for which the transcription was created
   #
-  # Returns: Appeal object or nil
+  # Returns: Appeal object
   def appeal
-    @appeal ||= parse_appeal if appeal_attributes_present?
-  end
-
-  # Purpose: Determines if file name includes sufficient identifiers to parse appeal
-  #
-  # Returns: boolean
-  def appeal_attributes_present?
-    file_name.include?("Appeal")
-  end
-
-  # Purpose: Parses appeal details from identifiers present in file name
-  #
-  # Returns: Appeal object or error if appeal not able to be found
-  def parse_appeal
-    begin
-      appeal_info = file_name.split(".").first
-      appeal_id = appeal_info.split("_")[1]
-      appeal_type = appeal_info.split("_")[2]
-      appeal_type.constantize.find(appeal_id)
-    rescue StandardError
-      raise TranscriptionFileNameError, "File name missing sufficient appeal/hearing identifiers"
-    end
+    @appeal ||= hearing.appeal
   end
 
   # Purpose: Docket number associated with the hearing for which the transcription was created
   #
   # Returns: string or error
   def docket_number
-    appeal&.docket_number || docket_number_from_hearing_day
+    appeal&.docket_number
   end
 
-  # TO-DO: How to implement parsing docket number from hearing day?
-  def docket_number_from_hearing_day
-    fail NotImplementedError
-  end
-
-  # Purpose: If job previously failed and retry initiated, finds existing transcription file record. Otherewise, create
+  # Purpose: Finds existing transcription file record if job previously failed and retry initiated. Otherewise, creates
   #          new record.
   #
   # Returns: TranscriptionFile object
@@ -152,14 +207,26 @@ class DownloadTranscriptionFileJob < CaseflowJob
     end
   end
 
-  # Purpose: Update sthe @transcription_file with success or failure status after download complets. If download
-  #          successful, update date_receipt_webex.
+  # Purpose: Updates the @transcription_file with success or failure status after download completes. If download
+  #          successful, updates date_receipt_webex.
   #
   # Returns: TranscriptionFile object
-  def update_file_status_after_download(status:, date: nil)
+  def update_file_after_download(status:, date: nil)
     @transcription_file.update!(
       file_status: Constants.TRANSCRIPTION_FILE_STATUSES.retrieval.send(status),
       date_receipt_webex: date
+    )
+  end
+
+  # Purpose: Updates the @transcription_file with success or failure status after upload to s3 completes. If upload
+  #          successful, updates date_upload_aws and aws_link.
+  #
+  # Returns: TranscriptionFile object
+  def update_file_after_upload(status:, aws_link: nil, date: nil)
+    @transcription_file.update!(
+      file_status: Constants.TRANSCRIPTION_FILE_STATUSES.upload.send(status),
+      aws_link: aws_link,
+      date_upload_aws: date
     )
   end
 end
