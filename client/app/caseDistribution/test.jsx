ca50d36f0e0adf1714b59ae920426bcd8c42072b
Matthew Roth <roth_matthew@bah.com>
2024-01-31 13:10:36 -0500

    Update test.jsx

--- client/app/caseDistribution/test.jsx
+++ client/app/caseDistribution/test.jsx
@@ -7,7 +7,7 @@ import PageRoute from '../components/PageRoute';
 import AppFrame from '../components/AppFrame';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import { LOGO_COLORS } from '../constants/AppConstants';
-import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer';import CaseSearchLink from '../components/CaseSearchLink';
+import Footer from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Footer'; import CaseSearchLink from '../components/CaseSearchLink';
 
 import CaseDistributionApp from './pages/CaseDistributionApp';
 
@@ -16,8 +16,8 @@ class CaseDistributionTest extends React.PureComponent {
     const Router = this.props.router || BrowserRouter;
     const appName = 'Case Distribution';
     const tablestyle = {
-      display: "block",
-      overflowX: "scroll"
+      display: 'block',
+      overflowX: 'scroll'
     };
 
     return (
@@ -45,12 +45,12 @@ class CaseDistributionTest extends React.PureComponent {
                 <div>
                   <PageRoute
                     exact
-                    path={'/acd-controls/test'}
+                    path="/acd-controls/test"
                     title="Case Distribution | Caseflow"
                     component={() => {
                       return (
                         <div>
-                          <h1 id='top'>Case Distribution Levers Dashboard</h1>
+                          <h1 id="top">Case Distribution Levers Dashboard</h1>
                           <p>This page is for lower env use only and provides a convient place to review Case Distribution related data</p>
                           <h2>Navigation</h2>
                           <ul>
@@ -61,33 +61,33 @@ class CaseDistributionTest extends React.PureComponent {
                               <a href="#audit"><button className="btn btn-primary">Case Distribution Audit Levers</button></a>
                             </li>
                             <li>
-                             <a href="#distribution_status"><button className="btn btn-primary">Distribution Status</button></a>
+                              <a href="#distribution_status"><button className="btn btn-primary">Distribution Status</button></a>
                             </li>
                           </ul>
-                          <h2 id='levers'> Case Distribution Levers </h2>
-                            <table id="case-table-description" className="usa-table-borderless undefined" style={tablestyle}>
-                              <thead>
-                                <tr>
-                                  <td> ID </td>
-                                  <td> Title </td>
-                                  <td> Item </td>
-                                  <td> Control Group </td>
-                                  <td> Lever Group </td>
-                                  <td> Lever Group Order </td>
-                                  <td> Description </td>
-                                  <td> Data Type </td>
-                                  <td> Value </td>
-                                  <td> Min / Max Value </td>
-                                  <td> Unit </td>
-                                  <td> Option Values</td>
-                                  <td> Toggle Active </td>
-                                  <td> Disabled In UI</td>
-                                  <td> Algorithms Used </td>
-                                  <td> Created At </td>
-                                  <td> Updated At </td>
-                                </tr>
-                              </thead>
-                              <tbody>
+                          <h2 id="levers"> Case Distribution Levers </h2>
+                          <table id="case-table-description" className="usa-table-borderless undefined" style={tablestyle}>
+                            <thead>
+                              <tr>
+                                <td> ID </td>
+                                <td> Title </td>
+                                <td> Item </td>
+                                <td> Control Group </td>
+                                <td> Lever Group </td>
+                                <td> Lever Group Order </td>
+                                <td> Description </td>
+                                <td> Data Type </td>
+                                <td> Value </td>
+                                <td> Min / Max Value </td>
+                                <td> Unit </td>
+                                <td> Option Values</td>
+                                <td> Toggle Active </td>
+                                <td> Disabled In UI</td>
+                                <td> Algorithms Used </td>
+                                <td> Created At </td>
+                                <td> Updated At </td>
+                              </tr>
+                            </thead>
+                            <tbody>
                               {this.props.acdLevers.map((lever, i) => {
                                 return [
                                   <tr>
@@ -103,18 +103,18 @@ class CaseDistributionTest extends React.PureComponent {
                                     <td> { lever.min_value }/{lever.max_value } </td>
                                     <td> { lever.unit } </td>
                                     <td> { lever.options?.map((option) => option.value).join(', ') } </td>
-                                    <td> { lever.is_toggle_active.toString() } </td>
-                                    <td> { lever.is_disabled_in_ui.toString()  } </td>
+                                    <td> { lever.is_toggle_active?.toString() } </td>
+                                    <td> { lever.is_disabled_in_ui?.toString() } </td>
                                     <td> { lever.algorithms_used } </td>
                                     <td> { lever.created_at } </td>
                                     <td> { lever.updated_at } </td>
                                   </tr>
                                 ];
                               })}
-                              </tbody>
-                            </table>
-                          <hr/>
-                          <h2 id='audit'> Case Distribution Audit Levers </h2>
+                            </tbody>
+                          </table>
+                          <hr />
+                          <h2 id="audit"> Case Distribution Audit Levers </h2>
                           <table>
                             <thead>
                               <tr>
@@ -131,8 +131,8 @@ class CaseDistributionTest extends React.PureComponent {
                               </tr>
                             </thead>
                             <tbody>
-                            {this.props.acdHistory.map((entry, i) => {
-                              return [
+                              {this.props.acdHistory.map((entry, i) => {
+                                return [
                                   <tr>
                                     <td> { entry.id } </td>
                                     <td> { entry.case_distribution_lever_id } </td>
@@ -145,13 +145,13 @@ class CaseDistributionTest extends React.PureComponent {
                                     <td> { entry.lever_data_type } </td>
                                     <td> { entry.lever_unit } </td>
                                   </tr>
-                              ];
-                            })}
+                                ];
+                              })}
 
                             </tbody>
                           </table>
-                          <hr/>
-                          <h2 id='distribution_status'>Distribution Status</h2>
+                          <hr />
+                          <h2 id="distribution_status">Distribution Status</h2>
                           <ul>
                             <li>
                               <a href="/appeals-ready-to-distribute?csv=1"><button className="btn btn-primary">Download Appeals Ready to Distribute CSV</button></a>
@@ -160,7 +160,7 @@ class CaseDistributionTest extends React.PureComponent {
                               <a href="/appeals-distributed?csv=1"><button className="btn btn-primary">Download Distributed Appeals CSV</button></a>
                             </li>
                           </ul>
-                          <hr/>
+                          <hr />
                           <a href="#top"><button className="btn btn-primary">Back to Top</button></a>
                         </div>
                       );
