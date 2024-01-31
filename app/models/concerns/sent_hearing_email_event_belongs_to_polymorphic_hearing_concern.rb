32716ba9b97541ce98fca1f9dee2f1ef8c78ceba
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-31 17:42:50 -0500

    🔥 SentHearingEmailEvent: Remove unused method overrides

--- app/models/concerns/sent_hearing_email_event_belongs_to_polymorphic_hearing_concern.rb
+++ app/models/concerns/sent_hearing_email_event_belongs_to_polymorphic_hearing_concern.rb
@@ -10,19 +10,9 @@ module SentHearingEmailEventBelongsToPolymorphicHearingConcern
                -> { includes(:email_events).where(sent_hearing_email_events: { hearing_type: "Hearing" }) },
                class_name: "Hearing", foreign_key: "hearing_id", optional: true
 
-    def ama_hearing
-      # `super()` will call the method created by the `belongs_to` above
-      super() if hearing_type == "Hearing"
-    end
-
     belongs_to :legacy_hearing,
                -> { includes(:email_events).where(sent_hearing_email_events: { hearing_type: "LegacyHearing" }) },
                class_name: "LegacyHearing", foreign_key: "hearing_id", optional: true
-
-    def legacy_hearing
-      # `super()` will call the method created by the `belongs_to` above
-      super() if hearing_type == "LegacyHearing"
-    end
 
     scope :ama, -> { where(hearing_type: "Hearing") }
     scope :legacy, -> { where(hearing_type: "LegacyHearing") }
