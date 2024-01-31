cf38a85f713f4f049ecdee5250781e501dbaf8aa
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-01-31 17:17:37 -0500

    ♻️ SentHearingEmailEvent: Rename factory traits for consistency

--- spec/models/concerns/belongs_to_polymorphic_hearing_concern_spec.rb
+++ spec/models/concerns/belongs_to_polymorphic_hearing_concern_spec.rb
@@ -3,14 +3,14 @@
 require "query_subscriber"
 
 describe BelongsToPolymorphicHearingConcern do
-  let!(:sent_hearing_email_event) { create(:sent_hearing_email_event, :with_hearing) }
-  let!(:legacy_sent_hearing_email_event) { create(:sent_hearing_email_event, hearing: create(:legacy_hearing)) }
+  let!(:ama_sent_hearing_email_event) { create(:sent_hearing_email_event, :ama) }
+  let!(:legacy_sent_hearing_email_event) { create(:sent_hearing_email_event, :legacy) }
   let!(:hearing_email_recipient) { create(:hearing_email_recipient, hearing: create(:hearing)) }
   let!(:legacy_hearing_email_recipient) { create(:hearing_email_recipient, hearing: create(:legacy_hearing)) }
 
   context "concern is included in SentHearingEmailEvent" do
     it "`ama_hearing` returns the AMA hearing" do
-      expect(sent_hearing_email_event.ama_hearing).to eq sent_hearing_email_event.hearing
+      expect(ama_sent_hearing_email_event.ama_hearing).to eq ama_sent_hearing_email_event.hearing
     end
 
     it "`legacy_hearing` returns the legacy hearing" do
