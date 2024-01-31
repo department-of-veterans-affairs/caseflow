16bf583d8306e02a863c0f56b37ebd12e0470d1d
sulak-j <justin.sulak@va.gov>
2024-01-31 15:32:02 -0600

    adding efolder failed upload task depending on parent task

--- app/models/tasks/efolder_upload_failed_task.rb
+++ app/models/tasks/efolder_upload_failed_task.rb
@@ -5,7 +5,7 @@ class EfolderUploadFailedTask < ReviewPackageTask
   def task_url
     if self.parent.is_a?(ReviewPackageTask)
       "/queue/correspondence/#{self.correspondence.uuid}/review_package"
-    elseif self.parent.is_a?(CorrespondenceIntakeTask)
+    elsif self.parent.is_a?(CorrespondenceIntakeTask)
       "/queue/correspondence/#{self.correspondence.uuid}/intake"
     end
   end
