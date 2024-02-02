8f1bf0e690cb622dcc5f3459390acace3e3575a7
Jeff Marks <jeff.t.marks@gmail.com>
2024-02-02 11:32:00 -0500

    APPEALS-38907 Polish error handling before moving on to tests

--- app/jobs/hearings/download_transcription_file_job.rb
+++ app/jobs/hearings/download_transcription_file_job.rb
@@ -18,50 +18,46 @@ class DownloadTranscriptionFileJob < CaseflowJob
   class FileUploadError < StandardError; end
   class HearingAssociationError < StandardError; end
 
-  retry_on(FileDownloadError) do |job, exception|
-    extra = {
-      application: job.class.app_name.to_s,
-      hearing_id: job.hearing.id,
-      file_name: job.file_name,
-      job_id: job.job_id
-    }
-    log_error(exception, extra)
-
-    job.send_error_email_to_va_ops(exception)
-  end
-
-  retry_on(FileUploadError, wait: :exponentially_longer) do |job, exception|
-    File.delete(tmp_location) if File.exist?(tmp_location)
-
-    extra = {
-      application: job.class.app_name.to_s,
-      hearing_id: job.hearing.id,
-      file_name: job.file_name,
-      job_id: job.job_id
-    }
-    log_error(exception, extra)
-  end
-
-  discard_on(FileNameError) do |_job, exception|
-    send_error_email_to_va_ops(exception)
-  end
-
   VALID_FILE_TYPES = %w[mp3 mp4 vtt].freeze
 
   S3_SUB_BUCKET = "vaec-appeals-caseflow"
 
-  S3_SUBDIRECTORIES = {
+  S3_SUB_FOLDERS = {
     mp3: "transcript_audio",
     mp4: "transcript_audio",
     vtt: "transcript_raw"
   }.freeze
 
-  def initialize
-    @folder_name = (Rails.deploy_env == :prod) ? S3_SUB_BUCKET : "#{S3_SUB_BUCKET}-#{Rails.deploy_env}"
+  retry_on(FileDownloadError, wait: :exponentially_longer) do |job, exception|
+    Rails.logger.error("#{job.class.name} (#{job.job_id}) failed with error: #{exception}")
+    extra = {
+      application: job.class.app_name.to_s,
+      hearing_id: job.hearing.id,
+      file_name: job.file_name,
+      job_id: job.job_id
+    }
+    Raven.capture_exception(exception, extra: extra)
+    job.send_error_email_to_va_ops(exception)
   end
 
-  # Purpose: Downloads audio (mp3), video (mp4), or transcript (vtt) file from temporary download link provided by
-  #          GetRecordingDetailsJob
+  retry_on(FileUploadError, wait: :exponentially_longer) do |job, exception|
+    File.delete(tmp_location) if File.exist?(tmp_location)
+    Rails.logger.error("#{job.class.name} (#{job.job_id}) failed with error: #{exception}")
+    extra = {
+      application: job.class.app_name.to_s,
+      hearing_id: job.hearing.id,
+      file_name: job.file_name,
+      job_id: job.job_id
+    }
+    Raven.capture_exception(exception, extra: extra)
+  end
+
+  discard_on(FileNameError) do |job, exception|
+    Rails.logger.error("#{job.class.name} (#{job.job_id}) discarded with error: #{exception}")
+    job.send_error_email_to_va_ops(exception)
+  end
+
+  # Purpose: Downloads audio (mp3), video (mp4), or transcript (vtt) file from Webex temporary download link
   #
   # Params: download_link - string, URI for temporary download link
   #         file_name - string, to be parsed for appeal/hearing identifiers
@@ -75,13 +71,13 @@ class DownloadTranscriptionFileJob < CaseflowJob
     upload_file_to_s3
   end
 
-  # Purpose: Location of temporary file in tmp/transcription_files/<file_type> directory
+  # Purpose: Removes temporary file (if it exists) from tmp folder after job success/fails
   #
   # Note: Public method to provide access during job retry
   #
-  # Returns: string, directory path
-  def tmp_location
-    File.join(Rails.root, "tmp", "transcription_files", file_type, file_name)
+  # Returns: integer value of 1 if file deleted, nil if file not found
+  def clean_up_tmp_location
+    File.delete(tmp_location) if File.exist?(tmp_location)
   end
 
   # Purpose: Hearing for which the transcription was created  #
@@ -91,6 +87,14 @@ class DownloadTranscriptionFileJob < CaseflowJob
   # Returns: Hearing object
   def hearing
     @hearing ||= parse_hearing
+  end
+
+  # Purpose: Sends email to Va Operations Team when download fails or hearing disposition not set to held
+  #
+  # Note: Public method to provide access during job retry
+  def send_error_email_to_va_ops(_error)
+    # To Be Implemented
+    Rails.logger.info("Sending email to Va Operations Team...")
   end
 
   private
@@ -113,7 +117,7 @@ class DownloadTranscriptionFileJob < CaseflowJob
     rescue OpenURI::HTTPError => error
       clean_up_tmp_location
       update_file_after_download(status: :failure)
-      raise FileDownloadError, error.message
+      raise FileDownloadError, "Webex temporary download link responded with #{error.message}"
     end
   end
 
@@ -122,42 +126,44 @@ class DownloadTranscriptionFileJob < CaseflowJob
   # Returns: Updated @transcription_file
   def upload_file_to_s3
     begin
-      S3Service.store_file(s3_folder, tmp_location, :filepath)
+      S3Service.store_file(s3_location, tmp_location, :filepath)
       clean_up_tmp_location
-      update_file_after_upload(status: :success, aws_link: s3_file_location, date: Time.zone.now)
       log_info(s3_success_message)
+      update_file_after_upload(status: :success, aws_link: s3_location, date: Time.zone.now)
     rescue Aws::S3::Errors::ServiceError => error
+      # In case of S3 upload failure, don't delete file from tmp in case it can be retrieved on retry
       update_file_after_upload(status: :failure)
-      raise FileUploadError, error.message
+      raise FileUploadError, "Amazon S3 service responded with #{error.message}"
     end
   end
 
-  # Purpose: Bucket name in s3 depending on if file type mp3, mp4, or vtt
+  # Purpose: Location of temporary file in tmp/transcription_files/<file_type> folder
   #
-  # Returns: string, folder name
-  def s3_folder
-    @folder_name + "/" + S3_SUBDIRECTORIES[file_type.to_sym]
+  # Returns: string, folder path
+  def tmp_location
+    File.join(Rails.root, "tmp", "transcription_files", file_type, file_name)
   end
 
-  # Purpose: Location of successfully uploaded file in s3
+  # Purpose: Location of uploaded file in s3
   #
   # Returns: string, s3 filepath
-  def s3_file_location
-    s3_folder + "/" + file_name
+  def s3_location
+    folder_name + "/" + S3_SUB_FOLDERS[file_type.to_sym] + "/" + file_name
+  end
+
+  # Purpose: Folder name in s3 depending on Rails environment
+  #
+  # Returns: string, folder name
+  def folder_name
+    (Rails.deploy_env == :prod) ? S3_SUB_BUCKET : "#{S3_SUB_BUCKET}-#{Rails.deploy_env}"
   end
 
   # Purpose: Builds on successful upload to S3
   #
   # Returns: string
   def s3_success_message
-    "TranscriptionFile (id: #{@transcription_file.id}) successfully uploaded to S3 location: '#{s3_file_location}'"
-  end
-
-  # Purpose: Removes temporary file (if it exists) from tmp folder after job success/fails
-  #
-  # Returns: integer value of 1 if file deleted, nil if file not found
-  def clean_up_tmp_location
-    File.delete(tmp_location) if File.exist?(tmp_location)
+    "TranscriptionFile (id: #{@transcription_file.id}, file_name: '#{file_name}')" \
+    "successfully uploaded to S3 location: '#{@transcription_file.aws_link}'"
   end
 
   # Purpose: Either mp3 (audio), mp4 (video), or vtt (transcript)
@@ -170,10 +176,10 @@ class DownloadTranscriptionFileJob < CaseflowJob
   # Purpose: Determines if file type parsed from file name is valid
   #
   # Returns: boolean
-  def file_type_invalid?
-    return false if VALID_FILE_TYPES.include?(file_type)
+  def file_type_valid?
+    return true if VALID_FILE_TYPES.include?(file_type)
 
-    fail FileNameError, "Invalid file type"
+    fail FileNameError, "Invalid file type of '.#{file_type}'"
   end
 
   # Purpose: Parses hearing details from identifiers present in file name
@@ -186,7 +192,7 @@ class DownloadTranscriptionFileJob < CaseflowJob
       hearing_type = hearing_details.split("_")[2]
       hearing_type.constantize.find(hearing_id)
     rescue StandardError
-      raise FileNameError, "File name missing sufficient hearing identifiers"
+      raise FileNameError, "File name '#{file_name}' missing sufficient hearing identifiers"
     end
   end
 
@@ -195,9 +201,9 @@ class DownloadTranscriptionFileJob < CaseflowJob
   def ensure_hearing_held
     return if hearing.held?
 
-    exception = HearingAssociationError.new("Hearing disposition not set to held")
-    Rails.logger.warn(exception)
-    send_error_email_to_va_ops(exception)
+    error = HearingAssociationError.new("Disposition for hearing (docket numner #{docket_number} not set to held")
+    Rails.logger.warn(error)
+    send_error_email_to_va_ops(error)
   end
 
   # Purpose: Appeal associated with the hearing for which the transcription was created
@@ -225,8 +231,8 @@ class DownloadTranscriptionFileJob < CaseflowJob
       appeal_id: appeal&.id,
       appeal_type: appeal&.class&.name
     ) do |file|
-      file.file_type = file_type unless file_type_invalid?
-      file.created_by_id = RequestStore[:current_user]
+      file.file_type = file_type if file_type_valid?
+      file.created_by_id = RequestStore[:current_user].id
     end
   end
 
@@ -238,7 +244,7 @@ class DownloadTranscriptionFileJob < CaseflowJob
     @transcription_file.update!(
       file_status: Constants.TRANSCRIPTION_FILE_STATUSES.retrieval.send(status),
       date_receipt_webex: date,
-      updated_by_id: RequestStore[:current_user]
+      updated_by_id: RequestStore[:current_user].id
     )
   end
 
@@ -251,18 +257,12 @@ class DownloadTranscriptionFileJob < CaseflowJob
       file_status: Constants.TRANSCRIPTION_FILE_STATUSES.upload.send(status),
       aws_link: aws_link,
       date_upload_aws: date,
-      updated_by_id: RequestStore[:current_user]
+      updated_by_id: RequestStore[:current_user].id
     )
   end
 
   # Purpose: Logging info messages to the console
   def log_info(message)
     Rails.logger.info(message)
-  end
-
-  # To Be Implemented
-  # Purpose: Sends email to Va Operations Team when download fails or hearing disposition not set to held
-  def send_error_email_to_va_ops(_error)
-    nil
   end
 end
