1a41cd998a27a448a957ba10debd9838aa4b9783
Enrilo Ugalde <ugalde_enrilo@bah.com>
2024-02-05 13:46:29 -0800

    APPEALS-38926 - Created DecisionReviewCreatedError Service Class, added logic for handling service error, updated DecisonReviewCreatedController, and DecisionReviewCreatedController spec, with the updated service logic

--- app/controllers/api/events/v1/decision_review_created_controller.rb
+++ app/controllers/api/events/v1/decision_review_created_controller.rb
@@ -6,6 +6,16 @@ class Api::Events::V1::DecisionReviewCreatedController < Api::ApplicationControl
   end
 
   def decision_review_created_error
-    render json: { message: "Error Creating Decision Review" }, status: :method_not_allowed
+    event_id = dcr_error_params[:event_id]
+    errored_claim_id = dcr_error_params[:errored_claim_id]
+    error = dcr_error_params[:error]
+    ::Events::DecisionReviewCreatedError.handle_service_error(event_id, errored_claim_id, error)
+    render json: { message: "Decision Review Created Error Saved in Caseflow" }, status: :created
+  end
+
+  private
+
+  def dcr_error_params
+    params.permit(:event_id, :errored_claim_id, :error)
   end
 end
---
+++ app/services/events/decision_review_created_error.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Events::DecisionReviewCreatedError
+  # Using macro-style definition. The locking scope will be TheClass#method and only one method can run at any
+  # given time.
+  include RedisMutex::Macro
+
+  # Default options for RedisMutex#with_lock
+  # :block  => 1    # Specify in seconds how long you want to wait for the lock to be released.
+  #                 # Specify 0 if you need non-blocking sematics and return false immediately. (default: 1)
+  # :sleep  => 0.1  # Specify in seconds how long the polling interval should be when :block is given.
+  #                 # It is NOT recommended to go below 0.01. (default: 0.1)
+  # :expire => 10   # Specify in seconds when the lock should be considered stale when something went wrong
+  #                 # with the one who held the lock and failed to unlock. (default: 10)
+  class << self
+    def handle_service_error(consumer_event_id, errored_claim_id, error_message)
+      event = find_by_consumer_event_id_or_create(consumer_event_id)
+      RedisMutex.with_lock("EndProductEstablishment:#{errored_claim_id}", block: 60, expire: 100) do
+        ActiveRecord::Base.transaction do
+          event&.update!(error: error_message)
+        end
+      end
+    rescue RedisMutex::LockError
+      Rails.logger.error("Failed to acquire lock for Claim ID: #{reference_id}! This Event is being"\
+                        " processed. Please try again later.")
+    rescue StandardError => error
+      Rails.logger.error(error.message)
+      event&.update!(error: error.message)
+      raise error
+    end
+
+    # check if consumer_event_id Event.reference_id exist if not Create DecisionReviewCreated Event
+    def find_by_consumer_event_id_or_create(consumer_event_id)
+      Event.find_by(reference_id: consumer_event_id) ||
+        DecisionReviewCreatedEvent.create!(reference_id: consumer_event_id)
+    end
+  end
+end
