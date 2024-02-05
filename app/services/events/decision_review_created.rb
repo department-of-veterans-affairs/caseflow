dd76c4131fe63ebc5d9ec8f60691f6248fd79a0a
Jonathan Tsang <tsang_jonathan@bah.com>
2024-02-05 10:14:28 -0500

    service class methods

--- app/services/events/decision_review_created.rb
+++ app/services/events/decision_review_created.rb
@@ -16,24 +16,33 @@ class Events::DecisionReviewCreated
 
       RedisMutex.with_lock("EndProductEstablishment:#{reference_id}", block: 60, expire: 100) do
       # key => "EndProductEstablishment:reference_id" aka "claim ID"
+        # Use the consumer_event_id to retrieve/create the Event object
+        event = find_or_create_event(consumer_event_id)
 
         ActiveRecord::Base.transaction do
-          # create/save Event to table
-          new_event = DecisionReviewCreatedEvent.create!(reference_id: consumer_event_id)
-
           # TODO: backfill models as needed, set Event.completed_at when finished
-          # new_event.update!(completed_at: Time.now)
+          # event.update!(completed_at: Time.now, error: nil)
         end
       end
     rescue RedisMutex::LockError
       Rails.logger.error("Failed to acquire lock for Claim ID: #{reference_id}! This Event is being"\
                          " processed. Please try again later.")
+    rescue StandardError => e
+      Rails.logger.error(e.message)
+      event.update!(error: e.message)
+      raise error
     end
 
     # Check if there's already a CF Event that references that Appeals-Consumer EventID and
     # was successfully completed
     def event_exists_and_is_completed?(consumer_event_id)
       Event.where(reference_id: consumer_event_id).where.not(completed_at: nil).exists?
+    end
+
+    # Check if there's already a CF Event that references that Appeals-Consumer EventID
+    # We will update the existing Event instead of creating a new one
+    def find_or_create_event(consumer_event_id)
+      Event.find_by(reference_id: consumer_event_id) || DecisionReviewCreatedEvent.create!(reference_id: consumer_event_id)
     end
   end
 end
