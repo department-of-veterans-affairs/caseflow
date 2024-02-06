d0919e19c6bbbfd3c34e4cc93bec1eab65414d7c
Dev-KRedd <khalin.redding@va.gov>
2024-02-06 14:32:18 -0500

    correspondence_queue_colomn.rb shell

--- app/models/correspondence_queue_column.rb
+++ app/models/correspondence_queue_column.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class CorrespondenceQueueColumn < QueueColumn
+  super
+  @filterable ||= false
+
   def to_hash(tasks)
     {
       name: name,
-      filterable: false,
-      filter_options: []
+      filterable: filterable,
+      filter_options: filterable ? filter_options(tasks) : []
     }
   end
+
+  FILTER_OPTIONS = {
+
+}.freeze
+
+def filter_options(tasks)
+  binding.pry
+  filter_option_func = FILTER_OPTIONS[name]
+  if filter_option_func
+    send(filter_option_func, tasks)
+  else
+    fail(
+      Caseflow::Error::MustImplementInSubclass,
+      "Filterable tasks must have an associated function to collect filter options"
+    )
+  end
+end
+
 end
