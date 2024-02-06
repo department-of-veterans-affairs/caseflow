2d673f66c10cab46683e690b1d755e131f4a0bfd
Craig Reese <Reese_Craig@bah.com>
2024-02-06 12:25:45 -0600

    fix seed to run from command line instead of only rails console

--- db/seeds/ama_affinity_cases.rb
+++ db/seeds/ama_affinity_cases.rb
@@ -6,6 +6,7 @@
 module Seeds
   class AmaAffinityCases < Base
     def initialize
+      RequestStore[:current_user] = User.system_user
       initial_file_number_and_participant_id
     end
 
@@ -73,7 +74,8 @@ module Seeds
         :dispatched,
         :direct_review_docket,
         associated_judge: judge,
-        associated_attorney: attorney
+        associated_attorney: attorney,
+        veteran: create_veteran
       )
 
       # go forward to when the remand is sent from CAVC. the source appeal's receipt_date will be the
@@ -107,7 +109,8 @@ module Seeds
         :dispatched,
         :direct_review_docket,
         associated_judge: judge,
-        associated_attorney: attorney
+        associated_attorney: attorney,
+        veteran: create_veteran
       )
 
       # go forward to when the remand is sent from CAVC. the source appeal's receipt_date will be the
@@ -130,7 +133,7 @@ module Seeds
     def create_case_ready_for_less_than_hearing_affinity_days(judge)
       # set system time and create the appeal
       Timecop.travel(4.years.ago)
-      appeal = create(:appeal, :hearing_docket, :with_post_intake_tasks)
+      appeal = create(:appeal, :hearing_docket, :with_post_intake_tasks, veteran: create_veteran)
 
       # travel to when the hearing was held, then create the held hearing and post-hearing tasks:
       # add 91 days for the amount of time the post-hearing tasks are open and remove 7 to make the case ready
@@ -151,7 +154,7 @@ module Seeds
     def create_case_ready_for_more_than_hearing_affinity_days(judge)
       # set system time and create the appeal
       Timecop.travel(4.years.ago)
-      appeal = FactoryBot.create(:appeal, :hearing_docket, :with_post_intake_tasks)
+      appeal = FactoryBot.create(:appeal, :hearing_docket, :with_post_intake_tasks, veteran: create_veteran)
 
       # travel to when the hearing was held, then create the held hearing and post-hearing tasks:
       # add 91 days for the amount of time the post-hearing tasks are open and add 7 more to make the case ready
