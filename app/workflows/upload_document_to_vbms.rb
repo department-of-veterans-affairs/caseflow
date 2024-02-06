ed99a1e88cd842126507db06f588fbcb94b062c7
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-02-06 16:20:14 -0500

    Cleanup files after uploading to VBMS

--- app/workflows/upload_document_to_vbms.rb
+++ app/workflows/upload_document_to_vbms.rb
@@ -19,25 +19,8 @@ class UploadDocumentToVbms
   rescue StandardError => error
     save_rescued_error!(error.to_s)
     raise error
-  end
-
-  # We have to always download the file from s3 to make sure it exists locally
-  # instead of storing it on the server and relying that it will be there
-  def pdf_location
-    S3Service.fetch_file(s3_location, output_location)
-    output_location
-  end
-
-  def source
-    "BVA"
-  end
-
-  def document_type_id
-    Document.type_id(document_type)
-  end
-
-  def cache_file
-    S3Service.store_file(s3_location, Base64.decode64(document.file))
+  ensure
+    cleanup_up_file
   end
 
   private
@@ -63,49 +46,5 @@ class UploadDocumentToVbms
     persist_efolder_version_info(upload_response, :upload_document_response)
 
     document.update!(uploaded_to_vbms_at: Time.zone.now)
-  end
-
-  def set_processed_at_to_current_time
-    document.update!(processed_at: Time.zone.now)
-  end
-
-  def save_rescued_error!(error)
-    document.update!(error: error)
-  end
-
-  def s3_location
-    "#{s3_bucket_by_doc_type}/#{pdf_name}"
-  end
-
-  def output_location
-    File.join(Rails.root, "tmp", "pdfs", pdf_name)
-  end
-
-  def pdf_name
-    "veteran-#{file_number}-doc-#{document.id}.pdf"
-  end
-
-  def file_number
-    document.veteran_file_number
-  end
-
-  def log_info(info_message)
-    uuid = SecureRandom.uuid
-    Rails.logger.info("#{info_message} ID: #{uuid}")
-  end
-
-  # Purpose: Get the s3_sub_bucket based on the document type
-  # S3_SUB_BUCKET was previously a constant defined for this class.
-  #
-  # Params: None
-  #
-  # Return: string for the sub-bucket
-  def s3_bucket_by_doc_type
-    case document_type
-    when "BVA Case Notifications"
-      "notification-reports"
-    else
-      "idt-uploaded-documents"
-    end
   end
 end
