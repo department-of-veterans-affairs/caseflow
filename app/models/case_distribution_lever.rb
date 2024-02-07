01374826d19c3a6def94ea433e4fdaacdbb39d6d
Matthew Roth <roth_matthew@bah.com>
2024-02-07 12:33:37 -0500

    Update case_distribution_lever.rb

--- app/models/case_distribution_lever.rb
+++ app/models/case_distribution_lever.rb
@@ -71,15 +71,17 @@ class CaseDistributionLever < ApplicationRecord
 
   class << self
 
-     def method_missing(name, *args)
-      if Constants.DISTRIBUTION.to_h.keys.include?(name)
+    def respond_to_missing?(name, include_private)
+      # ...
+    end
+
+    def method_missing(name, *args)
+      if Constants.DISTRIBUTION.to_h.key?(name)
         value = method_missing_value(name.to_s)
         return value unless value.nil?
-
-        super
-      else
-        super
       end
+
+      super
     end
 
     def update_acd_levers(current_levers, current_user)
