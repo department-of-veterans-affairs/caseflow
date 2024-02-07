c6c7cde99b40e6738a8d7a053b81e431ceb39416
= <tyler.broyles@va.gov>
2024-02-07 16:25:31 -0500

    Updated the SCT unassigned tasks tab to only be assigned tasks. Changed the SCT Action required tab to use the in progress tasks query. Updated the sct factory trait name from on_hold to action_required and changed it in the seeds and spec files. Added back a queue config constant that was missed during the merge.

--- app/models/queue_tabs/specialty_case_team_action_required_tasks_tab.rb
+++ app/models/queue_tabs/specialty_case_team_action_required_tasks_tab.rb
@@ -18,11 +18,7 @@ class SpecialtyCaseTeamActionRequiredTasksTab < QueueTab
   end
 
   def tasks
-<<<<<<< HEAD
     in_progress_tasks
-=======
-    on_hold_tasks
->>>>>>> dfe9fd4e30 (Added in the specialty case team queue tabs from APPEALS-35193 to preemptively test how it would intefere with bulk assign. Added the unassigned queue tab and a method to hide it from the queue via in order to give the SCT bulk assign page a tab that it could use as the basis for the page. Added a few more code comments and removed some old unused code.)
   end
 
   def column_names
