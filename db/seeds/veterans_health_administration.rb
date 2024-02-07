b059f577b4939afb4f6202d83abc1f1782f835d3
= <tyler.broyles@va.gov>
2024-02-07 16:36:26 -0500

    Updated seeds methods for code climate.

--- db/seeds/veterans_health_administration.rb
+++ db/seeds/veterans_health_administration.rb
@@ -195,15 +195,19 @@ module Seeds
     end
 
     def create_specialty_case_team_action_required
-      tasks = create_list(:specialty_case_team_assign_task, 5, :action_required)
-      tasks.last.appeal.veteran.date_of_death = 2.weeks.ago
-      tasks.last.appeal.veteran.save
+      tasks = create_list(:specialty_case_team_assign_task, 5, :action_required) do |task|
+        task.tap do |t|
+          t.appeal.veteran.date_of_death = 2.weeks.ago
+        end
+      end
     end
 
     def create_specialty_case_team_completed
-      tasks = create_list(:specialty_case_team_assign_task, 5, :completed)
-      tasks.last.appeal.veteran.date_of_death = 2.weeks.ago
-      tasks.last.appeal.veteran.save
+      tasks = create_list(:specialty_case_team_assign_task, 5, :completed) do |task|
+        task.tap do |t|
+          t.appeal.veteran.date_of_death = 2.weeks.ago
+        end
+      end
     end
 
     def create_specialty_case_team_assigned
