a6e41836428335b6d72a57b40dc6dfa567e484ef
William Medders <medders_william@bah.com>
2024-02-07 23:15:42 -0600

    Refactor queue correspondences seeds

--- db/seeds/helpers/queue_helpers.rb
+++ db/seeds/helpers/queue_helpers.rb
@@ -1,46 +1,46 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
 module QueueHelpers
   def create_veteran(options = {})
     @file_number += 1
     @participant_id += 1
+    @cmp_packet_number ||= 1_000_000_000
     params = {
       file_number: format("%<n>09d", n: @file_number),
       participant_id: format("%<n>09d", n: @participant_id)
     }
-    create(:veteran, params.merge(options))
+    veteran = create(:veteran, params.merge(options))
+    2.times do
+      appeal = create(:appeal, veteran_file_number: veteran.file_number)
+      InitialTasksFactory.new(appeal).create_root_and_sub_tasks!
+    end
+    veteran
   end
 
-  def create_appeal(veteran)
-    ::Appeal.create!(
-      veteran_file_number: veteran.file_number,
-      docket_type: Constants.AMA_DOCKETS.direct_review
-    )
-  end
-  def create_correspondence(appeal)
+  def create_correspondence
+    vet = create_veteran
     correspondence = ::Correspondence.create!(
       uuid: SecureRandom.uuid,
       portal_entry_date: Time.zone.now,
       source_type: "Mail",
       package_document_type_id: (1..20).to_a.sample,
-      correspondence_type_id: CorrespondenceType.find_by_name("Congressional interest").id,
+      correspondence_type_id: CorrespondenceType.all.sample&.id,
       cmp_queue_id: 1,
       cmp_packet_number: @cmp_packet_number,
       va_date_of_receipt: Time.zone.yesterday,
-      notes: "Notes from CMP - Multi Correspondence Seed",
+      notes: "Notes from CMP - Queue Correspondence Seed",
       assigned_by_id: 81,
       updated_by_id: 81,
-      veteran_id: appeal.veteran.id
+      veteran_id: vet.id
     ).tap { @cmp_packet_number += 1 }
 
-    correspondence.appeals << appeal
-
-    create_correspondence_document(correspondence, appeal.veteran)
+    # create_correspondence_document(correspondence, vet)
     correspondence
   end
 
   def create_correspondence_document(correspondence, veteran)
-    CorrespondenceDocument.create!(
+    CorrespondenceDocument.find_or_create_by!(
       document_file_number: veteran.file_number,
       uuid: SecureRandom.uuid,
       vbms_document_type_id: 1250,
@@ -50,50 +50,25 @@ module QueueHelpers
     )
   end
 
-  def create_correspondence_intake(correspondence, status:)
-    cit = CorrespondenceIntakeTask.create!(
-      appeal_id: correspondence.id,
-      appeal_type: "Correspondence",
-      assigned_to: MailTeamSupervisor.singleton
-    )
-
-    cit.update!(status: status)
+  def create_correspondence_intake(correspondence, user)
+    parent = correspondence&.root_task
+    cit = CorrespondenceIntakeTask.create_from_params(parent, user)
     cit
   end
 
-  def create_review_package_task(correspondence, status:)
-    review_package_task = ReviewPackageTask.find_or_create_by!(
-      appeal_id: correspondence.id,
-      assigned_to: MailTeamSupervisor.singleton,
-      appeal_type: "Correspondence",
-    )
-
-    review_package_task.update(status: status)
-    review_package_task
+  def assign_review_package_task(correspondence, user)
+    review_package_task = ReviewPackageTask.find_by(appeal_id: correspondence.id)
+    review_package_task.update!(assigned_to: user, status: Constants.TASK_STATUSES.assigned)
   end
 
-  def create_efolderupload_failed_task(correspondence, ptask:)
-    euft = EfolderUploadFailedTask.create!(
-      parent_id: ptask.id,
+  def create_efolderupload_failed_task(correspondence, parent)
+    EfolderUploadFailedTask.create!(
+      parent_id: parent.id,
       appeal_id: correspondence.id,
       appeal_type: "Correspondence",
-      assigned_to: MailTeamSupervisor.singleton
+      assigned_to: parent.assigned_to,
+      status: Constants.TASK_STATUSES.in_progress
     )
-
-    euft.update!(status: Constants.TASK_STATUSES.in_progress)
-
-    euft
-  end
-
-  def create_correspondence_root_task(correspondence, status:)
-    root_task = CorrespondenceRootTask.find_or_create_by!(
-      appeal_id: correspondence.id,
-      assigned_to: MailTeamSupervisor.singleton,
-      appeal_type: "Correspondence",
-    )
-
-    root_task.update(status: status)
-    root_task
   end
 
   def create_action_required_tasks(correspondence, status:, parent_task:, task_type:)
@@ -106,72 +81,53 @@ module QueueHelpers
     )
   end
 
-  def create_in_progress_root_task_and_completed_mail_task(correspondence, status:, parent_task:)
+  def create_and_complete_mail_task(correspondence, user)
+    process_correspondence(correspondence, user)
+    assigned_by = CorrespondenceIntakeTask.find_by(appeal_id: correspondence.id).completed_by
     cavct = CavcCorrespondenceMailTask.find_or_create_by!(
-      parent_id: parent_task.id,
+      parent_id: correspondence&.root_task&.id,
       appeal_id: correspondence.id,
       appeal_type: "Correspondence",
+      assigned_by: assigned_by,
       assigned_to: MailTeamSupervisor.singleton
     )
-    cavct.update!(status: status)
-
-
-    review_package_task = ReviewPackageTask.find_or_create_by!(
-      parent_id: parent_task.id,
-      appeal_id: correspondence.id,
-      appeal_type: "Correspondence",
-      assigned_to: MailTeamSupervisor.singleton
-    )
-
-    review_package_task.update!(status: status)
-
-    cit = CorrespondenceIntakeTask.find_or_create_by!(
-      parent_id: parent_task.id,
-      appeal_id: correspondence.id,
-      appeal_type: "Correspondence",
-      assigned_to: MailTeamSupervisor.singleton
-    )
-    cit.update!(status: status)
+    cavct.update!(status: Constants.TASK_STATUSES.completed)
+    cavct
   end
 
-  def pending_tab_cavc_mailtask(correspondence, status:)
+  def create_cavc_mailtask(correspondence, user)
+    process_correspondence(correspondence, user)
+    assigned_by = CorrespondenceIntakeTask.find_by(appeal_id: correspondence.id).completed_by
     cavct = CavcCorrespondenceMailTask.create!(
       appeal_id: correspondence.id,
       appeal_type: "Correspondence",
+      assigned_by: assigned_by,
       assigned_to: CavcLitigationSupport.singleton,
-      status: status
+      status: Constants.TASK_STATUSES.assigned
     )
     cavct
   end
 
-  def pending_tab_congress_interest_mailtask(correspondence, status:)
-   cmt = CongressionalInterestMailTask.create!(
+  def create_congress_interest_mailtask(correspondence, user)
+    process_correspondence(correspondence, user)
+    assigned_by = CorrespondenceIntakeTask.find_by(appeal_id: correspondence.id).completed_by
+    cmt = CongressionalInterestMailTask.create!(
       appeal_id: correspondence.id,
       appeal_type: "Correspondence",
+      assigned_by: assigned_by,
       assigned_to: LitigationSupport.singleton,
-      status: status
+      status: Constants.TASK_STATUSES.assigned,
+      parent_id: correspondence&.root_task&.id
     )
     cmt
   end
 
-  def create_pending_tasks_for_tasks_not_related_to_appeal(correspondence, parent_task:)
-    # Creating Completed ReviewPackageTask
-    review_package_task = ReviewPackageTask.find_by(
-      appeal_id: correspondence.id,
-      assigned_to: MailTeamSupervisor.singleton,
-      appeal_type: "Correspondence"
-    )
+  def process_correspondence(correspondence, user)
+    rpt = ReviewPackageTask.find_by(appeal_id: correspondence.id)
+    rpt.update!(status: Constants.TASK_STATUSES.completed, completed_by_id: user.id)
 
-    if review_package_task.present? && review_package_task.status != Constants.TASK_STATUSES.completed
-      review_package_task.update!(status: Constants.TASK_STATUSES.completed, parent_id: parent_task.id)
-    end
-    # Creating Completed CorrespondenceIntakeTask
-    correspondence_intake_task = CorrespondenceIntakeTask.create!(
-      parent_id: parent_task.id,
-      appeal_id: correspondence.id,
-      assigned_to: MailTeamSupervisor.singleton,
-      appeal_type: "Correspondence"
-    )
-    correspondence_intake_task.update!(status: Constants.TASK_STATUSES.completed)
+    cit = CorrespondenceIntakeTask.create_from_params(correspondence&.root_task, user)
+    cit.update!(status: Constants.TASK_STATUSES.completed, completed_by_id: user.id)
   end
 end
+# rubocop:enable Metrics/ModuleLength
