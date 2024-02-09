ac047b9bb5a5a47cff4c573dbe78e9665863df5c
Matt Roth <roth_matthew@bah.com>
2024-02-08 19:28:49 -0500

    Update leversActions.js (#20760)

--- client/app/caseDistribution/reducers/levers/leversActions.js
+++ client/app/caseDistribution/reducers/levers/leversActions.js
@@ -32,7 +32,7 @@ export const loadHistory = (historyList) =>
   };
 
 export const resetLevers = () => async (dispatch) => {
-  const resp = await ApiUtil.get('/case_distribution_levers/get_levers');
+  const resp = await ApiUtil.get('/case_distribution_levers/levers');
   const { levers } = resp.body;
 
   dispatch({
