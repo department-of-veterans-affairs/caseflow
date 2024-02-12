795f546f088632e267616bded68a573387011986
Clay Sheppard <csheepy@gmail.com>
2024-02-12 08:21:54 -0600

    frontend tests and move dummy data to a file

--- client/app/nonComp/components/IndividualClaimHistoryTable.jsx
+++ client/app/nonComp/components/IndividualClaimHistoryTable.jsx
@@ -4,6 +4,7 @@ import NonCompLayout from '../components/NonCompLayout';
 import Link from 'app/components/Link';
 import styled from 'styled-components';
 import QueueTable from '../../queue/QueueTable';
+import dummyData from 'test/data/nonComp/individualClaimHistoryData';
 
 const IndividualClaimHistoryTable = () => {
 
@@ -95,92 +96,6 @@ const IndividualClaimHistoryTable = () => {
     </p>;
   };
 
-  const dummyData = [
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Claim closed',
-      details: {
-        decisionDate: '07/05/23'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Claim created',
-      details: {
-        decisionDate: '07/05/23'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'J. Dudifer',
-      readableEventType: 'Completed disposition',
-      details: {
-        benefitType: 'Veterans Health Administration',
-        issueType: 'Beneficiary Travel',
-        issueDescription: 'Any notes will display here',
-        decisionDate: '05/31/2023',
-        disposition: 'Granted',
-        decisionDescription: 'Any notes from decision will display here'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Claim status - In progress',
-      details: {
-        decisionDate: '07/05/23'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Claim status - Incomplete',
-      details: {
-        decisionDate: ''
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Added issue',
-      details: {
-        benefitType: 'Veterans Health Administration',
-        issueType: 'Beneficiary Travel',
-        issueDescription: 'Any notes will display here',
-        decisionDate: '05/31/2023',
-        disposition: 'Granted',
-        decisionDescription: 'Any notes from decision will display here'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Added issue - No decision date',
-      details: {
-        benefitType: 'Veterans Health Administration',
-        issueType: 'Beneficiary Travel',
-        issueDescription: 'Any notes will display here',
-        decisionDate: '',
-        disposition: 'Granted',
-        decisionDescription: 'Any notes from decision will display here'
-      }
-    },
-    {
-      eventDate: '07/05/23, 15:00',
-      eventUser: 'System',
-      readableEventType: 'Withdrew issue',
-      details: {
-        benefitType: 'Veterans Health Administration',
-        issueType: 'Beneficiary Travel',
-        issueDescription: 'Any notes will display here',
-        decisionDate: '07/04/23',
-        withdrawalRequestDate: '07/05/23',
-      }
-    },
-  ];
-
   return <QueueTable
     id="individual_claim_history_table"
     columns={[
@@ -194,6 +109,7 @@ const IndividualClaimHistoryTable = () => {
         valueName: 'User',
         valueFunction: (row) => row.eventUser,
         enableFilter: true,
+        label: 'Filter by User',
         getSortValue: (row) => row.eventUser },
       { columnName: 'readableEventType',
         name: 'Activity',
@@ -201,6 +117,7 @@ const IndividualClaimHistoryTable = () => {
         valueName: 'Activity',
         valueFunction: (row) => row.readableEventType,
         enableFilter: true,
+        label: 'Filter by Activity',
         getSortValue: (row) => row.readableEventType },
       { name: 'details',
         header: 'Details',
---
+++ client/test/data/nonComp/individualClaimHistoryData.js
@@ -0,0 +1,85 @@
+export default [
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Claim closed',
+    details: {
+      decisionDate: '07/05/23'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Claim created',
+    details: {
+      decisionDate: '07/05/23'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'J. Dudifer',
+    readableEventType: 'Completed disposition',
+    details: {
+      benefitType: 'Veterans Health Administration',
+      issueType: 'Beneficiary Travel',
+      issueDescription: 'Any notes will display here',
+      decisionDate: '05/31/2023',
+      disposition: 'Granted',
+      decisionDescription: 'Any notes from decision will display here'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Claim status - In progress',
+    details: {
+      decisionDate: '07/05/23'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Claim status - Incomplete',
+    details: {
+      decisionDate: ''
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Added issue',
+    details: {
+      benefitType: 'Veterans Health Administration',
+      issueType: 'Beneficiary Travel',
+      issueDescription: 'Any notes will display here',
+      decisionDate: '05/31/2023',
+      disposition: 'Granted',
+      decisionDescription: 'Any notes from decision will display here'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Added issue - No decision date',
+    details: {
+      benefitType: 'Veterans Health Administration',
+      issueType: 'Beneficiary Travel',
+      issueDescription: 'Any notes will display here',
+      decisionDate: '',
+      disposition: 'Granted',
+      decisionDescription: 'Any notes from decision will display here'
+    }
+  },
+  {
+    eventDate: '07/05/23, 15:00',
+    eventUser: 'System',
+    readableEventType: 'Withdrew issue',
+    details: {
+      benefitType: 'Veterans Health Administration',
+      issueType: 'Beneficiary Travel',
+      issueDescription: 'Any notes will display here',
+      decisionDate: '07/04/23',
+      withdrawalRequestDate: '07/05/23',
+    }
+  },
+];
