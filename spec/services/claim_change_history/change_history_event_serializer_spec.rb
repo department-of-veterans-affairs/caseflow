72f67adc37cb366c87934d400a29f8bf4a69e1ca
= <tyler.broyles@va.gov>
2024-02-13 18:02:27 -0500

    Updated the task id to the correct value in the serialized hash values.

--- spec/services/claim_change_history/change_history_event_serializer_spec.rb
+++ spec/services/claim_change_history/change_history_event_serializer_spec.rb
@@ -58,7 +58,7 @@ describe ChangeHistoryEventSerializer do
           eventType: :added_issue,
           eventUser: "L. Roth",
           readableEventType: "Added Issue",
-          taskID: 1
+          taskID: vha_task.id
         }
       },
       {
@@ -81,7 +81,7 @@ describe ChangeHistoryEventSerializer do
           eventType: :claim_creation,
           eventUser: "L. Roth",
           readableEventType: "Claim created",
-          taskID: 1
+          taskID: vha_task.id
         }
       }
     ]
