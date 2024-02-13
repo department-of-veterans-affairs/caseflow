601388515f693c3e12bbb8faa55f359d6c09a154
= <tyler.broyles@va.gov>
2024-02-13 16:44:39 -0500

    Rearranged the spec test again to prevent a failure in github actions.

--- spec/services/claim_change_history/change_history_event_serializer_spec.rb
+++ spec/services/claim_change_history/change_history_event_serializer_spec.rb
@@ -31,59 +31,62 @@ describe ChangeHistoryEventSerializer do
     ClaimHistoryService.new(vha_org, task_id: vha_task.id).build_events
   end
 
+  let(:serialized_hash_array) do
+    [
+      {
+        id: expected_uuid,
+        type: :change_history_event,
+        attributes: {
+          claimType: "Higher-Level Review",
+          claimantName: events[0].claimant_name,
+          details:
+          {
+            benefitType: "vha",
+            decisionDate: events[0].decision_date,
+            decisionDescription: nil,
+            disposition: nil,
+            issueDescription: "Veterans Health Administration seeded HLR in progress",
+            issueType: "Other",
+            withdrawalRequestDate: nil
+          },
+          eventDate: events[0].event_date,
+          eventType: :added_issue,
+          eventUser: "L. Roth",
+          readableEventType: "Added Issue",
+          taskID: 1
+        }
+      },
+      {
+        id: expected_uuid,
+        type: :change_history_event,
+        attributes: {
+          claimType: "Higher-Level Review",
+          claimantName: events[1].claimant_name,
+          details:
+          {
+            benefitType: "vha",
+            decisionDate: nil,
+            decisionDescription: nil,
+            disposition: nil,
+            issueDescription: nil,
+            issueType: nil,
+            withdrawalRequestDate: nil
+          },
+          eventDate: events[1].event_date,
+          eventType: :claim_creation,
+          eventUser: "L. Roth",
+          readableEventType: "Claim created",
+          taskID: 1
+        }
+      }
+    ]
+  end
+
   subject { described_class.new(events).serializable_hash[:data] }
 
   describe "#as_json" do
     it "renders json data" do
-      serializable_hash = [
-        {
-          id: expected_uuid,
-          type: :change_history_event,
-          attributes: {
-            claimType: "Higher-Level Review",
-            claimantName: events[0].claimant_name,
-            details:
-            {
-              benefitType: "vha",
-              decisionDate: events[0].decision_date,
-              decisionDescription: nil,
-              disposition: nil,
-              issueDescription: "Veterans Health Administration seeded HLR in progress",
-              issueType: "Other",
-              withdrawalRequestDate: nil
-            },
-            eventDate: events[0].event_date,
-            eventType: :added_issue,
-            eventUser: "L. Roth",
-            readableEventType: "Added Issue",
-            taskID: 1
-          }
-        },
-        {
-          id: expected_uuid,
-          type: :change_history_event,
-          attributes: {
-            claimType: "Higher-Level Review",
-            claimantName: events[1].claimant_name,
-            details:
-            {
-              benefitType: "vha",
-              decisionDate: nil,
-              decisionDescription: nil,
-              disposition: nil,
-              issueDescription: nil,
-              issueType: nil,
-              withdrawalRequestDate: nil
-            },
-            eventDate: events[1].event_date,
-            eventType: :claim_creation,
-            eventUser: "L. Roth",
-            readableEventType: "Claim created",
-            taskID: 1
-          }
-        }
-      ]
-      expect(subject).to eq(serializable_hash)
+      expect(subject).to eq(serialized_hash_array)
     end
   end
 end
