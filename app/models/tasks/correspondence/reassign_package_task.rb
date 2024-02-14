bb71a1dd077fcc42e490338194980f152574e53d
William Medders <medders_william@bah.com>
2024-02-14 16:53:36 -0600

    fix wrong url for reassign package task

--- app/models/tasks/correspondence/reassign_package_task.rb
+++ app/models/tasks/correspondence/reassign_package_task.rb
@@ -4,6 +4,6 @@ class ReassignPackageTask < CorrespondenceTask
   before_create :verify_no_other_open_package_action_task_on_correspondence
 
   def task_url
-    Constants.CORRESPONDENCE_TASK_URL.REMOVE_PACKAGE_TASK_MODAL_URL
+    Constants.CORRESPONDENCE_TASK_URL.REASSIGN_PACKAGE_TASK_MODAL_URL
   end
 end
