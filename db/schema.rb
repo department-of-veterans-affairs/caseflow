1b752fe6e6a5ba0037d04072b788272e45551f34
Craig Reese <Reese_Craig@bah.com>
2024-02-15 15:33:46 -0600

    add index migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_16_132819) do
+ActiveRecord::Schema.define(version: 2024_02_15_212016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1024,6 +1024,7 @@ ActiveRecord::Schema.define(version: 2023_10_16_132819) do
     t.bigint "updated_by_id", comment: "The ID of the user who most recently updated the Hearing"
     t.uuid "uuid", default: -> { "uuid_generate_v4()" }, null: false
     t.string "witness", comment: "Witness/Observer present during hearing"
+    t.index ["appeal_id"], name: "index_hearings_on_appeal_id"
     t.index ["created_by_id"], name: "index_hearings_on_created_by_id"
     t.index ["disposition"], name: "index_hearings_on_disposition"
     t.index ["updated_at"], name: "index_hearings_on_updated_at"
