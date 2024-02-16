b71b2b544631c5325b40edf41ad42f2a6f419d3d
= <tyler.broyles@va.gov>
2024-02-16 17:43:40 -0500

    Removed unused methods and commented out code from the edit spec.

--- spec/feature/intake/appeal/edit_spec.rb
+++ spec/feature/intake/appeal/edit_spec.rb
@@ -684,33 +684,6 @@ feature "Appeal Edit issues", :all_dbs do
       expect(page).to have_current_path("/appeals/#{appeal.uuid}/edit/review_split")
     end
 
-    def wait_for_ajax
-      max_time = Capybara::Helpers.monotonic_time + Capybara.default_max_wait_time
-      while Capybara::Helpers.monotonic_time < max_time
-        finished = finished_all_ajax_requests?
-        if finished
-          break
-        else
-          sleep 0.1
-        end
-      end
-      fail "wait_for_ajax timeout" unless finished
-    end
-
-    def finished_all_ajax_requests?
-      page.evaluate_script(<<~EOS
-        ((typeof window.jQuery === 'undefined')
-        || (typeof window.jQuery.active === 'undefined')
-        || (window.jQuery.active === 0))
-        && ((typeof window.injectedJQueryFromNode === 'undefined')
-        || (typeof window.injectedJQueryFromNode.active === 'undefined')
-        || (window.injectedJQueryFromNode.active === 0))
-        && ((typeof window.httpClients === 'undefined')
-        || (window.httpClients.every(function (client) { return (client.activeRequestCount === 0); })))
-      EOS
-                          )
-    end
-
     scenario "When the user accesses the review_split page, the page renders as expected" do
       skill_form(appeal2)
 
@@ -756,9 +729,6 @@ feature "Appeal Edit issues", :all_dbs do
       skill_form(appeal2)
 
       click_button("Split appeal")
-      # wait_for_ajax
-      # page.find(:xpath, '/queue/appeals/#{appeal2.uuid}')
-      # assert_current_path("/queue/appeals/#{appeal2.uuid}")
       expect(page).to have_current_path("/queue/appeals/#{appeal2.uuid}", ignore_query: true)
     end
   end
