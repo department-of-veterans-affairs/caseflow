40c712f181205ba7a7fc168cdab58fb62e1bf218
Ryan Marshall <rnmarshall1993@gmail.com>
2024-02-16 13:17:21 -0800

    working test page with data

--- spec/feature/queue/correspondence/correspondence_cases_spec.rb
+++ spec/feature/queue/correspondence/correspondence_cases_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.feature("The Correspondence Cases page") do
+  include CorrespondenceTaskHelpers
   context "correspondece cases feature toggle" do
     let(:current_user) { create(:user) }
     before :each do
@@ -27,6 +28,28 @@ RSpec.feature("The Correspondence Cases page") do
       FeatureToggle.enable!(:correspondence_queue)
       User.authenticate!(user: current_user)
       @correspondence_uuid = "123456789"
+    end
+  end
+
+  context "correspondence tasks in-progress tab" do
+    let(:current_user) { create(:user) }
+    before :each do
+      MailTeam.singleton.add_user(current_user)
+      User.authenticate!(user: current_user)
+    end
+    before do
+      20.times do
+        correspondence = create(:correspondence)
+        parent_task = create_correspondence_intake(correspondence, current_user)
+        create_efolderupload_failed_task(correspondence, parent_task, user: current_user)
+      end
+    end
+    it "is my tests" do
+      FeatureToggle.enable!(:correspondence_queue)
+      FeatureToggle.enable!(:user_queue_pagination)
+      visit "/queue/correspondence?tab=correspondence_assigned&page=1&sort_by=vaDor&order=asc"
+
+      binding.pry
     end
   end
 end
