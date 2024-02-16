745b17f481b4701dddab36c09fd783833dbed447
Matthew Roth <roth_matthew@bah.com>
2024-02-16 08:53:21 -0500

    remove extra line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2024_02_15_212016) do
 
   # These are extensions that must be enabled in order to support this database
