f1f72d667b09fb8611f8d7d0e0a9bfbac7f79485
Sean Craig <sean.craig2@va.gov>
2024-02-20 15:19:27 -0600

    added another test to specialty_case_team_assign_task_spec

--- spec/models/tasks/specialty_case_team_assign_task_spec.rb
+++ spec/models/tasks/specialty_case_team_assign_task_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 describe SpecialtyCaseTeamAssignTask do
-  let(:user) { create(:user) }
-  let(:org) { SpecialtyCaseTeam.singleton }
-  let(:sct_task) { create(:specialty_case_team_assign_task) }
+  let(:sct_user) { create(:user) }
+  let(:sct_org) { SpecialtyCaseTeam.singleton }
+  let!(:attorney) do
+    create(:user, :with_vacols_attorney_record, full_name: "Saul Goodman")
+  end
+  let(:sct_task) { create(:specialty_case_team_assign_task, :action_required) }
   let(:expected_actions) do
     [
       Constants.TASK_ACTIONS.ASSIGN_TO_ATTORNEY.to_h
@@ -11,7 +14,8 @@ describe SpecialtyCaseTeamAssignTask do
   end
 
   before do
-    org.add_user(user)
+    sct_org.add_user(sct_user)
+    User.authenticate!(user: sct_user)
   end
 
   describe ".label" do
@@ -22,7 +26,15 @@ describe SpecialtyCaseTeamAssignTask do
 
   describe ".available_actions" do
     it "returns available actions" do
-      expect(sct_task.available_actions(user)).to eq(expected_actions)
+      expect(sct_task.available_actions(sct_user)).to eq(expected_actions)
+    end
+  end
+
+  describe ".execute actions" do
+    let(:first_available_action) { sct_task.available_actions(sct_user).first }
+
+    it "assigns to appeal to attorney" do
+      expect(first_available_action[:func]).to eq("assign_to_attorney_data")
     end
   end
 end
