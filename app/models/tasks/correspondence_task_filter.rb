2e680047f5340d2a0b30d4891ef67823f895adfa
divyadasari-va <divya.dasari@va.gov>
2024-02-21 11:32:43 -0700

    Improvement for filter logic

--- app/models/tasks/correspondence_task_filter.rb
+++ app/models/tasks/correspondence_task_filter.rb
@@ -34,17 +34,18 @@ class CorrespondenceTaskFilter < TaskFilter
   def filtered_tasks
     va_dor_params = filter_params.select { |param| param.include?("col=vaDor") }
     task_column_params = filter_params.select { |param| param.include?("col=completedDateColumn") }
+    result = tasks.all # Assuming Task is the name of your ActiveRecord model
     va_dor_params.each do |param|
       value_hash = Rack::Utils.parse_nested_query(param).deep_symbolize_keys
-      @tasks = filter_by_va_dor(value_hash[:val])
+      result = result.merge(filter_by_va_dor(value_hash[:val]))
     end
 
     task_column_params.each do |param|
       value_hash = Rack::Utils.parse_nested_query(param).deep_symbolize_keys
-      @tasks = filter_by_date(value_hash[:val])
+      result = result.merge(filter_by_date(value_hash[:val]))
     end
 
-    @tasks
+    result
   end
 
   private
