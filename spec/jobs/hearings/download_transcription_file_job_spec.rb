0315ad65b9e20ab9cafb8cbabd339e7e4b7a18af
msteele <Marc.Steele@va.gov>
2024-02-23 09:38:13 -0500

    Remove Hearings:: where I could find them

--- spec/jobs/hearings/download_transcription_file_job_spec.rb
+++ spec/jobs/hearings/download_transcription_file_job_spec.rb
@@ -9,7 +9,7 @@ describe Hearings::DownloadTranscriptionFileJob do
     let(:docket_number) { hearing.docket_number }
     let(:file_name) { "#{docket_number}_#{hearing.id}_#{hearing.class}.#{file_type}" }
     let(:tmp_location) { File.join(Rails.root, "tmp", "transcription_files", file_type, file_name) }
-    let(:transcription_file) { Hearings::TranscriptionFile.find_by(file_name: file_name) }
+    let(:transcription_file) { TranscriptionFile.find_by(file_name: file_name) }
     let(:s3_sub_bucket) { "vaec-appeals-caseflow" }
     let(:folder_name) { (Rails.deploy_env == :prod) ? s3_sub_bucket : "#{s3_sub_bucket}-#{Rails.deploy_env}" }
     let(:s3_sub_folders) do
@@ -54,7 +54,7 @@ describe Hearings::DownloadTranscriptionFileJob do
     shared_examples "failed download from Webex" do
       it "raises error and creates TranscriptionFileRecord" do
         expect { subject }.to raise_error(Hearings::DownloadTranscriptionFileJob::FileDownloadError)
-          .and change(Hearings::TranscriptionFile, :count).by(1)
+          .and change(TranscriptionFile, :count).by(1)
       end
 
       it "updates file_status of TranscriptionFile record, leaves date_receipt_webex nil" do
@@ -77,7 +77,7 @@ describe Hearings::DownloadTranscriptionFileJob do
 
         context "successful download from Webex and upload to S3" do
           it "creates new TranscriptionFile record" do
-            expect { subject }.to change(Hearings::TranscriptionFile, :count).by(1)
+            expect { subject }.to change(TranscriptionFile, :count).by(1)
             expect(transcription_file.file_type).to eq(file_type)
           end
 
@@ -100,7 +100,7 @@ describe Hearings::DownloadTranscriptionFileJob do
     shared_context "convertible file" do
       let(:converted_file_name) { file_name.gsub(file_type, conversion_type) }
       let(:converted_tmp_location) { tmp_location.gsub(file_type, conversion_type) }
-      let(:converted_transcription_file) { Hearings::TranscriptionFile.find_by(file_name: converted_file_name) }
+      let(:converted_transcription_file) { TranscriptionFile.find_by(file_name: converted_file_name) }
       let(:converted_s3_location) { "#{folder_name}/#{s3_sub_folders[conversion_type.to_sym]}/#{converted_file_name}" }
 
       it "updates date_receipt_webex of TranscriptionFile record" do
