138ab428efabc0b6a8f8f4ecd2fc5d10d0f210e8
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-02-27 17:47:41 -0500

    🚚 Move Rails 6.0 new framework defaults into 'application.rb'

--- config/application.rb
+++ config/application.rb
@@ -82,6 +82,48 @@ module CaseflowCertification
     # Default as of 5.2: true
     config.active_record.sqlite3.represent_boolean_as_integer = false
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Rails 6.0 default overrides
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # Don't force requests from old versions of IE to be UTF-8 encoded.
+    # config.action_view.default_enforce_utf8 = false
+
+    # Embed purpose and expiry metadata inside signed and encrypted
+    # cookies for increased security.
+    #
+    # This option is not backwards compatible with earlier Rails versions.
+    # It's best enabled when your entire app is migrated and stable on 6.0.
+    # config.action_dispatch.use_cookies_with_metadata = true
+
+    # Change the return value of `ActionDispatch::Response#content_type` to Content-Type header without modification.
+    # config.action_dispatch.return_only_media_type_on_content_type = false
+
+    # Return false instead of self when enqueuing is aborted from a callback.
+    # config.active_job.return_false_on_aborted_enqueue = true
+
+    # Send Active Storage analysis and purge jobs to dedicated queues.
+    # config.active_storage.queues.analysis = :active_storage_analysis
+    # config.active_storage.queues.purge    = :active_storage_purge
+
+    # When assigning to a collection of attachments declared via `has_many_attached`, replace existing
+    # attachments instead of appending. Use #attach to add new attachments without replacing existing ones.
+    # config.active_storage.replace_on_assign_to_many = true
+
+    # Use ActionMailer::MailDeliveryJob for sending parameterized and normal mail.
+    #
+    # The default delivery jobs (ActionMailer::Parameterized::DeliveryJob, ActionMailer::DeliveryJob),
+    # will be removed in Rails 6.1. This setting is not backwards compatible with earlier Rails versions.
+    # If you send mail in the background, job workers need to have a copy of
+    # MailDeliveryJob to ensure all delivery jobs are processed properly.
+    # Make sure your entire app is migrated and stable on 6.0 before using this setting.
+    # config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
+
+    # Enable the same cache key to be reused when the object being cached of type
+    # `ActiveRecord::Relation` changes by moving the volatile information (max updated at and count)
+    # of the relation's cache key into the cache version to support recycling cache key.
+    # config.active_record.collection_cache_versioning = true
+
     # ==================================================================================================================
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
