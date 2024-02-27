99c3015e7c3ab9e595bfb0d9b3560afb3870309e
Jeff Marks <jeff.t.marks@gmail.com>
2024-02-27 12:20:56 -0500

    APPEALS-40909 Uncomment out code in migration to see if tests pass on GHA

--- db/migrate/20240226201012_add_index_to_conference_links.rb
+++ db/migrate/20240226201012_add_index_to_conference_links.rb
@@ -2,6 +2,6 @@ class AddIndexToConferenceLinks < Caseflow::Migration
   disable_ddl_transaction!
 
   def change
-    # add_index :conference_links, [:hearing_id, :hearing_type], algorithm: :concurrently
+    add_index :conference_links, [:hearing_id, :hearing_type], algorithm: :concurrently
   end
 end
