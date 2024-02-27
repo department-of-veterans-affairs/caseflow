4afa1b397e0cc0caec9a1bbee38fcc33305f7bc2
Ariana Konhilas <konhilas_ariana@bah.com>
2024-02-27 12:22:38 -0500

    APPEALS-40915: updated comments to job

--- app/jobs/hearings/create_non_virtual_conference_job.rb
+++ app/jobs/hearings/create_non_virtual_conference_job.rb
@@ -7,10 +7,11 @@ class Hearings::CreateNonVirtualConferenceJob < CaseflowJob
 
   queue_with_priority :high_priority
   application_attr :hearing_schedule
+  attr_reader :hearing
 
   class IncompleteError < StandardError; end
   class HearingRequestCancelled < StandardError; end
-  class ConferenceNotCreatedError < StandardError; end
+  class HearingNotCreatedError < StandardError; end
   class LinkGenerationFailed < StandardError; end
 
   discard_on(LinkGenerationFailed) do |job, _exception|
@@ -29,7 +30,7 @@ class Hearings::CreateNonVirtualConferenceJob < CaseflowJob
     Rails.logger.error("#{job.class.name} (#{job.job_id}) failed with error: #{exception}")
   end
 
-  retry_on(VirtualHearingNotCreatedError, attempts: 10, wait: :exponentially_longer) do |job, exception|
+  retry_on(HearingNotCreatedError, attempts: 10, wait: :exponentially_longer) do |job, exception|
     Rails.logger.error("#{job.class.name} (#{job.job_id}) failed with error: #{exception}")
   end
 
@@ -41,14 +42,24 @@ class Hearings::CreateNonVirtualConferenceJob < CaseflowJob
     extra = {
       application: job.class.app_name.to_s,
       hearing_id: kwargs[:hearing_id],
-      hearing_type: kwargs[:hearing_type]
     }
 
     Raven.capture_exception(exception, extra: extra)
   end
 
-  def perform
-    create_conference(hearing)
+  def perform(hearing:)
+    # job also determines if the hearing creates conference based on that (tests guest/host link present)
+    # hearing.guest_link.nil? <== only able to do after Jeff's migrations are in
+    # test type of hearing (non-virtual = travel video central) hearing.guest_link.nil?
+    # hearing.readable_request_type (works for legacy hearings too)
+    # if hearing.determine_service_name == "webex"
+    ensure_current_user_is_set
+    response = create_conference(hearing)
+    hearing.update(
+      host_hearing_link: response.host_link,
+      co_host_hearing_link: response.co_host_link,
+      guest_hearing_link: response.guest_link
+    )
   end
 
   private
@@ -63,5 +74,10 @@ class Hearings::CreateNonVirtualConferenceJob < CaseflowJob
       api_endpoint: ENV["WEBEX_API_IC"],
       query: nil
     ).create_conference(hearing)
+  end
+
+  def ensure_hearing(hearing)
+    fail HearingNotCreatedError if hearing.nil?
+    fail HearingRequestCancelled if hearing.cancelled?
   end
 end
