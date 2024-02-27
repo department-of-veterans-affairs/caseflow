4cf166dafbec1948176f2e68faac561449bf4519
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-02-27 17:19:58 -0500

    💡 Update comments

--- config/application.rb
+++ config/application.rb
@@ -20,33 +20,45 @@ module CaseflowCertification
     # the framework and any gems in your application.
 
     # ==================================================================================================================
+    # Rails default overrides
+    #   These settings override the defaults set by `config.load_defaults`.
+    #   If and as appropriate, we can transition these overrides one-by-one to migrate to their respective defaults.
+    #   See the Rails Guides: Configuring Rails Applications for more info on each option.
+    #   https://guides.rubyonrails.org/configuring.html
+    #
+    # ==================================================================================================================
     # Rails 5.0 default overrides
     # ------------------------------------------------------------------------------------------------------------------
+
     # Enable per-form CSRF tokens.
-    # Default (starting v5.0): true
+    # Default as of 5.0: true
     config.action_controller.per_form_csrf_tokens = false
 
     # Enable origin-checking CSRF mitigation.
-    # Default (starting v5.0): true
+    # Default as of 5.0: true
     config.action_controller.forgery_protection_origin_check = false
 
     # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-    # Default (starting v5.0): true
+    # Default as of 5.0: true
     ActiveSupport.to_time_preserves_timezone = false
 
     # Require `belongs_to` associations by default.
-    # Default (starting v5.0): true
+    # Default as of 5.0: true
     config.active_record.belongs_to_required_by_default = false
-    # ==================================================================================================================
+
+    # ------------------------------------------------------------------------------------------------------------------
     # Rails 5.1 default overrides
     # ------------------------------------------------------------------------------------------------------------------
+
     # Make `form_with` generate non-remote forms.
-    # Default (starting v5.1): true
-    # Default (starting v6.1): false
+    # Default as of 5.1: true
+    # Default as of 6.1: false
     Rails.application.config.action_view.form_with_generates_remote_forms = false
-    # ==================================================================================================================
+
+    # ------------------------------------------------------------------------------------------------------------------
     # Rails 5.2 default overrides
     # ------------------------------------------------------------------------------------------------------------------
+
     # Use AES-256-GCM authenticated encryption for encrypted cookies.
     # Also, embed cookie expiry in signed or encrypted cookies for increased security.
     #
@@ -54,21 +66,22 @@ module CaseflowCertification
     # It's best enabled when your entire app is migrated and stable on 5.2.
     #
     # Existing cookies will be converted on read then written with the new scheme.
-    # Default (starting v5.2): true
+    # Default as of 5.2: true
     Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = false
     #
     # Use AES-256-GCM authenticated encryption as default cipher for encrypting messages
     # instead of AES-256-CBC, when use_authenticated_message_encryption is set to true.
-    # Default (starting v5.2): true
+    # Default as of 5.2: true
     Rails.application.config.active_support.use_authenticated_message_encryption = false
 
     # Add default protection from forgery to ActionController::Base instead of in ApplicationController.
-    # Default (starting v5.2): true
+    # Default as of 5.2: true
     Rails.application.config.action_controller.default_protect_from_forgery = false
 
     # Store boolean values in sqlite3 databases as 1 and 0 instead of 't' and 'f' after migrating old data.
-    # Default (starting v5.2): true
+    # Default as of 5.2: true
     Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = false
+
     # ==================================================================================================================
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
