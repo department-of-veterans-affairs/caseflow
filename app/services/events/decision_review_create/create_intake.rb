f162d080a5c32c3f9ae80449a4a23a82bccc6e12
Enrilo Ugalde <ugalde_enrilo@bah.com>
2024-02-27 09:40:58 -0800

    APPEALS-40954 - added logic to CreateIntake module

--- app/services/events/decision_review_create/create_intake.rb
+++ app/services/events/decision_review_create/create_intake.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
+# This module starts the intial Intake creation and backfills the EventRecord
+# when a Decision Review Created Event is triggered
 module Events::DecisionReviewCreate::CreateIntake
-  # preview of call: Events::DecisionReviewCreate::CreateIntake.process!(event, veteran, user)
-  # self.process! method
-  # create Intake, args needed veteran.file_number and user: { user }
-  # intake = Intake.create!(veteran_file_number: vetran.file_number, user: user)
-  # create EventRecord by passing event: { event } and backfill_record: { intake } created
-  # end
-
-  # Error Handling
-  # create custom error called DecisionReviewCreatedIntakeError
-  # rescue Caseflow::Error::DecisionReviewCreatedIntakeError =>
-  #   raise error
-  # end
+  # This starts the process of the Intake creation and EventRecord backfill by passing in the event, user, and veteran
+  # that was created in the DecisionReviewCreated Service.
+  def self.process!(event, user, veteran)
+    # create Intake
+    intake = Intake.create!(veteran_file_number: veteran.file_number, user: user)
+    # create EventRecord
+    EventRecord.create!(event: event, backfill_record: intake)
+    # Error Handling
+  rescue Caseflow::Error::DecisionReviewCreatedIntakeError => error
+    raise error
+  end
 end
