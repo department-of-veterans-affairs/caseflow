b990eb13aa40740f6711bf7fc89e5e3a05051822
Enrilo Ugalde <ugalde_enrilo@bah.com>
2024-02-27 12:17:35 -0800

    APPEALS-40954 - added Create Intake spec  with error handling - All pass

---
+++ spec/services/events/decision_review_create/create_intake_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe Events::DecisionReviewCreate::CreateIntake do
+  context "Events::DecisionReviewCreate::CreateIntake.process!" do
+    let(:event_double) { double("Event") }
+    let(:user_double) { double("User") }
+    let(:veteran_double) { double("Veteran", file_number: "DCR02272024") }
+    let(:intake_double) { double("Intake") }
+    let(:event_record_double) { double("EventRecord") }
+    it "creates an intake and event record" do
+      allow(Intake).to receive(:create!).and_return(intake_double)
+      allow(EventRecord).to receive(:create!).and_return(event_record_double)
+      expect(Intake).to receive(:create!).with(veteran_file_number: "DCR02272024", user: user_double)
+        .and_return(intake_double)
+      expect(EventRecord).to receive(:create!).with(event: event_double, backfill_record: intake_double)
+        .and_return(event_record_double)
+      described_class.process!(event_double, user_double, veteran_double)
+    end
+    context "when an error occurs" do
+      let(:error) { Caseflow::Error::DecisionReviewCreatedIntakeError.new("Unable to create Intake") }
+      it "raises the error" do
+        allow(Intake).to receive(:create!).and_raise(error)
+        expect { described_class.process!(event_double, user_double, veteran_double) }.to raise_error(error)
+      end
+    end
+  end
+end
