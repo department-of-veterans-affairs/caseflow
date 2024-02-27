6026be837b7cb70386282de102e4a6b2706d5277
Jeff Marks <jeff.t.marks@gmail.com>
2024-02-27 10:55:55 -0500

    APPEALS-40909 Write spec for add index to conference links

---
+++ spec/migrations/add_index_to_conference_links_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require Rails.root.join("db/migrate/20240226201012_add_index_to_conference_links.rb")
+
+def hearing_index
+  ActiveRecord::Base.connection.indexes("conference_links").find do |idx|
+    idx.name == "index_conference_links_on_hearing_id_and_hearing_type"
+  end
+end
+
+describe AddIndexToConferenceLinks do
+  let(:migration_paths) { Rails.root.join("db/migrate") }
+  let(:test_version) { 20240226201012 }
+  let(:previous_version) { 20240226140103 }
+
+  describe "up" do
+    before :each do
+      ActiveRecord::MigrationContext.new(migration_paths).migrate(previous_version)
+      ConferenceLink.reset_column_information
+    end
+
+    subject do
+      ActiveRecord::MigrationContext.new(migration_paths).migrate(test_version)
+      ConferenceLink.reset_column_information
+    end
+
+    it "adds index on hearing_id and hearing_type to conference_links table" do
+      expect { subject }.to change { hearing_index.present? }.from(false).to(true)
+    end
+  end
+end
