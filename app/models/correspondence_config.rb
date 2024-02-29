ae6acd01b81c7afce439394338aa846d30ef3c05
Dev-KRedd <khalin.redding@va.gov>
2024-02-29 11:23:41 -0500

    Complexity fixes

--- app/models/correspondence_config.rb
+++ app/models/correspondence_config.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# :reek:RepeatedConditional
 
 class CorrespondenceConfig < QueueConfig
   def to_hash
@@ -13,6 +14,7 @@ class CorrespondenceConfig < QueueConfig
 
   private
 
+  # :reek:FeatureEnvy
   def attach_tasks_to_tab(tab)
     task_pager = CorrespondenceTaskPager.new(
       assignee: assignee,
