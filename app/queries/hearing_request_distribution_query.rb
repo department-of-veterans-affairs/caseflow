3240b0b1b0453d908252ced4fd4a04d3ef493aa2
Michael Beard <michael.beard@va.gov>
2024-02-29 10:18:25 -0600

    "Updates to formatting for shovel methods"

--- app/queries/hearing_request_distribution_query.rb
+++ app/queries/hearing_request_distribution_query.rb
@@ -47,20 +47,12 @@ class HearingRequestDistributionQuery
 
   def only_genpop_appeals
     appeals_to_return = []
-
-  # Include most_recent_held_hearings_ama_aod_hearing_original_appeals
-  # if case_distribution_lever is not infinite or omit
-    if !case_distribution_lever.infinite? && !case_distribution_lever.omit?
-      appeals_to_return << most_recent_held_hearings_ama_aod_hearing_original_appeals
-    end
-
-    appeals_to_return = []
     no_hearings_or_no_held_hearings = with_no_hearings.or(with_no_held_hearings)
 
     # returning early as most_recent_held_hearings_not_tied_to_any_judge is redundant
     if @use_by_docket_date &&
-       !(FeatureToggle.enabled?(:acd_cases_tied_to_judges_no_longer_with_board) ||
-        FeatureToggle.enabled?(:acd_exclude_from_affinity))
+      !(FeatureToggle.enabled?(:acd_cases_tied_to_judges_no_longer_with_board) ||
+      FeatureToggle.enabled?(:acd_exclude_from_affinity))
       return [
         with_held_hearings,
         no_hearings_or_no_held_hearings
@@ -76,6 +68,11 @@ class HearingRequestDistributionQuery
     appeals_to_return << most_recent_held_hearings_tied_to_ineligible_judge
     appeals_to_return << no_hearings_or_no_held_hearings
     appeals_to_return << most_recent_held_hearings_tied_to_judges_with_exclude_appeals_from_affinity
+
+    # Include most_recent_held_hearings_ama_aod_hearing_original_appeals
+    # if case_distribution_lever is not infinite or omit
+    appeals_to_return << most_recent_held_hearings_ama_aod_hearing_original_appeals if !case_distribution_lever.infinite?
+    appeals_to_return << most_recent_held_hearings_ama_aod_hearing_original_appeals if !case_distribution_lever.omit?
     appeals_to_return.flatten.uniq
   end
 
