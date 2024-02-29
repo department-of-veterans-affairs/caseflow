b69cdd87219642c8bfbc42622f073c67ec5a17f1
breedbah <brandon.reed3@va.gov>
2024-02-29 13:54:15 -0500

    APPEALS-39907 adjusted lint

--- app/mailers/transcript_file_issues_mailer.rb
+++ app/mailers/transcript_file_issues_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class TranscriptFileIssuesMailer < ActionMailer::Base
+class TranscriptFileIssuesMailer < ApplicationMailer
   default from: "Board of Veterans' Appeals <BoardofVeteransAppealsHearings@messages.va.gov>"
   default to: "BVAHearingTeam@VA.gov"
   default cc: "OITAppealsHelpDesk@va.gov"
@@ -9,15 +9,15 @@ class TranscriptFileIssuesMailer < ActionMailer::Base
   def send_issue_details(details, appeal_id)
     @details = details
     @case_link = case Rails.deploy_env
-                  when :demo
-                    "https://caseflowdemo.com/queue/appeals/#{appeal_id}"
-                  when :staging
-                    "https://appeals.cf.uat.ds.va.gov/queue/appeals/#{appeal_id}"
-                  when :prod, :prodtest, :preprod
-                    "https://appeals.cf.ds.va.gov/queue/appeals/#{appeal_id}"
-                  else
-                    "localhost:3000/queue/appeals/#{appeal_id}"
-                  end
+                 when :demo
+                   "https://caseflowdemo.com/queue/appeals/#{appeal_id}"
+                 when :staging
+                   "https://appeals.cf.uat.ds.va.gov/queue/appeals/#{appeal_id}"
+                 when :prod, :prodtest, :preprod
+                   "https://appeals.cf.ds.va.gov/queue/appeals/#{appeal_id}"
+                 else
+                   "localhost:3000/queue/appeals/#{appeal_id}"
+                 end
     @subject = "File #{details[:action]} Error - #{details[:provider]} #{details[:docket_number]}"
     mail(subject: @subject) do |format|
       format.html { render "layouts/transcript_file_issues" }
