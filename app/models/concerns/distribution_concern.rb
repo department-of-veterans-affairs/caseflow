2d918860ab924d92690880fafa5e0b2b9e80935a
Sean Craig <sean.craig2@va.gov>
2024-03-01 09:09:59 -0600

    added missing code for distrubtion

--- app/models/concerns/distribution_concern.rb
+++ app/models/concerns/distribution_concern.rb
@@ -5,14 +5,30 @@ module DistributionConcern
 
   private
 
-  # rubocop:disable Metrics/UnusedMethodArgument
-  # :reek:UnusedParameters
   def assign_judge_tasks_for_appeals(appeals, judge)
     appeals.map do |appeal|
       # If an appeal does not have an open DistributionTask, then it has already been distributed by automatic
       # case distribution and a new JudgeAssignTask should not be created. This should only occur if two users
       # request a distribution simultaneously.
-      next nil unless appeal.tasks.open.of_type(:DistributionTask).any? && appeal.can_redistribute_appeal?
+      if appeal.tasks.open.of_type(:DistributionTask).any? && appeal.can_redistribute_appeal?
+        distribution_task_assignee_id = appeal.tasks.of_type(:DistributionTask).first.assigned_to_id
+        Rails.logger.info("Calling JudgeAssignTaskCreator for appeal #{appeal.id} with judge #{judge.css_id}")
+        JudgeAssignTaskCreator.new(appeal: appeal,
+                                   judge: judge,
+                                   assigned_by_id: distribution_task_assignee_id).call
+      else
+        msg = "Appeal ID #{appeal.id} cannot be distributed. Check its task tree and manually remediate if necessary"
+        title = "Appeal unable to be distributed"
+        SlackService.new(url: slack_url).send_notification(msg, title)
+
+        nil
+      end
+    end
+  end
+
+  def assign_sct_tasks_for_appeals(appeals)
+    appeals.map do |appeal|
+      next nil unless appeal.tasks.open.of_type(:DistributionTask).any?
 
       distribution_task_assignee_id = appeal.tasks.of_type(:DistributionTask).first.assigned_to_id
       Rails.logger.info("Calling SpecialtyCaseTeamAssignTaskCreator for appeal #{appeal.id}")
@@ -20,7 +36,6 @@ module DistributionConcern
                                              assigned_by_id: distribution_task_assignee_id).call
     end
   end
-  # rubocop:enable Metrics/UnusedMethodArgument
 
   def cancel_previous_judge_assign_task(appeal, judge_id)
     appeal.tasks.of_type(:JudgeAssignTask).where.not(assigned_to_id: judge_id).update(status: :cancelled)
