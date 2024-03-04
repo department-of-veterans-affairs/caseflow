6a02a9d859206af6c306d94457eaecf710575ae0
Ryan Marshall <rnmarshall1993@gmail.com>
2024-03-04 07:45:00 -0800

    refactored to be cleaner

--- app/models/tasks/correspondence_task_filter.rb
+++ app/models/tasks/correspondence_task_filter.rb
@@ -31,52 +31,33 @@ class CorrespondenceTaskFilter < TaskFilter
     end
   end
 
-  # def filter_by_task(task_type, results)
-  #   t = Task.where(type: task_type)
-  #   binding.pry
-  #   t
-  # end
-
-  # def filter_by_task(task_types, items)
-  #   collection = nil
-  #   task_types.each do |task_type|
-  #     t = Task.where(type: task_type)
-  #     binding.pry
-  #     items.merge(t)
-  #     collection = collection.nil? ? items.merge(t) : collection + items.merge(t)
-  #   end
-  #   collection
-  # end
-
-  def filter_by_task(task_types, items)
+  def filter_by_task(task_types)
     collection = nil
     task_types.each do |task_type|
-      t = items.where(type: task_type)
-      items.merge(t)
-      # collection = collection.nil? ? items.merge(t) : collection + items.merge(t)
-      collection = collection.nil? ? items.merge(t) : collection.or(items.merge(t))
-
+      t = tasks.where(type: task_type)
+      collection = collection.nil? ? tasks.merge(t) : collection.or(tasks.merge(t))
     end
-    # binding.pry
     collection
   end
 
   def filtered_tasks
     va_dor_params = filter_params.select { |param| param.include?("col=vaDor") }
     task_column_params = filter_params.select { |param| param.include?("col=taskColumn") }
+    # task_column_params comes in as a single string, delimited by |. Updates task_column_params
+    # to be an array of the incoming values.
     unless task_column_params == []
       task_column_params[0].slice!("col=taskColumn&val=")[0]
       task_column_params = task_column_params[0].split("|")
     end
 
-    result = tasks.all # Assuming Task is the name of your ActiveRecord model
+    result = tasks.all
     va_dor_params.each do |param|
       value_hash = Rack::Utils.parse_nested_query(param).deep_symbolize_keys
       result = result.merge(filter_by_va_dor(value_hash[:val]))
     end
 
-    result = task_column_params.length > 0 ? result.merge(filter_by_task(task_column_params, result)) : result
-    unless task_column_params.length == 0
+    unless task_column_params.empty?
+      result = result.merge(filter_by_task(task_column_params))
     end
     result
   end
