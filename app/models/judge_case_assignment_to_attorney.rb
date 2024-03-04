d4af994226caaef74b0c79c8be7a96cd89956b91
Craig Reese <Reese_Craig@bah.com>
2024-03-04 14:37:07 -0600

    fix bug when assigning to attorney because of new param

--- app/models/judge_case_assignment_to_attorney.rb
+++ app/models/judge_case_assignment_to_attorney.rb
@@ -3,7 +3,7 @@
 class JudgeCaseAssignmentToAttorney
   include ActiveModel::Model
 
-  attr_accessor :appeal_id, :assigned_to, :task_id, :assigned_by, :judge
+  attr_accessor :appeal_id, :assigned_to, :assigned_to_id, :task_id, :assigned_by, :judge
 
   validates :assigned_by, :assigned_to, presence: true
   validates :task_id, format: { with: /\A[0-9A-Z]+-[0-9]{4}-[0-9]{2}-[0-9]{2}\Z/i }, allow_blank: true
