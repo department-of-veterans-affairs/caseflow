f6e1b03cc184eea011d4231cf25c4605ba2c63e3
William Medders <medders_william@bah.com>
2024-03-04 15:26:57 -0600

    Fix error when submitting correspondence intake

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -11,11 +11,11 @@ class Correspondence < CaseflowRecord
   include PrintsTaskTree
   include AppealableCorrespondence
 
-  has_many :correspondence_documents, dependent: :destroy
-  has_many :correspondence_intakes, dependent: :destroy
-  has_many :correspondences_appeals, dependent: :destroy
+  has_many :correspondence_documents
+  has_many :correspondence_intakes
+  has_many :correspondences_appeals
   has_many :appeals, through: :correspondences_appeals
-  has_many :correspondence_relations, dependent: :destroy
+  has_many :correspondence_relations
   has_many :related_correspondences, through: :correspondence_relations, dependent: :destroy
   belongs_to :correspondence_type
   belongs_to :package_document_type
@@ -51,5 +51,9 @@ class Correspondence < CaseflowRecord
   # Alias for package_document_type.name
   def docket_name
     package_document_type.name
+  end
+
+  def veteran_full_name
+    veteran.name
   end
 end
