7c92e2c99b3d2e8c152a6c0a7bc3b4580564cfca
(Jeffrey) Aaron Willis <98484567+Aaron-Willis@users.noreply.github.com>
2024-03-05 15:06:35 -0500

    Revert "feature/APPEALS-28087"

--- app/serializers/api/v3/issues/vacols/vacols_issue_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Api::V3::Issues::Vacols::VacolsIssueSerializer
-  include FastJsonapi::ObjectSerializer
-
-  # attributes :vacols_issue
-
-  attributes :vacols_issue, &:vbms_attributes
-end
