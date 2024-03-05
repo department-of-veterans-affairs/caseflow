67a1341bcba21fe9eecfa8d6936ec11263d47964
William Medders <medders_william@bah.com>
2024-03-05 15:34:22 -0600

    create abstract base class for tasks not related to an appeal

---
+++ app/models/tasks/mail_tasks_not_related_to_appeal/correspondence_mail_task.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Abstract base class for "tasks not related to an appeal" added to a correspondence during Correspondence Intake.
+class CorrespondenceMailTask < CorrespondenceTask
+  abstract_class: true
+end
