d66b7be416c9b1765b6a13463059f2608bf7f2d6
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-03-05 23:04:57 -0500

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_30_191529) do
+ActiveRecord::Schema.define(version: 2024_01_06_021307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -602,25 +602,20 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
   create_table "conference_links", force: :cascade do |t|
     t.string "alias", comment: "Alias of the conference"
     t.string "alias_with_host", comment: "Alieas of the conference for the host"
-    t.string "co_host_link"
     t.boolean "conference_deleted", default: false, null: false, comment: "Flag to represent if a con ference has been deleted"
-    t.string "conference_id", comment: "Id of the conference"
+    t.integer "conference_id", comment: "Id of the conference"
     t.datetime "created_at", null: false, comment: "Date and Time of creation"
     t.bigint "created_by_id", null: false, comment: "User id of the user who created the record. FK on User table"
-    t.datetime "deleted_at", comment: "Needed column to make use of the paranoia gem."
     t.string "guest_hearing_link", comment: "Guest link for hearing daily docket."
     t.string "guest_pin_long", comment: "Pin provided for the guest, allowing them entry into the video conference."
     t.bigint "hearing_day_id", null: false, comment: "The associated hearing day id"
     t.string "host_link", comment: "Conference link generated from external conference service"
     t.integer "host_pin", comment: "Pin for the host of the conference to get into the conference"
     t.string "host_pin_long", limit: 8, comment: "Generated host pin stored as a string"
-    t.string "type", comment: "Pexip or Webex conference link"
     t.datetime "updated_at", comment: "Date and Time record was last updated"
     t.bigint "updated_by_id", comment: "user id of the user to last update the record. FK on the User table"
     t.index ["created_by_id"], name: "index_created_by_id"
-    t.index ["deleted_at"], name: "index_conference_links_on_deleted_at"
     t.index ["hearing_day_id"], name: "index_conference_links_on_hearing_day_id"
-    t.index ["type"], name: "index_conference_links_on_type"
     t.index ["updated_by_id"], name: "index_updated_by_id"
   end
 
@@ -1053,7 +1048,6 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
     t.boolean "prepped", comment: "Determines whether the judge has checked the hearing as prepped"
     t.string "representative_name", comment: "Name of Appellant's representative if applicable"
     t.string "room", comment: "The room at BVA where the hearing will take place; ported from associated HearingDay"
-    t.string "scheduled_in_timezone", comment: "Named TZ string that the hearing will have to provide accurate hearing times."
     t.time "scheduled_time", null: false, comment: "Date and Time when hearing will take place"
     t.text "summary", comment: "Summary of hearing"
     t.boolean "transcript_requested", comment: "Determines whether the veteran/appellant has requested the hearing transcription"
@@ -1220,7 +1214,6 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
     t.string "military_service", comment: "Periods and circumstances of military service"
     t.string "original_vacols_request_type", comment: "The original request type of the hearing in VACOLS, before it was changed to Virtual"
     t.boolean "prepped", comment: "Determines whether the judge has checked the hearing as prepped"
-    t.string "scheduled_in_timezone", comment: "Named TZ string that the legacy hearing will have to provide accurate hearing times."
     t.text "summary", comment: "Summary of hearing"
     t.datetime "updated_at", comment: "Timestamp when record was last updated."
     t.bigint "updated_by_id", comment: "The ID of the user who most recently updated the Legacy Hearing"
@@ -1261,13 +1254,6 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
     t.string "vacols_id", null: false, comment: "The VACOLS ID of the legacy appeal that the legacy issue is part of."
     t.integer "vacols_sequence_id", null: false, comment: "The sequence ID of the legacy issue on the legacy appeal. The vacols_id and vacols_sequence_id form a composite key to identify a specific legacy issue."
     t.index ["request_issue_id"], name: "index_legacy_issues_on_request_issue_id"
-  end
-
-  create_table "meeting_types", force: :cascade do |t|
-    t.bigint "conferenceable_id"
-    t.string "conferenceable_type"
-    t.integer "service_name", default: 0, comment: "Pexip or Webex Instant Connect"
-    t.index ["conferenceable_type", "conferenceable_id"], name: "conferenceable_association_idx"
   end
 
   create_table "membership_requests", force: :cascade do |t|
@@ -1857,30 +1843,6 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
     t.index ["updated_at"], name: "index_team_quotas_on_updated_at"
   end
 
-  create_table "transcription_files", force: :cascade do |t|
-    t.string "aws_link", comment: "Link to be used by HMB to download original or transformed file"
-    t.datetime "created_at", null: false
-    t.bigint "created_by_id", comment: "The user who created the transcription record"
-    t.datetime "date_converted", comment: "Timestamp when file was converted from vtt to rtf"
-    t.datetime "date_receipt_webex", comment: "Timestamp when file was added to webex"
-    t.datetime "date_upload_aws", comment: "Timestamp when file was loaded to AWS"
-    t.datetime "date_upload_box", comment: "Timestamp when file was added to box"
-    t.string "docket_number", null: false, comment: "Docket number of associated hearing"
-    t.string "file_name", null: false, comment: "File name, with extension, of the transcription file migrated by caseflow"
-    t.string "file_status", comment: "Status of the file, could be one of nil, 'Successful retrieval (Webex), Failed retrieval (Webex), Sucessful conversion, Failed conversion, Successful upload (AWS), Failed upload (AWS)'"
-    t.string "file_type", null: false, comment: "One of mp4, vtt, mp3, rtf, pdf, xls"
-    t.bigint "hearing_id", null: false, comment: "ID of the hearing associated with this record"
-    t.string "hearing_type", null: false, comment: "Type of hearing associated with this record"
-    t.datetime "updated_at", null: false
-    t.bigint "updated_by_id", comment: "The user who most recently updated the transcription file"
-    t.index ["aws_link"], name: "index_transcription_files_on_aws_link"
-    t.index ["docket_number"], name: "index_transcription_files_on_docket_number"
-    t.index ["file_name", "docket_number", "hearing_id", "hearing_type"], name: "idx_transcription_files_on_file_name_and_docket_num_and_hearing", unique: true
-    t.index ["file_type"], name: "index_transcription_files_on_file_type"
-    t.index ["hearing_id", "hearing_type", "docket_number"], name: "index_transcription_files_on_docket_number_and_hearing"
-    t.index ["hearing_id", "hearing_type"], name: "index_transcription_files_on_hearing_id_and_hearing_type"
-  end
-
   create_table "transcriptions", force: :cascade do |t|
     t.datetime "created_at", comment: "Automatic timestamp of when transcription was created"
     t.date "expected_return_date", comment: "Expected date when transcription would be returned by the transcriber"
@@ -2102,9 +2064,8 @@ ActiveRecord::Schema.define(version: 2024_01_30_191529) do
     t.boolean "appellant_email_sent", default: false, null: false, comment: "Determines whether or not a notification email was sent to the appellant"
     t.datetime "appellant_reminder_sent_at", comment: "The datetime the last reminder email was sent to the appellant."
     t.string "appellant_tz", limit: 50, comment: "Stores appellant timezone"
-    t.string "co_host_hearing_link"
     t.boolean "conference_deleted", default: false, null: false, comment: "Whether or not the conference was deleted from Pexip"
-    t.string "conference_id", comment: "ID of conference from Pexip"
+    t.integer "conference_id", comment: "ID of conference from Pexip"
     t.datetime "created_at", null: false, comment: "Automatic timestamp of when virtual hearing was created"
     t.bigint "created_by_id", null: false, comment: "User who created the virtual hearing"
     t.string "guest_hearing_link", comment: "Link used by appellants and/or representatives to join virtual hearing conference"
