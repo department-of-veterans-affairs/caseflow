9ff686225f6c6256174bee35bb6934ad13cbead8
Craig Reese <Reese_Craig@bah.com>
2024-03-07 08:59:56 -0600

    remove old commented code

--- app/queries/hearing_request_distribution_query.rb
+++ app/queries/hearing_request_distribution_query.rb
@@ -46,40 +46,6 @@ class HearingRequestDistributionQuery
   end
 
   def only_genpop_appeals
-    # no_hearings_or_no_held_hearings = with_no_hearings.or(with_no_held_hearings)
-
-    # returning early as most_recent_held_hearings_not_tied_to_any_judge is redundant
-    # if @use_by_docket_date &&
-    #    !(FeatureToggle.enabled?(:acd_cases_tied_to_judges_no_longer_with_board) ||
-    #     FeatureToggle.enabled?(:acd_exclude_from_affinity))
-    #   return [
-    #     with_held_hearings,
-    #     no_hearings_or_no_held_hearings
-    #   ].flatten.uniq
-    # end
-
-    # We are combining two queries using an array because using `or` doesn't work
-    # due to incompatibilities between the two queries.
-    # [
-    #   most_recent_held_hearings_not_tied_to_any_judge,
-    #   most_recent_held_hearings_exceeding_affinity_threshold,
-    #   most_recent_held_hearings_tied_to_ineligible_judge,
-    #   no_hearings_or_no_held_hearings,
-    #   most_recent_held_hearings_tied_to_judges_with_exclude_appeals_from_affinity
-    # ].flatten.uniq
-
-    # this is a test to see how the queries will change if we opt to do one large chain of activerecord queries
-    # instead of several small ones. this may be beneficial because of the inefficient joins for the most recently
-    # held hearing
-    # base_relation_with_joined_most_recent_hearings_and_dist_task
-    #   .exceeding_affinity_threshold
-    #   .or(base_relation_with_joined_most_recent_hearings_and_dist_task.tied_to_ineligible_judge)
-    #   .or(base_relation_with_joined_most_recent_hearings_and_dist_task.tied_to_judges_with_exclude_appeals_from_affinity)
-    #   .or(base_relation_with_joined_most_recent_hearings_and_dist_task.not_tied_to_any_judge)
-    #   .or(base_relation_with_joined_most_recent_hearings_and_dist_task.with_no_held_hearings)
-    #   .or(base_relation_with_joined_most_recent_hearings_and_dist_task.with_no_hearings)
-
-    # test which should allow us to merge scopes in based on whether the feature toggle is true
     result = base_relation_with_joined_most_recent_hearings_and_dist_task.exceeding_affinity_threshold
 
     if FeatureToggle.enabled?(:acd_cases_tied_to_judges_no_longer_with_board)
