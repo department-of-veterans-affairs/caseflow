d0eda9fb5b058abc4ca960fa1081f52727753f03
TuckerRose <tuckerrose@tuckerroses-mbp.lan>
2024-03-07 09:52:38 -0500

    change to a class

--- app/services/events/create_claimant_on_event.rb
+++ app/services/events/create_claimant_on_event.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 class Events::CreateClaimantOnEvent
-  def self.process(event, is_veteran_claimant)
-    epe = EndProductEstablishment.find_by(reference_id: event.reference_id)
-    veteran = epe.veteran
+  class << self
+    def process(event, is_veteran_claimant)
+      epe = EndProductEstablishment.find_by(reference_id: event.reference_id)
+      veteran = epe.veteran
 
-    if is_veteran_claimant
-      event.reference_id
-    else
-      claimant = Claimant.create!(
-        decision_review: epe.source,
-        participant_id: veteran.participant_id,
-        payee_code: epe.payee_code,
-        decision_review_type: epe.source_type
-      )
-      EventRecord.create!(event: event, backfill_record: claimant)
-      claimant.id
+      if is_veteran_claimant
+        event.reference_id
+      else
+        claimant = Claimant.create!(
+          decision_review: epe.source,
+          participant_id: veteran.participant_id,
+          payee_code: epe.payee_code,
+          decision_review_type: epe.source_type
+        )
+        EventRecord.create!(event: event, backfill_record: claimant)
+        claimant.id
+      end
     end
   end
 end
