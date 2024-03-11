0c1d42c6e9ac38940d2c76cba204c18f82627a2e
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-03-11 12:32:07 -0500

    Add scroll metric and reader specs

--- client/app/reader/PdfFile.jsx
+++ client/app/reader/PdfFile.jsx
@@ -44,6 +44,7 @@ export class PdfFile extends React.PureComponent {
     this.currentPage = 0;
     this.columnCount = 1;
     this.metricsIdentifier = null;
+    this.scrollTimer = null;
   }
 
   componentDidMount = () => {
@@ -215,6 +216,10 @@ export class PdfFile extends React.PureComponent {
     }
 
     this.metricsIdentifier = null;
+
+    if (this.scrollTimer) {
+      clearTimeout(this.scrollTimer);
+    }
   }
 
   getPage = ({ rowIndex, columnIndex, style, isVisible }) => {
@@ -444,6 +449,38 @@ export class PdfFile extends React.PureComponent {
       });
 
       this.onPageChange(minIndex, clientHeight);
+
+      if (this.scrollTimer) {
+        clearTimeout(this.scrollTimer);
+      }
+
+      this.scrollTimer = setTimeout(() => {
+        const scrollStart = performance.now();
+
+        const data = {
+          overscan: this.props.windowingOverscan,
+          documentType: this.props.documentType,
+          pageCount: this.props.pdfDocument.numPages,
+          pageIndex: this.pageIndex,
+          prefetchDisabled: this.props.featureToggles.prefetchDisabled,
+          start: scrollStart,
+          end: performance.now()
+        };
+
+        storeMetrics(
+          this.props.documentId,
+          data,
+          {
+            message: `Scroll to position ${this.scrollLeft}, ${this.scrollTop}`,
+            type: 'performance',
+            product: 'reader',
+            start: new Date(performance.timeOrigin + data.start),
+            end: new Date(performance.timeOrigin + data.end),
+            duration: data.start ? data.end - data.start : 0
+          },
+          this.metricIdentifier,
+        );
+      }, 300);
     }
   }
 
