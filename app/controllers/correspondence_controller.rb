67fd33bfb240849e2cb24f953dcdac62fe39619e
sulak-j <justin.sulak@va.gov>
2024-03-12 15:19:55 -0500

    moving methods that seem to be suited for the intake-specific controller

--- app/controllers/correspondence_controller.rb
+++ app/controllers/correspondence_controller.rb
@@ -8,37 +8,6 @@ class CorrespondenceController < ApplicationController
   before_action :auto_texts
   before_action :veteran_information
 
-  def intake
-    respond_to do |format|
-      format.html { return render "correspondence/intake" }
-      format.json do
-        render json: {
-          currentCorrespondence: current_correspondence,
-          correspondence: correspondence_load,
-          veteranInformation: veteran_information
-        }
-      end
-    end
-  end
-
-  def current_step
-    intake = CorrespondenceIntake.find_by(user: current_user, correspondence: current_correspondence) ||
-             CorrespondenceIntake.new(user: current_user, correspondence: current_correspondence)
-
-    intake.update(
-      current_step: params[:current_step],
-      redux_store: params[:redux_store]
-    )
-
-    if intake.valid?
-      intake.save!
-
-      render(json: {}, status: :ok) && return
-    else
-      render(json: intake.errors.full_messages, status: :unprocessable_entity) && return
-    end
-  end
-
   def correspondence_cases
     respond_to do |format|
       format.html { "correspondence_cases" }
@@ -50,20 +19,6 @@ class CorrespondenceController < ApplicationController
 
   def review_package
     render "correspondence/review_package"
-  end
-
-  def intake_update
-    begin
-      intake_appeal_update_tasks
-      if FeatureToggle.enabled?(:ce_api_demo_toggle)
-        upload_documents_to_claim_evidence
-      end
-      render json: { correspondence: correspondence }
-    rescue StandardError => error
-      Rails.logger.error(error.to_s)
-      Raven.capture_exception(error)
-      render json: {}, status: :bad_request
-    end
   end
 
   def veteran
@@ -163,15 +118,6 @@ class CorrespondenceController < ApplicationController
     )
   end
 
-  def process_intake
-    if correspondence_intake_processor.process_intake(params, current_user)
-      set_flash_intake_success_message
-      render json: {}, status: :created
-    else
-      render json: { error: "Failed to update records" }, status: :bad_request
-    end
-  end
-
   private
 
   # :reek:FeatureEnvy
@@ -246,10 +192,6 @@ class CorrespondenceController < ApplicationController
     end
 
     @correspondence
-  end
-
-  def correspondence_load
-    Correspondence.where(veteran_id: veteran_by_correspondence.id).where.not(uuid: params[:correspondence_uuid])
   end
 
   def veteran_by_correspondence
--- app/controllers/correspondence_intake_controller.rb
+++ app/controllers/correspondence_intake_controller.rb
@@ -1,5 +1,63 @@
 # frozen_string_literal: true
 
 class CorrespondenceIntakeController < CorrespondenceController
+  def intake
+    respond_to do |format|
+      format.html { return render "correspondence/intake" }
+      format.json do
+        render json: {
+          currentCorrespondence: current_correspondence,
+          correspondence: correspondence_load,
+          veteranInformation: veteran_information
+        }
+      end
+    end
+  end
 
+  def current_step
+    intake = CorrespondenceIntake.find_by(user: current_user, correspondence: current_correspondence) ||
+             CorrespondenceIntake.new(user: current_user, correspondence: current_correspondence)
+
+    intake.update(
+      current_step: params[:current_step],
+      redux_store: params[:redux_store]
+    )
+
+    if intake.valid?
+      intake.save!
+
+      render(json: {}, status: :ok) && return
+    else
+      render(json: intake.errors.full_messages, status: :unprocessable_entity) && return
+    end
+  end
+
+  def intake_update
+    begin
+      intake_appeal_update_tasks
+      if FeatureToggle.enabled?(:ce_api_demo_toggle)
+        upload_documents_to_claim_evidence
+      end
+      render json: { correspondence: correspondence }
+    rescue StandardError => error
+      Rails.logger.error(error.to_s)
+      Raven.capture_exception(error)
+      render json: {}, status: :bad_request
+    end
+  end
+
+  def process_intake
+    if correspondence_intake_processor.process_intake(params, current_user)
+      set_flash_intake_success_message
+      render json: {}, status: :created
+    else
+      render json: { error: "Failed to update records" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def correspondence_load
+    Correspondence.where(veteran_id: veteran_by_correspondence.id).where.not(uuid: params[:correspondence_uuid])
+  end
 end
