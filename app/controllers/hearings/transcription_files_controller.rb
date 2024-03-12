abe649591208c7e3a647bcd3eba11ea482d4ba59
Minhazur Rahaman <minhazur9@gmail.com>
2024-03-12 16:14:16 -0400

    APPEALS-37605 file now gets downloaded onto local computer

--- app/controllers/hearings/transcription_files_controller.rb
+++ app/controllers/hearings/transcription_files_controller.rb
@@ -13,13 +13,16 @@ class TranscriptionFilesController < ApplicationController
     nil
   end
 
+  # Downloads file and sends to user's local computer
   def download_transcription_file
+    hearing_id = params[:hearing_id]
     respond_to do |format|
       format.any(:vtt, :mp3, :mp4, :csv) do |_|
-        render json: {
-          message: "Route working, showing hearing of id: #{params[:hearing_id]}",
-          status: 200
-        }
+        type = format.format&.symbol
+        file = TranscriptionFile.find_by(hearing_id: hearing_id, file_type: type.to_s)
+        tmp_location = file.download_from_s3
+        File.open(tmp_location, "r") { |f| send_data f.read }
+        File.delete(tmp_location) if File.exist?(tmp_location)
       end
     end
   end
