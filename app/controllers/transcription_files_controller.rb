4c00505f3b5732cc22efb3490bc41dfdc739b158
Minhazur Rahaman <minhazur9@gmail.com>
2024-03-14 13:58:59 -0400

    APPEALS-37605 added more error handling

--- app/controllers/transcription_files_controller.rb
+++ app/controllers/transcription_files_controller.rb
@@ -3,6 +3,8 @@
 class TranscriptionFilesController < ApplicationController
   include HearingsConcerns::VerifyAccess
 
+  rescue_from ActionController::UnknownFormat, with: :render_page_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :render_page_not_found
   before_action :verify_access_to_hearings, only: [:download_transcription_file]
 
   def index
@@ -23,11 +25,15 @@ class TranscriptionFilesController < ApplicationController
     respond_to do |format|
       format.any(:vtt, :mp3, :mp4, :csv) do |_|
         type = format.format&.symbol.to_s
-        file = TranscriptionFile.find_by(hearing_id: hearing_id, file_type: type)
+        file = TranscriptionFile.find_by!(hearing_id: hearing_id, file_type: type)
         tmp_location = file.download_from_s3
         File.open(tmp_location, "r") { |f| send_data f.read }
         File.delete(tmp_location) if File.exist?(tmp_location)
       end
     end
+  end
+
+  def render_page_not_found
+    redirect_to "/404"
   end
 end
