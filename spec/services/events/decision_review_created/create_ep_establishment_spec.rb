5bfce8f3a4fa0d72dd3bb5fe67cc4dea44cbd132
Enrilo Ugalde <ugalde_enrilo@bah.com>
2024-03-14 09:23:28 -0700

    APPEALS-41931 - removed lint

--- spec/services/events/decision_review_created/create_ep_establishment_spec.rb
+++ spec/services/events/decision_review_created/create_ep_establishment_spec.rb
@@ -4,23 +4,22 @@ describe Events::DecisionReviewCreated::CreateEpEstablishment do
   context "Events::DecisionReviewCreated::CreateEpEstablishment.process!" do
     # set up variables station_id, end_product_establishment, claim_review, user, event
     let!(:station_id) { "101" }
-    let!(:user_double) { double("User", id: 1)}
+    let!(:user_double) { double("User", id: 1) }
     let!(:event_double) { double("Event") }
     let!(:claim_review) { create(:higher_level_review) }
-    let!(:commit) { 1702067145000 }
     let(:end_product_establishment_double) do
       instance_double("EndProductEstablishmentDouble",
-      payee_code: "00",
-      claim_date: 2.days.ago,
-      code: "030HLRRPMC",
-      committed_at: 1702067145000,
-      established_at: 1702067145000,
-      last_synced_at: 1702067145000,
-      limited_poa_access: nil,
-      limited_poa_code: nil,
-      modifier: "030",
-      reference_id: "337534",
-      synced_status: "RW")
+                      payee_code: "00",
+                      claim_date: 2.days.ago,
+                      code: "030HLRRPMC",
+                      committed_at: 170_206_714_500_0,
+                      established_at: 170_206_714_500_0,
+                      last_synced_at: 170_206_714_500_0,
+                      limited_poa_access: nil,
+                      limited_poa_code: nil,
+                      modifier: "030",
+                      reference_id: "337534",
+                      synced_status: "RW")
     end
     let(:event_record_double) { double("EventRecord") }
     it "calls.process!" do
