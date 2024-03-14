847b828293dd431ce955eedb955735dc5e2c25d4
Minhazur Rahaman <minhazur9@gmail.com>
2024-03-14 12:25:17 -0400

    APPEALS-37605 finished writing rspec tests

--- app/controllers/transcription_files_controller.rb
+++ app/controllers/transcription_files_controller.rb
@@ -20,10 +20,11 @@ class TranscriptionFilesController < ApplicationController
   # Downloads file and sends to user's local computer
   def download_transcription_file
     hearing_id = params[:hearing_id]
+    # status = 401
     respond_to do |format|
       format.any(:vtt, :mp3, :mp4, :csv) do |_|
-        type = format.format&.symbol
-        file = TranscriptionFile.find_by(hearing_id: hearing_id, file_type: type.to_s)
+        type = format.format&.symbol.to_s
+        file = TranscriptionFile.find_by(hearing_id: hearing_id, file_type: type)
         tmp_location = file.download_from_s3
         File.open(tmp_location, "r") { |f| send_data f.read }
         File.delete(tmp_location) if File.exist?(tmp_location)
---
+++ spec/controllers/transcription_files_controller_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+describe TranscriptionFilesController, :all_dbs, type: :controller do
+  let(:hearings_user) { create(:hearings_coordinator) }
+  before { User.authenticate!(user: hearings_user) }
+  describe "routes" do
+    let!(:hearing) { create(:hearing, :held) }
+    let(:options) { { format: :vtt, hearing_id: hearing.id } }
+    subject { get :download_transcription_file, params: options }
+
+    it "downloading file" do
+      subject
+      expect(response.status).to eq(302)
+    end
+
+    context "when not a hearings user" do
+      let(:vso_user) { create(:user, :vso_role) }
+      before do
+        User.unauthenticate!
+        User.authenticate!(user: vso_user)
+      end
+      it "downloading a file" do
+        subject
+        expect(response).to redirect_to("/unauthorized")
+      end
+    end
+  end
+end
