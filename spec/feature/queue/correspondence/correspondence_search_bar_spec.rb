74173bac99e84dcc15a6565022f4da7ea56241f9
divyadasari-va <divya.dasari@va.gov>
2024-03-15 12:04:23 -0600

    Feature test cases for Search bar

---
+++ spec/feature/queue/correspondence/correspondence_search_bar_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.feature("Search Bar for Correspondence") do
+  include CorrespondenceTaskHelpers
+  # alias this to avoid the method name collision
+  alias_method :create_efolderupload_task, :create_efolderupload_failed_task
+
+  context "correspondece cases feature toggle" do
+    let(:current_user) { create(:user) }
+    before :each do
+      MailTeam.singleton.add_user(current_user)
+      User.authenticate!(user: current_user)
+      @correspondence_uuid = "123456789"
+    end
+
+    it "routes user to /under_construction if the feature toggle is disabled" do
+      FeatureToggle.disable!(:correspondence_queue)
+      visit "/queue/correspondence"
+      expect(page).to have_current_path("/under_construction")
+    end
+
+    it "routes to correspondence cases if feature toggle is enabled" do
+      FeatureToggle.enable!(:correspondence_queue)
+      visit "/queue/correspondence"
+      expect(page).to have_current_path("/queue/correspondence?tab=correspondence_assigned&page=1&sort_by=vaDor&order=asc")
+    end
+  end
+
+  context "correspondence assigned tab - locate the search bar" do
+    let(:current_user) { create(:user) }
+    before :each do
+      MailTeam.singleton.add_user(current_user)
+      User.authenticate!(user: current_user)
+    end
+
+    before do
+      10.times do
+        review_correspondence = create(:correspondence)
+        rpt = ReviewPackageTask.find_by(appeal_id: review_correspondence.id)
+        rpt.update!(assigned_to: current_user, status: "assigned")
+        rpt.save!
+      end
+      FeatureToggle.enable!(:correspondence_queue)
+    end
+
+    it "successfully loads the assigned tab locate the search bar" do
+      visit "/queue/correspondence/team?tab=correspondence_team_assigned"
+      expect(page).to have_content("Filter table by any of its columns")
+      veteran = Veteran.first
+      find_by_id("searchBar").fill_in with: veteran.last_name
+      search_value = find("tbody > tr:nth-child(1) > td:nth-child(1)").text
+      expect(search_value.include?(veteran.last_name))
+    end
+  end
+end
