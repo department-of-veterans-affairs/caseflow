c842954429a936892dd82224935ae1f0211548d0
sulak-j <justin.sulak@va.gov>
2024-03-15 11:58:09 -0500

    removing a repeated method found

--- config/routes.rb
+++ config/routes.rb
@@ -328,7 +328,6 @@ Rails.application.routes.draw do
     post '/correspondence/:correspondence_uuid/correspondence_intake_task', to: 'correspondence_tasks#create_correspondence_intake_task'
     post '/correspondence/:id/remove_package', to: 'correspondence_tasks#remove_package'
     post '/correspondence/:id/completed_package', to: 'correspondence_tasks#completed_package'
-    get '/correspondence/:correspondence_uuid/intake', to: 'correspondence#intake'
     get '/correspondence/:correspondence_uuid/review_package', to: 'correspondence#review_package'
     get '/correspondence/edit_document_type_correspondence', to: 'correspondence#document_type_correspondence'
     patch '/correspondence/:correspondence_uuid/intake_update', to: 'correspondence_intake#intake_update'
