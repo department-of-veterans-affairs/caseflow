e02488abab1b3445b34572f095a7b42db6faad04
Craig Reese <Reese_Craig@bah.com>
2024-03-18 15:11:07 -0500

    remove comments

--- spec/models/dockets/hearing_request_docket_rewrite_spec.rb
+++ spec/models/dockets/hearing_request_docket_rewrite_spec.rb
@@ -588,7 +588,6 @@ describe HearingRequestDocket, :postgres do
     appeal
   end
 
-  # TODO: update
   def create_ready_cavc_appeal(tied_judge: nil, created_date: 1.year.ago)
     Timecop.travel(created_date - 6.months)
     if tied_judge
@@ -649,7 +648,6 @@ describe HearingRequestDocket, :postgres do
     appeal
   end
 
-  # TODO: update
   def create_not_ready_cavc_appeal(tied_judge: nil, created_date: 1.year.ago)
     Timecop.travel(created_date - 6.months)
     if tied_judge
