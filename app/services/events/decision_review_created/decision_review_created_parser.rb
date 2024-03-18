1e6565ec80bc2a1d8a793043520932764ed1b441
Jonathan Tsang <tsang_jonathan@bah.com>
2024-03-18 16:21:34 -0400

    added EPE attr

--- app/services/events/decision_review_created/decision_review_created_parser.rb
+++ app/services/events/decision_review_created/decision_review_created_parser.rb
@@ -17,6 +17,18 @@ class Events::DecisionReviewCreated::DecisionReviewCreatedParser
     Time.at(milliseconds / 1000).to_datetime
   end
 
+  def css_id
+    @payload.dig(:css_id)
+  end
+
+  def detail_type
+    @payload.dig(:detail_type)
+  end
+
+  def station_id
+    @payload.dig(:station)
+  end
+
   # Veteran attributes
   def veteran_file_number
     @veteran_file_number ||= @headers["X-VA-File-Number"].presence
@@ -59,5 +71,56 @@ class Events::DecisionReviewCreated::DecisionReviewCreatedParser
 
   # Intake attr
 
+  # EndProductEstablishment attr
+  def epe_benefit_type_code
+    @payload.dig(:end_product_establishment, :benefit_type_code)
+  end
 
+  def epe_claim_date
+    @payload.dig(:end_product_establishment, :claim_date)
+  end
+
+  def epe_code
+    @payload.dig(:end_product_establishment, :code)
+  end
+
+  def epe_modifier
+    @payload.dig(:end_product_establishment, :modifier)
+  end
+
+  def epe_payee_code
+    @payload.dig(:end_product_establishment, :payee_code)
+  end
+
+  def epe_reference_id
+    @payload.dig(:end_product_establishment, :reference_id)
+  end
+
+  def epe_limited_poa_access
+    @payload.dig(:end_product_establishment, :limited_poa_access)
+  end
+
+  def epe_limited_poa_code
+    @payload.dig(:end_product_establishment, :limited_poa_code)
+  end
+
+  def epe_committed_at
+    @payload.dig(:end_product_establishment, :committed_at)
+  end
+
+  def epe_established_at
+    @payload.dig(:end_product_establishment, :established_at)
+  end
+
+  def epe_last_synced_at
+    @payload.dig(:end_product_establishment, :last_synced_at)
+  end
+
+  def epe_synced_status
+    @payload.dig(:end_product_establishment, :synced_status)
+  end
+
+  def epe_development_item_reference_id
+    @payload.dig(:end_product_establishment, :development_item_reference_id)
+  end
 end
