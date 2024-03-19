4577fb9347ccd02b187143a96a3be73278b3d984
Jonathan Tsang <98970951+jtsangVA@users.noreply.github.com>
2024-03-19 14:50:31 -0400

    Jonathan/APPEALS-41957 (#21171)
    * added interface + parser class
    
    * edited veteran parse methods
    
    * renamed var to "payload"
    
    * started refactor
    
    * more refactor + rspec
    
    * added EPE attr
    
    * dateTime conversions
    
    * added class comments
    
    * rubocop lint changes
    
    * fixed test case
    
    ---------
    
    Co-authored-by: Jonathan Tsang <tsang_jonathan@bah.com>

---
+++ app/services/events/decision_review_created/decision_review_created_parser.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+# Parser Class that will be used to extract out datapoints from headers & payload for use with
+# DecisionReviewCreated and it's service Classes
+class Events::DecisionReviewCreated::DecisionReviewCreatedParser
+  include Events::VeteranExtractorInterface
+
+  attr_reader :headers, :payload
+
+  def initialize(headers, payload)
+    @payload = payload
+    @headers = headers
+    @veteran = @payload.dig(:veteran)
+  end
+
+
+  # Generic/universal methods
+  def convert_milliseconds_to_datetime(milliseconds)
+    Time.at(milliseconds / 1000).to_datetime
+  end
+
+  def css_id
+    @payload.dig(:css_id)
+  end
+
+  def detail_type
+    @payload.dig(:detail_type)
+  end
+
+  def station_id
+    @payload.dig(:station)
+  end
+
+  # Veteran attributes
+  def veteran_file_number
+    @veteran_file_number ||= @headers["X-VA-File-Number"].presence
+  end
+
+  def veteran_ssn
+    @veteran_ssn ||= @headers["X-VA-Vet-SSN"].presence
+  end
+
+  def veteran_first_name
+    @headers["X-VA-Vet-First-Name"]
+  end
+
+  def veteran_last_name
+    @headers["X-VA-Vet-Last-Name"]
+  end
+
+  def veteran_middle_name
+    @headers["X-VA-Vet-Middle-Name"]
+  end
+
+  def veteran_participant_id
+    @payload.dig(:veteran, :participant_id)
+  end
+
+  def veteran_bgs_last_synced_at
+    bgs_last_synced_at_milliseconds = @payload.dig(:veteran, :bgs_last_synced_at)
+    convert_milliseconds_to_datetime(bgs_last_synced_at_milliseconds)
+  end
+
+  def veteran_name_suffix
+    @payload.dig(:veteran, :name_suffix)
+  end
+
+  def veteran_date_of_death
+    @payload.dig(:veteran, :date_of_death)
+  end
+
+  # Claimant attr
+
+  # Intake attr
+
+  # EndProductEstablishment attr
+  def epe_benefit_type_code
+    @payload.dig(:end_product_establishment, :benefit_type_code)
+  end
+
+  def epe_claim_date
+    @payload.dig(:end_product_establishment, :claim_date)
+  end
+
+  def epe_code
+    @payload.dig(:end_product_establishment, :code)
+  end
+
+  def epe_modifier
+    @payload.dig(:end_product_establishment, :modifier)
+  end
+
+  def epe_payee_code
+    @payload.dig(:end_product_establishment, :payee_code)
+  end
+
+  def epe_reference_id
+    @payload.dig(:end_product_establishment, :reference_id)
+  end
+
+  def epe_limited_poa_access
+    @payload.dig(:end_product_establishment, :limited_poa_access)
+  end
+
+  def epe_limited_poa_code
+    @payload.dig(:end_product_establishment, :limited_poa_code)
+  end
+
+  def epe_committed_at
+    committed_at_milliseconds = @payload.dig(:end_product_establishment, :committed_at)
+    convert_milliseconds_to_datetime(committed_at_milliseconds)
+  end
+
+  def epe_established_at
+    established_at_milliseconds = @payload.dig(:end_product_establishment, :established_at)
+    convert_milliseconds_to_datetime(established_at_milliseconds)
+  end
+
+  def epe_last_synced_at
+    last_synced_at_milliseconds = @payload.dig(:end_product_establishment, :last_synced_at)
+    convert_milliseconds_to_datetime(last_synced_at_milliseconds)
+  end
+
+  def epe_synced_status
+    @payload.dig(:end_product_establishment, :synced_status)
+  end
+
+  def epe_development_item_reference_id
+    @payload.dig(:end_product_establishment, :development_item_reference_id)
+  end
+end
--- spec/services/events/create_veteran_on_event_spec.rb
+++ spec/services/events/create_veteran_on_event_spec.rb
@@ -2,11 +2,11 @@
 
 describe Events::CreateVeteranOnEvent do
   let!(:veteran) { create(:veteran) }
-  let!(:non_cf_veteran) { double("Veteran", file_number: "12345678X", participant_id: "1826209", bgs_last_synced_at: 1708533584000, name_suffix: nil, date_of_death: nil) }
+  let!(:non_cf_veteran) { double("Veteran", file_number: "12345678X", participant_id: "1826209", bgs_last_synced_at: 1_708_533_584_000, name_suffix: nil, date_of_death: nil) }
   let!(:event) { DecisionReviewCreatedEvent.create!(reference_id: "1") }
 
   describe "#veteran_exist?" do
-    subject { described_class}
+    subject { described_class }
 
     context "when there is no existing Veteran" do
       it "should return false" do
@@ -21,14 +21,16 @@ describe Events::CreateVeteranOnEvent do
     end
   end
 
-  describe "#create_backfill_veteran" do
-    subject { described_class}
+  describe "#handle_veteran_creation_on_event" do
+    subject { described_class }
 
     context "when creating a new Veteran" do
       it "should create successfully without calling BGS and also create an EventRecord" do
-        headers = get_headers()
+        headers = get_headers
+        payload = get_payload
+        parser = Events::DecisionReviewCreated::DecisionReviewCreatedParser.new(headers, payload)
 
-        backfilled_veteran = subject.create_backfill_veteran(event, headers, non_cf_veteran)
+        backfilled_veteran = subject.handle_veteran_creation_on_event(event, parser)
 
         expect(backfilled_veteran.ssn).to eq headers["X-VA-Vet-SSN"]
         expect(backfilled_veteran.file_number).to eq headers["X-VA-File-Number"]
@@ -37,17 +39,19 @@ describe Events::CreateVeteranOnEvent do
         expect(backfilled_veteran.middle_name).to eq headers["X-VA-Vet-Middle-Name"]
 
         expect(backfilled_veteran.participant_id).to eq non_cf_veteran.participant_id
-        expect(backfilled_veteran.bgs_last_synced_at).to eq convert_milliseconds_to_datetime(non_cf_veteran.bgs_last_synced_at)
+        expect(backfilled_veteran.bgs_last_synced_at).to eq parser.convert_milliseconds_to_datetime(non_cf_veteran.bgs_last_synced_at)
+        expect(backfilled_veteran.name_suffix).to eq nil
+        expect(backfilled_veteran.date_of_death).to eq nil
 
         expect(EventRecord.count).to eq 1
         event_record = EventRecord.first
 
-        expect(event_record.backfill_record).to eq (backfilled_veteran)
+        expect(event_record.backfill_record).to eq(backfilled_veteran)
       end
     end
 
-    def get_headers()
-      return {
+    def get_headers
+      {
         "X-VA-Vet-SSN" => "123456789",
         "X-VA-File-Number" => "123456789",
         "X-VA-Vet-First-Name" => "John",
@@ -56,8 +60,15 @@ describe Events::CreateVeteranOnEvent do
       }
     end
 
-    def convert_milliseconds_to_datetime(milliseconds)
-      Time.at(milliseconds / 1000).to_datetime
+    def get_payload
+      {
+        "veteran": {
+          "participant_id": "1826209",
+          "bgs_last_synced_at": 1_708_533_584_000,
+          "name_suffix": nil,
+          "date_of_death": nil
+        }
+      }
     end
   end
 end
