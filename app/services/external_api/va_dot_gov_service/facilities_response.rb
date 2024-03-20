ff6e8f9417a32230264d10b406a9852e033e9f34
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-03-19 23:33:03 -0400

    move or zero, remove to_i

--- app/services/external_api/va_dot_gov_service/facilities_response.rb
+++ app/services/external_api/va_dot_gov_service/facilities_response.rb
@@ -9,7 +9,7 @@ class ExternalApi::VADotGovService::FacilitiesResponse < ExternalApi::VADotGovSe
     return [] if body[:data].blank?
 
     facility_data = body[:data].map do |facility|
-      Facility.new(facility, distances[facility[:id].to_i]).format
+      Facility.new(facility, (distances[facility[:id]] || 0)).format
     end
 
     facility_data.sort_by { |facility| facility[:distance] }
@@ -38,7 +38,7 @@ class ExternalApi::VADotGovService::FacilitiesResponse < ExternalApi::VADotGovSe
 
     def initialize(facility, distance)
       @facility = facility
-      @distance = distance || 0
+      @distance = distance
     end
 
     def attrs
