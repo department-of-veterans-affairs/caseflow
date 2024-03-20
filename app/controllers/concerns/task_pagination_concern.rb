cc6c90db0ccc011421d8c13df50bbda4a692fc3c
divyadasari-va <divya.dasari@va.gov>
2024-03-20 11:36:39 -0600

    Removed logic for sort and pagination fix

--- app/controllers/concerns/task_pagination_concern.rb
+++ app/controllers/concerns/task_pagination_concern.rb
@@ -53,8 +53,7 @@ module TaskPaginationConcern
   def correspondence_json_tasks(tasks)
     { data: WorkQueue::CorrespondenceTaskColumnSerializer.new(
       tasks,
-      is_collection: true,
-      params: { columns: CorrespondenceQueueTab.serialize_columns }
+      is_collection: true
     ).serializable_hash[:data] }
   end
 
