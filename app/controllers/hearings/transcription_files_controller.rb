620aefccacdf0d1eb7ebb1015266e683eccbb8f5
minhazur9 <65432922+minhazur9@users.noreply.github.com>
2024-03-20 15:50:21 -0400

    Min/APPEALS-37605 (#21151)
    * APPEALS-37605 added route and test json response
    
    * APPEALS-37605 file now gets downloaded onto local computer
    
    * APPEALS-37605 added user verification
    
    * min/APPEALS-37605 added rspec and factory for transcription_file
    
    * APPEALS-37605 finished writing rspec tests
    
    * APPEALS-37605 finished writing rspec tests
    
    * APPEALS-37605 added more error handling
    
    * APPEALS-37605 Move controller back to hearings folder
    
    * APPEALS-37605 Remove unused methods from controller
    
    * APPEALS-37605 Move transcription_file_spec to hearings folder
    
    ---------
    
    Co-authored-by: msteele <Marc.Steele@va.gov>

--- app/controllers/hearings/transcription_files_controller.rb
+++ app/controllers/hearings/transcription_files_controller.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
-class TranscriptionFilesController < ApplicationController
-  def index
-    nil
+class Hearings::TranscriptionFilesController < ApplicationController
+  include HearingsConcerns::VerifyAccess
+
+  rescue_from ActionController::UnknownFormat, with: :render_page_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :render_page_not_found
+  before_action :verify_access_to_hearings, only: [:download_transcription_file]
+
+  # Downloads file and sends to user's local computer
+  def download_transcription_file
+    hearing_id = params[:hearing_id]
+    respond_to do |format|
+      format.any(:vtt, :mp3, :mp4, :csv) do |_|
+        type = format.format&.symbol.to_s
+        file = TranscriptionFile.find_by!(hearing_id: hearing_id, file_type: type)
+        tmp_location = file.download_from_s3
+        File.open(tmp_location, "r") { |f| send_data f.read }
+        File.delete(tmp_location) if File.exist?(tmp_location)
+      end
+    end
   end
 
-  def update
-    nil
-  end
-
-  def create
-    nil
+  def render_page_not_found
+    redirect_to "/404"
   end
 end
