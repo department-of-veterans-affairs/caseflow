9ea3666705c71676547d8480d063ffc6e4de034e
Jonathan Tsang <tsang_jonathan@bah.com>
2024-03-25 13:15:28 -0400

    initial commit

---
+++ app/services/events/decision_review_created/create_request_issues.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Events::DecisionReviewCreated::CreateRequestIssues
+  class << self
+    def handle_request_issue_creation_on_event()
+
+    end
+
+    def create_request_issue_backfill()
+
+    end
+  end
+end
