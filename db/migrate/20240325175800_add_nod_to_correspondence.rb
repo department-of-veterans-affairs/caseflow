a02f677ae8b4bd63ff21a08e9c537b74800d4bff
William Medders <medders_william@bah.com>
2024-03-26 14:43:10 -0400

    rollback nod migration

--- db/migrate/20240325175800_add_nod_to_correspondence.rb
+++
@@ -1,5 +0,0 @@
-class AddNodToCorrespondence < Caseflow::Migration
-  def change
-    add_column :correspondences, :nod, :boolean, default: false, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_25_175800) do
+ActiveRecord::Schema.define(version: 2024_02_27_154315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -668,7 +668,6 @@ ActiveRecord::Schema.define(version: 2024_03_25_175800) do
     t.integer "cmp_queue_id", comment: "Foreign key to CMP queues table"
     t.integer "correspondence_type_id", comment: "Foreign key for correspondence_types table"
     t.datetime "created_at", null: false, comment: "Standard created_at/updated_at timestamps"
-    t.boolean "nod", default: false, null: false
     t.text "notes", comment: "Comes from CMP; can be updated by user"
     t.integer "package_document_type_id", comment: "Represents entire CMP package document type"
     t.datetime "portal_entry_date", comment: "Time when correspondence is created in Caseflow"
