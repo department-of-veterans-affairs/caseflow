33dd559e1ae10f9cd6e5a81756b8a3b2d7b459f9
Matt Roth <roth_matthew@bah.com>
2024-03-27 11:25:58 -0400

    APPEALS-42941 Affinity Levers value updating when selecting omit or always (#21272)
    * revert breaking changes
    
    * Update LeversReducer.test.js
    
    * update comments
    
    * update comments
    
    * update parameter name to be clearer
    
    * Update leversActions.test.js

--- client/test/app/caseDistribution/reducers/levers/LeversReducer.test.js
+++ client/test/app/caseDistribution/reducers/levers/LeversReducer.test.js
@@ -11,6 +11,7 @@ import {
   mockTextLeverReturn,
   mockDocketDistributionPriorLeversReturn,
 } from 'test/data/adminCaseDistributionLevers';
+import ACD_LEVERS from '../../../../../constants/ACD_LEVERS';
 
 let mockInitialLevers = {
   static: mockStaticLevers,
@@ -181,9 +182,9 @@ describe('Lever reducer', () => {
     const action = {
       type: ACTIONS.UPDATE_RADIO_LEVER,
       payload: {
-        leverGroup: 'affinity',
+        leverGroup: ACD_LEVERS.lever_groups.affinity,
         leverItem: 'ama_hearing_case_affinity_days',
-        value: '0',
+        optionItem: ACD_LEVERS.value,
         optionValue: 0
       }
     };
@@ -193,7 +194,9 @@ describe('Lever reducer', () => {
       if (lever.item === 'ama_hearing_case_affinity_days') {
         return {
           ...lever,
-          currentValue: 80
+          value: 80,
+          selectedOption: ACD_LEVERS.value,
+          valueOptionValue: 80
         };
       }
 
