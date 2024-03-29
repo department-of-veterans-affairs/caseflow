b900f2b02efc6a7de5cd6aab0171299a80153b0c
Daniel Mage <dmage20@gmail.com>
2024-03-29 09:21:43 -0700

    fix tests for PdfPage

--- client/app/reader/PdfPage.jsx
+++ client/app/reader/PdfPage.jsx
@@ -207,7 +207,6 @@ export class PdfPage extends React.PureComponent {
   // and PDFJS page object.
   setUpPage = () => {
     // eslint-disable-next-line no-underscore-dangle
-
     if (this.props.pdfDocument && !this.props.pdfDocument._transport.destroyed) {
 
       const pageMetricData = {
