3a8dc0454a4ba94c51e76d77f1ae7609ec650ea5
Sean Parker <Sean.Parker3@va.gov>
2024-03-29 16:36:52 -0400

    commenting out non-required seeds

--- db/seeds_bkp.rb
+++ db/seeds_bkp.rb
@@ -46,44 +46,44 @@ class SeedDB
     call_and_log_seed_step Seeds::NotificationEvents
     call_and_log_seed_step Seeds::CaseDistributionLevers
     # End of required to exist dependencies
-      # call_and_log_seed_step Seeds::Tasks
-      # call_and_log_seed_step Seeds::Hearings
-      # call_and_log_seed_step Seeds::Intake
-      # call_and_log_seed_step Seeds::Dispatch
-      # call_and_log_seed_step Seeds::Jobs
-      # call_and_log_seed_step Seeds::Substitutions
-      # call_and_log_seed_step Seeds::DecisionIssues
-      # call_and_log_seed_step Seeds::CavcAmaAppeals
-      # call_and_log_seed_step Seeds::SanitizedJsonSeeds
-      # call_and_log_seed_step Seeds::VeteransHealthAdministration
-      # call_and_log_seed_step Seeds::MTV
-      # call_and_log_seed_step Seeds::Education
+    call_and_log_seed_step Seeds::Tasks
+    call_and_log_seed_step Seeds::Hearings
+    call_and_log_seed_step Seeds::Intake
+    call_and_log_seed_step Seeds::Dispatch
+    call_and_log_seed_step Seeds::Jobs
+    call_and_log_seed_step Seeds::Substitutions
+    call_and_log_seed_step Seeds::DecisionIssues
+    call_and_log_seed_step Seeds::CavcAmaAppeals
+    call_and_log_seed_step Seeds::SanitizedJsonSeeds
+    call_and_log_seed_step Seeds::VeteransHealthAdministration
+    call_and_log_seed_step Seeds::MTV
+    call_and_log_seed_step Seeds::Education
     # Case Distribution Seed Data
     # Creates 300+ priority cases ready for distribution
     # Warning a number are not setup correctly so cannot be used beyond
     # just distributing
-      # call_and_log_seed_step Seeds::PriorityDistributions
-      # call_and_log_seed_step Seeds::TestCaseData
-      # call_and_log_seed_step Seeds::CaseDistributionAuditLeverEntries
+    call_and_log_seed_step Seeds::PriorityDistributions
+    call_and_log_seed_step Seeds::TestCaseData
+    call_and_log_seed_step Seeds::CaseDistributionAuditLeverEntries
     # End of Case Distribution Seed Data
-      # call_and_log_seed_step Seeds::Notifications
-      # call_and_log_seed_step Seeds::CavcDashboardData
-      # call_and_log_seed_step Seeds::VbmsExtClaim
-      # call_and_log_seed_step Seeds::CasesTiedToJudgesNoLongerWithBoard
-      # call_and_log_seed_step Seeds::VhaChangeHistory
-      # call_and_log_seed_step Seeds::BgsServiceRecordMaker
-      # call_and_log_seed_step Seeds::MstPactLegacyCaseAppeals
-      # call_and_log_seed_step Seeds::AmaIntake
-      # call_and_log_seed_step Seeds::AmaAffinityCases
+    call_and_log_seed_step Seeds::Notifications
+    call_and_log_seed_step Seeds::CavcDashboardData
+    call_and_log_seed_step Seeds::VbmsExtClaim
+    call_and_log_seed_step Seeds::CasesTiedToJudgesNoLongerWithBoard
+    call_and_log_seed_step Seeds::VhaChangeHistory
+    call_and_log_seed_step Seeds::BgsServiceRecordMaker
+    call_and_log_seed_step Seeds::MstPactLegacyCaseAppeals
+    call_and_log_seed_step Seeds::AmaIntake
+    call_and_log_seed_step Seeds::AmaAffinityCases
     # Always run this as last one
-      # call_and_log_seed_step Seeds::StaticTestCaseData
-      # call_and_log_seed_step Seeds::StaticDispatchedAppealsTestData
-      # call_and_log_seed_step Seeds::RemandedAmaAppeals
-      # call_and_log_seed_step Seeds::RemandedLegacyAppeals
-      # call_and_log_seed_step Seeds::PopulateCaseflowFromVacols
+    call_and_log_seed_step Seeds::StaticTestCaseData
+    call_and_log_seed_step Seeds::StaticDispatchedAppealsTestData
+    call_and_log_seed_step Seeds::RemandedAmaAppeals
+    call_and_log_seed_step Seeds::RemandedLegacyAppeals
+    call_and_log_seed_step Seeds::PopulateCaseflowFromVacols
 
-    # Judge.list_all
-    # Attorney.list_all
+    Judge.list_all
+    Attorney.list_all
   end
   # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 end
