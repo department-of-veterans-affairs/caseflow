b3bdcd2e7b2770a81f06f53d3a001dd21926bead
IsaiahBar <isaiah.barnett@va.gov>
2024-04-01 14:55:09 -0400

    convert file back

--- db/migrate/20231204193100_create_correspondences_appeals.rb
+++ db/migrate/20231204193100_create_correspondences_appeals.rb
@@ -1,4 +1,4 @@
-class CreateCorrespondenceAppeals < Caseflow::Migration
+class CreateCorrespondencesAppeals < Caseflow::Migration
   def change
     create_table :correspondences_appeals do |t|
       t.references :correspondence, foreign_key: true, index: false
