d853be7660f2fa6560b69e4f998640aa65ec625c
Minhazur Rahaman <minhazur9@gmail.com>
2024-04-01 12:27:10 -0400

    min/APPEALS-42711 adjusted query

--- app/jobs/hearings/fetch_webex_recordings_list_job.rb
+++ app/jobs/hearings/fetch_webex_recordings_list_job.rb
@@ -10,8 +10,8 @@ class Hearings::FetchWebexRecordingsListJob < CaseflowJob
   application_attr :hearing_schedule
 
   retry_on(Caseflow::Error::WebexApiError, wait: :exponentially_longer) do |job, exception|
-    from = 2.days.ago.in_time_zone("America/New_York").end_of_day
-    to = 1.day.ago.in_time_zone("America/New_York").end_of_day
+    from = 2.hours.ago.in_time_zone("America/New_York")
+    to = 1.hour.ago.in_time_zone("America/New_York")
     max = 100
     query = "?max=#{max}?from=#{CGI.escape(from.iso8601)}?to=#{CGI.escape(to.iso8601)}"
     details = {
@@ -48,8 +48,8 @@ class Hearings::FetchWebexRecordingsListJob < CaseflowJob
   private
 
   def fetch_recordings_list
-    from = CGI.escape(2.days.ago.in_time_zone("America/New_York").end_of_day.iso8601)
-    to = CGI.escape(1.day.ago.in_time_zone("America/New_York").end_of_day.iso8601)
+    from = CGI.escape(2.hours.ago.in_time_zone("America/New_York").iso8601)
+    to = CGI.escape(1.hour.ago.in_time_zone("America/New_York").iso8601)
     max = 100
     query = { "from": from, "to": to, "max": max }
 
