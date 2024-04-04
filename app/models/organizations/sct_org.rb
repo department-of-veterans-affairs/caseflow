d8b6b67285d00d86e82adcdd3ca44f09c22a1329
= <tyler.broyles@va.gov>
2024-04-04 16:34:44 -0400

    Removed placeholder sct org.

--- app/models/organizations/sct_org.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# organization representing the VHA Claims and Appeals Modernization Office
-
-class SpecialtyCaseTeam < Organization
-  def self.singleton
-    SpecialtyCaseTeam.first || SpecialtyCaseTeam.create(name: "Specialty Case Team", url: "specialty-case-team")
-  end
-end
