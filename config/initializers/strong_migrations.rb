d97cc4e49d090acf1db3f497cb5422ae4c4dae35
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-04-09 11:29:50 -0400

    🔀 Squash merge jcroteau/APPEALS-43428-add-index-migrations-for-active-record-6-0

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,2 +1,20 @@
 StrongMigrations.start_after = 20190111000717
 StrongMigrations.auto_analyze = true
+
+# Customized error message when a new migration uses `add_index` non-concurrently
+StrongMigrations.error_messages[:add_index] = <<~TEXT
+  Adding a non-concurrent index locks the table for writes.
+  Instead, prefer using `Caseflow::Migrations::AddIndexConcurrently #add_safe_index`:
+   
+      class YourMigrationName < ActiveRecord::Migration%{migration_suffix}
+        include Caseflow::Migrations::AddIndexConcurrently
+        
+        def change
+          add_safe_index # add args here...
+        end
+      end
+
+  Nota Bene: Since adding indexes concurrently must occur outside of a transaction, 
+  one should avoid mixing in other DB changes when doing so. It is strongly recommended 
+  to segregate index additions from other DB changes in a separate migration.
+  TEXT
