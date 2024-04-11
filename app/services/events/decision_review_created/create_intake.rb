033f1fce7aa2896545dc75b0f13aa0db599ed3db
Jonathan Tsang <tsang_jonathan@bah.com>
2024-04-11 13:50:01 -0400

    updated createIntake test

--- app/services/events/decision_review_created/create_intake.rb
+++ app/services/events/decision_review_created/create_intake.rb
@@ -17,6 +17,8 @@ module Events::DecisionReviewCreated::CreateIntake
                             detail_type: parser.intake_detail_type)
     # create EventRecord
     EventRecord.create!(event: event, backfill_record: intake)
+
+    intake
     # Error Handling
   rescue Caseflow::Error::DecisionReviewCreatedIntakeError => error
     raise error
--- spec/services/events/decision_review_created/create_intake_spec.rb
+++ spec/services/events/decision_review_created/create_intake_spec.rb
@@ -5,22 +5,31 @@ describe Events::DecisionReviewCreated::CreateIntake do
     let(:event_double) { double("Event") }
     let(:user_double) { double("User") }
     let(:veteran_double) { double("Veteran", file_number: "DCR02272024") }
+    let(:parser) { Events::DecisionReviewCreated::DecisionReviewCreatedParser.load_example }
     let(:intake_double) { double("Intake") }
     let(:event_record_double) { double("EventRecord") }
+
     it "creates an intake and event record" do
       allow(Intake).to receive(:create!).and_return(intake_double)
       allow(EventRecord).to receive(:create!).and_return(event_record_double)
-      expect(Intake).to receive(:create!).with(veteran_file_number: "DCR02272024", user: user_double)
+      expect(Intake).to receive(:create!).with(veteran_file_number: "DCR02272024",
+                                               user: user_double,
+                                               started_at: parser.intake_started_at,
+                                               completion_started_at: parser.intake_completion_started_at,
+                                               completed_at: parser.intake_completed_at,
+                                               completion_status: parser.intake_completion_status,
+                                               type: parser.intake_type,
+                                               detail_type: parser.intake_detail_type)
         .and_return(intake_double)
       expect(EventRecord).to receive(:create!).with(event: event_double, backfill_record: intake_double)
         .and_return(event_record_double)
-      described_class.process!(event: event_double, user: user_double, veteran: veteran_double)
+      described_class.process!(event: event_double, user: user_double, veteran: veteran_double, parser: parser)
     end
     context "when an error occurs" do
       let(:error) { Caseflow::Error::DecisionReviewCreatedIntakeError.new("Unable to create Intake") }
       it "raises the error" do
         allow(Intake).to receive(:create!).and_raise(error)
-        expect { described_class.process!(event: event_double, user: user_double, veteran: veteran_double) }.to raise_error(error)
+        expect { described_class.process!(event: event_double, user: user_double, veteran: veteran_double, parser: parser) }.to raise_error(error)
       end
     end
   end
