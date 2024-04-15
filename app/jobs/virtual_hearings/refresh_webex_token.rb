617a20b9a8d76783edb7b123b787094b66dc5f0d
breedbah <brandon.reed3@va.gov>
2024-04-15 11:48:35 -0400

    APPEALS-34071 update cron job

--- app/jobs/virtual_hearings/refresh_webex_token.rb
+++ app/jobs/virtual_hearings/refresh_webex_token.rb
@@ -1,18 +1,23 @@
-
 # app/jobs/refresh_webex_access_token_job.rb
 
 class RefreshWebexAccessTokenJob < ApplicationJob
   queue_as :default
 
   def perform
-    new_token = VirtualHearings::ConferenceClient.refresh_webex_access_token
+    webex_service = ExternalApi::WebexService.new(host: ENV['WEBEX_HOST'], port: ENV['WEBEX_PORT'], aud: ENV['WEBEX_AUD'], apikey: ENV['WEBEX_API_KEY'], domain: ENV['WEBEX_DOMAIN'], api_endpoint: ENV['WEBEX_API_ENDPOINT'])
+    response = webex_service.refresh_access_token
 
-    if new_token.present?
+    if response.present?
+      new_token = JSON.parse(response)['access_token']
       CredstashService.update_access_token(new_token)
     else
       # Retry logic here
-    end
+  end
+
   rescue => e
     # Error handler logic here
+  ensure
+    # Schedule the job to run again in 24 hours
+    self.class.set(wait: 24.hours).perform_later
   end
 end
