088927ba8c5a27b7dc9c5b9bcbee4d61d87eaf9b
breedbah <brandon.reed3@va.gov>
2024-04-15 13:59:34 -0400

    APPEALS-34071 adding the deletion of old token version

--- app/jobs/virtual_hearings/refresh_webex_token.rb
+++ app/jobs/virtual_hearings/refresh_webex_token.rb
@@ -1,6 +1,9 @@
 # app/jobs/refresh_webex_access_token_job.rb
 
-class RefreshWebexAccessTokenJob < ApplicationJob
+require 'rcredstash'
+require 'aws-sdk-dynamodb'
+
+class RefreshWebexAccessTokenJob < CaseflowJob
   queue_as :default
 
   def perform
@@ -8,14 +11,28 @@ class RefreshWebexAccessTokenJob < ApplicationJob
     response = webex_service.refresh_access_token
 
     if response.present?
-      new_token = JSON.parse(response)['access_token']
-      CredstashService.update_access_token(new_token)
+      parsed_response = JSON.parse(response)
+      new_access_token = parsed_response['access_token']
+      new_refresh_token = parsed_response['refresh_token']
+
+      credstash = Rcredstash::Client.new
+      credstash.put('webex_access_token', new_access_token, context: {}, version: 1)
+      credstash.put('webex_refresh_token', new_refresh_token, context: {}, version: 1)
+
+      # Delete old versions of the secrets
+      dynamodb = Aws::DynamoDB::Client.new
+      dynamodb.scan(table_name: 'credential-store').items.each do |item|
+        if item['name'] == 'webex_access_token' || item['name'] == 'webex_refresh_token'
+          if item['version'] != '0000000000000000001'
+            dynamodb.delete_item(table_name: 'credential-store', key: { 'name' => item['name'], 'version' => item['version'] })
+          end
+        end
+      end
     else
       # Retry logic here
-  end
-  
-  rescue => e
-    # Error handler logic here
+    end
+  rescue StandardError => e
+    log_error(e)
   ensure
     # Schedule the job to run again in 24 hours
     self.class.set(wait: 24.hours).perform_later
