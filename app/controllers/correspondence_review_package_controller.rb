61f14776d0a48b27e12c8768c11f3ed792713ad6
HunJerBAH <Jeremy.Hunton@va.gov>
2024-04-16 08:54:27 -0400

    removed leftover binding pry

--- app/controllers/correspondence_review_package_controller.rb
+++ app/controllers/correspondence_review_package_controller.rb
@@ -12,7 +12,6 @@ class CorrespondenceReviewPackageController < CorrespondenceController
   end
 
   def show
-    binding.pry
     corres_docs = correspondence.correspondence_documents
     task_instructions = CorrespondenceTask.package_action_tasks.open
       .find_by(appeal_id: correspondence.id)&.instructions || ""
