d527562506a018442122cc7905dd5200aff575a3
breedbah <brandon.reed3@va.gov>
2024-04-17 15:58:39 -0400

    APPEALS-34071 update to have access and refresh toke in response

--- app/jobs/virtual_hearings/refresh_webex_access_token_job.rb
+++ app/jobs/virtual_hearings/refresh_webex_access_token_job.rb
@@ -10,8 +10,8 @@ class VirtualHearings::RefreshWebexAccessTokenJob < CaseflowJob
     response = webex_service.refresh_access_token
 
     if response.success?
-      new_access_token = response["access_token"]
-      new_refresh_token = response["refresh_token"]
+      new_access_token = response.access_token
+      new_refresh_token = response.refresh_token
 
       CredStash.put("webex_access_token", new_access_token, version: 1)
       CredStash.put("webex_refresh_token", new_refresh_token, version: 1)
