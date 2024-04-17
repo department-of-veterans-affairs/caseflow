0d1aad85419064192f475e08b95f18e316c19003
William Medders <medders_william@bah.com>
2024-04-17 13:34:30 -0500

    add tests for review package controller

--- spec/controllers/correspondence_review_package_controller_spec.rb
+++ spec/controllers/correspondence_review_package_controller_spec.rb
@@ -21,6 +21,47 @@ RSpec.describe CorrespondenceReviewPackageController, :all_dbs, type: :controlle
     allow(mock_doc_uploader).to receive(:upload_documents_to_claim_evidence).and_return(true)
   end
 
+  describe "GET #review_package" do
+    before do
+      MailTeam.singleton.add_user(current_user)
+      User.authenticate!(user: current_user)
+      get :review_package, params: { correspondence_uuid: correspondence.uuid }
+    end
+
+    it "returns a successful response" do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET #package_documents" do
+    before do
+      MailTeam.singleton.add_user(current_user)
+      User.authenticate!(user: current_user)
+      Seeds::PackageDocumentTypes.new.seed!
+      get :package_documents
+    end
+
+    it "returns a successful response" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns package document types" do
+      doc_types = JSON.parse(response.body, symbolize_names: true)[:package_document_types]
+
+      expect(doc_types).to be_an(Array)
+      expect(doc_types.empty?).to eq(false)
+
+      doc_types.each do |doc_type|
+        expect(doc_type).to be_a(Hash)
+        expect(doc_type.keys.include?(:name)).to eq true
+      end
+
+      names_from_db = PackageDocumentType.pluck(:name)
+      names_from_response = doc_types.map { |doc_type| doc_type[:name] }
+      expect(names_from_db).to eq(names_from_response)
+    end
+  end
+
   describe "GET #show" do
     before do
       MailTeam.singleton.add_user(current_user)
@@ -76,6 +117,20 @@ RSpec.describe CorrespondenceReviewPackageController, :all_dbs, type: :controlle
       expect(correspondence.reload.notes).to eq("Updated notes")
       expect(correspondence.reload.correspondence_type_id).to eq(correspondence_type.id)
       expect(correspondence.reload.updated_by_id).to eq(current_user.id)
+    end
+
+    it "returns an error message if something goes wrong" do
+      allow_any_instance_of(CorrespondenceReviewPackageController)
+        .to receive(:update_veteran_on_correspondence).and_return(false)
+      patch :update, params: {
+        correspondence_uuid: correspondence.uuid,
+        veteran: { file_number: new_file_number },
+        correspondence: valid_params
+      }
+
+      error = JSON.parse(response.body, symbolize_names: true)[:error]
+      expect(response.status).to eq 422
+      expect(error).to eq("Please enter a valid Veteran ID")
     end
   end
 
