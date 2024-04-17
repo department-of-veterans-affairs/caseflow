8712fd81c6b75b7ccfe6f36b7981b057b300b816
Jonathan Tsang <tsang_jonathan@bah.com>
2024-04-17 13:58:46 -0400

    added fields to hlr

--- app/services/events/decision_review_created/create_claim_review.rb
+++ app/services/events/decision_review_created/create_claim_review.rb
@@ -29,7 +29,9 @@ class Events::DecisionReviewCreated::CreateClaimReview
         establishment_last_submitted_at: parser.claim_review_establishment_last_submitted_at,
         establishment_processed_at: parser.claim_review_establishment_processed_at,
         establishment_submitted_at: parser.claim_review_establishment_submitted_at,
-        veteran_file_number: parser.veteran_file_number
+        veteran_file_number: parser.veteran_file_number,
+        informal_conference: parser.informal_conference,
+        same_office: parser.same_office
       )
     end
 
