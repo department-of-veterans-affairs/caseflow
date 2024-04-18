8fcdd99bba4ff66f963429d048c7837e161e4800
HunJerBAH <Jeremy.Hunton@va.gov>
2024-04-18 16:43:54 -0400

    updated test on intake spec

--- spec/feature/queue/correspondence/intake_spec.rb
+++ spec/feature/queue/correspondence/intake_spec.rb
@@ -70,9 +70,11 @@ RSpec.feature("The Correspondence Intake page") do
       expect(page).to have_current_path("/queue/correspondence/#{@correspondence_uuid}/intake")
     end
 
-    it "successfully navigates on cancel link click" do
-      click_on("button-Cancel")
-      expect(page).to have_content("Correspondence assigned to you:")
+    it "successfully navigates on return to queue and save intake" do
+      click_on("button-Return-to-queue")
+      page.all(".cf-form-radio-option")[3].click
+      click_on("Return-To-Queue-button-id-1")
+      expect(page).to have_content("You have successfully saved the intake form")
     end
 
     it "successfully advances to the second step" do
@@ -244,9 +246,10 @@ RSpec.feature("The Correspondence Intake page") do
         uuid: SecureRandom.uuid,
         va_date_of_receipt: Time.zone.local(2023, 1, 1)
       )
-      @correspondence_uuid = Correspondence.first.uuid
-      visit "/queue/correspondence/#{@correspondence_uuid}/intake"
+      correspondence_uuid = Correspondence.first.uuid
+      visit "/queue/correspondence/#{correspondence_uuid}/intake"
       click_on("button-continue")
+      binding.pry
       click_on("+ Add tasks")
     end
 
@@ -257,13 +260,14 @@ RSpec.feature("The Correspondence Intake page") do
       end
     end
 
-    it "The user can close the modal with the cancel button." do
+    it "The user can close the modal with the return to queue button." do
+      binding.pry
       find_by_id("addAutotext").click
       within find_by_id("autotextModal") do
         expect(page).to have_text("Cancel")
       end
       find_by_id("Add-autotext-button-id-0").click
-      cancel_count = all("#button-Cancel").length
+      cancel_count = all("#button-Return-to-queue").length
       expect(cancel_count).to eq 1
     end
 
@@ -273,7 +277,7 @@ RSpec.feature("The Correspondence Intake page") do
         expect(page).to have_text("Cancel")
       end
       find_by_id("Add-autotext-button-id-close").click
-      cancel_count = all("#button-Cancel").length
+      cancel_count = all("#button-Return-to-queue").length
       expect(cancel_count).to eq 1
     end
 
@@ -371,7 +375,8 @@ RSpec.feature("The Correspondence Intake page") do
       click_on("button-continue")
       click_on("button-continue")
       intake_path = current_path
-      click_on("button-Cancel")
+      binding.pry
+      click_on("button-Return-to-queue")
       visit "/queue/correspondence?tab=correspondence_in_progress"
       find("#task-link").click
       expect(current_path).to eq(intake_path)
@@ -410,7 +415,8 @@ RSpec.feature("The Correspondence Intake page") do
       click_on("Confirm")
       expect(page).to have_content("The correspondence's documents have failed to upload to the eFolder")
       intake_path = current_path
-      click_on("button-Cancel")
+      binding.pry
+      click_on("button-Return-to-queue")
       visit intake_path
       expect(page).to have_content("The correspondence's documents have failed to upload to the eFolder")
     end
