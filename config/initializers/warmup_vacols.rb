eee763b8c988b5c2aa4b0c8fe1b222cb582423c5
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-04-22 16:27:45 -0400

    See OCI8 tcp_keepalive property to true

--- config/initializers/warmup_vacols.rb
+++ config/initializers/warmup_vacols.rb
@@ -7,6 +7,7 @@ if defined? OCI8
   OCI8.properties[:connect_timeout] = 10
   OCI8.properties[:send_timeout] = 10
   OCI8.properties[:recv_timeout] = 20
+  OCI8.properties[:tcp_keepalive] = true
 end
 
 WARMUP_TABLES = ["vacols.brieff", "vacols.corres", "vacols.folder"]
