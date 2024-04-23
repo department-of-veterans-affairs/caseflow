e6f163460800a87a8a3ff91bdfaccfcb2cd3efce
Jonathan Cohen <cohen_jonathan@bah.com>
2024-04-23 14:52:19 -0400

    APPEALS-43706 migrations ran and expected columns and indexes are in table.

--- db/migrate/20240423172438_add_polymorphic_appeal_association_to_notifications_table.rb
+++ db/migrate/20240423172438_add_polymorphic_appeal_association_to_notifications_table.rb
@@ -2,6 +2,6 @@ class AddPolymorphicAppealAssociationToNotificationsTable < Caseflow::Migration
   disable_ddl_transaction!
 
   def change
-    add_reference(:notifications, :notifiable, polymorphic: true, index: true)
+    add_reference :notifications, :notifiable, polymorphic: true, index: false
   end
 end
