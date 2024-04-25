629be70f32b12a36e790e4db18513aa707ecc594
KiMauVA <Ki.Mau@va.gov>
2024-04-25 16:19:24 -0400

    APPEALS-41707 - Functionality and some CSS

--- client/app/queue/correspondence/intake/components/ConfirmCorrespondence/CorrespondenceDetailsTable.jsx
+++ client/app/queue/correspondence/intake/components/ConfirmCorrespondence/CorrespondenceDetailsTable.jsx
@@ -10,29 +10,15 @@ export const CorrespondenceDetailsTable = (props) => {
       <table className="corr-table-borderless">
         <tbody>
           <tr>
-            <th><strong>Portal Entry Date</strong></th>
-            <th><strong>Source Type</strong></th>
             <th><strong>Package Document Type</strong></th>
-            <th><strong>CM Packet Number</strong></th>
-            <th><strong>CMP Queue Name</strong></th>
             <th><strong>VA DOR</strong></th>
-          </tr>
-          <tr>
-            <td>{moment(props.correspondence.portalEntryDate).format('MM/DD/YYYY')}</td>
-            <td>{props.correspondence.sourceType}</td>
-            <td>{props.correspondence.packageDocumentType}</td>
-            <td>{props.correspondence.cmpPacketNumber}</td>
-            <td>{props.correspondence.cmpQueueId}</td>
-            <td>{moment(props.correspondence.vaDateOfReceipt).format('MM/DD/YYYY')}</td>
-          </tr>
-          <tr>
-            <th colSpan={2}><strong>Veteran</strong></th>
+            <th><strong>Veteran</strong></th>
             <th><strong>Correspondence Type</strong></th>
           </tr>
           <tr>
-            <td colSpan={2}>
-              {props.correspondence.veteranFullName} ({props.correspondence.veteranFileNumber})
-            </td>
+            <td>{props.correspondence.packageDocumentType}</td>
+            <td>{moment(props.correspondence.vaDateOfReceipt).format('MM/DD/YYYY')}</td>
+            <td>{props.correspondence.veteranFullName} ({props.correspondence.veteranFileNumber})</td>
             <td>{props.correspondence.correspondenceType}</td>
           </tr>
           <tr>
