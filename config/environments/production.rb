38b06f786131e3d705df8f319a8e84cbfc4459db
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-04-24 22:05:08 -0400

    🔥 Remove SSL redirect exemptions
    It turns out that HSTS is being enforced at the `va.gov` domain level,
    so exempting these routes at the Rails middleware level is futile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = { redirect: { exclude: SslRedirectExclusionPolicy } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
