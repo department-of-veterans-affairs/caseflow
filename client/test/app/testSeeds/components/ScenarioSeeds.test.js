84b24902d7de7b3db76578ebead919de62943993
SHarshain <spoosa@razortalent.com>
2024-04-26 11:08:18 -0400

    APPEALS-44441. Fix ScenarioSeeds jest test

--- client/test/app/testSeeds/components/ScenarioSeeds.test.js
+++ client/test/app/testSeeds/components/ScenarioSeeds.test.js
@@ -1,54 +1,86 @@
 import React from 'react';
-import {fireEvent, waitFor, screen} from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import ScenarioSeeds from 'app/testSeeds/components/ScenarioSeeds';
-import { mount } from 'enzyme';
-import COPY from '../../../../COPY';
 import TEST_SEEDS from '../../../../constants/TEST_SEEDS';
-import userEvent from '@testing-library/user-event';
+import ApiUtil from 'app/util/ApiUtil';
+
+jest.mock('app/util/ApiUtil', () => ({
+  post: jest.fn()
+}));
 
 describe('Scenario Seeds', () => {
+
+  beforeEach(() => {
+    // Reset mock implementation before each test
+    ApiUtil.post.mockReset();
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('renders Scenario Seeds', () => {
+  const component = new ScenarioSeeds();
 
-    let wrapper = mount(
-      <ScenarioSeeds />
-    );
+  it('should render input fields and buttons for each seed type', () => {
+    const { getByLabelText, getByText } = render(<ScenarioSeeds />);
+    // Check if input fields and buttons are rendered for each seed type
+    Object.keys(TEST_SEEDS).forEach((type) => {
+      const input = getByLabelText(`count-${type}`);
+      const button = getByText(`Run Demo ${component.formatSeedName(type)}`);
+      expect(input).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+    });
+  })
 
-    expect(Object.keys(TEST_SEEDS).length).toEqual(2);
+  it('should update input value when user types', () => {
+    const { getByLabelText } = render(<ScenarioSeeds />);
+    const seed_aod_type = Object.keys(TEST_SEEDS)[0];
+    const input = getByLabelText(`count-${seed_aod_type}`);
 
-    let inputField = wrapper.find('input[id="count-aod-seeds"]');
-
-    // Calls simulate change to set value outside of min/max range
-    waitFor(() => inputField.simulate('change', 2));
-
-    wrapper.update();
-
-    waitFor(() => expect(inputField.prop('value').toBe(2)));
-
-    expect(wrapper.find('#run_seeds').text()).toContain(COPY.TEST_SEEDS_RUN_SEEDS);
+    fireEvent.change(input, { target: { value: '10' } });
+    expect(input.value).toBe('10');
   });
 
-  // it('renders Scenario Seeds with button', async () => {
-  //   let wrapper = mount(
-  //     <ScenarioSeeds />
-  //   );
+  it('should make API call when button is clicked', async () => {
+    ApiUtil.post.mockResolvedValueOnce({ data: 'Success' });
+    const { getByText, getByLabelText } = render(<ScenarioSeeds />);
+    const seed_aod_type = Object.keys(TEST_SEEDS)[0];
+    const input = getByLabelText(`count-${seed_aod_type}`);
+    const button = getByText(`Run Demo ${component.formatSeedName(seed_aod_type)}`);
 
-  //   let buttonField = wrapper.find('#button-Run-Demo-Aod-Seeds');
-  //   expect(buttonField.text()).toContain('Run Demo Aod Seeds')
-  //   expect(buttonField.props().text()).to.be.eql('Run Demo Aod Seeds');
+    fireEvent.change(input, { target: { value: '10' } });
+    fireEvent.click(button);
 
-  //   await fireEvent.click(buttonField);
+    expect(ApiUtil.post).toHaveBeenCalledWith(`/seeds/run-demo/${seed_aod_type}/10`);
 
-  //   const seedButton = screen.getByText('Run Demo Aod Seeds');
+    // Wait for API call to resolve
+    await waitFor(() => {
+      expect(ApiUtil.post).toHaveBeenCalledTimes(1);
+    });
+  });
 
-  //   userEvent.click(seedButton);
+  it('should handle API call error', async () => {
+    // Spy on console.warn
+    const consoleWarnSpy = jest.spyOn(console, 'warn');
 
+    ApiUtil.post.mockRejectedValueOnce(new Error('API Error'));
+    const { getByText, getByLabelText } = render(<ScenarioSeeds />);
+    const seed_aod_type = Object.keys(TEST_SEEDS)[0];
+    const input = getByLabelText(`count-${seed_aod_type}`);
+    const button = getByText(`Run Demo ${component.formatSeedName(seed_aod_type)}`);
 
-  //   waitFor(() => expect(buttonField.text()).toContain('Reseed Run Demo Aod Seeds'));
-  // })
+    fireEvent.change(input, { target: { value: '10' } });
+    fireEvent.click(button);
+
+    expect(ApiUtil.post).toHaveBeenCalledWith(`/seeds/run-demo/${seed_aod_type}/10`);
+
+    // Wait for API call to reject
+    await waitFor(() => {
+      expect(ApiUtil.post).toHaveBeenCalledTimes(1);
+    });
+
+    // Check if error message is displayed
+    expect(consoleWarnSpy).toHaveBeenCalledWith(new Error('API Error'));
+  });
 });
 
