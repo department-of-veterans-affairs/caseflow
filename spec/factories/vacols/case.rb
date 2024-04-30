96587160f0d69be7809e3af69e369fcbe956a3b5
isaiahsaucedo <irsaucedo5@gmail.com>
2024-04-30 12:06:52 -0500

    AC3-4: attorney and judge additions

--- spec/factories/vacols/case.rb
+++ spec/factories/vacols/case.rb
@@ -246,10 +246,25 @@ FactoryBot.define do
     end
 
     trait :legacy_cavc_appeal do
+      transient do
+        judge { nil }
+        attorney { nil }
+      end
+
       bfmpro { "HIS" }
       bfddec { 1.day.ago }
 
-      after(:create) do |vacols_case|
+      after(:create) do |vacols_case, evaluator|
+        if evaluator.judge
+          existing_judge = VACOLS::Staff.find_by_sattyid(evaluator.judge.sattyid)
+          staff_judge = (existing_judge || create(:staff, :judge_role, user: evaluator.judge))
+        end
+
+        if evaluator.attorney
+          existing_attorney = VACOLS::Staff.find_by_sattyid(evaluator.attorney.sattyid)
+          staff_attorney = (existing_attorney || create(:staff, :attorney_role, user: evaluator.attorney))
+        end
+
         vacols_case.case_issues.each do |case_issue|
           case_issue.issdc = "3"
           case_issue.save
