90291b99de0aef3681f0e5198fa876189e0a1780
piedram <110848569+piedram@users.noreply.github.com>
2024-04-30 22:04:37 -0400

    Create new Workflow transcription_packages: Step One (#21499)
    * Create new Workflow transcription_packages: Step One
    
    * add some changes
    
    * Modified the parameters for ZipAndUploadTranscriptionFilesJob
    
    * Modified spec test
    
    * Update transcription_packages.rb
    
    * remove extra blank line
    
    ---------
    
    Co-authored-by: Jim Foley <james.foley4@va.gov>

---
+++ app/workflows/transcription_packages.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class TranscriptionPackages
+  include ActiveModel::Model
+  include ActiveModel::Validations
+
+  include MailRequestValidator::Distribution
+  include MailRequestValidator::DistributionDestination
+
+  def initialize(work_order_params)
+    @work_order_params = work_order_params
+  end
+
+  def call
+    create_work_order
+    create_zip_file
+  end
+
+  private
+
+  def create_work_order
+    # call job work_order
+    Hearings::WorkOrderJob.perform(@work_order_params)
+  end
+
+  def create_zip_file
+    # call job to create a zip File
+    Hearings::ZipAndUploadTranscriptionFilesJob.perform(@work_order_params.hearings)
+  end
+end
+
