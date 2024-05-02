b4142e9d0a0953ef912dbba0c02e09e69f9a7e3a
Dev-KRedd <khalin.redding@va.gov>
2024-05-02 10:34:57 -0400

    Edit CMP Test Passing

--- spec/feature/queue/correspondence/edit_cmp_information_spec.rb
+++ spec/feature/queue/correspondence/edit_cmp_information_spec.rb
@@ -54,11 +54,8 @@ RSpec.feature("The Correspondence Review Package page") do
     end
 
     it "Checking the VA DOR and Package document type values in modal" do
-      # click_button "Edit"
-      # binding.pry IDK RN
-      # binding.pry
-      expect(page).to have_content(correspondence.va_date_of_receipt.strftime("%m/%d/%Y"))
-      # expect(page).to have_content(package_document_type.name.to_s)
+      expect(find_field("VA DOR").value).to eq correspondence.va_date_of_receipt.strftime("%Y-%m-%d")
+      expect(find_field("Package document type").value).to have_content ("NOD") || ("Non-NOD")
     end
 
     it "Saving the VA DOR and Package document type values in modal" do
