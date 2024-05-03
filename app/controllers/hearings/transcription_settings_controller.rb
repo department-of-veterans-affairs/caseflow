4ca478bb6efc4430fde8376868b1eea1bb7cc204
Adam Ducker <adam@ozarkwebdevelopers.com>
2024-05-03 10:07:52 -0500

    APPEALS-43137 (#21511)
    * APPEALS-43190 initial migration file
    
    * APPEALS-43190 add seed data
    
    * APPEALS-43190 add magic comment
    
    * APPEALS-43190 move seed command
    
    * APPEALS-43190 add bang!
    
    * APPEALS-43190 update migration file
    
    * APPEALS-43190 attribute name fix and updates
    
    * APPEALS-43190 fix seed attribute
    
    * APPEALS-43190 stubbed more seed data
    
    * APPEALS-43190 remove seed comment
    
    * APPEALS-43190 remove comment
    
    * APPEALS-43137 - Basic controller and route
    
    * APPEALS-43137 - Test the new transcription settings controller
    
    * APPEALS-43137 - Add basic route and tests for index
    
    * APPEALS-43137 - Remove changes
    
    * APPEALS-43137 - Fix JSON responses and error
    
    * APPEALS-43137 - Change record to contractor
    
    ---------
    
    Co-authored-by: breedbah <brandon.reed3@va.gov>

---
+++ app/controllers/hearings/transcription_settings_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Hearings::TranscriptionSettingsController < ApplicationController
+  include HearingsConcerns::VerifyAccess
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+  before_action :verify_access_to_hearings
+
+  def index
+    respond_to do |format|
+      format.html do
+        render "hearings/index"
+      end
+
+      format.json do
+        @transcription_contractors = TranscriptionContractor.all
+        render json: { transcription_contractors: @transcription_contractors }
+      end
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html do
+        render "hearings/index"
+      end
+
+      format.json do
+        @transcription_contractor = TranscriptionContractor.find(params[:id])
+        render json: { transcription_contractor: @transcription_contractor }
+      end
+    end
+  end
+
+  def render_record_not_found
+    render json: {
+      "errors": [
+        "title": "Contractor Not Found",
+        "detail": "Contractor with that ID is not found"
+      ]
+    }, status: :not_found
+  end
+end
---
+++ spec/controllers/hearings/transcription_settings_controller_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+describe Hearings::TranscriptionSettingsController, :all_dbs do
+  let!(:user) { User.authenticate!(roles: ["Hearing Prep", "Edit HearSched", "Build HearSched", "RO ViewHearSched"]) }
+
+  before do
+    @transcripton_contractor_1 = TranscriptionContractor.create!(
+      id: 1,
+      name: "First Contractor",
+      directory: "directory name",
+      email: "test1@va.gov",
+      phone: "phone_number",
+      poc: "contact"
+    )
+    @transcripton_contractor_2 = TranscriptionContractor.create!(
+      id: 2,
+      name: "Second Contractor",
+      directory: "directory name",
+      email: "test2@va.gov",
+      phone: "phone_number",
+      poc: "contact"
+    )
+  end
+
+  context "GET index" do
+    context "with JSON request" do
+      subject { get :index, as: :json }
+      it "returns json result" do
+        response_body = JSON.parse(subject.body)
+        expect(subject.status).to eq 200
+        expect(response_body["transcription_contractors"][0]["id"]).to eq @transcripton_contractor_1.id
+        expect(response_body["transcription_contractors"][1]["id"]).to eq @transcripton_contractor_2.id
+      end
+    end
+
+    context "with HTML request" do
+      subject { get :index, as: :html }
+      it "no JSON is returned`" do
+        expect(subject.status).to eq 200
+        expect(subject.body).to eq ""
+      end
+    end
+  end
+
+  context "GET show" do
+    context "with JSON request" do
+      context "an invalid transcription contractor ID" do
+        subject { get :show, params: { id: 45 }, as: :json }
+        it "sends error JSON" do
+          response_body = JSON.parse(subject.body)
+          expect(subject.status).to eq 404
+          expect(response_body["errors"][0]["title"]).to eq "Contractor Not Found"
+          expect(response_body["errors"][0]["detail"]).to eq "Contractor with that ID is not found"
+        end
+      end
+
+      context "a valid transcription contractor ID" do
+        subject { get :show, params: { id: 1 }, as: :json }
+        it "returns json result" do
+          response_body = JSON.parse(subject.body)
+          expect(subject.status).to eq 200
+          expect(response_body["transcription_contractor"]["id"]).to eq @transcripton_contractor_1.id
+          expect(response_body["transcription_contractor"]["name"]).to eq @transcripton_contractor_1.name
+          expect(response_body["transcription_contractor"]["directory"]).to eq @transcripton_contractor_1.directory
+        end
+      end
+    end
+
+    context "with HTML request" do
+      subject { get :show, params: { id: 1 }, as: :html }
+      it "no JSON is returned`" do
+        expect(subject.status).to eq 200
+        expect(subject.body).to eq ""
+      end
+    end
+  end
+end
