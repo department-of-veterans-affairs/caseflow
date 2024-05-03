a5ce637c6e4c998d226e3e798fc5645cb640d5ad
lauren-e-thompson <lauren.thompson@va.gov>
2024-05-03 13:28:48 -0400

    Pushing with error - change to component style

--- client/app/hearings/components/transcriptionProcessing/TranscriptionSettings.jsx
+++ client/app/hearings/components/transcriptionProcessing/TranscriptionSettings.jsx
@@ -1,49 +1,94 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable max-len */
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { css } from 'glamor';
+import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 
-const TranscriptionSettings = (props) => {
-  const {
-    hearing,
-  } = props;
+import Button from 'app/components/Button';
 
-  const headerContainerStyling = css({
-    margin: '-2rem 0 0 0',
-    padding: '0 0 1.5rem 0',
-    '& > *': {
-      display: 'inline-block',
-      paddingRight: '15px',
-      paddingLeft: '15px',
-      verticalAlign: 'middle',
-      margin: 0
-    }
-  });
+const headerContainerStyling = css({
+  margin: '-2rem 0 0 0',
+  padding: '0 0 1.5rem 0',
+  '& > *': {
+    display: 'inline-block',
+    paddingRight: '15px',
+    paddingLeft: '15px',
+    verticalAlign: 'middle',
+    margin: 0
+  }
+});
 
-  const headerStyling = css({
-    paddingLeft: 0,
-  });
+const headerStyling = css({
+  paddingLeft: 0,
+});
 
-  return (
-    <React.Fragment>
-      <div {...headerContainerStyling}>
-        <h1 className="cf-margin-bottom-0" {...headerStyling}>
-          {`Transcription Settings`}
-        </h1>
+const buttonStyle = css({
+  padding: '1rem 2.5rem 2rem 0',
+  display: 'inline-block'
+});
+
+export default class TranscriptionSettings extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  // addContractorButton = () =>
+  //   <div {...buttonStyle}><Button
+  //     name={`Add Contractor`}
+  //     id={`Add-contractor`}
+  //     // classNames={['usa-button-primary']}
+  //     // on click add contractor modal opens
+  //     /></div>
+
+  // removeContractorButton = () =>
+  //   <div {...buttonStyle}><Button
+  //     name={`Remove Contractor`}
+  //     id={`Remove-contractor`}
+  //     // classNames={['usa-button-secondary']}
+  //     // on click contractor is removed
+  //     /></div>
+
+  mainContent = () => {
+    return (
+      <React.Fragment>
         <div {...headerContainerStyling}>
-          <h2 {...headerStyling}>
-            {`Edit Current Contractors`}
-          </h2>
+          <h1 className="cf-margin-bottom-0" {...headerStyling}>
+            {`Transcription Settings`}
+          </h1>
+          <div {...headerContainerStyling}>
+            <h2 {...headerStyling}>
+              {`Edit Current Contractors`}
+            </h2>
+            <span>
+              {/* {this.removeContractorButton()}
+              {this.addContractorButton()} */}
+            </span>
+          </div>
         </div>
-      </div>
+        <div>
+          <h2>{`Contractor A`}</h2>
+          <h4>{`Link to box.com: `}</h4>
+          <h4>{`POC:`}</h4>
+          <h4>{`Hearings sent to Contractor A this week:`}</h4>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-    </React.Fragment>
-  );
+  render = () => <LoadingDataDisplay>
+  <AppSegment filledBackground>
+    <div>
+      {this.mainContent()}
+    </div>
+  </AppSegment>
+</LoadingDataDisplay>;
 }
 
 TranscriptionSettings.propTypes = {
 
 };
-
-export default TranscriptionSettings;
