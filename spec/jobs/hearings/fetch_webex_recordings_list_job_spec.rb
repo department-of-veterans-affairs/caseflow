fa8aad39790d7dd911d87e29c6a1d1663ccc9f61
Jeff Marks <jeffrey.marks@va.gov>
2024-05-06 08:18:39 -0600

    APPEALS-445399-45401 Update spec files for webex list and details job

--- spec/jobs/hearings/fetch_webex_recordings_list_job_spec.rb
+++ spec/jobs/hearings/fetch_webex_recordings_list_job_spec.rb
@@ -2,18 +2,43 @@
 
 describe Hearings::FetchWebexRecordingsListJob, type: :job do
   include ActiveJob::TestHelper
+  let(:access_token) { "sample_#{Rails.deploy_env}_token" }
 
   subject { described_class.perform_now }
 
-  it "Returns the correct array of ids" do
-    allow_any_instance_of(Hearings::DownloadTranscriptionFileJob)
-      .to receive(:perform)
-      .and_return(nil)
-    expect(subject).to eq(%w[
-                            4f914b1dfe3c4d11a61730f18c0f5387
-                            3324fb76946249cfa07fc30b3ccbf580
-                            42b80117a2a74dcf9863bf06264f8075
-                          ])
+  before do
+    allow(CredStash).to receive(:get).with("webex_#{Rails.deploy_env}_access_token").and_return(access_token)
+  end
+
+  context "job success" do
+    before do
+      allow_any_instance_of(Hearings::DownloadTranscriptionFileJob)
+        .to receive(:perform)
+        .and_return(nil)
+    end
+
+    it "Returns the correct array of ids" do
+      expect(subject).to eq(%w[
+                              4f914b1dfe3c4d11a61730f18c0f5387
+                              3324fb76946249cfa07fc30b3ccbf580
+                              42b80117a2a74dcf9863bf06264f8075
+                            ])
+    end
+
+    it "Uses correct api key for correct environment" do
+      allow(WebexService).to receive(:new).and_call_original
+      expect(WebexService).to receive(:new).with(hash_including(apikey: access_token))
+      subject
+    end
+
+    it "Uses correctly formatted to and from query parameters" do
+      to_param = 1.hour.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601
+      from_param = 2.hours.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601
+
+      allow(WebexService).to receive(:new).and_call_original
+      expect(WebexService).to receive(:new).with(hash_including(query: { to: to_param, from: from_param, max: 100 }))
+      subject
+    end
   end
 
   context "job errors" do
