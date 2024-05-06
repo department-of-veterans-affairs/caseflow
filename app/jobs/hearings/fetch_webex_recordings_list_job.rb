05ec53cdadf1eceace53ccc6682b2087b46d6b02
Jeff Marks <106996298+jefftmarks@users.noreply.github.com>
2024-05-06 11:45:25 -0600

    hotfix/APPEALS-45399-45401-45472 (#21555)
    * APPEALS-45399-45401 Update fetch webex list and details to retrieve apikey from cred stash
    
    * APPEALS-45399-45401 Remove CGI escape from fetch webex list job
    
    * APPEALS-445399-45401 Update spec files for webex list and details job
    
    * APPEALS-45399-45401 Include error handling for transcription issue mailer failed delivery
    
    * Revert "APPEALS-45399-45401 Include error handling for transcription issue mailer failed delivery"
    
    This reverts commit d9ce585c59444f9793e681e58060d71872057e4c.

--- app/jobs/hearings/fetch_webex_recordings_list_job.rb
+++ app/jobs/hearings/fetch_webex_recordings_list_job.rb
@@ -42,8 +42,8 @@ class Hearings::FetchWebexRecordingsListJob < CaseflowJob
   private
 
   def fetch_recordings_list
-    from = CGI.escape(2.hours.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601)
-    to = CGI.escape(1.hour.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601)
+    from = 2.hours.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601
+    to = 1.hour.ago.in_time_zone("America/New_York").beginning_of_hour.iso8601
     max = 100
     query = { "from": from, "to": to, "max": max }
 
@@ -51,7 +51,7 @@ class Hearings::FetchWebexRecordingsListJob < CaseflowJob
       host: ENV["WEBEX_HOST_MAIN"],
       port: ENV["WEBEX_PORT"],
       aud: ENV["WEBEX_ORGANIZATION"],
-      apikey: ENV["WEBEX_BOTTOKEN"],
+      apikey: CredStash.get("webex_#{Rails.deploy_env}_access_token"),
       domain: ENV["WEBEX_DOMAIN_MAIN"],
       api_endpoint: ENV["WEBEX_API_MAIN"],
       query: query
--- spec/jobs/hearings/fetch_webex_recordings_details_job_spec.rb
+++ spec/jobs/hearings/fetch_webex_recordings_details_job_spec.rb
@@ -12,14 +12,25 @@ describe Hearings::FetchWebexRecordingsDetailsJob, type: :job do
   let(:mp3_file_name) { "180000304_1_LegacyHearing-1.mp3" }
   let(:hearing) { create(:hearing) }
   let(:file_name) { "#{hearing.docket_number}_#{hearing.id}_#{hearing.class}" }
+  let(:access_token) { "sample_#{Rails.deploy_env}_token" }
 
   subject { described_class.perform_now(id: id, file_name: file_name) }
+
+  before do
+    allow(CredStash).to receive(:get).with("webex_#{Rails.deploy_env}_access_token").and_return(access_token)
+  end
 
   context "method testing" do
     before do
       allow_any_instance_of(Hearings::DownloadTranscriptionFileJob)
         .to receive(:perform)
         .and_return(nil)
+    end
+
+    it "Uses correct api key for correct environment" do
+      allow(WebexService).to receive(:new).and_call_original
+      expect(WebexService).to receive(:new).with(hash_including(apikey: access_token))
+      subject
     end
 
     it "hits the webex API and returns recording details" do
