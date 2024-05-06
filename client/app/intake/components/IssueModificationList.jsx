d7dc48c628c5a59d4691f0ced5e38dd6c5723ebb
Sean Craig <sean.craig2@va.gov>
2024-05-06 15:24:29 -0500

    Created a new IssueModificationList component

---
+++ client/app/intake/components/IssueModificationList.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import IssueModification from './IssueModification';
+
+const IssueModificationList = (
+  {
+    sectionTitle,
+    issuesArr
+  }
+) => {
+  // const addionalIssuesArr = issuesObj.Addition;
+  // console.log('addionalIssuesArr', addionalIssuesArr);
+  // const addionalIssuesRows = [];
+  // const addionalIssueSection = addionalIssuesArr.map((issue) => {
+  //   addionalIssuesRows.push(
+  //     <IssueModification issue={issue} />
+  //   );
+
+  //   return addionalIssuesRows;
+  // });
+
+  // const modificationIssueArr = issuesObj.Modification;
+  // console.log('modificationIssueArr', modificationIssueArr);
+  // const modificationIssuesRows = [];
+  // const modificationIssueSection = modificationIssueArr.map((issue) => {
+  //   modificationIssuesRows.push(
+  //     <IssueModification issue={issue} />
+  //   );
+
+  //   return modificationIssuesRows;
+  // });
+
+  // const removalIssueArr = issuesObj.Removal;
+  // console.log('removalIssueArr', removalIssueArr);
+  // const removalIssuesRows = [];
+  // const removalIssuesSection = removalIssueArr.map((issue) => {
+  //   removalIssuesRows.push(
+  //     <IssueModification issue={issue} />
+  //   );
+
+  //   return removalIssuesRows;
+  // });
+
+  // const withdrawalIssueArr = issuesObj.Withdrawal;
+  // console.log('withdrawalIssueArr', withdrawalIssueArr);
+  // const withdrawalIssuesRows = [];
+  // const withdrawalIssueSection = withdrawalIssueArr.map((issue) => {
+  //   withdrawalIssuesRows.push(
+  //     <IssueModification issue={issue} />
+  //   );
+
+  //   return withdrawalIssuesRows;
+  // });
+
+  // let issuesArr = [];
+
+  // issuesArr = issuesArr.concat(addionalIssueSection);
+  // issuesArr = issuesArr.concat(modificationIssueSection);
+  // issuesArr = issuesArr.concat(removalIssuesSection);
+  // issuesArr = issuesArr.concat(withdrawalIssueSection);
+
+  const issues = issuesArr.map((issue) => {
+    return <li><IssueModification issue={issue} /></li>;
+  });
+
+  return (
+    <>
+      <div>
+        <h3>{sectionTitle}</h3>
+        <ol>
+          {issues}
+        </ol>
+      </div>
+      <hr />
+    </>
+  );
+
+};
+
+export default IssueModificationList;
