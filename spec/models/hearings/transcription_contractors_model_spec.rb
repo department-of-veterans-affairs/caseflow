9bcc7b96668c17993e8ea7d95e4ae472cd16f511
breedbah <brandon.reed3@va.gov>
2024-05-06 10:10:46 -0400

    APPEALS-43138 upudate transcription_c tests

--- spec/models/hearings/transcription_contractors_model_spec.rb
+++ spec/models/hearings/transcription_contractors_model_spec.rb
@@ -3,8 +3,8 @@
 require "rails_helper"
 
 RSpec.describe TranscriptionContractor, type: :model do
-  before do
-    @transcription_contractor = TranscriptionContractor.new(
+  subject(:transcription_contractor) do
+    described_class.new(
       name: "Genesis Government Solutions, Inc.",
       directory: "BVA Hearing Transcripts/Genesis Government Solutions, Inc.",
       poc: "Example POC",
@@ -13,24 +13,43 @@ RSpec.describe TranscriptionContractor, type: :model do
     )
   end
 
-  it "is valid with valid attributes" do
-    expect(@transcription_contractor).to be_valid
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:directory) }
   end
 
-  it "is not valid without a name" do
-    @transcription_contractor.name = nil
-    expect(@transcription_contractor).not_to be_valid
+  describe "default values" do
+    it "has correct default values" do
+      expect(transcription_contractor.is_available_for_work).to eq(false)
+      expect(transcription_contractor.previous_goal).to eq(0)
+      expect(transcription_contractor.current_goal).to eq(0)
+      expect(transcription_contractor.inactive).to eq(false)
+    end
   end
 
-  it "is not valid without a directory" do
-    @transcription_contractor.directory = nil
-    expect(@transcription_contractor).not_to be_valid
-  end
+  describe ".all_contractors" do
+    let!(:contractors) do
+      [
+        { name: "Genesis Government Solutions, Inc.",
+          directory: "BVA Hearing Transcripts/Genesis Government Solutions, Inc.",
+          email: "email_1@test.com",
+          phone: "888-888-8888",
+          poc: "Example POC" },
+        { name: "Jamison Professional Services",
+          directory: "BVA Hearing Transcripts/Jamison Professional Services",
+          email: "email_2@test.com",
+          phone: "888-888-8888",
+          poc: "Example POC" },
+        { name: "The Ravens Group, Inc.",
+          directory: "BVA Hearing Transcripts/The Ravens Group, Inc.",
+          email: "email_3@test.com",
+          phone: "888-888-8888",
+          poc: "Example POC" }
+      ].map { |attrs| described_class.create!(attrs) }
+    end
 
-  it "has correct default values" do
-    expect(@transcription_contractor.is_available_for_work).to eq(false)
-    expect(@transcription_contractor.previous_goal).to eq(0)
-    expect(@transcription_contractor.current_goal).to eq(0)
-    expect(@transcription_contractor.inactive).to eq(false)
+    it "returns all contractors" do
+      expect(described_class.all_contractors).to contain_exactly(*contractors)
+    end
   end
 end
