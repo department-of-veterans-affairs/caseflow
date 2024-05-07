86b2dcbdec43ac191c5365b1dae40fd44cf1d88c
Ariana Konhilas <konhilas_ariana@bah.com>
2024-05-07 16:45:36 -0400

    APPEALS-43193: move index to own file

--- db/migrate/20240507145931_add_columns_to_transcriptions.rb
+++ db/migrate/20240507145931_add_columns_to_transcriptions.rb
@@ -1,14 +1,9 @@
-class AddProcessingColumnsToTranscriptions < Caseflow::Migration
-  disable_ddl_transaction!
-
+class AddColumnsToTranscriptions < Caseflow::Migration
   def change
     add_column :transcriptions, :transcription_contractor_id, :int
     add_column :transcriptions, :created_by_id, :int
     add_column :transcriptions, :transcription_status, :string, comment: "Possible values: 'unassigned', 'in_transcription', 'completed', 'completed_overdue'"
     add_column :transcriptions, :updated_by_id, :int
     add_column :transcriptions, :deleted_at, :datetime, comment: "acts_as_paranoid in the model"
-
-    add_index :transcriptions, [:transcription_contractor_id], name: "index_transcriptions_on_transcription_contractor_id", algorithm: :concurrently
-    add_index :transcriptions, [:deleted_at], name: "index_transcriptions_on_deleted_at", algorithm: :concurrently
   end
 end
