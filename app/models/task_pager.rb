2fc8b496180db2289ce361e5e9537c70bb256d4c
Tyler Broyles <109369527+TylerBroyles@users.noreply.github.com>
2024-05-08 13:26:01 -0400

    Added a customizable per page task limitation attribute to QueueTab that is used by task_pager to allow an adjustable maximum number of tasks per tab. (#21583)
    Co-authored-by: Robert Travis Pierce <robert@roberttravispierce.com>

--- app/models/task_pager.rb
+++ app/models/task_pager.rb
@@ -22,11 +22,12 @@ class TaskPager
 
     fail(Caseflow::Error::MissingRequiredProperty, message: errors.full_messages.join(", ")) unless valid?
   end
-
+  
   def paged_tasks
     @paged_tasks ||= begin
-      tasks = sorted_tasks(filtered_tasks).not_correspondence
-      pagination_enabled ? tasks.page(page).per(TASKS_PER_PAGE) : tasks
+      tasks = sorted_tasks(filtered_tasks)
+      limit = queue_tab.custom_task_limit || TASKS_PER_PAGE
+      pagination_enabled ? tasks.page(page).per(limit) : tasks
     end
   end
 
