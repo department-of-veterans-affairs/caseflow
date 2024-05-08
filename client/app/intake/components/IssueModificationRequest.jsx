7ec3f934ce55d54f7dd1b13851540067396de44d
Sean Craig <sean.craig2@va.gov>
2024-05-08 12:48:41 -0500

    refactored the layout for the request

--- client/app/intake/components/IssueModificationRequest.jsx
+++ client/app/intake/components/IssueModificationRequest.jsx
@@ -1,86 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import COPY from '../../../COPY';
 
 const IssueModificationRequest = (
   {
     issue
   }
 ) => {
-  const additionalRequest = (
+  const modificationIssueInfo = (
     <div>
       <p>{issue?.nonrating_issue_category} - {issue?.decision_text}</p>
       <p>Benefit type: {issue?.benefit_type}</p>
       <p>Decision date: {issue?.decision_date}</p>
       <br />
-      <h4>Reason for requested issue addition:</h4>
-      <p>{issue?.request_reason}</p>
+    </div>
+  );
+
+  const requestedByUser = (
+    <div>
       <br />
       <h4>Requested by:</h4>
       <p>Monte Mann (ACBAUERVVHAH)</p>
+      <br />
+    </div>
+  );
+
+  const originalIssue = (
+    <div>
+      <h3>Originial Issue</h3>
+      <p>Originial_issue.nonrating_issue_category - Originial_issue.decision_text</p>
+      <p>Benefit type: Originial_issue.benefit_type</p>
+      <p>Decision date: Originial_issue.decision_date</p>
+    </div>
+  );
+
+  const additionalRequest = (
+    <div>
+      {modificationIssueInfo}
+      <h4>{COPY.ISSUE_MODIFICATION_REQUESTS.ADDITION.DETAILS}:</h4>
+      <p>{issue?.request_reason}</p>
+      {requestedByUser}
     </div>
   );
 
   const modificationRequest = (
     <div>
-      <p>{issue?.nonrating_issue_category} - {issue?.decision_text}</p>
-      <p>Benefit type: {issue?.benefit_type}</p>
-      <p>Decision date: {issue?.decision_date}</p>
-      <br />
-      <h4>Reason for requested modification:</h4>
+      {modificationIssueInfo}
+      <h4>{COPY.ISSUE_MODIFICATION_REQUESTS.MODIFICATION.DETAILS}:</h4>
       <p>{issue?.request_reason}</p>
+      {requestedByUser}
+      {originalIssue}
       <br />
-      <h4>Requested by:</h4>
-      <p>Monte Mann (ACBAUERVVHAH)</p>
-      <br />
-      <h3>Originial Issue</h3>
-      <p>stuff</p>
     </div>
   );
 
   const removalRequest = (
     <div>
-      <p>{issue?.nonrating_issue_category} - {issue?.decision_text}</p>
-      <p>Benefit type: {issue?.benefit_type}</p>
-      <p>Decision date: {issue?.decision_date}</p>
-      <br />
-      <h4>Reason for requested removal of issue:</h4>
+      {modificationIssueInfo}
+      <h4>{COPY.ISSUE_MODIFICATION_REQUESTS.REMOVAL.DETAILS}:</h4>
       <p>{issue?.request_reason}</p>
-      <br />
-      <h4>Requested by:</h4>
-      <p>Monte Mann (ACBAUERVVHAH)</p>
+      {requestedByUser}
     </div>
   );
 
   const withdrawalRequest = (
     <div>
-      <p>{issue?.nonrating_issue_category} - {issue?.decision_text}</p>
-      <p>Benefit type: {issue?.benefit_type}</p>
-      <p>Decision date: {issue?.decision_date}</p>
-      <br />
-      <h4>Reason for requested withdrawal of issue:</h4>
+      {modificationIssueInfo}
+      <h4>{COPY.ISSUE_MODIFICATION_REQUESTS.WITHDRAWAL.DETAILS}:</h4>
       <p>{issue?.request_reason}</p>
       <br />
-      <h4>Requested date for withdrawal:</h4>
+      <h4>{COPY.ISSUE_MODIFICATION_REQUESTS.WITHDRAWAL.DATE}:</h4>
       <p>{issue?.withdrawal_request_date}</p>
-      <br />
-      <h4>Requested by:</h4>
-      <p>Monte Mann (ACBAUERVVHAH)</p>
+      {requestedByUser}
     </div>
   );
 
   let requestIssue;
 
   switch (issue.request_type) {
-  case 'Addition':
+  case COPY.ISSUE_MODIFICATION_REQUESTS.ADDITION.REQUEST_TYPE:
     requestIssue = additionalRequest;
     break;
-  case 'Modification':
+  case COPY.ISSUE_MODIFICATION_REQUESTS.MODIFICATION.REQUEST_TYPE:
     requestIssue = modificationRequest;
     break;
-  case 'Removal':
+  case COPY.ISSUE_MODIFICATION_REQUESTS.REMOVAL.REQUEST_TYPE:
     requestIssue = removalRequest;
     break;
-  case 'Withdrawal':
+  case COPY.ISSUE_MODIFICATION_REQUESTS.WITHDRAWAL.REQUEST_TYPE:
     requestIssue = withdrawalRequest;
     break;
   default:
