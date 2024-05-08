be84d46c91bb3bd6c5ca11512157143e5c93312f
Ariana Konhilas <konhilas_ariana@bah.com>
2024-05-08 10:08:55 -0400

    APPEALS-43193: bigint

--- db/migrate/20240507145931_add_columns_to_transcriptions.rb
+++ db/migrate/20240507145931_add_columns_to_transcriptions.rb
@@ -1,9 +1,9 @@
 class AddColumnsToTranscriptions < Caseflow::Migration
   def change
     add_column :transcriptions, :transcription_contractor_id, :bigint
-    add_column :transcriptions, :created_by_id, :integer
+    add_column :transcriptions, :created_by_id, :bigint
     add_column :transcriptions, :transcription_status, :string, comment: "Possible values: 'unassigned', 'in_transcription', 'completed', 'completed_overdue'"
-    add_column :transcriptions, :updated_by_id, :integer
+    add_column :transcriptions, :updated_by_id, :bigint
     add_column :transcriptions, :deleted_at, :datetime, comment: "acts_as_paranoid in the model"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1903,7 +1903,7 @@ ActiveRecord::Schema.define(version: 2024_05_07_203310) do
 
   create_table "transcriptions", force: :cascade do |t|
     t.datetime "created_at", comment: "Automatic timestamp of when transcription was created"
-    t.integer "created_by_id"
+    t.bigint "created_by_id"
     t.datetime "deleted_at", comment: "acts_as_paranoid in the model"
     t.date "expected_return_date", comment: "Expected date when transcription would be returned by the transcriber"
     t.bigint "hearing_id", comment: "Hearing ID; use as FK to hearings"
@@ -1916,7 +1916,7 @@ ActiveRecord::Schema.define(version: 2024_05_07_203310) do
     t.bigint "transcription_contractor_id"
     t.string "transcription_status", comment: "Possible values: 'unassigned', 'in_transcription', 'completed', 'completed_overdue'"
     t.datetime "updated_at", comment: "Automatic timestamp of when transcription was updated"
-    t.integer "updated_by_id"
+    t.bigint "updated_by_id"
     t.date "uploaded_to_vbms_date", comment: "Date when the hearing transcription was uploaded to VBMS"
     t.index ["deleted_at"], name: "index_transcriptions_on_deleted_at"
     t.index ["hearing_id"], name: "index_transcriptions_on_hearing_id"
