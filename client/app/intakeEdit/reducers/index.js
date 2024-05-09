bcfe3ddfebd8afcdb7637aae56387de6e68cff17
Sean Craig <sean.craig2@va.gov>
2024-05-09 11:54:05 -0500

    removed unneed code

--- client/app/intakeEdit/reducers/index.js
+++ client/app/intakeEdit/reducers/index.js
@@ -14,7 +14,6 @@ export const mapDataToInitialState = function(props = {}) {
     userCanWithdrawIssues,
     userCanEditIntakeIssues,
     userCanSplitAppeal,
-    userIsOrgAdmin,
     isLegacy,
     hasDistributionTask,
     hasSpecialtyCaseTeamAssignTask
@@ -41,7 +40,6 @@ export const mapDataToInitialState = function(props = {}) {
     userCanWithdrawIssues,
     userCanEditIntakeIssues,
     userCanSplitAppeal,
-    userIsOrgAdmin,
     isLegacy,
     addDecisionDateModalVisible: false,
     addIssuesModalVisible: false,
