cec618ddeae9b4febc9a90fa21ddc2a5d72e4805
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-10 14:43:30 -0400

    Group notification types as scopes

--- app/jobs/quarterly_notifications_job.rb
+++ app/jobs/quarterly_notifications_job.rb
@@ -4,9 +4,13 @@ class QuarterlyNotificationsJob < CaseflowJob
   include Hearings::EnsureCurrentUserIsSet
 
   queue_with_priority :low_priority
-  application_attr :hearing_schedule
+  application_attr :va_notify
 
   QUERY_LIMIT = ENV["QUARTERLY_NOTIFICATIONS_JOB_BATCH_SIZE"]
+
+  NOTIFICATION_TYPES = Constants.QUARTERLY_STATUSES.to_h.tap do |types|
+    types.delete(:quarterly_notification)
+  end
 
   # Purpose: Loop through all open appeals quarterly and sends statuses for VA Notify
   #
@@ -16,36 +20,19 @@ class QuarterlyNotificationsJob < CaseflowJob
   def perform
     ensure_current_user_is_set
 
-    AppealState.where(
-      decision_mailed: false, appeal_cancelled: false
-    ).find_in_batches(batch_size: QUERY_LIMIT.to_i) do |batched_appeal_states|
-      batched_appeal_states.each do |appeal_state|
-        status = appeal_state.quarterly_notification_status
-
-        next unless status
-
-        begin
-          appeal = appeal_state.appeal
-          MetricsService.record("Creating Quarterly Notification for #{appeal.class} ID #{appeal.id}",
-                                name: "send_quarterly_notifications(appeal_state, appeal)") do
-            AppellantNotification.notify_appellant(appeal, Constants.QUARTERLY_STATUSES.quarterly_notification, status)
-          end
-        rescue StandardError => error
-          log_error(error, appeal_state.appeal_type, appeal_state.appeal_id)
+    begin
+      NOTIFICATION_TYPES.keys_each do |notification_type|
+        AppealState.eligible_for_quarterly.send(notification_type).each do |appeal_state|
+          NotificationInitializationJob.perform_later(
+            appeal_id: appeal_state.appeal_id,
+            appeal_type: appeal_state.appeal_type,
+            template_name: Constants.QUARTERLY_STATUSES.quarterly_notification,
+            appeal_status: notification_type.to_s
+          )
         end
       end
+    rescue StandardError => error
+      log_error(error)
     end
-  end
-
-  # Purpose: Log errors with the QuarterlyNotificationJob
-  #
-  # Params: none
-  #
-  # Response: none
-  def log_error(error, appeal_type, appeal_id)
-    Rails.logger.error("QuarterlyNotificationsJob::Error - Unable to send a notification for "\
-            "#{appeal_type} ID #{appeal_id} because of #{error}")
-
-    super(error)
   end
 end
