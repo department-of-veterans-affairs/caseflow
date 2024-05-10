d2114a18e3546ab1dae800ad3203a753dc6d993e
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-10 14:43:49 -0400

    Add new job

---
+++ app/jobs/notification_initialization_job.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class NotificationInitializationJob < CaseflowJob
+  include Hearings::EnsureCurrentUserIsSet
+
+  queue_with_priority :low_priority
+  application_attr :va_notify
+
+  def perform(appeal_id:, appeal_type:, template_name:, appeal_status: nil)
+    begin
+      ensure_current_user_is_set
+
+      appeal = appeal_type.constantize.find(appeal_id)
+
+      fail StandardError, "#{appeal_type} with ID #{appeal_id} could not be found." unless appeal
+
+      AppellantNotification.notify_appellant(
+        appeal,
+        template_name,
+        appeal_status
+      )
+    rescue StandardError => error
+      log_error(error)
+    end
+  end
+end
