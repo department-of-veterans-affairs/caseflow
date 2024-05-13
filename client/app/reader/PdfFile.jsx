38d5398a7ac9b89a6dfa0e44979f0b45f558e964
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-05-13 11:38:02 -0500

    Add scroll metrics start time fix and update metric message

--- client/app/reader/PdfFile.jsx
+++ client/app/reader/PdfFile.jsx
@@ -467,32 +467,26 @@ export class PdfFile extends React.PureComponent {
         clearTimeout(this.scrollTimer);
       }
 
+      const scrollStart = performance.now();
+
       this.scrollTimer = setTimeout(() => {
-        const scrollStart = performance.now();
+        const scrollEnd = performance.now();
+        const scrollMessage = `Scroll to page ${this.currentPage + 1}
+        (${(Math.round(this.scrollLeft * 100) / 100).toFixed(2)},
+        ${(Math.round(this.scrollTop * 100) / 100).toFixed(2)})`;
 
-        const data = {
-          overscan: this.props.windowingOverscan,
-          documentType: this.props.documentType,
-          pageCount: this.props.pdfDocument.numPages,
-          pageIndex: this.pageIndex,
-          prefetchDisabled: this.props.featureToggles.prefetchDisabled,
-          start: scrollStart,
-          end: performance.now()
-        };
-
-        const posx = (Math.round(this.scrollLeft * 100) / 100).toFixed(2);
-        const posy = (Math.round(this.scrollTop * 100) / 100).toFixed(2);
+        this.metricsAttributes.name = scrollMessage;
 
         storeMetrics(
           this.props.documentId,
           this.metricsAttributes,
           {
-            message: `Scroll to position ${posx}, ${posy}`,
+            message: scrollMessage,
             type: 'performance',
             product: 'reader',
-            start: new Date(performance.timeOrigin + data.start),
-            end: new Date(performance.timeOrigin + data.end),
-            duration: data.start ? data.end - data.start : 0
+            start: new Date(performance.timeOrigin + scrollStart),
+            end: new Date(performance.timeOrigin + scrollEnd),
+            duration: scrollStart ? scrollEnd - scrollStart : 0
           },
           this.metricsIdentifier,
         );
