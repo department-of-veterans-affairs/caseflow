8088dd7a7f99eb95ea8aeabd279ee38bcfcc9896
Michael Beard <michael.beard@va.gov>
2024-05-13 11:40:52 -0500

    uncomments seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,15 +66,15 @@ class SeedDB
     call_and_log_seed_step Seeds::TestCaseData
     call_and_log_seed_step Seeds::CaseDistributionAuditLeverEntries
     # End of Case Distribution Seed Data
-    # call_and_log_seed_step Seeds::Notifications
-    # call_and_log_seed_step Seeds::CavcDashboardData
-    # call_and_log_seed_step Seeds::VbmsExtClaim
-    # call_and_log_seed_step Seeds::CasesTiedToJudgesNoLongerWithBoard
-    # call_and_log_seed_step Seeds::VhaChangeHistory
-    # call_and_log_seed_step Seeds::AmaAffinityCases
-    # call_and_log_seed_step Seeds::BgsServiceRecordMaker
-    # call_and_log_seed_step Seeds::MstPactLegacyCaseAppeals
-    # call_and_log_seed_step Seeds::AmaIntake
+    call_and_log_seed_step Seeds::Notifications
+    call_and_log_seed_step Seeds::CavcDashboardData
+    call_and_log_seed_step Seeds::VbmsExtClaim
+    call_and_log_seed_step Seeds::CasesTiedToJudgesNoLongerWithBoard
+    call_and_log_seed_step Seeds::VhaChangeHistory
+    call_and_log_seed_step Seeds::AmaAffinityCases
+    call_and_log_seed_step Seeds::BgsServiceRecordMaker
+    call_and_log_seed_step Seeds::MstPactLegacyCaseAppeals
+    call_and_log_seed_step Seeds::AmaIntake
     # Always run this as last one
     call_and_log_seed_step Seeds::StaticTestCaseData
     call_and_log_seed_step Seeds::StaticDispatchedAppealsTestData
