c6aa64efd2ef2f8ee7c21f836c89e49cb00be6c0
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-12 21:55:17 -0400

    Batch send messages

--- app/jobs/quarterly_notifications_job.rb
+++ app/jobs/quarterly_notifications_job.rb
@@ -22,20 +22,39 @@ class QuarterlyNotificationsJob < CaseflowJob
 
     begin
       NOTIFICATION_TYPES.each_key do |notification_type|
-        AppealState.eligible_for_quarterly.send(notification_type)
-          .find_in_batches(batch_size: QUERY_LIMIT.to_i) do |batch_of_appeal_states|
-          batch_of_appeal_states.each do |appeal_state|
-            NotificationInitializationJob.perform_later(
-              appeal_id: appeal_state.appeal_id,
-              appeal_type: appeal_state.appeal_type,
+        jobs = AppealState.eligible_for_quarterly.send(notification_type).pluck(:appeal_id, :appeal_type)
+          .map do |entry|
+
+            NotificationInitializationJob.new(
+              appeal_id: entry.first,
+              appeal_type: entry.last,
               template_name: "Quarterly Notification",
               appeal_status: notification_type.to_s
             )
-          end
         end
+
+        jobs.in_groups_of(10) { |jobs_to_enqueue| enqueue_batch_of_jobs(jobs_to_enqueue.compact) }
       end
     rescue StandardError => error
       log_error(error)
     end
+  end
+
+  private
+
+  # Shoryuken::Client.sqs.get_queue_attributes(queue_url: Shoryuken::Client.queues(self.queue_name).url)
+
+  def serialize_job_for_enqueueing(job)
+    ActiveJob::QueueAdapters::ShoryukenAdapter.instance.send(:message, job)
+  end
+
+  # TODO: Allow for queue_name param, and make sure all jobs are configured to
+  # go onto that queue normally.
+  def enqueue_batch_of_jobs(jobs_to_enqueue)
+    fail StandardError, "Number of jobs must not exceed 10" if jobs_to_enqueue.size > 10
+
+    Shoryuken::Client.queues(queue_name).send_messages(
+      jobs_to_enqueue.map { |job| serialize_job_for_enqueueing(job) }
+    )
   end
 end
