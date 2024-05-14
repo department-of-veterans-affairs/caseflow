4dbf54fc557cb35b36a785b2cb1a425cfdcb9759
breedbah <brandon.reed3@va.gov>
2024-05-14 12:22:07 -0400

    APPEALS-43138 change back to original

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -127,7 +127,7 @@ module FeatureHelper
   end
 
   def dropdown_menu_visible?(dropdown)
-    dropdown.sibling(".cf-select__menu", wait: 5)
+    dropdown.sibling(".cf-select__menu", wait: false)
   rescue Capybara::ElementNotFound
     false
   else
@@ -135,7 +135,7 @@ module FeatureHelper
   end
 
   def click_dropdown_menu_item(dropdown, selector, keyword_args)
-    dropdown.sibling(".cf-select__menu", wait: 5).find(selector, **keyword_args).click
+    dropdown.sibling(".cf-select__menu").find(selector, **keyword_args).click
   end
 
   def generate_text(length)
