727a8a1481fc3f358a874b1e8be9813e7a7242ee
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-14 19:47:27 -0400

    Add RDoc comments to appeal_state

--- app/models/appeal_state.rb
+++ app/models/appeal_state.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# == Overview
+#
+# AppealState records are utilized to facilitate state machine-like behavior in order
+# to track which status each appeal (AMA and Legacy) being processed in Caseflow is in.
+#
+# These states are most prominently used in the determination of which status to place in the
+# 'Quarterly Notification' template whenever sending the quarterly correspondence to appellants.
+# This is performed by the QuarterlyNotificationsJob.
 class AppealState < CaseflowRecord
   include HasAppealUpdatedSince
   include CreatedAndUpdatedByUserConcern
@@ -18,6 +26,11 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals whose hearings have been cancelled
+  #   (postponed or marked as "scheduled in error") and have yet to be rescheduled.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with hearings awaiting reschedulement.
   scope :hearing_to_be_rescheduled, lambda {
     where(
       <<~SQL
@@ -31,6 +44,12 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals whose hearings have been cancelled
+  #   (postponed or marked as "scheduled in error") and have yet to be rescheduled.
+  #   In addition, these appeals have an active Privacy Act/FOIA request in their trees.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with hearings awaiting reschedulement with Privacy Act/FOIA tasks.
   scope :hearing_to_be_rescheduled_privacy_pending, lambda {
     where(
       <<~SQL
@@ -44,6 +63,10 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals whose hearings have been scheduled and waiting to be held.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with scheduled hearings without dispositions
   scope :hearing_scheduled, lambda {
     where(
       hearing_scheduled: true,
@@ -53,6 +76,12 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals whose hearings have been scheduled and waiting to be held.
+  #  In addition, these appeals have an active Privacy Act/FOIA request in their trees.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with scheduled hearings without dispositions, and those appeals
+  #     have open Privacy Act/FOIA-related tasks in their task trees.
   scope :hearing_scheduled_privacy_pending, lambda {
     where(
       hearing_scheduled: true,
@@ -62,6 +91,12 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals with open InformalHearingPresentationTasks.
+  #  In addition, these appeals have an open Privacy Act/FOIA request-related tasks in their trees.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with open InformalHearingPresentationTasks.
+  #     have open Privacy Act/FOIA-related tasks in their task trees.
   scope :ihp_pending_privacy_pending, lambda {
     where(
       vso_ihp_pending: true,
@@ -73,6 +108,10 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals with open InformalHearingPresentationTasks.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeal states for appeals with open InformalHearingPresentationTasks.
   scope :ihp_pending, lambda {
     where(
       vso_ihp_pending: true,
@@ -84,6 +123,11 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals with open Privacy Act/FOIA request-related tasks in their trees,
+  #   however no other actions have taken place on the appeal (aside from perhaps docketing).
+  #
+  # @return [ActiveRecord::Relation]
+  #  Appeal states for appeals with open Privacy Act/FOIA request-related tasks
   scope :privacy_pending, lambda {
     where(
       vso_ihp_pending: false,
@@ -95,6 +139,11 @@ class AppealState < CaseflowRecord
     )
   }
 
+  # Locates appeal states related to appeals that have either just been docketed, or have had their hearing withdrawn
+  #  causing them to return to their initial state.
+  #
+  # @return [ActiveRecord::Relation]
+  #  Appeal states for appeals that have been docketed and are awaiting further action.
   scope :appeal_docketed, lambda {
     where(
       <<~SQL
