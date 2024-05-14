9ba63765763755095d3e3589cf0045b167569811
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-14 11:16:38 -0400

    Fix linting issues

--- app/models/appeal_state.rb
+++ app/models/appeal_state.rb
@@ -5,6 +5,12 @@ class AppealState < CaseflowRecord
   include CreatedAndUpdatedByUserConcern
   include AppealStateBelongsToPolymorphicAppealConcern
 
+  # Locates appeal states that are related to appeals eligible to potentially receive quarterly notifications.
+  #   These appeals must not have been cancelled and their decisions must not have already been mailed.
+  #
+  # @return [ActiveRecord::Relation]
+  #   Appeals eligible (potentially, assuming claimant is listed and claimant doesn't have an NOD on file)
+  #    to receive a quarterly notification.
   scope :eligible_for_quarterly, lambda {
     where(
       appeal_cancelled: false,
@@ -13,25 +19,29 @@ class AppealState < CaseflowRecord
   }
 
   scope :hearing_to_be_rescheduled, lambda {
-    where("""
-      hearing_scheduled IS FALSE AND
-      privacy_act_pending IS FALSE AND
-      (
-        hearing_postponed IS TRUE OR
-        scheduled_in_error IS TRUE
-      )
-    """)
+    where(
+      <<~SQL
+          hearing_scheduled IS FALSE AND
+          privacy_act_pending IS FALSE AND
+        (
+          hearing_postponed IS TRUE OR
+          scheduled_in_error IS TRUE
+        )
+      SQL
+    )
   }
 
   scope :hearing_to_be_rescheduled_privacy_pending, lambda {
-    where("""
-      hearing_scheduled IS FALSE AND
-      privacy_act_pending IS TRUE AND
-      (
-        hearing_postponed IS TRUE OR
-        scheduled_in_error IS TRUE
-      )
-    """)
+    where(
+      <<~SQL
+        hearing_scheduled IS FALSE AND
+        privacy_act_pending IS TRUE AND
+        (
+          hearing_postponed IS TRUE OR
+          scheduled_in_error IS TRUE
+        )
+      SQL
+    )
   }
 
   scope :hearing_scheduled, lambda {
@@ -86,20 +96,22 @@ class AppealState < CaseflowRecord
   }
 
   scope :appeal_docketed, lambda {
-    where("""
-      appeal_docketed IS TRUE AND
-      hearing_postponed IS FALSE AND
-      scheduled_in_error IS FALSE AND
-      hearing_scheduled IS FALSE AND
-      (
+    where(
+      <<~SQL
+        appeal_docketed IS TRUE AND
+        hearing_postponed IS FALSE AND
+        scheduled_in_error IS FALSE AND
+        hearing_scheduled IS FALSE AND
         (
-          hearing_withdrawn IS FALSE AND
-          vso_ihp_pending IS FALSE AND
-          privacy_act_pending IS FALSE
-        ) OR (
-          hearing_withdrawn IS TRUE
+          (
+            hearing_withdrawn IS FALSE AND
+            vso_ihp_pending IS FALSE AND
+            privacy_act_pending IS FALSE
+          ) OR (
+            hearing_withdrawn IS TRUE
+          )
         )
-      )
-    """)
+      SQL
+    )
   }
 end
