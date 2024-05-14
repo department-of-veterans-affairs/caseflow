792649519a1101203b1abed53a191b55311f005f
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-05-14 19:53:00 -0400

    Add rdoc comment to init job

--- app/jobs/notification_initialization_job.rb
+++ app/jobs/notification_initialization_job.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Overview
+#
+# The NotificationInitializationJob encapsulates the instantiation of templates
+# for messages to be sent by VANotify.
+#
+# Information such as whether or not the claimant is deceased and if the veteran is still the
+#   primary claimant (or if an appellant substitution has taken place) is gathered and factored
+#   into what is then sent to the SendNotificationJob.
+#
+# This job was created in order to extract logic that causes calls to external services so that
+#   large batch notification queueing jobs, like the QuarterlyNotificationsJob, can run much more quickly.
 class NotificationInitializationJob < CaseflowJob
   include Hearings::EnsureCurrentUserIsSet
 
