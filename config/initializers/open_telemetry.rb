edc7dd3c44e35d46014c7b29c7b2ed5ae4a8ae9e
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2024-05-14 13:44:57 -0400

    Change config implementation.

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -2,27 +2,27 @@
 
 DT_API_URL = ENV["DT_API_URL"]
 DT_API_TOKEN = ENV["DT_API_TOKEN"]
-def init_opentelemetry # rubocop:disable Metrics/MethodLength
-  OpenTelemetry::SDK.configure do |c|
-    c.service_name = "caseflow"
-    c.service_version = "1.0.1"
-    # automatic instrumentation
-    c.use_all
-    for name in ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_metadata.properties", "/var/lib/dynatrace/enrichment/dt_host_metadata.properties"] do
-      begin
-        c.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
-      rescue
-      end
+
+OpenTelemetry::SDK.configure do |config|
+  config.service_name = "caseflow"
+  config.service_version = "1.0.1"
+  # automatic instrumentation
+  config.use_all
+  for name in ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_metadata.properties", "/var/lib/dynatrace/enrichment/dt_host_metadata.properties"] do
+    begin
+      config.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
+    rescue
     end
-    c.add_span_processor(
-      OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-        OpenTelemetry::Exporter::OTLP::Exporter.new(
-          endpoint: DT_API_URL + "/v1/traces",
-          headers: {
-            "Authorization": "Api-Token " + DT_API_TOKEN
-          }
-        )
+  end
+  config.add_span_processor(
+    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+      OpenTelemetry::Exporter::OTLP::Exporter.new(
+        endpoint: DT_API_URL + "/v1/traces",
+        headers: {
+          "Authorization": "Api-Token " + DT_API_TOKEN
+        }
       )
     )
-  end
+  )
 end
+
