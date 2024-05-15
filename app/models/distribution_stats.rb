0d1c166bbfb9faa8ffc5c87eca8bf53c9a1af618
Matthew Roth <roth_matthew@bah.com>
2024-05-15 15:40:13 -0400

    Update distribution_stats.rb

--- app/models/distribution_stats.rb
+++ app/models/distribution_stats.rb
@@ -2,17 +2,4 @@
 
 class DistributionStats < CaseflowRecord
   belongs_to :distribution
-
-  def initialize
-    puts "test"
-    super
-  end
-
-  def self.from_hash(stats)
-
-    create_distribution_stats!(
-      statistics: stats,
-      levers: CaseDistributionLever.snapshot
-    )
-  end
 end
