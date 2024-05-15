4fc06d83b4d54bf609b7f4583ae6fb87d688bcb5
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-05-15 15:43:23 -0400

    ⬆️ bin/rails app:update - Apply relevant changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,10 @@
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -49,6 +51,12 @@ Rails.application.configure do
   require_relative "../../app/services/deprecation_warnings/development_handler"
   ActiveSupport::Deprecation.behavior = DeprecationWarnings::DevelopmentHandler
 
+  # Raise exceptions for disallowed deprecations.
+  # config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  # config.active_support.disallowed_deprecation_warnings = []
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
@@ -64,7 +72,10 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
@@ -72,6 +83,9 @@ Rails.application.configure do
 
   # Bypass DNS rebinding protection for all `demo` sub-domains
   config.hosts << ".demo.appeals.va.gov"
+
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
 
   #=====================================================================================================================
   # Please keep custom config settings below this comment.
