e898eff64baf5a8bbd78a07b01c7d0ac721adc4f
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-05-16 15:38:52 -0400

    🔧 Assume default for `config.action_view.preload_links_header`
    This flag can be safely uncommented. Browsers that support Link
    headers will get a performance boost. Browsers that don’t will
    ignore them.
    
    We override in `development` environments to avoid an edge case
    leading to an HTTP response header overflow.
    
    For more background, see
    https://lilyreile.medium.com/rails-6-1-new-framework-defaults-what-they-do-and-how-to-safely-uncomment-them-c546b70f0c5e#3679

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -5,7 +5,3 @@
 # Once upgraded flip defaults one by one to migrate to the new default.
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
-
-# Generate a `Link` header that gives a hint to modern browsers about
-# preloading assets when using `javascript_include_tag` and `stylesheet_link_tag`.
-# Rails.application.config.action_view.preload_links_header = true
