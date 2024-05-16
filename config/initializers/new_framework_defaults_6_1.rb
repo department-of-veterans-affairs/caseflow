3ba1e0c9ad88b0a1a20a56643a09d65eda6af5f5
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-05-16 15:33:01 -0400

    🔧 Assume default for `config.action_mailer.deliver_later_queue_name`
    We're not using `ActionMailer::MessageDelivery #deliver_later` anywhere,
    so the default is safe to assume.

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -6,9 +6,6 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-# Set the default queue name for the mail deliver job to the queue adapter default.
-# Rails.application.config.action_mailer.deliver_later_queue_name = nil
-
 # Generate a `Link` header that gives a hint to modern browsers about
 # preloading assets when using `javascript_include_tag` and `stylesheet_link_tag`.
 # Rails.application.config.action_view.preload_links_header = true
