c0da7453b3623c027c501cc155afa49bbf7e7a4f
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-05-16 15:55:36 -0400

    🔧 Load defaults for Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "vbms"
 module CaseflowCertification
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
