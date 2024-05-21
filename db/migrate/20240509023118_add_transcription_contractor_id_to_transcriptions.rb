416cab71b8befc476347d8fdd0a195eab5e7eb57
Adam Ducker <adam.ducker@va.gov>
2024-05-21 14:57:45 -0500

    Remove bad migration

--- db/migrate/20240509023118_add_transcription_contractor_id_to_transcriptions.rb
+++
@@ -1,8 +0,0 @@
-class AddTranscriptionContractorIdToTranscriptions < ActiveRecord::Migration[6.0]
-  disable_ddl_transaction!
-
-  def change
-    add_column :transcriptions, :transcription_contractor_id, :bigint
-    add_index :transcriptions, :transcription_contractor_id, algorithm: :concurrently
-  end
-end
