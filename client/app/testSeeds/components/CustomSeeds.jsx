cf9c9efd510ab927cfa331d245cb2ec002db4137
SHarshain <spoosa@razortalent.com>
2024-05-21 01:20:03 -0400

    APPEALS-44911. Change the pure react components to functional components

--- client/app/testSeeds/components/CustomSeeds.jsx
+++ client/app/testSeeds/components/CustomSeeds.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import COPY from '../../../COPY';
 import ApiUtil from '../../util/ApiUtil';
 import Button from 'app/components/Button';
@@ -6,166 +7,180 @@ import NumberField from 'app/components/NumberField';
 import TextField from 'app/components/TextField';
 import cx from 'classnames';
 import CUSTOM_SEEDS from '../../../constants/CUSTOM_SEEDS';
+import { PlusIcon } from 'app/components/icons/PlusIcon';
+import { addCustomSeed, removeCustomSeed, saveCustomSeeds } from '../reducers/seeds/seedsActions';
 
-class CustomSeeds extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reseedingStatus: {
-        Aod: false,
-        NonAod: false,
-        Tasks: false,
-        Hearings: false,
-        Intake: false,
-        Dispatch: false,
-        Jobs: false,
-        Substitutions: false,
-        DecisionIssues: false,
-        CavcAmaAppeals: false,
-        SanitizedJsonSeeds: false,
-        VeteransHealthAdministration: false,
-        MTV: false,
-        Education: false,
-        PriorityDistributions: false,
-        TestCaseData: false,
-        CaseDistributionAuditLeverEntries: false,
-        Notifications: false,
-        CavcDashboardData: false,
-        VbmsExtClaim: false,
-        CasesTiedToJudgesNoLongerWithBoard: false,
-        StaticTestCaseData: false,
-        StaticDispatchedAppealsTestData: false,
-        RemandedAmaAppeals: false,
-        RemandedLegacyAppeals: false,
-        PopulateCaseflowFromVacols: false
-      },
-      seedRunningStatus: false,
-      seedRunningMsg: 'Seeds running'
-    };
-    this.seedByType = {};
-  }
+const CustomSeeds = () => {
+  const [reseedingStatus, setReseedingStatus] = useState({
+    Aod: false,
+    NonAod: false,
+    Tasks: false,
+    Hearings: false,
+    Intake: false,
+    Dispatch: false,
+    Jobs: false,
+    Substitutions: false,
+    DecisionIssues: false,
+    CavcAmaAppeals: false,
+    SanitizedJsonSeeds: false,
+    VeteransHealthAdministration: false,
+    MTV: false,
+    Education: false,
+    PriorityDistributions: false,
+    TestCaseData: false,
+    CaseDistributionAuditLeverEntries: false,
+    Notifications: false,
+    CavcDashboardData: false,
+    VbmsExtClaim: false,
+    CasesTiedToJudgesNoLongerWithBoard: false,
+    StaticTestCaseData: false,
+    StaticDispatchedAppealsTestData: false,
+    RemandedAmaAppeals: false,
+    RemandedLegacyAppeals: false,
+    PopulateCaseflowFromVacols: false
+  });
 
-  onChangeCaseType = (type, inputKey, value) => {
-    if (typeof this.seedByType[type] !== 'object') {
-      this.seedByType[type] = {};
-    }
-    this.seedByType[type][inputKey] = value;
-  }
+  // const [seedRunningStatus, setSeedRunningStatus] = useState(false);
+  // const [seedRunningMsg, setSeedRunningMsg] = useState('Seeds running');
+  // const [seedByType, setSeedByType] = useState({});
 
-  reseedByCaseType = (type) => {
-    const caseType = this.seedByType[type];
+  const theState = useSelector((state) => state);
+  console.log(theState.testSeeds.seeds);
+  const dispatch = useDispatch();
 
-    caseType.seed_type = type;
-
-    this.setState({ seedRunning: true, seedRunningMsg: '' });
-    this.setState((prevState) => ({
-      reseedingStatus: { ...prevState.reseedingStatus, [type]: true }
+  const onChangeCaseType = (type, inputKey, value) => {
+    setSeedByType(prevState => ({
+      ...prevState,
+      [type]: {
+        ...(prevState[type] || {}),
+        [inputKey]: value
+      }
     }));
-
-    // ApiUtil.post(`/seeds/run-demo/${type}`, { data: caseType }).then(() => {
-    ApiUtil.post('/seeds/run-demo', { data: caseType }).then(() => {
-      this.setState({ seedRunning: false });
-      this.setState((prevState) => ({
-        reseedingStatus: { ...prevState.reseedingStatus, [type]: false }
-      }));
-    }).
-      catch((err) => {
-        console.warn(err);
-        this.setState({ seedRunning: false });
-        this.setState((prevState) => ({
-          reseedingStatus: { ...prevState.reseedingStatus, [type]: false }
-        }));
-      });
   };
 
-  render() {
-    const seedTypes = Object.keys(CUSTOM_SEEDS);
+  const reseedByCaseType = (type) => {
 
-    return (
-      <div>
-        <>
-          <h2 id="run_custom_seeds">{COPY.TEST_SEEDS_CUSTOM_SEEDS}</h2>
-          <table className="seed-table-style" >
-            <thead>
-              <tr>
-                <th className={cx('table-header-styling')}>
-                  Case Type
-                </th>
-                <th className={cx('table-header-styling')}>
-                  Number of cases to create
-                </th>
-                <th className={cx('table-header-styling')}>
-                  Days Ago
-                </th>
-                <th className={cx('table-header-styling')}>
-                  Judge CSS_ID (optional)
-                </th>
-                <th className={cx('table-header-styling')}>
-                </th>
+    const caseType = seedByType[type];
+    caseType.seed_type = type;
+
+    dispatch(addCustomSeed(caseType));
+
+    // ApiUtil.post('/seeds/run-demo', { data: caseType })
+    //   .then(() => {
+    //     setSeedRunningStatus(false);
+    //     setReseedingStatus(prevState => ({
+    //       ...prevState,
+    //       [type]: false
+    //     }));
+    //   })
+    //   .catch(err => {
+    //     console.warn(err);
+    //     setSeedRunningStatus(false);
+    //     setReseedingStatus(prevState => ({
+    //       ...prevState,
+    //       [type]: false
+    //     }));
+    //   });
+  };
+
+  const saveSeeds = () => {
+    dispatch(saveCustomSeeds(theState.testSeeds.seeds));
+  };
+
+  const seedTypes = Object.keys(CUSTOM_SEEDS);
+
+  return (
+    <div>
+      <>
+        <h2 id="run_custom_seeds">{COPY.TEST_SEEDS_CUSTOM_SEEDS}</h2>
+        <table className="seed-table-style">
+          <thead>
+            <tr>
+              <th className={cx('table-header-styling')}>Case Type</th>
+              <th className={cx('table-header-styling')}>Number of cases to create</th>
+              <th className={cx('table-header-styling')}>Days Ago</th>
+              <th className={cx('table-header-styling')}>Judge CSS_ID (optional)</th>
+              <th className={cx('table-header-styling')}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {seedTypes.map(type => (
+              <tr key={type}>
+                <td>{CUSTOM_SEEDS[type]}</td>
+                <td>
+                  <div className={cx('lever-right', 'test-seeds-num-field')}>
+                    <NumberField
+                      ariaLabelText={`seed-count-${type}`}
+                      useAriaLabel
+                      inputID={`seed-count-${type}`}
+                      onChange={value => onChangeCaseType(type, 'seed_count', value)}
+                    />
+                  </div>
+                </td>
+                <td>
+                  <div className={cx('lever-right', 'test-seeds-num-field')}>
+                    <NumberField
+                      ariaLabelText={`days-ago-${type}`}
+                      useAriaLabel
+                      inputID={`days-ago-${type}`}
+                      onChange={value => onChangeCaseType(type, 'days_ago', value)}
+                    />
+                  </div>
+                </td>
+                <td>
+                  <div className={cx('lever-right', 'test-seeds-cssid-field')}>
+                    <TextField
+                      ariaLabelText={`css-id-${type}`}
+                      useAriaLabel
+                      inputID={`css-id-${type}`}
+                      onChange={value => onChangeCaseType(type, 'judge_css_id', value)}
+                    />
+                  </div>
+                </td>
+                <td>
+                  <div className="cf-add-comment-button">
+                    <Button onClick={() => reseedByCaseType(type)}>
+                      <span>
+                        <PlusIcon size={24} />
+                      </span>
+                    </Button>
+                  </div>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {seedTypes.map((type) => (
-                <tr>
-                  <td>
-                    {CUSTOM_SEEDS[type]}
-                  </td>
-                  <td>
-                    <div className={cx('lever-right', 'test-seeds-num-field')}>
-                      <NumberField
-                        ariaLabelText={`seed-count-${type}`}
-                        useAriaLabel
-                        inputID={`seed-count-${type}`}
-                        onChange={(value) => {
-                          this.onChangeCaseType(type, 'seed_count', value);
-                        }}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div className={cx('lever-right', 'test-seeds-num-field')}>
-                      <NumberField
-                        ariaLabelText={`days-ago-${type}`}
-                        useAriaLabel
-                        inputID={`days-ago-${type}`}
-                        onChange={(value) => {
-                          this.onChangeCaseType(type, 'days_ago', value);
-                        }}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div className={cx('lever-right', 'test-seeds-cssid-field')}>
-                      <TextField
-                        ariaLabelText={`css-id-${type}`}
-                        useAriaLabel
-                        inputID={`css-id-${type}`}
-                        onChange={(value) => {
-                          this.onChangeCaseType(type, 'judge_css_id', value);
-                        }}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div className="cf-btn-link lever-right test-seed-button-style">
-                      <Button
-                        onClick={() => this.reseedByCaseType(type)}
-                        id={`btn-${type}`}
-                        name="Create"
-                        loading={this.state.reseedingStatus[type]}
-                        loadingText={`Reseeding ${CUSTOM_SEEDS[type]}`}
-                      />
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <hr />
-        </>
-      </div>
-    );
-  }
-}
+            ))}
+          </tbody>
+        </table>
+        <hr />
+      </>
+      <>
+        <h2 id="show_seeds_preview">Preview</h2>
+        <table className="seed-table-style">
+          <thead>
+            <tr>
+              <th className={cx('table-header-styling')}>Case(s) Type</th>
+              <th className={cx('table-header-styling')}>Amount</th>
+              <th className={cx('table-header-styling')}>Days Ago</th>
+              <th className={cx('table-header-styling')}>Associated Judge</th>
+            </tr>
+          </thead>
+          <tbody>
+            {theState.testSeeds.seeds.map((obj, index) => (
+              <tr key={index}>
+                <td>{obj.seed_type}</td>
+                <td>{obj.seed_count} Cases</td>
+                <td>{obj.days_ago} Days Ago</td>
+                <td>{obj.judge_css_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="cf-btn-link lever-right test-seed-button-style cf-right-side">
+          <Button onClick={() => saveSeeds()} name={`Create ${theState.testSeeds.seeds.length} test cases`} />
+        </div>
+        <hr />
+      </>
+    </div>
+  );
+};
 
 export default CustomSeeds;
--- client/app/testSeeds/components/ScenarioSeeds.jsx
+++ client/app/testSeeds/components/ScenarioSeeds.jsx
@@ -1,127 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 import COPY from '../../../COPY';
 import ApiUtil from '../../util/ApiUtil';
 import Button from 'app/components/Button';
 import cx from 'classnames';
 import TEST_SEEDS from '../../../constants/TEST_SEEDS';
 
-class ScenarioSeeds extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reseedingStatus: {
-        Aod: false,
-        NonAod: false,
-        Tasks: false,
-        Hearings: false,
-        Intake: false,
-        Dispatch: false,
-        Jobs: false,
-        Substitutions: false,
-        DecisionIssues: false,
-        CavcAmaAppeals: false,
-        SanitizedJsonSeeds: false,
-        VeteransHealthAdministration: false,
-        MTV: false,
-        Education: false,
-        PriorityDistributions: false,
-        TestCaseData: false,
-        CaseDistributionAuditLeverEntries: false,
-        Notifications: false,
-        CavcDashboardData: false,
-        VbmsExtClaim: false,
-        CasesTiedToJudgesNoLongerWithBoard: false,
-        StaticTestCaseData: false,
-        StaticDispatchedAppealsTestData: false,
-        RemandedAmaAppeals: false,
-        RemandedLegacyAppeals: false,
-        PopulateCaseflowFromVacols: false
-      },
-      seedRunningStatus: false,
-      seedRunningMsg: 'Seeds running'
-    };
-    this.seedCounts = {};
-  }
+const ScenarioSeeds = () => {
+  const [reseedingStatus, setReseedingStatus] = useState({
+    Aod: false,
+    NonAod: false,
+    Tasks: false,
+    Hearings: false,
+    Intake: false,
+    Dispatch: false,
+    Jobs: false,
+    Substitutions: false,
+    DecisionIssues: false,
+    CavcAmaAppeals: false,
+    SanitizedJsonSeeds: false,
+    VeteransHealthAdministration: false,
+    MTV: false,
+    Education: false,
+    PriorityDistributions: false,
+    TestCaseData: false,
+    CaseDistributionAuditLeverEntries: false,
+    Notifications: false,
+    CavcDashboardData: false,
+    VbmsExtClaim: false,
+    CasesTiedToJudgesNoLongerWithBoard: false,
+    StaticTestCaseData: false,
+    StaticDispatchedAppealsTestData: false,
+    RemandedAmaAppeals: false,
+    RemandedLegacyAppeals: false,
+    PopulateCaseflowFromVacols: false
+  });
+  const [seedRunningStatus, setSeedRunningStatus] = useState(false);
+  const [seedRunningMsg, setSeedRunningMsg] = useState('Seeds running');
+  const [seedCounts, setSeedCounts] = useState({});
 
-  handleChange= (event, type) => {
-    this.seedCounts[type] = event.target.value;
-  }
-
-  formatSeedName = (name) => {
-    return name.split('-').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).
-      join(' ');
+  const handleChange = (event, type) => {
+    setSeedCounts(prevState => ({
+      ...prevState,
+      [type]: event.target.value
+    }));
   };
 
-  reseed = (type) => {
-    const seedCount = parseInt(this.seedCounts[type], 10) || 1;
+  const formatSeedName = (name) => {
+    return name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  };
 
-    this.setState({ seedRunning: true, seedRunningMsg: '' });
-    this.setState((prevState) => ({
-      reseedingStatus: { ...prevState.reseedingStatus, [type]: true }
+  const reseed = (type) => {
+    const seedCount = parseInt(seedCounts[type], 10) || 1;
+
+    setSeedRunningStatus(true);
+    setSeedRunningMsg('');
+    setReseedingStatus(prevState => ({
+      ...prevState,
+      [type]: true
     }));
 
-    // const endpoint = `/seeds/run-demo/${type}?seed_count=${seedCount}`;
     const endpoint = `/seeds/run-demo?seed_type=${type}&seed_count=${seedCount}`;
 
-    ApiUtil.post(endpoint).then(() => {
-      this.setState({ seedRunning: false });
-      this.setState((prevState) => ({
-        reseedingStatus: { ...prevState.reseedingStatus, [type]: false }
-      }));
-    }).
-      catch((err) => {
+    ApiUtil.post(endpoint)
+      .then(() => {
+        setSeedRunningStatus(false);
+        setReseedingStatus(prevState => ({
+          ...prevState,
+          [type]: false
+        }));
+      })
+      .catch(err => {
         console.warn(err);
-        this.setState({ seedRunning: false });
-        this.setState((prevState) => ({
-          reseedingStatus: { ...prevState.reseedingStatus, [type]: false }
+        setSeedRunningStatus(false);
+        setReseedingStatus(prevState => ({
+          ...prevState,
+          [type]: false
         }));
       });
   };
 
-  render() {
-    const seedTypes = Object.keys(TEST_SEEDS);
+  const seedTypes = Object.keys(TEST_SEEDS);
 
-    return (
-      <div>
-        <>
-          <h2 id="run_seeds">{COPY.TEST_SEEDS_RUN_SEEDS}</h2>
-          <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
-            {seedTypes.map((type) => (
-              <li key={type}>
-                <div className={cx('lever-right', 'test-seeds-num-field')}>
-                  <input
-                    aria-label={`count-${type}`}
-                    type="text"
-                    id={`count-${type}`}
-                    onChange={(event) => this.handleChange(event, type)}
-                  />
-                </div>
-                <div className="cf-btn-link test-seed-button-style">
-                  <Button
-                    onClick={() => this.reseed(type)}
-                    name={`Run Demo ${this.formatSeedName(type)}`}
-                    loading={this.state.reseedingStatus[type]}
-                    loadingText={`Reseeding ${this.formatSeedName(type)}`}
-                  />
-                </div>
-                <>
-                  {this.state.reseedingStatus[type] && (
-                    <div className="test-seed-alert-message">
-                      <span>{this.formatSeedName(type)} {COPY.TEST_SEEDS_ALERT_MESSAGE}</span>
-                    </div>
-                  )}
-                </>
-              </li>
-            ))}
-          </ul>
-        </>
-      </div>
-    );
-  }
-}
-
-// ScenarioSeeds.propTypes = {
-
-// }
+  return (
+    <div>
+      <>
+        <h2 id="run_seeds">{COPY.TEST_SEEDS_RUN_SEEDS}</h2>
+        <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+          {seedTypes.map(type => (
+            <li key={type}>
+              <div className={cx('lever-right', 'test-seeds-num-field')}>
+                <input
+                  aria-label={`count-${type}`}
+                  type="text"
+                  id={`count-${type}`}
+                  onChange={event => handleChange(event, type)}
+                />
+              </div>
+              <div className="cf-btn-link test-seed-button-style">
+                <Button
+                  onClick={() => reseed(type)}
+                  name={`Run Demo ${formatSeedName(type)}`}
+                  loading={reseedingStatus[type]}
+                  loadingText={`Reseeding ${formatSeedName(type)}`}
+                />
+              </div>
+              <>
+                {reseedingStatus[type] && (
+                  <div className="test-seed-alert-message">
+                    <span>{formatSeedName(type)} {COPY.TEST_SEEDS_ALERT_MESSAGE}</span>
+                  </div>
+                )}
+              </>
+            </li>
+          ))}
+        </ul>
+      </>
+    </div>
+  );
+};
 
 export default ScenarioSeeds;
