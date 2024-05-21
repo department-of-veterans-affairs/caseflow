fae9f79d1a6993b9dcfeb84dbb21cc88a21c567b
Prajwal Amatya <pamatya@take2it.com>
2024-05-21 11:49:09 -0600

    fixing linter issue

--- client/app/intake/components/UnidentifiedIssuesModal.jsx
+++ client/app/intake/components/UnidentifiedIssuesModal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { css } from 'glamor';
 import Modal from '../../components/Modal';
 import TextField from '../../components/TextField';
 import DateSelector from '../../components/DateSelector';
