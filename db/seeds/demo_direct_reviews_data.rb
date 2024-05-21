6c144e507262555a77162567d3f593025663d2c4
Christopher Detlef <>
2024-05-21 13:33:29 -0400

    Fix rspec tests and properly check for judge css id

--- db/seeds/demo_direct_reviews_data.rb
+++ db/seeds/demo_direct_reviews_data.rb
@@ -21,8 +21,6 @@ module Seeds
         :appeal,
         :direct_review_docket,
         :ready_for_distribution,
-        :tied_to_judge,
-        tied_judge: @associated_judge,
         associated_judge: @associated_judge,
         veteran: create_demo_veteran_for_docket,
         receipt_date: @days_ago
@@ -44,7 +42,7 @@ module Seeds
     end
 
     def find_or_create_demo_seed_judge(judge_ccs_id)
-      unless judge_ccs_id.nil?
+      unless judge_ccs_id.blank?
         User.find_by_css_id(judge_ccs_id) ||
           create(:user, :judge, :with_vacols_judge_record, css_id: judge_ccs_id, full_name: "Demo Judge " + judge_ccs_id)
       else
