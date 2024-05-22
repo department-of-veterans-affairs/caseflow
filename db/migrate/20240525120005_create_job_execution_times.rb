883ef00feb5420805abd323f76bafaa29eef4009
Prerna Devulapalli <prerna.devulapalli@va.gov>
2024-05-22 11:27:39 -0700

    APPEALS-46179 Add comment

--- db/migrate/20240525120005_create_job_execution_times.rb
+++ db/migrate/20240525120005_create_job_execution_times.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Add table to track the Time a Job was Last Executed
 class CreateJobExecutionTimes < Caseflow::Migration
   def change
     create_table :job_execution_times, id: :serial do |t|
