40c6a247b1255b412192ec3976919577635fc5c0
piedram <maite.piedrayera@va.gov>
2024-05-22 16:43:36 -0400

    add file in s3

--- app/jobs/hearings/zip_and_upload_transcription_package_job.rb
+++ app/jobs/hearings/zip_and_upload_transcription_package_job.rb
@@ -1,74 +1,89 @@
 # frozen_string_literal: true
 
 class Hearings::ZipAndUploadTranscriptionPackageJob < CaseflowJob
-
   queue_as :low_priority
+  S3_BUCKET = "vaec-appeals-caseflow"
   # attr_reader :tmp_files_to_cleanup
 
   class ZipTranscriptionPackageUploadError < StandardError; end
 
   retry_on TranscriptionFileUpload::FileUploadError, wait: :exponentially_longer do |job, _exception|
-    # job.cleanup_tmp_files
+    job.cleanup_tmp_files
     fail ZipTranscriptionPackageUploadError
   end
 
+  def initialize(*args)
+    super(*args)
+    @work_order_tmp_path = nil
+    @transcription_files_zip_tmp_path = []
+    @bom_file_tmp_path = nil
+    @all_path = []
+  end
 
   def perform(work_order)
-    work_order_tmp = fetch_work_order_file_tmp(work_order)
-    transcription_files_zip_tmp = fetch_all_transcription_files_zip_tmp()
-    bom_file_tmp = fetch_bom_file_tmp(work_order)
-    transcription_package_tmp = create_master_file_zip(work_order_tmp, transcription_files_zip_tmp, bom_file_tmp)
-    # Save this file in the DB?? create a new record yes
-    # upload_to_s3!(transcription_package)
+    @work_order_tmp_path = fetch_work_order_file_tmp(work_order)
+    fetch_all_transcription_files_zip_tmp
+    @bom_file_tmp_path = fetch_bom_file_tmp(work_order)
+    transcription_package_tmp = create_master_file_zip
+    upload_transcription_package_to_s3(transcription_package_tmp)
+    # save_transcription_package_table(transcription_package_tmp, work_order)
     true
+  end
+
+  def cleanup_tmp_files
+    @all_path&.each { |path| File.delete(path) if File.exist?(path) }
+    Rails.logger.info("Cleaned up the following files from tmp: #{@all_path}")
   end
 
   private
 
   def fetch_work_order_file_tmp(work_order)
-    File.open("tmp/transcription_files/xls/#{work_order[:work_order_name]}.xls")
+    current_path = File.open("tmp/transcription_files/xls/#{work_order[:work_order_name]}.xls")
+    @all_path << current_path
+    current_path
   end
 
-  def fetch_all_transcription_files_zip_tmp()
-    all_zips_files = []
+  def fetch_all_transcription_files_zip_tmp
     Dir.each_child("tmp/transcription_files/zip") do |zip_files|
-      all_zips_files << File.open("tmp/transcription_files/zip/#{zip_files}")
+      @transcription_files_zip_tmp_path << File.open("tmp/transcription_files/zip/#{zip_files}")
+      @all_path << File.open("tmp/transcription_files/zip/#{zip_files}")
     end
-    all_zips_files
   end
 
   def fetch_bom_file_tmp(work_order)
     bom_name = work_order[:work_order_name].sub("BVA", "BOM")
-    File.open("tmp/transcription_files/json/#{bom_name}.json")
+    current_path = File.open("tmp/transcription_files/json/#{bom_name}.json")
+    @all_path << current_path
+    current_path
   end
 
-  def create_master_file_zip(work_order_file, transcription_files_zip_tmp, bom_file_file)
-    zip_master_file_path = add_to_master(work_order_file, transcription_files_zip_tmp, bom_file_file)
-    formatted_zip_master_path = rename_before_upload(zip_master_file_path, work_order_file)
+  def create_master_file_zip
+    zip_master_file_path = add_to_master
+    rename_before_upload(zip_master_file_path)
   end
 
-  def generate_zip_master_file_name()
+  def generate_zip_master_file_name
     File.join(
       Rails.root, "tmp", "transcription_files", "zip", "master.zip"
     )
   end
 
-  def add_to_master(work_order_file, transcription_files_zip_tmp, bom_file_file)
-    tmp_master_zip_path = generate_zip_master_file_name()
+  def add_to_master
+    tmp_master_zip_path = generate_zip_master_file_name
     Zip::File.open(tmp_master_zip_path, create: true) do |zip_file|
-      zip_file.add(File.basename(work_order_file), work_order_file)
-      zip_file.add(File.basename(bom_file_file), bom_file_file)
-      transcription_files_zip_tmp.each { |path| zip_file.add(File.basename(path), path) }
+      zip_file.add(File.basename(@work_order_tmp_path), @work_order_tmp_path)
+      zip_file.add(File.basename(@bom_file_tmp_path), @bom_file_tmp_path)
+      @transcription_files_zip_tmp_path.each { |path| zip_file.add(File.basename(path), path) }
     end
     tmp_master_zip_path
   end
 
-  def rename_before_upload(zip_master_file_path, work_order_file)
+  def rename_before_upload(zip_master_file_path)
     checksum = xor_checksum(zip_master_file_path)
-    # creation_date = format_creation_date(zip_master_file)
-    new_name = File.basename(work_order_file).sub("BVA", "BVA#{checksum}").split(".")[0]
+    new_name = File.basename(@work_order_tmp_path).sub("BVA", "BVA#{checksum}").split(".")[0]
     new_path = zip_master_file_path.sub("master", new_name)
     File.rename(zip_master_file_path, new_path)
+    @all_path << new_path
     new_path
   end
 
@@ -77,9 +92,27 @@ class Hearings::ZipAndUploadTranscriptionPackageJob < CaseflowJob
     checksum.to_s(16)
   end
 
-  def format_creation_date(zip_master_file)
-    File.ctime(zip_master_file).strftime("%Y%m%d")
+  def upload_transcription_package_to_s3(transcription_package_tmp)
+    transcription_package_name = File.basename(transcription_package_tmp)
+    # work_order_s3_path = s3_location_work_order
+    begin
+      S3Service.store_file(s3_location_master_file(transcription_package_tmp), transcription_package_tmp, :filepath)
+    rescue StandardError => error
+      Rails.logger.error "Transcription Package Job failed to upload Transcription Package
+      #{transcription_package_name} to S3: #{error.message}"
+      raise TranscriptionFileUpload
+    end
   end
 
+  # def s3_location_work_order
+  #   file_name = File.basename(@work_order_tmp_path)
+  #   folder_name = (Rails.deploy_env == :prod) ? S3_BUCKET : "#{S3_BUCKET}-#{Rails.deploy_env}"
+  #   "#{folder_name}/transcript_text/#{file_name}"
+  # end
 
+  def s3_location_master_file(transcription_package_tmp)
+    file_name = File.basename(transcription_package_tmp)
+    folder_name = (Rails.deploy_env == :prod) ? S3_BUCKET : "#{S3_BUCKET}-#{Rails.deploy_env}"
+    "#{folder_name}/transcript_text/#{file_name}"
+  end
 end
