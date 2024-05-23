9f0b10fe391172bc1e295ad59f49164db23c7614
Kamala Madamanchi <kamala.madamanchi@va.gov>
2024-05-23 11:55:42 -0500

    APPEALS-45503 updated spec and factory for new migration

--- app/workflows/transcription_package_factory.rb
+++ app/workflows/transcription_package_factory.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class TranscriptionPackageFactory
-  def initialize(task_name, created_by_id, returned_at)
+  def initialize(transcription_package)
     @transcription_package = TranscriptionPackage.create!(
-      created_by_id: created_by_id,
-      returned_at: returned_at,
-      task_number: task_name
+      aws_link_zip: transcription_package[:aws_link_zip],
+      aws_link_work_order: transcription_package[:aws_link_work_order],
+      created_by_id: transcription_package[:created_by_id],
+      returned_at: transcription_package[:returned_at],
+      task_number: transcription_package[:task_name],
+      date_upload_box: transcription_package[:date_upload_box],
+      date_upload_aws: transcription_package[:date_upload_aws]
     )
   end
 end
--- spec/workflows/transcription_package_factory_spec.rb
+++ spec/workflows/transcription_package_factory_spec.rb
@@ -5,9 +5,18 @@ describe TranscriptionPackageFactory, :postgres do
     let(:user) { create(:user) }
     it "creates transcription_packages record" do
       expect(TranscriptionPackage.count).to eq 0
-      TranscriptionPackageFactory.new("#12345", user.id, DateTime.now)
-      TranscriptionPackageFactory.new("#6780", user.id, DateTime.now)
-      expect(TranscriptionPackage.count).to eq 2
+      date_time_now = DateTime.now
+      data = {
+        aws_link_zip: "aws_link/zip_file",
+        aws_link_work_order: "aws_link/work_order",
+        created_by_id: user.id,
+        task_number: "#12345",
+        returned_at: date_time_now,
+        date_upload_box: date_time_now,
+        date_upload_aws: date_time_now
+      }
+      TranscriptionPackageFactory.new(data)
+      expect(TranscriptionPackage.count).to eq 1
     end
   end
 end
