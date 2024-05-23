327e01352d40e0d96b827ec508a749c98fa72e34
Minhazur Rahaman <minhazur9@gmail.com>
2024-05-23 12:12:46 -0400

    APPEALS-43140 adjusted where permission checks and routing should be located

--- app/controllers/hearings/transcription_files_controller.rb
+++ app/controllers/hearings/transcription_files_controller.rb
@@ -3,29 +3,13 @@
 class Hearings::TranscriptionFilesController < ApplicationController
   include HearingsConcerns::VerifyAccess
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_page_not_found
   before_action :verify_access_to_hearings, only: [:download_transcription_file]
-  before_action :verify_user_organization, only: [:transcription_file_dispatch]
-
-  def verify_user_organization
-    if !TranscriptionTeam.singleton.user_has_access?(current_user)
-      redirect_to "/unauthorized"
-    end
-  end
 
   # Downloads file and sends to user's local computer
   def download_transcription_file
     tmp_location = file.fetch_file_from_s3!
     File.open(tmp_location, "r") { |stream| send_data stream.read, filename: file.file_name }
     file.clean_up_tmp_location
-  end
-
-  def transcription_file_dispatch
-    render "hearings/index"
-  end
-
-  def render_page_not_found
-    redirect_to "/404"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -239,7 +239,7 @@ Rails.application.routes.draw do
   get 'hearings/queue/appeals/:vacols_id', to: 'queue#index'
   get 'hearings/find_closest_hearing_locations', to: 'hearings#find_closest_hearing_locations'
   get 'hearings/transcription_file/:file_id/download', to: 'hearings/transcription_files#download_transcription_file'
-  get 'hearings/transcription_files', to: 'hearings/transcription_files#transcription_file_dispatch'
+  get 'hearings/transcription_files', to: 'hearings_application#transcription_file_dispatch'
 
   post 'hearings/hearing_view/:id', to: 'hearings/hearing_view#create'
 
