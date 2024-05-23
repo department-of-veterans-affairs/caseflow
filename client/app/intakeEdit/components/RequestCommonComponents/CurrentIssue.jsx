cb7c73aed8c0d4b891fb805b25f485aa00dfa1ef
Clay Sheppard <csheepy@gmail.com>
2024-05-23 09:29:17 -0500

    format decision date in current issue section

--- client/app/intakeEdit/components/RequestCommonComponents/CurrentIssue.jsx
+++ client/app/intakeEdit/components/RequestCommonComponents/CurrentIssue.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { formatDateStr } from 'app/util/DateUtil';
 
 export const CurrentIssue = ({ currentIssue }) => {
   return (
     <div style={{ marginBottom: '2.4rem' }}>
       <h2 style={{ marginBottom: '0px' }}>Current issue</h2>
       <strong>Issue type: </strong>{currentIssue.category}<br />
-      <strong>Decision date: </strong>{currentIssue.decisionDate}<br />
+      <strong>Prior decision date: </strong>{formatDateStr(currentIssue.decisionDate)}<br />
       <strong>Issue description: </strong>{currentIssue.nonRatingIssueDescription}<br />
     </div>
   );
