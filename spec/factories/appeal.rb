1cf695b3ad14e0e31732c06bbcbea0aa2a238742
Alexandra Ferencz <ferencz_alexandra@bah.com>
2024-05-24 09:06:40 -0600

    Fix linting issues

--- spec/factories/appeal.rb
+++ spec/factories/appeal.rb
@@ -371,10 +371,8 @@ FactoryBot.define do
 
     trait :with_evidence_submission_window_task do
       after(:create) do |appeal, _evaluator|
-        root_task = RootTask.find_or_create_by!(appeal: appeal, assigned_to: Bva.singleton)
-        parent = root_task
+        parent = RootTask.find_or_create_by!(appeal: appeal, assigned_to: Bva.singleton)
         parent = appeal.tasks.open.find_by(type: "HearingTask") if appeal.docket_type == Constants.AMA_DOCKETS.hearing
-
         EvidenceSubmissionWindowTask.create!(appeal: appeal, parent: parent)
       end
     end
@@ -483,7 +481,7 @@ FactoryBot.define do
         root_task = RootTask.find_or_create_by!(appeal: appeal, assigned_to: Bva.singleton)
         distribution_task = DistributionTask.create!(appeal: appeal, parent: root_task)
         sct_task = SpecialtyCaseTeamAssignTask.create!(appeal: appeal, parent: root_task, assigned_to: SpecialtyCaseTeam.singleton)
-        hearing_related_mail_task = HearingRelatedMailTask.create!(appeal: appeal, parent: root_task, assigned_to: Bva.singleton)
+        HearingRelatedMailTask.create!(appeal: appeal, parent: root_task, assigned_to: Bva.singleton)
         distribution_task.update(status: "completed")
         sct_task.update(status: "completed")
       end
