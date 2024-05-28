4a730f21d80a2509f0ca4ee6dc0163bbddac2ff1
piedram <maite.piedrayera@va.gov>
2024-05-28 09:46:23 -0400

    If failed after retries sent Notification sent

--- app/jobs/hearings/zip_and_upload_transcription_package_job.rb
+++ app/jobs/hearings/zip_and_upload_transcription_package_job.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class Hearings::ZipAndUploadTranscriptionPackageJob < CaseflowJob
+  include Hearings::SendTranscriptionIssuesEmail
   queue_as :low_priority
   S3_BUCKET = "vaec-appeals-caseflow"
   class ZipTranscriptionPackageUploadError < StandardError; end
 
   retry_on TranscriptionFileUpload::FileUploadError, wait: :exponentially_longer do |job, _exception|
     job.cleanup_tmp_files
+    details_hash = { error: { type: "upload" }, provider: "S3" }
+    error_details = job.build_error_details(exception, details_hash)
+    job.send_transcription_issues_email(error_details)
     fail ZipTranscriptionPackageUploadError
   end
 
@@ -24,7 +28,7 @@ class Hearings::ZipAndUploadTranscriptionPackageJob < CaseflowJob
     @bom_file_tmp_path = fetch_bom_file_tmp(work_order)
     transcription_package_tmp = create_master_file_zip
     upload_transcription_package_to_s3(transcription_package_tmp)
-    # save_transcription_package_in_database(transcription_package_tmp, work_order)
+    save_transcription_package_in_database(transcription_package_tmp, work_order)
     true
   end
 
@@ -119,8 +123,10 @@ class Hearings::ZipAndUploadTranscriptionPackageJob < CaseflowJob
       aws_link_zip: s3_location_master_file(transcription_package_tmp),
       aws_link_work_order: s3_location_work_order,
       created_by_id: RequestStore[:current_user].id,
-      returned_at: work_order[:return_date],
-      task_number: work_order[:work_order_name]
+      status: "Successful upload (AWS)",
+      returned_at: Time.parse.utc(work_order[:return_date]),
+      task_number: work_order[:work_order_name],
+      contractor_id: TranscriptionContractor.find_by(name: work_order[:contractor_name])&.id
     )
   rescue ActiveRecord::RecordInvalid => error
     Rails.logger.error "Failed to create transcription file: #{error.message}"
