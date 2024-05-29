8fa7e96629944de69617f0dc2d2db48306e95f26
William Medders <medders_william@bah.com>
2024-05-29 17:10:51 -0500

    Add relationships and methods to TranscriptionPackage

--- app/models/hearings/transcription_package.rb
+++ app/models/hearings/transcription_package.rb
@@ -1,5 +1,48 @@
 # frozen_string_literal: true
 
 class TranscriptionPackage < CaseflowRecord
-  validates :returned_at, presence: true
+  belongs_to :contractor, class_name: TranscriptionContractor.name, foreign_key: :contractor_id
+  has_many :transcription_package_hearings
+  has_many :hearings, through: :transcription_package_hearings
+  has_many :transcription_package_legacy_hearings
+  has_many :legacy_hearings, through: :transcription_package_legacy_hearings
+
+  def contractor_name
+    contractor&.name
+  end
+
+  def all_hearings
+    (hearings + legacy_hearings).map { |hearing| serialize_hearing(hearing) }
+  end
+
+  def formatted_date_upload_box
+    format_date_for_table(date_upload_box)
+  end
+
+  def formatted_returned_at
+    format_date_for_table(returned_at)
+  end
+
+  def contents_count
+    (hearings + legacy_hearings).length
+  end
+
+  private
+
+  def format_date_for_table(date)
+    date.strftime("%-d/%-m/%Y")
+  end
+
+  def format_case_details(hearing)
+    file_number = ["(", hearing.veteran_file_number, ")"].join
+    [hearing.veteran_first_name, hearing.veteran_last_name, file_number].join(" ")
+  end
+
+  def serialize_hearing(hearing)
+    {
+      docketNumber: hearing.docket_number,
+      caseDetails: format_case_details(hearing),
+      hearingType: hearing.class.name
+    }
+  end
 end
