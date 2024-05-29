8255d332f07faa76084e58750bff9a57d52f9f49
William Medders <medders_william@bah.com>
2024-05-29 17:11:48 -0500

    Add #show action for Hearings::TranscriptionPackagesController

--- app/controllers/hearings/transcription_packages_controller.rb
+++ app/controllers/hearings/transcription_packages_controller.rb
@@ -2,9 +2,9 @@
 
 class Hearings::TranscriptionPackagesController < ApplicationController
   def show
-    transcription_package = TranscriptionPackage.find_by(params[:task_number])
+    transcription_package = ::TranscriptionPackage.find_by(task_number: params[:task_number])
     return render json: { error_code: "Package not found" }, status: :not_found if transcription_package.nil?
 
-    render json: Hearings::TranscriptionPackagesSerializer.new(transcription_package).serializable_hash, status: :ok
+    render json: Hearings::TranscriptionPackageSerializer.new(transcription_package).serializable_hash, status: :ok
   end
 end
