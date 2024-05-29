0760dfa528adc6f5602004f94fae22d70154aa87
William Medders <medders_william@bah.com>
2024-05-29 17:11:15 -0500

    Update TranscriptionPackage Factory

--- spec/factories/transcription_package.rb
+++ spec/factories/transcription_package.rb
@@ -2,12 +2,12 @@
 
 FactoryBot.define do
   factory :transcription_package do
-    aws_link_zip { "aws_link/zip_file" }
-    aws_link_work_order { "aws_link/work_order" }
+    aws_link_zip { "vaec-appeals-caseflow-test/transcript_text/BVAaa-1111-0001.zip" }
+    aws_link_work_order { "vaec-appeals-caseflow-test/transcript_text/BVA-1111-0001.xls" }
     created_by_id { create(:user).id }
-    returned_at { DateTime.now }
-    task_number { "#BVA-1111-0001" }
-    date_upload_box { DateTime.now }
-    date_upload_aws { DateTime.now }
+    returned_at { Time.new(2050, 01, 01) }
+    task_number { "BVA-1111-0001" }
+    date_upload_box { Time.new(2050, 01, 01) }
+    date_upload_aws { Time.new(2050, 01, 01) }
   end
 end
