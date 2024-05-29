d33652c63f312549c875c0c7af9b5a7e2fd3a2ec
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-05-28 23:34:54 -0500

    Extract toolbar buttons out to ReaderToolbar component

---
+++ client/app/readerprototype/PdfPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+const PdfPage = ({ isSelected }) => {
+
+}
+
+PdfPage.propTypes = {
+  isSelected: PropTypes.bool,
+};
+
+export default PdfPage;
