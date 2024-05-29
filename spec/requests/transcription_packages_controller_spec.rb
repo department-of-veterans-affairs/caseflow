d30d379b5e1be431f5601b77371c135ab15bd814
William Medders <medders_william@bah.com>
2024-05-29 17:02:45 -0500

    Request test: Hearings::TranscriptionPackagesController show response

---
+++ spec/requests/transcription_packages_controller_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+RSpec.describe "the Hearings::TranscriptionPackagesController", type: :request do
+  describe "the show response" do
+    let(:hearings) { (1..5).map { create(:hearing, :with_transcription_files) } }
+    let(:legacy_hearings) { (1..5).map { create(:legacy_hearing, :with_transcription_files) } }
+    let(:transcription_package) { create(:transcription_package) }
+
+    before do
+      User.authenticate!
+      Seeds::TranscriptionContractor.new.seed!
+      hearings.each do |hearing|
+        ::TranscriptionPackageHearing.create!(
+          hearing_id: hearing.id,
+          transcription_package_id: transcription_package.id
+        )
+      end
+      legacy_hearings.each do |legacy_hearing|
+        ::TranscriptionPackageLegacyHearing.create!(
+          legacy_hearing_id: legacy_hearing.id,
+          transcription_package_id: transcription_package.id
+        )
+      end
+      transcription_package.update!(
+        contractor_id: TranscriptionContractor.first&.id,
+        status: "Successfully uploaded to Box.com"
+      )
+    end
+
+    it "returns ok status" do
+      get hearings_transcription_package_path(transcription_package.task_number)
+
+      expect(response.status).to eq(200)
+    end
+
+    it "returns json" do
+      get hearings_transcription_package_path(transcription_package.task_number)
+
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(data.keys).to eq([:id, :type, :attributes])
+
+      attrs = data[:attributes]
+
+      expect(attrs[:taskNumber]).to eq("BVA-1111-0001")
+      expect(attrs[:dateSent]).to eq("1/1/2050")
+      expect(attrs[:returnDate]).to eq("1/1/2050")
+      expect(attrs[:status]).to eq("Successfully uploaded to Box.com")
+      expect(attrs[:contractorName]).to eq("Genesis Government Solutions, Inc.")
+      expect(attrs[:orderContentsCount]).to eq(10)
+
+      attrs[:hearings].each do |hearing|
+        expect(hearing).to be_a(Hash)
+        expect(hearing.keys).to eq([:docketNumber, :caseDetails, :hearingType])
+      end
+    end
+  end
+end
