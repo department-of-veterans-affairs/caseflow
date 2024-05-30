1df345273a4d818b4028c194e138d4d960b4a2b1
AimanK <Aiman.Kayad@va.gov>
2024-05-30 16:43:25 -0400

    avoid IHP task creation with open pre-docket task

--- app/workflows/ihp_tasks_factory.rb
+++ app/workflows/ihp_tasks_factory.rb
@@ -9,6 +9,18 @@ class IhpTasksFactory
 
   def create_ihp_tasks!
     appeal = @parent.appeal
+
+    if appeal.status.send(:open_pre_docket_task?)
+      appeal.representatives.select { |org| org.should_write_ihp?(appeal) }.map do |vso_organization|
+        existing_task = InformalHearingPresentationTask.find_by(
+          appeal: appeal,
+          assigned_to: vso_organization
+        )
+        existing_task&.update!(status: Constants.TASK_STATUSES.cancelled)
+      end
+      return []
+    end
+
     appeal.representatives.select { |org| org.should_write_ihp?(appeal) }.map do |vso_organization|
       # For some RAMP appeals, this method may run twice.
       existing_task = InformalHearingPresentationTask.find_by(
