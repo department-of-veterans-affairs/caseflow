8b1e0f2770426124e886bbc800fc91ca75bf2680
Matthew Roth <roth_matthew@bah.com>
2024-05-31 12:36:55 -0400

    Setup yaml UI

---
+++ spec/requests/api/v1/jobs_spec.rb
@@ -0,0 +1,21 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/jobs', type: :request do
+
+  path '/api/v1/jobs' do
+
+    post('create job') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v2/appeals_spec.rb
@@ -0,0 +1,21 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v2/appeals', type: :request, openapi_spec: 'v2/swagger.yaml' do
+
+  path '/api/v2/appeals' do
+
+    get('list appeals') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
