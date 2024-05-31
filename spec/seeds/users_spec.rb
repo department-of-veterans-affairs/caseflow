cc267b2358b4596880884d63d77611725b1b07f1
Craig Reese <Reese_Craig@bah.com>
2024-05-31 10:59:46 -0500

    fix tests

--- spec/seeds/users_spec.rb
+++ spec/seeds/users_spec.rb
@@ -4,7 +4,7 @@ describe Seeds::Users do
   describe "#seed!" do
     subject { described_class.new.seed! }
 
-    it "creates all kinds of users and organizations" do
+    it "creates all kinds of users and organizations", :aggregate_failures do
       expect { subject }.to_not raise_error
       expect(User.count).to eq(148)
       # This is creating 70 locally and 72 in GHA
