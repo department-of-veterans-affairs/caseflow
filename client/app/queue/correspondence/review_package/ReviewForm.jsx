ef9f0b7b75f97da0a4d845525ef38cdaded42d47
Dev-KRedd <134650238+Dev-KRedd@users.noreply.github.com>
2024-05-31 11:04:57 -0400

    Khalin/appeals 45867 (#21703)
    * WIP
    
    * Reverting changes
    
    * Save Changes Bug Fixed
    
    * Return To Queue button working as intended
    
    * Prevents save action in case of errorMessage
    
    * WIP
    
    * WIP
    
    * Working as intended
    
    * Removing comments
    
    * fixing tests
    
    * fixing lint error
    
    ---------
    
    Co-authored-by: vinner57 <128258952+vinner57@users.noreply.github.com>

--- client/app/queue/correspondence/review_package/ReviewForm.jsx
+++ client/app/queue/correspondence/review_package/ReviewForm.jsx
@@ -1,5 +1,5 @@
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import TextField from '../../../components/TextField';
@@ -10,19 +10,50 @@ import ApiUtil from '../../../util/ApiUtil';
 import PropTypes from 'prop-types';
 import Modal from '../../../components/Modal';
 import DateSelector from '../../../components/DateSelector';
-import { updateCmpInformation } from '../correspondenceReducer/reviewPackageActions';
+import { updateCmpInformation, setCreateRecordIsReadOnly } from '../correspondenceReducer/reviewPackageActions';
 import { validateDateNotInFuture } from '../../../intake/util/issues';
 import moment from 'moment';
 
 export const ReviewForm = (props) => {
+  const correspondenceTypes = props.veteranInformation.correspondence_types;
+  // eslint-disable-next-line max-len
+  const [correspondenceTypeID, setCorrespondenceTypeID] = useState(props.editableData.default_select_value);
   // eslint-disable-next-line max-len
   const [vaDORDate, setVADORDate] = useState(moment.utc((props.correspondence.va_date_of_receipt)).format('YYYY-MM-DD'));
   const [dateError, setDateError] = useState(false);
+  const [saveChanges, setSaveChanges] = useState(true);
   const stateCorrespondence = useSelector(
     (state) => state.reviewPackage.correspondence
   );
 
+  useEffect(() => {
+    setCorrespondenceTypeID(-1);
+    setCreateRecordIsReadOnly('Select...');
+  }, []);
+
+  const handleCorrespondenceTypeEmpty = () => {
+    if (correspondenceTypeID < 0) {
+      return 'Select...';
+    }
+
+    return correspondenceTypes[correspondenceTypeID].name;
+  };
+
+  const isCorrTypeSelected = () => {
+    if (props.createRecordIsReadOnly === 'Select...') {
+      props.setCorrTypeSelected(true);
+    // eslint-disable-next-line no-negated-condition
+    } else if (props.createRecordIsReadOnly === '' && props.corrTypeSaved !== -1) {
+      props.setCorrTypeSelected(false);
+    } else {
+      props.setCorrTypeSelected(true);
+    }
+  };
+
   const handleFileNumber = (value) => {
+    setSaveChanges(false);
+    props.setIsReturnToQueue(true);
+    isCorrTypeSelected();
     const isNumeric = value === '' || (/^\d{0,9}$/).test(value);
 
     if (isNumeric) {
@@ -36,6 +67,8 @@ export const ReviewForm = (props) => {
   };
 
   const handleChangeNotes = (value) => {
+    setSaveChanges(false);
+    props.setIsReturnToQueue(true);
     const updatedNotes = {
       ...props.editableData,
       notes: value,
@@ -62,11 +95,16 @@ export const ReviewForm = (props) => {
   };
 
   const handleSelectCorrespondenceType = (val) => {
+    setSaveChanges(false);
+    props.setIsReturnToQueue(true);
+    setCorrespondenceTypeID(val.id - 1);
     const updatedSelectedValue = {
       ...props.editableData,
       default_select_value: val.id,
     };
 
+    props.setCreateRecordIsReadOnly(handleCorrespondenceTypeEmpty());
+    props.setCorrTypeSaved(updatedSelectedValue.default_select_value);
     props.setEditableData(updatedSelectedValue);
   };
 
@@ -100,6 +138,8 @@ export const ReviewForm = (props) => {
   };
 
   const handleSubmit = async () => {
+    props.setCreateRecordIsReadOnly('');
+    isCorrTypeSelected();
     const correspondence = props;
     const payloadData = {
       data: {
@@ -122,9 +162,10 @@ export const ReviewForm = (props) => {
 
       const { body } = response;
 
+      props.setDisableButton((current) => !current);
+      props.setIsReturnToQueue(false);
       if (body.status === 'ok') {
         props.fetchData();
-        props.setDisableButton((current) => !current);
         props.setErrorMessage('');
       }
     } catch (error) {
@@ -133,6 +174,20 @@ export const ReviewForm = (props) => {
       props.setErrorMessage(body.error);
     }
   };
+  // Tracking the Correspondence type value changing for the Create record button
+
+  useEffect(() => {
+    isCorrTypeSelected();
+  }, [handleSubmit]);
+
+  // Prevents save action in case of errorMessage
+  useEffect(() => {
+
+    if (props.errorMessage) {
+      return;
+    }
+    setSaveChanges(true);
+  }, []);
 
   const veteranFileNumStyle = () => {
     if (props.errorMessage) {
@@ -199,7 +254,7 @@ export const ReviewForm = (props) => {
           name="Save changes"
           href="/queue/correspondence/12/intake"
           classNames={['usa-button-primary']}
-          disabled={!props.disableButton || props.isReadOnly || dateError}
+          disabled={!props.disableButton || props.isReadOnly || dateError || saveChanges}
           onClick={handleSubmit}
         />
       </div>
@@ -249,7 +304,7 @@ export const ReviewForm = (props) => {
                 options={generateOptions(props.reviewDetails.dropdown_values)}
                 onChange={handleSelectCorrespondenceType}
                 readOnly={props.isReadOnly}
-                placeholder="Select..."
+                placeholder= {correspondenceTypeID < 0 ? 'Select...' : handleCorrespondenceTypeEmpty}
               />
             </div>
           </div>
@@ -294,10 +349,18 @@ ReviewForm.propTypes = {
     veteran_file_number: PropTypes.string,
     default_select_value: PropTypes.number,
   }),
+  veteranInformation: PropTypes.shape({
+    correspondence_types: PropTypes.array,
+  }),
   disableButton: PropTypes.bool,
+  setIsReturnToQueue: PropTypes.bool,
   setEditableData: PropTypes.func,
+  setCreateRecordIsReadOnly: PropTypes.func,
+  createRecordIsReadOnly: PropTypes.string,
+  setCorrTypeSaved: PropTypes.func,
   setDisableButton: PropTypes.func,
   setErrorMessage: PropTypes.func,
+  corrTypeSaved: PropTypes.number,
   fetchData: PropTypes.func,
   showModal: PropTypes.bool,
   handleModalClose: PropTypes.func,
@@ -308,6 +371,7 @@ ReviewForm.propTypes = {
   userIsCorrespondenceSuperuser: PropTypes.bool,
   userIsCorrespondenceSupervisor: PropTypes.bool,
   correspondence: PropTypes.object,
+  setCorrTypeSelected: PropTypes.bool
 };
 
 const mapStateToProps = (state) => ({
@@ -318,7 +382,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
-  updateCmpInformation
+  updateCmpInformation,
+  setCreateRecordIsReadOnly
 }, dispatch);
 
 export default
