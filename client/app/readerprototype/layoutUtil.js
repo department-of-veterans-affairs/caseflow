f801a54c41d2c04584f3cea40909b9d972b65b72
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-06-03 19:24:57 -0500

    Clean up components and css

--- client/app/readerprototype/layoutUtil.js
+++ client/app/readerprototype/layoutUtil.js
@@ -1,46 +1,63 @@
 import { css } from 'glamor';
 
-const sideBarSmall = '250px';
-const sideBarLarge = '380px';
-
-export const sidebarWrapper = css({
-  width: '28%',
-  minWidth: sideBarSmall,
-  maxWidth: sideBarLarge,
-  '@media(max-width: 920px)': { width: sideBarSmall },
-  '@media(min-width: 1240px)': { width: sideBarLarge }
+export const pdfDocumentStyles = css({
+  position: 'relative',
+  width: '100%',
+  height: '100%',
 });
 
-const pdfWrapperSmall = 1165;
+export const docViewerStyles = {
+  sidebarContainer: css({
+    width: '30%',
+    top: '0px',
+    bottom: '0px',
+    right: '0px',
+    position: 'absolute',
+  }),
+  documentContainer: css({
+    width: '70%',
+  })
+};
 
-export const pdfToolbarStyles = {
+const docWrapperSmall = 1165;
+
+export const docToolbarStyles = {
   openSidebarMenu: css({ marginRight: '2%' }),
   toolbar: css({ width: '33%' }),
   toolbarLeft: css({
-    '&&': { [`@media(max-width:${pdfWrapperSmall}px)`]: {
+    '&&': { [`@media(max-width:${docWrapperSmall}px)`]: {
       width: '18%' }
     }
   }),
   toolbarCenter: css({
-    '&&': { [`@media(max-width:${pdfWrapperSmall}px)`]: {
+    '&&': { [`@media(max-width:${docWrapperSmall}px)`]: {
       width: '24%' }
     }
   }),
   toolbarRight: css({
+    overflow: 'hidden',
     textAlign: 'right',
-    '&&': { [`@media(max-width:${pdfWrapperSmall}px)`]: {
+    '&&': { [`@media(max-width:${docWrapperSmall}px)`]: {
       width: '44%',
       '& .cf-pdf-button-text': { display: 'none' } }
     }
-  }),
-  footer: css({
+  })
+};
+
+export const docFooterStyles = {
+  container: css({
+    overflow: 'hidden',
     position: 'absolute',
     bottom: 0,
     display: 'flex',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    '&&': { [`@media(max-width:${pdfWrapperSmall}px)`]: {
+    '&&': { [`@media(max-width:${docWrapperSmall}px)`]: {
       '& .left-button-label': { display: 'none' },
       '& .right-button-label': { display: 'none' }
     } }
+  }),
+  pageNumInput: css({
+    display: 'inline-block',
   })
 };
