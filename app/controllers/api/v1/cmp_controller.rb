2f52556fdc1835ac79920f52c2e94c0f193d036e
Matthew Roth <roth_matthew@bah.com>
2024-06-05 16:35:54 -0400

    cmp endpoint to return 501

--- app/controllers/api/v1/cmp_controller.rb
+++ app/controllers/api/v1/cmp_controller.rb
@@ -2,13 +2,7 @@
 
 class Api::V1::CmpController < Api::ApplicationController
   def upload
-    status = if validate_payload?
-               :ok
-             else
-               :bad_request
-             end
-
-    render json: { result: "test" }, status: status
+    endpoint_disabled("Payload is valid: #{validate_payload?}")
   rescue StandardError => error
     raise error
   end
@@ -22,13 +16,13 @@ class Api::V1::CmpController < Api::ApplicationController
   def validate_payload?
     payload = JSON.parse(upload_params[:payload])
 
-    validate_provider_data?(payload[:providerData]) if payload.present?
+    validate_provider_data?(payload['providerData']) if payload.present?
   end
 
   def validate_provider_data?(provider_data)
     return false if provider_data.blank?
 
-    Rails.logger("provider data is #{provider_data}")
+    Rails.logger.info("provider data is #{provider_data}")
     true
   end
 end
