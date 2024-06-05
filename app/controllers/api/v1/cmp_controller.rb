dc35294af42e9595cac554169dc7a0184bea98b7
Matthew Roth <roth_matthew@bah.com>
2024-06-05 12:15:03 -0400

    Update cmp_controller.rb

--- app/controllers/api/v1/cmp_controller.rb
+++ app/controllers/api/v1/cmp_controller.rb
@@ -1,2 +1,34 @@
+# frozen_string_literal: true
+
 class Api::V1::CmpController < Api::ApplicationController
+  def upload
+    status = if validate_payload?
+               :ok
+             else
+               :bad_request
+             end
+
+    render json: { result: "test" }, status: status
+  rescue StandardError => error
+    raise error
+  end
+
+  private
+
+  def upload_params
+    params.permit(:payload, file: [])
+  end
+
+  def validate_payload?
+    payload = JSON.parse(upload_params[:payload])
+
+    validate_provider_data?(payload[:providerData]) if payload.present?
+  end
+
+  def validate_provider_data?(provider_data)
+    return false if provider_data.blank?
+
+    Rails.logger("provider data is #{provider_data}")
+    true
+  end
 end
