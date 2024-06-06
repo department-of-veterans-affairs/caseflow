475a936ce323df9d192a57d6d6895348d5c72536
Dev-KRedd <khalin.redding@va.gov>
2024-06-06 16:53:12 -0400

    AC 5 WIP

--- client/app/test/TestCorrespondence.jsx
+++ client/app/test/TestCorrespondence.jsx
@@ -5,7 +5,8 @@ import StringUtil from '../util/StringUtil';
 import SearchableDropdown from '../components/SearchableDropdown';
 import Button from '../components/Button';
 import TabWindow from '../components/TabWindow';
-import TextField from '../components/TextField';
+import TextareaField from '../components/TextareaField';
+import NumberField from '../components/NumberField';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import NavigationBar from '../components/NavigationBar';
 import AppFrame from '../components/AppFrame';
@@ -13,7 +14,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Alert from '../components/Alert';
 import { trim, escapeRegExp } from 'lodash';
 import { COLORS } from '@department-of-veterans-affairs/caseflow-frontend-toolkit/util/StyleConstants';
-import COPY from "../../COPY.json";
+import COPY from '../../COPY.json';
 
 export default function TestCorrespondence(props) {
 
@@ -34,6 +35,24 @@ export default function TestCorrespondence(props) {
           <h1>{COPY.CORRESPONDENCE_ADMIN.HEADER}</h1>
           <h3>{COPY.CORRESPONDENCE_ADMIN.SUB_HEADER}</h3>
           <p>{COPY.CORRESPONDENCE_ADMIN.DESCRIPTION}</p>
+          <div className="textarea-div-styling-test-correspondence">
+            <TextareaField
+              id= "textarea-styling-test-correspondence"
+              name="Enter up to 10 veteran file numbers separated by a comma."
+            />
+          </div>
+          <p>Enter the number of correspondence to be generated</p>
+          <div>
+            <NumberField
+              className={['number-field-styling-test-correspondence']}
+            />
+          </div>
+          <div>
+            <Button
+              name="Generate correspondence"
+              classNames={['correspondence-intake-appeal-button']}
+            />
+          </div>
         </AppSegment>
       </AppFrame>
     </div>
