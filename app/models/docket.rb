c46e64375bebb437bb0fcce4435adcab2b5ba2ef
SHarshain <133917878+SHarshain@users.noreply.github.com>
2024-06-06 14:37:12 -0400

    APPEALS-45191. Added columns to the appeals ready to distribute query and made some tweaks (#21834)
    * APPEALS-45191. Added columns to the appeals ready to distribute query and made some tweaks
    
    * APPEALS-45191. Refactor and fix lint issues
    
    ---------
    
    Co-authored-by: SHarshain <spoosa@razortalent.com>

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -169,7 +169,9 @@ class Docket
   end
 
   def docket_time_goal
-    @docket_time_goal ||= CaseDistributionLever.public_send("ama_#{docket_type}_docket_time_goals")
+    lever_item = CaseDistributionLever.find_by_item("ama_#{docket_type}_docket_time_goals")
+    lever_value = lever_item ? CaseDistributionLever.public_send("ama_#{docket_type}_docket_time_goals") : nil
+    @docket_time_goal ||= lever_value
   end
 
   def start_distribution_prior_to_goal
