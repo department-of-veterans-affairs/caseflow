e4ce0dcd09547e081e5ab004e9ef7789244a4801
psivakeshva <sivakesh.pachipulusu@va.gov>
2024-06-06 13:50:30 -0500

    #43239 - #AC4 added new correspondence_admin feature toggle

--- app/controllers/test/correspondence_controller.rb
+++ app/controllers/test/correspondence_controller.rb
@@ -3,8 +3,8 @@
 require "rake"
 
 class Test::CorrespondenceController < ApplicationController
-  before_action :verify_access, only: [:index]
-
+  before_action :verify_access
+  before_action :verify_feature_toggle
   def index
     return render_access_error unless access_allowed?
     # More code to come
@@ -14,7 +14,9 @@ class Test::CorrespondenceController < ApplicationController
   private
 
   def verify_access
-    current_user.admin? || current_user.inbound_ops_team_supervisor? || bva?
+    return true if current_user.admin? || current_user.inbound_ops_team_supervisor? || bva?
+
+    redirect_to "/unauthorized"
   end
 
   def bva?
@@ -34,4 +36,13 @@ class Test::CorrespondenceController < ApplicationController
     ).serialize_response)
   end
 
+  def verify_feature_toggle
+    correspondence_queue = FeatureToggle.enabled?(:correspondence_queue)
+    correspondence_admin = FeatureToggle.enabled?(:correspondence_admin)
+    if !correspondence_queue && verify_access
+      redirect_to "/under_construction"
+    elsif !correspondence_queue || !verify_access || correspondence_admin
+      redirect_to "/unauthorized"
+    end
+  end
 end
