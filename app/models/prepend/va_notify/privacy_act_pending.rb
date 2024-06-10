4b83439da72b440616fa51d345db16dbc8f9fdea
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-06-09 23:53:38 -0400

    Add conditional for FOIA and Privacy Act mail
    task to receives 'Privacy Act Pending' notifications

--- app/models/prepend/va_notify/privacy_act_pending.rb
+++ app/models/prepend/va_notify/privacy_act_pending.rb
@@ -15,22 +15,13 @@ module PrivacyActPending
     super_return_value
   end
 
-  # for foia/privacy act mail tasks
-  # original method defined in app/models/mail_task.rb
-  def create_twin_of_type(params)
-    super_return_value = super
-    if params[:type] == "PrivacyActRequestMailTask" || params[:type] == "FoiaRequestMailTask"
-      AppellantNotification.notify_appellant(appeal, Constants.EVENT_TYPE_FILTERS.privacy_act_request_pending)
-    end
-    super_return_value
-  end
-
   # for HearingAdminFoiaPrivacyRequestTask/PrivacyActTask
   # original method defined in app/models/task.rb
   def create_child_task(parent, current_user, params)
     super_return_value = super
-    if (params[:type] == "PrivacyActTask" && params[:assigned_to_type].include?("Organization")) ||
-       (params[:type] == "HearingAdminActionFoiaPrivacyRequestTask" && parent.type == "ScheduleHearingTask")
+    if organization_assigned_privacy_task?(params) ||
+       privacy_act_mail_task?(params) ||
+       valid_hearing_admin_foia_privacy_request?(params)
       AppellantNotification.notify_appellant(parent.appeal,
                                              Constants.EVENT_TYPE_FILTERS.privacy_act_request_pending)
     end
@@ -41,5 +32,19 @@ module PrivacyActPending
     if PRIVACY_ACT_TASKS.include?(type) && !PRIVACY_ACT_TASKS.include?(parent&.type)
       appeal.appeal_state.privacy_act_pending_appeal_state_update_action!
     end
+  end
+
+  private
+
+  def privacy_act_mail_task?(params)
+    params[:type] == "PrivacyActRequestMailTask" || params[:type] == "FoiaRequestMailTask"
+  end
+
+  def organization_assigned_privacy_task?(params)
+    params[:type] == "PrivacyActTask" && params[:assigned_to_type].include?("Organization")
+  end
+
+  def valid_hearing_admin_foia_privacy_request?(params)
+    params[:type] == "HearingAdminActionFoiaPrivacyRequestTask" && parent.type == "ScheduleHearingTask"
   end
 end
