a3d19b4b28a2a8721565e52d7760f6d8c0e1ee57
Dev-KRedd <khalin.redding@va.gov>
2024-06-12 12:51:26 -0400

    WIP generate_correspondence

--- app/controllers/test/correspondence_controller.rb
+++ app/controllers/test/correspondence_controller.rb
@@ -12,13 +12,20 @@ class Test::CorrespondenceController < ApplicationController
   def generate_correspondence
     nums = correspondence_params[:file_numbers].split(",").map(&:strip).reject(&:empty?)
     invalid_nums = invalid_file_numbers(nums)
+    valid_file_nums = valid_file_numbers(nums)
 
-    valid_file_nums = "78907"
+    begin
+      connect_corr_with_vet(valid_file_nums, correspondence_params[:count])
+      return render json: {
+        invalid_file_numbers: invalid_nums,
+        valid_file_nums: valid_file_nums,
+        # correspondence_size: correspondence_size
+      }, status: :created
+      rescue StandardError => error
+      log_error(error)
+    end
+
     # once after generating correspondence for these file numbers we have to send the response
-    render json: {
-      invalid_file_numbers: invalid_nums,
-      valid_file_nums: valid_file_nums
-    }, status: :created
   end
 
   private
@@ -61,28 +68,66 @@ class Test::CorrespondenceController < ApplicationController
   end
 
   def valid_veteran?(file_number)
-    # if Rails.deploy_env?(:uat)
-    # veteran = VeteranFinder.find_best_match(file_number)
 
-    # return veteran&.fetch_bgs_record.present?
+    if Rails.deploy_env?(:uat)
+    veteran = VeteranFinder.find_best_match(file_number)
+    return veteran&.fetch_bgs_record.present?
 
-    # elsif Rails.deploy_env?(:demo)
-    veterans = Veteran.all.map(&:file_number)
+    else
+      veteran = Veteran.find_by(file_number: vet_file_num)
+      return veteran if veteran.present?
+    end
 
-    veterans.any?(file_number.to_s)
-
-    # end
   end
 
   def invalid_file_numbers(file_number_arr)
+
     invalid_file_num = []
 
-    file_number_arr.map do |vet_file_num|
-      if valid_veteran?(vet_file_num) == false
-        invalid_file_num.push(vet_file_num)
+      file_number_arr.map do |vet_file_num|
+        if valid_veteran?(vet_file_num) === false
+          invalid_file_num.push(vet_file_num)
+        end
+      end
+
+    return invalid_file_num
+  end
+
+  def valid_file_numbers(file_number_arr)
+
+    valid_file_num = []
+
+      file_number_arr.map do |vet_file_num|
+        if valid_veteran?(vet_file_num) === true
+          valid_file_num.push(vet_file_num)
+        end
+      end
+
+    return valid_file_num
+  end
+
+  def initial_values
+    @cmp_packet_number = 3000000000
+  end
+
+  def connect_corr_with_vet(valid_veterans, count)
+    count.times do
+      valid_veterans.each do |veterans|
+        Correspondence.create!(
+          uuid: SecureRandom.uuid,
+          portal_entry_date: Time.zone.now,
+          source_type: "Mail",
+          package_document_type_id: rand(1..15),
+          correspondence_type_id: rand(1..8),
+          cmp_queue_id: 1,
+          cmp_packet_number: @cmp_packet_number,
+          va_date_of_receipt: Faker::Date.between(from: 90.days.ago, to: Time.zone.yesterday),
+          notes: "This is a test note",
+          veteran_id: veteran.id
+        )
+        @cmp_packet_number = @cmp_packet_number += 1
       end
     end
-
-    invalid_file_num
   end
+
 end
