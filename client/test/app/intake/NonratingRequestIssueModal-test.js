b5fa316b828e6b561b4735bc4b38778552027f4d
AdamShawBAH <adam.shaw@va.gov>
2024-06-13 14:48:52 -0400

    Enzyme -> RTL refactor (cont)

--- client/test/app/intake/NonratingRequestIssueModal-test.js
+++ client/test/app/intake/NonratingRequestIssueModal-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { logRoles } from '@testing-library/react';
 
 import { VHA_ADMIN_DECISION_DATE_REQUIRED_BANNER } from 'app/../COPY';
 import NonratingRequestIssueModal from '../../../app/intake/components/NonratingRequestIssueModal';
