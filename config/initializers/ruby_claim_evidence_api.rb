d12b2b45307f1ea0ab3eca04cb5073c04cb7e9e7
Alex Smith <alex.smith.99@thunderyard.com>
2024-06-13 09:29:19 -0600

    Add New CE REST API REST Functionality for Vet Docs (#21812)
    * Add New CE REST API REST Functionality for Vet Docs
    
    - Add feature toggle to enable/disable new functionality.
    
    - Call REST endpoint for veteran docs when the use_ce_api feature
    toggle is enabled.
    
    - Add supporting services and specs.
    
    * Fix Misc. Issues
    
    - Update JsonApiResponseAdapter with latest changes to respnse
    parsing found in caseflow-efolder testing.
    
    - Update ruby_claim_evidence_api to bugfix version.
    
    * Update received_at Date Parsing Logic
    
    - Use slashes instead of hyphens to help JS correctly parse the
    date.
    
    * Fix Double Quote Issue
    
    * Improve JsonApiResponseAdapter Blank Response Handling
    
    * Pass Rails Logger to VeteranFileFetcher

---
+++ config/initializers/ruby_claim_evidence_api.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+VeteranFileFetcher = ExternalApi::VeteranFileFetcher
+  .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
