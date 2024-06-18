7a61e22a9539b405f4f42e6a08c61a789b84991b
Calvin <Calvin.Costa@va.gov>
2024-06-18 12:32:38 -0400

    fixed spec error

--- spec/models/vacols/case_docket_spec.rb
+++ spec/models/vacols/case_docket_spec.rb
@@ -846,19 +846,19 @@ describe VACOLS::CaseDocket, :all_dbs do
         IneligibleJudgesJob.new.perform_now
         # {FOR LEVER HAVING A VALUE:}
         CaseDistributionLever.find_by_item(Constants.DISTRIBUTION.cavc_aod_affinity_days).update!(value: 14)
-        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort).to
-        match_array([ca1, ca4, ca10, ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
-         .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
+        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort)
+          .to match_array([ca1, ca4, ca10, ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
+            .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
         # {FOR LEVER BEING INFINITE:}
         CaseDistributionLever.find_by_item(Constants.DISTRIBUTION.cavc_aod_affinity_days).update!(value: "infinite")
-        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort).to
-        match_array([ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
-         .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
+        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort)
+          .to match_array([ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
+            .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
         # {FOR LEVER BEING OMIT:}
         CaseDistributionLever.find_by_item(Constants.DISTRIBUTION.cavc_aod_affinity_days).update!(value: "omit")
-        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort).to
-        match_array([ca1, ca2, ca3, ca4, ca5, ca6, ca10, ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
-         .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
+        expect(VACOLS::CaseDocket.distribute_priority_appeals(judge, "any", 100, true).map { |c| c["bfkey"] }.sort)
+          .to match_array([ca1, ca2, ca3, ca4, ca5, ca6, ca10, ca11, ca12, ca13, ca14, ca15, ca16, ca17, ca21, ca22, ca23, ca24, ca25, ca26, ca27, ca28, ca29, ca30]
+            .map { |c| (c["bfkey"].to_i + 1).to_s }.sort)
       end
     end
   end
