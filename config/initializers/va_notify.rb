f62460dc5ecaae1b0ea6a525cc5318f7edbc4b90
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-06-18 17:17:44 -0400

    Allow use of fake VA Notify service

--- config/initializers/va_notify.rb
+++ config/initializers/va_notify.rb
@@ -1,1 +1,7 @@
-VANotifyService = (ApplicationController.dependencies_faked? ? Fakes::VANotifyService : ExternalApi::VANotifyService)
+# frozen_string_literal: true
+
+VANotifyService = if ApplicationController.dependencies_faked? || ENV["FAKE_VA_NOTIFY_SERVICE"]
+                    Fakes::VANotifyService
+                  else
+                    ExternalApi::VANotifyService
+                  end
