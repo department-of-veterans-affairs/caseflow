8ed1861b1bd0d6a8ceaaad866ef00d233e01cdc8
AdamShawBAH <adam.shaw@va.gov>
2024-06-19 09:36:44 -0400

    AS Enzyme -> RTL refactor (cont)

--- client/test/app/queue/components/PoaRefresh.test.js
+++ client/test/app/queue/components/PoaRefresh.test.js
@@ -1,92 +1,55 @@
-// import React from 'react';
-// import { PoaRefresh } from 'app/queue/components/PoaRefresh';
-// import { mount } from 'enzyme';
-
-// import { queueWrapper } from 'test/data/stores/queueStore';
-
-// describe('PoaRefresh', () => {
-//   const powerOfAttorney = { poa_last_synced_at: '04/08/2021' };
-
-  // const setup = ({ poaToggled }) => {
-  //   return mount(
-  //     <PoaRefresh powerOfAttorney={powerOfAttorney} />,
-  //     {
-  //       wrappingComponent: queueWrapper,
-  //       wrappingComponentProps: {
-  //         ui: {
-  //           featureToggles: {
-  //             poa_last_synced_at: poaToggled
-  //           }
-  //         }
-  //       }
-  //     });
-  // };
-
-  // describe('feature toggles', () => {
-  //   describe('poa_last_synced_at', () => {
-  //     it('hides PoaRefresh text when not toggled', () => {
-  //       const poaText = setup({ poaToggled: false });
-
-  //       expect(poaText).toMatchSnapshot();
-  //     });
-
-  //     it('shows PoaRefresh text when toggled', () => {
-  //       const poaText = setup({ poaToggled: true });
-
-  //       expect(poaText).toMatchSnapshot();
-  //     });
-  //   });
-  // });
-// });
 import React from 'react';
 import { PoaRefresh } from 'app/queue/components/PoaRefresh';
-import { render, screen } from '@testing-library/react';
+import { mount } from 'enzyme';
+import { render as rtlRender, screen } from '@testing-library/react';
 
 import { queueWrapper } from 'test/data/stores/queueStore';
-import { useSelector } from 'react-redux';
 
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(),
-  useDispatch: jest.fn(),
-}));
 
-const Wrapper = ({ children, poaToggled }) => {
-  return queueWrapper({ children, ui: { featureToggles: { poa_last_synced_at: poaToggled } } });
+function customRender(ui, { wrapper: Wrapper, wrapperProps, ...options }) {
+  if (Wrapper) {
+    ui = <Wrapper {...wrapperProps}>{ui}</Wrapper>;
+  }
+  return rtlRender(ui, options);
+}
+
+const Wrapper = ({ children, ...props }) => {
+  console.log('props', props);
+  return queueWrapper({ children, ...props });
 };
-
 
 describe('PoaRefresh', () => {
   const powerOfAttorney = { poa_last_synced_at: '04/08/2021' };
 
   const setup = ({ poaToggled }) => {
-    useSelector.mockImplementation(callback => {
-      if (callback.toString().includes('selectPoaRefreshButton')) {
-        return poaToggled;
-      }
-      // Add other selectors if needed
-      return null;
-    });
-
-    return render(
-      <Wrapper poaToggled={poaToggled}>
-        <PoaRefresh powerOfAttorney={powerOfAttorney} />
-      </Wrapper>
-    );
+    return customRender(
+      <PoaRefresh powerOfAttorney={powerOfAttorney} />,
+      {
+        wrapper: Wrapper,
+        wrapperProps: {
+          ui: {
+            featureToggles: {
+              poa_last_synced_at: poaToggled
+            }
+          }
+        }
+      });
   };
-
 
   describe('feature toggles', () => {
     describe('poa_last_synced_at', () => {
       it('hides PoaRefresh text when not toggled', () => {
-        const poaText = setup({ poaToggled: false });
+        const {asFragment} = setup({ poaToggled: false });
+
         screen.debug();
-        // Add assertions as needed
+
+        // expect(poaText).toMatchSnapshot();
       });
 
-      it('shows PoaRefresh text when toggled', () => {
-        const poaText = setup({ poaToggled: true });
+      it.only('shows PoaRefresh text when toggled', () => {
+        const {asFragment} = setup({ poaToggled: true });
         screen.debug();
-        // Add assertions as needed
+        // expect(poaText).toMatchSnapshot();
       });
     });
   });
