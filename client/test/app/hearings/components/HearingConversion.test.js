e9e59471f6a206536b307a1927654c034c0bf5ae
AdamShawBAH <adam.shaw@va.gov>
2024-06-19 09:36:44 -0400

    Enzyme -> RTL refactor (cont)

--- client/test/app/hearings/components/HearingConversion.test.js
+++ client/test/app/hearings/components/HearingConversion.test.js
@@ -2,26 +2,68 @@ import React from 'react';
 
 import { HearingConversion } from 'app/hearings/components/HearingConversion';
 import { detailsStore, hearingDetailsWrapper } from 'test/data/stores/hearingsStore';
-import { mount } from 'enzyme';
+
+import { render as rtlRender, screen } from '@testing-library/react';
+
 import { userWithJudgeRole, amaHearing, vsoUser, anyUser } from 'test/data';
 import { HEARING_CONVERSION_TYPES } from 'app/hearings/constants';
-import { VirtualHearingSection } from 'app/hearings/components/VirtualHearings/Section';
 import * as DateUtil from 'app/util/DateUtil';
-import { AddressLine } from 'app/hearings/components/details/Address';
-import { HearingEmail } from 'app/hearings/components/details/HearingEmail';
-import { JudgeDropdown } from 'app/components/DataDropdowns';
-import { Timezone } from 'app/hearings/components/VirtualHearings/Timezone';
-import { Checkbox } from '../../../../../client/app/components/Checkbox'
-import RadioField from 'app/components/RadioField';
 import COPY from '../../../../../client/COPY.json'
+import superagent from 'superagent';
 
 const updateSpy = jest.fn();
 const defaultTitle = 'Convert to Virtual';
 const mockUpdateCheckboxes = jest.fn();
+jest.mock('superagent');
+
+const mockJudges = [
+  { id: 'judge1', name: 'Judge Judy' },
+  { id: 'judge2', name: 'Judge Dredd' },
+];
+
+// Setup the mock implementation
+superagent.get.mockImplementation((url) => {
+  const mockResponse = { body: mockJudges };
+  const mockRequest = {
+    set: jest.fn().mockReturnThis(),
+    query: jest.fn().mockReturnThis(),
+    timeout: jest.fn().mockReturnThis(),
+    on: jest.fn().mockReturnThis(),
+    use: jest.fn().mockReturnThis(), // Add the `use` method
+    then: jest.fn((callback) => {
+      callback(mockResponse);
+      return Promise.resolve(mockResponse);
+    }),
+    catch: jest.fn(() => Promise.resolve(mockResponse)),
+  };
+
+  if (url === '/users?role=Judge') {
+    return mockRequest;
+  } else {
+    return Promise.reject(new Error('connect ECONNREFUSED 127.0.0.1:80'));
+  }
+});
+
+
+function customRender(ui, { wrapper: Wrapper, wrapperProps, ...options }) {
+  if (Wrapper) {
+    ui = <Wrapper {...wrapperProps}>{ui}</Wrapper>;
+  }
+  return rtlRender(ui, options);
+}
+
+const Wrapper = ({ children, user, hearing, judge, store }) => {
+  const HearingDetails = hearingDetailsWrapper(user, hearing, judge);
+  return (
+    <HearingDetails store={store}>
+      {children}
+    </HearingDetails>
+  );
+};
 
 describe('HearingConversion', () => {
   test('Matches snapshot with default props', () => {
-    const conversion = mount(
+    const { asFragment } = customRender(
       <HearingConversion
         scheduledFor={amaHearing.scheduledFor.toString()}
         type={HEARING_CONVERSION_TYPES[0]}
@@ -31,46 +73,58 @@ describe('HearingConversion', () => {
         updateCheckboxes= {mockUpdateCheckboxes}
       />,
       {
-        wrappingComponent: hearingDetailsWrapper(
-          userWithJudgeRole,
-          amaHearing,
-          anyUser
-        ),
-        wrappingComponentProps: { store: detailsStore },
+        wrapper: Wrapper,
+        wrapperProps: {
+          store: detailsStore,
+          user: anyUser,
+          hearing: amaHearing,
+          judge: userWithJudgeRole
+        },
       }
     );
 
     // Assertions
-    expect(conversion.find(RadioField)).toHaveLength(0);
-    expect(conversion.find(VirtualHearingSection)).toHaveLength(3);
-    // Check for Instructional Text for Non-VSO User
-    expect(
-      conversion.containsMatchingElement(
-        <span>
-          Email notifications will be sent to the Veteran, POA / Representative, and Veterans Law Judge (VLJ).
-        </span>
-      )
-    ).toBeTruthy();
-    expect(
-      conversion.
-        findWhere((node) => node.prop('label') === 'Hearing Date').
-        prop('text')
-    ).toEqual(DateUtil.formatDateStr(amaHearing.scheduledFor));
-    expect(conversion.find(AddressLine)).toHaveLength(2);
-    expect(conversion.find(Timezone)).toHaveLength(2);
-    expect(conversion.find(HearingEmail)).toHaveLength(2);
-    expect(conversion.find(JudgeDropdown)).toHaveLength(1);
+    // Ensure the radio buttons are hidden
+    const allRadioButtons = screen.queryAllByRole('radio');
+    expect(allRadioButtons).toHaveLength(0);
 
-    expect(
-      conversion.
-        findWhere((node) => node.prop('label') === 'vsoCheckboxes')
-    ).toHaveLength(0);
-    expect(conversion.find(Checkbox)).toHaveLength(0);
-    expect(conversion).toMatchSnapshot();
+    const veteranHeader = screen.getByRole('heading', { name: 'Veteran' });
+    const poaHeader = screen.getByRole('heading', { name: 'Power of Attorney (POA)' });
+    const vljHeader = screen.getByRole('heading', { name: 'Veterans Law Judge (VLJ)' });
+    expect(veteranHeader).toBeInTheDocument();
+    expect(poaHeader).toBeInTheDocument();
+    expect(vljHeader).toBeInTheDocument();
+
+    // // Check for Instructional Text for Non-VSO User
+    expect(screen.getByText('Email notifications will be sent to the Veteran, POA / Representative, and Veterans Law Judge (VLJ).')).toBeInTheDocument();
+
+    const hearingDateElement = screen.getByText(DateUtil.formatDateStr(amaHearing.scheduledFor));
+    expect(hearingDateElement).toBeInTheDocument();
+
+    const AddressLineElementOne = screen.getByText('Bob Smith 9999 MISSION ST SAN FRANCISCO, CA 94103');
+    expect(AddressLineElementOne).toBeInTheDocument();
+    const AddressLineElementTwo = screen.getByText('PARALYZED VETERANS OF AMERICA, INC.');
+    expect(AddressLineElementTwo).toBeInTheDocument();
+
+    const veteranTimezone = screen.getByRole('combobox', { name: 'Veteran Timezone Required' });
+    const representativeTimezone = screen.getByRole('combobox', { name: 'POA/Representative Timezone Required' });
+    expect(veteranTimezone).toBeInTheDocument();
+    expect(representativeTimezone).toBeInTheDocument();
+
+    const emailFields = screen.getAllByRole('textbox');
+    expect(emailFields).toHaveLength(2);
+
+    const vljCombobox = screen.getByRole('combobox', { name: 'VLJ' });
+    expect(vljCombobox).toBeInTheDocument();
+
+    expect(screen.queryAllByLabelText('vsoCheckboxes')).toHaveLength(0);
+    expect(screen.queryAllByRole('checkbox')).toHaveLength(0);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Displays email fields when hearing type is switched from virtual', () => {
-    const conversion = mount(
+    const { asFragment } = customRender(
       <HearingConversion
         scheduledFor={amaHearing.scheduledFor.toString()}
         type={HEARING_CONVERSION_TYPES[1]}
@@ -81,35 +135,43 @@ describe('HearingConversion', () => {
         userVsoEmployee= {false}
       />,
       {
-        wrappingComponent: hearingDetailsWrapper(
-          userWithJudgeRole,
-          amaHearing,
-          anyUser
-        ),
-        wrappingComponentProps: { store: detailsStore },
+        wrapper: Wrapper,
+        wrapperProps: {
+          store: detailsStore,
+          user: anyUser,
+          hearing: amaHearing,
+          judge: userWithJudgeRole
+        },
       }
     );
 
     // Assertions
-    expect(conversion.find(RadioField)).toHaveLength(1);
+    const allRadioButtons = screen.getAllByRole('radio');
+    allRadioButtons.forEach((radioButton) => {
+      expect(radioButton).toBeInTheDocument();
+    });
 
     // Ensure the judge dropdown section is hidden
-    expect(
-      conversion.
-        findWhere((node) => node.prop('label') === 'Veterans Law Judge (VLJ)').
-        prop('hide')
-    ).toEqual(true);
+    const vljElement = screen.queryByText('Veterans Law Judge (VLJ)');
+    expect(vljElement).not.toBeInTheDocument();
 
     // Ensure the emails are displayed but not the judge
-    expect(conversion.find(Timezone)).toHaveLength(2);
-    expect(conversion.find(HearingEmail)).toHaveLength(2);
-    expect(conversion.find(JudgeDropdown)).toHaveLength(0);
+    const veteranTimezone = screen.getByRole('combobox', { name: 'Veteran Timezone Optional' });
+    const representativeTimezone = screen.getByRole('combobox', { name: 'POA/Representative Timezone Required' });
+    expect(veteranTimezone).toBeInTheDocument();
+    expect(representativeTimezone).toBeInTheDocument();
 
-    expect(conversion).toMatchSnapshot();
+    const emailFields = screen.getAllByRole('textbox');
+    expect(emailFields).toHaveLength(2);
+
+    const vljCombobox = screen.queryAllByRole('combobox', { name: 'VLJ' });
+    expect(vljCombobox).toHaveLength(0);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('When a VSO user converts to virtual, the checkboxes and banner appear on the form', () => {
-    const conversion = mount(
+    customRender(
       <HearingConversion
         scheduledFor={amaHearing.scheduledFor.toString()}
         type={HEARING_CONVERSION_TYPES[0]}
@@ -120,27 +182,21 @@ describe('HearingConversion', () => {
         userVsoEmployee
       />,
       {
-        wrappingComponent: hearingDetailsWrapper(
-          amaHearing,
-          vsoUser
-        ),
-        wrappingComponentProps: { store: detailsStore },
-      });
-
-    //  expect checkbox div to show
-    expect(
-      conversion.
-        findWhere((node) => node.prop('label') === 'vsoCheckboxes')
-    ).toHaveLength(1);
+        wrapper: Wrapper,
+        wrapperProps: {
+          store: detailsStore,
+          user: vsoUser,
+          hearing: amaHearing,
+          // judge: userWithJudgeRole
+        },
+      }
+    );
 
     //  expect both checkboxes to show
-    expect(conversion.find(Checkbox)).toHaveLength(2);
+    const vsoCheckboxes = screen.getAllByRole('checkbox');
+    expect(vsoCheckboxes).toHaveLength(2);
 
-    // expect span text to appear
-    expect(
-      conversion.containsMatchingElement(
-        <span>{COPY.CONVERT_HEARING_TYPE_SUBTITLE_3}</span>
-      )
-    ).toBeTruthy();
+    // // expect span text to appear
+    expect(screen.getByText(COPY.CONVERT_HEARING_TYPE_SUBTITLE_3)).toBeInTheDocument();
   });
 });
