f3ab9cadbd0f12682dfac70c4391428e90cd5fc6
= <tyler.broyles@va.gov>
2024-06-20 20:21:18 -0400

    Added code to make the associations for request issues to issue modification requests through the UI work correctly in the request_issues_update model for additions and modifications since it is creating brand new request issues instead of using existing records in the database.

--- app/models/request_issues_update.rb
+++ app/models/request_issues_update.rb
@@ -154,7 +154,16 @@ class RequestIssuesUpdate < CaseflowRecord
     before_issues
 
     @request_issues_data.map do |issue_data|
-      review.find_or_build_request_issue_from_intake_data(issue_data)
+      request_issue = review.find_or_build_request_issue_from_intake_data(issue_data)
+
+      # If the data has a issue modification request id here, then add it in as an association
+      issue_modification_request_id = issue_data[:issue_modification_request_id]
+      if issue_modification_request_id && request_issue
+        issue_modification_request = IssueModificationRequest.find(issue_modification_request_id)
+        request_issue.issue_modification_requests << issue_modification_request
+      end
+
+      request_issue
     end
   end
 
