db38717c2f9f077c30f2b0c672281503678d7e41
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2024-06-25 12:37:00 -0400

    try vendor suggested config

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -19,13 +19,13 @@ if !Rails.env.development? && !Rails.env.test? && !Rails.env.demo?
     config.service_version = "1.0.1"
     # automatic instrumentation
     config.use_all
-    # ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_metadata.properties", "/var/lib/dynatrace/enrichment/dt_host_metadata.properties"].each do |name|
-    #   begin
-    #     config.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
-    #   rescue StandardError => error
-    #     Rails.logger.error("OpenTelemetry config error for #{name}: #{error.full_message}")
-    #   end
-    # end
+    ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_host_metadata.properties"].each do |name|
+      begin
+        config.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
+      rescue StandardError => error
+        Rails.logger.error("OpenTelemetry config error for #{name}: #{error.full_message}")
+      end
+    end
     config.add_span_processor(
       OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
         OpenTelemetry::Exporter::OTLP::Exporter.new(
