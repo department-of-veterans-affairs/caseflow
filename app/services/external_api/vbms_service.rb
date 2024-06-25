e6a06d5197ccc39855a9e3ddcd535294bba1de01
SanthiParakal133 <132940479+SanthiParakal133@users.noreply.github.com>
2024-06-25 11:07:58 -0400

    update result_hash (#22011)

--- app/services/external_api/vbms_service.rb
+++ app/services/external_api/vbms_service.rb
@@ -20,7 +20,12 @@ class ExternalApi::VBMSService
   def self.fetch_documents_for(appeal, _user = nil)
     if FeatureToggle.enabled?(:use_ce_api)
       response = VeteranFileFetcher.fetch_veteran_file_list(veteran_file_number: appeal.veteran_file_number)
-      JsonApiResponseAdapter.new.adapt_fetch_document_series_for(response)
+      documents = JsonApiResponseAdapter.new.adapt_fetch_document_series_for(response)
+      {
+        manifest_vbms_fetched_at: nil,
+        manifest_vva_fetched_at: nil,
+        documents: DocumentsFromVbmsDocuments.new(documents: documents, file_number: appeal.veteran_file_number).call
+      }
     else
       ExternalApi::VbmsDocumentsForAppeal.new(file_number: appeal.veteran_file_number).fetch
     end
