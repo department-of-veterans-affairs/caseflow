1e4daf3e6ffdbacbf12f861c4ac9262e0d6dfd19
AdamShawBAH <adam.shaw@va.gov>
2024-06-26 10:34:10 -0400

    fixed badge test

--- client/app/components/badges/ContestedBadge/ContestedClaimBadge.test.js
+++ client/app/components/badges/ContestedBadge/ContestedClaimBadge.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from 'app/queue/reducers';
 import ContestedClaimBadge from './ContestedClaimBadge';
+import COPY from 'COPY';
 
 describe('ContestedClaimBadge', () => {
   const defaultAppeal = {
-    contested_claim: true,
+    id: '1234',
+    contestedClaim: true
   };
 
   const getStore = () => createStore(rootReducer, applyMiddleware(thunk));
@@ -24,9 +26,15 @@ describe('ContestedClaimBadge', () => {
     );
   };
 
-  it('renders correctly', () => {
+  it('renders correctly', async () => {
     const store = getStore();
     const { asFragment } = render(setupContestedClaimBadge(store));
+
+    waitFor(() => {
+      expect(screen.getByText('CC')).toBeInTheDocument();
+      expect(screen.getByText(COPY.CC_BADGE_TOOLTIP)).toBeInTheDocument();
+    });
+
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- client/app/components/badges/FnodBadge/FnodBadge.test.js
+++ client/app/components/badges/FnodBadge/FnodBadge.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from 'app/queue/reducers';
 import FnodBadge from './FnodBadge';
+import { tooltipListStyling } from 'app/components/badges/style';
+import { DateString } from 'app/util/DateUtil';
+import COPY from 'COPY';
+import Badge from '../Badge';
 
 describe('FnodBadge', () => {
-  const defaultAppeal = {
-    veteranAppellantDeceased: true,
-    veteranDateOfDeath: '2019-03-17'
-  };
+
+  const veteranAppellantDeceased = true;
+  const uniqueId = '1234'
+  const veteranDateOfDeath = '2019-03-17';
+  const tooltipText = <div>
+    <strong>Date of Death Reported</strong>
+    <ul {...tooltipListStyling}>
+      <li><strong>Source:</strong> {COPY.FNOD_SOURCE}</li>
+      {veteranDateOfDeath &&
+        <li><strong>Date of Death:</strong> <DateString date={veteranDateOfDeath} /></li>
+      }
+    </ul>
+  </div>;
 
   const getStore = () => createStore(rootReducer, applyMiddleware(thunk));
 
@@ -19,16 +32,23 @@ describe('FnodBadge', () => {
     return (
       <Provider store={store}>
         <FnodBadge
-          appeal={defaultAppeal}
+          uniqueId={uniqueId}
+          veteranAppellantDeceased={veteranAppellantDeceased}
+          tooltipText={tooltipText}
         />
       </Provider>
     );
   };
 
-  it('renders correctly', () => {
+  it.only('renders correctly', () => {
     const store = getStore();
     const { asFragment } = render(setupFnodBadge(store));
-    console.log(asFragment());
+
+    expect(screen.getByText('FNOD')).toBeInTheDocument();
+    expect(screen.getByText('Date of Death Reported')).toBeInTheDocument()
+    expect(screen.getByText('Source:')).toBeInTheDocument();
+    expect(screen.getByText('Date of Death:')).toBeInTheDocument();
+    expect(screen.getByText('03/17/19')).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 });
