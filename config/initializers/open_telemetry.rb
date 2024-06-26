9f2d3ca54a0a7fb1fa13c7d71c7fd99553d3e831
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2024-06-26 14:07:50 -0400

    otel include dt_host file

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -15,7 +15,7 @@ if !Rails.env.development? && !Rails.env.test? && !Rails.env.demo?
     c.service_name = 'ruby-quickstart'
     c.service_version = '1.0.1'
     c.use_all #application will be using all instrumentation provided by OpenTelemetry
-    %w[dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties /var/lib/dynatrace/enrichment/dt_metadata.properties].each { |name|
+    %w[dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties /var/lib/dynatrace/enrichment/dt_host_metadata.properties].each { |name|
       begin
         c.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
       rescue
