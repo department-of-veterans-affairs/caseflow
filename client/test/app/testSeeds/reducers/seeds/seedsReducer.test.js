7b916d91bdb64eabdc420583e37a664fc4384ab1
SHarshain <spoosa@razortalent.com>
2024-07-05 10:44:15 -0400

    APPEALS-49839. seedReducer test coverage

--- client/test/app/testSeeds/reducers/seeds/seedsReducer.test.js
+++ client/test/app/testSeeds/reducers/seeds/seedsReducer.test.js
@@ -2,18 +2,52 @@
 import seedsReducer from 'app/testSeeds/reducers/seeds/seedsReducer';
 import { ACTIONS } from 'app/testSeeds/reducers/seeds/seedsActionTypes';
 
-describe('Seed reducer', () => {
+describe('seedsReducer function declaration', () => {
 
   let initialState = {
     seeds: [],
     displayBanner: false
   };
 
+  it('should initialize the reducer with initialState', () => {
+    const state = seedsReducer(undefined, {});
+    expect(state).toEqual(initialState);
+  });
+});
+
+describe('Seed reducer', () => {
+
   let seed = {
     "seed_count": 1,
     "days_ago": 12,
     "judge_css_id": "keeling",
     "seed_type": "ama-aod-hearing-seeds"
+  };
+
+  let seed1 = {
+    "seed_count": 1,
+    "days_ago": 12,
+    "judge_css_id": "keeling",
+    "seed_type": "ama-aod-hearing-seeds"
+  };
+
+  let seed2 = {
+    "seed_count": 1,
+    "days_ago": 20,
+    "judge_css_id": "keeling1",
+    "seed_type": "ama-aod-hearing-seeds"
+  };
+
+  let seed3 = {
+    "seed_count": 1,
+    "days_ago": 25,
+    "judge_css_id": "keeling2",
+    "seed_type": "ama-aod-hearing-seeds"
+  };
+
+  let initialState = {
+    seeds: [seed1, seed2, seed3],
+    displayBanner: false
   };
 
   afterEach(() => {
@@ -28,31 +62,8 @@ describe('Seed reducer', () => {
       }
     };
 
-    const expectedState = {
-      ...initialState,
-      seeds: [action.payload.seed]
-    };
-
     const newState = seedsReducer(initialState, action);
-
-    expect(newState).toEqual(expectedState);
-
-    const removeAction = {
-      type: ACTIONS.REMOVE_CUSTOM_SEED,
-      payload: {
-        seed: seed,
-        index: 0
-      }
-    };
-
-    const expectedRemoveState = {
-      ...initialState,
-      seeds: []
-    };
-
-    const newStateFixed = seedsReducer(initialState, removeAction);
-
-    expect(newStateFixed).toEqual(expectedRemoveState);
+    expect(newState.seeds).toContain(seed);
   });
 
   it('Should handle RESET_CUSTOM_SEEDS action', () => {
@@ -68,5 +79,35 @@ describe('Seed reducer', () => {
     const newState = seedsReducer(initialState, action);
 
     expect(newState).toEqual(expectedState);
+  });
+
+  it('Should set displayBanner to true when dispatched SAVE_CUSTOM_SEEDS action', () => {
+    const action = {
+      type: ACTIONS.SAVE_CUSTOM_SEEDS
+    };
+
+    const newState = seedsReducer(initialState, action);
+    expect(newState.displayBanner).toBe(true);
+  });
+
+  it('should not modify the state for unknown action', () => {
+    const action = { type: 'UNKNOWN_ACTION' };
+
+    const newState = seedsReducer(initialState, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  it('should remove a custom seed when dispatche REMOVE_CUSTOM_SEED action', () => {
+    const indexToRemove = 1;
+    const action = {
+      type: ACTIONS.REMOVE_CUSTOM_SEED,
+      payload: {
+        index: indexToRemove
+      }
+    };
+
+    const newState = seedsReducer(initialState, action);
+    expect(newState.seeds).toHaveLength(initialState.seeds.length - 1);
+    expect(newState.seeds).not.toContain(initialState.seeds[indexToRemove]);
   });
 });
