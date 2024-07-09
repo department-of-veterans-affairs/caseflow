ab5aab403707773f4a01a7b799b2d4599f6ef18d
IsaiahBar <isaiah.barnett@va.gov>
2024-07-09 11:04:07 -0400

    call error method in auto assigner file

--- app/services/correspondence_auto_assigner.rb
+++ app/services/correspondence_auto_assigner.rb
@@ -103,6 +103,10 @@ class CorrespondenceAutoAssigner
     @assignable_user_finder ||= AutoAssignableUserFinder.new(current_user)
   end
 
+  def error
+    @error ||= NoAutoAssignableUsersError.new
+  end
+
   def run_verifier
     @run_verifier ||= CorrespondenceAutoAssignRunVerifier.new
   end
