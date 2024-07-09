a821384c8efee8aeb7127d1d5408365b24b64fb5
HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
2024-07-09 13:49:14 -0400

    APPEALS-51209: UAT Test Data Workflow Does Not Create Documents When Creating Correspondences (#22115)
    * added guard clause to response
    
    * added document creation to UAT test correspondence workflow
    
    * remove domplexity note creation

--- app/controllers/test/correspondence_controller.rb
+++ app/controllers/test/correspondence_controller.rb
@@ -95,14 +95,81 @@ class Test::CorrespondenceController < ApplicationController
     count.times do
       valid_veterans.each do |file|
         veteran = Veteran.find_by_file_number(file)
-        Correspondence.create!(
-          uuid: SecureRandom.uuid,
-          correspondence_type_id: rand(1..8),
-          va_date_of_receipt: Faker::Date.between(from: 90.days.ago, to: Time.zone.yesterday),
-          notes: "This is a test note",
-          veteran: veteran
-        )
+        create_correspondence(veteran)
       end
     end
+  end
+
+  # create correspondence for given veteran
+  def create_correspondence(veteran)
+    vet = veteran
+    corr_type = CorrespondenceType.all.sample
+    receipt_date = rand(1.month.ago..1.day.ago)
+    nod = [true, false].sample
+    doc_type = generate_vbms_doc_type(nod)
+
+    correspondence = ::Correspondence.create!(
+      uuid: SecureRandom.uuid,
+      va_date_of_receipt: receipt_date,
+      notes: "This is a test note",
+      veteran_id: vet.id,
+      nod: nod
+    )
+    create_correspondence_document(correspondence, vet, doc_type)
+  end
+
+  def generate_vbms_doc_type(nod)
+    return nod_doc if nod
+
+    non_nod_docs.sample
+  end
+
+  # :reek:UtilityFunction
+  def create_correspondence_document(correspondence, veteran, doc_type)
+    CorrespondenceDocument.find_or_create_by!(
+      document_file_number: veteran.file_number,
+      uuid: SecureRandom.uuid,
+      vbms_document_type_id: doc_type[:id],
+      document_type: doc_type[:id],
+      pages: rand(1..30),
+      correspondence_id: correspondence.id
+    )
+  end
+
+  def nod_doc
+    {
+      id: 1250,
+      description: "VA Form 10182, Decision Review Request: Board Appeal (Notice of Disagreement)"
+    }
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def non_nod_docs
+    [
+      {
+        id: 1419,
+        description: "Reissuance Beneficiary Notification Letter"
+      },
+      {
+        id: 1430,
+        description: "Bank Letter Beneficiary"
+      },
+      {
+        id: 1448,
+        description: "VR-69 Chapter 36 Decision Letter"
+      },
+      {
+        id: 1452,
+        description: "Apportionment - notice to claimant"
+      },
+      {
+        id: 1505,
+        description: "Higher-Level Review (HLR) Not Timely Letter"
+      },
+      {
+        id: 1578,
+        description: "Pension End of Day Letter"
+      }
+    ]
   end
 end
