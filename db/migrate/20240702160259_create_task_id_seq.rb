2f8ce7e44185b2ff6232aca925c553029dd99b1c
breedbah <brandon.reed3@va.gov>
2024-07-09 14:11:49 -0400

    APPEALS-49239 add migrations and trigger

--- db/migrate/20240702160259_create_task_id_seq.rb
+++ db/migrate/20240702160259_create_task_id_seq.rb
@@ -1,16 +1,19 @@
 class CreateTaskIdSeq < ActiveRecord::Migration[6.0]
   def up
-    execute <<-SQL
-      CREATE SEQUENCE task_id_seq
-      START WITH 1
-      INCREMENT BY 1
-      NOCACHE;
-    SQL
+    safety_assured {
+      execute <<-SQL
+        CREATE SEQUENCE task_id_seq
+        START WITH 1
+        INCREMENT BY 1
+      SQL
+    }
   end
 
   def down
-    execute <<-SQL
-      DROP SEQUENCE task_id_seq;
-    SQL
+    safety_assured {
+      execute <<-SQL
+        DROP SEQUENCE task_id_seq;
+      SQL
+    }
   end
 end
