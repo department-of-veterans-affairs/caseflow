368b531046ca3016984c6aa835633f22f9e8024c
kristeja <112115264+kristeja@users.noreply.github.com>
2024-07-10 10:39:55 -0700

    APPEALS-50756: Fix for Appeals Ready to Distribute Query includes dates that are NA (#22090)
    * APPEALS-50756 fix appeals ready for distribution csv report to exclude place holder dates for non priority appeals
    
    * fix linting issues of feature branch
    
    * fix rubocop offenses from feature branch
    
    * APPEALS-50756 updated login in ama rows method
    
    * APPEALS-50756 fix lint issues

--- spec/jobs/push_priority_appeals_to_judges_job_spec.rb
+++ spec/jobs/push_priority_appeals_to_judges_job_spec.rb
@@ -373,9 +373,9 @@ describe PushPriorityAppealsToJudgesJob, :all_dbs do
         appeal = create(:appeal,
                         :advanced_on_docket_due_to_age,
                         :ready_for_distribution,
-                        docket_type: Constants.AMA_DOCKETS.hearing,
                         :with_appeal_affinity,
                         affinity_start_date: i.months.ago,
+                        docket_type: Constants.AMA_DOCKETS.hearing,
                         receipt_date: 1.month.ago)
         appeal.tasks.find_by(type: DistributionTask.name).update(assigned_at: i.months.ago)
         appeal.reload
