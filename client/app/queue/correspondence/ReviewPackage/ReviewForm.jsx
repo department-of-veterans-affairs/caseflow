52b0ba06931b4a604995d7b754f62b7db18b467a
Rnmarshall93 <110805785+Rnmarshall93@users.noreply.github.com>
2024-07-11 05:52:10 -0700

    Refactor Review Package Frontend (#22073)
    * Call the serialized correspondence in Review Package
    
    * Added correspondenceTypes to review_package controller method
    
    * Added has_efolder_failed_task private method to review_package controller method response
    
    * Modified the efolder_upload_failed_before method in backend and frontend as per the comments
    
    * Replacing with serialized methods
    
    * safespot for rollback
    
    * reviewform appearing stable and not regressing
    
    * stable rework point
    
    * Fixed the banner alert when hasEfolderFailedTask is true
    
    * remaning banner updates to the useEffect function
    
    * Modified the change to display correspondenceDocuments array inside the hash of correspondence
    
    * Replaced apiresponse with props
    
    * Adjusted the display intake button to enabled after save changes
    
    * Removed show method
    
    * Commit changes for isEditableDataChanged with prop values
    
    * fixed issue with page load due to out of bounds array accessing
    
    * potential fix for form fields
    
    * Can able to edit notes
    
    * Removed correspondenceDocuments props and utilize the props from correspondence props
    
    * Modified the reviewPackageReducer.js
    
    * fixed params issue. removed unused method. fixed intake appeal logic.
    
    * fixed params issue. removed unused method. fixed intake appeal logic.
    
    * changes variable name from NEW_DATA_CHANGEME to reviewPackageData
    
    * Intake Appeal Button enabled fix
    
    * Fix for Document Type edit form
    
    * Fix for PDF reader when we click on edit link
    
    * fixed params issue
    
    * Removed puts
    
    * Spec fixes for review package controller
    
    * Rubocop fixes
    
    * fixed linting issues
    
    * Fix for form updates for review package
    
    * Changed disable flag.
    
    * fixed intake button logic
    
    * Updated the veteran file number fix
    
    * Modified to previous change for veteran file number fix
    
    * Rubocop fixes
    
    * Lint fix
    
    * Jest fixes
    
    * APPEALS-47125: Refactor frontend (#22102)
    
    * refactored review package to not use editable data
    
    * removed reviewDetails and refactored save button state management
    
    * fixed linting errors in routes page
    
    * refactored review form saving and error handling
    
    * updated efolder check to display review package actions if there is no efolder failed task
    
    * updated props for new props route
    
    * fixed bug where package action wouldn't be created if a review package had closed child tasks
    
    * added safe params
    
    * removed requirement for notes to be filled out in form
    
    * removed unused routes
    
    * resolved linting errors in correspondence test controller
    
    * removed outdated tests
    
    * fixed bug where document type wasn't updating NOD (redux action was disconnected)
    
    * refactored intake button to only show if NOD and the page isn't Read Only
    
    * removed unused document action
    
    * fixed failing lint issues and updated rspec
    
    * Jest tests fixed and working again.
    
    ---------
    
    Co-authored-by: divyadasari-va <divya.dasari@va.gov>
    Co-authored-by: divyadasari-va <135847343+divyadasari-va@users.noreply.github.com>
    Co-authored-by: HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
    Co-authored-by: HunJerBAH <Jeremy.Hunton@va.gov>

--- client/app/queue/correspondence/ReviewPackage/ReviewForm.jsx
+++ client/app/queue/correspondence/ReviewPackage/ReviewForm.jsx
@@ -1,7 +1,7 @@
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import React, { useState, useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import TextField from '../../../components/TextField';
 import SearchableDropdown from '../../../components/SearchableDropdown';
 import TextareaField from '../../../components/TextareaField';
@@ -12,62 +12,43 @@ import Modal from '../../../components/Modal';
 import DateSelector from '../../../components/DateSelector';
 import { updateCmpInformation, setCreateRecordIsReadOnly } from '../correspondenceReducer/reviewPackageActions';
 import { validateDateNotInFuture } from '../../../intake/util/issues';
-import moment from 'moment';
 
 export const ReviewForm = (props) => {
-  const correspondenceTypes = props.veteranInformation.correspondence_types;
-  // eslint-disable-next-line max-len
-  const [correspondenceTypeID, setCorrespondenceTypeID] = useState(props.correspondence.correspondence_type_id);
-  // eslint-disable-next-line max-len
-  const [vaDORDate, setVADORDate] = useState(moment.utc((props.correspondence.va_date_of_receipt)).format('YYYY-MM-DD'));
+  const { correspondenceTypeId, notes, vaDor, veteranFileNumber } = props;
+
+  const correspondenceTypes = props.correspondenceTypes;
   const [dateError, setDateError] = useState(false);
-  const [returnValue, setReturnValue] = useState(false);
-  const stateCorrespondence = useSelector(
-    (state) => state.reviewPackage.correspondence
-  );
 
   const handleCorrespondenceTypeEmpty = () => {
-    if (correspondenceTypeID < 0) {
+    if (correspondenceTypeId === null) {
       return 'Select...';
     }
 
-    return correspondenceTypes[correspondenceTypeID].name;
+    const type = correspondenceTypes.find((value) => value.id === correspondenceTypeId);
+
+    return type.name;
   };
 
-  const saveButtonDisabled = () => {
-    return returnValue;
-  };
-
-  const returnValueToUpdate = () => {
-    return Boolean(dateError);
+  // enable save button and enable return to queue
+  const enableSaveButton = () => {
+    props.setDisableSaveButton(false);
+    props.setIsReturnToQueue(true);
   };
 
   const handleFileNumber = (value) => {
-    setReturnValue(returnValueToUpdate());
+    // use reg expression to check the value only contains 9 numbers
+    const isNumeric = ((/^\d{0,9}$/).test(value));
 
-    props.setIsReturnToQueue(true);
-    const isNumeric = value === '' || (/^\d{0,9}$/).test(value);
-
+    // only attempt to update value if valid file number
     if (isNumeric) {
-      const updatedReviewDetails = {
-        ...props.editableData,
-        veteran_file_number: value,
-      };
-
-      props.setEditableData(updatedReviewDetails);
+      props.setVeteranFileNumber(value);
+      enableSaveButton();
     }
   };
 
   const handleChangeNotes = (value) => {
-    setReturnValue(returnValueToUpdate());
-
-    props.setIsReturnToQueue(true);
-    const updatedNotes = {
-      ...props.editableData,
-      notes: value,
-    };
-
-    props.setEditableData(updatedNotes);
+    props.setNotes(value);
+    enableSaveButton();
   };
 
   const generateOptions = (options) =>
@@ -77,36 +58,11 @@ export const ReviewForm = (props) => {
       id: option.id,
     }));
 
-  const fullName = (vetaranName) => {
-    const {
-      first_name: firstName = '',
-      middle_initial: middleInitial = '',
-      last_name: lastName = '',
-    } = vetaranName;
-
-    return `${firstName} ${middleInitial} ${lastName}`;
-  };
-
   const handleSelectCorrespondenceType = (val) => {
-    setReturnValue(returnValueToUpdate());
-    props.setIsReturnToQueue(true);
-    setCorrespondenceTypeID(val.id);
-    const updatedSelectedValue = {
-      ...props.editableData,
-      default_select_value: val.id,
-    };
-
-    props.setCreateRecordIsReadOnly(handleCorrespondenceTypeEmpty());
-    props.setEditableData(updatedSelectedValue);
-  };
-
-  const errorOnVADORDate = (val) => {
-
-    if (val.length === 10) {
-      const error = validateDateNotInFuture(val) ? null : 'Receipt date cannot be in the future';
-
-      return error;
-    }
+    // update the correspondence type id and update the correspondence type
+    // in the dropdown with placeholder
+    props.setCorrespondenceTypeId(val.id);
+    enableSaveButton();
   };
 
   const vaDORReadOnly = () => {
@@ -119,35 +75,33 @@ export const ReviewForm = (props) => {
   };
 
   const handleSelectVADOR = (val) => {
-    const errorOutput = errorOnVADORDate(val);
-    const returnVal = errorOutput !== null;
+    // check for future issue
+    const error = validateDateNotInFuture(val) ? false : 'Receipt date cannot be in the future';
 
-    setDateError(errorOutput);
-    setReturnValue(returnVal);
+    props.setVaDor(val);
+    setDateError(error);
 
-    setVADORDate(val);
-    const updatedSelectedDate = {
-      ...props.editableData,
-      va_date_of_receipt: val,
-    };
-
-    props.setEditableData(updatedSelectedDate);
+    // if no errors, enable the save button
+    if (!error) {
+      enableSaveButton();
+    }
   };
 
   const handleSubmit = async () => {
-    setReturnValue(returnValueToUpdate());
+    // disable the save button on submit
+    props.setDisableSaveButton(true);
 
     props.setCreateRecordIsReadOnly('');
     const correspondence = props;
     const payloadData = {
       data: {
         correspondence: {
-          notes: props.editableData.notes,
-          correspondence_type_id: correspondenceTypeID,
-          va_date_of_receipt: vaDORDate
+          notes,
+          correspondence_type_id: correspondenceTypeId,
+          va_date_of_receipt: vaDor
         },
         veteran: {
-          file_number: props.editableData.veteran_file_number,
+          file_number: veteranFileNumber
         }
       },
     };
@@ -160,10 +114,8 @@ export const ReviewForm = (props) => {
 
       const { body } = response;
 
-      props.setDisableButton((current) => !current);
       props.setIsReturnToQueue(false);
       if (body.status === 'ok') {
-        props.fetchData();
         props.setErrorMessage('');
       }
     } catch (error) {
@@ -173,27 +125,21 @@ export const ReviewForm = (props) => {
     }
   };
 
-  // Prevents save action in case of errorMessage
+  // enable save button if changes happen to form (with no errors)
   useEffect(() => {
 
-    if (props.errorMessage) {
-      return;
+    // error validation
+    if (dateError || props.errorMessage) {
+      props.setDisableSaveButton(true);
     }
-    setReturnValue(true);
-  }, []);
-
-  // disable the create record button if the save button is active
-  // or there is no correspondence type
-  useEffect(() => {
-    props.setCorrTypeSelected(!saveButtonDisabled() || correspondenceTypeID === null);
-  }, [saveButtonDisabled()]);
+  }, [handleChangeNotes, handleFileNumber, handleSelectCorrespondenceType, handleSelectVADOR]);
 
   const veteranFileNumStyle = () => {
     if (props.errorMessage) {
       return <div className="error-veternal-file-styling-review-form">
         <TextField
           label="Veteran file number"
-          value={props.editableData.veteran_file_number}
+          value={veteranFileNumber}
           onChange={handleFileNumber}
           name="veteran-file-number-input"
           useAriaLabel
@@ -206,7 +152,7 @@ export const ReviewForm = (props) => {
     return <div className="veternal-file-styling-review-form">
       <TextField
         label="Veteran file number"
-        value={props.editableData.veteran_file_number}
+        value={veteranFileNumber}
         onChange={handleFileNumber}
         name="veteran-file-number-input"
         useAriaLabel
@@ -214,7 +160,6 @@ export const ReviewForm = (props) => {
         readOnly={props.isReadOnly}
       />
     </div>;
-
   };
 
   const vaDORReadOnlyStyling = () => {
@@ -226,7 +171,7 @@ export const ReviewForm = (props) => {
         name="date"
         type="date"
         onChange={handleSelectVADOR}
-        value={vaDORDate}
+        value={vaDor}
         errorMessage={dateError}
         readOnly = {vaDORReadOnly() || props.isReadOnly}
       />;
@@ -239,7 +184,7 @@ export const ReviewForm = (props) => {
       name="date"
       type="date"
       onChange={handleSelectVADOR}
-      value={vaDORDate}
+      value={vaDor}
       errorMessage={dateError}
       readOnly = {vaDORReadOnly() || props.isReadOnly}
     />;
@@ -253,7 +198,7 @@ export const ReviewForm = (props) => {
           name="Save changes"
           href="/queue/correspondence/12/intake"
           classNames={['usa-button-primary']}
-          disabled={saveButtonDisabled()}
+          disabled={props.disableSaveButton}
           onClick={handleSubmit}
         />
       </div>
@@ -265,7 +210,7 @@ export const ReviewForm = (props) => {
               <div className="veternal-name-styling-review-form ">
                 <TextField
                   label="Veteran name"
-                  value={fullName(props.reviewDetails.veteran_name)}
+                  value={props.correspondence.veteranFullName}
                   readOnly
                   name="Veteran-name-display"
                   useAriaLabel
@@ -276,7 +221,7 @@ export const ReviewForm = (props) => {
               <TextField
                 name="correspondence-package-document-type"
                 label="Package document type"
-                value = {stateCorrespondence?.nod ? 'NOD' : 'Non-NOD'}
+                value = {props.correspondence?.nod ? 'NOD' : 'Non-NOD'}
                 readOnly
               />
             </div>
@@ -291,7 +236,7 @@ export const ReviewForm = (props) => {
               <TextareaField
                 id= "textarea-styling-review-form"
                 name="Notes"
-                value={props.editableData.notes}
+                value={notes}
                 onChange={handleChangeNotes}
                 disabled={props.isReadOnly}
               />
@@ -300,11 +245,10 @@ export const ReviewForm = (props) => {
               <SearchableDropdown
                 name="correspondence-dropdown"
                 label="Correspondence type"
-                options={generateOptions(props.reviewDetails.dropdown_values)}
+                options={generateOptions(props.correspondenceTypes)}
                 onChange={handleSelectCorrespondenceType}
                 readOnly={props.isReadOnly}
-                placeholder= {correspondenceTypeID < 0 ? 'Select...' :
-                correspondenceTypes[correspondenceTypeID - 1]?.name}
+                placeholder={handleCorrespondenceTypeEmpty()}
               />
             </div>
           </div>
@@ -335,45 +279,36 @@ export const ReviewForm = (props) => {
 };
 
 ReviewForm.propTypes = {
-  reviewDetails: PropTypes.shape({
-    veteran_name: PropTypes.shape({
-      first_name: PropTypes.string,
-      middle_initial: PropTypes.string,
-      last_name: PropTypes.string,
-    }),
-    dropdown_values: PropTypes.array,
-    correspondence_type_id: PropTypes.number
-  }),
-  editableData: PropTypes.shape({
-    notes: PropTypes.string,
-    veteran_file_number: PropTypes.string,
-    default_select_value: PropTypes.number,
-  }),
   veteranInformation: PropTypes.shape({
-    correspondence_types: PropTypes.array,
+    correspondenceTypes: PropTypes.array,
   }),
-  disableButton: PropTypes.bool,
+  correspondenceTypeId: PropTypes.number,
+  setCorrespondenceTypeId: PropTypes.func,
+  notes: PropTypes.string,
+  vaDor: PropTypes.string,
+  veteranFileNumber: PropTypes.string,
+  disableSaveButton: PropTypes.bool,
+  setDisableSaveButton: PropTypes.func,
   setIsReturnToQueue: PropTypes.bool,
-  setEditableData: PropTypes.func,
   setCreateRecordIsReadOnly: PropTypes.func,
-  setDisableButton: PropTypes.func,
   setErrorMessage: PropTypes.func,
-  fetchData: PropTypes.func,
+  setVeteranFileNumber: PropTypes.func,
+  setNotes: PropTypes.func,
+  setVaDor: PropTypes.func,
   showModal: PropTypes.bool,
   handleModalClose: PropTypes.func,
-  correspondenceDocuments: PropTypes.array,
   handleReview: PropTypes.func,
   errorMessage: PropTypes.any,
   isReadOnly: PropTypes.bool,
   isInboundOpsSuperuser: PropTypes.bool,
   userIsInboundOpsSupervisor: PropTypes.bool,
   correspondence: PropTypes.object,
-  setCorrTypeSelected: PropTypes.bool
+  setCorrTypeSelected: PropTypes.bool,
+  correspondenceTypes: PropTypes.array
 };
 
 const mapStateToProps = (state) => ({
   correspondence: state.reviewPackage.correspondence,
-  correspondenceDocuments: state.reviewPackage.correspondenceDocuments,
   packageDocumentType: state.reviewPackage.packageDocumentType,
   veteranInformation: state.reviewPackage.veteranInformation,
 });
