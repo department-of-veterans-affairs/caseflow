5d57343002cb9f1995e98254399d854b36df3f73
breedbah <brandon.reed3@va.gov>
2024-07-11 19:58:06 -0400

    APPEALS-49239 remove migrations

--- db/migrate/20240711165638_add_task_id_to_transcriptions.rb
+++
@@ -1,9 +0,0 @@
-class AddTaskIdToTranscriptions < ActiveRecord::Migration[6.0]
-  def up
-    add_column :transcriptions, :task_id, :bigint
-  end
-
-  def down
-    remove_column :transcriptions, :task_id
-  end
-end
