cab383eda09dc9242a03df54c64dba3a29b5039c
breedbah <brandon.reed3@va.gov>
2024-07-12 08:31:25 -0400

    APPEALS-49239 run migrations individually successful

--- db/migrate/20240711132549_create_task_id_seq.rb
+++ db/migrate/20240711132549_create_task_id_seq.rb
@@ -7,6 +7,11 @@ class CreateTaskIdSeq < ActiveRecord::Migration[6.0]
         INCREMENT BY 1
       SQL
     }
+    if sequence_exists?('task_id_seq')
+      puts "Sequence task_id_seq created successfully"
+    else
+      puts "Failed to create sequence task_id_seq"
+    end
   end
 
   def down
@@ -15,5 +20,29 @@ class CreateTaskIdSeq < ActiveRecord::Migration[6.0]
         DROP SEQUENCE IF EXISTS task_id_seq;
       SQL
     }
+    unless sequence_exists?('task_id_seq')
+      puts "Sequence task_id_seq dropped successfully"
+    else
+      puts "Failed to drop sequence task_id_seq"
+    end
+  end
+
+  private
+
+  def sequence_exists?(sequence_name)
+    result = ActiveRecord::Base.connection.execute("SELECT * FROM information_schema.sequences WHERE sequence_name = '#{sequence_name}'")
+    result.ntuples > 0
   end
 end
+
+
+# schemas = ["pg_toast", "pg_catalog", "public", "information_schema", "caseflow_audit"]
+
+# schemas.each do |schema|
+#   result = ActiveRecord::Base.connection.execute("SELECT * FROM information_schema.sequences WHERE sequence_schema = '#{schema}' AND sequence_name = 'task_id_seq'")
+#   if result.ntuples > 0
+#     puts "Sequence task_id_seq found in schema #{schema}"
+#   else
+#     puts "Sequence task_id_seq not found in schema #{schema}"
+#   end
+# end
