ef9baf716be4744828c489c31a9bd154cd8d733b
breedbah <brandon.reed3@va.gov>
2024-07-11 20:21:53 -0400

    APPEALS-49239 first tests pass

--- spec/migrations/hearings/add_task_id_to_transcriptions_spec.rb
+++ spec/migrations/hearings/add_task_id_to_transcriptions_spec.rb
@@ -1,33 +1,33 @@
 # require 'test_helper'
-# require '20240711165638_add_task_id_to_transcriptions'
+require Rails.root.join('db', 'migrate', '20240712001117_add_task_id_to_transcriptions')
 
-# class AddTaskIdToTranscriptionsTest < ActiveSupport::TestCase
-#   # Setup the migration instance
-#   def setup
-#     @migration = AddTaskIdToTranscriptions.new
-#   end
+class AddTaskIdToTranscriptionsTest < ActiveSupport::TestCase
+  # Setup the migration instance
+  def setup
+    @migration = AddTaskIdToTranscriptions.new
+  end
 
-#   # Test the up migration
-#   test "add task_id column" do
-#     # Run the up migration
-#     @migration.up
+  # Test the up migration
+  test "add task_id column" do
+    # Run the up migration
+    @migration.up
 
-#     # Check that the column exists
-#     assert_column_exists :transcriptions, :task_id
-#   end
+    # Check that the column exists
+    assert_column_exists :transcriptions, :task_id
+  end
 
-#   # Test the down migration
-#   test "remove task_id column" do
-#     # Run the up migration first to ensure the column exists
-#     @migration.up
+  # Test the down migration
+  test "remove task_id column" do
+    # Run the up migration first to ensure the column exists
+    @migration.up
 
-#     # Then run the down migration
-#     @migration.down
+    # Then run the down migration
+    @migration.down
 
-#     # Check that the column no longer exists
-#     assert_no_column_exists :transcriptions, :task_id
-#   end
-# end
+    # Check that the column no longer exists
+    assert_no_column_exists :transcriptions, :task_id
+  end
+end
 
 # 20220711165637_create_task_id_seq.rb
 
