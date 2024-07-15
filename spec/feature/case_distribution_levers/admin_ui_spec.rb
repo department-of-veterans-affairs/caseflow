77318d0e8000861d4f4e7f6202f5a61bf960c3d5
Craig Reese <Reese_Craig@bah.com>
2024-07-15 16:34:58 -0500

    refactor

--- spec/feature/case_distribution_levers/admin_ui_spec.rb
+++ spec/feature/case_distribution_levers/admin_ui_spec.rb
@@ -80,19 +80,9 @@ RSpec.feature "Admin UI" do
     it "the lever control page renders correctly and has no options to change/save values", :aggregate_failures do
       step "page renders" do
         visit "case-distribution-controls"
-        confirm_page_and_section_loaded
+        expect(page).to have_content("Case Distribution Algorithm Values")
         expect(page).not_to have_button("Cancel")
         expect(page).not_to have_button("Save")
-
-        disabled_lever_list.each do |item|
-          expect(find("#lever-wrapper-#{item}")).to match_css(".lever-disabled")
-          expect(find("#affinity-day-label-for-#{item}")).to match_css(".lever-disabled")
-        end
-
-        enabled_lever_list.each do |item|
-          expect(find("#lever-wrapper-#{item}")).not_to match_css(".lever-disabled")
-          expect(find("#affinity-day-label-for-#{item}")).not_to match_css(".lever-disabled")
-        end
 
         expect(find("##{ama_hearings}-lever-value > span")["data-disabled-in-ui"]).to eq("false")
         expect(find("##{ama_direct_reviews}-lever-value > span")["data-disabled-in-ui"]).to eq("false")
@@ -136,7 +126,7 @@ RSpec.feature "Admin UI" do
 
     it "the lever control page operates correctly" do
       visit "case-distribution-controls"
-      confirm_page_and_section_loaded
+      expect(page).to have_content("Case Distribution Algorithm Values")
 
       empty_error_message = "Please enter a value greater than or equal to 0"
 
@@ -170,27 +160,10 @@ RSpec.feature "Admin UI" do
 
       step "inactive levers are displayed with values" do
         # From inactive_data_elements_levers_spec.rb
-        maximum_direct_review_proportion_lever = CaseDistributionLever.find_by_item(maximum_direct_review_proportion)
-        minimum_legacy_proportion_lever = CaseDistributionLever.find_by_item(minimum_legacy_proportion)
-        nod_adjustment_lever = CaseDistributionLever.find_by_item(nod_adjustment)
-        bust_backlog_lever = CaseDistributionLever.find_by_item(bust_backlog)
-
-        converted_maximum_direct_review_proportion_value =
-          (maximum_direct_review_proportion_lever.value.to_f * 100).to_i.to_s
-        converted_minimum_legacy_proportion_value = (minimum_legacy_proportion_lever.value.to_f * 100).to_i.to_s
-        converted_nod_adjustment_value = (nod_adjustment_lever.value.to_f * 100).to_i.to_s
-        bust_backlog_value = bust_backlog_lever.value.humanize
-
-        expect(find("##{maximum_direct_review_proportion}-value")).to have_content(
-          converted_maximum_direct_review_proportion_value + maximum_direct_review_proportion_lever.unit
-        )
-        expect(find("##{minimum_legacy_proportion}-value")).to have_content(
-          converted_minimum_legacy_proportion_value + minimum_legacy_proportion_lever.unit
-        )
-        expect(find("##{nod_adjustment}-value")).to have_content(
-          converted_nod_adjustment_value + nod_adjustment_lever.unit
-        )
-        expect(find("##{bust_backlog}-value")).to have_content(bust_backlog_value + bust_backlog_lever.unit)
+        expect(find("##{maximum_direct_review_proportion}-value")).to have_content("7%")
+        expect(find("##{minimum_legacy_proportion}-value")).to have_content("90%")
+        expect(find("##{nod_adjustment}-value")).to have_content("40%")
+        expect(find("##{bust_backlog}-value")).to have_content("True")
       end
 
       step "cancelling changes resets values" do
@@ -290,51 +263,15 @@ RSpec.feature "Admin UI" do
         click_save_button
         click_modal_confirm_button
 
+        # Refresh page to validate back end saved the levers correctly
+        visit "case-distribution-controls"
+
         expect(find("#lever-history-table").has_content?("123 days")).to eq(true)
         expect(find("#lever-history-table").has_content?("456 days")).to eq(true)
         expect(find("#lever-history-table").has_content?("300 days")).to eq(true)
       end
     end
   end
-
-  # rubocop:disable Metrics/AbcSize
-  def confirm_page_and_section_loaded
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_TITLE)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_AFFINITY_DAYS_H2_TITLE)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_DOCKET_TIME_GOALS_SECTION_TITLE)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_BATCH_SIZE_H2_TITLE)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_HISTORY_TITLE)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_HISTORY_DESCRIPTION)
-    expect(page).to have_content(COPY::CASE_DISTRIBUTION_STATIC_LEVERS_TITLE)
-    expect(page).to have_content(Constants.DISTRIBUTION.ama_hearing_case_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.ama_hearing_case_aod_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.cavc_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.cavc_aod_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.aoj_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.aoj_aod_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.aoj_cavc_affinity_days_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.ama_hearings_section_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.ama_direct_review_section_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.ama_evidence_submission_section_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.alternative_batch_size_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.batch_size_per_attorney_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.request_more_cases_minimum_title)
-
-    # From inactive_data_elements_levers_spec.rb
-    expect(page).to have_content(Constants.DISTRIBUTION.maximum_direct_review_proportion_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.minimum_legacy_proportion_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.nod_adjustment_title)
-    expect(page).to have_content(Constants.DISTRIBUTION.bust_backlog_title)
-    expect(find("##{maximum_direct_review_proportion}-description")).to match_css(".description-styling")
-    expect(find("##{maximum_direct_review_proportion}-product")).to match_css(".value-styling")
-    expect(find("##{minimum_legacy_proportion}-description")).to match_css(".description-styling")
-    expect(find("##{minimum_legacy_proportion}-product")).to match_css(".value-styling")
-    expect(find("##{nod_adjustment}-description")).to match_css(".description-styling")
-    expect(find("##{nod_adjustment}-product")).to match_css(".value-styling")
-    expect(find("##{bust_backlog}-description")).to match_css(".description-styling")
-    expect(find("##{bust_backlog}-product")).to match_css(".value-styling")
-  end
-  # rubocop:enable Metrics/AbcSize
 
   def click_save_button
     find("#LeversSaveButton").click
