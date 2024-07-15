87891213080588a4a44aaa22a6bdd5578d8be0e8
isaiahsaucedo <irsaucedo5@gmail.com>
2024-07-15 09:54:47 -0500

    uncomment call to process legacy appeals

--- app/jobs/update_appeal_affinity_dates_job.rb
+++ app/jobs/update_appeal_affinity_dates_job.rb
@@ -39,8 +39,7 @@ class UpdateAppealAffinityDatesJob < CaseflowJob
     return if receipt_date_hashes_array.empty?
 
     process_ama_appeals_which_need_affinity_updates(receipt_date_hashes_array)
-    # Uncomment this while implementing legacy appeal affinities
-    # process_legacy_appeals_which_need_affinity_updates(receipt_date_hashes_array)
+    process_legacy_appeals_which_need_affinity_updates(receipt_date_hashes_array)
   end
 
   def latest_receipt_dates_from_distribution
