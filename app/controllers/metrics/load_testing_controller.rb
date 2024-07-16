9829fe43511d859933c7e7f997dbfbfb799be41b
Ariana Konhilas <konhilas_ariana@bah.com>
2024-07-16 17:02:18 -0400

    APPEALS-50018: add find features method

--- app/controllers/metrics/load_testing_controller.rb
+++ app/controllers/metrics/load_testing_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require "./scripts/enable_features_dev.rb"
 class LoadTestingController < ApplicationController
   def index
     render json: {
-      feature_toggles_available: { name: "name", default_status: "true or false" },
+      feature_toggles_available: find_features.map { |key, value| { name: key, default_status: value } },
       functions_available: "all the functions in the system",
       all_csum_roles: find_roles,
       all_organizations: find_orgs
@@ -20,7 +21,24 @@ class LoadTestingController < ApplicationController
     User.all.pluck(:roles).flatten.uniq.compact.sort
   end
 
-  # Only accessible non-prod environment
+  def find_features
+    disabled_flags = %w[
+      legacy_das_deprecation
+      cavc_dashboard_workflow
+      poa_auto_refresh
+      interface_version_2
+      cc_vacatur_visibility
+      acd_disable_legacy_lock_ready_appeals
+      justification_reason
+    ]
+    all_features = AllFeatureToggles.new.call.flatten.uniq
+    all_features.map! { |feature| feature.split(",")[0] }
+    all_features.sort.each_with_object({}) do |feature, hash|
+      hash[feature] = (disabled_flags.include? feature) ? "disabled" : "enabled"
+    end
+  end
+
+  # Only accessible from non-prod environment
   def check_environment
     return render status: :not_found if Rails.deploy_env == :production
   end
