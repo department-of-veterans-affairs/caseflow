9ed679c9e17b5b02354e443a0482bef9e667f534
raymond-hughes <raymond.hughes2@va.gov>
2024-07-17 09:50:32 -0400

    revert change

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -35,7 +35,7 @@ FactoryBot.define do
     time.concat(format("%<n>04d", n: n))
   end
 
-  User factory
+  # User factory
   sequence :css_id do |n|
     FactoryBot.sequence_by_name(:css_id).rewind if n == 999
     shortened_time.concat(format("%<n>03d", n: n))
