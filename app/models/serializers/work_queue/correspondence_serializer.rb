2830bd18be17fb88ee26bdcdb4e9d847e9d9edfd
Ryan Marshall <rnmarshall1993@gmail.com>
2024-07-17 13:58:51 -0700

    backend serializer updated, regressions fixed

--- app/models/serializers/work_queue/correspondence_serializer.rb
+++ app/models/serializers/work_queue/correspondence_serializer.rb
@@ -20,6 +20,30 @@ class WorkQueue::CorrespondenceSerializer
     object.correspondence_type&.name
   end
 
+  attribute :tasks_unrelated_to_appeal do |object|
+    filtered_tasks = object.tasks.reject do |task|
+      task.type == "ReviewPackageTask" ||
+        task.type == "CorrespondenceIntakeTask" ||
+        task.type == "CorrespondenceRootTask"
+    end
+
+    # next unless filtered_tasks.empty?
+
+    tasks = []
+    if !filtered_tasks.empty?
+      filtered_tasks.each do |task|
+        tasks <<
+          {
+            type: task.type,
+            assigned_to: task.assigned_to.name,
+            assigned_at: task.assigned_at.strftime("%m/%d/%Y"),
+            instructions: task.instructions
+          }
+      end
+    end
+    tasks
+  end
+
   attribute :veteran_full_name do |object|
     [object.veteran_full_name&.first_name, object.veteran_full_name&.last_name].join(" ")
   end
