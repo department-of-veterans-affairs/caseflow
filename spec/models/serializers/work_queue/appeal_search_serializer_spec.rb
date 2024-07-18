d8dcc1cfab4e94456ba50c3598c98b9887ff2a37
AimanK <Aiman.Kayad@va.gov>
2024-07-18 15:42:29 -0400

    Add more extensive test coverage (every restricted status type)

--- spec/models/serializers/work_queue/appeal_search_serializer_spec.rb
+++ spec/models/serializers/work_queue/appeal_search_serializer_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 describe WorkQueue::AppealSearchSerializer, :all_dbs do
   describe "#assigned_to_location" do
-    context "when appeal status is restricted" do
+    context "when appeal status is distributed to judge" do
       let!(:judge_user) { create(:user, :with_vacols_judge_record, full_name: "Judge Judy", css_id: "JUDGE_J") }
       let(:appeal) { create(:appeal, :assigned_to_judge, associated_judge: judge_user) }
 
@@ -21,7 +21,7 @@ describe WorkQueue::AppealSearchSerializer, :all_dbs do
         end
       end
 
-      context "when appeal status is restricted" do
+      context "when appeal status is assigned to attorney" do
         let(:appeal) { create(:appeal, :at_attorney_drafting) }
         let!(:attorney_user) { create(:user) }
         let!(:vacols_atty) { create(:staff, :attorney_role, sdomainid: attorney_user.css_id) }
@@ -40,7 +40,7 @@ describe WorkQueue::AppealSearchSerializer, :all_dbs do
         end
       end
 
-      context "when appeal status is restricted" do
+      context "when appeal status is ready for signature" do
         let!(:judge_user) { create(:user, :with_vacols_judge_record, full_name: "Judge Judy", css_id: "JUDGE_J") }
         let(:appeal) { create(:appeal, :at_judge_review, associated_judge: judge_user) }
         let!(:hearings_coordinator_user) do
@@ -63,7 +63,92 @@ describe WorkQueue::AppealSearchSerializer, :all_dbs do
         end
       end
 
-      context "when user is a vso representative" do
+      context "when status is distributed to judge" do
+        let!(:judge_user) { create(:user, :with_vacols_judge_record, full_name: "Judge Judy", css_id: "JUDGE_J") }
+        let(:appeal) { create(:appeal, :assigned_to_judge, associated_judge: judge_user) }
+        let(:vso_user) { create(:user, :vso_role) }
+
+        before do
+          User.authenticate!(user: vso_user)
+        end
+
+        subject { described_class.new(appeal, params: { user: vso_user }) }
+
+        it "does not show CSS ID to VSO user" do
+          expect(subject.serializable_hash[:data][:attributes][:assigned_to_location]).to be_nil
+        end
+      end
+
+      context "when status is ready for signature" do
+        let!(:judge_user) { create(:user, :with_vacols_judge_record, full_name: "Judge Judy", css_id: "JUDGE_J") }
+        let(:appeal) { create(:appeal, :at_judge_review, associated_judge: judge_user) }
+        let(:vso_user) { create(:user, :vso_role) }
+
+        before do
+          User.authenticate!(user: vso_user)
+        end
+
+        subject { described_class.new(appeal, params: { user: vso_user }) }
+
+        it "does not show CSS ID to VSO user" do
+          expect(subject.serializable_hash[:data][:attributes][:assigned_to_location]).to be_nil
+        end
+      end
+
+      context "when status is on hold" do
+        let(:appeal) do
+          create(:appeal).tap do |appeal|
+            create(:timed_hold_task, parent: create(:root_task, appeal: appeal))
+          end
+        end
+        let(:vso_user) { create(:user, :vso_role) }
+
+        before do
+          User.authenticate!(user: vso_user)
+        end
+
+        subject { described_class.new(appeal, params: { user: vso_user }) }
+
+        it "does not show CSS ID to VSO user" do
+          expect(subject.serializable_hash[:data][:attributes][:assigned_to_location]).to be_nil
+        end
+      end
+
+      context "when status is misc" do
+        let(:appeal) do
+          create(:appeal).tap do |appeal|
+            create(:ama_judge_dispatch_return_task, parent: create(:root_task, appeal: appeal))
+          end
+        end
+        let(:vso_user) { create(:user, :vso_role) }
+
+        before do
+          User.authenticate!(user: vso_user)
+        end
+
+        subject { described_class.new(appeal, params: { user: vso_user }) }
+
+        it "does not show CSS ID to VSO user" do
+          expect(subject.serializable_hash[:data][:attributes][:assigned_to_location]).to be_nil
+        end
+      end
+
+      context "when status is unknown" do
+        let(:appeal) { create(:appeal) }
+        let(:vso_user) { create(:user, :vso_role) }
+
+        before do
+          User.authenticate!(user: vso_user)
+        end
+
+        subject { described_class.new(appeal, params: { user: vso_user }) }
+
+        it "does not show CSS ID to VSO user" do
+          expect(subject.serializable_hash[:data][:attributes][:assigned_to_location]).to be_nil
+        end
+      end
+
+      context "when status is assigned to attorney" do
         let(:appeal) { create(:appeal, :at_attorney_drafting) }
         let(:vso_user) { create(:user, :vso_role) }
 
