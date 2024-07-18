6e71153e072e9d5c68eb848f30babf8e0f0c4090
SHarshain <spoosa@razortalent.com>
2024-07-18 15:20:15 -0400

    APPEALS-45232. Fixed the class name

--- spec/models/returned_appeal_job_spec.rb
+++ spec/models/returned_appeal_job_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe CaseDistributionLever, :all_dbs do
+RSpec.describe ReturnedAppealJob, :all_dbs do
   describe "factory" do
     it "is valid" do
       expect(build(:returned_appeal_job)).to be_valid
