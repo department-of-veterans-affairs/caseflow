34fccb146caa2af09b1c1b54bbd38b5a8d92c84d
Sean Craig <sean.craig2@va.gov>
2024-07-18 10:31:49 -0500

    updated migration name and fixed typo

--- db/migrate/20240716143816_update_supplemental_claims.rb
+++ db/migrate/20240716143816_update_supplemental_claims.rb
@@ -1,7 +1,7 @@
-class UpdateSupplementalClaims < ActiveRecord::Migration[6.0]
+class AddTypeColumnToSupplementalClaimsForRemandInheritance < ActiveRecord::Migration[6.0]
   def change
     safety_assured do
-      add_column :supplemental_claims, :type, :string, comment: "Single table inheritanc e column for remands"
+      add_column :supplemental_claims, :type, :string, comment: "Single table inheritance column for remands"
     end
   end
 end
