5f8e7ac84eb7439c6984c4a37917466eafed6578
Sean Parker <Sean.Parker3@va.gov>
2024-07-19 12:40:00 -0400

    moving aoj appeal repository file to the correct folder

--- app/models/docket_coordinator.rb
+++ app/models/docket_coordinator.rb
@@ -6,8 +6,7 @@ class DocketCoordinator
       legacy: LegacyDocket.new,
       direct_review: DirectReviewDocket.new,
       evidence_submission: EvidenceSubmissionDocket.new,
-      hearing: HearingRequestDocket.new,
-      aoj_appeal_repository: AojAppealRepository.new
+      hearing: HearingRequestDocket.new
     }
 
     if FeatureToggle.enabled?(:acd_disable_legacy_distributions, user: RequestStore.store[:current_user])
