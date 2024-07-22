3c916ccdf9c25f4413149066630202ac3c15906c
Ryan Marshall <rnmarshall1993@gmail.com>
2024-07-22 12:15:44 -0700

    Fixed organization having the ability to edit tasks.

--- client/app/queue/correspondence/CorrespondenceCaseTimeline.jsx
+++ client/app/queue/correspondence/CorrespondenceCaseTimeline.jsx
@@ -4,133 +4,53 @@ import CorrespondenceTaskRows from './CorrespondenceTaskRows.jsx';
 const CorrespondenceCaseTimeline = (props) => {
 
   const actions = [
-    { value: 'changeTask', label: 'Change task type' },
-    { value: 'changeTask', label: 'Assign to team' },
-    { value: 'changeTask', label: 'Assign to person' },
-    { value: 'changeTask', label: 'Mark task complete' },
-    { value: 'changeTask', label: 'Return to Inbound Ops' },
-    { value: 'changeTask', label: 'Cancel task' },
+    {value: "changeTask", label:"Change task type"},
+    {value: "changeTask", label:"Assign to team"},
+    {value: "changeTask", label:"Assign to person"},
+    {value: "changeTask", label:"Mark task complete"},
+    {value: "changeTask", label:"Return to Inbound Ops"},
+    {value: "changeTask", label:"Cancel task"},
   ];
 
   const getAvailableActions = (task) => {
-    // if(props.organizations.includes(task.assigned_to)) {
-    //   return true;
-    // }
-    // console.log(props)
-    // console.log(task)
-    console.log(props.userCssId);
-    console.log(task.assigned_to);
-    if (props.userCssId === task.assigned_to) {
-      console.log(props.currentUser);
-      console.log(task.assigned_to);
+    // console.log(task.assigned_to)
+    // console.log(props.organizations)
+    if(props.organizations.includes(task.assigned_to)) {
+      return actions;
+    }
+
+    if(props.userCssId === task.assigned_to) {
 
       return actions;
     }
 
     return [];
 
-  };
+  }
+
 
   const formatTaskData = () => {
     return (props.correspondence.tasksUnrelatedToAppeal.map((task) => {
-      console.log(getAvailableActions(task));
-
+      // console.log(getAvailableActions(task))
       return {
         assignedOn: task.assigned_at,
         assignedTo: task.assigned_to,
         label: task.type,
         instructions: task.instructions,
-        availableActions: actions,
+        availableActions: getAvailableActions(task),
       };
     }));
   };
-
-  formatTaskData();
-
-  const debugData = [
-    {
-      assignedOn: '2024-07-18T15:51:22.946-04:00',
-      assignedBy: 'test',
-      instructions: ['test2'],
-      availableActions: actions,
-      label: 'CHANGEME',
-    },
-    {
-      // uniqueId: '3083',
-      // isLegacy: false,
-      type: 'IssuesUpdateTask',
-      // appealType: 'LegacyAppeal',
-      // addedByCssId: 15,
-      // appealId: 13,
-      // externalAppealId: '3619838',
-      assignedOn: '2024-07-18T15:51:22.946-04:00',
-      // closestRegionalOffice: null,
-      createdAt: '2024-07-18T15:51:22.946-04:00',
-      // closedAt: '2024-07-18T15:51:22.992-04:00',
-      startedAt: null,
-      assigneeName: 'Special Issue Edit Team',
-      assignedTo: {
-        cssId: 'null',
-        name: 'Special Issue Edit Team',
-        id: 74,
-        isOrganization: true,
-        type: 'SpecialIssueEditTeam'
-      },
-      assignedBy: 'test2 name',
-      completedBy: {
-        cssId: 'BVADWISE'
-      },
-      cancelledBy: {
-        cssId: null
-      },
-      cancelReason: null,
-      convertedBy: {
-        cssId: null
-      },
-      convertedOn: null,
-      taskId: '3083',
-      parentId: 3082,
-      label: 'Issues Update Task',
-      caseType: 'Post Remand',
-      aod: false,
-      previousTaskAssignedOn: null,
-      placedOnHoldAt: null,
-      status: 'completed',
-      onHoldDuration: null,
-      instructions: [
-        [
-          null, null, 'test'
-        ]
-      ],
-      decisionPreparedBy: null,
-      availableActions: [],
-      timelineTitle: 'IssuesUpdateTask completed',
-      hideFromQueueTableView: true,
-      hideFromTaskSnapshot: true,
-      hideFromCaseTimeline: true,
-      availableHearingLocations: [],
-      latestInformalHearingPresentationTask: {},
-      canMoveOnDocketSwitch: false,
-      timerEndsAt: null,
-      unscheduledHearingNotes: {},
-      ownedBy: 'Special Issue Edit Team',
-      // daysSinceLastStatusChange: 10,
-      // daysSinceBoardIntake: 10,
-      id: '3083',
-      claimant: {},
-      appeal_receipt_date: 100,
-    }
-  ];
-
-  // console.log(props)
 
   return (
     <React.Fragment>
       <table id="case-timeline-table" summary="layout table">
         <tbody>
-          <CorrespondenceTaskRows appeal={props.correspondence}
+        <CorrespondenceTaskRows
+            organizations={props.organizations}
+            appeal={props.correspondence}
             taskList={formatTaskData()}
-            editNodDateEnabled
+            editNodDateEnabled={true}
             statusSplit
           />
         </tbody>
