91d3ae71b0c723b577350273d47d3df3a3da3cbf
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-07-21 22:24:46 -0400

    Add yarddoc comments to SqsService class

--- app/services/sqs_service.rb
+++ app/services/sqs_service.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
+# A service class to aid in interacting with Caseflow's SQS queues.
 class SqsService
   class << self
+    # Intializes an SQS client, or returns a cached version if one has already been initialized.
+    #
+    # @return [Aws::SQS::Client]
+    #  An SQS Client
     def sqs_client
       @sqs_client ||= initialize_sqs_client
     end
 
+    # Locates the URL for a SQS queue based on a provided substring.
+    #
+    # @param name [String] A substring of the queue's name being searched for.
+    # @param check_fifo [Boolean] Whether or not the queue being searched for should be for a FIFO queue.
+    #
+    # @return [String] The full URL of the SQS queue whose name contains the substring provided.
     def find_queue_url_by_name(name:, check_fifo: false)
       url = sqs_client.list_queues.queue_urls.find { _1.include? name }
 
@@ -19,6 +30,10 @@ class SqsService
       url
     end
 
+    # Removes the messages provided from a specified queue.
+    #
+    # @param queue_url [String] The URL of the SQS queue that the messages will be deleted from.
+    # @param messages [Array<Aws::SQS::Types::Message>] Messages to be deleted.
     def batch_delete_messages(queue_url:, messages:)
       messages.in_groups_of(10, false).flat_map do |msg_batch|
         sqs_client.delete_message_batch({
@@ -30,6 +45,11 @@ class SqsService
 
     private
 
+    # Intializes an SQS client. Takes into account SQS endpoint overrides and applies them
+    # to the instantiated client object.
+    #
+    # @return [Aws::SQS::Client]
+    #  An SQS Client
     def initialize_sqs_client
       sqs_client = Aws::SQS::Client.new
 
@@ -41,6 +61,12 @@ class SqsService
       sqs_client
     end
 
+    # Prepares a batch of messages to be in the format needed for the SQS SDK's delete_message_batch method.
+    #
+    # @param unprocessed_entries [Array<Aws::SQS::Types::Message>] Messages to be deleted.
+    #
+    # @return [Array<Hash>] An array where each entry is a hash that contains a unique (per batch)
+    # id and a message's receipt handle.
     def process_entries_for_batch_delete(unprocessed_entries)
       unprocessed_entries.map.with_index do |msg, index|
         {
