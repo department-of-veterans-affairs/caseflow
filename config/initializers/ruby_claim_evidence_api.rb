e5b06bc2f8d5b83744fea6e51c7a9a13200f7bb5
Alex Smith <alex.smith.99@thunderyard.com>
2024-07-23 09:15:59 -0600

    Allow Vet File Update via Claim Evidence API (#22194)
    * Allow Vet File Update via Claim Evidence API
    
    * Update Gem Version and Get Specs Passing

--- config/initializers/ruby_claim_evidence_api.rb
+++ config/initializers/ruby_claim_evidence_api.rb
@@ -2,3 +2,6 @@
 
 VeteranFileFetcher = ExternalApi::VeteranFileFetcher
   .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
+
+VeteranFileUpdater = ExternalApi::VeteranFileUpdater
+  .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
