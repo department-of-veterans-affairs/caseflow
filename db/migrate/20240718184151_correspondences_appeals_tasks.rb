23b5ca0ebd6342d2ff192f46b4b936b0b1b5cc05
Dev-KRedd <khalin.redding@va.gov>
2024-07-24 16:58:03 -0400

    WIP Failing tests

--- db/migrate/20240718184151_correspondences_appeals_tasks.rb
+++ db/migrate/20240718184151_correspondences_appeals_tasks.rb
@@ -3,24 +3,30 @@ class CorrespondencesAppealsTasks < Caseflow::Migration
     revert do
       reversible do |direction|
         self.up do
+          safety_assured do
           create_table :correspondences_appeals_tasks do |t|
             t.references :correspondence_appeal, foreign_key: true, index: false
             t.belongs_to :task, null: false, foreign_key: true
 
             t.timestamps
           end
+          end
         end
         self.down do
+         safety_assured do
          drop_table :correspondences_appeals_tasks
+        end
         end
       end
     end
 
     create_table :correspondences_appeals_tasks do |t|
+      safety_assured do
       t.references :correspondence, foreign_key: true, index: false
       t.belongs_to :task, null: false, foreign_key: true
 
       t.timestamps
+      end
   end
   end
 end
