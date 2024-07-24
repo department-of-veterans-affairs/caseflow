89327108dde86369f6d910d1e80d5f007070ba5e
Sean Craig <110493538+seancva@users.noreply.github.com>
2024-07-24 14:33:21 -0500

    SeanC/APPEALS-50711 | Refactor RSpecs - 2 (#22079)
    * replaced not_to have_content with has_no_content on higher_level_review_spec:1239
    
    * replaced not_to have_content with has_no_content on cavc_dashboard_spec:76
    
    * replaced not_to have_content with has_no_content on search_spec:514 and 677
    
    * removed sleep call, edit_spec:1104
    
    * removed skip and sleep calls, bgs_power_of_attorney_spec:1104
    
    * attempt at removing sleep from edit_hearsched_spec:372
    
    * reverting change in edit_hearsched_spec
    
    * added a skip that should not have been removed

--- spec/feature/intake/appeal/edit_spec.rb
+++ spec/feature/intake/appeal/edit_spec.rb
@@ -1101,8 +1101,6 @@ feature "Appeal Edit issues", :all_dbs do
       expect(withdrawn_issue).to_not be_nil
       expect(withdrawn_issue.closed_at).to eq(1.day.ago.to_date.to_datetime)
 
-      sleep 1
-
       # reload to verify that the new issues populate the form
       visit "appeals/#{appeal.uuid}/edit/"
 
