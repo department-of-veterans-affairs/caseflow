564420ce53f3b90ff2bbeaa37ed7bed226581f50
Christopher Aceves <christopher.aceves@va.gov>
2024-07-24 14:14:02 -0700

    Rubocop fixes

--- db/migrate/20240718184151_correspondences_appeals_tasks.rb
+++ db/migrate/20240718184151_correspondences_appeals_tasks.rb
@@ -1,32 +1,34 @@
+# frozen_string_literal: true
+
 class CorrespondencesAppealsTasks < Caseflow::Migration
   def change
     revert do
-      reversible do |direction|
-        self.up do
+      reversible do |_direction|
+        up do
           safety_assured do
-          create_table :correspondences_appeals_tasks do |t|
-            t.references :correspondence_appeal, foreign_key: true, index: false
-            t.belongs_to :task, null: false, foreign_key: true
+            create_table :correspondences_appeals_tasks do |t|
+              t.references :correspondence_appeal, foreign_key: true, index: false
+              t.belongs_to :task, null: false, foreign_key: true
 
-            t.timestamps
-          end
+              t.timestamps
+            end
           end
         end
-        self.down do
-         safety_assured do
-         drop_table :correspondences_appeals_tasks
-        end
+        down do
+          safety_assured do
+            drop_table :correspondences_appeals_tasks
+          end
         end
       end
     end
 
     create_table :correspondences_appeals_tasks do |t|
       safety_assured do
-      t.references :correspondence, foreign_key: true, index: false
-      t.belongs_to :task, null: false, foreign_key: true
+        t.references :correspondence, foreign_key: true, index: false
+        t.belongs_to :task, null: false, foreign_key: true
 
-      t.timestamps
+        t.timestamps
       end
-  end
+    end
   end
 end
