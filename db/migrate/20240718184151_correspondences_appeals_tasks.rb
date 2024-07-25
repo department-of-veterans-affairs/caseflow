78deeba3a64a3ce59679a112ad52f6754cf63b62
Dev-KRedd <khalin.redding@va.gov>
2024-07-25 11:26:14 -0400

    Updated migration file

--- db/migrate/20240718184151_correspondences_appeals_tasks.rb
+++ db/migrate/20240718184151_correspondences_appeals_tasks.rb
@@ -1,30 +1,16 @@
 # frozen_string_literal: true
 
 class CorrespondencesAppealsTasks < Caseflow::Migration
-  def change
-    revert do
-      reversible do |_direction|
-        up do
-          safety_assured do
-            create_table :correspondences_appeals_tasks do |t|
-              t.references :correspondence_appeal, foreign_key: true, index: false
-              t.belongs_to :task, null: false, foreign_key: true
-
-              t.timestamps
-            end
-          end
-        end
-        down do
-          safety_assured do
-            drop_table :correspondences_appeals_tasks
-          end
-        end
-      end
+  def up
+    safety_assured do
+      drop_table :correspondences_appeals_tasks
     end
+  end
 
-    create_table :correspondences_appeals_tasks do |t|
-      safety_assured do
-        t.references :correspondence, foreign_key: true, index: false
+  def down
+    safety_assured do
+      create_table :correspondences_appeals_tasks do |t|
+        t.references :correspondence_appeal, foreign_key: true, index: false
         t.belongs_to :task, null: false, foreign_key: true
 
         t.timestamps
