1e8ddeec9269877334966aef932698c7834bc598
noahhansen-gov <166541737+noahhansen-gov@users.noreply.github.com>
2024-07-26 09:23:40 -0600

    APPEALS-51087: Rewrite Api::V1::VaNotifyController#notification_update Controller Action to No Longer Utilize Redis (#22295)
    * initial push
    
    * rubocop'n
    
    * fixing tests?
    
    * rubocop'n
    
    * addressing most of matt's comments
    
    * removing yard doc files and keeping comment
    
    * rubocop
    
    ---------
    
    Co-authored-by: nhansen3 <noah.hansen1323@gmail.com>

--- app/controllers/api/v1/va_notify_controller.rb
+++ app/controllers/api/v1/va_notify_controller.rb
@@ -1,46 +1,62 @@
 # frozen_string_literal: true
 
 class Api::V1::VaNotifyController < Api::ApplicationController
-  # Purpose: POST request to VA Notify API to update status for a Notification entry
+  # Purpose: POST request to VA Notify API to update status for a Notification entry.
   #
   # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
   #
   # Response: Update corresponding Notification status
   def notifications_update
-    send "#{required_params[:notification_type]}_update"
+    send_sqs_message
+    render json: { message: "#{required_params[:notification_type_param]} \
+                   Notification successfully updated: ID #{required_params[:id]}" }
+  rescue StandardError => error
+    log_error(error, params["id"], params["notification_type"])
+    render json: { error: error.message }, status: :bad_request
   end
 
   private
 
-  # Purpose: Finds and updates notification if type is email
-  #
-  # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
-  #
-  # Response: Update corresponding email Notification status
-  def email_update
-    redis.set("email_update:#{required_params[:id]}:#{required_params[:status]}", 0)
-
-    render json: { message: "Email notification successfully updated: ID #{required_params[:id]}" }
-  end
-
-  # Purpose: Finds and updates notification if type is SMS
-  #
-  # Params: Params content can be found at https://vajira.max.gov/browse/APPEALS-21021
-  #
-  # Response: Update corresponding SMS Notification status
-  def sms_update
-    redis.set("sms_update:#{required_params[:id]}:#{required_params[:status]}", 0)
-
-    render json: { message: "SMS notification successfully updated: ID #{required_params[:id]}" }
-  end
-
   def required_params
-    id_param, notification_type_param, status_param = params.require([:id, :notification_type, :status])
-
-    { id: id_param, notification_type: notification_type_param, status: status_param }
+    id_param, notification_type_param,
+    to_param, status_param,
+    status_reason_param = params.require([:id, :notification_type, :to, :status, :status_reason])
+    {
+      external_id: id_param,
+      notification_type: notification_type_param,
+      receipient: to_param,
+      status: status_param,
+      status_reason: status_reason_param,
+      message: "#{notification_type_param} notification successfully updated: ID #{id_param}"
+    }
+  rescue StandardError => error
+    raise error
   end
 
-  def redis
-    @redis ||= Redis.new(url: Rails.application.secrets.redis_url_cache)
+  def build_sqs_message
+    sqs_url = SqsService.find_queue_url_by_name(name: "receive_notifications")
+
+    message_body = required_params.to_json
+
+    {
+      queue_url: sqs_url,
+      message_body: message_body,
+      message_deduplication_id: Digest::SHA256.hexdigest(message_body),
+      message_group_id: Constants.VA_NOTIFY_CONSTANTS.message_group_id
+    }
+  rescue StandardError => error
+    raise error
+  end
+
+  def send_sqs_message
+    sqs = SqsService.sqs_client
+    sqs.send_message(build_sqs_message)
+  end
+
+  def log_error(error, external_id, notification_type)
+    Rails.logger.error("#{error.message}\n#{error.backtrace.join("\n")}\n \
+                       external_id: #{external_id}\n \
+                       notification_type: #{notification_type}")
+    Raven.capture_exception(error)
   end
 end
