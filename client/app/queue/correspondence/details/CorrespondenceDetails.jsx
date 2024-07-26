4be1991927aa388c8ebe7f864d24a739fe334db4
vinner57 <128258952+vinner57@users.noreply.github.com>
2024-07-26 11:41:35 -0400

    sorting the appeals

--- client/app/queue/correspondence/details/CorrespondenceDetails.jsx
+++ client/app/queue/correspondence/details/CorrespondenceDetails.jsx
@@ -15,7 +15,20 @@ const CorrespondenceDetails = (props) => {
   const mailTasks = props.correspondence.mailTasks;
   const appealsResult = props.correspondence.appeals_information;
   const appeals = [];
-  const searchStoreAppeal = prepareAppealForSearchStore(appealsResult.appeals);
+  let filteredAppeals = []
+  let unfilteredAppeals = []
+  appealsResult.appeals.map((appeal) => {
+    if (correspondence.correspondenceAppealIds.includes(appeal.id)){
+      filteredAppeals.push(appeal)
+    }else{
+      unfilteredAppeals.push(appeal)
+    }
+  })
+  filteredAppeals = filteredAppeals.sort((leftAppeal, rightAppeal) => leftAppeal.id - rightAppeal.id)
+  unfilteredAppeals = unfilteredAppeals.sort((leftAppeal, rightAppeal) => leftAppeal.id - rightAppeal.id)
+  let sortedAppeals = filteredAppeals.concat(unfilteredAppeals)
+
+  const searchStoreAppeal = prepareAppealForSearchStore(sortedAppeals);
   const appeall = searchStoreAppeal.appeals;
   const appealldetail = searchStoreAppeal.appealDetails;
   const hashKeys = Object.keys(appeall);
