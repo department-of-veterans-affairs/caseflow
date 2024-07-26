16f5fa87028cb4d11f23fe1345e8a8cad396c65d
HunJerBAH <Jeremy.Hunton@va.gov>
2024-07-26 15:23:05 -0400

    added back in 10182 check for nod

--- app/models/correspondence_document.rb
+++ app/models/correspondence_document.rb
@@ -38,7 +38,7 @@ class CorrespondenceDocument < CaseflowRecord
   def update_correspondence_nod
     documents = correspondence.correspondence_documents
     nod = documents.any? do |doc|
-      doc["vbms_document_type_id"]
+      doc["vbms_document_type_id"] && Caseflow::DocumentTypes::TYPES[doc["vbms_document_type_id"]].include?("10182")
     end
     correspondence.update!(
       nod: nod
