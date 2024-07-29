9c0ecba808ea69e3880109cd61d700faa133b45a
Minhazur Rahaman <minhazur9@gmail.com>
2024-07-29 15:58:03 -0400

    APPEALS-43157 basic modal calculates return date including business days

--- client/app/hearings/components/transcriptionProcessing/PackageFilesModal.jsx
+++ client/app/hearings/components/transcriptionProcessing/PackageFilesModal.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../../../components/Modal';
 import COPY from '../../../../COPY';
+import { RadioField } from '../../../components/RadioField';
+import { SearchableDropdown } from '../../../components/SearchableDropdown';
 
 const PackageFilesModal = ({ onCancel }) => {
+  const renderWorkOrder = () => {
+    return (
+      <div>
+        <strong>Work Order</strong>
+        <p>#BVA20240001</p>
+      </div>
+    );
+  };
+
+  const calculateReturnDate = (days) => {
+    const date = new Date();
+
+    let dayofWeek = date.getDay();
+    let daysRemaining = days;
+    let totalDays = 0;
+
+    while (daysRemaining > 0) {
+      dayofWeek += 1;
+      if (dayofWeek < 6) {
+        daysRemaining -= 1;
+        totalDays += 1;
+      } else {
+        totalDays += 2;
+        dayofWeek = 0;
+      }
+    }
+
+    date.setDate(date.getDate() + totalDays);
+
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+  };
+
+  const returnDateOptions = () => {
+    const returnDate = calculateReturnDate(15);
+
+    return [
+      {
+        displayText: `Return in 15 days (${returnDate})`,
+        value: returnDate
+      },
+      {
+        displayText: 'Expedite (Maximum of 5 days)',
+        value: calculateReturnDate(5)
+      }
+    ];
+  };
+
+  const renderContractors = () => {
+    return <SearchableDropdown
+      name=<strong>Contractor</strong>
+      options={[]}
+    />;
+  };
+
+  const renderReturnDateSection = () => {
+    return (
+      <RadioField
+        name="returnDateRadioField"
+        label=<strong>Return date</strong>
+        options={returnDateOptions()}
+      />
+    );
+  };
+
   return (
     <Modal
       title="Package Files"
@@ -20,7 +87,11 @@ const PackageFilesModal = ({ onCancel }) => {
         },
       ]}
       closeHandler={onCancel}
-    />);
+    >
+      {renderWorkOrder()}
+      {renderReturnDateSection()}
+      {renderContractors()}
+    </Modal>);
 };
 
 PackageFilesModal.propTypes = {
