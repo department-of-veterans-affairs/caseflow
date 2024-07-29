35bc5761570c79f57283767b36bfc8b300315b55
IsaiahBar <isaiah.barnett@va.gov>
2024-07-29 15:16:35 -0400

    updating task_url

--- app/models/tasks/mail_tasks_not_related_to_appeal/cavc_correspondence_correspondence_task.rb
+++ app/models/tasks/mail_tasks_not_related_to_appeal/cavc_correspondence_correspondence_task.rb
@@ -4,4 +4,8 @@ class CavcCorrespondenceCorrespondenceTask < CorrespondenceMailTask
   def label
     COPY::FOIA_REQUEST_MAIL_TASK_LABEL
   end
+
+  def task_url
+    Constants.CORRESPONDENCE_TASK_URL.CORRESPONDENCE_TASK_DETAIL_URL.sub("uuid", correspondence.uuid)
+  end
 end
