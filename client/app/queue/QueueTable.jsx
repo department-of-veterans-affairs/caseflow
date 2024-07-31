986faae433e6e45d1a9bff5e690028f0762ff83e
Brandon Lee Dorner <Brandondorner@yahoo.com>
2024-07-31 08:55:35 -0500

    Origin/hotfix/appeals 46639 uat (#22354)
    * Initial commit and turning off queue tab variables used for the frontend pagination for the bulk assign tab for the specialty case team.
    
    * Altered the queue actions to work with the new task selection action and a new redux action to remove tasks from a cached queue state. Also updated queue table to work with the redux caching instead of local state caching.
    
    * Added in task selection function changes to TaskTable to work
    with the altered task selection redux action. Also added the ability to pass down the redux cache property to queue table. Refactored the UnassignedCasesPage to allow for backend pagination of queue table instead of only working via frontend loaded data from the redux store.
    
    * Added the queueTableCache reducer slice. Added a :advanced_on_docket trait to the task factory for specialty_case_team_assign_tasks. Added the caching combined reducer to the queue table reducers.
    
    * Moved columnsFromConfig and createColumnObject functions out of QueueTableBuilder.jsx into a utils file so it can be used in multiple components around the app.
    
    * Updated the setSelectionOfTaskOfUser to work with the new action and data that it should be expecting.
    
    * Updated the selectedTasksSelector to work with the new task selection reducer and action.
    
    * Added a parameter for useReduxCache for TaskTable to send down to queue table instead of always setting it to true. Removed some comments from the queueTableCacheSlice removeTaskIdsFromCache reducer method/action. Passed in the new useReduxCache property to TaskTable in the UnassignedCasesPage component for the sct bulk assign queue.
    
    * Removed code out of QueueTableBuilder that is now in queueTableUtils.js.
    
    * Fixed a bug if queueConfig was an empty hash in the UnassignedCasesPage component.
    
    * Serializer optimizations for contested issues and issue categories. Also included request issues in the includes for the sct queue to take advantage of these optimizations.
    
    * Fixed a bug where clear filters was not properly updating the get parameters anymore for queue table.
    
    * Added cavc_type trait to the sct assign task factory.
    
    * Fix code climate issues.
    
    ---------
    
    Co-authored-by: = <tyler.broyles@va.gov>

--- client/app/queue/QueueTable.jsx
+++ client/app/queue/QueueTable.jsx
@@ -355,10 +355,21 @@ export default class QueueTable extends React.PureComponent {
 
     if (this.props.rowObjects.length) {
       this.setState({ cachedResponses: { ...this.state.cachedResponses, [this.requestUrl()]: firstResponse } });
+
+      if (this.props.useReduxCache) {
+        this.props.updateReduxCache({ key: this.requestUrl(), value: firstResponse });
+      }
+
     }
   };
 
   componentDidUpdate = (previousProps, previousState) => {
+    if (this.props.useReduxCache &&
+      (this.props.reduxCache[this.requestUrl()]?.tasks?.length !==
+        previousProps.reduxCache[this.requestUrl()]?.tasks?.length)) {
+      this.setState({ tasksFromApi: this.props.reduxCache[this.requestUrl()].tasks });
+    }
+
     // Only refetch if the search query text changes
     if (this.props.tabPaginationOptions &&
       previousState.querySearchText !== this.props.tabPaginationOptions[QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM]) {
@@ -558,9 +569,29 @@ export default class QueueTable extends React.PureComponent {
 
   deepLink = () => {
     const base = `${window.location.origin}${window.location.pathname}`;
-    const tab = this.props.taskPagesApiEndpoint.split('?')[1];
+    const currentParams = new URLSearchParams(window.location.search);
+    const tableParams = new URLSearchParams(this.requestQueryString());
+    const tabParams = new URLSearchParams(this.props.taskPagesApiEndpoint.split('?')[1]);
 
-    return `${base}?${tab}${this.requestQueryString()}`;
+    // List of parameters that should be cleared if not present in tableParams
+    const paramsToClear = [
+      QUEUE_CONFIG.SEARCH_QUERY_REQUEST_PARAM,
+      `${QUEUE_CONFIG.FILTER_COLUMN_REQUEST_PARAM}[]`,
+    ];
+
+    // Remove paramsToClear from currentParams if they are not in tableParams
+    paramsToClear.forEach((param) => {
+      if (!tableParams.has(param)) {
+        currentParams.delete(param);
+      }
+    });
+
+    // Merge tableParams and tabParams into currentParams, overwriting any duplicate keys
+    for (const [key, value] of [...tabParams.entries(), ...tableParams.entries()]) {
+      currentParams.set(key, value);
+    }
+
+    return `${base}?${currentParams.toString()}`;
   };
 
   // /organizations/vlj-support-staff/tasks?tab=on_hold
@@ -624,7 +655,8 @@ export default class QueueTable extends React.PureComponent {
     const endpointUrl = this.requestUrl();
 
     // If we already have the tasks cached then we set the state and return early.
-    const responseFromCache = this.state.cachedResponses[endpointUrl];
+    const responseFromCache = this.props.useReduxCache ? this.props.reduxCache[endpointUrl] :
+      this.state.cachedResponses[endpointUrl];
 
     if (responseFromCache) {
       this.setState({ tasksFromApi: responseFromCache.tasks });
@@ -647,10 +679,20 @@ export default class QueueTable extends React.PureComponent {
         const preparedResponse = Object.assign(response.body, { tasks: preparedTasks });
 
         this.setState({
-          cachedResponses: { ...this.state.cachedResponses, [endpointUrl]: preparedResponse },
+          // cachedResponses: { ...this.state.cachedResponses, [endpointUrl]: preparedResponse },
+          ...(!this.props.useReduxCache && {
+            cachedResponses: {
+              ...this.state.cachedResponses,
+              [endpointUrl]: preparedResponse
+            }
+          }),
           tasksFromApi: preparedTasks,
           loadingComponent: null
         });
+
+        if (this.props.useReduxCache) {
+          this.props.updateReduxCache({ key: endpointUrl, value: preparedResponse });
+        }
 
         this.updateAddressBar();
       }).
@@ -678,6 +720,8 @@ export default class QueueTable extends React.PureComponent {
       bodyStyling,
       enablePagination,
       useTaskPagesApi,
+      reduxCache,
+      useReduxCache,
       searchValue
     } = this.props;
 
@@ -691,7 +735,7 @@ export default class QueueTable extends React.PureComponent {
 
         // If we already have the response cached then use the attributes of the response to set the pagination vars.
         const endpointUrl = this.requestUrl();
-        const responseFromCache = this.state.cachedResponses[endpointUrl];
+        const responseFromCache = useReduxCache ? reduxCache[endpointUrl] : this.state.cachedResponses[endpointUrl];
 
         if (responseFromCache) {
           numberOfPages = responseFromCache.task_page_count;
@@ -852,6 +896,9 @@ HeaderRow.propTypes = FooterRow.propTypes = Row.propTypes = BodyRows.propTypes =
   }),
   onHistoryUpdate: PropTypes.func,
   preserveFilter: PropTypes.bool,
+  useReduxCache: PropTypes.bool,
+  reduxCache: PropTypes.object,
+  updateReduxCache: PropTypes.func,
   isCorrespondenceTable: PropTypes.bool,
   searchValue: PropTypes.string,
   taskMatchesSearch: PropTypes.func
