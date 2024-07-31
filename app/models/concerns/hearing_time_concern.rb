7b123a6d37e57dcd5852c9a4e4c2e70405f10987
William Medders <medders_william@bah.com>
2024-07-31 15:51:32 -0500

    remove extra line

--- app/models/concerns/hearing_time_concern.rb
+++ app/models/concerns/hearing_time_concern.rb
@@ -6,7 +6,6 @@ module HearingTimeConcern
   delegate :central_office_time_string, :scheduled_time_string,
            to: :time
 
-
   def time
     @time ||= if self.is_a?(LegacyHearing) && scheduled_in_timezone || try(:scheduled_datetime)
                 HearingDatetimeService.new(hearing: self)
