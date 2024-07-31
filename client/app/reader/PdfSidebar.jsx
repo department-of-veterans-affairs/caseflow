9a31562fb9e5bebb62cfdbb0aad8294cd09addaf
Sean Parker <Sean.Parker3@va.gov>
2024-07-31 15:20:01 -0400

    remove inline styles from PdfSidebar and relocate them to _reviewer.scss, the styles appear to work

--- client/app/reader/PdfSidebar.jsx
+++ client/app/reader/PdfSidebar.jsx
@@ -1,6 +1,5 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { css } from 'glamor';
 import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
@@ -106,8 +105,6 @@ export class PdfSidebar extends React.Component {
     let comments = [];
 
     const { appeal } = this.props;
-    const sideBarSmall = '250px';
-    const sideBarLarge = '380px';
 
     comments = sortAnnotations(this.props.comments).map((comment, index) => {
       if (comment.editing) {
@@ -149,15 +146,7 @@ export class PdfSidebar extends React.Component {
       'cf-sidebar-wrapper',
       { 'hidden-sidebar': this.props.hidePdfSidebar });
 
-    const sidebarWrapper = css({
-      width: '28%',
-      minWidth: sideBarSmall,
-      maxWidth: sideBarLarge,
-      '@media(max-width: 920px)': { width: sideBarSmall },
-      '@media(min-width: 1240px)': { width: sideBarLarge }
-    });
-
-    return <div className={sidebarClass} {...sidebarWrapper}>
+    return <div className={sidebarClass} >
       <div className="cf-sidebar-header">
         <Button
           name="hide menu"
