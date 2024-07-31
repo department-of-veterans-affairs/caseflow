aa85bd4b09398df457f677d4204d1583a4de0ba7
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-07-31 18:02:34 -0400

    DRY poa_time and appellant_time methods
    They are only referred to by the Hearings::CalendarTemplateHelper module, and so that is where they'll live

--- app/services/hearing_datetime_service.rb
+++ app/services/hearing_datetime_service.rb
@@ -55,37 +55,4 @@ class HearingDatetimeService
 
     "#{local_time.strftime('%l:%M %p')} #{tz}".lstrip
   end
-
-  # the below methods could potentially be moved to Hearings::CalendarTemplateHelper
-
-  def normalized_time(timezone)
-    return local_time if timezone.nil?
-
-    # throws an error here if timezone is invalid
-    local_time.in_time_zone(timezone)
-  end
-
-  # hearing time in poa timezone
-  def poa_time
-    # Check if there's a recipient, and if it has a timezone, it it does use that to set tz
-    representative_tz_from_recipient = @hearing.representative_recipient&.timezone
-    return normalized_time(representative_tz_from_recipient) if representative_tz_from_recipient.present?
-    # If there's a virtual hearing, use that tz even if it's empty
-    return normalized_time(@hearing.virtual_hearing[:representative_tz]) if @hearing.virtual_hearing.present?
-
-    # No recipient and no virtual hearing? Use the normalized_time fallback
-    normalized_time(nil)
-  end
-
-  # hearing time in appellant timezone
-  def appellant_time
-    # Check if there's a recipient, and if it has a timezone, it it does use that to set tz
-    appellant_tz_from_recipient = @hearing.appellant_recipient&.timezone
-    return normalized_time(appellant_tz_from_recipient) if appellant_tz_from_recipient.present?
-    # If there's a virtual hearing, use that tz even if it's empty
-    return normalized_time(@hearing.virtual_hearing[:appellant_tz]) if @hearing.virtual_hearing.present?
-
-    # No recipient and no virtual hearing? Use the normalized_time fallback
-    normalized_time(nil)
-  end
 end
--- app/services/hearing_time_service.rb
+++ app/services/hearing_time_service.rb
@@ -87,37 +87,6 @@ class HearingTimeService
     local_time.in_time_zone(CENTRAL_OFFICE_TIMEZONE)
   end
 
-  def normalized_time(timezone)
-    return local_time if timezone.nil?
-
-    # throws an error here if timezone is invalid
-    local_time.in_time_zone(timezone)
-  end
-
-  # hearing time in poa timezone
-  def poa_time
-    # Check if there's a recipient, and if it has a timezone, it it does use that to set tz
-    representative_tz_from_recipient = @hearing.representative_recipient&.timezone
-    return normalized_time(representative_tz_from_recipient) if representative_tz_from_recipient.present?
-    # If there's a virtual hearing, use that tz even if it's empty
-    return normalized_time(@hearing.virtual_hearing[:representative_tz]) if @hearing.virtual_hearing.present?
-
-    # No recipient and no virtual hearing? Use the normalized_time fallback
-    normalized_time(nil)
-  end
-
-  # hearing time in appellant timezone
-  def appellant_time
-    # Check if there's a recipient, and if it has a timezone, it it does use that to set tz
-    appellant_tz_from_recipient = @hearing.appellant_recipient&.timezone
-    return normalized_time(appellant_tz_from_recipient) if appellant_tz_from_recipient.present?
-    # If there's a virtual hearing, use that tz even if it's empty
-    return normalized_time(@hearing.virtual_hearing[:appellant_tz]) if @hearing.virtual_hearing.present?
-
-    # No recipient and no virtual hearing? Use the normalized_time fallback
-    normalized_time(nil)
-  end
-
   def time_to_string(time)
     tz = ActiveSupport::TimeZone::MAPPING.key(@hearing.regional_office_timezone)
 
