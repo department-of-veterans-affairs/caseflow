ce7aabe87b01fe05d6db0221c61d53fe18877431
TuckerRose <tuckerrose@tuckerroses-mbp.lan>
2024-08-01 09:29:46 -0400

    updated dru params to match intake json

--- app/controllers/api/events/v1/decision_review_updated_controller.rb
+++ app/controllers/api/events/v1/decision_review_updated_controller.rb
@@ -54,41 +54,51 @@ class Api::Events::V1::DecisionReviewUpdatedController < Api::ApplicationControl
   end
 
   def dru_params
-    params.permit(:event_id,
-                  :claim_id,
-                  :css_id,
-                  :detail_type,
-                  :station,
-                  intake: {},
-                  veteran: {},
-                  claimant: {},
-                  claim_review: {},
-                  end_product_establishment: {},
-                  request_issues: [:benefit_type,
-                                   :contested_issue_description,
-                                   :contention_reference_id,
-                                   :contested_rating_decision_reference_id,
-                                   :contested_rating_issue_profile_date,
-                                   :contested_rating_issue_reference_id,
-                                   :contested_decision_issue_id,
-                                   :decision_date,
-                                   :ineligible_due_to_id,
-                                   :ineligible_reason,
-                                   :is_unidentified,
-                                   :unidentified_issue_text,
-                                   :nonrating_issue_category,
-                                   :nonrating_issue_description,
-                                   :untimely_exemption,
-                                   :untimely_exemption_notes,
-                                   :vacols_id,
-                                   :vacols_sequence_id,
-                                   :closed_at,
-                                   :closed_status,
-                                   :contested_rating_issue_diagnostic_code,
-                                   :ramp_claim_id,
-                                   :rating_issue_associated_at,
-                                   :nonrating_issue_bgs_id,
-                                   :nonrating_issue_bgs_source])
+    params.permit(
+      :event_id,
+      :css_id,
+      :detail_type,
+      :station,
+      claim_review: [
+        :auto_remand,
+        :remand_source_id,
+        :informal_conference,
+        :same_office,
+        :legacy_opt_in_approved
+      ],
+      end_product_establishments: [
+        :development_item_reference_id,
+        :reference_id
+      ],
+      request_issues: [
+        :id,
+        :benefit_type,
+        :closed_at,
+        :closed_status,
+        :contention_reference_id,
+        :contested_issue_description,
+        :contested_rating_issue_diagnostic_code,
+        :contested_rating_issue_reference_id,
+        :contested_rating_issue_profile_date,
+        :contested_decision_issue_id,
+        :decision_date,
+        :ineligible_due_to_id,
+        :ineligible_reason,
+        :is_unidentified,
+        :unidentified_issue_text,
+        :nonrating_issue_category,
+        :nonrating_issue_description,
+        :nonrating_issue_bgs_id,
+        :nonrating_issue_bgs_source,
+        :ramp_claim_id,
+        :rating_issue_associated_at,
+        :untimely_exemption,
+        :untimely_exemption_notes,
+        :vacols_id,
+        :vacols_sequence_id,
+        :veteran_participant_id
+      ]
+    )
   end
   # rubocop:enable Metrics/MethodLength
 end
