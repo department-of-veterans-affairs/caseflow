24b8eecfcc0b641f80a2486894131285a6b81872
Sean Parker <Sean.Parker3@va.gov>
2024-08-02 11:56:07 -0400

    remove glamor import

--- client/app/reader/PdfSidebar.jsx
+++ client/app/reader/PdfSidebar.jsx
@@ -1,6 +1,5 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { css } from 'glamor';
 import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
