f901722be485f4db629fb344ee4fe980a3f5b48f
Ryan Marshall <rnmarshall1993@gmail.com>
2024-08-02 10:01:38 -0700

    removed unused method.

--- app/models/tasks/congressional_interest_mail_task.rb
+++ app/models/tasks/congressional_interest_mail_task.rb
@@ -12,24 +12,4 @@ class CongressionalInterestMailTask < MailTask
   def self.default_assignee(_parent)
     LitigationSupport.singleton
   end
-
-  def self.available_actions(user)
-    return [] unless user
-
-    options = [
-      Constants.TASK_ACTIONS.CHANGE_CORR_TASK_TYPE.to_h,
-      Constants.TASK_ACTIONS.ASSIGN_CORR_TASK_TO_TEAM.to_h,
-      Constants.TASK_ACTIONS.MARK_TASK_COMPLETE.to_h,
-      Constants.TASK_ACTIONS.RETURN_TO_INBOUND_OPS.to_h,
-      Constants.TASK_ACTIONS.CANCEL_CORR_TASK.to_h
-    ]
-
-    if user.assigned_to.name == User.name
-      options.insert(2, Constants.TASK_ACTIONS.ASSIGN_CORR_TASK_TO_PERSON.to_h)
-    else
-      options.insert(2, Constants.TASK_ACTIONS.REASSIGN_CORR_TASK_TO_PERSON.to_h)
-    end
-
-    options
-  end
 end
