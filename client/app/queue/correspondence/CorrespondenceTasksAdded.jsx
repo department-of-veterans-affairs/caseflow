94744423b42a5879c8024c48854673bd385f2551
Ryan Marshall <rnmarshall1993@gmail.com>
2024-08-06 13:23:24 -0700

    fixed border and badge

--- client/app/queue/correspondence/CorrespondenceTasksAdded.jsx
+++ client/app/queue/correspondence/CorrespondenceTasksAdded.jsx
@@ -9,47 +9,48 @@ const CorrespondenceTasksAdded = (props) => {
 
   return (
     <>
-      <div className="correspondence-tasks-added ">
-        <div className="corr-tasks-added-col first-row">
-          <p>Docket number</p>
-          <span className="case-details-badge">
-            <DocketTypeBadge name="test" />
-            <CaseDetailsLink appeal={props.correspondence} getLinkText={() => props.task_added.docket_num} task={{}} userRole="red" />
-            <ExternalLinkIcon color="blue" />
-          </span>
+      <div className="tasks-added-container">
+        <div className="correspondence-tasks-added ">
+          <div className="corr-tasks-added-col first-row">
+            <p>Docket number</p>
+            <span className="case-details-badge">
+              <DocketTypeBadge name="test" />
+              <CaseDetailsLink appeal={props.correspondence} getLinkText={() => props.task_added.docket_num} task={{}} userRole="red" />
+              <span className='link-icon-container'><ExternalLinkIcon color="blue" /> </span>
+            </span>
+
+          </div>
+          <div className="corr-tasks-added-col">
+            <p>Appellant name</p>
+            {/* <p>{props.correspondence.veteran_name}</p> */}
+          </div>
+          <div className="corr-tasks-added-col">
+            <p>Appeal stream type</p>
+            <p>{props.task_added.stream_type}</p>
+          </div>
+          <div className="corr-tasks-added-col">
+            <p>Number of issues</p>
+            <p>{props.task_added.number_of_issues}</p>
+          </div>
+          <div className="corr-tasks-added-col">
+            <p>Status</p>
+            <p>{props.task_added.status}</p>
+          </div>
+          <div className="corr-tasks-added-col">
+            <p>Added to</p>
+            <p>{props.task_added.assigned_to}</p>
+          </div>
 
         </div>
-        <div className="corr-tasks-added-col">
-          <p>Appellant name</p>
-          {/* <p>{props.correspondence.veteran_name}</p> */}
-        </div>
-        <div className="corr-tasks-added-col">
-          <p>Appeal stream type</p>
-          <p>{props.task_added.stream_type}</p>
-        </div>
-        <div className="corr-tasks-added-col">
-          <p>Number of issues</p>
-          <p>{props.task_added.number_of_issues}</p>
-        </div>
-        <div className="corr-tasks-added-col">
-          <p>Status</p>
-          <p>{props.task_added.status}</p>
-        </div>
-        <div className="corr-tasks-added-col">
-          <p>Added to</p>
-          <p>{props.task_added.assigned_to}</p>
-        </div>
+        <span className="tasks-added-text">Tasks added to appeal</span>
 
+        <CorrespondenceCaseTimeline
+          organizations={['TEST']}
+          userCssId="INBOUND_OPS_TEAM_ADMIN_USER"
+          correspondence={props.task_added.corr}
+          tasksToDisplay={props.correspondence.tasksAddedToAppeal}
+        />
       </div>
-      <span className='tasks-added-text'>Tasks added to appeal</span>
-
-      <CorrespondenceCaseTimeline
-        organizations={['TEST']}
-        userCssId="INBOUND_OPS_TEAM_ADMIN_USER"
-        correspondence={props.task_added.corr}
-        tasksToDisplay={props.correspondence.tasksAddedToAppeal}
-      />
-
     </>
   );
 };
