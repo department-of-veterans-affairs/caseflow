e1fc1e6c65410a52373f9f2ca5e28897b83b4b6e
Sean Craig <sean.craig2@va.gov>
2024-08-13 12:29:03 -0500

    removed glamor import and updated style for CaseSearchLink

--- client/app/components/CaseSearchLink.jsx
+++ client/app/components/CaseSearchLink.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from 'glamor';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import { MagnifyingGlassIcon } from './icons/MagnifyingGlassIcon';
 import { COLORS } from '../constants/AppConstants';
 
-const containerStyling = css({
+const containerStyling = {
   display: 'inline table',
   float: 'left',
   marginRight: '3rem'
-});
-const iconStyling = css({
+};
+const iconStyling = {
   display: 'table-cell',
   padding: '0.75rem 0.25rem 0 0',
   verticalAlign: 'middle'
-});
-const textStyling = css({
+};
+const textStyling = {
   color: COLORS.PRIMARY,
   display: 'table-cell',
   fontSize: '1.7rem',
   fontWeight: 900,
   lineHeight: '4em',
   marginBottom: 0
-});
+};
 
 const CaseSearchLink = (props) => <div {...containerStyling}>
   <Link href="/search" target={props.newWindow ? '_blank' : '_self'}>
