44af9d61f326074780d938da64e7ffdaa5c33bdc
HunJerBAH <Jeremy.Hunton@va.gov>
2024-08-13 16:55:02 -0400

    removed unused attribute check from controller spec

--- spec/requests/correspondence_controller_spec.rb
+++ spec/requests/correspondence_controller_spec.rb
@@ -54,7 +54,6 @@ RSpec.describe "Correspondence Requests", :all_dbs, type: :request do
           expect(task[:attributes][:completion_date]).to eq(nil).or be_a(String)
           expect(task[:attributes][:days_waiting]).to be_a(Integer)
           expect(task[:attributes][:va_date_of_receipt]).to be_a(String)
-          expect(task[:attributes][:label]).to be_a(String)
           expect(task[:attributes][:assigned_to]).to be_a(Hash)
           expect(task[:attributes][:assigned_by]).to be_a(Hash)
         end
