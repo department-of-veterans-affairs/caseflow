60d8cbe6339b5202438c2ad3ab379a9d7469d079
Jonathan Cohen <cohen_jonathan@bah.com>
2024-08-14 15:17:26 -0400

    APPEALS-52861 moved case statement to another file to be the boolean check for constested status on an appeal.

--- app/models/decision_document.rb
+++ app/models/decision_document.rb
@@ -64,7 +64,12 @@ class DecisionDocument < CaseflowRecord
       # Below we're grabbing the boolean value at this point in time.
       # This will act as a point of truth that wont be affected by the
       # async behavior of the outcode function due to triggering jobs.
-      contested = appeal.contested_claim?
+      case appeal_type
+      when "Appeal"
+        contested = appeal.contested_claim?
+      when "LegacyAppeal"
+        contested = appeal.contested_claim
+      end
       ProcessDecisionDocumentJob.perform_later(id, contested, mail_package)
     end
   end
