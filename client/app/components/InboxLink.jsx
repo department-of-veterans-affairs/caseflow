6bd0447acc813dd7a96c5cf0564ab4e0bcc34d22
Sean Craig <sean.craig2@va.gov>
2024-08-14 12:00:04 -0500

    removed glamor import and refactored styles for InboxLink

--- client/app/components/InboxLink.jsx
+++ client/app/components/InboxLink.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { css } from 'glamor';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import { COLORS } from '../constants/AppConstants';
 import PropTypes from 'prop-types';
 
-const containerStyling = css({
+const containerStyling = {
   display: 'inline table',
   position: 'relative',
   float: 'left',
   marginRight: '3rem'
-});
-const textStyling = css({
+};
+const textStyling = {
   color: COLORS.PRIMARY,
   display: 'table-cell',
   fontSize: '1.7rem',
   lineHeight: '4em',
   paddingLeft: '0.5rem',
   marginBottom: 0
-});
-const youveGotMailStyle = css({
+};
+const youveGotMailStyle = {
   position: 'absolute',
   background: COLORS.RED_DARK,
   left: '10px',
@@ -27,14 +26,14 @@ const youveGotMailStyle = css({
   margin: '0',
   padding: '6px',
   borderRadius: '50%'
-});
+};
 
-const InboxLink = (props) => <div {...containerStyling}>
+const InboxLink = (props) => <div style={containerStyling}>
   <Link href="/inbox" target={props.newWindow ? '_blank' : '_self'}>
     <i className="fa fa-envelope-o" aria-hidden="true">
-      { props.youveGotMail && <span {...youveGotMailStyle}></span> }
+      { props.youveGotMail && <span style={youveGotMailStyle}></span> }
     </i>
-    <h3 {...textStyling}>Inbox</h3>
+    <h3 style={textStyling}>Inbox</h3>
   </Link>
 </div>;
 
