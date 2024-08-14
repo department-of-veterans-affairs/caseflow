a98b6f95fc1a6dcf378413435814d5ffc2189dd4
Jonathan Cohen <cohen_jonathan@bah.com>
2024-08-14 14:11:09 -0400

    APPEALS-52861 feature branch changes merged in with working branch and method name changes extracted.

--- app/models/prepend/va_notify/appeal_decision_mailed.rb
+++ app/models/prepend/va_notify/appeal_decision_mailed.rb
@@ -16,7 +16,11 @@ module AppealDecisionMailed
     super_return_value = super
     if processed?
       appeal.appeal_state.decision_mailed_appeal_state_update_action!
+    when "Appeal"
       template = contested ? CONTESTED_CLAIM : NON_CONTESTED_CLAIM
+    when "LegacyAppeal"
+      template = appeal.contested_claim ? CONTESTED_CLAIM : NON_CONTESTED_CLAIM
+    end
       AppellantNotification.notify_appellant(appeal, template)
     end
     super_return_value
