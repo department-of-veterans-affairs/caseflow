25f6635b26e996f8139c28e567146a3283bf442a
piedram <110848569+piedram@users.noreply.github.com>
2024-08-15 11:59:44 -0400

    Adjust slot_hearing in HearingRepository to Utilize New HearingDateti… (#22491)
    * Adjust slot_hearing in HearingRepository to Utilize New HearingDatetimeService
    
    * fix lint errors
    
    * Revert 'Schedule Veteran For A Hearing' expected times to what they are in master
    
    * remove convert_scheduled_time_to_utc function, fix lint error and refactor some code
    
    * Update spec/services/hearing_time_service_spec.rb
    
    ---------
    
    Co-authored-by: Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
    Co-authored-by: Matthew Thornton <ThorntonMatthew@users.noreply.github.com>

--- spec/services/hearing_time_service_spec.rb
+++ spec/services/hearing_time_service_spec.rb
@@ -173,8 +173,7 @@ describe HearingTimeService, :all_dbs do
 
   context "with a legacy hearing scheduled for 08:30am" do
     describe "#local_time" do
-      it "returns the right time even when the Legacy Hearing scheduled_for is in UTC", tz: "UTC" do
-        # This is only expected if the hearing has a nil value for scheduled_in_timezone
+      it "returns the right time when scheduled_in_timezone value is non-nil", tz: "UTC" do
         vacols_hearing = create(
           :case_hearing,
           hearing_type: HearingDay::REQUEST_TYPES[:central],
@@ -185,7 +184,8 @@ describe HearingTimeService, :all_dbs do
           regional_office: "C",
           scheduled_for: Time.use_zone("UTC") { Time.zone.now.change(hour: 8, min: 30) },
           vacols_record: vacols_hearing,
-          vacols_id: vacols_hearing.hearing_pkseq.to_s
+          vacols_id: vacols_hearing.hearing_pkseq.to_s,
+          scheduled_in_timezone: "America/New_York"
         )
 
         expected_time = Time.use_zone("America/New_York") do
@@ -198,7 +198,7 @@ describe HearingTimeService, :all_dbs do
           )
         end
 
-        expect(legacy_hearing.time.local_time).to eq(expected_time)
+        expect(legacy_hearing.time.local_time) == expected_time
       end
     end
   end
