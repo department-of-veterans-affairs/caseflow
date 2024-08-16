4f46b1ff206667c74579a1669abf8142536b3567
Dev-KRedd <khalin.redding@va.gov>
2024-08-16 10:36:52 -0400

    WIP

--- client/app/queue/correspondence/CorrespondenceCaseTimeline.jsx
+++ client/app/queue/correspondence/CorrespondenceCaseTimeline.jsx
@@ -1,56 +1,35 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CorrespondenceTaskRows from './CorrespondenceTaskRows';
 import Alert from '../../components/Alert';
-import { setTaskNotRelatedToAppealBanner, correspondenceInfo } from './correspondenceDetailsReducer/correspondenceDetailsActions';
+import {
+  setTaskNotRelatedToAppealBanner,
+  setShowActionsDropdown } from './correspondenceDetailsReducer/correspondenceDetailsActions';
+
 const CorrespondenceCaseTimeline = (props) => {
 
-  const { taskNotRelatedToAppealBanner, correspondenceInfo} = props;
-
-  const getAvailableActions = (task) => {
-    if (props.organizations.includes(task.assigned_to) || props.userCssId === task.assigned_to) {
-      return task.available_actions;
-    }
-
-    return [];
-
-  };
-
-  const formatTaskData = () => {
-    return (props.correspondence.tasksUnrelatedToAppeal.map((task) => {
-      return {
-        assignedOn: task.assigned_at,
-        assignedTo: task.assigned_to,
-        label: task.type,
-        instructions: task.instructions,
-        availableActions: getAvailableActions(task),
-        uniqueId: task.uniqueId
-      };
-    }));
-  };
-
-  useEffect(() => {
-    // Handle document search position
-    console.log(formatTaskData());
-    console.log(props.correspondenceInfo.tasksUnrelatedToAppeal);
-    console.log(taskNotRelatedToAppealBanner);
-
-  }, []);
+  const { taskNotRelatedToAppealBanner, correspondenceInfo } = props;
 
   return (
     <React.Fragment>
-      { (Object.keys(taskNotRelatedToAppealBanner).length > 0) && (<Alert
-        type={taskNotRelatedToAppealBanner.type}>
-        {taskNotRelatedToAppealBanner.message}
-      </Alert>)}
+      { (Object.keys(taskNotRelatedToAppealBanner).length > 0) && (
+        <div className="correspondence-details-alert-banner">
+
+          <Alert
+            type={taskNotRelatedToAppealBanner.type}>
+            {taskNotRelatedToAppealBanner.message}
+          </Alert>
+        </div>
+
+      )}
       <table id="case-timeline-table" summary="layout table">
         <tbody>
           <CorrespondenceTaskRows
             organizations={props.organizations}
             correspondence={props.correspondence}
-            taskList={formatTaskData()}
+            taskList={correspondenceInfo.tasksUnrelatedToAppeal}
             statusSplit
           />
         </tbody>
@@ -62,6 +41,8 @@ const CorrespondenceCaseTimeline = (props) => {
 CorrespondenceCaseTimeline.propTypes = {
   loadCorrespondence: PropTypes.func,
   correspondence: PropTypes.object,
+  correspondenceInfo: PropTypes.object,
+  taskNotRelatedToAppealBanner: PropTypes.object,
   organizations: PropTypes.array,
   userCssId: PropTypes.string
 };
@@ -69,13 +50,16 @@ CorrespondenceCaseTimeline.propTypes = {
 const mapStateToProps = (state) => ({
   correspondences: state.intakeCorrespondence.correspondences,
   taskNotRelatedToAppealBanner: state.correspondenceDetails.bannerAlert,
-  correspondenceInfo: state.correspondenceDetails.correspondenceInfo
+  correspondenceInfo: state.correspondenceDetails.correspondenceInfo,
+  tasksUnrelatedToAppeal: state.correspondenceDetails.tasksUnrelatedToAppeal,
+  showActionsDropdown: state.correspondenceDetails.showActionsDropdown,
+  dubuggo: state.correspondenceDetails.dubuggo,
 });
 
 const mapDispatchToProps = (dispatch) => (
   bindActionCreators({
     setTaskNotRelatedToAppealBanner,
-    correspondenceInfo
+    setShowActionsDropdown
   }, dispatch)
 );
 
