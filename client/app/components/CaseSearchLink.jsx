c6b13687cd9a386bcd82ce4c0a94bb87e8e39b8e
Sean Craig <110493538+seancva@users.noreply.github.com>
2024-08-19 14:52:29 -0500

    SeanC/APPEALS-54445 (#22470)
    * removed glamor import and updated style for AmaIssueList
    
    * removed glamor import and updated style for BareList
    
    * removed glamor import and updated style for CaseSearchLink
    
    * removed glamor import and updated style for CFRichTextEditor
    
    * removed glamor import and updated style for ContentSection
    
    * removed glamor import and updated style for CopyTextButton
    
    * added style attribute in ContentSection
    
    * added style attribute in ContentSection
    
    * added style attribute to CopyTextButton
    
    * added style attribute to CaseSearchLink
    
    * added style attribute to CFRichTextEditor
    
    * added style attribute to BareList
    
    * added style attribute to AmaIssueList
    
    * added className to propTypes to make linter happy
    
    * added toolbar and value to propTypes in CFRichTextEditor to make linter happy
    
    * updated snapshot for DailyDocketGuessLinkSection
    
    * updated snapshot for Details
    
    * updated snapshot for DetailsForm
    
    * updated snapshot for HearingLinks
    
    * updated snapshot for VirtualHearingFields
    
    * updated snapshot for Fields
    
    * updated snapshot for CaseHearingsDetail
    
    * updated snapshot for NotificationsView
    
    * cleaning up some lint

--- client/app/components/CaseSearchLink.jsx
+++ client/app/components/CaseSearchLink.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from 'glamor';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import { MagnifyingGlassIcon } from './icons/MagnifyingGlassIcon';
 import { COLORS } from '../constants/AppConstants';
 
-const containerStyling = css({
+const containerStyling = {
   display: 'inline table',
   float: 'left',
   marginRight: '3rem'
-});
-const iconStyling = css({
+};
+const iconStyling = {
   display: 'table-cell',
   padding: '0.75rem 0.25rem 0 0',
   verticalAlign: 'middle'
-});
-const textStyling = css({
+};
+const textStyling = {
   color: COLORS.PRIMARY,
   display: 'table-cell',
   fontSize: '1.7rem',
   fontWeight: 900,
   lineHeight: '4em',
   marginBottom: 0
-});
+};
 
-const CaseSearchLink = (props) => <div {...containerStyling}>
+const CaseSearchLink = (props) => <div style={containerStyling}>
   <Link href="/search" target={props.newWindow ? '_blank' : '_self'}>
-    <span {...iconStyling}><MagnifyingGlassIcon color={COLORS.PRIMARY} size={24} /></span>
-    <p {...textStyling}>Search cases</p>
+    <span style={iconStyling}><MagnifyingGlassIcon color={COLORS.PRIMARY} size={24} /></span>
+    <p style={textStyling}>Search cases</p>
   </Link>
 </div>;
 
