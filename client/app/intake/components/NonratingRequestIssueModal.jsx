05e7c8e69d756366d66c05b8df1ec4b06c0c38eb
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-08-19 21:24:19 -0400

    set back to false

--- client/app/intake/components/NonratingRequestIssueModal.jsx
+++ client/app/intake/components/NonratingRequestIssueModal.jsx
@@ -243,7 +243,7 @@ class NonratingRequestIssueModal extends React.Component {
 
     const options = intakeData.activeNonratingRequestIssues.
       filter(() => {
-        return category;
+        return false;
       }).
       map((issue) => {
         return {
