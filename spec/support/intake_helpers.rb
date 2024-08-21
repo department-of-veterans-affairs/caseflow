5b20633b3341fd18a887daca7439cb1007c6fd16
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-08-21 08:34:19 -0400

    ignored metrics abc size

--- spec/support/intake_helpers.rb
+++ spec/support/intake_helpers.rb
@@ -322,6 +322,7 @@ module IntakeHelpers
   end
 
   # rubocop:disable Metrics/ParameterLists
+  # rubocop:disable Metrics/AbcSize
   def add_intake_nonrating_issue(
     benefit_type: "Compensation",
     category: "Active Duty Adjustments",
@@ -358,6 +359,7 @@ module IntakeHelpers
     safe_click ".add-issue"
   end
   # rubocop:enable Metrics/ParameterLists
+  # rubocop:enable Metrics/AbcSize
 
   def add_intake_vha_issue(
     benefit_type: "Veterans Health Administration",
