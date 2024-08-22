c6fc441ca29d48a55141c96377871ba9e70a472b
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-08-22 13:51:53 -0400

    updated spec to check on each category

--- spec/feature/intake/nonrating_request_issue_modal_spec.rb
+++ spec/feature/intake/nonrating_request_issue_modal_spec.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'json'
+
+ISSUE_CATEGORIES = JSON.parse(File.read(Rails.root.join('client', 'constants', 'ISSUE_CATEGORIES.json')))
+
 feature "Nonrating Request Issue Modal", :postgres do
   include IntakeHelpers
 
@@ -34,7 +38,7 @@ feature "Nonrating Request Issue Modal", :postgres do
     expect(page).to_not have_content("PACT Act")
   end
 
-    # rubocop:disable Metrics/ParameterLists
+  # rubocop:disable Metrics/ParameterLists
   # rubocop:disable Metrics/AbcSize
   def add_intake_for_nonrating_issue(
     benefit_type: "Compensation",
@@ -74,7 +78,7 @@ feature "Nonrating Request Issue Modal", :postgres do
   # rubocop:enable Metrics/ParameterLists
   # rubocop:enable Metrics/AbcSize
 
-  def test_issue_categories(decision_review_type:, benefit_type:, included_category:, mst_pact:)
+  def test_issue_categories(decision_review_type:, benefit_type:, included_categories:, mst_pact:)
     case decision_review_type
     when "higher_level_review"
       start_higher_level_review(
@@ -90,18 +94,22 @@ feature "Nonrating Request Issue Modal", :postgres do
       start_appeal(veteran)
     end
 
-    visit_and_test_categories(included_category, benefit_type, mst_pact)
+    visit_and_test_categories(included_categories, benefit_type, mst_pact)
   end
 
-  def visit_and_test_categories(included_category, benefit_type, mst_pact)
+  def visit_and_test_categories(included_categories, benefit_type, mst_pact)
     visit "/intake"
     click_intake_continue
     click_intake_add_issue
     mst_pact ? check_for_mst_pact : check_for_no_mst_pact
     click_intake_nonrating_category_dropdown
-    expect(page).to have_content(included_category)
+
+    included_categories.each do |category|
+      expect(page).to have_content(category)
+    end
+
     add_intake_for_nonrating_issue(
-      category: included_category,
+      category: included_categories.first,
       description: "I am a description",
       date: Time.zone.today.mdY
     )
@@ -113,7 +121,7 @@ feature "Nonrating Request Issue Modal", :postgres do
     sleep 1
 
     expect(RequestIssue.find_by(
-             nonrating_issue_category: included_category
+             nonrating_issue_category: included_categories.first
            )).to_not be_nil
   end
 
@@ -122,21 +130,21 @@ feature "Nonrating Request Issue Modal", :postgres do
       test_issue_categories(
         decision_review_type: "higher_level_review",
         benefit_type: "pension",
-        included_category: "Eligibility | Wartime Service",
+        included_categories: ISSUE_CATEGORIES["pension"],
         mst_pact: false
       )
 
       test_issue_categories(
         decision_review_type: "higher_level_review",
         benefit_type: "vha",
-        included_category: "Eligibility for Dental Treatment",
+        included_categories: ISSUE_CATEGORIES["vha"],
         mst_pact: false
       )
 
       test_issue_categories(
         decision_review_type: "supplemental_claim",
         benefit_type: "fiduciary",
-        included_category: "Appointment of a Fiduciary (38 CFR 13.100)",
+        included_categories: ISSUE_CATEGORIES["fiduciary"],
         mst_pact: false
       )
     end
@@ -147,7 +155,7 @@ feature "Nonrating Request Issue Modal", :postgres do
       test_issue_categories(
         decision_review_type: "appeal",
         benefit_type: "not applicable to appeal",
-        included_category: "Unknown Issue Category",
+        included_categories: ["Unknown Issue Category"],
         mst_pact: true
       )
     end
