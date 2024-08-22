602ef07ad766c5c49102396a5ced29406e191848
ramon-chavez <Ramon.Chavez@va.gov>
2024-08-22 13:54:18 -0400

    Add comment

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,9 +2,8 @@ import { Preview } from '@storybook/react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import '../app/styles/app.scss';
 
-// Generate arguments for any props that start with "on" (onClick, onSubmit, etc)
-// This *should* work, but isn't currently on beta.21
-// addParameters({ actions: { argTypesRegex: '^on.*' } });
+//Preview has changed the way addParameters are handle, and addParameters are now
+//moved inside a new object called parameters
 const tags = ['autodocs'];
 
 const preview = {
