12cc74f45589f59c56a5ed4c6e35471f04e8a259
lauren-e-thompson <lauren.thompson@va.gov>
2024-08-22 14:15:27 -0400

    Remove unnecessary import

--- client/app/test/loadTest/LoadTest.jsx
+++ client/app/test/loadTest/LoadTest.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
 import AppFrame from '../../components/AppFrame';
 import { BrowserRouter } from 'react-router-dom';
