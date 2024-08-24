9c24ee07d17b9f87fda9c51272fc0db43023ce28
raymond-hughes <raymond.hughes2@va.gov>
2024-08-24 15:00:06 -0400

    Adding instruments

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -38,16 +38,17 @@ if !Rails.env.development? && !Rails.env.test? && !Rails.env.demo?
     c.use 'OpenTelemetry::Instrumentation::Rails'
 
     # c.use 'OpenTelemetry::Instrumentation::ActionPack'
-    # c.use 'OpenTelemetry::Instrumentation::ActionView'
-    # c.use 'OpenTelemetry::Instrumentation::ActiveJob'
-    # c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
-    # c.use 'OpenTelemetry::Instrumentation::AwsSdk'
-    # c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
-    # c.use 'OpenTelemetry::Instrumentation::Faraday'
-    # c.use 'OpenTelemetry::Instrumentation::HttpClient'
-    # c.use 'OpenTelemetry::Instrumentation::Redis'
-    # c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
-    # c.use 'OpenTelemetry::Instrumentation::PG'
+
+    c.use 'OpenTelemetry::Instrumentation::ActionView'
+    c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+    c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
+    c.use 'OpenTelemetry::Instrumentation::AwsSdk'
+    c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
+    c.use 'OpenTelemetry::Instrumentation::Faraday'
+    c.use 'OpenTelemetry::Instrumentation::HttpClient'
+    c.use 'OpenTelemetry::Instrumentation::Redis'
+    c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
+    c.use 'OpenTelemetry::Instrumentation::PG'
 
     Rails.logger.info("Loaded instruments")
 
