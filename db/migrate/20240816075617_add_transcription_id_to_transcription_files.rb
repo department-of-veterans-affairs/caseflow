09ff18974fa165c6959025ab87103378a00e461c
mounikamy <Mounika.Myana@va.gov>
2024-08-25 08:02:28 -0400

    Removed outdated transcription id field

--- db/migrate/20240816075617_add_transcription_id_to_transcription_files.rb
+++
@@ -1,8 +0,0 @@
-class AddTranscriptionIdToTranscriptionFiles < ActiveRecord::Migration[6.0]
-  disable_ddl_transaction!
-
-  def change
-    add_column :transcription_files, :transcription_id, :bigint, comment: 'ID of the transcription record'
-    add_index :transcription_files, :transcription_id, algorithm: :concurrently
-  end
-end
