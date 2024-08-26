2bdcf8d0a5f765a524cba4779bf4bc7e59f860eb
Calvin <Calvin.Costa@va.gov>
2024-08-26 15:16:44 -0400

    fixed remaining rspecs by adding to stats

--- spec/models/concerns/by_docket_date_distribution_spec.rb
+++ spec/models/concerns/by_docket_date_distribution_spec.rb
@@ -239,6 +239,8 @@ describe ByDocketDateDistribution, :all_dbs do
       expect(judge_stats).to have_key(:settings)
 
       @new_acd.dockets.each_key do |sym|
+        next if sym == :aoj_legacy
+
         # priority stats
         expect(ama_statistics).to have_key("#{sym}_priority_stats".to_sym)
 
