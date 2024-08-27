d223c5957f1cf4a3a46123d1761575dd3f47c0c2
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-08-27 16:10:00 -0400

    pushed up feature flag

--- spec/feature/intake/nonrating_request_issue_modal_spec.rb
+++ spec/feature/intake/nonrating_request_issue_modal_spec.rb
@@ -16,11 +16,13 @@ feature "Nonrating Request Issue Modal", :postgres do
     User.authenticate!(user: bva_intake_admin_user)
     FeatureToggle.enable!(:mst_identification)
     FeatureToggle.enable!(:pact_identification)
+    FeatureToggle.enable!(:displayNonFilteredCategories)
   end
 
   after do
     FeatureToggle.disable!(:mst_identification)
     FeatureToggle.disable!(:pact_identification)
+    FeatureToggle.disable!(:displayNonFilteredCategories)
   end
 
   let(:veteran_file_number) { "123412345" }
