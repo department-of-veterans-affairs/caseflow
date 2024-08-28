143b9a54443d319365a9a2a8d1ee10d9f6975414
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-08-28 08:40:46 -0400

    Monkeypatch pending migration task

---
+++ config/initializers/parallel_tests.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module ParallelTests
+  module Tasks
+    class << self
+      # Prevents attempting to establish and consult a schema_migrations table in our VACOLS database
+      #  upon initializing the test suite.
+      def abort_if_pending_migrations
+        pending_migrations = ActiveRecord::Base.configurations.configs_for(
+          env_name: ActiveRecord::Tasks::DatabaseTasks.env
+        ).flat_map do |db_config|
+          next if db_config.configuration_hash[:username].match?(/VACOLS/)
+
+          ActiveRecord::Base.establish_connection(db_config)
+
+          ActiveRecord::Base.connection.migration_context.open.pending_migrations
+        end
+
+        if pending_migrations.any?
+          Rails.logger.info(
+            "You have #{pending_migrations.size} pending #{pending_migrations.size > 1 ? 'migrations:' : 'migration:'}"
+          )
+          pending_migrations.each do |pending_migration|
+            Rails.logger.info("   #{pending_migration.version} #{pending_migration.name}")
+          end
+          abort %{Run `bin/rails db:migrate` to update your database then try again.}
+        end
+      ensure
+        ActiveRecord::Base.establish_connection(ActiveRecord::Tasks::DatabaseTasks.env.to_sym)
+      end
+
+      def check_for_pending_migrations
+        byebug
+        ["db:abort_if_pending_migrations", "app:db:abort_if_pending_migrations"].each do |abort_migrations|
+          if Rake::Task.task_defined?(abort_migrations)
+            abort_if_pending_migrations
+            break
+          end
+        end
+      end
+    end
+  end
+end
