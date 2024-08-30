6652591ca931487216c3b033780ee6bf06a01ae6
KonstantinShevtsov <Konstantin.Shevtsov@va.gov>
2024-08-30 18:05:15 -0400

    attributes_from_intake_data method overrided

--- app/models/request_issues_update_event.rb
+++ app/models/request_issues_update_event.rb
@@ -5,22 +5,22 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   # RequestIssuesUpdateEvent.new(
   #   user: user,
   #   review: review,
-  #   added_issues_data: parser.added_issues,
-  #   removed_issues_data: parser.removed_issues,
-  #   edited_issues_data: parser.updated_issues,
-  #   withdrawn_issues_data: parser.withdrawn_issues
-  #   eligible_to_ineligible_issues_data: parser.eligible_to_ineligible_issues
-  #   ineligible_to_eligible_issues_data: parser.ineligible_to_eligible_issues
-  #   ineligible_to_ineligible_issues_data: parser.ineligible_to_ineligible_issues
+  #   added_issue_data: parser.added_issues,
+  #   removed_issue_data: parser.removed_issues,
+  #   edited_issue_data: parser.updated_issues,
+  #   withdrawn_issue_data: parser.withdrawn_issues
+  #   eligible_to_ineligible_issue_data: parser.eligible_to_ineligible_issues
+  #   ineligible_to_eligible_issue_data: parser.ineligible_to_eligible_issues
+  #   ineligible_to_ineligible_issue_data: parser.ineligible_to_ineligible_issues
   # )
 
-  attr_writer :added_issues_data
-  attr_writer :removed_issues_data
-  attr_writer :edited_issues_data
-  attr_writer :withdrawn_issues_data
-  attr_writer :eligible_to_ineligible_issues_data
-  attr_writer :ineligible_to_eligible_issues_data
-  attr_writer :ineligible_to_ineligible_issues_data
+  attr_writer :added_issue_data
+  attr_writer :removed_issue_data
+  attr_writer :edited_issue_data
+  attr_writer :withdrawn_issue_data
+  attr_writer :eligible_to_ineligible_issue_data
+  attr_writer :ineligible_to_eligible_issue_data
+  attr_writer :ineligible_to_ineligible_issue_data
 
   def perform!
     return false unless validate_before_perform
@@ -76,19 +76,19 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   end
 
   def calculate_edited_issues
-    calculate_issues(@edited_issues_data)
+    calculate_issues(@edited_issue_data)
   end
 
   def calculate_added_issues
-    calculate_issues(@added_issues_data)
+    calculate_issues(@added_issue_data)
   end
 
   def calculate_withdrawn_issues
-    calculate_issues(@withdrawn_issues_data)
+    calculate_issues(@withdrawn_issue_data)
   end
 
   def calculate_removed_issues
-    @removed_issues_data.map do |issue_data|
+    @removed_issue_data.map do |issue_data|
       review.request_issues.find(issue_data[:id])
     end
   end
@@ -110,8 +110,60 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
     # that will be called from future UpdateRequestIssues service class
     # if not we HAVE TO update or override from_intake_data for new
     # attribute vbms_id
+    # RequestIssue.from_intake_data(issue_data, decision_review: review)
     RequestIssue.from_intake_data(issue_data, decision_review: review)
   end
+
+  def from_intake_data(data, decision_review: nil)
+    attrs = attributes_from_intake_data(data)
+    attrs = attrs.merge(decision_review: decision_review) if decision_review
+
+    RequestIssue.new(attrs).tap(&:validate_eligibility!)
+  end
+
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def attributes_from_intake_data(data)
+    contested_issue_present = attributes_look_like_contested_issue?(data)
+    issue_text = (data[:ri_is_unidentified] || data[:ri_verified_unidentified_issue]) ? data[:ri_decision_text] : nil
+
+    {
+      benefit_type: data[:ri_benefit_type],
+      closed_at: data[:ri_closed_at],
+      closed_status: data[:ri_closed_status],
+      contention_reference_id: data[:ri_contention_reference_id],
+      contested_decision_issue_id: data[:ri_contested_decision_issue_id],
+      contested_rating_issue_reference_id: data[:ri_contested_rating_issue_reference_id],
+      contested_rating_issue_diagnostic_code: data[:ri_contested_rating_issue_diagnostic_code],
+      contested_rating_decision_reference_id: data[:ri_rating_decision_reference_id],
+      contested_issue_description: contested_issue_present ? data[:ri_decision_text] : nil,
+      contested_rating_issue_profile_date: data[:ri_contested_rating_issue_profile_date],
+      nonrating_issue_description: data[:ri_nonrating_issue_category] ? data[:ri_decision_text] : nil,
+      unidentified_issue_text: issue_text,
+      decision_date: data[:ri_decision_date],
+      nonrating_issue_category: data[:ri_nonrating_issue_category],
+      is_unidentified: data[:ri_is_unidentified],
+      untimely_exemption: data[:ri_untimely_exemption],
+      untimely_exemption_notes: data[:ri_untimely_exemption_notes],
+      ramp_claim_id: data[:ri_ramp_claim_id],
+      vacols_id: data[:ri_vacols_id],
+      vacols_sequence_id: data[:ri_vacols_sequence_id],
+      ineligible_reason: data[:ri_ineligible_reason],
+      ineligible_due_to_id: data[:ri_ineligible_due_to_id],
+      vbms_id: data[:ri_vbms_id],
+      type: data[:ri_type],
+      veteran_participant_id: data[:ri_veteran_participant_id],
+      rating_issue_associated_at: data[:ri_rating_issue_associated_at],
+      nonrating_issue_bgs_source: data[:ri_nonrating_issue_bgs_source],
+      nonrating_issue_bgs_id: data[:ri_nonrating_issue_bgs_id]
+    }
+  end
+
+  def attributes_look_like_contested_issue?(data)
+    data[:ri_rating_issue_reference_id] ||
+      data[:ri_contested_decision_issue_id] ||
+      data[:ri_rating_decision_reference_id]
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
   def fetch_withdrawn_issues
     RequestIssue.where(id: withdrawn_request_issue_ids)
@@ -120,7 +172,7 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   def process_withdrawn_issues!
     return if withdrawn_issues.empty?
 
-    @withdrawn_issues_data.each do |withdrawn_issue|
+    @withdrawn_issue_data.each do |withdrawn_issue|
       request_issue = RequestIssue.find(withdrawn_issue[:id].to_s)
       request_issue.withdraw!(withdrawn_issue[:closed_at])
     end
@@ -140,7 +192,7 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   def process_eligible_to_ineligible_issues!
     return if eligible_to_ineligible_issues.empty?
 
-    eligible_to_ineligible_issues_data.each do |eligible_to_ineligible_issue|
+    eligible_to_ineligible_issue_data.each do |eligible_to_ineligible_issue|
       request_issue = RequestIssue.find(eligible_to_ineligible_issue[:id].to_s)
       next if !request_issue.ineligible_reason.nil?
 
@@ -152,7 +204,7 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   def process_ineligible_to_eligible_issues!
     return if ineligible_to_eligible_issues.empty?
 
-    ineligible_to_eligible_issues_data.each do |ineligible_to_eligible_issue|
+    ineligible_to_eligible_issue_data.each do |ineligible_to_eligible_issue|
       request_issue = RequestIssue.find(ineligible_to_eligible_issue[:id].to_s)
       next if request_issue.ineligible_reason.nil?
 
@@ -163,7 +215,7 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
   def process_ineligible_to_ineligible_issues!
     return if ineligible_to_ineligible_issues.empty?
 
-    ineligible_to_ineligible_issues_data.each do |ineligible_to_ineligible_issue|
+    ineligible_to_ineligible_issue_data.each do |ineligible_to_ineligible_issue|
       request_issue = RequestIssue.find(ineligible_to_ineligible_issue[:id].to_s)
       next if request_issue.ineligible_reason.nil?
 
