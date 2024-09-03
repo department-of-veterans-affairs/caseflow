d008bb8dabd6c7dd9e5041c0c4b3fbfb457d0bf8
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-09-03 10:06:37 -0400

    added audit service and call to update evenr records

--- app/services/events/decision_review_updated.rb
+++ app/services/events/decision_review_updated.rb
@@ -10,6 +10,11 @@ class Events::DecisionReviewUpdated
         event = find_or_create_event(consumer_event_id)
         # Update the Event after all backfills have completed
         event.update!(completed_at: Time.now.in_time_zone, error: nil, info: {})
+
+        # loop through and update request issues
+        request_issues.each do |request_issue|
+          DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: update_type).call
+        end
       end
     end
   end
