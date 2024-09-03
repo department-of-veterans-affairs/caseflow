8e562a47c1bd13ad34a5b6af90d04c4574f4293c
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-09-03 10:17:27 -0400

    logic for update type

--- app/services/events/decision_review_updated.rb
+++ app/services/events/decision_review_updated.rb
@@ -4,16 +4,18 @@ class Events::DecisionReviewUpdated
   class << self
     def update!(params, headers, payload)
       consumer_event_id = params[:consumer_event_id]
-      reference_id = params[:reference_id]
 
       ActiveRecord::Base.transaction do
         event = find_or_create_event(consumer_event_id)
         # Update the Event after all backfills have completed
         event.update!(completed_at: Time.now.in_time_zone, error: nil, info: {})
 
-        # loop through and update request issues
         request_issues.each do |request_issue|
-          DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: update_type).call
+          if request_issue.updated_at > request_issue.created_at
+            DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: "U").call
+          elsif request_issue.destroyed?
+            DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: "D").call
+          end
         end
       end
     end
