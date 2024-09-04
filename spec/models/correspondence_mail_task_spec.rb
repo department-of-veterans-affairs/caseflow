18234674d1c125d408a8b59c90393c496c723dfc
Dev-KRedd <khalin.redding@va.gov>
2024-09-04 19:41:19 -0400

    Adding more to the accessing the task portion of the test.

--- spec/models/correspondence_mail_task_spec.rb
+++ spec/models/correspondence_mail_task_spec.rb
@@ -55,16 +55,31 @@ RSpec.describe CorrespondenceMailTask, type: :model do
       ]
     end
 
-    user_array.each do |user|
-      context "for #{user[:class]} assigned to #{user[:assigned_to]}" do
-        let(:task) { user[:class].create!(appeal: root_task.appeal, parent: root_task, assigned_to: user[:assigned_to]) }
+    user_array.each do |example|
+      context "for #{example[:class]} assigned to #{example[:assigned_to]}" do
+        let(:task) { example[:class].create!(appeal: root_task.appeal, parent: root_task, assigned_to: example[:assigned_to]) }
+        let(:expected_actions) do
+          [
+            Constants.TASK_ACTIONS.CANCEL_CORRESPONDENCE_TASK.to_h,
+            Constants.TASK_ACTIONS.COMPLETE_CORRESPONDENCE_TASK.to_h,
+            Constants.TASK_ACTIONS.ASSIGN_CORR_TASK_TO_TEAM.to_h,
+            Constants.TASK_ACTIONS.CHANGE_TASK_TYPE.to_h
+          ]
+        end
 
         before do
-          allow_any_instance_of(User).to receive(:organization).and_return(user[:assigned_to])
+          allow_any_instance_of(User).to receive(:organization).and_return(example[:assigned_to])
+          # Assign user to organization
+          example[:assigned_to].add_user(litigation_user)
+        end
+
+        after do
+          # Remove user from organization
+          OrganizationsUser.remove_user_from_organization(litigation_user, example[:assigned_to])
         end
 
         it "allows the assigned user to access task actions" do
-          expect(task.available_actions(litigation_user)).not_to be_empty
+          expect(task.available_actions(litigation_user)).to eq(expected_actions)
         end
 
         it "does not allow unassigned users to access task actions" do
