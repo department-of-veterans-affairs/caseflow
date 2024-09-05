450a530bddfbe12151b27fca69552d3b662cd6a0
Jonathan Tsang <jonathan.tsang@va.gov>
2024-09-05 15:46:27 -0400

    rspec

--- app/services/events/decision_review_updated/update_informal_conference.rb
+++ app/services/events/decision_review_updated/update_informal_conference.rb
@@ -9,12 +9,13 @@ class Events::DecisionReviewUpdated::UpdateInformalConference
       parser = params[:parser]
       if parser.detail_type == "HigherLevelReview"
         # fetch the EPE and use it to get the source (i.e. HLR)
-        hlr = EndProductEstablishment.find_by(reference_id: parser.end_product_establishments_reference_id).source
+        hlr = EndProductEstablishment.find_by(reference_id: parser.end_product_establishments_reference_id)&.source
         hlr.update!(
           informal_conference: parser.claim_review_informal_conference,
           same_office: parser.claim_review_same_office
         )
         EventRecord.create!(event: event, evented_record: hlr)
+        hlr
       end
     rescue StandardError => error
       raise Caseflow::Error::DecisionReviewUpdatedInformalConferenceError, error.message
