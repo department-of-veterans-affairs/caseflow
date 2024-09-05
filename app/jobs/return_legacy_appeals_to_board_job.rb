36a547521715ef2635bee61fb3739ce908e2671d
SHarshain <spoosa@razortalent.com>
2024-09-05 16:59:18 -0400

    APPEALS-57428. Job stops if the returned appeals is nil

--- app/jobs/return_legacy_appeals_to_board_job.rb
+++ app/jobs/return_legacy_appeals_to_board_job.rb
@@ -10,23 +10,25 @@ class ReturnLegacyAppealsToBoardJob < CaseflowJob
   NO_RECORDS_FOUND_MESSAGE = [Constants.DISTRIBUTION.no_records_moved_message].freeze
 
   def perform
-    begin
-      returned_appeal_job = create_returned_appeal_job
+    catch(:abort) do
+      begin
+        returned_appeal_job = create_returned_appeal_job
 
-      appeals, moved_appeals = eligible_and_moved_appeals
+        appeals, moved_appeals = eligible_and_moved_appeals
 
-      check_appeals_available(moved_appeals, returned_appeal_job)
+        check_appeals_available(moved_appeals, returned_appeal_job)
 
-      complete_returned_appeal_job(returned_appeal_job, "Job completed successfully", moved_appeals)
+        complete_returned_appeal_job(returned_appeal_job, "Job completed successfully", moved_appeals)
 
-      # The rest of your code continues here
-      # Filter the appeals and send the filtered report
-      @filtered_appeals = filter_appeals(appeals, moved_appeals)
-      send_job_slack_report(slack_report)
-    rescue StandardError => error
-      handle_error(error, returned_appeal_job)
-    ensure
-      metrics_service_report_runtime(metric_group_name: "return_legacy_appeals_to_board_job")
+        # The rest of your code continues here
+        # Filter the appeals and send the filtered report
+        @filtered_appeals = filter_appeals(appeals, moved_appeals)
+        send_job_slack_report(slack_report)
+      rescue StandardError => error
+        handle_error(error, returned_appeal_job)
+      ensure
+        metrics_service_report_runtime(metric_group_name: "return_legacy_appeals_to_board_job")
+      end
     end
   end
 
@@ -171,6 +173,7 @@ class ReturnLegacyAppealsToBoardJob < CaseflowJob
     if moved_appeals.nil?
       complete_returned_appeal_job(returned_appeal_job, Constants.DISTRIBUTION.no_records_moved_message, [])
       send_job_slack_report(NO_RECORDS_FOUND_MESSAGE)
+      throw(:abort)
     end
   end
 
