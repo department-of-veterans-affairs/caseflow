bbcdfca605cfe1f5eb51e8127d4307d51f7b1fe7
AdamShawBAH <adam.shaw@va.gov>
2024-09-05 11:26:38 -0400

    fixed user_organization_spec.rb

--- spec/feature/queue/user_organization_spec.rb
+++ spec/feature/queue/user_organization_spec.rb
@@ -50,7 +50,9 @@ RSpec.feature "User organization", :postgres do
       expect(page).to have_content(format(COPY::USER_MANAGEMENT_PAGE_TITLE, organization.name))
 
       find(".cf-select__control", text: COPY::USER_MANAGEMENT_ADD_USER_TO_ORG_DROPDOWN_TEXT).click
-      fill_in("Add user", with: user_with_role.css_id)
+
+      set_field_value_with_delay('#add-user', user_with_role.css_id)
+
       expect(page).to have_content(user_with_role.full_name)
       expect(page).to_not have_content(user_without_role.full_name)
 
@@ -161,4 +163,15 @@ RSpec.feature "User organization", :postgres do
       expect(page).to have_content("Reviews needing action")
     end
   end
+end
+
+def set_field_value_with_delay(selector, value)
+  area = find(selector)
+  area.click
+
+  value.each_char do |char|
+    area.send_keys char
+    sleep 0.2
+  end
+  area.send_keys :space
 end
