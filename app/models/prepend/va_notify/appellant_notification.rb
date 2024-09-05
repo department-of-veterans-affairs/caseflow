3015bab4b71ce0a24c54b5e522405f228380b4e8
msteele <Marc.Steele@va.gov>
2024-09-05 11:02:40 -0400

    APPEALS-57351 add template name check to handle errors

--- app/models/prepend/va_notify/appellant_notification.rb
+++ app/models/prepend/va_notify/appellant_notification.rb
@@ -35,9 +35,9 @@ module AppellantNotification
 
   class NoAppealError < StandardError; end
 
-  def self.handle_errors(appeal)
+  def self.handle_errors(appeal, template_name)
     fail NoAppealError if appeal.nil?
-    fail InactiveAppealError, appeal.external_id if !appeal.active?
+    fail InactiveAppealError, appeal.external_id if template_name == "Quarterly Notification" && !appeal.active?
 
     message_attributes = {}
     message_attributes[:appeal_type] = appeal.class.to_s
@@ -79,7 +79,7 @@ module AppellantNotification
   end
 
   def self.create_payload(appeal, template_name, appeal_status = nil)
-    message_attributes = AppellantNotification.handle_errors(appeal)
+    message_attributes = AppellantNotification.handle_errors(appeal, template_name)
     VANotifySendMessageTemplate.new(message_attributes, template_name, appeal_status)
   end
 
