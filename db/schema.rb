b6f10aaa97471efdb31b4cc60b1d0d55a2fb995c
TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
2024-09-06 12:54:43 -0400

    remove <<<head

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2024_08_29_142914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
