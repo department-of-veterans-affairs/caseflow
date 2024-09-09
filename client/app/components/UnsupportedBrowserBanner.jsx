04902b71f0ab238272137b1b586e73c86b02e00f
Sean Craig <sean.craig2@va.gov>
2024-09-09 13:51:18 -0500

    Removed the glamor import and added style attribute for UnsupportedBrowserBanner

--- client/app/components/UnsupportedBrowserBanner.jsx
+++ client/app/components/UnsupportedBrowserBanner.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import StatusMessage from '../components/StatusMessage';
 import StringUtil from '../util/StringUtil';
 import { detect } from 'detect-browser';
-import { css } from 'glamor';
 import PropTypes from 'prop-types';
 
 export default class UnsupportedBrowserBanner extends React.PureComponent {
@@ -14,12 +13,12 @@ export default class UnsupportedBrowserBanner extends React.PureComponent {
 
     const errorTitle = `${browser.name !== 'chrome' && title}`;
 
-    const linkStyling = css({ textDecoration: 'underline' });
+    const linkStyling = { textDecoration: 'underline' };
 
     const message = <span> To access {this.props.appName}, you must use Chrome as your browser.<br />
     If you need to install Chrome on your computer, please call the <br />
     VA Enterprise Service Desk at
-      <a {...linkStyling} href="tel:855-673-4357"> 855-673-4357</a>.</span>;
+      <a style={linkStyling} href="tel:855-673-4357"> 855-673-4357</a>.</span>;
 
     return <div>
       <StatusMessage
