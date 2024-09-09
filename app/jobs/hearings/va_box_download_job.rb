b93a90974b1e40980f26adb1d34b14286720ac91
piedram <maite.piedrayera@va.gov>
2024-09-09 11:17:34 -0400

    fix lint error

--- app/jobs/hearings/va_box_download_job.rb
+++ app/jobs/hearings/va_box_download_job.rb
@@ -9,55 +9,75 @@ class VaBoxDownloadJob < CaseflowJob
   shoryuken_options retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours]
 
   class BoxDownloadError < StandardError; end
+  class BoxDownloadJobFileUploadError < StandardError; end
 
-  def perform()
-    box_service = ExternalApi::VaBoxService.new()
+  def perform
+    box_service = ExternalApi::VaBoxService.new
 
     files_info = [
-      {:type=>"file", :id=>"1640091891785", :name=>"242551_1_LegacyHearing.pdf", :created_at=>"2024-09-05T06:12:53-07:00"},
-      {:type=>"file", :id=>"1640098775769", :name=>"230903-1_1_Hearing.zip", :created_at=>"2024-09-05T06:12:51-07:00"},
-      {:type=>"file", :id=>"1640089532504", :name=>"242551_1_LegacyHearing.zip", :created_at=>"2024-09-05T06:12:50-07:00"},
-      {:type=>"file", :id=>"1640086914155", :name=>"242551_1_LegacyHearing.doc", :created_at=>"2024-09-05T06:12:49-07:00"},
-      {:type=>"file", :id=>"1640093104372", :name=>"240903-1_1_Hearing.doc", :created_at=>"2024-09-05T06:12:47-07:00"},
-      {:type=>"file", :id=>"1640086880469", :name=>"240903-1_1_Hearing.pdf", :created_at=>"2024-09-05T06:12:46-07:00"},
-      ]
+      { type: "file", id: "1640091891785", name: "242551_1_LegacyHearing.pdf", created_at: "2024-09-05T06:12:53-07:00" },
+      { type: "file", id: "1640098775769", name: "230903-1_1_Hearing.zip", created_at: "2024-09-05T06:12:51-07:00" },
+      { type: "file", id: "1640089532504", name: "242551_1_LegacyHearing.zip", created_at: "2024-09-05T06:12:50-07:00" },
+      { type: "file", id: "1640086914155", name: "242551_1_LegacyHearing.doc", created_at: "2024-09-05T06:12:49-07:00" },
+      { type: "file", id: "1640093104372", name: "240903-1_1_Hearing.doc", created_at: "2024-09-05T06:12:47-07:00" },
+      { type: "file", id: "1640086880469", name: "240903-1_1_Hearing.pdf", created_at: "2024-09-05T06:12:46-07:00" }
+    ]
 
     files_info.collect do |current_file|
       @file_status = "Successful upload (AWS)"
       @file_name = current_file[:name]
       tmp_folder = select_folder(@file_name)
-      box_service.download_file(current_file[:id], tmp_folder)
+      begin
+        box_service.download_file(current_file[:id], tmp_folder)
+      rescue StandardError => error
+        Rails.logger.error "Error to download the file #{@file_name} from Box: #{error.message}"
+        cleanup_tmp_file
+        raise BoxDownloadError
+      end
       if @file_extension == "zip"
         unzip_file(tmp_folder, current_file)
       else
-        # update_database(current_file)
+        upload_to_s3(tmp_folder)
+        update_database(current_file)
       end
     end
+    true
   end
 
   private
 
   def update_database(current_file)
-    # what happen when is a zip (multiple files inside, they have the same ID for all the file)
-    file ||= TranscriptionFile.find_by(id: current_file[:id])
-    if file
-      file.update!(
-        date_upload_aws: Time.zone.today,
-        updated_at: Time.zone.today,
-        date_returned_box: current_file[:created_at]
-      )
+    hearing_id = @file_name.split("_")[1]
+    hearing_type = @file_name.split("_")[2].split(".")[0]
+    docket_number = @file_name.split("_")[0]
+    file_type = File.extname(@file_name).delete(".").to_s
+
+    transcription_records = TranscriptionFile.where(
+      hearing_id: hearing_id, hearing_type: hearing_type, file_type: file_type
+    )
+
+    if transcription_records.count > 0
+      transcription_records.each do |tr|
+        tr.update!(
+          date_upload_aws: Time.zone.today,
+          updated_at: Time.zone.today,
+          date_returned_box: current_file[:created_at]
+        )
+      end
     else
       TranscriptionFile.create!(
-        hearing_id: @file_name.split("_")[1],
-        hearing_type: @file_name.split("_")[2].split(".")[0],
-        docket_number: @file_name.split("_")[0],
+        hearing_id: hearing_id,
+        hearing_type: hearing_type,
+        docket_number: docket_number,
         file_name: @file_name,
-        file_type: File.extname(@file_name).delete(".").to_s,
+        file_type: file_type,
         file_status: @file_status,
-        date_upload_aws: Time.zone.today
-        # aws_link: "vaec-appeals-caseflow-test/#{dir}/#{file_name}"
+        date_upload_aws: Time.zone.today,
+        aws_link: "vaec-appeals-caseflow-test/transcript_text/#{@file_name}"
       )
     end
+  rescue ActiveRecord::RecordInvalid => error
+    Rails.logger.error "Failed to create transcription file: #{error.message}"
   end
 
   def select_folder(filename)
@@ -75,12 +95,6 @@ class VaBoxDownloadJob < CaseflowJob
         FileUtils.mkdir_p(File.dirname(f_path))
         zip_file.extract(f, f_path) unless File.exist?(f_path)
         list_files << f_path unless File.directory?(f_path.to_s)
-
-        # list_files = Dir.entries(f_path.to_s).select { |my_file| File.file? File.join(f_path.to_s, my_file) }
-        # list_files.each do |current_file|
-        #   current_path = Rails.root.join("tmp", "file_from_box", @file_extension.to_s, f.name.to_s, current_file.to_s)
-        #   upload_s3_modified__transcription_file(current_path)
-        # end
       end
       list_files.each do |my_file|
         upload_s3_modified_transcription_file(my_file.to_s, current_file)
@@ -94,7 +108,7 @@ class VaBoxDownloadJob < CaseflowJob
     rescue StandardError => error
       Rails.logger.error "Error to upload #{@file_name} to S3: #{error.message}"
       @file_status = "Failed upload (AWS)"
-      raise WorkOrderFileUploadError
+      raise BoxDownloadJobFileUploadError
     end
   end
 
@@ -105,8 +119,7 @@ class VaBoxDownloadJob < CaseflowJob
 
   def upload_s3_modified_transcription_file(file_path, current_file)
     @file_name = File.basename(file_path)
-    # upload_to_s3(file_path)
+    upload_to_s3(file_path)
     update_database(current_file)
   end
-
 end
