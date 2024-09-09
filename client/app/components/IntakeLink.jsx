3001ca456996a64237076c0e778eb6f0d2ae5cd7
Sean Craig <sean.craig2@va.gov>
2024-09-09 13:48:54 -0500

    Removed the glamor import and added style attribute for IntakeLink

--- client/app/components/IntakeLink.jsx
+++ client/app/components/IntakeLink.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { css } from 'glamor';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import { COLORS } from '../constants/AppConstants';
 import PropTypes from 'prop-types';
 
-const containerStyling = css({
+const containerStyling = {
   display: 'inline table',
   position: 'relative',
   float: 'left',
   marginRight: '3rem'
-});
-const textStyling = css({
+};
+const textStyling = {
   color: COLORS.PRIMARY,
   display: 'table-cell',
   fontSize: '1.7rem',
   lineHeight: '4em',
   paddingLeft: '0.5rem',
   marginBottom: 0
-});
+};
 
-const IntakeLink = (props) => <div {...containerStyling}>
+const IntakeLink = (props) => <div style={containerStyling}>
   <Link href="/intake" target={props.newWindow ? '_blank' : '_self'}>
     <i className="fa fa-inbox" aria-hidden="true"></i>
-    <h3 {...textStyling}>Intake</h3>
+    <h3 style={textStyling}>Intake</h3>
   </Link>
 </div>;
 
