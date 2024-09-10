aef6a77bb3c6e2f3f0f169eb1e131685eb2191e6
William Medders <medders_william@bah.com>
2024-09-10 12:04:15 -0500

    Update tests with properly formatted file names

--- spec/jobs/hearings/monitor_box_job_spec.rb
+++ spec/jobs/hearings/monitor_box_job_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Hearings::MonitorBoxJob, type: :job do
     # see data setup in Fakes::VaBoxService for expectations
     it "returns an array of hashes with name, id, created_at, modified_at" do
       expect(subject).to be_an(Array)
+      expect(subject.empty?).to eq(false)
       subject.each do |hash|
         expect(hash).to have_key(:id)
         expect(hash).to have_key(:name)
@@ -20,12 +21,27 @@ RSpec.describe Hearings::MonitorBoxJob, type: :job do
       end
     end
 
-    # the file with the name "654321-1234-1234-AMA.zip" has the
+    it "returns AMA hearing files" do
+      file_names = subject.map { |file| file[:name] }
+      expect(file_names.any?("123456-1_5678_Hearing.doc")).to eq(true)
+    end
+
+    it "returns legacy hearing files" do
+      file_names = subject.map { |file| file[:name] }
+      expect(file_names.any?("1234567_2342_LegacyHearing.pdf")).to eq(true)
+    end
+
+    it "returns work order files" do
+      file_names = subject.map { |file| file[:name] }
+      expect(file_names.any?("BVA-2024-0001.xls")).to eq(true)
+    end
+
+    # the file with the name "654321-1_1234_Hearing.doc" has the
     # correct naming convention, but it's created_at value is
     # outside the allowed time range
     it "only returns most recently added files" do
       file_names = subject.map { |file| file[:name] }
-      expect(file_names.any?("654321-1234-1234-AMA.zip")).to eq(false)
+      expect(file_names.any?("654321-1_1234_Hearing.doc")).to eq(false)
     end
 
     # the file with the name "NOT_THE_CORRECT_NAMING_CONVENTION.zip" has
