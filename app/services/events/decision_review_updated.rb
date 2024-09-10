bf435b462a4fc91b41cfc5aaaaa9a7c82f45b9f4
Will Love <wlove29@gmail.com>
2024-09-10 10:45:26 -0400

    Create Audit Tables for SC, HLR, Request Issues Appeals 55403 (#22689)
    * migration added
    
    * schema updated correctly
    
    * added audit service and call to update evenr records
    
    * logic for update type
    
    * fixed typo in audit file and added spec
    
    * added insert
    
    * moved Insert DRUA to create request issues for created ones
    
    * updated event record to take in update type
    
    * passing in correct info attribute now
    
    * disabled unused params, can enable once we use them
    
    * remove <<<head
    
    * updated migration and added request_issue to info json
    
    * fixed spec where created at and updated timestamps format was different
    
    ---------
    
    Co-authored-by: TuckerRose <tuckerrose@TuckerRoses-MBP.lan>
    Co-authored-by: TuckerRose <tuckerrose@TuckerRoses-MacBook-Pro.local>

--- app/services/events/decision_review_updated.rb
+++ app/services/events/decision_review_updated.rb
@@ -2,8 +2,24 @@
 
 class Events::DecisionReviewUpdated
   class << self
+    # rubocop:disable Lint/UnusedMethodArgument
     def update!(params, headers, payload)
-      # logic here
+      consumer_event_id = params[:consumer_event_id]
+
+      ActiveRecord::Base.transaction do
+        event = find_or_create_event(consumer_event_id)
+        # Update the Event after all backfills have completed
+        event.update!(completed_at: Time.now.in_time_zone, error: nil, info: {})
+
+        request_issues.each do |request_issue|
+          if request_issue.updated_at > request_issue.created_at
+            DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: "U").call
+          elsif request_issue.destroyed?
+            DecisionReviewUpdatedAudit.new(event: event, request_issue: request_issue, update_type: "D").call
+          end
+        end
+      end
     end
+    # rubocop:enable Lint/UnusedMethodArgument
   end
 end
