858e87bf52df5a7b80ae38aa68daa5746e77aed1
HunJerBAH <Jeremy.Hunton@va.gov>
2024-09-11 13:24:21 -0400

    cleaned up test variable definitions

--- spec/models/correspondence_mail_task_spec.rb
+++ spec/models/correspondence_mail_task_spec.rb
@@ -8,18 +8,6 @@ RSpec.describe CorrespondenceMailTask, type: :model do
   let(:assigned_user) { create(:user) }
   let(:unassigned_user) { create(:user) }
 
-  # values needed for CorrespondenceTaskActionsHelpers
-  let(:privacy_user) { create(:user, css_id: "PRIVACY_TEAM_USER", full_name: "Leighton PrivacyAndFOIAUser Naumov") }
-  let(:cavc_user) { create(:user, css_id: "CAVC_LIT_SUPPORT_ADMIN", full_name: "CAVCLitSupportAdmin") }
-  let(:liti_user) { create(:user, css_id: "LITIGATION_SUPPORT_ADMIN", full_name: "LITIGATIONSUPPORT") }
-  let(:colocated_user) { create(:user, css_id: "COLOCATED_ADMIN", full_name: "ColocatedAdmin") }
-  let(:hearings_user) { create(:user, css_id: "HEARINGS_ADMIN", full_name: "HearingsAdmin") }
-  let(:user_team) { InboundOpsTeam.singleton }
-  let(:privacy_team) { PrivacyTeam.singleton }
-  let(:cavc_team) { CavcLitigationSupport.singleton }
-  let(:liti_team) { LitigationSupport.singleton }
-  let(:colocated_team) { Colocated.singleton }
-  let(:hearings_team) { HearingAdmin.singleton }
   let!(:veteran) { create(:veteran, first_name: "John", last_name: "Testingman", file_number: "8675309") }
   let!(:correspondence) { create(:correspondence, :completed, veteran: veteran) }
 
@@ -44,10 +32,13 @@ RSpec.describe CorrespondenceMailTask, type: :model do
               Caseflow::Error::ActionForbiddenError
             )
           end
-
         end
       end
     end
+  end
+
+  context ".reassign_users" do
+
   end
 
   context ".available_actions" do
@@ -60,6 +51,20 @@ RSpec.describe CorrespondenceMailTask, type: :model do
         Constants.TASK_ACTIONS.COMPLETE_CORRESPONDENCE_TASK.to_h
       ]
     end
+
+    # values needed for CorrespondenceTaskActionsHelpers
+    let(:privacy_user) { create(:user, css_id: "PRIVACY_TEAM_USER", full_name: "Leighton PrivacyAndFOIAUser Naumov") }
+    let(:cavc_user) { create(:user, css_id: "CAVC_LIT_SUPPORT_ADMIN", full_name: "CAVCLitSupportAdmin") }
+    let(:liti_user) { create(:user, css_id: "LITIGATION_SUPPORT_ADMIN", full_name: "LITIGATIONSUPPORT") }
+    let(:colocated_user) { create(:user, css_id: "COLOCATED_ADMIN", full_name: "ColocatedAdmin") }
+    let(:hearings_user) { create(:user, css_id: "HEARINGS_ADMIN", full_name: "HearingsAdmin") }
+    let(:user_team) { InboundOpsTeam.singleton }
+    let(:privacy_team) { PrivacyTeam.singleton }
+    let(:cavc_team) { CavcLitigationSupport.singleton }
+    let(:liti_team) { LitigationSupport.singleton }
+    let(:colocated_team) { Colocated.singleton }
+    let(:hearings_team) { HearingAdmin.singleton }
+
     # iterate through each mail task
     CorrespondenceTaskActionsHelpers::TASKS.each do |task_action|
       context "for #{task_action[:name]} tasks" do
