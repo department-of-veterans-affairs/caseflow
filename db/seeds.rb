ca3bc5c7b4dac0f474d0845686c5b1f4e02ec074
nader k <nader@nkconsulting.net>
2024-09-12 13:22:28 -0700

    fix merge conflic in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,11 +37,7 @@ class SeedDB
     RequestStore[:current_user] = User.system_user
     call_and_log_seed_step :clean_db
 
-<<<<<<< HEAD
-    call_and_log_seed_step Seeds::ConsumerApiKey
-=======
     call_and_log_seed_step Seeds::ApiKeys
->>>>>>> master
     call_and_log_seed_step Seeds::Annotations
     call_and_log_seed_step Seeds::Tags
     # These must be ran before others
