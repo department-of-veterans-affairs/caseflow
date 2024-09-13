2f5c304addf99fb85c254013799dbd8ed4da919f
Blake Manus <33578594+Blake-Manus@users.noreply.github.com>
2024-09-13 10:09:53 -0400

    Ricky/APPEALS-57057 (#22801)
    * Created outline for CSV test for appeals tied to non ssc avljs
    
    * Updated rspec for appeals tied to non ssc avlj to have full coverage
    
    * Created initial spec file for appeeals tied to avljs and vljs query
    
    * Added testing support appeals_tied_to_avljs_and_vljs_query
    
    * Updated testing on appeals_tied_to_avlj_and_vljs
    
    * Updated framework on location 63 appeals CSV rspec testing
    
    * Fixed base test for appeals in location 63 CSV
    
    * Updated data structure to properly check test
    
    * Updated location 63 CSV to have full coverage
    
    * Fixed linting

--- spec/queries/appeals_in_location_63_in_past_2_days_spec.rb
+++ spec/queries/appeals_in_location_63_in_past_2_days_spec.rb
@@ -1,68 +1,69 @@
 # frozen_string_literal: true
 
-require "appeals_in_location_63_in_past_2_days"
+require "./app/queries/appeals_in_location_63_in_past_2_days"
 
-RSpec.describe AppealsInLocation63InPast2Days do
-  describe "#loc_63_appeals" do
-    let(:docket_coordinator) { instance_double(DocketCoordinator) }
-    let(:docket) { instance_double(Docket) }
+describe AppealsInLocation63InPast2Days do
+  let(:avlj_name) { "Avlj Judge" }
+  let(:avlj_fname) { "Avlj" }
+  let(:avlj_lname) { "Judge" }
 
-    before do
-      allow(DocketCoordinator).to receive(:new).and_return(docket_coordinator)
+  let(:non_ssc_avlj) do
+    User.find_by_css_id("NONSSCTEST") ||
+      create(:user, :non_ssc_avlj_user, css_id: "NONSSCTEST", full_name: avlj_name)
+  end
+
+  let(:prev_deciding_judge) do
+    User.find_by_css_id("PREDECJUDG") ||
+      create(:user, :vlj_user, css_id: "PREDECJUDG", full_name: "Prevdec Judge")
+  end
+
+  let(:appeal) do
+    {
+      "tinum" => "150000999988855",
+      "aod" => false,
+      "cavc" => false,
+      "bfd19" => "2023-01-05 00:00:00 UTC",
+      "bfdloout" => "2024-08-27 09:19:55 UTC",
+      "ssn" => "999559999",
+      "snamef" => "Bob",
+      "snamel" => "Goodman",
+      "vlj" => non_ssc_avlj.vacols_staff.sattyid,
+      "vlj_namef" => avlj_fname,
+      "vlj_namel" => avlj_lname,
+      "prev_deciding_judge" => prev_deciding_judge.vacols_staff.sattyid,
+      "bfkey" => "99",
+      "bfdlocin" => "2024-09-10 14:40:58 UTC",
+      "bfcurloc" => "63"
+    }
+  end
+
+  context "#process and #tied_appeals" do
+    it "selects all appeals in location 63 and generates the CSV" do
+      allow(AppealRepository).to receive(:loc_63_appeals).and_return([appeal])
+      expect { described_class.process }.not_to raise_error
+      expect(described_class.loc_63_appeals.size).to eq 1
     end
+  end
 
-    context "when dockets hash contains a legacy docket" do
-      let(:appeals) do
-        [
-          {
-            "bfkey": "915660066",
-            "tinum": "150000915660066",
-            "snamef": "Bob",
-            "snamel": "Smithkreiger",
-            "ssn": "400797580",
-            "vlj_namef": "Some-moved",
-            "vlj_namel": "some-not"
-          },
-          {
-            "bfkey": "520100077",
-            "tinum": "150000520100077",
-            "snamef": "Bob",
-            "snamel": "Smithritchie",
-            "ssn": "244382041",
-            "vlj_namef": "Some-moved",
-            "vlj_namel": "some-not"
-          }
-        ]
-      end
+  context "Test the CSV generation" do
+    context "where it uses attributes " do
+      it "to create a hash Legacy rows moved to loc 63" do
+        subject_legacy = described_class.legacy_rows([appeal]).first
 
-      let(:legacy_docket) { instance_double(LegacyDocket) }
-
-      before do
-        allow(docket_coordinator).to receive(:dockets).and_return(legacy: legacy_docket)
-        allow(legacy_docket).to receive(:loc_63_appeals).and_return(appeals)
-        allow(described_class).to receive(:legacy_rows).with(appeals).and_return(appeals)
-      end
-
-      it "returns unique legacy rows based on docket_number" do
-        result = described_class.loc_63_appeals
-        expect(result).to eq([{ bfkey: "915660066",
-                                snamef: "Bob",
-                                snamel: "Smithkreiger",
-                                ssn: "400797580",
-                                tinum: "150000915660066",
-                                vlj_namef: "Some-moved",
-                                vlj_namel: "some-not" }])
-      end
-    end
-
-    context "when dockets hash does not contain a legacy docket" do
-      before do
-        allow(docket_coordinator).to receive(:dockets).and_return(other_key: docket)
-      end
-
-      it "returns an empty array" do
-        result = described_class.loc_63_appeals
-        expect(result).to eq([])
+        expect(subject_legacy[:docket_number]).to eq appeal["tinum"]
+        expect(subject_legacy[:aod]).to eq appeal["aod"]
+        expect(subject_legacy[:cavc]).to be appeal["cavc"]
+        expect(subject_legacy[:receipt_date]).to eq appeal["bfd19"]
+        expect(subject_legacy[:ready_for_distribution_at]).to eq appeal["bfdloout"]
+        expect(subject_legacy[:veteran_file_number]).to eq appeal["ssn"]
+        expect(subject_legacy[:veteran_name]).to eq "Bob Goodman"
+        expect(subject_legacy[:hearing_judge_id]).to eq non_ssc_avlj.vacols_staff.sdomainid
+        expect(subject_legacy[:hearing_judge_name]).to eq avlj_name
+        expect(subject_legacy[:deciding_judge_id]).to eq prev_deciding_judge.vacols_staff.sdomainid
+        expect(subject_legacy[:deciding_judge_name]).to eq prev_deciding_judge.full_name
+        expect(subject_legacy[:affinity_start_date]).to eq nil
+        expect(subject_legacy[:moved_date_time]).to eq appeal["bfdlocin"]
+        expect(subject_legacy[:bfcurloc]).to eq appeal["bfcurloc"]
       end
     end
   end
--- spec/queries/appeals_tied_to_non_ssc_avlj_query_spec.rb
+++ spec/queries/appeals_tied_to_non_ssc_avlj_query_spec.rb
@@ -1,91 +1,156 @@
 # frozen_string_literal: true
 
-require "appeals_tied_to_non_ssc_avlj_query"
+describe AppealsTiedToNonSscAvljQuery do
+  let(:hearing_judge) { create(:user, :judge, :with_vacols_judge_record) }
+  let(:original_deciding_judge) { create(:user, :judge, :with_vacols_judge_record) }
 
-RSpec.describe AppealsTiedToNonSscAvljQuery do
-  describe ".tied_appeals" do
-    let(:docket_coordinator) { instance_double(DocketCoordinator) }
-    let(:legacy_docket) { instance_double("LegacyDocket") }
+  avlj_name = "John Doe"
+  let(:non_ssc_avlj) do
+    User.find_by_css_id("NONSSCTEST") ||
+      create(:user, :non_ssc_avlj_user, css_id: "NONSSCTEST", full_name: avlj_name)
+  end
+  let(:veteran) { create(:veteran) }
 
-    before do
-      allow(DocketCoordinator).to receive(:new).and_return(docket_coordinator)
-      allow(docket_coordinator).to receive(:dockets)
-        .and_return(
-          {
-            legacy: legacy_docket
-          }
-        )
+  let(:correspondent) do
+    create(
+      :correspondent,
+      snamef: veteran.first_name,
+      snamel: veteran.last_name,
+      ssalut: "", ssn: veteran.file_number
+    )
+  end
+
+  let(:vacols_prio_case) do
+    create(
+      :case,
+      :aod,
+      :tied_to_judge,
+      :video_hearing_requested,
+      :type_original,
+      :ready_for_distribution,
+      tied_judge: non_ssc_avlj,
+      correspondent: correspondent,
+      bfcorlid: "#{veteran.file_number}S",
+      case_issues: create_list(:case_issue, 3, :compensation),
+      bfd19: 60.days.ago
+    )
+  end
+  let(:vacols_non_prio_case) do
+    create(
+      :case,
+      :tied_to_judge,
+      :video_hearing_requested,
+      :type_original,
+      :ready_for_distribution,
+      tied_judge: non_ssc_avlj,
+      correspondent: correspondent,
+      bfcorlid: "#{veteran.file_number}S",
+      case_issues: create_list(:case_issue, 3, :compensation),
+      bfd19: 7.days.ago
+    )
+  end
+
+  context "#process and #tied_appeals" do
+    # Base appeals not tied to non ssc avljs that should NOT be grabbed from the query
+    let!(:not_ready_ama_original_appeal) { create(:appeal, :evidence_submission_docket, :with_post_intake_tasks) }
+    let!(:ama_original_direct_review_appeal) { create(:appeal, :direct_review_docket, :ready_for_distribution) }
+    let!(:ama_original_evidence_submission_appeal) do
+      create(:appeal, :evidence_submission_docket, :ready_for_distribution)
+    end
+    let!(:ama_original_hearing_appeal) do
+      create(:appeal, :hearing_docket, :held_hearing_and_ready_to_distribute, tied_judge: hearing_judge)
     end
 
-    context "when there are legacy appeals tied to non-SSC AVLJs" do
-      let(:legacy_appeal1) { { "tinum" => "123456", "bfkey" => "1", "priority" => 1, "bfd19" => "2023-01-01" } }
-      let(:legacy_appeal2) { { "tinum" => "789012", "bfkey" => "2", "priority" => 0, "bfd19" => "2023-02-01" } }
+    let!(:not_ready_legacy_original_appeal) do
+      create(:case_with_form_9, :type_original, :travel_board_hearing_requested)
+    end
+    let!(:legacy_original_appeal_no_hearing) { create(:case, :type_original, :ready_for_distribution) }
+    let!(:legacy_original_appeal_with_hearing) do
+      create(:case, :type_original, :ready_for_distribution, case_hearings: [legacy_original_appeal_case_hearing])
+    end
+    let(:legacy_original_appeal_case_hearing) { build(:case_hearing, :disposition_held, user: hearing_judge) }
 
-      before do
-        allow(legacy_docket).to receive(:appeals_tied_to_non_ssc_avljs).and_return([legacy_appeal1, legacy_appeal2])
-        allow(described_class).to receive(:calculate_field_values)
-          .and_return(
-            {
-              veteran_file_number: "123456789",
-              veteran_name: "John Doe",
-              non_ssc_avlj: "Judge Smith",
-              hearing_judge: "Judge Smith",
-              most_recent_signing_judge: "Judge Johnson",
-              bfcurloc: "57"
-            }
-          )
-      end
-
-      it "returns formatted legacy appeals" do
-        result = described_class.tied_appeals
-
-        expect(result.length).to eq(2)
-        expect(result.first[:docket_number]).to eq("123456")
-        expect(result.first[:docket]).to eq("legacy")
-        expect(result.first[:priority]).to eq("True")
-        expect(result.first[:receipt_date]).to eq("2023-01-01")
-        expect(result.first[:veteran_file_number]).to eq("123456789")
-        expect(result.first[:veteran_name]).to eq("John Doe")
-        expect(result.first[:non_ssc_avlj]).to eq("Judge Smith")
-        expect(result.first[:hearing_judge]).to eq("Judge Smith")
-        expect(result.first[:most_recent_signing_judge]).to eq("Judge Johnson")
-        expect(result.first[:bfcurloc]).to eq("57")
-      end
+    # Appeals that should be grabbed with the Query
+    let!(:legacy_unsigned_priority_tied_to_non_ssc_avlj) do
+      legacy_appeal = create(
+        :legacy_appeal,
+        :with_root_task,
+        vacols_case: vacols_prio_case,
+        closest_regional_office: "RO17"
+      )
+      create(:available_hearing_locations, "RO17", appeal: legacy_appeal)
     end
 
-    context "when there are no legacy appeals tied to non-SSC AVLJs" do
-      before do
-        allow(legacy_docket).to receive(:appeals_tied_to_non_ssc_avljs).and_return([])
-      end
-
-      it "returns an empty array" do
-        expect(described_class.tied_appeals).to be_empty
-      end
+    let!(:legacy_unsigned_non_priority_tied_to_non_ssc_avlj) do
+      legacy_appeal = create(
+        :legacy_appeal,
+        :with_root_task,
+        vacols_case: vacols_non_prio_case,
+        closest_regional_office: "RO17"
+      )
+      create(:available_hearing_locations, "RO17", appeal: legacy_appeal)
     end
 
-    context "when there are duplicate legacy appeals" do
-      let(:duplicate_appeal) { { "tinum" => "123456", "bfkey" => "1", "priority" => 1, "bfd19" => "2023-01-01" } }
+    let!(:legacy_signed_non_priority_tied_to_non_ssc_avlj) do
+      create(:legacy_signed_appeal, :type_original, signing_avlj: non_ssc_avlj, assigned_avlj: non_ssc_avlj)
+    end
 
-      before do
-        allow(legacy_docket).to receive(:appeals_tied_to_non_ssc_avljs).and_return([duplicate_appeal, duplicate_appeal])
-        allow(described_class).to receive(:calculate_field_values)
-          .and_return(
-            {
-              veteran_file_number: "123456789",
-              veteran_name: "John Doe",
-              non_ssc_avlj: "Judge Smith",
-              hearing_judge: "Judge Smith",
-              most_recent_signing_judge: "Judge Johnson",
-              bfcurloc: "57"
-            }
-          )
+    let!(:legacy_signed_priority_tied_to_non_ssc_avlj) do
+      create(:legacy_signed_appeal, :type_cavc_remand, signing_avlj: non_ssc_avlj, assigned_avlj: non_ssc_avlj)
+    end
+
+    it "selects all appeals tied to non ssc avlj and generates the CSV" do
+      expect { described_class.process }.not_to raise_error
+      expect(described_class.tied_appeals.size).to eq 4
+    end
+  end
+
+  context "Test the CSV generation" do
+    let!(:legacy_signed_appeal_with_attributes) do
+      create(:legacy_signed_appeal, :type_original, signing_avlj: non_ssc_avlj, assigned_avlj: non_ssc_avlj)
+    end
+
+    let(:query_result) { VACOLS::CaseDocket.appeals_tied_to_non_ssc_avljs }
+
+    subject { described_class.legacy_rows(query_result, :legacy).first }
+
+    context "where it uses attributes " do
+      it "to create a hash for the row" do
+        corres = legacy_signed_appeal_with_attributes.reload.correspondent
+
+        expect(subject[:docket_number]).to eq legacy_signed_appeal_with_attributes.folder.tinum
+        expect(subject[:docket]).to eq "legacy"
+        expect(subject[:priority]).to be ""
+        expect(subject[:veteran_file_number]).to eq corres.ssn
+        expect(subject[:veteran_name]).to eq "#{corres.snamef} #{corres.snamel}"
+        expect(subject[:non_ssc_avlj]).to eq avlj_name
+        expect(subject[:hearing_judge]).to eq avlj_name
+        expect(subject[:most_recent_signing_judge]).to eq avlj_name
+        expect(subject[:bfcurloc]).to eq legacy_signed_appeal_with_attributes.bfcurloc
       end
 
-      it "returns unique appeals based on docket number" do
-        result = described_class.tied_appeals
+      context "to test getting the avlj name from appeal" do
+        it "where appeals vlj is nil" do
+          appeal = query_result.first
+          appeal["vlj"] = nil
+          expect(described_class.get_avlj_name(appeal)).to eq nil
+        end
+        it "where appeals vlj is not nil" do
+          appeal = query_result.first
+          expect(described_class.get_avlj_name(appeal)).to eq avlj_name
+        end
+      end
 
-        expect(result.length).to eq(1)
-        expect(result.first[:docket_number]).to eq("123456")
+      context "to test getting the prev judges name from appeal" do
+        it "where appeal has no prev deciding judge" do
+          appeal = query_result.first
+          appeal["prev_deciding_judge"] = nil
+          expect(described_class.get_prev_judge_name(appeal)).to eq nil
+        end
+        it "where appeal has a previous deciding judge" do
+          appeal = query_result.first
+          expect(described_class.get_prev_judge_name(appeal)).to eq avlj_name
+        end
       end
     end
   end
