487c45ce1234938595ca300f0a4237f6ee80588d
Zackary Borges-Rowe <borges-rowe_zackary@bah.com>
2024-09-13 09:50:54 -0600

    add logs for research purposes in UAT only

--- app/jobs/fetch_efolder_document_count_job.rb
+++ app/jobs/fetch_efolder_document_count_job.rb
@@ -8,5 +8,6 @@ class FetchEfolderDocumentCountJob < CaseflowJob
     RequestStore.store[:current_user] = user
 
     ExternalApi::EfolderService.fetch_document_count(file_number, user)
+    Rails.logger.info("Starting FetchEfolderDocumentCountJob: file_number: #{file_number}, user: #{user}")
   end
 end
