3e59ab22db49363d8f529a1219526772ee01e9e0
Konstantin Shevtsov <166068160+KonstantinShevtsov@users.noreply.github.com>
2024-09-17 15:12:30 -0400

    Update decision_review_updated.rb
    Lines 21,22 uncommented.

--- app/services/events/decision_review_updated.rb
+++ app/services/events/decision_review_updated.rb
@@ -18,8 +18,8 @@ class Events::DecisionReviewUpdated
 
         event = find_or_create_event(consumer_event_id)
 
-        # Events::DecisionReviewUpdated::UpdateInformalConference.process!(event: event, parser: parser)
-        # Events::DecisionReviewUpdated::UpdateClaimReview.process!(event: event, parser: parser)
+        Events::DecisionReviewUpdated::UpdateInformalConference.process!(event: event, parser: parser)
+        Events::DecisionReviewUpdated::UpdateClaimReview.process!(event: event, parser: parser)
 
         # Update the Event after all operations have completed
         event.update!(completed_at: Time.now.in_time_zone, error: nil, info: {})
