5b494b0af633012ad9936021ce50f363f13d61a8
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-18 17:26:05 -0400

    🔥 Prune `DeprecationWarnings` RegEx's from previous Rails versions
    Now that Caseflow is stable on Rails 6.1, we no longer need to worry about
    raising exceptions on fixed deprecation warnings targeting Rails 6.1
    or earlier.

--- lib/deprecation_warnings.rb
+++ lib/deprecation_warnings.rb
@@ -6,24 +6,7 @@ module DeprecationWarnings
     /Caseflow::Migration is deprecated/
   ].freeze
 
-  # Regular expressions for Rails 6.0 deprecation warnings that we have addressed in the codebase
-  RAILS_6_0_FIXED_DEPRECATION_WARNING_REGEXES = [
-    /Dangerous query method \(method whose arguments are used as raw SQL\) called with non\-attribute argument\(s\)/,
-    /The success\? predicate is deprecated and will be removed in Rails 6\.0/
-  ].freeze
-
-  # Regular expressions for Rails 6.1 deprecation warnings that we have addressed in the codebase
-  RAILS_6_1_FIXED_DEPRECATION_WARNING_REGEXES = [
-    /update_attributes is deprecated and will be removed from Rails 6\.1/,
-    /ActionView::Base instances should be constructed with a lookup context, assignments, and a controller./,
-    /ActionView::Base instances must implement `compiled_method_container`/,
-    /render file: should be given the absolute path to a file/,
-    /`ActiveRecord::Result#to_hash` has been renamed to `to_a`/,
-    /Class level methods will no longer inherit scoping/,
-    /Controller-level `force_ssl` is deprecated and will be removed from Rails 6\.1/,
-    /NOT conditions will no longer behave as NOR in Rails 6\.1/
-  ].freeze
-
+  # Regular expressions for Rails 7.0 deprecation warnings that we have addressed in the codebase
   RAILS_7_0_FIXED_DEPRECATION_WARNING_REGEXES = [
     /Initialization autoloaded the constant/
   ].freeze
@@ -31,8 +14,6 @@ module DeprecationWarnings
   # Regular expressions for deprecation warnings that should raise an exception on detection
   DISALLOWED_DEPRECATION_WARNING_REGEXES = [
     *CUSTOM_DEPRECATION_WARNING_REGEXES,
-    *RAILS_6_0_FIXED_DEPRECATION_WARNING_REGEXES,
-    *RAILS_6_1_FIXED_DEPRECATION_WARNING_REGEXES,
     *RAILS_7_0_FIXED_DEPRECATION_WARNING_REGEXES
   ].freeze
 
