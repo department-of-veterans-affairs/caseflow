3046b97d3d6a83148bcda83012a10bcaae41f54d
breedbah <brandon.reed3@va.gov>
2024-09-18 11:39:31 -0400

    APPEALS-43242 fix seeding data

--- db/seeds/education.rb
+++ db/seeds/education.rb
@@ -18,18 +18,38 @@ module Seeds
     private
 
     def setup_emo_org
-      regular_user = create(:user, full_name: "Paul EMOUser EMO", css_id: "EMOUSER")
-      admin_user = create(:user, full_name: "Julie EMOAdmin EMO", css_id: "EMOADMIN")
+      regular_user = User.find_or_initialize_by(css_id: "EMOUSER")
+      unless regular_user.persisted?
+        regular_user.update!(full_name: "Paul EMOUser EMO")
+      end
+
+      admin_user = User.find_or_initialize_by(css_id: "EMOADMIN")
+      unless admin_user.persisted?
+        admin_user.update!(full_name: "Julie EMOAdmin EMO")
+      end
+
       emo = EducationEmo.singleton
       emo.add_user(regular_user)
       OrganizationsUser.make_user_admin(admin_user, emo)
     end
 
     def setup_rpo_orgs!
-      RPOS.each { |name| EducationRpo.create!(name: name, url: name) }
+      RPOS.each do |name|
+        org = EducationRpo.find_or_initialize_by(url: name.parameterize)
+        unless org.persisted?
+          org.update!(name: name)
+        end
+      end
 
-      regular_user = create(:user, full_name: "Peter EDURPOUSER Campbell", css_id: "EDURPOUSER")
-      admin_user = create(:user, full_name: "Samuel EDURPOADMIN Clemens", css_id: "EDURPOADMIN")
+      regular_user = User.find_or_initialize_by(css_id: "EDURPOUSER")
+      unless regular_user.persisted?
+        regular_user.update!(full_name: "Peter EDURPOUSER Campbell")
+      end
+
+      admin_user = User.find_or_initialize_by(css_id: "EDURPOADMIN")
+      unless admin_user.persisted?
+        admin_user.update!(full_name: "Samuel EDURPOADMIN Clemens")
+      end
 
       EducationRpo.all.each do |org|
         org.add_user(regular_user)
