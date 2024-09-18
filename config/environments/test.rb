2924e98091ddc1539029348a7942d7a9fa13fa15
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-18 17:08:48 -0400

    🚚 Consolidate and move `DeprecationWarnings` module to `/lib`

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 require "fileutils"
-require_relative "../../config/initializers/deprecation_warnings"
+require_relative "../../lib/deprecation_warnings"
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
