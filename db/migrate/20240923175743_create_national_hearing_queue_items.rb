53a6513870f5074ff0767b103220174552bee5b9
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-09-23 15:06:26 -0400

    Add blank migration so I can switch branches

--- db/migrate/20240923175743_create_national_hearing_queue_items.rb
+++ db/migrate/20240923175743_create_national_hearing_queue_items.rb
@@ -1,4 +1,4 @@
-class CreateNhqMaterializedView < ActiveRecord::Migration[6.1]
+class CreateNationalHearingQueueItems < ActiveRecord::Migration[6.1]
   def up
     create_view :national_hearing_queue_entries, materialized: true
   end
