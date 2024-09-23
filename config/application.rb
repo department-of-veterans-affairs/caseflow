a9eb6965353545c264112046782db9fcccfebc33
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-23 18:00:57 -0400

    🔧 Collapse directory `/app/models/tasks/docket_switch` for Zeitwerk autoloader
    Resolves `NameError: uninitialized constant DocketSwitchMailTask`

--- config/application.rb
+++ config/application.rb
@@ -155,6 +155,7 @@ module CaseflowCertification
       "#{root}/app/models/prepend",
       "#{root}/app/models/prepend/va_notify",
       "#{root}/app/models/tasks",
+      "#{root}/app/models/tasks/docket_switch",
       "#{root}/app/models/tasks/pre_docket",
       "#{root}/app/models/validators",
     )
