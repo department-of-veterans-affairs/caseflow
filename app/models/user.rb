b227c84e91a7bf48433a22d5a446d0633a2f4c51
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-24 14:38:21 -0400

    ♻️ Rename `BgsService` to `BGSServiceConcern` to avoid naming collision
    We will need to introduce a Zeitwerk inflector for `BGSService` to accommodate
    constants `ExternalApi::BGSService` and `Fakes::BGSService`. However, this
    will have also forced us to change `BgsService` to `BGSService` as well, to
    satisfy the inflector. Since there is already a `BGSService` constant defined
    elsewhere, this would have led to a naming collision, and thus file shadowing
    by the Zeitwerk autoloader.
    
    To get around this problem, we rename `BgsService` to `BGSServiceConcern`.
    This also brings the name into consistency with the other `*Concerns`.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < CaseflowRecord # rubocop:disable Metrics/ClassLength
-  include BgsService
+  include BGSServiceConcern
   include ConferenceableConcern
   include EventConcern
 
