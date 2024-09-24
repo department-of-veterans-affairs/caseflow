df5c279560e128c5f0a14f70eaf38f06df6d2469
Anusha Palliyil <palliyil_anusha@bah.com>
2024-09-24 14:39:15 -0400

    testing

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 ENV["NLS_LANG"] = "AMERICAN_AMERICA.UTF8"
 
+require "ruby-oci8"
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
