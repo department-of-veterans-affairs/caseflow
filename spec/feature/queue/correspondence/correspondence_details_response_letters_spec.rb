f49e6cc97770147bfd2b04e664a92a0d520b3e3a
divyadasari-va <divya.dasari@va.gov>
2024-09-25 14:58:47 -0600

    Lint fixes

--- spec/feature/queue/correspondence/correspondence_details_response_letters_spec.rb
+++ spec/feature/queue/correspondence/correspondence_details_response_letters_spec.rb
@@ -46,7 +46,6 @@ RSpec.feature("Correspondence Details Response Letters Module") do
       expect(second_element_text).not_to include("Expired on")
     end
 
-
     # skipping this spec because it is flaky and needs to be investigated
     xit "Verify the Add button for Response Letters in details page" do
       correspondence = setup_response_letters_data
