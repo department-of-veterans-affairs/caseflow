9c51552e3de5003dc657b8fd9d9d83a20f87949c
christian-cain-bah <cain_christian@bah.com>
2024-09-25 09:23:14 -0500

    fix(supplemental claim): remove merge comments

--- app/models/supplemental_claim.rb
+++ app/models/supplemental_claim.rb
@@ -121,18 +121,15 @@ class SupplementalClaim < ClaimReview
 
   def build_request_issues_from_remand
     remanded_decision_issues_needing_request_issues.map do |remand_decision_issue|
-<<<<<<< Updated upstream
-=======
       # Sanitize invalid characters
       contested_issue_description = remand_decision_issue.description.gsub(DESCRIPTION_CHARACTERS_BLACKLIST, "")
 
->>>>>>> Stashed changes
       RequestIssue.new(
         decision_review: self,
         contested_decision_issue_id: remand_decision_issue.id,
         contested_rating_issue_reference_id: remand_decision_issue.rating_issue_reference_id,
         contested_rating_issue_profile_date: remand_decision_issue.rating_profile_date,
-        contested_issue_description: remand_decision_issue.description,
+        contested_issue_description: contested_issue_description,
         nonrating_issue_category: remand_decision_issue.nonrating_issue_category,
         benefit_type: benefit_type,
         decision_date: remand_decision_issue.approx_decision_date
