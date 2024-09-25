b8e755ed3d2783f12f1c06aba65d39040f1ac42b
divyadasari-va <divya.dasari@va.gov>
2024-09-25 08:53:05 -0600

    Trying to fix the flakey test spec

--- spec/feature/queue/correspondence/correspondence_details_response_letters_spec.rb
+++ spec/feature/queue/correspondence/correspondence_details_response_letters_spec.rb
@@ -50,12 +50,13 @@ RSpec.feature("Correspondence Details Response Letters Module") do
       correspondence = setup_response_letters_data
       visit "/queue/correspondence/#{correspondence.uuid}"
       find_by_id("tasks-tabwindow-tab-2", wait: 10).click
-      button = find_button("+ Add letter", class: "cf-left-side usa-button")
-      button.click
+      expect(page).to have_button("+ Add letter", visible: true, wait: 30)
+      click_button("+ Add letter")
       add_popup_response_letter
       containers = all(".response-letter-table-borderless-no-background")
       expect(containers.size).to eq(2)
-      button.click
+      expect(page).to have_button("+ Add letter", visible: true, wait: 30)
+      click_button("+ Add letter")
       add_popup_response_letter
       expect(page).not_to have_button("+ Add letter")
     end
