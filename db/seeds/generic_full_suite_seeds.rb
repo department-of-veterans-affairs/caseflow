acadb5b32c78a00d04a9aaa636c26c095d2fdbe2
631966 <amy.detwiler@va.gov>
2024-09-25 09:58:09 -0400

    APPEALS-43118 Changed the db/seeds/generic_full_suite_seeds.rb so that it will only load files that aren't excluded

--- db/seeds/generic_full_suite_seeds.rb
+++ db/seeds/generic_full_suite_seeds.rb
@@ -4,41 +4,38 @@ require "database_cleaner-active_record"
 
 # because db/seeds is not in the autoload path, we must load them explicitly here
 # base.rb needs to be loaded first because the other seeds inherit from it
+
+
 require Rails.root.join("db/seeds/base.rb").to_s
-Dir[Rails.root.join("db/seeds/*.rb")].sort.each { |f| require f }
+
+# filtered_files.each { |f| require f }
 
 class GenericFullSuiteSeeds
-  def clean_db
-    DatabaseCleaner.clean_with(:truncation)
-    cm = CacheManager.new
-    CacheManager::BUCKETS.each_key { |bucket| cm.clear(bucket) }
-    Fakes::EndProductStore.new.clear!
-    Fakes::RatingStore.new.clear!
-    Fakes::VeteranStore.new.clear!
-  end
+  def seed
+    excluded_files = %w[
+      db/seeds/hearings.rb
+    ]
 
-  # In development environments this log goes to
-  # caseflow/log/development.log
-  def call_and_log_seed_step(step)
-    msg = "Starting seed step #{step} at #{Time.zone.now.strftime('%m/%d/%Y %H:%M:%S')}"
-    Rails.logger.debug(msg)
+    all_files = Dir[Rails.root.join("db/seeds/*.rb")].sort
 
-    if step.is_a?(Symbol)
-      send(step)
-    else
-      step.new.seed!
+    filtered_files = all_files.reject do |file|
+      excluded_files.include?(file.sub("#{Rails.root}/", ""))
     end
 
-    msg = "Finished seed step #{step} at #{Time.zone.now.strftime('%m/%d/%Y %H:%M:%S')}"
-    Rails.logger.debug(msg)
+    filtered_files.each do |file|
+      # Extract the base name without the directory and extension
+      file_name = File.basename(file, '.rb') # e.g., "demo_non_aod_hearing_case_lever_test_data"
+
+      # Construct the Rake task name
+      task_name = "db:seed:#{file_name}"
+
+      # Re-enable and invoke the Rake task
+      Rake::Task[task_name].reenable
+      Rake::Task[task_name].invoke
+    end
+
   end
 
-  def seed
-    RequestStore[:current_user] = User.system_user
-    # call_and_log_seed_step :clean_db
-
-    call_and_log_seed_step Seeds::VeteransHealthAdministration
-  end
 end
 
 GenericFullSuiteSeeds.new.seed
