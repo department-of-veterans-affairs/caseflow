f141a44e15456ea71225bbeefa45c0e2e6cf58c7
Daniel Mage <dmage20@gmail.com>
2024-09-25 07:08:08 -0700

    Reader Prototype metrics

--- client/app/readerprototype/components/Page.jsx
+++ client/app/readerprototype/components/Page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import PropTypes from 'prop-types';
 import React, { useEffect, useRef } from 'react';
 import { ROTATION_DEGREES } from '../util/readerConstants';
@@ -28,11 +29,13 @@ import usePageVisibility from '../hooks/usePageVisibility';
 // When rotating, we swap height and width of the container.
 // The child is still centered in the container, so we must offset it put it back to the
 // top / center of the container.
-const Page = ({ page, rotation = ROTATION_DEGREES.ZERO, renderItem, scale }) => {
+const Page = ({ page, rotation = ROTATION_DEGREES.ZERO, renderItem, scale, setRenderingMetrics }) => {
   const canvasRef = useRef(null);
   const isVisible = usePageVisibility(canvasRef);
   const wrapperRef = useRef(null);
   const scaleFraction = scale / 100;
+  const reportedStats = useRef(false);
+  const renderTaskRef = useRef(null);
 
   const viewport = page.getViewport({ scale: scaleFraction });
   const scaledHeight = viewport.height;
@@ -65,12 +68,32 @@ const Page = ({ page, rotation = ROTATION_DEGREES.ZERO, renderItem, scale }) => 
 
   useEffect(() => {
     if (canvasRef.current && isVisible) {
-      page.render({ canvasContext: canvasRef.current?.getContext('2d', { alpha: false }), viewport }).
-        promise.catch(() => {
-        // this catch is necessary to prevent the error: Cannot use the same canvas during multiple render operations
-        });
+      if (!renderTaskRef.current) {
+        const renderTask = page.render(
+          { canvasContext: canvasRef.current?.getContext('2d', { alpha: false }), viewport }
+        );
+
+        renderTaskRef.current = renderTask;
+
+        renderTask.promise.
+          then(() => {
+            if (page._stats && Array.isArray(page._stats.times)) {
+
+              const renderingTimes = page._stats.times.find((time) => time.name === 'Rendering');
+
+              if (!reportedStats.current) {
+                setRenderingMetrics(renderingTimes.end - renderingTimes.start);
+                reportedStats.current = true;
+              }
+            }
+          }).
+          catch(() => {
+            // catch is necessary to prevent the error: Cannot use the same canvas during multiple render operations
+          });
+      }
     }
   }, [canvasRef.current, viewport, isVisible]);
+  // [page, isVisible, viewport, setRenderingMetrics]);
 
   return (
     <div
@@ -107,6 +130,7 @@ Page.propTypes = {
   rotation: PropTypes.string,
   renderItem: PropTypes.func,
   scale: PropTypes.number,
+  setRenderingMetrics: PropTypes.func,
 };
 
 export default Page;
--- client/app/readerprototype/components/PdfDocument.jsx
+++ client/app/readerprototype/components/PdfDocument.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Layer from './Comments/Layer';
 
 import { GlobalWorkerOptions, getDocument } from 'pdfjs-dist';
@@ -9,11 +10,19 @@ import ApiUtil from '../../util/ApiUtil';
 import Page from './Page';
 import TextLayer from './TextLayer';
 import DocumentLoadError from './DocumentLoadError';
+import { storeMetrics } from '../../util/Metrics';
 
 const PdfDocument = ({ doc, rotateDeg, setNumPages, zoomLevel, onLoad }) => {
   const [isDocumentLoadError, setIsDocumentLoadError] = useState(false);
   const [pdfDoc, setPdfDoc] = useState(null);
   const [pdfPages, setPdfPages] = useState([]);
+  const getDocumentEnd = useRef(null);
+  const getDocumentStart = useRef(null);
+  const renderedPageCount = useRef(0);
+  const renderedTimeTotal = useRef(0);
+  const [allPagesRendered, setAllPagesRendered] = useState(false);
+  const [metricsLogged, setMetricsLogged] = useState(false);
+  const metricsLoggedRef = useRef(metricsLogged);
 
   const containerStyle = {
     width: '100%',
@@ -25,10 +34,68 @@ const PdfDocument = ({ doc, rotateDeg, setNumPages, zoomLevel, onLoad }) => {
     gap: '5rem',
   };
 
+  const handleRenderingMetrics = (renderingTime) => {
+    if (renderingTime) {
+      renderedTimeTotal.current += renderingTime;
+      renderedPageCount.current += 1;
+      if (renderedPageCount.current === pdfPages.length && pdfPages.length > 0) {
+        setAllPagesRendered(true);
+      }
+    }
+  };
+
+  const getFirstPageOverallTime = () => {
+    if (pdfPages && pdfPages.length > 0) {
+      const firstPage = pdfPages[0];
+
+      if (firstPage._stats && Array.isArray(firstPage._stats.times)) {
+        const overallTime = firstPage._stats.times.find((time) => time.name === 'Overall');
+
+        if (overallTime) {
+          return overallTime.end - overallTime.start;
+        }
+      }
+    }
+
+    return 0;
+  };
+
+  const logMetrics = () => {
+    const calculatedAverage = Math.round(
+      renderedPageCount.current > 0 ? renderedTimeTotal.current / renderedPageCount.current : 0
+    );
+
+    storeMetrics(
+      doc.id,
+      {
+        document_request_time: getDocumentEnd.current - getDocumentStart.current,
+        number_of_pages_rendered: renderedPageCount.current,
+        rendering_time_for_allPages: renderedTimeTotal.current,
+        average_rendering_time_per_page: calculatedAverage,
+        first_page_overall_time: getFirstPageOverallTime(),
+      },
+      {
+        message: 'Reader Prototype times in milliseconds',
+        type: 'performance',
+        product: 'reader prototype',
+        start: null,
+        end: null,
+        duration: null,
+      },
+      null
+    );
+
+    setMetricsLogged(true);
+  };
+
   useEffect(() => {
     const getDocData = async () => {
+      renderedPageCount.current = 0;
+      renderedTimeTotal.current = 0;
       setPdfDoc(null);
       setPdfPages([]);
+      setAllPagesRendered(false);
+      setMetricsLogged(false);
       onLoad(true);
       const requestOptions = {
         cache: true,
@@ -36,12 +103,15 @@ const PdfDocument = ({ doc, rotateDeg, setNumPages, zoomLevel, onLoad }) => {
         timeout: true,
         responseType: 'arraybuffer',
       };
+
+      getDocumentStart.current = new Date().getTime();
       const byteArr = await ApiUtil.get(doc.content_url, requestOptions).then((response) => {
         return response.body;
       });
 
+      getDocumentEnd.current = new Date().getTime();
       onLoad(false);
-      const docProxy = await getDocument({ data: byteArr }).promise;
+      const docProxy = await getDocument({ data: byteArr, pdfBug: true }).promise;
 
       if (docProxy) {
         setPdfDoc(docProxy);
@@ -73,6 +143,25 @@ const PdfDocument = ({ doc, rotateDeg, setNumPages, zoomLevel, onLoad }) => {
     getPdfData();
   }, [pdfDoc]);
 
+  useEffect(() => {
+    if (allPagesRendered && !metricsLogged) {
+      logMetrics();
+    }
+  }, [allPagesRendered, metricsLogged]);
+
+  useEffect(() => {
+    return () => {
+      if (!metricsLoggedRef.current) {
+
+        logMetrics();
+      }
+    };
+  }, [doc.id]);
+
+  useEffect(() => {
+    metricsLoggedRef.current = metricsLogged;
+  }, [metricsLogged]);
+
   return (
     <div id="pdfContainer" style={containerStyle}>
       {isDocumentLoadError && <DocumentLoadError doc={doc} />}
@@ -87,6 +176,7 @@ const PdfDocument = ({ doc, rotateDeg, setNumPages, zoomLevel, onLoad }) => {
               <TextLayer page={page} zoomLevel={zoomLevel} rotation={rotateDeg} />
             </Layer>
           )}
+          setRenderingMetrics={handleRenderingMetrics}
         />
       ))}
     </div>
