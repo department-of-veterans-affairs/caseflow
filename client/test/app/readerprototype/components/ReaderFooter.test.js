ca663d6b99670725dabda593c894c6fb4c3421b8
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-09-25 10:29:51 -0500

    Add doc navigation test to ReaderFooter.test.js

--- client/test/app/readerprototype/components/ReaderFooter.test.js
+++ client/test/app/readerprototype/components/ReaderFooter.test.js
@@ -4,7 +4,8 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReaderFooter from 'app/readerprototype/components/ReaderFooter';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const getUnFilteredStore = () =>
   createStore(
@@ -378,5 +379,47 @@ describe('Filtered', () => {
     ;
 
     expect(getByTitle('filtered indicator')).toBeTruthy();
+  });
+});
+
+describe('Document Navigation', () => {
+  const showPdf = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('calls showPdf() when Next button is clicked', () => {
+    const { container, getByText } = render(<UnFilteredComponent docId={1} showPdf={() => showPdf} />);
+
+    expect(container).toHaveTextContent('1 of 5');
+    expect(container).not.toHaveTextContent('Previous');
+    userEvent.click(getByText('Next'));
+    expect(showPdf).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('calls showPdf() when Previous button is clicked', () => {
+    const { container, getByText } = render(<UnFilteredComponent docId={5} showPdf={() => showPdf} />);
+
+    expect(container).toHaveTextContent('5 of 5');
+    expect(container).not.toHaveTextContent('Next');
+    userEvent.click(getByText('Previous'));
+    expect(showPdf).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('calls showPdf() when right arrow key is pressed', () => {
+    const { container } = render(<UnFilteredComponent docId={1} showPdf={() => showPdf} />);
+
+    fireEvent.keyDown(container, { key: 'ArrowRight', code: 39 });
+    expect(showPdf).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls showPdf() when left arrow key is pressed', () => {
+    const { container } = render(<UnFilteredComponent docId={5} showPdf={() => showPdf} />);
+
+    fireEvent.keyDown(container, { key: 'ArrowLeft', code: 37 });
+    expect(showPdf).toHaveBeenCalledTimes(1);
   });
 });
