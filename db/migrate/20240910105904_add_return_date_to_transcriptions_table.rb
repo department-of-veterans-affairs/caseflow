6c5813f7f7c90732e894a99178540776c7f56b49
William Medders <medders_william@bah.com>
2024-09-26 11:30:37 -0500

    update migrations to follow 6.1 upgrade convention

--- db/migrate/20240910105904_add_return_date_to_transcriptions_table.rb
+++ db/migrate/20240910105904_add_return_date_to_transcriptions_table.rb
@@ -1,4 +1,4 @@
-class AddReturnDateToTranscriptionsTable < Caseflow::Migration
+class AddReturnDateToTranscriptionsTable < ActiveRecord::Migration[6.1]
   def change
     add_column :transcriptions, :return_date, :date, comment: "Date when the contractor returned their work product, box.com upload date"
   end
