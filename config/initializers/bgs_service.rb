058d776bbd9ff4c37581ae8334bdbbb67a71c90a
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 21:56:39 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/bgs_service.rb
+++ config/initializers/bgs_service.rb
@@ -1,1 +1,3 @@
-BGSService = (!ApplicationController.dependencies_faked? ? ExternalApi::BGSService : Fakes::BGSService)
+Rails.application.config.to_prepare do
+  BGSService = (!ApplicationController.dependencies_faked? ? ExternalApi::BGSService : Fakes::BGSService)
+end
