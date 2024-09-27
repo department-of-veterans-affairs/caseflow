99af3e992af834b6e0ff75b060d10ee486a5b509
Alex Smith <alex.smith.99@thunderyard.com>
2024-09-27 11:34:20 -0600

    Update Feature Toggle Logic (#23001)
    * Update Feature Toggle Logic
    
    - Remove references to the send_current_user_cred_to_ce_api flag
      as it is not yet implemented.
    
    - Update sensitivity checks in vbms_service to be controlled by
      the use_ce_api feature toggle.
    
    - Remove sensitivity checks from VbmsRequestWithFileNumber and
      child classes.
    
    * Revert Unintended DB Schema Changes

--- spec/services/external_api/vbms_document_series_for_appeal_spec.rb
+++ spec/services/external_api/vbms_document_series_for_appeal_spec.rb
@@ -2,33 +2,13 @@
 
 describe ExternalApi::VbmsDocumentSeriesForAppeal do
   let(:appeal) { create(:appeal) }
-  let(:mock_sensitivity_checker) { instance_double(SensitivityChecker, sensitivity_levels_compatible?: true) }
-
-  before do
-    allow(SensitivityChecker).to receive(:new).and_return(mock_sensitivity_checker)
-  end
 
   describe "#fetch" do
-    context "with send_current_user_cred_to_ce_api feature toggle enabled" do
-      let!(:user) do
-        user = create(:user)
-        RequestStore.store[:current_user] = user
-      end
+    it "successfully calls the service" do
+      expect(VBMS::Client).to receive(:from_env_vars).and_return(true)
+      expect(ExternalApi::VBMSService).to receive(:send_and_log_request).and_return(true)
 
-      before do
-        expect(VBMS::Client).to receive(:from_env_vars).and_return(true)
-        expect(ExternalApi::VBMSService).to receive(:send_and_log_request).and_return(true)
-        FeatureToggle.enable!(:send_current_user_cred_to_ce_api)
-      end
-
-      after { FeatureToggle.disable!(:send_current_user_cred_to_ce_api) }
-
-      it "check user sensitivity compatibility before calling any APIs" do
-        expect(mock_sensitivity_checker).to receive(:sensitivity_levels_compatible?)
-          .with(user: user, veteran: appeal.veteran).and_return(true)
-
-        described_class.new(file_number: appeal.veteran_file_number).fetch
-      end
+      described_class.new(file_number: appeal.veteran_file_number).fetch
     end
   end
 end
