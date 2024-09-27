16ea2ff3904e2c544de368e1537c8f480aaca494
ryanpmessner <163380175+ryanpmessner@users.noreply.github.com>
2024-09-27 15:33:01 -0400

    APPEALS-23420: search defect
      Add search query service for the api response for the `/search` page
      Add issues to legacy appeals
      Add assigned_to_judge
      Use correct id for list of request issues
      export proper attributes for hearing
      vacols query returns clob from json
      add tests for prodtest failures

---
+++ app/services/search_query_service/legacy_appeal_row.rb
@@ -0,0 +1,193 @@
+# frozen_string_literal: true
+
+class SearchQueryService::LegacyAppealRow
+  def initialize(search_row, vacols_row)
+    @search_row = search_row
+    @vacols_row = vacols_row
+  end
+
+  def search_response
+    SearchQueryService::SearchResponse.new(
+      legacy_appeal,
+      :legacy_appeal,
+      SearchQueryService::ApiResponse.new(
+        id: search_row["id"],
+        type: "legacy_appeal",
+        attributes: attributes
+      )
+    )
+  end
+
+  private
+
+  attr_reader :search_row, :vacols_row
+
+  # rubocop:disable Metrics/MethodLength
+  def attributes
+    SearchQueryService::LegacyAttributes.new(
+      aod: aod,
+      appellant_full_name: appellant_full_name,
+      assigned_attorney: assigned_attorney,
+      assigned_judge: assigned_judge,
+      assigned_to_location: vacols_row["bfcurloc"],
+      caseflow_veteran_id: search_row["veteran_id"],
+      decision_date: decision_date,
+      docket_name: "legacy",
+      docket_number: vacols_row["tinum"],
+      external_id: vacols_row["vacols_id"],
+      hearings: hearings,
+      issues: issues,
+      mst: mst,
+      overtime: search_row["overtime"],
+      pact: pact,
+      paper_case: paper_case,
+      readable_hearing_request_type: readable_hearing_request_type,
+      readable_original_hearing_request_type: readable_original_hearing_request_type,
+      status: status,
+      type: stream_type,
+      veteran_appellant_deceased: veteran_appellant_deceased,
+      veteran_file_number: search_row["veteran_file_number"],
+      veteran_full_name: veteran_full_name
+    )
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def hearings
+    vacols_json_array("hearings").map do |attrs|
+      HearingAttributes.new(attrs).call
+    end
+  end
+
+  class HearingAttributes
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def call
+      {
+        disposition: VACOLS::CaseHearing::HEARING_DISPOSITIONS[attributes["disposition"].try(:to_sym)],
+        request_type: attributes["type"],
+        appeal_type: VACOLS::Case::TYPES[attributes["bfac"]],
+        external_id: attributes["external_id"],
+        held_by: held_by,
+        is_virtual: false,
+        notes: attributes["notes"],
+        type: type,
+        created_at: nil,
+        scheduled_in_timezone: nil,
+        date: HearingMapper.datetime_based_on_type(
+          datetime: attributes["date"],
+          regional_office: regional_office(attributes["venue"]),
+          type: attributes["type"]
+        )
+      }
+    end
+
+    private
+
+    attr_reader :attributes
+
+    def type
+      Hearing::HEARING_TYPES[attributes["hearing_type"]&.to_sym]
+    end
+
+    def held_by
+      fname = attributes["held_by_first_name"]
+      lname = attributes["held_by_last_name"]
+
+      if fname.present? && lname.present?
+        "#{fname} #{lname}"
+      end
+    end
+
+    def regional_office(ro_key)
+      RegionalOffice.find!(ro_key)
+    rescue RegionalOffice::NotFoundError
+      nil
+    end
+  end
+
+  def issues
+    vacols_json_array("issues").map do |attrs|
+      WorkQueue::LegacyIssueSerializer.new(
+        Issue.load_from_vacols(attrs)
+      ).serializable_hash[:data][:attributes]
+    end
+  end
+
+  def assigned_attorney
+    json_array("assigned_attorney").first
+  end
+
+  def assigned_judge
+    json_array("assigned_judge").first
+  end
+
+  def json_array(key)
+    JSON.parse(search_row[key].presence || "[]")
+  end
+
+  def vacols_json_array(key)
+    JSON.parse(vacols_row[key].presence || "[]")
+  end
+
+  def veteran_appellant_deceased
+    search_row["date_of_death"].present? &&
+      search_row["person_first_name"].present?
+  end
+
+  def stream_type
+    VACOLS::Case::TYPES[vacols_row["bfac"]]
+  end
+
+  def status
+    VACOLS::Case::STATUS[vacols_row["bfmpro"]]
+  end
+
+  def paper_case
+    folder = Struct.new(:tivbms, :tisubj2).new(
+      vacols_row["tivbms"],
+      vacols_row["tisubj2"]
+    )
+    AppealRepository.folder_type_from(folder)
+  end
+
+  def mst
+    vacols_row["issues_mst_count"].to_i > 0
+  end
+
+  def pact
+    vacols_row["issues_pact_count"].to_i > 0
+  end
+
+  def appellant_full_name
+    FullName.new(vacols_row["sspare2"], "", vacols_row["sspare1"]).to_s
+  end
+
+  def veteran_full_name
+    FullName.new(vacols_row["snamef"], "", vacols_row["snamel"]).to_s
+  end
+
+  def aod
+    vacols_row["aod"].to_i == 1
+  end
+
+  def decision_date
+    AppealRepository.normalize_vacols_date(vacols_row["bfddec"])
+  end
+
+  def readable_original_hearing_request_type
+    legacy_appeal.readable_original_hearing_request_type
+  end
+
+  def readable_hearing_request_type
+    legacy_appeal.readable_current_hearing_request_type
+  end
+
+  def legacy_appeal
+    @legacy_appeal ||= begin
+      appeal_attrs, = JSON.parse search_row["appeal"]
+      SearchQueryService::QueriedLegacyAppeal.new(attributes: appeal_attrs)
+    end
+  end
+end
---
+++ app/services/search_query_service/vacols_query.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+class SearchQueryService::VacolsQuery
+  def query
+    <<-SQL
+      select
+        aod,
+        "cases".bfkey vacols_id,
+        "cases".bfcurloc,
+        "cases".bfddec,
+        "cases".bfmpro,
+        "cases".bfac,
+        "cases".bfcorlid,
+        "correspondents".snamef,
+        "correspondents".snamel,
+        "correspondents".sspare1,
+        "correspondents".sspare2,
+        "correspondents".slogid,
+        "folders".tinum,
+        "folders".tivbms,
+        "folders".tisubj2,
+        (select
+          JSON_ARRAYAGG(JSON_OBJECT(
+            'venue' value #{case_hearing_venue_select},
+            'external_id' value "h".hearing_pkseq,
+            'type' value "h".hearing_type,
+            'disposition' value "h".hearing_disp,
+            'date' value "h".hearing_date,
+            'held_by_first_name' value "s".snamef,
+            'held_by_last_name' value "s".snamel,
+            'notes' value "h".notes1
+          ) returning CLOB)
+          from hearsched "h"
+          left outer join staff "s" on "s".sattyid = "h".board_member
+          where "h".folder_nr="cases".bfkey
+        ) hearings,
+        (select
+          JSON_ARRAYAGG(JSON_OBJECT(
+            'id' value "i".isskey,
+            'vacols_sequence_id' value "i".issseq,
+            'issprog' value "i".issprog,
+            'isscode' value "i".isscode,
+            'isslev1' value "i".isslev1,
+            'isslev2' value "i".isslev2,
+            'isslev3' value "i".isslev3,
+            'issdc' value "i".issdc,
+            'issdesc' value "i".issdesc,
+            'issdcls' value "i".issdcls,
+            'issmst' value "i".issmst,
+            'isspact' value "i".isspact,
+            'issprog_label' value "iss".prog_desc,
+            'isscode_label' value "iss".iss_desc,
+            'isslev1_label' value case when "i".isslev1 is not null then
+              case when "iss".lev1_code = '##' then
+                "vft".ftdesc else "iss".lev1_desc
+              end
+            end ,
+            'isslev2_label' value case when "i".isslev2 is not null then
+              case when "iss".lev2_code = '##' then
+                "vft".ftdesc else "iss".lev2_desc
+              end
+            end,
+            'isslev3_label' value case when "i".isslev3 is not null then
+              case when "iss".lev3_code = '##' then
+                "vft".ftdesc else "iss".lev3_desc
+              end
+            end
+          ) RETURNING CLOB)
+          from issues "i"
+          inner join issref "iss"
+            on "i".issprog = "iss".prog_code
+            and "i".isscode = "iss".iss_code
+            and ("i".isslev1 is null
+                or "iss".lev1_code = '##'
+                or "i".isslev1 = "iss".lev1_code)
+            and ("i".isslev2 is null
+                or "iss".lev2_code = '##'
+                or "i".isslev2 = "iss".lev2_code)
+            and ("i".isslev3 is null
+                or "iss".lev3_code = '##'
+                or "i".isslev3 = "iss".lev3_code)
+          left join vftypes "vft"
+            on "vft".fttype = 'dg'
+            and (("iss".lev1_code = '##' and 'dg' || "i".isslev1 = "vft".ftkey)
+            or ("iss".lev2_code = '##' and 'dg' || "i".isslev2 = "vft".ftkey)
+            or ("iss".lev3_code = '##' and 'dg' || "i".isslev3 = "vft".ftkey))
+          where "i".isskey="cases".bfkey
+        ) issues,
+        (select count("hearings".hearing_pkseq) from hearsched "hearings" where "hearings".folder_nr="cases".bfkey) hearing_count,
+        (select count("issues".isskey) from issues "issues" where "issues".isskey="cases".bfkey and "issues".issmst='Y') issues_mst_count,
+        (select count("issues".isskey) from issues "issues" where "issues".isskey="cases".bfkey and "issues".isspact='Y') issues_pact_count
+      from
+        brieff "cases"
+      left join folder "folders"
+        on "cases".bfkey="folders".ticknum
+      left join corres "correspondents"
+        on "cases".bfcorkey="correspondents".stafkey
+      #{VACOLS::Case::JOIN_AOD}
+      where
+        "cases".bfkey in (?)
+    SQL
+  end
+
+  private
+
+  def case_hearing_venue_select
+    <<-SQL
+      case
+        when "h".hearing_type='#{VACOLS::CaseHearing::HEARING_TYPE_LOOKUP[:video]}' AND
+              "h".hearing_date < '#{VACOLS::CaseHearing::VACOLS_VIDEO_HEARINGS_END_DATE}'
+        then #{Rails.application.config.vacols_db_name}.HEARING_VENUE("h".vdkey)
+        else "cases".bfregoff
+      end
+    SQL
+  end
+end
