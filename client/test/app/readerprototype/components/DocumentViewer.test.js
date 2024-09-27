8fe494f8c4cd57668d0eae472020f739125cb8f5
Brian Bommarito <brian@bommarito.me>
2024-09-27 10:33:29 -0400

    Making document reads persist. (#22936)
    * Making document reads persist.
    
    * Adding test to insure api is called.
    
    * Change the store to use rootReducer.
    
    * Fixing linting issues.
    
    * Adding a test to make sure previous/next work.

--- client/test/app/readerprototype/components/DocumentViewer.test.js
+++ client/test/app/readerprototype/components/DocumentViewer.test.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
-import { render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router, MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import pdfViewerReducer from '../../../../app/reader/PdfViewer/PdfViewerReducer';
-import DocumentViewer from '../../../../app/readerprototype/DocumentViewer';
-import { MemoryRouter } from 'react-router-dom';
+import DocumentViewer from 'app/readerprototype/DocumentViewer';
+import ApiUtil from 'app/util/ApiUtil';
+import { rootReducer } from 'app/reader/reducers';
+import userEvent from "@testing-library/user-event";
 
 afterEach(() => jest.clearAllMocks());
 
@@ -18,36 +20,8 @@ const doc = {
   category_other: false,
   category_medical: false,
   category_case_summary: false,
+  opened_by_current_user: false,
 };
-
-const getStore = () =>
-  createStore(
-    pdfViewerReducer,
-    {
-      documents: { 1: doc },
-      pdfViewer: {
-        pdfSideBarError: {
-          category: {
-            visible: false,
-          },
-        },
-        tagOptions: [],
-        openedAccordionSections: ["Issue tags", "Comments", "Categories"],
-      },
-      documentList: {
-        searchCategoryHighlights: [{ 1: {} }, { 2: {} }],
-        filteredDocIds: [
-          1,
-          2,
-        ],
-        docFilterCriteria: {},
-      },
-      annotationLayer: {
-        annotations: 1,
-      },
-    },
-    applyMiddleware(thunk)
-  );
 
 const props = {
   allDocuments: [
@@ -56,25 +30,25 @@ const props = {
       category_medical: null,
       category_other: null,
       category_procedural: true,
-      created_at: "2024-09-17T12:30:52.925-04:00",
+      created_at: '2024-09-17T12:30:52.925-04:00',
       description: null,
-      file_number: "216979849",
+      file_number: '216979849',
       previous_document_version_id: null,
-      received_at: "2024-09-14",
-      series_id: "377120",
-      type: "NOD",
-      updated_at: "2024-09-17T12:41:11.000-04:00",
-      upload_date: "2024-09-15",
-      vbms_document_id: "1",
-      content_url: "/document/39/pdf",
-      filename: "filename-798447.pdf",
+      received_at: '2024-09-14',
+      series_id: '377120',
+      type: 'NOD',
+      updated_at: '2024-09-17T12:41:11.000-04:00',
+      upload_date: '2024-09-15',
+      vbms_document_id: '1',
+      content_url: '/document/39/pdf',
+      filename: 'filename-798447.pdf',
       category_case_summary: true,
-      serialized_vacols_date: "",
-      serialized_receipt_date: "09/14/2024",
+      serialized_vacols_date: '',
+      serialized_receipt_date: '09/14/2024',
       matching: false,
       opened_by_current_user: false,
       tags: [],
-      receivedAt: "2024-09-14",
+      receivedAt: '2024-09-14',
       listComments: false,
       wasUpdated: false,
     },
@@ -83,37 +57,71 @@ const props = {
       category_medical: null,
       category_other: null,
       category_procedural: true,
-      created_at: "2024-09-17T12:30:52.925-04:00",
+      created_at: '2024-09-17T12:30:52.925-04:00',
       description: null,
-      file_number: "216979849",
+      file_number: '216979849',
       previous_document_version_id: null,
-      received_at: "2024-09-14",
-      series_id: "377120",
-      type: "NOD",
-      updated_at: "2024-09-17T12:41:11.000-04:00",
-      upload_date: "2024-09-15",
-      vbms_document_id: "1",
-      content_url: "/document/39/pdf",
-      filename: "filename-798447.pdf",
+      received_at: '2024-09-14',
+      series_id: '377120',
+      type: 'NOD',
+      updated_at: '2024-09-17T12:41:11.000-04:00',
+      upload_date: '2024-09-15',
+      vbms_document_id: '1',
+      content_url: '/document/39/pdf',
+      filename: 'filename-798447.pdf',
       category_case_summary: true,
-      serialized_vacols_date: "",
-      serialized_receipt_date: "09/14/2024",
+      serialized_vacols_date: '',
+      serialized_receipt_date: '09/14/2024',
       matching: false,
       opened_by_current_user: false,
       tags: [],
-      receivedAt: "2024-09-14",
+      receivedAt: '2024-09-14',
       listComments: false,
       wasUpdated: false,
     },
   ],
   showPdf: jest.fn(),
-  documentPathBase: "/3575931/documents",
+  documentPathBase: '/3575931/documents',
   match: {
-    params: { docId: "1", vacolsId: "3575931" },
+    params: { docId: '1', vacolsId: '3575931' },
   },
 
 };
 
+const getStore = () =>
+  createStore(
+    rootReducer,
+    {
+      annotationLayer: {
+        annotations: 1,
+        deleteAnnotationModalIsOpenFor: null,
+        shareAnnotationModalIsOpenFor: null
+      },
+      documents: { 1: doc },
+      documentList: {
+        pdfList: {
+          lastReadDocId: null,
+        },
+        searchCategoryHighlights: [{ 1: {} }, { 2: {} }],
+        filteredDocIds: [
+          1,
+          2,
+        ],
+        docFilterCriteria: {},
+        pdfViewer: {
+          pdfSideBarError: {
+            category: {
+              visible: false,
+            },
+          },
+          tagOptions: [],
+          openedAccordionSections: ['Issue tags', 'Comments', 'Categories'],
+        },
+      },
+    },
+    applyMiddleware(thunk));
+
+// <Router history={history}>
 const Component = () => (
   <Provider store={getStore()}>
     <MemoryRouter>
@@ -121,6 +129,26 @@ const Component = () => (
     </MemoryRouter>
   </Provider>
 );
+
+describe('user visiting a document', () => {
+
+  beforeEach(() => {
+    jest.mock('app/util/ApiUtil', () => ({
+      patch: jest.fn(),
+    }));
+  });
+
+  it('records the viewing of the document', () => {
+    const spy = jest.spyOn(ApiUtil, 'patch');
+
+    render(<Component {...props} />);
+    expect(spy).
+      toHaveBeenCalledWith(
+        '/document/1/mark-as-read',
+        { start: '2020-07-06T06:00:00-04:00', t0: 'RUNNING_IN_NODE' },
+        'mark-doc-as-read');
+  });
+});
 
 describe('Open Document and Close Issue tags Sidebar Section', () => {
   it('Navigate to next document and verify Issue tags stay closed', async () => {
@@ -145,5 +173,6 @@ describe('Open Document and Close Issue tags Sidebar Section', () => {
     waitFor(() =>
       expect(container).not.toHaveTextContent('Select or tag issues')
     );
+
   });
 });
