60f0ddfd6e88f76c1f50fe91eca1f9ab1454ba32
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 23:45:16 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/vbms_service.rb
+++ config/initializers/vbms_service.rb
@@ -1,6 +1,8 @@
-VBMSService = (!ApplicationController.dependencies_faked? ? ExternalApi::VBMSService : Fakes::VBMSService)
+Rails.application.config.to_prepare do
+  VBMSService = (!ApplicationController.dependencies_faked? ? ExternalApi::VBMSService : Fakes::VBMSService)
 
-if ApplicationController.dependencies_faked?
-  VBMSService.manifest_vbms_fetched_at = Time.zone.now
-  VBMSService.manifest_vva_fetched_at = Time.zone.now
+  if ApplicationController.dependencies_faked?
+    VBMSService.manifest_vbms_fetched_at = Time.zone.now
+    VBMSService.manifest_vva_fetched_at = Time.zone.now
+  end
 end
