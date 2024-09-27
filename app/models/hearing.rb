5a6c37f72c1183741f6fde3ebbc0bd600eb2be1e
ryanpmessner <163380175+ryanpmessner@users.noreply.github.com>
2024-09-27 15:33:01 -0400

    hotfix/APPEALS-23420: search defect
    Add search query service for the api response for the `/search` page
      Add issues to legacy appeals
      Add assigned_to_judge
      Use correct id for list of request issues
      export proper attributes for hearing

--- app/models/hearing.rb
+++ app/models/hearing.rb
@@ -187,12 +187,15 @@ class Hearing < CaseflowRecord
       .first
   end
 
+  def scheduled_for
+    scheduled_for_hearing_day(hearing_day, updated_by, regional_office_timezone)
+  end
+
   # returns scheduled datetime object considering the timezones
   # @return [nil] if hearing_day is nil
   # @return [Time] in scheduled_in_timezone timezone - if scheduled_datetime and scheduled_in_timezone are present
   # @return [Time] else datetime in regional office timezone
-  # rubocop:disable Metrics/AbcSize
-  def scheduled_for
+  def scheduled_for_hearing_day(hearing_day, updated_by, regional_office_timezone)
     return nil unless hearing_day
 
     # returns datetime in scheduled_in_timezone timezone
@@ -234,7 +237,6 @@ class Hearing < CaseflowRecord
       )
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
   def scheduled_for_past?
     scheduled_for < DateTime.yesterday.in_time_zone(regional_office_timezone)
