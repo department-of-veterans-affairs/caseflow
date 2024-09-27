d5a656e05390fc60d37a3fda633cb18cb8b353bc
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 23:06:03 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/pexip_service.rb
+++ config/initializers/pexip_service.rb
@@ -1,1 +1,3 @@
-PexipService = (ApplicationController.dependencies_faked? ? Fakes::PexipService : ExternalApi::PexipService)
+Rails.application.config.to_prepare do
+  PexipService = (ApplicationController.dependencies_faked? ? Fakes::PexipService : ExternalApi::PexipService)
+end
