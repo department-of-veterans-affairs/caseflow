377e39632f77f7c86b0fc0c9afad0ec95f30c193
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 22:36:54 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/gov_delivery_service.rb
+++ config/initializers/gov_delivery_service.rb
@@ -1,5 +1,7 @@
-GovDeliveryService = if ApplicationController.dependencies_faked?
-                       Fakes::GovDeliveryService
-                     else
-                       ExternalApi::GovDeliveryService
-                     end
+Rails.application.config.to_prepare do
+  GovDeliveryService = if ApplicationController.dependencies_faked?
+                         Fakes::GovDeliveryService
+                       else
+                         ExternalApi::GovDeliveryService
+                       end
+end
