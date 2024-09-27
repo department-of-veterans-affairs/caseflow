067127b4502322f47d5e905a40a614bd48b8a1ac
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 23:20:03 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/s3_service.rb
+++ config/initializers/s3_service.rb
@@ -1,3 +1,5 @@
-require 'caseflow'
+Rails.application.config.to_prepare do
+  require 'caseflow'
 
-S3Service = (Rails.application.config.s3_enabled ? Caseflow::S3Service : Caseflow::Fakes::S3Service)
+  S3Service = (Rails.application.config.s3_enabled ? Caseflow::S3Service : Caseflow::Fakes::S3Service)
+end
