3750c1b768d02cc15f288102aa4e4effa0107362
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-09-26 22:25:58 -0400

    🔧 Run initializer on reloader callback
    This initializer autoloads constants.
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/efolder_service.rb
+++ config/initializers/efolder_service.rb
@@ -1,6 +1,8 @@
-EFolderService =
-  if !ApplicationController.dependencies_faked? || Rails.application.config.use_efolder_locally
-    ExternalApi::EfolderService
-  else
-    Fakes::VBMSService
-  end
+Rails.application.config.to_prepare do
+  EFolderService =
+    if !ApplicationController.dependencies_faked? || Rails.application.config.use_efolder_locally
+      ExternalApi::EfolderService
+    else
+      Fakes::VBMSService
+    end
+end
