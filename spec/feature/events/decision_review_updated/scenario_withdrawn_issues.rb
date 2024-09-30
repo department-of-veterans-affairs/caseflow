4549c1fc161bdfef82443f197d0e0b771a028e56
KonstantinShevtsov <Konstantin.Shevtsov@va.gov>
2024-09-30 16:31:28 -0400

    withdrawn feature test added

---
+++ spec/feature/events/decision_review_updated/scenario_withdrawn_issues.rb
@@ -0,0 +1,138 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Api::Events::V1::DecisionReviewUpdatedController, type: :controller do
+  let!(:current_user) { User.authenticate! }
+  let(:api_key) { ApiKey.create!(consumer_name: "API TEST TOKEN") }
+  let!(:epe) { create(:end_product_establishment, :active_hlr, reference_id: 12_345_678) }
+  let(:review) { epe.source }
+
+  context "tests processing withdrawn request issues" do
+    describe "POST #decision_review_updated" do
+      let!(:withdrawn_request_issues) do
+        create(:request_issue,
+               decision_review: review, reference_id: "1234", closed_status: nil, closed_at: nil,
+               contention_removed_at: DateTime.now, contention_reference_id: 100_500)
+      end
+
+      def json_test_payload
+        {
+          "event_id": 214_786,
+          "claim_id": 12_345_678,
+          "css_id": "BVADWISE101",
+          "detail_type": "HigherLevelReview",
+          "station": "101",
+          "intake": {
+            "started_at": 1_702_067_143_435,
+            "completion_started_at": 1_702_067_145_000,
+            "completed_at": 1_702_067_145_000,
+            "completion_status": "success",
+            "type": "HigherLevelReviewIntake",
+            "detail_type": "HigherLevelReview"
+          },
+          "veteran": {
+            "participant_id": "1826209",
+            "bgs_last_synced_at": 1_708_533_584_000,
+            "name_suffix": nil,
+            "date_of_death": nil
+          },
+          "claimant": {
+            "payee_code": "00",
+            "type": "VeteranClaimant",
+            "participant_id": "1826209",
+            "name_suffix": nil
+          },
+          "claim_review": {
+            "benefit_type": "compensation",
+            "filed_by_va_gov": false,
+            "legacy_opt_in_approved": false
+          },
+          "end_product_establishment": {
+            "code": "030HLRNR",
+            "reference_id": "12345678",
+            "last_synced_at": 1_702_067_145_000,
+            "synced_status": "RFD",
+            "development_item_reference_id": "1"
+          },
+          "updated_issues": [],
+          "removed_issues": [],
+          "withdrawn_issues": [
+            {
+              "original_caseflow_request_issue_id": 12_345,
+              "contested_rating_decision_reference_id": nil,
+              "contested_rating_issue_reference_id": nil,
+              "contested_decision_issue_id": nil,
+              "untimely_exemption": false,
+              "untimely_exemption_notes": nil,
+              "edited_description": "DIC: Service connection denied (UPDATED)",
+              "vacols_id": nil,
+              "vacols_sequence_id": nil,
+              "nonrating_issue_bgs_id": nil,
+              "type": "RequestIssue",
+              "decision_review_issue_id": 1234,
+              "contention_reference_id": 123_456,
+              "benefit_type": "compensation",
+              "contested_issue_description": nil,
+              "contested_rating_issue_profile_date": nil,
+              "decision_date": nil,
+              "ineligible_due_to_id": nil,
+              "ineligible_reason": nil,
+              "unidentified_issue_text": "An unidentified issue added during the edit",
+              "nonrating_issue_category": nil,
+              "nonrating_issue_description": nil,
+              "closed_at": 1_702_000_145_000,
+              "closed_status": nil,
+              "contested_rating_issue_diagnostic_code": nil,
+              "rating_issue_associated_at": nil,
+              "ramp_claim_id": nil,
+              "is_unidentified": true,
+              "nonrating_issue_bgs_source": nil
+            }
+          ],
+          "ineligible_to_eligible_issues": [],
+          "eligible_to_ineligible_issues": [],
+          "ineligible_to_ineligible_issues": []
+        }
+      end
+
+      let!(:valid_params) do
+        json_test_payload
+      end
+
+      context "withdrawn issue" do
+        before do
+          request.headers["Authorization"] = "Token token=#{api_key.key_string}"
+        end
+
+        it "returns success response whith updated closed_at date" do
+          request.headers["Authorization"] = "Token token=#{api_key.key_string}"
+          expect(withdrawn_request_issues.reference_id).to eq("1234")
+          # expect(withdrawn_request_issues.ineligible_reason).to eq(nil)
+          # expect(withdrawn_request_issues.contested_issue_description).to eq("original description")
+          # expect(withdrawn_request_issues.nonrating_issue_category).to eq("original category")
+          expect(withdrawn_request_issues.closed_at).to eq(nil)
+          expect(withdrawn_request_issues.contention_reference_id).to eq(100_500)
+          # byebug
+          post :decision_review_updated, params: valid_params
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to include("DecisionReviewUpdatedEvent successfully processed")
+          withdrawn_request_issues.reload
+          # byebug
+          expect(withdrawn_request_issues.closed_at).to eq("2023-12-07 20:49:05.000000000 -0500")
+          expect(withdrawn_request_issues.closed_status).to eq("withdrawn")
+          expect(withdrawn_request_issues.contention_removed_at).to be_within(100.seconds).of(DateTime.now)
+          expect(withdrawn_request_issues.updated_at).to be_within(100.seconds).of(DateTime.now)
+          # expect(withdrawn_request_issues.ineligible_reason).to eq(nil)
+          # expect(withdrawn_request_issues.contested_issue_description).to eq("UPDATED DESCRIPTION")
+          # expect(withdrawn_request_issues.nonrating_issue_category).to eq("Military Retired Pay UPDATED")
+          # expect(withdrawn_request_issues.nonrating_issue_description).to eq("UPDATED TESTING")
+          # expect(withdrawn_request_issues.closed_at).to eq(nil)
+          # expect(withdrawn_request_issues.closed_status).to eq(nil)
+          # expect(withdrawn_request_issues.contention_removed_at).to eq(nil)
+          # expect(withdrawn_request_issues.contention_reference_id).to eq(123_456)
+        end
+      end
+    end
+  end
+end
