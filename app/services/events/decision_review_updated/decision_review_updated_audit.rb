d789a03a8eb351ac2487f0a3105c83ab15e2ac8b
Nader Kutub <nader@nkconsulting.net>
2024-09-29 18:45:49 -0700

    bug fixes for error logs, audits, claim id

--- app/services/events/decision_review_updated/decision_review_updated_audit.rb
+++ app/services/events/decision_review_updated/decision_review_updated_audit.rb
@@ -1,17 +1,90 @@
 # frozen_string_literal: true
 
 class Events::DecisionReviewUpdated::DecisionReviewUpdatedAudit
-  def initialize(event:, request_issue:, update_type:)
+  def initialize(event:, parser:)
     @event = event
-    @request_issue = request_issue
-    @update_type = update_type
+    @parser = parser
   end
 
-  def call
-    EventRecord.create!(
-      event: @event,
-      evented_record: @request_issue,
-      info: { update_type: @update_type, record_data: @request_issue }
-    )
+  def call!
+    audit_updated_request_issue
+    audit_added_request_issue
+    audit_removed_request_issue
+    audit_ineligible_to_eligible_request_issue
+    audit_eligible_to_ineligible_request_issue
+    audit_ineligible_to_ineligible_request_issue
   end
+
+  def audit_updated_request_issue
+    @parser.updated_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "U", record_data: request_issue }
+      )
+    end
+  end
+
+  def audit_added_request_issue
+    @parser.added_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "A", record_data: request_issue, event_data: request_issue_data }
+      )
+    end
+  end
+
+  def audit_removed_request_issue
+    @parser.removed_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "R", record_data: request_issue, event_data: request_issue_data }
+      )
+    end
+  end
+
+  def audit_ineligible_to_eligible_request_issue
+    @parser.ineligible_to_eligible_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "I2E", record_data: request_issue, event_data: request_issue_data }
+      )
+    end
+  end
+
+  def audit_eligible_to_ineligible_request_issue
+    @parser.eligible_to_ineligible_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "E2I", record_data: request_issue, event_data: request_issue_data }
+      )
+    end
+  end
+
+  def audit_ineligible_to_ineligible_request_issue
+    @parser.ineligible_to_ineligible_issues.each do |request_issue_data|
+      request_issue = find_request_issue(request_issue_data)
+      EventRecord.create!(
+        event: @event,
+        evented_record: request_issue,
+        info: { update_type: "I2I", record_data: request_issue, event_data: request_issue_data }
+      )
+    end
+  end
+
+  def find_request_issue(request_issues_data)
+    RequestIssue.find_by(reference_id: request_issues_data[:reference_id]) ||
+      fail(Caseflow::Error::DecisionReviewUpdateMissingIssueError, request_issues_data[:reference_id])
+  end
+
+
 end
