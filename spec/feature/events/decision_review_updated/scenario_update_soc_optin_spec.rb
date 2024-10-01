6e06dc814c8e2e6e0794da582e56a208a4cbcca3
Jonathan Tsang <98970951+jtsangVA@users.noreply.github.com>
2024-09-30 21:56:16 -0400

    Jonathan/appeals 59166 (#22972)
    * feature test, update hlr/sc optin value
    
    * featuretoggle change
    
    * featuretoggle

--- spec/feature/events/decision_review_updated/scenario_update_soc_optin_spec.rb
+++ spec/feature/events/decision_review_updated/scenario_update_soc_optin_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Api::Events::V1::DecisionReviewUpdatedController, type: :controll
     let!(:current_user) { User.authenticate! }
     let(:api_key) { ApiKey.create!(consumer_name: "API TEST TOKEN") }
     let!(:epe) { create(:end_product_establishment, :active_hlr, reference_id: 1234567) }
+    let!(:hlr) { epe.source }
+    let!(:epe_2) { create(:end_product_establishment, :active_supp, reference_id: 9876543) }
+    let!(:sc) { epe_2.source }
 
     def json_test_payload
       {
@@ -18,14 +21,12 @@ RSpec.describe Api::Events::V1::DecisionReviewUpdatedController, type: :controll
         "claim_review": {
           "informal_conference": false,
           "same_office": false,
-          "legacy_opt_in_approved": false
+          "legacy_opt_in_approved": true
         },
         "end_product_establishment": {
           "code": "030HLRR",
           "development_item_reference_id": "1",
-          "reference_id": "1234567",
-          "synced_status": "RFD",
-          "last_synced_at": 1726688419000
+          "reference_id": "1234567"
         },
         "added_issues": nil,
         "updated_issues": nil,
@@ -37,23 +38,49 @@ RSpec.describe Api::Events::V1::DecisionReviewUpdatedController, type: :controll
       }
     end
 
+    def json_test_payload_2
+      {
+        "event_id": 214707,
+        "claim_id": 9876543,
+        "css_id": "BVADWISE101",
+        "detail_type": "SupplementalClaim",
+        "station": "101",
+        "claim_review": {
+          "informal_conference": false,
+          "same_office": false,
+          "legacy_opt_in_approved": true
+        },
+        "end_product_establishment": {
+          "reference_id": "9876543"
+        }
+      }
+    end
+
     let!(:valid_params) do
       json_test_payload
     end
 
-    context "a payload that ONLY affects the EPE" do
+    let!(:valid_params_2) do
+      json_test_payload_2
+    end
+
+    context "a payload that ONLY affects the soc optin on the claim review" do
       before do
         FeatureToggle.disable!(:disable_ama_eventing)
-        request.headers["Authorization"] = "Token token=#{api_key.key_string}"
       end
-      it "updates the EPE accordingly" do
+      it "updates the HLR accordingly" do
         request.headers["Authorization"] = "Token #{api_key.key_string}"
         post :decision_review_updated, params: valid_params
         expect(response).to have_http_status(:ok)
-        epe.reload
-        expect(epe.code).to eq("030HLRR")
-        expect(epe.synced_status).to eq("RFD")
-        expect(epe.last_synced_at).to_not be_nil
+        hlr.reload
+        expect(hlr.legacy_opt_in_approved).to eq(true)
+      end
+      it "also updates SC's accordingly" do
+        request.headers["Authorization"] = "Token #{api_key.key_string}"
+        post :decision_review_updated, params: valid_params_2
+        expect(response).to have_http_status(:ok)
+        sc.reload
+        expect(sc.legacy_opt_in_approved).to eq(true)
       end
     end
   end
