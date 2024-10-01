9301186bf6f1a08ca9592b375cc127932b897d57
Michael Beard <michael.beard@va.gov>
2024-10-01 11:56:51 -0500

    initial spec creation

---
+++ spec/feature/optional_seeds_response_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "acd-controls/test Page Run Generic Full Suite Appeals Seeds Button" do
+  let!(:current_user) do
+    user = create(:user, css_id: "BVALNICK")
+    CDAControlGroup.singleton.add_user(user)
+    User.authenticate!(user: user)
+  end
+
+  let(:success_response) do
+    HTTPI::Response.new(200, {}, {})
+  end
+
+  before do
+    allow(HTTPI).to receive(:post).with("/test/optional_seed").and_return(success_response)
+  end
+
+  scenario "user visits the acd-controls/test page and clicks the button" do
+    visit "/acd-controls/test"
+
+    click_button "Run Generic Full Suite Appeals Seeds"
+
+    expect(page).to have_content(COPY::TEST_RESEED_GENERIC_FULL_SUITE_APPEALS_ALERTMSG)
+  end
+end
