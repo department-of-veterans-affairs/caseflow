f090f7c4f135ad9081c663b69d085839b284b62a
laurenyj <44596134+laurenyj@users.noreply.github.com>
2024-10-03 13:14:21 -0500

    Clean up DocumentViewer.test.js

--- client/test/app/readerprototype/components/DocumentViewer.test.js
+++ client/test/app/readerprototype/components/DocumentViewer.test.js
@@ -51,15 +51,19 @@ const Component = () => {
   return (
     <Provider store={getStore()}>
       <MemoryRouter history={history}>
-        <DocumentViewer {...get.props} zoomLevel={zoomLevel}
-          onZoomChange={(newZoomLevel) => setZoomLevel(newZoomLevel)} />
+        <DocumentViewer
+          {...get.props}
+          onZoomChange={(newZoomLevel) => setZoomLevel(newZoomLevel)}
+          zoomLevel={zoomLevel}
+        />
       </MemoryRouter>
     </Provider>
   );
 };
 
-describe('user visiting a document', () => {
-  it('records the viewing of the document', () => {
+describe('Marked as Read', () => {
+  it('marks document with docId 1 as read', () => {
+    jest.clearAllMocks();
     const spy = jest.spyOn(ApiUtil, 'patch');
 
     render(<Component />);
@@ -72,12 +76,15 @@ describe('user visiting a document', () => {
 });
 
 describe('Sidebar Section', () => {
-  it('closes Sidebar and verify it stays closed on the next document', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
     jest.spyOn(ApiUtil, 'patch').mockResolvedValue();
+  });
 
+  it('closes the Issue Tags section and verify it stays closed on next document', async () => {
     const { container, getByText } = render(<Component />);
 
-    expect(container).toHaveTextContent('Document 1 of 2');
+    expect(container).toHaveTextContent('Document 1 of 5');
     // there are 3 open sections in the sidebar
     expect(container.querySelectorAll('div.rc-collapse-item-active').length).toEqual(3);
     userEvent.click(getByText('Issue tags'));
@@ -86,94 +93,66 @@ describe('Sidebar Section', () => {
 
     userEvent.click(getByText('Next'));
     // we make sure we are on the next document
-    await waitFor(() => expect(container).toHaveTextContent('Document 2 of 2'));
+    await waitFor(() => expect(container).toHaveTextContent('Document 2 of 5'));
     // there are still only 2 open sections in the sidebar
     expect(container.querySelectorAll('div.rc-collapse-item-active').length).toEqual(2);
   });
+
+  it('closes Sidebar and verify it stays closed on the next document', async () => {
+    const { container, getByText } = render(<Component />);
+
+    expect(container).toHaveTextContent('Document 1 of 5');
+    // Initially, the sidebar should be visible with button to close
+    expect(container).toHaveTextContent('Hide menu');
+    // Simulate clicking 'Hide menu' to close menu
+    userEvent.click(getByText('Hide menu'));
+    // Sidebar should have 'Open menu' to reopen sidebar
+    expect(container).toHaveTextContent('Open menu');
+
+    // Simulate navigating to another document
+    userEvent.click(getByText('Next'));
+    // we make sure we are on the next document
+    await waitFor(() => expect(container).toHaveTextContent('Document 2 of 5'));
+    // Sidebar should remain hidden and have open menu
+    expect(container).toHaveTextContent('Open menu');
+  });
 });
 
-it('should change zoom level to 90%, then to 80% to simulate parent states update', async () => {
-  jest.spyOn(ApiUtil, 'patch').mockResolvedValue();
+describe('Zoom', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.spyOn(ApiUtil, 'patch').mockResolvedValue();
+  });
 
-  const { container, getByRole } = render(<Component />);
+  it('zooms out and verify zoom level persists on next document', async () => {
+    const { container, getByRole, getByText } = render(<Component />);
 
-  expect(container).toHaveTextContent('100%');
-  const zoomOutButton = getByRole('button', { name: /zoom out/i });
+    await waitFor(() => expect(container).toHaveTextContent('Document 1 of 5'));
+    expect(container).toHaveTextContent('100%');
+    const zoomOutButton = getByRole('button', { name: /zoom out/i });
 
-  userEvent.click(zoomOutButton);
-  await waitFor(() => expect(container).toHaveTextContent('90%'));
-  userEvent.click(zoomOutButton);
-  await waitFor(() => expect(container).toHaveTextContent('80%'));
+    userEvent.click(zoomOutButton);
+    await waitFor(() => expect(container).toHaveTextContent('90%'));
+
+    userEvent.click(getByText('Next'));
+    // await waitFor(() => expect(container).toHaveTextContent('Document 2 of 5'));
+    await waitFor(() => expect(container).toHaveTextContent('90%'));
+  });
+
+  it('zooms in and verify zoom level persists on previous document', async () => {
+    const { container, getByRole, getByText } = render(<Component />);
+
+    await waitFor(() => expect(container).toHaveTextContent('Document 1 of 5'));
+    expect(container).toHaveTextContent('100%');
+    userEvent.click(getByText('Next'));
+    await waitFor(() => expect(container).toHaveTextContent('Document 2 of 5'));
+
+    const zoomInButton = getByRole('button', { name: /zoom in/i });
+
+    userEvent.click(zoomInButton);
+    await waitFor(() => expect(container).toHaveTextContent('110%'));
+    userEvent.click(getByText('Previous'));
+    // await waitFor(() => expect(container).toHaveTextContent('Document 1 of 5'));
+    await waitFor(() => expect(container).toHaveTextContent('110%'));
+  });
 });
-
-it('Sidebar remembers its state between document views', async () => {
-  jest.spyOn(ApiUtil, 'patch').mockResolvedValue();
-
-  const { container, getByText } = render(<Component />);
-
-  expect(container).toHaveTextContent('Document 1 of 2');
-  // Initially, the sidebar should be visible with button to close
-  expect(container).toHaveTextContent('Hide menu');
-
-  // Simulate clicking 'Hide menu' to close menu
-  userEvent.click(getByText('Hide menu'));
-
-  // Sidebar should have 'Open menu' to reopen sidebar
-  expect(container).toHaveTextContent('Open menu');
-
-  // Simulate navigating to another document
-  userEvent.click(getByText('Next'));
-
-  // we make sure we are on the next document
-  await waitFor(() => expect(container).toHaveTextContent('Document 2 of 2'));
-  // Sidebar should remain hidden and have open menu
-  expect(container).toHaveTextContent('Open menu');
-});
-
-// describe('Open Document and Test Column Layout', () => {
-//   it('should change layout from single column to double column at larger width', async () => {
-//     const raw = fs.readFileSync('test/fixtures/pdfs/Informal_Form9.pdf');
-//     const arrayBuffer = raw.buffer;
-
-//     jest.spyOn(ApiUtil, 'patch').mockResolvedValue();
-//     jest.spyOn(ApiUtil, 'get').mockResolvedValueOnce({ body: arrayBuffer });
-//     // Initial render at width 1080 (single column)
-//     global.innerWidth = 1080;
-//     const { container, getByTitle, debug } = render(<Component doc={{}} document={{}} />);
-
-//     // verify initial width
-//     expect(global.innerWidth).toBe(1080);
-//     await waitFor(() => expect(container).not.toHaveTextContent('Loading document...'));
-
-//     // Simulate typing 2 into the page number text box
-//     debug(container, 200000);
-//     const pageNumberTextBox = getByTitle('Page');
-
-//     userEvent.type(pageNumberTextBox, '{backspace}2{enter}');
-
-//     // Verify the textbox now holds "2"
-//     await waitFor(() =>
-//       expect(container.querySelector('#page-progress-indicator-input').value).toBe(2)
-//     );
-
-//     // Now simulate increasing the screen width to 2000px (double column layout)
-//     global.innerWidth = 2000;
-//     global.dispatchEvent(new Event('resize'));
-//     expect(global.innerWidth).toBe(2000);
-
-//     // After resizing, the layout should change and the text box should now display 1
-//     await waitFor(() =>
-//       expect(container).querySelector('#page-progress-indicator-input').value.toBe(1)
-//     );
-
-//     // Simulate a smaller width (1100px), where it should still be a single column layout
-//     global.innerWidth = 1100;
-//     global.dispatchEvent(new Event('resize'));
-//     expect(global.innerWidth).toBe(1100);
-
-//     // The page number should still remain 1
-//     await waitFor(() =>
-//       expect(container).querySelector('#page-progress-indicator-input').value.toBe('1')
-//     );
-//   });
-// });
--- client/test/app/readerprototype/mockReaderStore.js
+++ client/test/app/readerprototype/mockReaderStore.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from 'app/reader/reducers';
-import { def, get } from 'bdd-lazy-var/getter';
+import { get } from 'bdd-lazy-var/getter';
 
 const getStore = (documentListOverride = {}) => (
   createStore(
@@ -12,16 +12,19 @@ const getStore = (documentListOverride = {}) => (
         deleteAnnotationModalIsOpenFor: null,
         shareAnnotationModalIsOpenFor: null,
       },
-      documents: { 1: get.document1, 2: get.document2 },
+      documents: {
+        1: get.document1,
+        2: get.document2,
+        3: get.document3,
+        4: get.document4,
+        5: get.document5,
+      },
       documentList: {
         pdfList: {
           lastReadDocId: null,
         },
         searchCategoryHighlights: [{ 1: {} }, { 2: {} }],
-        filteredDocIds: [
-          1,
-          2,
-        ],
+        filteredDocIds: [1, 2, 3, 4, 5],
         docFilterCriteria: {},
         ...documentListOverride
       },
