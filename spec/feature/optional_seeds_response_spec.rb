cf008add0618f02c33fde5959035b04db319600b
Michael Beard <michael.beard@va.gov>
2024-10-03 11:06:53 -0500

    latest rspec updates and copy

--- spec/feature/optional_seeds_response_spec.rb
+++ spec/feature/optional_seeds_response_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "timecop"
 
 RSpec.feature "acd-controls/test Page Run Generic Full Suite Appeals Seeds Button" do
   let!(:current_user) do
@@ -11,7 +10,7 @@ RSpec.feature "acd-controls/test Page Run Generic Full Suite Appeals Seeds Butto
   end
 
   let(:success_response) do
-    HTTPI::Response.new(200, {}, { seeds_added: 4094 }.to_json)
+    HTTPI::Response.new(200, {}, { appealCount: 4094 }.to_json)
   end
 
   before do
@@ -22,7 +21,9 @@ RSpec.feature "acd-controls/test Page Run Generic Full Suite Appeals Seeds Butto
     visit "/acd-controls/test"
 
     click_button "Run Generic Full Suite Appeals Seeds"
-
-    expect(HTTPI).to have_received(:post)
+    expected_message = COPY::TEST_RESEED_GENERIC_FULL_SUITE_APPEALS_ALERTMSG.gsub("{count}", "4094")
+    expect(page).to have_css '.usa-alert-text'
+    expect(find(".usa-alert-text")).to have_content(expected_message)
+    # expect(page).to have_content(expected_message, wait: 20)
   end
 end
