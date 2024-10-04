347e5c0c3c6cd2e9482c91477b55a82055c0dcb4
Alexander Smith <alex.smith.99@thunderyard.com>
2024-10-04 15:56:43 -0600

    Update VBMSService to Pass ClaimEvidenceRequest to CE API Gem
    - This change will allow the CE API gem to call the CE API using
      the css_id and station_id in the passed ClaimEvidenceRequest
      object.
    
    - The send_current_user_cred_to_ce_api in Caseflow will control
      whether user credentials or system credentials flow the CE API
      gem.

--- spec/services/external_api/vbms_service_spec.rb
+++ spec/services/external_api/vbms_service_spec.rb
@@ -59,7 +59,10 @@ describe ExternalApi::VBMSService do
         expect(mock_sensitivity_checker).to receive(:sensitivity_levels_compatible?)
           .with(user: user, veteran: appeal.veteran).and_return(true)
         expect(VeteranFileFetcher).to receive(:fetch_veteran_file_list)
-          .with(veteran_file_number: appeal.veteran_file_number)
+          .with(
+            veteran_file_number: appeal.veteran_file_number,
+            claim_evidence_request: instance_of(ClaimEvidenceRequest)
+          )
         expect(mock_json_adapter).to receive(:adapt_fetch_document_series_for).and_return([])
 
         described.fetch_document_series_for(appeal)
@@ -93,7 +96,10 @@ describe ExternalApi::VBMSService do
         expect(mock_sensitivity_checker).to receive(:sensitivity_levels_compatible?)
           .with(user: user, veteran: appeal.veteran).and_return(true)
         expect(VeteranFileFetcher).to receive(:fetch_veteran_file_list)
-          .with(veteran_file_number: appeal.veteran_file_number)
+          .with(
+            veteran_file_number: appeal.veteran_file_number,
+            claim_evidence_request: instance_of(ClaimEvidenceRequest)
+          )
         expect(mock_json_adapter).to receive(:adapt_fetch_document_series_for).and_return([])
 
         described.fetch_documents_for(appeal)
@@ -126,7 +132,7 @@ describe ExternalApi::VBMSService do
           .with(user: user, veteran: appeal.veteran).and_return(true)
         expect(VeteranFileFetcher)
           .to receive(:get_document_content)
-          .with(doc_series_id: fake_document.series_id)
+          .with(doc_series_id: fake_document.series_id, claim_evidence_request: instance_of(ClaimEvidenceRequest))
           .and_return("Pdf Byte String")
 
         described.fetch_document_file(fake_document)
@@ -159,6 +165,7 @@ describe ExternalApi::VBMSService do
           .to receive(:update_veteran_file)
           .with(
             veteran_file_number: appeal.veteran_file_number,
+            claim_evidence_request: instance_of(ClaimEvidenceRequest),
             file_uuid: "12345",
             file_update_payload: mock_file_update_payload
           )
@@ -183,71 +190,138 @@ describe ExternalApi::VBMSService do
     end
   end
 
-  describe ExternalApi::VBMSService do
-    describe ".upload_document_to_vbms" do
-      let(:fake_document) do
-        instance_double(
-          "UploadDocumentToVbms",
-          pdf_location: "/path/to/test/location",
-          source: "my_source",
-          document_type_id: 1,
-          document_type: "test",
-          subject: "testing1",
+  describe ".upload_document_to_vbms" do
+    let(:fake_document) do
+      instance_double(
+        "UploadDocumentToVbms",
+        pdf_location: "/path/to/test/location",
+        source: "my_source",
+        document_type_id: 1,
+        document_type: "test",
+        subject: "testing1",
+        new_mail: true
+      )
+    end
+    let(:appeal) { create(:appeal) }
+
+    context "with use_ce_api feature toggle enabled" do
+      before { FeatureToggle.enable!(:use_ce_api) }
+      after { FeatureToggle.disable!(:use_ce_api) }
+
+      let(:mock_file_upload_payload) { instance_double("ClaimEvidenceFileUploadPayload") }
+
+      it "calls the CE API" do
+        allow(SecureRandom).to receive(:uuid).and_return("12345")
+        # rubocop:disable Rails/TimeZone
+        allow(Time).to receive(:current).and_return(Time.parse("2024-07-26"))
+        # rubocop:enable Rails/TimeZone
+        filename = "12345location"
+
+        expect(ClaimEvidenceFileUploadPayload).to receive(:new).with(
+          content_name: filename,
+          content_source: fake_document.source,
+          date_va_received_document: "2024-07-26",
+          document_type_id: fake_document.document_type_id,
+          subject: fake_document.document_type,
           new_mail: true
+        ).and_return(mock_file_upload_payload)
+
+        expect(VeteranFileUploader).to receive(:upload_veteran_file).with(
+          file_path: fake_document.pdf_location,
+          claim_evidence_request: instance_of(ClaimEvidenceRequest),
+          veteran_file_number: appeal.veteran_file_number,
+          doc_info: mock_file_upload_payload
         )
+        expect(mock_json_adapter).to receive(:adapt_upload_document)
+        described_class.upload_document_to_vbms(appeal, fake_document)
       end
-      let(:appeal) { create(:appeal) }
+    end
 
-      context "with use_ce_api feature toggle enabled" do
-        before { FeatureToggle.enable!(:use_ce_api) }
-        after { FeatureToggle.disable!(:use_ce_api) }
+    context "with use_ce_api feature toggle disabled" do
+      before { FeatureToggle.disable!(:use_ce_api) }
 
-        let(:mock_file_upload_payload) { instance_double("ClaimEvidenceFileUploadPayload") }
+      let(:mock_vbms_client) { instance_double("VBMS::Client") }
+      let(:mock_initialize_upload_response) { double(upload_token: "document-token") }
 
-        it "calls the CE API" do
-          allow(SecureRandom).to receive(:uuid).and_return("12345")
-          # rubocop:disable Rails/TimeZone
-          allow(Time).to receive(:current).and_return(Time.parse("2024-07-26"))
-          # rubocop:enable Rails/TimeZone
-          filename = "12345location"
+      it "calls the VBMS client" do
+        allow(described_class).to receive(:init_vbms_client).and_return(mock_vbms_client)
+        allow(described_class).to receive(:initialize_upload)
+          .with(appeal, fake_document).and_return(mock_initialize_upload_response)
+        allow(described_class).to receive(:upload_document)
+          .with(appeal.veteran_file_number, "document-token", fake_document.pdf_location)
 
-          expect(ClaimEvidenceFileUploadPayload).to receive(:new).with(
-            content_name: filename,
-            content_source: fake_document.source,
-            date_va_received_document: "2024-07-26",
-            document_type_id: fake_document.document_type_id,
-            subject: fake_document.document_type,
-            new_mail: true
-          ).and_return(mock_file_upload_payload)
+        described_class.upload_document_to_vbms(appeal, fake_document)
 
-          expect(VeteranFileUploader).to receive(:upload_veteran_file).with(
-            file_path: fake_document.pdf_location,
-            veteran_file_number: appeal.veteran_file_number,
-            doc_info: mock_file_upload_payload
-          )
-          expect(mock_json_adapter).to receive(:adapt_upload_document)
-          described_class.upload_document_to_vbms(appeal, fake_document)
+        expect(described_class).to have_received(:initialize_upload).with(appeal, fake_document)
+        expect(described_class).to have_received(:upload_document)
+          .with(appeal.veteran_file_number, "document-token", fake_document.pdf_location)
+      end
+    end
+  end
+
+  describe ".claim_evidence_request" do
+    context "with send_current_user_cred_to_ce_api feature toggle enabled" do
+      before { FeatureToggle.enable!(:send_current_user_cred_to_ce_api) }
+
+      context "when current_user is set in the RequestStore" do
+        let(:user) do
+          user = create(:user)
+          RequestStore[:current_user] = user
+        end
+
+        it "returns user credentials" do
+          result = described.claim_evidence_request
+
+          expect(result.user_css_id).to eq user.css_id
+          expect(result.station_id).to eq user.station_id
         end
       end
 
-      context "with use_ce_api feature toggle disabled" do
-        before { FeatureToggle.disable!(:use_ce_api) }
+      context "when current_user is NOT set in the RequestStore" do
+        before do
+          RequestStore[:current_user] = nil
+          ENV["CLAIM_EVIDENCE_VBMS_USER"] = "CSS_123"
+          ENV["CLAIM_EVIDENCE_STATION_ID"] = "123"
+        end
 
-        let(:mock_vbms_client) { instance_double("VBMS::Client") }
-        let(:mock_initialize_upload_response) { double(upload_token: "document-token") }
+        it "returns system credentials" do
+          result = described.claim_evidence_request
 
-        it "calls the VBMS client" do
-          allow(described_class).to receive(:init_vbms_client).and_return(mock_vbms_client)
-          allow(described_class).to receive(:initialize_upload)
-            .with(appeal, fake_document).and_return(mock_initialize_upload_response)
-          allow(described_class).to receive(:upload_document)
-            .with(appeal.veteran_file_number, "document-token", fake_document.pdf_location)
+          expect(result.user_css_id).to eq "CSS_123"
+          expect(result.station_id).to eq "123"
+        end
+      end
+    end
 
-          described_class.upload_document_to_vbms(appeal, fake_document)
+    context "with send_current_user_cred_to_ce_api feature toggle disabled" do
+      before do
+        FeatureToggle.disable!(:send_current_user_cred_to_ce_api)
+        ENV["CLAIM_EVIDENCE_VBMS_USER"] = "CSS_999"
+        ENV["CLAIM_EVIDENCE_STATION_ID"] = "999"
+      end
 
-          expect(described_class).to have_received(:initialize_upload).with(appeal, fake_document)
-          expect(described_class).to have_received(:upload_document)
-            .with(appeal.veteran_file_number, "document-token", fake_document.pdf_location)
+      context "when current_user is set in the RequestStore" do
+        let(:user) do
+          user = create(:user)
+          RequestStore[:current_user] = user
+        end
+
+        it "returns system credentials" do
+          result = described.claim_evidence_request
+
+          expect(result.user_css_id).to eq "CSS_999"
+          expect(result.station_id).to eq "999"
+        end
+      end
+
+      context "when current_user is NOT set in the RequestStore" do
+        before { RequestStore[:current_user] = nil }
+
+        it "returns system credentials" do
+          result = described.claim_evidence_request
+
+          expect(result.user_css_id).to eq "CSS_999"
+          expect(result.station_id).to eq "999"
         end
       end
     end
