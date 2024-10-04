c6e8138ca5545a23581e8ae7c601c078591a304a
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-10-04 12:08:22 -0400

    Add unrecognized timezones to tz initializer

--- config/initializers/timezone.rb
+++ config/initializers/timezone.rb
@@ -4,6 +4,8 @@
 ### Add timezones to here for more timezone support
 ### ###
 TIMEZONE_MAPPINGS = {
-  "Philippine Standard Time" => "Asia/Manila"
+  "Philippine Standard Time" => "Asia/Manila",
+  "Mountain Time (US & Canada)" => "America/Boise",
+  "Eastern Time (US & Canada)" => "America/Kentucky/Louisville"
 }
 ActiveSupport::TimeZone.const_set(:MAPPING, ActiveSupport::TimeZone::MAPPING.merge(TIMEZONE_MAPPINGS))
