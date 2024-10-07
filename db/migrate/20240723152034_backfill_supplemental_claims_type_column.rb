e5f3649544c9ee1ddcceaaae80d8a87a3a6300d3
= <tyler.broyles@va.gov>
2024-10-07 10:45:57 -0400

    Removed the timeout statements from the Remand backfill migration.

--- db/migrate/20240723152034_backfill_supplemental_claims_type_column.rb
+++ db/migrate/20240723152034_backfill_supplemental_claims_type_column.rb
@@ -5,13 +5,9 @@ class BackfillSupplementalClaimsTypeColumn < ActiveRecord::Migration[6.1]
     SupplementalClaim.in_batches do |batch|
       batch.update_all(
         <<-SQL
-          SET statement_timeout = '3000s';
-
           UPDATE supplemental_claims SET type = 'Remand'
             WHERE decision_review_remanded_id IS NOT NULL
               AND decision_review_remanded_type = 'Appeal';
-
-          SET statement_timeout = '30s';
         SQL
       )
     end
