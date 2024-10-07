143f1b0088b7ae3e74c38e57908dfe830bb40f05
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-10-07 14:23:02 -0400

    Update tz initializer spec

--- spec/initializers/timezone_spec.rb
+++ spec/initializers/timezone_spec.rb
@@ -1,31 +1,39 @@
 # frozen_string_literal: true
 
-describe "Timezone Overrides" do
-  subject do
-    ActiveSupport::TimeZone.find_tzinfo(tz_name).utc_offset
+describe "Timezone Initializer" do
+  describe "Timezone Overrides" do
+    subject { ActiveSupport::TimeZone.find_tzinfo(tz_name).utc_offset }
+
+    context "Asia/Manila" do
+      let(:tz_name) { "Asia/Manila" }
+
+      it { subject.to eq 28_800 }
+    end
   end
 
-  let(:comparable_offset) do
-    ActiveSupport::TimeZone.find_tzinfo(tz_name_to_compare_with).utc_offset
-  end
+  describe "Timezone Aliases" do
+    subject do
+      corresponding_tz = TIMEZONE_ALIASES[tz_name]
 
-  context "America/Boise" do
-    let(:tz_name) { "America/Boise" }
-    let(:tz_name_to_compare_with) { "America/Denver" }
+      ActiveSupport::TimeZone.find_tzinfo(corresponding_tz).utc_offset
+    end
 
-    it { subject.to eq comparable_offset }
-  end
+    context "America/Boise" do
+      let(:tz_name) { "America/Boise" }
+      let(:comparable_offset) do
+        ActiveSupport::TimeZone.find_tzinfo("America/Denver").utc_offset
+      end
 
-  context "America/Kentucky/Louisville" do
-    let(:tz_name) { "America/Kentucky/Louisville" }
-    let(:tz_name_to_compare_with) { "America/New_York" }
+      it { subject.to eq comparable_offset }
+    end
 
-    it { subject.to eq comparable_offset }
-  end
+    context "America/Kentucky/Louisville" do
+      let(:tz_name) { "America/Kentucky/Louisville" }
+      let(:comparable_offset) do
+        ActiveSupport::TimeZone.find_tzinfo("America/New_York").utc_offset
+      end
 
-  context "Asia/Manila" do
-    let(:tz_name) { "Asia/Manila" }
-
-    it { subject.to eq 28_800 }
+      it { subject.to eq comparable_offset }
+    end
   end
 end
