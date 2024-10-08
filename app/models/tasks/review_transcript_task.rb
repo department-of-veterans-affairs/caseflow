d457e7515b1653478036169032490d1c9309279a
William Medders <medders_william@bah.com>
2024-10-08 13:01:52 -0500

    Add task instructions to COPY file and fix style errors

--- app/models/tasks/review_transcript_task.rb
+++ app/models/tasks/review_transcript_task.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class ReviewTranscriptTask < Task
-
   USER_ACTIONS = [
     Constants.TASK_ACTIONS.TRANSCRIPT_NO_ERRORS_FOUND.to_h,
     Constants.TASK_ACTIONS.TRANSCRIPT_ERRORS_FOUND_AND_CORRECTED.to_h,
-    Constants.TASK_ACTIONS.CANCEL_REVIEW_TRANSCRIPT_TASK.to_h,
+    Constants.TASK_ACTIONS.CANCEL_REVIEW_TRANSCRIPT_TASK.to_h
   ].freeze
 
   def label
@@ -14,10 +13,11 @@ class ReviewTranscriptTask < Task
 
   def available_actions(user)
     return USER_ACTIONS if assigned_to == user
+
     []
   end
 
   def default_instructions
-    "Review the hearing transcript and upload the final to VBMS once it has been reviewed for errors or corrected."
+    COPY::REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS
   end
 end
