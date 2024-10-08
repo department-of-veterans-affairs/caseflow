1f81701b73866fd4eeb6453c3292fc63deefcd72
breedbah <brandon.reed3@va.gov>
2024-10-08 12:51:31 -0400

    APPEALS-59937 fix hearing validation

--- app/models/hearings/transcription.rb
+++ app/models/hearings/transcription.rb
@@ -3,7 +3,7 @@
 class Transcription < CaseflowRecord
   self.ignored_columns = ["hearing_id"]
 
-  belongs_to :hearing, polymorphic: true, dependent: :restrict_with_exception
+  belongs_to :hearing, polymorphic: true
   belongs_to :transcription_contractor
   has_many :transcription_files
   belongs_to :transcription_package, foreign_key: :task_number, primary_key: :task_number
@@ -11,6 +11,8 @@ class Transcription < CaseflowRecord
 
   validates :hearing_type, inclusion: { in: %w[Hearing LegacyHearing] }
   validates :hearing, presence: true
+
+  validate :hearing_must_exist
 
   scope :counts_for_this_week, lambda {
     where(sent_to_transcriber_date: Time.zone.today.beginning_of_week.yesterday..Time.zone.today)
@@ -31,5 +33,9 @@ class Transcription < CaseflowRecord
   def sequence_task_id
     self.task_id = Hearings::TranscriptionSequenceId.new(User.system_user.id, task_id)
       .before_insert_on_transcriptions(self)
+  end
+
+  def hearing_must_exist
+    errors.add(:hearing, "must exist") if hearing.blank?
   end
 end
