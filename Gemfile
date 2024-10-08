611b08ea8bcddd42a05c3a257953ca488d0de8d5
IsaiahBar <Isaiah.Barnett@va.gov>
2024-10-08 12:43:33 -0400

    change

--- Gemfile
+++ Gemfile
@@ -104,7 +104,7 @@ gem "ziptz"
 group :production, :staging, :ssh_forwarding, :development, :test do
   # Oracle DB
   gem "activerecord-oracle_enhanced-adapter", "~> 6.1.0"
-  gem "ruby-oci8", "~> 2.2"
+  gem "ruby-oci8", "~> 2.2.14"
 end
 
 group :test, :development, :demo, :make_docs do
--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -2260,42 +2260,42 @@ ActiveRecord::Schema.define(version: 2024_08_28_165652) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "vbms_ext_claim", primary_key: "CLAIM_ID", id: :decimal, precision: 38, force: :cascade do |t|
-    t.string "ALLOW_POA_ACCESS", limit: 5
-    t.decimal "CLAIMANT_PERSON_ID", precision: 38
+  create_table "vbms_ext_claim", primary_key: "CLAIM_ID", id: { type: :decimal, precision: 38 }, force: :cascade do |t|
     t.datetime "CLAIM_DATE"
-    t.string "CLAIM_SOJ", limit: 25
-    t.integer "CONTENTION_COUNT"
-    t.datetime "CREATEDDT", null: false
     t.string "EP_CODE", limit: 25
-    t.datetime "ESTABLISHMENT_DATE"
-    t.datetime "EXPIRATIONDT"
-    t.string "INTAKE_SITE", limit: 25
-    t.datetime "LASTUPDATEDT", null: false
-    t.string "LEVEL_STATUS_CODE", limit: 25
-    t.datetime "LIFECYCLE_STATUS_CHANGE_DATE"
-    t.string "LIFECYCLE_STATUS_NAME", limit: 50
-    t.string "ORGANIZATION_NAME", limit: 100
-    t.string "ORGANIZATION_SOJ", limit: 25
-    t.string "PAYEE_CODE", limit: 25
-    t.string "POA_CODE", limit: 25
-    t.integer "PREVENT_AUDIT_TRIG", limit: 2, default: 0, null: false
-    t.string "PRE_DISCHARGE_IND", limit: 5
-    t.string "PRE_DISCHARGE_TYPE_CODE", limit: 10
-    t.string "PRIORITY", limit: 10
-    t.string "PROGRAM_TYPE_CODE", limit: 10
-    t.string "RATING_SOJ", limit: 25
-    t.string "SERVICE_TYPE_CODE", limit: 10
-    t.string "SUBMITTER_APPLICATION_CODE", limit: 25
-    t.string "SUBMITTER_ROLE_CODE", limit: 25
     t.datetime "SUSPENSE_DATE"
     t.string "SUSPENSE_REASON_CODE", limit: 25
     t.string "SUSPENSE_REASON_COMMENTS", limit: 1000
-    t.decimal "SYNC_ID", precision: 38, null: false
+    t.decimal "CLAIMANT_PERSON_ID", precision: 38
+    t.integer "CONTENTION_COUNT"
+    t.string "CLAIM_SOJ", limit: 25
     t.string "TEMPORARY_CLAIM_SOJ", limit: 25
+    t.string "PRIORITY", limit: 10
     t.string "TYPE_CODE", limit: 25
-    t.decimal "VERSION", precision: 38, null: false
+    t.string "LIFECYCLE_STATUS_NAME", limit: 50
+    t.string "LEVEL_STATUS_CODE", limit: 25
+    t.string "SUBMITTER_APPLICATION_CODE", limit: 25
+    t.string "SUBMITTER_ROLE_CODE", limit: 25
     t.decimal "VETERAN_PERSON_ID", precision: 15
+    t.datetime "ESTABLISHMENT_DATE"
+    t.string "INTAKE_SITE", limit: 25
+    t.string "PAYEE_CODE", limit: 25
+    t.decimal "SYNC_ID", precision: 38, null: false
+    t.datetime "CREATEDDT", null: false
+    t.datetime "LASTUPDATEDT", null: false
+    t.datetime "EXPIRATIONDT"
+    t.decimal "VERSION", precision: 38, null: false
+    t.datetime "LIFECYCLE_STATUS_CHANGE_DATE"
+    t.string "RATING_SOJ", limit: 25
+    t.string "PROGRAM_TYPE_CODE", limit: 10
+    t.string "SERVICE_TYPE_CODE", limit: 10
+    t.integer "PREVENT_AUDIT_TRIG", limit: 2, default: 0, null: false
+    t.string "PRE_DISCHARGE_TYPE_CODE", limit: 10
+    t.string "PRE_DISCHARGE_IND", limit: 5
+    t.string "ORGANIZATION_NAME", limit: 100
+    t.string "ORGANIZATION_SOJ", limit: 25
+    t.string "ALLOW_POA_ACCESS", limit: 5
+    t.string "POA_CODE", limit: 25
     t.index ["CLAIM_ID"], name: "claim_id_index"
     t.index ["LEVEL_STATUS_CODE"], name: "level_status_code_index"
   end
