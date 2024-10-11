b00d32496e0c7e44e687ef8b45e440d94d380fcc
nhansen3 <noah.hansen1323@gmail.com>
2024-10-11 09:23:26 -0400

    initial push

--- db/views/national_hearing_queue_entries_v01.sql
+++ db/views/national_hearing_queue_entries_v01.sql
@@ -5,5 +5,6 @@ SELECT
     COALESCE(appeals.changed_hearing_request_type, appeals.original_hearing_request_type) AS hearing_request_type,
     appeals.receipt_date AS receipt_date,
     appeals.uuid AS external_id,
-
-  FROM appeals
+    appeals.stream_type as appeal_stream,
+    docket_number as appeals.stream_docket_number
+FROM appeals
