7a4aaa229f45517f1e209aba8b3f561f59aed473
breedbah <brandon.reed3@va.gov>
2024-10-11 11:23:58 -0400

    APPEALS-59937 return to original state

--- spec/feature/hearings/hearing_details_spec.rb
+++ spec/feature/hearings/hearing_details_spec.rb
@@ -937,7 +937,7 @@ RSpec.feature "Hearing Details", :all_dbs do
 
         click_button("Save")
 
-        expect(page).to have_content(expected_alert, wait: 90)
+        expect(page).to have_content(expected_alert)
       end
 
       context "when hearing already has transcription details" do
@@ -968,7 +968,7 @@ RSpec.feature "Hearing Details", :all_dbs do
             click_dropdown(name: "problemType", index: 0)
             click_button("Save")
 
-            expect(page).to have_content(expected_alert, wait: 90)
+            expect(page).to have_content(expected_alert)
 
             visit "hearings/#{hearing.external_id}/details"
 
