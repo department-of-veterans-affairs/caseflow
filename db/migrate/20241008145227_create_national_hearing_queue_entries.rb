3bd20b9d9ce308e07ad6c57cad5306f5f58af8ad
nhansen3 <noah.hansen1323@gmail.com>
2024-10-15 11:51:48 -0400

    rubocop

--- db/migrate/20241008145227_create_national_hearing_queue_entries.rb
+++ db/migrate/20241008145227_create_national_hearing_queue_entries.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateNationalHearingQueueEntries < ActiveRecord::Migration[6.1]
   def change
     drop_view :national_hearing_queue_entries, materialized: true
