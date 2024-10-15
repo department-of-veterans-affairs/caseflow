893d0b8ce06a5963a21259e1e0bc825642d4cb13
breedbah <brandon.reed3@va.gov>
2024-10-15 11:21:14 -0400

    APPEALS-59937 add legacy association

--- app/models/legacy_hearing.rb
+++ app/models/legacy_hearing.rb
@@ -77,6 +77,7 @@ class LegacyHearing < CaseflowRecord
   has_many :email_events, class_name: "SentHearingEmailEvent", foreign_key: :hearing_id
   has_many :email_recipients, class_name: "HearingEmailRecipient", foreign_key: :hearing_id
   has_many :transcription_files, as: :hearing
+  has_many :transcriptions, as: :hearing 
 
   alias_attribute :location, :hearing_location
   accepts_nested_attributes_for :hearing_location, reject_if: proc { |attributes| attributes.blank? }
