4f126919bec51c62c2c5e7163aae5b608a3d8394
nhansen3 <noah.hansen1323@gmail.com>
2024-10-17 12:19:43 -0600

    removing extra end

--- db/migrate/20241008145227_create_national_hearing_queue_entries.rb
+++ db/migrate/20241008145227_create_national_hearing_queue_entries.rb
@@ -4,9 +4,8 @@ class CreateNationalHearingQueueEntries < ActiveRecord::Migration[6.1]
   def up
     create_view :national_hearing_queue_entries, materialized: true
   end
-  
-    def down
-      drop_view :national_hearing_queue_entries, materialized: true
-    end
+
+  def down
+    drop_view :national_hearing_queue_entries, materialized: true
   end
 end
