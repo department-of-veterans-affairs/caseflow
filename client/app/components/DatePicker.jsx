4d90a7051379df78405a796559f4eab81d5d5213
Sean Craig <110493538+seancva@users.noreply.github.com>
2024-10-17 15:05:04 -0500

    cleaned up some lint

--- client/app/components/DatePicker.jsx
+++ client/app/components/DatePicker.jsx
@@ -223,7 +223,7 @@ class DatePicker extends React.PureComponent {
 
         disabled = startDate >= endDate;
       }
-    } else if (this.state.mode === 'before' || this.state.mode === 'after' ) {
+    } else if (this.state.mode === 'before' || this.state.mode === 'after') {
       const startDate = moment(`${this.state.startDate} 00:00:00`).valueOf();
       const currentDate = moment().valueOf();
 
