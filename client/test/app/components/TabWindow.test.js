aab655c2687ae9fc1180dac20834761c054952c5
hawkespoulterva <hawkes.poulter@va.gov>
2024-10-22 10:43:09 -0400

    Poulter/appeals 46624 react 18 upgrade final (#23317)
    * react 18 test upgrade
    
    * styled-components
    
    * EditPOAInformation test refactor, in progrss NonratingRIUModal test
    
    * additional tests refactored
    
    * Refactors Enzyme -> RTL
    
    * more changes
    
    * Enzyme -> RTL refactors (cont)
    
    * Enzyme -> RTL refactor (cont)
    
    * Enzyme -> RTL refactor (cont)
    
    * Enzyme -> RTL refactor (cont)
    
    * Enzyme -> RTL refactor (cont)
    
    * Cherry-picking from v2 branch --cont
    
    * fixed badge test
    
    * Enzyme -> RTL (establishClaim-test)
    
    * Enzyme -> RTL (cont, Batch 3)
    
    * Batch 3 refactor cont
    
    * Batch 3 In Progress
    
    * Batch 3 to review
    
    * Batch 4 updates
    
    * ScheduleVeteran.test refactor finished
    
    * Batch 4 and cleanups
    
    * Batch 4 continued
    
    * Outstanding Enzyme -> RTL refactors
    
    * batch 4 changes and non used enzyme test removal
    
    * clean up
    
    * snapshot and failing test clean up
    
    * Test updates and Snapshot fixes
    
    * Package updates with a couple fixed tests
    
    * Make test less brittle
    
    * switch to for...of instead of forEach to fix async problems
    
    * remove unnecessary
    import
    
    * lint fixes
    
    * disable linters
    
    * APPEALS-55525: Disable tests for seed files and lock Chrome version to v127 in Github Actions (#22567)
    
    * add exclude pattern to github actions rspec command
    
    * add test file exclude pattern constant
    
    * lock chrome version to 127.0.6533.119
    
    * React Dependency upgrade
    
    * force wepback-bundle.js
    
    * Fix for bva_dispatch_return_flow_spec
    
    * fixed spec method to click pdf
    
    * Fixed task_queue_spec.rb
    
    * add indicated_for_contested_claims flag to legacy appeals
    
    * update legacy appeal spec
    
    * fix for scm_judge_assignment_spec.rb
    
    * force state update
    
    * fixed admin_ui_spec.rb
    
    * fixed user_organization_spec.rb
    
    * flow modal refactor to functional component and fix state renders, remove null saveSuccessful
    
    * fixed motion_to_vacate_spec.rb
    
    * revert changes
    
    * revert all changes
    
    * stop letting null pass
    
    * revert more changes
    
    * test null condition
    
    * comment
    
    * revert test
    
    * add flushsync
    
    * remove enzyme and update snapshots
    
    * fixed EmailConfirmationModal.test.js
    
    * fixed NonratingRequestIssueModal-test.js
    
    * fixed Timezone.test.js
    
    * fixed ScheduleVeteranForm.test.js
    
    * fixed HearingTime.test.js
    
    * fixed IssueTags.test.js
    
    * linting
    
    * updated daily_docket_spec.rb
    
    * fix duplicated code for code climate
    
    * fix more shared test setup
    
    * hopefully the last duplicate code
    
    * remove sleeps
    
    * update for daily_docket_spec.rb
    
    * add flush sync and add initialState back in
    
    * added waits to vso_change_hearing_request_type_spec.rb
    
    * remove legacy appeal flag
    
    * Remove debugging statement
    
    * revert change spec and fix state sync
    
    * remove file changes
    
    * fix snapshot
    
    * Merge branch 'main' into Poulter/46624-react-18-upgrade-1
    
    * fixed tests
    
    * revert unnecesarry change
    
    * remove old spec changes
    
    * fix syncable job
    
    ---------
    
    Co-authored-by: Griffin Dooley <dooleygriffin@gmail.com>
    Co-authored-by: AdamShawBAH <adam.shaw@va.gov>
    Co-authored-by: Craig Reese <109101548+craigrva@users.noreply.github.com>

--- client/test/app/components/TabWindow.test.js
+++ client/test/app/components/TabWindow.test.js
@@ -1,83 +1,82 @@
 import React from 'react';
 import TabWindow from 'app/components/TabWindow';
-import { shallow, mount } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { tabList } from 'test/data';
 
 /* eslint-disable dot-location */
 describe('TabWindow', () => {
   test('Matches snapshot with default props', () => {
     // Set the component
-    const tabs = mount(<TabWindow tabs={tabList} />);
-
-    expect(tabs).toMatchSnapshot();
+    const {asFragment}=render(<TabWindow tabs={tabList} />);
 
     // We now expect just one child (the <Tabs> component)
-    expect(tabs.children()).toHaveLength(1);
+    expect(screen.getByRole('tablist')).toBeInTheDocument();
 
-    // Expect the tab navigation to contain both tabs
-    expect(tabs.find('.cf-tab-navigation').children()).toHaveLength(2);
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(0)
-        .find('button')
-        .hasClass('cf-active')
-    ).toEqual(true);
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(0)
-        .find('button')
-        .prop('aria-selected')
-    ).toEqual(true);
+    const button1 = screen.getByRole('tab', {name: 'Tab 1'});
+    expect(button1).toBeInTheDocument();
+    expect(button1).toHaveClass('cf-active');
+    expect(button1).toHaveAttribute('aria-selected', 'true');
+    expect(screen.getByText('Tab 1')).toBeInTheDocument();
+
+    const button2 = screen.getByRole('tab', {name: 'Tab 2'});
+    expect(button2).toBeInTheDocument();
+    expect(button2).not.toHaveClass('cf-active');
+    expect(button2).toHaveAttribute('aria-selected', 'false');
+    expect(screen.getByText('Tab 2')).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Renders an empty div when no tabs are passed', () => {
     // Set the component
-    const tabs = shallow(<TabWindow />);
+    const {asFragment}=render(<TabWindow />);
 
-    // Snapshot matcher
-    expect(tabs).toMatchSnapshot();
-    expect(tabs.children().exists()).toEqual(false);
+    const tabListDiv = screen.getByRole('tablist');
+    expect(tabListDiv).toBeInTheDocument();
+    expect(tabListDiv).toHaveClass('cf-tab-navigation');
+    expect(tabListDiv).toHaveTextContent('');
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Renders full page when the `fullPage` prop is true', () => {
     // Set the component
-    const tabs = mount(<TabWindow fullPage tabs={tabList} />);
+    const {container, asFragment}=render(<TabWindow fullPage tabs={tabList} />);
 
-    // Snapshot matcher
-    expect(tabs).toMatchSnapshot();
-    expect(tabs.find('.cf-tab-navigation-full-screen').exists()).toEqual(true);
+    expect(asFragment()).toMatchSnapshot();
+    expect(container.querySelector('.cf-tab-navigation-full-screen')).toBeInTheDocument();
   });
 
   test('Renders tab content when a tab is selected', () => {
     // Set the component
-    const tabs = mount(<TabWindow fullPage tabs={tabList} />);
+    const {asFragment}=render(<TabWindow fullPage tabs={tabList} />);
+
+    const button1 = screen.getByRole('tab', {name: 'Tab 1'});
+    expect(button1).toBeInTheDocument();
+    expect(button1).toHaveClass('cf-active');
+    expect(button1).toHaveAttribute('aria-selected', 'true');
+
+    const button2 = screen.getByRole('tab', {name: 'Tab 2'});
+    expect(button2).toBeInTheDocument();
+    expect(button2).not.toHaveClass('cf-active');
+    expect(button2).toHaveAttribute('aria-selected', 'false');
 
     // Run the test
-    tabs.find('#main-tab-1').simulate('click');
+    fireEvent.click(button2);
 
-    // Snapshot matcher
-    expect(tabs).toMatchSnapshot();
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(1)
-        .find('button')
-        .hasClass('cf-active')
-    ).toEqual(true);
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(1)
-        .find('button')
-        .prop('aria-selected')
-    ).toEqual(true);
+    expect(button1).not.toHaveClass('cf-active');
+    expect(button1).toHaveAttribute('aria-selected', 'false');
+
+    expect(button2).toHaveClass('cf-active');
+    expect(button2).toHaveAttribute('aria-selected', 'true');
+
+    expect(asFragment()).toMatchSnapshot();
+
   });
 
   test('Does not render tab content when a tab is disabled and clicked', () => {
     // Set the component
-    const tabs = mount(
+    const {asFragment}=render(
       <TabWindow
         fullPage
         tabs={[
@@ -90,47 +89,51 @@ describe('TabWindow', () => {
       />
     );
 
-    // Run the test
-    tabs.find('#main-tab-1').simulate('click');
+    const button1 = screen.getByRole('tab', {name: 'Tab 1'});
+    expect(button1).toBeInTheDocument();
+    expect(button1).toHaveClass('cf-active');
+    expect(button1).toHaveAttribute('aria-selected', 'true');
 
-    // Snapshot matcher
-    expect(tabs).toMatchSnapshot();
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(1)
-        .find('button')
-        .hasClass('cf-active')
-    ).toEqual(false);
-    expect(
-      tabs
-        .find('.cf-tab-navigation')
-        .childAt(1)
-        .find('button')
-        .prop('aria-selected')
-    ).toEqual(false);
+    const button2 = screen.getByRole('tab', {name: 'Tab 2'});
+    expect(button2).toBeInTheDocument();
+    expect(button2).not.toHaveClass('cf-active');
+    expect(button2).toHaveAttribute('aria-selected', 'false');
+
+    // Run the test
+    fireEvent.click(button2);
+
+    expect(button2).not.toHaveClass('cf-active');
+    expect(button2).toHaveAttribute('aria-selected', 'false');
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('calls onChange when tab changes', () => {
     const onChange = jest.fn();
 
     // Set the component
-    const tabs = mount(<TabWindow onChange={onChange} tabs={tabList} />);
+    render(<TabWindow onChange={onChange} tabs={tabList} />);
+
+    const button2 = screen.getByRole('tab', {name: 'Tab 2'});
 
     // Run the test
-    tabs.find('#main-tab-1').simulate('click');
+    fireEvent.click(button2);
     expect(onChange).toHaveBeenCalledWith(1);
   });
 
   test('renders tabs with one tab if alwaysShowTabs is true', () => {
-    const tabs = mount(
+    render(
       <TabWindow
         tabs={[tabList[0]]}
         alwaysShowTabs
       />
     );
 
-    expect(tabs.find('button.cf-tab')).toHaveLength(1);
+    const button1 = screen.getByRole('tab', {name: 'Tab 1'});
+    expect(button1).toBeInTheDocument();
+
+    const button2 = screen.queryByRole('tab', {name: 'Tab 2'});
+    expect(button2).not.toBeInTheDocument();
   });
 });
 /* eslint-enable dot-location */
