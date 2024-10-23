d2b1224cdf8c8356eaf8bd282ec33ffc8adc50a4
piedram <maite.piedrayera@va.gov>
2024-10-23 16:38:55 -0400

    No errors found upload transcript to VBMS modal

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -186,6 +186,23 @@ class TasksController < ApplicationController
     }
   end
 
+  def upload_transcription_to_vbms
+    if task.type == "ReviewTranscriptTask" && task.status == "in_progress"
+      new_instructions = task.instructions.push(params[:task][:instructions])
+
+      Transcription.where(task_id: task.id).update_all(
+        updated_by_id: current_user.id,
+        uploaded_to_vbms_date: Time.zone.now,
+        updated_at: Time.zone.now
+      )
+
+      Task.where(id: task.id).update_all(
+        instructions: new_instructions,
+        status: Constants.TASK_STATUSES.completed
+      )
+    end
+  end
+
   private
 
   def send_initial_notification_letter
