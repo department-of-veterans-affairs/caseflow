aca8c4d86a4b3715424c4079515f2870932d1373
Brian Bommarito <6898085+bbommarito@users.noreply.github.com>
2024-10-24 10:02:02 -0400

    Adding warning icon.

--- client/app/reader/DocSizeIndicator.js
+++ client/app/reader/DocSizeIndicator.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { filesize } from 'filesize';
+import { SizeWarningIcon } from '../components/icons/SizeWarningIcon';
+import { ICON_SIZES } from '../constants/AppConstants';
 
 const DocSizeIndicator = (props) => {
   return (
-    <span>{filesize(props.docSize)}</span>
+    <span>{filesize(props.docSize)} <SizeWarningIcon size={ICON_SIZES.SMALL} /></span>
   );
 };
 
