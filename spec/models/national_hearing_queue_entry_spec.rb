9adbaa8a8e30e563956a6f49f3c401c7fb928330
nhansen3 <noah.hansen1323@gmail.com>
2024-10-25 12:06:24 -0600

    addressing Matt's comments

--- spec/models/national_hearing_queue_entry_spec.rb
+++ spec/models/national_hearing_queue_entry_spec.rb
@@ -7,27 +7,51 @@ RSpec.describe NationalHearingQueueEntry, type: :model do
 
   context "when appeals have been staged" do
     it "refreshes the view and returns the proper appeals", bypass_cleaner: true do
+      #refresh in case anything was run in rails console previously
+      NationalHearingQueueEntry.refresh
+
       # AMA
       FactoryBot.create(:appeal, :with_schedule_hearing_tasks)
       FactoryBot.create(:appeal)
+      appeal_completed = FactoryBot.create(:appeal, :with_schedule_hearing_tasks)
+
+      ScheduleHearingTask.find_by(appeal_id: appeal_completed.id, appeal_type: "Appeal").update(status: 'completed')
 
       # LEGACY
       case1 = FactoryBot.create(:case, bfkey: "700230001041", bfcorlid: "100000101011")
       case2 = FactoryBot.create(:case, bfkey: "700230002041", bfcorlid: "100000102021")
+      case3 = FactoryBot.create(:case, bfkey: "700230002042", bfcorlid: "100000102022")
 
       FactoryBot.create(:legacy_appeal,
-                        :with_root_task,
+                        :with_schedule_hearing_tasks,
                         :with_veteran,
                         vacols_case: case1)
       FactoryBot.create(:legacy_appeal,
-                        :with_schedule_hearing_tasks,
                         :with_root_task,
                         :with_veteran,
                         vacols_case: case2)
+      legacy_appeal_completed = FactoryBot.create(:legacy_appeal,
+                        :with_schedule_hearing_tasks,
+                        :with_veteran,
+                        vacols_case: case3)
+
+      ScheduleHearingTask.find_by(appeal_id: legacy_appeal_completed.id, appeal_type: "LegacyAppeal").update(status: 'completed')
 
       expect(NationalHearingQueueEntry.count).to eq 0
       NationalHearingQueueEntry.refresh
+
       expect(NationalHearingQueueEntry.count).to eq 2
+      #first created appeal is the only ama appeal that should be in the db (id: 1)
+      expect(NationalHearingQueueEntry.first.appeal_id).to eq 1
+      #first created legacy appeal is the only legacy appeal that should be in the db (id: 1)
+      expect(NationalHearingQueueEntry.second.appeal_id).to eq 1
+
+
+      clean_up_after_threads
     end
+  end
+
+  def clean_up_after_threads
+    DatabaseCleaner.clean_with(:truncation, except: %w[vftypes issref notification_events])
   end
 end
