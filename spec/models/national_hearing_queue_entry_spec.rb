fd1a7b6e9a73b0a71542cca23affb484b7987a8b
nhansen3 <noah.hansen1323@gmail.com>
2024-10-25 09:02:33 -0600

    rubocop

--- spec/models/national_hearing_queue_entry_spec.rb
+++ spec/models/national_hearing_queue_entry_spec.rb
@@ -3,22 +3,27 @@
 require "rails_helper"
 
 RSpec.describe NationalHearingQueueEntry, type: :model do
-
   self.use_transactional_tests = false
 
-  context 'when appeals have been staged' do
-    it 'refreshes the view and returns the proper appeals', bypass_cleaner: true do
-
-      #AMA
+  context "when appeals have been staged" do
+    it "refreshes the view and returns the proper appeals", bypass_cleaner: true do
+      # AMA
       FactoryBot.create(:appeal, :with_schedule_hearing_tasks)
       FactoryBot.create(:appeal)
 
-      #LEGACY
+      # LEGACY
       case1 = FactoryBot.create(:case, bfkey: "700230001041", bfcorlid: "100000101011")
       case2 = FactoryBot.create(:case, bfkey: "700230002041", bfcorlid: "100000102021")
 
-      FactoryBot.create(:legacy_appeal, :with_root_task, :with_veteran, vacols_case: case1)
-      FactoryBot.create(:legacy_appeal, :with_schedule_hearing_tasks, :with_root_task, :with_veteran, vacols_case: case2)
+      FactoryBot.create(:legacy_appeal,
+                        :with_root_task,
+                        :with_veteran,
+                        vacols_case: case1)
+      FactoryBot.create(:legacy_appeal,
+                        :with_schedule_hearing_tasks,
+                        :with_root_task,
+                        :with_veteran,
+                        vacols_case: case2)
 
       expect(NationalHearingQueueEntry.count).to eq 0
       NationalHearingQueueEntry.refresh
