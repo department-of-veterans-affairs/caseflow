ad2d88069899f39817022037b283187a293894e2
Kevin Ma <104021955+Kevma50287@users.noreply.github.com>
2024-10-28 16:03:59 -0400

    Refactored to use existing module

--- spec/support/correspondence_helpers.rb
+++ spec/support/correspondence_helpers.rb
@@ -193,18 +193,6 @@ module CorrespondenceHelpers
     User.authenticate!(user: current_user)
   end
 
-  def retry_if_not_found(max_attempts: 3, cleanup: nil)
-    attempts ||= 0
-    begin
-      yield
-    rescue Capybara::ElementNotFound => error
-      attempts += 1
-      Rails.logger.warn("#{error} caught. Retry attempt: #{attempts}")
-      cleanup&.call
-      retry if attempts < max_attempts
-    end
-  end
-
   def click_page_body
     find("body").click
   end
