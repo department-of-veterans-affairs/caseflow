593dd79453e3e90e50a838c3429243583837d238
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-10-29 15:31:03 -0400

    adds spec file for person event service

---
+++ spec/services/events/person_updated/person_event_service_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Events::PersonUpdated::PersonEventService do
+  # let!(:event) { PersonUpdatedEvent.create!(participant_id: "1") }
+  # let!(:payload) { Events::PersonUpdated::PersonUpdatedEvent.example_response }
+
+  describe ".process" do
+  end
+  describe "veteran record associations" do
+  end
+  describe "person record associations" do
+  end
+end
