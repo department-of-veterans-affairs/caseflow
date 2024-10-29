bd7298aa597b2939ff50596fe5218afbdc4773c0
TuckerRose <6579225+buddylove954@users.noreply.github.com>
2024-10-29 09:11:11 -0400

    added a service to build the request issue for code climate fix

--- app/models/request_issues_update_event.rb
+++ app/models/request_issues_update_event.rb
@@ -236,37 +236,7 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
         fail Caseflow::Error::DecisionReviewCreatedRequestIssuesError, "reference_id cannot be null"
       end
 
-      ri = RequestIssue.create!(
-        reference_id: parser_issues.ri_reference_id,
-        benefit_type: parser_issues.ri_benefit_type,
-        contested_issue_description: parser_issues.ri_contested_issue_description,
-        contention_reference_id: parser_issues.ri_contention_reference_id,
-        contested_rating_decision_reference_id: parser_issues.ri_contested_rating_decision_reference_id,
-        contested_rating_issue_profile_date: parser_issues.ri_contested_rating_issue_profile_date,
-        contested_rating_issue_reference_id: parser_issues.ri_contested_rating_issue_reference_id,
-        contested_decision_issue_id: parser_issues.ri_contested_decision_issue_id,
-        decision_date: parser_issues.ri_decision_date,
-        ineligible_due_to_id: parser_issues.ri_ineligible_due_to_id,
-        ineligible_reason: parser_issues.ri_ineligible_reason,
-        is_unidentified: parser_issues.ri_is_unidentified,
-        unidentified_issue_text: parser_issues.ri_unidentified_issue_text,
-        nonrating_issue_category: parser_issues.ri_nonrating_issue_category,
-        nonrating_issue_description: parser_issues.ri_nonrating_issue_description,
-        untimely_exemption: parser_issues.ri_untimely_exemption,
-        untimely_exemption_notes: parser_issues.ri_untimely_exemption_notes,
-        vacols_id: parser_issues.ri_vacols_id,
-        vacols_sequence_id: parser_issues.ri_vacols_sequence_id,
-        closed_at: parser_issues.ri_closed_at,
-        closed_status: parser_issues.ri_closed_status,
-        contested_rating_issue_diagnostic_code: parser_issues.ri_contested_rating_issue_diagnostic_code,
-        ramp_claim_id: parser_issues.ri_ramp_claim_id,
-        rating_issue_associated_at: parser_issues.ri_rating_issue_associated_at,
-        nonrating_issue_bgs_id: parser_issues.ri_nonrating_issue_bgs_id,
-        nonrating_issue_bgs_source: parser_issues.ri_nonrating_issue_bgs_source,
-        end_product_establishment_id: @epe.id,
-        veteran_participant_id: parser_issues.ri_veteran_participant_id,
-        decision_review: @review
-      )
+      ri = .new(parser_issues, @epe.id, @review).build
       add_event_record(ri, "A", nil)
       newly_created_issues.push(ri)
 
