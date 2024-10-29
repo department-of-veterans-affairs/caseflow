6bd82bfddcd23b6bb574336e1b7c62723a0810cb
TuckerRose <6579225+buddylove954@users.noreply.github.com>
2024-10-29 08:59:39 -0400

    created a helper method for updating request issue

--- app/models/request_issues_update_event.rb
+++ app/models/request_issues_update_event.rb
@@ -93,41 +93,9 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
     newly_withdrawn_issues
   end
 
-  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def update_request_issue!(request_issue, parser_issue)
-    request_issue.update(
-      ineligible_reason: parser_issue.ri_ineligible_reason,
-      closed_at: parser_issue.ri_closed_at,
-      closed_status: parser_issue.ri_closed_status,
-      contested_issue_description: parser_issue.ri_contested_issue_description,
-      nonrating_issue_category: parser_issue.ri_nonrating_issue_category,
-      nonrating_issue_description: parser_issue.ri_nonrating_issue_description,
-      contention_updated_at: @parser.end_product_establishment_last_synced_at,
-      contention_reference_id: parser_issue.ri_contention_reference_id,
-      contested_decision_issue_id: parser_issue.ri_contested_decision_issue_id,
-      contested_rating_issue_reference_id: parser_issue.ri_contested_rating_issue_reference_id,
-      contested_rating_issue_diagnostic_code: parser_issue.ri_contested_rating_issue_diagnostic_code,
-      contested_rating_decision_reference_id: parser_issue.ri_contested_rating_decision_reference_id,
-      contested_rating_issue_profile_date: parser_issue.ri_contested_rating_issue_profile_date,
-      nonrating_issue_bgs_source: parser_issue.ri_nonrating_issue_bgs_source,
-      nonrating_issue_bgs_id: parser_issue.ri_nonrating_issue_bgs_id,
-      unidentified_issue_text: parser_issue.ri_unidentified_issue_text,
-      vacols_sequence_id: parser_issue.ri_vacols_sequence_id,
-      ineligible_due_to_id: parser_issue.ri_ineligible_due_to_id,
-      reference_id: parser_issue.ri_reference_id,
-      rating_issue_associated_at: parser_issue.ri_rating_issue_associated_at,
-      edited_description: parser_issue.ri_edited_description,
-      ramp_claim_id: parser_issue.ri_ramp_claim_id,
-      vacols_id: parser_issue.ri_vacols_id,
-      decision_date: parser_issue.ri_decision_date,
-      is_unidentified: parser_issue.ri_is_unidentified,
-      untimely_exemption: parser_issue.ri_untimely_exemption,
-      untimely_exemption_notes: parser_issue.ri_untimely_exemption_notes,
-      benefit_type: parser_issue.ri_benefit_type,
-      veteran_participant_id: parser_issue.ri_veteran_participant_id
-    )
+    request_issue.update(mapped_attributes(parser_issue))
   end
-  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
   # Set the closed_at date and closed_status for removed issues based on the event data
   def update_removed_issues!
@@ -364,5 +332,39 @@ class RequestIssuesUpdateEvent < RequestIssuesUpdate
     AppealRepository.issues(vacols_id).find do |issue|
       issue.vacols_sequence_id == vacols_sequence_id
     end
+  end
+
+  def mapped_attributes(parser_issue)
+    {
+      ineligible_reason: parser_issue.ri_ineligible_reason,
+      closed_at: parser_issue.ri_closed_at,
+      closed_status: parser_issue.ri_closed_status,
+      contested_issue_description: parser_issue.ri_contested_issue_description,
+      nonrating_issue_category: parser_issue.ri_nonrating_issue_category,
+      nonrating_issue_description: parser_issue.ri_nonrating_issue_description,
+      contention_updated_at: @parser.end_product_establishment_last_synced_at,
+      contention_reference_id: parser_issue.ri_contention_reference_id,
+      contested_decision_issue_id: parser_issue.ri_contested_decision_issue_id,
+      contested_rating_issue_reference_id: parser_issue.ri_contested_rating_issue_reference_id,
+      contested_rating_issue_diagnostic_code: parser_issue.ri_contested_rating_issue_diagnostic_code,
+      contested_rating_decision_reference_id: parser_issue.ri_contested_rating_decision_reference_id,
+      contested_rating_issue_profile_date: parser_issue.ri_contested_rating_issue_profile_date,
+      nonrating_issue_bgs_source: parser_issue.ri_nonrating_issue_bgs_source,
+      nonrating_issue_bgs_id: parser_issue.ri_nonrating_issue_bgs_id,
+      unidentified_issue_text: parser_issue.ri_unidentified_issue_text,
+      vacols_sequence_id: parser_issue.ri_vacols_sequence_id,
+      ineligible_due_to_id: parser_issue.ri_ineligible_due_to_id,
+      reference_id: parser_issue.ri_reference_id,
+      rating_issue_associated_at: parser_issue.ri_rating_issue_associated_at,
+      edited_description: parser_issue.ri_edited_description,
+      ramp_claim_id: parser_issue.ri_ramp_claim_id,
+      vacols_id: parser_issue.ri_vacols_id,
+      decision_date: parser_issue.ri_decision_date,
+      is_unidentified: parser_issue.ri_is_unidentified,
+      untimely_exemption: parser_issue.ri_untimely_exemption,
+      untimely_exemption_notes: parser_issue.ri_untimely_exemption_notes,
+      benefit_type: parser_issue.ri_benefit_type,
+      veteran_participant_id: parser_issue.ri_veteran_participant_id
+    }
   end
 end
