ac2e8d12d44ead6826cc68a9c8bc56a44f9b6324
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-10-30 16:35:16 -0400

    adds annotations in the remediate method

--- app/services/remediations/duplicate_person_remediation_service.rb
+++ app/services/remediations/duplicate_person_remediation_service.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-class DuplicatePersonRemediationService
+class Remediations::DuplicatePersonRemediationService
   def intitialize(person_ids)
     @person_ids = person_ids
   end
 
-  def remdiate; end
+  def remdiate
+    # in this method we will implement some logic to find and delete any duplicate persons
+    # will check by ssn
+  end
 end
