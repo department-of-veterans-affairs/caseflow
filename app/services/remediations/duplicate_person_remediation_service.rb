ce899184eed603fcdd2cd486af0eda566fa11e78
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-10-30 11:53:59 -0400

    adds remediation directory and corresponsing service classes to the service folder

---
+++ app/services/remediations/duplicate_person_remediation_service.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class DuplicatePersonRemediationService
+  def intitialize(person_ids)
+    @person_ids = person_ids
+  end
+
+  def remdiate; end
+end
---
+++ app/services/remediations/veteran_record_remediation_service.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class VeteranRecordRemedationService
+  def intitialize(vet_ids)
+    @vet_ids = vet_ids
+  end
+
+  def remediate; end
+end
