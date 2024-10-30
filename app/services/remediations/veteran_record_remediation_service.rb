9fbbe7e068fa7dbb3a137dff386cd611898c8bac
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-10-30 16:35:36 -0400

    adds annotations in remediate method

--- app/services/remediations/veteran_record_remediation_service.rb
+++ app/services/remediations/veteran_record_remediation_service.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-class VeteranRecordRemedationService
+class Remediations::VeteranRecordRemediationService
   def intitialize(vet_ids)
     @vet_ids = vet_ids
   end
 
-  def remediate; end
+  def remediate
+    # in this method we will implement some logic to find and update records associated
+    # with veterans that have updated file numbers
+    # will check by file number and possibly other
+  end
 end
