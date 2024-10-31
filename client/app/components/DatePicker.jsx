c8cdbc4b3adf085fc280448570486c53096592a3
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2024-10-31 09:37:10 -0500

    remove import

--- client/app/components/DatePicker.jsx
+++ client/app/components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from 'glamor';
 import FilterIcon from './icons/FilterIcon';
