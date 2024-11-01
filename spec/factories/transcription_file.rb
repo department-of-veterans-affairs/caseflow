d0e2bc4dc44fb53d1f539c166a44f7b2cd83677d
Adam Ducker <adam.ducker@va.gov>
2024-11-01 14:02:29 -0500

    Remove change to factory for now

--- spec/factories/transcription_file.rb
+++ spec/factories/transcription_file.rb
@@ -3,8 +3,6 @@
 FactoryBot.define do
   factory :transcription_file, class: "Hearings::TranscriptionFile" do
     hearing { create(:hearing, :held) }
-    hearing_id { hearing.id }
-    hearing_type { hearing.request_type }
     docket_number { hearing.docket_number }
     file_name { "transcript.vtt" }
     file_type { "vtt" }
