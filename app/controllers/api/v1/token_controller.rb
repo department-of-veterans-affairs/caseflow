cb70e6829e6592214d85bef29df069037a44801c
Matthew Roth <1094999+zurbergram@users.noreply.github.com>
2024-11-01 09:10:43 -0400

    fix linting issues

--- app/controllers/api/v1/token_controller.rb
+++ app/controllers/api/v1/token_controller.rb
@@ -4,10 +4,8 @@ class Api::V1::TokenController < Api::ApplicationController
   before_action :require_demo
   skip_before_action :verify_authentication_token, only: [:index]
 
-
   def index
     api_key = ApiKey.create!(consumer_name: token_params[:consumer_name])
-    puts api_key.key_string
 
     render json: { api_key: api_key.key_string }, status: :ok
   end
@@ -19,6 +17,6 @@ class Api::V1::TokenController < Api::ApplicationController
   end
 
   def require_demo
-    render :status => 404 unless Rails.deploy_env?(:demo)
+    render status: :not_found unless Rails.deploy_env?(:demo)
   end
 end
