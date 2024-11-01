d6abbd0478ec9e3cd92092f3dd66db42e6815791
Raymond Hughes <131811099+raymond-hughes@users.noreply.github.com>
2024-11-01 11:17:37 -0400

    Update dockerfile to build on arm instead of x86

--- docker-bin/startup.sh
+++ docker-bin/startup.sh
@@ -8,34 +8,34 @@ source $THIS_SCRIPT_DIR/env.sh
 # echo "Start DBus"
 # dbus-daemon --system
 
-echo "Waiting for dependencies to properly start up - 240 seconds"
-date
-sleep 240
+# echo "Waiting for dependencies to properly start up - 240 seconds"
+# date
+# sleep 240
 
-echo "Starting Appeals App"
-date
+# echo "Starting Appeals App"
+# date
 
-echo "Waiting for Vacols to be ready"
-rake local:vacols:wait_for_connection
+# echo "Waiting for Vacols to be ready"
+# rake local:vacols:wait_for_connection
 
-echo "Creating DB in PG"
-rake db:create:primary
-rake db:schema:load:primary
+# echo "Creating DB in PG"
+# rake db:create:primary
+# rake db:schema:load:primary
 
-echo "Seeding Facols"
-rake local:vacols:seed
+# echo "Seeding Facols"
+# rake local:vacols:seed
 
-echo "Seeding DB in PG"
-rake db:seed
+# echo "Seeding DB in PG"
+# rake db:seed
 
-echo "Enabling Feature Flags"
-bundle exec rails runner scripts/enable_features_dev.rb
+# echo "Enabling Feature Flags"
+# bundle exec rails runner scripts/enable_features_dev.rb
 
-echo "Enabling caching"
-touch tmp/caching-dev.txt
+# echo "Enabling caching"
+# touch tmp/caching-dev.txt
 
-echo "Initializing metabase"
-/caseflow/metabase/metabase_api_script_demo.sh
+# echo "Initializing metabase"
+# /caseflow/metabase/metabase_api_script_demo.sh
 
 echo "Starting Caseflow App RoR"
 rails server --binding 0.0.0.0 -p 3000
