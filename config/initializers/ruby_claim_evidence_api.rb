a36de9b979be89745510ae3e1c80d90b47cbe393
Jeremy Croteau <jeremiah.croteau@va.gov>
2024-11-04 17:27:47 -0500

    🐛 Run initializer on reloader callback
    This initializer autoloads reloadable constants (e.g. `ApplicationController`).
    
    Being able to do this is deprecated. Autoloading during initialization is going to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during initialization does not run again.
    
    In order to autoload safely at boot time, we can wrap this initializer in a reloader callback.

--- config/initializers/ruby_claim_evidence_api.rb
+++ config/initializers/ruby_claim_evidence_api.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-VeteranFileFetcher = ExternalApi::VeteranFileFetcher
-  .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
+Rails.application.config.to_prepare do
+  VeteranFileFetcher = ExternalApi::VeteranFileFetcher
+    .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
 
-VeteranFileUpdater = ExternalApi::VeteranFileUpdater
-  .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
+  VeteranFileUpdater = ExternalApi::VeteranFileUpdater
+    .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
 
-VeteranFileUploader = ExternalApi::VeteranFileUploader
-  .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
+  VeteranFileUploader = ExternalApi::VeteranFileUploader
+    .new(use_canned_api_responses: ApplicationController.dependencies_faked?, logger: Rails.logger)
+end
