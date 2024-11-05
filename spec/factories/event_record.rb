da8f70c547adcda5d1b884291961ad33a513073d
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-05 14:21:31 -0500

    adds traits for event_records

--- spec/factories/event_record.rb
+++ spec/factories/event_record.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :person_event_record do
+  factory :event_record do
     sequence(:id) { |n| n }
     event_record_id { 1 }
-    event_record_type { "Person" }
+    event_record_type { "" }
     created_at { DateTime.now }
     updated_at { DateTime.now }
     info { {} }
+
+    trait :person_event_record do
+      event_record_type { "Person" }
+    end
+
+    trait :veteran_event_record do
+      event_record_type { "Veteran" }
+    end
   end
 end
