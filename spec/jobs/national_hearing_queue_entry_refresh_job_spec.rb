20ed1ed7f34db014e7cd5009f96c14020bacd7ad
nhansen3 <noah.hansen1323@gmail.com>
2024-11-05 09:58:28 -0700

    editing tests

--- spec/jobs/national_hearing_queue_entry_refresh_job_spec.rb
+++ spec/jobs/national_hearing_queue_entry_refresh_job_spec.rb
@@ -1,44 +1,60 @@
 # frozen_string_literal: true
+include ActiveJob::TestHelper
 
 describe NationalHearingQueueEntryRefreshJob, :postgres do
   describe "#perform" do
     context "when it follows the happy path" do
-      it "completes the national hearing queue refresh without fail" do
-        expect { NationalHearingQueueEntryRefreshJob.perform_now }.not_to raise_error
+      it "completes the national hearing queue refresh without raising any errors" do
+        expect do
+          perform_enqueued_jobs { NationalHearingQueueEntryRefreshJob.perform_later }
+        end.not_to raise_error
       end
     end
 
     context "when the queue refresh is expecting to timeout" do
       # allow(object).to receive(:method).and_return(value)
+      let(:job) { NationalHearingQueueEntryRefreshJob.new }
       before(:each) do
         allow(NationalHearingQueueEntry).to receive(:refresh).and_raise(ActiveRecord::StatementTimeout)
       end
 
-      it "calls timeout_set to change the timeout value and calls perform" do
-        job = NationalHearingQueueEntryRefreshJob.new
-        expect(job).to receive(:perform).exactly(2).times.and_call_original
-        expect(job).to receive(:timeout_set).exactly(2).times
+      it "doesn't raise any errors" do
+        expect do
+          perform_enqueued_jobs { NationalHearingQueueEntryRefreshJob.perform_later }
+        end.not_to raise_error
+      end
 
-        job.perform_now
+      it "ensures perform is recursively ran" do
+        expect(job).to receive(:perform).exactly(2).times.and_call_original
+        job.perform
+      end
+
+      it "calls timeout_set to change the timeout value and calls perform2" do
+        expect(job).to receive(:timeout_set).exactly(2).times
+        job.perform
       end
 
       it "logs the error" do
-        job = NationalHearingQueueEntryRefreshJob.new
         expect(job).to receive(:log_error)
-
-        job.perform_now
+        job.perform
       end
     end
 
     context "when the queue refresh is expecting a standard error" do
+      let(:job) { NationalHearingQueueEntryRefreshJob.new }
       before do
         allow(NationalHearingQueueEntry).to receive(:refresh).and_raise(StandardError)
       end
-      it "logs the error" do
-        job = NationalHearingQueueEntryRefreshJob.new
-        expect(job).to receive(:log_error)
+      
+      it "doesn't raise any errors" do
+        expect do
+          perform_enqueued_jobs { NationalHearingQueueEntryRefreshJob.perform_later }
+        end.not_to raise_error
+      end
 
-        job.perform_now
+      it "logs the error" do
+        expect(job).to receive(:log_error)
+        job.perform
       end
     end
   end
