30e380e6071ccb73c7def130941123856fe1ebf3
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-11-06 11:47:41 -0500

    refactored to take in singular person and duplicate ids and remediate

--- app/services/remediations/duplicate_person_remediation_service.rb
+++ app/services/remediations/duplicate_person_remediation_service.rb
@@ -1,72 +1,36 @@
 # frozen_string_literal: true
 
 class Remediations::DuplicatePersonRemediationService
-  def initialize(person_ids)
-    @person_ids = person_ids
+  ASSOCIATIONS = [
+    Claimant,
+    DecisionIssue,
+    EndProductEstablishment,
+    RequestIssue,
+    Notification
+  ].freeze
+
+  def initialize(updated_person_id:, duplicate_person_ids:)
+    @updated_person_id = updated_person_id
+    @duplicate_person_ids = duplicate_person_ids
   end
 
   def remediate!
-    # in this method we will implement some logic to find and delete any duplicate persons
-    # will check by ssn
-    fix_duplicates
+    find_and_update_records
   end
 
-  def fix_duplicates
-    updated_people.each do |person|
-      find_and_update_records(person)
+  def find_and_update_records
+    dup_persons = Person.where(id: @duplicate_person_ids)
+    og_person = Person.find_by(id: @updated_person_id)
+
+    ASSOCIATIONS.each do |klass|
+      update_found_records(klass, dup_persons, og_person)
     end
-  end
-
-  def find_and_update_records(person)
-    dup_persons = Person.where(ssn: person.ssn)
-    og_person = dup_persons.find(person.id)
-    dup_person = dup_persons.where.not(id: person.id).first
-    find_and_update_claimants(dup_person, og_person)
-    # find_and_update_decision_issues()
-
     # destroy dup_person
   end
 
-  def updated_people
-    @person_ids.map do |id|
-      Person.find_by(id: id)
-    end
+  def update_found_records(klass, dup_persons, og_person)
+    column = klass.column_names.find { |name| name.end_with?("participant_id") }
+    records = klass.where("#{column}": dup_persons.pluck(:participant_id))
+    records.update_all("#{column}": og_person.participant_id)
   end
-
-  def find_and_update_claimants(dup_person, og_person)
-    claimants = Claimant.where(participant_id: dup_person.participant_id)
-
-    claimants.each do |claimant|
-      claimant.update!(participant_id: og_person.participant_id)
-    end
-  end
-
-  # def update_records_for_person(dup_person, og_person)
-  #   find_and_update_claimants(dup_person, og_person)
-  #   # find_and_update_decision_issues(dup_person, og_person)
-  #   # find_and_update_request_issues(dup_person, og_person)
-  # end
 end
-
-
-# hash = {
-#   og_person_id: 1234,
-#   dup_person_id: 2234
-# }
-
-# # array of claimants to update
-
-
-# def claimants_to_update(dup_person)
-#   dup_person = Person.find_by(id: hash[:dup_person_id])
-#   Claimant.where(participant_id: dup_person.particpant_id)
-# end
-
-
-# def update_claimants(claimants, hash)
-#   og_person = Person.find_by(id: hash[:og_person_id])
-#   claimants.map do |c|
-#     claimant.update!(participant_id: og_person.participant_id)
-#   end
-# end
-
