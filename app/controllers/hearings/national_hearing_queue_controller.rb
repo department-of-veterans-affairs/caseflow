2070f8744499354a4ec3df216f70a5dab06548a8
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-11-06 23:20:00 -0500

    Expand case statement

--- app/controllers/hearings/national_hearing_queue_controller.rb
+++ app/controllers/hearings/national_hearing_queue_controller.rb
@@ -34,10 +34,14 @@ class Hearings::NationalHearingQueueController < ApplicationController
     entries = NationalHearingQueueEntry.all.order(:priority_queue_number)
 
     filtered_entries = case tab_name
-                       when "all" then entries
-                       when "unassigned" then entries.where(assigned_to_type: "Organization", task_status: "assigned")
-                       when "on_hold" then entries.where(task_status: "on_hold")
-                       when "assigned" then entries.where(assigned_to_type: "User", task_status: "assigned")
+                       when "all"
+                         entries
+                       when "unassigned"
+                         entries.where(assigned_to_type: "Organization", task_status: "assigned")
+                       when "on_hold"
+                         entries.where(task_status: "on_hold")
+                       when "assigned"
+                         entries.where(assigned_to_type: "User", task_status: "assigned")
                        else
                          return render json: { error: "#{tab_name} is not a recognized tab name." }
                        end
