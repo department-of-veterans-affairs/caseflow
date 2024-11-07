bc9439a49a4142dce81f803c3e8f08946036c943
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-11-07 12:52:27 -0500

    fixed linting error

--- spec/services/remediations/duplicate_person_remediation_service_spec.rb
+++ spec/services/remediations/duplicate_person_remediation_service_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Remediations::DuplicatePersonRemediationService, type: :service d
   let(:duplicate_person_2) { create(:person) }
 
   let(:duplicate_person_ids) { [duplicate_person_1.id, duplicate_person_2.id] }
-  let(:dup_person_service) { described_class.new(updated_person_id: updated_person.id, duplicate_person_ids: duplicate_person_ids) }
+  let(:dup_person_service) do
+    described_class.new(updated_person_id: updated_person.id, duplicate_person_ids: duplicate_person_ids)
+  end
 
   describe "#remediate!" do
     it "calls the find_and_update_records method to find and update records" do
