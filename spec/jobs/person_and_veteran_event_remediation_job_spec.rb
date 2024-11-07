d52308f3208c194a77e3b7a664e55e6c6901c82e
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-11-07 11:41:41 -0500

    wip added tests

--- spec/jobs/person_and_veteran_event_remediation_job_spec.rb
+++ spec/jobs/person_and_veteran_event_remediation_job_spec.rb
@@ -1,72 +1,83 @@
 # frozen_string_literal: true
 
-require "support/shared_context/sync_vet_remediations"
+RSpec.describe PersonAndVeteranEventRemediationJob, type: :job do
+  let(:job) { described_class.new }
 
-RSpec.describe PersonAndVeteranEventRemediationJob do
-  include ActiveJob::TestHelper
+  # # Create event records using the factory with traits
+  # let!(:person_event_record) { create(:event_record, :person) }
+  # let!(:veteran_event_record) { create(:event_record, :veteran) }
 
-  include_context "sync_vet_remediations"
+  # Mocking external services and data
+  let(:event_record_person) { instance_double("EventRecord", evented_record_id: 1, evented_record_type: "Person", evented_record: person) }
+  let(:event_record_veteran) { instance_double("EventRecord", evented_record_id: 2, evented_record_type: "Veteran", info: event_info) }
 
-  context ".perform" do
-    let(:current_user) { create(:user, roles: ["System Admin"]) }
-    let(:create_job) do
-      PersonAndVeteranEventRemediationJob.new
-    end
-    let(:person_event_record) {
-      create(:person_event_record, info: { "before_data" =>
-      {
-        "id" => 5854,
-        "participant_id" => "601486438",
-        "date_of_birth" => "Thu, 01 Jan 1970",
-        "created_at" => "Wed, 30 Oct 2024 17:32:46.642838000 UTC +00:00",
-        "updated_at" => "Wed, 30 Oct 2024 17:32:47.112988000 UTC +00:00",
-        "first_name" => "HEIDI",
-        "last_name" => "HERMAN",
-        "middle_name" => nil,
-        "name_suffix" => nil,
-        "email_address" => nil,
-        "ssn" => "683378050"
-      }, "record_data" => {
-        "id" => 5854,
-        "participant_id" => "601486438",
-        "date_of_birth" => "Thu, 01 Jan 1970",
-        "created_at" => "Wed, 30 Oct 2024 17:32:46.642838000 UTC +00:00",
-        "updated_at" => "Wed, 30 Oct 2024 17:32:47.112988000 UTC +00:00",
-        "first_name" => "HEIDI",
-        "last_name" => "HERMAN",
-        "middle_name" => nil,
-        "name_suffix" => nil,
-        "email_address" => nil,
-        "ssn" => "683378050"
-      }, "update_type" => "U" })
-    }
+  let(:person) { instance_double("Person", ssn: "123-45-6789") }
+  let(:person_duplicate) { instance_double("Person", id: 2, ssn: "123-45-6789") }
+  let(:veteran_record) { instance_double("Veteran", file_number: "V1234") }
 
-    let(:person_1) { create(:person, participant_id: "601486438", ssn: "683378050") }
-    let(:person_2) { create(:person, participant_id: "601486439", ssn: "683378050") }
+  let(:event_info) { { "before_data" => { "file_number" => "V1234" }, "record_data" => { "file_number" => "V12345" } } }
 
-    subject { create_job }
-    # we are uncertain how to get staged data for an event to test these.
-    # would we need/want to create a factory or just mock up an Event hash?
+  # Mocks for remediation services
+  let(:duplicate_person_service) { instance_double("DuplicatePersonRemediationService") }
+  let(:veteran_remediation_service) { instance_double("VeteranRecordRemedationService") }
 
-    it "sets a current user" do
-      expect(current_user).to be_an_instance_of(User)
-    end
+  before do
+    allow(RequestStore.store).to receive(:[]=)
+    allow(User).to receive(:system_user).and_return(User.new) # Assuming there's a system_user
+  end
 
-    xit "finds and remediates duplicate person records" do
-      subject.perform_now
-      expect(person_2).to be_deleted
+  describe "#perform" do
+    context "when processing Person events" do
+      before do
+        # Mocking find_events to return an array with our event_record_person
+        allow(job).to receive(:find_events).with("Person").and_return([event_record_person])
+
+        # Mock Person.where to return duplicates
+        allow(Person).to receive(:where).with(ssn: "123-45-6789").and_return([person, person_duplicate])
+        allow(DuplicatePersonRemediationService).to receive(:new).and_return(duplicate_person_service)
+        allow(duplicate_person_service).to receive(:remediate!)
+      end
+
+      it "calls remediation service if duplicate persons are found" do
+        expect(duplicate_person_service).to receive(:remediate!)
+        job.perform
+      end
+
+      it "does not call remediation service if no duplicates are found" do
+        # Modify the mock to return only the original person
+        allow(Person).to receive(:where).with(ssn: "123-45-6789").and_return([person])
+        expect(duplicate_person_service).not_to receive(:remediate!)
+        job.perform
+      end
     end
 
-    xit "sends an array of found veteran ids with changed file numbers" do
-      # get some sort of count of veteran ids with changed file numbers that get returned
-    end
+    context "when processing Veteran events" do
+      before do
+        # Mocking find_events to return an array with our event_record_veteran
+        allow(job).to receive(:find_events).with("Veteran").and_return([event_record_veteran])
 
-    xit "sends an array of found person ids with same ssn numbers" do
-      # get some sort of count of dup person ids that get returned
-    end
+        allow(VeteranRecordRemedationService).to receive(:new).and_return(veteran_remediation_service)
+        allow(veteran_remediation_service).to receive(:remediate!)
+      end
 
-    xit "sends an array of found veteran ids with changed file numbers" do
-      # get some sort of count of veteran ids with changed file numbers that get returned
+      it "calls remediation service if file_number differs" do
+        expect(veteran_remediation_service).to receive(:remediate!)
+        job.perform
+      end
+
+      it "does not call remediation service if file_number is the same" do
+        # Modify event_info to make file numbers match
+        allow(event_record_veteran).to receive(:info).and_return({ "before_data" => { "file_number" => "V1234" }, "record_data" => { "file_number" => "V1234" } })
+        expect(veteran_remediation_service).not_to receive(:remediate!)
+        job.perform
+      end
+    end
+  end
+
+  describe "#setup_job" do
+    it "sets current_user in the RequestStore" do
+      expect(RequestStore.store).to receive(:[]=).with(:current_user, User.system_user)
+      job.send(:setup_job)  # Calling private method directly for test
     end
   end
 end
