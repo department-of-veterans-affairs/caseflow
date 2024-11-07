48a613aaea180c74b8493fd9e8454276ac3a8f38
Tyler Broyles <109369527+TylerBroyles@users.noreply.github.com>
2024-11-07 13:07:28 -0500

    TYLERB/APPEALS-63252: Ensure validation text exists for filter options (#23396)
    * Initial commit that adds the no future dates setting to the DatePicker filter component. Add error handling and messaging to support that new property. Changed the inputs to the DateSelector component for better component reuse and for the error messaging. Also added error messaging for the between values so that the to date must be after the from date.
    
    * Added noFutureDates to the DatePicker filter component state. Added checks to the filter apply button to work with noFutureDates. Cleaned up some unused code.
    
    * Added a new test for the noFutureDates setting for the DatePicker filter. Updated the DatePicker date check to use Date.now() instead of new Date(). Updated snapshots to match the new DateSelector objects.
    
    * Updated some failing jest tests.
    
    * Altered the downloads helper grep regex to hopefully redux flakyness for some spec tests that use it.
    
    * Pulled QueueTable test and snapshots from main.
    
    * Updated jest snapshots for DatePicker test.
    
    * Moved a couple of constant strings to the COPY.json file.
    
    ---------
    
    Co-authored-by: Brandon Lee Dorner <51007432+brandondorner@users.noreply.github.com>

--- client/app/components/DatePicker.jsx
+++ client/app/components/DatePicker.jsx
@@ -6,6 +6,7 @@ import SearchableDropdown from '../components/SearchableDropdown';
 import Button from '../components/Button';
 import COPY from '../../COPY';
 import moment from 'moment-timezone';
+import DateSelector from './DateSelector';
 
 const datePickerStyle = css({
   paddingLeft: '0.95rem',
@@ -146,6 +147,7 @@ class DatePicker extends React.PureComponent {
     const position = (props.settings && props.settings.position) || 'left';
     const buttons = (props.settings && props.settings.buttons) || false;
     const selected = (props.selected && props.selected) || false;
+    const noFutureDates = (props.settings && props.settings.noFutureDates) || false;
 
     this.state = {
       open: false,
@@ -154,7 +156,8 @@ class DatePicker extends React.PureComponent {
       endDate: '',
       position,
       buttons,
-      selected
+      selected,
+      noFutureDates
     };
   }
 
@@ -218,11 +221,22 @@ class DatePicker extends React.PureComponent {
     return this.state.open || this.state.selected;
   }
 
+  isDateInFuture = (date) => {
+    if (!date) {
+      return false;
+    }
+
+    return Boolean(Date.parse(date) > Date.now());
+  }
+
   buttonDisabled = () => {
     let disabled = true;
 
     if (this.state.mode === 'between') {
       if (this.state.startDate === '' || this.state.endDate === '') {
+        disabled = true;
+      } else if (this.state.noFutureDates &&
+        (this.isDateInFuture(this.state.startDate) || this.isDateInFuture(this.state.endDate))) {
         disabled = true;
       } else {
         const startDate = moment(`${this.state.startDate} 00:00:00`).valueOf();
@@ -230,6 +244,9 @@ class DatePicker extends React.PureComponent {
 
         disabled = startDate >= endDate;
       }
+
+    } else if (this.state.noFutureDates && this.isDateInFuture(this.state.startDate)) {
+      disabled = true;
     } else if (this.state.mode !== '') {
       disabled = this.state.startDate === '';
     } else if (this.state.mode === 'all') {
@@ -302,7 +319,33 @@ class DatePicker extends React.PureComponent {
     return defaultOptions;
   };
 
+  startDateErrorMessage = () => {
+    if (this.state.noFutureDates && this.state.startDate && this.isDateInFuture(this.state.startDate)) {
+      return COPY.DATE_PICKER_NO_FUTURE_DATES_ERROR_MESSAGE;
+    }
+
+    return '';
+  }
+
+  endDateErrorMessage = () => {
+    if (this.state.noFutureDates && this.state.endDate && this.isDateInFuture(this.state.endDate)) {
+      return COPY.DATE_PICKER_NO_FUTURE_DATES_ERROR_MESSAGE;
+    }
+
+    if (this.state.mode === 'between' && this.state.startDate !== '' && this.state.endDate !== '') {
+      const startDate = moment(`${this.state.startDate} 00:00:00`).valueOf();
+      const endDate = moment(`${this.state.endDate} 23:59:59`).valueOf();
+
+      if (startDate >= endDate) {
+        return COPY.DATE_PICKER_BETWEEN_DATES_ERROR_MESSAGE;
+      }
+    }
+
+    return '';
+  }
+
   render() {
+
     return <span {...datePickerStyle} ref={(rootElem) => {
       this.rootElem = rootElem;
     }}>
@@ -338,28 +381,31 @@ class DatePicker extends React.PureComponent {
             {additionalOptions.some((option) => option.value === this.state.mode) ?
               null :
               <div className="input-wrapper">
-                <label aria-label="start-date"
-                  htmlFor="start-date">
-                  {this.state.mode === 'between' ? COPY.DATE_PICKER_FROM : COPY.DATE_PICKER_DATE}</label>
-                <input
-                  id="start-date"
+                <DateSelector
+                  label={this.state.mode === 'between' ? COPY.DATE_PICKER_FROM : COPY.DATE_PICKER_DATE}
                   name="start-date"
+                  id="start-date"
+                  noFutureDates={this.state.noFutureDates}
                   defaultValue={this.state.startDate}
+                  errorMessage={this.startDateErrorMessage()}
+                  onChange={(value) => this.setState({ startDate: value })}
                   type="date"
-                  onChange={(event) => this.setState({ startDate: event.target.value })}
                 />
               </div>
+
             }
 
             {this.state.mode === 'between' &&
               <div className="input-wrapper">
-                <label aria-label="end-date" htmlFor="end-date">{COPY.DATE_PICKER_TO}</label>
-                <input
-                  id="end-date"
+                <DateSelector
+                  label={COPY.DATE_PICKER_TO}
                   name="end-date"
+                  id="end-date"
+                  noFutureDates={this.state.noFutureDates}
                   defaultValue={this.state.endDate}
+                  errorMessage={this.endDateErrorMessage()}
+                  onChange={(value) => this.setState({ endDate: value })}
                   type="date"
-                  onChange={(event) => this.setState({ endDate: event.target.value })}
                 />
               </div>
             }
