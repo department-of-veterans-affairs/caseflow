40d484c2f1d5f23a30e56dec6628dcfe5f965ec6
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-11-07 11:42:00 -0500

    wip added tests

--- spec/services/remediations/duplicate_person_remediation_service_spec.rb
+++ spec/services/remediations/duplicate_person_remediation_service_spec.rb
@@ -1,27 +1,17 @@
 # frozen_string_literal: true
 
-# require "support/shared_context/sync_vet_remediations"
+RSpec.describe Remediations::DuplicatePersonRemediationService, type: :service do
+  let(:updated_person) { create(:person) }
+  let(:duplicate_person_1) { create(:person) }
+  let(:duplicate_person_2) { create(:person) }
 
-# person_remediation_event_record <-- name this in the file
-RSpec.describe Remediations::DuplicatePersonRemediationService do
-  include ActiveJob::TestHelper
+  let(:duplicate_person_ids) { [duplicate_person_1.id, duplicate_person_2.id] }
+  let(:dup_person_service) { described_class.new(updated_person_id: updated_person.id, duplicate_person_ids: duplicate_person_ids) }
 
-  # include_context "sync_vet_remediations"
-
-  let(:person_ids) { %w[1234 5678 9101 1213] }
-
-  describe ".initialize" do
-    it "exists" do
-      expect(person_ids).to be_an(Array)
-      expect(person_ids.first).to eq("1234")
-      expect(person_ids.first).to be_a(String)
-    end
-  end
-
-  describe ".remediate" do
-    xit "remediates persoupdated ssn numbers" do
-      # this test will check the logic to find and delete any duplicate persons
-      # will check by ssn
+  describe "#remediate!" do
+    it "calls the find_and_update_records method to find and update records" do
+      expect(dup_person_service).to receive(:find_and_update_records)
+      dup_person_service.remediate!
     end
   end
 end
