5ceb0679c7cb5802e7c5b936123533d9e8b1fb4a
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-08 11:11:41 -0500

    updates remediate method to use long hand if/else. Flat_map dup_vet_collections

--- app/services/remediations/veteran_record_remediation_service.rb
+++ app/services/remediations/veteran_record_remediation_service.rb
@@ -11,10 +11,33 @@ class Remediations::VeteranRecordRemediationService
   end
 
   def remediate!
-    fix_vet_records
+    # check for dup
+    # if dup exists run fix_vet_records on
+    # run dup_fix if @before_fn == @after_fn : fix_vet_records
+    # Check for duplicates based on the file number
+    real_v = Veteran.find_by_file_number(@after_fn)
+    dups = Veteran.where(ssn: real_v.ssn).reject { |v| v.id == real_v.id }
+
+    if dups.any?
+      # If there are duplicates, run dup_fix on @after_fn
+      dup_fix(@after_fn)
+    else
+      # Otherwise, fix veteran records normally
+      fix_vet_records
+    end
+    # dup_fix(@after_fn) ? @before_fn == @after_fn : fix_vet_records
   end
 
   private
+
+  def dup_fix(file_number)
+    real_v = Veteran.find_by_file_number(file_number)
+    dups = Veteran.where(ssn: real_v.ssn).reject { |v| v.id == real_v.id }
+    duplicate_veterans_collections = dups.flat_map { |dup| grab_collections(dup.file_number) }
+    update_records!(duplicate_veterans_collections, file_number)
+    # may need to fix intakes with veteran id
+    # destroy dup
+  end
 
   def fix_vet_records
     # fixes file number
