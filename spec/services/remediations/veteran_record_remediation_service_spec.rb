ee3dd0205baefab8eb890261f52052b561fac0f0
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-08 11:12:14 -0500

    update test to account for duplicate vets to remediate

--- spec/services/remediations/veteran_record_remediation_service_spec.rb
+++ spec/services/remediations/veteran_record_remediation_service_spec.rb
@@ -34,27 +34,130 @@ RSpec.describe Remediations::VeteranRecordRemediationService do
       end
     end
 
+    let(:mock_veteran) { instance_double("Veteran", ssn: "123-45-6789", id: 1, file_number: before_fn) }
+    let(:duplicate_veteran) { instance_double("Veteran", ssn: "123-45-6789", id: 2, file_number: before_fn) }
+
     before do
+      # Stub Veteran.find_by_file_number to handle both before_fn and after_fn
+      allow(Veteran).to receive(:find_by_file_number).with(before_fn).and_return(mock_veteran)
+      allow(Veteran).to receive(:find_by_file_number).with(after_fn).and_return(mock_veteran)
+
+      # Stub Veteran.where to return mock_veteran and duplicate_veteran for the same SSN
+      allow(Veteran).to receive(:where).with(ssn: "123-45-6789").and_return([mock_veteran, duplicate_veteran])
+
+      # Allow grab_collections to return mock classes
       allow(vet_record_service).to receive(:grab_collections).with(before_fn).and_return(mock_classes)
     end
 
-    it "updates each associated object's file_number to the new value" do
-      mock_classes.each do |mock_instance|
-        column = column_mapping[mock_instance.class]
-        expect(mock_instance).to receive(:update!).with(after_fn).and_wrap_original do
-          mock_instance.send("#{column}=", after_fn)
+    context "when there are duplicate veterans" do
+      it "runs dup_fix and updates the file_number for duplicates" do
+        # We expect the `dup_fix` method to be called and the collections for the duplicate veterans to be updated.
+        expect(vet_record_service).to receive(:dup_fix).with(after_fn).and_call_original
+
+        mock_classes.each do |mock_instance|
+          column = column_mapping[mock_instance.class]
+          expect(mock_instance).to receive(:update!).with(after_fn).and_wrap_original do
+            mock_instance.send("#{column}=", after_fn)
+          end
+        end
+
+        vet_record_service.remediate!
+
+        # Verify that all mock instances' file numbers are updated
+        mock_classes.each do |mock_instance|
+          column = column_mapping[mock_instance.class]
+          expect(mock_instance.send(column)).to eq(after_fn)
         end
       end
+    end
 
-      vet_record_service.remediate!
+    context "when there are no duplicate veterans" do
+      before do
+        # Ensure no duplicates exist for the `before_fn`
+        allow(Veteran).to receive(:where).with(ssn: "123-45-6789").and_return([mock_veteran])
+      end
 
-      mock_classes.each do |mock_instance|
-        column = column_mapping[mock_instance.class]
-        expect(mock_instance.send(column)).to eq(after_fn)
+      it "does not run dup_fix and updates file_number normally" do
+        # Expect that `dup_fix` should not be called in this scenario.
+        expect(vet_record_service).not_to receive(:dup_fix)
+
+        mock_classes.each do |mock_instance|
+          column = column_mapping[mock_instance.class]
+          expect(mock_instance).to receive(:update!).with(after_fn).and_wrap_original do
+            mock_instance.send("#{column}=", after_fn)
+          end
+        end
+
+        vet_record_service.remediate!
+
+        # Verify that all mock instances' file numbers are updated
+        mock_classes.each do |mock_instance|
+          column = column_mapping[mock_instance.class]
+          expect(mock_instance.send(column)).to eq(after_fn)
+        end
       end
     end
   end
 end
+
+# -----------------------------
+# Below is the test that we made before adding the dup_fix method
+
+# RSpec.describe Remediations::VeteranRecordRemediationService do
+#   let(:before_fn) { "12345" }
+#   let(:after_fn) { "54321" }
+#   let(:vet_record_service) { described_class.new(before_fn, after_fn) }
+
+#   # Define a mapping of classes to their file number columns
+#   let(:column_mapping) do
+#     {
+#       Appeal => "veteran_file_number",
+#       AvailableHearingLocations => "veteran_file_number",
+#       BgsPowerOfAttorney => "file_number",
+#       Document => "file_number",
+#       EndProductEstablishment => "reference_id",
+#       Form8 => "file_number",
+#       HigherLevelReview => "veteran_file_number",
+#       Intake => "veteran_file_number",
+#       LegacyAppeal => "vbms_id",
+#       RampElection => "veteran_file_number",
+#       RampRefiling => "veteran_file_number",
+#       SupplementalClaim => "veteran_file_number"
+#     }
+#   end
+
+#   describe "#remediate!" do
+#     let(:mock_classes) do
+#       Remediations::VeteranRecordRemediationService::ASSOCIATED_OBJECTS.map do |klass|
+#         mock_instance = klass.new
+#         column = column_mapping[klass]
+#         mock_instance.send("#{column}=", before_fn)
+#         allow(FixFileNumberWizard::Collection).to receive(:new).with(klass, before_fn).and_return(mock_instance)
+#         mock_instance
+#       end
+#     end
+
+#     before do
+#       allow(vet_record_service).to receive(:grab_collections).with(before_fn).and_return(mock_classes)
+#     end
+
+#     it "updates each associated object's file_number to the new value" do
+#       mock_classes.each do |mock_instance|
+#         column = column_mapping[mock_instance.class]
+#         expect(mock_instance).to receive(:update!).with(after_fn).and_wrap_original do
+#           mock_instance.send("#{column}=", after_fn)
+#         end
+#       end
+
+#       vet_record_service.remediate!
+
+#       mock_classes.each do |mock_instance|
+#         column = column_mapping[mock_instance.class]
+#         expect(mock_instance.send(column)).to eq(after_fn)
+#       end
+#     end
+#   end
+# end
 
 # -------------------------
 
