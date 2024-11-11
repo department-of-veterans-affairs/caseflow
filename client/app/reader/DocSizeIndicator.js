97c84b7e4cba42d23f31223661f9a71d78080dbd
Anusha Palliyil <palliyil_anusha@bah.com>
2024-11-11 16:39:49 -0500

    Add warning icon hover to file size column

--- client/app/reader/DocSizeIndicator.js
+++ client/app/reader/DocSizeIndicator.js
@@ -4,11 +4,24 @@ import { filesize } from 'filesize';
 import { SizeWarningIcon } from '../components/icons/SizeWarningIcon';
 import { ICON_SIZES } from '../constants/AppConstants';
 
+
 const DocSizeIndicator = (props) => {
   const downloadTime = props.docSize / props.browserSpeedInBytes
 
   return (
     <span>{filesize(props.docSize)} {downloadTime > 15 ? <SizeWarningIcon size={ICON_SIZES.SMALL} /> : ''}</span>
+  );
+};
+
+const LARGE_FILE_SIZE_THRESHOLD = 30 * 1024 * 1024; // 30 MB in bytes
+
+const DocSizeIndicator = ({n filesize }) => {
+  const isLargeFile = fileSize > LARGE_FILE_SIZE_THRESHOLD;
+
+  return (
+    <div className="doc-size-indicator">
+      {isLargeFile && <SizeWarningIcon size={24} className="large-file-warning" />}
+    </div>
   );
 };
 
