ea15c1cda6aab9e4c412921108d9749421fc1ded
Sean Parker <155660052+seanrpa@users.noreply.github.com>
2024-11-12 15:11:03 -0500

    Formatting improvements and setting control case to be created by attorney and not judge

--- db/seeds/legacy_appeals_for_vlj_movement_testing.rb
+++ db/seeds/legacy_appeals_for_vlj_movement_testing.rb
@@ -2,9 +2,9 @@
 
 module Seeds
   class LegacyAppealsForVljMovementTesting < Base
-    def initialze
+    def initialize
       RequestStore[:current_user] = User.system_user
-      initial_scenario_id_6_values
+      initial_scenario_6_id_values
     end
 
     def seed!
@@ -43,19 +43,88 @@ module Seeds
       wqjudge_six = create_scenario_6_veteran(first_name: "Wqjudge", last_name: "Six")
       wqjudge_control = create_scenario_6_veteran(first_name: "Wqjudge", last_name: "Control")
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :aod, :assigned, user: User.find_by_css_id("BVACOTBJUDGE"), assigner: User.find_by_css_id("BVALSHIELDS"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_one.file_number}S", case_issues: create_list(:case_issue, 2, :compensation)))
+      # Case 1: Wqjudge One (Priority, 2 issues)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :aod,
+        :assigned,
+        user: User.find_by_css_id("BVACOTBJUDGE"),
+        assigner: User.find_by_css_id("BVALSHIELDS"),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_one.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :assigned, user: User.find_by_css_id("BVACOTBJUDGE"), assigner: User.find_by_css_id("BVACOTBJUDGE"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_two.file_number}S", case_issues: create_list(:case_issue, 1, :compensation)))
+      # Case 2: Wqjudge Two (Nonpriority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :assigned,
+        user: User.find_by_css_id("BVACOTBJUDGE"),
+        assigner: User.find_by_css_id("BVACOTBJUDGE"),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_two.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :assigned, user: User.find_by_css_id("BVACOTBJUDGE"), assigner: User.find_by_css_id(""), as_judge_assign_task: false, bfcorlid: "#{wqjudge_three.file_number}S", case_issues: create_list(:case_issue, 2, :compensation)))
+      # Case 3: Wqjudge Three (Nonpriority, 2 issues)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :assigned,
+        user: User.find_by_css_id("BVACOTBJUDGE"),
+        assigner: User.find_by_css_id(""),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_three.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :aod, :assigned, user: User.find_by_css_id("BVAGSPORER"), assigner: User.find_by_css_id("BVAJWEHNER1"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_four.file_number}S", case_issues: create_list(:case_issue, 1, :compensation)))
+      # Case 4: Wqjudge Four (Priority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :aod,
+        :assigned,
+        user: User.find_by_css_id("BVAGSPORER"),
+        assigner: User.find_by_css_id("BVAJWEHNER1"),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_four.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :aod, :assigned, user: User.find_by_css_id("BVAGSPORER"), assigner: User.find_by_css_id("BVAOTRANTOW"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_five.file_number}S", case_issues: create_list(:case_issue, 2, :compensation)))
+      # Case 5: Wqjudge Five (Priority, 2 issues)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :aod,
+        :assigned,
+        user: User.find_by_css_id("BVAGSPORER"),
+        assigner: User.find_by_css_id("BVAOTRANTOW"),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_five.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :assigned, user: User.find_by_css_id("BVAGSPORER"), assigner: User.find_by_css_id("BVAGSPORER"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_six.file_number}S", case_issues: create_list(:case_issue, 1, :compensation)))
+      # Case 6: Wqjudge Six (Nonpriority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :type_cavc_remand,
+        :assigned,
+        user: User.find_by_css_id("BVAGSPORER"),
+        assigner: User.find_by_css_id("BVAGSPORER"),
+        as_judge_assign_task: false,
+        bfcorlid: "#{wqjudge_six.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      create(:legacy_appeal, :with_veteran, vacols_case: create(:case, :type_cavc_remand, :aod, :assigned, user: User.find_by_css_id("BVAGSPORER"), assigner: User.find_by_css_id("BVAOTRANTOW"), as_judge_assign_task: false, bfcorlid: "#{wqjudge_control.file_number}S", case_issues: create_list(:case_issue, 1, :compensation)))
+      # Case 7: Wqjudge Control (priority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case,
+        :ready_for_distribution,
+        :aod,
+        bfcorlid: "#{wqjudge_control.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
     end
 
   end
