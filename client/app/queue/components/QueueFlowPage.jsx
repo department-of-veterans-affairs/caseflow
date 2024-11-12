9ed20a8b6ccde658abc0912ce107cc7523aff873
HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
2024-11-12 16:20:54 -0500

    added new message for AMA decisions to reflect that no changes are saved in the workflow

--- client/app/queue/components/QueueFlowPage.jsx
+++ client/app/queue/components/QueueFlowPage.jsx
@@ -39,8 +39,13 @@ class QueueFlowPage extends React.PureComponent {
     return true;
   });
 
-  cancelAttorneyCheckoutMsg = () => {
+  cancelAttorneyCheckoutMsg = (isLegacyAppeal) => {
+    if (!isLegacyAppeal) {
+      return COPY.MODAL_CANCEL_ATTORNEY_CHECKOUT_AMA;
+    }
+
     return COPY.MODAL_CANCEL_ATTORNEY_CHECKOUT_SPECIAL_ISSUES;
+
   }
 
   withUnblockedTransition = (callback = _.noop) => {
@@ -173,7 +178,7 @@ class QueueFlowPage extends React.PureComponent {
           onClick: this.cancelFlow
         }]}
         closeHandler={() => this.props.hideModal('cancelCheckout')}>
-        {this.cancelAttorneyCheckoutMsg()}
+        {this.cancelAttorneyCheckoutMsg(this.props.isLegacyAppeal)}
       </Modal>
     </div>}
     <AppSegment filledBackground>
