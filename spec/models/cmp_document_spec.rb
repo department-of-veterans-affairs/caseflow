07f46da2385ad319f71520ee9ca3cf26d3e5ea98
Alex Smith <2421172+tradesmanhelix@users.noreply.github.com>
2024-11-13 07:13:29 -0700

    Make Further Improvements to API Validations (#23479)
    - Required that dates have 4 digits in order to prevent dates too
      far in the past or future from being accepted.
    
    - Remove some presence validations to prevent duplicate validation
      errors for vbms_doctype_id and date_of_receipt.

--- spec/models/cmp_document_spec.rb
+++ spec/models/cmp_document_spec.rb
@@ -3,16 +3,20 @@
 RSpec.describe CmpDocument, type: :model do
   it { should validate_presence_of(:cmp_document_id) }
   it { should validate_presence_of(:cmp_document_uuid) }
-  it { should validate_presence_of(:date_of_receipt) }
   it { should validate_presence_of(:packet_uuid) }
-  it { should validate_presence_of(:vbms_doctype_id) }
-
-  it { should belong_to(:cmp_mail_packet).optional }
 
   it { should allow_value(Time.current.strftime(Date::DATE_FORMATS[:csv_date])).for(:date_of_receipt) }
+  it { should_not allow_value(nil).for(:date_of_receipt) }
   it { should_not allow_value("19900101").for(:date_of_receipt) }
+  it { should_not allow_value("0199-01-01").for(:date_of_receipt) }
+  it { should_not allow_value("1999-122-31").for(:date_of_receipt) }
+  it { should_not allow_value("1999-12-311").for(:date_of_receipt) }
+  it { should_not allow_value("1999-12-32").for(:date_of_receipt) }
   it { should_not allow_value("not really a date").for(:date_of_receipt) }
 
   it { should allow_value(Faker::Number.within(range: 1..100).to_s).for(:vbms_doctype_id) }
+  it { should_not allow_value(nil).for(:vbms_doctype_id) }
   it { should_not allow_value("not really an integer").for(:vbms_doctype_id) }
+
+  it { should belong_to(:cmp_mail_packet).optional }
 end
