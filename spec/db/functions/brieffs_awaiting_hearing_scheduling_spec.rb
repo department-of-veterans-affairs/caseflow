3f1c45bb4ee26ecd4e1bcd9d87138266aa42f7a8
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2024-11-14 13:12:49 -0500

    MattT/APPEALS-64534: Create PL/pgSQL Function to Return a Table of f_vacols_brieff Records Given a String Containing vacols_ids (#23484)
    * ➕  Add fx gem
    
    * Add gather_vacols_ids_of_hearing_schedulable_legacy_appeals function
    
    * Add a spec test
    
    * Lint roll
    
    * Add a user-defined brieff_record PG type
    
    * Add brieffs_awaiting_hearing_scheduling function
    
    * Update type in custom type
    
    * Add brieffs_awaiting_hearing_scheduling spec file
    
    * Update db/functions/brieffs_awaiting_hearing_scheduling_v01.sql
    
    * Update db/schema.rb
    
    * Add custom type creation to CI pipeline
    
    * Switch to using .fields
    
    * Fix test name
    
    * Add nullcheck to function to prevent errors
    
    ---------
    
    Co-authored-by: Matthew Thornton <ThorntonMatthew@users.noreply.github.com>

---
+++ spec/db/functions/brieffs_awaiting_hearing_scheduling_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe "brieffs_awaiting_hearing_scheduling" do
+  subject do
+    ActiveRecord::Base.connection.execute(
+      "SELECT * FROM brieffs_awaiting_hearing_scheduling()"
+    )
+  end
+
+  context "with legacy appeals meeting the criteria" do
+    include_context "Legacy appeals that may or may not appear in the NHQ"
+
+    after { DatabaseCleaner.clean_with(:truncation, except: %w[vftypes issref notification_events]) }
+
+    it "the expected brieff records are returned", bypass_cleaner: true do
+      expect(subject.to_a.pluck("bfkey")).to match_array(desired_vacols_ids)
+    end
+
+    it "the return type has all of the same columns as the source table", bypass_cleaner: true do
+      expect(VACOLS::Case.columns.map(&:name)).to match_array(subject.fields)
+    end
+  end
+
+  context "with no legacy apepals meeting the criteria" do
+    it "the function doesn't throw an error" do
+      expect { subject }.to_not raise_exception
+    end
+  end
+end
