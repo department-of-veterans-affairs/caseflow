17f0e561a4c841aa7a2ce42942d2aed465a332c6
Will Medders <93014155+wmedders21@users.noreply.github.com>
2024-11-14 14:30:43 -0600

    Wmedders21/appeals 61467 (#23400)
    * remove gold outer border from selected file input complonent
    
    * allow the placeholder text to show up in the actions dropdown
    
    * add action type copy
    
    * format case timeline instructions for ReviewTranscriptTask
    
    * add cancel action type to copy
    
    * Update jest expectation to match wireframes
    
    * Update tests for case timeline instructions on RevieTranscriptTask
    
    * remove test data from this location
    
    * fix unintended side effect to Currently Active tasks instructions after updating case timeline
    
    * correct task name in tests
    
    * change indentation
    
    * update snapshot
    
    ---------
    
    Co-authored-by: William Medders <wmedders21@users.noreply.github.com>
    Co-authored-by: Michael Bidwell <122634362+mchbidwell@users.noreply.github.com>

--- client/test/app/queue/components/TaskRows.test.js
+++ client/test/app/queue/components/TaskRows.test.js
@@ -3,84 +3,14 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import TaskRows from '../../../../app/queue/components/TaskRows';
 import COPY from '../../../../COPY';
 import {
-  completedReviewPackageTaskWithErrorsFoundData
+  completedReviewTranscriptTaskNoErrorsFoundData,
+  completedReviewPackageTaskWithErrorsFoundData,
+  cancelledReviewTranscriptTaskCancelTaskData
 } from '../../../data/queue/taskLists/index';
-
-const reviewTranscriptTask = {
-  uniqueId: '8115',
-  isLegacy: false,
-  type: 'ReviewTranscriptTask',
-  appealType: 'Appeal',
-  addedByCssId: null,
-  appealId: 1880,
-  externalAppealId: 'ba0ae03c-2331-4d79-ba1b-8c8089130979',
-  assignedOn: '2024-08-05T10:57:27.269-04:00',
-  closestRegionalOffice: null,
-  createdAt: '2024-09-04T10:57:27.317-04:00',
-  closedAt: '2024-09-04T10:57:27.315-04:00',
-  startedAt: '2024-08-12T10:57:27.315-04:00',
-  assigneeName: "Board of Veterans' Appeals",
-  assignedTo: {
-    cssId: null,
-    name: "Board of Veterans' Appeals",
-    id: 7,
-    isOrganization: true,
-    type: 'Bva'
-  },
-  assignedBy: {
-    firstName: 'Lauren',
-    lastName: 'Roth',
-    cssId: 'CSSID1847365',
-    pgId: 2579
-  },
-  completedBy: {
-    cssId: '123'
-  },
-  cancelledBy: {
-    cssId: null
-  },
-  cancelReason: null,
-  convertedBy: {
-    cssId: null
-  },
-  convertedOn: null,
-  taskId: '8110',
-  parentId: 8109,
-  label: 'Schedule hearing',
-  documentId: null,
-  externalHearingId: '6a59e69b-9ecb-4332-a0fb-b07fa2ce214f',
-  workProduct: null,
-  caseType: 'Original',
-  aod: false,
-  previousTaskAssignedOn: null,
-  placedOnHoldAt: null,
-  status: 'completed',
-  onHoldDuration: null,
-  instructions: [
-    'Schedule Veteran and/or appellant for Board hearing.'
-  ],
-  decisionPreparedBy: null,
-  availableActions: [],
-  timelineTitle: 'ReviewTranscriptTask completed',
-  hideFromQueueTableView: false,
-  hideFromTaskSnapshot: false,
-  hideFromCaseTimeline: false,
-  availableHearingLocations: [],
-  latestInformalHearingPresentationTask: {},
-  canMoveOnDocketSwitch: false,
-  timerEndsAt: null,
-  unscheduledHearingNotes: {},
-  ownedBy: "Board of Veterans' Appeals",
-  daysSinceLastStatusChange: 47,
-  daysSinceBoardIntake: 47,
-  id: '8115',
-  claimant: {},
-  appeal_receipt_date: '2024-06-18'
-};
 
 test('renders ReviewTranscriptTask details correctly', () => {
 
-  render(<TaskRows taskList={[reviewTranscriptTask]} appeal={{}} />);
+  render(<TaskRows taskList={[completedReviewTranscriptTaskNoErrorsFoundData]} appeal={{}} />);
 
   // Check if assigned by and assignee names are rendered correctly
   expect(screen.getByText('L. Roth')).toBeInTheDocument();
@@ -93,18 +23,59 @@ test('renders ReviewTranscriptTask details correctly', () => {
   expect(screen.getByText('View task instructions')).toBeInTheDocument();
 });
 
-test('toggles task instructions visibility', () => {
+test('toggles task instructions visibility - Action: No Errors found', () => {
 
-  render(<TaskRows taskList={[reviewTranscriptTask]} appeal={{}} />);
+  render(<TaskRows taskList={[completedReviewTranscriptTaskNoErrorsFoundData]} appeal={{}} />);
 
   // Check if the instructions are initially hidden
-  expect(screen.queryByText('Schedule Veteran and/or appellant for Board hearing.')).not.toBeInTheDocument();
+  expect(screen.queryByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).not.toBeInTheDocument();
+  expect(screen.queryByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_NO_ERRORS_ACTION_TYPE)).not.toBeInTheDocument();
+  expect(screen.queryByText('These are some notes')).not.toBeInTheDocument();
 
   // Click the toggle button
   fireEvent.click(screen.getByText('View task instructions'));
 
   // Check if the instructions are now visible
-  expect(screen.getByText('Schedule Veteran and/or appellant for Board hearing.')).toBeInTheDocument();
+  expect(screen.queryByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).toBeInTheDocument();
+  expect(screen.queryByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_NO_ERRORS_ACTION_TYPE)).toBeInTheDocument();
+  expect(screen.getByText('These are some notes')).toBeInTheDocument();
+});
+
+test('toggles task instructions visibility - Action: With Errors found', () => {
+
+  render(<TaskRows taskList={[completedReviewPackageTaskWithErrorsFoundData]} appeal={{}} />);
+
+  // Check if the instructions are initially hidden
+  expect(screen.queryByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).not.toBeInTheDocument();
+  expect(screen.queryByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_ERRORS_ACTION_TYPE)).not.toBeInTheDocument();
+  expect(screen.queryByText('test_file.pdf')).not.toBeInTheDocument();
+  expect(screen.queryByText('These are some notes')).not.toBeInTheDocument();
+
+  // Click the toggle button
+  fireEvent.click(screen.getByText('View task instructions'));
+
+  // Check if the instructions are now visible
+  expect(screen.getByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).toBeInTheDocument();
+  expect(screen.getByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_ERRORS_ACTION_TYPE)).toBeInTheDocument();
+  expect(screen.getByText('test_file.pdf')).toBeInTheDocument();
+});
+
+test('toggles task instructions visibility - Action: Cancel task', () => {
+
+  render(<TaskRows taskList={[cancelledReviewTranscriptTaskCancelTaskData]} appeal={{}} />);
+
+  // Check if the instructions are initially hidden
+  expect(screen.queryByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).not.toBeInTheDocument();
+  expect(screen.queryByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_CANCEL_ACTION_TYPE)).not.toBeInTheDocument();
+  expect(screen.queryByText('these are cancellation notes')).not.toBeInTheDocument();
+
+  // Click the toggle button
+  fireEvent.click(screen.getByText('View task instructions'));
+
+  // Check if the instructions are now visible
+  expect(screen.getByText(COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS)).toBeInTheDocument();
+  expect(screen.getByText(COPY.UPLOAD_TRANSCRIPTION_VBMS_CANCEL_ACTION_TYPE)).toBeInTheDocument();
+  expect(screen.getByText('these are cancellation notes')).toBeInTheDocument();
 });
 
 test('toggles task instructions visibility - Action: With Errors found', () => {
--- client/test/data/queue/taskLists/index.js
+++ client/test/data/queue/taskLists/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import { formatISO, sub } from 'date-fns';
 import COPY from '../../../../COPY';
 
@@ -346,4 +347,146 @@ export const completedReviewPackageTaskWithErrorsFoundData = {
   id: '8115',
   claimant: {},
   appeal_receipt_date: '2024-06-18'
+};
+
+export const completedReviewTranscriptTaskNoErrorsFoundData = {
+  uniqueId: '8115',
+  isLegacy: false,
+  type: 'ReviewTranscriptTask',
+  appealType: 'Appeal',
+  addedByCssId: null,
+  appealId: 1880,
+  externalAppealId: 'ba0ae03c-2331-4d79-ba1b-8c8089130979',
+  assignedOn: '2024-08-05T10:57:27.269-04:00',
+  closestRegionalOffice: null,
+  createdAt: '2024-09-04T10:57:27.317-04:00',
+  closedAt: '2024-09-04T10:57:27.315-04:00',
+  startedAt: '2024-08-12T10:57:27.315-04:00',
+  assigneeName: "Board of Veterans' Appeals",
+  assignedTo: {
+    cssId: null,
+    name: "Board of Veterans' Appeals",
+    id: 7,
+    isOrganization: true,
+    type: 'Bva'
+  },
+  assignedBy: {
+    firstName: 'Lauren',
+    lastName: 'Roth',
+    cssId: 'CSSID1847365',
+    pgId: 2579
+  },
+  completedBy: {
+    cssId: '123'
+  },
+  cancelledBy: {
+    cssId: null
+  },
+  cancelReason: null,
+  convertedBy: {
+    cssId: null
+  },
+  convertedOn: null,
+  taskId: '8110',
+  parentId: 8109,
+  label: 'Schedule hearing',
+  documentId: null,
+  externalHearingId: '6a59e69b-9ecb-4332-a0fb-b07fa2ce214f',
+  workProduct: null,
+  caseType: 'Original',
+  aod: false,
+  previousTaskAssignedOn: null,
+  placedOnHoldAt: null,
+  status: 'completed',
+  onHoldDuration: null,
+  instructions: [
+    COPY.REVIEW_TRANSCRIPT_TASK_DEFAULT_INSTRUCTIONS,
+    COPY.UPLOAD_TRANSCRIPTION_VBMS_NO_ERRORS_ACTION_TYPE,
+    'These are some notes'
+  ],
+  decisionPreparedBy: null,
+  availableActions: [],
+  timelineTitle: 'ReviewTranscriptTask completed',
+  hideFromQueueTableView: false,
+  hideFromTaskSnapshot: false,
+  hideFromCaseTimeline: false,
+  availableHearingLocations: [],
+  latestInformalHearingPresentationTask: {},
+  canMoveOnDocketSwitch: false,
+  timerEndsAt: null,
+  unscheduledHearingNotes: {},
+  ownedBy: "Board of Veterans' Appeals",
+  daysSinceLastStatusChange: 47,
+  daysSinceBoardIntake: 47,
+  id: '8115',
+  claimant: {},
+  appeal_receipt_date: '2024-06-18'
+};
+
+export const cancelledReviewTranscriptTaskCancelTaskData = {
+  uniqueId: '3188',
+  isLegacy: false,
+  type: 'ReviewTranscriptTask',
+  appealType: 'Appeal',
+  addedByCssId: null,
+  appealId: 23,
+  externalAppealId: '597513a0-9468-4785-8def-50cadf924138',
+  assignedOn: '2024-10-31T12:04:46.326-04:00',
+  closestRegionalOffice: null,
+  createdAt: '2024-10-31T12:04:46.326-04:00',
+  closedAt: '2024-10-31T12:07:50.081-04:00',
+  startedAt: null,
+  assigneeName: 'BVASYELLOW',
+  assignedTo: {
+    cssId: 'BVASYELLOW',
+    name: 'BVASYELLOW',
+    id: 121,
+    isOrganization: false,
+    type: 'User'
+  },
+  assignedBy: {
+    firstName: 'System',
+    lastName: 'User',
+    cssId: 'CASEFLOW1',
+    pgId: 1
+  },
+  completedBy: { cssId: null },
+  cancelledBy: { cssId: 'BVASYELLOW' },
+  cancelReason: null,
+  convertedBy: { cssId: null },
+  convertedOn: null,
+  taskId: '3188',
+  parentId: 84,
+  label: 'Review Transcript task',
+  documentId: null,
+  externalHearingId: null,
+  workProduct: null,
+  caseType: 'Original',
+  aod: false,
+  previousTaskAssignedOn: null,
+  placedOnHoldAt: null,
+  status: 'cancelled',
+  onHoldDuration: null,
+  instructions: [
+    'Review the hearing transcript and upload the final to VBMS once it has been reviewed for errors or corrected.',
+    'Cancel task',
+    'these are cancellation notes'
+  ],
+  decisionPreparedBy: null,
+  availableActions: [],
+  timelineTitle: 'ReviewTranscriptTask completed',
+  hideFromQueueTableView: false,
+  hideFromTaskSnapshot: false,
+  hideFromCaseTimeline: false,
+  availableHearingLocations: [],
+  latestInformalHearingPresentationTask: {},
+  canMoveOnDocketSwitch: false,
+  timerEndsAt: null,
+  unscheduledHearingNotes: {},
+  ownedBy: 'BVASYELLOW',
+  daysSinceLastStatusChange: 0,
+  daysSinceBoardIntake: 0,
+  id: '3188',
+  claimant: {},
+  appeal_receipt_date: '2024-09-12'
 };
