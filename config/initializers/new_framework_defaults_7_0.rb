332d3b1a65855a9205ddb2ce9ae7a777799c2957
Jeremy Croteau <3683255+jcroteau@users.noreply.github.com>
2024-11-15 14:11:39 -0500

    🔧 Assume default for `config.active_storage.video_preview_arguments`
    We're not using ActiveStorage, so it is safe to assume the default
    for this setting.

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -9,12 +9,6 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html
 
-# The ActiveStorage video previewer will now use scene change detection to generate
-# better preview images (rather than the previous default of using the first frame
-# of the video).
-# Rails.application.config.active_storage.video_preview_arguments =
-#   "-vf 'select=eq(n\\,0)+eq(key\\,1)+gt(scene\\,0.015),loop=loop=-1:size=2,trim=start_frame=1' -frames:v 1 -f image2"
-
 # Automatically infer `inverse_of` for associations with a scope.
 # Rails.application.config.active_record.automatic_scope_inversing = true
 
