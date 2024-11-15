6233f0bcfa0042bbbc44b2ef938dadb376c3583d
Marc Steele <71673522+msteele96@users.noreply.github.com>
2024-11-15 16:38:27 -0500

    APPEALS-64051 CSRF Updates (#23467)
    * APPEALS-64051 Migrate k6 routes to API controller
    
    * APPEALS-64051 Enable prodtest only concern, add/update rspecs, controller refactor
    
    * APPEALS-64051 Add new endpoint and controller action for CSRF token generation

--- config/routes.rb
+++ config/routes.rb
@@ -457,9 +457,11 @@ Rails.application.routes.draw do
     get "/seeds", to: "test_seeds#seeds" # test seed buttons routes
 
     resources :load_tests, only: [:index]
-    post "/load_tests/user", to: "load_tests#user", as: "user"
-    get "/load_tests/target", to: "load_tests#target", as: "target"
+    get "/load_tests/build_cookie", to: "load_tests#build_cookie", as: "build_cookie"
     post "/load_tests/run_load_tests", to: "load_tests#run_load_tests", as: "run_load_tests"
+
+    post "/load_test_api/user", to: "load_test_api#user", as: "user"
+    get "/load_test_api/target", to: "load_test_api#target", as: "target"
 
     resources :hearings, only: [:index]
 
