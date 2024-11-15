7131c122f14e7f99ca6de0d079f725f572448987
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-15 10:21:03 -0500

    remove commented out old code

--- spec/factories/event_record.rb
+++ spec/factories/event_record.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
-
-# FactoryBot.define do
-#   factory :event_record do
-#     sequence(:id) { |n| n }
-#     event_record_id { 1 }
-#     event_record_type { "" }
-#     created_at { DateTime.now }
-#     updated_at { DateTime.now }
-#     info { {} }
-
-#     trait :person_event_record do
-#       event_record_type { "Person" }
-#     end
-
-#     trait :veteran_event_record do
-#       event_record_type { "Veteran" }
-#     end
-#   end
-# end
-
 FactoryBot.define do
   factory :event_record do
     id { Faker::Number.unique.number(digits: 4) } # Unique id
@@ -78,22 +57,3 @@ FactoryBot.define do
     ssn { Faker::Number.number(digits: 9).to_s } # Random SSN as string
   end
 end
-
-# FactoryBot.define do
-#   factory :event_record do
-#     sequence(:id) { |n| n }
-#     event_record_id { 1 }
-#     event_record_type { "" }
-#     created_at { DateTime.now }
-#     updated_at { DateTime.now }
-#     info { {} }
-
-#     trait :person_event_record do
-#       event_record_type { "Person" }
-#     end
-
-#     trait :veteran_event_record do
-#       event_record_type { "Veteran" }
-#     end
-#   end
-# end
