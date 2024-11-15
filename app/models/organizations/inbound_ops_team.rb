b25910f7ec937216f286cc13afc4ca5306c4e135
sulak-j <95875751+sulak-j@users.noreply.github.com>
2024-11-15 16:02:25 -0600

    more reek errors, but according to discussions with the team, it sounds like this method is in the right place

--- app/models/organizations/inbound_ops_team.rb
+++ app/models/organizations/inbound_ops_team.rb
@@ -29,9 +29,10 @@ class InboundOpsTeam < Organization
 
   # inbound ops can only work Correspondence Tasks
   # inbound ops cannot work tasks not related to an appeal
-  def can_receive_task?(_task)
-    return false unless _task.is_a?(CorrespondenceTask)
+  # :reek:UtilityFunction
+  def can_receive_task?(task)
+    return false unless task.is_a?(CorrespondenceTask)
 
-    !CorrespondenceTask.tasks_not_related_to_an_appeal_names.include?(_task.class.name)
+    !CorrespondenceTask.tasks_not_related_to_an_appeal_names.include?(task.class.name)
   end
 end
