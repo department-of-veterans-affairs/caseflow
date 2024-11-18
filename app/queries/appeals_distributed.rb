5331d74d704f023cc013ef9e8a62ea5a453825f5
Craig Reese <109101548+craigrva@users.noreply.github.com>
2024-11-18 09:18:09 -0600

    refactor

--- app/queries/appeals_distributed.rb
+++ app/queries/appeals_distributed.rb
@@ -66,11 +66,21 @@ class AppealsDistributed
     Appeal.includes(:hearings, :tasks).where(uuid: ama_uuids).map { |appeal| ama_appeal(appeal, distributed_cases) }
   end
 
+  # Pull all the Legacy Appeals found
+  def self.legacy_appeals(legacy_bfkeys, distributed_cases)
+    # AOD is stored separately and doesn't come in as a part of VACOLS::Case relationships
+    aod_appeals = VACOLS::Case.aod(legacy_bfkeys)
+
+    LegacyAppeal.repository.vacols_records_for_appeals(legacy_bfkeys).map do |case_record|
+      legacy_appeal(case_record, aod_appeals[case_record.bfkey], distributed_cases)
+    end
+  end
+
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   # For each AMA appeal get its distributed case and use ActiveRecord relationships to get fields
   def self.ama_appeal(appeal, distributed_cases)
     distributed_case = distributed_cases.filter { |dc| dc.case_id == appeal.uuid }.first
-    hearing_record =
-      appeal.hearings&.filter { |hearing| hearing.disposition = Constants.HEARING_DISPOSITION_TYPES.held }&.first
+    hearing = appeal.hearings&.filter { |h| h.disposition = Constants.HEARING_DISPOSITION_TYPES.held }&.first
     original_task = appeal.cavc? ? ama_cavc_original_judge_task(appeal) : nil
 
     {
@@ -86,8 +96,8 @@ class AppealsDistributed
       genpop_query: distributed_case.genpop_query,
       original_judge: original_task&.assigned_to&.css_id,
       prior_decision_date: original_task&.closed_at,
-      hearing_judge: hearing_record&.judge&.full_name,
-      hearing_date: hearing_record&.scheduled_for,
+      hearing_judge: hearing&.judge&.full_name,
+      hearing_date: hearing&.scheduled_for,
       veteran_file_number: appeal.veteran_file_number,
       veteran_name: appeal.veteran&.name.to_s,
       affinity_start_date: appeal.appeal_affinity&.affinity_start_date,
@@ -95,36 +105,15 @@ class AppealsDistributed
     }
   end
 
-  # Pull all the Legacy Appeals found
-  def self.legacy_appeals(legacy_bfkeys, distributed_cases)
-    # AOD is stored separately and doesn't come in as a part of VACOLS::Case relationships
-    aod_appeals = VACOLS::Case.aod(legacy_bfkeys)
-
-    LegacyAppeal.repository.vacols_records_for_appeals(legacy_bfkeys).map do |case_record|
-      legacy_appeal(case_record, aod_appeals[case_record.bfkey], distributed_cases)
-    end
-  end
-
-  def self.normalize_vacols_date(datetime)
-    LegacyAppeal.repository.normalize_vacols_date(datetime)
-  end
-
   # For each Legacy appeal get its distributed case and use ActiveRecord relationships to get fields
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def self.legacy_appeal(case_record, aod, distributed_cases)
     distributed_case = distributed_cases.filter { |dc| dc.case_id == case_record.bfkey }.first
     correspondent_record = case_record.correspondent
     folder_record = case_record.folder
     judge_mem_id = VACOLS::Case.includes(:folder).where(folder: { tinum: folder_record.tinum },
                                                         bfddec: case_record.bfdpdcn).first.bfmemid
-    original_judge = VACOLS::Staff.find_by(sattyid: judge_mem_id)
-    veteran_name = FullName.new(correspondent_record.snamef, nil, correspondent_record.snamel).to_s
-    appeal_affinity = case_record.appeal_affinity
-
-    relevant_bfkeys =
-      VACOLS::Folder.where(tinum: folder_record.tinum, titrnum: folder_record.titrnum).map(&:ticknum)
-    hearing_record =
-      VACOLS::CaseHearing.where(folder_nr: relevant_bfkeys, hearing_disp: "H").max_by(&:hearing_date)
+    prior_bfkeys = VACOLS::Folder.where(tinum: folder_record.tinum, titrnum: folder_record.titrnum).map(&:ticknum)
+    hearing_record = VACOLS::CaseHearing.where(folder_nr: prior_bfkeys, hearing_disp: "H").max_by(&:hearing_date)
 
     {
       case_id: distributed_case.case_id,
@@ -132,18 +121,18 @@ class AppealsDistributed
       docket: distributed_case.docket,
       aod: aod,
       cavc: case_record.bfac == "7",
-      receipt_date: normalize_vacols_date(case_record.bfd19),
+      receipt_date: LegacyAppeal.repository.normalize_vacols_date(case_record.bfd19),
       ready_for_distribution_at: distributed_case.ready_at,
       distributed_at: distributed_case.created_at,
       distributed_to: distributed_case.distribution&.judge&.css_id,
       genpop_query: distributed_case.genpop_query,
-      original_judge: original_judge&.sdomainid,
+      original_judge: VACOLS::Staff.find_by(sattyid: judge_mem_id)&.sdomainid,
       prior_decision_date: case_record.bfdpdcn,
       hearing_judge: hearing_record&.staff&.sdomainid,
       hearing_date: hearing_record&.hearing_date,
       veteran_file_number: correspondent_record.ssn || case_record.bfcorlid,
-      veteran_name: veteran_name,
-      affinity_start_date: appeal_affinity&.affinity_start_date,
+      veteran_name: FullName.new(correspondent_record.snamef, nil, correspondent_record.snamel).to_s,
+      affinity_start_date: case_record.appeal_affinity&.affinity_start_date,
       affinity_status: "Not yet implemented"
     }
   end
