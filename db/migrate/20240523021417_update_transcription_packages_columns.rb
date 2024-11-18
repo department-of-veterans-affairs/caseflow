c9e911f4e2e4df4fe680c37d58b7301bbd262932
Mounika Myana <173168271+mounikamy@users.noreply.github.com>
2024-11-18 15:51:49 -0500

    Update migrations to support Rails upgrade (#23534)
    Co-authored-by: mounikamy <mounikamy@users.noreply.github.com>

--- db/migrate/20240523021417_update_transcription_packages_columns.rb
+++ db/migrate/20240523021417_update_transcription_packages_columns.rb
@@ -1,4 +1,4 @@
-class UpdateTranscriptionPackagesColumns < Caseflow::Migration
+class UpdateTranscriptionPackagesColumns < ActiveRecord::Migration[6.1]
 
   def change
     add_column :transcription_packages, :date_upload_box, :datetime, comment: "Date of successful delivery to box.com contractor endpoint"
