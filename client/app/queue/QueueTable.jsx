53785ecaa3cb91d095bf730b2989d802f905d185
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2024-11-18 08:39:29 -0600

    more guard

--- client/app/queue/QueueTable.jsx
+++ client/app/queue/QueueTable.jsx
@@ -293,9 +293,9 @@ export default class QueueTable extends React.PureComponent {
       Object.assign(state, defaultSort);
     }
 
-    const completedTab = (new URLSearchParams(this.props?.taskPagesApiEndpoint.split('?')[1])).has('tab', 'completed');
+    const completedTab = (new URLSearchParams(this.props?.taskPagesApiEndpoint?.split('?')[1])).has('tab', 'completed');
 
-    if (completedTab && this.props.isVhaOrg && _.isEmpty(state.filteredByList)) {
+    if (this.props.isVhaOrg && completedTab && _.isEmpty(state.filteredByList)) {
       state.filteredByList = { closedAt: [`last7,${moment().subtract(7, 'days')},`] };
     }
 
