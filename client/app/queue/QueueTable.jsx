e3861d2542bd15dcf06b4264cc9ba54500667b6f
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2024-11-18 08:31:50 -0600

    null guard and rearrange conditional

--- client/app/queue/QueueTable.jsx
+++ client/app/queue/QueueTable.jsx
@@ -293,7 +293,7 @@ export default class QueueTable extends React.PureComponent {
       Object.assign(state, defaultSort);
     }
 
-    const completedTab = (new URLSearchParams(this.props.taskPagesApiEndpoint.split('?')[1])).has('tab', 'completed');
+    const completedTab = (new URLSearchParams(this.props?.taskPagesApiEndpoint.split('?')[1])).has('tab', 'completed');
 
     if (completedTab && this.props.isVhaOrg && _.isEmpty(state.filteredByList)) {
       state.filteredByList = { closedAt: [`last7,${moment().subtract(7, 'days')},`] };
