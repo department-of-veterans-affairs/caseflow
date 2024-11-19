1b494e368d5c1273a6095b7039196fd521812f1e
breedbah <123968373+breedbah@users.noreply.github.com>
2024-11-19 12:47:58 -0500

    fix va_box_service

--- config/initializers/va_box_service.rb
+++ config/initializers/va_box_service.rb
@@ -1,1 +1,5 @@
-VaBoxService = Rails.deploy_env?(:test) ? Fakes::VaBoxService : ExternalApi::VaBoxService
+# config/initializers/va_box_service.rb
+
+Rails.application.reloader.to_prepare do
+  VaBoxService = Rails.deploy_env?(:test) ? Fakes::VaBoxService : ExternalApi::VaBoxService
+end
