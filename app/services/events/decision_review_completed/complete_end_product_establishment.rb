ce9a08d59efc138fc460643e12480ad8a363bf30
Jonathan Tsang <98970951+jtsangVA@users.noreply.github.com>
2024-11-19 11:45:09 -0500

    EPE completed class & error

--- app/services/events/decision_review_completed/complete_end_product_establishment.rb
+++ app/services/events/decision_review_completed/complete_end_product_establishment.rb
@@ -4,6 +4,22 @@
 # This is temporary empty class
 class Events::DecisionReviewCompleted::CompleteEndProductEstablishment
   class << self
-    def process!(params); end
+    def process!(params)
+      event = params[:event]
+      parser = params[:parser]
+      epe = EndProductEstablishment.find_by(
+        reference_id: parser.end_product_establishment_reference_id
+      )
+      epe.update!(
+        code: parser.end_product_establishment_code,
+        development_item_reference_id: parser.epe_development_item_reference_id,
+        synced_status: parser.end_product_establishment_synced_status,
+        last_synced_at: parser.end_product_establishment_last_synced_at
+      )
+      EventRecord.create!(event: event, evented_record: epe)
+      epe
+    rescue StandardError => error
+      raise Caseflow::Error::DecisionReviewCompletedEndProductEstablishmentError, error.message
+    end
   end
 end
