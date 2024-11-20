6a0deec652ad11acd929b10a9181177354f23ebe
Prajwal Amatya <122557351+pamatyatake2@users.noreply.github.com>
2024-11-20 05:22:49 -0700

    adding list of saved search to limitreachedmodal and button functionality

--- client/app/nonComp/components/ReportPage/SaveLimitReachedModal.jsx
+++ client/app/nonComp/components/ReportPage/SaveLimitReachedModal.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'app/components/Modal';
 import COPY from 'app/../COPY';
 import PropTypes from 'prop-types';
+import RadioField from 'app/components/RadioField';
+
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 export const SaveLimitReachedModal = ({ setShowLimitModal }) => {
+
+  const businessLineUrl = useSelector((state) => state.nonComp.businessLineUrl);
+  const userSearches = useSelector((state) => state.savedSearch.fetchedSearches?.userSearches);
+  const [disableDelete, setDisableDelete] = useState(true);
+
+  const history = useHistory();
 
   const handleCancel = () => {
     setShowLimitModal(false);
   };
 
-  const submitForm = () => {
+  const onClickDelete = () => {
     // console.log('this will be updated in next ticket');
   };
 
+  const onRadioSelect = (val) => {
+    setDisableDelete(false);
+  };
+
   const handleRedirect = () => {
-    // this will be handle in another ticket;
+    history.push(`/${businessLineUrl}/searches`);
+    setShowLimitModal(false);
+  };
+
+  const userSearchesList = () => {
+    return (
+      userSearches.map((search) =>
+        <RadioField
+          name="radioFieldSearchGroup"
+          options={[{ displayText: search.name, value: search.id }]}
+          hideLabel
+          onChange={(val) => onRadioSelect(val)}
+          vertical
+          optionsStyling={{ marginLeft: 5 }} />
+      )
+    );
   };
 
   return (
@@ -26,7 +55,8 @@ export const SaveLimitReachedModal = ({ setShowLimitModal }) => {
         },
         { classNames: ['usa-button', 'cf_add_margin'],
           name: 'Delete',
-          onClick: submitForm
+          disabled: disableDelete,
+          onClick: onClickDelete
         },
         { classNames: ['usa-button', 'usa-button-secondary'],
           name: 'View saved searches',
@@ -35,7 +65,8 @@ export const SaveLimitReachedModal = ({ setShowLimitModal }) => {
       ]}
       closeHandler={handleCancel}
     >
-      <p>{COPY.SAVE_LIMIT_REACH_MESSAGE}</p>
+      {COPY.SAVE_LIMIT_REACH_MESSAGE}
+      {userSearchesList()}
     </Modal>);
 };
 
