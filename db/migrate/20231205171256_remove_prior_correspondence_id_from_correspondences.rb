0fa2d9d7876de6f754fe246851b6ec4acd609526
HunJerBAH <99915461+HunJerBAH@users.noreply.github.com>
2024-11-21 15:09:54 -0500

    added down blocks for safe index additions so migrations can go up and down

--- db/migrate/20231205171256_remove_prior_correspondence_id_from_correspondences.rb
+++ db/migrate/20231205171256_remove_prior_correspondence_id_from_correspondences.rb
@@ -1,5 +1,12 @@
 class RemovePriorCorrespondenceIdFromCorrespondences < ActiveRecord::Migration[6.1]
-  def change
+  include Caseflow::Migrations::AddIndexConcurrently
+
+  def up
     safety_assured { remove_column :correspondences, :prior_correspondence_id, :integer }
+  end
+
+  def down
+    add_column :correspondences, :prior_correspondence_id, :integer, null: false
+    add_safe_index :correspondences, [:prior_correspondence_id], name: "index_on_prior_correspondence_id"
   end
 end
