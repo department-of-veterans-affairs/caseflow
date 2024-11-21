7af93524ad7481d359c8f53fc747860b0d7face0
kristeja <112115264+kristeja@users.noreply.github.com>
2024-11-21 08:46:41 -0800

    APPEALS-63207 revert seed data to previous state

--- db/seeds/legacy_appeals_for_vlj_movement_testing.rb
+++ db/seeds/legacy_appeals_for_vlj_movement_testing.rb
@@ -20,12 +20,12 @@ module Seeds
     # place seed methods below
     def scenario_4_seeds
       draft_one = create_scenario_4_veteran(first_name: "Attyswap", last_name: "One")
-      # draft_two = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Two")
-      # draft_three = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Three")
-      # draft_four = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Four")
-      # draft_five = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Five")
-      # draft_six = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Six")
-      # not_swappable = create_scenario_4_veteran(first_name: "NotSwappable", last_name: "Control")
+      draft_two = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Two")
+      draft_three = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Three")
+      draft_four = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Four")
+      draft_five = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Five")
+      draft_six = create_scenario_4_veteran(first_name: "Attyswap", last_name: "Six")
+      not_swappable = create_scenario_4_veteran(first_name: "NotSwappable", last_name: "Control")
 
       # Scenario 1: Draft One (Primary, age-based, CAVC, 2 issues)
       create(:legacy_appeal, :with_veteran, vacols_case: create(
@@ -34,89 +34,89 @@ module Seeds
         :status_active,
         :aod,
         :assigned,
-        user: User.find_by_css_id("BVARDUNKLE"),
+        user: User.find_by_css_id("BVALSHIELDS"),
         assigner: User.find_by_css_id("BVACOTBJUDGE"),
         as_judge_assign_task: true,
         bfcorlid: "#{draft_one.file_number}S",
         case_issues: create_list(:case_issue, 2, :compensation)
       ))
 
-      # # Scenario 2: Draft Two (Non-priority, 1 issue)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_original,
-      #   :status_active,
-      #   :assigned,
-      #   user: User.find_by_css_id("BVALCASPER1"),
-      #   assigner: User.find_by_css_id("BVALCASPER1"),
-      #   as_judge_assign_task: true,
-      #   bfcorlid: "#{draft_two.file_number}S",
-      #   case_issues: create_list(:case_issue, 1, :compensation)
-      # ))
+      # Scenario 2: Draft Two (Non-priority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :assigned,
+        user: User.find_by_css_id("BVALCASPER1"),
+        assigner: User.find_by_css_id("BVALCASPER1"),
+        as_judge_assign_task: true,
+        bfcorlid: "#{draft_two.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      # # Scenario 3: Draft Three (Non-priority, 2 issues)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_original,
-      #   :status_active,
-      #   :assigned,
-      #   user: User.find_by_css_id("BVALSHIELDS"),
-      #   assigner: User.find_by_css_id("BVACOTBJUDGE"),
-      #   as_judge_assign_task: true,
-      #   bfcorlid: "#{draft_three.file_number}S",
-      #   case_issues: create_list(:case_issue, 2, :compensation)
-      # ))
+      # Scenario 3: Draft Three (Non-priority, 2 issues)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :assigned,
+        user: User.find_by_css_id("BVALSHIELDS"),
+        assigner: User.find_by_css_id("BVACOTBJUDGE"),
+        as_judge_assign_task: true,
+        bfcorlid: "#{draft_three.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
 
-      # # Scenario 4: Draft Four (Priority, manually added, 1 issue)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_original,
-      #   :status_active,
-      #   :aod,
-      #   :assigned,
-      #   user: User.find_by_css_id("BVAOTRANTOW"),
-      #   assigner: User.find_by_css_id("BVAGSPORER"),
-      #   as_judge_assign_task: true,
-      #   bfcorlid: "#{draft_four.file_number}S",
-      #   case_issues: create_list(:case_issue, 1, :compensation)
-      # ))
+      # Scenario 4: Draft Four (Priority, manually added, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :aod,
+        :assigned,
+        user: User.find_by_css_id("BVAOTRANTOW"),
+        assigner: User.find_by_css_id("BVAGSPORER"),
+        as_judge_assign_task: true,
+        bfcorlid: "#{draft_four.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      # # Scenario 5: Draft Five (Priority, CAVC, 2 issues)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_cavc_remand,
-      #   :aod,
-      #   :status_active,
-      #   :assigned,
-      #   user: User.find_by_css_id("BVAGBOTSFORD"),
-      #   assigner: User.find_by_css_id("BVAGSPORER"),
-      #   as_judge_assign_task: true,
-      #   bfcorlid: "#{draft_five.file_number}S",
-      #   case_issues: create_list(:case_issue, 2, :compensation)
-      # ))
+      # Scenario 5: Draft Five (Priority, CAVC, 2 issues)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_cavc_remand,
+        :aod,
+        :status_active,
+        :assigned,
+        user: User.find_by_css_id("BVAGBOTSFORD"),
+        assigner: User.find_by_css_id("BVAGSPORER"),
+        as_judge_assign_task: true,
+        bfcorlid: "#{draft_five.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
 
-      # # Scenario 6: Draft Six (Non-priority, 1 issue)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_post_remand,
-      #   :status_active,
-      #   :assigned,
-      #   user: User.find_by_css_id("BVAJWEHNER1"),
-      #   assigner: User.find_by_css_id("BVAGSPORER"),
-      #   as_judge_assign_task: true,
-      #   bfcorlid: "#{draft_six.file_number}S",
-      #   case_issues: create_list(:case_issue, 1, :compensation)
-      # ))
+      # Scenario 6: Draft Six (Non-priority, 1 issue)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_post_remand,
+        :status_active,
+        :assigned,
+        user: User.find_by_css_id("BVAJWEHNER1"),
+        assigner: User.find_by_css_id("BVAGSPORER"),
+        as_judge_assign_task: true,
+        bfcorlid: "#{draft_six.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
 
-      # # Scenario 7: Not Swappable (Non-priority, 1 issue, not distributed yet)
-      # create(:legacy_appeal, :with_veteran, vacols_case: create(
-      #   :case_with_form_9,
-      #   :type_original,
-      #   :status_active,
-      #   :ready_for_distribution,
-      #   bfcorlid: "#{not_swappable.file_number}S",
-      #   case_issues: create_list(:case_issue, 1, :compensation)
-      # ))
+      # Scenario 7: Not Swappable (Non-priority, 1 issue, not distributed yet)
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{not_swappable.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
     end
   end
 end
