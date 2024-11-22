141a5c583f4b5b7ec9da9a09f9cc1634dba1a022
Sean Parker <155660052+seanrpa@users.noreply.github.com>
2024-11-22 10:30:55 -0500

    Reassign to Judge task action along with corresponding error handling and Feature Toggle

--- app/models/legacy_tasks/judge_legacy_decision_review_task.rb
+++ app/models/legacy_tasks/judge_legacy_decision_review_task.rb
@@ -5,6 +5,12 @@ class JudgeLegacyDecisionReviewTask < JudgeLegacyTask
     Constants.TASK_ACTIONS.JUDGE_LEGACY_CHECKOUT.to_h
   end
 
+  def reassign_action
+    if FeatureToggle.enabled?(:legacy_case_movement_vlj_to_vlj_for_evalnsign)
+      Constants.TASK_ACTIONS.REASSIGN_TO_JUDGE.to_h
+    end
+  end
+
   def available_actions(current_user, _role)
     # This must check judge_in_vacols? rather than role as judge, otherwise acting
     # VLJs cannot check out
@@ -12,7 +18,8 @@ class JudgeLegacyDecisionReviewTask < JudgeLegacyTask
 
     [
       Constants.TASK_ACTIONS.ADD_ADMIN_ACTION.to_h,
-      review_action
+      review_action,
+      reassign_action
     ]
   end
 
