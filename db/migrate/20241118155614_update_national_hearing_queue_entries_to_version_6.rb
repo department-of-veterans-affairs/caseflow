522d8ab1219ba60fa2b0f677405645653a35cc43
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-11-22 10:18:06 -0500

    Separate v6 and v7 of the NHQ view definition

--- db/migrate/20241118155614_update_national_hearing_queue_entries_to_version_6.rb
+++ db/migrate/20241118155614_update_national_hearing_queue_entries_to_version_6.rb
@@ -1,8 +1,8 @@
-class UpdateNationalHearingQueueEntriesToVersion6 < ActiveRecord::Migration[6.1]
+class UpdateNationalHearingQueueEntriesToVersion7 < ActiveRecord::Migration[6.1]
   def change
     update_view :national_hearing_queue_entries,
-      version: 6,
-      revert_to_version: 5,
+      version: 7,
+      revert_to_version: 6,
       materialized: true
   end
 end
