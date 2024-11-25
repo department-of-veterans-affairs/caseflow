4939e05e5e77bd3db6c08353f1fcf2cb4548e29b
Davy Wentworth <156839693+davywentwortht2it@users.noreply.github.com>
2024-11-25 08:35:38 -0800

    Allow deep linking to comments from document list.

--- client/app/readerprototype/components/ReaderFooter.jsx
+++ client/app/readerprototype/components/ReaderFooter.jsx
@@ -9,6 +9,7 @@ import { PageArrowLeftIcon } from '../../components/icons/PageArrowLeftIcon';
 import { PageArrowRightIcon } from '../../components/icons/PageArrowRightIcon';
 import { docListIsFiltered, getFilteredDocIds } from '../../reader/selectors';
 import { annotationPlacement } from '../selectors';
+import { isUserEditingText } from '../../reader/utils/user';
 
 const ReaderFooter = ({
   currentPage,
@@ -58,10 +59,12 @@ const ReaderFooter = ({
 
   useEffect(() => {
     const keyHandler = (event) => {
-      if (event.key === 'ArrowLeft' && !isPlacingAnnotation) {
+      const isEditingText = isUserEditingText();
+
+      if (event.key === 'ArrowLeft' && !isPlacingAnnotation && !isEditingText) {
         showPdf(getPrevDocId())();
       }
-      if (event.key === 'ArrowRight' && !isPlacingAnnotation) {
+      if (event.key === 'ArrowRight' && !isPlacingAnnotation && !isEditingText) {
         showPdf(getNextDocId())();
       }
     };
