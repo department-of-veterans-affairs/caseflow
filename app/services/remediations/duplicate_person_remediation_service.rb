9cc36dfa230ae15d17e47bdd2614cb30bf58ca98
Andrew Hadley <50673809+drew-hadley@users.noreply.github.com>
2024-11-25 11:56:49 -0500

    added event_record as initial arg

--- app/services/remediations/duplicate_person_remediation_service.rb
+++ app/services/remediations/duplicate_person_remediation_service.rb
@@ -9,9 +9,12 @@ class Remediations::DuplicatePersonRemediationService
     Notification
   ].freeze
 
-  def initialize(updated_person_id:, duplicate_person_ids:)
+  def initialize(updated_person_id:, duplicate_person_ids:, event_record:)
     @updated_person_id = updated_person_id
     @duplicate_person_ids = duplicate_person_ids
+    @event_record = event_record
+    @dup_persons = Person.where(id: @duplicate_person_ids)
+    @og_person = Person.find_by(id: @updated_person_id)
   end
 
   def remediate!
@@ -24,14 +27,17 @@ class Remediations::DuplicatePersonRemediationService
 
   def find_and_update_records
     begin
-      @dup_persons = Person.where(id: @duplicate_person_ids)
-      og_person = Person.find_by(id: @updated_person_id)
-
       ActiveRecord::Base.transaction do
         ASSOCIATIONS.each do |klass|
           column = klass.column_names.find { |name| name.end_with?("participant_id") }
           records = klass.where("#{column}": @dup_persons.map(&:participant_id))
-          records.update_all("#{column}": og_person.participant_id)
+          records.each do |record|
+            before_data = record.attributes
+            record.update!("#{column}": @og_person.participant_id)
+            add_remediation_audit(remediated_record: record,
+                                  before_data: before_data,
+                                  after_data: record.attributes)
+          end
         end
       end
       true
@@ -39,5 +45,19 @@ class Remediations::DuplicatePersonRemediationService
       Rails.logger.error "an error occured #{error}"
       false
     end
+  end
+
+  def add_remediation_audit(remediated_record:, before_data:, after_data:)
+    EventRemediationAudit.create!(
+      event_record: @event_record,
+      remediated_record_type: remediated_record.class.name,
+      remediated_record_id: remediated_record.id,
+      # remediated_record: remediated_record,
+      info: {
+        remediation_type: "DuplicatePersonRemediationService",
+        after_data: after_data,
+        before_data: before_data
+      }
+    )
   end
 end
