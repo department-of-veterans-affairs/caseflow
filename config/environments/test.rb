1fb5aea12b3e7d24bc7be47bedf4e740bd3366db
Jeremy Croteau <3683255+jcroteau@users.noreply.github.com>
2024-11-25 11:04:35 -0500

    🚧 Temporarily raise errors on deprecation warnings for documentation purposes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :raise
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
