e5f035c1065cd97eea82826630309c89abf69acb
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-11-26 15:29:13 -0500

    Swap foreign table usage out for new functions

---
+++ db/migrate/20241126181511_update_national_hearing_queue_entries_to_version_9.rb
@@ -0,0 +1,5 @@
+class UpdateNationalHearingQueueEntriesToVersion9 < ActiveRecord::Migration[6.1]
+  def change
+    update_view :national_hearing_queue_entries, version: 9, revert_to_version: 8
+  end
+end
