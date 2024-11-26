8e94d03676d070f3acfc9e769539694d4c6048d5
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-26 11:40:49 -0500

    adds validate statement for remediation_type and method

--- app/models/event_remediation_audit.rb
+++ app/models/event_remediation_audit.rb
@@ -6,7 +6,9 @@ class EventRemediationAudit < CaseflowRecord
   store_accessor :info
 
   validate :valid_remediated_record
+  validate :valid_remediation_type
 
+  # rubocop: disable Metrics/MethodLength
   def valid_remediated_record
     unless %w[
       Appeal
@@ -21,9 +23,27 @@ class EventRemediationAudit < CaseflowRecord
       RampElection
       RampRefiling
       SupplementalClaim
+      Claimant
+      DecisionIssue
+      RequestIssue
+      Notification
+      VeteranClaimant
+      DependentClaimant
+      AttorneyClaimant
     ].include?(remediated_record_type)
 
       errors.add(:remediated_record_type, "is not a valid remediated record")
+    end
+  end
+
+  def valid_remediation_type
+    valid_types = [
+      "VeteranRecordRemediationService",
+      "DuplicatePersonRemediationService"
+    ]
+
+    unless valid_types.include?(info["remediation_type"])
+      errors.add(:info, "remediation_type is not valid")
     end
   end
 end
