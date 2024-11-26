7547e173e43c13bae9851c8ee93cc93f57348de4
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-11-26 11:41:55 -0500

    updates factory to create an event and build info hash correctly

--- spec/factories/event_record.rb
+++ spec/factories/event_record.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
-
 FactoryBot.define do
   factory :event_record do
-    id { Faker::Number.unique.number(digits: 4) } # Unique id
-    event_id { Faker::Number.number(digits: 4) }  # Random event ID
-    created_at { Faker::Time.backward(days: 1, period: :evening) } # Time within the last day
-    updated_at { created_at } # Same as created_at for simplicity
-    evented_record_type { "Person" } # The type of the evented record
-    evented_record_id { Faker::Number.number(digits: 4) } # Random record ID
-
+    id { Faker::Number.unique.number(digits: 4) }
+    event { create(:event) } # Automatically create an associated event record
+    event_id { event.id } # Ensure the event_id is set to the created event's ID
+    created_at { Faker::Time.backward(days: 1, period: :evening) }
+    updated_at { created_at }
+    evented_record_type { "Person" }
+    evented_record_id { Faker::Number.number(digits: 4) }
     # Info containing both before_data and record_data (which can differ)
     info do
       {
-        "before_data" => before_data,
-        "record_data" => record_data, # Different from before_data
+        "before_data" => FactoryBot.attributes_for(:before_data), # Use the `attributes_for` method to generate the hash
+        "record_data" => FactoryBot.attributes_for(:record_data), # Use `attributes_for` here as well
         "update_type" => "U" # Update type, can be "U" or "I"
       }
     end
