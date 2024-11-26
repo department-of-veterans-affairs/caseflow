662367daa6866fbd6e4b522ba7b151631b8c7e16
Kevin Ma <104021955+Kevma50287@users.noreply.github.com>
2024-11-26 16:57:56 -0500

    Added submit functionality

--- client/app/queue/components/CorrespondenceReturnToInboundOpsModal.jsx
+++ client/app/queue/components/CorrespondenceReturnToInboundOpsModal.jsx
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-// import { get } from 'lodash';
-
-import { taskById } from '../selectors';
-import { requestPatch } from '../uiReducer/uiActions';
 import { taskActionData } from '../utils';
 import TextareaField from '../../components/TextareaField';
 import COPY from '../../../COPY';
@@ -14,51 +10,36 @@ import QueueFlowModal from './QueueFlowModal';
 import RadioField from '../../components/RadioField';
 import { RETURN_TYPES } from '../constants';
 import {
-  setTaskNotRelatedToAppealBanner,
-  assignTaskToTeam,
-  cancelTaskNotRelatedToAppeal
+  returnTaskToInboundOps
 } from '../correspondence/correspondenceDetailsReducer/correspondenceDetailsActions';
 
 /* eslint-disable camelcase */
 const CorrespondenceReturnToInboundOpsModal = (props) => {
-  const { task_id, correspondenceInfo } = props;
-
-  const taskList = correspondenceInfo?.tasksUnrelatedToAppeal?.find(
-    (task) => parseInt(task.uniqueId, 10) === parseInt(task_id, 10)
-  );
-
-  // const organizations = taskList?.organizations || [];
-  // const organizationOptions = organizations.map((org) => ({
-  //   label: org.label,
-  //   value: org.value
-  // }));
-
+  // Object key values comes from TaskActionRepository.rb
   const taskData = taskActionData(props);
 
   const returnReasonOptions = [
     { displayText: RETURN_TYPES.not_appropriate,
-      value: 'Not appropriate'},
+      value: RETURN_TYPES.not_appropriate },
     { displayText: RETURN_TYPES.clarification_needed,
-      value: 'Clarification needed'},
+      value: RETURN_TYPES.clarification_needed },
     { displayText: RETURN_TYPES.other,
-      value: 'Other'}
+      value: RETURN_TYPES.other }
   ];
 
   const [reasonSelected, setReasonSelected] = useState(null);
 
   const [otherReason, setOtherReason] = useState('');
 
-
-  const handleSetOtherReasonChange = (value) => setOtherReason(value)
-  const handleSetReasonSelectedChange = (value) => setReasonSelected(value)
-
+  const handleSetOtherReasonChange = (value) => setOtherReason(value);
+  const handleSetReasonSelectedChange = (value) => setReasonSelected(value);
 
   const validateForm = () => {
-    if (reasonSelected === "Other") {
-      return (otherReason.length > 0)
+    if (reasonSelected === RETURN_TYPES.other) {
+      return (otherReason.length > 0);
     }
     if (reasonSelected !== null) {
-      return true
+      return true;
     }
 
     return false;
@@ -67,8 +48,23 @@ const CorrespondenceReturnToInboundOpsModal = (props) => {
   // Could possibly use updateCorrespondence method from client/app/queue/components/CorrespondenceAssignTeamModal.jsx
 
   const submit = () => {
-    // Could possibly use submit method from client/app/queue/components/CorrespondenceAssignTeamModal.jsx
-    console.log(reasonSelected);
+    const updatedTask = props.correspondenceInfo.tasksUnrelatedToAppeal.find(
+      (task) => parseInt(props.task_id, 10) === parseInt(task.uniqueId, 10)
+    );
+
+    // We need to send over the task id, the reason selected, and other reason
+    const payload = {
+      data: { return_reason: reasonSelected === RETURN_TYPES.other ? otherReason : reasonSelected }
+    };
+
+    const frontendParams = {
+      taskId: props.task_id,
+      taskName: updatedTask.label,
+      assignedName: updatedTask.assignedTo
+    };
+
+    // We need to hit the path /correspondence/tasks/:task_id/return_to_inbound_ops
+    return props.returnTaskToInboundOps(payload, frontendParams, props.correspondenceInfo);
   };
 
   const modalProps = {
@@ -90,11 +86,11 @@ const CorrespondenceReturnToInboundOpsModal = (props) => {
         name={COPY.CORRESPONDENCE_RETURN_TO_INBOUND_OPS_MODAL_SUBTITLE}
         // required
         options={returnReasonOptions}
-        // value={returnReasonOptions[1]}
+        value={reasonSelected}
         // errorMessage={"Error"}
         onChange={handleSetReasonSelectedChange}
       />
-      { reasonSelected === "Other" && (
+      { reasonSelected === RETURN_TYPES.other && (
         <TextareaField
           name={taskData?.instructions_label ?? COPY.CORRESPONDENCE_RETURN_TO_INBOUND_OPS_MODAL_OTHER_REASON_TITLE}
           id="otherReturnReason"
@@ -108,7 +104,7 @@ const CorrespondenceReturnToInboundOpsModal = (props) => {
 /* eslint-enable camelcase */
 
 CorrespondenceReturnToInboundOpsModal.propTypes = {
-  requestPatch: PropTypes.func,
+  returnTaskToInboundOps: PropTypes.func,
   task: PropTypes.shape({
     assignedTo: PropTypes.shape({
       type: PropTypes.string
@@ -129,18 +125,14 @@ CorrespondenceReturnToInboundOpsModal.propTypes = {
   })
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  task: taskById(state, { taskId: ownProps.task_id }),
+const mapStateToProps = (state) => ({
   taskNotRelatedToAppealBanner: state.correspondenceDetails.bannerAlert,
   correspondenceInfo: state.correspondenceDetails.correspondenceInfo,
   showActionsDropdown: state.correspondenceDetails.showActionsDropdown,
 });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
-  requestPatch,
-  setTaskNotRelatedToAppealBanner,
-  cancelTaskNotRelatedToAppeal,
-  assignTaskToTeam
+  returnTaskToInboundOps
 }, dispatch);
 
 export default withRouter(
