2b25bb4ba80b42eb1f9293d545b3619aef3ecfd7
piedram <maite.piedrayera@va.gov>
2024-11-27 12:04:50 -0500

    Add testing for Submit action for 'No Errors Found' modal

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1667,4 +1667,57 @@ RSpec.describe TasksController, :all_dbs, type: :controller do
       end
     end
   end
+
+  describe "PATH tasks/:id/modal/upload_transcription_vbms" do
+    let(:file_type) { "pdf" }
+    let(:hearing) { create(:hearing) }
+    let(:docket_number) { hearing.docket_number }
+    let(:appeal_id) { hearing.appeal.uuid }
+    let(:file_name) { "#{docket_number}_#{hearing.id}_#{hearing.class}.#{file_type}" }
+    let(:tmp_location) { File.join(Rails.root, "tmp", "transcription_files", file_type, file_name) }
+    let!(:create_transcription_task) do
+      Hearings::TranscriptionFile.create!(
+        hearing_id: hearing.id,
+        hearing_type: "Hearing",
+        file_name: file_name,
+        file_type: file_type.to_s,
+        docket_number: hearing.docket_number,
+        file_status: "Successful upload (AWS)",
+        date_upload_aws: Time.zone.today,
+        aws_link: "vaec-appeals-caseflow-test/transcript_pdf/#{file_name}"
+      )
+    end
+    let(:transcription_file) { Hearings::TranscriptionFile.find_by(file_name: file_name) }
+    let(:user) { create(:default_user) }
+    let(:root_task) { create(:root_task, appeal: hearing.appeal) }
+    let(:distribution_task) { create(:distribution_task, parent: root_task) }
+    let(:parent_hearing_task) { create(:hearing_task, parent: distribution_task) }
+    let!(:review_transcript_task) do
+      ReviewTranscriptTask.create!(
+        appeal: hearing.appeal,
+        parent: root_task,
+        assigned_to: user
+      )
+    end
+
+    let(:params) do
+      {
+        task: {
+          instructions: "testing"
+        },
+        id: review_transcript_task.id.to_s
+      }
+    end
+
+    before do
+      review_transcript_task.update!(status: Constants.TASK_STATUSES.in_progress)
+    end
+
+    subject { patch :upload_transcription_to_vbms, params: params }
+
+    it "updates status ReviewTranscriptTask to completed" do
+      subject
+      expect(response.status).to eq 200
+    end
+  end
 end
