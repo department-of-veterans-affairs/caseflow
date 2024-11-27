54701443b1ac6cca8e140dc3a83ff45907ae7cb4
Kevin Ma <104021955+Kevma50287@users.noreply.github.com>
2024-11-27 15:37:54 -0500

    Move task action to corr mail task

--- app/models/tasks/correspondence/correspondence_mail_tasks/other_motion_correspondence_task.rb
+++ app/models/tasks/correspondence/correspondence_mail_tasks/other_motion_correspondence_task.rb
@@ -4,11 +4,4 @@ class OtherMotionCorrespondenceTask < CorrespondenceMailTask
   def label
     COPY::OTHER_MOTION_MAIL_TASK_LABEL
   end
-
-  def available_actions(user)
-    return [] unless user_can_work_task_correspondence_mail_task(user)
-
-    options = super
-    options.insert(options.length, Constants.TASK_ACTIONS.COR_RETURN_TO_INBOUND_OPS.to_h)
-  end
 end
