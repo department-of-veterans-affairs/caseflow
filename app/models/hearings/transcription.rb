db1f80b2a083e6f76a74a8214e4e755b5760fff5
William Medders <wmedders21@users.noreply.github.com>
2024-11-27 13:49:54 -0600

    change before_validation to before_create and remove unnecessary relationship definition

--- app/models/hearings/transcription.rb
+++ app/models/hearings/transcription.rb
@@ -2,11 +2,10 @@
 
 class Transcription < CaseflowRecord
   belongs_to :hearing, polymorphic: true
-  belongs_to :legacy_hearing, polymorphic: true
   belongs_to :transcription_contractor
   has_many :transcription_files, class_name: "TranscriptionFile"
   belongs_to :transcription_package, foreign_key: :task_number, primary_key: :task_number
-  before_validation :sequence_task_id
+  before_create :sequence_task_id
 
   validates :hearing_type, inclusion: { in: %w[Hearing LegacyHearing] }
   validates :hearing, presence: true
