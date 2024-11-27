ac78b82b1a0607dd9159b9dbd4018de98ad863ff
Ariana Konhilas <109693628+konhilas-ariana@users.noreply.github.com>
2024-11-26 20:02:56 -0500

    akonhilas/APPEALS-63887 (#23427)
    * APPEALS-63887: add roles, update user config, update render for scenarios
    
    * APPEALS-63887: handleScenarioSelect added
    
    * APPEALS-63887: final scenario state updates
    
    * APPEALS-63887: update load test scenarios json target values to match what k6 expects
    
    * APPEALS-63887: updated the state updates for each component to reflect k6 needs
    
    * APPEALS-63887: add custom validation methods and update styling
    
    * APPEALS-63887: Update feature toggle to be correct with default values
    
    * APPEALS-63887: add submit banner and refresh
    
    * APPEALS-63887: remove glamor, add/update snapshots ft func org role scenario tests
    
    * APPEALS-63887: load test form test, snapshots, remove unnecessary awaits
    
    * APPEALS-63887: update tests, fix ApiUtil.post data sent
    
    * APPEALS-63887: whitespace in snapshots
    
    * APPEALS-63887: adding back updated snapshots
    
    * APPEALS-63887: remove snapshots loadtest and loadtestform, update logic no target type
    
    * APPEALS-63887: update snapshots

--- client/app/test/loadTest/ScenarioConfiguration.jsx
+++ client/app/test/loadTest/ScenarioConfiguration.jsx
@@ -1,20 +1,73 @@
-/* eslint-disable max-lines, max-len */
-
 import React, { useState } from 'react';
-
 import SearchableDropdown from '../../components/SearchableDropdown';
 import Checkbox from '../../components/Checkbox';
 import TextField from '../../components/TextField';
 import PropTypes from 'prop-types';
-
 export default function ScenarioConfiguration(props) {
-  const [isChecked, scenarioIsChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const [targetId, setTargetId] = useState('');
 
-  let scenario = props.scenario;
-  let targetType = props.targetType;
+  const scenario = props.scenario;
+  const targetType = props.targetType;
+  const currentState = props.currentState;
+  const updateState = props.updateState;
+  const currentScenarios = currentState.scenarios;
+  const scenariosArray = currentState.scenarios.map((selection) =>
+    Object.keys(selection)[0]
+  );
+  const nullCheck = (target) => {
+    if (target === null) {
+      return null;
+    }
 
-  const onChangeHandle = () => {
-    scenarioIsChecked(!isChecked);
+    return target.value;
+  };
+
+  const handleScenarioSelect = (chosenScenario) => {
+    if (scenariosArray.find((selection) => selection === chosenScenario)) {
+      currentScenarios.splice(scenariosArray.indexOf(chosenScenario), 1);
+      setChecked(false);
+    } else {
+      currentScenarios.push({ [chosenScenario]: {} });
+      setChecked(true);
+    }
+
+    updateState(
+      {
+        ...currentState,
+        scenarios: currentScenarios
+      }
+    );
+  };
+
+  const handleTargetSelect = (chosenTarget, associatedScenario) => {
+    if (chosenTarget === null) {
+      currentScenarios[scenariosArray.indexOf(associatedScenario)] = { [associatedScenario]: {} };
+    } else {
+      currentScenarios[scenariosArray.indexOf(associatedScenario)] = {
+        [associatedScenario]: { targetType: chosenTarget, targetId: '' }
+      };
+    }
+
+    updateState(
+      {
+        ...currentState,
+        scenarios: currentScenarios
+      }
+    );
+  };
+
+  const handleTargetIdSelect = (value) => {
+    setTargetId(value);
+
+    currentScenarios[scenariosArray.indexOf(scenario)][scenario].targetId = value;
+
+    updateState(
+      {
+        ...currentState,
+        scenarios: currentScenarios
+      }
+    );
   };
 
   return (
@@ -22,24 +75,26 @@ export default function ScenarioConfiguration(props) {
       <Checkbox
         label={scenario}
         name={scenario}
-        onChange={() => {
-          onChangeHandle();
-        }}
-        value={isChecked}
+        onChange={() => handleScenarioSelect(scenario)}
+        isChecked={checked}
       />
-      {isChecked && targetType.length > 0 &&
+      {checked && targetType.length > 0 &&
         (<div className="load-test-checkbox-hidden-content">
           <SearchableDropdown
             name={`${scenario}-target-type`}
             label="Target Type"
             options={targetType}
+            onChange={(event) => handleTargetSelect(nullCheck(event), scenario)}
             isClearable
           />
+          {props.errors[scenario] && <div className="error">{props.errors[scenario].target_type}</div>}
           <br />
           <TextField
             name="testTargetID"
             label="Target Type ID"
+            onChange={handleTargetIdSelect}
             optional
+            value={targetId}
           />
         </div>
         )
@@ -50,5 +105,8 @@ export default function ScenarioConfiguration(props) {
 
 ScenarioConfiguration.propTypes = {
   scenario: PropTypes.string,
-  targetType: PropTypes.array
+  targetType: PropTypes.array,
+  currentState: PropTypes.object,
+  updateState: PropTypes.func,
+  errors: PropTypes.object
 };
--- client/test/app/test/LoadTest.test.js
+++ client/test/app/test/LoadTest.test.js
@@ -3,38 +3,45 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from 'app/queue/reducers';
 
 import LoadTest from '../../../app/test/loadTest/LoadTest';
 
-const createStoreWithReducer = (initialState) => {
-  const reducer = (state = initialState) => state;
-
-  return createStore(reducer, compose(applyMiddleware(thunk)));
-};
-
-const renderLoadTest = (props) => {
-  const store = createStoreWithReducer({ components: {} });
-
-  return render(
-    <Provider store={store}>
-      <Router>
-        <LoadTest {...props} />
-      </Router>
-    </Provider>
-  );
-};
-
 describe('LoadTest', () => {
-  it('renders the Test Target Configuration page', async () => {
-    const mockProps = {
-      form_values: {},
-      page: 'Test App',
-      featuresList: ['Toggle1', 'Toggle2', 'Toggle3'],
-    };
+  const defaultProps = {
+    form_values: {
+      all_csum_roles: ['Admin Intake', 'Build HearSched'],
+      all_organizations: ['AOD', 'Alexandra Jackson', 'BVA Intake'],
+      feature_toggles_available: [
+        {
+          name: 'acd_cases_tied_to_judges_no_longer_with_board',
+          default_status: true
+        },
+        {
+          name: 'acd_disable_legacy_lock_ready_appeals',
+          default_status: false
+        }
+      ],
+      functions_available: ['System Admin']
+    }
+  };
 
-    renderLoadTest(mockProps);
-    expect(await screen.findByText(/Test Target Configuration/)).toBeInTheDocument();
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+
+  const setup = (props) =>
+    render(
+      <Provider store={store}>
+        <Router>
+          <LoadTest {...props} />
+        </Router>
+      </Provider>
+    );
+
+  it('renders the Test Target Configuration page', async () => {
+
+    expect(setup(defaultProps)).toMatchSnapshot();
+    expect(await screen.findByText('Welcome to the Caseflow Load Test page')).toBeInTheDocument();
   });
 });
