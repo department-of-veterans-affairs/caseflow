71b7e01f9da4244578d6a39a7e332bf468355170
Michael Bidwell <122634362+mchbidwell@users.noreply.github.com>
2024-12-02 12:52:38 -0600

    Update DatePicker.test.js

--- client/test/app/components/DatePicker.test.js
+++ client/test/app/components/DatePicker.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import COPY from '../../../COPY';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import COPY from '../../../COPY';
 
 import {
   selectFromDropdown,
