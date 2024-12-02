1546c11068478ed34960b1b84fe96a418127855d
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-12-02 11:54:03 -0500

    updates remediate! and dup_fix methods with failure logic

--- app/services/remediations/veteran_record_remediation_service.rb
+++ app/services/remediations/veteran_record_remediation_service.rb
@@ -11,18 +11,28 @@ class Remediations::VeteranRecordRemediationService
   end
 
   def remediate!
-    real_v = Veteran.find_by_file_number(@after_fn)
-    @dups = Veteran.where(ssn: real_v.ssn).reject { |v| v.id == real_v.id }
+    begin
+      real_v = Veteran.find_by_file_number(@after_fn)
+      @dups = Veteran.where(ssn: real_v.ssn).reject { |v| v.id == real_v.id }
 
-    if @dups.any?
-      # If there are duplicates, run dup_fix on @after_fn
-      if dup_fix(@after_fn)
-        @dups.each(&:destroy!)
+      if @dups.any?
+        # If there are duplicates, run dup_fix on @after_fn
+        if dup_fix(@after_fn)
+          @dups.each(&:destroy!)
+        else
+          Rails.logger.error "dup_fix failed"
+          SlackService.new.send_notification("Job failed during record update", "Error in #{self.class.name}")
+          false
+        end
+      else
+        # Otherwise, fix veteran records normally
+        fix_vet_records
       end
-
-    else
-      # Otherwise, fix veteran records normally
-      fix_vet_records
+    rescue StandardError => error
+      # This will catch any errors that happen during the execution of find_and_update_records or subsequent operations
+      Rails.logger.error "An error occurred during remediation: #{error.message}"
+      SlackService.new.send_notification("Job failed during remediation: #{error.message}", "Error in #{self.class.name}")
+      false # Indicate failure
     end
   end
 
@@ -32,10 +42,12 @@ class Remediations::VeteranRecordRemediationService
     begin
       duplicate_veterans_collections = @dups.flat_map { |dup| grab_collections(dup.file_number) }
       update_records!(duplicate_veterans_collections, file_number)
-      SlackService.new.send_notification("Job completed successfully", self.class.name)
+      # SlackService.new.send_notification("Job completed successfully", self.class.name)
+      true
     rescue StandardError => error
-      SlackService.new.send_notification("Job failed with error: #{error.message}", "Error in #{self.class.name}")
       Rails.logger.error "an error occured #{error}"
+      SlackService.new.send_notification("Job failed with error: #{error.message}", "Error in #{self.class.name}")
+      false # Indicate failure
       # sentry log / metabase dashboard
     end
   end
