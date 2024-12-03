0885c1bb819a22fd246e396cec0223175c6cb20f
Dani <60626984+dcoleman21@users.noreply.github.com>
2024-12-03 13:01:08 -0500

    fixes lint error

--- app/services/remediations/veteran_record_remediation_service.rb
+++ app/services/remediations/veteran_record_remediation_service.rb
@@ -28,7 +28,8 @@ class Remediations::VeteranRecordRemediationService
     rescue StandardError => error
       # This will catch any errors that happen during the execution of find_and_update_records or subsequent operations
       Rails.logger.error "An error occurred during remediation: #{error.message}"
-      SlackService.new.send_notification("Job failed during remediation: #{error.message}", "Error in #{self.class.name}")
+      SlackService.new.send_notification("Job failed during remediation: #{error.message}",
+                                         "Error in #{self.class.name}")
       false # Indicate failure
     end
   end
