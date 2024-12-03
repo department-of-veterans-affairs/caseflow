2abfcb8ea80a93d8ae5928462757dca1a62a8a8c
Brian Bommarito <6898085+bbommarito@users.noreply.github.com>
2024-12-03 09:46:02 -0500

    Modifying tests to new "dumb" component

--- client/test/app/reader/BandwidthAlert-test.js
+++ client/test/app/reader/BandwidthAlert-test.js
@@ -2,40 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import BandwidthAlert from '../../../app/reader/BandwidthAlert';
 
-// variables being defined are in mbps
-const bandwidthUnderThreshold = 1.0;
-const bandwidthOverThreshold = 2.0;
-
-const mockNavigationConnection = (downlink) => {
-  Object.defineProperty(global.navigator, 'connection', {
-    value: {
-      downlink,
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn()
-    },
-    writable: true
-  });
-};
-
 describe('BandwidthAlert', () => {
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
 
-  it('should render warning alert if downlink is below 1.5', () => {
-    mockNavigationConnection(bandwidthUnderThreshold);
+  it('should render warning alert if displayBanner is true', () => {
 
-    render(<BandwidthAlert />);
+    render(<BandwidthAlert displayBanner />);
 
     const alertMessage = screen.getByText(/You may experience slower downloading times/i);
 
     expect(alertMessage).toBeInTheDocument();
   });
 
-  it('should not render alert if downlink is above 1.5', () => {
-    mockNavigationConnection(bandwidthOverThreshold);
-
-    render(<BandwidthAlert />);
+  it('should not render alert if displayBanner is false', () => {
+    render(<BandwidthAlert displayBanner={false} />);
 
     const alertMessage = screen.queryByText(/You may experience slower downloading times/i);
 
