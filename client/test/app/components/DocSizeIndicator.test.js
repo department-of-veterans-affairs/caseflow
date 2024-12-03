4d02eb65fdb4257a182eaf3aa382e28628257823
Brian Bommarito <6898085+bbommarito@users.noreply.github.com>
2024-12-03 10:24:10 -0500

    Fixing failing tests.

--- client/test/app/components/DocSizeIndicator.test.js
+++ client/test/app/components/DocSizeIndicator.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import DocSizeIndicator from '../../../app/reader/DocSizeIndicator';
 
+const bandwidthFunction = jest.fn();
+
 it('shows file size', () => {
   const connectionSpeed = 1;
   const downloadSpeedInBytes = connectionSpeed * 125000;
-  const { container } = render(<DocSizeIndicator docSize="1024" browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} />);
+  const { container } = render(<DocSizeIndicator docSize="1024" browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} enableBandwidthBanner={bandwidthFunction} />);
 
   expect(container).toHaveTextContent('1 kB');
 });
@@ -13,14 +15,14 @@ it('shows file size', () => {
 it('handles empty strings', () => {
   const connectionSpeed = 1;
   const downloadSpeedInBytes = connectionSpeed * 125000;
-  const { container } = render(<DocSizeIndicator docSize="" browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} />);
+  const { container } = render(<DocSizeIndicator docSize="" browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} enableBandwidthBanner={bandwidthFunction} />);
 
   expect(container).toHaveTextContent('0 B');
 });
 it('handles null', () => {
   const connectionSpeed = 1;
   const downloadSpeedInBytes = connectionSpeed * 125000;
-  const { container } = render(<DocSizeIndicator docSize={null} browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} />);
+  const { container } = render(<DocSizeIndicator docSize={null} browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} enableBandwidthBanner={bandwidthFunction} />);
 
   expect(container).toHaveTextContent('0 B');
 });
@@ -29,7 +31,7 @@ it('shows a file size warning when file size is large', () => {
   const downloadSpeedInBytes = connectionSpeed * 125000;
   const fileSize = downloadSpeedInBytes * 16;
 
-  const { getByTitle } = render(<DocSizeIndicator docSize={fileSize} browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} />);
+  const { getByTitle } = render(<DocSizeIndicator docSize={fileSize} browserSpeedInBytes={downloadSpeedInBytes} warningThreshold={1} enableBandwidthBanner={bandwidthFunction} />);
 
   expect(getByTitle('Large File Warning')).toBeInTheDocument();
 });
@@ -38,7 +40,7 @@ it('does not show a file size warning when file size is not large', () => {
   const downloadSpeedInBytes = connectionSpeed * 125000;
   const fileSize = downloadSpeedInBytes * 15;
 
-  const { queryByTitle } = render(<DocSizeIndicator docSize={fileSize} browserSpeedInBytes={downloadSpeedInBytes} />);
+  const { queryByTitle } = render(<DocSizeIndicator docSize={fileSize} browserSpeedInBytes={downloadSpeedInBytes} enableBandwidthBanner={bandwidthFunction} />);
 
   expect(queryByTitle('Large File Warning')).not.toBeInTheDocument();
 });
