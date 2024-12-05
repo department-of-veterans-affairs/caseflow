18c0deb5e91b5d19db9052c5f17a3d5d53bedf81
Jeremy Croteau <3683255+jcroteau@users.noreply.github.com>
2024-12-04 20:11:54 -0500

    🔧 Update disallowed deprecation warnings
    Add the following to disallowed deprecation warnings:
    
      ActiveModel::Errors#values is deprecated and will be removed in Rails 7.0.
      To achieve the same use: errors.map { |error| error.message }

--- lib/deprecation_warnings.rb
+++ lib/deprecation_warnings.rb
@@ -14,7 +14,8 @@ module DeprecationWarnings
     /Calling `delete` to an ActiveModel\:\:Errors messages hash is deprecated/,
     /Calling `<<` to an ActiveModel\:\:Errors message array in order to add an error is deprecated/,
     /ActiveModel\:\:Errors#to_xml is deprecated/,
-    /ActiveModel\:\:Errors#keys is deprecated/
+    /ActiveModel\:\:Errors#keys is deprecated/,
+    /ActiveModel\:\:Errors#values is deprecated/
   ].freeze
 
   # Regular expressions for deprecation warnings that should raise an exception on detection
