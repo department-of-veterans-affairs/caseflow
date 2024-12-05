890b499a3f56370ac06d4edda26713fd6add335a
Craig Reese <109101548+craigrva@users.noreply.github.com>
2024-12-05 08:33:28 -0600

    APPEALS-64905: Add seed data for testing scenario 2 (#23681)
    * seed data
    
    * fix name for appeal 9 veteran

--- db/seeds/legacy_appeals_for_vlj_movement_testing.rb
+++ db/seeds/legacy_appeals_for_vlj_movement_testing.rb
@@ -4,14 +4,35 @@ module Seeds
   class LegacyAppealsForVljMovementTesting < Base
     def initialize
       RequestStore[:current_user] = User.system_user
+      initial_scenario_2_id_values
       initial_scenario_6_id_values
     end
 
     def seed!
+      scenario_2_seeds
       scenario_6_seeds
     end
 
     private
+
+    def initial_scenario_2_id_values
+      @scenario_2_file_number ||= 123_000_000
+      @scenario_2_participant_id ||= 123_000_000
+      while Veteran.find_by(file_number: format("%<n>09d", n: @scenario_2_file_number + 1))
+        @scenario_2_file_number += 2000
+        @scenario_2_participant_id += 2000
+      end
+    end
+
+    def create_scenario_2_veteran(options = {})
+      @scenario_2_file_number += 1
+      @scenario_2_participant_id += 1
+      params = {
+        file_number: format("%<n>09d", n: @scenario_2_file_number),
+        participant_id: format("%<n>09d", n: @scenario_2_participant_id)
+      }
+      create(:veteran, params.merge(options))
+    end
 
     def initial_scenario_6_id_values
       @scenario_6_file_number ||= 700_000_000
@@ -30,6 +51,113 @@ module Seeds
         participant_id: format("%<n>09d", n: @scenario_6_participant_id)
       }
       create(:veteran, params.merge(options))
+    end
+
+    def scenario_2_seeds
+      s2_vet_1 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "One")
+      s2_vet_2 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Two")
+      s2_vet_3 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Three")
+      s2_vet_4 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Four")
+      s2_vet_5 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Five")
+      s2_vet_6 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Six")
+      s2_vet_7 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "CAVC")
+      s2_vet_8 = create_scenario_2_veteran(first_name: "HearNoBlock", last_name: "Eight")
+      s2_vet_9 = create_scenario_2_veteran(first_name: "HearBlock", last_name: "Nine")
+      s2_vet_10 = create_scenario_2_veteran(first_name: "DoesNot", last_name: "Qualify")
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :aod,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_1.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :aod,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_2.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_3.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_4.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_5.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :aod,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_6.file_number}S",
+        case_issues: create_list(:case_issue, 2, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_cavc_remand,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_7.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :ready_for_distribution,
+        bfcorlid: "#{s2_vet_8.file_number}S",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_schedule_hearing_tasks, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        :aod,
+        bfcorlid: "#{s2_vet_9.file_number}S",
+        bfcurloc: "CASEFLOW",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
+
+      create(:legacy_appeal, :with_veteran, vacols_case: create(
+        :case_with_form_9,
+        :type_original,
+        :status_active,
+        bfcorlid: "#{s2_vet_10.file_number}S",
+        bfcurloc: "33",
+        case_issues: create_list(:case_issue, 1, :compensation)
+      ))
     end
 
     def scenario_6_seeds
