ca43eff2f4a44a6dc994d12edd82c4289289db52
Craig Reese <109101548+craigrva@users.noreply.github.com>
2024-12-05 15:17:46 -0600

    added task for UI

--- app/models/queues/legacy_work_queue.rb
+++ app/models/queues/legacy_work_queue.rb
@@ -8,9 +8,9 @@ class LegacyWorkQueue
       tasks_from_vacols_tasks(vacols_tasks, user)
     end
 
-    def tasks_by_appeal_id(appeal_id)
+    def tasks_by_appeal_id(appeal_id, current_user = nil)
       vacols_tasks = repository.tasks_for_appeal(appeal_id)
-      tasks_from_vacols_tasks(vacols_tasks)
+      tasks_from_vacols_tasks(vacols_tasks, current_user)
     end
 
     def repository
@@ -19,12 +19,17 @@ class LegacyWorkQueue
 
     private
 
+    # rubocop:disable Metrics/CyclomaticComplexity
     def tasks_from_vacols_tasks(vacols_tasks, user = nil)
       return [] if vacols_tasks.empty?
 
       vacols_appeals = repository.appeals_by_vacols_ids(vacols_tasks.map(&:vacols_id))
 
       vacols_tasks.zip(vacols_appeals).map do |task, appeal|
+        if %w[81 83].include?(task["current_location"]) && user&.can_act_on_behalf_of_judges?
+          next AssignmentLegacyTask.from_vacols(task, appeal, user)
+        end
+
         user = validate_or_create_user(user, task.assigned_to_css_id)
 
         task_class = AttorneyLegacyTask
@@ -38,6 +43,7 @@ class LegacyWorkQueue
         task_class.from_vacols(task, appeal, user)
       end
     end
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     def validate_or_create_user(user, css_id)
       if css_id && (css_id == user&.css_id) && (user&.station_id == User::BOARD_STATION_ID)
