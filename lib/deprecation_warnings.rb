6b2d23f59ba76685d7026adc8b3d2acf3483e563
Jeremy Croteau <3683255+jcroteau@users.noreply.github.com>
2024-12-04 19:50:24 -0500

    🔧 Update disallowed deprecation warnings
    Add the following to disallowed deprecation warnings:
    
      ActiveModel::Errors#to_xml is deprecated and will be removed in Rails 7.0.

--- lib/deprecation_warnings.rb
+++ lib/deprecation_warnings.rb
@@ -12,7 +12,8 @@ module DeprecationWarnings
     /Calling `\[\]=` to an ActiveModel\:\:Errors is deprecated/,
     /Calling `clear` to an ActiveModel\:\:Errors message array in order to delete all errors is deprecated/,
     /Calling `delete` to an ActiveModel\:\:Errors messages hash is deprecated/,
-    /Calling `<<` to an ActiveModel\:\:Errors message array in order to add an error is deprecated/
+    /Calling `<<` to an ActiveModel\:\:Errors message array in order to add an error is deprecated/,
+    /ActiveModel\:\:Errors#to_xml is deprecated/
   ].freeze
 
   # Regular expressions for deprecation warnings that should raise an exception on detection
