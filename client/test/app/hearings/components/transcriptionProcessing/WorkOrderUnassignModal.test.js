c97b0302fc06fad57797d43478d7badab14841d9
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2024-12-05 14:17:15 -0500

    Fix a Jest test

--- client/test/app/hearings/components/transcriptionProcessing/WorkOrderUnassignModal.test.js
+++ client/test/app/hearings/components/transcriptionProcessing/WorkOrderUnassignModal.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import WorkOrderUnassignModal
   from '../../../../../app/hearings/components/transcriptionProcessing/WorkOrderUnassignModal';
 import ApiUtil from 'app/util/ApiUtil';
