1d5c20f10906e73422bd559327eb3d6e143ae884
vinner57 <Vineelkumar.sonnathi@va.gov>
2024-12-06 10:22:32 -0500

    fix rubocop erors

--- spec/controllers/correspondence_details_controller_spec.rb
+++ spec/controllers/correspondence_details_controller_spec.rb
@@ -135,7 +135,7 @@ RSpec.describe CorrespondenceDetailsController, :all_dbs, type: :controller do
     context "when correspondence status is action_required" do
       it "does not redirect" do
         allow(root_task).to receive(:correspondence_status)
-                          .and_return(Constants.CORRESPONDENCE_STATUSES.action_required)
+          .and_return(Constants.CORRESPONDENCE_STATUSES.action_required)
         controller.send(:correspondence_details_access)
         expect(controller).not_to have_received(:access_redirect)
       end
