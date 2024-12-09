df4f7d7d0c0a8119f01cd265bcae447c3ea54f2c
Ryan Marshall <rnmarshall1993@gmail.com>
2024-12-09 16:15:20 -0500

    resolved cyclomatic complexity linter warning.

--- app/models/cmp_document.rb
+++ app/models/cmp_document.rb
@@ -15,25 +15,27 @@ class CmpDocument < ApplicationRecord
   def date_of_receipt_must_be_a_date
     # Use the magic <attribute>_before_type_cast accessor to get the raw value
     before_val = date_of_receipt_before_type_cast
-
-    if before_val.blank?
-      errors.add(:date_of_receipt, :blank)
-      return
-    end
-
-    # For yyyy-mm-dd format:
-    # Require non-zero first digit; require the exact number of digits for each.
-    if !before_val.is_a?(String) && !/^[1-9]{1}\d{3}-\d{2}-\d{2}$/.match?(before_val.strftime("%Y-%m-%d"))
-      errors.add(:date_of_receipt, "date_of_receipt must use the format yyyy-mm-dd")
-      return
-    end
-
-    if before_val.is_a?(String) && !/^[1-9]{1}\d{3}-\d{2}-\d{2}$/.match?(before_val)
-      errors.add(:date_of_receipt, "date_of_receipt must use the format yyyy-mm-dd")
-      return
-    end
-
+    check_value_present(before_val)
+    check_date_format(before_val)
   rescue Date::Error
     errors.add(:date_of_receipt, "date_of_receipt must be a valid date")
+  end
+
+  private
+
+  def check_value_present(date)
+    if date.blank?
+      errors.add(:date_of_receipt, :blank)
+    end
+  end
+
+  def check_date_format(date)
+    # For yyyy-mm-dd format:
+    # Require non-zero first digit; require the exact number of digits for each.
+    if !date.is_a?(String) && !/^[1-9]{1}\d{3}-\d{2}-\d{2}$/.match?(date.strftime("%Y-%m-%d"))
+      errors.add(:date_of_receipt, "date_of_receipt must use the format yyyy-mm-dd")
+    elsif date.is_a?(String) && !/^[1-9]{1}\d{3}-\d{2}-\d{2}$/.match?(date)
+      errors.add(:date_of_receipt, "date_of_receipt must use the format yyyy-mm-dd")
+    end
   end
 end
