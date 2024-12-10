998dff30896c1a77a866acb8796446de4bfca99c
Prerna Devulapalli <132498915+prernadevbah@users.noreply.github.com>
2024-12-10 13:31:23 -0800

    APPEALS-65175 Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -2679,29 +2679,6 @@ ActiveRecord::Schema.define(version: 2024_12_10_095300) do
       END $function$
   SQL
 
-
-  create_trigger :appeal_states_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER appeal_states_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.appeal_states FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_appeal_states_audit()
-  SQL
-  create_trigger :priority_end_product_sync_queue_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER priority_end_product_sync_queue_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.priority_end_product_sync_queue FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_priority_end_product_sync_queue_audit()
-  SQL
-  create_trigger :vbms_communication_packages_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER vbms_communication_packages_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.vbms_communication_packages FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_vbms_communication_packages_audit()
-  SQL
-  create_trigger :vbms_distribution_destinations_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER vbms_distribution_destinations_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.vbms_distribution_destinations FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_vbms_distribution_destinations_audit()
-  SQL
-  create_trigger :vbms_distributions_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER vbms_distributions_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.vbms_distributions FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_vbms_distributions_audit()
-  SQL
-  create_trigger :vbms_uploaded_documents_audit_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER vbms_uploaded_documents_audit_trigger AFTER INSERT OR DELETE OR UPDATE ON public.vbms_uploaded_documents FOR EACH ROW EXECUTE FUNCTION caseflow_audit.add_row_to_vbms_uploaded_documents_audit()
-  SQL
-  create_trigger :update_claim_status_trigger, sql_definition: <<-SQL
-      CREATE TRIGGER update_claim_status_trigger AFTER INSERT OR UPDATE ON public.vbms_ext_claim FOR EACH ROW EXECUTE FUNCTION update_claim_status_trigger_function()
-  SQL
-
   create_view "national_hearing_queue_entries", materialized: true, sql_definition: <<-SQL
       WITH latest_cutoff_date AS (
            SELECT schedulable_cutoff_dates.cutoff_date
