3401056befe03b8151cd18588d5d144d7f4025c1
Ryan Marshall <rnmarshall1993@gmail.com>
2024-12-10 16:44:43 -0500

    removed migration

--- db/migrate/20241202132509_add_cmp_packet_id_to_cmp_mail_packet.rb
+++
@@ -1,8 +0,0 @@
-class AddCmpPacketIdToCmpMailPacket < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-
-  def change
-    add_reference :cmp_mail_packets, :cmp_mail_packet, foreign_key: true, index: false, null: true
-    add_index :cmp_mail_packets, :cmp_mail_packet_id, algorithm: :concurrently
-  end
-end
