902259775ac3f07b60a9e5716f2563b69fdadf56
Jonathan Cohen <121630615+JCohDev@users.noreply.github.com>
2024-12-10 14:49:55 -0500

    APPEALS-59475 materialized keyword added to the newly added migration, with the value of true.

--- db/migrate/20241211095300_update_national_hearing_queue_entries_to_version_13.rb
+++ db/migrate/20241211095300_update_national_hearing_queue_entries_to_version_13.rb
@@ -1,5 +1,8 @@
 class UpdateNationalHearingQueueEntriesToVersion13 < ActiveRecord::Migration[6.1]
   def change
-    update_view :national_hearing_queue_entries, version: 13, revert_to_version: 12
+    update_view :national_hearing_queue_entries,
+                version: 13,
+                revert_to_version: 12,
+                materialized: true
   end
 end
