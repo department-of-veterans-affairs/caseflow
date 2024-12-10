81dbe1e85b6b3359a4de75f4b2669acafb630884
Prajwal Amatya <122557351+pamatyatake2@users.noreply.github.com>
2024-12-10 02:54:30 -0700

    removing skip to see if they fail in GHA

--- spec/models/vacols/aoj_case_docket_spec.rb
+++ spec/models/vacols/aoj_case_docket_spec.rb
@@ -350,9 +350,9 @@ describe VACOLS::AojCaseDocket, :all_dbs do
       # We do not provide a range if this feature toggle is enabled
       before { FeatureToggle.disable!(:acd_distribute_by_docket_date) }
 
-      context "when the docket number is pre-y2k", skip: "flaky" do
+      context "when the docket number is pre-y2k" do
         let(:another_nonpriority_ready_case_docket_number) { "9901002" }
-        it "correctly orders the docket", skip: "flaky" do
+        it "correctly orders the docket" do
           expect(subject.count).to eq(1)
           expect(subject.first["bfkey"]).to eq another_nonpriority_ready_case.bfkey
           expect(nonpriority_ready_case.reload.bfcurloc).to eq("81")
@@ -436,7 +436,7 @@ describe VACOLS::AojCaseDocket, :all_dbs do
               .and_return(VACOLS::AojCaseDocket::HEARING_BACKLOG_LIMIT + number_of_cases_over_backlog)
           end
 
-          it "only distributes the one case to get back down to 30", skip: "flaky" do
+          it "only distributes the one case to get back down to 30" do
             expect(subject.count).to eq(number_of_cases_over_backlog)
             expect(subject.first["bfkey"]).to eq nonpriority_ready_case.bfkey
             expect(nonpriority_ready_case.reload.bfcurloc).to eq(judge.vacols_uniq_id)
