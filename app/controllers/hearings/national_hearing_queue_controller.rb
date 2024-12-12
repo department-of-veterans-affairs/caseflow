801f903f22219253a25ef79fcc02cf4a2bae5bf1
lauren-e-thompson <138795438+lauren-e-thompson@users.noreply.github.com>
2024-12-12 16:53:41 -0500

    lthompson/APPEALS-65140 (#23698)
    * Create update_cutoff_date controller action
    
    * Raise appropriate errors and statuses
    
    * Add new route to accept a POST request at /national_hearing_queue/cutoff_date
    
    * Add error code to rendered errors and log errors
    
    * Create rspec tests and adjust params
    
    * Remove duplicate module from routes file
    
    * Oops! I pushed up too many files in the last commit
    
    * Remove CSRF disabling from testing
    
    ---------
    
    Co-authored-by: Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
    Co-authored-by: Matthew Thornton <ThorntonMatthew@users.noreply.github.com>

--- app/controllers/hearings/national_hearing_queue_controller.rb
+++ app/controllers/hearings/national_hearing_queue_controller.rb
@@ -17,4 +17,56 @@ class Hearings::NationalHearingQueueController < ApplicationController
 
     render json: { cutoff_date: date, user_can_edit: false }
   end
+
+  def update_cutoff_date
+    params.require(:cutoff_date)
+
+    begin
+      Date.iso8601(params[:cutoff_date])
+
+      record = SchedulableCutoffDate.create!(
+        cutoff_date: params[:cutoff_date],
+        created_by_id: current_user.id,
+        created_at: Time.zone.now
+      )
+    rescue Date::Error => error
+      log_error(error)
+      return invalid_date
+    rescue StandardError => error
+      log_error(error)
+      return does_not_persist
+    end
+
+    render json: { cutoff_date_record: record }, status: :created
+  end
+
+  private
+
+  def invalid_date
+    render json: {
+      "errors": [
+        "status": "400",
+        "title": "Invalid Date",
+        "detail": "Please enter a valid date",
+        "error_code": error_uuid
+      ]
+    }, status: :bad_request
+  end
+
+  def does_not_persist
+    render json: {
+      "errors": [
+        "status": "500",
+        "title": "Internal Service Error",
+        "detail": "Cutoff date unable to persist, try again at a later time",
+        "error_code": error_uuid
+      ]
+    }, status: :internal_server_error
+  end
+
+  def log_error(error)
+    Rails.logger.error(error)
+    Rails.logger.error(error.backtrace.join("\n"))
+    Rails.logger.error(error_uuid)
+  end
 end
