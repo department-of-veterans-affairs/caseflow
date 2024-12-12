7b2b0dfbdb36c1b20ab10399eb84a131001e3171
Jonathan Hoang <111081469+jonathanh-va@users.noreply.github.com>
2024-12-11 19:30:20 -0600

    revert initializer

--- config/initializers/vbms_service.rb
+++ config/initializers/vbms_service.rb
@@ -2,7 +2,7 @@ Rails.application.config.to_prepare do
   VBMSService = (!ApplicationController.dependencies_faked? ? ExternalApi::VBMSService : Fakes::VBMSService)
 
   if ApplicationController.dependencies_faked?
-    VBMSService.manifest_vbms_fetched_at = VBMSService.manifest_vbms_fetched_at.try(:utc).try(:strftime, "%FT%T.%LZ")
-    VBMSService.manifest_vva_fetched_at = VBMSService.manifest_vva_fetched_at.try(:utc).try(:strftime, "%FT%T.%LZ")
+    VBMSService.manifest_vbms_fetched_at = Time.zone.now
+    VBMSService.manifest_vva_fetched_at = Time.zone.now
   end
 end
