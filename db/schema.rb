46e928ed37f3b5ad7ad0f5908bc9eb6fa9155f5a
Matt Ray <108031363+mattray-gov@users.noreply.github.com>
2024-12-13 13:05:00 -0600

    adding schema dont merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_12_181521) do
+ActiveRecord::Schema.define(version: 2024_12_13_185004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
